4e0cc2164e728894e261d05a1b3f41624078b452
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-20 06:36:35 -0400

    Update libmpfr from 3.1.2 to 3.1.3

--- packages/libmpfr/build.sh
+++ packages/libmpfr/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.mpfr.org/
 TERMUX_PKG_DESCRIPTION="C library for multiple-precision floating-point computations with correct rounding"
 TERMUX_PKG_DEPENDS="libgmp"
-TERMUX_PKG_VERSION=3.1.2
+TERMUX_PKG_VERSION=3.1.3
 TERMUX_PKG_SRCURL=http://www.mpfr.org/mpfr-current/mpfr-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_header_locale_h=no"
