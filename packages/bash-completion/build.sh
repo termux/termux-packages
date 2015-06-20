0f99c9d9a3b012ee9e02c981ddac50ef07c2d3d0
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-20 09:11:37 -0400

    Add bash-completion

---
+++ packages/bash-completion/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://bash-completion.alioth.debian.org/
+TERMUX_PKG_DESCRIPTION="Programmable completion for the bash shell"
+TERMUX_PKG_VERSION=2.1
+TERMUX_PKG_SRCURL=https://bash-completion.alioth.debian.org/files/bash-completion-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_DEPENDS="bash"
