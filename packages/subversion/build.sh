25a5638bb0aafd2646530d9ad81c564c835b9b36
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-21 06:34:36 -0400

    Remove outdated workaround for building subversion

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -4,6 +4,3 @@ TERMUX_PKG_VERSION=1.8.13
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl --without-libmagic"
-
-# subversion compiles with std=c90, but the NDK asm/byteorder.h uses C99 inline:
-CFLAGS+=" -std=c99"
