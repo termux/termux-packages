f44d3d210ecd91465b248b7a3d1b831a71f52d23
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-21 07:25:26 -0400

    Update vtutils to 0.5

--- packages/vtutils/build.sh
+++ packages/vtutils/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/fornwall/vtutils
 TERMUX_PKG_DESCRIPTION="Utilities for testing and working with virtual terminals"
-TERMUX_PKG_VERSION=0.4
+TERMUX_PKG_VERSION=0.5
 TERMUX_PKG_SRCURL=http://fornwall.net/releases/vtutils/vtutils-${TERMUX_PKG_VERSION}.tar.gz
