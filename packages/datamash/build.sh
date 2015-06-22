c20833919e761bffc7b0535d2c6c044a4a5ac9a1
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-22 19:11:07 -0400

    Update datamash from 1.0.6 to 1.0.7

--- packages/datamash/build.sh
+++ packages/datamash/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/datamash/
 TERMUX_PKG_DESCRIPTION="GNU datamash performs basic numeric,textual and statistical operations"
-TERMUX_PKG_VERSION=1.0.6
+TERMUX_PKG_VERSION=1.0.7
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/datamash/datamash-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
