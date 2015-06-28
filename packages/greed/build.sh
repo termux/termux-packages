b0527a7a141e78be6b289b2c4397e3d42fe45d9b
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-28 17:06:36 -0400

    Update greed from 3.11 to 4.1

--- packages/greed/build.sh
+++ packages/greed/build.sh
@@ -1,4 +1,4 @@
-TERMUX_PKG_VERSION=3.11
+TERMUX_PKG_VERSION=4.1
 TERMUX_PKG_HOMEPAGE=http://www.catb.org/~esr/greed/
 TERMUX_PKG_DESCRIPTION="Game where you try to eat as much as possible of the board before munching yourself into a corner"
 TERMUX_PKG_SRCURL=http://www.catb.org/~esr/greed/greed-${TERMUX_PKG_VERSION}.tar.gz
