135961c9b0a4f1d336a21f09b665c40284dc05ce
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-28 17:34:03 -0400

    Update lua from 5.3.0 to 5.3.1

--- packages/libtiff/build.sh
+++ packages/libtiff/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.remotesensing.org/libtiff/ # Note that http://libtiff.org/ is hijacked
 TERMUX_PKG_DESCRIPTION="Support for the Tag Image File Format (TIFF) for storing image data"
-TERMUX_PKG_VERSION=4.0.3
+TERMUX_PKG_VERSION=4.0.4
 TERMUX_PKG_SRCURL=http://download.osgeo.org/libtiff/tiff-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libjpeg-turbo, liblzma"
