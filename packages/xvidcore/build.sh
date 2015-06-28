270b6f06c3a335eab94b7e4aaa66f18160516547
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-28 19:30:01 -0400

    Update xvidcore from 1.3.3 to 1.3.4

--- packages/xvidcore/build.sh
+++ packages/xvidcore/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://www.xvid.org/
 TERMUX_PKG_DESCRIPTION="High performance and high quality MPEG-4 library"
-TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_VERSION=1.3.4
 TERMUX_PKG_SRCURL=http://downloads.xvid.org/downloads/xvidcore-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=xvidcore
 
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure () {
+	rm $TERMUX_PREFIX/lib/libxvid*
 	export TERMUX_PKG_BUILDDIR=$TERMUX_PKG_BUILDDIR/build/generic
 	export TERMUX_PKG_SRCDIR=$TERMUX_PKG_BUILDDIR
 }
