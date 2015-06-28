04b9145a09e18a3cb80a30f563443f191400dd40
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-28 17:11:35 -0400

    Update less from 471 to 478

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -1,6 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://www.greenwoodsoftware.com/less/
 TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION=471
+TERMUX_PKG_VERSION=478
 TERMUX_PKG_SRCURL=http://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
 
+termux_step_pre_configure () {
+	# Mistake in packaging less 478
+	chmod +x $TERMUX_PKG_SRCDIR/configure
+}
