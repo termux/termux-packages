60d17baa2703808a8d556136b208b3ee0d1ed508
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-28 19:21:38 -0400

    Update pango from 1.36.8 to 1.37.1

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
-_MAJOR_VERSION=1.36
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.8
+_MAJOR_VERSION=1.37
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/pango/${_MAJOR_VERSION}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="fontconfig,glib,harfbuzz,libcairo"
