42946eb04d61f9c903063e673ce7a7f9a6df1e0d
Fredrik Fornwall <fredrik@fornwall.net>
2015-06-28 17:04:53 -0400

    Update global from 6.4 to 6.5

--- packages/global/build.sh
+++ packages/global/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE="http://www.gnu.org/software/global/global.html"
 TERMUX_PKG_DESCRIPTION="GNU global source code tag system that works the same way across diverse environments"
-TERMUX_PKG_VERSION=6.4
+TERMUX_PKG_VERSION=6.5
 TERMUX_PKG_SRCURL=http://tamacom.com/global/global-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-posix-sort=$TERMUX_PREFIX/bin/sort"
 # coreutils provides the posix sort executable:
