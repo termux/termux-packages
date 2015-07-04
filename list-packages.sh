d42599b2e3bbb2343be0c1d1088455627be5fbf0
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-04 08:56:47 -0400

    Make list-packages.sh be runnable by sh

--- list-packages.sh
+++ list-packages.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # list-packages.sh - tool to list all package with home pages and descriptions
 
-for path in packages/*; do (
-	pkg=`basename $path`	
-	. $path/build.sh
-	echo "$pkg($TERMUX_PKG_VERSION): $TERMUX_PKG_HOMEPAGE"
-        echo "       $TERMUX_PKG_DESCRIPTION"
-) done
+for path in packages/*; do
+	export path
+	export pkg=`basename $path`	
+	bash -c '. $path/build.sh; echo "$pkg($TERMUX_PKG_VERSION): $TERMUX_PKG_HOMEPAGE"; echo "       $TERMUX_PKG_DESCRIPTION"'
+done
