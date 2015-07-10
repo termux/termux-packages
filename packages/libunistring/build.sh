1b8b1b626391b6f6cccdb1dff38c9d1bbb8eadf5
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-09 22:27:47 -0400

    Update libunistring from 0.9.5 to 0.9.6

--- packages/libunistring/build.sh
+++ packages/libunistring/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/libunistring/
 TERMUX_PKG_DESCRIPTION="Library providing functions for manipulating Unicode strings"
-TERMUX_PKG_VERSION=0.9.5
+TERMUX_PKG_VERSION=0.9.6
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/libunistring/libunistring-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_uselocale=no"
