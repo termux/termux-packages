73e0c9c1865ba7232ca03606c4b3507de2e51eaa
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-11 07:56:20 -0400

    Add packages/cboard

---
+++ packages/cboard/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://benkibbey.wordpress.com/cboard/
+TERMUX_PKG_DESCRIPTION="PGN browser, editor and chess engine frontend"
+TERMUX_PKG_VERSION=0.7.3
+TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/c-board/${TERMUX_PKG_VERSION}/cboard-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_DEPENDS="libandroid-support,libandroid-glob,gnuchess, ncurses, ncurses-ui-libs"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes"
+
+CFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
+LDFLAGS+=" -landroid-glob"
