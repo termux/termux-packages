a22720a01007123f9c5dd19ad21bc5375ee42cde
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-15 13:17:06 -0400

    Bump revision for tmux and openssh after updated pwd.h patch

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://tmux.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer implementing switching between several programs in one terminal, detaching them and reattaching them to a different terminal"
 TERMUX_PKG_DEPENDS="ncurses, libevent"
 TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/tmux/tmux/tmux-${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
