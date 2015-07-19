b3e3da66dc8183ef014168fa928abc6672cf08bd
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-19 17:06:03 -0400

    make: Patch default shell path

--- packages/make/build.sh
+++ packages/make/build.sh
@@ -1,4 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/make/
 TERMUX_PKG_DESCRIPTION="Tool which controls the generation of executables and other non-source files of a program from the program's source files"
 TERMUX_PKG_VERSION=4.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/make/make-${TERMUX_PKG_VERSION}.tar.gz
