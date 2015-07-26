8ab83e251bba2dd1d85079df59eeb5f76c2c1331
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-26 13:44:05 -0400

    termux-tools: Add pm wrapper script to unset LD_LIBRARY_PATH

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://termux.com/
 TERMUX_PKG_DESCRIPTION="Some tools for Termux"
-TERMUX_PKG_VERSION=0.6
+TERMUX_PKG_VERSION=0.7
 
 termux_step_make_install () {
 	$CXX $CFLAGS $LDFLAGS -std=c++14 -Wall -Wextra -pedantic -Werror $TERMUX_PKG_BUILDER_DIR/*.cpp -o $TERMUX_PREFIX/bin/termux-elf-cleaner
 
-	cp $TERMUX_PKG_BUILDER_DIR/{am,termux-fix-shebang,termux-reload-style,chsh,termux-open-url} $TERMUX_PREFIX/bin/
+	cp $TERMUX_PKG_BUILDER_DIR/{am,pm,termux-fix-shebang,termux-reload-style,chsh,termux-open-url} $TERMUX_PREFIX/bin/
 
 	sed -i "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/termux-fix-shebang
 }
