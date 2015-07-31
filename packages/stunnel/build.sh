8c02de4bc0bd466f625e7d5b00abd70b41c323db
Fredrik Fornwall <fredrik@fornwall.net>
2015-07-31 01:05:50 -0400

    stunnel: Update from 5.20 to 5.22

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.stunnel.org/
 TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary applications"
-TERMUX_PKG_VERSION=5.20
+TERMUX_PKG_VERSION=5.22
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
