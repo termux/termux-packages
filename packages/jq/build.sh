a365f94966b27702ac656ea2a545f7b52e6309c5
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-09 19:01:16 -0400

    jq: Update package src url

--- packages/jq/build.sh
+++ packages/jq/build.sh
@@ -1,4 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://stedolan.github.io/jq/
 TERMUX_PKG_DESCRIPTION="Command-line JSON processor"
 TERMUX_PKG_VERSION=1.4
-TERMUX_PKG_SRCURL=http://stedolan.github.io/jq/download/source/jq-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/stedolan/jq/archive/jq-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=jq-jq-${TERMUX_PKG_VERSION}
+
+termux_step_pre_configure () {
+	cd $TERMUX_PKG_SRCDIR
+	autoreconf -i
+}
