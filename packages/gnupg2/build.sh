7229a710227856f88ba42caabb9108355e2c99db
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-11 20:46:42 -0400

    gnupg2: Update from 2.1.6 to 2.1.7

--- packages/gnupg2/build.sh
+++ packages/gnupg2/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnupg.org/
 TERMUX_PKG_DESCRIPTION="Implementation of the OpenPGP standard for encrypting and signing data and communication"
-TERMUX_PKG_VERSION=2.1.6
+TERMUX_PKG_VERSION=2.1.7
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libassuan,libbz2,libgcrypt,libksba,libnpth,readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ldap"
