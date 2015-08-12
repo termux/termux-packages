4b81c4eb92bcca68c8a37886ceaeb4a3b8ca3802
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-11 20:36:39 -0400

    weechat: Link against libandroid-support for getting unicode characters right

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
 TERMUX_PKG_VERSION=1.2
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_DEPENDS="ncurses, libgcrypt, libcurl, libgnutls"
+TERMUX_PKG_DEPENDS="ncurses, libgcrypt, libcurl, libgnutls, libandroid-support"
 # weechat-curses is a symlink to weechat, so remove it:
 TERMUX_PKG_RM_AFTER_INSTALL="bin/weechat-curses share/icons"
 
