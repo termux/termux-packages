cc1ae02e2ecebd5006fe481473fc1f15f92b121a
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-14 15:27:47 -0400

    termux-api: Cleanup and put termux-api binary in libexec

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,11 +1,14 @@
 TERMUX_PKG_HOMEPAGE=http://termux.com/add-ons/api/
 TERMUX_PKG_DESCRIPTION="Termux API commands"
-TERMUX_PKG_VERSION=0.7
+TERMUX_PKG_VERSION=0.8
 
 termux_step_make_install () {
         mkdir -p $TERMUX_PREFIX/bin
-        for file in `ls $TERMUX_PKG_BUILDER_DIR/* | grep -v build.sh | grep -v termux-api.c`; do
-		cp $file $TERMUX_PREFIX/bin
+	local TERMUX_API_BINARY=$TERMUX_PREFIX/libexec/termux-api
+	cd $TERMUX_PKG_BUILDER_DIR
+        for file in `ls termux-* | grep -v termux-api.c`; do
+		sed "s|@TERMUX_API@|$TERMUX_API_BINARY|" $file > $TERMUX_PREFIX/bin/$file
+		chmod +x $TERMUX_PREFIX/bin/$file
         done
-        $CC $CFLAGS -std=c11 -Wall -Wextra -pedantic -Werror $LDFLAGS $TERMUX_PKG_BUILDER_DIR/termux-api.c -o $TERMUX_PREFIX/bin/termux-api
+        $CC $CFLAGS -std=c11 -Wall -Wextra -pedantic -Werror $LDFLAGS termux-api.c -o $TERMUX_API_BINARY
 }
