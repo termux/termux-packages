c0906860b1f4ccba9b9d944be03b1600aa18e459
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-18 14:51:37 -0400

    jq: Update from 1.4 to 1.5

--- packages/jq/build.sh
+++ packages/jq/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://stedolan.github.io/jq/
 TERMUX_PKG_DESCRIPTION="Command-line JSON processor"
-TERMUX_PKG_VERSION=1.4
+TERMUX_PKG_VERSION=1.5
 TERMUX_PKG_SRCURL=https://github.com/stedolan/jq/archive/jq-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=jq-jq-${TERMUX_PKG_VERSION}
 
