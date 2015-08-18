e9dec303d7fb3044b7b02ebf80c84f774acafb61
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-18 17:13:46 -0400

    subversion: Update to 1.9.0 from 1.8.13

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://subversion.apache.org/
 TERMUX_PKG_DESCRIPTION="Centralized version control system characterized by its simplicity"
-TERMUX_PKG_VERSION=1.8.13
+TERMUX_PKG_VERSION=1.9.0
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl --without-libmagic"
