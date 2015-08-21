d7fb7c29ef95209adddce5852080c99d7b97e117
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-21 18:15:06 -0400

    Add nmap package

---
+++ packages/nmap/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://nmap.org/
+TERMUX_PKG_DESCRIPTION="Utility for network discovery and security auditing"
+TERMUX_PKG_VERSION=6.49BETA4
+TERMUX_PKG_SRCURL=https://nmap.org/dist/nmap-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_DEPENDS="libpcap, pcre, openssl"
+# --without-nmap-update to avoid linking against libsvn_client:
+# --without-zenmap to avoid python scripts for graphical gtk frontend:
+# --with-liblua=included to use internal liblua, since only lua 5.2 supported:
+# --without-ndiff to avoid python2-using ndiff utility:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-static --with-liblua=included --without-nmap-update --without-zenmap --without-ndiff"
+TERMUX_PKG_BUILD_IN_SRC="yes"
