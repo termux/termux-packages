6824617e6462e216becf79d8e0d389baa75b8de5
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-23 08:54:05 -0400

    Add hexcurse package

---
+++ packages/hexcurse/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/LonnyGomes/hexcurse
+TERMUX_PKG_DESCRIPTION="Console hexeditor"
+TERMUX_PKG_VERSION=1.58
+TERMUX_PKG_SRCURL=https://github.com/LonnyGomes/hexcurse/archive/hexcurse-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=hexcurse-hexcurse-${TERMUX_PKG_VERSION}
+TERMUX_PKG_DEPENDS="ncurses"
