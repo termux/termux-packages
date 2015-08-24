ce80e040f50aec678130bb6725d9621ddfa44230
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-24 05:42:02 -0400

    libcurl: Update from 7.43 to 7.44

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_VERSION=7.43.0
+TERMUX_PKG_VERSION=7.44.0
 TERMUX_PKG_SRCURL=http://curl.haxx.se/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_BUILD_REVISION=2
 export TERMUX_CA_BUNDLE=$TERMUX_PREFIX/etc/tls/cert.pem
