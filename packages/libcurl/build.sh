e72e4caf5a421ac923c8d75132893bd7b95cc6ea
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-24 05:46:20 -0400

    libcurl: Remove build revision after version bump

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_VERSION=7.44.0
 TERMUX_PKG_SRCURL=http://curl.haxx.se/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_BUILD_REVISION=2
 export TERMUX_CA_BUNDLE=$TERMUX_PREFIX/etc/tls/cert.pem
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --with-ca-bundle=$TERMUX_CA_BUNDLE"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/curl-config share/man/man1/curl-config.1"
