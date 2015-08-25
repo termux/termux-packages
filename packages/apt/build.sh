de8d91bff3528f03e8b468d2703ef2e3b06b37fd
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-25 17:49:04 -0400

    apt: Update from 1.0.9.10 to 1.0.10.2

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_DEPENDS="libbz2, liblzma, libgnustl, dpkg, gnupg"
-TERMUX_PKG_VERSION=1.0.9.10
+TERMUX_PKG_VERSION=1.0.10.2
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--host=${TERMUX_ARCH}-linux --disable-rpath acl_cv_rpath=$TERMUX_PREFIX/lib gt_cv_func_CFPreferencesCopyAppValue=no gt_cv_func_CFLocaleCopyCurrent=no ac_cv_c_bigendian=no --no-create"
 TERMUX_PKG_FOLDERNAME=apt-${TERMUX_PKG_VERSION}
@@ -31,8 +31,8 @@ termux_step_make () {
 
 termux_step_make_install () {
         cp $TERMUX_PKG_BUILDDIR/bin/apt{,-get,-cache,-config,-key} $TERMUX_PREFIX/bin/
-        cp $TERMUX_PKG_BUILDDIR/bin/libapt-{pkg.so.4.12,private.so.0.0} $TERMUX_PREFIX/lib/
-        (cd $TERMUX_PREFIX/lib; rm -f libapt-pkg.so; ln -s libapt-pkg.so.4.12 libapt-pkg.so) # used by python-apt
+        cp $TERMUX_PKG_BUILDDIR/bin/libapt-{pkg.so.4.16,private.so.0.0} $TERMUX_PREFIX/lib/
+        (cd $TERMUX_PREFIX/lib; rm -f libapt-pkg.so; ln -s libapt-pkg.so.4.16 libapt-pkg.so) # used by python-apt
         mkdir -p $TERMUX_PREFIX/lib/apt/methods $TERMUX_PREFIX/share/man/man{5,8}
         cp $TERMUX_PKG_BUILDDIR/docs/apt{,-cache,-get}.8 $TERMUX_PREFIX/share/man/man8/
         cp $TERMUX_PKG_BUILDDIR/docs/{apt.conf,sources.list}.5 $TERMUX_PREFIX/share/man/man5/
