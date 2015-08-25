5828a775dbd12abf0d65cd482a2f48007441c5d7
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-25 18:41:57 -0400

    jack: Configure when running

--- packages/jack/jack.sh
+++ packages/jack/jack.sh
@@ -4,7 +4,12 @@
 export ANDROID_DATA=@TERMUX_PREFIX@/var/android/
 mkdir -p $ANDROID_DATA/dalvik-cache
 
-JACK_JAR=@TERMUX_PREFIX@/share/dex/jack.jar
-ANDROID_JAR=@TERMUX_PREFIX@/share/jack/android.jack
-
-dalvikvm -Xmx256m -cp $JACK_JAR com.android.jack.Main -cp $ANDROID_JAR $@
+LD_LIBRARY_PATH=/system/lib \
+		exec dalvikvm -Xmx256m \
+		-Djava.io.tmpdir=@TERMUX_PREFIX@/tmp \
+		-cp @TERMUX_PREFIX@/share/dex/jack.jar com.android.jack.Main \
+		-cp @TERMUX_PREFIX@/share/jack/android.jack \
+		-D jack.library.digest.algo=MD5 -D sched.vfs.case-insensitive.algo=MD5 \
+		-D jack.annotation-processor=off \
+		--output-dex=`pwd` \
+		$@
