2ff12a7aa0d4059ab03ab151c816b130e660d508
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-25 18:32:59 -0400

    jq: Simplify build

--- packages/jq/build.sh
+++ packages/jq/build.sh
@@ -3,8 +3,3 @@ TERMUX_PKG_DESCRIPTION="Command-line JSON processor"
 TERMUX_PKG_VERSION=1.5
 TERMUX_PKG_SRCURL=https://github.com/stedolan/jq/archive/jq-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=jq-jq-${TERMUX_PKG_VERSION}
-
-termux_step_pre_configure () {
-	cd $TERMUX_PKG_SRCDIR
-	autoreconf -i
-}
