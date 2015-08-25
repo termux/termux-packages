542a284d1fbc8c55a945de365f0938bcfa814963
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-25 17:03:16 -0400

    unrar: Update from 5.0.14 to 5.3.2

--- packages/unrar/build.sh
+++ packages/unrar/build.sh
@@ -1,4 +1,4 @@
-TERMUX_PKG_VERSION=5.0.14
+TERMUX_PKG_VERSION=5.3.2
 TERMUX_PKG_HOMEPAGE=http://www.rarlab.com/
 TERMUX_PKG_DESCRIPTION="Tool for extracting files from .rar archives"
 TERMUX_PKG_SRCURL=http://www.rarlab.com/rar/unrarsrc-${TERMUX_PKG_VERSION}.tar.gz
