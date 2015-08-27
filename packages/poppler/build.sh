19c5064ef99c5bd221300bb7742e154a07c92c29
Fredrik Fornwall <fredrik@fornwall.net>
2015-08-27 19:41:04 -0400

    poppler: Update from 0.34 to 0.35

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
-TERMUX_PKG_VERSION=0.34.0
+TERMUX_PKG_VERSION=0.35.0
 TERMUX_PKG_SRCURL=http://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
 TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, libgnustl, openjpeg"
