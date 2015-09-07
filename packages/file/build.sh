ee9fccfaf339f3efe8711b1432d3f1d312d09911
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-07 11:56:10 -0400

    file: Update to 5.24 from 5.23

--- packages/file/build.sh
+++ packages/file/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://darwinsys.com/file/
 TERMUX_PKG_DESCRIPTION="Command-line tool that tells you in words what kind of data a file contains"
-TERMUX_PKG_VERSION=5.23
+TERMUX_PKG_VERSION=5.24
 # TERMUX_PKG_SRCURL=ftp://ftp.astron.com/pub/file/file-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=http://fossies.org/linux/misc/file-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_MAKE_ARGS="FILE_COMPILE=$TERMUX_PKG_HOSTBUILD_DIR/src/file"
