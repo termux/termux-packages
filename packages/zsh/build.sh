4eb87e93aaa464496d2870ba800aec290bd6de45
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-09 10:11:46 -0400

    zsh: Avoid extra binaries

--- packages/zsh/build.sh
+++ packages/zsh/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE="http://www.zsh.org/"
 TERMUX_PKG_DESCRIPTION="Shell designed for interactive use, although it is also a powerful scripting language"
 TERMUX_PKG_VERSION=5.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://www.zsh.org/pub/zsh-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL="bin/zsh-${TERMUX_PKG_VERSION}"
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, termux-tools, command-not-found"
@@ -9,4 +10,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-etcdir=$TERMUX_PREFIX/etc --disable-gd
 termux_step_post_make_install () {
 	# /etc/zshrc - Run for interactive shells (http://zsh.sourceforge.net/Guide/zshguide02.html):
 	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" $TERMUX_PKG_BUILDER_DIR/etc-zshrc > $TERMUX_PREFIX/etc/zshrc
+
+	# Remove zsh.new/zsh.old/zsh-$version if any exists:
+	rm -f $TERMUX_PREFIX/{zsh-*,zsh.*}
 }
