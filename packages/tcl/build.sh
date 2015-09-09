df1fab9eedaad9ec88e57933114022e72ba79e33
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-09 09:55:40 -0400

    tcl: Pass --enable-man-symlinks to avoid hard links

--- packages/tcl/build.sh
+++ packages/tcl/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.tcl.tk/
 TERMUX_PKG_DESCRIPTION="Powerful but easy to learn dynamic programming language"
 _MAJOR_VERSION=8.6
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/tcl/Tcl/${TERMUX_PKG_VERSION}/tcl${TERMUX_PKG_VERSION}-src.tar.gz
 TERMUX_PKG_FOLDERNAME=tcl$TERMUX_PKG_VERSION
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="tcl_cv_strtod_buggy=ok --mandir=$TERMUX_PREFIX/share/man"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="tcl_cv_strtod_buggy=ok --mandir=$TERMUX_PREFIX/share/man --enable-man-symlinks"
 
 termux_step_pre_configure () {
 	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/unix
