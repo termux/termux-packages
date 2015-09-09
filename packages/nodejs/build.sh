93982adb09992eab1b804770a9ebccdd5e515fb9
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-08 22:04:06 -0400

    node: No longer depend on c-ares

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_VERSION=4.0.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/latest/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--dest-os=android --shared-openssl --shared-zlib --shared-libuv"
-TERMUX_PKG_DEPENDS="c-ares, openssl, libuv"
+TERMUX_PKG_DEPENDS="openssl, libuv"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
