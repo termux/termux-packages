858b2fa7484165582eb123b00645c2a3b70b2495
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-09 09:56:20 -0400

    detect-hardlinks.sh: Be more descriptive

--- detect-hardlinks.sh
+++ detect-hardlinks.sh
@@ -7,7 +7,7 @@ for f in *; do
 	if [ -d $f/massage ]; then
 		cd $f/massage
 		if [ -n "$(find . -type f -links +1)" ]; then
-	        	echo "$f contains hardlink"
+			echo "$f contains hardlink, which will not work on Android 6.0+"
 		fi
 	fi
 done
