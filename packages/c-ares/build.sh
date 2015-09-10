ff3f3c5ea99c3643d45d46a7e6fddf18801f22c1
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-10 17:48:47 -0400

    c-ares: Remove node.js patches

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -2,8 +2,4 @@ TERMUX_PKG_HOMEPAGE=http://c-ares.haxx.se/
 TERMUX_PKG_DESCRIPTION="C library for asynchronous DNS requests (including name resolves)"
 TERMUX_PKG_VERSION=1.10.0
 TERMUX_PKG_SRCURL=http://c-ares.haxx.se/download/c-ares-${TERMUX_PKG_VERSION}.tar.gz
-
-termux_step_post_make_install () {
-	# Install the nameser.h compatibility header used by node.js
-	cp $TERMUX_PKG_SRCDIR/nameser.h $TERMUX_PREFIX/include
-}
+TERMUX_PKG_BUILD_REVISION=1
