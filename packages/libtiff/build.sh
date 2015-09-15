64cd4cbe18009585aad2f4df05890aea9420f23e
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-15 06:33:32 -0400

    libtiff: Update version

--- packages/libtiff/build.sh
+++ packages/libtiff/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.remotesensing.org/libtiff/ # Note that http://libtiff.org/ is hijacked
 TERMUX_PKG_DESCRIPTION="Support for the Tag Image File Format (TIFF) for storing image data"
-TERMUX_PKG_VERSION=4.0.5
+TERMUX_PKG_VERSION=4.0.6
 TERMUX_PKG_SRCURL=http://download.osgeo.org/libtiff/tiff-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libjpeg-turbo, liblzma"
