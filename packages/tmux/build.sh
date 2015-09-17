b2d8bace5214ac2459a33d3751b46d1154524f84
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-16 21:09:30 -0400

    tmux: Bump revision to force update after rebuild

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://tmux.github.io/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer implementing switching between several programs in one terminal, detaching them and reattaching them to a different terminal"
 TERMUX_PKG_DEPENDS="ncurses, libevent, libutil"
 TERMUX_PKG_VERSION=2.0
-TERMUX_PKG_BUILD_REVISION=3
+TERMUX_PKG_BUILD_REVISION=4
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/tmux/tmux/tmux-${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
