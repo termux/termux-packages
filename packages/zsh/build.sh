84d1e6dcf26a2dfbf193e70dd361c2baf391f615
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-17 11:03:24 -0400

    zsh: Bump revision

--- packages/zsh/build.sh
+++ packages/zsh/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE="http://www.zsh.org/"
 TERMUX_PKG_DESCRIPTION="Shell designed for interactive use, although it is also a powerful scripting language"
 TERMUX_PKG_VERSION=5.1.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://www.zsh.org/pub/zsh-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL="bin/zsh-${TERMUX_PKG_VERSION}"
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, termux-tools, command-not-found"
