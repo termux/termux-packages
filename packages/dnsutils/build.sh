b402eeffd99d766b52fd96e2200f09893b416817
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-18 09:28:25 -0400

    dnsutils: Fix build

--- packages/dnsutils/build.sh
+++ packages/dnsutils/build.sh
@@ -1,26 +1,22 @@
 TERMUX_PKG_HOMEPAGE=https://www.isc.org/downloads/bind/
 TERMUX_PKG_DESCRIPTION="Clients provided with BIND"
 TERMUX_PKG_VERSION=9.10.3
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL="https://www.isc.org/downloads/file/bind-9-10-3/?version=tar-gz"
 TERMUX_PKG_FOLDERNAME="bind-${TERMUX_PKG_VERSION}"
-#TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
-#
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-gssapi=no --with-randomdev=/dev/random -with-ecdsa=yes --with-gost=yes --with-libxml2=no"
-#TERMUX_MAKE_PROCESSES=1
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-gssapi=no --with-randomdev=/dev/random -with-ecdsa=no --with-gost=no --with-libxml2=no"
 
-export BUILD_AR=$AR
-export BUILD_CC=$CC
-export BUILD_CFLAGS="$CFLAGS"
-export BUILD_CPPFLAGS="$CPPFLAGS"
-export BUILD_LDFLAGS="$LDFLAGS"
-export BUILD_RANLIB=$RANLIB
+export BUILD_AR=ar
+export BUILD_CC=gcc
+export BUILD_CFLAGS=
+export BUILD_CPPFLAGS=
+export BUILD_LDFLAGS=
+export BUILD_RANLIB=
 
-export AR=ar
-export CC=gcc
-export CFLAGS=
-export CPPFLAGS=
-export LDFLAGS=
-export RANLIB=ranlib
+_RESOLV_CONF=$TERMUX_PREFIX/etc/resolv.conf
+CFLAGS+=" $CPPFLAGS -DRESOLV_CONF=\\\"$_RESOLV_CONF\\\""
+LDFLAGS+=" -llog"
 
 termux_step_make () {
 	make -C lib/dns &&
@@ -33,4 +29,6 @@ termux_step_make () {
 
 termux_step_make_install () {
 	make -C bin/dig install
+
+	printf "nameserver 8.8.8.8\nnameserver 8.8.4.4" > $_RESOLV_CONF
 }
