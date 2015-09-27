99856c1d1ced379efec7659b9ef796882f5cf416
Fredrik Fornwall <fredrik@fornwall.net>
2015-09-27 06:10:07 -0400

    multitail: Update to 6.4.2

--- packages/multitail/build.sh
+++ packages/multitail/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.vanheusden.com/multitail/
 TERMUX_PKG_DESCRIPTION="Tool to monitor logfiles and command output in multiple windows in a terminal, colorize, filter and merge"
-TERMUX_PKG_VERSION=6.4.1
+TERMUX_PKG_VERSION=6.4.2
 TERMUX_PKG_SRCURL=http://www.vanheusden.com/multitail/multitail-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=yes
