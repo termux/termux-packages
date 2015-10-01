303fbc96d80f74f354b4d1d7bc59cd6d0ab418b7
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-01 19:00:02 -0400

    picolisp package improvements

--- disabled-packages/picolisp/build.sh
+++ disabled-packages/picolisp/build.sh
@@ -4,11 +4,24 @@ TERMUX_PKG_VERSION=3.1.11
 TERMUX_PKG_SRCURL=http://software-lab.de/picoLisp-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_FOLDERNAME=picoLisp
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_MAKE_PROCESSES=1
-# TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
-#TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl"
 
-termux_step_post_extract_package () {
+termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/src
-	CFLAGS+=" $LDFLAGS $CPPFLAGS"
+	TERMUX_PKG_BUILDDIR=$TERMUX_PKG_SRCDIR
+	CFLAGS+=" -c $LDFLAGS $CPPFLAGS"
+}
+
+termux_step_make_install () {
+	cp $TERMUX_PKG_SRCDIR/../man/man1/{pil,picolisp}.1 $TERMUX_PREFIX/share/man/man1/
+
+	rm -Rf $TERMUX_PREFIX/lib/picolisp
+	mkdir -p $TERMUX_PREFIX/lib/picolisp
+
+	cp -Rf $TERMUX_PKG_SRCDIR/../* $TERMUX_PREFIX/lib/picolisp/
+	rm -Rf $TERMUX_PREFIX/lib/picolisp/{src,src64,man,java,ersatz}
+
+	# Replace first line "#!/usr/bin/picolisp /usr/lib/picolisp/lib.l":
+	sed -i "1 s|^.*$|#!$TERMUX_PREFIX/bin/picolisp $TERMUX_PREFIX/lib/picolisp/lib.l|g" $TERMUX_PREFIX/lib/picolisp/bin/pil
+
+	( cd $TERMUX_PREFIX/bin && ln -f -s ../lib/picolisp/bin/picolisp picolisp && ln -f -s ../lib/picolisp/bin/pil pil )
 }
