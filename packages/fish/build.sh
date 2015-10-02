3e9c81f21ec084252633a0199def7f7f58f720a1
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-02 04:33:54 -0400

    fish: Update package to latest master

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
 TERMUX_PKG_VERSION=2.2.0.`date "+%Y%m%d%H%M"`
 # TERMUX_PKG_SRCURL=http://fishshell.com/files/${TERMUX_PKG_VERSION}/fish-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/archive/master.zip
+TERMUX_PKG_NO_SRC_CACHE=yes
 TERMUX_PKG_DEPENDS="ncurses, libgnustl, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_FOLDERNAME=fish-shell-master
