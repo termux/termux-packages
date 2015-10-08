f571a0663258fd2d5b818036c2e44a1a1457764a
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-08 17:59:52 -0400

    Bump: libmsgpack, libtermkey, libunibilium, nodejs

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=4.1.1
+TERMUX_PKG_VERSION=4.1.2
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/latest/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--dest-os=android --shared-openssl --shared-zlib --shared-libuv"
 TERMUX_PKG_DEPENDS="openssl, libuv"
