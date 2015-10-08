27f2a254ea189550eb50b8b8e167a067d059649e
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-08 18:01:09 -0400

    fish: Better version naming

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://fishshell.com/
 TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
-TERMUX_PKG_VERSION=2.2.0.`date "+%Y%m%d%H%M"`
+TERMUX_PKG_VERSION=2.2.0_`date "+%Y%m%d%H%M"`
 # TERMUX_PKG_SRCURL=http://fishshell.com/files/${TERMUX_PKG_VERSION}/fish-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/archive/master.zip
 TERMUX_PKG_NO_SRC_CACHE=yes
