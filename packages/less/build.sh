627d1d30e1174df8136841f2cc97a5404b6fba68
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-08 19:46:01 -0400

    less: Update to less-481

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.greenwoodsoftware.com/less/
 TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION=479
+TERMUX_PKG_VERSION=481
 TERMUX_PKG_SRCURL=http://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_pre_configure () {
