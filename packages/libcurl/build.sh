b14c5952c45c08d75a5651731e84db283f37cdd3
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-08 19:18:17 -0400

    Various minor version bumps

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_VERSION=7.44.0
+TERMUX_PKG_VERSION=7.45.0
 TERMUX_PKG_SRCURL=http://curl.haxx.se/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
 export TERMUX_CA_BUNDLE=$TERMUX_PREFIX/etc/tls/cert.pem
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --with-ca-bundle=$TERMUX_CA_BUNDLE"
