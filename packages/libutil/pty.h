f3c180bc2542eabcdf53f6e12d4ba9670156b506
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-09 21:42:22 -0400

    libutil: Wrap pty.h in __{BEGIN,END}_DECLS

--- packages/libutil/pty.h
+++ packages/libutil/pty.h
@@ -1,12 +1,17 @@
 #ifndef _PTY_H
 #define _PTY_H
 
+#include <sys/cdefs.h>
 #include <termios.h>
+
+__BEGIN_DECLS
 
 int openpty(int* amaster, int* aslave, char* name, struct termios* termp, struct winsize* winp);
 
 int login_tty(int fd);
 
 int forkpty(int* amaster, char* name, struct termios* termp, struct winsize* winp);
+
+__END_DECLS
 
 #endif
