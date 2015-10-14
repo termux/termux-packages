01c46e3b956ae6bac55ab3966e01722a2e81d0b6
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-14 06:40:49 -0400

    Fix ranger referencing python3.4 (closes #27)

--- packages/ranger/build.sh
+++ packages/ranger/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://ranger.nongnu.org/
 TERMUX_PKG_DESCRIPTION="File manager with VI key bindings"
-TERMUX_PKG_VERSION=1.7.1
+TERMUX_PKG_VERSION=1.7.2
 TERMUX_PKG_SRCURL=http://ranger.nongnu.org/ranger-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="python, file"
 TERMUX_PKG_FOLDERNAME=ranger-${TERMUX_PKG_VERSION}
@@ -9,4 +9,13 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
         python3.4 setup.py install --prefix=$TERMUX_PREFIX --force
+}
+
+termux_step_post_massage () {
+	find . -path '*/__pycache__*' -delete
+
+	# Work around python 3.5 not being available on ubuntu 15.04:
+	perl -p -i -e 's|python3.4|python3.5|g' bin/*
+	mv lib/python3.4 lib/python3.5
+	mv lib/python3.5/site-packages/ranger-${TERMUX_PKG_VERSION}-py3.{4,5}.egg-info
 }
