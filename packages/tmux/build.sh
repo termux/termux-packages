b4b35f2f10d8e226145f6cc0e0c46c6d7e12c17f
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-19 17:36:12 -0400

    tmux: Update to 2.1

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://tmux.github.io/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer implementing switching between several programs in one terminal, detaching them and reattaching them to a different terminal"
 TERMUX_PKG_DEPENDS="ncurses, libevent, libutil"
-TERMUX_PKG_VERSION=2.0
-TERMUX_PKG_BUILD_REVISION=4
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/tmux/tmux/tmux-${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=2.1
+TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/releases/download/${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_post_make_install () {
