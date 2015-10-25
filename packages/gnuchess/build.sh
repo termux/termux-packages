289f1725e75eae4df60d45125bfae76f069ed56b
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-25 16:55:08 -0400

    Update gnuchess

--- packages/gnuchess/build.sh
+++ packages/gnuchess/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/chess/
 TERMUX_PKG_DESCRIPTION="Chess-playing program"
-TERMUX_PKG_VERSION=6.2.1
+TERMUX_PKG_VERSION=6.2.2
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/chess/gnuchess-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="ncurses, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes"
