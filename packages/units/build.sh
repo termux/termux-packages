63dd5a9043f6a29038b58584f6c10258b29c2b31
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-25 16:55:17 -0400

    Update units

--- packages/units/build.sh
+++ packages/units/build.sh
@@ -1,7 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/units/
 TERMUX_PKG_DESCRIPTION="Converts between different systems of units"
-TERMUX_PKG_VERSION=2.11
+TERMUX_PKG_VERSION=2.12
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/units/units-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="readline"
-
-CFLAGS+=" $CPPFLAGS"
