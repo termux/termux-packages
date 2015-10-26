46559847b15548f812bb56e95ad7015cb33ea6a6
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-26 19:35:22 -0400

    Update busybox to 1.24.1

--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.busybox.net/
 TERMUX_PKG_DESCRIPTION="Tiny versions of many common UNIX utilities into a single small executable"
 TERMUX_PKG_ESSENTIAL=yes
-TERMUX_PKG_VERSION=1.23.2
-TERMUX_PKG_BUILD_REVISION=6
+TERMUX_PKG_VERSION=1.24.1
 TERMUX_PKG_SRCURL=http://www.busybox.net/downloads/busybox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_BUILD_IN_SRC=yes
 
