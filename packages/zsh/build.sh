e46c5cbca391809bf940832ca98ece8d1c902bda
Fredrik Fornwall <fredrik@fornwall.net>
2015-10-26 20:25:37 -0400

    Make zsh folder permissions to please compaudit
    Closes #31.

--- packages/zsh/build.sh
+++ packages/zsh/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE="http://www.zsh.org/"
 TERMUX_PKG_DESCRIPTION="Shell designed for interactive use, although it is also a powerful scripting language"
 TERMUX_PKG_VERSION=5.1.1
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://www.zsh.org/pub/zsh-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL="bin/zsh-${TERMUX_PKG_VERSION}"
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, termux-tools, command-not-found"
@@ -13,4 +13,15 @@ termux_step_post_make_install () {
 
 	# Remove zsh.new/zsh.old/zsh-$version if any exists:
 	rm -f $TERMUX_PREFIX/{zsh-*,zsh.*}
+
+	# This should perhaps be done in a more general way? Doing it here
+	# to silence "compaudit" warnings:
+	chmod 700 $TERMUX_PREFIX/share/{zsh,zsh/$TERMUX_PKG_VERSION}
+}
+
+termux_step_create_debscripts () {
+	# For already installed packages:
+	echo "chmod 700 $TERMUX_PREFIX/share/{zsh,zsh/$TERMUX_PKG_VERSION}" > postinst
+        echo "exit 0" >> postinst
+        chmod 0755 postinst
 }
