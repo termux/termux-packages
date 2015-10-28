0947a1115bf143f730dfba7416a9b1d6c112bf38
ebraminio <ebrahim@gnu.org>
2015-10-28 23:10:27 +0330

    Add ragel package

---
+++ packages/ragel/build.sh
@@ -0,0 +1,4 @@
+TERMUX_PKG_HOMEPAGE=http://www.colm.net/open-source/ragel/
+TERMUX_PKG_DESCRIPTION="Compiles finite state machines from regular languages into executable C, C++, Objective-C, or D code"
+TERMUX_PKG_VERSION=6.9
+TERMUX_PKG_SRCURL=http://www.colm.net/files/ragel/ragel-${TERMUX_PKG_VERSION}.tar.gz
