1189304e6fd2ec7e4ca4eb33dd8adca800258167
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-05 16:51:09 -0500

    nodejs: Update from 4.2.1 to 4.2.2

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=4.2.1
-TERMUX_PKG_SRCURL=https://nodejs.org/dist/latest/node-v${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION=4.2.2
+TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--dest-os=android --shared-openssl --shared-zlib --shared-libuv"
 TERMUX_PKG_DEPENDS="openssl, libuv"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
