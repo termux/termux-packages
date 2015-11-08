9f8c4baf6dd9db09e5103136e1cbaac49e65888d
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-08 01:49:04 -0500

    pkg-config: Update from 0.28 to 0.29

--- packages/pkg-config/build.sh
+++ packages/pkg-config/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.freedesktop.org/wiki/Software/pkg-config/
 TERMUX_PKG_DESCRIPTION="Helper tool used when compiling applications and libraries"
-TERMUX_PKG_VERSION=0.28
+TERMUX_PKG_VERSION=0.29
 TERMUX_PKG_SRCURL=http://pkgconfig.freedesktop.org/releases/pkg-config-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/*-pkg-config"
