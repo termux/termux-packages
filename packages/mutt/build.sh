a79f1752180e90ec7ea7003e5a10acd5444c1fe8
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-08 01:43:03 -0500

    mutt: Update from 1.5.23 to 1.5.24

--- packages/mutt/build.sh
+++ packages/mutt/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.mutt.org/
 TERMUX_PKG_DESCRIPTION="Mail client"
-TERMUX_PKG_VERSION=1.5.23
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=1.5.24
 TERMUX_PKG_SRCURL=https://bitbucket.org/mutt/mutt/downloads/mutt-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-smtp --enable-imap --enable-pop --with-mailpath=$TERMUX_PREFIX/var/mail --with-ssl --enable-compressed"
