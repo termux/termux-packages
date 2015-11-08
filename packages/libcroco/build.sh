496d605a8159232df5c4c7f2a7ce3f3ad7438f31
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-08 01:28:30 -0500

    libcroco: Minor version bump

--- packages/libcroco/build.sh
+++ packages/libcroco/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/GNOME/libcroco
 TERMUX_PKG_DESCRIPTION="CSS parsing and manipulation library"
 _MAJOR_VERSION=0.6
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.8
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.9
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/libcroco/${_MAJOR_VERSION}/libcroco-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="glib,liblzma,libxml2"
