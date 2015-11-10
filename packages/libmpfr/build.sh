c2c9b4775f75e1e56010aa1c48badc8fad273129
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-10 17:05:59 -0500

    libmpfr: Use updated tarball url

--- packages/libmpfr/build.sh
+++ packages/libmpfr/build.sh
@@ -3,5 +3,5 @@ TERMUX_PKG_DESCRIPTION="C library for multiple-precision floating-point computat
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_VERSION=3.1.3
 TERMUX_PKG_BUILD_REVISION=1
-TERMUX_PKG_SRCURL=http://www.mpfr.org/mpfr-current/mpfr-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/mpfr/mpfr-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_header_locale_h=no"
