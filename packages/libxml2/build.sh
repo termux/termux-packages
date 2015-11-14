d03dc20a3da274ed2dc6ef12089f0133c0ae5660
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-13 21:51:08 -0500

    libxml2: Add two patches

--- packages/libxml2/build.sh
+++ packages/libxml2/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.xmlsoft.org
 TERMUX_PKG_DESCRIPTION="Library for parsing XML documents"
 TERMUX_PKG_VERSION=2.9.2
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=ftp://xmlsoft.org/libxml2/libxml2-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-python"
 TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc bin/xml2-config bin/xmlcatalog lib/xml2Conf.sh share/man/man1/xml2-config.1 share/man/man1/xmlcatalog.1"
