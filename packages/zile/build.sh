89df5522c6eee1238e3efcec4379f7ee2d701820
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-15 16:43:01 -0500

    Enable the zile package
    The zile package no longer crashes after @iainnicol finding out
    that the issue was rpl_malloc, so enable it.
    
    See https://github.com/termux/termux-packages/pull/56

---
+++ packages/zile/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/zile/
+TERMUX_PKG_DESCRIPTION="Lightweight clone of the Emacs text editor"
+TERMUX_PKG_VERSION=2.4.11
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/zile/zile-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libgc, ncurses"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_pre_make () {
+	# zile uses help2man to build the zile.1 man page, which would require
+	# a host build. To avoid that just copy a pre-built man page.
+	cp $TERMUX_PKG_BUILDER_DIR/zile.1 $TERMUX_PKG_BUILDDIR/doc/zile.1
+	$TERMUX_TOUCH -d "next hour" $TERMUX_PKG_BUILDDIR/doc/zile.1*
+}
