089a0efe415afc9528dcf0c4d4638a62ea682bcf
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-19 17:50:02 -0500

    findutils: Remove locate and updated (use mlocate)

--- packages/findutils/build.sh
+++ packages/findutils/build.sh
@@ -5,3 +5,5 @@ TERMUX_PKG_SRCURL=http://prep.ai.mit.edu/gnu/findutils/findutils-${TERMUX_PKG_VE
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="gl_cv_func_fflush_stdin=yes SORT_SUPPORTS_Z=yes SORT=$TERMUX_PREFIX/bin/applets/sort"
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/oldfind share/man/man1/oldfind.1"
+# Remove locale and updatedb which in Termux is provided by mlocate:
+TERMUX_PKG_RM_AFTER_INSTALL+=" bin/locate bin/updatedb share/man/man1/locate.1 share/man/man1/updatedb.1 share/man/man5/locatedb.5"
