72dd9a3b355d1b0b0eb21702dbb61a04d87b19ea
Fredrik Fornwall <fredrik@fornwall.net>
2015-11-27 06:24:29 -0500

    Fix 32-bit picolisp loading of 'ext'

--- packages/picolisp/build.sh
+++ packages/picolisp/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://picolisp.com
 TERMUX_PKG_DESCRIPTION="Lisp interpreter and application server framework"
 TERMUX_PKG_VERSION=15.11
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://software-lab.de/picoLisp-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_FOLDERNAME=picoLisp
 TERMUX_PKG_BUILD_IN_SRC=true
