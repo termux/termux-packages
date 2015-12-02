c0630a14cb1c42d4edc986f996b397a0e7d60a96
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-02 06:18:16 -0500

    libcurl: Do not link in libidn

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_VERSION=7.45.0
 TERMUX_PKG_SRCURL=http://curl.haxx.se/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
 export TERMUX_CA_BUNDLE=$TERMUX_PREFIX/etc/tls/cert.pem
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --with-ca-bundle=$TERMUX_CA_BUNDLE"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --without-libidn --with-ca-bundle=$TERMUX_CA_BUNDLE"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/curl-config share/man/man1/curl-config.1"
