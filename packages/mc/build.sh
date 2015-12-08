f817e4a9a0d06d471e72cc92bdcd8bc7a29b13a8
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-08 02:46:43 -0500

    mc: Remove no longer needed define

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -5,6 +5,6 @@ TERMUX_PKG_DESCRIPTION="Midnight Commander - a powerful file manager"
 TERMUX_PKG_SRCURL="http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_DEPENDS="ncurses, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
-# mc uses the deprecated S_IREAD and S_IWRITE definitions, which android does not define:
+# mc uses the deprecated S_IWRITE definition, which android does not define:
 # https://code.google.com/p/android/issues/detail?id=19710
-CPPFLAGS="$CPPFLAGS -DS_IREAD=S_IRUSR -DS_IWRITE=S_IWUSR"
+CPPFLAGS="$CPPFLAGS -DS_IWRITE=S_IWUSR"
