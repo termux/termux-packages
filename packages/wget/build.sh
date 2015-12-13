62c2c23b61d4dfbddb0e31ed826535b6cdd479ae
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-13 15:05:31 -0500

    wget: Update from 1.17 to 1.17.1

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
 TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
-TERMUX_PKG_VERSION=1.17
+TERMUX_PKG_VERSION=1.17.1
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl"
