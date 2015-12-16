13633dba6aa52531b1d3f180d5b016155f4142b4
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-15 20:14:15 -0500

    dnsutils: Break out $PREFIX/etc/resolv.conf
    This introduces a new resolv-conf package containing
    $PREFIX/etc/resolv.conf which can be used by other packages.

---
+++ packages/resolv-conf/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://man7.org/linux/man-pages/man5/resolv.conf.5.html
+TERMUX_PKG_DESCRIPTION="Resolver configuration file"
+TERMUX_PKG_VERSION=1.0
+
+termux_step_make_install () {
+	_RESOLV_CONF=$TERMUX_PREFIX/etc/resolv.conf
+	printf "nameserver 8.8.8.8\nnameserver 8.8.4.4" > $_RESOLV_CONF
+}
