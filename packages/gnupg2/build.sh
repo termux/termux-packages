7b40bb69064a7239c89a7ffd9c5e560316dfc541
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-17 06:13:55 -0500

    gnupg2: Build with sqlite disabled for now

--- packages/gnupg2/build.sh
+++ packages/gnupg2/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnupg.org/
 TERMUX_PKG_DESCRIPTION="Implementation of the OpenPGP standard for encrypting and signing data and communication"
 TERMUX_PKG_VERSION=2.1.10
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libassuan,libbz2,libgcrypt,libksba,libnpth,readline"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ldap"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ldap --disable-sqlite"
 # Remove non-english help files and man pages shipped with the gnupg (1) package:
 TERMUX_PKG_RM_AFTER_INSTALL="share/gnupg/help.*.txt share/man/man1/gpg-zip.1 share/man/man7/gnupg.7"
 
