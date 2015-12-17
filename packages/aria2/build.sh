f657b221c00e6ff48a2c50be550d5d38c8b08d41
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-17 09:33:23 -0500

    aria2: Update from 1.19.2 to 1.19.3

--- packages/aria2/build.sh
+++ packages/aria2/build.sh
@@ -1,9 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://aria2.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Multi-protocol & multi-source command-line download utility supporting HTTP/HTTPS, FTP, BitTorrent and Metalink"
-TERMUX_PKG_VERSION=1.19.2
+TERMUX_PKG_VERSION=1.19.3
 TERMUX_PKG_SRCURL=https://github.com/tatsuhiro-t/aria2/releases/download/release-$TERMUX_PKG_VERSION/aria2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="c-ares, openssl, libxml2, libgnustl"
 # sqlite3 is only used for loading cookies from firefox or chrome:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-openssl --without-gnutls --without-libuv --without-sqlite3 ac_cv_search_getaddrinfo=no ac_cv_func_getaddrinfo=yes ac_cv_func_gettimeofday=yes ac_cv_func_sleep=yes ac_cv_func_usleep=yes ac_cv_func_basename=yes"
 
-export CXXFLAGS="$CXXFLAGS -lgnustl_shared"
+termux_step_pre_configure () {
+	export CXXFLAGS="$CXXFLAGS -lgnustl_shared"
+	cd $TERMUX_PKG_SRCDIR
+	autoreconf
+}
