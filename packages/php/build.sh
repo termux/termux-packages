e339be429f8c6f9f2d363002c5bff5c5f0bd94bd
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-22 05:37:56 -0500

    php: Enable phar and openssl module

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -1,20 +1,26 @@
 TERMUX_PKG_HOMEPAGE=http://php.net/
 TERMUX_PKG_DESCRIPTION="Server-side, HTML-embedded scripting language"
-TERMUX_PKG_VERSION=5.6.15
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=5.6.16
 TERMUX_PKG_SRCURL=http://php.net/get/php-${TERMUX_PKG_VERSION}.tar.xz/from/this/mirror
+TERMUX_PKG_NO_SRC_CACHE=yes
+# Build native php for phar to build (see pear-Makefile.frag.patch):
+TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_FOLDERNAME=php-${TERMUX_PKG_VERSION}
-TERMUX_PKG_DEPENDS="libxml2, liblzma"
+TERMUX_PKG_DEPENDS="libxml2, liblzma, openssl, pcre"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-iconv"
 # http://php.net/manual/en/libxml.installation.php
 # "If configure cannot find xml2-config in the directory specified by --with-libxml-dir,
 # then it'll continue on and check the default locations."
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-libxml-dir=$TERMUX_PREFIX"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-phar"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-openssl=$TERMUX_PREFIX"
+# http://php.net/manual/en/pcre.installation.php: pcre always enabled, use platform library:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="  --with-pcre-regex=$TERMUX_PREFIX"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_res_nsearch=no"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --mandir=$TERMUX_PREFIX/share/man"
 
 termux_step_pre_configure () {
+	export NATIVE_PHP_EXECUTABLE=$TERMUX_PKG_HOSTBUILD_DIR/sapi/cli/php
+
 	# Run autoconf since we have patched config.m4 files.
 	cd $TERMUX_PKG_SRCDIR
 	autoconf
