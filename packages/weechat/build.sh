59d4c3f2a8db38690065339d7ce1ebd2b49bd47e
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-25 18:25:10 -0500

    weechat: Rebuild to link against new ruby 2.3 lib

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
 TERMUX_PKG_VERSION=1.3
-TERMUX_PKG_BUILD_REVISION=4
+TERMUX_PKG_BUILD_REVISION=5
 TERMUX_PKG_SRCURL=http://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="ncurses, libgcrypt, libcurl, libgnutls, libandroid-support"
 # weechat-curses is a symlink to weechat, so remove it:
