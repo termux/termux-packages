54cb74f98d3114f0b85cfa7a7ca2a4a478b9ef3e
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-25 19:32:51 -0500

    apt: Update to 1.1.6, default disable SRV records
    By disabling SRV records support in apt by default we work around
    an issue with HTC devices (fixes #99).

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_DEPENDS="liblzma, libgnustl, dpkg, gnupg"
-TERMUX_PKG_VERSION=1.1.5
+TERMUX_PKG_VERSION=1.1.6
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--host=${TERMUX_ARCH}-linux --disable-rpath acl_cv_rpath=$TERMUX_PREFIX/lib gt_cv_func_CFPreferencesCopyAppValue=no gt_cv_func_CFLocaleCopyCurrent=no ac_cv_c_bigendian=no --no-create"
 # When ready to drop bz2 support:
