f83a5d8232d80a5900e88a6411d1a86457832bae
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-25 22:11:28 -0500

    lua: Update from 5.3.1 to 5.3.2

--- packages/lua/build.sh
+++ packages/lua/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.lua.org/
 TERMUX_PKG_DESCRIPTION="Powerful, fast, lightweight, embeddable scripting language"
-TERMUX_PKG_VERSION=5.3.1
+TERMUX_PKG_VERSION=5.3.2
 TERMUX_PKG_SRCURL=http://www.lua.org/ftp/lua-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="readline"
 TERMUX_PKG_EXTRA_MAKE_ARGS=linux
