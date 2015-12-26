e97137242f065544a59bdc917fa428bf0a1ebbd8
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-25 21:53:25 -0500

    libcairo: Update from 1.14.4 to 1.14.6

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://cairographics.org/
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
-TERMUX_PKG_VERSION=1.14.4
+TERMUX_PKG_VERSION=1.14.6
 TERMUX_PKG_SRCURL=http://cairographics.org/releases/cairo-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, libpixman, fontconfig, freetype"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-doc-html --enable-xlib=no"
