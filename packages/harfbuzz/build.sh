f9c08224a570b582c79412a8f587e6a302961361
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-27 06:33:28 -0500

    harfbuzz: Update from 1.0.6 to 1.1.2

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.0.6
+TERMUX_PKG_VERSION=1.1.2
 TERMUX_PKG_SRCURL=http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=no"
