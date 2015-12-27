9cd1f0e3259534e6bf3290aa1f20f9109f435a2b
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-27 07:00:01 -0500

    flex: Update from 2.5.39 to 2.6.0

--- packages/flex/build.sh
+++ packages/flex/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=http://flex.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Fast lexical analyser generator"
-TERMUX_PKG_VERSION=2.5.39
+TERMUX_PKG_VERSION=2.6.0
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/flex/flex-${TERMUX_PKG_VERSION}.tar.xz
