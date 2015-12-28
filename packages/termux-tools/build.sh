561edf1bacf2189f1e031fd2ddc5926fae3811d3
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-27 19:53:30 -0500

    termux-tools: Add termux-setup-storage

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://termux.com/
 TERMUX_PKG_DESCRIPTION="Some tools for Termux"
-TERMUX_PKG_VERSION=0.13
+TERMUX_PKG_VERSION=0.14
 
 termux_step_make_install () {
 	$CXX $CFLAGS $LDFLAGS -std=c++14 -Wall -Wextra -pedantic -Werror $TERMUX_PKG_BUILDER_DIR/*.cpp -o $TERMUX_PREFIX/bin/termux-elf-cleaner
 
-	cp $TERMUX_PKG_BUILDER_DIR/{am,pm,termux-fix-shebang,termux-reload-settings,chsh,termux-open-url} $TERMUX_PREFIX/bin/
+	cp $TERMUX_PKG_BUILDER_DIR/{am,pm,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
 
 	sed -i "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/termux-fix-shebang
 }
