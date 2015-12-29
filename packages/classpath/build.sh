279e44e4ce5c7a91a648b1dbc6ad2b92781420b8
smallpotato000 <smallpotato000@hotmail.com>
2015-12-29 17:35:19 +0800

    Fix GNU Classpath JNI issue.

--- packages/classpath/build.sh
+++ packages/classpath/build.sh
@@ -3,4 +3,4 @@ TERMUX_PKG_DESCRIPTION="GNU Classpath"
 TERMUX_PKG_VERSION=0.99
 TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.gnu.org/gnu/classpath/classpath-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-jni --without-x --disable-gtk-peer --disable-plugin --disable-gconf-peer --disable-Werror"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-peer --disable-plugin --disable-gconf-peer --disable-Werror"
