fc1feb2efc1c8d6488e12528e4a9197f63146be0
Fredrik Fornwall <fredrik@fornwall.net>
2015-12-31 04:36:51 -0500

    Update disabled-packages/nodejs5

--- disabled-packages/nodejs5/build.sh
+++ disabled-packages/nodejs5/build.sh
@@ -2,9 +2,9 @@
 # segfaults in v8::internal::DoubleToRadixCString() at ../deps/v8/src/conversions.cc:445
 TERMUX_PKG_HOMEPAGE=http://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=5.2.0
+TERMUX_PKG_VERSION=5.3.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--dest-os=android --shared-openssl --shared-zlib --shared-libuv"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--dest-os=android --shared-openssl --shared-zlib --shared-libuv --without-snapshot"
 TERMUX_PKG_DEPENDS="openssl, libuv"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
 TERMUX_PKG_BUILD_IN_SRC=yes
