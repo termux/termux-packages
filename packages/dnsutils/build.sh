46898407386ea170bd06d203e7721490c883ff95
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-03 05:59:39 -0500

    dnsutils: Update BIND-9.10.3 to BIND-9.10.3-P2

--- packages/dnsutils/build.sh
+++ packages/dnsutils/build.sh
@@ -1,9 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.isc.org/downloads/bind/
 TERMUX_PKG_DESCRIPTION="Clients provided with BIND"
-TERMUX_PKG_VERSION=9.10.3
-TERMUX_PKG_BUILD_REVISION=6
-TERMUX_PKG_SRCURL="https://www.isc.org/downloads/file/bind-9-10-3/?version=tar-gz"
-TERMUX_PKG_FOLDERNAME="bind-${TERMUX_PKG_VERSION}"
+# NOTE: When changing this version, you also needo to change TERMUX_PKG_SRCURL
+# and TERMUX_PKG_FOLDERNAME.
+TERMUX_PKG_VERSION=9.10.3.2
+# TERMUX_PKG_SRCURL="https://www.isc.org/downloads/file/bind-9-10-3-p2/?version=tar-gz"
+TERMUX_PKG_SRCURL="https://ftp.isc.org/isc/bind/cur/9.10/bind-9.10.3-P2.tar.gz"
+TERMUX_PKG_FOLDERNAME="bind-9.10.3-P2"
 TERMUX_PKG_DEPENDS="openssl, readline, resolv-conf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-gssapi=no --with-randomdev=/dev/random -with-ecdsa=no --with-gost=no --with-libxml2=no --with-libtool"
 
