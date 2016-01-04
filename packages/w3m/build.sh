6c79834fb1bba9402b9aab036df74eb5408f5d47
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-04 06:31:29 -0500

    w3m: Fix 64-bit build

--- packages/w3m/build.sh
+++ packages/w3m/build.sh
@@ -4,4 +4,6 @@ TERMUX_PKG_VERSION=0.5.3
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/w3m/w3m/w3m-$TERMUX_PKG_VERSION/w3m-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="libgc, ncurses, openssl"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_setpgrp_void=yes"
+# ac_cv_func_bcopy=yes to avoid w3m defining it's own bcopy function, which
+# breaks 64-bit builds where NDK headers define bcopy as a mactro:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_setpgrp_void=yes ac_cv_func_bcopy=yes"
