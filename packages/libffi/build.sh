5b6e93862c572ce228a0589de112e2d11fda4c7e
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-04 06:22:38 -0500

    libffi: Do not place lib in lib64 for 64-bit

--- packages/libffi/build.sh
+++ packages/libffi/build.sh
@@ -3,3 +3,10 @@ TERMUX_PKG_DESCRIPTION="Library providing a portable, high level programming int
 TERMUX_PKG_VERSION=3.2.1
 TERMUX_PKG_SRCURL=ftp://sourceware.org/pub/libffi/libffi-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_RM_AFTER_INSTALL="lib/libffi-${TERMUX_PKG_VERSION}/include"
+
+termux_step_post_make_install () {
+	if [ $TERMUX_ARCH_BITS = "64" ]; then
+		# Avoid libffi being placed in lib64 for 64-bit builds:
+		mv $TERMUX_PREFIX/lib64/libffi* $TERMUX_PREFIX/lib/
+	fi
+}
