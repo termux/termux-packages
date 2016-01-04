f98f7adc18f1f5db6b86a9dd6bd28c8f58f9e70c
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-04 06:24:46 -0500

    libgc: Use a master snapshot for instead of 7.4.2
    This fixes building for aarch64

--- packages/libgc/build.sh
+++ packages/libgc/build.sh
@@ -1,7 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://www.hboehm.info/gc/
 TERMUX_PKG_DESCRIPTION="Library providing the Boehm-Demers-Weiser conservative garbage collector"
-TERMUX_PKG_VERSION=7.4.2
-TERMUX_PKG_SRCURL=http://www.hboehm.info/gc/gc_source/gc-${TERMUX_PKG_VERSION}.tar.gz
+# The latest release 7.4.2 is too old for aarch64, use a master snapshot until 7.5:
+TERMUX_PKG_VERSION=7.4.`date "+%Y%m%d%H%M"`
+TERMUX_PKG_SRCURL=https://github.com/ivmai/bdwgc/archive/master.zip
+TERMUX_PKG_FOLDERNAME=bdwgc-master
+TERMUX_PKG_NO_SRC_CACHE=yes
 TERMUX_PKG_RM_AFTER_INSTALL="share/gc"
 
 # Avoid defining structs already defined in api level 21 or beyond
@@ -14,4 +17,6 @@ termux_step_post_extract_package () {
 	cd $TERMUX_PKG_SRCDIR
 	tar xf $TERMUX_PKG_CACHEDIR/$LIBATOMIC_FILE
 	mv libatomic_ops-${LIBATOMIC_VERSION} libatomic_ops
+
+	./autogen.sh
 }
