e4539a949f98823e7d368f9b26db88174bc58634
4679 <btchina@live.com>
2016-01-15 13:20:19 +0800

    add git to ubuntu-setup.sh and Dockerfile,add corkscrew

---
+++ packages/corkscrew/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://www.agroman.net/corkscrew/
+TERMUX_PKG_DESCRIPTION="A tool for tunneling SSH through HTTP proxies"
+TERMUX_PKG_DEPENDS="openssh"
+TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_SRCURL=http://agroman.net/corkscrew/corkscrew-${TERMUX_PKG_VERSION}.tar.gz
