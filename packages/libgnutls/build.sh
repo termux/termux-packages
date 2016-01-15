a49c8db0dda7baf333f8121b48783a9bc08a3466
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-14 22:41:15 -0500

    libgnutls: Depend in libidn

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
-TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates"
+TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn"
 _TERMUX_PKG_MAJOR_VERSION=3.4
 TERMUX_PKG_VERSION=${_TERMUX_PKG_MAJOR_VERSION}.8
 TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${_TERMUX_PKG_MAJOR_VERSION}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
