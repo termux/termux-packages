f3d9c5cf67b1c3b12fdba6118f48512b8c04c501
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-15 05:35:26 -0500

    nano: Update from 2.5.0 to 2.5.1

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.nano-editor.org/
 TERMUX_PKG_DESCRIPTION="Small, free and friendly text editor"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION=2.5.0
+TERMUX_PKG_VERSION=2.5.1
 TERMUX_PKG_SRCURL=http://www.nano-editor.org/dist/v2.5/nano-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support,libandroid-glob,ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-utf8 --disable-libmagic"
