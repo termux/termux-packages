7a9e707f15e51a38424cf78b59f0ed3c7f17a513
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-16 08:51:45 -0500

    hexcurse: Backport patch to show help
    Add patch from unreleased version which shows help if no filename
    argument is supplied.

--- packages/hexcurse/build.sh
+++ packages/hexcurse/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/LonnyGomes/hexcurse
 TERMUX_PKG_DESCRIPTION="Console hexeditor"
 TERMUX_PKG_VERSION=1.60.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/LonnyGomes/hexcurse/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=hexcurse-${TERMUX_PKG_VERSION}
 TERMUX_PKG_DEPENDS="ncurses"
