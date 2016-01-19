d24f3254d1a2b60df6fd73ec914401939a0fa45f
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-19 18:15:17 -0500

    Add libconfig as a disabled package

---
+++ disabled-packages/libconfig/build.sh
@@ -0,0 +1,6 @@
+# Builds, but currently not used by anything (ldc package may use it in the future).
+TERMUX_PKG_HOMEPAGE=http://www.hyperrealm.com/libconfig/
+TERMUX_PKG_DESCRIPTION="Library for processing structured configuration files"
+TERMUX_PKG_VERSION=1.5
+TERMUX_PKG_SRCURL=http://www.hyperrealm.com/libconfig/libconfig-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=libconfig-${TERMUX_PKG_VERSION}
