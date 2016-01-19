7e92f474924354c01b5376dc017914b424770eef
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-19 17:47:48 -0500

    lighttpd: Work around cyanogenmod dlopen problem
    This hopefully fixes #122.

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.lighttpd.net
 TERMUX_PKG_DESCRIPTION="Web server optimized for speed-critical environments while remaining standards-compliant, secure and flexible"
 TERMUX_PKG_VERSION=1.4.39
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
 TERMUX_PKG_DEPENDS="libbz2, openssl, pcre"
