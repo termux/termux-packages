59b03545abd0b27a2bf159461cefd898ce5cb9d0
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-28 17:40:13 -0500

    ca-certificates: Update to latest

--- packages/ca-certificates/build.sh
+++ packages/ca-certificates/build.sh
@@ -1,13 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://curl.haxx.se/docs/caextract.html
 TERMUX_PKG_DESCRIPTION="Common CA certificates"
-TERMUX_PKG_VERSION=20151028
-TERMUX_PKG_BUILD_REVISION=2
+TERMUX_PKG_VERSION=20160122
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
 	CERTFILE=$TERMUX_PKG_TMPDIR/cert.pem
 	curl -o $CERTFILE https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
-	if grep -q 'SHA1: 6d7d2f0a4fae587e7431be191a081ac1257d300a' $CERTFILE; then
+	if grep -q 'SHA1: 0ab47e2f41518f8d223eab517cb799e5b071231e' $CERTFILE; then
 		CERT_DIR=$TERMUX_PREFIX/etc/tls
 		mkdir -p $CERT_DIR
 		mv $CERTFILE $CERT_DIR/cert.pem
