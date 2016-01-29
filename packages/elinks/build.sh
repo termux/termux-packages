af0d7b02802692730b49762f43733466228f33ed
Fredrik Fornwall <fredrik@fornwall.net>
2016-01-29 08:24:49 -0500

    elinks: Avoid linking to libgc

--- packages/elinks/build.sh
+++ packages/elinks/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_VERSION=0.12pre6
 TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://elinks.or.cz/download/elinks-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libexpat, libidn, openssl, libbz2"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-256-colors --with-openssl --mandir=$TERMUX_PREFIX/share/man"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-256-colors --with-openssl --mandir=$TERMUX_PREFIX/share/man --without-gc"
