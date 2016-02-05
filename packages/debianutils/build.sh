08e7f324ff8f06b519c8b90a8697ed214df0e9f4
Fredrik Fornwall <fredrik@fornwall.net>
2016-02-05 18:05:00 -0500

    debianutils: Update from 4.5.1 to 4.7

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/debianutils
 TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installation scripts of Debian packages"
-TERMUX_PKG_VERSION=4.5.1
+TERMUX_PKG_VERSION=4.7
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL="bin/installkernel share/man/man8/installkernel.8"
-TERMUX_PKG_FOLDERNAME=debianutils
+TERMUX_PKG_FOLDERNAME=debianutils-$TERMUX_PKG_VERSION
