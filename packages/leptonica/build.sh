93779b4a2b5ab1599b30a2c5fcf51697c7134408
David Martínez <contacto@davidmartinez.net>
2016-02-06 13:57:02 +0100

    Added Leptonica and Tesseract packages

---
+++ packages/leptonica/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.leptonica.com/
+TERMUX_PKG_DESCRIPTION="Leptonica is a pedagogically-oriented open source site containing software that is broadly useful for image processing and image analysis applications"
+TERMUX_PKG_VERSION=1.73
+TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_DEPENDS="libtiff"
+TERMUX_PKG_SRCURL=http://www.leptonica.com/source/leptonica-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp"
---
+++ packages/tesseract/build.sh
@@ -0,0 +1,29 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tesseract-ocr/tesseract
+TERMUX_PKG_DESCRIPTION="Tesseract is probably the most accurate open source OCR engine available"
+TERMUX_PKG_VERSION=3.04.00
+TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_DEPENDS="libtool, libuuid, leptonica"
+TERMUX_PKG_SRCURL=https://github.com/tesseract-ocr/tesseract/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=tesseract-${TERMUX_PKG_VERSION}
+
+termux_step_pre_configure() {
+	export LIBLEPT_HEADERSDIR=${TERMUX_PREFIX}/include/leptonica
+
+	cd $TERMUX_PKG_SRCDIR
+	perl -p -i -e 's|ADD_RT, true|ADD_RT, false|g' configure.ac
+	./autogen.sh
+}
+
+termux_step_post_make_install() {
+	cd "${TERMUX_PREFIX}/share/tessdata"
+	rm -f eng.*
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.bigrams
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.fold
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.lm
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.nn
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.params
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.size
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.word-freq
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.tesseract_cube.nn
+	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.traineddata
+}
