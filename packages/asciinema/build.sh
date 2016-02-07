f636b95728f896ce8ac977f1df603b4471af8963
Fredrik Fornwall <fredrik@fornwall.net>
2016-02-07 17:05:57 -0500

    asciinema: Add the man page to the package

--- packages/asciinema/build.sh
+++ packages/asciinema/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://asciinema.org/
 TERMUX_PKG_DESCRIPTION="Record and share your terminal sessions, the right way"
 TERMUX_PKG_VERSION=1.1.1
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=asciinema-${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -18,4 +18,6 @@ termux_step_make_install () {
 	cd $GOPATH/src/github.com/asciinema/asciinema
 	PREFIX=$TERMUX_PREFIX make build
 	PREFIX=$TERMUX_PREFIX make install
+
+	cp $TERMUX_PKG_SRCDIR/man/asciinema.1 $TERMUX_PREFIX/share/man/man1/
 }
