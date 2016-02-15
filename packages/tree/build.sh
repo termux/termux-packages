7248502f4f59dc050999697e84ebc604d3942272
Fredrik Fornwall <fredrik@fornwall.net>
2016-02-15 06:40:27 -0500

    tree: Shorten description

--- packages/tree/build.sh
+++ packages/tree/build.sh
@@ -1,8 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://mama.indstate.edu/users/ice/tree/
-TERMUX_PKG_DESCRIPTION="displays an indented directory tree, in color
- Tree is a recursive directory listing command that produces a depth indented
- listing of files, which is colorized ala dircolors if the LS_COLORS environment
- variable is set and output is to tty."
+TERMUX_PKG_DESCRIPTION="Recursive directory lister producing a depth indented listing of files"
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_VERSION=1.7.0
 TERMUX_PKG_MAINTAINER=$(echo "Gert Scholten <gscholt at gmail dot com>" | sed 's/\sat\s/@/' | sed 's/\sdot\s/./')
