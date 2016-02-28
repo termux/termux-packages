de7b01b870cd34ab534e17d5f7e6bf4956d047cf
Fredrik Fornwall <fredrik@fornwall.net>
2016-02-28 01:59:43 -0500

    libsqlite: Update from 3.10.1 to 3.11.0

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=http://www.sqlite.org/
 TERMUX_PKG_DESCRIPTION="Software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine"
-TERMUX_PKG_VERSION=3.10.1
+TERMUX_PKG_VERSION=3.11.0
 TERMUX_PKG_SRCURL=http://www.sqlite.org/2015/sqlite-autoconf-3080803.tar.gz
