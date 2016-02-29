035e27f9f5a34a39ebd918392a23ebe0f31d848b
Fredrik Fornwall <fredrik@fornwall.net>
2016-02-29 01:54:01 -0500

    glib: Specify --disable-znodelete
    This avoids the ELF DF_1_NODELETE value being used which most Android
    5.0 linkers does not support.

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -2,12 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 _TERMUX_GLIB_MAJOR_VERSION=2.46
 TERMUX_PKG_VERSION=${_TERMUX_GLIB_MAJOR_VERSION}.2
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/glib/${_TERMUX_GLIB_MAJOR_VERSION}/glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libffi, pcre"
 
 # --enable-compile-warnings=no to get rid of format strings causing errors
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-compile-warnings --disable-gtk-doc --disable-gtk-doc-html --cache-file=termux_configure.cache --with-pcre=system"
+# --disable-znodelete to avoid DF_1_NODELETE which most Android 5.0 linkers
+# does not support:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-znodelete"
 TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc share/locale share/glib-2.0/gettext share/gdb/auto-load share/glib-2.0/codegen share/glib-2.0/gdb lib/glib-2.0/include bin/gtester-report bin/glib-mkenums bin/glib-gettextize bin/gdbus-codegen"
 
 # glib checks for __BIONIC__ instead of __ANDROID__
