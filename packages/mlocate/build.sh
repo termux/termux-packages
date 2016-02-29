08a9993c661f525b89b24be2eef96d7ac409e931
Fredrik Fornwall <fredrik@fornwall.net>
2016-02-29 12:33:44 -0500

    mlocate: Index home/ in addition to usr/

--- packages/mlocate/build.sh
+++ packages/mlocate/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://fedorahosted.org/mlocate/
 TERMUX_PKG_DESCRIPTION="Tool to find files anywhere in the filesystem based on their name"
 TERMUX_PKG_VERSION=0.26
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://fedorahosted.org/releases/m/l/mlocate/mlocate-${TERMUX_PKG_VERSION}.tar.xz
 
 CPPFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
