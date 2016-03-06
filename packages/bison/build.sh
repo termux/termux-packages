8de869ffaacf425812fbbcc195f5a5aef50811a0
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-06 01:19:52 -0500

    bison: Update build revision

--- packages/bison/build.sh
+++ packages/bison/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/bison/
 TERMUX_PKG_DESCRIPTION="General-purpose parser generator"
 TERMUX_PKG_VERSION=3.0.4
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/bison/bison-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_HOSTBUILD=true
