2e182e93718b94f6877d7b8a26b52d56434521fe
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-06 02:39:51 -0500

    hunspell-en-us: Mark as platform-independent

--- packages/hunspell-en-us/build.sh
+++ packages/hunspell-en-us/build.sh
@@ -1,7 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://sourceforge.net/projects/hunspell/files/Spelling%20dictionaries/en_US/
 TERMUX_PKG_DESCRIPTION="American english dictionary for hunspell"
 TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_DEPENDS="hunspell"
+TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
 	curl -L http://downloads.sourceforge.net/project/hunspell/Spelling%20dictionaries/en_US/en_US.zip > en_US.zip
