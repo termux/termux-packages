1a850952e5993656483225f65e20215e325f0252
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-07 18:47:26 -0500

    bash-completion: Update from 2.1 to 2.2

--- packages/bash-completion/build.sh
+++ packages/bash-completion/build.sh
@@ -1,7 +1,7 @@
-TERMUX_PKG_HOMEPAGE=https://bash-completion.alioth.debian.org/
+TERMUX_PKG_HOMEPAGE=https://github.com/scop/bash-completion
 TERMUX_PKG_DESCRIPTION="Programmable completion for the bash shell"
-TERMUX_PKG_VERSION=2.1
-TERMUX_PKG_BUILD_REVISION=1
-TERMUX_PKG_SRCURL=https://bash-completion.alioth.debian.org/files/bash-completion-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_VERSION=2.2
+TERMUX_PKG_SRCURL=https://github.com/scop/bash-completion/releases/download/${TERMUX_PKG_VERSION}/bash-completion-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_FOLDERNAME=bash-completion-${TERMUX_PKG_VERSION}
 TERMUX_PKG_DEPENDS="bash"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
