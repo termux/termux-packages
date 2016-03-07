47013c9ddc30e358538089d6ccbf7f22fca0a425
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-07 18:41:26 -0500

    c-ares: Update from 1.10 to 1.11

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -1,5 +1,4 @@
 TERMUX_PKG_HOMEPAGE=http://c-ares.haxx.se/
 TERMUX_PKG_DESCRIPTION="C library for asynchronous DNS requests (including name resolves)"
-TERMUX_PKG_VERSION=1.10.0
+TERMUX_PKG_VERSION=1.11.0
 TERMUX_PKG_SRCURL=http://c-ares.haxx.se/download/c-ares-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_BUILD_REVISION=1
