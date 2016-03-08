71963d19cc1e0821536212e8c5057e40b5db9ba8
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-07 19:00:16 -0500

    fzf: Update from 0.11.3 to 0.11.4

--- packages/fzf/build.sh
+++ packages/fzf/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/junegunn/fzf
 TERMUX_PKG_DESCRIPTION="Command-line fuzzy finder"
-TERMUX_PKG_VERSION=0.11.3
+TERMUX_PKG_VERSION=0.11.4
 TERMUX_PKG_SRCURL=https://github.com/junegunn/fzf/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=fzf-${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_IN_SRC="yes"
