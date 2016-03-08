8604f801a0676b2a26842394afa246dbb550b814
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-07 20:03:27 -0500

    libpixman: Update from 0.32.8 to 0.34.0

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=http://www.pixman.org/
 TERMUX_PKG_DESCRIPTION="Low-level library for pixel manipulation"
-TERMUX_PKG_VERSION=0.32.8
+TERMUX_PKG_VERSION=0.34.0
 TERMUX_PKG_SRCURL=http://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -2,5 +2,6 @@ TERMUX_PKG_HOMEPAGE=http://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 _MAJOR_VERSION=1.39
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/pango/${_MAJOR_VERSION}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="fontconfig,glib,harfbuzz,libcairo"
