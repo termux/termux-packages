3dcfb6c36826c9d63b11ce34cf536a1bbcedde9c
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-07 19:16:57 -0500

    iperf3: Update from 3.1.1 to 3.1.2

--- packages/iperf3/build.sh
+++ packages/iperf3/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/esnet/iperf
 TERMUX_PKG_DESCRIPTION="TCP, UDP, and SCTP network bandwidth measurement tool"
-TERMUX_PKG_VERSION=3.1.1
+TERMUX_PKG_VERSION=3.1.2
 TERMUX_PKG_SRCURL=http://downloads.es.net/pub/iperf/iperf-${TERMUX_PKG_VERSION}.tar.gz
