b4dab8e7c5b7323daeed392aca233161a0cedf81
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-14 22:15:40 -0400

    mc: Make mcedit work
    Fixes #165.

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_VERSION=4.8.16
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_HOMEPAGE=https://www.midnight-commander.org/
 TERMUX_PKG_DESCRIPTION="Midnight Commander - a powerful file manager"
 TERMUX_PKG_SRCURL="http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_DEPENDS="ncurses, glib"
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
 # mc uses the deprecated S_IWRITE definition, which android does not define:
 # https://code.google.com/p/android/issues/detail?id=19710
