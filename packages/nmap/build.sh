732c17a6881d5033e40835698f86dd86a0ba8135
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-18 16:45:12 -0400

    netcat: Split out package from nmap
    Split ncat (https://nmap.org/ncat/), the netcat implementation from
    the nmap project out into it's own package.
    
    Also setup netcat as a symlink to ncat for discoverability and
    for users used to netcat.
    
    Closes #162.

--- packages/nmap/build.sh
+++ packages/nmap/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://nmap.org/
 TERMUX_PKG_DESCRIPTION="Utility for network discovery and security auditing"
-TERMUX_PKG_VERSION=7.01
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=7.10
 TERMUX_PKG_SRCURL=https://nmap.org/dist/nmap-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libpcap, pcre, openssl, resolv-conf"
 # --without-nmap-update to avoid linking against libsvn_client:
@@ -10,3 +9,13 @@ TERMUX_PKG_DEPENDS="libpcap, pcre, openssl, resolv-conf"
 # --without-ndiff to avoid python2-using ndiff utility:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-static --with-liblua=included --without-nmap-update --without-zenmap --without-ndiff"
 TERMUX_PKG_BUILD_IN_SRC="yes"
+
+
+termux_step_post_make_install () {
+	# Setup netcat as symlink to ncat, since the other netcat implementations are outdated (gnu-netcat)
+	# or non-portable (openbsd-netcat).
+	cd $TERMUX_PREFIX/bin
+	ln -s -f ncat netcat
+	cd $TERMUX_PREFIX/share/man/man1
+	ln -s -f ncat.1 netcat.1
+}
