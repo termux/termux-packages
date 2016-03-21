afd500a774c78ce60ee799509846d4e6aa559939
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-22 00:11:52 +0100

    Add pass package (closes #147)

---
+++ packages/pass/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.passwordstore.org/
+TERMUX_PKG_DESCRIPTION="Lightweight directory-based password manager"
+TERMUX_PKG_VERSION=1.6.5
+TERMUX_PKG_SRCURL=https://git.zx2c4.com/password-store/snapshot/password-store-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="bash, coreutils, gpg, pwgen, tree"
+TERMUX_PKG_PLATFORM_INDEPENDENT=yes
+TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
