53372a3ce9248c36735e98a7151389822338d612
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-21 21:08:37 -0400

    proot: Build next branch instead of master
    This fixes aarch64 to work.

--- disabled-packages/proot/build.sh
+++ disabled-packages/proot/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://proot.me/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
-TERMUX_PKG_VERSION=5.1.0
-TERMUX_PKG_SRCURL=https://github.com/proot-me/PRoot/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_FOLDERNAME=PRoot-${TERMUX_PKG_VERSION}
+TERMUX_PKG_VERSION=5.1.99
+TERMUX_PKG_SRCURL=https://github.com/proot-me/PRoot/archive/next.zip
+TERMUX_PKG_FOLDERNAME=PRoot-next
 TERMUX_PKG_DEPENDS="libtalloc"
 
 export LD=$CC
