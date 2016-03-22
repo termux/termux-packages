8b352090bfee2d5b9b66610366e0f4bcea2e1643
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-21 20:08:39 -0400

    termux-tools: Add ping and logcat wrappers

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://termux.com/
 TERMUX_PKG_DESCRIPTION="Some tools for Termux"
-TERMUX_PKG_VERSION=0.16
+TERMUX_PKG_VERSION=0.17
 
 termux_step_make_install () {
 	$CXX $CFLAGS $LDFLAGS -std=c++14 -Wall -Wextra -pedantic -Werror $TERMUX_PKG_BUILDER_DIR/*.cpp -o $TERMUX_PREFIX/bin/termux-elf-cleaner
 
-	cp -p $TERMUX_PKG_BUILDER_DIR/{am,pm,df,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
+	cp -p $TERMUX_PKG_BUILDER_DIR/{am,pm,df,ping,logcat,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
 }
