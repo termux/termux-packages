dcad05c5aa9a4e2054730e1b6a82176c6c69319f
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-21 21:34:26 -0400

    termux-tools: Add dalvikvm wrapper

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,9 +1,17 @@
 TERMUX_PKG_HOMEPAGE=http://termux.com/
 TERMUX_PKG_DESCRIPTION="Some tools for Termux"
-TERMUX_PKG_VERSION=0.17
+TERMUX_PKG_VERSION=0.18
 
 termux_step_make_install () {
 	$CXX $CFLAGS $LDFLAGS -std=c++14 -Wall -Wextra -pedantic -Werror $TERMUX_PKG_BUILDER_DIR/*.cpp -o $TERMUX_PREFIX/bin/termux-elf-cleaner
 
-	cp -p $TERMUX_PKG_BUILDER_DIR/{am,pm,df,ping,logcat,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
+	# Remove LD_LIBRARY_PATH from environment to avoid conflicting
+	# with system libraries that am may link against.
+	for tool in am dalvikvm df logcat ping pm; do
+		echo '#!/bin/sh' > $TERMUX_PREFIX/bin/$tool
+		echo "LD_LIBRARY_PATH= exec /system/bin/$tool \$@" >> $TERMUX_PREFIX/bin/$tool
+		chmod +x $TERMUX_PREFIX/bin/$tool
+	done
+
+	cp -p $TERMUX_PKG_BUILDER_DIR/{termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
 }
