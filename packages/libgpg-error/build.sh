5f2951bea2faf1280206ffd512a5281c404c49a6
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-23 07:32:44 -0400

    libgpg-error: Fix posix lock objet on aarch64
    Bump build revision on dependant packages since this changes the
    libgpg ABI.

--- packages/libgpg-error/build.sh
+++ packages/libgpg-error/build.sh
@@ -1,12 +1,21 @@
-TERMUX_PKG_VERSION=1.21
 TERMUX_PKG_HOMEPAGE=http://www.gnupg.org/related_software/libgpg-error/
 TERMUX_PKG_DESCRIPTION="Small library that defines common error values for all GnuPG components"
+TERMUX_PKG_VERSION=1.21
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_RM_AFTER_INSTALL="share/common-lisp"
 
-termux_step_post_configure () {
-	# To fix non-arm builds, see:
-	# https://lists.gnupg.org/pipermail/gnupg-devel/2014-January/028203.html
-	# https://gitorious.org/vlc/vlc/commit/3054560987971aff19c496db38834458f8c29377
-	cp $TERMUX_PKG_SRCDIR/src/syscfg/lock-obj-pub.arm-unknown-linux-androideabi.h $TERMUX_PKG_SRCDIR/src/syscfg/lock-obj-pub.linux-android.h
+termux_step_post_extract_package () {
+	# Upstream only has Android definitions for platform-specific lock objects.
+	# See https://lists.gnupg.org/pipermail/gnupg-devel/2014-January/028203.html
+	# for how to generate a lock-obj header file on devices.
+
+	# For aarch64 this was generated on a device:
+	cp $TERMUX_PKG_BUILDER_DIR/lock-obj-pub.aarch64-unknown-linux-android.h $TERMUX_PKG_SRCDIR/src/syscfg/
+
+	if [ $TERMUX_ARCH = i686 ]; then
+		# Android i686 has same config as arm (verified by generating a file on a i686 device):
+		cp $TERMUX_PKG_SRCDIR/src/syscfg/lock-obj-pub.arm-unknown-linux-androideabi.h \
+		   $TERMUX_PKG_SRCDIR/src/syscfg/lock-obj-pub.linux-android.h
+	fi
 }
