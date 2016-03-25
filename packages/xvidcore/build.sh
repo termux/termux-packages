1b3fe9854c6803496eeea5f1a0e0516aba39db1c
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-24 21:38:17 -0400

    xvidcore: Avoid text relocations on i686

--- packages/xvidcore/build.sh
+++ packages/xvidcore/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.xvid.org/
 TERMUX_PKG_DESCRIPTION="High performance and high quality MPEG-4 library"
 TERMUX_PKG_VERSION=1.3.4
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.xvid.org/downloads/xvidcore-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=xvidcore
 
@@ -10,5 +11,10 @@ termux_step_pre_configure () {
 	rm -f $TERMUX_PREFIX/lib/libxvid*
 	export TERMUX_PKG_BUILDDIR=$TERMUX_PKG_BUILDDIR/build/generic
 	export TERMUX_PKG_SRCDIR=$TERMUX_PKG_BUILDDIR
+
+	if [ $TERMUX_ARCH = i686 ]; then
+		# Avoid text relocations:
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-assembly"
+	fi
 }
 
