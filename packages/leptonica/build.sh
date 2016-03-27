f27308373c06bcfd2891d6faa804f577c9a68da1
David Martínez <contacto@davidmartinez.net>
2016-03-27 13:21:42 +0200

    Added fmemopen support to Leptonica

--- packages/leptonica/build.sh
+++ packages/leptonica/build.sh
@@ -4,4 +4,20 @@ TERMUX_PKG_VERSION=1.73
 TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff"
 TERMUX_PKG_SRCURL=http://www.leptonica.com/source/leptonica-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp CPPFLAGS=-I/data/data/com.termux/files/usr/include"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp"
+
+termux_step_pre_configure() {
+	export ac_cv_func_fmemopen=yes
+}
+
+termux_step_post_configure() {
+	# add fmemopen support from https://github.com/j-jorge/android-stdioext
+	cd $TERMUX_PKG_SRCDIR/src
+
+	wget --quiet https://raw.githubusercontent.com/j-jorge/android-stdioext/master/include/stdioext.h
+	wget --quiet https://github.com/j-jorge/android-stdioext/raw/master/src/fmemopen.c
+	wget --quiet https://github.com/j-jorge/android-stdioext/raw/master/src/fopencookie.c
+	wget --quiet https://raw.githubusercontent.com/j-jorge/android-stdioext/master/src/open_memstream.c
+
+	echo '#include "stdioext.h"' >> alltypes.h
+}
--- packages/tesseract/build.sh
+++ packages/tesseract/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/tesseract-ocr/tesseract
 TERMUX_PKG_DESCRIPTION="Tesseract is probably the most accurate open source OCR engine available"
-TERMUX_PKG_VERSION=3.04.00
+TERMUX_PKG_VERSION=3.04.01
 TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_DEPENDS="libtool, libuuid, leptonica"
 TERMUX_PKG_SRCURL=https://github.com/tesseract-ocr/tesseract/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=tesseract-${TERMUX_PKG_VERSION}
 
 termux_step_pre_configure() {
+	# http://blog.matt-swain.com/post/26419042500/installing-tesseract-ocr-on-mac-os-x-lion
 	export LIBLEPT_HEADERSDIR=${TERMUX_PREFIX}/include/leptonica
 
 	cd $TERMUX_PKG_SRCDIR
@@ -15,6 +16,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_post_make_install() {
+	# download english trained data
 	cd "${TERMUX_PREFIX}/share/tessdata"
 	rm -f eng.*
 	wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.cube.bigrams
