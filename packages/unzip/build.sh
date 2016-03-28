22797e2894add79ea26e7708334a45fd73ead1e2
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-28 06:02:02 -0400

    unzip: Force updated build for aarch64

--- packages/unzip/build.sh
+++ packages/unzip/build.sh
@@ -1,4 +1,5 @@
 TERMUX_PKG_VERSION=6.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_HOMEPAGE=http://www.info-zip.org/
 TERMUX_PKG_DESCRIPTION="Tools for working with zip files"
 TERMUX_PKG_SRCURL="http://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz"
