786d3f9071716164a5a7190f5e041f3bd03957fd
Fredrik Fornwall <fredrik@fornwall.net>
2016-03-29 17:53:50 -0400

    gzip: Update from 1.6 to 1.7

--- packages/gzip/build.sh
+++ packages/gzip/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gzip/
 TERMUX_PKG_DESCRIPTION="Standard GNU file compression utilities"
-TERMUX_PKG_VERSION=1.6
+TERMUX_PKG_VERSION=1.7
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/gzip/gzip-${TERMUX_PKG_VERSION}.tar.xz
