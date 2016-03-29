a15ca16ffac2df8a79ee88384a3047685ec91592
David Martínez <contacto@davidmartinez.net>
2016-03-29 23:51:57 +0200

    Added zbar package

---
+++ packages/zbar/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://zbar.sourceforge.net
+TERMUX_PKG_DESCRIPTION="ZBar is an open source software suite for reading bar codes from various sources"
+TERMUX_PKG_VERSION=0.10
+TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_DEPENDS="imagemagick"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthread --disable-video --without-xshm --without-xv --without-gtk --without-qt --without-python --mandir=$TERMUX_PREFIX/share/man"
