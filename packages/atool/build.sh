5fd824d9676e73d90ddced8096794eb33b6b3af9
Gore Liu <goreliu@126.com>
2016-04-01 17:35:25 +0800

    atool: Add package

---
+++ packages/atool/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/atool
+TERMUX_PKG_DESCRIPTION="A script for managing file archives of various types"
+TERMUX_PKG_VERSION=0.39.0
+TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_SRCURL=https://savannah.nongnu.org/download/atool/atool-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="file, perl"
