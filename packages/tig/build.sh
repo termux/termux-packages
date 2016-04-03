752b897f47d2ebae3afbf790d4e1d792171340cc
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-03 12:41:55 -0400

    tig: Rebuild with updated 64-bit toolchain
    Fixes #197

--- packages/tig/build.sh
+++ packages/tig/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://jonas.nitro.dk/tig/
 TERMUX_PKG_DESCRIPTION="Ncurses-based text-mode interface for git"
 TERMUX_PKG_VERSION=2.1.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://jonas.nitro.dk/tig/releases/tig-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="ncurses, git, libandroid-support"
