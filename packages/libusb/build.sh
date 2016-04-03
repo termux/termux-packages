fd9ab71df574e9ad64e8b9e613026a05c612d515
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2016-04-03 02:06:08 +0200

    Add libusb

---
+++ packages/libusb/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://www.linux-usb.org/
+TERMUX_PKG_DESCRIPTION="userspace USB programming library"
+TERMUX_PKG_VERSION=0.1.12
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libusb/files/libusb-0.1%20%28LEGACY%29/0.1.12/libusb-0.1.12.tar.gz
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE=bin/libusb-config
