8ffb4cc20bf470c77b50dbd365d33a414a2736ae
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-09 17:14:25 -0400

    protobuf: Link against libgnustl_shared.so
    This avoids the situation where protobuf uses the default stripped
    down standard Android C++ library while e.g. mosh links to the more
    complete libgnustl_shared.so, which causes problems when mosh links
    both to libprotobuf.so and libgnustl_shared.so.

--- packages/protobuf/build.sh
+++ packages/protobuf/build.sh
@@ -1,7 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://code.google.com/p/protobuf/
 TERMUX_PKG_DESCRIPTION="Library for encoding structured data in an efficient yet extensible format"
 TERMUX_PKG_VERSION=2.6.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/google/protobuf/releases/download/v${TERMUX_PKG_VERSION}/protobuf-${TERMUX_PKG_VERSION}.tar.bz2
 # Build a host build first and use the host build protoc:
 TERMUX_PKG_HOSTBUILD=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-protoc=$TERMUX_PKG_HOSTBUILD_DIR/src/protoc"
+# Link against libgnustl_shared.so so that other C++ programs that
+# uses protobuf (e.g. mosh) may use libgnustl_shared.so.
+TERMUX_PKG_DEPENDS="libgnustl"
+
+LDFLAGS+=" -lgnustl_shared"
