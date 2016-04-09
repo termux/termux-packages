2145cf9068768c08c070226e485fbb834ea12edc
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-09 17:13:07 -0400

    mosh: Build from git master

--- disabled-packages/mosh/build.sh
+++ disabled-packages/mosh/build.sh
@@ -1,9 +1,18 @@
 TERMUX_PKG_HOMEPAGE=http://mosh.mit.edu/
 TERMUX_PKG_DESCRIPTION="Mobile shell that supports roaming and intelligent local echo"
-TERMUX_PKG_VERSION=1.2.5
+TERMUX_PKG_VERSION=1.2.5.20160402
 TERMUX_PKG_SRCURL=http://mosh.mit.edu/mosh-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libandroid-support, protobuf, ncurses, openssl, libutil, perl"
+_COMMIT=f30738e3256e90850e945c08624fce90b1ba78a1
+TERMUX_PKG_SRCURL=https://github.com/mobile-shell/mosh/archive/${_COMMIT}.zip
+TERMUX_PKG_FOLDERNAME=mosh-${_COMMIT}
+
+TERMUX_PKG_DEPENDS="libandroid-support, protobuf, ncurses, openssl, libutil, perl, libgnustl"
 
 export PROTOC=$TERMUX_TOPDIR/protobuf/host-build/src/protoc
 
 LDFLAGS+=" -lgnustl_shared"
+
+termux_step_pre_configure () {
+	cd $TERMUX_PKG_SRCDIR
+	./autogen.sh
+}
