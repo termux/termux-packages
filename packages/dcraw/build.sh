b7cf71a02fa21d984b825e3be4b27090f42c56d2
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-11 06:06:24 -0400

    dcraw: Install binary into $PREFIX/bin/ correctly
    Thanks live_the_dream

--- packages/dcraw/build.sh
+++ packages/dcraw/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.cybercom.net/~dcoffin/dcraw/
 TERMUX_PKG_DESCRIPTION="Raw digital camera images decoding utility"
 TERMUX_PKG_VERSION=9.26.0
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=dcraw
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -9,7 +9,7 @@ TERMUX_PKG_DEPENDS="littlecms, libjasper"
 
 termux_step_make_install () {
 	# See the "install" script for flags:
-	$CC $CFLAGS $CPPFLAGS $LDFLAGS dcraw.c $TERMUX_PKG_BUILDER_DIR/swab.c -lm -ljasper -ljpeg -llcms2 -o $TERMUX_PREFIX/dcraw
+	$CC $CFLAGS $CPPFLAGS $LDFLAGS dcraw.c $TERMUX_PKG_BUILDER_DIR/swab.c -lm -ljasper -ljpeg -llcms2 -o $TERMUX_PREFIX/bin/dcraw
         chmod +w dcraw.1 # Add missing write permission
 	cp dcraw.1 $TERMUX_PREFIX/share/man/man1/
 }
