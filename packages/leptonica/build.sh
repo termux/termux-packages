2763362397c1cc9f3bc9c63e12fd5b667f679fef
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-11 09:15:26 -0400

    leptonica: Avoid linking against openjpeg

--- packages/leptonica/build.sh
+++ packages/leptonica/build.sh
@@ -3,4 +3,4 @@ TERMUX_PKG_DESCRIPTION="Leptonica is a pedagogically-oriented open source site c
 TERMUX_PKG_VERSION=1.73
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff"
 TERMUX_PKG_SRCURL=http://www.leptonica.com/source/leptonica-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp --without-libopenjpeg"
