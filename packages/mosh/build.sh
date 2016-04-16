9c03757c06b5a10764f9d11f5a71964b9490f733
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-16 17:17:07 -0400

    mosh: Depend on openssh

--- packages/mosh/build.sh
+++ packages/mosh/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://mosh.mit.edu/
 TERMUX_PKG_DESCRIPTION="Mobile shell that supports roaming and intelligent local echo"
 TERMUX_PKG_VERSION=1.2.5.20160402
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://mosh.mit.edu/mosh-${TERMUX_PKG_VERSION}.tar.gz
 _COMMIT=f30738e3256e90850e945c08624fce90b1ba78a1
 TERMUX_PKG_SRCURL=https://github.com/mobile-shell/mosh/archive/${_COMMIT}.zip
 TERMUX_PKG_FOLDERNAME=mosh-${_COMMIT}
 
-TERMUX_PKG_DEPENDS="libandroid-support, protobuf, ncurses, openssl, libutil, perl, libgnustl"
+TERMUX_PKG_DEPENDS="libandroid-support, protobuf, ncurses, openssl, openssh, libutil, perl, libgnustl"
 
 export PROTOC=$TERMUX_TOPDIR/protobuf/host-build/src/protoc
 
