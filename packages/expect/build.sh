684c863203d4520caf89fb5b6d5eebdd8739579a
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-18 00:52:30 -0400

    expect: Depend on libutil

--- packages/expect/build.sh
+++ packages/expect/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://expect.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Tool for automating interactive terminal applications"
 TERMUX_PKG_VERSION=5.45
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/expect/Expect/${TERMUX_PKG_VERSION}/expect${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="tcl"
+TERMUX_PKG_DEPENDS="libutil, tcl"
 
 termux_step_pre_configure () {
 	cd $TERMUX_PKG_SRCDIR
