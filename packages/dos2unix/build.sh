a5b5a7d4fe02b0c65f0352a7b13bba49301dcdc9
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-18 00:37:44 -0400

    dos2unix: Add package

---
+++ packages/dos2unix/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://dos2unix.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="Converts between DOS and Unix text files"
+TERMUX_PKG_VERSION=7.3.3
+TERMUX_PKG_SRCURL=http://waterlan.home.xs4all.nl/dos2unix/dos2unix-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="libandroid-support"
