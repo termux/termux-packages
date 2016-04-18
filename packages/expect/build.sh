257e5f01711732d8ffcd30c44a69de84d942ecd1
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-18 00:37:30 -0400

    expect: Add package

---
+++ packages/expect/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=http://expect.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="Tool for automating interactive terminal applications"
+TERMUX_PKG_VERSION=5.45
+TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/expect/Expect/${TERMUX_PKG_VERSION}/expect${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="tcl"
+
+termux_step_pre_configure () {
+	cd $TERMUX_PKG_SRCDIR
+	autoconf
+}
+
+termux_step_post_make_install () {
+	cd $TERMUX_PREFIX/lib
+	ln -f -s expect${TERMUX_PKG_VERSION}/libexpect${TERMUX_PKG_VERSION}.so .
+}
