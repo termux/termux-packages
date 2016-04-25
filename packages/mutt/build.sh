7a5787d985fee44e8456f7b4e95628b1f4dcbda6
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-25 03:54:02 -0400

    mutt: Disable built-in gpgme support
    Most people seem to use the traditional way of calling gpg directly
    from mutt, and the gpgme integration doesn't seem to work at least
    in Termux, so remove it. Closes #152.
    
    Also update the version of the neomutt patches used.

--- packages/mutt/build.sh
+++ packages/mutt/build.sh
@@ -1,23 +1,24 @@
 TERMUX_PKG_HOMEPAGE=http://www.mutt.org/
 TERMUX_PKG_DESCRIPTION="Mail client with patches from neomutt"
-TERMUX_PKG_VERSION=1.6.0
-TERMUX_PKG_BUILD_REVISION=1
-TERMUX_PKG_SRCURL=ftp://ftp.mutt.org/pub/mutt/mutt-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libandroid-support, ncurses, gdbm, openssl, libsasl, gpgme"
+_MUTT_VERSION=1.6.0
+_NEOMUTT_PATCHES=20160416
+TERMUX_PKG_VERSION=${_MUTT_VERSION}.$_NEOMUTT_PATCHES
+TERMUX_PKG_SRCURL=ftp://ftp.mutt.org/pub/mutt/mutt-${_MUTT_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses, gdbm, openssl, libsasl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-smtp --enable-imap --enable-pop --with-mailpath=$TERMUX_PREFIX/var/mail --with-ssl --enable-compressed --without-idn --enable-hcache --with-sasl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-exec-shell=$TERMUX_PREFIX/bin/sh"
 # The mutt autoconf guesses no for working (v)snprintf and uses broken local versions - avoid that:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" mutt_cv_c99_snprintf=yes mutt_cv_c99_vsnprintf=yes"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-gpgme"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-gpgme"
 # TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-sasl"
 # bin/{flea,muttbug}: File bug against mutt:
 TERMUX_PKG_RM_AFTER_INSTALL="bin/flea bin/muttbug share/man/man1/muttbug.1 share/man/man1/flea.1"
 
 termux_step_post_extract_package () {
-	local PATCHES_TARNAME=neomutt-patches-20160404.tar.gz
-	local PATCHES_TARPATH=$TERMUX_PKG_CACHEDIR/neomutt-patches-20160404.tar.gz
+	local PATCHES_TARNAME=neomutt-patches-${_NEOMUTT_PATCHES}.tar.gz
+	local PATCHES_TARPATH=$TERMUX_PKG_CACHEDIR/neomutt-patches-${_NEOMUTT_PATCHES}.tar.gz
 	if [ ! -f $PATCHES_TARPATH ]; then
-		curl --retry 3 -L -o $PATCHES_TARPATH https://github.com/neomutt/neomutt/releases/download/neomutt-20160404/$PATCHES_TARNAME
+		curl --retry 3 -L -o $PATCHES_TARPATH https://github.com/neomutt/neomutt/releases/download/neomutt-${_NEOMUTT_PATCHES}/$PATCHES_TARNAME
 	fi
 
 	local PATCHES_DIR=$TERMUX_PKG_TMPDIR/neomutt-patches
@@ -25,7 +26,7 @@ termux_step_post_extract_package () {
 	$TERMUX_TAR -xf $PATCHES_TARPATH --directory $PATCHES_DIR
 
 	cd $TERMUX_PKG_SRCDIR
-	patch --forward -p1 < $PATCHES_DIR/neomutt-20160404/neomutt-20160404.patch || true
+	patch --forward -p1 < $PATCHES_DIR/neomutt-${_NEOMUTT_PATCHES}/neomutt-${_NEOMUTT_PATCHES}.patch || true
 }
 
 termux_step_post_configure () {
