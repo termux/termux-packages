05a4ec7133c44d51c020c13c56b05b919c8c324e
David Martínez <vaites@users.noreply.github.com>
2016-04-27 00:26:51 +0200

    Fixed download url for libmcrypt

--- packages/libmcrypt/build.sh
+++ packages/libmcrypt/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://mcrypt.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="MCrypt is a replacement for the old crypt() package and crypt(1) command, with extensions"
 TERMUX_PKG_VERSION=2.5.8
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.bz2
+TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/mcrypt/Libmcrypt/${TERMUX_PKG_VERSION}/libmcrypt-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_BUILD_IN_SRC=yes
 
