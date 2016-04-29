c36896f2ea2dfe8a461ea705ac5b5833bd025334
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-29 10:11:50 -0400

    libgd: Add --without-vpx for now

--- packages/libgd/build.sh
+++ packages/libgd/build.sh
@@ -3,3 +3,6 @@ TERMUX_PKG_DESCRIPTION="GD is an open source code library for the dynamic creati
 TERMUX_PKG_VERSION=2.1.1
 TERMUX_PKG_SRCURL=https://github.com/libgd/libgd/releases/download/gd-${TERMUX_PKG_VERSION}/libgd-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="freetype, fontconfig, libjpeg-turbo, libpng, libtiff"
+# Disable vpx support for now, look at https://github.com/gagern/libgd/commit/d41eb72cd4545c394578332e5c102dee69e02ee8
+# for enabling:
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-vpx"
