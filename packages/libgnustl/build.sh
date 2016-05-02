e3b2052e7faf6c61eac980f4211f57699b1dd670
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-02 00:49:09 -0400

    libgnustl: Revert back to hard-float on arm
    Fixes #245.

--- packages/libgnustl/build.sh
+++ packages/libgnustl/build.sh
@@ -1,16 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://gcc.gnu.org/onlinedocs/libstdc++/
 TERMUX_PKG_DESCRIPTION="The GNU Standard C++ Library (a.k.a. libstdc++-v3), necessary on android since the system libstdc++.so is stripped down"
 TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=3
 
 termux_step_make_install () {
-        LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_ARCH}-linux-android/lib/libgnustl_shared.so
-        if [ $TERMUX_ARCH = arm ]; then
-                LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/arm-linux-androideabi/lib/armv7-a/hard/libgnustl_shared.so
-        fi
+        LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/libgnustl_shared.so
+	if [ $TERMUX_ARCH = arm ]; then
+		LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/armv7-a/hard/libgnustl_shared.so
+	fi
 
 	cp $LIBFILE $TERMUX_PREFIX/lib/
-	cd $TERMUX_PREFIX/lib/
-
-	ln -s -f libgnustl_shared.so libstdc++.so
 }
