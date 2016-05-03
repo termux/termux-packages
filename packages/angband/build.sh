fd2442216a6bf92d827bc86f0ca8af4cc7f7ec8a
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-03 02:44:44 +0200

    angband: Update from 4.0.4 to 4.0.5

--- packages/angband/build.sh
+++ packages/angband/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_VERSION=4.0.4
+TERMUX_PKG_VERSION=4.0.5
 TERMUX_PKG_SRCURL=http://rephial.org/downloads/4.0/angband-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_HOMEPAGE=http://rephial.org/
 TERMUX_PKG_DESCRIPTION="Dungeon exploration game where you play an adventurer seeking riches, fighting monsters and preparing for a final battle with Morgoth, the Lord of Darkness"
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11 --bindir=$TERMUX_PREFIX/bin --sysconfdir=$TERMUX_PREFIX/share/angband"
-TERMUX_PKG_RM_AFTER_INSTALL="share/angband/xtra"
+TERMUX_PKG_RM_AFTER_INSTALL="share/angband/xtra share/angband/icons"
 
 termux_step_pre_configure () {
 	./autogen.sh
