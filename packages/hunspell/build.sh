32b2d5da4045b6e85a403029620b3fd69369c3dd
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-03 06:56:48 -0400

    hunspell: Update from 1.3.3 to 1.4.1

--- packages/hunspell/build.sh
+++ packages/hunspell/build.sh
@@ -1,7 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://hunspell.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Spell checker"
-TERMUX_PKG_VERSION=1.3.3
-TERMUX_PKG_BUILD_REVISION=1
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/hunspell/Hunspell/${TERMUX_PKG_VERSION}/hunspell-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_SRCURL=https://github.com/hunspell/hunspell/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ui --with-readline"
 TERMUX_PKG_DEPENDS="ncurses, readline, hunspell-en-us"
+TERMUX_PKG_FOLDERNAME=hunspell-${TERMUX_PKG_VERSION}
+TERMUX_PKG_BUILD_IN_SRC=yes
