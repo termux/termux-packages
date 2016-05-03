502442186a2501a1641a111d3b310925d8ece30e
David Martinez <contacto@davidmartinez.net>
2016-05-03 23:25:14 +0200

    Aspell: Added ncurses dependency

--- packages/aspell/build.sh
+++ packages/aspell/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://aspell.net/
 TERMUX_PKG_DESCRIPTION="GNU Aspell is a spell-checker which can be used either as a standalone application or embedded in other programs"
 TERMUX_PKG_VERSION=0.60.6.1
 TERMUX_PKG_SRCURL=ftp://ftp.gnu.org/gnu/aspell/aspell-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="iconv"
+TERMUX_PKG_DEPENDS="iconv, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-compile-in-filters"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
