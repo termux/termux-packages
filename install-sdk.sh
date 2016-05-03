ad3050005ccec7b2c0002c08459d390eb3765a27
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-03 11:50:38 +0200

    ecj: Update from 4.4.2 to 4.5.2

--- install-sdk.sh
+++ install-sdk.sh
@@ -5,4 +5,4 @@ test -f $HOME/.termuxrc && . $HOME/.termuxrc
 : ${ANDROID_HOME:="${HOME}/lib/android-sdk"}
 
 $ANDROID_HOME/tools/android update sdk --no-ui --all --no-https -t "build-tools-23.0.2"
-$ANDROID_HOME/tools/android update sdk --no-ui --all --no-https -t "android-21"
+$ANDROID_HOME/tools/android update sdk --no-ui --all --no-https -t "android-23"
