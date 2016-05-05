6501186f2b5d3f90f847e32ae7a8c2303435afa4
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-05 19:55:13 -0400

    Fix dalvikvm wrapper
    - Just unset LD_LIBRARY_PATH in the dalvikvm wrapper.
    - Let the dalvikvm wrapper setup $ANDROID_DATA.
    - Avoid using a custom dalvikvm wrapper in dx.

--- packages/dx/build.sh
+++ packages/dx/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://developer.android.com/tools/help/index.html
 TERMUX_PKG_DESCRIPTION="Command which takes in class files and reformulates them for usage on Android"
 TERMUX_PKG_VERSION=$TERMUX_ANDROID_BUILD_TOOLS_VERSION
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install () {
@@ -11,6 +12,5 @@ termux_step_make_install () {
                 $ANDROID_HOME/build-tools/${TERMUX_PKG_VERSION}/lib/dx.jar
 
 	install $TERMUX_PKG_BUILDER_DIR/dx $TERMUX_PREFIX/bin/dx
-	install $TERMUX_PKG_BUILDER_DIR/dalvikvm-wrapper $TERMUX_PREFIX/bin/dalvikvm-wrapper
-	perl -p -i -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/{dx,dalvikvm-wrapper}
+	perl -p -i -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/dx
 }
--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://termux.com/
 TERMUX_PKG_DESCRIPTION="Basic system tools for Termux"
-TERMUX_PKG_VERSION=0.27
+TERMUX_PKG_VERSION=0.28
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
 	# Remove LD_LIBRARY_PATH from environment to avoid conflicting
 	# with system libraries that am may link against.
-	for tool in am dalvikvm df getprop logcat ping ping6 ip pm settings; do
+	for tool in am df getprop logcat ping ping6 ip pm settings; do
 		WRAPPER_FILE=$TERMUX_PREFIX/bin/$tool
 		echo '#!/bin/sh' > $WRAPPER_FILE
 
@@ -22,5 +22,6 @@ termux_step_make_install () {
 		chmod +x $TERMUX_PREFIX/bin/$tool
 	done
 
-	cp -p $TERMUX_PKG_BUILDER_DIR/{su,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
+	cp -p $TERMUX_PKG_BUILDER_DIR/{dalvikvm,su,termux-fix-shebang,termux-reload-settings,termux-setup-storage,chsh,termux-open-url} $TERMUX_PREFIX/bin/
+	perl -p -i -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/dalvikvm
 }
