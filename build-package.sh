46de1c2b41c4046608c3eaec3ad01787e156881c
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-08 16:08:51 -0400

    ndk_patches: Remove sha1.h

--- build-package.sh
+++ build-package.sh
@@ -164,10 +164,8 @@ if [ ! -d $TERMUX_STANDALONE_TOOLCHAIN ]; then
 			patch -p1;
 		echo "PATCHING FILE $f done!"
 	done
-	# sha1.h was removed from android ndk for platforms above 19, but needed by the aapt package
-	# JNIHelp.h is also used by aapt
-	# sysexits.h is header-only and used by some unix code
-        cp $TERMUX_SCRIPTDIR/ndk_patches/{sha1.h,sysexits.h} $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include
+	# sysexits.h is header-only and used by a few programs:
+        cp $TERMUX_SCRIPTDIR/ndk_patches/sysexits.h $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include
 fi
 
 export TERMUX_COMMON_CACHEDIR="$TERMUX_TOPDIR/_cache"
