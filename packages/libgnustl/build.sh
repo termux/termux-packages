9fa59b1c259c3602abd78ef12c18596e75557ce3
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-18 18:49:08 -0400

    libgnustl: float=hard -> float=softfp for arm

--- packages/libgnustl/build.sh
+++ packages/libgnustl/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://gcc.gnu.org/onlinedocs/libstdc++/
 TERMUX_PKG_DESCRIPTION="The GNU Standard C++ Library (a.k.a. libstdc++-v3), necessary on android since the system libstdc++.so is stripped down"
 TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_BUILD_REVISION=3
+TERMUX_PKG_BUILD_REVISION=4
 
 termux_step_make_install () {
         local LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/libgnustl_shared.so
 	if [ $TERMUX_ARCH = arm ]; then
-		LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/armv7-a/hard/libgnustl_shared.so
+		LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/armv7-a/libgnustl_shared.so
 	fi
 
 	cp $LIBFILE $TERMUX_PREFIX/lib/
@@ -18,5 +18,5 @@ termux_step_post_massage () {
 	# where e.g. libm.so on some i686 devices links against libstdc++.so, so do
 	# this here in termux_step_post_massage.
 	cd $TERMUX_PREFIX/lib
-	ln -s libgnustl_shared.so libstdc++.so
+	ln -f -s libgnustl_shared.so libstdc++.so
 }
