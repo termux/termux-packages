344b88f958c0b579c5c858f909c757bd927bf63e
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-18 16:21:39 -0400

    libgnustl: Symlink libstdc++ -> libgnustl_shared
    This is used during building to get a fully-featured C++ standard
    library by default.

--- packages/libgnustl/build.sh
+++ packages/libgnustl/build.sh
@@ -4,10 +4,19 @@ TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
 TERMUX_PKG_BUILD_REVISION=3
 
 termux_step_make_install () {
-        LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/libgnustl_shared.so
+        local LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/libgnustl_shared.so
 	if [ $TERMUX_ARCH = arm ]; then
 		LIBFILE=$TERMUX_STANDALONE_TOOLCHAIN/${TERMUX_HOST_PLATFORM}/lib/armv7-a/hard/libgnustl_shared.so
 	fi
 
 	cp $LIBFILE $TERMUX_PREFIX/lib/
+}
+
+termux_step_post_massage () {
+	# Setup a libgnustl_shared.so in $PREFIX/lib, so that other C++ using packages
+	# links to it. We do however want to avoid installing this, to avoid problems
+	# where e.g. libm.so on some i686 devices links against libstdc++.so, so do
+	# this here in termux_step_post_massage.
+	cd $TERMUX_PREFIX/lib
+	ln -s libgnustl_shared.so libstdc++.so
 }
