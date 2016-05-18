029801b5c2bc40244e54dcde69149fc68a56b4f6
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-18 19:38:13 -0400

    ltrace: Move out of disabled

--- packages/ltrace/build.sh
+++ packages/ltrace/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.ltrace.org/
 TERMUX_PKG_DESCRIPTION="Tracks runtime library calls in dynamically linked programs"
 TERMUX_PKG_VERSION=0.7.3.20160411
-TERMUX_PKG_DEPENDS="elfutils"
+TERMUX_PKG_DEPENDS="elfutils, libgnustl"
 
 # TERMUX_PKG_SRCURL=http://www.ltrace.org/ltrace_${TERMUX_PKG_VERSION}.orig.tar.bz2
 # TERMUX_PKG_FOLDERNAME=ltrace-${TERMUX_PKG_VERSION}
