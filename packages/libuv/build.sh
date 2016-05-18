aa0da35ee7bb9a67413f83b1eea0de77db2d12dd
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-18 18:42:16 -0400

    libuv: Update from 1.9.0 to 1.9.1

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
-TERMUX_PKG_VERSION=1.9.0
+TERMUX_PKG_VERSION=1.9.1
 TERMUX_PKG_SRCURL=http://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-dtrace" # needed for building on mac
 
@@ -9,9 +9,4 @@ termux_step_pre_configure () {
 	export PLATFORM=android
 	cd $TERMUX_PKG_SRCDIR
 	sh autogen.sh
-}
-
-termux_step_post_make_install () {
-	# The installed include/uv-unix includes pthread-fixes.h inside ifdef __ANDROID__
-	cp $TERMUX_PKG_SRCDIR/include/pthread-fixes.h $TERMUX_PREFIX/include
 }
