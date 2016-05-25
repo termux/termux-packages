39d68a7f247dcdd54781a2e11d90df01ab20fb22
Fredrik Fornwall <fredrik@fornwall.net>
2016-05-25 16:26:57 -0400

    libxml2&libxslt: Update to latest releases

--- packages/libxslt/build.sh
+++ packages/libxslt/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://xmlsoft.org/libxslt/
 TERMUX_PKG_DESCRIPTION="XSLT processing library"
-TERMUX_PKG_VERSION=1.1.28
-TERMUX_PKG_BUILD_REVISION=3
+TERMUX_PKG_VERSION=1.1.29
 TERMUX_PKG_SRCURL=ftp://xmlsoft.org/libxslt/libxslt-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-python"
 TERMUX_PKG_DEPENDS="libxml2,libgcrypt"
