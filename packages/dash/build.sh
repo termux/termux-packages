0775a3e0cc01773a7693bb23017a98bb525b00e8
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-02 10:49:48 -0400

    dash: Switch download url

--- packages/dash/build.sh
+++ packages/dash/build.sh
@@ -3,7 +3,9 @@ TERMUX_PKG_DESCRIPTION="Small POSIX-compliant implementation of /bin/sh"
 TERMUX_PKG_ESSENTIAL=yes
 TERMUX_PKG_VERSION=0.5.8
 TERMUX_PKG_BUILD_REVISION=3
-TERMUX_PKG_SRCURL=http://gondor.apana.org.au/~herbert/dash/files/dash-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=http://ftp.osuosl.org/pub/blfs/svn/d/dash-${TERMUX_PKG_VERSION}.tar.gz
+# Official site, down as of 2016-06-02:
+# TERMUX_PKG_SRCURL=http://gondor.apana.org.au/~herbert/dash/files/dash-${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_post_make_install () {
 	# Symlink sh -> dash
