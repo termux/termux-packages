3dd02387f8c47eadd11ab4266e3ebbe2e6fe578d
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-03 10:11:58 -0400

    picolisp: Build with latest version

--- packages/picolisp/build.sh
+++ packages/picolisp/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://picolisp.com
 TERMUX_PKG_DESCRIPTION="Lisp interpreter and application server framework"
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 _PICOLISP_YEAR=16
-_PICOLISP_MONTH=5
-_PICOLISP_DAY=18
+_PICOLISP_MONTH=6
+_PICOLISP_DAY=3
 TERMUX_PKG_VERSION=${_PICOLISP_YEAR}.${_PICOLISP_MONTH}.${_PICOLISP_DAY}
 TERMUX_PKG_SRCURL=http://software-lab.de/picoLisp.tgz
+TERMUX_PKG_NO_SRC_CACHE=yes
 TERMUX_PKG_FOLDERNAME=picoLisp
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -13,17 +14,23 @@ termux_step_pre_configure() {
 	# Validate that we have the right version:
 	grep -q "$_PICOLISP_YEAR $_PICOLISP_MONTH $_PICOLISP_DAY" src64/version.l
 
-	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/src
 	if [ $TERMUX_ARCH_BITS = 64 ]; then
-		TERMUX_PKG_SRCDIR+="64"
+		cd $TERMUX_PKG_SRCDIR
 		if [ $TERMUX_ARCH = "aarch64" ]; then
 			export TERMUX_PKG_EXTRA_MAKE_ARGS=arm64.linux
+			termux_download http://software-lab.de/arm64.linux.tgz $TERMUX_PKG_TMPDIR/arm64.linux.tgz
+			tar xf $TERMUX_PKG_TMPDIR/arm64.linux.tgz
 		elif [ $TERMUX_ARCH = "x86_64" ]; then
 			export TERMUX_PKG_EXTRA_MAKE_ARGS=x86-64.linux
+			termux_download http://software-lab.de/x86-64.linux.tgz $TERMUX_PKG_TMPDIR/x86-64.linux.tgz
+			tar xf $TERMUX_PKG_TMPDIR/x86-64.linux.tgz
 		else
 			echo "Error: Unsupported arch: $TERMUX_ARCH"
 			exit 1
 		fi
+		TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/src64
+	else
+		TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/src
 	fi
 	TERMUX_PKG_BUILDDIR=$TERMUX_PKG_SRCDIR
 	ORIG_CFLAGS="$CFLAGS"
@@ -37,7 +44,7 @@ termux_step_make_install () {
 		$AS -pie -o ../bin/picolisp -rdynamic ${TERMUX_PKG_EXTRA_MAKE_ARGS}.base.s -lc -lm -ldl
 		chmod +x ../bin/picolisp
 		$AS -pie -o ../lib/ext -shared -export-dynamic ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ext.s
-		$AS --pie -o ../lib/ht -shared -export-dynamic ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ht.s
+		$AS -pie -o ../lib/ht -shared -export-dynamic ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ht.s
 	fi
 
 	mkdir -p $TERMUX_PREFIX/share/man/man1
