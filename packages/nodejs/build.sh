33aea6bfd3ec0b3583ab2020af9fbcd808814d11
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-03 10:12:46 -0400

    nodejs: Update from 6.2.0 to 6.2.1

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=6.2.0
+TERMUX_PKG_VERSION=6.2.1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="openssl, libuv, libgnustl, c-ares"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
