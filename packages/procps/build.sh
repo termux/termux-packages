8cace090a1101db4409883377c812b1cff651835
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-06 17:05:14 -0400

    procps: Configure with --disable-modern-top
    This seems to be the most popular defaults.

--- packages/procps/build.sh
+++ packages/procps/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/sid/procps
 TERMUX_PKG_DESCRIPTION="Utilities that give information about processes using the /proc filesystem"
 TERMUX_PKG_VERSION=3.3.11
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/procps-ng/Production/procps-ng-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_FOLDERNAME=procps-ng-${TERMUX_PKG_VERSION}
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_search_dlopen= --enable-sigwinch"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_search_dlopen= --enable-sigwinch --disable-modern-top"
 TERMUX_PKG_DEPENDS="ncurses"
 # https://bugs.launchpad.net/ubuntu/+source/coreutils/+bug/141168:
 # "For compatibility between distributions, can we have /bin/kill made available from coreutils?"
