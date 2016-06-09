f6e85769c9e29fcf18c503603624a2d05b47a151
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-09 10:05:43 -0400

    strace: Update from 4.11 to 4.12

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,10 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://sourceforge.net/projects/strace/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor the system calls used by a program and all the signals it receives"
-TERMUX_PKG_VERSION=4.11
+TERMUX_PKG_VERSION=4.12
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/strace/strace/${TERMUX_PKG_VERSION}/strace-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
-
-CFLAGS+=" -Du64=uint64_t"
 
 if [ $TERMUX_ARCH_BITS = "64" ]; then
 	# The strace configure script only looks for struct flock64 in <linux/fcntl.h>,
