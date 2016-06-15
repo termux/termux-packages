2b144dbd1900a848e981c30c80f32ae7f33c9378
Pierre Rudloff <contact@rudloff.pro>
2016-06-16 00:03:41 +0200

    sslscan package

---
+++ packages/sslscan/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/sslscan/
+TERMUX_PKG_DESCRIPTION="Fast SSL scanner"
+TERMUX_PKG_VERSION=1.8.2
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/sslscan/files/sslscan/sslscan-${TERMUX_PKG_VERSION}.tgz/download
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_FOLDERNAME=sslscan-${TERMUX_PKG_VERSION}
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
