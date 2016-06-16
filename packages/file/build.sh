baeb9a2e19d8a448c41753189600ff744c040331
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-16 17:52:02 -0400

    file: Update from 5.27 to 5.28

--- packages/file/build.sh
+++ packages/file/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://darwinsys.com/file/
 TERMUX_PKG_DESCRIPTION="Command-line tool that tells you in words what kind of data a file contains"
-TERMUX_PKG_VERSION=5.27
+TERMUX_PKG_VERSION=5.28
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/file-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_mmap_fixed_mapped=yes"
 TERMUX_PKG_EXTRA_MAKE_ARGS="FILE_COMPILE=$TERMUX_PKG_HOSTBUILD_DIR/src/file"
