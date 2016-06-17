c04a39e8c3e910a583e35c5e43d1b70e8e6afa9f
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-17 06:37:32 -0400

    libx264: Also disable asm for x86_64

--- packages/libx264/build.sh
+++ packages/libx264/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.videolan.org/developers/x264.html
 TERMUX_PKG_DESCRIPTION="Library for encoding video streams into the H.264/MPEG-4 AVC format"
 TERMUX_PKG_VERSION="20160503-2245"
 TERMUX_PKG_SRCURL=ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-${TERMUX_PKG_VERSION}.tar.bz2
-if [ $TERMUX_ARCH = "i686" ]; then
+if [ $TERMUX_ARCH = "i686" -o $TERMUX_ARCH = "x86_64" ]; then
         # Avoid text relocations. Only needed on i686, see:
         # https://mailman.videolan.org/pipermail/x264-devel/2016-March/011589.html
+	# Also disable on x86_64 for now due to build errors.
         TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-asm"
 fi
