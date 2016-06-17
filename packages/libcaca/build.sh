d4fce2d3f0f1acf426f41a5585a44cd264e8b5fc
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-17 06:36:18 -0400

    libcaca: Fix x86_64 build

--- packages/libcaca/build.sh
+++ packages/libcaca/build.sh
@@ -6,3 +6,10 @@ TERMUX_PKG_SRCURL=http://fossies.org/linux/privat/libcaca-${TERMUX_PKG_VERSION}.
 #                     http://caca.zoy.org/files/libcaca/libcaca-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-python --disable-java --disable-doc"
+
+termux_step_post_configure () {
+	if [ $TERMUX_ARCH = x86_64 ]; then
+		# Remove troublesome asm usage:
+		perl -p -i -e 's/#define HAVE_FLDLN2 1//' $TERMUX_PKG_BUILDDIR/config.h
+	fi
+}
