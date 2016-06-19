39c1918069f7d23e2d7ef4a303b39d39e6cd58a6
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-20 00:21:39 +0200

    Update setup-mac.sh

--- scripts/setup-mac.sh
+++ scripts/setup-mac.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e -u
 
+# Remaining mac build issues:
+# - perl does not build
+# - strace does not build
+# - dpkg calls ldconfig
+
 command -v brew >/dev/null 2>&1 || {
         echo >&2 "Install homebrew first: http://brew.sh"
         exit 1
@@ -20,6 +25,8 @@ brew install $PACKAGES
 
 # bison is keg-only, but we need updated 'bison' in path:
 brew link bison --force
+# gettext is also key-only, but we need msgfmt for apt and glib:
+brew link gettext --force
 
 sudo mkdir -p /data/data/com.termux/files/usr
 sudo chown -R `whoami` /data
