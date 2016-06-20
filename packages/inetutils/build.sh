4608382d11eae666285233e87f81d08ca9cb5cc3
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-19 20:20:27 -0400

    inetutils: Avoid linking against libcrypt

--- packages/inetutils/build.sh
+++ packages/inetutils/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/inetutils/inetutils-${TERMUX_PKG_VERSIO
 TERMUX_PKG_DEPENDS="readline, libutil"
 # These are old cruft / not suited for android:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ifconfig --disable-rcp --disable-rlogin --disable-rsh --disable-rexecd --disable-uucpd --disable-rexec --disable-ping --disable-ping6 --disable-hostname"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_lib_crypt_crypt=no"
 
 CPPFLAGS+=" -DLOGIN_PROCESS=6 -DDEAD_PROCESS=8 -DLOG_NFACILITIES=24"
 LDFLAGS+=" -llog" # for syslog
