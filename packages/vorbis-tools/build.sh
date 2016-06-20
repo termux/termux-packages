dfcb3173f5ae4be2333c007cda5ea7eee61c66d1
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-20 06:31:29 -0400

    vorbis-tools: Enable flac support

--- packages/vorbis-tools/build.sh
+++ packages/vorbis-tools/build.sh
@@ -1,7 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.vorbis.com
 TERMUX_PKG_DESCRIPTION="Ogg Vorbis tools"
 TERMUX_PKG_VERSION=1.4.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/vorbis-tools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libvorbis"
+# libflac for flac support in oggenc:
+TERMUX_PKG_DEPENDS="libvorbis, libflac"
 
 LDFLAGS+=" -lm"
