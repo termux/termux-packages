1d95577f6417921d0017372e11eb3997507732c4
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-21 19:27:43 -0400

    ndk-sysroot: Fix x86-64 build

--- packages/ndk-sysroot/build.sh
+++ packages/ndk-sysroot/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://developer.android.com/tools/sdk/ndk/index.html
 TERMUX_PKG_DESCRIPTION="System header and library files from the Android NDK needed for compiling C programs"
 TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_NO_DEVELSPLIT=yes
 # Depend on libandroid-support-dev so that iconv.h and libintl.h are available:
 TERMUX_PKG_DEPENDS="libandroid-support-dev"
@@ -9,7 +9,11 @@ TERMUX_PKG_DEPENDS="libandroid-support-dev"
 termux_step_extract_into_massagedir () {
 	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/pkgconfig $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include
 	cp -Rf $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include/* $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include
-	cp $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/*.o $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib
+	local _LIBDIR=lib
+	if [ "$TERMUX_ARCH" = "x86_64" ]; then
+		_LIBDIR=lib64
+	fi
+	cp $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/${_LIBDIR}/*.o $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib
 
 	cat > $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/pkgconfig/zlib.pc <<HERE
 Name: zlib
