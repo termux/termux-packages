33568e20fe54d6d0e3bc3e484c1319dd72276cc3
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-21 18:44:41 -0400

    lighttpd: Depend on libcrypt for mod_auth

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://www.lighttpd.net
 TERMUX_PKG_DESCRIPTION="Web server optimized for speed-critical environments while remaining standards-compliant, secure and flexible"
 TERMUX_PKG_VERSION=1.4.39
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
-TERMUX_PKG_DEPENDS="libbz2, openssl, pcre"
+TERMUX_PKG_DEPENDS="libbz2, openssl, pcre, libcrypt"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/lighttpd-angel"
 # liblog for syslog:
 LDFLAGS="$LDFLAGS -llog"
