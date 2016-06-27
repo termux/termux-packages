b5e2fe57e0a2fe0963755a520efd01da37cb01db
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-27 17:39:57 -0400

    bash: Update from 4.3.42 to 4.3.46

--- packages/bash/build.sh
+++ packages/bash/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/bash/
 TERMUX_PKG_DESCRIPTION="A sh-compatible shell that incorporates useful features from the Korn shell (ksh) and C shell (csh)"
 TERMUX_PKG_DEPENDS="ncurses, readline, libandroid-support, termux-tools, command-not-found"
 _MAIN_VERSION=4.3
-_PATCH_VERSION=42
+_PATCH_VERSION=46
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
-TERMUX_PKG_BUILD_REVISION=8
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/bash/bash-${_MAIN_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-multibyte --without-bash-malloc --with-installed-readline ac_cv_header_grp_h=no ac_cv_header_pwd_h=no ac_cv_rl_version=6.3"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_job_control_missing=present"
