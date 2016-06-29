ab6ed7affd03df6e5ed6a9fcb33998cb4077c1d0
Fredrik Fornwall <fredrik@fornwall.net>
2016-06-29 06:46:27 -0400

    screenfetch: Mark as arch-independent

--- packages/screenfetch/build.sh
+++ packages/screenfetch/build.sh
@@ -1,11 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/KittyKatt/screenFetch
 TERMUX_PKG_DESCRIPTION="Bash Screenshot Information Tool"
+TERMUX_PKG_DEPENDS="bash"
 TERMUX_PKG_VERSION=3.7.0
 TERMUX_PKG_SRCURL=https://github.com/KittyKatt/screenFetch/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_FOLDERNAME=screenFetch-${TERMUX_PKG_VERSION}
-TERMUX_PKG_DEPENDS=""
 TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
+TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
     install screenfetch-dev ${TERMUX_PREFIX}/bin/screenfetch
