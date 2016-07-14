98ee2c5ea19de147547406fc3ac0128171f8242e
Fredrik Fornwall <fredrik@fornwall.net>
2016-07-14 17:58:14 -0400

    direvent: Update from 5.0 to 5.1

--- packages/direvent/build.sh
+++ packages/direvent/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org.ua/software/direvent/
 TERMUX_PKG_DESCRIPTION="Monitor of events in file system directories"
-TERMUX_PKG_VERSION=5.0
+TERMUX_PKG_VERSION=5.1
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/direvent/direvent-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libandroid-glob"
 
-LDFLAGS+=" -llog"
+LDFLAGS+=" -llog -landroid-glob"
