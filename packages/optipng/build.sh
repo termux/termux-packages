58387835a945236e2bac0f3457fb5209ab6ac40c
Avery Magnotti <averymagnotti@outlook.com>
2016-07-15 19:37:56 -0400

    fix optipng URL

--- packages/optipng/build.sh
+++ packages/optipng/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://optipng.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="PNG optimizer that recompresses image files to a smaller size, without losing any information"
 TERMUX_PKG_DEPENDS="libpng"
 TERMUX_PKG_VERSION=0.7.6
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-${TERMUX_PKG_VERSION}/optipng-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/project/optipng/OptiPNG/optipng-${TERMUX_PKG_VERSION}/optipng-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-system-zlib --with-system-libpng --mandir=$TERMUX_PREFIX/share/man"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
