40cd393699ac5659df05793ce93de6e649c17dfd
Avery Magnotti <averymagnotti@outlook.com>
2016-07-15 18:01:06 -0400

    fix espeak URL

--- packages/espeak/build.sh
+++ packages/espeak/build.sh
@@ -1,6 +1,6 @@
 _MAJOR_VERSION=1.48
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.04
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/espeak/espeak/espeak-${_MAJOR_VERSION}/espeak-${TERMUX_PKG_VERSION}-source.zip
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/project/espeak/espeak/espeak-${_MAJOR_VERSION}/espeak-${TERMUX_PKG_VERSION}-source.zip
 TERMUX_PKG_FOLDERNAME=espeak-${TERMUX_PKG_VERSION}-source
 TERMUX_PKG_HOMEPAGE=http://espeak.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Compact software speech synthesizer"
