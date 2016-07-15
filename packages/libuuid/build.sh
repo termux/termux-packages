f0787777b4068b651eb83abc326e07bad811ee12
Avery Magnotti <averymagnotti@outlook.com>
2016-07-15 18:51:23 -0400

    fix libuuid URL

--- packages/libuuid/build.sh
+++ packages/libuuid/build.sh
@@ -1,4 +1,4 @@
 TERMUX_PKG_HOMEPAGE=http://sourceforge.net/projects/libuuid/
 TERMUX_PKG_DESCRIPTION="Portable uuid C library"
 TERMUX_PKG_VERSION=1.0.3
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/libuuid/libuuid-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/project/libuuid/libuuid-${TERMUX_PKG_VERSION}.tar.gz
