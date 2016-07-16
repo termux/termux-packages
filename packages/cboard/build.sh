b56b0de1941cdd60b0df2c3d39b54dd2185149a9
Avery Magnotti <averymagnotti@outlook.com>
2016-07-15 21:52:29 -0400

    fix cboard URL

--- packages/cboard/build.sh
+++ packages/cboard/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://benkibbey.wordpress.com/cboard/
 TERMUX_PKG_DESCRIPTION="PGN browser, editor and chess engine frontend"
 TERMUX_PKG_VERSION=0.7.3
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/c-board/${TERMUX_PKG_VERSION}/cboard-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/project/c-board/${TERMUX_PKG_VERSION}/cboard-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libandroid-support,libandroid-glob,gnuchess, ncurses, ncurses-ui-libs"
 
 CFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
