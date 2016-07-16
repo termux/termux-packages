931ea1e1702f0cdf7b406605d3e8562b09ee07c1
Avery Magnotti <averymagnotti@outlook.com>
2016-07-15 21:00:47 -0400

    fix wcalc URL

--- packages/wcalc/build.sh
+++ packages/wcalc/build.sh
@@ -2,5 +2,5 @@ TERMUX_PKG_HOMEPAGE=http://w-calc.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Capable calculator"
 TERMUX_PKG_VERSION=2.5
 TERMUX_PKG_BUILD_REVISION=2
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/w-calc/wcalc-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/w-calc/wcalc-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="flex,libmpfr,libgmp,readline"
