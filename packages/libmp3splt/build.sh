5441af4218bc8586f6e49ed4b023df3cd2ffefd5
Avery Magnotti <averymagnotti@outlook.com>
2016-07-16 08:23:53 -0400

    fix libmp3splt URL

--- packages/libmp3splt/build.sh
+++ packages/libmp3splt/build.sh
@@ -2,6 +2,6 @@ TERMUX_PKG_HOMEPAGE=http://mp3splt.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Mp3Splt-project is a utility to split mp3, ogg vorbis and native FLAC files selecting a begin and an end time position, without decoding"
 TERMUX_PKG_VERSION=0.9.2
 TERMUX_PKG_BUILD_REVISION=1
-TERMUX_PKG_SRCURL=http://prdownloads.sourceforge.net/mp3splt/libmp3splt-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/project/mp3splt/libmp3splt/${TERMUX_PKG_VERSION}/libmp3splt-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libmad, libid3tag, libvorbis, libflac, pcre, libtool"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-cutter"
