5938e62bd79070815799568ab294e34ed438d2c5
Avery Magnotti <averymagnotti@outlook.com>
2016-07-16 08:34:13 -0400

    fix mp3splt URL

--- packages/mp3splt/build.sh
+++ packages/mp3splt/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://mp3splt.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Mp3Splt-project is a utility to split mp3, ogg vorbis and native FLAC files selecting a begin and an end time position, without decoding"
 TERMUX_PKG_VERSION=2.6.2
 TERMUX_PKG_BUILD_REVISION=1
-TERMUX_PKG_SRCURL=http://prdownloads.sourceforge.net/mp3splt/mp3splt-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=http://heanet.dl.sourceforge.net/project/mp3splt/mp3splt/${TERMUX_PKG_VERSION}/mp3splt-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libmp3splt"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
 
