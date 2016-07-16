163a880862b25cf6e0ee2125f3f17a584592a490
Fredrik Fornwall <fredrik@fornwall.net>
2016-07-16 19:26:07 -0400

    man: Update from 1.13.3 to 1.13.4

--- packages/man/build.sh
+++ packages/man/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://mdocml.bsd.lv/
 TERMUX_PKG_DESCRIPTION="Man page viewer from the mandoc toolset"
-TERMUX_PKG_VERSION=1.13.3
+TERMUX_PKG_VERSION=1.13.4
 TERMUX_PKG_SRCURL=http://mdocml.bsd.lv/snapshots/mdocml-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="less,libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -11,7 +11,8 @@ termux_step_post_extract_package () {
 	LDFLAGS+=" -landroid-glob"
 	echo "PREFIX=\"$TERMUX_PREFIX\"" > configure.local
 	echo "MANDIR=\"$TERMUX_PREFIX/share/man\"" >> configure.local
-	echo "CFLAGS=\"$CFLAGS -DNULL=0 $CPPFLAGS $LDFLAGS\"" >> configure.local
+	echo "CFLAGS=\"$CFLAGS -DNULL=0 $CPPFLAGS\"" >> configure.local
+	echo "LDFLAGS=\"$LDFLAGS\"" >> configure.local
 	for HAVING in HAVE_FGETLN HAVE_MMAP HAVE_STRLCAT HAVE_STRLCPY; do
 		echo "$HAVING=1" >> configure.local
 	done
@@ -19,7 +20,7 @@ termux_step_post_extract_package () {
 	echo "HAVE_SQLITE3=0" >> configure.local
 }
 
-termux_step_make_install () {
-	make -j 1 install
-	echo "_whatdb         $TERMUX_PREFIX/share/man/mandoc.db" > $TERMUX_PREFIX/etc/man.conf
-}
+#termux_step_make_install () {
+#make -j 1 install
+#echo "_whatdb         $TERMUX_PREFIX/share/man/mandoc.db" > $TERMUX_PREFIX/etc/man.conf
+#}
