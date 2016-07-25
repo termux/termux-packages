fbc5ceee0de0e93dae2224fe64918fdba123a2d8
Daniel Thau <danthau@gmail.com>
2016-07-25 17:01:18 -0400

    Add abook package

---
+++ packages/abook/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://abook.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="Abook is a text-based addressbook program designed to use with mutt mail client"
+TERMUX_PKG_VERSION=0.6.0pre2
+TERMUX_PKG_SRCURL=http://abook.sourceforge.net/devel/abook-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses, readline"
