2cbdeb5e714701630773b7d1b136a85fbc598e6b
Fredrik Fornwall <fredrik@fornwall.net>
2016-07-26 08:55:25 -0400

    jack: Clean up build

--- packages/jack/build.sh
+++ packages/jack/build.sh
@@ -7,15 +7,13 @@ TERMUX_PKG_VERSION=$TERMUX_ANDROID_BUILD_TOOLS_VERSION
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make () {
-	JACK_JAR=$ANDROID_HOME/build-tools/$TERMUX_ANDROID_BUILD_TOOLS_VERSION/jack.jar
-	JILL_JAR=$ANDROID_HOME/build-tools/$TERMUX_ANDROID_BUILD_TOOLS_VERSION/jill.jar
-
 	# Create $USR/share/dex for dex files, and $USR/share/jack for .jack library files produced by jill:
         mkdir -p $TERMUX_PREFIX/share/{dex,jack}
 	$TERMUX_DX \
+		-JXmx4096M --num-threads=4 \
 		--dex \
 		--output=$TERMUX_PREFIX/share/dex/jack.jar \
-		$JACK_JAR
+		$TERMUX_JACK
 
 	cd $TERMUX_PKG_TMPDIR
 	rm -rf android-jar
@@ -25,11 +23,11 @@ termux_step_make () {
         # We need the android.jar clases in jill format (.jack extension) for jack to compile against.
 	cp $ANDROID_HOME/platforms/android-23/android.jar .
 	# Remove resources not needed for compilation to reduce size:
-	unzip android.jar
+	unzip -q android.jar
 	rm -Rf android.jar resources.arsc res assets
-	zip -r android.jar .
+	zip -r -q android.jar .
 
-	java -jar $JILL_JAR $TERMUX_PKG_TMPDIR/android-jar/android.jar --output $TERMUX_PREFIX/share/jack/android.jack
+	java -jar $TERMUX_JILL $TERMUX_PKG_TMPDIR/android-jar/android.jar --output $TERMUX_PREFIX/share/jack/android.jack
 	mkdir -p $TERMUX_PREFIX/bin
 	install $TERMUX_PKG_BUILDER_DIR/jack.sh $TERMUX_PREFIX/bin/jack
 	perl -p -i -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/jack
