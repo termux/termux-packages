5d0fc15b4e47617acb06f414b131bee5006deb4f
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-02 08:00:52 -0400

    libgnutls: Switch back to main ftp for source

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -3,8 +3,7 @@ TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS 
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn"
 _TERMUX_PKG_MAJOR_VERSION=3.4
 TERMUX_PKG_VERSION=${_TERMUX_PKG_MAJOR_VERSION}.14
-# TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${_TERMUX_PKG_MAJOR_VERSION}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SRCURL=http://mirrors.dotsrc.org/gcrypt/gnutls/v${_TERMUX_PKG_MAJOR_VERSION}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${_TERMUX_PKG_MAJOR_VERSION}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-hardware-acceleration --disable-cxx --disable-openssl-compatibility --with-included-libtasn1 --without-p11-kit --with-default-trust-store-file=$TERMUX_PREFIX/etc/tls/cert.pem"
 
 CFLAGS+=" -std=c99"
