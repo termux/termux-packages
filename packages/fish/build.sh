8fa10e01933f7e4e86ca366b4660e611d6688b86
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-06 17:37:29 -0400

    fish: Depend on man for apropos
    The apropos command is used by tab completion.

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -1,9 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://fishshell.com/
 TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
 TERMUX_PKG_VERSION=2.3.1
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/releases/download/$TERMUX_PKG_VERSION/fish-${TERMUX_PKG_VERSION}.tar.gz
-# fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line:
-TERMUX_PKG_DEPENDS="ncurses, libgnustl, libandroid-support, ncurses-utils"
+# fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line.
+# man is needed since fish calls apropos during command completion.
+TERMUX_PKG_DEPENDS="ncurses, libgnustl, libandroid-support, ncurses-utils, man"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_FOLDERNAME=fish-$TERMUX_PKG_VERSION
 
