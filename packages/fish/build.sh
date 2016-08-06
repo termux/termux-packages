6250534f1ee25ae7a93cd1e06c4b3b486a962729
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-06 04:14:04 -0400

    fish: Update to 2.3.1 from git snapshot

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -1,20 +1,19 @@
 TERMUX_PKG_HOMEPAGE=http://fishshell.com/
 TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
-_COMMIT=c76d86631717929b3a2f259615e8603e69e13256
-TERMUX_PKG_VERSION=2.2.201605030720
-TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/archive/${_COMMIT}.zip
+TERMUX_PKG_VERSION=2.3.1
+TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/releases/download/$TERMUX_PKG_VERSION/fish-${TERMUX_PKG_VERSION}.tar.gz
 # fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line:
 TERMUX_PKG_DEPENDS="ncurses, libgnustl, libandroid-support, ncurses-utils"
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_FOLDERNAME=fish-shell-$_COMMIT
+TERMUX_PKG_FOLDERNAME=fish-$TERMUX_PKG_VERSION
 
-termux_step_pre_configure () {
-	cd $TERMUX_PKG_SRCDIR
-	autoconf
+CXXFLAGS+=" $CPPFLAGS"
 
-	CXXFLAGS+=" $CPPFLAGS"
+termux_step_post_make_install () {
+	cat >> $TERMUX_PREFIX/etc/fish/config.fish <<HERE
 
-	LDFLAGS+=" -lgnustl_shared"
-
-	export PCRE2_CONFIG_EXTRAS="--host=$TERMUX_HOST_PLATFORM"
+function __fish_command_not_found_handler --on-event fish_command_not_found
+	$TERMUX_PREFIX/libexec/termux/command-not-found \$argv[1]
+end
+HERE
 }
