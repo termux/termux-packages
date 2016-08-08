636366f8dc0f7fc7fd0aaf16b22f3bcdce5ac2e5
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-08 16:48:04 -0400

    ired: Remove explict termux_step_make step

--- packages/ired/build.sh
+++ packages/ired/build.sh
@@ -1,11 +1,6 @@
-TERMUX_PKG_VERSION=0.5
 TERMUX_PKG_HOMEPAGE=https://github.com/radare/ired
+TERMUX_PKG_DESCRIPTION="Minimalist hexadecimal editor"
+TERMUX_PKG_VERSION=0.5
 TERMUX_PKG_SRCURL=https://github.com/radare/ired/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=ired-${TERMUX_PKG_VERSION}
-TERMUX_PKG_DESCRIPTION="Minimalist hexadecimal editor"
-
-termux_step_make () {
-	cd ../src
-	make
-	make install PREFIX=${TERMUX_PREFIX}
-}
+TERMUX_PKG_BUILD_IN_SRC=yes
