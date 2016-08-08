5b7b986b833363fa8cc56f58017e0a5d7e0a51cf
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-08 16:14:16 -0400

    mtr: Update build.sh (work in progress)

--- disabled-packages/mtr/build.sh
+++ disabled-packages/mtr/build.sh
@@ -1,16 +1,13 @@
-# Status: Needs hcreate()/hsearch()/hdestroy() in search.h,
-#         which is not included in the NDK.
-
+# Status: Needs a working resolv.h.
+#         Perhaps add libres?
+#         http://dan.drown.org/android/src/libres/
 TERMUX_PKG_HOMEPAGE=https://github.com/traviscross/mtr
 TERMUX_PKG_DESCRIPTION="Network diagnostic tool"
-_DATE=20160609
-# Note that the newdns branch is used.
-_COMMIT=66de3ecbab28b054b868a73fbb57f30549d770ac
-TERMUX_PKG_VERSION=0.86.${_DATE}
-TERMUX_PKG_SRCURL=https://github.com/traviscross/mtr/archive/${_COMMIT}.zip
-TERMUX_PKG_FOLDERNAME=mtr-$_COMMIT
+TERMUX_PKG_VERSION=0.87
+TERMUX_PKG_SRCURL=https://github.com/traviscross/mtr/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=mtr-$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-gtk"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-gtk --disable-ipv6"
 
 termux_step_pre_configure() {
 	cd $TERMUX_PKG_SRCDIR
