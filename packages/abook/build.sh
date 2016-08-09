5e7f900d5321347f8df36f338cc0e7a4b73a3c6e
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-09 16:05:23 -0400

    abook: Fix mandir and 32-bit build

--- packages/abook/build.sh
+++ packages/abook/build.sh
@@ -3,3 +3,4 @@ TERMUX_PKG_DESCRIPTION="Abook is a text-based addressbook program designed to us
 TERMUX_PKG_VERSION=0.6.0pre2
 TERMUX_PKG_SRCURL=http://abook.sourceforge.net/devel/abook-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, readline"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
