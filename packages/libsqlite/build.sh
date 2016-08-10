52fe38a80a7e416961e8a2d33ed8b4327af810f8
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-10 18:00:45 -0400

    sqlite: Compute src url from version

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -1,5 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.sqlite.org/
 TERMUX_PKG_DESCRIPTION="Software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine"
-TERMUX_PKG_VERSION=3.14.0
-TERMUX_PKG_SRCURL=https://www.sqlite.org/2016/sqlite-autoconf-3130000.tar.gz
+_SQLITE_MAJOR=3
+_SQLITE_MINOR=14
+_SQLITE_PATCH=0
+TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
+TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_SRCURL=https://www.sqlite.org/2016/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-readline"
