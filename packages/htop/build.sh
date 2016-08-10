265909ff5ed51fff7797376f348357b1ab0b4ac4
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-10 17:59:53 -0400

    htop: Update from 2.0.1 to 2.0.2

--- packages/htop/build.sh
+++ packages/htop/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://hisham.hm/htop/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_DEPENDS="ncurses, libandroid-support"
-TERMUX_PKG_VERSION=2.0.1
-TERMUX_PKG_BUILD_REVISION=2
+TERMUX_PKG_VERSION=2.0.2
 TERMUX_PKG_SRCURL=http://hisham.hm/htop/releases/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_ncursesw6_addnwstr=yes"
