de15ecbe2b52e3b33eea5566531efe5570cff59a
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-11 22:43:08 -0400

    fsmon: Small cleanup
    - Update homepage to where fsmon is mentioned.
    - Add a bit to the description.
    - Remove termux_step_make_install (PREFIX is already exported).

--- packages/fsmon/build.sh
+++ packages/fsmon/build.sh
@@ -1,14 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://www.nowsecure.com
-TERMUX_PKG_DESCRIPTION="FileSystem Monitor"
+TERMUX_PKG_HOMEPAGE=https://github.com/nowsecure/fsmon
+TERMUX_PKG_DESCRIPTION="Filesystem monitor with fanotify and inotify backends"
 TERMUX_PKG_VERSION=1.4
 TERMUX_PKG_SRCURL=https://github.com/nowsecure/fsmon/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME="fsmon-$TERMUX_PKG_VERSION"
-TERMUX_PKG_BUILD_IN_SRC="yes"
+TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_make() {
-	make CC=${CC} FANOTIFY_CFLAGS="-DHAVE_FANOTIFY=1 -DHAVE_SYS_FANOTIFY=0"
-}
-
-termux_step_make_install() {
-	make install PREFIX="${prefix}"
+	make FANOTIFY_CFLAGS="-DHAVE_FANOTIFY=1 -DHAVE_SYS_FANOTIFY=0"
 }
