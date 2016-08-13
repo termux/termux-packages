5226ce7f62695ae7ad289ded6a66e5b4212821c1
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-12 22:10:51 -0400

    poppler: Update from 0.43.0 to 0.46.0

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,10 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
-TERMUX_PKG_VERSION=0.43.0
+TERMUX_PKG_VERSION=0.46.0
 TERMUX_PKG_SRCURL=http://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
 TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-poppler-glib"
-
-# $NDK/docs/STANDALONE-TOOLCHAIN.html: "If you use the GNU libstdc++, you will need to explicitly link with libsupc++ if you use these features"
-export LDFLAGS="$LDFLAGS -lgnustl_shared" # -lsupc++"
