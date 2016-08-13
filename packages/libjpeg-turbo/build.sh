b67b9cdaccb2b87f4e1669bb527f8834022ad4ea
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-12 20:40:50 -0400

    libjpeg-turbo: Update from 1.4.2 to 1.5.0

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -1,6 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://libjpeg-turbo.virtualgl.org/
 TERMUX_PKG_DESCRIPTION="Library for reading and writing JPEG image files"
-TERMUX_PKG_VERSION=1.4.2
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=1.5.0
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/libjpeg-turbo/${TERMUX_PKG_VERSION}/libjpeg-turbo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
