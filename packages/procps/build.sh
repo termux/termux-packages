b316825b017ed303931181ae6db176d74a41724f
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-14 18:13:13 -0400

    procps: Update from 3.3.11 to 3.3.12

--- packages/procps/build.sh
+++ packages/procps/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/sid/procps
 TERMUX_PKG_DESCRIPTION="Utilities that give information about processes using the /proc filesystem"
-TERMUX_PKG_VERSION=3.3.11
-TERMUX_PKG_BUILD_REVISION=2
+TERMUX_PKG_VERSION=3.3.12
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/procps-ng/Production/procps-ng-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_FOLDERNAME=procps-ng-${TERMUX_PKG_VERSION}
@@ -10,3 +9,5 @@ TERMUX_PKG_DEPENDS="ncurses"
 # https://bugs.launchpad.net/ubuntu/+source/coreutils/+bug/141168:
 # "For compatibility between distributions, can we have /bin/kill made available from coreutils?"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/kill share/man/man1/kill.1 usr/bin/w share/man/man1/w.1 usr/bin/slabtop share/man/man1/slabtop.1"
+
+CFLAGS+=" -DHOST_NAME_MAX=255"
