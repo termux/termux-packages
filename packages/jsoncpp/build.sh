4e51e8d8bae920159f5a76f2b1be34ed0aa0e062
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-14 17:40:55 -0400

    jsoncpp: Set CMAKE_SYSTEM_NAME=Android
    This avoid a versioned soname.

--- packages/jsoncpp/build.sh
+++ packages/jsoncpp/build.sh
@@ -23,7 +23,7 @@ termux_step_configure () {
 		-DCMAKE_LINKER=`which ${TERMUX_HOST_PLATFORM}-ld` \
                 -DCMAKE_MAKE_PROGRAM=`which make` \
 		-DCMAKE_RANLIB=`which ${TERMUX_HOST_PLATFORM}-ranlib` \
-		-DCMAKE_SYSTEM_NAME=Linux \
+		-DCMAKE_SYSTEM_NAME=Android \
                 -DPKG_CONFIG_EXECUTABLE=$PKG_CONFIG \
 		-DZLIB_LIBRARY:FILEPATH="$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/libz.so" \
 		-DZLIB_INCLUDE_DIR:PATH="$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include" \
