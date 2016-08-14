eaae2cf5f0b9f30b8dd04fc61b5a441924dc0da8
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-14 09:28:58 -0400

    elinks: Bump build efter libexpat update

--- packages/elinks/build.sh
+++ packages/elinks/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://elinks.or.cz
 TERMUX_PKG_DESCRIPTION="Full-Featured Text WWW Browser"
 TERMUX_PKG_VERSION=0.12pre6
-TERMUX_PKG_BUILD_REVISION=2
+TERMUX_PKG_BUILD_REVISION=3
 # TERMUX_PKG_SRCURL=http://elinks.or.cz/download/elinks-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=http://archive.ubuntu.com/ubuntu/pool/universe/e/elinks/elinks_0.12~pre6.orig.tar.gz
 TERMUX_PKG_FOLDERNAME=elinks-0.12pre6
