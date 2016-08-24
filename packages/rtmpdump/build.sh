a43f8ae3a618debc62187c94e816a4c23e10a20f
Pierre Rudloff <contact@rudloff.pro>
2016-08-24 15:05:11 +0200

    rtmpdump package (#321)

---
+++ packages/rtmpdump/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://rtmpdump.mplayerhq.hu/
+TERMUX_PKG_DESCRIPTION="Small dumper for media content streamed over the RTMP protocol"
+TERMUX_PKG_VERSION=2.3
+TERMUX_PKG_SRCURL=https://rtmpdump.mplayerhq.hu/download/rtmpdump-${TERMUX_PKG_VERSION}.tgz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_FOLDERNAME=rtmpdump-${TERMUX_PKG_VERSION}
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
