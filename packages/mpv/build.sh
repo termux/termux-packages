8d90dd47175333366be9ae6eed7f2d1350c47740
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-26 18:50:24 -0400

    mpv: Enable openal output and fix quoting issue

--- packages/mpv/build.sh
+++ packages/mpv/build.sh
@@ -7,9 +7,10 @@
 TERMUX_PKG_HOMEPAGE=https://mpv.io/
 TERMUX_PKG_DESCRIPTION="Command-line media player"
 TERMUX_PKG_VERSION=0.19.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=mpv-${TERMUX_PKG_VERSION}
-TERMUX_PKG_DEPENDS="ffmpeg"
+TERMUX_PKG_DEPENDS="ffmpeg, openal-soft"
 
 termux_step_make_install () {
 	cd $TERMUX_PKG_SRCDIR
@@ -25,7 +26,8 @@ termux_step_make_install () {
 		--disable-gl \
 		--disable-jpeg \
 		--disable-lcms2 \
-		--disable-libass
+		--disable-libass \
+		--enable-openal
 
 	./waf install
 
@@ -36,6 +38,6 @@ termux_step_make_install () {
 	local SYSTEM_LIBFOLDER=lib64
 	if [ $TERMUX_ARCH_BITS = 32 ]; then SYSTEM_LIBFOLDER=lib; fi
 	echo "#!/bin/sh" > $TERMUX_PREFIX/bin/mpv
-	echo "LD_LIBRARY_PATH=/system/$SYSTEM_LIBFOLDER:$TERMUX_PREFIX/lib $TERMUX_PREFIX/libexec/mpv \$@" >> $TERMUX_PREFIX/bin/mpv
+	echo "LD_LIBRARY_PATH=/system/$SYSTEM_LIBFOLDER:$TERMUX_PREFIX/lib $TERMUX_PREFIX/libexec/mpv \"\$@\"" >> $TERMUX_PREFIX/bin/mpv
 	chmod +x $TERMUX_PREFIX/bin/mpv
 }
