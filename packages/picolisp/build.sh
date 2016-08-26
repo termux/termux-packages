63fa37857aaf2310c06e66e02ac9e94e33116866
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-26 17:20:56 -0400

    picolisp: Update to latest version

--- packages/picolisp/build.sh
+++ packages/picolisp/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="Lisp interpreter and application server framework"
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 _PICOLISP_YEAR=16
 _PICOLISP_MONTH=8
-_PICOLISP_DAY=3
+_PICOLISP_DAY=26
 TERMUX_PKG_VERSION=${_PICOLISP_YEAR}.${_PICOLISP_MONTH}.${_PICOLISP_DAY}
 # We use our bintray mirror since old version snapshots are not kept on main site.
 # TERMUX_PKG_SRCURL=http://software-lab.de/picoLisp.tgz
 TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/picolisp_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=018015672e2abdc58b01b4da9b68899f9fc3d671e3faf464d77367a839621c7c
 TERMUX_PKG_FOLDERNAME=picoLisp
 TERMUX_PKG_BUILD_IN_SRC=true
 # The assembly is not position-independent (would be a major rewrite):
