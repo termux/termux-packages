efe97ca65d9acae819af330ef0da7536cc293039
Bangun K <bangun@users.noreply.github.com>
2016-08-29 10:45:46 +0700

    Add opus-tools package

---
+++ packages/opus-tools/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.opus-codec.org/
+TERMUX_PKG_DESCRIPTION="Utilities to encode and decode .opus files"
+TERMUX_PKG_VERSION=0.1.9
+TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/opus/opus-tools-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libopus, libogg, libflac"
