4790864684b716eba502f1fa92ee4a3f0b563a1f
Fredrik Fornwall <fredrik@fornwall.net>
2016-08-30 07:50:34 -0400

    proot: Bump to latest commit (with link2symlink)

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://proot.me/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
 # Just bump commit and version when needed:
-_COMMIT=d1f88afc447f2c6eee883ad5d88383084ffab6a1
-TERMUX_PKG_VERSION=5.1.100
-TERMUX_PKG_BUILD_REVISION=1
+_COMMIT=6c2b210e3887386a34bcafa154997aa03c291f7f
+TERMUX_PKG_VERSION=5.1.101
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_FOLDERNAME=proot-$_COMMIT
 TERMUX_PKG_DEPENDS="libtalloc"
