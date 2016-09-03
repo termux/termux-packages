5d383d990b7267dbc5f3c10bce679896e02ad0eb
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-03 17:36:59 -0400

    transmission: Avoid modifying LDFLAGS directly

--- packages/transmission/build.sh
+++ packages/transmission/build.sh
@@ -6,12 +6,12 @@ TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://transmission.cachefly.net/transmission-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk --enable-lightweight --cache-file=termux_configure.cache"
 
-# liblog for android logging in syslog hack
-export LDFLAGS="$LDFLAGS -llog"
+termux_step_pre_configure() {
+	# liblog for android logging in syslog hack:
+	LDFLAGS+=" -llog"
 
-termux_step_pre_configure () {
-        cd $TERMUX_PKG_SRCDIR
+	cd $TERMUX_PKG_SRCDIR
 	echo "ac_cv_func_getmntent=no" >> termux_configure.cache
 	echo "ac_cv_search_getmntent=false" >> termux_configure.cache
-	chmod a-w termux_configure.cache		# prevent configure from changing
+	chmod a-w termux_configure.cache
 }
