4ced24e6858c6763885c0fe3bdf6ad9b90fe55f3
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-03 18:45:08 -0400

    Cleanup of more packages

--- root-packages/macchanger/build.sh
+++ root-packages/macchanger/build.sh
@@ -3,8 +3,6 @@ TERMUX_PKG_DESCRIPTION="Utility that makes the maniputation of MAC addresses of 
 TERMUX_PKG_VERSION=1.7.0
 TERMUX_PKG_SRCURL=https://github.com/alobbs/macchanger/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=macchanger-${TERMUX_PKG_VERSION}
-# TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
-# TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl --disable-iri"
 
 termux_step_pre_configure() {
 	cd $TERMUX_PKG_SRCDIR
