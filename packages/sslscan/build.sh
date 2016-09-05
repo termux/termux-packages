761884a9dc14099b55d42bf10b8c6299aeb63a91
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-05 17:21:35 -0400

    sslscan: Update to https://github.com/rbsec/sslscan

--- packages/sslscan/build.sh
+++ packages/sslscan/build.sh
@@ -1,8 +1,9 @@
-TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/sslscan/
+TERMUX_PKG_HOMEPAGE=https://github.com/rbsec/sslscan
 TERMUX_PKG_DESCRIPTION="Fast SSL scanner"
-TERMUX_PKG_VERSION=1.8.2
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/sslscan/files/sslscan/sslscan-${TERMUX_PKG_VERSION}.tgz/download
+TERMUX_PKG_VERSION=1.11.7
+TERMUX_PKG_SRCURL=https://github.com/rbsec/sslscan/archive/${TERMUX_PKG_VERSION}-rbsec.tar.gz
+TERMUX_PKG_SHA256=1a2787b427eabc3e61350c0692970e4ab679891f6310e2dd855bab549ff8cf73
+TERMUX_PKG_FOLDERNAME=sslscan-${TERMUX_PKG_VERSION}-rbsec
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_FOLDERNAME=sslscan-${TERMUX_PKG_VERSION}
 TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
+TERMUX_PKG_MAINTAINER="Pierre Rudloff @Rudloff"
