18bf425df5575fb0671f716a88611051d7fc939e
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-05 17:34:10 -0400

    tasksh: Add package

---
+++ packages/tasksh/build.sh
@@ -0,0 +1,31 @@
+TERMUX_PKG_HOMEPAGE=https://taskwarrior.org
+TERMUX_PKG_DESCRIPTION="Shell command wrapping Taskwarrior commands"
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SRCURL=http://taskwarrior.org/download/tasksh-latest.tar.gz
+TERMUX_PKG_SHA256=5bc4791c47abecf34e3b1264d8c6b2671ac3cd4657a8588c57404893db3d4564
+TERMUX_PKG_DEPENDS="readline, taskwarrior, libandroid-glob"
+TERMUX_PKG_FOLDERNAME=tasksh-$TERMUX_PKG_VERSION
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-glob"
+}
+
+termux_step_configure () {
+	cd $TERMUX_PKG_BUILDDIR
+	cmake -G "Unix Makefiles" $TERMUX_PKG_SRCDIR \
+		-DCMAKE_AR=`which ${TERMUX_HOST_PLATFORM}-ar` \
+		-DCMAKE_BUILD_TYPE=MinSizeRel \
+		-DCMAKE_C_FLAGS="$CFLAGS $CPPFLAGS" \
+		-DCMAKE_CROSSCOMPILING=True \
+		-DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+		-DCMAKE_FIND_ROOT_PATH=$TERMUX_PREFIX \
+		-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+		-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
+		-DCMAKE_LINKER=`which ${TERMUX_HOST_PLATFORM}-ld` \
+		-DCMAKE_MAKE_PROGRAM=`which make` \
+		-DCMAKE_RANLIB=`which ${TERMUX_HOST_PLATFORM}-ranlib` \
+		-DCMAKE_SKIP_INSTALL_RPATH=ON \
+		-DCMAKE_SYSTEM_NAME=Linux \
+		$TERMUX_PKG_SRCDIR
+}
