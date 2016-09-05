51720bdf6cde8c9b36583783893abe938d609801
Oliver Schmidhauser <oliver.schmidhauser@gmail.com>
2016-09-06 00:29:07 +0200

    Add profanity, libstrophe and libotr (#431)
    * add libstrophe package
    * add libotr package
    * add profanity package

---
+++ packages/libotr/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://otr.cypherpunks.ca
+TERMUX_PKG_DESCRIPTION="Off-the-Record (OTR) Messaging allows you to have private conversations over instant messaging by providing: Encryption, Authentication, Deniability, Perfect forward secrecy"
+TERMUX_PKG_VERSION=4.1.1
+TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
+TERMUX_PKG_SRCURL=https://otr.cypherpunks.ca/libotr-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libgcrypt"
+TERMUX_PKG_BUILD_IN_SRC=yes
