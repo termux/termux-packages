9211b553796238aa4899e7e63da365d68538864a
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-07 06:55:56 -0400

    Update the disabled ghc package

--- disabled-packages/ghc/build.sh
+++ disabled-packages/ghc/build.sh
@@ -2,11 +2,6 @@
 #     https://ghc.haskell.org/trac/ghc/wiki/Building/Preparation/Linux
 # and
 #     https://github.com/neurocyte/ghc-android
-#
-# Status: Currently fails at "error: cannot find -lpthread"
-#         right after message about building bin/hpc.
-#         As libpthread does not exist on Android (pthread
-#         is built into libc, this needs to be patched away.
 TERMUX_PKG_HOMEPAGE=https://www.haskell.org/ghc/
 TERMUX_PKG_DESCRIPTION="The Glasgow Haskell Compilation system"
 TERMUX_PKG_VERSION=8.0.1
@@ -18,26 +13,28 @@ TERMUX_PKG_DEPENDS="clang, ncurses"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-iconv-includes=$TERMUX_PREFIX/include -with-iconv-libraries=$TERMUX_PREFIX/lib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-curses-includes=$TERMUX_PREFIX/include/ncursesw -with-curses-libraries=$TERMUX_PREFIX/lib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --host=${TERMUX_HOST_PLATFORM}"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --target=${TERMUX_HOST_PLATFORM}"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --host=x86_64-unknown-linux --build=x86_64-unknown-linux"
 
-ORIG_CFLAGS="$CFLAGS"
-ORIG_CPPFLAGS="$CPPFLAGS"
-ORIG_LDFLAGS="$LDFLAGS"
+termux_step_post_extract_package() {
+	ORIG_CFLAGS="$CFLAGS"
+	ORIG_CPPFLAGS="$CPPFLAGS"
+	ORIG_LDFLAGS="$LDFLAGS"
 
-unset AR
-unset AS
-unset CC
-export CFLAGS=""
-unset CPP
-export CPPFLAGS=""
-unset CXXFLAGS
-unset CXX
-export LDFLAGS=""
-unset LD
-unset PKG_CONFIG
-unset RANLIB
+	unset AR
+	unset AS
+	unset CC
+	export CFLAGS=""
+	unset CPP
+	export CPPFLAGS=""
+	unset CXXFLAGS
+	unset CXX
+	export LDFLAGS=""
+	unset LD
+	unset PKG_CONFIG
+	unset RANLIB
+
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --target=${TERMUX_HOST_PLATFORM}"
+}
 
 termux_step_pre_configure () {
 	echo "INTEGER_LIBRARY = integer-simple" > mk/build.mk
