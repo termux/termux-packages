9d5d6e98c60eb2ba639c3ab0b931373b33380de8
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-08 16:37:17 -0400

    ca-certificates: Bump to latest version available

--- packages/ca-certificates/build.sh
+++ packages/ca-certificates/build.sh
@@ -1,13 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://curl.haxx.se/docs/caextract.html
 TERMUX_PKG_DESCRIPTION="Common CA certificates"
-TERMUX_PKG_VERSION=20160429
+TERMUX_PKG_VERSION=20160907
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
 	local CERTDIR=$TERMUX_PREFIX/etc/tls
 	local CERTFILE=$CERTDIR/cert.pem
 	# If the checksum has changed, it may be time to update the package version..
-	local CERTFILE_SHA256=40f7c492be077f486df440b2497d2f68ae796619c57c1d32b82db18db853fb15
+	local CERTFILE_SHA256=e39faef9a13c44ddc75f7c0d0d6977fb2bc707ae1d08d47469b9c5ac8f260e40
 
 	mkdir -p $CERTDIR
 
