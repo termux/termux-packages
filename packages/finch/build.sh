f45367c4a5776eb56d1f0a66d737f94dedce8d0e
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-08 16:18:44 -0400

    finch: Update from 2.10.12 to 2.11.0

--- packages/finch/build.sh
+++ packages/finch/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://pidgin.im/
 TERMUX_PKG_DESCRIPTION="Text-based multi-protocol instant messaging client"
-TERMUX_PKG_VERSION=2.10.12
-TERMUX_PKG_BUILD_REVISION=3
+TERMUX_PKG_VERSION=2.11.0
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/pidgin/Pidgin/${TERMUX_PKG_VERSION}/pidgin-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libgnutls, libxml2, ncurses-ui-libs, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtkui --disable-gstreamer --disable-vv --disable-idn --disable-meanwhile --disable-avahi --disable-dbus --disable-perl --disable-tcl --without-zephyr --with-ncurses-headers=$TERMUX_PREFIX/include --without-python"
