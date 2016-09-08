e8cf9c285e8bf2f82d8ecdfa910f98010d225b74
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-08 11:24:01 -0400

    curl: Update from 7.50.1 to 7.50.2, enable http2

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -1,8 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
-TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_VERSION=7.50.1
+TERMUX_PKG_DEPENDS="openssl, libnghttp2"
+TERMUX_PKG_VERSION=7.50.2
 TERMUX_PKG_SRCURL=http://curl.askapache.com/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
-export TERMUX_CA_BUNDLE=$TERMUX_PREFIX/etc/tls/cert.pem
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --without-libidn --with-ca-bundle=$TERMUX_CA_BUNDLE"
+TERMUX_PKG_SHA256=0c72105df4e9575d68bcf43aea1751056c1d29b1040df6194a49c5ac08f8e233
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --without-libidn"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-ca-bundle=$TERMUX_PREFIX/etc/tls/cert.pem"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-nghttp2"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/curl-config share/man/man1/curl-config.1"
