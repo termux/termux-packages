94f1c11e25a1606a2e3dc621c9b5171e587b5f68
David Martínez <vaites@users.noreply.github.com>
2016-09-08 15:23:31 +0200

    Add Qalculate package (#393)

---
+++ packages/libcln/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=http://www.ginac.de/CLN/
+TERMUX_PKG_DESCRIPTION="CLN is a library for efficient computations with all kinds of numbers in arbitrary precision"
+TERMUX_PKG_VERSION=1.3.4
+TERMUX_PKG_SRCURL=http://www.ginac.de/CLN/cln-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-gnu-ld=no"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_pre_configure() {
+    cd $TERMUX_PKG_SRCDIR
+    sed -i -e 's%tests/Makefile %%' configure.ac
+    sed -i -e 's%examples/Makefile %%' configure.ac
+    sed -i -e 's%benchmarks/Makefile %%' configure.ac
+    
+    autoreconf
+}
+
+termux_step_post_configure() {
+    cd $TERMUX_PKG_SRCDIR
+    sed -i -e 's% tests%%' Makefile
+    sed -i -e 's% examples%%' Makefile
+    sed -i -e 's% benchmarks%%' Makefile
+}
