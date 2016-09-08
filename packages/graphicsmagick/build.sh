03fb3464623587b9efcc19971f38026117e88ae1
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-08 16:19:01 -0400

    graphicsmagick: Update from 1.3.24 to 1.3.25

--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.graphicsmagick.org/
 TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
-TERMUX_PKG_VERSION=1.3.24
+TERMUX_PKG_VERSION=1.3.25
 # Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="littlecms, libtiff, freetype, libjasper, libjpeg-turbo, libpng, libbz2, libxml2, liblzma"
