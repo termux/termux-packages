9d6bb14e0840b6cb244f189bd9b1117db566352b
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-09 09:35:03 -0400

    disabled-packages/attr: Fix compilation, add note

--- disabled-packages/attr/build.sh
+++ disabled-packages/attr/build.sh
@@ -1,3 +1,5 @@
+# Package disabled - does Android support setting extended file attributes?
+# I'm getting 'Operation not supported on transport endpoint' on all tests.
 TERMUX_PKG_HOMEPAGE=http://savannah.nongnu.org/projects/attr/
 TERMUX_PKG_DESCRIPTION="Commands for manipulating filesystem extended attributes."
 TERMUX_PKG_VERSION=2.4.47
@@ -6,5 +8,3 @@ TERMUX_PKG_FOLDERNAME=attr-$TERMUX_PKG_VERSION
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gettext=no"
 TERMUX_PKG_MAKE_INSTALL_TARGET="install install-lib"
-
-CPPFLAGS+=" -D__THROW="
