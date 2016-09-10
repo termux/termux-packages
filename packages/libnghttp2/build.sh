058757f2c153fe4469f818de111b2e88ed4a6304
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-10 10:24:25 -0400

    libnghttp2: Update from 1.14.0 to 1.14.1

--- packages/libnghttp2/build.sh
+++ packages/libnghttp2/build.sh
@@ -1,6 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://nghttp2.org/
 TERMUX_PKG_DESCRIPTION="nghttp HTTP 2.0 library"
-TERMUX_PKG_VERSION=1.14.0
+TERMUX_PKG_VERSION=1.14.1
 TERMUX_PKG_SRCURL=https://github.com/nghttp2/nghttp2/releases/download/v${TERMUX_PKG_VERSION}/nghttp2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=938f93a0b0c31fd70cd07c96506b8c4ad040ef98ad3f204bf77ec4476685942f
+TERMUX_PKG_SHA256=2dfa665cd898b0b03ce304d9fc2e20c6c5facf56b212e4b2b1834ed6314c897e
 TERMUX_PKG_FOLDERNAME="nghttp2-$TERMUX_PKG_VERSION"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-lib-only"
+# The tools are not built due to --enable-lib-only:
+TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1 share/nghttp2/fetch-ocsp-response"
