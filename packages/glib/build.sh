ed7e023032dbebbb3483dc5b93239f992b9754f2
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-12 06:23:37 -0400

    glib: Do not throw away lib/glib-2.0/glibconfig.h
    Include lib/glib-2.0/glibconfig.c in glib-dev instead of removing
    it, and let glib-dev depend on pcre-dev (needed for pkg-config to
    work).
    
    Fixes #447.

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -2,9 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 _TERMUX_GLIB_MAJOR_VERSION=2.48
 TERMUX_PKG_VERSION=${_TERMUX_GLIB_MAJOR_VERSION}.2
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/glib/${_TERMUX_GLIB_MAJOR_VERSION}/glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libffi, pcre"
-TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc share/locale share/glib-2.0/gettext share/gdb/auto-load share/glib-2.0/codegen share/glib-2.0/gdb lib/glib-2.0/include bin/gtester-report bin/glib-mkenums bin/glib-gettextize bin/gdbus-codegen"
+TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc share/locale share/glib-2.0/gettext share/gdb/auto-load share/glib-2.0/codegen share/glib-2.0/gdb bin/gtester-report bin/glib-mkenums bin/glib-gettextize bin/gdbus-codegen"
+# Needed by pkg-config for glib-2.0:
+TERMUX_PKG_DEVPACKAGE_DEPENDS="pcre-dev"
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="lib/glib-2.0/include"
 
 # --enable-compile-warnings=no to get rid of format strings causing errors
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-compile-warnings --disable-gtk-doc --disable-gtk-doc-html --cache-file=termux_configure.cache --with-pcre=system"
