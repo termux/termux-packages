038fecd027a83c50555d6f000f8aea7c50e98022
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-14 16:45:24 -0400

    taskwarrior: Fix indentation

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -10,9 +10,9 @@ termux_step_pre_configure() {
 
 termux_step_configure () {
 	cd $TERMUX_PKG_BUILDDIR
-	cmake -G "Unix Makefiles" .. \
+	cmake -G "Unix Makefiles" $TERMUX_PKG_SRCDIR \
 		-DCMAKE_AR=`which ${TERMUX_HOST_PLATFORM}-ar` \
-                -DCMAKE_BUILD_TYPE=MinSizeRel \
+		-DCMAKE_BUILD_TYPE=MinSizeRel \
 		-DCMAKE_C_FLAGS="$CFLAGS $CPPFLAGS" \
 		-DCMAKE_CROSSCOMPILING=True \
 		-DCMAKE_CXX_FLAGS="$CXXFLAGS" \
@@ -21,13 +21,12 @@ termux_step_configure () {
 		-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
 		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
 		-DCMAKE_LINKER=`which ${TERMUX_HOST_PLATFORM}-ld` \
-                -DCMAKE_MAKE_PROGRAM=`which make` \
+		-DCMAKE_MAKE_PROGRAM=`which make` \
 		-DCMAKE_RANLIB=`which ${TERMUX_HOST_PLATFORM}-ranlib` \
-                -DCMAKE_SKIP_INSTALL_RPATH=ON \
+		-DCMAKE_SKIP_INSTALL_RPATH=ON \
 		-DCMAKE_SYSTEM_NAME=Linux \
-                -DCMAKE_USE_SYSTEM_LIBRARIES=True \
-                -DZLIB_INCLUDE_DIR=$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include \
-                -DZLIB_LIBRARY=$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/libz.so \
-                -DBUILD_TESTING=OFF \
-		$TERMUX_PKG_SRCDIR
+		-DCMAKE_USE_SYSTEM_LIBRARIES=True \
+		-DZLIB_INCLUDE_DIR=$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include \
+		-DZLIB_LIBRARY=$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/libz.so \
+		-DBUILD_TESTING=OFF
 }
