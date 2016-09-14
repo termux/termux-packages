4b22de5815a26977275bce5b2a308bd2b334e80e
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-14 18:51:48 -0400

    curl: Fix package checksum

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl, libnghttp2"
 TERMUX_PKG_VERSION=7.50.3
 TERMUX_PKG_SRCURL=http://curl.askapache.com/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=0c72105df4e9575d68bcf43aea1751056c1d29b1040df6194a49c5ac08f8e233
+TERMUX_PKG_SHA256=7b7347d976661d02c84a1f4d6daf40dee377efdc45b9e2c77dedb8acf140d8ec
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --without-libidn"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-ca-bundle=$TERMUX_PREFIX/etc/tls/cert.pem"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-nghttp2"
