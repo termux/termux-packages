6428f23518f37ee8a6f6293491a99f8312e15376
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-14 16:48:03 -0400

    sqlite: Update from 3.14.1 to 3.14.2

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Software library that implements a self-contained, serve
 # Note: Updating this version requires bumping the tcl package as well.
 _SQLITE_MAJOR=3
 _SQLITE_MINOR=14
-_SQLITE_PATCH=1
+_SQLITE_PATCH=2
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
 TERMUX_PKG_SRCURL=https://www.sqlite.org/2016/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-readline"
--- packages/tcl/build.sh
+++ packages/tcl/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Powerful but easy to learn dynamic programming language"
 TERMUX_PKG_DEPENDS="libsqlite"
 _MAJOR_VERSION=8.6
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.6
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/tcl/Tcl/${TERMUX_PKG_VERSION}/tcl${TERMUX_PKG_VERSION}-src.tar.gz
 TERMUX_PKG_FOLDERNAME=tcl$TERMUX_PKG_VERSION
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="tcl_cv_strtod_buggy=ok tcl_cv_strstr_unbroken=ok"
