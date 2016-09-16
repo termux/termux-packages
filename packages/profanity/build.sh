9b084a741bb2e845385e617925846e571dee2bfe
Oliver Schmidhauser <oliver.schmidhauser@gmail.com>
2016-09-16 14:06:09 +0200

    update profanity to version 0.5.0

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -1,11 +1,19 @@
 TERMUX_PKG_HOMEPAGE=http://profanity.im
 TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C using ncurses and libstrophe, inspired by Irssi"
-TERMUX_PKG_VERSION=0.4.7
+TERMUX_PKG_VERSION=0.5.0
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_SRCURL=http://profanity.im/profanity-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses,glib,libstrophe,libcurl,readline,libuuid,libotr,gpgme"
+TERMUX_PKG_DEPENDS="ncurses,glib,libstrophe,libcurl,readline,libuuid,libotr,gpgme,python-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
+#Enforce python-plugin-support. Building without it works
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --enable-python-plugins"
 
-termux_step_pre_configure() {
-./bootstrap.sh
-}
+#What I tried so far:
+#termux_step_pre_configure() {
+	#export PYTHON_LDFLAGS+=" -L${PREFIX}/lib/python3.5"
+	#export PYTHON_CPPFLAGS+=" -I${PREFIX}/include/python3.5m"
+	#export PYTHON="$PREFIX/bin/python"
+	#export PYTHON_VERSION=3.5
+	#export PYTHON_NOVERSIONCHECK=yes
+	#export PYTHON_CONFIG_EXISTS=yes
+#}
