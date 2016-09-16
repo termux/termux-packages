f2748b7e93b91b70fe4019e4cc20720f7eb8cd51
Oliver Schmidhauser <oliver.schmidhauser@gmail.com>
2016-09-17 01:44:12 +0200

    Update Profanity and Libstrophe to their latest versions (#461)
    * Update libstrophe to version 0.9.1.
    * Update profanity to version 0.5.0.
    * Enable python plugin support for profanity.

--- packages/libstrophe/build.sh
+++ packages/libstrophe/build.sh
@@ -1,11 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://strophe.im/libstrophe/
 TERMUX_PKG_DESCRIPTION="libstrophe is a minimal XMPP library written in C"
-TERMUX_PKG_VERSION=0.8.20160905
+TERMUX_PKG_VERSION=0.9.1
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
-# Using latest commit because since 0.8.8 it has been somewhat optimized for compilation on Android
-_COMMIT=936ddb0d150ba3705e7190be274761331ef4fdca
-TERMUX_PKG_SRCURL=https://github.com/strophe/libstrophe/archive/${_COMMIT}.tar.gz
-TERMUX_PKG_FOLDERNAME=libstrophe-$_COMMIT
+TERMUX_PKG_SRCURL=https://github.com/strophe/libstrophe/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=libstrophe-$TERMUX_PKG_VERSION
 # Would also work with libxml2
 TERMUX_PKG_DEPENDS="openssl,libexpat"
 TERMUX_PKG_BUILD_IN_SRC=yes
--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -1,11 +1,20 @@
 TERMUX_PKG_HOMEPAGE=http://profanity.im
 TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C using ncurses and libstrophe, inspired by Irssi"
-TERMUX_PKG_VERSION=0.4.7
+TERMUX_PKG_VERSION=0.5.0
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_SRCURL=http://profanity.im/profanity-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses,glib,libstrophe,libcurl,readline,libuuid,libotr,gpgme"
+TERMUX_PKG_DEPENDS="ncurses,glib,libstrophe,libcurl,readline,libuuid,libotr,gpgme,python"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-python-plugins"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
-./bootstrap.sh
+  CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.5m"
+  LDFLAGS+=" -lpython3.5m"
+}
+
+termux_step_post_configure() {
+  # Enable python support manually, as trying to go using --enable-python-plugins
+  # causes configure trying to execut python:
+  echo '#define HAVE_PYTHON 1' >> $TERMUX_PKG_SRCDIR/src/config.h
+  perl -p -i -e 's|#am__objects_2|am__objects_2|' $TERMUX_PKG_SRCDIR/Makefile
 }
