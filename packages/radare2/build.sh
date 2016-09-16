2f176d0ddbeddb88481ac0410c5b0c8e210eff16
pancake <pancake@nopcode.org>
2016-09-16 10:44:14 +0200

    Add radare2-0.10.5 package

---
+++ packages/radare2/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://rada.re
+TERMUX_PKG_DESCRIPTION="Advanced Hexadecimal Editor"
+TERMUX_PKG_VERSION=0.10.5
+TERMUX_PKG_SRCURL=https://github.com/radare/radare2/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_FOLDERNAME="radare2-$TERMUX_PKG_VERSION"
+TERMUX_PKG_BUILD_IN_SRC="yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-compiler=termux-host"
