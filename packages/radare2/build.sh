d7e3209c36203ed212477ca1803aaf50c408671d
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-22 02:04:02 +0200

    radare2: Fix build if binutils has been built

--- packages/radare2/build.sh
+++ packages/radare2/build.sh
@@ -5,3 +5,10 @@ TERMUX_PKG_SRCURL=https://github.com/radare/radare2/archive/$TERMUX_PKG_VERSION.
 TERMUX_PKG_FOLDERNAME="radare2-$TERMUX_PKG_VERSION"
 TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-compiler=termux-host"
+
+termux_step_pre_configure() {
+	# Unset CPPFLAGS to avoid -I$TERMUX_PREFIX/include. This is because
+	# radare2 build will put it's own -I flags after ours, which causes
+	# problems due to name clashes (binutils header files).
+	unset CPPFLAGS
+}
