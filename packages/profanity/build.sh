47703d4d8b78beb7f3cf0d8112e9c357dcc5e57b
Oliver Schmidhauser <oliver.schmidhauser@gmail.com>
2016-09-28 20:16:33 +0200

    Build Profanity with libmesode instead of libstrophe (#480)

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://profanity.im
 TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C using ncurses and libstrophe, inspired by Irssi"
 TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_SRCURL=http://profanity.im/profanity-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses,glib,libstrophe,libcurl,readline,libuuid,libotr,gpgme,python"
+TERMUX_PKG_DEPENDS="ncurses,glib,libmesode,libcurl,readline,libuuid,libotr,gpgme,python"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-python-plugins"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
