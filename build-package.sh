720575ff8ea8a374b4535c1e838f81a3a931a073
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-03 17:00:01 -0400

    Update outdated comment about x86_64

--- build-package.sh
+++ build-package.sh
@@ -10,7 +10,7 @@ test -f $HOME/.termuxrc && . $HOME/.termuxrc
 : ${NDK:="${HOME}/lib/android-ndk"}
 : ${TERMUX_MAKE_PROCESSES:='4'}
 : ${TERMUX_TOPDIR:="$HOME/.termux-build"}
-: ${TERMUX_ARCH:="aarch64"} # (arm|aarch64|i686|x86_64) - the x86_64 arch is not yet used or tested.
+: ${TERMUX_ARCH:="aarch64"} # arm, aarch64, i686 or x86_64.
 : ${TERMUX_PREFIX:='/data/data/com.termux/files/usr'}
 : ${TERMUX_ANDROID_HOME:='/data/data/com.termux/files/home'}
 : ${TERMUX_DEBUG:=""}
