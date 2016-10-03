34b46b56de338349dfa321a01397364b3c52b449
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-03 08:41:27 -0400

    grep: Update from 2.25 to 2.26

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -1,6 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/grep/
 TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines containing a match to a specified pattern"
 TERMUX_PKG_DEPENDS="pcre"
-TERMUX_PKG_VERSION=2.25
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=2.26
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/grep/grep-${TERMUX_PKG_VERSION}.tar.xz
