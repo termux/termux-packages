6b53c23af4704fd8abb80b38648e86815450e3a6
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-03 10:02:43 -0400

    curl: Avoid oportunistic linking against librtmp

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -7,4 +7,5 @@ TERMUX_PKG_SHA256=7b7347d976661d02c84a1f4d6daf40dee377efdc45b9e2c77dedb8acf140d8
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --without-libidn"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-ca-bundle=$TERMUX_PREFIX/etc/tls/cert.pem"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-nghttp2"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --without-librtmp"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/curl-config share/man/man1/curl-config.1"
