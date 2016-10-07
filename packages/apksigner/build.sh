92f5e5c345f3da8b97f8bc4655d1d6d7372ae0a8
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-07 08:43:09 -0400

    apksigner: Create $TERMUX_PREFIX/share/dex directory if needed

--- packages/apksigner/build.sh
+++ packages/apksigner/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make () {
 	ant
+	mkdir -p $TERMUX_PREFIX/share/dex
 	$TERMUX_DX --dex --output $TERMUX_PREFIX/share/dex/apksigner.dex apksigner.jar
 	echo '#!/bin/sh' > $TERMUX_PREFIX/bin/apksigner
 	echo "dalvikvm -cp $TERMUX_PREFIX/share/dex/apksigner.dex net.fornwall.apksigner.Main \$@" >> $TERMUX_PREFIX/bin/apksigner
