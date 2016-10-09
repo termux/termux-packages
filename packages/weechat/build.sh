3c11c91f69f968289c7e8f0fac9dadd98af6250c
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-09 16:20:38 -0400

    weechat: Update from 1.5 to 1.6

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,16 +1,17 @@
 TERMUX_PKG_HOMEPAGE=http://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
-TERMUX_PKG_VERSION=1.5
+TERMUX_PKG_VERSION=1.6
 TERMUX_PKG_SRCURL=http://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=73eca5212fadb7056f5f72d9dfc96453dea0342c963d1f1ca36c84def5653c34
 TERMUX_PKG_DEPENDS="ncurses, libgcrypt, libcurl, libgnutls, libandroid-support"
 # weechat-curses is a symlink to weechat, so remove it:
 TERMUX_PKG_RM_AFTER_INSTALL="bin/weechat-curses share/icons"
 
 termux_step_configure () {
 	cd $TERMUX_PKG_BUILDDIR
-	cmake -G "Unix Makefiles" .. \
+	cmake -G "Unix Makefiles" $TERMUX_PKG_SRCDIR \
 		-DCMAKE_AR=`which ${TERMUX_HOST_PLATFORM}-ar` \
-                -DCMAKE_BUILD_TYPE=MinSizeRel \
+		-DCMAKE_BUILD_TYPE=MinSizeRel \
 		-DCMAKE_CROSSCOMPILING=True \
 		-DCMAKE_C_FLAGS="$CFLAGS $CPPFLAGS" \
 		-DCMAKE_CXX_FLAGS="$CXXFLAGS" \
@@ -19,10 +20,10 @@ termux_step_configure () {
 		-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
 		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
 		-DCMAKE_LINKER=`which ${TERMUX_HOST_PLATFORM}-ld` \
-                -DCMAKE_MAKE_PROGRAM=`which make` \
+		-DCMAKE_MAKE_PROGRAM=`which make` \
 		-DCMAKE_RANLIB=`which ${TERMUX_HOST_PLATFORM}-ranlib` \
 		-DCMAKE_SYSTEM_NAME=Linux \
-                -DPKG_CONFIG_EXECUTABLE=$PKG_CONFIG \
+		-DPKG_CONFIG_EXECUTABLE=$PKG_CONFIG \
 		-DZLIB_LIBRARY:FILEPATH="$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/libz.so" \
 		-DZLIB_INCLUDE_DIR:PATH="$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include" \
 		-DCA_FILE="$TERMUX_PREFIX/etc/tls/cert.pem" \
@@ -30,6 +31,5 @@ termux_step_configure () {
 		-DENABLE_MAN=ON \
 		-DENABLE_PYTHON3=ON \
 		-DENABLE_PERL=ON \
-		-DENABLE_TCL=OFF \
-		$TERMUX_PKG_SRCDIR
+		-DENABLE_TCL=OFF
 }
