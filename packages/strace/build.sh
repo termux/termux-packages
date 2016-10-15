d3dc1c5888d508524c79774f9aa5bf3c5f0517ac
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-15 14:19:01 -0400

    strace: Update from 4.13 to 4.14

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,7 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://sourceforge.net/projects/strace/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor the system calls used by a program and all the signals it receives"
-TERMUX_PKG_VERSION=4.13
+TERMUX_PKG_VERSION=4.14
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/strace/strace/${TERMUX_PKG_VERSION}/strace-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5bed5110b243dce6864bedba269446c18c8c63f553cdd7fd4f808d89a764712f
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 
 termux_step_pre_configure () {
