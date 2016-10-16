3ed9ff3aebc3b5f65f15edc24757ace76ed5da09
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-15 20:31:44 -0400

    sqlite: Update from 3.14.2 to 3.15.0

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://www.sqlite.org/
 TERMUX_PKG_DESCRIPTION="Software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine"
 # Note: Updating this version requires bumping the tcl package as well.
 _SQLITE_MAJOR=3
-_SQLITE_MINOR=14
-_SQLITE_PATCH=2
-TERMUX_PKG_BUILD_REVISION=1
+_SQLITE_MINOR=15
+_SQLITE_PATCH=0
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
 TERMUX_PKG_SRCURL=https://www.sqlite.org/2016/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-readline"
