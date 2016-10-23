6d7d00d547f7ab185da5831aa1147ad10aeca257
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-23 16:41:52 -0400

    aria2: Update from 1.27.1 to 1.28.0

--- packages/aria2/build.sh
+++ packages/aria2/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://aria2.github.io
 TERMUX_PKG_DESCRIPTION="Multi-protocol & multi-source command-line download utility supporting HTTP/HTTPS, FTP, BitTorrent and Metalink"
-TERMUX_PKG_VERSION=1.27.1
+TERMUX_PKG_VERSION=1.28.0
 TERMUX_PKG_SRCURL=https://github.com/aria2/aria2/releases/download/release-${TERMUX_PKG_VERSION}/aria2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=c09627ef31602cfdfa7c9925a6c3b05fe7d2097d83f42dcfdef68664bd106f08
+TERMUX_PKG_SHA256=e0a125a557b9aacf23468374f63215e9b1d6165c990bd8ecb0603cdc1d5d72f0
 TERMUX_PKG_DEPENDS="c-ares, openssl, libxml2"
 # sqlite3 is only used for loading cookies from firefox or chrome:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-openssl --without-gnutls --without-libuv --without-sqlite3 ac_cv_search_getaddrinfo=no ac_cv_func_getaddrinfo=yes ac_cv_func_gettimeofday=yes ac_cv_func_sleep=yes ac_cv_func_usleep=yes ac_cv_func_basename=yes"
