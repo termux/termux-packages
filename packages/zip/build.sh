6a69d51f4c81006850f90b654b3193fb0c539548
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-28 14:31:02 -0400

    zip: Fix src download url

--- packages/zip/build.sh
+++ packages/zip/build.sh
@@ -1,7 +1,8 @@
 TERMUX_PKG_VERSION=3.0
 TERMUX_PKG_HOMEPAGE=http://www.info-zip.org/
 TERMUX_PKG_DESCRIPTION="Tools for working with zip files"
-TERMUX_PKG_SRCURL="http://downloads.sourceforge.net/project/infozip/Zip%203.x%20%28latest%29/3.0/zip30.tar.gz"
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/infozip/zip30.tar.gz
+TERMUX_PKG_SHA256=f0e8bb1f9b7eb0b01285495a2699df3a4b766784c1765a8f1aeedf63c0806369
 TERMUX_PKG_DEPENDS="libandroid-support,bzip2"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
