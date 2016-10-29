3fc9ead8d5867f3ebe5d573d1b2f6a106e531f97
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-29 16:25:44 -0400

    gnuchess: Update from 6.2.3 to 6.2.4

--- packages/gnuchess/build.sh
+++ packages/gnuchess/build.sh
@@ -1,6 +1,6 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/chess/
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/chess/
 TERMUX_PKG_DESCRIPTION="Chess-playing program"
-TERMUX_PKG_VERSION=6.2.3
+TERMUX_PKG_VERSION=6.2.4
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/chess/gnuchess-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="ncurses, readline"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/gnuchessu bin/gnuchessx"
