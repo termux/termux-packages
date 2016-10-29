abcc889a2c4626eeabfa26738ef0263d38bc4789
Fredrik Fornwall <fredrik@fornwall.net>
2016-10-29 17:55:51 -0400

    harfbuzz: Update from 1.3.1 to 1.3.3

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://www.freedesktop.org/wiki/Software/HarfBuzz/
+TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.3.1
-TERMUX_PKG_SRCURL=http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=2620987115a4122b47321610dccbcc18f7f121115fd7b88dc8a695c8b66cb3c9
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=no"
 
