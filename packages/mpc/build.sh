90f578a753863adc705146f272aeead402aaf5cc
Matthew Klein <mklein994@gmail.com>
2016-11-03 16:09:15 -0500

    mpc: fix source url

--- packages/mpc/build.sh
+++ packages/mpc/build.sh
@@ -5,6 +5,6 @@ TERMUX_PKG_DEPENDS="libmpdclient"
 _MAIN_VERSION=0
 _PATCH_VERSION=28
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
-TERMUX_PKG_SRCURL=https://www.musicpd.org/download/${pkgname}/${_MAIN_VERSION}/${pkgname}-${_MAIN_VERSION}.tar.xz
+TERMUX_PKG_SRCURL=https://www.musicpd.org/download/${pkgname}/${_MAIN_VERSION}/${pkgname}-${_MAIN_VERSION}.${_PATCH_VERSION}.tar.xz
 # Include some useful scripts for editing playlists
 TERMUX_PKG_KEEP_SHARE_DOC=yes
