8a544cfcaf92c06df31ddf2d740a39be38f46c46
Matthew Klein <mklein994@gmail.com>
2016-11-03 16:07:00 -0500

    libmpdclient: fix source url

--- packages/libmpdclient/build.sh
+++ packages/libmpdclient/build.sh
@@ -5,6 +5,6 @@ TERMUX_PKG_DEPENDS=""
 _MAIN_VERSION=2
 _PATCH_VERSION=10
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
-TERMUX_PKG_SRCURL=https://www.musicpd.org/download/${pkgname}/${_MAIN_VERSION}/${pkgname}-${_MAIN_VERSION}.tar.xz
+TERMUX_PKG_SRCURL=https://www.musicpd.org/download/${pkgname}/${_MAIN_VERSION}/${pkgname}-${_MAIN_VERSION}.${_PATCH_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-documentation"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-default-socket=${TERMUX_PREFIX}/var/run/mpd/socket"
