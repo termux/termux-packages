36c207bc214b2ce76bb8f60d2e4973e3c9cf4722
Fredrik Fornwall <fredrik@fornwall.net>
2016-11-07 18:08:31 -0500

    screen: Update the disabled package

--- disabled-packages/screen/build.sh
+++ disabled-packages/screen/build.sh
@@ -1,6 +1,12 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/screen/
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/screen/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with VT100/ANSI terminal emulation"
-TERMUX_PKG_VERSION=4.3.1
-TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/screen/screen-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=4.4.0
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/screen/screen-${TERMUX_PKG_VERSION}.tar.gz
 # TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
 # TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl"
+
+termux_step_pre_configure () {
+	# Run autoreconf since we have patched configure.ac
+	cd $TERMUX_PKG_SRCDIR
+	autoconf
+}
