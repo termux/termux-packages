9f04f4c4a9204cdfd46028790e232db12d69a4f5
Fredrik Fornwall <fredrik@fornwall.net>
2016-11-20 16:05:38 -0500

    libvpx: Build with --disable-realtime-only
    Fixes #554.

--- packages/libvpx/build.sh
+++ packages/libvpx/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.webmproject.org
 TERMUX_PKG_DESCRIPTION="VP8 & VP9 Codec SDK"
 TERMUX_PKG_VERSION=1.6.0
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_BUILD_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/webmproject/libvpx/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=libvpx-${TERMUX_PKG_VERSION}
 
@@ -25,11 +25,18 @@ termux_step_configure () {
 		echo "Unsupported arch: $TERMUX_ARCH"
 		exit 1
 	fi
+
+	# For --disable-realtime-only, see
+	# https://bugs.chromium.org/p/webm/issues/detail?id=800
+	# "The issue is that on android we soft enable realtime only.
+	#  [..] You can enable non-realtime by setting --disable-realtime-only"
+	# Discovered in https://github.com/termux/termux-packages/issues/554
 	$TERMUX_PKG_SRCDIR/configure \
 		--sdk-path=$NDK \
 		$_CONFIGURE_TARGET \
 		--prefix=$TERMUX_PREFIX \
 		--disable-examples \
+		--disable-realtime-only \
 		--enable-vp8 \
 		--enable-shared \
 		--enable-small
