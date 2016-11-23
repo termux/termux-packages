8c7007fa414ae431682532101188b70c1b40a672
Fredrik Fornwall <fredrik@fornwall.net>
2016-11-23 18:47:13 -0500

    mpv: Add /system/vendor/lib(64) to LD_LIBRARY_PATH
    Fixes issue with not found libqc-opt.so on some devices, reported
    by BrainDamage on #termux.

--- packages/mpv/build.sh
+++ packages/mpv/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://mpv.io/
 TERMUX_PKG_DESCRIPTION="Command-line media player"
 TERMUX_PKG_VERSION=0.22.0
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c0f9ac8f0e37a391d19007b333ef8787c2f45d75a0d4401c0098cde52c5082f6
 TERMUX_PKG_FOLDERNAME=mpv-${TERMUX_PKG_VERSION}
@@ -39,6 +40,8 @@ termux_step_make_install () {
 	local SYSTEM_LIBFOLDER=lib64
 	if [ $TERMUX_ARCH_BITS = 32 ]; then SYSTEM_LIBFOLDER=lib; fi
 	echo "#!/bin/sh" > $TERMUX_PREFIX/bin/mpv
-	echo "LD_LIBRARY_PATH=/system/$SYSTEM_LIBFOLDER:$TERMUX_PREFIX/lib $TERMUX_PREFIX/libexec/mpv \"\$@\"" >> $TERMUX_PREFIX/bin/mpv
+	# /system/vendor/lib(64) needed for libqc-opt.so on
+	# a xperia z5 c, reported by BrainDamage on #termux.
+	echo "LD_LIBRARY_PATH=/system/$SYSTEM_LIBFOLDER:/system/vendor/$SYSTEM_LIBFOLDER:$TERMUX_PREFIX/lib $TERMUX_PREFIX/libexec/mpv \"\$@\"" >> $TERMUX_PREFIX/bin/mpv
 	chmod +x $TERMUX_PREFIX/bin/mpv
 }
