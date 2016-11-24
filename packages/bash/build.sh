4643d4be2cdcd1448d08ac0408a813b3b66836d7
Fredrik Fornwall <fredrik@fornwall.net>
2016-11-23 19:33:04 -0500

    bash: Specify bash_cv_getcwd_malloc=yes
    When cross compiling bash assumes that getcwd is broken, so a
    wrapper is used which does not work with non-accessible parent
    directories (/data when running in Termux). We tell configure
    that this is not necessary.
    
    Fixes https://github.com/termux/termux-app/issues/200

--- packages/bash/build.sh
+++ packages/bash/build.sh
@@ -1,8 +1,9 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/bash/
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/bash/
 TERMUX_PKG_DESCRIPTION="A sh-compatible shell that incorporates useful features from the Korn shell (ksh) and C shell (csh)"
 TERMUX_PKG_DEPENDS="ncurses, readline, libandroid-support, termux-tools, command-not-found"
 _MAIN_VERSION=4.4
 _PATCH_VERSION=5
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/bash/bash-${_MAIN_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-multibyte --without-bash-malloc --with-installed-readline ac_cv_header_grp_h=no ac_cv_rl_version=7.0"
@@ -13,6 +14,12 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_unusable_rtsigs=no"
 # Use bash_cv_dev_fd=whacky to use /proc/self/fd instead of /dev/fd.
 # After making this change process substitution such as in 'cat <(ls)' works.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_dev_fd=whacky"
+# Bash assumes that getcwd is broken and provides a wrapper which
+# does not work when not all parent directories up to root are
+# accessible, which they are no under Android (/data). See
+# - http://permalink.gmane.org/gmane.linux.embedded.yocto.general/25204
+# - https://github.com/termux/termux-app/issues/200
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_getcwd_malloc=yes"
 
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/bashbug.1 bin/bashbug"
 
