4a8dd7187048dff05c69d450aa3fa8e570224e6c
David Martinez <contacto@davidmartinez.net>
2016-11-28 15:29:58 +0100

    Fixed Megatools build for 1.9.98 version

--- packages/megatools/build.sh
+++ packages/megatools/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/megous/megatools
 TERMUX_PKG_DESCRIPTION="Open-source command line tools and C library (libmega) for accessing Mega.co.nz cloud storage"
-TERMUX_PKG_VERSION=1.9.97
+TERMUX_PKG_VERSION=1.9.98
 TERMUX_PKG_SRCURL=https://github.com/megous/megatools/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_FOLDERNAME=megatools-$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, libgmp, openssl"
