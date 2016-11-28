ae707f42d05d47f2758715fd5080b5628a21d08e
Fredrik Fornwall <fredrik@fornwall.net>
2016-11-28 18:18:09 -0500

    silversearcher-ag: Update from 0.33.0 to 1.0.1

--- packages/silversearcher-ag/build.sh
+++ packages/silversearcher-ag/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://geoff.greer.fm/ag/
 TERMUX_PKG_DESCRIPTION="Fast grep-like program, alternative to ack-grep"
-TERMUX_PKG_VERSION=0.33.0
+TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SRCURL=http://geoff.greer.fm/ag/releases/the_silver_searcher-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d9682a70f5842197d10391e9b857576c9b6fc4e930cd6d241c249e7b4982f2ca
 TERMUX_PKG_FOLDERNAME=the_silver_searcher-${TERMUX_PKG_VERSION}
 TERMUX_PKG_DEPENDS="pcre, liblzma"
