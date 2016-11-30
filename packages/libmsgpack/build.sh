c934097d0b423ffd6ad58fd1e9923bec495a80be
Fredrik Fornwall <fredrik@fornwall.net>
2016-11-29 20:07:47 -0500

    Remove unused cmake argument

--- packages/libmsgpack/build.sh
+++ packages/libmsgpack/build.sh
@@ -8,7 +8,7 @@ termux_step_configure () {
 	cd $TERMUX_PKG_BUILDDIR
 	cmake -G "Unix Makefiles" .. \
 		-DCMAKE_AR=`which ${TERMUX_HOST_PLATFORM}-ar` \
-                -DCMAKE_BUILD_TYPE=MinSizeRel \
+		-DCMAKE_BUILD_TYPE=MinSizeRel \
 		-DCMAKE_CROSSCOMPILING=True \
 		-DCMAKE_C_FLAGS="$CFLAGS $CPPFLAGS" \
 		-DCMAKE_CXX_FLAGS="$CXXFLAGS" \
@@ -17,10 +17,9 @@ termux_step_configure () {
 		-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
 		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
 		-DCMAKE_LINKER=`which ${TERMUX_HOST_PLATFORM}-ld` \
-                -DCMAKE_MAKE_PROGRAM=`which make` \
+		-DCMAKE_MAKE_PROGRAM=`which make` \
 		-DCMAKE_RANLIB=`which ${TERMUX_HOST_PLATFORM}-ranlib` \
 		-DCMAKE_SYSTEM_NAME=Android \
-                -DPKG_CONFIG_EXECUTABLE=$PKG_CONFIG \
 		-DMSGPACK_BUILD_EXAMPLES=OFF \
 		$TERMUX_PKG_SRCDIR
 }
