f63405a98a21e1939b4a1bad52033af927f4a1f9
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-02 15:54:36 -0500

    binutils: Setup some arch-prefixed symlinks

--- packages/binutils/build.sh
+++ packages/binutils/build.sh
@@ -1,6 +1,7 @@
-TERMUX_PKG_VERSION=2.27
-TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/binutils/
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/binutils/
 TERMUX_PKG_DESCRIPTION="Collection of binary tools, the main ones being ld, the GNU linker, and as, the GNU assembler"
+TERMUX_PKG_VERSION=2.27
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/binutils/binutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-werror"
 TERMUX_PKG_EXTRA_MAKE_ARGS="tooldir=$TERMUX_PREFIX"
@@ -11,4 +12,11 @@ export LEXLIB=
 
 termux_step_post_make_install () {
 	cp $TERMUX_PKG_BUILDER_DIR/ldd $TERMUX_PREFIX/bin/ldd
+	cd $TERMUX_PREFIX/bin
+
+	# Setup symlinks as these are used when building, so used by
+	# system setup in e.g. python, perl and libtool:
+	for b in ar ld nm objdump ranlib readelf strip; do
+		ln -s -f $b $TERMUX_HOST_PLATFORM-$b
+	done
 }
