68129b3c5d45ecf51ea9082378e5e7ed70117402
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-07 22:04:28 -0500

    vim: Patch to enable channel support
    The vim configure script had a bug which caused the channel support
    to be disabled. Patch this while awaiting a fix merged upstream.
    
    Fixes #577.

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_HOMEPAGE=http://www.vim.org/
 TERMUX_PKG_DEPENDS="ncurses, vim-runtime"
 
 # Vim 8.0 patches described at ftp://ftp.vim.org/pub/vim/patches/8.0/README
-TERMUX_PKG_VERSION=8.0.0104
+TERMUX_PKG_VERSION=8.0.0124
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_FOLDERNAME=vim-${TERMUX_PKG_VERSION}
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="vim_cv_toupper_broken=no vim_cv_terminfo=yes vim_cv_tty_group=world"
