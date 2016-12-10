732d62c400b9843dd2815a5ad264393d148e6906
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-09 22:13:02 -0500

    libpixman: Configure with --disable-libpng

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -2,3 +2,4 @@ TERMUX_PKG_HOMEPAGE=http://www.pixman.org/
 TERMUX_PKG_DESCRIPTION="Low-level library for pixel manipulation"
 TERMUX_PKG_VERSION=0.34.0
 TERMUX_PKG_SRCURL=http://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
