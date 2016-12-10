0338ce958f6068f6f3b9041d352f8f5dbd8b162d
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-09 21:41:34 -0500

    gsl: Update from 2.2.1 to 2.3

--- packages/gsl/build.sh
+++ packages/gsl/build.sh
@@ -1,7 +1,6 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/gsl/
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gsl/
 TERMUX_PKG_DESCRIPTION="GNU Scientific Library (GSL) providing a wide range of mathematical routines"
-TERMUX_PKG_VERSION=2.2.1
-TERMUX_PKG_BUILD_REVISION=1
+TERMUX_PKG_VERSION=2.3
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gsl/gsl-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_RM_AFTER_INSTALL="bin/ share/man/man1"
 
