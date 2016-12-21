d628b9b0a5c82db3d36fde49fb45cd8efe45fb05
its-pointless <its-pointless@users.noreply.github.com>
2016-12-21 13:02:46 +1100

    Termux alpine (#593)
    * alpine should compile
    
    * fix arm build
    
    * combined patches and adjusted build.sh
    
    * add maildir support

--- disabled-packages/alpine/build.sh
+++ disabled-packages/alpine/build.sh
@@ -3,10 +3,21 @@ TERMUX_PKG_DESCRIPTION="Fast, easy to use email client"
 TERMUX_PKG_VERSION=2.20
 TERMUX_PKG_SRCURL=http://patches.freeiz.com/alpine/release/src/alpine-${TERMUX_PKG_VERSION}.tar.xz
 #TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-tcl --without-ldap --without-krb5 --disable-debug"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="  --with-c-client-target=lnx --without-pthread --without-password-prog --without-tcl --without-ldap --without-krb5 --disable-debug --with-system-pinerc=/data/data/com.termux/files/usr/etc/pine.conf --with-password-prog=no"
+export TERMUX_HOST_PLATFORM="${TERMUX_ARCH}-linux-android"
+if [ $TERMUX_ARCH = "arm" ]; then export TERMUX_HOST_PLATFORM="${TERMUX_HOST_PLATFORM}eabi"; fi
 TERMUX_PKG_BUILD_IN_SRC=yes
-
 termux_step_pre_configure () {
+LDFLAGS+=" -lcrypt -llog"
+	cp $TERMUX_PKG_BUILDER_DIR/getpass.c $TERMUX_PKG_SRCDIR/include/
+	cp $TERMUX_PKG_BUILDER_DIR/getpass.h $TERMUX_PKG_SRCDIR/include/
 	cd $TERMUX_PKG_SRCDIR
-	autoconf
+	autoreconf -if
+touch $TERMUX_PKG_SRCDIR/imap/lnxok
+}
+export TPATH=$PATH
+termux_step_pre_make () {
+	cd pith
+        $CC_FOR_BUILD help_c_gen.c -o help_c_gen
+	$CC_FOR_BUILD help_h_gen.c -o help_h_gen
 }
