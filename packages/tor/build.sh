ec75fbd3fb97ec3adc2cc15cae2095ab013c0f2a
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-20 21:59:23 -0500

    tor: Update from 0.2.8.10 to 0.2.9.8

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE='https://www.torproject.org'
 TERMUX_PKG_DESCRIPTION='The Onion Router anonymizing overlay network.'
 TERMUX_PKG_DEPENDS="libevent (>=2.0.22-2), openssl"
-TERMUX_PKG_VERSION='0.2.8.10'
+TERMUX_PKG_VERSION=0.2.9.8
 TERMUX_PKG_SRCURL="https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_CONFFILES='etc/tor/torrc'
-TERMUX_PKG_SHA256='c88b8c57b34ebf44b731df5d68f73eb6b6708bcf4e42cf7b4817fd4e304c9c4d'
+TERMUX_PKG_SHA256=fbdd33d3384574297b88744622382008d1e0f9ddd300d330746c464b7a7d746a
 TERMUX_PKG_MAINTAINER='Vishal Biswas @vishalbiswas'
 
 termux_step_pre_configure () {
