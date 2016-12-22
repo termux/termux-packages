83f72247486edc80af69b09f23fdc4be8995753b
vishalbiswas <vshlbiswas@ymail.com>
2016-12-22 10:51:13 +0530

    add packages liblzo and lzop

---
+++ packages/lzop/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://www.lzop.org
+TERMUX_PKG_DESCRIPTION='File compressor using lzo lib.'
+TERMUX_PKG_VERSION=1.03
+TERMUX_PKG_SRCURL=http://www.lzop.org/download/lzop-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=c1425b8c77d49f5a679d5a126c90ea6ad99585a55e335a613cae59e909dbb2c9
+TERMUX_PKG_DEPENDS="liblzo"
+TERMUX_PKG_MAINTAINER='Vishal Biswas @vishalbiswas'
+
