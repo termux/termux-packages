650d55cdcabd06098d9660a9df222c7806e2b154
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-22 14:18:35 -0500

    gdk-pixbuf: Update from 2.36.0 to 2.36.2

--- packages/gdk-pixbuf/build.sh
+++ packages/gdk-pixbuf/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/gdk-pixbuf/
 TERMUX_PKG_DESCRIPTION="Library for image loading and manipulation"
 _MAJOR_VERSION=2.36
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
 TERMUX_PKG_SRCURL=ftp://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/${_MAJOR_VERSION}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo"
 # Avoid runtime test which does not work when cross compiling:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gio-sniffing"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gio-sniffing --disable-glibtest"
