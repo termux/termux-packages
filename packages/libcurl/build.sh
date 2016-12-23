23fed2ad41b4e17be985a078252cd829937eda4a
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-23 14:58:47 -0500

    curl: Update from 7.52.0 to 7.52.1

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl, libnghttp2"
-TERMUX_PKG_VERSION=7.52.0
+TERMUX_PKG_VERSION=7.52.1
 TERMUX_PKG_SRCURL=http://curl.askapache.com/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=b9a2e18b4785eb75ad84598720e1559e1c53550ea011c0e00becdb94e2df5cc6
+TERMUX_PKG_SHA256=d16185a767cb2c1ba3d5b9096ec54e5ec198b213f45864a38b3bda4bbf87389b
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl --without-libidn"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-ca-bundle=$TERMUX_PREFIX/etc/tls/cert.pem"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-nghttp2"
