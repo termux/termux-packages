1d740dea046be3098e54654449df8753f6b34543
Oliver Schmidhauser <oli@glow.li>
2016-12-23 08:59:56 +0100

    Add ed package (#607)
    * Add ed package
    
    Fixes #606
    
    * change ed url to main mirror

---
+++ packages/ed/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/ed/
+TERMUX_PKG_DESCRIPTION="Ed is a line-oriented text editor. It is used to create, display, modify and otherwise manipulate text files, both interactively and via shell scripts. "
+TERMUX_PKG_VERSION=1.13
+TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
+TERMUX_PKG_SRCURL=http://ftp.gnu.org/gnu/ed/ed-${TERMUX_PKG_VERSION}.tar.lz
