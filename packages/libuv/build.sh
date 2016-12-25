23d4faf045eb763764bc4ef2812604eb74ef9693
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-25 18:50:52 -0500

    libuv: Update from 1.10.0 to 1.10.1

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
-TERMUX_PKG_VERSION=1.10.0
+TERMUX_PKG_VERSION=1.10.1
 TERMUX_PKG_SRCURL=http://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0307a0eec6caddd476f9cad39e18fdd6f22a08aa58103c4b0aead96d638be15e
+TERMUX_PKG_SHA256=4886badd96dcc74debbb5736381960f22bae2308166bc0a1074bcd6d364d1a7f
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-dtrace" # needed for building on mac
 
 termux_step_pre_configure () {
