9b88f344b06aad9a325003a8b17deac4347bea26
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-25 18:55:38 -0500

    aria2: Remove running autoreconf

--- packages/aria2/build.sh
+++ packages/aria2/build.sh
@@ -6,9 +6,3 @@ TERMUX_PKG_SHA256=bf6c5366d11d2a6038c8e19f01f9b874041793aaf317e0206120e3e8c9c431
 TERMUX_PKG_DEPENDS="c-ares, openssl, libxml2"
 # sqlite3 is only used for loading cookies from firefox or chrome:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-openssl --without-gnutls --without-libuv --without-sqlite3 ac_cv_search_getaddrinfo=no ac_cv_func_getaddrinfo=yes ac_cv_func_gettimeofday=yes ac_cv_func_sleep=yes ac_cv_func_usleep=yes ac_cv_func_basename=yes"
-
-termux_step_pre_configure () {
-	# Run autoreconf since we have patched Makefile.am:
-	cd $TERMUX_PKG_SRCDIR
-	autoreconf
-}
