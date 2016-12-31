e8743e96b0d598430094b1c022717c5c05dc2725
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-31 07:38:02 -0500

    harfbuzz: Update from 1.3.3 to 1.3.4

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,13 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_VERSION=1.3.4
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=2620987115a4122b47321610dccbcc18f7f121115fd7b88dc8a695c8b66cb3c9
+TERMUX_PKG_SHA256=718aa6fcadef1a6548315b8cfe42cc27e926256302c337f42df3a443843f6a2b
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=no"
-
-termux_step_pre_configure () {
-	# Needed to work with automake 1.15 since we patch Makefile.am
-	cd $TERMUX_PKG_SRCDIR
-	autoreconf
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=no --disable-gtk-doc-html"
