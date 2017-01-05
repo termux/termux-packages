5e190dd4b301b23816f6932aa748893089fd2fd6
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-05 13:57:52 -0500

    getconf: Add package

---
+++ packages/getconf/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/termux/getconf
+TERMUX_PKG_DESCRIPTION="Utility to print configuration values"
+TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_SRCURL=https://github.com/termux/getconf/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME=getconf-$TERMUX_PKG_VERSION
+TERMUX_PKG_BUILD_IN_SRC=yes
