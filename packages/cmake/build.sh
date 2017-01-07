deeb04b5609eda52cbcdd8e46c38d54d5696d447
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-07 01:00:39 -0500

    cmake: Update from 3.6.2 to 3.7.1

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://www.cmake.org/
+TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
-_MAJOR_VERSION=3.6
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
-TERMUX_PKG_SRCURL=http://www.cmake.org/files/v${_MAJOR_VERSION}/cmake-${TERMUX_PKG_VERSION}.tar.gz
+_MAJOR_VERSION=3.7
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${_MAJOR_VERSION}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, ncurses, jsoncpp"
 
 termux_step_configure () {
