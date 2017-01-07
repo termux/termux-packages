627fdeb1628e78ae5c617c0e137b375daa4dbe37
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-07 01:45:46 -0500

    ndk_patches: Define _POSIX2_VERSION to 200809
    coreutils looks at _POSIX2_VERSION and enables broken obsolete
    behaviour if not set.
    
    Fixes https://github.com/termux/termux-app/issues/232.

--- packages/coreutils/build.sh
+++ packages/coreutils/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/coreutils/
 TERMUX_PKG_DESCRIPTION="Basic file, shell and text manipulation utilities from the GNU project"
 TERMUX_PKG_VERSION=8.26
+TERMUX_PKG_BUILD_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/coreutils/coreutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=155e94d748f8e2bc327c66e0cbebdb8d6ab265d2f37c3c928f7bf6c3beba9a8e
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_mkfifo=yes gl_cv_host_operating_system=Android --without-gmp --enable-single-binary=symlinks ac_cv_func_endpwent=no"
---
+++ packages/coreutils/tests/tails-c-flag.sh
@@ -0,0 +1,7 @@
+set -e -u
+
+RESULT=$(echo -n 123456 | tail -c 3)
+if [ "$RESULT" != 456 ]; then
+	echo "Test failed - expectd 456, got $RESULT"
+	exit 1
+fi
