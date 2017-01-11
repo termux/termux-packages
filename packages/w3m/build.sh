1094cd450b56bf88b7725123498bd5fd2ab1fb66
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-11 11:53:33 -0500

    w3m: Switch to maintained debian fork

--- packages/w3m/build.sh
+++ packages/w3m/build.sh
@@ -1,8 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://w3m.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Text based Web browser and pager"
-TERMUX_PKG_VERSION=0.5.3
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/w3m/w3m/w3m-$TERMUX_PKG_VERSION/w3m-${TERMUX_PKG_VERSION}.tar.gz
+_MAJOR_VERSION=0.5.3
+_MINOR_VERSION=20170102
+TERMUX_PKG_VERSION=0.5.3.$_MINOR_VERSION
+# The upstream w3m project is dead, but every linux distribution uses
+# this maintained fork in debian:
+TERMUX_PKG_SRCURL=https://github.com/tats/w3m/archive/v${_MAJOR_VERSION}+git${_MINOR_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7eb8cadfa39daa6e8ecf8244847991f614fdc5319e601f58fc02956bc05b5ab1
+TERMUX_PKG_FOLDERNAME=w3m-${_MAJOR_VERSION}-git${_MINOR_VERSION}
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="libgc, ncurses, openssl"
 # ac_cv_func_bcopy=yes to avoid w3m defining it's own bcopy function, which
