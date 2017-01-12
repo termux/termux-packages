0cbf1a57348e7f143698b11ee5f7bf0e7bde4ef4
Joey Hess <joeyh@joeyh.name>
2017-01-12 15:11:51 +1100

    add myrepos

---
+++ packages/myrepos/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://myrepos.branchable.com/
+TERMUX_PKG_DESCRIPTION="tool to manage all your version control repos"
+TERMUX_PKG_VERSION=1.20160123
+TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/m/myrepos/myrepos_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="make, perl"
+TERMUX_PKG_BUILD_IN_SRC=yes
