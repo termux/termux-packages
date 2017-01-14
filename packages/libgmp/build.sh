710af709670af016280da69135ce9adeec235a84
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-14 03:41:59 -0500

    libgmp: Update from 6.1.1 to 6.1.2

--- packages/libgmp/build.sh
+++ packages/libgmp/build.sh
@@ -1,9 +1,5 @@
-TERMUX_PKG_VERSION=6.1.1
 TERMUX_PKG_HOMEPAGE=https://gmplib.org/
-TERMUX_PKG_DESCRIPTION="Library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers"
+TERMUX_PKG_DESCRIPTION="Library for arbitrary precision arithmetic"
+TERMUX_PKG_VERSION=6.1.2
 TERMUX_PKG_SRCURL=https://gmplib.org/download/gmp/gmp-${TERMUX_PKG_VERSION}.tar.lz
-
-termux_step_pre_configure() {
-	# https://gmplib.org/list-archives/gmp-bugs/2012-April/002620.html
-	CFLAGS+=" $LDFLAGS"
-}
+TERMUX_PKG_SHA256=12fed0532d440d2dc902e64f016aa89a33af6044b90bd1f7bca7396635105dbb
