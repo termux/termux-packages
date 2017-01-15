0eb726c2dfe9f1bfee3cb65961895945ec742e42
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-15 19:39:19 +0100

    sed: Note that it doesn't build with clang

--- packages/sed/build.sh
+++ packages/sed/build.sh
@@ -4,6 +4,8 @@ TERMUX_PKG_VERSION=4.3
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/sed/sed-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=47c20d8841ce9e7b6ef8037768aac44bc2937fff1c265b291c824004d56bd0aa
 TERMUX_PKG_BUILD_IN_SRC=yes
+# XXX: A build with clang causes undefined references to __muloti4:
+TERMUX_PKG_CLANG=no
 
 termux_step_post_configure () {
 	$TERMUX_TOUCH -d "next hour" $TERMUX_PKG_SRCDIR/doc/sed.1
