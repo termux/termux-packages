d33a847d03c234f2b8ad83273f21f1a4d99e065c
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-18 01:59:40 +0100

    datamash: Do not depend on libandroid-support
    Depending on libandroid-support is not necessary for datamash,
    and removing it fixes building with clang.

--- packages/datamash/build.sh
+++ packages/datamash/build.sh
@@ -1,6 +1,6 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/datamash/
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/datamash/
 TERMUX_PKG_DESCRIPTION="GNU datamash performs basic numeric,textual and statistical operations"
 TERMUX_PKG_VERSION=1.1.0
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/datamash/datamash-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libandroid-support"
+TERMUX_PKG_SHA256=a9e5acc86af4dd64c7ac7f6554718b40271aa67f7ff6e9819bdd919a25904bb0
 TERMUX_PKG_BUILD_IN_SRC=yes
