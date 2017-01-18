8ceb11939be44947b46e9988eb27fa81dd61f9c6
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-18 01:45:10 +0100

    luarocks: Add patch and mark platform-independent

--- packages/luarocks/build.sh
+++ packages/luarocks/build.sh
@@ -2,8 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://luarocks.org/
 TERMUX_PKG_DESCRIPTION="Deployment and management system for Lua modules"
 TERMUX_PKG_VERSION=2.4.2
 TERMUX_PKG_SRCURL=https://luarocks.org/releases/luarocks-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0e1ec34583e1b265e0fbafb64c8bd348705ad403fe85967fd05d3a659f74d2e5
 TERMUX_PKG_DEPENDS="curl, lua"
 TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_configure () {
 	./configure --prefix=$TERMUX_PREFIX \
