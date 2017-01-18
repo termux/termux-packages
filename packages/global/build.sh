14d4c6be80ef7a7dc22ceddb9c022ce6f2db93b8
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 00:32:39 +0100

    global: Update from 6.5.5 to 6.5.6

--- packages/global/build.sh
+++ packages/global/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE="http://www.gnu.org/software/global/global.html"
-TERMUX_PKG_DESCRIPTION="GNU global source code tag system that works the same way across diverse environments"
-TERMUX_PKG_VERSION=6.5.5
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/global/
+TERMUX_PKG_DESCRIPTION="Source code search and browse tools"
+TERMUX_PKG_VERSION=6.5.6
 TERMUX_PKG_SRCURL=http://tamacom.com/global/global-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=122f9afa69a8daa0f64c12db7f02981fe573f51a163fa3829ed4f832cd281505
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-posix-sort=$TERMUX_PREFIX/bin/sort"
 # coreutils provides the posix sort executable:
-TERMUX_PKG_DEPENDS="coreutils, libtool, ncurses, libltdl"
+TERMUX_PKG_DEPENDS="coreutils, ncurses, libltdl"
