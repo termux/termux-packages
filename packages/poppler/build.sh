0e180bd6c9ad756ae256c62c295ec6e913f3b0ca
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 04:25:42 +0100

    poppler: Update from 0.47 to 0.51

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,7 +1,7 @@
-TERMUX_PKG_HOMEPAGE=http://poppler.freedesktop.org/
+TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
-TERMUX_PKG_VERSION=0.47.0
-TERMUX_PKG_SRCURL=http://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION=0.51.0
+TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
 TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg"
 #texlive needs the xpdf headers
