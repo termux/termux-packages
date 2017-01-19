c142337984c8cd72615883e16ba1ab15cc61f0ee
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 04:29:59 +0100

    libpng: Update from 1.6.27 to 1.6.28

--- packages/libpng/build.sh
+++ packages/libpng/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.libpng.org/pub/png/libpng.html
 TERMUX_PKG_DESCRIPTION="Official PNG reference library"
-TERMUX_PKG_VERSION=1.6.27
+TERMUX_PKG_VERSION=1.6.28
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/libpng/libpng16/${TERMUX_PKG_VERSION}/libpng-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=fca2ffd97336356cdab9bfa8936b9d6dfd580a70205e5dfead3ac42cb054b57b
+TERMUX_PKG_SHA256=d8d3ec9de6b5db740fefac702c37ffcf96ae46cb17c18c1544635a3852f78f7a
 TERMUX_PKG_RM_AFTER_INSTALL="bin/libpng-config bin/libpng16-config bin/png-fix-itxt bin/pngfix"
