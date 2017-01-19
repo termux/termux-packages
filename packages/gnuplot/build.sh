4e7803b98618e1bbf49f2f9e5651dc45b5aa6341
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 21:17:39 +0100

    gnuplot: Bump revision after libgd update

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -1,6 +1,7 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnuplot.info
+TERMUX_PKG_HOMEPAGE=http://gnuplot.info/
 TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
 TERMUX_PKG_VERSION=5.0.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-x --with-lua=no --with-bitmap-terminals"
 TERMUX_PKG_DEPENDS="libandroid-support, readline, pango, libgd"
