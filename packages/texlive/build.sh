290d898df56eab14adb6f40404dc59468dd5ab8f
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 06:01:54 +0100

    texlive: Some work in progress

--- packages/texlive/build.sh
+++ packages/texlive/build.sh
@@ -3,53 +3,63 @@ TERMUX_PKG_DESCRIPTION="TeX Live is a distribution of the TeX typesetting system
 TERMUX_PKG_VERSION=20160523
 TERMUX_PKG_SRCURL=ftp://tug.org/historic/systems/texlive/${TERMUX_PKG_VERSION:0:4}/texlive-${TERMUX_PKG_VERSION}b-source.tar.xz
 TERMUX_PKG_SHA256="a8b32ca47f0a403661a09e202f4567a995beb718c18d8f81ca6d76daa1da21ed"
-TERMUX_PKG_DEPENDS="libpng,libgd,freetype,poppler,liblua,libicu"
+TERMUX_PKG_DEPENDS="freetype, libpng, libgd, libgmp, libmpfr, libicu, liblua, poppler"
 TERMUX_PKG_FOLDERNAME=texlive-${TERMUX_PKG_VERSION}-source
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="\
 AR=ar \
 RANLIB=ranlib \
 BUILDAR=ar \
 BUILDRANLIB=ranlib \
---prefix=$TERMUX_PREFIX/local/texlive/${TERMUX_PKG_VERSION:0:4} \
---bindir=$TERMUX_PREFIX/local/texlive/${TERMUX_PKG_VERSION:0:4}/bin/pkg \
---libdir=$TERMUX_PREFIX/lib \
+--prefix=$TERMUX_PREFIX \
 --build=$TERMUX_BUILD_TUPLE \
+--enable-ttfdump=no \
+--enable-makeindexk=no \
+--enable-makejvf=no \
+--enable-mendexk=no \
+--enable-musixtnt=no \
+--enable-ps2pk=no \
+--enable-seetexk=no \
+--enable-gregorio=no \
 --disable-native-texlive-build \
---without-x \
---disable-multiplatform \
+--disable-bibtexu \
+--disable-dvisvgm \
 --disable-dialog \
 --disable-psutils \
+--disable-multiplatform \
 --disable-t1utils \
---disable-bibtexu \
---disable-xz  \
+--disable-luatex \
+--disable-luajittex \
+--disable-mflua \
+--disable-mfluajit \
+--disable-xz \
 --without-texinfo \
 --without-xdvipdfmx \
 --without-texi2html \
---with-system-icu \
---with-system-poppler \
+--with-system-cairo \
+--with-system-graphite2 \
+--with-system-harfbuzz \
 --with-system-gd \
---with-system-luajit \
+--with-system-gmp \
+--with-system-icu \
+--with-system-lua \
+--with-system-mpfr \
+--with-system-poppler \
+--with-system-zlib \
 --with-system-xpdf \
---enable-luatex \
---enable-ttfdump=no \
---enable-makeindexk=no --enable-makejvf=no --enable-mendexk=no --enable-musixtnt=no --enable-ps2pk=no \
---enable-seetexk=no --enable-gregorio=no \
---with-banner-add=/termux \
-     "
+--without-x \
+--with-banner-add=/Termux"
 
-termux_step_post_extract_package () {        
- rm -r $TERMUX_PKG_SRCDIR/utils/pmx
+termux_step_post_extract_package () {
+	rm -r $TERMUX_PKG_SRCDIR/utils/pmx
 }
 
 termux_step_post_make_install () {
- cp $TERMUX_PKG_BUILDER_DIR/termux-install-tl $TERMUX_PREFIX/bin
- chmod +x $TERMUX_PREFIX/bin/termux-install-tl
- sed -E -i "s@/bin/sh@$PREFIX/bin/sh@" "$TERMUX_PREFIX/local/texlive/${TERMUX_PKG_VERSION:0:4}/bin/pkg/tlmgr"
+	cp $TERMUX_PKG_BUILDER_DIR/termux-install-tl $TERMUX_PREFIX/bin
 }
 
 termux_step_create_debscripts () {
- echo 'echo "retrieving texlive..."' > postinst
- echo "termux-install-tl" >> postinst	
- echo "exit 0" >> postinst	
- chmod 0755 postinst
+	echo 'echo "retrieving texlive..."' > postinst
+	echo "termux-install-tl" >> postinst
+	echo "exit 0" >> postinst
+	chmod 0755 postinst
 }
