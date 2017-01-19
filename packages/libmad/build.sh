a282b3d33e7a4ea018bb89fe33a7f22226651ff9
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 13:28:55 +0100

    libmad: Fix arm build

--- packages/libmad/build.sh
+++ packages/libmad/build.sh
@@ -4,6 +4,13 @@ TERMUX_PKG_VERSION=0.15.1b
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.mars.org/pub/mpeg/libmad-${TERMUX_PKG_VERSION}.tar.gz
 
+termux_step_pre_configure() {
+	if [ $TERMUX_ARCH = arm ]; then
+		# See https://www.mars.org/pipermail/mad-dev/2013-July/001433.html
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-fpm=default"
+	fi
+}
+
 termux_post_configure() {
 	cd $TERMUX_PKG_SRCDIR
 	sed -i -e 's/-force-mem//g' Makefile
