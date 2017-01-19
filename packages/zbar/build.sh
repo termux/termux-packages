615461b9821abfd0718bca500615ee784eddd022
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 11:17:21 +0100

    zbar: Bump revision after openjpeg update

--- packages/zbar/build.sh
+++ packages/zbar/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://zbar.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Software suite for reading bar codes from various sources"
 TERMUX_PKG_VERSION=0.10
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="imagemagick"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthread
