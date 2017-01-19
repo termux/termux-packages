750a2201c915ac849a77ad91585a90a023041a0e
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-19 04:26:33 +0100

    openjpeg: Update from 2.1.1 to 2.1.2

--- packages/openjpeg/build.sh
+++ packages/openjpeg/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.openjpeg.org/
 TERMUX_PKG_DESCRIPTION="JPEG 2000 image compression library"
-TERMUX_PKG_VERSION=2.1.1
+TERMUX_PKG_VERSION=2.1.2
 TERMUX_PKG_SRCURL=https://github.com/uclouvain/openjpeg/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=openjpeg-$TERMUX_PKG_VERSION
 TERMUX_PKG_RM_AFTER_INSTALL="lib/openjpeg-2.1/*.cmake"
