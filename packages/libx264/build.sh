75198e14fc5e9782b09fdef32582d5298d943b4b
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-23 00:18:18 +0100

    libx264: Update from 20160503 to 20170122

--- packages/libx264/build.sh
+++ packages/libx264/build.sh
@@ -1,13 +1,16 @@
-TERMUX_PKG_HOMEPAGE=http://www.videolan.org/developers/x264.html
+TERMUX_PKG_HOMEPAGE=https://www.videolan.org/developers/x264.html
 TERMUX_PKG_DESCRIPTION="Library for encoding video streams into the H.264/MPEG-4 AVC format"
-TERMUX_PKG_VERSION="20160503-2245"
-TERMUX_PKG_SRCURL=ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_VERSION=20170122
+TERMUX_PKG_SRCURL=ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-${TERMUX_PKG_VERSION}-2245-stable.tar.bz2
+TERMUX_PKG_SHA256=f833239a2ff81629242b1137c797e670516a3af7ce79e6bb7ef234a3e60fd8fd
 
 termux_step_pre_configure () {
-	if [ $TERMUX_ARCH = "i686" -o $TERMUX_ARCH = "x86_64" ]; then
-		# Avoid text relocations. Only needed on i686, see:
+	#if [ $TERMUX_ARCH = "i686" -o $TERMUX_ARCH = "x86_64" ]; then
+	if [ $TERMUX_ARCH = "i686" ]; then
+		# Avoid text relocations on i686, see:
 		# https://mailman.videolan.org/pipermail/x264-devel/2016-March/011589.html
-		# Also disable on x86_64 for now due to build errors.
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-asm"
+	elif [ $TERMUX_ARCH = "x86_64" ]; then
+		AS=yasm
 	fi
 }
