ffc287c18cc51974a7bbf9bf71c5559f1eb47a77
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-24 01:01:28 +0100

    fftw: Update from 3.3.5 to 3.3.6

--- packages/fftw/build.sh
+++ packages/fftw/build.sh
@@ -1,10 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.fftw.org/
 TERMUX_PKG_DESCRIPTION="Library for computing the Discrete Fourier Transform (DFT) in one or more dimensions"
-TERMUX_PKG_VERSION=3.3.5
-TERMUX_PKG_SRCURL=http://www.fftw.org/fftw-${TERMUX_PKG_VERSION}.tar.gz
-
-termux_step_pre_configure () {
-	# We are patching autoconf.ac.
-	cd $TERMUX_PKG_SRCDIR
-	autoreconf
-}
+TERMUX_PKG_VERSION=3.3.6
+TERMUX_PKG_SRCURL=http://www.fftw.org/fftw-${TERMUX_PKG_VERSION}-pl1.tar.gz
+TERMUX_PKG_SHA256=1ef4aa8427d9785839bc767f3eb6a84fcb5e9a37c31ed77a04e7e047519a183d
