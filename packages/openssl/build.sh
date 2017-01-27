f717f6aec29b97c826c106cd3466c6c1dd56d14f
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-27 22:42:06 +0100

    openssl: Update from 1.0.2j to 1.0.2k

--- packages/openssl/build.sh
+++ packages/openssl/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.openssl.org/
 TERMUX_PKG_DESCRIPTION="Library implementing the SSL and TLS protocols as well as general purpose cryptography functions"
 TERMUX_PKG_DEPENDS="ca-certificates"
-TERMUX_PKG_VERSION=1.0.2j
+TERMUX_PKG_VERSION=1.0.2k
 TERMUX_PKG_SRCURL=https://www.openssl.org/source/openssl-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e7aff292be21c259c6af26469c7a9b3ba26e9abaaffd325e3dccc9785256c431
+TERMUX_PKG_SHA256=6b3977c61f2aedf0f96367dcfb5c6e578cf37e7b8d913b4ecb6643c3cb88d8c0
 TERMUX_PKG_RM_AFTER_INSTALL="bin/c_rehash etc/ssl/misc"
 TERMUX_PKG_BUILD_IN_SRC=yes
 # Avoid assembly errors, see
