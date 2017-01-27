636fdba8daa840b6d01ae08d16e9f7b89f05bd38
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-27 22:58:03 +0100

    gcal: Update from 4 to 4.1

--- packages/gcal/build.sh
+++ packages/gcal/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gcal/
 TERMUX_PKG_DESCRIPTION="Program for calculating and printing calendars"
-TERMUX_PKG_VERSION=4
+TERMUX_PKG_VERSION=4.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gcal/gcal-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=91b56c40b93eee9bda27ec63e95a6316d848e3ee047b5880ed71e5e8e60f61ab
 TERMUX_PKG_DEPENDS="ncurses"
