b18f952d998dbab244b31a35339d5c57f83cbf3c
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-28 10:42:52 +0100

    libevent: Update from 2.0.22 to 2.1.8

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer implementing switching between seve
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
 TERMUX_PKG_DEPENDS="ncurses, libevent, libutil, libandroid-support"
 TERMUX_PKG_VERSION=2.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/releases/download/${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
