9fdd0c9a86d9791f78c649e30cd84811f51ea406
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-28 16:17:25 +0100

    calcurse: Update from 4.2.1 to 4.2.2

--- packages/calcurse/build.sh
+++ packages/calcurse/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://calcurse.org/
 TERMUX_PKG_DESCRIPTION="calcurse is a calendar and scheduling application for the command line"
-TERMUX_PKG_VERSION=4.2.1
+TERMUX_PKG_VERSION=4.2.2
 TERMUX_PKG_SRCURL=http://calcurse.org/files/calcurse-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9d5db3fa920e82d76e43a08c4bd3554ffdde023385b0f9c37e6f0e99d8d00598
+TERMUX_PKG_SHA256=c6881ddbd1cc7fbd02898187ac0fb4c6d8ac4c2715909b1cf00fb7a90cf08046
 TERMUX_PKG_DEPENDS="ncurses"
 
 termux_step_pre_configure() {
