4d1b187cf3f44c89ddf90101188bfde0d052440c
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-30 00:50:58 +0100

    tty-clock: Add TERMUX_PKG_HOMEPAGE and shorten description

--- packages/tty-clock/build.sh
+++ packages/tty-clock/build.sh
@@ -1,4 +1,5 @@
-TERMUX_PKG_DESCRIPTION="tty-clock is a simple ncurses-based clock that shows the time and date using a large display. It has a few commandline options to customize the output."
+TERMUX_PKG_HOMEPAGE=https://github.com/xorg62/tty-clock
+TERMUX_PKG_DESCRIPTION="Simple terminal clock"
 TERMUX_PKG_VERSION=2.3
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_SRCURL=https://github.com/xorg62/tty-clock/archive/v${TERMUX_PKG_VERSION}.tar.gz
