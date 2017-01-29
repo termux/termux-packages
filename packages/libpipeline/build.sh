fe35a2af1548842ddc65f62668a05acbaf061851
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-29 23:44:44 +0100

    libpipeline: Update from 1.4.0 to 1.4.1

--- packages/libpipeline/build.sh
+++ packages/libpipeline/build.sh
@@ -1,4 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://libpipeline.nongnu.org/
 TERMUX_PKG_DESCRIPTION="C library for manipulating pipelines of subprocesses in a flexible and convenient way"
-TERMUX_PKG_VERSION=1.4.0
+TERMUX_PKG_VERSION=1.4.1
 TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/libpipeline/libpipeline-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=da46d7b20163aadb9db2faae483f734e9096a7550c84b94029abeab62dd1b9ee
