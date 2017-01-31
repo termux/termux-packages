236d269a2b3cd3d998f7ecf41e7472f507916a29
Fredrik Fornwall <fredrik@fornwall.net>
2017-01-31 11:04:39 +0100

    hydra: Add as disabled package

---
+++ disabled-packages/hydra/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/vanhauser-thc/thc-hydra
+TERMUX_PKG_DESCRIPTION="Network logon cracker supporting different services"
+TERMUX_PKG_VERSION=8.4
+TERMUX_PKG_SRCURL=https://github.com/vanhauser-thc/thc-hydra/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b478157618e602e0a8adc412efacc1c2a5d95a8f5bfb30579fbf5997469cd8b4
+TERMUX_PKG_FOLDERNAME=thc-hydra-$TERMUX_PKG_VERSION
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="openssl"
+
+termux_step_configure() {
+	# Skip the ./configure file (which does not support cross compilation)
+	# and configure the build manually.
+	CFLAGS+=" -Dindex=strchr -DLIBOPENSSL"
+	export MANDIR=/share/man/man1
+	export LIBS="-lcrypto -lssl"
+	cat Makefile.am | sed 's/^install:.*/install: all/'  >> Makefile
+}
