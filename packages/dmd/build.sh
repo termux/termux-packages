5dbf3c7a9c89af00aff221847ec3ccb37392c9f1
Wetitpig <winsto003@hotmail.com>
2017-02-01 15:09:23 +0800

    Create DMD package
    Compiler for D

---
+++ packages/dmd/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://dlang.org
+TERMUX_PKG_DESCRIPTION="Official Compiler for D"
+TERMUX_PKG_VERSION=2.073.0
+TERMUX_PKG_YEAR=$(date +'%Y')
+TERMUX_PKG_SRCURL=http://downloads.dlang.org/releases/${TERMUX_PKG_YEAR}/dmd.${TERMUX_PKG_VERSION}.linux.tar.xz
