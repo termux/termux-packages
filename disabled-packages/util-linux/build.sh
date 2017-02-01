ef395df35c0307344e1645d995e981eacd6f7fac
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-01 23:45:58 +0100

    util-linux: Fixes, update 2.28 to 2.29.1

--- disabled-packages/util-linux/build.sh
+++ disabled-packages/util-linux/build.sh
@@ -1,11 +1,30 @@
 TERMUX_PKG_HOMEPAGE=https://en.wikipedia.org/wiki/Util-linux
 TERMUX_PKG_DESCRIPTION="Miscellaneous system utilities"
-TERMUX_PKG_VERSION=2.28
-TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/util-linux/v${TERMUX_PKG_VERSION}/util-linux-${TERMUX_PKG_VERSION}.tar.xz
-#TERMUX_PKG_DEPENDS="pcre, openssl, libuuid, libandroid-support"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-last --disable-ipcrm --disable-ipcs"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-eject --disable-switch_root --disable-pivot_root"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-logger --disable-agetty --disable-kill"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-wall"
+TERMUX_PKG_VERSION=2.29.1
+TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/util-linux/v2.29/util-linux-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=0ce40600b934ec2fecfa6bfc4efe6982d051ba96c2832b05201347aec582f54f
+TERMUX_PKG_DEPENDS="ncurses, libutil"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_func_setns=yes
+ac_cv_func_unshare=yes
+--disable-agetty
+--disable-eject
+--disable-ipcrm
+--disable-ipcs
+--disable-kill
+--disable-last
+--disable-libuuid
+--disable-logger
+--disable-pivot_root
+--disable-switch_root
+--disable-wall
+"
+# bin/column is currently bundled in the fish package:
+TERMUX_PKG_RM_AFTER_INSTALL="bin/column share/man/man1/column.1"
 
-CPPFLAGS+=" -DMAXNAMLEN=NAME_MAX"
+termux_step_pre_configure() {
+	if [ $TERMUX_ARCH_BITS = 64 ]; then
+		# prlimit() is only available in 64-bit bionic.
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_prlimit=yes"
+	fi
+}
