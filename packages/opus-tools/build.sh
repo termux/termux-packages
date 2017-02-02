6e1483c0b556ede516a283373e436d53abf2f62b
Wetitpig <winsto003@hotmail.com>
2017-02-02 14:58:56 +0800

    Update build.sh

--- packages/opus-tools/build.sh
+++ packages/opus-tools/build.sh
@@ -2,5 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://www.opus-codec.org/
 TERMUX_PKG_DESCRIPTION="Utilities to encode and decode .opus files"
 TERMUX_PKG_VERSION=0.1.9
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/opus/opus-tools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b1873dd78c7fbc98cf65d6e10cfddb5c2c03b3af93f922139a2104baedb4643a
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=b1873dd78c7fbc98cf65d6e10cfddb5c2c03b3af93f922139a2104baedb4643a
 TERMUX_PKG_DEPENDS="libopus, libogg, libflac"
