a66474ea23b615e5c8d2665b1ecdca2c404df3df
Wetitpig <winsto003@hotmail.com>
2017-02-02 15:02:12 +0800

    Update build.sh

--- packages/pick/build.sh
+++ packages/pick/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/calleerlandsson/pick
 TERMUX_PKG_DESCRIPTION="Utility to choose one option from a set of choices with fuzzy search functionality"
 TERMUX_PKG_VERSION=1.5.4
 TERMUX_PKG_SRCURL=https://github.com/calleerlandsson/pick/releases/download/v${TERMUX_PKG_VERSION}/pick-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=61de8057b1955501a8fc38227eb3ad9430bb8617480ca32c648e03c3f2c29253
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=61de8057b1955501a8fc38227eb3ad9430bb8617480ca32c648e03c3f2c29253
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_search_setupterm=-lncurses"
