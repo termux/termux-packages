ab12124753fdfc05ca17b72f021d54a55404b6ab
Wetitpig <winsto003@hotmail.com>
2017-02-02 15:13:56 +0800

    Update build.sh

--- packages/tmate/build.sh
+++ packages/tmate/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with instant terminal sharing"
 TERMUX_PKG_VERSION=2.2.1
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tmate-io/tmate/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d9c2ac59f42e65aac5f500f0548ea8056fd79c9c5285e5af324d833e2a84c305
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=d9c2ac59f42e65aac5f500f0548ea8056fd79c9c5285e5af324d833e2a84c305
 TERMUX_PKG_FOLDERNAME=tmate-${TERMUX_PKG_VERSION}
 TERMUX_PKG_DEPENDS="libandroid-support, libevent, libmsgpack, libssh, libutil, ncurses"
 
