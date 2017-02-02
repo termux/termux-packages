48ea5c292c722a096a20f6b3bd3675cb4e4da6ca
Wetitpig <winsto003@hotmail.com>
2017-02-02 15:10:45 +0800

    Update build.sh

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -2,7 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor the system calls used by a program and all the signals it receives"
 TERMUX_PKG_VERSION=4.15
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/strace/strace/${TERMUX_PKG_VERSION}/strace-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=c0cdc094d6141fd9dbf6aaad605142d651ae10998b660fda57fc61f7ad583ca9
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=c0cdc094d6141fd9dbf6aaad605142d651ae10998b660fda57fc61f7ad583ca9
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 
 termux_step_pre_configure () {
