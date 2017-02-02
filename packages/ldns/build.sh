2f6a0e5d891dd5e22e4adb682a4bec7362a93a14
Wetitpig <winsto003@hotmail.com>
2017-02-02 13:51:02 +0800

    Update build.sh

--- packages/ldns/build.sh
+++ packages/ldns/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Library for simplifying DNS programming and supporting r
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_VERSION=1.7.0
 TERMUX_PKG_SRCURL=http://www.nlnetlabs.nl/downloads/ldns/ldns-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c19f5b1b4fb374cfe34f4845ea11b1e0551ddc67803bd6ddd5d2a20f0997a6cc
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=c19f5b1b4fb374cfe34f4845ea11b1e0551ddc67803bd6ddd5d2a20f0997a6cc
 # --disable-dane-verify needed until openssl 1.1.0:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-ssl=$TERMUX_PREFIX
