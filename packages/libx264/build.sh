dfc8a5a247d0d2aecadeffa5c6146eaeda0c8835
Wetitpig <winsto003@hotmail.com>
2017-02-02 14:36:33 +0800

    Update build.sh

--- packages/libx264/build.sh
+++ packages/libx264/build.sh
@@ -2,7 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.videolan.org/developers/x264.html
 TERMUX_PKG_DESCRIPTION="Library for encoding video streams into the H.264/MPEG-4 AVC format"
 TERMUX_PKG_VERSION=20170122
 TERMUX_PKG_SRCURL=ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-${TERMUX_PKG_VERSION}-2245-stable.tar.bz2
-TERMUX_PKG_SHA256=f833239a2ff81629242b1137c797e670516a3af7ce79e6bb7ef234a3e60fd8fd
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=f833239a2ff81629242b1137c797e670516a3af7ce79e6bb7ef234a3e60fd8fd
 # Avoid linking against ffmpeg libraries to avoid circular dependency:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-swscale
