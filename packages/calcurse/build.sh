30cdb1cb2e26d12a8d34c4c637eefc3124cc8689
Wetitpig <winsto003@hotmail.com>
2017-02-02 12:39:09 +0800

    Sync with build-package.sh

--- packages/calcurse/build.sh
+++ packages/calcurse/build.sh
@@ -2,7 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://calcurse.org/
 TERMUX_PKG_DESCRIPTION="calcurse is a calendar and scheduling application for the command line"
 TERMUX_PKG_VERSION=4.2.2
 TERMUX_PKG_SRCURL=http://calcurse.org/files/calcurse-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c6881ddbd1cc7fbd02898187ac0fb4c6d8ac4c2715909b1cf00fb7a90cf08046
+TERMUX_PKG_CHECKTYPE=SHA256
+TERMUX_PKG_CHECKSUM=c6881ddbd1cc7fbd02898187ac0fb4c6d8ac4c2715909b1cf00fb7a90cf08046
 TERMUX_PKG_DEPENDS="ncurses"
 
 termux_step_pre_configure() {
