e2d8172bc6aa7356cb1d838dcf5a6afa3c346821
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-05 23:49:31 +0100

    Tweak some descriptions

--- packages/ne/build.sh
+++ packages/ne/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://ne.di.unimi.it/
-TERMUX_PKG_DESCRIPTION="ne is a free (GPL'd) text editor based on the POSIX standard"
+TERMUX_PKG_DESCRIPTION="Easy-to-use and powerful text editor"
 TERMUX_PKG_MAINTAINER="David Martínez @vaites"
 TERMUX_PKG_VERSION=3.0.1
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/ne-${TERMUX_PKG_VERSION}.tar.xz
--- packages/sed/build.sh
+++ packages/sed/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/sed/
-TERMUX_PKG_DESCRIPTION="GNU stream text editor"
+TERMUX_PKG_DESCRIPTION="GNU stream editor for filtering/transforming text"
 TERMUX_PKG_VERSION=4.4
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/sed/sed-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=cbd6ebc5aaf080ed60d0162d7f6aeae58211a1ee9ba9bb25623daa6cd942683b
