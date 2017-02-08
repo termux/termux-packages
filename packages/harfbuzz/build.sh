f2391961f7e89b4d4aca9d09be69e09ec28bb731
olg <olg@arbeitstierchen.fritz.box>
2017-02-08 07:46:42 +0100

    harfbuzz: enable libicu and libgraphite support

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,7 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_REVISION="1"
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=85a27fab639a1d651737dcb6b69e4101e3fd09522fdfdcb793df810b5cb315bd
-TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=no --disable-gtk-doc-html"
+TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libicu,libgraphite"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=yes --with-graphite2=yes --disable-gtk-doc-html"
