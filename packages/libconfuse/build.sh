aeb1a5ea4b7bed13db1079c753488873105763de
David Martínez <vaites@users.noreply.github.com>
2017-02-09 09:28:35 +0100

    bmon: added package and its dependencies (libconfuse and libnl3) (#758)
    * bmon: added package and its dependencies (libconfuse and libnl3)
    
    * libnl3: fixed homepage, source url and bumped revision

---
+++ packages/libconfuse/build.sh
@@ -0,0 +1,4 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/martinh/libconfuse
+TERMUX_PKG_DESCRIPTION="Small configuration file parser library for C"
+TERMUX_PKG_VERSION=3.0
+TERMUX_PKG_SRCURL=https://github.com/martinh/libconfuse/releases/download/v$TERMUX_PKG_VERSION/confuse-$TERMUX_PKG_VERSION.tar.gz
