ad4b7eac074ba22cd4a35378c3ab71ae1ded2a13
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-09 10:26:49 +0100

    libnl3: Rename to libnl

--- packages/bmon/build.sh
+++ packages/bmon/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_VERSION=4.0
 TERMUX_PKG_SRCURL=https://github.com/tgraf/bmon/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d5e503ff6b116c681ebf4d10e238604dde836dceb9c0008eb92416a96c87ca40
 TERMUX_PKG_FOLDERNAME=bmon-$TERMUX_PKG_VERSION
-TERMUX_PKG_DEPENDS="libconfuse, libnl3, ncurses"
+TERMUX_PKG_DEPENDS="libconfuse, libnl, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
