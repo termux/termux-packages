98d2be4cab19f8c32dc9898c28b030236421f9e5
David Martinez <contacto@davidmartinez.net>
2017-02-09 09:16:39 +0100

    libnl3: fixed homepage, source url and bumped revision

--- packages/libnl3/build.sh
+++ packages/libnl3/build.sh
@@ -1,5 +1,6 @@
-TERMUX_PKG_HOMEPAGE=https://www.infradead.org/~tgr/libnl/
+TERMUX_PKG_HOMEPAGE=https://github.com/thom311/libnl
 TERMUX_PKG_DESCRIPTION="Collection of libraries providing APIs to netlink protocol based Linux kernel interfaces"
-TERMUX_PKG_VERSION=3.2.25
-TERMUX_PKG_SRCURL=https://www.infradead.org/~tgr/libnl/files/libnl-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_VERSION=3.2.29
+TERMUX_PKG_VERSION_DASH=`echo $TERMUX_PKG_VERSION | sed -e 's/\./_/g'`
+TERMUX_PKG_SRCURL=https://github.com/thom311/libnl/releases/download/libnl$TERMUX_PKG_VERSION_DASH/libnl-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthreads --disable-cli"
