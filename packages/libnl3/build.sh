65fece08137b11de794d999d044b87f2c9892014
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-09 09:34:09 +0100

    Use source checksums for new packages

--- packages/libnl3/build.sh
+++ packages/libnl3/build.sh
@@ -3,4 +3,5 @@ TERMUX_PKG_DESCRIPTION="Collection of libraries providing APIs to netlink protoc
 TERMUX_PKG_VERSION=3.2.29
 TERMUX_PKG_VERSION_DASH=`echo $TERMUX_PKG_VERSION | sed -e 's/\./_/g'`
 TERMUX_PKG_SRCURL=https://github.com/thom311/libnl/releases/download/libnl$TERMUX_PKG_VERSION_DASH/libnl-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=0beb593dc6abfffa18a5c787b27884979c1b7e7f1fd468c801e3cc938a685922
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthreads --disable-cli"
