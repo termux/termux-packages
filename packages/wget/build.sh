cfa043f9b4ae4d1c16b03b567ec806054acbe7d5
Vishal Biswas <vshlbiswas@ymail.com>
2017-02-13 01:31:54 +0530

    update packages (#766)
    * file: update from 5.29 to 5.30
    
    * ffmpeg: update from 3.2.2 to 3.2.4
    
    * mpv: update from 0.23.0 to 0.24.0
    
    also prevent opportunistic linking with x11
    
    * grep: update from 2.28 to 3.0
    
    * fish: update from 2.4.0 to 2.5.0
    
    add tarball checksum
    remove patches no longer needed
    see https://github.com/fish-shell/fish-shell/pull/3585
    
    * wget: update from 1.19 to 1.19.1

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
 TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
-TERMUX_PKG_VERSION=1.19
+TERMUX_PKG_VERSION=1.19.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=0f1157bbf4daae19f3e1ddb70c6ccb2067feb834a6aa23c9d9daa7f048606384
+TERMUX_PKG_SHA256=0c950b9671881222a4d385b013c9604e98a8025d1988529dfca0e93617744cd2
 TERMUX_PKG_DEPENDS="pcre, openssl, libuuid, libandroid-support"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl --disable-iri"
