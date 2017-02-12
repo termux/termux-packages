23289d81e99de8bee64e2296ea0b87be39f80fc2
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-12 22:15:13 +0100

    Add some more source checksums

--- packages/patch/build.sh
+++ packages/patch/build.sh
@@ -2,3 +2,4 @@ TERMUX_PKG_HOMEPAGE=http://savannah.gnu.org/projects/patch/
 TERMUX_PKG_DESCRIPTION="GNU patch which applies diff files to create patched files"
 TERMUX_PKG_VERSION=2.7.5
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/patch/patch-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=fd95153655d6b95567e623843a0e77b81612d502ecf78a489a4aed7867caa299
