59657960e1b916f7ddb2a738c6984fc2eaf874c9
David Martinez <contacto@davidmartinez.net>
2017-02-17 00:18:39 +0100

    icecast: depend on mime-support

--- packages/icecast/build.sh
+++ packages/icecast/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://icecast.org
 TERMUX_PKG_DESCRIPTION="Icecast is a streaming media (audio/video) server"
 TERMUX_PKG_VERSION=2.4.3
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/icecast/icecast-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_DEPENDS="libcurl, libgnutls, libogg, libvorbis, libxml2, libxslt, openssl"
+TERMUX_PKG_DEPENDS="libcurl, libgnutls, libogg, libvorbis, libxml2, libxslt, mime-support, openssl"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
