db68d20c96ffae4aa6db34bfa600e171ef7c5ba9
Henrik Grimler <Henrik@Grimler.se>
2017-02-16 21:29:32 +0100

    Splitted TERMUX_PKG_VERSION into a _MAJOR_VERSION and a _MINOR_VERSION part.
    This removed the a hardcoded minor version letter ("b") from SRCURL. Had to
    use _MAJOR_VERSION in PKG_FOLDERNAME though.

--- packages/texlive/build.sh
+++ packages/texlive/build.sh
@@ -1,10 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://www.tug.org/texlive/
 TERMUX_PKG_DESCRIPTION="TeX Live is a distribution of the TeX typesetting system."
-TERMUX_PKG_VERSION=20160523
-TERMUX_PKG_SRCURL=ftp://tug.org/historic/systems/texlive/${TERMUX_PKG_VERSION:0:4}/texlive-${TERMUX_PKG_VERSION}b-source.tar.xz
+_MAJOR_VERSION=20160523
+_MINOR_VERSION=b
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}${_MINOR_VERSION}
+TERMUX_PKG_SRCURL=ftp://tug.org/historic/systems/texlive/${TERMUX_PKG_VERSION:0:4}/texlive-${TERMUX_PKG_VERSION}-source.tar.xz
 TERMUX_PKG_SHA256="a8b32ca47f0a403661a09e202f4567a995beb718c18d8f81ca6d76daa1da21ed"
 TERMUX_PKG_DEPENDS="freetype, libpng, libgd, libgmp, libmpfr, libicu, liblua, poppler, libgraphite, harfbuzz, perl, xz-utils"
-TERMUX_PKG_FOLDERNAME=texlive-${TERMUX_PKG_VERSION}-source
+TERMUX_PKG_FOLDERNAME=texlive-${_MAJOR_VERSION}-source
 
 # change the bin directory to "$TERMUX_PREFIX/local/texlive/2016/bin/pkg" because the installer will sinlink this to the actual bin dir..
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
