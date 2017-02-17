84ba008445bd1166df74ee7e869454f1cf8d729f
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-17 11:51:59 +0100

    netcat: Make -c work (fix bin/sh, closes #777)

--- packages/nmap/build.sh
+++ packages/nmap/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://nmap.org/
 TERMUX_PKG_DESCRIPTION="Utility for network discovery and security auditing"
 TERMUX_PKG_VERSION=7.40
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nmap.org/dist/nmap-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=9e14665fffd054554d129d62c13ad95a7b5c7a046daa2290501909e65f4d3188
 # Depend on netcat so that it gets installed automatically when installing
