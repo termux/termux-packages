3c3acdb11e8fa0f32f223406569ae504cc9fa615
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-20 04:34:39 +0100

    vim: Update to latest patch

--- packages/vim-python/build.sh
+++ packages/vim-python/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_HOMEPAGE=http://www.vim.org/
 TERMUX_PKG_DEPENDS="ncurses, vim-runtime, python"
 
 # Vim 8.0 patches described at ftp://ftp.vim.org/pub/vim/patches/8.0/README
-TERMUX_PKG_VERSION=8.0.0314
+TERMUX_PKG_VERSION=8.0.0340
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=b9f6dbd5cc626d2c9cf34873915448503e832bbc564f3f4d4e1bf072992f3edc
+TERMUX_PKG_SHA256=5d03b277504840d8f2e00307a0821357c70709e6dd5b0c44dd69a8dabf169e9e
 TERMUX_PKG_FOLDERNAME=vim-${TERMUX_PKG_VERSION}
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="vim_cv_toupper_broken=no vim_cv_terminfo=yes vim_cv_tty_group=world"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" vim_cv_getcwd_broken=no vim_cv_stat_ignores_slash=no vim_cv_memmove_handles_overlap=yes"
