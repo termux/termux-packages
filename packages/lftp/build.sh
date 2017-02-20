f3b094fe32f423994d0a48f0cf5ee04db4e37374
Vishal Biswas <vshlbiswas@ymail.com>
2017-02-20 09:32:33 +0530

    Another round of minor updates (#781)
    * lftp: update from 4.7.5 to 4.7.6
    
    * strace: update from 4.15 to 4.16
    
    * glib: update from 2.50.2 to 2.50.3
    
    * getmail: update from 4.53.0 to 4.54.0
    
    * poppler: update from 0.51.0 to 0.52.0

--- packages/lftp/build.sh
+++ packages/lftp/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://lftp.tech/
 TERMUX_PKG_DESCRIPTION="FTP/HTTP client and file transfer program"
-TERMUX_PKG_VERSION=4.7.5
+TERMUX_PKG_VERSION=4.7.6
 TERMUX_PKG_SRCURL=https://lftp.tech/ftp/lftp-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=53b5c5eba2e38b418bcf451776f2df596050dff4db90ab4ea73d872f8b1fd0d8
+TERMUX_PKG_SHA256=6e26254fdc7e1b92ceb31116a2c7bdcd891a07c72a7823144aaaf147a1841b7b
 # (1) Android has dn_expand, but lftp assumes that dn_skipname then exists, which it does not on android.
 # (2) Use --with-openssl to use openssl instead of gnutls.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_dn_expand=no --with-openssl --with-expat=$TERMUX_PREFIX"
