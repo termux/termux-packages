2b965d842e9893c0c464b99c8a75d7eb6aeb6235
its-pointless <bored@goat.si>
2017-02-25 08:38:26 +1100

    Adds gettext software

---
+++ packages/gettext/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gettext/
+TERMUX_PKG_DESCRIPTION="multi-lingual Stuff *need better description"
+TERMUX_PKG_VERSION=0.19.8
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/gettext/gettext-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=9c1781328238caa1685d7bc7a2e1dcf1c6c134e86b42ed554066734b621bd12f
+TERMUX_PKG_BUILD_IN_SRC=yes
+termux_step_pre_configure () {
+	autoreconf
+}
