067571ecf4a2882d1716cbf6eec0a2f299b79662
its-pointless <bored@goat.si>
2017-02-25 10:19:20 +1100

    debians description

--- packages/gettext/build.sh
+++ packages/gettext/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gettext/
-TERMUX_PKG_DESCRIPTION="multi-lingual Stuff *need better description"
+TERMUX_PKG_DESCRIPTION="GNU Internationalization utilities"
 TERMUX_PKG_VERSION=0.19.8
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/gettext/gettext-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=9c1781328238caa1685d7bc7a2e1dcf1c6c134e86b42ed554066734b621bd12f
