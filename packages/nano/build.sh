ae394a2341f3903308461478fc24543f558fa553
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-25 02:15:59 +0100

    nano: Update from 2.7.4 to 2.7.5

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.nano-editor.org/
 TERMUX_PKG_DESCRIPTION="Small, free and friendly text editor"
 _MAJOR_VERSION=2.7
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
 TERMUX_PKG_SRCURL=https://www.nano-editor.org/dist/v${_MAJOR_VERSION}/nano-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=23ffc2de52d687739fed6dc2fc94df36aa7da7bb52c8740c523fdd7336fdbc8c
+TERMUX_PKG_SHA256=226be22f46359007aa76499623739f4a7d5fa294a2899c70b21bc09b8d81d907
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-utf8 --disable-libmagic"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/rnano share/man/man1/rnano.1 share/nano/man-html"
