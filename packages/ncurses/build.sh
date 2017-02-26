73a8b62a634591eb17bb1e3ef2723273243d2e3d
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-26 11:56:25 +0100

    ncurses: Update to latest patch version

--- packages/ncurses/build.sh
+++ packages/ncurses/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://invisible-island.net/ncurses/
 TERMUX_PKG_DESCRIPTION="Library for text-based user interfaces in a terminal-independent manner"
 _MAJOR_VERSION=6.0
-_MINOR_VERSION=20170114
+_MINOR_VERSION=20170225
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.${_MINOR_VERSION}
 TERMUX_PKG_SRCURL=ftp://invisible-island.net/ncurses/current/ncurses-${_MAJOR_VERSION}-${_MINOR_VERSION}.tgz
-TERMUX_PKG_SHA256=6861b00bf85116efdb083ce5a8387268616c2c5500b5b17c692b13975049ac97
+TERMUX_PKG_SHA256=40f3f3dce25579a34c1da449f61acf7fea913a8b3adcac61ddf78a58d6120965
 # --without-normal disables static libraries:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-overwrite --enable-const --without-cxx-binding --without-normal --without-static --with-shared --without-debug --enable-widec --enable-ext-colors --enable-ext-mouse --enable-pc-files --with-pkg-config-libdir=$PKG_CONFIG_LIBDIR --without-ada --without-tests --mandir=$TERMUX_PREFIX/share/man ac_cv_header_locale_h=no"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/ncursesw6-config share/man/man1/ncursesw6-config.1 bin/infotocap share/man/man1/infotocap.1m bin/captoinfo share/man/man1/captoinfo.1m"
