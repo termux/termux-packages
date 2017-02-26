00a42137561b25b5fa13ed25dc159e3c2fa9db7d
Fredrik Fornwall <fredrik@fornwall.net>
2017-02-26 12:33:40 +0100

    libdb: Updates from @vishalbiswas

--- packages/libdb/build.sh
+++ packages/libdb/build.sh
@@ -1,16 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://www.oracle.com/database/berkeley-db
-TERMUX_PKG_DESCRIPTION="The Berkeley Database (Berkeley DB) is a programmatic toolkit that provides embedded database support"
-TERMUX_PKG_VERSION=5.3.28
-# Oracle needs an user account to download packages, so we use a mirror
-TERMUX_PKG_SRCURL=https://download.freenas.org/distfiles/bdb/db-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DESCRIPTION="The Berkeley DB embedded database system (library)"
+TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
+TERMUX_PKG_VERSION=6.2.23
+TERMUX_PKG_SRCURL=http://download.oracle.com/berkeley-db/db-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=47612c8991aa9ac2f6be721267c8d3cdccf5ac83105df8e50809daea24e95dc7
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-hash
+--enable-smallbuild
+db_cv_atomic=gcc-builtin
+"
+TERMUX_PKG_RM_AFTER_INSTALL="docs"
 
 termux_step_pre_configure() {
-    TERMUX_PKG_BUILDDIR=$TERMUX_PKG_SRCDIR/build_unix
-    TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/dist
-}
-
-termux_step_post_massage() {
-    cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX
-    rm -rf docs
+	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/dist
 }
