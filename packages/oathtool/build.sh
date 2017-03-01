3b3b279e6f50ffe475e3aa87146101b1f56da5e0
David Martinez <contacto@davidmartinez.net>
2017-03-02 00:31:13 +0100

    oathtool: added xmlsec dependency

--- packages/oathtool/build.sh
+++ packages/oathtool/build.sh
@@ -3,4 +3,5 @@ TERMUX_PKG_DESCRIPTION="One-time password components"
 TERMUX_PKG_VERSION=2.6.2
 TERMUX_PKG_SRCURL=http://download.savannah.nongnu.org/releases/oath-toolkit/oath-toolkit-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b03446fa4b549af5ebe4d35d7aba51163442d255660558cd861ebce536824aa0
+TERMUX_PKG_DEPENDS="xmlsec"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pam"
