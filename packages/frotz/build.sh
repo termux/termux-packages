7a08b43589ee461307276e1fd4e4795bb7f5e740
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-01 22:12:43 +0100

    frotz: Depend on curl for the zgames script
    Fixes #779.

--- packages/frotz/build.sh
+++ packages/frotz/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://frotz.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Interpreter for Infocom and other Z-machine interactive fiction (IF) games"
-# frotz does not depend on dialog, but the script zgames we bundle below in termux_step_make_install() do.
-TERMUX_PKG_DEPENDS="ncurses, dialog"
+# frotz does not depend on dialog or curl, but the zgames script we bundle below in termux_step_make_install() do.
+TERMUX_PKG_DEPENDS="ncurses, dialog, curl"
 TERMUX_PKG_VERSION=2.44
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/DavidGriffith/frotz/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=frotz-${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_IN_SRC=yes
