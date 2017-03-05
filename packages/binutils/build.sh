120f5337631749bdd06ff93e9c827fd967c0de77
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-05 03:09:16 +0100

    binutils: Update from 2.27 to 2.28

--- packages/binutils/build.sh
+++ packages/binutils/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/binutils/
 TERMUX_PKG_DESCRIPTION="Collection of binary tools, the main ones being ld, the GNU linker, and as, the GNU assembler"
-TERMUX_PKG_VERSION=2.27
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.28
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/binutils/binutils-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-werror"
+TERMUX_PKG_SHA256=cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-werror --with-system-zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="tooldir=$TERMUX_PREFIX"
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/windmc.1 share/man/man1/windres.1 bin/ld.bfd"
 
