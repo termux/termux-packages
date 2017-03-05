507dd6e730d7ddd546940a9de4cdffb8b74cdf18
olg <olg@arbeitstierchen.fritz.box>
2017-03-05 15:32:53 +0100

    texlive: make it depend on harfbuzz-icu

--- packages/texlive/build.sh
+++ packages/texlive/build.sh
@@ -5,7 +5,7 @@ _MINOR_VERSION=b
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}${_MINOR_VERSION}
 TERMUX_PKG_SRCURL=ftp://tug.org/historic/systems/texlive/${TERMUX_PKG_VERSION:0:4}/texlive-${TERMUX_PKG_VERSION}-source.tar.xz
 TERMUX_PKG_SHA256="a8b32ca47f0a403661a09e202f4567a995beb718c18d8f81ca6d76daa1da21ed"
-TERMUX_PKG_DEPENDS="freetype, libpng, libgd, libgmp, libmpfr, libicu, liblua, poppler, libgraphite, harfbuzz, perl, xz-utils"
+TERMUX_PKG_DEPENDS="freetype, libpng, libgd, libgmp, libmpfr, libicu, liblua, poppler, libgraphite, harfbuzz-icu, perl, xz-utils"
 TERMUX_PKG_FOLDERNAME=texlive-${_MAJOR_VERSION}-source
 
 # change the bin directory to "$TERMUX_PREFIX/opt/texlive/2016/bin/pkg" because the installer will symlink this to the actual bin dir..
