a950838920f0a210883962e9dc801e95ddb8d242
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-05 12:09:20 +0100

    postgresql: Specify USE_UNNAMED_POSIX_SEMAPHORES=1

--- disabled-packages/postgresql/build.sh
+++ disabled-packages/postgresql/build.sh
@@ -5,7 +5,13 @@ TERMUX_PKG_VERSION=9.6.2
 TERMUX_PKG_SRCURL=https://ftp.postgresql.org/pub/source/v$TERMUX_PKG_VERSION/postgresql-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=0187b5184be1c09034e74e44761505e52357248451b0c854dddec6c231fe50c9
 TERMUX_PKG_DEPENDS="openssl, libcrypt, readline, libandroid-shmem"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --without-gssapi --with-readline --with-openssl --with-system-tzdata=/system/usr/share/zoneinfo"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+USE_UNNAMED_POSIX_SEMAPHORES=1
+--with-openssl
+--without-gssapi
+--with-readline
+--with-system-tzdata=/system/usr/share/zoneinfo
+"
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -s"
 
 termux_step_pre_configure () {
