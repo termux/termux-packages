4182e8c864dd0405ec5fde7d1eb42dd3bd8ac75a
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-07 14:31:35 +0100

    mc: Update from 4.8.18 to 4.8.19

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.midnight-commander.org/
 TERMUX_PKG_DESCRIPTION="Midnight Commander - a powerful file manager"
-TERMUX_PKG_VERSION=4.8.18
+TERMUX_PKG_VERSION=4.8.19
 TERMUX_PKG_SRCURL="http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=f7636815c987c1719c4f5de2dcd156a0e7d097b1d10e4466d2bdead343d5bece
+TERMUX_PKG_SHA256=eb9e56bbb5b2893601d100d0e0293983049b302c5ab61bfb544ad0ee2cc1f2df
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
 
