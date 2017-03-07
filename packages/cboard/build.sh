91f8996a010d254c66c11c7fdcc046794a1defc3
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-07 01:24:04 +0100

    cboard: Rebuild against latest ncurses

--- packages/cboard/build.sh
+++ packages/cboard/build.sh
@@ -1,7 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://benkibbey.wordpress.com/cboard/
 TERMUX_PKG_DESCRIPTION="PGN browser, editor and chess engine frontend"
 TERMUX_PKG_VERSION=0.7.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/c-board/${TERMUX_PKG_VERSION}/cboard-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=8caeabc5cccd35c0f093d36ec0088667530d92e178d328ef345c4c20052d25e2
 TERMUX_PKG_DEPENDS="libandroid-support,libandroid-glob,gnuchess, ncurses, ncurses-ui-libs"
 
 termux_step_pre_configure () {
