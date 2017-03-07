ae1ebed26215702e905c599375e530e59f992e16
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-07 11:05:55 +0100

    harfbuzz: Update from 1.4.3 to 1.4.4

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.4.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.4.4
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=838c17400a88a3a451eb401573ef94cdd50919730d98255547c459fef1d85321
+TERMUX_PKG_SHA256=35d2f8ca476cbbec64ee824eca6b0209ff8db0334990b9f5af893b94f119d255
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=yes --with-graphite2=yes --disable-gtk-doc-html"
