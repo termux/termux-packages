3f100e5f1139e20f3bc51e9d1e81c45f032325c6
Wetitpig <winsto003@hotmail.com>
2017-03-08 18:05:45 +0800

    Add sc package

---
+++ packages/sc/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Wetitpig/sc
+TERMUX_PKG_DESCRIPTION="Spreadsheet Calculator for linux."
+TERMUX_PKG_VERSION=7.16.1
+TERMUX_PKG_SRCURL=https://github.com/Wetitpig/sc/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libncurses5-dev"
