b047428ac300310f77304c9db5a58ce34ee9e6dc
Wetitpig <winsto003@hotmail.com>
2017-03-08 18:09:39 +0800

    Add package zlib

---
+++ packages/zlib/build.sh
@@ -0,0 +1,4 @@
+TERMUX_PKG_HOMEPAGE=http://www.zlib.net
+TERMUX_PKG_DESCRIPTION="zlib is designed to be a free, general-purpose, lossless data-compression library for use on virtually any computer hardware and operating system."
+TERMUX_PKG_VERSION=1.2.11
+TERMUX_PKG_SRCURL=http://www.zlib.net/zlib-${TERMUX_PKG_VERSION}.tar.gz
