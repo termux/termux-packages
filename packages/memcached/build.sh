cce97c34ce8d5aebaf994ce90195a695b35b5e82
David Martinez <contacto@davidmartinez.net>
2017-03-08 21:13:49 +0100

    memcached: disabled coverage and added sasl

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_VERSION=1.4.35
 TERMUX_PKG_SRCURL=http://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f4815ac95aa06c0f360052a0a12010533b2b78c3bfe475b171606c1b61469476
 TERMUX_PKG_DEPENDS="libevent, libsasl"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-docs"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-sasl --disable-coverage"
 
 termux_step_pre_configure() {
     export ac_cv_c_endian=little
