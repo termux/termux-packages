918447b39cc0c770c1aa7a5dafe3ab5ad89a03bd
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-08 02:09:31 +0100

    postgresql: Build with bundled time zone database

--- disabled-packages/postgresql/build.sh
+++ disabled-packages/postgresql/build.sh
@@ -8,18 +8,27 @@ TERMUX_PKG_DEPENDS="openssl, libcrypt, readline, libandroid-shmem"
 # - pgac_cv_prog_cc_ldflags__Wl___as_needed: Inform that the linker supports as-needed. It's
 #   not stricly necessary but avoids unnecessary linking of binaries.
 # - USE_UNNAMED_POSIX_SEMAPHORES: Avoid using System V semaphores which are disabled on Android.
-# - with-system-tzdata: Doesn't currently work as Android uses a single timezone file. But
-#   if not specified the build uses zic to build timezone files using hard links, which doesn't
-#   work on Android 6.0+. TODO: Either patch to work with the combined timezone file, or
-#   replace the hard links with symlinks.
+# - ZIC=...: The zic tool is used to build the time zone database bundled with postgresql.
+#   We specify a binary built in termux_step_host_build which has been patched to use symlinks
+#   over hard links (which are not supported as of Android 6.0+).
+#   There exists a --with-system-tzdata configure flag, but that does not work here as Android
+#   uses a custom combined tzdata file.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 pgac_cv_prog_cc_ldflags__Wl___as_needed=yes
 USE_UNNAMED_POSIX_SEMAPHORES=1
 --with-openssl
---with-system-tzdata=/system/usr/share/zoneinfo
+ZIC=$TERMUX_PKG_HOSTBUILD_DIR/src/timezone/zic
 "
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -s"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/libecpg* bin/ecpg share/man/man1/ecpg.1"
+TERMUX_PKG_HOSTBUILD=yes
+
+termux_step_host_build() {
+	# Build a native zic binary which we have patched to
+	# use symlinks instead of hard links.
+	$TERMUX_PKG_SRCDIR/configure
+	make ./src/timezone/zic
+}
 
 termux_step_post_make_install() {
 	# Man pages are not installed by default:
