b0a1ca2bc42332c0ba2ba51091569f7236d662b1
David Martinez <contacto@davidmartinez.net>
2017-03-09 01:09:09 +0100

    memcached: fixed SASL configuration path

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -5,9 +5,13 @@ TERMUX_PKG_SRCURL=http://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.t
 TERMUX_PKG_SHA256=f4815ac95aa06c0f360052a0a12010533b2b78c3bfe475b171606c1b61469476
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-sasl --disable-coverage"
+TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
     export ac_cv_c_endian=little
+
+    # fix SASL configuration path
+    perl -p -i -e "s#/etc/sasl#$TERMUX_PREFIX/etc/sasl#" $TERMUX_PKG_BUILDDIR/sasl_defs.c
 
     # getsubopt() taken from https://github.com/lxc/lxc/blob/master/src/include/getsubopt.c
     cp $TERMUX_PKG_BUILDER_DIR/getsubopt.c $TERMUX_PKG_SRCDIR
