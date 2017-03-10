9654ae303d9e93dd38e22ad45dffae278f852db2
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-10 15:20:10 +0100

    vim: Update from patch 393 to 442

--- packages/vim-python/build.sh
+++ packages/vim-python/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_HOMEPAGE=http://www.vim.org/
 TERMUX_PKG_DEPENDS="ncurses, vim-runtime, python"
 
 # Vim 8.0 patches described at ftp://ftp.vim.org/pub/vim/patches/8.0/README
-TERMUX_PKG_VERSION=8.0.0393
+TERMUX_PKG_VERSION=8.0.0442
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=9cd36327dd4e2558c42ba39d8aa4ea31554a5ef6ccd1d872a17fe3f5f52a12fa
+TERMUX_PKG_SHA256=ca54acea294aa8fb5127a1a8f252fcb9cbecb1632cdf96f842ecd15f4412294b
 TERMUX_PKG_FOLDERNAME=vim-${TERMUX_PKG_VERSION}
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="vim_cv_toupper_broken=no vim_cv_terminfo=yes vim_cv_tty_group=world"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" vim_cv_getcwd_broken=no vim_cv_stat_ignores_slash=no vim_cv_memmove_handles_overlap=yes"
