2100eed584d43141ea89f7bc67a5668e340517be
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-13 22:48:01 +0100

    gdbm: Build without readline for now

--- packages/gdbm/build.sh
+++ packages/gdbm/build.sh
@@ -3,3 +3,4 @@ TERMUX_PKG_DESCRIPTION="Library of database functions that use extensible hashin
 TERMUX_PKG_VERSION=1.13
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gdbm/gdbm-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9d252cbd7d793f7b12bcceaddda98d257c14f4d1890d851c386c37207000a253
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-readline"
