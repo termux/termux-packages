d899d7938b0fea7f8b39a2b91a245f8067f1358f
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-19 16:39:23 +0100

    libunwind: Update from 1.2-rc1 to 1.2

--- disabled-packages/libunwind/build.sh
+++ disabled-packages/libunwind/build.sh
@@ -3,5 +3,6 @@
 # causes problems due to elf types being redefined.
 TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/libunwind/
 TERMUX_PKG_DESCRIPTION="Library to determine the call-chain of a program"
-TERMUX_PKG_VERSION=1.2~rc1
-TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2-rc1.tar.gz
+TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/libunwind/libunwind-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1de38ffbdc88bd694d10081865871cd2bfbb02ad8ef9e1606aee18d65532b992
