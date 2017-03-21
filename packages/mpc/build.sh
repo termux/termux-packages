1f6d05d38b88e3168f7126001e52c0b32b3bebfc
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-22 00:21:14 +0100

    mpc: Bump package revision
    We need to rebuild the package against the updated libmpdclient
    which dropped a version suffix from the shared library file.

--- packages/mpc/build.sh
+++ packages/mpc/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_DEPENDS="libmpdclient"
 _MAIN_VERSION=0
 _PATCH_VERSION=28
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.musicpd.org/download/mpc/${_MAIN_VERSION}/mpc-${_MAIN_VERSION}.${_PATCH_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a4337d06c85dc81a638821d30fce8a137a58d13d510be34a11c1cce95cabc547
 # Include some useful scripts for editing playlists
