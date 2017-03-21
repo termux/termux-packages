476836c76e35475a03237c6974c50c771ef324dd
David Martinez <contacto@davidmartinez.net>
2017-03-21 15:21:32 +0100

    memcached: updated to 1.4.36

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
-TERMUX_PKG_VERSION=1.4.35
+TERMUX_PKG_VERSION=1.4.36
 TERMUX_PKG_SRCURL=http://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f4815ac95aa06c0f360052a0a12010533b2b78c3bfe475b171606c1b61469476
+TERMUX_PKG_SHA256=773b6bb20bf80223ca6a15d75f570fbab346ad11ec98595e5af5e33f54bd82d8
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-sasl --disable-coverage"
 TERMUX_PKG_BUILD_IN_SRC=yes
