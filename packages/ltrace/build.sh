4e7d603e9468535e335a575eb8478cdaa216b354
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-23 22:44:06 +0100

    ltrace: Build with --without-libunwind
    This fixes building when libunwind is available.

--- packages/ltrace/build.sh
+++ packages/ltrace/build.sh
@@ -2,16 +2,18 @@ TERMUX_PKG_HOMEPAGE=http://www.ltrace.org/
 TERMUX_PKG_DESCRIPTION="Tracks runtime library calls in dynamically linked programs"
 TERMUX_PKG_VERSION=0.7.3.20160411
 TERMUX_PKG_DEPENDS="elfutils"
-_COMMIT=2def9f1217374cc8371105993003b2c663aefda7
+local _COMMIT=2def9f1217374cc8371105993003b2c663aefda7
 TERMUX_PKG_SRCURL=https://github.com/dkogan/ltrace/archive/${_COMMIT}.zip
+TERMUX_PKG_SHA256=d089ae8affd8af782c12aed7ba97f44c7f55b60033ec78ab07e415e7b091a90c
 TERMUX_PKG_FOLDERNAME=ltrace-${_COMMIT}
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_host=$TERMUX_ARCH-generic-linux-gnu"
-# Needed for clang build:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-werror"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-werror
+--without-libunwind
+ac_cv_host=$TERMUX_ARCH-generic-linux-gnu
+"
 
 termux_step_pre_configure () {
-	#CFLAGS+=" -Wno-error=maybe-uninitialized"
-	# rindex is obsolete name of strrchr which is not available in Android:
+	# rindex is an obsolete version of strrchr which is not available in Android:
 	CFLAGS+=" -Drindex=strrchr"
 
 	autoreconf -i ../src
