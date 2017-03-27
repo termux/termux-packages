4db4da2c5789237290a532daac3fcc6a0ac644e6
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-27 03:29:43 +0200

    Use more source checksums

--- packages/abook/build.sh
+++ packages/abook/build.sh
@@ -3,5 +3,6 @@ TERMUX_PKG_DESCRIPTION="Abook is a text-based addressbook program designed to us
 TERMUX_PKG_VERSION=0.6.0pre2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://abook.sourceforge.net/devel/abook-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=59d444504109dd96816e003b3023175981ae179af479349c34fa70bc12f6d385
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
 _MAJOR_VERSION=3.7
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${_MAJOR_VERSION}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dc1246c4e6d168ea4d6e042cfba577c1acd65feea27e56f5ff37df920c30cae0
 TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv"
