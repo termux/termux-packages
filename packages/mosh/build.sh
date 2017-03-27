a847ab5da265340f81f26f46a4e9523cc25e55c0
Fredrik Fornwall <fredrik@fornwall.net>
2017-03-27 05:43:18 +0200

    mosh: Update from 1.3.0-rc2 to 1.3.0

--- packages/mosh/build.sh
+++ packages/mosh/build.sh
@@ -1,16 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://mosh.org
 TERMUX_PKG_DESCRIPTION="Mobile shell that supports roaming and intelligent local echo"
-TERMUX_PKG_VERSION=1.3.0~rc2
-TERMUX_PKG_SRCURL=https://github.com/mobile-shell/mosh/releases/download/mosh-1.3.0-rc2/mosh-1.3.0-rc2.tar.gz
-TERMUX_PKG_SHA256=8b6bff33c469ccea0438877c68774a6b2ded6fccd99b1db180222da82f0654ae
-TERMUX_PKG_FOLDERNAME=mosh-1.3.0-rc2
+TERMUX_PKG_VERSION=1.3.0
+TERMUX_PKG_SRCURL=https://github.com/mobile-shell/mosh/releases/download/mosh-${TERMUX_PKG_VERSION}/mosh-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=320e12f461e55d71566597976bd9440ba6c5265fa68fbf614c6f1c8401f93376
+TERMUX_PKG_FOLDERNAME=mosh-$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="libandroid-support, libprotobuf, ncurses, openssl, openssh, libutil"
 
 termux_step_pre_configure () {
 	export PROTOC=$TERMUX_TOPDIR/libprotobuf/host-build/src/protoc
-
-	cd $TERMUX_PKG_SRCDIR
-	./autogen.sh
 }
 
 termux_step_post_make_install () {
