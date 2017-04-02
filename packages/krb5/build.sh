c50b57f73e77523e83b0f7feaef8e4e0d84e7ca5
Vishal Biswas <vshlbiswas@ymail.com>
2017-04-02 20:36:58 +0530

    Krb5 (#893)
    * libdb: enable 1.85 compat api for krb5
    
    * krb5: improve package
    
    remove dependency on c-ares
    use external shared libdb
    use sample confs for defaults
    provide check_krb5 aclocal
    fix krb5-config --libs
    
    * krb5: change defaults in kdc.conf

--- packages/krb5/build.sh
+++ packages/krb5/build.sh
@@ -1,12 +1,20 @@
 TERMUX_PKG_HOMEPAGE=https://web.mit.edu/kerberos
 TERMUX_PKG_DESCRIPTION="The Kerberos network authentication system"
 TERMUX_PKG_VERSION=1.15.1
-TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, readline, c-ares, openssl, libutil"
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, readline, openssl, libutil, libdb"
 TERMUX_PKG_SRCURL="https://web.mit.edu/kerberos/dist/krb5/1.15/krb5-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=437c8831ddd5fde2a993fef425dedb48468109bb3d3261ef838295045a89eb45
 TERMUX_PKG_FOLDERNAME="krb5-$TERMUX_PKG_VERSION/src"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-readline --with-netlib=-lcares --without-system-verto"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-readline
+--without-system-verto
+--with-netlib=-lc
+--enable-dns-for-realm
+--sbindir=$TERMUX_PREFIX/bin
+--with-size-optimizations
+--with-system-db
+"
 
 termux_step_pre_configure () {
     # cannot test these when cross compiling
@@ -18,4 +26,16 @@ termux_step_pre_configure () {
     cp "$TERMUX_PKG_BUILDER_DIR/netbsd_getpass.c" "$TERMUX_PKG_SRCDIR/clients/kpasswd/"
 
     LDFLAGS="$LDFLAGS -landroid-glob -llog"
+}
+
+termux_step_post_make_install () {
+	# Sample KDC config file
+	install -dm 700 $TERMUX_PREFIX/var/krb5kdc
+	install -pm 600 $TERMUX_PKG_SRCDIR/config-files/kdc.conf $TERMUX_PREFIX/var/krb5kdc/kdc.conf
+
+	# Default configuration file
+	install -pm 600 $TERMUX_PKG_SRCDIR/config-files/krb5.conf $TERMUX_PREFIX/etc/krb5.conf
+
+	install -dm 700 $TERMUX_PREFIX/share/aclocal
+	install -m 600 $TERMUX_PKG_SRCDIR/util/ac_check_krb5.m4 $TERMUX_PREFIX/share/aclocal
 }
