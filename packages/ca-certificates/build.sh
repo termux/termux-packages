4b2eaa59bfa710daf63161c5f6ea3a9cc9518a7c
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-03 01:14:47 +0200

    ca-certificates: Make ca-certificates-java subpkg

--- packages/ca-certificates/build.sh
+++ packages/ca-certificates/build.sh
@@ -14,4 +14,22 @@ termux_step_make_install () {
 	termux_download https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt \
 		$CERTFILE \
 		$CERTFILE_SHA256
+	touch $CERTFILE
+
+	# Build java keystore which is split out into a ca-certificates-java subpackage:
+	local KEYUTIL_JAR=$TERMUX_PKG_CACHEDIR/keyutil-0.4.0.jar
+	termux_download \
+		https://github.com/use-sparingly/keyutil/releases/download/0.4.0/keyutil-0.4.0.jar \
+		$KEYUTIL_JAR \
+		18f1d2c82839d84949b1ad015343c509e81ef678c24db6112acc6c0761314610
+
+	local JAVA_KEYSTORE_DIR=$PREFIX/lib/jvm/openjdk-9/lib/security
+	mkdir -p $JAVA_KEYSTORE_DIR
+
+	java -jar $KEYUTIL_JAR \
+		--import \
+		--new-keystore $JAVA_KEYSTORE_DIR/jssecacerts \
+		--password changeit \
+		--force-new-overwrite \
+		--import-pem-file $CERTFILE
 }
