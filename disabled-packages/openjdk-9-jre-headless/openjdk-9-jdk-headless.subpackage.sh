89c0fa87f0b5b463f64ff073105eefc0e0fd530f
Vishal Biswas <vshlbiswas@ymail.com>
2017-04-03 21:04:39 +0530

    openjdk-9-jre-headless (#895)
    * openjdk-9: move to ubuntu package naming and layout convention
    
    update to latest changesets (tag 162)
    update cups headers to 2.2.3
    split into jre, jdk and source packages
    use the client jvm, renove libffi dependency
    create wrappers for binaries for LD_LIBRARY_PATH
    various fixes to get it working
    
    * openjdk-9: depend on ca-certificates-java

---
+++ disabled-packages/openjdk-9-jre-headless/openjdk-9-jdk-headless.subpackage.sh
@@ -0,0 +1,33 @@
+TERMUX_SUBPKG_INCLUDE="$_jvm_dir/bin/idlj bin/idlj
+$_jvm_dir/bin/jar bin/jar
+$_jvm_dir/bin/jarsigner bin/jarsigner
+$_jvm_dir/bin/javac bin/javac
+$_jvm_dir/bin/javadoc bin/javadoc
+$_jvm_dir/bin/javah bin/javah
+$_jvm_dir/bin/javap bin/javap
+$_jvm_dir/bin/jcmd bin/jcmd
+$_jvm_dir/bin/jdb bin/jdb
+$_jvm_dir/bin/jdeprscan bin/jdeprscan
+$_jvm_dir/bin/jdeps bin/jdeps
+$_jvm_dir/bin/jhsdb bin/jhsdb
+$_jvm_dir/bin/jimage bin/jimage
+$_jvm_dir/bin/jinfo bin/jinfo
+$_jvm_dir/bin/jlink bin/jlink
+$_jvm_dir/bin/jmap bin/jmap
+$_jvm_dir/bin/jmod bin/jmod
+$_jvm_dir/bin/jps bin/jps
+$_jvm_dir/bin/jrunscript bin/jrunscript
+$_jvm_dir/bin/jshell bin/jshell
+$_jvm_dir/bin/jstack bin/jstack
+$_jvm_dir/bin/jstat bin/jstat
+$_jvm_dir/bin/jstatd bin/jstatd
+$_jvm_dir/bin/policytool bin/policytool
+$_jvm_dir/bin/rmic bin/rmic
+$_jvm_dir/bin/schemagen bin/schemagen
+$_jvm_dir/bin/serialver bin/serialver
+$_jvm_dir/bin/wsgen bin/wsgen
+$_jvm_dir/bin/wsimport bin/wsimport
+$_jvm_dir/bin/xjc bin/xjc
+$_jvm_dir/include"
+TERMUX_SUBPKG_DEPENDS="openjdk-9-jre-headless"
+TERMUX_SUBPKG_DESCRIPTION="OpenJDK Development Kit (JDK) (headless)"
