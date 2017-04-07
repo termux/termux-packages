dd0b2c27f2682d3b489501874014b1f31bc9d809
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-07 03:26:01 +0200

    libedit: Remove file clash with readline

--- packages/libedit/build.sh
+++ packages/libedit/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_VERSION=20170329-3.1
 TERMUX_PKG_SRCURL=http://thrysoee.dk/editline/libedit-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=91f2d90fbd2a048ff6dad7131d9a39e690fd8a8fd982a353f1333dd4017dd4be
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+TERMUX_PKG_RM_AFTER_INSTALL="share/man/man7/editline.7 share/man/man3/history.3"
 
 termux_step_pre_configure() {
 	CFLAGS+=" -D__STDC_ISO_10646__=201103L -DNBBY=CHAR_BIT"
