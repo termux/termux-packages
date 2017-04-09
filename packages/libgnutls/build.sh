65c3d5a15eef994764c4b785d6e4199486fa6440
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-10 00:02:43 +0200

    gnutls: Update from 3.5.10 to 3.5.11

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn, libunistring"
 _TERMUX_PKG_MAJOR_VERSION=3.5
-TERMUX_PKG_VERSION=${_TERMUX_PKG_MAJOR_VERSION}.10
+TERMUX_PKG_VERSION=${_TERMUX_PKG_MAJOR_VERSION}.11
 TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${_TERMUX_PKG_MAJOR_VERSION}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=af443e86ba538d4d3e37c4732c00101a492fe4b56a55f4112ff0ab39dbe6579d
+TERMUX_PKG_SHA256=51765cc5579e250da77fbd7871507c517d01b15353cc40af7b67e9ec7b6fe28f
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-hardware-acceleration --disable-cxx --disable-openssl-compatibility --with-included-libtasn1 --without-p11-kit --with-default-trust-store-file=$TERMUX_PREFIX/etc/tls/cert.pem"
 
 termux_step_pre_configure() {
