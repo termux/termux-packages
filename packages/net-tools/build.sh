40355d22d83c279758fccf15e389ca2a088be1aa
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-09 22:24:24 +0200

    net-tools: Fix SBINDIR to be $PREFIX/bin

--- packages/net-tools/build.sh
+++ packages/net-tools/build.sh
@@ -5,7 +5,7 @@ _commit=479bb4a7e11a4084e2935c0a576388f92469225b
 TERMUX_PKG_SRCURL=https://sourceforge.net/code-snapshots/git/n/ne/net-tools/code.git/net-tools-code-$_commit.zip
 TERMUX_PKG_SHA256=7e9f8e8dcbabed0c8eeb976100496567abae7ac9d92c72cebd1a9d965473e943
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="BINDIR=$TERMUX_PREFIX/bin SBINDIR=$TERMUX_PREFIX HAVE_HOSTNAME_TOOLS=0"
+TERMUX_PKG_EXTRA_MAKE_ARGS="BINDIR=$TERMUX_PREFIX/bin SBINDIR=$TERMUX_PREFIX/bin HAVE_HOSTNAME_TOOLS=0"
 
 termux_step_configure () {
 	CFLAGS="$CFLAGS -D_LINUX_IN6_H -Dindex=strchr -Drindex=strrchr"
