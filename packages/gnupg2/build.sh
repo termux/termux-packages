f73cd2105d806c0756131d47ae440fc226d133a7
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-10 00:19:01 +0200

    gnupg2: Update from 2.1.19 to 2.1.20

--- packages/gnupg2/build.sh
+++ packages/gnupg2/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/
 TERMUX_PKG_DESCRIPTION="Implementation of the OpenPGP standard for encrypting and signing data and communication"
-TERMUX_PKG_VERSION=2.1.19
+TERMUX_PKG_VERSION=2.1.20
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=46cced1f5641ce29cc28250f52fadf6e417e649b3bfdec49a5a0d0b22a639bf0
+TERMUX_PKG_SHA256=24cf9a69369be64a9f6f8cc11a1be33ab7780ad77a6a1b93719438f49f69960d
 TERMUX_PKG_DEPENDS="libassuan,libbz2,libgcrypt,libksba,libnpth,readline,pinentry"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ldap --disable-sqlite"
 # Remove non-english help files and man pages shipped with the gnupg (1) package:
