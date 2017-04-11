e3cdbbfaa1820cc942413c9865b1c291b19a3adf
its-pointless <bored@goat.si>
2017-04-11 14:31:47 +1000

    add proper description...

--- packages/syncthing/build.sh
+++ packages/syncthing/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://syncthing.net/
-TERMUX_PKG_DESCRIPTION="Command-line wrapper for git that makes you better at GitHub"
+TERMUX_PKG_DESCRIPTION="decentralized file synchronization"
 TERMUX_PKG_VERSION=0.14.27-rc.2
 TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/archive/v0.14.27-rc.2.tar.gz
 TERMUX_PKG_SHA256=2243d3ddec966fef7f6563e3028c17f55916479a4e8fe5b30cd0960825f9f3b0
