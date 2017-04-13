11c3dea9a5ca80e02f3182096923d8d215ad80b3
vishalbiswas <vshlbiswas@ymail.com>
2017-04-13 10:41:54 +0530

    cmake: add librhash dependency

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -4,7 +4,7 @@ _MAJOR_VERSION=3.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${_MAJOR_VERSION}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cab99162e648257343a20f61bcd0b287f5e88e36fcb2f1d77959da60b7f35969
-TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv"
+TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv, librhash"
 TERMUX_PKG_FORCE_CMAKE=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DKWSYS_LFS_WORKS=ON"
 
