e00dfc00e14697759c8ed2a0c0da99a342b05371
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-20 09:00:02 +0200

    Make ubuntu 17.04 the reference build environment

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_VERSION=2.40.16
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.40/librsvg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d48bcf6b03fa98f07df10332fb49d8c010786ddca6ab34cbba217684f533ff2e
 TERMUX_PKG_DEPENDS="libcroco,pango,gdk-pixbuf"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-introspection"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-introspection --disable-pixbuf-loader"
