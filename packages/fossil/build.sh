497da8498a47df9da862f0ea33354c9aa3f407c5
Vishal Biswas <vshlbiswas@ymail.com>
2017-04-21 15:45:57 +0530

    Updates (#944)
    * valac: update from 0.36.0 to 0.36.1
    
    * fossil: update from 2.1 to 2.2
    
    * libicu: update from 58.2 to 59.1
    
    * libdb: update from 6.2.23 to 6.2.32

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
-TERMUX_PKG_DESCRIPTION='DSCM with built-in wiki, http interface and server, tickets database'
-TERMUX_PKG_MAINTAINER='Vishal Biswas @vishalbiswas'
-TERMUX_PKG_VERSION=2.1
+TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
+TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
+TERMUX_PKG_VERSION=2.2
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=85dcdf10d0f1be41eef53839c6faaa73d2498a9a140a89327cfb092f23cfef05
+TERMUX_PKG_SHA256=9b8f82196eb89e4a2e82b4bcc51e314ae509a22c37073a18a0b325f11c53cf71
 TERMUX_PKG_FOLDERNAME=fossil-${TERMUX_PKG_VERSION}
-TERMUX_PKG_DEPENDS='libsqlite, openssl'
+TERMUX_PKG_DEPENDS="libsqlite, openssl"
 
 termux_step_pre_configure () {
 	# Avoid mixup of flags between cross compilation
