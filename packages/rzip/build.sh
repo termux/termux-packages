a878434b0fc5b1fcf437014b6be89b2e00d0e573
Wetitpig <winsto003@hotmail.com>
2017-04-21 16:09:38 +0800

    Add rzip.

---
+++ packages/rzip/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://rzip.samba.org
+TERMUX_PKG_DESCRIPTION="A compression program efficient for large files."
+TERMUX_PKG_VERSION=2.1
+TERMUX_PKG_SRCURL=http://rzip.samba.org/ftp/rzip/rzip-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4bb96f4d58ccf16749ed3f836957ce97dbcff3e3ee5fd50266229a48f89815b7
+TERMUX_PKG_DEPENDS="libbz2"
+TERMUX_PKG_BUILD_IN_SRC=yes
