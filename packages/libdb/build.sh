d25da32bac913361eaa6236ba7fb23e754ba8916
Vishal Biswas <vshlbiswas@ymail.com>
2017-04-21 15:45:57 +0530

    Updates (#944)
    * valac: update from 0.36.0 to 0.36.1
    
    * fossil: update from 2.1 to 2.2
    
    * libicu: update from 58.2 to 59.1
    
    * libdb: update from 6.2.23 to 6.2.32

--- packages/libdb/build.sh
+++ packages/libdb/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.oracle.com/database/berkeley-db
 TERMUX_PKG_DESCRIPTION="The Berkeley DB embedded database system (library)"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION=6.2.23
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=6.2.32
 TERMUX_PKG_SRCURL=http://download.oracle.com/berkeley-db/db-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=47612c8991aa9ac2f6be721267c8d3cdccf5ac83105df8e50809daea24e95dc7
+TERMUX_PKG_SHA256=a9c5e2b004a5777aa03510cfe5cd766a4a3b777713406b02809c17c8e0e7a8fb
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-hash
 --enable-smallbuild
