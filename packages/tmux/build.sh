17fe6d2990737684c77e96e3ac7c3c9bd58d0519
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-22 02:26:00 +0200

    tmux: Update from 2.3 to 2.4

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -1,12 +1,15 @@
 TERMUX_PKG_HOMEPAGE=http://tmux.github.io/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer implementing switching between several programs in one terminal, detaching them and reattaching them to a different terminal"
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
-TERMUX_PKG_DEPENDS="ncurses, libevent, libutil, libandroid-support"
-TERMUX_PKG_VERSION=2.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_DEPENDS="ncurses, libevent, libutil, libandroid-support, libandroid-glob"
+TERMUX_PKG_VERSION=2.4
+TERMUX_PKG_SHA256=757d6b13231d0d9dd48404968fc114ac09e005d475705ad0cd4b7166f799b349
 TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/releases/download/${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=55313e132f0f42de7e020bf6323a1939ee02ab79c48634aa07475db41573852b
 TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-glob"
+}
 
 termux_step_post_make_install () {
 	cp $TERMUX_PKG_BUILDER_DIR/tmux.conf $TERMUX_PREFIX/etc/tmux.conf
