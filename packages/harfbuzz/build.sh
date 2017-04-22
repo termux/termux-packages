c35073b819a2d8fb26c51852b8e9814f7506453f
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-22 02:04:03 +0200

    harfbuzz&texlive: Bump after libicu 59.1 update

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_VERSION=1.4.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=d0e05438165884f21658154c709075feaf98c93ee5c694b951533ac425a9a711
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
