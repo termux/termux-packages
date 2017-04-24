1ecb0954ce1e4309dfacc7a4764b2c456f7ad2a5
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-24 17:09:49 +0200

    weechat: Update from 1.7 to 1.7.1

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
-TERMUX_PKG_VERSION=1.7
+TERMUX_PKG_VERSION=1.7.1
+TERMUX_PKG_SHA256=c0f9786cdfcf7411bfadca828c5e0819ac641717f775da804d8fa2aa69a84080
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=c78a0063eee2f4cf3e67a3ff3936cfba0ed0012b1b7e5ec381c2d981f2cb64d0
 TERMUX_PKG_DEPENDS="ncurses, libgcrypt, libcurl, libgnutls, libandroid-support"
 # weechat-curses is a symlink to weechat, so remove it:
 TERMUX_PKG_RM_AFTER_INSTALL="bin/weechat-curses share/icons"
