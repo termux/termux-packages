74243a4e669f8e48d517d728881b8f2207527518
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-24 23:43:11 +0200

    screen: Update from 4.4.0 to 4.5.1
    Also patch some more.

--- disabled-packages/screen/build.sh
+++ disabled-packages/screen/build.sh
@@ -1,14 +1,24 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/screen/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with VT100/ANSI terminal emulation"
-TERMUX_PKG_VERSION=4.4.0
+TERMUX_PKG_VERSION=4.5.1
+TERMUX_PKG_SHA256=97db2114dd963b016cd4ded34831955dcbe3251e5eee45ac2606e67e9f097b2d
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/screen/screen-${TERMUX_PKG_VERSION}.tar.gz
-# TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-colors256 --with-ssl=openssl --disable-socket-dir"
+TERMUX_PKG_DEPENDS="ncurses, libcrypt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-socket-dir
+--enable-colors256
+--with-ssl=openssl
+"
 
 termux_step_pre_configure () {
 	# Run autoreconf since we have patched configure.ac
 	cd $TERMUX_PKG_SRCDIR
 	autoconf
 	CFLAGS+=" -DGETUTENT"
-	LDFLAGS+=" -llog"
+	LDFLAGS+=" -llog -lcrypt"
+}
+
+termux_step_post_configure() {
+	echo '#define HAVE_SVR4_PTYS 1' >> $TERMUX_PKG_BUILDDIR/config.h
+	echo 'mousetrack on' > "$TERMUX_PREFIX/etc/screenrc"
 }
