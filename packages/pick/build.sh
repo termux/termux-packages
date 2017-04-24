94356a9959cd0640999a72ebd40bdad3ac5de047
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-24 17:10:00 +0200

    pick: Update from 1.6.0 to 1.6.1

--- packages/pick/build.sh
+++ packages/pick/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/calleerlandsson/pick
 TERMUX_PKG_DESCRIPTION="Utility to choose one option from a set of choices with fuzzy search functionality"
-TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_VERSION=1.6.1
+TERMUX_PKG_SHA256=b2d0384c376b697b7e8f17843485e6373514f001b35be18b6414a2fd5ced8d9b
 TERMUX_PKG_SRCURL=https://github.com/calleerlandsson/pick/releases/download/v${TERMUX_PKG_VERSION}/pick-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d034fd75256ccb3e8c6523196ac250b44b18170a6594944ed6d23d1bcabfae6a
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_search_setupterm=-lncurses"
