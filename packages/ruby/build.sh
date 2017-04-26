2b6ca764bb602d5a5cd2294695af5f59fc3b72da
Fredrik Fornwall <fredrik@fornwall.net>
2017-04-26 23:20:01 +0200

    ruby: Fix syslog module to link against liblog
    Fixes #962.

--- packages/ruby/build.sh
+++ packages/ruby/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.ruby-lang.org/
 TERMUX_PKG_DESCRIPTION="Dynamic programming language with a focus on simplicity and productivity"
 _MAJOR_VERSION=2.4
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://cache.ruby-lang.org/pub/ruby/${_MAJOR_VERSION}/ruby-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4fc8a9992de3e90191de369270ea4b6c1b171b7941743614cc50822ddc1fe654
 # libbffi is used by the fiddle extension module:
