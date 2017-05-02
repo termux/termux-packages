8f7ae5ba7211c63cacee655fb780106f4c0315ff
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-02 11:13:33 +0200

    lzip: Update from 1.18 to 1.19

--- packages/lzip/build.sh
+++ packages/lzip/build.sh
@@ -1,5 +1,5 @@
-TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/lzip/lzip.html
+TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/lzip/
 TERMUX_PKG_DESCRIPTION="Lossless data compressor similar to gzip and bzip2"
-TERMUX_PKG_VERSION=1.18
-TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/lzip-1.18.tar.lz
-TERMUX_PKG_SHA256=f579f3873a1bfa821b38f8d723e3016f69d22ff451ce9f0c61144db595fe914a
+TERMUX_PKG_VERSION=1.19
+TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/lzip-${TERMUX_PKG_VERSION}.tar.lz
+TERMUX_PKG_SHA256=c8b7a098a42429d34ca1504afcdfd9aeeaf249469d52039df05d0b5a2ad7ac77
