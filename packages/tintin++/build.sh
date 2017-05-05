bb385341df0b7dd8f8cf8a9098b84c9c02da6196
its-pointless <its-pointless@users.noreply.github.com>
2017-05-06 07:58:52 +1000

    mud client tintin++ (#986)

---
+++ packages/tintin++/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE="http://tintin.sourceforge.net"
+TERMUX_PKG_DESCRIPTION="classic text-based MUD client"
+TERMUX_PKG_VERSION=2.01.2
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/tintin/TinTin%2B%2B%20Source%20Code/2.01.2/tintin-2.01.2.tar.gz
+TERMUX_PKG_FOLDERNAME=tt/src
+TERMUX_PKG_SHA256=01e11e3cded48ff686b2ea16e767acf1f6b5ea326551ecff091552e89f4a038e
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_file__dev_ptmx=no"
+TERMUX_PKG_DEPENDS="pcre, libgnutls, libutil"
+TERMUX_PKG_BUILD_IN_SRC=yes
+termux_step_pre_configure() {
+	CFLAGS+=" $CPPFLAGS"
+}
