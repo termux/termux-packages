95edc8a47e1ae7b6811750723d1f95921bba6795
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-07 00:38:18 +0200

    utf8cpp: Update from 2.3.4 to 2.3.5

--- packages/utf8cpp/build.sh
+++ packages/utf8cpp/build.sh
@@ -1,12 +1,20 @@
-TERMUX_PKG_HOMEPAGE=http://utfcpp.sourceforge.net/
+TERMUX_PKG_HOMEPAGE=https://github.com/nemtrif/utfcpp
 TERMUX_PKG_DESCRIPTION="UTF8-CPP: UTF-8 with C++ in a Portable Way"
-TERMUX_PKG_VERSION=2.3.4
-TERMUX_PKG_SRCURL=http://pkgs.fedoraproject.org/repo/extras/utf8cpp/utf8_v2_3_4.zip/c5e9522fde3debcad5e8922d796b2bd0/utf8_v2_3_4.zip
-TERMUX_PKG_SHA256=3373cebb25d88c662a2b960c4d585daf9ae7b396031ecd786e7bb31b15d010ef
-TERMUX_PKG_FOLDERNAME="source"
+TERMUX_PKG_VERSION=2.3.5
+TERMUX_PKG_SHA256=f3ffe0ef6c02f48ebafe42369cbd741e844143baad27c13baad1cd14b863983d
+TERMUX_PKG_SRCURL=https://github.com/nemtrif/utfcpp/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_FOLDERNAME="utfcpp-$TERMUX_PKG_VERSION"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_NO_DEVELSPLIT=yes
 
+termux_step_configure() {
+	return 0;
+}
+
+termux_step_make() {
+	return 0;
+}
+
 termux_step_make_install(){
-	cp * -r $TERMUX_PREFIX/include
+	cp -r source/* $TERMUX_PREFIX/include/
 }
