6185d1ba9af5a7426ea4676919e80763ac9b75c0
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-07 00:44:36 +0200

    ledger: Note dependency on libmpfr
    Also remove adding -std=c++ to CXXFLAGS as the ledger build takes
    care of that itself.

--- packages/ledger/build.sh
+++ packages/ledger/build.sh
@@ -4,7 +4,4 @@ TERMUX_PKG_VERSION=3.1.1
 TERMUX_PKG_SRCURL=https://github.com/ledger/ledger/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME="ledger-${TERMUX_PKG_VERSION}"
 TERMUX_PKG_SHA256=90f06561ab692b192d46d67bc106158da9c6c6813cc3848b503243a9dfd8548a
-TERMUX_PKG_DEPENDS="libedit, boost, utf8cpp"
-termux_step_pre_configure() {
-	CXXFLAGS+=" -std=c++11"
-}
+TERMUX_PKG_DEPENDS="boost, libedit, libmpfr, utf8cpp"
