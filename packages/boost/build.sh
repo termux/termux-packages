c372f7f6f9c4c61d18e6db17bef5f805ee9e4081
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-07 00:39:50 +0200

    boost: Build without libicu
    As libicu is a rather heavy dependency we avoid it for now - it can
    always be added on demand.

--- packages/boost/build.sh
+++ packages/boost/build.sh
@@ -1,22 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://boost.org
 TERMUX_PKG_DESCRIPTION="Free peer-reviewed portable C++ source libraries"
 TERMUX_PKG_VERSION=1.64.0
-TERMUX_PKG_SRCURL=http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.bz2
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=http://sourceforge.net/projects/boost/files/boost/${TERMUX_PKG_VERSION}/boost_${TERMUX_PKG_VERSION//./_}.tar.bz2
 TERMUX_PKG_SHA256=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332
-TERMUX_PKG_FOLDERNAME="boost_1_64_0"
+TERMUX_PKG_FOLDERNAME="boost_${TERMUX_PKG_VERSION//./_}"
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="libbz2, libicu"
-
-termux_step_configure(){
-	return 0;
-}
-
-termux_step_make() {
-	return 0;
-}
+TERMUX_PKG_DEPENDS="libbz2"
 
 termux_step_make_install() {
-	CXXFLAGS+="  -std=c++11"
+	CXXFLAGS+=" -std=c++11"
 
 	rm $TERMUX_PREFIX/lib/libboost* -f
 	rm $TERMUX_PREFIX/include/boost -rf
@@ -25,9 +18,8 @@ termux_step_make_install() {
 
 	echo "using clang : $TERMUX_ARCH : $CXX : <linkflags>-L/data/data/com.termux/files/usr/lib ; " >> project-config.jam
 
-	./b2  target-os=android -j${TERMUX_MAKE_PROCESSES} \
+	./b2 target-os=android -j${TERMUX_MAKE_PROCESSES} \
 		include=/data/data/com.termux/files/usr/include \
-		include=/data/data/com.termux/files/usr/include/python2.7 \
 		toolset=clang-$TERMUX_ARCH \
 		--prefix="$TERMUX_PREFIX"  \
 		-q \
@@ -36,6 +28,7 @@ termux_step_make_install() {
 		--without-context \
 		--without-log \
 		--without-python \
+		--disable-icu \
 		cxxflags="$CXXFLAGS" \
 		link=shared \
 		threading=multi \
