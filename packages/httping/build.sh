0bb1ae265faff4fde2eeb4f3f86a4800e8c68c50
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-07 17:47:51 +0200

    httping: Update deps and configure arguments
    - Using the gettext functions in libandroid-support is enough,
      we do not need to depend on gettext.
    - Build with fftw3, ncurses and openssl enabled.

--- packages/httping/build.sh
+++ packages/httping/build.sh
@@ -4,5 +4,6 @@ TERMUX_PKG_VERSION=2.5
 TERMUX_PKG_SRCURL=https://www.vanheusden.com/httping/httping-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_SHA256=3e895a0a6d7bd79de25a255a1376d4da88eb09c34efdd0476ab5a907e75bfaf8
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="openssl, gettext"
+TERMUX_PKG_DEPENDS="fftw, libandroid-support, ncurses, openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-fftw3 --with-ncurses --with-openssl"
 TERMUX_PKG_MAINTAINER="Pierre Rudloff @Rudloff"
