5315b7ec750f345b832d0e97ae63504602e967e4
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-11 01:41:54 +0200

    json-glib: Build from release tarball

--- packages/json-glib/build.sh
+++ packages/json-glib/build.sh
@@ -1,13 +1,7 @@
-TERMUX_PKG_HOMEPAGE="https://git.gnome.org/browse/json-glib/"
+TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/JsonGlib
 TERMUX_PKG_DESCRIPTION="GLib JSON manipulation library"
-TERMUX_PKG_VERSION=1.2.8
-TERMUX_PKG_SRCURL=https://git.gnome.org/browse/json-glib/snapshot/json-glib-1.2.8.tar.xz
-#TERMUX_PKG_FOLDERNAME=babl-BABL_0_1_24
-TERMUX_PKG_SHA256=3decd608d4bdb19c2bdfe0be49d67486ec7e238bbb5d32ebfa334eedfdba3593
+local _MAJOR_VERSION=1.2
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.8
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/json-glib/${_MAJOR_VERSION}/json-glib-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=fd55a9037d39e7a10f0db64309f5f0265fa32ec962bf85066087b83a2807f40a
 TERMUX_PKG_DEPENDS="glib"
-TERMUX_PKG_BUILD_IN_SRC=yes
-termux_step_pre_configure() {
-export THREAD_LIB="-lc"
-	LDFLAGS+=" -landroid-support"
-	NOCONFIGURE=1 	./autogen.sh
-}
