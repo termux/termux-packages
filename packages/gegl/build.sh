00ca7412de74ef991bd3fe71a1aa5d544b425587
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-11 01:47:44 +0200

    gegl: Build from release tarball, deps

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -1,12 +1,11 @@
-TERMUX_PKG_HOMEPAGE="gegl.org"
-TERMUX_PKG_DESCRIPTION="data flow based image processing framework"
-TERMUX_PKG_VERSION=0.3.14
-TERMUX_PKG_SRCURL=https://git.gnome.org/browse/gegl/snapshot/GEGL_0_3_14.tar.xz
-TERMUX_PKG_FOLDERNAME=GEGL_0_3_14
-TERMUX_PKG_SHA256=941b8d48a0abd4299717be9ae92b084d478775503eb0ef79ca9d4bfffbd2623b
-TERMUX_PKG_DEPENDS="ffmpeg,  babl, json-glib, libpng, libffi"
-TERMUX_PKG_BUILD_IN_SRC=yes
-termux_step_pre_configure() {
-	LDFLAGS+=" -landroid-support"
-        NOCONFIGURE=1 ./autogen.sh
-}
+TERMUX_PKG_HOMEPAGE=http://gegl.org/
+TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
+local _MAJOR_VERSION=0.3
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.14
+TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/$_MAJOR_VERSION/gegl-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_FOLDERNAME=gegl-$TERMUX_PKG_VERSION
+TERMUX_PKG_SHA256=09f5e2e6899697641d4660e3e274aed696f5bacc96ba389ac77674ee1156590a
+TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--without-gdk-pixbuf
+"
