c4d21b48c3c1f95b68b114a644a05cc5b32f4f95
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-13 19:08:18 +0200

    gpgme: Update from 1.8.0 up 1.9.0

--- packages/gpgme/build.sh
+++ packages/gpgme/build.sh
@@ -1,9 +1,9 @@
-TERMUX_PKG_HOMEPAGE=http://www.gnupg.org/related_software/gpgme/
+TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/gpgme/
 TERMUX_PKG_DESCRIPTION="Library designed to make access to GnuPG easier"
 TERMUX_PKG_DEPENDS="gnupg2, libassuan"
-TERMUX_PKG_VERSION=1.8.0
+TERMUX_PKG_VERSION=1.9.0
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=596097257c2ce22e747741f8ff3d7e24f6e26231fa198a41b2a072e62d1e5d33
+TERMUX_PKG_SHA256=1b29fedb8bfad775e70eafac5b0590621683b2d9869db994568e6401f4034ceb
 # Use "--disable-gpg-test" to avoid "No rule to make target `../../src/libgpgme-pthread.la":
 # Use "--enable-languages=no" to only build the C library.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
