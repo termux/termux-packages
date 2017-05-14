2a8107ad296b8dfe22b742601fb432430b6f3d41
Wetitpig <winsto003@hotmail.com>
2017-05-14 08:40:15 +0800

    Do not gzip tar

--- build-package.sh
+++ build-package.sh
@@ -817,11 +817,11 @@ termux_step_post_make_install() {
 }
 
 termux_step_extract_into_massagedir() {
-	local TARBALL_ORIG=$TERMUX_PKG_PACKAGEDIR/${TERMUX_PKG_NAME}_orig.tar.gz
+	local TARBALL_ORIG=$TERMUX_PKG_PACKAGEDIR/${TERMUX_PKG_NAME}_orig.tar
 
 	# Build diff tar with what has changed during the build:
 	cd $TERMUX_PREFIX
-	tar -N "$TERMUX_BUILD_TS_FILE" -czf "$TARBALL_ORIG" .
+	tar -N "$TERMUX_BUILD_TS_FILE" -cf "$TARBALL_ORIG" .
 
 	# Extract tar in order to massage it
 	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"
