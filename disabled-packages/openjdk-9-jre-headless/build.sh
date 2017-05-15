1f3323ce42782fcd1c9ba2cfa725da4ce5998da9
Vishal Biswas <vshlbiswas@ymail.com>
2017-05-15 18:57:45 +0530

    some disabled package updates (#1018)
    * openvpn: update from 2.4.2 to 2.4.1
    
    * openjdk: update to tag 168 (aarch64 does not build)
    
    * openjdk: remove unused patch

--- disabled-packages/openjdk-9-jre-headless/build.sh
+++ disabled-packages/openjdk-9-jre-headless/build.sh
@@ -1,17 +1,15 @@
 TERMUX_PKG_HOMEPAGE=http://openjdk.java.net
 TERMUX_PKG_DESCRIPTION="OpenJDK 9 Java Runtime Environment (prerelease)"
-_jbuild=162
+_jbuild=168
 _hg_tag="jdk-9+${_jbuild}"
 _jvm_dir="lib/jvm/openjdk-9"
-TERMUX_PKG_VERSION="9.2017.4.3"
+TERMUX_PKG_VERSION="9.2017.5.12"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_HOMEPAGE=http://openjdk.java.net/projects/jdk9
 TERMUX_PKG_DEPENDS="freetype, libpng, ca-certificates-java"
 TERMUX_PKG_CONFFILES="$_jvm_dir/lib/jvm.cfg"
-# currently upstream has no support building for these arches on android
-# this will change in the future
-TERMUX_PKG_BLACKLISTED_ARCHES="aarch64 x86_64"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-aot
 --disable-hotspot-gtest
 --disable-option-checking
 --disable-warnings-as-errors
@@ -20,30 +18,29 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-libpng=system
 --with-zlib=system
 --with-jdk-variant=normal
---with-jvm-variants=client
+--with-jvm-variants=server
 "
 TERMUX_PKG_CLANG=no
 TERMUX_PKG_RM_AFTER_INSTALL="$_jvm_dir/demo $_jvm_dir/sample"
-
 _cups_ver=2.2.3
 
-changesets=('d9c3e4f30936'
-	'493011dee80e'
-	'dc3346496843'
-	'0d44d05a4c96'
-	'3890f96e8995'
-	'92a38c75cd27'
-	'24582dd2649a'
-	'5e5e436543da')
+changesets=('d3e4e68dc2a4'
+	'b2218d41edef'
+	'2982a1d6ecfc'
+	'69b4c97b87b5'
+	'912cf69806d5'
+	'5d9d2a65fb26'
+	'0e522ff8b9f5'
+	'131e25008015')
 
-sha256sums=('36ca35e4fe90ae1b1966d9f909c108f39fe411b2e783faa49102d2088909be8e'
-	'9d24cab2e16c17f51d591b9786005062bee3e60e394d1b78dddbdfb01b9a5ea6'
-	'312204f76d4f23be09aa7121273ac791588de5c6a3c25d49b3087a6eb547bb7a'
-	'9ef21b7013ef2a0b0870d4741fc961d1fce0fcb91f44a1f8cdad865455583246'
-	'8d925111270630a171b0165b740bdd5d8d07c4aa1f9ea3caa86076b9f896d3ed'
-	'1fec470e1480472ec7e4ff402bc4b6de5e095cfd5f787bdc250814eb9479a3c0'
-	'db95a143078cdf3cefff5479c5350b678b1a779fcdcf7e066049559a537d81e1'
-	'115601bbce2f5d9df66ce49d4ca6f6db327f1e17865537911160f0cde919e7bf'
+sha256sums=('6e38d7ff3ae082206b43973fd43c07c88d1e5b30985e0c4d8357c364f51cf66c'
+	'3173a9b6bc380ebdda014c512f0f88871a8ebdea3fda0d9ad350dba42a3493d6'
+	'ff1342410d63ccec54f3e043b74fbc0549b01260bb15170515c68a8dfb4f7b0b'
+	'cda559ee5cae0f8ab0c7ea19d70eaff58e7a44462786c78dfa8811f24d548aef'
+	'a24208276b465e64085890d7daf5c4d25eb8abf629d636477f85e8849a7c978a'
+	'a1b1a77118183000cf20bac7e10e7272145dc64e1120ccb4559b8df7160a48db'
+	'3f4517e94efb689a291dce74ac2a12e496767d417a05d7fc6c74c483f3d14cca'
+	'961adb3a0facfec181ff3b999b6fa079dffb1ff4a8f5607905f5ea28dcc597bf'
 	'66701fe15838f2c892052c913bde1ba106bbee2e0a953c955a62ecacce76885f')
 
 reponames=(dev corba hotspot jdk jaxws jaxp langtools nashorn cups)
@@ -93,17 +90,27 @@ HERE
 	export ANDROID_DEVKIT=$TERMUX_STANDALONE_TOOLCHAIN
 
 	if [ -n "$TERMUX_DEBUG" ]; then TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-debug-level=slowdebug"; fi
+	BUILD_TRIPLE=$TERMUX_ARCH-linux-gnu
+	test "$TERMUX_ARCH" == "arm" && BUILD_TRIPLE+="eabi"
+
+	JVM_FEATURES="compiler1,compiler2,jvmti,fprof,vm-structs,jni-check,services,management,all-gcs,nmt,cds"
+	# enable features specific to some arches
+	if [ "$TERMUX_ARCH" == "aarch64" ] || [ "$TERMUX_ARCH" == "x86_64" ]; then JVM_FEATURES+=",jvmci,graal"; fi
+
+	# remove sa_proc support
+	rm $TERMUX_PKG_SRCDIR/hotspot/make/lib/Lib-jdk.hotspot.agent.gmk
 }
 
 termux_step_configure () {
 	bash $TERMUX_PKG_SRCDIR/configure \
 		--prefix=$TERMUX_PREFIX \
-		--openjdk-target=$TERMUX_HOST_PLATFORM \
+		--openjdk-target=$BUILD_TRIPLE \
 		--libexecdir=$TERMUX_PREFIX/libexec \
 		--with-devkit=$ANDROID_DEVKIT \
 		--with-extra-cflags="$CPPFLAGS $CFLAGS" \
 		--with-extra-cxxflags="$CPPFLAGS $CXXFLAGS" \
 		--with-extra-ldflags="$LDFLAGS" \
+		--with-jvm-features="$JVM_FEATURES" \
 		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
 
