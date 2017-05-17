994b7d480cd0169117e4db712db2b0678b8c8d89
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-17 23:22:56 +0200

    imagemagick: Update from 7.0.5-1 to 7.0.5-6

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,10 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://www.imagemagick.org/
+TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
-TERMUX_PKG_VERSION="7.0.5-1"
-# Since older releases are removed from imagemagick.org we use our bintray mirror to avoid breaking
-# the build for each minor version bump.
+TERMUX_PKG_VERSION="7.0.5-6"
 TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/ImageMagick-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=981c8de7dd65f26f21a9ebce0d0da1123859d9b16eda37021bac5fb5901fd737
+TERMUX_PKG_SHA256=f60c3e3466ccbf9a0e643284ffec3467363edf9611f16a98387f927a974b49ab
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-largefile
 --without-x
