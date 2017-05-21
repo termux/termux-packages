1cdebe5cb19e9632dcc74b76f8d72959aa5976c3
Vishal Biswas <vshlbiswas@ymail.com>
2017-05-21 11:35:46 +0530

    minor updates for tor and valac (#1039)
    * tor: update from 0.3.0.6 to 0.3.0.7
    
    * valac: update from 0.36.1 to 0.36.3

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_DEPENDS="libevent, openssl"
-TERMUX_PKG_VERSION=0.3.0.6
+TERMUX_PKG_VERSION=0.3.0.7
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a3e512e93cb555601fd207d914d7c5fe981d66d6ebb5821ecdf5dea738c2fb14
+TERMUX_PKG_SHA256=9640c4448ef3cad7237c68ed6984e705db8fb2b9d6bb74c8815d01bb06527d02
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 
