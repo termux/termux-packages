9818f686411710900a685c6beca98b16777473e3
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-23 01:22:25 +0200

    ncmpcpp: Update from git snapshot to 0.8 release

--- packages/ncmpcpp/build.sh
+++ packages/ncmpcpp/build.sh
@@ -1,10 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://rybczak.net/ncmpcpp/
 TERMUX_PKG_DESCRIPTION="NCurses Music Player Client (Plus Plus)"
-TERMUX_PKG_VERSION=0.7.7
-local _COMMIT=8134e6e23b2787322fa10e65e44d286da82eea91
-TERMUX_PKG_SRCURL=https://github.com/arybczak/ncmpcpp/archive/${_COMMIT}.zip
-TERMUX_PKG_FOLDERNAME="ncmpcpp-$_COMMIT"
-TERMUX_PKG_SHA256=aced88b623ef79f6ccf619f769e7f4b6680d395aef9a789cee6d019927577c62
+TERMUX_PKG_VERSION=0.8
+TERMUX_PKG_SRCURL=https://rybczak.net/ncmpcpp/stable/ncmpcpp-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=2f0f2a1c0816119430880be6932e5eb356b7875dfa140e2453a5a802909f465a
 TERMUX_PKG_DEPENDS="fftw, boost, readline, libcurl, libmpdclient"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_KEEP_SHARE_DOC=yes
