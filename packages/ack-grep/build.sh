ea9b37be3b1a518b236c7383e4583fc606379967
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-22 23:51:38 +0200

    ack-grep: Update from 2.14 to 2.18

--- packages/ack-grep/build.sh
+++ packages/ack-grep/build.sh
@@ -1,11 +1,15 @@
-TERMUX_PKG_HOMEPAGE=http://beyondgrep.com/
+TERMUX_PKG_HOMEPAGE=https://beyondgrep.com/
 TERMUX_PKG_DESCRIPTION="Tool like grep optimized for programmers"
-TERMUX_PKG_VERSION=2.14
+TERMUX_PKG_VERSION=2.18
 # Depend on coreutils for bin/env.
 TERMUX_PKG_DEPENDS="perl, coreutils"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
-	curl -o $TERMUX_PREFIX/bin/ack http://beyondgrep.com/ack-${TERMUX_PKG_VERSION}-single-file
+	termux_download \
+		https://beyondgrep.com/ack-${TERMUX_PKG_VERSION}-single-file \
+		$TERMUX_PREFIX/bin/ack \
+		6e41057c8f50f661d800099471f769209480efa53b8a886969d7ec6db60a2208
+	touch $TERMUX_PREFIX/bin/ack
 	chmod +x $TERMUX_PREFIX/bin/ack
 }
