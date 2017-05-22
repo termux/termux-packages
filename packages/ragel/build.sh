d046cbcc5886971d0b0e36df1ae93a6774db881f
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-23 01:36:21 +0200

    ragel: Update from 6.9 to 6.10

--- packages/ragel/build.sh
+++ packages/ragel/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.colm.net/open-source/ragel/
 TERMUX_PKG_DESCRIPTION="Compiles finite state machines from regular languages into executable C, C++, Objective-C, or D code"
-TERMUX_PKG_VERSION=6.9
+TERMUX_PKG_VERSION=6.10
 TERMUX_PKG_SRCURL=http://www.colm.net/files/ragel/ragel-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6e07be0fab5ca1d9c2d9e177718a018fc666141f594a5d6e7025658620cf660a
+TERMUX_PKG_SHA256=5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f
