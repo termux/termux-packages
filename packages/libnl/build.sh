8f02d74cee0936b21ce96b7fd3e8b5c20d52f5eb
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-23 00:41:56 +0200

    libnl: Update from 3.2.29 to 3.3.0

--- packages/libnl/build.sh
+++ packages/libnl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/thom311/libnl
 TERMUX_PKG_DESCRIPTION="Collection of libraries providing APIs to netlink protocol based Linux kernel interfaces"
-TERMUX_PKG_VERSION=3.2.29
+TERMUX_PKG_VERSION=3.3.0
 TERMUX_PKG_VERSION_DASH=`echo $TERMUX_PKG_VERSION | sed -e 's/\./_/g'`
 TERMUX_PKG_SRCURL=https://github.com/thom311/libnl/releases/download/libnl$TERMUX_PKG_VERSION_DASH/libnl-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0beb593dc6abfffa18a5c787b27884979c1b7e7f1fd468c801e3cc938a685922
+TERMUX_PKG_SHA256=705468b5ae4cd1eb099d2d1c476d6a3abe519bc2810becf12fb1e32de1e074e4
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthreads --disable-cli"
