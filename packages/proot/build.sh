4480f511b7b1472c32b0fe066a067e1a6c220ed3
michalbednarski <michalbednarski@users.noreply.github.com>
2017-05-25 22:15:42 +0200

    proot: Update and mount /vendor (#1055)
    * Update to fix emulated ptrace() on ARM thumb
    * Mount /vendor (fixes termux/proot#2)

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://proot.me/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
 # Just bump commit and version when needed:
-_COMMIT=edc869d60c7f5b6abf67052a327ef099aded7777
-TERMUX_PKG_VERSION=5.1.104
+_COMMIT=58d216143b81558d71bcb29686cfa9ab1bfe3f4b
+TERMUX_PKG_VERSION=5.1.105
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=3b1a579a108c17e897ef8da02ed7af8d77000c5d9946ed72f113f87ce61f5637
+TERMUX_PKG_SHA256=a2e33023d32d1a107be66d95b7411f98e19b2ddb3f440947cf220244d453918e
 TERMUX_PKG_FOLDERNAME=proot-$_COMMIT
 TERMUX_PKG_DEPENDS="libtalloc"
 
