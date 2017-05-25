cf404fbed9c1d637e5588acd30eb7fbace8563bb
Fredrik Fornwall <fredrik@fornwall.net>
2017-05-25 22:59:59 +0200

    sqlite: Update from 3.19.0 to 3.19.2

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Software library that implements a self-contained, serve
 # Note: Updating this version requires bumping the tcl package as well.
 _SQLITE_MAJOR=3
 _SQLITE_MINOR=19
-_SQLITE_PATCH=0
+_SQLITE_PATCH=2
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
 TERMUX_PKG_SRCURL=https://www.sqlite.org/2017/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
-TERMUX_PKG_SHA256=b516e665952c6659638eab3283f09f317afb212fd8e39d6415b4b6259c37b0f5
+TERMUX_PKG_SHA256=ca5361fb01cc3ad63d6fd4eb2cb0b6398e629595896d3558f7e121d37dac2ffc
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-readline"
