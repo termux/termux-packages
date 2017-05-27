bd717d94f2c4964e86f8188db75167e8c710f31d
its-pointless <bored@goat.si>
2017-05-27 22:39:42 +1000

    snake game ala nokia phones..

---
+++ packages/nsnake/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/alexdantas/nSnake
+TERMUX_PKG_DESCRIPTION="The classic snake game with textual interface "
+TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_SRCURL=https://github.com/alexdantas/nSnake/archive/v3.0.1.tar.gz
+TERMUX_PKG_SHA256=7240dafe35e17b01134591d7ae8f09f5a375cded8b01e43ba97ca3610a09ea61
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_FOLDERNAME=nSnake-3.0.1
