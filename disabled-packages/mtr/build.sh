aaebf817503365efd93dbc62c3a2e28d317788c0
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-02 13:59:49 +0200

    mtr: Update disabled package from 0.87 to 0.92

--- disabled-packages/mtr/build.sh
+++ disabled-packages/mtr/build.sh
@@ -3,7 +3,8 @@
 #         http://dan.drown.org/android/src/libres/
 TERMUX_PKG_HOMEPAGE=https://github.com/traviscross/mtr
 TERMUX_PKG_DESCRIPTION="Network diagnostic tool"
-TERMUX_PKG_VERSION=0.87
+TERMUX_PKG_VERSION=0.92
+TERMUX_PKG_SHA256=568a52911a8933496e60c88ac6fea12379469d7943feb9223f4337903e4bc164
 TERMUX_PKG_SRCURL=https://github.com/traviscross/mtr/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=mtr-$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="ncurses"
