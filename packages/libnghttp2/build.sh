7ff7aa38a85448ffd4cc18045e9f3e25ae90797b
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-02 13:53:48 +0200

    nghttp2: Update from 1.23.0 to 1.23.1

--- packages/libnghttp2/build.sh
+++ packages/libnghttp2/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://nghttp2.org/
 TERMUX_PKG_DESCRIPTION="nghttp HTTP 2.0 library"
-TERMUX_PKG_VERSION=1.23.0
+TERMUX_PKG_VERSION=1.23.1
 TERMUX_PKG_SRCURL=https://github.com/nghttp2/nghttp2/releases/download/v${TERMUX_PKG_VERSION}/nghttp2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e414096abafa87d4ca4ffdc5417c172c0a43414a4288318f80c257f51cb619c0
+TERMUX_PKG_SHA256=fb75e8c0d6cf9c4381fff242d2dc04cdcc2691af8dc125c6ca349efecf5ccc21
 TERMUX_PKG_FOLDERNAME="nghttp2-$TERMUX_PKG_VERSION"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-lib-only"
 # The tools are not built due to --enable-lib-only:
