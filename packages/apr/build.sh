9684634c6b0de3e09769fa77c3c1eb663e3ae34f
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-06 11:48:57 +0200

    apr&apr-util: Bump build revision

--- packages/apr/build.sh
+++ packages/apr/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://apr.apache.org/
 TERMUX_PKG_VERSION=1.5.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net/apr/apr-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=7d03ed29c22a7152be45b8e50431063736df9e1daa1ddf93f6a547ba7a28f67a
 TERMUX_PKG_DESCRIPTION="Apache Portable Runtime - library providing a predictable and consistent interface to underlying platform-specific implementations"
