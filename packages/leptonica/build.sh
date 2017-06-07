3cd5c96efbbba063434fcae3a1a659aac17e79d5
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-07 11:07:57 +0200

    leptonica: Update from 1.74.1 to 1.74.2

--- packages/leptonica/build.sh
+++ packages/leptonica/build.sh
@@ -1,7 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://www.leptonica.com/
-TERMUX_PKG_DESCRIPTION="Leptonica is a pedagogically-oriented open source site containing software that is broadly useful for image processing and image analysis applications"
-TERMUX_PKG_VERSION=1.74.1
+TERMUX_PKG_DESCRIPTION="Library for image processing and image analysis"
+TERMUX_PKG_VERSION=1.74.2
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff"
-TERMUX_PKG_SRCURL=http://www.leptonica.com/source/leptonica-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=746a517a47a3bd2a90bc8d581ca6464c10f30e91a60209735efe45b3778bec62
+TERMUX_PKG_SRCURL=https://github.com/DanBloomberg/leptonica/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8b2e9bacd23597d0ee21625a7d8443177b1cb85daf196d86b67ad75044e29c27
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp --without-libopenjpeg"
+TERMUX_PKG_FOLDERNAME=leptonica-$TERMUX_PKG_VERSION
+
+termux_step_pre_configure() {
+	./autobuild
+}
