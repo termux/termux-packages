39797b9f24d87319cad31349b2bb71c8e84c48ff
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-07 12:01:48 +0200

    libopus: Update from 1.1.4 to 1.1.5

--- packages/libopus/build.sh
+++ packages/libopus/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://www.opus-codec.org/
 TERMUX_PKG_DESCRIPTION="Reference implementation of the Opus codec"
-TERMUX_PKG_VERSION=1.1.4
-TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/opus/opus-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9122b6b380081dd2665189f97bfd777f04f92dc3ab6698eea1dbb27ad59d8692
+TERMUX_PKG_VERSION=1.1.5
+TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/opus/opus-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=eb84981ca0f40a3e5d5e58d2e8582cb2fee05a022825a6dfe14d14b04eb563e4
