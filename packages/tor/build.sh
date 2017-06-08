25f73326c4f1da31d932cc41c83221e65e09605f
vishalbiswas <vshlbiswas@ymail.com>
2017-06-08 23:34:41 +0530

    tor: update from 0.3.0.7 to 0.3.0.8

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_DEPENDS="libevent, openssl"
-TERMUX_PKG_VERSION=0.3.0.7
+TERMUX_PKG_VERSION=0.3.0.8
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9640c4448ef3cad7237c68ed6984e705db8fb2b9d6bb74c8815d01bb06527d02
+TERMUX_PKG_SHA256=663a3ba7b8a124c0f8a7351eaa2dda6fd518de3f3c4ee28fff869bfb03860d48
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 
