1af04c0ed64b4bbe9956e7d94eeb1aec876708cf
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-19 15:26:27 +0200

    cairo: Update from 1.4.8 to 1.4.10
    This required rebuilding cairo-using packages due to a shared
    library name change (libcairo.so.2 -> libcairo.so).

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_VERSION=1.4.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=21a78b81cd20cbffdb04b59ac7edfb410e42141869f637ae1d6778e74928d293
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
-TERMUX_PKG_VERSION="7.0.5-6"
+TERMUX_PKG_VERSION="7.0.6-0"
 TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/ImageMagick-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=f60c3e3466ccbf9a0e643284ffec3467363edf9611f16a98387f927a974b49ab
+TERMUX_PKG_SHA256=31587e2aa4d46301461329fd88d4ed6fccb0bd4ae786d629a153df72dbac89ee
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-largefile
 --without-x
@@ -16,3 +16,10 @@ ac_cv_func_ftime=no
 ac_cv_header_complex_h=no"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/Magick-config bin/MagickCore-config bin/MagickWand-config bin/Wand-config share/ImageMagick-6/francais.xml share/man/man1/Magick-config.1 share/man/man1/MagickCore-config.1 share/man/man1/MagickWand-config.1 share/man/man1/Wand-config.1"
 TERMUX_PKG_DEPENDS="fftw, pango, glib, libbz2, libjpeg-turbo, liblzma, libpng, libtiff, libxml2, openjpeg, littlecms"
+
+termux_step_pre_configure() {
+	if [ $TERMUX_ARCH = "i686" ]; then
+		# Avoid "libMagickCore-7.Q16HDRI.so: error: undefined reference to '__atomic_load'":
+		LDFLAGS+=" -latomic"
+	fi
+}
