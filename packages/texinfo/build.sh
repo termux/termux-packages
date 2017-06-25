1c7d0e33472bf23bf89fd9c45d720c0572b01546
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-25 22:10:28 +0200

    texinfo: Update from 6.3 to 6.4

--- packages/texinfo/build.sh
+++ packages/texinfo/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/texinfo/
 TERMUX_PKG_DESCRIPTION="Documentation system for on-line information and printed output"
-TERMUX_PKG_VERSION=6.3
+TERMUX_PKG_VERSION=6.4
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/texinfo/texinfo-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=246cf3ffa54985118ec2eea2b8d0c71b92114efe6282c2ae90d65029db4cf93a
+TERMUX_PKG_SHA256=6ae2e61d87c6310f9af7c6f2426bd0470f251d1a6deb61fba83a3b3baff32c3a
 TERMUX_PKG_DEPENDS="ncurses, perl"
