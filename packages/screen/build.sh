c81f3e6fe98338c88df25332bc49ae4e66629df3
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-30 21:30:15 +0200

    screen: Depend on libutil

--- packages/screen/build.sh
+++ packages/screen/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with VT100/ANSI terminal emulation"
 TERMUX_PKG_VERSION=4.6.0
 TERMUX_PKG_SHA256=9433706b653e941cc4c745f28e252e57be2a141eded923e61cc2c4a09768fed4
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/screen/screen-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses, libcrypt"
+TERMUX_PKG_DEPENDS="ncurses, libcrypt, libutil"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-socket-dir
 --enable-colors256
