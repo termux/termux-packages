c6f81a7fbf5092176ae7d05f46baf170deff0720
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-01 00:54:18 +0200

    lzop: Switch for fossies.org for src

--- packages/lzop/build.sh
+++ packages/lzop/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.lzop.org
 TERMUX_PKG_DESCRIPTION='File compressor using lzo lib.'
 TERMUX_PKG_VERSION=1.03
-TERMUX_PKG_SRCURL=http://www.lzop.org/download/lzop-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c1425b8c77d49f5a679d5a126c90ea6ad99585a55e335a613cae59e909dbb2c9
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/lzop-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=4a5843a99584ecd8a57876af53d7a9cc37aae2a2a6f533c25bee0114ceb233f0
 TERMUX_PKG_DEPENDS="liblzo"
 TERMUX_PKG_MAINTAINER='Vishal Biswas @vishalbiswas'
 
