b84ef4b8842273526508b89d3de3021a715761fc
Fredrik Fornwall <fredrik@fornwall.net>
2017-06-30 21:41:25 +0200

    gl_cv_func_realpath_works=yes in build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -731,6 +731,7 @@ termux_step_configure_autotools () {
 	AVOID_GNULIB+=" gl_cv_func_memchr_works=yes"
 	AVOID_GNULIB+=" gl_cv_func_mkdir_trailing_dot_works=yes"
 	AVOID_GNULIB+=" gl_cv_func_mkdir_trailing_slash_works=yes"
+	AVOID_GNULIB+=" gl_cv_func_realpath_works=yes"
 	AVOID_GNULIB+=" gl_cv_func_select_detects_ebadf=yes"
 	AVOID_GNULIB+=" gl_cv_func_snprintf_posix=yes"
 	AVOID_GNULIB+=" gl_cv_func_snprintf_retval_c99=yes"
