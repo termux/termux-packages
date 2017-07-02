aaff9cad571f822c333ba817ed0e1fae1965ba14
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-02 11:21:08 +0200

    ncurses: Format configure args
    Also remove --with-pkg-config-libdir which is not necessary when
    already having set the PKG_CONFIG_LIBDIR environment variable.

--- packages/ncurses/build.sh
+++ packages/ncurses/build.sh
@@ -6,7 +6,23 @@ TERMUX_PKG_VERSION=${_MAJOR_VERSION}.${_MINOR_VERSION}
 TERMUX_PKG_SRCURL=ftp://invisible-island.net/ncurses/current/ncurses-${_MAJOR_VERSION}-${_MINOR_VERSION}.tgz
 TERMUX_PKG_SHA256=4c196bd0f1bf0ce643c1547cc7fc25cee713d21299a660eeb9b5dfa8becacb45
 # --without-normal disables static libraries:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-overwrite --enable-const --without-cxx-binding --without-normal --without-static --with-shared --without-debug --enable-widec --enable-ext-colors --enable-ext-mouse --enable-pc-files --with-pkg-config-libdir=$PKG_CONFIG_LIBDIR --without-ada --without-tests --mandir=$TERMUX_PREFIX/share/man ac_cv_header_locale_h=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_header_locale_h=no
+--enable-const
+--enable-ext-colors
+--enable-ext-mouse
+--enable-overwrite
+--enable-pc-files
+--enable-widec
+--mandir=$TERMUX_PREFIX/share/man
+--without-ada
+--without-cxx-binding
+--without-debug
+--without-normal
+--without-static
+--without-tests
+--with-shared
+"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/ncursesw6-config share/man/man1/ncursesw6-config.1 bin/infotocap share/man/man1/infotocap.1m bin/captoinfo share/man/man1/captoinfo.1m"
 
 termux_step_post_make_install () {
