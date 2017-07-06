bc9c5bba0d13afb027fd63ecc5b69a697b106efa
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-06 23:47:52 +0200

    libuv: Update from 1.12.0 to 1.13.0

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,12 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
-TERMUX_PKG_VERSION=1.12.0
+TERMUX_PKG_VERSION=1.13.0
 TERMUX_PKG_SRCURL=http://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=01730d1b5e9e278eec67394e7d44021ea318d5d29520c5aa8b65bc8a18b953c7
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-dtrace" # needed for building on mac
+TERMUX_PKG_SHA256=708e51f8845e1728a2df50d2aa8438dbd97a48d4a86fcbbcdead0c406d710315
 
 termux_step_pre_configure () {
-	export LINK=$CXX
 	export PLATFORM=android
 	sh autogen.sh
 }
