a0777ffec29b690b3f3cef6f9cdeb93068e878b0
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-08 22:29:06 +0200

    libuv: Update from 1.13.0 to 1.13.1

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
-TERMUX_PKG_VERSION=1.13.0
+TERMUX_PKG_VERSION=1.13.1
 TERMUX_PKG_SRCURL=http://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=708e51f8845e1728a2df50d2aa8438dbd97a48d4a86fcbbcdead0c406d710315
+TERMUX_PKG_SHA256=824fe07476a9cab519d2d3e2be6d49f1faa1e7bb59224f0d81a7d6a307ce3937
 
 termux_step_pre_configure () {
 	export PLATFORM=android
