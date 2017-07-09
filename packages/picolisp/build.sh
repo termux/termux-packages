75ec26b30e90d743d4b06fd83e2628c694de3b34
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-10 01:31:21 +0200

    picolisp: Update from 17.6.6 to 17.7.9
    Also switch to using clang for building 64-bit picolisp.

--- packages/picolisp/build.sh
+++ packages/picolisp/build.sh
@@ -2,26 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://picolisp.com
 TERMUX_PKG_DESCRIPTION="Lisp interpreter and application server framework"
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 _PICOLISP_YEAR=17
-_PICOLISP_MONTH=6
-_PICOLISP_DAY=6
+_PICOLISP_MONTH=7
+_PICOLISP_DAY=9
 TERMUX_PKG_VERSION=${_PICOLISP_YEAR}.${_PICOLISP_MONTH}.${_PICOLISP_DAY}
 # We use our bintray mirror since old version snapshots are not kept on main site.
 TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/picolisp_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=aeb90c3002b5fb2a708d3bb189ed1c2d9c2e5b699c873c60689867672d04967e
+TERMUX_PKG_SHA256=22eff19214f3250099935dc871045a5be6728724ad91a0534b9c29bcd8fde858
 TERMUX_PKG_FOLDERNAME=picoLisp
 TERMUX_PKG_BUILD_IN_SRC=true
 # The assembly is not position-independent (would be a major rewrite):
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
 if [ $TERMUX_ARCH_BITS = 32 ]; then
 	# "Variable length array in structure won't be supported"
-	TERMUX_PKG_CLANG=no
-else
-	# FIXME: Use gcc for linking, as a clang build causes (tzo),
-	# the time zone offset, to return 0:
-	# Also, this call (and probably more) hangs:
-	# (call "termux-notification" "--title" "Title" "--content" "Message")
-	# These two problems only happen when using the gold linker, which
-	# Termux does by default).
 	TERMUX_PKG_CLANG=no
 fi
 
@@ -57,11 +49,14 @@ termux_step_make_install () {
 		$TERMUX_HOST_PLATFORM-as -o ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ext.o ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ext.s
 		$TERMUX_HOST_PLATFORM-as -o ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ht.o ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ht.s
 
+		# Use -fuse-ld=bfd to avoid using the gold linker (which Termux
+		# patches NDK to use by default) as it causes (tzo), the time
+		# zone offset, to always be 0 (and probably other problems):
 		$CC -o ../bin/picolisp ${TERMUX_PKG_EXTRA_MAKE_ARGS}.base.o \
-			-Wl,--no-as-needed -rdynamic -lc -lm -ldl -pie
+			-Wl,--no-as-needed -rdynamic -lc -lm -ldl -pie -fuse-ld=bfd
 		chmod +x ../bin/picolisp
-		$CC -o ../lib/ext -shared -rdynamic ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ext.o
-		$CC -o ../lib/ht -shared -rdynamic ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ht.o
+		$CC -o ../lib/ext -shared -rdynamic -fuse-ld=bfd ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ext.o
+		$CC -o ../lib/ht -shared -rdynamic -fuse-ld=bfd ${TERMUX_PKG_EXTRA_MAKE_ARGS}.ht.o
 	fi
 
 	mkdir -p $TERMUX_PREFIX/share/man/man1
