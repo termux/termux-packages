5a89c5e2fd2432a0e1db60335f29a631adac971e
Stephen <stephengroat@users.noreply.github.com>
2017-07-10 09:28:51 -0700

    Continuous output and failure
    some attempt at using dpkg for termux package installs instead of
    rebuilding from source

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
 TERMUX_PKG_VERSION=1.13.1
 TERMUX_PKG_SRCURL=http://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=824fe07476a9cab519d2d3e2be6d49f1faa1e7bb59224f0d81a7d6a307ce3937
-
 termux_step_pre_configure () {
 	export PLATFORM=android
 	sh autogen.sh
