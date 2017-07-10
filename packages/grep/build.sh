adcb0d0854233c6e5dfff7073869f85de06c81f0
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-10 02:16:37 +0200

    grep: Update from 3.0 to 3.1

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/grep/
 TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines containing a match to a specified pattern"
 TERMUX_PKG_DEPENDS="pcre"
-TERMUX_PKG_VERSION=3.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/grep/grep-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e2c81db5056e3e8c5995f0bb5d0d0e1cad1f6f45c3b2fc77b6e81435aed48ab5
+TERMUX_PKG_SHA256=db625c7ab3bb3ee757b3926a5cfa8d9e1c3991ad24707a83dde8a5ef2bf7a07e
