5b2e1e289b89bbe8d125890d7a9aac5fb590aa13
Joakim <git@joakim.fea.st>
2017-07-14 09:48:57 +0000

    ecj: Add android-16 API jar too

--- packages/ecj/build.sh
+++ packages/ecj/build.sh
@@ -33,6 +33,15 @@ termux_step_make () {
 	zip -q -r android.jar .
 
 	cp $TERMUX_PKG_TMPDIR/android-jar/android.jar $TERMUX_PREFIX/share/java/android.jar
+
+	# Bundle in an android.jar from an older API also, for those who want to
+	# build apps that run on older Android versions.
+	cp $ANDROID_HOME/platforms/android-16/android.jar android.jar
+	unzip -q android.jar
+	rm -Rf android.jar resources.arsc res assets
+	zip -q -r android-16.jar .
+	cp $TERMUX_PKG_TMPDIR/android-jar/android-16.jar $TERMUX_PREFIX/share/java/
+
 	rm -Rf $TERMUX_PREFIX/bin/javac
 	install $TERMUX_PKG_BUILDER_DIR/ecj $TERMUX_PREFIX/bin/ecj
 	perl -p -i -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PREFIX/bin/ecj
