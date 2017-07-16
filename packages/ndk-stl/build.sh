7874ff5b5c81740f7d54f1ca06601e23a7dfec48
its-pointless <bored@goat.si>
2017-07-16 15:43:56 +1000

    fixes building cstddef include for g++

--- packages/ndk-stl/build.sh
+++ packages/ndk-stl/build.sh
@@ -7,7 +7,8 @@ TERMUX_PKG_NO_DEVELSPLIT=yes
 termux_step_extract_into_massagedir () {
 	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 	cp -Rf $TERMUX_STANDALONE_TOOLCHAIN/include/c++/4.9.x/* $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
-
+	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
+	sed "s%\@TERMUX_HOST_PLATFORM\@%${TERMUX_HOST_PLATFORM}%g" $TERMUX_SCRIPTDIR/ndk-patches/cstddef.cpppatch | patch -p1 -R
 	# fenv.h is a C++ compatibility header which should be included with the compiler
 	#rm -Rf $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/{arm-linux-androideabi,tr1,tr2,fenv.h,complex.h,stdio.h,wctype.h}
 }
