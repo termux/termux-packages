71d2e0642909edeaf566c12302d2824361a81316
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-16 18:42:11 +0200

    angband: Update from 4.0.5 to 4.1.0

--- packages/angband/build.sh
+++ packages/angband/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://rephial.org/
 TERMUX_PKG_DESCRIPTION="Dungeon exploration adventure game"
-TERMUX_PKG_VERSION=4.0.5
-TERMUX_PKG_SRCURL=http://rephial.org/downloads/4.0/angband-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0d769a0f349842b0c78cbcd1804a9e08f064e75ca26b957710e4c2a3eb14f852
-TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+TERMUX_PKG_VERSION=4.1.0
+TERMUX_PKG_SRCURL=http://rephial.org/downloads/4.1/angband-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ea52266e52b66d6bf2ab3728b3bc6c7c3875130973430021e31bf56000c0df8b
+TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11 --bindir=$TERMUX_PREFIX/bin --sysconfdir=$TERMUX_PREFIX/share/angband"
 TERMUX_PKG_RM_AFTER_INSTALL="share/angband/xtra share/angband/icons"
+TERMUX_PKG_FOLDERNAME=angband-master
 
 termux_step_pre_configure () {
 	./autogen.sh
