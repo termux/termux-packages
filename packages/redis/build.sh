979088f593339dbca0119c7396587c758096a55c
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-16 15:10:10 +0200

    redis: Update from 3.2.9 to 4.0.0

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://redis.io/
-TERMUX_PKG_DESCRIPTION="Redis is an open source (BSD licensed), in-memory data structure store, used as a database, cache and message broker"
-TERMUX_PKG_VERSION=3.2.9
+TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache and message broker"
+TERMUX_PKG_VERSION=4.0.0
 TERMUX_PKG_SRCURL=http://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6eaacfa983b287e440d0839ead20c2231749d5d6b78bbe0e0ffa3a890c59ff26
+TERMUX_PKG_SHA256=d539ae309295721d5c3ed7298939645b6f86ab5d25fdf2a0352ab575c159df2d
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_CONFFILES="etc/redis.conf"
 
@@ -11,6 +11,10 @@ termux_step_pre_configure() {
     export USE_JEMALLOC=no
 
     LDFLAGS+=" -llog"
+
+    if [ $TERMUX_ARCH = "i686" ]; then
+	    sed -i 's/FINAL_LIBS=-lm/FINAL_LIBS=-lm -latomic/' $TERMUX_PKG_SRCDIR/src/Makefile
+    fi
 }
 
 termux_step_post_make_install() {
