b754bd2638f1645221cca7a3460d0a984914a48b
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-17 20:26:00 +0200

    Bump packages after apr and apr-util update

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://subversion.apache.org
 TERMUX_PKG_DESCRIPTION="Centralized version control system characterized by its simplicity"
 TERMUX_PKG_VERSION=1.9.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=dbcbc51fb634082f009121f2cb64350ce32146612787ffb0f7ced351aacaae19
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite"
