96a8efac530e3c1b857ca15b46e2afeeac9ba796
Henrik Grimler <Henrik@Grimler.se>
2017-07-22 23:58:44 +0200

    ghostscript: add libexpat as dependency

--- packages/ghostscript/build.sh
+++ packages/ghostscript/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interpreter for the PostScript language and for PDF"
 TERMUX_PKG_VERSION=9.21
 TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs${TERMUX_PKG_VERSION//.}/ghostpdl-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=82abf56e96e27cf4d1b17c0671f9ab3c5222454131588a49d06c97a332988e8d
-TERMUX_PKG_DEPENDS="libandroid-support, libtiff, libjpeg-turbo, libpng"
+TERMUX_PKG_DEPENDS="libandroid-support, libtiff, libjpeg-turbo, libpng, libexpat"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-system-libtiff \
 --enable-little-endian \
 --without-x \
@@ -11,8 +11,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-system-libtiff \
 CCAUX=gcc \
 --build=$TERMUX_BUILD_TUPLE \
 --without-pcl"
-TERMUX_PKG_BLACKLISTED_ARCHES="i686,x86_64"
-# Blacklisted until we have arch-i686.h and arch-x86_64.h
 
 termux_step_post_extract_package () {
         rm -rdf $TERMUX_PKG_SRCDIR/{jpeg,libpng,expat,jasper,freetype,lcms,tiff}
