eaa1dc91930e187f015bbca6e0581874d4db7453
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-26 15:12:00 +0200

    libconfuse: Update from 3.0 to 3.2

--- packages/libconfuse/build.sh
+++ packages/libconfuse/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/martinh/libconfuse
 TERMUX_PKG_DESCRIPTION="Small configuration file parser library for C"
-TERMUX_PKG_VERSION=3.0
+TERMUX_PKG_VERSION=3.2
+TERMUX_PKG_SHA256=166d1d0611de05587447d91fb44b31188cb229c9aaa5aff79ae4706e04dffa09
 TERMUX_PKG_SRCURL=https://github.com/martinh/libconfuse/releases/download/v$TERMUX_PKG_VERSION/confuse-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f1f326d9443103036d19c32d3f3efec3a85c3b081d99534463668d29992c4648
