3f16c2e382b22d855fead20a70b9a47cf22edbbf
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-26 13:53:12 +0200

    gifsicle: Update from 1.88 to 1.89

--- packages/gifsicle/build.sh
+++ packages/gifsicle/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.lcdf.org/gifsicle/
 TERMUX_PKG_DESCRIPTION="Tool for creating, editing, and getting information about GIF images and animations"
-TERMUX_PKG_VERSION=1.88
+TERMUX_PKG_VERSION=1.89
+TERMUX_PKG_SHA256=15b4cd27fff502c3769c245f71ed5484a38efd3f15106714a65db3a21b066f3e
 TERMUX_PKG_SRCURL=http://www.lcdf.org/gifsicle/gifsicle-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4585d2e683d7f68eb8fcb15504732d71d7ede48ab5963e61915201f9e68305be
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gifview"
