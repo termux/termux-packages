a3aa35d2ad1f563d85e07904afb8b5fe89bd0603
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-29 00:35:36 +0200

    imagemagick: Update from 7.0.6-0 to 7.0.6-3

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
-TERMUX_PKG_VERSION="7.0.6-0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="7.0.6-3"
 TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/ImageMagick-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=31587e2aa4d46301461329fd88d4ed6fccb0bd4ae786d629a153df72dbac89ee
+TERMUX_PKG_SHA256=720a9f60a414d35ba13bc6fe592842de27cc6061b03866096d04038d953c89fc
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-largefile
 --without-x
@@ -15,7 +14,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-lzma
 ac_cv_func_ftime=no
 ac_cv_header_complex_h=no"
-TERMUX_PKG_RM_AFTER_INSTALL="bin/Magick-config bin/MagickCore-config bin/MagickWand-config bin/Wand-config share/ImageMagick-6/francais.xml share/man/man1/Magick-config.1 share/man/man1/MagickCore-config.1 share/man/man1/MagickWand-config.1 share/man/man1/Wand-config.1"
+TERMUX_PKG_RM_AFTER_INSTALL="
+bin/Magick-config
+bin/MagickCore-config
+bin/MagickWand-config
+bin/Wand-config
+share/ImageMagick-6/francais.xml
+share/man/man1/Magick-config.1
+share/man/man1/MagickCore-config.1
+share/man/man1/MagickWand-config.1
+share/man/man1/Wand-config.1
+"
 TERMUX_PKG_DEPENDS="fftw, pango, glib, libbz2, libjpeg-turbo, liblzma, libpng, libtiff, libxml2, openjpeg, littlecms"
 
 termux_step_pre_configure() {
