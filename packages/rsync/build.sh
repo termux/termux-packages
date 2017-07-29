a64a2ee6e609d5e72bf63dedf0df5516b00de85f
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-29 21:43:26 +0200

    libpopt&rsync: Rebuild to fix __moddi3 problem
    Fixes #1178.

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://rsync.samba.org/
 TERMUX_PKG_DESCRIPTION="Utility that provides fast incremental file transfer"
 TERMUX_PKG_VERSION=3.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ecfa62a7fa3c4c18b9eccd8c16eaddee4bd308a76ea50b5c02a5840f09c0a1c2
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-rsyncd-conf=$TERMUX_PREFIX/etc/rsyncd.conf --with-included-zlib=no --disable-debug"
