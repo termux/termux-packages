2ea9ba2c8fd462ccd05b490ca728c0effd5451e1
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-29 02:17:29 +0200

    libmpdclient: Update from 2.11 to 2.13
    This is the first package to be built using the meson build system.

--- packages/libmpdclient/build.sh
+++ packages/libmpdclient/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.musicpd.org/libs/libmpdclient/
 TERMUX_PKG_DESCRIPTION="Asynchronous API library for interfacing MPD in the C, C++ & Objective C languages"
 TERMUX_PKG_MAINTAINER="Matthew Klein @mklein994"
 _MAIN_VERSION=2
-_PATCH_VERSION=11
+_PATCH_VERSION=13
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
+TERMUX_PKG_SHA256=5115bd52bc20a707c1ecc7587e6389c17305348e2132a66cf767c62fc55ed45d
 TERMUX_PKG_SRCURL=https://www.musicpd.org/download/libmpdclient/${_MAIN_VERSION}/libmpdclient-${_MAIN_VERSION}.${_PATCH_VERSION}.tar.xz
-TERMUX_PKG_SHA256=15fe693893c0d7ea3f4c35c4016fbd0332836164178b20983eec9b470846baf6
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-documentation"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-default-socket=${TERMUX_PREFIX}/var/run/mpd/socket"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -D default_socket==${TERMUX_PREFIX}/var/run/mpd/socket"
