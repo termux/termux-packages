192b8d2869008aef8288410a00a3d29cc074fbac
Gus Caplan <me@gus.host>
2017-07-30 13:26:05 -0500

    add undocumented api commands

--- disabled-packages/nodejs-current/termux.js
+++ disabled-packages/nodejs-current/termux.js
@@ -2,18 +2,27 @@ const util = require('util');
 const child_process = require('child_process');
 const pexec = util.promisify(child_process.exec);
 
-const exec = function exec(command, argv = [], options = {}) {
-  const args = Object.entries(options).reduce((a, [k, v]) => {
-    const key = k[0];
-    if (v === true) a.push('-' + key);
-    else a.push(`-${key} ${v}`);
-    return a;
-  }, []).join(' ');
-  argv = Array.isArray(argv) ? argv.length ? ` ${argv.join(' ')}` : '' : ` ${argv}`;
-  return pexec(`${command}${argv} ${args}`)
-    .then(({ stdout, stderr }) =>
-      ({ stdout: JSON.parse(stdout), stderr: JSON.parse(stderr) }));
-};
+
+function bind(command, { json = false } = {}) {
+  return function(argv, options) {
+    if (!options && argv && !Array.isArray(argv) && typeof argv !== 'string') {
+      options = argv;
+      argv = [];
+    }
+    const args = Object.entries(options).reduce((a, [k, v]) => {
+      const key = k[0];
+      if (v === true) a.push('-' + key);
+      else a.push(`-${key} ${v}`);
+      return a;
+    }, []).join(' ');
+    argv = Array.isArray(argv) ? argv.length ? ` ${argv.join(' ')}` : '' : ` ${argv}`.trim();
+    pexec(`${command}${argv} ${args}`.trim())
+      .then((ret) => {
+        if (json) return { stdout: JSON.parse(ret.stdout), stderr: JSON.parse(ret.stderr) };
+        return ret;
+      });
+  }
+}
 
 const API_COMMANDS = [
   'battery-status',
@@ -31,23 +40,17 @@ const API_COMMANDS = [
   'share',
   'sms-inbox',
   'sms-send',
+  'storage-get',
+  'telephony-call',
   'telephony-cellinfo',
   'telephony-deviceinfo',
   'toast',
   'tts-engines',
   'tts-speak',
   'vibrate',
+  'wifi-connectioninfo',
+  'wifi-scaninfo',
 ];
-
-function bind(command) {
-  return function(args, options) {
-    if (!options && args && !Array.isArray(args) && typeof args !== 'string') {
-      options = args;
-      args = [];
-    }
-    return exec(command, args, options);
-  }
-}
 
 module.exports.api = {};
 for (const command of API_COMMANDS) {
@@ -58,7 +61,17 @@ for (const command of API_COMMANDS) {
     if (!value[prop]) value[prop] = {};
     value = value[prop];
   }
-  value[last] = bind(`termux-${command}`);
+  value[last] = bind(`termux-${command}`, { json: true });
 }
+
+module.exports.wake = {
+  lock: bind('termux-wake-lock'),
+  unlock: bind('termux-wake-unlock'),
+};
+
+module.exports.open = bind('termux-open');
+module.exports.open.url = bind('termux-open-url');
+
+module.exports.fixShebang = bind('termux-fix-shebang');
 
 module.exports.playAudio = bind('play-audio');
