adb80cd537ed8ea7d8a1ae9d0e6387def0fb2c50
Stephen <stephengroat@users.noreply.github.com>
2017-07-30 15:50:18 -0700

    add apr-1-config to apr-dev
    required for autoconf detection of apr

--- packages/apr/build.sh
+++ packages/apr/build.sh
@@ -7,4 +7,5 @@ TERMUX_PKG_DEPENDS="libuuid"
 TERMUX_PKG_BUILD_IN_SRC="yes"
 # "ac_cv_search_crypt=" to avoid needlessly linking to libcrypt.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-installbuilddir=$TERMUX_PKG_TMPDIR ac_cv_file__dev_zero=yes ac_cv_func_setpgrp_void=yes apr_cv_process_shared_works=no apr_cv_tcp_nodelay_with_cork=yes ac_cv_sizeof_struct_iovec=8 ac_cv_search_crypt="
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/apr-1-config"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/apr-1-config lib/apr.exp"
