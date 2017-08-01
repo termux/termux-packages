51a245de2be8b61a4a3ecf02134803ae50d58589
Henrik Grimler <Henrik@Grimler.se>
2017-08-01 18:33:16 +0200

    texlive: minor clean up of texlive/build.sh. Update patches to avoid (silent) complaints from patching. Add previously forgotten install-tl.list with list of files to take from install-tl-unx.tar.gz.

--- packages/texlive/build.sh
+++ packages/texlive/build.sh
@@ -13,7 +13,7 @@ TERMUX_PKG_SHA256=("3f63708b77f8615ec6f2f7c93259c5f584d1b89dd335a28f2362aef9e6f0
 "afe49758c26fb51c2fae2e958d3f0c447b5cc22342ba4a4278119d39f5176d7f"
 "d4e07ed15dace1ea7fabe6d225ca45ba51f1cb7783e17850bc9fe3b890239d6d")
 TERMUX_PKG_DEPENDS="wget, perl, xz-utils, gnupg2, texlive-bin (>= 20170524)"
-TERMUX_PKG_FOLDERNAME=("texlive-$_MAJOR_VERSION-texmf" 
+TERMUX_PKG_FOLDERNAME=("texlive-$_MAJOR_VERSION-texmf"
 "texlive-$_MAJOR_VERSION-extra"
 "install-tl-$_MAJOR_VERSION")
 TL_FILE_LISTS=("texlive-texmf.list"
@@ -25,15 +25,15 @@ TL_ROOT=$TERMUX_PREFIX/opt/texlive/${TERMUX_PKG_VERSION:0:4}
 TL_BINDIR=$TL_ROOT/bin/custom
 
 termux_step_extract_package() {
-        mkdir -p "$TERMUX_PKG_SRCDIR"
+	mkdir -p "$TERMUX_PKG_SRCDIR"
 	
 	cd "$TERMUX_PKG_TMPDIR"
-	for index in $(seq 0 2); do 
+	for index in $(seq 0 2); do
 		local filename
 		filename=$(basename "${TERMUX_PKG_SRCURL[$index]}")
 		local file="$TERMUX_PKG_CACHEDIR/$filename"
 		termux_download "${TERMUX_PKG_SRCURL[$index]}" "$file" "${TERMUX_PKG_SHA256[$index]}"
-			
+		
 		folder=${TERMUX_PKG_FOLDERNAME[$index]}
 		
 		rm -Rf $folder
