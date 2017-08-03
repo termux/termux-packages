e6b7ea9ee1cddf4813c70bbec03c5465e0baba53
Stephen <stephengroat@users.noreply.github.com>
2017-08-03 07:54:01 -0700

    include libassuan-config in libassuan-dev

--- packages/libassuan/build.sh
+++ packages/libassuan/build.sh
@@ -3,5 +3,5 @@ TERMUX_PKG_DESCRIPTION="Library implementing the Assuan IPC protocol used betwee
 TERMUX_PKG_VERSION=2.4.3
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=22843a3bdb256f59be49842abf24da76700354293a066d82ade8134bb5aa2b71
-TERMUX_PKG_RM_AFTER_INSTALL="bin/libassuan-config"
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/libassuan-config"
 TERMUX_PKG_DEPENDS="libgpg-error"
