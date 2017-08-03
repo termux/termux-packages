847ddf20521b55ed6495a0e57173e106be131584
Vladimir Serbinenko <phcoder@google.com>
2017-08-03 03:58:27 +0200

    mtools: New package
    This is set of tools used to manipulate FAT images in userspace.

---
+++ packages/mtools/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/mtools/
+TERMUX_PKG_DESCRIPTION="Tool for manipulating FAT images."
+TERMUX_PKG_VERSION=4.0.18
+TERMUX_PKG_SRCURL=ftp://ftp.gnu.org/gnu/mtools/mtools-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=59e9cf80885399c4f229e5d87e49c0c2bfeec044e1386d59fcd0b0aead6b2f85
+TERMUX_PKG_DEPENDS="libandroid-support"
+
