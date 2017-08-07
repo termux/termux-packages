b6488fce33fc432bb724eccb856e6fb2e6249cfc
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-08-07 18:55:27 +0300

    tor: update from 0.3.0.9 to 0.3.0.10

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_DEPENDS="libevent, openssl"
-TERMUX_PKG_VERSION=0.3.0.9
+TERMUX_PKG_VERSION=0.3.0.10
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=48d4880bf6ccb19ce9af2abde6946d7cf0635cc807548badbf4a221a79581e42
+TERMUX_PKG_SHA256=9a8e6e49a1688dae64dca10f84a414ec9a4f393fb2256ae28e0c2e3239185ab1
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 
