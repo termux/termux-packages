7dcbf4089d3ba305cb274eacb631932176280402
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-07 22:20:59 +0200

    apr&apr-util: Bump revision after package updates

--- packages/apr-util/build.sh
+++ packages/apr-util/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://apr.apache.org/
 TERMUX_PKG_DESCRIPTION="Apache Portable Runtime Utility Library"
 TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_DEPENDS="apr, libexpat"
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net/apr/apr-util-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=8474c93fa74b56ac6ca87449abe3e155723d5f534727f3f33283f6631a48ca4c
--- packages/apr/build.sh
+++ packages/apr/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://apr.apache.org/
 TERMUX_PKG_VERSION=1.6.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net/apr/apr-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=09109cea377bab0028bba19a92b5b0e89603df9eab05c0f7dbd4dd83d48dcebd
 TERMUX_PKG_DESCRIPTION="Apache Portable Runtime - library providing a predictable and consistent interface to underlying platform-specific implementations"
