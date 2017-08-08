51815cc1dca4a0116354651180dfd56671c84839
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-08 14:26:08 +0200

    libksba: Bump revision after package update

--- packages/libksba/build.sh
+++ packages/libksba/build.sh
@@ -1,6 +1,7 @@
-TERMUX_PKG_HOMEPAGE=http://gnupg.org/related_software/libksba/
+TERMUX_PKG_HOMEPAGE=https://gnupg.org/related_software/libksba/
 TERMUX_PKG_DESCRIPTION="Library for using X.509 certificates and CMS (Cryptographic Message Syntax) easily accessible"
 TERMUX_PKG_VERSION=1.3.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/libksba/libksba-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=41444fd7a6ff73a79ad9728f985e71c9ba8cd3e5e53358e70d5f066d35c1a340
 TERMUX_PKG_DEPENDS="libgpg-error"
