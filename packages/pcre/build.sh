642d0f91b7cd5f6165fb0e41495e6501f8af962a
Stephen <stephengroat@users.noreply.github.com>
2017-08-08 14:26:32 -0700

    bump version

--- packages/pcre/build.sh
+++ packages/pcre/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.pcre.org/
 TERMUX_PKG_DESCRIPTION="Library implementing regular expression pattern matching using the same syntax and semantics as Perl 5"
 TERMUX_PKG_VERSION=8.41
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://ftp.pcre.org/pub/pcre/pcre-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/pcre-config"
