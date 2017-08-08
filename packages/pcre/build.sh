605df489a6265425534c3cb743a497bb4450536d
Stephen <stephengroat@users.noreply.github.com>
2017-08-07 21:34:16 -0700

    add pcre-config to pcre-dev
    for apache2
    
    #1169

--- packages/pcre/build.sh
+++ packages/pcre/build.sh
@@ -3,5 +3,6 @@ TERMUX_PKG_DESCRIPTION="Library implementing regular expression pattern matching
 TERMUX_PKG_VERSION=8.41
 TERMUX_PKG_SRCURL=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530
-TERMUX_PKG_RM_AFTER_INSTALL="bin/pcre-config bin/pcregrep bin/pcretest share/man/man1/pcre*.1 lib/libpcreposix.so lib/libpcreposix.so.0 lib/libpcreposix.so.0.0.2"
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/pcre-config"
+TERMUX_PKG_RM_AFTER_INSTALL="bin/pcregrep bin/pcretest share/man/man1/pcre*.1 lib/libpcreposix.so lib/libpcreposix.so.0 lib/libpcreposix.so.0.0.2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-cpp --enable-jit --enable-utf8 --enable-unicode-properties"
