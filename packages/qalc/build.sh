bc0e66beff41b3ce10265ac0533011f4794fb911
David Martinez <contacto@davidmartinez.net>
2017-08-10 00:23:25 +0200

    qalc: update from 9.9.11 to 1.0.0

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://qalculate.github.io/
 TERMUX_PKG_DESCRIPTION="Powerful and easy to use command line calculator"
-TERMUX_PKG_VERSION=0.9.11
+TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v${TERMUX_PKG_VERSION}/libqalculate-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fb5014b04e0f80d85d85d96974c4d069a14791cb2d6aef016c72a24e30917f14
+TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v${TERMUX_PKG_VERSION}a/libqalculate-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7cc5a67356374d7f4fb20619dc4dd7976bbe9ae8d5bc3a40b44c46264de9549b
 TERMUX_PKG_DEPENDS="glib, gnuplot, libcln, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
