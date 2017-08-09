f2b37eb861a2694cf499f23032152139beda568e
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-09 23:45:46 +0200

    openssl: Enable PSK
    Also rebuild stunnel with PSK support (fixes #1235).

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.stunnel.org/
 TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary applications"
 TERMUX_PKG_VERSION=5.42
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=1b6a7aea5ca223990bc8bd621fb0846baa4278e1b3e00ff6eee279cb8e540fab
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="openssl, libutil"
