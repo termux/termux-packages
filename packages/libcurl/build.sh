5218e97e85be962b055242de23095d628b3c4710
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-11 00:50:45 +0200

    libcurl: Bump revision after package update

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl, libnghttp2"
 TERMUX_PKG_VERSION=7.55.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=af1d69ec6f15fe70a2cabaa98309732bf035ef2a735e4e1a3e08754d2780e5b1
 TERMUX_PKG_SRCURL=http://curl.askapache.com/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
