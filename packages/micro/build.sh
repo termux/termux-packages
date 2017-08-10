c0dce434c7b44e382cadfbe2b728ec8d8e7bca32
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-11 00:26:18 +0200

    micro: Update from 1.2.0 to 1.3.1

--- packages/micro/build.sh
+++ packages/micro/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://micro-editor.github.io/
 TERMUX_PKG_DESCRIPTION="Modern and intuitive terminal-based text editor"
-TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_SHA256=abf9f65107fac696fb5d0525ed5a3669724910f8300e0c67ee3ff89c243a57e3
+TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_SHA256=fdede583ea2f67588c42be30a820699acc376d59f0652ca0b50c9120511f2caf
 TERMUX_PKG_SRCURL=https://github.com/zyedidia/micro/releases/download/v${TERMUX_PKG_VERSION}/micro-${TERMUX_PKG_VERSION}-src.tar.gz
 TERMUX_PKG_FOLDERNAME=micro
-TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_make() {
 	return
@@ -13,12 +12,14 @@ termux_step_make() {
 termux_step_make_install() {
 	termux_setup_golang
 
-	local HASH="$(git rev-parse --short HEAD)"
-	local VERSION="$TERMUX_PKG_VERSION"
-	local DATE=`date -I`
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	local MICRO_SRC=$GOPATH/src/github.com/zyedidia/micro
 
-	go get -d ./cmd/micro
-	go build -ldflags "-s -w -X main.Version=$VERSION -X main.CommitHash=$HASH -X 'main.CompileDate=$DATE'" \
-		-o $TERMUX_PREFIX/bin/micro \
-		./cmd/micro
+	cd $TERMUX_PKG_SRCDIR
+	mkdir -p $MICRO_SRC
+	cp -R . $MICRO_SRC
+
+	cd $MICRO_SRC
+	make build-quick
+	mv micro $TERMUX_PREFIX/bin/micro
 }
