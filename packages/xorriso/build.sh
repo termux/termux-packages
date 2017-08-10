0eb3f709c9ff6fcd193d177d8d270d3bcc66d098
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-10 02:32:23 +0200

    xorriso: Depend on readline&libbz2

--- packages/xorriso/build.sh
+++ packages/xorriso/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tool for creating ISO files."
 TERMUX_PKG_VERSION=1.4.7
 TERMUX_PKG_SRCURL=https://www.gnu.org/software/xorriso/xorriso-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b54997b71b979768b19ddc38ff15d773ce44cf449e63fb453e20be9d687d396a
-TERMUX_PKG_DEPENDS="iconv, libandroid-support"
+TERMUX_PKG_DEPENDS="iconv, libandroid-support, readline, libbz2"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-jtethreads"
 
