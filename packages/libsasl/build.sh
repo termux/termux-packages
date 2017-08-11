5838845ea6bf3e007f7ea86e8cb3584cc0c3ef07
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-11 13:12:50 +0200

    libsasl: Build with --disable-gssapi
    This avoids trying to link in libgssapi if kerberas has been built.

--- packages/libsasl/build.sh
+++ packages/libsasl/build.sh
@@ -5,12 +5,18 @@ TERMUX_PKG_SRCURL=ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-${TERMUX_PKG_VER
 TERMUX_PKG_SHA256=8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3
 # Seems to be race issues in build (symlink creation)::
 TERMUX_MAKE_PROCESSES=1
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" ac_cv_func_syslog=no ac_cv_header_syslog_h=no"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --without-des --disable-otp"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-plugindir=$TERMUX_PREFIX/lib/sasl2 --sysconfdir=$TERMUX_PREFIX/etc"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-dblib=none"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-dbpath=$TERMUX_PREFIX/var/lib/sasldb --with-saslauthd=$TERMUX_PREFIX/var/run/saslauthd"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --without-saslauthd"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_func_syslog=no
+ac_cv_header_syslog_h=no
+--disable-gssapi
+--disable-otp
+--sysconfdir=$TERMUX_PREFIX/etc
+--with-dblib=none
+--with-dbpath=$TERMUX_PREFIX/var/lib/sasldb
+--without-des
+--without-saslauthd
+--with-plugindir=$TERMUX_PREFIX/lib/sasl2
+"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/pluginviewer"
 
 termux_step_post_configure () {
