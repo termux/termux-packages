13f3261b285c76de6cad21db4af1e41181bfdea1
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-11 15:00:43 +0200

    libsoup: Update from 2.58.0 to 2.58.2
    - Avoid linking in kerberos.
    - Depend on libxml2.

--- packages/libsoup/build.sh
+++ packages/libsoup/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE="https://wiki.gnome.org/action/show/Projects/libsoup"
 TERMUX_PKG_DESCRIPTION="libsoup is an HTTP client/server library for GNOME"
-TERMUX_PKG_VERSION=2.58.0
-TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/libsoup/2.58/libsoup-2.58.0.tar.xz
-TERMUX_PKG_SHA256=b61567e25ed61f4b89bb23a36713c807df6b76a8451beb786d8cc362e8f097f5
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-vala"
-TERMUX_PKG_DEPENDS="glib, krb5, libffi, libsqlite"
+TERMUX_PKG_VERSION=2.58.2
+TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/libsoup/${TERMUX_PKG_VERSION:0:4}/libsoup-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=442300ca1b1bf8a3bbf2f788203287ff862542d4fc048f19a92a068a27d17b72
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-vala --without-gssapi"
+TERMUX_PKG_DEPENDS="glib, libsqlite, libxml2"
