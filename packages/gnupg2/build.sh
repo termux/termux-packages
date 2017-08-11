017732e3b80ca788f37c0b1ef1136f67907685cc
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-11 17:55:20 +0200

    gnupg2: Update from 2.1.22 to 2.1.23

--- packages/gnupg2/build.sh
+++ packages/gnupg2/build.sh
@@ -1,10 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/
 TERMUX_PKG_DESCRIPTION="Implementation of the OpenPGP standard for encrypting and signing data and communication"
-TERMUX_PKG_VERSION=2.1.22
+TERMUX_PKG_VERSION=2.1.23
+TERMUX_PKG_SHA256=a94476391595e9351f219188767a9d6ea128e83be5ed3226a7890f49aa2d0d77
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=46716faf9e1b92cfca86609f3bfffbf5bb4b6804df90dc853ff7061cfcfb4ad7
 TERMUX_PKG_DEPENDS="libassuan,libbz2,libgcrypt,libksba,libnpth,readline,pinentry"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ldap --disable-sqlite"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-ldap
+--disable-sqlite
+--enable-gpg-is-gpg2
+"
 # Remove non-english help files and man pages shipped with the gnupg (1) package:
 TERMUX_PKG_RM_AFTER_INSTALL="share/gnupg/help.*.txt share/man/man1/gpg-zip.1 share/man/man7/gnupg.7"
 
