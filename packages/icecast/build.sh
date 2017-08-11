3f38f8361d7550973064a33e14b65fe88e021d72
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-11 12:51:30 +0200

    icecast: Add source checksum

--- packages/icecast/build.sh
+++ packages/icecast/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://icecast.org
+TERMUX_PKG_HOMEPAGE=https://icecast.org
 TERMUX_PKG_DESCRIPTION="Icecast is a streaming media (audio/video) server"
 TERMUX_PKG_VERSION=2.4.3
-TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/icecast/icecast-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SRCURL=https://downloads.xiph.org/releases/icecast/icecast-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=c85ca48c765d61007573ee1406a797ae6cb31fb5961a42e7f1c87adb45ddc592
 TERMUX_PKG_DEPENDS="libcurl, libgnutls, libogg, libvorbis, libxml2, libxslt, mime-support, openssl"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
