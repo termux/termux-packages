f09de70a4c6a7d08e80d32e82fd44130b467a77b
Stephen <stephengroat@users.noreply.github.com>
2017-08-14 11:18:36 -0700

    add libs to qalc
    required for #1169, should not affect current build process

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -6,3 +6,7 @@ TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v$
 TERMUX_PKG_SHA256=7cc5a67356374d7f4fb20619dc4dd7976bbe9ae8d5bc3a40b44c46264de9549b
 TERMUX_PKG_DEPENDS="glib, gnuplot, libcln, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lintl -landroid-support -liconv"
+}
