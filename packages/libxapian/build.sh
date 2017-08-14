c87e861bae3d1b01535ec676ec7d84beaea54102
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-15 00:04:11 +0200

    libxapian: Bump revision after change

--- packages/libxapian/build.sh
+++ packages/libxapian/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://xapian.org
 TERMUX_PKG_DESCRIPTION="Xapian search engine library"
 TERMUX_PKG_VERSION=1.4.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://oligarchy.co.uk/xapian/${TERMUX_PKG_VERSION}/xapian-core-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a6a985a9841a452d75cf2169196b7ca6ebeef27da7c607078cd401ad041732d9
 # ac_cv_c_bigendian=no: We can use /proc/sys/kernel/random/uuid and do not need libuuid.
