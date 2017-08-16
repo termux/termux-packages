eaa7b4910796e8181cddc294af119ee8c56db45f
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-17 00:34:44 +0200

    Make libintl.h provide inline stubs
    Previously libandroid-support contained inline stubs for libintl.h
    functions (gettext(), dgettext() and friends).
    
    We now provide inline versions of them in libintl.h directly so
    libandroid-support is no longer necessary to get them.

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
-TERMUX_PKG_VERSION=21
-TERMUX_PKG_SHA256=e355c822d0891ba610275eac4b13e9b0fb4dd57e8e2891b9fd98d11edc6fd40d
+TERMUX_PKG_VERSION=22
+TERMUX_PKG_SHA256=667f20d0821a6305c50c667363486d546b293e846f31d02f559947d50121f51e
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_FOLDERNAME=libandroid-support-$TERMUX_PKG_VERSION
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -17,11 +17,11 @@ termux_step_make_install () {
 
 	cp libandroid-support.so $TERMUX_PREFIX/lib/
 
-	(cd $TERMUX_PREFIX/lib; ln -f -s libandroid-support.so libiconv.so; ln -f -s libandroid-support.so libintl.so)
+	(cd $TERMUX_PREFIX/lib; ln -f -s libandroid-support.so libiconv.so; )
 
 	rm -Rf $TERMUX_PREFIX/include/libandroid-support
 	mkdir -p $TERMUX_PREFIX/include/libandroid-support
 	cp -Rf include/* $TERMUX_PREFIX/include/libandroid-support/
 
-	(cd $TERMUX_PREFIX/include; ln -f -s libandroid-support/libintl.h libintl.h; ln -f -s libandroid-support/iconv.h iconv.h)
+	(cd $TERMUX_PREFIX/include; ln -f -s libandroid-support/iconv.h iconv.h)
 }
