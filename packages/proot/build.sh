2f90522ad337a206e8e0a219b05b756f77c5b951
Oliver Schmidhauser <oli@glow.li>
2017-08-16 09:08:12 +0200

    termux-chroot: Specify command to execute Allow a command to be specified when running termux-chroot. Example: ``` $ termux-chroot ls / bin  data  dev  etc  home  lib  proc  property_contexts  share  storage  system  tmp  usr  var  vendor ``` This makes it possible to run scripts that include a part that needs to be run in a chrooted environment, like a compile script. ``` ./configure make termux-chroot make install ```
    Previously it had to be done like this:
    ```
    ./configure
    make
    termux-chroot
    cd program
    make install
    exit
    ```

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root 
 # Just bump commit and version when needed:
 _COMMIT=ccf3d7ab4804c1d01dd4d8f970da033a872c3152
 TERMUX_PKG_VERSION=5.1.106
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=16c2a450dae0c321c2b949bab322ebcd8bff4d78ce989ef3024f2235d2b7fbbf
 TERMUX_PKG_FOLDERNAME=proot-$_COMMIT
