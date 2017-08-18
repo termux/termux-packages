136219a8ff3b6b245943b7c9c710a9eae2089323
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-18 21:00:28 +0200

    xorriso: Update to new development snapshot

--- packages/xorriso/build.sh
+++ packages/xorriso/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/xorriso
 TERMUX_PKG_DESCRIPTION="Tool for creating ISO files."
 TERMUX_PKG_VERSION=1.4.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.gnu.org/software/xorriso/xorriso-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b54997b71b979768b19ddc38ff15d773ce44cf449e63fb453e20be9d687d396a
+TERMUX_PKG_SHA256=8c684ae685a5d3151db0f80e60ec4e96aca99c412d5ec6f65965a06ee30cf5e0
 TERMUX_PKG_DEPENDS="iconv, libandroid-support, readline, libbz2"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-jtethreads"
