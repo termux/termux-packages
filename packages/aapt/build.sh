79e16f68e41ce0280caf5e3d60ed66d11bbcb024
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-23 21:59:09 +0200

    aapt: Remove __android_log_error_write reference
    Removing a reference to __android_log_error_write hopefully fixes
    an issue on Android 5 (see #1306).

--- packages/aapt/build.sh
+++ packages/aapt/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_DESCRIPTION="Android Asset Packaging Tool"
 _TAG_VERSION=7.1.2
 _TAG_REVISION=33
 TERMUX_PKG_VERSION=${_TAG_VERSION}.${_TAG_REVISION}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="libexpat, libpng, libzopfli"
 
@@ -198,7 +199,7 @@ termux_step_make_install () {
 		zip_archive.cc \
 		zip_archive_stream_entry.cc \
 		zip_writer.cc"
-	sed -i 's%next_in = reinterpret_cast<const uint8_t\*>(data)%next_in = const_cast<uint8_t\*>(reinterpret_cast<const uint8_t\*>(data))%' zip_writer.cc
+	patch -p0 < $TERMUX_PKG_BUILDER_DIR/libziparchive.patch.txt
 	$CXX $CXXFLAGS $LDFLAGS -std=c++11 \
 		-DZLIB_CONST \
 		-isystem $AOSP_INCLUDE_DIR \
