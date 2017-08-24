f01e68f137c88f2346a3fb6ade48d452c6c4dc68
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-24 20:59:18 +0200

    datamash: Update from 1.1.1 to 1.2

--- packages/datamash/build.sh
+++ packages/datamash/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/datamash/
 TERMUX_PKG_DESCRIPTION="GNU datamash performs basic numeric,textual and statistical operations"
-TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_SHA256=e8d46fb22ccc77e5380f26cde622a733f363d388b04a2c22e7fb6de0e9d85996
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/datamash/datamash-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=420819b3d7372ee3ce704add847cff7d08c4f8176c1d48735d4a632410bb801b
 TERMUX_PKG_BUILD_IN_SRC=yes
