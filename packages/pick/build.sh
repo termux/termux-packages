b4c24083c7cb08e604365a4ee0df2a6b8289721a
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-28 12:06:50 +0200

    pick: Update from 1.7.0 to 1.8.0

--- packages/pick/build.sh
+++ packages/pick/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/calleerlandsson/pick
 TERMUX_PKG_DESCRIPTION="Utility to choose one option from a set of choices with fuzzy search functionality"
-TERMUX_PKG_VERSION=1.7.0
-TERMUX_PKG_SHA256=950531c56edc4be375fe4e89291caa807322b298143043e2e2963de34e96de15
+TERMUX_PKG_VERSION=1.8.0
+TERMUX_PKG_SHA256=7834d3aef9e575ce07414f961d1f024776b49bb23c5dc3b7bb8f6b734131067d
 TERMUX_PKG_SRCURL=https://github.com/calleerlandsson/pick/releases/download/v${TERMUX_PKG_VERSION}/pick-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_search_setupterm=-lncurses"
