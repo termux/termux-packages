f8a432bf5da293fff36a996a7c1b19b55992a265
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-28 14:56:29 +0200

    mc: Remove no longer needed CPPFLAG

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -6,9 +6,3 @@ TERMUX_PKG_SRCURL="http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.ta
 TERMUX_PKG_SHA256=eb9e56bbb5b2893601d100d0e0293983049b302c5ab61bfb544ad0ee2cc1f2df
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
-
-termux_step_pre_configure() {
-	# mc uses the deprecated S_IWRITE definition, which android does not define:
-	# https://code.google.com/p/android/issues/detail?id=19710
-	CPPFLAGS+=" -DS_IWRITE=S_IWUSR"
-}
