8577a78e1d5bda7f2ef70378766a182299caffbd
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-29 23:17:31 +0200

    gnutls: Update from 3.5.14 to 3.5.15

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -1,11 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn, libunistring"
-_TERMUX_PKG_MAJOR_VERSION=3.5
-TERMUX_PKG_VERSION=${_TERMUX_PKG_MAJOR_VERSION}.14
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${_TERMUX_PKG_MAJOR_VERSION}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4aa12dec92f42a0434df794aca3d02f6f2a35b47b48c01252de65f355c051bda
+TERMUX_PKG_VERSION=3.5.15
+TERMUX_PKG_SHA256=046081108b8b1fe455a13a4c5a4eaa0368e185b678f1670fe09a11a2d7ecfad5
+TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${TERMUX_PKG_VERSION:0:3}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-cxx
 --disable-hardware-acceleration
@@ -14,7 +12,3 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-included-libtasn1
 --without-p11-kit
 "
-
-termux_step_pre_configure() {
-	CFLAGS+=" -std=c99"
-}
