d19c66f7854c78ab8b2e15d7a8c92a6eb080a0b6
Fredrik Fornwall <fredrik@fornwall.net>
2017-08-31 15:56:20 +0200

    qalc: Avoid depending on gnuplot

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://qalculate.github.io/
 TERMUX_PKG_DESCRIPTION="Powerful and easy to use command line calculator"
 TERMUX_PKG_VERSION=2.0.0
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v${TERMUX_PKG_VERSION}/libqalculate-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=86d04362f37aa5acbc78108e0044b96fbffbaa33c309c19c8c37ac4fb46c5485
-ERMUX_PKG_DEPENDS="gnuplot, libcurl, libgmp, libmpfr, libxml2, iconv, ncurses"
-TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="libcurl, libmpfr, libxml2, readline"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-icu"
