96debdd275b3b517e5766a2a82d4cd855c8410ed
olg <olg@arbeitstierchen.fritz.box>
2017-09-01 22:35:55 +0200

    clinfo,clpeak: add packages

---
+++ packages/clinfo/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_VERSION=2.2.17.08.25
+TERMUX_PKG_HOMEPAGE=https://github.com/Oblomov/clinfo
+TERMUX_PKG_DESCRIPTION="An application that enumerates the properties of OpenCL platforms and devices"
+_COMMIT=$TERMUX_PKG_VERSION
+TERMUX_PKG_SRCURL="https://github.com/Oblomov/clinfo/archive/${_COMMIT}.zip"                                       
+TERMUX_PKG_FOLDERNAME=clinfo-${_COMMIT}
+TERMUX_PKG_DEPENDS="libopencl-stub"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+
+termux_step_pre_configure() 
+{	
+	LDFLAGS+=" -ldl"
+}
+
+
+termux_step_make_install () {
+	cp clinfo $TERMUX_PREFIX/bin
+}
---
+++ packages/clpeak/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_HOMEPAGE=https://github.com/krrishnarraj/clpeak
+TERMUX_PKG_DESCRIPTION="A synthetic benchmarking tool to measure peak capabilities of a opencl device. This tool is intended toward developers to fine-tune opencl kernels for a particular device/class of device."
+_COMMIT=8edab23fbc867adbada21378d65774c670c2aaf9                                      
+TERMUX_PKG_SRCURL="https://github.com/krrishnarraj/clpeak/archive/${_COMMIT}.zip"                                       
+TERMUX_PKG_FOLDERNAME=clpeak-${_COMMIT}
+TERMUX_PKG_DEPENDS="libopencl-stub"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_make_install () {        
+	cp clpeak $TERMUX_PREFIX/bin
+}
