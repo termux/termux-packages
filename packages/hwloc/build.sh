32ccb5f877bf7430dcdfc2a6fcd32e2b27a44ce8
olg <olg@arbeitstierchen.fritz.box>
2017-09-01 22:30:44 +0200

    hwloc: new package

---
+++ packages/hwloc/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://www.open-mpi.de/software/hwloc
+TERMUX_PKG_DESCRIPTION="The Portable Hardware Locality (hwloc) software package provides a portable abstraction of the hierarchical topology of modern architectures, including NUMA memory nodes, sockets, shared caches, cores and simultaneous multithreading."
+TERMUX_PKG_VERSION=1.11.7
+TERMUX_PKG_SRCURL="http://www.open-mpi.de/software/hwloc/v1.11/downloads/hwloc-$TERMUX_PKG_VERSION.tar.bz2"
+TERMUX_PKG_SHA256=ab6910e248eed8c85d08b529917a6aae706b32b346e886ba830895e36a809729
+
+
+
