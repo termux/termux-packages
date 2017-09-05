62fd5afdacc2814d2f18da1c9e57d18169775d0b
Henrik Grimler <Henrik@Grimler.se>
2017-09-05 21:11:38 +0200

    metasploit: patches instead of sed

--- packages/metasploit-framework/build.sh
+++ packages/metasploit-framework/build.sh
@@ -15,15 +15,9 @@ termux_step_make () {
 }
 
 termux_step_post_massage() {
-        mv $TERMUX_PREFIX/share/metasploit-framework-4.16.4 $TERMUX_PREFIX/share/metasploit-framework
-        cd $PREFIX/share/metasploit-framework
-        sed '/rbnacl/d' -i Gemfile.lock
-        sed '/rbnacl/d' -i metasploit-framework.gemspec
         gem install --install-dir TERMUX_PREFIX/lib/ruby/gems/2.4.0 bundler
 	
         gem install nokogiri -- --use-system-libraries --install-dir $TERMUX_PREFIX/lib/ruby/gems/2.4.0
-
-        sed 's|grpc (.*|grpc (1.4.1)|g' -i $TERMUX_PREFIX/share/metasploit-framework/Gemfile.lock
         gem unpack grpc -v 1.4.1
         cd grpc-1.4.1
         curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
