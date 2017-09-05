489733ebc1ef3bafb459ece1f392d873802eec1a
Auxilus <raghavsphadke@gmail.com>
2017-09-05 13:28:34 +0530

    Create build.sh

---
+++ disabled-packages/metasploit-framework/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://www.metasploit.com/
+TERMUX_PKG_DESCRIPTION="framework for pentesting"
+TERMUX_PKG_VERSION=4.16.2
+# Depend on coreutils for bin/env
+TERMUX_PKG_DEPENDS="wget, ruby, curl"
+TERMUX_PKG_PLATFORM_INDEPENDENT=yes
+
+termux_step_make_install () {
+	
+	cd $PREFIX/share/
+  curl -L https://Auxilus.github.io/metasploit.sh | bash
+}
