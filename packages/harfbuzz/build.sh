477dc4ee89b5b8f05c66ff7429a0299550041da3
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-06 17:18:09 +0200

    harfbuzz: Update from 1.4.8 to 1.5.1

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.4.8
-TERMUX_PKG_SHA256=ccec4930ff0bb2d0c40aee203075447954b64a8c2695202413cc5e428c907131
+TERMUX_PKG_VERSION=1.5.1
+TERMUX_PKG_SHA256=56838dfdad2729b8866763c82d623354d138a4d99d9ffb710c7d377b5cfc7c51
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=yes --with-graphite2=yes --disable-gtk-doc-html"
