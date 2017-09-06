c062d22012a93e2b3dfeb24d13d81e7a3485b364
Stephen <stephengroat@users.noreply.github.com>
2017-09-06 09:58:43 -0700

    add libpng dependency to fontconfig
    #1169

--- packages/fontconfig/build.sh
+++ packages/fontconfig/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for configuring and customizing font access"
 TERMUX_PKG_VERSION=2.12.4
 TERMUX_PKG_SHA256=668293fcc4b3c59765cdee5cee05941091c0879edcc24dfec5455ef83912e45c
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/fontconfig/release/fontconfig-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_DEPENDS="freetype, libxml2"
+TERMUX_PKG_DEPENDS="freetype, libxml2, libpng"
 # TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-libxml2
