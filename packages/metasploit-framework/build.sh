22c5b6baad869edf055ce9b7183bdd361da14790
Auxilus <raghavsphadke@gmail.com>
2017-09-06 20:19:15 +0530

    Update build.sh
    Ill just make sure if it extracts correctly and creates symlink

--- packages/metasploit-framework/build.sh
+++ packages/metasploit-framework/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_VERSION=4.16.2
 TERMUX_PKG_SRCURL=https://github.com/rapid7/metasploit-framework/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=564072e633da3243252c3eb2cd005e406c005e0e4bbff56b22f7ae0640a3ee34
 TERMUX_PKG_FOLDERNAME=metasploit-framework-$TERMUX_PKG_VERSION
-TERMUX_PKG_DEPENDS="pkg-config, make, clang"
+#TERMUX_PKG_DEPENDS=
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_configure () {
@@ -15,18 +15,18 @@ termux_step_configure () {
 termux_step_make () {
         gem install --install-dir TERMUX_PREFIX/lib/ruby/gems/2.4.0 bundler --platform arm-linux
 	
-        gem install --install-dir TERMUX_PREFIX/lib/ruby/gems/2.4.0 nokogiri -- --use-system-libraries --install-dir $TERMUX_PREFIX/lib/ruby/gems/2.4.0 --platform arm-linux
-        gem unpack grpc -v 1.4.1
-        cd grpc-1.4.1
-        patch -p1 < $TERMUX_PKG_BUILD_DIR/extconf.patch.grpc
-        gem build grpc.gemspec
-        gem install grpc-1.4.1.gem
-        cd ..
-        rm -r grpc-1.4.1
+        #gem install --install-dir TERMUX_PREFIX/lib/ruby/gems/2.4.0 nokogiri -- --use-system-libraries --install-dir $TERMUX_PREFIX/lib/ruby/gems/2.4.0 --platform arm-linux
+        #gem unpack grpc -v 1.4.1
+        #cd grpc-1.4.1
+        #patch -p1 < $TERMUX_PKG_BUILD_DIR/extconf.patch.grpc
+        #gem build grpc.gemspec
+        #gem install grpc-1.4.1.gem
+        #cd ..
+        #rm -r grpc-1.4.1
 
 
         cd $TERMUX_PREFIX/share/metasploit-framework
-        bundle install -j5
+        #bundle install -j5
 
         ln -s $TERMUX_PREFIX/share/metasploit-framework/msfconsole $TERMUX_PREFIX/bin/
 }
