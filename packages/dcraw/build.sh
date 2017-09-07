ff120c5a96e5fe61516642989dfaf84dbf12eca8
Stephen <stephengroat@users.noreply.github.com>
2017-09-07 08:51:39 -0700

    add libjpeg-turbo to dcraw dependency list

--- packages/dcraw/build.sh
+++ packages/dcraw/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_SRCURL=http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-${TERMUX_
 TERMUX_PKG_SHA256=c1d8cc4f19752a3d3aaab1fceb712ea85b912aa25f1f33f68c69cd42ef987099
 TERMUX_PKG_FOLDERNAME=dcraw
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="littlecms, libjasper"
+TERMUX_PKG_DEPENDS="littlecms, libjasper, libjpeg-turbo"
 
 termux_step_make_install () {
 	# See the "install" script for flags:
