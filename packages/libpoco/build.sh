e0092e1e282ca01d2b3d97dd746c25da82f4f18a
its-pointless <bored@goat.si>
2017-09-09 08:07:57 +1000

    fix version num...

--- packages/libpoco/build.sh
+++ packages/libpoco/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/pocoproject/poco
 TERMUX_PKG_DESCRIPTION="C++ Portable Components (POCO)"
-TERMUX_PKG_VERSION=pre-cpp11
+TERMUX_PKG_VERSION=1.pre-cpp11
 TERMUX_PKG_SRCURL=https://github.com/pocoproject/poco/archive/pre-cpp11.tar.gz
 TERMUX_PKG_SHA256=552ce21ad5fbbc22b288fedcd1bb7e1b73362c66a02ea7ea6e680603fac433ca
 TERMUX_PKG_FORCE_CMAKE=yes
