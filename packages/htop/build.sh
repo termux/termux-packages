eb37ec09c5cce7fea6d359bc993f7bbad741b57f
Auxilus <raghavsphadke@gmail.com>
2017-09-08 23:59:05 +0530

    Update build.sh

--- packages/htop/build.sh
+++ packages/htop/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://hisham.hm/htop/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
-TERMUX_PKG_DEPENDS="ncurses, libandroid-support"
+TERMUX_PKG_DEPENDS="ncurses, strace, libandroid-support"
 TERMUX_PKG_VERSION=2.0.2
 TERMUX_PKG_SRCURL=http://hisham.hm/htop/releases/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=179be9dccb80cee0c5e1a1f58c8f72ce7b2328ede30fb71dcdf336539be2f487
