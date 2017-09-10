fd8bd09a833cbfab71af26015fba4f2388041bab
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-10 21:56:10 +0200

    pass: Add comment on why we depend on coreutils

--- packages/pass/build.sh
+++ packages/pass/build.sh
@@ -4,6 +4,8 @@ TERMUX_PKG_VERSION=1.7.1
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://git.zx2c4.com/password-store/snapshot/password-store-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
+# Depend on coreutils as pass uses [:graph:] when calling tr, which
+# busybox tr does not support:
 TERMUX_PKG_DEPENDS="bash, gnupg2, tree, coreutils"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
