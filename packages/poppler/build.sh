d284397abecfb76bd9d6e546652062776a4b7fbc
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-11 20:04:20 +0200

    poppler: Fix source checksum

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_VERSION=0.59.0
-TERMUX_PKG_REVISION=0
-TERMUX_PKG_SHA256=b808ebbfb794a900828baa9b93d3eaea5671d1be03104d12a98d8d0c9f3658d3
+TERMUX_PKG_SHA256=a3d626b24cd14efa9864e12584b22c9c32f51c46417d7c10ca17651f297c9641
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
 TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg"
