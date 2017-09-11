52e1b7a7d017218b973e8254132b3d6af9cafcab
Auxilus <raghavsphadke@gmail.com>
2017-09-11 13:26:28 +0530

    Update build.sh

--- packages/libgpg-error/build.sh
+++ packages/libgpg-error/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/libgpg-error/
 TERMUX_PKG_DESCRIPTION="Small library that defines common error values for all GnuPG components"
-TERMUX_PKG_VERSION=1.27
+TERMUX_PKG_VERSION=1.9
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4f93aac6fecb7da2b92871bb9ee33032be6a87b174f54abf8ddf0911a22d29d2
+TERMUX_PKG_SHA256=54d9326bfbd46c705963c507953f65a40576d04b374e131008dc5b732ff1e3c3
 TERMUX_PKG_RM_AFTER_INSTALL="share/common-lisp"
 
 termux_step_post_extract_package () {
