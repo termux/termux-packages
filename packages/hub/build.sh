f916964d7c7f80db2200d38378aaa97c2d287957
Stephen <stephengroat@users.noreply.github.com>
2017-09-11 16:12:03 -0700

    fix hub hash

--- packages/hub/build.sh
+++ packages/hub/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://hub.github.com/
 TERMUX_PKG_DESCRIPTION="Command-line wrapper for git that makes you better at GitHub"
 TERMUX_PKG_VERSION=2.2.8
 TERMUX_PKG_SRCURL=https://github.com/github/hub/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=61f67d94d495bdd2f8e7eae3054fab8a5ec8f5d839f27a63dbacc2a4e230d847
+TERMUX_PKG_SHA256=5ff475990a5bd335bad8da2d140ea0966c33e6d03dac26bf1558bda94a02b924
 TERMUX_PKG_DEPENDS="git"
 TERMUX_PKG_FOLDERNAME=hub-${TERMUX_PKG_VERSION}
 
