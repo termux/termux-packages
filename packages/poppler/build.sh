eabf68bf18d5f1c84577ba2341a2addf3f37c8aa
Stephen <stephengroat@users.noreply.github.com>
2017-09-11 16:47:09 -0700

    add dependencies to poppler
    #1169

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -4,6 +4,6 @@ TERMUX_PKG_VERSION=0.59.0
 TERMUX_PKG_SHA256=a3d626b24cd14efa9864e12584b22c9c32f51c46417d7c10ca17651f297c9641
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
-TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg"
+TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg, freetype, libxml2"
 #texlive needs the xpdf headers
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-poppler-glib  --enable-xpdf-headers"
