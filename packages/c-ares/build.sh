1f7084bb4ebb7c3371c8af884edc3897b7113816
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-12 13:30:39 +0300

    c-ares: fix path to file '/etc/hosts'

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://c-ares.haxx.se
 TERMUX_PKG_DESCRIPTION="C library for asynchronous DNS requests (including name resolves)"
 TERMUX_PKG_VERSION=1.13.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://c-ares.haxx.se/download/c-ares-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7
