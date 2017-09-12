03b4c3fed0c69f827a2d64b93a3b16fb58f0c56e
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-12 12:21:32 +0300

    libssh: fix path to file 'ssh_config'

--- packages/libssh/build.sh
+++ packages/libssh/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.libssh.org/
 TERMUX_PKG_DESCRIPTION="Tiny C SSH library"
 TERMUX_PKG_VERSION=0.7.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://git.libssh.org/projects/libssh.git/snapshot/libssh-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=ff914e2ec6596badec453efaa63ef68c82c60074466f951167a4c60e49903336
 TERMUX_PKG_DEPENDS="openssl"
