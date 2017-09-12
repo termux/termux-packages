e52a17e6ff90a8260e335617338ac54c032471e6
Auxilus <raghavsphadke@gmail.com>
2017-09-12 23:54:52 +0530

    Update build.sh

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -2,6 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/debianutils
 TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installation scripts of Debian packages"
 TERMUX_PKG_VERSION=4.8.2
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+TERMUX_PKG_SHA256=4deb5f293fd3e43c5d4a625a30b18d0fb07662ff77f769e3272841cdb61e7c68
 TERMUX_PKG_RM_AFTER_INSTALL="bin/installkernel share/man/man8/installkernel.8"
 TERMUX_PKG_FOLDERNAME=debianutils-$TERMUX_PKG_VERSION
