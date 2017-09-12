85042fa9379db3b50ff7b48afbe593ae054be756
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-12 13:22:28 +0300

    libpcap: fix path to file '/etc/ethers'

--- packages/libpcap/build.sh
+++ packages/libpcap/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.tcpdump.org/
 TERMUX_PKG_DESCRIPTION="Library for network traffic capture"
 TERMUX_PKG_VERSION=1.8.1
+TERMUX_PKG_REVISION=1
 # The main tcpdump.org was down 2017-04-12, so we're using a mirror:
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/libpcap-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8360a05884fc03d1acb8ae40f92eabacf30ef18a4fb4662ac4bc50eab8c37eb0
