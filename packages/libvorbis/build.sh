febb84da7d5380c8b4ec8589dde83c29aee35520
Stephen <stephengroat@users.noreply.github.com>
2017-09-11 17:48:51 -0700

    Update build.sh

--- packages/libvorbis/build.sh
+++ packages/libvorbis/build.sh
@@ -4,3 +4,4 @@ TERMUX_PKG_VERSION=1.3.5
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/libvorbis-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=54f94a9527ff0a88477be0a71c0bab09a4c3febe0ed878b24824906cd4b0e1d1
 TERMUX_PKG_DEPENDS="libogg"
+TERMUX_PKG_DEVPACKAGE_DEPENDS="libogg-dev"
