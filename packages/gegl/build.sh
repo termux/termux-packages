f5a0d746d2f6866eb743c3600f347b3ff640eadd
Stephen <stephengroat@users.noreply.github.com>
2017-09-11 18:06:29 -0700

    add glib dependency to gegl depends
    #1169

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/$_MAJOR_VERSION/gegl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_FOLDERNAME=gegl-$TERMUX_PKG_VERSION
 TERMUX_PKG_SHA256=d7858ef26ede136d14e3de188a9e9c0de7707061a9fb96d7d615fab4958491fb
-TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg"
+TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-gdk-pixbuf
 "
