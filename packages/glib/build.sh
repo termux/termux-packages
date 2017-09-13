2207a0f1706a62951c9adb46612fe7a8773d5926
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-13 14:59:39 +0300

    glib: bump revision

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_VERSION=2.54.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=fe22998ff0394ec31e6e5511c379b74011bee61a4421bca7fcab223dfbe0fc6a
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
 # libandroid-support to get langinfo.h in include path.
