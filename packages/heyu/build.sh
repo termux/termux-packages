2cea1499115547bb6ae1159a5f1074608d18a942
Stephen <stephengroat@users.noreply.github.com>
2017-09-14 09:25:12 -0700

    fix minor linking issues

--- packages/heyu/build.sh
+++ packages/heyu/build.sh
@@ -3,3 +3,9 @@ TERMUX_PKG_DESCRIPTION="A text-based console program for remotely controlling li
 TERMUX_PKG_VERSION=2.11-rc2
 TERMUX_PKG_SRCURL=https://github.com/HeyuX10Automation/heyu/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_FOLDERNAME=heyu-${TERMUX_PKG_VERSION}
+
+termux_step_pre_configure () {
+	# rindex is an obsolete version of strrchr which is not available in Android:
+	CFLAGS+=" -Drindex=strrchr"
+	LDFLAGS+=" -llog"
+}
