66b38fd7833f754eb478c9034a8e37243eb699d0
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-14 20:21:50 +0200

    poppler: Revert back to 0.57.0 (see #1486)
    The package version number is 0.59.0.1 to get a higher version
    number, but the poppler version used is 0.57.0.
    
    This ugliness will be temporary until we can update to a later
    version.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,8 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
-TERMUX_PKG_VERSION=0.59.0
-TERMUX_PKG_SHA256=a3d626b24cd14efa9864e12584b22c9c32f51c46417d7c10ca17651f297c9641
-TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
+# Do not update before pdflatex is fixed to work with newer poppler versions:
+TERMUX_PKG_VERSION=0.59.0.1
+local _REAL_VERSION=0.57.0
+TERMUX_PKG_SHA256=0ea37de71b7db78212ebc79df59f99b66409a29c2eac4d882dae9f2397fe44d8
+TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${_REAL_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
 TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg"
 #texlive needs the xpdf headers
