b9d07ce5e768c49c7cebd495cd427ae71dcb3d02
Leonid Pliushch <leonid.pliushch@gmail.com>
2017-09-14 20:50:38 +0300

    strace: update to 4.19

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor the system calls used by a program and all the signals it receives"
-TERMUX_PKG_VERSION=4.16
+TERMUX_PKG_VERSION=4.19
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/strace/strace/${TERMUX_PKG_VERSION}/strace-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=98487cb5178ec1259986cc9f6e2a844f50e5d1208c112cc22431a1e4d9adf0ef
+TERMUX_PKG_SHA256=7c93ebc6c29280f47c24a0eb86873a99ccb2cac6512c60a60ba4ef99ab807281
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="st_cv_m32_mpers=no"
