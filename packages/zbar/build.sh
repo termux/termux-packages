c7527292003497408d513fda879da34aeb3e757a
Stephen <stephengroat@users.noreply.github.com>
2017-09-14 16:50:00 -0700

    add libjpeg-turbo to zbar depends
    #1169

--- packages/zbar/build.sh
+++ packages/zbar/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_VERSION=0.10
 TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=234efb39dbbe5cef4189cc76f37afbe3cfcfb45ae52493bfe8e191318bdbadc6
-TERMUX_PKG_DEPENDS="imagemagick"
+TERMUX_PKG_DEPENDS="imagemagick, libjpeg-turbo"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthread
 --disable-video --without-xshm --without-xv
 --without-x --without-gtk --without-qt
