7e8148c66bf7aaa5362c1299ac61178cc0be129f
Stephen <stephengroat@users.noreply.github.com>
2017-09-14 10:04:56 -0700

    add hoedown
    closes #1479

---
+++ packages/hoedown/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/hoedown/hoedown
+TERMUX_PKG_DESCRIPTION="Hoedown is a revived fork of Sundown, the Markdown parser based on the original code of the Upskirt library"
+TERMUX_PKG_VERSION=3.0.7
+TERMUX_PKG_SRCURL=https://github.com/hoedown/hoedown/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_FOLDERNAME=hoedown-${TERMUX_PKG_VERSION}
+TERMUX_PKG_BUILD_IN_SRC=yes
