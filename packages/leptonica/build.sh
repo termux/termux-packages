5af2533f0c21ed223bd002f23ce0dcdd21193bfa
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-14 18:57:31 +0300

    leptonica: fixed wrong checksum

--- packages/leptonica/build.sh
+++ packages/leptonica/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for image processing and image analysis"
 TERMUX_PKG_VERSION=1.74.4
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff"
 TERMUX_PKG_SRCURL=https://github.com/DanBloomberg/leptonica/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=29c35426a416bf454413c6fec24c24a0b633e26144a17e98351b6dffaa4a833b
+TERMUX_PKG_SHA256=2092e126652ff07bc2569971a1d6c6411e1d481539d39c98031534c6f83dfc82
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-giflib --without-libwebp --without-libopenjpeg"
 TERMUX_PKG_FOLDERNAME=leptonica-$TERMUX_PKG_VERSION
 
