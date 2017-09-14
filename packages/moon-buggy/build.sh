957a54c31abe0626b4a78cfaedd6483b396debd4
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-15 00:38:35 +0200

    Rebuild all packages with control.tar.gz
    Bump the version of all old packages that contained a control.tar.gz
    file - as of now only control.tar.xz is used. See #1398.

--- packages/moon-buggy/build.sh
+++ packages/moon-buggy/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://www.seehuhn.de/pages/moon-buggy
 TERMUX_PKG_DESCRIPTION="Simple game where you drive a car across the moon's surface"
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_VERSION=1.0.51
+TERMUX_PKG_REVISION=1
 # Main site down 2017-01-06.
 # TERMUX_PKG_SRCURL=http://m.seehuhn.de/programs/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=ftp://ftp.netbsd.org/pub/pkgsrc/distfiles/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz
