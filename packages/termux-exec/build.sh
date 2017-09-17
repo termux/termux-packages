acf19a7ba28b084415182b32801b8562065ffcd8
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-17 22:13:38 +0200

    termux-exec: Update from 0.1 to 0.2

--- packages/termux-exec/build.sh
+++ packages/termux-exec/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-exec
 TERMUX_PKG_DESCRIPTION="An execve() wrapper to make /bin and /usr/bin shebangs work"
-TERMUX_PKG_VERSION=0.1
-TERMUX_PKG_SHA256=624add76fbefd2f45a3dc3207f64c12c9993c5c5afac8654f787175c72dac0e9
+TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_SHA256=f8ee0a2e1bba03fbce9ece0ecc83d1fc3f0b0be0cd5d72b202ca3de2f3f359a8
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_FOLDERNAME=termux-exec-$TERMUX_PKG_VERSION
 TERMUX_PKG_BUILD_IN_SRC=yes
