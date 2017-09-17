fe1dfcd80faf2a9b2d6834665aece6c3c9e5fab2
Oliver Schmidhauser <oli@glow.li>
2017-09-17 09:05:43 +0200

    Update to v0.3

--- packages/fixedshell/build.sh
+++ packages/fixedshell/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/its-pointless/fixedshell
 TERMUX_PKG_DESCRIPTION="Make traditional shebangs work in termux without performance impact"
-TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_VERSION=0.3
 TERMUX_PKG_SRCURL=https://github.com/its-pointless/fixedshell/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=309620e52db314c943b856dc4dabde11312297f5e2cd56686e204f6a5ce8c265
+TERMUX_PKG_SHA256=4fbe81585f4685b9c4588d7bd478a02d6176b34b80919b25772167d1b77ba218
 TERMUX_PKG_FOLDERNAME=fixedshell-$TERMUX_PKG_VERSION
 TERMUX_PKG_BUILD_IN_SRC=yes
