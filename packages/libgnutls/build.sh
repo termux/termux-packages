73efcf5ef8422f4b38b95a985c3559fb006a2eb5
Stephen <stephengroat@users.noreply.github.com>
2017-09-17 18:20:27 -0700

    add devpackage depends to gnutls
    #1169
    
    for ffmpeg

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -2,8 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn, libunistring"
 TERMUX_PKG_VERSION=3.5.15
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=046081108b8b1fe455a13a4c5a4eaa0368e185b678f1670fe09a11a2d7ecfad5
 TERMUX_PKG_SRCURL=ftp://ftp.gnutls.org/gcrypt/gnutls/v${TERMUX_PKG_VERSION:0:3}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_DEVPACKAGE_DEPENDS="libidn-dev, libnettle-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-cxx
 --disable-hardware-acceleration
