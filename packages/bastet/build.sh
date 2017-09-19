93b6b7e9f8a782e70995b151b5e2930dc528a7ec
its-pointless <bored@goat.si>
2017-09-19 19:22:55 +1000

    bastard tetris

---
+++ packages/bastet/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://fph.altervista.org/prog/bastet.html
+TERMUX_PKG_DESCRIPTION="bastard tetris"
+TERMUX_PKG_VERSION=0.43.2
+TERMUX_PKG_FOLDERNAME=bastet-$TERMUX_PKG_VERSION
+TERMUX_PKG_SRCURL=https://github.com/fph/bastet/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="boost, ncurses"
+TERMUX_PKG_SHA256=f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f
