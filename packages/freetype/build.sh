dd06f9252de25aa35d3f0d7ab7bf938f3de054fb
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-20 00:55:17 +0200

    freetype: Update from 2.8 to 2.8.1

--- packages/freetype/build.sh
+++ packages/freetype/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.freetype.org
 TERMUX_PKG_DESCRIPTION="Software font engine capable of producing high-quality output"
-TERMUX_PKG_VERSION=2.8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.8.1
+TERMUX_PKG_SHA256=e5435f02e02d2b87bb8e4efdcaa14b1f78c9cf3ab1ed80f94b6382fb6acc7d78
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/freetype/freetype2/${TERMUX_PKG_VERSION}/freetype-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a3c603ed84c3c2495f9c9331fe6bba3bb0ee65e06ec331e0a0fb52158291b40b
 TERMUX_PKG_DEPENDS="libbz2, libpng"
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/freetype-config share/man/man1/freetype-config.1"
 # Use with-harfbuzz=no to avoid circular dependency between freetype and harfbuzz:
