0c98a3d3e725290e383e67b7e4dd5d2246336cd9
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-20 23:16:46 +0200

    Remove disabled-packages/llvm

--- disabled-packages/llvm/clang-dev.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="include/clang include/clang-c lib/cmake/clang lib/clang/$TERMUX_PKG_VERSION"
-TERMUX_SUBPKG_DESCRIPTION='C language family frontend for LLVM (development files)'
-TERMUX_SUBPKG_DEPENDS="clang"
--- disabled-packages/llvm/lldb.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="bin/lldb* lib/liblldb*"
-TERMUX_SUBPKG_DESCRIPTION='Next generation, high-performance debugger'
-TERMUX_SUBPKG_DEPENDS="llvm, ncurses"
