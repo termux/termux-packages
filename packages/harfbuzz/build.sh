6fcac94774c5c6327760f0246c0abf84077f9b9e
Stephen <stephengroat@users.noreply.github.com>
2017-09-20 13:20:07 -0700

    add libicu to harfbuzz build dependencies (#1445)
    #1169

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -4,5 +4,6 @@ TERMUX_PKG_VERSION=1.5.1
 TERMUX_PKG_SHA256=56838dfdad2729b8866763c82d623354d138a4d99d9ffb710c7d377b5cfc7c51
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
+TERMUX_PKG_BUILD_DEPENDS="libicu"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libgraphite-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=yes --with-graphite2=yes --disable-gtk-doc-html"
