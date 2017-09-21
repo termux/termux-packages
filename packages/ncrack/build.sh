791573dbe885bf09b8f140899517324143186e70
Auxilus <raghavsphadke@gmail.com>
2017-09-21 21:38:45 +0530

    Update build.sh

--- packages/ncrack/build.sh
+++ packages/ncrack/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_SHA256=4dd4abfa7c38edafd0da3a3f2ad76f9fae476e147042def87d027879444bd0
 TERMUX_PKG_SRCURL=https://github.com/nmap/ncrack/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="openssl, openssh, openssl-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_vsnprintf=no ac_cv_header_unistd=no" 
 
 termux_step_post_configure () {
         cd opensshlib
