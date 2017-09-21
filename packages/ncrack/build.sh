2fd71183197caf1c9c52fa8f947ad34b46b9479c
Auxilus <raghavsphadke@gmail.com>
2017-09-21 12:53:52 +0530

    Add package ncrack

---
+++ packages/ncrack/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://nmap.org/ncrack/
+TERMUX_PKG_DESCRIPTION="high-speed network authentication cracking tool"
+TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_SHA256=4dd4abfa7c38edafd0da3a3f2ad76f9fae476e147042def87d027879444bd0b9
+TERMUX_PKG_SRCURL=https://github.com/nmap/ncrack/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_DEPENDS="openssl, openssh, openssl-dev"
+
+termux_step_pre_make ()  {
+        cd $TERMUX_PKG_SRCDIR/opensshlib
+        rm cipher.c
+        curl -LO https://raw.githubusercontent.com/Auxilus/Auxilus.github.io/master/cipher.c
+        rm explicit_bzero.c
+        https://raw.githubusercontent.com/Auxilus/Auxilus.github.io/master/explicit_bzero.c
+        rm bsd-snprintf.c
+        https://raw.githubusercontent.com/Auxilus/Auxilus.github.io/master/bsd-snprintf.c
+        
+        sed '/explicit_bzero/d' -i /*.c
+}
