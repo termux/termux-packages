fd1128ca1a938426d6d6ddcea5ca9e52263b384e
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-21 22:52:21 +0200

    cmake: Update from 3.9.2 to 3.9.3

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
-_MAJOR_VERSION=3.9
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
-TERMUX_PKG_SHA256=954a5801a456ee48e76f01107c9a4961677dd0f3e115275bbd18410dc22ba3c1
-TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${_MAJOR_VERSION}/cmake-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=3.9.3
+TERMUX_PKG_SHA256=8eaf75e1e932159aae98ab5e7491499545554be62a08cbcbc7c75c84b999f28a
+TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:3}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv, librhash, make, clang"
 TERMUX_PKG_FORCE_CMAKE=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DKWSYS_LFS_WORKS=ON"
