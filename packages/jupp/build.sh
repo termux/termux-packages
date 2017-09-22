4e5f1ac1735211815a0100f6a629e96876647f60
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-23 01:46:30 +0200

    jupp: Update from 3.1jupp30 to 3.1jupp31

--- packages/jupp/build.sh
+++ packages/jupp/build.sh
@@ -3,12 +3,17 @@ TERMUX_PKG_DESCRIPTION="User friendly full screen text editor"
 TERMUX_PKG_MAINTAINER="Dominik George @Natureshadow"
 TERMUX_PKG_DEPENDS="ncurses, libutil"
 TERMUX_PKG_CONFLICTS="joe"
-TERMUX_PKG_VERSION=3.1jupp30
+TERMUX_PKG_VERSION=3.1jupp31
+TERMUX_PKG_SHA256=1a50607b0417cf230f7b3609c091e71e8d8e91185a4a3897f7925cd3b44cceba
 TERMUX_PKG_SRCURL=https://pub.allbsd.org/MirOS/dist/jupp/joe-${TERMUX_PKG_VERSION}.tgz
-TERMUX_PKG_SHA256=65ddb346364a056c1d78a1cb406b0ebf6c9c2fbd753cd404b1b4c8fd3fa9916d
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-termcap --disable-getpwnam --disable-termidx --disable-dependency-tracking --enable-sysconfjoesubdir=/jupp"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-dependency-tracking
+--disable-getpwnam
+--disable-termcap
+--disable-termidx
+--enable-sysconfjoesubdir=/jupp
+"
 
 termux_step_post_extract_package() {
-	cd $TERMUX_PKG_SRCDIR
-	chmod +x configure
+	chmod +x $TERMUX_PKG_SRCDIR/configure
 }
