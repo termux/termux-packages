041b6d083f9c7173bb6732795cda1e655ba14981
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-23 02:08:37 +0200

    heyu: Remove unused TERMUX_PKG_FOLDERNAME

--- packages/heyu/build.sh
+++ packages/heyu/build.sh
@@ -2,7 +2,6 @@ TERMUX_PKG_HOMEPAGE=http://www.heyu.org/
 TERMUX_PKG_DESCRIPTION="A text-based console program for remotely controlling lights and appliances in the home or office"
 TERMUX_PKG_VERSION=2.11-rc2
 TERMUX_PKG_SRCURL=https://github.com/HeyuX10Automation/heyu/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_FOLDERNAME=heyu-${TERMUX_PKG_VERSION}
 
 termux_step_pre_configure () {
 	# rindex is an obsolete version of strrchr which is not available in Android:
