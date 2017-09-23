69e03c965262171a5c5cebc33bdc29dd6d21a74f
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-23 10:25:54 +0200

    mlocate: Fix segfault when using wildcards

--- packages/mlocate/build.sh
+++ packages/mlocate/build.sh
@@ -1,7 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://pagure.io/mlocate
 TERMUX_PKG_DESCRIPTION="Tool to find files anywhere in the filesystem based on their name"
+# If not linking to libandroid-support we segfault in
+# the libc mbsnrtowcs() function when using a wildcard
+# like in '*.deb'.
+TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_VERSION=0.26
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://releases.pagure.org/mlocate/mlocate-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3063df79fe198fb9618e180c54baf3105b33d88fe602ff2d8570aaf944f1263e
 
