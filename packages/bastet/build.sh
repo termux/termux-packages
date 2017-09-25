5681cda33d97ccd79ec543bc8be1d5cf5414b6fa
Auxilus <raghavsphadke@gmail.com>
2017-09-26 00:49:53 +0530

    Fixed dependencies

--- packages/bastet/build.sh
+++ packages/bastet/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://fph.altervista.org/prog/bastet.html
 TERMUX_PKG_DESCRIPTION="ncurses-based brick game"
-TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, ncurses-dev, ncurses-utils"
+TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, ncurses-dev, ncurses-utils, boost, boost-dev"
 TERMUX_PKG_VERSION=0.43.2
 TERMUX_PKG_SRCURL=https://github.com/fph/bastet/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f
