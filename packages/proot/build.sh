01d207a6b56ea75b4c259105579c02ed412d2f44
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-25 17:28:32 +0300

    proot: add fakeroot script to simulate UID 0

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root 
 # Just bump commit and version when needed:
 _COMMIT=6671bfed4ddcbd393d8ca7b2754d58e41ed9595b
 TERMUX_PKG_VERSION=5.1.106
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=e3565bd6789659d86106d6e08eb204d8606af94664aebcb20dcf68a1852c4ba2
 TERMUX_PKG_DEPENDS="libtalloc"
@@ -23,5 +23,6 @@ termux_step_make_install () {
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp $TERMUX_PKG_SRCDIR/doc/proot/man.1 $TERMUX_PREFIX/share/man/man1/proot.1
 
+	cp $TERMUX_PKG_BUILDER_DIR/fakeroot $TERMUX_PREFIX/bin/
 	cp $TERMUX_PKG_BUILDER_DIR/termux-chroot $TERMUX_PREFIX/bin/
 }
