0899ffd56fef0b8526032e191a6132a78a100052
Oliver Schmidhauser <oli@glow.li>
2017-09-25 14:15:09 +0200

    Add package: proxychains-ng Closes #330 (#395)
    I've tested it with a simple ssh tunnel and it seems to work really
    well. It works better than tsocks because I was able use python
    programs (getmail) with this. I hope it holds up when more experienced
    users put it through it's paces.

---
+++ packages/proxychains-ng/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dgoulet/torsocks
+TERMUX_PKG_DESCRIPTION="Wrapper to safely torify applications"
+TERMUX_PKG_VERSION=4.12
+TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
+TERMUX_PKG_SRCURL=https://github.com/rofl0r/proxychains-ng/releases/download/v${TERMUX_PKG_VERSION}/proxychains-ng-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=482a549935060417b629f32ddadd14f9c04df8249d9588f7f78a3303e3d03a4e
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --prefix=${TERMUX_PREFIX}"
+TERMUX_PKG_BUILD_IN_SRC=yes
+termux_step_post_make_install(){
+    # Remove conf file from previous build, otherwise nothing will be done and it won't be included in the package
+    rm -f $TERMUX_PREFIX/etc/proxychains.conf
+    make install-config
+}
+
+
