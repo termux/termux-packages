b783391dac0ffa5f5365e054e964d02694b481e9
Leonid Plyushch <leonid.plyushch@gmail.com>
2017-09-26 13:36:56 +0300

    htop: fix segfault which may occur in some cases
    This already fixed in htop's repo at commit cceab15b9dbdc2f0914c8d479cefa429d1c0daaf

--- packages/htop/build.sh
+++ packages/htop/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
 TERMUX_PKG_DEPENDS="ncurses, libandroid-support"
 TERMUX_PKG_VERSION=2.0.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://hisham.hm/htop/releases/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=179be9dccb80cee0c5e1a1f58c8f72ce7b2328ede30fb71dcdf336539be2f487
 TERMUX_PKG_BUILD_IN_SRC="yes"
