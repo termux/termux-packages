8a4fc835ac19871edcec7d46600248006bd7b58c
Stephen <stephengroat@users.noreply.github.com>
2017-09-27 09:44:00 -0700

    Update build.sh

--- packages/libexpat/build.sh
+++ packages/libexpat/build.sh
@@ -3,4 +3,5 @@ TERMUX_PKG_DESCRIPTION="XML parsing C library"
 TERMUX_PKG_VERSION=2.2.4
 TERMUX_PKG_SHA256=b5dcb503e40f615a0296a18acc6d975f2f1a3d01c7b3a4b3bb69de27bc9475c3
 TERMUX_PKG_SRCURL=https://github.com/libexpat/libexpat/archive/R_2_2_4.tar.gz
+TERMUX_PKG_FOLDERNAME=libexpat-R_2_2_4
 TERMUX_PKG_RM_AFTER_INSTALL="bin/xmlwf share/man/man1/xmlwf.1"
