7ee53f93d9d395caf9e29ac39443d19e5f40a8c1
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-29 00:15:57 +0200

    libc: Place TERMUX_PKG_REVISION beneath version

--- packages/libgc/build.sh
+++ packages/libgc/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://www.hboehm.info/gc/
 TERMUX_PKG_DESCRIPTION="Library providing the Boehm-Demers-Weiser conservative garbage collector"
 TERMUX_PKG_VERSION=7.6.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://www.hboehm.info/gc/gc_source/gc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90
 TERMUX_PKG_RM_AFTER_INSTALL="share/gc"
-TERMUX_PKG_REVISION=1
+
 termux_step_post_extract_package () {
 	LIBATOMIC_VERSION=7.4.6
 	LIBATOMIC_FILE=libatomic_ops-${LIBATOMIC_VERSION}.tar.gz
