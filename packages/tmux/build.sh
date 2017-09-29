bb9727d29fe385a373887a48ed6cc7e74fe92d22
Fredrik Fornwall <fredrik@fornwall.net>
2017-09-29 14:59:11 +0200

    tmux: Bump revision after change

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer implementing switching between seve
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
 TERMUX_PKG_DEPENDS="ncurses, libevent, libutil, libandroid-support, libandroid-glob"
 TERMUX_PKG_VERSION=2.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=ae135ec37c1bf6b7750a84e3a35e93d91033a806943e034521c8af51b12d95df
 TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/releases/download/${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
