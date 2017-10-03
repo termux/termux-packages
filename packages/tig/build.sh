de529acb992009b770c91e7574c4ecede7228140
Stephen <stephengroat@users.noreply.github.com>
2017-10-03 10:39:20 -0700

    update tig to 2.3.0

--- packages/tig/build.sh
+++ packages/tig/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://jonas.github.io/tig/
 TERMUX_PKG_DESCRIPTION="Ncurses-based text-mode interface for git"
-TERMUX_PKG_VERSION=2.2.2
+TERMUX_PKG_VERSION=2.3.0
 TERMUX_PKG_SRCURL=https://github.com/jonas/tig/releases/download/tig-$TERMUX_PKG_VERSION/tig-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=316214d87f7693abc0cbe8ebbb85decdf5e1b49d7ad760ac801af3dd73385e35
 TERMUX_PKG_BUILD_IN_SRC=yes
