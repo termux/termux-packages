7b640b299a19998c8d656bbaca09564993423281
its-pointless <bored@goat.si>
2017-10-04 11:19:58 +1100

    forget deps

--- packages/vifm/build.sh
+++ packages/vifm/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Vifm is an ncurses based file manager with vi like keybi
 TERMUX_PKG_VERSION=0.9
 TERMUX_PKG_SRCURL=https://github.com/vifm/vifm/archive/v0.9.tar.gz
 TERMUX_PKG_SHA256=837337935627dafaff90da300dea0e35dd9ad1eb932294274ababee814f91649
+TERMUX_PKG_DEPENDS="ncurses, file"
 termux_step_pre_configure() {
 autoreconf -if
 }
