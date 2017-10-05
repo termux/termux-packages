daf2f8006a53fdab7c3f3cc1c0825bf0d99c2507
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-05 08:25:12 +0200

    libuv: Update from 1.14.1 to 1.15.0

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
-TERMUX_PKG_VERSION=1.14.1
-TERMUX_PKG_SHA256=f32243d2ad8eb7604cdfff8f719353bbcc4ebbc41def78930569372d7f257d22
+TERMUX_PKG_VERSION=1.15.0
+TERMUX_PKG_SHA256=28b1b334ae79fdbb025c7a4dacf3cb14738f9d336998bc42bbdbe72b8799fe85
 TERMUX_PKG_SRCURL=http://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_pre_configure () {
