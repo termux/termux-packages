2864ccba1883ac6813730a5a9ba5ce8459824ad3
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-06 22:52:15 +0200

    libsodium: Update from 1.0.14 to 1.0.15

--- packages/libsodium/build.sh
+++ packages/libsodium/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://libsodium.org/
 TERMUX_PKG_DESCRIPTION="Network communication, cryptography and signaturing library"
-TERMUX_PKG_VERSION=1.0.14
-TERMUX_PKG_SHA256=3cfc84d097fdc891b40d291f2ac2c3f99f71a87e36b20cc755c6fa0e97a77ee7
+TERMUX_PKG_VERSION=1.0.15
+TERMUX_PKG_SHA256=fb6a9e879a2f674592e4328c5d9f79f082405ee4bb05cb6e679b90afe9e178f4
 TERMUX_PKG_SRCURL=https://github.com/jedisct1/libsodium/releases/download/${TERMUX_PKG_VERSION}/libsodium-${TERMUX_PKG_VERSION}.tar.gz
