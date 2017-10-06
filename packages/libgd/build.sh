d4389f5b7cc8c26811aa3c555ab2fc8f84a2d62d
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-06 22:51:45 +0200

    libgd: Update from 2.2.4 to 2.2.5

--- packages/libgd/build.sh
+++ packages/libgd/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://libgd.github.io/
 TERMUX_PKG_DESCRIPTION="GD is an open source code library for the dynamic creation of images by programmers"
-TERMUX_PKG_VERSION=2.2.4
+TERMUX_PKG_VERSION=2.2.5
+TERMUX_PKG_SHA256=a66111c9b4a04e818e9e2a37d7ae8d4aae0939a100a36b0ffb52c706a09074b5
 TERMUX_PKG_SRCURL=https://github.com/libgd/libgd/releases/download/gd-${TERMUX_PKG_VERSION}/libgd-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=487a650aa614217ed08ab1bd1aa5d282f9d379cfd95c756aed0b43406381be65
 TERMUX_PKG_DEPENDS="freetype, fontconfig, libjpeg-turbo, libpng, libtiff"
 # Disable vpx support for now, look at https://github.com/gagern/libgd/commit/d41eb72cd4545c394578332e5c102dee69e02ee8
 # for enabling:
