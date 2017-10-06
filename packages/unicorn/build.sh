21d9ed8c0ba7bb64cde8bb8ecf2445c386d8a39f
Stephen G <stephen@egroat.com>
2017-10-06 08:24:17 -0700

    Add Unicorn Engine

---
+++ packages/unicorn/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://www.unicorn-engine.org/
+TERMUX_PKG_DESCRIPTION="Unicorn is a lightweight multi-platform, multi-architecture CPU emulator framework."
+TERMUX_PKG_VERSION="1.0.1"
+TERMUX_PKG_SRCURL=https://github.com/unicorn-engine/unicorn/archive/1.0.1.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
