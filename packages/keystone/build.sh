209ad5d9b8b489ffd1910be3ec5a09a2d6867468
Stephen <stephengroat@users.noreply.github.com>
2017-10-07 11:51:58 -0700

    add keystone

---
+++ packages/keystone/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://www.keystone-engine.org/
+TERMUX_PKG_DESCRIPTION="Keystone is a lightweight multi-platform, multi-architecture assembler framework"
+TERMUX_PKG_VERSION="0.9.1"
+TERMUX_PKG_SRCURL=https://github.com/keystone-engine/keystone/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
