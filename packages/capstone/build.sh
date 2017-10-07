f03ccec52f4025ddbcb97b9a1044d1d7549ed32d
Stephen <stephengroat@users.noreply.github.com>
2017-10-07 11:47:25 -0700

    add capstone

---
+++ packages/capstone/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://www.capstone-engine.org/
+TERMUX_PKG_DESCRIPTION="Capstone is a disassembly framework"
+TERMUX_PKG_VERSION="3.0.4"
+TERMUX_PKG_SRCURL=https://github.com/aquynh/capstone/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
