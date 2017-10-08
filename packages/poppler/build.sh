c6ea843b9219f6523633f3ad71198d11b347b1ff
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-08 15:38:23 +0200

    poppler: Update from 0.59.0 to 0.60.1
    This update switches the build system from autoconf to cmake.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,10 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
-# When upgrading to 0.60.0, remove extra .2 and fix SRCURL...
-TERMUX_PKG_VERSION=0.59.0.2
-TERMUX_PKG_SHA256=a3d626b24cd14efa9864e12584b22c9c32f51c46417d7c10ca17651f297c9641
-TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION:0:6}.tar.xz
+TERMUX_PKG_VERSION=0.60.1
+TERMUX_PKG_SHA256=19f185e05c3b59b4a1de2cec8ef39f5446035493d17bbed41d02fb9a77c8a93c
+TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
-TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg, freetype, libxml2"
+TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg, freetype, libcurl"
 #texlive needs the xpdf headers
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-poppler-glib  --enable-xpdf-headers"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DENABLE_GLIB=OFF
+-DENABLE_XPDF_HEADERS=ON
+"
