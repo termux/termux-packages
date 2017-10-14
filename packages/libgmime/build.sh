51dd8291faa8dcc769f8ad4bc5e2376c13b2a643
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-14 23:35:15 +0200

    libgmime: Update from 3.0.1 to 3.0.2

--- packages/libgmime/build.sh
+++ packages/libgmime/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://spruce.sourceforge.net/gmime/
 TERMUX_PKG_DESCRIPTION="MIME message parser and creator"
-local _MAJOR_VERSION=3.0
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
-TERMUX_PKG_SHA256=c28459ea86107e3a04ad06081f0b2b96b57f0774db44bae7a72ae18ad6483e00
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gmime/$_MAJOR_VERSION/gmime-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION=3.0.2
+TERMUX_PKG_SHA256=0deb460111ffa2ec672677da339b82dedeb28b258ccdb216daa21c81a9472fb2
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gmime/${TERMUX_PKG_VERSION:0:3}/gmime-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="glib, libidn"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_have_iconv_detect_h=yes
