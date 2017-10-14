1e72d13a90ec75d51aa5145ac583d986f84af135
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-14 21:08:13 +0200

    vifm: Fix docker build
    Fix building in the docker container where groff is not available.

--- packages/vifm/build.sh
+++ packages/vifm/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_VERSION=0.9
 TERMUX_PKG_SRCURL=https://github.com/vifm/vifm/archive/v0.9.tar.gz
 TERMUX_PKG_SHA256=837337935627dafaff90da300dea0e35dd9ad1eb932294274ababee814f91649
 TERMUX_PKG_DEPENDS="ncurses, file"
+
 termux_step_pre_configure() {
-autoreconf -if
+	autoreconf -if
 }
