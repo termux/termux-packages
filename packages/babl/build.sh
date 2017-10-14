0845100f4e12c5ff96fe9a2a1841237f16b33552
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-14 21:05:10 +0200

    babl: Update from 0.1.30 to 0.1.34

--- packages/babl/build.sh
+++ packages/babl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE="http://gegl.org/babl/"
 TERMUX_PKG_DESCRIPTION="Dynamic pixel format translation library"
-TERMUX_PKG_VERSION=0.1.30
-TERMUX_PKG_SHA256=562ba7b1290d93d55029a92f700f2ec8602349b3acc7ae201188146b96e186be
+TERMUX_PKG_VERSION=0.1.34
+TERMUX_PKG_SHA256=d21ac99c97d9fb0bc61ecaf77a9ae1af95b75c83ccf066e223415818f9ce0b60
 TERMUX_PKG_SRCURL=https://github.com/GNOME/babl/archive/BABL_${TERMUX_PKG_VERSION//./_}.tar.gz
 
 termux_step_pre_configure() {
