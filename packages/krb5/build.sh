d1a44d3896a4709a7b6e228618fafacb662fc8a4
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-14 22:40:17 +0200

    krb5: Update from 1.15.1 to 1.15.2

--- packages/krb5/build.sh
+++ packages/krb5/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://web.mit.edu/kerberos
 TERMUX_PKG_DESCRIPTION="The Kerberos network authentication system"
-TERMUX_PKG_VERSION=1.15.1
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, readline, openssl, libutil, libdb"
+TERMUX_PKG_VERSION=1.15.2
+TERMUX_PKG_SHA256=1639e392edf25e3b6cfec2ae68f97eb53e07c2dbe74bfeede0108465d5d1c87e
 TERMUX_PKG_SRCURL="https://web.mit.edu/kerberos/dist/krb5/1.15/krb5-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=437c8831ddd5fde2a993fef425dedb48468109bb3d3261ef838295045a89eb45
+TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, readline, openssl, libutil, libdb"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_CONFFILES="etc/krb5.conf var/krb5kdc/kdc.conf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-readline
