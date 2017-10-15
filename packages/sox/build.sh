2ed8a0b241e408031e682558c0632dddcd7a8c5b
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-15 02:06:35 +0200

    sox: Avoid accidental linkage

--- packages/sox/build.sh
+++ packages/sox/build.sh
@@ -4,7 +4,13 @@ TERMUX_PKG_VERSION=14.4.2
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/sox/sox/${TERMUX_PKG_VERSION}/sox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=81a6956d4330e75b5827316e44ae381e6f1e8928003c6aa45896da9041ea149c
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-oggvorbis --without-libltdl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-oggvorbis
+--without-libltdl
+--without-opus
+--without-pulseaudio
+--without-sndfile
+"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/play bin/rec share/man/man1/play.1 share/man/man1/rec.1"
 # Depend on file for libmagic.so linking:
 TERMUX_PKG_DEPENDS="file, libpng, libmp3lame, libogg, libvorbis, libandroid-glob, libflac, libid3tag, libmad"
