22de0d5037906ba2cdc8a2091da24aed232beaa6
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-15 03:20:11 +0200

    Cleanup gst-plugins-{bad,good,ugly}

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -1,9 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Ugly Plug-ins"
 TERMUX_PKG_VERSION=1.12.3
-TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=e88ca584c94ea78eeecbf3af00ef7f134b66bdee7408aa4aa6c547235e060052
+TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="gst-plugins-base"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-tests --disable-examples"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-android_media"
-TERMUX_PKG_SHA256=13e7f479296891fef5a686438f20ba7d534680becf2269ecc5ee24aa83b45f03
+TERMUX_PKG_DEPENDS="gst-plugins-base, libmp3lame, libx264"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-tests
+--disable-examples
+--disable-android_media
+"
+TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/"
