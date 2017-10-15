02fc8fdebb07e8b9a702f075217a5dcf4ec9233d
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-15 02:07:13 +0200

    ghostscript: Add dependencies

--- packages/ghostscript/build.sh
+++ packages/ghostscript/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interpreter for the PostScript language and for PDF"
 TERMUX_PKG_VERSION=9.22
 TERMUX_PKG_SHA256=3f84d8404c840ea62b46b5ebf215dfdb83a20c3ba2419fd6985eb7bce050f007
 TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs${TERMUX_PKG_VERSION//.}/ghostpdl-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libandroid-support, libtiff, libjpeg-turbo, libpng, libexpat"
+TERMUX_PKG_DEPENDS="libandroid-support, libtiff, libjpeg-turbo, libpng, libexpat, freetype, fontconfig, libidn"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-system-libtiff \
 --enable-little-endian \
 --without-x \
