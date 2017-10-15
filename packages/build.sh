d0b0cd08fa51a4312333e3b78baa50e70d5eb15b
Auxilus <raghavsphadke@gmail.com>
2017-10-15 08:32:02 +0530

    Update build.sh

--- packages/build.sh
+++ packages/build.sh
@@ -4,4 +4,5 @@ TERMUX_PKG_DESCRIPTION="an 802.11 WEP and WPA-PSK keys cracking program"
 TERMUX_PKG_SRCURL=https://Auxilus.github.io/aircrack-ng.tar.gz
 TERMUX_PKG_SHA256=9992129ce2d3b5297e422e529a8230a6a449df9fec2edd04636d206c6851e7d5
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
+TERMUX_PKG_DEPENDS="libnl, libnl-dev" 
 TERMUX_PKG_BUILD_IN_SRC=yes
