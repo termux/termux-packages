ba1ff28f878bd8364bc1284d438ae12240ab51ec
Auxilus <raghavsphadke@gmail.com>
2017-10-18 07:51:41 +0530

    Dependencies fix

--- packages/build.sh
+++ packages/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_DESCRIPTION="an 802.11 WEP and WPA-PSK keys cracking program"
 TERMUX_PKG_SRCURL=https://Auxilus.github.io/aircrack-ng.tar.gz
 TERMUX_PKG_SHA256=9992129ce2d3b5297e422e529a8230a6a449df9fec2edd04636d206c6851e7d5
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_DEPENDS="libnl, libnl-dev" 
+TERMUX_PKG_DEPENDS="libnl, libnl-dev, openssl,openssl-dev " 
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 # Some of the scripts such as `airmon-ng` are defaultly stored at sbin dir so we have to move them to $PREFIX/bin
