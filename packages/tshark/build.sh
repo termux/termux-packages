4adc838a641fb371f52c36c2e15b65828818dcb8
Henrik Grimler <hgrimler@kth.se>
2017-10-18 10:01:20 +0200

    tshark: add libpcap to dependencies.
    I guess the -dev packages could be moved to build-dependencies?

--- packages/tshark/build.sh
+++ packages/tshark/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_VERSION=2.4.2
 TERMUX_PKG_SRCURL=https://github.com/wireshark/wireshark/archive/wireshark-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b9b60b3a23e2f24f0f9a75d67364b8eef649960c9d1926697739825a3a7a9861
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_DEPENDS="libnl, openssl, libpcap-dev"
+TERMUX_PKG_DEPENDS="libnl, openssl, libpcap, libpcap-dev"
 # As GUI is not supported by termux we need to disable wireshark
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-qt=no --disable-wireshark"
 
