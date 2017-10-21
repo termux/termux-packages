0886efcb8364c697bcca1d2d02b159acb9c2773b
Oliver Schmidhauser <oli@glow.li>
2017-10-22 01:02:12 +0200

    Provide a better description for the rcs package (#1691)
    When i misspelled `cp` to `co` it told me to install the package `rcs`
    for it. Not knowing what it was I called `apt show rcs`. But the
        description didn't help me at all.
    So I copied the description from Debian.

--- packages/rcs/build.sh
+++ packages/rcs/build.sh
@@ -1,7 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/s/rcs
-TERMUX_PKG_DESCRIPTION="comp"
+TERMUX_PKG_DESCRIPTION="The GNU Revision Control System
+ The Revision Control System (RCS) manages multiple
+ revisions of files. RCS automates the storing, retrieval,
+ logging, identification, and merging of revisions. RCS is
+ useful for text that is revised frequently, for example
+ programs, documentation, graphics, papers, and form letters."
 TERMUX_PKG_VERSION=5.9.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://gnu.uberglobalmirror.com/rcs/rcs-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_SHA256=063d5a0d7da1821754b80c639cdae2c82b535c8ff4131f75dc7bbf0cd63a5dff
