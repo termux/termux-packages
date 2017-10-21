fe8aca072efc311c3bc7160f9df9828222b8b94c
Auxilus <raghavsphadke@gmail.com>
2017-10-21 10:41:10 -0700

    add package GNU barcode

---
+++ packages/barcode/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/barcode/
+TERMUX_PKG_DESCRIPTION="tool to convert text strings to printed bars"
+TERMUX_PKG_VERSION=0.99
+TERMUX_PKG_SRCURL=http://mirrors.kernel.org/gnu/barcode/barcode-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=e87ecf6421573e17ce35879db8328617795258650831affd025fba42f155cdc6
+TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
+
+termux_step_post_configure () {
+
+          rm plessey.c code11.c
+          wget https://raw.githubusercontent.com/Auxilus/Auxilus.github.io/master/code11.c
+          wget https://raw.githubusercontent.com/Auxilus/Auxilus.github.io/master/plessey.c
+
+}
