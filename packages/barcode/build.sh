2b1c58842a00421aad55df868d2886d20a6beb74
Auxilus <raghavsphadke@gmail.com>
2017-10-22 00:01:28 +0530

    Update build.sh

--- packages/barcode/build.sh
+++ packages/barcode/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_VERSION=0.99
 TERMUX_PKG_SRCURL=http://mirrors.kernel.org/gnu/barcode/barcode-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e87ecf6421573e17ce35879db8328617795258650831affd025fba42f155cdc6
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
+TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_post_configure () {
 
