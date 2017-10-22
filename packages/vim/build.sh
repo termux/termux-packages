8060b8be5a0ed3941e8db695def1f27a4c2195a4
Auxilus <raghavsphadke@gmail.com>
2017-10-22 09:14:30 -0700

    update vim to patch 8.0.1212

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_HOMEPAGE=http://www.vim.org/
 TERMUX_PKG_DEPENDS="ncurses, vim-runtime"
 
 # Vim 8.0 patches described at ftp://ftp.vim.org/pub/vim/patches/8.0/README
-TERMUX_PKG_VERSION=8.0.1203
-TERMUX_PKG_SHA256=a2facf294111a71c04cbd49d4681066be442005e7944d4eef7cd6593052cb921
+TERMUX_PKG_VERSION=8.0.1212
+TERMUX_PKG_SHA256=896f6aeeeceb829f02a3c7cc37ab6f5f159246670accff39bc7d3c03c7bfd8e0
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 vim_cv_getcwd_broken=no
