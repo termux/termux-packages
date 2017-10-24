5b9584ffba6af19e05bfaf0baa48079972b5c586
Auxilus <raghavsphadke@gmail.com>
2017-10-24 20:33:00 +0530

    Update units to 2.15

--- packages/units/build.sh
+++ packages/units/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/units/
 TERMUX_PKG_DESCRIPTION="Converts between different systems of units"
-TERMUX_PKG_VERSION=2.14
+TERMUX_PKG_VERSION=2.15
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/units/units-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9d33893d82f3ddd831d5822992007c40bcd0826ae67d3cbc96539951fb0a82e8
+TERMUX_PKG_SHA256=25d6334fafc801e15b3b0388e207e1efc5d4ae88af6dc7b17d3913de6441f47c
 TERMUX_PKG_DEPENDS="readline, libandroid-support"
