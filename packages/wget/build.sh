10ddc1871bf3af559b4f765ab71e9e12027ce69e
Auxilus <raghavsphadke@gmail.com>
2017-10-26 23:27:16 +0530

    Update wget to 1.19.2
    Also the `*.tar.xz` is changed to `*.tar.gz`

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
 TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
-TERMUX_PKG_VERSION=1.19.1
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=0c950b9671881222a4d385b013c9604e98a8025d1988529dfca0e93617744cd2
+TERMUX_PKG_VERSION=1.19.2
+TERMUX_PKG_SRCURL=http://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4f4a673b6d466efa50fbfba796bd84a46ae24e370fa562ede5b21ab53c11a920
 TERMUX_PKG_DEPENDS="pcre, openssl, libuuid, libandroid-support"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl --disable-iri"
