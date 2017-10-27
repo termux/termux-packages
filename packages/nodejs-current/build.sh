b0ebb9d56cb2de7c7b66539cd627d4264f6ca2dd
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-27 22:23:11 +0200

    nodejs-current: Update from 8.7.0 to 8.8.1

--- packages/nodejs-current/build.sh
+++ packages/nodejs-current/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=8.7.0
-TERMUX_PKG_SHA256=c7184526e5504fa2a91aff1aacd7c344451da539d73775b6d9ea8efe948d0fa8
-TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=8.8.1
+TERMUX_PKG_SHA256=5ce2c47ab779992db49942f757a01cc5131db5cef4e0dd270e48151b0887b57a
+TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
