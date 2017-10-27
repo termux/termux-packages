9bda49ea4f5a7b1f117dd9778ecf5a1e7e714937
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-27 23:24:40 +0200

    wget: Depend on libunistring

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -3,5 +3,5 @@ TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS 
 TERMUX_PKG_VERSION=1.19.2
 TERMUX_PKG_SHA256=d59a745ad2c522970660bb30d38601f9457b151b322e01fa20a5a0da0f55df07
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
-TERMUX_PKG_DEPENDS="pcre, openssl, libuuid, libandroid-support"
+TERMUX_PKG_DEPENDS="pcre, openssl, libuuid, libandroid-support, libunistring"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl --disable-iri"
