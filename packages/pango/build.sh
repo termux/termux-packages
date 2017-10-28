10a0a02cdf5016eaa98d54bf79abed212d6fca77
Fredrik Fornwall <fredrik@fornwall.net>
2017-10-28 08:35:27 +0200

    pango: Update from 1.40.12 to 1.40.13

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
-TERMUX_PKG_VERSION=1.40.12
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=75f1a9a8e4e2b28cbc078b50c1fa927ee4ded994d1ade97c5603e2d1f3161cfc
+TERMUX_PKG_VERSION=1.40.13
+TERMUX_PKG_SHA256=f84e98db1078772ff4935b40a1629ff82ef0dfdd08d2cbcc0130c8c437857196
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="fontconfig,glib,harfbuzz,libcairo"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libcairo-dev, libpixman-dev, fontconfig-dev, harfbuzz-dev"
