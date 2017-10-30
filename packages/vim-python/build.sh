73beb61836f7e6e9bb50f5c55be569d5a7c3e3b4
Auxilus <raghavsphadke@gmail.com>
2017-10-30 20:58:54 +0530

    Update build.sh

--- packages/vim-python/build.sh
+++ packages/vim-python/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_HOMEPAGE=http://www.vim.org/
 TERMUX_PKG_DEPENDS="ncurses, vim-runtime, python"
 
 # Vim 8.0 patches described at ftp://ftp.vim.org/pub/vim/patches/8.0/README
-TERMUX_PKG_VERSION=8.0.1227
-TERMUX_PKG_SHA256=7b935c680a902fde35c697f2825d8e3956b9aa4f28484baf06cd5ee2e92bb974
+TERMUX_PKG_VERSION=8.0.1238
+TERMUX_PKG_SHA256=0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 vim_cv_getcwd_broken=no
