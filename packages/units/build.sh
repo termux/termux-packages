d7d99570e04ad85ef02cfc8dec6ba172cd8bc3d8
Auxilus <raghavsphadke@gmail.com>
2017-11-01 21:06:06 +0530

    Update build.sh

--- packages/units/build.sh
+++ packages/units/build.sh
@@ -4,3 +4,4 @@ TERMUX_PKG_VERSION=2.16
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/units/units-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dcf3f78482a13b150fc6bf0f8d611510816a3424bc2b26229b85b1cadeb81686
 TERMUX_PKG_DEPENDS="readline, libandroid-support"
+TERMUX_PKG_BUILD_IN_SRC=yes
