1fb3e9fca06a788e6972fc01f6bc65e1cd4f572d
Auxilus <raghavsphadke@gmail.com>
2017-11-05 17:02:54 +0530

    add package: ethtool

---
+++ root-packages/ethtool/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=hhttps://www.kernel.org/pub/software/network/ethtool/
+TERMUX_PKG_DESCRIPTION="standard Linux utility for controlling network drivers and hardware, particularly for wired Ethernet devices"
+TERMUX_PKG_VERSION=4.11 
+TERMUX_PKG_SRCURL=https://www.kernel.org/pub/software/network/ethtool/ethtool-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=af2fd9692f3159d3ab1e41e6f9b7d8db2a4693f1cb22348c88ba89f70f0e6503
+TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
+TERMUX_PKG_DEPENDS="libnl, libnl-dev" 
+TERMUX_PKG_BUILD_IN_SRC=yes
