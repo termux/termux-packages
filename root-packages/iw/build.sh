dedca8bf7d4874e0309ffa55628aa3892003f197
Auxilus <raghavsphadke@gmail.com>
2017-11-05 15:23:01 +0530

    move iw to termux-root-packages
    https://github.com/termux/termux-packages/pull/1667

---
+++ root-packages/iw/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://wireless.wiki.kernel.org/en/users/documentation/iw
+TERMUX_PKG_DESCRIPTION="CLI configuration utility for wireless devices"
+TERMUX_PKG_VERSION=4.9
+TERMUX_PKG_SRCURL=https://www.kernel.org/pub/software/network/iw/iw-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=324cc805fad52cba2c16b9ab569906889fb645cc962aac4cfda1db85d2de97ce
+TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
+TERMUX_PKG_DEPENDS="libnl, libnl-dev, pkg-config"
+TERMUX_PKG_BUILD_IN_SRC=yes
