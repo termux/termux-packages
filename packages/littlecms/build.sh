06145af9bc95728d579c6c2a1d718387ddf8444e
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-07 02:43:03 +0100

    littlecms: Update from 2.8 to 2.9

--- packages/littlecms/build.sh
+++ packages/littlecms/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.littlecms.com/
 TERMUX_PKG_DESCRIPTION="Color management library"
-TERMUX_PKG_VERSION=2.8
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/lcms/lcms/${TERMUX_PKG_VERSION}/lcms2-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=66d02b229d2ea9474e62c2b6cd6720fde946155cd1d0d2bffdab829790a0fb22
+TERMUX_PKG_VERSION=2.9
+TERMUX_PKG_SHA256=00756bed09ce059a68289f10de56b00267667647393ddc30400cb87c0d9037d5
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/lcms/lcms/${TERMUX_PKG_VERSION}/lcms2-${TERMUX_PKG_VERSION}.tar.gz
