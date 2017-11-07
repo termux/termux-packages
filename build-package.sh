acf993adf2d8f771b60ea20cd9f308ab352932d0
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-07 02:26:49 +0100

    cmake: Update from 3.9.4 to 3.9.5

--- build-package.sh
+++ build-package.sh
@@ -88,7 +88,7 @@ termux_setup_golang() {
 	( cd "$TERMUX_COMMON_CACHEDIR"; tar xf "$TERMUX_BUILDGO_TAR"; mv go "$TERMUX_BUILDGO_FOLDER"; rm "$TERMUX_BUILDGO_TAR" )
 }
 
-# Utility function for cmake-built packages to setup a current ninja.
+# Utility function to setup a current ninja build system.
 termux_setup_ninja() {
 	local NINJA_VERSION=1.8.2
 	local NINJA_FOLDER=$TERMUX_COMMON_CACHEDIR/ninja-$NINJA_VERSION
@@ -103,7 +103,7 @@ termux_setup_ninja() {
 	export PATH=$NINJA_FOLDER:$PATH
 }
 
-# Utility function for cmake-built packages to setup a current meson.
+# Utility function to setup a current meson build system.
 termux_setup_meson() {
 	termux_setup_ninja
 	local MESON_VERSION=0.43.0
@@ -157,10 +157,10 @@ termux_setup_meson() {
 	fi
 }
 
-# Utility function for cmake-built packages to setup a current cmake.
+# Utility function to setup a current cmake build system
 termux_setup_cmake() {
 	local TERMUX_CMAKE_MAJORVESION=3.9
-	local TERMUX_CMAKE_MINORVERSION=4
+	local TERMUX_CMAKE_MINORVERSION=5
 	local TERMUX_CMAKE_VERSION=$TERMUX_CMAKE_MAJORVESION.$TERMUX_CMAKE_MINORVERSION
 	local TERMUX_CMAKE_TARNAME=cmake-${TERMUX_CMAKE_VERSION}-Linux-x86_64.tar.gz
 	local TERMUX_CMAKE_TARFILE=$TERMUX_PKG_TMPDIR/$TERMUX_CMAKE_TARNAME
@@ -168,7 +168,7 @@ termux_setup_cmake() {
 	if [ ! -d "$TERMUX_CMAKE_FOLDER" ]; then
 		termux_download https://cmake.org/files/v$TERMUX_CMAKE_MAJORVESION/$TERMUX_CMAKE_TARNAME \
 		                "$TERMUX_CMAKE_TARFILE" \
-		                6d8573377fc7fca86ed1952b76f62880e2159a3de6508761dd0d619a8e96551b
+		                bbe7e2ea703835661bb92fedd485d62dc587ee36daaedaa0a7bebcbf937a1cea
 		rm -Rf "$TERMUX_PKG_TMPDIR/cmake-${TERMUX_CMAKE_VERSION}-Linux-x86_64"
 		tar xf "$TERMUX_CMAKE_TARFILE" -C "$TERMUX_PKG_TMPDIR"
 		mv "$TERMUX_PKG_TMPDIR/cmake-${TERMUX_CMAKE_VERSION}-Linux-x86_64" \
