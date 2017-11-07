19228a4a51eda54ad2a5f42aecc627a07f6c8d74
Stephen <stephengroat@users.noreply.github.com>
2017-11-06 20:20:50 -0800

    Update build.sh

--- packages/wireguard-tools/build.sh
+++ packages/wireguard-tools/build.sh
@@ -4,3 +4,11 @@ TERMUX_PKG_VERSION=0.0.20171101
 TERMUX_PKG_SRCURL=https://git.zx2c4.com/WireGuard/snapshot/WireGuard-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=096b6482a65e566c7bf8c059f5ee6aadb2de565b04b6d810c685f1c377540325
 TERMUX_PKG_DEPENDS="libmnl"
+
+termux_step_make() {
+  make -C src/tools
+}
+
+termux_step_make_install() {
+  make -C src/tools PREFIX=$TERMUX_PREFIX
+}
