c11e987af21bb78a9582cba7799fe686d8950a94
Stephen <stephengroat@users.noreply.github.com>
2017-11-07 13:21:50 -0800

    allow make to run without *akefile if args are set

--- build-package.sh
+++ build-package.sh
@@ -934,7 +934,7 @@ termux_step_post_configure () {
 }
 
 termux_step_make() {
-	if ls ./*akefile &> /dev/null; then
+	if ls ./*akefile &> /dev/null || [ ! -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
 		if [ -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
 			make -j $TERMUX_MAKE_PROCESSES
 		else
@@ -944,7 +944,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	if ls ./*akefile &> /dev/null; then
+	if ls ./*akefile &> /dev/null || [ ! -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
 		: "${TERMUX_PKG_MAKE_INSTALL_TARGET:="install"}"
 		# Some packages have problem with parallell install, and it does not buy much, so use -j 1.
 		if [ -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
