3c0504e87c45d46e6eaefe38a05b11daafc0e37b
Auxilus <raghavsphadke@gmail.com>
2017-11-08 17:56:53 +0530

    fix checksum

--- packages/hashcat/build.sh
+++ packages/hashcat/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://hashcat.net/hashcat
 TERMUX_PKG_DESCRIPTION="World's fastest and most advanced password recovery utility"
 TERMUX_PKG_VERSION=4.0.0
-TERMUX_PKG_SHA256=05a87eb018507b24afca970081f067e64441460319fb75ca1e64c4a1f322b80b
+TERMUX_PKG_SHA256=e14c169d6966830e7b7423e17e678f3333e074ec50dae50bdde40e8e0e8658be
 TERMUX_PKG_SRCURL=https://github.com/hashcat/hashcat/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
