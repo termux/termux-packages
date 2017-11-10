9a304a286674af5d12ab62e742ad12a5d1f895df
Auxilus <raghavsphadke@gmail.com>
2017-11-10 08:04:53 +0530

    Update build.sh

--- packages/hashcat/build.sh
+++ packages/hashcat/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="World's fastest and most advanced password recovery util
 TERMUX_PKG_VERSION=4.0.0
 TERMUX_PKG_SHA256=e14c169d6966830e7b7423e17e678f3333e074ec50dae50bdde40e8e0e8658be
 TERMUX_PKG_SRCURL=https://github.com/hashcat/hashcat/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libandroid-support libandroid-support-dev"
+TERMUX_PKG_DEPENDS="libandroid-support, libandroid-support-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 
