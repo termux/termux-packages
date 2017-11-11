0d2991c980a7427fddfae81b93092c33620c1575
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-11 19:34:14 +0100

    aria2: Update from 1.33.0 to 1.33.1

--- packages/aria2/build.sh
+++ packages/aria2/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://aria2.github.io
 TERMUX_PKG_DESCRIPTION="Download utility supporting HTTP/HTTPS, FTP, BitTorrent and Metalink"
-TERMUX_PKG_VERSION=1.33.0
-TERMUX_PKG_SHA256=996e3fc2fd07ce2dd517e20a1f79b8b3dbaa5c7e27953b5fc19dae38f3874b8c
+TERMUX_PKG_VERSION=1.33.1
+TERMUX_PKG_SHA256=2539e4844f55a1f1f5c46ad42744335266053a69162e964d9a2d80a362c75e1b
 TERMUX_PKG_SRCURL=https://github.com/aria2/aria2/releases/download/release-${TERMUX_PKG_VERSION}/aria2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="c-ares, openssl, libxml2"
 # sqlite3 is only used for loading cookies from firefox or chrome:
