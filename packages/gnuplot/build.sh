ef6ddba894611a652969e5a8d4b67ff1f1af655c
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-15 20:35:13 +0100

    gnuplot: Update from 5.2.0 to 5.2.1

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://gnuplot.info/
 TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
-TERMUX_PKG_VERSION=5.2.0
-TERMUX_PKG_SHA256=7dfe6425a1a6b9349b1fb42dae46b2e52833b13e807a78a613024d6a99541e43
+TERMUX_PKG_VERSION=5.2.1
+TERMUX_PKG_SHA256=7dc6b0fb6b321691e89e344387310a8e6614f0c4e5eb15a90dc742a53d807e88
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-x --with-lua=no --with-bitmap-terminals"
 TERMUX_PKG_DEPENDS="libandroid-support, readline, pango, libgd"
