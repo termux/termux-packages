7712869cf58889541bc4f900bc32aa60805d0e48
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-15 23:53:25 +0100

    Bump packages after libogg update

--- packages/libsndfile/build.sh
+++ packages/libsndfile/build.sh
@@ -1,8 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://www.mega-nerd.com/libsndfile
-# Await 1.0.29 for update which includes fix for Android build,
-# https://github.com/erikd/libsndfile/issues/295
-TERMUX_PKG_VERSION=1.0.27
-TERMUX_PKG_SRCURL=http://www.mega-nerd.com/libsndfile/files/libsndfile-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a391952f27f4a92ceb2b4c06493ac107896ed6c76be9a613a4731f076d30fac0
+TERMUX_PKG_DESCRIPTION="Library for reading/writing audio files"
+# Use a git master snapshot until 1.0.29 is released:
+TERMUX_PKG_VERSION=1.0.29~pre1
+TERMUX_PKG_SRCURL=https://github.com/erikd/libsndfile/archive/826d5296da54c016e3cb0f7f00de3b9e295b9c4a.zip
+TERMUX_PKG_SHA256=84651201a8468c448f1fd172a48ccf47f3761d23fd0f59bcaaf908050e9eeb7c
 TERMUX_PKG_DEPENDS="libflac, libvorbis"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sqlite --disable-alsa"
+TERMUX_PKG_RM_AFTER_INSTALL="bin/ share/man/man1/"
+
+termux_step_pre_configure () {
+	sh autogen.sh
+}
