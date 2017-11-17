12f13c7cf6943a0092b0a01d1f914b30f0adb7b0
Cswl Coldwind <cswl1337@gmail.com>
2017-11-17 09:35:14 +0545

    Update tsu to v0.3, fixes su detection

--- packages/tsu/build.sh
+++ packages/tsu/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/cswl/tsu
 TERMUX_PKG_DESCRIPTION="A su wrapper for Termux"
-TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_VERSION=0.3
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
-	termux_download https://raw.githubusercontent.com/cswl/tsu/717b902303a9afd546e46722f36feca549376471/tsu \
+	termux_download https://raw.githubusercontent.com/cswl/tsu/f0d3a5c6d179c34be18fb91b14f2f3da2100365a/tsu \
 	                $TERMUX_PREFIX/bin/tsu \
-	                519fe419b009b3d9772602255a626c4077cb7b0a5a109b07844fb44d6ec11e9a
+	                e2babd61269765fec16a199b84fbefc3fa852b5d653bae4e52fc1f54b81f4966
 	touch $TERMUX_PREFIX/bin/tsu
 	chmod +x $TERMUX_PREFIX/bin/tsu
 }
