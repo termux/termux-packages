5f2de3eb3e4f8724fad6f6c2898c2315e90f723d
Henrik Grimler <Henrik@Grimler.se>
2017-11-18 23:23:01 +0100

    poppler: disable multithreading. Fixes #1657.
    valgrind still complains though.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_VERSION=0.61.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=1266096343f5163c1a585124e9a6d44474e1345de5cdfe55dc7b47357bcfcda9
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
@@ -9,4 +10,5 @@ TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, little
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_GLIB=OFF
 -DENABLE_XPDF_HEADERS=ON
+-DENABLE_MULTITHREAD=OFF
 "
