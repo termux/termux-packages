fccb3f172ed190262a488758daf956faa0431aef
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-20 15:28:21 +0100

    libnettle: Update from 3.3 to 3.4

--- packages/libnettle/build.sh
+++ packages/libnettle/build.sh
@@ -1,6 +1,6 @@
-TERMUX_PKG_HOMEPAGE=http://www.lysator.liu.se/~nisse/nettle/
+TERMUX_PKG_HOMEPAGE=https://www.lysator.liu.se/~nisse/nettle/
 TERMUX_PKG_DESCRIPTION="Cryptographic library that is designed to fit easily in more or less any context"
 TERMUX_PKG_DEPENDS="libgmp"
-TERMUX_PKG_VERSION=3.3
+TERMUX_PKG_VERSION=3.4
+TERMUX_PKG_SHA256=ae7a42df026550b85daca8389b6a60ba6313b0567f374392e54918588a411e94
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/nettle/nettle-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=46942627d5d0ca11720fec18d81fc38f7ef837ea4197c1f630e71ce0d470b11e
