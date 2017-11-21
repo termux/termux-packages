24a6f380abaa035c4af89b6fa08f863ac1b6ba7f
Stephen <stephengroat@users.noreply.github.com>
2017-11-21 13:55:58 -0800

    add nim

---
+++ packages/nim/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://nim-lang.org/
+TERMUX_PKG_VERSION=0.17.2
+TERMUX_PKG_SHA256=aaff1b5023fc4a5708f1d7d9fd8e2a29f1a7f58bf496532ff1e9d7e7c7ec82bd
+TERMUX_PKG_SRCURL=https://nim-lang.org/download/nim-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_DESCRIPTION="Nim is a systems and applications programming language. Statically typed and compiled, it provides unparalleled performance in an elegant package"
+TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob"
+TERMUX_PKG_BUILD_IN_SRC="yes"
