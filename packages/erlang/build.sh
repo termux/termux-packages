f8af14d10a296d26e7ea48cfc718486bafd86f69
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-22 16:43:28 +0100

    erlang: Update from 20.1 to 20.1.6

--- packages/erlang/build.sh
+++ packages/erlang/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.erlang.org/
 TERMUX_PKG_DESCRIPTION="General-purpose concurrent functional programming language developed by Ericsson"
-TERMUX_PKG_VERSION=20.1
-TERMUX_PKG_SHA256=900d35eb563607785a8e27f4b4c03cf6c98b4596028c5d6958569ddde5d4ddbf
+TERMUX_PKG_VERSION=20.1.6
+TERMUX_PKG_SHA256=21358072bc7052639b0af33d726e5d38cd4d2bb92581ceb227a347282482476a
+TERMUX_PKG_SRCURL=https://github.com/erlang/otp/archive/OTP-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="openssl, ncurses, libutil"
-TERMUX_PKG_SRCURL="http://erlang.org/download/otp_src_${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_HOSTBUILD="yes"
 TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-javac --with-ssl=${TERMUX_PREFIX} --with-termcap"
@@ -12,6 +12,7 @@ TERMUX_PKG_EXTRA_MAKE_ARGS="noboot"
 termux_step_post_extract_package() {
 	# We need a host build every time:
 	rm -Rf "$TERMUX_PKG_HOSTBUILD_DIR"
+	./otp_build autoconf
 }
 
 termux_step_host_build () {
