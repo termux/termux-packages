cfc93cb35a7779c04b63ed64e7cf1b27334b8ea9
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-25 23:57:36 +0100

    neovim: Try termxux clipboard before tmux
    Fixes #1828.

--- packages/neovim/build.sh
+++ packages/neovim/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://neovim.io/
 TERMUX_PKG_DESCRIPTION="Ambitious Vim-fork focused on extensibility and agility (nvim)"
-local _COMMIT=eacd788cf56fee586901f4252ae7492aaf007b3f
-TERMUX_PKG_VERSION=0.2.2~2017.11.16
-TERMUX_PKG_SHA256=bcbbde12729a86f3952daf363826ea70d5ee9fc9126eca6e5474beacb8cd35d1
+local _COMMIT=b57d9a4ff08fc1ef8db79d422b441b792493ff4e
+TERMUX_PKG_VERSION=0.2.3~2017.11.25
+TERMUX_PKG_SHA256=356f04786e443c387f44bd4946067a87bcb50534896dd55f86d4ab61b51c1711
 TERMUX_PKG_SRCURL=https://github.com/neovim/neovim/archive/${_COMMIT}.zip
 TERMUX_PKG_DEPENDS="libuv, libmsgpack, libandroid-support, libvterm, libtermkey, libutil, liblua, libunibilium"
 TERMUX_PKG_HOSTBUILD=true
@@ -24,9 +24,7 @@ termux_step_host_build () {
 
 	mkdir -p $TERMUX_PKG_HOSTBUILD_DIR/deps
 	cd $TERMUX_PKG_HOSTBUILD_DIR/deps
-	cmake \
-		-DENABLE_JEMALLOC=OFF \
-		$TERMUX_PKG_SRCDIR/third-party
+	cmake $TERMUX_PKG_SRCDIR/third-party
 	make -j 1
 
 	cd $TERMUX_PKG_SRCDIR
