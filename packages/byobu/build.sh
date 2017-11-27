ba5eead53635928edd9ae1fb369dea33b01066d3
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-27 14:57:51 +0100

    Remove empty TERMUX_PKG_REVISION=

--- packages/byobu/build.sh
+++ packages/byobu/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://byobu.co/
 TERMUX_PKG_DESCRIPTION="Byobu is a GPLv3 open source text-based window manager and terminal multiplexer"
 TERMUX_PKG_VERSION=5.123
-TERMUX_PKG_REVISION=
 TERMUX_PKG_SRCURL=https://launchpad.net/byobu/trunk/${TERMUX_PKG_VERSION}/+download/byobu_${TERMUX_PKG_VERSION}.orig.tar.gz
 TERMUX_PKG_SHA256=2e5a5425368d2f74c0b8649ce88fc653420c248f6c7945b4b718f382adc5a67d
 TERMUX_PKG_DEPENDS="gawk, tmux"
