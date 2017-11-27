201db6d715b8a73c0ca6a410555a6fe1a0fd0331
Fredrik Fornwall <fredrik@fornwall.net>
2017-11-27 14:51:44 +0100

    stunnel: Update from 5.43 to 5.44

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.stunnel.org/
 TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary applications"
-TERMUX_PKG_VERSION=5.43
-TERMUX_PKG_SHA256=05915babf705a0494886a72a7367913d403d07fc908ebb7b380d639e2d8bcee2
+TERMUX_PKG_VERSION=5.44
+TERMUX_PKG_SHA256=990a325dbb47d77d88772dd02fbbd27d91b1fea3ece76c9ff4461eca93f12299
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="openssl, libutil"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
