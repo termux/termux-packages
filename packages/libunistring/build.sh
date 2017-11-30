6494b463d94fb918cc50a97efb6ad4eb55de3e46
Auxilus <raghavsphadke@gmail.com>
2017-11-30 19:08:31 +0530

    Fix checksum

--- packages/libunistring/build.sh
+++ packages/libunistring/build.sh
@@ -2,5 +2,5 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libunistring/
 TERMUX_PKG_DESCRIPTION="Library providing functions for manipulating Unicode strings"
 TERMUX_PKG_VERSION=0.9.8
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libunistring/libunistring-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7b9338cf52706facb2e18587dceda2fbc4a2a3519efa1e15a3f2a68193942f80
+TERMUX_PKG_SHA256=b792f2bd05d0fa7b339e39e353da7232b2e514e0db2cf5ed95beeff3feb53cf5
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_uselocale=no"
