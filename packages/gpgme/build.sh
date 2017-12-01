650eb48b849dd82d754baa84e229d1e9c96737f5
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-01 12:53:03 +0100

    gpgme: Bump revision (fixes #1873)

--- packages/gpgme/build.sh
+++ packages/gpgme/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library designed to make access to GnuPG easier"
 TERMUX_PKG_DEPENDS="gnupg2, libassuan, libgpg-error"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libgpg-error"
 TERMUX_PKG_VERSION=1.9.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1b29fedb8bfad775e70eafac5b0590621683b2d9869db994568e6401f4034ceb
 # Use "--disable-gpg-test" to avoid "No rule to make target `../../src/libgpgme-pthread.la":
