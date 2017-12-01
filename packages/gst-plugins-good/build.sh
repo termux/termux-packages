6ddbb3dd54e16c136ff8dd30b3ae7fac1a706b5a
Stephen <stephengroat@users.noreply.github.com>
2017-11-30 16:17:48 -0800

    add depends to gst-plugins-good
    #1169

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_VERSION=1.12.3
 TERMUX_PKG_SHA256=13e7f479296891fef5a686438f20ba7d534680becf2269ecc5ee24aa83b45f03
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="gst-plugins-base,libcaca,libsoup,libjpeg-turbo,libpng,libflac,libbz2,libvpx,libpulseaudio"
+TERMUX_PKG_DEPENDS="gst-plugins-base,libcaca,libsoup,libjpeg-turbo,libpng,libflac,libbz2,libvpx,libpulseaudio,glib,gstreamer"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-android_media
 --disable-cairo
