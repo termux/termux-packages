88a899d5d7a802cd3236ae82a2e1f28082cd9ba9
Stephen <stephengroat@users.noreply.github.com>
2017-12-01 07:20:40 -0800

    add glib-dev to gdk-pixbuf dev depends
    #1169

--- packages/gdk-pixbuf/build.sh
+++ packages/gdk-pixbuf/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/gdk-pixbuf/
 TERMUX_PKG_DESCRIPTION="Library for image loading and manipulation"
 _MAJOR_VERSION=2.36
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=1b6e5eef09d98f05f383014ecd3503e25dfb03d7e5b5f5904e5a65b049a6a4d8
 TERMUX_PKG_SRCURL=ftp://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/${_MAJOR_VERSION}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo"
-TERMUX_PKG_DEVPACKAGE_DEPENDS="libpng-dev"
+TERMUX_PKG_DEVPACKAGE_DEPENDS="libpng-dev, glib-dev"
 # Avoid runtime test which does not work when cross compiling:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gio-sniffing --disable-glibtest"
