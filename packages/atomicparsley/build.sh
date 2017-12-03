4ee05bca0d608831228559548045f39971bd7192
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-03 14:23:53 +0100

    atomicparsley: Donload tarball from bitbucket

--- packages/atomicparsley/build.sh
+++ packages/atomicparsley/build.sh
@@ -1,21 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://bitbucket.org/wez/atomicparsley
-TERMUX_PKG_DESCRIPTION="read, parse and set metadata of MPEG-4 and 3gp files"
+TERMUX_PKG_DESCRIPTION="Read, parse and set metadata of MPEG-4 and 3gp files"
 TERMUX_PKG_VERSION=0.9.6
-TERMUX_PKG_SRCURL=https://bitbucket.org/wez/atomicparsley/get/9183fff907bf.zip
-TERMUX_PKG_SHA256=2c8c2ad9c400637396c43d0212a2a26d3866b15004a46525d15265d3a1543672
-TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_CLANG=no
-termux_step_extract_package() {
-	echo $PWD
-	cd $TERMUX_PKG_CACHEDIR
-	termux_download \
-		$TERMUX_PKG_SRCURL \
-		$TERMUX_PKG_CACHEDIR/9183fff907bf.zip \
-		$TERMUX_PKG_SHA256
-			
-	unzip 9183fff907bf.zip
-	mv wez-atomicparsley-9183fff907bf ../src
-}
+TERMUX_PKG_SRCURL=https://bitbucket.org/wez/atomicparsley/get/${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=e28d46728be86219e6ce48695ea637d831ca0170ca6bdac99810996a8291ee50
+
 termux_step_pre_configure() {
 	./autogen.sh
 }
