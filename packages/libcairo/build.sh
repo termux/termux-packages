73afb85207053b1f8ce5dcc998fd7a8fbe926b36
Stephen <stephengroat@users.noreply.github.com>
2017-12-04 10:16:03 -0800

    add revision

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://cairographics.org
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
 TERMUX_PKG_VERSION=1.14.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/cairo-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=7e87878658f2c9951a14fc64114d4958c0e65ac47530b8ac3078b2ce41b66a09
 TERMUX_PKG_DEPENDS="libandroid-support, libpixman, fontconfig, freetype, libpng"
