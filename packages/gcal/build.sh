880a80ad49040362fd5bd05872aa9a69524cf2cb
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-05 23:55:10 +0100

    gcal: Do not include share/info/dir

--- packages/gcal/build.sh
+++ packages/gcal/build.sh
@@ -1,8 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gcal/
 TERMUX_PKG_DESCRIPTION="Program for calculating and printing calendars"
 TERMUX_PKG_VERSION=4.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=91b56c40b93eee9bda27ec63e95a6316d848e3ee047b5880ed71e5e8e60f61ab
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gcal/gcal-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_KEEP_INFOPAGES=yes
+
+termux_step_post_make_install() {
+	# XXX: share/info/dir is currently included in emacs.
+	# We should probably make texinfo regenerate that file
+	# just as the man package does with the man database.
+	rm -f $TERMUX_PREFIX/share/info/dir
+}
