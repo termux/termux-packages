dfaf1e308d5c00c81deb3afe23c497637890c3ec
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-05 22:36:18 +0100

    Improve scripts/list-packages.sh

--- scripts/list-packages.sh
+++ scripts/list-packages.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # list-packages.sh - tool to list all package with home pages and descriptions
 
+show_package() {
+	. $1/build.sh
+	local pkg=`basename $1`
+	echo "$pkg($TERMUX_PKG_VERSION): $TERMUX_PKG_HOMEPAGE"
+	echo "       $TERMUX_PKG_DESCRIPTION"
+}
+
 for path in packages/*; do
-	export path
-	export pkg=`basename $path`	
-	bash -c '. $path/build.sh; echo "$pkg($TERMUX_PKG_VERSION): $TERMUX_PKG_HOMEPAGE"; echo "       $TERMUX_PKG_DESCRIPTION"'
+	( show_package $path )
 done
