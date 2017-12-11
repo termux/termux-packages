8f7251891a1ee86b996168d356df46ba5c5bfcce
Stephen <stephengroat@users.noreply.github.com>
2017-12-11 08:10:46 -0800

    cheap trick to fix + issue in package search

--- build-package.sh
+++ build-package.sh
@@ -431,7 +431,8 @@ termux_step_start_build() {
 			while IFS=',' read -ra PKG; do
 				for p in "${PKG[@]}"; do
 					p="$(echo -e "${p}" | tr -d '[:space:]')"
-					apt-get $TERMUX_APT install "^${p}(-dev)?$":any
+					# Install packages and dev packages and escape + with \+ to avoid apt regex
+					apt-get $TERMUX_APT install "^${p//+/\\+}(-dev)?$":any
 					sudo chown -R builder:builder /data
 				done
 			done <<< "${!i}"
@@ -443,7 +444,8 @@ termux_step_start_build() {
 			while IFS=',' read -ra PKG; do
 				for p in "${PKG[@]}"; do
 					p="$(echo -e "${p}" | tr -d '[:space:]')"
-					apt-get $TERMUX_APT install "^${p}(-dev)?$":any
+					# Install packages and dev packages and escape + with \+ to avoid apt regex
+					apt-get $TERMUX_APT install "^${p//+/\\+}(-dev)?$":any
 					sudo chown -R builder:builder /data
 				done
 			done <<< $TERMUX_SUBPKG_DEPENDS
