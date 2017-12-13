017ac1f5b7142c52950df6161a30d99277791dc7
Sam <coltonezekiel@gmail.com>
2017-12-13 08:23:43 +0530

    Create build.sh

---
+++ packages/libsamplerate/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/erikd/libsamplerate
+TERMUX_PKG_DESCRIPTION="libsamplerate (also known as Secret Rabbit Code) is a library for
+perfroming sample rate conversion of audio data"
+TERMUX_PKG_VERSION=0.1.9
+TERMUX_PKG_SRCURL=https://github.com/erikd/libsamplerate/archive/${TERMUX_PKG_VERSION}.zip
+TERMUX_PKG_SHA256=5d6b41a9627ceee8ff125327f8e2d7c0cef58e1e6862da16c0c5a005fae68b95
+TERMUX_PKG_MAINTAINER="Sam @SamSanuch"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
