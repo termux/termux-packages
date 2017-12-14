f3fcbe40b2e258f3da1cb64f10279492ca33948b
Henrik Grimler <henka20@hotmail.com>
2017-12-14 12:59:05 +0100

    espeak: fix floating point exception on arm (#1925)
    * espeak: fix issue on arm (#1680)
    
    * ruby: move exception rule, build-package.sh -> build.sh

--- packages/espeak/build.sh
+++ packages/espeak/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="Compact software speech synthesizer"
 # See https://github.com/espeak-ng/espeak-ng/issues/180
 # about cross compilation of espeak-ng.
 TERMUX_PKG_VERSION=1.49.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=cf7ed86850b99bafe819548c73a6651a74300980dd15f319ff22e2bd72ea20b4
 TERMUX_PKG_SRCURL=https://github.com/espeak-ng/espeak-ng/releases/download/${TERMUX_PKG_VERSION}/espeak-ng-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -24,6 +25,11 @@ termux_step_host_build() {
 	#(cd $TERMUX_PREFIX/share/man/man1 && ln -s -f espeak-ng.1 espeak.1)
 
 	make install
+}
+
+termux_step_pre_configure() {
+	# Oz flag causes problems. See https://github.com/termux/termux-packages/issues/1680:
+	CFLAGS=${CFLAGS/Oz/O2}
 }
 
 termux_step_make() {
