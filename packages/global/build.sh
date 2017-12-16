4f44f2eb4ca64b45e493cc83be307eb5864f98ba
Auxilus <raghavsphadke@gmail.com>
2017-12-17 04:34:04 +0530

    Update global to 6.6.1 (#1938)
    Also move SRCURL to mirrors.kernel.org/gnu

--- packages/global/build.sh
+++ packages/global/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/global/
 TERMUX_PKG_DESCRIPTION="Source code search and browse tools"
-TERMUX_PKG_VERSION=6.5.7
-TERMUX_PKG_SRCURL=http://tamacom.com/global/global-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d9c08fa524f9499b54241cb2d72f8a7df01453b6d5e012a63784ded08e3acd32
+TERMUX_PKG_VERSION=6.6.1
+TERMUX_PKG_SRCURL=http://mirrors.kernel.org/gnu/global/global-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=943dc440382d82454786bfd92b86946961cb2196039eceffd7eb551ac83759e4
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_posix1_2008_realpath=yes
 --with-posix-sort=$TERMUX_PREFIX/bin/sort
