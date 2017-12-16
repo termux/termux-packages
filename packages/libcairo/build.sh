a6d7ffb00b99e46919e3f287545311f864859bf8
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-17 00:04:36 +0100

    libcairo: Update from 1.14.10 to 1.14.12

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://cairographics.org
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
-TERMUX_PKG_VERSION=1.14.10
+TERMUX_PKG_VERSION=1.14.12
+TERMUX_PKG_SHA256=8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/cairo-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7e87878658f2c9951a14fc64114d4958c0e65ac47530b8ac3078b2ce41b66a09
 TERMUX_PKG_DEPENDS="libandroid-support, libpixman, fontconfig, freetype, libpng"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-doc-html --enable-xlib=no"
 TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/html"
