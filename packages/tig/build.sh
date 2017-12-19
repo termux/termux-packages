4ee12b6e7eeaf10a84ce83f14a410427497572f5
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-20 00:26:14 +0100

    tig: Update from 2.3.1 to 2.3.2

--- packages/tig/build.sh
+++ packages/tig/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://jonas.github.io/tig/
 TERMUX_PKG_DESCRIPTION="Ncurses-based text-mode interface for git"
-TERMUX_PKG_VERSION=2.3.1
-TERMUX_PKG_SHA256=4ba555a23475946aa1c51b7f4d70beeb4a5a535cdf9d1f06978d24e2003e9201
+TERMUX_PKG_VERSION=2.3.2
+TERMUX_PKG_SHA256=6410e51c6149d76eac3510d04f9a736139f85e7c881646937d009caacf98cff1
 TERMUX_PKG_SRCURL=https://github.com/jonas/tig/releases/download/tig-$TERMUX_PKG_VERSION/tig-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="ncurses, git, libandroid-support"
