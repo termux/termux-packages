da2e34160fc3441f475207f9e37fd5fbeee943b8
Leonid Plyushch <25881154+xeffyr@users.noreply.github.com>
2017-12-20 01:03:56 +0200

    global: fix path to 'echo' in globash (#1949)
    Fixes error 'globash: line 47: /bin/echo: No such file or directory'

--- packages/global/build.sh
+++ packages/global/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/global/
 TERMUX_PKG_DESCRIPTION="Source code search and browse tools"
 TERMUX_PKG_VERSION=6.6.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://mirrors.kernel.org/gnu/global/global-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=943dc440382d82454786bfd92b86946961cb2196039eceffd7eb551ac83759e4
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
