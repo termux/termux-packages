38d5bebff64907d222e16f9afba6b93e40f8131f
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-20 00:43:39 +0100

    pinentry: Update from 1.0.0 to 1.1.0

--- packages/pinentry/build.sh
+++ packages/pinentry/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/pinentry/index.html
 TERMUX_PKG_DESCRIPTION="Dialog allowing secure password entry"
-TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SHA256=68076686fa724a290ea49cdf0d1c0c1500907d1b759a3bcbfbec0293e8f56570
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=1672c2edc1feb036075b187c0773787b2afd0544f55025c645a71b4c2f79275a
 TERMUX_PKG_DEPENDS="libandroid-support, libassuan, ncurses"
 TERMUX_PKG_BUILD_DEPENDS="libgpg-error"
