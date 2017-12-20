c12cc9c53fae94a051119d72077fb65f5265058e
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-20 02:02:26 +0100

    libgc: Update libatomic_ops from 7.4.6 to 7.4.8

--- packages/libgc/build.sh
+++ packages/libgc/build.sh
@@ -7,13 +7,13 @@ TERMUX_PKG_SHA256=a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d
 TERMUX_PKG_RM_AFTER_INSTALL="share/gc"
 
 termux_step_post_extract_package () {
-	LIBATOMIC_VERSION=7.4.6
+	LIBATOMIC_VERSION=7.4.8
 	LIBATOMIC_FILE=libatomic_ops-${LIBATOMIC_VERSION}.tar.gz
 	test ! -f $TERMUX_PKG_CACHEDIR/$LIBATOMIC_FILE && \
 		termux_download \
 		https://github.com/ivmai/libatomic_ops/releases/download/v${LIBATOMIC_VERSION}/libatomic_ops-${LIBATOMIC_VERSION}.tar.gz \
 		$TERMUX_PKG_CACHEDIR/$LIBATOMIC_FILE \
-		96e88ba450ae5fa10aa8e94e6b151a63ffbe47f8069574bd12da22ae80c686db
+		c405d5524b118c197499bc311b8ebe18f7fe47fc820f2c2fcefde1753fbd436a
 	tar xf $TERMUX_PKG_CACHEDIR/$LIBATOMIC_FILE
 	mv libatomic_ops-${LIBATOMIC_VERSION} libatomic_ops
 	./autogen.sh
