c3acf438a7ecaeb72f1f2c696fad25f5259d0877
Fredrik Fornwall <fredrik@fornwall.net>
2017-12-21 05:26:17 +0100

    weechat: Update from 2.0 to 2.0.1

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
-TERMUX_PKG_VERSION=2.0
-TERMUX_PKG_SHA256=b6e1ea2ca6c70d6909d2a52da386d60f6c22478de128b05ded844c318fa1a149
+TERMUX_PKG_VERSION=2.0.1
+TERMUX_PKG_SHA256=425f3268fc9cb2a70610554380f1d5711bb8471178ef97159ec85918bea77b4b
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="ncurses, libgcrypt, libcurl, libgnutls, libandroid-support"
 # weechat-curses is a symlink to weechat, so remove it:
