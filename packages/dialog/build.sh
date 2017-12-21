d545d6744f956739dfac7edf8784e6de54bfcd82
Henrik Grimler <Henrik@Grimler.se>
2017-12-21 19:01:33 +0100

    dialog: update SHA256

--- packages/dialog/build.sh
+++ packages/dialog/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://invisible-island.net/dialog/
 TERMUX_PKG_DESCRIPTION="Application used in shell scripts which displays text user interface widgets"
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_VERSION="1.3-20170509"
-TERMUX_PKG_SHA256=2ff1ba74c632b9d13a0d0d2c942295dd4e8909694eeeded7908a467d0bcd4756
+TERMUX_PKG_SHA256=20139442119e2eff5c35236e8e5e313c901539008d9cccf8c8ab3851b41267e6
 TERMUX_PKG_SRCURL=http://invisible-island.net/datafiles/release/dialog.tar.gz
 # This will break when a new version is released (the URL unfortunately does not change)
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncursesw --enable-widec --with-pkg-config"
