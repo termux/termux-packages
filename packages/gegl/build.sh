c7c67f92b5f11eca9be52f7a40dff2efb49d83c6
Stephen <stephengroat@users.noreply.github.com>
2017-12-28 08:39:10 -0800

    remove glib from dependencies
    covered by json-glib

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_VERSION=0.3.26
 TERMUX_PKG_SHA256=6eff9844c4776546213f5e187e1ebcf646d0d4804ebc6d3dd62003cd4d5c3fa9
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, glib"
+TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-gdk-pixbuf
 "
