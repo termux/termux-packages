89eb1e66f1d2644f3020f6a9db669d6c18952a5b
Stephen <stephengroat@users.noreply.github.com>
2017-12-30 11:36:15 -0800

    add json-glib-dev to gegl dev depends
    instead of #1935

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_VERSION=0.3.26
 TERMUX_PKG_SHA256=6eff9844c4776546213f5e187e1ebcf646d0d4804ebc6d3dd62003cd4d5c3fa9
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg"
+TERMUX_PKG_DEVPACKAGE_DEPENDS="json-glib-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-gdk-pixbuf
 "
