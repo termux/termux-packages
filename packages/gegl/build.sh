47b3e5afc0223f86126d40b9b889f76bb9ff2616
Stephen <stephengroat@users.noreply.github.com>
2017-12-30 11:38:48 -0800

    add revision

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://gegl.org/
 TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_VERSION=0.3.26
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=6eff9844c4776546213f5e187e1ebcf646d0d4804ebc6d3dd62003cd4d5c3fa9
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg"
