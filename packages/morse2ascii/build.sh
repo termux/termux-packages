e3ef8bb2028c51c032d0a2a62c340e109dfbd675
Auxilus <raghavsphadke@gmail.com>
2017-12-31 01:28:35 +0530

    Update build.sh

--- packages/morse2ascii/build.sh
+++ packages/morse2ascii/build.sh
@@ -1,9 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/sid/morse2ascii
-TERMUX_PKG_DESCRIPTION="A tool  to decode the morse codes from a PCM WAV file as well as from text and RAW PCM files"
+TERMUX_PKG_DESCRIPTION="A tool to decode the morse codes from a PCM WAV file as well as from text and RAW PCM files"
 TERMUX_PKG_VERSION=0.2
-#TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/m/morse2ascii/morse2ascii_${TERMUX_PKG_VERSION}+dfsg.orig.tar.xz
 TERMUX_PKG_SHA256=0d41f544818f27db2fb211b2950e48893ff4856f1d114ab1878c91e6e27e4c49
-#TERMUX_PKG_DEPENDS="libconfuse, libnl, ncurses"
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
 TERMUX_PKG_BUILD_IN_SRC=yes
