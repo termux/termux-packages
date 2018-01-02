f5c1ec8da8bde62cd13c13c48bfc1b1b203da464
Stephen <stephengroat@users.noreply.github.com>
2018-01-02 13:54:55 -0800

    add jpeg8 to libturbo-jpeg
    closes #1891

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://libjpeg-turbo.virtualgl.org/
 TERMUX_PKG_DESCRIPTION="Library for reading and writing JPEG image files"
 TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=9098943b270388727ae61de82adec73cf9f0dbb240b3bc8b172595ebf405b528
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/libjpeg-turbo/${TERMUX_PKG_VERSION}/libjpeg-turbo-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man --with-jpeg8"
