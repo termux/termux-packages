ba15d1cccb13f9a2bf69d2c881b004488a87f59c
Stephen <stephengroat@users.noreply.github.com>
2018-01-04 12:15:07 -0800

    add samefile
    closes #1988

---
+++ packages/samefile/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE="http://www.schweikhardt.net/samefile/"
+TERMUX_PKG_DESCRIPTION="finds files with identical contents, independent of file name"
+TERMUX_PKG_VERSION=2.14
+TERMUX_PKG_SRCURL=http://www.schweikhardt.net/samefile-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
