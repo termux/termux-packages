4975d2e4f37636a56889995bf511b76e2ce7c6a2
its-pointless <bored@goat.si>
2018-01-06 18:33:17 +1100

    update boost rebuild things that use boost...

--- packages/ncmpcpp/build.sh
+++ packages/ncmpcpp/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_DEPENDS="fftw, boost, readline, libcurl, libmpdclient, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_KEEP_SHARE_DOC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-visualizer --enable-outputs --enable-clock"
-
+TERMUX_PKG_REVISION=1
 termux_step_pre_configure() {
 	./autogen.sh
 	CXXFLAGS+=" -DNCURSES_WIDECHAR -U_XOPEN_SOURCE"
