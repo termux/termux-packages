b71c76db4135da0c26822bb5d1b5bf3fbdec1c04
Fredrik Fornwall <fredrik@fornwall.net>
2018-01-06 20:14:46 +0100

    boost: Do not change the build id

--- packages/boost/libboost-python.subpackage.sh
+++ packages/boost/libboost-python.subpackage.sh
@@ -1,3 +1,3 @@
-TERMUX_SUBPKG_INCLUDE="lib/libboost_python-3.so.1.65.1 libboost_python-3.so"
+TERMUX_SUBPKG_INCLUDE="lib/libboost_python*"
 TERMUX_SUBPKG_DESCRIPTION="Boost.Python Library"
 TERMUX_SUBPKG_DEPENDS="python, boost"
