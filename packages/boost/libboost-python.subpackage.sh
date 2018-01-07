882c2b5c28f03bf57c235e29094b138a437fbd5b
its-pointless <bored@goat.si>
2018-01-07 11:50:17 +1100

    fix up

--- packages/boost/libboost-python.subpackage.sh
+++ packages/boost/libboost-python.subpackage.sh
@@ -1,3 +1,3 @@
-TERMUX_SUBPKG_INCLUDE="lib/libboost_python-3.so.1.65.1 libboost_python-3.so"
+TERMUX_SUBPKG_INCLUDE="lib/libboost_python3.so.$TERMUX_PKG_VERSION lib/libboost_python3.so"
 TERMUX_SUBPKG_DESCRIPTION="Boost.Python Library"
 TERMUX_SUBPKG_DEPENDS="python, boost"
