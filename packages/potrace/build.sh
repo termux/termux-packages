2f954f7c846d27aef7375a69e3358476d0e4e27b
its-pointless <bored@goat.si>
2018-01-07 17:10:03 +1100

    potrace request from #1987

---
+++ packages/potrace/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://http://potrace.sourceforge.net
+TERMUX_PKG_DESCRIPTION="tool for transforming a bitmap into a smooth, scalable image"
+TERMUX_PKG_VERSION=1.15
+TERMUX_PKG_SRCURL=http://potrace.sourceforge.net/download/1.15/potrace-1.15.tar.gz
+TERMUX_PKG_SHA256=a9b33904ace328340c850a01458199e0064e03ccaaa731bc869a842b1b8d529d
