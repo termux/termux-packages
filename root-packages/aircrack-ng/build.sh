738752f01aa1dbdb5ad054d430c847276d64a8f9
Henrik Grimler <Henrik@Grimler.se>
2018-01-11 19:57:14 +0100

    aircrack-ng: update SRCURL and SHA256

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org
 TERMUX_PKG_DESCRIPTION="an 802.11 WEP and WPA-PSK keys cracking program"
 TERMUX_PKG_VERSION=1.2-rc4
-TERMUX_PKG_SRCURL=https://Auxilus.github.io/aircrack-ng-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9992129ce2d3b5297e422e529a8230a6a449df9fec2edd04636d206c6851e7d5
+TERMUX_PKG_SRCURL=https://download.aircrack-ng.org/aircrack-ng-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d93ac16aade5b4d37ab8cdf6ce4b855835096ccf83deb65ffdeff6d666eaff36
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
 TERMUX_PKG_DEPENDS="libnl, libnl-dev, openssl, openssl-dev, libpcap, libpcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
