2072fbcd836850f88a41f6b88326ba99c1cc9c46
its-pointless <bored@goat.si>
2018-01-12 11:10:54 +1100

    alpine improvements

--- packages/alpine/build.sh
+++ packages/alpine/build.sh
@@ -2,7 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://repo.or.cz/alpine.git
 TERMUX_PKG_DESCRIPTION="Fast, easy to use email client"
 TERMUX_PKG_VERSION=2.21
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/alpine-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_DEPENDS="libcrypt, ncurses, openssl"
+TERMUX_PKG_DEPENDS="libcrypt, ncurses, openssl-tool"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-debug
 --with-c-client-target=lnx
@@ -40,5 +41,15 @@ termux_step_post_configure() {
 	$CC_FOR_BUILD help_c_gen.c -o help_c_gen
 	$CC_FOR_BUILD help_h_gen.c -o help_h_gen
 	touch -d "next hour" help_c_gen help_h_gen
+}
+termux_step_create_debscripts () {
+
+        echo "#!$TERMUX_PREFIX/bin/sh" >> postinst
+	echo "if [ ! -e $TERMUX_ANDROID_HOME/.alpine-smime ] && [ ! -e $TERMUX_ANDROID_HOME/.pine-passfile ]; then" >> postinst
+        echo "echo 'warning making a passwordless masterpasword'" >> postinst
+	echo "mkdir -p \$HOME/.alpine-smime/public \$HOME/.alpine-smime/.pwd \$HOME/.alpine-smime/private \$HOME/.alpine-smime/ca" >> postinst
+	echo "openssl req -x509 -newkey rsa:2048 -keyout \$HOME/.alpine-smime/.pwd/MasterPassword.key -out \$HOME/.alpine-smime/.pwd/MasterPassword.crt -days 10000 -nodes -subj '/C=US/ST=dont/L=use/O=this Name/OU=for/CN=anything.com.termux'" >> postinst
+	echo "touch \$HOME/.pine-passfile" >> postinst
+	echo "fi" >> postinst
 }
 
