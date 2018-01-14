b1fe43e4e656c0fc2b23477815248ff1043f56f7
Fredrik Fornwall <fredrik@fornwall.net>
2018-01-14 22:24:17 +0100

    hydra: Update to latest git master (see #2033)

--- packages/hydra/build.sh
+++ packages/hydra/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/vanhauser-thc/thc-hydra
 TERMUX_PKG_DESCRIPTION="Network logon cracker supporting different services"
-TERMUX_PKG_VERSION=8.6
-TERMUX_PKG_SHA256=05a87eb018507b24afca970081f067e64441460319fb75ca1e64c4a1f322b80b
-TERMUX_PKG_SRCURL=https://github.com/vanhauser-thc/thc-hydra/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=8.6.20180104
+TERMUX_PKG_SHA256=794e5e10e32c9ef3eaf20bbefc0348b997bbb3824dda6de3b5aab91f49beec31
+TERMUX_PKG_SRCURL=https://github.com/vanhauser-thc/thc-hydra/archive/9597bafb178a57f839502abdd3d62b0b43028993.zip
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="openssl, pcre, libssh"
 
