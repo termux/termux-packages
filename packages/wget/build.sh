fd1350e42710a44ff6de1c0fd9914f4b0543710b
Fredrik Fornwall <fredrik@fornwall.net>
2018-01-22 09:56:26 +0100

    wget: Update from 1.19.3 to 1.19.4 (fixes #2052)

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
 TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
-TERMUX_PKG_VERSION=1.19.3
-TERMUX_PKG_SHA256=a391528ba9f6a1d1bdfeb0e53894156c32f22f4d44966f6df1eaa50f42a78562
+TERMUX_PKG_VERSION=1.19.4
+TERMUX_PKG_SHA256=2fc0ffb965a8dc8f1e4a89cbe834c0ae7b9c22f559ebafc84c7874ad1866559a
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_DEPENDS="pcre, openssl, libuuid, libandroid-support, libunistring"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl --disable-iri"
