fd223b6f74d42948c338c951254ab3dd3905084b
sogaiu <33044872+sogaiu@users.noreply.github.com>
2018-01-23 02:32:24 +0000

    Add package: pdfgrep
    Lightly tested.

---
+++ packages/pdfgrep/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://pdfgrep.org/
+TERMUX_PKG_DESCRIPTION="command line utility to search text in PDF files"
+TERMUX_PKG_VERSION=2.0.1
+TERMUX_PKG_SRCURL=https://pdfgrep.org/download/pdfgrep-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0370d744b3072d47383dbed2cb9c8b0b64b83c084da5a8961f8d4bc7669e941e
+TERMUX_PKG_DEPENDS="libgcrypt, pcre, poppler"
