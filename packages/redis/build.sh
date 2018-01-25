7df6eb38a1e419f5e29d0213e3f13f149077cd27
Fredrik Fornwall <fredrik@fornwall.net>
2018-01-25 20:47:45 +0100

    redis: Update from 4.0.6 to 4.0.7

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://redis.io/
 TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache and message broker"
-TERMUX_PKG_VERSION=4.0.6
-TERMUX_PKG_SHA256=769b5d69ec237c3e0481a262ff5306ce30db9b5c8ceb14d1023491ca7be5f6fa
+TERMUX_PKG_VERSION=4.0.7
+TERMUX_PKG_SHA256=1bba546d44fb40e1fd8be1a15e1a9cc6484bceeea0bbd52919eebc656661ecd1
 TERMUX_PKG_SRCURL=http://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_CONFFILES="etc/redis.conf"
