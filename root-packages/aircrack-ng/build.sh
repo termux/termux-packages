94d220c1818ccce509c4b0df6899910cf22a81da
Henrik Grimler <Henrik@Grimler.se>
2018-01-25 22:59:23 +0100

    aircrack-ng: blacklist aarch64 for now (see #11)

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -6,3 +6,5 @@ TERMUX_PKG_SHA256=d93ac16aade5b4d37ab8cdf6ce4b855835096ccf83deb65ffdeff6d666eaff
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
 TERMUX_PKG_DEPENDS="libnl, libnl-dev, openssl, openssl-dev, libpcap, libpcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_BLACKLISTED_ARCHES="aarch64"
+# See https://github.com/termux/termux-root-packages/issues/11
