a56a59b21cc62bcd865e62b6e62f81f44a8cb2a3
Fredrik Fornwall <fredrik@fornwall.net>
2018-01-25 22:18:27 +0100

    libjansson: Skip default maintainer

--- packages/libjansson/build.sh
+++ packages/libjansson/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_VERSION=2.10
 TERMUX_PKG_SRCURL=https://github.com/akheron/jansson/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b0a899f90ade82e42da0ecabc8af1fa296d69691e7c0786c4994fb79d4833ebb
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_MAINTAINER="Fredrik Fornwall @fornwall"
 
 termux_step_pre_configure () {
 	autoreconf -fi
