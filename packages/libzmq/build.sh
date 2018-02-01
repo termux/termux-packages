241323a97ea93080eb621cfc0e6fee83067b7a60
Fredrik Fornwall <fredrik@fornwall.net>
2018-02-01 11:27:20 +0100

    libzmq: Update from 4.2.2 to 4.2.3

--- packages/libzmq/build.sh
+++ packages/libzmq/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://zeromq.org/
 TERMUX_PKG_DESCRIPTION="Fast messaging system built on sockets. C and C++ bindings. aka 0MQ, ZMQ."
-TERMUX_PKG_VERSION=4.2.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.2.3
+TERMUX_PKG_SHA256=8f1e2b2aade4dbfde98d82366d61baef2f62e812530160d2e6d0a5bb24e40bc0
 TERMUX_PKG_SRCURL=https://github.com/zeromq/libzmq/releases/download/v${TERMUX_PKG_VERSION}/zeromq-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5b23f4ca9ef545d5bd3af55d305765e3ee06b986263b31967435d285a3e6df6b
 TERMUX_PKG_DEPENDS="libsodium"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libsodium"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libsodium --disable-libunwind"
 
 termux_step_post_extract_package() {
 	./autogen.sh
