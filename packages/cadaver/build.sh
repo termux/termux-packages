89ab94537d711fd432697c43198899b263a2e0bb
Fredrik Fornwall <fredrik@fornwall.net>
2018-02-04 23:54:28 +0100

    cadaver: Drop dependency on libgcrypt

--- packages/cadaver/build.sh
+++ packages/cadaver/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_VERSION=0.23.3
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://fossies.org/linux/www/cadaver-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=123772d7d33f06a76742dba874b1c444423b52ad3a7bbb87559616ec78b9ae5e
-TERMUX_PKG_DEPENDS="libgcrypt, openssl, libxml2, ncurses, readline"
+TERMUX_PKG_DEPENDS="openssl, libxml2, ncurses, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-libxml2
 --without-gssapi
