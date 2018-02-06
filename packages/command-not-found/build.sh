1d3ee4b67514c1db0b2051aca75f8f93f6b3cdfb
Fredrik Fornwall <fredrik@fornwall.net>
2018-02-06 22:33:19 +0100

    command-not-found: Update with latest commands

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://termux.com
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
-TERMUX_PKG_VERSION=1.29
+TERMUX_PKG_VERSION=1.30
 
 termux_step_make_install () {
 	TERMUX_LIBEXEC_DIR=$TERMUX_PREFIX/libexec/termux
