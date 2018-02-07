c05c2647309aa798bb3859441d02e376c8308bad
Auxilus <raghavsphadke@gmail.com>
2018-02-07 07:22:27 +0530

    Update patch to 2.7.6

--- packages/patch/build.sh
+++ packages/patch/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://savannah.gnu.org/projects/patch/
 TERMUX_PKG_DESCRIPTION="GNU patch which applies diff files to create patched files"
-TERMUX_PKG_VERSION=2.7.5
+TERMUX_PKG_VERSION=2.7.6
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/patch/patch-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=fd95153655d6b95567e623843a0e77b81612d502ecf78a489a4aed7867caa299
+TERMUX_PKG_SHA256=ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd
