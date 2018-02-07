336a90daf29492f7a2466769f3203509b94099ac
ayatakesi <ayanokoji.takesi@gmail.com>
2018-02-07 14:08:44 +0900

    add mu-1.0

--- packages/gmime2/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/jstedfast/gmime
-TERMUX_PKG_DESCRIPTION="A C/C++ MIME creation and parser library with support for S/MIME, PGP, and Unix mbox spools"
-TERMUX_PKG_VERSION=2.6.23
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/gmime/2.6/gmime-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7149686a71ca42a1390869b6074815106b061aaeaaa8f2ef8c12c191d9a79f6a
-TERMUX_PKG_DEPENDS="glib"
-TERMUX_PKG_BUILD_DEPENDS="glib-dev, pkg-config"
---
+++ packages/mu/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.djcbsoftware.nl/code/mu/
+TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_SRCURL=https://github.com/djcb/mu/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=533149abab967e2809f72e9fe62c6deb71d45b6ad2a5846247733a1dd2de69a0
+TERMUX_PKG_DEPENDS="glib, libxapian, libgmime2"
+TERMUX_PKG_BUILD_DEPENDS="glib-dev, libxapian-dev, libgmime2-dev"
