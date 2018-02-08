4ce66ced5228bd0860dae140bf27bf10fc407ca9
ayatakesi <ayanokoji.takesi@gmail.com>
2018-02-08 17:54:06 +0900

    add autogen.sh setting

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -5,3 +5,8 @@ TERMUX_PKG_SRCURL=https://github.com/djcb/mu/archive/v${TERMUX_PKG_VERSION}.tar.
 TERMUX_PKG_SHA256=533149abab967e2809f72e9fe62c6deb71d45b6ad2a5846247733a1dd2de69a0
 TERMUX_PKG_DEPENDS="glib, libxapian, libgmime2"
 TERMUX_PKG_BUILD_DEPENDS="glib-dev, libxapian-dev, libgmime2-dev"
+
+termux_step_pre_configure() {
+	NOCONFIGURE=1	./autogen.sh
+}
+
