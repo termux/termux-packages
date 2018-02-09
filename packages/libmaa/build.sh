ce00f96c94ad5bf40a4572dff9f12d02eab9fc58
ayatakesi <ayanokoji.takesi@gmail.com>
2018-02-09 20:00:48 +0900

    add autoreconf pre configure

--- packages/libmaa/build.sh
+++ packages/libmaa/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Provides many low-level data structures which are helpfu
 TERMUX_PKG_VERSION=1.3.2
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dict/libmaa/libmaa-${TERMUX_PKG_VERSION}/libmaa-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=59a5a01e3a9036bd32160ec535d25b72e579824e391fea7079e9c40b0623b1c5
+
 termux_step_pre_configure() {
+    autoreconf -ivf
     LDFLAGS+=" -llog"
 }
-
