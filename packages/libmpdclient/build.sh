08fe7a13e919b1acde929a5a0c411fdddb5dfee4
Fredrik Fornwall <fredrik@fornwall.net>
2018-02-12 20:09:51 +0100

    libmpdclient: Update from 2.13 to 2.14

--- packages/libmpdclient/build.sh
+++ packages/libmpdclient/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.musicpd.org/libs/libmpdclient/
 TERMUX_PKG_DESCRIPTION="Asynchronous API library for interfacing MPD in the C, C++ & Objective C languages"
 TERMUX_PKG_MAINTAINER="Matthew Klein @mklein994"
 _MAIN_VERSION=2
-_PATCH_VERSION=13
+_PATCH_VERSION=14
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
-TERMUX_PKG_SHA256=5115bd52bc20a707c1ecc7587e6389c17305348e2132a66cf767c62fc55ed45d
+TERMUX_PKG_SHA256=0a84e2791bfe3077cf22ee1784c805d5bb550803dffe56a39aa3690a38061372
 TERMUX_PKG_SRCURL=https://www.musicpd.org/download/libmpdclient/${_MAIN_VERSION}/libmpdclient-${_MAIN_VERSION}.${_PATCH_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -D default_socket==${TERMUX_PREFIX}/var/run/mpd/socket"
