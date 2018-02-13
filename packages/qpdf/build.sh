d4c849534ce2870a6e4251d0761ccdf43710cb04
Tom Yan <tom.ty89@gmail.com>
2018-02-13 17:58:24 +0800

    qpdf: Update from 7.0.0 to 7.1.1

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="A Content-Preserving PDF Transformation System"
-TERMUX_PKG_VERSION=7.0.0
+TERMUX_PKG_VERSION=7.1.1
 TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/archive/release-qpdf-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a599c1118c42b2300c27b95c86d005dedf5901f3b60c324c968296126b15594f
+TERMUX_PKG_SHA256=21822dc365eaee55bc449d84eb760b9845c4871783ab0e4c4f3b244052718a1a
 TERMUX_PKG_DEPENDS="libjpeg-turbo"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-random=/dev/urandom"
