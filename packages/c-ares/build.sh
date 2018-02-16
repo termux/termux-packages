ea09f74ecf3c204ea08ce913e6aa175aacdf54b1
Fredrik Fornwall <fredrik@fornwall.net>
2018-02-17 00:43:47 +0100

    c-ares: Update from 1.13.0 to 1.14.0

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -1,6 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://c-ares.haxx.se
-TERMUX_PKG_DESCRIPTION="C library for asynchronous DNS requests (including name resolves)"
-TERMUX_PKG_VERSION=1.13.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=http://c-ares.haxx.se/download/c-ares-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7
+TERMUX_PKG_DESCRIPTION="Library for asynchronous DNS requests (including name resolves)"
+TERMUX_PKG_VERSION=1.14.0
+TERMUX_PKG_SHA256=45d3c1fd29263ceec2afc8ff9cd06d5f8f889636eb4e80ce3cc7f0eaf7aadc6e
+TERMUX_PKG_SRCURL=https://c-ares.haxx.se/download/c-ares-${TERMUX_PKG_VERSION}.tar.gz
+# Build with cmake to install cmake/c-ares/*.cmake files:
+TERMUX_PKG_FORCE_CMAKE=yes
+TERMUX_PKG_RM_AFTER_INSTALL="bin/"
