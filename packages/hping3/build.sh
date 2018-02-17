89306e38373311cbb2f192edd09b767e2458ed18
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-17 16:10:09 +0530

    Add build.sh

---
+++ packages/hping3/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://www.hping.org
+TERMUX_PKG_DESCRIPTION="hping is a command-line oriented TCP/IP packet assembler/analyzer."
+TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_SHA256=4b6157ec77bc2b3e47702227575ea83dc71e1017bb174e9730ef1c4f6b34c25c
+TERMUX_PKG_SRCURL=https://hax4us.github.io/hping/hping3-${TERMUX_PKG_VERSION}.tar.gz
