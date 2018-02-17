2301fb13a7272cb628d5125ecef0a6742af3a2b1
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-17 17:49:30 +0530

    Build.sh

---
+++ packages/hping3/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://www.hping.org
+TERMUX_PKG_DESCRIPTION="hping is a command-line oriented TCP/IP packet assembler/analyzer."
+TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_DEPENDS="libandroid-shmem, libandroid-shmem-dev, libpcap, libpcap-dev, tcl, tcl-dev"
+TERMUX_PKG_SHA256=7b5a87240ba56109b7aac24d05aff671c06091fd03cc2d26521828c59bd538df
+TERMUX_PKG_SRCURL=https://hax4us.github.io/hping/hping3-${TERMUX_PKG_VERSION}.tar.gz
