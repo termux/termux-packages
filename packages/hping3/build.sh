7f39c4af56eb1fe43963b61607d2cc44597bde1b
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-18 13:30:42 +0530

    Update build.sh

--- packages/hping3/build.sh
+++ packages/hping3/build.sh
@@ -3,6 +3,6 @@ TERMUX_PKG_DESCRIPTION="hping is a command-line oriented TCP/IP packet assembler
 TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libandroid-shmem, libpcap, tcl"
-TERMUX_PKG_SHA256=636c04e4546dddc07e8c0d38c0151baa9b0de701290c87976792cac2ca27d2b0
+TERMUX_PKG_SHA256=ea9cc31dc3efb0c89180d5dedaa84ec1b7f738e088ac32dba5d1d077ea62c83b
 TERMUX_PKG_SRCURL=https://hax4us.github.io/hping/hping3-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-tcl"
