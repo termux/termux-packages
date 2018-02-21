fc3268043bb98ce57aed2166d6aad796a67d5148
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-21 19:30:36 +0530

    Update build.sh

--- packages/hping3/build.sh
+++ packages/hping3/build.sh
@@ -3,9 +3,6 @@ TERMUX_PKG_DESCRIPTION="hping is a command-line oriented TCP/IP packet assembler
 TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_DEPENDS="libandroid-shmem, libpcap"
-TERMUX_PKG_SHA256=f1684e568b6d00ba096e1c77b18be4500ca2b8d2715bb2059aff22b8b8717eff
+TERMUX_PKG_SHA256=2a9ac0247c4e3ce56f25cdd44d2e589b90def24c399a91db839e29a350a1d6d7
 TERMUX_PKG_SRCURL=https://hax4us.github.io/hping/hping3-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-tcl"
-termux_step_pre_configure {
-  sudo apt-get install -y libpcap0.8-dev
-  }
