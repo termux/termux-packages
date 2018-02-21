956d8b8be20b2e63c599c0fdcc8afc54b7063eb5
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-21 16:36:07 +0530

    Update build.sh

--- packages/hping3/build.sh
+++ packages/hping3/build.sh
@@ -7,5 +7,5 @@ TERMUX_PKG_SHA256=f1684e568b6d00ba096e1c77b18be4500ca2b8d2715bb2059aff22b8b8717e
 TERMUX_PKG_SRCURL=https://hax4us.github.io/hping/hping3-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-tcl"
 termux_step_pre_configure {
-  sudo apt-get install -y libpcap-dev
+  sudo apt-get install -y libpcap0.8-dev
   }
