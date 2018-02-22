ce80f8df79a9d74eadfb21318036f243c22729bf
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-22 18:56:22 +0530

    Update build.sh

--- packages/autoconf-archive/build.sh
+++ packages/autoconf-archive/build.sh
@@ -1,9 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/autoconf-archive/
-TERMUX_PKG_DESCRIPTION="Autoconf Macro Archive
- Autoconf Macro Archive aims to provide a central repository of
-  useful and tested Autoconf macros for software developers around the
-   world. This package contains the complete content of the Autoconf
-    Macro Archive as found at http://www.gnu.org/software/autoconf-archive/."
+TERMUX_PKG_DESCRIPTION="Autoconf Macro Archive"
 TERMUX_PKG_VERSION=09.28
 TERMUX_PKG_SRCURL=http://ftp.wayne.edu/gnu/autoconf-archive/autoconf-archive-2017.${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5c9fb5845b38b28982a3ef12836f76b35f46799ef4a2e46b48e2bd3c6182fa01
