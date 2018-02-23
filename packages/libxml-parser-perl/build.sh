eb46154baba68e853c1b9f40e4b3ceb990494053
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-23 22:32:50 +0530

    Update build.sh

--- packages/libxml-parser-perl/build.sh
+++ packages/libxml-parser-perl/build.sh
@@ -9,6 +9,7 @@ termux_step_make() {
  export LDDFLAGS="$CFLAGS -shared"
  export PERL_DIR=$TERMUX_PREFIX/lib/perl5/5.26.1
  perl Makefile.PL PERL_LIB=$PERL_DIR MAKEMAKER=$PERL_DIR/ExtUtils/MakeMaker.pm PERL_INC=$PERL_DIR/${TERMUX_ARCH}-android/CORE PERL_INCDEP=$PERL_DIR/${TERMUX_ARCH}-android/CORE  PERL_ARCHLIB=$PERL_DIR/${TERMUX_ARCH}-android PERL_ARCHLIBDEP=$PERL_DIR/${TERMUX_ARCH}-android CONFIGDEP=$PERL_DIR/${TERMUX_ARCH}-android/Config.pm EXPATLIBPATH=$TERMUX_PREFIX/lib EXPATINCPATH=$TERMUX_PREFIX/include PREFIX=$TERMUX_PREFIX AR=$AR CC=$CC RANLIB=$RANLIB LDDFLAGS=$LDDFLAGS LDFLAGS=$LDFLAGS LD=$LD CCFLAGS="$CPPFLAGS"
- sed -i "s/x86_64-linux-gnu-gcc/${TERMUX_ARCH}-linux-android-clang/g" Makefile 
+ sed -i "s/x86_64-linux-gnu-gcc/${TERMUX_ARCH}-linux-android-clang/g" Makefile
+  sed -i "s/x86_64-linux-gnu-gcc/${TERMUX_ARCH}-linux-android-clang/g" Expat/Makefile
   make -j 4
  }
