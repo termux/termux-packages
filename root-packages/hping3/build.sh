b06389927c5018d99426f55799c08cdbd3c38f98
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-23 10:43:31 +0530

    Add original source url

--- root-packages/hping3/build.sh
+++ root-packages/hping3/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="hping is a command-line oriented TCP/IP packet assembler
 TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_DEPENDS="libandroid-shmem, libpcap"
-TERMUX_PKG_SHA256=5992a89e8fdfcec255aeb07467423e9cdda0d4d61b3086776c74d5cdb7ff16ec
-TERMUX_PKG_SRCURL=https://hax4us.github.io/hping/hping3-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f5a671a62a11dc8114fa98eade19542ed1c3aa3c832b0e572ca0eb1a5a4faee8
+TERMUX_PKG_SRCURL=http://www.hping.org/hping3-20051105.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-tcl"
 termux_step_post_make_install() {
-mkdir -p "$TERMUX_PREFIX/share/man/man8"
+ mkdir -p "$TERMUX_PREFIX/share/man/man8"
 	cp "$TERMUX_PKG_SRCDIR/docs/hping3.8" "$TERMUX_PREFIX/share/man/man8/"
-  }
+}
