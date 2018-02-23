4cfcf98bd279833c6231e9f658460bab9bdecb4f
Auxilus <raghavsphadke@gmail.com>
2018-02-23 08:28:01 +0530

    Update parallel to 20180222

--- packages/parallel/build.sh
+++ packages/parallel/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/parallel/
 TERMUX_PKG_DESCRIPTION="GNU Parallel is a shell tool for executing jobs in parallel using one or more machines"
-TERMUX_PKG_VERSION=20180122
-TERMUX_PKG_SHA256=e906c271dfa0e973a9ea151fe7caaba742e0f400e86a69687ea6b04ad6bd6bf2
+TERMUX_PKG_VERSION=20180222
+TERMUX_PKG_SHA256=e81997dbabef18708de30635bd5d05af63395d869452a91c6a047c9c590e9daf
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/parallel/parallel-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
