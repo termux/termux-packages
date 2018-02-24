409cb18bc689999c52da280319ec82d48e4d958a
its-pointless <bored@goat.si>
2018-02-24 16:32:07 +1100

    forgot depends... also needs to install texi2html

--- packages/indent/build.sh
+++ packages/indent/build.sh
@@ -3,5 +3,7 @@ TERMUX_PKG_DESCRIPTION="C language source code formatting program"
 TERMUX_PKG_VERSION=2.2.11
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/i/indent/indent_$TERMUX_PKG_VERSION.orig.tar.gz
 TERMUX_PKG_SHA256=aaff60ce4d255efb985f0eb78cca4d1ad766c6e051666073050656b6753a0893
+TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_CLANG=no
