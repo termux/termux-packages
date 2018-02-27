02041f7e5b08b6b309e024c8e5ef45e60817cd4c
Fredrik Fornwall <fredrik@fornwall.net>
2018-02-27 23:49:09 +0100

    automake: Update from 1.15.1 to 1.16

--- packages/automake/build.sh
+++ packages/automake/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/automake/
 TERMUX_PKG_DESCRIPTION="Tool for automatically generating Makefile.in files"
-TERMUX_PKG_VERSION=1.15.1
+TERMUX_PKG_VERSION=1.16
+TERMUX_PKG_SHA256=f98f2d97b11851cbe7c2d4b4eaef498ae9d17a3c2ef1401609b7b4ca66655b8a
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/automake/automake-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=af6ba39142220687c500f79b4aa2f181d9b24e4f8d8ec497cea4ba26c64bedaf
 TERMUX_PKG_DEPENDS="autoconf"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
