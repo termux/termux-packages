65cb7904c5b538fe218ec375b77d3b96472437d8
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-02-27 17:59:12 +0530

    Delete build.sh

--- packages/exiv2/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=http://www.exiv2.org
-TERMUX_PKG_DESCRIPTION="Exiv2 is a Cross-platform C++ library and a command line utility to manage image metadata."
-TERMUX_PKG_VERSION=0.26
-TERMUX_PKG_DEPENDS="libexpat, libandroid-support"
-TERMUX_PKG_SHA256=c75e3c4a0811bf700d92c82319373b7a825a2331c12b8b37d41eb58e4f18eafb
-TERMUX_PKG_SRCURL=http://www.exiv2.org/builds/exiv2-${TERMUX_PKG_VERSION}-trunk.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-nls"
