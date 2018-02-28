bdbb3f71d04f67905f3523f65aa30f5d568d73dc
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-01 00:50:13 +0100

    imagemagick: Update from 7.0.7.22 to 7.0.7.24

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
-TERMUX_PKG_VERSION=7.0.7.22
-TERMUX_PKG_SHA256=ce060a56e6d8a7c51af603dd14d9ded3480a71ce675d7bc2a5532ef1fb737b93
+TERMUX_PKG_VERSION=7.0.7.24
+TERMUX_PKG_SHA256=25711a7efd487c1602757f9cbf3f35dbad7b357310c69cda031b41e3d3b34220
 local _download_version=`echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/'`
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/${_download_version}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
