8f4952ffae0420be817ad1149f55a27db7597bd3
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-01 11:01:34 +0100

    libunistring: Update from 0.9.8 to 0.9.9

--- packages/libunistring/build.sh
+++ packages/libunistring/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libunistring/
 TERMUX_PKG_DESCRIPTION="Library providing functions for manipulating Unicode strings"
-TERMUX_PKG_VERSION=0.9.8
+TERMUX_PKG_VERSION=0.9.9
+TERMUX_PKG_SHA256=f5e90c08f9e5427ca3a2c0c53f19aa38b25c500913510ad25afef86448bea84a
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libunistring/libunistring-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b792f2bd05d0fa7b339e39e353da7232b2e514e0db2cf5ed95beeff3feb53cf5
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_uselocale=no"
