373317ab76eb0ea61f09eba2823bd0aca0823920
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-08 17:35:44 +0100

    harfbuzz: Update from 1.7.5 to 1.7.6

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.7.5
-TERMUX_PKG_SHA256=84574e1b1f65ca694cb8fb6905309665c0368af18a312357f8ff886ee2f29563
+TERMUX_PKG_VERSION=1.7.6
+TERMUX_PKG_SHA256=da7bed39134826cd51e57c29f1dfbe342ccedb4f4773b1c951ff05ff383e2e9b
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
 TERMUX_PKG_BUILD_DEPENDS="libicu"
