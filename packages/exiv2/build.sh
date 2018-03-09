6c20f9fa222111037804e8a7fa314ed47b3ce0b2
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-03-09 12:58:14 +0530

    Update build.sh

--- packages/exiv2/build.sh
+++ packages/exiv2/build.sh
@@ -6,6 +6,6 @@ TERMUX_PKG_SHA256=c75e3c4a0811bf700d92c82319373b7a825a2331c12b8b37d41eb58e4f18ea
 TERMUX_PKG_SRCURL=http://www.exiv2.org/builds/exiv2-${TERMUX_PKG_VERSION}-trunk.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-nls"
 TERMUX_PKG_BUILD_IN_SRC=yes
-termux_step_pre_configure {
- patch -p1 < $TERMUX_PKG_BUILDER_DIR/configure
- }
+
+
+
