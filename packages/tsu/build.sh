ee1d546b859bb3149ffa7ccf7cec988e436497a4
Cswl Coldwind <cswl1337@gmail.com>
2018-03-12 09:31:44 +0545

    Update tsu to v2.0.0

--- packages/tsu/build.sh
+++ packages/tsu/build.sh
@@ -1,12 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/cswl/tsu
 TERMUX_PKG_DESCRIPTION="A su wrapper for Termux"
-TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_VERSION=2.0
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install () {
-	termux_download https://raw.githubusercontent.com/cswl/tsu/ed8940c02ee789ad93f81a443266c45cf2ce3b74/tsu \
+	termux_download https://raw.githubusercontent.com/cswl/tsu/c0a2603b57511a1492dfe6ec4e6ef452e01a58e6/tsu \
 	                $TERMUX_PREFIX/bin/tsu \
-	                0dd75d7c9a7dc5b7b007a3a8ac08a05040c14199ff336a8b47589345e31e73d1
+	                ee33a58fd19bfc6c99aab8af4c836ede4b749b4f1ca5957cbb66503828bec7c7
+	termux_download https://raw.githubusercontent.com/cswl/tsu/c0a2603b57511a1492dfe6ec4e6ef452e01a58e6/tsudo \
+					$TERMUX_PREFIX/bin/tsudo \
+					275d025ce7e241f989fcbb1d12f49ed9ab927b3b0e8a1a41de2a89fc1bfee6b0
 	touch $TERMUX_PREFIX/bin/tsu
 	chmod +x $TERMUX_PREFIX/bin/tsu
+	touch $TERMUX_PREFIX/bin/tsudo
+	chmod +x $TERMUX_PREFIX/bin/tsudo
 }
