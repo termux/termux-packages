d7455b1376d9116a35732db489fc2137c3bc2d70
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-12 11:39:33 +0100

    unrar: Update from 5.5.8 to 5.6.1

--- packages/unrar/build.sh
+++ packages/unrar/build.sh
@@ -1,7 +1,7 @@
-TERMUX_PKG_HOMEPAGE=http://www.rarlab.com/
+TERMUX_PKG_HOMEPAGE=https://www.rarlab.com/
 TERMUX_PKG_DESCRIPTION="Tool for extracting files from .rar archives"
-TERMUX_PKG_VERSION=5.5.8
-TERMUX_PKG_SRCURL=http://www.rarlab.com/rar/unrarsrc-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9b66e4353a9944bc140eb2a919ff99482dd548f858f5e296d809e8f7cdb2fcf4
+TERMUX_PKG_VERSION=5.6.1
+TERMUX_PKG_SHA256=67c339dffa95f6c1bedcca40045e99de5852919dbfaa06e4a9c8f18cd5064e70
+TERMUX_PKG_SRCURL=https://www.rarlab.com/rar/unrarsrc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support,readline"
 TERMUX_PKG_BUILD_IN_SRC=yes
