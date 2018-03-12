53ad9afa767c2c6c1e2a3311683161d7630542d3
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-12 13:42:03 +0100

    valac: Update from 0.38.7 to 0.40.0

--- packages/valac/build.sh
+++ packages/valac/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/Vala
 TERMUX_PKG_DESCRIPTION="C# like language for the GObject system"
-TERMUX_PKG_VERSION=0.38.7
-TERMUX_PKG_SHA256=09d10f5f7d0e4b3c905d9f636cb5ac55f317494e42c75e6572be4138b0603405
+TERMUX_PKG_VERSION=0.40.0
+TERMUX_PKG_SHA256=15888fcb5831917cd67367996407b28fdfc6cd719a30e6a8de38a952a8a48e71
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/vala/${TERMUX_PKG_VERSION:0:4}/vala-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_DEPENDS="clang, glib-dev, pkg-config"
