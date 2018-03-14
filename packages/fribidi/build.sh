f309f84c58a82fa9cbaf963ab99570967dddeef3
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-14 17:11:54 +0100

    fribidi: Add package

---
+++ packages/fribidi/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/fribidi/fribidi/
+TERMUX_PKG_DESCRIPTION="Implementation of the Unicode Bidirectional Algorithm"
+TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_SHA256=c1b182d70590b6cdb5545bab8149de33b966800f27f2d9365c68917ed5a174e4
+TERMUX_PKG_SRCURL=https://github.com/fribidi/fribidi/releases/download/v$TERMUX_PKG_VERSION/fribidi-$TERMUX_PKG_VERSION.tar.bz2
