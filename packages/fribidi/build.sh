c59873d247b2b04a3dfaff2dbe738e355a15c4fc
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-14 17:16:11 +0100

    fribidi: Depend on glib

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -3,3 +3,4 @@ TERMUX_PKG_DESCRIPTION="Implementation of the Unicode Bidirectional Algorithm"
 TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SHA256=c1b182d70590b6cdb5545bab8149de33b966800f27f2d9365c68917ed5a174e4
 TERMUX_PKG_SRCURL=https://github.com/fribidi/fribidi/releases/download/v$TERMUX_PKG_VERSION/fribidi-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_DEPENDS="glib"
