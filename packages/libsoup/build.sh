7aa3e20577194b5b168fe49310d443e4620c0bfa
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-14 17:01:48 +0100

    libsoup: Update from 2.61.90 to 2.62.0

--- packages/libsoup/build.sh
+++ packages/libsoup/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/libsoup
 TERMUX_PKG_DESCRIPTION="HTTP client and server library"
-TERMUX_PKG_VERSION=2.61.90
-TERMUX_PKG_SHA256=49deec092f1b9973feec69fda8f36b2514d0b02c5ff1f7481f4542c3425ebfbc
+TERMUX_PKG_VERSION=2.62.0
+TERMUX_PKG_SHA256=ab7c7ae8d19d0a27ab3b6ae21599cec8c7f7b773b3f2b1090c5daf178373aaac
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libsoup/${TERMUX_PKG_VERSION:0:4}/libsoup-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-vala --without-gssapi"
 TERMUX_PKG_DEPENDS="glib, libsqlite, libxml2"
