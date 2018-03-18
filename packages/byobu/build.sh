767c209247761f0352e02f03803397e731849876
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-18 21:01:41 +0100

    byobu: Update from 5.124 to 5.125

--- packages/byobu/build.sh
+++ packages/byobu/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://byobu.co/
 TERMUX_PKG_DESCRIPTION="Byobu is a GPLv3 open source text-based window manager and terminal multiplexer"
-TERMUX_PKG_VERSION=5.124
+TERMUX_PKG_VERSION=5.125
+TERMUX_PKG_SHA256=5022c82705a5d57f1d4e8dcb1819fd04628af2d4b4618b7d44fa27ebfcdda9db
 TERMUX_PKG_SRCURL=https://launchpad.net/byobu/trunk/${TERMUX_PKG_VERSION}/+download/byobu_${TERMUX_PKG_VERSION}.orig.tar.gz
-TERMUX_PKG_SHA256=4eca1287b95093ac4697e6ebf7312308d54af90630db151669c5f328e0bef122
 TERMUX_PKG_DEPENDS="gawk, tmux"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
