a465e17ac955a7a8fd237e9a24820bc4e3cd4c9b
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-18 20:59:53 +0100

    cmake: Update from 3.10.2 to 3.10.3

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
-TERMUX_PKG_VERSION=3.10.2
-TERMUX_PKG_SHA256=80d0faad4ab56de07aa21a7fc692c88c4ce6156d42b0579c6962004a70a3218b
+TERMUX_PKG_VERSION=3.10.3
+TERMUX_PKG_SHA256=0c3a1dcf0be03e40cf4f341dda79c96ffb6c35ae35f2f911845b72dab3559cf8
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv, librhash, make, clang"
 TERMUX_PKG_FORCE_CMAKE=yes
