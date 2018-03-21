242f48f94b16344d4b7018dcb21c43a4a83258de
Henrik Grimler <Henrik@Grimler.se>
2018-03-21 21:58:03 +0100

    mc: skip -D_FORTIFY_SOURCE=2 for debug build

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -6,3 +6,13 @@ TERMUX_PKG_SHA256=017ee7f4f8ae420a04f4d6fcebaabe5b494661075c75442c76e9c8b1923d50
 TERMUX_PKG_SRCURL=http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
+
+termux_step_pre_configure() {
+	if [ "$TERMUX_DEBUG" == "true" ]; then
+		# Debug build fails with:
+		# /home/builder/.termux-build/mc/src/src/filemanager/file.c:2019:37: error: 'umask' called with invalid mode
+		# src_mode = umask (-1);
+		#                     ^
+		export CFLAGS=${CFLAGS/-D_FORTIFY_SOURCE=2/}
+	fi
+}
