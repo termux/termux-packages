e5eede45919faebb9f4991cf5a5f8a553c84596c
Henrik Grimler <Henrik@Grimler.se>
2018-03-21 21:41:08 +0100

    libflac: don't use -g3 for debug build
    Configure script removes it otherwise.

--- packages/libflac/build.sh
+++ packages/libflac/build.sh
@@ -5,3 +5,13 @@ TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/flac/flac-1.3.1.tar.xz
 TERMUX_PKG_SHA256=4773c0099dba767d963fd92143263be338c48702172e8754b9bc5103efe1c56c
 TERMUX_PKG_DEPENDS="libogg"
+
+termux_step_pre_configure () {
+	if [ "$TERMUX_DEBUG" == "true" ]; then
+		# libflac does removes the flag "-g" from CFLAGS in the configure script
+		# Not sure why this is done but lets assume there is a good reason for it.
+		# -g3 is normally passed for a debug build in termux, -g is then removed
+		# and only "3" is left which isn't a valid option to the compiler.
+		export CFLAGS="${CFLAGS/-g3/}"
+	fi
+}
