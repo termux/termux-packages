669ae09cb3c6e8758b741aab1c5386c153ef2ac9
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-22 22:48:00 +0100

    nodejs-current: Update from 9.8.0 to 9.9.0

--- packages/nodejs-current/build.sh
+++ packages/nodejs-current/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=9.8.0
-TERMUX_PKG_SHA256=0706bb49e4fa5fa64c6c51941becb4b3854a6c0335425d7312bc086c37b41eac
+TERMUX_PKG_VERSION=9.9.0
+TERMUX_PKG_SHA256=ab8a721b8db1193754885e891ebde9a982c82fd9207df04a2aebafbb9fb27042
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
