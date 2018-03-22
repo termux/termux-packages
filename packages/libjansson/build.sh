b35054e2a2427ca20f55ebae33d5f6b8272af501
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-22 23:10:07 +0100

    libjansson: Update from 2.10 to 2.11

--- packages/libjansson/build.sh
+++ packages/libjansson/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.digip.org/jansson/
 TERMUX_PKG_DESCRIPTION="C library for encoding, decoding and manipulating JSON data"
-TERMUX_PKG_VERSION=2.10
+TERMUX_PKG_VERSION=2.11
+TERMUX_PKG_SHA256=6ff0eab3a8baf64d21cae25f88a0311fb282006eb992080722a9099469c32881
 TERMUX_PKG_SRCURL=https://github.com/akheron/jansson/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b0a899f90ade82e42da0ecabc8af1fa296d69691e7c0786c4994fb79d4833ebb
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure () {
