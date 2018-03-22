7ec420f0b0eb748afdaa3acc0da1ba17a7446174
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-03-23 03:03:43 +0530

    qalc: Update from 2.1.0 to 2.2.1

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://qalculate.github.io/
 TERMUX_PKG_DESCRIPTION="Powerful and easy to use command line calculator"
-TERMUX_PKG_VERSION=2.1.0
-TERMUX_PKG_SHA256=7668ed9ab32d46d3638297985a03bc995b6aedf8b8335685e1a43393ba236f12
+TERMUX_PKG_VERSION=2.2.1
+TERMUX_PKG_SHA256=1752b6a046eaf84ee62a7dc8d2c874b538517956ce8affac80df09a3b55bef5d
 TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v${TERMUX_PKG_VERSION}/libqalculate-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libcurl, libmpfr, libxml2, readline, libgmp"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-icu"
