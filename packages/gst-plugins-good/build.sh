e9e49839ef9753c0689aba4bbb9d4ca69407781b
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-22 23:32:36 +0100

    Rebuild packages linking against libflac

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_VERSION=1.12.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=649f49bec60892d47ee6731b92266974c723554da1c6649f21296097715eb957
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
--- packages/vorbis-tools/build.sh
+++ packages/vorbis-tools/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.vorbis.com
 TERMUX_PKG_DESCRIPTION="Ogg Vorbis tools"
 TERMUX_PKG_VERSION=1.4.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/vorbis-tools-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a389395baa43f8e5a796c99daf62397e435a7e73531c9f44d9084055a05d22bc
 # libflac for flac support in oggenc:
