b2f2e3b7f5a1165008a4b8fc61add7c69f29d239
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-22 22:26:56 +0100

    libtalloc: Update from 2.1.11 to 2.1.12

--- packages/libtalloc/build.sh
+++ packages/libtalloc/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://talloc.samba.org/talloc/doc/html/index.html
 TERMUX_PKG_DESCRIPTION="Hierarchical, reference counted memory pool system with destructors"
-TERMUX_PKG_VERSION=2.1.11
-TERMUX_PKG_SHA256=639eb35556a0af999123c4d883e79be05ff9f00ab4f9e4ac2e5775f9c5eeeed3
+TERMUX_PKG_VERSION=2.1.12
+TERMUX_PKG_SHA256=987c0cf6815e948d20caaca04eba9b823e67773f361ffafe676e24b953cc604b
 TERMUX_PKG_SRCURL=https://www.samba.org/ftp/talloc/talloc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC="yes"
 
