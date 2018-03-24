6fd709fd0aa7dfedd6c226bc539d395f900a8fe4
Tom Yan <tom.ty89@gmail.com>
2018-03-24 15:54:40 +0800

    ndk-stl: copy libc++ headers from untouched copy
    it seems like we want different patches to be applied for the copy
    that are used on termux anyway
    
    let's just copy from the untouched copy, where the paths are less
    confusing as well

--- packages/ndk-stl/build.sh
+++ packages/ndk-stl/build.sh
@@ -6,13 +6,10 @@ TERMUX_PKG_NO_DEVELSPLIT=yes
 
 termux_step_extract_into_massagedir () {
 	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
-	cp -Rf $TERMUX_STANDALONE_TOOLCHAIN/include/c++/4.9.x/* $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
+	cp -Rf $NDK/sources/cxx-stl/llvm-libc++{,abi}/include/* $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 
 	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 	patch -p1 < $TERMUX_PKG_BUILDER_DIR/math-header.diff
-	# Revert the patch for <cstddef> that's only used for using g++
-	# from the ndk (https://github.com/android-ndk/ndk/issues/215):
-	sed "s%\@TERMUX_HOST_PLATFORM\@%${TERMUX_HOST_PLATFORM}%g" $TERMUX_SCRIPTDIR/ndk-patches/cstddef.cpppatch | patch -p1 -R
 }
 
 termux_step_massage () {
