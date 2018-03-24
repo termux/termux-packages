846cea4374213380c13a6410f3f21799434c5b34
Tom Yan <tom.ty89@gmail.com>
2018-03-24 15:48:17 +0800

    ndk-stl: cosmetic fix

--- packages/ndk-stl/build.sh
+++ packages/ndk-stl/build.sh
@@ -8,11 +8,10 @@ termux_step_extract_into_massagedir () {
 	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 	cp -Rf $TERMUX_STANDALONE_TOOLCHAIN/include/c++/4.9.x/* $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 
-	( cd  $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/ && patch -p1 < $TERMUX_PKG_BUILDER_DIR/math-header.diff )
-
+	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
+	patch -p1 < $TERMUX_PKG_BUILDER_DIR/math-header.diff
 	# Revert the patch for <cstddef> that's only used for using g++
 	# from the ndk (https://github.com/android-ndk/ndk/issues/215):
-	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 	sed "s%\@TERMUX_HOST_PLATFORM\@%${TERMUX_HOST_PLATFORM}%g" $TERMUX_SCRIPTDIR/ndk-patches/cstddef.cpppatch | patch -p1 -R
 }
 
