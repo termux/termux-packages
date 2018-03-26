a4bc5ce889381956f955c70ad2a8c43ac37e5dbf
Tom Yan <tom.ty89@gmail.com>
2018-03-26 17:30:23 +0800

    ndk-stl: include libstdc++ headers
    at the very least they are needed for building berkeley db when
    configured with --enable-libcxx

--- packages/ndk-stl/build.sh
+++ packages/ndk-stl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://developer.android.com/tools/sdk/ndk/index.html
-TERMUX_PKG_DESCRIPTION="Header files from the Android NDK needed for compiling C++ programs using STL"
+TERMUX_PKG_DESCRIPTION="Header files from the Android NDK needed for compiling C++ programs using LLVM STL"
 TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_NO_DEVELSPLIT=yes
 
 termux_step_extract_into_massagedir () {
@@ -10,4 +10,18 @@ termux_step_extract_into_massagedir () {
 
 	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/v1/
 	patch -p1 < $TERMUX_PKG_BUILDER_DIR/math-header.diff
+
+	local ARCH
+	if [ "$TERMUX_ARCH" = arm ]; then
+	    ARCH=armeabi-v7a
+	elif [ "$TERMUX_ARCH" = aarch64 ]; then
+	    ARCH=arm64-v8a
+	elif [ "$TERMUX_ARCH" = i686 ]; then
+	    ARCH=x86
+	elif [ "$TERMUX_ARCH" = x86_64 ]; then
+	    ARCH=x86_64
+	fi
+
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/4.9/
+	cp -Rf $NDK/sources/cxx-stl/gnu-libstdc++/4.9{,/libs/$ARCH}/include/* $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/c++/4.9/
 }
