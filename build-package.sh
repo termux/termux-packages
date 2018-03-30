7ddd51409c1702475e0995100260fd6107ed62ba
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-31 00:48:49 +0200

    cmake: Update from 3.10.3 to 3.11.0

--- build-package.sh
+++ build-package.sh
@@ -164,8 +164,8 @@ termux_setup_meson() {
 
 # Utility function to setup a current cmake build system
 termux_setup_cmake() {
-	local TERMUX_CMAKE_MAJORVESION=3.10
-	local TERMUX_CMAKE_MINORVERSION=3
+	local TERMUX_CMAKE_MAJORVESION=3.11
+	local TERMUX_CMAKE_MINORVERSION=0
 	local TERMUX_CMAKE_VERSION=$TERMUX_CMAKE_MAJORVESION.$TERMUX_CMAKE_MINORVERSION
 	local TERMUX_CMAKE_TARNAME=cmake-${TERMUX_CMAKE_VERSION}-Linux-x86_64.tar.gz
 	local TERMUX_CMAKE_TARFILE=$TERMUX_PKG_TMPDIR/$TERMUX_CMAKE_TARNAME
@@ -173,7 +173,7 @@ termux_setup_cmake() {
 	if [ ! -d "$TERMUX_CMAKE_FOLDER" ]; then
 		termux_download https://cmake.org/files/v$TERMUX_CMAKE_MAJORVESION/$TERMUX_CMAKE_TARNAME \
 		                "$TERMUX_CMAKE_TARFILE" \
-		                9e7c48b797484f74c5ee3ae55132b40b16ed8e81ee762402da8971205b0a896b
+		                5babc7953b50715028a05823d18fd91b62805b10aa7811e5fd02b27224d60f10
 		rm -Rf "$TERMUX_PKG_TMPDIR/cmake-${TERMUX_CMAKE_VERSION}-Linux-x86_64"
 		tar xf "$TERMUX_CMAKE_TARFILE" -C "$TERMUX_PKG_TMPDIR"
 		mv "$TERMUX_PKG_TMPDIR/cmake-${TERMUX_CMAKE_VERSION}-Linux-x86_64" \
--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
-TERMUX_PKG_VERSION=3.10.3
-TERMUX_PKG_SHA256=0c3a1dcf0be03e40cf4f341dda79c96ffb6c35ae35f2f911845b72dab3559cf8
+TERMUX_PKG_VERSION=3.11.0
+TERMUX_PKG_SHA256=c313bee371d4d255be2b4e96fd59b11d58bc550a7c78c021444ae565709a656b
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv, librhash, make, clang"
 TERMUX_PKG_FORCE_CMAKE=yes
