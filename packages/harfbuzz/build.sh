ef4c52d10d54a088f12f4c4dbb3270b2d6270451
Fredrik Fornwall <fredrik@fornwall.net>
2018-03-31 10:18:51 +0200

    Rebuild packages after libicu update

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_VERSION=1.7.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=da7bed39134826cd51e57c29f1dfbe342ccedb4f4773b1c951ff05ff383e2e9b
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
