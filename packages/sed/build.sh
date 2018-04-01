14734bd8a213d7e0313cfe0e0c7c9dc17e21423d
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-01 23:45:27 +0200

    sed: Update from 4.4 to 4.5

--- packages/sed/build.sh
+++ packages/sed/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/sed/
 TERMUX_PKG_DESCRIPTION="GNU stream editor for filtering/transforming text"
-TERMUX_PKG_VERSION=4.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.5
+TERMUX_PKG_SHA256=7aad73c8839c2bdadca9476f884d2953cdace9567ecd0d90f9959f229d146b40
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/sed/sed-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cbd6ebc5aaf080ed60d0162d7f6aeae58211a1ee9ba9bb25623daa6cd942683b
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_post_configure () {
