475126a87ff8b55c0074c0777e7b4ef15035f56b
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-02 00:04:32 +0200

    lynx: Update from 2.8.9dev.16 to 2.8.9dev.17

--- packages/lynx/build.sh
+++ packages/lynx/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://lynx.browser.org/
 TERMUX_PKG_DESCRIPTION="The text web browser"
-TERMUX_PKG_VERSION=2.8.9dev.16
-TERMUX_PKG_SHA256=04318a100b052d079d0018fa371aa28cfb41ab68db3a959f3b75c2170eea1bc8
+TERMUX_PKG_VERSION=2.8.9dev.17
+TERMUX_PKG_SHA256=a43811b9078c46ccd6d91a3d5ae0a8bc1f247a4dd89c1a6ccdbcd6c2b3d56ed3
 TERMUX_PKG_SRCURL=http://invisible-mirror.net/archives/lynx/tarballs/lynx${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="ncurses, openssl, libbz2, libidn"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-screen=ncursesw --enable-widec --enable-scrollbar --enable-nested-tables --enable-htmlized-cfg --with-ssl --with-zlib --with-bzlib --enable-cjk --enable-japanese-utf8 --enable-progressbar --enable-prettysrc --enable-forms-options --enable-8bit-toupper --enable-ascii-ctypes --disable-font-switch"
