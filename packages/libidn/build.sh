0b646a838c061c64790b900d59476689ca39fdd9
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-01 23:44:11 +0200

    libidn: Update from 1.33 to 1.34

--- packages/libidn/build.sh
+++ packages/libidn/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/libidn/
 TERMUX_PKG_DESCRIPTION="GNU Libidn library, implementation of IETF IDN specifications"
-TERMUX_PKG_VERSION=1.33
+TERMUX_PKG_VERSION=1.34
+TERMUX_PKG_SHA256=3719e2975f2fb28605df3479c380af2cf4ab4e919e1506527e4c7670afff6e3c
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libidn/libidn-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=44a7aab635bb721ceef6beecc4d49dfd19478325e1b47f3196f7d2acc4930e19
 # Remove the idn tool for now, add it as subpackage if desired::
 TERMUX_PKG_RM_AFTER_INSTALL="bin/idn share/man/man1/idn.1 share/emacs"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ld-version-script"
