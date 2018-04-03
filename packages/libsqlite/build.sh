cd87c90fb046c9141d649f4e1a1e6def3034957c
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-03 23:35:00 +0200

    libsqlite: Update from 3.22.0 to 3.23.0

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.sqlite.org
 TERMUX_PKG_DESCRIPTION="Software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine"
 # Note: Updating this version requires bumping the tcl package as well.
 _SQLITE_MAJOR=3
-_SQLITE_MINOR=22
+_SQLITE_MINOR=23
 _SQLITE_PATCH=0
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
-TERMUX_PKG_SHA256=2824ab1238b706bc66127320afbdffb096361130e23291f26928a027b885c612
+TERMUX_PKG_SHA256=b7711a1800a071674c2bf76898ae8584fc6c9643cfe933cfc1bc54361e3a6e49
 TERMUX_PKG_SRCURL=https://www.sqlite.org/2018/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 # ac_cv_func_strerror_r=no as strerror_r() with the
 # GNU signature is only # available in android-23:
