3cf404e0519711af432b2e5688cec2d8ac6cef05
its-pointless <its-pointless@users.noreply.github.com>
2018-04-07 08:50:40 +1000

    llvm fixes using jit and calling getProcessTriple (#2299)
    * llvm fixes using jit and calling getProcessTriple
    
    * works properly with rustc
    
    * this allows python llvmlite to built

--- packages/libllvm/build.sh
+++ packages/libllvm/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://clang.llvm.org/
 TERMUX_PKG_DESCRIPTION="Modular compiler and toolchain technologies library"
 _PKG_MAJOR_VERSION=6.0
 TERMUX_PKG_VERSION=${_PKG_MAJOR_VERSION}.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=1ff53c915b4e761ef400b803f07261ade637b0c269d99569f18040f3dcee4408
 TERMUX_PKG_SRCURL=https://releases.llvm.org/${TERMUX_PKG_VERSION}/llvm-${TERMUX_PKG_VERSION}.src.tar.xz
 TERMUX_PKG_HOSTBUILD=true
@@ -14,7 +15,7 @@ bin/macho-dump
 lib/libgomp.a
 lib/libiomp5.a
 "
-TERMUX_PKG_DEPENDS="binutils, ncurses, ndk-sysroot, ndk-stl"
+TERMUX_PKG_DEPENDS="binutils, ncurses, ndk-sysroot, ndk-stl, libffi"
 # Replace gcc since gcc is deprecated by google on android and is not maintained upstream.
 # Conflict with clang versions earlier than 3.9.1-3 since they bundled llvm.
 TERMUX_PKG_CONFLICTS="gcc, clang (<< 3.9.1-3)"
@@ -40,6 +41,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DSPHINX_OUTPUT_MAN=ON
 -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly
 -DPERL_EXECUTABLE=$(which perl)
+-DLLVM_ENABLE_FFI=ON
 "
 TERMUX_PKG_FORCE_CMAKE=yes
 TERMUX_PKG_KEEP_STATIC_LIBRARIES=true
@@ -103,6 +105,7 @@ termux_step_pre_configure () {
         # see CMakeLists.txt and tools/clang/CMakeLists.txt
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_DEFAULT_TARGET_TRIPLE=$LLVM_DEFAULT_TARGET_TRIPLE"
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_TARGET_ARCH=$LLVM_TARGET_ARCH -DLLVM_TARGETS_TO_BUILD=all"
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLLVM_HOST_TRIPLE=$LLVM_DEFAULT_TARGET_TRIPLE"
 }
 termux_step_post_make_install () {
 	if [ $TERMUX_ARCH = "arm" ]; then
@@ -121,7 +124,7 @@ termux_step_post_make_install () {
 
 termux_step_post_massage () {
 	sed $TERMUX_PKG_BUILDER_DIR/llvm-config.in \
-		-e "s|@_PKG_MAJOR_VERSION@|$_PKG_MAJOR_VERSION|g" \
+		-e "s|@TERMUX_PKG_VERSION@|$TERMUX_PKG_VERSION|g" \
 		-e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
 		-e "s|@TERMUX_PKG_SRCDIR@|$TERMUX_PKG_SRCDIR|g" \
 		-e "s|@LLVM_TARGET_ARCH@|$LLVM_TARGET_ARCH|g" \
