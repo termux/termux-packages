81da923cf69403d54fd7d8dd98f3bef88426fe89
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-07 01:33:38 +0200

    postgresql: Build the fuzzystrmatch extension

--- packages/postgresql/postgresql-contrib.subpackage.sh
+++ packages/postgresql/postgresql-contrib.subpackage.sh
@@ -1,10 +1,9 @@
 TERMUX_SUBPKG_INCLUDE="
+lib/postgresql/fuzzystrmatch.so
 lib/postgresql/hstore.so
 lib/postgresql/pgcrypto.so
 lib/postgresql/pg_stat_statements.so
-share/postgresql/extension/hstore*
-share/postgresql/extension/pgcrypto*
-share/postgresql/extension/pg_stat_statements*
+share/postgresql/extension/
 "
 TERMUX_SUBPKG_DESCRIPTION="Additional facilities for PostgreSQL"
 TERMUX_SUBPKG_DEPENDS="postgresql"
