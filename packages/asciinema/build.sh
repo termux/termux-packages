52c7c3399346341b6d26c7f27e4e6e9b834aa1e5
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-08 23:45:06 +0200

    asciinema: Update from 1.4.0 to 2.0.1

--- packages/asciinema/build.sh
+++ packages/asciinema/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://asciinema.org/
 TERMUX_PKG_DESCRIPTION="Record and share your terminal sessions, the right way"
-TERMUX_PKG_VERSION=1.4.0
-# ncurses-utils for tput which asciinema uses:
-TERMUX_PKG_DEPENDS="python, ncurses-utils"
+TERMUX_PKG_VERSION=2.0.1
+TERMUX_PKG_SHA256=7087b247dae36d04821197bc14ebd4248049592b299c9878d8953c025ac802e4
 TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=841a55b0f51988d5e155e99badbd6ce5cf3b43cca2ba15cd20c971a19719dc9a
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
+# ncurses-utils for tput which asciinema uses:
+TERMUX_PKG_DEPENDS="python, ncurses-utils"
 
 termux_step_make () {
 	return
