9d134eae2e1b4b1c5eb1bcae57d5e1a3fb414824
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-09 11:23:48 +0200

    libsasl: Update homepage and src url

--- packages/libsasl/build.sh
+++ packages/libsasl/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://asg.web.cmu.edu/sasl/
+TERMUX_PKG_HOMEPAGE=https://www.cyrusimap.org/sasl/
 TERMUX_PKG_DESCRIPTION="Cyrus SASL - authentication abstraction library"
 TERMUX_PKG_VERSION=2.1.26
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://cyrusimap.org/releases/cyrus-sasl-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3
 # Seems to be race issues in build (symlink creation)::
 TERMUX_MAKE_PROCESSES=1
