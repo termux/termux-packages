9db19a6702523c3107b5d67d4e021401568c4c79
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-09 14:14:31 +0200

    zsh: Update from 5.4.2 to 5.5

--- packages/zsh/build.sh
+++ packages/zsh/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.zsh.org
 TERMUX_PKG_DESCRIPTION="Shell with lots of features"
-TERMUX_PKG_VERSION=5.4.2
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=62f5d034d5f4bbaa7a6b08f49aeb16a9c7dc327fd9b3d5a8017d08c66b1beb92
+TERMUX_PKG_VERSION=5.5
+TERMUX_PKG_SHA256=c3d57b92d75e29c8a49d2ff6d146ab63c5968bb7187bf1a9dd35464d22b33b6f
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/zsh-${TERMUX_PKG_VERSION}.tar.xz
 # Remove hard link to bin/zsh as Android does not support hard links:
 TERMUX_PKG_RM_AFTER_INSTALL="bin/zsh-${TERMUX_PKG_VERSION}"
