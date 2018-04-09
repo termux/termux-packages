e54148a7605042f3ac45bf0520ea97ce44c91378
its-pointless <bored@goat.si>
2018-04-10 07:53:05 +1000

    add libtiff cxx interface

---
+++ packages/libtiff/libtiffxx.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="lib/libtiffxx.so"
+TERMUX_SUBPKG_DESCRIPTION="Support for the Tag Image File Format (TIFF) for storing image data -- C++ interface"
+TERMUX_SUBPKG_DEPENDS="libtiff"
