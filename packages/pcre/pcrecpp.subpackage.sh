246858d5bf61312be0506fc80eb9e29f7e4fc01d
its-pointless <bored@goat.si>
2018-04-10 08:10:02 +1000

    pcre: enable c++

---
+++ packages/pcre/pcrecpp.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="lib/libpcrecpp.so"
+TERMUX_SUBPKG_DESCRIPTION="Library implementing regular expression pattern matching using the same syntax and semantics as Perl -- c++"
+TERMUX_SUBPKG_DEPENDS="pcre"
