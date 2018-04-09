82fc3e793692e71013c8a293543c456f5a6c3d41
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-09 14:14:00 +0200

    gdk-pixbuf: Update from 2.36.11 to 2.36.12

--- packages/gdk-pixbuf/build.sh
+++ packages/gdk-pixbuf/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/gdk-pixbuf/
 TERMUX_PKG_DESCRIPTION="Library for image loading and manipulation"
-TERMUX_PKG_VERSION=2.36.11
-TERMUX_PKG_SHA256=ae62ab87250413156ed72ef756347b10208c00e76b222d82d9ed361ed9dde2f3
+TERMUX_PKG_VERSION=2.36.12
+TERMUX_PKG_SHA256=fff85cf48223ab60e3c3c8318e2087131b590fd6f1737e42cb3759a3b427a334
 TERMUX_PKG_SRCURL=ftp://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/${TERMUX_PKG_VERSION:0:4}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libpng-dev, glib-dev"
