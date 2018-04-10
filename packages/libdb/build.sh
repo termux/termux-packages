09799d7331e5b79af8f6454e207004ee28334aae
its-pointless <bored@goat.si>
2018-04-10 22:21:43 +1000

    libdb enabled libc++

--- packages/libdb/build.sh
+++ packages/libdb/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Berkeley DB embedded database system (library)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_VERSION=18.1.32
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=fa1fe7de9ba91ad472c25d026f931802597c29f28ae951960685cde487c8d654
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/db-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -11,6 +11,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-smallbuild
 --enable-compat185
 db_cv_atomic=gcc-builtin
+--enable-cxx
 "
 TERMUX_PKG_RM_AFTER_INSTALL="docs"
 
