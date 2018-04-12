4dcaf6681d6137ed3097d1802fab882b610398b0
tomty89 <tom.ty89@gmail.com>
2018-04-12 13:58:50 +0800

    postgresql: do not split postgresql-contrib (#2336)
    Three reasons:
    1. build-package.sh doesn't have the facility to do this:
    
    $ tar -xf /sdcard/Download/postgresql-9.6_9.6.7-0+deb9u1.debian.tar.xz
    $ cd debian/
    $ grep extension postgresql-9.6.install postgresql-contrib-9.6.install
    postgresql-9.6.install:usr/share/postgresql/*/extension/plpgsql*
    postgresql-contrib-9.6.install:usr/share/postgresql/*/extension/*
    $
    
    while initdb requires extension/plpgsql*.
    
    2. postgresql-contrib takes up only 492kB out of 17.3MB
    
    $ pkg unins postgresql
    Reading package lists... Done
    Building dependency tree
    Reading state information... Done
    The following package was automatically installed and is no longer required:
      libandroid-shmem
    Use 'apt autoremove' to remove it.
    The following packages will be REMOVED:
      postgresql postgresql-contrib
    0 upgraded, 0 newly installed, 2 to remove and 0 not upgraded.
    After this operation, 17.3 MB disk space will be freed.
    Do you want to continue? [Y/n] n
    Abort.
    $ pkg unins postgresql-contrib
    Reading package lists... Done
    Building dependency tree
    Reading state information... Done
    The following packages will be REMOVED:
      postgresql-contrib
    0 upgraded, 0 newly installed, 1 to remove and 0 not upgraded.
    After this operation, 492 kB disk space will be freed.
    Do you want to continue? [Y/n] n
    Abort.
    $
    
    3. Both Ubuntu and Debian has stopped splitting postgresql-contrib.
    It is now only a virtual package provided by postgresql in the 10
    branch.

--- packages/postgresql/postgresql-contrib.subpackage.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="
-lib/postgresql/fuzzystrmatch.so
-lib/postgresql/hstore.so
-lib/postgresql/pgcrypto.so
-lib/postgresql/pg_stat_statements.so
-share/postgresql/extension/
-"
-TERMUX_SUBPKG_DESCRIPTION="Additional facilities for PostgreSQL"
-TERMUX_SUBPKG_DEPENDS="postgresql"
