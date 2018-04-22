9889c15a5f62df42727323f88b0230c59d8a1fd7
Vadzim Miliantsei <Vadzim.Miliantsei@gmail.com>
2018-04-22 23:04:59 +0300

    Use PCRE (#2349)

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -3,4 +3,5 @@ TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a te
 TERMUX_PKG_VERSION=530
 TERMUX_PKG_SHA256=503f91ab0af4846f34f0444ab71c4b286123f0044a4964f1ae781486c617f2e2
 TERMUX_PKG_SRCURL=http://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses"
+TERMUX_PKG_DEPENDS="ncurses, pcre"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-regex=pcre"
