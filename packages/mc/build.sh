b6d04ff11ac618f18f1e67ad568553a6054e016c
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-23 00:48:19 +0200

    mc: Fix path to zip and unzip (fixes #2362)
    Variant of solution in #2363 by Hax4us.

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -1,8 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://www.midnight-commander.org/
 TERMUX_PKG_DESCRIPTION="Midnight Commander - a powerful file manager"
 TERMUX_PKG_VERSION=4.8.20
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=017ee7f4f8ae420a04f4d6fcebaabe5b494661075c75442c76e9c8b1923d501c
 TERMUX_PKG_SRCURL=http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_path_UNZIP=$PREFIX/bin/unzip
+ac_cv_path_ZIP=$PREFIX/bin/zip
+--with-ncurses-libs=$TERMUX_PREFIX/lib
+--with-screen=ncurses
+"
