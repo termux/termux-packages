e478e30e6edeec2c62180fd1e349c419bc7fc032
Andrew Sun <adsun701@gmail.com>
2018-04-23 16:08:45 -0400

    add package: libnftnl

---
+++ packages/libnftnl/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://www.netfilter.org/projects/libnftnl/
+TERMUX_PKG_DESCRIPTION="Netfilter library providing interface to the nf_tables subsystem"
+TERMUX_PKG_VERSION=1.0.9
+TERMUX_PKG_DEPENDS='libmnl'
+TERMUX_PKG_SRCURL=http://netfilter.org/projects/libnftnl/files/libnftnl-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=fec1d824aee301e59a11aeaae2a2d429cb99ead81e6bafab791a4dd6569b3635
