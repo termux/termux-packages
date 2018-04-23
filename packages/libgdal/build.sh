d898c01e9b0ce0bbe30fe655c0dfd755d6192ace
Your Name <you@example.com>
2018-04-23 23:41:36 +0200

    libgdal: add package

---
+++ packages/libgdal/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=http://www.gdal.org/
+TERMUX_PKG_DESCRIPTION="gdal is a translator library for raster and vector geospatial data formats."
+TERMUX_PKG_VERSION=2.2.4
+TERMUX_PKG_SHA256=441eb1d1acb35238ca43a1a0a649493fc91fdcbab231d0747e9d462eea192278
+TERMUX_PKG_SRCURL=http://download.osgeo.org/gdal/$TERMUX_PKG_VERSION/gdal-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_DEPENDS="libjpeg-turbo"
+TERMUX_PKG_BUILD_IN_SRC=YES
+
+
+
