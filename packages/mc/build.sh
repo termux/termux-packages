dedeba6abda242e1a02f2d32e99f1390967fc3bc
Tom Yan <tom.ty89@gmail.com>
2018-04-23 20:25:55 +0800

    mc: use $TERMUX_PREFIX for ac_cv_path_(UN)ZIP

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -6,8 +6,8 @@ TERMUX_PKG_SHA256=017ee7f4f8ae420a04f4d6fcebaabe5b494661075c75442c76e9c8b1923d50
 TERMUX_PKG_SRCURL=http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-ac_cv_path_UNZIP=$PREFIX/bin/unzip
-ac_cv_path_ZIP=$PREFIX/bin/zip
+ac_cv_path_UNZIP=$TERMUX_PREFIX/bin/unzip
+ac_cv_path_ZIP=$TERMUX_PREFIX/bin/zip
 --with-ncurses-libs=$TERMUX_PREFIX/lib
 --with-screen=ncurses
 "
