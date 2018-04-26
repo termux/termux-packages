d73958ee712b13d1556da2f893c2ad0f5dfd6bc6
michalbednarski <michalbednarski@users.noreply.github.com>
2018-04-26 21:37:19 +0200

    proot: Update for Linux >= 4.8 (#2374)

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://proot-me.github.io/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
 # Just bump commit and version when needed:
-_COMMIT=c24fa3a43af2336a93f63fe3fb3eac599f0e3592
+_COMMIT=47138da843bbfcea2273a11ce9d83b40031465bf
 TERMUX_PKG_VERSION=5.1.107
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=185e01d02a0bbf93036d143500e747e06b95fa6056fc1b8c1a093764b467e5b8
+TERMUX_PKG_SHA256=0e6bdc043e13275a1d2d2377190d6f02823576f454ad81ce574884330458a0ce
 TERMUX_PKG_DEPENDS="libtalloc"
 
 termux_step_pre_configure() {
