7c3f8a5eab1ffa7882bb072dbe1cac7f71f8f4dd
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-26 22:17:20 +0200

    gstreamer: Update from 1.12.3 to 1.14.0

--- packages/gst-libav/build.sh
+++ packages/gst-libav/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer libav/ffmpeg wrapper"
-TERMUX_PKG_VERSION=1.12.3
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=015ef8cab6f7fb87c8fb42642486423eff3b6e6a6bccdcd6a189f436a3619650
+TERMUX_PKG_VERSION=1.14.0
+TERMUX_PKG_SHA256=fb134b4d3e054746ef8b922ff157b0c7903d1fdd910708a45add66954da7ef89
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="gst-plugins-base,ffmpeg"
--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
-TERMUX_PKG_VERSION=1.12.4
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=649f49bec60892d47ee6731b92266974c723554da1c6649f21296097715eb957
+TERMUX_PKG_VERSION=1.14.0
+TERMUX_PKG_SHA256=6afa35747d528d3ab4ed8f5eac13f7235d7d28100d6a24dd78f81ec7c0d04688
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="gst-plugins-base,libcaca,libsoup,libjpeg-turbo,libpng,libflac,libbz2,libvpx,libpulseaudio"
+TERMUX_PKG_DEPENDS="gst-plugins-base,libcaca,libsoup,libjpeg-turbo,libpng,libflac,libbz2,libvpx,libpulseaudio,libmp3lame"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-android_media
 --disable-cairo
