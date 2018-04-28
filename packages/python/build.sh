71554b29e0a9b266283815c13e6603d8520f26fa
Tom Yan <tom.ty89@gmail.com>
2018-04-28 20:41:28 +0800

    python(2): remove easy_install in prerm

--- packages/python/build.sh
+++ packages/python/build.sh
@@ -96,8 +96,8 @@ termux_step_create_debscripts () {
 	echo "find $TERMUX_PREFIX/lib/python${_MAJOR_VERSION} -depth -name __pycache__ -exec rm -rf {} \;" >> prerm
 	# Remove contents of site-packages/ folder:
 	echo "rm -Rf $TERMUX_PREFIX/lib/python${_MAJOR_VERSION}/site-packages/*" >> prerm
-	# Remove bin/pip (and bin/pip3* variants) installed by ensurepip in postinst:
-	echo "rm -f $TERMUX_PREFIX/bin/pip $TERMUX_PREFIX/bin/pip3*" >> prerm
+	# Remove pip and easy_install installed by ensurepip in postinst:
+	echo "rm -f $TERMUX_PREFIX/bin/pip $TERMUX_PREFIX/bin/pip3* $TERMUX_PREFIX/bin/easy_install $TERMUX_PREFIX/bin/easy_install-3*" >> prerm
 
 	echo "exit 0" >> postinst
 	echo "exit 0" >> prerm
