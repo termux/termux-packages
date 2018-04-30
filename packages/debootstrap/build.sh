e63f7399e1f3e2bd7eedc8f84995dff4162dcc13
Fredrik Fornwall <fredrik@fornwall.net>
2018-04-30 03:26:35 +0200

    debootstrap: Update from 1.0.93 to 1.0.97

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.debian.org/Debootstrap
 TERMUX_PKG_DESCRIPTION="Bootstrap a basic Debian system"
-TERMUX_PKG_VERSION=1.0.93
-TERMUX_PKG_SHA256=cdad4d2be155bd933acbe4f3479e1765e5f4447fb50564e30e33f7b3b84bd7db
+TERMUX_PKG_VERSION=1.0.97
+TERMUX_PKG_SHA256=d3e6bef403dbabade11d098214030d5063c6b238d3751b159f727af7556c5cf0
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="wget, proot, perl"
