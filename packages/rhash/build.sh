5cbaad1df04a42025fa23f8ee419c31446d1d48f
Auxilus <raghavsphadke@gmail.com>
2018-05-01 15:54:57 +0530

    Add package : RHash

---
+++ packages/rhash/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://rhash.anz.ru
+TERMUX_PKG_DESCRIPTION="console utility for computing and verifying hash sums of files"
+TERMUX_PKG_VERSION=1.3.6
+TERMUX_PKG_SHA256=964df972b60569b5cb35ec989ced195ab8ea514fc46a74eab98e86569ffbcf92
+TERMUX_PKG_DEPENDS="openssl, openssl-dev, gettext, gettext-dev"
+TERMUX_PKG_SRCURL=https://github.com/rhash/RHash/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
