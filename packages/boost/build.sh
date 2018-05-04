ae4f2ea25e8e265b65351cebb263618708427cd0
Tom Yan <tom.ty89@gmail.com>
2018-05-04 15:21:04 +0800

    boost: add python2 variant of libboost_python

--- packages/boost/build.sh
+++ packages/boost/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_SHA256=2684c972994ee57fc5632e03bf044746f6eb45d4920c343937a465fd67a5ad
 TERMUX_PKG_SRCURL=https://dl.bintray.com/boostorg/release/$TERMUX_PKG_VERSION/source/boost_${TERMUX_PKG_VERSION//./_}.tar.bz2
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="libbz2, liblzma"
-TERMUX_PKG_BUILD_DEPENDS="python"
+TERMUX_PKG_BUILD_DEPENDS="python, python2"
 TERMUX_PKG_BREAKS="libboost-python (<= 1.65.1-2)"
 TERMUX_PKG_REPLACES="libboost-python (<= 1.65.1-2)"
 
@@ -33,4 +33,22 @@ termux_step_make_install() {
 		link=shared \
 		threading=multi \
 		install
+
+	./bootstrap.sh --with-libraries=python
+
+	echo "using clang : $TERMUX_ARCH : $CXX : <linkflags>-L/data/data/com.termux/files/usr/lib ; " >> project-config.jam
+
+	echo "using python : 2.7 : $TERMUX_PREFIX/bin/python2 : $TERMUX_PREFIX/include/python2.7 : $TERMUX_PREFIX/lib ;" >> project-config.jam
+
+	./b2 target-os=android -j${TERMUX_MAKE_PROCESSES} \
+		include=/data/data/com.termux/files/usr/include \
+		toolset=clang-$TERMUX_ARCH \
+		--stagedir="$TERMUX_PREFIX"  \
+		-q \
+		-a \
+		--disable-icu \
+		cxxflags="$CXXFLAGS" \
+		link=shared \
+		threading=multi \
+		stage
 }
