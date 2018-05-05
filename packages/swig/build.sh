41104b12fed29be74133409e658fb90a48d7a6ad
raku-cat <raku@raku.party>
2018-05-05 12:50:40 -0500

    remove swig, clean up znc dependencies

--- packages/swig/build.sh
+++
@@ -1,5 +0,0 @@
-TERMUX_PKG_HOMEPAGE=http://swig.org
-TERMUX_PKG_DESCRIPTION="A software development tool"
-TERMUX_PKG_VERSION=3.0.12
-TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/project/swig/swig/swig-${TERMUX_PKG_VERSION}/swig-${TERMUX_PKG_VERSION}.tar.gz?r=http%3A%2F%2Fwww.swig.org%2Fdownload.html&ts=1525111933&use_mirror=cytranet"
-TERMUX_PKG_DEPENDS="python, tcl, perl, pcre"
--- packages/znc/build.sh
+++ packages/znc/build.sh
@@ -4,17 +4,7 @@ TERMUX_PKG_VERSION=1.7.0
 TERMUX_PKG_MAINTAINER=raku-cat
 TERMUX_PKG_SHA256=c07e31439ac6b948a577bd61a9d5f61a6d191d387423779b937aa1404051b96f
 TERMUX_PKG_SRCURL=https://znc.in/releases/znc-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libandroid-support, readline, openssl, python, tcl, perl, python, libicu, swig, readline, libsasl, bthread"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWANT_ZLIB=true -DWANT_OPENSSL=true -DWANT_PYTHON=true -DWANT_TCL=true -DWANT_SWIG=true -DPERL_EXECUTABLE=/usr/bin/perl"
+TERMUX_PKG_BUILD_DEPENDS="libandroid-support, readline, python, perl, tcl"
+TERMUX_PKG_DEPENDS="bthread, libicu, libsasl, boost"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWANT_ZLIB=true -DWANT_OPENSSL=true -DWANT_PYTHON=true -DWANT_TCL=true -DWANT_I18N=true"
 TERMUX_PKG_FORCE_CMAKE=yes
-
-#termux_step_pre_configure() {
-#    mkdir repo
-#    git clone --bare https://github.com/znc/znc.git repo
-#    mkdir .git
-#    mv repo/* .git/
-#    git init
-#    git pull
-#    git reset HEAD
-#    git submodule update --init --recursive
-#}
