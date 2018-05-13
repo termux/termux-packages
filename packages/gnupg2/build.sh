cb281f9ba88e375e588fa52309fe38e5f9bbb006
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-13 02:32:10 +0200

    gnupg2: Update from 2.2.6 to 2.2.7

--- packages/gnupg2/build.sh
+++ packages/gnupg2/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/
 TERMUX_PKG_DESCRIPTION="Implementation of the OpenPGP standard for encrypting and signing data and communication"
-TERMUX_PKG_VERSION=2.2.6
-TERMUX_PKG_SHA256=e64d8c5fa2d05938a5080cb784a98ac21be0812f2a26f844b18f0d6a0e711984
+TERMUX_PKG_VERSION=2.2.7
+TERMUX_PKG_SHA256=d95b361ee6ef7eff86af40c8c72bf9313736ac9f7010d6604d78bf83818e976e
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libassuan,libbz2,libgcrypt,libksba,libsqlite,libnpth,readline,pinentry,libgpg-error"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
