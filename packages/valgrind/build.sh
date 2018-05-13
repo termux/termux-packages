72e3d20413808d292d62a87e6889e5b5993e49d6
Henrik Grimler <henrik@grimler.se>
2018-05-13 11:11:31 +0000

    valgrind: use clang on all arches except aarch64

--- packages/valgrind/build.sh
+++ packages/valgrind/build.sh
@@ -1,15 +1,17 @@
 TERMUX_PKG_HOMEPAGE=http://valgrind.org/
 TERMUX_PKG_DESCRIPTION="Instrumentation framework for building dynamic analysis tools"
 TERMUX_PKG_VERSION=3.13.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=ftp://sourceware.org/pub/valgrind/valgrind-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=d76680ef03f00cd5e970bbdcd4e57fb1f6df7d2e2c071635ef2be74790190c3b
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-tmpdir=$TERMUX_PREFIX/tmp"
 # - Does not build on x86_64 due to lacking upstream support of that arch on android.
 #   See https://bugs.kde.org/show_bug.cgi?id=348342
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
-# With a clang build on aarch64:
-# "error: the clang compiler does not support '-mcpu=cortex-a8'":
-TERMUX_PKG_CLANG=no
+# Fails on aarch64 with `__builtin_longjmp is not supported for the current target`
+if [ $TERMUX_ARCH = aarch64 ]; then
+       TERMUX_PKG_CLANG=no
+fi
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" == "arm" ]; then
