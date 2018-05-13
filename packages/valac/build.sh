202554e4bf7cdf5e62a515895e4023d862d6939c
its-pointless <bored@goat.si>
2018-05-14 09:51:38 +1000

    symlink fix

--- packages/valac/build.sh
+++ packages/valac/build.sh
@@ -5,8 +5,8 @@ TERMUX_PKG_SHA256=379354a2a2f7ee5c4d6e0f5e88b0e32620dcd5f51972baf6d90d9f18eb6891
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/vala/${TERMUX_PKG_VERSION:0:4}/vala-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_DEPENDS="clang, glib-dev, pkg-config"
-
+TERMUX_PKG_REVISION=1
 termux_step_post_make_install() {
 	cd $TERMUX_PREFIX/lib
-	ln -s -f vala-0.38/libvalaccodegen.so libvalaccodegen.so
+	ln -s -f vala-0.40/libvalaccodegen.so libvalaccodegen.so
 }
