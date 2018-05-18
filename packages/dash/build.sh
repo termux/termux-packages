a487cad9e61848015baf9acade4d533d8401981c
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-19 00:16:15 +0200

    dash: Update from 0.5.10.1 to 0.5.10.2

--- packages/dash/build.sh
+++ packages/dash/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://gondor.apana.org.au/~herbert/dash/
 TERMUX_PKG_DESCRIPTION="Small POSIX-compliant implementation of /bin/sh"
 TERMUX_PKG_ESSENTIAL=yes
-TERMUX_PKG_VERSION=0.5.10.1
-TERMUX_PKG_SHA256=daac92be392ea5d9b03e8879384e437828e636b9f491a275a607f85d7e5e84ae
+TERMUX_PKG_VERSION=0.5.10.2
+TERMUX_PKG_SHA256=3c663919dc5c66ec991da14c7cf7e0be8ad00f3db73986a987c118862b5f6071
 TERMUX_PKG_SRCURL=http://gondor.apana.org.au/~herbert/dash/files/dash-${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_post_make_install () {
