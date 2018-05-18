0f60236951fb8a174295774a43b6e6481833b50d
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-19 00:55:59 +0200

    inotify-tools: Update from 3.14 to 3.20.1

--- packages/inotify-tools/build.sh
+++ packages/inotify-tools/build.sh
@@ -1,17 +1,18 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/rvoicilas/inotify-tools/wiki
 TERMUX_PKG_DESCRIPTION="Programs providing a simple interface to inotify"
-TERMUX_PKG_VERSION=3.14
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/downloads/rvoicilas/inotify-tools/inotify-tools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=222bcca8893d7bf8a1ce207fb39ceead5233b5015623d099392e95197676c92f
+TERMUX_PKG_VERSION=3.20.1
+TERMUX_PKG_SHA256=a433cc1dedba851078276db69b0e97f9fe41e4ba3336d2971adfca4b3a6242ac
+TERMUX_PKG_SRCURL=https://github.com/rvoicilas/inotify-tools/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
+	./autogen.sh
+
 	LDFLAGS+=" -llog"
 }
 
 termux_step_make () {
-	continue
+	:
 }
 
 termux_step_make_install () {
