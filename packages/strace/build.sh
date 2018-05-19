f74a9f45a12135c55831c7376192c80c3c15403f
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-19 02:57:29 +0200

    strace: Update from 4.20 to 4.22

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,11 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor the system calls used by a program and all the signals it receives"
-TERMUX_PKG_VERSION=4.20
-TERMUX_PKG_SHA256=5bf3148dd17306a42566f7da17368fdd781afa147db05ea63a4ca2b50f58c523
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/strace/strace/${TERMUX_PKG_VERSION}/strace-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION=4.22
+TERMUX_PKG_SHA256=068cd09264c95e4d591bbcd3ea08f99a693ed8663cd5169b0fdad72eb5bdb39d
+TERMUX_PKG_SRCURL=https://strace.io/files/$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="st_cv_m32_mpers=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+st_cv_m32_mpers=no
+--enable-mpers=no
+"
 
 termux_step_pre_configure () {
 	if [ $TERMUX_ARCH_BITS = "64" ]; then
