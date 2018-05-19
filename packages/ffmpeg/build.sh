6b27b56f4414a28493f83a3d5f869eaae119be87
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-19 02:37:04 +0200

    ffmpeg: Bump revision after libsoxr and libx265 updates

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://ffmpeg.org
 TERMUX_PKG_DESCRIPTION="Tools and libraries to manipulate a wide range of multimedia formats and protocols"
 # NOTE: mpv has to be rebuilt and version bumped after updating ffmpeg.
 TERMUX_PKG_VERSION=3.4.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=2b92e9578ef8b3e49eeab229e69305f5f4cbc1fdaa22e927fc7fca18acccd740
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
 # libbz2 is used by matroska decoder:
