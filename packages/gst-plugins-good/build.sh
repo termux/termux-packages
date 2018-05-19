e508eed1474f32b37ea82b1d62a42d997608487b
vishalbiswas <vshlbiswas@ymail.com>
2018-05-19 08:36:35 +0530

    Revert "gst-plugins-good: disable video4linux2"
    This reverts commit 43850b81ccf60033c25cb13dfd326c1b0528773a.
    Already applied on base branch.

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_VERSION=1.14.0
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=6afa35747d528d3ab4ed8f5eac13f7235d7d28100d6a24dd78f81ec7c0d04688
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -14,6 +13,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-oss
 --disable-oss4
 --disable-tests
---disable-gst_v4l2
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/"
