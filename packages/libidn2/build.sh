3c5858d640bd45fecb649f070028fc10a5f70959
Hax4us <lkpandey950@gmail.com>
2018-05-19 22:27:06 +0530

    update build.sh

--- packages/libidn2/build.sh
+++ packages/libidn2/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_VERSION=2.0.5
 TERMUX_PKG_MAINTAINER="lokesh @hax4us"
 TERMUX_PKG_DEPENDS="libunistring, libandroid-support"
 TERMUX_PKG_SHA256=53f69170886f1fa6fa5b332439c7a77a7d22626a82ef17e2c1224858bb4ca2b8
-TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libidn/libidn2-{TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libidn/libidn2-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-doc --enable-static=no"
