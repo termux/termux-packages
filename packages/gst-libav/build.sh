23f23ada1499ef4642ba1f39b415e7464da78238
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-19 02:36:37 +0200

    gstreamer: Update from 1.14.0 to 1.14.1

--- packages/gst-libav/build.sh
+++ packages/gst-libav/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer libav/ffmpeg wrapper"
-TERMUX_PKG_VERSION=1.14.0
-TERMUX_PKG_SHA256=fb134b4d3e054746ef8b922ff157b0c7903d1fdd910708a45add66954da7ef89
+TERMUX_PKG_VERSION=1.14.1
+TERMUX_PKG_SHA256=eff80a02d2f2fb9f34b67e9a26e9954d3218c7aa18e863f2a47805fa7066029d
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="gst-plugins-base,ffmpeg"
