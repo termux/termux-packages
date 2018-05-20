67d37de0cc7fb27a8dfddbaa8ccfdc3cbc783120
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-20 14:36:54 +0200

    p7zip: Minor cleanup

--- packages/p7zip/build.sh
+++ packages/p7zip/build.sh
@@ -1,13 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://p7zip.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Command-line version of the 7zip compressed file archiver"
 TERMUX_PKG_MAINTAINER="Francisco Demartino @franciscod"
-# adapted from archlinux PKGBUILD
-pkgname=p7zip
-pkgver=16.02
-TERMUX_PKG_VERSION=$pkgver
+TERMUX_PKG_VERSION=16.02
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL="http://downloads.sourceforge.net/project/${pkgname}/${pkgname}/${pkgver}/${pkgname}_${pkgver}_src_all.tar.bz2"
 TERMUX_PKG_SHA256=5eb20ac0e2944f6cb9c2d51dd6c4518941c185347d4089ea89087ffdd6e2341f
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/p7zip/p7zip/${TERMUX_PKG_VERSION}/p7zip_${TERMUX_PKG_VERSION}_src_all.tar.bz2
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_configure () {
@@ -15,7 +12,8 @@ termux_step_configure () {
 }
 
 termux_step_make () {
-	LD="$CC $LDFLAGS" CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS" make -j $TERMUX_MAKE_PROCESSES all3 OPTFLAGS="${CXXFLAGS}" DEST_HOME=$TERMUX_PREFIX
+	LD="$CC $LDFLAGS" CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS" \
+		make -j $TERMUX_MAKE_PROCESSES all3 OPTFLAGS="${CXXFLAGS}" DEST_HOME=$TERMUX_PREFIX
 }
 
 termux_step_make_install () {
