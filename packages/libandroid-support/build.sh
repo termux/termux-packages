de749d96c573361fef12c79da9fd67ff0b1de85f
Tom Yan <tom.ty89@gmail.com>
2018-05-25 06:24:40 +0800

    libandroid-support: add missing symlink for langinfo.h

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
 TERMUX_PKG_VERSION=22
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=667f20d0821a6305c50c667363486d546b293e846f31d02f559947d50121f51e
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -16,11 +17,14 @@ termux_step_make_install () {
 
 	cp libandroid-support.so $TERMUX_PREFIX/lib/
 
-	(cd $TERMUX_PREFIX/lib; ln -f -s libandroid-support.so libiconv.so; )
+	(cd $TERMUX_PREFIX/lib
+	 ln -sf libandroid-support.so libiconv.so)
 
 	rm -Rf $TERMUX_PREFIX/include/libandroid-support
 	mkdir -p $TERMUX_PREFIX/include/libandroid-support
 	cp -Rf include/* $TERMUX_PREFIX/include/libandroid-support/
 
-	(cd $TERMUX_PREFIX/include; ln -f -s libandroid-support/iconv.h iconv.h)
+	(cd $TERMUX_PREFIX/include
+	 ln -sf libandroid-support/iconv.h
+	 ln -sf libandroid-support/langinfo.h)
 }
