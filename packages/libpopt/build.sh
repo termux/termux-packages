551bae4776009144c590e5b79b9b9393b67ea042
Fredrik Fornwall <fredrik@fornwall.net>
2018-05-25 21:14:48 +0200

    libpopt: Update to working download url

--- packages/libpopt/build.sh
+++ packages/libpopt/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://www.linuxfromscratch.org/blfs/view/svn/general/popt.h
 TERMUX_PKG_DESCRIPTION="Library for parsing cmdline parameters"
 TERMUX_PKG_VERSION=1.16
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=http://rpm5.org/files/popt/popt-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/popt-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8
 TERMUX_PKG_DEPENDS="libandroid-glob"
 
