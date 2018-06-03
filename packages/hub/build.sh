67f12d98606437fd195a503af9b18243f3c7d34e
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-04 00:42:01 +0200

    hub: Update from 2.2.8 to 2.3.0

--- packages/hub/build.sh
+++ packages/hub/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://hub.github.com/
 TERMUX_PKG_DESCRIPTION="Command-line wrapper for git that makes you better at GitHub"
-TERMUX_PKG_VERSION=2.2.8
+TERMUX_PKG_VERSION=2.3.0
+TERMUX_PKG_SHA256=69e48105f7287537e7afaf969825666c1f09267eae3507515151900487342fae
 TERMUX_PKG_SRCURL=https://github.com/github/hub/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5ff475990a5bd335bad8da2d140ea0966c33e6d03dac26bf1558bda94a02b924
 TERMUX_PKG_DEPENDS="git"
 
 termux_step_make_install() {
@@ -13,6 +13,6 @@ termux_step_make_install() {
 	./script/build
 
 	cp bin/hub $TERMUX_PREFIX/bin/
-	mkdir -p $TERMUX_PREFIX/share/man/man1/
-	cp man/hub.1 $TERMUX_PREFIX/share/man/man1/
+	#mkdir -p $TERMUX_PREFIX/share/man/man1/
+	#cp man/hub.1 $TERMUX_PREFIX/share/man/man1/
 }
