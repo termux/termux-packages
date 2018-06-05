d149b255a14db0ebda8a1891b792e71d98ee91d0
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-05 23:34:45 +0200

    termux-api: Update from 0.28 to 0.29

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://termux.com/add-on-api.html
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
-TERMUX_PKG_VERSION=0.28
-TERMUX_PKG_SHA256=b01b5310db26510b4fd98c76b0fc29051745b2cb6bb60b1cb9c230889213899c
+TERMUX_PKG_VERSION=0.29
+TERMUX_PKG_SHA256=8b2a4cff4d8f9b78bcba5ab63e22a31929987ff566f74d1b144d8ac63c5e87e3
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
