42f5e27284026182939e714cf2efdc038ec29830
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-05 23:35:28 +0200

    fossil: Update from 2.5 to 2.6

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION=2.5
-TERMUX_PKG_SHA256=f9b07360811f432dfb36ebfb44c83886872556ecce1c80387629ce90e1e4aad3
+TERMUX_PKG_VERSION=2.6
+TERMUX_PKG_SHA256=76a794555918be179850739a90f157de0edb8568ad552b4c40ce186c79ff6ed9
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libsqlite, openssl"
 
