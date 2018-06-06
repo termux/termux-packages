2c51dfcd5410f59f7cc6ef961d48d295ed3e9ace
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-07 00:21:55 +0200

    fribidi: Update from 1.0.3 to 1.0.4

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/fribidi/fribidi/
 TERMUX_PKG_DESCRIPTION="Implementation of the Unicode Bidirectional Algorithm"
-TERMUX_PKG_VERSION=1.0.3
-TERMUX_PKG_SHA256=8d214b17b6eedcb416e53142c196c2896b9a685fca2a5bddc098a73d2b02ce12
+TERMUX_PKG_VERSION=1.0.4
+TERMUX_PKG_SHA256=94bdfe553e004d8bd095b109e182682311dd511740d5083326d1582f1df237be
 TERMUX_PKG_SRCURL=https://github.com/fribidi/fribidi/releases/download/v$TERMUX_PKG_VERSION/fribidi-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-docs"
