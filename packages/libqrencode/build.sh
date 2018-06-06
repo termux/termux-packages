59c1a410bfbbf67fe9e27932e020d0c331565c91
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-07 00:22:08 +0200

    libqrencode: Update from 4.0.0 to 4.0.2

--- packages/libqrencode/build.sh
+++ packages/libqrencode/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://fukuchi.org/works/qrencode/
 TERMUX_PKG_DESCRIPTION="Libqrencode is a fast and compact library for encoding data in a QR Code symbol, a 2D symbology that can be scanned by handy terminals"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=4.0.0
+TERMUX_PKG_VERSION=4.0.2
+TERMUX_PKG_SHA256=43091fea4752101f0fe61a957310ead10a7cb4b81e170ce61e5baa73a6291ac2
 TERMUX_PKG_SRCURL=https://github.com/fukuchi/libqrencode/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c2c8a8110354463a3332cb48abf8581c8d94136af4dc1418f891cc9c7719e3c1
 TERMUX_PKG_DEPENDS="libandroid-support, libpng"
