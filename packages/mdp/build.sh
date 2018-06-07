0616e1f382b4534a0d1be2ecd2739903fa0acaae
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-06-08 04:31:38 +0530

    mdp: Add package

---
+++ packages/mdp/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/visit1985/mdp
+TERMUX_PKG_DESCRIPTION=" A command-line based markdown presentation tool."
+TERMUX_PKG_VERSION=1.0.12
+TERMUX_PKG_MAINTAINER='lokesh @hax4us'
+TERMUX_PKG_DEPENDS="ncurses"
+TERMUX_PKG_SHA256=9faca0245babd54aa47cbbb72c14244ca9a9a84f63021a679078041da4f6e98b
+TERMUX_PKG_SRCURL=https://github.com/visit1985/mdp/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=yes
