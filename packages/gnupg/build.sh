80eebb3457bf2097317a3dbc21c25b0a76ac32c3
Oliver Schmidhauser <oli@glow.li>
2018-06-12 10:34:05 +0200

    Update gnupg to 1.4.23

--- packages/gnupg/build.sh
+++ packages/gnupg/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/
 TERMUX_PKG_DESCRIPTION="OpenPGP implementation for encrypting and signing data and communication"
-TERMUX_PKG_VERSION=1.4.22
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=9594a24bec63a21568424242e3f198b9d9828dea5ff0c335e47b06f835f930b4
+TERMUX_PKG_VERSION=1.4.23
+TERMUX_PKG_SHA256=c9462f17e651b6507848c08c430c791287cd75491f8b5a8b50c6ed46b12678ba
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
 # disable readline since gnupg is used in bootstrap, so nice to avoid readline+ncurses dependencies.
 # ac_cv_sys_symbol_underscore=no needed for i686 build to avoid "undefined reference to `mpihelp_sub_n'" errors
