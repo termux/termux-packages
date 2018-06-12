2130a84ac5c9660136f599f03344b515d0a010c2
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-13 01:11:08 +0200

    imagemagick: Update from 7.0.7.38 to 7.0.7.39

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
-TERMUX_PKG_VERSION=7.0.7.38
-TERMUX_PKG_SHA256=ac957ef303fb870cb92331947ebcdcef5b553e80c7897c0aec866889f35e1a23
+TERMUX_PKG_VERSION=7.0.7.39
+TERMUX_PKG_SHA256=e7c1b19923bb97ed456c78b63b3259b809ebc8e3967c6d086450370c67eedf06
 local _download_version=`echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/'`
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/${_download_version}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -13,13 +13,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-xml=yes
 --with-lzma
 ac_cv_func_ftime=no
-ac_cv_header_complex_h=no"
+ac_cv_header_complex_h=no
+"
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/Magick-config
 bin/MagickCore-config
 bin/MagickWand-config
 bin/Wand-config
-share/ImageMagick-6/francais.xml
+share/ImageMagick-7/francais.xml
 share/man/man1/Magick-config.1
 share/man/man1/MagickCore-config.1
 share/man/man1/MagickWand-config.1
