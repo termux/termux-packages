6251709f9a1cc11512da5f167338d10eb8571562
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-13 02:11:42 +0200

    Update build.sh

--- packages/mc/build.sh
+++ packages/mc/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_VERSION=4.8.21
 TERMUX_PKG_SHA256=8f37e546ac7c31c9c203a03b1c1d6cb2d2f623a300b86badfd367e5559fe148c
 TERMUX_PKG_SRCURL=http://ftp.midnight-commander.org/mc-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, glib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncurses-libs=$TERMUX_PREFIX/lib --with-screen=ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_UNZIP=$TERMUX_PREFIX/bin/unzip
 ac_cv_path_ZIP=$TERMUX_PREFIX/bin/zip
