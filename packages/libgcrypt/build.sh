2f9f9a64edfb6217b86cab6971686331ef367d3f
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-13 21:09:42 +0200

    libgcrypt: Update from 1.8.2 to 1.8.3

--- packages/libgcrypt/build.sh
+++ packages/libgcrypt/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libgcrypt/
 TERMUX_PKG_DESCRIPTION="General purpose cryptographic library based on the code from GnuPG"
-TERMUX_PKG_VERSION=1.8.2
-TERMUX_PKG_SHA256=c8064cae7558144b13ef0eb87093412380efa16c4ee30ad12ecb54886a524c07
+TERMUX_PKG_VERSION=1.8.3
+TERMUX_PKG_SHA256=66ec90be036747602f2b48f98312361a9180c97c68a690a5f376fa0f67d0af7c
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libgpg-error"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libgpg-error-dev"
