78600253c88307994b1262ce352dd516eef8d1ec
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-14 23:30:56 +0200

    nodejs: Update from 8.11.2 to 8.11.3

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
-TERMUX_PKG_VERSION=8.11.2
-TERMUX_PKG_SHA256=539946c0381809576bed07424a35fc1740d52f4bd56305d6278d9e76c88f4979
+TERMUX_PKG_VERSION=8.11.3
+TERMUX_PKG_SHA256=577c751fdca91c46c60ffd8352e5b465881373bfdde212c17c3a3c1bd2616ee0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
