d9c56fd35e423855260165ea466f8aef2891e144
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-19 15:12:18 +0300

    libcurl: use correct path for 'ntlm_auth'

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -2,9 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_DEPENDS="openssl, libnghttp2"
 TERMUX_PKG_VERSION=7.60.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=897dfb2204bd99be328279f88f55b7c61592216b0542fcbe995c60aa92871e9b
 TERMUX_PKG_SRCURL=http://curl.askapache.com/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-ntlm-wb=$TERMUX_PREFIX/bin/ntlm_auth
 --with-ca-bundle=$TERMUX_PREFIX/etc/tls/cert.pem
 --with-nghttp2
 --without-libidn
