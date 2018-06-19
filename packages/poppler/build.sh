379adf84453eb3f510c8efbb58e96213afc2eeff
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-19 16:06:38 +0300

    poppler: fix fonts-related paths

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_VERSION=0.65.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=89c8cf73f83efda78c5a9bd37c28f4593ad0e8a51556dbe39ed81e1ae2dd8f07
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
