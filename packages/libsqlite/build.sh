1231bdf90b14f2152da609cb91d0fa57e327b85b
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-06-19 16:40:58 +0300

    libsqlite: use correct path for temp dirs

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -6,6 +6,7 @@ _SQLITE_MINOR=24
 _SQLITE_PATCH=0
 TERMUX_PKG_SHA256=d9d14e88c6fb6d68de9ca0d1f9797477d82fc3aed613558f87ffbdbbc5ceb74a
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.sqlite.org/2018/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 # ac_cv_func_strerror_r=no as strerror_r() with the
 # GNU signature is only # available in android-23:
