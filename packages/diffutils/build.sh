97bc151cce441759801d063206c8ae06bd9068d7
Quasic <qbasic010@aim.com>
2018-06-19 01:24:25 -0400

    diffutils: no cmd command, only cmp

--- packages/diffutils/build.sh
+++ packages/diffutils/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/diffutils/
-TERMUX_PKG_DESCRIPTION="Programs (cmd, diff, diff3 and sdiff) related to finding differences between files"
+TERMUX_PKG_DESCRIPTION="Programs (cmp, diff, diff3 and sdiff) related to finding differences between files"
 TERMUX_PKG_VERSION=3.6
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/diffutils/diffutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d621e8bdd4b573918c8145f7ae61817d1be9deb4c8d2328a65cea8e11d783bd6
