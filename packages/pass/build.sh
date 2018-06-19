c3a784b9d0703e774a3011671ecf6555fdef152b
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-20 01:15:57 +0200

    pass: Update from 1.7.1 to 1.7.2

--- packages/pass/build.sh
+++ packages/pass/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.passwordstore.org/
 TERMUX_PKG_DESCRIPTION="Lightweight directory-based password manager"
-TERMUX_PKG_VERSION=1.7.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=1.7.2
+TERMUX_PKG_SHA256=4768c5e1965c4d2aeb28818681e484fb105b6f46cbd75a97608615c4ec6980ea
 TERMUX_PKG_SRCURL=https://git.zx2c4.com/password-store/snapshot/password-store-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 # Depend on coreutils as pass uses [:graph:] when calling tr, which
@@ -11,4 +11,3 @@ TERMUX_PKG_RECOMMENDS="git"
 TERMUX_PKG_SUGGESTS="pass-otp"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
-TERMUX_PKG_SHA256=f6d2199593398aaefeaa55e21daddfb7f1073e9e096af6d887126141e99d9869
