304acd0131e566b616ccfb0986718c1250e497f7
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-20 01:25:16 +0200

    erlang: Update from 20.3.7 to 20.3.8

--- packages/erlang/build.sh
+++ packages/erlang/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.erlang.org/
 TERMUX_PKG_DESCRIPTION="General-purpose concurrent functional programming language developed by Ericsson"
-TERMUX_PKG_VERSION=20.3.7
-TERMUX_PKG_SHA256=de78b01942694d0505814a2f372ee0d756bc4f27e5ba1fbb2a975893208fff66
+TERMUX_PKG_VERSION=20.3.8
+TERMUX_PKG_SHA256=a7e59c531605fbd19ceac0568ae81c89ec690449e861da6e3598399f9c4b747f
 TERMUX_PKG_SRCURL=https://github.com/erlang/otp/archive/OTP-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="openssl, ncurses, libutil"
 TERMUX_PKG_HOSTBUILD="yes"
