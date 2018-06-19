240705a7f6411579f687d7c2bc5b7280763f14a3
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-06-19 17:51:14 +0300

    libcairo: fix tmpdir path for 'libcairo-script-interpreter.so'

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://cairographics.org
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
 TERMUX_PKG_VERSION=1.14.12
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/cairo-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, libpixman, fontconfig, freetype, libpng"
