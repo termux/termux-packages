22217dc4c4d5b496089df537a1252aa2b2c19f07
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-21 00:32:54 +0200

    gdbm: Update from 1.14.1 to 1.15

--- packages/gdbm/build.sh
+++ packages/gdbm/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org.ua/software/gdbm/
 TERMUX_PKG_DESCRIPTION="Library of database functions that use extensible hashing"
-TERMUX_PKG_VERSION=1.14.1
-TERMUX_PKG_SHA256=cdceff00ffe014495bed3aed71c7910aa88bf29379f795abc0f46d4ee5f8bc5f
+TERMUX_PKG_VERSION=1.15
+TERMUX_PKG_SHA256=f9fde3207f67ed8a5a5ddd8ad5e7acf7b27c2cf0f20dfbdde876dcd6e3d2dc0e
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gdbm/gdbm-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-readline"
