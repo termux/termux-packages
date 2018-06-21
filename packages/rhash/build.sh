d7c92ffe9641e8036477ca0d535dd3e1121db738
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-22 01:22:14 +0200

    librhash: Rename to rhash for discoverability

--- packages/rhash/build.sh
+++ packages/rhash/build.sh
@@ -1,12 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/rhash/RHash
 TERMUX_PKG_DESCRIPTION="Console utility for calculation and verification of magnet links and a wide range of hash sums"
 TERMUX_PKG_VERSION=1.3.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SHA256=964df972b60569b5cb35ec989ced195ab8ea514fc46a74eab98e86569ffbcf92
 TERMUX_PKG_SRCURL=https://github.com/rhash/RHash/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
+TERMUX_PKG_CONFLICTS=librhash
+TERMUX_PKG_REPLACES=librhash
 
 termux_step_make () {
 	CFLAGS="-DOPENSSL_RUNTIME $CPPFLAGS $CFLAGS"
