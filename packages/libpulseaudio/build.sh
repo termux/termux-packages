ede252d426a52acd4bb664b3a03a4e71f0b10830
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-21 23:33:11 +0200

    libpulseaudio: Build with --disable-gsettings

--- packages/libpulseaudio/build.sh
+++ packages/libpulseaudio/build.sh
@@ -14,6 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-neon-opt
 --without-caps
 --with-database=simple
 --disable-memfd
+--disable-gsettings
 --bindir=$TERMUX_PREFIX/libexec
 ax_cv_PTHREAD_PRIO_INHERIT=no"
 TERMUX_PKG_CONFFILES="etc/pulse/client.conf etc/pulse/daemon.conf etc/pulse/default.pa etc/pulse/system.pa"
