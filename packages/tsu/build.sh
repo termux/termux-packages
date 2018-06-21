0fac83ccb47072adb3ce4b670f5628f9c922674e
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-22 01:03:26 +0200

    tsu: Simplify build
    This also ensures that we have a srcurl for repology.

--- packages/tsu/build.sh
+++ packages/tsu/build.sh
@@ -2,16 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cswl/tsu
 TERMUX_PKG_DESCRIPTION="A su wrapper for Termux"
 TERMUX_PKG_VERSION=2.0
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
+TERMUX_PKG_SRCURL=https://github.com/cswl/tsu/archive/ce32547e7ca441ed449b12447539da959b889e95.zip
+TERMUX_PKG_SHA256=11c0b0c0b1c9acb64d354ce9b0348d3d950e06e274d567498b1b46e8fd51fb9e
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_make() {
+	:
+}
 
 termux_step_make_install () {
-	termux_download https://raw.githubusercontent.com/cswl/tsu/c0a2603b57511a1492dfe6ec4e6ef452e01a58e6/tsu \
-	                $TERMUX_PREFIX/bin/tsu \
-	                ee33a58fd19bfc6c99aab8af4c836ede4b749b4f1ca5957cbb66503828bec7c7
-	termux_download https://raw.githubusercontent.com/cswl/tsu/c0a2603b57511a1492dfe6ec4e6ef452e01a58e6/tsudo \
-					$TERMUX_PREFIX/bin/tsudo \
-					275d025ce7e241f989fcbb1d12f49ed9ab927b3b0e8a1a41de2a89fc1bfee6b0
-	touch $TERMUX_PREFIX/bin/tsu
+	cp tsu $TERMUX_PREFIX/bin/tsu
 	chmod +x $TERMUX_PREFIX/bin/tsu
-	touch $TERMUX_PREFIX/bin/tsudo
+
+	cp tsudo $TERMUX_PREFIX/bin/tsudo
 	chmod +x $TERMUX_PREFIX/bin/tsudo
 }
