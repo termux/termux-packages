14ce94c2244137990f4f28d74b72249953b2bd65
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 05:09:28 +0200

    teckit: Update from 2.5.7 to 2.5.8

--- packages/teckit/build.sh
+++ packages/teckit/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://scripts.sil.org/teckitdownloads
 TERMUX_PKG_DESCRIPTION="TECkit is a library for encoding conversion"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=2.5.7
-TERMUX_PKG_SHA256="d6a820b201b1a8f369a3002975d30fd96a01d9af3119514121e4d793daeb88c4"
-TERMUX_PKG_SRCURL="https://github.com/silnrsi/teckit/archive/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_VERSION=2.5.8
+TERMUX_PKG_SHA256=0ea52b1304f430aaebff99fec355bc7e4ad75b16ba58959fae5079627f925f93
+TERMUX_PKG_SRCURL=https://github.com/silnrsi/teckit/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_expat_XML_ExpatVersion=no"
 
 termux_step_pre_configure() {
