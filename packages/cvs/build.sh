a306f1d611e03cf0ff56be20436a6e607e02cdc1
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 03:54:43 +0200

    cvs: Update from 1.11.23 to 1.12.13

--- packages/cvs/build.sh
+++ packages/cvs/build.sh
@@ -1,7 +1,11 @@
-TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/cvs/
+TERMUX_PKG_HOMEPAGE=https://www.nongnu.org/cvs/
 TERMUX_PKG_DESCRIPTION="Concurrent Versions System"
-TERMUX_PKG_VERSION=1.11.23
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/non-gnu/cvs/source/stable/${TERMUX_PKG_VERSION}/cvs-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=400f51b59d85116e79b844f2d5dbbad4759442a789b401a94aa5052c3d7a4aa9
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-server"
+TERMUX_PKG_VERSION=1.12.13
+TERMUX_PKG_SHA256=27f3bc1be1f538a8390faab1d452db3ea90f55065e3dc0efe095457243a409b3
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/old/cvs-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+cvs_cv_func_printf_ptr=yes
+ac_cv_header_syslog_h=no
+--disable-server
+"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/cvsbug share/man/man8/cvsbug.8"
