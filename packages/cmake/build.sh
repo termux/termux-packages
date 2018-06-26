380c2d7bc399461cb22c7859b26b8155e99e4939
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 10:48:42 +0200

    Bump some c++-using packages after libc++ update

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
 TERMUX_PKG_VERSION=3.11.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=8f864e9f78917de3e1483e256270daabc4a321741592c5b36af028e72bff87f5
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libarchive, libcurl, libexpat, jsoncpp, libuv, rhash, make, clang"
