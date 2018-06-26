6aba3c6fcae87f47c04729e38c8135f06eeeca2d
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 03:48:07 +0200

    pngquant: Update from 2.11.7 to 2.12.1

--- packages/pngquant/build.sh
+++ packages/pngquant/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://pngquant.org
 TERMUX_PKG_DESCRIPTION="PNG image optimising utility"
-TERMUX_PKG_VERSION=2.11.7
-TERMUX_PKG_SHA256=(0ca09a1f253b264e5aab8477b7f0e3cde51d9f88ed668b38ae057ced24076bda
-		   6b912616cfb60c5ff49f316649e1279bd7a1d797d6ace0bdbb532ebdf778a8bd)
+TERMUX_PKG_VERSION=2.12.1
+TERMUX_PKG_SHA256=(352ff60420fd5ab7a94f548be6f87dbdfa15eb28e3cc8f61c089f4e0be7ee1a0
+		   c0f6ddfb1ed214a520b18f6fff16fe09f39d427caf9679b05657e32dfdc2cb27)
 # If both archives are .tar.gz then they overwrite eachother since they are the same version and hence the same name.
 # Work around this by using .zip for one of them...
 TERMUX_PKG_SRCURL=(https://github.com/pornel/pngquant/archive/$TERMUX_PKG_VERSION.tar.gz
