f782b4cc49df4710d81ae237eaa35a809c483126
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 05:39:11 +0200

    redir: Update from 2.2.1 to 3.2

--- packages/redir/build.sh
+++ packages/redir/build.sh
@@ -1,7 +1,11 @@
-TERMUX_PKG_HOMEPAGE=http://sammy.net/~sammy/hacks/
-TERMUX_PKG_DESCRIPTION="Redirect TCP connections"
-TERMUX_PKG_VERSION=2.2.1
-TERMUX_PKG_SRCURL=http://sammy.net/~sammy/hacks/redir-2.2.1.tar.gz
-TERMUX_PKG_SHA256=7ea504f835338e448f674ca8637512f511bf74538418ad43ab39039017090e6c
+TERMUX_PKG_HOMEPAGE=https://github.com/troglobit/redir
+TERMUX_PKG_DESCRIPTION="TCP port redirector for UNIX"
+TERMUX_PKG_VERSION=3.2
+TERMUX_PKG_SHA256=e3c61bc4a51cf9228f916aeee3cb510bdff29ec2cdc2a1c8ae43d597bec89baa
+TERMUX_PKG_SRCURL=https://github.com/troglobit/redir/releases/download/v$TERMUX_PKG_VERSION/redir-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_BUILD_IN_SRC="yes"
 TERMUX_PKG_RM_AFTER_INSTALL="share/applications share/pixmaps"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -llog"
+}
