b4709565b54c68d30f9bb2a7273384105f49ce7e
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 04:54:43 +0200

    newsboat: Update from 2.11.1 to 2.12

--- packages/newsboat/build.sh
+++ packages/newsboat/build.sh
@@ -1,7 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://newsboat.org/
 TERMUX_PKG_DESCRIPTION="RSS/Atom feed reader for the text console"
-TERMUX_PKG_VERSION=2.11.1
-TERMUX_PKG_SHA256=aab67dcd386a3512812e1af0fddb8e1f4dce08d2a106946fe5ad405210ed37cf
+TERMUX_PKG_API_LEVEL=24
+TERMUX_PKG_VERSION=2.12
+TERMUX_PKG_SHA256=9bbdbc2bca9e0c75a75588d89de3862f72e3fcb41c5d7db6210e2b491ffd43f4
 TERMUX_PKG_SRCURL=https://newsboat.org/releases/${TERMUX_PKG_VERSION}/newsboat-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, json-c, libsqlite, libcurl, libxml2, stfl, ncurses, openssl"
 TERMUX_PKG_BUILD_IN_SRC=yes
