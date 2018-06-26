5ffcba108f18501760e18b1c4c39c284a4e6fff5
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 03:48:26 +0200

    mime-support: Update from 3.60 to 3.61

--- packages/mime-support/build.sh
+++ packages/mime-support/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/en/stretch/mime-support
 TERMUX_PKG_DESCRIPTION="MIME type associations for file types"
-TERMUX_PKG_VERSION=3.60
+TERMUX_PKG_VERSION=3.61
+TERMUX_PKG_SHA256=027fa5b709b3dabfb8dcdf68ab8c59e1f534a37eb1d1b5160ee1fd788103131d
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/m/mime-support/mime-support_$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f31d81f68dc007f56567cc14fb3b2effbd42d1dd087e414508e14e33d1a6a3a4
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 TERMUX_PKG_CONFFILES="etc/mime.types"
 # etc/mime.types was previously in mutt:
