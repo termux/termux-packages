b83ab76029ff60d6a3e977a13e5215282a2093a4
Fredrik Fornwall <fredrik@fornwall.net>
2018-06-26 03:50:44 +0200

    msmtp: Update from 1.6.6 to 1.6.7

--- packages/msmtp/build.sh
+++ packages/msmtp/build.sh
@@ -1,12 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://msmtp.sourceforge.net
-TERMUX_PKG_DESCRIPTION="light SMTP client"
-TERMUX_PKG_VERSION=1.6.6
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/msmtp/files/msmtp/1.6.6/msmtp-1.6.6.tar.xz
-TERMUX_PKG_SHA256=da15db1f62bd0201fce5310adb89c86188be91cd745b7cb3b62b81a501e7fb5e
-#TERMUX_PKG_BUILD_IN_SRC=yes
-#TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
-#TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_HOMEPAGE=https://marlam.de/msmtp/
+TERMUX_PKG_DESCRIPTION="Lightweight SMTP client"
+TERMUX_PKG_VERSION=1.6.7
+TERMUX_PKG_SHA256=419da2ae177e95eb8fe698725d2cae43e50c77d11d5a3992ecc2739a05964357
+TERMUX_PKG_SRCURL=https://marlam.de/msmtp/releases/msmtp-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_DEPENDS="libgnutls, libidn"
+
 termux_step_pre_configure () {
 	LDFLAGS=" -llog"
 	cp $TERMUX_SCRIPTDIR/packages/alpine/getpass* src/
