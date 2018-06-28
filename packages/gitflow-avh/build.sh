95e63b44f15226062f9f1b158e14335395c3bd26
Hax4us <lkpandey950@gmail.com>
2018-06-28 14:44:01 +0530

    add gitflow

---
+++ packages/gitflow-avh/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/petervanderdoes/gitflow/
+TERMUX_PKG_DESCRIPTION="Extend git with Vincent Driessen's branching model. The AVH Edition adds more functionality."
+TERMUX_PKG_VERSION=1.11.0
+TERMUX_PKG_MAINTAINER="lokesh @hax4us"
+TERMUX_PKG_DEPENDS="util-linux"
+TERMUX_PKG_SHA256=06ad2110088e46e3712f799a43bf6cc5c3720fc25c69dbb3bbf4cf486cf2f330
+TERMUX_PKG_SRCURL=https://github.com/petervanderdoes/gitflow/archive/${TERMUX_PKG_VERSION}.tar.gz
