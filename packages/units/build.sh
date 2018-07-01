02278d56f236cec289732fe8e641b1629f694946
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-02 00:26:08 +0200

    units: Update from 2.16 to 2.17

--- packages/units/build.sh
+++ packages/units/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/units/
 TERMUX_PKG_DESCRIPTION="Converts between different systems of units"
-TERMUX_PKG_VERSION=2.16
+TERMUX_PKG_VERSION=2.17
+TERMUX_PKG_SHA256=1f03c9f9aef1fb0ccddc6fb545f2925f7754de2bfa67d4e62f8758d7edfd5dd8
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/units/units-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=dcf3f78482a13b150fc6bf0f8d611510816a3424bc2b26229b85b1cadeb81686
 TERMUX_PKG_DEPENDS="readline, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
