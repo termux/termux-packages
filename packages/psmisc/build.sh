fdd4879dc08598c078ff2a1d0fd5044f81312438
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-02 01:06:58 +0200

    psmisc: Update from 22.21 to 23.1

--- packages/psmisc/build.sh
+++ packages/psmisc/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/psmisc/psmisc
 TERMUX_PKG_DESCRIPTION="Some small useful utilities that use the proc filesystem"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION=22.21
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/psmisc/psmisc/psmisc-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=97323cad619210845b696d7d722c383852b2acb5c49b5b0852c4f29c77a8145a
+TERMUX_PKG_VERSION=23.1
+TERMUX_PKG_SHA256=2e84d474cf75dfbe3ecdacfb797bbfab71a35c7c2639d1b9f6d5f18b2149ba30
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/psmisc-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_RM_AFTER_INSTALL="bin/pstree.x11"
