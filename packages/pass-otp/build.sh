73b0efcdfe5e4653e85cf375915289d7b81e445e
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-02 00:18:54 +0200

    pass-otp: Update from 1.1.0 to 1.1.1

--- packages/pass-otp/build.sh
+++ packages/pass-otp/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=htts://github.com/tadfisher/pass-otp
 TERMUX_PKG_DESCRIPTION="A pass extension for managing one-time-password (OTP) tokens"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SHA256=edb3142ab81d70af4e6d1c7f13abebd7c349be474a3f9293d9648ee91b75b458
 TERMUX_PKG_SRCURL=https://github.com/tadfisher/pass-otp/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3971467475f8ed573eb860c7a44bd268d464d169dddbc0a4da89232d5beee144
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 TERMUX_PKG_DEPENDS="pass, oathtool"
