edd512a8f0c141dd4f8d4d978102f59b0e56f9fa
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-01 23:19:57 +0200

    erlang: Update from 20.3.8 to 21.0.1

--- packages/erlang/build.sh
+++ packages/erlang/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.erlang.org/
 TERMUX_PKG_DESCRIPTION="General-purpose concurrent functional programming language developed by Ericsson"
-TERMUX_PKG_VERSION=20.3.8
-TERMUX_PKG_SHA256=a7e59c531605fbd19ceac0568ae81c89ec690449e861da6e3598399f9c4b747f
+TERMUX_PKG_VERSION=21.0.1
+TERMUX_PKG_SHA256=65a5f1c54bd52a5e9840cda536a2b37611bb08ab869295bba812c06c2bc7c9bc
 TERMUX_PKG_SRCURL=https://github.com/erlang/otp/archive/OTP-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="openssl, ncurses, libutil"
 TERMUX_PKG_HOSTBUILD="yes"
@@ -22,6 +22,8 @@ termux_step_host_build () {
 }
 
 termux_step_pre_configure () {
+	(cd erts && autoreconf)
+
 	# liblog is needed for syslog usage:
 	LDFLAGS+=" -llog"
 	# Put binaries built in termux_step_host_build at start of PATH:
