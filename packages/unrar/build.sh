d10cbfa4ec010c8954a2463283757848eeac416c
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-07-03 14:09:45 +0300

    unrar: fix getpass()
    1. Use TCSANOW instead of TSAFLUSH to prevent failure on Android 8.
    2. Return empty password if failed to read from console, fixes segfault.

--- packages/unrar/build.sh
+++ packages/unrar/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.rarlab.com/
 TERMUX_PKG_DESCRIPTION="Tool for extracting files from .rar archives"
 TERMUX_PKG_VERSION=5.6.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=eba36a421bf41491818dee9507d934064622bc0bd9db6bbb8422a4706f200898
 TERMUX_PKG_SRCURL=https://www.rarlab.com/rar/unrarsrc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support,readline"
