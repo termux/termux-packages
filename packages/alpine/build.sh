c8fdfdf90095c7b809cfabc38c72a70d5cf5653a
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-07-04 00:18:11 +0300

    fix packages that require getpass()
     Changed packages:
    
      * alpine
      * dropbear
      * isync
      * msmtp
      * newsboat
      * screen
      * texlive-bin
      * unrar
      * w3m

--- packages/alpine/build.sh
+++ packages/alpine/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://repo.or.cz/alpine.git
 TERMUX_PKG_DESCRIPTION="Fast, easy to use email client"
 TERMUX_PKG_VERSION=2.21
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SHA256=6030b6881b8168546756ab3a5e43628d8d564539b0476578e287775573a77438
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/alpine-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_DEPENDS="libcrypt, ncurses, openssl-tool"
+TERMUX_PKG_DEPENDS="libcrypt, libgetpass, ncurses, openssl-tool"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-debug
 --with-c-client-target=lnx
@@ -24,13 +24,11 @@ termux_step_pre_configure () {
 	export alpine_SSLVERSION=old
 	export TPATH=$PATH
 
-	LDFLAGS+=" -lcrypt -llog"
+	LDFLAGS+=" -lcrypt -lgetpass -llog"
 
 	# To get S_IREAD and friends:
 	CPPFLAGS+=" -D__USE_BSD"
 
-	cp $TERMUX_PKG_BUILDER_DIR/getpass.c $TERMUX_PKG_SRCDIR/include/
-	cp $TERMUX_PKG_BUILDER_DIR/getpass.h $TERMUX_PKG_SRCDIR/include/
 	cp $TERMUX_PKG_BUILDER_DIR/pine.conf $TERMUX_PREFIX/etc/pine.conf
 
 	touch $TERMUX_PKG_SRCDIR/imap/lnxok
--- packages/screen/build.sh
+++ packages/screen/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with VT100/ANSI terminal emulation"
 TERMUX_PKG_VERSION=4.6.2
 TERMUX_PKG_SHA256=1b6922520e6a0ce5e28768d620b0f640a6631397f95ccb043b70b91bb503fa3a
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/screen/screen-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses, libcrypt, libutil"
+TERMUX_PKG_DEPENDS="ncurses, libcrypt, libgetpass, libutil"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-socket-dir
 --enable-colors256
@@ -14,7 +14,7 @@ termux_step_pre_configure () {
 	# Run autoreconf since we have patched configure.ac
 	autoconf
 	CFLAGS+=" -DGETUTENT"
-	LDFLAGS+=" -llog -lcrypt"
+	LDFLAGS+=" -llog -lcrypt -lgetpass"
 }
 
 termux_step_post_configure() {
