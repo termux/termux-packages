8e0450e4d7a66e2660fa4024bea6ab4c0e0fb691
Tom Yan <tom.ty89@gmail.com>
2018-07-04 16:44:26 +0800

    gmic: build with cmake

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -4,23 +4,17 @@ TERMUX_PKG_VERSION=2.3.2
 TERMUX_PKG_SHA256=160ef2fff116eedf56ea4635762a347f499761034cc21f0f16d1baea20c4e9ec
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="graphicsmagick++, libcurl, fftw, libpng, libjpeg-turbo, libtiff"
-TERMUX_PKG_BUILD_DEPENDS="graphicsmagick-dev"
-TERMUX_PKG_NO_DEVELSPLIT=yes
-TERMUX_PKG_BUILD_IN_SRC=yes
-
-termux_step_configure() {
-	return 0;
-}
-
-termux_step_make () {
-	cd src/
-	make cli
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_LIB=off -DBUILD_LIB_STATIC=off -DENABLE_X=off -DENABLE_FFMPEG=off -DENABLE_OPENCV=off -DENABLE_OPENEXR=off -DENABLE_OPENMP=off"
+TERMUX_PKG_FORCE_CMAKE=yes
 
 termux_step_make_install() {
-	cp src/gmic $TERMUX_PREFIX/bin/gmic
-	cp src/*.h $TERMUX_PREFIX/include/
-	gunzip man/gmic.1.gz
+	cp gmic $TERMUX_PREFIX/bin/gmic
+
+	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp man/gmic.1 $TERMUX_PREFIX/share/man/man1
+
+	mkdir -p $TERMUX_PREFIX/share/bash-completion/completions
+	cp resources/gmic_bashcompletion.sh \
+	   $TERMUX_PREFIX/share/bash-completion/completions
 }
 
