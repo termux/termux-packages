38c5c97038a8b6fddc92e15669a24be59c5f4235
its-pointless <bored@goat.si>
2018-07-09 08:57:34 +1000

    fix htmlcxx

--- packages/htmlcxx/build.sh
+++ packages/htmlcxx/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/dhoerl/htmlcxx
+TERMUX_PKG_HOMEPAGE=http://htmlcxx.sourceforge.net
 TERMUX_PKG_DESCRIPTION="simple HTML/CSS1 parser library for C++"
-TERMUX_PKG_VERSION=0.85
-TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/h/htmlcxx/htmlcxx_$TERMUX_PKG_VERSION.orig.tar.gz
-TERMUX_PKG_SHA256=ab02a0c4addc82f82d564f7d163fe0cc726179d9045381c288f5b8295996bae5
+TERMUX_PKG_VERSION=0.86
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/htmlcxx/files/htmlcxx/$TERMUX_PKG_VERSION/htmlcxx-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=07542b5ea2442143b125ba213b6823ff4a23fff352ecdd84bbebe1d154f4f5c1
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
 termux_step_pre_configure() {
