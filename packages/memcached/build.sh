1e4080785e11841716ae6a0c1caaff3f9a2d5d22
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-07-09 13:42:22 +0300

    memcached: update to 1.5.9

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
-TERMUX_PKG_VERSION=1.5.8
-TERMUX_PKG_SHA256=7388c3989442a46d7e04e8ac2631464e9ce44d5114d79c41dd8780b4b186f6de
+TERMUX_PKG_VERSION=1.5.9
+TERMUX_PKG_SHA256=4af3577dbf71cb0a748096dc6562ccd587cddb7565c720f1fdb23e8a34241d06
 TERMUX_PKG_SRCURL=https://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-sasl --disable-coverage"
