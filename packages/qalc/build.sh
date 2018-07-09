759f5262cbadee03cccba2c504392b4bfba9e50d
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-09 23:05:10 +0200

    qalc: Update from 2.6.0b to 2.6.1

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -1,10 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://qalculate.github.io/
 TERMUX_PKG_DESCRIPTION="Powerful and easy to use command line calculator"
-# Note: Strange download url:
-TERMUX_PKG_VERSION=2.6.0b
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=d85c18398fd273e85c9f259e7aa9050aa51f190036815bafb832de779f8a03a7
-# TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v${TERMUX_PKG_VERSION}/libqalculate-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v2.6.0a/libqalculate-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=2.6.1
+TERMUX_PKG_SHA256=ed4c467ef8b87611f8c15794b16aef15628ba8f45aaa428619f2d58ed4248609
+TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v$TERMUX_PKG_VERSION/libqalculate-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libcurl, libmpfr, libxml2, readline, libgmp"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-icu"
