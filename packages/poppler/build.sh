8b997baa3eeb1b3c7692799b8c65547df815906f
Tom Yan <tom.ty89@gmail.com>
2018-07-09 22:18:39 +0800

    poppler: make use of fontconfig
    The CMakeLists.txt shipped by upstream assumes that fontconfig is
    not available in Android, which is not the case in Termux. Enable
    the use of fontconfig with cached value.
    
    Also remove the patch that matters only when poppler is built
    without fontconfig support.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_VERSION=0.66.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=2c096431adfb74bc2f53be466889b7646e1b599f28fa036094f3f7235cc9eae7
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
@@ -10,4 +10,5 @@ TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, little
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_GLIB=OFF
 -DENABLE_XPDF_HEADERS=ON
+-DFONT_CONFIGURATION=fontconfig
 "
