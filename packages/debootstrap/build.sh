3624eb83e25e8f2e73c507fb088608960ebd8e96
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-07-10 17:46:43 +0300

    debootstrap: update to 1.0.106

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.debian.org/Debootstrap
 TERMUX_PKG_DESCRIPTION="Bootstrap a basic Debian system"
-TERMUX_PKG_VERSION=1.0.102
-TERMUX_PKG_SHA256=09675c7338ff0a328a1d67d84af93af46716a618d6c8cdb8c76d1ce07cb70ec5
+TERMUX_PKG_VERSION=1.0.106
+TERMUX_PKG_SHA256=4ffcd12477540cbccdbed7c254f77797b886228ca94e749414d0380eab62c26e
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="wget, proot, perl"
