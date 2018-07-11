632d193f2a143389254b2de4ed198e16f0771794
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-11 11:29:47 +0200

    tor: Update from 0.3.3.7 to 0.3.3.8

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_DEPENDS="libevent, openssl, liblzma"
-TERMUX_PKG_VERSION=0.3.3.7
-TERMUX_PKG_SHA256=ea6bb512c4adfbc4e05b22e4c2d06bddff5b358a53de982273fec846b75bde0c
+TERMUX_PKG_VERSION=0.3.3.8
+TERMUX_PKG_SHA256=3cf5b4fea2491cbfefa1077bf1b2855e169052d381bd920cd57aa9bfaff6b3a5
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-zstd --disable-unittests"
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
