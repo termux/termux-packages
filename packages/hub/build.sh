b80cfec6218e89211d43be1d7bb4c7e511b9acb5
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-11 11:55:14 +0200

    hub: Update from 2.4.0 to 2.5.0

--- packages/hub/build.sh
+++ packages/hub/build.sh
@@ -1,18 +1,19 @@
 TERMUX_PKG_HOMEPAGE=https://hub.github.com/
 TERMUX_PKG_DESCRIPTION="Command-line wrapper for git that makes you better at GitHub"
-TERMUX_PKG_VERSION=2.4.0
-TERMUX_PKG_SHA256=894eb112be9aa0464fa2c63f48ae8e573ef9e32a00bad700e27fd09a0cb3be4b
+TERMUX_PKG_VERSION=2.5.0
+TERMUX_PKG_SHA256=8e3bda092ddc81eaf208c5fd2b87f66e030012129d55fa631635c6adf8437941
 TERMUX_PKG_SRCURL=https://github.com/github/hub/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="git"
 
 termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR
-
 	termux_setup_golang
 
-	./script/build
+	cd $TERMUX_PKG_SRCDIR
 
-	cp bin/hub $TERMUX_PREFIX/bin/
-	#mkdir -p $TERMUX_PREFIX/share/man/man1/
-	#cp man/hub.1 $TERMUX_PREFIX/share/man/man1/
+	export GOPATH="${TERMUX_PKG_BUILDDIR}"
+	mkdir -p "${GOPATH}/src/github.com/github"
+	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/github/hub"
+	cd "${GOPATH}/src/github.com/github/hub"
+	make bin/hub prefix=$TERMUX_PREFIX
+	cp ./bin/hub $TERMUX_PREFIX/bin/hub
 }
