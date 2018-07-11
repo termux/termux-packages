a4107d154af2651ddaef651016b97b06998eddc0
Henrik Grimler <henrik@grimler.se>
2018-07-11 16:39:41 +0200

    libmnl: setting LIBDIR doesn't seem necessary
    I can no longer reproduce the issue

--- packages/libmnl/build.sh
+++ packages/libmnl/build.sh
@@ -3,4 +3,3 @@ TERMUX_PKG_DESCRIPTION="a minimalistic user-space library oriented to Netlink de
 TERMUX_PKG_VERSION=1.0.4
 TERMUX_PKG_SRCURL=https://netfilter.org/projects/libmnl/files/libmnl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=171f89699f286a5854b72b91d06e8f8e3683064c5901fb09d954a9ab6f551f81
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" LIBDIR=$TERMUX_PREFIX/lib"
