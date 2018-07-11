fc42b0aae9c167a1bcb927b8118764984e52bae8
Henrik Grimler <henka20@hotmail.com>
2018-07-11 19:03:26 +0200

    libmnl: change to https url (#2642)

--- packages/libmnl/build.sh
+++ packages/libmnl/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://www.netfilter.org/projects/libmnl/
 TERMUX_PKG_DESCRIPTION="a minimalistic user-space library oriented to Netlink developers"
 TERMUX_PKG_VERSION=1.0.4
-TERMUX_PKG_SRCURL=http://ftp.netfilter.org/pub/libmnl/libmnl-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SRCURL=https://netfilter.org/projects/libmnl/files/libmnl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=171f89699f286a5854b72b91d06e8f8e3683064c5901fb09d954a9ab6f551f81
