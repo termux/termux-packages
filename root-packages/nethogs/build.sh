9b2006d794a49f6c15fceb4e6690d31f0a140086
Henrik Grimler <Henrik@Grimler.se>
2018-07-12 20:21:09 +0200

    nethogs: bump package to fix https://github.com/termux/termux-root-packages/issues/33

--- root-packages/nethogs/build.sh
+++ root-packages/nethogs/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/raboof/nethogs
 TERMUX_PKG_DESCRIPTION="Net top tool grouping bandwidth per process"
 TERMUX_PKG_VERSION=0.8.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/raboof/nethogs/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_FOLDERNAME=nethogs-${TERMUX_PKG_VERSION}
