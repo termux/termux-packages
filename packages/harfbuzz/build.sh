518a58c595fc66cf5094ecd7a20d3242142faed2
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-14 00:26:20 +0200

    harfbuzz: Update from 1.8.2 to 1.8.3

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
-TERMUX_PKG_VERSION=1.8.2
-TERMUX_PKG_SHA256=c8a3f88f416185a82f899a68b5714d6a40d7dc4d1d7379057348918d55aac657
+TERMUX_PKG_VERSION=1.8.3
+TERMUX_PKG_SHA256=2ac2ad1ca26253bfe7f437adcd034c84dad0e7c2c0fe008d6051b754fa987a9e
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="freetype,glib,libbz2,libpng,libgraphite"
 TERMUX_PKG_BUILD_DEPENDS="libicu"
