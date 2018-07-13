ce590d2d74036fcd5e5a5309169e8500cf185d12
Karol Baraniecki <karol@karcom.pl>
2018-07-13 18:04:31 +0200

    calc: switch to clang, add utils for travis,
    and remove unneccesary configuration file with $TERMUX_PATH in it

--- packages/calc/build.sh
+++ packages/calc/build.sh
@@ -5,24 +5,26 @@ TERMUX_PKG_DEPENDS="readline"
 TERMUX_PKG_SRCURL="http://www.isthe.com/chongo/src/calc/calc-${TERMUX_PKG_VERSION}.tar.bz2"
 TERMUX_PKG_SHA256='3a4f1acde15941048214f393beb97f9e12fc1ef2585fe0ab026e93ebcd19dd46'
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_CLANG=no
 TERMUX_PKG_EXTRA_MAKE_ARGS="T=$TERMUX_PREFIX/.."
 
 termux_step_make() {
+  # compile our own `col` as its needed for compilation and Travis doesn't provide one
+  cc -lbsd $TERMUX_PKG_BUILDER_DIR/col.c -o col
+
   # Configure the project to be able to generate included help files
-  make -j1 hsrc
+  make -j1 hsrc $TERMUX_PKG_EXTRA_MAKE_ARGS
 
   # Generate the included help
-  make -j1 help/builtin
+  make -j1 help/builtin $TERMUX_PKG_EXTRA_MAKE_ARGS
 
   cp $TERMUX_PKG_BUILDER_DIR/configured-for-android/* .
 
   # Stop make from configuring again. Not doing this causes the build to
   # break nondeterministically as it sometimes decides to reconfigure itself in this step.
-  sed -i 's/\(.*h\): /\1.do-not-make: /' Makefile
+  sed -i '/^conf\.h/! s/\(.*h\): /\1.do-not-make: /' Makefile
 
   make -j1 all \
-    T="$TERMUX_PREFIX/.." \
+    $TERMUX_PKG_EXTRA_MAKE_ARGS \
     USE_READLINE="-DUSE_READLINE" \
     READLINE_LIB="-L$TERMUX_PREFIX/lib -lreadline" \
     READLINE_EXTRAS="-lhistory -lncurses" \
