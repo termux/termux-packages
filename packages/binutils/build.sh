71bf1621a958a4cdac5e2558aa010745c82fa93f
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-15 17:15:45 +0200

    binutils: Update from 2.30 to 2.31

--- packages/binutils/build.sh
+++ packages/binutils/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/binutils/
 TERMUX_PKG_DESCRIPTION="Collection of binary tools, the main ones being ld, the GNU linker, and as, the GNU assembler"
-TERMUX_PKG_VERSION=2.30
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.31
+TERMUX_PKG_SHA256=5a9de9199f22ca7f35eac378f93c45ead636994fc59f3ac08f6b3569f73fcf6f
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/binutils/binutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gold --enable-plugins --disable-werror --with-system-zlib --enable-new-dtags"
-TERMUX_PKG_SHA256=8c3850195d1c093d290a716e20ebcaa72eda32abf5e3d8611154b39cff79e9ea
 TERMUX_PKG_EXTRA_MAKE_ARGS="tooldir=$TERMUX_PREFIX"
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/windmc.1 share/man/man1/windres.1 bin/ld.bfd"
 TERMUX_PKG_KEEP_STATIC_LIBRARIES=true
