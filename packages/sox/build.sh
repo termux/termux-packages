51abf56995846c9f5487fa2c9ef3d531897c00ba
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-15 17:58:22 +0200

    sox: Rebuild after pulseaudio update

--- packages/sox/build.sh
+++ packages/sox/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://sox.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Command line utility for converting between and applying effects to various audio files formats"
 TERMUX_PKG_VERSION=14.4.2
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_REVISION=9
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/sox/sox/${TERMUX_PKG_VERSION}/sox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=81a6956d4330e75b5827316e44ae381e6f1e8928003c6aa45896da9041ea149c
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
