60cc86723e38bb6556888b6ade68145bdbb050c5
Henrik Grimler <Henrik@Grimler.se>
2018-07-29 11:06:49 +0200

    gnupg2: rename to gnupg and split out gpgv
    Change all gnupg2 dependencies to gnupg.

--- packages/gnupg/build.sh
+++ packages/gnupg/build.sh
@@ -5,6 +5,8 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=6278eaabffa1ebc9fa2ceb3dc53eea9a1505ab02a668a86dd6fec06951af2164
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libassuan,libbz2,libgcrypt,libksba,libsqlite,libnpth,readline,pinentry,libgpg-error"
+TERMUX_PKG_CONFLICTS="gnupg2 (<< 2.2.9-1)"
+TERMUX_PKG_REPLACES="gnupg2 (<< 2.2.9-1)"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-ldap
 --enable-sqlite
@@ -15,4 +17,9 @@ TERMUX_PKG_RM_AFTER_INSTALL="share/gnupg/help.*.txt share/man/man1/gpg-zip.1 sha
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -Ddn_skipname=__dn_skipname"
+}
+
+termux_step_post_make_install() {
+	cd $TERMUX_PREFIX/bin
+	ln -sf gpg gpg2
 }
