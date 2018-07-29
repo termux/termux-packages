708648a2349dc1c4b2024c9d85472f3260eb767c
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-29 22:35:25 +0200

    gpgv: Add dependencies

--- packages/gnupg/gpgv.subpackage.sh
+++ packages/gnupg/gpgv.subpackage.sh
@@ -2,3 +2,4 @@ TERMUX_SUBPKG_INCLUDE="bin/gpgv share/man/man1/gpgv.1"
 TERMUX_SUBPKG_DESCRIPTION="GNU privacy guard - signature verification tool"
 # Conflict with older gnupg package before splitting out gpgv:
 TERMUX_SUBPKG_CONFLICTS="gnupg (<= 1.4.21)"
+TERMUX_SUBPKG_DEPENDS="libbz2, libgcrypt, libgpg-error"
