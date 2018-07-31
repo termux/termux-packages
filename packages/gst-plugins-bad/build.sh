96089dac9e388651404c5aa99d97db1c55370205
Fredrik Fornwall <fredrik@fornwall.net>
2018-07-31 22:20:17 +0200

    gstreamer: Update from 1.14.1 to 1.14.2

--- packages/gst-plugins-bad/build.sh
+++ packages/gst-plugins-bad/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Bad Plug-ins"
-TERMUX_PKG_VERSION=1.14.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=2a77c6908032aafdf2cd2e5823fec948f16a25c2d1497a953828d762dc20d61a
+TERMUX_PKG_VERSION=1.14.2
+TERMUX_PKG_SHA256=34fab7da70994465a64468330b2168a4a0ed90a7de7e4c499b6d127c6c1b1eaf
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="gst-plugins-base, libx264, libbz2, libpng, librsvg, libcurl, libx265, libsndfile, openjpeg, openal-soft, littlecms"
--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
-TERMUX_PKG_VERSION=1.14.1
-TERMUX_PKG_SHA256=34ec062ddb766a32377532e039781f4a16fbc3e8b449e642605bacab26a99172
+TERMUX_PKG_VERSION=1.14.2
+TERMUX_PKG_SHA256=c0575e2811860bfff59b865b8d125153859a01f0615fa41e279b64d88d25caad
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="gst-plugins-base,libcaca,libsoup,libjpeg-turbo,libpng,libflac,libbz2,libvpx,libpulseaudio,libmp3lame"
