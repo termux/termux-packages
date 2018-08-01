b4e503afb616615d700a3dd9728ddf4dd83e25d3
tomty89 <tom.ty89@gmail.com>
2018-08-02 04:23:56 +0800

    mpd: avoid seccomp-filtered ioprio_set (#2695)
    Causes SIGSYS (SYS_SECCOMP) on arm. mpd will fail to start if no
    database exists.

--- packages/mpd/build.sh
+++ packages/mpd/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.musicpd.org
 TERMUX_PKG_DESCRIPTION="Music player daemon"
 TERMUX_PKG_VERSION=0.20.20
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=d804b47a981b0398f119e9f398775706422c3d4d887dd7c0f5460904df89bbf2
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/MPD/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libcurl, libid3tag, libopus, libevent, fftw, libpulseaudio, libmpdclient, boost, openal-soft, libvorbis, libsqlite, ffmpeg, libmp3lame, libbz2"
