73817beeb863b7e7cc3827c63131a26db9b44485
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-01 22:48:44 +0200

    megatools: Update from 1.10.1 to 1.10.2

--- packages/megatools/build.sh
+++ packages/megatools/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/megous/megatools
 TERMUX_PKG_DESCRIPTION="Open-source command line tools and C library (libmega) for accessing Mega.co.nz cloud storage"
-TERMUX_PKG_VERSION=1.10.1
-TERMUX_PKG_SHA256=87f703423e8ef470e6bb7af47f2450d8d1ac7ec3fcff463dd4898d64950b867e
+TERMUX_PKG_VERSION=1.10.2
+TERMUX_PKG_SHA256=48468536492bfecd8b10a42e7608129eba9922e03cbce0a11dd9e338e2a0632d
 TERMUX_PKG_SRCURL=https://github.com/megous/megatools/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, libgmp, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-fuse --enable-docs-build"
