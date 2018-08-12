f43280bc810003d5c73c6a94e52ae2edbe7c54d6
Henrik Grimler <Henrik@Grimler.se>
2018-08-12 14:41:23 +0200

    transmission: update url

--- packages/transmission/build.sh
+++ packages/transmission/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://www.transmissionbt.com/
 TERMUX_PKG_DESCRIPTION="Easy, lean and powerful BitTorrent client"
 TERMUX_PKG_DEPENDS="libevent, openssl, libcurl"
 TERMUX_PKG_VERSION=2.94
-TERMUX_PKG_SRCURL=https://github.com/transmission/transmission/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=440c2fd0f89b1ab59d8a4b79ecd7bffd61bc000e36fb5b6c8e88142a4fadbb1f
+TERMUX_PKG_SRCURL=https://github.com/transmission/transmission-releases/raw/master/transmission-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=35442cc849f91f8df982c3d0d479d650c6ca19310a994eccdaa79a4af3916b7d
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk --enable-lightweight --cache-file=termux_configure.cache"
 
 termux_step_pre_configure() {
