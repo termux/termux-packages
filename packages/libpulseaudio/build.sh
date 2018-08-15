c23221e7abca00c621769d48b3c83d88cc3c2633
Tom Yan <tom.ty89@gmail.com>
2018-08-15 09:54:04 +0800

    libpulseaudio: add module-aaudio-sink
    Add a sink module that makes use of the AAudio API, which is
    available in Oreo or later.
    
    The sink will have its sample rate and latency configured based on
    the info we got with the API. They will also be updated accordingly
    when the output device is changed.

--- packages/libpulseaudio/build.sh
+++ packages/libpulseaudio/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/PulseAudio
 TERMUX_PKG_DESCRIPTION="A featureful, general-purpose sound server - shared libraries"
 TERMUX_PKG_VERSION=12.2
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SHA256=809668ffc296043779c984f53461c2b3987a45b7a25eb2f0a1d11d9f23ba4055
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libltdl, libsndfile, libandroid-glob, libsoxr"
@@ -23,6 +23,8 @@ TERMUX_PKG_CONFFILES="etc/pulse/client.conf etc/pulse/daemon.conf etc/pulse/defa
 termux_step_pre_configure () {
 	mkdir $TERMUX_PKG_SRCDIR/src/modules/sles
 	cp $TERMUX_PKG_BUILDER_DIR/module-sles-sink.c $TERMUX_PKG_SRCDIR/src/modules/sles
+	mkdir $TERMUX_PKG_SRCDIR/src/modules/aaudio
+	cp $TERMUX_PKG_BUILDER_DIR/module-aaudio-sink.c $TERMUX_PKG_SRCDIR/src/modules/aaudio
 	intltoolize --automake --copy --force
 	LDFLAGS+=" -llog -landroid-glob"
 }
@@ -39,6 +41,7 @@ termux_step_post_make_install () {
 	sed -i $TERMUX_PREFIX/etc/pulse/default.pa \
 		-e '/^load-module module-detect$/s/^/#/'
 	echo "load-module module-sles-sink" >> $TERMUX_PREFIX/etc/pulse/default.pa
+	echo "#load-module module-aaudio-sink" >> $TERMUX_PREFIX/etc/pulse/default.pa
 
 	if [ "$TERMUX_ARCH_BITS" = 32 ]; then
 		SYSTEM_LIB=lib
