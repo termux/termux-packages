94e51f2616b2190e73a4ebf15230c3a46889c0a5
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-16 12:01:42 +0200

    squid: Update from 3.5.28 to 4.2

--- packages/squid/build.sh
+++ packages/squid/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://www.squid-cache.org
 TERMUX_PKG_DESCRIPTION="Full-featured Web proxy cache server"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION=3.5.28
-TERMUX_PKG_SHA256=fd41b622e65c661ada9a98b0338c59e6f2d2ffdb367fe5c8c7212c535e298ed8
-TERMUX_PKG_SRCURL=http://www.squid-cache.org/Versions/v3/3.5/squid-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION=4.2
+TERMUX_PKG_SHA256=994807762c59991b32449caf29418fd0ec9d2329746b18eb19bd930b6806d208
+TERMUX_PKG_SRCURL=http://www.squid-cache.org/Versions/v4/squid-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_DEPENDS="libcrypt, openssl, libnettle, libltdl"
 # disk-io requires msgctl and store-io requires disk-io
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_c_bigendian=no
 ac_cv_lib_sasl2_sasl_errstring=no
 ac_cv_dbopen_libdb=no
 squid_cv_gnu_atomics=yes
