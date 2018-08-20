6ceb1397cfc271a2527f680e8554866d0c6cb448
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-20 21:08:09 +0200

    cmark: Update from 0.28.3 to 0.28.3.gfm.14

--- packages/cmark/build.sh
+++ packages/cmark/build.sh
@@ -1,6 +1,6 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/jgm/cmark
+TERMUX_PKG_HOMEPAGE=https://github.com/github/cmark
 TERMUX_PKG_DESCRIPTION="CommonMark parsing and rendering program"
-TERMUX_PKG_VERSION=0.28.3
-TERMUX_PKG_SHA256=acc98685d3c1b515ff787ac7c994188dadaf28a2d700c10c1221da4199bae1fc
-TERMUX_PKG_SRCURL=https://github.com/jgm/cmark/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=0.28.3.gfm.14
+TERMUX_PKG_SHA256=c79e12309ff29ed36fb3e5166cb0e77331e708e087e1cc5bee01cf5127a9d2ac
+TERMUX_PKG_SRCURL=https://github.com/github/cmark/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib"
