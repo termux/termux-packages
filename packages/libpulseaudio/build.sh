e6e160acb7c86d0e9be58e96c2a51559c34cad91
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-21 00:25:06 +0200

    libpulseaudio: Build against api level 21
    Building against later api levels causes problems such as
    
    CANNOT LINK EXECUTABLE: could not load library "libpulse.so" [...];
    caused by could not load library "libpulsecommon-12.2.so" needed by
    "libpulse.so"; caused by cannot locate symbol "__register_atfork"
    referenced by "libpulsecommon-12.2.so"...

--- packages/libpulseaudio/build.sh
+++ packages/libpulseaudio/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/PulseAudio
 TERMUX_PKG_DESCRIPTION="A featureful, general-purpose sound server - shared libraries"
 TERMUX_PKG_VERSION=12.2
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SHA256=809668ffc296043779c984f53461c2b3987a45b7a25eb2f0a1d11d9f23ba4055
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libltdl, libsndfile, libandroid-glob, libsoxr"
@@ -20,15 +20,27 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-neon-opt
 ax_cv_PTHREAD_PRIO_INHERIT=no
 ac_cv_func_posix_madvise=no"
 TERMUX_PKG_CONFFILES="etc/pulse/client.conf etc/pulse/daemon.conf etc/pulse/default.pa etc/pulse/system.pa"
-TERMUX_PKG_API_LEVEL=26
 
 termux_step_pre_configure () {
+	# Our aaudio sink module needs libaaudio.so from a later android api version:
+	local _NDK_ARCHNAME=$TERMUX_ARCH
+	if [ "$TERMUX_ARCH" = "aarch64" ]; then
+		_NDK_ARCHNAME=arm64
+	elif [ "$TERMUX_ARCH" = "i686" ]; then
+		_NDK_ARCHNAME=x86
+	fi
+	mkdir $TERMUX_PKG_TMPDIR/libaaudio
+	cp $NDK/platforms/android-26/arch-$_NDK_ARCHNAME/usr/lib*/libaaudio.so \
+		$TERMUX_PKG_TMPDIR/libaaudio/
+	LDFLAGS+=" -L$TERMUX_PKG_TMPDIR/libaaudio/"
+
 	mkdir $TERMUX_PKG_SRCDIR/src/modules/sles
 	cp $TERMUX_PKG_BUILDER_DIR/module-sles-sink.c $TERMUX_PKG_SRCDIR/src/modules/sles
 	mkdir $TERMUX_PKG_SRCDIR/src/modules/aaudio
 	cp $TERMUX_PKG_BUILDER_DIR/module-aaudio-sink.c $TERMUX_PKG_SRCDIR/src/modules/aaudio
+
 	intltoolize --automake --copy --force
-	CFLAGS+=" -D__ANDROID_API__=21"
+
 	LDFLAGS+=" -llog -landroid-glob"
 }
 
