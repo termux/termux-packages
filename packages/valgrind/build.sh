113fc60dfb2d7459a8295510dc6171d2786f9f94
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-20 21:25:43 +0200

    valgrind: Bump revision after clang switch

--- packages/valgrind/build.sh
+++ packages/valgrind/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://valgrind.org/
 TERMUX_PKG_DESCRIPTION="Instrumentation framework for building dynamic analysis tools"
 TERMUX_PKG_VERSION=3.13.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=ftp://sourceware.org/pub/valgrind/valgrind-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=d76680ef03f00cd5e970bbdcd4e57fb1f6df7d2e2c071635ef2be74790190c3b
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-tmpdir=$TERMUX_PREFIX/tmp"
@@ -15,7 +15,7 @@ TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" == "aarch64" ]; then
 		cp $TERMUX_PKG_BUILDER_DIR/aarch64-setjmp.S $TERMUX_PKG_SRCDIR
-		autoreconf -if 
+		autoreconf -if
 	fi
 	if [ "$TERMUX_ARCH" == "arm" ]; then
 		# valgrind doesn't like arm; armv7 works, though.
