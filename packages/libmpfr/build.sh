6b2cea585ec35aac3c4bd261620790c4c798ffe3
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-20 15:04:40 +0200

    libmpfr: Work around clang issue

--- packages/libmpfr/build.sh
+++ packages/libmpfr/build.sh
@@ -5,7 +5,10 @@ TERMUX_PKG_VERSION=4.0.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/mpfr/mpfr-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=67874a60826303ee2fb6affc6dc0ddd3e749e9bfcb4c8655e3953d0458a6e16e
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_header_locale_h=no"
-if [ "$TERMUX_ARCH" = i686 ]; then
-	# Avoid clang bug with NDK r16:
-	TERMUX_PKG_CLANG=no
-fi
+
+termux_step_pre_configure() {
+	if [ "$TERMUX_ARCH" = i686 ]; then
+		# Fix clang internal error (on clang in ndk r17):
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-float128"
+	fi
+}
