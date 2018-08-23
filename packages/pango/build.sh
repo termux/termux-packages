9a5a4e3357abccabd5818e32b6bdb4b93d56ec64
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-23 14:45:26 +0200

    pango: Update from 1.42.3 to 1.42.4

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -1,7 +1,7 @@
-TERMUX_PKG_HOMEPAGE=http://www.pango.org/
+TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
-TERMUX_PKG_VERSION=1.42.3
-TERMUX_PKG_SHA256=fb3d875305d5143f02cde5c72fec3903e60dc35844759dc14b2df4955b5dde3a
+TERMUX_PKG_VERSION=1.42.4
+TERMUX_PKG_SHA256=1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="fontconfig,fribidi,glib,harfbuzz,libcairo"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libcairo-dev, libpixman-dev, fontconfig-dev, harfbuzz-dev"
