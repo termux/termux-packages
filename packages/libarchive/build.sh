191b47653e410813c5845fab8a65c616277721f7
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-25 16:54:53 +0200

    libarchive: Configure with --without-lz4
    This prevents opportunistic linkage against liblz4.so.

--- packages/libarchive/build.sh
+++ packages/libarchive/build.sh
@@ -4,6 +4,8 @@ TERMUX_PKG_VERSION=3.3.2
 TERMUX_PKG_SHA256=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
 TERMUX_PKG_SRCURL=https://www.libarchive.org/downloads/libarchive-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libbz2, liblzma, libxml2, openssl"
-
 # --without-nettle to use openssl instead:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-nettle"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--without-nettle
+--without-lz4
+"
