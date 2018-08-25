88d13a57716457bd799f78ea6894ae7c3456bbf0
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-25 23:37:40 +0200

    openssh: Update from 7.7p1 to 7.8p1

--- packages/openssh/build.sh
+++ packages/openssh/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.openssh.com/
 TERMUX_PKG_DESCRIPTION="Secure shell for logging into a remote machine"
-TERMUX_PKG_VERSION=7.7p1
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SHA256=d73be7e684e99efcd024be15a30bffcbe41b012b2f7b3c9084aed621775e6b8f
+TERMUX_PKG_VERSION=7.8p1
+TERMUX_PKG_SHA256=1a484bb15152c183bb2514e112aa30dd34138c3cfb032eee5490a66c507144ca
 TERMUX_PKG_SRCURL=https://fastly.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support, ldns, openssl, libedit, libutil"
 TERMUX_PKG_CONFLICTS="dropbear"
