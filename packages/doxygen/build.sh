6604fde6eb14f67241a2f0c27398bd7f0fc77bb1
Fredrik Fornwall <fredrik@fornwall.net>
2018-08-27 12:23:58 +0200

    doxygen: Specify path to flex and bison
    This fixes a build issue if building after flex or bison has already
    been built, so the build tries to use the cross-compiled binary.

--- packages/doxygen/build.sh
+++ packages/doxygen/build.sh
@@ -4,7 +4,13 @@ TERMUX_PKG_VERSION=1.8.14
 TERMUX_PKG_MAINTAINER="lokesh @hax4us"
 TERMUX_PKG_SHA256=d1757e02755ef6f56fd45f1f4398598b920381948d6fcfa58f5ca6aa56f59d4d
 TERMUX_PKG_SRCURL=http://ftp.stack.nl/pub/doxygen/doxygen-${TERMUX_PKG_VERSION}.src.tar.gz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DCMAKE_BUILD_TYPE=Release -Dbuild_parse=yes -Dbuild_xmlparser=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBISON_EXECUTABLE=`which bison`
+-Dbuild_parse=yes
+-Dbuild_xmlparser=yes
+-DCMAKE_BUILD_TYPE=Release
+-DFLEX_EXECUTABLE=`which flex`
+"
 termux_step_post_make_install () {
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp $TERMUX_PKG_SRCDIR/doc/doxygen.1 $TERMUX_PREFIX/share/man/man1
