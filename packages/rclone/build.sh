521ed8fd95d9bb0a16dbf14a85a73b86891c4aa3
araczkowski <araczkowski@gmail.com>
2018-09-03 19:10:02 +0200

    update from 1.42 to 1.43

--- packages/rclone/build.sh
+++ packages/rclone/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://rclone.org/
 TERMUX_PKG_DESCRIPTION="rsync for cloud storage"
-TERMUX_PKG_VERSION=1.42
-TERMUX_PKG_SHA256=193aba6db91ff565bbeb7beb0e07773e77cc9c25285d0966a9406589a87bdb44
+TERMUX_PKG_VERSION=1.43
+TERMUX_PKG_SHA256=d30527b00cecb4e5e7188dddb78e5cec62d67cf2422dab82190db58512b5a4e3
 TERMUX_PKG_SRCURL=https://github.com/ncw/rclone/releases/download/v${TERMUX_PKG_VERSION}/rclone-v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_make_install() {
