f27e72730dcc85039dbb0f5024d9d5f1747b76d6
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-08 01:55:54 +0200

    tinyproxy: Update from 1.8.4 to 1.10.0

--- packages/tinyproxy/build.sh
+++ packages/tinyproxy/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://tinyproxy.github.io/
 TERMUX_PKG_DESCRIPTION="Light-weight HTTP proxy daemon for POSIX operating systems"
-TERMUX_PKG_VERSION=1.8.4
-TERMUX_PKG_SHA256=a41f4ddf0243fc517469cf444c8400e1d2edc909794acda7839f1d644e8a5000
+TERMUX_PKG_VERSION=1.10.0
+TERMUX_PKG_SHA256=59be87689c415ba0d9c9bc6babbdd3df3b372d60b21e526b118d722dbc995682
 TERMUX_PKG_SRCURL=https://github.com/tinyproxy/tinyproxy/releases/download/${TERMUX_PKG_VERSION}/tinyproxy-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-regexcheck"
 
