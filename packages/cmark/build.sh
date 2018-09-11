0eb7142a75df6d93a9f380e2fe7e01c3538c17ad
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-11 21:50:47 +0200

    cmark: Update from 0.28.3.gfm.15 to 0.28.3.gfm.16
    Also setup cmark as symlink to cmark-gfm.

--- packages/cmark/build.sh
+++ packages/cmark/build.sh
@@ -1,6 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/github/cmark
 TERMUX_PKG_DESCRIPTION="CommonMark parsing and rendering program"
-TERMUX_PKG_VERSION=0.28.3.gfm.15
-TERMUX_PKG_SHA256=9e43c0b5dbd6678059fb7eeb5f57520b480812083b91567034c9e2890cc32f21
+TERMUX_PKG_VERSION=0.28.3.gfm.16
+TERMUX_PKG_SHA256=5594e42f13e529e2530bcc8e4681832e888714d5c89f4eaf3adefc731e21a3e2
 TERMUX_PKG_SRCURL=https://github.com/github/cmark/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib"
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="lib/cmake-gfm-extensions"
+
+termux_step_post_make_install() {
+    cd $TERMUX_PREFIX/bin
+    ln -f -s cmark-gfm cmark
+
+    cd $TERMUX_PREFIX/share/man/man1
+    ln -f -s cmark-gfm.1 cmark.1
+}
