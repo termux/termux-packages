ab882503e15f7e900c8f5c825bb0a2e5a4c1e8bf
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-09-11 22:13:33 +0300

    apt: enable X11 repository in sources.list

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_DEPENDS="libcurl, liblzma, dpkg, gpgv, libc++, termux-exec"
 TERMUX_PKG_VERSION=1.4.8
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SHA256=767ad7d6efb64cde52faececb7d3c0bf49800b9fe06f3a5b0132ab4c01a5b8f8
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -34,7 +34,15 @@ lib/libapt-inst.so
 "
 
 termux_step_post_make_install() {
-	printf "# The main termux repository:\ndeb https://termux.net stable main\n" > $TERMUX_PREFIX/etc/apt/sources.list
+	{
+		echo "# The main termux repository:"
+		echo "deb https://termux.net stable main"
+		echo
+		echo "# The X11 termux repository:"
+		echo "deb https://termux-x11.ml x11 main"
+	} > $TERMUX_PREFIX/etc/apt/sources.list
+
+
 	cp $TERMUX_PKG_BUILDER_DIR/trusted.gpg $TERMUX_PREFIX/etc/apt/
 	rm $TERMUX_PREFIX/include/apt-pkg -r
 
