d03084465377478849c487256e1a725ea43dc04a
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-12 20:04:04 +0300

    the-powder-toy: use Lua 5.2.x

--- x11-packages/the-powder-toy/build.sh
+++ x11-packages/the-powder-toy/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=http://powdertoy.co.uk/
 TERMUX_PKG_DESCRIPTION="The Powder Toy is a free physics sandbox game"
 TERMUX_PKG_VERSION=93.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/ThePowderToy/The-Powder-Toy/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6c5f5914e4ae0ac8500c7203ec69517a2fd4380dece6b33247ed719aa12e074e
-TERMUX_PKG_DEPENDS="fftw, libbz2, libc++, liblua51, libsdl, libx11"
+TERMUX_PKG_DEPENDS="fftw, libbz2, libc++, liblua52, libsdl, libx11"
 TERMUX_PKG_FOLDERNAME=The-Powder-Toy-${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -15,7 +15,7 @@ termux_step_make()
     export CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include"
     export CXXFLAGS="${CFLAGS}"
     export LDFLAGS="-L${TERMUX_PREFIX}/lib"
-    scons -j4 --lin --64bit --no-sse
+    scons -j4 --lin --64bit --no-sse --lua52
 }
 
 termux_step_make_install()
