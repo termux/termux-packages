c6979c6a7e602870393ec39157fa309e73d8fb35
Henrik Grimler <Henrik@Grimler.se>
2018-09-13 22:11:30 +0200

    openmpi: update to 3.1.2

--- packages/openmpi/build.sh
+++ packages/openmpi/build.sh
@@ -1,17 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://www.open-mpi.org
 TERMUX_PKG_DESCRIPTION="The Open MPI Project is an open source Message Passing Interface implementation that is developed and maintained by a consortium of academic, research, and industry partners"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION="2.1.1"
-TERMUX_PKG_SRCURL=https://www.open-mpi.org/software/ompi/v${TERMUX_PKG_VERSION:0:3}/downloads/openmpi-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=afe4bef3c4378bc76eea96c623d5aa4c1c98b9e057d281c646e68869292a77dc
+TERMUX_PKG_VERSION=3.1.2
+TERMUX_PKG_SRCURL=https://download.open-mpi.org/release/open-mpi/v${TERMUX_PKG_VERSION:0:3}/openmpi-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=363c8a1258d12df28657e9b13bfe41e30fc129a4cfb89fcf53ff68bbe1bd65b3
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-x
 ac_cv_header_ifaddrs_h=no
-ac_cv_header_stdint_h=no
-ac_cv_header_stdlib_h=no
---disable-dlopen"
+--disable-dlopen
+--disable-mpi-fortran
+"
 
 termux_step_pre_configure () {
 	# rindex is an obsolete version of strrchr which is not available in Android:
-	CFLAGS+=" -Drindex=strrchr"
+	CFLAGS+=" -Drindex=strrchr -Dbcmp=memcmp"
 	LDFLAGS+=" -llog"
 }
