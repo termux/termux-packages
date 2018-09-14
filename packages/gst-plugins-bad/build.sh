78e7df0a4fece5d84ef572d9898f846b066f0328
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-14 20:57:02 +0200

    gst-plugins-bad: Depend on libcurl, disable webp

--- packages/gst-plugins-bad/build.sh
+++ packages/gst-plugins-bad/build.sh
@@ -5,13 +5,14 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=34fab7da70994465a64468330b2168a4a0ed90a7de7e4c499b6d127c6c1b1eaf
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="gst-plugins-base, libx264, libbz2, libpng, librsvg, libcurl, libx265, libsndfile, openjpeg, openal-soft, littlecms"
+TERMUX_PKG_DEPENDS="gst-plugins-base, libbz2, libcurl, libpng, librsvg, libssh2, libsndfile, libx264, libx265, littlecms, openal-soft, openjpeg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-tests
---disable-examples
 --disable-android_media
---disable-zbar
+--disable-examples
 --disable-rtmp
+--disable-tests
+--disable-zbar
+--disable-webp
 --with-hls-crypto=openssl
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/"
