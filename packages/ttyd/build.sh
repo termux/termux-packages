3c17329d21cf7d2d59f791ea38e1b581566d59dc
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-15 01:47:10 +0200

    ttyd: Add package at version 1.4.2

---
+++ packages/ttyd/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://tsl0922.github.io/ttyd/
+TERMUX_PKG_DESCRIPTION="Command-line tool for sharing terminal over the web"
+TERMUX_PKG_VERSION=1.4.2
+TERMUX_PKG_SHA256=ff1a66b418df6cd741868a8ea84f69cd63f15e52e3fa117641ec57d3c37a1315
+TERMUX_PKG_SRCURL=https://github.com/tsl0922/ttyd/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_DEPENDS="json-c, libwebsockets, libutil"
