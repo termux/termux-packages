552362a78a64e2b31207804e375b976244a9279d
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-15 01:46:16 +0200

    Some http -> https updates

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -1,4 +1,4 @@
-TERMUX_PKG_HOMEPAGE=http://libjpeg-turbo.virtualgl.org/
+TERMUX_PKG_HOMEPAGE=https://libjpeg-turbo.virtualgl.org
 TERMUX_PKG_DESCRIPTION="Library for reading and writing JPEG image files"
 TERMUX_PKG_VERSION=1.5.3
 TERMUX_PKG_SHA256=b24890e2bb46e12e72a79f7e965f409f4e16466d00e1dd15d93d73ee6b592523
