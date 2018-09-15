d4fd3e0d6d1293d94bd4e2358615714a5f8459f3
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-15 22:10:26 +0300

    qt5-base: manually build tools for custom spec (linux-termux-clang)
    Seems that tools like 'qmake' are build only for host (x86_64)
    architecture.

--- x11-packages/qt5-base/build.sh
+++ x11-packages/qt5-base/build.sh
@@ -8,8 +8,6 @@ TERMUX_PKG_SHA256=39602cb08f9c96867910c375d783eed00fc4a244bffaa93b801225d17950fb
 TERMUX_PKG_DEPENDS="libsqlite, libjpeg-turbo, libpng, pcre2, openssl, libandroid-support, freetype, harfbuzz, libwebp, fontconfig, libopus, libevent, jsoncpp"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/"
-
 termux_step_pre_configure () {
     sed -e "s|@TERMUX_HOST_PLATFORM@|$TERMUX_HOST_PLATFORM|g" \
         -e "s|@CFLAGS@|$CPPFLAGS $CFLAGS|" \
@@ -28,7 +26,7 @@ termux_step_configure () {
         -datadir "${TERMUX_PREFIX}/share/qt" \
         -sysconfdir "${TERMUX_PREFIX}/etc/xdg" \
         -examplesdir "${TERMUX_PREFIX}/share/doc/qt/examples" \
-        -xplatform linux-termux-clang \
+        -xplatform "linux-termux-clang" \
         -plugindir "$TERMUX_PREFIX/libexec/qt" \
         -opensource \
         -confirm-license \
@@ -36,7 +34,6 @@ termux_step_configure () {
         -optimized-qmake \
         -nomake examples \
         -gui \
-        -no-widgets \
         -no-dbus \
         -no-accessibility \
         -no-glib \
@@ -78,4 +75,51 @@ termux_step_make() {
 
 termux_step_make_install() {
     make -C qtbase install
+
+    cd "${TERMUX_PKG_SRCDIR}/qtbase/src/tools/bootstrap" && {
+        make clean
+
+        "${TERMUX_PKG_SRCDIR}/qtbase/bin/qmake" \
+            -spec "${TERMUX_PKG_SRCDIR}/qtbase/mkspecs/linux-termux-clang"
+
+        make -j "${TERMUX_MAKE_PROCESSES}"
+    }
+
+    for i in moc qlalr qvkgen rcc uic; do
+        cd "${TERMUX_PKG_SRCDIR}/qtbase/src/tools/${i}" && {
+            make clean
+
+            "${TERMUX_PKG_SRCDIR}/qtbase/bin/qmake" \
+                -spec "${TERMUX_PKG_SRCDIR}/qtbase/mkspecs/linux-termux-clang"
+
+            sed \
+                -i 's@-lpthread@@g' \
+                "${TERMUX_PKG_SRCDIR}/qtbase/src/tools/${i}/Makefile"
+
+            make -j "${TERMUX_MAKE_PROCESSES}"
+
+            install \
+                -Dm700 "${TERMUX_PKG_BUILDDIR}/qtbase/bin/${i}" \
+                "${TERMUX_PREFIX}/bin/${i}"
+        }
+    done
+    unset i
+
+    cd "${TERMUX_PKG_SRCDIR}/qtbase/qmake" && {
+        make clean
+
+        make \
+            -j "${TERMUX_MAKE_PROCESSES}" \
+            AR="${AR} cqs" \
+            CC="${CC}" \
+            CXX="${CXX}" \
+            LINK="${CXX}" \
+            STRIP="${STRIP}" \
+            QMAKESPEC="${TERMUX_PKG_SRCDIR}/qtbase/mkspecs/linux-termux-clang" \
+            QMAKE_LFLAGS="${TERMUX_PREFIX}/lib/libc++_shared.so"
+
+        install \
+            -Dm700 "${TERMUX_PKG_BUILDDIR}/qtbase/bin/qmake" \
+            "${TERMUX_PREFIX}/bin/qmake"
+    }
 }
