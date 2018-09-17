67d96765b7999dc1174cfaa3a56b94f77db6afdd
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-17 14:27:14 +0300

    enable package: qemu-system

---
+++ x11-packages/qemu-system/qemu-system-aarch64.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/qemu-system-aarch64
+share/man/man1/qemu-system-aarch64.1
+"
+
+TERMUX_SUBPKG_DEPENDS="qemu-system"
+TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (AArch64 target)"
