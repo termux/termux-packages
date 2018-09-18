b0a210901b5ea8c9b0b8bdd60a22cfe518945972
its-pointless <bored@goat.si>
2018-09-18 11:06:23 +1000

    update openal-soft enable audio on toxic

--- packages/c-toxcore/build.sh
+++ packages/c-toxcore/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Backend library for the Tox protocol"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.2.7
 TERMUX_PKG_SHA256=bef6dbb06d921af58c6a2d7629e6f8eeafa1e541d379e4ff2ccb4ff5d3499cd1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/TokTok/toxcore/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libsodium"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -15,6 +16,6 @@ termux_step_configure() {
 			-DCMAKE_INSTALL_LIBDIR="${TERMUX_PREFIX}/lib" \
 			-DBOOTSTRAP_DAEMON=off \
 			-DDHT_BOOTSTRAP=off \
-			-DBUILD_TOXAV=off \
+			-DBUILD_TOXAV=on \
 			-DBUILD_AV_TEST=off
 }
--- packages/toxic/build.sh
+++ packages/toxic/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://github.com/JFreegman/toxic
 TERMUX_PKG_DESCRIPTION="A command line client for Tox"
 TERMUX_PKG_VERSION=0.8.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/JFreegman/toxic/archive/v${TERMUX_PKG_VERSION}/toxic-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=53bdbed3d72d000f9e43b8823523c589540b811bc237ed6f3b4ddf0fe6f9a6b7
-TERMUX_PKG_DEPENDS="c-toxcore, libconfig, libcurl, libqrencode, ncurses"
+TERMUX_PKG_DEPENDS="c-toxcore, libconfig, libcurl, libqrencode, ncurses, libfreealut"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
@@ -20,4 +20,10 @@ termux_step_make() {
 
 termux_step_make_install() {
 	make PREFIX="${TERMUX_PREFIX}" install
+	mv $TERMUX_PREFIX/bin/toxic $TERMUX_PREFIX/libexec/toxic
+	local SYSTEM_LIBFOLDER=lib64
+	if [ $TERMUX_ARCH_BITS = 32 ]; then SYSTEM_LIBFOLDER=lib; fi
+	echo "#!/bin/sh" > $TERMUX_PREFIX/bin/toxic
+	echo "LD_LIBRARY_PATH=/system/$SYSTEM_LIBFOLDER:/system/vendor/$SYSTEM_LIBFOLDER:$TERMUX_PREFIX/lib exec $TERMUX_PREFIX/libexec/toxic \"\$@\"" >> $TERMUX_PREFIX/bin/toxic
+	chmod +x $TERMUX_PREFIX/bin/toxic
 }
