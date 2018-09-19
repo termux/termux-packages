6d8e9a7939ba63272cdb07bb981c6f6fc8b1b4eb
Henrik Grimler <Henrik@Grimler.se>
2018-09-19 09:42:23 +0200

    arm: don't build with -fno-integrated-as
    Fixes ruby segfault (without the need for -O1).

--- packages/ruby/build.sh
+++ packages/ruby/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.ruby-lang.org/
 TERMUX_PKG_DESCRIPTION="Dynamic programming language with a focus on simplicity and productivity"
 _MAJOR_VERSION=2.5
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SHA256=886ac5eed41e3b5fc699be837b0087a6a5a3d10f464087560d2d21b3e71b754d
 TERMUX_PKG_SRCURL=https://cache.ruby-lang.org/pub/ruby/${_MAJOR_VERSION}/ruby-${TERMUX_PKG_VERSION}.tar.xz
 # libbffi is used by the fiddle extension module:
@@ -22,10 +22,6 @@ termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH_BITS" = 32 ]; then
 		# process.c:function timetick2integer: error: undefined reference to '__mulodi4'
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" rb_cv_builtin___builtin_mul_overflow=no"
-		if [ "$TERMUX_ARCH" = arm ]; then
-			# Workaround for segmentation fault:
-			export CFLAGS="${CFLAGS/-Os/} -O1"
-		fi
 	fi
 }
 
