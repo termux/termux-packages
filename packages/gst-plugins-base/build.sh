194af6bb6ec71d9391f6358c375c67963aba67e2
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-20 00:42:28 +0200

    gstreamer: Update from 1.14.2 to 1.14.3

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
-TERMUX_PKG_VERSION=1.14.2
-TERMUX_PKG_SHA256=a4b7e80ba869f599307449b17c9e00b5d1e94d3ba1d8a1a386b8770b2ef01c7c
+TERMUX_PKG_VERSION=1.14.3
+TERMUX_PKG_SHA256=f0b319c36be0ffc2a00380c77ba269cdf04e2b39bbc49d30b641fc35aa0b7952
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="gstreamer, libjpeg-turbo, libopus, libpng, libvorbis"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
