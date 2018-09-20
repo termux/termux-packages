5304c8302f00642301e0ccc0c6b75b3f727b3c8c
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-20 22:28:39 +0200

    libpixman: Fix arm build with -fno-integrated-as

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -4,3 +4,9 @@ TERMUX_PKG_VERSION=0.34.0
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=21b6b249b51c6800dc9553b65106e1e37d0e25df942c90531d4c3997aa20a88e
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
+
+termux_step_pre_configure () {
+	if [ $TERMUX_ARCH = arm ]; then
+		CFLAGS+=" -fno-integrated-as"
+	fi
+}
