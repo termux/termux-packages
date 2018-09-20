7c5510e53f6ba1fe7c472e9427e194671168944c
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-21 01:01:56 +0200

    picolisp: Update from 18.7.18 to 18.9.18

--- packages/picolisp/build.sh
+++ packages/picolisp/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://picolisp.com
 TERMUX_PKG_DESCRIPTION="Lisp interpreter and application server framework"
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
-TERMUX_PKG_VERSION=18.7.18
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=b88ab3c65d014b653be2aac90b36bb303b6f888954f994cb98eb2a44852f19df
+TERMUX_PKG_VERSION=18.9.18
+TERMUX_PKG_SHA256=aa14a35b7d071c068545d835db74c77538b115b37316dd1b331bd0dee2af0f24
 # We use our bintray mirror since old version snapshots are not kept on main site.
 TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/picolisp_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
