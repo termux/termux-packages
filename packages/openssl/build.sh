cadecb5ef3a2ad8ec9ff27ebad72672fb2990d03
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-09-21 00:55:06 +0300

    openssl: force install file 'openssl.cnf'
    Fixes https://github.com/termux/termux-packages/issues/2865

--- packages/openssl/build.sh
+++ packages/openssl/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.openssl.org/
 TERMUX_PKG_DESCRIPTION="Library implementing the SSL and TLS protocols as well as general purpose cryptography functions"
 TERMUX_PKG_DEPENDS="ca-certificates"
 TERMUX_PKG_VERSION=1.1.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=2836875a0f89c03d0fdf483941512613a50cfb421d6fd94b9f41d7279d586a3d
 TERMUX_PKG_SRCURL=https://www.openssl.org/source/openssl-${TERMUX_PKG_VERSION/\~/-}.tar.gz
 TERMUX_PKG_RM_AFTER_INSTALL="bin/c_rehash etc/ssl/misc"
+TERMUX_PKG_CONFFILES="etc/tls/openssl.cnf"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_CONFLICTS="libcurl (<< 7.61.0-1)"
 
@@ -43,4 +44,5 @@ termux_step_make () {
 termux_step_make_install () {
 	# "install_sw" instead of "install" to not install man pages:
 	make -j 1 install_sw MANDIR=$TERMUX_PREFIX/share/man MANSUFFIX=.ssl
+	install -Dm600 "${TERMUX_PKG_SRCDIR}/apps/openssl.cnf" "${TERMUX_PREFIX}/etc/tls/openssl.cnf"
 }
