8c6ca7e7dbb7652103b035d684002420b3186e49
Fredrik Fornwall <fredrik@fornwall.net>
2018-09-20 21:00:20 +0000

    jupp: Fix download problem

--- packages/jupp/build.sh
+++ packages/jupp/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_DEPENDS="ncurses, libutil"
 TERMUX_PKG_CONFLICTS="joe"
 TERMUX_PKG_VERSION=3.1jupp36
 TERMUX_PKG_SHA256=99df61b8644613144f4bcb9e6b8fc53a722c39521cdc7a0413c5a9146fd63e9f
-TERMUX_PKG_SRCURL=https://www.mirbsd.org/MirOS/dist/jupp/joe-${TERMUX_PKG_VERSION}.tgz
+TERMUX_PKG_SRCURL=http://www.mirbsd.org/MirOS/dist/jupp/joe-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-dependency-tracking
 --disable-getpwnam
