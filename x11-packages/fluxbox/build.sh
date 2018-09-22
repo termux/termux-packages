fad66aecc50f9bda0f2e95ba6db8322191b24690
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-23 00:57:07 +0300

    fluxbox: add '-Wno-c++11-narrowing' to CXXFLAGS
    Fixes build failure on at least ARM.

--- x11-packages/fluxbox/build.sh
+++ x11-packages/fluxbox/build.sh
@@ -7,12 +7,15 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/fluxbox/fluxbox-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=fc8c75fe94c54ed5a5dd3fd4a752109f8949d6df67a48e5b11a261403c382ec0
 TERMUX_PKG_DEPENDS="feh, fribidi, imlib2, libandroid-support, libc++, libxft, libxinerama, libxpm, libxrandr, xorg-xmessage"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-imlib2
 --enable-xft
 --enable-xinerama
-CFLAGS=-I${TERMUX_PREFIX}/include/libandroid-support
 "
 
-TERMUX_PKG_BUILD_IN_SRC=true
+termux_step_pre_configure() {
+    export CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include/libandroid-support"
+    export CXXFLAGS="${CXXFLAGS} -Wno-c++11-narrowing"
+}
