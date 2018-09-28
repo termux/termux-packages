13683a8c4b1d993ffc21f86b6f44298a97dcc226
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-28 16:47:36 +0300

    libxss: update to 1.2.3

--- x11-packages/libxss/build.sh
+++ x11-packages/libxss/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 Screen Saver extension library"
-TERMUX_PKG_VERSION=1.2.2
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.2.3
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXScrnSaver-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=8ff1efa7341c7f34bcf9b17c89648d6325ddaae22e3904e091794e0b4426ce1d
+TERMUX_PKG_SHA256=f917075a1b7b5a38d67a8b0238eaab14acd2557679835b154cf2bca576e89bf8
 TERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="xorgproto"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
