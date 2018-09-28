9f68325798b646575c8f0c2affdef45c7d80b576
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-28 16:52:14 +0300

    libxinerama: update to 1.1.4

--- x11-packages/libxinerama/build.sh
+++ x11-packages/libxinerama/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 Xinerama extension library"
-TERMUX_PKG_VERSION=1.1.3
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=1.1.4
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXinerama-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=7a45699f1773095a3f821e491cbd5e10c887c5a5fce5d8d3fced15c2ff7698e2
+TERMUX_PKG_SHA256=0008dbd7ecf717e1e507eed1856ab0d9cf946d03201b85d5dcf61489bb02d720
 TERMUX_PKG_DEPENDS="libxext"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="xorgproto"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
