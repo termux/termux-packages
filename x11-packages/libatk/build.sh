ab5e3491a09e1f4335d7853385cdf74d480eb392
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-28 17:16:44 +0300

    libatk: update to 2.30.0

--- x11-packages/libatk/build.sh
+++ x11-packages/libatk/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://www.gtk.org
 TERMUX_PKG_DESCRIPTION="The interface definitions of accessibility infrastructure"
-_major=2.28
-_minor=1
+_major=2.30
+_minor=0
 TERMUX_PKG_VERSION=${_major}.${_minor}
-TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/atk/${_major}/atk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cd3a1ea6ecc268a2497f0cd018e970860de24a6d42086919d6bf6c8e8d53f4fc
+TERMUX_PKG_SHA256=dd4d90d4217f2a0c1fee708a555596c2c19d26fef0952e1ead1938ab632c027b
 TERMUX_PKG_DEPENDS="glib, libandroid-support"
