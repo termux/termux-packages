8083ef2383567f61aeb3830df6197947edef4661
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-09-28 16:24:25 +0300

    script 'get-latest-version.sh' now can suggest whether package update is needed

--- x11-packages/get-latest-version.sh
+++ x11-packages/get-latest-version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ##
 ##  Script for querying latest version of package.
 ##
@@ -11,11 +11,43 @@ if [ -z "${1}" ]; then
     exit 1
 fi
 
+check_if_update_needed() {
+    local SCRIPT_DIR SCRIPT_PATH
+    SCRIPT_PATH=$(realpath "${0}")
+    SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+
+    if [ -f "${SCRIPT_DIR}/${1}/build.sh" ]; then
+        CURRENT_VERSION=$(grep 'TERMUX_PKG_VERSION=' "${SCRIPT_DIR}/${1}/build.sh" | cut -d= -f2)
+        if [ "${CURRENT_VERSION}" != "${2}" ]; then
+            echo "Current version: ${CURRENT_VERSION}"
+            echo
+            echo "[!] Package should be updated."
+            echo
+        fi
+    elif [ -f "${SCRIPT_DIR}/lib${1}/build.sh" ]; then
+        ## Same packages in ArchLinux and Termux may have different names.
+        ## Example: GTK-2 in Termux has name 'libgtk2' but in ArchLinux it
+        ## named as 'gtk2'.
+        CURRENT_VERSION=$(grep 'TERMUX_PKG_VERSION=' "${SCRIPT_DIR}/lib${1}/build.sh" | cut -d= -f2)
+        if [ "${CURRENT_VERSION}" != "${2}" ]; then
+            echo "Current version: ${CURRENT_VERSION}"
+            echo
+            echo "[!] Package should be updated."
+            echo
+        fi
+    else
+        ## If no 'build.sh' script found, then do nothing.
+        :
+    fi
+}
+
 for repo in extra community core; do
     for arch in x86_64 any; do
-        VERSION=$(curl -s https://www.archlinux.org/packages/${repo}/${arch}/${1}/ | grep 'itemprop="version"' | cut -d'"' -f4 | cut -d- -f1)
+        VERSION=$(curl -s "https://www.archlinux.org/packages/${repo}/${arch}/${1}/" | grep 'itemprop="version"' | cut -d'"' -f4 | cut -d- -f1)
         if [ -n "${VERSION}" ]; then
-            echo "${1}: ${VERSION}"
+            echo "Package: ${1}"
+            echo "Latest version: ${VERSION}"
+            check_if_update_needed "${1}" "${VERSION}"
             exit 0
         fi
     done
