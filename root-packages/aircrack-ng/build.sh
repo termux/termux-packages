96482ccfdcb93ea5fcefe30121c5b146f3f1c83d
Henrik Grimler <Henrik@Grimler.se>
2018-09-29 18:35:42 +0200

    aircrack-ng: rebuild against openssl 1.1.1

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org
 TERMUX_PKG_DESCRIPTION="an 802.11 WEP and WPA-PSK keys cracking program"
 TERMUX_PKG_VERSION=1.2-rc4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://download.aircrack-ng.org/aircrack-ng-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d93ac16aade5b4d37ab8cdf6ce4b855835096ccf83deb65ffdeff6d666eaff36
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
