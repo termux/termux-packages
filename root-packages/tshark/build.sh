17acb26b3bd115b5da06cee64ee6c54276479dc1
Henrik Grimler <Henrik@Grimler.se>
2018-09-29 10:17:52 +0200

    tshark: update to 2.6.3
    Also rebuilds against openssl 1.1.1

--- root-packages/tshark/build.sh
+++ root-packages/tshark/build.sh
@@ -1,14 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org
 TERMUX_PKG_DESCRIPTION="network protocol analyzer and sniffer"
-TERMUX_PKG_VERSION=2.4.4
+TERMUX_PKG_VERSION=2.6.3
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/all-versions/wireshark-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=049a758e39422dcd536d7f75cebbfaa44e4f305d602bf22964d6459821126f58
+TERMUX_PKG_SHA256=d158a8a626dc0997a826cf12b5316a3d393fb9f93d84cc86e75b212f0044a3ec
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_DEPENDS="libnl, openssl, libpcap, libpcap-dev, libcrypt, libgcrypt-dev, glib"
-# As GUI is not supported by termux we need to disable wireshark
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-qt=no --disable-wireshark"
+TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-qt=no
+--disable-wireshark
+--with-lua=no
+"
 
 termux_step_pre_configure () {
 	./autogen.sh
-	LDFLAGS+=" -L$TERMUX_PKG_BUILDDIR/wsutil/.libs -L$TERMUX_PKG_BUILDDIR/epan/.libs" #A bit hacky.. 
+	LDFLAGS+=" -L$TERMUX_PKG_BUILDDIR/wsutil/.libs -L$TERMUX_PKG_BUILDDIR/epan/.libs" #A bit hacky..
 }
