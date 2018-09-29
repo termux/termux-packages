8b9aeec7eee273de71433d46d5fe73f0fcbc5974
Henrik Grimler <Henrik@Grimler.se>
2018-09-29 12:27:01 +0200

    pciutils: update to 3.6.2

--- root-packages/pciutils/build.sh
+++ root-packages/pciutils/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://mj.ucw.cz/sw/pciutils/
 TERMUX_PKG_DESCRIPTION="The PCI Utilities are a collection of programs for inspecting and manipulating configuration of PCI devices"
-TERMUX_PKG_VERSION=3.5.6
+TERMUX_PKG_VERSION=3.6.2
 TERMUX_PKG_SRCURL=ftp://atrey.karlin.mff.cuni.cz/pub/linux/pci/pciutils-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0d4d507d395e727384737d3c45cf8c5a0023864a6eb5c6ed7caf7d483995391d
+TERMUX_PKG_SHA256=30005e341bb0ffa734174e592dc8f0dd928e1c9368b859715812149ed91d8f93
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure () {
