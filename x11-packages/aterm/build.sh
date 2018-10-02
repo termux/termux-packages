b5e4b72e16bc481110be8b9c0cad1d59494d5f05
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-02 20:49:49 +0300

    aterm: add libxt to build depends

--- x11-packages/aterm/build.sh
+++ x11-packages/aterm/build.sh
@@ -7,6 +7,8 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/sourceforge/aterm/aterm-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=a161c3b2d9c7149130a41963899993af21eae92e8e362f4b5b3c7c4cb16760ce
 TERMUX_PKG_DEPENDS="libice, libsm, libx11, libxext"
+TERMUX_PKG_BUILD_DEPENDS="libxt"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-transparency=yes
