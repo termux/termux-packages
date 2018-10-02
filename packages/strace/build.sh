d1793da4b4f2c0f857af1977ff4567dba228eab7
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-10-03 01:17:03 +0300

    strace: fix crash on Android 9 (Pie)
    Disables setreuid() and similar calls that are disallowed
    by seccomp.

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 TERMUX_PKG_VERSION=4.24
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=1f4e59fc1edfa2bfb4adf2a748623dc25b105ec79713dd84404199f91b0b0634
 TERMUX_PKG_SRCURL=https://strace.io/files/$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
