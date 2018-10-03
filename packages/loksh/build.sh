f2186f9192caef86cb2bad02578a3b1a1477f6ab
Karol Baraniecki <karol@karcom.pl>
2018-10-04 00:27:53 +0200

    loksh: Remove the dot from the description
    as most packages don't have it.

--- packages/loksh/build.sh
+++ packages/loksh/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dimkr/loksh
-TERMUX_PKG_DESCRIPTION="A Linux port of OpenBSD's ksh."
+TERMUX_PKG_DESCRIPTION="A Linux port of OpenBSD's ksh"
 TERMUX_PKG_VERSION=6.3
 TERMUX_PKG_SHA256=4c8bccf3fef58dce1c67395cffbf4e95fb6ee597d582ff2d2b3a851b1e302b44
 TERMUX_PKG_SRCURL=https://github.com/dimkr/loksh/archive/${TERMUX_PKG_VERSION}.tar.gz
