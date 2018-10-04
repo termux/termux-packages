06746a80aaff3fffeb34383d9c8402b5d7d7547c
its-pointless <bored@goat.si>
2018-10-04 19:26:51 +1000

    i686 fd fix

--- packages/fd/build.sh
+++ packages/fd/build.sh
@@ -4,6 +4,13 @@ TERMUX_PKG_VERSION=7.1.0
 TERMUX_PKG_SHA256=9385a55738947f69fd165781598de6c980398c6214a4927fce13a4f7f1e63d4d
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/fd/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
+termux_step_pre_configure() {
+	if [ $TERMUX_ARCH = "i686" ]; then
+		for ARCH in $TERMUX_PKG_BUILDER_DIR/*.i686; do
+			patch -p1 < $ARCH
+		done
+	fi
+}
 
 termux_step_post_make_install() {
 	mkdir -p  $TERMUX_PREFIX/share/man/man1
