dc8930b5b958259d124cb3841e4b34095bd9ad67
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-04 15:27:04 +0300

    mpv-x: update to 0.29.1

--- x11-packages/mpv-x/build.sh
+++ x11-packages/mpv-x/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://mpv.io/
 TERMUX_PKG_DESCRIPTION="Command-line media player"
-TERMUX_PKG_VERSION=0.29.0
-TERMUX_PKG_REVISION=7
-TERMUX_PKG_SHA256=772af878cee5495dcd342788a6d120b90c5b1e677e225c7198f1e76506427319
+TERMUX_PKG_VERSION=0.29.1
+TERMUX_PKG_SHA256=f9f9d461d1990f9728660b4ccb0e8cb5dce29ccaa6af567bec481b79291ca623
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
 
 TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-shmem, libdrm, littlecms, libjpeg-turbo, libcaca, liblua52, libpulseaudio, libx11, libxext, libxinerama, libxss, libxrandr, openal-soft"
