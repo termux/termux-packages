ba5ed5006ea283217a7e37cbb532d97658e4b7bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-05 16:54:11 +0300

    libx11, libxcb: add 'xorg-xauth' to recommends
    This will be useful for people that use SSH X Forwarding - xorg-xauth will be
    installed automatically (unless install-recommends was disabled in APT).

--- x11-packages/libx11/build.sh
+++ x11-packages/libx11/build.sh
@@ -3,11 +3,13 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 client-side library"
 TERMUX_PKG_VERSION=1.6.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=65fe181d40ec77f45417710c6a67431814ab252d21c2e85c75dd1ed568af414f
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros, xtrans"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="xorgproto, xtrans"
+TERMUX_PKG_RECOMMENDS="xorg-xauth"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-xthreads --enable-malloc0returnsnull"
 
 termux_step_post_make_install() {
