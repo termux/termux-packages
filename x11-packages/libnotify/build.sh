35f6b32a7398eb4e536bd3d5407087b6d5095287
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-05 22:24:55 +0300

    new package: uget - download manager featuring download classification and HTML import

---
+++ x11-packages/libnotify/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+
+TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/notification-spec/
+TERMUX_PKG_DESCRIPTION="Library for sending desktop notifications"
+TERMUX_PKG_VERSION=0.7.7
+TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/libnotify/0.7/libnotify-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=9cb4ce315b2655860c524d46b56010874214ec27e854086c1a1d0260137efc04
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib"
