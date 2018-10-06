b6f2923beb88c95cd6b047aed66819b1b1e7c091
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-06 17:00:04 +0300

    adwaita-icon-theme, geany, transmission-gtk: don't store icon theme cache in *.deb file

--- x11-packages/adwaita-icon-theme/build.sh
+++ x11-packages/adwaita-icon-theme/build.sh
@@ -3,10 +3,12 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.gnome.org
 TERMUX_PKG_DESCRIPTION="Freedesktop.org Hicolor icon theme"
 TERMUX_PKG_VERSION=3.30.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/GNOME/adwaita-icon-theme/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=14b70916d763f841865c295b4d6dd0aebba6f0829a29c019348d0f775178aeba
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_DEPENDS="hicolor-icon-theme"
+TERMUX_PKG_RM_AFTER_INSTALL="share/icons/Adwaita/icon-theme.cache"
 
 termux_step_pre_configure() {
     autoreconf -fvi
@@ -15,7 +17,7 @@ termux_step_pre_configure() {
 termux_step_post_make_install() {
     # fix location of adwaita-icon-theme.pc
     if [ -f "${TERMUX_PREFIX}/share/pkgconfig/adwaita-icon-theme.pc" ]; then
-        mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig" || exit 1
-        mv -f "${TERMUX_PREFIX}/share/pkgconfig/adwaita-icon-theme.pc" "${TERMUX_PREFIX}/lib/pkgconfig/adwaita-icon-theme.pc" || exit 1
+        mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig"
+        mv -f "${TERMUX_PREFIX}/share/pkgconfig/adwaita-icon-theme.pc" "${TERMUX_PREFIX}/lib/pkgconfig/adwaita-icon-theme.pc"
     fi
 }
--- x11-packages/geany/build.sh
+++ x11-packages/geany/build.sh
@@ -3,13 +3,17 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.geany.org/
 TERMUX_PKG_DESCRIPTION="Fast and lightweight IDE"
 TERMUX_PKG_VERSION=1.33.0
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://download.geany.org/geany-${TERMUX_PKG_VERSION/.0}.tar.bz2
 TERMUX_PKG_SHA256=66baaff43f12caebcf0efec9a5533044dc52837f799c73a1fd7312caa86099c2
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, fribidi, gdk-pixbuf, glib, gtk3, harfbuzz, libandroid-glob, libandroid-shmem, libandroid-support, libbz2, libc++, libcairo-x, libffi, libgraphite, liblzma, libpixman, libpng, libuuid, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, libxrender, pango-x, pcre"
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk3"
-TERMUX_PKG_RM_AFTER_INSTALL="share/icons/hicolor/icon-theme.cache"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+share/icons/hicolor/icon-theme.cache
+share/icons/Tango/icon-theme.cache
+"
 
 termux_step_pre_configure() {
     export LIBS="-landroid-glob"
