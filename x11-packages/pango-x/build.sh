3fcef638de80af42b25f447db1eb7865f66670e6
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-06 22:16:22 +0300

    pango-x: update to 1.42.4

--- x11-packages/pango-x/build.sh
+++ x11-packages/pango-x/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=http://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text (with X)"
-TERMUX_PKG_VERSION=1.42.2
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=b1e416b4d40416ef6c8224cf146492b86848703264ba88f792290992cf3ca1e2
+TERMUX_PKG_VERSION=1.42.4
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d
 
 TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo-x, libxft"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="fontconfig-dev, harfbuzz-dev, libcairo-x-dev, libpixman-dev"
