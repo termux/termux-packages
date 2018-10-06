e1e97fc99c3aa60ca114fcbc0ea458a1a5f5428a
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-06 16:59:19 +0300

    desktop-file-utils: simplify hooks

--- x11-packages/desktop-file-utils/build.sh
+++ x11-packages/desktop-file-utils/build.sh
@@ -3,13 +3,18 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/desktop-file-utils
 TERMUX_PKG_DESCRIPTION="Command line utilities for working with desktop entries"
 TERMUX_PKG_VERSION=0.23
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=6c094031bdec46c9f621708f919084e1cb5294e2c5b1e4c883b3e70cb8903385
 TERMUX_PKG_DEPENDS="glib"
 
 termux_step_create_debscripts() {
-    cp "${TERMUX_PKG_BUILDER_DIR}/postinst" ./
-    cp "${TERMUX_PKG_BUILDER_DIR}/postrm"   ./
-    cp "${TERMUX_PKG_BUILDER_DIR}/triggers" ./
+    for i in postinst postrm triggers; do
+        sed \
+            "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+            "${TERMUX_PKG_BUILDER_DIR}/hooks/${i}.in" > ./${i}
+        chmod 755 ./${i}
+    done
+    unset i
+    chmod 644 ./triggers
 }
