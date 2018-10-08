0b13e90fa7f025de6e71c6598f7b65ec691a1b76
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-08 22:52:29 +0300

    libxtst: add libxfixes to build depends

--- x11-packages/libxtst/build.sh
+++ x11-packages/libxtst/build.sh
@@ -6,4 +6,4 @@ TERMUX_PKG_VERSION=1.2.3
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXtst-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=4655498a1b8e844e3d6f21f3b2c4e2b571effb5fd83199d428a6ba7ea4bf5204
 TERMUX_PKG_DEPENDS="libx11, libxext, libxi"
-TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
+TERMUX_PKG_BUILD_DEPENDS="libxfixes, xorg-util-macros"
