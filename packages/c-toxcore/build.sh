7e4005a32a54bb056079545a6b6092658ee87778
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-14 19:36:00 +0200

    c-toxcore: Update from 0.2.7 to 0.2.8

--- packages/c-toxcore/build.sh
+++ packages/c-toxcore/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://tox.chat
 TERMUX_PKG_DESCRIPTION="Backend library for the Tox protocol"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=0.2.7
-TERMUX_PKG_SHA256=bef6dbb06d921af58c6a2d7629e6f8eeafa1e541d379e4ff2ccb4ff5d3499cd1
+TERMUX_PKG_VERSION=0.2.8
+TERMUX_PKG_SHA256=47fe125d8558775a03a3ded58e9faa07a01cfecd65257e24b544b4b5698d9362
 TERMUX_PKG_SRCURL=https://github.com/TokTok/toxcore/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libsodium"
 TERMUX_PKG_BUILD_IN_SRC=true
