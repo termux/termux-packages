7d45a8511d8926fde245b52ac4f96e8eaca2e693
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-18 20:23:42 +0200

    gegl: Update from 0.4.8 to 0.4.10

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://gegl.org/
 TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
-TERMUX_PKG_VERSION=0.4.8
-TERMUX_PKG_SHA256=719468eec56ac5b191626a0cb6238f3abe9117e80594890c246acdc89183ae49
+TERMUX_PKG_VERSION=0.4.10
+TERMUX_PKG_SHA256=1c419659bde1aa8bb7845231a7132bd7dcdaa4ed724adf676b07478b41f0c5d3
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libandroid-support, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
