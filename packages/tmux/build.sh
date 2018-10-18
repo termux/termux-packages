0c0f31f155d405dd4d52c07a223440115739bc47
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-18 19:07:18 +0200

    tmux: Update from 2.7 to 2.8

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://tmux.github.io/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer"
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
 TERMUX_PKG_DEPENDS="ncurses, libevent, libutil, libandroid-support, libandroid-glob"
-TERMUX_PKG_VERSION=2.7
-TERMUX_PKG_SHA256=9ded7d100313f6bc5a87404a4048b3745d61f2332f99ec1400a7c4ed9485d452
+TERMUX_PKG_VERSION=2.8
+TERMUX_PKG_SHA256=7f6bf335634fafecff878d78de389562ea7f73a7367f268b66d37ea13617a2ba
 TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/releases/download/${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
