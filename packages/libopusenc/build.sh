bde5f6c6d9b26122cde35d06b1308f089b5b7f8e
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-18 06:50:19 +0200

    libopusenc: Update from 0.2 to 0.2.1

--- packages/libopusenc/build.sh
+++ packages/libopusenc/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/xiph/libopusenc
 TERMUX_PKG_DESCRIPTION="High-level API for encoding .opus files"
-TERMUX_PKG_VERSION=0.2
-TERMUX_PKG_SHA256=c79e95eeee43a0b965e9b2c59a243763a8f8b0a7e71441df2aa9084f6171c73a
+TERMUX_PKG_VERSION=0.2.1
+TERMUX_PKG_SHA256=8298db61a8d3d63e41c1a80705baa8ce9ff3f50452ea7ec1c19a564fe106cbb9
 TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/opus/libopusenc-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libopus"
