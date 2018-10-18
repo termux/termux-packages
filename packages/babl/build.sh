aea27c5612fd5d85ddd1feda37e98c3c189ef005
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-18 20:23:17 +0200

    babl: Update from 0.1.56 to 0.1.58

--- packages/babl/build.sh
+++ packages/babl/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://gegl.org/babl/
 TERMUX_PKG_DESCRIPTION="Dynamic pixel format translation library"
-TERMUX_PKG_VERSION=0.1.56
-TERMUX_PKG_SHA256=aff4bed10615be5d0c43576c34bf15ac1ddb6fef9079c8251fbb98d630aaebaa
+TERMUX_PKG_VERSION=0.1.58
+TERMUX_PKG_SHA256=e51eeb6c9b8d1c25235bee5b2cb8fd3e6057752536751ebcf8c7db9b51305e03
 TERMUX_PKG_SRCURL=https://github.com/GNOME/babl/archive/BABL_${TERMUX_PKG_VERSION//./_}.tar.gz
 
 termux_step_pre_configure() {
