5dcb47cb90151d88c7d947b3e120733c9632c9c8
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-18 22:31:00 +0200

    libicu: Update from 62.1 to 63.1

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://site.icu-project.org/home
 TERMUX_PKG_DESCRIPTION='International Components for Unicode library'
-TERMUX_PKG_VERSION=62.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=2e7948296f56f40038e5969a1a59c63e9fd21c7711eb7f1783527e533c8c268c
+TERMUX_PKG_VERSION=63.1
+TERMUX_PKG_SHA256=150a1e278fbc5a2ae77aa2efdfb87dcd43321390fe211158192049baaba799b0
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/icu4c-${TERMUX_PKG_VERSION//./_}-src.tar.xz
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/icu-config share/man/man1/icu-config.1 lib/icu share/icu"
 TERMUX_PKG_HOSTBUILD=true
