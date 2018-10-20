2a660563f02c3e592d870af041d269d76ee23ac2
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-20 22:00:58 +0200

    file: Update from 5.34 to 5.35

--- packages/file/build.sh
+++ packages/file/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://darwinsys.com/file/
 TERMUX_PKG_DESCRIPTION="Command-line tool that tells you in words what kind of data a file contains"
-TERMUX_PKG_VERSION=5.34
-TERMUX_PKG_SHA256=f15a50dbbfa83fec0bd1161e8e191b092ec832720e30cd14536e044ac623b20a
+TERMUX_PKG_VERSION=5.35
+TERMUX_PKG_SHA256=30c45e817440779be7aac523a905b123cba2a6ed0bf4f5439e1e99ba940b5546
 TERMUX_PKG_SRCURL=ftp://ftp.astron.com/pub/file/file-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_mmap_fixed_mapped=yes"
 TERMUX_PKG_EXTRA_MAKE_ARGS="FILE_COMPILE=$TERMUX_PKG_HOSTBUILD_DIR/src/file"
