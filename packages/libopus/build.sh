b8a790dbf3b300501befbc2cb165010d1748c134
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-20 22:01:17 +0200

    libopus: Update from 1.2.1 to 1.3

--- packages/libopus/build.sh
+++ packages/libopus/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://www.opus-codec.org/
 TERMUX_PKG_DESCRIPTION="Reference implementation of the Opus codec"
-TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_VERSION=1.3
+TERMUX_PKG_SHA256=4f3d69aefdf2dbaf9825408e452a8a414ffc60494c70633560700398820dc550
 TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/opus/opus-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cfafd339ccd9c5ef8d6ab15d7e1a412c054bf4cb4ecbbbcc78c12ef2def70732
