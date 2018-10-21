b19653b13e6e846e8086c26a8b26a76351de1928
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-21 15:20:43 +0300

    new package: termux-auth

---
+++ packages/termux-auth/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/xeffyr/termux-auth
+TERMUX_PKG_DESCRIPTION="Password authentication library and utility for Termux"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_SRCURL=https://github.com/xeffyr/termux-auth/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=37a1ccebc73caced72a1baead20d50ebfc630dce97f68aa35fb30a1133b7206e
+TERMUX_PKG_DEPENDS="openssl"
