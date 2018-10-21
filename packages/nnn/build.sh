e39852692775add1d5124a8dea85c553505bcced
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-21 20:16:12 +0200

    nnn: Update from 1.9 to 2.0

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -1,11 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jarun/nnn
 TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
-TERMUX_PKG_VERSION=1.9
-TERMUX_PKG_SHA256=7ba298a55a579640fe0ae37f553be739957da0c826f532beac46acfb56e2d726
+TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_SHA256=0029efd3d009c197b1646d260350f3f87edca76eef3be6b81846af133d58d6a9
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="ncurses, readline"
+TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
-
-termux_step_pre_configure() {
-	CFLAGS+=" -Dushort=uint32_t"
-}
