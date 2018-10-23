3b9fa3a2808183e9ef0da7859dcd2210fd78b137
Henrik Grimler <Henrik@Grimler.se>
2018-10-23 21:37:28 +0200

    openmpi: avoid error/warning on arm

--- packages/openmpi/build.sh
+++ packages/openmpi/build.sh
@@ -2,12 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.open-mpi.org
 TERMUX_PKG_DESCRIPTION="The Open MPI Project is an open source Message Passing Interface implementation that is developed and maintained by a consortium of academic, research, and industry partners"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=3.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.open-mpi.org/release/open-mpi/v${TERMUX_PKG_VERSION:0:3}/openmpi-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=363c8a1258d12df28657e9b13bfe41e30fc129a4cfb89fcf53ff68bbe1bd65b3
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-x
 ac_cv_header_ifaddrs_h=no
 --disable-dlopen
 --disable-mpi-fortran
+ac_cv_member_struct_ifreq_ifr_hwaddr=no
 "
 
 termux_step_pre_configure () {
