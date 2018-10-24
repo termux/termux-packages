7d848c0d239c6af81927117dd6ef65680ddc9ec4
its-pointless <bored@goat.si>
2018-10-25 08:44:46 +1100

    fixed cmake not working on arm

--- packages/make/build.sh
+++ packages/make/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/make/
 TERMUX_PKG_DESCRIPTION="Tool to control the generation of non-source files from source files"
 TERMUX_PKG_VERSION=4.2.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=e40b8f018c1da64edd1cc9a6fce5fa63b2e707e404e20cad91fbae337c98a5b7
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/make/make-${TERMUX_PKG_VERSION}.tar.gz
 # Prevent linking against libelf:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_elf_elf_begin=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_elf_elf_begin=no ac_cv_func_sigsetmask=yes"
