da8eeb29896a538d0c9c8307d90ca9aa9fd85592
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-28 00:47:44 +0300

    tigervnc, xorg-server-xvfb: fix GLX extension by providing 'swrast_dri.so' for these packages
    OpenGL can be used without GLX extension, but Qt requires it anyway. So we have to provide
    swrast driver.

--- x11-packages/tigervnc/build.sh
+++ x11-packages/tigervnc/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=http://www.tigervnc.org/
 TERMUX_PKG_VERSION=1.9.0
-TERMUX_PKG_REVISION=15
+TERMUX_PKG_REVISION=16
 TERMUX_PKG_DESCRIPTION="Suite of VNC servers. Based on the VNC 4 branch of TightVNC."
 TERMUX_PKG_SRCURL=https://github.com/TigerVNC/tigervnc/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f15ced8500ec56356c3bf271f52e58ed83729118361c7103eab64a618441f740
 
-TERMUX_PKG_DEPENDS="freetype, libandroid-support, libandroid-shmem, libbz2, libc++, libdrm, libexpat, libgnutls, libjpeg-turbo, libpixman, libpng, libuuid, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxxf86vm, libxfixes, libxfont2, libxshmfence, mesa, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp"
+TERMUX_PKG_DEPENDS="freetype, libandroid-support, libandroid-shmem, libbz2, libc++, libdrm, libexpat, libgnutls, libjpeg-turbo, libpixman, libpng, libuuid, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxxf86vm, libxfixes, libxfont2, libxshmfence, mesa, mesa-swrast, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-font-util, xorg-util-macros, xorg-server-xvfb, xtrans"
 TERMUX_PKG_SUGGESTS="aterm, xorg-twm"
 
