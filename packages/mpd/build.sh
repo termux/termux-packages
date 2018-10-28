2b5585b236d2c5c1243e540cb2a79bb766c23da3
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-29 00:54:34 +0100

    mpd: Update from 0.20.21 to 0.20.22

--- packages/mpd/build.sh
+++ packages/mpd/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.musicpd.org
 TERMUX_PKG_DESCRIPTION="Music player daemon"
-TERMUX_PKG_VERSION=0.20.21
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=ae190faf43fd0cbe9d8d2d13bdabf97deba47fce16d9f0c5b775763cb5e1d15a
+TERMUX_PKG_VERSION=0.20.22
+TERMUX_PKG_SHA256=c7747a98d0958ef4ad41c24f2005c58f8b312745cf79dbcf0f26354356b51c88
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/MPD/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libcurl, libid3tag, libopus, libpulseaudio, libmpdclient, openal-soft, libvorbis, libsqlite, ffmpeg, libmp3lame, libbz2"
 TERMUX_PKG_BUILD_DEPENDS="boost"
