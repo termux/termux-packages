1e1c9f930da95902c4e1fe78bd922600b0b812ef
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-28 21:36:06 +0200

    libdrm: update to 2.4.96

--- x11-packages/libdrm/build.sh
+++ x11-packages/libdrm/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=http://dri.freedesktop.org
 TERMUX_PKG_DESCRIPTION="Userspace interface to kernel DRM services"
-TERMUX_PKG_VERSION=2.4.95
+TERMUX_PKG_VERSION=2.4.96
 TERMUX_PKG_SRCURL=https://dri.freedesktop.org/libdrm/libdrm-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ef772a51b4bed97a2c243194d9a98da97319e0dbdf800d07773b025aacc895c6
+TERMUX_PKG_SHA256=0d561acf7bb4cc59dc82415100e6c1a44860e8c380e00f9592923e3cd08db393
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-intel
@@ -14,6 +14,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-vmwgfx
 "
 
-termux_step_pre_configure () {
+termux_step_pre_configure() {
     CFLAGS="${CFLAGS} -DANDROID"
 }
