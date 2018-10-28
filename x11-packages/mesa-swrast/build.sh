79ae4f43ebe45a71620a3d6ded98e4ce6cb3d8b5
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-10-28 03:00:58 +0300

    mesa-swrast: install file 'dri_interface.h'

--- x11-packages/mesa-swrast/build.sh
+++ x11-packages/mesa-swrast/build.sh
@@ -10,6 +10,7 @@ TERMUX_PKG_HOMEPAGE=https://www.mesa3d.org
 TERMUX_PKG_DESCRIPTION="The legacy/original Mesa software rasterizer"
 ## Use 17.3.x branch because 18.x.x requires 'pthread_barrier_t'.
 TERMUX_PKG_VERSION=17.3.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/mesa-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c5beb5fc05f0e0c294fefe1a393ee118cb67e27a4dca417d77c297f7d4b6e479
 
@@ -35,4 +36,9 @@ termux_step_pre_configure () {
 
 termux_step_make_install() {
     install -Dm600 ./lib/swrast_dri.so "${TERMUX_PREFIX}/lib/dri/swrast_dri.so"
+
+    install \
+        -Dm600 \
+        "${TERMUX_PKG_SRCDIR}/include/GL/internal/dri_interface.h" \
+        "${TERMUX_PREFIX}/include/GL/internal/dri_interface.h"
 }
