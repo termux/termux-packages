4e3c78e4a6d8df8e9aacfbc009b9d7e5f8395e86
Auxilus <raghavsphadke@gmail.com>
2018-10-30 08:05:59 -0700

    Add package: hexedit

---
+++ hexedit/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=http://rigaux.org/hexedit.html
+TERMUX_PKG_DESCRIPTION="view and edit files in hexadecimal or in ASCII"
+TERMUX_PKG_VERSION=1.2.13
+TERMUX_PKG_SHA256=6a126da30a77f5c0b08038aa7a881d910e3b65d13767fb54c58c983963b88dd7
+TERMUX_PKG_SRCURL=http://rigaux.org/hexedit-${TERMUX_PKG_VERSION}.src.tgz
