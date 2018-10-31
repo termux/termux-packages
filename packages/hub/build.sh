b32ff80949f4a94fc525f369e5d286b870023992
Fredrik Fornwall <fredrik@fornwall.net>
2018-10-31 23:10:43 +0100

    hub: Update from 2.5.1 to 2.6.0

--- packages/hub/build.sh
+++ packages/hub/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://hub.github.com/
 TERMUX_PKG_DESCRIPTION="Command-line wrapper for git that makes you better at GitHub"
-TERMUX_PKG_VERSION=2.5.1
-TERMUX_PKG_SHA256=35fecdbcaf0afb6b7273a160cc169f76ec62b95105037ac3fc833b24573f9a4f
+TERMUX_PKG_VERSION=2.6.0
+TERMUX_PKG_SHA256=c4df5ce953aa5dd5c4fa57ada96559b4b76bbd30a9e961e10e881be869cf4f2c
 TERMUX_PKG_SRCURL=https://github.com/github/hub/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="git"
 
