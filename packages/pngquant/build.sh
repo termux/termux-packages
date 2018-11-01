085693eeb8de12e7f47d16f003199d8f1c51eafd
Fredrik Fornwall <fredrik@fornwall.net>
2018-11-01 03:23:42 +0100

    pngquant: Update frmo 2.12.1 to 2.12.2

--- packages/pngquant/build.sh
+++ packages/pngquant/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://pngquant.org
 TERMUX_PKG_DESCRIPTION="PNG image optimising utility"
-TERMUX_PKG_VERSION=2.12.1
-TERMUX_PKG_SHA256=(352ff60420fd5ab7a94f548be6f87dbdfa15eb28e3cc8f61c089f4e0be7ee1a0
-		   c0f6ddfb1ed214a520b18f6fff16fe09f39d427caf9679b05657e32dfdc2cb27)
+TERMUX_PKG_VERSION=2.12.2
+TERMUX_PKG_SHA256=(5edf7c5bffd07e5d28fcc6d4d94a187c30b532c52ac986b3e45aff3dce0567dc
+                   34036379d3b68de79b5f180b3fe3989b9d2e81c1909ee3f85acaedb74f46003b)
 # If both archives are .tar.gz then they overwrite eachother since they are the same version and hence the same name.
 # Work around this by using .zip for one of them...
 TERMUX_PKG_SRCURL=(https://github.com/pornel/pngquant/archive/$TERMUX_PKG_VERSION.tar.gz
