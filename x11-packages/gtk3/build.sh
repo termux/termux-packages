36fcf8e4acfaf255226d97f96dc09c39d3f96be8
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-11-04 22:13:04 +0200

    gtk3 (devpackage): depend on pango-x-dev
    See issue https://github.com/termux/x11-packages/issues/17

--- x11-packages/gtk3/build.sh
+++ x11-packages/gtk3/build.sh
@@ -3,14 +3,14 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=http://www.gtk.org/
 TERMUX_PKG_DESCRIPTION="GObject-based multi-platform GUI toolkit"
 TERMUX_PKG_VERSION=3.24.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/GNOME/gtk/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3dd9a8d52e6832e9294182c3a9d3b3979e9593db181101476323241ae67b4a44
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, gdk-pixbuf, glib, glib-bin, gtk-update-icon-cache, libcairo-x, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango-x, shared-mime-info"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_DEVPACKAGE_DEPENDS="xorgproto"
+TERMUX_PKG_DEVPACKAGE_DEPENDS="xorgproto, pango-x-dev"
 TERMUX_PKG_CONFLICTS="libgtk3"
 TERMUX_PKG_REPLACES="libgtk3"
 
