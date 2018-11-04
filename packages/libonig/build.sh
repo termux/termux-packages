6c7ce6d4edf118bdfe57cf5d535397b43467cb92
valpogus <32718480+valpogus@users.noreply.github.com>
2018-11-04 17:14:25 +0100

    Updated jq to version 1.6 (including dependencies)

---
+++ packages/libonig/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/kkos/oniguruma
+TERMUX_PKG_DESCRIPTION="Regular expression library"
+TERMUX_PKG_VERSION=6.9.0
+TERMUX_PKG_SRCURL=https://github.com/kkos/oniguruma/releases/download/v${TERMUX_PKG_VERSION}/onig-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=91bfb25e050ce3c301483204dd3f0d03a7c05472e20d48fe227a383d4534e7c9
+
