1411e0b341bfb7e0a4b87024858bdb16d58f74c5
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-11-07 22:16:35 +0200

    new package: xorg-xev

---
+++ x11-packages/xorg-xev/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+
+TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
+TERMUX_PKG_DESCRIPTION="Print contents of X events"
+TERMUX_PKG_VERSION=1.2.2
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/app/xev-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=d94ae62a6c1af56c2961d71f5782076ac4116f0fa4e401420ac7e0db33dc314f
+TERMUX_PKG_DEPENDS="libx11, libxrandr"
