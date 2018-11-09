4003efd0986263e8ae2d7b443a272311035af6af
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-11-09 13:44:23 +0200

    syncthing: update to 0.14.52

--- packages/syncthing/build.sh
+++ packages/syncthing/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://syncthing.net/
 TERMUX_PKG_DESCRIPTION="Decentralized file synchronization"
-TERMUX_PKG_VERSION=0.14.51
-TERMUX_PKG_SHA256=10bf5de6f5f01dc605667185ac0b485548ae9088abaa8401fc0f3a2528b2f09e
+TERMUX_PKG_VERSION=0.14.52
+TERMUX_PKG_SHA256=4954c2b05c60b6de29104d4783193a51164485cfa4a810c5a0696d72a83816c2
 TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/releases/download/v${TERMUX_PKG_VERSION}/syncthing-source-v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_make(){
