02d7ce83f6051779ba3990b897b29ef174e73104
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-11-09 15:04:27 +0200

    icecast: update to 2.4.4

--- packages/icecast/build.sh
+++ packages/icecast/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://icecast.org
 TERMUX_PKG_DESCRIPTION="Icecast is a streaming media (audio/video) server"
-TERMUX_PKG_VERSION=2.4.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.4.4
 TERMUX_PKG_SRCURL=https://downloads.xiph.org/releases/icecast/icecast-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c85ca48c765d61007573ee1406a797ae6cb31fb5961a42e7f1c87adb45ddc592
+TERMUX_PKG_SHA256=49b5979f9f614140b6a38046154203ee28218d8fc549888596a683ad604e4d44
 TERMUX_PKG_DEPENDS="libcurl, libgnutls, libogg, libvorbis, libxml2, libxslt, mime-support, openssl"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
