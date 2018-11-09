609d09282b6caa907b9d1028906d105000be6143
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-11-09 13:40:38 +0200

    cowsay: update to 3.04

--- packages/cowsay/build.sh
+++ packages/cowsay/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://en.wikipedia.org/wiki/Cowsay
 TERMUX_PKG_DESCRIPTION="Program which generates ASCII pictures of a cow with a message"
-TERMUX_PKG_VERSION=3.03
+TERMUX_PKG_VERSION=3.04
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/c/cowsay/cowsay_3.03+dfsg1.orig.tar.gz
 TERMUX_PKG_SHA256=10bae895d9afb2d720d2211db58f396352b00fe1386c369ca3608cbf6497b839
 TERMUX_PKG_DEPENDS="perl"
