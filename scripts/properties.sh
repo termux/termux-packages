bf47765bd95cad4e9ae113a246b80a443cdc9785
Henrik Grimler <henrik@grimler.se>
2018-11-09 11:59:11 +0100

    properties.sh: 'source'->'.'

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -1,7 +1,7 @@
 TERMUX_NDK_VERSION=18
 TERMUX_ANDROID_BUILD_TOOLS_VERSION=28.0.2
 
-test -f "$HOME/.termuxrc" && source "$HOME/.termuxrc"
+test -f "$HOME/.termuxrc" && . "$HOME/.termuxrc"
 
 : "${ANDROID_HOME:="${HOME}/lib/android-sdk"}"
 : "${NDK:="${HOME}/lib/android-ndk"}"
