dc615a39a5b429915b33aa37862f372233d8731c
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-11-10 15:15:34 +0200

    i3: update to 4.16

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://i3wm.org/
 TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
-TERMUX_PKG_VERSION=4.15
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=4.16
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=217d524d1fbc85ae346b25f6848d1b7bcd2c23184ec88d29114bf5a621385326
+TERMUX_PKG_SHA256=cc60a5f518c5e37b4143b644a840bb2ad2a63ea7a771954c332cd5637db655b4
 TERMUX_PKG_DEPENDS="glib, i3status, libandroid-glob, libandroid-shmem, libcairo-x, libev, libposix-shm, libxcb, libxkbcommon, pango-x, pcre, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
 
 TERMUX_PKG_CONFFILES="
