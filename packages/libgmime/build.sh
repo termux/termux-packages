272332e31cfa3d9caeb9dd726b124482db51bd59
Fredrik Fornwall <fredrik@fornwall.net>
2018-11-11 02:35:52 +0100

    libgmime: Use libidn2, not libidn

--- packages/libgmime/build.sh
+++ packages/libgmime/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="MIME message parser and creator"
 TERMUX_PKG_VERSION=3.2.2
 TERMUX_PKG_SHA256=613014ffcd327b3f6b06aa45cf4bf3adc25cd831f14537c4132880022dda7209
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gmime/${TERMUX_PKG_VERSION:0:3}/gmime-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_DEPENDS="glib, libidn"
+TERMUX_PKG_DEPENDS="glib, libidn2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_have_iconv_detect_h=yes
 --disable-glibtest
