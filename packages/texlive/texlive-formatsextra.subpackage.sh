ed69e2bec5e3e09a7bbab7fc0e49f6b133ff8316
Henrik Grimler <henrik@grimler.se>
2018-11-17 14:07:34 +0100

    texlive-full: add dependency for subpackages

--- packages/texlive/texlive-formatsextra.subpackage.sh
+++ packages/texlive/texlive-formatsextra.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-formatsextra"
-TERMUX_SUBPKG_DEPENDS="texlive"
+TERMUX_SUBPKG_DEPENDS="texlive, texlive-langcyrillic, texlive-mathscience, texlive-fontsrecommended, texlive-plaingeneric"
 TERMUX_SUBPKG_INCLUDE=$(python3 $TERMUX_PKG_BUILDER_DIR/parse_tlpdb.py $(echo $SUB_PKG_NAME | awk -F"-" '{print $2}') $TERMUX_PKG_TMPDIR/texlive.tlpdb)
