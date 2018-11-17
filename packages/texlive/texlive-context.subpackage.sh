6372d9a1addfd57c4301741f836bccf0947f8410
Henrik Grimler <henrik@grimler.se>
2018-11-17 14:07:34 +0100

    texlive-full: add dependency for subpackages

--- packages/texlive/texlive-context.subpackage.sh
+++ packages/texlive/texlive-context.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-context"
-TERMUX_SUBPKG_DEPENDS="texlive"
+TERMUX_SUBPKG_DEPENDS="texlive, texlive-mathscience, texlive-fontsrecommended, texlive-metapost, texlive-xetex"
 TERMUX_SUBPKG_INCLUDE=$(python3 $TERMUX_PKG_BUILDER_DIR/parse_tlpdb.py $(echo $SUB_PKG_NAME | awk -F"-" '{print $2}') $TERMUX_PKG_TMPDIR/texlive.tlpdb)
--- packages/texlive/texlive-langcyrillic.subpackage.sh
+++ packages/texlive/texlive-langcyrillic.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-langcyrillic"
-TERMUX_SUBPKG_DEPENDS="texlive"
+TERMUX_SUBPKG_DEPENDS="texlive, texlive-fontsextra, texlive-fontsrecommended, texlive-langgreek, texlive-latexrecommended"
 TERMUX_SUBPKG_INCLUDE=$(python3 $TERMUX_PKG_BUILDER_DIR/parse_tlpdb.py $(echo $SUB_PKG_NAME | awk -F"-" '{print $2}') $TERMUX_PKG_TMPDIR/texlive.tlpdb)
