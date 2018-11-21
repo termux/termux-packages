ef6b080214953f2eb35ff3e201ce7d7ba66cc0e1
Fredrik Fornwall <fredrik@fornwall.net>
2018-11-22 00:16:00 +0100

    libpixman: Update from 0.34.0 to 0.36.0

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.pixman.org/
 TERMUX_PKG_DESCRIPTION="Low-level library for pixel manipulation"
-TERMUX_PKG_VERSION=0.34.0
+TERMUX_PKG_VERSION=0.36.0
+TERMUX_PKG_SHA256=1ca19c8d4d37682adfbc42741d24977903fec1169b4153ec05bb690d4acf9fae
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=21b6b249b51c6800dc9553b65106e1e37d0e25df942c90531d4c3997aa20a88e
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
 
 termux_step_pre_configure () {
