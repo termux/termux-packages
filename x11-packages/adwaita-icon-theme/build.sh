ad7759da3fe6991902191a372df28d9e7d5a4849
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-11-23 23:20:36 +0200

    adwaita-icon-theme: update to 3.30.1

--- x11-packages/adwaita-icon-theme/build.sh
+++ x11-packages/adwaita-icon-theme/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://www.gnome.org
 TERMUX_PKG_DESCRIPTION="Freedesktop.org Hicolor icon theme"
-TERMUX_PKG_VERSION=3.30.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.30.1
 TERMUX_PKG_SRCURL=https://github.com/GNOME/adwaita-icon-theme/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=14b70916d763f841865c295b4d6dd0aebba6f0829a29c019348d0f775178aeba
+TERMUX_PKG_SHA256=3b4a5e97f5583d7493cbba4ced00654c34d60006f4efed69be49f3fefadb982f
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_DEPENDS="hicolor-icon-theme"
 TERMUX_PKG_RM_AFTER_INSTALL="share/icons/Adwaita/icon-theme.cache"
