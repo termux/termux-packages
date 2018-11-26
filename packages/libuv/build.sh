a6c32998a146114f2f723656247aa8d99f56d164
Fredrik Fornwall <fredrik@fornwall.net>
2018-11-27 00:23:39 +0100

    libuv: Update from 1.23.2 to 1.24.0

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
-TERMUX_PKG_VERSION=1.23.2
-TERMUX_PKG_SHA256=8ab472ed816bb70c147ddbdfa7b9becfa10921df8a03faf2c87a54eac642c355
+TERMUX_PKG_VERSION=1.24.0
+TERMUX_PKG_SHA256=380532974a82a9d4dbc9ab3863922edd08aa6f03fcae79e3a5bda2ecf407ef06
 TERMUX_PKG_SRCURL=https://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_pre_configure () {
