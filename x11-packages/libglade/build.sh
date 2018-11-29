dc3c1798baa5b27c5a1056421e0ff05ef4c981cf
Leonid Pliushch <leonid.pliushch@gmail.com>
2018-11-29 16:06:59 +0200

    libglade: fix build error

--- x11-packages/libglade/build.sh
+++ x11-packages/libglade/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.gnome.org/
 TERMUX_PKG_DESCRIPTION="Allows you to load glade interface files in a program at runtime"
 TERMUX_PKG_VERSION=2.6.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libglade/2.6/libglade-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=64361e7647839d36ed8336d992fd210d3e8139882269bed47dc4674980165dec
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libcairo-x, libxml2, pango-x"
@@ -10,6 +11,10 @@ TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc"
 
 # For libglade-convert.
 TERMUX_PKG_SUGGESTS="python2"
+
+termux_step_pre_configure() {
+    export LIBS="-lgmodule-2.0"
+}
 
 termux_step_post_make_install() {
     sed \
