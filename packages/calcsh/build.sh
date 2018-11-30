0e7552be930b7c3060bef1f758a6a4857cb0bed5
dukealexanderthefirst <dukealexanderthefirst@gmail.com>
2018-11-30 08:20:59 +0100

    added a shasum

--- packages/calcsh/build.sh
+++ packages/calcsh/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dukealexanderthefirst/calcsh
 TERMUX_PKG_DESCRIPTION="calculator in a shell"
 TERMUX_PKG_VERSION=0.1
-TERMUX_PKG_SHA256=""
+TERMUX_PKG_SHA256="09334c6718da4a8e544b54c5f4e8c40ca5555bb65fc76af047480a53e437a050"
 TERMUX_PKG_SRCURL=https://github.com/dukealexanderthefirst/calcsh/archive/calcsh-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="python"
 TERMUX_PKG_BUILD_IN_SRC=yes
