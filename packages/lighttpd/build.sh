dab28877493bcc6519977afce6e6e39f69c34bb3
Fredrik Fornwall <fredrik@fornwall.net>
2018-11-30 01:42:52 +0100

    lighttpd: Update from 1.4.51 to 1.4.52

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.lighttpd.net
 TERMUX_PKG_DESCRIPTION="Fast webserver with minimal memory footprint"
-TERMUX_PKG_VERSION=1.4.51
-TERMUX_PKG_SHA256=2af9fdb265d1f025bfa634e13770239712ecbd585e4975b8226edf1df74e9c82
+TERMUX_PKG_VERSION=1.4.52
+TERMUX_PKG_SHA256=27bc0991c530b7c6335e6efff2181934d3c1a1c516f7401ea71d8302cefda764
 TERMUX_PKG_SRCURL=http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
 TERMUX_PKG_DEPENDS="libbz2, openssl, pcre, libcrypt, libandroid-glob"
