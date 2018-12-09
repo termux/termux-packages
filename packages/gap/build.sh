04b179adebd1f8c0a6e6a8c7b65997f057f7f23a
Henrik Grimler <henrik@grimler.se>
2018-12-09 14:02:22 +0100

    gap: add package

---
+++ packages/gap/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://www.gap-system.org/
+TERMUX_PKG_DESCRIPTION="GAP is a system for computational discrete algebra, with particular emphasis on Computational Group Theory"
+TERMUX_PKG_VERSION=4.10.0
+TERMUX_PKG_SHA256=314b8d949c0bb01ef8bee199b29571d42714df660aba0d76ded254f58295904f
+TERMUX_PKG_SRCURL=https://www.gap-system.org/pub/gap/gap-${TERMUX_PKG_VERSION:0:4}/tar.gz/gap-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="readline, libgmp"
