4fc09e327149480fc49bc5facea280cac14701de
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-10 01:18:07 +0100

    rust: Update from 1.30.1 to 1.31.0

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -1,27 +1,28 @@
 TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
-TERMUX_PKG_VERSION=1.30.1
+TERMUX_PKG_VERSION=1.31.0
+TERMUX_PKG_SHA256=9581c5673937f8b1c3c21060ef7c1fcd3e5574a0fc0b27e1888cb37c4b2ec393
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
-TERMUX_PKG_SHA256=76c824e26b6a554cd48f45993e42001816c2f515dd1b252e3c8315fe200155ae
 TERMUX_PKG_DEPENDS="clang, openssl, lld"
 
 termux_step_configure () {
 	termux_setup_cmake
 	termux_setup_rust
+
 	# it breaks building rust tools without doing this because it tries to find
-	# ../lib from bin location
+	# ../lib from bin location:
 	export PATH=$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin:$PATH
 	
 	local RUSTC=$(which rustc)
-        local CARGO=$(which cargo)
+	local CARGO=$(which cargo)
 
 	sed "s%\\@TERMUX_PREFIX\\@%$TERMUX_PREFIX%g" \
 		$TERMUX_PKG_BUILDER_DIR/config.toml \
 		| sed "s%\\@TERMUX_STANDALONE_TOOLCHAIN\\@%$TERMUX_STANDALONE_TOOLCHAIN%g" \
 		| sed "s%\\@triple\\@%$CARGO_TARGET_NAME%g" \
 		| sed "s%\\@RUSTC\\@%$RUSTC%g" \
-                | sed "s%\\@CARGO\\@%$CARGO%g" \
+		| sed "s%\\@CARGO\\@%$CARGO%g" \
 		> config.toml
 
 	local env_host=`printf $CARGO_TARGET_NAME | tr a-z A-Z | sed s/-/_/g`
@@ -30,13 +31,15 @@ termux_step_configure () {
 	export ${env_host}_OPENSSL_DIR=$TERMUX_PREFIX
 	export X86_64_UNKNOWN_LINUX_GNU_OPENSSL_LIB_DIR=/usr/lib/x86_64-linux-gnu
 	export X86_64_UNKNOWN_LINUX_GNU_OPENSSL_INCLUDE_DIR=/usr/include
-	
+
 	unset CC CXX CPP LD CFLAGS CXXFLAGS CPPFLAGS LDFLAGS PKG_CONFIG
 }
 
 termux_step_make () {
-	$TERMUX_PKG_SRCDIR/x.py dist --host $CARGO_TARGET_NAME --target $CARGO_TARGET_NAME --target wasm32-unknown-unknown 
-
+	$TERMUX_PKG_SRCDIR/x.py dist \
+		--host $CARGO_TARGET_NAME \
+		--target $CARGO_TARGET_NAME \
+		--target wasm32-unknown-unknown
 }
 
 termux_step_make_install () {
@@ -44,8 +47,13 @@ termux_step_make_install () {
 		$TERMUX_PREFIX/lib/rustlib/manifest-rust-analysis-x86_64-unknown-linux-gnu \
 		$TERMUX_PREFIX/lib/rustlib/manifest-rust-std-x86_64-unknown-linux-gnu"
 
-	$TERMUX_PKG_SRCDIR/x.py install --host $CARGO_TARGET_NAME --target $CARGO_TARGET_NAME --target wasm32-unknown-unknown  && rm -rf $host_files_to_remove
+	$TERMUX_PKG_SRCDIR/x.py install \
+		--host $CARGO_TARGET_NAME \
+		--target $CARGO_TARGET_NAME \
+		--target wasm32-unknown-unknown && \
+		rm -rf $host_files_to_remove
+
 	cd "$TERMUX_PREFIX/lib"
-        ln -sf rustlib/$CARGO_TARGET_NAME/lib/*.so .
+	ln -sf rustlib/$CARGO_TARGET_NAME/lib/*.so .
 	ln -sf $TERMUX_PREFIX/bin/lld $TERMUX_PREFIX/bin/rust-lld
 }
