8fff40c4f630ba6aece3db640a24511238461b03
Henrik Grimler <henrik@grimler.se>
2018-12-17 08:21:40 +0100

    apt: add apt-ftparchive and libapt-pkg subpackages

---
+++ packages/apt/apt-ftparchive.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_DESCRIPTION="apt-ftparchive is the command line tool that generates the index files that APT uses to access a distribution source"
+TERMUX_SUBPKG_DEPENDS="libdb"
+TERMUX_SUBPKG_INCLUDE="
+bin/apt-ftparchive
+"
