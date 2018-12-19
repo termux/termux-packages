102645ac6da8f55cdb24035e4227ee6dbb0e8188
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-19 01:12:03 +0100

    libassuan: Update from 2.5.1 to 2.5.2

--- packages/libassuan/build.sh
+++ packages/libassuan/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/libassuan/
 TERMUX_PKG_DESCRIPTION="Library implementing the Assuan IPC protocol used between most newer GnuPG components"
-TERMUX_PKG_VERSION=2.5.1
-TERMUX_PKG_SHA256=47f96c37b4f2aac289f0bc1bacfa8bd8b4b209a488d3d15e2229cb6cc9b26449
+TERMUX_PKG_VERSION=2.5.2
+TERMUX_PKG_SHA256=986b1bf277e375f7a960450fbb8ffbd45294d06598916ad4ebf79aee0cb788e7
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/libassuan-config"
 TERMUX_PKG_DEPENDS="libgpg-error"
