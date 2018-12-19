6f7a24df6e8c5338ea3656323257dee3aaf2144b
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-19 01:36:44 +0100

    jhead: Update from 3.00 to 3.02

--- packages/jhead/build.sh
+++ packages/jhead/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.sentex.net/~mwandel/jhead/
 TERMUX_PKG_DESCRIPTION="Exif Jpeg header manipulation tool"
-TERMUX_PKG_VERSION=3.00
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.02
+TERMUX_PKG_SHA256=85c9737c3dcc84b440a67307de9f76cbee7610697bfaf4877dd46afd71c7ed2a
 TERMUX_PKG_SRCURL=http://www.sentex.net/~mwandel/jhead/jhead-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=88cc01da018e242fe2e05db73f91b6288106858dd70f27506c4989a575d2895e
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_make_install () {
