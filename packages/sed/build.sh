1c78066b61917295ddd6dabdc237a79c12cf70f6
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-24 00:03:11 +0100

    sed: Build with -D__USE_FORTIFY_LEVEL=2

--- packages/sed/build.sh
+++ packages/sed/build.sh
@@ -5,6 +5,10 @@ TERMUX_PKG_SHA256=2885768cd0a29ff8d58a6280a270ff161f6a3deb5690b2be6c49f46d4c67bd
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/sed/sed-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
+termux_step_pre_configure() {
+	CFLAGS+=" -D__USE_FORTIFY_LEVEL=2"
+}
+
 termux_step_post_configure () {
 	touch -d "next hour" $TERMUX_PKG_SRCDIR/doc/sed.1
 }
