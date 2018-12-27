48ce9bbf6a0dd04246dabba4d2cc5f22c20a0e32
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-28 00:56:19 +0100

    strace: Update from 4.25 to 4.26

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
-TERMUX_PKG_VERSION=4.25
-TERMUX_PKG_SHA256=d685f8e65470b7832c3aff60c57ab4459f26ff89f07c10f92bd70ee89efac701
+TERMUX_PKG_VERSION=4.26
+TERMUX_PKG_SHA256=7c4d2ffeef4f7d1cdc71062ca78d1130eb52f947c2fca82f59f6a1183bfa1e1c
 TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
