d993690cdb657f3317f12e8edbf17ae499943d5b
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-28 10:02:06 +0100

    gmic: Update from 2.4.2 to 2.4.3

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
-TERMUX_PKG_VERSION=2.4.2
-TERMUX_PKG_SHA256=0fae452232501ed7e50bbe2eaa96900a5b6daa7fb1d4366d207311bdc27d0762
+TERMUX_PKG_VERSION=2.4.3
+TERMUX_PKG_SHA256=eaa263854cb6e88ee5d2704ac97cb891191bd03fd157cea211f98aff1c96ae5a
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="graphicsmagick++, libcurl, fftw, libpng, libjpeg-turbo, libtiff"
 TERMUX_PKG_BUILD_DEPENDS="graphicsmagick-dev"
