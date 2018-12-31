12a053a1476c3072e45fe5540d6439cc700831c1
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-31 02:41:44 +0100

    librsvg: Depend on libcairo-gobject

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_VERSION=2.45.2
 TERMUX_PKG_SHA256=c0a7bc5fc3944a172f95fc3c2be024f7d00fc7eca7e8724f19b7545f77503e34
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_DEPENDS="libcroco,pango,gdk-pixbuf"
+TERMUX_PKG_DEPENDS="libcroco,pango,gdk-pixbuf,libcairo-gobject"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-introspection --disable-pixbuf-loader"
 
 termux_step_pre_configure() {
