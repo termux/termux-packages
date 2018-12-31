78e88101d9a51aa0703ce6293279c141a028e5d1
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-31 03:01:48 +0100

    privoxy: Update from 3.0.26 to 3.0.27

--- packages/privoxy/build.sh
+++ packages/privoxy/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.privoxy.org
 TERMUX_PKG_DESCRIPTION="Non-caching web proxy with advanced filtering capabilities"
-TERMUX_PKG_VERSION=3.0.26
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.0.27
+TERMUX_PKG_SHA256=67540360e28c881c892481d1a224632f5b58569e003285467fea6e4b59316378
 TERMUX_PKG_SRCURL=https://www.privoxy.org/sf-download-mirror/Sources/$TERMUX_PKG_VERSION%20%28stable%29/privoxy-$TERMUX_PKG_VERSION-stable-src.tar.gz
-TERMUX_PKG_SHA256=57e415b43ee5dfdca74685cc034053eaae962952fdabd086171551a86abf9cd8
 TERMUX_PKG_CONFFILES='etc/privoxy/config etc/privoxy/match-all.action etc/privoxy/trust etc/privoxy/user.action etc/privoxy/user.filter etc/privoxy/default.action etc/privoxy/default.filter'
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-dynamic-pcre --sysconfdir=$TERMUX_PREFIX/etc/privoxy"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--sysconfdir=$TERMUX_PREFIX/etc/privoxy"
+TERMUX_PKG_DEPENDS="pcre"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_KEEP_SHARE_DOC=yes
 
