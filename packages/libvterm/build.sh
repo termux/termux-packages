cf95540964359c546f344cd1ad07e2f3a9d66686
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-03 00:48:17 +0100

    libvterm: Update to latest git master

--- packages/libvterm/build.sh
+++ packages/libvterm/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://libvterm.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Terminal emulator library"
 # libvterm does not do releases, take a specific commit for now:
-TERMUX_PKG_VERSION=18.02.20
-_COMMIT=005845cd58ca409a970d822b74e1a02a503d32e7
-TERMUX_PKG_SHA256=2424ee587bc13910894ade21ba75649d558b8c161fa1eb5ed69802ecca47b516
+TERMUX_PKG_VERSION=18.12.22
+_COMMIT=26c3468c51d1c8f1a26aa1252783b04845ccc9f9
+TERMUX_PKG_SHA256=ea0abe90cdeac7c4141933540dc5868bc4ef4004d135e798716f8c9021e1951f
 TERMUX_PKG_SRCURL=https://github.com/neovim/libvterm/archive/$_COMMIT.zip
 TERMUX_PKG_BUILD_IN_SRC=yes
 
