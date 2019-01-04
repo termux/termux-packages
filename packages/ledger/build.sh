8305099a6a658c524a1caafdd5d2e6d7c483617d
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-04 22:35:14 +0100

    ledger: Bump revision after patch

--- packages/ledger/build.sh
+++ packages/ledger/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.ledger-cli.org
 TERMUX_PKG_DESCRIPTION="Powerful, double-entry accounting system"
 TERMUX_PKG_VERSION=3.1.1
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://github.com/ledger/ledger/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=90f06561ab692b192d46d67bc106158da9c6c6813cc3848b503243a9dfd8548a
 TERMUX_PKG_DEPENDS="boost, libedit, libmpfr, utf8cpp, libgmp"
