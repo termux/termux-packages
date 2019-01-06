4c0686af0412a7f764f91d6a2e9840726f4bf2e6
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-07 00:43:24 +0100

    vttest: Update from 20180911 to 20190105

--- packages/vttest/build.sh
+++ packages/vttest/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://invisible-island.net/vttest/
 TERMUX_PKG_DESCRIPTION="Program for testing the VT100 compatibility of terminal emulators"
-TERMUX_PKG_VERSION=20180911
-TERMUX_PKG_SHA256=91571f0a9df6220c6fc3da74bd017abbf472f2ccd2e102143cfbe960badb2c6b
+TERMUX_PKG_VERSION=20190105
+TERMUX_PKG_SHA256=5703ef4783c8eb90b30205740c585652fa779ea28bdd876a2af81af6b9574f71
 # vttest does not use a version in the tar URL, but we will detect
 # an update with a checksum mismatch.
 TERMUX_PKG_SRCURL=http://invisible-island.net/datafiles/release/vttest.tar.gz
