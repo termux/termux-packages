44462482af60537985b98ce73111593ef5f07cc3
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-09 01:35:04 +0200

    i3: rebuild due to updated libev dependency

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://i3wm.org/
 TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_VERSION=4.16
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=cc60a5f518c5e37b4143b644a840bb2ad2a63ea7a771954c332cd5637db655b4
 TERMUX_PKG_DEPENDS="glib, i3status, libandroid-glob, libandroid-shmem, libcairo-x, libev, libposix-shm, libxcb, libxkbcommon, pango-x, pcre, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
