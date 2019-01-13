fe6c4f817144c98efa351b21067ddd135242381c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-13 13:19:08 +0200

    unzip: apply patches to fix some CVEs
    Unzip was not updated from on 20 April 2009 but contain serious
    issues already fixed in Linux distributions.
    
    Termux should implement these fixes as well.
    
    This will fix:
    
     * CVE-2014-8139
     * CVE-2014-8140
     * CVE-2014-8141
     * CVE-2014-9636
     * CVE-2015-7696
     * CVE-2015-7697
    
    Patches were obtained from https://git.archlinux.org/svntogit/packages.git/tree/trunk?h=packages/unzip.

--- packages/unzip/build.sh
+++ packages/unzip/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.info-zip.org/
 TERMUX_PKG_DESCRIPTION="Tools for working with zip files"
 TERMUX_PKG_VERSION=6.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/infozip/unzip60.tar.gz
 TERMUX_PKG_SHA256=036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37
 TERMUX_PKG_DEPENDS="libandroid-support, libbz2"
