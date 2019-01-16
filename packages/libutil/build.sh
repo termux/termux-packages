cfe33c5469ecc4dd1b900469b35814404893990f
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-16 23:05:32 +0100

    libutil: Bump version after TCSAFLUSH change

--- packages/libutil/build.sh
+++ packages/libutil/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://refspecs.linuxbase.org/LSB_2.1.0/LSB-generic/LSB-generic/libutil.html
 TERMUX_PKG_DESCRIPTION="Library with terminal functions"
-TERMUX_PKG_VERSION=0.3
+TERMUX_PKG_VERSION=0.4
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_make_install () {
