2879d3945ee299a4760b7849a6d5c0e689f8c012
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-18 01:09:04 +0200

    libcap: update to 2.26

--- x11-packages/libcap/build.sh
+++ x11-packages/libcap/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://sites.google.com/site/fullycapable/
 TERMUX_PKG_DESCRIPTION="POSIX 1003.1e capabilities"
-TERMUX_PKG_VERSION=2.25
+TERMUX_PKG_VERSION=2.26
 TERMUX_PKG_SRCURL=https://kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=693c8ac51e983ee678205571ef272439d83afe62dd8e424ea14ad9790bc35162
+TERMUX_PKG_SHA256=b630b7c484271b3ba867680d6a14b10a86cfa67247a14631b14c06731d5a458b
 TERMUX_PKG_DEPENDS="attr"
 TERMUX_PKG_BUILD_IN_SRC=true
 
