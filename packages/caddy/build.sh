da0bef9b045dc2d348a9012ff89613f1bd56dd78
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-18 18:28:24 +0200

    new package: caddy
    Fast, cross-platform HTTP/2 web server.

---
+++ packages/caddy/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://caddyserver.com/
+TERMUX_PKG_DESCRIPTION="Fast, cross-platform HTTP/2 web server"
+TERMUX_PKG_VERSION=0.11.2
+TERMUX_PKG_SRCURL=https://github.com/mholt/caddy/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=61779a09959bf6a0e7007e8ff5c2a94811dd12b7628166cb31e9648a97c0e75b
+
+termux_step_make() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+
+	mkdir -p $GOPATH/src/github.com/mholt/
+	cp -a $TERMUX_PKG_SRCDIR $GOPATH/src/github.com/mholt/caddy
+
+	cd $GOPATH/src/github.com/mholt/caddy/caddy
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 $GOPATH/src/github.com/mholt/caddy/caddy/caddy \
+		$TERMUX_PREFIX/bin/caddy
+}
