e4dc4d2ddab9515ef36188bf99cf0fa22a91450c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-19 19:12:07 +0200

    pango-x: add manpage for "pango-view"

--- x11-packages/pango-x/build.sh
+++ x11-packages/pango-x/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text (with X)"
 TERMUX_PKG_VERSION=1.42.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d
 
@@ -12,3 +13,8 @@ TERMUX_PKG_DEVPACKAGE_DEPENDS="fontconfig-dev, harfbuzz-dev, libcairo-x-dev, lib
 TERMUX_PKG_PROVIDES="pango"
 TERMUX_PKG_REPLACES="${TERMUX_PKG_PROVIDES}"
 TERMUX_PKG_CONFLICTS="${TERMUX_PKG_PROVIDES}, pango-dev"
+
+termux_step_post_make_install() {
+    install -Dm600 $TERMUX_PKG_BUILDER_DIR/pango-view.1 \
+        $TERMUX_PREFIX/share/man/man1/pango-view.1
+}
