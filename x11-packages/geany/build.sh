a1eac3880c0357ee656e99658b9b77567aaac383
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-19 20:15:09 +0200

    geany: update to 1.34.1

--- x11-packages/geany/build.sh
+++ x11-packages/geany/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://www.geany.org/
 TERMUX_PKG_DESCRIPTION="Fast and lightweight IDE"
-TERMUX_PKG_VERSION=1.33.0
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=1.34.1
 TERMUX_PKG_SRCURL=https://download.geany.org/geany-${TERMUX_PKG_VERSION/.0}.tar.bz2
-TERMUX_PKG_SHA256=66baaff43f12caebcf0efec9a5533044dc52837f799c73a1fd7312caa86099c2
+TERMUX_PKG_SHA256=e765efd89e759defe3fd797d8a2052afbb4b23522efbcc72e3a72b7f1093ec11
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, fribidi, gdk-pixbuf, glib, gtk3, harfbuzz, libandroid-glob, libandroid-shmem, libandroid-support, libbz2, libc++, libcairo-x, libffi, libgraphite, liblzma, libpixman, libpng, libuuid, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, libxrender, pango-x, pcre"
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk3"
