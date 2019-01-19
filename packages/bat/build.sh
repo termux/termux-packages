6e50c5c6eee13c778269fbbed91bddf1f64b5c12
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-19 09:58:17 +0100

    bat: Depend on less (fixes #2842)

--- packages/bat/build.sh
+++ packages/bat/build.sh
@@ -1,10 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/bat
 TERMUX_PKG_DESCRIPTION="A cat(1) clone with wings"
 TERMUX_PKG_VERSION=0.9.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=4ce9c118cf5da1159a882dea389f3c5737b5d98192e9a619b0fe8c1730341cc6
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/bat/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
+# bat calls less with '--RAW-CONTROL-CHARS' which busybox less does not support:
+TERMUX_PKG_DEPENDS="less"
 
 termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/share/man/man1
