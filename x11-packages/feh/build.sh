3ec85cc6efae572118753c97f0de8850dd1a0edc
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-19 20:07:14 +0200

    feh: update to 3.1.1

--- x11-packages/feh/build.sh
+++ x11-packages/feh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 
 TERMUX_PKG_HOMEPAGE=https://feh.finalrewind.org/
 TERMUX_PKG_DESCRIPTION="Fast and light imlib2-based image viewer"
-TERMUX_PKG_VERSION=3.0
+TERMUX_PKG_VERSION=3.1.1
 TERMUX_PKG_SRCURL=https://feh.finalrewind.org/feh-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=b67b4e5c6e1fb45dd2a4567e395b413c7565246db6780a46fb1bcdf33d72dc01
+TERMUX_PKG_SHA256=61d0242e3644cf7c5db74e644f0e8a8d9be49b7bd01034265cc1ebb2b3f9c8eb
 TERMUX_PKG_DEPENDS="imlib2, libandroid-shmem, libcurl, libexif, libpng, libx11, libxinerama"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
 TERMUX_PKG_BUILD_IN_SRC=true
