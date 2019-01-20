8d260e5ea5fae6cfc302ebdc59808ea1ff5877b0
Auxilus <raghavsphadke@gmail.com>
2019-01-20 09:06:03 +0530

    add libgnutls as dependency

--- root-packages/tshark/build.sh
+++ root-packages/tshark/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_DESCRIPTION="Network protocol analyzer and sniffer"
 TERMUX_PKG_VERSION=2.6.5
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/all-versions/wireshark-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=93155b798544b2f07693920f4ac1b531c952965ee4eb1d98419961240177438a
-TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, openssl"
+TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, libgnutls, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-qt=no
 --disable-wireshark
 --with-lua=no
