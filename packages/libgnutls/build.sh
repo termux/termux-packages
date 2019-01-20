a3fb891637b28fa48232cba9cf7b2e2afdebdb02
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-20 19:19:05 +0100

    libgnutls: Update from 3.5.19 to 3.6.5

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn2, libunistring"
-TERMUX_PKG_VERSION=3.5.19
-TERMUX_PKG_SHA256=1936eb64f03aaefd6eb16cef0567457777618573826b94d03376bb6a4afadc44
+TERMUX_PKG_VERSION=3.6.5
+TERMUX_PKG_SHA256=073eced3acef49a3883e69ffd5f0f0b5f46e2760ad86eddc6c0866df4e7abb35
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnutls/v${TERMUX_PKG_VERSION:0:3}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libidn2-dev, libnettle-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
