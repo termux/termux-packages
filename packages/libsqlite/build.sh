a41bea41e8b4c671eb3276bbd263ed91043f74e4
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-20 19:22:20 +0100

    libsqlite: Update from 3.25.3 to 3.26.0

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.sqlite.org
 TERMUX_PKG_DESCRIPTION="Library implementing a self-contained and transactional SQL database engine"
 # Note: Updating this version requires bumping the tcl package as well.
 _SQLITE_MAJOR=3
-_SQLITE_MINOR=25
-_SQLITE_PATCH=3
-TERMUX_PKG_SHA256=00ebf97be13928941940cc71de3d67e9f852698233cd98ce2d178fd08092f3dd
+_SQLITE_MINOR=26
+_SQLITE_PATCH=0
+TERMUX_PKG_SHA256=5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
 TERMUX_PKG_SRCURL=https://www.sqlite.org/2018/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 # ac_cv_func_strerror_r=no as strerror_r() with the
