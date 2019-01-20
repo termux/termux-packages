bd36177bbfd539a46bb6274c079d752f114a26c4
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-20 22:55:31 +0100

    libgc: Update from 7.6.8 to 8.0.2

--- packages/libgc/build.sh
+++ packages/libgc/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.hboehm.info/gc/
 TERMUX_PKG_DESCRIPTION="Library providing the Boehm-Demers-Weiser conservative garbage collector"
-TERMUX_PKG_VERSION=(7.6.8
-                    7.6.6)
-TERMUX_PKG_SHA256=(040ac5cdbf1bebc7c8cd4928996bbae0c54497c151ea5639838fa0128102e258
-                   99feabc5f54877f314db4fadeb109f0b3e1d1a54afb6b4b3dfba1e707e38e074)
+TERMUX_PKG_VERSION=(8.0.2
+                    7.6.8)
+TERMUX_PKG_SHA256=(4e8ca4b5b72a3a27971daefaa9b621f0a716695b23baa40b7eac78de2eeb51cb
+                   1d6a279edf81767e74d2ad2c9fce09459bc65f12c6525a40b0cb3e53c089f665)
 TERMUX_PKG_SRCURL=(https://github.com/ivmai/bdwgc/releases/download/v$TERMUX_PKG_VERSION/gc-$TERMUX_PKG_VERSION.tar.gz
 		   https://github.com/ivmai/libatomic_ops/releases/download/v${TERMUX_PKG_VERSION[1]}/libatomic_ops-${TERMUX_PKG_VERSION[1]}.tar.gz)
 TERMUX_PKG_RM_AFTER_INSTALL="share/gc"
