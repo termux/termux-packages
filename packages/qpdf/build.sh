8e268c3f35364e7f746b6aee8c9861dbd6b56c11
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-20 20:15:39 +0100

    qpdf: Update from 8.2.1 to 8.3.0

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Content-Preserving PDF Transformation System"
-TERMUX_PKG_VERSION=8.2.1
-TERMUX_PKG_SHA256=f61319dbb44ca45f3a200bd59e8a86a906b61e2d223b104d9d2fbddcab9100ab
+TERMUX_PKG_VERSION=8.3.0
+TERMUX_PKG_SHA256=8cc02ebf69231fde3d2e19dda383ee3f2b2cb5f4afdff7386c3d9c1a8cb863d2
 TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/archive/release-qpdf-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libjpeg-turbo"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
@@ -9,5 +9,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-random=/dev/urandom"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
-	autoreconf -fi
+	./autogen.sh
 }
