7220eb7a444cdf1a675e043230cf0674be5d5fb0
Henrik Grimler <henrik@grimler.se>
2019-01-21 17:52:19 +0100

    mtools: --disable-floppyd to avoid build error

--- packages/mtools/build.sh
+++ packages/mtools/build.sh
@@ -5,4 +5,7 @@ TERMUX_PKG_VERSION=4.0.23
 TERMUX_PKG_SHA256=f188db26751aeb5692a79b2380b440ecc05fd1848a52f869d7ca1193f2ef8ee3
 TERMUX_PKG_SRCURL=ftp://ftp.gnu.org/gnu/mtools/mtools-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libandroid-support"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_bsd_main=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-floppyd
+ac_cv_lib_bsd_main=no
+"
