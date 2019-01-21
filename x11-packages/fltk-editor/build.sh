c6cbc81217c427b68ee3bc07589ed5c1524812cd
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-21 21:06:50 +0200

    fltk-editor: bump revision due to added homepage

--- x11-packages/fltk-editor/build.sh
+++ x11-packages/fltk-editor/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_DEPENDS="fltk, libc++"
 TERMUX_PKG_VERSION=1.0-termux
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 
 termux_step_make() {
 	"${CXX}" \
