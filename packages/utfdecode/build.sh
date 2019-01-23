5fb7260a317a39fb2a77e22f8ce7550be17d0f68
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-23 01:00:41 +0100

    utfdecode: Mark with Apache-2.0 license

--- packages/utfdecode/build.sh
+++ packages/utfdecode/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/fornwall/utfdecode
 TERMUX_PKG_DESCRIPTION="Tool for decoding, encoding and debugging unicode"
-TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=0.3.4
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=5fd1473f6cf6bdedeb33569c3b0f6823aceda366c714f6a475e2d28767f99b5a
