3ab4c8f8e09b1811df15360f6edc3fd1c5a4e0ea
Fredrik Fornwall <fredrik@fornwall.net>
2019-01-25 01:58:51 +0100

    Remove langinfo.h from libandroid-support
    Instead provide langinfo.h with inline implemention of nl_langinfo()
    as a system header.
    
    This avoids linking in libandroid-support.so in some places, and
    improves compatibility when building code on-device.

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -1,14 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=22
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=667f20d0821a6305c50c667363486d546b293e846f31d02f559947d50121f51e
+TERMUX_PKG_VERSION=23
+TERMUX_PKG_SHA256=9683eccb6da260715babd56c8f2b8e48b21caf3f6e878bd80c631f3b9d76e13f
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_ESSENTIAL=yes
 
 termux_step_make_install () {
+	# Remove file previously in package to avoid this being left
+	# in build environments.
+	rm -f $TERMUX_PREFIX/include/langinfo.h
+
 	_C_FILES="src/musl-*/*.c"
 	$CC $CFLAGS -std=c99 -DNULL=0 $CPPFLAGS $LDFLAGS \
 		-Iinclude \
