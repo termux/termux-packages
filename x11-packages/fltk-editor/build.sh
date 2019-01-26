360b58097f765467605f4d35bf7571362ba755e4
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-26 23:57:02 +0200

    fltk, fltk-editor: use https for URLs

--- x11-packages/fltk-editor/build.sh
+++ x11-packages/fltk-editor/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://www.fltk.org/
+TERMUX_PKG_HOMEPAGE=https://www.fltk.org/
 TERMUX_PKG_DESCRIPTION="Simple text editor"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_DEPENDS="fltk, libc++"
 TERMUX_PKG_VERSION=1.0-termux
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_REVISION=8
 
 termux_step_make() {
 	"${CXX}" \
