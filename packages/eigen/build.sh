8d11634f792fa6077f6b813fc1a51630c6cad935
Henrik Grimler <henrik@grimler.se>
2019-01-26 02:26:51 +0100

    eigen: update to 3.3.7

--- packages/eigen/build.sh
+++ packages/eigen/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://eigen.tuxfamily.org
 TERMUX_PKG_DESCRIPTION="Eigen is a C++ template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms"
 TERMUX_PKG_LICENSE="MPL-2.0"
-TERMUX_PKG_VERSION=3.3.5
-TERMUX_PKG_SHA256=7352bff3ea299e4c7d7fbe31c504f8eb9149d7e685dec5a12fbaa26379f603e2
+TERMUX_PKG_VERSION=3.3.7
+TERMUX_PKG_SHA256=9f13cf90dedbe3e52a19f43000d71fdf72e986beb9a5436dddcd61ff9d77a3ce
 TERMUX_PKG_SRCURL=http://bitbucket.org/eigen/eigen/get/${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=Release"
