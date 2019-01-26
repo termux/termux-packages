a172e997811849cec2736d3b101a791c2170fbe0
Henrik Grimler <henrik@grimler.se>
2019-01-26 02:26:06 +0100

    netcdf-cxx: add missing dependency netcdf-c

--- packages/netcdf-cxx/build.sh
+++ packages/netcdf-cxx/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=4.3.0
 TERMUX_PKG_SHA256=25da1c97d7a01bc4cee34121c32909872edd38404589c0427fefa1301743f18f
 TERMUX_PKG_SRCURL=https://github.com/Unidata/netcdf-cxx4/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libhdf5, libcurl, openssl, libnghttp2"
+TERMUX_PKG_DEPENDS="libhdf5, libcurl, openssl, libnghttp2, netcdf-c"
