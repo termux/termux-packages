bf4e4a64895df2c5c67d3d483d04ada2c64f7042
Henrik Grimler <henrik@grimler.se>
2019-01-26 02:00:18 +0100

    open-adventure: update to 1.7

--- packages/open-adventure/build.sh
+++ packages/open-adventure/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.catb.org/~esr/open-adventure/
 TERMUX_PKG_DESCRIPTION="Forward-port of the original Colossal Cave Adventure from 1976-77 which was the origin of all later text adventures, dungeon-crawl (computer) games, and computer-hosted roleplaying games"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.4
-TERMUX_PKG_SHA256=7bb027a1f8076cf4d4803e8b2d9fda1934e95ea4385c3ace9ed2e571dc4e0f02
+TERMUX_PKG_VERSION=1.7
+TERMUX_PKG_SHA256=6b2d3d3b6b68544856d3328317f357ec95f82c5652bb4cc9aa70aa778f2c65f9
 TERMUX_PKG_SRCURL=https://gitlab.com/esr/open-adventure/-/archive/${TERMUX_PKG_VERSION}/open-adventure-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libedit"
 TERMUX_PKG_BUILD_DEPENDS="libedit-dev"
