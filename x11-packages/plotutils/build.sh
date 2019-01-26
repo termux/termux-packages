eef5a85985fca4cddc3ebb4830f7edef7e69d056
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-27 00:02:37 +0200

    plotutils: update homepage

--- x11-packages/plotutils/build.sh
+++ x11-packages/plotutils/build.sh
@@ -1,8 +1,9 @@
-TERMUX_PKG_HOMEPAGE=http://directory.fsf.org/graphics/plotutils.html
+TERMUX_PKG_HOMEPAGE=https://directory.fsf.org/wiki/Plotutils
 TERMUX_PKG_DESCRIPTION="Set of utilities and libraries for plotting."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=2.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.gnu.org/pub/gnu/plotutils/plotutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4f4222820f97ca08c7ea707e4c53e5a3556af4d8f1ab51e0da6ff1627ff433ab
 TERMUX_PKG_DEPENDS="libpng, libxaw"
