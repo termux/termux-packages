1a7723904c258bfc77738ab2fff7064523d6df99
Henrik Grimler <henrik@grimler.se>
2019-01-26 11:26:05 +0100

    build-package: make TERMUX_PKG_MAINTAINER overridable
    Makes it possible to change default maintainer for another repo

--- build-package.sh
+++ build-package.sh
@@ -321,6 +321,7 @@ termux_step_setup_variables() {
 	: "${TERMUX_DEBUG:=""}"
 	: "${TERMUX_PKG_API_LEVEL:="21"}"
 	: "${TERMUX_DEBDIR:="${TERMUX_SCRIPTDIR}/debs"}"
+	: "${TERMUX_PKG_MAINTAINER:="Fredrik Fornwall @fornwall"}"
 
 	if [ "x86_64" = "$TERMUX_ARCH" ] || [ "aarch64" = "$TERMUX_ARCH" ]; then
 		TERMUX_ARCH_BITS=64
@@ -385,7 +386,6 @@ termux_step_setup_variables() {
 	TERMUX_PKG_DEVPACKAGE_DEPENDS=""
 	# Set if a host build should be done in TERMUX_PKG_HOSTBUILD_DIR:
 	TERMUX_PKG_HOSTBUILD=""
-	TERMUX_PKG_MAINTAINER="Fredrik Fornwall @fornwall"
 	TERMUX_PKG_FORCE_CMAKE=no # if the package has autotools as well as cmake, then set this to prefer cmake
 	TERMUX_CMAKE_BUILD=Ninja # Which cmake generator to use
 	TERMUX_PKG_HAS_DEBUG=yes # set to no if debug build doesn't exist or doesn't work, for example for python based packages
