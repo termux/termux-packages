6e74e1bea2b78de6045cff720056112da1fa3766
Henrik Grimler <henrik@grimler.se>
2019-01-27 21:09:37 +0100

    nudoku: add license and cleanup

--- packages/nudoku/build.sh
+++ packages/nudoku/build.sh
@@ -1,10 +1,12 @@
-TERMUX_PKG_HOMEPAGE="http://jubalh.github.io/nudoku/"
+TERMUX_PKG_HOMEPAGE=http://jubalh.github.io/nudoku/
 TERMUX_PKG_DESCRIPTION="ncurses based sudoku game"
-TERMUX_PKG_VERSION=0.2.5
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/jubalh/nudoku/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1478c339409abe8f1b857bf3e54c5edbeb43954432fb6e427e52a3ff6251cc25
-TERMUX_PKG_FOLDERNAME=nudoku-$TERMUX_PKG_VERSION
+TERMUX_PKG_SHA256=80fb9996c28642920951c20cfd5ca6e370d75240255bc6f11067ae68b6e44eca
+TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
+
 termux_step_pre_configure() {
 	autoreconf -i
 }
