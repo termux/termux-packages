d6aab93dfe53d3444485678acbca25a67a1a813c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-27 18:55:16 +0200

    glu: drop libandroid-support dependency

--- x11-packages/glu/build.sh
+++ x11-packages/glu/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Mesa OpenGL Utility library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=9.0.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=ftp://ftp.freedesktop.org/pub/mesa/glu/glu-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1f7ad0d379a722fcbd303aa5650c6d7d5544fde83196b42a73d1193568a4df12
 
-TERMUX_PKG_DEPENDS="libandroid-shmem, libandroid-support, libc++, libdrm, libexpat, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxfixes, libxshmfence, mesa"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libexpat, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxfixes, libxshmfence, mesa"
 TERMUX_PKG_CONFLICTS="libglu"
 TERMUX_PKG_REPLACES="libglu"
