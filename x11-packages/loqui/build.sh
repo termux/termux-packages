c99bfa7cfeb54d54bbbaac10487e446197c6dc1b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-28 00:56:20 +0200

    loqui: drop libandroid-support dependency

--- x11-packages/loqui/build.sh
+++ x11-packages/loqui/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="IRC client for the Gtk+-2.0 environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.6.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://launchpad.net/loqui/${TERMUX_PKG_VERSION:0:3}/${TERMUX_PKG_VERSION}/+download/loqui-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d0718f0eb095fbfe2af1b4ca5a0d05cd85e969322be3f5bc9fad26f042910b36
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libandroid-support, libandroid-shmem, libcairo-x, libffi, pango-x, pcre"
+TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libandroid-shmem, libcairo-x, libffi, pango-x, pcre"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-glibtest --disable-gtktest"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
 
