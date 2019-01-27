c786bdedaf3e326be2af711cc64c2f7390bf6d9a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-27 18:16:25 +0200

    libxt: drop libandroid-support dependency

--- x11-packages/libxt/build.sh
+++ x11-packages/libxt/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="X11 toolkit intrinsics library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=1.1.5
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXt-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=46eeb6be780211fdd98c5109286618f6707712235fdd19df4ce1e6954f349f1a
-TERMUX_PKG_DEPENDS="libandroid-support, libice, libsm, libuuid, libx11, libxau, libxcb, libxdmcp"
+TERMUX_PKG_DEPENDS="libice, libsm, libuuid, libx11, libxau, libxcb, libxdmcp"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
 
