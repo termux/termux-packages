676212cb2c43037998e320176195b55c7e42300c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-27 18:55:35 +0200

    libcairo-x: drop libandroid-support dependency

--- x11-packages/libcairo-x/build.sh
+++ x11-packages/libcairo-x/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library (with X)"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=1.16.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/cairo-${TERMUX_PKG_VERSION}.tar.xz
 
-TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libandroid-shmem, libandroid-support, liblzo, libpixman, libpng, librsvg, libx11, libxcb, libxext, libxrender, poppler"
+TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libandroid-shmem, liblzo, libpixman, libpng, librsvg, libx11, libxcb, libxext, libxrender, poppler"
 TERMUX_PKG_PROVIDES="libcairo, libcairo-gobject"
 TERMUX_PKG_REPLACES="${TERMUX_PKG_PROVIDES}"
 TERMUX_PKG_CONFLICTS="${TERMUX_PKG_PROVIDES}, libcairo-dev"
