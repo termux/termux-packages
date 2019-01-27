0163382027a430cb29d3fdfa77d9eeab60b601c9
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-01-28 00:23:09 +0200

    sdl2-ttf: drop libandroid-support dependency

--- x11-packages/sdl2-ttf/build.sh
+++ x11-packages/sdl2-ttf/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="A library that allows you to use TrueType fonts in your 
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=2.0.14
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=34db5e20bcf64e7071fe9ae25acaa7d72bdc4f11ab3ce59acc768ab62fe39276
-TERMUX_PKG_DEPENDS="freetype, libandroid-support, mesa, sdl2"
+TERMUX_PKG_DEPENDS="freetype, mesa, sdl2"
 
 termux_step_pre_configure() {
 	./autogen.sh
