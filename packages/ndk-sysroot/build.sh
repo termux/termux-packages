16e8dc06059542394baf3646ea62f985883d3fec
Henrik Grimler <henrik@grimler.se>
2019-01-27 12:08:07 +0100

    ndk-sysroot: conflict with libandroid-support-dev

--- packages/ndk-sysroot/build.sh
+++ packages/ndk-sysroot/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://developer.android.com/tools/sdk/ndk/index.html
 TERMUX_PKG_DESCRIPTION="System header and library files from the Android NDK needed for compiling C programs"
 TERMUX_PKG_LICENSE="NCSA"
 TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_REVISION=10
 TERMUX_PKG_NO_DEVELSPLIT=yes
 TERMUX_PKG_KEEP_STATIC_LIBRARIES="true"
-# This package has taken over <pty.h> from the previous libutil-dev:
-TERMUX_PKG_CONFLICTS="libutil-dev, libgcc"
-TERMUX_PKG_REPLACES="libutil-dev, libgcc"
+# This package has taken over <pty.h> from the previous libutil-dev
+# and iconv.h from libandroid-support-dev:
+TERMUX_PKG_CONFLICTS="libutil-dev, libgcc, libandroid-support-dev"
+TERMUX_PKG_REPLACES="libutil-dev, libgcc, libandroid-support-dev"
 
 termux_step_extract_into_massagedir () {
 	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/pkgconfig \
