33ad5c16e414767c32739f8b1cacd93aa72a0625
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 18:12:03 +0200

    move common gpg keys to the single package

--- packages/root-repo/build.sh
+++ packages/root-repo/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-root-packages
 TERMUX_PKG_DESCRIPTION="Package repository containing programs for rooted devices"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_VERSION=1.3
+TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/etc/apt/{sources.list.d,trusted.gpg.d}
+	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
 	echo "deb https://grimler.se root stable" > $TERMUX_PREFIX/etc/apt/sources.list.d/root.list
-	cp $TERMUX_PKG_BUILDER_DIR/grimler.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 }
 
 termux_step_create_debscripts () {
--- packages/x11-repo/build.sh
+++ packages/x11-repo/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://termux-x11.ml
 TERMUX_PKG_DESCRIPTION="Package repository containing X11 programs and libraries"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_VERSION=1.3
+TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/etc/apt/{sources.list.d,trusted.gpg.d}
+	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
 	echo "deb https://termux-x11.ml x11 main" > $TERMUX_PREFIX/etc/apt/sources.list.d/x11.list
-	cp $TERMUX_PKG_BUILDER_DIR/xeffyr.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 }
 
 termux_step_create_debscripts () {
