c0551da453b2b7c1591045a2e1bc938a72f64aa4
Henrik Grimler <henrik@grimler.se>
2019-02-01 20:07:30 +0100

    openmpi: update to 4.0.0

--- packages/openmpi/build.sh
+++ packages/openmpi/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.open-mpi.org
 TERMUX_PKG_DESCRIPTION="The Open MPI Project is an open source Message Passing Interface implementation that is developed and maintained by a consortium of academic, research, and industry partners"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=3.1.3
+TERMUX_PKG_VERSION=4.0.0
 TERMUX_PKG_SRCURL=https://download.open-mpi.org/release/open-mpi/v${TERMUX_PKG_VERSION:0:3}/openmpi-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0254627d8a9b12a8f50213ed01e7a94dd7e91b340abf5c53bcf0b89afe6fb77d
+TERMUX_PKG_SHA256=36f10daa3f1b1d37530f686bf7f70966b2a13c0bc6e2e05aebc7e85e3d21b10d
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-x
 ac_cv_header_ifaddrs_h=no
 --disable-dlopen
