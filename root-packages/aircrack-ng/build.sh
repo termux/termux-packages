3ce3318f356a1576902ef964c593e458f9ebf0e1
Henrik Grimler <henrik@grimler.se>
2019-02-03 19:01:28 +0100

    aircrack: write version comment on another line
    To not upset version extraction function

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -2,7 +2,8 @@ TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
 TERMUX_PKG_HOMEPAGE=https://www.aircrack-ng.org/
 TERMUX_PKG_DESCRIPTION="WiFi security auditing tools suite"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.2-rc4 #1.5.1 (requires autoconf, automake, libtool, shtool, pkg-config (and maybe libstdc and make/gmake) support)
+#1.5.1 (requires autoconf, automake, libtool, shtool, pkg-config (and maybe libstdc and make/gmake) support)
+TERMUX_PKG_VERSION=1.2-rc4
 TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/aircrack-ng/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ed241ea423cab1c86e7882e901d5200d91b2f97d7efd703b0acf17742be47f9b
