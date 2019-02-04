d01f1af66d665c4157918bbe95ec4b7a80eb865b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-04 20:09:08 +0200

    qemu: fix subpackage for "qemu-system-riscv32"

--- x11-packages/qemu-system-x86_64/qemu-system-riscv32.subpackage.sh
+++ x11-packages/qemu-system-x86_64/qemu-system-riscv32.subpackage.sh
@@ -1,7 +1,7 @@
 TERMUX_SUBPKG_INCLUDE="
-bin/qemu-system-riscv64
-share/man/man1/qemu-system-riscv64.1
+bin/qemu-system-riscv32
+share/man/man1/qemu-system-riscv32.1
 "
 
 TERMUX_SUBPKG_DEPENDS="glib, libandroid-shmem, libbz2, libc++, libcurl, libgnutls, libjpeg-turbo, liblzo, libnettle, libpixman, libpng, libsasl, libssh2, libutil, ncurses, qemu-common, sdl2"
-TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator (riscv64)"
+TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator (riscv32)"
