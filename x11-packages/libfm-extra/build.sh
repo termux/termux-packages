4d45d5db40a0e5752b7454bdfc997b8e86a51832
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-07 01:19:43 +0200

    new package: menu-cache

---
+++ x11-packages/libfm-extra/build.sh
@@ -0,0 +1,16 @@
+## This is stripped down version of 'libfm' package.
+## Primarily used for compiling 'menu-cache'.
+
+TERMUX_PKG_HOMEPAGE=https://lxde.org/
+TERMUX_PKG_DESCRIPTION="Extra library for file management"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pcmanfm/libfm-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=96b1244bde41ca0eef0332cfb5c67bb16725dfd102128f3e6f74fadc13a1cfe4
+TERMUX_PKG_DEPENDS="gtk3, libexif"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-gtk=3
+--with-extra-only
+"
