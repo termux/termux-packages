961f8ef0bf2aa202b1529c67636ac8374c5c7847
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-07 02:26:24 +0200

    pcmanfm: force link with libandroid-shmem
    Prevents "Bad system call" error on Android 8.

--- x11-packages/pcmanfm/build.sh
+++ x11-packages/pcmanfm/build.sh
@@ -5,9 +5,13 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=1.3.1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pcmanfm/pcmanfm-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=6804043b3ee3a703edde41c724946174b505fe958703eadbd7e0876ece836855
-TERMUX_PKG_DEPENDS="atk, glib, gtk3, libcairo-x, libfm, libx11, lxmenu-data, pango-x"
+TERMUX_PKG_DEPENDS="atk, glib, gtk3, libandroid-shmem, libcairo-x, libfm, libx11, lxmenu-data, pango-x"
 TERMUX_PKG_RECOMMENDS="xarchiver"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-gtk=3
 "
+
+termux_step_pre_configure() {
+    export LIBS="-landroid-shmem"
+}
