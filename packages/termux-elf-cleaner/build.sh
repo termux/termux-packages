9160ebf696a3279ee047c5ebec5b08649a4cfb90
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-08 13:52:34 +0200

    termux-elf-cleaner: do not remove DT_RUNPATH

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 #       to create a native binary. Bumping this version will need
 #       updating the checksum used there.
 TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=956d35778bf40523543a98ecec79d7d895a08ac69da1018acb84658a1c94e82b
 TERMUX_PKG_BUILD_IN_SRC=yes
