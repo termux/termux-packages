94b3a94d12994c58154c108f687245a847d7fe82
Fredrik Fornwall <fredrik@fornwall.net>
2019-02-09 00:41:22 +0100

    sqlite: Update from 3.26.0 to 3.27.1

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Library implementing a self-contained and transactional 
 TERMUX_PKG_LICENSE="Public Domain"
 # Note: Updating this version requires bumping the tcl package as well.
 _SQLITE_MAJOR=3
-_SQLITE_MINOR=26
-_SQLITE_PATCH=0
-TERMUX_PKG_SHA256=5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d
+_SQLITE_MINOR=27
+_SQLITE_PATCH=1
+TERMUX_PKG_SHA256=54a92b8ff73ff6181f89b9b0c08949119b99e8cccef93dbef90e852a8b10f4f8
 TERMUX_PKG_VERSION=${_SQLITE_MAJOR}.${_SQLITE_MINOR}.${_SQLITE_PATCH}
-TERMUX_PKG_SRCURL=https://www.sqlite.org/2018/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
+TERMUX_PKG_SRCURL=https://www.sqlite.org/2019/sqlite-autoconf-${_SQLITE_MAJOR}${_SQLITE_MINOR}0${_SQLITE_PATCH}00.tar.gz
 # ac_cv_func_strerror_r=no as strerror_r() with the
 # GNU signature is only # available in android-23:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
