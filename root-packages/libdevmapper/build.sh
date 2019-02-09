f658d682b21364d699b8651b5030f7ec7bab4be2
Henrik Grimler <henrik@grimler.se>
2019-02-09 18:37:04 +0100

    libdevmapper: set fake version to 2.03.01
    real version 2.02.177

--- root-packages/libdevmapper/build.sh
+++ root-packages/libdevmapper/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://sourceware.org/lvm2/
 TERMUX_PKG_DESCRIPTION="A device-mapper library from LVM2 package"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.02.177
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/sourceware/lvm2/releases/LVM2.${TERMUX_PKG_VERSION}.tgz
+# REAL VERSION: TERMUX_PKG_VERSION=2.02.177
+TERMUX_PKG_VERSION=2.03.01
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/sourceware/lvm2/releases/LVM2.2.02.177.tgz
 TERMUX_PKG_SHA256=4025a23ec9b15c2cb7486d151c29dc953b75efc4d452cfe9dbbc7c0fac8e80f2
 TERMUX_PKG_DEPENDS="libandroid-support, libaio, readline"
 TERMUX_PKG_BUILD_IN_SRC=yes
