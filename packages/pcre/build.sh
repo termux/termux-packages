27d9773a019a59afca8d5d3940240af033eb43ac
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-10 01:03:55 +0200

    bump revisions of mpv deps

--- packages/pcre/build.sh
+++ packages/pcre/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.pcre.org
 TERMUX_PKG_DESCRIPTION="Library implementing regular expression pattern matching using the same syntax and semantics as Perl 5"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=8.42
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=2cd04b7c887808be030254e8d77de11d3fe9d4505c39d4b15d2664ffe8bf9301
 TERMUX_PKG_SRCURL=https://ftp.pcre.org/pub/pcre/pcre-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/pcre-config"
