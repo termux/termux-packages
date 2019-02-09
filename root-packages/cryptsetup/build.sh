0a807b5488cef324f23c891630ef02bd0f43ca27
Henrik Grimler <henrik@grimler.se>
2019-02-09 12:14:42 +0100

    cryptsetup: fix default lock path

--- root-packages/cryptsetup/build.sh
+++ root-packages/cryptsetup/build.sh
@@ -2,7 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/cryptsetup/cryptsetup/
 TERMUX_PKG_DESCRIPTION="Userspace setup tool for transparent encryption of block devices using dm-crypt"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.0.6
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/linux/utils/cryptsetup/v${TERMUX_PKG_VERSION:0:3}/cryptsetup-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=7c51fae0f0e7ea9af0f515b2ac77009fb2969a6619ebab47d097dca38b083d30
 TERMUX_PKG_DEPENDS="json-c, libdevmapper, libgcrypt, libpopt, libuuid, util-linux"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-luks2-lock-path=$PREFIX/var/run
+"
