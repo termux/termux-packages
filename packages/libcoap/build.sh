ec9a2cac811d69c042890ce7c572af1c05ef9e89
Fredrik Fornwall <fredrik@fornwall.net>
2019-02-10 02:16:32 +0100

    libcoap: Add package

---
+++ packages/libcoap/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://libcoap.net/
+TERMUX_PKG_DESCRIPTION="Implementation of CoAP, a lightweight protocol for resource constrained devices"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_VERSION=4.2.0-rc4
+TERMUX_PKG_SHA256=8aa70c11fb1f1f443a9565c855f98027a50e8e0040310da86933770193e73b24
+TERMUX_PKG_SRCURL=https://github.com/obgm/libcoap/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-openssl --disable-doxygen"
+
+termux_step_pre_configure() {
+	NOCONFIGURE=1 ./autogen.sh
+}
