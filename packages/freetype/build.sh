40f6d64bedf68614b4fc54ce238ef4ab110800c0
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-10 13:38:28 +0200

    freetype: fix setting of revision

--- packages/freetype/build.sh
+++ packages/freetype/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/freetype/freetype2/${TERMUX_PKG_VERSION}/freetype-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libbz2, libpng"
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/freetype-config share/man/man1/freetype-config.1"
 # Use with-harfbuzz=no to avoid circular dependency between freetype and harfbuzz:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-harfbuzz=no"
