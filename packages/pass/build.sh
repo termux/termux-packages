7d2c9a6e838646a2de0c5c4647b94b591463c162
Oliver Schmidhauser <oli@glow.li>
2019-02-12 00:16:46 +0100

    pass: don't add newline to output. Fixes #3310
    Since https://github.com/termux/termux-api/commit/5b3f31bf14d8588b2f0fee02b5b70f9dc46be17d fixes the newline being added to termux-clipboard-get, this workaround is no longer required here and actually breaks the comparission.

--- packages/pass/build.sh
+++ packages/pass/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.passwordstore.org
 TERMUX_PKG_DESCRIPTION="Lightweight directory-based password manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.7.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=2b6c65846ebace9a15a118503dcd31b6440949a30d3b5291dfb5b1615b99a3f4
 TERMUX_PKG_SRCURL=https://git.zx2c4.com/password-store/snapshot/password-store-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
