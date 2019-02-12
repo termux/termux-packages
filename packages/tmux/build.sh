e39ea9907fc00cd4db8e7515e0dea177c3f8c3f9
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-13 00:33:47 +0200

    specify CONFFILES for bash, nano, resolv-conf, tmux
    Prevent overwriting user's configuration files if package was upgraded.

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -4,10 +4,12 @@ TERMUX_PKG_LICENSE="BSD"
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
 TERMUX_PKG_DEPENDS="ncurses, libevent, libutil, libandroid-support, libandroid-glob"
 TERMUX_PKG_VERSION=2.8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=7f6bf335634fafecff878d78de389562ea7f73a7367f268b66d37ea13617a2ba
 TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/releases/download/${TERMUX_PKG_VERSION}/tmux-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
+
+TERMUX_PKG_CONFFILES="etc/tmux.conf"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
