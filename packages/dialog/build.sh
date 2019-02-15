9fd1551381aceb9a7b953f329b72a23bb3116128
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-15 23:10:29 +0200

    dialog: update to 1.3-20190211 and fix download URL

--- packages/dialog/build.sh
+++ packages/dialog/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://invisible-island.net/dialog/
 TERMUX_PKG_DESCRIPTION="Application used in shell scripts which displays text user interface widgets"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION="1.3-20181107"
-TERMUX_PKG_SHA256=efeaca8027dda53a9f3cf6c7b5c1a77093825b7a9b85c23c0c6c96afc3506457
-TERMUX_PKG_SRCURL=http://invisible-island.net/datafiles/release/dialog.tar.gz
+TERMUX_PKG_VERSION="1.3-20190211"
+TERMUX_PKG_SHA256=49c0e289d77dcd7806f67056c54f36a96826a9b73a53fb0ffda1ffa6f25ea0d3
+TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/dialog/dialog-$TERMUX_PKG_VERSION.tgz
 # This will break when a new version is released (the URL unfortunately does not change)
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncursesw --enable-widec --with-pkg-config"
 
