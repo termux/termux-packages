d38331d1564b2fc4cddce21b0c360ecfbfdcfbda
Fredrik Fornwall <fredrik@fornwall.net>
2019-02-15 23:36:44 +0100

    libjpeg-turbo: Update from 2.0.1 to 2.0.2

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://libjpeg-turbo.virtualgl.org
 TERMUX_PKG_DESCRIPTION="Library for reading and writing JPEG image files"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=2.0.1
-TERMUX_PKG_SHA256=e5f86cec31df1d39596e0cca619ab1b01f99025a27dafdfc97a30f3a12f866ff
+TERMUX_PKG_VERSION=2.0.2
+TERMUX_PKG_SHA256=acb8599fe5399af114287ee5907aea4456f8f2c1cc96d26c28aebfdf5ee82fed
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/libjpeg-turbo/${TERMUX_PKG_VERSION}/libjpeg-turbo-${TERMUX_PKG_VERSION}.tar.gz
