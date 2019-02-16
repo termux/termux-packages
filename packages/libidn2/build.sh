69c9ac860c7a32e101a7bf7d14b154cd09e640e0
Fredrik Fornwall <fredrik@fornwall.net>
2019-02-16 01:40:05 +0100

    Switch from ftp.gnu.org to mirrors.kernel.org

--- packages/libidn2/build.sh
+++ packages/libidn2/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="lokesh @hax4us"
 TERMUX_PKG_DEPENDS="libunistring, libandroid-support"
 TERMUX_PKG_VERSION=2.1.1a
 TERMUX_PKG_SHA256=57666bcf6ecf54230d7bac95c392379561954b57a673903aed4d3336b3048b72
-TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libidn/libidn2-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libidn/libidn2-$TERMUX_PKG_VERSION.tar.gz
