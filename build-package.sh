2d391054b5f4731a871e05a1a3f3ff8b30977050
Henrik Grimler <henrik@grimler.se>
2019-02-16 16:31:06 +0100

    build-package.sh: set TERMUX_NO_CLEAN to false per default
    Ensures -i builds are run in fresh environments

--- build-package.sh
+++ build-package.sh
@@ -324,7 +324,7 @@ termux_step_setup_variables() {
 	: "${TERMUX_ANDROID_HOME:="/data/data/com.termux/files/home"}"
 	: "${TERMUX_DEBUG:=""}"
 	: "${TERMUX_PKG_API_LEVEL:="21"}"
-	: "${TERMUX_NO_CLEAN:="true"}"
+	: "${TERMUX_NO_CLEAN:="false"}"
 	: "${TERMUX_QUIET_BUILD:="false"}"
 	: "${TERMUX_DEBDIR:="${TERMUX_SCRIPTDIR}/debs"}"
 	: "${TERMUX_SKIP_DEPCHECK:="false"}"
