b1988a732d575bb0b598a88cd8470eaf30e94f20
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-17 18:42:34 +0200

    disabled-packages: remove obsolete version of 'caddy'
    We have up-to-date 'caddy' as enabled package.

--- disabled-packages/caddy/build.sh
+++
@@ -1,4 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/mholt/caddy
-TERMUX_PKG_DESCRIPTION="Fast, cross-platform HTTP/2 web server with automatic HTTPS"
-TERMUX_PKG_VERSION=0.8.0
-TERMUX_PKG_SRCURL=https://github.com/mholt/caddy/archive/v${TERMUX_PKG_VERSION}.tar.gz
