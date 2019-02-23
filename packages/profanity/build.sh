61120ec9080ff370d41ba8a41473af1bd6dd2095
Henrik Grimler <henrik@grimler.se>
2019-02-23 08:53:17 +0100

    profanity: use tab instead of spaces

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -12,13 +12,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-python-plugins"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
-  CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.7m"
-  LDFLAGS+=" -lpython3.7m"
+	CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.7m"
+	LDFLAGS+=" -lpython3.7m"
 }
 
 termux_step_post_configure() {
-  # Enable python support manually, as trying to go using --enable-python-plugins
-  # causes configure trying to execut python:
-  echo '#define HAVE_PYTHON 1' >> $TERMUX_PKG_SRCDIR/src/config.h
-  perl -p -i -e 's|#am__objects_2|am__objects_2|' $TERMUX_PKG_SRCDIR/Makefile
+	# Enable python support manually, as trying to go using --enable-python-plugins
+	# causes configure trying to execute python:
+	echo '#define HAVE_PYTHON 1' >> $TERMUX_PKG_SRCDIR/src/config.h
+	perl -p -i -e 's|#am__objects_2|am__objects_2|' $TERMUX_PKG_SRCDIR/Makefile
 }
