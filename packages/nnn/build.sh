0c9fea97293f8c84eef849a2f44ec70b46c5c528
Fredrik Fornwall <fredrik@fornwall.net>
2019-02-24 18:21:45 +0100

    nnn: Depend on readline

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_VERSION=2.3
 TERMUX_PKG_SHA256=eaad2ccf0d781aeeb38fdfc4ad75a0405ca3da4f82ded64cce766a74a2b299ab
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="file, findutils, ncurses"
+TERMUX_PKG_DEPENDS="file, findutils, readline"
 TERMUX_PKG_BUILD_IN_SRC=yes
