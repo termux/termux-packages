c19fd73c97aec370708c3974a1fd781d5d3c85d3
Henrik Grimler <henrik@grimler.se>
2019-02-26 21:27:00 +0100

    build-package: mv termux_step_handle_hostbuild to scripts/build/

--- build-package.sh
+++ build-package.sh
@@ -746,23 +746,7 @@ termux_step_post_extract_package() {
 }
 
 # Optional host build. Not to be overridden by packages.
-termux_step_handle_hostbuild() {
-	if [ "x$TERMUX_PKG_HOSTBUILD" = "x" ]; then return; fi
-
-	cd "$TERMUX_PKG_SRCDIR"
-	for patch in $TERMUX_PKG_BUILDER_DIR/*.patch.beforehostbuild; do
-		test -f "$patch" && sed "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" "$patch" | patch --silent -p1
-	done
-
-	local TERMUX_HOSTBUILD_MARKER="$TERMUX_PKG_HOSTBUILD_DIR/TERMUX_BUILT_FOR_$TERMUX_PKG_VERSION"
-	if [ ! -f "$TERMUX_HOSTBUILD_MARKER" ]; then
-		rm -Rf "$TERMUX_PKG_HOSTBUILD_DIR"
-		mkdir -p "$TERMUX_PKG_HOSTBUILD_DIR"
-		cd "$TERMUX_PKG_HOSTBUILD_DIR"
-		termux_step_host_build
-		touch "$TERMUX_HOSTBUILD_MARKER"
-	fi
-}
+source scripts/build/termux_step_handle_hostbuild.sh
 
 # Perform a host build. Will be called in $TERMUX_PKG_HOSTBUILD_DIR.
 # After termux_step_post_extract_package() and before termux_step_patch_package()
