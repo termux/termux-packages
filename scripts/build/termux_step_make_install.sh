617764f4f4203ad5a783be01aac9afb7ec997b40
Henrik Grimler <henrik@grimler.se>
2019-02-27 19:50:20 +0100

    build-package: mv termux_step_make_install to scripts/build/

---
+++ scripts/build/termux_step_make_install.sh
@@ -0,0 +1,23 @@
+termux_step_make_install() {
+	if test -f build.ninja; then
+		ninja -w dupbuild=warn -j $TERMUX_MAKE_PROCESSES install
+	elif ls ./*akefile &> /dev/null || [ ! -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
+		: "${TERMUX_PKG_MAKE_INSTALL_TARGET:="install"}"
+		# Some packages have problem with parallell install, and it does not buy much, so use -j 1.
+		if [ -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
+			make -j 1 ${TERMUX_PKG_MAKE_INSTALL_TARGET}
+		else
+			make -j 1 ${TERMUX_PKG_EXTRA_MAKE_ARGS} ${TERMUX_PKG_MAKE_INSTALL_TARGET}
+		fi
+	elif test -f Cargo.toml; then
+		termux_setup_rust
+		cargo install \
+			--path . \
+			--force \
+			--target $CARGO_TARGET_NAME \
+			--root $TERMUX_PREFIX \
+			$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+		# https://github.com/rust-lang/cargo/issues/3316:
+		rm $TERMUX_PREFIX/.crates.toml
+	fi
+}
