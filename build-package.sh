00d6e0c28237965bb1958b8f51833b002f0b5304
Henrik Grimler <henrik@grimler.se>
2019-02-27 19:49:39 +0100

    build-package: mv termux_step_make to scripts/build/

--- build-package.sh
+++ build-package.sh
@@ -91,22 +91,8 @@ termux_step_post_configure() {
 	return
 }
 
-termux_step_make() {
-	local QUIET_BUILD=
-	if [ $TERMUX_QUIET_BUILD = true ]; then
-		QUIET_BUILD="-s"
-	fi
-
-	if test -f build.ninja; then
-		ninja -w dupbuild=warn -j $TERMUX_MAKE_PROCESSES
-	elif ls ./*akefile &> /dev/null || [ ! -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
-		if [ -z "$TERMUX_PKG_EXTRA_MAKE_ARGS" ]; then
-			make -j $TERMUX_MAKE_PROCESSES $QUIET_BUILD
-		else
-			make -j $TERMUX_MAKE_PROCESSES $QUIET_BUILD ${TERMUX_PKG_EXTRA_MAKE_ARGS}
-		fi
-	fi
-}
+# Make package, either with ninja or make
+source scripts/build/termux_step_make.sh
 
 termux_step_make_install() {
 	if test -f build.ninja; then
