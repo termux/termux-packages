9f5ba4bcc6ac38268e84ec37fb44287f2cefa5a1
Henrik Grimler <henrik@grimler.se>
2019-02-27 19:53:53 +0100

    build-package: mv termux_step_extract_into_massagedir to scripts/build/

--- build-package.sh
+++ build-package.sh
@@ -102,21 +102,8 @@ termux_step_post_make_install() {
 	return
 }
 
-termux_step_extract_into_massagedir() {
-	local TARBALL_ORIG=$TERMUX_PKG_PACKAGEDIR/${TERMUX_PKG_NAME}_orig.tar.gz
-
-	# Build diff tar with what has changed during the build:
-	cd $TERMUX_PREFIX
-	tar -N "$TERMUX_BUILD_TS_FILE" \
-		--exclude='lib/libc++_shared.so' --exclude='lib/libstdc++.so' \
-		-czf "$TARBALL_ORIG" .
-
-	# Extract tar in order to massage it
-	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"
-	cd "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"
-	tar xf "$TARBALL_ORIG"
-	rm "$TARBALL_ORIG"
-}
+# Function to cp (through tar) installed files to massage dir
+source scripts/build/termux_step_extract_into_massagedir.sh
 
 termux_step_massage() {
 	cd "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"
