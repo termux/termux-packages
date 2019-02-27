e1d23a979a134460d798e0cd36ab896bf3ec55d8
kenneth-Q <11911577+kenneth-Q@users.noreply.github.com>
2019-02-27 18:53:39 +0800

    Add jp2a package

---
+++ packages/jp2a/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/cslarsen/jp2a
+TERMUX_PKG_DESCRIPTION="a simple JPEG to ASCII converter."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_DEPENDS="ncurses, libjpeg-turbo, libcurl"
+_COMMIT=61d205f6959d88e0cc8d8879fe7d66eb0932ecca
+TERMUX_PKG_VERSION=1.0.8
+TERMUX_PKG_SRCURL=https://github.com/cslarsen/jp2a/archive/${_COMMIT}.zip
+TERMUX_PKG_SHA256=26f655e4b977bf24b8193150f34c015195c07c1116d064375223dd46ea5b8b4e
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_pre_configure() {
+	autoreconf -vi
+}
