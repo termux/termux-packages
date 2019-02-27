f5dd6a1f94029ce04559edf3bf97c998bdcbb9e7
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-28 00:28:03 +0200

    bump C++ packages

--- x11-packages/fltk-editor/build.sh
+++ x11-packages/fltk-editor/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_DEPENDS="fltk, libc++"
 TERMUX_PKG_VERSION=1.0-termux
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_REVISION=9
 
 termux_step_make() {
 	"${CXX}" \
