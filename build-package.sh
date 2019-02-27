8e759381d2f4d0e162672562e79f7494fa930122
Henrik Grimler <henrik@grimler.se>
2019-02-27 19:39:41 +0100

    build-package: mv termux_step_replace_guess_scripts to scripts/build

--- build-package.sh
+++ build-package.sh
@@ -68,11 +68,7 @@ source scripts/build/termux_step_setup_toolchain.sh
 source scripts/build/termux_step_patch_package.sh
 
 # Replace autotools build-aux/config.{sub,guess} with ours to add android targets.
-termux_step_replace_guess_scripts() {
-	cd "$TERMUX_PKG_SRCDIR"
-	find . -name config.sub -exec chmod u+w '{}' \; -exec cp "$TERMUX_SCRIPTDIR/scripts/config.sub" '{}' \;
-	find . -name config.guess -exec chmod u+w '{}' \; -exec cp "$TERMUX_SCRIPTDIR/scripts/config.guess" '{}' \;
-}
+source scripts/build/termux_step_replace_guess_scripts.sh
 
 # For package scripts to override. Called in $TERMUX_PKG_BUILDDIR.
 termux_step_pre_configure() {
