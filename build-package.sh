f1e5560580de4c217929e7214c6cdc3ff756392d
Henrik Grimler <henrik@grimler.se>
2019-02-27 19:58:49 +0100

    build-package: mv termux_step_create_datatar to scripts/build/

--- build-package.sh
+++ build-package.sh
@@ -113,21 +113,7 @@ termux_step_post_massage() {
 }
 
 # Create data.tar.gz with files to package. Not to be overridden by package scripts.
-termux_step_create_datatar() {
-	# Create data tarball containing files to package:
-	cd "$TERMUX_PKG_MASSAGEDIR"
-
-	local HARDLINKS
-	HARDLINKS="$(find . -type f -links +1)"
-	if [ -n "$HARDLINKS" ]; then
-		termux_error_exit "Package contains hard links: $HARDLINKS"
-	fi
-
-	if [ -z "${TERMUX_PKG_METAPACKAGE+x}" ] && [ "$(find . -type f)" = "" ]; then
-		termux_error_exit "No files in package"
-	fi
-	tar -cJf "$TERMUX_PKG_PACKAGEDIR/data.tar.xz" .
-}
+source scripts/build/termux_step_create_datatar.sh
 
 termux_step_create_debscripts() {
 	return
