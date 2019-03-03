9030cd93c3857a64f5f7bc088ac64c69bcb3b8fa
o-nly <46415839+o-nly@users.noreply.github.com>
2019-03-03 12:46:53 +0000

    Create build.sh
    Add package: Guile-json

---
+++ packages/guile-xyz/guile-json/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/aconchillo/guile-json
+TERMUX_PKG_DESCRIPTION="Guile-JSON supports parsing and building JSON documents according to the specification(http://json.org)."
+TERMUX_PKG_LICENSE="GPL3 or later"
+TERMUX_PKG_VERSION=3.1.0
+TERMUX_PKG_NAME=guile-json
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SHA256=e303d0db3f34ac515b8f61db8ed35be8b6c5014d7ab4a5a8ef9b44723ed3d8f9
+TERMUX_PKG_SRCURL=https://download.savannah.nongnu.org/releases/${TERMUX_PKG_NAME}/${TERMUX_PKG_NAME}-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="guile pkg-config"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
