16025250e2fb7271a0f7589a288894f7b9bb586f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-03 15:44:33 +0200

    guile: add libltdl to dependencies

--- packages/guile-json/build.sh
+++ packages/guile-json/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=3.1.0
 TERMUX_PKG_SRCURL=https://download.savannah.nongnu.org/releases/guile-json/guile-json-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e303d0db3f34ac515b8f61db8ed35be8b6c5014d7ab4a5a8ef9b44723ed3d8f9
-TERMUX_PKG_DEPENDS="guile"
+TERMUX_PKG_DEPENDS="guile, libltdl"
--- packages/guile/build.sh
+++ packages/guile/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_VERSION=2.2.4
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/guile/guile-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=33b904c0bf4e48e156f3fb1d0e6b0392033bd610c6c9d9a0410c6e0ea96a3e5c
-TERMUX_PKG_DEPENDS="libffi, libgc, libgmp, libtool, libunistring"
+TERMUX_PKG_DEPENDS="libffi, libgc, libgmp, libtool, libltdl, libunistring"
