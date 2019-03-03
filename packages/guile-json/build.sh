665fb5aef48f12fcef39b1cbab4b0375d6c63cdb
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-03 15:19:42 +0200

    fixes for guile build scripts

---
+++ packages/guile-json/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/aconchillo/guile-json
+TERMUX_PKG_DESCRIPTION="Guile-JSON supports parsing and building JSON documents"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=3.1.0
+TERMUX_PKG_SRCURL=https://download.savannah.nongnu.org/releases/guile-json/guile-json-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e303d0db3f34ac515b8f61db8ed35be8b6c5014d7ab4a5a8ef9b44723ed3d8f9
+TERMUX_PKG_DEPENDS="guile"
--- packages/guile/build.sh
+++ packages/guile/build.sh
@@ -1,9 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/guile/
-TERMUX_PKG_DESCRIPTION="Guile is the official extension language of the GNU system."
-TERMUX_PKG_LICENSE="LGPL-3.0 or later"
+TERMUX_PKG_DESCRIPTION="Guile is the official extension language of the GNU system"
+TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_VERSION=2.2.4
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=33b904c0bf4e48e156f3fb1d0e6b0392033bd610c6c9d9a0410c6e0ea96a3e5c
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/guile/guile-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libffi libunistring libtool libgc libgmp"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
+TERMUX_PKG_SHA256=33b904c0bf4e48e156f3fb1d0e6b0392033bd610c6c9d9a0410c6e0ea96a3e5c
+TERMUX_PKG_DEPENDS="libffi, libgc, libgmp, libtool, libunistring"
