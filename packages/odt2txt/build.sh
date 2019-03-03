6ae91337c313d51b18a5160dbb56297726ee8033
Laurent Arnoud <laurent@spkdev.net>
2019-03-03 19:27:37 +0100

    Add libzip and odt2txt packages

---
+++ packages/odt2txt/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dstosberg/odt2txt
+TERMUX_PKG_DESCRIPTION="simple converter from OpenDocument Text to plain text."
+TERMUX_PKG_VERSION=0.5
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SHA256=23a889109ca9087a719c638758f14cc3b867a5dcf30a6c90bf6a0985073556dd
+TERMUX_PKG_SRCURL=https://github.com/dstosberg/odt2txt/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libandroid-support, libzip"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_make () {
+    make \
+        CC="$CC" \
+        CFLAGS="$CFLAGS $CPPFLAGS -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" \
+        LDFLAGS="$LDFLAGS"
+}
+
+termux_step_make_install () {
+    make install \
+        DESTDIR="$TERMUX_PREFIX" \
+        MANDIR="$TERMUX_PREFIX/share/man/man1"
+}
