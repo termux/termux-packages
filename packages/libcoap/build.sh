14742449fa164233a367d6b28b3e3f0e0dbb8878
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-04 22:51:30 +0100

    libcoap: Update from 4.2.0-rc4 to 4.2.0

--- packages/libcoap/build.sh
+++ packages/libcoap/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://libcoap.net/
 TERMUX_PKG_DESCRIPTION="Implementation of CoAP, a lightweight protocol for resource constrained devices"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=4.2.0-rc4
-TERMUX_PKG_SHA256=8aa70c11fb1f1f443a9565c855f98027a50e8e0040310da86933770193e73b24
+TERMUX_PKG_VERSION=4.2.0
+TERMUX_PKG_SHA256=9523e38da6ee8b2a8f5ce83ded64107dd1e514c7ad00cd74ccfe3454b679c271
 TERMUX_PKG_SRCURL=https://github.com/obgm/libcoap/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-openssl --disable-doxygen"
