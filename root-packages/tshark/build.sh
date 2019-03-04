b4efb2162d0bbc8f49fedbb4d9120c2cc5bd6506
Henrik Grimler <henrik@grimler.se>
2019-03-04 23:54:03 +0100

    tshark: start working on update to 3.0.0
    fails with /storage/termux-build/_cache/19b-aarch64-21-v1/lib64/clang/8.0.2/include/mmintrin.h:64:12: error: invalid conversion between vector type '__m64' (vector of 1 'long long' value) and integer type 'int' of different size
        return (__m64)__builtin_ia32_vec_init_v2si(__i, 0);

--- root-packages/tshark/build.sh
+++ root-packages/tshark/build.sh
@@ -2,19 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.wireshark.org/
 TERMUX_PKG_DESCRIPTION="Network protocol analyzer and sniffer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_VERSION=2.6.7
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/all-versions/wireshark-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=747b3e7a37414942959f76f198be49dcbcca936bda538c4408942ce71bfd2b71
+TERMUX_PKG_SHA256=bc4f30f5b2e94f3a696fef9de44673cdf402db90aac5299966da647f708f009e
 TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, libgnutls, openssl, libiconv"
 # Needed by pkg-config (since they are dependenies for libgnutls and glib)
 TERMUX_PKG_BUILD_DEPENDS="pcre-dev, libnettle-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-qt=no
---disable-wireshark
---with-lua=no
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_wireshark=OFF
+-DENABLE_LUA=OFF
 "
-
-termux_step_pre_configure () {
-	./autogen.sh
-	LDFLAGS+=" -L$TERMUX_PKG_BUILDDIR/wsutil/.libs -L$TERMUX_PKG_BUILDDIR/epan/.libs" #A bit hacky...
-}
