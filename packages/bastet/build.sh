1e63194f862740c2cca257128055b97245f9784a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-05 20:32:32 +0200

    new package: bastet
    From https://github.com/termux/termux-packages/pull/1543.

---
+++ packages/bastet/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=http://fph.altervista.org/prog/bastet.html
+TERMUX_PKG_DESCRIPTION="Tetris clone with 'bastard' block-choosing AI"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=0.43.2
+TERMUX_PKG_SRCURL=https://github.com/fph/bastet/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f
+TERMUX_PKG_DEPENDS="boost, ncurses"
+TERMUX_PKG_FOLDERNAME=bastet-$TERMUX_PKG_VERSION
+TERMUX_PKG_BUILD_IN_SRC=yes
