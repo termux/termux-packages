18dd7fc9fcf89f6b252ad3710a9fd0ca84cae49b
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-06 23:36:02 +0100

    cmus: Make dependencies required for now

--- packages/cmus/build.sh
+++ packages/cmus/build.sh
@@ -3,10 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small, fast and powerful console music player"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.8.0
 TERMUX_PKG_SHA256=e575fe61ff6eb2dd7ea71e13790fb5df22534309cc3406144dede2a112db703d
-TERMUX_PKG_DEPENDS="libandroid-support, ncurses, pulseaudio"
-TERMUX_PKG_BUILD_DEPENDS="libflac, libmad, libvorbis, opusfile, ffmpeg"
-TERMUX_PKG_RECOMMENDS="ffmpeg"
-TERMUX_PKG_SUGGESTS="libflac, libmad, libvorbis, opusfile"
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses, pulseaudio, ffmpeg, libmad, opusfile"
 TERMUX_PKG_SRCURL=https://github.com/cmus/cmus/archive/d93f9c39b9190b2bc5eb3a2816564bdca8e945b7.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 
