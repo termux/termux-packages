3a9c850dbf3ea9b233b9d874c1efb30e6628fccd
Tom Yan <tom.ty89@gmail.com>
2019-03-07 23:32:01 +0800

    openjpeg-dev: depends on openjpeg-tools for fast build

--- packages/openjpeg/build.sh
+++ packages/openjpeg/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SHA256=3dc787c1bb6023ba846c2a0d9b1f6e179f1cd255172bde9eb75b01f1e6c7d71a
 TERMUX_PKG_SRCURL=https://github.com/uclouvain/openjpeg/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_STATIC_LIBS=OFF"
+# for fast building packages that depend on openjpeg with cmake
+TERMUX_PKG_DEVPACKAGE_DEPENDS="openjpeg-tools"
 
 termux_step_pre_configure() {
 	# Force symlinks to be overwritten:
