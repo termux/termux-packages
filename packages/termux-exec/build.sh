dd5888842be20a2e09153a6781fe9347f92a1077
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-10 22:42:30 +0100

    termux-exec: Fix DT_RUNPATH

--- packages/termux-exec/build.sh
+++ packages/termux-exec/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-exec
 TERMUX_PKG_DESCRIPTION="An execve() wrapper to make /bin and /usr/bin shebangs work"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.3
-TERMUX_PKG_SHA256=7561fcb2ee2c5e0300047d7798d7c203509913e61607cbf650cffdca5664ae56
+TERMUX_PKG_VERSION=0.4
+TERMUX_PKG_SHA256=9a8d42d211a7d461d61dcd4e3ef984014c2c2c696cfd6394bae389af13572627
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
