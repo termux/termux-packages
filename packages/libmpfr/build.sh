0276ae32d2c91bad73861ece6291df7bbe1ce362
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-11 00:19:37 +0100

    libmpfr&unrar: Bump packages
    These seems to work with DT_RUNPATH now (see #3490).

--- packages/libmpfr/build.sh
+++ packages/libmpfr/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="C library for multiple-precision floating-point computat
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_VERSION=4.0.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=1d3be708604eae0e42d578ba93b390c2a145f17743a744d8f3f8c2ad5855a38a
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/mpfr/mpfr-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_header_locale_h=no"
