d6f30daf5e7aeddc1b2fcc1d7554bab96739790e
Henrik Grimler <henrik@grimler.se>
2019-03-10 09:19:14 +0100

    ipset: update to 7.1

--- root-packages/ipset/build.sh
+++ root-packages/ipset/build.sh
@@ -1,10 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://ipset.netfilter.org
-TERMUX_PKG_VERSION=6.31
-TERMUX_PKG_SRCURL=http://ipset.netfilter.org/ipset-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=498e411cc1d134201a31a56def6c0936c642958c2d4b4ce7d9955240047a45fe
-TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_DESCRIPTION="Administration tool for kernel IP sets"
-TERMUX_PKG_DEPENDS="libmnl"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
+TERMUX_PKG_VERSION=7.1
+TERMUX_PKG_SRCURL=http://ipset.netfilter.org/ipset-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=7b5eb3b93205c20cdc39e3fc8b6e5f7bb214bf79a7c0c00729dd4a31ce16adc4
+
+TERMUX_PKG_DEPENDS="libmnl, libltdl"
+TERMUX_PKG_BUILD_DEPENDS="libtool"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-kmod=no
 "
