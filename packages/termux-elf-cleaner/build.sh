9711b01b2b06b56899569106f187eb72b5d06f49
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-12 14:20:07 +0200

    termux-elf-cleaner: update to v1.5
    Now it no longer removes GNU version sections.

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 # NOTE: The termux-elf-cleaner.cpp file is used by build-package.sh
 #       to create a native binary. Bumping this version will need
 #       updating the checksum used there.
-TERMUX_PKG_VERSION=1.4
+TERMUX_PKG_VERSION=1.5
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a88829b74bef2830fddd5b83eb6569db46dd8045c3eeb3670ab43382cfb7ab3c
+TERMUX_PKG_SHA256=7f4ee9e8921ae72054c9ad075d8c48bb3d44d71326ef0ef423b9add2e0461a09
 TERMUX_PKG_BUILD_IN_SRC=yes
