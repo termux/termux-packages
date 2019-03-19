5cdad9f78d3be23cc9aeead592747da336ae8a73
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-20 00:17:25 +0100

    nnn: Update from 2.3 to 2.4

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jarun/nnn
 TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=2.3
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=eaad2ccf0d781aeeb38fdfc4ad75a0405ca3da4f82ded64cce766a74a2b299ab
+TERMUX_PKG_VERSION=2.4
+TERMUX_PKG_SHA256=859ba5bd4892016aed9a4830ee1e1b03eb74e94c4f1bd82f0288dc559f7327eb
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="file, findutils, readline"
 TERMUX_PKG_BUILD_IN_SRC=yes
