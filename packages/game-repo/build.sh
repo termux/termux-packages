243d3bcdf7ea313b2cc4c84469c941f68152bd06
Henrik Grimler <henrik@grimler.se>
2019-03-20 10:13:17 +0100

    game-repo, science-repo: fix function formatting

--- packages/game-repo/build.sh
+++ packages/game-repo/build.sh
@@ -11,7 +11,7 @@ termux_step_make_install() {
 	echo "deb https://dl.bintray.com/grimler/game-packages-21 games stable" > $TERMUX_PREFIX/etc/apt/sources.list.d/game.list
 }
 
-termux_step_create_debscripts () {
+termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
 	echo "echo Downloading updated package list ..." >> postinst
 	echo "apt update" >> postinst
