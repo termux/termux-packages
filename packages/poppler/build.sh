da4ede9380e7bf2ba333a544984b3ce76b60b4c9
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-20 15:08:38 +0200

    poppler: enable glib bindings
    Needed for some x11 packages.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.74.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=92e09fd3302567fd36146b36bb707db43ce436e8841219025a82ea9fb0076b2f
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
-TERMUX_PKG_DEPENDS="fontconfig, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg, freetype, libcurl"
+TERMUX_PKG_DEPENDS="fontconfig, glib, libcairo, libpng, libjpeg-turbo, libtiff, littlecms, openjpeg, freetype, libcurl"
 #texlive needs the xpdf headers
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DENABLE_GLIB=OFF
+-DENABLE_GLIB=ON
 -DENABLE_UNSTABLE_API_ABI_HEADERS=ON
 -DENABLE_QT5=OFF
 -DFONT_CONFIGURATION=fontconfig
