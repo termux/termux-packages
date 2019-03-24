ed2db1e8dbb9db4c6a0440f83532dee83a8d2de3
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-24 23:07:49 +0100

    e2fsprogs: Avoid conflicting with krb5-dev

--- packages/e2fsprogs/build.sh
+++ packages/e2fsprogs/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="EXT 2/3/4 filesystem utilities"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0, MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=1.44.5
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v$TERMUX_PKG_VERSION/e2fsprogs-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=ba5eb3069d69160d96818bb9700de9ab5a8458d9add1fd85d427c0000d34c5b9
 TERMUX_PKG_KEEP_STATIC_LIBRARIES=true
@@ -23,9 +23,11 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-libblkid
 --disable-uuidd"
 
+# Remove com_err.h to avoid conflicting with krb5-dev:
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/compile_et
 bin/mk_cmds
+include/com_err.h
 share/et
 share/ss
 share/man/man1/compile_et.1
