c6bb9423333c0fc1491365d2796b9f545d53db87
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-03-25 16:26:38 +0200

    mime-support: fix license name

--- packages/mime-support/build.sh
+++ packages/mime-support/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/en/stretch/mime-support
 TERMUX_PKG_DESCRIPTION="MIME type associations for file types"
-TERMUX_PKG_LICENSE="Public-Domain"
+TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_VERSION=3.62
 TERMUX_PKG_SHA256=54e0a03e0cd63c7c9fe68a18ead0a2143fd3c327604215f989d85484d0409f4a
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/m/mime-support/mime-support_$TERMUX_PKG_VERSION.tar.gz
