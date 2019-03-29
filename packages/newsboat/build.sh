b6dc47b02ca6024224919dc0ce292664db5b0f27
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-30 00:30:10 +0100

    newsboat: Update from 2.14.1 to 2.15

--- packages/newsboat/build.sh
+++ packages/newsboat/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://newsboat.org/
 TERMUX_PKG_DESCRIPTION="RSS/Atom feed reader for the text console"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.14.1
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SHA256=4bd0d3b1901a3fc7e0ef73b800587c28181a57b175c36b547dbd84636330df66
+TERMUX_PKG_VERSION=2.15
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SHA256=da68ce93c02dda908a471ef8994bb3c668f060eb6046d486c3f05649c6650db7
 TERMUX_PKG_SRCURL=https://newsboat.org/releases/${TERMUX_PKG_VERSION}/newsboat-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, json-c, libsqlite, libcurl, libxml2, stfl, ncurses, openssl"
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -14,6 +14,9 @@ TERMUX_PKG_REPLACES=newsbeuter
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	export CXX_FOR_BUILD=g++
+	export CXXFLAGS_FOR_BUILD="-O2"
 
 	# Used by newsboat Makefile:
 	export CARGO_BUILD_TARGET=$CARGO_TARGET_NAME
