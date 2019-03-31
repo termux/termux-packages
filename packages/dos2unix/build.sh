2c7f8e733494e557054bbedca19c949967847bd2
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-31 19:17:00 +0200

    dos2unix: Rebuild package (fixes #3577)

--- packages/dos2unix/build.sh
+++ packages/dos2unix/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://waterlan.home.xs4all.nl/dos2unix.html
 TERMUX_PKG_DESCRIPTION="Converts between DOS and Unix text files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_VERSION=7.4.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://waterlan.home.xs4all.nl/dos2unix/dos2unix-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bac765abdbd95cdd87a71989d4382c32cf3cbfeee2153f0086cb9cf18261048a
 TERMUX_PKG_BUILD_IN_SRC=yes
