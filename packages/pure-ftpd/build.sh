7b160ef6a79e77037316cc473f8457abe89001de
Fredrik Fornwall <fredrik@fornwall.net>
2019-03-31 22:48:14 +0200

    pure-ftpd: Update from 1.0.47 to 1.0.48

--- packages/pure-ftpd/build.sh
+++ packages/pure-ftpd/build.sh
@@ -1,16 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://www.pureftpd.org/project/pure-ftpd
 TERMUX_PKG_DESCRIPTION="Pure-FTPd is a free (BSD), secure, production-quality and standard-conformant FTP server"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=1.0.47
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.0.48
+TERMUX_PKG_SHA256=b47f7d4582206ff0639c6849704dac7b5a0cf724b32d9de3485c61041ce521f7
 TERMUX_PKG_SRCURL=https://download.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=4740c316f5df879a2d68464489fb9b8b90113fe7dce58e2cdd2054a4768f27ad
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_elf_elf_begin=no
 ac_cv_lib_sodium_crypto_pwhash_scryptsalsa208sha256_str=no
 --with-ftpwho
---with-minimal
 --with-nonroot
 --with-puredb
 --with-tls
