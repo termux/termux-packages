164b0c14e3d4596c28a8e958cb6025cbd39c5236
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-03-31 20:54:22 +0300

    poppler: do not store glib-mkenums in deb file
    Sed modifies file timestamp. This causes file to be included into
    package.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.75.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=3bbaedb0fa2797cac933a0659d144303e4d09eec6892c65600da987d8707199a
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
@@ -16,5 +16,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	sed -i '1 i\#!/usr/bin/python' /data/data/com.termux/files/usr/bin/glib-mkenums
+	sed -i '1 i\#!/usr/bin/python' $TERMUX_PREFIX/bin/glib-mkenums
+}
+
+termux_step_post_massage() {
+	rm -f $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/glib-mkenums
 }
