619ac04ca90bb63ffb358b9d70243e0c6d054158
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-03-31 20:07:05 +0300

    poppler: quick fix for glib-mkenums execution

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.75.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=3bbaedb0fa2797cac933a0659d144303e4d09eec6892c65600da987d8707199a
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 # libcairo and littlecms is used by pdftocairo:
@@ -13,3 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_QT5=OFF
 -DFONT_CONFIGURATION=fontconfig
 "
+
+termux_step_pre_configure() {
+	sed -i '1 i\#!/usr/bin/python' /data/data/com.termux/files/usr/bin/glib-mkenums
+}
