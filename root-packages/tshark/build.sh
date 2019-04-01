52c2235b67d56c80a7565158b9276b10a28618a8
Henrik Grimler <henrik@grimler.se>
2019-04-01 22:21:24 +0200

    tshark: update to 2.6.7

--- root-packages/tshark/build.sh
+++ root-packages/tshark/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.wireshark.org/
 TERMUX_PKG_DESCRIPTION="Network protocol analyzer and sniffer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_VERSION=2.6.6
+TERMUX_PKG_VERSION=2.6.7
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/all-versions/wireshark-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=487933ea075bdbb25d8df06017d9c4f49fc20eb7f6ec80af086718ed5550e863
+TERMUX_PKG_SHA256=e3f4172cef86928d09cefb5eade249bc340a93a2565e4f622dc2514c7b36c1bb
 TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, libgnutls, openssl"
 # Needed by pkg-config (since they are dependenies for libgnutls and glib)
 TERMUX_PKG_BUILD_DEPENDS="pcre-dev, libnettle-dev"
