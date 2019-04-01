33d14df7754f9e99e6c3ee7d92d93601668adc9e
Henrik Grimler <henrik@grimler.se>
2019-04-01 22:33:19 +0200

    tshark: fix hashsum

--- root-packages/tshark/build.sh
+++ root-packages/tshark/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
 TERMUX_PKG_VERSION=2.6.7
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/all-versions/wireshark-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e3f4172cef86928d09cefb5eade249bc340a93a2565e4f622dc2514c7b36c1bb
+TERMUX_PKG_SHA256=747b3e7a37414942959f76f198be49dcbcca936bda538c4408942ce71bfd2b71
 TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, libgnutls, openssl"
 # Needed by pkg-config (since they are dependenies for libgnutls and glib)
 TERMUX_PKG_BUILD_DEPENDS="pcre-dev, libnettle-dev"
