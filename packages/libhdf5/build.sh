7af23331fe3c0a69cbe39d17738f7a14342004f4
Henrik Grimler <henrik@grimler.se>
2019-04-02 21:27:14 +0200

    libhdf5: update to 1.10.5

--- packages/libhdf5/build.sh
+++ packages/libhdf5/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://portal.hdfgroup.org/display/support
 TERMUX_PKG_DESCRIPTION="Hierarchical Data Format 5 (HDF5)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=1.10.4
-TERMUX_PKG_SHA256=1267ff06aaedc04ca25f7c6026687ea2884b837043431195f153401d942b28df
+TERMUX_PKG_VERSION=1.10.5
+TERMUX_PKG_SHA256=68d6ea8843d2a106ec6a7828564c1689c7a85714a35d8efafa2fee20ca366f44
 TERMUX_PKG_SRCURL=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${TERMUX_PKG_VERSION:0:4}/hdf5-$TERMUX_PKG_VERSION/src/hdf5-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_DEPENDS="libzopfli"
 TERMUX_PKG_FORCE_CMAKE=true
@@ -12,6 +12,8 @@ termux_step_pre_configure () {
 	cp $TERMUX_PKG_BUILDER_DIR/$TERMUX_ARCH/{H5Tinit.c,H5lib_settings.c} $TERMUX_PKG_BUILDDIR/
 	mkdir -p $TERMUX_PKG_BUILDDIR/shared/
 	cp $TERMUX_PKG_BUILDER_DIR/$TERMUX_ARCH/{H5Tinit.c,H5lib_settings.c} $TERMUX_PKG_BUILDDIR/shared/
+	touch $TERMUX_PKG_BUILDDIR/gen_SRCS.stamp1 $TERMUX_PKG_BUILDDIR/gen_SRCS.stamp2
+	touch $TERMUX_PKG_BUILDDIR/shared/shared_gen_SRCS.stamp1 $TERMUX_PKG_BUILDDIR/shared/shared_gen_SRCS.stamp2
 }
 
 termux_step_post_configure () {
