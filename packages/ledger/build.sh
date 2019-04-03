8572a185626e9d64bf3b5d27aa3985405e710df3
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-04 00:11:36 +0200

    ledger: Update from 3.1.2 to 3.1.3

--- packages/ledger/build.sh
+++ packages/ledger/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.ledger-cli.org
 TERMUX_PKG_DESCRIPTION="Powerful, double-entry accounting system"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.1.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=3ecebe00e8135246e5437e4364bb7a38869fad7c3250b849cf8c18ca2628182e
-TERMUX_PKG_SRCURL=https://github.com/ledger/ledger/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=3.1.3
+TERMUX_PKG_SHA256=b248c91d65c7a101b9d6226025f2b4bf3dabe94c0c49ab6d51ce84a22a39622b
+TERMUX_PKG_SRCURL=https://github.com/ledger/ledger/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="boost, libedit, libmpfr, utf8cpp, libgmp"
