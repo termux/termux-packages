37793f5231e7c1df4d421da2050c79096c51b20b
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-04 00:00:48 +0200

    wget: Update from 1.20.1 to 1.20.2

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
 TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.20.1
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=0f63e84dd23dc53ab3ab6f483c3afff8301e54c165783f772101cdd9b1c64928
+TERMUX_PKG_VERSION=1.20.2
+TERMUX_PKG_SHA256=84d3cbece8c08e130a8da0a72cf6e543a2adf58ca8ecf28726560b06243d4ce6
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_DEPENDS="pcre2, openssl, libuuid, libandroid-support, libunistring"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
