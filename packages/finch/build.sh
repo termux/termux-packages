d5e7b5d8dc300c9581031e032680f46cdbaeefb1
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-09 15:04:24 +0300

    finch: remove broken symlink libymsg.so
    Seems like libpurple/protocols/yahoo is no longer available and libymsh.so
    cannot be built.

--- packages/finch/build.sh
+++ packages/finch/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://pidgin.im/
 TERMUX_PKG_DESCRIPTION="Text-based multi-protocol instant messaging client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.13.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=2747150c6f711146bddd333c496870bfd55058bab22ffb7e4eb784018ec46d8f
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/pidgin/Pidgin/${TERMUX_PKG_VERSION}/pidgin-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="libgnutls, libxml2, ncurses-ui-libs, glib"
@@ -43,7 +44,7 @@ termux_step_post_configure() {
 
 termux_step_post_make_install() {
 	cd $TERMUX_PREFIX/lib
-	for lib in jabber oscar ymsg; do
+	for lib in jabber oscar; do
 		ln -f -s purple-2/lib${lib}.so .
 	done
 }
