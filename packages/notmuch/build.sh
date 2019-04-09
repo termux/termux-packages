a764d92b2de22fcfb33e03badcf4ead53914c656
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-09 17:47:50 +0300

    notmuch: rebuild to fix broken symlink in manpages

--- packages/notmuch/build.sh
+++ packages/notmuch/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://notmuchmail.org
 TERMUX_PKG_DESCRIPTION="Thread-based email index, search and tagging system"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=0.28.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=4e212d8b4ae30da04edb05d836dcdb569488ff6760706cecb882488eb1710eec
 TERMUX_PKG_SRCURL=https://notmuchmail.org/releases/notmuch-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
