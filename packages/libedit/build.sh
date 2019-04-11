c62cc9e3bf67fe29292e059313c77df402891d83
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-11 23:06:49 +0200

    libedit: Update from 20181209 to 20190324

--- packages/libedit/build.sh
+++ packages/libedit/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://thrysoee.dk/editline/
 TERMUX_PKG_DESCRIPTION="Library providing line editing, history, and tokenization functions"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=20181209-3.1
-TERMUX_PKG_SHA256=2811d70c0b000f2ca91b7cb1a37203134441743c4fcc9c37b0b687f328611064
+TERMUX_PKG_VERSION=20190324-3.1
+TERMUX_PKG_SHA256=ac8f0f51c1cf65492e4d1e3ed2be360bda41e54633444666422fbf393bba1bae
 TERMUX_PKG_SRCURL=https://thrysoee.dk/editline/libedit-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man7/editline.7 share/man/man3/history.3"
