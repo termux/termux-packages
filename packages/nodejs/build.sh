9d21678983e7d77e7bd2589778f3bb5ff3203abe
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-11 23:07:53 +0200

    nodejs: Update from 11.13.0 to 11.14.0

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=11.13.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=11.14.0
+TERMUX_PKG_SHA256=9df61d885765fd56148807092d019f07ad593c85faab2c7caaad21b8e9f66de7
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7411952da11f6033d4ca5993d68f52b0d7eb90538099102c37d5221c445dc38a
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
