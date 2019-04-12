5cb52a45034df5c0f7c48b3fd1b6f12e15e6f83f
Henrik Grimler <henrik@grimler.se>
2019-04-12 21:34:33 +0200

    libprotobuf-c: add missing dependency protobuf

--- packages/libprotobuf-c/build.sh
+++ packages/libprotobuf-c/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=1.3.1
 TERMUX_PKG_SHA256=51472d3a191d6d7b425e32b612e477c06f73fe23e07f6a6a839b11808e9d2267
 TERMUX_PKG_SRCURL=https://github.com/protobuf-c/protobuf-c/releases/download/v${TERMUX_PKG_VERSION}/protobuf-c-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libprotobuf"
+TERMUX_PKG_DEPENDS="libprotobuf, protobuf"
