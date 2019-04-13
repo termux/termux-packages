75bf13f98a14fddf4778d3f64ecac0c4b4f3e7ee
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-13 09:30:45 +0200

    cmatrix: Update from 1.2 to 2.0

--- packages/cmatrix/build.sh
+++ packages/cmatrix/build.sh
@@ -1,14 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/abishekvashok/cmatrix
 TERMUX_PKG_DESCRIPTION="Command producing a Matrix-style animation"
 TERMUX_PKG_LICENSE="GPL-3.0"
-local _REAL_VERSION=1.2
-TERMUX_PKG_VERSION=${_REAL_VERSION}b
-TERMUX_PKG_SHA256=6b0b9aff4585147843c4cf8a8c9c6048500f66dc4887a38922197dfa326b57c8
-TERMUX_PKG_SRCURL=https://github.com/abishekvashok/cmatrix/archive/${_REAL_VERSION}.tar.gz
+TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_SHA256=ad93ba39acd383696ab6a9ebbed1259ecf2d3cf9f49d6b97038c66f80749e99a
+TERMUX_PKG_SRCURL=https://github.com/abishekvashok/cmatrix/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
 TERMUX_PKG_DEPENDS="ncurses"
 
 termux_step_pre_configure() {
+	autoreconf -i
+
 	export ac_cv_file__usr_lib_kbd_consolefonts=no
 	export ac_cv_file__usr_share_consolefonts=no
 	export ac_cv_file__usr_lib_X11_fonts_misc=no
