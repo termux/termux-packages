7def3315187bee7f3d91f233d2398b7bb42c8ada
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-14 20:20:10 +0200

    libdb: Update from 6.2.32 to 18.1.32

--- packages/libdb/build.sh
+++ packages/libdb/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.oracle.com/database/berkeley-db
 TERMUX_PKG_DESCRIPTION="The Berkeley DB embedded database system (library)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION=6.2.32
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=http://download.oracle.com/berkeley-db/db-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a9c5e2b004a5777aa03510cfe5cd766a4a3b777713406b02809c17c8e0e7a8fb
+TERMUX_PKG_VERSION=18.1.32
+TERMUX_PKG_SHA256=fa1fe7de9ba91ad472c25d026f931802597c29f28ae951960685cde487c8d654
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/db-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-hash
 --enable-smallbuild
