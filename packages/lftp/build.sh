9058eb41706e059eb3e6d0999f764e87b79e8fbf
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-15 02:03:47 +0300

    lftp: specify correct zlib location

--- packages/lftp/build.sh
+++ packages/lftp/build.sh
@@ -12,12 +12,12 @@ ac_cv_func_dn_expand=no
 --with-openssl
 --with-expat=$TERMUX_PREFIX
 --with-readline=$TERMUX_PREFIX
+--with-zlib=$TERMUX_PREFIX
 "
 TERMUX_PKG_DEPENDS="libexpat, openssl, readline, libutil, libidn2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="ncurses-dev"
 
 termux_step_pre_configure() {
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-zlib=$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr"
 	if [ "$TERMUX_DEBUG" == "true" ]; then
 		# When doing debug build, -D_FORTIFY_SOURCE=2 gives this error:
 		# /home/builder/.termux-build/_lib/16-aarch64-21-v3/bin/../sysroot/usr/include/bits/fortify/string.h:79:26: error: use of undeclared identifier '__USE_FORTIFY_LEVEL'
