e67e71c740be14bd49191b3167898d68c19bb5b4
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-15 01:36:55 +0300

    nodejs-lts: link with shared zlib

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=10.15.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=4e22d926f054150002055474e452ed6cbb85860aa7dc5422213a2002ed9791d5
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
-TERMUX_PKG_DEPENDS="openssl, c-ares, libicu"
+TERMUX_PKG_DEPENDS="openssl, c-ares, libicu, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_CONFLICTS="nodejs"
@@ -39,6 +39,7 @@ termux_step_configure() {
 		--dest-os=android \
 		--shared-cares \
 		--shared-openssl \
+		--shared-zlib \
 		--with-intl=system-icu \
 		--without-snapshot \
 		--cross-compiling
