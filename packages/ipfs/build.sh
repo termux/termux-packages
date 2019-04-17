497693404e10d3414c03f8f9629ba1fe6bfae48a
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-17 23:57:24 +0200

    ipfs: Update from 0.4.19 to 0.4.20

--- packages/ipfs/build.sh
+++ packages/ipfs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://ipfs.io/
 TERMUX_PKG_DESCRIPTION="A peer-to-peer hypermedia distribution protocol"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=0.4.19
+TERMUX_PKG_VERSION=0.4.20
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=de0540e6d10aebbef602dc70da39c666952f980ceaccbd4aafb1578301706139
+TERMUX_PKG_SHA256=ddaeb8b8747efc81b560bb9c3e751c4277b089f3cd113e6001d1392993fb0fd0
 TERMUX_PKG_SRCURL=https://github.com/ipfs/go-ipfs/archive/v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_make() {
@@ -14,11 +14,11 @@ termux_step_make() {
     export GOARCH=${TERMUX_ARCH}
 
     if [ "${TERMUX_ARCH}" = "aarch64" ]; then
-	export GOARCH="arm64"
+        GOARCH="arm64"
     elif [ "${TERMUX_ARCH}" = "i686" ]; then
-	export GOARCH="386"
+        GOARCH="386"
     elif [ "${TERMUX_ARCH}" = "x86_64" ]; then
-	export GOARCH="amd64"
+        GOARCH="amd64"
     fi
 
     mkdir -p "${GOPATH}/src/github.com/ipfs"
@@ -26,6 +26,10 @@ termux_step_make() {
     cd "${GOPATH}/src/github.com/ipfs/go-ipfs"
 
     make build
+
+    # Fix folders without write permissions preventing which fails repeating builds:
+    cd $TERMUX_PKG_BUILDDIR
+    find . -type d -exec chmod u+w {} \;
 }
 
 termux_step_make_install() {
