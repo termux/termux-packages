968ab4195a2f6b76441eaea93f1ce6bbf6cdbf0e
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-18 00:07:42 +0200

    hugo: Update from 0.55.1 to 0.55.2

--- packages/hugo/build.sh
+++ packages/hugo/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://gohugo.io/
 TERMUX_PKG_DESCRIPTION="A fast and flexible static site generator"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.55.1
-TERMUX_PKG_SHA256=be05b4715d66b1de61da9ded58eaa33f7944729d5623ac65e734bce597101cf8
+TERMUX_PKG_VERSION=0.55.2
+TERMUX_PKG_SHA256=d423b3e14c48bc0217fcbae533d49c9f4c6581c30429c0e7505f34a55df53bf9
 TERMUX_PKG_SRCURL=https://github.com/gohugoio/hugo/archive/v$TERMUX_PKG_VERSION.tar.gz
 
 termux_step_make() {
