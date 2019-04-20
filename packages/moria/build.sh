78f707805c9786ed8c12b4b22504be12cff9c0cb
Henrik Grimler <henrik@grimler.se>
2019-04-20 23:50:54 +0200

    moria: allow out of src builds

--- packages/moria/build.sh
+++ packages/moria/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_SRCURL=https://github.com/dungeons-of-moria/umoria/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a43b695d42692245208e9fca4c89976c24947ea7931caa80f79e7f051305b512
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dbuild_dir=$TERMUX_PKG_BUILDDIR"
 
 termux_step_create_debscripts() {
     # Create scores file in a debscript, so an update to the package wouldn't erease any scores
