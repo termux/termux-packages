be0806e36e0b3b3b6ac762086bb1ba47659b9577
Henrik Grimler <henrik@grimler.se>
2019-04-20 09:15:37 +0200

    proj: add package

---
+++ packages/proj/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://trac.osgeo.org/proj/
+TERMUX_PKG_DESCRIPTION="Generic coordinate transformation software"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
+TERMUX_PKG_VERSION=6.0.0
+TERMUX_PKG_SHA256=8c2bc0b31ba266d59771bac14b589814a8e38b23822210b4dc038be737d61d7d
+TERMUX_PKG_SRCURL=https://github.com/OSGeo/proj.4/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_DEPENDS="libsqlite, sqlite"
