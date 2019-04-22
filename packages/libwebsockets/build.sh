ad1bbe320b26d52da6757abb7fe5cee16a497d1b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-04-22 18:24:39 +0300

    rebuild ttyd and libwebsockets
    On ARM libwebsockets header requires sys/capability.h for some reason. On
    AArch64 not.

--- packages/libwebsockets/build.sh
+++ packages/libwebsockets/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://libwebsockets.org
 TERMUX_PKG_DESCRIPTION="Lightweight C websockets library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=3.1.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=db948be74c78fc13f1f1a55e76707d7baae3a1c8f62b625f639e8f2736298324
 TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="openssl"
