2fdfcfbcdf63a52d15eceab3e9554116cb75a768
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-22 16:52:38 +0200

    libnghttp2: Update from 1.37.0 to 1.38.0

--- packages/libnghttp2/build.sh
+++ packages/libnghttp2/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://nghttp2.org/
 TERMUX_PKG_DESCRIPTION="nghttp HTTP 2.0 library"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.37.0
-TERMUX_PKG_SHA256=aa090b164b17f4b91fe32310a1c0edf3e97e02cd9d1524eef42d60dd1e8d47b7
+TERMUX_PKG_VERSION=1.38.0
+TERMUX_PKG_SHA256=ef75c761858241c6b4372fa6397aa0481a984b84b7b07c4ec7dc2d7b9eee87f8
 TERMUX_PKG_SRCURL=https://github.com/nghttp2/nghttp2/releases/download/v${TERMUX_PKG_VERSION}/nghttp2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-lib-only"
 # The tools are not built due to --enable-lib-only:
