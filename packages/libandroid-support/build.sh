d0658910c517d525e2a0fca262a2eb003c3f08ee
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-22 23:08:12 +0300

    new package: libiconv

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -2,24 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=24
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e14e262429a60bea733d5bed69d2f3a1cada53fcadaf76787fca5c8b0d4dae2f
-TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_ESSENTIAL=yes
 
 termux_step_make_install() {
-	# Remove files previously in package to avoid them being left
-	# in build environments.
-	rm -f $TERMUX_PREFIX/include/langinfo.h $TERMUX_PREFIX/include/iconv.h
-
 	_C_FILES="src/musl-*/*.c"
 	$CC $CFLAGS -std=c99 -DNULL=0 $CPPFLAGS $LDFLAGS \
 		-Iinclude \
 		$_C_FILES \
 		-shared -fpic \
 		-o $TERMUX_PREFIX/lib/libandroid-support.so
-
-	ln -sf libandroid-support.so $TERMUX_PREFIX/lib/libiconv.so
-
-	rm -Rf $TERMUX_PREFIX/include/libandroid-support/
 }
