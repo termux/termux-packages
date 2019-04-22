edfed6494610c585b0e85202fe019500a5842a71
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-04-22 23:08:12 +0300

    new package: libiconv

--- scripts/build/termux_step_setup_toolchain.sh
+++ scripts/build/termux_step_setup_toolchain.sh
@@ -128,19 +128,19 @@ termux_step_setup_toolchain() {
 				sed "s%\@TERMUX_HOME\@%${TERMUX_ANDROID_HOME}%g" | \
 				patch --silent -p1;
 		done
-		# elf.h: Taken from glibc since the elf.h in the NDK is lacking.
 		# ifaddrs.h: Added in android-24 unified headers, use a inline implementation for now.
+		# libintl.h: Inline implementation gettext functions.
 		# langinfo.h: Inline implementation of nl_langinfo().
-		# iconv.h: Header for iconv, implemented in libandroid-support.
-		cp "$TERMUX_SCRIPTDIR"/ndk-patches/{ifaddrs.h,libintl.h,langinfo.h,iconv.h} usr/include
+		cp "$TERMUX_SCRIPTDIR"/ndk-patches/{ifaddrs.h,libintl.h,langinfo.h} usr/include
 
 		# Remove <sys/capability.h> because it is provided by libcap-dev.
 		# Remove <sys/shm.h> from the NDK in favour of that from the libandroid-shmem.
 		# Remove <sys/sem.h> as it doesn't work for non-root.
 		# Remove <glob.h> as we currently provide it from libandroid-glob.
+		# Remove <iconv.h> as it's provided by libiconv.
 		# Remove <spawn.h> as it's only for future (later than android-27).
 		# Remove <zlib.h> and <zconf.h> as we build our own zlib
-		rm usr/include/sys/{capability.h,shm.h,sem.h} usr/include/{glob.h,spawn.h,zlib.h,zconf.h}
+		rm usr/include/sys/{capability.h,shm.h,sem.h} usr/include/{glob.h,iconv.h,spawn.h,zlib.h,zconf.h}
 
 		sed -i "s/define __ANDROID_API__ __ANDROID_API_FUTURE__/define __ANDROID_API__ $TERMUX_PKG_API_LEVEL/" \
 			usr/include/android/api-level.h
