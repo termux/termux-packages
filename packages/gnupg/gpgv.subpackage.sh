6b42510d2727b1fd84e6b6c0c8c53f460801b8d7
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-25 21:31:31 +0200

    gpgv: Add dependency on zlib

--- packages/gnupg/gpgv.subpackage.sh
+++ packages/gnupg/gpgv.subpackage.sh
@@ -2,4 +2,4 @@ TERMUX_SUBPKG_INCLUDE="bin/gpgv share/man/man1/gpgv.1.gz"
 TERMUX_SUBPKG_DESCRIPTION="GNU privacy guard - signature verification tool"
 # Conflict with older gnupg package before splitting out gpgv:
 TERMUX_SUBPKG_CONFLICTS="gnupg (<= 1.4.21)"
-TERMUX_SUBPKG_DEPENDS="libbz2, libgcrypt, libgpg-error"
+TERMUX_SUBPKG_DEPENDS="libbz2, libgcrypt, libgpg-error,zlib"
