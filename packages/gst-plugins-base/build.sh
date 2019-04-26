cdc307298ed6a8ee5801a023e32da4b4c64c36fc
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-26 22:43:31 +0200

    gst-plugins-base: Fix build
    Avoid picking up glib tools in $TERMUX_PREFIX/bin

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -7,6 +7,8 @@ TERMUX_PKG_SHA256=ca6139490e48863e7706d870ff4e8ac9f417b56f3b9e4b3ce490c13b09a774
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="gstreamer, libjpeg-turbo, libopus, libpng, libvorbis, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+GLIB_GENMARSHAL=/usr/bin/glib-genmarshal
+GLIB_MKENUMS=/usr/bin/glib-mkenums
 --disable-tests
 --disable-examples
 --disable-pango
