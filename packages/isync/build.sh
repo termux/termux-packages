db79db8c4bc72142342fdd6cc60b81c660a786bb
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-26 10:47:25 +0200

    isync: Fix incorrect TERMUX_PKG_DEPENDS

--- packages/isync/build.sh
+++ packages/isync/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://isync.sourceforge.net
 TERMUX_PKG_DESCRIPTION="IMAP and MailDir mailbox synchronizer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.3.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/isync/isync/${TERMUX_PKG_VERSION}/isync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8d5f583976e3119705bdba27fa4fc962e807ff5996f24f354957178ffa697c9c
-TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl ac_cv_header_db=no ac_cv_berkdb4=no"
-TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_DEPENDS="openssl, zlib"
