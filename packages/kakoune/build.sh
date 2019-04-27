b41bd0604b8ec0bbacaa5f26a9230ff18cdb324a
Fredrik Fornwall <fredrik@fornwall.net>
2019-04-27 22:15:58 +0200

    kakoune: Build from tar.bz2 file
    This fixes 'kak -version', see
    https://github.com/mawww/kakoune/issues/2540

--- packages/kakoune/build.sh
+++ packages/kakoune/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mawww/kakoune
 TERMUX_PKG_DESCRIPTION="Code editor heavily inspired by Vim"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_VERSION=2019.01.20
-TERMUX_PKG_SHA256=0d116e7990134a6dba5cad47c5cded39b86eb8c5e061dc43a42c279ec71b9765
-TERMUX_PKG_SRCURL=https://github.com/mawww/kakoune/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SHA256=991103a227be00ca1b10ad575fd6c749fa4c99eb19763971c7b1e113e299b995
+TERMUX_PKG_SRCURL=https://github.com/mawww/kakoune/releases/download/v$TERMUX_PKG_VERSION/kakoune-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src debug=no"
