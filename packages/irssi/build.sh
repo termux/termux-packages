dd714454bf148002011b7b54242c23bd4ce3ff8d
Henrik Grimler <henrik@grimler.se>
2019-04-29 16:35:25 +0200

    irssi: depend on utf8proc
    Fixes https://github.com/termux/termux-packages/issues/3732

--- packages/irssi/build.sh
+++ packages/irssi/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://irssi.org/
 TERMUX_PKG_DESCRIPTION="Terminal based IRC client"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_DEPENDS="ncurses, openssl, glib, libandroid-glob"
+TERMUX_PKG_DEPENDS="ncurses, openssl, glib, libandroid-glob, utf8proc"
 TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=1643fca1d8b35e5a5d7b715c9c889e1e9cdb7e578e06487901ea959e6ab3ebe5
 TERMUX_PKG_SRCURL=https://github.com/irssi/irssi/releases/download/$TERMUX_PKG_VERSION/irssi-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_BUILD_IN_SRC=yes
