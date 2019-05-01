e0fc0fcd108f97d2fb3306a6e5cbf74007f8cdb1
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-02 01:37:03 +0300

    libcap: update to 2.27

--- packages/libcap/build.sh
+++ packages/libcap/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://sites.google.com/site/fullycapable/
 TERMUX_PKG_DESCRIPTION="POSIX 1003.1e capabilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=2.26
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.27
 TERMUX_PKG_SRCURL=https://kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b630b7c484271b3ba867680d6a14b10a86cfa67247a14631b14c06731d5a458b
+TERMUX_PKG_SHA256=dac1792d0118bee6aae6ba7fb93ff1602c6a9bda812fd63916eee1435b9c486a
 TERMUX_PKG_DEPENDS="attr"
 TERMUX_PKG_BUILD_IN_SRC=true
 
