a52eb5792d1d9094f8e585af586e9d7aea3ac97a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-02 01:40:53 +0300

    unrar: update to 5.7.5

--- packages/unrar/build.sh
+++ packages/unrar/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.rarlab.com/
 TERMUX_PKG_DESCRIPTION="Tool for extracting files from .rar archives"
-TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_VERSION=5.7.4
-TERMUX_PKG_SHA256=582dd038fd4632f32493928cae5b37dbb436752813da08a1ee5df2ab1ee7e7b4
+TERMUX_PKG_LICENSE="non-free"
+TERMUX_PKG_VERSION=5.7.5
+TERMUX_PKG_SHA256=e1c2fddaa87a88b1535bfc10ca484f3c5af4e5a55fbb933f8819e26203bbe2ee
 TERMUX_PKG_SRCURL=https://www.rarlab.com/rar/unrarsrc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
