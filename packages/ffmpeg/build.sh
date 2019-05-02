82b19b3d6c120e84a71458de25156a62e6d60291
Tom Yan <tom.ty89@gmail.com>
2019-05-02 23:17:05 +0800

    ffmpeg: enable lzma

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -3,13 +3,10 @@ TERMUX_PKG_DESCRIPTION="Tools and libraries to manipulate a wide range of multim
 TERMUX_PKG_LICENSE="GPL-3.0"
 # NOTE: mpv has to be rebuilt and version bumped after updating ffmpeg.
 TERMUX_PKG_VERSION=4.1.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=0c3020452880581a8face91595b239198078645e7d7184273b8bcc7758beb63d
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
-# libbz2 is used by matroska decoder:
-# libvpx is the VP8 & VP9 video encoder for WebM, see
-# https://trac.ffmpeg.org/wiki/Encode/VP8 and https://trac.ffmpeg.org/wiki/Encode/VP9
-TERMUX_PKG_DEPENDS="libbz2, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib"
+TERMUX_PKG_DEPENDS="libbz2, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma"
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="share/ffmpeg/examples"
 TERMUX_PKG_CONFLICTS="libav"
 
@@ -36,9 +33,6 @@ termux_step_configure() {
 		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
 	fi
 
-	# --disable-lzma to avoid problem with shared library clashes, see
-	# https://github.com/termux/termux-packages/issues/511
-	# Only used for LZMA compression support for tiff decoder.
 	$TERMUX_PKG_SRCDIR/configure \
 		--arch=${_ARCH} \
 		--as=$AS \
@@ -48,7 +42,6 @@ termux_step_configure() {
 		--disable-avdevice \
 		--disable-static \
 		--disable-symver \
-		--disable-lzma \
 		--enable-cross-compile \
 		--enable-gnutls \
 		--enable-gpl \
