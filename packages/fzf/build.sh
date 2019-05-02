fb6a2c85ad6c34848874332ed7d06fd111ede74a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-02 03:03:02 +0300

    fzf: fix build errors

--- packages/fzf/build.sh
+++ packages/fzf/build.sh
@@ -4,38 +4,31 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.18.0
 TERMUX_PKG_SHA256=5406d181785ea17b007544082b972ae004b62fb19cdb41f25e265ea3cc8c2d9d
 TERMUX_PKG_SRCURL=https://github.com/junegunn/fzf/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_BUILD_IN_SRC="yes"
+
 # Depend on findutils as fzf uses the -fstype option, which busybox
 # find does not support, when invoking find:
 TERMUX_PKG_DEPENDS="bash, findutils"
 
 termux_step_make() {
-	:
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+
+	mkdir -p $GOPATH/src/github.com/junegunn
+	ln -sf $TERMUX_PKG_SRCDIR $GOPATH/src/github.com/junegunn/fzf
+
+	cd $GOPATH/src/github.com/junegunn/fzf
+	go get -d -v github.com/junegunn/fzf
+	go build
 }
 
 termux_step_make_install() {
-	termux_setup_golang
-	export CGO_CFLAGS="-I$TERMUX_PREFIX/include"
+	cd $GOPATH/src/github.com/junegunn/fzf
 
-	# See the fzf Makefile:
-	local _BINARY="target/fzf-${GOOS}_"
-	if [ $TERMUX_ARCH = "arm" ]; then
-		_BINARY+="arm7"
-	elif [ $TERMUX_ARCH = "i686" ]; then
-		_BINARY+="386"
-	elif [ $TERMUX_ARCH = "x86_64" ]; then
-		_BINARY+="amd64"
-	elif [ $TERMUX_ARCH = "aarch64" ]; then
-		_BINARY+="arm8"
-	else
-		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
-	fi
-
-	LDFLAGS="-pie" make $_BINARY
-	cp $_BINARY $TERMUX_PREFIX/bin/fzf
+	install -Dm700 fzf $TERMUX_PREFIX/bin/fzf
 
 	# Install fzf-tmux, a bash script for launching fzf in a tmux pane:
-	cp $TERMUX_PKG_SRCDIR/bin/fzf-tmux $TERMUX_PREFIX/bin
+	install -Dm700 $TERMUX_PKG_SRCDIR/bin/fzf-tmux $TERMUX_PREFIX/bin/fzf-tmux
 
 	# Install the fzf.1 man page:
 	mkdir -p $TERMUX_PREFIX/share/man/man1/
@@ -50,8 +43,8 @@ termux_step_make_install() {
 	cp $TERMUX_PKG_SRCDIR/shell/completion.bash $TERMUX_PREFIX/share/bash-completion/completions/fzf
 
 	# Install the rest of the shell scripts:
-	mkdir -p "$TERMUX_PREFIX/share/fzf"
-	cp $TERMUX_PKG_SRCDIR/shell/* "$TERMUX_PREFIX/share/fzf"
+	mkdir -p $TERMUX_PREFIX/share/fzf
+	cp $TERMUX_PKG_SRCDIR/shell/* $TERMUX_PREFIX/share/fzf/
 
 	# Install the nvim plugin:
 	mkdir -p $TERMUX_PREFIX/share/nvim/runtime/plugin
