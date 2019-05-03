e58154615ed9ddf69c77bcdfa8e3d171a8790409
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-03 17:21:10 +0300

    imagemagick: rebuild due to versioned liblzma

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_VERSION=7.0.8.40
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=f82e2aace538c2cefcb777dc0c5f60a239b7d1abdde4c2989f26be3daf0910f0
 local _download_version=$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/')
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/${_download_version}.tar.gz
