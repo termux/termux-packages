45c5a7568f1aaf0e680e0de4b3d7282a56af5790
Michal Bednarski <bednarski.michal2@gmail.com>
2019-05-05 17:31:43 +0200

    proot: Update for nested chroot() support
    termux/proot#34

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://proot-me.github.io/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
 TERMUX_PKG_LICENSE="GPL-2.0"
 # Just bump commit and version when needed:
-_COMMIT=2a78bab91d01c723ecb7ce08069a096f6ff654c5
+_COMMIT=0717de26d1394fec3acf90efdc1d172e01bc932b
 TERMUX_PKG_VERSION=5.1.107
-TERMUX_PKG_REVISION=20
+TERMUX_PKG_REVISION=21
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=44cfaa66f44d5fe2b01e7f5549410966593f5a95be5ee767e4919b42ab7b02df
+TERMUX_PKG_SHA256=0ff540373197cab850f33de9b35818bcf0b098596adeee36d44e6f68c50eb7ee
 TERMUX_PKG_DEPENDS="libtalloc"
 
 # Install loader in libexec instead of extracting it every time
