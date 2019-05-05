a230e33e4f6abe4d8d2c5cdcd4dbfced9e5da188
Henrik Grimler <henrik@grimler.se>
2019-05-05 20:09:47 +0200

    termux_step_install_license: use license file from $TERMUX_PKG_LICENSE_FILE if specified
    Otherwise use the licenses specified in $TERMUX_PKG_LICENSE

--- scripts/build/termux_step_install_license.sh
+++ scripts/build/termux_step_install_license.sh
@@ -1,21 +1,22 @@
 termux_step_install_license() {
 	mkdir -p "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME"
-	local COUNTER=0
-	for LICENSE in $(echo $TERMUX_PKG_LICENSE | sed 's/,/ /g'); do
-		if [ -f "$TERMUX_SCRIPTDIR/packages/termux-licenses/LICENSES/${LICENSE}.txt" ]; then
-			if [[ $COUNTER > 0 ]]; then
-				ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE.${COUNTER}"
-			else
-				ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE"
-			fi
-		else
-			for POSSIBLE_LICENSE in "LICENSE*" "license*" "COPYRIGHT" "copyright"; do
-				if [ -f "$TERMUX_PKG_SRCDIR/$POSSIBLE_LICENSE" ]; then
-					cp "$TERMUX_PKG_SRCDIR/$POSSIBLE_LICENSE" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/"
-					break
+	if [ ! "${TERMUX_PKG_LICENSE_FILE}" = "" ]; then
+		for LICENSE in $TERMUX_PKG_LICENSE_FILE; do
+			if [ ! -f "$TERMUX_PKG_SRCDIR/$LICENSE" ]; then termux_error_exit "$TERMUX_PKG_SRCDIR/$LICENSE does not exist"; fi
+			cp ${TERMUX_PKG_SRCDIR}/${LICENSE} ${TERMUX_PREFIX}/share/${TERMUX_PKG_NAME}
+		done
+	else
+		local COUNTER=0
+		for LICENSE in $(echo $TERMUX_PKG_LICENSE | sed 's/,/ /g'); do
+			echo "checking for $TERMUX_SCRIPTDIR/packages/termux-licenses/LICENSES/${LICENSE}.txt"
+			if [ -f "$TERMUX_SCRIPTDIR/packages/termux-licenses/LICENSES/${LICENSE}.txt" ]; then
+				if [[ $COUNTER > 0 ]]; then
+					ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE.${COUNTER}"
+				else
+					ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE"
 				fi
-			done
-		fi
-		COUNTER=$((COUNTER + 1))
-	done
+			fi
+			COUNTER=$((COUNTER + 1))
+		done
+	fi
 }
