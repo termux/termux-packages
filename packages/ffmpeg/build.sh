d85fd3862a588b2647eced4ec4950e2c77c19dd3
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-05 17:45:07 +0300

    add libiconv to dependencies where needed

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Tools and libraries to manipulate a wide range of multim
 TERMUX_PKG_LICENSE="GPL-3.0"
 # NOTE: mpv has to be rebuilt and version bumped after updating ffmpeg.
 TERMUX_PKG_VERSION=4.1.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SHA256=0c3020452880581a8face91595b239198078645e7d7184273b8bcc7758beb63d
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_DEPENDS="libbz2, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma"
+TERMUX_PKG_DEPENDS="libbz2, libiconv, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma"
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="share/ffmpeg/examples"
 TERMUX_PKG_CONFLICTS="libav"
 
--- packages/mtools/build.sh
+++ packages/mtools/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/mtools/
 TERMUX_PKG_DESCRIPTION="Tool for manipulating FAT images."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=4.0.23
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=f188db26751aeb5692a79b2380b440ecc05fd1848a52f869d7ca1193f2ef8ee3
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/mtools/mtools-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_DEPENDS="libandroid-support"
+TERMUX_PKG_DEPENDS="libandroid-support, libiconv"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-floppyd
 ac_cv_lib_bsd_main=no
