a9800780fc01ec01ac8b8353a3eee53255e3f8d1
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-05 21:07:06 +0200

    libunibilium: Include pkg-config file

--- packages/libunibilium/build.sh
+++ packages/libunibilium/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mauke/unibilium
 TERMUX_PKG_DESCRIPTION="Terminfo parsing library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=78997d38d4c8177c60d3d0c1aa8c53fd0806eb21825b7b335b1768d7116bc1c1
 TERMUX_PKG_SRCURL=https://github.com/mauke/unibilium/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -17,4 +18,10 @@ termux_step_make_install() {
 	$CC $CFLAGS -c -fPIC uniutil.c -o uniutil.o
 	$CC -shared -fPIC $LDFLAGS -o $TERMUX_PREFIX/lib/libunibilium.so unibilium.o uninames.o uniutil.o
 	cp unibilium.h $TERMUX_PREFIX/include/
+
+	mkdir -p $PKG_CONFIG_LIBDIR
+	sed "s|@VERSION@|$TERMUX_PKG_VERSION|" unibilium.pc.in | \
+		sed "s|@INCDIR@|$TERMUX_PREFIX/include|" | \
+		sed "s|@LIBDIR@|$TERMUX_PREFIX/lib|" > \
+		$PKG_CONFIG_LIBDIR/unibilium.pc
 }
