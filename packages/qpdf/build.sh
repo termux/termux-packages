7a4ad15dd90019d85908448d355a47bf4eea242e
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-06 00:08:29 +0200

    qpdf: Update from 8.4.0 to 8.4.1

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Content-Preserving PDF Transformation System"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=8.4.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=71d9b6c77ac56521d86a481cb9e52c0774de106c1dcf7a0644a05bd1230be87e
+TERMUX_PKG_VERSION=8.4.1
+TERMUX_PKG_SHA256=b8068d4ce23b5c8b3600fd83d6d88744f897c1e1b62a63bbff668e72984c38bb
 TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/archive/release-qpdf-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libjpeg-turbo, zlib"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
