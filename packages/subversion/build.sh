3a36f89eef81a4064d9d297112fa3f1f7de2fe5d
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-06 13:13:27 +0200

    subversion: Update from 1.11.1 to 1.12.0

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -1,12 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://subversion.apache.org
 TERMUX_PKG_DESCRIPTION="Centralized version control system characterized by its simplicity"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.11.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=9efd2750ca4d72ec903431a24b9c732b6cbb84aad9b7563f59dd96dea5be60bb
+TERMUX_PKG_VERSION=1.12.0
+TERMUX_PKG_SHA256=7fae7c73d8a007c107c0ae5eb372bc0bb013dbfe966fcd5c59cd5a195a5e2edf
 TERMUX_PKG_SRCURL=https://www.apache.org/dist/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite, liblz4, utf8proc, zlib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl --without-libmagic"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+svn_cv_pycfmt_apr_int64_t=UNUSED_REMOVE_AFTER_NEXT_UPDATE
+--without-sasl
+--without-libmagic
+"
 
 termux_step_pre_configure() {
 	CFLAGS+=" -std=c11"
