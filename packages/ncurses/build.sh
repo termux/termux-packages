1724f2a22d71f69e31f7e42d972ac9d33f57c4d9
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-06 17:27:42 +0300

    ncurses: bump revision after 0d1170cc

--- packages/ncurses/build.sh
+++ packages/ncurses/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for text-based user interfaces in a terminal-ind
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=(6.1.20181117
 		    9.22)
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=(b5568d53d31787cda727f949b68bcb60c667dba5deff72bd7ade40dbe45e4b16
 		   e94628e9bcfa0adb1115d83649f898d6edb4baced44f5d5b769c2eeb8b95addd)
 TERMUX_PKG_SRCURL=(https://dl.bintray.com/termux/upstream/ncurses-${TERMUX_PKG_VERSION:0:3}-${TERMUX_PKG_VERSION:4}.tgz
