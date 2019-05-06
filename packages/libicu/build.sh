e19bf0852b828a44d36ec79777872c0aabbd083e
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-06 17:24:58 +0300

    libicu: bump revision after 0d1170cc

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://site.icu-project.org/home
 TERMUX_PKG_DESCRIPTION='International Components for Unicode library'
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=64.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=09762184afa33c3b1042715192da1777f9fda31688cab5b03b8b71fad1dcd0c7
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/icu4c-${TERMUX_PKG_VERSION//./_}-src.tar.xz
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/icu-config share/man/man1/icu-config.1 lib/icu share/icu"
