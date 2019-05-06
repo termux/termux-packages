3378cba92064ac840a2e4d14ba9d69f4f1cfb80f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-06 17:26:19 +0300

    ruby: bump revision after 0d1170cc

--- packages/ruby/ruby-ri.subpackage.sh
+++ packages/ruby/ruby-ri.subpackage.sh
@@ -1,4 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="share/ri share/man/man1/ri.1.gz bin/ri"
 TERMUX_SUBPKG_DESCRIPTION="Ruby documentation index"
-TERMUX_SUBPKG_DEPENDS="ruby"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
