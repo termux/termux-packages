d68feb774221d7ec4c06def8b5460a67e3274654
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-06 16:54:19 +0300

    gnupg: bump revision after 0d1170cc

--- packages/gnupg/dirmngr.subpackage.sh
+++ packages/gnupg/dirmngr.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="bin/dirmngr bin/dirmngr-client share/man/man8/dirmngr.8.gz share/man/man1/dirmngr-client.1.gz"
 TERMUX_SUBPKG_DESCRIPTION="Server for managing certificate revocation lists"
-TERMUX_SUBPKG_DEPENDS="gnupg (>= 2.2.9-1), libgnutls, resolv-conf"
+TERMUX_SUBPKG_DEPENDS="libgnutls, resolv-conf"
