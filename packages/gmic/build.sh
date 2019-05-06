b34f67f9472681c16d94416dcbd47b3f49bbb9f6
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-07 01:02:58 +0300

    gmic fix for ARM

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
 TERMUX_PKG_LICENSE="CeCILL-2.1"
 TERMUX_PKG_VERSION=2.5.7
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=1cc71b48c1c3da928031b7f32cf3fe4757a07b4f21c3af4167389d54cf2dee5f
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="graphicsmagick++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
@@ -16,7 +16,11 @@ termux_step_configure() {
 
 termux_step_make() {
 	cd src/
-	make cli
+	if [ $TERMUX_ARCH = "arm" ]; then
+		make cli  LIBS=" -lunwind -L/data/data/com.termux/files/usr/lib  -lfftw3 -lfftw3_threads -lcurl -lpng16 -lz -ljpeg -ltiff -lGraphicsMagick++ -lGraphicsMagick"
+	else
+		make cli
+	fi
 }
 
 termux_step_make_install() {
