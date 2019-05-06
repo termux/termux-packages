dffdfc671a93aa43cdd0eb5102a6bca7f13388cf
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-06 19:47:43 +0300

    rust: bump revision after 0d1170cc

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed an
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
 TERMUX_PKG_VERSION=1.34.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=e0efb1e6aba0d4900de57bd2db64e32e7c5b440a95a675d5303839c9a2c3328f
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
 TERMUX_PKG_DEPENDS="clang, openssl, lld, zlib"
