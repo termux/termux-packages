c2e94ae8abea5d5e7598897f1b95fa6ccf46fe64
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-06 17:25:39 +0300

    weechat: bump revision after 0d1170cc

--- packages/weechat/weechat-python-plugin.subpackage.sh
+++ packages/weechat/weechat-python-plugin.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="lib/weechat/plugins/python.so"
 TERMUX_SUBPKG_DESCRIPTION="WeeChat Python3 Plugin"
-TERMUX_SUBPKG_DEPENDS="weechat, python"
+TERMUX_SUBPKG_DEPENDS="python"
--- packages/weechat/weechat-ruby-plugin.subpackage.sh
+++ packages/weechat/weechat-ruby-plugin.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="lib/weechat/plugins/ruby.so"
 TERMUX_SUBPKG_DESCRIPTION="WeeChat Ruby Plugin"
-TERMUX_SUBPKG_DEPENDS="weechat, ruby"
+TERMUX_SUBPKG_DEPENDS="ruby"
