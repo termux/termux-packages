335a1d4c1420293c0d6aa4b2eeb6453a99fe0248
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-08 01:41:26 +0200

    pick: Update from 2.0.2 to 3.0.1

--- packages/pick/build.sh
+++ packages/pick/build.sh
@@ -1,8 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/calleerlandsson/pick
 TERMUX_PKG_DESCRIPTION="Utility to choose one option from a set of choices with fuzzy search functionality"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.0.2
-TERMUX_PKG_SHA256=f2b43aaa540ad3ff05a256a531c2f47d3d95145b82c1d1b0d62dfb40d793d385
+TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_SHA256=668c863751f94ad90e295cf861a80b4d94975e06645f401d7f82525e607c0266
 TERMUX_PKG_SRCURL=https://github.com/calleerlandsson/pick/releases/download/v${TERMUX_PKG_VERSION}/pick-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_search_setupterm=-lncurses"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_pre_configure() {
+	export MANDIR=$TERMUX_PREFIX/share/man
+}
