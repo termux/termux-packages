be06ea11381866c5fe64f61756f296d5df66832a
Tom Yan <tom.ty89@gmail.com>
2019-05-09 08:33:45 +0800

    graphicsmagick: unsplit graphicsmagick++{,-dev}

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
 TERMUX_PKG_LICENSE="CeCILL-2.1"
 TERMUX_PKG_VERSION=2.6.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=48de7045a36eb718d55b0dfd68a797380b5390af99c9737a4dfba0fb678ed2b4
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_DEPENDS="graphicsmagick++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
-TERMUX_PKG_BUILD_DEPENDS="graphicsmagick-dev"
+TERMUX_PKG_DEPENDS="graphicsmagick, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_NO_DEVELSPLIT=yes
 TERMUX_PKG_BUILD_IN_SRC=yes
 
