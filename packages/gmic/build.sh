1102ba8ec95d2988e14b00dda8a82ded698a824a
Tom Yan <tom.ty89@gmail.com>
2019-05-11 00:39:24 +0800

    gmic: workaround libunwind.a linking issue with a better approach
    also building variant that does not make use of graphicsmagick

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
 TERMUX_PKG_LICENSE="CeCILL-2.1"
 TERMUX_PKG_VERSION=2.6.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SHA256=48de7045a36eb718d55b0dfd68a797380b5390af99c9737a4dfba0fb678ed2b4
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_DEPENDS="graphicsmagick, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
+TERMUX_PKG_DEPENDS="libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
+TERMUX_PKG_BUILD_DEPENDS="graphicsmagick"
 TERMUX_PKG_NO_DEVELSPLIT=yes
 TERMUX_PKG_BUILD_IN_SRC=yes
 
@@ -15,17 +16,14 @@ termux_step_configure() {
 
 termux_step_make() {
 	cd src/
-	if [ $TERMUX_ARCH = "arm" ]; then
-		make cli  LIBS=" -lunwind -L/data/data/com.termux/files/usr/lib  -lfftw3 -lfftw3_threads -lcurl -lpng16 -lz -ljpeg -ltiff -lGraphicsMagick++ -lGraphicsMagick"
-	else
-		make cli
-	fi
+	make USR="$TERMUX_PREFIX" STRIP="$STRIP" \
+	     CFLAGS="$CXXFLAGS" LIBS="$LDFLAGS -lgcc" cli
 }
 
 termux_step_make_install() {
-	cp src/gmic $TERMUX_PREFIX/bin/gmic
-	cp src/*.h $TERMUX_PREFIX/include/
-	gunzip man/gmic.1.gz
-	cp man/gmic.1 $TERMUX_PREFIX/share/man/man1
+	cp src/gmic $TERMUX_PREFIX/bin/
+	cp src/gmic-gm $TERMUX_PREFIX/bin/
+	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/
+	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/gmic-gm.1.gz
 }
 
