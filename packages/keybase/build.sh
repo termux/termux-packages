7a47a12900c320f81c325be0972f475c20800e90
Brandon Philips <brandon.philips@coreos.com>
2019-05-10 18:18:04 -0700

    packages: bump keybase to v4.0.0
    - bump to the latest release on Github
    - drop kbfs package, kbfs moved to keybase/client
    
    Warning! An environment variable needs to be set to make this work:
    
    See https://github.com/keybase/client/issues/17203

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -1,10 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://keybase.io
 TERMUX_PKG_DESCRIPTION="Key directory that maps social media identities to encryption keys"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.2.2
+TERMUX_PKG_VERSION=4.0.0
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=ed977c45f3a888d291aaf46afc1e1e32e4851d44b26a39854f00a0003b7556b5
+TERMUX_PKG_SHA256=503e068e7e89a57c497e40cee5b161704c23919bc59846a6507dfc5ecd418091
 TERMUX_PKG_SRCURL=https://github.com/keybase/client/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REPLACES="kbfs"
+TERMUX_PKG_CONFLICTS="kbfs"
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
@@ -16,6 +18,23 @@ termux_step_make_install() {
 	export GOPATH="$PWD/.gopath"
 
 	go build -v -tags 'production' -o keybase github.com/keybase/client/go/keybase
+	go build -v -tags 'production' -o git-remote-keybase github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase
+	go build -v -tags 'production' -o kbfsfusebin github.com/keybase/client/go/kbfs/kbfsfuse
 
 	cp keybase $TERMUX_PREFIX/bin/keybase
+	cp git-remote-keybase $TERMUX_PREFIX/bin/git-remote-keybase
+	cp kbfsfusebin $TERMUX_PREFIX/bin/kbfsfuse
+
+	mkdir -p $TERMUX_PREFIX/etc/profile.d/
+	echo "export KEYBASE_SECRET_STORE_FILE=1" > $TERMUX_PREFIX/etc/profile.d/keybase.sh
+}
+
+termux_step_create_debscripts() {
+    {
+	echo "#!$TERMUX_PREFIX/bin/sh"
+	echo "echo Before using keybase restart the termux session. Otherwise,"
+	echo "echo keybase will throw an error about a not functional secret store"
+	echo "exit 0"
+    } > postinst
+    chmod 0755 postinst
 }
