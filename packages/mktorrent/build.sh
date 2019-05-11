0d1920a0611389a94a80ea79a1fac20197a214cd
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-11 18:27:56 +0300

    move some packages from unstable repository
    New packages that will be added:
    
     * antiword
     * beanshell
     * cabextract
     * deutex
     * direnv
     * exiv2
     * git-lfs
     * gitea
     * hoedown
     * httrack
     * iverilog
     * links
     * loksh
     * mksh
     * mktorrent
     * pdfgrep
     * shellharden
     * swig
     * tidy
     * tweego
     * wordgrinder

---
+++ packages/mktorrent/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=http://mktorrent.sourceforge.net
+TERMUX_PKG_DESCRIPTION="command line utility to create BitTorrent metainfo files"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://github.com/Rudde/mktorrent/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=d0f47500192605d01b5a2569c605e51ed319f557d24cfcbcb23a26d51d6138c9
+TERMUX_PKG_BUILD_IN_SRC=yes
