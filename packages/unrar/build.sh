38c3a5d5b60ed44cedc215bb2830454dee2539bb
Henrik Grimler <henrik@grimler.se>
2019-05-11 06:43:40 +0200

    Specify license files for packages with custom licenses

--- packages/unrar/build.sh
+++ packages/unrar/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_SHA256=e1c2fddaa87a88b1535bfc10ca484f3c5af4e5a55fbb933f8819e26203bbe2
 TERMUX_PKG_SRCURL=https://www.rarlab.com/rar/unrarsrc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_LICENSE_FILE="license.txt"
