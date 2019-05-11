aec3cab6ed09b9ff1c1dcf69e69d6a05c1e37db2
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-11 23:27:58 +0200

    glib: Update from 2.58.3 to 2.60.2

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=2.58.3
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=8f43c31767e88a25da72b52a40f3301fefc49a665b56dc10ee7cc9565cbe7481
+TERMUX_PKG_VERSION=2.60.2
+TERMUX_PKG_SHA256=2ef15475060addfda0443a7e8a52b28a10d5e981e82c083034061daf9a8f80d9
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
 # libandroid-support to get langinfo.h in include path.
 TERMUX_PKG_DEPENDS="libffi, pcre, libandroid-support, zlib"
@@ -11,31 +10,11 @@ TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc lib/locale share/glib-2.0/gettext sha
 # Needed by pkg-config for glib-2.0:
 TERMUX_PKG_DEVPACKAGE_DEPENDS="pcre-dev"
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="lib/glib-2.0/include"
-
-# --enable-compile-warnings=no to get rid of format strings causing errors.
-# --disable-znodelete to avoid DF_1_NODELETE which most Android 5.0 linkers does not support.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---cache-file=termux_configure.cache
---disable-compile-warnings
---disable-gtk-doc
---disable-gtk-doc-html
---disable-libelf
---disable-libmount
---disable-znodelete
---with-pcre=system
+-Dlibmount=false
 "
 
 termux_step_pre_configure() {
-	NOCONFIGURE=1 ./autogen.sh
-
 	# glib checks for __BIONIC__ instead of __ANDROID__:
-	CFLAGS="$CFLAGS -D__BIONIC__=1"
-
-	cd $TERMUX_PKG_BUILDDIR
-
-	# https://developer.gnome.org/glib/stable/glib-cross-compiling.html
-	echo "glib_cv_long_long_format=ll" >> termux_configure.cache
-	echo "glib_cv_stack_grows=no" >> termux_configure.cache
-	echo "glib_cv_uscore=no" >> termux_configure.cache
-	chmod a-w termux_configure.cache
+	CFLAGS+=" -D__BIONIC__=1"
 }
