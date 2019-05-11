412ce3f34f8a24bb25b6afe2ac79deaaf5bc8714
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-12 00:46:41 +0200

    keybase: Default KEYBASE_SECRET_STORE_FILE to true
    See https://github.com/termux/termux-packages/pull/3791.

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://keybase.io
 TERMUX_PKG_DESCRIPTION="Key directory that maps social media identities to encryption keys"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=4.0.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=503e068e7e89a57c497e40cee5b161704c23919bc59846a6507dfc5ecd418091
 TERMUX_PKG_SRCURL=https://github.com/keybase/client/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_REPLACES="kbfs"
@@ -24,17 +24,4 @@ termux_step_make_install() {
 	cp keybase $TERMUX_PREFIX/bin/keybase
 	cp git-remote-keybase $TERMUX_PREFIX/bin/git-remote-keybase
 	cp kbfsfusebin $TERMUX_PREFIX/bin/kbfsfuse
-
-	mkdir -p $TERMUX_PREFIX/etc/profile.d/
-	echo "export KEYBASE_SECRET_STORE_FILE=1" > $TERMUX_PREFIX/etc/profile.d/keybase.sh
-}
-
-termux_step_create_debscripts() {
-    {
-	echo "#!$TERMUX_PREFIX/bin/sh"
-	echo "echo Before using keybase restart the termux session. Otherwise,"
-	echo "echo keybase will throw an error about a not functional secret store"
-	echo "exit 0"
-    } > postinst
-    chmod 0755 postinst
 }
