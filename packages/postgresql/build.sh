4dad7cfa5aadbe0b0ed41d4e1e6364b127f2752a
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-11 21:40:40 +0200

    postgresql: Update from 11.2 to 11.3

--- packages/postgresql/build.sh
+++ packages/postgresql/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.postgresql.org
 TERMUX_PKG_DESCRIPTION="Object-relational SQL database"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER='Vishal Biswas @vishalbiswas'
-TERMUX_PKG_VERSION=11.2
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SHA256=2676b9ce09c21978032070b6794696e0aa5a476e3d21d60afc036dc0a9c09405
+TERMUX_PKG_VERSION=11.3
+TERMUX_PKG_SHA256=2a85e082fc225944821dfd23990e32dfcd2284c19060864b0ad4ca537d30522d
 TERMUX_PKG_SRCURL=https://ftp.postgresql.org/pub/source/v$TERMUX_PKG_VERSION/postgresql-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_DEPENDS="openssl, libcrypt, readline, libandroid-shmem, libuuid, libxml2, libicu, zlib"
 # - pgac_cv_prog_cc_ldflags__Wl___as_needed: Inform that the linker supports as-needed. It's
