18a4dfdc21ae9b8ebe73bfa22e7c27ac4ffa02a5
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-12 21:46:36 +0200

    iconv: Update from 1.15 to 1.16

--- packages/iconv/build.sh
+++ packages/iconv/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libiconv/
 TERMUX_PKG_DESCRIPTION="Utility converting between different character encodings"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.15
+TERMUX_PKG_VERSION=1.16
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libiconv/libiconv-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178
+TERMUX_PKG_SHA256=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04
 # Only install the binary, not the library since we use libandroid-support for iconv functions:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-static --disable-shared"
 
