bf8b561763e4d6e57517bb532a76c29944f97d1c
Henrik Grimler <henrik@grimler.se>
2019-05-12 20:40:58 +0200

    termux_step_install_license: install to usr/share/doc/ instead of usr/share/

--- scripts/build/termux_step_install_license.sh
+++ scripts/build/termux_step_install_license.sh
@@ -1,23 +1,23 @@
 termux_step_install_license() {
-	mkdir -p "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME"
+	mkdir -p "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME"
 	if [ ! "${TERMUX_PKG_LICENSE_FILE}" = "" ]; then
 		for LICENSE in $TERMUX_PKG_LICENSE_FILE; do
 			if [ ! -f "$TERMUX_PKG_SRCDIR/$LICENSE" ]; then termux_error_exit "$TERMUX_PKG_SRCDIR/$LICENSE does not exist"; fi
-			cp -f ${TERMUX_PKG_SRCDIR}/${LICENSE} ${TERMUX_PREFIX}/share/${TERMUX_PKG_NAME}
+			cp -f ${TERMUX_PKG_SRCDIR}/${LICENSE} ${TERMUX_PREFIX}/share/doc/${TERMUX_PKG_NAME}
 		done
 	else
 		local COUNTER=0
 		for LICENSE in "$(echo $TERMUX_PKG_LICENSE | sed 's/,/ /g')"; do
 			if [ -f "$TERMUX_SCRIPTDIR/packages/termux-licenses/LICENSES/${LICENSE}.txt" ]; then
 				if [[ $COUNTER > 0 ]]; then
-					ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE.${COUNTER}"
+					ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE.${COUNTER}"
 				else
-					ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE"
+					ln -sf "../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE"
 				fi
 			fi
 			COUNTER=$((COUNTER + 1))
 		done
-		if [ ! -f $TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE* ] && [ ! -L $TERMUX_PREFIX/share/$TERMUX_PKG_NAME/LICENSE* ]; then
+		if [ ! -f $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE* ] && [ ! -L $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE* ]; then
 			termux_error_exit "No LICENSE file was installed for $TERMUX_PKG_NAME"
 		fi
 	fi
