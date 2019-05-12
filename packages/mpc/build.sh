376f3e727ef2f7b1dae42cdb33ce219deb98084d
Henrik Grimler <henrik@grimler.se>
2019-05-12 21:55:06 +0200

    termux_step_massage: drop TERMUX_PKG_KEEP_SHARE_DOC

--- packages/mpc/build.sh
+++ packages/mpc/build.sh
@@ -6,7 +6,5 @@ TERMUX_PKG_DEPENDS="libmpdclient"
 TERMUX_PKG_VERSION=0.31
 TERMUX_PKG_SHA256=62373e83a8a165b2ed43967975efecd3feee530f4557d6b861dd08aa89d52b2d
 TERMUX_PKG_SRCURL=https://www.musicpd.org/download/mpc/${TERMUX_PKG_VERSION:0:1}/mpc-$TERMUX_PKG_VERSION.tar.xz
-# Include some useful scripts for editing playlists
-TERMUX_PKG_KEEP_SHARE_DOC=yes
 # There seems to be issues with sphinx-build when using concurrent builds:
 TERMUX_MAKE_PROCESSES=1
--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://taskwarrior.org/download/task-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d87bcee58106eb8a79b850e9abc153d98b79e00d50eade0d63917154984f2a15
 TERMUX_PKG_DEPENDS="libgnutls, libuuid, libandroid-glob"
-TERMUX_PKG_KEEP_SHARE_DOC=yes
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
