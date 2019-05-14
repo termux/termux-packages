376128c489deebe6be1c1a1433940a6f18c3fee8
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-15 02:54:10 +0300

    termux-api: update homepage URL

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE=https://termux.com/add-on-api.html
+TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.40
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=b06b6604befce4607164fdc2de7c8ee301d82780c6dc5d48ea09a722b2ce439a
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
