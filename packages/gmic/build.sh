4202d0ac6e65adfb52795c2b5ca2ca822b2ce19c
Tom Yan <tom.ty89@gmail.com>
2019-05-14 09:40:44 +0800

    gmic: remove -lgcc hack

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
 TERMUX_PKG_LICENSE="CeCILL-2.1"
 TERMUX_PKG_VERSION=2.6.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SHA256=48de7045a36eb718d55b0dfd68a797380b5390af99c9737a4dfba0fb678ed2b4
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
@@ -17,7 +17,7 @@ termux_step_configure() {
 termux_step_make() {
 	cd src/
 	make USR="$TERMUX_PREFIX" STRIP="$STRIP" \
-	     CFLAGS="$CXXFLAGS" LIBS="$LDFLAGS -lgcc" cli
+	     CFLAGS="$CXXFLAGS" LIBS="$LDFLAGS" cli
 }
 
 termux_step_make_install() {
