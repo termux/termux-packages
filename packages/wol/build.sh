dcc5bd46a63c45be3a1a6fc63d568b394d62c11f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-15 02:51:13 +0300

    wol: use https for homepage

--- packages/wol/build.sh
+++ packages/wol/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://sourceforge.net/projects/wake-on-lan/
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/wake-on-lan/
 TERMUX_PKG_DESCRIPTION="Program implementing Wake On LAN functionality"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.7.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/wake-on-lan/wol/${TERMUX_PKG_VERSION}/wol-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e0086c9b9811df2bdf763ec9016dfb1bcb7dba9fa6d7858725b0929069a12622
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
