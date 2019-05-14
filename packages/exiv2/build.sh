0cba87ce8b2a49980e33f530b265dacf2a943f2c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-15 02:43:48 +0300

    exiv2: use https for homepage

--- packages/exiv2/build.sh
+++ packages/exiv2/build.sh
@@ -1,9 +1,9 @@
-TERMUX_PKG_HOMEPAGE=http://exiv2.org
+TERMUX_PKG_HOMEPAGE=https://exiv2.org/
 TERMUX_PKG_DESCRIPTION="Exif, Iptc and XMP metadata manipulation library and tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.26
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://github.com/Exiv2/exiv2/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=51cffa8d19d67e1da6c1d0f570a75b8f6c814113367318c2c0407691888c5f01
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libexpat, zlib"
