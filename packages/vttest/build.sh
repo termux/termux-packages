def32235d1a9f46dd6674f1b094d10ad464c1f61
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-15 02:50:08 +0300

    vttest: use versioned URL

--- packages/vttest/build.sh
+++ packages/vttest/build.sh
@@ -3,7 +3,5 @@ TERMUX_PKG_DESCRIPTION="Program for testing the VT100 compatibility of terminal 
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=20190105
 TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/vttest/vttest-$TERMUX_PKG_VERSION.tgz
 TERMUX_PKG_SHA256=5703ef4783c8eb90b30205740c585652fa779ea28bdd876a2af81af6b9574f71
-# vttest does not use a version in the tar URL, but we will detect
-# an update with a checksum mismatch.
-TERMUX_PKG_SRCURL=http://invisible-island.net/datafiles/release/vttest.tar.gz
