5a7be8401c1a2792bdb9fa9613ec9d1c48d9a453
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-19 00:26:50 +0300

    atk: fix source URL

--- x11-packages/atk/build.sh
+++ x11-packages/atk/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="The interface definitions of accessibility infrastructur
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=2.32.0
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/atk/${TERMUX_PKG_VERSION:0:3}/atk-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/atk/${TERMUX_PKG_VERSION:0:4}/atk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c3212e849fffe1f8099fe05e1f5e2d357a56458e5b288bfdceabdd5d6c814441
 
 TERMUX_PKG_DEPENDS="glib"
