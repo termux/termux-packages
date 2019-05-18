cd12ae19be4b1f0bdf9ad95c068827c0cfe7bd02
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-18 22:19:28 +0300

    mpd: rebuild package and dependencies
    And remove `-lunwind -Wl,--exclude-libs=libunwind.a`.

--- packages/libnfs/build.sh
+++ packages/libnfs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="NFS client library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=4.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/sahlberg/libnfs/archive/libnfs-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6ee77e9fe220e2d3e3b1f53cfea04fb319828cc7dbb97dd9df09e46e901d797d
 
