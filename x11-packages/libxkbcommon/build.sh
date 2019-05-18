611a24d1ea9fcfb5cef0a0e787f202dc7ed68c9b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-19 00:40:18 +0300

    libxkbcommon: update to 0.8.4

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://xkbcommon.org/
 TERMUX_PKG_DESCRIPTION="Keymap handling library for toolkits and window systems"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.8.2
+TERMUX_PKG_VERSION=0.8.4
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fd19874aefbcbc9da751292ba7abee8952405cd7d9042466e41a9c6ed3046322
+TERMUX_PKG_SHA256=299b47558188017047354995f5882d43c2c8a60367df553319dcecebadb73e1d
 TERMUX_PKG_DEPENDS="libxcb, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 
