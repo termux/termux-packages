3d8bba523cb712c79f0b298359c7412451325feb
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-19 15:11:42 +0300

    xorg-mkfontscale: package replaces 'xorg-mkfontdir'

--- x11-packages/xorg-mkfontscale/build.sh
+++ x11-packages/xorg-mkfontscale/build.sh
@@ -3,7 +3,16 @@ TERMUX_PKG_DESCRIPTION="Create an index of scalable font files for X"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/app/mkfontscale-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=ca0495eb974a179dd742bfa6199d561bda1c8da4a0c5a667f21fd82aaab6bac7
 TERMUX_PKG_DEPENDS="freetype, libfontenc, zlib"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros, xorgproto"
+TERMUX_PKG_CONFLICTS="xorg-mkfontdir"
+TERMUX_PKG_REPLACES="xorg-mkfontdir"
+
+termux_step_create_debscripts() {
+	cp -f "${TERMUX_PKG_BUILDER_DIR}/postinst" ./
+	cp -f "${TERMUX_PKG_BUILDER_DIR}/postrm"   ./
+	cp -f "${TERMUX_PKG_BUILDER_DIR}/triggers" ./
+}
