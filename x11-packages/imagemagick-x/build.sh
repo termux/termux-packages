799d958a66e41fa92db2741b6003533a78859b86
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-20 00:57:01 +0300

    imagemagick-x: update to 7.0.8.46

--- x11-packages/imagemagick-x/build.sh
+++ x11-packages/imagemagick-x/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
-TERMUX_PKG_VERSION=7.0.8.45
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=7.0.8.46
 local _download_version=`echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/'`
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/${_download_version}.tar.gz
-TERMUX_PKG_SHA256=37dfadbbf8eab0546ba99de50e4eb664a068f711ad39b87c1ee8557ad7ed521b
+TERMUX_PKG_SHA256=e6f49b5883d26c7f85207779397bdb083e629acec8de423e6b1d32038e23ded8
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo-x, libffi, libgraphite, libjpeg-turbo, liblzma, libpixman, libpng, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango-x, pcre, zlib"
 
 TERMUX_PKG_CONFLICTS="imagemagick"
