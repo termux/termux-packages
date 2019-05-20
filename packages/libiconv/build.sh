14561210482b81d16832b82b85f5ec09e5d81300
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-21 01:14:21 +0200

    libiconv: Move man page to iconv subpackage

--- packages/libiconv/build.sh
+++ packages/libiconv/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libiconv/
 TERMUX_PKG_DESCRIPTION="An implementation of iconv()"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=1.16
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/libiconv/libiconv-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04
 TERMUX_PKG_BREAKS="libandroid-support (<= 24)"
--- packages/libiconv/iconv.subpackage.sh
+++ packages/libiconv/iconv.subpackage.sh
@@ -1,3 +1,3 @@
-TERMUX_SUBPKG_INCLUDE="bin/iconv"
+TERMUX_SUBPKG_INCLUDE="bin/iconv share/man/man1"
 TERMUX_SUBPKG_DESCRIPTION="Utility converting between different character encodings"
 TERMUX_SUBPKG_DEPENDS="libiconv"
