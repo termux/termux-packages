78c857302b6c90bea42a4847eff14b862a816355
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 23:25:25 +0300

    gstreamer (and plugins): update to 1.16.0

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.14.4
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=ca6139490e48863e7706d870ff4e8ac9f417b56f3b9e4b3ce490c13b09a77461
+TERMUX_PKG_VERSION=1.16.0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=4093aa7b51e28fb24dfd603893fead8d1b7782f088b05ed0f22a21ef176fb5ae
 TERMUX_PKG_DEPENDS="gstreamer, libjpeg-turbo, libopus, libpng, libvorbis, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 GLIB_GENMARSHAL=/usr/bin/glib-genmarshal
--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.14.4
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=5f8b553260cb0aac56890053d8511db1528d53cae10f0287cfce2cb2acc70979
+TERMUX_PKG_VERSION=1.16.0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_SHA256=654adef33380d604112f702c2927574cfc285e31307b79e584113858838bb0fd
 TERMUX_PKG_DEPENDS="gst-plugins-base, libcaca, libsoup, libjpeg-turbo, libpng, libflac, libbz2, libvpx, libpulseaudio, libmp3lame, gstreamer, libogg, zlib"
+TERMUX_PKG_BUILD_IN_SRC=yes
 # pcre needed by glib. libxml2 needed by libsoup
 TERMUX_PKG_BUILD_DEPENDS="glib, pcre, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
