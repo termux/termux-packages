c4f2b93754b785477ad279d3f6d88aabbb6b8db9
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 14:25:09 +0300

    tracepath: update to 20190515

--- packages/tracepath/build.sh
+++ packages/tracepath/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/iputils/iputils
 TERMUX_PKG_DESCRIPTION="Tool to trace the network path to a remote host"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=20180629
-TERMUX_PKG_SHA256=da14105291dd491f28ea91ade854ed10aee8ba019641c80eed233de3908be7c5
+TERMUX_PKG_VERSION=20190515
 TERMUX_PKG_SRCURL=https://github.com/iputils/iputils/archive/s${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9b5125eb5ef9f4e947ad8fdddcf77f538f53b8f47b53eb5bc5347cb16d01c8fd
 TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="libidn"
 
 termux_step_configure() {
 	return
@@ -16,10 +15,21 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	$CC $CFLAGS $LDFLAGS -lidn -o $TERMUX_PREFIX/bin/tracepath tracepath.c
+	CPPFLAGS+=" -DPACKAGE_VERSION=\"$TERMUX_PKG_VERSION\" -Dprogram_invocation_short_name=\"tracepath\""
+	$CC $CFLAGS $CPPFLAGS $LDFLAGS -o $TERMUX_PREFIX/bin/tracepath iputils_common.c tracepath.c
+
 	local MANDIR=$TERMUX_PREFIX/share/man/man8
 	mkdir -p $MANDIR
-	cp $TERMUX_PKG_BUILDER_DIR/tracepath.8 $MANDIR/
+	cd $TERMUX_PKG_SRCDIR/doc
+	xsltproc \
+		--stringparam man.output.quietly 1 \
+		--stringparam funcsynopsis.style ansi \
+		--stringparam man.th.extra1.suppress 1 \
+		--stringparam iputils.version $TERMUX_PKG_VERSION \
+		custom-man.xsl \
+		tracepath.xml
+	cp tracepath.8 $MANDIR/
+
 	# Setup traceroute as an alias for tracepath, since traceroute
 	# requires root which most Termux user does not have, and tracepath
 	# is probably good enough for most:
