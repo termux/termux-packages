eb05aedcde18b63610090e6845db33a935032882
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 14:41:13 +0300

    kona: update to 20190226

--- packages/kona/build.sh
+++ packages/kona/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/kevinlawler/kona
 TERMUX_PKG_DESCRIPTION="Open-source implementation of the APL-like K programming language"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION="3.39-69"
-TERMUX_PKG_SRCURL="https://github.com/kevinlawler/kona/archive/Win.${TERMUX_PKG_VERSION}.zip"
-TERMUX_PKG_SHA256=ba6d6e9a1ed58678011cdccdd94a2e42cc7bff8abfee94e8b2de9cd93f71785b
-TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_MAINTAINER="Jonathan Badger @jhbadger"
+TERMUX_PKG_VERSION=20190226
+TERMUX_PKG_SRCURL=https://github.com/kevinlawler/kona/archive/Win64-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=26cfebd47a3ae60753fc8a910132f45cad58381155255fbac2129e4507fef403
+TERMUX_PKG_BUILD_IN_SRC=yes
