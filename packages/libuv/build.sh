ff2cad21d3f97c6607b1f3b79f817f3cbd120bfa
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 23:45:15 +0300

    libuv: update to 1.29.1

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libuv.org
 TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=1.29.0
+TERMUX_PKG_VERSION=1.29.1
 TERMUX_PKG_SRCURL=https://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f7bf07c82efe991eeddaf70ee8fa753f9b6a9a699d1fb7a08aceb8659dd7547f
+TERMUX_PKG_SHA256=1486043da5ccaf86d451a5fe93920c5b66770b132d92b6d0a15d636346ca570c
 
 termux_step_pre_configure() {
 	export PLATFORM=android
