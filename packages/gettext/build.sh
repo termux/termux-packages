204baab0577eefb336749f0793bff8fe20fd832b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 15:13:35 +0300

    gettext: update to 0.20.1

--- packages/gettext/build.sh
+++ packages/gettext/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gettext/
 TERMUX_PKG_DESCRIPTION="GNU Internationalization utilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.19.8.1
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SHA256=105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4
+TERMUX_PKG_VERSION=0.20.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gettext/gettext-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_SHA256=53f02fbbec9e798b0faaf7c73272f83608e835c6288dd58be6c9bb54624a3800
 TERMUX_PKG_DEPENDS="libiconv, pcre, liblzma, libxml2, libcroco, ncurses, libunistring, zlib"
+TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
 	autoreconf
