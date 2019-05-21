c38e39e56497380e569a1c7f4cb82d2a7e2f503d
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 14:30:58 +0300

    qpdf: update to 8.4.2

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -1,13 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Content-Preserving PDF Transformation System"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=8.4.1
-TERMUX_PKG_SHA256=b8068d4ce23b5c8b3600fd83d6d88744f897c1e1b62a63bbff668e72984c38bb
-TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/archive/release-qpdf-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_DEPENDS="libjpeg-turbo, zlib"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-random=/dev/urandom"
+TERMUX_PKG_VERSION=8.4.2
+TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/archive/release-qpdf-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=741a171295e561eadf0285ae6b0b821632f9773b8db0daa95df4dc565bf566bb
+TERMUX_PKG_DEPENDS="libjpeg-turbo, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-random=/dev/urandom"
 
 termux_step_pre_configure() {
 	./autogen.sh
