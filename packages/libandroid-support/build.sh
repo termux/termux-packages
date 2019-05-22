ce9473fdca11bcaa11adc6340118768e969d8792
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-23 02:26:10 +0300

    libandroid-support: do not use iconv.h header

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=24
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e14e262429a60bea733d5bed69d2f3a1cada53fcadaf76787fca5c8b0d4dae2f
 TERMUX_PKG_PRE_DEPENDS="dpkg (>= 1.19.4-3)"
