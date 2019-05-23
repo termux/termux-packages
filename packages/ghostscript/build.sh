d32337206018a43bea9ceb9aac28a3fd77c2a467
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-23 21:32:55 +0300

    ghostscript: rebuild due to updated libandroid-support
    Now ghostscript will pick `libiconv_open()` from GNU iconv.

--- packages/ghostscript/build.sh
+++ packages/ghostscript/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.ghostscript.com/
 TERMUX_PKG_DESCRIPTION="Interpreter for the PostScript language and for PDF"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_VERSION=9.27
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=9e089546624296bf4aca14c2adcb0762b323ca77ae14176d21127b749baac8d6
 TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs${TERMUX_PKG_VERSION//.}/ghostpdl-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libtiff, libjpeg-turbo, libpng, libexpat, freetype, fontconfig, libidn, littlecms, openjpeg"
@@ -19,4 +19,10 @@ CCAUX=gcc
 
 termux_step_post_extract_package() {
 	rm -rdf $TERMUX_PKG_SRCDIR/{jpeg,libpng,expat,jasper,freetype,lcms2,tiff,openjpeg}
+}
+
+termux_step_pre_configure() {
+	# Use `make -j1` otherwise build may fail with error
+	# about missing 'arch.h'.
+	TERMUX_MAKE_PROCESSES=1
 }
