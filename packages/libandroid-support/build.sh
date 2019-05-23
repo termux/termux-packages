2085a125654d8c058de30e23c0f2e16d258945dc
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-23 21:16:05 +0300

    libandroid-support: finally drop iconv implementation
    It is replaced by GNU iconv.

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=24
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=25
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e14e262429a60bea733d5bed69d2f3a1cada53fcadaf76787fca5c8b0d4dae2f
+TERMUX_PKG_SHA256=42e7b55c2e5fc91cc0447fa8bea432e7a75ec78b03469330668af17aea56f660
 TERMUX_PKG_PRE_DEPENDS="dpkg (>= 1.19.4-3)"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_ESSENTIAL=yes
