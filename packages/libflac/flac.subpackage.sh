6dc0b65fb978e799cf82dcc90cfee39447379b05
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-25 00:13:17 +0300

    libflac: bump revision

--- packages/libflac/flac.subpackage.sh
+++ packages/libflac/flac.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="bin/flac bin/metaflac share/man/man1"
 TERMUX_SUBPKG_DESCRIPTION="FLAC (Free Lossless Audio Codec) command-line tool"
-TERMUX_SUBPKG_DEPENDS="libflac"
+TERMUX_SUBPKG_DEPENDS="libflac, libiconv"
