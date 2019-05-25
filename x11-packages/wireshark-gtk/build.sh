d563629b4bb88f3db553847a11a7d1d6baa15035
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 21:24:44 +0300

    wireshark-gtk: update to 2.6.9

--- x11-packages/wireshark-gtk/build.sh
+++ x11-packages/wireshark-gtk/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.wireshark.org/
 TERMUX_PKG_DESCRIPTION="Network protocol analyzer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.6.8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.6.9
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/wireshark-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b3d7dd57b637353dde2743b6ae1d12e3516ad832443d171f50f5c40312dab506
+TERMUX_PKG_SHA256=0d8c1a95a915919222f6b1a8e7fa1151a198138ab0d9327f1e93091c3b5106be
 
 TERMUX_PKG_DEPENDS="atk, c-ares, desktop-file-utils, gdk-pixbuf, glib, gtk3, hicolor-icon-theme, krb5, libandroid-shmem, libcairo-x, libgcrypt, libgnutls, libgpg-error, liblua52, liblz4, libmaxminddb, libnghttp2, libnl, libpcap, libssh, libxml2, pango-x, zlib"
 TERMUX_PKG_CONFLICTS="tshark, wireshark, wireshark-cli"
