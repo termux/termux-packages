f927140a38da33e4eb8166b2d7bf594ebac3bf71
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 19:28:24 +0300

    pcmanfm: bump revision

--- x11-packages/pcmanfm/build.sh
+++ x11-packages/pcmanfm/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Extremely fast and lightweight file manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pcmanfm/pcmanfm-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=6804043b3ee3a703edde41c724946174b505fe958703eadbd7e0876ece836855
 TERMUX_PKG_DEPENDS="atk, glib, gtk3, libandroid-shmem, libcairo-x, libfm, libx11, lxmenu-data, pango-x"
