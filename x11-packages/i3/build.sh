b20686fa77ac824d154b45bb8c6cc34e3b529540
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 17:12:19 +0300

    i3: bump revision

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=4.16.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=ab65c25c06acbdc67cba3ff23e2a82ea17911def65f4fd0200bb6f9382378576
 TERMUX_PKG_DEPENDS="glib, i3status, libandroid-glob, libandroid-shmem, libcairo-x, libev, libxcb, libxkbcommon, pango-x, pcre, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
