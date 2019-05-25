ffb40274e606e58c3489deeca7b35255aee8532a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 19:05:47 +0300

    libevdev: bump revision

--- x11-packages/libevdev/build.sh
+++ x11-packages/libevdev/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Wrapper library for evdev devices"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/libevdev/libevdev-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=f5005c865987d980cc1279b9ec6131b06a89fd9892f649f2a68262b8786ef814
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-test-run"
