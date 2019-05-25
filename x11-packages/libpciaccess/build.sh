d798896ebfd88c36bf587ad89b407729aab556c2
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 19:05:49 +0300

    libpciaccess: bump revision

--- x11-packages/libpciaccess/build.sh
+++ x11-packages/libpciaccess/build.sh
@@ -3,6 +3,6 @@ TERMUX_PKG_DESCRIPTION="X11 PCI access library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=0.14
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libpciaccess-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=3df543e12afd41fea8eac817e48cbfde5aed8817b81670a4e9e493bb2f5bf2a4
