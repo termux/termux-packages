651257408d45b38faa3c8c463cdc46f63b1c51df
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 19:05:52 +0300

    libxkbcommon: bump revision

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Keymap handling library for toolkits and window systems"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=0.8.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=299b47558188017047354995f5882d43c2c8a60367df553319dcecebadb73e1d
 TERMUX_PKG_DEPENDS="libxcb, xkeyboard-config"
