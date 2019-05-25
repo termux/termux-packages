f3178334ab5b6215ee5650583d54e2f588d04319
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 19:05:49 +0300

    libx11: bump revision

--- x11-packages/libx11/build.sh
+++ x11-packages/libx11/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="X11 client-side library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.6.7
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=910e9e30efba4ad3672ca277741c2728aebffa7bc526f04dcfa74df2e52a1348
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb, x11-repo (>= 1.3)"
