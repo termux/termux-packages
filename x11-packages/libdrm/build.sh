14cffe4019e86eb6b900f30e40f1427019c602ca
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-25 19:05:43 +0300

    libdrm: bump revision

--- x11-packages/libdrm/build.sh
+++ x11-packages/libdrm/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Userspace interface to kernel DRM services"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=2.4.98
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://dri.freedesktop.org/libdrm/libdrm-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=8be0edccaca3abde8b6bb1431b46354c7fab46e9b91cc6946ba65b51f56f1894
 
