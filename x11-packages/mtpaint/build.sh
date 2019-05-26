964a12bc4b3d53f4bfae4d44e34541f31e62d983
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-26 19:45:33 +0300

    mtpaint: depend on libwebp
    %ci:reset-backlog

--- x11-packages/mtpaint/build.sh
+++ x11-packages/mtpaint/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Simple paint program for creating icons and pixel based 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=3.49.16
+TERMUX_PKG_REVISION=1
 _COMMIT=d4cc3e5a3e51b447b091c5623e221ce03f5ac6c8
 TERMUX_PKG_SRCURL=https://github.com/wjaguar/mtPaint/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=30bbd72463d4cb871f4b78e4ff9a0cfc25ef27929518aa4b092035f1a6fb24f6
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, giflib, glib, gtk2, libandroid-glob, libandroid-shmem, libcairo-x, libjpeg-turbo, littlecms, pango-x, libpng, libtiff, libx11, zlib"
+TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, giflib, glib, gtk2, libandroid-glob, libandroid-shmem, libcairo-x, libjpeg-turbo, littlecms, pango-x, libpng, libtiff, libwebp, libx11, zlib"
 TERMUX_PKG_RECOMMENDS="gifsicle"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=${TERMUX_PREFIX}/share/man man GIF jpeg tiff cflags lcms2"
