5a93d933caa19082b55712ccd2326215531c8d0a
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-26 20:16:15 +0200

    hashdeep: Blacklist 32-bit arches

--- packages/hashdeep/build.sh
+++ packages/hashdeep/build.sh
@@ -5,10 +5,8 @@ TERMUX_PKG_VERSION=4.4
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/jessek/hashdeep/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ad78d42142f9a74fe8ec0c61bc78d6588a528cbb9aede9440f50b6ff477f3a7f
-if [ "$TERMUX_ARCH_BITS" = 32 ]; then
-	# hashdeep requires 64-bit off_t.
-	TERMUX_PKG_API_LEVEL=24
-fi
+# hashdeep requires 64-bit off_t.
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
 	sh bootstrap.sh
