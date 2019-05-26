8ca7d2912d92682bc3fbe023ad96e80c9afec941
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-26 15:55:42 +0300

    imagemagick-x: bump revision

--- x11-packages/imagemagick-x/build.sh
+++ x11-packages/imagemagick-x/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_VERSION=7.0.8.46
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 local _download_version=`echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/'`
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/${_download_version}.tar.gz
 TERMUX_PKG_SHA256=e6f49b5883d26c7f85207779397bdb083e629acec8de423e6b1d32038e23ded8
@@ -23,7 +23,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-openmp
 ac_cv_func_ftime=no"
 
-if [ "$TERMUX_PKG_API_LEVEL" -lt 24 ]; then
+if [ $TERMUX_PKG_API_LEVEL -lt 24 ]; then
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_header_complex_h=no"
 fi
 
