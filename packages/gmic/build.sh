48f1aeaf36acfc12c8e6832c272c79238fb06edc
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-05-26 21:06:05 +0300

    gmic: update to 2.6.4

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
 TERMUX_PKG_LICENSE="CeCILL-2.1"
-TERMUX_PKG_VERSION=2.6.3
+TERMUX_PKG_VERSION=2.6.4
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9a3947850ad8e6997039ebf8d03ce8ac371f2a01c3ec6330e7d8d447932cbb47
+TERMUX_PKG_SHA256=4cd88b2dca6b9b1a330ab4556d36656bafb98e4e9814bf0448545b27ef18dae3
 TERMUX_PKG_DEPENDS="libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_BUILD_DEPENDS="graphicsmagick"
 TERMUX_PKG_NO_DEVELSPLIT=yes
