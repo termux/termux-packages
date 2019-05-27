31f55349c96f30cac2693a8a0ce65013391902ba
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-27 23:57:28 +0200

    libevent: Update from 2.1.8 to 2.1.10

--- packages/libevent/build.sh
+++ packages/libevent/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libevent.org/
 TERMUX_PKG_DESCRIPTION="Library that provides asynchronous event notification"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=2.1.8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.1.10
+TERMUX_PKG_SHA256=52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79
 TERMUX_PKG_SRCURL=https://github.com/libevent/libevent/archive/release-${TERMUX_PKG_VERSION}-stable.tar.gz
-TERMUX_PKG_SHA256=316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d
 # Strip away libevent core, extra and openssl libraries until someone uses them
 TERMUX_PKG_RM_AFTER_INSTALL="bin/event_rpcgen.py lib/libevent_*"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
