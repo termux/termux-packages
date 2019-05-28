867d498c037e58f7fc2d29d5e98316750e6f07ec
Henrik Grimler <henrik@grimler.se>
2019-05-28 21:10:05 +0200

    geoip2-database: update to 20190528

--- packages/geoip2-database/build.sh
+++ packages/geoip2-database/build.sh
@@ -4,16 +4,16 @@ TERMUX_PKG_LICENSE="CC0-1.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_VERSION=20190521
+TERMUX_PKG_VERSION=20190528
 
 TERMUX_PKG_SRCURL=('https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz'
 				   'https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz'
 				   'https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz')
 
 ## If these checksums becomes invalid - it's time to update package.
-TERMUX_PKG_SHA256=('7b1e3ea942591e5bf1da152103c36475d51fd00a35670c7499d9662560b231b6'
-				   'c8ca2ef688d51cf0dfe253443e63316705477b754a94f0744f421351eb55b3a5'
-				   'a34e80b309de801993759fef376be8c44b16564e58e8e174c6acb41cd42e5d4f')
+TERMUX_PKG_SHA256=('84c1d9c6f4aca2fb2a125fb2eaf3afa7601b3e4f9a76081c72d71b52931e79ca'
+				   'f5584976158d92627b4380c97ce0a0424198894a637752746cbed12c83d9c49d'
+				   'c3a966865e5d814c2ca9fdf874e41c882a5d4a1132de1fc036776841d9922db7')
 
 termux_step_make_install() {
 	install -Dm600 \
