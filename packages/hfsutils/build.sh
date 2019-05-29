766c698dae1f423f74fddff361f526b1b1490819
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-29 23:52:01 +0200

    hfsutils: Bump revision after man path change

--- packages/hfsutils/build.sh
+++ packages/hfsutils/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.mars.org/home/rob/proj/hfs/
 TERMUX_PKG_DESCRIPTION="Tool for manipulating HFS images."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=3.2.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.mars.org/pub/hfs/hfsutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bc9d22d6d252b920ec9cdf18e00b7655a6189b3f34f42e58d5bb152957289840
 TERMUX_PKG_DEPENDS="libandroid-support"
