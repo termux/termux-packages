d74718ff7ddc0951614b36046d08049191a8be21
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-29 23:44:05 +0200

    lighttpd: Update from 1.4.53 to 1.4.54

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.lighttpd.net
 TERMUX_PKG_DESCRIPTION="Fast webserver with minimal memory footprint"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=1.4.53
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=3bdfce1cf3e9650a556a8c26fb15342c5717c63f530c54693db632b0371dcb78
+TERMUX_PKG_VERSION=1.4.54
+TERMUX_PKG_SHA256=cf14cce2254a96d8fcb6d3181e1a3c29a8f832531c3e86ff6f2524ecda9a8721
 TERMUX_PKG_SRCURL=https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
 TERMUX_PKG_DEPENDS="libbz2, openssl, pcre, libcrypt, libandroid-glob, zlib"
