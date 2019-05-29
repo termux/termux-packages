48dec3e52896c7de7f02d270af2fe40d38493fdd
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-30 00:18:20 +0200

    gnuit: Depend on libandroid-support properly

--- packages/gnuit/build.sh
+++ packages/gnuit/build.sh
@@ -5,10 +5,8 @@ TERMUX_PKG_VERSION=4.9.5
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gnuit/gnuit-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6b6e96db13bafa5ad35c735b2277699d4244088c709a3e134fb1a3e8c8a8557c
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-transition"
-TERMUX_PKG_DEPENDS="ncurses"
-termux_step_pre_configure() {
-	LDFLAGS+=" -landroid-support"
-}
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+
 termux_step_post_massage() {
 	cd $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/gnuit
 	ln -s gnuitrc.xterm-color gnuitrc.xterm-256color
