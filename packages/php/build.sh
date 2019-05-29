cccc7c6ed73bbd9df5f9a21f87c8958ae33c9ea2
Fredrik Fornwall <fredrik@fornwall.net>
2019-05-29 23:09:29 +0200

    php: Update from 7.3.5 to 7.3.6

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://php.net
 TERMUX_PKG_DESCRIPTION="Server-side, HTML-embedded scripting language"
 TERMUX_PKG_LICENSE="PHP-3.0"
-TERMUX_PKG_VERSION=7.3.5
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=e1011838a46fd4a195c8453b333916622d7ff5bce4aca2d9d99afac142db2472
+TERMUX_PKG_VERSION=7.3.6
+TERMUX_PKG_SHA256=fefc8967daa30ebc375b2ab2857f97da94ca81921b722ddac86b29e15c54a164
 TERMUX_PKG_SRCURL=https://secure.php.net/distributions/php-${TERMUX_PKG_VERSION}.tar.xz
 # Build native php for phar to build (see pear-Makefile.frag.patch):
 TERMUX_PKG_HOSTBUILD=true
