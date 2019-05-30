9b5e2a3eaef507ec6986c6052dbb64e97cf2d2cc
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-30 13:13:54 +0300

    tor: reformat build.sh & bump revision after libevent update

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,14 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib"
+TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_VERSION=0.4.0.5
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=b5a2cbf0dcd3f1df2675dbd5ec10bbe6f8ae995c41b68cebe2bc95bffc90696e
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=b5a2cbf0dcd3f1df2675dbd5ec10bbe6f8ae995c41b68cebe2bc95bffc90696e
+TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-zstd --disable-unittests"
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
-TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 
 termux_step_pre_configure() {
 	LDFLAGS="$LDFLAGS -llog"
