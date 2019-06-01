088f35d6ef6c0faf206f14224db48f95213d4b78
Henrik Grimler <henrik@grimler.se>
2019-06-01 11:17:01 +0200

    libllvm: add note about debug build status

--- packages/libllvm/build.sh
+++ packages/libllvm/build.sh
@@ -53,6 +53,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 TERMUX_PKG_FORCE_CMAKE=yes
 TERMUX_PKG_KEEP_STATIC_LIBRARIES=true
 TERMUX_PKG_HAS_DEBUG=no
+# Debug build succeeds but make install with:
+# cp: cannot stat '../src/projects/openmp/runtime/exports/common.min.50.ompt.optional/include/omp.h': No such file or directory
+# common.min.50.ompt.optional should be common.deb.50.ompt.optional when doing debug build
 
 termux_step_post_extract_package() {
 	mv cfe-${TERMUX_PKG_VERSION}.src tools/clang
