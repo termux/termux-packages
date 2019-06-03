6cd6c450c36f527f4ce0e2fd1f77969ec39d883c
Fredrik Fornwall <fredrik@fornwall.net>
2019-06-03 12:23:22 +0200

    mu: Add package at version 1.2

---
+++ packages/mu/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
+TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.0.tar.xz
+TERMUX_PKG_SHA256=f634c7f244dc6844ff71dc3c3e1893e48e193caa9e0e747eba616309775f053a
+TERMUX_PKG_DEPENDS="glib, libxapian, libgmime"
