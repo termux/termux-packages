8a00e12de6550fba8d7f8312d83a79181f5b4ecd
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-03 16:21:56 +0300

    xsel: fix build.sh

--- x11-packages/xsel/build.sh
+++ x11-packages/xsel/build.sh
@@ -3,27 +3,12 @@ TERMUX_PKG_DESCRIPTION="Command-line program for getting and setting the content
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Doron Behar <me@doronbehar.com>"
 TERMUX_PKG_VERSION=1.2.0.20190505
-_commit=24bee9c7f4dc887eabb2783f21cbf9734d723d72
-TERMUX_PKG_REVISION=1
+local _COMMIT=24bee9c7f4dc887eabb2783f21cbf9734d723d72
+TERMUX_PKG_SRCURL=https://github.com/kfish/xsel/archive/24bee9c7f4dc887eabb2783f21cbf9734d723d72.zip
+TERMUX_PKG_SHA256=9b9b92deece971201d75b8932a743e8a6b597c4b146de193f54d09a58da15624
 TERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
 
-termux_step_extract_package() {
-	mkdir -p $TERMUX_PKG_SRCDIR
-	cd $TERMUX_PKG_SRCDIR
-	git clone https://github.com/kfish/xsel
-	cd xsel
-	git checkout $_commit
-	./autogen.sh --prefix=$TERMUX_PREFIX
-}
-
-termux_step_make() {
-	cd $TERMUX_PKG_SRCDIR/xsel
-	make
-}
-
-termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR/xsel
-	make install
-	install -D -m0644 COPYING $TERMUX_PREFIX/share/LICENSES/xsel.txt
+termux_step_pre_configure() {
+	autoreconf -fi
 }
