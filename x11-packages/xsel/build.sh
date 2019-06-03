37da0227428a731855b6ea24105a098fde5aa41b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-03 16:12:21 +0300

    xsel: fix license field

--- x11-packages/xsel/build.sh
+++ x11-packages/xsel/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.kfish.org/software/xsel/
 TERMUX_PKG_DESCRIPTION="Command-line program for getting and setting the contents of the X selection"
-TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Doron Behar <me@doronbehar.com>"
 TERMUX_PKG_VERSION=1.2.0.20190505
 _commit=24bee9c7f4dc887eabb2783f21cbf9734d723d72
