33e0630fe811d0ced06eab55b23cf3ba209b254d
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-03 21:01:48 +0300

    ack-grep: update to 3.0.0

--- packages/ack-grep/build.sh
+++ packages/ack-grep/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://beyondgrep.com/
 TERMUX_PKG_DESCRIPTION="Tool like grep optimized for programmers"
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
-TERMUX_PKG_VERSION=2.28
-TERMUX_PKG_SHA256=0ab3df19513a2c71aa7901f7f522a5baf72ce69e6e0e34879979f157210734f6
-TERMUX_PKG_SRCURL=https://beyondgrep.com/ack-${TERMUX_PKG_VERSION}-single-file
+TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_SRCURL=https://beyondgrep.com/ack-v${TERMUX_PKG_VERSION}
+TERMUX_PKG_SHA256=b9e342095fabb03da0159c69b4e5f99ce458c38ca8cbeafc7d03abcfd789536e
+TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_SKIP_SRC_EXTRACT=yes
-# Depend on coreutils for bin/env
-TERMUX_PKG_DEPENDS="perl, coreutils"
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_make_install() {
