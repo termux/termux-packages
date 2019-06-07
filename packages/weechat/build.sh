958bca45cd39e4f46b858bf5d796a9b3636e9ffa
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-08 01:18:34 +0300

    weechat: update to 2.5

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.4
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SHA256=61a6afe9849b96e99c1f3cde612d1748a03c807059dad459e3a6acbc5cf100fd
+TERMUX_PKG_VERSION=2.5
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=4c772c949c59949ff4156560c66900c5aaa9ab2da4439ee278b7c21b24daf191
 TERMUX_PKG_DEPENDS="libiconv, ncurses, libgcrypt, libcurl, libgnutls, libandroid-support, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/weechat-curses share/man/man1/weechat-headless.1 share/icons"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCA_FILE=$TERMUX_PREFIX/etc/tls/cert.pem
 -DENABLE_HEADLESS=OFF
