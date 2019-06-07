09d18441d03837499f8b6a2c4851c92e4419c76c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-08 01:24:21 +0300

    ncdc: update to 1.22.1

--- packages/ncdc/build.sh
+++ packages/ncdc/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://dev.yorhel.nl/ncdc
 TERMUX_PKG_DESCRIPTION="Modern and lightweight direct connect client with a friendly ncurses interface"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.22
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=fd41ef85cec3eca0107d83583ad25faa8804dd22d76f6da7fc157e0233b13a59
+TERMUX_PKG_VERSION=1.22.1
 TERMUX_PKG_SRCURL=https://dev.yorhel.nl/download/ncdc-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d15fd378aa345f423e59a38691c668f69b516cd4b8afbbcdc446007740c3afad
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libgnutls, libsqlite, bzip2, glib, ncurses, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libbz2-dev, libnettle-dev, libidn-dev"
 TERMUX_PKG_BUILD_IN_SRC="yes"
