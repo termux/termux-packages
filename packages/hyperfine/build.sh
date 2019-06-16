7f69c5eca0ad4ea7bbb73a551913c4725802e754
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-16 14:54:52 +0300

    new package: hyperfine

---
+++ packages/hyperfine/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/hyperfine
+TERMUX_PKG_DESCRIPTION="A command-line benchmarking tool"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/sharkdp/hyperfine/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=0cb251453147421c0c807587123e733b32346b9756c8594018c637ec374a63f4
+TERMUX_PKG_BUILD_IN_SRC=yes
