c6d7f677e71d838d7867bade503fa2a5030b7518
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-16 14:59:34 +0300

    new package: ncompress
    https://github.com/termux/unstable-packages/issues/41

---
+++ packages/ncompress/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/vapier/ncompress
+TERMUX_PKG_DESCRIPTION="The classic unix compression utility which can handle the ancient .Z archive"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=4.2.4.5
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=https://github.com/vapier/ncompress/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f575bbcdd5f844ce22d753b9acd23d2a6b73ffc15f204bebbaf8bd6f6574903b
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make_install() {
+	install -Dm700 compress "$TERMUX_PREFIX"/bin/
+	install -Dm600 compress.1 "$TERMUX_PREFIX"/share/man/man1/
+}
