18c50250a81db5b56fe95b486b198e2389cf4a08
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-16 14:54:22 +0300

    new package: gotty

---
+++ packages/gotty/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/yudai/gotty
+TERMUX_PKG_DESCRIPTION="Share your terminal as a web application"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=https://github.com/yudai/gotty/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=32695d70a79f55efdf4fba6f06f830515a2055abc58fc15e2124dff5be75695b
+
+termux_step_make() {
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/yudai
+	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/yudai/gotty
+
+	cd "$GOPATH"/src/github.com/yudai/gotty
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 \
+		"$GOPATH"/src/github.com/yudai/gotty/gotty \
+		"$TERMUX_PREFIX"/bin/
+}
