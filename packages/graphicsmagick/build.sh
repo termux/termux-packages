f0f873c9c4218cff4d5cf87cc586c77b5188d7cb
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-16 18:24:02 +0300

    graphicsmagick: update to 1.3.32

--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://www.graphicsmagick.org/
 TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.3.31
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SHA256=096bbb59d6f3abd32b562fc3b34ea90d88741dc5dd888731d61d17e100394278
+TERMUX_PKG_VERSION=1.3.32
 # Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=b842a5a0d6c84fd6c5f161b5cd8e02bbd210b0c0b6728dd762b7c53062ba94e1
 TERMUX_PKG_DEPENDS="littlecms, libtiff, freetype, libjasper, libjpeg-turbo, libpng, libbz2, libxml2, liblzma, zstd, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_ftime=no
