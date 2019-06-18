6655dfcb9fa54fd2d75512d094b23d6fbaf044be
Henrik Grimler <henrik@grimler.se>
2019-06-18 20:05:59 +0200

    libduktape: fix dev-package's BREAKS and REPLACES

--- packages/libduktape/build.sh
+++ packages/libduktape/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.duktape.org/
 TERMUX_PKG_DESCRIPTION="An embeddable Javascript engine with a focus on portability and compact footprint"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=2.3.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_REPLACES="duktape (<< 2.3.0-1)"
 TERMUX_PKG_BREAKS="duktape (<< 2.3.0-1)"
-TERMUX_SUBPKG_BREAKS="duktape-dev"
-TERMUX_SUBPKG_REPLACES="duktape-dev"
+TERMUX_PKG_DEVPACKAGE_BREAKS="duktape-dev"
+TERMUX_PKG_DEVPACKAGE_REPLACES="duktape-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_extract_package() {
