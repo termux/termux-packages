fbbe9854df0311a183a772d00d5901106b074d36
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-20 00:31:12 +0300

    libxapian: bump revision

--- packages/libxapian/build.sh
+++ packages/libxapian/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://xapian.org
 TERMUX_PKG_DESCRIPTION="Xapian search engine library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.4.11
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=9f16b2f3e2351a24034d7636f73566ab74c3f0729e9e0492934e956b25c5bc07
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://oligarchy.co.uk/xapian/${TERMUX_PKG_VERSION}/xapian-core-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=9f16b2f3e2351a24034d7636f73566ab74c3f0729e9e0492934e956b25c5bc07
 # Note that we cannot /proc/sys/kernel/random/uuid (permission denied on
 # new android versions) so need libuuid.
-TERMUX_PKG_DEPENDS="libuuid, zlib"
+TERMUX_PKG_DEPENDS="libc++, libuuid, zlib"
 TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/xapian-config share/man/man1/xapian-config.1"
