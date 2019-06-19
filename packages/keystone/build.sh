8bc873c82eca53bd98a9bb996dce073475f7e5e0
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-20 00:00:35 +0300

    keystone: bump revision

--- packages/keystone/build.sh
+++ packages/keystone/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Keystone is a lightweight multi-platform, multi-architec
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=0.9.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/keystone-engine/keystone/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e9d706cd0c19c49a6524b77db8158449b9c434b415fbf94a073968b68cf8a9f0
+TERMUX_PKG_DEPENDS="libc++"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DPYTHON_EXECUTABLE=$(which python2.7)
