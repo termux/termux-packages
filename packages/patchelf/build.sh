a648c638dfcea848a0b7f59b0daaaa496620b68e
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-20 00:51:21 +0300

    patchelf: bump revision

--- packages/patchelf/build.sh
+++ packages/patchelf/build.sh
@@ -2,8 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://nixos.org/patchelf.html
 TERMUX_PKG_DESCRIPTION="Utility to modify the dynamic linker and RPATH of ELF executables"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=0.10
-TERMUX_PKG_SHA256=b3cb6bdedcef5607ce34a350cf0b182eb979f8f7bc31eae55a93a70a3f020d13
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/NixOS/patchelf/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=b3cb6bdedcef5607ce34a350cf0b182eb979f8f7bc31eae55a93a70a3f020d13
+TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
