fd2579d8cb06f12e74ca47de1e48a5b738095b39
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-20 21:46:30 +0300

    aircrack-ng: bump revision

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -4,10 +4,10 @@ TERMUX_PKG_DESCRIPTION="WiFi security auditing tools suite"
 TERMUX_PKG_LICENSE="GPL-2.0"
 #1.5.1 (requires autoconf, automake, libtool, shtool, pkg-config (and maybe libstdc and make/gmake) support)
 TERMUX_PKG_VERSION=1.2-rc4
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/aircrack-ng/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ed241ea423cab1c86e7882e901d5200d91b2f97d7efd703b0acf17742be47f9b
-TERMUX_PKG_DEPENDS="libnl, openssl, libpcap, pciutils"
+TERMUX_PKG_DEPENDS="libc++, libnl, openssl, libpcap, pciutils"
 TERMUX_PKG_BUILD_DEPENDS="libnl-dev, openssl-dev, libpcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
