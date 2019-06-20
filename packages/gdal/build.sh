381cb9d48f02b9ed1d96039fb5b6f67a7acd57a3
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-20 14:22:57 +0300

    gdal: bump revision

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -1,11 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://gdal.org
 TERMUX_PKG_DESCRIPTION="A translator library for raster and vector geospatial data formats"
-TERMUX_PKG_MAINTAINER="Bjoern Schilberg @BjoernSchilberg"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
+TERMUX_PKG_MAINTAINER="Bjoern Schilberg @BjoernSchilberg"
 TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ad316fa052d94d9606e90b20a514b92b2dd64e3142dfdbd8f10981a5fcd5c43e
-TERMUX_PKG_DEPENDS="openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos"
+TERMUX_PKG_DEPENDS="libc++, openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-sqlite3=$TERMUX_PREFIX"
