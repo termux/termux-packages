5e04fa9f714235cdb3f211ca0ded1e3f0d9bed0c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-21 10:55:11 +0300

    rust: bump revision

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed an
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
 TERMUX_PKG_VERSION=1.35.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
 TERMUX_PKG_SHA256=169756df2298957bcf02da6a612996c24a51b9ac3b23409e6507d69eb2e6f523
-TERMUX_PKG_DEPENDS="clang, openssl, lld, zlib"
+TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib"
 
 termux_step_configure() {
 	termux_setup_cmake
@@ -15,7 +16,7 @@ termux_step_configure() {
 	# ../lib from bin location:
 	# this is about to get ugly but i have to make sure a rustc in a proper bin lib
 	# configuration is used otherwise it fails a long time into the build...
-	# like 30 to 40 + minutes ... so lets get it right 
+	# like 30 to 40 + minutes ... so lets get it right
 
 	export PATH=$HOME/.rustup/toolchains/$TERMUX_PKG_VERSION-x86_64-unknown-linux-gnu/bin:$PATH
 	local RUSTC=$(which rustc)
