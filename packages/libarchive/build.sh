4de695117ea7b89dade25f850d30a5a5e37c8c8d
Fredrik Fornwall <fredrik@fornwall.net>
2019-06-24 22:18:28 +0200

    libarchive: Update from 3.3.3 to 3.4.0

--- packages/libarchive/build.sh
+++ packages/libarchive/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.libarchive.org/
 TERMUX_PKG_DESCRIPTION="Multi-format archive and compression library"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=3.3.3
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SHA256=ba7eb1781c9fbbae178c4c6bad1c6eb08edab9a1496c64833d1715d022b30e2e
-TERMUX_PKG_SRCURL=https://www.libarchive.org/downloads/libarchive-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=3.4.0
+TERMUX_PKG_SHA256=8643d50ed40c759f5412a3af4e353cffbce4fdf3b5cf321cb72cacf06b2d825e
+TERMUX_PKG_SRCURL=https://github.com/libarchive/libarchive/releases/download/v$TERMUX_PKG_VERSION/libarchive-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_DEPENDS="libbz2, libiconv, liblzma, libxml2, openssl, zlib"
 
 # --without-nettle to use openssl instead:
