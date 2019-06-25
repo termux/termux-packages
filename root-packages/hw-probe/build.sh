d048a73a44b15f9c6ec0cb88cc4540a9061f0d18
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-25 23:48:12 +0300

    new package: hw-probe

---
+++ root-packages/hw-probe/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/linuxhw/hw-probe
+TERMUX_PKG_DESCRIPTION="Tool to probe for hardware and check its operability"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.4
+TERMUX_PKG_SRCURL=https://github.com/linuxhw/hw-probe/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=90f3ea83bf641348b209e4a2a910f65d836ae7828c0be0f660236ea413bc46bb
+TERMUX_PKG_DEPENDS="curl, hwinfo, net-tools, perl"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	install -Dm700 hw-probe.pl "$TERMUX_PREFIX"/bin/hw-probe
+}
