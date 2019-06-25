9f6dc23595c51c4e8cc63f7492bc8a738dac3c22
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-25 23:14:55 +0300

    new package: hwinfo

---
+++ root-packages/hwinfo/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/openSUSE/hwinfo
+TERMUX_PKG_DESCRIPTION="Hardware detection tool from openSUSE"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=21.60
+TERMUX_PKG_SRCURL=https://github.com/openSUSE/hwinfo/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=7d07ecd799a6c0e25d1dcd5ec7cbba1f1a4f2c69938593900a38bfd32a7d083a
+TERMUX_PKG_DEPENDS="libandroid-shmem, libuuid, libx86emu"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_configure() {
+	echo 'touch changelog' > git2log
+	LDFLAGS+=" -landroid-shmem"
+}
+
+termux_step_make() {
+	make -j1 HWINFO_VERSION="$TERMUX_PKG_VERSION" \
+		LIBDIR="$TERMUX_PREFIX/lib"
+}
+
+termux_step_make_install() {
+	make HWINFO_VERSION="$TERMUX_PKG_VERSION" \
+		LIBDIR="$TERMUX_PREFIX/lib" PREFIX="$TERMUX_PREFIX" install
+}
