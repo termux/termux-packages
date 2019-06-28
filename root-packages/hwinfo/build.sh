471ef45ffc47be70823aa18b574a600ac06ef18b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-29 02:19:08 +0300

    hwinfo: fix the installation path of libhd.so

--- root-packages/hwinfo/build.sh
+++ root-packages/hwinfo/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/openSUSE/hwinfo
 TERMUX_PKG_DESCRIPTION="Hardware detection tool from openSUSE"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=21.60
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/openSUSE/hwinfo/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=7d07ecd799a6c0e25d1dcd5ec7cbba1f1a4f2c69938593900a38bfd32a7d083a
 TERMUX_PKG_DEPENDS="libandroid-shmem, libuuid, libx86emu"
@@ -19,5 +20,5 @@ termux_step_make() {
 
 termux_step_make_install() {
 	make HWINFO_VERSION="$TERMUX_PKG_VERSION" \
-		LIBDIR="$TERMUX_PREFIX/lib" PREFIX="$TERMUX_PREFIX" install
+		DESTDIR="$TERMUX_PREFIX" install
 }
