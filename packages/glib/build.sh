1b4796a146207bad7f3edbf3ae7edb3e07d68382
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-28 19:19:24 +0300

    libffi: disable versioning + rebuild dependent packages

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=2.60.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2b941ec5dcb92e5ea83fe42f9eb55a827bc8a12c153ad2489d551c31d04733dd
 # libandroid-support to get langinfo.h in include path.
