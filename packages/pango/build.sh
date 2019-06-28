a9b08c2cc828c4fb7bef6ee743b8515cc3629f21
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-28 19:19:24 +0300

    libffi: disable versioning + rebuild dependent packages

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=1.42.4
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_DEPENDS="fontconfig,fribidi,glib,harfbuzz,libcairo"
+TERMUX_PKG_SHA256=1d2b74cd63e8bd41961f2f8d952355aa0f9be6002b52c8aa7699d9f5da597c9d
+TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo, libffi"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libcairo-dev, libpixman-dev, fontconfig-dev, harfbuzz-dev"
 
 termux_step_post_make_install() {
