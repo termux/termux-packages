9bbd77222d16c9e48511ea7a57154f39029bce86
Henrik Grimler <henrik@grimler.se>
2019-06-28 18:01:11 +0200

    libusb: update to 1.0.23-rc1

--- root-packages/libusb/build.sh
+++ root-packages/libusb/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libusb.info/
 TERMUX_PKG_DESCRIPTION="A C library that provides generic access to USB devices"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=1.0.22
+TERMUX_PKG_VERSION=1.0.23-rc1
 TERMUX_PKG_SRCURL=https://github.com/libusb/libusb/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3500f7b182750cd9ccf9be8b1df998f83df56a39ab264976bdb3307773e16f48
+TERMUX_PKG_SHA256=e8b42af53a54488286bf164266766c9ca7fb692773fe7b47ea0ccca4d6fbf4a0
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-udev"
 
 termux_step_pre_configure() {
