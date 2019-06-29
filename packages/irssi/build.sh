e61a9c154731f7dec1582e40ca7251f4c8837541
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-29 17:53:44 +0300

    irssi: update to 1.2.1

--- packages/irssi/build.sh
+++ packages/irssi/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://irssi.org/
 TERMUX_PKG_DESCRIPTION="Terminal based IRC client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_DEPENDS="libiconv, ncurses, openssl, glib, libandroid-glob, utf8proc"
-TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SHA256=1643fca1d8b35e5a5d7b715c9c889e1e9cdb7e578e06487901ea959e6ab3ebe5
+TERMUX_PKG_VERSION=1.2.1
 TERMUX_PKG_SRCURL=https://github.com/irssi/irssi/releases/download/$TERMUX_PKG_VERSION/irssi-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=5466a1ed9612cfa707d9a37d60b29d027b4ac7d83c74ceb1a410e2b59edba92c
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
