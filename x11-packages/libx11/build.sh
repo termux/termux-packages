d3f3967388082cd9a111d13cb28d3f23bbe73fec
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-29 21:31:42 +0300

    libx11: update to 1.6.8

--- x11-packages/libx11/build.sh
+++ x11-packages/libx11/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 client-side library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.6.7
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=1.6.8
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=910e9e30efba4ad3672ca277741c2728aebffa7bc526f04dcfa74df2e52a1348
+TERMUX_PKG_SHA256=b289a845c189e251e0e884cc0f9269bbe97c238df3741e854ec4c17c21e473d5
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb, x11-repo (>= 1.3)"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros, xtrans"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="xorgproto, xtrans"
