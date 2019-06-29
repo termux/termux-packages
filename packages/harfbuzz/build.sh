c035b722f7e5f129854545a907fee3b6ef7b2ffb
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-29 17:53:40 +0300

    harfbuzz: update to 2.5.3

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.5.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.5.3
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6d4834579abd5f7ab3861c085b4c55129f78b27fe47961fd96769d3704f6719e
+TERMUX_PKG_SHA256=fed00dc797b7ba3ca943225f0a854baaed4c1640fff8a31d455cd3b5caec855c
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
 TERMUX_PKG_BUILD_DEPENDS="libicu"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libgraphite-dev"
