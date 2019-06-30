d88f633e4917d159e167d0dd2643b11823048e47
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-06-30 03:01:01 +0300

    sync gmic and graphicsmagick with branch 'master'
     * Unsplit graphicsmagick++{,-dev} subpackages.
    
     * Add tmpdir path fixes.

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="CeCILL-2.1"
 TERMUX_PKG_VERSION=2.6.7
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=2e5535d1bae66254136e928428750aac8efcef6f4413fc352b6de9ce8ac8b0ff
-TERMUX_PKG_DEPENDS="libc++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib, graphicsmagick++"
+TERMUX_PKG_DEPENDS="libc++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_BUILD_DEPENDS="graphicsmagick"
 TERMUX_PKG_NO_DEVELSPLIT=yes
 TERMUX_PKG_BUILD_IN_SRC=yes
@@ -15,13 +15,14 @@ termux_step_configure() {
 
 termux_step_make() {
 	cd src/
-	make cli
+	make USR="$TERMUX_PREFIX" STRIP="$STRIP" \
+	     CFLAGS="$CXXFLAGS" LIBS="$LDFLAGS" cli
 }
 
 termux_step_make_install() {
-	cp src/gmic $TERMUX_PREFIX/bin/gmic
-	cp src/*.h $TERMUX_PREFIX/include/
-	gunzip man/gmic.1.gz
-	cp man/gmic.1 $TERMUX_PREFIX/share/man/man1
+	cp src/gmic $TERMUX_PREFIX/bin/
+	cp src/gmic-gm $TERMUX_PREFIX/bin/
+	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/
+	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/gmic-gm.1.gz
 }
 
--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -2,10 +2,13 @@ TERMUX_PKG_HOMEPAGE=http://www.graphicsmagick.org/
 TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.3.32
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://fossies.org/linux/privat/GraphicsMagick-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_REVISION=2
+# Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
+TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b842a5a0d6c84fd6c5f161b5cd8e02bbd210b0c0b6728dd762b7c53062ba94e1
 TERMUX_PKG_DEPENDS="littlecms, libc++, libtiff, freetype, libjasper, libjpeg-turbo, libpng, libbz2, libxml2, liblzma, zstd, zlib"
+TERMUX_PKG_REPLACES="graphicsmagick++"
+TERMUX_PKG_DEVPACKAGE_REPLACES="graphicsmagick++-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_ftime=no
 --with-fontpath=/system/fonts
