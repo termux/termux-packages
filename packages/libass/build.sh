1e6a626c036a24b84cf364118fdc745184995b2d
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-06-30 03:09:00 +0300

    new package: libass
    Currently used only by mpv-x in x11-packages, but also required for
    https://github.com/termux/termux-packages/issues/2399.

---
+++ packages/libass/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/libass/libass
+TERMUX_PKG_DESCRIPTION="A portable library for SSA/ASS subtitles rendering"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=0.14.0
+TERMUX_PKG_SRCURL=https://github.com/libass/libass/releases/download/$TERMUX_PKG_VERSION/libass-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=881f2382af48aead75b7a0e02e65d88c5ebd369fe46bc77d9270a94aa8fd38a2
+TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz"
