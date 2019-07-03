a3a71e0c6e30884be59a08c05a61f91aebe446f3
Zaoqi <zaomir@outlook.com>
2019-07-03 21:03:01 +0800

    Update build.sh

--- packages/xmlstarlet/build.sh
+++ packages/xmlstarlet/build.sh
@@ -8,3 +8,7 @@ TERMUX_PKG_SHA256=15d838c4f3375332fd95554619179b69e4ec91418a3a5296e7c631b7ed19e7
 TERMUX_PKG_DEPENDS="libxslt, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libxml-include-prefix=${TERMUX_PREFIX}/include/libxml2"
 TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_post_make_install() {
+	ln -s $TERMUX_PREFIX/bin/xml $TERMUX_PREFIX/bin/xmlstarlet
+}
