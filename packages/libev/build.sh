47d7e655a1361b7e8669aac3acaea5dca20ea580
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-04 23:01:03 +0200

    libev: Update from 4.25 to 4.27

--- packages/libev/build.sh
+++ packages/libev/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://software.schmorp.de/pkg/libev.html
 TERMUX_PKG_DESCRIPTION="Full-featured and high-performance event loop library"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=4.25
-TERMUX_PKG_SHA256=78757e1c27778d2f3795251d9fe09715d51ce0422416da4abb34af3929c02589
+TERMUX_PKG_VERSION=4.27
+TERMUX_PKG_SHA256=2d5526fc8da4f072dd5c73e18fbb1666f5ef8ed78b73bba12e195cfdd810344e
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/libev-$TERMUX_PKG_VERSION.tar.gz
