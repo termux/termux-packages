6db158d742b69fd186a98c6fe18200a482d013ab
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-04 17:15:14 +0300

    fcgi: mark license as "custom"
    Required for Bintray package uploader.

--- packages/fcgi/build.sh
+++ packages/fcgi/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.fastcgi.com/
 TERMUX_PKG_DESCRIPTION="A language independent, high performant extension to CGI"
-TERMUX_PKG_LICENSE="OpenMarket"
+TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TERMS"
 TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/packages/fcgi/fcgi-$TERMUX_PKG_VERSION.tar.gz
