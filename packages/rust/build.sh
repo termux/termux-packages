5106ab4982eb93376e26b80097a1e881197dd50c
its-pointless <bored@goat.si>
2019-07-07 16:04:06 +1000

    1.36.0 rust

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
-TERMUX_PKG_VERSION=1.35.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.36.0
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
-TERMUX_PKG_SHA256=169756df2298957bcf02da6a612996c24a51b9ac3b23409e6507d69eb2e6f523
+TERMUX_PKG_SHA256=f51645b9f787af4a5d94db17f6af39db0c55980ed24fe366cad55b57900f8f2d
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib"
 
 termux_step_configure() {
@@ -18,7 +17,9 @@ termux_step_configure() {
 	# configuration is used otherwise it fails a long time into the build...
 	# like 30 to 40 + minutes ... so lets get it right
 
-	export PATH=$HOME/.rustup/toolchains/$TERMUX_PKG_VERSION-x86_64-unknown-linux-gnu/bin:$PATH
+	# 1.36 needs 1.35 to build revert to using $TERMUX_PKG_VERSION next time..
+	rustup install 1.35.0
+	export PATH=$HOME/.rustup/toolchains/1.35.0-x86_64-unknown-linux-gnu/bin:$PATH
 	local RUSTC=$(which rustc)
 	local CARGO=$(which cargo)
 
@@ -47,11 +48,7 @@ return 0;
 }
 
 termux_step_make_install() {
-	STAGE=""
-	if [ $TERMUX_ARCH = "x86_64"  ]; then
-		STAGE=" --stage 1" ; fi
-
-	$TERMUX_PKG_SRCDIR/x.py install $STAGE \
+	$TERMUX_PKG_SRCDIR/x.py install  \
 		--host $CARGO_TARGET_NAME \
 		--target $CARGO_TARGET_NAME \
 		--target wasm32-unknown-unknown
