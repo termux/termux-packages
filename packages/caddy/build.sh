0d03b8b0afbe37b13b9f5efbce071f8683d2e166
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-07 13:51:45 +0300

    caddy: update to 1.0.1

--- packages/caddy/build.sh
+++ packages/caddy/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://caddyserver.com/
 TERMUX_PKG_DESCRIPTION="Fast, cross-platform HTTP/2 web server"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SHA256=1c8b435a79e21b9832c7a8a88c44e70bc80434ca3719853d2b1092ffbbbbff7d
+TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SRCURL=https://github.com/mholt/caddy/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=7cbd80c798a0db4195381f730f5409680c6e1239fbc06295b90b34f666a05692
 
 termux_step_make() {
 	termux_setup_golang
