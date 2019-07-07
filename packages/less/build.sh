b7c64b9c3608590755ea216cbd8c6dfba7c4b1c2
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-07 14:15:44 +0300

    less: update to 551

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.greenwoodsoftware.com/less/
 TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=530
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=503f91ab0af4846f34f0444ab71c4b286123f0044a4964f1ae781486c617f2e2
+TERMUX_PKG_VERSION=551
 TERMUX_PKG_SRCURL=http://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d
 TERMUX_PKG_DEPENDS="ncurses"
