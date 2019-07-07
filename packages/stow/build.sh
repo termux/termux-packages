1f825001ae747358036f86c991dd8cbe9722b917
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-07 14:16:23 +0300

    stow: update to 2.3.0

--- packages/stow/build.sh
+++ packages/stow/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/stow/
 TERMUX_PKG_DESCRIPTION="Tool for managing the installation of multiple software packages into a single directory"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.2.2
+TERMUX_PKG_VERSION=2.3.0
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/stow-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e2f77649301b215b9adbc2f074523bedebad366812690b9dc94457af5cf273df
+TERMUX_PKG_SHA256=0524eaa0d4485d1bffb78b5cdf5b6fc13e39444666ca2e51336f66ddb3c81841
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
