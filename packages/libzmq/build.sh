504a740d1adcd2ac58be4d80697747f7ce947421
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-11 10:31:35 +0200

    libzmq: Update from 4.3.1 to 4.3.2

--- packages/libzmq/build.sh
+++ packages/libzmq/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://zeromq.org/
 TERMUX_PKG_DESCRIPTION="Fast messaging system built on sockets. C and C++ bindings. aka 0MQ, ZMQ."
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=4.3.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=4.3.2
+TERMUX_PKG_SHA256=ebd7b5c830d6428956b67a0454a7f8cbed1de74b3b01e5c33c5378e22740f763
 TERMUX_PKG_SRCURL=https://github.com/zeromq/libzmq/releases/download/v${TERMUX_PKG_VERSION}/zeromq-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb
 TERMUX_PKG_DEPENDS="libc++, libsodium"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libsodium --disable-libunwind --disable-Werror"
 
