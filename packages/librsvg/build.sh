294a19be90105d827ce657f15b0f5eef271ae4e2
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-11 22:46:42 +0200

    librsvg: Update from 2.45.6 to 2.45.7

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -1,14 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.45.6
-TERMUX_PKG_SHA256=0e6e26cb5c79cfa73c0ddab06808ace4d10c4a626b81c31a75ead37c6cb4df41
+TERMUX_PKG_VERSION=2.45.7
+TERMUX_PKG_SHA256=d4399eb76eb4e955fd36a5e536e4cb965ccfafbbc72e3b43495e08ac2a61f993
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libcroco,pango,gdk-pixbuf,libcairo-gobject,zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-introspection --disable-pixbuf-loader"
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	LDFLAGS+=" -fuse-ld=lld"
 
 	# See https://github.com/GNOME/librsvg/blob/master/COMPILING.md
 	export RUST_TARGET=$CARGO_TARGET_NAME
