4c8eb4f819cd42e5c1dfdeeb6621b21d04e1cfa8
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-11 14:32:52 +0200

    hub: Update from 2.12.1 to 2.12.2

--- packages/hub/build.sh
+++ packages/hub/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://hub.github.com/
 TERMUX_PKG_DESCRIPTION="Command-line wrapper for git that makes you better at GitHub"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.12.1
+TERMUX_PKG_VERSION=2.12.2
 TERMUX_PKG_SRCURL=https://github.com/github/hub/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=315114e46bbf583e23341e171c8e1a5efdbe96c04868ff0b343cf367f65f67a0
+TERMUX_PKG_SHA256=3db0719e41731c57b8765b7f486aa91cee57bb7d39d5553d480d87a4ac6c2ee6
 TERMUX_PKG_DEPENDS="git"
 
 termux_step_make_install() {
