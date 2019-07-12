7026b609093f1fdc6dd4c06bfd1cd0450a9fad0a
Tristan Ross <spaceboyross@yandex.com>
2019-07-12 11:18:16 -0700

    added dbus package

---
+++ packages/dbus/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://dbus.freedesktop.org
+TERMUX_PKG_DESCRIPTION="Freedesktop.org message bus system"
+TERMUX_PKG_LICENSE="GPL"
+TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
+TERMUX_PKG_VERSION=1.12.16
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://dbus.freedesktop.org/releases/dbus/dbus-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=7ce4760cf66c69148f6bd6c92feaabb8812dee30846b24cd0f7395c436d7e825
+TERMUX_PKG_DEPENDS="libexpat"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libaudit --disable-systemd"
