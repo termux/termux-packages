601e301a4d52e0ab6e15937c6db8ed93936802c5
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-13 17:01:58 +0300

    perform hostbuild

--- packages/dbus-glib/build.sh
+++ packages/dbus-glib/build.sh
@@ -7,8 +7,15 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=7ce4760cf66c69148f6bd6c92feaabb8812dee30846b24cd0f7395c436d7e825
 TERMUX_PKG_DEPENDS="dbus, glib"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	(cd $TERMUX_PKG_SRCDIR && autoconf -i)
+	$TERMUX_PKG_SRCDIR/configure
+	make -j $TERMUX_MAKE_PROCESSES
+}
 
 termux_step_pre_configure() {
 	autoconf -i
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-dbus-binding-tool=$TERMUX_PKG_HOSTBUILD_DIR/dbus/dbus-binding-tool"
 }
