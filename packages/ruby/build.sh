bec4c40320f908442309e48a3ab6654a3c802123
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-14 20:06:49 +0200

    ruby: Bump revision after optimization switch

--- packages/ruby/build.sh
+++ packages/ruby/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Dynamic programming language with a focus on simplicity 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 _MAJOR_VERSION=2.6
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SHA256=11a83f85c03d3f0fc9b8a9b6cad1b2674f26c5aaa43ba858d4b0fcc2b54171e1
 TERMUX_PKG_SRCURL=https://cache.ruby-lang.org/pub/ruby/${_MAJOR_VERSION}/ruby-${TERMUX_PKG_VERSION}.tar.xz
 # libbffi is used by the fiddle extension module:
