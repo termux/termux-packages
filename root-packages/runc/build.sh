2f2362bfc95e18e1522d69306cf3b921c89280ab
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-14 15:34:08 +0300

    runc: print info on post-installation + bump revision

--- root-packages/runc/build.sh
+++ root-packages/runc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A tool for spawning and running containers according to 
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.0.0-rc8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/opencontainers/runc/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=efe4ff9bbe49b19074346d65c914d809c0a3e90d062ea9619fe240f931f0b700
 TERMUX_PKG_DEPENDS="libseccomp"
@@ -21,4 +22,25 @@ termux_step_make() {
 termux_step_make_install() {
     cd "${GOPATH}/src/github.com/opencontainers/runc"
     install -Dm755 runc "${TERMUX_PREFIX}/bin/runc"
+}
+
+termux_step_create_debscripts() {
+	{
+		echo "#!$TERMUX_PREFIX/bin/sh"
+		echo "echo"
+		echo 'echo "RunC requires support for devices cgroup support in kernel."'
+		echo "echo"
+		echo 'echo "If CONFIG_CGROUP_DEVICE was enabled during compile time,"'
+		echo 'echo "you need to run the following commands (as root) in order"'
+		echo 'echo "to use the RunC:"'
+		echo "echo"
+		echo 'echo "  mount -t tmpfs -o mode=755 tmpfs /sys/fs/cgroup"'
+		echo 'echo "  mkdir -p /sys/fs/cgroup/devices"'
+		echo 'echo "  mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices"'
+		echo "echo"
+		echo 'echo "If you got error when running commands listed above, this"'
+		echo 'echo "usually means that your kernel lacks CONFIG_CGROUP_DEVICE."'
+		echo "echo"
+		echo "exit 0"
+	} > postinst
 }
