2df59927a4be234152bac6e03728440990b2df07
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-14 20:13:47 +0300

    fossil: update to 2.9

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION=2.8
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SHA256=6a32bec73de26ff5cc8bbb0b7b45360f4e4145931fd215ed91414ed190b3715d
+TERMUX_PKG_VERSION=2.9
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1cb2ada92d43e3e7e008fe77f5e743d301c7ea34d4c36c42f255f873e73d8b4f
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
