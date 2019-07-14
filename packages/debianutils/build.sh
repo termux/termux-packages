42bc4c007c2719337addebe952739bfe498014a5
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-14 20:11:10 +0300

    debianutils: update to 4.8.6.2

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/debianutils
 TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installation scripts of Debian packages"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=4.8.6.1
-TERMUX_PKG_SHA256=099f1e8a7278b26145a2ba2dda84c4118403bfab38c8d7070a6235a7ffcb55ed
+TERMUX_PKG_VERSION=4.8.6.2
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=aca388ba52d19f7c2f7991183a1c15c4d375c692a6cb4bae62cd17b485b10bba
 TERMUX_PKG_RM_AFTER_INSTALL="bin/installkernel share/man/man8/installkernel.8"
