ddb227dbed91d1b3e1c39a200d2ffb11bcde906d
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-14 20:18:23 +0300

    strace: update to 5.2

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=5.0
-TERMUX_PKG_SHA256=3b7ad77eb2b81dc6078046a9cc56eed5242b67b63748e7fc28f7c2daf4e647da
+TERMUX_PKG_VERSION=5.2
 TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=d513bc085609a9afd64faf2ce71deb95b96faf46cd7bc86048bc655e4e4c24d2
 TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
