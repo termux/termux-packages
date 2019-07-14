32de55cecea124fcf38c86487d94ef28b0ea8e68
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-14 20:03:07 +0200

    Always use -Oz optimization level

--- packages/scrypt/build.sh
+++ packages/scrypt/build.sh
@@ -2,13 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.tarsnap.com/scrypt.html
 TERMUX_PKG_DESCRIPTION="scrypt KDF library and file encryption tool"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_VERSION=1.2.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://www.tarsnap.com/scrypt/scrypt-1.2.1.tgz
 TERMUX_PKG_SHA256=4621f5e7da2f802e20850436219370092e9fcda93bd598f6d4236cce33f4c577
 TERMUX_PKG_DEPENDS="openssl"
-
-termux_step_pre_configure() {
-	# Work around miscompilation on at least aarch64 with -Oz,
-	# see https://github.com/termux/termux-packages/issues/3260:
-	export CFLAGS=${CFLAGS/-Oz/-Os}
-}
