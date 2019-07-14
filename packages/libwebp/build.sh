51246581c34df660c31efe0d8b809f8762dea214
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-14 19:23:48 +0200

    libwebp: Update from 1.0.2 to 1.0.3

--- packages/libwebp/build.sh
+++ packages/libwebp/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/webmproject/libwebp
 TERMUX_PKG_DESCRIPTION="Library to encode and decode images in WebP format"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=1.0.2
-TERMUX_PKG_SHA256=347cf85ddc3497832b5fa9eee62164a37b249c83adae0ba583093e039bf4881f
+TERMUX_PKG_VERSION=1.0.3
+TERMUX_PKG_SHA256=082d114bcb18a0e2aafc3148d43367c39304f86bf18ba0b2e766447e111a4a91
 TERMUX_PKG_SRCURL=https://github.com/webmproject/libwebp/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-gif
