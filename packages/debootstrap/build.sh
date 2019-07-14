4d5569fb1461e03c262f4cfee57b772860365a82
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-14 20:11:22 +0300

    debootstrap: update to 1.0.115

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.debian.org/Debootstrap
 TERMUX_PKG_DESCRIPTION="Bootstrap a basic Debian system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.0.114
-TERMUX_PKG_SHA256=a8e1456816a9ed55bf329de1cc93a199ad2099a21a66804b78e1aa0e170a9c92
-TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_BUILD_IN_SRC=yes
-TERMUX_PKG_DEPENDS="wget, proot, perl"
 TERMUX_PKG_MAINTAINER="Pierre Rudloff @Rudloff"
+TERMUX_PKG_VERSION=1.0.115
+TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1
+TERMUX_PKG_DEPENDS="wget, proot, perl"
+TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_PLATFORM_INDEPENDENT=yes
 
 termux_step_post_make_install() {
