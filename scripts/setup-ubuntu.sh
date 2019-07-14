cb88166c87aa449dd1960aa8f57bd01aad9bea51
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-14 19:53:38 +0300

    docker image: sync setup-ubuntu.sh with master branch

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -16,6 +16,7 @@ PACKAGES+=" git" # Used by the neovim build.
 PACKAGES+=" gperf" # Used by the fontconfig build.
 PACKAGES+=" help2man"
 PACKAGES+=" intltool" # Used by qalc build.
+PACKAGES+=" libdbus-1-dev" # Used by dbus-glib build.
 PACKAGES+=" libglib2.0-dev" # Provides 'glib-genmarshal' which the glib build uses.
 PACKAGES+=" libtool-bin"
 PACKAGES+=" libncurses5-dev" # Used by mariadb for host build part.
