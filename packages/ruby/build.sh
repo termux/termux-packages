824c4838ebbc1734499420d0b97bbef346f2e72d
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-14 20:03:07 +0200

    Always use -Oz optimization level

--- packages/ruby/build.sh
+++ packages/ruby/build.sh
@@ -23,7 +23,6 @@ termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH_BITS" = 32 ]; then
 		# process.c:function timetick2integer: error: undefined reference to '__mulodi4'
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" rb_cv_builtin___builtin_mul_overflow=no"
-		CFLAGS="${CFLAGS/-Os/-Oz}"
 	fi
 }
 
