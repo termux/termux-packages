9096d687ef696d1bcd5b03ac70f85ac3fabac175
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-18 01:51:38 +0300

    libdrm: update to 2.4.99

--- x11-packages/libdrm/build.sh
+++ x11-packages/libdrm/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://dri.freedesktop.org/wiki/
 TERMUX_PKG_DESCRIPTION="Userspace interface to kernel DRM services"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.4.98
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.4.99
 TERMUX_PKG_SRCURL=https://dri.freedesktop.org/libdrm/libdrm-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=8be0edccaca3abde8b6bb1431b46354c7fab46e9b91cc6946ba65b51f56f1894
+TERMUX_PKG_SHA256=4dbf539c7ed25dbb2055090b77ab87508fc46be39a9379d15fed4b5517e1da5e
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-intel
