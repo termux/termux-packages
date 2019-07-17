eba052e0e1cc503df3428dd9a2464f37e6bc2ba8
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-18 01:50:22 +0300

    dosbox: update to 0.74.3

--- x11-packages/dosbox/build.sh
+++ x11-packages/dosbox/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/dosbox/
 TERMUX_PKG_DESCRIPTION="Emulator with builtin DOS for running DOS Games"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.74.2
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=0.74.3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/dosbox/dosbox-${TERMUX_PKG_VERSION/.2/-2}.tar.gz
-TERMUX_PKG_SHA256=7077303595bedd7cd0bb94227fa9a6b5609e7c90a3e6523af11bc4afcb0a57cf
+TERMUX_PKG_SHA256=8cf5ce27d21490c24eedf91e0ac2bc4a748ba8f4eb20cb7c1fc9442d2d580008
 TERMUX_PKG_DEPENDS="libc++, libpng, libx11, sdl, sdl-net, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
