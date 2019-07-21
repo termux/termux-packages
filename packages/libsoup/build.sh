c40620ca0f25340d7eac8f7b3f9488fb60e10755
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-21 18:51:56 +0300

    libsoup: update to 2.66.2 + add new dep package libpsl

--- packages/libsoup/build.sh
+++ packages/libsoup/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/libsoup
 TERMUX_PKG_DESCRIPTION="HTTP client and server library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.62.3
-TERMUX_PKG_SHA256=d312ade547495c2093ff8bda61f9b9727a98cfdae339f3263277dd39c0451172
+TERMUX_PKG_VERSION=2.66.2
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libsoup/${TERMUX_PKG_VERSION:0:4}/libsoup-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-vala --without-gssapi"
-TERMUX_PKG_DEPENDS="glib, libsqlite, libxml2"
+TERMUX_PKG_SHA256=bd2ea602eba642509672812f3c99b77cbec2f3de02ba1cc8cb7206bf7de0ae2a
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dvapi=false -Dgssapi=false"
+TERMUX_PKG_DEPENDS="glib, libpsl, libsqlite, libxml2"
