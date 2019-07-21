ad47bed9bb06243b4a438f784b5d8f832e152e9e
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-21 21:23:46 +0300

    specify essential packages
     * libbz2 / bzip2
     * coreutils
     * libcurl / curl
     * gzip
     * less
     * tar
     * liblzma / xz-utils

--- packages/coreutils/build.sh
+++ packages/coreutils/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/coreutils/
 TERMUX_PKG_DESCRIPTION="Basic file, shell and text manipulation utilities from the GNU project"
 TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_ESSENTIAL=yes
 TERMUX_PKG_VERSION=8.31
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SHA256=ff7a9c918edce6b4f4b2725e3f9b37b0c4d193531cac49a48b56c4d0d3a9e9fd
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/coreutils/coreutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv"
