e46107874ee0fb8b122698550e5894a9a196cd72
Henrik Grimler <henrik@grimler.se>
2019-07-21 11:36:41 +0200

    bettercap: fix build error in docker image

--- root-packages/bettercap/build.sh
+++ root-packages/bettercap/build.sh
@@ -2,20 +2,25 @@ TERMUX_PKG_HOMEPAGE=https://www.bettercap.org
 TERMUX_PKG_DESCRIPTION="The Swiss Army knife for 802.11, BLE and Ethernet networks reconnaissance and MITM attacks"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.24.1
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/bettercap/bettercap/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=590cef2b2b24fd9f67c57c8cb19ab8ff08b11d43bfc23b468013ddad907bb8b8
 TERMUX_PKG_DEPENDS="libpcap, libusb"
-TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_configure() {
 	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+
+	mkdir -p "$GOPATH"/src/github.com/bettercap/
+	cp -a "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/bettercap/bettercap
+        go get github.com/bettercap/recording
 }
 
 termux_step_make() {
+	cd src/github.com/bettercap/bettercap
 	make build
 }
 
 termux_step_make_install() {
-	install bettercap $TERMUX_PREFIX/bin/
+	cd src/github.com/bettercap/bettercap
+	make install
 }
