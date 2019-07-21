862f382f61eecef00f06922b0450b13d97b71be0
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-21 17:01:19 +0300

    abuild: update to 3.4.0

--- packages/abuild/build.sh
+++ packages/abuild/build.sh
@@ -1,10 +1,11 @@
+## TODO: restore fakeroot functionality
+
 TERMUX_PKG_HOMEPAGE=https://github.com/alpinelinux/abuild
 TERMUX_PKG_DESCRIPTION="Build script to build Alpine packages"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.2.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=45b26674ca416e71612ff7924169a890a2cc45e945ecca33bc382f98e9ec3eb7
+TERMUX_PKG_VERSION=3.4.0
 TERMUX_PKG_SRCURL=https://github.com/alpinelinux/abuild/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f6f704e34f9d388a0228b645050dc7db7bf92f15a088835ae2c9b244420b9b61
 TERMUX_PKG_DEPENDS="apk-tools, autoconf, automake, bash, clang, curl, libtool, make, openssl-tool, pkg-config, tar, zlib"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_MAKE_ARGS="sysconfdir=$TERMUX_PREFIX/etc"
