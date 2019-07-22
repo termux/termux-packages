c282b69d16a78139924b8fe0e1aac09d24ad58e2
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-22 04:02:45 +0300

    gpgme: update to 1.13.1

--- packages/gpgme/build.sh
+++ packages/gpgme/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/gpgme/
 TERMUX_PKG_DESCRIPTION="Library designed to make access to GnuPG easier"
 TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_VERSION=1.13.1
+TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=c4e30b227682374c23cddc7fdb9324a99694d907e79242a25a4deeedb393be46
 TERMUX_PKG_DEPENDS="gnupg (>= 2.2.9-1), libassuan, libgpg-error"
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libgpg-error-dev"
-TERMUX_PKG_VERSION=1.13.0
-TERMUX_PKG_SHA256=d4b23e47a9e784a63e029338cce0464a82ce0ae4af852886afda410f9e39c630
-TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
 # Use "--disable-gpg-test" to avoid "No rule to make target `../../src/libgpgme-pthread.la":
 # Use "--enable-languages=no" to only build the C library.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
