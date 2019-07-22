2fdced2725105c934a952939189c1fb234c12f1f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-22 04:03:30 +0300

    libmosquitto: update to 1.6.3

--- packages/libmosquitto/mosquitto.subpackage.sh
+++ packages/libmosquitto/mosquitto.subpackage.sh
@@ -1,3 +1,3 @@
-TERMUX_SUBPKG_DEPENDS="libmosquitto, libuuid"
+TERMUX_SUBPKG_DEPENDS="libuuid"
 TERMUX_SUBPKG_INCLUDE="bin/ share/man/ etc/"
 TERMUX_SUBPKG_DESCRIPTION="MQTT version 3.1/3.1.1 compatible message broker and clients"
