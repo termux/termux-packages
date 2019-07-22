e35b667ede311374b315dd5e5876e0dc46996343
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-22 14:44:25 +0300

    ncurses: move clear/reset/tset to main package
    To make these utilities available in bootstrap.

--- packages/ncurses/ncurses-utils.subpackage.sh
+++ packages/ncurses/ncurses-utils.subpackage.sh
@@ -1,19 +1,13 @@
 TERMUX_SUBPKG_INCLUDE="
 bin/toe
 bin/tput
-bin/clear
-bin/tset
 bin/tabs
 bin/infocmp
-bin/reset
 bin/tic
 share/man/man1/toe.1.gz
 share/man/man1/tput.1.gz
-share/man/man1/clear.1.gz
-share/man/man1/tset.1.gz
 share/man/man1/tabs.1.gz
 share/man/man1/infocmp.1.gz
-share/man/man1/reset.1.gz
 share/man/man1/tic.1.gz
 "
 TERMUX_SUBPKG_DESCRIPTION="Tools for working with terminals"
