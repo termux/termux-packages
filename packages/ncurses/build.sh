7558ee77b9f31e9d90f4bef64e7de1eeb173ff60
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-22 14:44:25 +0300

    ncurses: move clear/reset/tset to main package
    To make these utilities available in bootstrap.

--- packages/ncurses/build.sh
+++ packages/ncurses/build.sh
@@ -4,13 +4,17 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=(6.1.20190511
 		    9.22
 		    15)
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SHA256=(fdbd39234fc7e7f8e5fd08d2329014e085fa5c8d0a9cc9a919e94bbc9d411c0e
 		   e94628e9bcfa0adb1115d83649f898d6edb4baced44f5d5b769c2eeb8b95addd
 		   3ae9ebef28aad081c6c11351f086776e2fd9547563b2f900732b41c376bec05a)
 TERMUX_PKG_SRCURL=(https://dl.bintray.com/termux/upstream/ncurses-${TERMUX_PKG_VERSION:0:3}-${TERMUX_PKG_VERSION:4}.tgz
 		   https://fossies.org/linux/misc/rxvt-unicode-${TERMUX_PKG_VERSION[1]}.tar.bz2
 		   https://github.com/thestinger/termite/archive/v${TERMUX_PKG_VERSION[2]}.tar.gz)
+
+# tset/reset/clear are moved to package 'ncurses'.
+TERMUX_PKG_BREAKS="ncurses-utils (<< 6.1.20190511-4)"
+TERMUX_PKG_REPLACES="ncurses-utils (<< 6.1.20190511-4)"
 
 # --without-normal disables static libraries:
 # --disable-stripping to disable -s argument to install which does not work when cross compiling:
