7a4e9ce18aa9ccc514b59fffd3e41a1a6056f585
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-22 04:04:18 +0300

    libsoduim: update to 1.0.18

--- packages/libsodium/build.sh
+++ packages/libsodium/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://libsodium.org/
 TERMUX_PKG_DESCRIPTION="Network communication, cryptography and signaturing library"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=1.0.17
-TERMUX_PKG_SHA256=0cc3dae33e642cc187b5ceb467e0ad0e1b51dcba577de1190e9ffa17766ac2b1
-TERMUX_PKG_SRCURL=https://github.com/jedisct1/libsodium/releases/download/${TERMUX_PKG_VERSION}/libsodium-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=1.0.18
+TERMUX_PKG_SRCURL=https://github.com/jedisct1/libsodium/archive/${TERMUX_PKG_VERSION}-RELEASE.tar.gz
+TERMUX_PKG_SHA256=b7292dd1da67a049c8e78415cd498ec138d194cfdb302e716b08d26b80fecc10
