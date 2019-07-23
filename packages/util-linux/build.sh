766e9ca1785b2bb1ec996d1639d4fbbbe55f2a3a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-23 17:42:29 +0300

    util-linux: update to 2.34

--- packages/util-linux/build.sh
+++ packages/util-linux/build.sh
@@ -1,22 +1,27 @@
 TERMUX_PKG_HOMEPAGE=https://en.wikipedia.org/wiki/Util-linux
 TERMUX_PKG_DESCRIPTION="Miscellaneous system utilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.33.2
+TERMUX_PKG_VERSION=2.34
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/util-linux/v${TERMUX_PKG_VERSION:0:4}/util-linux-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=631be8eac6cf6230ba478de211941d526808dba3cd436380793334496013ce97
+TERMUX_PKG_SHA256=743f9d0c7252b6db246b659c1e1ce0bd45d8d4508b4dfa427bbb4a3e9b9f62b5
 TERMUX_PKG_DEPENDS="ncurses, libcrypt, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_setns=yes
 ac_cv_func_unshare=yes
 --disable-agetty
+--disable-ctrlaltdel
 --disable-eject
+--disable-fdformat
 --disable-ipcrm
 --disable-ipcs
 --disable-kill
 --disable-last
 --disable-libuuid
 --disable-logger
+--disable-mesg
+--disable-nologin
 --disable-pivot_root
+--disable-raw
 --disable-switch_root
 --disable-wall
 --disable-libmount
