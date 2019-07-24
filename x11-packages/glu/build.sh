c5928c1008bbb85a4ba67f7d240c7035b7a9d90a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-24 17:22:30 +0300

    glu: update to 9.0.1

--- x11-packages/glu/build.sh
+++ x11-packages/glu/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://cgit.freedesktop.org/mesa/glu/
 TERMUX_PKG_DESCRIPTION="Mesa OpenGL Utility library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=9.0.0
-TERMUX_PKG_REVISION=6
-TERMUX_PKG_SRCURL=ftp://ftp.freedesktop.org/pub/mesa/glu/glu-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=1f7ad0d379a722fcbd303aa5650c6d7d5544fde83196b42a73d1193568a4df12
+TERMUX_PKG_VERSION=9.0.1
+TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/glu/glu-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f6f484cfcd51e489afe88031afdea1e173aa652697e4c19ddbcb8260579a10f7
 
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libexpat, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxfixes, libxshmfence, mesa, zlib"
 TERMUX_PKG_CONFLICTS="libglu"
