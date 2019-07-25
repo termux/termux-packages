3d4aea2ebc0ddb9d61ce506f08e761d4bc42f879
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 15:16:03 +0300

    dcraw: sources disappeared from official page, switching to mirror

--- packages/dcraw/build.sh
+++ packages/dcraw/build.sh
@@ -4,7 +4,8 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=9.28.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=2890c3da2642cd44c5f3bfed2c9b2c1db83da5cec09cc17e0fa72e17541fb4b9
-TERMUX_PKG_SRCURL=http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-${TERMUX_PKG_VERSION}.tar.gz
+#TERMUX_PKG_SRCURL=http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://mirrors.dotsrc.org/pub/mirrors/exherbo/dcraw-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="littlecms, libjasper, libjpeg-turbo"
 
