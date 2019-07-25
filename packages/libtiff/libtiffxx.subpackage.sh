483ba61fecc0f759b408944a21163a94b30f4101
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 15:34:03 +0300

    libtiff: do not split c++ part into a separate package

--- packages/libtiff/libtiffxx.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="lib/libtiffxx.so"
-TERMUX_SUBPKG_DESCRIPTION="Support for the Tag Image File Format (TIFF) for storing image data -- C++ interface"
-TERMUX_SUBPKG_DEPENDS="libtiff"
