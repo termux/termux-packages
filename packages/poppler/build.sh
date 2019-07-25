f5004f4a086e8bbf6ad7397eb2d338dcfa501802
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 15:05:07 +0300

    bump revisions of packages using libjpeg-turbo

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.79.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f985a4608fe592d2546d9d37d4182e502ff6b4c42f8db4be0a021a1c369528c8
 # libcairo and littlecms is used by pdftocairo:
