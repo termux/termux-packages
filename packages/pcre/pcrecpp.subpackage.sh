bc5d31abf1bc1b17cc00a8491de1a123e404ac93
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 15:40:26 +0300

    pcre: do not split c++ part into a separate package

--- packages/pcre/pcrecpp.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="lib/libpcrecpp.so"
-TERMUX_SUBPKG_DESCRIPTION="Library implementing regular expression pattern matching using the same syntax and semantics as Perl -- c++"
-TERMUX_SUBPKG_DEPENDS="pcre"
