db72d3daba96fc79d5303ecf0bcac749c7f0ff8c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 18:06:16 +0300

    ccnet: update to 6.1.8

--- packages/ccnet/build.sh
+++ packages/ccnet/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/haiwen/ccnet
 TERMUX_PKG_DESCRIPTION="Ccnet is a framework for writing networked applications in C"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=6.1.7
+TERMUX_PKG_VERSION=6.1.8
 TERMUX_PKG_SRCURL=https://github.com/haiwen/ccnet/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f9c81586181a8f331d3b00f334003e6b3f1af774f647cba30d5c9f9c546f7fce
+TERMUX_PKG_SHA256=b55636bf95232dc1db70d3604d0bebb7c3d730292d15b9b6c5b951307eb69762
 TERMUX_PKG_DEPENDS="libuuid, libevent, libsearpc, libsqlite, openssl"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
