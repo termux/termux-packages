5b89917051b60d2d8bbdd45e959ee68937966bd7
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 15:05:07 +0300

    bump revisions of packages using libjpeg-turbo

--- packages/dcraw/build.sh
+++ packages/dcraw/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://www.cybercom.net/~dcoffin/dcraw/
 TERMUX_PKG_DESCRIPTION="Raw digital camera images decoding utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=9.28.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=2890c3da2642cd44c5f3bfed2c9b2c1db83da5cec09cc17e0fa72e17541fb4b9
 TERMUX_PKG_SRCURL=http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://www.graphicsmagick.org/
 TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.3.32
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 # Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b842a5a0d6c84fd6c5f161b5cd8e02bbd210b0c0b6728dd762b7c53062ba94e1
