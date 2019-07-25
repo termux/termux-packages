da8f3fce511787bc7ea9cda186e1bf940376f0ef
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-25 18:12:57 +0300

    seafile-client: update to 7.0.1 + fix build.sh

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://seafile.com
-TERMUX_PKG_DESCRIPTION="Seafile is a file syncinc and sharing software with file encryption and group sharing."
-TERMUX_PKG_VERSION=6.1.7
-TERMUX_PKG_SHA256=91a4b923155f3ae7c7391d272afec8eebc40866767e45b1ce5a78783a3523fa4
+TERMUX_PKG_DESCRIPTION="Seafile is a file syncing and sharing software with file encryption and group sharing"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_VERSION=7.0.1
 TERMUX_PKG_SRCURL=https://github.com/haiwen/seafile/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c79a186e3a512454cb65d863bd45ecc8ae2c66a0dd2010533f3fed7862fa92f8
 TERMUX_PKG_DEPENDS="ccnet, libcurl"
 TERMUX_PKG_BUILD_IN_SRC=yes
-
 
 termux_step_pre_configure() {
 	./autogen.sh
@@ -13,7 +13,6 @@ termux_step_pre_configure() {
 
 termux_step_post_configure() {
 	# the package has trouble to prepare some headers
-	cd lib
+	cd $TERMUX_PKG_SRCDIR/lib
 	python $TERMUX_PREFIX/bin/searpc-codegen.py $TERMUX_PKG_SRCDIR/lib/rpc_table.py
-	cd ..
 }
