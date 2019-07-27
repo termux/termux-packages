b319f89f0ecffbda8439c25812e95ba3baf496a9
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-28 00:39:00 +0300

    libgnutls: update to 3.6.9

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn2, libunistring"
-TERMUX_PKG_VERSION=3.6.8
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.6.9
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnutls/v${TERMUX_PKG_VERSION:0:3}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=aa81944e5635de981171772857e72be231a7e0f559ae0292d2737de475383e83
+TERMUX_PKG_SHA256=4331fca55817ecdd74450b908a6c29b4f05bb24dd13144c6284aa34d872e1fcb
 TERMUX_PKG_DEVPACKAGE_DEPENDS="libidn2-dev, libnettle-dev"
+TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-cxx
 --disable-hardware-acceleration
