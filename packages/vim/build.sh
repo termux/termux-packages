27302df5367067871a932f85261881667b7da65a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-27 19:05:27 +0300

    vim, vim-python: bump revision

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update both vim and vim-python to the same version in one PR.
 TERMUX_PKG_VERSION=8.1.1700
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=aa1d52f8e6f5ef9726a1b5924d4fe37548b53c89d7e5af25bcc45ef1bd60f154
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
