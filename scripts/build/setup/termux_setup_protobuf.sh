4778a36e5417d99264913e4edd51a10d5815090a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-28 02:02:34 +0300

    update download checksum in termux_setup_protobuf.sh

--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -7,7 +7,7 @@ termux_setup_protobuf() {
 		termux_download \
 				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
 				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				24ea6924faaf94d4a0c5850fdb278290a326eff9a68f36ee5809654faccd0e10
+				15e395b648a1a6dda8fd66868824a396e9d3e89bc2c8648e3b9ab9801bea5d55
 
 		rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
 		unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
