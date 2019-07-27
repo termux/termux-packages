d39fb1dc66347769c15857007143c8488cfe8099
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-27 19:08:26 +0300

    wireguard-tools: update to 0.0.20190702

--- packages/wireguard-tools/build.sh
+++ packages/wireguard-tools/build.sh
@@ -1,15 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://www.wireguard.com
 TERMUX_PKG_DESCRIPTION="Tools for the WireGuard secure network tunnel"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.0.20190406
-TERMUX_PKG_SHA256=2f06f3adf70b95e74a7736a22dcf6e9ef623b311a15b7d55b5474e57c3d0415b
+TERMUX_PKG_VERSION=0.0.20190702
 TERMUX_PKG_SRCURL=https://git.zx2c4.com/WireGuard/snapshot/WireGuard-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=1a1311bc71abd47a72c47d918be3bacc486b3de90734661858af75cc990dbaac
 TERMUX_PKG_DEPENDS="libmnl, tsu"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src/tools WITH_BASHCOMPLETION=yes WITH_WGQUICK=no WITH_SYSTEMDUNITS=no"
 
 termux_step_post_make_install() {
     cd src/tools/wg-quick
-    $CC $CFLAGS -DWG_CONFIG_SEARCH_PATHS="\"$TERMUX_ANDROID_HOME/.wireguard $TERMUX_PREFIX/etc/wireguard /data/misc/wireguard /data/data/com.wireguard.android/files\"" -o wg-quick android.c
+    $CC $CFLAGS $LDFLAGS -DWG_CONFIG_SEARCH_PATHS="\"$TERMUX_ANDROID_HOME/.wireguard $TERMUX_PREFIX/etc/wireguard /data/misc/wireguard /data/data/com.wireguard.android/files\"" -o wg-quick android.c
     install -m 0755 wg-quick $TERMUX_PREFIX/bin
 }
