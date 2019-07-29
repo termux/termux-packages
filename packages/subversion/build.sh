76b9e5b1a287b8b2069c0c96da8d05e00aba5176
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-30 01:44:06 +0200

    subversion: Update from 1.12.0 to 1.12.2

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://subversion.apache.org
 TERMUX_PKG_DESCRIPTION="Centralized version control system characterized by its simplicity"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.12.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=7fae7c73d8a007c107c0ae5eb372bc0bb013dbfe966fcd5c59cd5a195a5e2edf
+TERMUX_PKG_VERSION=1.12.2
+TERMUX_PKG_SHA256=3bd0b5c8e4c5175263dc9a92fd9aef94ce917e80af034f26fe5c45fde7e0f771
 TERMUX_PKG_SRCURL=https://www.apache.org/dist/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite, liblz4, utf8proc, zlib"
 TERMUX_PKG_BREAKS="subversion-dev"
