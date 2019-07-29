123061c1ffc15fed161fc6a97bba9cb4a94a3f91
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-29 12:11:20 +0300

    ccnet: breaks and replaces -dev package

--- packages/ccnet/build.sh
+++ packages/ccnet/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/haiwen/ccnet
 TERMUX_PKG_DESCRIPTION="Ccnet is a framework for writing networked applications in C"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=6.1.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/haiwen/ccnet/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b55636bf95232dc1db70d3604d0bebb7c3d730292d15b9b6c5b951307eb69762
 TERMUX_PKG_DEPENDS="libuuid, libevent, libsearpc, libsqlite, openssl"
+TERMUX_PKG_BREAKS="ccnet-dev"
+TERMUX_PKG_REPLACES="ccnet-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
