15e00d2072a7000530eaf9720a6c07ee26f79afd
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-29 16:09:26 +0300

    scripts/buildorder.py: do not handle static library subpackages
    * They are not used during normal build procedures.
    
    * During "fast-builds" (option '-i') they just consume our bandwidth.

--- scripts/buildorder.py
+++ scripts/buildorder.py
@@ -50,14 +50,6 @@ def parse_build_file_dependencies(path):
 
     return set(dependencies)
 
-def staticsplit(path):
-    with open(path, encoding="utf-8") as build_script:
-        for line in build_script:
-            if line.startswith('TERMUX_PKG_NO_STATICSPLIT'):
-                return False
-
-    return True
-
 class TermuxPackage(object):
     "A main package definition represented by a directory with a build.sh file."
     def __init__(self, dir_path, fast_build_mode):
@@ -86,11 +78,6 @@ class TermuxPackage(object):
             self.subpkgs.append(subpkg)
             self.deps.add(subpkg.name)
             self.deps |= subpkg.deps
-
-        if staticsplit(build_sh_path):
-            subpkg = TermuxSubPackage(self.dir + '/' + self.name + '-static' + '.subpackage.sh', self, virtual=True)
-            self.subpkgs.append(subpkg)
-            self.deps.add(subpkg.name)
 
         # Do not depend on itself
         self.deps.discard(self.name)
