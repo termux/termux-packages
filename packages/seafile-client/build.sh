acd7a30551ea27de3db851d948768d11647a81b6
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-07-29 14:37:56 +0300

    seafile-client: breaks & replaces seafile-client-dev

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=http://seafile.com
 TERMUX_PKG_DESCRIPTION="Seafile is a file syncing and sharing software with file encryption and group sharing"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=7.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/haiwen/seafile/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c79a186e3a512454cb65d863bd45ecc8ae2c66a0dd2010533f3fed7862fa92f8
 TERMUX_PKG_DEPENDS="ccnet, libcurl"
+TERMUX_PKG_BREAKS="seafile-client-dev"
+TERMUX_PKG_REPLACES="seafile-client-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 termux_step_pre_configure() {
