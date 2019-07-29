d6c7367df2a8c4c65650ca4a635b80fb1f7824f6
Henrik Grimler <henrik@grimler.se>
2019-07-29 09:11:02 +0200

    hwinfo: bump and conflict with dev pkg
    %ci:no-build

--- root-packages/hwinfo/build.sh
+++ root-packages/hwinfo/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/openSUSE/hwinfo
 TERMUX_PKG_DESCRIPTION="Hardware detection tool from openSUSE"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=21.60
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/openSUSE/hwinfo/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=7d07ecd799a6c0e25d1dcd5ec7cbba1f1a4f2c69938593900a38bfd32a7d083a
 TERMUX_PKG_DEPENDS="libandroid-shmem, libuuid, libx86emu"
+TERMUX_PKG_BREAKS="hwinfo-dev"
+TERMUX_PKG_REPLACES="hwinfo-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
