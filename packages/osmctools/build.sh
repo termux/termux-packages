ec502546076fe9190f5bd6f25ad51c08d13f3d0c
Wetitpig <winsto003@hotmail.com>
2019-07-29 23:22:19 +0800

    Add osmctools

---
+++ packages/osmctools/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.com/osm-c-tools/osmctools
+TERMUX_PKG_DESCRIPTION="Simple tools for OpenStreetMap processing."
+TERMUX_PKG_LICENSE="agplv3"
+TERMUX_PKG_VERSION=0.9
+TERMUX_PKG_SRCURL=https://gitlab.com/osm-c-tools/osmctools/-/archive/master/osmctools-master.tar.gz
+TERMUX_PKG_SHA256=af755766fd7168a16727aba122f8e427542c4c52669be1f463002d19c4c04ab1
