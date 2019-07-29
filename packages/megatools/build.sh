66bda67d8ac9dce0586d1fb112372222ed65a98b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-29 22:16:31 +0300

    megatools: source code is no longer hosted on github

--- packages/megatools/build.sh
+++ packages/megatools/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/megous/megatools
+TERMUX_PKG_HOMEPAGE=https://megatools.megous.com/
 TERMUX_PKG_DESCRIPTION="Open-source command line tools and C library (libmega) for accessing Mega.co.nz cloud storage"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.10.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=48468536492bfecd8b10a42e7608129eba9922e03cbce0a11dd9e338e2a0632d
-TERMUX_PKG_SRCURL=https://github.com/megous/megatools/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://megatools.megous.com/builds/megatools-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=179e84c68e24696c171238a72bcfe5e28198e4c4e9f9043704f36e5c0b17c38a
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, libgmp, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-fuse --enable-docs-build"
 TERMUX_PKG_BUILD_IN_SRC=yes
