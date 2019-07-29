2c599022ff1946a5802fc34859d4f851285933c6
Henrik Grimler <henrik@grimler.se>
2019-07-29 09:26:29 +0200

    libx86emu: bump and conflict with dev pkg
    %ci:no-build

--- root-packages/libx86emu/build.sh
+++ root-packages/libx86emu/build.sh
@@ -2,8 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/wfeldt/libx86emu
 TERMUX_PKG_DESCRIPTION="x86 emulation library"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=2.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/wfeldt/libx86emu/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b9664d1099f1dfe496f362f4923a2815d3e5785a1ce71edf08011a858731c3fc
+TERMUX_PKG_BREAKS="libx86emu-dev"
+TERMUX_PKG_REPLACES="libx86emu-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
