740ec2c1c71bad0a50e4ba08113dbbae7fe3928b
Henrik Grimler <henrik@grimler.se>
2019-07-29 09:25:20 +0200

    libusb: bump and conflict with dev pkg
    %ci:no-build

--- root-packages/libusb/build.sh
+++ root-packages/libusb/build.sh
@@ -2,8 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://libusb.info/
 TERMUX_PKG_DESCRIPTION="A C library that provides generic access to USB devices"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=1.0.23-rc1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libusb/libusb/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e8b42af53a54488286bf164266766c9ca7fb692773fe7b47ea0ccca4d6fbf4a0
+TERMUX_PKG_BREAKS="libusb-dev"
+TERMUX_PKG_REPLACES="libusb-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-udev"
 
 termux_step_pre_configure() {
