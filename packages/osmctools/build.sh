7c03faccaab9883897bec504a075e24883853db5
Henrik Grimler <henrik@grimler.se>
2019-07-30 20:57:05 +0200

    osmctools: fix sha256sum

--- packages/osmctools/build.sh
+++ packages/osmctools/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simple tools for OpenStreetMap processing"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_VERSION=0.9
 TERMUX_PKG_SRCURL=https://gitlab.com/osm-c-tools/osmctools/-/archive/${TERMUX_PKG_VERSION}/osmctools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=af755766fd7168a16727aba122f8e427542c4c52669be1f463002d19c4c04ab1
+TERMUX_PKG_SHA256=2f5298be5b4ba840a04f360c163849b34a31386ccd287657885e21268665f413
 
 termux_step_pre_configure () {
 	autoreconf --install
