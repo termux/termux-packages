1de5c0be82dc827fab52972fd084ce815a750268
Henrik Grimler <henrik@grimler.se>
2019-07-30 21:45:49 +0200

    aircrack-ng: remove -dev dependencies
    %ci-no-build

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -8,7 +8,6 @@ TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/aircrack-ng/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ed241ea423cab1c86e7882e901d5200d91b2f97d7efd703b0acf17742be47f9b
 TERMUX_PKG_DEPENDS="libc++, libnl, openssl, libpcap, pciutils"
-TERMUX_PKG_BUILD_DEPENDS="libnl-dev, openssl-dev, libpcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
 # TODO: in termux-packages, add support for these packages: shtool, libstdc, gmake
