77aec6efb55d70f6ef0044da64b533c855aef9ac
Wetitpig <winsto003@hotmail.com>
2019-07-30 09:24:24 +0800

    Rename License to LICENSE

--- packages/osmctools/build.sh
+++ packages/osmctools/build.sh
@@ -4,3 +4,7 @@ TERMUX_PKG_LICENSE="agplv3"
 TERMUX_PKG_VERSION=0.9
 TERMUX_PKG_SRCURL=https://gitlab.com/osm-c-tools/osmctools/-/archive/master/osmctools-master.tar.gz
 TERMUX_PKG_SHA256=af755766fd7168a16727aba122f8e427542c4c52669be1f463002d19c4c04ab1
+
+termux_step_post_extract_package () {
+	mv COPYING LICENSE
+}
