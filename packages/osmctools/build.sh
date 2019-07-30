6aee8eefa30c619ce931aeaf459c73aa9473120d
Henrik Grimler <henrik@grimler.se>
2019-07-30 20:16:44 +0200

    osmctools: fix build script

--- packages/osmctools/build.sh
+++ packages/osmctools/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/osm-c-tools/osmctools
-TERMUX_PKG_DESCRIPTION="Simple tools for OpenStreetMap processing."
-TERMUX_PKG_LICENSE="agplv3"
+TERMUX_PKG_DESCRIPTION="Simple tools for OpenStreetMap processing"
+TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_VERSION=0.9
-TERMUX_PKG_SRCURL=https://gitlab.com/osm-c-tools/osmctools/-/archive/master/osmctools-master.tar.gz
+TERMUX_PKG_SRCURL=https://gitlab.com/osm-c-tools/osmctools/-/archive/${TERMUX_PKG_VERSION}/osmctools-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=af755766fd7168a16727aba122f8e427542c4c52669be1f463002d19c4c04ab1
-TERMUX_PKG_BUILD_IN_SRC=yes
 
-termux_step_post_extract_package () {
-	mv COPYING LICENSE
+termux_step_pre_configure () {
+	autoreconf --install
 }
