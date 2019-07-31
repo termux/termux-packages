cc7e698858f4e2a8a3e5c08bfb5f0c3429e60ac0
Fredrik Fornwall <fredrik@fornwall.net>
2019-07-31 23:07:15 +0200

    exiv2: Update from 0.27.1 to 0.27.2

--- packages/exiv2/build.sh
+++ packages/exiv2/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://exiv2.org/
 TERMUX_PKG_DESCRIPTION="Exif, Iptc and XMP metadata manipulation library and tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.27.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/Exiv2/exiv2/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1b3766b2c203ce213a4195de14d61694017ec1a69d15d4575bccecef130990fe
+TERMUX_PKG_VERSION=0.27.2
+TERMUX_PKG_SRCURL=https://github.com/Exiv2/exiv2/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=3dbcaf01fbc5b98d42f091d1ff0d4b6cd9750dc724de3d9c0d113948570b2934
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libexpat, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
