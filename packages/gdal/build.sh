38bf8e065526b16ea034e0dd2dccdd271a54de99
Henrik Grimler <henrik@grimler.se>
2019-07-31 07:29:28 +0200

    gdal: conflict with dev pkg
    %ci-no-build

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -7,5 +7,7 @@ TERMUX_PKG_VERSION=3.0.1
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=45b4ae25dbd87282d589eca76481c426f72132d7a599556470d5c38263b09266
 TERMUX_PKG_DEPENDS="libc++, openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos"
+TERMUX_PKG_BREAKS="gdal-dev"
+TERMUX_PKG_REPLACES="gdal-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-sqlite3=$TERMUX_PREFIX"
