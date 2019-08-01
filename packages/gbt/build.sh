9565c00dd3cc2dfa872c901a1917652117c3c773
Jiri Tyr <jiri.tyr@gmail.com>
2019-08-01 02:03:19 +0100

    gbt: Update from 1.2.0 to 2.0.0

--- packages/gbt/build.sh
+++ packages/gbt/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jtyr/gbt
 TERMUX_PKG_DESCRIPTION="Highly configurable prompt builder for Bash and ZSH written in Go"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jtyr/gbt/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=eb309bb93e01ee168a528fb0ed3a4920a702a9dad5a8f205642717086b9162a0
+TERMUX_PKG_SHA256=b324695dc432e8e22bc257f7a6ec576f482ec418fb9c9a8301f47bfdf7766998
+_COMMIT=29dc3dac6c06518073a8e879d2b6ec65291ddab2
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
@@ -15,7 +16,7 @@ termux_step_make_install() {
 	mkdir -p $GOPATH/{bin,pkg,src/github.com/jtyr}
 	ln -fs $TERMUX_PKG_SRCDIR $GOPATH/src/github.com/jtyr/gbt
 
-	go build -ldflags='-s -w' -o $TERMUX_PREFIX/bin/gbt github.com/jtyr/gbt/cmd/gbt
+	go build -ldflags="-s -w -X main.version=$TERMUX_PKG_VERSION -X main.build=${_COMMIT::6}" -o $TERMUX_PREFIX/bin/gbt github.com/jtyr/gbt/cmd/gbt
 
 	mkdir -p $TERMUX_PREFIX/{doc/gbt,share/gbt}
 	cp -r $TERMUX_PKG_SRCDIR/{sources,themes} $TERMUX_PREFIX/share/gbt/
