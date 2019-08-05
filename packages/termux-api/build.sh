f1833f01fc0b67ce85c4ae82896625a86691cfdd
Fredrik Fornwall <fredrik@fornwall.net>
2019-08-06 00:52:28 +0200

    termux-api: Update from 0.41 to 0.42

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.41
-TERMUX_PKG_SHA256=b1adb682754d5ece04ab0133eced333c436bbec4e1d0ec75a75c31bb8cecb326
+TERMUX_PKG_VERSION=0.42
+TERMUX_PKG_SHA256=fbe6b2fbc208512d11d418418137d95f6e8944ed53b5f0045b23d1d3aa027eff
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_DEPENDS="bash"
