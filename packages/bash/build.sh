e9eb87dd84f9c616dd744648aa41fd92f829c25c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-05 14:10:55 +0300

    bash: make "command-not-found" optional
    Though, it will be pre-installed via bootstraps as previously.
    Possible after changes to bashrc introduced in b45da7b8ba.
    
    Related issue: https://github.com/termux/termux-packages/issues/661

--- packages/bash/build.sh
+++ packages/bash/build.sh
@@ -4,10 +4,11 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 _MAIN_VERSION=5.0
 _PATCH_VERSION=7
 TERMUX_PKG_VERSION=${_MAIN_VERSION}.${_PATCH_VERSION}
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/bash/bash-${_MAIN_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d
-TERMUX_PKG_DEPENDS="command-not-found, libandroid-support, libiconv, ncurses, readline (>= 8.0), termux-tools"
+TERMUX_PKG_DEPENDS="libandroid-support, libiconv, ncurses, readline (>= 8.0), termux-tools"
+TERMUX_PKG_RECOMMENDS="command-not-found"
 TERMUX_PKG_BREAKS="bash-dev"
 TERMUX_PKG_REPLACES="bash-dev"
 TERMUX_PKG_ESSENTIAL=true
