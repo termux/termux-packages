ab647b7530aa2b838cda7e380eeb3d64d2ed0a3a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-06 16:58:26 +0300

    libandroid-glob: bump package version

--- packages/libandroid-glob/build.sh
+++ packages/libandroid-glob/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://man7.org/linux/man-pages/man3/glob.3.html
 TERMUX_PKG_DESCRIPTION="Shared library for the glob(3) system function"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=0.5
+TERMUX_PKG_VERSION=0.6
 TERMUX_PKG_BREAKS="libandroid-glob-dev"
 TERMUX_PKG_REPLACES="libandroid-glob-dev"
 TERMUX_PKG_BUILD_IN_SRC=yes
