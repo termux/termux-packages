dbc6c2657358b9c5c950ec789fe1ad50f62fe6e7
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-11 19:37:34 +0300

    undo "Unsplit essential packages"
    Seems it is not possible to unsplit these packages currently.
    
    %ci:reset-backlog

--- packages/libcurl/build.sh
+++ packages/libcurl/build.sh
@@ -2,13 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://curl.haxx.se/
 TERMUX_PKG_DESCRIPTION="Easy-to-use client-side URL transfer library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=7.65.3
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://curl.haxx.se/download/curl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=0a855e83be482d7bc9ea00e05bdb1551a44966076762f9650959179c89fce509
 TERMUX_PKG_DEPENDS="libnghttp2, openssl (>= 1.1.1), zlib"
-TERMUX_PKG_CONFLICTS="apt (<< 1.4.8-8), curl, libcurl-dev"
-TERMUX_PKG_REPLACES="curl, libcurl-dev"
-TERMUX_PKG_PROVIDES="curl"
+TERMUX_PKG_BREAKS="libcurl-dev"
+TERMUX_PKG_REPLACES="libcurl-dev"
 TERMUX_PKG_ESSENTIAL=yes
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -21,3 +20,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-brotli
 --with-ssl
 "
+
+
+# Starting with version 7.62 curl started enabling http/2 by default.
+# Support for http/2 as added in version 1.4.8-8 of the apt package, so we
+# conflict with previous versions to avoid broken installations.
+TERMUX_PKG_CONFLICTS="apt (<< 1.4.8-8)"
