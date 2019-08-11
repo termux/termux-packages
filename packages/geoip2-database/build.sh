4f86b042b8a1cfb9b40f6aeddb0450ece062617f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-12 02:42:17 +0300

    geoip2-database: disable checksums
    Now just bump version approximately once per month for updating package.
    
    Database checksums are constantly changing, so disable them to prevent
    build errors when geoip2-database is a dependency.

--- packages/geoip2-database/build.sh
+++ packages/geoip2-database/build.sh
@@ -4,16 +4,13 @@ TERMUX_PKG_LICENSE="CC0-1.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_VERSION=20190730
 
+# Bump date ~once per month to update package.
+TERMUX_PKG_VERSION=20190730
 TERMUX_PKG_SRCURL=('https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz'
 				   'https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz'
 				   'https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz')
-
-## If these checksums become invalid - it's time to update package.
-TERMUX_PKG_SHA256=('08e0d8a7b937e17416d58ee8290bb89ecabb9ed5323836d795ba88d564084f83'
-				   '50142e7dace481fe35b84bf319799281fb438a989f8835c2ba2f714e5d63479b'
-				   'c20c58fe8f97a76b5809fdee58e848fe6823338c14a23fb7fd9d8fc01e2e4b37')
+TERMUX_PKG_SHA256=('SKIP_CHECKSUM' 'SKIP_CHECKSUM' 'SKIP_CHECKSUM')
 
 termux_step_make_install() {
 	install -Dm600 \
