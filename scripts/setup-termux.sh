8a00114eca35654b8a2b7d78d694536807f7bd38
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-11 14:52:38 +0300

    scripts/setup-termux.sh: split packages on "tier 1" & "tier 2" groups
    And remove packages that are installed with bootstrap archive (essential ones).

--- scripts/setup-termux.sh
+++ scripts/setup-termux.sh
@@ -1,50 +1,38 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Tier 1 packages are required by the core build scripts in scripts/build/.
-# Tier 2 packages are required to build many packages.
-# Some packages are installed by default and aren't labeled here.
-PACKAGES="autoconf"
+# Tier 1: requirements for the core build scripts in scripts/build/.
+PACKAGES="binutils-gold"	# Part of binutils which is dependency of clang.
+PACKAGES+=" clang"			# Required to build termux-elf-cleaner as well as other
+							# C/C++ packages.
+PACKAGES+=" file"			# Used in termux_step_massage().
+PACKAGES+=" lzip"			# Used by tar to extract *.tar.lz source archives.
+PACKAGES+=" patch"			# Used for applying patches on source code.
+PACKAGES+=" python"			# Used buildorder.py core script.
+PACKAGES+=" unzip"			# Used to extract *.zip source archives.
+
+# Tier 2: requirements for building many other packages.
+PACKAGES+=" autoconf"
 PACKAGES+=" automake"
 PACKAGES+=" bc"
-PACKAGES+=" binutils-gold"
 PACKAGES+=" bison"
-PACKAGES+=" bzip2"
-PACKAGES+=" clang"         # Tier 1: required to build termux-elf-cleaner, which
-                           # is built first by the core scripts.
 PACKAGES+=" cmake"
-PACKAGES+=" coreutils"
-PACKAGES+=" curl"
-PACKAGES+=" diffutils"
 PACKAGES+=" ed"
-PACKAGES+=" file"          # Tier 1: required by a core script
-PACKAGES+=" findutils"
 PACKAGES+=" flex"
-PACKAGES+=" gawk"
 PACKAGES+=" gettext"
 PACKAGES+=" git"
 PACKAGES+=" golang"
 PACKAGES+=" gperf"
-PACKAGES+=" grep"
-PACKAGES+=" gzip"
 PACKAGES+=" libtool"
-PACKAGES+=" lzip"
-PACKAGES+=" lzop"
 PACKAGES+=" m4"
-PACKAGES+=" make"          # Tier 2: used for all Makefile projects and to build itself
-PACKAGES+=" ninja"         # Tier 2: used by default to build all CMake projects
-PACKAGES+=" patch"
+PACKAGES+=" make"			# Used for all Makefile projects and to build itself.
+PACKAGES+=" ninja"			# Used by default to build all CMake projects.
 PACKAGES+=" perl"
-PACKAGES+=" pkg-config"    # Tier 2: used to build many packages
+PACKAGES+=" pkg-config"
 PACKAGES+=" protobuf"
-PACKAGES+=" python"        # Tier 1: required for buildorder.py core script
 PACKAGES+=" python2"
 PACKAGES+=" rust"
-PACKAGES+=" sed"
-PACKAGES+=" tar"
 PACKAGES+=" texinfo"
-PACKAGES+=" unzip"
 PACKAGES+=" valac"
-PACKAGES+=" xz-utils"
 
 apt update
 apt dist-upgrade -y
