ceb385a9a8fa7aa3bb887d79948c4f5c9cb63507
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-13 01:59:25 +0300

    build-package.sh: fix metapackage creation
    Metapackages shouldn't have data inside.
    
    %ci:reset-backlog

--- scripts/build/termux_step_create_datatar.sh
+++ scripts/build/termux_step_create_datatar.sh
@@ -8,8 +8,14 @@ termux_step_create_datatar() {
 		termux_error_exit "Package contains hard links: $HARDLINKS"
 	fi
 
-	if [ "${TERMUX_PKG_METAPACKAGE-false}" = "false" ] && [ "$(find . -type f)" = "" ]; then
-		termux_error_exit "No files in package"
+	if [ "${TERMUX_PKG_METAPACKAGE-false}" = "true" ]; then
+		# Metapackage doesn't have data inside.
+		rm -rf data
+	else
+		if [ "$(find . -type f)" = "" ]; then
+			termux_error_exit "No files in package"
+		fi
 	fi
+
 	tar -cJf "$TERMUX_PKG_PACKAGEDIR/data.tar.xz" .
 }
