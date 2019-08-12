4beb4d8841ee22b7860944734e1c41056cf3b3a4
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-12 20:09:16 +0300

    thunar: remove bad debscript

--- x11-packages/thunar/build.sh
+++ x11-packages/thunar/build.sh
@@ -6,12 +6,3 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/thunar/1.8/Thunar-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=7a758e7ac03501c520c304f8845353315c954d429b17d591b8eea8b14f1350b9
 TERMUX_PKG_DEPENDS="libandroid-shmem, libxfce4ui, libexo, hicolor-icon-theme, libexif, libnotify, libpng"
-
-termux_step_create_debscripts () {
-        cat > postinst <<HERE
-ln -sf ${TERMUX_PREFIX}/bin/thunar ${TERMUX_PREFIX}/bin/Thunar
-HERE
-        chmod 0755 postinst
-}
-
-
