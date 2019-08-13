c08fb25770443fc348328dfe86bb9848707ef1d3
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-13 03:07:32 +0300

    some disabled packages can be removed
     * elixir: https://github.com/termux/unstable-packages/tree/master/packages/elixir
     * libgeos: https://github.com/termux/science-packages/tree/master/packages/libgeos
     * nethack: https://github.com/termux/game-packages/tree/master/packages/nethack

--- disabled-packages/elixir/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=http://elixir-lang.org/
-TERMUX_PKG_DESCRIPTION="Dynamic and functional language leveraging the Erlang VM"
-TERMUX_PKG_VERSION=1.3.4
-TERMUX_PKG_SRCURL=https://github.com/elixir-lang/elixir/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="erlang"
-# TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl --disable-iri"
-TERMUX_PKG_BUILD_IN_SRC=true
