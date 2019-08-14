56c33a34addb16b4f58a23bea5662faf309ab54f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-14 11:30:32 +0300

    libxapian: rebuild package

--- packages/libxapian/build.sh
+++ packages/libxapian/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://xapian.org
 TERMUX_PKG_DESCRIPTION="Xapian search engine library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.4.12
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://oligarchy.co.uk/xapian/${TERMUX_PKG_VERSION}/xapian-core-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4f8a36da831712db41d38a039fefb5251869761a58be28ba802994bb930fac7c
 # Note that we cannot /proc/sys/kernel/random/uuid (permission denied on
