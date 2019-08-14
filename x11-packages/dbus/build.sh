3933096cc8b8e739dd6f86b39c5e2563460ce208
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-14 20:03:15 +0300

    dbus: enable x11 autolaunch
    %ci:no-build

--- x11-packages/dbus/build.sh
+++ x11-packages/dbus/build.sh
@@ -1,16 +1,24 @@
 TERMUX_PKG_HOMEPAGE=https://dbus.freedesktop.org
 TERMUX_PKG_DESCRIPTION="Freedesktop.org message bus system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.12.16
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL="https://dbus.freedesktop.org/releases/dbus/dbus-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=54a22d2fa42f2eb2a871f32811c6005b531b9613b1b93a0d269b05e7549fec80
-TERMUX_PKG_DEPENDS="libexpat"
+TERMUX_PKG_DEPENDS="libexpat, libx11"
 TERMUX_PKG_BREAKS="dbus-dev"
 TERMUX_PKG_REPLACES="dbus-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libaudit --disable-systemd --disable-tests"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-libaudit
+--disable-systemd
+--disable-tests
+--enable-x11-autolaunch
+--with-test-socket-dir=$TERMUX_PREFIX/tmp
+--with-x
+"
 
 termux_step_pre_configure() {
 	export LIBS="-llog"
