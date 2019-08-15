dddafe095bd8fa0566348d4a6abbc25f6fa85771
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-16 02:27:19 +0300

    docbook-xml: minor improvements

--- packages/docbook-xml/build.sh
+++ packages/docbook-xml/build.sh
@@ -2,45 +2,41 @@ TERMUX_PKG_HOMEPAGE=https://www.oasis-open.org/docbook/
 TERMUX_PKG_DESCRIPTION="A widely used XML scheme for writing documentation and help"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=4.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_DEPENDS="libxml2-utils"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_extract_package() {
 	mkdir -p $TERMUX_PKG_SRCDIR
-
 	cd $TERMUX_PKG_SRCDIR
 
 	termux_download "https://docbook.org/xml/4.1.2/docbkx412.zip" \
-		docbkx412.zip \
+		$TERMUX_PKG_CACHEDIR/docbkx412.zip \
 		30f0644064e0ea71751438251940b1431f46acada814a062870f486c772e7772
 
-	unzip -d docbook-xml-4.1.2 docbkx412.zip
-
 	termux_download "https://docbook.org/xml/4.2/docbook-xml-4.2.zip" \
-		"docbook-xml-4.2.zip" \
+		$TERMUX_PKG_CACHEDIR/docbook-xml-4.2.zip \
 		acc4601e4f97a196076b7e64b368d9248b07c7abf26b34a02cca40eeebe60fa2
 
-	unzip -d docbook-xml-4.2 docbook-xml-4.2.zip
-
 	termux_download "https://docbook.org/xml/4.3/docbook-xml-4.3.zip" \
-		"docbook-xml-4.3.zip" \
+		$TERMUX_PKG_CACHEDIR/docbook-xml-4.3.zip \
 		23068a94ea6fd484b004c5a73ec36a66aa47ea8f0d6b62cc1695931f5c143464
 
-	unzip -d docbook-xml-4.3 docbook-xml-4.3.zip
-
 	termux_download "https://docbook.org/xml/4.4/docbook-xml-4.4.zip" \
-		"docbook-xml-4.4.zip" \
+		$TERMUX_PKG_CACHEDIR/docbook-xml-4.4.zip \
 		02f159eb88c4254d95e831c51c144b1863b216d909b5ff45743a1ce6f5273090
 
-	unzip -d docbook-xml-4.4 docbook-xml-4.4.zip
-
 	termux_download "https://docbook.org/xml/4.5/docbook-xml-4.5.zip" \
-		"docbook-xml-4.5.zip" \
+		$TERMUX_PKG_CACHEDIR/docbook-xml-4.5.zip \
 		4e4e037a2b83c98c6c94818390d4bdd3f6e10f6ec62dd79188594e26190dc7b4
 
-	unzip -d docbook-xml-4.5 docbook-xml-4.5.zip
+	unzip -d docbook-xml-4.1.2 $TERMUX_PKG_CACHEDIR/docbkx412.zip
+
+	local ver
+	for ver in 4.{2..5}; do
+		unzip -d docbook-xml-${ver} $TERMUX_PKG_CACHEDIR/docbook-xml-${ver}.zip
+	done
 }
 
 termux_step_make_install() {
@@ -121,7 +117,6 @@ termux_step_create_debscripts() {
 		else
 			xmlcatalog --noout --del "file://$TERMUX_PREFIX/etc/xml/docbook-xml" \
 				$TERMUX_PREFIX/etc/xml/catalog
-			xmlcatalog --noout --create "$TERMUX_PREFIX/etc/xml/catalog"
 		fi
 		xmlcatalog --noout --add "delegatePublic" \
 			"-//OASIS//ENTITIES DocBook XML" \
