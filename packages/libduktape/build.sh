2ba219ae2cc4e8806a39e337aa11b8aec85a94c5
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-16 14:34:51 +0300

    libduktape: split library and interpreter into separate packages

--- packages/libduktape/build.sh
+++ packages/libduktape/build.sh
@@ -1,15 +1,22 @@
 TERMUX_PKG_HOMEPAGE=https://www.duktape.org/
-TERMUX_PKG_DESCRIPTION="The Duktape JavaScript interpreter"
+TERMUX_PKG_DESCRIPTION="An embeddable Javascript engine with a focus on portability and compact footprint"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.3.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_BREAKS="duktape-dev"
-TERMUX_PKG_REPLACES="duktape-dev"
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_VERSION=2.4.0
+TERMUX_PKG_REPLACES="duktape (<< 2.3.0-1), libduktape-dev"
+TERMUX_PKG_BREAKS="duktape (<< 2.3.0-1), libduktape-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_extract_package() {
 	git clone --depth=1 https://github.com/svaarala/duktape.git -b v${TERMUX_PKG_VERSION} ${TERMUX_PKG_SRCDIR}
+}
+
+termux_step_pre_configure() {
+	if $TERMUX_ON_DEVICE_BUILD; then
+		# configure.py requires 'yaml' python2 module.
+		if ! pip2 show pyyaml > /dev/null 2>&1; then
+			pip2 install pyyaml
+		fi
+	fi
 }
 
 termux_step_make() {
@@ -17,9 +24,9 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	cp libduktape.so.1.0.0 ${TERMUX_PREFIX}/lib/libduktape.so
-	cp duk ${TERMUX_PREFIX}/bin
-	cp prep/nondebug/*.h ${TERMUX_PREFIX}/include
+	install libduktape.so.1.0.0 ${TERMUX_PREFIX}/lib/libduktape.so
+	install duk ${TERMUX_PREFIX}/bin
+	install prep/nondebug/*.h ${TERMUX_PREFIX}/include
 }
 
 termux_step_post_make_install() {
