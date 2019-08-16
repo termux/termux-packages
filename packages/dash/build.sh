88b9243d4fdce02cb95531e6f21a81941d9e56f9
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-16 03:31:14 +0300

    dash: do not attempt to do a static linking
    Fixes on-device builds.

--- packages/dash/build.sh
+++ packages/dash/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://gondor.apana.org.au/~herbert/dash/
 TERMUX_PKG_DESCRIPTION="Small POSIX-compliant implementation of /bin/sh"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=0.5.10.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://gondor.apana.org.au/~herbert/dash/files/dash-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3c663919dc5c66ec991da14c7cf7e0be8ad00f3db73986a987c118862b5f6071
 TERMUX_PKG_ESSENTIAL=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
 
 termux_step_post_make_install() {
 	# Symlink sh -> dash
