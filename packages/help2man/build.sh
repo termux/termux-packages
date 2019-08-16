6d0832558aa0c3a080353fab3de18f2607b5c294
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-16 15:37:57 +0300

    help2man: enable package

---
+++ packages/help2man/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/help2man/
+TERMUX_PKG_DESCRIPTION="Conversion tool to create man files."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.47.10
+TERMUX_PKG_SRCURL=http://mirrors.kernel.org/gnu/help2man/help2man-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=f371cbfd63f879065422b58fa6b81e21870cd791ef6e11d4528608204aa4dcfb
+TERMUX_PKG_DEPENDS="perl"
+TERMUX_PKG_BUILD_IN_SRC=true
