4a1c051c31275f092e6493fa1ed50a1be22e7371
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-16 20:22:55 +0300

    libpcap: use `--without-libnl` to disable libnl linking instead of ac_cv_lib_nl_3_nl_socket_alloc
    Fixes on-device build.

--- packages/libpcap/build.sh
+++ packages/libpcap/build.sh
@@ -3,12 +3,11 @@ TERMUX_PKG_DESCRIPTION="Library for network traffic capture"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=1.9.0
 TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/libpcap-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b4f87ce52bba24111faf048d3f6450f116b42fe849bc1b997e494f605b3d2735
 TERMUX_PKG_BREAKS="libpcap-dev"
 TERMUX_PKG_REPLACES="libpcap-dev"
-# The main tcpdump.org was down 2017-04-12, so we're using a mirror:
-TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/libpcap-${TERMUX_PKG_VERSION}.tar.xz
-# ac_cv_lib_nl_3_nl_socket_alloc=no to avoid linking against libnl:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_nl_3_nl_socket_alloc=no --with-pcap=linux"
-TERMUX_PKG_RM_AFTER_INSTALL="bin/pcap-config share/man/man1/pcap-config.1"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-pcap=linux --without-libnl"
+TERMUX_PKG_RM_AFTER_INSTALL="bin/pcap-config share/man/man1/pcap-config.1"
