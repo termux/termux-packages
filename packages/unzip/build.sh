862803f4fe8c5599805a5e8776ffdb04d5b5378e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-16 04:11:19 +0300

    unzip: fix for on-device build
    Also use flags https://git.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/unzip#n51

--- packages/unzip/build.sh
+++ packages/unzip/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://www.info-zip.org/
 TERMUX_PKG_DESCRIPTION="Tools for working with zip files"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=6.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/infozip/unzip60.tar.gz
 TERMUX_PKG_SHA256=036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37
 TERMUX_PKG_DEPENDS="libbz2"
@@ -13,5 +13,24 @@ termux_step_configure() {
 }
 
 termux_step_make() {
-	LD="$CC $LDFLAGS -lbz2" CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS -DNO_LCHMOD -DUSE_BZIP2" make -j $TERMUX_MAKE_PROCESSES generic
+	CFLAGS+=" $CPPFLAGS"
+	CFLAGS+=" -DACORN_FTYPE_NFS"
+	CFLAGS+=" -DWILD_STOP_AT_DIR"
+	CFLAGS+=" -DLARGE_FILE_SUPPORT"
+	CFLAGS+=" -DUNICODE_SUPPORT"
+	CFLAGS+=" -DUNICODE_WCHAR"
+	CFLAGS+=" -DUTF8_MAYBE_NATIVE"
+	CFLAGS+=" -DNO_LCHMOD"
+	CFLAGS+=" -DDATE_FORMAT=DF_YMD"
+	CFLAGS+=" -DUSE_BZIP2"
+	CFLAGS+=" -DNOMEMCPY"
+	CFLAGS+=" -DNO_WORKING_ISPRINT"
+	CFLAGS+=" -I."
+
+	make -f unix/Makefile prefix=$TERMUX_PREFIX D_USE_BZ2=-DUSE_BZIP2 \
+		L_BZ2=-lbz2 LF2="$LDFLAGS" CC="$CC" CF="$CFLAGS" LD="$CC" unzips
+}
+
+termux_step_make_install() {
+	make -f unix/Makefile prefix=$TERMUX_PREFIX install
 }
