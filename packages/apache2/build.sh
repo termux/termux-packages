771834ffdaf7c95702b1674a33e92b9f3b52c69b
Fredrik Fornwall <fredrik@fornwall.net>
2019-08-17 23:52:05 +0200

    apache2: Update from 2.4.39 to 2.4.41

--- packages/apache2/build.sh
+++ packages/apache2/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://httpd.apache.org
 TERMUX_PKG_DESCRIPTION="Apache Web Server"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=2.4.39
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=b4ca9d05773aa59b54d66cd8f4744b945289f084d3be17d7981d1783a5decfa2
+TERMUX_PKG_VERSION=2.4.41
+TERMUX_PKG_SHA256=133d48298fe5315ae9366a0ec66282fa4040efa5d566174481077ade7d18ea40
 TERMUX_PKG_SRCURL=https://www.apache.org/dist/httpd/httpd-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_DEPENDS="apr, apr-util, pcre, openssl, libcrypt, libandroid-support, libnghttp2, libexpat, libuuid, zlib"
 TERMUX_PKG_BREAKS="apache2-dev"
