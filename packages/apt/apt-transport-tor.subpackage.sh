ba7f9f1c538c70ee122c0e25a3735823a358b93f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-20 14:50:17 +0300

    apt: fix subpackage script

--- packages/apt/apt-transport-tor.subpackage.sh
+++ packages/apt/apt-transport-tor.subpackage.sh
@@ -6,4 +6,4 @@ lib/apt/methods/tor+https
 
 TERMUX_SUBPKG_DESCRIPTION="APT transport for anonymous package downloads via Tor"
 TERMUX_SUBPKG_DEPENDS="tor"
-TERMUX_SUBPKG_PLATFORM_INDEPENDENT=yes
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
