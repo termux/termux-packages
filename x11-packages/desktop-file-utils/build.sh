db34205c94369f9bd1486e3d7508d2e1b320ad95
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-20 18:34:24 +0300

    desktop-file-utils: update to 0.24

--- x11-packages/desktop-file-utils/build.sh
+++ x11-packages/desktop-file-utils/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/desktop-file-utils
 TERMUX_PKG_DESCRIPTION="Command line utilities for working with desktop entries"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.23
-TERMUX_PKG_REVISION=13
+TERMUX_PKG_VERSION=0.24
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6c094031bdec46c9f621708f919084e1cb5294e2c5b1e4c883b3e70cb8903385
+TERMUX_PKG_SHA256=a1de5da60cbdbe91e5c9c10ac9afee6c3deb019e0cee5fdb9a99dddc245f83d9
 TERMUX_PKG_DEPENDS="glib"
 
 termux_step_create_debscripts() {
