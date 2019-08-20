73c89073945db5ac404c4bb76c813418a864f24f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-20 20:24:10 +0300

    iso-codes: fix download url

--- x11-packages/iso-codes/build.sh
+++ x11-packages/iso-codes/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Lists of the country, language, and currency names"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=4.3
-TERMUX_PKG_SRCURL=https://salsa.debian.org/iso-codes-team/iso-codes/uploads/049ce6aac94d842be809f4063950646c/iso-codes-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=891da4006a71011358ffc27657ac66e7b2814bb05a8e9d5d9ea4c14df5ca2892
+TERMUX_PKG_SRCURL=https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/iso-codes-$TERMUX_PKG_VERSION/iso-codes-iso-codes-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=6a321921b35b0ac73796f3b4ae41b29abaa523a01b0313313aa1d539c80ccfa0
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 #If you install ISO codes over a previous installed version, the install step will fail when creating some symlinks
