864a7fb98daf6958a3d9c6bf4e5140320ddf564d
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-08-21 10:58:40 +0300

    libiconv: breaks & replaces libandroid-support-dev
    For replacement of libandroid-support-dev v22.

--- packages/libiconv/build.sh
+++ packages/libiconv/build.sh
@@ -5,8 +5,8 @@ TERMUX_PKG_VERSION=1.16
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/libiconv/libiconv-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04
-TERMUX_PKG_BREAKS="libandroid-support (<= 24), libiconv-dev"
-TERMUX_PKG_REPLACES="libandroid-support (<= 24), libiconv-dev"
+TERMUX_PKG_BREAKS="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
+TERMUX_PKG_REPLACES="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 
 # Enable extra encodings (such as CP437) needed by some programs:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-extra-encodings"
