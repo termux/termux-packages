6c0f16353cadf87582646f7de2a6caa74739d594
Henrik Grimler <henrik@grimler.se>
2019-08-22 21:38:35 +0200

    transmission: mark service scripts as config files

--- packages/transmission/build.sh
+++ packages/transmission/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Easy, lean and powerful BitTorrent client"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_DEPENDS="libevent, openssl, libcurl"
 TERMUX_PKG_VERSION=2.94
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/transmission/transmission/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=440c2fd0f89b1ab59d8a4b79ecd7bffd61bc000e36fb5b6c8e88142a4fadbb1f
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk --enable-lightweight --cache-file=termux_configure.cache"
+TERMUX_PKG_CONFFILES="var/service/transmission/run var/service/transmission/log/run"
 
 termux_step_pre_configure() {
 	./autogen.sh
