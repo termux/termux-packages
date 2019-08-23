6565c64ae502dd32a6b272d74dbee1a637c41275
Bruno Bigras <bigras.bruno@gmail.com>
2019-08-23 15:28:35 -0400

    starship: add package

---
+++ packages/starship/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://starship.rs
+TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_VERSION=0.13.1
+TERMUX_PKG_SHA256=990b0e418224900824179bc6c8fca89566696be79d68b8af191da107b7414f46
+TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="openssl, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
