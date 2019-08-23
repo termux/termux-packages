57f98b4b530563dc85e40051d4bfcc2023030e03
Fredrik Fornwall <fredrik@fornwall.net>
2019-08-23 10:07:56 +0200

    shellharden: Update from 4.1 to 4.1.1

--- packages/shellharden/build.sh
+++ packages/shellharden/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/anordal/shellharden
 TERMUX_PKG_DESCRIPTION="The corrective bash syntax highlighter"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=4.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=4.1.1
 TERMUX_PKG_SRCURL=https://github.com/anordal/shellharden/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2714b827f72c336b7abf87f5a291ec182443a5479ec3eee516d6e04c81d56414
+TERMUX_PKG_SHA256=7d7ac3443f35eb74abfc78fa67db2947d60b7d0782f225f55d6eefafcf294c7c
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
