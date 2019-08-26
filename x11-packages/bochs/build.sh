13f395a6e045a0103f312de1ad150332ee4fa317
12101111 <w12101111@gmail.com>
2019-08-26 22:01:15 +0800

    fix readline compile enable some devices

--- x11-packages/bochs/build.sh
+++ x11-packages/bochs/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=2.6.9
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ee5b677fd9b1b9f484b5aeb4614f43df21993088c0c0571187f93acb0866e98c
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, pango-x, libandroid-shmem, libc++, libcairo-x, libgraphite, libx11, libxpm, libxrandr, ncurses"
+TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, pango-x, libc++, libcairo-x, libgraphite, libx11, libxpm, libxrandr, ncurses, readline"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-wx
@@ -12,16 +12,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-x
 --with-term
 --disable-docbook
---enable-cpu-level=6
---enable-fpu
---enable-3dnow
---enable-disasm
---enable-smp
 --enable-x86-64
---enable-avx
---enable-long-phy-address
---enable-disasm
---enable-usb
+--enable-smp
 --enable-debugger
---disable-readline
+--enable-disasm
+--enable-3dnow
+--enable-avx
+--enable-usb
+--enable-usb-ehci
+--enable-ne2000
+--enable-e1000
+--enable-clgd54xx
+--enable-voodoo
 "
