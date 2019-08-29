dd9867b337b0caef7d12f427ffb7596853080d2f
Butta <repo@butta.fastem.com>
2019-08-30 00:01:44 +0530

    libandroid-spawn: switch to modified upstream implementation and use it for ninja

--- packages/libandroid-spawn/build.sh
+++ packages/libandroid-spawn/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://man7.org/linux/man-pages/man3/posix_spawn.3.html
 TERMUX_PKG_DESCRIPTION="Shared library for the posix_spawn system function"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=0.1
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_VERSION=0.2
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	$CC $CFLAGS $CPPFLAGS -I$TERMUX_PKG_BUILDER_DIR -c $TERMUX_PKG_BUILDER_DIR/posix_spawn.c
+	$CC $CFLAGS $CPPFLAGS -I$TERMUX_PKG_BUILDER_DIR -c $TERMUX_PKG_BUILDER_DIR/posix_spawn.cpp
 	$CC $LDFLAGS -shared posix_spawn.o -o libandroid-spawn.so
 	$AR rcu libandroid-spawn.a posix_spawn.o
 }
--- packages/ninja/build.sh
+++ packages/ninja/build.sh
@@ -2,10 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://ninja-build.org
 TERMUX_PKG_DESCRIPTION="A small build system with a focus on speed"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=1.9.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/ninja-build/ninja/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_DEPENDS="libc++, libandroid-spawn"
+
+termux_step_pre_configure() {
+	CXXFLAGS+=" $CPPFLAGS"
+	LDFLAGS+=" -landroid-spawn"
+}
 
 termux_step_configure() {
 	$TERMUX_PKG_SRCDIR/configure.py
