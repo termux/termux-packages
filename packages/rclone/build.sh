ca0c82ab222b3e85667b070ee8a6fad2b9cddbda
Fredrik Fornwall <fredrik@fornwall.net>
2019-08-29 14:43:51 +0200

    rclone: Update from 1.48.0 to 1.49.1

--- packages/rclone/build.sh
+++ packages/rclone/build.sh
@@ -1,17 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://rclone.org/
 TERMUX_PKG_DESCRIPTION="rsync for cloud storage"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.48.0
-TERMUX_PKG_SRCURL=https://github.com/ncw/rclone/releases/download/v${TERMUX_PKG_VERSION}/rclone-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8cfed0b0e0c341c74d466d3ecc84e35e32666391d075445d50fe623035cd03e4
+TERMUX_PKG_VERSION=1.49.1
+TERMUX_PKG_SRCURL=https://github.com/rclone/rclone/releases/download/v${TERMUX_PKG_VERSION}/rclone-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bdb02ac657099a152a30c706323af1355b2152b01f1a47f11f7d15eb8edc18b4
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
 
 	termux_setup_golang
 
-	mkdir -p .gopath/src/github.com/ncw
-	ln -sf "$PWD" .gopath/src/github.com/ncw/rclone
+	mkdir -p .gopath/src/github.com/rclone
+	ln -sf "$PWD" .gopath/src/github.com/rclone/rclone
 	export GOPATH="$PWD/.gopath"
 
 	# google.golang.org/grpc/internal/syscall does not build with GO111MODULE=on
