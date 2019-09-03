c1ae58638747a12a00848a1621f6b5bb898a6490
Butta <repo@butta.fastem.com>
2019-09-03 14:38:01 +0530

    llbuild: add package

---
+++ packages/llbuild/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/apple/swift-llbuild
+TERMUX_PKG_DESCRIPTION="A low-level build system, used by the Swift Package Manager"
+TERMUX_PKG_LICENSE="Apache-2.0, NCSA"
+TERMUX_PKG_VERSION=0.1.1
+TERMUX_PKG_SRCURL=https://github.com/apple/swift-llbuild/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8f04c76dcc65bd47e197574998b30dfef1eab10a81aba704b605d12bd9c31b68
+TERMUX_PKG_DEPENDS="libc++, libandroid-spawn, libsqlite"
