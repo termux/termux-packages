094ce1b19dbf6216a56a65452c5e4a74f64fe2b2
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-08 02:50:41 +0300

    liblz4: update to 1.9.2

--- packages/liblz4/build.sh
+++ packages/liblz4/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://lz4.github.io/lz4/
 TERMUX_PKG_DESCRIPTION="Fast LZ compression algorithm library"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.9.1
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=f8377c89dad5c9f266edc0be9b73595296ecafd5bfa1000de148096c50052dc4
+TERMUX_PKG_VERSION=1.9.2
+TERMUX_PKG_SRCURL=https://github.com/lz4/lz4/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc
 TERMUX_PKG_BREAKS="liblz4-dev"
 TERMUX_PKG_REPLACES="liblz4-dev"
-TERMUX_PKG_SRCURL=https://github.com/lz4/lz4/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
