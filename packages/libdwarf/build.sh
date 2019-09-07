d75ffc183e3bfd0ef20985bb3fa7e05ca4dcf5a7
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-07 22:39:49 +0300

    Revert "new package: libdwarf"
    This reverts commit a2986eb6e52dd5e64ebe5ba4460ae75aea56bbca.
    
    Wrong package, uftrace expects libdw but not libdwarf.

--- packages/libdwarf/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://www.prevanders.net/dwarf.html
-TERMUX_PKG_DESCRIPTION="A library for handling DWARF Debugging Information Format"
-TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=20190529
-TERMUX_PKG_SRCURL=https://www.prevanders.net/libdwarf-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b414c3bff758df211d972de72df1da9f496224da3f649b950b7d7239ec69172c
-TERMUX_PKG_DEPENDS="libelf"
