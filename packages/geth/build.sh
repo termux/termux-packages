b45fc730dc82bd99ddbdf3206b1229a02d97cd48
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-08 17:08:35 +0300

    move some packages from unstable repo
    * geth
    * jbig2dec
    * myrepos
    * oleo
    * samefile
    * sdcv
    * yadm

---
+++ packages/geth/build.sh
@@ -0,0 +1,30 @@
+TERMUX_PKG_HOMEPAGE=https://geth.ethereum.org/
+TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
+TERMUX_PKG_LICENSE="LGPL-3.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.9.3
+TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=d16e8b42b37a23b448775f27f0d2018425e6a1e6fc215f764aad3665a20bf493
+
+termux_step_make() {
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/ethereum
+	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/ethereum/go-ethereum
+
+	cd "$GOPATH"/src/github.com/ethereum/go-ethereum
+	for applet in geth abigen bootnode ethkey evm rlpdump puppeth; do
+		(cd ./cmd/"$applet" && go build -v)
+	done
+	unset applet
+}
+
+termux_step_make_install() {
+	for applet in geth abigen bootnode ethkey evm rlpdump puppeth; do
+		install -Dm700 \
+			"$TERMUX_PKG_SRCDIR/cmd/$applet/$applet" \
+			"$TERMUX_PREFIX"/bin/
+	done
+	unset applet
+}
