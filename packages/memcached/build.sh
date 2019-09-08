3568e0d9451ab7a93cc497dbb6e18fc9b1e1de1a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-08 03:01:05 +0300

    memcached: update to 1.5.17

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.5.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.5.17
 TERMUX_PKG_SRCURL=https://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=45a22c890dc1edb27db567fb4c9c25b91bfd578477c08c5fb10dca93cc62cc5a
+TERMUX_PKG_SHA256=ac7c5af6e8b1acf5d5c644d162e046d8acf9302174af9f24c3f18e5481ce3a0d
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_BREAKS="memcached-dev"
 TERMUX_PKG_REPLACES="memcached-dev"
