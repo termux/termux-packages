0deda6baa14a968e71913027e0497210bd967d0e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-08 16:04:48 +0300

    move some packages from unstable repo
    * algernon
    * apg
    * aptly
    * arj
    * codecrypt
    * crunch
    * cryptopp
    * diskus
    * google-glog
    * hexyl
    * jp2a
    * libgfshare
    * libmcrypt
    * libmhash
    * libmicrohttpd
    * libnftnl
    * libsigsegv
    * lua-lpeg
    * morse2ascii
    * seccure
    * secure-delete
    * ssss
    * steghide
    * taglib
    * unicorn
    * vegeta
    * vis
    * wren
    * wuzz
    * zsync

---
+++ packages/taglib/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=http://taglib.github.io/
+TERMUX_PKG_DESCRIPTION="A Library for reading and editing the meta-data of several popular audio formats."
+TERMUX_PKG_LICENSE="LGPL-2.0, MPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=1.11.1
+TERMUX_PKG_REVISION=7
+TERMUX_PKG_SRCURL=http://taglib.github.io/releases/taglib-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=b6d1a5a610aae6ff39d93de5efd0fdc787aa9e9dc1e7026fa4c961b26563526b
+TERMUX_PKG_BUILD_DEPENDS="boost, zlib"
+TERMUX_PKG_BREAKS="taglib-dev"
+TERMUX_PKG_REPLACES="taglib-dev"
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="bin/taglib-config"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_SHARED_LIBS=ON
+-DWITH_MP4=ON
+-DWITH_ASF=ON"
