2383de1482d814e00863b55e2e5d495416a5449d
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-08 17:08:35 +0300

    move some packages from unstable repo
    * geth
    * jbig2dec
    * myrepos
    * oleo
    * samefile
    * sdcv
    * yadm

---
+++ packages/jbig2dec/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://jbig2dec.com/
+TERMUX_PKG_DESCRIPTION="Decoder implementation of the JBIG2 image compression format"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=0.16
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs927/jbig2dec-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=a4f6bf15d217e7816aa61b92971597c801e81f0a63f9fe1daee60fb88e0f0602
+TERMUX_PKG_DEPENDS="libpng"
+TERMUX_PKG_BREAKS="jbig2dec-dev"
+TERMUX_PKG_REPLACES="jbig2dec-dev"
