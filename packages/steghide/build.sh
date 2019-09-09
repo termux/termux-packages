ffc316917ff1802cbfa177219f1ce83ef550eb4a
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-09 12:31:38 +0200

    steghide: Fix mandir

--- packages/steghide/build.sh
+++ packages/steghide/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Embeds a message in a file by replacing some of the leas
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=0.5.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/steghide/steghide-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=78069b7cfe9d1f5348ae43f918f06f91d783c2b3ff25af021e6a312cf541b47b
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libmcrypt, libmhash, zlib"
@@ -11,4 +11,5 @@ TERMUX_PKG_DEPENDS="libjpeg-turbo, libmcrypt, libmhash, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_file__dev_random=yes
 ac_cv_file__dev_urandom=yes
+--mandir=$TERMUX_PREFIX/share/man
 "
