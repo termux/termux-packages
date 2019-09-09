42e58e830ee5d62b8b661b76f19f62c454f80a02
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-09 12:31:26 +0200

    libmcrypt: Fix mandir

--- packages/libmcrypt/build.sh
+++ packages/libmcrypt/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A library which provides a uniform interface to several 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=2.5.8
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/mcrypt/libmcrypt-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=bf2f1671f44af88e66477db0982d5ecb5116a5c767b0a0d68acb34499d41b793
 TERMUX_PKG_BREAKS="libmcrypt-dev"
 TERMUX_PKG_REPLACES="libmcrypt-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
