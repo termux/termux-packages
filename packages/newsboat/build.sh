30ce913d612b4da4f55c87230bb1f67c3dd41c7f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-10 16:14:52 +0300

    newsboat: bump revision

--- packages/newsboat/build.sh
+++ packages/newsboat/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://newsboat.org/
 TERMUX_PKG_DESCRIPTION="RSS/Atom feed reader for the text console"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=2.16.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://newsboat.org/releases/${TERMUX_PKG_VERSION}/newsboat-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4023c817b36fc08a3191283eec2c7161949c0727633f60ad837e11c599d3ad53
 TERMUX_PKG_DEPENDS="libc++, libiconv, libandroid-support, libandroid-glob, json-c, libsqlite, libcurl, libxml2, stfl, ncurses, openssl"
