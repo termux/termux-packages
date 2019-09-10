b61dd9efbac85bafa621a7a43c48ec9c4d670779
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-10 22:09:37 +0200

    bat: Update from 0.11.0 to 0.12.1

--- packages/bat/build.sh
+++ packages/bat/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/bat
 TERMUX_PKG_DESCRIPTION="A cat(1) clone with wings"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.11.0
-TERMUX_PKG_SHA256=bb4e39efadfab71c0c929a92b82dac58deacfe2a4eb527d4256ac0634e042ed2
+TERMUX_PKG_VERSION=0.12.1
+TERMUX_PKG_SHA256=1dd184ddc9e5228ba94d19afc0b8b440bfc1819fef8133fe331e2c0ec9e3f8e2
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/bat/archive/v$TERMUX_PKG_VERSION.tar.gz
 # bat calls less with '--RAW-CONTROL-CHARS' which busybox less does not support:
 TERMUX_PKG_DEPENDS="less, zlib"
@@ -10,6 +10,10 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	CFLAGS="$CFLAGS $CPPFLAGS"
+
+	# See https://github.com/nagisa/rust_libloading/issues/54
+	export CC_x86_64_unknown_linux_gnu=gcc
+	export CFLAGS_x86_64_unknown_linux_gnu=""
 }
 
 termux_step_post_make_install() {
