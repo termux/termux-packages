09be2d92363af3a15b9f4f681e8e22924899b0b0
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-10 21:45:36 +0200

    librsvg: Update from 2.45.90 to 2.46.0

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.45.90
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.46.0
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a7a9be634c452d0d9ecb3e2266d277e88c254957de62c579fd878f12afd38552
+TERMUX_PKG_SHA256=96c81e52cb81450f3b2e915e6409fd7d1e3c01e4661974b3a98c09a7c45743d1
 TERMUX_PKG_DEPENDS="libcroco, pango, gdk-pixbuf, libcairo-gobject, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
