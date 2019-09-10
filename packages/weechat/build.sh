ba0e10ac1a446f3bd9ca7d653e0d8aaffbd832bb
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-10 21:35:07 +0200

    weechat: Update from 2.5 to 2.6

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.6
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4c772c949c59949ff4156560c66900c5aaa9ab2da4439ee278b7c21b24daf191
+TERMUX_PKG_SHA256=0b06e551e69018dbd4828113a56a9657a16cb787eb44bc83adf45424e65759d9
 TERMUX_PKG_DEPENDS="libiconv, ncurses, libgcrypt, libcurl, libgnutls, libandroid-support, zlib"
 TERMUX_PKG_BREAKS="weechat-dev"
 TERMUX_PKG_REPLACES="weechat-dev"
