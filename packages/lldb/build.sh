79970f0e50143bac704ecd6365b00a8b9889f895
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-11 10:17:24 +0200

    lldb: Update from 8.0.0 to 8.0.1

--- packages/lldb/build.sh
+++ packages/lldb/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://lldb.llvm.org
 TERMUX_PKG_DESCRIPTION="LLVM based debugger"
 TERMUX_PKG_LICENSE="NCSA"
-TERMUX_PKG_VERSION=8.0.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://releases.llvm.org/${TERMUX_PKG_VERSION}/lldb-$TERMUX_PKG_VERSION.src.tar.xz
-TERMUX_PKG_SHA256=49918b9f09816554a20ac44c5f85a32dc0a7a00759b3259e78064d674eac0373
+TERMUX_PKG_VERSION=8.0.1
+TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/lldb-$TERMUX_PKG_VERSION.src.tar.xz
+TERMUX_PKG_SHA256=e8a79baa6d11dd0650ab4a1b479f699dfad82af627cbbcd49fa6f2dc14e131d7
 TERMUX_PKG_DEPENDS="libc++, libedit, libllvm, libxml2, ncurses-ui-libs"
 TERMUX_PKG_BREAKS="lldb-dev"
 TERMUX_PKG_REPLACES="lldb-dev"
