fdccc4f6f27845546eb226eba09d8dfb8395aa39
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-11 04:25:38 +0300

    libandroid-support: fix typo in build.sh
    Now libandroid-support.a shouldn't be empty.

--- packages/libandroid-support/build.sh
+++ packages/libandroid-support/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-support
 TERMUX_PKG_DESCRIPTION="Library extending the Android C library (Bionic) for additional multibyte, locale and math support"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=25
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=42e7b55c2e5fc91cc0447fa8bea432e7a75ec78b03469330668af17aea56f660
 TERMUX_PKG_PRE_DEPENDS="dpkg (>= 1.19.4-3)"
@@ -19,7 +19,7 @@ termux_step_make() {
 	done
 
 	cd objects
-	ar rcu ../libandroid-support.a
+	ar rcu ../libandroid-support.a *.o
 	$CC $LDFLAGS -shared -o ../libandroid-support.so *.o
 }
 
