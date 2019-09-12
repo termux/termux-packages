f7dca60f61a87a851197fa4109d7d3f023864850
Henrik Grimler <henrik@grimler.se>
2019-09-12 07:06:16 +0200

    bettercap: fix so that caplets can be installed correctly

--- root-packages/bettercap/build.sh
+++ root-packages/bettercap/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.bettercap.org
 TERMUX_PKG_DESCRIPTION="The Swiss Army knife for 802.11, BLE and Ethernet networks reconnaissance and MITM attacks"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.24.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/bettercap/bettercap/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=590cef2b2b24fd9f67c57c8cb19ab8ff08b11d43bfc23b468013ddad907bb8b8
 TERMUX_PKG_DEPENDS="libpcap, libusb"
