7cd1070e55bc2f0da3be73813f54ccdca2f5e5aa
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-13 15:26:17 +0300

    libevent: don't remove additional libs

--- packages/libevent/build.sh
+++ packages/libevent/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://libevent.org/
 TERMUX_PKG_DESCRIPTION="Library that provides asynchronous event notification"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=2.1.11
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/libevent/libevent/archive/release-${TERMUX_PKG_VERSION}-stable.tar.gz
 TERMUX_PKG_SHA256=229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e
 TERMUX_PKG_BREAKS="libevent-dev"
 TERMUX_PKG_REPLACES="libevent-dev"
-TERMUX_PKG_SRCURL=https://github.com/libevent/libevent/archive/release-${TERMUX_PKG_VERSION}-stable.tar.gz
-# Strip away libevent core, extra and openssl libraries until someone uses them
-TERMUX_PKG_RM_AFTER_INSTALL="bin/event_rpcgen.py lib/libevent_*"
+TERMUX_PKG_RM_AFTER_INSTALL="bin/event_rpcgen.py"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DEVENT__BUILD_SHARED_LIBRARIES=ON
 -DEVENT__DISABLE_BENCHMARK=ON
