168dfe699f824aec7e97b4ca4e6a3ac61b6de8ae
Stefan <info@bitcoinprivacy.net>
2019-09-13 16:51:17 +0200

    added conffiles to bitcoin,lnd

--- packages/lnd/build.sh
+++ packages/lnd/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=(https://github.com/lightningnetwork/lnd/releases/download/v$T
 TERMUX_PKG_SHA256=(ae8cb77eb7567ed9f8041a17eb6f65280cf81f4fbc3bf10eb671a423f62fc948
                    3bbfa000e2b4c7702f92d24235b5a098f37fd7b5830ca42586678f03d7cf9da3)
 TERMUX_PKG_DEPENDS="bitcoin"
+TERMUX_PKG_CONFFILES="var/service/lnd/run var/service/lnd/log/run"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_extract_package() { #modded without stripping
