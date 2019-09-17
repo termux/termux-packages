7839a796275822370529b2217d070b035db83bd5
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-17 21:14:06 +0200

    libwebsockets: Update from 3.2.0 to 3.2.99.1

--- packages/libwebsockets/build.sh
+++ packages/libwebsockets/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libwebsockets.org
 TERMUX_PKG_DESCRIPTION="Lightweight C websockets library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=3.2.0
-TERMUX_PKG_SHA256=5e731c536a20d9c03ae611631db073f05cd77bf0906a8c30d2a13638d4c8c667
-TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_VERSION=3.2.99.1
+TERMUX_PKG_SHA256=c0964b19cdd0311834352235216226298685b3bdf80b337c63db53fca686c284
+TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/216abea32c4f281632c2e45800acc26c1483c4a0.zip
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BREAKS="libwebsockets-dev"
 TERMUX_PKG_REPLACES="libwebsockets-dev"
