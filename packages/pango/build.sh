6945765d5e98e654b2fcf2694cf922b4f192b89f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-18 17:19:36 +0300

    pango: update to 1.44.6

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.44.5
+TERMUX_PKG_VERSION=1.44.6
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8527dfcbeedb4390149b6f94620c0fa64e26046ab85042c2a7556438847d7fc1
+TERMUX_PKG_SHA256=3e1e41ba838737e200611ff001e3b304c2ca4cdbba63d200a20db0b0ddc0f86c
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false"
 TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo, libffi"
 TERMUX_PKG_BREAKS="pango-dev"
