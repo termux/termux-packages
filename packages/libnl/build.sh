42a50e1dadd7ab63326d22f74fa4fb2449769edf
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-18 17:16:49 +0300

    libnl: update to 3.5.0

--- packages/libnl/build.sh
+++ packages/libnl/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/thom311/libnl
 TERMUX_PKG_DESCRIPTION="Collection of libraries providing APIs to netlink protocol based Linux kernel interfaces"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.4.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=b7287637ae71c6db6f89e1422c995f0407ff2fe50cecd61a312b6a9b0921f5bf
+TERMUX_PKG_VERSION=3.5.0
+TERMUX_PKG_SRCURL=https://github.com/thom311/libnl/releases/download/libnl${TERMUX_PKG_VERSION//./_}/libnl-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=352133ec9545da76f77e70ccb48c9d7e5324d67f6474744647a7ed382b5e05fa
 TERMUX_PKG_BREAKS="libnl-dev"
 TERMUX_PKG_REPLACES="libnl-dev"
-TERMUX_PKG_SRCURL=https://github.com/thom311/libnl/releases/download/libnl${TERMUX_PKG_VERSION//./_}/libnl-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pthreads --disable-cli"
 
 termux_step_pre_configure() {
