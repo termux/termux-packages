b98e4f23a09f1337b87d198b3d3a90fc358f4548
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-18 17:19:57 +0300

    parallel: update to 20190822

--- packages/parallel/build.sh
+++ packages/parallel/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/parallel/
 TERMUX_PKG_DESCRIPTION="GNU Parallel is a shell tool for executing jobs in parallel using one or more machines"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=20190722
+TERMUX_PKG_VERSION=20190822
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/parallel/parallel-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=0ed0863184dbb396b4c030848e754b1ea76329c06ae9e43314bc0915eb6cbca7
+TERMUX_PKG_SHA256=77eaf5b58830c23e9607ced9e8a916ac4e17f40cfc86f224289df1e6505023d6
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
