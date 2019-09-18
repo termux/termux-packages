c9522ba1939eba36744050d4a3fd3e4092803d4c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-18 21:27:04 +0300

    libx11: do not disable xthreads

--- x11-packages/libx11/build.sh
+++ x11-packages/libx11/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="X11 client-side library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.6.8
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=b289a845c189e251e0e884cc0f9269bbe97c238df3741e854ec4c17c21e473d5
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb, x11-repo (>= 1.3)"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros, xtrans"
 TERMUX_PKG_RECOMMENDS="xorg-xauth"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-xthreads --enable-malloc0returnsnull"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
 
 termux_step_post_make_install() {
 	ln -sfr "${TERMUX_PREFIX}/lib/libX11.so" "${TERMUX_PREFIX}/lib/libX11.so.6"
