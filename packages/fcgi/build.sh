2da0913d9f16eea230dd9b34cd9252394b52c84c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-18 17:36:58 +0300

    fcgi: update to 2.4.2
    %ci:reset-backlog

--- packages/fcgi/build.sh
+++ packages/fcgi/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.fastcgi.com/
 TERMUX_PKG_DESCRIPTION="A language independent, high performant extension to CGI"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TERMS"
-TERMUX_PKG_VERSION=2.4.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/packages/fcgi/fcgi-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=66fc45c6b36a21bf2fbbb68e90f780cc21a9da1fffbae75e76d2b4402d3f05b9
+TERMUX_PKG_VERSION=2.4.2
+TERMUX_PKG_SRCURL=https://github.com/FastCGI-Archives/fcgi2/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1fe83501edfc3a7ec96bb1e69db3fd5ea1730135bd73ab152186fd0b437013bc
 TERMUX_PKG_BREAKS="fcgi-dev"
 TERMUX_PKG_REPLACES="fcgi-dev"
 
