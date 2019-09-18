c91b61587cc3d484660cad7b77814a621c08cda8
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-18 15:31:02 +0300

    datamash: update to 1.5

--- packages/datamash/build.sh
+++ packages/datamash/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/datamash/
 TERMUX_PKG_DESCRIPTION="GNU datamash performs basic numeric,textual and statistical operations"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.4
-TERMUX_PKG_SHA256=fa44dd2d5456bcb94ef49dfc6cfe62c83fd53ac435119a85d34e6812f6e6472a
+TERMUX_PKG_VERSION=1.5
+TERMUX_PKG_SHA256=226249d5fe54024f96404798778f45963a3041714229d4225cd5d9acdaba21ad
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/datamash/datamash-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 
