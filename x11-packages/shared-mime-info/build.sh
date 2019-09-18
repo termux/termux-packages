8f854ce22e619dcd94cec4304c9fc2604611645b
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-18 20:02:11 +0300

    shared-mime-info: update to 1.13.1

--- x11-packages/shared-mime-info/build.sh
+++ x11-packages/shared-mime-info/build.sh
@@ -2,18 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://freedesktop.org/Software/shared-mime-info
 TERMUX_PKG_DESCRIPTION="Freedesktop.org Shared MIME Info"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.12
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/shared-mime-info/uploads/80c7f1afbcad2769f38aeb9ba6317a51/shared-mime-info-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=18b2f0fe07ed0d6f81951a5fd5ece44de9c8aeb4dc5bb20d4f595f6cc6bd403e
+TERMUX_PKG_VERSION=1.13.1
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/Release-${TERMUX_PKG_VERSION//./-}/shared-mime-info-Release-${TERMUX_PKG_VERSION//./-}.tar.gz
+TERMUX_PKG_SHA256=faf73a596aeef2370cdc15f95d7eece038004b5206a438cd9186a9f8d76c19ad
 TERMUX_PKG_DEPENDS="coreutils, glib, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-update-mimedb ac_cv_func_fdatasync=no"
+
+termux_step_pre_configure() {
+	NOCONFIGURE=1 bash ./autogen.sh
+}
 
 termux_step_post_make_install() {
 	# fix location of shared-mime-info.pc
 	if [ -f "${TERMUX_PREFIX}/share/pkgconfig/shared-mime-info.pc" ]; then
-		mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig" || exit 1
-		mv -f "${TERMUX_PREFIX}/share/pkgconfig/shared-mime-info.pc" "${TERMUX_PREFIX}/lib/pkgconfig/shared-mime-info.pc" || exit 1
+		mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig"
+		mv -f "${TERMUX_PREFIX}/share/pkgconfig/shared-mime-info.pc" "${TERMUX_PREFIX}/lib/pkgconfig/shared-mime-info.pc"
 	fi
 }
 
