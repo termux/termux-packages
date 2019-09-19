39d4a088b9bcd3c2542cb494f9ef90be55ecb864
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-20 00:52:24 +0300

    liblua52: set prefix from @TERMUX_PREFIX@ in lua.pc

--- packages/liblua52/build.sh
+++ packages/liblua52/build.sh
@@ -12,9 +12,9 @@ TERMUX_PKG_BUILD_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
-	sed \
-		"s/%VER%/${TERMUX_PKG_VERSION%.*}/g;s/%REL%/${TERMUX_PKG_VERSION}/g" \
-		"$TERMUX_PKG_BUILDER_DIR"/lua.pc > lua.pc
+	sed -e "s/%VER%/${TERMUX_PKG_VERSION%.*}/g;s/%REL%/${TERMUX_PKG_VERSION}/g" \
+		-e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" \
+		"$TERMUX_PKG_BUILDER_DIR"/lua.pc.in > lua.pc
 }
 
 termux_step_make() {
@@ -35,7 +35,7 @@ termux_step_make_install() {
 		INSTALL_INC="$TERMUX_PREFIX/include/lua5.2" \
 		INSTALL_MAN="$TERMUX_PREFIX/share/man/man1" \
 		install
-	install -Dm644 lua.pc "$TERMUX_PREFIX"/lib/pkgconfig/lua52.pc
+	install -Dm600 lua.pc "$TERMUX_PREFIX"/lib/pkgconfig/lua52.pc
 
 	mv -f "$TERMUX_PREFIX"/share/man/man1/lua.1 "$TERMUX_PREFIX"/share/man/man1/lua5.2.1
 	mv -f "$TERMUX_PREFIX"/share/man/man1/luac.1 "$TERMUX_PREFIX"/share/man/man1/luac5.2.1
