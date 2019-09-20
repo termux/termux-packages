8c3403df45e6a657a0b074a1c22fad79189c67d1
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-20 16:30:33 +0300

    gptfdisk: depend on ncurses

--- root-packages/gptfdisk/build.sh
+++ root-packages/gptfdisk/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.0.4
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$TERMUX_PKG_VERSION/gptfdisk-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b663391a6876f19a3cd901d862423a16e2b5ceaa2f4a3b9bb681e64b9c7ba78d
-TERMUX_PKG_DEPENDS="libpopt, libuuid"
+TERMUX_PKG_DEPENDS="libpopt, libuuid, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
