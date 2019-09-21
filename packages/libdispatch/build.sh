1fc79849b573ff12bb2fad2bcf724779356b7efc
Butta <repo@butta.fastem.com>
2019-09-21 23:21:56 +0530

    libdispatch: switch from snapshot to latest 5.1 release

--- packages/libdispatch/build.sh
+++ packages/libdispatch/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/apple/swift-corelibs-libdspatch
 TERMUX_PKG_DESCRIPTION="The libdispatch project, for concurrency on multicore hardware"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=2019-09-04
-TERMUX_PKG_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-DEVELOPMENT-SNAPSHOT-${TERMUX_PKG_VERSION}-a.tar.gz
-TERMUX_PKG_SHA256=fada9fc9f4c6fac1c8b11d4187deb67329744f412df656d1b77122765db06147
+TERMUX_PKG_VERSION=5.1
+TERMUX_PKG_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${TERMUX_PKG_VERSION}-RELEASE.tar.gz
+TERMUX_PKG_SHA256=da24d299eecc10e7d4b40a24e0700ca2f73da622795ecf6f4a5da4d33c486662
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DENABLE_TESTING=OFF"
