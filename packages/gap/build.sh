a258c940fefcc6f3dc5ef67d70d7a7a60edb412b
Henrik Grimler <henrik@grimler.se>
2019-09-22 21:07:58 +0200

    gap: update to 4.10.2

--- packages/gap/build.sh
+++ packages/gap/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gap-system.org/
 TERMUX_PKG_DESCRIPTION="GAP is a system for computational discrete algebra, with particular emphasis on Computational Group Theory"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=4.10.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=314b8d949c0bb01ef8bee199b29571d42714df660aba0d76ded254f58295904f
+TERMUX_PKG_VERSION=4.10.2
+TERMUX_PKG_SHA256=11175bed0234101643d510d4ab94c44db30bd303bfe63d86b9b86ae67cff9e49
 TERMUX_PKG_SRCURL=https://www.gap-system.org/pub/gap/gap-${TERMUX_PKG_VERSION:0:4}/tar.gz/gap-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="readline, libgmp, zlib"
 TERMUX_PKG_BREAKS="gap-dev"
