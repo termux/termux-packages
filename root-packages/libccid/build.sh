8e195be71f9de930a6af2dc9e7b207d849d2b058
Henrik Grimler <henrik@grimler.se>
2019-09-22 21:55:35 +0200

    libccid: update to 1.4.31

--- root-packages/libccid/build.sh
+++ root-packages/libccid/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://ccid.apdu.fr/
 TERMUX_PKG_DESCRIPTION="A generic USB CCID (Chip/Smart Card Interface Devices) driver and ICCD (Integrated Circuit(s) Card Devices)."
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=1.4.30
+TERMUX_PKG_VERSION=1.4.31
 TERMUX_PKG_SRCURL=https://ccid.apdu.fr/files/ccid-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ac17087be08880a0cdf99a8a2799a4ef004dc6ffa08b4d9b0ad995f39a53ff7c
+TERMUX_PKG_SHA256=6b48d7b6e4390e038d25630f8664fe81618ab00f232d6efbe0e3cc6df28ce8f7
 TERMUX_PKG_DEPENDS="libpcsclite, libusb, flex"
 
 termux_step_pre_configure() {
