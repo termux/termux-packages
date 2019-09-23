f1ce48aae3ef53ad2795a9e33ccdd5fd1a7abe9f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-09-23 19:40:05 +0300

    new package: plzip
    Closes https://github.com/termux/termux-packages/issues/4108.

---
+++ packages/plzip/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/lzip/plzip.html
+TERMUX_PKG_DESCRIPTION="A massively parallel lossless data compressor based on the lzlib compression library"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.8
+TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/plzip/plzip-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=edafae3c15142ac0ebd84c2231ff81da4f68db58359a737e750f2780686c3612
+TERMUX_PKG_DEPENDS="lzlib"
