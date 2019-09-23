e37e415af3b83b9e66a61c3c44e4d5f79ac6b7aa
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-23 18:22:28 +0300

    squid: bump revision

--- packages/squid/build.sh
+++ packages/squid/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Full-featured Web proxy cache server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_VERSION=4.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://squid.mirror.globo.tech/archive/4/squid-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=78cdb324d93341d36d09d5f791060f6e8aaa5ff3179f7c949cd910d023a86210
 TERMUX_PKG_DEPENDS="libc++, libcrypt, libxml2, libltdl, openssl, resolv-conf"
