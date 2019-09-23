f21a208de585ced0f5f65c6509855b98c66b4ded
Quasic <qbasic010@aim.com>
2019-09-23 14:53:42 -0400

    Sox: add pulseaudio as a dependency
    As in #3364, Sox seems to require pulseaudio as well as libpulseaudio (a dependency of pulseaudio) to play audio. If pulseaudio is not installed, I get errors, instead.

--- packages/sox/build.sh
+++ packages/sox/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_VERSION=14.4.2
 TERMUX_PKG_REVISION=13
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/sox/sox/${TERMUX_PKG_VERSION}/sox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=81a6956d4330e75b5827316e44ae381e6f1e8928003c6aa45896da9041ea149c
-TERMUX_PKG_DEPENDS="file, libpng, libmp3lame, libvorbis, libandroid-glob, libflac, libid3tag, libmad, libpulseaudio, opusfile, libsndfile, zlib"
+TERMUX_PKG_DEPENDS="file, libpng, libmp3lame, libvorbis, libandroid-glob, libflac, libid3tag, libmad, pulseaudio, opusfile, libsndfile, zlib"
 TERMUX_PKG_BREAKS="sox-dev"
 TERMUX_PKG_REPLACES="sox-dev"
 
