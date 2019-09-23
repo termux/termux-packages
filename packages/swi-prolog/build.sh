605d8f4fddee754b2890d68437d0a5b7d00066d1
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-23 18:19:58 +0300

    Revert "swi-prolog: install documentation"
    This reverts commit 12d8fc27ff7ef6545119369e26e596dd4ada3926.

--- packages/swi-prolog/build.sh
+++ packages/swi-prolog/build.sh
@@ -2,7 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://swi-prolog.org/
 TERMUX_PKG_DESCRIPTION="Most popular and complete prolog implementation"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_VERSION=8.1.14
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://www.swi-prolog.org/download/devel/src/swipl-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1947453d674711dbf2a0ab50d88880f6c11bc41fbcecd8b4f0f5e84526b5c833
 TERMUX_PKG_DEPENDS="libarchive, libcrypt, libgmp, libjpeg-turbo, libyaml, ncurses, ncurses-ui-libs, pcre, readline, ossp-uuid, zlib"
@@ -10,7 +9,7 @@ TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_HOSTBUILD=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DINSTALL_DOCUMENTATION=ON
+-DINSTALL_DOCUMENTATION=OFF
 -DUSE_GMP=ON
 -DSWIPL_NATIVE_FRIEND=${TERMUX_PKG_HOSTBUILD_DIR}
 -DPOSIX_SHELL=${TERMUX_PREFIX}/bin/sh
