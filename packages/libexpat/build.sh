bf30f56d1219d25e5af60f058acc873b7edff375
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-28 15:45:04 +0300

    libexpat: update to 2.2.9

--- packages/libexpat/build.sh
+++ packages/libexpat/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libexpat.github.io/
 TERMUX_PKG_DESCRIPTION="XML parsing C library"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=2.2.8
-TERMUX_PKG_SHA256=9a130948b05a82da34e4171d5f5ae5d321d9630277af02c8fa51e431f6475102
+TERMUX_PKG_VERSION=2.2.9
+TERMUX_PKG_SRCURL=https://github.com/libexpat/libexpat/releases/download/R_${TERMUX_PKG_VERSION//./_}/expat-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=f1063084dc4302a427dabcca499c8312b3a32a29b7d2506653ecc8f950a9a237
 TERMUX_PKG_BREAKS="libexpat-dev"
 TERMUX_PKG_REPLACES="libexpat-dev"
-TERMUX_PKG_SRCURL=https://github.com/libexpat/libexpat/releases/download/R_${TERMUX_PKG_VERSION//./_}/expat-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-xmlwf --without-docbook"
