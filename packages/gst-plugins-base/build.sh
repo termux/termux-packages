7e069b2305584ca8c40290adf56e29b34fc9106f
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-28 16:53:19 +0300

    gst-plugins-base: update to 1.16.1

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.16.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.16.1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4093aa7b51e28fb24dfd603893fead8d1b7782f088b05ed0f22a21ef176fb5ae
+TERMUX_PKG_SHA256=5c3cc489933d0597087c9bc6ba251c93693d64554bcc563539a084fa2d5fcb2b
 TERMUX_PKG_DEPENDS="gstreamer, libjpeg-turbo, libopus, libpng, libvorbis, zlib"
 TERMUX_PKG_BREAKS="gst-plugins-base-dev"
 TERMUX_PKG_REPLACES="gst-plugins-base-dev"
