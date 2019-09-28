a21137f62343a775ced531d9066c3701130d785b
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-28 14:46:26 +0300

    assimp: update to 5.0.0

--- packages/assimp/build.sh
+++ packages/assimp/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://assimp.sourceforge.net/index.html
 TERMUX_PKG_DESCRIPTION="Library to import various well-known 3D model formats in an uniform manner"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=4.1.0
+TERMUX_PKG_VERSION=5.0.0
 TERMUX_PKG_SRCURL=https://github.com/assimp/assimp/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=3520b1e9793b93a2ca3b797199e16f40d61762617e072f2d525fad70f9678a71
+TERMUX_PKG_SHA256=b0110a91650d6bb4000e3d5c2185bf77b0ff0a2e7a284bc2c4af81b33988b63c
 TERMUX_PKG_DEPENDS="boost, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DASSIMP_BUILD_SAMPLES=OFF"
