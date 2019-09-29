72c098ef098951aac7deaa602cd02bba9a482378
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-29 03:03:40 +0200

    golang: Update from 1.13 to 1.13.1

--- scripts/build/setup/termux_setup_golang.sh
+++ scripts/build/setup/termux_setup_golang.sh
@@ -20,7 +20,7 @@ termux_setup_golang() {
 	fi
 
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		local TERMUX_GO_VERSION=go1.13
+		local TERMUX_GO_VERSION=go1.13.1
 		local TERMUX_GO_PLATFORM=linux-amd64
 
 		local TERMUX_BUILDGO_FOLDER=$TERMUX_COMMON_CACHEDIR/${TERMUX_GO_VERSION}
@@ -33,7 +33,7 @@ termux_setup_golang() {
 		rm -Rf "$TERMUX_COMMON_CACHEDIR/go" "$TERMUX_BUILDGO_FOLDER"
 		termux_download https://storage.googleapis.com/golang/${TERMUX_GO_VERSION}.${TERMUX_GO_PLATFORM}.tar.gz \
 			"$TERMUX_BUILDGO_TAR" \
-			68a2297eb099d1a76097905a2ce334e3155004ec08cdea85f24527be3c48e856
+			94f874037b82ea5353f4061e543681a0e79657f787437974214629af8407d124
 
 		( cd "$TERMUX_COMMON_CACHEDIR"; tar xf "$TERMUX_BUILDGO_TAR"; mv go "$TERMUX_BUILDGO_FOLDER"; rm "$TERMUX_BUILDGO_TAR" )
 	else
