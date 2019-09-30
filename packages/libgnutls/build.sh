ff3fe74c8e3de663b65f5599e11b4abfeebd6768
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-09-30 19:52:48 +0300

    reset revision after update

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -2,7 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=3.6.10
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnutls/v${TERMUX_PKG_VERSION:0:3}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b1f3ca67673b05b746a961acf2243eaae0ffe658b6a6494265c648e7c7812293
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn2, libunistring"
