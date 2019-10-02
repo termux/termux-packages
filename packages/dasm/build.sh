df7ea634ea2f3e356e1dad6dd4f4c3b383740a90
araczkowski <araczkowski@gmail.com>
2019-10-02 11:19:37 +0200

    dasm moved from sourceforge to github

--- packages/dasm/build.sh
+++ packages/dasm/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://dasm-dillon.sourceforge.io/
 TERMUX_PKG_DESCRIPTION="Macro assembler with support for several 8-bit microprocessors"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.20.11
-TERMUX_PKG_SHA256=a9330adae534aeffbfdb8b3ba838322b92e1e0bb24f24f05b0ffb0a656312f36
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dasm-dillon/dasm-dillon/${TERMUX_PKG_VERSION}/dasm-${TERMUX_PKG_VERSION}-2014.03.04-source.tar.gz
+TERMUX_PKG_SHA256=c69bbe192159dcf75677ca13bba65c3318dc443f2df45fccd3c060b2e092c7f5
+TERMUX_PKG_SRCURL=https://github.com/dasm-assembler/dasm/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
