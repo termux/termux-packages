d77ce526f7ad99449ee8b2c8b6f00ec19922f661
Fredrik Fornwall <fredrik@fornwall.net>
2019-10-06 10:18:57 +0200

    texinfo: Update from 6.6 to 6.7

--- packages/texinfo/build.sh
+++ packages/texinfo/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/texinfo/
 TERMUX_PKG_DESCRIPTION="Documentation system for on-line information and printed output"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=6.6
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=9bb9ca00da53f26a7e5725eee49689cd4a1e18d25d5b061ac8b2053018d93d66
+TERMUX_PKG_VERSION=6.7
+TERMUX_PKG_SHA256=988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/texinfo/texinfo-${TERMUX_PKG_VERSION}.tar.xz
 # gawk is used by texindex:
 TERMUX_PKG_DEPENDS="libiconv, ncurses, perl, gawk"
