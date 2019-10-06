b15dada26179cdf7fec4a730b5bb2277ceeec2b6
Fredrik Fornwall <fredrik@fornwall.net>
2019-10-06 18:37:46 +0200

    tintin++: Update from 2.01.90 to 2.01.91

--- packages/tintin++/build.sh
+++ packages/tintin++/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://tintin.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Classic text-based MUD client"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.01.90
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/tintin/TinTin%2B%2B%20Source%20Code/${TERMUX_PKG_VERSION:0:4}/tintin-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6b3eef2a993250d7094c5fcd4aa6ea3e2356228b006c70062f5757577c86936c
+TERMUX_PKG_VERSION=2.01.91
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/tintin/files/tintin-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=3596a6d540d58162dc60c318c520cb94ee52fb438b5db8a2fa2513c47fbe6359
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_file__dev_ptmx=no"
 TERMUX_PKG_DEPENDS="pcre, libgnutls, libutil, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
