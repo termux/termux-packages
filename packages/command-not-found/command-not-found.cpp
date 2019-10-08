6a9370f4d401ee3a8d27a97ee8ac1e5e4b19dd21
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-10-08 14:35:12 +0300

    add mem allocation check where needed

--- packages/command-not-found/command-not-found.cpp
+++ packages/command-not-found/command-not-found.cpp
@@ -116,6 +116,12 @@ int termux_levenshtein_distance(char const* s1, char const* s2) {
   }
   for (x = 1; x <= s2len; x++) {
     matrix[x] = (int *) malloc(sizeof *matrix[x] * (s1len+1));
+
+    if (!matrix[x]) {
+      cerr << "Memory allocation seem to have failed" << endl;
+      return -4;
+    }
+
     matrix[x][0] = matrix[x-1][0] + 1;
   }
   for (y = 1; y <= s1len; y++) {
