1551a285ec77fc6e147395021a3650a71eb34c0d
Henrik Grimler <henrik@grimler.se>
2019-10-08 23:24:47 +0200

    command-not-found: use sources in github.com/termux/command-not-found
    Updated sources support multiple repos

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,12 +1,6 @@
-TERMUX_PKG_HOMEPAGE=https://termux.com
+TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.39
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
-
-termux_step_make_install() {
-	TERMUX_LIBEXEC_DIR=$TERMUX_PREFIX/libexec/termux
-	mkdir -p $TERMUX_LIBEXEC_DIR
-	$CC -Wall -Wextra -Werror -pedantic $CFLAGS $LDFLAGS -std=c11 $TERMUX_PKG_BUILDER_DIR/command-not-found.c \
-		-o $TERMUX_LIBEXEC_DIR/command-not-found
-}
+TERMUX_PKG_VERSION=1.42
+TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=eb3b5c23bb574f7e4c5c78d9e97fb54af2fbe98a589efe6eb629287089e5efc2
