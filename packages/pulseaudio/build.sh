b72f9358c1878cd3dc3d9ed4a109f3c4fe11d099
Tom Yan <tom.ty89@gmail.com>
2019-10-08 09:27:36 +0800

    pulseaudio: declare conf files

--- packages/pulseaudio/build.sh
+++ packages/pulseaudio/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/PulseAudio
 TERMUX_PKG_DESCRIPTION="A featureful, general-purpose sound server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=12.2
-TERMUX_PKG_REVISION=19
+TERMUX_PKG_REVISION=20
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/pulseaudio/releases/pulseaudio-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=809668ffc296043779c984f53461c2b3987a45b7a25eb2f0a1d11d9f23ba4055
 TERMUX_PKG_DEPENDS="libltdl, libsndfile, libandroid-glob, libsoxr, speexdsp"
@@ -23,6 +23,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-neon-opt
 --bindir=$TERMUX_PREFIX/libexec
 ax_cv_PTHREAD_PRIO_INHERIT=no
 ac_cv_func_posix_madvise=no"
+TERMUX_PKG_CONFFILES="etc/pulse/client.conf etc/pulse/daemon.conf etc/pulse/default.pa etc/pulse/system.pa"
 
 termux_step_pre_configure() {
 	# Avoid aclocal-1.15 dependency:
