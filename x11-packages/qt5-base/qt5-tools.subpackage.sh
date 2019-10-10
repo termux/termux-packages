b2c2d707a2366145b8015457e22b9e1da7501a83
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-10 18:12:45 +0300

    qt5-base: no develsplit

--- x11-packages/qt5-base/qt5-tools.subpackage.sh
+++ x11-packages/qt5-base/qt5-tools.subpackage.sh
@@ -1,5 +1,7 @@
 TERMUX_SUBPKG_DESCRIPTION="Qt development tools (incomplete !)"
 TERMUX_SUBPKG_DEPENDS="qt5-base"
+TERMUX_SUBPKG_BREAKS="qt5-tools-dev"
+TERMUX_SUBPKG_REPLACES="qt5-tools-dev"
 
 TERMUX_SUBPKG_INCLUDE="
 bin/lrelease
@@ -9,8 +11,25 @@ bin/qtattributionsscanner
 bin/qtplugininfo
 bin/qtpaths
 bin/qtdiag
+include/qt/QtHelp
+include/qt/QtUiPlugin
+include/qt/QtUiTools
+lib/cmake/Qt5Help
+lib/cmake/Qt5UiPlugin
+lib/cmake/Qt5UiTools
+lib/cmake/Qt5LinguistTools
+lib/libQt5Help.prl
 lib/libQt5Help.so
 lib/libQt5Help.so.5
 lib/libQt5Help.so.5.11
 lib/libQt5Help.so.5.11.2
+lib/libQt5UiTools.prl
+lib/libQt5UiTools.a
+lib/pkgconfig/Qt5Help.pc
+lib/pkgconfig/Qt5UiTools.pc
+lib/qt/mkspecs/modules/qt_lib_help.pri
+lib/qt/mkspecs/modules/qt_lib_help_private.pri
+lib/qt/mkspecs/modules/qt_lib_uiplugin.pri
+lib/qt/mkspecs/modules/qt_lib_uitools.pri
+lib/qt/mkspecs/modules/qt_lib_uitools_private.pri
 "
--- x11-packages/qt5-base/qt5-x11extras.subpackage.sh
+++ x11-packages/qt5-base/qt5-x11extras.subpackage.sh
@@ -1,9 +1,17 @@
 TERMUX_SUBPKG_DESCRIPTION="Qt platform-specific APIs for X11"
 TERMUX_SUBPKG_DEPENDS="qt5-base"
+TERMUX_PKG_BREAKS="qt5-x11extras-dev"
+TERMUX_PKG_REPLACES="qt5-x11extras-dev"
 
 TERMUX_SUBPKG_INCLUDE="
+include/qt/QtX11Extras
+lib/cmake/Qt5X11Extras
+lib/libQt5X11Extras.prl
 lib/libQt5X11Extras.so
 lib/libQt5X11Extras.so.5
 lib/libQt5X11Extras.so.5.11
 lib/libQt5X11Extras.so.5.11.2
+lib/pkgconfig/Qt5X11Extras.pc
+lib/qt/mkspecs/modules/qt_lib_x11extras.pri
+lib/qt/mkspecs/modules/qt_lib_x11extras_private.pri
 "
