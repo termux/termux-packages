cbdf6bfcde4f93ae168bb9abbb535c36bb680357
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-13 22:03:27 +0300

    new package: fvwm
    Requested in https://github.com/termux/x11-packages/issues/104.

---
+++ x11-packages/fvwm/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=http://www.fvwm.org/
+TERMUX_PKG_DESCRIPTION="A multiple large virtual desktop window manager originally derived from twm."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=2.6.9
+TERMUX_PKG_SRCURL=https://github.com/fvwmorg/fvwm/releases/download/2.6.9/fvwm-2.6.9.tar.gz
+TERMUX_PKG_SHA256=1bc64cf3ccd0073008758168327a8265b8059def9b239b451d6b9fab2cc391ae
+TERMUX_PKG_DEPENDS="fribidi, librsvg, libxcursor, libxinerama, libxpm, imlib2, libandroid-shmem"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_setpgrp_void=yes ac_cv_func_mkstemp=no ac_cv_func_getpwuid=no"
+
+termux_step_pre_configure() {
+	export LIBS="-landroid-shmem"
+}
