4ad00c128790e9aca0fc486bc11536a3d02a32bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-15 14:21:35 +0300

    fakeroot: fix postinst script

--- packages/fakeroot/build.sh
+++ packages/fakeroot/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tool for simulating superuser privileges (with tcp ipc)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.24
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_${TERMUX_PKG_VERSION}.orig.tar.gz
 TERMUX_PKG_SHA256=2e045b3160370b8ab4d44d1f8d267e5d1d555f1bb522d650e7167b09477266ed
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ipc=tcp"
@@ -17,10 +17,10 @@ termux_step_create_debscripts() {
 	{
 		echo "#!$TERMUX_PREFIX/bin/sh"
 		echo "echo"
-		echo "echo 'Fakeroot doesn't give you any real root permissions. Utility is primarily intended to be used for development purposes.'"
-		echo "echo 'More info about usage at https://wiki.debian.org/FakeRoot.'"
+		echo "echo Fakeroot doesn't give you any real root permissions. Utility is primarily intended to be used for development purposes."
+		echo "echo More info about usage at https://wiki.debian.org/FakeRoot."
 		echo "echo"
-		echo "echo 'Programs requiring real root permissions will not run under fakeroot. Do not post bug reports about this.'"
+		echo "echo Programs requiring real root permissions will not run under fakeroot. Do not post bug reports about this."
 		echo "echo"
 	} > ./postinst
 }
