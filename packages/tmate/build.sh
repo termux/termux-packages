cd70ee10a9acc1c720daee19c5b3f6eb1c884274
Fredrik Fornwall <fredrik@fornwall.net>
2019-10-17 09:51:15 +0200

    tmate: Update from 2.3.0 to 2.3.1

--- packages/tmate/build.sh
+++ packages/tmate/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://tmate.io
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with instant terminal sharing"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=2.3.0
+TERMUX_PKG_VERSION=2.3.1
 TERMUX_PKG_SRCURL=https://github.com/tmate-io/tmate/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f837514edbc19180d06b27713628466e68aba91654d748d6e65bfad308f5b00a
+TERMUX_PKG_SHA256=21cb6029d09e3809e37b9b8f1cd96b452197b8c2e28d3551d674b8e580bf4048
 TERMUX_PKG_DEPENDS="libandroid-support, libevent, libmsgpack, libssh, libutil, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
 
