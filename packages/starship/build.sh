2bbbe67801c39b64eaee659f84a31b0b17f2ca34
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-10-18 10:57:21 +0000

    starship: update to 0.25.0
    $ci:reset-backlog

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://starship.rs
 TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=0.20.2
+TERMUX_PKG_VERSION=0.25.0
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=7d69b44db4f8fb31f92c0f9766598496156c8ede8f44ee0681436129aefdfc3c
+TERMUX_PKG_SHA256=623d545318fc430f443665aff79921df9a2077c40e2f74b5870bb56772c9eebd
 TERMUX_PKG_DEPENDS="openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
