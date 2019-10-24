821205db22abe1c59a44377d199bffdb328b8251
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-24 20:17:18 +0300

    dictd: add zlib to dependencies

--- packages/dictd/build.sh
+++ packages/dictd/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.12.1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dict/dictd/dictd-${TERMUX_PKG_VERSION}/dictd-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a237f6ecdc854ab10de5145ed42eaa2d9b6d51ffdc495f7daee59b05cc363656
-TERMUX_PKG_DEPENDS="libmaa"
+TERMUX_PKG_DEPENDS="libmaa, zlib"
