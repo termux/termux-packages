c528c9a65e3256d449e265ffd336138a9df71d42
Henrik Grimler <henrik@grimler.se>
2019-10-24 21:56:57 +0200

    glulxe: Clean build script and update to 0.5.4 for real

--- packages/glulxe/build.sh
+++ packages/glulxe/build.sh
@@ -1,29 +1,20 @@
-TERMUX_PKG_HOMEPAGE=http://www.eblong.com/zarf/glulx/
+TERMUX_PKG_HOMEPAGE=https://www.eblong.com/zarf/glulx/
 TERMUX_PKG_DESCRIPTION="Interpreter for the Glulx portable VM for interactive fiction (IF) games"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.5.4
-TERMUX_PKG_SRCURL=http://www.eblong.com/zarf/glulx/glulxe-051.tar.gz
-TERMUX_PKG_SHA256=33c563bdbd0fdbae625e1a2441e9a6f40f1491f1cdc2a197bbd6cf2c32c3830d
-TERMUX_PKG_BUILD_IN_SRC="yes"
+TERMUX_PKG_VERSION=(0.5.4
+                    1.0.4)
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=(https://www.eblong.com/zarf/glulx/glulxe-${TERMUX_PKG_VERSION[0]//.}.tar.gz
+                   https://www.eblong.com/zarf/glk/glktermw-${TERMUX_PKG_VERSION[1]//.}.tar.gz)
+TERMUX_PKG_SHA256=(1fc26f8aa31c880dbc7c396ede196c5d2cdff9bdefc6b192f320a96c5ef3376e
+                   5968630b45e2fd53de48424559e3579db0537c460f4dc2631f258e1c116eb4ea)
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="ncurses"
 
-termux_step_configure () {
-	termux_download http://eblong.com/zarf/glk/glktermw-104.tar.gz \
-		$TERMUX_PKG_CACHEDIR/glktermw-104.tar.gz \
-		5968630b45e2fd53de48424559e3579db0537c460f4dc2631f258e1c116eb4ea
-	tar xf $TERMUX_PKG_CACHEDIR/glktermw-104.tar.gz
-}
-
-termux_step_make () {
-	cd $TERMUX_PKG_SRCDIR/glkterm
-	patch -p1 < $TERMUX_PKG_BUILDER_DIR/glkterm.patch.special
-	CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS" PREFIX=$TERMUX_PREFIX make -j 1
-
-	cd ..
-	make
-	cp glulxe $TERMUX_PREFIX/bin
+termux_step_post_configure () {
+	CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS" PREFIX=$TERMUX_PREFIX make -C glkterm
 }
 
 termux_step_make_install () {
-	echo "Do nothing..."
+	install glulxe $TERMUX_PREFIX/bin
 }
