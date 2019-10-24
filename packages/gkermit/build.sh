91c424e3a103a8f45f6cf9637c7af782826347de
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-10-24 20:26:43 +0300

    fix build.sh

--- packages/gkermit/build.sh
+++ packages/gkermit/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://www.columbia.edu/kermit/gkermit.html
 TERMUX_PKG_DESCRIPTION="Simple, Portable, Free File Transfer Software for UNIX"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.00
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=ftp://kermit.columbia.edu/kermit/archives/gku100.tar.gz
+TERMUX_PKG_SRCURL=ftp://kermit.columbia.edu/kermit/archives/gku${TERMUX_PKG_VERSION/./}.tar.gz
 TERMUX_PKG_SHA256=3dbe63291277c4795255343b48b860777fb0a160163d7e1d30b1ee68585593eb
 TERMUX_PKG_DEPENDS="libandroid-support"
-TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_MAKE_PROCESSES=1
 
 termux_step_post_extract_package() {
