edfe129cb88b9f46936ab97352903cd2a12b4db2
Henrik Grimler <henrik@grimler.se>
2019-10-25 11:46:00 +0200

    openbabel: bump to trigger ci

--- packages/openbabel/build.sh
+++ packages/openbabel/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://openbabel.org/wiki/Main_Page
 TERMUX_PKG_DESCRIPTION="Open Babel is a chemical toolbox designed to speak the many languages of chemical data"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/openbabel/openbabel/archive/openbabel-${TERMUX_PKG_VERSION//./-}.tar.gz
 TERMUX_PKG_SHA256=5c630c4145abae9bb4ab6c56a940985acb6dadf3a8c3a8073d750512c0220f30
 TERMUX_PKG_DEPENDS="libc++, libcairo, libxml2, eigen, boost"
