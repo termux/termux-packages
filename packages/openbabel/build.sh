8927f78444b1fa1ed11059dbfbb3a1b7891f3aa5
Henrik Grimler <henrik@grimler.se>
2019-10-25 11:35:33 +0200

    openbabel: update to 3.0.0

--- packages/openbabel/build.sh
+++ packages/openbabel/build.sh
@@ -1,14 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://openbabel.org/wiki/Main_Page
 TERMUX_PKG_DESCRIPTION="Open Babel is a chemical toolbox designed to speak the many languages of chemical data"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.4.1
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/openbabel/files/openbabel/${TERMUX_PKG_VERSION}/openbabel-${TERMUX_PKG_VERSION}.tar.gz/download
-TERMUX_PKG_SHA256=204136582cdfe51d792000b20202de8950218d617fd9c6e18cee36706a376dfc
-TERMUX_PKG_DEPENDS="libc++, libcairo, libxml2, eigen"
+TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_SRCURL=https://github.com/openbabel/openbabel/archive/openbabel-${TERMUX_PKG_VERSION//./-}.tar.gz
+TERMUX_PKG_SHA256=5c630c4145abae9bb4ab6c56a940985acb6dadf3a8c3a8073d750512c0220f30
+TERMUX_PKG_DEPENDS="libc++, libcairo, libxml2, eigen, boost"
 TERMUX_PKG_BREAKS="openbabel-dev"
 TERMUX_PKG_REPLACES="openbabel-dev"
-
-termux_step_pre_configure () {
-	export CXXFLAGS="$CXXFLAGS -Wno-c++11-narrowing"
-}
+# MAEPARSER gives an error related to boost's unit_test_framework during configure
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWITH_MAEPARSER=off"
