aaceda4c49dccfd02a6afec73921d255b0a3a3aa
Henrik Grimler <henrik@grimler.se>
2019-10-25 16:31:50 +0200

    openmpi: update to 4.0.2

--- packages/openmpi/build.sh
+++ packages/openmpi/build.sh
@@ -2,24 +2,24 @@ TERMUX_PKG_HOMEPAGE=https://www.open-mpi.org
 TERMUX_PKG_DESCRIPTION="The Open MPI Project is an open source Message Passing Interface implementation that is developed and maintained by a consortium of academic, research, and industry partners"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=4.0.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.0.2
 TERMUX_PKG_SRCURL=https://download.open-mpi.org/release/open-mpi/v${TERMUX_PKG_VERSION:0:3}/openmpi-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e55e213fe09a214ab9f2c722acfd8bf7b39bbc1800e4b7a464d38df15e707f59
+TERMUX_PKG_SHA256=662805870e86a1471e59739b0c34c6f9004e0c7a22db068562d5388ec4421904
+TERMUX_PKG_DEPENDS="libandroid-shmem"
 TERMUX_PKG_BREAKS="openmpi-dev"
 TERMUX_PKG_REPLACES="openmpi-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-x
-ac_cv_header_ifaddrs_h=no
 --disable-dlopen
 --disable-mpi-fortran
+ac_cv_header_ifaddrs_h=no
 ac_cv_member_struct_ifreq_ifr_hwaddr=no
 "
 
 termux_step_pre_configure () {
 	# rindex is an obsolete version of strrchr which is not available in Android:
 	CFLAGS+=" -Drindex=strrchr -Dbcmp=memcmp"
-	LDFLAGS+=" -llog"
+	LDFLAGS+=" -llog -landroid-shmem"
 	if [ $TERMUX_ARCH == "i686" ]; then
 		# fails with "undefined reference to __atomic..."
 		LDFLAGS+=" -latomic"
