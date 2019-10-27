bff7a926a6c7b70fb9d2930e7300ac35a86646ae
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-27 21:45:29 +0200

    atk: update to 2.34.1

--- x11-packages/atk/build.sh
+++ x11-packages/atk/build.sh
@@ -2,11 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gtk.org
 TERMUX_PKG_DESCRIPTION="The interface definitions of accessibility infrastructure"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.32.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.34.1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/atk/${TERMUX_PKG_VERSION:0:4}/atk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cb41feda7fe4ef0daa024471438ea0219592baf7c291347e5a858bb64e4091cc
-
+TERMUX_PKG_SHA256=d4f0e3b3d21265fcf2bc371e117da51c42ede1a71f6db1c834e6976bb20997cb
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_CONFLICTS="libatk"
 TERMUX_PKG_REPLACES="libatk"
