0b86c14d9050e82e646b5dce659a6efbdab30c4e
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-27 22:06:00 +0200

    libxklavier: update to 5.4

--- x11-packages/libxklavier/build.sh
+++ x11-packages/libxklavier/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/LibXklavier/
 TERMUX_PKG_DESCRIPTION="High-level API for X Keyboard Extension"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=5.3
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libxklavier/${TERMUX_PKG_VERSION}/libxklavier-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ebec3bc54b5652838502b96223152fb1cd8fcb14ace5cb02d718fc3276bbd404
+TERMUX_PKG_VERSION=5.4
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/archived-projects/libxklavier/-/archive/libxklavier-${TERMUX_PKG_VERSION}/libxklavier-libxklavier-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e1638599e9229e6f6267b70b02e41940b98ba29b3a37e221f6e59ff90100c3da
 TERMUX_PKG_DEPENDS="glib, iso-codes, libxi, libxkbfile, libxml2, xkeyboard-config"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-xkb-base=$TERMUX_PREFIX/share/X11/xkb"
