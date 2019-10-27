3aac060aed3ec67a938b6086935e194267d2b300
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-27 21:21:42 +0200

    libdrm: update to 2.4.100
    %ci:reset-backlog

--- x11-packages/libdrm/build.sh
+++ x11-packages/libdrm/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://dri.freedesktop.org/wiki/
 TERMUX_PKG_DESCRIPTION="Userspace interface to kernel DRM services"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.4.99
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.4.100
 TERMUX_PKG_SRCURL=https://dri.freedesktop.org/libdrm/libdrm-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4dbf539c7ed25dbb2055090b77ab87508fc46be39a9379d15fed4b5517e1da5e
+TERMUX_PKG_SHA256=c77cc828186c9ceec3e56ae202b43ee99eb932b4a87255038a80e8a1060d0a5d
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-intel
