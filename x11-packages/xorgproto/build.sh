86d71d37a4793aff9410119e1ad2652fff548d4f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-29 01:23:48 +0200

    xorgproto: remove extensions/XKBgeom.h since it is now part of libx11

--- x11-packages/xorgproto/build.sh
+++ x11-packages/xorgproto/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="X.Org X11 Protocol headers"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=2019.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/proto/xorgproto-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=46ecd0156c561d41e8aa87ce79340910cdf38373b759e737fcbba5df508e7b8e
 TERMUX_PKG_DEPENDS="xorg-util-macros"
@@ -16,6 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dlegacy=true"
 TERMUX_PKG_RM_AFTER_INSTALL="
 include/X11/extensions/apple*
 include/X11/extensions/windows*
+include/X11/extensions/XKBgeom.h
 lib/pkgconfig/applewmproto.pc
 lib/pkgconfig/windowswmproto.pc
 "
