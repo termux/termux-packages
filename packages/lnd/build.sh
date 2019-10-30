8c2aeea594736efbda8147f3f30ced10233fd79e
stefanwouldgo <stefan@sblbs.de>
2019-10-30 10:19:45 +0100

    bump lnd version to 0.8.0

--- packages/lnd/build.sh
+++ packages/lnd/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/lightningnetwork/lnd
 TERMUX_PKG_DESCRIPTION="Lightning Network Daemon"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.7.1-beta
+TERMUX_PKG_VERSION=0.8.0-beta
 TERMUX_PKG_SRCURL=(https://github.com/lightningnetwork/lnd/archive/v${TERMUX_PKG_VERSION}.tar.gz
                    https://github.com/lightningnetwork/lnd/releases/download/v${TERMUX_PKG_VERSION}/vendor.tar.gz)
-TERMUX_PKG_SHA256=(13e1ef723a1dc0fc5af9fe48ca5766417e3c7de48c87a082a8fe83a6b38bb6a4
-                   3bbfa000e2b4c7702f92d24235b5a098f37fd7b5830ca42586678f03d7cf9da3)
+TERMUX_PKG_SHA256=(ab6003ee617d86f11237334c25718ce81a279c2b4b5867a7b2e0d16e85a0d687
+                   22fd5504519ff2328bf3c4e416c2c37ce9ab111859c987c3602c66513dcf5fb6)
 TERMUX_PKG_DEPENDS="bitcoin"
 TERMUX_PKG_CONFFILES="var/service/lnd/run var/service/lnd/log/run"
 TERMUX_PKG_BUILD_IN_SRC=true
