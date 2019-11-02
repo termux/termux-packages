fe484b7332c0ca3903f7f068f0a5c84627d4089c
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-11-03 01:40:12 +0200

    glib: update to 2.62.2

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=2.60.7
+TERMUX_PKG_VERSION=2.62.2
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8b12c0af569afd3b71200556ad751bad4cf4bf7bc4b5f880638459a42ca86310
+TERMUX_PKG_SHA256=698824a413f76df039739c2a78f45b10939d526ae7495bab4e694e6730deb3f1
 # libandroid-support to get langinfo.h in include path.
 TERMUX_PKG_DEPENDS="libffi, libiconv, pcre, libandroid-support, zlib"
 TERMUX_PKG_BREAKS="glib-dev"
@@ -12,7 +12,7 @@ TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc lib/locale share/glib-2.0/gettext sha
 # Needed by pkg-config for glib-2.0:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibmount=false
--Diconv=gnu
+-Diconv=external
 "
 
 termux_step_pre_configure() {
