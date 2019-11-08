08595bace07823fbab6f22676968265462578464
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-11-08 14:05:57 +0200

    imagemagick: bump revision

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_VERSION=7.0.8.68
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.gz
 TERMUX_PKG_SHA256=c3aa886789b36d3fe1f6f99fc1a32bbbf26d4ce51f485b32c3a4b85e176a53c9
 TERMUX_PKG_DEPENDS="fftw, pango, glib, libbz2, libjpeg-turbo, liblzma, libpng, libtiff, libxml2, openjpeg, littlecms, libwebp, librsvg"
