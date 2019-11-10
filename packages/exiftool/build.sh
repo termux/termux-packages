0fe5c9dce74a0bcf2b0c758758699ed85fe4c1f2
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-10 18:30:55 +0200

    move from unstable: exiftool

---
+++ packages/exiftool/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://www.sno.phy.queensu.ca/~phil/exiftool/index.html
+TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta information in a wide variety of files."
+TERMUX_PKG_LICENSE="Artistic-License-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=11.74
+TERMUX_PKG_SRCURL="https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=93b94c03ce35201d4959da56a229874cf2204373c2c003592c2ced00f33cca55
+TERMUX_PKG_DEPENDS="perl"
+
+termux_step_make_install() {
+	# Change this after package 'perl' was upgraded.
+	local current_perl_version=5.30.0
+
+	install -Dm700 "$TERMUX_PKG_SRCDIR"/exiftool "$TERMUX_PREFIX"/bin/exiftool
+	find "$TERMUX_PKG_SRCDIR"/lib -name "*.pod" -delete
+	mkdir -p "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}"
+	rm -rf "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}"/{Image,File}
+	cp -a "$TERMUX_PKG_SRCDIR"/lib/{Image,File} "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}/"
+}
