d62c70b39242ed18abd0068739f54ab2fc46c7b6
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-11-10 18:57:25 +0200

    Revert nodejs update to 13.1.0
    Delaying update since it generates a lot of undefined reference compile-time errors.

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=13.1.0
+TERMUX_PKG_VERSION=13.0.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d42c056cbd33b35836b0f5cfb2c56712b965ea76e188ef79af492614cf14cb68
+TERMUX_PKG_SHA256=45ff3b40afc3fc93fd62e31c0f5dfa046f307f0b33d0f32e09019f306bc74767
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
