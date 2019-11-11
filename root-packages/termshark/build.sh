c52479b83038b9f1204549f79e714dcb2a0efd66
Graham Clark <grclark@gmail.com>
2019-11-11 00:54:04 -0500

    termshark: update to 2.0.2

--- root-packages/termshark/build.sh
+++ root-packages/termshark/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://termshark.io
 TERMUX_PKG_DESCRIPTION="A terminal UI for tshark, inspired by Wireshark"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=669bba0e8dd7df54ade6321a5c7d2ec20563ffd777f7b3b0394a11f88da64698
+TERMUX_PKG_VERSION=2.0.2
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SHA256=7e14a3e7b7f898282fbe90e8b783e52c03efe78c0145dcf32fb10d8e5eaff30c
 TERMUX_PKG_SRCURL=https://github.com/gcla/termshark/archive/v${TERMUX_PKG_VERSION}.tar.gz
 
 TERMUX_PKG_DEPENDS="tshark"
@@ -15,11 +15,11 @@ termux_step_make() {
 	export GO111MODULE=on
 
 	cd $TERMUX_PKG_BUILDDIR
-	go get -d -v github.com/gcla/termshark@v${TERMUX_PKG_VERSION}
-	go build -ldflags="-X github.com/gcla/termshark.Version=${TERMUX_PKG_VERSION}" github.com/gcla/termshark/cmd/termshark
+	go get -d -v github.com/gcla/termshark/v2/cmd/termshark@e185fa59d87c06fe1bafb83ce6dc15591434ccc8
+	go install github.com/gcla/termshark/v2/cmd/termshark
 }
 
 termux_step_make_install() {
-	cd $TERMUX_PKG_BUILDDIR
-	install -Dm700 termshark $TERMUX_PREFIX/bin/termshark
+        cd $TERMUX_PKG_BUILDDIR
+        install -Dm700 bin/android_arm64/termshark $TERMUX_PREFIX/bin/termshark
 }
