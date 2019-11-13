83946743d5be590365522b118438e85f6aa1e681
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-13 17:08:28 +0200

    apt: move gnupg to suggests to prevent its automatic installation during upgrade

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -2,15 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.4.9
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d4d65e7c84da86f3e6dcc933bba46a08db429c9d933b667c864f5c0e880bac0d
 # apt-key requires utilities from coreutils, findutils, gpgv, grep, sed.
 TERMUX_PKG_DEPENDS="coreutils, dpkg, findutils, gpgv, grep, libc++, libcurl, liblzma, sed, termux-licenses, zlib"
 TERMUX_PKG_CONFLICTS="apt-transport-https"
 TERMUX_PKG_REPLACES="apt-transport-https"
-TERMUX_PKG_RECOMMENDS="game-repo, gnupg, science-repo"
-TERMUX_PKG_SUGGESTS="unstable-repo, x11-repo"
+TERMUX_PKG_RECOMMENDS="game-repo, science-repo"
+TERMUX_PKG_SUGGESTS="gnupg, unstable-repo, x11-repo"
 TERMUX_PKG_ESSENTIAL=true
 
 TERMUX_PKG_CONFFILES="
