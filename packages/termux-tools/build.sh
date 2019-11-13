dc14c1294090aa0961a394761a02f2d86aab7a90
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-11-13 16:38:40 +0200

    apt: prevent usage of certain commands as root
    Usage of package manager as root has certain bad effects in Termux such as
    messed up SELinux contexts and ownership. Root checks done in 'pkg' wrapper
    are not reliable because one can execute 'apt' directly or with third-party
    script downloaded from the Internet.
    
    This commit adds user id check and if it found that uid is 0, apt will
    refuse to do any work in root session. These checks done in such way so
    they cannot be bypassed in any way unless command is executed as non-root
    user.
    
    Those who use Termux via ADB root shell should be able to switch to Termux
    user id with command 'su' in order to have package manager working.
    
    ---
    
    This change also affects the 'termux-info' utility:
    
     * It will no longer use 'apt policy' to detect subscribed repositories. Each
       source will be checked by script manually.
    
     * Information will be copied to clipboard only if 'termux-api' is installed.
    
     * Syntax error in timeout command is fixed: 'timeout' doesn't understand the
       argument '-t'.
    
     * Minor information entries reordering.

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://termux.com/
 TERMUX_PKG_DESCRIPTION="Basic system tools for Termux"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.73
+TERMUX_PKG_VERSION=0.74
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_ESSENTIAL=true
-TERMUX_PKG_CONFFILES="etc/motd"
 TERMUX_PKG_CONFLICTS="procps (<< 3.3.15-2)"
+TERMUX_PKG_SUGGESTS="termux-api"
+TERMUX_PKG_CONFFILES="etc/motd"
 
 # Some of these packages are not dependencies and used only to ensure
 # that core packages are installed after upgrading (we removed busybox
