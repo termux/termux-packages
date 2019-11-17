d8e252b55069e1744c0df6a552752e7fd440e975
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-17 15:46:07 +0200

    transmission: reformat build.sh

--- packages/transmission/build.sh
+++ packages/transmission/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://transmissionbt.com/
 TERMUX_PKG_DESCRIPTION="Easy, lean and powerful BitTorrent client"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_DEPENDS="libevent, openssl, libcurl"
 TERMUX_PKG_VERSION=2.94
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/transmission/transmission/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=440c2fd0f89b1ab59d8a4b79ecd7bffd61bc000e36fb5b6c8e88142a4fadbb1f
+TERMUX_PKG_DEPENDS="libevent, openssl, libcurl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk --enable-lightweight --cache-file=termux_configure.cache"
 TERMUX_PKG_CONFFILES="var/service/transmission/run var/service/transmission/log/run"
 
@@ -20,12 +20,12 @@ termux_step_pre_configure() {
 }
 
 termux_step_post_make_install() {
-    	# Setup transmission service script
+	# Setup transmission service script
 	mkdir -p $TERMUX_PREFIX/var/service
 	cd $TERMUX_PREFIX/var/service
 	mkdir -p transmission/log
 	echo "#!$TERMUX_PREFIX/bin/sh" > transmission/run
-        echo 'mkdir -p ~/torrent/torrent-files' >> transmission/run
+	echo 'mkdir -p ~/torrent/torrent-files' >> transmission/run
 	echo 'mkdir -p ~/torrent/download' >> transmission/run
 	echo 'exec transmission-daemon \' >> transmission/run
 	echo '    -f \' >> transmission/run
@@ -38,5 +38,5 @@ termux_step_post_make_install() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > transmission/log/run
 	echo 'mkdir -p "$LOGDIR/sv/transmission"' >> transmission/log/run
 	echo 'exec svlogd "$LOGDIR/sv/transmission"' >> transmission/log/run
-        chmod +x transmission/log/run
+	chmod +x transmission/log/run
 }
