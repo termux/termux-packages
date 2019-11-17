df5a2b42cba7d47da2caf4eb8783c270614517bb
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-18 00:55:10 +0200

    pango: merge with x11-packages/pango-x

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=1.44.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=66a5b6cc13db73efed67b8e933584509f8ddb7b10a8a40c3850ca4a985ea1b1f
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false"
-TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo, libffi"
+TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo, libxft, zlib"
 TERMUX_PKG_BREAKS="pango-dev"
 TERMUX_PKG_REPLACES="pango-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false"
 
 termux_step_post_make_install() {
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/pango-view.1 \
