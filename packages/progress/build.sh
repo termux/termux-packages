2b07f7cd15d08e77bdfe5863a2f7dd9c03c0dd50
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-18 21:37:53 +0200

    move from unstable: progress

---
+++ packages/progress/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Xfennec/progress
+TERMUX_PKG_DESCRIPTION="Linux tool to show progress for cp, mv, dd and more"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=0.14
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=https://github.com/Xfennec/progress/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=214a0d84b3ee5dde57ec9952ec9aa68ad9261fb336ef025324b344ed7ab48af1
+TERMUX_PKG_DEPENDS="ncurses"
+TERMUX_PKG_BUILD_IN_SRC=true
+
