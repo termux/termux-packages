320abbef31a6974f8c5e2912db32ed576de74562
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-11-18 03:42:37 +0200

    bump all packages
    %ci:no-build

--- x11-packages/termux-x11/build.sh
+++ x11-packages/termux-x11/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/x11-packages
 TERMUX_PKG_DESCRIPTION="Utility to start X11 Termux add-on"
 TERMUX_PKG_LICENSE="GPL-3.0" # same as Termux:X11 add-on app
 TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_DEPENDS="libwayland"
 
