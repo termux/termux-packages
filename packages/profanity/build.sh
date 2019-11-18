e5ad2a8a45ea2593edc7e855bee15b91e2a0c1dd
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-18 02:21:26 +0200

    profanity: update for python 3.8

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C us
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_VERSION=0.7.1
-TERMUX_PKG_SHA256=3fe442948ff2ee258681c3812e878d39179dcf92e1c67bc8fe0ef8896440b05b
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/profanity-im/profanity/releases/download/$TERMUX_PKG_VERSION/profanity-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=3fe442948ff2ee258681c3812e878d39179dcf92e1c67bc8fe0ef8896440b05b
 TERMUX_PKG_DEPENDS="libandroid-support, libffi, ncurses, glib, libmesode, libcurl, readline, libuuid, libotr, gpgme, python, libassuan, libgpg-error, zlib, libsignal-protocol-c"
 TERMUX_PKG_BREAKS="profanity-dev"
 TERMUX_PKG_REPLACES="profanity-dev"
@@ -14,8 +15,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-python-plugins"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.7m"
-	LDFLAGS+=" -lpython3.7m"
+	CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.8"
+	LDFLAGS+=" -lpython3.8"
 }
 
 termux_step_post_configure() {
