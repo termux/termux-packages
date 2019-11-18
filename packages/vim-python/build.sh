f6e6addf36bd995e98d13733b8bc83ac9615b801
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-18 20:23:44 +0200

    vim-python: fix python support
    Issue https://github.com/termux/termux-packages/issues/4563.

--- packages/vim-python/build.sh
+++ packages/vim-python/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update both vim and vim-python to the same version in one PR.
 TERMUX_PKG_VERSION=8.1.2200
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=75a60a72402b3dd3780d680764ce3cd2283df178bd82bf8448f4900375f55c5a
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -40,6 +40,7 @@ TERMUX_PKG_CONFFILES="share/vim/vimrc"
 TERMUX_PKG_CONFLICTS="vim"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
 vi_cv_path_python3_pfx=$TERMUX_PREFIX
+vi_cv_var_python3_abiflags=
 vi_cv_var_python3_version=3.8
 --enable-python3interp
 --with-python3-config-dir=$TERMUX_PREFIX/lib/python3.8/config-3.8/
