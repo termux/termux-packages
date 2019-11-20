6d7ed31f103364496936536edef5c0387a298b8d
Fredrik Fornwall <fredrik@fornwall.net>
2019-11-21 00:00:49 +0100

    libgmime: Update from 3.2.4 to 3.2.5

--- packages/libgmime/build.sh
+++ packages/libgmime/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://spruce.sourceforge.net/gmime/
 TERMUX_PKG_DESCRIPTION="MIME message parser and creator"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.2.4
+TERMUX_PKG_VERSION=3.2.5
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gmime/${TERMUX_PKG_VERSION:0:3}/gmime-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=249ea7c0e080b067aa9669162c36b181b402f6cf6cebc4999d838c6f1e81d024
+TERMUX_PKG_SHA256=fb7556501f85c3bf3e65fdd82697cbc4fa4b55dccd33ad14239ce0197e78ba59
 TERMUX_PKG_DEPENDS="glib, libffi, libiconv, libidn2, zlib"
 TERMUX_PKG_BREAKS="libgmime-dev"
 TERMUX_PKG_REPLACES="libgmime-dev"
