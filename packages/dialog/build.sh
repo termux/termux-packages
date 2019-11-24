60a0661bfacb478ce2c47036b2ca463fb47177d9
Fredrik Fornwall <fredrik@fornwall.net>
2019-11-24 19:38:21 +0100

    dialog: Update from 1.3-20190808 to 1.3-20191110

--- packages/dialog/build.sh
+++ packages/dialog/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://invisible-island.net/dialog/
 TERMUX_PKG_DESCRIPTION="Application used in shell scripts which displays text user interface widgets"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION="1.3-20190808"
+TERMUX_PKG_VERSION="1.3-20191110"
 TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/dialog/dialog-$TERMUX_PKG_VERSION.tgz
-TERMUX_PKG_SHA256=886e12f2cf3df36cde65f32f6ae52bc598eb2599a611b1d8ce5dfdea599e47e2
+TERMUX_PKG_SHA256=d92a7d0910404c02c908e3ce774ac652edc822f45c6536c7d72fafa10e1d0e63
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ncursesw --enable-widec --with-pkg-config"
 
 termux_step_pre_configure() {
