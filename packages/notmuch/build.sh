3fb627b354dbe637debf8f514513ca433321fc68
Fredrik Fornwall <fredrik@fornwall.net>
2019-11-27 23:14:54 +0100

    notmuch: Update from 0.29.2 to 0.29.3

--- packages/notmuch/build.sh
+++ packages/notmuch/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://notmuchmail.org
 TERMUX_PKG_DESCRIPTION="Thread-based email index, search and tagging system"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.29.2
-TERMUX_PKG_SHA256=1bda6149b2fb0840f4d146391cab89e541d30a778f1f6e6fa95b456097cd55de
+TERMUX_PKG_VERSION=0.29.3
+TERMUX_PKG_SHA256=d5f704b9a72395e43303de9b1f4d8e14dd27bf3646fdbb374bb3dbb7d150dc35
 TERMUX_PKG_SRCURL=https://notmuchmail.org/releases/notmuch-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_DEPENDS="glib, libc++, libgmime, libtalloc, libxapian, zlib"
 TERMUX_PKG_BREAKS="notmuch-dev"
