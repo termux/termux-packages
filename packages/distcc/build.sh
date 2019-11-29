531c4e7f700751bcecfb40c794f247146fb7f84a
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-29 14:12:00 +0200

    move some packages from unstable repo
    Some of them *may* have unresolved issues.

---
+++ packages/distcc/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=http://distcc.org/
+TERMUX_PKG_DESCRIPTION="Distributed C/C++ compiler."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=3.3.3
+TERMUX_PKG_SRCURL=https://github.com/distcc/distcc/releases/download/v$TERMUX_PKG_VERSION/distcc-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=bead25471d5a53ecfdf8f065a6fe48901c14d5008956c318c700e56bc87bf0bc
+TERMUX_PKG_DEPENDS="libpopt"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-pump-mode
+--without-avahi
+--without-gtk
+--without-libiberty"
+
+termux_step_pre_configure() {
+	./autogen.sh
+	export LIBS="-llog"
+}
---
+++ packages/fetchmail/build.sh
@@ -0,0 +1,17 @@
+##
+## NOTE: script 'fetchmailconf' uses TkInter which is not available
+##       in Termux variant of python2.
+##
+TERMUX_PKG_HOMEPAGE=http://www.fetchmail.info/
+TERMUX_PKG_DESCRIPTION="A remote-mail retrieval utility"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=6.4.1
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/fetchmail/files/branch_${TERMUX_PKG_VERSION:0:3}/fetchmail-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=3f33f11dd08c3e8cc3e9d18eec686b1626d4818f4d5a72791507bbc4dce6a9a0
+TERMUX_PKG_DEPENDS="libcrypt, openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=$TERMUX_PREFIX"
+
+termux_step_pre_configure() {
+	export LIBS="-llog"
+}
