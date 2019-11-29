93d281d2fa53a82f4591d5d35abdfa775a5cc9cc
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-11-29 14:19:06 +0200

    termux-elf-cleaner: update to 1.8

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -4,8 +4,8 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 # NOTE: The termux-elf-cleaner.cpp file is used by build-package.sh
 #       to create a native binary. Bumping this version will need
 #       updating the checksum used there.
-TERMUX_PKG_VERSION=1.7
+TERMUX_PKG_VERSION=1.8
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cf74cabfcf5c22e0308074e6683ca7efa14f1a3c801d1656b96e38ff7301ae0b
+TERMUX_PKG_SHA256=c5870051fc996a2064b4d4e2ab6139edd8add9127ab6761925e47de925100a87
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
