b328de80cb25c6b7718560692b2d3cca3a01c975
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-02 01:05:59 +0200

    restic: update to 0.9.6

--- packages/restic/build.sh
+++ packages/restic/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://restic.net/
 TERMUX_PKG_DESCRIPTION="Fast, secure, efficient backup program"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=0.9.5
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.9.6
 # Use a snapshot to fix building with go 1.13:
 #TERMUX_PKG_SRCURL=https://github.com/restic/restic/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SRCURL=https://github.com/restic/restic/archive/604b18aa7426148a55f76307ca729e829ff6b61d.zip
