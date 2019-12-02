fabee4f118e9a16e225eebf27860c6e46d264c47
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-02 02:03:16 +0200

    libzopfli: delete unneeded patch

--- packages/libzopfli/build.sh
+++ packages/libzopfli/build.sh
@@ -7,12 +7,4 @@ TERMUX_PKG_SHA256=e955a7739f71af37ef3349c4fa141c648e8775bceb2195be07e86f8e638814
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BREAKS="libzopfli-dev"
 TERMUX_PKG_REPLACES="libzopfli-dev"
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_post_make_install() {
-	make zopflipng
-	cp zopfli zopflipng $TERMUX_PREFIX/bin/
-
-	mkdir -p $TERMUX_PREFIX/include/zopfli/
-	cp $TERMUX_PKG_SRCDIR/src/zopfli/*h $TERMUX_PREFIX/include/zopfli/
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DZOPFLI_BUILD_SHARED=ON"
