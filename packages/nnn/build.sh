ed403d3dd7213da1b66d5eb848030f43f33dd9a5
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-04 16:37:24 +0100

    nnn: Update from 2.7 to 2.8

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jarun/nnn
 TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=2.7
-TERMUX_PKG_SHA256=0592c7cbcf2cf66cacac49e9204636480820b1bc74e4187dd7ee06945a6d07c5
+TERMUX_PKG_VERSION=2.8
+TERMUX_PKG_SHA256=95fd4b8b48f7aadc3bf6ab3f902adb7157a6985a5163bc1deb182d6c44ce6540
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="file, findutils, readline, wget, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
