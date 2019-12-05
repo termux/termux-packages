c6f2eb1f67d774d67018ab151aba83fe375ae41f
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-05 23:45:32 +0100

    seafile-client: Update from 7.0.2 to 7.0.4

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://seafile.com
 TERMUX_PKG_DESCRIPTION="Seafile is a file syncing and sharing software with file encryption and group sharing"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=7.0.2
+TERMUX_PKG_VERSION=7.0.4
 TERMUX_PKG_SRCURL=https://github.com/haiwen/seafile/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b6041d96fd04a6ef05f7cacbaffcbdde96a63b8ec4f824d7dbd741eae83d9389
+TERMUX_PKG_SHA256=75f98e1c08939b933134fa998f21fafc208aa02d980f5b8624dbf02d7b0cae13
 TERMUX_PKG_DEPENDS="ccnet, libcurl"
 TERMUX_PKG_BREAKS="seafile-client-dev"
 TERMUX_PKG_REPLACES="seafile-client-dev"
