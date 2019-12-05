61dc12949b32b2158aae646f033e7cc96412e029
Henrik Grimler <henrik@grimler.se>
2019-12-05 06:23:15 +0100

    lxc: remove dirmngr from deps
    Since dirmngr is now part of gnupg

--- root-packages/lxc/build.sh
+++ root-packages/lxc/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=3.2.1
 TERMUX_PKG_SRCURL=https://linuxcontainers.org/downloads/lxc-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=5f903986a4b17d607eea28c0aa56bf1e76e8707747b1aa07d31680338b1cc3d4
-TERMUX_PKG_DEPENDS="dirmngr, gnupg, libcap, libseccomp, rsync, wget"
+TERMUX_PKG_DEPENDS="gnupg, libcap, libseccomp, rsync, wget"
 TERMUX_PKG_BREAKS="lxc-dev"
 TERMUX_PKG_REPLACES="lxc-dev"
 
