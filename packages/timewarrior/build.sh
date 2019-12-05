4ba279a95fb5333e95566f461caf14e4090ad2fa
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-05 23:29:49 +0100

    timewarrior: Update from 1.1.1 to 1.2.0

--- packages/timewarrior/build.sh
+++ packages/timewarrior/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://taskwarrior.org/docs/timewarrior/
 TERMUX_PKG_DESCRIPTION="Command-line time tracker"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.1.1
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://taskwarrior.org/download/timew-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1f7d9a62e55fc5a3126433654ccb1fd7d2d135f06f05697f871897c9db77ccc9
+TERMUX_PKG_VERSION=1.2.0
+TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/timewarrior/releases/download/v$TERMUX_PKG_VERSION/timew-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2c4d153105a32536ae328038246ebd846a5abd96df7ed29c11100866eaed8e3c
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++"
 
 termux_step_pre_configure() {
