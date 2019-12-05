6c4d50ec23314bc02b3dabb9f267d0a7839c5df5
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-05 23:28:26 +0100

    syncthing: Update from 1.3.1 to 1.3.2

--- packages/syncthing/build.sh
+++ packages/syncthing/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://syncthing.net/
 TERMUX_PKG_DESCRIPTION="Decentralized file synchronization"
 TERMUX_PKG_LICENSE="MPL-2.0"
-TERMUX_PKG_VERSION=1.3.1
-TERMUX_PKG_SHA256=159619dfe4f22efd4e863e1871124e04c531d96940fad843c44ba605c2dd79ef
+TERMUX_PKG_VERSION=1.3.2
+TERMUX_PKG_SHA256=0de3096bd307d6f845a236100e6dc3096145c51d852443b57cd2c5a9a138537b
 TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/releases/download/v${TERMUX_PKG_VERSION}/syncthing-source-v${TERMUX_PKG_VERSION}.tar.gz
 
 termux_step_make(){
