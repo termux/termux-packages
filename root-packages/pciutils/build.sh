0c261877e4239232afdd92ea4c6e39f635beff7a
Henrik Grimler <henrik@grimler.se>
2019-12-05 06:36:11 +0100

    pciutils: switch upstream url and depend on zlib

--- root-packages/pciutils/build.sh
+++ root-packages/pciutils/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mj.ucw.cz/sw/pciutils/
 TERMUX_PKG_DESCRIPTION="a collection of programs for inspecting and manipulating configuration of PCI devices"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=3.6.2
-TERMUX_PKG_SRCURL=ftp://atrey.karlin.mff.cuni.cz/pub/linux/pci/pciutils-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://mj.ucw.cz/download/linux/pci/pciutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=30005e341bb0ffa734174e592dc8f0dd928e1c9368b859715812149ed91d8f93
+TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
