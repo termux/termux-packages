271c9e4503c78ad70bbf4cb49c400ed7d4e2ec9c
Henrik Grimler <henrik@grimler.se>
2019-12-07 10:45:46 +0100

    runc: update to 1.0.0-rc9

--- root-packages/runc/build.sh
+++ root-packages/runc/build.sh
@@ -2,26 +2,25 @@ TERMUX_PKG_HOMEPAGE=https://www.opencontainers.org/
 TERMUX_PKG_DESCRIPTION="A tool for spawning and running containers according to the OCI specification"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.0.0-rc8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.0.0-rc9
 TERMUX_PKG_SRCURL=https://github.com/opencontainers/runc/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=efe4ff9bbe49b19074346d65c914d809c0a3e90d062ea9619fe240f931f0b700
+TERMUX_PKG_SHA256=2ec69c25df9f02c6fd38eb287145f8afba6772f809abe01df4534b5bfd68e8d4
 TERMUX_PKG_DEPENDS="libseccomp"
 
 termux_step_make() {
-    termux_setup_golang
+	termux_setup_golang
 
-    export GOPATH="${PWD}/go"
+	export GOPATH="${PWD}/go"
 
-    mkdir -p "${GOPATH}/src/github.com/opencontainers"
-    ln -sf "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/opencontainers/runc"
+	mkdir -p "${GOPATH}/src/github.com/opencontainers"
+	ln -sf "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/opencontainers/runc"
 
-    cd "${GOPATH}/src/github.com/opencontainers/runc" && make
+	cd "${GOPATH}/src/github.com/opencontainers/runc" && make
 }
 
 termux_step_make_install() {
-    cd "${GOPATH}/src/github.com/opencontainers/runc"
-    install -Dm755 runc "${TERMUX_PREFIX}/bin/runc"
+	cd "${GOPATH}/src/github.com/opencontainers/runc"
+	install -Dm755 runc "${TERMUX_PREFIX}/bin/runc"
 }
 
 termux_step_create_debscripts() {
