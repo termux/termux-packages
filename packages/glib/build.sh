9f7212277f792ae5650821638896ba8075ceace2
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-11 20:31:05 +0200

    glib: build with -DTERMUX_EXPOSE_FILE_OFFSET64
    Related issue: https://github.com/termux/termux-packages/issues/4643

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=2.62.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4400adc9f0d3ffcfe8e84225210370ce3f9853afb81812ddadb685325aa655c4
 # libandroid-support to get langinfo.h in include path.
@@ -17,5 +18,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	# glib checks for __BIONIC__ instead of __ANDROID__:
-	CFLAGS+=" -D__BIONIC__=1"
+	CFLAGS+=" -D__BIONIC__=1 -DTERMUX_EXPOSE_FILE_OFFSET64"
 }
