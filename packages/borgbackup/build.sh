b3854d77b17084f1deb288d1fd49c2f8580b4ed7
Rahul Amaram <amaramrahul@users.sourceforge.net>
2019-12-12 03:43:14 +0530

    Addressed PR comments

--- packages/borgbackup/build.sh
+++ packages/borgbackup/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.borgbackup.org/
 TERMUX_PKG_DESCRIPTION="Deduplicating and compressing backup program"
-TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=1.1.10
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=efb41416d24ff1d13c7952c7f4eaf41ef6fc5e1000354217db55cd62c905e7de
@@ -14,12 +13,8 @@ termux_step_make_install () {
   export CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.8"
   export LDFLAGS+=" -lpython3.8"
   export LDSHARED="$CC -shared"
-  export BORG_OPENSSL_PREFIX="/data/data/com.termux/files/usr"
-  export BORG_LIBLZ4_PREFIX="/data/data/com.termux/files/usr"
-  export BORG_LIBZSTD_PREFIX="/data/data/com.termux/files/usr"
+  export BORG_OPENSSL_PREFIX=$TERMUX_PREFIX
+  export BORG_LIBLZ4_PREFIX=$TERMUX_PREFIX
+  export BORG_LIBZSTD_PREFIX=$TERMUX_PREFIX
   python3.8 setup.py install --prefix=$TERMUX_PREFIX --force
-}
-
-termux_step_post_massage () {
-  find . -path '*/__pycache__*' -delete
 }
