c6ccb1eae2353e227e2764c76f3bf0af3f2e952d
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-12 00:41:06 +0100

    tor: Update from 0.4.1.6 to 0.4.2.5

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION=0.4.1.6
+TERMUX_PKG_VERSION=0.4.2.5
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2a88524ce426079fb9b828bc1b789f2c8ade3ed53c130851102debc3518bed71
+TERMUX_PKG_SHA256=4d5975862e7808faebe9960def6235669fafeeac844cb76965501fa7af79d8c2
 TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-zstd --disable-unittests"
 TERMUX_PKG_CONFFILES="etc/tor/torrc var/service/tor/run var/service/tor/log/run"
