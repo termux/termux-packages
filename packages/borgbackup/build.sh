4f8dfe6ace65a6ca3470504e4992a03bc8cca761
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-13 12:56:10 +0200

    borgbackup: remove unneeded things (site.py + easy-install.pth)
    These files are not used at runtime and can be removed to
    prevent conflicts with other packaged python modules.
    
    Issue: https://github.com/termux/termux-packages/issues/4647

--- packages/borgbackup/build.sh
+++ packages/borgbackup/build.sh
@@ -2,11 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://www.borgbackup.org/
 TERMUX_PKG_DESCRIPTION="Deduplicating and compressing backup program"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=1.1.10
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=efb41416d24ff1d13c7952c7f4eaf41ef6fc5e1000354217db55cd62c905e7de
 TERMUX_PKG_DEPENDS="libacl,liblz4,zstd,openssl,python"
 TERMUX_PKG_SRCURL=https://github.com/borgbackup/borg/releases/download/${TERMUX_PKG_VERSION}/borgbackup-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/python3.8/site-packages/easy-install.pth
+lib/python3.8/site-packages/site.py
+"
 
 termux_step_make_install () {
   export PYTHONPATH=$TERMUX_PREFIX/lib/python3.8/site-packages
