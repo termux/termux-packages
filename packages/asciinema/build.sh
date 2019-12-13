d312fb15a87afd526b58af219893ecac1d7b7f3f
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-13 12:46:50 +0200

    asciinema: remove unneeded things (site.py + easy-install.pth)
    These files are not used at runtime and can be removed to
    prevent conflicts with other packaged python modules.
    
    Issue: https://github.com/termux/termux-packages/issues/4647

--- packages/asciinema/build.sh
+++ packages/asciinema/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://asciinema.org/
 TERMUX_PKG_DESCRIPTION="Record and share your terminal sessions, the right way"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.0.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SHA256=2578a1b5611e5375771ef6582a6533ef8d40cdbed1ba1c87786fd23af625ab68
 TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -10,6 +10,10 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_HAS_DEBUG=false
 # ncurses-utils for tput which asciinema uses:
 TERMUX_PKG_DEPENDS="python, ncurses-utils"
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/python3.8/site-packages/easy-install.pth
+lib/python3.8/site-packages/site.py
+"
 
 termux_step_make() {
 	return
