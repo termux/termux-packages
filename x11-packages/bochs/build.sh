dc65484708761cea0bd830a5c4f08ba0e039be81
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-15 19:34:06 +0200

    bochs: update to 2.6.10
    %ci:reset-backlog

--- x11-packages/bochs/build.sh
+++ x11-packages/bochs/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://bochs.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Bochs is a highly portable open source IA-32 (x86) PC emulator and debugger written in C++."
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.6.9
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.6.10
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ee5b677fd9b1b9f484b5aeb4614f43df21993088c0c0571187f93acb0866e98c
+TERMUX_PKG_SHA256=723f808b2c2ac6f886d90c4eb2000b8d075992661e6e47c8474e668e16e175b0
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libc++, libcairo, libgraphite, libx11, libxpm, libxrandr, ncurses, pango, readline"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
