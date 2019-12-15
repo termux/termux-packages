d4308878ccc002484a8d2d0b5f92e973585969d6
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-15 21:32:34 +0200

    imlib2: update to 1.6.1

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/enlightenment/
 TERMUX_PKG_DESCRIPTION="Library that does image file loading and saving as well as rendering, manipulation, arbitrary polygon support"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=672a8abed2f6531ff09b69449f58dfb723a0fd6560d6f94a71e7abdb77d6f987
+TERMUX_PKG_SHA256=668696d0a8961bb57353fe96669f44bbdf1696221a65da563408a5010c5bd735
 TERMUX_PKG_DEPENDS="freetype, giflib, libandroid-shmem, libbz2, libid3tag, libjpeg-turbo, libpng, libtiff, libxext, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBS=-landroid-shmem"
