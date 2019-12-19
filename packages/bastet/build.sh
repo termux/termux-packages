51822272bc790c9c728c9baa8f066e6b6d7bd7f4
its-pointless <bored@goat.si>
2019-12-19 11:36:02 +1100

    use static lib to avoid large boost dep

--- packages/bastet/build.sh
+++ packages/bastet/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="Tetris clone with 'bastard' block-choosing AI"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=0.43.2
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/fph/bastet/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f
-TERMUX_PKG_DEPENDS="boost, libc++, ncurses"
+TERMUX_PKG_DEPENDS="libc++, ncurses"
+TERMUX_PKG_BUILD_DEPENDS="boost-static"
+TERMUX_PKG_EXTRA_MAKE_ARGS=" BOOST_PO=$TERMUX_PREFIX/lib/libboost_program_options.a"
 TERMUX_PKG_BUILD_IN_SRC=true
