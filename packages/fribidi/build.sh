e2260faba53d9301a1f4a2e8c3241bc79fe362a5
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-21 16:05:00 +0200

    fribidi: update to 1.0.8

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/fribidi/fribidi/
 TERMUX_PKG_DESCRIPTION="Implementation of the Unicode Bidirectional Algorithm"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.0.7
+TERMUX_PKG_VERSION=1.0.8
 TERMUX_PKG_SRCURL=https://github.com/fribidi/fribidi/releases/download/v$TERMUX_PKG_VERSION/fribidi-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=5ab5f21e9f2fc57b4b40f8ea8f14dba78a5cc46d9cf94bc5e00a58e6886a935d
+TERMUX_PKG_SHA256=94c7b68d86ad2a9613b4dcffe7bbeb03523d63b5b37918bdf2e4ef34195c1e6c
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BREAKS="fribidi-dev"
 TERMUX_PKG_REPLACES="fribidi-dev"
