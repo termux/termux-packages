22650f782be781a3cd0af0a9f1441136ee17c105
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-21 16:45:50 +0200

    linux-man-pages: update to 5.04

--- packages/linux-man-pages/build.sh
+++ packages/linux-man-pages/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.kernel.org/doc/man-pages/
 TERMUX_PKG_DESCRIPTION="Man pages for linux kernel and C library interfaces"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=(5.03
+TERMUX_PKG_VERSION=(5.04
 		    2013)
-TERMUX_PKG_SHA256=(7fe13aeaf9081f4c134c711270a66e264dce24dae3df7e9fec3dd18c8a485120
+TERMUX_PKG_SHA256=(7d04fb1094bb7bd218cf7a9e4493871903576054e7d1e89d8f06cb4584e6a4af
 		   19633a5c75ff7deab35b1d2c3d5b7748e7bd4ef4ab598b647bb7e7f60b90a808)
 TERMUX_PKG_SRCURL=(https://www.kernel.org/pub/linux/docs/man-pages/man-pages-${TERMUX_PKG_VERSION}.tar.xz
 		   https://www.kernel.org/pub/linux/docs/man-pages/man-pages-posix/man-pages-posix-${TERMUX_PKG_VERSION[1]}-a.tar.xz)
