305af255bb718eee91c9a0d80e8fd2d3a7212b80
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-21 16:21:02 +0200

    gst-plugins-bad: update to 1.16.2

--- packages/gst-plugins-bad/build.sh
+++ packages/gst-plugins-bad/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Bad Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.16.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.16.2
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=56481c95339b8985af13bac19b18bc8da7118c2a7d9440ed70e7dcd799c2adb5
+TERMUX_PKG_SHA256=f1cb7aa2389569a5343661aae473f0a940a90b872001824bc47fa8072a041e74
 TERMUX_PKG_DEPENDS="gst-plugins-base, libbz2, libcurl, libiconv, libpng, librsvg, libssh2, libsndfile, libx264, libx265, littlecms, openal-soft, openjpeg"
 TERMUX_PKG_BREAKS="gst-plugins-bad-dev"
 TERMUX_PKG_REPLACES="gst-plugins-bad-dev"
