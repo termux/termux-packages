d1ab9d1ba76359ed0fad325e2121e1005d20f776
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-21 15:59:06 +0200

    file: update to 5.38

--- packages/file/build.sh
+++ packages/file/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://darwinsys.com/file/
 TERMUX_PKG_DESCRIPTION="Command-line tool that tells you in words what kind of data a file contains"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=5.37
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.38
 TERMUX_PKG_SRCURL=ftp://ftp.astron.com/pub/file/file-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e9c13967f7dd339a3c241b7710ba093560b9a33013491318e88e6b8b57bae07f
+TERMUX_PKG_SHA256=593c2ffc2ab349c5aea0f55fedfe4d681737b6b62376a9b3ad1e77b2cc19fa34
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BREAKS="file-dev"
 TERMUX_PKG_REPLACES="file-dev"
