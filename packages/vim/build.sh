f1db0b6ccfd594eaa5d1b928993d88bfc77f9767
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-21 13:27:30 +0200

    vim: correctly split vim-runtime files
    Previous commit was no-op...

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update both vim and vim-python to the same version in one PR.
 TERMUX_PKG_VERSION=8.2.0000
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=8a72323817210daf40abe545bdf7637591b9b541a0fb3560baed76e436132dba
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
