669e44983f92a4c5cd4c1af5c096d11a6461b025
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-21 17:57:53 +0200

    xorriso: update to 1.5.3

--- packages/xorriso/build.sh
+++ packages/xorriso/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/xorriso
 TERMUX_PKG_DESCRIPTION="Tool for creating ISO files."
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.5.2
-TERMUX_PKG_SHA256=3b69f5c93ae7c40c5bbe4a847fa3963f5efc9c565551622f77121c5792fc17e7
+TERMUX_PKG_VERSION=1.5.3
 TERMUX_PKG_SRCURL=https://www.gnu.org/software/xorriso/xorriso-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bb8db9274caecd44993e8242e6acff4298b3508b83ac3c51c9fd970b9e765adc
 TERMUX_PKG_DEPENDS="libiconv, libandroid-support, readline, libbz2, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-jtethreads"
