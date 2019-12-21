b4b4018c7aa92e1b7d75f30d73552dea1fdcdcff
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-21 16:45:26 +0200

    libsoup: update to 2.68.3

--- packages/libsoup/build.sh
+++ packages/libsoup/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/libsoup
 TERMUX_PKG_DESCRIPTION="HTTP client and server library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.68.2
+TERMUX_PKG_VERSION=2.68.3
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libsoup/${TERMUX_PKG_VERSION:0:4}/libsoup-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=51ad3001a946fe3bcf29b692dc9ffe05cdf702ea6ca0ee8c3099a99a2f4e3933
+TERMUX_PKG_SHA256=534bb08e35b0ff3702f3adfde87d3441e27c12f9f5ec351f056fe04cba02bafb
 TERMUX_PKG_DEPENDS="glib, libpsl, libsqlite, libxml2"
 TERMUX_PKG_BREAKS="libsoup-dev"
 TERMUX_PKG_REPLACES="libsoup-dev"
