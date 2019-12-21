5e341a2c19412ca6fc1e5ca21fc83944b8c94eae
Henrik Grimler <henrik@grimler.se>
2019-12-21 19:14:18 +0100

    frotz: update to 2.50, switch upstream url

--- packages/frotz/build.sh
+++ packages/frotz/build.sh
@@ -1,23 +1,19 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/DavidGriffith/frotz
+TERMUX_PKG_HOMEPAGE=https://gitlab.com/DavidGriffith/frotz
 TERMUX_PKG_DESCRIPTION="Interpreter for Infocom and other Z-machine interactive fiction (IF) games"
 TERMUX_PKG_LICENSE="GPL-2.0"
 # frotz does not depend on dialog or curl, but the zgames script we bundle below in termux_step_make_install() do.
+TERMUX_PKG_VERSION=2.50
+TERMUX_PKG_SRCURL=https://gitlab.com/DavidGriffith/frotz/-/archive/${TERMUX_PKG_VERSION}/frotz-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0352dfc458fb5cc7a932c568bd86aabdde943bee25ea0cce58c46f8c893f554f
 TERMUX_PKG_DEPENDS="ncurses, dialog, curl"
-TERMUX_PKG_VERSION=2.44
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/DavidGriffith/frotz/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=dbb5eb3bc95275dcb984c4bdbaea58bc1f1b085b20092ce6e86d9f0bf3ba858f
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
-	CFLAGS+=" -Drindex=strrchr"
+	export CURSES_CFLAGS="-I$TERMUX_PREFIX/include"
+	export SYSCONFDIR="$TERMUX_PREFIX/include"
+	export SOUND_TYPE="none"
 }
 
-termux_step_make () {
-	CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS" PREFIX=$TERMUX_PREFIX make -j $TERMUX_MAKE_PROCESSES install
-}
-
-termux_step_make_install () {
-	cp $TERMUX_PKG_BUILDER_DIR/zgames $TERMUX_PREFIX/bin/zgames
-	chmod +x $TERMUX_PREFIX/bin/zgames
+termux_step_post_make_install () {
+	install -m755 $TERMUX_PKG_BUILDER_DIR/zgames $TERMUX_PREFIX/bin/zgames
 }
