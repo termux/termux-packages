0669f5b8c242c6c5b37fd9c2873228d6f6dd358a
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-21 16:26:09 +0200

    libical: update to 3.0.7

--- packages/libical/build.sh
+++ packages/libical/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://libical.github.io/libical/
 TERMUX_PKG_DESCRIPTION="Libical is an Open Source implementation of the iCalendar protocols and protocol data units"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=3.0.6
+TERMUX_PKG_VERSION=3.0.7
 TERMUX_PKG_SRCURL=https://github.com/libical/libical/releases/download/v$TERMUX_PKG_VERSION/libical-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=5c8a21c2b732ece4a33e5c862970b4f35a8548bbcda50de5695f6fc211ac4d97
+TERMUX_PKG_SHA256=0abe66df1ea826e57db7f281c704ede834c84139012e6c686ea7adafd4e763fc
 TERMUX_PKG_DEPENDS="libc++, libxml2"
 TERMUX_PKG_BREAKS="libical-dev"
 TERMUX_PKG_REPLACES="libical-dev"
