c9b3ccbdb27388100c6db02cbb80cf9f6dc2da3e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-21 16:21:27 +0200

    gst-plugins-good: update to 1.16.2

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.16.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.16.2
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=9fbabe69018fcec707df0b71150168776040cde6c1a26bb5a82a136755fa8f1f
+TERMUX_PKG_SHA256=40bb3bafda25c0b739c8fc36e48380fccf61c4d3f83747e97ac3f9b0171b1319
 TERMUX_PKG_DEPENDS="gst-plugins-base, libcaca, libsoup, libjpeg-turbo, libpng, libflac, libbz2, libvpx, pulseaudio, libmp3lame, gstreamer, libogg, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 # pcre needed by glib. libxml2 needed by libsoup
