55566ce0d8c1a7c39dd27e7e27ffde0be086e356
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-23 14:19:24 +0200

    sc-im: enable support for gnuplot
    Requested in https://github.com/termux/termux-packages/issues/4606.

--- packages/sc-im/build.sh
+++ packages/sc-im/build.sh
@@ -7,10 +7,11 @@ TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://github.com/andmarti1424/sc-im/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=87225918cb6f52bbc068ee6b12eaf176c7c55ba9739b29ca08cb9b6699141cad
 TERMUX_PKG_DEPENDS="libandroid-support, libzip, ncurses"
+TERMUX_PKG_SUGGESTS="gnuplot"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_configure() {
-	CFLAGS+=" $CPPFLAGS -I$TERMUX_PREFIX/include/libandroid-support -I$TERMUX_PKG_BUILDER_DIR"
+	CFLAGS+=" $CPPFLAGS -I$TERMUX_PREFIX/include/libandroid-support -I$TERMUX_PKG_BUILDER_DIR -DGNUPLOT"
 	cp $TERMUX_PKG_BUILDER_DIR/wordexp.c .
 	cp -rf src/* .
 }
