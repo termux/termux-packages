7635aa53a0af33d5f94ffa69585f6ac83778c7df
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-30 22:47:00 +0100

    kakoune: Get rid of -no-integrated-as for arm

--- packages/kakoune/build.sh
+++ packages/kakoune/build.sh
@@ -2,17 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mawww/kakoune
 TERMUX_PKG_DESCRIPTION="Code editor heavily inspired by Vim"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_VERSION=2019.12.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mawww/kakoune/releases/download/v$TERMUX_PKG_VERSION/kakoune-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=66ad8d28cecb29b08e5975e659ee91206ca676a0194c730ca4eed026bafa2ff8
 TERMUX_PKG_DEPENDS="libc++, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src debug=no"
-
-termux_step_pre_configure() {
-	if [ $TERMUX_ARCH = "arm" ]; then
-		CXXFLAGS+=" -no-integrated-as"
-	fi
-}
 
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
