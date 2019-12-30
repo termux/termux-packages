2fbc79a6800c1c62e81d306ab90b7efbe30e324e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-12-30 03:31:39 +0200

    asciinema: remove pycache from correct location

--- packages/asciinema/build.sh
+++ packages/asciinema/build.sh
@@ -2,18 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://asciinema.org/
 TERMUX_PKG_DESCRIPTION="Record and share your terminal sessions, the right way"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.0.2
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SHA256=2578a1b5611e5375771ef6582a6533ef8d40cdbed1ba1c87786fd23af625ab68
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2578a1b5611e5375771ef6582a6533ef8d40cdbed1ba1c87786fd23af625ab68
+# ncurses-utils for tput which asciinema uses:
+TERMUX_PKG_DEPENDS="python, ncurses-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_HAS_DEBUG=false
-# ncurses-utils for tput which asciinema uses:
-TERMUX_PKG_DEPENDS="python, ncurses-utils"
+
+_PYTHON_VERSION=3.8
+
 TERMUX_PKG_RM_AFTER_INSTALL="
-lib/python3.8/site-packages/easy-install.pth
-lib/python3.8/site-packages/site.py
-lib/python3.8/__pycache__
+lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
+lib/python${_PYTHON_VERSION}/site-packages/site.py
+lib/python${_PYTHON_VERSION}/site-packages/__pycache__
 "
 
 termux_step_make() {
@@ -21,6 +24,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python3.8/site-packages/
-	python3.8 setup.py install --prefix=$TERMUX_PREFIX --force
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/
+	python${_PYTHON_VERSION} setup.py install --prefix=$TERMUX_PREFIX --force
 }
