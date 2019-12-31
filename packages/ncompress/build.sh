01dcb8aed9d9fe9cb3c6a0957ee54dbf10657ce4
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-12-31 14:55:16 +0200

    ncompress: update to 4.2.4.6

--- packages/ncompress/build.sh
+++ packages/ncompress/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vapier/ncompress
 TERMUX_PKG_DESCRIPTION="The classic unix compression utility which can handle the ancient .Z archive"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=4.2.4.5
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=4.2.4.6
 TERMUX_PKG_SRCURL=https://github.com/vapier/ncompress/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f575bbcdd5f844ce22d753b9acd23d2a6b73ffc15f204bebbaf8bd6f6574903b
+TERMUX_PKG_SHA256=fb7b6a00060bd4c6e35ba4cc96a5ca7e78c193e6267487dd53376d80e061836b
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
