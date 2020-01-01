2c2931370d4fc5abec8b0dcc3f9f6bfa7b55e896
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-01 19:47:56 +0200

    new package: scrub
    Requested in https://github.com/termux/termux-packages/issues/4444.

---
+++ packages/scrub/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=http://code.google.com/p/diskscrub/
+TERMUX_PKG_DESCRIPTION="Iteratively writes patterns on files or disk devices to make retreiving the data more difficult"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=2.6.1
+TERMUX_PKG_SRCURL=https://github.com/chaos/scrub/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=838b061b2e1932b342fb9695c5579cdff5d2d72506cb41d6d8032eba18aed969
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
