1cb569787884ce2164781d11cf9e1cc605505769
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-02 22:31:37 +0100

    fdupes: Update from 1.6.1 to 2.0.0

--- packages/fdupes/build.sh
+++ packages/fdupes/build.sh
@@ -1,12 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/adrianlopezroche/fdupes
 TERMUX_PKG_DESCRIPTION="Duplicates file detector"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=1.6.1
-TERMUX_PKG_SRCURL=https://github.com/adrianlopezroche/fdupes/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9d6b6fdb0b8419815b4df3bdfd0aebc135b8276c90bbbe78ebe6af0b88ba49ea
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_configure() {
-    sed -i "s,PREFIX = /usr/local,PREFIX = ${TERMUX_PREFIX}," "$TERMUX_PKG_SRCDIR/Makefile"
-}
-
+TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_SRCURL=https://github.com/adrianlopezroche/fdupes/releases/download/$TERMUX_PKG_VERSION/fdupes-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=eb9e3bd3e722ebb2a272e45a1073f78c60f8989b151c3661421b86b14b203410
+TERMUX_PKG_DEPENDS="ncurses,pcre2"
