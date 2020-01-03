aca27e51eec02b438974440b58e9960a6c503696
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-03 02:49:57 +0200

    new package: mkvtoolnix

---
+++ packages/libmatroska/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://dl.matroska.org/downloads/libmatroska/
+TERMUX_PKG_DESCRIPTION="Matroska library"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_SRCURL=https://github.com/Matroska-Org/libmatroska/archive/release-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=8d03020ca3a657fe8d1ff57209b4218b6001df2ecd11ae956b428de941f2811f
