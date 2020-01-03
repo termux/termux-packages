8ee1b0b1db0358eda79d16e8e04e07c6826f7f58
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-03 04:08:41 +0200

    new package: mkvtoolnix
    Requested in https://github.com/termux/termux-packages/issues/4731.

---
+++ packages/mkvtoolnix/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://www.bunkus.org/videotools/mkvtoolnix/
+TERMUX_PKG_DESCRIPTION="Set of tools to create, edit and inspect Matroska files"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=42.0.0
+TERMUX_PKG_SRCURL=https://gitlab.com/mbunkus/mkvtoolnix/-/archive/release-$TERMUX_PKG_VERSION/mkvtoolnix-release-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=31bcc50d8955a338480c06c31fdd26595f3cd1f25a7e35c7ae5d250444ee1bac
+TERMUX_PKG_DEPENDS="boost, file, fmt, libebml, libflac, libmatroska, libogg, libvorbis, zlib"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-static
+--with-boost-filesystem=boost_filesystem
+--with-boost-system=boost_system
+--with-boost-date-time=boost_date_time
+"
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
+
+termux_step_make() {
+	rake
+}
+
+termux_step_make_install() {
+	rake install
+}
