7be84e4f62067b22677aae1a26719064277c0d14
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-11 21:14:55 +0200

    libpipeline: update to 1.5.2

--- packages/libpipeline/build.sh
+++ packages/libpipeline/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://libpipeline.nongnu.org/
 TERMUX_PKG_DESCRIPTION="C library for manipulating pipelines of subprocesses in a flexible and convenient way"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.5.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=d633706b7d845f08b42bc66ddbe845d57e726bf89298e2cee29f09577e2f902f
+TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/libpipeline/libpipeline-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fd59c649c1ae9d67604d1644f116ad4d297eaa66f838e3dfab96b41e85b059fb
 TERMUX_PKG_BREAKS="libpipeline-dev"
 TERMUX_PKG_REPLACES="libpipeline-dev"
-TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/libpipeline/libpipeline-${TERMUX_PKG_VERSION}.tar.gz
