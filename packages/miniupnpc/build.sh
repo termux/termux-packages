a8809fe654e23ca91b0e845b00eb661d533d4b39
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-11 21:13:54 +0200

    miniupnpc: update to 2.1.20191224

--- packages/miniupnpc/build.sh
+++ packages/miniupnpc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://miniupnp.tuxfamily.org/
 TERMUX_PKG_DESCRIPTION="Small UPnP client library and tool to access Internet Gateway Devices"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.1.20190824
+TERMUX_PKG_VERSION=2.1.20191224
 TERMUX_PKG_SRCURL=https://miniupnp.tuxfamily.org/files/miniupnpc-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1fadefea0ecdceb3e35bb5d901f0db880c826740e93148ddfb087b4b4389e165
+TERMUX_PKG_SHA256=447b427854b6c027ea28bc838b18582c5a7e71db84f6ff36df93bd91e46d66cf
 TERMUX_PKG_BREAKS="miniupnpc-dev"
 TERMUX_PKG_REPLACES="miniupnpc-dev"
 
