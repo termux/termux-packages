6fa12fcf39650820945f163cdce3a8f3fa07a941
ron42 <51223319+ron42@users.noreply.github.com>
2020-01-11 15:09:46 +0530

    Update tintin++ from 2.01.92 to 2.02.00

--- packages/tintin++/build.sh
+++ packages/tintin++/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://tintin.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Classic text-based MUD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.01.92
+TERMUX_PKG_VERSION=2.02.00
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/tintin/files/tintin-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=3386d49810f2dbc162f890f26bb81e3807af34753d7a4ead64cc40e845cba845
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_file__dev_ptmx=no"
