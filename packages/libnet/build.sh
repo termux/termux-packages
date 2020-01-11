32442cfe16a52c37063a551ce6242d7d97e5f480
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-11 21:11:04 +0200

    libnet: update to 1.2

--- packages/libnet/build.sh
+++ packages/libnet/build.sh
@@ -1,6 +1,6 @@
-TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/libnet-dev
+TERMUX_PKG_HOMEPAGE=https://github.com/libnet/libnet
 TERMUX_PKG_DESCRIPTION="A library which provides API for commonly used low-level net functions"
-TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=1.1.6
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libnet-dev/files/libnet-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d392bb5825c4b6b672fc93a0268433c86dc964e1500c279dc6d0711ea6ec467a
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_SRCURL=https://github.com/libnet/libnet/releases/download/v$TERMUX_PKG_VERSION/libnet-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=caa4868157d9e5f32e9c7eac9461efeff30cb28357f7f6bf07e73933fb4edaa7
