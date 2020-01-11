d82949c014ea810ad87aef70d6cb8190a9f4ec63
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-11 20:43:05 +0200

    algernon: update to 1.12.6

--- packages/algernon/build.sh
+++ packages/algernon/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://algernon.roboticoverlords.org/
 TERMUX_PKG_DESCRIPTION="Small self-contained web server with Lua, Markdown, QUIC, Redis and PostgreSQL support"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.12.5
+TERMUX_PKG_VERSION=1.12.6
 TERMUX_PKG_SRCURL=https://github.com/xyproto/algernon/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b7fe682748e060c404aff5cbcbeca83feb9afb25de15c91a14c1fe78485b423b
+TERMUX_PKG_SHA256=688f7fbd9f0cd62426a5d88d1d4ac0e300537f4bae2b5b6efcfea89f480c2cd2
 
 termux_step_make() {
 	termux_setup_golang
