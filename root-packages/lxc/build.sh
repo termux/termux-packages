2626b63dcb00a63809409dea663fd7f51700eacc
Henrik Grimler <henrik@grimler.se>
2020-01-11 09:36:32 +0100

    lxc: bump revision to remove dirmngr dependency
    Also remove no longer needed LIBS=-llog after https://github.com/termux/termux-packages/pull/4639

--- root-packages/lxc/build.sh
+++ root-packages/lxc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Linux Containers"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=3.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://linuxcontainers.org/downloads/lxc-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=5f903986a4b17d607eea28c0aa56bf1e76e8707747b1aa07d31680338b1cc3d4
 TERMUX_PKG_DEPENDS="gnupg, libcap, libseccomp, rsync, wget"
@@ -23,10 +24,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-examples
 --disable-werror
 "
-
-termux_step_pre_configure() {
-	export LIBS="-llog"
-}
 
 termux_step_post_make_install() {
 	# Simple helper script for mounting cgroups.
