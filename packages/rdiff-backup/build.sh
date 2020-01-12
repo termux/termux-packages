5774f83a095774684eee24369493ec848876f88b
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-12 20:05:52 +0200

    rdiff-backup: update to 1.3.3

--- packages/rdiff-backup/build.sh
+++ packages/rdiff-backup/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/rdiff-backup/
 TERMUX_PKG_DESCRIPTION="A utility for local/remote mirroring and incremental backups"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.2.8
+TERMUX_PKG_VERSION=1.3.3
 TERMUX_PKG_SRCURL=https://savannah.nongnu.org/download/rdiff-backup/rdiff-backup-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0d91a85b40949116fa8aaf15da165c34a2d15449b3cbe01c8026391310ac95db
+TERMUX_PKG_SHA256=ee030ce638df0eb1047cf72578e0de15d9a3ee9ab24da2dc0023e2978be30c06
 TERMUX_PKG_DEPENDS="librsync, python2"
 TERMUX_PKG_BUILD_IN_SRC=true
 
