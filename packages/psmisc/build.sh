0fc2fdf84906b2baf9cbad44fad7cc565702a2fe
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-12 01:33:38 +0100

    psmisc: Use sysinfo() for uptime (fixes #4783)

--- packages/psmisc/build.sh
+++ packages/psmisc/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/psmisc/psmisc
 TERMUX_PKG_DESCRIPTION="Some small useful utilities that use the proc filesystem"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=23.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/psmisc-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=41750e1a5abf7ed2647b094f58127c73dbce6876f77ba4e0a7e0995ae5c7279a
 TERMUX_PKG_DEPENDS="ncurses"
