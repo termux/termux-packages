9966c8ee6941297bc6838e96a769d87e39430fa7
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-13 01:23:46 +0200

    libcln: update to 1.3.6

--- packages/libcln/build.sh
+++ packages/libcln/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.ginac.de/CLN/
 TERMUX_PKG_DESCRIPTION="CLN is a library for efficient computations with all kinds of numbers in arbitrary precision"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.3.5
+TERMUX_PKG_VERSION=1.3.6
 TERMUX_PKG_SRCURL=https://www.ginac.de/CLN/cln-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=78810064a50b4299a0a3c16cade54a7d2e72ac92a8ee295f9a9177efc81e842d
+TERMUX_PKG_SHA256=f492530e8879bda529009b6033e1923c8f4aae843149fc28c667c20b094d984a
 TERMUX_PKG_DEPENDS="libc++, libgmp"
 TERMUX_PKG_BREAKS="libcln-dev"
 TERMUX_PKG_REPLACES="libcln-dev"
