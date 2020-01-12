6afc5d4b12e1c1e82b787ccf06799dba77de567d
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-13 01:31:39 +0200

    dictd: update home page

--- packages/dictd/build.sh
+++ packages/dictd/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://sourceforge.net/projects/dict/
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/dict/
 TERMUX_PKG_DESCRIPTION="Online dictionary client and server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.12.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dict/dictd/dictd-${TERMUX_PKG_VERSION}/dictd-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a237f6ecdc854ab10de5145ed42eaa2d9b6d51ffdc495f7daee59b05cc363656
 TERMUX_PKG_DEPENDS="libmaa, zlib"
