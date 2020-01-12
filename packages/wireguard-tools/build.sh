76a1919d639152737aa74cfd93427bcb08f9a8e4
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-12 19:40:11 +0200

    wireguard-tools: update to 1.0.20200102

--- packages/wireguard-tools/build.sh
+++ packages/wireguard-tools/build.sh
@@ -1,15 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://www.wireguard.com
 TERMUX_PKG_DESCRIPTION="Tools for the WireGuard secure network tunnel"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.0.20191219
-TERMUX_PKG_SRCURL=https://git.zx2c4.com/WireGuard/snapshot/WireGuard-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=5aba6f0c38e97faa0b155623ba594bb0e4bd5e29deacd8d5ed8bda8d8283b0e7
+TERMUX_PKG_VERSION=1.0.20200102
+TERMUX_PKG_SRCURL=https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-$TERMUX_PKG_VERSION.zip
+TERMUX_PKG_SHA256=fffee62b8c5520658895d9f94b420fd62204d2f8b223fa683416906600f62517
 TERMUX_PKG_DEPENDS="libmnl, tsu"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src/tools WITH_BASHCOMPLETION=yes WITH_WGQUICK=no WITH_SYSTEMDUNITS=no"
+TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src WITH_BASHCOMPLETION=yes WITH_WGQUICK=no WITH_SYSTEMDUNITS=no"
 
 termux_step_post_make_install() {
-    cd src/tools/wg-quick
+    cd src/wg-quick
     $CC $CFLAGS $LDFLAGS -DWG_CONFIG_SEARCH_PATHS="\"$TERMUX_ANDROID_HOME/.wireguard $TERMUX_PREFIX/etc/wireguard /data/misc/wireguard /data/data/com.wireguard.android/files\"" -o wg-quick android.c
-    install -m 0755 wg-quick $TERMUX_PREFIX/bin
+    install -Dm0700 wg-quick $TERMUX_PREFIX/bin/wg-quick
 }
