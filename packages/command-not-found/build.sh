4e9d9661ac82f2b7c326d133d13f2c0ecfa28fb8
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-13 17:53:38 +0200

    command-not-found: update to 1.46

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.45
+TERMUX_PKG_VERSION=1.46
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7d736b50731ea61e080bfc3ac94677adf6394dae7b8aa30f0a9b5a7253a50179
+TERMUX_PKG_SHA256=888daf225fd3b340d19270476a690c2f80c667df9cf77b4cdf51dff364fceeda
