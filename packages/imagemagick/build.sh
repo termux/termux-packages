6dbbc0d572a2fbdf2a9a6d28dd2a3d010ca26bec
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-13 22:53:06 +0200

    imagemagick: do not disable largefile support

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_VERSION=7.0.9.16
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.gz
 TERMUX_PKG_SHA256=c2e6b02328110fe190fc1b8a1d4c096a18c1cb2baa1c69bb37e94d7359c34b3b
 TERMUX_PKG_DEPENDS="fftw, pango, glib, libbz2, libjpeg-turbo, liblzma, libpng, libtiff, libxml2, openjpeg, littlecms, libwebp, librsvg"
@@ -9,7 +10,6 @@ TERMUX_PKG_BREAKS="imagemagick-dev"
 TERMUX_PKG_REPLACES="imagemagick-dev"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-largefile
 --without-x
 --without-gvc
 --with-magick-plus-plus=no
