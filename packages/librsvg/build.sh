c15f2beebe9f73980832c08603aa6c69bbe01b5b
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-14 00:10:30 +0200

    librsvg: enable pixbuf loader
    Requirement for https://github.com/termux/x11-packages/issues/139.

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -2,12 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=2.46.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b45b9ee3b64c58baaf800bcdff5fcd04d79930dba4c56e46e0d3b0aead40cc29
 TERMUX_PKG_DEPENDS="gdk-pixbuf, libcairo, libcroco, pango, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-introspection --disable-pixbuf-loader"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_path_GDK_PIXBUF_QUERYLOADERS=$TERMUX_PREFIX/bin/gdk-pixbuf-query-loaders
+--disable-introspection
+"
 
 termux_step_pre_configure() {
 	termux_setup_rust
