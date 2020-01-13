fa3e5edef700219f8339d9c30757bd642947c5ec
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-13 13:23:29 +0200

    imagemagick: update to 7.0.9.16

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
-TERMUX_PKG_VERSION=7.0.9.15
+TERMUX_PKG_VERSION=7.0.9.16
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.gz
-TERMUX_PKG_SHA256=69ce9dd4cefd6e89835cc94e84c9415256e0da56db5ad7dfb0e9f0208b4ff62c
+TERMUX_PKG_SHA256=c2e6b02328110fe190fc1b8a1d4c096a18c1cb2baa1c69bb37e94d7359c34b3b
 TERMUX_PKG_DEPENDS="fftw, pango, glib, libbz2, libjpeg-turbo, liblzma, libpng, libtiff, libxml2, openjpeg, littlecms, libwebp, librsvg"
 TERMUX_PKG_BREAKS="imagemagick-dev"
 TERMUX_PKG_REPLACES="imagemagick-dev"
