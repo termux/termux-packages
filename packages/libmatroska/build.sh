bfae53ea93ae0811eb3935bc79fadd043905441b
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-13 18:12:26 +0200

    libmatroska: do not split static libraries

--- packages/libmatroska/build.sh
+++ packages/libmatroska/build.sh
@@ -2,5 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://dl.matroska.org/downloads/libmatroska/
 TERMUX_PKG_DESCRIPTION="Matroska library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Matroska-Org/libmatroska/archive/release-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=8d03020ca3a657fe8d1ff57209b4218b6001df2ecd11ae956b428de941f2811f
+TERMUX_PKG_NO_STATICSPLIT=true
