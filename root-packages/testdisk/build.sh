a6a79de7e8b6e4751da40ba0ca374b23400eedd6
infamix <34374618+infamix@users.noreply.github.com>
2020-01-14 16:40:13 +0100

    testdisk: add package

---
+++ root-packages/testdisk/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://www.cgsecurity.org/wiki/TestDisk
+TERMUX_PKG_DESCRIPTION="Partition Recovery and File Undelete"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=7.1
+TERMUX_PKG_SRCURL=https://www.cgsecurity.org/testdisk-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=1413c47569e48c5b22653b943d48136cb228abcbd6f03da109c4df63382190fe
+TERMUX_PKG_DEPENDS="libuuid, zlib, libjpeg-turbo, libiconv, ncurses"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--bindir=$TERMUX_PREFIX/bin
+--sysconfdir=$TERMUX_PREFIX/etc
+--localstatedir=$TERMUX_PREFIX/var
+--mandir=$TERMUX_PREFIX/share/man
+--without-ewf
+--without-ntfs3g
+--without-ntfs
+--without-reiserfs
+"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+        make -j2 static
+}
