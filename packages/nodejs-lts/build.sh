4eeb7cf55984bc9875da24ea6c19ab103df68117
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-16 03:01:11 +0200

    nodejs-lts: avoid FICLONE ioctl

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=12.14.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=877b4b842318b0e09bc754faf7343f2f097f0fc4f88ab9ae57cf9944e88e7adb
 # Note that we do not use a shared libuv to avoid an issue with the Android
