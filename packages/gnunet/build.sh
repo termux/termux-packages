493229eb89b0b9769eb0ca84a66a30798b7aeb64
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-18 11:48:14 +0200

    gnunet: update to 0.12.2

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://gnunet.org
 TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.12.1
+TERMUX_PKG_VERSION=0.12.2
 TERMUX_PKG_DEPENDS="gnurl, libgcrypt, libgmp, libidn, libjansson, libltdl, libmicrohttpd, libsqlite, libunistring"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_have_decl_struct_in6_ifreq=yes"
