3d3b170f180c0bd446a1416da00919aa8c6a17cf
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-19 14:03:56 +0200

    python: disable LTO due to issues with build config
    References package builder directory for paths of certain
    binaries like llvm-ar and causes failure when installing
    modules like Numpy.
    
    See https://www.reddit.com/r/termux/comments/eqhpoq/unable_to_install_numpy_on_termux/.

--- packages/python/build.sh
+++ packages/python/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python 3 programming language intended to enable clear p
 TERMUX_PKG_LICENSE="PythonPL"
 _MAJOR_VERSION=3.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://www.python.org/ftp/python/${TERMUX_PKG_VERSION}/Python-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=75894117f6db7051c1b34f37410168844bbb357c139a8a10a352e9bf8be594e8
 TERMUX_PKG_DEPENDS="gdbm, libandroid-support, libbz2, libcrypt, libffi, liblzma, libsqlite, ncurses, ncurses-ui-libs, openssl, readline, zlib"
@@ -30,8 +30,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_buggy_getaddrinfo=no"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-loadable-sqlite-extensions"
 # Fix https://github.com/termux/termux-packages/issues/2236:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_little_endian_double=yes"
-# Enable optimizations:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-lto"
 # Force disable semaphores (Android does not support them).
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_sem_open=no"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_sem_timedwait=no"
@@ -52,9 +50,6 @@ termux_step_pre_configure() {
 	CPPFLAGS+=" -I$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/include"
 	LDFLAGS+=" -L$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib"
 	if [ $TERMUX_ARCH = x86_64 ]; then LDFLAGS+=64; fi
-
-	# -Oz causes linking error when -flto is enabled.
-	CFLAGS="${CFLAGS/-Oz/-O3}"
 }
 
 termux_step_post_make_install() {
