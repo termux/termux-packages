499784d716fb412538c86fcc3effe89dbfb84ee6
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-22 23:10:30 +0200

    fsmon: update to 1.6.1

--- packages/fsmon/build.sh
+++ packages/fsmon/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/nowsecure/fsmon
 TERMUX_PKG_DESCRIPTION="Filesystem monitor with fanotify and inotify backends"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.5
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://github.com/nowsecure/fsmon/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=37ea1c83297976f5c7058637a328150dea57743d5eb55ebfc3a8075d262d67c2
+TERMUX_PKG_SHA256=79b6b0c256ac2464cb02f0e36566cda66bf9cbb5dd4005290beb89ee815d2067
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
