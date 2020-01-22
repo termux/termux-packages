2b8d5e05079ea242ac3b417de24671957ea5befa
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-22 23:20:33 +0200

    html2text: fix version

--- packages/html2text/build.sh
+++ packages/html2text/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.mbayer.de/html2text/
 TERMUX_PKG_DESCRIPTION="Utility that converts HTML documents into plain text"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.3.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=http://www.mbayer.de/html2text/downloads/html2text-${TERMUX_PKG_VERSION}a.tar.gz
+TERMUX_PKG_VERSION=1:1.3.2a
+TERMUX_PKG_SRCURL=http://www.mbayer.de/html2text/downloads/html2text-${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=000b39d5d910b867ff7e087177b470a1e26e2819920dcffd5991c33f6d480392
 TERMUX_PKG_BUILD_IN_SRC=true
 
