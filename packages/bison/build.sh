5eae7389787445ade228b438cd7569b48d5702b5
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-23 19:23:58 +0200

    bison: remove ./share/info/dir from package

--- packages/bison/build.sh
+++ packages/bison/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/bison/
 TERMUX_PKG_DESCRIPTION="General-purpose parser generator"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=3.5.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/bison/bison-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3e7e097bd9709a2d5e40e69446b74b149733b3de864fadb7a9b54eca7b2a4dd0
 TERMUX_PKG_DEPENDS="m4"
@@ -11,3 +11,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 M4=m4
 ac_cv_header_spawn_h=no
 "
+
+TERMUX_PKG_RM_AFTER_INSTALL="share/info/dir"
