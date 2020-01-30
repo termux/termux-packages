af66dc90e3e832d51348fcb1dc55630d2d940382
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-30 22:14:20 +0200

    parallel: rebuild

--- packages/parallel/build.sh
+++ packages/parallel/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/parallel/
 TERMUX_PKG_DESCRIPTION="GNU Parallel is a shell tool for executing jobs in parallel using one or more machines"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=20200122
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/parallel/parallel-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=116f1e428da5cdb26cda3ee5f249bdb20c5f96f139c0a284ad142919d6d80c1c
 TERMUX_PKG_DEPENDS="perl"
