4c5f3a73989aa90cdaa515ff7e10980af4d1f8ec
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-30 21:00:13 +0200

    gegl: update to 0.4.20

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://gegl.org/
 TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.4.18
+TERMUX_PKG_VERSION=0.4.20
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=c946dfb45beb7fe0fb95b89a25395b449eda2b205ba3e8a1ffb1ef992d9eca64
+TERMUX_PKG_SHA256=23bd8bb42b20de7c2a8c314eeb5301dc20141feda20a4b1ed455020b69b73dff
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, zlib"
 TERMUX_PKG_BREAKS="gegl-dev"
 TERMUX_PKG_REPLACES="gegl-dev"
