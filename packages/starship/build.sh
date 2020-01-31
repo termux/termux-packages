65f3e11b5d4cb2662b27bb6b7d0d8da4da266a0a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-01-31 15:15:26 +0200

    Revert "starship: update to 0.33.1"
    This reverts commit f6b19109c5a06592c9e497a7b8b756fdf9b6aadb.
    
    Compilation fails with following error:
    
    Compiling rustls-native-certs v0.1.0
    error[E0432]: unresolved import `platform`
      --> /home/builder/.cargo/registry/src/github.com-1ecc6299db9ec823/rustls-native-certs-0.1.0/src/lib.rs:22:9
       |
    22 | pub use platform::load_native_certs;
       |         ^^^^^^^^ use of undeclared type or module `platform`

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://starship.rs
 TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=0.33.1
+TERMUX_PKG_VERSION=0.32.2
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=90e1f2e795cd0e694d7a514faeee19f881c35ec46811298adde692d606837d5d
+TERMUX_PKG_SHA256=ca8cbb2a34f38bf80839d3e9e26efc3c5009bc43aca040a03d0006a7c6f0845e
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
