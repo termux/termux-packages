e399fa5cd6691010f34fa1b4630854f56d818f5a
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-01-31 14:56:42 +0200

    poppler: update to 0.85.0

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.84.0
+TERMUX_PKG_VERSION=0.85.0
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=c7a130da743b38a548f7a21fe5940506fb1949f4ebdd3209f0e5b302fa139731
+TERMUX_PKG_SHA256=2bc875eb323002ae6b287e09980473518e2b2ed6b5b7d2e1089e36a6cd00d94b
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, zlib"
 TERMUX_PKG_BREAKS="poppler-dev"
 TERMUX_PKG_REPLACES="poppler-dev"
