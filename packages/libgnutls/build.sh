a3aaa6a69a7bc0c8f61e2c60233b009615c9eda4
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-02-04 16:45:58 +0200

    libgnutls: update to 3.6.12

--- packages/libgnutls/build.sh
+++ packages/libgnutls/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnutls.org/
 TERMUX_PKG_DESCRIPTION="Secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=3.6.11.1
+TERMUX_PKG_VERSION=3.6.12
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/gnutls/v${TERMUX_PKG_VERSION:0:3}/gnutls-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=fbba12f3db9a55dbf027e14111755817ec44b57eabec3e8089aac8ac6f533cf8
+TERMUX_PKG_SHA256=bfacf16e342949ffd977a9232556092c47164bd26e166736cf3459a870506c4b
 TERMUX_PKG_DEPENDS="libgmp, libnettle, ca-certificates, libidn2, libunistring"
 TERMUX_PKG_BREAKS="libgnutls-dev"
 TERMUX_PKG_REPLACES="libgnutls-dev"
