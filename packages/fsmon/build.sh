4e4f9b70a9359266015327c50bf58af8d6c5c6c1
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-02-05 12:43:16 +0200

    fsmon: rebuild package

--- packages/fsmon/build.sh
+++ packages/fsmon/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/nowsecure/fsmon
 TERMUX_PKG_DESCRIPTION="Filesystem monitor with fanotify and inotify backends"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.6.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/nowsecure/fsmon/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=79b6b0c256ac2464cb02f0e36566cda66bf9cbb5dd4005290beb89ee815d2067
 TERMUX_PKG_BUILD_IN_SRC=true
