f931ce16d4b732c246353a90b345ccd1c8353843
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-02-05 13:57:13 +0200

    openal-soft: rebuild package

--- packages/openal-soft/build.sh
+++ packages/openal-soft/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://openal-soft.org/
 TERMUX_PKG_DESCRIPTION="Software implementation of the OpenAL API"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=1.20.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kcat/openal-soft/archive/openal-soft-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=c32d10473457a8b545aab50070fe84be2b5b041e1f2099012777ee6be0057c13
 TERMUX_PKG_BREAKS="openal-soft-dev"
