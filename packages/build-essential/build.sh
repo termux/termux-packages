970391d6abfb4efc1bde95bc1865d50a2894eb07
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-02-07 13:28:49 +0200

    build-essential: keep bare minimum in dependencies & move others to recommends

--- packages/build-essential/build.sh
+++ packages/build-essential/build.sh
@@ -2,23 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-packages
 TERMUX_PKG_DESCRIPTION="A metapackage that installs essential development tools"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=3.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=4.0
+TERMUX_PKG_DEPENDS="clang, make, pkg-config"
+TERMUX_PKG_RECOMMENDS="autoconf, automake, bc, bison, cmake, flex, gperf, libtool, m4"
+TERMUX_PKG_SUGGESTS="git, golang, nodejs, patchelf, proot, python, python2, ruby, rust, subversion"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_METAPACKAGE=true
-TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
-#
-# Common utilities for various build systems:
-#
-#   autoconf, automake, bison, clang, cmake, flex, gperf, libtool, m4, make, ninja, pkg-config, util-linux
-#
-# These packages replace busybox's applets:
-#
-#   bc, bzip2, coreutils, diffutils, ed, findutils, gawk, grep, gzip, lzip, lzop, patch, procps, psmisc, sed, tar, xz-utils
-#
-
-TERMUX_PKG_DEPENDS="autoconf, automake, bison, clang, cmake, flex, gperf, libtool, m4, make, ninja, pkg-config, bc, bzip2, coreutils, diffutils, ed, findutils, gawk, grep, gzip, lzip, lzop, patch, procps, psmisc, sed, tar, util-linux, xz-utils"
-
-# Other packages that may be interesting.
-TERMUX_PKG_SUGGESTS="git, golang, nodejs, patchelf, proot, python, python2, ruby, rust, subversion"
