22e1fb22dbaf59b9bec1495ff64afdd4a68ab74e
Henrik Grimler <henrik@grimler.se>
2020-02-08 23:32:20 +0100

    command-not-found: update to 1.47 (#4890)

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.46
+TERMUX_PKG_VERSION=1.47
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=888daf225fd3b340d19270476a690c2f80c667df9cf77b4cdf51dff364fceeda
+TERMUX_PKG_SHA256=230e527d8cadb9c2d89c51047bdce3c0f448cb24323106eaa09afbd275a4a0d6
