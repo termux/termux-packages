33765666628ed2675fcd588bdb121f74084b1f0b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-02-10 14:58:36 +0200

    xkeyboard-config: update to 2.29

--- x11-packages/xkeyboard-config/build.sh
+++ x11-packages/xkeyboard-config/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/XKeyboardConfig/
 TERMUX_PKG_DESCRIPTION="X keyboard configuration files"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.28
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.29
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=69adb25b0fc64e4075f8ec0eab8d869892419f474f91fb69db1713de2062bdce
+TERMUX_PKG_SHA256=1d4175278bf06000683656763a8b1d3282c61a314b6db41260c8efe92d621802
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_DEPENDS="libx11"
 
