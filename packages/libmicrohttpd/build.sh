e0e7f1cfc6ddfea37e783598f40c214e3a09a1ab
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-02-10 13:59:52 +0200

    libmicrohttpd: rebuild package

--- packages/libmicrohttpd/build.sh
+++ packages/libmicrohttpd/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A small C library that is supposed to make it easy to ru
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=0.9.70
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=90d0a3d396f96f9bc41eb0f7e8187796049285fabef82604acd4879590977307
 TERMUX_PKG_DEPENDS="libgnutls"
