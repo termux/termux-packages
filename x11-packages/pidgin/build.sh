b7e4e06a82926c47d0fc5b8c19297d330271f3c4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-02-11 16:59:44 +0200

    new package: pidgin
    Requested in https://github.com/termux/x11-packages/issues/87.

---
+++ x11-packages/pidgin/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://pidgin.im/
+TERMUX_PKG_DESCRIPTION="Multi-protocol instant messaging client"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_VERSION=2.13.0
+TERMUX_PKG_SRCURL=https://bitbucket.org/pidgin/main/downloads/pidgin-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=2747150c6f711146bddd333c496870bfd55058bab22ffb7e4eb784018ec46d8f
+TERMUX_PKG_DEPENDS="gtk2, libgnutls, libidn, libsm, libxext, libxss"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-gtkspell
+--disable-vv
+--disable-meanwhile
+--disable-avahi
+--disable-dbus
+--disable-perl
+--disable-tcl
+--disable-tk
+"
