7415eafb25d84e46b0a4650aad0fd3202cc6ea64
Fredrik Fornwall <fredrik@fornwall.net>
2020-02-15 23:28:18 +0100

    ttyd: Update from 1.5.2 to 1.6.0

--- packages/ttyd/build.sh
+++ packages/ttyd/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://tsl0922.github.io/ttyd/
 TERMUX_PKG_DESCRIPTION="Command-line tool for sharing terminal over the web"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.5.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.6.0
 TERMUX_PKG_SRCURL=https://github.com/tsl0922/ttyd/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b5b62ec2ce08add0173e6d1dfdd879e55f02f9490043e89f389981a62e87d376
-TERMUX_PKG_DEPENDS="json-c, libwebsockets"
+TERMUX_PKG_SHA256=d14740bc82be0d0760dd0a3c97acbcbde490412a4edc61edabe46d311b068f83
+TERMUX_PKG_DEPENDS="json-c, libuv, libwebsockets, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_XXD=$TERMUX_PKG_TMPDIR/xxd"
 
 termux_step_pre_configure() {
