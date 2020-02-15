2a9a1c8304ed39b1a8094364dbd537921c96d8dc
Fredrik Fornwall <fredrik@fornwall.net>
2020-02-15 23:17:20 +0100

    fish: Update from 3.0.2 to 3.1.0

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -1,18 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://fishshell.com/
 TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.0.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.1.0
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/releases/download/$TERMUX_PKG_VERSION/fish-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=14728ccc6b8e053d01526ebbd0822ca4eb0235e6487e832ec1d0d22f1395430e
+TERMUX_PKG_SHA256=e5db1e6839685c56f172e1000c138e290add4aa521f187df4cd79d4eab294368
 # fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line.
 # man is needed since fish calls apropos during command completion.
 TERMUX_PKG_DEPENDS="libc++, ncurses, libandroid-support, ncurses-utils, man, bc, pcre2"
 TERMUX_PKG_BUILD_IN_SRC=true
+# Prevent clash with ripgrep package:
+TERMUX_PKG_RM_AFTER_INSTALL="share/fish/completions/rg.fish"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-ac_cv_file__proc_self_stat=yes
-ac_cv_func_wcstod_l=no
---without-included-pcre2
+-DBUILD_DOCS=OFF
 "
 
 termux_step_pre_configure() {
