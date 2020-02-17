b1b07440abc6ab829ef87b55abd0bb5e333d10f6
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-02-17 15:44:59 +0200

    libprotobuf: update to 3.11.4

--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -8,7 +8,7 @@ termux_setup_protobuf() {
 			termux_download \
 				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
 				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				39f5d64b0f31117c94651c880d0a776159e49eab42b2066229569934b936a5e7
+				6d0f18cd84b918c7b3edd0203e75569e0c8caecb1367bbbe409b45e28514f5be
 
 			rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
 			unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
