52a670d50c96e577fa10e9d9afd317c5aace004d
Fredrik Fornwall <fredrik@fornwall.net>
2020-02-20 15:00:31 +0100

    geth: Update from 1.9.10 to 1.9.11

--- packages/geth/build.sh
+++ packages/geth/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://geth.ethereum.org/
 TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.9.10
+TERMUX_PKG_VERSION=1.9.11
 TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=d16afd0686a083e2bb3c18862c7d736505355e42edff4c401112dc643c4fad15
+TERMUX_PKG_SHA256=226cf65cda4eab3e66e8042c3d8bcee8ed07c619ae0ec2421ed4b0abe97bb055
 
 termux_step_make() {
 	termux_setup_golang
