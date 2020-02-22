d6e3c98a130571520a46248f848c751438e745f1
Henrik Grimler <henrik@grimler.se>
2020-02-22 21:29:44 +0100

    gptfdisk: update to 1.0.5

--- root-packages/gptfdisk/build.sh
+++ root-packages/gptfdisk/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.rodsbooks.com/gdisk/
 TERMUX_PKG_DESCRIPTION="A text-mode partitioning tool that works on GUID Partition Table (GPT) disks"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.0.4
+TERMUX_PKG_VERSION=1.0.5
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$TERMUX_PKG_VERSION/gptfdisk-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b663391a6876f19a3cd901d862423a16e2b5ceaa2f4a3b9bb681e64b9c7ba78d
+TERMUX_PKG_SHA256=0e7d3987cd0488ecaf4b48761bc97f40b1dc089e5ff53c4b37abe30bc67dcb2f
 TERMUX_PKG_DEPENDS="libpopt, libuuid, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
