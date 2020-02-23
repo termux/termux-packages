ae74f8a245f0e4b9d512820895211a2221193a77
Henrik Grimler <henrik@grimler.se>
2020-02-23 21:57:21 +0100

    rgbds: use our LDFLAGS, and clean up build.sh a bit

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -2,12 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://rednex.github.io/rgbds/
 TERMUX_PKG_DESCRIPTION="Rednex Game Boy Development System"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.3.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rednex/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e58af4aa548b2a6f799da8db6ea951e2dbbc7a70a56b7539a0eea80e2b0dbbf3
 TERMUX_PKG_DEPENDS="libpng, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=${TERMUX_PREFIX} Q= STRIP="
 
-termux_step_pre_configure() {
-	CFLAGS+=" $LDFLAGS"
-}
