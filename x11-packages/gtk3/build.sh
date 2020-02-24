fa9c940c22e730dc8b3934fab60bdc634f00a473
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-02-24 21:16:11 +0200

    gtk3: update to 3.24.14

--- x11-packages/gtk3/build.sh
+++ x11-packages/gtk3/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gtk.org/
 TERMUX_PKG_DESCRIPTION="GObject-based multi-platform GUI toolkit"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=3.24.13
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.24.14
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gtk/-/archive/$TERMUX_PKG_VERSION/gtk-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=3ba1f64ef425a42334c5a4a6fa8738764ae9f5f82968e45ba1d19a9fb4d975a9
+TERMUX_PKG_SHA256=c81879fb446e9fac7a7cab9966a23f7820d98ade097adbceff5b7dcae2275dd6
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, gdk-pixbuf, glib, glib-bin, gtk-update-icon-cache, libcairo, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango, shared-mime-info, ttf-dejavu"
