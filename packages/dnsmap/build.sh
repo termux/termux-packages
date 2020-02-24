b836f218b31776343fbccc5ab54788021785b524
xploitednoob <xploitednoob@gmail.com>
2020-02-24 23:47:59 +0600

    new package: dnsmap (build keep failing)

---
+++ packages/dnsmap/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://code.google.com/archive/p/dnsmap/
+TERMUX_PKG_DESCRIPTION="Subdomain Bruteforcing Tool"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
+TERMUX_PKG_VERSION=0.30
+TERMUX_PKG_SRCURL=https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dnsmap/dnsmap-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fcf03a7b269b51121920ac49f7d450241306cfff23c76f3da94b03792f6becbc
+TERMUX_PKG_DEPENDS='ndk-sysroot'
