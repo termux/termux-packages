a117713a04e016a2a91b15606aff6759a2ce2094
xploitednoob <xploitednoob@gmail.com>
2020-02-25 00:29:24 +0600

    new package: dnsmap

--- packages/dnsmap/build.sh
+++ packages/dnsmap/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
 TERMUX_PKG_VERSION=0.30
 TERMUX_PKG_SRCURL=https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dnsmap/dnsmap-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fcf03a7b269b51121920ac49f7d450241306cfff23c76f3da94b03792f6becbc
-TERMUX_PKG_DEPENDS='ndk-sysroot'
+TERMUX_PKG_BUILD_IN_SRC=true
