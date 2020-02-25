1a166741a322216d96f6cef4b57b4845ec13b525
Fredrik Fornwall <fredrik@fornwall.net>
2020-02-25 11:31:50 +0100

    dnsmap: Update from 0.30 to 0.35

--- packages/dnsmap/build.sh
+++ packages/dnsmap/build.sh
@@ -1,8 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://code.google.com/archive/p/dnsmap/
+TERMUX_PKG_HOMEPAGE=https://github.com/resurrecting-open-source-projects/dnsmap
 TERMUX_PKG_DESCRIPTION="Subdomain Bruteforcing Tool"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
-TERMUX_PKG_VERSION=0.30
-TERMUX_PKG_SRCURL=https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dnsmap/dnsmap-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fcf03a7b269b51121920ac49f7d450241306cfff23c76f3da94b03792f6becbc
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_VERSION=0.35
+TERMUX_PKG_SRCURL=https://github.com/resurrecting-open-source-projects/dnsmap/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=6c8d28e461530721ed19314a6788f756a46c98356b5d66fa8b54294778c1f193
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
