1a0834b5539ca1da16805e12bbdf25e25b1d86ff
xploitednoob <xploitednoob@gmail.com>
2020-02-25 10:36:00 +0600

    wafw00f

---
+++ packages/wafw00f/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/EnableSecurity/wafw00f
+TERMUX_PKG_DESCRIPTION="Identify and Fingerprint Web Application Firewall"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=2.1.0
+TERMUX_PKG_SRCURL=https://github.com/EnableSecurity/wafw00f/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=611efa7e21f5acd40681eb1ed83af3500bccd4764201bd3b76c23ab7a74d6a45
+TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_BUILD_IN_SRC=true
