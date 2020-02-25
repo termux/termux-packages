2cf32c444ed3ac5f125708c175bbf608bea64156
Fredrik Fornwall <fredrik@fornwall.net>
2020-02-25 13:54:12 +0100

    rdiff-backup: Update from 1.3.3 to 1.9.1b0

--- packages/rdiff-backup/build.sh
+++ packages/rdiff-backup/build.sh
@@ -1,14 +1,18 @@
-TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/rdiff-backup/
+TERMUX_PKG_HOMEPAGE=https://rdiff-backup.net
 TERMUX_PKG_DESCRIPTION="A utility for local/remote mirroring and incremental backups"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.3.3
-TERMUX_PKG_SRCURL=https://savannah.nongnu.org/download/rdiff-backup/rdiff-backup-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ee030ce638df0eb1047cf72578e0de15d9a3ee9ab24da2dc0023e2978be30c06
-TERMUX_PKG_DEPENDS="librsync, python2"
+TERMUX_PKG_VERSION=1.9.1~b0
+TERMUX_PKG_SRCURL=https://github.com/rdiff-backup/rdiff-backup/releases/download/v${TERMUX_PKG_VERSION/\~/}/rdiff-backup-${TERMUX_PKG_VERSION/\~/}.tar.gz
+TERMUX_PKG_SHA256=c6a7335841d958ad7cda0bd640cef63a6c25fd3821b7a38af2a642557e18ad9b
+TERMUX_PKG_DEPENDS="librsync, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_make() {
+	continue
+}
+
 termux_step_make_install() {
-	local _PYTHON_VERSION=2.7
+	local _PYTHON_VERSION=3.8
 	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
 	export CPPFLAGS+=" -I${TERMUX_PREFIX}/include/python${_PYTHON_VERSION}"
 	export LDFLAGS+=" -lpython${_PYTHON_VERSION}"
