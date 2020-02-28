dc9674aa6fda84c8eef945f47eb6f508f6bc33cd
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-02-28 13:05:30 +0200

    xmake: remove unneeded dependencies
    Can't find any references to these in generated binary.

--- packages/xmake/build.sh
+++ packages/xmake/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xmake-io/xmake/releases/download/v${TERMUX_PKG_VERSION}/xmake-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c927efad5412c3bdb8bad1be5b1b2ea40a998dff2a252edb443782865b7472b9
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="clang, make, readline"
 
 termux_step_make() {
 	make build
