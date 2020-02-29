114f969a85da0f28dbb7e425dee49614ecc1c5f9
Marlin Sööse <30473690+msoose@users.noreply.github.com>
2020-02-29 08:13:07 +0800

    Create build.sh

---
+++ packages/ssmtp/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/ajwans/sSMTP"
+TERMUX_PKG_DESCRIPTION="sSMTP is a simple MTA to deliver mail from a computer to a mail hub (SMTP server)."
+TERMUX_PKG_LICENSE="GPL-2.0"
+_COMMIT=7642415100f108875af71343f19e08bf0b799124
+TERMUX_PKG_VERSION=2013.12.18-${_COMMIT}
+TERMUX_PKG_SRCURL=https://github.com/ajwans/sSMTP/archive/${_COMMIT}.tar.gz
+TERMUX_PKG_SHA256=227d31106ef8f3ac849acd48b0e64b2af9e2bf0858bc43dcd0dc39a772eb8dd3
+TERMUX_PKG_BUILD_IN_SRC="true"
