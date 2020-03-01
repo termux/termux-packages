60772a7c1059da2ae76e9dad00742246b1f88ec2
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-02 00:41:19 +0200

    git-delta: fix builds for ARM and i686

--- packages/git-delta/build.sh
+++ packages/git-delta/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dandavison/delta
 TERMUX_PKG_DESCRIPTION="A syntax-highlighter for git and diff output"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.0.16
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dandavison/delta/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=631349484bb52f3bb1c22385e19d98903c094212e83fe109f97aeff0281cf00e
 TERMUX_PKG_DEPENDS="git"
 TERMUX_PKG_BUILD_IN_SRC=true
-# Build fails on these arches when cross-compiling with error
-#  cargo:warning=error: unknown target CPU
-TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
 	rm -f Makefile
+	export CC_x86_64_unknown_linux_gnu=gcc
+	export CFLAGS_x86_64_unknown_linux_gnu="-O2"
 }
 
 termux_step_post_make_install() {
