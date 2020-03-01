8c39e89e8a52527b146e0868933a780df34ce272
Henrik Grimler <henrik@grimler.se>
2020-03-01 08:41:49 +0100

    scripts: add termux-build-chroot script for on-device builds
    Makes it possible to `make` packages as normal and then `make install`
    into an empty $PREFIX in $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX. The
    installed files can then be packaged into a deb as when
    cross-compiling.
    
    `termux-build-chroot` so far has some drawbacks: packages that
    override termux_step_make_install or termux_step_post_make_install
    will not be possible to build on device unless the overriding function
    is changed to add `termux-build-chroot` to the commands that install
    files. This could be solved more conveniently by making it possible to
    run a bash function from (like termux_step_make_install) from proot,
    but this probably requires some changes to proot.

---
+++ scripts/termux-build-chroot.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Script for setting up a fake $PREFIX, on device, where packages can be
+# installed without modifying our normal $PREFIX on device. The installed
+# files can then be packaged into a .deb file. This script is just a simplified
+# version of the termux-chroot script.
+
+termux-build-chroot() {
+	SCRIPTNAME=termux-build-chroot
+
+	# For the /system/bin/linker(64) to be found:
+	ARGS="-b /system:/system"
+
+	# Bind $TERMUX_PKG_MASSAGEDIR/data to /data so that
+        # `make install DESTDIR=/data/data/com.termux/files/usr/` installs to
+        # this subdirectory in $TERMUX_PKG_MASSAGEDIR.
+	ARGS="$ARGS -b $TERMUX_PKG_MASSAGEDIR/data:/data"
+
+	# Keep home as it is. Packages shouldn't install files to it anyway
+	ARGS="$ARGS -b $HOME:$HOME"
+
+	# Mimic traditional Linux file system hierarchy - other Termux dirs:
+	for f in bin etc lib share tmp var; do
+		ARGS="$ARGS -b $PREFIX/$f:/$f"
+	done
+
+	# Mimic traditional Linux file system hierarchy- system dirs:
+	for f in dev proc; do
+		ARGS="$ARGS -b /$f:/$f"
+	done
+
+	# # Set $TERMUX_PKG_BUILDDIR as current directory:
+	# ARGS="$ARGS --cwd=$TERMUX_PKG_BUILDDIR"
+
+	# Root of the file system:
+	ARGS="$ARGS -r $(realpath $PREFIX/..)"
+
+	# Shell to execute:
+	PROGRAM=bash
+	if [ -x $HOME/.termux/shell ]; then
+		PROGRAM=`readlink -f $HOME/.termux/shell`
+	fi
+
+	export PATH=/bin:$PATH
+	export LD_LIBRARY_PATH=/lib
+	unset LD_PRELOAD
+
+	proot $ARGS $@
+}
+
+# Make script standalone executable as well as sourceable
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+	termux-build-chroot "$@"
+fi
