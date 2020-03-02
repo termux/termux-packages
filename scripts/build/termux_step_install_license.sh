a1595e3c938c2ce3415801c78388d8ddcbf0ea7c
Henrik Grimler <henrik@grimler.se>
2020-03-02 11:14:20 +0100

    Revert "termux_step_install_{license,service_script}: install to massage dir"
    This reverts commit aa61bbf0b8e7ce69a0ff34baa4b794ec4f73742e.

--- scripts/build/termux_step_install_license.sh
+++ scripts/build/termux_step_install_license.sh
@@ -1,7 +1,7 @@
 termux_step_install_license() {
 	[ "$TERMUX_PKG_METAPACKAGE" = "true" ] && return
 
-	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME"
+	mkdir -p "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME"
 
 	if [ ! "${TERMUX_PKG_LICENSE_FILE}" = "" ]; then
 		local LICENSE
@@ -9,7 +9,7 @@ termux_step_install_license() {
 			if [ ! -f "$TERMUX_PKG_SRCDIR/$LICENSE" ]; then
 				termux_error_exit "$TERMUX_PKG_SRCDIR/$LICENSE does not exist"
 			fi
-			cp -f "${TERMUX_PKG_SRCDIR}/${LICENSE}" "$TERMUX_MASSAGEDIR/${TERMUX_PREFIX}/share/doc/${TERMUX_PKG_NAME}"/
+			cp -f "${TERMUX_PKG_SRCDIR}/${LICENSE}" "${TERMUX_PREFIX}/share/doc/${TERMUX_PKG_NAME}"/
 		done
 	else
 		local COUNTER=0
@@ -17,16 +17,16 @@ termux_step_install_license() {
 		while read -r LICENSE; do
 			if [ -f "$TERMUX_SCRIPTDIR/packages/termux-licenses/LICENSES/${LICENSE}.txt" ]; then
 				if [[ $COUNTER -gt 0 ]]; then
-					ln -sf "../../LICENSES/${LICENSE}.txt" "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE.${COUNTER}"
+					ln -sf "../../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE.${COUNTER}"
 				else
-					ln -sf "../../LICENSES/${LICENSE}.txt" "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE"
+					ln -sf "../../LICENSES/${LICENSE}.txt" "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE"
 				fi
 			fi
 			COUNTER=$((COUNTER + 1))
 		done < <(echo "$TERMUX_PKG_LICENSE" | sed "s/,/\n/g")
 
-		for LICENSE in "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME"/LICENSE*; do
-			if [ "$LICENSE" = "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE*" ]; then
+		for LICENSE in "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME"/LICENSE*; do
+			if [ "$LICENSE" = "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE*" ]; then
 				termux_error_exit "No LICENSE file was installed for $TERMUX_PKG_NAME"
 			fi
 		done
