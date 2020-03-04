59288f56c651b524e01df2917a23f3251030cbeb
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-04 02:54:40 +0200

    bump packages dependent on libx265

--- packages/gst-plugins-bad/build.sh
+++ packages/gst-plugins-bad/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Bad Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=1.16.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f1cb7aa2389569a5343661aae473f0a940a90b872001824bc47fa8072a041e74
 TERMUX_PKG_DEPENDS="gst-plugins-base, libbz2, libcurl, libiconv, libpng, librsvg, libssh2, libsndfile, libx264, libx265, littlecms, openal-soft, openjpeg"
