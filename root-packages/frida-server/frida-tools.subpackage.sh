020d570487df8e6dfdf519be467dcebfde292bb7
Henrik Grimler <henrik@grimler.se>
2020-03-05 18:49:37 +0100

    frida-tools: fix typo in postinst script

--- root-packages/frida-server/frida-tools.subpackage.sh
+++ root-packages/frida-server/frida-tools.subpackage.sh
@@ -11,5 +11,5 @@ bin/frida-trace
 
 termux_step_create_subpkg_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo "pip3.8 prompt_toolkit colorama pygments" >> postinst
+	echo "pip3.8 install prompt_toolkit colorama pygments" >> postinst
 }
