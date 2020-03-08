10e660de7c80e3cae539da31b98ca69ba08d9c2f
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-08 16:24:36 +0200

    bgrep: switch upstream & upgrade to 1.0

--- packages/bgrep/build.sh
+++ packages/bgrep/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://debugmo.de/2009/04/bgrep-a-binary-grep/
 TERMUX_PKG_DESCRIPTION="Binary string grep tool"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
-TERMUX_PKG_VERSION=0.2
-TERMUX_PKG_SRCURL=https://github.com/tmbinc/bgrep/archive/bgrep-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=24c02393fb436d7a2eb02c6042ec140f9502667500b13a59795388c1af91f9ba
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_SRCURL=https://github.com/rsharo/bgrep/archive/bgrep-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ba5ddae672e84bf2d8ce91429a4ce8a5e3a154ee7e64d1016420f7dc7481ec0a
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX"/bin bgrep
+termux_step_pre_configure() {
+	./bootstrap
 }
