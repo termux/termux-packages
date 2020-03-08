181b28b569f8636c1365a19c6397bbe89224a949
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-08 22:34:39 +0200

    mkvtoolnix: update to 44.0.0

--- packages/mkvtoolnix/build.sh
+++ packages/mkvtoolnix/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.bunkus.org/videotools/mkvtoolnix/
 TERMUX_PKG_DESCRIPTION="Set of tools to create, edit and inspect Matroska files"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=43.0.0
+TERMUX_PKG_VERSION=44.0.0
 TERMUX_PKG_SRCURL=https://gitlab.com/mbunkus/mkvtoolnix/-/archive/release-$TERMUX_PKG_VERSION/mkvtoolnix-release-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=d64b8f78f4702b6bfdf0283baf99888b9558ca70fb56cd68bbcb7201e62c7916
+TERMUX_PKG_SHA256=e61aea461b48655b6b929e72ec47ea75305f62eb7d6bfa52815a4091d66d0477
 TERMUX_PKG_DEPENDS="boost, file, libflac, libogg, libvorbis, zlib"
 TERMUX_PKG_BUILD_DEPENDS="fmt, libebml, libmatroska"
 TERMUX_PKG_BUILD_IN_SRC=true
