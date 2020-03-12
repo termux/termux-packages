75f192e7dfd12af1716dd2d85ae721861dc03781
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-12 19:46:51 +0200

    restart build for libicu and dependent packages

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='International Components for Unicode library'
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
 TERMUX_PKG_VERSION=66.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/icu4c-${TERMUX_PKG_VERSION//./_}-src.tar.xz
 TERMUX_PKG_SHA256=fc4cb13d2ff25c848584bb08058d849cc03b9f45d3c06bc607b2f1aa930b7f24
 TERMUX_PKG_DEPENDS="libc++"
