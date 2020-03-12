07c696d17a3fa198ab6b8d967aa1a57234ef5298
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-12 19:08:44 +0200

    gitea: update to 1.11.3

--- packages/gitea/build.sh
+++ packages/gitea/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitea.io
 TERMUX_PKG_DESCRIPTION="Git with a cup of tea, painless self-hosted git service"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.11.2
+TERMUX_PKG_VERSION=1.11.3
 TERMUX_PKG_SRCURL=https://github.com/go-gitea/gitea/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=fca0ba2bd7c43c211cdb409f6bec3623d4b9d490bdd15a554abd4007033c1800
+TERMUX_PKG_SHA256=2161d32de58df8afce361cd08d843aa62bd43afbe22f18000cf6d6f6868dc672
 TERMUX_PKG_DEPENDS="dash, git"
 TERMUX_PKG_CONFFILES="etc/gitea/app.ini"
 
