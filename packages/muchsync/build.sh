358273414c32335987190ae53bd9f7392612b8b7
Matthias Beyer <mail@beyermatthias.de>
2020-03-13 12:37:24 +0100

    new package: muchsync (#5027)

---
+++ packages/muchsync/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.muchsync.org/
+TERMUX_PKG_DESCRIPTION="Synchronize notmuch mail across machines"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=5
+TERMUX_PKG_SRCURL=http://www.muchsync.org/src/muchsync-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8b0afc2ce2dca636ae318659452902e26ac804d1b8b1982e74dbc4222f2155cc
+TERMUX_PKG_DEPENDS="libc++, libsqlite, libxapian, notmuch, openssl"
