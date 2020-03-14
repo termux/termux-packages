cdf302ebe4f1de6f31a249ae7c4c96c00fdf1f6a
Richard de Boer <richard@tubul.net>
2020-03-14 22:37:44 +0100

    tintin++: Update from 2.02.1 to 2.02.2, new homepage URL (#5036)

--- packages/tintin++/build.sh
+++ packages/tintin++/build.sh
@@ -1,9 +1,9 @@
-TERMUX_PKG_HOMEPAGE=http://tintin.sourceforge.net
+TERMUX_PKG_HOMEPAGE=https://tintin.mudhalla.net
 TERMUX_PKG_DESCRIPTION="Classic text-based MUD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.02.01
+TERMUX_PKG_VERSION=2.02.02
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/tintin/files/tintin-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=decc933d18f91e0d890e13325d8e9e60eff4238bdf3f431a647dac0c9ad15295
+TERMUX_PKG_SHA256=c5d8b6c930ec0beb9f45de434e079dddb17b48f8a3acff08acbc9d1bd15dd487
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_file__dev_ptmx=no"
 TERMUX_PKG_DEPENDS="pcre, libgnutls, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
