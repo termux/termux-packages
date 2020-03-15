ac3b0d74b6862528f859299140ab349e65d55d10
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-15 23:45:36 +0200

    rebuild rust-based packages which were updated recently
    See https://github.com/termux/termux-packages/issues/5037.

--- packages/git-delta/build.sh
+++ packages/git-delta/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dandavison/delta
 TERMUX_PKG_DESCRIPTION="A syntax-highlighter for git and diff output"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.0.17
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dandavison/delta/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ac1f26ac5ea10d43b300675189c49437dcae7a9fca7e51f615058ab0550d27e5
 TERMUX_PKG_DEPENDS="git"
--- packages/termimage/build.sh
+++ packages/termimage/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Terminal image viewer"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/nabijaczleweli/termimage/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=faf1a73b0520f2abba1954cb1af214aee6ddd1b0851621ba6a883a0be2edaf56
 TERMUX_PKG_BUILD_IN_SRC=true
