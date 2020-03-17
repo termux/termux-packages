560e8e5490f10e8385cff244d30c1220928c28e7
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-17 15:23:00 +0200

    lnd: update to 0.9.2-beta

--- packages/lnd/build.sh
+++ packages/lnd/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/lightningnetwork/lnd
 TERMUX_PKG_DESCRIPTION="Lightning Network Daemon"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.9.1-beta
+TERMUX_PKG_VERSION=0.9.2-beta
 TERMUX_PKG_SRCURL=(https://github.com/lightningnetwork/lnd/archive/v${TERMUX_PKG_VERSION}.tar.gz
                    https://github.com/lightningnetwork/lnd/releases/download/v${TERMUX_PKG_VERSION}/vendor.tar.gz)
-TERMUX_PKG_SHA256=(b0b0386e7e5a0edf08f3d333523915cd9c1255c7bf854f56d2e19078ecef0e3a
-                   b5c8285ac709672c051a1b1b92a281183b90d4a84102f497cb45bb47f8f007f8)
+TERMUX_PKG_SHA256=(8143d9990bcd6ed6f2b5028e4e2dc49515079200d1d8a6f4af64245a17400e39
+                   9ad9113b3a0ba0a1408ec8ee9ac59961e5ccf619b5ffd181110f37a451b6c165)
 TERMUX_PKG_DEPENDS="bitcoin"
 TERMUX_PKG_CONFFILES="var/service/lnd/run var/service/lnd/log/run"
 TERMUX_PKG_BUILD_IN_SRC=true
