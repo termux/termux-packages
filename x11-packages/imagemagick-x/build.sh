47e72c4d7436424a9788c481bc588f8a771d35bf
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-19 11:03:50 +0200

    imagemagick-x: update to 7.0.10.0

--- x11-packages/imagemagick-x/build.sh
+++ x11-packages/imagemagick-x/build.sh
@@ -1,37 +1,28 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
-TERMUX_PKG_VERSION=7.0.9.16
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=7.0.10.0
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.gz
-TERMUX_PKG_SHA256=c2e6b02328110fe190fc1b8a1d4c096a18c1cb2baa1c69bb37e94d7359c34b3b
-TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libjpeg-turbo, liblzma, libpixman, libpng, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib"
-
+TERMUX_PKG_SHA256=baf31e019aa5331ba91f5b750f85e1a107ae26e1ccc4c480393c48b58b4c5e80
+TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libjpeg-turbo, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib"
 TERMUX_PKG_CONFLICTS="imagemagick"
 TERMUX_PKG_REPLACES="imagemagick"
 TERMUX_PKG_PROVIDES="imagemagick"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-largefile
 --with-x
 --without-gvc
 --with-magick-plus-plus=no
+--with-bzlib=yes
 --with-xml=yes
+--with-rsvg=yes
 --with-lzma
 --disable-openmp
 ac_cv_func_ftime=no
 "
 
 TERMUX_PKG_RM_AFTER_INSTALL="
-bin/Magick-config
-bin/MagickCore-config
-bin/MagickWand-config
-bin/Wand-config
 share/ImageMagick-7/francais.xml
-share/man/man1/Magick-config.1
-share/man/man1/MagickCore-config.1
-share/man/man1/MagickWand-config.1
-share/man/man1/Wand-config.1
 "
 
 termux_step_pre_configure() {
