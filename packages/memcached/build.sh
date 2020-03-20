6acfc963a052cddc2daac56ed86d9504c5629ed7
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-20 13:38:06 +0200

    memcached: update to 1.6.1

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.5.22
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c2b47e9d20575a2367087c229636ffc3fb699a6c3a7f3a22f44402f25f5f1f93
+TERMUX_PKG_SHA256=881c30a222e019657e53b12230aaf629d7b5c080e4b74378c2eb5e78800d57d6
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_BREAKS="memcached-dev"
 TERMUX_PKG_REPLACES="memcached-dev"
@@ -11,12 +11,14 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-sasl --disable-coverage"
 
 termux_step_pre_configure() {
-    export ac_cv_c_endian=little
+	CPPFLAGS+=" -D__USE_GNU"
 
-    # fix SASL configuration path
-    perl -p -i -e "s#/etc/sasl#$TERMUX_PREFIX/etc/sasl#" $TERMUX_PKG_BUILDDIR/sasl_defs.c
+	export ac_cv_c_endian=little
 
-    # getsubopt() taken from https://github.com/lxc/lxc/blob/master/src/include/getsubopt.c
-    cp $TERMUX_PKG_BUILDER_DIR/getsubopt.c $TERMUX_PKG_SRCDIR
-    cp $TERMUX_PKG_BUILDER_DIR/getsubopt.h $TERMUX_PKG_SRCDIR
+	# fix SASL configuration path
+	perl -p -i -e "s#/etc/sasl#$TERMUX_PREFIX/etc/sasl#" $TERMUX_PKG_BUILDDIR/sasl_defs.c
+
+	# getsubopt() taken from https://github.com/lxc/lxc/blob/master/src/include/getsubopt.c
+	cp $TERMUX_PKG_BUILDER_DIR/getsubopt.c $TERMUX_PKG_SRCDIR
+	cp $TERMUX_PKG_BUILDER_DIR/getsubopt.h $TERMUX_PKG_SRCDIR
 }
