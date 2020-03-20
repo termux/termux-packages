106c4e67ac20fc8ab2baa67eb69e9089cad2f9ad
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-20 17:08:16 +0200

    cryptsetup: update to 2.3.1

--- root-packages/cryptsetup/build.sh
+++ root-packages/cryptsetup/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/cryptsetup/cryptsetup/
 TERMUX_PKG_DESCRIPTION="Userspace setup tool for transparent encryption of block devices using dm-crypt"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.3.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.3.1
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/linux/utils/cryptsetup/v${TERMUX_PKG_VERSION:0:3}/cryptsetup-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=395690de99509428354d3cd15cf023bed01487e6f1565b2181e013dc847bbc85
+TERMUX_PKG_SHA256=92aba4d559a2cf7043faed92e0f22c5addea36bd63f8c039ba5a8f3a159fe7d2
 TERMUX_PKG_DEPENDS="json-c, libdevmapper, libgcrypt, libpopt, libuuid, util-linux, openssl, libiconv"
 TERMUX_PKG_BREAKS="cryptsetup-dev"
 TERMUX_PKG_REPLACES="cryptsetup-dev"
