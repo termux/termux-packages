0a40da7c6c590b0aeaa011bbf9c0105567b6ba56
Henrik Grimler <henrik@grimler.se>
2020-03-20 09:40:41 +0100

    texlive-bin: use newer source tar
    * Modify pdftoepdf-poppler0.86.0.cc to not use c++14 features
    * Switch to liblua52 instead of liblua. I cannot find patches/sources
      for luatex that support liblua 5.3

--- packages/texlive-bin/build.sh
+++ packages/texlive-bin/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="TeX Live is a distribution of the TeX typesetting system
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=20190410
-TERMUX_PKG_REVISION=12
-TERMUX_PKG_SRCURL=https://github.com/TeX-Live/texlive-source/archive/build-svn50882.tar.gz
-TERMUX_PKG_SHA256=a7462f8e29163faa52ad2ac658727b60f95241449832f1a4dac8d8a406d18233
-TERMUX_PKG_DEPENDS="libc++, libiconv, freetype, libpng, libgd, libgmp, libmpfr, libicu, liblua, poppler, libgraphite, harfbuzz, harfbuzz-icu, teckit, libpixman, libcairo, zlib"
+TERMUX_PKG_REVISION=13
+_SVN_VERSION=54408
+TERMUX_PKG_SRCURL=https://github.com/TeX-Live/texlive-source/archive/svn${_SVN_VERSION}.tar.gz
+TERMUX_PKG_SHA256=aa394d66ce32858645ebe9a70c5b3aad4b8f8a79054c35918aaef7f91908d8b7
+TERMUX_PKG_DEPENDS="libc++, libiconv, freetype, libpng, libgd, libgmp, libmpfr, libicu, liblua52, poppler, libgraphite, harfbuzz, harfbuzz-icu, teckit, libpixman, libcairo, zlib"
 # libpcre, glib, fonconfig are dependencies to libcairo. pkg-config gives an error if they are missing
 # libuuid, libxml2 are needed by fontconfig
 TERMUX_PKG_BUILD_DEPENDS="icu-devtools, pcre, glib, fontconfig, libuuid, libxml2"
