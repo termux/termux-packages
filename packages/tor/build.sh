7b6aea556967a475b6c6ceff0a292154e39b3545
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-20 13:26:15 +0200

    tor: update to 0.4.2.7

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=0.4.2.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.4.2.7
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0500102433849bbe3231c590973d126c2d2d6b3943b4b9f9962bdb108436e6c4
+TERMUX_PKG_SHA256=06a1d835ddf382f6bca40a62e8fb40b71b2f73d56f0d53523c8bd5caf9b3026d
 TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-zstd --disable-unittests"
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
