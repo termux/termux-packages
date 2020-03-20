1358b42a30a3e1eca78a00eccb30e934f2dd9e66
Henrik Grimler <henrik@grimler.se>
2020-03-20 11:35:03 +0100

    command-not-found: update command lists

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.47
+TERMUX_PKG_VERSION=1.48
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=230e527d8cadb9c2d89c51047bdce3c0f448cb24323106eaa09afbd275a4a0d6
+TERMUX_PKG_SHA256=cf5f391e324cf73e5b480a0e3e66c8f18b9e4283b2a2e95a72149a34b4a09462
