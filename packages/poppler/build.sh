6801d6fd33454388170d98ce54b8230ddb95d6c8
Henrik Grimler <henrik@grimler.se>
2020-03-20 08:10:35 +0100

    poppler: add boost to build depends
    Improves performance.

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_VERSION=0.86.1
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=af630a277c8e194c31339c5446241834aed6ed3d4b4dc7080311e51c66257f6c
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, zlib"
+TERMUX_PKG_BUILD_DEPENDS="boost"
 TERMUX_PKG_BREAKS="poppler-dev"
 TERMUX_PKG_REPLACES="poppler-dev"
 #texlive needs the xpdf headers
