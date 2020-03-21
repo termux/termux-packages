9c138429bcc35432578bdb4504a4fe0d2abce193
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-21 22:53:32 +0200

    libgmime: update to 3.2.7

--- packages/libgmime/build.sh
+++ packages/libgmime/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://spruce.sourceforge.net/gmime/
 TERMUX_PKG_DESCRIPTION="MIME message parser and creator"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.2.6
+TERMUX_PKG_VERSION=3.2.7
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gmime/${TERMUX_PKG_VERSION:0:3}/gmime-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=abff194c7c4802fba2e233890d09dde8bf7170c3ad5e13000601c8d5b3c44717
+TERMUX_PKG_SHA256=2aea96647a468ba2160a64e17c6dc6afe674ed9ac86070624a3f584c10737d44
 TERMUX_PKG_DEPENDS="glib, libffi, libiconv, libidn2, zlib"
 TERMUX_PKG_BREAKS="libgmime-dev"
 TERMUX_PKG_REPLACES="libgmime-dev"
