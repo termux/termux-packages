196371a922732c4f28351de8da59c5554b503cf5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-21 23:05:35 +0200

    restart build of updated packages

--- packages/libgmime/build.sh
+++ packages/libgmime/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://spruce.sourceforge.net/gmime/
 TERMUX_PKG_DESCRIPTION="MIME message parser and creator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=3.2.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gmime/${TERMUX_PKG_VERSION:0:3}/gmime-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2aea96647a468ba2160a64e17c6dc6afe674ed9ac86070624a3f584c10737d44
 TERMUX_PKG_DEPENDS="glib, libffi, libiconv, libidn2, zlib"
