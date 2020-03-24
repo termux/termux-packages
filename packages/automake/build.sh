856209e7b07df58b638fd7b20b434fee62477cee
Henrik Grimler <henrik@grimler.se>
2020-03-24 12:41:25 +0100

    automake: update to 1.16.2

--- packages/automake/build.sh
+++ packages/automake/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/automake/
 TERMUX_PKG_DESCRIPTION="Tool for automatically generating Makefile.in files"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.16.1
+TERMUX_PKG_VERSION=1.16.2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/automake/automake-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=5d05bb38a23fd3312b10aea93840feec685bdf4a41146e78882848165d3ae921
+TERMUX_PKG_SHA256=ccc459de3d710e066ab9e12d2f119bd164a08c9341ca24ba22c9adaa179eedd0
 TERMUX_PKG_DEPENDS="autoconf"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
