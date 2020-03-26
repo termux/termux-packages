bbd5100825704e77798e0c1f20d5226d61229926
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-26 15:28:00 +0200

    boinctui: depend on libandroid-support

--- packages/boinctui/build.sh
+++ packages/boinctui/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.5.0
 TERMUX_PKG_SRCURL="https://sourceforge.net/projects/boinctui/files/boinctui_${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="173dd1231b29eaf5357fa5de612dbce231537c40e631d598a2f49a4f32bafd9b"
-TERMUX_PKG_DEPENDS="glib, libexpat, ncurses, openssl"
+TERMUX_PKG_DEPENDS="glib, libandroid-support, libexpat, ncurses, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-gnutls"
 
