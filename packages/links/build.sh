912b99fa6da31195e10fac4a81c5914798589f99
Henrik Grimler <henrik@grimler.se>
2020-03-26 14:14:22 +0100

    links: add zstd as dependency (#5080)

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Links is a text and graphics mode WWW browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=2.20.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f0ec13b019e5501ef29858bf61a3bf89cece0b0f065b5064be242bce84e675bd
-TERMUX_PKG_DEPENDS="libbz2, libevent, liblzma, openssl, zlib"
+TERMUX_PKG_DEPENDS="libbz2, libevent, liblzma, openssl, zlib, zstd"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --mandir=$TERMUX_PREFIX/share/man"
