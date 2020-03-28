dab3e6cb17bfac9751341778bedf9a218a6835ec
ruki <waruqi@gmail.com>
2020-03-28 22:20:31 +0800

    xmake: fix shasum to fix some serious bug (#5094)
    * xmake: fix shasum to fix some serious bug
    
    * xmake: bump revision as well to not confuse apt
    
    Co-authored-by: Henrik Grimler <henrik@grimler.se>

--- packages/xmake/build.sh
+++ packages/xmake/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="A cross-platform build utility based on Lua"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Ruki Wang @waruqi"
 TERMUX_PKG_VERSION=2.3.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xmake-io/xmake/releases/download/v${TERMUX_PKG_VERSION}/xmake-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=704dba105dd46ee50a08e64ebbfd93ad89c61b0b6bbd399482cb371cba969930
+TERMUX_PKG_SHA256=b189074320ce1b215f85f8f20142fe173d981eb17c2c157f0fa6756fc00dac4e
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
