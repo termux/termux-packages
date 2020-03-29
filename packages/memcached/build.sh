d6de6cf30a8cb479a3aa6b40e819d117e33f5b4c
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-29 22:11:34 +0300

    memcached: update to 1.6.3

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.6.1
+TERMUX_PKG_VERSION=1.6.3
 TERMUX_PKG_SRCURL=https://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=881c30a222e019657e53b12230aaf629d7b5c080e4b74378c2eb5e78800d57d6
+TERMUX_PKG_SHA256=1c64b3220ee345d28f939f0b96e30a59adf435599a51237b42c887dfc7a31540
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_BREAKS="memcached-dev"
 TERMUX_PKG_REPLACES="memcached-dev"
