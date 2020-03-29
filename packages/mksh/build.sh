2db37c106c522c94f46893272307feb7a0ba5a5d
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-29 22:12:08 +0300

    mksh: update to 58

--- packages/mksh/build.sh
+++ packages/mksh/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mirbsd.org/mksh.htm
 TERMUX_PKG_DESCRIPTION="The MirBSD Korn Shell - an enhanced version of the public domain ksh"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=57
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=58
 TERMUX_PKG_SRCURL=https://www.mirbsd.org/MirOS/dist/mir/mksh/mksh-R${TERMUX_PKG_VERSION/./}.tgz
-TERMUX_PKG_SHA256=3d101154182d52ae54ef26e1360c95bc89c929d28859d378cc1c84f3439dbe75
+TERMUX_PKG_SHA256=608beb7b71870b23309ba1da8ca828da0e4540f2b9bd981eb39e04f8b7fc678c
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
