d93baa328786ade8fbdfd5a0a40f558c820ac78d
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-03-29 22:16:03 +0300

    starship: update to 0.38.1

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://starship.rs
 TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=0.38.0
+TERMUX_PKG_VERSION=0.38.1
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ad2ea8a4c02edcabd29b2b14ca0af2d42a38141eeb6ef05d72e9d541d6671d15
+TERMUX_PKG_SHA256=3eb01abfa038994ed24d520bfabbb30273ed90909ee87b58a854d92ad5a6db9a
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
