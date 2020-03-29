259fb1150f783cf93230645fc0378a82fb86c2c6
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-29 22:19:32 +0300

    vegeta: update to 12.8.3

--- packages/vegeta/build.sh
+++ packages/vegeta/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/tsenart/vegeta
 TERMUX_PKG_DESCRIPTION="HTTP load testing tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=12.8.1
+TERMUX_PKG_VERSION=12.8.3
 TERMUX_PKG_SRCURL=https://github.com/tsenart/vegeta/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4fabf66f0137a5dde73c615330b8f163783b83bdfd8b06b365f1841c732b80ee
+TERMUX_PKG_SHA256=2e6326b2fe0ef273ae784600e2181e32d307b62beb29cc84ffc8ddd0d5352df9
 
 termux_step_make() {
 	termux_setup_golang
