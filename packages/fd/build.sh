eb4316d016723f88eb512cea3ddbba58b14e750a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-03-29 22:04:39 +0300

    fd: update to 7.5.0

--- packages/fd/build.sh
+++ packages/fd/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/fd
 TERMUX_PKG_DESCRIPTION="Simple, fast and user-friendly alternative to find"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=7.4.0
+TERMUX_PKG_VERSION=7.5.0
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/fd/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=33570ba65e7f8b438746cb92bb9bc4a6030b482a0d50db37c830c4e315877537
+TERMUX_PKG_SHA256=8a78ca24323c832cf205c1fce8276fc25ae90371531c32e155301937986ea713
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
