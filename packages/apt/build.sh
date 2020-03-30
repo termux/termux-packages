1f69d49eed77baeaac0171753fe1dc5eba9b4ffa
lucy phipps <landfillbaby@users.noreply.github.com>
2020-03-30 05:02:05 +0100

    don't use termux.org/packages
    it's just a redirect to xeffyr's mirror

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -56,7 +56,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_post_make_install() {
-	printf "# The main termux repository:\ndeb https://termux.org/packages/ stable main\n" > $TERMUX_PREFIX/etc/apt/sources.list
+	printf "# The main termux repository:\ndeb https://dl.bintray.com/termux/termux-packages-24 stable main\n" > $TERMUX_PREFIX/etc/apt/sources.list
 	cp $TERMUX_PKG_BUILDER_DIR/trusted.gpg $TERMUX_PREFIX/etc/apt/
 
 	# apt-transport-tor
