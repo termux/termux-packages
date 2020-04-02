faf325b6d0f41d51092cbfada41e9d333b01d93c
buttaface <repo@butta.fastem.com>
2020-04-03 01:02:31 +0530

    TERMUX_PKG_QUICK_REBUILD: disable reapplying the host build patches too (#5110)

--- scripts/build/termux_step_patch_package.sh
+++ scripts/build/termux_step_patch_package.sh
@@ -6,7 +6,7 @@ termux_step_patch_package() {
 	if [ "$TERMUX_DEBUG" = "true" ]; then
 		DEBUG_PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name \*.patch.debug)
 	fi
-	if [ "$TERMUX_PKG_QUICK_REBUILD" != "true" ]; then
+	if [ "$TERMUX_PKG_QUICK_REBUILD" = "false" ]; then
 		# Suffix patch with ".patch32" or ".patch64" to only apply for these bitnesses:
 		shopt -s nullglob
 		for patch in $TERMUX_PKG_BUILDER_DIR/*.patch{$TERMUX_ARCH_BITS,} $DEBUG_PATCHES; do
