fc7519f749042c799409ee6d20648381293223cb
Henrik Grimler <henrik@grimler.se>
2020-04-03 21:29:40 +0200

    librsvg: update to 2.48.2

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.48.1
+TERMUX_PKG_VERSION=2.48.2
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=91d7630cffbbe006c989ca575b404257abc2a4a636f0571ffb4607d69fd58f3a
+TERMUX_PKG_SHA256=272658a0e04fce21eb1368c345cc1305bfeada4ca6d7db99e77323ed0668bdca
 TERMUX_PKG_DEPENDS="gdk-pixbuf, libcairo, libcroco, pango, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
