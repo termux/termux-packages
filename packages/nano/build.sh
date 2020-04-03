6b2a7ef1a9dd5732b29a5d6c498709a0dafcdd5c
Henrik Grimler <henrik@grimler.se>
2020-04-03 14:46:45 +0200

    nano: update to 4.9.1

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.nano-editor.org/
 TERMUX_PKG_DESCRIPTION="Small, free and friendly text editor"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=4.9
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.9.1
 TERMUX_PKG_SRCURL=https://nano-editor.org/dist/latest/nano-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=0e399729d105cb1a587b4140db5cf1b23215a0886a42b215efa98137164233a6
+TERMUX_PKG_SHA256=52cd5a0cefaa6be199bf1a8f5295e2ef1f787f9533d1ab9ed1e52d3a242aba6c
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_header_pwd_h=no
