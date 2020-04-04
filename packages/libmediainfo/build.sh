ee3fb2f12d7364e2d5e6855c6da7599c3adf9a19
valpogus <32718480+valpogus@users.noreply.github.com>
2020-04-04 21:01:12 +0200

    Update packages libzen, libmediainfo and mediainfo

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://mediaarea.net/en/MediaInfo
 TERMUX_PKG_DESCRIPTION="Library for reading information from media files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=19.09
+TERMUX_PKG_VERSION=20.03
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/libmediainfo/${TERMUX_PKG_VERSION}/libmediainfo_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=602131fc1730154ca083c8bec6fe42aa2f1f43b36dbaedf26917e27dd8a38e73
+TERMUX_PKG_SHA256=90ea6395a5b5b6a138c2f85e9afe9723cb97bca6f9137529a0a2052acf2fe0d7
 TERMUX_PKG_DEPENDS="libcurl, libzen, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-shared --enable-static --with-libcurl"
 
--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://mediaarea.net/en/MediaInfo
 TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=19.09
+TERMUX_PKG_VERSION=20.03
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1db9a8d9416e0d276a209eee784d9865b8b4c71a26873c6b72badacc2a4ee670
+TERMUX_PKG_SHA256=55bea28ce0d39c88677c58e3a002bbb95e206749be1d3d0c9134514b4c27acdc
 TERMUX_PKG_DEPENDS=libmediainfo
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-dll"
 
