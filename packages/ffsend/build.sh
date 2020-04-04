38227f42d63bc0d6fbeb365a0669a55f351d0550
Henrik Grimler <henrik@grimler.se>
2020-04-04 03:49:59 +0200

    Update various packages (#5114)
    * cpio: update to 2.13
    
    * gmic: update to 2.9.0
    
    * ffsend: update to 0.2.59
    
    * libgrpc: update to 1.28.0
    
    * librsvg: update to 2.48.2
    
    * perl: update to 5.30.2
    
    * ruby: update to 2.7.1
    
    * ripgrep: update to 12.0.1
    
    * repology/excluded_packages: ignore aircrack-ng and guile18
    
    * mpd: update to 0.21.22

--- packages/ffsend/build.sh
+++ packages/ffsend/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/timvisee/ffsend
 TERMUX_PKG_DESCRIPTION="A fully featured Firefox Send client"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.2.58
+TERMUX_PKG_VERSION=0.2.59
 TERMUX_PKG_SRCURL=https://gitlab.com/timvisee/ffsend/-/archive/v$TERMUX_PKG_VERSION/ffsend-v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=5a2e2b792d6763fdce4db6b4e815bbaa822bd7d4e6cf6c19fd3ec21ed4937700
+TERMUX_PKG_SHA256=02df26f8f880b9f991656c6f4cf0c95bee6636dbe1ce7c120ae424c0cbfe81c4
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features send2,send3,history,archive,qrcode,urlshorten,infer-command"
