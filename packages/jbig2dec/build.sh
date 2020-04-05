4799915c1a3964964745c0fd15ef76e59cca999d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-05 21:54:01 +0300

    jbig2dec: run autogen.sh

--- packages/jbig2dec/build.sh
+++ packages/jbig2dec/build.sh
@@ -8,4 +8,7 @@ TERMUX_PKG_SHA256=9e19775237350e299c422b7b91b0c045e90ffa4ba66abf28c8fb5eb005772f
 TERMUX_PKG_DEPENDS="libpng"
 TERMUX_PKG_BREAKS="jbig2dec-dev"
 TERMUX_PKG_REPLACES="jbig2dec-dev"
-TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
