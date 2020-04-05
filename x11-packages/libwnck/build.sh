4dbded356970141b149ed05f7df7b15f99a689ae
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-05 22:16:40 +0300

    libwnck: fix build issues

--- x11-packages/libwnck/build.sh
+++ x11-packages/libwnck/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/libwnck/${TERMUX_PKG_VE
 TERMUX_PKG_SHA256=bc508150b3ed5d22354b0e6774ad4eee465381ebc0ace45eb0e2d3a4186c925f
 TERMUX_PKG_DEPENDS="gtk2, startup-notification"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
-
-termux_step_pre_configure() {
-	NOCONFIGURE=1 ./autogen.sh
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=disabled
+"
