395111625a798b64e65922f6ff14c33c84ead4e0
Henrik Grimler <henrik@grimler.se>
2020-04-07 13:26:02 +0200

    nethogs: update to 0.8.6

--- root-packages/nethogs/build.sh
+++ root-packages/nethogs/build.sh
@@ -1,16 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/raboof/nethogs
 TERMUX_PKG_DESCRIPTION="Net top tool grouping bandwidth per process"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="Pierre Rudloff <contact@rudloff.pro>"
-TERMUX_PKG_VERSION=0.8.5-git
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/raboof/nethogs/archive/master.tar.gz
-TERMUX_PKG_SHA256=846560132c7162fe5f90bd6264b877f7a7c132b4aa13bf56763292ef09150bb4
-TERMUX_PKG_FOLDERNAME=nethogs-${TERMUX_PKG_VERSION}
+TERMUX_PKG_VERSION=0.8.6
+TERMUX_PKG_SRCURL=https://github.com/raboof/nethogs/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=317c1d5235d4be677e494e931c41d063a783ac0ac51e35e345e621d261c2e5a0
 TERMUX_PKG_DEPENDS="libc++, ncurses, libpcap"
 TERMUX_PKG_EXTRA_MAKE_ARGS="nethogs"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure () {
-	export CPPFLAGS="$CPPFLAGS -Wno-c++11-narrowing"
-}
