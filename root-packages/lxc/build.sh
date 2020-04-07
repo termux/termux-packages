9dd57830afbbaf46b118ef235dfeeb6f36988451
Henrik Grimler <henrik@grimler.se>
2020-04-07 13:26:48 +0200

    frida: update to 12.8.20

--- root-packages/lxc/build.sh
+++ root-packages/lxc/build.sh
@@ -11,7 +11,7 @@ TERMUX_PKG_REPLACES="lxc-dev"
 
 # Do not build for ARM due to
 # error: /home/builder/.termux-build/_cache/android-r20-api-24-v1/bin/../lib/gcc/arm-linux-androideabi/4.9.x/armv7-a/thumb/libgcc_real.a(pr-support.o): multiple definition of '__gnu_unwind_frame'
-#TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+TERMUX_PKG_BLACKLISTED_ARCHES="arm"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-distro=termux
