b60d6368670b450eb106a6a7a1cc09b1503b6160
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-10 21:05:37 +0300

    c-ares: do not look properties for fallback dns
    They're working on old versions of Android only.
    
    Logcat:
    
      04-10 20:50:22.854 22854 22854 E libc    : Access denied finding property "net.dns1"

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://c-ares.haxx.se
 TERMUX_PKG_DESCRIPTION="Library for asynchronous DNS requests (including name resolves)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.16.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/c-ares/c-ares/archive/cares-${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=b045ef78cda05edd4c677ab351f209315aab8fb041051031bdbe185aaf74baf3
 TERMUX_PKG_BREAKS="c-ares-dev"
