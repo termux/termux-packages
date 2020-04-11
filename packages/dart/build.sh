e14128f3ec81f9ea76666e0b892d89b701643043
Arun <57064186+zorro@users.noreply.github.com>
2020-04-11 12:09:46 +0530

    Dart: Update to 2.7.2

--- packages/dart/build.sh
+++ packages/dart/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.dartlang.org/
 TERMUX_PKG_DESCRIPTION="Dart is a general-purpose programming language"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=2.7.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.7.2
 TERMUX_PKG_BUILD_IN_SRC=true
 
 # Dart uses tar and gzip to extract downloaded packages.
