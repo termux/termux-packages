08fac776ec2b6625a3ad618d49dc1bb61de6bb17
Henrik Grimler <henrik@grimler.se>
2020-04-11 07:23:01 +0200

    exiftool: bump after perl update
    Fixes https://github.com/termux/termux-packages/issues/5142

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta informatio
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
 TERMUX_PKG_VERSION=11.93
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=cecdcbd8befac5a0850ec74152d86ebb6ecad260cc75f208fde79c70c9907629
 TERMUX_PKG_DEPENDS="perl"
 
 termux_step_make_install() {
 	# Change this after package 'perl' was upgraded.
-	local current_perl_version=5.30.1
+	local current_perl_version=5.30.2
 
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/exiftool "$TERMUX_PREFIX"/bin/exiftool
 	find "$TERMUX_PKG_SRCDIR"/lib -name "*.pod" -delete
