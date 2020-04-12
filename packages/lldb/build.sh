5be5f783624e3b8d3fef7ec9e24bea61e5103631
Butta <repo@butta.fastem.com>
2020-04-12 12:43:15 +0530

    lldb: update from 9.0.1 to 10.0.0

--- packages/lldb/build.sh
+++ packages/lldb/build.sh
@@ -1,39 +1,59 @@
 TERMUX_PKG_HOMEPAGE=https://lldb.llvm.org
 TERMUX_PKG_DESCRIPTION="LLVM based debugger"
 TERMUX_PKG_LICENSE="NCSA"
-TERMUX_PKG_VERSION=9.0.1
-TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/lldb-$TERMUX_PKG_VERSION.src.tar.xz
-TERMUX_PKG_SHA256=8a7b9fd795c31a3e3cba6ce1377a2ae5c67376d92888702ce27e26f0971beb09
+TERMUX_PKG_VERSION=10.0.0
+TERMUX_PKG_SRCURL=(https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/lldb-$TERMUX_PKG_VERSION.src.tar.xz
+		   https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/llvm-$TERMUX_PKG_VERSION.src.tar.xz)
+TERMUX_PKG_SHA256=(dd1ffcb42ed033f5167089ec4c6ebe84fbca1db4a9eaebf5c614af09d89eb135
+		   df83a44b3a9a71029049ec101fb0077ecbbdf5fe41e395215025779099a98fdf)
 TERMUX_PKG_DEPENDS="libc++, libedit, libllvm, libxml2, ncurses-ui-libs"
 TERMUX_PKG_BUILD_DEPENDS="libllvm-static"
 TERMUX_PKG_BREAKS="lldb-dev, lldb-static"
 TERMUX_PKG_REPLACES="lldb-dev, lldb-static"
+TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_HAS_DEBUG=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DLLDB_DISABLE_CURSES=0
--DLLDB_DISABLE_LIBEDIT=0
--DLLDB_DISABLE_PYTHON=1
--DLLVM_CONFIG=$TERMUX_PREFIX/bin/llvm-config
+-DLLDB_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/bin/lldb-tblgen
+-DLLVM_ENABLE_SPHINX=ON
 -DLLVM_ENABLE_TERMINFO=1
 -DLLVM_LINK_LLVM_DYLIB=ON
 -DLLVM_DIR=$TERMUX_PREFIX/lib/cmake/llvm
--DClang_DIR=$TERMUX_PREFIX/lib/cmake/clang
--DLLVM_NATIVE_BUILD=$TERMUX_PREFIX/bin
+-DLLVM_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/llvm/bin/llvm-tblgen
 "
+
+termux_step_host_build() {
+	termux_setup_cmake
+	termux_setup_ninja
+
+	mkdir llvm
+	cd llvm
+
+	cmake -G Ninja $TERMUX_PKG_SRCDIR/llvm-${TERMUX_PKG_VERSION}.src
+	ninja -j $TERMUX_MAKE_PROCESSES llvm-tblgen
+
+	cd ..
+	cmake -G Ninja $TERMUX_PKG_SRCDIR -DLLDB_INCLUDE_TESTS=OFF \
+	-DLLVM_DIR=$TERMUX_PKG_HOSTBUILD_DIR/llvm/lib/cmake/llvm
+	ninja -j $TERMUX_MAKE_PROCESSES lldb-tblgen
+}
+
 termux_step_pre_configure() {
-	cd $TERMUX_PKG_TMPDIR
-	termux_download https://its-pointless.github.io/tblgen-llvm-lldb-9.0.1.tar.xz tblgen-llvm-lldb-9.0.1.tar.xz \
-		9cfd0aa3d9988e66838d4390ea9b2f701d1d8c87c44e226e10b8afd42c004622
-	tar xvf tblgen-llvm-lldb-9.0.1.tar.xz
-	mv llvm-tblgen $TERMUX_PREFIX/bin
-	PATH=$PATH:$TERMUX_PKG_TMPDIR
-	if [ $TERMUX_ARCH = "x86_64" ]; then
-		export LD_LIBRARY_PATH=/lib/x86_64-linux-gnu/
+	# This will be there if libllvm was built from scratch, but not if the pre-built
+	# package was extracted. Not really needed but the stupid clang CMake config makes
+	# sure it's there.
+	if [ ! -f "$TERMUX_PREFIX/bin/clang-import-test" ]; then
+		touch $TERMUX_PREFIX/bin/clang-import-test
+		touch $TERMUX_PKG_BUILDDIR/rm-fake-ci-test
 	fi
-	touch $TERMUX_PREFIX/bin/clang-import-test
+}
+
+termux_step_make() {
+	ninja -w dupbuild=warn -j $TERMUX_MAKE_PROCESSES all docs-lldb-man
 }
 
 termux_step_post_make_install() {
-	cp $TERMUX_PKG_SRCDIR/docs/lldb.1 $TERMUX_PREFIX/share/man/man1
-	rm -f  $TERMUX_PREFIX/bin/llvm-tblgen $TERMUX_PREFIX/bin/clang-import-test
+	cp $TERMUX_PKG_BUILDDIR/docs/man/lldb.1 $TERMUX_PREFIX/share/man/man1
+	if [ -f "$TERMUX_PKG_BUILDDIR/rm-fake-ci-test" ]; then
+		rm $TERMUX_PREFIX/bin/clang-import-test
+	fi
 }
