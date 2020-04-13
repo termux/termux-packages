b27afe35a2fbe6805c47a7083f2c9d4b231dea4f
Henrik Grimler <henrik@grimler.se>
2020-04-13 11:57:33 +0200

    dnsmasq: update to 2.81

--- root-packages/dnsmasq/build.sh
+++ root-packages/dnsmasq/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.thekelleys.org.uk/dnsmasq/doc.html
 TERMUX_PKG_DESCRIPTION="Dnsmasq provides network infrastructure for small networks"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.80
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.81
 TERMUX_PKG_SRCURL=http://www.thekelleys.org.uk/dnsmasq/dnsmasq-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cdaba2785e92665cf090646cba6f94812760b9d7d8c8d0cfb07ac819377a63bb
+TERMUX_PKG_SHA256=749ca903537c5197c26444ac24b0dce242cf42595fdfe6b9a5b9e4c7ad32f8fb
 TERMUX_PKG_BUILD_IN_SRC=true
