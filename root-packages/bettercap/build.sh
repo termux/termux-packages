a2fc1947e1096486645f584cf78f1e5a2a6a8596
Henrik Grimler <henrik@grimler.se>
2020-04-13 11:57:49 +0200

    bettercap: update to 2.27

--- root-packages/bettercap/build.sh
+++ root-packages/bettercap/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.bettercap.org
 TERMUX_PKG_DESCRIPTION="The Swiss Army knife for 802.11, BLE and Ethernet networks reconnaissance and MITM attacks"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.26.1
+TERMUX_PKG_VERSION=2.27
 TERMUX_PKG_SRCURL=https://github.com/bettercap/bettercap/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=75530015ee27e5ba05faff0295486ca85489ecd9de3161ca398a9b577522c578
+TERMUX_PKG_SHA256=0b6e86e9d2e05663fcd4130f41befb176ad4d2c953c8f7346402e0dd05a072a8
 TERMUX_PKG_DEPENDS="libpcap, libusb"
 # x86_64 seem to depend on libnetfilter_queue
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
