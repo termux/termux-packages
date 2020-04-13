5f34d5e4aef155a551d0af04fa5599e5e85b0085
Henrik Grimler <henrik@grimler.se>
2020-04-13 12:17:24 +0200

    memcached: update to 1.6.4

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.6.3
+TERMUX_PKG_VERSION=1.6.4
 TERMUX_PKG_SRCURL=https://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1c64b3220ee345d28f939f0b96e30a59adf435599a51237b42c887dfc7a31540
+TERMUX_PKG_SHA256=6b36b3a8a10c58c63933b6c279418e2f3dd2a5f75c18fb492ab1b8bf35ed5cbc
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_BREAKS="memcached-dev"
 TERMUX_PKG_REPLACES="memcached-dev"
@@ -21,4 +21,6 @@ termux_step_pre_configure() {
 	# getsubopt() taken from https://github.com/lxc/lxc/blob/master/src/include/getsubopt.c
 	cp $TERMUX_PKG_BUILDER_DIR/getsubopt.c $TERMUX_PKG_SRCDIR
 	cp $TERMUX_PKG_BUILDER_DIR/getsubopt.h $TERMUX_PKG_SRCDIR
+
+	autoreconf -vfi
 }
