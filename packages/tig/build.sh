9241979c577e66b72c6ec817f6a331825348f082
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-14 15:49:57 +0300

    tig: update to 2.5.1

--- packages/tig/build.sh
+++ packages/tig/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://jonas.github.io/tig/
 TERMUX_PKG_DESCRIPTION="Ncurses-based text-mode interface for git"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.5.0
+TERMUX_PKG_VERSION=2.5.1
 TERMUX_PKG_SRCURL=https://github.com/jonas/tig/releases/download/tig-$TERMUX_PKG_VERSION/tig-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ff537c67af9201e7e7276ce8a0ff9961e9d9c6a8a78790f5817124bd7755aef4
+TERMUX_PKG_SHA256=500d5d34524f6b856edd5cae01f1404d14f3b51a9a53fd7357f4cebb3d4c9e64
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libiconv, ncurses, git, libandroid-support"
 
