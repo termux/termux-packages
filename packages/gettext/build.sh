d2af7ab6913767bb8f49885b59899e9a3d6579df
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-14 22:24:41 +0300

    gettext: update to 0.20.2

--- packages/gettext/build.sh
+++ packages/gettext/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gettext/
 TERMUX_PKG_DESCRIPTION="GNU Internationalization utilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.20.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=0.20.2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gettext/gettext-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=53f02fbbec9e798b0faaf7c73272f83608e835c6288dd58be6c9bb54624a3800
+TERMUX_PKG_SHA256=b22b818e644c37f6e3d1643a1943c32c3a9bff726d601e53047d2682019ceaba
 TERMUX_PKG_DEPENDS="libc++, libiconv, pcre, liblzma, libxml2, libcroco, ncurses, libunistring, zlib"
 TERMUX_PKG_BREAKS="gettext-dev"
 TERMUX_PKG_REPLACES="gettext-dev"
