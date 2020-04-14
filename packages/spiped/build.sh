80f08f2af29bf107efc81b50fbf52e3b1f1db0e4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-14 22:32:57 +0300

    spiped: update to 1.6.1

--- packages/spiped/build.sh
+++ packages/spiped/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.tarsnap.com/spiped.html
 TERMUX_PKG_DESCRIPTION="a utility for creating symmetrically encrypted and authenticated pipes between socket addresses"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.6.0
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://github.com/Tarsnap/spiped/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c80a49b131aeb017c06dcc10e2461c25a333e1e32ed99a0bb6af169014306815
+TERMUX_PKG_SHA256=d14a0c802f97bfe4da6c1a6c1ec882b7ffb94d28aee1eea11e6ad532fc21254c
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
