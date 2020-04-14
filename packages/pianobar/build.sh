35136b979be74faca7f2d6cb639a48dde6797c44
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-14 22:27:07 +0300

    pianobar: update to 2020.04.05

--- packages/pianobar/build.sh
+++ packages/pianobar/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://6xq.net/pianobar/
 TERMUX_PKG_DESCRIPTION="pianobar is a free/open-source, console-based client for the personalized online radio Pandora."
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2019.02.14
+TERMUX_PKG_VERSION=2020.04.05
 TERMUX_PKG_SRCURL=https://github.com/PromyLOPh/pianobar/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=415858f8bf938a84af06b15fd49daa49fd2089f3c66f55356c0987ac4fce20d7
+TERMUX_PKG_SHA256=cb319b56ee4163ac040be1844e04de37f94d8e8b058f3bf9500ed380fe385883
 TERMUX_PKG_DEPENDS="libao, ffmpeg, libgcrypt, libcurl, json-c"
 TERMUX_PKG_BUILD_DEPENDS="libao, ffmpeg, libgcrypt, libcurl, json-c"
 TERMUX_PKG_BUILD_IN_SRC=true
