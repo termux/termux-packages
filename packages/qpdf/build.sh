f185711d3097aa6bbd41c76f26186df240d1b2a9
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-14 22:28:31 +0300

    qpdf: update to 10.0.1

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Content-Preserving PDF Transformation System"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=9.1.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=10.0.1
 TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/releases/download/release-qpdf-$TERMUX_PKG_VERSION/qpdf-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a2c6fc410ec45df0ca0996b039e00bb6f39cfc76d42d784b7ad7bc9e163f4236
+TERMUX_PKG_SHA256=5d2277c738188b7f4e3f01a6db7f2937ed6df54671f1fba834cd3d7ff865827b
 TERMUX_PKG_DEPENDS="libc++, libjpeg-turbo, zlib"
 TERMUX_PKG_BREAKS="qpdf-dev"
 TERMUX_PKG_REPLACES="qpdf-dev"
