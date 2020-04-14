1b48c74d7d4240f2713f66716f026569e232ed5b
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-14 15:46:43 +0300

    vim, vim-python: update to 8.2.0550

--- packages/vim-python/build.sh
+++ packages/vim-python/build.sh
@@ -5,9 +5,9 @@ TERMUX_PKG_DEPENDS="libiconv, ncurses, vim-runtime, python"
 TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update both vim and vim-python to the same version in one PR.
-TERMUX_PKG_VERSION=8.2.0500
+TERMUX_PKG_VERSION=8.2.0550
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=93e4aca6a37bdbe7039eebc79e963799ca079ed4fbc5cc8f8e801b362f313343
+TERMUX_PKG_SHA256=7a11d8c7e927e8fdc5182e1b8c6e8657a57e98740f9690d23afddcb3a3081e9b
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 vim_cv_getcwd_broken=no
 vim_cv_memmove_handles_overlap=yes
