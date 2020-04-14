af21fb07406f620cc4236662560f88968522d250
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-14 15:44:06 +0300

    nnn: update to 3.1

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jarun/nnn
 TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=3.0
+TERMUX_PKG_VERSION=3.1
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=04db6d6710ce1232c779bf70137a86557e486614e20327717122bb63f36348f7
+TERMUX_PKG_SHA256=d70c21b4afa30fa733ab17bb8b45291e8651f2b823fad748ffbdb3b5c3fb0489
 TERMUX_PKG_DEPENDS="file, findutils, readline, wget, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
