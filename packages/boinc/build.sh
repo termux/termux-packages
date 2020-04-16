b198873c047978103bf2171d8b745af3836f032f
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-16 14:53:18 +0300

    boinc: update patches
    As suggested in https://github.com/termux/termux-packages/issues/5048#issuecomment-614472026.

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open-source software for volunteer computing"
 TERMUX_PKG_LICENSE="GPL-3.0"
 _MAJOR_VERSION=7.16
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/BOINC/boinc/archive/client_release/${_MAJOR_VERSION}/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=caa567da8d3eb50859efe2eeba1c23c7b27d3b0f15b548136e75302713b25303
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libcurl, openssl, zlib"
@@ -18,7 +19,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 TERMUX_PKG_CONFFILES="etc/boinc-client.conf"
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -DANDROID"
-	LDFLAGS+=" -llog"
+	LDFLAGS+=" -landroid-shmem"
 	./_autosetup
 }
