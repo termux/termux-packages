60c4dede11fad75aa04e17876ccaa62dd5157f59
Henrik Grimler <henrik@grimler.se>
2020-04-17 17:47:15 +0200

    open-zwave: switch repo and depend on libusb

--- root-packages/open-zwave/build.sh
+++ root-packages/open-zwave/build.sh
@@ -1,9 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/OpenZWave/open-zwave
 TERMUX_PKG_DESCRIPTION="A C++ library to control Z-Wave Networks via a USB Z-Wave Controller"
 TERMUX_PKG_LICENSE="LGPL-3.0"
-TERMUX_PKG_VERSION=1.6.1
-TERMUX_PKG_SRCURL=https://github.com/sviete/open-zwave/archive/v$TERMUX_PKG_VERSION.zip
-TERMUX_PKG_SHA256=ecf10aa7cd5dd51172e960c0fd3ea418a24ad39d3d5db026d6bfc3d8c08ef4a0
+TERMUX_PKG_VERSION=1.6-git
+_COMMIT=5db357e3169945f990c4b2be01b38e506745097a
+TERMUX_PKG_SRCURL=https://github.com/OpenZWave/open-zwave/archive/${_COMMIT}.tar.gz
+TERMUX_PKG_SHA256=c7493ef20e59d10733b0c285c88fec97e3b6578d3ecb7879f4eefaf8c2ddd674
+TERMUX_PKG_DEPENDS="libusb"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
