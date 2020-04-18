63be80bc37fa34b826ef09485c5ae48510117648
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-19 00:33:23 +0200

    redis: Update from 5.0.8 to 5.0.9

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://redis.io/
 TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache and message broker"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=5.0.8
+TERMUX_PKG_VERSION=5.0.9
 TERMUX_PKG_SRCURL=http://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f3c7eac42f433326a8d981b50dba0169fdfaf46abb23fcda2f933a7552ee4ed7
+TERMUX_PKG_SHA256=53d0ae164cd33536c3d4b720ae9a128ea6166ebf04ff1add3b85f1242090cb85
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/redis.conf"
 
