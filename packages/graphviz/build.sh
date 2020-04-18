bb47310cfcc6a5b05620f6b9b248ec635a55b2c4
Henrik Grimler <henrik@grimler.se>
2020-04-18 19:24:31 +0200

    graphviz: fix regex problem when graphviz parses plugins
    Fixes https://github.com/termux/termux-packages/issues/5135

--- packages/graphviz/build.sh
+++ packages/graphviz/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.graphviz.org/
 TERMUX_PKG_DESCRIPTION="Rich set of graph drawing tools"
 TERMUX_PKG_LICENSE="EPL-2.0"
 TERMUX_PKG_VERSION=2.44.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www2.graphviz.org/Packages/stable/portable_source/graphviz-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9aabd13a8018b708ab3c822de2326c19d0a52ed59f50a6b0f9318c07e2a6d93b
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, libcairo, pango, libexpat, libltdl, librsvg, libgd, zlib"
