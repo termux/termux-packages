d0d8bb69db00f12735078ad868a89396c8775859
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-19 00:59:13 +0200

    kubectl: Update from 1.18.1 to 1.18.2

--- packages/kubectl/build.sh
+++ packages/kubectl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://kubernetes.io/
 TERMUX_PKG_DESCRIPTION="Kubernetes.io client binary"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.18.1
+TERMUX_PKG_VERSION=1.18.2
 TERMUX_PKG_SRCURL=https://dl.k8s.io/v$TERMUX_PKG_VERSION/kubernetes-src.tar.gz
-TERMUX_PKG_SHA256=aa30c9ecb9f38e1825d81af438b68ed187d86569355dcce2dd26e5d98fd3964d
+TERMUX_PKG_SHA256=155371355c8aaf6253a2bed2a2526293ec18ac7d3a7e787f3b9886175c4327ef
 
 termux_step_extract_package() {
 	mkdir -p "$TERMUX_PKG_CACHEDIR"
