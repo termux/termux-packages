075c0629d7ef49653043aaac7b20020d66d1dbbf
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-19 01:00:15 +0200

    mksh: Update from 58 to 59

--- packages/mksh/build.sh
+++ packages/mksh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mirbsd.org/mksh.htm
 TERMUX_PKG_DESCRIPTION="The MirBSD Korn Shell - an enhanced version of the public domain ksh"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=58
+TERMUX_PKG_VERSION=59
 TERMUX_PKG_SRCURL=https://www.mirbsd.org/MirOS/dist/mir/mksh/mksh-R${TERMUX_PKG_VERSION/./}.tgz
-TERMUX_PKG_SHA256=608beb7b71870b23309ba1da8ca828da0e4540f2b9bd981eb39e04f8b7fc678c
+TERMUX_PKG_SHA256=592a28ba67bea8a285f003d7a5d21b65e718546c8fcb375d7d696f3d5dd390ba
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
