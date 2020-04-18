0fa6a806288813c0c8d24e4c6c741b6566cdb61f
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-19 00:59:27 +0200

    git-delta: Update from 0.0.17 to 0.0.18

--- packages/git-delta/build.sh
+++ packages/git-delta/build.sh
@@ -1,15 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dandavison/delta
 TERMUX_PKG_DESCRIPTION="A syntax-highlighter for git and diff output"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.0.17
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.0.18
 TERMUX_PKG_SRCURL=https://github.com/dandavison/delta/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ac1f26ac5ea10d43b300675189c49437dcae7a9fca7e51f615058ab0550d27e5
+TERMUX_PKG_SHA256=e159f2eacfbcfe7de97047d304e4eb404e1e54cc01aa6670e159c0e771dbe104
 TERMUX_PKG_DEPENDS="git"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	rm -f Makefile
+	rm -f Makefile release.Makefile
 	export CC_x86_64_unknown_linux_gnu=gcc
 	export CFLAGS_x86_64_unknown_linux_gnu="-O2"
 }
