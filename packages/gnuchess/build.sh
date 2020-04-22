9464c06e8ba453f9178e49efbff073f8f5ada8f8
Henrik Grimler <henrik@grimler.se>
2020-04-22 20:12:31 +0200

    gnuchess: move to game-packages

--- packages/gnuchess/build.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/chess/
-TERMUX_PKG_DESCRIPTION="Chess-playing program"
-TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=6.2.6
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/chess/gnuchess-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d617f875d6411378c6ce521663ebda42db9006a5eb5706bcd821a918c06eb04f
-TERMUX_PKG_DEPENDS="libc++, ncurses, readline"
-TERMUX_PKG_RM_AFTER_INSTALL="bin/gnuchessu bin/gnuchessx"
