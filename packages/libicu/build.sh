5e0da0f1d1281cec87bc4030d0072a184ac6e0d8
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-26 15:51:00 +0300

    nodejs/nodejs-lts: fix typo in ICU source URL

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD"
 # Function `icu::number::FormattedNumber::getAllFieldPositions` was removed
 # from v67 and that breaks packages nodejs, nodejs-lts.
 TERMUX_PKG_VERSION=66.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
 TERMUX_PKG_SHA256=52a3f2209ab95559c1cf0a14f24338001f389615bf00e2585ef3dbc43ecf0a2e
 TERMUX_PKG_DEPENDS="libc++"
