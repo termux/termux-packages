84c586afbd8c756951c8dcc02e328272595dc0d5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-26 15:51:00 +0300

    nodejs/nodejs-lts: fix typo in ICU source URL

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -37,7 +37,7 @@ termux_step_post_extract_package() {
 termux_step_host_build() {
 	local ICU_VERSION=66.1
 	local ICU_TAR=icu4c-${ICU_VERSION//./_}-src.tgz
-	local ICU_DOWNLOAD=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/$ICU_TAR
+	local ICU_DOWNLOAD=https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION//./-}/$ICU_TAR
 	termux_download \
 		$ICU_DOWNLOAD\
 		$TERMUX_PKG_CACHEDIR/$ICU_TAR \
