86f4375e925bc30cea18af8ff6e6e39fb0a4e016
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-27 16:28:51 +0300

    azpainter: update to 2.1.6

--- x11-packages/azpainter/build.sh
+++ x11-packages/azpainter/build.sh
@@ -2,11 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Symbian9/azpainter
 TERMUX_PKG_DESCRIPTION="Full color painting software for Unix-like systems for illustration drawing (unofficial)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-_COMMIT=4bf18c83bb3edc5c0a2c557095ddee61b0b66a7a
-TERMUX_PKG_VERSION=1:2.1.5
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://github.com/Symbian9/azpainter/archive/${_COMMIT}.tar.gz
-TERMUX_PKG_SHA256=84c150611407cdbdb757f0e872bb3da76892fd3c1ea45931385d3c1f9619e405
+TERMUX_PKG_VERSION=1:2.1.6
+TERMUX_PKG_SRCURL=https://github.com/Symbian9/azpainter/releases/download/v${TERMUX_PKG_VERSION:2}/azpainter-${TERMUX_PKG_VERSION:2}.tar.xz
+TERMUX_PKG_SHA256=85f3f935e76b420f2e5e272514368fdfbe4c25c01daa1e161ac28a6e1edb0d2c
 TERMUX_PKG_DEPENDS="fontconfig, hicolor-icon-theme, libandroid-shmem, libjpeg-turbo, libxfixes, libxi"
 TERMUX_PKG_BUILD_IN_SRC=true
 
