a3258da1f1a9a746e8a3b22e777e134b5967a597
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-04-27 21:39:47 +0300

    espeak: build with pcaudiolib
    Add package 'pcaudiolib' and use it as dependency for espeak.
    
    Issue: https://github.com/termux/termux-packages/issues/5213.

---
+++ packages/pcaudiolib/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/espeak-ng/pcaudiolib
+TERMUX_PKG_DESCRIPTION="Portable C Audio Library"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_SRCURL=https://github.com/espeak-ng/pcaudiolib/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=699a5a347b1e12dc5b122e192e19f4db01621826bf41b9ebefb1cbc63ae2180b
+TERMUX_PKG_DEPENDS="pulseaudio"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
