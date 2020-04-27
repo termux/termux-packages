b222011541f68cd3daed405b736c425dce003ba0
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-04-27 16:37:43 +0300

    openttd: update to 1.10.1

--- x11-packages/openttd/build.sh
+++ x11-packages/openttd/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.openttd.org/
 TERMUX_PKG_DESCRIPTION="An engine for running Transport Tycoon Deluxe"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=1.9.3
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=1.10.1
 TERMUX_PKG_SRCURL=https://github.com/OpenTTD/OpenTTD/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1bbbc65c226d9beaf7aa68d36593152c39563a4802c823eee4faec2cd07e2bc2
+TERMUX_PKG_SHA256=cea59318607d51aef9b9de114bbb4f7d0d4bb0b1dd40e9dd4386469ed44eeeb4
 TERMUX_PKG_DEPENDS="desktop-file-utils, fontconfig, hicolor-icon-theme, libicu, liblzma, liblzo, libpng, openttd-gfx, openttd-msx, openttd-sfx, sdl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
