247c293098d55862b2b3869282cd2a75e2027f90
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-29 01:03:05 +0200

    xmlsec: Update from 1.2.29 to 1.2.30

--- packages/xmlsec/build.sh
+++ packages/xmlsec/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.aleksey.com/xmlsec/
 TERMUX_PKG_DESCRIPTION="XML Security Library"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=1.2.29
+TERMUX_PKG_VERSION=1.2.30
 TERMUX_PKG_SRCURL=http://www.aleksey.com/xmlsec/download/xmlsec1-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b1d1deba966019930f608d1f2b95c40ca3450f1393bcd3a3c001a8ba1d2839ab
+TERMUX_PKG_SHA256=2d84360b03042178def1d9ff538acacaed2b3a27411db7b2874f1612ed71abc8
 TERMUX_PKG_DEPENDS="libxslt, openssl, libgcrypt, libgpg-error, libxml2"
 TERMUX_PKG_BREAKS="xmlsec-dev"
 TERMUX_PKG_REPLACES="xmlsec-dev"
