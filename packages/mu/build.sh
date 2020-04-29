38b498a4974b0de700f70aedb1331ccd407a5d3c
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-29 14:23:16 +0200

    mu: Fix i686 build error

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=1.4.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=f819db50a21eb200f015ff3486e9bb9db51e0a616aa9dd257b6654512c7c782a
 TERMUX_PKG_DEPENDS="glib, libc++, libxapian, libgmime"
