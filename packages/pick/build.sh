33bea46c3a6e8a5a6a8c7c633384d9cfda6075ed
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-29 11:48:57 +0200

    pick: Update from 3.0.1 to 4.0.0

--- packages/pick/build.sh
+++ packages/pick/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/calleerlandsson/pick
 TERMUX_PKG_DESCRIPTION="Utility to choose one option from a set of choices with fuzzy search functionality"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_VERSION=4.0.0
 TERMUX_PKG_SRCURL=https://github.com/calleerlandsson/pick/releases/download/v${TERMUX_PKG_VERSION}/pick-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=668c863751f94ad90e295cf861a80b4d94975e06645f401d7f82525e607c0266
+TERMUX_PKG_SHA256=de768fd566fd4c7f7b630144c8120b779a61a8cd35898f0db42ba8af5131edca
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
