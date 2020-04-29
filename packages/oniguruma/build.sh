d153c9a3ed3a49ec23a5da53a8979456e38a47ea
Fredrik Fornwall <fredrik@fornwall.net>
2020-04-29 11:49:12 +0200

    oniguruma: Update from 6.9.4 to 6.9.5

--- packages/oniguruma/build.sh
+++ packages/oniguruma/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/kkos/oniguruma
-TERMUX_PKG_DESCRIPTION="regular expressions library"
-TERMUX_PKG_VERSION=6.9.4
-TERMUX_PKG_SRCURL=https://github.com/kkos/oniguruma/releases/download/v6.9.4/onig-6.9.4.tar.gz
-TERMUX_PKG_SHA256=4669d22ff7e0992a7e93e116161cac9c0949cd8960d1c562982026726f0e6d53
+TERMUX_PKG_DESCRIPTION="Regular expressions library"
+TERMUX_PKG_VERSION=6.9.5
+TERMUX_PKG_SRCURL=https://github.com/kkos/oniguruma/releases/download/v$TERMUX_PKG_VERSION/onig-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2f25cc3165e6da4b12dcabdb6b77c48f436d835e127ec2e3cad7abae9ea8e9a6
 TERMUX_PKG_LICENSE="BSD"
 
