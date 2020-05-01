5165e81efab536ca7f32497a24ba143a6f12d92c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-01 14:31:45 +0300

    gnunet: drop no-in6_ifreq-redef.patch

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://gnunet.org
 TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.12.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_DEPENDS="gnurl, libgcrypt, libgmp, libidn, libjansson, libltdl, libmicrohttpd, libsqlite, libunistring"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_have_decl_struct_in6_ifreq=yes"
@@ -28,5 +29,6 @@ termux_step_extract_package() {
 }
 
 termux_step_pre_configure() {
+	CPPFLAGS+=" -D_LINUX_IN6_H"
 	./bootstrap
 }
