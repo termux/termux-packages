76f38c644a03ff85a856d6697fd4e2fb6ec07a54
Henrik Grimler <henrik@grimler.se>
2020-05-01 23:37:22 +0200

    texlive: mv to science-packages instead (#5231)
    * texlive: mv to science-packages
    
    Might help decrease the bandwidth a little bit
    
    * texlive-bin: mv binaries to subfolder bin/texlive/
    
    New package texlive-installer needs to be given a directory with texlive-binaries and then parses it to look for various programs

--- packages/texlive/texlive-langcyrillic.subpackage.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-langcyrillic"
-TERMUX_SUBPKG_DEPENDS="texlive-fontsextra, texlive-fontsrecommended, texlive-langgreek, texlive-latexrecommended"
-TERMUX_SUBPKG_INCLUDE=$(python3 $TERMUX_SCRIPTDIR/packages/texlive/parse_tlpdb.py langcyrillic $TERMUX_PKG_TMPDIR/texlive.tlpdb)
-TERMUX_SUBPKG_CONFLICTS="texlive-bin (<< 20190410), texlive (<< 20190410)"
-
-termux_step_create_subpkg_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo mktexlsr >> postinst
-}
--- packages/texlive/texlive-mathscience.subpackage.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-mathscience"
-TERMUX_SUBPKG_DEPENDS="texlive-langgreek"
-TERMUX_SUBPKG_INCLUDE=$(python3 $TERMUX_SCRIPTDIR/packages/texlive/parse_tlpdb.py mathscience $TERMUX_PKG_TMPDIR/texlive.tlpdb)
-TERMUX_SUBPKG_CONFLICTS="texlive-bin (<< 20190410), texlive (<= 20190410-2)"
-
-termux_step_create_subpkg_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo mktexlsr >> postinst
-}
