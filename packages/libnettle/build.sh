0a294cff0823668e9e6449b30662f3eb01d7c71e
Henrik Grimler <henrik@grimler.se>
2020-05-02 11:00:47 +0200

    libnettle: update to 3.6

--- packages/libnettle/build.sh
+++ packages/libnettle/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.lysator.liu.se/~nisse/nettle/
 TERMUX_PKG_DESCRIPTION="Cryptographic library that is designed to fit easily in more or less any context"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.5.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.6
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/nettle/nettle-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=75cca1998761b02e16f2db56da52992aef622bf55a3b45ec538bc2eedadc9419
+TERMUX_PKG_SHA256=d24c0d0f2abffbc8f4f34dcf114b0f131ec3774895f3555922fe2f40f3d5e3f1
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_BREAKS="libnettle-dev"
 TERMUX_PKG_REPLACES="libnettle-dev"
