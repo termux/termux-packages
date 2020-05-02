120f7ed090002cb8578ef6aa88d0f245e3aae547
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-02 15:03:13 +0300

    rebuild packages which may be broken due to libnettle (dep of libgnutls)
    soname of libnettle was changed.

--- packages/tintin++/build.sh
+++ packages/tintin++/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://tintin.mudhalla.net
 TERMUX_PKG_DESCRIPTION="Classic text-based MUD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.02.02
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/tintin/files/tintin-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=c5d8b6c930ec0beb9f45de434e079dddb17b48f8a3acff08acbc9d1bd15dd487
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_file__dev_ptmx=no"
