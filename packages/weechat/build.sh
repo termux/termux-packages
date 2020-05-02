2540593358cfd4550326211b28b19eecd90a3fb9
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-05-02 15:03:13 +0300

    rebuild packages which may be broken due to libnettle (dep of libgnutls)
    soname of libnettle was changed.

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1f01f60013145358bb82174376fced8890d6e51828134febcd87c882d2a1ff76
 TERMUX_PKG_DEPENDS="libiconv, ncurses, libgcrypt, libcurl, libgnutls, libandroid-support, zlib"
