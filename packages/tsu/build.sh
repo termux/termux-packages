667d4e2d7b8945c2268c379a823da9de8efdc180
Cswl C <12976647+cswl@users.noreply.github.com>
2020-05-02 09:53:41 +0545

    tsu: Update to v8 release of v8.2.0

--- packages/tsu/build.sh
+++ packages/tsu/build.sh
@@ -1,40 +1,21 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/cswl/tsu
 TERMUX_PKG_DESCRIPTION="A su wrapper for Termux"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=2.3
+TERMUX_PKG_VERSION=8.2.0
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_extract_package() {
-	local CHECKED_OUT_FOLDER=$TERMUX_PKG_CACHEDIR/tsu-checkout-$TERMUX_PKG_VERSION
-	if [ ! -d $CHECKED_OUT_FOLDER ]; then
-		local TMP_CHECKOUT=$TERMUX_PKG_TMPDIR/tmp-checkout
-		rm -Rf $TMP_CHECKOUT
-		mkdir -p $TMP_CHECKOUT
-
-		git clone --depth 1 \
-			--branch master \
-			https://github.com/cswl/tsu.git \
-			$TMP_CHECKOUT
-		cd $TMP_CHECKOUT
-		git fetch --all --tags --prune
-		git checkout "tags/v$TERMUX_PKG_VERSION" -b "$TERMUX_PKG_VERSION"
-		mv $TMP_CHECKOUT $CHECKED_OUT_FOLDER
-	fi
-
-	mkdir $TERMUX_PKG_SRCDIR
-	cd $TERMUX_PKG_SRCDIR
-	cp -Rf $CHECKED_OUT_FOLDER/* .
-}
+TERMUX_PKG_SRCURL=(https://github.com/cswl/tsu/archive/v$TERMUX_PKG_VERSION.tar.gz)
+TERMUX_PKG_SHA256=(583c5c2a9a2545ae35bddcb50e9759b468f6a37aed68b90029d005403d3f4e24)
 
 termux_step_make() {
-	:
+	python3 ./extract_usage.py
 }
 
 termux_step_make_install() {
-	cp tsu $TERMUX_PREFIX/bin/tsu
-	chmod +x $TERMUX_PREFIX/bin/tsu
-
-	cp tsudo $TERMUX_PREFIX/bin/tsudo
-	chmod +x $TERMUX_PREFIX/bin/tsudo
+	# There is no install.sh script in the repository for now
+	mkdir -p "$TERMUX_PREFIX/bin"
+	cp $TERMUX_PKG_SRCDIR/tsu "$TERMUX_PREFIX/bin/tsu"
+	chmod 0755 "$TERMUX_PREFIX/bin/tsu"
+	# sudo - is an included addon in tsu now
+	ln -sf "$TERMUX_PREFIX/bin/tsu" "$TERMUX_PREFIX/bin/sudo"
 }
