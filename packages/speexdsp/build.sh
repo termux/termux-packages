f506173da29d7c48be4bf3505781891e07cdf058
Henrik Grimler <henrik@grimler.se>
2020-05-06 11:46:58 +0200

    speexdsp: switch to github for src tar
    xiph.org is currently down

--- packages/speexdsp/build.sh
+++ packages/speexdsp/build.sh
@@ -3,9 +3,13 @@ TERMUX_PKG_DESCRIPTION="Speex audio processing library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=1.2.0
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/speex/speexdsp-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=682042fc6f9bee6294ec453f470dadc26c6ff29b9c9e9ad2ffc1f4312fd64771
+TERMUX_PKG_SRCURL=https://github.com/xiph/speexdsp/archive/SpeexDSP-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d7032f607e8913c019b190c2bccc36ea73fc36718ee38b5cdfc4e4c0a04ce9a4
 TERMUX_PKG_BREAKS="speexdsp-dev"
 TERMUX_PKG_REPLACES="speexdsp-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-neon"
 TERMUX_PKG_RM_AFTER_INSTALL="share/doc/speexdsp/manual.pdf"
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
