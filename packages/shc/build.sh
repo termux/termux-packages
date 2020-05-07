9293d952d04e26113bad13670f78835b13694b65
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-05-07 15:23:20 +0530

    shc : build.sh

--- packages/shc/build.sh
+++ packages/shc/build.sh
@@ -8,7 +8,27 @@ TERMUX_PKG_SHA256=7d7fa6a9f5f53d607ab851d739ae3d3b99ca86e2cb1425a6cab9299f673aee
 TERMUX_PKG_DEPENDS="clang"
 
 termux_step_make_install() {
-        $TERMUX_PKG_SRCDIR/configure --prefix=$TERMUX_PREFIX
+        cd $TERMUX_PKG_BUILDDIR
+
+
+                if [ $TERMUX_ARCH = "arm" ]; then
+                        _ARCH="armeabi-v7a"
+
+                elif [ $TERMUX_ARCH = "i686" ]; then
+                        _ARCH="x86"
+             
+                elif [ $TERMUX_ARCH = "x86_64" ]; then
+                        _ARCH="x86_64"
+
+                elif [ $TERMUX_ARCH = "aarch64" ]; then
+                        _ARCH=$TERMUX_ARCH
+
+                else
+                        termux_error_exit "Unsupported arch: $TERMUX_ARCH"
+                fi
+
+  
+        $TERMUX_PKG_SRCDIR/configure --prefix=$TERMUX_PREFIX --host=${_ARCH}
         make
         make install
 }
