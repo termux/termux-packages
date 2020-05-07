e86f0a410168c9c1d19736976520b79027cc4a0d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-07 23:17:31 +0300

    swaks: build manpage

--- packages/swaks/build.sh
+++ packages/swaks/build.sh
@@ -2,12 +2,16 @@ TERMUX_PKG_HOMEPAGE=http://jetmore.org/john/code/swaks/
 TERMUX_PKG_DESCRIPTION="Swiss Army Knife for SMTP"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=20190914.0
-TERMUX_PKG_SRCURL=https://github.com/jetmore/swaks/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0ea331fdb036dda84bd67b4c9800250521d13224c2ccf0455f90d11e57facc5f
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://jetmore.org/john/code/swaks/files/swaks-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=5733a51a5c3f74f62274c17dc825f177c22ed52703c97c3b23a5354d7ec15c89
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	install -Dm700 swaks "$TERMUX_PREFIX"/bin/swaks
+	cd doc
+	pod2man ref.pod swaks.1
+	install -Dm600 swaks.1 "$TERMUX_PREFIX"/share/man/man1/swaks.1
 }
