d158083ee5f364c4360aa23256f0c0b422798ef6
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-05-08 02:17:08 +0300

    Update build.sh

--- packages/wrk/build.sh
+++ packages/wrk/build.sh
@@ -6,5 +6,23 @@ TERMUX_PKG_SRCURL=https://github.com/wg/wrk/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6fa1020494de8c337913fd139d7aa1acb9a020de6f7eb9190753aa4b1e74271e
 TERMUX_PKG_DEPENDS="openssl, luajit"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="WITH_OPENSSL=$TERMUX_PREFIX WITH_LUAJIT=$TERMUX_PREFIX"
 
+termux_step_make() {
+	local _ARCH
+
+	if [ "$TERMUX_ARCH" = "i686" ]; then
+		_ARCH="x86"
+	elif [ "$TERMUX_ARCH" = "x86_64" ]; then
+		_ARCH="x64"
+	elif [ "$TERMUX_ARCH" = "aarch64" ]; then
+		_ARCH="arm64"
+	else
+		_ARCH=$TERMUX_ARCH
+	fi
+
+	make WITH_OPENSSL=$TERMUX_PREFIX WITH_LUAJIT=$TERMUX_PREFIX _ARCH=$_ARCH
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "$TERMUX_PREFIX"/bin wrk
+}
