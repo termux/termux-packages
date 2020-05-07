4e14ef144956f0b15e1a7dd175be719b052ffc78
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-05-07 18:25:50 +0300

    undo changes for sleuthkit/build.sh

--- packages/sleuthkit/build.sh
+++ packages/sleuthkit/build.sh
@@ -1,5 +1,4 @@
 TERMUX_PKG_HOMEPAGE=https://sleuthkit.org/
-<<<<<<< HEAD
 TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library for digital forensics tools. "
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="licenses/*"
