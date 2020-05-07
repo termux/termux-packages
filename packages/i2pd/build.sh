f2ae0d1c076ed7e7947dae735881fd59c7de212e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-08 00:50:59 +0300

    i2pd: install configs

--- packages/i2pd/build.sh
+++ packages/i2pd/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://i2pd.website/
 TERMUX_PKG_DESCRIPTION="A full-featured C++ implementation of the I2P router"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=2.31.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/PurpleI2P/i2pd/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=7e37abcf49e9f59ef6939069f4d74fc6bf29b09deec111bd3561021fc1349528
 TERMUX_PKG_DEPENDS="boost, miniupnpc, openssl, zlib"
@@ -9,7 +10,34 @@ TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWITH_UPNP:BOOL=ON"
 TERMUX_PKG_RM_AFTER_INSTALL="src"
 
+TERMUX_PKG_CONFFILES="
+etc/i2pd/i2pd.conf
+etc/i2pd/tunnels.conf
+"
+
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+="/build"
 	CXXFLAGS="${CXXFLAGS/-Oz/-O2}"
+}
+
+termux_step_post_make_install() {
+	cd $TERMUX_PKG_SRCDIR/../
+
+	install -Dm600 -t "$TERMUX_PREFIX"/etc/i2pd \
+		./contrib/i2pd.conf \
+		./contrib/tunnels.conf
+
+	local _file _dir
+	while read -r -d '' _file; do
+		_dir="${_file#contrib/certificates}"
+		_dir="${_dir%/*}"
+		install -Dm600 "$_file" -t "${TERMUX_PREFIX}/share/i2pd/certificates/${_dir}"
+	done < <(find contrib/certificates -type f -print0)
+
+	install -Dm600 -t "${TERMUX_PREFIX}"/share/doc/i2pd/tunnels.d \
+		./contrib/tunnels.d/README \
+		./contrib/tunnels.d/IRC-Ilita.conf \
+		./contrib/tunnels.d/IRC-Irc2P.conf
+
+	install -Dm600 -t "${TERMUX_PREFIX}"/share/man/man1 ./debian/i2pd.1
 }
