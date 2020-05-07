21c27144d2428c800e43f27991024bd5c5d878d2
FF <dd@haha.local>
2020-05-07 20:53:23 +0800

    update DESCRIPTION

--- packages/sleuthkit/build.sh
+++ packages/sleuthkit/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://sleuthkit.org/
-TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library and collection of command line digital forensics tools that allow you to investigate volume and file system data. "
+TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library for digital forensics tools. "
 TERMUX_PKG_LICENSE="MIT, BSD 3-clause, IBM, Common Public License"
 TERMUX_PKG_VERSION=4.9.0
 TERMUX_PKG_SRCURL=https://github.com/sleuthkit/sleuthkit/archive/sleuthkit-${TERMUX_PKG_VERSION}.tar.gz
@@ -7,4 +7,6 @@ TERMUX_PKG_SHA256=00faf25c8ffd9271b1c144586a4197fa3ed4cefa70adab1d73ad39376ae009
 TERMUX_PKG_DEPENDS="libsqlite, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-
+termux_step_pre_configure() {
+	./bootstrap
+}
