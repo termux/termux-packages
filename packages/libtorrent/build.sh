fa6b755527fa7e0c7f3cc0d09ea9e42f9eb4be92
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-05-07 20:15:41 +0300

    Update build.sh

--- packages/libtorrent/build.sh
+++ packages/libtorrent/build.sh
@@ -6,7 +6,8 @@ TERMUX_PKG_VERSION=0.13.8
 TERMUX_PKG_SRCURL=https://github.com/rakshasa/libtorrent/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0f6c2e7ffd3a1723ab47fdac785ec40f85c0a5b5a42c1d002272205b988be722
 TERMUX_PKG_DEPENDS="openssl"
-termux_step_pre_configure() {
-        ./autogen.sh
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-aligned=true"
 
+termux_step_pre_configure() {
+	./autogen.sh
 }
