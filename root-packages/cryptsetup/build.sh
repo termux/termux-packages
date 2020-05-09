0a3a67f45e9293f1ac73bb6a78b1b433875be483
Henrik Grimler <henrik@grimler.se>
2020-05-09 11:01:00 +0200

    cryptsetup: bump after json-c update

--- root-packages/cryptsetup/build.sh
+++ root-packages/cryptsetup/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/cryptsetup/cryptsetup/
 TERMUX_PKG_DESCRIPTION="Userspace setup tool for transparent encryption of block devices using dm-crypt"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.3.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/linux/utils/cryptsetup/v${TERMUX_PKG_VERSION:0:3}/cryptsetup-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a89e13dff0798fd0280e801d5f0cc8cfdb2aa5b1929bec1b7322e13d3eca95fb
 TERMUX_PKG_DEPENDS="json-c, libdevmapper, libgcrypt, libpopt, libuuid, util-linux, openssl, libiconv"
