7d026f5c1a40f292152c8d0acb2fabe999d72e09
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-05-09 16:22:19 +0530

    new package: youtubedr (#5268)

---
+++ packages/youtubedr/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/kkdai/youtube
+TERMUX_PKG_DESCRIPTION="Download youtube video in Golang"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_SRCURL=https://github.com/kkdai/youtube/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=20946b843b9584dd044ec4d8b412365511338c568a37a6170acabea2bb3e2661
+
+termux_step_make() {
+        termux_setup_golang
+
+        cd "$TERMUX_PKG_SRCDIR"
+
+        export GOPATH="${TERMUX_PKG_BUILDDIR}"
+        mkdir -p "${GOPATH}/src/github.com/kkdai/"
+        cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/kkdai/youtube"
+        cd "${GOPATH}/src/github.com/kkdai/youtube/"
+        go get -d -v
+        go build
+}
+
+termux_step_make_install() {
+        install -Dm700 \
+                "$GOPATH"/src/github.com/kkdai/youtube/youtubedr/ \
+                "$TERMUX_PREFIX"/bin/
+}
