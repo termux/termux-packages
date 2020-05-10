4d7f60cb0be4887fe2a8908f3fdfee21170b005f
Krishna Kanhaiya <kkrishna7638@gmail.com>
2020-05-11 01:52:57 +0530

    gh: GitHub’s official command line tool  (#5274)

---
+++ packages/gh/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://cli.github.com/
+TERMUX_PKG_DESCRIPTION="GitHub’s official command line tool"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=0.7.0
+TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c8966ee2c9fe8138ae7773c66b9a85dd2bfbffc7ca26ce189b294ae0b3e4c05c
+
+termux_step_make() {
+	termux_setup_golang
+
+	cd "$TERMUX_PKG_SRCDIR"
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/cli/
+	mkdir -p "$TERMUX_PREFIX"/share/doc/gh
+	cp -a "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/cli/cli
+	cd "$GOPATH"/src/github.com/cli/cli/cmd/gh
+	go get -d -v
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/cli/cli/cmd/gh/gh
+	install -Dm600 -t "$TERMUX_PREFIX"/share/doc/gh/ "$TERMUX_PKG_SRCDIR"/docs/*
+}
