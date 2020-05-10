ee8c492b3a3e9993b6c2fa2c4198fa6c9a943ee7
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-05-10 23:22:24 +0300

    Update build.sh

--- packages/gh/build.sh
+++ packages/gh/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=0.7.0
 TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/v${TERMUX_PKG_VERSION}.tar.gz
-#TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/v0.7.0.tar.gz
 TERMUX_PKG_SHA256=c8966ee2c9fe8138ae7773c66b9a85dd2bfbffc7ca26ce189b294ae0b3e4c05c
 
 termux_step_make() {
@@ -12,28 +11,16 @@ termux_step_make() {
 
 	cd "$TERMUX_PKG_SRCDIR"
 
-	export GOPATH="${TERMUX_PKG_BUILDDIR}"
-	mkdir -p "${GOPATH}/src/github.com/cli/"
-	mkdir -p "$TERMUX_PREFIX/share/doc/gh"
-	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/cli/cli"
-	cd "${GOPATH}/src/github.com/cli/cli/cmd/gh"
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/cli/
+	mkdir -p "$TERMUX_PREFIX"/share/doc/gh
+	cp -a "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/cli/cli
+	cd "$GOPATH"/src/github.com/cli/cli/cmd/gh
 	go get -d -v
 	go build
 }
 
 termux_step_make_install() {
-	install -Dm700 "$GOPATH/src/github.com/cli/cli/cmd/gh/gh" \
-			"$TERMUX_PREFIX/bin"
-
-	install $TERMUX_PKG_SRCDIR/docs/*  \
-			"$TERMUX_PREFIX/share/doc/gh"
-#
-#	install $GOPATH/src/github/cli/cli/docs/gh-vs-hub.md  \ 
-#			"$TERMUX_PREFIX/share/doc/"
-#
-#	install $GOPATH/src/github/cli/cli/docs/releasing.md  \ 
-#			"$TERMUX_PREFIX/share/doc/"
-#			
-#	install $GOPATH/src/github/cli/cli/docs/source.md  \
-#			 "$TERMUX_PREFIX/share/doc/"
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/cli/cli/cmd/gh/gh
+	install -Dm600 -t "$TERMUX_PREFIX"/share/doc/gh/ "$TERMUX_PKG_SRCDIR"/docs/*
 }
