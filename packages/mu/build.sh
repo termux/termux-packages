cf2088b94292579afb2a7ec15c1b44e6183f78ed
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-11 22:56:41 +0300

    mu: update to 1.4.5

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.4.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.4.5
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=f819db50a21eb200f015ff3486e9bb9db51e0a616aa9dd257b6654512c7c782a
+TERMUX_PKG_SHA256=3e16ad39b2bc9af5dfe2e47f3aa76b3b7c3e7af245e89c2d189a9b5c43c9939e
 TERMUX_PKG_DEPENDS="glib, libc++, libxapian, libgmime"
