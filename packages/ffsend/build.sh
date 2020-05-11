655753cac01918a774fabf61a030ed51d31e52dc
timvisee <tim@visee.me>
2020-05-11 22:26:46 +0200

    ffsend: Update from 0.2.59 to 0.2.61

--- packages/ffsend/build.sh
+++ packages/ffsend/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/timvisee/ffsend
 TERMUX_PKG_DESCRIPTION="A fully featured Firefox Send client"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.2.59
+TERMUX_PKG_VERSION=0.2.61
 TERMUX_PKG_SRCURL=https://gitlab.com/timvisee/ffsend/-/archive/v$TERMUX_PKG_VERSION/ffsend-v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=02df26f8f880b9f991656c6f4cf0c95bee6636dbe1ce7c120ae424c0cbfe81c4
+TERMUX_PKG_SHA256=5444bc94c4a1177264f9d82e999b2c1b9cc05a5ee66e17e5c1a6f667fa014865
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features send2,send3,history,archive,qrcode,urlshorten,infer-command"
