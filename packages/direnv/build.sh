1119a00f3166ee5e8283b18e68039ce39b0006d3
Fredrik Fornwall <fredrik@fornwall.net>
2020-05-11 09:44:19 +0200

    direnv: Update from 2.21.2 to 2.21.3

--- packages/direnv/build.sh
+++ packages/direnv/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/direnv/direnv
 TERMUX_PKG_DESCRIPTION="Environment switcher for shell"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
-TERMUX_PKG_VERSION=2.21.2
+TERMUX_PKG_VERSION=2.21.3
 TERMUX_PKG_SRCURL=https://github.com/direnv/direnv/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ce5551b09f71fe26111bbf94a09cfdbce14794ec9a723015b67efea804986d68
+TERMUX_PKG_SHA256=012651a79e47150de4a386d1c3c81a017d5ceac14f5a0c24b0596a2215cde8be
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
