35582e67d25780b49d00c8b9d44db2a96f3f4a56
Leonid Plyushch <leonid.plyushch@gmail.com>
2020-05-11 22:50:33 +0300

    freetype: update to 2.10.3

--- packages/freetype/build.sh
+++ packages/freetype/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.freetype.org
 TERMUX_PKG_DESCRIPTION="Software font engine capable of producing high-quality output"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.10.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.10.2
 TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/freetype/freetype-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f
+TERMUX_PKG_SHA256=1543d61025d2e6312e0a1c563652555f17378a204a61e99928c9fcef030a2d8b
 TERMUX_PKG_DEPENDS="libbz2, libpng, zlib"
 TERMUX_PKG_BREAKS="freetype-dev"
 TERMUX_PKG_REPLACES="freetype-dev"
