26891b7efb718b3222b75ce091acdcfd363a393f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-13 11:33:41 +0300

    c-ares: update to 1.16.1

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://c-ares.haxx.se
 TERMUX_PKG_DESCRIPTION="Library for asynchronous DNS requests (including name resolves)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.16.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.16.1
 TERMUX_PKG_SRCURL=https://github.com/c-ares/c-ares/archive/cares-${TERMUX_PKG_VERSION//./_}.tar.gz
-TERMUX_PKG_SHA256=b045ef78cda05edd4c677ab351f209315aab8fb041051031bdbe185aaf74baf3
+TERMUX_PKG_SHA256=870962cc8f6b352303c404ce848e2ea1f1072f3c0a940042209a72179511c08c
 TERMUX_PKG_BREAKS="c-ares-dev"
 TERMUX_PKG_REPLACES="c-ares-dev"
 # Build with cmake to install cmake/c-ares/*.cmake files:
