2db4c998479f380382d8b3a7024217d45b08b73e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-13 12:00:55 +0300

    translate-shell: update to 0.9.6.12

--- packages/translate-shell/build.sh
+++ packages/translate-shell/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.soimort.org/translate-shell
 TERMUX_PKG_DESCRIPTION="Command-line translator using Google Translate, Bing Translator, Yandex.Translate, etc."
 TERMUX_PKG_LICENSE="Public Domain"
-TERMUX_PKG_VERSION=0.9.6.11
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.9.6.12
 TERMUX_PKG_SRCURL=https://github.com/soimort/translate-shell/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=589505248212726dff2b3e8828514036491f019fcee8657c0d94bb1a5dac6c5b
+TERMUX_PKG_SHA256=4c4843a8c66276190535b8435775ecb5d9c8286083a33cdbe2db608eba93ca97
 TERMUX_PKG_DEPENDS="bash, curl, gawk, less, rlwrap"
 # hunspell - spell checking
 # mpv - text-to-speech functionality
