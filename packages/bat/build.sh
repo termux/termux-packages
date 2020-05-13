4d07a3509d7002f2fe8ad05b4e93d582e0af3af8
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-13 11:29:33 +0300

    bat: update to 0.15.1

--- packages/bat/build.sh
+++ packages/bat/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/bat
 TERMUX_PKG_DESCRIPTION="A cat(1) clone with wings"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.15.0
+TERMUX_PKG_VERSION=0.15.1
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/bat/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=702b330d2938426679b3b0d50334b1518b4fb1d779220f2e32e68dfa1bb9fc0f
+TERMUX_PKG_SHA256=6f64899870bb16abd82a86c6a34c165f78603514e6ebb8ddf920b59095eba57d
 # bat calls less with '--RAW-CONTROL-CHARS' which busybox less does not support:
 TERMUX_PKG_DEPENDS="less, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
