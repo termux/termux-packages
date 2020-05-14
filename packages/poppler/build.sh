402ccd1b6cc4c1d0a5576ad48119501caa025ddf
Henrik Grimler <henrik@grimler.se>
2020-05-14 21:59:29 +0200

    poppler: update to 0.88.0
    Works with texlive without updated patches it seems

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.87.0
+TERMUX_PKG_VERSION=0.88.0
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6f602b9c24c2d05780be93e7306201012e41459f289b8279a27a79431ad4150e
+TERMUX_PKG_SHA256=b4453804e9a5a519e6ceee0ac8f5efc229e3b0bf70419263c239124474d256c7
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost"
 TERMUX_PKG_BREAKS="poppler-dev"
