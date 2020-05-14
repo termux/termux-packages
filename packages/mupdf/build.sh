492762f4cb8099bcde2185887c4147ce1419230e
Henrik Grimler <henrik@grimler.se>
2020-05-14 21:41:11 +0200

    mupdf: update to 1.17.0

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://mupdf.com/
 TERMUX_PKG_DESCRIPTION="Lightweight PDF and XPS viewer (library)"
 TERMUX_PKG_LICENSE="AGPL-V3"
-TERMUX_PKG_VERSION=1.16.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.17.0
 TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.xz
-TERMUX_PKG_SHA256=6fe78184bd5208f9595e4d7f92bc8df50af30fbe8e2c1298b581c84945f2f5da
+TERMUX_PKG_SHA256=c935fb2593d9a28d9b56b59dad6e3b0716a6790f8a257a68fa7dcb4430bc6086
 TERMUX_PKG_DEPENDS="freetype, harfbuzz, jbig2dec, libjpeg-turbo, openjpeg, zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX build=release libs"
 TERMUX_PKG_BUILD_IN_SRC=true
