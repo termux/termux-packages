077bf9ccc8aa36269dea17387a799d01c7068d25
Nuruddin Ashr <uudashr@gmail.com>
2020-05-15 10:36:43 +0700

    Upgrade golang to version 1.14.3

--- scripts/build/setup/termux_setup_golang.sh
+++ scripts/build/setup/termux_setup_golang.sh
@@ -1,7 +1,7 @@
 # Utility function for golang-using packages to setup a go toolchain.
 termux_setup_golang() {
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		local TERMUX_GO_VERSION=go1.14.2
+		local TERMUX_GO_VERSION=go1.14.3
 		local TERMUX_GO_PLATFORM=linux-amd64
 
 		local TERMUX_BUILDGO_FOLDER=$TERMUX_COMMON_CACHEDIR/${TERMUX_GO_VERSION}
@@ -14,7 +14,7 @@ termux_setup_golang() {
 		rm -Rf "$TERMUX_COMMON_CACHEDIR/go" "$TERMUX_BUILDGO_FOLDER"
 		termux_download https://storage.googleapis.com/golang/${TERMUX_GO_VERSION}.${TERMUX_GO_PLATFORM}.tar.gz \
 			"$TERMUX_BUILDGO_TAR" \
-			6272d6e940ecb71ea5636ddb5fab3933e087c1356173c61f4a803895e947ebb3
+			1c39eac4ae95781b066c144c58e45d6859652247f7515f0d2cba7be7d57d2226
 
 		( cd "$TERMUX_COMMON_CACHEDIR"; tar xf "$TERMUX_BUILDGO_TAR"; mv go "$TERMUX_BUILDGO_FOLDER"; rm "$TERMUX_BUILDGO_TAR" )
 	else
