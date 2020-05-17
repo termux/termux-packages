68090512540fa7fdcc00b053898ef5165ce41518
Henrik Grimler <henrik@grimler.se>
2020-05-17 12:19:59 +0200

    sqlcipher: update to 4.4.0

--- packages/sqlcipher/build.sh
+++ packages/sqlcipher/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/sqlcipher/sqlcipher
 TERMUX_PKG_DESCRIPTION="SQLCipher is an SQLite extension that provides 256 bit AES encryption of database files"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=4.3.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.4.0
 TERMUX_PKG_SRCURL=https://github.com/sqlcipher/sqlcipher/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=fccb37e440ada898902b294d02cde7af9e8706b185d77ed9f6f4d5b18b4c305f
+TERMUX_PKG_SHA256=0924b2ae1079717954498bda78a30de20ce2a6083076b16214a711567821d148
 TERMUX_PKG_DEPENDS="libsqlite, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-tempstore=yes"
 
