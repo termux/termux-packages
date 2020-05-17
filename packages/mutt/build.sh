9c4e1f008e6188a951685b20b6a82430d4b52ab7
Henrik Grimler <henrik@grimler.se>
2020-05-17 10:33:59 +0200

    mutt: update to 1.14.1

--- packages/mutt/build.sh
+++ packages/mutt/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.mutt.org/
 TERMUX_PKG_DESCRIPTION="Mail client with patches from neomutt"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.14.0
+TERMUX_PKG_VERSION=1.14.1
 TERMUX_PKG_SRCURL=ftp://ftp.mutt.org/pub/mutt/mutt-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=df2e82a83ad38c9a9fd36f44c5ab2b726defa4bb1a17de794dd3800f3353f9f8
+TERMUX_PKG_SHA256=35d564c335f7364b8a43349fe0115a7cdc8ebf1ba8bf809d3fc51b9828844425
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, gdbm, openssl, libsasl, mime-support, zlib, libiconv"
 TERMUX_PKG_BUILD_IN_SRC=true
 
