0e39e07a0211a51b24595eecdafbe448e0bf8816
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-25 22:31:02 +0300

    ffmpeg: update to 4.2.3

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://ffmpeg.org
 TERMUX_PKG_DESCRIPTION="Tools and libraries to manipulate a wide range of multimedia formats and protocols"
 TERMUX_PKG_LICENSE="GPL-3.0"
 # NOTE: mpv has to be rebuilt and version bumped after updating ffmpeg.
-TERMUX_PKG_VERSION=4.2.2
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=4.2.3
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cb754255ab0ee2ea5f66f8850e1bd6ad5cac1cd855d0a2f4990fb8c668b0d29c
+TERMUX_PKG_SHA256=9df6c90aed1337634c1fb026fb01c154c29c82a64ea71291ff2da9aacb9aad31
 TERMUX_PKG_DEPENDS="libass, libbz2, libdav1d, libiconv, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma, libvidstab, libwebp"
 TERMUX_PKG_CONFLICTS="libav"
 TERMUX_PKG_BREAKS="ffmpeg-dev"
