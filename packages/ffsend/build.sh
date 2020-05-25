d5b1045c19a0672fe7abd60b14e6091be55ccc23
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-26 00:19:40 +0300

    ffsend: add crypto-openssl feature (for crypto backend)

--- packages/ffsend/build.sh
+++ packages/ffsend/build.sh
@@ -6,4 +6,4 @@ TERMUX_PKG_SRCURL=https://gitlab.com/timvisee/ffsend/-/archive/v$TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=fdd2fd28e29cf4034b210558b5b5049592bc970344cbf122637d37ef8dc77589
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features send2,send3,history,archive,qrcode,urlshorten,infer-command"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features crypto-openssl,send2,send3,history,archive,qrcode,urlshorten,infer-command"
