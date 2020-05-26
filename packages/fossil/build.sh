51f016c6d42f3762ba7537c825187cbfc7a9d184
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-26 16:01:02 +0300

    fossil: update to 2.11

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=2.10
+TERMUX_PKG_VERSION=2.11
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d8a3776d2ce77385ed5ff20a2776d13bb534fb2508e87351e14e94f91cd12b10
+TERMUX_PKG_SHA256=3d5a04bbe0075edd7f04c64ffc8f3af71d16565bf606b753f5d5752e48ff3631
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
