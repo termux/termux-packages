aef4cdc61f21dbfc7c5e7f4cc8cc42c31b326a3b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-26 16:56:32 +0300

    ledger: update to 3.2.1

--- packages/ledger/build.sh
+++ packages/ledger/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.ledger-cli.org
 TERMUX_PKG_DESCRIPTION="Powerful, double-entry accounting system"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.2.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.2.1
 TERMUX_PKG_SRCURL=https://github.com/ledger/ledger/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9a2f1886be0181bfa0a8b3ea05207d5c6e55497d7f821af3d7e60a8e53ba11d0
+TERMUX_PKG_SHA256=92bf09bc385b171987f456fe3ee9fa998ed5e40b97b3acdd562b663aa364384a
 TERMUX_PKG_DEPENDS="boost, libc++, libedit, libmpfr, libgmp"
 TERMUX_PKG_BREAKS="ledger-dev"
 TERMUX_PKG_REPLACES="ledger-dev"
