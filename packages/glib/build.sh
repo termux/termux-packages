acea27d67c1fb1e08006a3cfbd453bd7aacc3fc9
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-05-28 16:11:47 +0300

    glib: provide gdbus-codegen
    Issue https://github.com/termux/termux-packages/issues/5342.

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -2,17 +2,26 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=2.64.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=fe9cbc97925d14c804935f067a3ad77ef55c0bbe9befe68962318f5a767ceb22
-# libandroid-support to get langinfo.h in include path.
 TERMUX_PKG_DEPENDS="libffi, libiconv, pcre, libandroid-support, zlib"
 TERMUX_PKG_BREAKS="glib-dev"
 TERMUX_PKG_REPLACES="glib-dev"
-TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc lib/locale share/glib-2.0/gettext share/gdb/auto-load share/glib-2.0/codegen share/glib-2.0/gdb bin/gtester-report bin/glib-gettextize bin/gdbus-codegen"
-# Needed by pkg-config for glib-2.0:
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibmount=disabled
 -Diconv=external
+"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/locale
+share/gdb/auto-load
+share/glib-2.0/gdb
+share/glib-2.0/gettext
+share/gtk-doc
+bin/gtester-report
+bin/glib-gettextize
 "
 
 termux_step_pre_configure() {
