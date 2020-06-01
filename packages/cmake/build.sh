8c9f7fc4f851ff8df6f9532f822f06a29ce2a727
Fredrik Fornwall <fredrik@fornwall.net>
2020-06-01 23:44:10 +0200

    cmake: Update from 3.17.2 to 3.17.3

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.17.2
+TERMUX_PKG_VERSION=3.17.3
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fc77324c4f820a09052a7785549b8035ff8d3461ded5bbd80d252ae7d1cd3aa5
+TERMUX_PKG_SHA256=0bd60d512275dc9f6ef2a2865426a184642ceb3761794e6b65bff233b91d8c40
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, make, clang, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DKWSYS_LFS_WORKS=ON -DBUILD_CursesDialog=ON"
