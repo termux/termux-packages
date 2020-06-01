9662bc81ab6f9b2b761b2aa079a5124ea3371c4f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-01 14:49:43 +0300

    fix maintainer field for some packages

--- root-packages/libseccomp/build.sh
+++ root-packages/libseccomp/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/seccomp/libseccomp
 TERMUX_PKG_DESCRIPTION="Enhanced seccomp library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=2.4.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/seccomp/libseccomp/releases/download/v${TERMUX_PKG_VERSION}/libseccomp-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cf15d1421997fac45b936515af61d209c4fd788af11005d212b3d0fd71e7991d
 TERMUX_PKG_BREAKS="libseccomp-dev"
--- root-packages/wavemon/build.sh
+++ root-packages/wavemon/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/uoaerg/wavemon
 TERMUX_PKG_DESCRIPTION="Ncurses-based monitoring application for wireless network devices"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com>"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=0.9.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/uoaerg/wavemon/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=5ebd5b79d3b7c546bc16b95161872c699a75e9acdfc6e3f02ec48dad10802067
 TERMUX_PKG_DEPENDS="libcap, libnl, ncurses"
