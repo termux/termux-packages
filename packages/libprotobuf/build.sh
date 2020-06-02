2b06e823591674c2dc93b899a7c132a87889e141
Fredrik Fornwall <fredrik@fornwall.net>
2020-06-02 08:34:56 +0200

    libprotobuf: Update from 3.11.4 to 3.12.2

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/google/protobuf
 TERMUX_PKG_DESCRIPTION="Protocol buffers C++ library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.11.4
+TERMUX_PKG_VERSION=3.12.2
 TERMUX_PKG_SRCURL=https://github.com/google/protobuf/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a79d19dcdf9139fa4b81206e318e33d245c4c9da1ffed21c87288ed4380426f9
+TERMUX_PKG_SHA256=bb8ce9ba11eb7bccf080599fe7cad9cc461751c8dd1ba61701c0070d58cde973
 TERMUX_PKG_DEPENDS="libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev"
 TERMUX_PKG_REPLACES="libprotobuf-dev"
