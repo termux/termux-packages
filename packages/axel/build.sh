ff593aee6ae4ab80a7b7370f65ada5e703c703d5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-04 01:32:47 +0300

    axel: update to 2.17.9

--- packages/axel/build.sh
+++ packages/axel/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/axel-download-accelerator/axel
 TERMUX_PKG_DESCRIPTION="light command line download accelerator"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.17.8
+TERMUX_PKG_VERSION=2.17.9
 TERMUX_PKG_SRCURL=https://github.com/axel-download-accelerator/axel/releases/download/v${TERMUX_PKG_VERSION}/axel-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5e5ae1d4f8788e227ed96dab711cadcfb315d737b0e049c34954bd6dafe1b5b8
+TERMUX_PKG_SHA256=e5a06a155c28b01dd62ecc2b3d00ff1eb80b016e9f4c2627a49db4444b97b554
 TERMUX_PKG_DEPENDS="openssl"
