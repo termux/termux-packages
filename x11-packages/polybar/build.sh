43a659a53328181e2fb8dc5a39dc094b5a90475d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-03 22:42:09 +0300

    polybar: update to 3.4.3

--- x11-packages/polybar/build.sh
+++ x11-packages/polybar/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://polybar.github.io
 TERMUX_PKG_DESCRIPTION="A fast and easy-to-use status bar"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
-TERMUX_PKG_VERSION=3.4.2
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_VERSION=3.4.3
 TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/$TERMUX_PKG_VERSION/polybar-$TERMUX_PKG_VERSION.tar"
-TERMUX_PKG_SHA256=4d22c977969a561f561fdc7a609073854d8fea8a9eec6941e12a80457edcb63a
+TERMUX_PKG_SHA256=d4ed121c1d3960493f8268f966d65a94d94c4646a4abb131687e37b63616822f
 TERMUX_PKG_DEPENDS="pulseaudio, fontconfig, freetype, libandroid-glob, libandroid-shmem, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm"
 TERMUX_PKG_BUILD_IN_SRC=true
 
