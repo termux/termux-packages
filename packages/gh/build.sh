bd3a56330be1bbbcf3e434f7c344fa0b7cffc476
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-06-04 17:20:50 +0530

    Github cli(gh) update from 0.8.0 to 0.9.0 (#5368)

--- packages/gh/build.sh
+++ packages/gh/build.sh
@@ -2,15 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://cli.github.com/
 TERMUX_PKG_DESCRIPTION="GitHub’s official command line tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_VERSION=0.9.0
 TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6439f2b01681be33b2b3fa313abdb700e5f6344ddff5e0fe8e01226c20d36442
+TERMUX_PKG_SHA256=318295e5a662f785662751f1e2cd4b1f613ec3aced1c4e7f1755d27922dbfdbf
 
 termux_step_make() {
 	termux_setup_golang
 
 	cd "$TERMUX_PKG_SRCDIR"
-
+	(
+		unset GOOS GOARCH CGO_LDFLAGS
+		unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+		go run ./cmd/gen-docs --man-page --doc-path $TERMUX_PREFIX/share/man/man1/
+	)
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	mkdir -p "$GOPATH"/src/github.com/cli/
 	mkdir -p "$TERMUX_PREFIX"/share/doc/gh
