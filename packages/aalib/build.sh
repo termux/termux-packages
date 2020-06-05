7a996768c646dbae2914945f7fa7e57a80257001
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-05 23:14:06 +0300

    new package: aalib

---
+++ packages/aalib/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://aa-project.sourceforge.net/aalib/
+TERMUX_PKG_DESCRIPTION="A portable ASCII art graphic library"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_VERSION=1.4rc5
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/aa-project/aalib-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=fbddda9230cf6ee2a4f5706b4b11e2190ae45f5eda1f0409dc4f99b35e0a70ee
+TERMUX_PKG_DEPENDS="libx11"
