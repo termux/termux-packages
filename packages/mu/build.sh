8cf975f15f37af05fbd326a88401f4df5658fe52
Henrik Grimler <henrik@grimler.se>
2020-06-06 19:52:11 +0200

    mu: update to 1.4.8

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.4.5
+TERMUX_PKG_VERSION=1.4.8
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=3e16ad39b2bc9af5dfe2e47f3aa76b3b7c3e7af245e89c2d189a9b5c43c9939e
+TERMUX_PKG_SHA256=b5cc7e716f5456a55dc83f25069b0ddbd076ddd0ea4319bd24d45d938f459c6f
 TERMUX_PKG_DEPENDS="glib, libc++, libxapian, libgmime"
