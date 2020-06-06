c19f5ba64cda2acc9d28a6017cf20f3ef1404b7e
Henrik Grimler <henrik@grimler.se>
2020-06-06 20:10:22 +0200

    subversion: update to 1.14.0

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://subversion.apache.org
 TERMUX_PKG_DESCRIPTION="Centralized version control system characterized by its simplicity"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.13.0
+TERMUX_PKG_VERSION=1.14.0
 TERMUX_PKG_SRCURL=https://www.apache.org/dist/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=bc50ce2c3faa7b1ae9103c432017df98dfd989c4239f9f8270bb3a314ed9e5bd
+TERMUX_PKG_SHA256=6ba8e218f9f97a83a799e58a3c6da1221d034b18d9d8cbbcb6ec52ab11722102
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite, liblz4, utf8proc, zlib"
 TERMUX_PKG_BREAKS="subversion-dev"
 TERMUX_PKG_REPLACES="subversion-dev"
