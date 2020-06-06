b7cb81bb92b304127b492180df0e290f5f76c4e2
Henrik Grimler <henrik@grimler.se>
2020-06-06 14:32:14 +0200

    ninvaders: switch to maintained github repo, update to 0.1.2

--- packages/ninvaders/build.sh
+++ packages/ninvaders/build.sh
@@ -1,12 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://ninvaders.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Space Invaders clone based on ncurses for ASCII output"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.1.1
-TERMUX_PKG_SRCURL=https://netix.dl.sourceforge.net/project/ninvaders/ninvaders/${TERMUX_PKG_VERSION}/ninvaders-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bfbc5c378704d9cf5e7fed288dac88859149bee5ed0850175759d310b61fd30b
+TERMUX_PKG_VERSION=0.1.2
+TERMUX_PKG_SRCURL=https://github.com/sf-refugees/ninvaders/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=80fbdfd396efc3674c0a697e0d7c219647518cefd22e948d504bb3a677e58df3
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make_install() {
-	install -Dm700 nInvaders $TERMUX_PREFIX/bin/
-}
