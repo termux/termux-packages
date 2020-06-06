10b95bf51bd742d3dae463052bf0c9f469a0a281
Henrik Grimler <henrik@grimler.se>
2020-06-06 19:43:21 +0200

    libzip: update to 1.7.0

--- packages/libzip/build.sh
+++ packages/libzip/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libzip.org/
 TERMUX_PKG_DESCRIPTION="Library for reading, creating, and modifying zip archives"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=1.6.1
+TERMUX_PKG_VERSION=1.7.0
 TERMUX_PKG_SRCURL=https://libzip.org/download/libzip-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=06eb8e9141fd19e2788cabaea9c9c2fd4d488d9e1484eb474bbfcac78e7b1d88
+TERMUX_PKG_SHA256=cfe303cc224bf23c5256a458de5e21a3d466dd352201f2fe44e99297aeae8b21
 TERMUX_PKG_DEPENDS="libbz2, openssl, zlib"
 TERMUX_PKG_BREAKS="libzip-dev"
 TERMUX_PKG_REPLACES="libzip-dev"
