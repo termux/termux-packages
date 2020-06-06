891257e0eda0310c709e52706584fbe5360980d3
Henrik Grimler <henrik@grimler.se>
2020-06-06 14:37:34 +0200

    pciutils: update to 3.7.0

--- root-packages/pciutils/build.sh
+++ root-packages/pciutils/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://mj.ucw.cz/sw/pciutils/
 TERMUX_PKG_DESCRIPTION="a collection of programs for inspecting and manipulating configuration of PCI devices"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.6.4
+TERMUX_PKG_VERSION=3.7.0
 TERMUX_PKG_SRCURL=https://mj.ucw.cz/download/linux/pci/pciutils-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1e87de2308683006ad0174f29c60df5c1f76045a8947c558a9376cc65034c64e
+TERMUX_PKG_SHA256=08c27e01030d1fcc700d02bc2ea66c638f58a3d150e45e58852aa82ad4160d84
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
