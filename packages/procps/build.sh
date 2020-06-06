8cf25938fcf954ef77cb6714b8852dbe214b4b87
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-07 02:20:01 +0300

    procps: fix & enable utility 'uptime'
    Replace 'uptime' from busybox.

--- packages/procps/build.sh
+++ packages/procps/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/procps-ng/procps
 TERMUX_PKG_DESCRIPTION="Utilities that give information about processes using the /proc filesystem"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=3.3.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/procps-ng-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=925eacd65dedcf9c98eb94e8978bbfb63f5de37294cc1047d81462ed477a20af
 TERMUX_PKG_DEPENDS="ncurses"
@@ -24,12 +24,10 @@ ac_cv_header_stdio_ext_h=no
 
 # About kill: https://bugs.launchpad.net/ubuntu/+source/coreutils/+bug/141168:
 # "For compatibility between distributions, can we have /bin/kill made available from coreutils?"
-# About uptime: Does not work on later android versions as /proc/uptime cannot be read.
 # About top: The system top works better.
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/top share/man/man1/top.1
 bin/kill share/man/man1/kill.1
 bin/slabtop share/man/man1/slabtop.1
-bin/uptime share/man/man1/uptime.1
 bin/w share/man/man1/w.1
 "
