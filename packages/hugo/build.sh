f96e41ea6831ce8b34efee029ee5e6964c2421c1
Henrik Grimler <henrik@grimler.se>
2020-06-06 19:37:38 +0200

    hugo: update to 0.72.0

--- packages/hugo/build.sh
+++ packages/hugo/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gohugo.io/
 TERMUX_PKG_DESCRIPTION="A fast and flexible static site generator"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.71.1
+TERMUX_PKG_VERSION=0.72.0
 TERMUX_PKG_SRCURL=https://github.com/gohugoio/hugo/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=4157a023059e199a1912d3c8026144c4b1c41cdbde7f1ffc826dd7ac01bb7b76
+TERMUX_PKG_SHA256=abde319397224dcaad69d2437a9aa7a45c0e9a929d734a2f75b1dc7ec2afa23f
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_make() {
