76863241de72facb5da5cec429658dd26572f91a
Henrik Grimler <henrik@grimler.se>
2020-06-06 15:09:48 +0200

    ethtool: update to 5.7

--- root-packages/ethtool/build.sh
+++ root-packages/ethtool/build.sh
@@ -1,9 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://mirrors.edge.kernel.org/pub/software/network/ethtool/
 TERMUX_PKG_DESCRIPTION="standard Linux utility for controlling network drivers and hardware, particularly for wired Ethernet devices"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="Auxilus @Auxilus"
-TERMUX_PKG_VERSION=5.4
+TERMUX_PKG_VERSION=5.7
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/software/network/ethtool/ethtool-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=22f81e4927d6eb6a13a259d705248a96005fce2005e15257f639f5a1c8c52b6b
-TERMUX_PKG_DEPENDS="libnl"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_SHA256=724eb8bd3c3a389fe285735959a1902fbd9310624656ad3220c5f23df1053c39
+TERMUX_PKG_DEPENDS="libmnl"
