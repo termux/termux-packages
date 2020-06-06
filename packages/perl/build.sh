ebe094122e092970a403834e4153b4e9c4ebe1d1
Henrik Grimler <henrik@grimler.se>
2020-06-06 20:10:10 +0200

    perl: update to 5.30.3

--- packages/perl/build.sh
+++ packages/perl/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.perl.org/
 TERMUX_PKG_DESCRIPTION="Capable, feature-rich programming language"
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
-TERMUX_PKG_VERSION=(5.30.2
-                    1.3.2)
-TERMUX_PKG_SHA256=(66db7df8a91979eb576fac91743644da878244cf8ee152f02cd6f5cd7a731689
-                   defa12f0ad7be0b6c48b4f76e2fb5b37c1b37fbeb6e9ebe938279cd539a0c20c)
+TERMUX_PKG_VERSION=(5.30.3
+                    1.3.4)
+TERMUX_PKG_SHA256=(32e04c8bb7b1aecb2742a7f7ac0eabac100f38247352a73ad7fa104e39e7406f
+                   755aa0ca8141a942188a269564f86c3c82349f82c346ed5c992495d7f35138ba)
 TERMUX_PKG_SRCURL=(http://www.cpan.org/src/5.0/perl-${TERMUX_PKG_VERSION}.tar.gz
 		   https://github.com/arsv/perl-cross/releases/download/${TERMUX_PKG_VERSION[1]}/perl-cross-${TERMUX_PKG_VERSION[1]}.tar.gz)
 TERMUX_PKG_BUILD_IN_SRC=true
