6ef0e5876aa5dcb1b29066a3fcfed99b707117f2
Henrik Grimler <henrik@grimler.se>
2020-06-07 07:24:03 +0200

    Bump pending packages again
    Seems previous build failed due to game-packages metadata being updated.

--- packages/syncthing/build.sh
+++ packages/syncthing/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://syncthing.net/
 TERMUX_PKG_DESCRIPTION="Decentralized file synchronization"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_VERSION=1.6.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/releases/download/v${TERMUX_PKG_VERSION}/syncthing-source-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f42518d453f7475022b5632420f21a6e0ab8d77f19c29b65ad582bc98e00a0a5
 
