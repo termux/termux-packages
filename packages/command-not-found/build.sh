3f349c2fa93e552d8bc2120abf8739c43cd25922
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-07 22:20:11 +0300

    command-not-found: fix checksum

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -3,4 +3,4 @@ TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell se
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=1.52
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2ec3936cfd899b1911c15f9802324a7edbf2bc95c9a11634fb3a3a6b2b263042
+TERMUX_PKG_SHA256=e33cd602be1ab646d5d9a5e339da7608a675c5030ae6ee27a4ac8717ea117945
