1b8350a61c0b98a222cfee00d0068da8cd40ff5c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-07 22:15:06 +0300

    command-not-found: update to 1.52

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.51
+TERMUX_PKG_VERSION=1.52
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7f286f592c2e4a9b668345bd277f029bbcc63fddf31b2f4e431065ca8f75d579
+TERMUX_PKG_SHA256=2ec3936cfd899b1911c15f9802324a7edbf2bc95c9a11634fb3a3a6b2b263042
