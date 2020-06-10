20c6958bb95b0ee60f4c37a2acb7efa6b4909482
x0b <x0bdev@gmail.com>
2020-06-10 21:21:11 +0200

    rclone: update to 1.52.1

--- packages/rclone/build.sh
+++ packages/rclone/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://rclone.org/
 TERMUX_PKG_DESCRIPTION="rsync for cloud storage"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.52.0
+TERMUX_PKG_VERSION=1.52.1
 TERMUX_PKG_SRCURL=https://github.com/rclone/rclone/releases/download/v${TERMUX_PKG_VERSION}/rclone-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b8cd06f62fa1623aa77e8a8f60f4a30c02c84c97488ded159968bf07beedf31b
+TERMUX_PKG_SHA256=a95fbac3768bc5fa563fff8305786f6cfddd3457deb570a3ada4c776f1a87553
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
