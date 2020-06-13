32b56d7f2d5fb2955a39e2ec524f1eb4b86df5d2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-13 12:05:07 +0300

    rebuild exiftool & irssi

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta informatio
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=11.99
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=33ed6da3d292652bfbc3131ac533bd8fdfbd576699f9232dbcdfe74beccb70f9
 TERMUX_PKG_DEPENDS="perl"
 
 termux_step_make_install() {
-	# Change this after package 'perl' was upgraded.
-	local current_perl_version=5.30.2
+	local current_perl_version=$(. $TERMUX_SCRIPTDIR/packages/perl/build.sh; echo $TERMUX_PKG_VERSION)
 
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/exiftool "$TERMUX_PREFIX"/bin/exiftool
 	find "$TERMUX_PKG_SRCDIR"/lib -name "*.pod" -delete
