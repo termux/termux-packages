e54d6b11c4103bb6b7596ec82d9ae835c8f341a2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-13 18:13:14 +0300

    new package: tin-summer
    Requested in https://github.com/termux/termux-packages/issues/4406.

---
+++ packages/tin-summer/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/vmchale/tin-summer
+TERMUX_PKG_DESCRIPTION="Find build artifacts that are taking up disk space"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_VERSION=1.21.8
+TERMUX_PKG_SRCURL=https://github.com/vmchale/tin-summer/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=d9a6f4b41c759c291c91348914635243df0f13d38985d398bbb48a39ab4b338c
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	sed -i 's/linux/android/g' src/utils.rs
+}
