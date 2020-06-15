018a36ceb853e0b3bf3d4324909026c3c035db08
Faheem Pervez <qwerty12@users.noreply.github.com>
2020-06-15 02:15:11 +0100

    shfmt: add package

---
+++ packages/shfmt/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/mvdan/sh
+TERMUX_PKG_DESCRIPTION="A shell parser and formatter"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_VERSION=3.1.1
+TERMUX_PKG_SRCURL=https://github.com/mvdan/sh/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=db5d86d9caeb025e41307cb834b21d7a35e171afe5257277e74cb3e44a17ae00
+
+termux_step_make_install() {
+	cd "$TERMUX_PKG_SRCDIR"
+
+	termux_setup_golang
+
+	export GOPATH="$TERMUX_PKG_BUILDDIR"
+	mkdir -p "$GOPATH/src/github.com/mvdan"
+	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH/src/github.com/mvdan/sh"
+
+	go build -modcacherw \
+		-ldflags "-X main.version=$TERMUX_PKG_VERSION" \
+		-o "$TERMUX_PREFIX/bin/shfmt" \
+		./cmd/shfmt
+}
