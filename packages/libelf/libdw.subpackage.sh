550852b4dd6596ba3ecb527be0c816fe4ff1d4b6
Faheem Pervez <qwerty12@users.noreply.github.com>
2020-06-20 18:05:31 +0100

    libelf: build libasm, libdw (#5411)

---
+++ packages/libelf/libdw.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="lib/libdw*.so* lib/pkgconfig/libdw.pc include/elfutils/known-dwarf.h include/elfutils/libdwfl.h include/elfutils/libdw.h include/dwarf.h"
+TERMUX_SUBPKG_DESCRIPTION="Library to read DWARF information"
+TERMUX_SUBPKG_DEPENDS="libblocksruntime"
