61fdaa0affb0543155a10061ff338c68a01e4f8d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-20 21:17:54 +0300

    chezmoi: update to 1.8.2

--- packages/chezmoi/build.sh
+++ packages/chezmoi/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://chezmoi.io
 TERMUX_PKG_DESCRIPTION="Manage your dotfiles across multiple machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.8.1
+TERMUX_PKG_VERSION=1.8.2
 TERMUX_PKG_SRCURL=https://github.com/twpayne/chezmoi/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=45a2fe6932555e9fe26d1a5370cbbda9b51fd185d5df0c5b51e5f979c8e729d0
+TERMUX_PKG_SHA256=0229780e3a10c6eafefd5ca3326574bef1d7fe787d13f2998ac39028399d8e1c
 
 termux_step_make_install() {
 	termux_setup_golang
@@ -16,10 +16,10 @@ termux_step_make_install() {
 	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/twpayne/chezmoi"
 	cd "${GOPATH}/src/github.com/twpayne/chezmoi"
 
-	go get -d -v 
+	go get -d -v
 	go build
 	install -Dm700 -t $TERMUX_PREFIX/bin/ ${GOPATH}/src/github.com/twpayne/chezmoi/chezmoi
- 
+
 	mkdir -p $TERMUX_PREFIX/share/bash-completions \
 		$TERMUX_PREFIX/share/fish/completions \
 		$TERMUX_PREFIX/share/doc/chezmoi
