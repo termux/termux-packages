af505cb975d136f94263de8481738474d941391b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-20 23:45:34 +0300

    libnftnl: update to 1.1.7

--- packages/libnftnl/build.sh
+++ packages/libnftnl/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.netfilter.org/projects/libnftnl/
 TERMUX_PKG_DESCRIPTION="Netfilter library providing interface to the nf_tables subsystem"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.1.6
+TERMUX_PKG_VERSION=1.1.7
 TERMUX_PKG_SRCURL=http://netfilter.org/projects/libnftnl/files/libnftnl-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=c1eb5a696fc1d4b3b412770586017bc01af93da3ddd25233d34a62979dee1eca
+TERMUX_PKG_SHA256=20dbc13f11004aea2c9e479cfb90359cb11fe3446c3140811c18e4ec1648ed8f
 TERMUX_PKG_DEPENDS="libmnl"
 TERMUX_PKG_BREAKS="libnftnl-dev"
 TERMUX_PKG_REPLACES="libnftnl-dev"
