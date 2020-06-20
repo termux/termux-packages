d4bb6449725f1ce590318b0dfc67ca906c414768
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-20 23:04:02 +0300

    dasm: update to 2.20.13

--- packages/dasm/build.sh
+++ packages/dasm/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://dasm-dillon.sourceforge.io/
 TERMUX_PKG_DESCRIPTION="Macro assembler with support for several 8-bit microprocessors"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.20.11
+TERMUX_PKG_VERSION=2.20.13
 TERMUX_PKG_SRCURL=https://github.com/dasm-assembler/dasm/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c69bbe192159dcf75677ca13bba65c3318dc443f2df45fccd3c060b2e092c7f5
+TERMUX_PKG_SHA256=48be84858d578dd7e1ac702fb2dca713a2e0be930137cbb3d6ecbeac1944ff5c
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
