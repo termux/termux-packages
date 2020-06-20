a1ed2bbde69c71fbb7e96341db8cfc5ec8d8ba73
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-20 23:35:15 +0300

    gmic: update to 2.9.1

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gmic.eu
 TERMUX_PKG_DESCRIPTION="Full-featured framework for image processing"
 TERMUX_PKG_LICENSE="CeCILL-2.1"
-TERMUX_PKG_VERSION=2.9.0
+TERMUX_PKG_VERSION=2.9.1
 TERMUX_PKG_SRCURL=https://gmic.eu/files/source/gmic_$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6233695f9b27999dcc7cc3aa7480b0f192bd44de85209091a3b4b8a65ae8c4b5
+TERMUX_PKG_SHA256=50c9dd418b8d0e80c703c2e2b179b6049343567483d37d9ff6c642c6ede95d8d
 TERMUX_PKG_DEPENDS="libc++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_BUILD_DEPENDS="graphicsmagick"
 TERMUX_PKG_BUILD_IN_SRC=true
