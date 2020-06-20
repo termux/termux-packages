2f58b9f435b35605a1bcfebc7a8bd58b04de20f4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-20 22:43:34 +0300

    build-package.sh: add function for setting up GHC

--- build-package.sh
+++ build-package.sh
@@ -41,6 +41,10 @@ source "$TERMUX_SCRIPTDIR/scripts/build/termux_error_exit.sh"
 # shellcheck source=scripts/build/termux_download.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/termux_download.sh"
 
+# Utility function for setting up GHC toolchain.
+# shellcheck source=scripts/build/setup/termux_setup_ghc.sh
+source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_ghc.sh"
+
 # Utility function for golang-using packages to setup a go toolchain.
 # shellcheck source=scripts/build/setup/termux_setup_golang.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_golang.sh"
