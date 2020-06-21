6fbecc6d40612d7a6fbd1772ec3da211a5f18060
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 22:31:42 +0300

    rsync: bump revision

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://rsync.samba.org/
 TERMUX_PKG_DESCRIPTION="Utility that provides fast incremental file transfer"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=3.2.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=90127fdfb1a0c5fa655f2577e5495a40907903ac98f346f225f867141424fa25
 TERMUX_PKG_DEPENDS="libiconv, libpopt, openssh | dropbear, zlib, zstd"
