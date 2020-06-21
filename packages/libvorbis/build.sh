83d7afe1793c9eacab86816624be7e8ce355856e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 23:56:11 +0300

    libvorbis: update homepage

--- packages/libvorbis/build.sh
+++ packages/libvorbis/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://www.vorbis.com/
+TERMUX_PKG_HOMEPAGE=https://xiph.org/vorbis/
 TERMUX_PKG_DESCRIPTION="Library for using the Ogg Vorbis compressed audio format"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=1.3.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/libvorbis-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=af00bb5a784e7c9e69f56823de4637c350643deedaf333d0fa86ecdba6fcb415
 TERMUX_PKG_DEPENDS="libogg"
