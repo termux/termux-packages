0f57e39144b642c45c2476f9f12dd267421b31f4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 23:14:19 +0300

    rsync: fix build error

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -5,11 +5,15 @@ TERMUX_PKG_VERSION=3.2.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=90127fdfb1a0c5fa655f2577e5495a40907903ac98f346f225f867141424fa25
-TERMUX_PKG_DEPENDS="libiconv, libpopt, openssh | dropbear, zlib, zstd"
+TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl-tool, zlib, zstd"
+TERMUX_PKG_BUILD_IN_SRC=true
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-rsyncd-conf=$TERMUX_PREFIX/etc/rsyncd.conf
+--with-included-popt=no
 --with-included-zlib=no
---disable-xattr-support
 --disable-debug
+--disable-simd
+--disable-xattr-support
 --disable-xxhash
 "
