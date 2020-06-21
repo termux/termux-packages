155ce70b8994aafeb2313747caeafc519c232c31
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 23:54:38 +0300

    libandroid-spawn: update homepage

--- packages/libandroid-spawn/build.sh
+++ packages/libandroid-spawn/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://man7.org/linux/man-pages/man3/posix_spawn.3.html
+TERMUX_PKG_HOMEPAGE=https://man7.org/linux/man-pages/man3/posix_spawn.3.html
 TERMUX_PKG_DESCRIPTION="Shared library for the posix_spawn system function"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_REVISION=1
 
 termux_step_make() {
 	$CXX $CFLAGS $CPPFLAGS -I$TERMUX_PKG_BUILDER_DIR -c $TERMUX_PKG_BUILDER_DIR/posix_spawn.cpp
