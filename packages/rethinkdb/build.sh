d5febc112915be77a98db250099f6d542bb886cf
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 17:00:39 +0300

    Update build.sh

--- packages/rethinkdb/build.sh
+++ packages/rethinkdb/build.sh
@@ -6,9 +6,13 @@ TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL=https://github.com/rethinkdb/rethinkdb/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e3749b644af0b230685a32ef31317a9dfebfee5582ded47c444c1c0eaa83712e
 TERMUX_PKG_DEPENDS="ncurses, boost, curl, openssl, libcrypt, python, jemalloc, protobuf,libprotobuf-c, wget, nodejs"
-#TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--allow-fecth --prefix $TERMUX_PREFIX --sysconfdir $TERMUX_PREFIX/etc --localstatedir $TERMUX_PREFIX/var"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-        termux_setup_protobuf
+    ./configure --prefix $TERMUX_PREFIX \
+        --sysconfdir $TERMUX_PREFIX/etc \
+        --localstatedir $TERMUX_PREFIX/var \
+        --allow-fetch \
+        --fetch protoc \
+        CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
 }
