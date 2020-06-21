aa9724df92e252cfc9f15edc01ba57c88442d0ed
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 17:58:05 +0300

    html2text: fix build error

--- packages/html2text/build.sh
+++ packages/html2text/build.sh
@@ -4,9 +4,10 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1:2.0.0
 TERMUX_PKG_SRCURL=https://github.com/grobian/html2text/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=061125bfac658c6d89fa55e9519d90c5eeb3ba97b2105748ee62f3a3fa2449de
+TERMUX_PKG_DEPENDS="libiconv"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CXX="$CXX $LDFLAGS"
+	CXX="$CXX $CXXFLAGS $CPPFLAGS $LDFLAGS"
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 }
