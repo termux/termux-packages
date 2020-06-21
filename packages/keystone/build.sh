0eedfbfa72b563f6db25714a135ae21c16d1037c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 23:51:30 +0300

    keystone: update homepage

--- packages/keystone/build.sh
+++ packages/keystone/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://www.keystone-engine.org/
+TERMUX_PKG_HOMEPAGE=https://www.keystone-engine.org/
 TERMUX_PKG_DESCRIPTION="Keystone is a lightweight multi-platform, multi-architecture assembler framework"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.9.1
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://github.com/keystone-engine/keystone/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e9d706cd0c19c49a6524b77db8158449b9c434b415fbf94a073968b68cf8a9f0
 TERMUX_PKG_DEPENDS="libc++"
