8151d38865de7913ef1f2d470a147c87ae42579c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-21 17:24:40 +0300

    should depend on libcurl and not on host curl from /usr/bin

--- packages/rethinkdb/build.sh
+++ packages/rethinkdb/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_LICENSE_FILE="COPYRIGHT"
 TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL=https://github.com/rethinkdb/rethinkdb/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e3749b644af0b230685a32ef31317a9dfebfee5582ded47c444c1c0eaa83712e
-TERMUX_PKG_DEPENDS="ncurses, boost, curl, openssl, libcrypt, python, protobuf,libprotobuf-c, nodejs"
+TERMUX_PKG_DEPENDS="ncurses, boost, libcurl, openssl, libcrypt, python, protobuf,libprotobuf-c, nodejs"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_MAKE_PROCESSES=1
