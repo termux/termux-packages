8f2e33d85972e0a0522f2c1de2e50b576804756d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 15:24:02 +0300

    xfdesktop: bump revision

--- x11-packages/xfdesktop/build.sh
+++ x11-packages/xfdesktop/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A desktop manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=4.15.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.xfce.org/lastonestanding/xfdesktop/-/archive/xfdesktop-${TERMUX_PKG_VERSION}/xfdesktop-xfdesktop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d1b44662fe4f063f349a4f72d1edebfdbf50697be29ee1055ee0c9c162080d62
 TERMUX_PKG_DEPENDS="exo, garcon, hicolor-icon-theme, libwnck, libxfce4ui, startup-notification, thunar"
