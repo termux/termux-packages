80cc734abdd23e44adfd4ab99a15de6a57c5a58d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 14:39:11 +0300

    v8 - fix python command in Makefile

--- packages/rethinkdb/build.sh
+++ packages/rethinkdb/build.sh
@@ -27,5 +27,6 @@ termux_step_configure() {
 termux_step_make() {
 	make fetch
 	sed -i 's/python /python2 /' ./external/v8_*-patched/build/gyp/gyp
+	sed -i 's/python /python2 /' ./external/v8_*-patched/Makefile
 	make
 }
