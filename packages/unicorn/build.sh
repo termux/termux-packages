477c7064437b1e0cea0c49317d387d11b94caf73
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 15:08:19 +0300

    unicorn: update homepage

--- packages/unicorn/build.sh
+++ packages/unicorn/build.sh
@@ -1,8 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://www.unicorn-engine.org/
+TERMUX_PKG_HOMEPAGE=https://www.unicorn-engine.org/
 TERMUX_PKG_DESCRIPTION="Unicorn is a lightweight multi-platform, multi-architecture CPU emulator framework."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.0.1
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://github.com/unicorn-engine/unicorn/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=3a6a4f2b8c405ab009040ca43af8e4aa10ebe44d9c8b336aa36dc35df955017c
 TERMUX_PKG_BREAKS="unicorn-dev"
