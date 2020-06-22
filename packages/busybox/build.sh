08018d3d87d3b1d140175b9772d70467043fee6a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 11:27:49 +0300

    busybox: finishing deprecation & applets removal

--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -6,8 +6,7 @@ TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=https://busybox.net/downloads/busybox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=d0f940a72f648943c1f2211e0e3117387c31d765137d92bd8284a3fb9752a998
 TERMUX_PKG_BUILD_IN_SRC=true
-# We replace env in the old coreutils package:
-TERMUX_PKG_CONFLICTS="coreutils (<< 8.25-4)"
+
 TERMUX_PKG_SERVICE_SCRIPT=(
 	"telnetd" 'exec busybox telnetd -F'
 	"ftpd" 'exec busybox tcpsvd -vE 0.0.0.0 8021 busybox ftpd $HOME'
@@ -43,28 +42,6 @@ termux_step_post_make_install() {
 		install -Dm700 busybox_unstripped $PREFIX/bin/busybox
 	fi
 
-	# Utilities (like crond/crontab) are useful but not available
-	# as standalone package in Termux.
-	#
-	# Few notes:
-	#
-	#  * runsv, runsvdir, sv - for things like in https://github.com/termux/termux-packages/pull/3460.
-	#  * tcpsvd - required for ftpd applet.
-	#
-	rm -Rf $TERMUX_PREFIX/bin/applets
-	mkdir -p $TERMUX_PREFIX/bin/applets
-	cd $TERMUX_PREFIX/bin/applets
-	for f in crond crontab inotifyd lsusb runsv runsvdir sendmail sv svlogd; do
-		ln -s ../busybox $f
-	done
-	unset f
-
-	# Install busybox man page
-	mkdir -p $TERMUX_PREFIX/share/man/man1
-	cp $TERMUX_PKG_SRCDIR/docs/busybox.1 $TERMUX_PREFIX/share/man/man1
-
-	# Needed for 'crontab -e' to work out of the box:
-	local _CRONTABS=$TERMUX_PREFIX/var/spool/cron/crontabs
-	mkdir -p $_CRONTABS
-	echo "Used by the busybox crontab and crond tools" > $_CRONTABS/README.termux
+	# Install busybox man page.
+	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 $TERMUX_PKG_SRCDIR/docs/busybox.1
 }
