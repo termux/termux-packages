fbd2ad31d922d5c8c9cf370b1dba3b4702acbaf7
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-23 02:10:57 +0300

    command-not-found: bump revision
    Package will be built and uploaded locally. Attempt to fix x86_64 metadata.
    
    %ci:no-build

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -2,5 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=1.53
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6c3e2cc67913f35b275b122b0d16ecc45093b3b0496fdd2682db56348b931214
