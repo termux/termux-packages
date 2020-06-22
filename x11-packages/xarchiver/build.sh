b9019156331be5fe15affcbcc03b04dadc2bd378
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 21:05:00 +0300

    build & upload second half of packages

--- x11-packages/xarchiver/build.sh
+++ x11-packages/xarchiver/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GTK+ frontend to various command line archivers"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=0.5.4.15
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ib/xarchiver/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2a18f5a8932516ecc5c50152ff6ce77bc3f58a0b221d6db9c70d089cf90d7182
 TERMUX_PKG_DEPENDS="atk, binutils, bzip2, cpio, glib, gtk3, gzip, libandroid-shmem, libcairo, lzip, lzop, p7zip, tar, unrar, unzip, xz-utils, zip, zstd, zlib"
--- x11-packages/xfconf/build.sh
+++ x11-packages/xfconf/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Flexible, easy-to-use configuration management system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=4.14.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/xfconf/${TERMUX_PKG_VERSION:0:4}/xfconf-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=589052a0efc6151c5fb5f438da463502a4fd91848cae7b9376d417be4c5a0c02
 TERMUX_PKG_DEPENDS="dbus, libxfce4util"
