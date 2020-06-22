b4a997c83713a91dbe20e1aacd54f0e7590db8f3
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-22 16:29:38 +0300

    fix build failure for thunar and xfdesktop

--- x11-packages/thunar/build.sh
+++ x11-packages/thunar/build.sh
@@ -7,3 +7,8 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.xfce.org/xfce/thunar/-/archive/thunar-${TERMUX_PKG_VERSION}/thunar-thunar-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c9b99606cad98fdaad039bae052ce9ee4376ef7b4844bbd458d1390342a8299e
 TERMUX_PKG_DEPENDS="desktop-file-utils, exo, hicolor-icon-theme, libexif, libnotify, libpng, libxfce4ui, libxfce4util"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-maintainer-mode"
+
+termux_step_pre_configure() {
+	NOCONFIGURE=1 ./autogen.sh
+}
--- x11-packages/xfdesktop/build.sh
+++ x11-packages/xfdesktop/build.sh
@@ -3,8 +3,12 @@ TERMUX_PKG_DESCRIPTION="A desktop manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=4.15.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://gitlab.xfce.org/lastonestanding/xfdesktop/-/archive/xfdesktop-${TERMUX_PKG_VERSION}/xfdesktop-xfdesktop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d1b44662fe4f063f349a4f72d1edebfdbf50697be29ee1055ee0c9c162080d62
 TERMUX_PKG_DEPENDS="exo, garcon, hicolor-icon-theme, libwnck, libxfce4ui, startup-notification, thunar"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-notifications"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-notifications --enable-maintainer-mode"
+
+termux_step_pre_configure() {
+	NOCONFIGURE=1 ./autogen.sh
+}
