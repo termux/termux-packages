2274552e39642fc55d54c5a8186a62984a95c3a8
ifurther <55025025+ifurther@users.noreply.github.com>
2020-06-23 23:43:50 +0800

    add depend libicu

--- packages/rethinkdb/build.sh
+++ packages/rethinkdb/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_LICENSE_FILE="COPYRIGHT"
 TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL=https://github.com/rethinkdb/rethinkdb/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e3749b644af0b230685a32ef31317a9dfebfee5582ded47c444c1c0eaa83712e
-TERMUX_PKG_DEPENDS="ncurses, libcurl, openssl, libcrypt, python, protobuf, libprotobuf-c, nodejs"
+TERMUX_PKG_DEPENDS="ncurses, libcurl, openssl, libcrypt, python, protobuf, libprotobuf-c, nodejs, libicu"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686, x86_64"
 
