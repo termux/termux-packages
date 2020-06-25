6da6eb5b8cc1de49e2adda8f9182851a9864b3d1
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-26 01:22:06 +0300

    harfbuzz: update to 2.6.8

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.6.7
+TERMUX_PKG_VERSION=2.6.8
 TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/releases/download/${TERMUX_PKG_VERSION}/harfbuzz-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=49e481d06cdff97bf68d99fa26bdf785331f411614485d892ea4c78eb479b218
+TERMUX_PKG_SHA256=6648a571a27f186e47094121f0095e1b809e918b3037c630c7f38ffad86e3035
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
 TERMUX_PKG_BREAKS="harfbuzz-dev"
 TERMUX_PKG_REPLACES="harfbuzz-dev"
