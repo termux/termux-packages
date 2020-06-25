eb4b11ecaac607c44cadfd6c9d368a56b0cb329e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-26 01:19:45 +0300

    exiftool: update to 12.01

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://exiftool.org/
 TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta information in a wide variety of files."
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=12.00
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=12.01
 TERMUX_PKG_SRCURL="http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=d0792cc94ab58a8b3d81b18ccdb8b43848c8fb901b5b7caecdcb68689c6c855a
+TERMUX_PKG_SHA256=1ab849ba41972fbdc608287da3112e779100ebb0961dd7b71449651407e3d586
 TERMUX_PKG_DEPENDS="perl"
 
 termux_step_make_install() {
