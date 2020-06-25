eb65994cb3cc9b52990d1ebec09a3e48946f55b2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-06-26 01:24:18 +0300

    libconfuse: update to 3.3

--- packages/libconfuse/build.sh
+++ packages/libconfuse/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/martinh/libconfuse
 TERMUX_PKG_DESCRIPTION="Small configuration file parser library for C"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=3.2.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.3
 TERMUX_PKG_SRCURL=https://github.com/martinh/libconfuse/releases/download/v$TERMUX_PKG_VERSION/confuse-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=71316b55592f8d0c98924242c98dbfa6252153a8b6e7d89e57fe6923934d77d0
+TERMUX_PKG_SHA256=3a59ded20bc652eaa8e6261ab46f7e483bc13dad79263c15af42ecbb329707b8
 TERMUX_PKG_BREAKS="libconfuse-dev"
 TERMUX_PKG_REPLACES="libconfuse-dev"
