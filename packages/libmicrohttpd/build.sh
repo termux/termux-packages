0a5d5f19d2f18cb7fccacbf5141b4b5fde61eed3
Henrik Grimler <henrik@grimler.se>
2020-07-04 09:14:14 +0200

    libmicrohttpd: update to 0.9.71

--- packages/libmicrohttpd/build.sh
+++ packages/libmicrohttpd/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/libmicrohttpd/
 TERMUX_PKG_DESCRIPTION="A small C library that is supposed to make it easy to run an HTTP server as part of another application"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=0.9.70
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.9.71
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=90d0a3d396f96f9bc41eb0f7e8187796049285fabef82604acd4879590977307
+TERMUX_PKG_SHA256=e8f445e85faf727b89e9f9590daea4473ae00ead38b237cf1eda55172b89b182
 TERMUX_PKG_DEPENDS="libgnutls"
 TERMUX_PKG_BREAKS="libmicrohttpd-dev"
 TERMUX_PKG_REPLACES="libmicrohttpd-dev"
