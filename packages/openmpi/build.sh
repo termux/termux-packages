91b0a021fafafcd95a924901157d8b06d0aee157
Henrik Grimler <henrik@grimler.se>
2020-07-04 13:18:07 +0200

    bump openmpi and proj to restart build

--- packages/openmpi/build.sh
+++ packages/openmpi/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open source Message Passing Interface implementation"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=4.0.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.open-mpi.org/release/open-mpi/v${TERMUX_PKG_VERSION:0:3}/openmpi-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=dca264f420411f540a496bdd131bffd83e325fc9006286b39dd19b62d7368233
 TERMUX_PKG_DEPENDS="libandroid-shmem"
--- packages/proj/build.sh
+++ packages/proj/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Generic coordinate transformation software"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=7.1.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/OSGeo/proj.4/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=76650d22a64ad67af055f8644aa501aa0a1b8d1cc1a98f73d9db80637aa27956
 TERMUX_PKG_DEPENDS="libc++, libsqlite, sqlite, libtiff, libcurl"
