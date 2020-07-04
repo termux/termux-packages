0696575ab0cbc4669410af10b85b6bb08d4874e9
Henrik Grimler <henrik@grimler.se>
2020-07-04 09:13:34 +0200

    shfmt: update to 3.1.2

--- packages/shfmt/build.sh
+++ packages/shfmt/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/mvdan/sh
 TERMUX_PKG_DESCRIPTION="A shell parser and formatter"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.1.1
+TERMUX_PKG_VERSION=3.1.2
 TERMUX_PKG_SRCURL=https://github.com/mvdan/sh/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=db5d86d9caeb025e41307cb834b21d7a35e171afe5257277e74cb3e44a17ae00
+TERMUX_PKG_SHA256=133fcdb4645ee0c2893319b1ce5b83c88b8576c1e3936b1fa14b967df1501ee5
 
 termux_step_make_install() {
 	cd "$TERMUX_PKG_SRCDIR"
