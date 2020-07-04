6a7a1cf7ccd9393448d321f2365549e17faafb0c
Henrik Grimler <henrik@grimler.se>
2020-07-04 10:36:52 +0200

    perl: update to 5.32.0

--- packages/perl/build.sh
+++ packages/perl/build.sh
@@ -4,10 +4,10 @@ TERMUX_PKG_LICENSE="Artistic-License-2.0"
 # Packages which should be rebuilt after version change:
 # - exiftool
 # - irssi
-TERMUX_PKG_VERSION=(5.30.3
+# - psutils
+TERMUX_PKG_VERSION=(5.32.0
                     1.3.4)
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=(32e04c8bb7b1aecb2742a7f7ac0eabac100f38247352a73ad7fa104e39e7406f
+TERMUX_PKG_SHA256=(efeb1ce1f10824190ad1cadbcccf6fdb8a5d37007d0100d2d9ae5f2b5900c0b4
                    755aa0ca8141a942188a269564f86c3c82349f82c346ed5c992495d7f35138ba)
 TERMUX_PKG_SRCURL=(http://www.cpan.org/src/5.0/perl-${TERMUX_PKG_VERSION}.tar.gz
 		   https://github.com/arsv/perl-cross/releases/download/${TERMUX_PKG_VERSION[1]}/perl-cross-${TERMUX_PKG_VERSION[1]}.tar.gz)
