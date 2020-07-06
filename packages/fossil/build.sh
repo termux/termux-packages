733532957b919e73e4be61527f07d90f7d9d2853
Henrik Grimler <henrik@grimler.se>
2020-07-06 16:36:34 +0200

    fossil: specify license file

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_LICENSE_FILE="COPYRIGHT-BSD2.txt"
 TERMUX_PKG_VERSION=2.11.1
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
