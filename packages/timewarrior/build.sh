bd99de5602c465343bc01323fe71e9f340a08dd7
Henrik Grimler <henrik@grimler.se>
2020-07-06 16:00:50 +0200

    timewarrior: fix src url
    It seems to have been changed.

--- packages/timewarrior/build.sh
+++ packages/timewarrior/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Command-line time tracker"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/timewarrior/releases/download/v$TERMUX_PKG_VERSION/timew-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1f3b9166a96637d3c098a7cfcff74ca61c41f13e2ca21f6c7ad6dd54cc74ac70
+TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/timewarrior/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=6199304fc9697a2eb78c542357aec984924bc2ecad90f3bedf1f6299fe345484
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++"
 
 termux_step_pre_configure() {
