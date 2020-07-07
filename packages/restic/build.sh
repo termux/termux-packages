a76f6cfba2c5169747eb6d7ebd0a3205d52a7963
Henrik Grimler <henrik@grimler.se>
2020-07-07 11:13:04 +0200

    restic: switch back to using 0.9.6 archive

--- packages/restic/build.sh
+++ packages/restic/build.sh
@@ -3,10 +3,8 @@ TERMUX_PKG_DESCRIPTION="Fast, secure, efficient backup program"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=0.9.6
 TERMUX_PKG_REVISION=2
-# Use a snapshot to fix building with go 1.13:
-#TERMUX_PKG_SRCURL=https://github.com/restic/restic/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SRCURL=https://github.com/restic/restic/archive/604b18aa7426148a55f76307ca729e829ff6b61d.zip
-TERMUX_PKG_SHA256=e1d8c977c541d9503a6aba643cf10896f0de488a20db96f162a3e91a4e9def70
+TERMUX_PKG_SRCURL=https://github.com/restic/restic/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1cc8655fa99f06e787871a9f8b5ceec283c856fa341a5b38824a0ca89420b0fe
 TERMUX_PKG_SUGGESTS="openssh, rclone"
 
 termux_step_make() {
