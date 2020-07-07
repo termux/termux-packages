ea2583bb0210c051377282281dfd6265cdab53de
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-07 19:33:14 +0300

    gawk: bump revision

--- packages/gawk/build.sh
+++ packages/gawk/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gawk/
 TERMUX_PKG_DESCRIPTION="Programming language designed for text processing"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=5.1.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gawk/gawk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=cf5fea4ac5665fd5171af4716baab2effc76306a9572988d5ba1078f196382bd
 TERMUX_PKG_DEPENDS="libandroid-support, libgmp, libmpfr, readline"
