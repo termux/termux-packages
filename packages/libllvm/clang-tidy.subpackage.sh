b93e708848e691ddd042f6bf2b9ffd3790d9a065
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-09 02:03:28 +0300

    Update clang-tidy.subpackage.sh

--- packages/libllvm/clang-tidy.subpackage.sh
+++ packages/libllvm/clang-tidy.subpackage.sh
@@ -3,4 +3,4 @@ bin/clang-tidy
 share/clang/clang-tidy-diff.py
 share/clang/run-clang-tidy.py
 "
-TERMUX_SUBPKG_DESCRIPTION="clang-tidy is a clang-based C++ “linter” tool."
+TERMUX_SUBPKG_DESCRIPTION="clang-tidy is a clang-based C++ 'linter' tool."
