e5ecfb397baa9bf93c36d16f91fba31d1ed05b60
Henrik Grimler <henrik@grimler.se>
2020-07-08 13:54:50 +0200

    hunspell{,-ru}: fix accidental build.sh mix up..

--- packages/hunspell-ru/build.sh
+++ packages/hunspell-ru/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://hunspell.github.io
 TERMUX_PKG_DESCRIPTION="Russian dictionary for hunspell"
 TERMUX_PKG_LICENSE="LGPL-3.0"
-TERMUX_PKG_VERSION=20170303
+TERMUX_PKG_VERSION=20200604
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -13,9 +13,9 @@ termux_step_make_install() {
 	# In which case we need to bump version and checksum used.
 	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/ru_RU/ru_RU.aff \
 			$TERMUX_PREFIX/share/hunspell/ru_RU.aff \
-			709cf9b41208961226e995a3ab75a2da834aaf4f9707cb87cbb37d4943b6a50d
+			38ce7d4af78e211e9bafe4bf7e3d6a2c420591136cb738ec6648f8fdf6524cd7
 	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/ru_RU/ru_RU.dic \
 			$TERMUX_PREFIX/share/hunspell/ru_RU.dic \
-			c0d81126b0a905ccc6fd891c923b43d39b4ce449da5a333859229354c510168f
+			3280afe7829128ff589b2c5d8f46810ed9b0eddcc85e9b1cc3d12dc08d4c1a0b
 	touch $TERMUX_PREFIX/share/hunspell/ru_RU.{aff,dic}
 }
