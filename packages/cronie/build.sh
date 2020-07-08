4d1c72ea0ef2b348df24b5278414bf07912550a2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-08 22:56:17 +0300

    cronie: set default editor to $PREFIX/bin/nano
    Nano is part of Termux bootstrap.

--- packages/cronie/build.sh
+++ packages/cronie/build.sh
@@ -5,4 +5,8 @@ TERMUX_PKG_VERSION=1.5.5
 TERMUX_PKG_SRCURL=https://github.com/cronie-crond/cronie/releases/download/cronie-${TERMUX_PKG_VERSION}/cronie-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=be34c79505e5544323281854744b9955ff16b160ee569f9df7c0dddae5720eac
 TERMUX_PKG_DEPENDS="bash"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-anacron --disable-pam"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-anacron
+--disable-pam
+--with-editor=$TERMUX_PREFIX/bin/nano
+"
