46ae6589e883b31adf257a301be9893850570b9c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-09 01:13:14 +0300

    kakoune: bump revision

--- packages/kakoune/build.sh
+++ packages/kakoune/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mawww/kakoune
 TERMUX_PKG_DESCRIPTION="Code editor heavily inspired by Vim"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_VERSION=2020.01.16
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mawww/kakoune/releases/download/v$TERMUX_PKG_VERSION/kakoune-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=a094f1689f0228308f631e371b382b0c0522391fc8b6c23a6cbc71ff404a0dae
 TERMUX_PKG_DEPENDS="libc++, ncurses"
