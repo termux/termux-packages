069cb52515096c35e4d55882e9bcdb395337c248
Henrik Grimler <henrik@grimler.se>
2020-07-08 22:07:20 +0200

    Revert "Update command-not-found lists"
    This reverts commit 81735731ca0ac4522a8bdcd630a16ccca592d7f8.
    
    Something went wrong with the latest list update:
    https://github.com/termux/command-not-found/issues/4

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.54
+TERMUX_PKG_VERSION=1.53
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d76d375ab50a00f8841f5ce095eb37f0d0d8f4880536f835ada82048ee4bcdea
+TERMUX_PKG_SHA256=6c3e2cc67913f35b275b122b0d16ecc45093b3b0496fdd2682db56348b931214
