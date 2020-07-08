ba5d2e08e3b8532bc7125cab0d5514ead3789edd
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-09 00:53:36 +0300

    finalize the busybox deprecation, add cronie as busybox crond replacement (#5499)

---
+++ packages/cronie/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/cronie-crond/cronie/
+TERMUX_PKG_DESCRIPTION="Daemon that runs specified programs at scheduled times and related tools"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_VERSION=1.5.5
+TERMUX_PKG_SRCURL=https://github.com/cronie-crond/cronie/releases/download/cronie-${TERMUX_PKG_VERSION}/cronie-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=be34c79505e5544323281854744b9955ff16b160ee569f9df7c0dddae5720eac
+TERMUX_PKG_DEPENDS="bash"
+TERMUX_PKG_RECOMMENDS="nano"
+TERMUX_PKG_SUGGESTS="termux-services"
+TERMUX_PKG_CONFLICTS="busybox (<< 1.31.1-11)"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-anacron
+--disable-pam
+--with-editor=$TERMUX_PREFIX/bin/nano
+"
+
+TERMUX_PKG_SERVICE_SCRIPT=("crond" 'exec crond -n -s')
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	mkdir -p $TERMUX_PREFIX/var/run
+	mkdir -p $TERMUX_PREFIX/var/spool/cron
+	EOF
+}
