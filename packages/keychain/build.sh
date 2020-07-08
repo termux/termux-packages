aa0971da42dec951b44916a3d4809845d6566ec2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-09 01:13:45 +0300

    keychain: bump revision

--- packages/keychain/build.sh
+++ packages/keychain/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.funtoo.org/Keychain
 TERMUX_PKG_DESCRIPTION="keychain ssh-agent front-end"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.8.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/funtoo/keychain/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dcce703e5001211c8ebc0528f45b523f84d2bceeb240600795b4d80cb8475a0b
 TERMUX_PKG_DEPENDS="dash, gnupg"
