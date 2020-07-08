2086fb6b883469d0d6622497f0024d275912670e
Henrik Grimler <henrik@grimler.se>
2020-07-08 13:54:50 +0200

    hunspell{,-ru}: fix accidental build.sh mix up..

--- packages/hunspell/build.sh
+++ packages/hunspell/build.sh
@@ -1,21 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://hunspell.github.io
-TERMUX_PKG_DESCRIPTION="Russian dictionary for hunspell"
-TERMUX_PKG_LICENSE="LGPL-3.0"
-TERMUX_PKG_VERSION=20200604
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
-TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_DESCRIPTION="Spell checker"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.7.0
+TERMUX_PKG_REVISION=5
+TERMUX_PKG_SRCURL=https://github.com/hunspell/hunspell/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bb27b86eb910a8285407cf3ca33b62643a02798cf2eef468c0a74f6c3ee6bc8a
+TERMUX_PKG_DEPENDS="libc++, libiconv, ncurses, readline, hunspell-en-us"
+TERMUX_PKG_BREAKS="hunspell-dev"
+TERMUX_PKG_REPLACES="hunspell-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ui --with-readline"
+TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/share/hunspell/
-	# On checksum mismatch the files may have been updated:
-	#  https://cgit.freedesktop.org/libreoffice/dictionaries/log/ru_RU/ru_RU.aff
-	#  https://cgit.freedesktop.org/libreoffice/dictionaries/log/ru_RU/ru_RU.dic
-	# In which case we need to bump version and checksum used.
-	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/ru_RU/ru_RU.aff \
-			$TERMUX_PREFIX/share/hunspell/ru_RU.aff \
-			38ce7d4af78e211e9bafe4bf7e3d6a2c420591136cb738ec6648f8fdf6524cd7
-	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/ru_RU/ru_RU.dic \
-			$TERMUX_PREFIX/share/hunspell/ru_RU.dic \
-			3280afe7829128ff589b2c5d8f46810ed9b0eddcc85e9b1cc3d12dc08d4c1a0b
-	touch $TERMUX_PREFIX/share/hunspell/ru_RU.{aff,dic}
+termux_step_pre_configure() {
+	autoreconf -vfi
 }
