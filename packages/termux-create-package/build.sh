a0d48d2300fdf43bf1df8898cbed14255dc39e92
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-11 01:18:29 +0300

    termux-create-package: update to 0.10

--- packages/termux-create-package/build.sh
+++ packages/termux-create-package/build.sh
@@ -1,13 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-create-package
 TERMUX_PKG_DESCRIPTION="Utility to create Termux packages"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.9
+TERMUX_PKG_VERSION=0.10
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-create-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f472bb1e4c117fd35913dfddebf5a867764c824376ecb61b092d067e50c6d5af
+TERMUX_PKG_SHA256=5a5bda0e66475a4bb25f5a4f6f16a715eea98991bdb9471e84fb6d27488358dd
 TERMUX_PKG_DEPENDS="python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
-	cp termux-create-package $TERMUX_PREFIX/bin/termux-create-package
+	install -Dm700 -t $TERMUX_PREFIX/bin termux-create-package
 }
