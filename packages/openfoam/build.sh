cb8e5946b288028abebfdc181823cff878ae901c
Henrik Grimler <henrik@grimler.se>
2020-07-12 09:15:11 +0200

    openfoam: update to 21.06
    Finally a working build :)
    
    Tests done: run cavity tutorial on aarch64 and arm.
    
    TODO/things that would be nice to test:
    
    * Check if we actually need to set WM_{CC,CXX,CFLAGS,CXXFLAGS} in
      etc/config.sh/settings.
    * Test compilation with Gcc on device
    * Look at the __libc_malloc->malloc patch in sigFpe.C again, and try
      to upstream the fix so that it isn't as glibc specific.
    
    We need to remove -std=c++11 from CXXFLAGS, or else we get errors when building with
    our (rather new) version of CGAL, for example:
    
    /data/data/com.termux/files/usr/include/CGAL/array.h:72:44: error: no template named 'index_sequence' in namespace 'std'
    make_filled_array_aux(const T& value, std::index_sequence<Is...>)
                                          ~~~~~^
    
    /data/data/com.termux/files/usr/include/CGAL/number_utils.h:320:10: warning: 'decltype(auto)' type specifier is a C++14 extension [-Wc++14-extensions]
      decltype(auto) approximate_sqrt(const NT& nt)
               ^
    /data/data/com.termux/files/usr/include/CGAL/number_utils.h:320:1: error: deduced return types are a C++14 extension
      decltype(auto) approximate_sqrt(const NT& nt)
      ^

--- packages/openfoam/build.sh
+++ packages/openfoam/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.openfoam.com
 TERMUX_PKG_DESCRIPTION="OpenFOAM is a CFD software written in C++"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1906
-TERMUX_PKG_SRCURL=https://sourceforge.net/projects/openfoamplus/files/v$TERMUX_PKG_VERSION/OpenFOAM-v$TERMUX_PKG_VERSION.tgz
-TERMUX_PKG_SHA256=15e38c2dc659b63753a0dd3dff913222cc46d6a40089a1b76973dd741145f61a
-TERMUX_PKG_DEPENDS="openmpi, flex, boost, libc++"
+TERMUX_PKG_VERSION=2106
+TERMUX_PKG_SRCURL=https://develop.openfoam.com/Development/openfoam/-/archive/OpenFOAM-v${TERMUX_PKG_VERSION}/openfoam-OpenFOAM-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f8be6743ed4c534a54024a6837a22e3a7392a454e29e1a4655ae6a009064d64a
+TERMUX_PKG_DEPENDS="openmpi, flex, boost, cgal, fftw, readline, libc++"
 TERMUX_PKG_RM_AFTER_INSTALL="opt/OpenFOAM-v${TERMUX_PKG_VERSION}/build"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
@@ -21,46 +21,59 @@ termux_step_host_build() {
 		set -u
 		make
 	)
-	mkdir -p wmake/platforms
-	mv $TERMUX_PKG_SRCDIR/wmake/platforms/linuxGcc wmake/platforms/
-	mv $TERMUX_PKG_SRCDIR/wmake/platforms/linux64Gcc wmake/platforms/
+	mkdir -p platforms/tools
+	mv $TERMUX_PKG_SRCDIR/platforms/tools/linuxGcc platforms/tools/
+	mv $TERMUX_PKG_SRCDIR/platforms/tools/linux64Gcc platforms/tools/
 }
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" == "aarch64" ]; then
-		ARCH_FOLDER=linuxARM64Clang80
+		ARCH_FOLDER=linuxARM64Clang
 		TERMUX_COMPILER_PREFIX="aarch64-linux-android"
-		export ARCH=aarch64
+		ARCH=aarch64
 	elif [ "$TERMUX_ARCH" == "arm" ]; then
-		ARCH_FOLDER=linuxARM7Clang80
+		ARCH_FOLDER=linuxARM7Clang
 		TERMUX_COMPILER_PREFIX="arm-linux-androideabi"
-		export ARCH=armv7l
+		ARCH=armv7l
 	elif [ "$TERMUX_ARCH" == "i686" ]; then
-		ARCH_FOLDER=linuxClang80
+		ARCH_FOLDER=linuxClang
 		TERMUX_COMPILER_PREFIX="i686-linux-android"
-		export ARCH=i686
+		ARCH=i686
 	elif [ "$TERMUX_ARCH" == "x86_64" ]; then
-		ARCH_FOLDER=linux64Clang80
+		ARCH_FOLDER=linux64Clang
 		TERMUX_COMPILER_PREFIX="x86_64-linux-android"
-		export ARCH=x86_64
+		ARCH=x86_64
 	fi
 	sed -i "s%\@TERMUX_COMPILER_PREFIX\@%${TERMUX_COMPILER_PREFIX}%g" "$TERMUX_PKG_SRCDIR/wmake/rules/General/Clang/c"
 	sed -i "s%\@TERMUX_COMPILER_PREFIX\@%${TERMUX_COMPILER_PREFIX}%g" "$TERMUX_PKG_SRCDIR/wmake/rules/General/Clang/c++"
 	sed -i "s%\@TERMUX_COMPILER_PREFIX\@%${TERMUX_COMPILER_PREFIX}%g" "$TERMUX_PKG_SRCDIR/wmake/rules/General/general"
-	#Lots and lots of unset env. variables that "set -u" complains about
-	set +u
-	source "$TERMUX_PKG_SRCDIR/etc/bashrc" || true
-	set -u
-	mkdir -p wmake/platforms
-	cp -r $TERMUX_PKG_HOSTBUILD_DIR/wmake/platforms/linux64Gcc wmake/platforms/${ARCH_FOLDER}
+
+	mkdir -p platforms/tools
+	cp -r $TERMUX_PKG_HOSTBUILD_DIR/platforms/tools/linux64Gcc platforms/tools/${ARCH_FOLDER}
 	if [ $TERMUX_ARCH_BITS = 32 ]; then
-		cp -r $TERMUX_PKG_HOSTBUILD_DIR/wmake/platforms/linuxGcc wmake/platforms/${ARCH_FOLDER}
+		cp -r $TERMUX_PKG_HOSTBUILD_DIR/platforms/tools/linuxGcc platforms/tools/${ARCH_FOLDER}
 	else
-		cp -r $TERMUX_PKG_HOSTBUILD_DIR/wmake/platforms/linux64Gcc wmake/platforms/${ARCH_FOLDER}
+		cp -r $TERMUX_PKG_HOSTBUILD_DIR/platforms/tools/linux64Gcc platforms/tools/${ARCH_FOLDER}
 	fi
 }
 
 termux_step_make() {
+	# Set ARCH here again so that continued builds work
+	if [ "$TERMUX_ARCH" == "aarch64" ]; then
+		ARCH=aarch64
+	elif [ "$TERMUX_ARCH" == "arm" ]; then
+		ARCH=armv7l
+	elif [ "$TERMUX_ARCH" == "i686" ]; then
+		ARCH=i686
+	elif [ "$TERMUX_ARCH" == "x86_64" ]; then
+		ARCH=x86_64
+	fi
+
+	# Lots and lots of unset env. variables that "set -u"
+	# complains about, so disable exit on error temporarily
+	set +u
+	source "$TERMUX_PKG_SRCDIR"/etc/bashrc || true
+	set -u
 	unset LD_LIBRARY_PATH
 	./Allwmake
 	cd wmake/src
