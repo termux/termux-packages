40c5bc62c94dc2762c74a530f439da043c08e9d0
Henrik Grimler <henrik@grimler.se>
2020-07-12 10:54:01 +0200

    open-adventure: bump to include LICENSE file in package

--- packages/open-adventure/build.sh
+++ packages/open-adventure/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Forward-port of the original Colossal Cave Adventure fro
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=1.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=5dd0df7a7caebf78e7450535109b70e834fff2e4b1252284d69ec7ce36583409
 TERMUX_PKG_SRCURL=https://gitlab.com/esr/open-adventure/-/archive/${TERMUX_PKG_VERSION}/open-adventure-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libedit"
