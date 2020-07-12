831c87acc4a7ebea4d55fc992fd81720987eaf70
ifurther <55025025+ifurther@users.noreply.github.com>
2020-07-12 15:54:14 +0800

    fix version in url

--- packages/talib/build.sh
+++ packages/talib/build.sh
@@ -8,5 +8,5 @@ TERMUX_PKG_SHA256=9ff41efcb1c011a4b4b6dfc91610b06e39b1d7973ed5d4dee55029a0ac4dc6
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	curl https://sourceforge.net/p/ta-lib/code/HEAD/tree/tags/release-0-4-0/ta-lib/LICENSE.TXT?format=raw -o $TERMUX_PKG_SRCDIR/LICENSE
+	curl https://sourceforge.net/p/ta-lib/code/HEAD/tree/tags/release-${TERMUX_PKG_VERSION//./-}/ta-lib/LICENSE.TXT?format=raw -o $TERMUX_PKG_SRCDIR/LICENSE
 }
