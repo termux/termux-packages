180561ad02cbcd22989b035914a89b2811314d9f
Henrik Grimler <henrik@grimler.se>
2020-07-12 09:32:57 +0200

    netcdf-c: bump to include license file in package

--- packages/netcdf-c/build.sh
+++ packages/netcdf-c/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="NetCDF is a set of software libraries and self-describin
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=4.7.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=99930ad7b3c4c1a8e8831fb061cb02b2170fc8e5ccaeda733bd99c3b9d31666b
 TERMUX_PKG_SRCURL=https://github.com/Unidata/netcdf-c/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libhdf5, libcurl, openssl, libnghttp2"
