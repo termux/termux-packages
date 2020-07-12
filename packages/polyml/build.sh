c563d3985b539d77734505c033b9c04a5ca61b82
ifurther <55025025+ifurther@users.noreply.github.com>
2020-07-12 15:53:26 +0800

    add package polyml

---
+++ packages/polyml/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.polyml.org
+TERMUX_PKG_DESCRIPTION="Poly/ML is a Standard ML implementation originally written in an experimental language called Poly."
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_VERSION=5.8
+TERMUX_PKG_SRCURL=https://github.com/polyml/polyml/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=6bcc2c5af91f361ef9e0bb28f39ce20171b0beae73b4db3674df6fc793cec8bf
+#TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_BUILD_IN_SRC=true
