e8993f371a97edcbe5a95577c4fff5cb91c8f939
ifurther <55025025+ifurther@users.noreply.github.com>
2020-07-13 17:59:24 +0800

    add dependency libffi

--- packages/polyml/build.sh
+++ packages/polyml/build.sh
@@ -4,6 +4,6 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=5.8
 TERMUX_PKG_SRCURL=https://github.com/polyml/polyml/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6bcc2c5af91f361ef9e0bb28f39ce20171b0beae73b4db3674df6fc793cec8bf
-#TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_DEPENDS="libelf,libffi"
 TERMUX_PKG_BUILD_IN_SRC=true
-#TERMUX_PKG_EXTRA_CONFIGURE_ARGS=
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-system-libffi --with-pic ac_cv_host=$TERMUX_ARCH-generic-linux-gnu"
