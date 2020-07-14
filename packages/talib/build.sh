62d2c66dfe6d20f103dd9be9f64d5305339a9093
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-15 00:33:22 +0300

    Update build.sh

--- packages/talib/build.sh
+++ packages/talib/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=0.4.0
 TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/ta-lib/ta-lib-$TERMUX_PKG_VERSION-src.tar.gz
 TERMUX_PKG_SHA256=9ff41efcb1c011a4b4b6dfc91610b06e39b1d7973ed5d4dee55029a0ac4dc651
-#TERMUX_PKG_DEPENDS=""
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
