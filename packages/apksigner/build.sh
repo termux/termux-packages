c6bf6a602f1d80dce78e0d135b9be53ce7cb8161
Wetitpig <winsto003@hotmail.com>
2020-07-16 13:09:44 +0800

    Use full apksigner

--- packages/apksigner/build.sh
+++ packages/apksigner/build.sh
@@ -1,31 +1,35 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/fornwall/apksigner
+TERMUX_PKG_HOMEPAGE=https://developer.android.com/studio/command-line/apksigner
 TERMUX_PKG_DESCRIPTION="APK signing tool"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.7
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/fornwall/apksigner/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=340560c4f75af3501f037452bcf184fa48fd18bc877a4cce9a51a3fa047b4b38
+TERMUX_PKG_VERSION=${TERMUX_ANDROID_BUILD_TOOLS_VERSION}
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
+termux_step_pre_configure() {
+	# Requires Android SDK, not available on device
+	if $TERMUX_ON_DEVICE_BUILD; then
+		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
+	fi
+}
+
 termux_step_make() {
-	mkdir -p $TERMUX_PREFIX/share/{dex,man/man1}
+	mkdir -p $TERMUX_PREFIX/share/dex
+	SOURCEFILE=$ANDROID_HOME/build-tools/${TERMUX_PKG_VERSION}/lib/apksigner.jar
 
-	cp apksigner.1 $TERMUX_PREFIX/share/man/man1/
-
-	GRADLE_OPTS=" -Dorg.gradle.daemon=false" ./gradlew
 	$TERMUX_D8 \
 		--classpath $ANDROID_HOME/platforms/android-$TERMUX_PKG_API_LEVEL/android.jar \
 		--release \
 		--min-api $TERMUX_PKG_API_LEVEL \
 		--output $TERMUX_PKG_TMPDIR \
-		./build/libs/src-all.jar
+		$SOURCEFILE
 
 	cd $TERMUX_PKG_TMPDIR
-	jar cf apksigner.jar classes.dex
+	unzip $SOURCEFILE */*.txt
+	jar cf apksigner.jar classes.dex com/
 	mv apksigner.jar $TERMUX_PREFIX/share/dex/apksigner.jar
 
 	echo '#!/bin/sh' > $TERMUX_PREFIX/bin/apksigner
-	echo "dalvikvm -cp $TERMUX_PREFIX/share/dex/apksigner.jar net.fornwall.apksigner.Main \$@" >> $TERMUX_PREFIX/bin/apksigner
+	echo "dalvikvm -cp $TERMUX_PREFIX/share/dex/apksigner.jar com.android.apksigner.ApkSignerTool \$@" >> $TERMUX_PREFIX/bin/apksigner
 	chmod +x $TERMUX_PREFIX/bin/apksigner
 }
