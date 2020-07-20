824df64cab9b5f0653593fbcc00776e83c30d8c4
Henrik Grimler <henrik@grimler.se>
2020-07-20 21:41:55 +0200

    elvish: update to 0.14.0

--- packages/elvish/build.sh
+++ packages/elvish/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/elves/elvish
 TERMUX_PKG_DESCRIPTION="A friendly and expressive Unix shell"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=0.13.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.14.0
 TERMUX_PKG_SRCURL=https://github.com/elves/elvish/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=83275a4c36f66b831ba4864d1f5ffdc823616ed0a8e41b2a9a3e9fcba9279e27
+TERMUX_PKG_SHA256=36ed5e0318d75f7e7d616398e42477cfbd6fd2a1d1f108dee7941621cfb7378a
 
 termux_step_make() {
 	termux_setup_golang
