87edae0d54bf34cd420a693ef51fe2c1504f222d
Henrik Grimler <henrik@grimler.se>
2020-07-20 22:28:45 +0200

    gnuplot: update to 5.4.0

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://gnuplot.info/
 TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=5.2.8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.4.0
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=60a6764ccf404a1668c140f11cc1f699290ab70daa1151bb58fed6139a28ac37
+TERMUX_PKG_SHA256=eb4082f03a399fd1e9e2b380cf7a4f785e77023d8dcc7e17570c1b5570a49c47
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, readline, pango, libgd, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-x
@@ -20,8 +19,8 @@ termux_step_host_build() {
 }
 
 termux_step_post_make_install() {
-	mkdir -p $TERMUX_PREFIX/share/gnuplot/5.2/
+	mkdir -p $TERMUX_PREFIX/share/gnuplot/${TERMUX_PKG_VERSION:0:3}/
 
 	cp $TERMUX_PKG_HOSTBUILD_DIR/docs/gnuplot.gih \
-	   $TERMUX_PREFIX/share/gnuplot/5.2/gnuplot.gih
+	   $TERMUX_PREFIX/share/gnuplot/${TERMUX_PKG_VERSION:0:3}/gnuplot.gih
 }
