7749601c6fcbbc0ef2ac59edc2ff5fea64057165
Henrik Grimler <henrik@grimler.se>
2020-07-20 21:54:01 +0200

    fribidi: update to 1.0.10

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/fribidi/fribidi/
 TERMUX_PKG_DESCRIPTION="Implementation of the Unicode Bidirectional Algorithm"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.0.9
+TERMUX_PKG_VERSION=1.0.10
 TERMUX_PKG_SRCURL=https://github.com/fribidi/fribidi/releases/download/v$TERMUX_PKG_VERSION/fribidi-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=c5e47ea9026fb60da1944da9888b4e0a18854a0e2410bbfe7ad90a054d36e0c7
+TERMUX_PKG_SHA256=7f1c687c7831499bcacae5e8675945a39bacbad16ecaa945e9454a32df653c01
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BREAKS="fribidi-dev"
 TERMUX_PKG_REPLACES="fribidi-dev"
