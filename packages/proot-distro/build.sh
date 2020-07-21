955a7b7c5c8c9bdcc77c0b57c3c674eea6431e02
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-21 04:51:38 +0300

    proot-distro: update to 0.2.1
    Fixed out-of-sync version tag in script.

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_VERSION=0.2.1
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8ff992ad894fff4f2f52d3e575bc912c09881615f08ddf879c6998fd9c846ecb
+TERMUX_PKG_SHA256=4f24903d9b26c42382a6b9a2576dc522aea553a3c2912e4f1c441982eb7076de
 TERMUX_PKG_DEPENDS="bash, coreutils, curl, findutils, proot, tar"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
