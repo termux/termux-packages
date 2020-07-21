89586524cdd424441a26ef215f22fff74bfb6731
Henrik Grimler <henrik@grimler.se>
2020-07-21 20:30:35 +0200

    fdupes: update to 2.1.1

--- packages/fdupes/build.sh
+++ packages/fdupes/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/adrianlopezroche/fdupes
 TERMUX_PKG_DESCRIPTION="Duplicates file detector"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=2.0.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/adrianlopezroche/fdupes/releases/download/$TERMUX_PKG_VERSION/fdupes-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=eb9e3bd3e722ebb2a272e45a1073f78c60f8989b151c3661421b86b14b203410
-TERMUX_PKG_DEPENDS="ncurses,pcre2"
+TERMUX_PKG_VERSION=2.1.1
+TERMUX_PKG_SRCURL=https://github.com/adrianlopezroche/fdupes/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=37b1a499f6eb8b625b45641a2600be69ef591cd1e737e75afd50aa72ac215ea4
+TERMUX_PKG_DEPENDS="ncurses, pcre2"
