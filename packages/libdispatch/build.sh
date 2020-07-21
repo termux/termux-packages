8d200b419500c83e651c702357a2048efaf564b7
Henrik Grimler <henrik@grimler.se>
2020-07-21 21:46:02 +0200

    libdispatch: update to 5.2.3

--- packages/libdispatch/build.sh
+++ packages/libdispatch/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/apple/swift-corelibs-libdspatch
 TERMUX_PKG_DESCRIPTION="The libdispatch project, for concurrency on multicore hardware"
 TERMUX_PKG_LICENSE="Apache-2.0"
-_VERSION=5.2
+_VERSION=5.2.3
 TERMUX_PKG_VERSION=1:${_VERSION}
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${_VERSION}-RELEASE.tar.gz
-TERMUX_PKG_SHA256=11797a43c509a603434c64c1041f208f0f25c3233d6c6493d53704e682634edb
+TERMUX_PKG_SHA256=89b5910e80599d3168096f1dc9fb8883d6ecb042cdee144209f03b783249bdda
 TERMUX_PKG_DEPENDS="libc++, libblocksruntime"
