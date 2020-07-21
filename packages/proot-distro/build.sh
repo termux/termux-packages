f2410a47cb96c5b9af88a4817c79adedec7d0a1c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-21 04:28:17 +0300

    proot-distro: update to 0.2

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -1,12 +1,19 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.1
-TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v0.1.tar.gz
-TERMUX_PKG_SHA256=bf58e46a97fd2b98b8c48311acedf794f7618d0921834c0c6d8644c246665d04
-TERMUX_PKG_DEPENDS="bash, coreutils, proot, tar"
+TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8ff992ad894fff4f2f52d3e575bc912c09881615f08ddf879c6998fd9c846ecb
+TERMUX_PKG_DEPENDS="bash, coreutils, curl, findutils, proot, tar"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+# Allow to edit distribution plug-ins.
+TERMUX_PKG_CONFFILES="
+etc/proot-distro/alpine.sh
+etc/proot-distro/archlinux.sh
+etc/proot-distro/ubuntu.sh
+"
 
 termux_step_make_install() {
 	./install.sh
