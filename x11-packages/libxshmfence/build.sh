e13519446378eaf29e5aa3cb1ab87f7030de191f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-22 20:57:58 +0300

    libxshmfence: fix build-time dependencies

--- x11-packages/libxshmfence/build.sh
+++ x11-packages/libxshmfence/build.sh
@@ -6,4 +6,5 @@ TERMUX_PKG_VERSION=1.3
 TERMUX_PKG_REVISION=25
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libxshmfence-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=b884300d26a14961a076fbebc762a39831cb75f92bed5ccf9836345b459220c7
+TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-futex"
