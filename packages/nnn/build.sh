5a4eedcd45f61df6dc87bd5aeb78569bc5da720e
Fredrik Fornwall <fredrik@fornwall.net>
2020-07-23 16:01:23 +0200

    nnn: Update from 3.2 to 3.3

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jarun/nnn
 TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=3.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.3
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4ebbd024776153ecb79c75d1a58fc5cd7cd168c6e8217100b5edf322fdf9d4fd
+TERMUX_PKG_SHA256=16b245cf984d81a7e35f1a6a6f52bede4810784f06074d69369f664efdf32aca
 TERMUX_PKG_DEPENDS="file, findutils, readline, wget, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
