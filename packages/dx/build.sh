b5d8bc8ced4b0ca3a81ef9d4ce4733918eef4b6d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-26 03:20:58 +0300

    dx: use '-Xcompiler-option --compiler-filter=speed' dalvikvm flag

--- packages/dx/build.sh
+++ packages/dx/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://developer.android.com/tools/help/index.html
 TERMUX_PKG_DESCRIPTION="Command which takes in Java class files and converts them to format executable by Dalvik VM"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=1:1.16
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://dl.bintray.com/xeffyr/sources/dx/dx-android-${TERMUX_PKG_VERSION:2}.jar
 TERMUX_PKG_SHA256=b9b7917267876b74c8ff6707e7a576c93b6dfe8cacc4f1cc791d606bcbbb7bd5
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
@@ -19,6 +19,7 @@ termux_step_make_install() {
 	cat <<- EOF > "$TERMUX_PREFIX"/bin/dx
 	#!${TERMUX_PREFIX}/bin/sh
 	exec dalvikvm \
+		-Xcompiler-option --compiler-filter=speed \
 		-Xmx256m \
 		-cp ${TERMUX_PREFIX}/share/dex/dx.jar \
 		dx.dx.command.Main "\$@"
@@ -28,6 +29,7 @@ termux_step_make_install() {
 	cat <<- EOF > "$TERMUX_PREFIX"/bin/dx-merge
 	#!${TERMUX_PREFIX}/bin/sh
 	exec dalvikvm \
+		-Xcompiler-option --compiler-filter=speed \
 		-Xmx256m \
 		-cp ${TERMUX_PREFIX}/share/dex/dx.jar \
 		dx.dx.merge.DexMerger "\$@"
