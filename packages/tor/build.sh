0bdc9bc85ca33448989e7f6b218da5c298374194
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-27 23:43:46 +0300

    tor: bump revision

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=0.4.3.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6a2d0637d4e514be2ec574723a05065245cce51da78a21cec1dc831be5ccac62
 TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib"
