0a0ab99705f12d897d33f0300fc1790969ccb987
Fredrik Fornwall <fredrik@fornwall.net>
2020-07-27 09:46:05 +0200

    bash-completion: Update from 2.10 to 2.11

--- packages/bash-completion/build.sh
+++ packages/bash-completion/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/scop/bash-completion
 TERMUX_PKG_DESCRIPTION="Programmable completion for the bash shell"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.10
+TERMUX_PKG_VERSION=2.11
 TERMUX_PKG_SRCURL=https://github.com/scop/bash-completion/releases/download/${TERMUX_PKG_VERSION}/bash-completion-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=123c17998e34b937ce57bb1b111cd817bc369309e9a8047c0bcf06ead4a3ec92
+TERMUX_PKG_SHA256=73a8894bad94dee83ab468fa09f628daffd567e8bef1a24277f1e9a0daf911ac
 TERMUX_PKG_DEPENDS="bash"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
