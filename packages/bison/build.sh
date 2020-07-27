2113c22b9d3870ba2b4134652366eb84482f06e4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-27 23:42:40 +0300

    bison: bump revision

--- packages/bison/build.sh
+++ packages/bison/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/bison/
 TERMUX_PKG_DESCRIPTION="General-purpose parser generator"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=3.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/bison/bison-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=07b25fbf4de2f2c686e8bff50fdb69efda49b6f9f7377dad1884f9508a28592b
 TERMUX_PKG_DEPENDS="m4"
