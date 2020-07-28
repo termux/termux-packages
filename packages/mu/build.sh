27a9b7851eb53b4512e492a42e807c75ca6196fe
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-28 13:44:09 +0300

    mu: bump revision

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=1.4.12
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=98295cb697da051b5c0d8048bca2cc516551fede243f7f0915903b3e7f2f3dce
 TERMUX_PKG_DEPENDS="glib, libc++, libxapian, libgmime"
