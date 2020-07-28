a530c9da4a6a4d911b341ce20950700e6b73b92c
Fredrik Fornwall <fredrik@fornwall.net>
2020-07-28 11:03:46 +0200

    libmpfr: Update from 4.0.2 to 4.1.0

--- packages/libmpfr/build.sh
+++ packages/libmpfr/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.mpfr.org/
 TERMUX_PKG_DESCRIPTION="C library for multiple-precision floating-point computations with correct rounding"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=4.0.2
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=4.1.0
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/mpfr/mpfr-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=1d3be708604eae0e42d578ba93b390c2a145f17743a744d8f3f8c2ad5855a38a
+TERMUX_PKG_SHA256=0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_BREAKS="libmpfr-dev"
 TERMUX_PKG_REPLACES="libmpfr-dev"
