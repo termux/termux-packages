bbaf6b07d028eb6ca055b4e67a017bb7b6553873
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-07-29 00:47:42 +0530

    update: newsboat 2.20

--- packages/newsboat/build.sh
+++ packages/newsboat/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://newsboat.org/
 TERMUX_PKG_DESCRIPTION="RSS/Atom feed reader for the text console"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.19
+TERMUX_PKG_VERSION=2.20
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://newsboat.org/releases/${TERMUX_PKG_VERSION}/newsboat-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ba484c825bb903daf6d33d55126107b59e41111b455d368362208f1825403d1b
+TERMUX_PKG_SHA256=d3a7d5d5a33a89f0970b8651cb95239816e3361d02eb153effeaa1c915a7e402
 TERMUX_PKG_DEPENDS="libc++, libiconv, libandroid-support, libandroid-glob, json-c, libsqlite, libcurl, libxml2, stfl, ncurses, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="share/locale"
