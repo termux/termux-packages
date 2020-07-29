5ad3cbf58a852835965df3e997015ec86937fcd1
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-07-30 00:32:19 +0530

    Update build.sh

--- packages/youtubedr/build.sh
+++ packages/youtubedr/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Download youtube video in Golang"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=1.2.4
-TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/kkdai/youtube/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d6e7baa7ea61d366c3aaafd88215e675254a378e035fe52709a9de2d9c16d56e
 
