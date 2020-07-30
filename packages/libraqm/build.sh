386bafa6b4e5f1514c0707a035bae70f4c578ba5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-07-30 23:24:11 +0300

    Update build.sh

--- packages/libraqm/build.sh
+++ packages/libraqm/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.7.0
 TERMUX_PKG_SRCURL=https://github.com/HOST-Oman/libraqm/releases/download/v$TERMUX_PKG_VERSION/raqm-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e28575ecdd4e8a1d277d9be8268bb663ce1e476aaf55eb0456787821ddf0f941
-TERMUX_PKG_DEPENDS="python,freetype,harfbuzz,fribidi"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="freetype, harfbuzz, fribidi"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-doc"
