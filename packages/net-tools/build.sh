3457d841aac0af13591835d64cff2a57abfcba02
Henrik Grimler <henrik@grimler.se>
2020-08-01 08:48:19 +0200

    Remove "local" from places it does not do anything
    Needs to be used in functions to have an effect.

--- packages/net-tools/build.sh
+++ packages/net-tools/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Configuration tools for Linux networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.60.2017.02.21
 TERMUX_PKG_REVISION=3
-local commit=479bb4a7e11a4084e2935c0a576388f92469225b
+commit=479bb4a7e11a4084e2935c0a576388f92469225b
 # We use a mirror to avoid using
 # https://sourceforge.net/code-snapshots/git/n/ne/net-tools/code.git/net-tools-code-$commit.zip
 # which does not work all the time (sourceforge caching system):
