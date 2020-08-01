888a30a32c35fa39f9efd5c22acf3ee68eb2456d
Henrik Grimler <henrik@grimler.se>
2020-08-01 12:51:36 +0200

    xcb-proto: remove __pycache__ dir to not conflict with python
    Both python and xcb-proto contains xcbgen/__pycache__ directory with
    common files.

--- packages/xcb-proto/build.sh
+++ packages/xcb-proto/build.sh
@@ -4,9 +4,12 @@ TERMUX_PKG_DESCRIPTION="XML-XCB protocol descriptions"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.14
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=186a3ceb26f9b4a015f5a44dcc814c93033a5fc39684f36f1ecc79834416a605
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_CONFLICTS="xcbproto"
 TERMUX_PKG_REPLACES="xcbproto"
+
+python_version=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+TERMUX_PKG_RM_AFTER_INSTALL="lib/python${python_version}/site-packages/xcbgen/__pycache__"
