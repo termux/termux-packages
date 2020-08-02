ef5e91c2edd0cd6460b345fa5bd0fff9da3cdcc9
ifurther <55025025+ifurther@users.noreply.github.com>
2020-08-02 23:26:39 +0800

    add some patch

--- packages/freeimage/build.sh
+++ packages/freeimage/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0, GPL-2.0, other"
 TERMUX_PKG_VERSION=3.18.0
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/freeimage/files/Source%20Distribution/$TERMUX_PKG_VERSION/FreeImage${TERMUX_PKG_VERSION//./}.zip/download
 TERMUX_PKG_SHA256=f41379682f9ada94ea7b34fe86bf9ee00935a3147be41b6569c9605a53e438fd
-TERMUX_PKG_DEPENDS="ndk-sysroot"
+#TERMUX_PKG_DEPENDS="ndk-sysroot"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_get_source() {
@@ -16,6 +16,7 @@ termux_step_get_source() {
 	#cd $TERMUX_PKG_SRCDIR
 	unzip -d $TERMUX_PKG_SRCDIR $TERMUX_PKG_CACHEDIR/freeimage.zip
         mv $TERMUX_PKG_SRCDIR/FreeImage/* $TERMUX_PKG_SRCDIR/
-	wget https://android.googlesource.com/platform/bionic/+/2491b2377a0adf3e0487bebc0d63d04dd1ac4c25/libc/include/bits/swab.h -P $TERMUX_PKG_SRCDIR/Source/LibRawLite/internal/
+	#curl https://android.googlesource.com/platform/bionic/+/master/libc/bionic/swab.cpp -o
+	
 }
 
