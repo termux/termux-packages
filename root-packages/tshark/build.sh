06d4e51809ebdf3bb6b52cc261949d2e4708c487
Henrik Grimler <henrik@grimler.se>
2020-08-03 10:57:15 +0200

    tshark: update to 3.2.5

--- root-packages/tshark/build.sh
+++ root-packages/tshark/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.wireshark.org/
 TERMUX_PKG_DESCRIPTION="Network protocol analyzer and sniffer"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.2.4
+TERMUX_PKG_VERSION=3.2.5
 TERMUX_PKG_SRCURL=https://www.wireshark.org/download/src/all-versions/wireshark-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d17d461e849e2d0b033431c45f71d8ee8ec3c8faa232a6ad63069a47927db8aa
+TERMUX_PKG_SHA256=bd89052a5766cce08b1090df49628567e48cdd24bbaa47667c851bac6aaac940
 TERMUX_PKG_DEPENDS="glib, libgpg-error, libgcrypt, libnl, libpcap, libgnutls, openssl, libiconv, libcap"
 TERMUX_PKG_BREAKS="tshark-dev"
 TERMUX_PKG_REPLACES="tshark-dev"
