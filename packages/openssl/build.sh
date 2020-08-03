73f0e9654d66748c5181f9ba711e2f62b199edde
Henrik Grimler <henrik@grimler.se>
2020-08-03 13:47:02 +0200

    openssl: make package safe for on device builds
    Need to verify that library symlinks are created when not compiling on
    device though.

--- packages/openssl/build.sh
+++ packages/openssl/build.sh
@@ -15,16 +15,10 @@ TERMUX_PKG_BREAKS="openssl-tool (<< 1.1.1b-1), openssl-dev"
 TERMUX_PKG_REPLACES="openssl-tool (<< 1.1.1b-1), openssl-dev"
 
 termux_step_configure() {
-	# Certain packages are not safe to build on device because their
-	# build.sh script deletes specific files in $TERMUX_PREFIX.
-	if $TERMUX_ON_DEVICE_BUILD; then
-		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
-	fi
-
 	CFLAGS+=" -DNO_SYSLOG"
 
 	perl -p -i -e "s@TERMUX_CFLAGS@$CFLAGS@g" Configure
-	rm -Rf $TERMUX_PREFIX/lib/libcrypto.* $TERMUX_PREFIX/lib/libssl.*
+
 	test $TERMUX_ARCH = "arm" && TERMUX_OPENSSL_PLATFORM="android-arm"
 	test $TERMUX_ARCH = "aarch64" && TERMUX_OPENSSL_PLATFORM="android-arm64"
 	test $TERMUX_ARCH = "i686" && TERMUX_OPENSSL_PLATFORM="android-x86"
@@ -40,21 +34,20 @@ termux_step_configure() {
 		no-tests
 }
 
-termux_step_make() {
+termux_step_post_configure() {
 	make depend
-	make -j $TERMUX_MAKE_PROCESSES all
 }
 
 termux_step_make_install() {
 	# "install_sw" instead of "install" to not install man pages:
 	make -j 1 install_sw MANDIR=$TERMUX_PREFIX/share/man MANSUFFIX=.ssl
 
-	mkdir -p $TERMUX_PREFIX/etc/tls/
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/tls/
 
-	cp apps/openssl.cnf $TERMUX_PREFIX/etc/tls/openssl.cnf
+	cp apps/openssl.cnf $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/tls/openssl.cnf
 
 	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
 		$TERMUX_PKG_BUILDER_DIR/add-trusted-certificate \
-		> $TERMUX_PREFIX/bin/add-trusted-certificate
-	chmod 700 $TERMUX_PREFIX/bin/add-trusted-certificate
+		> $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/add-trusted-certificate
+	chmod 700 $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/add-trusted-certificate
 }
