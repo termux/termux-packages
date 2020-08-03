57e3d3161a1e333ce6f5088275d9ad42ec7a3df4
Henrik Grimler <henrik@grimler.se>
2020-08-03 11:30:13 +0200

    frida: switch to using the termux_git_clone_src function

--- root-packages/frida-server/build.sh
+++ root-packages/frida-server/build.sh
@@ -6,6 +6,7 @@ _MAJOR_VERSION=12
 _MINOR_VERSION=8
 _MICRO_VERSION=20
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.${_MINOR_VERSION}.${_MICRO_VERSION}
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_SRCURL=https://github.com/frida/frida.git
 TERMUX_PKG_DEPENDS="libiconv, python"
 TERMUX_PKG_BUILD_DEPENDS="openssl"
@@ -16,25 +17,6 @@ ANDROID_NDK_ROOT=$NDK
 TERMUX_PKG_HOSTBUILD=true
 _PYTHON_VERSION=3.8
 
-termux_step_extract_package() {
-	local CHECKED_OUT_FOLDER=$TERMUX_PKG_CACHEDIR/checkout-$TERMUX_PKG_VERSION
-	if [ ! -d $CHECKED_OUT_FOLDER ]; then
-		local TMP_CHECKOUT=$TERMUX_PKG_TMPDIR/tmp-checkout
-		rm -Rf $TMP_CHECKOUT
-		mkdir -p $TMP_CHECKOUT
-
-		git clone --depth 1 \
-			--branch $TERMUX_PKG_VERSION \
-			$TERMUX_PKG_SRCURL \
-			$TMP_CHECKOUT
-		cd $TMP_CHECKOUT
-		git submodule update --init --recursive
-		mv $TMP_CHECKOUT $CHECKED_OUT_FOLDER
-	fi
-
-	rm -rf $TERMUX_PKG_SRCDIR
-	cp -Rf $CHECKED_OUT_FOLDER $TERMUX_PKG_SRCDIR
-}
 
 termux_step_host_build () {
 	local node_version=13.9.0
