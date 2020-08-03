365eda8923dcc4e403f3c27412739f2b5c33f376
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-03 23:12:37 +0300

    new package: okc-agents
    Requested in https://github.com/termux/termux-packages/issues/4352.

---
+++ packages/okc-agents/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/DDoSolitary/okc-agents
+TERMUX_PKG_DESCRIPTION="OpenKeychain agents for GnuPG and OpenSSH"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=0.1.1
+TERMUX_PKG_SRCURL=https://github.com/DDoSolitary/okc-agents/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=51be3713c7326676aad3015cf40db91dc966a917ec806aebcf066509f40c5b86
+TERMUX_PKG_BUILD_IN_SRC=true
