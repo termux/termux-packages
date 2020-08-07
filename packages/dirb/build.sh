097a1eb9f587f940ef93f9357a1e3f8b4da4a59f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-07 17:30:37 +0300

    dirb: rename gendict to dirb-gendict
    Avoid conflict with icu-devtools.
    
    https://github.com/termux/termux-packages/issues/5661

--- packages/dirb/build.sh
+++ packages/dirb/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Web Directory Fuzzer."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
 TERMUX_PKG_VERSION=2.22
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/dirb/files/dirb/${TERMUX_PKG_VERSION}/dirb${TERMUX_PKG_VERSION/./}.tar.gz
 TERMUX_PKG_SHA256=f3748ade231ca211a01acbec31cc6a3b576f6c56c906d73329d7dbb79f60fc2c
 TERMUX_PKG_DEPENDS="libcurl"
@@ -16,4 +17,5 @@ termux_step_post_make_install() {
 	mkdir -p "$TERMUX_PREFIX"/share/dirb
 	cp -rf  "$TERMUX_PKG_SRCDIR"/wordlists "$TERMUX_PREFIX"/share/dirb/wordlists
 	find "$TERMUX_PREFIX"/share/dirb/wordlists -type f | xargs chmod 600
+	mv -f "$TERMUX_PREFIX"/bin/gendict "$TERMUX_PREFIX"/bin/dirb-gendict
 }
