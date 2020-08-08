5659583c650c1f6114773ee9a6d8ec7a5576dcdb
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-09 01:07:29 +0200

    libjasper: Update from 2.0.17 to 2.0.19

--- packages/libjasper/build.sh
+++ packages/libjasper/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.ece.uvic.ca/~frodo/jasper/
 TERMUX_PKG_DESCRIPTION="Library for manipulating JPEG-2000 files"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=2.0.17
+TERMUX_PKG_VERSION=2.0.19
 TERMUX_PKG_SRCURL=https://github.com/mdadams/jasper/archive/version-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9a3524aa17795ea10f476d7071e27dd9fc0077d9ffbf2ea49b9f18de0bfe7fa1
+TERMUX_PKG_SHA256=b9d16162a088617ada36450f2374d72165377cb64b33ed197c200bcfb73ec76c
 TERMUX_PKG_DEPENDS="libjpeg-turbo"
 TERMUX_PKG_BREAKS="libjasper-dev"
 TERMUX_PKG_REPLACES="libjasper-dev"
