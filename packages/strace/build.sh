c2f338cba5a91f0795bf1694d58f6c2776a905c2
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-09 00:39:52 +0200

    strace: Update from 5.7 to 5.8

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=5.7
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=5.8
 TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=b284b59f9bcd95b9728cea5bd5c0edc5ebe360af73dc76fbf6334f11c777ccd8
+TERMUX_PKG_SHA256=df4a669f7fff9cc302784085bd4b72fab216a426a3f72c892b28a537b71e7aa9
 TERMUX_PKG_DEPENDS="libdw"
 
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
