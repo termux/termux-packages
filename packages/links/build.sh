b2ffe448a4b26bc423f31ae6d838366f15fece94
Butta <repo@butta.fastem.com>
2020-08-09 20:23:00 +0530

    links: update from 2.20.2 to 2.21.0

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://links.twibright.com
 TERMUX_PKG_DESCRIPTION="Links is a text and graphics mode WWW browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.20.2
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=2.21
 TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f0ec13b019e5501ef29858bf61a3bf89cece0b0f065b5064be242bce84e675bd
+TERMUX_PKG_SHA256=f0e1fd131b5e9ff943bcc667e0aa6331048749ad4b921b6fadd14e919d9f79ac
 TERMUX_PKG_DEPENDS="libbz2, libevent, liblzma, libpng, librsvg, libtiff, libx11, openssl, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
 
