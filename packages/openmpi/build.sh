c54915dafd97af1efcd0c4deadaf9aafd8bc106f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-10 00:36:32 +0300

    openmpi: disable -Wl,--as-needed to force-link with libandroid-shmem

--- packages/openmpi/build.sh
+++ packages/openmpi/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open source Message Passing Interface implementation"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=4.0.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://download.open-mpi.org/release/open-mpi/v${TERMUX_PKG_VERSION:0:3}/openmpi-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=dca264f420411f540a496bdd131bffd83e325fc9006286b39dd19b62d7368233
 TERMUX_PKG_DEPENDS="libandroid-shmem"
@@ -20,6 +20,7 @@ ac_cv_member_struct_ifreq_ifr_hwaddr=no
 termux_step_pre_configure () {
 	# rindex is an obsolete version of strrchr which is not available in Android:
 	CFLAGS+=" -Drindex=strrchr -Dbcmp=memcmp"
+	LDFLAGS="${LDFLAGS/-Wl,--as-needed/}"
 	LDFLAGS+=" -landroid-shmem"
 	if [ $TERMUX_ARCH == "i686" ]; then
 		# fails with "undefined reference to __atomic..."
