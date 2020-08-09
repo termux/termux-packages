163cabae43e236a5043c4a8bb9efadd49339d641
Wetitpig <winsto003@hotmail.com>
2020-08-09 09:20:58 +0800

    Add solidity

---
+++ packages/solidity/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://solidity.readthedocs.io
+TERMUX_PKG_DESCRIPTION="An Etherem smart contract-oriented language."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=0.7.0
+TERMUX_PKG_SRCURL=https://github.com/ethereum/solidity/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=11104522684d6869e01f7fb0ddf3cfd6475c06ee2f1203d65e8bf636ddc7053f
+TERMUX_PKG_DEPENDS="boost-static, z3"
+
