c62e6c2d3cfed9473851a8c4baa9707fb28a9ddf
Butta <repo@butta.fastem.com>
2020-08-09 22:42:33 +0530

    wren: update from 0.1.0 to 0.3.0

--- packages/wren/build.sh
+++ packages/wren/build.sh
@@ -1,17 +1,26 @@
 TERMUX_PKG_HOMEPAGE=http://wren.io/
 TERMUX_PKG_DESCRIPTION="Small, fast, class-based concurrent scripting language"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.1.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.3.0
 TERMUX_PKG_SRCURL=https://github.com/wren-lang/wren/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ebf8687dfdb55997a3fc263d41f306c6f40d9562ccbd945d9c12c48795388eae
-TERMUX_PKG_DEPENDS="libuv"
+TERMUX_PKG_SHA256=c566422b52a18693f57b15ae4c9459604e426ea64eddb5fbf2844d8781aa4eb7
 TERMUX_PKG_BREAKS="wren-dev"
 TERMUX_PKG_REPLACES="wren-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_NO_STATICSPLIT=true
+
+termux_step_make() {
+	local QUIET_BUILD=
+	if [ "$TERMUX_QUIET_BUILD" = true ]; then
+		QUIET_BUILD="-s"
+	fi
+
+	cd projects/make
+	make -j $TERMUX_MAKE_PROCESSES $QUIET_BUILD
+}
 
 termux_step_make_install() {
-	install -Dm700 "$TERMUX_PKG_SRCDIR"/bin/wren \
+	install -Dm700 "$TERMUX_PKG_SRCDIR"/bin/wren_test \
 		"$TERMUX_PREFIX"/bin/wren
 
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/src/include/wren.h \
@@ -19,4 +28,7 @@ termux_step_make_install() {
 
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/lib/libwren.so \
 		"$TERMUX_PREFIX"/lib/libwren.so
+
+	install -Dm600 "$TERMUX_PKG_SRCDIR"/lib/libwren.a \
+		"$TERMUX_PREFIX"/lib/libwren.a
 }
