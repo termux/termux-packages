5ff147a90d80a6e7deee168c84b6f533fd09247a
Butta <repo@butta.fastem.com>
2020-08-09 14:02:13 +0530

    gegl: update from 0.4.24 to 0.4.26

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://gegl.org/
 TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.4.24
+TERMUX_PKG_VERSION=0.4.26
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7765499f27341b0d16032e665319cbc12876483ff6a944fcdf24a9c58e3e254a
+TERMUX_PKG_SHA256=0f371e2ed2b92162fefd3dde743e648ca08a6a1b2b05004867fbddc7e211e424
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, zlib"
 TERMUX_PKG_BREAKS="gegl-dev"
 TERMUX_PKG_REPLACES="gegl-dev"
