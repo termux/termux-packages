9ae491e849448dbd853679353fe337f15c82300e
Butta <repo@butta.fastem.com>
2020-08-10 01:18:44 +0530

    newsboat: revert to 2.19

--- packages/newsboat/build.sh
+++ packages/newsboat/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://newsboat.org/
 TERMUX_PKG_DESCRIPTION="RSS/Atom feed reader for the text console"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.20.1
+TERMUX_PKG_VERSION=2.19
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://newsboat.org/releases/${TERMUX_PKG_VERSION}/newsboat-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=82782079b75fe307f7a5a17dff9e712aa5975678fa550fb728d5a46867943566
+TERMUX_PKG_SHA256=ba484c825bb903daf6d33d55126107b59e41111b455d368362208f1825403d1b
 TERMUX_PKG_DEPENDS="libc++, libiconv, libandroid-support, libandroid-glob, json-c, libsqlite, libcurl, libxml2, stfl, ncurses, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="share/locale"
