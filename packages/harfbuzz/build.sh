e19c4ca9ed91e28f1d7952438a929897cb61a576
Butta <repo@butta.fastem.com>
2020-08-09 19:02:06 +0530

    harfbuzz: update from 2.6.8 to 2.7.0 and switch to meson build

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,12 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.6.8
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/releases/download/${TERMUX_PKG_VERSION}/harfbuzz-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6648a571a27f186e47094121f0095e1b809e918b3037c630c7f38ffad86e3035
+TERMUX_PKG_VERSION=2.7.0
+TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4dba05de1fd44705f54c40d801e0e3d4833555d004cb611cc18675173feae75b
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
 TERMUX_PKG_BREAKS="harfbuzz-dev"
 TERMUX_PKG_REPLACES="harfbuzz-dev"
 TERMUX_PKG_BUILD_DEPENDS="libicu"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-icu=yes --with-graphite2=yes --disable-gtk-doc-html"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgraphite=enabled"
+
+termux_step_post_get_source() {
+	mv CMakeLists.txt CMakeLists.txt.unused
+}
