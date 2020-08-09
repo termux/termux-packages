cb619351d118ce4912aec388fbb040ef4338c090
Wetitpig <winsto003@hotmail.com>
2020-08-09 09:39:37 +0800

    Fix link to release

--- packages/solidity/build.sh
+++ packages/solidity/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://solidity.readthedocs.io
 TERMUX_PKG_DESCRIPTION="An Etherem smart contract-oriented language."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=0.7.0
-TERMUX_PKG_SRCURL=https://github.com/ethereum/solidity/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=11104522684d6869e01f7fb0ddf3cfd6475c06ee2f1203d65e8bf636ddc7053f
+TERMUX_PKG_SRCURL=https://github.com/ethereum/solidity/releases/download/v${TERMUX_PKG_VERSION}/solidity_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=86e782a88eaaf4aa98f4e1e915f46b5bc5f596ea86c784fb911dc6e4c04309bf
 TERMUX_PKG_DEPENDS="boost-static, z3"
 
