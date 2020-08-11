f027cd96e113b310c2532483a41657237d5cd2c6
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-11 20:47:48 +0300

    new package: libfuse3

---
+++ root-packages/libfuse3/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/libfuse/libfuse
+TERMUX_PKG_DESCRIPTION="FUSE (Filesystem in Userspace) is an interface for userspace programs to export a filesystem to the Linux kernel"
+TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
+TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
+TERMUX_PKG_VERSION=3.9.1
+TERMUX_PKG_SRCURL=https://github.com/libfuse/libfuse/archive/fuse-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4f3dea4979c30fbd85f46b8812bee5945bd6bb4487165eb01ecde57bbfbb332f
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Ddisable-mtab=true
+-Dexamples=false
+-Dsbindir=bin
+-Dmandir=share/man
+-Dudevrulesdir=$TERMUX_PREFIX/etc/udev/rules.d
+-Duseroot=false
+"
