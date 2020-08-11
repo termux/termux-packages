ae6547a3bbc5b7694ca4459248a1cf611c5609d4
valpogus <32718480+valpogus@users.noreply.github.com>
2020-08-11 13:14:56 +0200

    mediainfo: update from 20.03 to 20.08

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mediaarea.net/en/MediaInfo
 TERMUX_PKG_DESCRIPTION="Library for reading information from media files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
-TERMUX_PKG_VERSION=20.03
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=20.08
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/libmediainfo/${TERMUX_PKG_VERSION}/libmediainfo_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=90ea6395a5b5b6a138c2f85e9afe9723cb97bca6f9137529a0a2052acf2fe0d7
+TERMUX_PKG_SHA256=700365b85259b86dcf0278568c6a7f998e6caa9ff88504dda4a8f8b4b45bbd2b
 TERMUX_PKG_DEPENDS="libcurl, libzen, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-shared --enable-static --with-libcurl"
 
