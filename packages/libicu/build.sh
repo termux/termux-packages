ab299fe2b2f135ed734d4ee05d35d3de15c3b387
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-12 13:43:45 +0200

    libicu: Update from 66.1 to 67.1
    node.js updates will come as soon as this package has been built.

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -4,12 +4,9 @@ TERMUX_PKG_LICENSE="BSD"
 # We override TERMUX_PKG_SRCDIR termux_step_post_get_source so need to do
 # this hack to be able to find the license file.
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
-# Function `icu::number::FormattedNumber::getAllFieldPositions` was removed
-# from v67 and that breaks packages nodejs, nodejs-lts.
-TERMUX_PKG_VERSION=66.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=67.1
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
-TERMUX_PKG_SHA256=52a3f2209ab95559c1cf0a14f24338001f389615bf00e2585ef3dbc43ecf0a2e
+TERMUX_PKG_SHA256=94a80cd6f251a53bd2a997f6f1b5ac6653fe791dfab66e1eb0227740fb86d5dc
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BREAKS="libicu-dev"
 TERMUX_PKG_REPLACES="libicu-dev"
