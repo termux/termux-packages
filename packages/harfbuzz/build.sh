aa5f007f829308997ddf2466c2e1fda833de66ce
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-12 16:05:21 +0200

    harfbuzz: Bump after libicu update

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=2.7.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4dba05de1fd44705f54c40d801e0e3d4833555d004cb611cc18675173feae75b
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
