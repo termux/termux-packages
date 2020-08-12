fc56fe2e0d92e43b9a55e32661e6a0123487f3a3
Butta <repo@butta.fastem.com>
2020-08-12 10:03:05 +0530

    exiftool: update from 12.03 to 12.04
    gettext: update from 0.20.2 to 0.21
    
    gifski: update from 0.10.4 to 1.2.0
    
    git-delta: update from 0.3.0 to 0.4.1

--- packages/git-delta/build.sh
+++ packages/git-delta/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dandavison/delta
 TERMUX_PKG_DESCRIPTION="A syntax-highlighter for git and diff output"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.3.0
+TERMUX_PKG_VERSION=0.4.1
 TERMUX_PKG_SRCURL=https://github.com/dandavison/delta/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4ff8d5864306f130be8e0da3d8013bcc4ece082835d4cc5395775c669111ed77
+TERMUX_PKG_SHA256=5c2e46e398702b13b2768043ba5dc6bea899fb34271120bad4608ff9a64b0434
 TERMUX_PKG_DEPENDS="git"
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -16,6 +16,6 @@ termux_step_pre_configure() {
 termux_step_post_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin \
 		"$TERMUX_PKG_SRCDIR/target/$CARGO_TARGET_NAME"/release/delta
-	install -Dm600 "$TERMUX_PKG_SRCDIR"/completion/completion.bash \
+	install -Dm600 "$TERMUX_PKG_SRCDIR"/etc/completion/completion.bash \
 		"$TERMUX_PREFIX"/share/bash-completion/completions/delta
 }
