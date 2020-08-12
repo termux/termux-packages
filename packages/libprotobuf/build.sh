fcdd6d7ca083d95c598f983f2657c0419b8f8eb4
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-12 12:07:38 +0200

    libprotobuf: Update from 3.12.2 to 3.12.4

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Protocol buffers C++ library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 # TERMUX_PKG_SRCDIR is overriden below so we need to specify license file
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
-TERMUX_PKG_VERSION=1:3.12.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1:3.12.4
 TERMUX_PKG_SRCURL=https://github.com/google/protobuf/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=bb8ce9ba11eb7bccf080599fe7cad9cc461751c8dd1ba61701c0070d58cde973
+TERMUX_PKG_SHA256=512e5a674bf31f8b7928a64d8adf73ee67b8fe88339ad29adaa3b84dbaa570d8
 TERMUX_PKG_DEPENDS="libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev"
 TERMUX_PKG_REPLACES="libprotobuf-dev"
