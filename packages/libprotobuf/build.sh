a19b76b1592d9abe3c1ebd252d30c3c559e8e078
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-12 12:40:07 +0200

    Rebump libprotof and users

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD 3-Clause"
 # TERMUX_PKG_SRCDIR is overriden below so we need to specify license file
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_VERSION=1:3.12.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/google/protobuf/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=512e5a674bf31f8b7928a64d8adf73ee67b8fe88339ad29adaa3b84dbaa570d8
 TERMUX_PKG_DEPENDS="libc++, zlib"
