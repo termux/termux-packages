08996943f849def5c22e5eb0c2022fda9a415b00
Butta <repo@butta.fastem.com>
2020-08-13 16:25:09 +0530

    Bump knockd, kubectl, and libcap packages

--- packages/libcap/build.sh
+++ packages/libcap/build.sh
@@ -10,6 +10,7 @@ TERMUX_PKG_REPLACES="libcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
+	# bump
 	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" PREFIX="$TERMUX_PREFIX"
 }
 
