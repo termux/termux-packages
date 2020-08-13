a770bd4cd5b680f1cd7782b8725f4335a79a1ea4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-13 20:39:06 +0000

    sdl2-mixer: specify license file

--- x11-packages/sdl2-mixer/build.sh
+++ x11-packages/sdl2-mixer/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://www.libsdl.org/projects/SDL_mixer
 TERMUX_PKG_DESCRIPTION="A simple multi-channel audio mixer"
 TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="COPYING.txt"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=2.0.4
 TERMUX_PKG_REVISION=12
