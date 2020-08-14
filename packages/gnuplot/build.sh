6f5f526bb5ae2dc0096c73887dee6e3c5fb38d59
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-14 16:01:23 +0300

    gnuplot: provide support for X11
    Merging with gnuplot-x from x11-packages repository as this
    one already has necessary dependencies.

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -2,16 +2,20 @@ TERMUX_PKG_HOMEPAGE=http://gnuplot.info/
 TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=5.4.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=eb4082f03a399fd1e9e2b380cf7a4f785e77023d8dcc7e17570c1b5570a49c47
-TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, readline, pango, libgd, zlib"
+TERMUX_PKG_DEPENDS="libandroid-support, libc++, libgd, libiconv, libx11, pango, readline, zlib"
+TERMUX_PKG_BREAKS="gnuplot-x"
+TERMUX_PKG_REPLACES="gnuplot-x"
+TERMUX_PKG_HOSTBUILD=true
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---without-x
+--with-x
 --without-lua
 --with-bitmap-terminals
 --without-latex
 "
-TERMUX_PKG_HOSTBUILD=true
 
 termux_step_host_build() {
 	"$TERMUX_PKG_SRCDIR/configure"
