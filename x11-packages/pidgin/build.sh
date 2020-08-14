0b13665a9e31b1e148fc0e87a7455f3a022a38cf
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-14 13:12:36 +0000

    pidgin: update to 2.14.1
    %ci:no-build

--- x11-packages/pidgin/build.sh
+++ x11-packages/pidgin/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://pidgin.im/
 TERMUX_PKG_DESCRIPTION="Multi-protocol instant messaging client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.13.0
-TERMUX_PKG_REVISION=14
-TERMUX_PKG_SRCURL=https://bitbucket.org/pidgin/main/downloads/pidgin-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=2747150c6f711146bddd333c496870bfd55058bab22ffb7e4eb784018ec46d8f
+TERMUX_PKG_VERSION=2.14.1
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/pidgin/files/Pidgin/${TERMUX_PKG_VERSION}/pidgin-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=21f0b20fee1421cf25bdf5c592b8a92eed9ca48c5c1e85b1fba90d965eff8db0
 TERMUX_PKG_DEPENDS="gtk2, libgnutls, libidn, libsm, libxext, libxss"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-gstreamer
 --disable-gtkspell
