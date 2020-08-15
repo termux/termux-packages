895eda49505c0e85bf5f787202ce706d6ac598cf
Butta <repo@butta.fastem.com>
2020-08-15 11:04:54 +0530

    kubectl: update from 1.18.7 to 1.18.8
    lftp: update from 4.9.1 to 4.9.2
    
    libgrpc: update from 1.30.2 to 1.31.0

--- packages/lftp/build.sh
+++ packages/lftp/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://lftp.tech/
 TERMUX_PKG_DESCRIPTION="FTP/HTTP client and file transfer program"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=4.9.1
+TERMUX_PKG_VERSION=4.9.2
 TERMUX_PKG_SRCURL=https://lftp.tech/ftp/lftp-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=5969fcaefd102955dd882f3bcd8962198bc537224749ed92f206f415207a024b
+TERMUX_PKG_SHA256=c517c4f4f9c39bd415d7313088a2b1e313b2d386867fe40b7692b83a20f0670d
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libexpat, libiconv, openssl, readline, libidn2, zlib"
 
 # (1) Android has dn_expand, but lftp assumes that dn_skipname then exists, which it does not on android.
