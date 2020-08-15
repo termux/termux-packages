e1fefe7400b791e4fe8aabe9e8694103be44a184
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-08-15 12:17:54 +0000

    youtubedr: update to 2.0.0 (#5692)

--- packages/youtubedr/build.sh
+++ packages/youtubedr/build.sh
@@ -2,24 +2,23 @@ TERMUX_PKG_HOMEPAGE=https://github.com/kkdai/youtube
 TERMUX_PKG_DESCRIPTION="Download youtube video in Golang"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=1.2.4
+TERMUX_PKG_VERSION=2.0.0
 TERMUX_PKG_SRCURL=https://github.com/kkdai/youtube/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d6e7baa7ea61d366c3aaafd88215e675254a378e035fe52709a9de2d9c16d56e
+TERMUX_PKG_SHA256=6ddaf2407d1c8dc1888e5a14411264b22b759970be4a20ddff2b47462a975e95
 
 termux_step_make() {
 	termux_setup_golang
 
 	cd "$TERMUX_PKG_SRCDIR"
-
 	export GOPATH="${TERMUX_PKG_BUILDDIR}"
 	mkdir -p "${GOPATH}/src/github.com/kkdai/"
 	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/kkdai/youtube"
 	cd "${GOPATH}/src/github.com/kkdai/youtube/"
 	go get -d -v
-	cd youtubedr
+	cd cmd/youtubedr
 	go build .
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/kkdai/youtube/youtubedr/youtubedr
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/kkdai/youtube/cmd/youtubedr/youtubedr
 }
