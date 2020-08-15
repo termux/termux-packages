b59a7f025db590508e08f113c1ee7751917e224f
Butta <repo@butta.fastem.com>
2020-08-15 11:04:54 +0530

    kubectl: update from 1.18.7 to 1.18.8
    lftp: update from 4.9.1 to 4.9.2
    
    libgrpc: update from 1.30.2 to 1.31.0

--- packages/kubectl/build.sh
+++ packages/kubectl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://kubernetes.io/
 TERMUX_PKG_DESCRIPTION="Kubernetes.io client binary"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.18.7
+TERMUX_PKG_VERSION=1.18.8
 TERMUX_PKG_SRCURL=https://dl.k8s.io/v$TERMUX_PKG_VERSION/kubernetes-src.tar.gz
-TERMUX_PKG_SHA256=dd80f48ccbee9c0a118f9ecfe7ebf076fbc6ec5f20c552be2e19c40a78783ca8
+TERMUX_PKG_SHA256=b4ca40cc24bef3e89dc920992422e0061c376e8616d2455c89d6c18b90e1b141
 
 termux_step_get_source() {
 	mkdir -p "$TERMUX_PKG_CACHEDIR"
