bde7255be0ac922a8a0b09dcfd997c6b2afee12d
ifurther <55025025+ifurther@users.noreply.github.com>
2020-08-15 19:59:01 +0800

    New package: libzim

---
+++ packages/libzim/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://openzim.org
+TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the ZIM file format."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=6.1.8
+TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1be605b1d22a0a7c225e80eab2cb1a249bf0f1b500ec8ed42e1091fc96c49fb3
+TERMUX_PKG_DEPENDS="zstd, libuuid, zlib, libicu, liblzma, libxapian, googletest"
+
