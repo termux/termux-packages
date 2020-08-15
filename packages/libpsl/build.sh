1d3b637c448a342cd4ab69330d4073a4d0b82303
Butta <repo@butta.fastem.com>
2020-08-15 17:01:30 +0530

    libpsl: update from 0.21.0 to 0.21.1
    mariadb: update from 10.5.4 to 10.5.5

--- packages/libpsl/build.sh
+++ packages/libpsl/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/rockdaboot/libpsl
 TERMUX_PKG_DESCRIPTION="Public Suffix List library"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.21.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/rockdaboot/libpsl/releases/download/libpsl-${TERMUX_PKG_VERSION}/libpsl-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=41bd1c75a375b85c337b59783f5deb93dbb443fb0a52d257f403df7bd653ee12
+TERMUX_PKG_VERSION=0.21.1
+TERMUX_PKG_SRCURL=https://github.com/rockdaboot/libpsl/releases/download/${TERMUX_PKG_VERSION}/libpsl-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ac6ce1e1fbd4d0254c4ddb9d37f1fa99dec83619c1253328155206b896210d4c
 TERMUX_PKG_DEPENDS="libidn2, libunistring"
 TERMUX_PKG_BREAKS="libpsl-dev"
 TERMUX_PKG_REPLACES="libpsl-dev"
