38ab83d39fa55c886d1d5ebf204c058b3b30f77a
Butta <repo@butta.fastem.com>
2020-08-16 13:27:12 +0530

    boost: update from 1.73.0 to 1.74.0 and bump dependent packages

--- packages/mkvtoolnix/build.sh
+++ packages/mkvtoolnix/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.bunkus.org/videotools/mkvtoolnix/
 TERMUX_PKG_DESCRIPTION="Set of tools to create, edit and inspect Matroska files"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=47.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.com/mbunkus/mkvtoolnix/-/archive/release-$TERMUX_PKG_VERSION/mkvtoolnix-release-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=51e8ee207603771c531385bff7385d4dcdd102511be00bcabb36c341957c6598
 TERMUX_PKG_DEPENDS="boost, file, libflac, libogg, libvorbis, zlib"
