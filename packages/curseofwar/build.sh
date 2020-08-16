25371fdac1780f5200d76b6c16961264fd519331
Henrik Grimler <henrik@grimler.se>
2020-08-16 19:31:49 +0200

    curseofwar: update to 1.3.0

--- packages/curseofwar/build.sh
+++ packages/curseofwar/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://a-nikolaev.github.io/curseofwar/
 TERMUX_PKG_DESCRIPTION="Fast-paced action strategy game focusing on high-level strategic planning"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/a-nikolaev/curseofwar/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=91b7781e26341faa6b6999b6baf6e74ef532fa94303ab6a2bf9ff6d614a3f670
+TERMUX_PKG_SHA256=2a90204d95a9f29a0e5923f43e65188209dc8be9d9eb93576404e3f79b8a652b
 TERMUX_PKG_DEPENDS="libc++, ncurses"
 
 termux_step_make_install () {
