ffb33b8023ca09f4c80a9b911ade545082117dfd
Butta <repo@butta.fastem.com>
2020-08-17 11:16:59 +0530

    elvish: update from 0.14.0 to 0.14.1
    libcap: update from 2.42 to 2.43
    
    libmaxminddb: update from 1.4.2 to 1.4.3
    
    libpipeline: update from 1.5.2 to 1.5.3
    
    libsndfile: update from 1.0.29~pre2 to 1.0.29

--- packages/libsndfile/build.sh
+++ packages/libsndfile/build.sh
@@ -1,16 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://www.mega-nerd.com/libsndfile
 TERMUX_PKG_DESCRIPTION="Library for reading/writing audio files"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-# Use a git master snapshot until 1.0.29 is released:
-TERMUX_PKG_VERSION=1.0.29~pre2
-TERMUX_PKG_SRCURL=https://github.com/erikd/libsndfile/archive/826d5296da54c016e3cb0f7f00de3b9e295b9c4a.zip
-TERMUX_PKG_SHA256=84651201a8468c448f1fd172a48ccf47f3761d23fd0f59bcaaf908050e9eeb7c
+TERMUX_PKG_VERSION=1.0.29
+TERMUX_PKG_SRCURL=https://github.com/erikd/libsndfile/releases/download/v${TERMUX_PKG_VERSION}/libsndfile-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=2ba20d44817c8176f097ab25eff44ef0aeec9e00973def5a7174c5ae0764b22f
 TERMUX_PKG_DEPENDS="libflac, libvorbis"
 TERMUX_PKG_BREAKS="libsndfile-dev"
 TERMUX_PKG_REPLACES="libsndfile-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sqlite --disable-alsa"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/ share/man/man1/"
-
-termux_step_pre_configure() {
-	sh autogen.sh
-}
