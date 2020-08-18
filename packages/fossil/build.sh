a4780e375c307357dddbd2492240fbb589fc7519
Butta <repo@butta.fastem.com>
2020-08-18 08:34:05 +0530

    fossil: update from 2.11.1 to 2.12
    jfrog-cli: update from 1.38.4 to 1.39.0
    
    mkvtoolnix: update from 47.0.0 to 49.0.0

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT-BSD2.txt"
-TERMUX_PKG_VERSION=2.11.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.12
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b391f34fada5f16eba452f36d2ad8baa3fe2a5267bf8f4169cb1b9832c0cb8eb
+TERMUX_PKG_SHA256=8de5367c462d0671b0ca579d909ab1e25efaaf294f37ce9af93f04b965694166
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
