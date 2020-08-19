29a8b1cdad8457055dd15834794d07fc56665959
Butta <repo@butta.fastem.com>
2020-08-19 20:34:22 +0530

    glib: update from 2.64.4 to 2.64.5
    libcurl: update from 7.71.1 to 7.72.0
    
    libgrpc: update from 1.31.0 to 1.31.1
    
    ninja: update from 1.10.0 to 1.10.1

--- packages/ninja/build.sh
+++ packages/ninja/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://ninja-build.org
 TERMUX_PKG_DESCRIPTION="A small build system with a focus on speed"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.10.0
+TERMUX_PKG_VERSION=1.10.1
 TERMUX_PKG_SRCURL=https://github.com/ninja-build/ninja/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3810318b08489435f8efc19c05525e80a993af5a55baa0dfeae0465a9d45f99f
+TERMUX_PKG_SHA256=a6b6f7ac360d4aabd54e299cc1d8fa7b234cd81b9401693da21221c62569a23e
 TERMUX_PKG_DEPENDS="libc++, libandroid-spawn"
 
 termux_step_pre_configure() {
