6a3d5295f3a73485ee7465bd5dd6b0dc3636ab89
Butta <repo@butta.fastem.com>
2020-08-20 13:49:09 +0530

    links: disable using OpenMP for now

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://links.twibright.com
 TERMUX_PKG_DESCRIPTION="Links is a text and graphics mode WWW browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.21
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f0e1fd131b5e9ff943bcc667e0aa6331048749ad4b921b6fadd14e919d9f79ac
 TERMUX_PKG_DEPENDS="libbz2, libevent, liblzma, libpng, librsvg, libtiff, libx11, openssl, zlib, zstd"
@@ -10,4 +11,5 @@ TERMUX_PKG_BUILD_DEPENDS="libxt"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --mandir=$TERMUX_PREFIX/share/man
 --enable-graphics
+--without-openmp
 "
