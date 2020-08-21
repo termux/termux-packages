86ca5ced3559406480a1078b11ab9faf9b992b3d
Butta <repo@butta.fastem.com>
2020-08-21 10:56:55 +0530

    cmake: update from 3.18.1 to 3.18.2
    debianutils: update from 4.11 to 4.11.1
    
    fossil: update from 2.12 to 2.12.1
    
    imagemagick: update from 7.0.10.27 to 7.0.10.28
    
    nano: update from 5.0 to 5.1

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://cmake.org/
 TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package software"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=3.18.1
+TERMUX_PKG_VERSION=3.18.2
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c0e3338bd37e67155b9d1e9526fec326b5c541f74857771b7ffed0c46ad62508
+TERMUX_PKG_SHA256=5d4e40fc775d3d828c72e5c45906b4d9b59003c9433ff1b36a1cb552bbd51d7e
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, make, clang, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DKWSYS_LFS_WORKS=ON -DBUILD_CursesDialog=ON"
--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT-BSD2.txt"
-TERMUX_PKG_VERSION=2.12
+TERMUX_PKG_VERSION=2.12.1
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8de5367c462d0671b0ca579d909ab1e25efaaf294f37ce9af93f04b965694166
+TERMUX_PKG_SHA256=822326ffcfed3748edaf4cfd5ab45b23225dea840304f765d1d55d2e6c7d6603
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
