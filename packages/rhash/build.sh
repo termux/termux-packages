dc3f63a53e4c772ec8cc9dff105c27004f69bf96
Butta <repo@butta.fastem.com>
2020-08-22 01:24:18 +0530

    Revert parted and rhash updates as they will require more patching

--- packages/rhash/build.sh
+++ packages/rhash/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/rhash/RHash
 TERMUX_PKG_DESCRIPTION="Console utility for calculation and verification of magnet links and a wide range of hash sums"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.4.0
+TERMUX_PKG_VERSION=1.3.9
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/rhash/RHash/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2ea39540f5c580da0e655f7b483c19e0d31506aed4202d88e8459fa7aeeb8861
+TERMUX_PKG_SHA256=42b1006f998adb189b1f316bf1a60e3171da047a85c4aaded2d0d26c1476c9f6
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_CONFLICTS="librhash, rhash-dev"
 TERMUX_PKG_REPLACES="librhash, rhash-dev"
