12329e98bd522e2f1c726e6baf85855ec8aacac8
ifurther <55025025+ifurther@users.noreply.github.com>
2020-08-24 21:05:50 +0800

    fix edk2-platform link

--- packages/ovmf/build.sh
+++ packages/ovmf/build.sh
@@ -7,18 +7,20 @@ TERMUX_PKG_VERSION+=(202005)
 TERMUX_PKG_VERSION+=(1.0.7)
 TERMUX_PKG_SRCURL=(https://github.com/tianocore/edk2/archive/edk2-stable${TERMUX_PKG_VERSION}.tar.gz
 		   https://github.com/google/brotli/archive/v${TERMUX_PKG_VERSION[1]}.tar.gz
-	   	   https://github.com/tianocore/edk2-platforms.git)
+	   	   https://github.com/tianocore/edk2-platforms/archive/69009e5b91a70b6fc15cdf4bc410256052cb5752.tar.gz)
 TERMUX_PKG_SHA256=(373c3eff3497316a48fcf4be8dcee227431cbce86dcd80a004950e992f0297e2
 		   4c61bfb0faca87219ea587326c467b95acb25555b53d1a421ffa3c8a9296ee2c
-		   98ef5ceb62832d1665004c7c8ac47c1519b6d4f52080e20e2af8ce2f5d9f508b) 
+		   7a226a6ea783b9b442c9269dca8ff76577afa62f6c00eaf5355779fca08ed4b1) 
 TERMUX_PKG_DEPENDS="libuuid"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	export PATH=$TERMUX_PKG_SRCDIR/bin:$PATH
 	mv brotli-${TERMUX_PKG_VERSION[1]}/* BaseTools/Source/C/BrotliCompress/brotli/
+	export WORKSPACE=$TERMUX_PKG_SRCDIR
+	export EDK_TOOLS_PATH=$TERMUX_PKG_SRCDIR/BaseTools
+	export CONF_PATH=$TERMUX_PKG_SRCDIR/Conf
 	make -C BaseTools
-	export EDK_TOOLS_PATH="${TERMUX_PKG_SRCDIR}"/edk2/BaseTools
 	. edksetup.sh BaseTools
 	unset ARCH WORKSPACE
 	export PACKAGES_PATH=$PWD:$PWD/edk2-platforms
