fa3ae7ee9169a8b0b1da4ee495a7c189bb2812af
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-25 14:11:52 +0300

    Update build.sh

--- packages/hors/build.sh
+++ packages/hors/build.sh
@@ -6,3 +6,7 @@ TERMUX_PKG_VERSION=0.6.9
 TERMUX_PKG_SRCURL=https://github.com/WindSoilder/hors/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a4737bc04b77d035b7b57d99cad875e709044ef829febdf9da87a6dde67d8c79
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	rm -f Makefile
+}
