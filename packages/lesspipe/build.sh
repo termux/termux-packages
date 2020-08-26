b2ece7d6d5e6cd92ccc435830a33eab538e5a1f0
Butta <repo@butta.fastem.com>
2020-08-26 18:20:23 +0530

    jfrog-cli: update from 1.39.0 to 1.39.1
    lesspipe: update from 1.84 to 1.85
    
    libuv: update from 1.38.1 to 1.39.0
    
    libx11: update from 1.6.11 to 1.6.12

--- packages/lesspipe/build.sh
+++ packages/lesspipe/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www-zeuthen.desy.de/~friebel/unix/lesspipe.html
 TERMUX_PKG_DESCRIPTION="An input filter for the pager less"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.84
+TERMUX_PKG_VERSION=1.85
 TERMUX_PKG_SRCURL=https://github.com/wofr06/lesspipe/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=5eb4811cc8ded108e98448bd83057e730906f643ad689ccd695828f2a46c4410
+TERMUX_PKG_SHA256=cffbb432396ea4abf551bdda17adee9be3543486bc398c5c6838908e299210f9
 TERMUX_PKG_DEPENDS="less"
 TERMUX_PKG_SUGGESTS="imagemagick, p7zip, unrar, unzip"
 TERMUX_PKG_BUILD_IN_SRC=true
