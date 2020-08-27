fd2d5867d30055e599bf1cd3e0c720725b8e6ea9
ifurther <55025025+ifurther@users.noreply.github.com>
2020-08-27 20:50:57 +0800

    fix license error

--- packages/libimobiledevice/build.sh
+++ packages/libimobiledevice/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org/
 TERMUX_PKG_DESCRIPTION="A library to communicate with services on iOS devices using native protocols."
-TERMUX_PKG_LICENSE="GPL-2.1"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/libimobiledevice/archive/$TERMUX_PKG_VERSION.tar.gz
