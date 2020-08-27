4779e6f483341ecf548e5fddaf6cd0fb7a5f93e3
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-08-27 22:55:14 +0300

    pango: update to 1.46.1

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.pango.org/
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.46.0
+TERMUX_PKG_VERSION=1.46.1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION:0:4}/pango-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=9a81572ebb946187fbdd69d5ffc57e2f7a1f768cd8d2fd89dbb03fb9002a99b5
+TERMUX_PKG_SHA256=fe516b10711bbb6fd75011d66dd08fabfce18f7931aed7415136d53c4aadf1c5
 TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo, libxft, zlib"
 TERMUX_PKG_BREAKS="pango-dev"
 TERMUX_PKG_REPLACES="pango-dev"
