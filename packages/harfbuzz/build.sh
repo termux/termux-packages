d091232b0e66dd022e3388311cf3dccaef84174c
Butta <repo@butta.fastem.com>
2020-08-29 15:26:15 +0530

    harfbuzz: update from 2.7.1 to 2.7.2
    hexedit: update from 1.4.2 to 1.5

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/HarfBuzz/
 TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.7.1
+TERMUX_PKG_VERSION=2.7.2
 TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=431c856ff18eeca89b2a36b58f2c2d56273cd522c34c0ffbc9dd6f7da4b9bd08
+TERMUX_PKG_SHA256=8ec112ee108642477478b75fc7906422abed404d7530e47ba0a4875f553f1b59
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
 TERMUX_PKG_BREAKS="harfbuzz-dev"
 TERMUX_PKG_REPLACES="harfbuzz-dev"
