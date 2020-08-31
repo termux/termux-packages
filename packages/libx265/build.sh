2198957a59ba6168c506c40399f13b4e54da1f00
tomek@sviete.pl <tomek@sviete.pl>
2020-08-31 10:49:11 +0200

    fix libx265 src url

--- packages/libx265/build.sh
+++ packages/libx265/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://x265.org/
 TERMUX_PKG_DESCRIPTION="H.265/HEVC video stream encoder library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=3.3
-TERMUX_PKG_SRCURL=https://bitbucket.org/multicoreware/x265/downloads/x265_$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f26e148ed1f4dfb33fd1eb3ff5e60e08078d1b2017e88bcbb045b3fb58300b9c
+TERMUX_PKG_SRCURL=https://bitbucket.org/multicoreware/x265_git/downloads/x265_$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=c6d744a87eda55560da715f56f878640554ddc06e2d0fcbd822fa330affc22cc
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BREAKS="libx265-dev"
 TERMUX_PKG_REPLACES="libx265-dev"
@@ -15,4 +15,3 @@ termux_step_pre_configure() {
 	fi
 	TERMUX_PKG_SRCDIR="$TERMUX_PKG_SRCDIR/source"
 }
-
