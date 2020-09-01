47cf758e46ae5beaf6586bb7085fe3d319c9cd14
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-02 02:10:58 +0300

    phpmyadmin: fix packaging

--- packages/phpmyadmin/build.sh
+++ packages/phpmyadmin/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.phpmyadmin.net
 TERMUX_PKG_DESCRIPTION="A PHP tool for administering MySQL databases"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=5.0.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://files.phpmyadmin.net/phpMyAdmin/$TERMUX_PKG_VERSION/phpMyAdmin-$TERMUX_PKG_VERSION-all-languages.tar.xz
 TERMUX_PKG_SHA256=cbcc78d1499308d9329950fcba2ebaa84c559a934fe54efc027d459d8e4161c8
 TERMUX_PKG_DEPENDS="apache2, php"
@@ -14,5 +15,6 @@ termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/phpmyadmin
 	cp $TERMUX_PKG_SRCDIR/config.sample.inc.php $TERMUX_PREFIX/etc/phpmyadmin/config.inc.php
 	ln -s $TERMUX_PREFIX/etc/phpmyadmin/config.inc.php $TERMUX_PREFIX/usr/share/phpmyadmin
-	cp $TERMUX_PKG_BUILDER_DIR/phpmyadmin.conf $TERMUX_PREFIX/etc/apache2/conf.d
+	mkdir -p $TERMUX_PREFIX/etc/apache2/conf.d
+	cp $TERMUX_PKG_BUILDER_DIR/phpmyadmin.conf $TERMUX_PREFIX/etc/apache2/conf.d/
 }
