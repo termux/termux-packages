66d543d2be8089a52f1bf7547fb6cda32d0f14bc
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-02 15:31:27 +0300

    move 'htop' to root-repo and add htop-legacy corresponding to htop v2.x

--- packages/htop-legacy/build.sh
+++ packages/htop-legacy/build.sh
@@ -1,20 +1,15 @@
-TERMUX_PKG_HOMEPAGE=https://htop.dev/
+TERMUX_PKG_HOMEPAGE=https://hisham.hm/htop/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_VERSION=2.2.0
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/htop-dev/htop/archive/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1c0661f0ae5f4e2874da250b60cd515e4ac4c041583221adfe95f10e18d1a4e6
+TERMUX_PKG_SRCURL=http://hisham.hm/htop/releases/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d9d6826f10ce3887950d709b53ee1d8c1849a70fa38e91d5896ad8cbc6ba3c57
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
-TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+TERMUX_PKG_DEPENDS="ncurses, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="share/applications share/pixmaps"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_ncursesw6_addnwstr=yes
 LIBS=-landroid-support
 "
-
-termux_step_pre_configure() {
-	./autogen.sh
-}
