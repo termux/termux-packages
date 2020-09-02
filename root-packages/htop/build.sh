2a60acc66ef3d82208d259474cb8d6a8394e0a8c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-02 23:00:45 +0300

    htop: rebuild

--- root-packages/htop/build.sh
+++ root-packages/htop/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://htop.dev/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=3.0.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/htop-dev/htop/archive/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1c0661f0ae5f4e2874da250b60cd515e4ac4c041583221adfe95f10e18d1a4e6
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
