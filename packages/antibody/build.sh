b2aa478c9783732e37187ade3d50fe29d6810f4a
Butta <repo@butta.fastem.com>
2020-09-04 16:07:23 +0530

    antibody: update from 6.1.0 to 6.1.1
    gitea: update from 1.12.3 to 1.12.4
    
    global: update from 6.6.4 to 6.6.5

--- packages/antibody/build.sh
+++ packages/antibody/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/getantibody/antibody
 TERMUX_PKG_DESCRIPTION="The fastest shell plugin manager"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=6.1.0
+TERMUX_PKG_VERSION=6.1.1
 TERMUX_PKG_SRCURL=https://github.com/getantibody/antibody/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b2cf67af801ebf10c0d52b1767cbdb5e3ab5a1713e6c3d28616a109e1d7906a7
+TERMUX_PKG_SHA256=87bced5fba8cf5d587ea803d33dda72e8bcbd4e4c9991a9b40b2de4babbfc24f
 
 termux_step_make() {
 	termux_setup_golang
--- packages/global/build.sh
+++ packages/global/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/global/
 TERMUX_PKG_DESCRIPTION="Source code search and browse tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=6.6.4
+TERMUX_PKG_VERSION=6.6.5
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/global/global-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=987e8cb956c53f8ebe4453b778a8fde2037b982613aba7f3e8e74bcd05312594
+TERMUX_PKG_SHA256=9c3730bd9e975d94231f3402d5526b79c0b23cc665d624c9829c948dfad37b83
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_posix1_2008_realpath=yes
 --with-posix-sort=$TERMUX_PREFIX/bin/sort
