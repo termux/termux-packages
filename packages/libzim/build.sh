9b1056b2076f7532b0ec27fa60a72a17347f461c
Butta <repo@butta.fastem.com>
2020-09-04 16:55:21 +0530

    Update libzim hash and bump radare2 on CI

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -3,6 +3,6 @@ TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=6.2.2
 TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=20fa806d30fe3b3b353387f93dcac2bf8dbc08120f78ac0fad9bc3e4fd822984
+TERMUX_PKG_SHA256=6619035d35c9ba057c80be5758fa86922802c74aca40e5fffd40b77f0f263af2
 TERMUX_PKG_DEPENDS="zstd, libuuid, zlib, libicu, liblzma, libxapian, googletest"
 
