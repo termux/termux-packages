28898a836a3518bc77daab39e77a58d640159246
Butta <repo@butta.fastem.com>
2020-09-04 16:17:57 +0530

    gmic: update from 2.9.1 to 2.9.2
    gnupg: update from 2.2.22 to 2.2.23
    
    jfrog-cli: update from 1.39.2 to 1.39.3
    
    libwebsockets: update from 4.0.20 to 4.0.21

--- packages/libwebsockets/build.sh
+++ packages/libwebsockets/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libwebsockets.org
 TERMUX_PKG_DESCRIPTION="Lightweight C websockets library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=4.0.20
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.0.21
 TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a26d243f2642a9b810e7d91f1e66b149d1da978decdca58ce1c9218c454f397e
+TERMUX_PKG_SHA256=6ece1f422c6d38aabedec2476f2ac12e9aede8691b08137068ad85545ce3ff78
 TERMUX_PKG_DEPENDS="openssl, libuv, zlib"
 TERMUX_PKG_BREAKS="libwebsockets-dev"
 TERMUX_PKG_REPLACES="libwebsockets-dev"
