ccb74a9ec5b452c4f86fb78224c3c15d7ef55908
Butta <repo@butta.fastem.com>
2020-09-04 16:41:29 +0530

    libzim: update from 6.2.1 to 6.2.2
    radare2: update from 4.5.0 to 4.5.1

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://openzim.org
 TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the ZIM file format."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=6.2.1
+TERMUX_PKG_VERSION=6.2.2
 TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=20fa806d30fe3b3b353387f93dcac2bf8dbc08120f78ac0fad9bc3e4fd822984
 TERMUX_PKG_DEPENDS="zstd, libuuid, zlib, libicu, liblzma, libxapian, googletest"
