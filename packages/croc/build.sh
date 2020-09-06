a522d94817fbf4e3a86335948e95ec60e849f813
Ilya Mateyko <astrophena@gmail.com>
2020-09-06 21:06:54 +0000

    New package: croc

---
+++ packages/croc/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
+TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another."
+TERMUX_PKG_LICENSE=MIT
+TERMUX_PKG_VERSION=8.3.1
+TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
+TERMUX_PKG_SHA256=58dbe55cdbd0c8255c40ac5045676b7e8a0af8e21732f80502efc8cd27330d87
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	cd $TERMUX_PKG_SRCDIR
+
+	termux_setup_golang
+
+	go build -o croc -trimpath
+}
+
+termux_step_make_install() {
+	install -m755 croc $TERMUX_PREFIX/bin/croc
+}
