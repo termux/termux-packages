23683eb28875ce225a0ee2b8d012f65488ddc324
Butta <repo@butta.fastem.com>
2020-09-06 23:47:16 +0530

    mpdscribble: fix switch to Meson build by adding boost and libgcrypt dependencies

--- packages/mpdscribble/build.sh
+++ packages/mpdscribble/build.sh
@@ -5,7 +5,8 @@ TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=0.23
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/mpdscribble/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8b3b79538fa9871ce61d7fb793d442c72b6aadf80af4c5eb664286f613d1feee
-TERMUX_PKG_DEPENDS="libcurl, mpd, libmpdclient, glib"
+TERMUX_PKG_DEPENDS="libcurl, libgcrypt, mpd, libmpdclient, glib"
+TERMUX_PKG_BUILD_DEPENDS="boost"
 TERMUX_PKG_CONFFILES="etc/mpdscribble.conf"
 # mpdscribble already puts timestamps in the info printed to stdout so no need for svlogd -tt,
 # therefore we override the mpdscribble/log run script
@@ -14,13 +15,14 @@ TERMUX_PKG_SERVICE_SCRIPT=(
 	"mpdscribble/log" 'mkdir -p "$LOGDIR/sv/mpdscribble"\nexec svlogd "$LOGDIR/sv/mpdscribble"'
 )
 
-termux_step_pre_configure () {
-	NOCONFIGURE=1 ./autogen.sh
+termux_step_pre_configure() {
+	export BOOST_ROOT=$TERMUX_PREFIX
 }
 
 termux_step_post_make_install () {
 	install $TERMUX_PKG_SRCDIR/doc/mpdscribble.conf $TERMUX_PREFIX/etc/
 }
+
 termux_step_create_debscripts () {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
 	echo "mkdir -p ~/.mpdscribble" >> postinst
