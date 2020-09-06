1b6137f5c1ecc496c4546d1192588bd3703c595e
Butta <repo@butta.fastem.com>
2020-09-06 19:20:44 +0530

    bison: update from 3.7.1 to 3.7.2
    git-delta: update from 0.4.1 to 0.4.3
    
    librsvg: update from 2.49.4 to 2.49.5

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.49.4
+TERMUX_PKG_VERSION=2.49.5
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a7a66ac14827e6ca1e29cb757b022601108458d8cdf6cea22fa9699156b249f1
+TERMUX_PKG_SHA256=daa64941bb4732bdf51b902a72c6e04063235cfce6986d910ba0759c76917795
 TERMUX_PKG_DEPENDS="gdk-pixbuf, libcairo, libcroco, pango, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
