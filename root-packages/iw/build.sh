a8f87d2f123c5a12c7f7b72db8065e7d113441ee
Henrik Grimler <henrik@grimler.se>
2020-09-07 19:34:50 +0200

    iw: update to 5.8

--- root-packages/iw/build.sh
+++ root-packages/iw/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wireless.wiki.kernel.org/en/users/documentation/iw
 TERMUX_PKG_DESCRIPTION="CLI configuration utility for wireless devices"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=5.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.8
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/software/network/iw/iw-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a2469f677088d7b1070a7fbb28f3c747041697e8f6ec70783339cb1bc27a395f
+TERMUX_PKG_SHA256=b59c4bacb0b6675cc09f2f30ebdceb91174cb65443d0a062fba2c09fad772db4
 TERMUX_PKG_DEPENDS="libnl"
 TERMUX_PKG_BUILD_IN_SRC=true
