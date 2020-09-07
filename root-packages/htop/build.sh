93f20fdcc4b18c2eb5e30737be183dcd2eab4aea
Henrik Grimler <henrik@grimler.se>
2020-09-07 19:43:22 +0200

    htop: update to 3.0.1

--- root-packages/htop/build.sh
+++ root-packages/htop/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://htop.dev/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.0.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=3.0.1
 TERMUX_PKG_SRCURL=https://github.com/htop-dev/htop/archive/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1c0661f0ae5f4e2874da250b60cd515e4ac4c041583221adfe95f10e18d1a4e6
+TERMUX_PKG_SHA256=8465164bc085f5f1813e1d3f6c4b9b56bf4c95cc12226a5367e65794949b01ca
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
