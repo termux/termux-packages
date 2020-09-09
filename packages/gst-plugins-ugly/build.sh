4ff42baeee02422263c726f09ab7be4c29033eae
Butta <repo@butta.fastem.com>
2020-09-09 22:17:25 +0530

    gst-plugins-{bad,good,ugly}: update to Meson build and remove unused flags

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -5,10 +5,6 @@ TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=686644e45e08258ae240c4519376668ad8d34ea6d0f6ab556473c317bfb7e082
 TERMUX_PKG_DEPENDS="gst-plugins-base, libx264"
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-tests
---disable-examples
---disable-android_media
+-Dtests=disabled
 "
-TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/"
