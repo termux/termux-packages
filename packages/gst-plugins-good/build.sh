d1c9e1bd820d1b3221f823a5e5ae86424ed1341b
Butta <repo@butta.fastem.com>
2020-09-09 21:57:57 +0530

    gst-plugins-good: update to Meson build and add libandroid-shmem dependency

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -4,19 +4,21 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=6329bc377a3afea5cf1ab329a91cc4a906f075781780d1edc59e8bea8c1a8bac
-TERMUX_PKG_DEPENDS="gst-plugins-base, libcaca, libsoup, libjpeg-turbo, libpng, libflac, libbz2, libvpx, pulseaudio, libmp3lame, gstreamer, libogg, zlib"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="gst-plugins-base, libandroid-shmem, libcaca, libsoup, libjpeg-turbo, libpng, libflac, libbz2, libvpx, pulseaudio, libmp3lame, gstreamer, libogg, zlib"
 # pcre needed by glib. libxml2 needed by libsoup
 TERMUX_PKG_BUILD_DEPENDS="glib, pcre, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-android_media
---disable-cairo
---disable-examples
---disable-gdk_pixbuf
---disable-oss
---disable-oss4
---disable-tests
---disable-gst_v4l2
---disable-aalib
+-Dcairo=disabled
+-Dexamples=disabled
+-Dgdk-pixbuf=disabled
+-Doss=disabled
+-Doss4=disabled
+-Dtests=disabled
+-Dv4l2=disabled
+-Daalib=disabled
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-shmem"
+}
