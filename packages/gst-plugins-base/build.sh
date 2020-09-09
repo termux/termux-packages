445b2c586ad64e7557707006ca83250e9614482b
Butta <repo@butta.fastem.com>
2020-09-09 19:26:34 +0530

    gstreamer, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gst-plugins-ugly: update from 1.16.2 to 1.18.0

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.16.2
+TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b13e73e2fe74a4166552f9577c3dcb24bed077021b9c7fa600d910ec6987816a
+TERMUX_PKG_SHA256=762abdd1a950809a1cea62fff7f86b5f7d6bd5f6841e3e585c700b823cdb7897
 TERMUX_PKG_DEPENDS="gstreamer, libjpeg-turbo, libopus, libpng, libvorbis, zlib"
 TERMUX_PKG_BREAKS="gst-plugins-base-dev"
 TERMUX_PKG_REPLACES="gst-plugins-base-dev"
--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.16.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=40bb3bafda25c0b739c8fc36e48380fccf61c4d3f83747e97ac3f9b0171b1319
+TERMUX_PKG_SHA256=6329bc377a3afea5cf1ab329a91cc4a906f075781780d1edc59e8bea8c1a8bac
 TERMUX_PKG_DEPENDS="gst-plugins-base, libcaca, libsoup, libjpeg-turbo, libpng, libflac, libbz2, libvpx, pulseaudio, libmp3lame, gstreamer, libogg, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 # pcre needed by glib. libxml2 needed by libsoup
