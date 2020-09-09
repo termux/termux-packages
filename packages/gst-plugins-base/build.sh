f8b3bf7d11b8061e41d55aed79060e5985874332
Butta <repo@butta.fastem.com>
2020-09-09 21:18:34 +0530

    Update gstreamer and gst-plugins-base to Meson build

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -8,9 +8,7 @@ TERMUX_PKG_DEPENDS="gstreamer, libjpeg-turbo, libopus, libpng, libvorbis, zlib"
 TERMUX_PKG_BREAKS="gst-plugins-base-dev"
 TERMUX_PKG_REPLACES="gst-plugins-base-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-GLIB_GENMARSHAL=/usr/bin/glib-genmarshal
-GLIB_MKENUMS=/usr/bin/glib-mkenums
---disable-tests
---disable-examples
---disable-pango
+-Dtests=disabled
+-Dexamples=disabled
+-Dpango=disabled
 "
