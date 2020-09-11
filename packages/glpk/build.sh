23d37f05a0f59283f2940031d32a13e050f46085
Henrik Grimler <henrik@grimler.se>
2020-09-11 22:14:11 +0200

    glpk: add package
    Requested in https://github.com/termux/science-packages/issues/53

---
+++ packages/glpk/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/glpk/
+TERMUX_PKG_DESCRIPTION="GNU Linear Programming Kit: solve LP, MIP and other problems"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=4.65
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/glpk/glpk-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4281e29b628864dfe48d393a7bedd781e5b475387c20d8b0158f329994721a10
+TERMUX_PKG_DEPENDS=libgmp
