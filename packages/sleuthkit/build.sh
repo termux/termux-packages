8987e6be691478e04cbe987b2416857f58bb26fd
Butta <repo@butta.fastem.com>
2020-09-11 23:37:33 +0530

    Fix sleuthkit license list and bump libssh and redis on the CI

--- packages/sleuthkit/build.sh
+++ packages/sleuthkit/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://sleuthkit.org/
 TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library for digital forensics tools. "
 TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_LICENSE_FILE="licenses/*"
+TERMUX_PKG_LICENSE_FILE="licenses/mit.txt, licenses/GNU-COPYING, licenses/bsd.txt, licenses/cpl1.0.txt, licenses/IBM-LICENSE"
 TERMUX_PKG_VERSION=4.10.0
 TERMUX_PKG_SRCURL=https://github.com/sleuthkit/sleuthkit/archive/sleuthkit-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5684d95858bec8f6ed1f97ebb9e216715a28b53768abf04eac2fbedac22b1755
