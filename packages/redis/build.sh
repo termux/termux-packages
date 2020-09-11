e15725398ad1112300b74216ec270ff0c9b9a4e3
Butta <repo@butta.fastem.com>
2020-09-11 22:51:19 +0530

    libssh: update from 0.9.4 to 0.9.5
    redis: update from 6.0.7 to 6.0.8
    
    sleuthkit: update from 4.9.0 to 4.10.0

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://redis.io/
 TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache and message broker"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=6.0.7
+TERMUX_PKG_VERSION=6.0.8
 TERMUX_PKG_SRCURL=http://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c2aaa1a4c7e72c70adedf976fdd5e1d34d395989283dab9d7840e0a304bb2393
+TERMUX_PKG_SHA256=04fa1fddc39bd1aecb6739dd5dd73858a3515b427acd1e2947a66dadce868d68
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/redis.conf"
 
