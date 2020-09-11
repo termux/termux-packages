9d4c20382c60b6277b1bef4082c7e86e4967dd0b
Butta <repo@butta.fastem.com>
2020-09-11 22:02:37 +0530

    jbig2dec: update from 0.18 to 0.19
    kubectl: update from 1.19.0 to 1.19.1
    
    librsvg: update from 2.49.5 to 2.50.0

--- packages/jbig2dec/build.sh
+++ packages/jbig2dec/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://jbig2dec.com/
 TERMUX_PKG_DESCRIPTION="Decoder implementation of the JBIG2 image compression format"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.18
-TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs952/jbig2dec-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9e19775237350e299c422b7b91b0c045e90ffa4ba66abf28c8fb5eb005772f5e
+TERMUX_PKG_VERSION=0.19
+TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9530/jbig2dec-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=279476695b38f04939aa59d041be56f6bade3422003a406a85e9792c27118a37
 TERMUX_PKG_DEPENDS="libpng"
 TERMUX_PKG_BREAKS="jbig2dec-dev"
 TERMUX_PKG_REPLACES="jbig2dec-dev"
--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.49.5
+TERMUX_PKG_VERSION=2.50.0
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=daa64941bb4732bdf51b902a72c6e04063235cfce6986d910ba0759c76917795
+TERMUX_PKG_SHA256=b3fadba240f09b9c9898ab20cb7311467243e607cf8f928b7c5f842474ee3df4
 TERMUX_PKG_DEPENDS="gdk-pixbuf, libcairo, libcroco, pango, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
