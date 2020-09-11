acf12000f1ec11c2674bffd495028ae2866ab006
Butta <repo@butta.fastem.com>
2020-09-11 23:44:44 +0530

    Revert z3 update and bump vim and vim-python on the CI

--- packages/z3/build.sh
+++ packages/z3/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/Z3Prover/z3
 TERMUX_PKG_DESCRIPTION="Z3 is a theorem prover from Microsoft Research."
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=4.8.9
+TERMUX_PKG_VERSION=4.8.8
 TERMUX_PKG_SRCURL=https://github.com/Z3Prover/z3/archive/z3-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c9fd04b9b33be74fffaac3ec2bc2c320d1a4cc32e395203c55126b12a14ff3f4
+TERMUX_PKG_SHA256=6962facdcdea287c5eeb1583debe33ee23043144d0e5308344e6a8ee4503bcff
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
