c9be7b35ee81d33347387bad4fc0b6a7d34a1584
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-12 19:37:03 +0000

    libxkbcommon: update to 1.0.1

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://xkbcommon.org/
 TERMUX_PKG_DESCRIPTION="Keymap handling library for toolkits and window systems"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.10.0
-TERMUX_PKG_REVISION=13
+TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9b4635cf5d9fc0fb9611ceec1780aafc0944299e9a29ab09c18ec2633923b9c3
+TERMUX_PKG_SHA256=270e2ad4ce5699f633e49042114cb68a5697fa1ed45b65c1d96a833cfac20954
 TERMUX_PKG_DEPENDS="libxcb, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 
