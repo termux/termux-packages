ec73f710fffe784166ec734078a7189a4750817a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-12 19:59:15 +0000

    thunar: update to 4.15.2

--- x11-packages/thunar/build.sh
+++ x11-packages/thunar/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/xfce/thunar/start
 TERMUX_PKG_DESCRIPTION="Modern file manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=4.15.0
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=4.15.2
 TERMUX_PKG_SRCURL=https://gitlab.xfce.org/xfce/thunar/-/archive/thunar-${TERMUX_PKG_VERSION}/thunar-thunar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c9b99606cad98fdaad039bae052ce9ee4376ef7b4844bbd458d1390342a8299e
+TERMUX_PKG_SHA256=0c816a16d62fa3d6325543e05df42e022a1620d9a5152264abe50498fc9ed86c
 TERMUX_PKG_DEPENDS="desktop-file-utils, exo, hicolor-icon-theme, libexif, libnotify, libpng, libxfce4ui, libxfce4util"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-maintainer-mode"
 
