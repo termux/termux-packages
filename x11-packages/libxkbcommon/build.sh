7ad94967dd8db5771e86c6d3b8a2e9ddf713e262
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-12 19:43:57 +0000

    libxkbcommon: add libxml2 dependency and update patch

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=270e2ad4ce5699f633e49042114cb68a5697fa1ed45b65c1d96a833cfac20954
-TERMUX_PKG_DEPENDS="libxcb, xkeyboard-config"
+TERMUX_PKG_DEPENDS="libxcb, libxml2, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
