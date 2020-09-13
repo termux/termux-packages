420ee1ce71bcac156504cc65ecc5ee828a4816d7
Jia Yuan Lo <jylo06g@gmail.com>
2020-09-14 00:50:12 +0800

    boinc: update to 7.16.11 (#5795)
    * boinc: update to 7.16.11
    
    * boinc: update patches for 7.16.11
    
    * boinc: disable switcher
    
    termux/termux-packages#5668
    
    * boinc: add oobe improvement patches
    
    * boinc: simplify disable-whetneon.patch
    
    termux/termux-packages#5795
    The old patch is getting hard to maintain because someone think it is a good
    idea to support non-NEON Android devices in 2020.
    The new patch simplify things by only disable 32bit ARM host detection so that
    whetneon and whetvfp will not be built which can make the client too complex.
    
    Apply this patch and please dont use "-DANDROID" and "--host arm-linux*"
    
    * boinc: fix 32bit compatibility on 64bit Termux
    
    Remove blocking request 32bit executables and fix it instead.
    Add patch that unset LD_PRELOAD environment variable before running 32bit
    BOINC executables.
    This is applicable for 64bit Android devices running BOINC on Termux.
    No manual unset from user is needed.
    
    * boinc: simplify disable-whetneon.patch v2

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -1,22 +1,23 @@
 TERMUX_PKG_HOMEPAGE=https://boinc.berkeley.edu/
 TERMUX_PKG_DESCRIPTION="Open-source software for volunteer computing"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=7.16.7
+TERMUX_PKG_VERSION=7.16.11
 TERMUX_PKG_SRCURL=https://github.com/BOINC/boinc/archive/client_release/${TERMUX_PKG_VERSION:0:4}/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=38130d532031e75701eee910da64b9eb837e5bfeff9979dbb200c37146be3fed
+TERMUX_PKG_SHA256=8a3f9453b16acfd8c636d18b4939c461c751aa1dd9e108cf60b90a3909bfa0a9
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libcurl, openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-server
---enable-client
 --disable-manager
 "
 
 TERMUX_PKG_CONFFILES="etc/boinc-client.conf"
 
 termux_step_pre_configure() {
+	CFLAGS="${CFLAGS/-Oz/-O2}"
+	CXXFLAGS="${CXXFLAGS/-Oz/-O2}"
 	LDFLAGS+=" -landroid-shmem"
 	./_autosetup
 }
