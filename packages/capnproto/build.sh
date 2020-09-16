d89cc4933876acd8b5f9ffa2ebd4bca9e15a5652
ihsakashi <yuvch122@gmail.com>
2020-09-16 14:50:59 -0600

    let termux configure

--- packages/capnproto/build.sh
+++ packages/capnproto/build.sh
@@ -5,9 +5,3 @@ TERMUX_PKG_VERSION=0.8.0
 TERMUX_PKG_SRCURL=https://capnproto.org/capnproto-c++-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d1f40e47574c65700f0ec98bf66729378efabe3c72bc0cda795037498541c10d
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_configure() {
-    ./configure \
-        --prefix=$TERMUX_PREFIX \
-        --host=$TERMUX_HOST_PLATFORM
-}
