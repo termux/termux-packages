ade3c5314eb0e7c68bd786dab2b9f74cd2729340
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-09-17 16:54:26 +0530

    Update hors to 0.7.1

--- packages/hors/build.sh
+++ packages/hors/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/WindSoilder/hors
 TERMUX_PKG_DESCRIPTION="Instant coding answers via the command line (howdoi in rust)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.6.10
+TERMUX_PKG_VERSION=0.7.1
 TERMUX_PKG_SRCURL=https://github.com/WindSoilder/hors/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f2457015b87c6ce0094ece1928f5c23c97ce0686314fdc0d2e351dd4fefde2e0
+TERMUX_PKG_SHA256=7feceb88d2c27a8cf7fc3996c8dc464e8b6d1fe42412ce8b80e4047c7c790a16
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="arm"
 
