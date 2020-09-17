96bbb3a824acb34bf9c9865b2860be35a9432b30
Butta <repo@butta.fastem.com>
2020-09-17 11:51:41 +0530

    libdispatch: update from 5.2.3 to 5.3

--- packages/libdispatch/build.sh
+++ packages/libdispatch/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/apple/swift-corelibs-libdispatch
 TERMUX_PKG_DESCRIPTION="The libdispatch project, for concurrency on multicore hardware"
 TERMUX_PKG_LICENSE="Apache-2.0"
-_VERSION=5.2.3
+_VERSION=5.3
 TERMUX_PKG_VERSION=1:${_VERSION}
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${_VERSION}-RELEASE.tar.gz
-TERMUX_PKG_SHA256=89b5910e80599d3168096f1dc9fb8883d6ecb042cdee144209f03b783249bdda
+TERMUX_PKG_SHA256=6805b555aab65d740fccaa99570fd29b32efa6c310fd42524913e44509dc4969
 TERMUX_PKG_DEPENDS="libc++, libblocksruntime"
