2dd5645364798787c9355b4a0bc898026b3e6cae
ifurther <55025025+ifurther@users.noreply.github.com>
2020-09-18 21:36:55 +0800

    New package: sysbench

---
+++ packages/sysbench/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/akopytov/sysbench
+TERMUX_PKG_DESCRIPTION=" Scriptable multi-threaded benchmark tool for databases and systems. Popular and used to benchmark many different systems."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.0.20
+TERMUX_PKG_SRCURL=https://github.com/akopytov/sysbench/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=e8ee79b1f399b2d167e6a90de52ccc90e52408f7ade1b9b7135727efe181347f
+TERMUX_PKG_DEPENDS="luajit, libluajit, postgresql, libaio, concurrencykit"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-pgsql --without-mysql --with-system-luajit --with-system-ck"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	./autogen.sh 
+}
