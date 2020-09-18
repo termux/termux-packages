62275a0134b11bf4f3806ebd62cad1902f40d635
ifurther <55025025+ifurther@users.noreply.github.com>
2020-09-18 21:35:55 +0800

    New package: libaio

---
+++ packages/libaio/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://lse.sourceforge.net/io/aio.html
+TERMUX_PKG_DESCRIPTION="The Linux-native asynchronous I/O facility (aio) library."
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_VERSION=0.3.112
+TERMUX_PKG_SRCURL=https://releases.pagure.org/libaio/libaio-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=ab0462f2c9d546683e5147b1ce9c195fe95d07fac5bf362f6c01637955c3b492
+#TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_BUILD_IN_SRC=true
