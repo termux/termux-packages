1bfb88cdd0f632eaef10ec99ad15012c29c0bac7
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-19 19:38:33 +0300

    disabled packages: delete 'ffsend' as useless now
    As Mozilla closed 'Firefox Send' service forever, client utility
    is useless now.
    
    https://support.mozilla.org/en-US/kb/what-happened-firefox-send:
    > Firefox Send has been discontinued as of September 17th, 2020.
    > You will no longer be able to upload or receive files. We’d like
    > to thank all of you who tried Firefox Send.

--- disabled-packages/ffsend/build.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://gitlab.com/timvisee/ffsend
-TERMUX_PKG_DESCRIPTION="A fully featured Firefox Send client"
-TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.2.64
-TERMUX_PKG_SRCURL=https://gitlab.com/timvisee/ffsend/-/archive/v$TERMUX_PKG_VERSION/ffsend-v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=fdd2fd28e29cf4034b210558b5b5049592bc970344cbf122637d37ef8dc77589
-TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features crypto-openssl,send2,send3,history,archive,qrcode,urlshorten,infer-command"
