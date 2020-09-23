e9a7dd856ab20eb7c0cf893255c2964980074666
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-23 23:26:49 +0300

    transmission-gtk: update to 3.00

--- x11-packages/transmission-gtk/build.sh
+++ x11-packages/transmission-gtk/build.sh
@@ -1,30 +1,23 @@
 TERMUX_PKG_HOMEPAGE=https://transmissionbt.com/
 TERMUX_PKG_DESCRIPTION="Easy, lean and powerful BitTorrent client (gtk3)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.94
-TERMUX_PKG_REVISION=20
-TERMUX_PKG_SRCURL=https://github.com/transmission/transmission/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=440c2fd0f89b1ab59d8a4b79ecd7bffd61bc000e36fb5b6c8e88142a4fadbb1f
-
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, libcairo, libcurl, libevent, openssl, pango, zlib"
+TERMUX_PKG_VERSION=3.00
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_SRCURL=https://github.com/transmission/transmission.git
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, libcairo, libcurl, libevent, miniupnpc, openssl, pango, zlib"
 TERMUX_PKG_CONFLICTS="transmission"
 TERMUX_PKG_REPLACES="transmission"
-
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-nls
---enable-cli
---enable-gtk
---enable-lightweight
---cache-file=termux_configure.cache
-"
-
-TERMUX_PKG_RM_AFTER_INSTALL="share/icons/hicolor/icon-theme.cache"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-nls --enable-cli --enable-gtk --enable-lightweight --cache-file=termux_configure.cache"
+# transmission already puts timestamps in the info printed to stdout so no need for svlogd -tt,
+# therefore we override the transmission/log run script
+TERMUX_PKG_SERVICE_SCRIPT=(
+	"transmission" 'exec transmission-daemon -f 2>&1'
+	"transmission/log" 'mkdir -p "$LOGDIR/sv/transmission"\nexec svlogd "$LOGDIR/sv/transmission"'
+)
 
 termux_step_pre_configure() {
+	CFLAGS+=" -D_POSIX_C_SOURCE=200809L"
 	./autogen.sh
-	# liblog for android logging in syslog hack:
-	LDFLAGS+=" -landroid-shmem -llog"
 
 	echo "ac_cv_func_getmntent=no" >> termux_configure.cache
 	echo "ac_cv_search_getmntent=false" >> termux_configure.cache
