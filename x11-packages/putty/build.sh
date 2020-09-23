cb23c90d3952a8ab873c7a60b5251d07e66818c5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-23 23:03:04 +0300

    putty: update to 0.74

--- x11-packages/putty/build.sh
+++ x11-packages/putty/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.chiark.greenend.org.uk/~sgtatham/putty/
 TERMUX_PKG_DESCRIPTION="A terminal integrated SSH/Telnet client"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.73
-TERMUX_PKG_REVISION=18
+TERMUX_PKG_VERSION=0.74
 TERMUX_PKG_SRCURL=https://the.earth.li/~sgtatham/putty/${TERMUX_PKG_VERSION}/putty-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3db0b5403fb41aecd3aa506611366650d927650b6eb3d839ad4dcc782519df1c
+TERMUX_PKG_SHA256=ddd5d388e51dd9e6e294005b30037f6ae802239a44c9dc9808c779e6d11b847d
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, libandroid-glob, libandroid-shmem, libcairo, libx11, pango"
 
 termux_step_pre_configure() {
