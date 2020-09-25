7ab5be42b193cef7601ce637a2adbe58ab27af4d
ihsakashi <63768664+ihsakashi@users.noreply.github.com>
2020-09-25 08:27:15 -0600

    new package: capnproto (#5813)

---
+++ packages/capnproto/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://capnproto.org/
+TERMUX_PKG_DESCRIPTION="Data interchange format and capability-based RPC system."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_SRCURL=https://capnproto.org/capnproto-c++-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d1f40e47574c65700f0ec98bf66729378efabe3c72bc0cda795037498541c10d
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_FORCE_CMAKE=true
