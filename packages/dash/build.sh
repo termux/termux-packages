9d92a7360cc63663bc8aaf8b7fb1273122c8c9f1
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-09-27 23:43:47 +0300

    add patches for prefix verification - prevent use of prebuilt packages by third parties
    Add a small stub which verifies whether /data/data/com.termux/files/usr is readable
    into few important packages such like Bash or APT. If this check fails, e.g.
    in case if binary was executed in program with different package name (non-Termux),
    program will print warning and exit with error status immediately.
    
    This should prevent use of prebuilt packages by third-party applications.
    
    NOTE: for now it uses /data/data/com.termux/files/usr in reversed order to
    prevent automatic "blind" patching with sed. Will replace with randomized
    XOR-based variant in case if that measure won't be enough.

--- packages/dash/build.sh
+++ packages/dash/build.sh
@@ -2,10 +2,19 @@ TERMUX_PKG_HOMEPAGE=http://gondor.apana.org.au/~herbert/dash/
 TERMUX_PKG_DESCRIPTION="Small POSIX-compliant implementation of /bin/sh"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_VERSION=0.5.11.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://gondor.apana.org.au/~herbert/dash/files/dash-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=00fb7d68b7599cc41ab151051c06c01e9500540183d8aa72116cb9c742bd6d5f
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
+
+termux_step_pre_configure() {
+	# Prefix verification patch should be applied only for the
+	# builds with original prefix.
+	if [ "$TERMUX_PREFIX" = "/data/data/com.termux/files/usr" ]; then
+		patch -p1 -i $TERMUX_PKG_BUILDER_DIR/verify-prefix.patch.txt
+	fi
+}
 
 termux_step_post_make_install() {
 	# Symlink sh -> dash
