798f50aec04a6047a1a9580a61d4f6cd68e14aa8
Henrik Grimler <henrik@grimler.se>
2020-09-28 19:53:47 +0200

    ghostscript: update to 9.53.2

--- packages/ghostscript/build.sh
+++ packages/ghostscript/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.ghostscript.com/
 TERMUX_PKG_DESCRIPTION="Interpreter for the PostScript language and for PDF"
 TERMUX_PKG_LICENSE="AGPL-V3"
-TERMUX_PKG_VERSION=9.52
+TERMUX_PKG_VERSION=9.53.2
 TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs${TERMUX_PKG_VERSION//.}/ghostpdl-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8f6e48325c106ae033bbae3e55e6c0b9ee5c6b57e54f7cd24fb80a716a93b06a
+TERMUX_PKG_SHA256=5559b2d094a6b977e7a71e7e9f587f1addc608137fdaae10c19faa332bc369ba
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libtiff, libjpeg-turbo, libpng, libexpat, freetype, fontconfig, libidn, littlecms, openjpeg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_lcms2__cmsCreateMutex=yes
