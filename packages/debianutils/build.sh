353e8904bb43b43d660473120a78a6a1c36b915e
Henrik Grimler <henrik@grimler.se>
2020-09-28 19:54:12 +0200

    debianutils: update to 4.11.2

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/debianutils
 TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installation scripts of Debian packages"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=4.11.1
+TERMUX_PKG_VERSION=4.11.2
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8be869f19c55c18d53d9f0414b68bb966a068b2154e9fbbfc6193827d6af983c
+TERMUX_PKG_SHA256=3b680e81709b740387335fac8f8806d71611dcf60874e1a792e862e48a1650de
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/add-shell
