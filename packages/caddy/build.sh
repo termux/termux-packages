0b36cca25b78dc75c79dd2696c87626498c97515
Henrik Grimler <henrik@grimler.se>
2020-09-28 19:54:26 +0200

    caddy: update to 2.2.0

--- packages/caddy/build.sh
+++ packages/caddy/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://caddyserver.com/
 TERMUX_PKG_DESCRIPTION="Fast, cross-platform HTTP/2 web server"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=2.1.1
+TERMUX_PKG_VERSION=2.2.0
 TERMUX_PKG_SRCURL=https://github.com/mholt/caddy/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=77beb13b39b670bfe9e0cc1c71b720d5b037cca60e1426a9a485bbfae34ba8d2
+TERMUX_PKG_SHA256=82dde471af56db86f96a2dffc0631c4e0a9eea19debe966605a73b89fbb945b4
 
 termux_step_make() {
 	termux_setup_golang
