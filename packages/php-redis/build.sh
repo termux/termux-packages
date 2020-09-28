12721c78ce82475502b72b669a38a609bbf379e1
qubist-pixel <72007939+qubist-pixel@users.noreply.github.com>
2020-09-28 17:48:38 +0530

    new package: php-redis (#5859)

---
+++ packages/php-redis/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/phpredis/phpredis
+TERMUX_PKG_DESCRIPTION="PHP extension for interfacing with Redis"
+TERMUX_PKG_LICENSE="PHP-3.0"
+TERMUX_PKG_VERSION=5.3.1
+TERMUX_PKG_SRCURL=https://github.com/phpredis/phpredis/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=930dc88ef126509b8991c52757fdc68908c753b476ad6f25dae0ce6925870f14
+TERMUX_PKG_DEPENDS=php
+
+termux_step_pre_configure() {
+	$TERMUX_PREFIX/bin/phpize
+}
