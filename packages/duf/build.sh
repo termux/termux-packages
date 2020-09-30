f181dafcca56ada1bfc54e96d25320547f4cfaac
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-01 00:27:24 +0530

    Add duf
    A Disk usage/free utility

---
+++ packages/duf/build.sh
@@ -0,0 +1,29 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/muesli/duf
+TERMUX_PKG_DESCRIPTION="Disk usage/free utility"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=0.3.1
+TERMUX_PKG_SRCURL=https://github.com/muesli/duf/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=98b5cdc4a7b3c5eddc0ec397c65860110b446e85cc4f3cfe3e32f31ce524e90d
+
+termux_step_make() {
+        termux_setup_golang
+
+        cd "$TERMUX_PKG_SRCDIR"
+
+        mkdir -p "${TERMUX_PKG_BUILDDIR}/src/github.com/muesli"
+        cp -a "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/src/github.com/muesli/duf"
+        cd "${TERMUX_PKG_BUILDDIR}/src/github.com/muesli/duf"
+
+        go get -d -v
+        go build
+}
+
+termux_step_make_install() {
+        install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/muesli/duf/duf \
+                         $TERMUX_PREFIX/bin/duf
+        mkdir -p $TERMUX_PREFIX/share/doc/duf
+
+        install ${TERMUX_PKG_BUILDDIR}/src/github.com/muesli/duf/README.md \
+                        $TERMUX_PREFIX/share/doc/duf
+}
