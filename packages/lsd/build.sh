c99e3f451ac2c3d6a7e4b06f7fc2eef8b2ac5f73
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-01 00:14:52 +0530

    Add new package: lsd
    Next generation ls command

---
+++ packages/lsd/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Peltoche/lsd 
+TERMUX_PKG_DESCRIPTION="Next gen ls command"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=0.18.0
+TERMUX_PKG_SRCURL=https://github.com/Peltoche/lsd/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2905e5d8d61d98d60c65544f3d41ae77459c8a92a4c7acafadcfb0233b2b7f95
+TERMUX_PKG_BUILD_IN_SRC=true
