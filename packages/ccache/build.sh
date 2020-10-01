738f779cb6f741bfa6b01b049230b5b5299b7e0f
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-01 19:13:55 +0530

    ccache: update to 3.7.12 (#5874)

--- packages/ccache/build.sh
+++ packages/ccache/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://ccache.samba.org
 TERMUX_PKG_DESCRIPTION="Compiler cache for fast recompilation of C/C++ code"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=3.7.11
+TERMUX_PKG_VERSION=3.7.12
 TERMUX_PKG_SRCURL=https://github.com/ccache/ccache/releases/download/v$TERMUX_PKG_VERSION/ccache-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=8d450208099a4d202bd7df87caaec81baee20ce9dd62da91e9ea7b95a9072f68
+TERMUX_PKG_SHA256=a02f4e8360dc6618bc494ca35b0ae21cea080f804a4898eab1ad3fcd108eb400
 TERMUX_PKG_DEPENDS="zlib"
