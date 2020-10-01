b660ee9a61f8a671a5c06a799f1a90b069c59c5c
Henrik Grimler <henrik@grimler.se>
2020-10-01 16:18:16 +0300

    lua-lpeg: use liblua5.3

--- packages/lua-lpeg/build.sh
+++ packages/lua-lpeg/build.sh
@@ -3,18 +3,18 @@ TERMUX_PKG_DESCRIPTION="Pattern-matching library for Lua 5.3"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.0.2
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=http://www.inf.puc-rio.br/~roberto/lpeg/lpeg-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=48d66576051b6c78388faad09b70493093264588fcd0f258ddaab1cdd4a15ffe
-TERMUX_PKG_DEPENDS="liblua"
+TERMUX_PKG_DEPENDS="liblua53"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	make \
 		CC="$CC" \
-		CFLAGS="$CFLAGS -fPIC" \
-		LDFLAGS="$LDFLAGS -llua" \
-		LUADIR="$TERMUX_PREFIX"/include
+		CFLAGS="$CFLAGS -fPIC -I$TERMUX_PREFIX/include/lua5.3" \
+		LDFLAGS="$LDFLAGS -L$TERMUX_PREFIX/lib/lua/5.3 -llua5.3" \
+		LUADIR="$TERMUX_PREFIX"/include/lua/5.3
 }
 
 termux_step_make_install() {
