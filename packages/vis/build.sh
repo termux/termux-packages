790829d4e929283773e2d1807a88fbed0b982ac2
Henrik Grimler <henrik@grimler.se>
2020-10-01 16:18:27 +0300

    vis: depend on liblua53

--- packages/vis/build.sh
+++ packages/vis/build.sh
@@ -3,8 +3,12 @@ TERMUX_PKG_DESCRIPTION="Modern, legacy free, simple yet efficient vim-like edito
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=0.6
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/martanne/vis/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9ab4a3f1c5953475130b3c286af272fe5cfdf7cbb7f9fbebd31e9ea4f34e487d
-TERMUX_PKG_DEPENDS="liblua, libtermkey, lua-lpeg, ncurses"
+TERMUX_PKG_DEPENDS="liblua53, libtermkey, lua-lpeg, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	CFLAGS+=" -I$TERMUX_PREFIX/include/lua5.3"
+}
