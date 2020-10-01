6e07d4322463c1542fa3ab67150149317e6d30aa
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-01 18:29:37 +0530

    Update build.sh

--- packages/ccache/build.sh
+++ packages/ccache/build.sh
@@ -3,5 +3,5 @@ TERMUX_PKG_DESCRIPTION="Compiler cache for fast recompilation of C/C++ code"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=3.7.12
 TERMUX_PKG_SRCURL=https://github.com/ccache/ccache/releases/download/v$TERMUX_PKG_VERSION/ccache-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=9d561dfb435d5d2bf6d8648266dd9bcc94979b8b4b9df7dfb24fb83f29af5101
+TERMUX_PKG_SHA256=a02f4e8360dc6618bc494ca35b0ae21cea080f804a4898eab1ad3fcd108eb400
 TERMUX_PKG_DEPENDS="zlib"
