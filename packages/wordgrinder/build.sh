8b32453985f6a928b5cd2fd11a2b0dab06b9f2ec
Henrik Grimler <henrik@grimler.se>
2020-10-01 16:18:32 +0300

    wordgrinder: depend on liblua53

--- packages/wordgrinder/build.sh
+++ packages/wordgrinder/build.sh
@@ -3,21 +3,21 @@ TERMUX_PKG_DESCRIPTION="A Unicode-aware character cell word processor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="licenses/COPYING.Lua, licenses/COPYING.LuaBitOp, licenses/COPYING.LuaFileSystem, licenses/COPYING.Minizip, licenses/COPYING.Scowl, licenses/COPYING.uthash, licenses/COPYING.wcwidth, licenses/COPYING.WordGrinder, licenses/COPYING.xpattern"
 TERMUX_PKG_VERSION=0.7.2
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://github.com/davidgiven/wordgrinder/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=4e1bc659403f98479fe8619655f901c8c03eb87743374548b4d20a41d31d1dff
-TERMUX_PKG_DEPENDS="liblua, ncurses, zlib"
+TERMUX_PKG_DEPENDS="liblua53, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_ninja
 	make CC=gcc OBJDIR="$PWD/build" "$PWD"/build/lua
-	make OBJDIR="$PWD/build" LUA_PACKAGE=lua
+	make OBJDIR="$PWD/build" LUA_PACKAGE=lua53
 }
 
 termux_step_make_install() {
 	install -Dm700 \
-		"$TERMUX_PKG_SRCDIR"/bin/wordgrinder-lua-curses-release \
+		"$TERMUX_PKG_SRCDIR"/bin/wordgrinder-lua53-curses-release \
 		"$TERMUX_PREFIX"/bin/wordgrinder
 	install -Dm600 \
 		"$TERMUX_PKG_SRCDIR"/bin/wordgrinder.1 \
