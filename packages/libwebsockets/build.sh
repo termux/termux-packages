9637c6213c36639c694c863c72953d770daf1cc6
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-01 12:43:04 +0300

    libwebsockets: bump revision

--- packages/libwebsockets/build.sh
+++ packages/libwebsockets/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://libwebsockets.org
 TERMUX_PKG_DESCRIPTION="Lightweight C websockets library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=4.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f15a7189c5fe6109d260615dec8a0c6dfc962ed5931fb6f0fddd72fbe49f02b0
 TERMUX_PKG_DEPENDS="openssl, libuv, zlib"
