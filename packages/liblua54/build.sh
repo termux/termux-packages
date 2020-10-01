a0f65f038c720e956755c772c9531add469d1042
Henrik Grimler <henrik@grimler.se>
2020-10-01 16:18:08 +0300

    liblua: rename to liblua53 and add liblua54

---
+++ packages/liblua54/build.sh
@@ -0,0 +1,38 @@
+TERMUX_PKG_HOMEPAGE=https://www.lua.org/
+TERMUX_PKG_DESCRIPTION="Shared library for the Lua interpreter"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=5.4.0
+TERMUX_PKG_SRCURL=https://www.lua.org/ftp/lua-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=eac0836eb7219e421a96b7ee3692b93f0629e4cdb0c788432e3d10ce9ed47e28
+TERMUX_PKG_EXTRA_MAKE_ARGS=linux
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BREAKS="liblua-dev"
+TERMUX_PKG_REPLACES="liblua-dev"
+TERMUX_PKG_BUILD_DEPENDS="readline"
+
+termux_step_configure() {
+	sed -e "s/%VER%/${TERMUX_PKG_VERSION%.*}/g;s/%REL%/${TERMUX_PKG_VERSION}/g" \
+		-e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" \
+		"$TERMUX_PKG_BUILDER_DIR"/lua.pc.in > lua.pc
+}
+
+termux_step_pre_configure() {
+	AR+=" rcu"
+	CFLAGS+=" -fPIC -DLUA_COMPAT_5_2 -DLUA_COMPAT_UNPACK"
+	export MYLDFLAGS=$LDFLAGS
+}
+
+termux_step_make_install() {
+	make \
+		TO_BIN="lua5.4 luac5.4" \
+		TO_LIB="liblua5.4.so liblua5.4.so.5.4 liblua5.4.so.${TERMUX_PKG_VERSION}" \
+		INSTALL_DATA="cp -d" \
+		INSTALL_TOP="$TERMUX_PREFIX" \
+		INSTALL_INC="$TERMUX_PREFIX/include/lua5.4" \
+		INSTALL_MAN="$TERMUX_PREFIX/share/man/man1" \
+		install
+	install -Dm600 lua.pc "$TERMUX_PREFIX"/lib/pkgconfig/lua54.pc
+
+	mv -f "$TERMUX_PREFIX"/share/man/man1/lua.1 "$TERMUX_PREFIX"/share/man/man1/lua5.4.1
+	mv -f "$TERMUX_PREFIX"/share/man/man1/luac.1 "$TERMUX_PREFIX"/share/man/man1/luac5.4.1
+}
