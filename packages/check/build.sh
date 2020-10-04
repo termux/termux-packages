6eaad9e8b26d98d56d665b255a8cb3650366b14e
Rabby Sheikh <xploitednoob@gmail.com>
2020-10-04 22:46:47 +0600

    new package:check (#5886)

---
+++ packages/check/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://libcheck.github.io/check
+TERMUX_PKG_DESCRIPTION="A unit testing framework for C"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
+TERMUX_PKG_VERSION=0.15.2
+TERMUX_PKG_SRCURL=https://github.com/libcheck/check/releases/download/${TERMUX_PKG_VERSION}/check-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=a8de4e0bacfb4d76dd1c618ded263523b53b85d92a146d8835eb1a52932fa20a
+TERMUX_PKG_DEPENDS="libtool, pkg-config, texinfo"
+TERMUX_PKG_BUILD_IN_SRC=true
