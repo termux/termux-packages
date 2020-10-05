380fb6e4773ee12ac921c5cfa1f99961ee0a152b
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-05 07:55:49 +0530

    Update croc to 8.4.0

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to ano
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_VERSION=8.4.0
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=7818976d1c773f79c4f92f83693ee4c02fc57852d99dfb09548427953c2b8fb3
+TERMUX_PKG_SHA256=2f0de360e252974dbb7d2ab688f17b0ad6e842b48a33bd33ec9272055aca2780
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
