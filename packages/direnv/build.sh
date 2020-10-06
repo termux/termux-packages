dbebc5f8bad0fa38f461459afea1a47fb1affbe3
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-06 15:26:04 +0530

    direnv: update to 2.22.1 (#5897)

--- packages/direnv/build.sh
+++ packages/direnv/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/direnv/direnv
 TERMUX_PKG_DESCRIPTION="Environment switcher for shell"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.22.0
+TERMUX_PKG_VERSION=2.22.1
 TERMUX_PKG_SRCURL=https://github.com/direnv/direnv/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=bf35d267a28dcace4109c3256f2cd7cb0ca3b8d461d2fbf848db3f65b809befd
+TERMUX_PKG_SHA256=b902f30deadd332b8553ca0f34aacf6daf62bd104b5d57ecc9bee2fdcd70d1c7
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
