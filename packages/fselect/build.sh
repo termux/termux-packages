146e0ba7086d1ce2e0353ba0747bb3e11a71a100
valpogus <32718480+valpogus@users.noreply.github.com>
2020-10-08 17:14:01 +0200

    fselect: update to 0.7.1

--- packages/fselect/build.sh
+++ packages/fselect/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jhspetersson/fselect
 TERMUX_PKG_DESCRIPTION="Find files with SQL-like queries"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
-TERMUX_PKG_VERSION=0.6.10
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.7.1
 TERMUX_PKG_SRCURL=https://github.com/jhspetersson/fselect/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1fa86cd22fdb4a38338c343f9a917e579a6f680c961e9dca8d1a2178ab4d926b
+TERMUX_PKG_SHA256=38cb9c56aa6568088d6dbdc9512c0fd16e05b07f55684b2c8c9fa09533f58f84
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
