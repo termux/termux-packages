2c3fb311c6373a39b27f7706384a39679612b5c7
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-08 18:57:20 +0300

    rdiff-backup: rebuild with python 3.9

--- packages/rdiff-backup/build.sh
+++ packages/rdiff-backup/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://rdiff-backup.net
 TERMUX_PKG_DESCRIPTION="A utility for local/remote mirroring and incremental backups"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.0.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rdiff-backup/rdiff-backup/releases/download/v${TERMUX_PKG_VERSION/\~/}/rdiff-backup-${TERMUX_PKG_VERSION/\~/}.tar.gz
 TERMUX_PKG_SHA256=2bb7837b4a9712b6efaebfa7da8ed6348ffcb02fcecff0e19d8fff732e933b87
 TERMUX_PKG_DEPENDS="librsync, python"
@@ -12,7 +13,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	local _PYTHON_VERSION=3.8
+	local _PYTHON_VERSION=3.9
 	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
 	export CPPFLAGS+=" -I${TERMUX_PREFIX}/include/python${_PYTHON_VERSION}"
 	export LDFLAGS+=" -lpython${_PYTHON_VERSION}"
