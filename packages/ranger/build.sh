7b97c119419b5c47f6358a21666e424459b7a9c8
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-08 18:56:38 +0300

    ranger: rebuild with python 3.9

--- packages/ranger/build.sh
+++ packages/ranger/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://ranger.github.io/
 TERMUX_PKG_DESCRIPTION="File manager with VI key bindings"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.9.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ranger/ranger/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ce088a04c91c25263a9675dc5c43514b7ec1b38c8ea43d9a9d00923ff6cdd251
 TERMUX_PKG_DEPENDS="python, file"
@@ -13,7 +14,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	python3.8 setup.py install --prefix=$TERMUX_PREFIX --force
+	python3.9 setup.py install --prefix=$TERMUX_PREFIX --force
 }
 
 termux_step_post_massage() {
