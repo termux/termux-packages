4117ac54b55e0a194720b4ceaae164abbaf047bd
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-09 11:59:10 +0530

    Add new package k9s
    #5910

---
+++ packages/k9s/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://k9scli.io
+TERMUX_PKG_DESCRIPTION="Kubernetes CLI To Manage Your Clusters In Style!"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=0.22.1
+TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1ec1b3f0c18d74f73cca0fa957ca94d3577ec558afb32702ea887ecda286af13
+
+termux_step_make() {
+        termux_setup_golang                                                      
+        cd "$TERMUX_PKG_SRCDIR"
+        mkdir -p "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed"
+        cp -a "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s"
+        cd "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s"
+
+        go get -d -v
+        go build
+}
+
+termux_step_make_install() {
+        install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s/k9s \
+                 $TERMUX_PREFIX/bin/k9s
+}
