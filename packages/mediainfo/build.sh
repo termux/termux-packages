ee8699f8f10fc495562971be8a15e986fb1132ec
valpogus <32718480+valpogus@users.noreply.github.com>
2020-10-09 18:04:03 +0200

    mediainfo: update to 20.09

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mediaarea.net/en/MediaInfo
 TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
-TERMUX_PKG_VERSION=20.08
+TERMUX_PKG_VERSION=20.09
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cf7dcd874a6d7f7208f01fc73f02652d4aab213b236e41cdcf2c69d9823a45a7
+TERMUX_PKG_SHA256=fad981d2c8154c98f4107da1d5bfafbe59fd7198519cc470e06bdd335c7819c3
 TERMUX_PKG_DEPENDS="libmediainfo"
 
 termux_step_pre_configure() {
