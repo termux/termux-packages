154933d428ae1384e43a2caf6a29badda42ac239
Deshdeepak <rkdeshdeepak1@gmail.com>
2020-10-10 10:55:34 +0530

    starship: update to 0.46.0

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://starship.rs
 TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_VERSION=0.45.2
+TERMUX_PKG_VERSION=0.46.0
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=99bbcd2eb0437627741feba5eb7c7b208334146ef2c5ffb5ceaafebf32bdde2a
+TERMUX_PKG_SHA256=e53087b4bd206fb971fb21daf27b1640a7c72adddcdbed1e469f0f3a0863d4ae
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
