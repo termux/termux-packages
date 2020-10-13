54788eaa1166ec6a3faf16244db52c6fefae64c5
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-10-13 15:03:19 +0530

    croc: update to 8.5.0 (#5938)

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another."
 TERMUX_PKG_LICENSE=MIT
-TERMUX_PKG_VERSION=8.4.0
+TERMUX_PKG_VERSION=8.5.0
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=2f0de360e252974dbb7d2ab688f17b0ad6e842b48a33bd33ec9272055aca2780
+TERMUX_PKG_SHA256=54cad5b72a8099d879c69d9d4bb5e190bd4f861b4ccab509afdae5ffbd23d141
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
