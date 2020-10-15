76f2fbec5ef544bfd3e4bc96d189df7acff7ec2e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-15 15:19:33 +0300

    debootstrap: do not run 'chown 0:0' on fstab
    https://github.com/termux/termux-packages/issues/5932

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wiki.debian.org/Debootstrap
 TERMUX_PKG_DESCRIPTION="Bootstrap a basic Debian system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.0.123
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5e5a8147ecdd6be0eea5ac4d6ed8192cc653e93f744dd3306c9b1cc51d6ca328
-TERMUX_PKG_DEPENDS="wget, proot, perl"
+TERMUX_PKG_DEPENDS="perl, proot, wget"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
