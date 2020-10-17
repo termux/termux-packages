9136d624cf2178611924452a479469ae7e7da3cf
Henrik Grimler <henrik@grimler.se>
2020-10-17 20:50:39 +0200

    lazygit: update to 0.23.6

--- packages/lazygit/build.sh
+++ packages/lazygit/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/jesseduffield/lazygit
 TERMUX_PKG_DESCRIPTION="Simple terminal UI for git commands"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.22.8
+TERMUX_PKG_VERSION=0.23.6
 TERMUX_PKG_SRCURL=https://github.com/jesseduffield/lazygit/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1771c113b8b93db3321a90ff8270e736628a156a44bd6963200656a49ab488c1
+TERMUX_PKG_SHA256=b2383a70e54a67382b0f052149891d3d0f3e1eba952a0fa5009214c6591dfc6b
 TERMUX_PKG_RECOMMENDS=git
 
 termux_step_make() {
