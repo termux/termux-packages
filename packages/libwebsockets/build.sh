515bdbd5f990cdd30d7e311598ef81241a63969c
Henrik Grimler <henrik@grimler.se>
2020-10-17 19:04:27 +0200

    libwebsockets: update to 4.1.3

--- packages/libwebsockets/build.sh
+++ packages/libwebsockets/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libwebsockets.org
 TERMUX_PKG_DESCRIPTION="Lightweight C websockets library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=4.1.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.1.3
 TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f15a7189c5fe6109d260615dec8a0c6dfc962ed5931fb6f0fddd72fbe49f02b0
+TERMUX_PKG_SHA256=228a0fce3a382b98f3ae140620711c1d855f4dd80ec06f4d08a3c4e093ac3fa8
 TERMUX_PKG_DEPENDS="openssl, libuv, zlib"
 TERMUX_PKG_BREAKS="libwebsockets-dev"
 TERMUX_PKG_REPLACES="libwebsockets-dev"
