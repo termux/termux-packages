b1dd1822e6d425b62ea8248532509475af67e336
Henrik Grimler <henrik@grimler.se>
2020-10-17 20:48:43 +0200

    fastmod: update to 0.4.1

--- packages/fastmod/build.sh
+++ packages/fastmod/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/facebookincubator/fastmod
 TERMUX_PKG_DESCRIPTION="Regex-based code refactoring utility"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_VERSION=0.4.1
 TERMUX_PKG_SRCURL=https://github.com/facebookincubator/fastmod/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c36786acd75944cf50b77f6f61e22cf6e6833a0647b1f3bafbc7ff5c3e2c8153
+TERMUX_PKG_SHA256=0e0c2d50489c90cb8c8904772be2e9d31353fbed814d9088ee6013c27c2d111b
 TERMUX_PKG_BUILD_IN_SRC=true
