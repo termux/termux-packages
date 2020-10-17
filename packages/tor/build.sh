9915861e5cae70dcdb57013f71dae9354ecd1a62
Henrik Grimler <henrik@grimler.se>
2020-10-17 20:52:32 +0200

    tor: update to 0.4.4.5

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.torproject.org
 TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=0.4.3.6
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.4.4.5
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6a2d0637d4e514be2ec574723a05065245cce51da78a21cec1dc831be5ccac62
+TERMUX_PKG_SHA256=a45ca00afe765e3baa839767c9dd6ac9a46dd01720a3a8ff4d86558c12359926
 TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-zstd --disable-unittests"
 TERMUX_PKG_CONFFILES="etc/tor/torrc"
