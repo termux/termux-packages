ea17dbad8df1101dc2c79374771523addd260a2d
Henrik Grimler <henrik@grimler.se>
2020-10-17 22:19:09 +0200

    poppler: update to 20.10.0

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=20.09.0
+TERMUX_PKG_VERSION=20.10.0
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4ed6eb5ddc4c37f2435c9d78ff9c7c4036455aea3507d1ce8400070aab745363
+TERMUX_PKG_SHA256=434ecbbb539c1a75955030a1c9b24c7b58200b7f68d2e4269e29acf2f8f13336
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost"
 TERMUX_PKG_BREAKS="poppler-dev"
