a37e6dff83c6bf60eb6893743d5353590a121a11
Henrik Grimler <henrik@grimler.se>
2020-10-17 20:53:21 +0200

    croc: update to 8.5.1

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another."
 TERMUX_PKG_LICENSE=MIT
-TERMUX_PKG_VERSION=8.5.0
+TERMUX_PKG_VERSION=8.5.1
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=54cad5b72a8099d879c69d9d4bb5e190bd4f861b4ccab509afdae5ffbd23d141
+TERMUX_PKG_SHA256=b76e5523a8c621ddbaf6dacf3e70c6f614b8a35d494be43412082e9bf7323df2
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
