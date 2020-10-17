a8f29cf879222194419003a7ac5a0d866ab4afea
ruki <waruqi@gmail.com>
2020-10-18 07:57:39 +0800

    xmake: update v2.3.8

--- packages/xmake/build.sh
+++ packages/xmake/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://xmake.io/
 TERMUX_PKG_DESCRIPTION="A cross-platform build utility based on Lua"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Ruki Wang @waruqi"
-TERMUX_PKG_VERSION=2.3.7
+TERMUX_PKG_VERSION=2.3.8
 TERMUX_PKG_SRCURL=https://github.com/xmake-io/xmake/releases/download/v${TERMUX_PKG_VERSION}/xmake-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=95f1cbfc090b06b615efcf89195798eb1f6f0928e8ceeadaf9b03d9dce867ff8
+TERMUX_PKG_SHA256=21eb3428036a22d5230fcf765ad64b19941896e27118ddfe25aed248c3091331
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
