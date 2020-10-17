6edebec4f92d338fa98504e4c504b2a03432d8f9
Henrik Grimler <henrik@grimler.se>
2020-10-17 22:18:44 +0200

    glib: update to 2.66.1

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_VERSION=2.66.0
+TERMUX_PKG_VERSION=2.66.1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION:0:4}/glib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=c5a66bf143065648c135da4c943d2ac23cce15690fc91c358013b2889111156c
+TERMUX_PKG_SHA256=a269ffe69fbcc3a21ff1acb1b6146b2a5723499d6e2de33ae16ccb6d2438ef60
 TERMUX_PKG_DEPENDS="libffi, libiconv, pcre, libandroid-support, zlib"
 TERMUX_PKG_BREAKS="glib-dev"
 TERMUX_PKG_REPLACES="glib-dev"
