e5b39c3babbeba4017476f83bd726d02252bcd0d
Henrik Grimler <henrik@grimler.se>
2020-10-17 21:13:18 +0200

    vttest: update to 20200920

--- packages/vttest/build.sh
+++ packages/vttest/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://invisible-island.net/vttest/
 TERMUX_PKG_DESCRIPTION="Program for testing the VT100 compatibility of terminal emulators"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=20200610
+TERMUX_PKG_VERSION=20200920
 TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/vttest/vttest-$TERMUX_PKG_VERSION.tgz
-TERMUX_PKG_SHA256=8b52bc63b0e19feb15e3a044f0814a73e4f43306dd93362f9ad8bd94d2a40105
+TERMUX_PKG_SHA256=c9619d6bbe5804181dda18ec2901d51ce5551259565b9ccb13d5ef86b3bfb301
