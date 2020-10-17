912c5240f3d80d62003f8cca4cc0ab6e6e24f597
Henrik Grimler <henrik@grimler.se>
2020-10-17 18:59:36 +0200

    stunnel: update to 5.57

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.stunnel.org/
 TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=5.56
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.57
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7384bfb356b9a89ddfee70b5ca494d187605bb516b4fff597e167f97e2236b22
+TERMUX_PKG_SHA256=af5ab973dde11807c38735b87bdd87563a47d2fa1c72a07929fcfce80a600fe1
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/stunnel3 share/man/man8/stunnel.*.8"
