be861ff4e769f4f063d0a5c67a4d6a0adecc39b0
Henrik Grimler <henrik@grimler.se>
2020-10-17 22:57:36 +0200

    units: update to 2.20

--- packages/units/build.sh
+++ packages/units/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/units/
 TERMUX_PKG_DESCRIPTION="Converts between different systems of units"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.19
+TERMUX_PKG_VERSION=2.20
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/units/units-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4262136bdfc152b63ff5a9b93a7d80ce18b5e8bebdcffddc932dda769e306556
+TERMUX_PKG_SHA256=c5e298c87516711f3bedb9315583bad0965c5d3d0bb587f9837a9af12a50fadc
 TERMUX_PKG_DEPENDS="readline, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
