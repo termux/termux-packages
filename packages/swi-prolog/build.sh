7951c1445c261565bdb527cdc8d4b8f986d9fba7
Henrik Grimler <henrik@grimler.se>
2020-10-17 21:12:40 +0200

    swi-prolog: update to 8.3.9

--- packages/swi-prolog/build.sh
+++ packages/swi-prolog/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://swi-prolog.org/
 TERMUX_PKG_DESCRIPTION="Most popular and complete prolog implementation"
 TERMUX_PKG_LICENSE="ISC"
 # Use "development" versions.
-TERMUX_PKG_VERSION=8.3.5
+TERMUX_PKG_VERSION=8.3.9
 TERMUX_PKG_SRCURL=https://www.swi-prolog.org/download/devel/src/swipl-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fed1dff5b6e74eaee0235e6f6823aa6ef09a4f1e20677f38279cc1c959491943
+TERMUX_PKG_SHA256=f809756b0bc29708ea1f93a397b34a5ce3cf3d6f89e3ab1454b31e30efbbc506
 TERMUX_PKG_DEPENDS="libarchive, libcrypt, libgmp, libjpeg-turbo, libyaml, ncurses, ncurses-ui-libs, pcre, readline, ossp-uuid, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_HOSTBUILD=true
