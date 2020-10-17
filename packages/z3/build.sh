95bc92ed581ec9a90de0eef55f5b8d8d81f840ec
Henrik Grimler <henrik@grimler.se>
2020-10-18 00:47:12 +0200

    z3: give build with TERMUX_MAKE_PROCESSES=1 a shot
    Based on xeffyrs suggestion.
    Already available in repo so this is just to check if it fixes the build.

--- packages/z3/build.sh
+++ packages/z3/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_VERSION=4.8.9
 TERMUX_PKG_SRCURL=https://github.com/Z3Prover/z3/archive/z3-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=c9fd04b9b33be74fffaac3ec2bc2c320d1a4cc32e395203c55126b12a14ff3f4
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_MAKE_PROCESSES=1
 
 termux_step_configure() {
 	_PYTHON_VERSION=$(source $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
