ab74cdb12b4cbb2f26175da7a901d78dff39de17
Henrik Grimler <henrik@grimler.se>
2020-10-17 19:02:07 +0200

    micro: update to 2.0.8

--- packages/micro/build.sh
+++ packages/micro/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://micro-editor.github.io/
 TERMUX_PKG_DESCRIPTION="Modern and intuitive terminal-based text editor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_SRCURL=https://github.com/zyedidia/micro.git
-TERMUX_PKG_VERSION=2.0.7
+TERMUX_PKG_VERSION=2.0.8
 
 termux_step_make() {
 	return
