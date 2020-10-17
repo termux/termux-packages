a0c55edc5034ffd71b3e412dbbe0f895ca4b6741
Henrik Grimler <henrik@grimler.se>
2020-10-17 23:14:21 +0200

    golang: bump revision

--- packages/golang/build.sh
+++ packages/golang/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD 3-Clause"
 _MAJOR_VERSION=1.15.3
 # Use the ~ deb versioning construct in the future:
 TERMUX_PKG_VERSION=2:${_MAJOR_VERSION}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://storage.googleapis.com/golang/go${_MAJOR_VERSION}.src.tar.gz
 TERMUX_PKG_SHA256=896a602570e54c8cdfc2c1348abd4ffd1016758d0bd086ccd9787dbfc9b64888
 TERMUX_PKG_DEPENDS="clang"
