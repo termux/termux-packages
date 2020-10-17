b0e5a5082c64e3bf4d5e5fdb76339bbb75eb3c08
Henrik Grimler <henrik@grimler.se>
2020-10-17 22:59:35 +0200

    notmuch: update to 0.31

--- packages/notmuch/build.sh
+++ packages/notmuch/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://notmuchmail.org
 TERMUX_PKG_DESCRIPTION="Thread-based email index, search and tagging system"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.30
+TERMUX_PKG_VERSION=0.31
 TERMUX_PKG_SRCURL=https://notmuchmail.org/releases/notmuch-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=5e3baa6fe11d65c67e26ae488be11b320bae04e336acc9c64621f7e3449096fa
+TERMUX_PKG_SHA256=571fa0e1539c86612b1f2b2c80a398e08ecfef52e27ef7e48cf8e3b84fa18394
 TERMUX_PKG_DEPENDS="glib, libc++, libgmime, libtalloc, libxapian, zlib"
 TERMUX_PKG_BREAKS="notmuch-dev"
 TERMUX_PKG_REPLACES="notmuch-dev"
