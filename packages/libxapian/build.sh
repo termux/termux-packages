afe33d90f8637f7417fbab37cdcc61d8818902af
Henrik Grimler <henrik@grimler.se>
2020-10-17 19:03:19 +0200

    libxapian: update to 1.4.17

--- packages/libxapian/build.sh
+++ packages/libxapian/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://xapian.org
 TERMUX_PKG_DESCRIPTION="Xapian search engine library"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.4.16
+TERMUX_PKG_VERSION=1.4.17
 TERMUX_PKG_SRCURL=http://oligarchy.co.uk/xapian/${TERMUX_PKG_VERSION}/xapian-core-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4937f2f49ff27e39a42150e928c8b45877b0bf456510f0785f50159a5cb6bf70
+TERMUX_PKG_SHA256=b5eb8556dea1b0cad4167a66223522e66d670ec1eba16c7fdc844ed6b652572e
 # Note that we cannot /proc/sys/kernel/random/uuid (permission denied on
 # new android versions) so need libuuid.
 TERMUX_PKG_DEPENDS="libc++, libuuid, zlib"
