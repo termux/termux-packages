22ffb21a6c9809f471eb358762deb52ffc48121e
Henrik Grimler <henrik@grimler.se>
2020-10-17 19:05:54 +0200

    libsndfile: update to 1.0.30

--- packages/libsndfile/build.sh
+++ packages/libsndfile/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.mega-nerd.com/libsndfile
 TERMUX_PKG_DESCRIPTION="Library for reading/writing audio files"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.0.29
+TERMUX_PKG_VERSION=1.0.30
 TERMUX_PKG_SRCURL=https://github.com/erikd/libsndfile/releases/download/v${TERMUX_PKG_VERSION}/libsndfile-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=2ba20d44817c8176f097ab25eff44ef0aeec9e00973def5a7174c5ae0764b22f
+TERMUX_PKG_SHA256=9df273302c4fa160567f412e10cc4f76666b66281e7ba48370fb544e87e4611a
 TERMUX_PKG_DEPENDS="libflac, libvorbis"
 TERMUX_PKG_BREAKS="libsndfile-dev"
 TERMUX_PKG_REPLACES="libsndfile-dev"
