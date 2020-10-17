ec28ec286a5f7b45a0e9139bf492f6a4bc6a9ce0
Henrik Grimler <henrik@grimler.se>
2020-10-17 23:14:21 +0200

    vttest: bump revision

--- packages/vttest/build.sh
+++ packages/vttest/build.sh
@@ -2,5 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://invisible-island.net/vttest/
 TERMUX_PKG_DESCRIPTION="Program for testing the VT100 compatibility of terminal emulators"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=20200920
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/vttest/vttest-$TERMUX_PKG_VERSION.tgz
 TERMUX_PKG_SHA256=c9619d6bbe5804181dda18ec2901d51ce5551259565b9ccb13d5ef86b3bfb301
