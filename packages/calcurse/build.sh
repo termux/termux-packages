fd63b7d1cf03263ca2c34e1185b9d0bd69dd2941
Henrik Grimler <henrik@grimler.se>
2020-10-18 10:36:49 +0200

    calcurse: update to 4.7.0

--- packages/calcurse/build.sh
+++ packages/calcurse/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://calcurse.org/
 TERMUX_PKG_DESCRIPTION="calcurse is a calendar and scheduling application for the command line"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=4.6.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=4.7.0
 TERMUX_PKG_SRCURL=https://calcurse.org/files/calcurse-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=fa090307a157e24e790819b20c93e037b89c6132f473abaaa7b21c3be76df043
+TERMUX_PKG_SHA256=ef6675966a53f41196006ce624ece222fe400da0563f4fed1ae0272ad45c8435
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 
 termux_step_pre_configure() {
