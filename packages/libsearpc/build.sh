2db237094bcf7920027897930d023cd659391640
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-23 02:00:09 +0300

    libsearpc: use v3.2.0 instead of v3.2-latest
    v3.2-latest tag seems to point on latest commit and is being
    updated periodically.

--- packages/libsearpc/build.sh
+++ packages/libsearpc/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/haiwen/libsearpc
 TERMUX_PKG_DESCRIPTION="A simple C language RPC framework (mainly for seafile)"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=3.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/haiwen/libsearpc/archive/v${TERMUX_PKG_VERSION}-latest.tar.gz
-TERMUX_PKG_SHA256=08411c2581e4c3967f4590f25a357952cd7d71d83229171a3c1e1f321d457806
+TERMUX_PKG_VERSION=1:3.2.0
+TERMUX_PKG_SRCURL=https://github.com/haiwen/libsearpc/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=cd00197fcc40b45b1d5e892b2d08dfa5947f737e0d80f3ef26419334e75b0bff
 TERMUX_PKG_DEPENDS="glib, libjansson"
 TERMUX_PKG_BREAKS="libsearpc-dev"
 TERMUX_PKG_REPLACES="libsearpc-dev"
