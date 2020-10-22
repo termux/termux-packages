16d55adac2a7e583581b7a407edaf2259b19d8dd
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-22 22:52:42 +0300

    libprotobuf: package has been moved to protocolbuffers/protobuf + downgrade to 3.13.0
    Has to downgrade to 3.13.0 as host tools are not available for 3.13.0.1. Error:
    
     Downloading https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0.1/protoc-3.13.0.1-linux-x86_64.zip
       % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
       0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
     curl: (22) The requested URL returned error: 404 Not Found
     Failed to download https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0.1/protoc-3.13.0.1-linux-x86_64.zip

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -1,12 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/google/protobuf
+TERMUX_PKG_HOMEPAGE=https://github.com/protocolbuffers/protobuf
 TERMUX_PKG_DESCRIPTION="Protocol buffers C++ library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 # TERMUX_PKG_SRCDIR is overriden below so we need to specify license file
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
-TERMUX_PKG_VERSION=1:3.13.0.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/google/protobuf/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=7d663c8dc81d282dc92e884b38e9c179671e31ccacce311154420e65f7d142c6
+TERMUX_PKG_VERSION=2:3.13.0
+TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=9b4ee22c250fe31b16f1a24d61467e40780a3fbb9b91c3b65be2a376ed913a1a
 TERMUX_PKG_DEPENDS="libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev"
 TERMUX_PKG_REPLACES="libprotobuf-dev"
