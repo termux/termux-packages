48afb2c8241a4aecd595657423acb610f4756672
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-23 00:35:20 +0300

    atomicparsley: package is no longer available on bitbucket, using github link
    Versioning scheme has been changed too.

--- packages/atomicparsley/build.sh
+++ packages/atomicparsley/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://bitbucket.org/wez/atomicparsley
 TERMUX_PKG_DESCRIPTION="Read, parse and set metadata of MPEG-4 and 3gp files"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=0.9.6
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://bitbucket.org/wez/atomicparsley/get/${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=e28d46728be86219e6ce48695ea637d831ca0170ca6bdac99810996a8291ee50
+TERMUX_PKG_VERSION=1:20200701.154658.b0d6223
+TERMUX_PKG_SRCURL=https://github.com/wez/atomicparsley/archive/${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=52f11dc0cbd8964fcdaf019bfada2102f9ee716a1d480cd43ae5925b4361c834
 TERMUX_PKG_DEPENDS="libc++, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_lroundf=yes"
 
