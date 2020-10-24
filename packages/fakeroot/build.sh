643f8f8db47a94adeccb18d1b626404279b1f234
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-24 12:02:18 +0300

    fakeroot: update to 1.25.3

--- packages/fakeroot/build.sh
+++ packages/fakeroot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://packages.debian.org/fakeroot
 TERMUX_PKG_DESCRIPTION="Tool for simulating superuser privileges (with tcp ipc)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.25.1
+TERMUX_PKG_VERSION=1.25.3
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_${TERMUX_PKG_VERSION}.orig.tar.gz
-TERMUX_PKG_SHA256=95cd11cf057c1ae38905c1c6be29a1d1e402f15cde4707b5009cc9cb995256c5
+TERMUX_PKG_SHA256=8e903683357f7f5bcc31b879fd743391ad47691d4be33d24a76be3b6c21e956c
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ipc=tcp"
 TERMUX_PKG_BUILD_DEPENDS="libcap"
 
