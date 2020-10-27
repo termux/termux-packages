b0ab94f75eccc23239b648fef1ee49630185fcbd
Deshdeepak <rkdeshdeepak1@gmail.com>
2020-10-27 14:34:27 +0530

    Update hors to 0.7.2 (#5970)
    * fix arm

--- packages/hors/build.sh
+++ packages/hors/build.sh
@@ -2,12 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://github.com/WindSoilder/hors
 TERMUX_PKG_DESCRIPTION="Instant coding answers via the command line (howdoi in rust)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.7.1
+TERMUX_PKG_VERSION=0.7.2
 TERMUX_PKG_SRCURL=https://github.com/WindSoilder/hors/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7feceb88d2c27a8cf7fc3996c8dc464e8b6d1fe42412ce8b80e4047c7c790a16
+TERMUX_PKG_SHA256=f59db8b343b5829dd90e80e8554ad673db714dd8459c353b08a8afbac24ab6e2
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BLACKLISTED_ARCHES="arm"
 
 termux_step_pre_configure() {
+	if [ $TERMUX_ARCH = arm ]; then
+		CFLAGS+=" -fno-integrated-as"
+	fi
+
 	rm -f Makefile
 }
