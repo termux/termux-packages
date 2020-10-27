f6200596207c4a82453aab8690687d20cfcd72de
Henrik Grimler <henrik@grimler.se>
2020-10-27 13:07:29 +0100

    nodejs-lts: bump package
    To try to fix https://github.com/termux/termux-packages/issues/5972.

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=12.18.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=71158026579487422fd13cc2553b34cddb76519098aa6030faab52f88c6e0d0e
 # Note that we do not use a shared libuv to avoid an issue with the Android
