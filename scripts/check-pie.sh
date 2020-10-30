6ddf9b8f1df8544433335f806b9d8ac48f5dc280
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-30 21:33:32 +0200

    check-pie.sh: use $TERMUX_PREFIX

--- scripts/check-pie.sh
+++ scripts/check-pie.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # check-pie.sh - script to detect non-PIE binaries (which does not work on Android)
 
-cd /data/data/com.termux/files/usr/bin/
+. $(dirname "$(realpath "$0")")/properties.sh
+
+cd ${TERMUX_PREFIX}/bin
+
 for file in *; do
 	if readelf -h $file 2>/dev/null | grep -q 'Type:[[:space:]]*EXEC'; then
 		echo $file
