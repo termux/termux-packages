6fa120c617aa1ada6b9ac8add948af92ae7a7c90
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-30 22:24:07 +0200

    properties.sh: introduce $TERMUX_BASE_DIR

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -15,6 +15,7 @@ fi
 
 # Termux packages configuration.
 TERMUX_APP_PACKAGE="com.termux"
-TERMUX_PREFIX="/data/data/${TERMUX_APP_PACKAGE}/files/usr"
-TERMUX_ANDROID_HOME="/data/data/${TERMUX_APP_PACKAGE}/files/home"
+TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
 TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
+TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
+TERMUX_PREFIX="${TERMUX_BASE_DIR}/usr"
