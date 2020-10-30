119b9737375eb4750e5866573da428e5e5ca7ef7
Radomír Polách <rp@t4d.cz>
2020-10-30 15:17:38 +0100

    change: simplify build script

--- packages/proton-bridge/build.sh
+++ packages/proton-bridge/build.sh
@@ -2,19 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ProtonMail/proton-bridge
 TERMUX_PKG_DESCRIPTION="ProtonMail Bridge application"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=1.4.5
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
-
-termux_step_get_source() {
-	termux_setup_golang
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	mkdir -p $TERMUX_PKG_SRCDIR
-
-	git clone https://github.com/ProtonMail/proton-bridge.git $TERMUX_PKG_SRCDIR
-	cd $TERMUX_PKG_SRCDIR
-	git checkout "br-$TERMUX_PKG_VERSION"
-}
+TERMUX_PKG_SRCURL=https://github.com/ProtonMail/proton-bridge.git
+TERMUX_PKG_GIT_BRANCH=br-$TERMUX_PKG_VERSION 
+TERMUX_PKG_MAINTAINER="Radomír Polách rp@t4d.cz"
 
 termux_step_make_install() {
+	termux_setup_golang
 	export BUILDDIR=$TERMUX_PREFIX/bin
 	cd $TERMUX_PKG_SRCDIR
 	make build-nogui
