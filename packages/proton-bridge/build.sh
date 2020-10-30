f0e3e064b1a9c87686cf2546e957c7be33c8394c
Alexander Weps <mail@radomirpolach.cz>
2020-10-30 17:18:25 +0100

    new package: proton-bridge (#5979)

---
+++ packages/proton-bridge/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ProtonMail/proton-bridge
+TERMUX_PKG_DESCRIPTION="ProtonMail Bridge application"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=1.4.5
+TERMUX_PKG_SRCURL=https://github.com/ProtonMail/proton-bridge.git
+TERMUX_PKG_GIT_BRANCH=br-$TERMUX_PKG_VERSION 
+TERMUX_PKG_MAINTAINER="Radomír Polách <rp@t4d.cz>"
+
+termux_step_make_install() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	export BUILDDIR=$TERMUX_PREFIX/bin
+	cd $TERMUX_PKG_SRCDIR
+	make build-nogui
+	install -Dm700 Desktop-Bridge "$TERMUX_PREFIX"/bin/proton-bridge
+}
