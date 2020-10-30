463132f9a739f7d6f51197b759e596da14e8449a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-30 22:48:55 +0200

    misc hardcoded home/prefix fixes

--- disabled-packages/apt-updated/build.sh
+++ disabled-packages/apt-updated/build.sh
@@ -35,5 +35,5 @@ lib/libapt-inst.so
 termux_step_post_make_install() {
 	printf "# The main termux repository:\ndeb [arch=all,${TERMUX_ARCH}] https://termux.net stable main\n" > $TERMUX_PREFIX/etc/apt/sources.list
 	cp $TERMUX_PKG_BUILDER_DIR/trusted.gpg $TERMUX_PREFIX/etc/apt/
-	rm /data/data/com.termux/files/usr/include/apt-pkg -r
+	rm $TERMUX_PREFIX/include/apt-pkg -r
 }
