880022dea25503fc4584436b754b64c71a1e7dc4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-31 00:12:35 +0200

    termux-auth: update to 1.4

--- packages/termux-auth/build.sh
+++ packages/termux-auth/build.sh
@@ -2,9 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-auth
 TERMUX_PKG_DESCRIPTION="Password authentication library and utility for Termux"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.3
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-auth/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bf21c358fdbef44751b5c03b9b37bae609962a9fd55a95669597e209f291404d
+TERMUX_PKG_VERSION=1.4
+TERMUX_PKG_SRCURL=https://github.com/termux/termux-auth/archive/a7f557c40d0cf05e4c086ec04ae2544136c83b79.tar.gz
+TERMUX_PKG_SHA256=af9d06cbea60ede83da32315ad8d6046ea803de2a182a2a9e472a554204ab830
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BREAKS="termux-auth-dev"
 TERMUX_PKG_REPLACES="termux-auth-dev"
+
+termux_step_pre_configure() {
+	CPPFLAGS+=" -DTERMUX_HOME=\\\"${TERMUX_ANDROID_HOME}\\\" -DTERMUX_PREFIX=\\\"${TERMUX_PREFIX}\\\""
+}
