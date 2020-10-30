ed0b81c3ac47d8da0ade808a6976ac5ec44d57c2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-10-30 23:52:35 +0200

    termux-exec: update to 0.6

--- packages/termux-exec/build.sh
+++ packages/termux-exec/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-exec
 TERMUX_PKG_DESCRIPTION="An execve() wrapper to make /bin and /usr/bin shebangs work"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.5
+TERMUX_PKG_VERSION=0.6
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=de669112cab002197c175cea866f4a644dd4a51cb673b3a3146d5e8a81f8e115
+TERMUX_PKG_SHA256=5d70ec1f469405a047086ad281c8f574e620f8c0f461a2662c2dad9bf4dd5df6
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
