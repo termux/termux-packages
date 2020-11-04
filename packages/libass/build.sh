978b0334f4fdf6d03552cbac081843b58fe7c64c
Henrik Grimler <henrik@grimler.se>
2020-11-04 21:42:41 +0100

    libass: update to 0.15.0

--- packages/libass/build.sh
+++ packages/libass/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/libass/libass
 TERMUX_PKG_DESCRIPTION="A portable library for SSA/ASS subtitles rendering"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=0.14.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=0.15.0
 TERMUX_PKG_SRCURL=https://github.com/libass/libass/releases/download/$TERMUX_PKG_VERSION/libass-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=881f2382af48aead75b7a0e02e65d88c5ebd369fe46bc77d9270a94aa8fd38a2
+TERMUX_PKG_SHA256=9f09230c9a0aa68ef7aa6a9e2ab709ca957020f842e52c5b2e52b801a7d9e833
 TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz"
 TERMUX_PKG_BREAKS="libass-dev"
 TERMUX_PKG_REPLACES="libass-dev"
