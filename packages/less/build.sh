ab276148635eca571c00531b85a7f55670e58dba
Henrik Grimler <henrik@grimler.se>
2020-11-04 21:42:19 +0100

    less: update to 563

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://www.greenwoodsoftware.com/less/
 TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=551
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=563
 TERMUX_PKG_SRCURL=http://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d
+TERMUX_PKG_SHA256=ce5b6d2b9fc4442d7a07c93ab128d2dff2ce09a1d4f2d055b95cf28dd0dc9a9a
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_ESSENTIAL=true
