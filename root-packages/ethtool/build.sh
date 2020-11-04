b32fe47e015f4c9e3983243eb4954859ff4c1c91
Henrik Grimler <henrik@grimler.se>
2020-11-04 21:16:17 +0100

    ethtool: update to 5.9

--- root-packages/ethtool/build.sh
+++ root-packages/ethtool/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://mirrors.edge.kernel.org/pub/software/network/ethtool/
 TERMUX_PKG_DESCRIPTION="standard Linux utility for controlling network drivers and hardware, particularly for wired Ethernet devices"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=5.7
+TERMUX_PKG_VERSION=5.9
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/software/network/ethtool/ethtool-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=724eb8bd3c3a389fe285735959a1902fbd9310624656ad3220c5f23df1053c39
+TERMUX_PKG_SHA256=512932010af87bc3e09773c89baea44bca96c1da1151bbcf109fa0e055a88a6f
 TERMUX_PKG_DEPENDS="libmnl"
