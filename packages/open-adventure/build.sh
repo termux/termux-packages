def5490b2f6fadd735223c55070781820d8fbbbe
Henrik Grimler <henrik@grimler.se>
2020-11-04 22:09:52 +0100

    open-adventure: update to 1.9

--- packages/open-adventure/build.sh
+++ packages/open-adventure/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.catb.org/~esr/open-adventure/
-TERMUX_PKG_DESCRIPTION="Forward-port of the original Colossal Cave Adventure from 1976-77 which was the origin of all later text adventures, dungeon-crawl (computer) games, and computer-hosted roleplaying games"
+TERMUX_PKG_DESCRIPTION="Forward-port of the original Colossal Cave Adventure from 1976-77"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.8
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=5dd0df7a7caebf78e7450535109b70e834fff2e4b1252284d69ec7ce36583409
+TERMUX_PKG_VERSION=1.9
+TERMUX_PKG_SHA256=13a8bf9556014021b0e1f144f23e00afc05e92e4952b2ee7ad1dab0c079a6694
 TERMUX_PKG_SRCURL=https://gitlab.com/esr/open-adventure/-/archive/${TERMUX_PKG_VERSION}/open-adventure-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libedit"
 TERMUX_PKG_BUILD_IN_SRC=true
