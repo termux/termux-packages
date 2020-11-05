0f6cfed8f0c5e78f26ff5c1c2e29899bb938380d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-05 21:43:10 +0000

    findomain: blacklist arm builds currently

--- packages/findomain/build.sh
+++ packages/findomain/build.sh
@@ -5,3 +5,5 @@ TERMUX_PKG_VERSION=2.1.4
 TERMUX_PKG_SRCURL=https://github.com/Findomain/Findomain/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dd193f3b2d81b05769efbf2a2c7af7479395617204343c84fb5d7e1b0bec9d83
 TERMUX_PKG_BUILD_IN_SRC=true
+# Compilation error related to assembly code.
+TERMUX_PKG_BLACKLISTED_ARCHES="arm"
