dfea05d2099e8a048787b9ded46867aae3e38a90
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-05 23:36:36 +0000

    new package: html-xml-utils
    Closes https://github.com/termux/termux-packages/issues/4873.

---
+++ packages/html-xml-utils/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://www.w3.org/Tools/HTML-XML-utils/
+TERMUX_PKG_DESCRIPTION="A number of simple utilities for manipulating HTML and XML files"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_VERSION=7.9
+TERMUX_PKG_SRCURL=https://www.w3.org/Tools/HTML-XML-utils/html-xml-utils-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d86ac96ea660316bef814c17b2a96d54cdf91c69e59614459865c2bfdaee433f
+TERMUX_PKG_DEPENDS="libcurl, libidn2"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	automake
+}
