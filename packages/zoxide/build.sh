493c77e22535fc6f684f1394e0f92bad477037df
Ajeet D'Souza <98ajeet@gmail.com>
2020-11-05 15:17:33 +0530

    new package: zoxide (#5987)

---
+++ packages/zoxide/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ajeetdsouza/zoxide
+TERMUX_PKG_DESCRIPTION="A faster way to navigate your filesystem"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_SRCURL=https://github.com/ajeetdsouza/zoxide/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=62b7a2ced73d5ac0a183b3855d54d6619166b4d8d8c74299bb610265ccc4b193
+TERMUX_PKG_BUILD_IN_SRC=true
