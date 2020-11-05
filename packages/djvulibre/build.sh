4b84b696200ed3ae4b7cbbdfd2c0424c0fc497e4
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-05 20:31:45 +0000

    new package: djvulibre
    Closes https://github.com/termux/termux-packages/issues/5485.
    
    Note: I have replaced pthread_cancel() with pthread_exit(NULL) as Android
    doesn't implement it. Not sure whether package will work correctly. If
    there will be a bug, someone need to review source and replace pthread_cancel
    with atomic flags in correct places.

---
+++ packages/djvulibre/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://djvu.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="Suite to create, manipulate and view DjVu ('déjà vu') documents"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=3.5.27
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/djvu/DjVuLibre/${TERMUX_PKG_VERSION}/djvulibre-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e69668252565603875fb88500cde02bf93d12d48a3884e472696c896e81f505f
+TERMUX_PKG_DEPENDS="libtiff"
+#TERMUX_PKG_BUILD_IN_SRC=true
