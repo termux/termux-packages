8f31b53ca165c0af5697f1706b17dfbc2da20e17
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-05 18:39:03 +0200

    update and enable package 'sslscan'

---
+++ packages/sslscan/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/rbsec/sslscan
+TERMUX_PKG_DESCRIPTION="Utility to discover supported cipher suites on SSL/TLS enabled servers"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=2.0.5
+TERMUX_PKG_SRCURL=https://github.com/rbsec/sslscan/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=34a557a7996bb5c2f69fe512b7ef14ba272094178e76140535e50691bf934f99
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_BUILD_IN_SRC=true
