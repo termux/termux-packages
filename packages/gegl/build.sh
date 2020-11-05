88880232b62441b8e93ffee987754d63531acc57
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-05 12:51:08 +0200

    gegl: https homepage

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://gegl.org/
+TERMUX_PKG_HOMEPAGE=https://gegl.org/
 TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=0.4.26
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0f371e2ed2b92162fefd3dde743e648ca08a6a1b2b05004867fbddc7e211e424
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, zlib"
