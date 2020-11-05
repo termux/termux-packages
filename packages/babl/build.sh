d268e6202255f90d270fed5431607e228c17f2b8
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-05 12:50:30 +0200

    babl: https homepage

--- packages/babl/build.sh
+++ packages/babl/build.sh
@@ -1,7 +1,8 @@
-TERMUX_PKG_HOMEPAGE=http://gegl.org/babl/
+TERMUX_PKG_HOMEPAGE=https://gegl.org/babl/
 TERMUX_PKG_DESCRIPTION="Dynamic pixel format translation library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_VERSION=0.1.82
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/babl/0.1/babl-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=c62d93d4ad6774cb8e3231bbbc7f2e61e551e7242d78640d757505ee1a9fadc5
 TERMUX_PKG_DEPENDS="littlecms"
