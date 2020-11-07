90a00b24bcd67dfaa07acbb245b6b7e3a1299fa2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-07 04:22:02 +0200

    seafile-client: hard dependency on python 3.9.x
    We need to do same for other packages to ensure that nothing will be broken if user
    will attempt to upgrade python package without updating dependent ones.

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=http://seafile.com
 TERMUX_PKG_DESCRIPTION="Seafile is a file syncing and sharing software with file encryption and group sharing"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=7.0.9
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/haiwen/seafile/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f127b735e5d4cbd7e692d52eda54113f1a4ca31bd8328fbccf01a5462bbba5e8
-TERMUX_PKG_DEPENDS="ccnet, libcurl, python"
+TERMUX_PKG_DEPENDS="ccnet, libcurl, python (>= 3.9.0 | << 3.9.99)"
 TERMUX_PKG_BREAKS="seafile-client-dev"
 TERMUX_PKG_REPLACES="seafile-client-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
