65dedc3eeb8a4df7f1fbce90caee0b03aba1810f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-09 00:31:01 +0200

    kubectl: bump revision

--- packages/kubectl/build.sh
+++ packages/kubectl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Kubernetes.io client binary"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.19.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://dl.k8s.io/v$TERMUX_PKG_VERSION/kubernetes-src.tar.gz
 TERMUX_PKG_SHA256=4201b907ab7ff28554af2233615d271eca066b846b382cd3329e972d3ffefcf3
 
