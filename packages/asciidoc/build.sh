e30d51bf60eeb88d4251054f24608c2c136414f8
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 22:52:58 +0200

    asciidoc: update to 9.0.4

--- packages/asciidoc/build.sh
+++ packages/asciidoc/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://asciidoc.org
 TERMUX_PKG_DESCRIPTION="Text document format for short documents, articles, books and UNIX man pages."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=9.0.2
+TERMUX_PKG_VERSION=9.0.4
 TERMUX_PKG_SRCURL=https://github.com/asciidoc/asciidoc-py3/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ea73425151f56f278433e442f8b5085599765fa120574db65e6d053eb52927e2
+TERMUX_PKG_SHA256=9e269f336a71e8685d03a00c71b55ca029eed9f7baf1afe67c447be32206b176
 TERMUX_PKG_DEPENDS="docbook-xsl, libxml2-utils, python, xsltproc"
 TERMUX_PKG_SUGGESTS="w3m"
 TERMUX_PKG_BUILD_IN_SRC=true
