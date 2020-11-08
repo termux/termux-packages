e0eb39f13427c3f115157d031221a07ca5c64fdb
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 22:46:49 +0200

    seafile-client: update to 7.0.10

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://seafile.com
 TERMUX_PKG_DESCRIPTION="Seafile is a file syncing and sharing software with file encryption and group sharing"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=7.0.9
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=7.0.10
 TERMUX_PKG_SRCURL=https://github.com/haiwen/seafile/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f127b735e5d4cbd7e692d52eda54113f1a4ca31bd8328fbccf01a5462bbba5e8
+TERMUX_PKG_SHA256=9ac0a1c25e6f2bf874b5b080a165319f29cb3d03f044da8db8aed8412c8b4f45
 TERMUX_PKG_DEPENDS="ccnet, libcurl, python"
 TERMUX_PKG_BREAKS="seafile-client-dev"
 TERMUX_PKG_REPLACES="seafile-client-dev"
@@ -20,11 +19,4 @@ termux_step_post_configure() {
 	#the package has trouble to prepare some headers
 	cd $TERMUX_PKG_SRCDIR/lib
 	python $TERMUX_PREFIX/bin/searpc-codegen.py $TERMUX_PKG_SRCDIR/lib/rpc_table.py
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	pip install future
-	EOF
 }
