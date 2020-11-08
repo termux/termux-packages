3a6d32c0654c150f004a2b0af621339025bd95a0
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 23:10:04 +0200

    lazygit: update to 0.23.7

--- packages/lazygit/build.sh
+++ packages/lazygit/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/jesseduffield/lazygit
 TERMUX_PKG_DESCRIPTION="Simple terminal UI for git commands"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.23.6
+TERMUX_PKG_VERSION=0.23.7
 TERMUX_PKG_SRCURL=https://github.com/jesseduffield/lazygit/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b2383a70e54a67382b0f052149891d3d0f3e1eba952a0fa5009214c6591dfc6b
+TERMUX_PKG_SHA256=0a5284d4ed513cd3094e1e4871da8b99c7b1d8b04a8aaaf4dc77bb932f630f1c
 TERMUX_PKG_RECOMMENDS=git
 
 termux_step_make() {
