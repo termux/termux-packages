923afe4de7abf86338935aa0787d32c01d20464e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-09 00:31:27 +0200

    libnftnl: bump revision

--- packages/libnftnl/build.sh
+++ packages/libnftnl/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.netfilter.org/projects/libnftnl/
 TERMUX_PKG_DESCRIPTION="Netfilter library providing interface to the nf_tables subsystem"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.1.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://netfilter.org/projects/libnftnl/files/libnftnl-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=04a3fa5b08b736268f7e65836b9f05d9d5f438181467bee3c76c3c4a4f3ab711
 TERMUX_PKG_DEPENDS="libmnl"
