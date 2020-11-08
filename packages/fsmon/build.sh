265ee0cba137baace9220e28c08bce0d70f9d92c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 23:00:03 +0200

    fsmon: update to 1.8.2

--- packages/fsmon/build.sh
+++ packages/fsmon/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/nowsecure/fsmon
 TERMUX_PKG_DESCRIPTION="Filesystem monitor with fanotify and inotify backends"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=1.8.1
+TERMUX_PKG_VERSION=1.8.2
 TERMUX_PKG_SRCURL=https://github.com/nowsecure/fsmon/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=124b5407b1818ae44bef8ef9b887d56b672a1f468b32b7072d6834a561a1cc19
+TERMUX_PKG_SHA256=297ee746f035a6d06511ec1e6c44d5193d41c4edd6931903731ed8e7caef95ed
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
