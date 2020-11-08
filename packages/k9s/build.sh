93928f84e2e9b3c749110fd16b68dcc6f34c569d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-09 00:30:49 +0200

    k9s: bump revision

--- packages/k9s/build.sh
+++ packages/k9s/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Kubernetes CLI To Manage Your Clusters In Style!"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=0.23.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9608b16b60ec6b39b7a3de1ab5bb75fca00d22844bb9c81107eace65302c7038
 
