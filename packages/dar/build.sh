42e959938628236489a8b57f1e76cc0d7a1ae6ec
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 22:57:29 +0200

    dar: update to 2.6.13

--- packages/dar/build.sh
+++ packages/dar/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://dar.linux.free.fr/
 TERMUX_PKG_DESCRIPTION="A full featured command-line backup tool, short for Disk ARchive"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.6.12
+TERMUX_PKG_VERSION=2.6.13
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/dar/dar/${TERMUX_PKG_VERSION}/dar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d3ef3ad2fc9b5199333fc462e68b49498eb6e2c3e5472e6f9b7aa42b6a548f4b
+TERMUX_PKG_SHA256=3fea9ff9e55fb9827e17a080de7d1a2605b82c2320c0dec969071efefdbfd097
 TERMUX_PKG_DEPENDS="attr, libbz2, libgcrypt, liblzma, liblzo, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
