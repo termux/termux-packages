eea0f5870cbc2648efba09022e0771985d2b364c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 22:21:21 +0200

    proot-distro: needs a latest version of proot

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.0
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dd2e1be1496eb20c7eb4de88b3f2e74dfcfc722b736d8eff87187581931a453b
-TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot, sed, tar, xz-utils"
+TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
