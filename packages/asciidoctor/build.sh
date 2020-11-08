1f476b0b990b89c3d15f740b6c0559cc8c7b372b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-08 22:56:36 +0200

    asciidoctor: update to 2.0.11

--- packages/asciidoctor/build.sh
+++ packages/asciidoctor/build.sh
@@ -1,8 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://asciidoctor.org/
 TERMUX_PKG_DESCRIPTION="An implementation of AsciiDoc in Ruby"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=2.0.10
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.0.11
 TERMUX_PKG_DEPENDS="ruby"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -19,12 +18,15 @@ termux_step_make_install() {
 	gem install --ignore-dependencies --no-user-install --verbose \
 		-i "$gemdir" -n "$TERMUX_PREFIX/bin" asciidoctor -v "$TERMUX_PKG_VERSION"
 
-	mkdir -p $TERMUX_PREFIX/share/doc/asciidoctor
-	cp $gemdir/gems/asciidoctor-2.0.10/LICENSE $TERMUX_PREFIX/share/doc/asciidoctor/
-
 	sed -i -E "1 s@^(#\!)(.*)@\1${TERMUX_PREFIX}/bin/ruby@" \
 		"$TERMUX_PREFIX/bin/asciidoctor"
 
 	install -Dm600 "$gemdir/gems/asciidoctor-${TERMUX_PKG_VERSION}/man/asciidoctor.1" \
 		"$TERMUX_PREFIX/share/man/main1/asciidoctor.1"
+}
+
+termux_step_install_license() {
+	local gemdir="$TERMUX_PREFIX/lib/ruby/gems/${_RUBY_VERSION}.0"
+	mkdir -p $TERMUX_PREFIX/share/doc/asciidoctor
+	cp $gemdir/gems/asciidoctor-${TERMUX_PKG_VERSION}/LICENSE $TERMUX_PREFIX/share/doc/asciidoctor/
 }
