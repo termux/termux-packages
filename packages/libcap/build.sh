b82faa4a6f131f589de2cb5b740af455526cf611
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-09 00:37:58 +0200

    libcap: build without pthreads

--- packages/libcap/build.sh
+++ packages/libcap/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://sites.google.com/site/fullycapable/
 TERMUX_PKG_DESCRIPTION="POSIX 1003.1e capabilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.45
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d66639f765c0e10557666b00f519caf0bd07a95f867dddaee131cd284fac3286
 TERMUX_PKG_DEPENDS="attr"
@@ -10,9 +11,9 @@ TERMUX_PKG_REPLACES="libcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" PREFIX="$TERMUX_PREFIX"
+	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" PREFIX="$TERMUX_PREFIX" PTHREADS=no
 }
 
 termux_step_make_install() {
-	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" prefix="$TERMUX_PREFIX" RAISE_SETFCAP=no lib=/lib install
+	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" prefix="$TERMUX_PREFIX" RAISE_SETFCAP=no lib=/lib PTHREADS=no install
 }
