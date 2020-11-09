9ef38b8d5a2e7d748ee5dc68754c0b4ef26f0132
Ludea <ludealol.olol@gmail.com>
2020-11-09 13:37:22 +0100

    Fix path

--- packages/helm/build.sh
+++ packages/helm/build.sh
@@ -18,6 +18,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-        install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/helm/helm/helm \
+        install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/helm/helm/bin/helm \
                  $TERMUX_PREFIX/bin/helm
 }
