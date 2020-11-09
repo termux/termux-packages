9ca57c7ea1a3a70a7e56b7fcc290e70e5a0156d7
Ludea <ludealol.olol@gmail.com>
2020-11-09 10:23:36 +0100

    Wrong URL download

--- packages/helm/build.sh
+++ packages/helm/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="https://github.com/helm/helm/blob/master/OWNERS" 
 TERMUX_PKG_VERSION=3.4.0
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://get.helm.sh/helm-v${TERMUX_PKG_VERSION}-linux-arm64.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/helm/helm/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=83cd7a30f4c5ce83eb2afb4974777bf99bb74a5f587c85774d747e3d32e3cb48
 
 termux_step_make() {
