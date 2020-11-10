f1d62c9ac474d6d1b9c6e78a04e7652ba6570b8f
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-10 21:04:36 +0200

    grep: update to 3.6

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/grep/
 TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines containing a match to a specified pattern"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=3.5
+TERMUX_PKG_VERSION=3.6
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/grep/grep-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b82ac77707c2ab945520c8404c9fa9f890f7791a62cf2103cf6238acad87a44a
+TERMUX_PKG_SHA256=667e15e8afe189e93f9f21a7cd3a7b3f776202f417330b248c2ad4f997d9373e
 TERMUX_PKG_DEPENDS="libandroid-support, pcre"
 TERMUX_PKG_ESSENTIAL=true
 
