3664d29563d38a1a35e35ad3d94d20c3fead2580
Alexander Weps <mail@radomirpolach.cz>
2020-11-13 18:32:16 +0100

    proton-bridge: update to 1.5.0 (#6022)

--- packages/proton-bridge/build.sh
+++ packages/proton-bridge/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ProtonMail/proton-bridge
 TERMUX_PKG_DESCRIPTION="ProtonMail Bridge application"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.4.5
+TERMUX_PKG_VERSION=1.5.0
 TERMUX_PKG_SRCURL=https://github.com/ProtonMail/proton-bridge.git
 TERMUX_PKG_GIT_BRANCH=br-$TERMUX_PKG_VERSION 
 TERMUX_PKG_MAINTAINER="Radomír Polách <rp@t4d.cz>"
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_make_install() {
 	termux_setup_golang
