4e5254b686b51822d8c24adf2fadd18b9ee38f12
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-13 15:08:35 +0200

    scripts/properties.sh: load ~/.termuxrc at the end to allow override the configuration

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -3,8 +3,6 @@ TERMUX_NDK_VERSION_NUM=21
 TERMUX_NDK_REVISION="d"
 TERMUX_NDK_VERSION=$TERMUX_NDK_VERSION_NUM$TERMUX_NDK_REVISION
 
-test -f "$HOME/.termuxrc" && . "$HOME/.termuxrc"
-
 if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
 	export ANDROID_HOME=${TERMUX_SCRIPTDIR}/build-tools/android-sdk
 	export NDK=${TERMUX_SCRIPTDIR}/build-tools/android-ndk
@@ -19,3 +17,6 @@ TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
 TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
 TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
 TERMUX_PREFIX="${TERMUX_BASE_DIR}/usr"
+
+# Allow to override setup.
+test -f "$HOME/.termuxrc" && . "$HOME/.termuxrc"
