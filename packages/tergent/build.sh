4c7746055452b670e5ab061101500a6b8f63b634
lennonhill <31923882+lennonhill@users.noreply.github.com>
2020-11-14 19:21:34 +0000

    Update tergent to 1.0.0

--- packages/tergent/build.sh
+++ packages/tergent/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/aeolwyr/tergent
-TERMUX_PKG_DESCRIPTION="An ssh-agent implementation for Termux that uses Android Keystore as its backend"
-TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.1.1
+TERMUX_PKG_DESCRIPTION="A PKCS#11 (cryptoki) library that uses Android Keystore as its backend"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/aeolwyr/tergent/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b16e5ba1a9333d6e14b94d8cf3f5ad27d3af124f6f2bbebb0eca62ac95783281
+TERMUX_PKG_SHA256=0b59cf0ced3f693fb19396a986326963f3763e6bf65d3e56af0a03d206d69428
 TERMUX_PKG_DEPENDS="termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
