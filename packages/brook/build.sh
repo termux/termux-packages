91b448d092e21bdd3a58ad87d842f3db601a0901
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-11-18 05:08:03 +0530

    new package: brook (#6034)
    Closes https://github.com/termux/termux-packages/issues/4110.

---
+++ packages/brook/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/txthinking/brook
+TERMUX_PKG_DESCRIPTION="A cross-platform strong encryption and not detectable proxy. Zero-Configuration."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION="20200909"
+TERMUX_PKG_SRCURL=https://github.com/txthinking/brook/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=410c1a64921f79b1a5db384e981cc54b8188b58e8ddcabc466d6828316c6bfeb
+
+termux_step_make() {
+	termux_setup_golang
+
+	cd "$TERMUX_PKG_SRCDIR"
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/txthinking
+	mkdir -p "$TERMUX_PREFIX"/share/doc/brook
+	cp -a "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/txthinking/brook
+	cd "$GOPATH"/src/github.com/txthinking/brook/cli/brook
+	go get -d -v
+	go build -o brook 
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/txthinking/brook/cli/brook/brook
+	cp -r "$TERMUX_PKG_SRCDIR"/docs/* "$TERMUX_PREFIX"/share/doc/brook
+}
