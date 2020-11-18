6310f8689b07efce82749da3c3718c76b6eb2c0d
Krishna Kanhaiya <kcubeterm@gmail.com>
2020-11-19 00:14:49 +0530

    new package: whatprovides (#6047)

---
+++ packages/whatprovides/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/termux/whatprovides
+TERMUX_PKG_DESCRIPTION="Whatprovides utility for termux"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=0.1.3
+TERMUX_PKG_SRCURL=https://github.com/termux/whatprovides/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f0aec6ddbb1451f5d022ae591d21df59d3855df4b7e8aea071fd22e219dce913
+TERMUX_PKG_BUILD_IN_SRC=true
