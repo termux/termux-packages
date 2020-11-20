087084bea72d1f38c0353be7d87d12d31ebb5415
Ubaldo Porcheddu <ubaldo@eja.it>
2020-11-20 15:53:17 +0000

    Update build.sh
    remove header, remove make extra args, update homepage.

--- packages/eja/build.sh
+++ packages/eja/build.sh
@@ -1,12 +1,7 @@
-# Skeleton build.sh script for new package.
-# For reference about available fields, check the Termux Developer's Wiki page:
-# https://github.com/termux/termux-packages/wiki/Creating-new-package
-
-TERMUX_PKG_HOMEPAGE=http://eja.it
+TERMUX_PKG_HOMEPAGE=https://github.com/eja/eja
 TERMUX_PKG_DESCRIPTION="eja micro web server"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=13.11.20
 TERMUX_PKG_SRCURL=https://github.com/eja/eja/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d19a8071d524373bcfceb5dec834546e2c66108593f0bf54ef984d8a0819e050
-TERMUX_PKG_EXTRA_MAKE_ARGS="DESTDIR=$TERMUX_PREFIX prefix=/"
 TERMUX_PKG_BUILD_IN_SRC=true
