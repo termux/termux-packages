87bf7df117bb86ab5c98891d8395741137fcba18
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-21 23:34:26 +0000

    xfwm4: update to 4.15.3

--- x11-packages/xfwm4/build.sh
+++ x11-packages/xfwm4/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Window manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=4.15.1
+TERMUX_PKG_VERSION=4.15.3
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/xfwm4/${TERMUX_PKG_VERSION:0:4}/xfwm4-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=8494d1c79c8254c2e222cb8247dce928e5bb7254bfd53202f6a437f3fbaecac4
+TERMUX_PKG_SHA256=7a0121557812bce60be76af8050ec07e6d1fd06b3b120412bf21a5f3bf80c3a1
 TERMUX_PKG_DEPENDS="hicolor-icon-theme, libepoxy, libwnck, libxfce4ui, startup-notification, xfconf"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
