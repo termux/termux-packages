acdae19514e740b1fa5087147e12d3ecb5ff0b96
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-21 22:55:43 +0000

    libvte: update to 0.62.1

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/vte/
 TERMUX_PKG_DESCRIPTION="Virtual Terminal library"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.62.0
+TERMUX_PKG_VERSION=0.62.1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/vte/${TERMUX_PKG_VERSION:0:4}/vte-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4d2d246ed47f08cc73f6471aa8e9f378998b7d0f7dcb0c433f29da5839dbb016
+TERMUX_PKG_SHA256=c369e87c0c8284e09109d0a9aac821f543558f51c0cb9c7acfff3df64153308d
 TERMUX_PKG_DEPENDS="fribidi, gtk3, libgnutls, libicu, pcre2"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgir=false -Dvapi=false"
