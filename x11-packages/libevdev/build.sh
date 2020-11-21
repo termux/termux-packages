fb336523d500af8e811f1868a86b9dcfafaf1652
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-21 22:55:33 +0000

    libevdev: update to 1.10.0

--- x11-packages/libevdev/build.sh
+++ x11-packages/libevdev/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/libevdev/
 TERMUX_PKG_DESCRIPTION="Wrapper library for evdev devices"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.9.1
+TERMUX_PKG_VERSION=1.10.0
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/libevdev/libevdev-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=f5603c48c5afd76b14df7a5124e0a94a102f8da0d45826192325069d1bbc7acb
+TERMUX_PKG_SHA256=3522c26e2c148be0ad68ce26fbced408a4185dea90bfe8079dc82b8ace962d4a
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-test-run"
 
 termux_step_pre_configure() {
