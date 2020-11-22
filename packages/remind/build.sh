669cfc35b25f9b478ac1a09d21759cd31ccc3cbd
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 17:11:24 +0000

    remind: update to 03.03.03

--- packages/remind/build.sh
+++ packages/remind/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://dianne.skoll.ca/projects/remind/
 TERMUX_PKG_DESCRIPTION="Sophisticated calendar and alarm program"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1:03.03.01
+TERMUX_PKG_VERSION=1:03.03.03
 TERMUX_PKG_SRCURL=https://dianne.skoll.ca/projects/remind/download/remind-${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=d1a164d1c2d1e963d5f1f251457a8065cae12f36e3914cac1e54275180499478
+TERMUX_PKG_SHA256=10f946fd5b5cd83e4ba435ca493d3ebff235a5808aef1f871010942e95ea8e02
 TERMUX_PKG_DEPENDS="libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="bin/tkremind share/man/man1/tkremind.1 bin/cm2rem.tcl share/man/man1/cm2rem.1"
