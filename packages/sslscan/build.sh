9785947fe3ca53fe7fef768a85c428f347ec2111
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 20:31:56 +0200

    sslscan: bump revision

--- packages/sslscan/build.sh
+++ packages/sslscan/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/rbsec/sslscan
 TERMUX_PKG_DESCRIPTION="Utility to discover supported cipher suites on SSL/TLS enabled servers"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=2.0.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rbsec/sslscan/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9a2263246ffde5f950d49a51e46ddafea0ac95573e0a5a3a5f9194725fe561a4
 TERMUX_PKG_DEPENDS="openssl"
