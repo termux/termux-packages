42c68a06aa8f03e5bd6584118a8c0c1c4e0d0e54
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 15:08:49 +0000

    mupdf: depend on gumbo-parser

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.xz
 TERMUX_PKG_SHA256=592d4f6c0fba41bb954eb1a41616661b62b134d5b383e33bd45a081af5d4a59a
-TERMUX_PKG_DEPENDS="freetype, harfbuzz, jbig2dec, libjpeg-turbo, openjpeg, zlib"
+TERMUX_PKG_DEPENDS="freetype, gumbo-parser, harfbuzz, jbig2dec, libjpeg-turbo, openjpeg, zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX build=release libs"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_DEVELSPLIT=true
