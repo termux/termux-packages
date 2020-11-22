27a4397307efe4515e27a42b4a56eb202cd9096e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 15:32:55 +0000

    pngquant: update to 2.13.0

--- packages/pngquant/build.sh
+++ packages/pngquant/build.sh
@@ -1,14 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://pngquant.org
 TERMUX_PKG_DESCRIPTION="PNG image optimising utility"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.12.6
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.13.0
 # If both archives are .tar.gz then they overwrite eachother since they are the same version and hence the same name.
 # Work around this by using .zip for one of them...
 TERMUX_PKG_SRCURL=(https://github.com/pornel/pngquant/archive/$TERMUX_PKG_VERSION.tar.gz
 		   https://github.com/ImageOptim/libimagequant/archive/$TERMUX_PKG_VERSION.zip)
-TERMUX_PKG_SHA256=(f1c9608668100fa2dd07b3158679b75c33a6854b044b21442d6762d52ed10718
-                   80bf900e8ee8e2e3b6da974c873a63abb8e0847f35d84ba12da2e5c604dc5168)
+TERMUX_PKG_SHA256=(aebf96914dcce978f23da5ce3542f4ef30e635e24e386f2e5673a3d7215c646a
+		    2e10c58d020732ab1bcb5ed568f7dae4d037973fac79f889dc95b4633db3630e)
 TERMUX_PKG_DEPENDS="libpng, littlecms, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sse"
