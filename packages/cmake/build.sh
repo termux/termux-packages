c17c417c435b91c7891f0fb2083db8c27cdf4542
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 13:02:46 +0000

    cmake: bump revision

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package soft
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 # When updating version here, please update termux_setup_cmake.sh as well.
 TERMUX_PKG_VERSION=3.19.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fdda688155aa7e72b7c63ef6f559fca4b6c07382ea6dca0beb5f45aececaf493
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, make, clang, zlib"
