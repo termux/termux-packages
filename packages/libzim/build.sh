bd395fca8cd2bc0c59de0e8b640b790d3818d0bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 20:31:00 +0200

    libzim: bump revision

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://openzim.org
 TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the ZIM file format."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=6.3.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=984d2f3881c507c9f0212dc2bc1b808d25cdeef6a62043f9211fcb78a231a986
 TERMUX_PKG_DEPENDS="zstd, libuuid, zlib, libicu, liblzma, libxapian, googletest"
