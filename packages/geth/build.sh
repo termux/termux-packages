77fb42aba70d6f7d23e6e4efb67c1f86bc150563
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 00:52:44 +0000

    geth: update to 1.9.24

--- packages/geth/build.sh
+++ packages/geth/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://geth.ethereum.org/
 TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.9.23
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.9.24
 TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=3b3484b26889c5f54f209ff0a0896c5409159ea8e8ccd95ad6f933e6511ebcf6
+TERMUX_PKG_SHA256=5e5d69dafc0c93af47fdc5fae782048b15a3a16ac722db39c9cda2eacdf21234
 
 termux_step_make() {
 	termux_setup_golang
