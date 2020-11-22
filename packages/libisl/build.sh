e2d7627836eb6f572274b9eecdfd825c92abcf90
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 01:28:22 +0000

    libisl: update to 0.23

--- packages/libisl/build.sh
+++ packages/libisl/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://isl.gforge.inria.fr/
 TERMUX_PKG_DESCRIPTION="Library for manipulating sets and relations of integer points bounded by linear constraints"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.22.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.23
 TERMUX_PKG_SRCURL=http://isl.gforge.inria.fr/isl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=28658ce0f0bdb95b51fd2eb15df24211c53284f6ca2ac5e897acc3169e55b60f
+TERMUX_PKG_SHA256=5efc53efaef151301f4e7dde3856b66812d8153dede24fab17673f801c8698f2
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_BREAKS="libisl-dev"
 TERMUX_PKG_REPLACES="libisl-dev"
