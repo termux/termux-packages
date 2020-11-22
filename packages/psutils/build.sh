24568954479975436462ff39ba81217f9444629a
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 15:28:11 +0000

    psutils: update to 2.04

--- packages/psutils/build.sh
+++ packages/psutils/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/rrthomas/psutils
 TERMUX_PKG_DESCRIPTION="A set of postscript utilities"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.03
+TERMUX_PKG_VERSION=2.04
 TERMUX_PKG_SRCURL=https://github.com/rrthomas/psutils/releases/download/v$TERMUX_PKG_VERSION/psutils-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a24d1dab2aba74743ac5870f60f27d3d04d30a1075c92db5500df2cc0a6b0ba5
+TERMUX_PKG_SHA256=6e25e3a04d769cf591f8f3b346626f6a7d0fdf9c4ae2ac9bedd5df8eef8210ad
 TERMUX_PKG_DEPENDS="ghostscript, perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
