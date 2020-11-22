a9c12495012525715592a52ba1038e1a452cd36b
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 16:30:59 +0000

    libzim: update to 6.3.0

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://openzim.org
 TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the ZIM file format."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=6.2.2
+TERMUX_PKG_VERSION=6.3.0
 TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6619035d35c9ba057c80be5758fa86922802c74aca40e5fffd40b77f0f263af2
+TERMUX_PKG_SHA256=984d2f3881c507c9f0212dc2bc1b808d25cdeef6a62043f9211fcb78a231a986
 TERMUX_PKG_DEPENDS="zstd, libuuid, zlib, libicu, liblzma, libxapian, googletest"
 
