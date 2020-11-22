bfd362df210ada3b17cde3e67ad4d46fe28ff704
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 14:03:22 +0000

    mtools: update to 4.0.25

--- packages/mtools/build.sh
+++ packages/mtools/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/mtools/
 TERMUX_PKG_DESCRIPTION="Tool for manipulating FAT images."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=4.0.24
+TERMUX_PKG_VERSION=4.0.25
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/mtools/mtools-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=24f4a2da9219f98498eb1b340cd96db7ef9b684c067d1bdeb6e85efdd13b2fb9
+TERMUX_PKG_SHA256=fd161eec3bb7a93d13936db67725ad3e17f2d5f4e6fa8f7667fbc7ac728e2c15
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
