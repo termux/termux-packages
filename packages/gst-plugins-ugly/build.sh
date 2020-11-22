ec60aa89aca7bb0c6b8985de51647012a8df0343
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 01:34:32 +0000

    gst-plugins-ugly: update to 1.18.1

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Ugly Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.18.0
+TERMUX_PKG_VERSION=1.18.1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=686644e45e08258ae240c4519376668ad8d34ea6d0f6ab556473c317bfb7e082
+TERMUX_PKG_SHA256=18cd6cb829eb9611ca63cbcbf46aca0f0de1dd28b2df18caa2834326a75ff725
 TERMUX_PKG_DEPENDS="gst-plugins-base, libx264"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=disabled
