e5b39116c10e12c90588cf457fdf274afa05a032
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 01:27:50 +0000

    gpgme: update to 1.15.0

--- packages/gpgme/build.sh
+++ packages/gpgme/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/gpgme/
 TERMUX_PKG_DESCRIPTION="Library designed to make access to GnuPG easier"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.14.0
+TERMUX_PKG_VERSION=1.15.0
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=cef1f710a6b0d28f5b44242713ad373702d1466dcbe512eb4e754d7f35cd4307
+TERMUX_PKG_SHA256=0b472bc12c7d455906c8a539ec56da0a6480ef1c3a87aa5b74d7125df68d0e5b
 TERMUX_PKG_DEPENDS="gnupg (>= 2.2.9-1), libassuan, libgpg-error"
 TERMUX_PKG_BREAKS="gpgme-dev"
 TERMUX_PKG_REPLACES="gpgme-dev"
