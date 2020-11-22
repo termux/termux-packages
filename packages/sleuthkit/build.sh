25995b6d7eb367ecee5ac38ac90960d3ca960cdb
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 20:31:31 +0200

    sleuthkit: bump revision

--- packages/sleuthkit/build.sh
+++ packages/sleuthkit/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library for digital forensic
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="licenses/mit.txt, licenses/GNU-COPYING, licenses/bsd.txt, licenses/cpl1.0.txt, licenses/IBM-LICENSE"
 TERMUX_PKG_VERSION=4.10.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/sleuthkit/sleuthkit/archive/sleuthkit-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=18e587f5f1869f9d87edc4e77e7d5ddcacfd0966d15e5fb45ce520b0fe9ff03e
 TERMUX_PKG_DEPENDS="libsqlite, ncurses"
