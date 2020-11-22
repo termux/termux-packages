21acf117f4862cae0c965669923983650f5019bd
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 14:02:44 +0000

    mariadb: update to 10.5.8

--- packages/mariadb/build.sh
+++ packages/mariadb/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://mariadb.org
 TERMUX_PKG_DESCRIPTION="A drop-in replacement for mysql server"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2:10.5.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2:10.5.8
 TERMUX_PKG_SRCURL=http://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-${TERMUX_PKG_VERSION:2}/source/mariadb-${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=cf6b2c061754c07bbb1d20b29ae111bf2d098297c9b8c321b810638a179088a4
+TERMUX_PKG_SHA256=eb4824f6f2c532cd3fc6a6bce7bf78ea7c6b949f8bdd07656b2c84344e757be8
 TERMUX_PKG_DEPENDS="libc++, libiconv, liblzma, ncurses, libedit, openssl, pcre2, libcrypt, libandroid-support, libandroid-glob, zlib, liblz4"
 TERMUX_PKG_BREAKS="mariadb-dev"
 TERMUX_PKG_REPLACES="mariadb-dev"
