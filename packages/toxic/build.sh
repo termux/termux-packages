2d96706ac85c457a2c347d9e2da89e1e4eba582d
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 22:22:15 +0200

    toxic: update to 0.9.0

--- packages/toxic/build.sh
+++ packages/toxic/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/JFreegman/toxic
 TERMUX_PKG_DESCRIPTION="A command line client for Tox"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.8.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.9.0
 TERMUX_PKG_SRCURL=https://github.com/JFreegman/toxic/archive/v${TERMUX_PKG_VERSION}/toxic-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=97f26ba2c257c10439fd2ff280ca90c37ed225d86f46740a08f02ff2e4459e0e
+TERMUX_PKG_SHA256=723b43a2bc84c94a8a432a6daab95a989466a6dce80ca917a89f47b616d81e4f
 TERMUX_PKG_DEPENDS="c-toxcore, libconfig, libcurl, libqrencode, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
