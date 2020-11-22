a29657b770be854cabd2123ea86b6a2ac3cc1f36
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 15:45:14 +0000

    Revert "qpdf: update to 10.0.4"
    This reverts commit 570189baf38a492c321aee501d3683ab1b74e7e9.
    
    libtool: install: ./install-sh libqpdf/build/.libs/libqpdf.lai /data/data/com.termux/files/usr/lib/libqpdf.la
    ./install-sh: libqpdf/build/.libs/libqpdf.lai does not exist.

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Content-Preserving PDF Transformation System"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=10.0.4
+TERMUX_PKG_VERSION=10.0.1
 TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/releases/download/release-qpdf-$TERMUX_PKG_VERSION/qpdf-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b191dc4af4ea4630cdc404a4ee95a0f21c12f56ecc4ae045f04425464d7c696e
+TERMUX_PKG_SHA256=5d2277c738188b7f4e3f01a6db7f2937ed6df54671f1fba834cd3d7ff865827b
 TERMUX_PKG_DEPENDS="libc++, libjpeg-turbo, zlib"
 TERMUX_PKG_BREAKS="qpdf-dev"
 TERMUX_PKG_REPLACES="qpdf-dev"
