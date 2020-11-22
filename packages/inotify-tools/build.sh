c613095aa65bdd5de4ec8a8a5c4fa2307b3b43cf
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 13:45:41 +0000

    inotify-tools: update to 3.20.11.0

--- packages/inotify-tools/build.sh
+++ packages/inotify-tools/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/rvoicilas/inotify-tools/wiki
 TERMUX_PKG_DESCRIPTION="Programs providing a simple interface to inotify"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.20.2.2
+TERMUX_PKG_VERSION=3.20.11.0
 TERMUX_PKG_SRCURL=https://github.com/rvoicilas/inotify-tools/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=770fb1d94ace659f975d7494e3ab8b421a6aab930b9c37c7c290ab5280abb7b8
+TERMUX_PKG_SHA256=58a3cde89e4a5111a87ac16b56b06a8f885460fca0aea51b69c856ce30a37a14
 TERMUX_PKG_BREAKS="inotify-tools-dev"
 TERMUX_PKG_REPLACES="inotify-tools-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
