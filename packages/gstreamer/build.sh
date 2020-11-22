938134b2bfa4b5f8f74843200b46c082e4d914cc
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-22 01:34:49 +0000

    gstreamer: update to 1.18.1

--- packages/gstreamer/build.sh
+++ packages/gstreamer/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Open source multimedia framework"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=1.18.0
+TERMUX_PKG_VERSION=1.18.1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=0ff09245b06c0aeb5d9a156edcab088a7e8213a0bf9c84a1ff0318f9c00c7805
+TERMUX_PKG_SHA256=79df8de21f284a105a5c1568527f8c559c583c85c0f2bd7bdb5b0372b8beecba
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BREAKS="gstreamer-dev"
 TERMUX_PKG_REPLACES="gstreamer-dev"
