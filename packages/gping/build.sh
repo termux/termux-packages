ab11f5a258aed7fc6befab5af406f524ec0a6d72
kcubeterm <kcubeterm@gmail.com>
2020-11-23 09:20:25 +0000

    Add new pkg gping

---
+++ packages/gping/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/orf/gping
+TERMUX_PKG_DESCRIPTION="Ping, but with a graph"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=0.1.7
+TERMUX_PKG_SRCURL=https://github.com/orf/gping/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=5c2ff5e1d72fe1cc55b69eab073bb6ddec0fca1303313865e9c1d43bc52e85a3
+TERMUX_PKG_BUILD_IN_SRC=true
+
