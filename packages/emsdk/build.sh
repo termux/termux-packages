dc4a374bf6a4fc5dcd4630d0a3954bb43a834fa2
Ludea <ludealol.olol@gmail.com>
2020-11-29 09:16:27 +0100

    refactor installation

--- packages/emsdk/build.sh
+++ packages/emsdk/build.sh
@@ -5,9 +5,14 @@ TERMUX_PKG_VERSION=2.0.8
 TERMUX_PKG_SRCURL=https://github.com/emscripten-core/emsdk/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=248d716dbe2e2424ee3f205c41024b4efde974e8987f2204a97edb315c588ff3
 TERMUX_PKG_DEPENDS="llvm"
-TERMUX_PKG_BUILD_DEPENDS="python, nodejs"
+TERMUX_PKG_BUILD_IN_SOURCE=true
 
-termux_step_make() {
-    ./emsdk install latest
-  
+termux_step_post_get_source() {
+	./emsdk install latest 
+	./emsdk activate latest 	
+}
+
+termux_step_make_instal() {
+	rm $TERMUX_PREFIX/lib/emsdk -rf
+	mv . $TERMUX_PREFIX/lib/emsdk 
 }
