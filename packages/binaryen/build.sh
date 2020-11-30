cb21b9ad1dda263daf155f5a3764618ede4744c5
Ludea <ludealol.olol@gmail.com>
2020-11-30 18:58:02 +0100

    package Binaryen (#6105)
    * package Binaryen
    
    Add Binaryen as a single package.

---
+++ packages/binaryen/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/WebAssembly/binaryen
+TERMUX_PKG_DESCRIPTION="Binaryen is a compiler and toolchain infrastructure library for WebAssembly"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_VERSION=98
+TERMUX_PKG_SRCURL=https://github.com/WebAssembly/binaryen/archive/version_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9f805db6735869ab52cde7c0404879c90cf386888c0f587e944737550171c1c4
