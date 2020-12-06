bfd8b624fa80d0fa069999f36ac77e0c85eef1bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-06 23:22:55 +0200

    termux-api: update to 0.54

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.53
+TERMUX_PKG_VERSION=0.54
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=89141dcbb0b4b55b1d6c40e19ede74287eb5873b0cb9e9323d66f3f04d26e657
+TERMUX_PKG_SHA256=ae551fa81d679a6fc4ff2cf479f43bf5bd20d3b7d5c0fc6e81e5aca20c1643a9
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash, util-linux"
