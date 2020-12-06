a608c83c481e65d55c7502c69284e1e23e98c8c1
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-06 02:10:18 +0200

    proot-distro: update to 1.2

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_VERSION=1.2
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=07affedb793f369e21ef81658c940c9151cb7f80b638b7cd7e16a99906872b6c
+TERMUX_PKG_SHA256=e3e722b7315f472333b0c37437bb233de44c7357a2da0552ab771f4f0cad9a30
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -13,6 +13,7 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_CONFFILES="
 etc/proot-distro/alpine.sh
 etc/proot-distro/archlinux.sh
+etc/proot-distro/debian-buster.sh
 etc/proot-distro/nethunter.sh
 etc/proot-distro/ubuntu-18.04.sh
 etc/proot-distro/ubuntu-20.04.sh
