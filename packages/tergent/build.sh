83fe49ffa6931c39c64db90ac91f7d0ff23a693a
Daniil Gentili <daniil@daniil.it>
2020-12-06 21:10:16 +0100

    Tergent: Automatically unlock keystore using fingerprint, if possible

--- packages/tergent/build.sh
+++ packages/tergent/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/aeolwyr/tergent
+TERMUX_PKG_HOMEPAGE=https://github.com/danog/tergent
 TERMUX_PKG_DESCRIPTION="A cryptoki/PKCS#11 library for Termux that uses Android Keystore as its backend"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/aeolwyr/tergent/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0b59cf0ced3f693fb19396a986326963f3763e6bf65d3e56af0a03d206d69428
+TERMUX_PKG_SRCURL=https://github.com/danog/tergent/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3a9b0f27646275de5f344c3209fc563aca9bd685aea9d34388cfaabff07b1956
 TERMUX_PKG_DEPENDS="termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
