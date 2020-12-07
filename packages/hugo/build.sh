10f7f38cf2e23ba5b8ff74af3b2c0a17a5fe082e
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 20:43:58 +0000

    hugo: update to 0.79.0

--- packages/hugo/build.sh
+++ packages/hugo/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gohugo.io/
 TERMUX_PKG_DESCRIPTION="A fast and flexible static site generator"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=0.78.2
+TERMUX_PKG_VERSION=0.79.0
 TERMUX_PKG_SRCURL=https://github.com/gohugoio/hugo/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=bbfacf3bf379911bc348e6300912838fd07a75ad6de6d41082c54226e0055029
+TERMUX_PKG_SHA256=83e9b7e4bd3b321d140d1f35c75eafa6a70d3b814f2cac8e2f78b11feb23f1b2
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_make() {
