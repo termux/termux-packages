5488de48cd4b4597559ae5c5f693ae7f206190bc
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 21:37:02 +0000

    k9s: update to 0.24.2

--- packages/k9s/build.sh
+++ packages/k9s/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://k9scli.io
 TERMUX_PKG_DESCRIPTION="Kubernetes CLI To Manage Your Clusters In Style!"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=0.24.0
+TERMUX_PKG_VERSION=0.24.2
 TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=41df392b23cf38799ae2d4ab1fb3b808efa0e224d1f1a8b02dfdc893f0426453
+TERMUX_PKG_SHA256=0f4e51a202c1221b8a6f18790a9e3e18c33a3807136d07bd40b02deac7d91f23
 
 termux_step_make() {
         termux_setup_golang
