b7419a2e8de93a76e98268b44506fcab3cf12dd5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 21:44:14 +0000

    libmesode: update to 0.10.1

--- packages/libmesode/build.sh
+++ packages/libmesode/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/boothj5/libmesode
 TERMUX_PKG_DESCRIPTION="Minimal XMPP library written for use with Profanity XMPP client"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=0.10.0
+TERMUX_PKG_VERSION=0.10.1
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_SRCURL=https://github.com/boothj5/libmesode/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ddf50aaaf778e039b0c00b69f40f3d51238418e09b7c674c6388fedcac48adf9
+TERMUX_PKG_SHA256=c9dd90648e73d92b90f2b0ae41a75d8f469b116d3e6aa297c14cd57be937d99e
 TERMUX_PKG_DEPENDS="openssl,libexpat"
 TERMUX_PKG_BREAKS="libmesode-dev"
 TERMUX_PKG_REPLACES="libmesode-dev"
