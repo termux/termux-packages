f994d15f23a0097ca1e2f44f07a218052598ed96
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 23:09:48 +0000

    ssss: update to 0.5.7

--- packages/ssss/build.sh
+++ packages/ssss/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://point-at-infinity.org/ssss/
+TERMUX_PKG_HOMEPAGE=https://github.com/MrJoy/ssss
 TERMUX_PKG_DESCRIPTION="Simple command-line implementation of Shamir's Secret Sharing Scheme"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.5
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=http://point-at-infinity.org/ssss/ssss-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=5d165555105606b8b08383e697fc48cf849f51d775f1d9a74817f5709db0f995
+TERMUX_PKG_VERSION=0.5.7
+TERMUX_PKG_SRCURL=https://github.com/MrJoy/ssss/archive/releases/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=dbb1f03797cb3fa69594530f9b2c36010f66705b9d5fbbc27293dce72b9c9473
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -13,10 +12,7 @@ termux_step_make_install() {
 	install -Dm700 ssss-split "$TERMUX_PREFIX"/bin/
 	ln -sfr "$TERMUX_PREFIX"/bin/ssss-split $TERMUX_PREFIX/bin/ssss-combine
 
-	mkdir -p "$TERMUX_PREFIX"/share/man/man1/
-	install -Dm600 \
-		"$TERMUX_PKG_BUILDER_DIR"/ssss.1 \
-		"$TERMUX_PREFIX"/share/man/man1/
+	install -Dm600 ssss.1 "$TERMUX_PREFIX"/share/man/man1/ssss.1
 	ln -sfr \
 		"$TERMUX_PREFIX"/share/man/man1/ssss.1 \
 		"$TERMUX_PREFIX"/share/man/man1/ssss-combine.1
