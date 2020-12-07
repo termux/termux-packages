491feb10f0bd8e39aa68f59654b2fd2cfa5bb375
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 20:34:27 +0000

    fd: update to 8.2.1

--- packages/fd/build.sh
+++ packages/fd/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/fd
 TERMUX_PKG_DESCRIPTION="Simple, fast and user-friendly alternative to find"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=8.1.1
+TERMUX_PKG_VERSION=8.2.1
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/fd/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=7b327dc4c2090b34c7fb3e5ac7147f7bbe6266c2d44b182038d36f3b1d347cc1
+TERMUX_PKG_SHA256=429de7f04a41c5ee6579e07a251c72342cd9cf5b11e6355e861bb3fffa794157
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
