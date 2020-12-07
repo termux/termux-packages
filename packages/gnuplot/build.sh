338b8dbfd29d5b57f6e66020dc320bf8065c4bec
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 21:09:06 +0000

    gnuplot: update to 5.4.1

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://gnuplot.info/
 TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
 TERMUX_PKG_LICENSE="BSD"
-TERMUX_PKG_VERSION=5.4.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.4.1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=eb4082f03a399fd1e9e2b380cf7a4f785e77023d8dcc7e17570c1b5570a49c47
+TERMUX_PKG_SHA256=6b690485567eaeb938c26936e5e0681cf70c856d273cc2c45fabf64d8bc6590e
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libgd, libiconv, libx11, pango, readline, zlib"
 TERMUX_PKG_BREAKS="gnuplot-x"
 TERMUX_PKG_REPLACES="gnuplot-x"
