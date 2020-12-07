2bbc6b814f9cee7157c236096db93edf3847c5e0
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-07 21:39:57 +0000

    libarchive: update to 3.5.0

--- packages/libarchive/build.sh
+++ packages/libarchive/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.libarchive.org/
 TERMUX_PKG_DESCRIPTION="Multi-format archive and compression library"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=3.4.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.5.0
 TERMUX_PKG_SRCURL=https://github.com/libarchive/libarchive/releases/download/v$TERMUX_PKG_VERSION/libarchive-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ee1e749213c108cb60d53147f18c31a73d6717d7e3d2481c157e1b34c881ea39
+TERMUX_PKG_SHA256=fc4bc301188376adc18780d35602454cc8df6396e1b040fbcbb0d4c0469faf54
 TERMUX_PKG_DEPENDS="libbz2, libiconv, liblzma, libxml2, openssl, zlib"
 TERMUX_PKG_BREAKS="libarchive-dev"
 TERMUX_PKG_REPLACES="libarchive-dev"
