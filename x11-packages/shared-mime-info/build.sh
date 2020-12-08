bf96f29ac0ad4506b30c0a144ec46ac5a26dce6c
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-08 11:21:22 +0000

    shared-mime-info: update to 2.0

--- x11-packages/shared-mime-info/build.sh
+++ x11-packages/shared-mime-info/build.sh
@@ -2,24 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://freedesktop.org/Software/shared-mime-info
 TERMUX_PKG_DESCRIPTION="Freedesktop.org Shared MIME Info"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.15
-TERMUX_PKG_REVISION=14
-TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/Release-${TERMUX_PKG_VERSION//./-}/shared-mime-info-Release-${TERMUX_PKG_VERSION//./-}.tar.gz
-TERMUX_PKG_SHA256=42d7fca08c2856ddf71743fad96afd2fd363eabf0b0dc67adc2da07a0e7f50a8
+TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/${TERMUX_PKG_VERSION}/shared-mime-info-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0884a57f5fb10dfce146312f6d5826f767e47863d8f543ab4ec0228336e468f8
 TERMUX_PKG_DEPENDS="coreutils, glib, libxml2"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-update-mimedb ac_cv_func_fdatasync=no"
-
-termux_step_pre_configure() {
-	NOCONFIGURE=1 bash ./autogen.sh
-}
-
-termux_step_post_make_install() {
-	# fix location of shared-mime-info.pc
-	if [ -f "${TERMUX_PREFIX}/share/pkgconfig/shared-mime-info.pc" ]; then
-		mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig"
-		mv -f "${TERMUX_PREFIX}/share/pkgconfig/shared-mime-info.pc" "${TERMUX_PREFIX}/lib/pkgconfig/shared-mime-info.pc"
-	fi
-}
 
 termux_step_create_debscripts() {
     cp "${TERMUX_PKG_BUILDER_DIR}/postinst" ./postinst
