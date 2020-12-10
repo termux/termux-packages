6de7b2d141302554b9694fd7d4c6daf38945d8c5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-10 22:57:24 +0200

    composer: platform independent + fix shebang
    PHARs do not like to be edited after compilation, so shebang
    has to be fixed before it was created.

--- packages/composer/build.sh
+++ packages/composer/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://getcomposer.org/
 TERMUX_PKG_DESCRIPTION="Dependency Manager for PHP"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=2.0.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_SRCURL=https://github.com/composer/composer.git
 TERMUX_PKG_DEPENDS="php"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	composer install
