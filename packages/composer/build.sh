0062793ae7ebd0f387f94aad24855019ca3572ec
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-12-11 01:42:59 +0630

    Resolved:Code Formatting,Fake Pkg version name and  build step

--- packages/composer/build.sh
+++ packages/composer/build.sh
@@ -1,15 +1,25 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/composer/composer
-TERMUX_PKG_DESCRIPTION="Composer is a tool for dependency management in PHP."
-TERMUX_PKG_LICENSE=MIT
-TERMUX_PKG_VERSION=2
-TERMUX_PKG_DEPENDS="php"
+TERMUX_PKG_HOMEPAGE=https://php.net
+TERMUX_PKG_DESCRIPTION="Dependency Manager for PHP"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=2.0.8
+TERMUX_PKG_DEPENDS="php,unzip"
 
-termux_step_make_install() {
-  if [ -f $TERMUX_PREFIX/composer ]; then
-  rm $PREFIX/bin/composer
-  else
-  curl -sS https://getcomposer.org/installer | php -- --install-dir=$TERMUX_PREFIX/bin --filename=composer
-   composer 
-  fi
-
+termux_step_post_make_install() {
+    cd 
+    EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+    cd
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+      >&2 echo 'ERROR: Invalid installer checksum'
+      rm composer-setup.php
+      exit 1
+    fi
+    
+    php composer-setup.php --quiet
+    RESULT=$?
+    rm composer-setup.php
+    mv composer.phar $TERMUX_PREFIX/bin/composer
+    exit $RESULT
 }
