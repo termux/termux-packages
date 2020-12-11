faf0a2e1abbd499be57c88a40c41e35cd1ca9f83
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-11 18:13:39 +0200

    libxfce4ui: update to 4.15.6

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=4.15.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.15.6
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/libxfce4ui/${TERMUX_PKG_VERSION:0:4}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=cc1faff6ee48e21b087de613ab4e4cd4fbdae384b86b534faad07c83f6546501
+TERMUX_PKG_SHA256=97e437f2481769de49f1b378f4b45fb8809b9de7ad9d0b776d51232771f0f4e9
 TERMUX_PKG_DEPENDS="gtk2, gtk3, hicolor-icon-theme, libsm, libxfce4util, startup-notification, xfconf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-gtk3
