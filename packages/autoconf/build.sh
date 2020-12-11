c6f622b77a7f0c31a4b7d979fce0b5397bc28c19
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-11 17:41:14 +0200

    autoconf: update to 2.70

--- packages/autoconf/build.sh
+++ packages/autoconf/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/autoconf/autoconf.html
 TERMUX_PKG_DESCRIPTION="Creator of shell scripts to configure source code packages"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=2.69
+TERMUX_PKG_VERSION=2.70
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/autoconf/autoconf-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684
+TERMUX_PKG_SHA256=fa9e227860d9d845c0a07f63b88c8d7a2ae1aa2345fb619384bb8accc19fecc6
 TERMUX_PKG_DEPENDS="m4, make, perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
