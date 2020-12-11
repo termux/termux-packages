ca4359c9815cad0c001d779b833eaeb6df721ab2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-11 17:38:20 +0200

    apt: update to 2.1.13

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=2.1.12
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.1.13
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ad9a3ace79547d8949cee0f6a23fa3ff168a020799d2e1c745e600425d8de027
+TERMUX_PKG_SHA256=ed56790183618df99d60d4a37729a7abca7e780607ad91f5d9f2a4ee84f7c9a1
 # apt-key requires utilities from coreutils, findutils, gpgv, grep, sed.
 TERMUX_PKG_DEPENDS="coreutils, dpkg, findutils, gpgv, grep, libandroid-glob, libbz2, libc++, libcurl, libgnutls, liblz4, liblzma, sed, termux-licenses, zlib"
 TERMUX_PKG_CONFLICTS="apt-transport-https, libapt-pkg"
