f3830c88dbc0aa10e94e01611de5088bd57e64bc
WilliamOfBlobs <75296966+WilliamOfBlobs@users.noreply.github.com>
2020-12-11 22:16:55 -0500

    Update build.sh

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -3,5 +3,5 @@ TERMUX_PKG_DESCRIPTION="Rednex Game Boy Development System - An assembly toolcha
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.4.2
 TERMUX_PKG_SRCURL=https://github.com/gbdev/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2579cbd6cc47bc944038d17ec3af640e2782c67fdffe7093e6083430543c9780
+TERMUX_PKG_SHA256=0bac46f0d3cfabf8683c62145b9f01a607c703295ef2978a3e548f106f50fac1
 TERMUX_PKG_DEPENDS="libpng, pkg-config, bison"
