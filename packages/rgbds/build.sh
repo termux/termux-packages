343c00e0798d05447b4dfdd1cc708d141aec043c
WilliamOfBlobs <75296966+WilliamOfBlobs@users.noreply.github.com>
2020-12-11 21:59:35 -0500

    rgbds: update rgbds version from 0.4.1 to 0.4.2

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -1,7 +1,7 @@
-TERMUX_PKG_HOMEPAGE=https://rednex.github.io/rgbds/
-TERMUX_PKG_DESCRIPTION="Rednex Game Boy Development System"
+TERMUX_PKG_HOMEPAGE=https://rgbds.gbdev.io
+TERMUX_PKG_DESCRIPTION="Rednex Game Boy Development System - An assembly toolchain for the Nintendo Game Boy & Game Boy Color"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.4.1
-TERMUX_PKG_SRCURL=https://github.com/rednex/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e4253de7f1fdf1cd202276b07150010e182490052c6da6ae6ddac5685b9fec45
-TERMUX_PKG_DEPENDS="libpng, zlib"
+TERMUX_PKG_VERSION=0.4.2
+TERMUX_PKG_SRCURL=https://github.com/gbdev/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2579cbd6cc47bc944038d17ec3af640e2782c67fdffe7093e6083430543c9780
+TERMUX_PKG_DEPENDS="libpng, pkg-config, bison"
