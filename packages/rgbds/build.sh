c08ec9593399793d8a79923a471149f86bd4cece
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-12 14:32:41 +0200

    do not specify host build tools in depends

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.4.2
 TERMUX_PKG_SRCURL=https://github.com/gbdev/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0bac46f0d3cfabf8683c62145b9f01a607c703295ef2978a3e548f106f50fac1
-TERMUX_PKG_DEPENDS="libpng, pkg-config, bison"
+TERMUX_PKG_DEPENDS="libpng"
