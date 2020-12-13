6caf10405506f6b65ccb96c950fd8fb4ddbbe411
kcubeterm <kcubeterm@gmail.com>
2020-12-13 18:08:42 +0000

    Add new pkg fzy

---
+++ packages/fzy/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/jhawthorn/fzy
+TERMUX_PKG_DESCRIPTION="A simple, fast fuzzy finder for the terminal"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_SRCURL=https://github.com/jhawthorn/fzy/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=80257fd74579e13438b05edf50dcdc8cf0cdb1870b4a2bc5967bd1fdbed1facf
+TERMUX_PKG_BUILD_IN_SRC=true
