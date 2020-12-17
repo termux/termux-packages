846ee8e41a9cb818e7da1cbb90c519886fae627a
道玄 <yozman@sina.com>
2020-12-17 22:41:51 +0800

    phpmyadmin: fix path (#6149)

--- packages/phpmyadmin/build.sh
+++ packages/phpmyadmin/build.sh
@@ -9,12 +9,12 @@ TERMUX_PKG_DEPENDS="apache2, php"
 TERMUX_PKG_CONFFILES="etc/phpmyadmin/config.inc.php"
 
 termux_step_make_install() {
-	rm -rf $TERMUX_PREFIX/usr/share/phpmyadmin
-	mkdir -p $TERMUX_PREFIX/usr/share/phpmyadmin
-	cp -a $TERMUX_PKG_SRCDIR/* $TERMUX_PREFIX/usr/share/phpmyadmin/
+	rm -rf $TERMUX_PREFIX/share/phpmyadmin
+	mkdir -p $TERMUX_PREFIX/share/phpmyadmin
+	cp -a $TERMUX_PKG_SRCDIR/* $TERMUX_PREFIX/share/phpmyadmin/
 	mkdir -p $TERMUX_PREFIX/etc/phpmyadmin
 	cp $TERMUX_PKG_SRCDIR/config.sample.inc.php $TERMUX_PREFIX/etc/phpmyadmin/config.inc.php
-	ln -s $TERMUX_PREFIX/etc/phpmyadmin/config.inc.php $TERMUX_PREFIX/usr/share/phpmyadmin
+	ln -s $TERMUX_PREFIX/etc/phpmyadmin/config.inc.php $TERMUX_PREFIX/share/phpmyadmin
 	mkdir -p $TERMUX_PREFIX/etc/apache2/conf.d
 	sed -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" $TERMUX_PKG_BUILDER_DIR/phpmyadmin.conf \
 		> $TERMUX_PREFIX/etc/apache2/conf.d/phpmyadmin.conf
