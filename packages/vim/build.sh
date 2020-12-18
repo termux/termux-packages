d4b61874900f57d67888704a5dd3d9bddf294f1a
Jorge Juan-Chico <jjchico@gmail.com>
2020-12-18 20:00:58 +0100

    Increment TERMUX_PKG_REVISION

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update both vim and vim-python to the same version in one PR.
 TERMUX_PKG_VERSION=8.2.2100
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=5912f417d2818b7c889470d5e357809609eea6a5817491d344f7609d7fdf81fa
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
