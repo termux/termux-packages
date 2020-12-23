4018a396dd7b47a0d2818a5b63bb06b44315e9d5
Henrik Grimler <henrik@grimler.se>
2020-12-23 16:45:24 +0100

    termux-am: update to 0.4

--- packages/termux-am/build.sh
+++ packages/termux-am/build.sh
@@ -2,21 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/TermuxAm
 TERMUX_PKG_DESCRIPTION="Android Oreo-compatible am command reimplementation"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
-TERMUX_PKG_VERSION=0.3
-TERMUX_PKG_SRCURL=https://github.com/termux/TermuxAm/archive/v$TERMUX_PKG_VERSION.zip
-TERMUX_PKG_SHA256=3abfaca319f59fa769480c33d50e2ee1ff73122eac7c658f9e2a66a2da29ebb7
+TERMUX_PKG_VERSION=0.4
+TERMUX_PKG_SRCURL=https://github.com/termux/TermuxAm/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f13e7de6682b2a8303ac69e6a908fc7bd2ae091b3941eb33ecc35f47ea77a435
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFLICTS="termux-tools (<< 0.51)"
+_GRADLE_VERSION=6.5.1
 
 termux_step_make() {
 	# Download and use a new enough gradle version to avoid the process hanging after running:
 	termux_download \
-		https://services.gradle.org/distributions/gradle-5.4-bin.zip \
-		$TERMUX_PKG_CACHEDIR/gradle-5.4-bin.zip \
-		c8c17574245ecee9ed7fe4f6b593b696d1692d1adbfef425bef9b333e3a0e8de
+		https://services.gradle.org/distributions/gradle-$_GRADLE_VERSION-bin.zip \
+		$TERMUX_PKG_CACHEDIR/gradle-$_GRADLE_VERSION-bin.zip \
+		50a7d30529fa939721fe9268a0205142f3f2302bcac5fb45b27a3902e58db54a
 	mkdir $TERMUX_PKG_TMPDIR/gradle
-	unzip -q $TERMUX_PKG_CACHEDIR/gradle-5.4-bin.zip -d $TERMUX_PKG_TMPDIR/gradle
+	unzip -q $TERMUX_PKG_CACHEDIR/gradle-$_GRADLE_VERSION-bin.zip -d $TERMUX_PKG_TMPDIR/gradle
 
 	# Avoid spawning the gradle daemon due to org.gradle.jvmargs
 	# being set (https://github.com/gradle/gradle/issues/1434):
@@ -25,7 +26,7 @@ termux_step_make() {
 	export ANDROID_HOME
 	export GRADLE_OPTS="-Dorg.gradle.daemon=false -Xmx1536m"
 
-	$TERMUX_PKG_TMPDIR/gradle/gradle-5.4/bin/gradle \
+	$TERMUX_PKG_TMPDIR/gradle/gradle-$_GRADLE_VERSION/bin/gradle \
 		:app:assembleRelease
 }
 
