eca835c631d1d2ce4146764bb67ae1a15081bf1f
Henrik Grimler <henrik@grimler.se>
2020-12-24 23:39:16 +0100

    ovmf: update to 20201222

--- packages/ovmf/build.sh
+++ packages/ovmf/build.sh
@@ -2,16 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://www.tianocore.org/
 TERMUX_PKG_DESCRIPTION="Open Virtual Machine Firmware"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20200515.1441.g5ffcbc4690
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=20201222.1526.ge2747dbb5a
 TERMUX_PKG_SRCURL=(https://www.kraxel.org/repos/jenkins/edk2/edk2.git-aarch64-0-${TERMUX_PKG_VERSION}.noarch.rpm
 		   https://www.kraxel.org/repos/jenkins/edk2/edk2.git-arm-0-${TERMUX_PKG_VERSION}.noarch.rpm
 		   https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-ia32-0-${TERMUX_PKG_VERSION}.noarch.rpm
 		   https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-x64-0-${TERMUX_PKG_VERSION}.noarch.rpm)
-TERMUX_PKG_SHA256=(4e465593b4d6c3a5e5243243b1eb08d813c469a017498074853e87aa9b685df9
-		   91f809a8fc60a3dbe5d1d42a82203ae0332a1014d223be7c48b1210633d99e74
-		   25b65ef44310a9a006107e55d9c8f6dbfae7e8c33b8c7a1ac111dfe0b06e82dc
-		   e11cfa89df7459c7cac0a066e8e340fae3ebe36b2ed6201d67092779eee4092e)
+TERMUX_PKG_SHA256=(605a26ccf228d0577c4095d2549c94b145978373e26b2d6eb845457901d249e7
+		   04118ec32022c8469b96b0c4b9cbecf02db8caa925377b035baece303ca1997d
+		   0df2deac39bf0045f0827400291686d81280c0f2769bef67701a50fe070b44b5
+		   021825fe51fbf07d1ed3b0fd8cd0d2da491155c23338495e5b1dc8f4641c96bb)
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
