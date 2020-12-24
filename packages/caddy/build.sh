0a6db230d26b724e4284c40f8c43389b654450b2
Henrik Grimler <henrik@grimler.se>
2020-12-24 12:46:49 +0100

    caddyserver: change to new url

--- packages/caddy/build.sh
+++ packages/caddy/build.sh
@@ -3,21 +3,21 @@ TERMUX_PKG_DESCRIPTION="Fast, cross-platform HTTP/2 web server"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2.1
-TERMUX_PKG_SRCURL=https://github.com/mholt/caddy/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/caddyserver/caddy/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=dd1f313968a54c3e6be9b5d007b8b2ed88c46fbefc1eed49dcac35c08fbef1e2
 
 termux_step_make() {
 	termux_setup_golang
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 
-	mkdir -p $GOPATH/src/github.com/mholt/
-	cp -a $TERMUX_PKG_SRCDIR $GOPATH/src/github.com/mholt/caddy
+	mkdir -p $GOPATH/src/github.com/caddyserver/
+	cp -a $TERMUX_PKG_SRCDIR $GOPATH/src/github.com/caddyserver/caddy
 
-	cd $GOPATH/src/github.com/mholt/caddy/cmd/caddy
+	cd $GOPATH/src/github.com/caddyserver/caddy/cmd/caddy
 	export GO111MODULE=on
 	go build -v .
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin $GOPATH/src/github.com/mholt/caddy/cmd/caddy/caddy
+	install -Dm700 -t $TERMUX_PREFIX/bin $GOPATH/src/github.com/caddyserver/caddy/cmd/caddy/caddy
 }
