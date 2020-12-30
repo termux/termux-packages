353476a7fda968a7d774e9a96e768838e6c856f5
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-12-30 15:26:11 +0200

    deutex: switch to source archives generated by Github
    We don't have zstd utility in Docker image currently.

--- packages/deutex/build.sh
+++ packages/deutex/build.sh
@@ -3,6 +3,10 @@ TERMUX_PKG_DESCRIPTION="WAD composer for Doom, Heretic, Hexen, and Strife"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.2.2
-TERMUX_PKG_SRCURL=https://github.com/Doom-Utils/deutex/releases/download/v$TERMUX_PKG_VERSION/deutex-$TERMUX_PKG_VERSION.tar.zst
+TERMUX_PKG_SRCURL=https://github.com/Doom-Utils/deutex/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=10ed0e7a533ec97cb6d03548d4258fbec88852a45b5ea4cf5434376ad4174b5f
 TERMUX_PKG_DEPENDS="libpng, zlib"
+
+termux_step_pre_configure() {
+	./bootstrap
+}
