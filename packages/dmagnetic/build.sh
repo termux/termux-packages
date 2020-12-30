db03c10e1c440b7dfcdad7113209b5e692d93660
Henrik Grimler <henrik@grimler.se>
2020-12-30 19:37:37 +0100

    dmagnetic: add package

---
+++ packages/dmagnetic/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.dettus.net/dMagnetic
+TERMUX_PKG_DESCRIPTION="Interpreter for classic text adventure games and interactive fiction"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_VERSION=0.29
+TERMUX_PKG_SRCURL=https://www.dettus.net/dMagnetic/dMagnetic_${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=a980e35f85c9661fe0d98c670f9d6be56000da2bbc8b3e8e78697eac05ae5b47
+TERMUX_PKG_BUILD_IN_SRC=true
