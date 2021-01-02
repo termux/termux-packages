0d5858d98a73876d2616d6257f15e021915138ba
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-02 22:45:40 +0200

    hydroxide: remove libc++ dependency
    It was left after copy-pasting from another build.sh and is not needed here.

--- packages/hydroxide/build.sh
+++ packages/hydroxide/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A third-party, open-source ProtonMail CardDAV, IMAP and 
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2.17
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/emersion/hydroxide/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d1e24ce95c181fdad8cef78dc93ed6ba259302a21c0b160b7552ffc6b346bde8
-TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_make() {
 	termux_setup_golang
