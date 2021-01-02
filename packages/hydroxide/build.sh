a1bbdb4f9bc8ffc06eae7766b5cbcd02fb2adbb2
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-02 22:01:34 +0200

    new package: hydroxide
    Requested in https://github.com/termux/termux-packages/issues/6194.

---
+++ packages/hydroxide/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/emersion/hydroxide
+TERMUX_PKG_DESCRIPTION="A third-party, open-source ProtonMail CardDAV, IMAP and SMTP bridge"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.2.17
+TERMUX_PKG_SRCURL=https://github.com/emersion/hydroxide/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d1e24ce95c181fdad8cef78dc93ed6ba259302a21c0b160b7552ffc6b346bde8
+TERMUX_PKG_DEPENDS="libc++"
+
+termux_step_make() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+
+	cd $TERMUX_PKG_SRCDIR
+	go build ./cmd/hydroxide
+}
+
+termux_step_make_install() {
+	install -Dm755 -t "${TERMUX_PREFIX}"/bin "${TERMUX_PKG_SRCDIR}"/hydroxide
+}
