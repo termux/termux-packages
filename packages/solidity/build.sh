97f6c91f62f4fee11b37aba1a2289d5b3ff35b7b
Wetitpig <winsto003@hotmail.com>
2021-01-05 23:32:58 +0800

    Avoid ninja

--- packages/solidity/build.sh
+++ packages/solidity/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=https://github.com/ethereum/solidity/releases/download/v${TERM
 TERMUX_PKG_SHA256=5a8f9f421dcf65d552b2e6fea4929aef68706a8db8b2e626e7a81e4e5ee11549
 TERMUX_PKG_DEPENDS="boost-static"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DUSE_Z3=OFF -DUSE_CVC4=OFF"
+TERMUX_CMAKE_BUILD="Unix Makefiles"
