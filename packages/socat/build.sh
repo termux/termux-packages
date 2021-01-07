5cec106f74e17cceab2587ee7bead900aa105ec7
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-07 17:39:49 +0200

    socat: disable 32bit builds because they are broken

--- packages/socat/build.sh
+++ packages/socat/build.sh
@@ -8,3 +8,5 @@ TERMUX_PKG_SHA256=3f1e01a4af181c5d1b32b2986089282dc6a789d2aa4c3ac67070b27979195b
 TERMUX_PKG_DEPENDS="openssl, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_header_resolv_h=no ac_cv_c_compiler_gnu=yes ac_compiler_gnu=yes" # sc_cv_sys_crdly_shift=9 sc_cv_sys_csize_shift=4 sc_cv_sys_tabdly_shift=11"
 TERMUX_PKG_BUILD_IN_SRC=true
+# Broken 32bit support.
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
