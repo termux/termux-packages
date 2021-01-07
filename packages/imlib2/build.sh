3b71cdae4736602efdba9f298d705f0c86049d1b
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-07 14:55:23 +0200

    imlib2: update to 1.7.1

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/enlightenment/
 TERMUX_PKG_DESCRIPTION="Library that does image file loading and saving as well as rendering, manipulation, arbitrary polygon support"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.7.0
+TERMUX_PKG_VERSION=1.7.1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fde6cc188a35e34eff251ee249d2b5624aa0d85f83a689237ec2fe90ceca718a
+TERMUX_PKG_SHA256=af30cf36e956febf18f9d33a81a4b43fea8f761ce74a67715d2ad157bb92c090
 TERMUX_PKG_DEPENDS="freetype, giflib, libandroid-shmem, libbz2, libid3tag, libjpeg-turbo, libpng, libtiff, libxext, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBS=-landroid-shmem"
