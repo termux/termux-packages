b829e6f6ab9e3776d22622372f5a20c9679ab1fb
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-07 16:54:03 +0200

    zziplib: update to 0.13.72

--- packages/zziplib/build.sh
+++ packages/zziplib/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://zziplib.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Provides read access to zipped files in a zip-archive, using compression based on free algorithms"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.13.71
+TERMUX_PKG_VERSION=0.13.72
 TERMUX_PKG_SRCURL=https://github.com/gdraheim/zziplib/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2ee1e0fbbb78ec7cc46bde5b62857bc51f8d665dd265577cf93584344b8b9de2
+TERMUX_PKG_SHA256=93ef44bf1f1ea24fc66080426a469df82fa631d13ca3b2e4abaeab89538518dc
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DZZIPTEST=off -DZZIPDOCS=off"
 TERMUX_PKG_DEPENDS="zlib"
