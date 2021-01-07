bd7e64bc29eb5cd730fdc7c87a92e15f7685d154
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-07 15:55:35 +0200

    net-tools: update to 2.10.0

--- packages/net-tools/build.sh
+++ packages/net-tools/build.sh
@@ -2,14 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://net-tools.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Configuration tools for Linux networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.60.2017.02.21
-TERMUX_PKG_REVISION=3
-commit=479bb4a7e11a4084e2935c0a576388f92469225b
-# We use a mirror to avoid using
-# https://sourceforge.net/code-snapshots/git/n/ne/net-tools/code.git/net-tools-code-$commit.zip
-# which does not work all the time (sourceforge caching system):
-TERMUX_PKG_SRCURL=https://dl.bintray.com/termux/upstream/net-tools-code-${commit}.zip
-TERMUX_PKG_SHA256=7e9f8e8dcbabed0c8eeb976100496567abae7ac9d92c72cebd1a9d965473e943
+TERMUX_PKG_VERSION=2.10.0
+TERMUX_PKG_SRCURL=https://sourceforge.net/projects/net-tools/files/net-tools-2.10.tar.xz
+TERMUX_PKG_SHA256=b262435a5241e89bfa51c3cabd5133753952f7a7b7b93f32e08cb9d96f580d69
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="BINDIR=$TERMUX_PREFIX/bin SBINDIR=$TERMUX_PREFIX/bin HAVE_HOSTNAME_TOOLS=0"
 
@@ -17,8 +12,4 @@ termux_step_configure() {
 	CFLAGS="$CFLAGS -D_LINUX_IN6_H -Dindex=strchr -Drindex=strrchr"
 	sed -i "s#/usr#$TERMUX_PREFIX#" $TERMUX_PKG_SRCDIR/man/Makefile
 	yes "" | make config || true
-}
-
-termux_step_make_install() {
-	make $TERMUX_PKG_EXTRA_MAKE_ARGS update
 }
