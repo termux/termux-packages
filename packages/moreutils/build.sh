9c879051230ef56f7e258c66b4621e0aa306eac6
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-07 15:37:24 +0200

    moreutils: update to 0.65

--- packages/moreutils/build.sh
+++ packages/moreutils/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://joeyh.name/code/moreutils/
 TERMUX_PKG_DESCRIPTION="A growing collection of the unix tools that nobody thought to write thirty years ago"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.64
+TERMUX_PKG_VERSION=0.65
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/m/moreutils/moreutils_$TERMUX_PKG_VERSION.orig.tar.xz
-TERMUX_PKG_SHA256=802ce261864d6f9985b56423af422fc5d5cb8e5226aaaf02a6522faa3035662c
+TERMUX_PKG_SHA256=ba0cfaa1ff6ead2b15c62a67292de66a366f9b815a09697b54677f7e15f5a2b2
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
