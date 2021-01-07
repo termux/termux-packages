ab1666ca01f4a011ce8c35237430ab2df45fb496
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-07 15:40:13 +0200

    mutt: update to 2.0.4

--- packages/mutt/build.sh
+++ packages/mutt/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.mutt.org/
 TERMUX_PKG_DESCRIPTION="Mail client with patches from neomutt"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.3
+TERMUX_PKG_VERSION=2.0.4
 TERMUX_PKG_SRCURL=ftp://ftp.mutt.org/pub/mutt/mutt-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9c327cafb7acbfd4a57e7c817148fe438720a4545a5f628926f7745bc752c1ed
+TERMUX_PKG_SHA256=45087e78105e9abc7cc37827865d319081587cb53a94412aa4e3118e937991d4
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses, gdbm, openssl, libsasl, mime-support, zlib, libiconv"
 TERMUX_PKG_BUILD_IN_SRC=true
 
