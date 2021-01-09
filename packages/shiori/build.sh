6d1e28d2f819e24a7263bd6fbd04dc840bf4479d
kcubeterm <kcubeterm@gmail.com>
2021-01-09 12:44:26 +0000

    New package: Shiori

---
+++ packages/shiori/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/go-shiori/shiori
+TERMUX_PKG_DESCRIPTION="Simple bookmark manager built with Go"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
+TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_SRCURL=https://github.com/go-shiori/shiori/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c2ebc0f009feb22f891c7ab0fa7b8c0d71e1cfc34a974c7503a702ec07d8e9ee
+
+termux_step_make() {
+	termux_setup_golang
+
+	cd "$TERMUX_PKG_SRCDIR"
+	
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "$GOPATH"/src/github.com/go-shiori/
+	mkdir -p "$TERMUX_PREFIX"/share/doc/shiori
+	cp -a "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/go-shiori/shiori
+	cd "$GOPATH"/src/github.com/go-shiori/shiori/
+	go get -d -v
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/go-shiori/shiori/shiori
+	cp -a "$TERMUX_PKG_SRCDIR"/docs/ "$TERMUX_PREFIX"/share/doc/shiori
+}
