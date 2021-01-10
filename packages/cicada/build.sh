29d48878c4c3c47a4aa2dcd2f0db060785ef30a5
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-10 13:38:11 +0200

    cicada: disable builds for 32bit CPUs

--- packages/cicada/build.sh
+++ packages/cicada/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=0.9.17
 TERMUX_PKG_SRCURL=https://github.com/mitnk/cicada/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ed4fe063354ff163d8595e88b975245bfddb1df1a57ce62f987ccff77d8e933e
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
     rm -f Makefile
