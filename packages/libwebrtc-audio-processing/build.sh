852a38408647cc4e92b596b1727d7efb77166297
Krishna कन्हैया <kcubeterm@gmail.com>
2021-01-18 02:48:09 +0530

    Added TERMUX_PKG_BUILD_IN_SRC

--- packages/libwebrtc-audio-processing/build.sh
+++ packages/libwebrtc-audio-processing/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_MAINTAINER="Patrick Gaskin @pgaskin"
 TERMUX_PKG_VERSION=0.3.1
 TERMUX_PKG_SRCURL=https://freedesktop.org/software/pulseaudio/webrtc-audio-processing/webrtc-audio-processing-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4dabdd0789acd117d88d53f0a793cf4e906c6a93ee9aa6975ec928eafbf1dfe5
+TERMUX_PKG_BUILD_IN_SRC=true
