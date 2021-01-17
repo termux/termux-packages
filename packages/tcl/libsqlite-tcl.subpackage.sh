f2f0b6386ed875821e2dff9ed98af0fa43be2721
Lucy Phipps <landfillbaby@outlook.com>
2021-01-17 06:38:05 +0000

    fix sqlcipher and tcl conflict

---
+++ packages/tcl/libsqlite-tcl.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_DESCRIPTION="tcl bindings for SQLite"
+TERMUX_SUBPKG_DEPENDS="libsqlite"
+TERMUX_SUBPKG_CONFLICTS="sqlcipher-tcl"
+TERMUX_SUBPKG_BREAKS="sqlcipher (<< 4.4.2-1), tcl (<< 8.6.10-4)"
+TERMUX_SUBPKG_INCLUDE="lib/tcl*/sqlite/"
