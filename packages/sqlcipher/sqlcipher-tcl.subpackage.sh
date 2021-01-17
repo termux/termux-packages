50c6624b0cc7fa9c878a2a1a0ee60804fb6fa15f
Lucy Phipps <landfillbaby@outlook.com>
2021-01-17 13:21:14 +0000

    unsplit sqlcipher-tcl

--- packages/sqlcipher/sqlcipher-tcl.subpackage.sh
+++
@@ -1,4 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="tcl bindings for SQLCipher"
-TERMUX_SUBPKG_DEPENDS="tcl"
-TERMUX_SUBPKG_BREAKS="sqlcipher (<< 4.4.2-1), tcl (<< 8.6.10-4)"
-TERMUX_SUBPKG_INCLUDE="lib/tcl*/sqlite/"
