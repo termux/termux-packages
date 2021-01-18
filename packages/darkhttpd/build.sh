5e6023bdb45402b23f7887f7d69feea321b267cb
kcubeterm <kcubeterm@gmail.com>
2021-01-18 17:49:18 +0000

    darkhttpd: Update to 1.13

--- packages/darkhttpd/build.sh
+++ packages/darkhttpd/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://unix4lyfe.org/darkhttpd
 TERMUX_PKG_DESCRIPTION="A simple webserver, implemented in a single .c file."
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="David Paskevic @casept"
-TERMUX_PKG_VERSION=1.12
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://fossies.org/linux/www/darkhttpd-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=2652bc7e3dab35fcb64453616771016017a135e4b263ef73a36d29662593d472
+TERMUX_PKG_VERSION=1.13
+TERMUX_PKG_SRCURL=https://github.com/emikulic/darkhttpd/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1d88c395ac79ca9365aa5af71afe4ad136a4ed45099ca398168d4a2014dc0fc2
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
