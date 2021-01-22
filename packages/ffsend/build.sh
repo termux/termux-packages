8214a60e86c0ea2a251e2a0f282eba3a76be9146
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-22 13:36:12 +0200

    ffsend: fix maintainer info

--- packages/ffsend/build.sh
+++ packages/ffsend/build.sh
@@ -1,7 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/timvisee/ffsend
 TERMUX_PKG_DESCRIPTION="A fully featured Firefox Send client"
 TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2.68
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.com/timvisee/ffsend/-/archive/v$TERMUX_PKG_VERSION/ffsend-v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ffd8b161ef13c823c43417ac00402c1f4b0dfa4b3e6e4c5f7cdf0ceae9511c1d
 TERMUX_PKG_DEPENDS="openssl"
