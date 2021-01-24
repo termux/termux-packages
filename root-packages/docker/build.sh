b836a1fa37925f6b0c8817580d2eb7fbded0b264
Frederico F. de Oliveira <FreddieOliveira@users.noreply.github.com>
2021-01-24 04:19:13 +0000

    docker: fix LDFLAGS for building docker-cli

--- root-packages/docker/build.sh
+++ root-packages/docker/build.sh
@@ -104,6 +104,7 @@ termux_step_make() {
 	# issue the build command
 	export VERSION=v${TERMUX_PKG_VERSION}-ce
 	export DISABLE_WARN_OUTSIDE_CONTAINER=1
+	export LDFLAGS="-L ${TERMUX_PREFIX}/lib -r ${TERMUX_PREFIX}/lib"
 	make -j ${TERMUX_MAKE_PROCESSES} dynbinary
 	unset GOOS GOARCH CGO_LDFLAGS CC CXX CFLAGS CXXFLAGS LDFLAGS
 	make -j ${TERMUX_MAKE_PROCESSES} manpages
