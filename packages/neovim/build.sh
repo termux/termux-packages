07b992be24beaebf28bc76900a95885f606c1be8
shadmansaleh <shadmansaleh3@gmail.com>
2021-01-26 07:14:54 +0600

    Made Neovim build with luajit insted of lua5.3

--- packages/neovim/build.sh
+++ packages/neovim/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=0.4.4
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/neovim/neovim/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2f76aac59363677f37592e853ab2c06151cca8830d4b3fe4675b4a52d41fc42c
-TERMUX_PKG_DEPENDS="libiconv, libuv, luv, libmsgpack, libandroid-support, libvterm, libtermkey, liblua53, libunibilium"
+TERMUX_PKG_DEPENDS="libiconv, libuv, luv, libmsgpack, libandroid-support, libvterm, libtermkey, libluajit, libunibilium"
 TERMUX_PKG_HOSTBUILD=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -17,8 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLUA_PRG=$TERMUX_PKG_HOSTBUILD_DIR/deps/usr/bin/luajit
 -DPKG_CONFIG_EXECUTABLE=$(which pkg-config)
 -DXGETTEXT_PRG=$(which xgettext)
--DPREFER_LUA=ON
--DLUA_INCLUDE_DIR=$TERMUX_PREFIX/include/lua5.3
+-DLUA_INCLUDE_DIR=$TERMUX_PREFIX/include/luajit-1:2
 "
 TERMUX_PKG_CONFFILES="share/nvim/sysinit.vim"
 
