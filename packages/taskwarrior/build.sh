0db330edbe22476a400735834626334a2c579d0f
Michael Bryant <shadow53@shadow53.com>
2021-01-26 20:33:24 -0800

    taskwarrior: remove pkg revision

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Utility for managing your TODO list"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.5.3
-TERMUX_PKG_REVISION=9
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${TERMUX_PKG_VERSION}/task-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7243d75e0911d9e2c9119ad94a61a87f041e4053e197f7280c42410aa1ee963b
 TERMUX_PKG_DEPENDS="libc++, libgnutls, libuuid, libandroid-glob"
