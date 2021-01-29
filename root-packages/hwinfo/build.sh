ff45dfdfe01a0bf5003ae04bb66b7021541fdcc9
Henrik Grimler <henrik@grimler.se>
2021-01-29 07:22:26 +0100

    hwinfo: update to 21.71

--- root-packages/hwinfo/build.sh
+++ root-packages/hwinfo/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/openSUSE/hwinfo
 TERMUX_PKG_DESCRIPTION="Hardware detection tool from openSUSE"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=21.70
+TERMUX_PKG_VERSION=21.71
 TERMUX_PKG_SRCURL=https://github.com/openSUSE/hwinfo/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=bc3c4a4498e4abc32a54497ced715bbae5dfd19dd999da294bca6d69fea2db52
+TERMUX_PKG_SHA256=c4c573eb15cbc10103f5044b485d7e4ff941500ed559743a1c98e6a6deb0ebda
 TERMUX_PKG_DEPENDS="libandroid-shmem, libuuid, libx86emu"
 TERMUX_PKG_BREAKS="hwinfo-dev"
 TERMUX_PKG_REPLACES="hwinfo-dev"
