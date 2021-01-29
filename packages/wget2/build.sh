d5cf89fba7fdc16ef07f921c0a588278b6eea04a
Rabby Sheikh <iamyahik0.1337@gmail.com>
2021-01-29 18:11:24 +0600

    fix typo

--- packages/wget2/build.sh
+++ packages/wget2/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Rabby Sheikh @shoya0x00"
 TERMUX_PKG_VERSION=1.99.2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget2-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cbc48f55fa22ed2acbccf032c208c133cc59c7432cda8518a4992eb5882b6563
-TERMUX_PKG_DEPENDS="openssl, libandroid-glob, libgpg-error, lassuan, gpgme"
+TERMUX_PKG_DEPENDS="openssl, libandroid-glob, libgpg-error, libassuan, gpgme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_header_spawn_h=no --with-ssl=openssl"
 
 termux_step_pre_configure() {
