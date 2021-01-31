ee607a57d0c9a9163c3f6e0e274b92f9e2ec20a9
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-01-31 23:30:50 +0200

    libopenmpt: fix license name
    Bintray requires a specific format of license field, otherwise
    package will not be published.

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://lib.openmpt.org/libopenmpt/
 TERMUX_PKG_DESCRIPTION="OpenMPT based module player library and libopenmpt based command-line player"
-TERMUX_PKG_LICENSE="BSD-3-Clause"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.4
 TERMUX_PKG_SRCURL=https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${TERMUX_PKG_VERSION}+release.autotools.tar.gz
