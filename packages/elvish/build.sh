85bbdd3b78bf2106735a339dfaf174a952a1d973
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-04 20:47:02 +0000

    elvish: update to 0.15.0

--- packages/elvish/build.sh
+++ packages/elvish/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/elves/elvish
 TERMUX_PKG_DESCRIPTION="A friendly and expressive Unix shell"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.14.1
+TERMUX_PKG_VERSION=0.15.0
 TERMUX_PKG_SRCURL=https://github.com/elves/elvish/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=8a8113c0a1325785b212ed59410869bdea30ca2cb7400f95ebb3fbc8689eb6d8
+TERMUX_PKG_SHA256=761739307c68fcbc51fd46c052c0a20ae848a30dba1ce3fbb6d27f99672f58e0
 
 termux_step_make() {
 	termux_setup_golang
