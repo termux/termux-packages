5145fb70bd157b5e5e71969783a4c12a217d4433
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-04 20:08:04 +0000

    docopt: update to 0.6.3

--- packages/docopt/build.sh
+++ packages/docopt/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://docopt.org
 TERMUX_PKG_DESCRIPTION="Command line arguments parser for C++11 and later"
 TERMUX_PKG_LICENSE="BSL-1.0, MIT"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=0.6.2
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.6.3
 TERMUX_PKG_SRCURL=https://github.com/docopt/docopt.cpp/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c05542245232420d735c7699098b1ea130e3a92bade473b64baf876cdf098a17
+TERMUX_PKG_SHA256=28af5a0c482c6d508d22b14d588a3b0bd9ff97135f99c2814a5aa3cbff1d6632
 
 termux_step_install_license() {
 	install -Dm600 -t "$TERMUX_PREFIX/share/doc/docopt" \
