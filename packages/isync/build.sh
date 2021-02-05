f4ef7ea51ead330377a187348ff54768f11e15ea
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-05 21:05:05 +0000

    isync: update to 1.4.0

--- packages/isync/build.sh
+++ packages/isync/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=http://isync.sourceforge.net
 TERMUX_PKG_DESCRIPTION="IMAP and MailDir mailbox synchronizer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_VERSION=1.4.0
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/isync/isync/${TERMUX_PKG_VERSION}/isync-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f2213bf7f90266e1295deafe39b02d1ba0b4c7f3b897c09cd17c60f0d4f4c882
+TERMUX_PKG_SHA256=84f8bf3ed293365d6d73702ae4680077efddf641bf1ef63fccbda0589bde785e
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl ac_cv_header_db=no ac_cv_berkdb4=no"
 TERMUX_PKG_DEPENDS="openssl, zlib"
