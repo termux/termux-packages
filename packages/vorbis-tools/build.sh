1eaf8f1d163c3a3fdc95407cb0aed5d6ad8156de
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-07 21:37:02 +0000

    vorbis-tools: update to 1.4.2

--- packages/vorbis-tools/build.sh
+++ packages/vorbis-tools/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xiph/vorbis-tools
 TERMUX_PKG_DESCRIPTION="Ogg Vorbis tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.0
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=1.4.2
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/vorbis-tools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a389395baa43f8e5a796c99daf62397e435a7e73531c9f44d9084055a05d22bc
+TERMUX_PKG_SHA256=db7774ec2bf2c939b139452183669be84fda5774d6400fc57fde37f77624f0b0
 # libflac for flac support in oggenc:
 TERMUX_PKG_DEPENDS="libiconv, libvorbis, libflac, libogg"
 
