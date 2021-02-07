17629eeacc1a6b99a2d4e57875e50fdce9ae25f7
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-07 20:58:32 +0000

    redis: update to 6.0.10

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://redis.io/
 TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache and message broker"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.0.9
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=6.0.10
 TERMUX_PKG_SRCURL=http://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=dc2bdcf81c620e9f09cfd12e85d3bc631c897b2db7a55218fd8a65eaa37f86dd
+TERMUX_PKG_SHA256=79bbb894f9dceb33ca699ee3ca4a4e1228be7fb5547aeb2f99d921e86c1285bd
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/redis.conf"
 
