bc25f0fd63294eb951a19e3e598032487289f361
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-08 00:14:31 +0200

    gst-plugins-ugly: bump revision

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GStreamer Ugly Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.18.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=70f7429b25dd2f714eb18e80af61b1363b1f63019e16cd28e086e3a619eaa992
 TERMUX_PKG_DEPENDS="gst-plugins-base, libx264"
