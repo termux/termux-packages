db81cf30c5baece63cbdb65e5d9c7c9c265a48ba
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-07 20:03:15 +0000

    minidlna: update to 1.3.0

--- packages/minidlna/build.sh
+++ packages/minidlna/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/azatoth/minidlna
 TERMUX_PKG_DESCRIPTION="A server software with the aim of being fully compliant with DLNA/UPnP-AV clients"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/m/minidlna/minidlna_${TERMUX_PKG_VERSION}+dfsg.orig.tar.xz
-TERMUX_PKG_SHA256=72f688c4dd0412fb7a9389bf4ade3bad773924eae9cb31f510440414af3785a0
+TERMUX_PKG_SHA256=0b536ff6c689973781f23fb9f9decb7f5ab902b39d57e1991789574de8d5ea5c
 TERMUX_PKG_DEPENDS="ffmpeg, libexif, libflac, libid3tag, libjpeg-turbo, libsqlite, libvorbis"
 TERMUX_PKG_BUILD_IN_SRC=true
 
