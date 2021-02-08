cd9bbae4ff52421ce5a92e8af5427f735257a7c6
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-08 17:21:13 +0200

    xorg-util-macros: update to 1.19.3

--- packages/xorg-util-macros/build.sh
+++ packages/xorg-util-macros/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X.Org Autotools macros"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.19.2
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_VERSION=1.19.3
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/util/util-macros-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=d7e43376ad220411499a79735020f9d145fdc159284867e99467e0d771f3e712
+TERMUX_PKG_SHA256=0f812e6e9d2786ba8f54b960ee563c0663ddbe2434bf24ff193f5feab1f31971
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_post_make_install() {
