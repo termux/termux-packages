08e72771608035320a8992a311f26a3a1b250b32
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-08 17:56:07 +0200

    virustotal-cli: update to 0.9.0

--- packages/virustotal-cli/build.sh
+++ packages/virustotal-cli/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/VirusTotal/vt-cli
 TERMUX_PKG_DESCRIPTION="Command line interface for VirusTotal"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.7.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.9.0
 TERMUX_PKG_SRCURL=https://github.com/VirusTotal/vt-cli/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=39a566be0e2ee1102c0bd9d3ddefa4a0e423c9ffe02962d4a48897a875312c95
+TERMUX_PKG_SHA256=ec418c60697d03fd859bbf3a36abe4f30d59111651e3065de6cd581040a19027
 TERMUX_PKG_BREAKS="vt-cli"
 TERMUX_PKG_REPLACES="vt-cli"
 
@@ -17,10 +16,6 @@ termux_step_make() {
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/VirusTotal/vt-cli
 
 	cd "$GOPATH"/src/github.com/VirusTotal/vt-cli
-
-	GOOS=linux GOARCH=amd64 CC=gcc LD=gcc \
-		go get -u github.com/golang/dep/cmd/dep
-	"$GOPATH"/bin/dep ensure
 
 	go build \
 		-ldflags "-X github.com/VirusTotal/vt-cli/cmd.Version=$TERMUX_PKG_VERSION" \
