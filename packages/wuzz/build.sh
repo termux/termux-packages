839761cf7d87041b01cc024a9df2dacf31965a5a
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-08 17:21:07 +0200

    wuzz: update to 0.5.0

--- packages/wuzz/build.sh
+++ packages/wuzz/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/asciimoo/wuzz
 TERMUX_PKG_DESCRIPTION="Interactive command line tool for HTTP inspection"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4.0
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=0.5.0
 TERMUX_PKG_SRCURL=https://github.com/asciimoo/wuzz/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=acf8e82481740d1403b744c58918b9089128d91c3c6edc15b76b6e1c97ead645
+TERMUX_PKG_SHA256=721ea7343698e9f3c69e09eab86b9b1fef828057655f7cebc1de728c2f75151e
 
 termux_step_make() {
 	termux_setup_golang
@@ -15,7 +14,6 @@ termux_step_make() {
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/asciimoo/wuzz
 
 	cd "$GOPATH"/src/github.com/asciimoo/wuzz
-	go get -d -v github.com/asciimoo/wuzz
 	go build
 }
 
