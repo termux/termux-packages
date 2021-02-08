476e7c3d2e79871df0096f485b95b380d7293b9e
Rabby Sheikh <iamyahik0.1337@gmail.com>
2021-02-09 00:00:36 +0600

    new package: wget2 (#6328)

---
+++ packages/wget2/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.com/gnuwget/wget2
+TERMUX_PKG_DESCRIPTION="The successor of GNU Wget, a file and recursive website downloader"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Rabby Sheikh @shoya0x00"
+TERMUX_PKG_VERSION=1.99.2
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget2-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=cbc48f55fa22ed2acbccf032c208c133cc59c7432cda8518a4992eb5882b6563
+TERMUX_PKG_DEPENDS="gpgme, libandroid-glob, libassuan, libgnutls, libgpg-error, pcre2"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_header_spawn_h=no
+-with-openssl=no
+--with-ssl=gnutls
+"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-glob"
+	CFLAGS+=" -DNO_INLINE_GETPASS=1"
+}
