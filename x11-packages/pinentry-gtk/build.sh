e9169e55213317416866b345956798957dca8a91
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-09 20:28:22 +0000

    pinentry-gtk: update to 1.1.1

--- x11-packages/pinentry-gtk/build.sh
+++ x11-packages/pinentry-gtk/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/pinentry/index.html
 TERMUX_PKG_DESCRIPTION="Dialog allowing secure password entry (with gtk2 support)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.1.0
-TERMUX_PKG_REVISION=23
+TERMUX_PKG_VERSION=1.1.1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=68076686fa724a290ea49cdf0d1c0c1500907d1b759a3bcbfbec0293e8f56570
+TERMUX_PKG_SHA256=cd12a064013ed18e2ee8475e669b9f58db1b225a0144debdb85a68cecddba57f
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libandroid-shmem, libassuan, libcairo, libgpg-error, ncurses, pango"
 
 TERMUX_PKG_CONFLICTS="pinentry"
