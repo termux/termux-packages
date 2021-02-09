8944c96bf9459f31b7e643b2bcab199c5b6509f1
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-09 13:09:57 +0200

    age: update to 1.0.0-beta7

--- packages/age/build.sh
+++ packages/age/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/FiloSottile/age
 TERMUX_PKG_DESCRIPTION="A simple, modern and secure encryption tool with small explicit keys, no config options, and UNIX-style composability"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.0-beta6
+TERMUX_PKG_VERSION=1.0.0-beta7
 TERMUX_PKG_SRCURL=https://github.com/FiloSottile/age/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=40cb04ecb0d36ae1007bb15339e731ba8ea86277cf704629a5d3d35744452012
+TERMUX_PKG_SHA256=0ff39751464a5660360575023c9912558f267263f18b10058c27764a3a6806ed
 
 termux_step_make() {
 	termux_setup_golang
