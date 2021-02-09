7acd642d22150dab5527d50d633475e85e8c0473
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-09 20:28:16 +0000

    pcmanfm: update to 1.3.2

--- x11-packages/pcmanfm/build.sh
+++ x11-packages/pcmanfm/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/pcmanfm/
 TERMUX_PKG_DESCRIPTION="Extremely fast and lightweight file manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.3.1
-TERMUX_PKG_REVISION=21
+TERMUX_PKG_VERSION=1.3.2
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pcmanfm/pcmanfm-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=6804043b3ee3a703edde41c724946174b505fe958703eadbd7e0876ece836855
+TERMUX_PKG_SHA256=14cb7b247493c4cce65fbb5902611e3ad00a7a870fbc1e50adc50428c5140cf7
 TERMUX_PKG_DEPENDS="atk, glib, gtk3, libandroid-shmem, libcairo, libfm, libx11, lxmenu-data, pango"
 TERMUX_PKG_RECOMMENDS="xarchiver"
 
