ac03e0122475426acd8b2585382d7e276842eb13
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-09 20:01:01 +0000

    shared-mime-info: update to 2.1

--- x11-packages/shared-mime-info/build.sh
+++ x11-packages/shared-mime-info/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://freedesktop.org/Software/shared-mime-info
 TERMUX_PKG_DESCRIPTION="Freedesktop.org Shared MIME Info"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/${TERMUX_PKG_VERSION}/shared-mime-info-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0884a57f5fb10dfce146312f6d5826f767e47863d8f543ab4ec0228336e468f8
+TERMUX_PKG_SHA256=3aeeee25ad445f257f614ed53837dee79fab70524b56e59b767f0d69e11fdff9
 TERMUX_PKG_DEPENDS="coreutils, glib, libxml2"
 
 termux_step_create_debscripts() {
