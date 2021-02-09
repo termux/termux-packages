79b84f36c5affe1fb346157e66f3c3c9e4d50f8a
Ludea <ludealol.olol@gmail.com>
2021-02-09 19:51:50 +0100

    add icu to harfbuzz

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.7.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=daff8a4003ac420a8550760ed303ce33b310c8ea17b7f15b307d1969cabcebcb
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
 TERMUX_PKG_BREAKS="harfbuzz-dev"
 TERMUX_PKG_REPLACES="harfbuzz-dev"
 TERMUX_PKG_BUILD_DEPENDS="libicu"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgraphite=enabled"
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DHB_HAVE_ICU=ON -DHB_HAVE_GRAPHITE2"
 termux_step_post_get_source() {
+
 	mv CMakeLists.txt CMakeLists.txt.unused
 }
