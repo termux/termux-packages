d69aa0335f496eefe4034d68f9984974fa371515
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-09 20:04:38 +0000

    xfdesktop: update to 4.16.0

--- x11-packages/xfdesktop/build.sh
+++ x11-packages/xfdesktop/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="A desktop manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=4.15.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.16.0
 TERMUX_PKG_SRCURL=https://gitlab.xfce.org/xfce/xfdesktop/-/archive/xfdesktop-${TERMUX_PKG_VERSION}/xfdesktop-xfdesktop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a0ba26b44e7a2e55369bb01aea9b0abe4848f493b4c7807363f6bb640c69126a
+TERMUX_PKG_SHA256=9835d1b532c3a4db010b22b35c6d0870c3037d43deecf58274125e906452268a
 TERMUX_PKG_DEPENDS="exo, garcon, hicolor-icon-theme, libwnck, libxfce4ui, startup-notification, thunar"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-notifications --enable-maintainer-mode"
 
