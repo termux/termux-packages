6a5be9ce372ba722c923828621a452034446d7ba
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-10 22:34:33 +0200

    use git + remove unneeded var

--- packages/libtasn1/build.sh
+++ packages/libtasn1/build.sh
@@ -3,11 +3,9 @@ TERMUX_PKG_DESCRIPTION="This is GNU Libtasn1, a small ASN.1 library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.16.0
-TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1/-/archive/libtasn1_${TERMUX_PKG_VERSION//./_}/libtasn1-libtasn1_${TERMUX_PKG_VERSION//./_}.tar.gz
-TERMUX_PKG_SHA256=4032d6ba64b5d378aba80b777feb1d8b367b5bcd8270f43c7a566a247008c539
-
-TERMUX_PKG_BUILD_DEPENDS="git, texinfo, help2man"
+TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1.git
+TERMUX_PKG_GIT_BRANCH=libtasn1_${TERMUX_PKG_VERSION//./_}
 
 termux_step_post_get_source() {
-  ./bootstrap
+	./bootstrap
 }
