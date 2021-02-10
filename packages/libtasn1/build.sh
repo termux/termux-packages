5b7e66e274a81eca5d64cd05d4258d50269e7f85
Ludea <ludealol.olol@gmail.com>
2021-02-10 22:09:22 +0100

    new package: libtasn1 (#6374)

---
+++ packages/libtasn1/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libtasn1/
+TERMUX_PKG_DESCRIPTION="This is GNU Libtasn1, a small ASN.1 library"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=4.16.0
+TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1.git
+TERMUX_PKG_GIT_BRANCH=libtasn1_${TERMUX_PKG_VERSION//./_}
+
+termux_step_post_get_source() {
+	./bootstrap
+}
