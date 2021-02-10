0e243ce50b64a5d100ad97ae130d6b7c4ba70fdc
Ludea <ludealol.olol@gmail.com>
2021-02-10 19:43:40 +0100

    Right checksum

--- packages/libtasn1/build.sh
+++ packages/libtasn1/build.sh
@@ -4,7 +4,8 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.16.0
 TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1/-/archive/libtasn1_${TERMUX_PKG_VERSION//./_}/libtasn1-libtasn1_${TERMUX_PKG_VERSION//./_}.tar.gz
-TERMUX_PKG_SHA256=2d1b0cbd26edfcb54694b2339106a02a81d630a7dedc357461aeb186874cc7c0
+TERMUX_PKG_SHA256=4032d6ba64b5d378aba80b777feb1d8b367b5bcd8270f43c7a566a247008c539
+
 TERMUX_PKG_BUILD_DEPENDS="texinfo, help2man"
 
 termux_step_post_get_source() {
