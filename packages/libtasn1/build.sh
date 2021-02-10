866b4aa3e52634ad54997c0290d3ec541eada19c
Ludea <ludealol.olol@gmail.com>
2021-02-10 20:12:33 +0100

    Missing deps

--- packages/libtasn1/build.sh
+++ packages/libtasn1/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=4.16.0
 TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1/-/archive/libtasn1_${TERMUX_PKG_VERSION//./_}/libtasn1-libtasn1_${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=4032d6ba64b5d378aba80b777feb1d8b367b5bcd8270f43c7a566a247008c539
 
-TERMUX_PKG_BUILD_DEPENDS="texinfo, help2man"
+TERMUX_PKG_BUILD_DEPENDS="gtk-doc, texinfo, help2man"
 
 termux_step_post_get_source() {
   ./bootstrap
