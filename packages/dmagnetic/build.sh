488e0708ecad89c797f8dda05185d9208f94ce5d
dettus <dettus@dettus.net>
2021-02-10 22:21:12 +0100

    Update build.sh
    Updating to release 0.30

--- packages/dmagnetic/build.sh
+++ packages/dmagnetic/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.dettus.net/dMagnetic
 TERMUX_PKG_DESCRIPTION="Interpreter for classic text adventure games and interactive fiction"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_VERSION=0.29
+TERMUX_PKG_VERSION=0.30
 TERMUX_PKG_SRCURL=https://www.dettus.net/dMagnetic/dMagnetic_${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a980e35f85c9661fe0d98c670f9d6be56000da2bbc8b3e8e78697eac05ae5b47
+TERMUX_PKG_SHA256=9eb825290495150b8ae4eeb0ff04aba724c1fd8f6052b0c4cb90865787f922be
 TERMUX_PKG_BUILD_IN_SRC=true
