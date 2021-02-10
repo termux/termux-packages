2fbd32640723fd5b8878ca7074cd574c455787ca
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-10 20:31:01 +0200

    TERMUX_PKG_MAINTAINER required by all packages

--- packages/libtasn1/build.sh
+++ packages/libtasn1/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libtasn1/
 TERMUX_PKG_DESCRIPTION="This is GNU Libtasn1, a small ASN.1 library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.16.0
 TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1/-/archive/libtasn1_${TERMUX_PKG_VERSION//./_}/libtasn1-libtasn1_${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=2d1b0cbd26edfcb54694b2339106a02a81d630a7dedc357461aeb186874cc7c0
