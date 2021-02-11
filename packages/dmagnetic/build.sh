b7bf4d0d3dbb8ec3651a01070377910582dcc1dd
Henrik Grimler <henrik@grimler.se>
2021-02-11 08:27:07 +0100

    dmagnetic: update patches for 0.30
    Add host_build step to build dMagnetic for host architecture, to avoid
    
      ./dMagnetic -helpini >dMagnetic.ini
      bin/sh: 1: ./dMagnetic: Exec format error

--- packages/dmagnetic/build.sh
+++ packages/dmagnetic/build.sh
@@ -5,3 +5,16 @@ TERMUX_PKG_VERSION=0.30
 TERMUX_PKG_SRCURL=https://www.dettus.net/dMagnetic/dMagnetic_${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=9eb825290495150b8ae4eeb0ff04aba724c1fd8f6052b0c4cb90865787f922be
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	cd $TERMUX_PKG_BUILDDIR
+	make -j $TERMUX_MAKE_PROCESSES dMagnetic
+	mv dMagnetic ../$TERMUX_PKG_HOSTBUILD_DIR/
+	make clean
+}
+
+termux_step_post_configure() {
+	# find our host-built dMagnetic
+	export PATH=$TERMUX_PKG_HOSTBUILD_DIR:$PATH
+}
