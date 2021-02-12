af793907336b3fd3d681d407c0309149c636d1ea
Ludea <ludealol.olol@gmail.com>
2021-02-12 10:20:51 +0100

    gobject-introspection

---
+++ packages/gobject-introspection/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/programming-guidelines/stable/introspection.html.en
+TERMUX_PKG_DESCRIPTION="describe the APIs and collect them in
+a uniform, machine readable format"
+TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.66.1
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/${TERMUX_PKG_VERSION}/gobject-introspection-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=89eabf9dfd246e1bde58fc5589c01be865429bffb9ffd511b531ca9cf6a5ec9d
+TERMUX_PKG_BUILD_DEPENDS="iconv, python" 
