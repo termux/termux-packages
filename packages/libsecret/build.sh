e7ad631f916a73a49a3c4537dd5467a9feabb750
Ludea <ludealol.olol@gmail.com>
2021-02-12 07:41:49 +0100

    Typo

--- packages/libsecret/build.sh
+++ packages/libsecret/build.sh
@@ -2,6 +2,6 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/Libsecret
 TERMUX_PKG_DESCRIPTION="A GObject-based library for accessing the Secret Service API"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TEEMUX_PKG_VERSION=0.20.4
+TERMUX_PKG_VERSION=0.20.4
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/libsecret/-/archive/${TERMUX_PKG_VERSION}/libsecret-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6b1bb3e0efcfcb6e4a8e18b6a5f1cac27cda203d46a7dfbb0f150784a47e908f
