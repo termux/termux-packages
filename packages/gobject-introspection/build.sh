fd69c2dc5a74c7e347d5098eea446ff5c927dc06
Ludea <ludealol.olol@gmail.com>
2021-02-12 14:20:17 +0100

    Typo

--- packages/gobject-introspection/build.sh
+++ packages/gobject-introspection/build.sh
@@ -7,4 +7,4 @@ TERMUX_PKG_VERSION=1.66.1
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/${TERMUX_PKG_VERSION}/gobject-introspection-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=89eabf9dfd246e1bde58fc5589c01be865429bffb9ffd511b531ca9cf6a5ec9d
 TERMUX_PKG_BUILD_DEPENDS="iconv, python" 
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-I/$TERMUX_PREFIX/include/python3.9"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-I$TERMUX_PREFIX/include/python3.9"
