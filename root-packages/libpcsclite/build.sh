230d1d9adfb827a75d282fbe6b8b9ba9ae482d8c
Henrik Grimler <henrik@grimler.se>
2021-02-13 12:09:03 +0100

    libpcsclite: update to 1.9.0

--- root-packages/libpcsclite/build.sh
+++ root-packages/libpcsclite/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://pcsclite.apdu.fr/
 TERMUX_PKG_DESCRIPTION="Middleware to access a smart card using SCard API (PC/SC)."
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_VERSION=1.8.26
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.9.0
 TERMUX_PKG_SRCURL=https://pcsclite.apdu.fr/files/pcsc-lite-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=3eb7be7d6ef618c0a444316cf5c1f2f9d7227aedba7a192f389fe3e7c0dfbbd9
+TERMUX_PKG_SHA256=0148d403137124552c5d0f10f8cdab2cbb8dfc7c6ce75e018faf667be34f2ef9
 TERMUX_PKG_DEPENDS="libusb, python2"
 TERMUX_PKG_BREAKS="libpcsclite-dev"
 TERMUX_PKG_REPLACES="libpcsclite-dev"
