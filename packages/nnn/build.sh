dc254326d68ff974f0760a5fba4713005f4d1101
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-13 02:05:11 +0200

    nnn: bump revision

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e636d4035499a112a0ad33f1557838132ed2e39d8857c5b219714fe9f64681f3
 TERMUX_PKG_DEPENDS="debianutils, file, findutils, readline, wget, libandroid-support"
