64385bb14de2d04e614a1f78edd9f0698a3248ab
Henrik Grimler <henrik@grimler.se>
2021-02-13 12:41:23 +0100

    dopewars: update to 1.6.1

--- packages/dopewars/build.sh
+++ packages/dopewars/build.sh
@@ -1,12 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://dopewars.sourceforge.io
 TERMUX_PKG_DESCRIPTION="Drug-dealing game set in streets of New York City"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.5.12
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/dopewars/dopewars-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=23059dcdea96c6072b148ee21d76237ef3535e5be90b3b2d8239d150feee0c19
-TERMUX_PKG_DEPENDS="ncurses, glib, pcre"
+TERMUX_PKG_SHA256=83127903a61d81cda251a022f9df150d11e27bdd040e858c09c57927cc0edea6
+TERMUX_PKG_DEPENDS="ncurses, glib, pcre, curl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-sdl
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/gnome"
+
+termux_step_pre_configure() {
+	autoreconf -vfi
+}
