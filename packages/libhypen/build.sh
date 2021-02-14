5282a3a70ba23b0ccb6092a9864657ea32dc2849
Ludea <ludealol.olol@gmail.com>
2021-02-14 10:18:39 +0100

    libhypen

---
+++ packages/libhypen/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/hunspell/hyphen
+TERMUX_PKG_DESCRIPTION="hyphenation library to use converted TeX hyphenation patterns"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.8.8-7
+TERMUX_PKG_SRCURL=https://github.com/hunspell/hyphen.git
+TERMUX_PKG_GIT_BRANCH=master
