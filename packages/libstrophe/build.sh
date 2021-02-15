a786330d3cc0dcb45e2ad434ffabdd44a3ae7663
Further <55025025+ifurther@users.noreply.github.com>
2021-02-15 19:39:46 +0800

    new package: libstrophe (#6399)

---
+++ packages/libstrophe/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://strophe.im/libstrophe
+TERMUX_PKG_DESCRIPTION="libstrophe is a minimal XMPP library written in C."
+TERMUX_PKG_LICENSE="MIT, GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.10.1
+TERMUX_PKG_SRCURL=https://github.com/strophe/libstrophe/releases/download/${TERMUX_PKG_VERSION}/libstrophe-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4918c47029ecdea2deab4b0f9336ca4a8bb12c28b72b2cec397d98664b94c771
+TERMUX_PKG_DEPENDS="openssl, libexpat"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	./bootstrap.sh
+}
