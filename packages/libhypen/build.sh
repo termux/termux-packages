36345786ff00f5fb48db6de8a93e08d68275db04
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-16 15:58:23 +0200

    source urls must be versioned

--- packages/libhypen/build.sh
+++ packages/libhypen/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/hunspell/hyphen
 TERMUX_PKG_DESCRIPTION="hyphenation library to use converted TeX hyphenation patterns"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.8.8-7
-TERMUX_PKG_SRCURL=https://github.com/hunspell/hyphen/archive/master.zip
-TERMUX_PKG_SHA256=30d9e6f3eb36e84e9e18561990cfaa12220c348ce4eb3630ab58c294dc73a6fe
+TERMUX_PKG_VERSION=2.8.8
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/hunspell/hyphen-${TERMUX_PKG_SRCURL}.tar.gz
+TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 
 termux_step_pre_configure() {
 	autoreconf -fvi
