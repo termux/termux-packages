4c04ebbf521446199820b1a989e2ec66e5e0166e
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-16 16:19:01 +0200

    Update build.sh

--- packages/libhypen/build.sh
+++ packages/libhypen/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="hyphenation library to use converted TeX hyphenation pat
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.8.8
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/hunspell/hyphen-${TERMUX_PKG_SRCURL}.tar.gz
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/hunspell/hyphen-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 
 termux_step_pre_configure() {
