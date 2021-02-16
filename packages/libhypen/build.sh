b6e3eb27d0d712fe6c2042bb0d420abadc5f42a2
Ludea <ludealol.olol@gmail.com>
2021-02-16 07:47:53 +0100

    autoreconf

--- packages/libhypen/build.sh
+++ packages/libhypen/build.sh
@@ -5,3 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.8.8-7
 TERMUX_PKG_SRCURL=https://github.com/hunspell/hyphen/archive/master.zip
 TERMUX_PKG_SHA256=30d9e6f3eb36e84e9e18561990cfaa12220c348ce4eb3630ab58c294dc73a6fe
+
+termux_step_pre_configure() {
+ autoreconf -fvi
+} 
