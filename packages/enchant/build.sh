22c6c5cd942e8ae98743cdd84a6e4775621cadf6
Ludea <ludealol.olol@gmail.com>
2021-02-16 06:35:25 +0100

    enchant

---
+++ packages/enchant/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://abiword.github.io/enchant/
+TERMUX_PKG_DESCRIPTION="Wraps a number of different spelling libraries and programs with a consistent interface."
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.2.15
+TERMUX_PKG_SRCURL=https://github.com/AbiWord/enchant/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=add272bb09e6384a4833ffca4896350fdb16e0ca22df68c0384773c67a175594
