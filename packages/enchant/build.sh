d803fb377f5a78c15049d1c0831109906eee0b43
Ludea <ludealol.olol@gmail.com>
2021-02-16 08:02:22 +0100

    glib deps

--- packages/enchant/build.sh
+++ packages/enchant/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_VERSION=2.2.15
 TERMUX_PKG_SRCURL=https://github.com/AbiWord/enchant/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=85295934102a4ab94f209cbc7c956affcb2834e7a5fb2101e2db436365e2922d
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-relocatable" 
+TERMUX_PKG_DEPENDS="glib"
+
 termux_step_post_get_source() {
 	./bootstrap
 }
