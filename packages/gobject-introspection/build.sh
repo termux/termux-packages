744bb6a97518cf68d05de784ccb31aaf2dae6680
Ludea <ludealol.olol@gmail.com>
2021-02-17 13:53:35 +0100

    Remove python, using only static python

--- packages/gobject-introspection/build.sh
+++ packages/gobject-introspection/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.66.1
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/${TERMUX_PKG_VERSION}/gobject-introspection-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=89eabf9dfd246e1bde58fc5589c01be865429bffb9ffd511b531ca9cf6a5ec9d
-TERMUX_PKG_DEPENDS="glib, iconv, python, python-static"
+TERMUX_PKG_DEPENDS="glib, iconv, python-static"
