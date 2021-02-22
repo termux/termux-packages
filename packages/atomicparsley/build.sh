f0ff7fb707f1b395cde7d480296c85e3898daf30
Lucy Phipps <landfillbaby69@gmail.com>
2021-02-22 14:53:28 +0000

    atomicparsley: update to 20210124.204813.840499f

--- packages/atomicparsley/build.sh
+++ packages/atomicparsley/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/wez/atomicparsley
 TERMUX_PKG_DESCRIPTION="Read, parse and set metadata of MPEG-4 and 3gp files"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:20210114.184825.1dbe1be
+TERMUX_PKG_VERSION=1:20210124.204813.840499f
 TERMUX_PKG_SRCURL=https://github.com/wez/atomicparsley/archive/${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=8877262c86d0ad231a5b0eaa8ab9c0c1d4e06fafea0b96a819d9a5e565a28b8c
+TERMUX_PKG_SHA256=21e6eb6791b63ba1d06c6ffa9cf40ee81cec80da944e8a29c6dbd11d9de50b28
 TERMUX_PKG_DEPENDS="libc++, zlib"
 
 termux_step_make_install() {
