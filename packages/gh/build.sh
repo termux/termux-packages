f62aee7fc728ae78433a21d67e59a51ae8ea6507
Lucy Phipps <landfillbaby69@gmail.com>
2021-02-23 00:07:54 +0000

    gh: update to 1.6.1

--- packages/gh/build.sh
+++ packages/gh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://cli.github.com/
 TERMUX_PKG_DESCRIPTION="GitHub’s official command line tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=49c42a3b951b67e29bc66e054fedb90ac2519f7e1bfc5c367e82cb173e4bb056
+TERMUX_PKG_SHA256=b942d4daef4250de5b996871be09214309963de64f9513c8f0b039ced99f2950
 
 termux_step_make() {
 	termux_setup_golang
