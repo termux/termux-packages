a161dee7c6e0e4ddfc347b5397fd0d7c505047ed
Henrik Grimler <henrik@grimler.se>
2021-02-25 22:10:12 +0100

    moon-buggy: bump to test github action script

--- packages/moon-buggy/build.sh
+++ packages/moon-buggy/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simple game where you drive a car across the moon's surf
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_VERSION=1.0.51
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 # Main site down 2017-01-06.
 # TERMUX_PKG_SRCURL=http://m.seehuhn.de/programs/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=ftp://ftp.netbsd.org/pub/pkgsrc/distfiles/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz
