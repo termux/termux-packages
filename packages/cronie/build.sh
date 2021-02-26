e01b80c24ffcdf8b7fe6725756fe22699705cc53
jer194 <jerjer194@online.de>
2021-02-26 20:50:17 +0100

    cronie:  create $PREFIX/etc/cron.d (#6461)
    Issue https://github.com/termux/termux-packages/issues/6460.

--- packages/cronie/build.sh
+++ packages/cronie/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Daemon that runs specified programs at scheduled times a
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.5
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://github.com/cronie-crond/cronie/releases/download/cronie-${TERMUX_PKG_VERSION}/cronie-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=be34c79505e5544323281854744b9955ff16b160ee569f9df7c0dddae5720eac
 TERMUX_PKG_DEPENDS="dash"
@@ -24,5 +24,6 @@ termux_step_create_debscripts() {
 	#!$TERMUX_PREFIX/bin/sh
 	mkdir -p $TERMUX_PREFIX/var/run
 	mkdir -p $TERMUX_PREFIX/var/spool/cron
+	mkdir -p $TERMUX_PREFIX/etc/cron.d
 	EOF
 }
