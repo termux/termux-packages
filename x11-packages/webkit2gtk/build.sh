9439bca45bfbd55a9f8c60136b46b429e8d827e2
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-03-01 12:25:00 +0200

    rename package: webkitgtk --> webkit2gtk

--- x11-packages/webkit2gtk/build.sh
+++ x11-packages/webkit2gtk/build.sh
@@ -8,5 +8,5 @@ TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DPORT=GTK -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_GAMEPAD=OFF -DUSE_SYSTEMD=OFF -DUSE_LIBSECRET=OFF -DENABLE_INTROSPECTION=OFF"
 TERMUX_PKG_SHA256=6b1bb3e0efcfcb6e4a8e18b6a5f1cac27cda203d46a7dfbb0f150784a47e908f
 TERMUX_PKG_DEPENDS="libtasn1, libxt, libnotify, libcairo, libgcrypt, gtk3, libsoup, libwebp, libxslt, woff2, enchant, libhyphen, openjpeg, gst-plugins-base, gstreamer"
-TERMUX_PKG_BREAKS="webkit"
-TERMUX_PKG_REPLACES="webkit"
+TERMUX_PKG_BREAKS="webkit, webkitgtk"
+TERMUX_PKG_REPLACES="webkit, webkitgtk"
