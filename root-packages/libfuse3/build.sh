963ca6a291fec34448eb9e8210e5ba1466a830bc
Henrik Grimler <henrik@grimler.se>
2021-03-01 20:29:54 +0100

    libfuse3: update to 3.10.2

--- root-packages/libfuse3/build.sh
+++ root-packages/libfuse3/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/libfuse/libfuse
 TERMUX_PKG_DESCRIPTION="FUSE (Filesystem in Userspace) is an interface for userspace programs to export a filesystem to the Linux kernel"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=3.10.1
+TERMUX_PKG_VERSION=3.10.2
 TERMUX_PKG_SRCURL=https://github.com/libfuse/libfuse/archive/fuse-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d8954e7b4c022c651aa80db3bb4a161437dd285cd5f1a23d0e25f055dcebe00d
+TERMUX_PKG_SHA256=a16f93cc083264afd0d2958a0dc88f24c6c5d40a9f3842c645b1909e13edb75f
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddisable-mtab=true
