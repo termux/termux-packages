c841cdc5081ef8cb13c5a00d1489219678282b76
Saurabh Charde <saurabhchardereal@gmail.com>
2021-03-15 23:29:40 +0530

    direnv: Fix installation location and update to 2.28.0
    direnv installs it binary to $DESTDIR/$BINDIR, where
    $BINDIR=${PREFIX}/bin and $PREFIX=/usr/local
    
    We set $DESTDIR=$TERMUX_PREFIX while leaving direnv's $PREFIX unchanged
    causing the binary to install in $TERMUX_PREFIX/usr/local/bin instead of
    $TERMUX_PREFIX/bin (assuming we aren't installing it in
    $TERMUX_PREFIX/local/bin like most termux packages)
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- packages/direnv/build.sh
+++ packages/direnv/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/direnv/direnv
 TERMUX_PKG_DESCRIPTION="Environment switcher for shell"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.27.0
+TERMUX_PKG_VERSION=2.28.0
 TERMUX_PKG_SRCURL=https://github.com/direnv/direnv/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9dc5ce43c63d9d9ff510c6bcd6ae06f3f2f907347e7cbb2bb6513bfb0f151621
+TERMUX_PKG_SHA256=fa539c63034b6161d8238299bb516dcec79e8905cd43ff2b9559ad6bf047cc93
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
@@ -13,5 +13,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	make install DESTDIR=$TERMUX_PREFIX
+	make install PREFIX=$TERMUX_PREFIX
 }
