0087c4c6d5fdcb0778707defce3caae6a1b69673
tuwile <73517616+Mr-Elliot-Alderson@users.noreply.github.com>
2021-03-17 02:24:00 +0530

    build tree-sitter than cargo install 6

--- packages/neovim/build.sh
+++ packages/neovim/build.sh
@@ -19,7 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DXGETTEXT_PRG=$(which xgettext)
 -DPREFER_LUA=ON
 -DLUA_INCLUDE_DIR=$TERMUX_PREFIX/include/lua5.3
--DTreeSitter_INCLUDE_DIR=$TERMUX_PREFIX/include/tree_sitter
+-DTreeSitter_INCLUDE_DIR=$TERMUX_PREFIX/include
 -DTreeSitter_LIBRARY=$TERMUX_PREFIX/lib
 "
 TERMUX_PKG_CONFFILES="share/nvim/sysinit.vim"
