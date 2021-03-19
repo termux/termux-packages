ef842443296346f7f24f49404787ee7a1ec49300
marcusz <65075766+WMCB-Tech@users.noreply.github.com>
2021-03-19 14:06:58 +0800

    Some Fixes

--- packages/squashfs-tools-ng/build.sh
+++ packages/squashfs-tools-ng/build.sh
@@ -5,12 +5,15 @@ TERMUX_PKG_MAINTAINER="WMCB-Tech @marcusz"
 TERMUX_PKG_VERSION="1.0.4"
 TERMUX_PKG_SRCURL="https://github.com/AgentD/squashfs-tools-ng/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="addcd60f94971b7f7d7f65a973efa19714d273515a4f4bfc90138b0db0a4c9e9"
+TERMUX_PKG_DEPENDS="zlib, liblz4, liblzo, zstd"
 
-termux_step_make_install(){
+termux_step_pre_configure(){
 	cd $TERMUX_PKG_SRCDIR
 	./autogen.sh
 	./configure --prefix=$TERMUX_PREFIX
+}
 
+termux_step_make_install(){
 	make
 	make install
 }
