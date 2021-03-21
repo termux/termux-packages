ebf4e36990881b31cffcb1d16caf79066aa52c10
Saurabh Charde <saurabhchardereal@gmail.com>
2021-03-21 18:10:33 +0530

    fish: Update to 3.2.1

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://fishshell.com/
 TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.2.0
+TERMUX_PKG_VERSION=3.2.1
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/releases/download/$TERMUX_PKG_VERSION/fish-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4f0293ed9f6a6b77e47d41efabe62f3319e86efc8bf83cc58733044fbc6f9211
+TERMUX_PKG_SHA256=d8e49f4090d3778df17dd825e4a2a80192015682423cd9dd02b6675d65c3af5b
 # fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line.
 # man is needed since fish calls apropos during command completion.
 TERMUX_PKG_DEPENDS="libc++, ncurses, libandroid-support, ncurses-utils, man, bc, pcre2, libandroid-spawn"
