054395ea7f39ea773b9f307313c9d633feecd09f
Saurabh Charde <saurabhchardereal@gmail.com>
2021-03-22 20:47:07 +0530

    fzf: Update to 0.26.0
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- packages/fzf/build.sh
+++ packages/fzf/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/junegunn/fzf
 TERMUX_PKG_DESCRIPTION="Command-line fuzzy finder"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.25.1
+TERMUX_PKG_VERSION=0.26.0
 TERMUX_PKG_SRCURL=https://github.com/junegunn/fzf/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b97cf9ab528391a49dfa45b459c767fb2626ade9f3a3f99d0108d7274f2eca8b
+TERMUX_PKG_SHA256=a8dc01f16b3bf453fdc9e9a2cd0ca39db7a1b44386517bb7859805b053aa7810
 
 # Depend on findutils as fzf uses the -fstype option, which busybox
 # find does not support, when invoking find:
