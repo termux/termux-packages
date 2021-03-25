960f0b3753fc75f81358b3dd9fdf39bbac74e5b4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-03-25 23:30:27 +0200

    move from unstable: gopass
    https://github.com/termux/termux-packages/issues/6561

---
+++ packages/gopass/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/gopasspw/gopass
+TERMUX_PKG_DESCRIPTION="The slightly more awesome standard unix password manager for teams."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
+TERMUX_PKG_VERSION=1.12.4
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=https://github.com/gopasspw/gopass/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1e3d7ad389a8462256ff7e439de1fbed8012c38ec33bded63b11af74f0525279
+TERMUX_PKG_DEPENDS="git, gnupg"
+TERMUX_PKG_SUGGESTS="termux-api, openssh"
+
+termux_step_make() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+
+	mkdir -p ./src
+	mkdir -p ./src/github.com/gopasspw
+	ln -sf "$TERMUX_PKG_SRCDIR" ./src/github.com/gopasspw/gopass
+
+	rm -f ./src/github.com/gopasspw/gopass/gopass
+	make -C ./src/github.com/gopasspw/gopass build CLIPHELPERS="-X github.com/gopasspw/gopass/pkg/clipboard.Helpers=termux-api'"
+	install -Dm700 \
+		./src/github.com/gopasspw/gopass/gopass \
+		"$TERMUX_PREFIX"/bin/gopass
+}
