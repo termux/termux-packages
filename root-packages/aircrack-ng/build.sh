ee84a8911dda04fdf2c41735a9d4fd80ad7eedc7
Henrik Grimler <henrik@grimler.se>
2021-03-28 15:45:07 +0200

    aircrack-ng: create library symlink
    Partial fix for https://github.com/termux/termux-root-packages/issues/230

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="WiFi security auditing tools suite"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1:1.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/aircrack-ng/archive/${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=c9e7894ba30f5e45b8a20ec43b4599225ac739a795a5bdb98e3f1bbb854d54f7
 TERMUX_PKG_DEPENDS="libc++, libnl, openssl, libpcap, pciutils, ethtool"
@@ -14,4 +14,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
 
 termux_step_pre_configure() {
 	NOCONFIGURE=1 ./autogen.sh
+}
+
+termux_step_post_make_install() {
+	ln -sf libaircrack-ce-wpa-1.6.0.so $TERMUX_PREFIX/lib/libaircrack-ce-wpa.so
 }
