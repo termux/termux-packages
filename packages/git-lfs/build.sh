892836d693cb43c3052f8760a36228d9d4186dde
Lucy Phipps <landfillbaby69@gmail.com>
2021-04-01 11:09:42 +0100

    git-lfs: update to 2.13.3

--- packages/git-lfs/build.sh
+++ packages/git-lfs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://git-lfs.github.com/
 TERMUX_PKG_DESCRIPTION="Git extension for versioning large files"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.13.2
+TERMUX_PKG_VERSION=2.13.3
 TERMUX_PKG_SRCURL=https://github.com/git-lfs/git-lfs/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=782e6275df9ca370730945112e16a0b8c64b9819f0b61fae52ba1ebbc8dce2d5
+TERMUX_PKG_SHA256=f8bd7a06e61e47417eb54c3a0db809ea864a9322629b5544b78661edab17b950
 
 termux_step_make() {
 	termux_setup_golang
