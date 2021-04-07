aa078ae94aa25644be8f7b8006823af727bb21c2
Further <55025025+ifurther@users.noreply.github.com>
2021-04-08 00:44:24 +0800

    new package: jove (#6569)

---
+++ packages/jove/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://directory.fsf.org/wiki/Jove
+TERMUX_PKG_DESCRIPTION="Jove is a compact, powerful, Emacs-style text-editor."
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=4.17.3.7
+TERMUX_PKG_SRCURL=https://github.com/jonmacs/jove/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b3918b94cc7fa973c7ebaf17cae25bf9643281ed4129265155abc3837ddf22bf
+TERMUX_PKG_DEPENDS="ncurses, ncurses-utils"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_LICENSE_FILE=LICENSE
