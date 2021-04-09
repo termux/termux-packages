b5dcfcb1caa4954279bf159aabd6c41193eab600
Matthias Beyer <mail@beyermatthias.de>
2021-04-09 16:01:59 +0200

    neomutt: Add notmuch as dependency

--- packages/neomutt/build.sh
+++ packages/neomutt/build.sh
@@ -5,12 +5,13 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=20210205
 TERMUX_PKG_SRCURL=https://github.com/neomutt/neomutt/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=77e177780fc2d8abb475d9cac4342c7e61d53c243f6ce2f9bc86d819fc962cdb
-TERMUX_PKG_DEPENDS="gdbm, krb5, libiconv, libsasl, ncurses, openssl, zlib, zstd"
+TERMUX_PKG_DEPENDS="gdbm, krb5, libiconv, libsasl, ncurses, notmuch, openssl, zlib, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/neomuttrc"
 
 termux_step_configure() {
 	./configure --host=$TERMUX_HOST_PLATFORM \
 		--prefix=$TERMUX_PREFIX --with-mailpath=$PREFIX/var/mail \
+		--notmuch \
 		--disable-gpgme --disable-idn --zstd --sasl --ssl --gdbm --gss
 }
