9179e35917caccae4d666c8d1532b23c720eef47
Henrik Grimler <henrik@grimler.se>
2021-04-12 21:50:39 +0200

    0verkill: track @ravener's repository
    Closes https://github.com/termux/game-packages/issues/73

--- packages/0verkill/build.sh
+++ packages/0verkill/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Bloody 2D action deathmatch-like game in ASCII-ART"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.16-git
-_COMMIT=522f11a3e40670bbf85e0fada285141448167968
-TERMUX_PKG_SRCURL=https://github.com/hackndev/0verkill/archive/${_COMMIT}.tar.gz
-TERMUX_PKG_SHA256=91360d5e248ef3b883c6fc994a1564ef5f863b663d1a6f0f751122384b845bb8
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/ravener/0verkill/archive/refs/tags/v${TERMUX_PKG_VERSION:0:4}.tar.gz
+TERMUX_PKG_SHA256=d337e4a7dd91f26c837e96492d960c7fd77c75bc24bcc6ed8d350df39edf8bb8
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
