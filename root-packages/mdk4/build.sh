843d66d21bc1646ef8a5e126d11ff1bc54b885df
gitdebpro <81520025+gitdebpro@users.noreply.github.com>
2021-04-17 18:54:47 +0530

    Added MDK4 for Termux (#236)

---
+++ root-packages/mdk4/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_HOMEPAGE=https://github.com/aircrack-ng/mdk4
+TERMUX_PKG_DESCRIPTION="MDK is a proof-of-concept tool to exploit common IEEE 802.11 protocol weaknesses."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=4.1
+TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/mdk4/archive/fc03b0ece893fd8d3f546d915511f01ba3faebc8.zip
+TERMUX_PKG_SHA256=fdbefff87b29710374e7bef3682e36dd5cda074091447fd7300bb26ac8a62839
+TERMUX_PKG_DEPENDS="aircrack-ng, libnl, libpcap"
+TERMUX_PKG_BUILD_IN_SRC=true
