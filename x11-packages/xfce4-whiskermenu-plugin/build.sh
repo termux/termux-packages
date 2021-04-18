2ed9e08b62baa4121b8a99aa1eed4d9004f24ec3
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-04-18 17:22:41 +0300

    xfce4-whiskermenu-plugin: fix maintainer

--- x11-packages/xfce4-whiskermenu-plugin/build.sh
+++ x11-packages/xfce4-whiskermenu-plugin/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-whiskermenu-plugin/start
 TERMUX_PKG_DESCRIPTION="Alternate menu plugin for the Xfce desktop environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=2.5.3
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-whiskermenu-plugin/2.5/xfce4-whiskermenu-plugin-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=39cc34c8a83381997c6faaacb6bf792339234303438a1fccd15c9a1770b87daf
