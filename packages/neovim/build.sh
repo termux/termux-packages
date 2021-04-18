9dc764f0c75c5b271d51b61fd4d77732ffb7b705
shadmansaleh <shadmansaleh3@gmail.com>
2021-04-18 17:26:47 +0600

    Build neovim with luajit

--- packages/neovim/build.sh
+++ packages/neovim/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=0.4.4
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/neovim/neovim/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2f76aac59363677f37592e853ab2c06151cca8830d4b3fe4675b4a52d41fc42c
-TERMUX_PKG_DEPENDS="libiconv, libuv, luv, libmsgpack, libandroid-support, libvterm, libtermkey, liblua53, libunibilium"
+TERMUX_PKG_DEPENDS="libiconv, libuv, luv, libmsgpack, libandroid-support, libvterm, libtermkey, libluajit, libunibilium"
 TERMUX_PKG_HOSTBUILD=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -14,11 +14,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DGETTEXT_MSGFMT_EXECUTABLE=$(which msgfmt)
 -DGETTEXT_MSGMERGE_EXECUTABLE=$(which msgmerge)
 -DGPERF_PRG=$TERMUX_PKG_HOSTBUILD_DIR/deps/usr/bin/gperf
--DLUA_PRG=$TERMUX_PKG_HOSTBUILD_DIR/deps/usr/bin/luajit
+-DLUAJIT_PRG=$TERMUX_PKG_HOSTBUILD_DIR/deps/usr/bin/luajit
 -DPKG_CONFIG_EXECUTABLE=$(which pkg-config)
 -DXGETTEXT_PRG=$(which xgettext)
--DPREFER_LUA=ON
--DLUA_INCLUDE_DIR=$TERMUX_PREFIX/include/lua5.3
+-DLUAJIT_INCLUDE_DIR=$TERMUX_PREFIX/include/luajit-1:2
+-DUSE_BUNDLED_LUAJIT=OFF
+-DUSE_BUNDLED_LIBUV=OFF
+-DUSE_BUNDLED_LIBTERMKEY=OFF
+-DUSE_BUNDLED_UNIBILIUM=OFF
+-DUSE_BUNDLED_MSGPACK=OFF
+-DUSE_BUNDLED_LIBVTERM=OFF
 "
 TERMUX_PKG_CONFFILES="share/nvim/sysinit.vim"
 
