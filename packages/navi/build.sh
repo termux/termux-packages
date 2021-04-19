33f8467a85c26fd8c14ca26860bd7bd955c474aa
Denis Isidoro <denis.isidoro@uber.com>
2021-04-19 14:57:04 -0300

    Add package for navi

---
+++ packages/navi/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/denisidoro/navi
+TERMUX_PKG_DESCRIPTION="An interactive cheatsheet tool for the command-line"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.16.0
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/denisidoro/navi/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f4767e4ad833c16be556d690b2cac0c9bf0a3ddfc4b782a832f6f1f1c3add9c0
+TERMUX_PKG_BUILD_IN_SRC=true
