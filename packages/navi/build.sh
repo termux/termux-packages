c055a48ecb902e9a4f9f8fa2399138dde9149719
Denis Isidoro <denis.isidoro@uber.com>
2021-04-20 09:07:20 -0300

    Fix build

--- packages/navi/build.sh
+++ packages/navi/build.sh
@@ -7,3 +7,13 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/denisidoro/navi/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f4767e4ad833c16be556d690b2cac0c9bf0a3ddfc4b782a832f6f1f1c3add9c0
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	rm -f Makefile
+}
+
+termux_step_make_install() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/navi
+}
