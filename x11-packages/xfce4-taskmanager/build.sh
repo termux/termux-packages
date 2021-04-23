323e032a0d807704bf95dc1cf9d7e99a92cf8744
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-23 19:45:19 +0530

    new package:xfce4-taskmanager (#283)

---
+++ x11-packages/xfce4-taskmanager/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-taskmanager/start
+TERMUX_PKG_DESCRIPTION="Easy to use task manager"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-taskmanager/1.5/xfce4-taskmanager-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=bd25143f47a29000b4148874863dffa521b1a37cb01dbc026f423ea3160f9a35
+TERMUX_PKG_DEPENDS="xfce4-panel, libxmu"
