6ce93701a02117c994f52e27e76cccf9d7ce7b05
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-23 19:44:36 +0530

    new package: xfce4-clipman-plugin (#282)

---
+++ x11-packages/xfce4-clipman-plugin/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-clipman-plugin/start
+TERMUX_PKG_DESCRIPTION="Clipman is a clipboard manager for Xfce"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.6.1
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-clipman-plugin/1.6/xfce4-clipman-plugin-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=b3e8317796289196fa78c4526626fe061fd35d11e848cf300ca1ace47febceb4
+TERMUX_PKG_DEPENDS="libqrencode, libxtst, xfce4-panel"
