2cf5805d1df83a5fb172e60afcfbfb9e45094a1c
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-24 22:46:00 +0530

    new package: ristretto (#285)

---
+++ x11-packages/ristretto/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/ristretto/start
+TERMUX_PKG_DESCRIPTION="The Ristretto Image Viewer is an application that can be used to view, and scroll through images."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.10.0
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/ristretto/0.10/ristretto-0.10.0.tar.bz2
+TERMUX_PKG_SHA256=16225dd74245eb6e0f82b9c72c6112f161bb8d8b11f3fc77277b7bc3432d4769
+TERMUX_PKG_DEPENDS="gtk3, libexif, libxfce4util, libxfce4ui"
+TERMUX_PKG_RECOMMENDS="tumbler"
