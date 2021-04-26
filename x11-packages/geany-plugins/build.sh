9662c8561d80bb45a758cd8751ecc04ecc300ef8
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-26 14:02:46 +0530

    new package: geany-plugins (#288)

---
+++ x11-packages/geany-plugins/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://plugins.geany.org/install.html
+TERMUX_PKG_DESCRIPTION="This is a plug-in for the editor Geany"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@suhan-paradkar"
+TERMUX_PKG_VERSION=1.37
+TERMUX_PKG_SRCURL=https://plugins.geany.org/geany-plugins/geany-plugins-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=51a76b098232146f47d94cd93a6bd2b5988a22f2a5b3dc0830c48e85af012a8a
+TERMUX_PKG_DEPENDS="geany"
