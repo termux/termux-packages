170b2f6041bdc4ac060bbfed9c6dba7299212ab4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-04-26 13:56:34 +0300

    unset revision instead of using value "0"

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="GStreamer Ugly Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.18.4
-TERMUX_PKG_REVISION=0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=218df0ce0d31e8ca9cdeb01a3b0c573172cc9c21bb3d41811c7820145623d13c
 TERMUX_PKG_DEPENDS="gst-plugins-base, libx264"
