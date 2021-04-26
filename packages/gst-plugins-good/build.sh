3071839726d956250b4dff9b1c860bc7d52ec661
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-04-26 13:56:16 +0300

    unset revision instead of using value "0"

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.18.4
-TERMUX_PKG_REVISION=0
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b6e50e3a9bbcd56ee6ec71c33aa8332cc9c926b0c1fae995aac8b3040ebe39b0
 TERMUX_PKG_DEPENDS="gst-plugins-base, libandroid-shmem, libcaca, libsoup, libjpeg-turbo, libpng, libflac, libbz2, libvpx, pulseaudio, libmp3lame, gstreamer, libogg, zlib"
