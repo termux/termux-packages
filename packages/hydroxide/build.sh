af3052c490f742b6f595e51d87d34ed24fd6c91b
suhan-paradkar <12suhangp34@gmail.com>
2021-04-27 04:36:54 +0000

    Update hydroxide

--- packages/hydroxide/build.sh
+++ packages/hydroxide/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/emersion/hydroxide
 TERMUX_PKG_DESCRIPTION="A third-party, open-source ProtonMail CardDAV, IMAP and SMTP bridge"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.2.17
+TERMUX_PKG_VERSION=0.2.18
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/emersion/hydroxide/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d1e24ce95c181fdad8cef78dc93ed6ba259302a21c0b160b7552ffc6b346bde8
+TERMUX_PKG_SHA256=93865423bad0e37fd231d5ee52f9b98d63b6950cfc664e2d557fc579681a400b
 
 termux_step_make() {
 	termux_setup_golang
