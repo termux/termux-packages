0572410fbb39b477bc6cf1a48e9f25b76c604f6c
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-29 15:59:27 +0530

    update packages (#6703)
    asciidoctor: 2.0.15
    
    dpkg: 1.20.9
    
    elixir: 1.11.4
    
    findomain: 4.0.1
    
    fish: 3.2.2
    
    gnunet: 0.14.1

--- packages/findomain/build.sh
+++ packages/findomain/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://findomain.app/
 TERMUX_PKG_DESCRIPTION="Findomain is the fastest subdomain enumerator and the only one written in Rust"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_VERSION=4.0.1
 TERMUX_PKG_SRCURL=https://github.com/Findomain/Findomain/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7bbedd088a64557876ccd7e7da92f1f8a5b67cfed1aaf335cd5d2b2a5f1824ad
+TERMUX_PKG_SHA256=4ef2f88beb87d7af7f53470df77f556e6d00a0c8745da4066902e98c47e8451d
 TERMUX_PKG_BUILD_IN_SRC=true
-# Compilation error related to assembly code.
-TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+
--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://fishshell.com/
 TERMUX_PKG_DESCRIPTION="Shell geared towards interactive use"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.2.1
+TERMUX_PKG_VERSION=3.2.2
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/releases/download/$TERMUX_PKG_VERSION/fish-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d8e49f4090d3778df17dd825e4a2a80192015682423cd9dd02b6675d65c3af5b
+TERMUX_PKG_SHA256=5944da1a8893d11b0828a4fd9136ee174549daffb3d0adfdd8917856fe6b4009
 # fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line.
 # man is needed since fish calls apropos during command completion.
 TERMUX_PKG_DEPENDS="libc++, ncurses, libandroid-support, ncurses-utils, man, bc, pcre2, libandroid-spawn"
