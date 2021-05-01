86aa4a557b7a8a6893231c03fdacaf77d3abd8a9
Ajeet D'Souza <98ajeet@gmail.com>
2021-05-02 04:57:38 +0530

    Update zoxide

--- packages/zoxide/build.sh
+++ packages/zoxide/build.sh
@@ -2,7 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ajeetdsouza/zoxide
 TERMUX_PKG_DESCRIPTION="A faster way to navigate your filesystem"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_VERSION=0.7.0
 TERMUX_PKG_SRCURL=https://github.com/ajeetdsouza/zoxide/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=62b7a2ced73d5ac0a183b3855d54d6619166b4d8d8c74299bb610265ccc4b193
+TERMUX_PKG_SHA256=e9d97ce48f67ebcb99d4a880789128a79634573f48e8402b053f0c833ddcbca8
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_make_install() {
+  # Install man page:
+  mkdir -p $TERMUX_PREFIX/share/man/man1/
+  cp $TERMUX_PKG_SRCDIR/man/* $TERMUX_PREFIX/share/man/man1/
+}
