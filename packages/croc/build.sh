0a585ea75da75f05708102d5a8eebb1fd67de2b3
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-03 18:10:35 +0000

    croc: update to 9.1.1

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another."
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=9.1.0
+TERMUX_PKG_VERSION=9.1.1
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=575069fc1c966518e66fafae50415811133398179182310ee710708e36a08d89
+TERMUX_PKG_SHA256=8afe88b30c784c1015a81755e4dbd53b2354c5e53f0eb99464db891c416663e7
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
