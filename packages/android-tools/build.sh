46c77e5ab6a6e9f8525f95d26743113048436aa7
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-03 17:51:10 +0000

    android-tools: update to 31.0.0p1

--- packages/android-tools/build.sh
+++ packages/android-tools/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://developer.android.com/
 TERMUX_PKG_DESCRIPTION="Android platform tools"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=30.0.5p1
+TERMUX_PKG_VERSION=31.0.0p1
 TERMUX_PKG_SRCURL=https://github.com/nmeum/android-tools/releases/download/$TERMUX_PKG_VERSION/android-tools-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=8400387db0ff3c7d030418b9f63ea171917c26e6bcc82d9dc666c8c4c02e9806
+TERMUX_PKG_SHA256=51a4c3ba5f16945905449c4bd2c1c781a4df7469f6b7362f8837f4f640d8c7b6
 TERMUX_PKG_DEPENDS="libc++, libusb, pcre2, googletest, libprotobuf, brotli, zstd, liblz4"
 
 termux_step_pre_configure() {
