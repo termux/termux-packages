3e0a4fcea49b3850f3939c89d74691146a0e8419
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-04 17:00:23 +0000

    libopenmpt: update to 0.5.8

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://lib.openmpt.org/libopenmpt/
 TERMUX_PKG_DESCRIPTION="OpenMPT based module player library and libopenmpt based command-line player"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.5.5
+TERMUX_PKG_VERSION=0.5.8
 TERMUX_PKG_SRCURL=https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${TERMUX_PKG_VERSION}+release.autotools.tar.gz
-TERMUX_PKG_SHA256=f1e01483ebf1a680d9ec030c9af20f5f2a5ac0f1e0642c18bd5593cfaa9ceb6b
+TERMUX_PKG_SHA256=29e2c21174b73f67f2ba5ee76808d62f182b130e4f704ee2d9ae8283982d8acd
 TERMUX_PKG_DEPENDS="libflac, mpg123, pulseaudio"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
