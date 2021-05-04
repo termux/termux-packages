6b8a168861b8508447231530f821efd48fe28fb5
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-04 14:13:47 +0000

    fossil: update to 2.15.1

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tick
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT-BSD2.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.14
+TERMUX_PKG_VERSION=2.15.1
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b8d0c920196dd8ae29152fa7448e513a1fa7c588871b785e3fbfc07b42a05fb9
+TERMUX_PKG_SHA256=80d27923c663b2a2c710f8ae8cd549862e04f8c04285706274c34ae3c8ca17d1
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
