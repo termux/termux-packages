1f8a3a9149ea65738c519e9feb516009a60b48e4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-04 18:01:38 +0000

    privoxy: update to 3.0.32

--- packages/privoxy/build.sh
+++ packages/privoxy/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.privoxy.org
 TERMUX_PKG_DESCRIPTION="Non-caching web proxy with advanced filtering capabilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.31
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.0.32
 TERMUX_PKG_SRCURL=https://www.privoxy.org/sf-download-mirror/Sources/$TERMUX_PKG_VERSION%20%28stable%29/privoxy-$TERMUX_PKG_VERSION-stable-src.tar.gz
-TERMUX_PKG_SHA256=077729a3aac79222a4e8d88a650d9028d16fd4b0d6038da8f5f5e47120d004eb
+TERMUX_PKG_SHA256=c61de4008c62445ec18f1f270407cbf2372eaba93beaccdc9e3238bb2defeed7
 # Termux-services adds the run scripts to TERMUX_PKG_CONFFILES. Those ones can not be copied in termux_step_post_massage so setup special variable for that
 DEFAULT_CONFFILES='etc/privoxy/config etc/privoxy/match-all.action etc/privoxy/trust etc/privoxy/user.action etc/privoxy/user.filter etc/privoxy/default.action etc/privoxy/default.filter'
 TERMUX_PKG_CONFFILES=$DEFAULT_CONFFILES
