819b17a34724b7d0c99d5a0ca9677aac4326dcba
Henrik Grimler <henrik@grimler.se>
2021-05-05 21:09:10 +0200

    starship: add --features http arg and remove patch
    Following discussion in
    https://github.com/termux/termux-packages/pull/6501.

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prom
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.53.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=42353977aba8cf711591381d713814140d9bb602015e8d6c589ef1a42d116ae1
-TERMUX_PKG_DEPENDS="zlib"
+TERMUX_PKG_DEPENDS="zlib, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features http"
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
 
 termux_step_pre_configure() {
@@ -16,4 +17,3 @@ termux_step_pre_configure() {
 		CFLAGS+=" -fno-integrated-as"
 	fi
 }
-
