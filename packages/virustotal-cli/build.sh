4a75c55f12587acd55affbd1cbb1eb6a631ee9f6
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-05 17:45:47 +0000

    virustotal-cli: update to 0.9.4

--- packages/virustotal-cli/build.sh
+++ packages/virustotal-cli/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/VirusTotal/vt-cli
 TERMUX_PKG_DESCRIPTION="Command line interface for VirusTotal"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.9.0
+TERMUX_PKG_VERSION=0.9.4
 TERMUX_PKG_SRCURL=https://github.com/VirusTotal/vt-cli/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ec418c60697d03fd859bbf3a36abe4f30d59111651e3065de6cd581040a19027
+TERMUX_PKG_SHA256=b3c097b79067a251f0175d0e6ac7cb12431fb707e84ac9d0e6ef188707211bb0
 TERMUX_PKG_BREAKS="vt-cli"
 TERMUX_PKG_REPLACES="vt-cli"
 
