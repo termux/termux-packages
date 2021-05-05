4702d9cbaa970fa6f280debd07013ea7a0094a78
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-05 20:21:14 +0000

    xfce4-settings: update to 4.16.1

--- x11-packages/xfce4-settings/build.sh
+++ x11-packages/xfce4-settings/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Settings manager for XFCE environment"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=4.16.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.16.1
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/xfce4-settings/${TERMUX_PKG_VERSION:0:4}/xfce4-settings-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=67a1404fc754c675c6431e22a8fe0e5d79644fdfadbfe25a4523d68e1442ddc2
+TERMUX_PKG_SHA256=bb28e1be7aa34d0edb1cfbaacc509a4267db56828b36cd6be312a202973635c6
 TERMUX_PKG_DEPENDS="exo, garcon, libnotify, libxfce4ui, libxklavier, lxde-icon-theme"
 
 TERMUX_PKG_RM_AFTER_INSTALL="
