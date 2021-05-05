9df3344e29908f0e23306c71628ce67f624afc6d
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-05 14:38:06 +0000

    sops: update to 3.7.1

--- packages/sops/build.sh
+++ packages/sops/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mozilla/sops
 TERMUX_PKG_DESCRIPTION="Simple and flexible tool for managing secrets"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="Philipp Schmitt @pschmitt"
-TERMUX_PKG_VERSION=3.6.1
+TERMUX_PKG_VERSION=3.7.1
 TERMUX_PKG_SRCURL="https://github.com/mozilla/sops/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=bb6611eb70580ff74a258aa8b9713fdcb9a28de5a20ee716fe6b516608a60237
+TERMUX_PKG_SHA256=536ee140d888b53b71c1e8edd669f4c11bc573428983fbea644fbbfcd7d7079a
 
 termux_step_make_install() {
 	termux_setup_golang
