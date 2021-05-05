7de945984b1babbd780d86cb01929753485fb1b3
Max Ferrer <maxi.fg13@gmail.com>
2021-05-04 21:42:19 -0300

    new package: macchina (#6744)

---
+++ packages/macchina/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Macchina-CLI/macchina
+TERMUX_PKG_DESCRIPTION="A system information fetcher, with an emphasis on performance and minimalism."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Max Ferrer @PandaFoss"
+TERMUX_PKG_VERSION=0.7.4
+TERMUX_PKG_SRCURL=https://github.com/Macchina-CLI/macchina/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=66349e2274c48d8eb1ac1a627eb16dca0ee96e62c2fa34f9d9fadbe75a029d9f
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
+
+termux_step_make_install() {
+	termux_setup_rust
+	
+	cargo build --jobs ${TERMUX_MAKE_PROCESSES} --target ${CARGO_TARGET_NAME} --release
+	install -Dm755 -t ${TERMUX_PREFIX}/bin target/${CARGO_TARGET_NAME}/release/macchina
+}
