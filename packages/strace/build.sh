2c663cf0e55b7b3dc6b0f4d57516e704c16d8045
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-05 15:39:09 +0000

    strace: update to 5.12

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.11
+TERMUX_PKG_VERSION=5.12
 TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=ffe340b10c145a0f85734271e9cce56457d23f21a7ea5931ab32f8cf4e793879
+TERMUX_PKG_SHA256=29171edf9d252f89c988a4c340dfdec662f458cb8c63d85431d64bab5911e7c4
 TERMUX_PKG_DEPENDS="libdw"
 
 # Without st_cv_m32_mpers=no the build fails if gawk is installed.
