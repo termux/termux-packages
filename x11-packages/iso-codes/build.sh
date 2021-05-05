043351a460aa06d9337d8a20593f18a13f439ae4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-05 20:16:34 +0000

    iso-codes: update to 4.6.0

--- x11-packages/iso-codes/build.sh
+++ x11-packages/iso-codes/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://salsa.debian.org/iso-codes-team/iso-codes
 TERMUX_PKG_DESCRIPTION="Lists of the country, language, and currency names"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=4.5.0
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=4.6.0
 TERMUX_PKG_SRCURL=https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/iso-codes-$TERMUX_PKG_VERSION/iso-codes-iso-codes-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=94b53580571a9ea47ea00fa476614f506083517195be1ba26faadb4813855b95
+TERMUX_PKG_SHA256=c1f5204d4913fba25fdcae6ff5fe9606135717301f99c9ab0b225235023e7d9f
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 #If you install ISO codes over a previous installed version, the install step will fail when creating some symlinks
