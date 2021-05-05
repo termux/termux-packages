b0fd553f6dc7651cbcf838d6f54aa3e42aecd90d
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-05 15:04:21 +0000

    rlwrap: update to 0.45

--- packages/rlwrap/build.sh
+++ packages/rlwrap/build.sh
@@ -2,13 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/hanslub42/rlwrap
 TERMUX_PKG_DESCRIPTION="Wrapper using readline to enable editing of keyboard input for commands"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.44
+TERMUX_PKG_VERSION=0.45
 TERMUX_PKG_SRCURL=https://fossies.org/linux/privat/rlwrap-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8a57524e47cc5eaa00ddc438690cb3cbda9348b35c015f17558cd0061063743f
+TERMUX_PKG_SHA256=0ac719413abd5751481bb67561937ca46028fd8473e7e4a7e4089aadbd9c673e
+TERMUX_PKG_DEPENDS="ncurses, readline"
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_grantpt=yes
 ac_cv_func_unlockpt=yes
 ac_cv_lib_util_openpty=no
 ptyttylib_cv_ptys=STREAMS
 "
-TERMUX_PKG_DEPENDS="ncurses, readline"
+
+termux_step_pre_configure() {
+	autoreconf -vfi
+}
