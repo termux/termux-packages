27ea21e7632592285c9615e86479f52a99f17536
LeapofAzzam-backup <76893443+LeapofAzzam-backup@users.noreply.github.com>
2021-05-07 19:20:14 +0700

    new package: sdl2-net (#294)

---
+++ x11-packages/sdl2-net/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://www.libsdl.org/projects/SDL_net/
+TERMUX_PKG_DESCRIPTION="A small sample cross-platform networking library"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="COPYING.txt"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
+TERMUX_PKG_VERSION=2.0.1
+TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_net/release/SDL2_net-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21
+TERMUX_PKG_DEPENDS="sdl2, libandroid-glob, libflac, libogg, libsndfile, libvorbis, libx11, libxau, libxcb, libxcursor, libxdmcp, libxext, libxfixes, libxi, libxinerama, libxrandr, libxrender, libxss, libxxf86vm, pulseaudio"
