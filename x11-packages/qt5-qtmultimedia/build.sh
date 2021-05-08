cc67e0e0aa98f6a3f20ba367a70aa848223cb66c
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-09 00:53:09 +0300

    qt5-qtmultimedia: bump revision

--- x11-packages/qt5-qtmultimedia/build.sh
+++ x11-packages/qt5-qtmultimedia/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Qt 5 Multimedia Library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=5.12.10
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/5.12/${TERMUX_PKG_VERSION}/submodules/qtmultimedia-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=cc98f19eb54b581f4a270855c4dc07ae9272e775ae7c5729c885e6c5e197a03c
 # qt5-qtdeclarative is not needed because quick widget requires OpenGL
