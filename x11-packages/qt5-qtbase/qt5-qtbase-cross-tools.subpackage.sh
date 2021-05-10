fdd543f2991509b09e73ed1213176b8d6a1c4943
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-10 22:25:38 +0300

    specify qt cross tools as platform-dependent

--- x11-packages/qt5-qtbase/qt5-qtbase-cross-tools.subpackage.sh
+++ x11-packages/qt5-qtbase/qt5-qtbase-cross-tools.subpackage.sh
@@ -1,6 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="Tools for cross build on the host (NOT for Termux)"
 TERMUX_SUBPKG_DEPENDS="qt5-qtbase"
-TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 TERMUX_SUBPKG_INCLUDE="
 opt/qt/cross/bin/*
 opt/qt/cross/lib/*
