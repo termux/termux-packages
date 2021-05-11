47beda5163c35a2e5f1582b2acd2367789fba446
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-05-11 09:48:35 +0900

    Bump `TERMUX_PKG_REVISION`

--- packages/ruby/build.sh
+++ packages/ruby/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Dynamic programming language with a focus on simplicity 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://cache.ruby-lang.org/pub/ruby/${TERMUX_PKG_VERSION:0:3}/ruby-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d06bccd382d03724b69f674bc46cd6957ba08ed07522694ce44b9e8ffc9c48e2
 # libbffi is used by the fiddle extension module:
