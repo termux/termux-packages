e64b320abf2491ae6a23461c559f463d05e8f798
Simeon Huang <hzwhuang@gmail.com>
2021-05-11 20:24:17 +0100

    remove redundant cmake args

--- x11-packages/libqtxdg/build.sh
+++ x11-packages/libqtxdg/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Qt 5 implementation of freedesktop.org XDG specification
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=3.7.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/libqtxdg/releases/download/${TERMUX_PKG_VERSION}/libqtxdg-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=477cbe76b3305071ff5f5bfa31dbcddcc51f3434b9ed75d91988219296d88a9b
-TERMUX_PKG_DEPENDS="qt5-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="cmake, lxqt-build-tools, glib, qt5-qtsvg, qt5-qttools-cross-tools"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}"
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, glib"
+TERMUX_PKG_BUILD_DEPENDS="cmake, lxqt-build-tools, qt5-qttools-cross-tools"
 
