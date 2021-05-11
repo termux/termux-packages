ec3365e240c7d0104571165721e4776f62182115
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-05-11 16:45:10 +0530

    new package: zenity (#316)

---
+++ x11-packages/zenity/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/zenity
+TERMUX_PKG_DESCRIPTION="a rewrite of gdialog, the GNOME port of dialog"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=3.32.0
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/zenity/${TERMUX_PKG_VERSION:0:4}/zenity-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=e786e733569c97372c3ef1776e71be7e7599ebe87e11e8ad67dcc2e63a82cd95
+TERMUX_PKG_DEPENDS="glib, gtk3"
+termux_step_pre_configure() {
+	autoreconf -fi
+}
