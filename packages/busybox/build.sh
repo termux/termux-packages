d4bcc8298271d6eeb975a7d63a87be48d39c551a
Henrik Grimler <henrik@grimler.se>
2021-05-12 16:19:41 +0200

    busybox: install into TERMUX_PKG_MASSAGEDIR

--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -13,14 +13,6 @@ TERMUX_PKG_SERVICE_SCRIPT=(
 	"busybox-httpd" "exec busybox httpd -f -p 0.0.0.0:8080 -h $TERMUX_PREFIX/srv/www/ 2>&1"
 )
 
-termux_step_pre_configure() {
-	# Certain packages are not safe to build on device because their
-	# build.sh script deletes specific files in $TERMUX_PREFIX.
-	if $TERMUX_ON_DEVICE_BUILD; then
-		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
-	fi
-}
-
 termux_step_configure() {
 	# Prevent spamming logs with useless warnings to make them more readable.
 	CFLAGS+=" -Wno-ignored-optimization-argument -Wno-unused-command-line-argument"
@@ -38,10 +30,11 @@ termux_step_configure() {
 
 termux_step_make_install() {
 	# Using unstripped variant. The post-massage step will strip binaries anyway.
-	install -Dm700 ./0_lib/busybox_unstripped $TERMUX_PREFIX/bin/busybox
-	install -Dm700 ./0_lib/libbusybox.so.${TERMUX_PKG_VERSION}_unstripped $TERMUX_PREFIX/lib/libbusybox.so.${TERMUX_PKG_VERSION}
-	ln -sfr $TERMUX_PREFIX/lib/libbusybox.so.${TERMUX_PKG_VERSION} $TERMUX_PREFIX/lib/libbusybox.so
+	install -Dm700 ./0_lib/busybox_unstripped $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/busybox
+	install -Dm700 ./0_lib/libbusybox.so.${TERMUX_PKG_VERSION}_unstripped $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/libbusybox.so.${TERMUX_PKG_VERSION}
+	ln -sfr $TERMUX_PREFIX/lib/libbusybox.so.${TERMUX_PKG_VERSION} $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/libbusybox.so
 
 	# Install busybox man page.
-	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 $TERMUX_PKG_SRCDIR/docs/busybox.1
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/man/man1 \
+		$TERMUX_PKG_SRCDIR/docs/busybox.1
 }
