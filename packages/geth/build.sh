939e962fc5bd4277cda48e531cf217de99f10d9e
Henrik Grimler <henrik@grimler.se>
2021-05-13 22:07:14 +0200

    geth: install to TERMUX_PKG_MASSAGEDIR

--- packages/geth/build.sh
+++ packages/geth/build.sh
@@ -26,7 +26,7 @@ termux_step_make_install() {
 	for applet in abidump abigen bootnode clef devp2p ethkey evm faucet geth p2psim rlpdump; do
 		install -Dm700 \
 			"$TERMUX_PKG_SRCDIR/cmd/$applet/$applet" \
-			"$TERMUX_PREFIX"/bin/
+			"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/bin/
 	done
 	unset applet
 }
