25c93cca83ec62a8877599d0cc6617b81dc66077
Henrik Grimler <henrik@grimler.se>
2021-05-13 22:19:39 +0200

    ipfs: install to TERMUX_PKG_MASSAGEDIR

--- packages/ipfs/build.sh
+++ packages/ipfs/build.sh
@@ -27,5 +27,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX"/bin "${TERMUX_PKG_BUILDDIR}/src/github.com/ipfs/kubo/cmd/ipfs/ipfs"
+	install -Dm700 -t "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/bin \
+		"${TERMUX_PKG_BUILDDIR}/src/github.com/ipfs/kubo/cmd/ipfs/ipfs"
 }
