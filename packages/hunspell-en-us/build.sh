e8924ce66d4c58d00565a2d4fd6db226115664b6
Henrik Grimler <henrik@grimler.se>
2021-05-13 14:28:45 +0200

    hunspell-en-us: update en_US.dic to 2021-01-01 version
    Maybe we should add hunspell-en-gb as well, the British dictionary
    seem to be updated much more frequently.

--- packages/hunspell-en-us/build.sh
+++ packages/hunspell-en-us/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://hunspell.github.io
 TERMUX_PKG_DESCRIPTION="American english dictionary for hunspell"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20181025
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=20210101
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -18,6 +17,6 @@ termux_step_make_install() {
 			c7a8c4d08c29d237880844b1623099f59092602f189be38ce3912e457ff38bc1
 	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.dic \
 			$TERMUX_PREFIX/share/hunspell/en_US.dic \
-		        e2aec1aff3e2b840c59e2a4f49f7eb9388b6078c681d960153fc72ce183086cc
+		        f0b1a234bd178bdd01875b2a392a9647f888b8fe879f79c52aae62c2759b3647
 	touch $TERMUX_PREFIX/share/hunspell/en_US.{aff,dic}
 }
