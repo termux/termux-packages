175a1263fec1d3c1db6908ef784a119f6014d5c0
Henrik Grimler <henrik@grimler.se>
2021-05-13 22:43:40 +0200

    libvterm: install to TERMUX_PKG_MASSAGEDIR

--- packages/libvterm/build.sh
+++ packages/libvterm/build.sh
@@ -7,15 +7,13 @@ TERMUX_PKG_SRCURL=https://www.leonerd.org.uk/code/libvterm/libvterm-${TERMUX_PKG
 TERMUX_PKG_SHA256=91eb5088069f4e6edab69e14c4212f6da0192e65695956dc048016a0dab8bcf6
 TERMUX_PKG_BREAKS="libvterm-dev"
 TERMUX_PKG_REPLACES="libvterm-dev"
+TERMUX_PKG_EXTRA_MAKE_ARGS="src/encoding/DECdrawing.inc src/encoding/uk.inc"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make() {
-	make src/encoding/DECdrawing.inc src/encoding/uk.inc
-}
-
 termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR/src
-	$CC -std=c99 -shared -fPIC $LDFLAGS -o $TERMUX_PREFIX/lib/libvterm.so \
+	cd src
+	$CC -std=c99 -shared -fPIC $LDFLAGS -o \
+		$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/libvterm.so \
 		-Wl,-soname=libvterm.so *.c -I../include -I.
-	cp ../include/*.h $TERMUX_PREFIX/include/
+	install -m600 ../include/*.h $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/
 }
