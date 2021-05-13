64ed71d5d94b7ce5e4cd047cfb59170c62ec605f
Henrik Grimler <henrik@grimler.se>
2021-05-13 22:42:56 +0200

    libunibilium: install to TERMUX_PKG_MASSAGEDIR

--- packages/libunibilium/build.sh
+++ packages/libunibilium/build.sh
@@ -15,20 +15,20 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	return
-}
-
-termux_step_make_install() {
 	CFLAGS+=" -DTERMINFO_DIRS=\"$TERMUX_PREFIX/share/terminfo/\""
 	$CC $CFLAGS -c -fPIC unibilium.c -o unibilium.o
 	$CC $CFLAGS -c -fPIC uninames.c -o uninames.o
 	$CC $CFLAGS -c -fPIC uniutil.c -o uniutil.o
-	$CC -shared -fPIC $LDFLAGS -o $TERMUX_PREFIX/lib/libunibilium.so unibilium.o uninames.o uniutil.o
-	cp unibilium.h $TERMUX_PREFIX/include/
+	$CC -shared -fPIC $LDFLAGS -o libunibilium.so unibilium.o uninames.o uniutil.o
+}
 
-	mkdir -p $PKG_CONFIG_LIBDIR
+termux_step_make_install() {
+	install -m600 libunibilium.so $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/
+	install -m600 unibilium.h $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/include/
+
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$PKG_CONFIG_LIBDIR
 	sed "s|@VERSION@|$TERMUX_PKG_VERSION|" unibilium.pc.in | \
 		sed "s|@INCDIR@|$TERMUX_PREFIX/include|" | \
 		sed "s|@LIBDIR@|$TERMUX_PREFIX/lib|" > \
-		$PKG_CONFIG_LIBDIR/unibilium.pc
+		$TERMUX_PKG_MASSAGEDIR/$PKG_CONFIG_LIBDIR/unibilium.pc
 }
