899d7e0a6361dba34c503057824c945b9e9be7f5
suhan-paradkar <12suhangp34@gmail.com>
2021-05-14 08:49:53 +0000

    Add package:xfce4-datetime-plugin

---
+++ x11-packages/xfce4-datetime-plugin/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-datetime-plugin/start
+TERMUX_PKG_DESCRIPTION="xfce4-datetime-plugin shows the date and time in the panel; when left-clicked, a popup calendar appears. "
+TERMUX_PKG_LICENSE="GPL-2.0"
+_MAJOR_VERSION=0.8
+TERMUX_PKG_VERSION=$_MAJOR_VERSION.1
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-datetime-plugin/$_MAJOR_VERSION/xfce4-datetime-plugin-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=e9f6f15be29ceb5c45718006b46dbd19b89981617d0768b2ef942b5a70af2540
+TERMUX_PKG_DEPENDS="libxfce4ui, xfce4-panel"
+TERMUX_PKG_BUILD_DEPENDS="intltool"
