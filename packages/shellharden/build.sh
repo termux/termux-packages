1aa0265d3d63fa873187781e9d85a53ff06b45f9
Henrik Grimler <henrik@grimler.se>
2021-05-14 14:42:23 +0200

    shellharden: install into TERMUX_PKG_MASSAGEDIR

--- packages/shellharden/build.sh
+++ packages/shellharden/build.sh
@@ -11,6 +11,6 @@ TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 termux_step_post_make_install() {
 	install -Dm700 \
-		"$TERMUX_PKG_SRCDIR/target/$CARGO_TARGET_NAME"/release/shellharden \
-		"$TERMUX_PREFIX"/bin/
+		"target/$CARGO_TARGET_NAME"/release/shellharden \
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/bin/
 }
