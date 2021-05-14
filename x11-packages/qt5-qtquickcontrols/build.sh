6f87a9d7060a14e48cd8022504e748cf1aa38f1d
Simeon Huang <hzwhuang@gmail.com>
2021-05-14 14:16:59 +0100

    add Qt Quick Controls (1) module

---
+++ x11-packages/qt5-qtquickcontrols/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://www.qt.io/
+TERMUX_PKG_DESCRIPTION="Qt Quick Controls module"
+TERMUX_PKG_LICENSE="LGPL-3.0"
+TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
+TERMUX_PKG_VERSION=5.12.10
+TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/${TERMUX_PKG_NAME#qt5-}-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=67d2756737f79855f7d7afcd2338ba48c7d117c792fee66d7625b99fa8dcc9b3
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_NO_STATICSPLIT=true
+
+termux_step_configure () {
+    "${TERMUX_PREFIX}/opt/qt/cross/bin/qmake" \
+        -spec "${TERMUX_PREFIX}/lib/qt/mkspecs/termux-cross"
+}
+
+termux_step_post_make_install() {
+    #######################################################
+    ##
+    ##  Fixes & cleanup.
+    ##
+    #######################################################
+
+    ## Remove *.la files.
+    find "${TERMUX_PREFIX}/lib" -iname \*.la -delete
+}
+
