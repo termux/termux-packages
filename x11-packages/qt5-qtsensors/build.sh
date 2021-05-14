50c608041c29c99b7cdc9e2524c64106d0f0b67f
Simeon Huang <hzwhuang@gmail.com>
2021-05-14 15:31:17 +0100

    add qt5 qtsensors module

---
+++ x11-packages/qt5-qtsensors/build.sh
@@ -0,0 +1,32 @@
+TERMUX_PKG_HOMEPAGE=https://www.qt.io/
+TERMUX_PKG_DESCRIPTION="Qt 5 Sensors Library"
+TERMUX_PKG_LICENSE="LGPL-3.0"
+TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
+TERMUX_PKG_VERSION=5.12.10
+TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/${TERMUX_PKG_NAME#qt5-}-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=b508dc30eef7fe4171f5426e7a234a47e54457bce3910722937c1eb6a505c081
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_NO_STATICSPLIT=true
+
+termux_step_configure () {
+    "${TERMUX_PREFIX}/opt/qt/cross/bin/qmake" \
+        -spec "${TERMUX_PREFIX}/lib/qt/mkspecs/termux-cross"
+}
+
+termux_step_post_make_install() {
+    #######################################################
+    ##
+    ##  Fixes & cleanup.
+    ##
+    #######################################################
+
+    ## Drop QMAKE_PRL_BUILD_DIR because reference the build dir.
+    find "${TERMUX_PREFIX}/lib" -type f -name "libQt5Sensors*.prl" \
+        -exec sed -i -e '/^QMAKE_PRL_BUILD_DIR/d' "{}" \;
+
+    ## Remove *.la files.
+    find "${TERMUX_PREFIX}/lib" -iname \*.la -delete
+}
+
