bc9ef11c466c2aaf7faf7fdbc04d39d3b5c79e19
Henrik Grimler <henrik@grimler.se>
2021-05-14 14:38:48 +0200

    proxychains-ng: install into TERMUX_PKG_MASSAGEDIR

--- packages/proxychains-ng/build.sh
+++ packages/proxychains-ng/build.sh
@@ -8,16 +8,6 @@ TERMUX_PKG_SHA256=5f66908044cc0c504f4a7e618ae390c9a78d108d3f713d7839e440693f43b5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
-	# Certain packages are not safe to build on device because their
-	# build.sh script deletes specific files in $TERMUX_PREFIX.
-	if $TERMUX_ON_DEVICE_BUILD; then
-		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
-	fi
-}
-
 termux_step_post_make_install() {
-	# Remove conf file from previous build, otherwise nothing will be done and it won't be included in the package
-	rm -f "$TERMUX_PREFIX"/etc/proxychains.conf
-	make install-config
+	make install-config DESTDIR=$TERMUX_PKG_MASSAGEDIR
 }
