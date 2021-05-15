f505558bd3c9f0df4beeb702124531338a7cc593
Henrik Grimler <henrik@grimler.se>
2021-05-15 22:12:59 +0200

    xmake: avoid overriding termux_step_make{,_install}

--- packages/xmake/build.sh
+++ packages/xmake/build.sh
@@ -6,10 +6,3 @@ TERMUX_PKG_VERSION=2.5.3
 TERMUX_PKG_SRCURL=https://github.com/xmake-io/xmake/releases/download/v${TERMUX_PKG_VERSION}/xmake-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=337edd61de22b043720556a02bf7b1c4d4881e200ecce6bb2406d0442f2db92e
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make() {
-	make build
-}
-termux_step_make_install() {
-	make install PREFIX="${TERMUX_PREFIX}"
-}
