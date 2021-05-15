4eb8526b7bca3e95a470591084795fded0201ff2
Henrik Grimler <henrik@grimler.se>
2021-05-15 22:08:06 +0200

    termux-keyring: install to TERMUX_PKG_MASSAGEDIR

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -8,25 +8,27 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_ESSENTIAL=true
 
 termux_step_make_install() {
-	local GPG_SHARE_DIR="$TERMUX_PREFIX/share/termux-keyring"
+	local GPG_SHARE_DIR="$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/termux-keyring"
 
 	mkdir -p $GPG_SHARE_DIR
 
+	cd $TERMUX_PKG_BUILDER_DIR
 	# Maintainer-specific keys.
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/agnostic-apollo.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/mradityaalok.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/2096779623.gpg $GPG_SHARE_DIR
+	install -Dm600 agnostic-apollo.gpg $GPG_SHARE_DIR
+	install -Dm600 grimler.gpg $GPG_SHARE_DIR
+	install -Dm600 kcubeterm.gpg $GPG_SHARE_DIR
+	install -Dm600 landfillbaby.gpg $GPG_SHARE_DIR
+	install -Dm600 mradityaalok.gpg $GPG_SHARE_DIR
+	install -Dm600 2096779623.gpg $GPG_SHARE_DIR
 
 	# Key for automatic builds (via CI).
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $GPG_SHARE_DIR
+	install -Dm600 termux-autobuilds.gpg $GPG_SHARE_DIR
 
 	# Key for pacman package manager.
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-pacman.gpg $GPG_SHARE_DIR
+	install -Dm600 termux-pacman.gpg $GPG_SHARE_DIR
 
-	for GPG_DIR in "$TERMUX_PREFIX/etc/apt/trusted.gpg.d" "$TERMUX_PREFIX/share/pacman/keyrings"; do
+	for GPG_DIR in "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/apt/trusted.gpg.d" \
+		       "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/pacman/keyrings"; do
 		mkdir -p $GPG_DIR
 		for GPG_FILE in $GPG_SHARE_DIR/*.gpg; do
 			if [[ "$GPG_DIR" == *"/apt/"* && "$GPG_FILE" == *"termux-pacman.gpg"* ]]; then
