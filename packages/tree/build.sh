1c9e7107d846001babb4e7f7e5d88d83db6fd226
Henrik Grimler <henrik@grimler.se>
2021-05-15 22:04:32 +0200

    tree: install to TERMUX_PKG_MASSAGEDIR

--- packages/tree/build.sh
+++ packages/tree/build.sh
@@ -10,15 +10,6 @@ TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make() {
-	make \
-		CC="$CC" \
-		CFLAGS="$CFLAGS $CPPFLAGS -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" \
-		LDFLAGS="$LDFLAGS"
-}
-
-termux_step_make_install() {
-	make install \
-		PREFIX="$TERMUX_PREFIX" \
-		MANDIR="$TERMUX_PREFIX/share/man/man1"
+termux_step_pre_configure() {
+	export CFLAGS="$CFLAGS $CPPFLAGS -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64"
 }
