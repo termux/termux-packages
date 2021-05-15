014ff4357eadb51340045ded45e002480dbdb2bb
Henrik Grimler <henrik@grimler.se>
2021-05-15 22:08:06 +0200

    termux-keyring: install to TERMUX_PKG_MASSAGEDIR

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -9,27 +9,28 @@ TERMUX_PKG_ESSENTIAL=true
 
 termux_step_make_install() {
 	if [ "$TERMUX_PACKAGE_FORMAT" = "debian" ]; then
-		local GPG_DIR="$TERMUX_PREFIX/etc/apt/trusted.gpg.d"
+		local GPG_DIR="$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/apt/trusted.gpg.d"
 	else
-		local GPG_DIR="$TERMUX_PREFIX/share/pacman/keyrings"
+		local GPG_DIR="$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/pacman/keyrings"
 	fi
 
 	mkdir -p $GPG_DIR
 
+	cd $TERMUX_PKG_BUILDER_DIR
 	# Maintainer-specific keys.
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/agnostic-apollo.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/mradityaalok.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/2096779623.gpg $GPG_DIR
+	install -Dm600 agnostic-apollo.gpg $GPG_DIR
+	install -Dm600 grimler.gpg $GPG_DIR
+	install -Dm600 kcubeterm.gpg $GPG_DIR
+	install -Dm600 landfillbaby.gpg $GPG_DIR
+	install -Dm600 mradityaalok.gpg $GPG_DIR
+	install -Dm600 2096779623.gpg $GPG_DIR
 
 	# Key for automatic builds (via CI).
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $GPG_DIR
+	install -Dm600 termux-autobuilds.gpg $GPG_DIR
 
 	# Creation of trusted files
 	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ]; then
-		install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-pacman.gpg $GPG_DIR
+		install -Dm600 termux-pacman.gpg $GPG_DIR
 		echo "998DE27318E867EA976BA877389CEED64573DFCA:4:" > $GPG_DIR/termux-pacman-trusted
 	fi
 }
