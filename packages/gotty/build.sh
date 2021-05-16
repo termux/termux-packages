7d6c69d4c085e1a007bb3af38138a2c4247bb511
Henrik Grimler <henrik@grimler.se>
2021-05-16 11:16:37 +0200

    gotty: fix build

--- packages/gotty/build.sh
+++ packages/gotty/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Share your terminal as a web application"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.1
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://github.com/yudai/gotty/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=32695d70a79f55efdf4fba6f06f830515a2055abc58fc15e2124dff5be75695b
 
@@ -15,6 +15,8 @@ termux_step_make() {
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/yudai/gotty
 
 	cd "$GOPATH"/src/github.com/yudai/gotty
+	go mod init
+	go mod tidy
 	go build
 }
 
