3661e21fd5b6af4e962fb59eb63ddff478472b83
Henrik Grimler <henrik@grimler.se>
2021-05-16 11:16:44 +0200

    rush: fix build

--- packages/rush/build.sh
+++ packages/rush/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A cross-platform command-line tool for executing jobs in
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=0.4.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/shenwei356/rush/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=801b17e279406c1649b401a2214148d344f673d0a1c9eed1b417655c1fefd114
 
@@ -16,6 +16,7 @@ termux_step_make_install() {
 	mkdir -p "${GOPATH}/src/github.com/shenwei356"
 	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/shenwei356/rush"
 	cd "${GOPATH}/src/github.com/shenwei356/rush"
+	go mod init rush
 	go get -d -v
 	go install
 
