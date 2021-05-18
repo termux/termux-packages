63bc4da5a8a9e48110bc5a590fea1c3d7c89a49e
Simeon Huang <hzwhuang@gmail.com>
2021-05-18 19:50:33 +0100

    enable C++ for gpgme to create gpgmepp packages

--- packages/gpgme/build.sh
+++ packages/gpgme/build.sh
@@ -3,16 +3,16 @@ TERMUX_PKG_DESCRIPTION="Library designed to make access to GnuPG easier"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.15.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=eebc3c1b27f1c8979896ff361ba9bb4778b508b2496c2fc10e3775a40b1de1ad
 TERMUX_PKG_DEPENDS="gnupg (>= 2.2.9-1), libassuan, libgpg-error"
 TERMUX_PKG_BREAKS="gpgme-dev"
 TERMUX_PKG_REPLACES="gpgme-dev"
 # Use "--disable-gpg-test" to avoid "No rule to make target `../../src/libgpgme-pthread.la":
-# Use "--enable-languages=no" to only build the C library.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-gpg-test
---enable-languages=no
+--enable-languages=cpp
 --with-gpg=$TERMUX_PREFIX/bin/gpg2
 --without-g13
 --without-gpgconf
