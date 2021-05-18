1b081fe081a8625c8a5c945f7f5cc168c0cb13ae
Simeon Huang <hzwhuang@gmail.com>
2021-05-18 19:30:26 +0100

    Add package script for mimetic

---
+++ packages/mimetic/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.codesink.org/mimetic_mime_library.html
+TERMUX_PKG_DESCRIPTION="A C++ Email library (MIME)"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
+TERMUX_PKG_VERSION=0.9.8
+TERMUX_PKG_SRCURL=http://www.codesink.org/download/mimetic-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3a07d68d125f5e132949b078c7275d5eb0078dd649079bd510dd12b969096700
