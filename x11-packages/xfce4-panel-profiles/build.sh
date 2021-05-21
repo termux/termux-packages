c76711d763944e78167051d9bec1d912aaa2711e
suhan-paradkar <12suhangp34@gmail.com>
2021-05-21 08:04:31 +0000

    Add package:xfce4-panel-profiles

---
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-panel-profiles/start
+TERMUX_PKG_DESCRIPTION="a simple application to manage Xfce panel layouts. "
+TERMUX_PKG_LICENSE="GPL-2.0"
+_MAJOR_VERSION=1.0
+TERMUX_PKG_VERSION=$_MAJOR_VERSION.13
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/$_MAJOR_VERSION/xfce4-panel-profiles-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=bc387c13f94109422dc72b0fcb919b0dc11619ba589d03e492252b0d2513b170
+TERMUX_PKG_DEPENDS="xfce4-panel, python"
+termux_step_configure() {
+    sed -e s,@prefix@,$PREFIX, Makefile.in.in > Makefile.in
+    sed \
+    -e s,@appname@,xfce4-panel-profiles,g \
+    -e s,@version@,$TERMUX_PKG_VERSION,g \
+    -e s,@mandir@,$TERMUX_PREFIX/share/man,g \
+    -e s,@docdir@,$TERMUX_PREFIX/share/doc,g \
+    -e s,@python@,$TERMUX_PREFIX/bin/python,g \
+     Makefile.in > Makefile
+}
