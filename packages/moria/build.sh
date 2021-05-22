2cf76ea919d254cf92716e865340be6af0661865
Henrik Grimler <henrik@grimler.se>
2021-05-22 07:53:08 +0200

    moria: update to 5.7.14
    And fix path to help and score file.

--- packages/moria/build.sh
+++ packages/moria/build.sh
@@ -2,16 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://umoria.org
 TERMUX_PKG_DESCRIPTION="Rogue-like game with an infinite dungeon"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.7.13
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.7.14
 TERMUX_PKG_SRCURL=https://github.com/dungeons-of-moria/umoria/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a9dae819aca650ea2cd0a5d05853d768f815617ff61de87962325231758f62b9
+TERMUX_PKG_SHA256=6c5cfd470ba15b2a93606bfa7b655683ace9e55dc783053346849881fdb17f2c
 TERMUX_PKG_DEPENDS="libc++, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dbuild_dir=$TERMUX_PKG_BUILDDIR"
 
 termux_step_create_debscripts() {
     # Create scores file in a debscript, so an update to the package wouldn't erease any scores
-    echo "mkdir -p $TERMUX_PREFIX/var/games/moria/" > postinst
-    echo "touch $TERMUX_PREFIX/var/games/moria/scores" >> postinst
+    echo "mkdir -p $TERMUX_PREFIX/lib/games/moria/" > postinst
+    echo "touch $TERMUX_PREFIX/lib/games/moria/scores.dat" >> postinst
     chmod 0755 postinst
 }
