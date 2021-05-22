cdad0fdd0ee42b14664f9597bf858514c71a5bde
suhan-paradkar <12suhangp34@gmail.com>
2021-05-22 03:08:15 +0000

    Update croc

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another."
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=9.1.1
+TERMUX_PKG_VERSION=9.1.4
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=8afe88b30c784c1015a81755e4dbd53b2354c5e53f0eb99464db891c416663e7
+TERMUX_PKG_SHA256=a65f5004106858a74ec3834e232cff9a16728582700afff58c244c806438cc88
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
