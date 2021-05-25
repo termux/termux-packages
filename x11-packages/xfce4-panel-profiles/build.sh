1d68e5de68c23d1a4b416474527c3c8d1a104da0
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-25 17:22:00 +0300

    xfce4-panel-profiles: build in source directory

--- x11-packages/xfce4-panel-profiles/build.sh
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_VERSION=1.0.13
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION:0:3}/xfce4-panel-profiles-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=bc387c13f94109422dc72b0fcb919b0dc11619ba589d03e492252b0d2513b170
 TERMUX_PKG_DEPENDS="xfce4-panel, python"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
     sed -e s,@prefix@,$TERMUX_PREFIX, Makefile.in.in > Makefile.in
