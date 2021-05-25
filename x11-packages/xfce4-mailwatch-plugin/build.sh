1d8d3d4d51a197f1e418925aebe22bc321080e4e
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-25 14:29:43 +0300

    xfce4-mailwatch-plugin: remove intltool build-time dependency
    It is not necessary to specify host build tools (installed by setup-ubuntu.sh) as build depends.

--- x11-packages/xfce4-mailwatch-plugin/build.sh
+++ x11-packages/xfce4-mailwatch-plugin/build.sh
@@ -6,4 +6,3 @@ TERMUX_PKG_VERSION=$_MAJOR_VERSION.0
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-mailwatch-plugin/$_MAJOR_VERSION/xfce4-mailwatch-plugin-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=20f91ebefd2880b27f421f773115b3740f67de2bf60feace3841bfd1a09cbe2e
 TERMUX_PKG_DEPENDS="glib, libxfce4ui, libxfce4util, xfce4-panel"
-TERMUX_PKG_BUILD_DEPENDS="intltool"
