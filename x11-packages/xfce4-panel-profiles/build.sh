7c2d7c6ddd16e4e7a440d80ce2b15cca6d4abb6f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-25 14:37:07 +0300

    xfce4-panel-profiles: fix description

--- x11-packages/xfce4-panel-profiles/build.sh
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-panel-profiles/start
-TERMUX_PKG_DESCRIPTION="a simple application to manage Xfce panel layouts. "
+TERMUX_PKG_DESCRIPTION="A simple application to manage Xfce panel layouts."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.0.13
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION:0:3}/xfce4-panel-profiles-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=bc387c13f94109422dc72b0fcb919b0dc11619ba589d03e492252b0d2513b170
 TERMUX_PKG_DEPENDS="xfce4-panel, python"
+
 termux_step_configure() {
     sed -e s,@prefix@,$PREFIX, Makefile.in.in > Makefile.in
     sed \
