719957fa2ae970ddf7bf1c5cf4ac3d6e272d4c09
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-26 16:44:34 +0300

    Revert "fix" for curl in Github Actions
    It doesn't work.

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -3,8 +3,6 @@ set -e -u
 
 CONTAINER_HOME_DIR=/home/builder
 UNAME=$(uname)
-: ${GITHUB_ACTIONS:="false"}
-
 if [ "$UNAME" = Darwin ]; then
 	# Workaround for mac readlink not supporting -f.
 	REPOROOT=$PWD
@@ -33,15 +31,8 @@ else
 fi
 
 $SUDO docker start $CONTAINER_NAME >/dev/null 2>&1 || {
-	if [ "$GITHUB_ACTIONS" = "true" ]; then
-		SECURITY_OPT="--cap-add SYS_ADMIN --cap-add NET_ADMIN --security-opt apparmor:unconfined --security-opt seccomp=unconfined"
-	else
-		SECURITY_OPT=""
-	fi
-
 	echo "Creating new container..."
 	$SUDO docker run \
-		$SECURITY_OPT \
 		--detach \
 		--name $CONTAINER_NAME \
 		--volume $REPOROOT:$CONTAINER_HOME_DIR/termux-packages \
@@ -57,10 +48,6 @@ $SUDO docker start $CONTAINER_NAME >/dev/null 2>&1 || {
 		fi
 	fi
 }
-
-if [ "$GITHUB_ACTIONS" = "true" ]; then
-	$SUDO docker exec $DOCKER_TTY $CONTAINER_NAME sudo ethtool -K eth0 tx off rx off
-fi
 
 if [ "$#" -eq  "0" ]; then
 	$SUDO docker exec --interactive $DOCKER_TTY $CONTAINER_NAME bash
