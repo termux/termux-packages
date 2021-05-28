01edf86d6292c677a587425cd774adb57b70897c
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-28 14:00:14 +0300

    remove cmake and perl from LXQT build dependencies
    CMake - installed by build-package.sh when necessary or by hook inside
    package build.sh script. Target package is useless when cross-compiling.
    When building on-device, cmake is installed by setup-termux.sh.
    
    Perl - installed by default in build environment by setup-ubuntu.sh or
    setup-termux.sh. When cross-compiling, target package is useless.

--- x11-packages/lxqt-notificationd/build.sh
+++ x11-packages/lxqt-notificationd/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="The LXQt notification daemon"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=0.17.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-notificationd/releases/download/${TERMUX_PKG_VERSION}/lxqt-notificationd-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=d392cac3d7300a6b8be3431fdde37f453ac439b6fd52b39e6bdeba9ad7a1cab7
 TERMUX_PKG_DEPENDS="qt5-qtbase, liblxqt"
-TERMUX_PKG_BUILD_DEPENDS="cmake, perl, lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
