74c02e9217b53f688102045c4f39725d252a5212
vhqtvn <vhqtvn@users.noreply.github.com>
2021-05-28 20:38:47 +0700

    commit

--- packages/mailutils/build.sh
+++ packages/mailutils/build.sh
@@ -6,7 +6,9 @@ TERMUX_PKG_VERSION=3.12
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/mailutils/mailutils-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=6d43fa217c4ac63f057de87890c562d170bb92bc402368b5fbc579e4c2b3a158
 TERMUX_PKG_BUILD_DEPENDS="libandroid-glob, libcrypt"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	export LIBS="-landroid-glob"
+	autoreconf -vfi
 }
