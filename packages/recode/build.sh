4a402a814de00f3da3509e02fe6882742f31d91e
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-29 14:37:00 +0300

    recode: remove unneeded changes
    I have done 3 builds:
    
    * With TERMUX_MAKE_PROCESSES=1
    * With TERMUX_MAKE_PROCESSES=1 & TERMUX_PKG_BUILD_IN_SRC=true
    * With autoreconf in pre configure step
    
    None of them failed and recode manpage was generated properly.

--- packages/recode/build.sh
+++ packages/recode/build.sh
@@ -7,10 +7,3 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rrthomas/recode/releases/download/v${TERMUX_PKG_VERSION}/recode-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4fb75cacc7b80fda7147ea02580eafd2b4493461fb75159e9a49561d3e10cfa7
 TERMUX_PKG_DEPENDS="libiconv"
-TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_MAKE_PROCESSES=1
-
-termux_step_pre_configure() {
-	cp -f "$TERMUX_PKG_BUILDER_DIR/invert-l-to-h" "./build-aux/"
-    sed -i 's/scriptversion/if [ "$2" = "\/usr\/bin\/help2man" ]; then\nexit\nfi\nscriptversion/' ./build-aux/missing
-}
