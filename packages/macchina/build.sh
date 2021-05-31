440b495b9b565f8094fa9b992ca6109de0ade0a4
Panda Foss <maxi.fg13@gmail.com>
2021-05-31 20:07:40 -0300

    Update macchina

--- packages/macchina/build.sh
+++ packages/macchina/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Macchina-CLI/macchina
 TERMUX_PKG_DESCRIPTION="A system information fetcher, with an emphasis on performance and minimalism."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Max Ferrer @PandaFoss"
-TERMUX_PKG_VERSION=0.7.4
+TERMUX_PKG_VERSION=0.8.21
 TERMUX_PKG_SRCURL=https://github.com/Macchina-CLI/macchina/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=66349e2274c48d8eb1ac1a627eb16dca0ee96e62c2fa34f9d9fadbe75a029d9f
+TERMUX_PKG_SHA256=0aff873ae30715f79c6e37f593e48d9505594d4a171e77e04377f9beec54c106
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
