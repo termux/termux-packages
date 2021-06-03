51db577a58ecd6054fccaf0e970bb3d709ad8a3d
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-06-03 20:36:23 +0300

    kirigami2: remove cmake from build depends

--- x11-packages/kirigami2/build.sh
+++ x11-packages/kirigami2/build.sh
@@ -7,4 +7,4 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="http://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kirigami2-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=f323efb96a809dc9e572a0e68e04c4f485fc27f9ae65ffa3988830e348151356
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtdeclarative, qt5-qtquickcontrols2, qt5-qtsvg"
-TERMUX_PKG_BUILD_DEPENDS="cmake, extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
