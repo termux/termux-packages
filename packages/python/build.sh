adde159d07d3bb1ca02a7c8a70ca13f579208ff2
Suhan G Paradkar <12suhangp34@gmail.com>
2021-06-03 14:12:11 +0000

    Corrections as per review

--- packages/python/build.sh
+++ packages/python/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_LICENSE="PythonPL"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=3.9
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.python.org/ftp/python/${TERMUX_PKG_VERSION}/Python-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0c5a140665436ec3dbfbb79e2dfb6d192655f26ef4a29aeffcb6d1820d716d83
 TERMUX_PKG_DEPENDS="gdbm, libandroid-support, libbz2, libcrypt, libffi, liblzma, libsqlite, ncurses, ncurses-ui-libs, openssl, readline, zlib"
@@ -65,8 +64,6 @@ termux_step_post_make_install() {
 	 ln -sf pydoc${_MAJOR_VERSION} pydoc)
 	(cd $TERMUX_PREFIX/share/man/man1
 	 ln -sf python${_MAJOR_VERSION}.1 python.1)
-	(cd $TERMUX_PREFIX/include/python3.9
-	 patch -p0 < $TERMUX_PKG_BUILDER_DIR/cpython.patch.txt)
 }
 
 termux_step_post_massage() {
