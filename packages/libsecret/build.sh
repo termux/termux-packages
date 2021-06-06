0da297449cfef9f44fbf190f36147fe23d64adca
Suhan G Paradkar <12suhangp34@gmail.com>
2021-06-06 12:06:20 +0000

    Add package: libsecret

---
+++ packages/libsecret/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/libsecret/
+TERMUX_PKG_VERSION=0.20.4
+TERMUX_PKG_DEPENDS="gobject-introspection, libgcrypt"
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/libsecret/-/archive/$TERMUX_PKG_VERSION/libsecret-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_SHA256=ca34e69b210df221ae5da6692c2cb15ef169bb4daf42e204442f24fdb0520d4b
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dintrospection=false -Dgtk_doc=false"
