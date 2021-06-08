422ed69903ee7f890ab86fb5656a7bc51539fb7f
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-06-08 14:37:57 +0700

    sdl2-net: fix description and change maintainer (#356)

--- x11-packages/sdl2-net/build.sh
+++ x11-packages/sdl2-net/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.libsdl.org/projects/SDL_net/
-TERMUX_PKG_DESCRIPTION="A small sample cross-platform networking library"
+TERMUX_PKG_DESCRIPTION="A small sample cross-platform networking library (version 2)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING.txt"
-TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
+TERMUX_PKG_MAINTAINER="Leap of Azzam (@leapofazzam123)"
 TERMUX_PKG_VERSION=2.0.1
 TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_net/release/SDL2_net-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21
