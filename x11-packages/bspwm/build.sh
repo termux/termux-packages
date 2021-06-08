20d5da0f67d7b93b6331358fc601aedf00f2f10f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-06-08 10:47:54 +0300

    bspwm, sdl2-net, sxhkd: fix maintainer field
    No free-form data: there are specific formats:
    * @nickname
    * Real Name @nickname
    * Real Name <email@example.com>

--- x11-packages/bspwm/build.sh
+++ x11-packages/bspwm/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/baskerville/bspwm"
 TERMUX_PKG_DESCRIPTION="A tiling window manager based on binary space partitioning"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_MAINTAINER="Leap of Azzam (@leapofazzam123)"
+TERMUX_PKG_MAINTAINER="@leapofazzam123"
 TERMUX_PKG_VERSION=0.9.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/baskerville/bspwm/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256="0d0f5e465b9c27a5c2143c86189392fa297fad55253b9ed8f6854473d3928f8f"
 TERMUX_PKG_DEPENDS="libx11, libxcb, libxkbcommon, sxhkd, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm"
