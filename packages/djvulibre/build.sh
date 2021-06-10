5b264f21f1de269c3a6162c0d637c3f87e0dc15b
Krishna Kanhaiya <kcubeterm@gmail.com>
2021-06-10 16:07:42 +0530

    djvulibre: bump revision

--- packages/djvulibre/build.sh
+++ packages/djvulibre/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Suite to create, manipulate and view DjVu ('déjà vu') 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.5.28
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/djvu/djvulibre-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=82e392a9cccfee94fa604126c67f06dbc43ed5f9f0905d15b6c8164f83ed5655
 TERMUX_PKG_DEPENDS="libtiff"
