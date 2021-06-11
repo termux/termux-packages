a2cfbb785f86dd560411f4116b46c27a1437e544
Henrik Grimler <henrik@grimler.se>
2021-06-11 22:15:55 +0200

    Bump updated packages to restart build

--- root-packages/cryptsetup/build.sh
+++ root-packages/cryptsetup/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Userspace setup tool for transparent encryption of block
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/linux/utils/cryptsetup/v${TERMUX_PKG_VERSION:0:3}/cryptsetup-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b296b7a21ea576c2b180611ccb19d06aec8dddaedf7c704b0c6a81210c25635f
 TERMUX_PKG_DEPENDS="json-c, libdevmapper, libgcrypt, libpopt, libuuid, util-linux, openssl, libiconv"
