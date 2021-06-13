0a7eb9d8cbfdd609477f64d7828cd52484b5377a
YAKSH BARIYA <yakshbari4@gmail.com>
2021-06-14 00:03:32 +0530

    Bump `caddy` to 2.4.2 (#7001)

--- packages/caddy/build.sh
+++ packages/caddy/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://caddyserver.com/
 TERMUX_PKG_DESCRIPTION="Fast, cross-platform HTTP/2 web server"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.4.1
+TERMUX_PKG_VERSION=2.4.2
 TERMUX_PKG_SRCURL=https://github.com/caddyserver/caddy/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=60850f68c76043ad9581cb11de9eaeb35816b521301e61940f9c98d3f62f0650
+TERMUX_PKG_SHA256=f6319d36f4e27a197c9aa832524a256ca9a9760f8334b52fbb6c618e63b22bf9
 
 termux_step_make() {
 	termux_setup_golang
