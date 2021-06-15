841f6602acad2457bd7b656c9071f0d9cb4af42a
Ostap <58697558+OldiLo@users.noreply.github.com>
2021-06-15 20:54:13 +0300

    update golang to 1.16.5

--- packages/golang/build.sh
+++ packages/golang/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://golang.org/
 TERMUX_PKG_DESCRIPTION="Go programming language compiler"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=1.16.3
+_MAJOR_VERSION=1.16.5
 # Use the ~ deb versioning construct in the future:
 TERMUX_PKG_VERSION=2:${_MAJOR_VERSION}
 TERMUX_PKG_SRCURL=https://storage.googleapis.com/golang/go${_MAJOR_VERSION}.src.tar.gz
