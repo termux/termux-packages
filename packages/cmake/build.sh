1ce096c0ab805c23468d0ef31f36661d09f0ada1
Butta <repo@butta.fastem.com>
2021-06-17 09:37:45 +0530

    cmake: update from 3.20.2 to 3.20.4

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package soft
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # When updating version here, please update termux_setup_cmake.sh as well.
-TERMUX_PKG_VERSION=3.20.2
+TERMUX_PKG_VERSION=3.20.4
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=aecf6ecb975179eb3bb6a4a50cae192d41e92b9372b02300f9e8f1d5f559544e
+TERMUX_PKG_SHA256=87a4060298f2c6bb09d479de1400bc78195a5b55a65622a7dceeb3d1090a1b16
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, make, clang, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DKWSYS_LFS_WORKS=ON -DBUILD_CursesDialog=ON"
