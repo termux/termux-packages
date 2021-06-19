4d25f98bbff782d012ed85838d67a6d0f6cd1214
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-06-19 18:36:57 +0300

    qt5-qtquickcontrols: bump revision

--- x11-packages/qt5-qtquickcontrols/build.sh
+++ x11-packages/qt5-qtquickcontrols/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Qt Quick Controls module"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=5.12.11
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/${TERMUX_PKG_NAME#qt5-}-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=cd6b81fda691ab15d25ac60b6a3437667a892e401438e07a64c88cadd3481389
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtdeclarative"
