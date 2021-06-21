88e142252bd20c7683777b3e39b3665170ad2e19
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-06-21 20:35:40 +0300

    gtk4: fix build.sh
    Depend on graphene.
    
    Disable sassc.

--- x11-packages/gtk4/build.sh
+++ x11-packages/gtk4/build.sh
@@ -6,11 +6,12 @@ TERMUX_PKG_VERSION=4.3.0
 TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gtk/-/archive/$TERMUX_PKG_VERSION/gtk-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=cea146508fcbf2c234247e1be34a556d0bbde3db30e3faea9e5189735f5551a4
-TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, gdk-pixbuf, glib, glib-bin, gtk-update-icon-cache, libcairo, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango, shared-mime-info, ttf-dejavu, libxkbcommon"
+TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, gdk-pixbuf, glib, glib-bin, graphene, gtk-update-icon-cache, libcairo, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango, shared-mime-info, ttf-dejavu, libxkbcommon"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dsassc=disabled
 -Dwayland-backend=false
 -Ddemos=false
 -Dbuild-examples=false
