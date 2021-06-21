de44d76b9e57157ecc9189d9ef2e81a69e720965
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-06-21 20:26:20 +0300

    new package: graphene
    Required by gtk4

---
+++ x11-packages/graphene/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://ebassi.github.io/graphene/
+TERMUX_PKG_DESCRIPTION="A thin layer of graphic data types"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.10.6
+TERMUX_PKG_SRCURL=https://github.com/ebassi/graphene/releases/download/${TERMUX_PKG_VERSION}/graphene-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=80ae57723e4608e6875626a88aaa6f56dd25df75024bd16e9d77e718c3560b25
+TERMUX_PKG_DEPENDS="glib"
