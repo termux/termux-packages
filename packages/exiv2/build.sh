0abd84f13d428ea5df828e5dc31c8082788b3d5f
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-22 10:04:41 +0530

    `exiv2`: Revise version to force rebuild

--- packages/exiv2/build.sh
+++ packages/exiv2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Exif, Iptc and XMP metadata manipulation library and too
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.27.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Exiv2/exiv2/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9fb2752c92f63c9853e0bef9768f21138eeac046280f40ded5f37d06a34880d9
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libexpat, zlib"
