b60f3bac108a1b0f9b7380240fe10855daad45a3
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-22 20:08:14 +0530

    `gpsbabel`: Revise version to force rebuild

--- packages/gpsbabel/build.sh
+++ packages/gpsbabel/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # 1.4.4 is the last version that does not require Qt dependency.
 TERMUX_PKG_VERSION=1.4.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/gpsbabel/gpsbabel/archive/gpsbabel_${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=22860e913f093aa9124e295d52d1d4ae1afccaa67ed6bed6f1f8d8b0a45336d1
 TERMUX_PKG_DEPENDS="libexpat"
