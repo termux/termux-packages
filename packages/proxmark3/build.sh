dca725daf5bb2d961ee181070d504f5f146c7ee8
Marlin Sööse <30473690+msoose@users.noreply.github.com>
2021-06-25 16:30:57 -0600

    proxmark3: fixup
    Added dependency
    Follow standards better

--- packages/proxmark3/build.sh
+++ packages/proxmark3/build.sh
@@ -5,16 +5,16 @@ TERMUX_PKG_MAINTAINER="Marlin Sööse <marlin.soose@laro.se>"
 TERMUX_PKG_VERSION="1:4.13441"
 TERMUX_PKG_SRCURL=https://github.com/RfidResearchGroup/proxmark3/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=49c4f1854b364aa7ea7083581351f867128e71ea783d0ecd71fc41bcf7f63584
-TERMUX_PKG_DEPENDS="libc++, readline"
+TERMUX_PKG_DEPENDS="libbz2, libc++, readline"
 TERMUX_PKG_BUILD_IN_SRC="true"
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, x86_64"
+TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=generic SKIPREVENGTEST=1 SKIPQT=1 SKIPBT=1 SKIPPTHREAD=1 SKIPGPU=1 PLATFORM=PM3GENERIC"
 
-termux_step_make() {
-	LDLIBS="-L${TERMUX_PREFIX}/lib" INCLUDES="-I${TERMUX_PREFIX}/include" \
-	make client -j $TERMUX_MAKE_PROCESSES CC=$CC CXX=$CXX LD=$CXX cpu_arch=generic SKIPREVENGTEST=1 SKIPQT=1 SKIPBT=1 SKIPPTHREAD=1
+termux_step_post_configure() {
+        LDLIBS="-L${TERMUX_PREFIX}/lib" INCLUDES="-I${TERMUX_PREFIX}/include"
 }
 
 termux_step_make_install() {
-	install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 \
-		"$TERMUX_PREFIX"/bin/proxmark3
+        install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 \
+                "$TERMUX_PREFIX"/bin/proxmark3
 }
