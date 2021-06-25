fb69ea8dfe019c83de83e49e0a9e49eab7e33479
Butta <repo@butta.fastem.com>
2021-06-25 12:28:47 +0530

    vis: update from 0.6 to 0.7

--- packages/vis/build.sh
+++ packages/vis/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.brain-dump.org/projects/vis/
 TERMUX_PKG_DESCRIPTION="Modern, legacy free, simple yet efficient vim-like editor"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=0.6
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.7
 TERMUX_PKG_SRCURL=https://github.com/martanne/vis/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9ab4a3f1c5953475130b3c286af272fe5cfdf7cbb7f9fbebd31e9ea4f34e487d
+TERMUX_PKG_SHA256=359ebb12a986b2f4e2a945567ad7587eb7d354301a5050ce10d51544570635eb
 TERMUX_PKG_DEPENDS="liblua53, libtermkey, lua-lpeg, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CFLAGS+=" -I$TERMUX_PREFIX/include/lua5.3"
+	CFLAGS+=" -I$TERMUX_PREFIX/include -I$TERMUX_PREFIX/include/lua5.3"
 }
 
 termux_step_create_debscripts() {
