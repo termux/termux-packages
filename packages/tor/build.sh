7bc8dc19bf3df6c8bbd559fbfab87962a0cd1315
vhnvn <vh@fake.com>
2021-06-27 17:27:45 +0700

    update revisions

--- packages/tor/build.sh
+++ packages/tor/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Onion Router anonymizing overlay network"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.4.5.7
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://www.torproject.org/dist/tor-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=447fcaaa133e2ef22427e98098a60a9c495edf9ff3e0dd13f484b9ad0185f074
 TERMUX_PKG_DEPENDS="libevent, openssl, liblzma, zlib, libandroid-glob"
