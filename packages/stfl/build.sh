f87115b669ba085a8fc667687b7f06239837ce7b
vhnvn <vh@fake.com>
2021-06-27 17:18:07 +0700

    stfl disable perl and ruby

--- packages/stfl/build.sh
+++ packages/stfl/build.sh
@@ -11,6 +11,15 @@ TERMUX_PKG_BREAKS="stfl-dev"
 TERMUX_PKG_REPLACES="stfl-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_pre_configure(){
+	# mkmf.rb can't find header files for ruby at /usr/lib/ruby/include/ruby.h
+	sed -i 's/FOUND_RUBY = 1/FOUND_RUBY = 0/g' Makefile.cfg
+	
+	# /usr/bin/ld: ../libstfl.a(public.o): Relocations in generic ELF (EM: 183)
+	# /usr/bin/ld: ../libstfl.a: error adding symbols: file in wrong format
+	sed -i 's/FOUND_PERL5 = 1/FOUND_PERL5 = 0/g' Makefile.cfg
+}
+
 termux_step_configure() {
 	CC+=" $CPPFLAGS"
 	export LDLIBS="-liconv"
