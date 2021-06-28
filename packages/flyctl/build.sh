4c28d839f16df3e176888c8e77e92f70c8728b68
YAKSH BARIYA <yakshbari4@gmail.com>
2021-06-28 23:58:55 +0530

    new package: flyctl (#7065)

---
+++ packages/flyctl/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://fly.io
+TERMUX_PKG_DESCRIPTION="Command line tools for fly.io services"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
+TERMUX_PKG_VERSION=0.0.227
+TERMUX_PKG_SRCURL=https://github.com/superfly/flyctl/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=39e7cc4c235434c49b3cdaca3a9592077b9c89eb83c58b65256e988d026b0fc6
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BLACKLISTED_ARCHES="i686, arm"
+
+termux_step_post_get_source() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_SRCDIR/go
+	go get
+	chmod 777 $GOPATH -R
+}
+
+termux_step_make() {
+	export GOPATH=$TERMUX_PKG_SRCDIR/go
+	go build -o bin/flyctl
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "$TERMUX_PKG_SRCDIR/bin/flyctl"
+}
