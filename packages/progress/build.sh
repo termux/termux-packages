f920e2dfae5f84d6a15ca7a14ad49d30baec015f
Lucy Phipps <landfillbaby69@gmail.com>
2021-06-28 20:03:33 +0100

    progress, renameutils, sc-im: -landroid-wordexp

--- packages/progress/build.sh
+++ packages/progress/build.sh
@@ -3,8 +3,12 @@ TERMUX_PKG_DESCRIPTION="Linux tool to show progress for cp, mv, dd and more"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.16
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Xfennec/progress/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=59944ee35f8ae6d62ed4f9b643eee2ae6d03825da288d9779dc43de41164c834
-TERMUX_PKG_DEPENDS="ncurses"
+TERMUX_PKG_DEPENDS="libandroid-wordexp, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-wordexp"
+}
--- packages/sc-im/build.sh
+++ packages/sc-im/build.sh
@@ -3,13 +3,15 @@ TERMUX_PKG_DESCRIPTION="An improved version of sc, a spreadsheet calculator"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/andmarti1424/sc-im/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7f00c98601e7f7709431fb4cbb83707c87016a3b015d48e5a7c2f018eff4b7f7
-TERMUX_PKG_DEPENDS="libandroid-support, libzip, ncurses"
+TERMUX_PKG_DEPENDS="libandroid-support, libandroid-wordexp, libzip, ncurses"
 TERMUX_PKG_SUGGESTS="gnuplot"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CFLAGS+=" $CPPFLAGS -I$TERMUX_PREFIX/include/libandroid-support -DGNUPLOT -DNO_WORDEXP"
+	CFLAGS+=" $CPPFLAGS -I$TERMUX_PREFIX/include/libandroid-support -DGNUPLOT"
+	LDFLAGS+=" -landroid-wordexp"
 	TERMUX_PKG_BUILDDIR+="/src"
 }
