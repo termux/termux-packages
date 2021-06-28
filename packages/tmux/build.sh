e251d79ea5be06a7237e8a874ff2e146c84f7ecc
buttaface <repo@butta.fastem.com>
2021-06-29 00:39:58 +0530

    tmux: update to 3.2a (#7051)

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
 TERMUX_PKG_DEPENDS="ncurses, libevent, libandroid-support, libandroid-glob"
-TERMUX_PKG_VERSION=3.2
+TERMUX_PKG_VERSION=3.2a
 TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=290a2f25a2f26c649f7ec7f2880586b8d3f43e24d7cb42c691f430941edb4fcf
+TERMUX_PKG_SHA256=497bc4ee16f10b53b161bf0253b6f9e20cd0f86c5d0104f149212cb0778ae13a
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
 TERMUX_PKG_BUILD_IN_SRC=true
 
