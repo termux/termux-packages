ea9383e5412fe3c8b1a79efe5aff1096b1ea0cf0
MrAdityaAlok <dev.aditya.alok@gmail.com>
2021-06-28 09:47:39 +0530

    cavif-rs : remove build steps handled by build scripts

--- packages/cavif-rs/build.sh
+++ packages/cavif-rs/build.sh
@@ -5,17 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/kornelski/cavif-rs/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1ee6198b2dc0843cddaeeaece964d2615ca84997265b72ff69ca3ef9e18ddf23
-TERMUX_PKG_BUILD_DEPENDS="nasm"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure(){
-	termux_setup_rust
-}
-
-termux_step_make(){
-	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
-}
-
-termux_step_make_install(){	
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/cavif
-}
