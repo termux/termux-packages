f204bb0555ad1a6a1375092a31671e5884a41ef6
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-29 10:52:37 +0530

    `enchant`: Bump to v2.3.0

--- packages/enchant/build.sh
+++ packages/enchant/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://abiword.github.io/enchant/
 TERMUX_PKG_DESCRIPTION="Wraps a number of different spelling libraries and programs with a consistent interface."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2.15
+TERMUX_PKG_VERSION=2.3.0
 TERMUX_PKG_SRCURL=https://github.com/AbiWord/enchant/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=85295934102a4ab94f209cbc7c956affcb2834e7a5fb2101e2db436365e2922d
+TERMUX_PKG_SHA256=e9b03d836f93a221b06334919227ccf4354796a9b05fb91340956fd12f0a4d57
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-relocatable" 
 TERMUX_PKG_DEPENDS="glib"
 
