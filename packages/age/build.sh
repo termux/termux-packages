a6d87b67d3a3c6e76343075b58631a6ca451e30c
Ridhwan Ikhwanto <ridhwan@ikhwanto.com>
2021-06-30 15:57:01 +0700

    `age`: bump to 1.0.0-rc.3

--- packages/age/build.sh
+++ packages/age/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/FiloSottile/age
 TERMUX_PKG_DESCRIPTION="A simple, modern and secure encryption tool with small explicit keys, no config options, and UNIX-style composability"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.0-rc.1
+TERMUX_PKG_VERSION=1.0.0-rc.3
 TERMUX_PKG_SRCURL=https://github.com/FiloSottile/age/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2ef0839b0e2e79435c037662c67df675b4ebc50d7bf001079d24b0bdf1d0c098
+TERMUX_PKG_SHA256=e352d8b4e08e0b493adf7ab3ea15b349fe07ab833d20395012aa516ea0c36708
 
 termux_step_make() {
 	termux_setup_golang
