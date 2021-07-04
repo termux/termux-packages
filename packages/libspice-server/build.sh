5185c0149ff0d70f207607b9d049e8113a11c8da
mjuned <junedmomin574@gmail.com>
2021-07-04 16:31:34 +0000

    New package : libspice-server v0.14.91

---
+++ packages/libspice-server/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://www.spice-space.org/
+TERMUX_PKG_DESCRIPTION="Implements the server side of the SPICE protocol"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.14.91
+TERMUX_PKG_SRCURL=https://www.spice-space.org/download/releases/spice-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=d29acdfe56a09a4f68ddb11e1be27029d478dd863c80245a02e2eaae54afa10f
+TERMUX_PKG_DEPENDS="glib, libjpeg-turbo, gstreamer, gst-plugins-base, libsasl, libspice-protocol, liborc, openssl, libopus, libpixman, liblz4, zlib, libiconv"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-manual=no
+--disable-tests
+"
