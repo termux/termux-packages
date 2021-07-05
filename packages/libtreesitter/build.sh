0dc7a0a5be7cafaa17edc3aebc2c0148d827c261
shadmansaleh <shadmansaleh3@gmail.com>
2021-07-05 07:14:13 +0600

    Remove override for make install

--- packages/libtreesitter/build.sh
+++ packages/libtreesitter/build.sh
@@ -6,10 +6,3 @@ TERMUX_PKG_VERSION=0.20.0
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4a8070b9de17c3b8096181fe8530320ab3e8cca685d8bee6a3e8d164b5fb47da
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make_install() {
-  make \
-    SONAME_MAJOR="0.20"\
-    SONAME_MINOR="0"\
-    install
-}
