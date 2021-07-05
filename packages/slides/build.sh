2be2df12246e35dbe3c64546c360b99499bab3f1
Yaksh Bariya <yakshbari4@gmail.com>
2021-07-05 17:10:06 +0530

    `slides`: Cache Golang dependencies

--- packages/slides/build.sh
+++ packages/slides/build.sh
@@ -5,12 +5,11 @@ TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
 TERMUX_PKG_VERSION=0.4.0
 TERMUX_PKG_SRCURL=https://github.com/maaslalani/slides/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=320285f179c4c2de7810f945f6c27c1d57d97f3b4ee322867248f7f1a7c06adf
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_golang
-
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	cd "$TERMUX_PKG_SRCDIR"
+	termux_go_get
 	make build
 }
 
