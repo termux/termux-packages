684734a2c500d9cd5be2b5bf387c2d8965099c91
Krishna Kanhaiya <kcubeterm@gmail.com>
2021-07-06 11:20:25 +0000

    xfce4-panel-profiles: Keep doc in subdirectory.. (#377)
    * xfce4-panel-profiles: Keep doc in subdirectory..
    
    $PREFIX/share/doc/xfce4-panel-profiles

--- x11-packages/xfce4-panel-profiles/build.sh
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-panel-profiles/start
 TERMUX_PKG_DESCRIPTION="A simple application to manage Xfce panel layouts."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.0.13
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION:0:3}/xfce4-panel-profiles-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=bc387c13f94109422dc72b0fcb919b0dc11619ba589d03e492252b0d2513b170
 TERMUX_PKG_DEPENDS="xfce4-panel, python"
@@ -14,7 +14,7 @@ termux_step_configure() {
     -e s,@appname@,xfce4-panel-profiles,g \
     -e s,@version@,$TERMUX_PKG_VERSION,g \
     -e s,@mandir@,$TERMUX_PREFIX/share/man,g \
-    -e s,@docdir@,$TERMUX_PREFIX/share/doc,g \
+    -e s,@docdir@,$TERMUX_PREFIX/share/doc/xfce4-panel-profiles,g \
     -e s,@python@,$TERMUX_PREFIX/bin/python,g \
      Makefile.in > Makefile
 }
