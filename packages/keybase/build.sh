eb88c48d392f37674ea18e2d0e88de6f2fe6d3c5
YAKSH BARIYA <yakshbari4@gmail.com>
2021-07-06 15:09:47 +0530

    `keybase`: Do not go mod tidy

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -17,7 +17,6 @@ termux_step_make_install() {
 	ln -sf "$PWD" .gopath/src/github.com/keybase/client
 	export GOPATH="$PWD/.gopath"
 	
-	go mod tidy
 	# TODO: Cache Golang dependencies
 	go build -v -tags 'production' -o keybase github.com/keybase/client/go/keybase
 	go build -v -tags 'production' -o git-remote-keybase github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase
