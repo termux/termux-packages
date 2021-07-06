4ef4eee4ea51824f61be0a749e5a1c0af2a0ef37
Yaksh Bariya <yakshbari4@gmail.com>
2021-07-06 17:02:07 +0530

    Fix build errors for keybase

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/keybase/client/archive/v${TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=9b51262582db59c0f9b3b63f488632f4cad2b769aded0aaaa8a4e5c85988e5bb
 TERMUX_PKG_REPLACES="kbfs"
 TERMUX_PKG_CONFLICTS="kbfs"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
@@ -18,9 +19,9 @@ termux_step_make_install() {
 	export GOPATH="$PWD/.gopath"
 	
 	# TODO: Cache Golang dependencies
-	go build -v -tags 'production' -o keybase github.com/keybase/client/go/keybase/main.go
-	go build -v -tags 'production' -o git-remote-keybase github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase/main.go
-	go build -v -tags 'production' -o kbfsfusebin github.com/keybase/client/go/kbfs/kbfsfuse/main.go
+	go build -v -tags 'production' -o keybase ./keybase/client/go/keybase/main.go
+	go build -v -tags 'production' -o git-remote-keybase ./keybase/client/go/kbfs/kbfsgit/git-remote-keybase/main.go
+	go build -v -tags 'production' -o kbfsfusebin ./keybase/client/go/kbfs/kbfsfuse/main.go
 
 	cp keybase $TERMUX_PREFIX/bin/keybase
 	cp git-remote-keybase $TERMUX_PREFIX/bin/git-remote-keybase
