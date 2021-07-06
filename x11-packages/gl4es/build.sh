c12dcc8ea53d7f437630f588efe8ba4791351ba3
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-07-06 18:35:48 +0530

    new package: gl4es (#373)

---
+++ x11-packages/gl4es/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://ptitseb.github.io/gl4es/
+TERMUX_PKG_DESCRIPTION="OpenGL driver for GLES devices"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@suhan-paradkar"
+TERMUX_PKG_VERSION=1.1.4
+TERMUX_PKG_SRCURL=https://github.com/ptitSeb/gl4es.git
+TERMUX_PKG_DEPENDS="libandroid-shmem, libexpat, libdrm, libx11, libxdamage, libxext, libxml2, libxshmfence, zlib"
+
