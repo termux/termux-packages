abf0a939766af0a7bda8f5e24aba48a42248de9a
Yonle <63401712+Yonle@users.noreply.github.com>
2021-07-06 19:54:59 +0700

    new package: sdl-ttf (#376)

---
+++ x11-packages/sdl-ttf/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.libsdl.org/projects/SDL_ttf
+TERMUX_PKG_DESCRIPTION="A companion library to SDL for working with TrueType (tm) fonts"
+TERMUX_PKG_LICENSE="ZLIB"
+TERMUX_PKG_MAINTAINER="@Yonle"
+TERMUX_PKG_VERSION=2.0.11
+TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=724cd895ecf4da319a3ef164892b72078bd92632a5d812111261cde248ebcdb7
+TERMUX_PKG_DEPENDS="sdl"
