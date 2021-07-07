dd7c8282bc15c386e9a578ac6568526d890e575d
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-08 00:07:07 +0300

    dtc: package switched to meson build system

--- packages/dtc/build.sh
+++ packages/dtc/build.sh
@@ -7,5 +7,3 @@ TERMUX_PKG_SRCURL=https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/dtc-
 TERMUX_PKG_SHA256=6401c9a0f577a270df4632bf0f3e5454ccc7a5ca3caefa67a3e1c29c9c6b8c60
 TERMUX_PKG_BREAKS="dtc-dev"
 TERMUX_PKG_REPLACES="dtc-dev"
-TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
-TERMUX_PKG_BUILD_IN_SRC=true
