03465edffa771b18feb7e712fe1f7ab104230c93
Yaksh Bariya <yakshbari4@gmail.com>
2021-07-09 19:42:09 +0530

    Indo all keybase changes

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://keybase.io
 TERMUX_PKG_DESCRIPTION="Key directory that maps social media identities to encryption keys"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.7.1
+TERMUX_PKG_VERSION=5.6.1
 TERMUX_PKG_SRCURL=https://github.com/keybase/client/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9b51262582db59c0f9b3b63f488632f4cad2b769aded0aaaa8a4e5c85988e5bb
+TERMUX_PKG_SHA256=d54c2444e18b83e6c3f39c3d96ee87d1c178fd2812cda0718b70d10289b76685
 TERMUX_PKG_REPLACES="kbfs"
 TERMUX_PKG_CONFLICTS="kbfs"
-TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
@@ -17,11 +16,10 @@ termux_step_make_install() {
 	mkdir -p .gopath/src/github.com/keybase
 	ln -sf "$PWD" .gopath/src/github.com/keybase/client
 	export GOPATH="$PWD/.gopath"
-	
-	# TODO: Cache Golang dependencies
-	go build -v -tags 'production' -o keybase ./keybase/client/go/keybase/main.go
-	go build -v -tags 'production' -o git-remote-keybase ./keybase/client/go/kbfs/kbfsgit/git-remote-keybase/main.go
-	go build -v -tags 'production' -o kbfsfusebin ./keybase/client/go/kbfs/kbfsfuse/main.go
+
+	go build -v -tags 'production' -o keybase github.com/keybase/client/go/keybase
+	go build -v -tags 'production' -o git-remote-keybase github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase
+	go build -v -tags 'production' -o kbfsfusebin github.com/keybase/client/go/kbfs/kbfsfuse
 
 	cp keybase $TERMUX_PREFIX/bin/keybase
 	cp git-remote-keybase $TERMUX_PREFIX/bin/git-remote-keybase
