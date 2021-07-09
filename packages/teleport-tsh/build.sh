0902b7e02a9e8cb925a497fa9bcb32c7cf7deceb
Yaksh Bariya <yakshbari4@gmail.com>
2021-07-09 22:15:12 +0530

    Add todo comment for teleport-tsh

--- packages/teleport-tsh/build.sh
+++ packages/teleport-tsh/build.sh
@@ -2,21 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gravitational/teleport
 TERMUX_PKG_DESCRIPTION="Secure Access for Developers that doesn't get in the way"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.1.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_VERSION=6.2.7
+TERMUX_PKG_SRCURL=https://github.com/gravitational/teleport/archive/refs/tags/v$TERMUX_PKG_VERSION.zip
+TERMUX_PKG_SHA256=89ac7e5164f6e40c00b469065727d4c71d8c4c6180b60c43958b2154e18069d5
 
 termux_step_make_install() {
 	termux_setup_golang
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	export BUILDDIR=$TERMUX_PREFIX/bin
-
-	mkdir -p $GOPATH/src/github.com/gravitational
-	cd $GOPATH/src/github.com/gravitational
-	git clone https://github.com/gravitational/teleport.git
-	cd teleport
-
-	git checkout "v$TERMUX_PKG_VERSION"
-
-	make $BUILDDIR/tsh
+	# TODO: Cache Golang dependency
+	make full
 }
