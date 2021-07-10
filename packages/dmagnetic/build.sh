c633044cc890e295f153d441eac7800c6226c5c7
Henrik Grimler <henrik@grimler.se>
2021-07-10 22:32:51 +0200

    dmagnetic: add magnetic-scrolls script for downloading games
    Helper script for downloading the game files, through a TUI.
    
    Also update some patch offsets while we are at it.

--- packages/dmagnetic/build.sh
+++ packages/dmagnetic/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interpreter for classic text adventure games and interac
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.31
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.dettus.net/dMagnetic/dMagnetic_${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1a0356f04d3a5e252225b0fd38b9047957f292f67338ba83579958b46f184139
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -18,4 +19,11 @@ termux_step_host_build() {
 termux_step_post_configure() {
 	# find our host-built dMagnetic
 	export PATH=$TERMUX_PKG_HOSTBUILD_DIR:$PATH
+}
+
+termux_step_post_make_install() {
+	sed "s%@TERMUX_PREFIX@%$TERMUX_PREFIX%g" \
+	    $TERMUX_PKG_BUILDER_DIR/magnetic-scrolls.in \
+	    > $TERMUX_PREFIX/bin/magnetic-scrolls
+	chmod 700 $TERMUX_PREFIX/bin/magnetic-scrolls
 }
