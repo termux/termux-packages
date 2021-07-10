9ccae8a6779c50d1e2c237e45c0e8530a03e1053
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2021-07-10 14:59:26 +0800

    squashfs-tools-ng: update to 1.1.2

--- packages/squashfs-tools-ng/build.sh
+++ packages/squashfs-tools-ng/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/AgentD/squashfs-tools-ng
 TERMUX_PKG_DESCRIPTION="New set of tools for working with SquashFS images"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="WMCB-Tech @marcusz"
-TERMUX_PKG_VERSION="1.1.1"
+TERMUX_PKG_VERSION="1.1.2"
 TERMUX_PKG_SRCURL="https://github.com/AgentD/squashfs-tools-ng/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256="e4d3dfc9f354267c65bf2dd7e483ad23a148d49b3946e2ab3918e1fbd69015c1"
+TERMUX_PKG_SHA256="da4c00938a1fdec89808e05f43c6875180de1990e71837030071d2b639456f22"
 TERMUX_PKG_DEPENDS="zlib, liblz4, liblzo, zstd"
 
 termux_step_pre_configure(){
