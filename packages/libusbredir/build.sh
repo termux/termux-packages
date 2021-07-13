cf36ebface8151cd59f149dec7d2696ad2ac5700
mjuned <junedmomin574@gmail.com>
2021-07-13 14:57:41 +0000

    New package : libusbredir

---
+++ packages/libusbredir/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.spice-space.org/
+TERMUX_PKG_DESCRIPTION="network protocol for sending USB device traffic over a network connection"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.7.1
+TERMUX_PKG_SRCURL=https://www.spice-space.org/download/usbredir/usbredir-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=407e9e27a1369f01264d5501ffbe88935ddd7d5de675f5835db05dc9c9ac56f3
+TERMUX_PKG_DEPENDS="libusb"
