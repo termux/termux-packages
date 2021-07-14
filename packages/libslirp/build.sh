138f6f603889ba3f42809bd39f692562fbaa6e95
ifurther <55025025+ifurther@users.noreply.github.com>
2021-07-14 22:23:39 +0800

    New package: libslirp

---
+++ packages/libslirp/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.freedesktop.org/slirp/libslirp
+TERMUX_PKG_DESCRIPTION="A general purpose TCP-IP emulator used by virtual machine hypervisors to provide virtual networking services."
+TERMUX_PKG_LICENSE="BSD-3-Clause"
+TERMUX_PKG_LICENSE_FILE="COPYRIGHT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=4.6.1
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v${TERMUX_PKG_VERSION}/libslirp-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=69ad4df0123742a29cc783b35de34771ed74d085482470df6313b6abeb799b11
+TERMUX_PKG_DEPENDS="glib, valgrind"
+#TERMUX_PKG_BUILD_IN_SRC=true
