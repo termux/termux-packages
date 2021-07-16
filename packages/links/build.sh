c4ff20ccf48915cbecad4301350bbb9fb980eefe
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-17 00:17:57 +0300

    links: update to 2.23

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://links.twibright.com
 TERMUX_PKG_DESCRIPTION="Links is a text and graphics mode WWW browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.22
+TERMUX_PKG_VERSION=2.23
 TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=991d77cf352238479ffbc36e03a37e8230c5757163350e05ba988a28939e284b
+TERMUX_PKG_SHA256=d7118bd9781608129f4ab5e56fcac1fdeeacfa5c2f404d29cbf89daab85b951d
 TERMUX_PKG_DEPENDS="brotli, libbz2, libevent, liblzma, libpng, librsvg, libtiff, libx11, openssl, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
 
