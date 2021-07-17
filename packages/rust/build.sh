8bff2a0005fd3652522035110adaf6451503ef96
wormus <annoyingwormus@gmail.com>
2021-07-16 23:57:10 -0300

    rust: update to 1.53.0

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
-TERMUX_PKG_VERSION=1.52.1
+TERMUX_PKG_VERSION=1.53.0
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
-TERMUX_PKG_SHA256=521bbaebce262264e44acb164d327cba27acf6f08bd7de672d8d1e16e7ed959e
+TERMUX_PKG_SHA256=e53a2df2c2c26f8929d551852eeae242b9a6167fba34481f92010b30def32dec
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib, libllvm"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/llvm-dwp"
 
@@ -19,7 +19,7 @@ termux_step_configure() {
 	# like 30 to 40 + minutes ... so lets get it right
 
 	# upstream only tests build ver one version behind $TERMUX_PKG_VERSION
-	local BOOTSTRAP_VERSION=1.51.0
+	local BOOTSTRAP_VERSION=1.52.0
 	rustup install $BOOTSTRAP_VERSION
 	rustup default $BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu
 	export PATH=$HOME/.rustup/toolchains/$BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu/bin:$PATH
