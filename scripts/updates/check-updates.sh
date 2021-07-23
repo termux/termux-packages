5b27021474a92ea58863d987cca0c63b30f1edc9
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-23 14:48:55 +0300

    automatic updates: perform verification build for at least 2 architectures
    Avoid cases when automatically submitted update passed build for AArch64 but
    failed for ARM.

--- scripts/updates/check-updates.sh
+++ scripts/updates/check-updates.sh
@@ -68,7 +68,8 @@ if [ -f "${BASEDIR}/github-projects.txt" ]; then
 				}
 
 				echo "Trying to build package '${package}'."
-				if "${BASEDIR}/../run-docker.sh" ./build-package.sh -a aarch64 -I "$package"; then
+				if "${BASEDIR}/../run-docker.sh" ./build-package.sh -a aarch64 -I "$package" && \
+					"${BASEDIR}/../run-docker.sh" ./build-package.sh -a arm -I "$package"; then
 					if [ "$GIT_COMMIT_PACKAGES" = "true" ]; then
 						git add "${BASEDIR}/../../packages/${package}"
 						git commit -m "$(echo -e "${package}: update to ${latest_version}\n\nThis commit has been automatically submitted by Github Actions.")"
