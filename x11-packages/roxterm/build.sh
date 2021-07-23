693bb8a03bf9ee17c7438c38f8b4dfa600d14b62
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-07-23 06:06:43 -0500

    new package: roxterm (#387)

---
+++ x11-packages/roxterm/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/realh/roxterm
+TERMUX_PKG_DESCRIPTION="ROXTerm is a terminal emulator based on the VTE library"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Yisus7u7"
+TERMUX_PKG_VERSION=3.11.1
+TERMUX_PKG_SRCURL=https://github.com/realh/roxterm/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=a362d4b6ca89091d277c710c6a4dcec67a9429d519086b911941b66af1e8e3e3
+TERMUX_PKG_DEPENDS="gtk3, libvte, xsltproc, pcre2, dbus-glib"
+TERMUX_PKG_BUILD_IN_SRC=true
