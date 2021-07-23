7cec68ac9a50c5a3019449cc7219c21019b5d43f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-23 16:53:49 +0300

    webkit2gtk: depend on libicu & fix build.sh formatting

--- x11-packages/webkit2gtk/build.sh
+++ x11-packages/webkit2gtk/build.sh
@@ -3,10 +3,18 @@ TERMUX_PKG_DESCRIPTION="A full-featured port of the WebKit rendering engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.31.1
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DPORT=GTK -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_GAMEPAD=OFF -DUSE_SYSTEMD=OFF -DUSE_LIBSECRET=OFF -DENABLE_INTROSPECTION=OFF"
 TERMUX_PKG_SHA256=6b1bb3e0efcfcb6e4a8e18b6a5f1cac27cda203d46a7dfbb0f150784a47e908f
-TERMUX_PKG_DEPENDS="libtasn1, libxt, libnotify, libcairo, libgcrypt, gtk3, libsoup, libwebp, libxslt, woff2, enchant, libhyphen, openjpeg, gst-plugins-base, gstreamer"
+TERMUX_PKG_DEPENDS="enchant, gst-plugins-base, gstreamer, gtk3, libcairo, libgcrypt, libhyphen, libicu, libnotify, libsoup, libtasn1, libwebp, libxslt, libxt, openjpeg, woff2"
 TERMUX_PKG_BREAKS="webkit, webkitgtk"
 TERMUX_PKG_REPLACES="webkit, webkitgtk"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DPORT=GTK
+-DCMAKE_BUILD_TYPE=RelWithDebInfo
+-DENABLE_GAMEPAD=OFF
+-DUSE_SYSTEMD=OFF
+-DUSE_LIBSECRET=OFF
+-DENABLE_INTROSPECTION=OFF
+"
