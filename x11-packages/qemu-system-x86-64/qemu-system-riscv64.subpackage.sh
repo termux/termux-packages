fc8eecbab9901c7322893242f1479dc3d1bfa9c9
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-23 15:59:46 +0300

    qemu: fix typo in subpackage definitions

--- x11-packages/qemu-system-x86-64/qemu-system-riscv64.subpackage.sh
+++ x11-packages/qemu-system-x86-64/qemu-system-riscv64.subpackage.sh
@@ -1,6 +1,6 @@
 TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator and virtualizer"
 TERMUX_SUBPKG_DEPENDS="attr, glib, libbz2, libc++, libcap-ng, libcurl, libgcrypt, libiconv, libjpeg-turbo, liblzo, libnfs, libpixman, libpng, libssh, libx11, ncurses, qemu-common, resolv-conf, sdl2, sdl2-image, zlib"
-TERMUX_SUBPKG_CONFLICTS="qemy-system-riscv64-headless"
+TERMUX_SUBPKG_CONFLICTS="qemu-system-riscv64-headless"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=no
 
 TERMUX_SUBPKG_INCLUDE="
