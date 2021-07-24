2e0bbf7234546e726979b14c161a2b35c21e4122
Yaksh Bariya <yakshbari4@gmail.com>
2021-07-24 22:28:50 +0530

    gping: update to 1.2.3

--- packages/gping/build.sh
+++ packages/gping/build.sh
@@ -2,8 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://github.com/orf/gping
 TERMUX_PKG_DESCRIPTION="Ping, but with a graph"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=1.2.1
-TERMUX_PKG_SRCURL=https://github.com/orf/gping/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ca11655f4357278a152476373cbc131b4183990e431196889c366706aedf6787
+TERMUX_PKG_VERSION=1.2.3
+TERMUX_PKG_SRCURL=https://github.com/orf/gping/archive/refs/tags/gping-v$TERMUX_PKG_VERSION.zip
+TERMUX_PKG_SHA256=647ac4643c0467a6ba0717f12e1813968554beb7c2709fa2d95941425d2abdc0
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_pre_configure() {
+	termux_setup_rust
+}
+
+termux_step_make() {
+	cd gping
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+	cd ..
+}
+
+termux_step_make_install() {
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/gping
+}
