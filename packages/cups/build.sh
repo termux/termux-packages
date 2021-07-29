b44048eb35d478c31f9584d3f2bbe0df038ce907
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-29 16:54:10 +0300

    cups: specify maintainer

--- packages/cups/build.sh
+++ packages/cups/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE="https://www.cups.org/"
 TERMUX_PKG_DESCRIPTION="Common UNIX Printing System"
 TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.3.3"
 TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL="https://github.com/apple/cups/releases/download/v${TERMUX_PKG_VERSION}/cups-${TERMUX_PKG_VERSION}-source.tar.gz"
