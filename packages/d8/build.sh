9163bdc7f809eef4283c42bdefc91e2b6127d570
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-07-30 21:20:38 +0300

    d8: create wrapper for r8 and specify main class in d8

--- packages/d8/build.sh
+++ packages/d8/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="DEX bytecode compiler from Android SDK"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=${TERMUX_ANDROID_BUILD_TOOLS_VERSION}
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_DEPENDS="openjdk-17"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -19,9 +19,16 @@ termux_step_pre_configure() {
 termux_step_make_install() {
 	install -Dm600 $ANDROID_HOME/build-tools/${TERMUX_PKG_VERSION}/lib/d8.jar \
 		$TERMUX_PREFIX/share/java/d8.jar
+
 	cat <<- EOF > $TERMUX_PREFIX/bin/d8
 	#!${TERMUX_PREFIX}/bin/sh
-	exec java -jar $TERMUX_PREFIX/share/java/d8.jar "\$@"
+	exec java -cp $TERMUX_PREFIX/share/java/d8.jar com.android.tools.r8.D8 "\$@"
 	EOF
-	chmod 700 $TERMUX_PREFIX/bin/d8
+
+	cat <<- EOF > $TERMUX_PREFIX/bin/r8
+	#!${TERMUX_PREFIX}/bin/sh
+	exec java -cp $TERMUX_PREFIX/share/java/d8.jar com.android.tools.r8.R8 "\$@"
+	EOF
+
+	chmod 700 $TERMUX_PREFIX/bin/d8 $TERMUX_PREFIX/bin/r8
 }
