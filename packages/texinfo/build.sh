9bfcffa286be4fbac60e17f4644230137b8f67c8
Yaksh Bariya <yakshbari4@gmail.com>
2021-08-02 19:11:12 +0530

    texinfo: Bump to 6.8

--- packages/texinfo/build.sh
+++ packages/texinfo/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/texinfo/
 TERMUX_PKG_DESCRIPTION="Documentation system for on-line information and printed output"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.7
+TERMUX_PKG_VERSION=6.8
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/texinfo/texinfo-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa
+TERMUX_PKG_SHA256=8eb753ed28bca21f8f56c1a180362aed789229bd62fff58bf8368e9beb59fec4
 # gawk is used by texindex:
 TERMUX_PKG_DEPENDS="libiconv, ncurses, perl, gawk"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-perl-xs"
