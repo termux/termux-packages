e24b836099fa60b645776f36290fe5e3e9c85dea
Ian Hu <hu2008yinxiang@163.com>
2021-08-02 08:38:08 +0000

    php-psr: remove optional TERMUX_PKG_LICENSE_FILE

--- packages/php-psr/build.sh
+++ packages/php-psr/build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/jbboehr/php-psr
 TERMUX_PKG_DESCRIPTION="PHP extension providing the accepted PSR interfaces"
 TERMUX_PKG_LICENSE="BSD Simplified"
-TERMUX_PKG_LICENSE_FILE=LICENSE.md
 TERMUX_PKG_MAINTAINER="ian4hu <hu2008yinxiang@163.com>"
 TERMUX_PKG_VERSION=1.1.0
 TERMUX_PKG_SRCURL=https://github.com/jbboehr/php-psr/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
