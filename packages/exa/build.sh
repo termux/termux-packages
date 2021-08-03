10607b2a6bc47c45cc7747ecb82763c978c5acbc
Laurent le Beau-Martin <laurent@lebeaumartin.com>
2021-08-03 15:56:01 -0400

    fix location of bash completions

--- packages/exa/build.sh
+++ packages/exa/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A modern replacement for ls"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.10.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/ogham/exa/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ff0fa0bfc4edef8bdbbb3cabe6fdbd5481a71abbbcc2159f402dea515353ae7c
 TERMUX_PKG_DEPENDS="zlib, libgit2"
@@ -25,14 +25,14 @@ termux_step_post_make_install() {
 	# but they were moved after the release
 	if [ -f 'completions/completions.bash' ]; then
 		install -Dm600 completions/completions.bash \
-			$TERMUX_PREFIX/share/bash-completions/exa
+			$TERMUX_PREFIX/share/bash-completion/completions/exa
 		install -Dm600 completions/completions.fish \
 			$TERMUX_PREFIX/share/fish/completions/exa.fish
 		install -Dm600 completions/completions.zsh \
 			$TERMUX_PREFIX/share/zsh/site-functions/_exa
 	else
 		install -Dm600 completions/bash/exa \
-			$TERMUX_PREFIX/share/bash-completions/exa
+			$TERMUX_PREFIX/share/bash-completion/completions/exa
 		install -Dm600 completions/fish/exa.fish \
 			$TERMUX_PREFIX/share/fish/completions/exa.fish
 		install -Dm600 completions/zsh/_exa \
