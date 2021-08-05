9e0233c06bb1248cb2e3a60031541fa9411194af
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-08-05 22:11:57 +0530

    new package: gradle (#7227)

---
+++ packages/gradle/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE=https://gradle.org/
+TERMUX_PKG_DESCRIPTION="Powerful build system for the JVM"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=7.2-rc-1
+TERMUX_PKG_SRCURL=https://services.gradle.org/distributions/gradle-$TERMUX_PKG_VERSION-all.zip
+TERMUX_PKG_SHA256=f5d0a78d8a838b5967bbabd10ecc414474e05b373fb34b33c917265138d78c57
+TERMUX_PKG_DEPENDS="openjdk-17"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+termux_step_make_install() {
+	rm -f ./bin/*.bat
+	rm -rf $TERMUX_PREFIX/opt/gradle
+	mkdir -p $TERMUX_PREFIX/opt/gradle
+	cp -r ./* $TERMUX_PREFIX/opt/gradle/
+	for i in $TERMUX_PREFIX/opt/gradle/bin/*; do
+		if [ ! -f "$i" ]; then
+			continue
+		fi
+		ln -sfr $i $TERMUX_PREFIX/bin/$(basename $i)
+	done
+}
+
