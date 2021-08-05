16f6bce53b3962b5e79d5b211912ee600dfcaef5
Lucy Phipps <landfillbaby69@gmail.com>
2021-08-05 02:53:04 +0100

    fix sleuthkit build.sh

--- packages/sleuthkit/build.sh
+++ packages/sleuthkit/build.sh
@@ -1,16 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://sleuthkit.org/
 TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library for digital forensics tools. "
 TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_LICENSE_FILE="
-licenses/README.md
-licenses/GNUv2-COPYING
-licenses/GNUv3-COPYING
-licenses/IBM-LICENSE
-licenses/Apache-LICENSE-2.0.txt
-licenses/cpl1.0.txt
-licenses/bsd.txt
-licenses/mit.txt
-"
+TERMUX_PKG_LICENSE_FILE="licenses/README.md, licenses/GNUv2-COPYING, licenses/GNUv3-COPYING, licenses/IBM-LICENSE, licenses/Apache-LICENSE-2.0.txt, licenses/cpl1.0.txt, licenses/bsd.txt, licenses/mit.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.11.0
 TERMUX_PKG_SRCURL=https://github.com/sleuthkit/sleuthkit/archive/sleuthkit-${TERMUX_PKG_VERSION}.tar.gz
