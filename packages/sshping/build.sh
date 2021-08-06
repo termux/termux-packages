f103586abaeb1df6f899d7621bd1b0fdd0a4f7c8
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-07 01:24:00 +0300

    Update build.sh

--- packages/sshping/build.sh
+++ packages/sshping/build.sh
@@ -6,8 +6,14 @@ TERMUX_PKG_VERSION=0.1.4
 TERMUX_PKG_SRCURL=https://github.com/spook/sshping/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=589623e3fbe88dc1d423829e821f9d57f09aef0d9a2f04b7740b50909217863a
 TERMUX_PKG_DEPENDS="libssh"
+TERMUX_PKG_EXTRA_MAKE_ARGS="sshping man"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	rm -f CMakeLists.txt
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin ./bin/sshping
+	install -Dm600 -t $TERMUX_PREFIX/share/man/man8 ./doc/sshping.8.gz
 }
