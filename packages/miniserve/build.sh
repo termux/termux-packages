9a7416dcd56e7e39cb652d11a9970dfd763c9ec9
Pooya Moradi <pvonmoradi@gmail.com>
2021-08-06 20:43:11 +0430

    miniserve: install shell completions and add warn for default behaviour

--- packages/miniserve/build.sh
+++ packages/miniserve/build.sh
@@ -11,3 +11,23 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_configure() {
 	rm -f Makefile
 }
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+
+	# Generating completions
+	mkdir -p "$TERMUX_PREFIX"/share/{bash-completion/completions,zsh/site-functions,fish/vendor_completions.d}
+	miniserve --print-completions bash \
+		> "$TERMUX_PREFIX"/share/bash-completion/completions/miniserve
+	miniserve --print-completions zsh \
+		> "$TERMUX_PREFIX"/share/zsh/site-functions/_miniserve
+	miniserve --print-completions fish \
+		> "$TERMUX_PREFIX"/share/fish/vendor_completions.d/miniserve.fish
+
+	# Warn user on default behaviour of miniserve
+	printf "\e[1;33mWARNING:\e[0m %s %s\n%s\n" \
+		"miniserve follows symlinks in selected directory by default." \
+		"Consider aliasing it with '--no-symlinks' for safety." \
+		"See: https://github.com/svenstaro/miniserve/issues/498"
+	EOF
+}
