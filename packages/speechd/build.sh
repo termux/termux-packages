2820acc69177d8c8a10471559756f34fde57e02f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-06 17:43:28 +0300

    speechd: add note about pthread_cancel/pthread_kill issue

--- packages/speechd/build.sh
+++ packages/speechd/build.sh
@@ -1,17 +1,26 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/brailcom/speechd
 TERMUX_PKG_DESCRIPTION="Common interface to speech synthesis"
-TERMUX_PKG_VERSION=0.10.2
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.10.2
 TERMUX_PKG_SHA256=056efb80186f2bfc3f6ef468ab9905ca2d652cc2180b13514b752a9d616a6930
 TERMUX_PKG_SRCURL=https://github.com/brailcom/speechd/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libtool, glib, pulseaudio, espeak, dotconf, python"
+
+##
+## Note: package needs patching for proper fix of pthread_cancel usage.
+## Right now it uses pthread_kill(t, 0) which does nothing and is not
+## correct solution to the missing pthread_cancel on Android OS.
+## See package "calcurse" for example of proper pthread_cancel replacement.
+##
+
 # Fails to find generated headers
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-pulse
 --with-espeak-ng
 "
+
 # spd-conf needs python stuff, so remove for now
 TERMUX_PKG_RM_AFTER_INSTALL="bin/spd-conf"
 
