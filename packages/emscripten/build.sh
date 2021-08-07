5d0218f24ca502c23e65fcf434ffaef233b078ce
Jia Yuan Lo <jylo06g@gmail.com>
2021-08-08 00:07:37 +0800

    emscripten: change from using zip to tar.gz

--- packages/emscripten/build.sh
+++ packages/emscripten/build.sh
@@ -21,19 +21,19 @@ TERMUX_PKG_NO_STATICSPLIT=true
 #DEPS_JSON=$(echo -e "{\n${DEPS_REVISION}EOL" | sed -e "s|,EOL|\n}|")
 #LLVM_COMMIT=$(echo $DEPS_JSON | python3 -c "import json,sys;print(json.load(sys.stdin)[\"llvm_project_revision\"])")
 #BINARYEN_COMMIT=$(echo $DEPS_JSON | python3 -c "import json,sys;print(json.load(sys.stdin)[\"binaryen_revision\"])")
-#curl -LOC - https://github.com/llvm/llvm-project/archive/$LLVM_COMMIT.zip
-#curl -LOC - https://github.com/WebAssembly/binaryen/archive/$BINARYEN_COMMIT.zip
-#sha256sum $LLVM_COMMIT.zip $BINARYEN_COMMIT.zip
+#curl -LOC - https://github.com/llvm/llvm-project/archive/$LLVM_COMMIT.tar.gz
+#curl -LOC - https://github.com/WebAssembly/binaryen/archive/$BINARYEN_COMMIT.tar.gz
+#sha256sum $LLVM_COMMIT.tar.gz $BINARYEN_COMMIT.tar.gz
 
 # https://github.com/emscripten-core/emscripten/issues/11362
 # can switch to stable LLVM to save space once above is fixed
 LLVM_COMMIT=31e75512174e1bdaa242ee5c7f30fe56e68c3748
-LLVM_ZIP_SHA256=0e791aa12720be6ff02437e4e43312892e1bc37fa219e7c79c15b2349b235372
+LLVM_TGZ_SHA256=90cb885839e32ffd415bddcae21aef5f0f6f939c578723beb66f837be6fd9340
 
 # https://github.com/emscripten-core/emscripten/issues/12252
 # upstream says better bundle the right binaryen revision for now
 BINARYEN_COMMIT=f09bb989a15451960c1078426b61dcc50f232a0a
-BINARYEN_ZIP_SHA256=1c739ee262cd4634cb595afdff5abd2f9ea87c32e2977163bac0860b0862e282
+BINARYEN_TGZ_SHA256=6f3371632bfeeb02cdebccc0b5ea66342acc84ba8d558a86ab40469d90994e18
 
 # https://github.com/emscripten-core/emsdk/blob/main/emsdk.py
 # https://chromium.googlesource.com/emscripten-releases/+/refs/heads/main/src/build.py
@@ -80,15 +80,15 @@ BINARYEN_BUILD_ARGS="
 
 termux_step_post_get_source() {
 	termux_download \
-		"https://github.com/llvm/llvm-project/archive/$LLVM_COMMIT.zip" \
-		"$TERMUX_PKG_CACHEDIR/llvm.zip" \
-		"$LLVM_ZIP_SHA256"
+		"https://github.com/llvm/llvm-project/archive/$LLVM_COMMIT.tar.gz" \
+		"$TERMUX_PKG_CACHEDIR/llvm.tar.gz" \
+		"$LLVM_TGZ_SHA256"
 	termux_download \
-		"https://github.com/WebAssembly/binaryen/archive/$BINARYEN_COMMIT.zip" \
-		"$TERMUX_PKG_CACHEDIR/binaryen.zip" \
-		"$BINARYEN_ZIP_SHA256"
-	unzip -qo "$TERMUX_PKG_CACHEDIR/llvm.zip" -d "$TERMUX_PKG_CACHEDIR"
-	unzip -qo "$TERMUX_PKG_CACHEDIR/binaryen.zip" -d "$TERMUX_PKG_CACHEDIR"
+		"https://github.com/WebAssembly/binaryen/archive/$BINARYEN_COMMIT.tar.gz" \
+		"$TERMUX_PKG_CACHEDIR/binaryen.tar.gz" \
+		"$BINARYEN_TGZ_SHA256"
+	tar -xf "$TERMUX_PKG_CACHEDIR/llvm.tar.gz" -C "$TERMUX_PKG_CACHEDIR"
+	tar -xf "$TERMUX_PKG_CACHEDIR/binaryen.tar.gz" -C "$TERMUX_PKG_CACHEDIR"
 }
 
 termux_step_host_build() {
