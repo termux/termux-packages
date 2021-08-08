c7133d02e6c8cfe9ddf85426e1e6b2054782e08c
DLC01 <74525818+DLC01@users.noreply.github.com>
2021-08-08 11:12:44 +0530

    remove styles.css from license

--- packages/exhale/build.sh
+++ packages/exhale/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/ecodis/exhale
 TERMUX_PKG_DESCRIPTION="Open source xHE-AAC encoder"
 TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_LICENSE_FILE="include/License.htm, include/styles.css"
+TERMUX_PKG_LICENSE_FILE="include/License.htm"
 TERMUX_PKG_MAINTAINER="@DLC01"
 TERMUX_PKG_VERSION=1.1.6
 TERMUX_PKG_SRCURL=https://gitlab.com/ecodis/exhale/-/archive/v${TERMUX_PKG_VERSION}/exhale-v${TERMUX_PKG_VERSION}.tar.gz
