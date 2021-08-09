ef747abe8f39eb1577e83b0b6bcbbc9ea010ed9f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-09 15:07:33 +0300

    pidgin: update to 2.14.6

--- x11-packages/pidgin/build.sh
+++ x11-packages/pidgin/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://pidgin.im/
 TERMUX_PKG_DESCRIPTION="Multi-protocol instant messaging client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.14.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.14.6
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/pidgin/files/Pidgin/${TERMUX_PKG_VERSION}/pidgin-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=58652e692539d4d2cc775649f1d43dd08351607d8dc8fbab97a93629e01225c1
+TERMUX_PKG_SHA256=3bbeaf777da68efa6547bee9e15f7e863a23df255c55b625ef83d63819f8e457
 TERMUX_PKG_DEPENDS="gtk2, libgnutls, libidn, libsasl, libsm, libxext, libxss"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
