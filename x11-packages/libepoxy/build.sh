0b442977f8da47a0211d6870d5d21f4152c44917
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-09 14:49:48 +0300

    libepoxy: update to 1.5.8

--- x11-packages/libepoxy/build.sh
+++ x11-packages/libepoxy/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/anholt/libepoxy
 TERMUX_PKG_DESCRIPTION="Library handling OpenGL function pointer management"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.5.7
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.5.8
 TERMUX_PKG_SRCURL=https://github.com/anholt/libepoxy/releases/download/${TERMUX_PKG_VERSION}/libepoxy-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=9479cc0146ffb395fdecf9bd2a5930834fd0bce490cbcc4681ffd716bb3a0763
+TERMUX_PKG_SHA256=cf05e4901778c434aef68bb7dc01bea2bce15440c0cecb777fb446f04db6fe0d
 TERMUX_PKG_DEPENDS="mesa"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
