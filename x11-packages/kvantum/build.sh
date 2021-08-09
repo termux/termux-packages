f479963ab415e8ed3f18547343abcc999b13e5e3
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-09 14:50:58 +0300

    kvantum: update to 0.20.1

--- x11-packages/kvantum/build.sh
+++ x11-packages/kvantum/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/tsujan/Kvantum
 TERMUX_PKG_DESCRIPTION="SVG-based theme engine for Qt5"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=0.19.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.20.1
 TERMUX_PKG_SRCURL="https://github.com/tsujan/Kvantum/releases/download/V${TERMUX_PKG_VERSION}/Kvantum-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=c3c73ee62c23c60342b6a2aaca670e2a6316bc17b51d8666278399c36085ee7b
+TERMUX_PKG_SHA256=c823c70eabd7b02ac4661b989d858ac411da29d7a0de96afd464da8855132925
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtx11extras, qt5-qtsvg, kwindowsystem"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
