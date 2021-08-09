d794384dc908b749a37ccf04069d5353e967910f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-09 15:59:59 +0300

    revert mate packages update
    Need automake v1.16.2 but we have v1.16.1 in Docker image.

--- x11-packages/mate-session-manager/build.sh
+++ x11-packages/mate-session-manager/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://mate-session-manager.mate-desktop.dev/
 TERMUX_PKG_DESCRIPTION="mate-session contains the MATE session manager, as well as a configuration program to choose applications starting on login."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.26.0
+TERMUX_PKG_VERSION=1.24.3
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-session-manager/releases/download/v$TERMUX_PKG_VERSION/mate-session-manager-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=5915a2f6583c0e5e58afb3abae3f4adbbe6a003c174a5b6e3d204b4e398a1816
+TERMUX_PKG_SHA256=90a0aec5b59b6287b4d2c4d452b0b6410f9d12490ca1f890e81ba2801bdab0a2
 TERMUX_PKG_DEPENDS="libsm, dbus-glib, gtk3, glib"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
--- x11-packages/mate-terminal/build.sh
+++ x11-packages/mate-terminal/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://mate-terminal.mate-desktop.dev/
 TERMUX_PKG_DESCRIPTION="This is the MATE terminal emulator application"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.26.0
+TERMUX_PKG_VERSION=1.24.1
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-terminal/releases/download/v$TERMUX_PKG_VERSION/mate-terminal-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=7727e714c191c3c55e535e30931974e229ca5128e052b62ce74dcc18f7eaaf22
+TERMUX_PKG_SHA256=550d38f223d21ab12d39b00af6cd75f083d3790c38d53051537df2ac6a87be62
 TERMUX_PKG_DEPENDS="libvte, dconf, gtk3, libsm"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
