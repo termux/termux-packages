7c4c5a7f120a9e45ae9aadae1e271facf96cabad
Pooya Moradi <pvonmoradi@gmail.com>
2021-08-10 12:11:16 +0430

    new package: angle-grinder

---
+++ packages/angle-grinder/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/rcoh/angle-grinder
+TERMUX_PKG_DESCRIPTION="Slice and dice logs on the command line"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.17.0
+TERMUX_PKG_SRCURL="https://github.com/rcoh/angle-grinder/archive/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=5a2054ba9eb5fcee1fef2d5fbbf1735e71bc563fc7b273890097f63297e64bf4
+TERMUX_PKG_BUILD_IN_SRC=true
