2fed51323f9811c852ef9db826ef655dd5d66702
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-13 12:25:49 +0300

    proxmark3-git: remove revision when updating package version

--- packages/proxmark3-git/build.sh
+++ packages/proxmark3-git/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="The Swiss Army Knife of RFID Research - RRG/Iceman repo"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Marlin Sööse <marlin.soose@laro.se>"
 _COMMIT="a6cb675a5b47de42cec5f8aafc9eefd60439f052"
-TERMUX_PKG_REVISION=6
 TERMUX_PKG_VERSION="2021.08.12-${_COMMIT:0:8}"
 TERMUX_PKG_SRCURL="https://github.com/RfidResearchGroup/proxmark3/archive/a6cb675a5b47de42cec5f8aafc9eefd60439f052.tar.gz"
 TERMUX_PKG_SHA256="2ff93e4d673666793f6a9fa098ab4bcac151c62ed11d909354446dc862a94398"
