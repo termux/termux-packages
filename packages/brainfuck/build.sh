a4ea745c86c714a35369d3a224b6b93c60f6c5fb
Nisheri-kun <50684527+nisheri-ascar@users.noreply.github.com>
2021-08-17 19:35:09 +0800

    Add brainfuck package

---
+++ packages/brainfuck/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/fabianishere/brainfuck
+TERMUX_PKG_DESCRIPTION="Brainfuck Interpreter written in C"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.7.1
+TERMUX_PKG_SRCURL="https://github.com/nisheri-ascar/brainfuck/archive/refs/tags/v2.7.1-termux.tar.gz"
+TERMUX_PKG_SHA256=e6be51a1f37b481eac42529d8398fc45b921c026c141777c333fb7379ad7d6b9
