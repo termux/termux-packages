502adaa1109b6c49c453d78b534823b8cd471127
Lucy Phipps <landfillbaby69@gmail.com>
2021-08-17 11:20:59 +0100

    revert the imagamegick-libjxl stuff, sorry

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a v
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.1.0.4
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_REVISION=8
 TERMUX_PKG_SRCURL=https://download.imagemagick.org/ImageMagick/download/ImageMagick-$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.lz
 TERMUX_PKG_SHA256=5959267fe286698ad1f249dd85a8776f121a1a181012b3358b5142957da58b3a
-TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libheif, libjpeg-turbo, libjxl, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib"
+TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libheif, libjpeg-turbo, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -17,7 +17,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-xml=yes
 --with-rsvg=yes
 --with-lzma
---with-jxl=yes
 --disable-openmp
 ac_cv_func_ftime=no
 "
@@ -31,7 +30,4 @@ termux_step_pre_configure() {
 		#Avoid "libMagickCore-7.Q16HDRI.so: error: undefined reference to '__atomic_load'"
 		LDFLAGS+=" -latomic"
 	fi
-
-	# for allow-static-libjxl.patch
-	autoconf
 }
