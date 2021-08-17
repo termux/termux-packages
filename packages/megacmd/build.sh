b1c4f5d467a1d9cb4028b7d18884965c11c353f1
Aditya Alok <dev.aditya.alok@gmail.com>
2021-08-17 16:14:06 +0530

    new

---
+++ packages/megacmd/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/meganz/MEGAcmd"
+TERMUX_PKG_DESCRIPTION="Command Line Interactive and Scriptable Application to access MEGA Resources"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.4.0_Linux"
+TERMUX_PKG_SRCURL="https://github.com/meganz/MEGAcmd/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=030303030lsleo03eo
+TERMUX_PKG_DEPENDS="cryptopp, zlib, libsqlite, c-ares, libuv, openssl, curl, libsodium, readline, pcre"
+TERMUX_PKG_BUILD_IN_SRC=true
