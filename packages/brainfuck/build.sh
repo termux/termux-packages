4572ec9ece5314e97393a7c56f2f55a7a80153e3
Nisheri-kun <50684527+nisheri-ascar@users.noreply.github.com>
2021-08-17 20:01:42 +0800

    Update build.sh

--- packages/brainfuck/build.sh
+++ packages/brainfuck/build.sh
@@ -3,5 +3,5 @@ TERMUX_PKG_DESCRIPTION="Brainfuck Interpreter written in C"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.7.1
-TERMUX_PKG_SRCURL="https://github.com/nisheri-ascar/brainfuck/archive/refs/tags/v2.7.1-termux.tar.gz"
-TERMUX_PKG_SHA256=e6be51a1f37b481eac42529d8398fc45b921c026c141777c333fb7379ad7d6b9
+TERMUX_PKG_SRCURL="https://github.com/fabianishere/brainfuck/archive/refs/tags/2.7.1.tar.gz"
+TERMUX_PKG_SHA256=06534de715dbc614f08407000c2ec6d497770069a2d7c84defd421b137313d71
