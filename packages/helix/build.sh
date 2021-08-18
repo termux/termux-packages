3c952cfba4e95c387559f3cdb027d51fba52348a
Aditya Alok <dev.aditya.alok@gmail.com>
2021-08-18 15:50:43 +0530

    Helix: fix libc++_shared not found
    helix: try to fix
    
    add LDFLAGS manually
    
    helix: remove LD_LIBRARY_PATH
    
    helix: fix patch
    
    helix: try using RUSTFLAGS
    
    helix: add verbose for debug
    
    helix: try few more ENV variables
    
    helix: try patching build command
    
    helix: fix compiler not found
    
    helix: debug
    
    helix: remove debug command
    
    helix: use CC instead of CXX
    
    helix: maybe this could fix
    
    helix: debug
    
    helix: debug
    
    helix: debug
    
    helix: debug
    
    helix: debug
    
    helix: try to fail and check what flags are being passed
    
    helix: try again
    
    helix: try again
    
    helix: try again
    
    helix: should be fixed now
    
    helix: fix flags
    
    helix: fix flags
    
    helix: fix libc++_shared not found
    
    finally did it!

--- packages/helix/build.sh
+++ packages/helix/build.sh
@@ -1,23 +1,31 @@
 TERMUX_PKG_HOMEPAGE="https://helix-editor.com/"
 TERMUX_PKG_DESCRIPTION="A post-modern modal text editor written in rust"
 TERMUX_PKG_LICENSE="MPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="@MrAdityaAlok <dev.aditya.alok@gmail.com>"
 TERMUX_PKG_VERSION=0.4.1
 TERMUX_PKG_SRCURL="https://github.com/helix-editor/helix.git"
-TERMUX_PKG_GIT_BRANCH=v$TERMUX_PKG_VERSION
+TERMUX_PKG_GIT_BRANCH="v$TERMUX_PKG_VERSION"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	termux_setup_rust
-	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+
+	sed -i "s%\@TERMUX_CC\@%${CC}%g" ./helix-syntax/build.rs
+
+	cargo build --jobs "${TERMUX_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
 
 	cat > "hx" <<- EOF
 		#!${TERMUX_PREFIX}/bin/sh
-		HELIX_RUNTIME=${TERMUX_PREFIX}/lib/helix/runtime exec ${TERMUX_PREFIX}/lib/helix/hx "\$@"
-	EOF
-	install -Dm755 ./hx $TERMUX_PREFIX/bin/hx
 
-	mkdir -p ${TERMUX_PREFIX}/lib/helix
-	cp -r runtime ${TERMUX_PREFIX}/lib/helix
-	install -Dm755 -t ${TERMUX_PREFIX}/lib/helix target/${CARGO_TARGET_NAME}/release/hx
+		HELIX_RUNTIME=${TERMUX_PREFIX}/lib/helix/runtime \\
+		exec ${TERMUX_PREFIX}/lib/helix/hx "\$@"
+	EOF
+	install -Dm755 ./hx "${TERMUX_PREFIX}/bin/hx"
+
+	mkdir -p "${TERMUX_PREFIX}/lib/helix"
+	cp -r runtime "${TERMUX_PREFIX}/lib/helix"
+	install -Dm755 -t "${TERMUX_PREFIX}/lib/helix" "target/${CARGO_TARGET_NAME}/release/hx"
+
+	mkdir -p "${TERMUX_ANDROID_HOME}/.config/helix"
+	install -Dm666 -t "${TERMUX_ANDROID_HOME}/.config/helix" languages.toml
 }
