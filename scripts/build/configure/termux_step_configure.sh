12e395d3a9386b1ad9a7ca70fd516c32bc96fe09
Henrik Grimler <henrik@grimler.se>
2021-08-22 12:48:47 +0200

    step_configure_cmake: mv cmake and ninja setup functions to step_configure
    Only keep actual configure steps in termux_step_configure_cmake. This
    way we can skip termux_step_configure_cmake on a continued build.

--- scripts/build/configure/termux_step_configure.sh
+++ scripts/build/configure/termux_step_configure.sh
@@ -7,14 +7,19 @@ termux_step_configure() {
 		fi
 		termux_step_configure_autotools
 	elif [ "$TERMUX_PKG_FORCE_CMAKE" = "true" ] || [ -f "$TERMUX_PKG_SRCDIR/CMakeLists.txt" ]; then
-		if [ "$TERMUX_CONTINUE_BUILD" == "true" ]; then
-			termux_setup_cmake
-			if [ "$TERMUX_CMAKE_BUILD" = Ninja ]; then
-				termux_setup_ninja
-			fi
-			return;
+		termux_setup_cmake
+		if [ "$TERMUX_CMAKE_BUILD" = Ninja ]; then
+			termux_setup_ninja
 		fi
-		termux_step_configure_cmake
+
+		# Some packages, for example swift, uses cmake
+		# internally, but cannot be configured with our
+		# termux_step_configure_cmake function (CMakeLists.txt
+		# is not in src dir)
+		if [ -f "$TERMUX_PKG_SRCDIR/CMakeLists.txt" ] && \
+			[ "$TERMUX_CONTINUE_BUILD" == "false" ]; then
+			termux_step_configure_cmake
+		fi
 	elif [ -f "$TERMUX_PKG_SRCDIR/meson.build" ]; then
 		if [ "$TERMUX_CONTINUE_BUILD" == "true" ]; then
 			return;
