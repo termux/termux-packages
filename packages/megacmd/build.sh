a8ffe32bb65d2d24deade521637721c8744c7731
Aditya Alok <dev.aditya.alok@gmail.com>
2021-08-23 09:28:07 +0530

    megacmd: try again

--- packages/megacmd/build.sh
+++ packages/megacmd/build.sh
@@ -7,8 +7,8 @@ TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}_Linux"
 TERMUX_PKG_SRCURL="https://github.com/meganz/MEGAcmd.git"
 TERMUX_PKG_DEPENDS="cryptopp, zlib, libsqlite, c-ares, libuv, openssl, curl, libsodium, readline, pcre, freeimage,ffmpeg, libmediainfo"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-examples --disable-silent-rules"
-TERMUX_PKG_EXTRA_MAKE_ARGS="CXXFLAGS=-std=c++11"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-examples"
+# TERMUX_PKG_EXTRA_MAKE_ARGS="CXXFLAGS=-std=c++11"
 
 termux_step_pre_configure() {
 	sh autogen.sh
