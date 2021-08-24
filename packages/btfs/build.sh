df5b043c39e4b346d6bcdffb60f027e238d5034e
Lucy Phipps <landfillbaby69@gmail.com>
2021-08-24 20:10:15 +0100

    bump btfs, busybox, chezmoi
    https://github.com/termux/termux-packages/pull/7335#issuecomment-904202191

--- packages/btfs/build.sh
+++ packages/btfs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Decentralized file system integrating with TRON network"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/TRON-US/go-btfs/archive/refs/tags/btfs-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a7054a8cce28a6dee6f6eb1be262309128bbca8d572ca73154907997fd97d7ed
 TERMUX_PKG_BUILD_IN_SRC=true
