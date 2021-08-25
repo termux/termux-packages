9335848a5c6140caff83920eb7e2c5cb634aa3a5
ifurther <55025025+ifurther@users.noreply.github.com>
2021-08-25 21:24:08 +0800

    test some setting

--- packages/simh/build.sh
+++ packages/simh/build.sh
@@ -5,10 +5,10 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.11-1
 TERMUX_PKG_SRCURL=https://github.com/simh/simh/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c8a2fc62bfa9369f75935950512a4cac204fd813ce6a9a222b2c6a76503befdb
-TERMUX_PKG_DEPENDS="libpcap, libsdl, libpng"
+TERMUX_PKG_DEPENDS="libc++, libpcap, libsdl, libpng"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	sed 's/math/c++\/v1\/math/g' -i */*.c
-	make GCC=$CC LDFLAGS="$LDFLAGS" LIBRARIES="${TERMUX_PREFIX}/lib" INCLUDES="${TERMUX_PREFIX}/include"
+	make GCC=$CC LDFLAGS="$LDFLAGS" LIBRARIES="${TERMUX_PREFIX}/lib" INCLUDES="${TERMUX_PREFIX}/include:${TERMUX_PREFIX}/include/c++/v1"
 }
