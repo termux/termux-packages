adf80396dd70a49713adb8d1dc0952ecf179f8a7
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-25 14:43:06 +0300

    lxappearace: switch upstream & fix packaging
    Fixes https://github.com/termux/x11-packages/issues/424

--- x11-packages/lxappearance/build.sh
+++ x11-packages/lxappearance/build.sh
@@ -2,18 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://www.lxde.org/
 TERMUX_PKG_DESCRIPTION="LXDE GTK+ theme switcher"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7"
-TERMUX_PKG_REVISION=2
 TERMUX_PKG_VERSION=0.6.3
-TERMUX_PKG_SRCURL=https://github.com/lxde/lxappearance/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0b74a0ed0eb6f221b027a30c4829015794fefab7b2ce714ed6194454af7c08ce
-TERMUX_PKG_DEPENDS="gtk2, glib, gtk3, libx11, xsltproc"
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/lxde/lxappearance-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=7222d858b8fef4b7967c42142d61e82ded6dd42dc5ef1d59caad775795928b38
+TERMUX_PKG_DEPENDS="dbus-glib, glib, gtk3, libx11, xsltproc"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk3 --enable-dbus"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	./autogen.sh
-}
-
-termux_step_make_install () {
-	cd $TERMUX_PKG_SRCDIR/src
-	make install 
-}
+TERMUX_PKG_MAKE_PROCESSES=1
