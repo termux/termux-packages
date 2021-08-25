985b4435d0d83434e24c36e21ac9b35df1385c26
Momin Juned <68558066+mjuned47@users.noreply.github.com>
2021-08-25 17:08:56 +0530

    libspice-server : update to 0.15.0

--- packages/libspice-server/build.sh
+++ packages/libspice-server/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.spice-space.org/
 TERMUX_PKG_DESCRIPTION="Implements the server side of the SPICE protocol"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.14.91
-TERMUX_PKG_SRCURL=https://www.spice-space.org/download/releases/spice-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=d29acdfe56a09a4f68ddb11e1be27029d478dd863c80245a02e2eaae54afa10f
+TERMUX_PKG_VERSION=0.15.0
+TERMUX_PKG_SRCURL=https://www.spice-space.org/download/releases/spice-server/spice-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=b320cf8f4bd2852750acb703c15b72856027e5a8554f8217dfbb3cc09deba0f5
 TERMUX_PKG_DEPENDS="glib, libjpeg-turbo, gstreamer, gst-plugins-base, libsasl, libspice-protocol, liborc, openssl, libopus, libpixman, liblz4, zlib, libiconv"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
