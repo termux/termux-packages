014a4bd82576c89e5e60345472e56f5347508f35
Aditya Alok <dev.aditya.alok@gmail.com>
2021-08-26 15:07:16 +0530

    megacmd: try again

--- packages/megacmd/build.sh
+++ packages/megacmd/build.sh
@@ -15,7 +15,7 @@ termux_step_get_source() {
 	termux_download "https://github.com/meganz/MEGAcmd/archive/${TERMUX_PKG_VERSION}_Linux.tar.gz" \
 		"${TERMUX_PKG_CACHEDIR}/mega-${TERMUX_PKG_VERSION}_Linux.tar.gz" \
 		2a3626a9f1d22303fe2123f984a8ecf3779d6d59ac6c67c1bf43c2423dcb832d
-	termux_download "https://github.com/meganz/sdk/archive/${_MEGA_SDK_VERSION}.tar.gz" \
+	termux_download "https://github.com/meganz/sdk/archive/v${_MEGA_SDK_VERSION}.tar.gz" \
 		"${TERMUX_PKG_CACHEDIR}/mega-sdk-${_MEGA_SDK_VERSION}.tar.gz" \
 		50d3c7ec8bf0b8dddadda3715530fb2e4a6d867c93faa2c6cffdbb6ea868eea3
 }
