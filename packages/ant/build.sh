5997e0a072d149c57b0c2f6fb1772bf7c252431c
Wisest_wizard <76830230+suhan-paradkar@users.noreply.github.com>
2021-08-27 15:36:16 +0530

    Enable package: ant (#7397)

--- packages/ant/build.sh
+++ packages/ant/build.sh
@@ -1,17 +1,23 @@
 TERMUX_PKG_HOMEPAGE=http://ant.apache.org/
 TERMUX_PKG_DESCRIPTION="Java based build tool like make"
-TERMUX_PKG_VERSION=1.9.6
+TERMUX_PKG_VERSION=1.10.11
 TERMUX_PKG_SRCURL=http://apache.mirrors.spacedump.net//ant/binaries/apache-ant-${TERMUX_PKG_VERSION}-bin.tar.bz2
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_SHA256=ffc6c9eb2d153db76e18df70fa145744ddd0d754339df5a797a47603cfeaa8a6
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_RECOMMENDS="openjdk-17"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/share/ant/lib
 
 	for jar in ant ant-launcher; do
-		$TERMUX_DX \
-			--dex \
-			--output=$TERMUX_PREFIX/share/ant/lib/${jar}.jar \
+		$TERMUX_D8 \
+		--classpath $ANDROID_HOME/platforms/android-$TERMUX_PKG_API_LEVEL/android.jar \
+		--release \
+		--min-api $TERMUX_PKG_API_LEVEL \
+		--output $TERMUX_PREFIX/share/ant/lib/${jar}.jar \
 			lib/${jar}.jar
 	done
 
