5720b333be649f8c01530476e498d25ff0554333
Aditya Alok <dev.aditya.alok@gmail.com>
2021-08-27 19:54:28 +0530

    elfutils: fix license

--- packages/elfutils/build.sh
+++ packages/elfutils/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE="https://sourceware.org/elfutils/Debuginfod.html"
 TERMUX_PKG_DESCRIPTION="A client/server in elfutils 0.178+ that automatically distributes elf/dwarf/source-code from servers to clients such as debuggers across HTTP"
-TERMUX_PKG_LICENSE="GPL"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.185
 TERMUX_PKG_SRCURL="https://sourceware.org/elfutils/ftp/${TERMUX_PKG_VERSION}/elfutils-${TERMUX_PKG_VERSION}.tar.bz2"
