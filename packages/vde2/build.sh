0130cd74fc3fec99fb01c4bd63df6844de07cab1
Further <55025025+ifurther@users.noreply.github.com>
2021-08-27 18:29:24 +0800

    Update build.sh

--- packages/vde2/build.sh
+++ packages/vde2/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/virtualsquare/vde-2
 TERMUX_PKG_DESCRIPTION="Virtual Distributed Ethernet for emulators like qemu"
-TERMUX_PKG_LICENSE="CUSTOM, GPL-2.0"
+TERMUX_PKG_LICENSE="custom, GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.2
