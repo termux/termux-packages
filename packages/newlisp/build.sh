6c25ef0d42c33d4e6846723f5c6185d45fa5ba8e
ifurther <55025025+ifurther@users.noreply.github.com>
2021-08-27 18:02:04 +0800

    fix license name

--- packages/newlisp/build.sh
+++ packages/newlisp/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.newlisp.org/
 TERMUX_PKG_DESCRIPTION="newLISP is a Lisp-like, general-purpose scripting language."
-TERMUX_PKG_LICENSE="GPL"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=10.7.5
 TERMUX_PKG_SRCURL=http://www.newlisp.org/downloads/newlisp-${TERMUX_PKG_VERSION}.tgz
