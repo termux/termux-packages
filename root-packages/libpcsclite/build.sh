b36a75391674dfff50ad2cb8d121a6bf9ff1726f
Henrik Grimler <henrik@grimler.se>
2021-08-27 13:36:04 +0200

    libpcsclite: update to 1.9.3

--- root-packages/libpcsclite/build.sh
+++ root-packages/libpcsclite/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://pcsclite.apdu.fr/
 TERMUX_PKG_DESCRIPTION="Middleware to access a smart card using SCard API (PC/SC)."
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.9.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.9.3
 TERMUX_PKG_SRCURL=https://pcsclite.apdu.fr/files/pcsc-lite-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=73c4789b7876a833a70f493cda21655dfe85689d9b7e29701c243276e55e683a
+TERMUX_PKG_SHA256=6956433c71dd17a902cd3f4a394ce48a1ea787faed526faf557c95cc434d3e59
 TERMUX_PKG_DEPENDS="libusb, python2"
 TERMUX_PKG_BREAKS="libpcsclite-dev"
 TERMUX_PKG_REPLACES="libpcsclite-dev"
@@ -16,3 +15,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-ipcdir=$TERMUX_PREFIX/var/run
 --disable-libsystemd
 --disable-libudev"
+
+
+termux_step_create_debscripts() {
+	# "pcscd fails to start if this folder does not exist"
+	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
+	echo "mkdir -p $TERMUX_PREFIX/lib/pcsc/drivers" >> postinst
+}
