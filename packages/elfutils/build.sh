d25766699ffc0ac8ea342aa26fa84edf4d5fd3e6
Aditya Alok <dev.aditya.alok@gmail.com>
2021-08-27 21:23:41 +0530

    elfutils: add argp and libelf as dependency

--- packages/elfutils/build.sh
+++ packages/elfutils/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.185
 TERMUX_PKG_SRCURL="https://sourceware.org/elfutils/ftp/${TERMUX_PKG_VERSION}/elfutils-${TERMUX_PKG_VERSION}.tar.bz2"
 TERMUX_PKG_SHA256=dc8d3e74ab209465e7f568e1b3bb9a5a142f8656e2b57d10049a73da2ae6b5a6
-#TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_DEPENDS="argp, libelf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
