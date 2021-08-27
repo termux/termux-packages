93b92188ca81e086926198ba307927fb88ad0dec
ifurther <55025025+ifurther@users.noreply.github.com>
2021-08-27 17:57:14 +0800

    New package: newlisp

---
+++ packages/newlisp/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=http://www.newlisp.org/
+TERMUX_PKG_DESCRIPTION="newLISP is a Lisp-like, general-purpose scripting language."
+TERMUX_PKG_LICENSE="GPL"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=10.7.5
+TERMUX_PKG_SRCURL=http://www.newlisp.org/downloads/newlisp-${TERMUX_PKG_VERSION}.tgz
+TERMUX_PKG_SHA256=dc2d0ff651c2b275bc4af3af8ba59851a6fb6e1eaddc20ae75fb60b1e90126ec
+#TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_BUILD_IN_SRC=true
