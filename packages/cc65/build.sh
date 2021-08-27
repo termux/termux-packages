30e6dc2c676a77f82c332228e97a33f25c6979d8
omecamtiv <nandan.sudipta@gmail.com>
2021-08-27 16:18:10 +0530

    fix

--- packages/cc65/build.sh
+++ packages/cc65/build.sh
@@ -6,3 +6,12 @@ TERMUX_PKG_VERSION=2.19
 TERMUX_PKG_SRCURL=https://github.com/cc65/cc65/archive/refs/tags/V${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=157b8051aed7f534e5093471e734e7a95e509c577324099c3c81324ed9d0de77
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+    mkdir -p cc65/
+    cd $TERMUX_PKG_SRCDIR/
+    make clean
+    make
+    prefix=${TERMUX_PKG_HOSTBUILD_DIR}/cc65 make install
+}
