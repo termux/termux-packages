4e92b3b7dd3011c2902ff45aa8d509e6a04cbef1
ifurther <55025025+ifurther@users.noreply.github.com>
2021-08-29 00:18:32 +0800

    fix some error

--- packages/newlisp/build.sh
+++ packages/newlisp/build.sh
@@ -5,20 +5,20 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=10.7.5
 TERMUX_PKG_SRCURL=http://www.newlisp.org/downloads/newlisp-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_SHA256=dc2d0ff651c2b275bc4af3af8ba59851a6fb6e1eaddc20ae75fb60b1e90126ec
-TERMUX_PKG_DEPENDS="readline"
+TERMUX_PKG_DEPENDS="readline, libffi, util-linux"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 
 termux_step_post_configure() {
-	cp makefile_linux_openwrt makefile_linux_termux
-	sed -e "s%mips-openwrt-linux-gcc%$TERMUX_ARCH-linux-android-clang%g" \
-	    -e "s%mips-openwrt-linux-ld%$TERMUX_ARCH-linux-android-ld%g"  \
-	    -e "/upx/d" -e "/mv/d" -e "s/rm/strip/g" \
-	    -e "s%-DLINUX%-DLINUX\ -DREADLINE\ -lreadline\ -lncurses%g" \
-	    -e 's/makefile_linux_openwrt/makefile_linux_termux/g' \
-	    -e "s/\s\$(OBJS)/\$(OBJS)/g" \
-	    -e "s/\s\s\s\s\$(CC)\ /\t\$(CC)\ /g" \
-	    -e "s%\$(TARGET_DIR)/usr%$TERMUX_PREFIX%g" -i makefile_linux_termux
+	cp makefile_linux_utf8 makefile_linux_termux
+	sed -e "s%gcc%$TERMUX_ARCH-linux-android-clang%g" \
+	    -e "10a LDFLAGS = -L$TERMUX_PREFIX/lib -lm -ldl" \
+	    -e "12a LD = $TERMUX_ARCH-linux-android-ld" \
+	    -e "s/m32/m64\ -lffi\ -fuse-ld=lld/g" \
+	    -e "s%-o newlisp%-o newlisp \$(LDFLAGS)%g" \
+	    -e "s%-DLINUX%-DLINUX\  -I$TERMUX_PREFIX/include%g" \
+	    -e 's/makefile_linux_utf8/makefile_linux_termux/g' \
+	    -i makefile_linux_termux
 	sed -i "s%/usr/bin%$TERMUX_PREFIX/bin%g" newlisp.c
 	sed -i "s%sys/sem.h%linux/sem.h%g" nl-filesys.c
 }
