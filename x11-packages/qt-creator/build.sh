b0b825ee80d5c61d28bfae23731e41440b39925c
Simeon Huang <symeon@librehat.com>
2021-08-31 16:47:00 +0100

    force linking libc++_shared for ClangTools, fixing #391

--- x11-packages/qt-creator/build.sh
+++ x11-packages/qt-creator/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Integrated Development Environment for Qt"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=4.12.4
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL="https://github.com/qt-creator/qt-creator/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=2469a19ee230eb600467e614c23ed678b1b414adc16efdedcfc0404bf40d8015
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtdeclarative, qt5-qtxmlpatterns, qt5-qttools, qt5-qtx11extras, qt5-qtsvg, llvm, clang"
@@ -31,6 +31,10 @@ termux_step_post_configure() {
     # clangbackend's Makefile lacks -lc++_shared to link against libc++ on x86_64
     sed -i 's|^LIBS          = $(SUBLIBS)|LIBS = $(SUBLIBS) -lc++_shared|' \
         ${TERMUX_PKG_SRCDIR}/src/tools/clangbackend/Makefile
+
+    # make sure clangtools link against libc++_shared on x86_64
+    sed -i 's|^LIBS          = $(SUBLIBS)|LIBS = $(SUBLIBS) -lc++_shared|' \
+        ${TERMUX_PKG_SRCDIR}/src/plugins/clangtools//Makefile
 
     # required by make install, otherwise it installs to '/'
     export INSTALL_ROOT="${TERMUX_PREFIX}"
