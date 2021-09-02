512da3ab061a41ca60aebb203210d7ce7a0817f2
Butta <repo@butta.fastem.com>
2021-09-02 21:13:34 +0530

    termux_step_patch_package: apply patches in alphabetical order
    Before 32c67fa, patches were applied in alphabetical order, which some packages
    like swift relied on, so go back to that.

--- scripts/build/termux_step_patch_package.sh
+++ scripts/build/termux_step_patch_package.sh
@@ -5,16 +5,16 @@ termux_step_patch_package() {
 	# Suffix patch with ".patch32" or ".patch64" to only apply for
 	# these bitnesses
 	local PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 \
-			     -name \*.patch -o -name \*.patch$TERMUX_ARCH_BITS)
+			     -name \*.patch -o -name \*.patch$TERMUX_ARCH_BITS | sort)
 	local DEBUG_PATCHES=""
 	if [ "$TERMUX_DEBUG_BUILD" = "true" ]; then
-		DEBUG_PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name \*.patch.debug)
+		DEBUG_PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name \*.patch.debug | sort)
 	fi
 	local ON_DEVICE_PATCHES=""
 	# .patch.ondevice patches should only be applied when building
 	# on device
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
-		ON_DEVICE_PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name \*.patch.ondevice)
+		ON_DEVICE_PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name \*.patch.ondevice | sort)
 	fi
 	shopt -s nullglob
 	for patch in $PATCHES $DEBUG_PATCHES $ON_DEVICE_PATCHES; do
