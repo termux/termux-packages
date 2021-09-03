d4134d5323ef890523981dee7cf814aec6d7c25e
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-03 20:28:39 +0300

    remove  build depends field - misused

--- packages/bacula-fd/build.sh
+++ packages/bacula-fd/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION=11.0.5
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/bacula/files/bacula/${TERMUX_PKG_VERSION}/bacula-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ef5b3b67810442201b80dc1d47ccef77b5ed378fe1285406f3a73401b6e8111a
 TERMUX_PKG_DEPENDS="openssl"
-TERMUX_PKG_BUILD_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES=etc/bacula/bacula-fd.conf
 TERMUX_PKG_SERVICE_SCRIPT=("bacula-fd" "${TERMUX_PREFIX}/bin/bacula-fd")
