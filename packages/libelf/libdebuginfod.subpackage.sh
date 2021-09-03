c547904ef164f1e605928ca582c6b57a1e8e4c43
Aditya Alok <dev.aditya.alok@gmail.com>
2021-09-03 20:54:06 +0530

    libelf: fix Termux variable name

--- packages/libelf/libdebuginfod.subpackage.sh
+++ packages/libelf/libdebuginfod.subpackage.sh
@@ -1,3 +1,3 @@
-TERMUX_SUBPACKAGE_DESCRIPTION="Library for debuginfod"
-TERMUX_SUBPACKAGE_INCLUDE="lib/libdebuginfod*.so* include/elfutils/debuginfod.h lib/pkgconfig/libdebuginfod.pc"
+TERMUX_SUBPKG_DESCRIPTION="Library for debuginfod"
+TERMUX_SUBPKG_INCLUDE="lib/libdebuginfod*.so* include/elfutils/debuginfod.h lib/pkgconfig/libdebuginfod.pc"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
