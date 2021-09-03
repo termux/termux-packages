805b18fb0949660842c282f1ff3895ed8d3abe1f
Aditya Alok <dev.aditya.alok@gmail.com>
2021-09-03 23:37:15 +0530

    libelf: enable full elfutils package (#7431)

--- packages/libelf/build.sh
+++ packages/libelf/build.sh
@@ -2,22 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://sourceware.org/elfutils/
 TERMUX_PKG_DESCRIPTION="ELF object file access library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-# NOTE: We only build the libelf part of elfutils for now,
-# as other parts are not clang compatible.
-TERMUX_PKG_VERSION=0.182
-TERMUX_PKG_SRCURL=ftp://sourceware.org/pub/elfutils/${TERMUX_PKG_VERSION}/elfutils-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ecc406914edf335f0b7fc084ebe6c460c4d6d5175bfdd6688c1c78d9146b8858
+TERMUX_PKG_VERSION=0.185
+TERMUX_PKG_SRCURL="https://sourceware.org/elfutils/ftp/${TERMUX_PKG_VERSION}/elfutils-${TERMUX_PKG_VERSION}.tar.bz2"
+TERMUX_PKG_SHA256=dc8d3e74ab209465e7f568e1b3bb9a5a142f8656e2b57d10049a73da2ae6b5a6
 # libandroid-support for langinfo.
-TERMUX_PKG_DEPENDS="libandroid-support, zlib, libcurl"
-TERMUX_PKG_BUILD_DEPENDS="argp"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_c99=yes --disable-symbol-versioning --disable-debuginfod"
-TERMUX_PKG_CONFLICTS="elfutils, libelf-dev"
-TERMUX_PKG_REPLACES="elfutils, libelf-dev"
+TERMUX_PKG_DEPENDS="libandroid-support, zlib, libcurl, libmicrohttpd, libsqlite, libarchive"
+TERMUX_PKG_BUILD_DEPENDS="argp, libbz2, liblzma, zstd"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_c99=yes --disable-symbol-versioning"
+TERMUX_PKG_CONFLICTS="libelf-dev"
+TERMUX_PKG_REPLACES="libelf-dev"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	CXXFLAGS+=" -Wno-unused-const-variable"
 	CFLAGS+=" -Wno-error=unused-value -Wno-error=format-nonliteral -Wno-error"
 
-	# Exposes ACCESSPERMS in <sys/stat.h> which elfutils uses:
+	# Exposes ACCESSPERMS in <sys/stat.h> which elfutils uses
 	CFLAGS+=" -D__USE_BSD"
 
 	CFLAGS+=" -DFNM_EXTMATCH=0"
@@ -26,30 +26,11 @@ termux_step_pre_configure() {
 		CFLAGS="${CFLAGS/-Oz/-O1}"
 	fi
 
-	cp $TERMUX_PKG_BUILDER_DIR/error.h .
 	cp $TERMUX_PKG_BUILDER_DIR/stdio_ext.h .
 	cp $TERMUX_PKG_BUILDER_DIR/obstack.h .
 	cp $TERMUX_PKG_BUILDER_DIR/qsort_r.h .
 	cp $TERMUX_PKG_BUILDER_DIR/aligned_alloc.c libelf
-	autoreconf -if
-}
+	cp -r $TERMUX_PKG_BUILDER_DIR/search src/
 
-termux_step_make() {
-	make -j $TERMUX_MAKE_PROCESSES -C lib
-	make -j $TERMUX_MAKE_PROCESSES -C libelf
-	make -j $TERMUX_MAKE_PROCESSES -C libdwfl
-	make -j $TERMUX_MAKE_PROCESSES -C libebl
-	make -j $TERMUX_MAKE_PROCESSES -C backends
-	make -j $TERMUX_MAKE_PROCESSES -C libcpu
-	make -j $TERMUX_MAKE_PROCESSES -C libdwelf
-	make -j $TERMUX_MAKE_PROCESSES -C libdw
-}
-
-termux_step_make_install() {
-	make -j $TERMUX_MAKE_PROCESSES -C libelf install
-	make -j $TERMUX_MAKE_PROCESSES -C libdwfl install
-	make -j $TERMUX_MAKE_PROCESSES -C libdw install
-	make -j $TERMUX_MAKE_PROCESSES -C libasm install
-	make install-pkgincludeHEADERS
-	make -C config install
+	autoreconf -ivf
 }
