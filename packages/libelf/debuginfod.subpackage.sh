3b821f380f99b7002895a83301f76094cac2fda0
Aditya Alok <dev.aditya.alok@gmail.com>
2021-09-03 21:43:10 +0530

    libelf: include few more files in subpackages

--- packages/libelf/debuginfod.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="A client/server that automatically distributes elf/dwarf/source-code from servers to clients."
-TERMUX_SUBPKG_INCLUDE="bin/debuginfod*"
-TERMUX_SUBPKG_DEPENDS="libdw, libdebuginfod"
