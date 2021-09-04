2b21719b002c20e80370d04d9181d4112e158bc1
Lucinda May Phipps <landfillbaby69@gmail.com>
2021-09-04 23:05:07 +0100

    libdav1d: update to 0.9.2

--- packages/libdav1d/build.sh
+++ packages/libdav1d/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://code.videolan.org/videolan/dav1d/
 TERMUX_PKG_DESCRIPTION="AV1 cross-platform decoder focused on speed and correctness"
-TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.1
+TERMUX_PKG_VERSION=0.9.2
 TERMUX_PKG_SRCURL=https://downloads.videolan.org/pub/videolan/dav1d/${TERMUX_PKG_VERSION}/dav1d-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a35d6468013eb14e8093ea463594f8b89aba1775a3005fc9ec6fa36b2d2c71d7
+TERMUX_PKG_SHA256=e3235ab6c43c0135b0db1d131e1923fad4c84db9d85683e30b91b33a52d61c71
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Denable_asm=false
 -Denable_tools=false
