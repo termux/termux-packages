4632fe69ab3ef114cce1ee5531c4a1117400feed
Aditya Alok <dev.aditya.alok@gmail.com>
2021-09-04 23:37:43 +0530

    libelf: move dependencies to respective subpackages

--- packages/libelf/elfutils.subpackage.sh
+++ packages/libelf/elfutils.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="A collection of utilities to read, create and modify ELF binary files"
 TERMUX_SUBPKG_INCLUDE="bin/eu-* bin/debuginfod* etc/profile.d/debuginfod.* share/man/man8/* share/man/man1/*"
-TERMUX_SUBPKG_DEPENDS="libasm, libdw, libdebuginfod"
+TERMUX_SUBPKG_DEPENDS="libasm, libdw, libdebuginfod, libmicrohttpd, libsqlite, libarchive"
