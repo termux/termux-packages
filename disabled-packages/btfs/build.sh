c0029f2d8f00c3c3c698dbe12de9ab4e730aa33b
Henrik Grimler <henrik@grimler.se>
2021-09-07 21:37:18 +0200

    btfs: install to TERMUX_PKG_MASSAGEDIR

--- disabled-packages/btfs/build.sh
+++ disabled-packages/btfs/build.sh
@@ -17,5 +17,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin $TERMUX_PKG_SRCDIR/cmd/btfs/btfs
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin cmd/btfs/btfs
 }
