4d9480a85efaf7d5f7fe0efbf791568f7d486698
Henrik Grimler <henrik@grimler.se>
2021-09-08 15:01:05 +0200

    helix: install to TERMUX_PKG_MASSAGEDIR

--- packages/helix/build.sh
+++ packages/helix/build.sh
@@ -8,22 +8,24 @@ TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_make_install() {
+termux_step_make() {
 	termux_setup_rust
 
 	cargo build --jobs "${TERMUX_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
+}
 
+termux_step_make_install() {
 	local datadir="${TERMUX_PREFIX}/share/${TERMUX_PKG_NAME}"
-	mkdir -p "${datadir}"
+	mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${datadir}"
 
-	cat >"${TERMUX_PREFIX}/bin/hx" <<-EOF
+	cat >"${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin/hx" <<-EOF
 		#!${TERMUX_PREFIX}/bin/sh
 		HELIX_RUNTIME=${datadir}/runtime exec ${datadir}/hx "\$@"
 	EOF
-	chmod 0700 "${TERMUX_PREFIX}/bin/hx"
+	chmod 0700 "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin/hx"
 
-	install -Dm700 target/"${CARGO_TARGET_NAME}"/release/hx "${datadir}/hx"
+	install -Dm700 target/"${CARGO_TARGET_NAME}"/release/hx "${TERMUX_PKG_MASSAGEDIR}/${datadir}/hx"
 
-	cp -r ./runtime "${datadir}"
-	find "${datadir}"/runtime/grammars -type f -name "*.so" -exec chmod 0700 {} \;
+	cp -r ./runtime "${TERMUX_PKG_MASSAGEDIR}/${datadir}"
+	find "${TERMUX_PKG_MASSAGEDIR}/${datadir}"/runtime/grammars -type f -name "*.so" -exec chmod 0700 {} \;
 }
