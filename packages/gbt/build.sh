f8c4776e66e8d87a91b3bbebc2e11548d8d13b64
Henrik Grimler <henrik@grimler.se>
2021-09-08 14:38:08 +0200

    gbt: install to TERMUX_PKG_MASSAGEDIR

--- packages/gbt/build.sh
+++ packages/gbt/build.sh
@@ -8,10 +8,9 @@ TERMUX_PKG_SRCURL=https://github.com/jtyr/gbt/archive/v${TERMUX_PKG_VERSION}.tar
 TERMUX_PKG_SHA256=b324695dc432e8e22bc257f7a6ec576f482ec418fb9c9a8301f47bfdf7766998
 TERMUX_PKG_AUTO_UPDATE=true
 _COMMIT=29dc3dac6c06518073a8e879d2b6ec65291ddab2
+TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR
-
+termux_step_make() {
 	termux_setup_golang
 
 	export GOPATH=$HOME/go
@@ -20,9 +19,13 @@ termux_step_make_install() {
 
 	go mod init gbt
 	go mod tidy
-	go build -ldflags="-s -w -X main.version=$TERMUX_PKG_VERSION -X main.build=${_COMMIT::6}" -o $TERMUX_PREFIX/bin/gbt github.com/jtyr/gbt/cmd/gbt
-
-	mkdir -p $TERMUX_PREFIX/{doc/gbt,share/gbt}
-	cp -r $TERMUX_PKG_SRCDIR/{sources,themes} $TERMUX_PREFIX/share/gbt/
-	cp -r $TERMUX_PKG_SRCDIR/{LICENSE,README.md} $TERMUX_PREFIX/doc/gbt/
+	go build -ldflags="-s -w -X main.version=$TERMUX_PKG_VERSION -X main.build=${_COMMIT::6}" \
+		-o $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/gbt github.com/jtyr/gbt/cmd/gbt
+}
+termux_step_make_install() {
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/{doc/gbt,share/gbt}
+	cp -r $TERMUX_PKG_SRCDIR/{sources,themes} \
+		$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/gbt/
+	cp -r $TERMUX_PKG_SRCDIR/{LICENSE,README.md} \
+		$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/doc/gbt/
 }
