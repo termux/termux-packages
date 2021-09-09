9b8ea3e24a7af3c78a7f65560338cb08140901ea
Lucy Phipps <landfillbaby69@gmail.com>
2021-09-09 11:52:29 +0100

    grep, gzip: update gnulib patch
    should be the final time, all upstream issues are fixed :)

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/grep/grep-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5c10da312460aec721984d5d83246d24520ec438dd48d7ab5a05dbc0d6d6823c
 TERMUX_PKG_DEPENDS="libandroid-support, pcre"
