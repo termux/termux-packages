c3be5d85a0171048296d76eaf8294a7f9deb4b93
Lucy Phipps <landfillbaby69@gmail.com>
2021-09-10 17:04:58 +0100

    termux-keyring: mark as essential

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="GPG public keys for the official Termux repositories"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_ESSENTIAL=true
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/trusted.gpg.d
