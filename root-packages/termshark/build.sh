a8523b3af0ed1895a1aee92ab445eb802603dead
Henrik Grimler <henrik@grimler.se>
2021-09-10 08:54:16 +0200

    termshark: git clone instead of downloading archive
    And reset TERMUX_PKG_REVISION.

--- root-packages/termshark/build.sh
+++ root-packages/termshark/build.sh
@@ -3,10 +3,7 @@ TERMUX_PKG_DESCRIPTION="A terminal UI for tshark, inspired by Wireshark"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=8e2a22534773b1ab0ce4327e929bbbe413d3695bab2d55c985d1f61961698610
-TERMUX_PKG_SRCURL=https://github.com/gcla/termshark/archive/v${TERMUX_PKG_VERSION}.tar.gz
-
+TERMUX_PKG_SRCURL=https://github.com/gcla/termshark.git
 TERMUX_PKG_DEPENDS="tshark"
 
 termux_step_make() {
@@ -16,13 +13,9 @@ termux_step_make() {
 	export GO111MODULE=on
 
 	cd $TERMUX_PKG_SRCDIR
-	git clone https://github.com/gcla/termshark
-	cd termshark
-	git checkout v2.3.0
 	go install ./...
 }
 
 termux_step_make_install() {
-	cd $TERMUX_PKG_BUILDDIR
 	install -Dm700 bin/android_${GOARCH}/termshark $TERMUX_PREFIX/bin/termshark
 }
