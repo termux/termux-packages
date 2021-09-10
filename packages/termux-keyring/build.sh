d00376c914b7920458a84992a205f40d5196e91c
Lucy Phipps <landfillbaby69@gmail.com>
2021-09-10 16:53:26 +0100

    termux-keyring: add my key

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux
 TERMUX_PKG_DESCRIPTION="GPG public keys for the official Termux repositories"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_VERSION=2.1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -13,6 +13,7 @@ termux_step_make_install() {
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/xeffyr.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 
 	# Key for automatic builds (via CI).
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
