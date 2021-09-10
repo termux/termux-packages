07852c874bf49dbfe800333b25fccd381b4a814d
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-10 19:10:02 +0300

    build-package.sh: allow to select package formats between debian and pacman

--- scripts/build/termux_step_create_datatar.sh
+++
@@ -1,8 +0,0 @@
-termux_step_create_datatar() {
-	if [ "$TERMUX_PKG_METAPACKAGE" = "true" ]; then
-		# Metapackage doesn't have data inside.
-		rm -rf data
-	fi
-
-	tar -cJf "$TERMUX_PKG_PACKAGEDIR/data.tar.xz" -H gnu .
-}
