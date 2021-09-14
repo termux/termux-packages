8c782569a9c427e6360839d9a0de4bde63208ace
Lucy Phipps <landfillbaby69@gmail.com>
2021-09-14 15:49:38 +0100

    mariadb: update to 10.6.4

--- packages/mariadb/build.sh
+++ packages/mariadb/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mariadb.org
 TERMUX_PKG_DESCRIPTION="A drop-in replacement for mysql server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2:10.5.8
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2:10.6.4
 TERMUX_PKG_SRCURL=http://ftp.hosteurope.de/mirror/archive.mariadb.org/mariadb-${TERMUX_PKG_VERSION:2}/source/mariadb-${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=eb4824f6f2c532cd3fc6a6bce7bf78ea7c6b949f8bdd07656b2c84344e757be8
+TERMUX_PKG_SHA256=75bf9b147a95d38160d01a73b098d50a1960563b46d16a235971fff64d99643c
 TERMUX_PKG_DEPENDS="libc++, libiconv, liblzma, ncurses, libedit, openssl, pcre2, libcrypt, libandroid-support, libandroid-glob, zlib, liblz4"
 TERMUX_PKG_BREAKS="mariadb-dev"
 TERMUX_PKG_REPLACES="mariadb-dev"
@@ -61,6 +60,7 @@ TERMUX_PKG_CONFLICTS="mysql"
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/mysqltest*
 share/man/man1/mysql-test-run.pl.1
+share/mysql/mysql-test
 mysql-test
 sql-bench
 "
