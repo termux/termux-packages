be8318053222aa681fba2578b5f6ac59e942f870
Yonle <63401712+Yonle@users.noreply.github.com>
2021-09-14 09:10:19 +0700

    New Package: uwufetch
    A meme system info tool for Linux, based on nyan/uwu trend on r/linuxmasterrace. 
    
    Known available on ArchLinux AUR at https://aur.archlinux.org/packages/uwufetch/

---
+++ packages/uwufetch/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/TheDarkBug/uwufetch"
+TERMUX_PKG_DESCRIPTION="A meme system info tool for Linux, based on nyan/uwu trend on r/linuxmasterrace"
+TERMUX_PKG_VERSION="1.6"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Yonle <yonle@protonmail.com>"
+TERMUX_PKG_SRCURL="https://github.com/TheDarkBug/uwufetch/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="252cddab47309bc1707d060e4b90ead34e48092b666005d2c137886fc86556ed"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	make termux
+}
+
+termux_step_make_install() {
+	# Copy manpage file to ${TERMUX_PREFIX}
+	cp uwufetch.1.gz ${TERMUX_PREFIX}/share/man/man1
+}
+
+termux_step_install_license() {
+	# Copy license file to ${TERMUX_PREFIX}
+	mkdir -p ${TERMUX_PREFIX}/share/doc/uwufetch
+	cp LICENSE ${TERMUX_PREFIX}/share/doc/uwufetch
+}
