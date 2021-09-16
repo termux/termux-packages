96b8294f6625a2b63af26b09bf13b2fa37c079be
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-09-16 18:40:25 +0700

    Update build.sh

--- packages/deno/build.sh
+++ packages/deno/build.sh
@@ -6,11 +6,11 @@ TERMUX_PKG_VERSION=1.14.0
 TERMUX_PKG_SRCURL=https://github.com/denoland/deno/releases/download/v${TERMUX_PKG_VERSION}/deno_src.tar.gz
 TERMUX_PKG_SHA256=56f370825f019e4bdb95f354a49576c057f90ef1cc07b7491566763b2147edc6
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
+TERMUX_PKG_DEPENDS="libandroid-support, libffi"
 
 termux_step_make() {
 	termux_setup_rust
-	V8_FROM_SOURCE=1 RUST_BACKTRACE=1 cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release -vv
+	V8_FROM_SOURCE=1 cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
 termux_step_make_install() {
