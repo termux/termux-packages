3c7bf860dd1c7ad75cc610eddfc7571e8bc45b84
Tom Yan <tom.ty89@gmail.com>
2021-09-16 17:42:56 +0800

    qpdf: Update from 10.0.1 to 10.3.2
    Apparently upstream commit 56d96e2260ada51e31cceded0dc9b46937c0cb64
    is flawed. Since --disable-rpath had actually been ignored, let's
    just have it overriden for now.

--- packages/qpdf/build.sh
+++ packages/qpdf/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=http://qpdf.sourceforge.net
 TERMUX_PKG_DESCRIPTION="Content-Preserving PDF Transformation System"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=10.0.1
+TERMUX_PKG_VERSION=10.3.2
 TERMUX_PKG_SRCURL=https://github.com/qpdf/qpdf/releases/download/release-qpdf-$TERMUX_PKG_VERSION/qpdf-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=5d2277c738188b7f4e3f01a6db7f2937ed6df54671f1fba834cd3d7ff865827b
+TERMUX_PKG_SHA256=062808c40ef8741ec8160ae00168638a712cfa1d4bf673e8e595ab5eba1da947
 TERMUX_PKG_DEPENDS="libc++, libjpeg-turbo, zlib"
 TERMUX_PKG_BREAKS="qpdf-dev"
 TERMUX_PKG_REPLACES="qpdf-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-random=/dev/urandom"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-random=/dev/urandom --enable-rpath"
 
 termux_step_pre_configure() {
 	./autogen.sh
