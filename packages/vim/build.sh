4f5d80bc292d2e6e823c83b4f414a1e0ed338392
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-09-16 16:32:19 +0600

    Make has(android/termux) return true in vim

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update both vim and vim-python to the same version in one PR.
 TERMUX_PKG_VERSION=8.2.2800
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=14bda51b9a21c992b5ab39e9ac0f1306717f6a1aacf6892d87d0fb1a40865cf7
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
