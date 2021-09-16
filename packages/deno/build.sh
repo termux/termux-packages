6007f48bfc83fac7bb5a3103e112b122ccde5c2c
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-09-16 17:22:44 +0700

    Update build.sh

--- packages/deno/build.sh
+++ packages/deno/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@leapofazzam123"
 TERMUX_PKG_VERSION=1.14.0
 TERMUX_PKG_SRCURL=https://github.com/denoland/deno/releases/download/v${TERMUX_PKG_VERSION}/deno_src.tar.gz
 TERMUX_PKG_SHA256=56f370825f019e4bdb95f354a49576c057f90ef1cc07b7491566763b2147edc6
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_rust
