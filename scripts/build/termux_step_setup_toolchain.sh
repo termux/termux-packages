946c8557a922f866b934788287ce5b6e6639702a
Henrik Grimler <henrik@grimler.se>
2021-09-17 13:40:15 +0200

    termux_step_setup_toolchain: do not add triple prefix to pkg-config
    This leads to us only having a pkg-config script for the arch that was
    active when the toolchain was setup (so typically
    aarch64-linux-android-pkg-config).

--- scripts/build/termux_step_setup_toolchain.sh
+++ scripts/build/termux_step_setup_toolchain.sh
@@ -20,7 +20,7 @@ termux_step_setup_toolchain() {
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
 		export PATH=$TERMUX_STANDALONE_TOOLCHAIN/bin:$PATH
 		export CC_FOR_BUILD=gcc
-		export PKG_CONFIG=$TERMUX_STANDALONE_TOOLCHAIN/bin/${TERMUX_HOST_PLATFORM}-pkg-config
+		export PKG_CONFIG=$TERMUX_STANDALONE_TOOLCHAIN/bin/pkg-config
 		export CCTERMUX_HOST_PLATFORM=$TERMUX_HOST_PLATFORM$TERMUX_PKG_API_LEVEL
 		if [ $TERMUX_ARCH = arm ]; then
 			CCTERMUX_HOST_PLATFORM=armv7a-linux-androideabi$TERMUX_PKG_API_LEVEL
