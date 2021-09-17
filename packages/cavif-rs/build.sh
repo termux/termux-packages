b399bc3c8cc4835201767660f77dadb7ae688a1a
Henrik Grimler <henrik@grimler.se>
2021-09-17 10:46:15 +0200

    cavif-rs: fix build error

--- packages/cavif-rs/build.sh
+++ packages/cavif-rs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Encoder/converter for AVIF images. Based on rav1e and av
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.1-win
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kornelski/cavif-rs/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0f76ae20081d2031881d51871dd5b66cf5c0bec441a22c605f5bbba7dc55c8a8
 TERMUX_PKG_BUILD_IN_SRC=true
