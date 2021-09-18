969d0eff53ac4faabf69bedb36503022e2d3748a
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-09-18 18:12:21 +0700

    Update build.sh

--- packages/deno/build.sh
+++ packages/deno/build.sh
@@ -11,6 +11,16 @@ TERMUX_PKG_BUILD_DEPENDS="glib"
 termux_step_make() {
 	termux_setup_rust
 	termux_setup_gn
+	export GN_ARGS='target_os="android"'
+	if [[ $TERMUX_ARCH == "arm" ]]; then
+		GN_ARGS+=' target_cpu="arm"'
+	elif [[ $TERMUX_ARCH == "aarch64" ]]; then
+		GN_ARGS+=' target_cpu="arm64"'
+	elif [[ $TERMUX_ARCH == "i686" ]]; then
+		GN_ARGS+=' target_cpu="x86"'
+	elif [[ $TERMUX_ARCH == "x86_64" ]]; then
+		GN_ARGS+=' target_cpu="x64"'
+	fi
 	V8_FROM_SOURCE=1 cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release --locked
 }
 
