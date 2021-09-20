2074158880d2fa9538c4e0367d288f64d213e915
Aditya Alok <dev.aditya.alok@gmail.com>
2021-09-20 22:07:33 +0530

    age: add epoch

--- packages/age/build.sh
+++ packages/age/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/FiloSottile/age
 TERMUX_PKG_DESCRIPTION="A simple, modern and secure encryption tool with small explicit keys, no config options, and UNIX-style composability"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_VERSION=1:1.0.0
 TERMUX_PKG_SRCURL=https://github.com/FiloSottile/age/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8d27684f62f9dc74014035e31619e2e07f8b56257b1075560456cbf05ddbcfce
 
