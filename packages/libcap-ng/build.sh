e2bcb420461c34abe8221b562ddccd1aae5a2304
Henrik Grimler <henrik@grimler.se>
2021-09-22 14:01:45 +0200

    libcap-ng: disable python2 as well

--- packages/libcap-ng/build.sh
+++ packages/libcap-ng/build.sh
@@ -3,11 +3,14 @@ TERMUX_PKG_DESCRIPTION="Library making programming with POSIX capabilities easie
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/stevegrubb/libcap-ng/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=65b86885b8d873e55c05bd49427fd370d559b26f0c2089ac9194828e6a2fe233
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-python3"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--without-python
+--without-python3
+"
 
 termux_step_pre_configure() {
 	./autogen.sh
