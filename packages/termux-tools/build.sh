646e619b0211e3a067b8767fe73cfb94abb49da4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-25 14:26:10 +0300

    termux-tools: add rudimentary backup/restore tools

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -34,9 +34,10 @@ termux_step_make_install() {
 		chmod +x $WRAPPER_FILE
 	done
 
-	for script in chsh dalvikvm login pkg su termux-fix-shebang termux-info \
-		termux-open termux-open-url termux-reload-settings termux-reset \
-		termux-setup-storage termux-wake-lock termux-wake-unlock termux-change-repo; do
+	for script in chsh dalvikvm login pkg su termux-fix-shebang termux-backup \
+		termux-info termux-open termux-open-url termux-reload-settings \
+		termux-reset termux-restore termux-setup-storage termux-wake-lock \
+		termux-wake-unlock termux-change-repo; do
 			install -Dm700 $TERMUX_PKG_BUILDER_DIR/$script $TERMUX_PREFIX/bin/$script
 			sed -i -e "s%\@TERMUX_APP_PACKAGE\@%${TERMUX_APP_PACKAGE}%g" \
 				-e "s%\@TERMUX_BASE_DIR\@%${TERMUX_BASE_DIR}%g" \
