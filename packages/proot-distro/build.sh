872cabb1c13fb0231cef6303c7ea00da1bd138a7
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-25 12:03:30 +0300

    proot-distro: update to 2.6.1

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.6.0
+TERMUX_PKG_VERSION=2.6.1
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ffdba85094cc7a56f48df3c5ce35e34d71b68ce7a832fd80015b2367cdbe526c
+TERMUX_PKG_SHA256=445296d4c29e1bd63b81564b854b0836e6ae9d7ad59ea6dfc78e99b6947fc707
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
