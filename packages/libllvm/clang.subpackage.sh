7882de88b9d902edce3e91ad5613e3da190f8b07
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-09-29 11:55:21 +0300

    pacman packages format: add clang to base-devel (#7615)
    %ci:no-build

--- packages/libllvm/clang.subpackage.sh
+++ packages/libllvm/clang.subpackage.sh
@@ -31,3 +31,4 @@ share/man/man1/scan-*
 TERMUX_SUBPKG_DESCRIPTION="C language frontend for LLVM"
 TERMUX_SUBPKG_BREAKS="libllvm (<< 11.0.0-1), clangd"
 TERMUX_SUBPKG_REPLACES="libllvm (<< 11.0.0-1), clangd"
+TERMUX_SUBPKG_GROUPS="base-devel"
