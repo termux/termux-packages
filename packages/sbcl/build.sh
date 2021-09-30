b9e2faec787db00714e213a873e199814cb17a3c
Yan <yan@metatem.net>
2021-09-30 06:29:34 +0200

    sbcl: added new solution
    Use SBCL if we have it already, otherwise ECL.
    Build with ECL takes around seven times longer than with SBCL.
    Yes, in true Lisp fashion, it can build itself. ;)
    Possibility to use itself is useful for upgrades.
    
    With the tweaks in the patch file, all internal tests pass.
    
    ansi-test on termux-aarch64 reports 154 failures.
    ansi-test on darwin-x8664 reports 145 failures.
    So most likely nothing to worry about.

---
+++ packages/sbcl/build.sh
@@ -0,0 +1,30 @@
+TERMUX_PKG_HOMEPAGE=http://www.sbcl.org/
+TERMUX_PKG_DESCRIPTION="SBCL (Steel Bank Common Lisp) is a high performance Common Lisp compiler."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@metayan"
+TERMUX_PKG_VERSION=2.1.9
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sbcl/sbcl-${TERMUX_PKG_VERSION}-source.tar.bz2
+TERMUX_PKG_SHA256=9e746ff12c4f78d2deabd95e48169b552f9472808cf8b8fc801d84df3e962fa1
+TERMUX_PKG_DEPENDS="ecl"
+
+termux_step_make() {
+    cd $TERMUX_PKG_SRCDIR
+
+    # Use SBCL if we have it already, otherwise ECL
+    if [ "`which sbcl`" ]; then
+	XC_HOST="`which sbcl` --no-sysinit --no-userinit"
+    else
+	XC_HOST="ecl --norc"
+    fi
+
+    ./make.sh \
+       --xc-host="$XC_HOST" \
+       --fancy \
+       --without-gcc-tls \
+       --prefix=$TERMUX_PREFIX
+}
+
+termux_step_make_install() {
+    cd $TERMUX_PKG_SRCDIR
+    ./install.sh
+}
