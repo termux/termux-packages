940ab97821b6b4b1eba19978ebf40ba632cca347
Aditya Alok <dev.aditya.alok@gmail.com>
2021-10-03 01:02:24 +0530

    lua-language-server: update to 2.4.1

--- packages/lua-language-server/build.sh
+++ packages/lua-language-server/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE="https://github.com/sumneko/lua-language-server"
 TERMUX_PKG_DESCRIPTION="Sumneko Lua Language Server coded in Lua"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@MrAdityaAlok <dev.aditya.alok@gmail.com>"
-TERMUX_PKG_VERSION=2.3.6
+TERMUX_PKG_VERSION=2.4.1
 TERMUX_PKG_SRCURL=https://github.com/sumneko/lua-language-server.git
 TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
@@ -25,7 +25,7 @@ termux_step_make_install() {
 
 	local INSTALL_DIR="${TERMUX_PREFIX}/lib/lua-language-server"
 
-	cat > "lua-language-server" <<- EOF
+	cat >"lua-language-server" <<-EOF
 		#!${TERMUX_PREFIX}/bin/bash
 
 		if [ "\$1" = "--start-lsp" ]; then
@@ -47,11 +47,11 @@ termux_step_make_install() {
 }
 
 termux_step_create_debscripts() {
-	cat > prerm <<- EOF
+	cat >prerm <<-EOF
 		#!${TERMUX_PREFIX}/bin/bash
 		if [ "$TERMUX_PACKAGE_FORMAT" != "pacman" ] && [ "\$1" != "remove" ]; then exit 0; fi
 
-		# since lsp creates log file, therefore not removed automatically
+		# log files created by lsp is not removed automatically
 		rm -rf "${TERMUX_PREFIX}/lib/lua-language-server"
 	EOF
 }
