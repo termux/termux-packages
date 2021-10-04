27188f3f893934934f104bed42b8a2dd2b450954
MedzikUser <nivua1fn@duck.com>
2021-10-04 21:04:07 +0200

    update

--- packages/gotop/build.sh
+++ packages/gotop/build.sh
@@ -13,7 +13,11 @@ TERMUX_PKG_NO_STATICSPLIT=true
 termux_step_make_install() {
 	termux_setup_golang
 
+	ls -alh
+
 	cd $TERMUX_PKG_SRCDIR
+
+	ls -alh
 
 	GOOS=android go build -o gotop \
 	    -ldflags "-X main.Version=v${_MAJOR_VERSION} -X main.BuildDate=$(date +%Y%m%dT%H%M%S)" \
