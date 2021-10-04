3f0c345d204d9d75702115feac924024fecdb2cd
MedzikUser <nivua1fn@duck.com>
2021-10-04 20:36:17 +0200

    update

--- packages/gotop/build.sh
+++ packages/gotop/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/xxxserxxx/gotop
 TERMUX_PKG_DESCRIPTION="A terminal based graphical activity monitor inspired by gtop and vtop"
-TERMUX_PKG_LICENSE="MIT (Festival variant)"
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@medzikuser"
 _MAJOR_VERSION=4.1.2
 # Use the ~ deb versioning construct in the future:
