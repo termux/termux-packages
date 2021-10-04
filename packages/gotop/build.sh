80bd3a2a6a9ae14c566c9ce68ddfb754a284caea
MedzikUser <nivua1fn@duck.com>
2021-10-04 20:26:50 +0200

    update

---
+++ packages/gotop/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://golang.org/
+TERMUX_PKG_DESCRIPTION="Go programming language compiler"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+_MAJOR_VERSION=4.1.2
+# Use the ~ deb versioning construct in the future:
+TERMUX_PKG_VERSION=1:${_MAJOR_VERSION}
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/xxxserxxx/gotop/archive/refs/tags/v${_MAJOR_VERSION}.tar.gz
+TERMUX_PKG_SHA256=
+TERMUX_PKG_NO_STATICSPLIT=true
+
+termux_step_make_install() {
+	termux_setup_golang
+
+	DAT=$(date +%Y%m%dT%H%M%S) GOOS=android GOARCH=$TERMUX_ARCH go build -o gotop \
+	    -ldflags "-X main.Version=v${_MAJOR_VERSION} -X main.BuildDate=${DAT}" \
+	    ./cmd/gotop
+}
+
+termux_step_post_massage() {
+	find . -path '*/testdata*' -delete
+}
