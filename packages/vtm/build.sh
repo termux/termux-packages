57222b6523da61901ff01aec67e47946f3356550
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-04 19:47:48 +0300

    vtm: fix 32bit builds

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-p
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.15
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fa18ae2f1eee9615c6e0df5be4b248fb15268c5e7401380f129ccee8cb9254f0
 TERMUX_PKG_DEPENDS="libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+=/src
