91671a8558f4e26e657c26b711cf58f6c542364b
MedzikUser <nivua1fn@duck.com>
2021-10-04 22:23:12 +0200

    update

--- packages/gotop/build.sh
+++ packages/gotop/build.sh
@@ -2,23 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xxxserxxx/gotop
 TERMUX_PKG_DESCRIPTION="A terminal based graphical activity monitor inspired by gtop and vtop"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@medzikuser"
-_MAJOR_VERSION=4.1.2
-# Use the ~ deb versioning construct in the future:
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/xxxserxxx/gotop/archive/refs/tags/v${_MAJOR_VERSION}.tar.gz
+TERMUX_PKG_VERSION=4.1.2
+TERMUX_PKG_SRCURL=https://github.com/xxxserxxx/gotop/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=81518fecfdab4f4c25a4713e24d9c033ba8311bbd3e2c0435ba76349028356da
 
 termux_step_make_install() {
 	termux_setup_golang
 
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+
 	cd $TERMUX_PKG_SRCDIR
 
-	echo $GOARCH
-
-	GOOS=android go build -o gotop \
-	    -ldflags "-X main.Version=v${_MAJOR_VERSION} -X main.BuildDate=$(date +%Y%m%dT%H%M%S)" \
+	go build -o gotop \
+	    -ldflags "-X main.Version=v${TERMUX_PKG_VERSION} -X main.BuildDate=$(date +%Y%m%dT%H%M%S)" \
 	    ./cmd/gotop
 
-	mv gotop $TERMUX_PREFIX/bin/gotop
+	install -Dm700 -t $TERMUX_PREFIX/bin/gotop ./gotop
 }
