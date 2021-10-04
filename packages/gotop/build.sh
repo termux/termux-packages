d9f17bda45f3009b6c4e5f815c758527c09c6cb7
MedzikUser <nivua1fn@duck.com>
2021-10-04 21:23:19 +0200

    update

--- packages/gotop/build.sh
+++ packages/gotop/build.sh
@@ -4,11 +4,10 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@medzikuser"
 _MAJOR_VERSION=4.1.2
 # Use the ~ deb versioning construct in the future:
-TERMUX_PKG_VERSION=1:${_MAJOR_VERSION}
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xxxserxxx/gotop/archive/refs/tags/v${_MAJOR_VERSION}.tar.gz
 TERMUX_PKG_SHA256=81518fecfdab4f4c25a4713e24d9c033ba8311bbd3e2c0435ba76349028356da
-TERMUX_PKG_NO_STATICSPLIT=true
 
 termux_step_make_install() {
 	termux_setup_golang
