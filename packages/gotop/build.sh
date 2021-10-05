846fcc97a26ed6af80eed70a810d36b32eb0db3c
Oskar <nivua1fn@duck.com>
2021-10-05 15:27:19 +0200

    Update build.sh

--- packages/gotop/build.sh
+++ packages/gotop/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xxxserxxx/gotop
 TERMUX_PKG_DESCRIPTION="A terminal based graphical activity monitor inspired by gtop and vtop"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@medzikuser"
-TERMUX_PKG_VERSION=4.1.2
-TERMUX_PKG_SRCURL=https://github.com/xxxserxxx/gotop/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+_VERSION=4.1.2
+TERMUX_PKG_VERSION=1:${_VERSION}
+TERMUX_PKG_SRCURL=https://github.com/xxxserxxx/gotop/archive/refs/tags/v${_VERSION}.tar.gz
 TERMUX_PKG_SHA256=81518fecfdab4f4c25a4713e24d9c033ba8311bbd3e2c0435ba76349028356da
 
 termux_step_make_install() {
