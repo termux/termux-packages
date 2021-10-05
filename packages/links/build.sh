c24021850f3145e47b66e6a21ebfee6428ed7248
Yaksh Bariya <yakshbari4@gmail.com>
2021-10-05 07:59:42 +0530

    links: bump revision

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Links is a text and graphics mode WWW browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.24
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=225c808ec6fa238ba5956f24750cea5f374075f8aa2e95385883d4f51a0fc37e
 TERMUX_PKG_DEPENDS="brotli, libbz2, libevent, liblzma, libpng, librsvg, libtiff, libx11, openssl, zlib, zstd"
