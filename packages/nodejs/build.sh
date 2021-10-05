6df3d7ec413f630c221b5070c639b4c8a176e680
Yaksh Bariya <yakshbari4@gmail.com>
2021-10-05 08:04:47 +0530

    nodejs: bump revision

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environme
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
 TERMUX_PKG_VERSION=16.10.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=97dc1aca232b4911e0b9e5a23a03200ab8ef05157e03c732315b579481bf7912
 # Note that we do not use a shared libuv to avoid an issue with the Android
