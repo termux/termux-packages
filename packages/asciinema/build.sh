c887f4772641dc34f8f05d333e75a301f8c1f6e6
Lucy Phipps <landfillbaby69@gmail.com>
2021-10-05 17:33:07 +0100

    asciinema: update to 2.1.0

--- packages/asciinema/build.sh
+++ packages/asciinema/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://asciinema.org/
 TERMUX_PKG_DESCRIPTION="Record and share your terminal sessions, the right way"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.2
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=2.1.0
 TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2578a1b5611e5375771ef6582a6533ef8d40cdbed1ba1c87786fd23af625ab68
+TERMUX_PKG_SHA256=f4f4a04ab891dd513c99f72527c83fa9e634e858f2744a17e80258b90a6bf409
 TERMUX_PKG_AUTO_UPDATE=true
 # ncurses-utils for tput which asciinema uses:
 TERMUX_PKG_DEPENDS="python, ncurses-utils"
@@ -13,7 +12,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_HAS_DEBUG=false
 
-_PYTHON_VERSION=3.9
+_PYTHON_VERSION=3.10
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
