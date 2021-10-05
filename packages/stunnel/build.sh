43463df241e983eb914661c2ae4283e801041194
Yaksh Bariya <yakshbari4@gmail.com>
2021-10-05 08:11:18 +0530

    stunnel: bump revision

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.60
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c45d765b1521861fea9b03b425b9dd7d48b3055128c0aec673bba5ef9b8f787d
 TERMUX_PKG_DEPENDS="openssl"
