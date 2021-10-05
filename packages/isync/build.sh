87bcc0cf9f9efac6bb193af37d4c4ce20155b76f
Yaksh Bariya <yakshbari4@gmail.com>
2021-10-05 07:58:41 +0530

    isync: bump revision

--- packages/isync/build.sh
+++ packages/isync/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="IMAP and MailDir mailbox synchronizer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/isync/isync/${TERMUX_PKG_VERSION}/isync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1935e7ed412fd6b5928aaea656f290aa8d3222c5feda31534903934ce4755343
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl ac_cv_header_db=no ac_cv_berkdb4=no"
