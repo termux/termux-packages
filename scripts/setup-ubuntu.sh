bbdaa639f77fa4d48a4c3c6c6254f954e631cde4
Lucy Phipps <landfillbaby69@gmail.com>
2021-10-05 15:25:05 +0100

    update docker image to Ubuntu 21.04

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -132,8 +132,11 @@ PACKAGES+=" patchelf"
 # Needed by wrk.
 PACKAGES+=" luajit"
 
-# Needed by libduktape (2.5.0 still uses python2 unfortunately)
-PACKAGES+=" python-yaml"
+# Needed by gitea.
+PACKAGES+=" npm"
+
+# Needed by libduktape
+PACKAGES+=" bc"
 
 # Java.
 PACKAGES+=" openjdk-8-jdk openjdk-16-jdk"
@@ -164,8 +167,8 @@ PACKAGES+=" libjansson-dev"
 PACKAGES+=" libparse-yapp-perl"
 PACKAGES+=" libreadline-dev"
 PACKAGES+=" libunistring-dev"
-PACKAGES+=" llvm-10-dev"
-PACKAGES+=" llvm-10-tools"
+PACKAGES+=" llvm-12-dev"
+PACKAGES+=" llvm-12-tools"
 
 # Needed by packages in X11 repository.
 PACKAGES+=" alex"
