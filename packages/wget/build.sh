4e52dc7d23a2807f119ba61094191d349c5d72b5
Yaksh Bariya <yakshbari4@gmail.com>
2021-10-05 08:11:20 +0530

    wget: bump revision

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.21.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_SHA256=db9bbe5347e6faa06fc78805eeb808b268979455ead9003a608569c9d4fc90ad
 TERMUX_PKG_DEPENDS="libiconv, pcre2, openssl, libuuid, libandroid-support, libunistring, zlib"
