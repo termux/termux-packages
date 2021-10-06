8a6fd0cdb2c6f4590d1bf0ccae42c17b14ffc4e8
Leap of Azzam <leapofazzam@gmail.com>
2021-10-06 08:27:01 +0700

    Add pulseaudio to all system subpackages

--- packages/qemu-system-x86-64-headless/qemu-system-aarch64-headless.subpackage.sh
+++ packages/qemu-system-x86-64-headless/qemu-system-aarch64-headless.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (headless)"
-TERMUX_SUBPKG_DEPENDS="glib, libbz2, libc++, libcurl, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libspice-server, libssh, libusb, libusbredir, ncurses, qemu-common, resolv-conf, zlib, zstd"
+TERMUX_SUBPKG_DEPENDS="glib, libbz2, libc++, libcurl, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libspice-server, libssh, libusb, libusbredir, ncurses, pulseaudio, qemu-common, resolv-conf, zlib, zstd"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=no
 
 TERMUX_SUBPKG_INCLUDE="
