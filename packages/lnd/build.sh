9f07ce08b01c833c6d8ddbee6e6b967bece895be
Oskar <nivua1fn@duck.com>
2021-10-06 18:48:19 +0200

    update to 0.13.3-beta

--- packages/lnd/build.sh
+++ packages/lnd/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/lightningnetwork/lnd
 TERMUX_PKG_DESCRIPTION="Lightning Network Daemon"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.13.2-beta
+TERMUX_PKG_VERSION=0.13.3-beta
 TERMUX_PKG_SRCURL=(https://github.com/lightningnetwork/lnd/archive/v${TERMUX_PKG_VERSION}.tar.gz
                    https://github.com/lightningnetwork/lnd/releases/download/v${TERMUX_PKG_VERSION}/vendor.tar.gz)
-TERMUX_PKG_SHA256=(b0bc64124a399b55f39e584a06b778bcf474c7042d44b1320aae3f49c67cb1d8
+TERMUX_PKG_SHA256=(c642c14bbf34e61d1ac566d22a9775e97e3671fff90552a0060d791e533a5af2
                    304a59ce38ffc03c1fd4bdf9294560793865f6b19646aea03b1d5da482ff83d3)
 TERMUX_PKG_DEPENDS="bitcoin"
 TERMUX_PKG_CONFFILES="var/service/lnd/run var/service/lnd/log/run"
