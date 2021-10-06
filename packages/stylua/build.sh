1ad30019aacdfdbbf49133136503cd9e6de3b96a
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-10-06 22:58:36 +0600

    bump: stylua to v0.11.0

--- packages/stylua/build.sh
+++ packages/stylua/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/JohnnyMorganz/StyLua
 TERMUX_PKG_DESCRIPTION="An opinionated Lua code formatter"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@shadmansaleh"
-TERMUX_PKG_VERSION=0.10.1
+TERMUX_PKG_VERSION=0.11.0
 TERMUX_PKG_SRCURL=https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=03999c22779f4dcf68fdf92d143a95afccd6c6554da27af308fd826179db80f9
+TERMUX_PKG_SHA256=39f0bad4cc175202eae2551e4ddaf3dd6a229943e8da3e462d4fa15a024fd0fa
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
