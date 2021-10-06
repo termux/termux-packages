152a202e16e6a968495b6aea6fcf33bf2612a07c
Oskar <nivua1fn@duck.com>
2021-10-06 18:10:10 +0200

    bump dar to 2.7.2 (#7678)

--- packages/dar/build.sh
+++ packages/dar/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://dar.linux.free.fr/
 TERMUX_PKG_DESCRIPTION="A full featured command-line backup tool, short for Disk ARchive"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.7.1
+TERMUX_PKG_VERSION=2.7.2
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/dar/dar/${TERMUX_PKG_VERSION}/dar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=76dade8adbeb817ffc78bf592c8200487ab5650234cf539539a9cbc5d346beef
+TERMUX_PKG_SHA256=973fa977c19b32b1f9ecb62153c810ba8696f644eca048f214c77ad0e8eca255
 TERMUX_PKG_DEPENDS="attr, libbz2, libgcrypt, liblzma, liblzo, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
