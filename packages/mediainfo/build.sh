615f111487b80d246ec3cc83aa392bd05a1fde02
Oskar <nivua1fn@duck.com>
2021-10-06 21:53:02 +0200

    bump mediainfo to 21.09 (#7688)

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.03
-TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c246912af47aa344f4484bf447021e64a8ff813ddf2ce971b12fb8564cb04636
+TERMUX_PKG_VERSION=21.09
+TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=7b8fd9a502ec64afe1315072881b6385ba57ca69f2f82c625b1672e151c50c57
 TERMUX_PKG_DEPENDS="libmediainfo"
 
 termux_step_pre_configure() {
