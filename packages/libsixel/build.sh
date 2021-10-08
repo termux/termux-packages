493d933d4eec8b0cebdab79981239fc1a9ef2d9a
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-08 17:53:09 +0300

    move from unstable: libsixel

---
+++ packages/libsixel/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://saitoha.github.io/libsixel/
+TERMUX_PKG_DESCRIPTION="Encoder/decoder implementation for DEC SIXEL graphics"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
+TERMUX_PKG_VERSION=1.8.6
+TERMUX_PKG_REVISION=4
+TERMUX_PKG_SRCURL=https://github.com/saitoha/libsixel/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=37611d60c7dbcee701346967336dbf135fdd5041024d5f650d52fae14c731ab9
+TERMUX_PKG_DEPENDS="libcurl, libjpeg-turbo, libpng"
+TERMUX_PKG_BREAKS="libsixel-dev"
+TERMUX_PKG_REPLACES="libsixel-dev"
