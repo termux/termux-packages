c7627c027bf0ce697fe658c95aeb16c26942af82
Ian Hu <hu2008yinxiang@163.com>
2021-10-09 09:47:33 +0800

    fix android-tools patch

--- packages/android-tools/build.sh
+++ packages/android-tools/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://developer.android.com/
 TERMUX_PKG_DESCRIPTION="Android platform tools"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=31.0.0p1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=31.0.2
 TERMUX_PKG_SRCURL=https://github.com/nmeum/android-tools/releases/download/$TERMUX_PKG_VERSION/android-tools-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=51a4c3ba5f16945905449c4bd2c1c781a4df7469f6b7362f8837f4f640d8c7b6
+TERMUX_PKG_SHA256=61b3bf6c240cb0b4d0ccfef696c55986e06657843627df950fdcf6881af0f8d4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libusb, pcre2, googletest, libprotobuf, brotli, zstd, liblz4"
 
