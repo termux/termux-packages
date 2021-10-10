d2a63ff071c15b11bb991b4abcb57f0e1f6739d7
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-10 18:41:47 +0300

    tvheadend: fix include lookup order
    Avoid mixing in-source spawn.h/spawn.c with libandroid-spawn, otherwise
    compilation errors may occur.

--- packages/tvheadend/build.sh
+++ packages/tvheadend/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="TV streaming server for Linux and Android supporting DVB
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="MrAdityaAlok <dev.aditya.alok@gmail.com>"
 TERMUX_PKG_VERSION=4.2.8
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL="https://github.com/tvheadend/tvheadend/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=1aef889373d5fad2a7bd2f139156d4d5e34a64b6d38b87b868a2df415f01f7ad
 TERMUX_PKG_DEPENDS="libiconv, ffmpeg, libopus, libx264, libx265, libvpx, libfdk-aac, libogg, libtheora, libvorbis, pngquant"
@@ -23,7 +23,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	termux_setup_cmake
 
-	CFLAGS+=" -I${TERMUX_PREFIX}/include"
+	CFLAGS=" -I$TERMUX_PKG_BUILDDIR/src $CFLAGS $CPPFLAGS"
 
 	# Arm does not support mmx and sse2 instructions, still checks return true
 	if [ "${TERMUX_ARCH}" = "arm" ] || [ "${TERMUX_ARCH}" = "aarch64" ]; then
