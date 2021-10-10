a5eff026167368ddba4871bfdad8a83ea6a3364f
Henrik Grimler <henrik@grimler.se>
2021-10-10 18:02:37 +0200

    wuzz: fix build with our golang version

--- packages/wuzz/build.sh
+++ packages/wuzz/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interactive command line tool for HTTP inspection"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/asciimoo/wuzz/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=721ea7343698e9f3c69e09eab86b9b1fef828057655f7cebc1de728c2f75151e
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,6 +16,8 @@ termux_step_make() {
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/asciimoo/wuzz
 
 	cd "$GOPATH"/src/github.com/asciimoo/wuzz
+	go mod download github.com/BurntSushi/toml
+	go get github.com/asciimoo/wuzz
 	go build
 }
 
