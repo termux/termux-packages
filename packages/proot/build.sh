09dc5052af447bb8bf43ac514c5353d228607d44
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-10 16:38:50 +0300

    proot: assign Michal Bednarski as maintainer

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://proot-me.github.io/
 TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root users"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
 # Just bump commit and version when needed:
 _COMMIT=b56071ff3f849ee1e7981e1088e6b3f3375e04e4
 TERMUX_PKG_VERSION=5.1.107
-TERMUX_PKG_REVISION=45
+TERMUX_PKG_REVISION=46
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=1e3205d67c2b07117179eb2eba99b29103e8f3eba543ead1f0dd4cfab5257218
 TERMUX_PKG_DEPENDS="libtalloc"
