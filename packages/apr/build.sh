3f8ed120f68a0dbe7c4a7a30ebc9ae0b6fd04a75
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-10-11 04:38:36 +0900

    apr: Add configure args for cross compilation

--- packages/apr/build.sh
+++ packages/apr/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Apache Portable Runtime Library"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.7.0
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://dlcdn.apache.org/apr/apr-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=e2e148f0b2e99b8e5c6caa09f6d4fb4dd3e83f744aa72a952f94f5a14436f7ea
 TERMUX_PKG_DEPENDS="libuuid"
@@ -15,10 +15,27 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-installbuilddir=$TERMUX_PREFIX/share/apr-1/build
 ac_cv_func_getrandom=no
 ac_cv_have_decl_SYS_getrandom=no
+ap_cv_atomic_builtins=yes
+apr_cv_mutex_recursive=yes
+apr_cv_epoll=yes
+apr_cv_epoll_create1=yes
+apr_cv_dup3=yes
+apr_cv_accept4=yes
+apr_cv_sock_cloexec=yes
 ac_cv_file__dev_zero=yes
+ac_cv_strerror_r_rc_int=yes
 ac_cv_func_setpgrp_void=yes
-apr_cv_process_shared_works=no
+ac_cv_struct_rlimit=yes
+ac_cv_func_sem_open=no
+apr_cv_process_shared_works=yes
+apr_cv_mutex_robust_shared=no
+ac_cv_o_nonblock_inherited=no
 apr_cv_tcp_nodelay_with_cork=yes
+apr_cv_gai_addrconfig=yes
+ac_cv_sizeof_pid_t=4
+ac_cv_sizeof_ssize_t=$(( TERMUX_ARCH_BITS==32 ? 4 : 8 ))
+ac_cv_sizeof_size_t=$(( TERMUX_ARCH_BITS==32 ? 4 : 8 ))
+ac_cv_sizeof_off_t=$(( TERMUX_ARCH_BITS==32 ? 4 : 8 ))
 ac_cv_sizeof_struct_iovec=$(( TERMUX_ARCH_BITS==32 ? 8 : 16 ))
 ac_cv_search_crypt="
 TERMUX_PKG_RM_AFTER_INSTALL="lib/apr.exp"
