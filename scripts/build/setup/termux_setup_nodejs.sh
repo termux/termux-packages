97a654bd9615f4627840150e30709cd2726a219d
Oskar <nivua1fn@duck.com>
2021-10-10 10:36:44 +0200

    Create termux_setup_nodejs.sh

---
+++ scripts/build/setup/termux_setup_nodejs.sh
@@ -0,0 +1,21 @@
+termux_setup_nodejs() {
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+		sudo apt-get install -y nodejs
+	else
+		if [ "$(dpkg-query -W -f '${db:Status-Status}\n' golang 2>/dev/null)" != "installed" ]; then
+			echo "Package 'nodejs' is not installed."
+			echo "You can install it with"
+			echo
+			echo "  pkg install nodejs-lts"
+			echo
+			echo "or build it from source with"
+			echo
+			echo "  ./build-package.sh nodejs-lts"
+			echo
+			exit 1
+		fi
+
+		export GOROOT="$TERMUX_PREFIX/lib/go"
+	fi
+}
