cc30f23a27c8f904530d5aac9d45565d9db719b2
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-10-10 10:59:35 +0900

    calc: Set `LONG_BITS`

--- packages/calc/build.sh
+++ packages/calc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Arbitrary precision console calculator"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.13.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/lcn2/calc/releases/download/v$TERMUX_PKG_VERSION/calc-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=6ae538f57785c5701a70112ccf007ab5553abd332ae2deaadaf564f401c734ad
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,7 +15,8 @@ termux_step_make() {
 	make USE_READLINE="-DUSE_READLINE" READLINE_EXTRAS="-lhistory -lncurses" \
 		READLINE_LIB="-L$TERMUX_PREFIX/lib -lreadline" \
 		READLINE_INCLUDE="-I$TERMUX_PREFIX/include" \
-		DEFAULT_LIB_INSTALL_PATH="$TERMUX_PREFIX/lib" CC="$CC" -j1
+		DEFAULT_LIB_INSTALL_PATH="$TERMUX_PREFIX/lib" CC="$CC" -j1 \
+		LONG_BITS=$TERMUX_ARCH_BITS
 }
 
 termux_step_make_install() {
