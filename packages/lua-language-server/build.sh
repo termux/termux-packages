058c3b424af25a3c009c624b8b9e9603e5313bdd
Henrik Grimler <henrik@grimler.se>
2021-10-10 18:57:46 +0200

    lua-language-server: fix hostbuild step
    Package still fails to build though:
    
    FAILED: build/obj/source_bee/subprocess_posix.obj
    aarch64-linux-android-clang -MMD -MT build/obj/source_bee/subprocess_posix.obj -MF build/obj/source_bee/subprocess_posix.obj.d -std=c++17 -O2 -Wall -Werror -fvisibility=hidden -I3rd/bee.lua/bee/nonstd -I3rd/bee.lua -DBEE_INLINE -DNDEBUG  -fstack-protector-strong -Oz -I/data/data/com.termux/files/usr/include -o build/obj/source_bee/subprocess_posix.obj -c 3rd/bee.lua/bee/subprocess/subprocess_posix.cpp
    3rd/bee.lua/bee/subprocess/subprocess_posix.cpp:152:17: error: 'posix_spawnattr_init' is unavailable: introduced in Android 28
            int r = posix_spawnattr_init(&spawnattr_);
                    ^
    /data/data/com.termux/files/usr/include/spawn.h:56:5: note: 'posix_spawnattr_init' has been explicitly marked unavailable here
    int posix_spawnattr_init(posix_spawnattr_t* __attr) __INTRODUCED_IN(28);
        ^

--- packages/lua-language-server/build.sh
+++ packages/lua-language-server/build.sh
@@ -8,22 +8,22 @@ TERMUX_PKG_SRCURL=https://github.com/sumneko/lua-language-server.git
 TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_host_build() {
 	termux_setup_ninja
-
-	cd $TERMUX_PKG_SRCDIR/3rd/luamake
+	mkdir 3rd
+        cp -a $TERMUX_PKG_SRCDIR/3rd/luamake 3rd/
+        cd 3rd/luamake
 	./compile/install.sh
 }
 
 termux_step_make() {
-	cd $TERMUX_PKG_SRCDIR
-	./3rd/luamake/luamake -cc "${CC}" -flags "${CFLAGS} ${CPPFLAGS}" -hostos "android"
+	termux_setup_ninja
+	$TERMUX_PKG_HOSTBUILD_DIR/3rd/luamake/luamake -cc "${CC}" -flags "${CFLAGS} ${CPPFLAGS}" -hostos "android"
 }
 
 termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR
-
 	local INSTALL_DIR="${TERMUX_PREFIX}/lib/lua-language-server"
 
 	cat >"lua-language-server" <<-EOF
