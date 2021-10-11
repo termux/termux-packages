f8d76fd8f0208cd214f35ba4f358671c3e507754
ifurther <55025025+ifurther@users.noreply.github.com>
2021-10-11 16:02:14 +0800

    Add some part

--- packages/dog/build.sh
+++ packages/dog/build.sh
@@ -5,8 +5,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.1.0
 TERMUX_PKG_SRCURL=https://github.com/ogham/dog/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=82387d38727bac7fcdb080970e84b36de80bfe7923ce83f993a77d9ac7847858
-#TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_LICENSE_FILE="LICENCE"
 
 termux_step_pre_configure() {
 	termux_setup_rust
