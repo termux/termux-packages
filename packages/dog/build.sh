9abcd65783ddd58343213a7ec75d3ef8ddd1698d
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-11 12:21:51 +0300

    Update build.sh

--- packages/dog/build.sh
+++ packages/dog/build.sh
@@ -8,15 +8,3 @@ TERMUX_PKG_SRCURL=https://github.com/ogham/dog/archive/refs/tags/v${TERMUX_PKG_V
 TERMUX_PKG_SHA256=82387d38727bac7fcdb080970e84b36de80bfe7923ce83f993a77d9ac7847858
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-#termux_step_pre_configure() {
-#	termux_setup_rust
-#}
-
-#termux_step_make() {
-#	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
-#}
-
-#termux_step_make_install() {
-#	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/dog
-#}
