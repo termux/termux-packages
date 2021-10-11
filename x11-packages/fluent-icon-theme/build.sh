144d6e2c1a11eb3be91e2a9d66a3566b1f1bc8f2
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-11 20:26:57 +0300

    fluent-icon-theme: fix typo in build.sh

--- x11-packages/fluent-icon-theme/build.sh
+++ x11-packages/fluent-icon-theme/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="Fluent icon theme for linux desktops"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=2021.10.07
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/vinceliuice/Fluent-icon-theme/archive/${TERMUX_PKG_VERSION//./-}.tar.gz
 TERMUX_PKG_SHA256=d119e57f1612582dfd50ebc73565254d741af7e9489dbe39f21c3ec9b40b62b7
 TERMUX_PKG_DEPENDS="hicolor-icon-theme"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_RM_ADTER_INSTALL="share/icons/*/icon-theme.cache"
+TERMUX_PKG_RM_AFTER_INSTALL="share/icons/*/icon-theme.cache"
 
 termux_step_make_install(){
 	./install.sh -d ${TERMUX_PREFIX}/share/icons -b
