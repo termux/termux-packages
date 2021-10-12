8754db21d638ff8cf2e7d9916705ed33b8bffbc4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-12 23:05:21 +0300

    taskwarrior: update to 2.6.0

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://taskwarrior.org
 TERMUX_PKG_DESCRIPTION="Utility for managing your TODO list"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.5.3
+TERMUX_PKG_VERSION=2.6.0
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${TERMUX_PKG_VERSION}/task-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7243d75e0911d9e2c9119ad94a61a87f041e4053e197f7280c42410aa1ee963b
+TERMUX_PKG_SHA256=3d0b445d45ffc578c3fefadc82501e35de898d09e8cd7460709077751e55b9c5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libgnutls, libuuid, libandroid-glob"
+TERMUX_CMAKE_BUILD="Unix Makefiles"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
