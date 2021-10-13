611b4539fc163bbf02ff062ea642a4d5f2c20a20
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-10-13 17:43:54 +0800

    squashfs-tools-ng: bump revision

--- packages/squashfs-tools-ng/build.sh
+++ packages/squashfs-tools-ng/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="New set of tools for working with SquashFS images"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@WMCB-Tech"
 TERMUX_PKG_VERSION="1.1.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/AgentD/squashfs-tools-ng/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="da4c00938a1fdec89808e05f43c6875180de1990e71837030071d2b639456f22"
 TERMUX_PKG_DEPENDS="zlib, liblz4, liblzo, zstd"
