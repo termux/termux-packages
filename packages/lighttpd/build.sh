9203f851a8347006ed12fee929964ad0231854ee
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 14:19:08 +0300

    lighttpd: fix syntax error in build.sh

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -9,7 +9,7 @@ TERMUX_PKG_SHA256=fb953db273daef08edb6e202556cae8a3d07eed6081c96bd9903db957d1084
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
 TERMUX_PKG_DEPENDS="libbz2, openssl, pcre, libcrypt, libandroid-glob, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/lighttpd-angel"
-TERMUX_PKG_SERVICE_SCRIPT=("lighttpd" "if [ -f \"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/lighttpd/lighttpd.conf\"; fi\nexec lighttpd -D -f \$CONFIG 2>&1')
+TERMUX_PKG_SERVICE_SCRIPT=("lighttpd" "if [ -f \"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/lighttpd/lighttpd.conf\"; fi\nexec lighttpd -D -f \$CONFIG 2>&1")
 
 termux_step_pre_configure() {
 	LDFLAGS="$LDFLAGS -landroid-glob"
