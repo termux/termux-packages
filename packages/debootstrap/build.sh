62e56db70a6036aa67d670fa953850d3cb5666ba
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 12:50:40 +0300

    debootstrap: do not rely on PREFIX environment variable

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Bootstrap a basic Debian system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.124
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7700e9864b29f166b26c54153ed8c275b46f4b9a60468bb607e019a64c16cd8f
 TERMUX_PKG_DEPENDS="perl, proot, wget"
