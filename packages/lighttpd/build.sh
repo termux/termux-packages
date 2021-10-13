798316f903bf6acfaefd708d801f214550a9750a
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 13:21:06 +0300

    lighttpd: expand home & prefix directory path in service script

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="Fast webserver with minimal memory footprint"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4.59
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=fb953db273daef08edb6e202556cae8a3d07eed6081c96bd9903db957d1084d5
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
 TERMUX_PKG_DEPENDS="libbz2, openssl, pcre, libcrypt, libandroid-glob, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/lighttpd-angel"
-TERMUX_PKG_SERVICE_SCRIPT=("lighttpd" 'if [ -f "$HOME/.lighttpd/lighttpd.conf" ]; then CONFIG="$HOME/.lighttpd/lighttpd.conf"; else CONFIG="$PREFIX/etc/lighttpd/lighttpd.conf"; fi\nexec lighttpd -D -f $CONFIG 2>&1')
+TERMUX_PKG_SERVICE_SCRIPT=("lighttpd" "if [ -f \"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/lighttpd/lighttpd.conf\"; fi\nexec lighttpd -D -f \$CONFIG 2>&1')
 
 termux_step_pre_configure() {
 	LDFLAGS="$LDFLAGS -landroid-glob"
