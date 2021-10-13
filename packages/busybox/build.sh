a292529fae70b3f5419d68959d80795387574c85
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 12:48:35 +0300

    busybox: expand prefix/home for service scripts

--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="Tiny versions of many common UNIX utilities into a singl
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.33.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://busybox.net/downloads/busybox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=12cec6bd2b16d8a9446dd16130f2b92982f1819f6e1c5f5887b6db03f5660d28
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_SERVICE_SCRIPT=(
 	"telnetd" 'exec busybox telnetd -F'
-	"ftpd" 'exec busybox tcpsvd -vE 0.0.0.0 8021 busybox ftpd -w $HOME'
-	"busybox-httpd" 'exec busybox httpd -f -p 0.0.0.0:8080 -h $PREFIX/srv/www/ 2>&1'
+	"ftpd" "exec busybox tcpsvd -vE 0.0.0.0 8021 busybox ftpd -w $TERMUX_ANDROID_HOME"
+	"busybox-httpd" "exec busybox httpd -f -p 0.0.0.0:8080 -h $TERMUX_PREFIX/srv/www/ 2>&1"
 )
 
 termux_step_pre_configure() {
