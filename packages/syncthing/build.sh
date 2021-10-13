9b4d05f9b706c83d5797bb7680d1f13ef84348bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 13:45:14 +0300

    syncthing: use TERMUX_PREFIX instead of PREFIX

--- packages/syncthing/build.sh
+++ packages/syncthing/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # NOTE: as of 1.12.0 compilation fails when package zstd is
 # present in TERMUX_PREFIX.
 TERMUX_PKG_VERSION=1.18.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/releases/download/v${TERMUX_PKG_VERSION}/syncthing-source-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=32c11a56f467b5f9a45ff30da38953387da10d71b4d3cb41643c6a6f5c073c7a
 TERMUX_PKG_AUTO_UPDATE=true
@@ -43,7 +44,7 @@ termux_step_make_install() {
 	cp go/src/github.com/syncthing/syncthing/syncthing $TERMUX_PREFIX/bin/
 
 	for section in 1 5 7; do
-		local MANDIR=$PREFIX/share/man/man$section
+		local MANDIR=$TERMUX_PREFIX/share/man/man$section
 		mkdir -p $MANDIR
 		cp $TERMUX_PKG_SRCDIR/man/*.$section $MANDIR
 	done
