85b8877516b7d982bcaf8a8cdc248f77397fd717
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 13:44:46 +0300

    mpdscribble: expand home & prefix directory path in service script

--- packages/mpdscribble/build.sh
+++ packages/mpdscribble/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A Music Player Daemon (MPD) client which submits informa
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=0.23
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/mpdscribble/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8b3b79538fa9871ce61d7fb793d442c72b6aadf80af4c5eb664286f613d1feee
 TERMUX_PKG_DEPENDS="libcurl, libgcrypt, mpd, libmpdclient, glib"
@@ -12,7 +12,7 @@ TERMUX_PKG_CONFFILES="etc/mpdscribble.conf"
 # mpdscribble already puts timestamps in the info printed to stdout so no need for svlogd -tt,
 # therefore we override the mpdscribble/log run script
 TERMUX_PKG_SERVICE_SCRIPT=(
-	"mpdscribble" 'if [ -f "$HOME/.mpdscribble/mpdscribble.conf" ]; then CONFIG="$HOME/.mpdscribble/mpdscribble.conf"; else CONFIG="$PREFIX/etc/mpdscribble.conf"; fi\nexec mpdscribble -D --log /proc/self/fd/1 --conf $CONFIG'
+	"mpdscribble" "if [ -f \"$TERMUX_ANDROID_HOME/.mpdscribble/mpdscribble.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.mpdscribble/mpdscribble.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/mpdscribble.conf\"; fi\nexec mpdscribble -D --log /proc/self/fd/1 --conf $CONFIG"
 	"mpdscribble/log" 'mkdir -p "$LOGDIR/sv/mpdscribble"\nexec svlogd "$LOGDIR/sv/mpdscribble"'
 )
 
