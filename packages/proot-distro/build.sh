01cd7c13822e33bd9066fb01e7a2689e40b53c77
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-13 15:57:56 +0300

    proot-distro: update to 2.6.7

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.6.6
+TERMUX_PKG_VERSION=2.6.7
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0cec903067376b389a4a433c2af7670d2bd236ced9f0b9573fb1d606ffcb5892
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
@@ -11,7 +11,10 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 
 termux_step_make_install() {
-	TERMUX_PREFIX="$TERMUX_PREFIX" TERMUX_ANDROID_HOME="$TERMUX_ANDROID_HOME" ./install.sh
+	env TERMUX_APP_PACKAGE="$TERMUX_APP_PACKAGE" \
+		TERMUX_PREFIX="$TERMUX_PREFIX" \
+		TERMUX_ANDROID_HOME="$TERMUX_ANDROID_HOME" \
+		./install.sh
 }
 
 termux_step_create_debscripts() {
