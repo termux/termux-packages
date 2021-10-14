6832de43657b061b06e77a59502b90158d180274
Ian Hu <hu2008yinxiang@163.com>
2021-10-14 16:30:03 +0800

    upgrade erlang to 24.1.2

--- packages/erlang/build.sh
+++ packages/erlang/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.erlang.org/
 TERMUX_PKG_DESCRIPTION="General-purpose concurrent functional programming language"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=22.3.4.22
+TERMUX_PKG_VERSION=24.1.2
 TERMUX_PKG_SRCURL=https://github.com/erlang/otp/archive/OTP-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=dd306ac9987bbe80b20bf1efc8f94b7d6c0248c9fcb2dc761580c2c28d06cfba
+TERMUX_PKG_SHA256=c141a046bb7184a7bb5c3d6da2ed013e465d1fbe4ff5cd16e0fbb7a0e786a152
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_AUTO_UPDATE_TAG_REGEXP="\d+(\.\d+)+"
 TERMUX_PKG_DEPENDS="openssl, ncurses, zlib"
@@ -12,26 +12,21 @@ TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-javac --with-ssl=${TERMUX_PREFIX} --with-termcap"
-TERMUX_PKG_EXTRA_MAKE_ARGS="noboot"
 
 termux_step_post_get_source() {
-	# We need a host build every time:
+	# We need a host build every time, because we dont know the full output of host build and have no idea to cache it.
 	rm -Rf "$TERMUX_PKG_HOSTBUILD_DIR"
-	./otp_build autoconf
 }
 
 termux_step_host_build() {
-	cd $TERMUX_PKG_SRCDIR
-	./configure --enable-bootstrap-only
-	make -j "$TERMUX_MAKE_PROCESSES"
+	cd $TERMUX_PKG_BUILDDIR
+	# Erlang cross compile reference: https://github.com/erlang/otp/blob/master/HOWTO/INSTALL-CROSS.md#building-a-bootstrap-system
+	# Build erlang bootstrap system.
+	./configure --enable-bootstrap-only --without-javac --without-ssl --without-termcap
+	make -j $TERMUX_MAKE_PROCESSES
 }
 
 termux_step_pre_configure() {
-	(cd erts && autoreconf)
-
-	# liblog is needed for syslog usage:
-	LDFLAGS+=" -llog"
-	# Put binaries built in termux_step_host_build at start of PATH:
-	cp bin/*/* $TERMUX_PKG_SRCDIR/bootstrap/bin
-	export PATH="$TERMUX_PKG_SRCDIR/bootstrap/bin:$PATH"
+	# Add --build flag for erlang cross build
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --build=$(./erts/autoconf/config.guess)"
 }
