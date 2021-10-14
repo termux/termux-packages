b6af8c5fb615288e81c98e063f72c7213f1e2deb
Lucinda May Phipps <landfillbaby69@gmail.com>
2021-10-14 05:43:35 +0100

    pathpicker: don't install tests

--- packages/pathpicker/build.sh
+++ packages/pathpicker/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Facebook PathPicker - a terminal-based file picker"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.9.2
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/facebook/PathPicker/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4b3deac0100fb9405adca17a1c75e133359a6cbb1351c392276a55ef8ef40857
 TERMUX_PKG_AUTO_UPDATE=true
@@ -13,7 +13,7 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	_PKG_DIR=$TERMUX_PREFIX/share/pathpicker
-	rm -Rf $_PKG_DIR
+	rm -Rf $_PKG_DIR src/__tests__
 	mkdir -p $_PKG_DIR
 	cp -Rf src/ $_PKG_DIR
 	cp fpp $_PKG_DIR/fpp
