95a6737e9549eebe572f89e066470c8d58bdc00d
Henrik Grimler <henrik@grimler.se>
2021-10-15 19:38:27 +0200

    ki18n: fix build
    Seems we need to depend on qt5-qtdeclarative and disable python
    bindings (though the latter could probably be fixed somehow). Error
    from python bindings was:
    
    CMake Error at /data/data/com.termux/files/usr/share/ECM/find-modules/FindPythonModuleGeneration.cmake:170 (message):
      The max python version in PythonModuleGeneration must be updated.
    Call Stack (most recent call first):
      src/CMakeLists.txt:63 (find_package)

--- x11-packages/ki18n/build.sh
+++ x11-packages/ki18n/build.sh
@@ -7,4 +7,4 @@ TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL="http://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/ki18n-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=f2fc8c40c10576da8b74070b7dc8e752fdd04204cb2bfe522f37a0458fbaf881
 TERMUX_PKG_DEPENDS="qt5-qtbase, gettext"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, python, qt5-qtbase-cross-tools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, python, qt5-qtbase-cross-tools, qt5-qtdeclarative"
