745e153c04cbcb912decf3bdaf030b2bbb8f55ca
Maxython <mixython@gmail.com>
2021-10-18 10:58:54 +0300

    Add pacman

---
+++ packages/pacman/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://archlinux.org/pacman/
+TERMUX_PKG_DESCRIPTION="a simple library-based package manager"
+TERMUX_PKG_LICENSE="GNU-2.0"
+TERMUX_PKG_MAINTAINER="@Maxython"
+TERMUX_PKG_VERSION=6.0.0
+TERMUX_PKG_REVISION=0
+TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/pacman/pacman-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=004448085a7747bdc7a0a4dd5d1fb7556c6b890111a06e029ab088f9905d4808
+TERMUX_PKG_DEPENDS="bash, libarchive, curl, gpgme"
+TERMUX_PKG_BUILD_DEPENDS="doxygen, asciidoc"
+TERMUX_PKG_GROUPS="base-devel"
+
+termux_step_pre_configure() {
+	rm pacman-${TERMUX_PKG_VERSION}/scripts/libmakepkg/executable/sudo.sh.in
+	rm pacman-${TERMUX_PKG_VERSION}/scripts/libmakepkg/executable/fakeroot.sh.in
+}
