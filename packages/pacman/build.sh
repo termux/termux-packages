0213f62670c717a6fc1113e6f2db89a6ca3a3ca4
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-18 14:04:38 +0300

    Update build.sh

--- packages/pacman/build.sh
+++ packages/pacman/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="a simple library-based package manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Maxython"
 TERMUX_PKG_VERSION=6.0.0
-#TERMUX_PKG_REVISION=0
 TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/pacman/pacman-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=004448085a7747bdc7a0a4dd5d1fb7556c6b890111a06e029ab088f9905d4808
 TERMUX_PKG_DEPENDS="bash, libarchive, curl, gpgme"
@@ -12,6 +11,6 @@ TERMUX_PKG_GROUPS="base-devel"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	rm pacman-${TERMUX_PKG_VERSION}/scripts/libmakepkg/executable/sudo.sh.in
-	rm pacman-${TERMUX_PKG_VERSION}/scripts/libmakepkg/executable/fakeroot.sh.in
+	rm -f ./scripts/libmakepkg/executable/sudo.sh.in
+	rm -f ./scripts/libmakepkg/executable/fakeroot.sh.in
 }
