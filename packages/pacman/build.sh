1c00fd7fd205116b7cf7cbfa1160bb90d8718fd6
Maxython <mixython@gmail.com>
2021-10-19 12:54:17 +0300

    Fix bug

--- packages/pacman/build.sh
+++ packages/pacman/build.sh
@@ -6,9 +6,8 @@ TERMUX_PKG_VERSION=6.0.0
 TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/pacman/pacman-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=004448085a7747bdc7a0a4dd5d1fb7556c6b890111a06e029ab088f9905d4808
 TERMUX_PKG_DEPENDS="bash, libarchive, curl, gpgme"
-TERMUX_PKG_BUILD_DEPENDS="doxygen, asciidoc, nettle"
+TERMUX_PKG_BUILD_DEPENDS="doxygen, asciidoc (>= 9.0.2), nettle"
 TERMUX_PKG_GROUPS="base-devel"
-TERMUX_PKG_HOSTBUILD=true
 
 termux_step_pre_configure() {
 	rm -f ./scripts/libmakepkg/executable/sudo.sh.in
