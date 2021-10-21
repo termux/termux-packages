a75de594ba5e5c199b0cc4f47f103dfa1f720370
Henrik Grimler <henrik@grimler.se>
2021-10-21 08:38:51 +0200

    termux_setup_protobuf: update checksum

--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -14,7 +14,7 @@ termux_setup_protobuf() {
 			termux_download \
 				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
 				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				220bd1704c73dbf4d0a91399a2ecf9d19938b5cd80c8a38839a023d8b87bb772
+				2994b7256f7416b90ad831dbf76a27c0934386deb514587109f39141f2636f37
 
 			rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
 			unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
