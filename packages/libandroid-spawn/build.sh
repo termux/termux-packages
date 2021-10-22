d821abed8fb38b5a2f28797a3066bca3db8dcbee
Butta <repo@butta.fastem.com>
2021-10-22 11:11:30 +0530

    libandroid-spawn: NDK 23 actually checks the INTRODUCED_IN() macros, so indicate that this library has been backported to API 24

--- packages/libandroid-spawn/build.sh
+++ packages/libandroid-spawn/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://man7.org/linux/man-pages/man3/posix_spawn.3.html
 TERMUX_PKG_DESCRIPTION="Shared library for the posix_spawn system function"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.2
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=0.3
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
