e91e3d31c10f41e156f9eb4e5b815190dfe6724b
Lucy Phipps <landfillbaby69@gmail.com>
2021-10-22 21:39:48 +0100

    alpine: update to 2.25

--- packages/alpine/build.sh
+++ packages/alpine/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://repo.or.cz/alpine.git
+TERMUX_PKG_HOMEPAGE=http://alpine.x10host.com/
 TERMUX_PKG_DESCRIPTION="Fast, easy to use email client"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.24.2
-TERMUX_PKG_SRCURL=https://repo.or.cz/alpine.git/snapshot/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=8e4654bce91837e053adf9d5b512f8c54234f527148a238f60ad49598581fb61
+TERMUX_PKG_VERSION=2.25
+TERMUX_PKG_SRCURL=http://alpine.x10host.com/alpine/release/src/alpine-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=658a150982f6740bb4128e6dd81188eaa1212ca0bf689b83c2093bb518ecf776
 TERMUX_PKG_DEPENDS="libcrypt, ncurses, openssl-tool"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-debug
