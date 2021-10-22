46992c2993418345d790ff9e06acbab817dbcb4d
Lucy Phipps <landfillbaby69@gmail.com>
2021-10-22 15:50:08 +0100

    rinetd: update to 0.73

--- packages/rinetd/build.sh
+++ packages/rinetd/build.sh
@@ -1,9 +1,8 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/boutell/rinetd
+TERMUX_PKG_HOMEPAGE=https://github.com/samhocevar/rinetd
 TERMUX_PKG_DESCRIPTION="A user-mode TCP port redirection server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=0973bd2d1c187da9706fd83c8aa68fabc1775a31
-TERMUX_PKG_VERSION=2019.10.22-${_COMMIT:0:8}
-TERMUX_PKG_SRCURL=https://github.com/boutell/rinetd/archive/${_COMMIT}.tar.gz
-TERMUX_PKG_SHA256=66d04c1385b8308e423f388678b8b9236e0b036a1d5dcce758d7b777a554b818
+TERMUX_PKG_VERSION=1:0.73
+TERMUX_PKG_SRCURL=https://github.com/samhocevar/rinetd/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=62d3e5800299bb3940aea847ffd7662722deb5d4b85625fcfe8f7ebbc0424c80
 TERMUX_PKG_BUILD_IN_SRC=true
