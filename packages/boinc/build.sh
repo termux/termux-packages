950544685d2aa0e4e709747b5829bbf417822576
Jia Yuan Lo <jylo06g@gmail.com>
2021-10-23 10:36:21 +0800

    boinc: bump revision and small patch to build

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open-source software for volunteer computing"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.16.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/BOINC/boinc/archive/client_release/${TERMUX_PKG_VERSION:0:4}/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0d5656a9f8ed1048936a5764270848b892d63f27bdb863d0ace447f1eaae6002
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libcurl, openssl, zlib"
@@ -18,8 +18,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 TERMUX_PKG_CONFFILES="etc/boinc-client.conf"
 
 termux_step_pre_configure() {
-	CFLAGS="${CFLAGS/-Oz/-O2}"
-	CXXFLAGS="${CXXFLAGS/-Oz/-O2}"
+	# for benchmark purposes
+	CFLAGS="${CFLAGS/-Oz/-Os}"
+	CXXFLAGS="${CXXFLAGS/-Oz/-Os}"
 	LDFLAGS+=" -landroid-shmem"
 	./_autosetup
 }
