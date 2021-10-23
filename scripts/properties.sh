9d020f82ec10f83c722c8ca6db2f3b67ff5f6714
Lucinda May Phipps <landfillbaby69@gmail.com>
2021-10-23 13:55:04 +0100

    properties.sh: notes for next ndk update
    we forgot to reset the revision numbers this time

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -1,7 +1,14 @@
+# keep repology-metadata in sync with this
+
 TERMUX_ANDROID_BUILD_TOOLS_VERSION=30.0.3
 TERMUX_NDK_VERSION_NUM=23
 TERMUX_NDK_REVISION="b"
 TERMUX_NDK_VERSION=$TERMUX_NDK_VERSION_NUM$TERMUX_NDK_REVISION
+# when changing the above:
+# remove TERMUX_PKG_REVISION in:
+#   libc++, ndk-multilib, ndk-sysroot, vulkan-loader-android
+# update SHA256 sums in scripts/setup-android-sdk.sh
+# check all packages build and run correctly and bump if needed
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
