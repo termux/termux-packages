6c63fc1cc727acb85613730a566e8bde6eec08a0
Henrik Grimler <henrik@grimler.se>
2021-10-25 12:57:51 +0200

    emscripten: install to $TERMUX_PKG_MASSAGEDIR

--- packages/emscripten/build.sh
+++ packages/emscripten/build.sh
@@ -70,7 +70,7 @@ _LLVM_BUILD_ARGS="
 -DCMAKE_BUILD_TYPE=MinSizeRel
 -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
 -DCMAKE_CROSSCOMPILING=ON
--DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}/opt/emscripten-llvm
+-DCMAKE_INSTALL_PREFIX=$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/opt/emscripten-llvm
 
 -DDEFAULT_SYSROOT=$(dirname ${TERMUX_PREFIX})
 -DGENERATOR_IS_MULTI_CONFIG=ON
@@ -105,7 +105,7 @@ _LLVM_BUILD_ARGS="
 
 # https://github.com/WebAssembly/binaryen/blob/main/CMakeLists.txt
 _BINARYEN_BUILD_ARGS="
--DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}/opt/emscripten-binaryen
+-DCMAKE_INSTALL_PREFIX=${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-binaryen
 -DBUILD_TESTS=OFF
 -DBYN_ENABLE_LTO=ON
 "
@@ -292,11 +292,11 @@ termux_step_make_install() {
 	sed -e "s|-git||" -i "${TERMUX_PKG_SRCDIR}/emscripten-version.txt"
 
 	# skip using Makefile which does host npm install
-	rm -fr "${TERMUX_PREFIX}/opt/emscripten"
-	./tools/install.py "${TERMUX_PREFIX}/opt/emscripten"
+	rm -fr "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten"
+	./tools/install.py "$${TERMUX_PKG_MASSAGEDIR}/{TERMUX_PREFIX}/opt/emscripten"
 
 	# subpackage optional third party test suite files
-	cp -fr "${TERMUX_PKG_SRCDIR}/test/third_party" "${TERMUX_PREFIX}/opt/emscripten/test/third_party"
+	cp -fr "${TERMUX_PKG_SRCDIR}/test/third_party" "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten/test/third_party"
 
 	# first run generates .emscripten and exits immediately
 	rm -f "${TERMUX_PKG_SRCDIR}/.emscripten"
@@ -307,31 +307,34 @@ termux_step_make_install() {
 		-e "s|^BINARYEN_ROOT.*|BINARYEN_ROOT = '${TERMUX_PREFIX}/opt/emscripten-binaryen' # directory|" \
 		-e "s|^NODE_JS.*|NODE_JS = '${TERMUX_PREFIX}/bin/node' # executable|"
 	grep "${TERMUX_PREFIX}" "${TERMUX_PKG_SRCDIR}/.emscripten"
-	install -Dm644 "${TERMUX_PKG_SRCDIR}/.emscripten" "${TERMUX_PREFIX}/opt/emscripten/.emscripten"
+	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/opt/emscripten
+	install -Dm644 "${TERMUX_PKG_SRCDIR}/.emscripten" \
+		"${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten/.emscripten"
 
 	# add emscripten directory to PATH var
 	cat <<- EOF > "${TERMUX_PKG_TMPDIR}/emscripten.sh"
 	#!${TERMUX_PREFIX}/bin/sh
 	export PATH=\${PATH}:${TERMUX_PREFIX}/opt/emscripten
 	EOF
-	install -Dm644 "${TERMUX_PKG_TMPDIR}/emscripten.sh" "${TERMUX_PREFIX}/etc/profile.d/emscripten.sh"
+	install -Dm644 "${TERMUX_PKG_TMPDIR}/emscripten.sh" "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/etc/profile.d/emscripten.sh"
 
 	# add useful tools not installed by LLVM_INSTALL_TOOLCHAIN_ONLY=ON
 	for tool in llvm-{addr2line,dwarfdump,dwp,link,nm,objdump,ranlib,readobj,size,strings}; do
-		install -Dm755 "${TERMUX_PKG_BUILDDIR}/build-llvm/bin/${tool}" "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/${tool}"
+		install -Dm755 "${TERMUX_PKG_BUILDDIR}/build-llvm/bin/${tool}" \
+			"${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/${tool}"
 	done
 
 	# wasm32 triplets
-	rm -fr "${TERMUX_PREFIX}"/opt/emscripten-llvm/bin/wasm32-{clang,clang++,wasi-clang,wasi-clang++}
-	rm -fr "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm-ld"
-	ln -fs "clang"   "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-clang"
-	ln -fs "clang++" "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-clang++"
-	ln -fs "clang"   "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-wasi-clang"
-	ln -fs "clang++" "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-wasi-clang++"
-	ln -fs "lld"     "${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm-ld"
+	rm -fr "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/opt/emscripten-llvm/bin/wasm32-{clang,clang++,wasi-clang,wasi-clang++}
+	rm -fr "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm-ld"
+	ln -fs "clang"   "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-clang"
+	ln -fs "clang++" "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-clang++"
+	ln -fs "clang"   "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-wasi-clang"
+	ln -fs "clang++" "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm32-wasi-clang++"
+	ln -fs "lld"     "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/opt/emscripten-llvm/bin/wasm-ld"
 
 	# termux_step_massage strip does not cover opt dir
-	for path in "${TERMUX_PREFIX}"/opt/emscripten-{llvm,binaryen}/{bin,lib}; do
+	for path in "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/opt/emscripten-{llvm,binaryen}/{bin,lib}; do
 		find "${path}" -type f -exec "${STRIP}" "{}" \;
 	done
 
