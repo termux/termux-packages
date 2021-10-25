8653c74e7ef8bfc0bb336416b132e055f585f380
Henrik Grimler <henrik@grimler.se>
2021-10-25 16:03:25 +0200

    pandoc: install to $TERMUX_PKG_MASSAGEDIR

--- packages/pandoc/build.sh
+++ packages/pandoc/build.sh
@@ -29,9 +29,12 @@ termux_step_make_install() {
 	tar xf "pandoc-${TERMUX_PKG_VERSION}.tar.gz"
 	cd "pandoc-${TERMUX_PKG_VERSION}"
 
-	install -Dm700 "./bin/pandoc" "$TERMUX_PREFIX/bin/pandoc"
-	install -Dm600 "./share/man/man1/pandoc.1.gz" "$TERMUX_PREFIX/share/man/man1/pandoc.1.gz"
-	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/bash-completion/completions/pandoc
+	install -Dm700 "./bin/pandoc" \
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/pandoc"
+	install -Dm600 "./share/man/man1/pandoc.1.gz" \
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/man/man1/pandoc.1.gz"
+	install -Dm644 /dev/null \
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/share/bash-completion/completions/pandoc
 }
 
 termux_step_create_debscripts() {
