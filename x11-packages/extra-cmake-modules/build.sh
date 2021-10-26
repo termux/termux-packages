387090d190940c040e7d8027f4c20d10132fa085
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-26 16:55:51 +0000

    extra-cmake-modules: update to 5.87.0
    %ci:no-build

--- x11-packages/extra-cmake-modules/build.sh
+++ x11-packages/extra-cmake-modules/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Extra CMake modules (KDE)"
 TERMUX_PKG_LICENSE="BSD-3-Clause"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.82.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=5.87.0
 TERMUX_PKG_SRCURL="http://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/extra-cmake-modules-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=5972ec6d78c3e95ab9cbecdb0661c158570e868466357c5cec2b63a4251ecce4
+TERMUX_PKG_SHA256=541ca70d8e270614d19d8fd9e55f97b55fa1dc78d6538c6f6757be372ef8bcab
 TERMUX_PKG_DEPENDS="cmake"
 
 termux_step_install_license() {
