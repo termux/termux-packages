533659dde4800a5c6af4376abe6c8976221ad69f
Oskar <nivua1fn@duck.com>
2021-10-27 23:33:16 +0200

    Create build.sh

---
+++ packages/libtomcrypt/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/libtom/libtomcrypt
+TERMUX_PKG_DESCRIPTION="LibTomCrypt is a fairly comprehensive, modular and portable cryptographic toolkit that provides developers with a vast array of well known published block ciphers, one-way hash functions, chaining modes, pseudo-random number generators, public key cryptography and a plethora of other routines."
+TERMUX_PKG_LICENSE="The Unlicense"
+TERMUX_PKG_MAINTAINER="@medzikuser"
+TERMUX_PKG_VERSION=1.18.2
+TERMUX_PKG_SRCURL=https://github.com/libtom/libtomcrypt/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+d870fad1e31cb787c85161a8894abb9d7283c2a654a9d3d4c6d45a1eba59952c
+TERMUX_PKG_BUILD_IN_SRC=true
