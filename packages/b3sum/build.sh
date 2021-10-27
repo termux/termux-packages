a48c64fe4b3a74e8954f82ddb486570b132ac048
MedzikUser <nivua1fn@duck.com>
2021-10-27 16:31:01 +0200

    update

--- packages/b3sum/build.sh
+++ packages/b3sum/build.sh
@@ -6,13 +6,12 @@ TERMUX_PKG_VERSION=1.1.0
 TERMUX_PKG_SRCURL=https://github.com/BLAKE3-team/BLAKE3/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=0bfba4ba71a9b04afbaa6bfc45c38e0598ce404e2cc5094b1d4ef45e83db2ca1
 
-termux_step_configure() {
-	termux_setup_rust
-}
-
 termux_step_make() {
+	termux_setup_rust
+
 	cd $TERMUX_PKG_SRCDIR/b3sum
-	cargo build --release --host $CARGO_TARGET_NAME
+
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
 termux_step_make_install() {
