f6c51a8fc62ee02825a8459bc0547fe45534b982
Oskar <nivua1fn@duck.com>
2021-10-28 18:16:21 +0200

    add package stoken-cli

---
+++ packages/stoken-cli/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/cernekee/stoken
+TERMUX_PKG_DESCRIPTION="RSA SecurID-compatible software token for Linux/UNIX systems"
+TERMUX_PKG_LICENSE="MIT, Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.92
+TERMUX_PKG_SRCURL=https://github.com/cernekee/stoken/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9b9c5e0f09ca14a54454319b64af98a02d0ae1b3eb1122c95e2130736f440cd1
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="libtomcrypt, libxml2"
+
+termux_step_pre_configure() {
+	cd $TERMUX_PKG_SRCDIR
+	./configure
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin $TERMUX_PKG_SRCDIR/stoken
+}
