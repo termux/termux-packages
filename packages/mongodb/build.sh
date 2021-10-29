dfb2005ab4d2e2a2ee4b6b7115e1a606dd8a3787
Oskar <nivua1fn@duck.com>
2021-10-29 18:22:42 +0200

    Update build.sh

--- packages/mongodb/build.sh
+++ packages/mongodb/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="MIT" # Edit this
 TERMUX_PKG_MAINTAINER="@medzikuser"
 TERMUX_PKG_VERSION=5.0.3
 TERMUX_PKG_SRCURL=https://github.com/mongodb/mongo/archive/refs/tags/r${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=04441784c364caf6d2603307c93c21c3b55808d0a3efae00bd34b4edf37e492d
+TERMUX_PKG_SHA256=dc1363049afe51f52510848acc8799dc228a451bb64c8c11a26a300db9480b0c
 
 termux_step_make() {
 	termux_setup_scons
