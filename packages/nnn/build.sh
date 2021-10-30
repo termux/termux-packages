01e5d3ca68a5ed8723925ba38d9206b6f44e9c66
Lucy Phipps <landfillbaby69@gmail.com>
2021-10-30 14:52:16 +0100

    nnn: don't depend on debianutils
    https://github.com/jarun/nnn/commit/f343f315b5cb1a24feec2797b80ed21889f49154

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b6df8e262e5613dd192bac610a6da711306627d56573f1a770a173ef078953bb
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="debianutils, file, findutils, readline, wget, libandroid-support, lzip"
+TERMUX_PKG_DEPENDS="file, findutils, readline, wget, libandroid-support, lzip"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
@@ -18,4 +18,3 @@ termux_step_post_make_install() {
 	install -Dm600 misc/auto-completion/zsh/_nnn \
 		$TERMUX_PREFIX/share/zsh/site-functions/_nnn
 }
-
