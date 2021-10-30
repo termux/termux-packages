29c42171794709f4ff8f31a62409143019b98665
Henrik Grimler <henrik@grimler.se>
2021-10-30 16:06:41 +0200

    proton-bridge: update to 1.8.10
    Fixes https://github.com/termux/termux-packages/issues/7872.

--- packages/proton-bridge/build.sh
+++ packages/proton-bridge/build.sh
@@ -1,19 +1,30 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ProtonMail/proton-bridge
 TERMUX_PKG_DESCRIPTION="ProtonMail Bridge application"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.5.7
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.8.10
 TERMUX_PKG_SRCURL=https://github.com/ProtonMail/proton-bridge.git
 TERMUX_PKG_GIT_BRANCH=br-$TERMUX_PKG_VERSION
 TERMUX_PKG_MAINTAINER="Radomír Polách <rp@t4d.cz>"
+TERMUX_PKG_DEPENDS=libsecret
+
+# On 32bit arches we get:
+# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:2756: constant 4230534781 overflows int
+# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:2780: constant 4230534781 overflows int
+# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:6683: constant 2740715144 overflows int
+# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:6683: constant 4294967167 overflows int
+# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:11728: constant 4244674173 overflows int
+# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:12153: constant 4292870143 overflows int
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
-termux_step_make_install() {
+termux_step_make() {
 	termux_setup_golang
 	export GOPATH=$TERMUX_PKG_BUILDDIR
-	export BUILDDIR=$TERMUX_PREFIX/bin
 	cd $TERMUX_PKG_SRCDIR
-	go mod tidy
+
 	make build-nogui
-	install -Dm700 proton-bridge "$TERMUX_PREFIX"/bin/proton-bridge
+}
+
+termux_step_make_install() {
+	install -Dm700 $TERMUX_PKG_SRCDIR/proton-bridge \
+		"$TERMUX_PREFIX"/bin/proton-bridge
 }
