808a6cb9c03a506bd498ce2db55ce7d064610711
Henrik Grimler <henrik@grimler.se>
2021-10-30 16:39:17 +0200

    rust-analyzer: install to TERMUX_PKG_MASSAGEDIR

--- packages/rust-analyzer/build.sh
+++ packages/rust-analyzer/build.sh
@@ -14,5 +14,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rust-analyzer
+	install -Dm755 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
+		target/${CARGO_TARGET_NAME}/release/rust-analyzer
 }
