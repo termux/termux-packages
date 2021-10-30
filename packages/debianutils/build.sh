74dad8b1aea2e05e9be8e0abd9d857779ebedff2
Lucy Phipps <landfillbaby69@gmail.com>
2021-10-30 14:50:26 +0100

    debianutils: update to 5.5

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -2,15 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/debianutils
 TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installation scripts of Debian packages"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.11.2
-TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3b680e81709b740387335fac8f8806d71611dcf60874e1a792e862e48a1650de
+TERMUX_PKG_VERSION=5.5
+TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.orig.tar.xz
+TERMUX_PKG_SHA256=2b0fad5c00eb2b8461523b2950e6f06e6ddbb0ac3384c5a3377867d51098d102
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/add-shell
 bin/installkernel
 bin/remove-shell
+bin/update-shells
 share/man/man8/add-shell.8
 share/man/man8/installkernel.8
 share/man/man8/remove-shell.8
+share/man/man8/update-shells.8
 "
