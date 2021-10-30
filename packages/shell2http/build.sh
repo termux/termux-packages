73f510ffa435f5eaf85071716aa8649c5a82c76f
Henrik Grimler <henrik@grimler.se>
2021-10-30 16:56:08 +0200

    shell2http: install to TERMUX_PKG_MASSAGEDIR

--- packages/shell2http/build.sh
+++ packages/shell2http/build.sh
@@ -21,5 +21,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/msoap/shell2http/shell2http
+	install -Dm700 -t "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/bin \
+		"$GOPATH"/src/github.com/msoap/shell2http/shell2http
 }
