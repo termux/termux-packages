3878b565dac0b7aa1dd3bfeff6547c0dc7b431e1
Henrik Grimler <henrik@grimler.se>
2021-10-31 15:33:00 +0100

    dte: avoid overriding termux_step_make and termux_step_make_install

--- packages/dte/build.sh
+++ packages/dte/build.sh
@@ -3,16 +3,12 @@ TERMUX_PKG_DESCRIPTION='A small, configurable console text editor'
 TERMUX_PKG_LICENSE='GPL-2.0'
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.10
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://craigbarnes.gitlab.io/dist/dte/dte-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=db62aab235764f735adc8378f796d6474596582b7dae357e0bddf31304189800
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, libiconv"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make() {
-	make V=1 LDLIBS='-landroid-support -landroid-glob -liconv'
-}
-
-termux_step_make_install() {
-	make install V=1 prefix="$TERMUX_PREFIX"
+termux_step_pre_configure() {
+	export LDLIBS="-landroid-support -landroid-glob -liconv"
 }
