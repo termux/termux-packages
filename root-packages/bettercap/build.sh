c0bc0b3a444959dd05c3dc9b76a9808a5a4cb337
etmatrix <etmatrix@etmatrix>
2021-11-01 10:56:02 +0100

    update to version 2.32.0 now it's require library libnetfilter_queue

--- root-packages/bettercap/build.sh
+++ root-packages/bettercap/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.bettercap.org
 TERMUX_PKG_DESCRIPTION="The Swiss Army knife for 802.11, BLE and Ethernet networks reconnaissance and MITM attacks"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.29
+TERMUX_PKG_VERSION=2.32.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/bettercap/bettercap/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c414be98a48d9f279460b325dddaef8479132a1470c6f305790d79e04dac1297
-TERMUX_PKG_DEPENDS="libpcap, libusb"
+TERMUX_PKG_SHA256=ea28d4d533776a328a54723a74101d1720016ffe7d434bf1d7ab222adb397ac6
+TERMUX_PKG_DEPENDS="libpcap, libusb, libnetfilter_queue"
 # x86_64 seem to depend on libnetfilter_queue
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
 
