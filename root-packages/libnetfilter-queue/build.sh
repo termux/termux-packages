533c1b807c91d31520cc013ccf1ac9c7a823abe0
etmatrix <3065939+etmatrix@users.noreply.github.com>
2021-11-01 11:37:08 +0100

    missing dependency libmnl

--- root-packages/libnetfilter-queue/build.sh
+++ root-packages/libnetfilter-queue/build.sh
@@ -7,4 +7,4 @@ TERMUX_PKG_VERSION=1.0.5
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://ftp.netfilter.org/pub/libnetfilter_queue/libnetfilter_queue-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=f9ff3c11305d6e03d81405957bdc11aea18e0d315c3e3f48da53a24ba251b9f5
-TERMUX_PKG_DEPENDS="libnfnetlink"
+TERMUX_PKG_DEPENDS="libnfnetlink, libmnl"
