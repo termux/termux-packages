e2ba9352e05e3b2bc320f1d749f411a3fe4bc046
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-11-02 12:26:45 +0200

    dtach: use mode 700 when installing binary

--- packages/dtach/build.sh
+++ packages/dtach/build.sh
@@ -7,6 +7,6 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/dtach/dtach-${TE
 TERMUX_PKG_SHA256=32e9fd6923c553c443fab4ec9c1f95d83fa47b771e6e1dafb018c567291492f3
 
 termux_step_make_install() {
-	install -Dm755 -t ${TERMUX_PREFIX}/bin dtach
+	install -Dm700 -t ${TERMUX_PREFIX}/bin dtach
 	install -Dm600 -t ${TERMUX_PREFIX}/share/man/man1 ${TERMUX_PKG_SRCDIR}/dtach.1
 }
