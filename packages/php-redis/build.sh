d486626c8b51e9866bfaae0262c4cb9a8bf4a976
Yaksh Bariya <yakshbari4@gmail.com>
2021-11-04 22:02:36 +0530

    Rebuild packages dependent on php and remove x86_64 as blacklisted arch

--- packages/php-redis/build.sh
+++ packages/php-redis/build.sh
@@ -3,14 +3,10 @@ TERMUX_PKG_DESCRIPTION="PHP extension for interfacing with Redis"
 TERMUX_PKG_LICENSE="PHP-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.3.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/phpredis/phpredis/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c0df53dc4e8cd2921503fefa224cfd51de7f74561324a6d3c66f30d4016178b3
 TERMUX_PKG_DEPENDS=php
-# php is (currently) blacklisted for x86_64. Need to blacklist
-# php-redis as well for the same arch for
-#   ./build-package.sh -a all -i php-redis
-# to succeed
-TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
 
 termux_step_pre_configure() {
 	$TERMUX_PREFIX/bin/phpize
--- packages/php-zephir-parser/build.sh
+++ packages/php-zephir-parser/build.sh
@@ -3,17 +3,13 @@ TERMUX_PKG_DESCRIPTION="The Zephir Parser delivered as a C extension for the PHP
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="ian4hu <hu2008yinxiang@163.com>"
 TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/zephir-lang/php-zephir-parser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8e6a5356e27e82ff9fa070d1b717a866188165a64dd02dee4494e7baff3739e4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS=php
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
-# php is (currently) blacklisted for x86_64. Need to blacklist
-# php-zephir-parser well for the same arch for
-#   ./build-package.sh -a all -i php-zephir-parser
-# to succeed
-TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
 
 termux_step_pre_configure() {
 	# copy host build `lemon`
