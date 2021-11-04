b5c84608b4d622fa77ab779a295be1e39f42df9f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-11-04 19:48:56 +0000

    qt-creator: fix linking issues
    Not a correct fix though...

--- x11-packages/qt-creator/build.sh
+++ x11-packages/qt-creator/build.sh
@@ -29,12 +29,16 @@ termux_step_post_configure() {
         ${TERMUX_PKG_SRCDIR}/src/libs/utils/Makefile.process_stub
 
     # clangbackend's Makefile lacks -lc++_shared to link against libc++ on x86_64
-    sed -i 's|^LIBS          = $(SUBLIBS)|LIBS = $(SUBLIBS) -lc++_shared|' \
+    sed -i -e 's|^LIBS          = $(SUBLIBS)|LIBS = $(SUBLIBS) -lc++_shared|' \
+	-e 's|-Wl,-rpath,/home/builder/.termux-build/_cache/android-r23b-api-24-v1/lib64||g' \
+	-e 's|-L/home/builder/.termux-build/_cache/android-r23b-api-24-v1/lib64||g' \
         ${TERMUX_PKG_SRCDIR}/src/tools/clangbackend/Makefile
 
     # make sure clangtools link against libc++_shared on x86_64
-    sed -i 's|^LIBS          = $(SUBLIBS)|LIBS = $(SUBLIBS) -lc++_shared|' \
-        ${TERMUX_PKG_SRCDIR}/src/plugins/clangtools//Makefile
+    sed -i -e 's|^LIBS          = $(SUBLIBS)|LIBS = $(SUBLIBS) -lc++_shared|' \
+	-e 's|-Wl,-rpath,/home/builder/.termux-build/_cache/android-r23b-api-24-v1/lib64||g' \
+	-e 's|-L/home/builder/.termux-build/_cache/android-r23b-api-24-v1/lib64||g' \
+        ${TERMUX_PKG_SRCDIR}/src/plugins/clangtools/Makefile
 
     # required by make install, otherwise it installs to '/'
     export INSTALL_ROOT="${TERMUX_PREFIX}"
