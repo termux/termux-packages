1295c2ed59ca414c53ed50e80931bc09b6f4c00b
Leap of Azzam <leapofazzam@gmail.com>
2021-11-06 22:36:59 +0700

    fix libluajit

--- packages/libluajit/libluajit-static.subpackage.sh
+++ packages/libluajit/libluajit-static.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="Static libraries for LuaJIT"
-TERMUX_SUBPKG_DEPENDS="liblua52"
+TERMUX_SUBPKG_DEPENDS="libluajit"
 TERMUX_SUBPKG_INCLUDE="lib/libluajit-5.1.a lib/libluajit.a"
