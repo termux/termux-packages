af2e16028539aadc3d620cd51bbfa42030b60a07
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-11-06 21:54:02 +0200

    libcap: update to 2.60

--- packages/libcap/build.sh
+++ packages/libcap/build.sh
@@ -2,18 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://sites.google.com/site/fullycapable/
 TERMUX_PKG_DESCRIPTION="POSIX 1003.1e capabilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.51
+TERMUX_PKG_VERSION=2.60
 TERMUX_PKG_SRCURL=https://kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6609f3ab7aebcc8f9277f53a577c657d9f3056d1352ea623da7fd7c0f00890f9
+TERMUX_PKG_SHA256=06a92076ce39a78bd28089e32085f1bde7f3bfa448fad37d895c2358f760b2eb
 TERMUX_PKG_DEPENDS="attr"
 TERMUX_PKG_BREAKS="libcap-dev"
 TERMUX_PKG_REPLACES="libcap-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" PREFIX="$TERMUX_PREFIX" PTHREADS=no
+	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" OBJCOPY=llvm-objcopy PREFIX="$TERMUX_PREFIX" PTHREADS=no
 }
 
 termux_step_make_install() {
-	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" prefix="$TERMUX_PREFIX" RAISE_SETFCAP=no lib=/lib PTHREADS=no install
+	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" OBJCOPY=llvm-objcopy prefix="$TERMUX_PREFIX" RAISE_SETFCAP=no lib=/lib PTHREADS=no install
 }
