2841283d71468f13b801ec5a167295dd80b668b1
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-11-06 21:38:48 +0000

    pacman: add (unofficial) x11 repo url to config (adding x11 packages) (#7932)

--- packages/pacman/build.sh
+++ packages/pacman/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://archlinux.org/pacman/
-TERMUX_PKG_DESCRIPTION="a simple library-based package manager"
+TERMUX_PKG_DESCRIPTION="A library-based package manager with dependency support"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Maxython"
 TERMUX_PKG_VERSION=6.0.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/pacman/pacman-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0db61456e56aa49e260e891c0b025be210319e62b15521f29d3e93b00d3bf731
 TERMUX_PKG_DEPENDS="bash, libarchive, curl, gpgme"
