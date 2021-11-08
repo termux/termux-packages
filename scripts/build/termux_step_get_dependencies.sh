04d36c228b635e9acaa269c9f6676c3b6ce856af
Henrik Grimler <henrik@grimler.se>
2021-11-08 21:02:18 +0100

    termux_step_get_dependencies: put built dependency deb in cache dir
    And then extract and install it with same code as when we download it.

--- scripts/build/termux_step_get_dependencies.sh
+++ scripts/build/termux_step_get_dependencies.sh
@@ -37,26 +37,23 @@ termux_step_get_dependencies() {
 
 			if ! termux_download_deb_pac $PKG $DEP_ARCH $DEP_VERSION $DEP_VERSION_PAC; then
 				echo "Download of $PKG@$DEP_VERSION from $TERMUX_REPO_URL failed, building instead"
-				TERMUX_BUILD_IGNORE_LOCK=true ./build-package.sh ${TERMUX_FORCE_BUILD+-f} -I --format $TERMUX_PACKAGE_FORMAT "${PKG_DIR}"
-				continue
+				TERMUX_BUILD_IGNORE_LOCK=true ./build-package.sh ${TERMUX_FORCE_BUILD+-f} -I -o $TERMUX_COMMON_CACHEDIR-$DEP_ARCH --format $TERMUX_PACKAGE_FORMAT "${PKG_DIR}"
 			fi
-			if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-				if [ ! "$TERMUX_QUIET_BUILD" = true ]; then
-					echo "extracting $PKG to $TERMUX_COMMON_CACHEDIR-$DEP_ARCH..."
+			if [ ! "$TERMUX_QUIET_BUILD" = true ]; then
+				echo "extracting $PKG to $TERMUX_COMMON_CACHEDIR-$DEP_ARCH..."
+			fi
+			(
+				cd $TERMUX_COMMON_CACHEDIR-$DEP_ARCH
+				ar x ${PKG}_${DEP_VERSION}_${DEP_ARCH}.deb data.tar.xz
+				if tar -tf data.tar.xz|grep "^./$">/dev/null; then
+					# Strip prefixed ./, to avoid possible
+					# permission errors from tar
+					tar -xf data.tar.xz --strip-components=1 \
+						--no-overwrite-dir -C /
+				else
+					tar -xf data.tar.xz --no-overwrite-dir -C /
 				fi
-				(
-					cd $TERMUX_COMMON_CACHEDIR-$DEP_ARCH
-					ar x ${PKG}_${DEP_VERSION}_${DEP_ARCH}.deb data.tar.xz
-					if tar -tf data.tar.xz|grep "^./$">/dev/null; then
-						# Strip prefixed ./, to avoid possible
-						# permission errors from tar
-						tar -xf data.tar.xz --strip-components=1 \
-							--no-overwrite-dir -C /
-					else
-						tar -xf data.tar.xz --no-overwrite-dir -C /
-					fi
-				)
-			fi
+			)
 
 			mkdir -p $TERMUX_BUILT_PACKAGES_DIRECTORY
 			echo "$DEP_VERSION" > "$TERMUX_BUILT_PACKAGES_DIRECTORY/$PKG"
