13f3be6c7a07034d49ac055da83a6241bcee358f
Henrik Grimler <henrik@grimler.se>
2021-11-09 21:51:59 +0100

    frogcomposband: install to TERMUX_PKG_MASSAGEDIR

--- packages/frogcomposband/build.sh
+++ packages/frogcomposband/build.sh
@@ -20,5 +20,5 @@ termux_step_post_make_install () {
 
 termux_step_install_license() {
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/LICENSE \
-		$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE
+		$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE
 }
