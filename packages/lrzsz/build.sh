242d2ed706dc6d113f0c5135490cee80c1d132ba
Henrik Grimler <henrik@grimler.se>
2021-11-10 10:36:30 +0100

    lrzsz: set maintainer as well

--- packages/lrzsz/build.sh
+++ packages/lrzsz/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://ohse.de/uwe/software/lrzsz.html
 TERMUX_PKG_DESCRIPTION="Tools for zmodem/xmodem/ymodem file transfer"
 TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.12.21-rc1
 _COMMIT=8cb2a6a29f6345f84d5e8248e2d3376166ab844f
 TERMUX_PKG_SRCURL=https://github.com/UweOhse/lrzsz/archive/$_COMMIT.tar.gz
