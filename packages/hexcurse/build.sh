e69c2e315b76fa0b4d56c5d7cba40bc4d6d7aa0d
Henrik Grimler <henrik@grimler.se>
2021-11-10 20:17:06 +0100

    hexcurse: fix build with ndk-r23

--- packages/hexcurse/build.sh
+++ packages/hexcurse/build.sh
@@ -3,8 +3,12 @@ TERMUX_PKG_DESCRIPTION="Console hexeditor"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.60.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/LonnyGomes/hexcurse/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f6919e4a824ee354f003f0c42e4c4cef98a93aa7e3aa449caedd13f9a2db5530
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ncurses"
+
+termux_step_pre_configure() {
+	export CFLAGS+=" -fcommon"
+}
