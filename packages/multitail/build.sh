5620a058b42286d38628c3f3f8d490bd378196a2
Henrik Grimler <henrik@grimler.se>
2021-11-10 21:07:30 +0100

    multitail: change Makefile patch so we respect DESTDIR

--- packages/multitail/build.sh
+++ packages/multitail/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tool to monitor logfiles and command output in multiple 
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/folkertvanheusden/multitail/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=23f85f417a003544be38d0367c1eab09ef90c13d007b36482cf3f8a71f9c8fc5
 TERMUX_PKG_DEPENDS="libandroid-glob, ncurses, ncurses-ui-libs"
