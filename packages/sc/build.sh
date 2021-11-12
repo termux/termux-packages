5058c3df89a6a28b4aac8ad26ed1eb5a386709e7
Henrik Grimler <henrik@grimler.se>
2021-11-12 22:55:13 +0100

    sc: update Makefile patch so installation to DESTDIR works

--- packages/sc/build.sh
+++ packages/sc/build.sh
@@ -8,8 +8,7 @@ TERMUX_PKG_SRCURL=http://www.ibiblio.org/pub/Linux/apps/financial/spreadsheet/sc
 TERMUX_PKG_SHA256=1997a00b6d82d189b65f6fd2a856a34992abc99e50d9ec463bbf1afb750d1765
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="SIMPLE=-DSIMPLE"
 
-termux_step_post_configure () {
+termux_step_pre_configure () {
 	CFLAGS+=" -I$TERMUX_PREFIX/include"
 }
