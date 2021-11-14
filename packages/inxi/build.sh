ab63744e2127e1bd421c04852a7468e944811d70
Henrik Grimler <henrik@grimler.se>
2021-11-14 21:42:33 +0100

    inxi: install to TERMUX_PKG_MASSAGEDIR

--- packages/inxi/build.sh
+++ packages/inxi/build.sh
@@ -10,6 +10,6 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin/ inxi
-	install -Dm600 -t $TERMUX_PREFIX/share/man/man1/ inxi.1
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/ inxi
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/man/man1/ inxi.1
 }
