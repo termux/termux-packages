a56bdfdb82d71812c322728101f4c1dde5b4ad62
Henrik Grimler <henrik@grimler.se>
2021-11-14 21:39:20 +0100

    cowsay: install to TERMUX_PKG_MASSAGEDIR

--- packages/cowsay/build.sh
+++ packages/cowsay/build.sh
@@ -7,8 +7,8 @@ TERMUX_PKG_SRCURL=http://http.debian.net/debian/pool/main/c/cowsay/cowsay_3.03+d
 TERMUX_PKG_SHA256=10bae895d9afb2d720d2211db58f396352b00fe1386c369ca3608cbf6497b839
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR
-	sh install.sh
+	DESTDIR=$TERMUX_PKG_MASSAGEDIR sh install.sh
 }
