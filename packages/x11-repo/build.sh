bc77499ba763339b7326522cb7daab65fb034536
Henrik Grimler <henrik@grimler.se>
2021-11-15 20:25:54 +0100

    x11-repo: install to TERMUX_PKG_MASSAGEDIR

--- packages/x11-repo/build.sh
+++ packages/x11-repo/build.sh
@@ -9,13 +9,13 @@ TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/apt/sources.list.d
 	{
 		echo "# The x11 termux repository, with cloudflare cache"
 		echo "deb https://packages-cf.termux.dev/apt/termux-x11/ x11 main"
 		echo "# The x11 termux repository, without cloudflare cache"
 		echo "# deb https://packages.termux.dev/apt/termux-x11/ x11 main"
-	} > $TERMUX_PREFIX/etc/apt/sources.list.d/x11.list
+	} > $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/apt/sources.list.d/x11.list
 }
 
 termux_step_create_debscripts() {
