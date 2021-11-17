7c476cfd40383c803238927ccb83b149a042b420
Henrik Grimler <henrik@grimler.se>
2021-11-17 21:40:24 +0100

    dart: install to TERMUX_PKG_MASSAGEDIR

--- packages/dart/build.sh
+++ packages/dart/build.sh
@@ -49,32 +49,34 @@ termux_step_make_install() {
 	if [ $TERMUX_ARCH = "arm" ]; then
 		python2 ./tools/build.py --no-goma --mode release --arch=arm --os=android create_sdk
 		chmod +x ./out/ReleaseAndroidARM/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidARM/dart-sdk ${TERMUX_PREFIX}/lib
+		cp -r ./out/ReleaseAndroidARM/dart-sdk $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/lib
 	elif [ $TERMUX_ARCH = "i686" ]; then
 		python2 ./tools/build.py --no-goma --mode release --arch=ia32 --os=android create_sdk
 		chmod +x ./out/ReleaseAndroidIA32/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidIA32/dart-sdk ${TERMUX_PREFIX}/lib
+		cp -r ./out/ReleaseAndroidIA32/dart-sdk $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/lib
 	elif [ $TERMUX_ARCH = "aarch64" ]; then
 		python2 ./tools/build.py --no-goma --mode release --arch=arm64c --os=android create_sdk
 		chmod +x ./out/ReleaseAndroidARM64C/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidARM64C/dart-sdk ${TERMUX_PREFIX}/lib
+		cp -r ./out/ReleaseAndroidARM64C/dart-sdk $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/lib
 	elif [ $TERMUX_ARCH = "x86_64" ]; then
 		python2 ./tools/build.py --no-goma --mode release --arch=x64c --os=android create_sdk
 		chmod +x ./out/ReleaseAndroidX64C/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidX64C/dart-sdk ${TERMUX_PREFIX}/lib
+		cp -r ./out/ReleaseAndroidX64C/dart-sdk $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/lib
 	else
 		termux_error_exit "Unsupported arch '$TERMUX_ARCH'"
 	fi
 
-	for file in ${TERMUX_PREFIX}/lib/dart-sdk/bin/*; do
+	for file in $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/lib/dart-sdk/bin/*; do
 		if [[ -f "$file" ]]; then
-			echo -e "#!${TERMUX_PREFIX}/bin/sh\nexec $file  \"\$@\"" > ${TERMUX_PREFIX}/bin/$(basename $file)
-			chmod +x ${TERMUX_PREFIX}/bin/$(basename $file)
+			echo -e "#!${TERMUX_PREFIX}/bin/sh\nexec $file  \"\$@\"" \
+			        > $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/bin/$(basename $file)
+			chmod +x $TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}/bin/$(basename $file)
 		fi
 	done
 }
 
 termux_step_post_make_install() {
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/profile.d
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/dart-pub-bin.sh \
-		$TERMUX_PREFIX/etc/profile.d/dart-pub-bin.sh
+		$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/etc/profile.d/dart-pub-bin.sh
 }
