fa46323d841b098bbca1af959b649c48ada694a0
Henrik Grimler <henrik@grimler.se>
2021-11-17 20:56:10 +0100

    seccure: install to TERMUX_PKG_MASSAGEDIR
    And avoid overriding termux_step_make.

--- packages/seccure/build.sh
+++ packages/seccure/build.sh
@@ -7,19 +7,18 @@ TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=http://point-at-infinity.org/seccure/seccure-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6566ce4afea095f83690b93078b910ca5b57b581ebc60e722f6e3fe8e098965b
 TERMUX_PKG_DEPENDS="libgcrypt"
+TERMUX_PKG_EXTRA_MAKE_ARGS="seccure-key"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make() {
-	make seccure-key
-}
-
 termux_step_make_install() {
-	install -Dm700 seccure-key "$TERMUX_PREFIX"/bin/
-	install -Dm600 seccure.1 "$TERMUX_PREFIX"/share/man/man1/
+	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/share/man/man1/
+	install -Dm700 seccure-key "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/bin/
+	install -Dm600 seccure.1 \
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/share/man/man1/
 
-	for i in encrypt decrypt sign verify signcrypt veridec dh; do
-		ln -sfr "$TERMUX_PREFIX"/bin/seccure-key "$TERMUX_PREFIX"/bin/seccure-${i}
-		ln -sfr "$TERMUX_PREFIX"/share/man/man1/seccure.1 "$TERMUX_PREFIX"/share/man/man1/seccure-${i}.1
+	cd "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"
+	for prog in encrypt decrypt sign verify signcrypt veridec dh; do
+		ln -sfr seccure-key bin/seccure-${prog}
+		ln -sfr seccure.1 share/man/man1/seccure-${prog}.1
 	done
-	unset i
 }
