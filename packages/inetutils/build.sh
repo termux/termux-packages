239ae0c5fc9505c064bb017cb2b46c568fb2cfe2
Henrik Grimler <henrik@grimler.se>
2021-11-18 08:13:41 +0100

    inetutils: fix build with ndk-r23

--- packages/inetutils/build.sh
+++ packages/inetutils/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Collection of common network programs"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.9.4
-TERMUX_PKG_REVISION=11
+TERMUX_PKG_REVISION=12
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/inetutils/inetutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=849d96f136effdef69548a940e3e0ec0624fc0c81265296987986a0dd36ded37
 TERMUX_PKG_DEPENDS="readline"
@@ -26,6 +26,6 @@ ac_cv_lib_crypt_crypt=no
 "
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -DLOGIN_PROCESS=6 -DDEAD_PROCESS=8 -DLOG_NFACILITIES=24"
+	CPPFLAGS+=" -DLOGIN_PROCESS=6 -DDEAD_PROCESS=8 -DLOG_NFACILITIES=24 -fcommon"
 	touch -d "next hour" ./man/whois.1
 }
