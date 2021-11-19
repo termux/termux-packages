8d2bb7520abaf24a0d0feba11618e2ad2e9fcbcd
Henrik Grimler <henrik@grimler.se>
2021-11-19 17:24:40 +0100

    torsocks: blacklist for aarch64
    It just hangs when run on aarch64 (behaviour could possibly depend on
    kernel version). Lets blacklist the package for aarch64 and promote
    use of proxychains-ng instead.
    
    Fixes https://github.com/termux/termux-packages/issues/2711.

--- packages/torsocks/build.sh
+++ packages/torsocks/build.sh
@@ -3,9 +3,11 @@ TERMUX_PKG_DESCRIPTION="Wrapper to safely torify applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dgoulet/torsocks/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=817c143e8a9d217f41a223a85139c6ca28e1b99556c547fcdb4c72dbc170b6c9
 TERMUX_PKG_DEPENDS="tor"
+TERMUX_PKG_BLACKLISTED_ARCHES="aarch64"
 
 termux_step_pre_configure() {
 	./autogen.sh
