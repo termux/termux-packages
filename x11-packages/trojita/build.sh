205f1379087461da199e25cebb313389c7dec22a
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-11-20 03:46:01 -0500

    new package: trojita (#498)

---
+++ x11-packages/trojita/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/KDE/trojita
+TERMUX_PKG_DESCRIPTION="Fast, lightweight and standard-compliant IMAP e-mail client"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
+TERMUX_PKG_VERSION=0.7
+TERMUX_PKG_SRCURL=https://github.com/KDE/trojita/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7cf5e2202343508904e553db239b02754a98aebf6d2a2d90aa2a089724029a20
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, librsvg, libpng, qt5-qtxmlpatterns, qt5-qtwebsockets, qt5-qtwebkit, qt5-qtwebchannel"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+# Cmake cannot find Qt5WebKitWidgets on i686 
+TERMUX_PKG_BLACKLISTED_ARCHES="i686"
