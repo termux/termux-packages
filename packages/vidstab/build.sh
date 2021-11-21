f1b529f1d0bf7a7ab7b0d757cc6450099478f15a
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-11-21 18:08:11 +0900

    new package: vidstab

---
+++ packages/vidstab/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://public.hronopik.de/vid.stab/
+TERMUX_PKG_DESCRIPTION="Transcode video stabilization plugin"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SRCURL=https://github.com/georgmartius/vid.stab/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=14d2a053e56edad4f397be0cb3ef8eb1ec3150404ce99a426c4eb641861dc0bb
