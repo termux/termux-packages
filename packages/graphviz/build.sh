062e1d5193cbc1e5a57a0d5182d08eb4b283c8af
Henrik Grimler <henrik@grimler.se>
2021-11-21 18:32:41 +0100

    graphviz: update to 2.49.3
    Running some programs like gvpack fails on device with
    
    CANNOT LINK EXECUTABLE "gvpack": cannot locate symbol "__extendsftf2" referenced by "/data/data/com.termux/files/usr/lib/graphviz/libgvplugin_neato_layout.so"...
    
    which is the same error as we saw for tesseract [1] with
    configure&make. There seem to be an issue somewhere that might be
    affecting many configure&make packages.
    
    [1] https://github.com/termux/termux-packages/issues/7953

--- packages/graphviz/build.sh
+++ packages/graphviz/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.graphviz.org/
 TERMUX_PKG_DESCRIPTION="Rich set of graph drawing tools"
 TERMUX_PKG_LICENSE="EPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.44.1
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://www2.graphviz.org/Packages/stable/portable_source/graphviz-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=8e1b34763254935243ccdb83c6ce108f531876d7a5dfd443f255e6418b8ea313
+TERMUX_PKG_VERSION=2.49.3
+TERMUX_PKG_SRCURL=https://gitlab.com/graphviz/graphviz/-/archive/${TERMUX_PKG_VERSION}/graphviz-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=5801664769ab88c2fb8ccb6ab0957cceabe6d4632b193041440e97790f53a9df
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, libcairo, pango, libexpat, libltdl, librsvg, libgd, zlib"
 TERMUX_PKG_BREAKS="graphviz-dev"
 TERMUX_PKG_REPLACES="graphviz-dev"
@@ -31,6 +30,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-x=no
 "
 TERMUX_PKG_RM_AFTER_INSTALL="bin/*-config share/man/man1/*-config.1"
+
+# In file included from /home/builder/.termux-build/graphviz/src/lib/expr/expr.h:28:
+# /home/builder/.termux-build/graphviz/src/lib/expr/exparse.h:4:10: fatal error: 'expr/y.tab.h' file not found
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	./autogen.sh NOCONFIG
