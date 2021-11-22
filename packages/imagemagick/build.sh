844477f98bf07c4477d98a9f3b05e126d190f83e
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-11-22 14:52:37 +0700

    imagemagick: enable Magick++ (#8034)
    * imagemagick: enable Magick++

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.1.0.13
+TERMUX_PKG_VERSION=7.1.0.15
 TERMUX_PKG_SRCURL=https://download.imagemagick.org/ImageMagick/download/ImageMagick-$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.lz
-TERMUX_PKG_SHA256=22da11ef55c42a7292379bf88f4713c6c931b1302d2c4be83a75eb0186ad458d
+TERMUX_PKG_SHA256=6c56fc02906e642603c5f7070ca0b44d3314a3d37904d4bb6ef22aad4577720c
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libheif, libjpeg-turbo, libjxl, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib, zstd"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-x
 --without-gvc
---with-magick-plus-plus=no
+--with-magick-plus-plus=yes
 --with-bzlib=yes
 --with-xml=yes
 --with-rsvg=yes
