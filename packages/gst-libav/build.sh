df4060a04a4b32097c25042be489228ac07ad274
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-11-24 17:38:55 -0500

    new package: gst-libav (#8044)

---
+++ packages/gst-libav/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/modules/gst-libav.html
+TERMUX_PKG_DESCRIPTION="GStreamer Libav plug-in contains one plugin with a set of elements using the Libav library code"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
+TERMUX_PKG_VERSION=1.18.5
+TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=822e008a910e9dd13aedbdd8dc63fedef4040c0ee2e927bab3112e9de693a548
+TERMUX_PKG_DEPENDS="python, ffmpeg, gstreamer, gst-plugins-base"
