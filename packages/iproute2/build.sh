e34ceed1e312ba7471056281ccd3d5dc33548817
Tee KOBAYASHI <xtkoba@gmail.com>
2021-11-24 20:51:25 +0900

    Fix `PREFIX` and other paths

--- packages/iproute2/build.sh
+++ packages/iproute2/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-$
 TERMUX_PKG_SHA256=38e3e4a5f9a7f5575c015027a10df097c149111eeb739993128e5b2b35b291ff
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="DESTDIR=${TERMUX_PREFIX} PREFIX="
 
 termux_step_pre_configure() {
 	CFLAGS+=" -fPIC"
