40f17c722decbe274d2776cd2a3308c944857b5b
Yonle <yonle@protonmail.com>
2021-11-25 09:51:01 +0000

    Change sha256sum

--- packages/gst-libav/build.sh
+++ packages/gst-libav/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=1.19.3
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=822e008a910e9dd13aedbdd8dc63fedef4040c0ee2e927bab3112e9de693a548
+TERMUX_PKG_SHA256=6ad23508b337e91a4053321d7c49b19471b9859468d544c63148ac636472c98a
 TERMUX_PKG_DEPENDS="python, ffmpeg, gstreamer, gst-plugins-base"
