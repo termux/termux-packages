8c499088317b74b339b0a3a33ec618c3b5ffbf51
Tee KOBAYASHI <xtkoba@gmail.com>
2021-11-26 13:04:09 +0900

    new package: unbound

---
+++ packages/unbound/build.sh
@@ -0,0 +1,30 @@
+TERMUX_PKG_HOMEPAGE=https://unbound.net/
+TERMUX_PKG_DESCRIPTION="A validating, recursive, caching DNS resolver"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.13.2
+TERMUX_PKG_SRCURL=https://nlnetlabs.nl/downloads/unbound/unbound-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0a13b547f3b92a026b5ebd0423f54c991e5718037fd9f72445817f6a040e1a83
+TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_func_chown=no
+ac_cv_func_chroot=no
+ac_cv_func_getpwnam=no
+--enable-event-api
+--enable-ipsecmod
+--enable-linux-ip-local-port-range
+--enable-tfo-server
+--with-libevent=$TERMUX_PREFIX
+--with-libexpat=$TERMUX_PREFIX
+--without-libhiredis
+--without-libmnl
+--with-libnghttp2=$TERMUX_PREFIX
+--with-ssl=$TERMUX_PREFIX
+--with-pidfile=$TERMUX_PREFIX/var/run/unbound.pid
+--with-username=
+"
+
+termux_step_post_massage() {
+	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/var/run"
+}
