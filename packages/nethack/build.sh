9a0afd7e9b7f3dd8309beb93b80538adeda8085e
Henrik Grimler <henrik@grimler.se>
2021-11-27 12:12:04 +0100

    nethack: build host-tools in termux_step_host_build
    And simplify the related Makefile patches

--- packages/nethack/build.sh
+++ packages/nethack/build.sh
@@ -3,40 +3,47 @@ TERMUX_PKG_DESCRIPTION="Dungeon crawl game"
 TERMUX_PKG_LICENSE="Nethack"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=3.6.6
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=http://www.nethack.org/download/${TERMUX_PKG_VERSION}/nethack-${TERMUX_PKG_VERSION//./}-src.tgz
 TERMUX_PKG_SHA256=cfde0c3ab6dd7c22ae82e1e5a59ab80152304eb23fb06e3129439271e5643ed2
 TERMUX_PKG_DEPENDS="gzip, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
 
-termux_step_make() {
-	cd $TERMUX_PKG_SRCDIR/sys/unix
+termux_step_host_build() {
+	cp -r $TERMUX_PKG_SRCDIR/* .
+	pushd sys/unix
 	sh setup.sh hints/linux
-	CFLAGS="$CPPFLAGS $CFLAGS"
-	export LFLAGS="$LDFLAGS"
-	cd $TERMUX_PKG_SRCDIR
-	cd util
+	popd && cd util
 	if [ $TERMUX_ARCH_BITS = 32 ]; then
 		HOST_CC="gcc -m32"
 	else
 		HOST_CC="gcc"
 	fi
 	CFLAGS="" CC="$HOST_CC" LD="ld" make makedefs
-	cp makedefs makedefs.host
 	CFLAGS="" CC="$HOST_CC" LD="ld" make lev_comp
-	cp lev_comp lev_comp.host
 	CFLAGS="" CC="$HOST_CC" LD="ld" make dgn_comp dlb recover
-	cp dgn_comp dgn_comp.host
-	cp dlb dlb.host
-	cd ../
-	make clean
-	make WINTTYLIB="$LDFLAGS -lcurses"  -j $TERMUX_MAKE_PROCESSES
+}
+
+termux_step_pre_configure() {
+	WINTTYLIB="$LDFLAGS -lcurses"
+	export LFLAGS="$LDFLAGS"
+	export CFLAGS="$CPPFLAGS $CFLAGS"
+	cd sys/unix
+	sh setup.sh hints/linux
+}
+
+termux_step_post_configure() {
+	# cp hostbuilt tools from hostbuild dir
+	cp $TERMUX_PKG_HOSTBUILD_DIR/util/{makedefs,lev_comp,dgn_comp,dlb} \
+		util/
+	touch -d "next hour" util/*
 }
 
 termux_step_post_make_install() {
 	cd doc
 	mkdir -p $TERMUX_PREFIX/share/man/man6
-	cp nethack.6 $TERMUX_PREFIX/share/man/man6/
+	install -m600 nethack.6 $TERMUX_PREFIX/share/man/man6/
 	ln -sf $TERMUX_PREFIX/games/nethack $TERMUX_PREFIX/bin/
 }
 
