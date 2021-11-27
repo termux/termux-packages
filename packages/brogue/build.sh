2b3f8572b67f4ae0beedce2afa93b0bead6924ec
Henrik Grimler <henrik@grimler.se>
2021-11-27 08:11:26 +0100

    brogue: fix build with ndk r23

--- packages/brogue/build.sh
+++ packages/brogue/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Roguelike dungeon crawling game"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.7.5
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/termux/distfiles/releases/download/2021.01.04/brogue-${TERMUX_PKG_VERSION}-linux-amd64.tbz2
 TERMUX_PKG_SHA256=a74ff18139564c597d047cfb167f74ab1963dd8608b6fb2e034e7635d6170444
 TERMUX_PKG_DEPENDS="ncurses"
@@ -11,9 +11,10 @@ TERMUX_PKG_EXTRA_MAKE_ARGS="curses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
+        CFLAGS+=" -fcommon"
 	CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS"
 }
 
 termux_step_make_install () {
-	cp bin/brogue $TERMUX_PREFIX/bin
+	install -m700 bin/brogue $TERMUX_PREFIX/bin
 }
