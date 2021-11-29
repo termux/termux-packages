0be130b1447da5693f5a73076f6e0286c52edc48
Henrik Grimler <henrik@grimler.se>
2021-11-29 16:00:25 +0100

    libdaemon: bump, package is missing on repo
    Reported on irc by Spads.

--- packages/libdaemon/build.sh
+++ packages/libdaemon/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A lightweight C library that eases the writing of UNIX d
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.14
+TERMUX_PKG_REVISON=1
 TERMUX_PKG_SRCURL=https://0pointer.de/lennart/projects/libdaemon/libdaemon-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fd23eb5f6f986dcc7e708307355ba3289abe03cc381fc47a80bca4a50aa6b834
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_setpgrp_void=yes"
