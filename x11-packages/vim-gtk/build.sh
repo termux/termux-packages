fa6de4e814d070c0f922c1c2a1da5b4733a396f3
Lucinda May Phipps <landfillbaby69@gmail.com>
2021-11-30 07:36:16 +0000

    vim-gtk: update to 8.2.3700

--- x11-packages/vim-gtk/build.sh
+++ x11-packages/vim-gtk/build.sh
@@ -4,10 +4,9 @@ TERMUX_PKG_LICENSE="VIM License"
 TERMUX_PKG_MAINTAINER="@termux"
 
 # vim should only be updated every 50 releases on multiples of 50.
-TERMUX_PKG_VERSION=8.2.3500
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=8.2.3700
 TERMUX_PKG_SRCURL=https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7f280d08800d99d270bb0f722db89b04798600a0214d0d74d410ec9d57c5d2aa
+TERMUX_PKG_SHA256=c6ec0fbfc1493c4538050cc154c6285441ad4a5ebc424ab6cad33df0eac89ba9
 
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libcanberra, libice, libiconv, liblua53, libsm, libx11, libxpm, libxt, ncurses, pango, python"
 TERMUX_PKG_CONFLICTS="vim, vim-python, vim-runtime"
