055957914777ff8e5cb59e1eda54fbabf6ab21b9
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-02 18:40:38 +0900

    mailutils: Bump revision

--- packages/mailutils/build.sh
+++ packages/mailutils/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Mailutils is a swiss army knife of electronic mail handl
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@suhan-paradkar"
 TERMUX_PKG_VERSION=3.13
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/mailutils/mailutils-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=d920971dcb49878a009911774fd6404f13d27bd101e2d59b664a28659a4094c7
 TERMUX_PKG_BUILD_DEPENDS="libandroid-glob, libcrypt"
