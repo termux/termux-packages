3d7c5aa219de440ac818a91a239d68ea83b20b90
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-03 18:46:30 +0900

    liblua5{2,3,4}: Use update-alternatives

--- packages/liblua53/lua53.subpackage.sh
+++ packages/liblua53/lua53.subpackage.sh
@@ -3,3 +3,27 @@ TERMUX_SUBPKG_DESCRIPTION="Simple, extensible, embeddable programming language"
 TERMUX_SUBPKG_DEPENDS="readline"
 TERMUX_SUBPKG_BREAKS="lua (<< 5.3.5-6)"
 TERMUX_SUBPKG_REPLACES="lua (<< 5.3.5-6)"
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" = "configure" ] || [ "\$1" = "abort-upgrade" ]; then
+		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
+			update-alternatives --install \
+				$TERMUX_PREFIX/bin/lua lua $TERMUX_PREFIX/bin/lua5.3 130
+			update-alternatives --install \
+				$TERMUX_PREFIX/bin/luac luac $TERMUX_PREFIX/bin/luac5.3 130
+		fi
+	fi
+	EOF
+
+	cat <<- EOF > ./prerm
+	#!$TERMUX_PREFIX/bin/sh
+	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" != "upgrade" ]; then
+		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
+			update-alternatives --remove lua $TERMUX_PREFIX/bin/lua
+			update-alternatives --remove luac $TERMUX_PREFIX/bin/luac
+		fi
+	fi
+	EOF
+}
