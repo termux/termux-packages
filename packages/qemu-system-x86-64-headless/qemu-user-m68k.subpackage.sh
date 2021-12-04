4574c95beda3a99561fd127201a0648fc75c68ec
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-04 23:49:22 +0900

    qemu: Include m68k targets

---
+++ packages/qemu-system-x86-64-headless/qemu-user-m68k.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DESCRIPTION="QEMU Linux user mode emulator"
+TERMUX_SUBPKG_DEPENDS="glib, libandroid-shmem, libc++, libgnutls, libpixman"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
+TERMUX_SUBPKG_INCLUDE="bin/qemu-m68k"
