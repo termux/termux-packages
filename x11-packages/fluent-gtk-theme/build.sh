233d55edb3475eab7a6a908e35f8858aaf687c4b
Yisus7u7 <jesuspixel5@gmail.com>
2021-12-04 04:32:44 -0500

    fluent-gtk-theme: update to 2021.11.23 (#513)

--- x11-packages/fluent-gtk-theme/build.sh
+++ x11-packages/fluent-gtk-theme/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vinceliuice/Fluent-gtk-theme
 TERMUX_PKG_DESCRIPTION="Fluent is a Fluent design theme for GNOME/GTK based desktop environments"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7 <jesuspixel5@gmail.com>"
-TERMUX_PKG_VERSION=2021.10.03
+TERMUX_PKG_VERSION=2021.11.23
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/vinceliuice/Fluent-gtk-theme/archive/refs/tags/${TERMUX_PKG_VERSION//./-}.tar.gz
-TERMUX_PKG_SHA256=7f19d16c35c22eca007b925925ee26f479030672f792619f793c2ecdf57bb98f
-TERMUX_PKG_DEPENDS="gtk3"
+TERMUX_PKG_SHA256=c416900ae9f3e036dde53279e25c00c6f055a0f54cb7f680bf9f6caeefd2ce92
+TERMUX_PKG_DEPENDS="gtk3, gtk2-engines-murrine"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
