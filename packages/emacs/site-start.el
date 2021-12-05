697977c0b13824e90e8c622c5f54d83e53d07c6b
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-05 17:01:56 +0900

    emacs: Assume UTF-8 keyboard input for emacsclient

--- packages/emacs/site-start.el
+++ packages/emacs/site-start.el
@@ -1,4 +1,9 @@
-; Enable terminal mouse events:
+;; Enable terminal mouse events:
 (xterm-mouse-mode 1)
 (global-set-key [mouse-4] 'scroll-down-line)
 (global-set-key [mouse-5] 'scroll-up-line)
+
+;; Assume UTF-8 keyboard input for emacsclient:
+(add-hook 'server-visit-hook
+          '(lambda ()
+             (set-keyboard-coding-system 'utf-8-unix)))
