04bc978525a7baf63f254de7dd561f90060de958
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-05 11:42:33 +0200

    cairo-dock-core: fix messed up library installation path

--- x11-packages/cairo-dock-core/build.sh
+++ x11-packages/cairo-dock-core/build.sh
@@ -4,18 +4,11 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=3.4.1
 _COMMIT=6c569e67a2a366e7634224a0133ede51755629cb
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Cairo-Dock/cairo-dock-core/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=e59e99147ce9c901b46d4b56b88bd53aeda34292b86fd9fbf2c55d158153f2ec
 TERMUX_PKG_DEPENDS="atk, dbus-glib, glib, gtk3, pango, libcairo, libx11, xsltproc, libcurl, gdk-pixbuf, harfbuzz, mesa, glu, librsvg, libxcomposite, libwayland, libwayland-protocols, libxinerama, libxrandr, libxrender, libxtst, libxml2"
 TERMUX_PKG_BUILD_DEPENDS="valac"
-
-if [ $TERMUX_ARCH == "arm" ]; then
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=\"Release\" -DLIB_SUFFIX=32"
-elif [ $TERMUX_ARCH == "i686" ]; then
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=\"Release\" -DLIB_SUFFIX=32"
-else
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=\"Release\" -DFORCE_LIB64=yes"
-fi
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DFORCE_NOT_LIB64=yes"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
 
