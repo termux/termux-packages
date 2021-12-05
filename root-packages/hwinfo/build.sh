87e73428376274e26c36124aafa5ddec7e5a83c7
Henrik Grimler <henrik@grimler.se>
2021-12-05 15:10:59 +0100

    hwinfo: update to 21.78
    And avoiud overriding termux_step_make and termux_step_make_install.

--- root-packages/hwinfo/build.sh
+++ root-packages/hwinfo/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/openSUSE/hwinfo
 TERMUX_PKG_DESCRIPTION="Hardware detection tool from openSUSE"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.74
+TERMUX_PKG_VERSION=21.78
 TERMUX_PKG_SRCURL=https://github.com/openSUSE/hwinfo/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e826865af657c4966c37c8e915499dc148ff6df4879d3d86f71885defee8335c
+TERMUX_PKG_SHA256=8693d4e8bc134139fc62e659ca608e138dc089a0cb1194dc93ee278fdef10bba
 TERMUX_PKG_DEPENDS="libandroid-shmem, libuuid, libx86emu"
 TERMUX_PKG_BREAKS="hwinfo-dev"
 TERMUX_PKG_REPLACES="hwinfo-dev"
@@ -13,14 +13,5 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_configure() {
 	echo 'touch changelog' > git2log
 	LDFLAGS+=" -landroid-shmem"
-}
-
-termux_step_make() {
-	make -j1 HWINFO_VERSION="$TERMUX_PKG_VERSION" \
-		LIBDIR="$TERMUX_PREFIX/lib"
-}
-
-termux_step_make_install() {
-	make HWINFO_VERSION="$TERMUX_PKG_VERSION" \
-		DESTDIR="$TERMUX_PREFIX" install
+        export HWINFO_VERSION="$TERMUX_PKG_VERSION"
 }
