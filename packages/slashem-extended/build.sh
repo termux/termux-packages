ed9e88a7fe1adaff375a2e3062b42e763594011a
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-07 21:26:14 +0900

    slashem-extended: Fix postinst

--- packages/slashem-extended/build.sh
+++ packages/slashem-extended/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A variant of SLASH'EM (a variant of NetHack)"
 TERMUX_PKG_LICENSE="Nethack"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.7.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/SLASHEM-Extended/SLASHEM-Extended/archive/refs/tags/slex-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=54d301bcb8d79d92030a30195f091e694f843d4656061dbce85730fc12023dee
 TERMUX_PKG_DEPENDS="ncurses"
@@ -32,11 +33,11 @@ termux_step_make_install() {
 
 termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo "mkdir -p \$TERMUX_PREFIX/var/games/slex" >> postinst
-	echo "touch \$TERMUX_PREFIX/var/games/slex/perm" >> postinst
-	echo "touch \$TERMUX_PREFIX/var/games/slex/record" >> postinst
-	echo "mkdir -p \$TERMUX_PREFIX/var/games/slex/save" >> postinst
-	echo "mkdir -p \$TERMUX_PREFIX/var/games/slex/unshare" >> postinst
+	echo "mkdir -p $TERMUX_PREFIX/var/games/slex" >> postinst
+	echo "touch $TERMUX_PREFIX/var/games/slex/perm" >> postinst
+	echo "touch $TERMUX_PREFIX/var/games/slex/record" >> postinst
+	echo "mkdir -p $TERMUX_PREFIX/var/games/slex/save" >> postinst
+	echo "mkdir -p $TERMUX_PREFIX/var/games/slex/unshare" >> postinst
 	echo "exit 0" >> postinst
 	chmod 0755 postinst
 }
