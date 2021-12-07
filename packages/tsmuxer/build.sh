5d75686611e447b68e15c10ffe99bc9d68432338
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-07 21:15:24 +0900

    new package: tsmuxer

---
+++ packages/tsmuxer/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/justdan96/tsMuxer
+TERMUX_PKG_DESCRIPTION="A transport stream muxer for remuxing/muxing elementary streams"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+_VERSION_REAL=nightly-2021-12-05-02-09-30
+TERMUX_PKG_VERSION=$(cut -d- -f2,3,4 <<< "$_VERSION_REAL" | tr '-' '.')
+TERMUX_PKG_SRCURL=https://github.com/justdan96/tsMuxer/archive/refs/tags/${_VERSION_REAL}.tar.gz
+TERMUX_PKG_SHA256=676f7741fb897ec53a975fcb6a0c4d5466d680a12e82b54b39cbf48d91bb14fb
+TERMUX_PKG_DEPENDS="freetype, libc++, zlib"
