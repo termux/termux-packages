d9b4de04fc1632224ab72052984185a437d346fb
tareksander <57038324+tareksander@users.noreply.github.com>
2021-12-08 11:42:42 +0100

    Add termux-am-2
    Client to use the new am implementation from this PR: https://github.com/termux/termux-app/pull/2458

---
+++ packages/termux-am-2/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tareksander/termux-am-2
+
+TERMUX_PKG_DESCRIPTION="A faster version of am with less features that only works while Termux is running."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+
+TERMUX_PKG_VERSION=1.0
+
+TERMUX_PKG_SRCURL=https://github.com/tareksander/termux-am-2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+
+TERMUX_PKG_SHA256=f5e85f2cf9f0d052626ff078215ac30728c72a83648015202b99c878c2fe19bf
