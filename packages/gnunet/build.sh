affe5b26eb4c28383d89dfd5b0c7dc498bf8e718
Yaksh Bariya <yakshbari4@gmail.com>
2021-12-08 19:00:20 +0530

    gnunet: update to 0.15.3

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -3,8 +3,7 @@ TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_SRCURL=https://git.gnunet.org/git/gnunet.git
-TERMUX_PKG_VERSION=0.14.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.15.3
 TERMUX_PKG_DEPENDS="gnurl, libgcrypt, libgmp, libidn, libjansson, libltdl, libmicrohttpd, libsqlite, libunistring, libsodium"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_have_decl_struct_in6_ifreq=yes"
