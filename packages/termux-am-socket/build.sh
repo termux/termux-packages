aa6260916974ff458781ed130aa355f71c30451d
Henrik Grimler <henrik@grimler.se>
2021-12-09 15:35:38 +0100

    termux-am-socket: rename from termux-am-2, switch to termux repo
    And remove empty lines from build.sh.

---
+++ packages/termux-am-socket/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-am-socket
+TERMUX_PKG_DESCRIPTION="A faster version of am with less features that only works while Termux is running"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.01
+TERMUX_PKG_SRCURL=https://github.com/termux/termux-am-socket/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b50b02902c613df1888640975c8d637b911913a476128cf19adad697b5e44428
