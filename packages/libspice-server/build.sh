72dfe5266b2cdba96df678bbdceb8617b92122f9
Yaksh Bariya <yakshbari4@gmail.com>
2021-12-11 18:38:18 +0530

    revbump packages dependent on glib

--- packages/libspice-server/build.sh
+++ packages/libspice-server/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Implements the server side of the SPICE protocol"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.15.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.spice-space.org/download/releases/spice-server/spice-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=b320cf8f4bd2852750acb703c15b72856027e5a8554f8217dfbb3cc09deba0f5
 TERMUX_PKG_DEPENDS="glib, libjpeg-turbo, gstreamer, gst-plugins-base, libsasl, libspice-protocol, liborc, openssl, libopus, libpixman, liblz4, zlib, libiconv"
