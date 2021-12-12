2f1eff3662be5a4e502e837022b020787c196561
Yaksh Bariya <yakshbari4@gmail.com>
2021-12-12 10:17:07 +0530

    file-roller: bump revision

--- x11-packages/file-roller/build.sh
+++ x11-packages/file-roller/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="File Roller is an archive manager for the GNOME desktop 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=3.40.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/GNOME/file-roller/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=81e72121430ba6ea7396fe84d75b07252e550674addfb38bc6066665170941f3
 TERMUX_PKG_DEPENDS="bzip2, desktop-file-utils, cpio, glib, gtk3, gzip, libcairo, lzip, lzop, p7zip, tar, unrar, unzip, xz-utils, zip, bsdtar, zlib, json-glib, libnotify, libarchive"
