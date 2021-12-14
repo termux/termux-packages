69827b35312f6957054ff39a37160eb298118d2d
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-14 20:51:06 +0900

    new package: tsmuxergui

---
+++ x11-packages/tsmuxergui/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/justdan96/tsMuxer
+TERMUX_PKG_DESCRIPTION="A transport stream muxer for remuxing/muxing elementary streams"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+_VERSION_REAL=nightly-2021-12-05-02-09-30
+TERMUX_PKG_VERSION=$(cut -d- -f2,3,4 <<< "$_VERSION_REAL" | tr '-' '.')
+TERMUX_PKG_SRCURL=https://github.com/justdan96/tsMuxer/archive/refs/tags/${_VERSION_REAL}.tar.gz
+TERMUX_PKG_SHA256=676f7741fb897ec53a975fcb6a0c4d5466d680a12e82b54b39cbf48d91bb14fb
+TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtdeclarative, qt5-qtmultimedia, qt5-qttools"
+TERMUX_PKG_BUILD_DEPENDS="freetype, qt5-qtbase-cross-tools, qt5-qttools-cross-tools, zlib"
+TERMUX_PKG_RM_AFTER_INSTALL="bin/tsmuxer"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_DATADIR=share -DTSMUXER_GUI=ON"
