64e1857b4186847be9dd9e93ae6d48aff1614154
Yaksh Bariya <yakshbari4@gmail.com>
2021-12-15 19:09:44 +0530

    libzim: update to 7.0.0

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -2,9 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://openzim.org
 TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the ZIM file format."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.3.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=7.0.0
 TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=648fb5af65dd28a6b221b9294217939bf5abf929d706e7323ebb4196849a923c
+TERMUX_PKG_SHA256=91702b383d33f4aff0216596cc01b46dc3c6f8d60c069ffd14affa8ff6459f5d
 TERMUX_PKG_DEPENDS="zstd, libuuid, zlib, libicu, liblzma, libxapian, googletest"
 
