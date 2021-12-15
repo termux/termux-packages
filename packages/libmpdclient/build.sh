ec53507d580793d50ec55f4abc5090bd9d55507b
Yaksh Bariya <yakshbari4@gmail.com>
2021-12-15 19:39:40 +0530

    libmpdclient: update to 2.20

--- packages/libmpdclient/build.sh
+++ packages/libmpdclient/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.musicpd.org/libs/libmpdclient/
 TERMUX_PKG_DESCRIPTION="Asynchronous API library for interfacing MPD in the C, C++ & Objective C languages"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.19
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.20
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/libmpdclient/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=12b566b75c3b6789ff8fc94698497d1f3fbaf0cbf9fa6c3a1e3906ef0d2bcbbb
+TERMUX_PKG_SHA256=3c5a671e7bc5736c677ef75017eaf008e8838ac03605953410e8d4de6b363b73
 TERMUX_PKG_BREAKS="libmpdclient-dev"
 TERMUX_PKG_REPLACES="libmpdclient-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -D default_socket==${TERMUX_PREFIX}/var/run/mpd/socket"
