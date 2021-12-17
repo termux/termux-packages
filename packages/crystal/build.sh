dc2d10007eb9d93d90cd53af6d95df7c3ddc4657
Leap of Azzam <leapofazzam@gmail.com>
2021-12-17 15:58:02 +0700

    fuck it
    Signed-off-by: Leap of Azzam <leapofazzam@gmail.com>

--- packages/crystal/build.sh
+++ packages/crystal/build.sh
@@ -8,22 +8,37 @@ TERMUX_PKG_SHA256=6d963a71ef5f6c73faa272a0f81b50e9ddbf814b1ec07e557ce5c95f84d607
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="llvm, libllvm, libgc"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-release=1
-stats=1
-threads=$TERMUX_MAKE_PROCESSES
-LLVM_CONFIG=$TERMUX_PREFIX/bin/llvm-config
-CRYSTAL_CONFIG_VERSION=1.2.2
-CRYSTAL_CONFIG_PATH=lib:$TERMUX_PREFIX/lib/crystal
-"
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	make PREFIX=$TERMUX_PKG_HOSTBUILD_DIR/crystal-host clean crystal install
+}
+
+termux_step_make() {
+	make deps CXX=$CXX
+	$TERMUX_PKG_HOSTBUILD_DIR/crystal-host/bin/crystal build --cross-compile \
+		--target aarch64-unknown-linux-musl src/compiler/crystal.cr \
+		-Dwithout_playground
+
+	$CC crystal.o -o .build/crystal -rdynamic src/llvm/ext/llvm_ext.o \
+		`$TERMUX_PKG_HOSTBUILD_DIR/bin/llvm-config --libs --system-libs --ldflags` \
+		-lstdc++ -lpcre -lm -lgc -lpthread -levent -lrt -ldl
+
+	[ -d "shards" ] || \
+		git clone "https://github.com/crystal-lang/shards.git" && \
+		git -C shards pull && \
+		git -C shards reset --hard ff94dd2ee46791e8e331f725fa1e4acdc13d5e9f
+
+	cd shards
+	make release=1 CRYSTAL=$TERMUX_PKG_HOSTBUILD_DIR/crystal-host/bin/crystal SHARDS=false
+}
 
 termux_step_make_install() {
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/.build/crystal "$TERMUX_PREFIX"/bin/crystal
+	install -Dm700 "$TERMUX_PKG_SRCDIR"/shards/bin/shards "$TERMUX_PREFIX"/bin/shards
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/man/crystal.1 "$TERMUX_PREFIX"/share/man/man1/crystal.1
-	install -Dm600 "$TERMUX_PKG_SRCDIR"/etc/completion.bash \
-		"$TERMUX_PREFIX"/share/bash-completion/completions/crystal
-	install -Dm600 "$TERMUX_PKG_SRCDIR"/etc/completion.zsh \
-		"$TERMUX_PREFIX"/share/zsh/site-functions/_crystal
+	install -Dm600 "$TERMUX_PKG_SRCDIR"/etc/completion.bash "$TERMUX_PREFIX"/share/bash-completion/completions/crystal
+	install -Dm600 "$TERMUX_PKG_SRCDIR"/etc/completion.zsh "$TERMUX_PREFIX"/share/zsh/site-functions/_crystal
 
 	mkdir -p "$TERMUX_PREFIX/lib/crystal"
 	cp -r "$TERMUX_PKG_SRCDIR"/src/* "$TERMUX_PREFIX/lib/crystal"
