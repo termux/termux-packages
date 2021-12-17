1c2ba2b3f6d7560c61059979fd79fc2347e58c2f
Yisus7u7 <jesuspixel5@gmail.com>
2021-12-16 22:56:57 -0500

    xfce4-session: depends on pango

--- x11-packages/xfce4-session/build.sh
+++ x11-packages/xfce4-session/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="A session manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.16.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/xfce4-session/${TERMUX_PKG_VERSION:0:4}/xfce4-session-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=22f273f212481d71e0b5618c62710cd85f69aea74f5ea5c0093f7918b07d17b7
-TERMUX_PKG_DEPENDS="gnupg, hicolor-icon-theme, libsm, libwnck, libxfce4ui, xfce4-settings, xfdesktop, xfwm4, xorg-iceauth, xorg-xrdb"
+TERMUX_PKG_DEPENDS="gnupg, hicolor-icon-theme, libsm, libwnck, libxfce4ui, xfce4-settings, xfdesktop, pango, xfwm4, xorg-iceauth, xorg-xrdb"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_ICEAUTH=${TERMUX_PREFIX}/bin/iceauth
