8b0aa2706abd47aaeccbe8c2b0f75b24826344ee
Butta <repo@butta.fastem.com>
2021-12-18 02:37:05 +0530

    llbuild: build latest version and don't auto-update

--- packages/llbuild/build.sh
+++ packages/llbuild/build.sh
@@ -5,5 +5,4 @@ TERMUX_PKG_MAINTAINER="@buttaface"
 TERMUX_PKG_VERSION=5.5.2
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-llbuild/archive/swift-${TERMUX_PKG_VERSION}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=6767df1c14d09c990e72c2e9ec9c61765610c1fe7801c92894afa36f9928d320
-TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libandroid-spawn, libsqlite"
