ada4dd167588be63cacafc2b06c47209cf59ff72
Yisus7u7 <jesuspixel5@gmail.com>
2021-12-17 07:50:28 -0500

    gdk-pixbuf: depends on zstd (#8264)

--- packages/gdk-pixbuf/build.sh
+++ packages/gdk-pixbuf/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Library for image loading and manipulation"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.42.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/archive/${TERMUX_PKG_VERSION}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c4f3a84a04bc7c5f4fbd97dce7976ab648c60628f72ad4c7b79edce2bbdb494d
-TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo"
+TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo, zstd"
 TERMUX_PKG_BREAKS="gdk-pixbuf-dev"
 TERMUX_PKG_REPLACES="gdk-pixbuf-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgio_sniffing=false"
