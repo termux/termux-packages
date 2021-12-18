c8032b1d34f97e68f0889c5eaab7bf8fdeb5420f
Efreak <efreak@users.noreply.github.com>
2021-12-18 13:55:28 -0800

    new package: peaclock

---
+++ packages/peaclock/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/octobanana/peaclock
+TERMUX_PKG_DESCRIPTION="A responsive and customizable clock, timer, and stopwatch for the terminal."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@efreak"
+TERMUX_PKG_VERSION=0.4.3
+TERMUX_PKG_SRCURL=https://github.com/octobanana/peaclock/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3de32486e45ddf6a62c591619b5373d23c8eaf885975441ae581ca7241c48012
+TERMUX_PKG_DEPENDS="libicu"
+TERMUX_PKG_AUTO_UPDATE=true
