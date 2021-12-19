741eda348e0b58c106874e753db4866832f1c4b2
Efreak <efreak@users.noreply.github.com>
2021-12-19 01:51:51 -0800

    Don't override make scripts, don't build in source

--- packages/peaclock/build.sh
+++ packages/peaclock/build.sh
@@ -7,13 +7,4 @@ TERMUX_PKG_SRCURL=https://github.com/octobanana/peaclock/archive/0.4.3.tar.gz
 TERMUX_PKG_SHA256=3de32486e45ddf6a62c591619b5373d23c8eaf885975441ae581ca7241c48012
 TERMUX_PKG_DEPENDS="libicu"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make() {
-	cmake . -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX="$TERMUX_PREFIX"
-	make
-}
-
-termux_step_make_install() {
-	make install PREFIX="$TERMUX_PREFIX"
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}"
