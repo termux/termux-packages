39b7505c46f80811ec36cfe428a10a1f9e90ebba
Efreak <efreak@users.noreply.github.com>
2021-12-19 01:25:11 -0800

    pipes.sh/build.sh
    use commit date for version, commit zip instead of master

--- packages/pipes.sh/build.sh
+++ packages/pipes.sh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pipeseroni/pipes.sh
 TERMUX_PKG_DESCRIPTION="Animated pipes terminal screensaver"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@Efreak"
-TERMUX_PKG_VERSION=1.3.0+ #if there's ever a new release, switch to it
-TERMUX_PKG_SRCURL=https://github.com/pipeseroni/pipes.sh/archive/refs/heads/master.zip # in the meantime, master has a bunch of changes
-TERMUX_PKG_SHA256=2a606f57e7029c4a1f0317045ed078aa37b463cc1871d36e3e0e6f4aa130a9f8
+TERMUX_PKG_VERSION=2018-02-24
+TERMUX_PKG_SRCURL=https://github.com/pipeseroni/pipes.sh/archive/581792d4e0ea51e15889ba14a85db1bc9727b83d.zip
+TERMUX_PKG_SHA256=79c7b29a687e24e0661e84cdc838520f6296470aa72f63d413cedd825d45fa1e
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS=bash
 
