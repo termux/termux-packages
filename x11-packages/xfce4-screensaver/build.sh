e918d0e2af1b2099161bbac2bb923d0d04036c10
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-20 16:11:17 +0200

    rebuild packages dependent on 'dbus'

--- x11-packages/xfce4-screensaver/build.sh
+++ x11-packages/xfce4-screensaver/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Xfce Screensaver is a screen saver and locker that aims 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=4.16.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-screensaver/4.16/xfce4-screensaver-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=6d4d143e3e62db679ce83ce7da97903390773ee0a8ceb05ff4c3dac36616268d
 TERMUX_PKG_DEPENDS="atk, gtk3, garcon, libwnck, libxext, libxklavier, libxss, termux-auth, libcairo, dbus-glib, pango, libxfce4ui, xfconf"
