adedcebbc4f38c6f5a77f0f8b11a2e4095016585
Yisus7u7 <jesuspixel5@gmail.com>
2021-12-20 09:08:31 -0500

    dbus: update to 1.13.18 (#8217)

--- packages/dbus/build.sh
+++ packages/dbus/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://dbus.freedesktop.org
 TERMUX_PKG_DESCRIPTION="Freedesktop.org message bus system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.12.20
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL="https://dbus.freedesktop.org/releases/dbus/dbus-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=f77620140ecb4cdc67f37fb444f8a6bea70b5b6461f12f1cbe2cec60fa7de5fe
+TERMUX_PKG_VERSION=1.13.18
+TERMUX_PKG_SRCURL="https://dbus.freedesktop.org/releases/dbus/dbus-$TERMUX_PKG_VERSION.tar.xz"
+TERMUX_PKG_SHA256=8078f5c25e34ab907ce06905d969dc8ef0ccbec367e1e1707c7ecf8460f4254e
 TERMUX_PKG_DEPENDS="libexpat, libx11"
 TERMUX_PKG_BREAKS="dbus-dev"
 TERMUX_PKG_REPLACES="dbus-dev"
@@ -23,7 +22,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	export LIBS="-llog"
-	autoreconf -fi
 }
 
 termux_step_create_debscripts() {
