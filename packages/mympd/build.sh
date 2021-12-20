53f5ca2080141b23cd48a62208869cc131764e62
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-20 11:39:39 +0200

    Update build.sh

--- packages/mympd/build.sh
+++ packages/mympd/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=8.1.6
 TERMUX_PKG_SRCURL=https://github.com/jcorporation/myMPD/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=eddeec8e598aca50e47d6da7f09103f2772d763abb65df7da2599013056a00ef
-TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre"
+TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre, resolv-conf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
