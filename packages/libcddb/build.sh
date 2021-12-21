2ebca713c672abab2189634ab138cee4644a1512
tathastu871 <daichiash1997@gmail.com>
2021-12-21 23:10:58 +0530

    Minor changes

--- packages/libcddb/build.sh
+++ packages/libcddb/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://libcddb.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Libcddb is a C library to access data on a CDDB server"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.3.2
 TERMUX_PKG_SRCURL=http://prdownloads.sourceforge.net/libcddb/libcddb-${TERMUX_PKG_VERSION}.tar.bz2
