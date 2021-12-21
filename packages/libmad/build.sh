888ab97a3010cec2694beedcc06f8d9befb8a260
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-21 20:21:21 +0900

    libmad: Install pkg-config file

--- packages/libmad/build.sh
+++ packages/libmad/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="MAD is a high-quality MPEG audio decoder"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.15.1b
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=ftp://ftp.mars.org/pub/mpeg/libmad-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bbfac3ed6bfbc2823d3775ebb931087371e142bb0e9bb1bee51a76a6e0078690
 TERMUX_PKG_BREAKS="libmad-dev"
@@ -19,4 +19,21 @@ termux_step_pre_configure() {
 termux_post_configure() {
 	cd $TERMUX_PKG_SRCDIR
 	sed -i -e 's/-force-mem//g' Makefile
+}
+
+termux_step_post_make_install() {
+	mkdir -p $TERMUX_PKG_CONFIG_LIBDIR
+	cat > $TERMUX_PKG_CONFIG_LIBDIR/mad.pc <<-EOF
+		prefix=$TERMUX_PREFIX
+		exec_prefix=\${prefix}
+		libdir=$TERMUX_PREFIX/lib
+		includedir=\${prefix}/include
+
+		Name: mad
+		Description: MPEG Audio Decoder
+		Requires:
+		Version: $TERMUX_PKG_VERSION
+		Libs: -L\${libdir} -lmad
+		Cflags: -I\${includedir}
+	EOF
 }
