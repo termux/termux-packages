140ae791bf33ae24dda8beca986a5b1e20f75eb2
tathastu871 <daichiash1997@gmail.com>
2021-12-22 22:16:45 +0530

    New Package : frei0r-plugins

---
+++ packages/frei0r-plugins/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://frei0r.dyne.org/
+TERMUX_PKG_DESCRIPTION="A large collection of free and portable video plugins"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.7.0
+TERMUX_PKG_SRCURL=https://github.com/dyne/frei0r/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=6f7cf95ea2257687cc31db0ed9c9bc0eec152e953d515f346eabec048ed2b29d
+TERMUX_PKG_SUGGESTS="opencv, libcairo, libcairo-static, gavl"
