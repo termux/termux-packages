11d5f9724bbbb7282f482d141b36f6800dda96d5
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-22 08:14:32 +0200

    mate-desktop: depends on startup-notification

--- x11-packages/mate-desktop/build.sh
+++ x11-packages/mate-desktop/build.sh
@@ -5,5 +5,5 @@ TERMUX_PKG_VERSION=1.24.1
 TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-desktop/releases/download/v$TERMUX_PKG_VERSION/mate-desktop-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=d1e8cfae3828d8f083d624b1bfaa332a68ff37c145286432e9e6875a916da1d9
-TERMUX_PKG_DEPENDS="libmatekbd, dconf"
+TERMUX_PKG_DEPENDS="libmatekbd, dconf, startup-notification"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
