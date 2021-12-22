f4ba244cee2f6bd36d8cb65fb32fe6058aa6b251
tathastu871 <daichiash1997@gmail.com>
2021-12-22 14:12:39 +0530

    Minor changes

--- packages/libcddb/build.sh
+++ packages/libcddb/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.3.2
 TERMUX_PKG_SRCURL=http://prdownloads.sourceforge.net/libcddb/libcddb-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=35ce0ee1741ea38def304ddfe84a958901413aa829698357f0bee5bb8f0a223b
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--build=aarch64-unknown-linux-gnu"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static --build=aarch64-unknown-linux-gnu"
