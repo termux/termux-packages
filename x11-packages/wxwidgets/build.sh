3d29dc6a48a7faa9c203236bfcce1c335f237c36
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-23 05:26:16 +0900

    wxwidgets: Install `include/wx/android`

--- x11-packages/wxwidgets/build.sh
+++ x11-packages/wxwidgets/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="docs/gpl.txt, docs/lgpl.txt, docs/licence.txt, docs/licendoc.txt, docs/preamble.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.1.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/wxWidgets/wxWidgets/releases/download/v${TERMUX_PKG_VERSION}/wxWidgets-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=d7b3666de33aa5c10ea41bb9405c40326e1aeb74ee725bb88f90f1d50270a224
 TERMUX_PKG_DEPENDS="glu, gtk3, libcurl, libexpat, libiconv, libjpeg-turbo, liblzma, libnotify, libpng, libsecret, libsm, libtiff, mesa, sdl2"
@@ -14,3 +15,12 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-sdl
 wx_cv_func_snprintf_pos_params=yes
 "
+
+termux_step_pre_configure() {
+	_WX_RELEASE=$(awk '/^WX_RELEASE =/ { print $3 }' "$TERMUX_PKG_SRCDIR"/Makefile.in)
+}
+
+termux_step_post_make_install() {
+	cp -fr "$TERMUX_PKG_SRCDIR"/include/wx/android \
+		$TERMUX_PREFIX/include/wx-${_WX_RELEASE}/wx/android
+}
