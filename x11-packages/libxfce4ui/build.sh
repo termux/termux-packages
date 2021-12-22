f26680a542c7658420b8097364c1da23d0d8bc54
NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>
2021-12-22 17:33:30 +0800

    Xfce4 update (#553)
    Upgrading:
    * libxfce4ui
    * xfce4-goodies
    * xfce4-terminal

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.17.1
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=4.17.2
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/libxfce4ui/4.17/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ec5347e108cdfbce6d62a8d6db55d5d0506a7ca67779228dae30af2a4b4595fc
+TERMUX_PKG_SHA256=c3ba2056dd4d515db5a14b1a589b7afc88e4e2662e27fe93e2054a0e9d09df24
 TERMUX_PKG_DEPENDS="gtk2, gtk3, hicolor-icon-theme, libsm, libxfce4util, startup-notification, xfconf, glib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-gtk3
