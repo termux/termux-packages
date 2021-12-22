b2f6471989c1d88c4fdb4391e066cdb5bbcfc937
tathastu871 <daichiash1997@gmail.com>
2021-12-22 13:42:38 +0530

    Minor Changes

--- packages/xpup/build.sh
+++ packages/xpup/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ericchiang/xpup
 TERMUX_PKG_DESCRIPTION="command line tool for processing XML"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.0.0-20150709172731
-_COMMIT=8b964504de6fe798bfcdfb0e5303517342ceab42
-TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_SRCURL=https://github.com/ericchiang/xpup.git
+TERMUX_PKG_VERSION=0.0.0+20150709172731.8b964504de6f-1
+TERMUX_PKG_SRCURL="https://github.com/ericchiang/xpup/archive/8b964504de6fe798bfcdfb0e5303517342ceab42.tar.gz"
+TERMUX_PKG_SHA256=f7597d4d098d82e24a5c067a00df5791f57803223a0cd522f8d4c4b7403b7593
 
 termux_step_make() {
 	termux_setup_golang
