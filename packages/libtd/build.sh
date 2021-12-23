70074799d2fe728d891e3080e69ea2f382e56bfd
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-23 15:34:06 +0200

    libtd: add maintainer information

--- packages/libtd/build.sh
+++ packages/libtd/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://core.telegram.org/tdlib/
 TERMUX_PKG_DESCRIPTION="Library for building Telegram clients"
 TERMUX_PKG_LICENSE="BSL-1.0"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.7.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/tdlib/td/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3daaf419f1738b7e0ac0e8a08f07e01a1faaf51175a59c0b113c15e30c69e173
 TERMUX_PKG_DEPENDS="readline, openssl (>= 1.1.1), zlib"
