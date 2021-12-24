db9805f21f75e5704b1e0f7fafa8b481698980ed
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-24 09:19:11 +0900

    new package: libneon

---
+++ packages/libneon/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://notroj.github.io/neon/
+TERMUX_PKG_DESCRIPTION="An HTTP/1.1 and WebDAV client library, with a C interface"
+TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.32.1
+TERMUX_PKG_SRCURL=https://notroj.github.io/neon/neon-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=05c54bc115030c89e463a4fb28d3a3f8215879528ba5ca70d676d3d21bf3af52
+TERMUX_PKG_DEPENDS="libxml2, openssl, zlib"
