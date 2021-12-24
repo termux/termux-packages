332d24ba2b1f8abf8cd10c63ec90d6aa076ab80e
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-25 06:22:09 +0900

    gauche: Update to 0.9.11

--- packages/gauche/build.sh
+++ packages/gauche/build.sh
@@ -2,19 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://practical-scheme.net/gauche/
 TERMUX_PKG_DESCRIPTION="An R7RS Scheme implementation developed to be a handy script interpreter"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.10
+TERMUX_PKG_VERSION=0.9.11
 TERMUX_PKG_SRCURL=https://github.com/shirok/Gauche/releases/download/release${TERMUX_PKG_VERSION//./_}/Gauche-${TERMUX_PKG_VERSION}.tgz
-TERMUX_PKG_SHA256=0f39df1daec56680b542211b085179cb22e8220405dae15d9d745c56a63a2532
+TERMUX_PKG_SHA256=395e4ffcea496c42a5b929a63f7687217157c76836a25ee4becfcd5f130f38e4
 TERMUX_PKG_DEPENDS="binutils, ca-certificates, gdbm, libcrypt, libiconv, mbedtls, zlib"
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-ca-bundle=$TERMUX_PREFIX/etc/tls/cert.pem
 --with-libatomic-ops=no
 --with-slib=$TERMUX_PREFIX/share/slib
 "
-# 0.9.10 does not support MbedTLS 3.0: remove --with-tls=... when updating
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-tls=axtls"
 # As of 0.9.10 some code hangs with threads enabled, e.g.
 # ```
 # (use rfc.uri)
@@ -25,7 +24,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-threads=none"
 termux_step_host_build() {
 	_PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/prefix
 	mkdir -p $_PREFIX_FOR_BUILD
-	"$TERMUX_PKG_SRCDIR"/configure --prefix=$_PREFIX_FOR_BUILD
+
+	find "$TERMUX_PKG_SRCDIR" -mindepth 1 -maxdepth 1 ! -name build_gosh -exec cp -a \{\} ./ \;
+	./configure --prefix=$_PREFIX_FOR_BUILD
 	make -j $TERMUX_MAKE_PROCESSES
 	make install
 }
