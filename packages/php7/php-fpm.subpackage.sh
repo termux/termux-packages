e7fa7bf0fd6a5a9da70994a56e5795258425feab
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-25 20:12:43 +0900

    new package: php7
    Copied from php package as of commit e6ef1b554576b1de647b3b0ee3e13b32c6f0e868.

---
+++ packages/php7/php-fpm.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="bin/php-fpm etc/php-fpm.conf etc/php-fpm.d/www.conf share/man/man8/php-fpm.8.gz var/service/php-fpm"
+TERMUX_SUBPKG_CONFFILES="etc/php-fpm.conf etc/php-fpm.d/www.conf"
+TERMUX_SUBPKG_DESCRIPTION="FastCGI Process Manager for PHP"
