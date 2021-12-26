6a2a9d41256b4121c070e329741337947ff406a9
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-26 17:38:57 +0900

    new package: enblend

---
+++ packages/enblend/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=http://enblend.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="A tool for compositing images using a Burt&Adelson multiresolution spline"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=4.2.0_p20161007
+TERMUX_PKG_SRCURL=https://dev.gentoo.org/~soap/distfiles/enblend-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=4fe05af3d697bd6b2797facc8ba5aeabdc91e233156552301f1c7686232ff4c3
+TERMUX_PKG_DEPENDS="gsl, libandroid-glob, libc++, libtiff, libvigra, littlecms"
+TERMUX_PKG_BUILD_DEPENDS="boost, libjpeg-turbo, libpng, zlib"
+
+termux_step_pre_configure() {
+	autoreconf -fi
+
+	LDFLAGS+=" -landroid-glob"
+}
