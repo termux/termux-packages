01e46c8bb8bdcb1c7a20782037b69fbec619ff85
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-26 18:37:16 +0200

    apt: update message shown on `apt update` failure

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.13
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e519e55f1015d656425e68f8c7c7bebd1908df3ac87836eb6e7b6b58ce3f965d
 # apt-key requires utilities from coreutils, findutils, gpgv, grep, sed.
