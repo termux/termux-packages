251267e80ed95f9ba81d9e66ef9028d2b1b77039
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-26 20:05:22 +0200

    tsmuxer: bump revision

--- packages/tsmuxer/build.sh
+++ packages/tsmuxer/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _VERSION_REAL=nightly-2021-12-05-02-09-30
 TERMUX_PKG_VERSION=$(cut -d- -f2,3,4 <<< "$_VERSION_REAL" | tr '-' '.')
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/justdan96/tsMuxer/archive/refs/tags/${_VERSION_REAL}.tar.gz
 TERMUX_PKG_SHA256=676f7741fb897ec53a975fcb6a0c4d5466d680a12e82b54b39cbf48d91bb14fb
 TERMUX_PKG_DEPENDS="freetype, libc++, zlib"
