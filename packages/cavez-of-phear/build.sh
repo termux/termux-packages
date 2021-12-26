9b33ddeebb8f3d0a8930435cbd42e6e77a5e36f6
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-26 18:04:31 +0200

    cavez-of-phear: bump revision

--- packages/cavez-of-phear/build.sh
+++ packages/cavez-of-phear/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A Boulder Dash like game for consoles/terminals"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Dmitry Marakasov <amdmi3@amdmi3.ru>"
 TERMUX_PKG_VERSION=0.6.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/AMDmi3/cavezofphear/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=29ea76dd1a0f38904cd09e36b7205d4a4c01324d2ba28d03f15d9ae53881aa10
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
