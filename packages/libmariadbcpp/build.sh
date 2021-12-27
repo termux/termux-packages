247759e570ec2877ea50848435821dfc1ae486f8
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-27 22:37:53 +0900

    new package: libmariadbcpp

---
+++ packages/libmariadbcpp/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://mariadb.com/docs/clients/mariadb-connectors/connector-cpp/
+TERMUX_PKG_DESCRIPTION="Enables C++ applications to establish client connections to MariaDB Enterprise over TLS"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SRCURL=https://github.com/mariadb-corporation/mariadb-connector-cpp.git
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_DEPENDS="libc++, openssl, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DINSTALL_LIB_SUFFIX=lib
+-DINSTALL_LIBDIR=lib/mariadbcpp
+-DINSTALL_PLUGINDIR=lib/mariadbcpp/plugin
+-DWITH_EXTERNAL_ZLIB=ON
+"
+
+termux_step_post_get_source() {
+	git submodule update --init --recursive
+}
+
+termux_step_pre_configure() {
+	LDFLAGS="-Wl,-rpath=$TERMUX_PREFIX/lib/mariadbcpp $LDFLAGS"
+}
