036861c8a59bf8f0b0dbe16446083c49058d113f
NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>
2021-12-27 14:26:28 +0800

    Featherpad:update to 1.1.0
    Signed-off-by: NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>

--- x11-packages/featherpad/build.sh
+++ x11-packages/featherpad/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/tsujan/FeatherPad
 TERMUX_PKG_DESCRIPTION="Lightweight Qt Plain-Text Editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_VERSION=1.1.0
 TERMUX_PKG_SRCURL=https://github.com/tsujan/FeatherPad/releases/download/V${TERMUX_PKG_VERSION}/FeatherPad-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=bb605569f851527d4151c779b63bc06b6f974bbcc689cd2b80ef84e4f7fabafb
+TERMUX_PKG_SHA256=210b7025f821ad1196b514d432033526eda7ddc77e613cdf385560b943d4c4be
 TERMUX_PKG_DEPENDS="hicolor-icon-theme, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, hunspell"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
