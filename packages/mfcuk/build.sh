556c70362621d1d9ef4ee5cb90b1858dda0964d8
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-27 20:12:10 +0900

    new package: mfcuk

---
+++ packages/mfcuk/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/nfc-tools/mfcuk
+TERMUX_PKG_DESCRIPTION="MiFare Classic Universal toolKit (MFCUK)"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=b333a7925a3be80d9496c88c9fef816777827a83
+TERMUX_PKG_VERSION=2018.07.14
+TERMUX_PKG_SRCURL=https://github.com/nfc-tools/mfcuk.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="libnfc"
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
+		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
+}
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
