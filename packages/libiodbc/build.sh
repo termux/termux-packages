b7eb0a8ea904be2a28a520f2fa87a8d33e44d582
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-27 14:40:30 +0900

    new package: libiodbc

---
+++ packages/libiodbc/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=http://www.iodbc.org/
+TERMUX_PKG_DESCRIPTION="Independent Open DataBase Connectivity for Linux, MacOS X and Unix systems"
+TERMUX_PKG_LICENSE="LGPL-2.0, BSD 3-Clause"
+TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.BSD, LICENSE.LGPL"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.52.15
+TERMUX_PKG_SRCURL=https://github.com/openlink/iODBC/releases/download/v${TERMUX_PKG_VERSION}/libiodbc-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c74541e3327fc9a1c7ccf103645471c67bc014542d70f572476eb07c0b2dd43c
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-libodbc
+--with-layout=gnu
+"
