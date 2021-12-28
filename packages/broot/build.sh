17bc21256e4ed89c50fc72a300a74697f42fa432
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-12-28 23:32:13 +0900

    new package: broot (#8357)

---
+++ packages/broot/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Canop/broot
+TERMUX_PKG_DESCRIPTION="A better way to navigate directories"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.8.0
+TERMUX_PKG_SRCURL=https://github.com/Canop/broot/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=baa107a2cd458c9f4eda9cafa8fac54570918efafaadfc34df43442f7cd237b9
+TERMUX_PKG_DEPENDS="libgit2"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+
+	mkdir -p build
+	cp man/page build/broot.1
+	sed -i "s/#version/$TERMUX_PKG_VERSION/g" build/broot.1
+	sed -i "s/#date/$(date -r man/page +'%Y\/%m\/%d')/g" build/broot.1
+
+}
+
+termux_step_make_install() {
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/broot
+	install -Dm644 -t $TERMUX_PREFIX/share/man/man1 build/broot.1
+}
