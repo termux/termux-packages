5e8c53ec28e905edb2b482c8d4ebcc598b976774
Tee KOBAYASHI <xtkoba@gmail.com>
2021-12-29 04:08:25 +0900

    php7: Update to 7.4.27

--- packages/php7/build.sh
+++ packages/php7/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://php.net
 TERMUX_PKG_DESCRIPTION="Server-side, HTML-embedded scripting language"
 TERMUX_PKG_LICENSE="PHP-3.0"
-TERMUX_PKG_VERSION=7.4.12
+TERMUX_PKG_VERSION=7.4.27
 TERMUX_PKG_MAINTAINER="@xtkoba"
 TERMUX_PKG_SRCURL=https://github.com/php/php-src/archive/php-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a920b81cbfd866f2e686a2b8cc49e6e60a72c3e84e80851529d39e4c9c59e6b2
+TERMUX_PKG_SHA256=198c0aae5a4aa56eca5d576e582be9547df3ac8c42e0aa829f3e299843f2bcc3
 # Build native php for phar to build (see pear-Makefile.frag.patch):
 TERMUX_PKG_HOSTBUILD=true
 # Build the native php without xml support as we only need phar:
