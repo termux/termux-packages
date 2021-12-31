1a2adf64cb24f16686678d32688552fd3fa7d521
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-12-31 10:55:04 +0200

    ovmf: update to 20211216.94.gb451c69088
    Closes https://github.com/termux/termux-packages/pull/7809

--- packages/ovmf/build.sh
+++ packages/ovmf/build.sh
@@ -2,15 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://www.tianocore.org/
 TERMUX_PKG_DESCRIPTION="Open Virtual Machine Firmware"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20210804.56.g769e63999f
+TERMUX_PKG_VERSION=20211216.94.gb451c69088
 TERMUX_PKG_SRCURL=(https://www.kraxel.org/repos/jenkins/edk2/edk2.git-aarch64-0-${TERMUX_PKG_VERSION}.noarch.rpm
 		   https://www.kraxel.org/repos/jenkins/edk2/edk2.git-arm-0-${TERMUX_PKG_VERSION}.noarch.rpm
 		   https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-ia32-0-${TERMUX_PKG_VERSION}.noarch.rpm
 		   https://www.kraxel.org/repos/jenkins/edk2/edk2.git-ovmf-x64-0-${TERMUX_PKG_VERSION}.noarch.rpm)
-TERMUX_PKG_SHA256=(53137d5572993cbfe0899f14f8269214a83210e96f0f535719b63d0f3b129564
-		   76a872b4a9fa599430bef7b150cb667f92516e10a0c66b5a7e4bd083ad501e06
-		   354fb1dc105f3a78167270d50e2fa2d769984ed4e11c7418a1bf1ddb7bb35f67
-		   ddab310e73588eee165ca86e433a1570966880decb3d55c4554cf68a5123ee5b)
+TERMUX_PKG_SHA256=(c54da54841de2873e822aae1775c21161cb276b984d2d64ed698bb126b9e114c
+		   007b3b02b16dafa48a78be52d10991ebd93180bb2aae24a3f574cba567f546c2
+		   937fc34e2663ff561b49784043092b3cfa510a6f74a47b22f627c2183bf31272
+		   79dcf24dd261b6317b47fdba71926e42eec2d622a8debbee9e88fe07b8aeb8a5)
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
