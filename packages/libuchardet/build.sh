8c277be9b05487bb919d7fda7b3c5c6e1cbdf807
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-02 20:00:06 +0900

    new package: libuchardet

---
+++ packages/libuchardet/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/uchardet/
+TERMUX_PKG_DESCRIPTION="An encoding detector library"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.0.7
+TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/uchardet/releases/uchardet-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=3fc79408ae1d84b406922fa9319ce005631c95ca0f34b205fad867e8b30e45b1
+TERMUX_PKG_DEPENDS="libc++"
