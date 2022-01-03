c15b7f17bf6913be56d2d545219009d24c53f27c
Yaksh Bariya <yakshbari4@gmail.com>
2022-01-03 19:21:36 +0530

    ndk-sysroot: bump revision after removing patch for mntent.h
    Also bump toolchain version

--- packages/ndk-sysroot/build.sh
+++ packages/ndk-sysroot/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="System header and library files from the Android NDK nee
 TERMUX_PKG_LICENSE="NCSA"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 # This package has taken over <pty.h> from the previous libutil-dev
 # and iconv.h from libandroid-support-dev:
