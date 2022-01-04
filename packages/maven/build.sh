4cbb3eaf0ae8b4a2b114114d4863e017aa69757b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-05 02:22:53 +0900

    maven: Depend on libjansi

--- packages/maven/build.sh
+++ packages/maven/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A Java software project management and comprehension too
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@masterjavaofficial"
 TERMUX_PKG_VERSION=3.8.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://dlcdn.apache.org/maven/maven-3/${TERMUX_PKG_VERSION}/binaries/apache-maven-${TERMUX_PKG_VERSION}-bin.tar.gz
 TERMUX_PKG_SHA256=2cdc9c519427bb20fdc25bef5a9063b790e4abd930e7b14b4e9f4863d6f9f13c
-TERMUX_PKG_DEPENDS="openjdk-17"
+TERMUX_PKG_DEPENDS="libjansi, openjdk-17"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -14,6 +15,7 @@ termux_step_make_install() {
 	rm -f bin/*.cmd
 	# Remove DLL for Windows
 	rm -rf lib/jansi-native/Windows
+	ln -sf $TERMUX_PREFIX/lib/libjansi.so lib/jansi-native/
 	rm -rf $TERMUX_PREFIX/opt/maven
 	mkdir -p $TERMUX_PREFIX/opt
 	cp -a $TERMUX_PKG_SRCDIR $TERMUX_PREFIX/opt/maven/
