d2fb891ac0b7e84b0920dd28cdec8b97b7a2035d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-07 07:59:10 +0900

    libtirpc: Fix type of `xdr_{,u_}longlong_t`
    Causing build error for asymptote:
    
    ```
    note: candidate function not viable: no known conversion from 'unsigned long long *' to 'uint64_t *' (aka 'unsigned long *') for 2nd argument
    ```

--- packages/libtirpc/build.sh
+++ packages/libtirpc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Transport Independent RPC library"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/libtirpc/libtirpc-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=e24eb88b8ce7db3b7ca6eb80115dd1284abc5ec32a8deccfed2224fc2532b9fd
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gssapi"
