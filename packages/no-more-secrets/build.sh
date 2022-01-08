3f783e428822cb4713e493ee50644e7e076b7878
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-09 02:00:37 +0900

    no-more-secrets: Build both with and without ncurses

--- packages/no-more-secrets/build.sh
+++ packages/no-more-secrets/build.sh
@@ -3,13 +3,15 @@ TERMUX_PKG_DESCRIPTION="This project provides a command line tool called nms tha
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/bartobri/no-more-secrets/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4422e59bb3cf62bca3c73d1fdae771b83aab686cd044f73fe14b1b9c2af1cb1b
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="all-ncurses"
+TERMUX_PKG_EXTRA_MAKE_ARGS="all all-ncurses"
 
 termux_step_pre_configure() {
-	CFLAGS+=" $(pkg-config --cflags ncurses)"
-	LDFLAGS+=" $(pkg-config --libs ncurses)"
+	CFLAGS+=" $CPPFLAGS"
+	export NCURSES_CFLAGS="$(pkg-config --cflags ncurses)"
+	export NCURSES_LIBS="$(pkg-config --libs ncurses)"
 }
