337417f4c243540837ab5dfc4a4a9cc84bde352f
agnostic-apollo <agnosticapollo@gmail.com>
2022-01-08 21:43:48 +0500

    Changed(termux-create-package): Bump to v0.12.0

--- packages/termux-create-package/build.sh
+++ packages/termux-create-package/build.sh
@@ -2,14 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-create-package
 TERMUX_PKG_DESCRIPTION="Utility to create Termux packages"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.10
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.12.0
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-create-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5a5bda0e66475a4bb25f5a4f6f16a715eea98991bdb9471e84fb6d27488358dd
+TERMUX_PKG_SHA256=13bcc1264844e9865eeab19805f24ff28bbfac8d39c11bca66f4357fa70e6ace
 TERMUX_PKG_DEPENDS="binutils, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin termux-create-package
+	install -Dm700 -t $TERMUX_PREFIX/bin src/termux-create-package
 }
