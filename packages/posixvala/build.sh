57aa12e25cc27dd90674c18caf274191977a379b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-09 05:11:11 +0900

    new package: posixvala

---
+++ packages/posixvala/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/radare/posixvala
+TERMUX_PKG_DESCRIPTION="Aims to bring back the posix profile for Vala"
+TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_SRCURL=https://github.com/radare/posixvala/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d35d1e333d7941e130e094b0f55d53132855b990a7df699c14061e0a0e6853e8
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
+
+termux_step_make() {
+	:
+}
