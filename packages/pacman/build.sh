9d1b7f542c81fffbcd134dc9b3e75e6f5c0d312e
Maxython <mixython@gmail.com>
2022-01-09 14:08:28 +0300

    pacman: transition from old hosting to new one and installation improvement

--- packages/pacman/build.sh
+++ packages/pacman/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="A library-based package manager with dependency support"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Maxython"
 TERMUX_PKG_VERSION=6.0.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/pacman/pacman-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0db61456e56aa49e260e891c0b025be210319e62b15521f29d3e93b00d3bf731
 TERMUX_PKG_DEPENDS="bash, libarchive, curl, gpgme"
 TERMUX_PKG_BUILD_DEPENDS="doxygen, asciidoc, nettle"
 TERMUX_PKG_GROUPS="base-devel"
+TERMUX_PKG_CONFFILES="etc/pacman.conf etc/makepkg.conf"
 
 # A temporary solution to the problem with compiling the documentation.
 # https://github.com/termux/termux-packages/pull/7759#issuecomment-945664581
@@ -31,5 +32,6 @@ termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/bash" > postinst
 	echo "mkdir -p $TERMUX_PREFIX/var/lib/pacman/sync" >> postinst
 	echo "mkdir -p $TERMUX_PREFIX/var/lib/pacman/local" >> postinst
+	echo "mkdir -p $TERMUX_PREFIX/var/cache/pacman/pkg" >> postinst
 	chmod 755 postinst
 }
