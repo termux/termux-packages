03ca3acb29477e595242b905df7e36b865b957bf
Aditya Alok <dev.aditya.alok@gmail.com>
2022-01-10 18:48:21 +0530

    Chore(lua-language-server): update to 2.5.6 (#8493)
    * Chore(lua-language-server): update to 2.5.6
    
    - remove arm from TERMUX_PKG_BLACKLISTED_ARCHES
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>
    
    * fix(lua-language-server): update `make.lua` patch
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>
    
    * Fix(lua-language-server): wrong install path
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/lua-language-server/build.sh
+++ packages/lua-language-server/build.sh
@@ -2,18 +2,16 @@ TERMUX_PKG_HOMEPAGE="https://github.com/sumneko/lua-language-server"
 TERMUX_PKG_DESCRIPTION="Sumneko Lua Language Server coded in Lua"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="MrAdityaAlok <dev.aditya.alok@gmail.com>"
-TERMUX_PKG_VERSION=2.4.7
-TERMUX_PKG_SRCURL=https://github.com/sumneko/lua-language-server.git
+TERMUX_PKG_VERSION=2.5.6
 TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}"
+TERMUX_PKG_SRCURL="https://github.com/sumneko/lua-language-server.git"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-# no cpu_relax support present for these archs.
-# https://github.com/actboy168/bee.lua/blob/32f65b92739fa236d87fc1b2e7617470d47f0355/bee/thread/spinlock.h#L14
-TERMUX_PKG_BLACKLISTED_ARCHES="arm,i686"
+TERMUX_PKG_BLACKLISTED_ARCHES="i686"
 
-_patch() {
+_patch_on_device() {
 	if [ "${TERMUX_ON_DEVICE_BUILD}" = true ]; then
 		current_dir=$(pwd)
 
@@ -25,7 +23,7 @@ _patch() {
 }
 
 termux_step_host_build() {
-	_patch
+	_patch_on_device
 	termux_setup_ninja
 
 	mkdir 3rd
@@ -56,10 +54,10 @@ termux_step_make_install() {
 		# determine its version, so provide it manually.
 		if [ "\$1" = "--version" ]; then
 			echo "${TERMUX_PKG_NAME}: ${TERMUX_PKG_VERSION}"
-		else 
+		else
 			TMPPATH=\$(mktemp -d "${TERMUX_PREFIX}/tmp/${TERMUX_PKG_NAME}.XXXX")
 
-			exec ${INSTALL_DIR}/bin/Android/${TERMUX_PKG_NAME} \\
+			exec ${INSTALL_DIR}/bin/${TERMUX_PKG_NAME} \\
 				--logpath="\${TMPPATH}/log" \\
 				--metapath="\${TMPPATH}/meta" \\
 				"\${@}"
@@ -69,9 +67,9 @@ termux_step_make_install() {
 
 	chmod 744 "${TERMUX_PREFIX}/bin/${TERMUX_PKG_NAME}"
 
-	install -Dm744 -t "${INSTALL_DIR}"/bin/Android ./bin/Android/"${TERMUX_PKG_NAME}"
+	install -Dm744 -t "${INSTALL_DIR}"/bin ./bin/"${TERMUX_PKG_NAME}"
 	install -Dm644 -t "${INSTALL_DIR}" ./{main,debugger}.lua
-	install -Dm644 -t "${INSTALL_DIR}"/bin/Android ./bin/Android/main.lua
+	install -Dm644 -t "${INSTALL_DIR}"/bin ./bin/main.lua
 
 	cp -r ./script ./meta ./locale "${INSTALL_DIR}"
 }
