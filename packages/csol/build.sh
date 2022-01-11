3968e65d89e0b6eb2687eeccc9e096197cfc2ce5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-12 06:33:34 +0900

    new package: csol

---
+++ packages/csol/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/nielssp/csol
+TERMUX_PKG_DESCRIPTION="A small collection of solitaire games implemented in C using ncurses"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_SRCURL=https://github.com/nielssp/csol/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4117a9b7d6dbddb8549fa7bc8911e4bf8639f275629d428f160e7a824a29ba76
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+TERMUX_CMAKE_BUILD="Unix Makefiles"
