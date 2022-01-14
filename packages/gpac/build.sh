423b1fe70801345a4bae393e0b8cdddabf218219
Henrik Grimler <henrik@grimler.se>
2022-01-14 21:42:54 +0100

    gpac: avoid building with x11 support

--- packages/gpac/build.sh
+++ packages/gpac/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/v${TERMUX_PKG_VERSION}.ta
 TERMUX_PKG_SHA256=3b0ffba73c68ea8847027c23f45cd81d705110ec47cf3c36f60e669de867e0af
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="STRIP=:"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11"
 
 termux_step_pre_configure() {
 	CFLAGS+=" -fPIC"
