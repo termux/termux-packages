9c667814aba450670f2045065912850c1f0d965d
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-01-14 13:58:47 +0200

    Update build.sh

--- packages/crowbook/build.sh
+++ packages/crowbook/build.sh
@@ -19,5 +19,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/crowbook
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/crowbook
 }
