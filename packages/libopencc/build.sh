bcc1b7c434b8ca57e9df6590dfeefc08117f73bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-01-15 13:54:05 +0200

    libopencc: bump revision

--- packages/libopencc/build.sh
+++ packages/libopencc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An opensource project for conversions between Traditiona
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/BYVoid/OpenCC/archive/ver.${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=99a9af883b304f11f3b0f6df30d9fb4161f15b848803f9ff9c65a96d59ce877f
 TERMUX_PKG_DEPENDS="libc++, marisa"
