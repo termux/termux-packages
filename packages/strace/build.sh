c1f8de128836591020936c6dbb75f4df618d6d2c
Faheem Pervez <trippin1@gmail.com>
2022-01-16 03:20:37 +0000

    strace: update to 5.16

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://strace.io/
 TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.14
+TERMUX_PKG_VERSION=5.16
 TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=901bee6db5e17debad4530dd9ffb4dc9a96c4a656edbe1c3141b7cb307b11e73
+TERMUX_PKG_SHA256=dc7db230ff3e57c249830ba94acab2b862da1fcaac55417e9b85041a833ca285
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdw"
 
@@ -20,6 +20,5 @@ TERMUX_PKG_RM_AFTER_INSTALL="bin/strace-graph"
 
 termux_step_pre_configure() {
 	autoreconf # for configure.ac in 5.11fix.patch
-	CPPFLAGS+=" -Dfputs_unlocked=fputs -DPR_SET_VMA=0x53564d41"
-	# remove the -DPR_SET_VMA for >5.13
+	CPPFLAGS+=" -Dfputs_unlocked=fputs"
 }
