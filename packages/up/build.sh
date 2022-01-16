8bc572dcecbd3ba411090521e9397b6b03ed8e4a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 10:34:57 +0900

    new package: up

---
+++ packages/up/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/akavel/up
+TERMUX_PKG_DESCRIPTION="Helps interactively and incrementally explore textual data in Linux"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4
+TERMUX_PKG_SRCURL=https://github.com/akavel/up.git
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin up
+}
