fb319f9f8b4b95e73144655cc7ec7b2edc3891a6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 17:14:31 +0900

    new package: mdbook-toc

---
+++ packages/mdbook-toc/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/badboy/mdbook-toc
+TERMUX_PKG_DESCRIPTION="A preprocessor for mdbook to add inline Table of Contents support"
+TERMUX_PKG_LICENSE="MPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.7.0
+TERMUX_PKG_SRCURL=https://github.com/badboy/mdbook-toc.git
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-toc
+}
