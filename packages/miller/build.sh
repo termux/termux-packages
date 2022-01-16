8dc766f51d5f4a990dfc5e23f2b0ef795fa7466e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 10:31:49 +0900

    new package: miller

---
+++ packages/miller/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://miller.readthedocs.io/
+TERMUX_PKG_DESCRIPTION="Like awk, sed, cut, join, and sort for name-indexed data such as CSV, TSV, and tabular JSON"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=6.0.0
+TERMUX_PKG_SRCURL=https://github.com/johnkerl/miller.git
+TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	local dir="$GOPATH"/src/github.com/johnkerl/miller
+	mkdir -p "$(dirname "${dir}")"
+	ln -sfT "$TERMUX_PKG_SRCDIR" "${dir}"
+	TERMUX_PKG_BUILDDIR="${dir}"
+	cd "${dir}"
+}
+
+termux_step_configure() {
+	:
+}
