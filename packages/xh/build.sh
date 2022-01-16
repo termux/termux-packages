1361c118d1ce74a04dd431a2ad3162ac4b80936c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 19:21:09 +0900

    new package: xh

---
+++ packages/xh/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ducaale/xh
+TERMUX_PKG_DESCRIPTION="A friendly and fast tool for sending HTTP requests"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.14.1
+TERMUX_PKG_SRCURL=https://github.com/ducaale/xh.git
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/xh
+}
