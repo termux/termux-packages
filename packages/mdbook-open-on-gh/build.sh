fcfb07dcc4e2c70433f67bd5672cbe5b7982f0c1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 17:26:10 +0900

    new package: mdbook-open-on-gh

---
+++ packages/mdbook-open-on-gh/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/badboy/mdbook-open-on-gh
+TERMUX_PKG_DESCRIPTION="A preprocessor for mdbook to add a open-on-github link on every page"
+TERMUX_PKG_LICENSE="MPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.0.1
+TERMUX_PKG_SRCURL=https://github.com/badboy/mdbook-open-on-gh.git
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-open-on-gh
+}
