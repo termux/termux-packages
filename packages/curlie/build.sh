e4a2f6968583ff13ab7d0cbea436c676198ef19a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 10:00:57 +0900

    new package: curlie

---
+++ packages/curlie/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://curlie.io/
+TERMUX_PKG_DESCRIPTION="The power of curl, the ease of use of httpie"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.6.7
+TERMUX_PKG_SRCURL=https://github.com/rs/curlie.git
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin curlie
+}
