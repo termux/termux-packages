6805212893b45557bbab8e8dc55dc7cd9c491428
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 10:05:41 +0900

    new package: tealdeer

---
+++ packages/tealdeer/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://dbrgn.github.io/tealdeer/
+TERMUX_PKG_DESCRIPTION="A very fast implementation of tldr in Rust"
+TERMUX_PKG_LICENSE="Apache-2.0, MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE-APACHE, LICENSE-MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_SRCURL=https://github.com/dbrgn/tealdeer.git
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/tldr
+}
