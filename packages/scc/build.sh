cabb4234adc8237b7e3a044c1f609a804fffb329
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-16 10:17:39 +0900

    new package: scc

---
+++ packages/scc/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/boyter/scc
+TERMUX_PKG_DESCRIPTION="Counts physical the lines of code, blank lines, comment lines, and physical lines of source code"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_SRCURL=https://github.com/boyter/scc.git
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin scc
+}
