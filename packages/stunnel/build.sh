4b1040af3e4e93af5b8f5130df143492b4360d24
flutcher22 <89228379+flutcher22@users.noreply.github.com>
2022-01-17 03:37:39 +0700

    stunnel: update to 5.61

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.stunnel.org/
 TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.60
+TERMUX_PKG_VERSION=5.61
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c45d765b1521861fea9b03b425b9dd7d48b3055128c0aec673bba5ef9b8f787d
+TERMUX_PKG_SHA256=91ea0ca6482d8f7e7d971ee64ab4f86a2817d038a372f0893e28315ef2015d7a
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/stunnel3 share/man/man8/stunnel.*.8"
