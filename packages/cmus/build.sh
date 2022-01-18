c20a125219e21113ab08caaaeb1b7cf9939524f9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-18 15:01:24 +0900

    cmus: Bump revision to rebuild
    as a follow-up of ffmpeg version bump.

--- packages/cmus/build.sh
+++ packages/cmus/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small, fast and powerful console music player"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.9.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, ncurses, pulseaudio, ffmpeg, libmad, opusfile, libflac, libvorbis"
 TERMUX_PKG_SRCURL=https://github.com/cmus/cmus/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6fb799cae60db9324f03922bbb2e322107fd386ab429c0271996985294e2ef44
