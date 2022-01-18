989801561e397eb1c4591512cb82b6fa187a1015
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-18 19:09:49 +0900

    new package: procs

---
+++ packages/procs/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dalance/procs
+TERMUX_PKG_DESCRIPTION="A modern replacement for ps"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.12.0
+TERMUX_PKG_SRCURL=https://github.com/dalance/procs/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=04dbb3ddc734a9d1305b26b546a913e3e04d556b5863212a2675d29268aebed3
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/procs
+}
