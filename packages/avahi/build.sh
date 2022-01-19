b66bc94c7fa9756651ef8b988b02be706e9db846
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-20 02:01:05 +0900

    avahi: Enable use of D-Bus

--- packages/avahi/build.sh
+++ packages/avahi/build.sh
@@ -3,16 +3,19 @@ TERMUX_PKG_DESCRIPTION="A system for service discovery on a local network via mD
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/lathiat/avahi/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c15e750ef7c6df595fb5f2ce10cac0fee2353649600e6919ad08ae8871e4945f
-TERMUX_PKG_DEPENDS="glib, libandroid-glob, libcap, libdaemon, libevent, libexpat"
+TERMUX_PKG_DEPENDS="dbus, glib, libandroid-glob, libcap, libdaemon, libevent, libexpat"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-dbus
+--enable-dbus
 --disable-gdbm
 --disable-gtk3
+--disable-mono
 --disable-pygobject
+--disable-python
+--disable-python-dbus
 --disable-qt5
 --with-distro=none
 ac_cv_func_chroot=no
