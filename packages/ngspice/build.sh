3bd7fa428c54447bc5a4ab99954f75919b66470c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-21 02:23:39 +0900

    new package: ngspice

---
+++ packages/ngspice/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=http://ngspice.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="A mixed-level/mixed-signal circuit simulator"
+TERMUX_PKG_LICENSE="BSD 3-Clause, LGPL-2.1"
+TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=35
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/ngspice/ngspice-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c1b7f5c276db579acb3f0a7afb64afdeb4362289a6cab502d4ca302d6e5279ec
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_GROUPS="science"
