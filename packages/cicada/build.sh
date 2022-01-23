c14d6ba231159a690dbddbf3b83062b68c35c391
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-23 18:27:16 +0900

    cicada: Fix build for x86_64

--- packages/cicada/build.sh
+++ packages/cicada/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A bash like Unix shell"
 TERMUX_PKG_MAINTAINER="Hugo Wang <w@mitnk.com>"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.9.23
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mitnk/cicada/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fafc780b78153bbbb7160a2ec1354dde313fa380df86593bb806b69602bce145
 TERMUX_PKG_AUTO_UPDATE=true
@@ -10,5 +11,14 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
-    rm -f Makefile
+	rm -f Makefile
+
+	if [ "$TERMUX_ARCH" == "x86_64" ]; then
+		local libdir=target/x86_64-linux-android/release/deps
+		mkdir -p $libdir
+		pushd $libdir
+		local libgcc="$($CC -print-libgcc-file-name)"
+		echo "INPUT($libgcc -l:libunwind.a)" > libgcc.so
+		popd
+	fi
 }
