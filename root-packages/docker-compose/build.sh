678eb1fbab84f6ca7eff98be06664f1c334d9838
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-01-24 01:35:33 -0300

    Update docker-compose version
    * Change Args build.

--- root-packages/docker-compose/build.sh
+++ root-packages/docker-compose/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/docker/compose
 TERMUX_PKG_DESCRIPTION="Compose is a tool for defining and running multi-container Docker applications."
-TERMUX_PKG_SHA256=001cf72f6bc8a8c43d100389e0bbd3d4d5f5c523f4e3f7ddd53f6a4cd2d6cb18
-TERMUX_PKG_VERSION="2.2.2"
+TERMUX_PKG_SHA256=22210187e73732edd9fc02f122ea61481806c703af7b73d0a7351f2e8ed7c0b8
+TERMUX_PKG_VERSION="2.2.3"
 TERMUX_PKG_SRCURL="https://github.com/docker/compose/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_DEPENDS=docker
@@ -11,7 +11,8 @@ termux_step_make() {
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	cd $TERMUX_PKG_SRCDIR
 	mkdir bin/
-	go build -o bin/docker-compose ./cmd
+        if ! [ -z "$GOOS" ];then export GOOS=android;fi
+	go build -o bin/docker-compose -ldflags="-s -w -X github.com/docker/compose/v2/internal.Version=${TERMUX_PKG_VERSION}" ./cmd
 }
 
 termux_step_make_install() {
