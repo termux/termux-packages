121072e068529b346473f4bd0e525ced3276d6ff
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-26 18:09:17 +0900

    new package: libxlsxwriter

---
+++ packages/libxlsxwriter/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://libxlsxwriter.github.io/
+TERMUX_PKG_DESCRIPTION="A C library for creating Excel XLSX files"
+TERMUX_PKG_LICENSE="BSD 2-Clause, BSD 3-Clause, ZLIB, MPL-2.0, MIT, Public Domain"
+TERMUX_PKG_LICENSE_FILE="License.txt"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.4
+TERMUX_PKG_SRCURL=https://github.com/jmcnamara/libxlsxwriter/archive/refs/tags/RELEASE_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b379eb35fdd9c653ebe72485b9c992f612c7ea66f732784457997d6e782f619b
+TERMUX_PKG_DEPENDS="zlib"
