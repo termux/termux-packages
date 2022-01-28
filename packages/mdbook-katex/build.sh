247b966f004fd3ac5f6414b5a59c71f6f2e3ab31
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-28 16:23:42 +0900

    new package: mdbook-katex

---
+++ packages/mdbook-katex/build.sh
@@ -0,0 +1,37 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/lzanini/mdbook-katex
+TERMUX_PKG_DESCRIPTION="A preprocessor for mdBook, pre-rendering LaTex equations to HTML at build time"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=a16a13f4358cf67db5570ecff5e1002578c161d5
+_COMMIT_DATE=2022.01.27
+TERMUX_PKG_VERSION=0.2.10p${_COMMIT_DATE//./}
+TERMUX_PKG_SRCURL=https://github.com/lzanini/mdbook-katex.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$_COMMIT_DATE" ]; then
+		echo -n "ERROR: The specified commit date \"$_COMMIT_DATE\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
+}
+
+termux_step_pre_configure() {
+	export OPENSSL_INCLUDE_DIR=$TERMUX_PREFIX/include/openssl
+	export OPENSSL_LIB_DIR=$TERMUX_PREFIX/lib
+}
+
+termux_step_make() {
+	termux_setup_rust
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-katex
+}
