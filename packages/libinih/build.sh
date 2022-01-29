c0510c6848c711e33d466d0f71df2a5b85465bd3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-30 07:42:02 +0900

    new package: libinih

---
+++ packages/libinih/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/benhoyt/inih
+TERMUX_PKG_DESCRIPTION="A simple .INI file parser written in C"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=53
+TERMUX_PKG_SRCURL=https://github.com/benhoyt/inih/archive/refs/tags/r${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=01b0366fdfdf6363efc070c2f856f1afa33e7a6546548bada5456ad94a516241
