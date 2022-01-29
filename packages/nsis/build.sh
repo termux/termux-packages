4790b734e552b312dad3ae8988ccd4dd6d748f24
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-30 04:00:42 +0900

    new package: nsis

---
+++ packages/nsis/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/nsis/
+TERMUX_PKG_DESCRIPTION="A professional open source system to create Windows installers"
+# Licenses: zlib/libpng, bzip2, CPL-1.0
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.08
+TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/nsis/nsis-${TERMUX_PKG_VERSION}-src.tar.bz2
+TERMUX_PKG_SHA256=a85270ad5386182abecb2470e3d7e9bec9fe4efd95210b13551cb386830d1e87
+TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, zlib"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make_install() {
+	scons \
+		CC="$(which $CC)" \
+		CXX="$(which $CXX)" \
+		APPEND_CCFLAGS="$CFLAGS $CPPFLAGS" \
+		APPEND_LINKFLAGS="$LDFLAGS" \
+		SKIPSTUBS=all \
+		SKIPPLUGINS=all \
+		SKIPUTILS=all \
+		SKIPMISC=all \
+		NSIS_CONFIG_CONST_DATA_PATH=no \
+		PREFIX="$TERMUX_PREFIX/opt/nsis" \
+		install-compiler
+}
