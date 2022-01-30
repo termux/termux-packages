d890099b50ea2a03104bb337401fab24ca9021e0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-01-31 02:54:20 +0900

    new package: avrdude

---
+++ root-packages/avrdude/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://savannah.nongnu.org/projects/avrdude
+TERMUX_PKG_DESCRIPTION="Software for programming Microchip (former Atmel) AVR Microcontrollers"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=6.4
+TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/avrdude/avrdude-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=a9be7066f70a9dcf4bf0736fcf531db6a3250aed1a24cc643add27641b7110f9
+TERMUX_PKG_DEPENDS="libusb"
