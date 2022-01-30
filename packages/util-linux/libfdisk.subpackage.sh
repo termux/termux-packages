37b2305361e31d04f64c7d5916d7e5e6641d8ba5
Henrik Grimler <henrik@grimler.se>
2022-01-31 00:28:45 +0100

    libuuid: remove package, build as part of util-linux instead
    This adds new subpackages libuuid and libfdisk to util-linux.  By
    switching to libuuid from util-linux we get the uuidgen tool, which is
    a dependency for flang.

---
+++ packages/util-linux/libfdisk.subpackage.sh
@@ -0,0 +1,13 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/cfdisk
+bin/fdisk
+bin/sfdisk
+include/libfdisk
+lib/libfdisk.so
+lib/pkgconfig/fdisk.pc
+share/bash-completion/completions/cfdisk
+share/bash-completion/completions/fdisk
+share/bash-completion/completions/sfdisk
+share/man/man8/uuid*
+"
+TERMUX_SUBPKG_DESCRIPTION="Collection of partitioning utilities"
---
+++ packages/util-linux/libuuid.subpackage.sh
@@ -0,0 +1,11 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/uuid*
+include/uuid/uuid.h
+lib/libuuid.so
+lib/pkgconfig/uuid.pc
+share/bash-completion/completions/uuid*
+share/man/man1/uuid*
+share/man/man3/uuid*
+share/man/man8/uuid*
+"
+TERMUX_SUBPKG_DESCRIPTION="Universally Unique ID library"
