7b355a29d05fa8570c2d9e25b55d135db96d6725
Henrik Grimler <henrik@grimler.se>
2022-01-30 16:49:12 +0100

    texlive{,-full}: remove package
    It is one (soon two) years behind, and is a big hack: some of the debs
    are larger than our upload system can handle, and the file lists are
    generated from the tlpdb in a not so nice way that breaks horribly on
    every texlive release.  Disk space is somewhat of a concern again on
    the host that fosshost gives us at no (!) cost, and removing
    texlive-full gives us ~1 GB of space.
    
    The reasonable thing to do would be to set up a separate
    "termux-texlive" repo, and create a package for every texlive package
    rather than for every collection as done here.  Debian, ubuntu and
    friends properly creates subpackages.  I am not really motivated to do
    this though, so if someone else has use of a properly packaged texlive
    and wants to look into it, then that would be great.
    
    Users that want to use texlive should install texlive-installer
    instead, it anyways allows for more convenient installation (you can
    freely choose which scheme and which packages to install).

--- packages/texlive/texlive-bibtexextra.subpackage.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-bibtexextra"
-TERMUX_SUBPKG_DEPENDS="texlive-binextra"
-TERMUX_SUBPKG_INCLUDE=$($TERMUX_PKG_BUILDER_DIR/parse_tlpdb.py collection-bibtexextra $TERMUX_PKG_TMPDIR/texlive.tlpdb)
-TERMUX_SUBPKG_CONFLICTS="texlive-bin (<< 20190410), texlive (<= 20190410-2)"
-
-termux_step_create_subpkg_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo 'PATH=$PATH:$PREFIX/bin/texlive mktexlsr' >> postinst
-}
--- packages/texlive/texlive-formatsextra.subpackage.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Texlive's collection-formatsextra"
-TERMUX_SUBPKG_DEPENDS="texlive-langcyrillic, texlive-mathscience, texlive-fontsrecommended, texlive-plaingeneric"
-TERMUX_SUBPKG_INCLUDE=$($TERMUX_PKG_BUILDER_DIR/parse_tlpdb.py collection-formatsextra $TERMUX_PKG_TMPDIR/texlive.tlpdb)
-TERMUX_SUBPKG_CONFLICTS="texlive-bin (<< 20190410), texlive (<< 20190410)"
-
-termux_step_create_subpkg_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo 'PATH=$PATH:$PREFIX/bin/texlive mktexlsr' >> postinst
-}
