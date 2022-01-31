8a0de974528e14febe918d94c58de6c6d76c507b
xeffyr <25881154+xeffyr@users.noreply.github.com>
2022-01-31 20:38:04 +0200

    proot-distro: update to 3.0.0

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.9.5
+TERMUX_PKG_VERSION=3.0.0
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2c17f539e911a536b2aa9a8903298b84b414dff1b3c25ff189d96775c7bd72c3
+TERMUX_PKG_SHA256=7e60744146463ba095ca9aca3af62da4df29d4364a7142e2087861567f6eeee7
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
