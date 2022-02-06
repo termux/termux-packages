75b3526c80408aa8f03ed879ffbef2b05d40d27f
Yaksh Bariya <yakshbari4@gmail.com>
2022-02-06 15:30:52 +0530

    calc: update to v2.14.0.14

--- packages/calc/build.sh
+++ packages/calc/build.sh
@@ -2,23 +2,30 @@ TERMUX_PKG_HOMEPAGE=http://www.isthe.com/chongo/tech/comp/calc/
 TERMUX_PKG_DESCRIPTION="Arbitrary precision console calculator"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.14.0.8
+TERMUX_PKG_VERSION=2.14.0.14
 TERMUX_PKG_SRCURL=https://github.com/lcn2/calc/releases/download/v$TERMUX_PKG_VERSION/calc-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=ad6a2c44eeea7e06efca5384f107ffaee7564407a7474d7cd4928a53b1a28386
+TERMUX_PKG_SHA256=f7727835a103d9712c571958e924e9c254bd148f08eb4348019bc34f8e71c55d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	# Fails with -j$TERMUX_MAKE_PROCESSES for some reason
-	make USE_READLINE="-DUSE_READLINE" READLINE_EXTRAS="-lhistory -lncurses" \
+	make CC="$CC" \
+		USE_READLINE="-DUSE_READLINE" \
+		READLINE_EXTRAS="-lhistory -lncurses" \
 		READLINE_LIB="-L$TERMUX_PREFIX/lib -lreadline" \
 		READLINE_INCLUDE="-I$TERMUX_PREFIX/include" \
-		DEFAULT_LIB_INSTALL_PATH="$TERMUX_PREFIX/lib" CC="$CC" -j1 \
-		LONG_BITS=$TERMUX_ARCH_BITS
+		DEFAULT_LIB_INSTALL_PATH="$TERMUX_PREFIX/lib" \
+		LONG_BITS=$TERMUX_ARCH_BITS \
+		T="$TERMUX_PREFIX" \
+		BINDIR="$TERMUX_PREFIX/bin" \
+		LIBDIR="$TERMUX_PREFIX/lib" \
+		INCDIR="$TERMUX_PREFIX/include" \
+		SCRIPTDIR="$TERMUX_PREFIX/share/calc/cscript" \
+		CALC_SHAREDIR="$TERMUX_PREFIX/share/calc" \
+		install 
 }
 
 termux_step_make_install() {
-	make T="$TERMUX_PREFIX" BINDIR="/bin" LIBDIR="/lib" INCDIR="/include" \
-		SCRIPTDIR="/share/calc/cscript" CALC_SHAREDIR="/share/calc" install 
+	:
 }
