a43cb0b9f648161d28b54212ed171af17aa6e268
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-07 23:58:00 +0900

    new package: libsophia

---
+++ packages/libsophia/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=http://sophia.systems/
+TERMUX_PKG_DESCRIPTION="Advanced transactional MVCC key-value/row storage library"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.2
+TERMUX_PKG_SRCURL=https://github.com/pmwkaa/sophia.git
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make_install() {
+	install -Dm600 -t $TERMUX_PREFIX/lib libsophia.a
+	install -Dm600 -t $TERMUX_PREFIX/lib libsophia.so.2.2.0
+	ln -sfT libsophia.so.2.2.0 $TERMUX_PREFIX/lib/libsophia.so.2.2
+	ln -sfT libsophia.so.2.2.0 $TERMUX_PREFIX/lib/libsophia.so
+}
