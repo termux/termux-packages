e22ce602fa1f4742e170e57d5410f8da66c7cf26
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-07 14:06:37 +0900

    cliaoke: Fix for working with fluidsynth

--- packages/cliaoke/build.sh
+++ packages/cliaoke/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="Command line karaoke"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jessfraz/cliaoke/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=df601b7d118acb8c0b7251c42b5b2623335bfc51f5dc94135fa6722850955f50
+TERMUX_PKG_RECOMMENDS="fluidsynth"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
@@ -17,4 +19,17 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin cliaoke
+}
+
+termux_step_create_debscripts() {
+	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
+	echo "if [ ! -e $TERMUX_PREFIX/share/soundfonts/FluidR3_GM.sf2 ]; then" >> postinst
+	echo "  echo" >> postinst
+	echo "  echo You may need to get \\\`FluidR3_GM.sf2\\' from somewhere and put it into:" >> postinst
+	echo "  echo" >> postinst
+	echo "  echo '    '$TERMUX_PREFIX/share/soundfonts/" >> postinst
+	echo "  echo" >> postinst
+	echo "fi" >> postinst
+	echo "exit 0" >> postinst
+	chmod 0755 postinst
 }
