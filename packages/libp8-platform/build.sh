cbb644ef7954557396f4f95f26fd735fce35609c
Henrik Grimler <henrik@grimler.se>
2022-02-07 22:43:42 +0100

    libp8-platform: build shared library instead of static one
    libcec (and perhaps other) looks only for a shared one.

--- packages/libp8-platform/build.sh
+++ packages/libp8-platform/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Platform support library used by libCEC and binary add-o
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.1.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Pulse-Eight/platform/archive/p8-platform-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=064f8d2c358895c7e0bea9ae956f8d46f3f057772cb97f2743a11d478a0f68a0
 TERMUX_PKG_DEPENDS="libc++"
