d4b6b67d419ec13a2b7b43aa3c3d40d7bc3159a2
wmcbtech30 <bautista.wyatt.marcus30@gmail.com>
2022-02-08 13:13:56 +0800

    chocolate-doom: update maintainer and bump revision

--- x11-packages/chocolate-doom/build.sh
+++ x11-packages/chocolate-doom/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.chocolate-doom.org
 TERMUX_PKG_DESCRIPTION="Historically-accurate Doom, Heretic, Hexen, and Strife ports."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@WMCB-Tech"
+TERMUX_PKG_MAINTAINER="@wmcbtech30"
 TERMUX_PKG_VERSION=3.0.1
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_REVISION=9
 TERMUX_PKG_SRCURL=https://github.com/chocolate-doom/chocolate-doom/archive/refs/tags/chocolate-doom-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a54383beef6a52babc5b00d58fcf53a454f012ced7b1936ba359b13f1f10ac66
 TERMUX_PKG_DEPENDS="sdl2, sdl2-mixer, sdl2-net, mpg123"
