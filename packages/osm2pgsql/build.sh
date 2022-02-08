cd807b8c8c1067fdd9dfe5621777fcbe315e0f65
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-08 23:24:39 +0900

    osm2pgsql: Update to 1.6.0
    * Make sure to use bundled fmt headers

--- packages/osm2pgsql/build.sh
+++ packages/osm2pgsql/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://osm2pgsql.org/
 TERMUX_PKG_DESCRIPTION="osm2pgsql imports OpenStreetMap (OSM) data into a PostgreSQL/PostGIS database"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.5.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.6.0
 TERMUX_PKG_SRCURL=https://github.com/openstreetmap/osm2pgsql/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d31be268dda21cf31418f68c78b8e0cbbd90cd8a283c4dfe0c134a7960936fce
-TERMUX_PKG_DEPENDS="libexpat, proj, bzip2, zlib, boost, postgresql, lua54"
+TERMUX_PKG_SHA256=0ec8b58ab972ac8356185af4161270c1b625a77299f09e5fb7f45e616ef1a9a5
+TERMUX_PKG_DEPENDS="libexpat, proj, bzip2, zlib, postgresql, lua54"
+TERMUX_PKG_BUILD_DEPENDS="boost"
 TERMUX_PKG_GROUPS="science"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DEXTERNAL_FMT=OFF
+"
