a091164790a46bb99bfaaf11178f4a3db7faa853
Elliana May <me@mause.me>
2022-02-09 21:33:10 +0800

    chore: Switch to using termux_step_get_source

--- packages/libduckdb/build.sh
+++ packages/libduckdb/build.sh
@@ -3,9 +3,13 @@ TERMUX_PKG_DESCRIPTION="An in-process SQL OLAP database management system"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.2
-TERMUX_PKG_SRCURL=https://github.com/duckdb/duckdb/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=65fa44665a4cd187d08a51e2069da953061a3c7c6bbf06f996704ee3f1953d3c
 TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
+
+termux_step_get_source() {
+	# we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
+	git clone https://github.com/duckdb/duckdb -b v$TERMUX_PKG_VERSION $TERMUX_PKG_SRCDIR
+}
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
