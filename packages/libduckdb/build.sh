441a28dc476f705aac0ff246c79c41f88434969a
Elliana May <me@mause.me>
2022-02-09 21:46:08 +0800

    chore: Switch to using a git url in TERMUX_PKG_SRCURL instead

--- packages/libduckdb/build.sh
+++ packages/libduckdb/build.sh
@@ -3,13 +3,10 @@ TERMUX_PKG_DESCRIPTION="An in-process SQL OLAP database management system"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.2
+TERMUX_PKG_GIT_BRANCH=v$TERMUX_PKG_VERSION
+# we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
+TERMUX_PKG_SRCURL=https://github.com/duckdb/duckdb.git
 TERMUX_PKG_DEPENDS="libc++"
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
-
-termux_step_get_source() {
-	# we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
-	git clone https://github.com/duckdb/duckdb -b v$TERMUX_PKG_VERSION $TERMUX_PKG_SRCDIR
-}
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
