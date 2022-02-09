f6b84b35667bb682fa49f63af791bbeb7e4d6ebe
Yaksh Bariya <yakshbari4@gmail.com>
2022-02-09 20:17:43 +0530

    gpgme: update to 1.17.0

--- packages/gpgme/build.sh
+++ packages/gpgme/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/gpgme/
 TERMUX_PKG_DESCRIPTION="Library designed to make access to GnuPG easier"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.16.0
+TERMUX_PKG_VERSION=1.17.0
 TERMUX_PKG_SRCURL=ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6c8cc4aedb10d5d4c905894ba1d850544619ee765606ac43df7405865de29ed0
+TERMUX_PKG_SHA256=4ed3f50ceb7be2fce2c291414256b20c9ebf4c03fddb922c88cda99c119a69f5
 TERMUX_PKG_DEPENDS="gnupg (>= 2.2.9-1), libassuan, libgpg-error"
 TERMUX_PKG_BREAKS="gpgme-dev"
 TERMUX_PKG_REPLACES="gpgme-dev"
--- packages/wget2/build.sh
+++ packages/wget2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The successor of GNU Wget, a file and recursive website 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget2-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_SHA256=da95b1477fa4ede1ac712f179c1354d9624fbc91e778d89cf8d2ae46aa15e3d2
 TERMUX_PKG_DEPENDS="gpgme, libandroid-glob, libassuan, libgnutls, libgpg-error, pcre2"
