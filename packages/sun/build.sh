1e2b67b69cb23305470a71fd7fe3842822e70e6b
Henrik Grimler <henrik@grimler.se>
2022-02-11 19:47:42 +0100

    sun: install TERMUX_PKG_MASSAGEDIR

--- packages/sun/build.sh
+++ packages/sun/build.sh
@@ -19,6 +19,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin \
-		${TERMUX_PKG_BUILDDIR}/src/github.com/basbossink/sun/sun
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
+	        src/github.com/basbossink/sun/sun
 }
