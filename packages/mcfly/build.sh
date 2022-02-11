72bd1ac09146a0d1dbe9475bbc5a512e2faa3bb3
Henrik Grimler <henrik@grimler.se>
2022-02-11 14:52:25 +0100

    mcfly: install to TERMUX_PKG_MASSAGEDIR

--- packages/mcfly/build.sh
+++ packages/mcfly/build.sh
@@ -25,6 +25,8 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mcfly
-	install -Dm600 -t $TERMUX_PREFIX/share/mcfly mcfly.{fi,z}sh
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
+		target/${CARGO_TARGET_NAME}/release/mcfly
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/mcfly \
+		mcfly.{fi,z}sh
 }
