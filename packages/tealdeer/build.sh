619d9bf340b5cc025edfc59ad5f45e159bc54347
Henrik Grimler <henrik@grimler.se>
2022-02-11 19:47:28 +0100

    tealdeer: install to TERMUX_PKG_MASSAGEDIR

--- packages/tealdeer/build.sh
+++ packages/tealdeer/build.sh
@@ -14,5 +14,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/tldr
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
+		target/${CARGO_TARGET_NAME}/release/tldr
 }
