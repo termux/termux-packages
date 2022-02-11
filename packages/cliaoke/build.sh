2cb70583450e8a0745b572fdb3244787f5e08155
Henrik Grimler <henrik@grimler.se>
2022-02-11 08:51:17 +0100

    cliaoke: install to TERMUX_PKG_MASSAGEDIR

--- packages/cliaoke/build.sh
+++ packages/cliaoke/build.sh
@@ -18,7 +18,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin cliaoke
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin cliaoke
 }
 
 termux_step_create_debscripts() {
