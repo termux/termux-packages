3c32d6a00d27f8575be1d66c38b39efaf4dfb099
Henrik Grimler <henrik@grimler.se>
2022-02-11 17:08:26 +0100

    mdbook-plantuml: install to TERMUX_PKG_MASSAGEDIR

--- packages/mdbook-plantuml/build.sh
+++ packages/mdbook-plantuml/build.sh
@@ -25,5 +25,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-plantuml
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
+		target/${CARGO_TARGET_NAME}/release/mdbook-plantuml
 }
