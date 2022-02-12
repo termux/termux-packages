f010c2bab3b3a8edfa90051e00f1ec5c2847e64f
Jia Yuan Lo <jylo06g@gmail.com>
2022-02-13 00:06:14 +0800

    opencl packages: rebuild against latest headers

--- packages/clpeak/build.sh
+++ packages/clpeak/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A tool which profiles OpenCL devices to find their peak 
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_SRCURL=https://github.com/krrishnarraj/clpeak.git
 TERMUX_PKG_BUILD_DEPENDS="opencl-headers, opencl-clhpp"
