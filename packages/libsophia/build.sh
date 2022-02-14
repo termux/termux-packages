2b489ee590fbb25a66a7041c3c12b59d2dcccabf
Henrik Grimler <henrik@grimler.se>
2022-02-14 21:23:43 +0100

    libsophia: install to TERMUX_PKG_MASSAGEDIR

--- packages/libsophia/build.sh
+++ packages/libsophia/build.sh
@@ -7,8 +7,8 @@ TERMUX_PKG_SRCURL=https://github.com/pmwkaa/sophia.git
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
-	install -Dm600 -t $TERMUX_PREFIX/lib libsophia.a
-	install -Dm600 -t $TERMUX_PREFIX/lib libsophia.so.2.2.0
-	ln -sfT libsophia.so.2.2.0 $TERMUX_PREFIX/lib/libsophia.so.2.2
-	ln -sfT libsophia.so.2.2.0 $TERMUX_PREFIX/lib/libsophia.so
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib libsophia.a
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib libsophia.so.2.2.0
+	ln -sfT libsophia.so.2.2.0 $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/libsophia.so.2.2
+	ln -sfT libsophia.so.2.2.0 $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/lib/libsophia.so
 }
