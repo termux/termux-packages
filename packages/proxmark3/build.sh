269bba9f24f5340e33cc14de6f1472f3242b903c
s00se <95649696+s00se@users.noreply.github.com>
2022-02-14 16:28:29 -0700

    proxmark3: update build options
    debug mode to workaround unresolved bug

--- packages/proxmark3/build.sh
+++ packages/proxmark3/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE="https://github.com/RfidResearchGroup/proxmark3"
 TERMUX_PKG_DESCRIPTION="The Swiss Army Knife of RFID Research - RRG/Iceman repo"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Marlin Sööse <marlin.soose@laro.se>"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_VERSION=1:4.14831
 TERMUX_PKG_SRCURL=https://github.com/RfidResearchGroup/proxmark3/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=36006cf30c30a533027de851fe9a96593a093e2e03cddf1703aa81940b9bb9a9
@@ -13,7 +14,7 @@ TERMUX_PKG_BLACKLISTED_ARCHES="i686, x86_64"
 termux_step_post_configure() {
 	export LDLIBS="-L${TERMUX_PREFIX}/lib"
 	export INCLUDES="-I${TERMUX_PREFIX}/include"
-	TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=generic SKIPREVENGTEST=1 SKIPQT=1 SKIPBT=1 SKIPPTHREAD=1 PLATFORM=PM3GENERIC"
+	TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=generic SKIPREVENGTEST=1 SKIPQT=1 SKIPPTHREAD=1 PLATFORM=PM3GENERIC DEBUG=1"
 }
 
 termux_step_make_install() {
