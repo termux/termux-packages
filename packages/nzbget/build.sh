ae993e618c448544217ded0e656e047a69847d85
Yaksh Bariya <yakshbari4@gmail.com>
2022-02-19 15:12:13 +0530

    nzbget: link against legacy openssl-1.1

--- packages/nzbget/build.sh
+++ packages/nzbget/build.sh
@@ -3,13 +3,25 @@ TERMUX_PKG_DESCRIPTION="The most efficient usenet downloader"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=21.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/nzbget/nzbget/releases/download/v${TERMUX_PKG_VERSION}/nzbget-${TERMUX_PKG_VERSION}-src.tar.gz
 TERMUX_PKG_SHA256=4e8fc1beb80dc2af2d6a36a33a33f44dedddd4486002c644f4c4793043072025
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, libxml2, ncurses, openssl, p7zip, zlib"
+TERMUX_PKG_DEPENDS="libc++, libxml2, ncurses, openssl-1.1, p7zip, zlib"
 TERMUX_PKG_RECOMMENDS="unrar"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-openssl=$TERMUX_PREFIX
+--with-openssl-includes=$TERMUX_PREFIX/include/openssl-1.1
+--with-openssl-libraries=$TERMUX_PREFIX/lib/openssl-1.1
+"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	CFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CFLAGS"
+	CPPFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CPPFLAGS"
+	CXXFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CXXFLAGS"
+	LDFLAGS="-L$TERMUX_PREFIX/lib/openssl-1.1 -Wl,-rpath=$TERMUX_PREFIX/lib/openssl-1.1 $LDFLAGS"
+}
 
 termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
