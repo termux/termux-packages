434f774f20398107e68d224da629afab26c1506a
Yaksh Bariya <yakshbari4@gmail.com>
2022-02-19 09:27:35 +0530

    nushell: update to 0.44.0

--- packages/nushell/build.sh
+++ packages/nushell/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.nushell.sh
 TERMUX_PKG_DESCRIPTION="A new type of shell operating on structured data"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.35.0
+TERMUX_PKG_VERSION=0.44.0
 TERMUX_PKG_SRCURL=https://github.com/nushell/nushell/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2e93366a2f089bdbe0ae52eafcda5390119642c66e541b26e8eeb1ab4bc13823
+TERMUX_PKG_SHA256=94bb003fd05b604a174a686c40286fef460bc06616a7d273c387a54a07576ecc
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -14,4 +14,17 @@ termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = "i686" ]; then
 		RUSTFLAGS+=" -C link-arg=-latomic"
 	fi
+
+	mv $TERMUX_PREFIX/lib/libz.so.1{,.tmp}
+	mv $TERMUX_PREFIX/lib/libz.so{,.tmp}
+}
+
+termux_step_post_make_install() {
+	mv $TERMUX_PREFIX/lib/libz.so.1{.tmp,}
+	mv $TERMUX_PREFIX/lib/libz.so{.tmp,}
+}
+
+termux_step_post_massage() {
+	rm -f lib/libz.so.1
+	rm -f lib/libz.so
 }
