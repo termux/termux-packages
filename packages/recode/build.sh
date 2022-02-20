8740e29a3c4264c3b3716a3b23d23eefbe631ef2
s00se <95649696+s00se@users.noreply.github.com>
2022-02-20 12:04:50 -0700

    recode: bump to version 3.7.12 (#9081)

--- packages/recode/build.sh
+++ packages/recode/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://github.com/pinard/Recode"
 TERMUX_PKG_DESCRIPTION="Charset converter tool and library"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Marlin Sööse <marlin.soose@laro.se>"
-TERMUX_PKG_VERSION=3.7.9
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.7.12
 TERMUX_PKG_SRCURL=https://github.com/rrthomas/recode/releases/download/v${TERMUX_PKG_VERSION}/recode-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e4320a6b0f5cd837cdb454fb5854018ddfa970911608e1f01cc2c65f633672c4
+TERMUX_PKG_SHA256=4db1c9076f04dbaa159726f5000847e5e5a83aec8e5c64f8ca04383f6cda12d5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libiconv"
 # recode needs to be explicitly linked to avoid:
