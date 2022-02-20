1736fc472d72dc13c1dab5f6fd01dd4973fc0985
Roman Kredentser <shareed2k@gmail.com>
2022-02-20 18:15:10 +0200

    teleport-tsh: update to 8.3.1 (#9085)

--- packages/teleport-tsh/build.sh
+++ packages/teleport-tsh/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gravitational/teleport
 TERMUX_PKG_DESCRIPTION="Secure Access for Developers that doesn't get in the way"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.1.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=8.3.1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 
 termux_step_make_install() {
