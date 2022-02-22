256555adec5cbc56ecb929636209535f6939bd30
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-23 01:17:28 +0900

    libunistring: Update to 1.0

--- packages/libunistring/build.sh
+++ packages/libunistring/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libunistring/
 TERMUX_PKG_DESCRIPTION="Library providing functions for manipulating Unicode strings"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-3.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.10
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=1.0
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libunistring/libunistring-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a82e5b333339a88ea4608e4635479a1cfb2e01aafb925e1290b65710d43f610b
+TERMUX_PKG_SHA256=3c0184c0e492d7c208ce31d25dd1d2c58f0c3ed6cbbe032c5b248cddad318544
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv"
 TERMUX_PKG_BREAKS="libunistring-dev"
 TERMUX_PKG_REPLACES="libunistring-dev"
