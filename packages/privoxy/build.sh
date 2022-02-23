b79578e672f77f82dd7a337c79acde1cf382a82f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-24 05:58:10 +0900

    privoxy: Update to 3.0.33

--- packages/privoxy/build.sh
+++ packages/privoxy/build.sh
@@ -2,12 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://www.privoxy.org
 TERMUX_PKG_DESCRIPTION="Non-caching web proxy with advanced filtering capabilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.32
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.0.33
 TERMUX_PKG_SRCURL=https://www.privoxy.org/sf-download-mirror/Sources/$TERMUX_PKG_VERSION%20%28stable%29/privoxy-$TERMUX_PKG_VERSION-stable-src.tar.gz
-TERMUX_PKG_SHA256=c61de4008c62445ec18f1f270407cbf2372eaba93beaccdc9e3238bb2defeed7
-# Termux-services adds the run scripts to TERMUX_PKG_CONFFILES. Those ones can not be copied in termux_step_post_massage so setup special variable for that
-DEFAULT_CONFFILES='etc/privoxy/config etc/privoxy/match-all.action etc/privoxy/trust etc/privoxy/user.action etc/privoxy/user.filter etc/privoxy/default.action etc/privoxy/default.filter'
+TERMUX_PKG_SHA256=04b104e70dac61561b9dd110684b250fafc8c13dbe437a60fae18ddd9a881fae
+# Termux-services adds the run scripts to TERMUX_PKG_CONFFILES. Those ones can
+# not be copied in termux_step_post_massage so setup special variable for that
+DEFAULT_CONFFILES="\
+etc/privoxy/config
+etc/privoxy/match-all.action
+etc/privoxy/trust
+etc/privoxy/user.action
+etc/privoxy/user.filter
+etc/privoxy/default.action
+etc/privoxy/default.filter"
 TERMUX_PKG_CONFFILES=$DEFAULT_CONFFILES
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_pcreposix_regcomp=no
@@ -15,7 +22,11 @@ ac_cv_lib_pcreposix_regcomp=no
 "
 TERMUX_PKG_DEPENDS="pcre, libpcreposix, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_SERVICE_SCRIPT=("privoxy" "if [ -f \"$TERMUX_ANDROID_HOME/.config/privoxy/config\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.config/privoxy/config\"; else CONFIG=\"$TERMUX_PREFIX/etc/privoxy/config\"; fi\nexec privoxy --no-daemon \$CONFIG 2>&1")
+TERMUX_PKG_SERVICE_SCRIPT=("privoxy"
+"if [ -f \"$TERMUX_ANDROID_HOME/.config/privoxy/config\" ]; then \
+CONFIG=\"$TERMUX_ANDROID_HOME/.config/privoxy/config\"; else \
+CONFIG=\"$TERMUX_PREFIX/etc/privoxy/config\"; fi\n\
+exec privoxy --no-daemon \$CONFIG 2>&1")
 
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
@@ -24,13 +35,14 @@ termux_step_pre_configure() {
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
 
-    autoheader
-    autoconf
+	autoheader
+	autoconf
 }
 
 termux_step_post_massage() {
-    # copy default config files
-    for f in $DEFAULT_CONFFILES; do
-	cp "$TERMUX_PKG_SRCDIR/$(basename $f)" "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/$f"
-    done
+	# copy default config files
+	for f in $DEFAULT_CONFFILES; do
+		cp "$TERMUX_PKG_SRCDIR/$(basename $f)" \
+			"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/$f"
+	done
 }
