0de20f27babcab8f51da9d489517a0dd90ce4f4d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-23 21:53:46 +0900

    gnuplot: Update to 5.4.3

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=http://gnuplot.info/
 TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
-TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="Copyright"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.4.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=5.4.3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e57c75e1318133951d32a83bcdc4aff17fed28722c4e71f2305cfc2ae1cae7ba
-TERMUX_PKG_DEPENDS="libandroid-support, libc++, libgd, libiconv, libx11, pango, readline, zlib"
+TERMUX_PKG_SHA256=51f89bbab90f96d3543f95235368d188eb1e26eda296912256abcd3535bd4d84
+TERMUX_PKG_DEPENDS="libandroid-support, libgd, libiconv, libx11, pango, readline"
 TERMUX_PKG_BREAKS="gnuplot-x"
 TERMUX_PKG_REPLACES="gnuplot-x"
 TERMUX_PKG_HOSTBUILD=true
@@ -16,6 +16,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-lua
 --with-bitmap-terminals
 --without-latex
+ac_cv_search_iconv_open=-liconv
 "
 
 termux_step_host_build() {
