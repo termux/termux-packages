0410a2a1bd231ae68cb4ac24fdd8e0b9c30a2847
Wetitpig <winsto003@hotmail.com>
2022-02-24 14:59:38 +0800

    LLVM Tools (#7952)

---
+++ packages/libllvm/llvm-tools.subpackage.sh
@@ -0,0 +1,13 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/FileCheck
+bin/count
+bin/lli-child-target
+bin/llvm-PerfectShuffle
+bin/llvm-jitlink-executor
+bin/not
+bin/obj2yaml
+bin/yaml2obj
+bin/yaml-bench
+share/man/man1/FileCheck.1.gz
+"
+TERMUX_SUBPKG_DESCRIPTION="LLVM Development Tools"
