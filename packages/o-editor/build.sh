cc70386413621f6482a0d9de10e2e63c78caeaaa
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-28 13:11:38 +0900

    o-editor: Update to 2.48.3

--- packages/o-editor/build.sh
+++ packages/o-editor/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/xyproto/o
 TERMUX_PKG_DESCRIPTION="Small, fast and limited text editor"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Alexander F. Rødseth <xyproto@archlinux.org>"
-TERMUX_PKG_VERSION=2.47.0
+TERMUX_PKG_VERSION=2.48.3
 TERMUX_PKG_SRCURL=https://github.com/xyproto/o/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=d9e7626650c3b8be7ddc276e92a191ae38e05df5e1476d18d1862a8342b2f2ad
+TERMUX_PKG_SHA256=6c49163bf0c01e32bda22de86e0043bcec8fe3f38775a3bb71d9fda0de77738a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="o"
 TERMUX_PKG_REPLACES="o"
@@ -16,11 +16,13 @@ termux_step_make() {
 	mkdir -p "$GOPATH"/src/github.com/xyproto
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/xyproto/o
 
-	cd "$GOPATH"/src/github.com/xyproto/o
+	cd "$GOPATH"/src/github.com/xyproto/o/src
 	go build
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/xyproto/o/o
-	install -Dm600 -t "$TERMUX_PREFIX"/share/man/man1 "$TERMUX_PKG_SRCDIR"/o.1
+	install -Dm700 -t "$TERMUX_PREFIX"/bin \
+		"$GOPATH"/src/github.com/xyproto/o/src/o
+	install -Dm600 -t "$TERMUX_PREFIX"/share/man/man1 \
+		"$TERMUX_PKG_SRCDIR"/o.1
 }
