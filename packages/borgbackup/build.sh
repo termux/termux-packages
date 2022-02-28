1a4e7a3391e670da6e99a026223ffb4940352aa0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-01 05:21:08 +0900

    borgbackup: Build against openssl-1.1

--- packages/borgbackup/build.sh
+++ packages/borgbackup/build.sh
@@ -1,13 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://www.borgbackup.org/
 TERMUX_PKG_DESCRIPTION="Deduplicating and compressing backup program"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.17
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://github.com/borgbackup/borg/releases/download/${TERMUX_PKG_VERSION}/borgbackup-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7ab924fc017b24929bedceba0dcce16d56f9868bf9b5050d2aae2eb080671674
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libacl, liblz4, openssl, python, zstd"
+TERMUX_PKG_DEPENDS="libacl, liblz4, openssl-1.1, python, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 _PYTHON_VERSION=3.10
@@ -23,6 +23,12 @@ termux_step_make_install() {
 	export CPPFLAGS+=" -I${TERMUX_PREFIX}/include/python${_PYTHON_VERSION}"
 	export LDFLAGS+=" -lpython${_PYTHON_VERSION}"
 	export LDSHARED="$CC -shared"
+
+	CFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CFLAGS"
+	CPPFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CPPFLAGS"
+	CXXFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CXXFLAGS"
+	LDFLAGS="-L$TERMUX_PREFIX/lib/openssl-1.1 -Wl,-rpath=$TERMUX_PREFIX/lib/openssl-1.1 $LDFLAGS"
+
 	export BORG_OPENSSL_PREFIX=$TERMUX_PREFIX
 	export BORG_LIBLZ4_PREFIX=$TERMUX_PREFIX
 	export BORG_LIBZSTD_PREFIX=$TERMUX_PREFIX
