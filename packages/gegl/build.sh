110b379e5454968212a6d2a47fd97a6d29ffcd5f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-01 08:48:50 +0900

    gegl: Update to 0.4.36

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://gegl.org/
 TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4.34
-TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION:0:3}/gegl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ef63f0bca5b431c6119addd834ca7fbb507c900c4861c57b3667b6f4ccfcaaaa
+_MAJOR_VERSION=0.4
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.36
+TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${_MAJOR_VERSION}/gegl-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=6fd58a0cdcc7702258adaeffb573a389228ae8f0eff47578efda2309b61b2ca6
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, zlib"
+TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_BREAKS="gegl-dev"
 TERMUX_PKG_REPLACES="gegl-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false"
