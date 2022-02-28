785eab4bdc1bdf970ec52df3c98fc70bba9b0b5c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-28 11:31:17 +0900

    libprotobuf-c: Update to 1.4.0

--- packages/libprotobuf-c/build.sh
+++ packages/libprotobuf-c/build.sh
@@ -1,12 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/protobuf-c/protobuf-c
 TERMUX_PKG_DESCRIPTION="Protocol buffers C library"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.3.3
-TERMUX_PKG_REVISION=11
+TERMUX_PKG_VERSION=1.4.0
 TERMUX_PKG_SRCURL=https://github.com/protobuf-c/protobuf-c/releases/download/v${TERMUX_PKG_VERSION}/protobuf-c-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=22956606ef50c60de1fabc13a78fbc50830a0447d780467d3c519f84ad527e78
+TERMUX_PKG_SHA256=26d98ee9bf18a6eba0d3f855ddec31dbe857667d269bc0b6017335572f85bbcb
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libprotobuf, protobuf"
 TERMUX_PKG_BREAKS="libprotobuf-c-dev"
 TERMUX_PKG_REPLACES="libprotobuf-c-dev"
+
+termux_step_pre_configure() {
+	termux_setup_protobuf
+	export PROTOC=$(command -v protoc)
+}
