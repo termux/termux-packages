b889ac3c793e8677418be97910c719f261f73a3a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-28 10:14:20 +0900

    gping: Update to 1.3.0

--- packages/gping/build.sh
+++ packages/gping/build.sh
@@ -2,15 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://github.com/orf/gping
 TERMUX_PKG_DESCRIPTION="Ping, but with a graph"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION=1.2.6
+TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/orf/gping/archive/refs/tags/gping-v$TERMUX_PKG_VERSION.zip
-TERMUX_PKG_SHA256=406c4df10c99cf48b0806568e17a4b184486098fc974b0a7350190e6feb40536
+TERMUX_PKG_SHA256=f60419eb93323b66178c6d6da831726c4ea89498a4dbd8112257efeaf2586a49
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_AUTO_UPDATE_TAG_REGEXP="\d+\.\d+\.\d+"
+TERMUX_PKG_BUILD_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	mv $TERMUX_PREFIX/lib/libz.so.1{,.tmp}
+	mv $TERMUX_PREFIX/lib/libz.so{,.tmp}
 }
 
 termux_step_make() {
@@ -21,4 +25,14 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/gping
+}
+
+termux_step_post_make_install() {
+	mv $TERMUX_PREFIX/lib/libz.so.1{.tmp,}
+	mv $TERMUX_PREFIX/lib/libz.so{.tmp,}
+}
+
+termux_step_post_massage() {
+	rm -f lib/libz.so.1
+	rm -f lib/libz.so
 }
