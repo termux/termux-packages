01c7f332e4800d0fc53744b40c07e7a9bd8ac42e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-28 12:20:52 +0900

    cava: Update to 0.7.5

--- packages/cava/build.sh
+++ packages/cava/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/karlstav/cava
 TERMUX_PKG_DESCRIPTION="Console-based Audio Visualizer. Works with MPD and Pulseaudio"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
-TERMUX_PKG_VERSION=0.7.4
+TERMUX_PKG_VERSION=0.7.5
 TERMUX_PKG_SRCURL=https://github.com/karlstav/cava/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fefd3cc04d41b03ca416630cafadbfda6c75e2ca0869da1f03963dcb13e1ecb7
+TERMUX_PKG_SHA256=863578e7d091735fd9c673239058515300faa79ff76304849a93c37ae3bd93cf
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="ncurses,fftw,pulseaudio"
+TERMUX_PKG_DEPENDS="fftw, libiniparser, ncurses, pulseaudio"
 TERMUX_PKG_BUILD_DEPENDS="libtool"
 TERMUX_PKG_BUILD_IN_SRC=true
 
