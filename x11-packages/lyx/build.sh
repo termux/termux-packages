b9e9dea016f1d6eb9944d323304fc6596d24c6bc
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-01 23:27:55 +0900

    boost: Make revdeps build-depend on boost-headers
    %ci:no-build

--- x11-packages/lyx/build.sh
+++ x11-packages/lyx/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL="https://ftp.lip6.fr/pub/lyx/stable/2.3.x/lyx-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=c6bed2633419898e01a7cc350310d7d934bf591cab7259fc2581aa4c00eafa78
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, zlib, imagemagick, ghostscript, texlive-bin, hunspell"
-TERMUX_PKG_BUILD_DEPENDS="boost, qt5-qtbase-cross-tools"
+TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-build-type=rel
 --enable-qt5
