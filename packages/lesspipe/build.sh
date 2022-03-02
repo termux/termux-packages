8163a49db72d2d41fb167cdfea8cbe4b30ae6699
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-02 14:21:45 +0900

    lesspipe: Do not try to invoke `locale` command

--- packages/lesspipe/build.sh
+++ packages/lesspipe/build.sh
@@ -3,11 +3,13 @@ TERMUX_PKG_DESCRIPTION="An input filter for the pager less"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.04
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/wofr06/lesspipe/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b65ee79b7b51510e6f2097a22d3c7985805a53d9e33b0afdfe129f2a1f89cc23
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="less"
 TERMUX_PKG_SUGGESTS="imagemagick, p7zip, unrar, unzip"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
