9daf21c2e26517cdac1b88ace72ba4100816e8aa
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-04 05:11:05 +0900

    mg: Update to 20220212

--- packages/mg/build.sh
+++ packages/mg/build.sh
@@ -2,14 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://github.com/hboetes/mg
 TERMUX_PKG_DESCRIPTION="microscopic GNU Emacs-style editor"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20180927
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=20220212
 TERMUX_PKG_SRCURL=https://github.com/hboetes/mg/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=fbb09729ea00fe42dcdbc96ac7fc1d2b89eac651dec49e4e7af52fad4f5788f6
+TERMUX_PKG_SHA256=dd94e6c43bf08f35a7342c416c386529c58d756654720f1797c5708ebcd5eb73
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_AUTO_UPDATE_TAG_REGEXP="\d{8}"
 TERMUX_PKG_DEPENDS="libbsd, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	rm -f CMakeLists.txt
+}
 
 termux_step_pre_configure() {
 	CFLAGS+=" $CPPFLAGS -fcommon"
