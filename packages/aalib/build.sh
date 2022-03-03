11221d57818f4b0f3b020a14816cb010ef0bf1de
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-04 08:49:16 +0900

    aalib: Build shared libs

--- packages/aalib/build.sh
+++ packages/aalib/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/aa-project/
 TERMUX_PKG_DESCRIPTION="A portable ASCII art graphic library"
-TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4rc5
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/aa-project/aalib-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fbddda9230cf6ee2a4f5706b4b11e2190ae45f5eda1f0409dc4f99b35e0a70ee
-TERMUX_PKG_DEPENDS="libx11, ncurses"
+TERMUX_PKG_DEPENDS="ncurses"
