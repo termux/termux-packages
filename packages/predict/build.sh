05997ce6f6b4eaa9511ff399be46ba3909da92ee
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-03-04 21:21:03 +0800

    Add Packages:predict

---
+++ packages/predict/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://qsl.net/kd2bd/predict.html
+TERMUX_PKG_DESCRIPTION="Satellite tracking, orbital prediction, open-source software."
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.2.7
+TERMUX_PKG_SRCURL=https://qsl.net/kd2bd/predict-${TERMUX_PKG_VERSION}-termux.tar.gz
+TERMUX_PKG_SHA256=b5e454c13818a4c2dcb1e2146352f41d806a76cfa85410cb5e93d02a458f60a9
