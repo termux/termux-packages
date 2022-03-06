600463a343ad517dc493731534ee0ab0de650cdf
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-03-06 11:38:51 +0800

    Update build.sh

--- packages/predict/build.sh
+++ packages/predict/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2.7
 TERMUX_PKG_SRCURL=https://qsl.net/kd2bd/predict-${TERMUX_PKG_VERSION}-termux.tar.gz
 TERMUX_PKG_SHA256=b5e454c13818a4c2dcb1e2146352f41d806a76cfa85410cb5e93d02a458f60a9
-TERMUX_PKG_DEPENDS="ncurses,man,play-audio,termux-api,wget"
+TERMUX_PKG_DEPENDS="libllvm,ncurses,man,play-audio,termux-api,wget"
 
 termux_step_pre_configure() {
     mkdir -p $TERMUX_PREFIX/opt/predict
