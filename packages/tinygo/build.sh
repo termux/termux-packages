117b7fd83a0d5bfb4d770082ae44b2933c6854e6
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-03-06 19:56:10 +0800

    fix depends

--- packages/tinygo/build.sh
+++ packages/tinygo/build.sh
@@ -7,8 +7,10 @@ TERMUX_PKG_SRCURL=https://github.com/tinygo-org/tinygo/archive/v${TERMUX_PKG_VER
 TERMUX_PKG_SHA256=d8456c82682d1ece1285887573eb7c421a47e87ffe59ed76ecd87af6de3ba886
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="golang,libllvm,cmake,ninja,build-essential,git,python"
+TERMUX_PKG_BUILD_DEPENDS="ninja,cmake,build-essential"
+TERMUX_PKG_DEPENDS="golang,libllvm,git,python"
 TERMUX_PKG_FORCE_CMAKE=true
+
 termux_step_pre_configure() {
 	termux_setup_golang
 	export CGO_ENABLED=1
