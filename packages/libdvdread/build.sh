425f13873c85ef37683f4673cad0ad7b2e88abca
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-07 08:49:02 +0900

    new package: libdvdread

---
+++ packages/libdvdread/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://code.videolan.org/videolan/libdvdread
+TERMUX_PKG_DESCRIPTION="A library that allows easy use of sophisticated DVD navigation features"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=6.1.2
+TERMUX_PKG_SRCURL=https://downloads.videolan.org/pub/videolan/libdvdread/${TERMUX_PKG_VERSION}/libdvdread-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=cc190f553758ced7571859e301f802cb4821f164d02bfacfd320c14a4e0da763
