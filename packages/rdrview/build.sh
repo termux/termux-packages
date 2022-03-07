f240349b97b7187aabe6be3f9812fe078533e2a2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-07 10:55:24 +0900

    rdrview: Add seccomp rules

--- packages/rdrview/build.sh
+++ packages/rdrview/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _COMMIT=9bde19f9e53562790b363bb2e3b15707c8c67676
 TERMUX_PKG_VERSION=2021.09.12
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/eafer/rdrview.git
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libcurl, libiconv, libseccomp, libxml2"
@@ -12,6 +13,13 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_post_get_source() {
 	git fetch --unshallow
 	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
+		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
 }
 
 termux_step_pre_configure() {
