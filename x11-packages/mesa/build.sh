4b5e7686915cf1d954c7dbc296fad04027a1d1e9
Henrik Grimler <grimler@termux.org>
2022-03-09 09:29:57 +0100

    mesa: conflict with ndk-sysroot 23b-5 and older
    ndk-sysroot as well as mesa contained the header KHR/khrplatform.h. It
    was removed from ndk-sysroot in 23b-6.  Mesa 21.3.7 also depends on
    libexpat.
    
    Fixes https://github.com/termux/x11-packages/issues/630.

--- x11-packages/mesa/build.sh
+++ x11-packages/mesa/build.sh
@@ -4,11 +4,12 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=21.3.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.mesa3d.org/mesa-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b4fa9db7aa61bf209ef0b40bef83080999d86ad98df8b8b4fada7c128a1efc3d
-TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libx11, libxext, zlib"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libx11, libxext, zlib, libexpat"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_CONFLICTS="libmesa"
+TERMUX_PKG_CONFLICTS="libmesa, ndk-sysroot (<< 23b-6)"
 TERMUX_PKG_REPLACES="libmesa"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
