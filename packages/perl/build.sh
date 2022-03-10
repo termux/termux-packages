f7878010074ade49759efea52a3b6c56919c8067
Henrik Grimler <grimler@termux.org>
2022-03-10 10:37:46 +0100

    perl: define ranlib as well when configuring
    Otherwise we get warnings like:
    
      CONFIG key 'ranlib' does not exist in Config.pm
      Use of uninitialized value $m[72] in join or string at /home/builder/.termux-build/perl/src/cpan/ExtUtils-MakeMaker/lib/ExtUtils/MM_Unix.pm line 365.
    
    which prevents installation/compilation of some perl modules.

--- packages/perl/build.sh
+++ packages/perl/build.sh
@@ -11,7 +11,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # - subversion
 TERMUX_PKG_VERSION=(5.34.0
                     1.3.6)
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SHA256=(551efc818b968b05216024fb0b727ef2ad4c100f8cb6b43fab615fa78ae5be9a
                    4010f41870d64e3957b4b8ce70ebba10a7c4a3e86c5551acb4099c3fcbb37ce5)
 TERMUX_PKG_SRCURL=(http://www.cpan.org/src/5.0/perl-${TERMUX_PKG_VERSION}.tar.gz
@@ -61,11 +61,12 @@ termux_step_configure() {
 		cd $TERMUX_PKG_BUILDDIR
 		$TERMUX_PKG_SRCDIR/configure \
 			--target=$TERMUX_HOST_PLATFORM \
+			--with-cc="$ORIG_CC" \
+			--with-ranlib="$ORIG_RANLIB" \
 			-Dosname=android \
 			-Dsysroot=$TERMUX_STANDALONE_TOOLCHAIN/sysroot \
 			-Dprefix=$TERMUX_PREFIX \
 			-Dsh=$TERMUX_PREFIX/bin/sh \
-			-Dcc="$ORIG_CC" \
 			-Dld="$ORIG_CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" \
 			-Dar="$ORIG_AR" \
 			-Duseshrplib \
