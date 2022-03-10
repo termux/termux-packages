e408cbcacaa2ae2a8275f35b8e6e5f0da757e020
Henrik Grimler <grimler@termux.org>
2022-03-10 22:03:40 +0100

    cloudflared: update to 2022.3.1

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.3.0
+TERMUX_PKG_VERSION=2022.3.1
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=619426d20457e3f40b3e604d528d645df123042d6f9edcf6fc98d00433f3d094
+TERMUX_PKG_SHA256=e3459bc83d9749d89d93e6ab2dcbc51872568d1fb144d7ab7d3656b29785e364
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
