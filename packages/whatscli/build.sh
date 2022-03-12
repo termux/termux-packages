c131e7e2724b1a710e09cfaecf85e5b6bf817bf2
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-03-12 11:08:00 +0800

    Fix LICENSE File

--- packages/whatscli/build.sh
+++ packages/whatscli/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/normen/whatscli
 TERMUX_PKG_DESCRIPTION="A command line interface for WhatsApp"
 TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="README.md"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.11
 TERMUX_PKG_SRCURL=https://github.com/normen/whatscli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
