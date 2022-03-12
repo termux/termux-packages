27f5a32f7e1edd3ea277c24a620c162e8231055a
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-03-12 10:59:46 +0800

    Create build.sh

---
+++ packages/whatscli/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/normen/whatscli
+TERMUX_PKG_DESCRIPTION="A command line interface for WhatsApp"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.0.11
+TERMUX_PKG_SRCURL=https://github.com/normen/whatscli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b4b2ceb1c4babe5fc53284714aebf102477543df247e2a25b533e4271d0622d7
+termux_step_pre_configure() {
+	termux_setup_golang
+}
