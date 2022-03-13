b8723bc0890593091a6ea2c22b4d4bcedfd054f3
Henrik Grimler <grimler@termux.org>
2022-03-13 08:01:40 +0100

    exiftool: update to 12.40

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://exiftool.org/
 TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta information in a wide variety of files."
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=12.38
+TERMUX_PKG_VERSION=12.40
 TERMUX_PKG_SRCURL="http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=dcbb9ac3b913636ff95542022fcf036e8e92c0cc2e518f9b06d21b44bb5ecddf
+TERMUX_PKG_SHA256=7ae3f6e1183926aff8a0440eb8071bb5b2a335fc1522597f109780ab16617f5f
 TERMUX_PKG_DEPENDS="perl"
 
 termux_step_make_install() {
