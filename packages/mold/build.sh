77a4a9d3c8a42593e39091c8c337b29f22e251c0
Jia Yuan Lo <jylo06g@gmail.com>
2022-03-13 21:02:30 +0800

    mold: update to 1.1.1

--- packages/mold/build.sh
+++ packages/mold/build.sh
@@ -2,18 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/rui314/mold
 TERMUX_PKG_DESCRIPTION="mold: A Modern Linker"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=ce744aeca423954c9152a6c1c2185f149bf50ad8
-TERMUX_PKG_VERSION=1.0.3.20220212
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/rui314/mold.git
-TERMUX_PKG_GIT_BRANCH=main
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SRCURL=https://github.com/rui314/mold/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=47c5ddfe60beffc01da954191c819d78924e4d1eb96aeebfa24e1862cb3a33f9
 TERMUX_PKG_DEPENDS="libc++, openssl, zlib, libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_post_get_source() {
-	git fetch --unshallow
-	git reset --hard ${_COMMIT}
-}
 
 termux_step_pre_configure() {
 	# onetbb use cmake
@@ -35,7 +28,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	make -j 1 install \
+	make -j ${TERMUX_MAKE_PROCESSES} install \
 		PREFIX="${TERMUX_PREFIX}" \
 		CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include" \
 		CXXFLAGS="${CXXFLAGS}" \
