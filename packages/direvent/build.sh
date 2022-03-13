0448c65aa09e3c3b02f6bba4a0981770644b85de
Henrik Grimler <grimler@termux.org>
2022-03-13 07:40:11 +0100

    direvent: update to 5.3

--- packages/direvent/build.sh
+++ packages/direvent/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org.ua/software/direvent/
 TERMUX_PKG_DESCRIPTION="Monitor of events in file system directories"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.3
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/direvent/direvent-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=239822cdda9ecbbbc41a69181b34505b2d3badd4df5367e765a0ceb002883b55
+TERMUX_PKG_SHA256=9405a8a77da49fe92bbe4af18bf925ff91f6d3374c10b7d700a031bacb94c497
 TERMUX_PKG_DEPENDS="libandroid-glob"
 
 termux_step_pre_configure() {
