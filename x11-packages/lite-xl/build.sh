94efd571b556aeb8f8ab19cedef9480320ced479
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-13 15:26:54 +0900

    sdl2: Revbump revdeps

--- x11-packages/lite-xl/build.sh
+++ x11-packages/lite-xl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A lightweight text editor written in Lua"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@leapofazzam123"
 TERMUX_PKG_VERSION=2.0.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lite-xl/lite-xl/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=6c8a4ea284f102f772e3aff583236e89d5b1171664526dd501000b681ae5c4e2
 TERMUX_PKG_DEPENDS="sdl2, freetype, liblua52"
--- x11-packages/milkytracker/build.sh
+++ x11-packages/milkytracker/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="music creation tool inspired by Fast Tracker 2"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.03.00
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/milkytracker/MilkyTracker/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=72d5357e303380b52383b66b51f944a77cd77e2b3bfeb227d87cc0e72ab292f7
 TERMUX_PKG_DEPENDS="sdl2"
