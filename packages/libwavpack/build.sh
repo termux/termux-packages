c404a11983cbab60ddce4a5c4e06bb310af057e2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-15 14:23:19 +0900

    libwavpack: Add security patch
    from https://github.com/dbry/WavPack/commit/773f9d0803c6888ae7d5391878d7337f24216f4a.

--- packages/libwavpack/build.sh
+++ packages/libwavpack/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A completely open audio compression format providing los
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.4.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dbry/WavPack/releases/download/${TERMUX_PKG_VERSION}/wavpack-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4bde6a6b2a86614a6bd2579e60dcc974e2c8f93608d2281110a717c1b3c28b79
-TERMUX_PKG_DEPENDS="libandroid-glob, libiconv"
+TERMUX_PKG_DEPENDS="libandroid-glob"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
