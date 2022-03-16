923e3a8f04281b996922791250e3421589c77ac8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-16 22:49:54 +0900

    Revert "cloudflared: Bump revision"
    This reverts commit be0273d1d6870f662222d38eee9c0b9c463dbdc0.

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudfl
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2022.3.1
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e3459bc83d9749d89d93e6ab2dcbc51872568d1fb144d7ab7d3656b29785e364
 TERMUX_PKG_BUILD_IN_SRC=true
