03e46d79b6937637ed1578b8c509a7aa5d6e3321
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-16 20:44:09 +0900

    croc: Bump revision

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to ano
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=9.5.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
 TERMUX_PKG_SHA256=cccf83c4d52bbbd22091f11046c2e4b7f5117cd676064b28a0ccde9bc83e62ac
 TERMUX_PKG_AUTO_UPDATE=true
