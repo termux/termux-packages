71ad1f296e07b47b652705ff3f2b7a50ec149750
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-16 21:12:17 +0900

    sops: Bump revision

--- packages/sops/build.sh
+++ packages/sops/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simple and flexible tool for managing secrets"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="Philipp Schmitt @pschmitt"
 TERMUX_PKG_VERSION=3.7.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/mozilla/sops/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=536ee140d888b53b71c1e8edd669f4c11bc573428983fbea644fbbfcd7d7079a
 
