2abf59a564ad639a413ac4261d99eea6c6d70015
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-16 20:54:13 +0900

    git-sizer: Bump revision

--- packages/git-sizer/build.sh
+++ packages/git-sizer/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Compute various size metrics for a Git repository"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
 TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/github/git-sizer/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=07a5ac5f30401a17d164a6be8d52d3d474ee9c3fb7f60fd83a617af9f7e902bb
 TERMUX_PKG_DEPENDS="git"
