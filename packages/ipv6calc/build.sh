5b5d657a89958c85cad2029493823bedc0823df7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-17 15:40:24 +0900

    new package: ipv6calc

---
+++ packages/ipv6calc/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://www.deepspace6.net/projects/ipv6calc.html
+TERMUX_PKG_DESCRIPTION="Does some format changes and calculations of IPv6 addresses"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=4.0.1
+TERMUX_PKG_SRCURL=https://github.com/pbiering/ipv6calc/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0de7dbe66c0e3602becc18bb8f69a278a02eeaf5d58779cd1c0e0069eb80a7e6
+TERMUX_PKG_DEPENDS="openssl, perl"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-external-db=$TERMUX_PREFIX/share/ipv6calc/db
+--with-dbip-db=$TERMUX_PREFIX/share/DBIP
+--with-ip2location-db=$TERMUX_PREFIX/share/IP2Location
+"
+TERMUX_PKG_EXTRA_MAKE_ARGS="exec_prefix=$TERMUX_PREFIX"
+
+termux_step_pre_configure() {
+	CFLAGS+=" $CPPFLAGS"
+	LDFLAGS+=" -lcrypto"
+}
