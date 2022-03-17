f2bde3cc6453748db79f662a5bce3f6a549fce51
Oskar <nivua1fn@duck.com>
2022-03-17 20:27:08 +0000

    bump cloudflared to 2022.3.2

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,17 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.3.1
+TERMUX_PKG_VERSION=2022.3.2
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e3459bc83d9749d89d93e6ab2dcbc51872568d1fb144d7ab7d3656b29785e364
+TERMUX_PKG_SHA256=58ed53896b7ee2cc4ac84ec2d6f47423476b4292a56b5379aa9715103a4cb226
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_golang
-
-	go mod init || :
-	go mod tidy
-	go mod vendor
 
 	local _VERSION=$TERMUX_PKG_VERSION
 	local _DATE=$(date -u '+%Y-%m-%d-%H%M UTC')
