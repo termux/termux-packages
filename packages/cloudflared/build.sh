0eaabd0927aaba38d2d43b06548991e125e50131
Oskar <nivua1fn@duck.com>
2022-03-18 19:30:34 +0000

    bump cloudflared to 2022.3.3

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,17 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.3.2
+TERMUX_PKG_VERSION=2022.3.3
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=58ed53896b7ee2cc4ac84ec2d6f47423476b4292a56b5379aa9715103a4cb226
+TERMUX_PKG_SHA256=da352ea84032f8d1143b239041e8092eff0a35a4966b63cf46d073522dc37672
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_golang
 
-	local _VERSION=$TERMUX_PKG_VERSION
 	local _DATE=$(date -u '+%Y.%m.%d-%H:%M UTC')
-	go build -v -ldflags "-X \"main.Version=$_VERSION\" -X \"main.BuildTime=$_DATE\"" \
+	go build -v -ldflags "-X \"main.Version=$TERMUX_PKG_VERSION\" -X \"main.BuildTime=$_DATE\"" \
 		./cmd/cloudflared
 }
 
