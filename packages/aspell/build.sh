4dc3247a470a270aff5b7c4308bb93db95b67ee5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 16:02:24 +0900

    aspell: Depend on `libc++`

--- packages/aspell/build.sh
+++ packages/aspell/build.sh
@@ -3,8 +3,13 @@ TERMUX_PKG_DESCRIPTION="A free and open source spell checker designed to replace
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.60.8
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/aspell/aspell-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f9b77e515334a751b2e60daab5db23499e26c9209f5e7b7443b05235ad0226f2
+TERMUX_PKG_DEPENDS="libc++"
 # To use the same compiled dictionaries on every platform:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-32-bit-hash-fun"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
