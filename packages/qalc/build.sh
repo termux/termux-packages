2299baeae4262410784a3cbd51c70aa9d93065c5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 22:32:08 +0900

    libmpfr: Rebuild with NDK r23b
    to stop leaking compiler-rt builtin functions out of shared libraries.
    
    Also rebuild some revdeps to get rid of reliance on leaked builtins.

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Powerful and easy to use command line calculator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.1.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v$TERMUX_PKG_VERSION/libqalculate-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d943e5285bdc0b3cd77b8f7a10391d7c753fc19b0ddd48e5d4179decf709d6ff
 TERMUX_PKG_AUTO_UPDATE=true
@@ -10,3 +11,7 @@ TERMUX_PKG_DEPENDS="libc++, libiconv, libcurl, libmpfr, libxml2, readline, libgm
 TERMUX_PKG_BREAKS="qalc-dev"
 TERMUX_PKG_REPLACES="qalc-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-icu"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
