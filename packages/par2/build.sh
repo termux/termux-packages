032bab77569e6ed6481fd0c2bf9355156e668387
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 16:11:28 +0900

    par2: Depend on `libc++`

--- packages/par2/build.sh
+++ packages/par2/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="par2cmdline is a PAR 2.0 compatible file verification an
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_VERSION=0.8.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/Parchive/par2cmdline/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=529f85857ec44e501cd8d95b0c8caf47477d7daa5bfb989e422c800bb71b689a
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
