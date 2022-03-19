2775376aa229e5b1dc17c52f271726dfd86d3706
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 16:04:10 +0900

    gengetopt: Depend on `libc++`

--- packages/gengetopt/build.sh
+++ packages/gengetopt/build.sh
@@ -3,5 +3,7 @@ TERMUX_PKG_DESCRIPTION="gengetopt is a tool to write command line option parsing
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.23
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/gengetopt/gengetopt-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b941aec9011864978dd7fdeb052b1943535824169d2aa2b0e7eae9ab807584ac
+TERMUX_PKG_DEPENDS="libc++"
