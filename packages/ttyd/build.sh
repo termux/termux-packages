0f22413ac0a0eb9e2e926439b7f3dbc552d43ba8
Yaksh Bariya <yakshbari4@gmail.com>
2022-03-19 20:30:24 +0530

    ttyd: depend on openssl

--- packages/ttyd/build.sh
+++ packages/ttyd/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Command-line tool for sharing terminal over the web"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.6.3
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://github.com/tsl0922/ttyd/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=1116419527edfe73717b71407fb6e06f46098fc8a8e6b0bb778c4c75dc9f64b9
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="json-c, libuv, libwebsockets, zlib, libcap"
+TERMUX_PKG_DEPENDS="json-c, libcap, libuv, libwebsockets, openssl, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_XXD=$TERMUX_PKG_TMPDIR/xxd"
 
 termux_step_pre_configure() {
