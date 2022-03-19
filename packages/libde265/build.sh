a8e995291c65f3860840bb9015eb3c902068e31a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 16:09:26 +0900

    libde265: Depend on `libc++`

--- packages/libde265/build.sh
+++ packages/libde265/build.sh
@@ -1,14 +1,18 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/strukturag/libde265
 TERMUX_PKG_DESCRIPTION="H.265/HEVC video stream decoder library"
-TERMUX_PKG_LICENSE="LGPL-3.0"
+TERMUX_PKG_LICENSE="LGPL-3.0, MIT"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.8
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/strukturag/libde265/releases/download/v$TERMUX_PKG_VERSION/libde265-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=24c791dd334fa521762320ff54f0febfd3c09fc978880a8c5fbc40a88f21d905
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sherlock265 --disable-arm --disable-encoder"
 
 termux_step_pre_configure() {
-    autoreconf -fi
+	autoreconf -fi
+
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
