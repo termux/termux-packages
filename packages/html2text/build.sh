fa1034759b7a0a4b42fbc660f6cb1856634f5de3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 16:04:23 +0900

    html2text: Depend on `libc++`

--- packages/html2text/build.sh
+++ packages/html2text/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Utility that converts HTML documents into plain text"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1:2.0.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/grobian/html2text/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=061125bfac658c6d89fa55e9519d90c5eeb3ba97b2105748ee62f3a3fa2449de
-TERMUX_PKG_DEPENDS="libiconv"
+TERMUX_PKG_DEPENDS="libc++, libiconv"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
