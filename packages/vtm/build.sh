5b3c02b435cc84c59f2e2550136e29393cb49a85
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 14:48:29 +0900

    vtm: Depend on `libc++`

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-p
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.15
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fa18ae2f1eee9615c6e0df5be4b248fb15268c5e7401380f129ccee8cb9254f0
-TERMUX_PKG_DEPENDS="libandroid-spawn"
+TERMUX_PKG_DEPENDS="libandroid-spawn, libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
