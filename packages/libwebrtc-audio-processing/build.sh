4c88cce6f611f88a533cda9497bb849a37215b54
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-19 16:10:36 +0900

    libwebrtc-audio-processing: Depend on `libc++`

--- packages/libwebrtc-audio-processing/build.sh
+++ packages/libwebrtc-audio-processing/build.sh
@@ -3,6 +3,12 @@ TERMUX_PKG_DESCRIPTION="A library containing the AudioProcessing module from the
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Patrick Gaskin @pgaskin"
 TERMUX_PKG_VERSION=0.3.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://freedesktop.org/software/pulseaudio/webrtc-audio-processing/webrtc-audio-processing-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4dabdd0789acd117d88d53f0a793cf4e906c6a93ee9aa6975ec928eafbf1dfe5
+TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
