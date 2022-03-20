e6e088ac0d4b225ffd21ac1b72fd31b3ac12c524
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 07:31:42 +0900

    gnunet: Get rid of references to leaked builtins
    out of previous `libmicrohttpd` shared libraries.

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gnunet.org
 TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_SRCURL=https://git.gnunet.org/git/gnunet.git
 TERMUX_PKG_VERSION=0.16.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_DEPENDS="gnurl, libgcrypt, libgmp, libidn, libjansson, libltdl, libmicrohttpd, libsqlite, libunistring, libsodium"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_have_decl_struct_in6_ifreq=yes"
