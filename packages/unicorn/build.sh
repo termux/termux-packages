9f2ab9d55d1525b233c5683d4cfb78098131e717
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 07:26:01 +0900

    unicorn: Rebuild with NDK r23b

--- packages/unicorn/build.sh
+++ packages/unicorn/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Unicorn is a lightweight multi-platform, multi-architect
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/unicorn-engine/unicorn/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=64fba177dec64baf3f11c046fbb70e91483e029793ec6a3e43b028ef14dc0d65
 TERMUX_PKG_AUTO_UPDATE=true
