3b4209059606964f11ae241ad05183e1c9cc8728
Yonle <yonle@duck.com>
2022-03-20 19:11:05 +0700

    fluidsynth: update to 2.2.6

--- packages/fluidsynth/build.sh
+++ packages/fluidsynth/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/FluidSynth/fluidsynth
 TERMUX_PKG_DESCRIPTION="Software synthesizer based on the SoundFont 2 specifications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Yonle <yonle@protonmail.com>"
-TERMUX_PKG_VERSION=2.2.5
+TERMUX_PKG_VERSION=2.2.6
 TERMUX_PKG_SRCURL=https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9037e703617f91c4c36039a5059e0f624164799d856af715bcd8a23c07ba03b8
+TERMUX_PKG_SHA256=ca90fe675cacd9a7b442662783c4e7fa0e1fd638b28d64105a4e3fe0f618d20f
 TERMUX_PKG_DEPENDS="glib, pulseaudio"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DLIB_INSTALL_DIR=${TERMUX_PREFIX}/lib"
 
