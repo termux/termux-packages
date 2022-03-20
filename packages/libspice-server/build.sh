bf5b4328b3bccaa57e7c6fd07a4d86d9768d0dd6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 00:08:09 +0900

    libspice-server: Get rid of references to leaked builtins

--- packages/libspice-server/build.sh
+++ packages/libspice-server/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Implements the server side of the SPICE protocol"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.15.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://www.spice-space.org/download/releases/spice-server/spice-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=b320cf8f4bd2852750acb703c15b72856027e5a8554f8217dfbb3cc09deba0f5
 TERMUX_PKG_DEPENDS="glib, libjpeg-turbo, gstreamer, gst-plugins-base, libsasl, libspice-protocol, liborc, openssl, libopus, libpixman, liblz4, zlib, libiconv"
@@ -13,6 +13,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-tests
 "
 termux_step_post_make_install() {
-        ln -sfr "${TERMUX_PREFIX}"/lib/libspice-server.so \
-                "${TERMUX_PREFIX}"/lib/libspice-server.so.1
+	ln -sf libspice-server.so "${TERMUX_PREFIX}"/lib/libspice-server.so.1
 }
