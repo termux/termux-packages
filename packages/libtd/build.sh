d4be246f4a066cf09a55b524f300916ac56cbadf
aicynide <97217381+aicynide@users.noreply.github.com>
2022-03-20 12:01:42 +0530

    Update build.sh

--- packages/libtd/build.sh
+++ packages/libtd/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://core.telegram.org/tdlib/
 TERMUX_PKG_DESCRIPTION="Library for building Telegram clients"
 TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.8.0
-TERMUX_PKG_SRCURL=https://github.com/tdlib/td/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=30d560205fe82fb811cd57a8fcbc7ac853a5b6195e9cb9e6ff142f5e2d8be217
+_COMMIT=1d15bafb5003cf36dbfdd4c839dbcc56368fb48e
+TERMUX_PKG_VERSION="1.8.2-6529-g${_COMMIT:0:8}"
+TERMUX_PKG_SRCURL="https://github.com/tdlib/td.git"
+TERMUX_PKG_GIT_BRANCH=${_COMMIT}
 TERMUX_PKG_DEPENDS="readline, openssl (>= 1.1.1), zlib"
 TERMUX_PKG_HOSTBUILD=true
 
