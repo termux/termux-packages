d790428a78a67a12e3e905a9d5d79fce3979e16a
Yonle <yonle@duck.com>
2022-03-20 19:34:29 +0700

    fluidsynth: Update maintainer email address
    Signed-off-by: Yonle <yonle@duck.com>

--- packages/fluidsynth/build.sh
+++ packages/fluidsynth/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/FluidSynth/fluidsynth
 TERMUX_PKG_DESCRIPTION="Software synthesizer based on the SoundFont 2 specifications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_MAINTAINER="Yonle <yonle@protonmail.com>"
+TERMUX_PKG_MAINTAINER="Yonle <yonle@duck.com>"
 TERMUX_PKG_VERSION=2.2.6
 TERMUX_PKG_SRCURL=https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ca90fe675cacd9a7b442662783c4e7fa0e1fd638b28d64105a4e3fe0f618d20f
