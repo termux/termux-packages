986cc59704ed8210f3eb9d200b499922ee8089b3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-20 22:52:01 +0900

    fontconfig: Rebuild with NDK r23b
    to stop leaking builtin functions out of shared libraries.

--- packages/fontconfig/build.sh
+++ packages/fontconfig/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/fontconfig/
 TERMUX_PKG_DESCRIPTION="Library for configuring and customizing font access"
-TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.13.1
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/fontconfig/release/fontconfig-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=f655dd2a986d7aa97e052261b36aa67b0a64989496361eca8d604e6414006741
 TERMUX_PKG_DEPENDS="freetype, libxml2, libpng, libuuid, ttf-dejavu, zlib"
