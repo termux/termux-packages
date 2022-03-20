fcf49e95e4fc8465f261e0eedead394c3c0c7ea7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 07:28:11 +0900

    libmicrohttpd: Rebuild with NDK r23b

--- packages/libmicrohttpd/build.sh
+++ packages/libmicrohttpd/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/libmicrohttpd/
 TERMUX_PKG_DESCRIPTION="A small C library that is supposed to make it easy to run an HTTP server as part of another application"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.9.73
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=a37b2f1b88fd1bfe74109586be463a434d34e773530fc2a74364cfcf734c032e
 TERMUX_PKG_DEPENDS="libgnutls"
