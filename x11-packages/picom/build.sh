98df8df4493a0de34a2b418b88bf9515f50052cb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 00:39:15 +0900

    picom: Get rid of references to leaked builtins

--- x11-packages/picom/build.sh
+++ x11-packages/picom/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT, MPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSES/MIT, LICENSES/MPL-2.0"
 TERMUX_PKG_MAINTAINER="Rafael Kitover <rkitover@gmail.com>"
 TERMUX_PKG_VERSION=9.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/yshui/picom/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8700ac71bd496c91262c8576e29cb3aecf2b4ef48c04394a929509d3cb37b87d
 TERMUX_PKG_DEPENDS="libx11, libxcb, xcb-util, xcb-util-image, xcb-util-renderutil, libxext, xorgproto, libpixman, dbus, libconfig, mesa, pcre, libev, uthash"
