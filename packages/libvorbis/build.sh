dcc4ba5ecf8cd218d3305aa89b753c772b5c5513
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 23:08:44 +0900

    libvorbis: Rebuild with NDK r23b

--- packages/libvorbis/build.sh
+++ packages/libvorbis/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for using the Ogg Vorbis compressed audio format
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/libvorbis-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b33cc4934322bcbf6efcbacf49e3ca01aadbea4114ec9589d1b1e9d20f72954b
 TERMUX_PKG_DEPENDS="libogg"
