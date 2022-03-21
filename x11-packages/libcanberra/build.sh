b9cc80f7f1ff7216a5f3fd2691a1cec758d7d0f6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 20:29:30 +0900

    libcanberra: Get rid of references to leaked builtins

--- x11-packages/libcanberra/build.sh
+++ x11-packages/libcanberra/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://0pointer.de/lennart/projects/libcanberra/
 TERMUX_PKG_DESCRIPTION="libcanberra defines a simple abstract interface for playing event sounds."
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_VERSION=0.30
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://0pointer.de/lennart/projects/libcanberra/libcanberra-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=c2b671e67e0c288a69fc33dc1b6f1b534d07882c2aceed37004bf48c601afa72
 TERMUX_PKG_DEPENDS="libltdl, libvorbis, gstreamer, gtk3, pulseaudio, zenity"
