67ee795f4c21b3a99e5bc17a687c7b91c1df964a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 20:26:02 +0900

    tint2: Get rid of references to leaked builtins

--- x11-packages/tint2/build.sh
+++ x11-packages/tint2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lightweight panel, Highly customizable"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=17.0.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.com/o9000/tint2/-/archive/${TERMUX_PKG_VERSION}/tint2-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f87f147e176d32f31f12fc1737f75f54c4c6f77961c6a2615ef9d64cb29ce24c
 TERMUX_PKG_DEPENDS="xorg-xrandr, libxinerama, libx11, xorgproto, libandroid-glob, libandroid-shmem, pango, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm, pulseaudio, libxcomposite, libxdamage, gtk2, imlib2, libandroid-wordexp, librsvg, startup-notification, libcairo"
