5a555a88fabaebf7e14a075d727f0039c6532843
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-22 02:53:33 +0900

    libxapian: Link against compiler-rt builtins

--- packages/libxapian/build.sh
+++ packages/libxapian/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Xapian search engine library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4.18
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://oligarchy.co.uk/xapian/${TERMUX_PKG_VERSION}/xapian-core-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=196ddbb4ad10450100f0991a599e4ed944cbad92e4a6fe813be6dce160244b77
 # Note that we cannot /proc/sys/kernel/random/uuid (permission denied on
@@ -11,3 +11,7 @@ TERMUX_PKG_SHA256=196ddbb4ad10450100f0991a599e4ed944cbad92e4a6fe813be6dce160244b
 TERMUX_PKG_DEPENDS="libc++, libuuid, zlib"
 TERMUX_PKG_BREAKS="libxapian-dev"
 TERMUX_PKG_REPLACES="libxapian-dev"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
