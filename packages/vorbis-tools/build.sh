0e797f556021e162c60209fb47935841c1a01fd6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 23:14:21 +0900

    vorbis-tools: Get rid of references to leaked builtins

--- packages/vorbis-tools/build.sh
+++ packages/vorbis-tools/build.sh
@@ -3,10 +3,14 @@ TERMUX_PKG_DESCRIPTION="Ogg Vorbis tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/vorbis-tools-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=db7774ec2bf2c939b139452183669be84fda5774d6400fc57fde37f77624f0b0
 # libflac for flac support in oggenc:
 TERMUX_PKG_DEPENDS="libiconv, libvorbis, libflac, libogg"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-ogg123
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -lm"
