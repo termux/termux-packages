fd68e6317c0b040cca4cd863585f75665f257080
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 20:05:58 +0900

    jbig2dec: Rebuild with NDK r23b

--- packages/jbig2dec/build.sh
+++ packages/jbig2dec/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://jbig2dec.com/
 TERMUX_PKG_DESCRIPTION="Decoder implementation of the JBIG2 image compression format"
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.19
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9530/jbig2dec-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=279476695b38f04939aa59d041be56f6bade3422003a406a85e9792c27118a37
 TERMUX_PKG_DEPENDS="libpng"
