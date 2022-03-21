3166929f56e899a6dd49834aee5aeec055164671
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-22 03:03:07 +0900

    xfce4-panel: Get rid of references to leaked builtins

--- x11-packages/xfce4-panel/build.sh
+++ x11-packages/xfce4-panel/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Panel for the XFCE environment"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.16.3
-TERMUX_PKG_REVISION=17
+TERMUX_PKG_REVISION=18
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/xfce4-panel/4.16/xfce4-panel-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=5934eaed8a76da52c29b734ccd79600255420333dd6ebd8fd9f066379af1e092
 TERMUX_PKG_DEPENDS="desktop-file-utils, exo, hicolor-icon-theme, garcon, libwnck, libxfce4ui, xfconf"
