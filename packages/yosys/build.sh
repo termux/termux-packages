aea8bae42d97fb619932a1b3bc465c09a4735af2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-21 18:54:30 +0900

    yosys: Get rid of references to leaked builtins
    out of previous `tcl` shared libraries.

--- packages/yosys/build.sh
+++ packages/yosys/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A framework for RTL synthesis tools"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.13
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/YosysHQ/yosys/archive/refs/tags/yosys-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=004c203cb516887a8c76678a1fd76381198a8c46a17f4d893c34b7521df894b5
 TERMUX_PKG_DEPENDS="graphviz, libandroid-glob, libandroid-spawn, libc++, libffi, readline, tcl, zlib"
