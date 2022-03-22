7144b18e1ef85b25eec4df8f1a3de38319b459dc
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-23 08:21:19 +0900

    libimobiledevice: Get rid of references to leaked builtins

--- packages/libimobiledevice/build.sh
+++ packages/libimobiledevice/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org/
 TERMUX_PKG_DESCRIPTION="A library to communicate with services on iOS devices using native protocols."
-TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
+TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.LESSER"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/libimobiledevice/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=acbfb73eabee162e64c0d9de207d71c0a5f47c40cd5ad32a5097f734328ce10a
 TERMUX_PKG_AUTO_UPDATE=true
@@ -12,6 +12,7 @@ TERMUX_PKG_DEPENDS="usbmuxd, libusbmuxd, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
-        ./autogen.sh --prefix=$TERMUX_PREFIX \
-		     --host=$TERMUX_HOST_PLATFORM
+	./autogen.sh \
+		--prefix=$TERMUX_PREFIX \
+		--host=$TERMUX_HOST_PLATFORM
 }
