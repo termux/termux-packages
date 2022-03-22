e6fb2909c867cf0ff673d5295d40cb22e999250c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-23 08:32:36 +0900

    libopenmpt: Link against compiler-rt builtins

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="OpenMPT based module player library and libopenmpt based
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.12
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${TERMUX_PKG_VERSION}+release.autotools.tar.gz
 TERMUX_PKG_SHA256=892aea7a599b5d21842bebf463b5aafdad5711be7008dd84401920c6234820af
 TERMUX_PKG_DEPENDS="libflac, mpg123, pulseaudio"
@@ -11,3 +12,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-portaudio
 --without-portaudiocpp
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
