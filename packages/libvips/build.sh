5547b6bdfd050ff23c4a91e8d724760d7f37cb30
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-23 06:01:47 +0900

    libvips: Add dependency
    %ci:no-build

--- packages/libvips/build.sh
+++ packages/libvips/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/libvips/libvips/releases/download/v${TERMUX_PKG_VERSION}/vips-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=565252992aff2c7cd10c866c7a58cd57bc536e03924bde29ae0f0cb9e074010b
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="fftw, giflib, imagemagick, libexif, libexpat, libjpeg-turbo, libtiff, libwebp"
+TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, giflib, glib, harfbuzz, imagemagick, libc++, libcairo, libexif, libexpat, libheif, libjpeg-turbo, libjxl, libpng, librsvg, libtiff, libwebp, littlecms, openjpeg, pango"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-doc"
 
 termux_step_pre_configure() {
