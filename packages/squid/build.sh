a451345d06677cbef71c09c66a22f8323d6ce16b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-24 07:22:57 +0900

    squid: Update to 4.17

--- packages/squid/build.sh
+++ packages/squid/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.squid-cache.org
 TERMUX_PKG_DESCRIPTION="Full-featured Web proxy cache server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.17
 TERMUX_PKG_SRCURL=http://squid.mirror.globo.tech/archive/4/squid-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=7e00e891757c1c02dae546c9898f440c6031b684d8c243d6edab529076e3ba63
+TERMUX_PKG_SHA256=cb928ac08c7c86b151b1c8f827abe1a84d83181a2a86e0d512286163e1e31418
 TERMUX_PKG_DEPENDS="libc++, libcrypt, libxml2, libltdl, libgnutls, resolv-conf"
 
 #disk-io uses XSI message queue which are not available on Android.
