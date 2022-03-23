384f8f8e7837197efbafd1da9092e55ca86a4e89
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-23 21:36:35 +0900

    parted: Get rid of references to leaked builtins

--- packages/parted/build.sh
+++ packages/parted/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Versatile partition editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/parted/parted-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e1298022472da5589b7f2be1d5ee3c1b66ec3d96dfbad03dc642afd009da5342
 TERMUX_PKG_DEPENDS="libiconv, libuuid, readline"
@@ -13,6 +14,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-    CFLAGS+=" -Wno-gnu-designator"
-    export LIBS="-liconv"
+	CFLAGS+=" -Wno-gnu-designator"
+	export LIBS="-liconv"
 }
