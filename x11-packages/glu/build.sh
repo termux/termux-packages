f21853c714d146e314e7aa16211942d48bbec6e9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-23 12:19:43 +0900

    glu: Get rid of references to leaked libunwind

--- x11-packages/glu/build.sh
+++ x11-packages/glu/build.sh
@@ -1,12 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://cgit.freedesktop.org/mesa/glu/
 TERMUX_PKG_DESCRIPTION="Mesa OpenGL Utility library"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=9.0.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/glu/glu-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=24effdfb952453cc00e275e1c82ca9787506aba0282145fff054498e60e19a65
 
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libexpat, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxfixes, libxshmfence, mesa, zlib"
 TERMUX_PKG_CONFLICTS="libglu"
 TERMUX_PKG_REPLACES="libglu"
+
+termux_step_post_get_source() {
+	cp "${TERMUX_PKG_BUILDER_DIR}"/LICENSE ./
+}
