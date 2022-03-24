983a8f62a86be167ec8fbe7f8aa3442489167004
Chongyun Lee <uchkks@protonmail.com>
2022-03-24 20:50:52 +0800

    findutils: use fflush wrapper

--- packages/findutils/build.sh
+++ packages/findutils/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Utilities to find files meeting specified criteria and p
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.9.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/findutils/findutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe
 TERMUX_PKG_DEPENDS="libandroid-support"
@@ -10,7 +11,7 @@ TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_GROUPS="base-devel"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-gl_cv_func_fflush_stdin=yes
+gl_cv_func_fflush_stdin=no
 SORT_SUPPORTS_Z=yes
 SORT=$TERMUX_PREFIX/bin/sort
 "
