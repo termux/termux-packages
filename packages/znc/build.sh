bd72e54f701f2ae8dfa0e55a965e8177212549ef
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-25 05:43:07 +0900

    znc: Fix logic for `pthread_cancel` alternative

--- packages/znc/build.sh
+++ packages/znc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An advanced IRC bouncer"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.8.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://znc.in/releases/archive/znc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ff238aae3f2ae0e44e683c4aee17dc8e4fdd261ca9379d83b48a7d422488de0d
 TERMUX_PKG_DEPENDS="libc++, libicu, libsasl, openssl, zlib"
