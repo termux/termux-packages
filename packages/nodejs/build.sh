61e350af627573c3c9255b701018bd3dfcf3c191
Yaksh Bariya <yakshbari4@gmail.com>
2022-03-26 22:13:05 +0530

    nodejs: update to v17.8.0

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
-TERMUX_PKG_VERSION=17.7.2
+TERMUX_PKG_VERSION=17.8.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3ae5e74e0b16228cf7edf6885359a9d2e019ac843906c5e01948d746faba4aaf
+TERMUX_PKG_SHA256=7981144faf674292eff7e075f640a7084ebd927be11d1af78ff76741b6364e4b
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
