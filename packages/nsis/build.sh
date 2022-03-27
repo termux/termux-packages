617e582b135dba613bfca67baeb4d6f786bf6c2a
Yaksh Bariya <yakshbari4@gmail.com>
2022-03-27 13:46:38 +0530

    nsis: do not use deprecated debianutils' which

--- packages/nsis/build.sh
+++ packages/nsis/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.08
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/nsis/nsis-${TERMUX_PKG_VERSION}-src.tar.bz2
 TERMUX_PKG_SHA256=a85270ad5386182abecb2470e3d7e9bec9fe4efd95210b13551cb386830d1e87
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, zlib"
@@ -13,8 +13,8 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	scons \
-		CC="$(which $CC)" \
-		CXX="$(which $CXX)" \
+		CC="$(command -v $CC)" \
+		CXX="$(command -v $CXX)" \
 		APPEND_CCFLAGS="$CFLAGS $CPPFLAGS" \
 		APPEND_LINKFLAGS="$LDFLAGS" \
 		SKIPSTUBS=all \
