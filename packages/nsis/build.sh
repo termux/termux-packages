e58d1df8afc6da2b1b1cb508e255944910856b62
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-27 20:43:27 +0900

    nsis: Fix install prefix

--- packages/nsis/build.sh
+++ packages/nsis/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.08
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/nsis/nsis-${TERMUX_PKG_VERSION}-src.tar.bz2
 TERMUX_PKG_SHA256=a85270ad5386182abecb2470e3d7e9bec9fe4efd95210b13551cb386830d1e87
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, zlib"
@@ -22,10 +22,10 @@ termux_step_make_install() {
 		SKIPUTILS=all \
 		SKIPMISC=all \
 		NSIS_CONFIG_CONST_DATA_PATH=no \
-		PREFIX="$TERMUX_PREFIX/opt/nsis" \
+		PREFIX="$TERMUX_PREFIX/opt/nsis/nsis" \
 		install-compiler
 }
 
 termux_step_post_make_install() {
-	ln -sf $TERMUX_PREFIX/opt/nsis/makensis $TERMUX_PREFIX/bin/
+	ln -sf $TERMUX_PREFIX/opt/nsis/nsis/makensis $TERMUX_PREFIX/bin/
 }
