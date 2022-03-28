bff848d47bf373df448565158a9ad55258bd2c02
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-29 03:46:20 +0900

    iproute2: Update to 5.17.0

--- packages/iproute2/build.sh
+++ packages/iproute2/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wiki.linuxfoundation.org/networking/iproute2
 TERMUX_PKG_DESCRIPTION="Utilities for controlling networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.15.0
+TERMUX_PKG_VERSION=5.17.0
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=38e3e4a5f9a7f5575c015027a10df097c149111eeb739993128e5b2b35b291ff
+TERMUX_PKG_SHA256=6e384f1b42c75e1a9daac57866da37dcff909090ba86eb25a6e764da7893660e
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
 
