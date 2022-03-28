71d45f19224d32c7dda94d4914ad8cb0ce142d47
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-29 03:15:53 +0900

    dbus: Update to 1.14.0

--- packages/dbus/build.sh
+++ packages/dbus/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://dbus.freedesktop.org
 TERMUX_PKG_DESCRIPTION="Freedesktop.org message bus system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.13.18
+TERMUX_PKG_VERSION=1.14.0
 TERMUX_PKG_SRCURL="https://dbus.freedesktop.org/releases/dbus/dbus-$TERMUX_PKG_VERSION.tar.xz"
-TERMUX_PKG_SHA256=8078f5c25e34ab907ce06905d969dc8ef0ccbec367e1e1707c7ecf8460f4254e
+TERMUX_PKG_SHA256=ccd7cce37596e0a19558fd6648d1272ab43f011d80c8635aea8fd0bad58aebd4
 TERMUX_PKG_DEPENDS="libexpat, libx11"
 TERMUX_PKG_BREAKS="dbus-dev"
 TERMUX_PKG_REPLACES="dbus-dev"
@@ -17,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-x11-autolaunch
 --with-test-socket-dir=$TERMUX_PREFIX/tmp
 --with-session-socket-dir=$TERMUX_PREFIX/tmp
---with-x
+--with-x=auto
 "
 
 termux_step_pre_configure() {
