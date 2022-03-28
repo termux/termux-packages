cade52fbdd6ad8b5148806e397e47690b2252742
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-29 03:08:48 +0900

    libsndfile: Update to 1.1.0

--- packages/libsndfile/build.sh
+++ packages/libsndfile/build.sh
@@ -1,13 +1,25 @@
 TERMUX_PKG_HOMEPAGE=http://www.mega-nerd.com/libsndfile
 TERMUX_PKG_DESCRIPTION="Library for reading/writing audio files"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.31
-TERMUX_PKG_SRCURL=https://github.com/erikd/libsndfile/releases/download/${TERMUX_PKG_VERSION}/libsndfile-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a8cfb1c09ea6e90eff4ca87322d4168cdbe5035cb48717b40bf77e751cc02163
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SRCURL=https://github.com/libsndfile/libsndfile/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=642a876bd61b63f9346628dba5f8a0356a3ad750c7f6f42019d26ce60ba6a15b
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libflac, libvorbis"
+TERMUX_PKG_DEPENDS="libflac, libogg, libopus, libvorbis"
 TERMUX_PKG_BREAKS="libsndfile-dev"
 TERMUX_PKG_REPLACES="libsndfile-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sqlite --disable-alsa"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-sqliter
+--disable-alsa
+--disable-mpeg
+"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/ share/man/man1/"
+
+termux_step_post_get_source() {
+	rm -f CMakeLists.txt
+}
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
