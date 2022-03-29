1b6bd3518295d695412da40eef99cf1cc8ff3704
Yaksh Bariya <yakshbari4@gmail.com>
2022-03-29 11:26:13 +0530

    entr: update to 5.1

--- packages/entr/build.sh
+++ packages/entr/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://eradman.com/entrproject/
 TERMUX_PKG_DESCRIPTION="Event Notify Test Runner - run arbitrary commands when files change"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.0
+TERMUX_PKG_VERSION=5.1
 TERMUX_PKG_SRCURL=http://eradman.com/entrproject/code/entr-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=2a87bb7d9e5e89b6f614495937b557dbb8144ea53d0c1fa1812388982cd41ebb
+TERMUX_PKG_SHA256=0f87f577bce87641c525addb9bcc60bbaa579fe981dab759043e3ce1556dbb92
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
