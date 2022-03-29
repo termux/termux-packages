88d72220eed69da3195732c038df2814e59db343
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-30 05:11:12 +0900

    libepoxy: Update to 1.5.10

--- x11-packages/libepoxy/build.sh
+++ x11-packages/libepoxy/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/anholt/libepoxy
 TERMUX_PKG_DESCRIPTION="Library handling OpenGL function pointer management"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.5.8
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/anholt/libepoxy/releases/download/${TERMUX_PKG_VERSION}/libepoxy-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cf05e4901778c434aef68bb7dc01bea2bce15440c0cecb777fb446f04db6fe0d
+TERMUX_PKG_VERSION=1.5.10
+TERMUX_PKG_SRCURL=https://github.com/anholt/libepoxy/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=a7ced37f4102b745ac86d6a70a9da399cc139ff168ba6b8002b4d8d43c900c15
 TERMUX_PKG_DEPENDS="mesa"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
