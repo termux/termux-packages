8edeb5384ce825ab198614e1f7e815e85c063dd5
Aditya Alok <dev.aditya.alok@gmail.com>
2022-03-31 01:15:42 +0530

    feat(helix): enable auto-update and update to 22.03
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/helix/build.sh
+++ packages/helix/build.sh
@@ -1,16 +1,15 @@
 TERMUX_PKG_HOMEPAGE="https://helix-editor.com/"
 TERMUX_PKG_DESCRIPTION="A post-modern modal text editor written in rust"
 TERMUX_PKG_LICENSE="MPL-2.0"
-TERMUX_PKG_MAINTAINER="MrAdityaAlok <dev.aditya.alok@gmail.com>"
-TERMUX_PKG_VERSION=0.6.0
+TERMUX_PKG_MAINTAINER="Aditya Alok <dev.aditya.alok@gmail.com>"
+TERMUX_PKG_VERSION="22.03"
 TERMUX_PKG_SRCURL="https://github.com/helix-editor/helix.git"
-TERMUX_PKG_GIT_BRANCH="v$TERMUX_PKG_VERSION"
+TERMUX_PKG_GIT_BRANCH="$TERMUX_PKG_VERSION"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make_install() {
 	termux_setup_rust
-
-	sed -i "s%\@TERMUX_CC\@%${CC}%g" ./helix-syntax/build.rs
 
 	cargo build --jobs "${TERMUX_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
 
@@ -21,9 +20,10 @@ termux_step_make_install() {
 		exec ${TERMUX_PREFIX}/lib/helix/hx "\$@"
 	EOF
 	install -Dm744 ./hx "${TERMUX_PREFIX}/bin/hx"
-
 	install -Dm744 -t "${TERMUX_PREFIX}"/lib/helix target/"${CARGO_TARGET_NAME}"/release/hx
-	install -Dm644 -t "${TERMUX_PREFIX}"/lib/helix languages.toml
 
-	cp -r runtime "${TERMUX_PREFIX}/lib/helix"
+	install -Dm644 -t "${TERMUX_PREFIX}"/lib/helix/runtime ./runtime/tutor.txt
+	cp -r ./runtime/{queries,themes} "${TERMUX_PREFIX}"/lib/helix/runtime
+	find ./runtime/grammars -maxdepth 1 -type f -name "*.so" \
+		-exec install -Dm744 -t "${TERMUX_PREFIX}"/lib/helix/runtime/grammars {} \;
 }
