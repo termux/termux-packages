c8f78f147297cdb2f09e06bbbd3104628f79c796
Yisus7u7 <dev.yisus@hotmail.com>
2022-03-29 23:47:43 -0500

    add packages: adwaita-qt and gnome-themes-extra

---
+++ x11-packages/adwaita-qt/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/FedoraQt/adwaita-qt
+TERMUX_PKG_DESCRIPTION="A style to bend Qt applications to look like they belong into GNOME Shell"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
+TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_SRCURL="https://github.com/FedoraQt/adwaita-qt/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=834f2caaf031306b63b31ad6fec489482feda35e9aa260f205cbb976cde97b1d
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtx11extras, qt5-qtsvg"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
---
+++ x11-packages/gnome-themes-extra/gtk2-engines-adwaita-static.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+lib/gtk-2.0/2.10.0/engines/libadwaita.la
+lib/gtk-2.0/2.10.0/engines/libadwaita.a
+"
+
+TERMUX_SUBPKG_DEPENDS="gtk2-engines-adwaita"
+TERMUX_SUBPKG_DESCRIPTION="Static libraries for gtk2-engines-adwaita"
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=false
