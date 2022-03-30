808a2f955765c024e141e6495501944232b78659
Tee KOBAYASHI <xtkoba@gmail.com>
2022-03-31 05:39:02 +0900

    new package: libolm

---
+++ packages/libolm/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://matrix.org/docs/projects/other/olm/
+TERMUX_PKG_DESCRIPTION="An implementation of the Double Ratchet cryptographic ratchet"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.2.10
+TERMUX_PKG_SRCURL=https://gitlab.matrix.org/matrix-org/olm/-/archive/${TERMUX_PKG_VERSION}/olm-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=c41ed0a0fe613ed8ca78144855d1f82477a3539329e1dd73f47b6b2e2739e8b5
