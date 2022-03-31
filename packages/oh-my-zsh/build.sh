0a5a1f18d0890031f56dc0feef3d67ff207fc45a
sumithemmadi <sumithemmadi244@gmail.com>
2022-03-31 18:02:07 +0530

    added oh-my-zsh package

--- packages/oh-my-zsh/build.sh
+++ packages/oh-my-zsh/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/sumithemmadi/ohmyzsh
 TERMUX_PKG_NAME="oh-my-zsh"
 TERMUX_PKG_DESCRIPTION="A community-driven framework for managing your zsh configuration. Includes 180+ optional plugins and over 120 themes to spice up your morning, and an auto-update tool so that makes it easy to keep up with the latest updates from the community"
 TERMUX_PKG_LICENSE="MIT License"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="Sumith Emmadi <sumithemmadi244@gmail.com>"
 TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/sumithemmadi/ohmyzsh.git
