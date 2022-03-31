ad1188ea4dcc900cfef964acea1a09d2ffdc2069
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-01 06:19:13 +0900

    mediainfo: Update to 22.03

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -3,11 +3,10 @@ TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.09
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=22.03
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7b8fd9a502ec64afe1315072881b6385ba57ca69f2f82c625b1672e151c50c57
-TERMUX_PKG_DEPENDS="libandroid-support, libmediainfo"
+TERMUX_PKG_SHA256=6236f92a1d57a8174b3f30ea6dde8aab538d8f620f728224d851373169802be4
+TERMUX_PKG_DEPENDS="libandroid-support, libc++, libmediainfo, libzen"
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR="${TERMUX_PKG_SRCDIR}/Project/GNU/CLI"
