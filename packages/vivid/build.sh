5040651963f33a565e8fc56892f821ce7a38d4ac
Freed-Wu <wuzy01@qq.com>
2022-03-31 10:56:34 +0800

    new package: vivid

---
+++ packages/vivid/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/sharkdp/vivid
+TERMUX_PKG_DESCRIPTION="A themeable LS_COLORS generator with a rich filetype datebase"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_SRCURL=https://github.com/sharkdp/vivid/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=e58e0936db25c81ff257775463f1d422d97c706aec2d0134e39b62151ded23cb
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
