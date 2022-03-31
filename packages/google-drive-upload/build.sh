34117d1c692bf71e5f1fc5baa000300d0b905342
Freed-Wu <wuzy01@qq.com>
2022-03-31 15:25:23 +0800

    new package: google-drive-upload

---
+++ packages/google-drive-upload/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/labbots/google-drive-upload
+TERMUX_PKG_DESCRIPTION="Bash scripts to upload files to google drive"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=4.2
+TERMUX_PKG_SRCURL=$TERMUX_PKG_HOMEPAGE/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=7c57720736884f26ea69d3a76a540f021e4e13dc84ba30f7081d5f7c69c7bda4
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS='bash, curl'
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+termux_step_make_install() {
+	install -D bash/release/* -t "$TERMUX_PREFIX/bin"
+}
