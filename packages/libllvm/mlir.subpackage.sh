039f6ba809916a00c25798d65b1b432e31dced3b
Butta <repo@butta.fastem.com>
2022-03-31 17:34:37 +0530

    libllvm: add mlir

---
+++ packages/libllvm/mlir.subpackage.sh
@@ -0,0 +1,9 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/mlir-*
+include/mlir*
+lib/cmake/mlir/
+lib/libMLIR.so
+lib/libmlir*so
+"
+TERMUX_SUBPKG_DESCRIPTION="A Multi-Level Intermediate Representation for compilers from LLVM"
+TERMUX_SUBPKG_DEPENDS="libc++, ncurses"
