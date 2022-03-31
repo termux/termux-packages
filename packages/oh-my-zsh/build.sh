f8e6a728cd054d2aab24ad6766fa2eff8d13cf8f
sumithemmadi <sumithemmadi244@gmail.com>
2022-03-31 17:26:08 +0530

    added oh-my-zsh package

--- packages/oh-my-zsh/build.sh
+++ packages/oh-my-zsh/build.sh
@@ -5,10 +5,25 @@ TERMUX_PKG_LICENSE="MIT License"
 TERMUX_PKG_MAINTAINER="Sumith Emmadi <sumithemmadi244@gmail.com>"
 TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/sumithemmadi/ohmyzsh.git
-TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_GIT_BRANCH=main
 TERMUX_PKG_DEPENDS="zsh,git,python"
 
 termux_step_make_install() {
-  install -D -m644 "${TERMUX_PKG_SRCDIR}/LICENSE.txt" "${TERMUX_PREFIX}/usr/share/LICENSES/${TERMUX_PKG_NAME}/LICENSE"
-  cp -r "${TERMUX_PKG_SRCDIR}/.." "${TERMUX_PREFIX}/usr/share/oh-my-zsh"
+  cd "${TERMUX_TOPDIR}/${TERMUX_PKG_NAME}/package"
+  mkdir -p "${TERMUX_PREFIX}/usr/share/oh-my-zsh"
+  mkdir -p "${TERMUX_PREFIX}/usr/share/LICENSES/${TERMUX_PKG_NAME}"
+  # install -D -m644 "zshrc" "${HOME}/.zshrc"
+  install -D -m644 "LICENSE.txt" "${TERMUX_PREFIX}/usr/share/LICENSES/${TERMUX_PKG_NAME}/LICENSE"
+  cp -rf * "${TERMUX_PREFIX}/usr/share/oh-my-zsh/"
+}
+
+termux_step_make_install() {
+  cd "${TERMUX_PKG_SRCDIR}"
+
+  mkdir -p "${TERMUX_PREFIX}/usr/share/oh-my-zsh"
+  mkdir -p "${TERMUX_PREFIX}/usr/share/LICENSES/${TERMUX_PKG_NAME}"
+  echo $PWD
+  # install -D -m644 "zshrc" "${HOME}/.zshrc"
+  install -m644 "${TERMUX_PKG_SRCDIR}/LICENSE.txt" "${TERMUX_PREFIX}/usr/share/LICENSES/${TERMUX_PKG_NAME}/LICENSE"
+  install -Dm644 "$PWD/*" "${TERMUX_PREFIX}/usr/share/oh-my-zsh/"
 }
