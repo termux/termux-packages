8edebd4bb78dd3d39fb45bc85326bdae5eb49c8b
Aditya Alok <dev.aditya.alok@gmail.com>
2022-03-31 15:36:04 +0530

    fix(profanity): update to 0.12.0 and add `libstrophe` to depends
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://profanity-im.github.io
 TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C using ncurses and libstrophe, inspired by Irssi"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
-TERMUX_PKG_VERSION=0.11.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=0.12.0
 TERMUX_PKG_SRCURL=https://github.com/profanity-im/profanity/releases/download/$TERMUX_PKG_VERSION/profanity-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6f1b4df6c2971f51d03d48d2bfd4f69b4404410d800b43f029ea1cf08a02bd45
+TERMUX_PKG_SHA256=9e60de8cb7f747f3d433a5b091e4d439d6e2b71a958e1a7607681660638793d6
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-support, libffi, ncurses, glib, libmesode, libcurl, readline, libuuid, libotr, gpgme, python, libassuan, libgpg-error, zlib, libsignal-protocol-c"
+TERMUX_PKG_DEPENDS="libandroid-support, libffi, ncurses, glib, libmesode, libcurl, readline, libuuid, libotr, gpgme, python, libassuan, libgpg-error, zlib, libsignal-protocol-c, libstrophe"
 TERMUX_PKG_BREAKS="profanity-dev"
 TERMUX_PKG_REPLACES="profanity-dev"
 # openssl, libexpat needed by libmesode, pcre needed by glib:
