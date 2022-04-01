5fe300628f5e1f435d8ec405852801d1fdf3f588
NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>
2022-04-01 11:31:26 +0800

    Polybar:Updated to v3.6.1
    Signed-off-by: NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>

--- x11-packages/polybar/build.sh
+++ x11-packages/polybar/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://polybar.github.io
 TERMUX_PKG_DESCRIPTION="A fast and easy-to-use status bar"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
-TERMUX_PKG_VERSION=3.5.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.6.1
 TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/${TERMUX_PKG_VERSION}/polybar-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=dfe602fc6ac96eac2ae0f5deb2f87e0dd1f81ea5d0f04ad3b3bfd71efd5cc038
-TERMUX_PKG_DEPENDS="jsoncpp, pulseaudio, fontconfig, freetype, libandroid-glob, libandroid-shmem, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm"
+TERMUX_PKG_SHA256=a898ba8c847895be41efe5a31bb1b839da2c10b130b8411d5016982fbaadff7b
+TERMUX_PKG_DEPENDS="jsoncpp, pulseaudio, fontconfig, freetype, libandroid-glob, libandroid-shmem, libc++, libcairo, libcurl, libuv, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
