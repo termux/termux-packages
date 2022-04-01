3c57ee5330c8818fe3409b12203e31255bcc81b1
Yonle <yonle@duck.com>
2022-04-01 08:41:07 +0700

    libretls: Prepare LICENSE file and install them out
    Signed-off-by: Yonle <yonle@duck.com>

--- packages/libretls/build.sh
+++ packages/libretls/build.sh
@@ -1,9 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://git.causal.agency/libretls/about/
 TERMUX_PKG_DESCRIPTION="libtls for OpenSSL"
 TERMUX_PKG_LICENSE="ISC"
-# There's no license file in the tarball source
-# Instead, copy README.7 as it also includes what LICENSE that they use.
-TERMUX_PKG_LICENSE_FILE="README.7"
 TERMUX_PKG_MAINTAINER="@Yonle <yonle@duck.com>"
 TERMUX_PKG_VERSION=3.5.1
 TERMUX_PKG_SRCURL=https://git.causal.agency/libretls/snapshot/libretls-${TERMUX_PKG_VERSION}.tar.gz
@@ -12,6 +9,10 @@ TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_REPLACES="libtls"
 TERMUX_PKG_PROVIDES="libtls"
 
-termux_step_pre_configure(){
+termux_step_pre_configure() {
 	autoreconf -fi
+}
+
+termux_step_install_license() {
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/LICENSE ${TERMUX_PREFIX}/share/doc/$TERMUX_PKG_NAME
 }
