27a3f3012b5eaed7fa28c35908aeb01d2874bfba
j.r <j.r@jugendhacker.de>
2022-04-01 16:21:18 +0200

    xmppc: update to 0.1.2

--- packages/xmppc/build.sh
+++ packages/xmppc/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://codeberg.org/Anoxinon_e.V./xmppc
 TERMUX_PKG_DESCRIPTION="Command Line Interface Tool for XMPP"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="j.r <j.r@jugendhacker.de>"
-TERMUX_PKG_VERSION=0.1.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.1.2
 TERMUX_PKG_SRCURL=https://codeberg.org/Anoxinon_e.V./xmppc/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=98d68deb57924e5ed06613d8b275fb0bf98aab822fb590fe8d9894410a8544ee
+TERMUX_PKG_SHA256=05259ec5cba25f693edfe01389a3405835404539c7817fb208c201e29480e6b7
 TERMUX_PKG_DEPENDS="libstrophe, glib, gpgme"
 
 termux_step_pre_configure() {
