a740bd235e5e5d3d3352c1a51b6b06253d9552c7
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-01 19:55:38 +0530

    fix(profanity): bump after removal of `configure.ac` patch
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -3,25 +3,14 @@ TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C us
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_VERSION=0.12.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/profanity-im/profanity/releases/download/$TERMUX_PKG_VERSION/profanity-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9e60de8cb7f747f3d433a5b091e4d439d6e2b71a958e1a7607681660638793d6
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-support, libffi, ncurses, glib, libmesode, libcurl, readline, libuuid, libotr, gpgme, python, libassuan, libgpg-error, zlib, libsignal-protocol-c, libstrophe"
+TERMUX_PKG_DEPENDS="libandroid-support, libffi, ncurses, glib, libcurl, readline, libuuid, libotr, gpgme, python, libassuan, libgpg-error, zlib, libsignal-protocol-c, libstrophe"
 TERMUX_PKG_BREAKS="profanity-dev"
 TERMUX_PKG_REPLACES="profanity-dev"
-# openssl, libexpat needed by libmesode, pcre needed by glib:
-TERMUX_PKG_BUILD_DEPENDS="openssl, libexpat, pcre, libgcrypt, libcrypt"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-python-plugins --without-xscreensaver"
+# pcre needed by glib:
+TERMUX_PKG_BUILD_DEPENDS="pcre, libgcrypt, libcrypt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --enable-plugins --without-xscreensaver"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	CPPFLAGS+=" -I$TERMUX_PREFIX/include/python3.10"
-	LDFLAGS+=" -lpython3.10"
-}
-
-termux_step_post_configure() {
-	# Enable python support manually, as trying to go using --enable-python-plugins
-	# causes configure trying to execute python:
-	echo '#define HAVE_PYTHON 1' >> $TERMUX_PKG_SRCDIR/src/config.h
-	perl -p -i -e 's|#am__objects_2|am__objects_2|' $TERMUX_PKG_SRCDIR/Makefile
-}
