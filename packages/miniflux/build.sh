2c9f1e51cd82a9c7e22df03d577f435cf3a6e098
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-02 09:55:44 +0900

    new package: miniflux

---
+++ packages/miniflux/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://miniflux.app/
+TERMUX_PKG_DESCRIPTION="A minimalist and opinionated feed reader"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.0.36
+TERMUX_PKG_SRCURL=https://github.com/miniflux/v2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=15ac4ad4ab6f87de2143a85f52252e80d7c216a69aab334363e1972b81f231fd
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="VERSION=$TERMUX_PKG_VERSION"
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin miniflux
+	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 miniflux.1
+}
