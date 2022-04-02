80014608d428527acc69e5a48165ad01971502e0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-02 14:04:18 +0900

    new package: libmd

---
+++ packages/libmd/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://www.hadrons.org/software/libmd/
+TERMUX_PKG_DESCRIPTION="Message Digest functions from BSD systems"
+# Licenses: BSD 3-Clause, BSD 2-Clause, ISC, Beerware, Public Domain
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.0.4
+TERMUX_PKG_SRCURL=https://archive.hadrons.org/software/libmd/libmd-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=f51c921042e34beddeded4b75557656559cf5b1f2448033b4c1eec11c07e530f
