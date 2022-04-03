ee519804fbacf98eb301fc4cb35135ab2b88d09c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-03 10:34:29 +0900

    new package: libbluray

---
+++ packages/libbluray/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://code.videolan.org/videolan/libbluray/
+TERMUX_PKG_DESCRIPTION="An open-source library designed for Blu-Ray Discs playback for media players"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_SRCURL=https://downloads.videolan.org/pub/videolan/libbluray/${TERMUX_PKG_VERSION}/libbluray-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=c24b0f41c5b737bbb65c544fe63495637a771c10a519dfc802e769f112b43b75
+TERMUX_PKG_DEPENDS="fontconfig, freetype, libudfread, libxml2"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-bdjava-jar
+--disable-optimizations
+"
+
+termux_step_pre_configure() {
+	unset JDK_HOME
+}
