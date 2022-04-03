c306f6574aa45223491d76a433e792c5b6f14bee
Henrik Grimler <grimler@termux.org>
2022-04-03 13:04:18 +0200

    libqtxdg: fix build with latest cmake and/or glib
    Previously failed to find gio or gio-unix:
    
    -- Checking for module 'glib-2.0'
    --   Found glib-2.0, version 2.72.0
    -- Checking for module 'gio-unix-2.0'
    --   Found gio-unix-2.0, version 2.72.0
    CMake Error at /home/builder/.termux-build/_cache/cmake-3.23.0/share/cmake-3.23/Modules/FindPackageHandleStandardArgs.cmake:230 (message):
      Could NOT find GLIB (missing: GLIB_GIO_UNIX_INCLUDE_DIR) (found suitable
      version "2.72.0", minimum required is "2.41.0")
    Call Stack (most recent call first):
      /home/builder/.termux-build/_cache/cmake-3.23.0/share/cmake-3.23/Modules/FindPackageHandleStandardArgs.cmake:594 (_FPHSA_FAILURE_MESSAGE)
      /data/data/com.termux/files/usr/share/cmake/lxqt-build-tools/find-modules/FindGLIB.cmake:113 (FIND_PACKAGE_HANDLE_STANDARD_ARGS)
      CMakeLists.txt:26 (find_package)

--- x11-packages/libqtxdg/build.sh
+++ x11-packages/libqtxdg/build.sh
@@ -3,8 +3,11 @@ TERMUX_PKG_DESCRIPTION="Qt 5 implementation of freedesktop.org XDG specification
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=3.8.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/libqtxdg/releases/download/${TERMUX_PKG_VERSION}/libqtxdg-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=ce824fb2dda47758b44e57e8e4b63437a5bc5bd476cf37dbba61442990d7ccec
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg, glib"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qttools-cross-tools"
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DGLIB_GIO_UNIX_INCLUDE_DIR=$TERMUX_PREFIX/include/gio-unix-2.0
+"
