2fba968abfb422513711c911673b307ee0ba6be4
Henrik Grimler <grimler@termux.org>
2022-04-03 10:48:58 +0200

    fetchmail: update to 6.4.29

--- packages/fetchmail/build.sh
+++ packages/fetchmail/build.sh
@@ -2,12 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.fetchmail.info/
 TERMUX_PKG_DESCRIPTION="A remote-mail retrieval utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.4.28
+TERMUX_PKG_VERSION=6.4.29
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/fetchmail/files/branch_${TERMUX_PKG_VERSION:0:3}/fetchmail-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=a003f9ac88bf083a232c9451ef5f3f88473fad2c7f2822d3f7455a6d32bc3a97
+TERMUX_PKG_SHA256=397df984082abae51edec6831700d68636f0e117cfaffcbdd3eed66d04b97321
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=$TERMUX_PREFIX"
-
-termux_step_pre_configure() {
-	export LIBS="-llog"
-}
