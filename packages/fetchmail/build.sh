f8638576dc129a36df8afedfb2f89a78d52e43d1
Henrik Grimler <grimler@termux.org>
2022-04-03 10:52:26 +0200

    fetchmail: remove __pycache__ directory

--- packages/fetchmail/build.sh
+++ packages/fetchmail/build.sh
@@ -3,7 +3,9 @@ TERMUX_PKG_DESCRIPTION="A remote-mail retrieval utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=6.4.29
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/fetchmail/files/branch_${TERMUX_PKG_VERSION:0:3}/fetchmail-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=397df984082abae51edec6831700d68636f0e117cfaffcbdd3eed66d04b97321
 TERMUX_PKG_DEPENDS="libcrypt, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=$TERMUX_PREFIX"
+TERMUX_PKG_RM_AFTER_INSTALL="lib/python3.10/site-packages/__pycache__"
