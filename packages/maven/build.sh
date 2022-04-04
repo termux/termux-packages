6c539151132c51b03b4d117ffc5f1a8e60654fd1
tempaccforissue <102426645+tempaccforissue@users.noreply.github.com>
2022-04-04 11:28:06 +0600

    maven: update to 3.8.5 (#9862)

--- packages/maven/build.sh
+++ packages/maven/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://maven.apache.org/
 TERMUX_PKG_DESCRIPTION="A Java software project management and comprehension tool"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@masterjavaofficial"
-TERMUX_PKG_VERSION=3.8.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.8.5
 TERMUX_PKG_SRCURL=https://dlcdn.apache.org/maven/maven-3/${TERMUX_PKG_VERSION}/binaries/apache-maven-${TERMUX_PKG_VERSION}-bin.tar.gz
-TERMUX_PKG_SHA256=2cdc9c519427bb20fdc25bef5a9063b790e4abd930e7b14b4e9f4863d6f9f13c
+TERMUX_PKG_SHA256=88e30700f32a3f60e0d28d0f12a3525d29b7c20c72d130153df5b5d6d890c673
 TERMUX_PKG_DEPENDS="libjansi, openjdk-17"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -20,7 +19,7 @@ termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/opt
 	cp -a $TERMUX_PKG_SRCDIR $TERMUX_PREFIX/opt/maven/
 	# Symlink only starter scripts for Linux
-	ln -sfr $TERMUX_PREFIX/opt/maven/bin/mvn $TERMUX_PREFIX/bin/mvn
-	ln -sfr $TERMUX_PREFIX/opt/maven/bin/mvnDebug $TERMUX_PREFIX/bin/mvnDebug
-	ln -sfr $TERMUX_PREFIX/opt/maven/bin/mvnyjp $TERMUX_PREFIX/bin/mvnyjp
+	for i in mvn mvnDebug mvnyjp; do
+		ln -sfr $TERMUX_PREFIX/opt/maven/bin/$i $TERMUX_PREFIX/bin/$i
+	done
 }
