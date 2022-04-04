bb2bff8cdd7d62fcd16d9b42816d6c73543f01fd
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-04 11:48:29 +0900

    new package: slashtime

---
+++ packages/slashtime/build.sh
@@ -0,0 +1,35 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/istathar/slashtime
+TERMUX_PKG_DESCRIPTION="A small program which displays the time in various places"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=7326857683542102d383f969c002286845b1429f
+TERMUX_PKG_VERSION=2020.10.01
+TERMUX_PKG_SRCURL=https://github.com/istathar/slashtime.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="perl"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
+		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
+}
+
+termux_step_configure() {
+	:
+}
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	install -Dm700 -T slashtime.pl $TERMUX_PREFIX/bin/slashtime
+}
