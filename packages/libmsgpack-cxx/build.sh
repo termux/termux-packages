2c11650b050abd9acf7d04e6ffbee540c0d347b4
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-05 23:50:12 +0530

    fix(libmsgpack{,-cxx}): regexp in tag name check
    %ci:no-build
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/libmsgpack-cxx/build.sh
+++ packages/libmsgpack-cxx/build.sh
@@ -15,9 +15,9 @@ termux_pkg_auto_update() {
 	local tag
 	tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}")"
 	# check if this is not a c release:
-	if grep -qP "^cpp-${TERMUX_PKG_UPDATE_VERSION_REGEXP}$" <<<"$tag"; then
+	if grep -qP "^cpp-${TERMUX_PKG_UPDATE_VERSION_REGEXP}\$" <<<"$tag"; then
 		termux_pkg_upgrade_version "$tag"
 	else
-		echo "WARNING: Skipping auto-update of ${TERMUX_PKG_NAME}: Not a cpp release($tag)"
+		echo "WARNING: Skipping auto-update: Not a cpp release($tag)"
 	fi
 }
