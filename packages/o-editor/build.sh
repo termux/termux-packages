c9933a62969e6bad9657f95584af5849fb391a6a
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-06 00:04:57 +0530

    fix(o-editor): fix src dir, update to 2.49.1
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/o-editor/build.sh
+++ packages/o-editor/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xyproto/o
 TERMUX_PKG_DESCRIPTION="Small, fast and limited text editor"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Alexander F. Rødseth <xyproto@archlinux.org>"
-TERMUX_PKG_VERSION=2.48.3
+TERMUX_PKG_VERSION=2.49.1
 TERMUX_PKG_SRCURL=https://github.com/xyproto/o/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6c49163bf0c01e32bda22de86e0043bcec8fe3f38775a3bb71d9fda0de77738a
+TERMUX_PKG_SHA256=26d701184d4efe467ce43a096498f8ffea6cd65ab5a7d7b37459183e730ebafa
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="o"
 TERMUX_PKG_REPLACES="o"
@@ -16,13 +16,13 @@ termux_step_make() {
 	mkdir -p "$GOPATH"/src/github.com/xyproto
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/xyproto/o
 
-	cd "$GOPATH"/src/github.com/xyproto/o/src
+	cd "$GOPATH"/src/github.com/xyproto/o/v2
 	go build
 }
 
 termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin \
-		"$GOPATH"/src/github.com/xyproto/o/src/o
+		"$GOPATH"/src/github.com/xyproto/o/v2/o
 	install -Dm600 -t "$TERMUX_PREFIX"/share/man/man1 \
 		"$TERMUX_PKG_SRCDIR"/o.1
 }
