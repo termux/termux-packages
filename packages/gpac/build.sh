68cad482fe179da05d5b592e2079057c1c56f6e1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-08 11:24:24 +0900

    gpac: Update to 2.0.0

--- packages/gpac/build.sh
+++ packages/gpac/build.sh
@@ -1,11 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://gpac.wp.imt.fr/
 TERMUX_PKG_DESCRIPTION="An open-source multimedia framework focused on modularity and standards compliance"
+# License: LGPL-2.1-or-later
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.1
-TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3b0ffba73c68ea8847027c23f45cd81d705110ec47cf3c36f60e669de867e0af
-TERMUX_PKG_DEPENDS="zlib"
+TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c820386f9d583c740fc3f57cb93864a8164838516dabc96de2f61d5b7429e0b6
+TERMUX_PKG_DEPENDS="ffmpeg, freetype, liba52, libjpeg-turbo, liblzma, libmad, libnghttp2, libpng, libtheora, libvorbis, openjpeg, openssl, pulseaudio, xvidcore, zlib"
+TERMUX_PKG_BUILD_DEPENDS="libogg"
 TERMUX_PKG_EXTRA_MAKE_ARGS="STRIP=:"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11"
 
@@ -17,4 +19,17 @@ termux_step_pre_configure() {
 	for f in $LDFLAGS; do
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --extra-ldflags=$f"
 	done
+
+	_NEED_DUMMY_LIBPTHREAD_A=
+	_LIBPTHREAD_A=$TERMUX_PREFIX/lib/libpthread.a
+	if [ ! -e $_LIBPTHREAD_A ]; then
+		_NEED_DUMMY_LIBPTHREAD_A=true
+		echo '!<arch>' > $_LIBPTHREAD_A
+	fi
+}
+
+termux_step_post_make_install() {
+	if [ $_NEED_DUMMY_LIBPTHREAD_A ]; then
+		rm -f $_LIBPTHREAD_A
+	fi
 }
