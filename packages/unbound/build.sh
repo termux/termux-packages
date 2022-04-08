b8d695f530520099fff0d51db19677a5a0935567
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-08 17:21:42 +0900

    unbound: Update to 1.15.0 and enable Python support

--- packages/unbound/build.sh
+++ packages/unbound/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://unbound.net/
 TERMUX_PKG_DESCRIPTION="A validating, recursive, caching DNS resolver"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.13.2
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.15.0
 TERMUX_PKG_SRCURL=https://nlnetlabs.nl/downloads/unbound/unbound-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0a13b547f3b92a026b5ebd0423f54c991e5718037fd9f72445817f6a040e1a83
-TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl"
+TERMUX_PKG_SHA256=a480dc6c8937447b98d161fe911ffc76cfaffa2da18788781314e81339f1126f
+TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl, python"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_chown=no
@@ -20,11 +20,27 @@ ac_cv_func_getpwnam=no
 --with-libexpat=$TERMUX_PREFIX
 --without-libhiredis
 --without-libmnl
+--with-pyunbound
+--with-pythonmodule
 --with-libnghttp2=$TERMUX_PREFIX
 --with-ssl=$TERMUX_PREFIX
 --with-pidfile=$TERMUX_PREFIX/var/run/unbound.pid
 --with-username=
 "
+
+termux_step_pre_configure() {
+	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	export PYTHON_SITE_PKG=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
+}
 
 termux_step_post_massage() {
 	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/var/run"
