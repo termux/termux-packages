6b2259e24cfe25ef6bfa43f495b750c15fbfbb21
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-08 18:32:34 +0900

    new package: hash-slinger

---
+++ packages/hash-slinger/build.sh
@@ -0,0 +1,34 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/letoams/hash-slinger
+TERMUX_PKG_DESCRIPTION="Various tools to generate special DNS records"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.1
+TERMUX_PKG_SRCURL=https://github.com/letoams/hash-slinger/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2f0de62d561e585747732e44ce9ea5fcef93c75c95d66b684bd13b4e70374df6
+TERMUX_PKG_DEPENDS="ca-certificates, gnupg, openssh, python, swig, unbound"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	_PKG_PYTHON_DEPENDS="dnspython ipaddr m2crypto python-gnupg"
+}
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	local f
+	for f in openpgpkey sshfp tlsa; do
+		install -Dm700 -t $TERMUX_PREFIX/bin ${f}
+		install -Dm600 -t $TERMUX_PREFIX/share/man/man1 ${f}.1
+	done
+}
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	echo "Installing dependencies through pip..."
+	pip3 install ${_PKG_PYTHON_DEPENDS}
+	EOF
+}
