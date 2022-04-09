e24f9dfbea8a7c1fd3be6b263dbd3062a5dc6f4d
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-04-09 21:10:52 +0800

    Create build.sh

---
+++ packages/autoconf-archive/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/autoconf-archive
+TERMUX_PKG_DESCRIPTION="The GNU Autoconf Archive is a collection of more than 500 macros for GNU Autoconf"
+TERMUX_PKG_LICENSE="GPL-1.3"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2022.02.11
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/autoconf-archive/autoconf-archive-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=78a61b611e2eeb55a89e0398e0ce387bcaf57fe2dd53c6fe427130f777ad1e8c
+TERMUX_PKG_DEPENDS="gawk"
