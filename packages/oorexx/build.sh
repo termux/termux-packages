d04d9286f9184730307b5c6123c649b978eb695f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-09 20:32:47 +0900

    oorexx: Link against libandroid-posix-semaphore

--- packages/oorexx/build.sh
+++ packages/oorexx/build.sh
@@ -5,10 +5,10 @@ TERMUX_PKG_LICENSE_FILE="CPLv1.0.txt, NOTICE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.2.0
 _SOVERSION=${TERMUX_PKG_VERSION%%.*}
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/oorexx/oorexx/${TERMUX_PKG_VERSION}/ooRexx-${TERMUX_PKG_VERSION}-source.tar.gz
 TERMUX_PKG_SHA256=ac5af11e7d4d239d2ebe06f40092f4aebf87fc40740b46458bff3b4069ce6e0b
-TERMUX_PKG_DEPENDS="libandroid-wordexp, libc++, libcrypt, libandroid-posix-semaphore"
+TERMUX_PKG_DEPENDS="libandroid-posix-semaphore, libandroid-wordexp, libc++, libcrypt"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_catopen=no"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="CFLAGS=-O0"
@@ -21,6 +21,7 @@ termux_step_pre_configure() {
 	export PATH=$TERMUX_PKG_HOSTBUILD_DIR:$PATH
 
 	CFLAGS+=" -fwrapv -fno-strict-aliasing"
+	LDFLAGS+=" -landroid-posix-semaphore"
 	LDFLAGS+=" -landroid-wordexp -lcrypt $($CC -print-libgcc-file-name)"
 
 	local dummylibdir=$TERMUX_PKG_BUILDDIR/_dummylib
