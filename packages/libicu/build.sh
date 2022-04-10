0edf2de15dff2ee0efb63c30d026efb773546b1f
Yaksh Bariya <yakshbari4@gmail.com>
2022-04-10 20:18:23 +0530

    libicu: update to 71.1

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -5,9 +5,9 @@ TERMUX_PKG_LICENSE="BSD"
 # this hack to be able to find the license file.
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=70.1
+TERMUX_PKG_VERSION=71.1
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
-TERMUX_PKG_SHA256=8d205428c17bf13bb535300669ed28b338a157b1c01ae66d31d0d3e2d47c3fd5
+TERMUX_PKG_SHA256=67a7e6e51f61faf1306b6935333e13b2c48abd8da6d2f46ce6adca24b1e21ebf
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BREAKS="libicu-dev"
 TERMUX_PKG_REPLACES="libicu-dev"
