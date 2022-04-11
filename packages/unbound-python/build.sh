bcb851f939e8fc1596775c3bb02d17b911fc801e
Henrik Grimler <grimler@termux.org>
2022-04-11 10:14:12 +0200

    unbound: add unbound-python as alternative package
    unbound-python then contains the python dependency and python
    bindings, while unbound is built without the python dependency.
    
    Since libgnutls is a core package and depend on unbound it is good to
    keep its dependency chain as small as possible.  Packages like
    hash-slinger that need the python bindings can then depend on
    unbound-python instead.
    
    Since unbound-python has TERMUX_PKG_PROVIDES="unbound", it is possible
    to install unbound-python (which uninstalls unbound) and still satisfy
    libgnutls's dependencies.

--- packages/unbound-python/build.sh
+++ packages/unbound-python/build.sh
@@ -1,14 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://unbound.net/
-TERMUX_PKG_DESCRIPTION="A validating, recursive, caching DNS resolver"
+TERMUX_PKG_DESCRIPTION="A validating, recursive, caching DNS resolver with python support"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-# Update unbound-python at the same time as unbound
+# Update unbound at the same time as unbound-python
 TERMUX_PKG_VERSION=1.15.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nlnetlabs.nl/downloads/unbound/unbound-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a480dc6c8937447b98d161fe911ffc76cfaffa2da18788781314e81339f1126f
-TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl"
+TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl, python"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_CONFLICTS="unbound"
+TERMUX_PKG_REPLACES="unbound"
+TERMUX_PKG_PROVIDES="unbound"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_chown=no
@@ -22,11 +25,27 @@ ac_cv_func_getpwnam=no
 --with-libexpat=$TERMUX_PREFIX
 --without-libhiredis
 --without-libmnl
+--with-pyunbound
+--with-pythonmodule
 --with-libnghttp2=$TERMUX_PREFIX
 --with-ssl=$TERMUX_PREFIX
 --with-pidfile=$TERMUX_PREFIX/var/run/unbound.pid
 --with-username=
 "
+
+termux_step_pre_configure() {
+	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	export PYTHON_SITE_PKG=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
+}
 
 termux_step_post_massage() {
 	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/var/run"
