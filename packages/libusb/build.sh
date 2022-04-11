f20fb9c1b20c7e8420faa748c63da8e06971db3a
Henrik Grimler <henrik@grimler.se>
2022-04-11 20:21:35 +0200

    libusb: use official tar.bz2 archive instead
    Saves us the trouble of autoconfiguring.
    
    Fixes https://github.com/termux/termux-packages/issues/9962.

--- packages/libusb/build.sh
+++ packages/libusb/build.sh
@@ -3,13 +3,10 @@ TERMUX_PKG_DESCRIPTION="A C library that provides generic access to USB devices"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.0.26"
-TERMUX_PKG_SRCURL=https://github.com/libusb/libusb/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a09bff99c74e03e582aa30759cada218ea8fa03580517e52d463c59c0b25e240
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/libusb/libusb/releases/download/v${TERMUX_PKG_VERSION}/libusb-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="libusb-dev"
 TERMUX_PKG_REPLACES="libusb-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-udev"
-
-termux_step_pre_configure() {
-	NOCONFIGURE=true ./autogen.sh
-}
