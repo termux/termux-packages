43b9d06b504e341fb835c8b97b0ea831ca06df21
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-11 13:39:26 +0900

    new package: libimagequant

---
+++ packages/libimagequant/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://pngquant.org/lib/
+TERMUX_PKG_DESCRIPTION="Small, portable C library for high-quality conversion of RGBA images to 8-bit indexed-color (palette) images"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE_FILE="COPYRIGHT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.17.0
+TERMUX_PKG_SRCURL=https://github.com/ImageOptim/libimagequant/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9f6cc50182be4d2ece75118aa0b0fd3e9bbad06e94fd6b9eb3a4c08129c2dd26
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-sse
+"
+
+termux_step_post_get_source() {
+	rm -f CMakeLists.txt
+}
