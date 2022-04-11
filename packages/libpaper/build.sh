9ed431e1fb78ed881b1ba813e537d79269711f6e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-11 12:40:15 +0900

    new package: libpaper

---
+++ packages/libpaper/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://packages.debian.org/unstable/source/libpaper
+TERMUX_PKG_DESCRIPTION="Library for handling paper characteristics"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.28
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/libp/libpaper/libpaper_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c8bb946ec93d3c2c72bbb1d7257e90172a22a44a07a07fb6b802a5bb2c95fddc
+TERMUX_PKG_DEPENDS="debianutils"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--sysconfdir=$TERMUX_PREFIX/etc
+"
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	mkdir -p $TERMUX_PREFIX/etc/libpaper.d
+	EOF
+}
