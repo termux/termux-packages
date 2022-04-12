12ebdd7e13c0881928f90d5829578f5f5d8ecca6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-12 18:30:21 +0900

    new package: suil

---
+++ packages/suil/build.sh
@@ -0,0 +1,29 @@
+TERMUX_PKG_HOMEPAGE=https://drobilla.net/software/suil.html
+TERMUX_PKG_DESCRIPTION="A library for loading and wrapping LV2 plugin UIs"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.10.10
+TERMUX_PKG_SRCURL=https://download.drobilla.net/suil-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=750f08e6b7dc941a5e694c484aab02f69af5aa90edcc9fb2ffb4fb45f1574bfb
+TERMUX_PKG_DEPENDS="lv2"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--no-gtk
+--no-qt
+--no-x11
+"
+
+termux_step_configure() {
+	./waf configure \
+		--prefix=$TERMUX_PREFIX \
+		LINKFLAGS="$LDFLAGS" \
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+}
+
+termux_step_make() {
+	./waf
+}
+
+termux_step_make_install() {
+	./waf install
+}
