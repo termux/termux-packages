c5d3c5e4d67a5a76475b656bfee203dec9f397bb
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-15 01:53:51 +0530

    feat(sops): enable auto-update, upgrade to 3.7.2
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/sops/build.sh
+++ packages/sops/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mozilla/sops
 TERMUX_PKG_DESCRIPTION="Simple and flexible tool for managing secrets"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="Philipp Schmitt @pschmitt"
-TERMUX_PKG_VERSION=3.7.1
+TERMUX_PKG_VERSION=3.7.2
 TERMUX_PKG_SRCURL="https://github.com/mozilla/sops/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=536ee140d888b53b71c1e8edd669f4c11bc573428983fbea644fbbfcd7d7079a
+TERMUX_PKG_SHA256=905d0d85e6f3a36eb0586bae5c1bf501445841303e597136f69a33040f5123b2
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make_install() {
 	termux_setup_golang
