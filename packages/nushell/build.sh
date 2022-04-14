930042d031694118cb63c1ea09982edea883e8c4
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-14 12:35:34 +0530

    chore(nushell): update to 0.61.0
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/nushell/build.sh
+++ packages/nushell/build.sh
@@ -2,22 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://www.nushell.sh
 TERMUX_PKG_DESCRIPTION="A new type of shell operating on structured data"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.60.0
+TERMUX_PKG_VERSION=0.61.0
 TERMUX_PKG_SRCURL=https://github.com/nushell/nushell/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9b9deb8e42cf18ccf328833a4d051fc14cef0be468afc3b0dbde6657deb9f079
+TERMUX_PKG_SHA256=9d12eafd790fbd7bde4d4a71bf5121cc4897fbaf18c0ac50167fb6153167fe57
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, zlib"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_CONFIGURE_EXTRA_ARGS="--features=extra"
 
 termux_step_pre_configure() {
-	# CARGO_TARGET_NAME is not set before termux_setup_rust
-	local _CARGO_TARGET_NAME
-	if [ $TERMUX_ARCH = "arm" ]; then
-		_CARGO_TARGET_NAME=armv7-linux-androideabi
-	else
-		_CARGO_TARGET_NAME=$TERMUX_ARCH-linux-android
-	fi
-	local _CARGO_TARGET_LIBDIR=target/$_CARGO_TARGET_NAME/release/deps
+	termux_setup_rust
+
+	export CFLAGS="${TARGET_CFLAGS}"
+
+	local _CARGO_TARGET_LIBDIR="target/${CARGO_TARGET_NAME}/release/deps"
 	mkdir -p $_CARGO_TARGET_LIBDIR
 
 	if [ $TERMUX_ARCH = "i686" ]; then
@@ -25,9 +24,20 @@ termux_step_pre_configure() {
 	elif [ $TERMUX_ARCH = "x86_64" ]; then
 		pushd $_CARGO_TARGET_LIBDIR
 		local libgcc="$($CC -print-libgcc-file-name)"
-		echo "INPUT($libgcc -l:libunwind.a)" > libgcc.so
+		echo "INPUT($libgcc -l:libunwind.a)" >libgcc.so
 		popd
 	fi
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	for d in $CARGO_HOME/registry/src/github.com-*/libgit2-sys-*/libgit2; do
+		patch --silent -p1 -d ${d} \
+			<$TERMUX_SCRIPTDIR/packages/libgit2/src-rand.c.patch || :
+		cp $TERMUX_SCRIPTDIR/packages/libgit2/getloadavg.c ${d}/src/ || :
+	done
 
 	mv $TERMUX_PREFIX/lib/libz.so.1{,.tmp}
 	mv $TERMUX_PREFIX/lib/libz.so{,.tmp}
