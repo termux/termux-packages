796df09724cbd883397a25e5c91b771956d643da
Yaksh Bariya <yakshbari4@gmail.com>
2022-04-14 11:50:56 +0530

    ccls: build from latest commit & llvm 14 rebuild

--- packages/ccls/build.sh
+++ packages/ccls/build.sh
@@ -2,14 +2,25 @@ TERMUX_PKG_HOMEPAGE=https://github.com/MaskRay/ccls
 TERMUX_PKG_DESCRIPTION="C/C++/ObjC language server"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.20210330
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/MaskRay/ccls/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=28c228f49dfc0f23cb5d581b7de35792648f32c39f4ca35f68ff8c9cb5ce56c2
+_COMMIT=790daca4b2d9d5873623fee86283cd61212df674
+_COMMIT_DATE=2022.01.30
+TERMUX_PKG_VERSION=0.20210330p${_COMMIT_DATE//./}
+TERMUX_PKG_SRCURL=https://github.com/MaskRay/ccls.git
+TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libllvm"
 TERMUX_PKG_BUILD_DEPENDS="rapidjson, libllvm-static"
 
 termux_step_pre_configure() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$_COMMIT_DATE" ]; then
+		echo -n "ERROR: The specified commit date \"$_COMMIT_DATE\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
+
 	touch $TERMUX_PREFIX/bin/{clang-import-test,clang-offload-wrapper}
 }
 
