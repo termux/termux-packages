c6090e7d2f11d375fafa27d41b027b8710dc95a9
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-15 01:45:17 +0530

    feat(uwsgi): enable auto-update, upgrade to 2.0.20
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/uwsgi/build.sh
+++ packages/uwsgi/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://projects.unbit.it/uwsgi
 TERMUX_PKG_DESCRIPTION="uWSGI application server container"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.19.1
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=2.0.20
 TERMUX_PKG_SRCURL=https://github.com/unbit/uwsgi/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bf17cdbb9bd8bcb7c1633e34d9d7308cb4cc19eb0ff2d61057f840c1ba1fc41b
+TERMUX_PKG_SHA256=88ab9867d8973d8ae84719cf233b7dafc54326fcaec89683c3f9f77c002cdff9
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-sysv-semaphore, libcap, libcrypt, libjansson, libuuid, libxml2, openssl, pcre, python"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 termux_step_post_get_source() {
 	cp $TERMUX_PKG_BUILDER_DIR/sys_time.c ./core/
