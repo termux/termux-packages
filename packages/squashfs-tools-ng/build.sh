440f1e77511e1de58f96446fb526501cf7e8b9ad
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-15 01:51:30 +0530

    feat(squashfs-tools-ng): enable auto-update, upgrade to 1.1.4
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/squashfs-tools-ng/build.sh
+++ packages/squashfs-tools-ng/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/AgentD/squashfs-tools-ng
 TERMUX_PKG_DESCRIPTION="New set of tools for working with SquashFS images"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@wmcbtech30"
-TERMUX_PKG_VERSION="1.1.3"
+TERMUX_PKG_VERSION=1.1.4
 TERMUX_PKG_SRCURL="https://github.com/AgentD/squashfs-tools-ng/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256="4214083eb921b89d194e41697c31dc086c059518e51fe0ee4eee88318c19ad0d"
+TERMUX_PKG_SHA256=21fda419865684e564f7300b88c761e881bffd2cdcb46d0f8721ae10bfda6eeb
 TERMUX_PKG_DEPENDS="zlib, liblz4, liblzo, zstd"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 termux_step_pre_configure(){
 	autoreconf -fi
