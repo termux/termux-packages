c7477769249adf0c276d028a1a205d1d2a30f5b5
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-16 16:06:53 +0530

    feat(vtm): enable auto-update, upgrade to 0.6.0
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://vtm.netxs.online/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-party session sharing"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.5.15
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.6.0
 TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fa18ae2f1eee9615c6e0df5be4b248fb15268c5e7401380f129ccee8cb9254f0
+TERMUX_PKG_SHA256=b572a7f7f4b1271faf6dac5c0f91505b878eee1cfcbcca4a9323b97e9325e16b
 TERMUX_PKG_DEPENDS="libandroid-spawn, libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+=/src
