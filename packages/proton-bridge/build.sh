80e0b6b97612c93f543eebd7fbbd900c6239d325
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-16 16:18:17 +0530

    feat(proton-bridge): enable auto-update, upgrade to 2.1.3
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/proton-bridge/build.sh
+++ packages/proton-bridge/build.sh
@@ -1,19 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ProtonMail/proton-bridge
 TERMUX_PKG_DESCRIPTION="ProtonMail Bridge application"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=1.8.10
+TERMUX_PKG_VERSION=2.1.3
 TERMUX_PKG_SRCURL=https://github.com/ProtonMail/proton-bridge.git
 TERMUX_PKG_GIT_BRANCH=br-$TERMUX_PKG_VERSION
 TERMUX_PKG_MAINTAINER="Radomír Polách <rp@t4d.cz>"
 TERMUX_PKG_DEPENDS=libsecret
-
-# On 32bit arches we get:
-# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:2756: constant 4230534781 overflows int
-# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:2780: constant 4230534781 overflows int
-# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:6683: constant 2740715144 overflows int
-# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:6683: constant 4294967167 overflows int
-# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:11728: constant 4244674173 overflows int
-# ../build/pkg/mod/github.com/!proton!mail/go-rfc5322@v0.8.0/parser/rfc5322_parser.go:12153: constant 4292870143 overflows int
+TERMUX_PKG_AUTO_UPDATE=true
+# The go-rfc5322 module cannot currently be compiled for 32-bit OSes:
+# https://github.com/ProtonMail/proton-bridge/blob/v2.1.1/BUILDS.md#prerequisites
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_make() {
