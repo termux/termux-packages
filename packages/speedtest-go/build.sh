48e1a6a923876d5fc74a0a71fe06c43ddf2f5525
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-17 00:45:35 +0900

    new package: speedtest-go

---
+++ packages/speedtest-go/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/showwin/speedtest-go/
+TERMUX_PKG_DESCRIPTION="Command line interface to test internet speed using speedtest.net"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.5
+TERMUX_PKG_SRCURL=https://github.com/showwin/speedtest-go/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e005e0244267ea3e3e12ea2c2d50e8631f90f8f21e4bd31415ad45b24d267cea
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin speedtest-go
+}
