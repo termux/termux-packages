43b02253507d14237faa3711ed613b749312af94
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-16 15:20:18 +0530

    feat(youtubedr): add shell completions
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/youtubedr/build.sh
+++ packages/youtubedr/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Download youtube video in Golang"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=2.7.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kkdai/youtube/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=04a4f0b745094884fb2902945ac3b1c966fbe4fbd67a027cb482d491662900f9
 TERMUX_PKG_AUTO_UPDATE=true
@@ -22,4 +23,26 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/kkdai/youtube/cmd/youtubedr/youtubedr
+}
+
+termux_step_create_debscripts() {
+	cat <<-EOF >./postinst
+		#!${TERMUX_PREFIX}/bin/sh
+
+		mkdir -p ${TERMUX_PREFIX}/share/bash-completion/completions
+		mkdir -p ${TERMUX_PREFIX}/share/zsh/site-functions
+		mkdir -p ${TERMUX_PREFIX}/share/fish/vendor_completions.d
+
+		youtubedr completion bash > ${TERMUX_PREFIX}/share/bash-completion/completions/youtubedr
+		youtubedr completion zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_youtubedr
+		youtubedr completion fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/youtubedr.fish
+	EOF
+
+	cat <<-EOF >./prerm
+		#!${TERMUX_PREFIX}/bin/sh
+
+		rm -f ${TERMUX_PREFIX}/share/bash-completion/completions/youtubedr
+		rm -f ${TERMUX_PREFIX}/share/zsh/site-functions/_youtubedr
+		rm -f ${TERMUX_PREFIX}/share/fish/vendor_completions.d/youtubedr.fish
+	EOF
 }
