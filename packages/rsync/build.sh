e01a3d406ac37889be6a16660279b85ede529ccb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-17 01:13:28 +0900

    rsync: Update to 3.2.4

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://rsync.samba.org/
 TERMUX_PKG_DESCRIPTION="Utility that provides fast incremental file transfer"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.2.3
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=3.2.4
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=becc3c504ceea499f4167a260040ccf4d9f2ef9499ad5683c179a697146ce50e
+TERMUX_PKG_SHA256=6f761838d08052b0b6579cf7f6737d93e47f01f4da04c5d24d3447b7f2a5fad1
 TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl, openssl-tool, zlib, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
 
