9380f7c598e496dc4c21a39a303c879826b939ba
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-17 03:37:09 +0900

    enchant: Apply compiler-rt builtins hack

--- packages/enchant/build.sh
+++ packages/enchant/build.sh
@@ -3,12 +3,17 @@ TERMUX_PKG_DESCRIPTION="Wraps a number of different spelling libraries and progr
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.3.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/AbiWord/enchant/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5952a792570eb232af464331b537381aca8bc711a1d8c4251e1f035e9cca8cfb
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-relocatable" 
-TERMUX_PKG_DEPENDS="aspell, glib, hunspell"
+TERMUX_PKG_DEPENDS="aspell, glib, hunspell, libc++"
 
 termux_step_post_get_source() {
 	./bootstrap
+}
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
