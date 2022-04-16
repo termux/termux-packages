b493600720e1ce7cf62e9634acf99dd06ffa22b8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-16 16:44:07 +0900

    xdelta3: Define `_FILE_OFFSET_BITS` before `#include`s

--- packages/xdelta3/build.sh
+++ packages/xdelta3/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='xdelta3 - VCDIFF (RFC 3284) binary diff tool'
 TERMUX_PKG_LICENSE=Apache-2.0
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.1.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jmacd/xdelta/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7515cf5378fca287a57f4e2fee1094aabc79569cfe60d91e06021a8fd7bae29d
 TERMUX_PKG_DEPENDS=liblzma
@@ -13,4 +14,6 @@ termux_step_post_get_source() {
 
 termux_step_pre_configure() {
 	autoreconf --install
+
+	CPPFLAGS+=" -DXD3_USE_LARGEFILE64 -D_FILE_OFFSET_BITS=64"
 }
