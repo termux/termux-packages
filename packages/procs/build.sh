cd3cd5b5e180f2d5c4918a990f29710981394acd
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-16 16:19:23 +0530

    feat(procs): enable auto-update, upgrade to 0.12.1
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/procs/build.sh
+++ packages/procs/build.sh
@@ -2,11 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dalance/procs
 TERMUX_PKG_DESCRIPTION="A modern replacement for ps"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.12.0
+TERMUX_PKG_VERSION=0.12.1
 TERMUX_PKG_SRCURL=https://github.com/dalance/procs/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=04dbb3ddc734a9d1305b26b546a913e3e04d556b5863212a2675d29268aebed3
+TERMUX_PKG_SHA256=ce84e98dd85cb8d5afda871ad2ddceb4faec077d9e019469aa668a75821e4fc2
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
+# This package contains makefiles to run the tests. So, we need to override build steps.
 termux_step_make() {
 	termux_setup_rust
 	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
