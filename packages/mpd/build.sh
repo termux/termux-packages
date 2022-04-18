a8b48fb720e96c9d3a7a1809ee14ce371bc70de4
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-18 16:28:49 +0530

    feat(mpd): enable auto-update, upgrade to 0.23.6
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/mpd/build.sh
+++ packages/mpd/build.sh
@@ -2,24 +2,27 @@ TERMUX_PKG_HOMEPAGE=https://www.musicpd.org
 TERMUX_PKG_DESCRIPTION="Music player daemon"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.23.5
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.23.6
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/MPD/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=d57aec9100539dc6bdb8b06d40114e44d765481bee6bc900d6403e7b67dc8e92
-TERMUX_PKG_DEPENDS="ffmpeg, libbz2, libc++, libcurl, libexpat, libflac, libid3tag, libmp3lame, libmpdclient, libnfs, libogg, libopus, libsndfile, libsoxr, libsqlite, libvorbis, openal-soft, pulseaudio, zlib"
+TERMUX_PKG_SHA256=38d537ff1ea38c727d299d7878f8622fba6d41453ea5e9821a61528b38c7ec7a
+TERMUX_PKG_DEPENDS="ffmpeg, libbz2, libc++, libcurl, libexpat, libflac, libid3tag, libmp3lame, libmpdclient, libnfs, libogg, libopus, libsndfile, libsoxr, libsqlite, libvorbis, openal-soft, pulseaudio, zlib, libao, libmad, libiconv, libicu, zlib, dbus"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dalsa=disabled
--Dao=disabled
 -Depoll=false
--Diconv=disabled
--Dicu=disabled
--Dmad=disabled
--Dpcre=disabled
 -Dsndio=disabled
+-Dzlib=enabled
+-Dicu=enabled
+-Diconv=enabled
+-Dpcre=enabled
+-Dexpat=enabled
+-Ddbus=enabled
+-Dipv6=enabled
 "
 TERMUX_PKG_CONFFILES="etc/mpd.conf"
 TERMUX_PKG_SERVICE_SCRIPT=("mpd" "if [ -f \"$TERMUX_ANDROID_HOME/.mpd/mpd.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.mpd/mpd.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/mpd.conf\"; fi\nexec mpd --stdout --no-daemon \$CONFIG 2>&1")
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
@@ -40,6 +43,6 @@ termux_step_post_make_install() {
 }
 
 termux_step_create_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo 'mkdir -p $HOME/.mpd/playlists' >> postinst
+	echo "#!$TERMUX_PREFIX/bin/sh" >postinst
+	echo 'mkdir -p $HOME/.mpd/playlists' >>postinst
 }
