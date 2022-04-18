ae3275eec6dcb530634d5807d35005f9d2aaa08e
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-18 16:17:57 +0530

    feat(onefetch): enable auto-update, upgrade to 2.12.0
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/onefetch/build.sh
+++ packages/onefetch/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/o2sh/onefetch
 TERMUX_PKG_DESCRIPTION="A command-line Git information tool written in Rust"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@ELWAER-M"
-TERMUX_PKG_VERSION=2.11.0
+TERMUX_PKG_VERSION=2.12.0
 TERMUX_PKG_SRCURL=https://github.com/o2sh/onefetch/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ffd3cc3bd24e299ede1fada2b2da8bf066d59219da167477e1997c860650c192
+TERMUX_PKG_SHA256=f57b16dfa2bb95dd1fb805257a1761baa20d69eb9ce7c311d369609894c53897
 TERMUX_PKG_DEPENDS="libgit2"
 TERMUX_PKG_BUILD_IN_SRC=true
 	
@@ -19,3 +19,4 @@ termux_step_make() {
 termux_step_make_install() {
 	install -Dm700 target/"${CARGO_TARGET_NAME}"/release/onefetch "$TERMUX_PREFIX"/bin
 }
+TERMUX_PKG_AUTO_UPDATE=true
