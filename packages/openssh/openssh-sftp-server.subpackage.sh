dc18dc5ff374cfc0e1e2a7428e751d3143936932
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-18 19:50:23 +0900

    openssh: Update to 9.0p1
    * Separate sftp-server as a subpackage (named openssh-sftp-server)

---
+++ packages/openssh/openssh-sftp-server.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_INCLUDE="
+libexec/sftp-server
+share/man/man8/sftp-server.8.gz
+"
+TERMUX_SUBPKG_DESCRIPTION="OpenSSH SFTP server subsystem"
+TERMUX_SUBPKG_DEPENDS="libandroid-support, openssl"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
