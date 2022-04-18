f3ea5b1d57ccd819738cbbc83b2a629423f13235
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-18 16:31:18 +0530

    feat(mdbook-mermaid): enable auto-update, upgrade to 0.10.0
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/mdbook-mermaid/build.sh
+++ packages/mdbook-mermaid/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/badboy/mdbook-mermaid
 TERMUX_PKG_DESCRIPTION="A preprocessor for mdbook to add mermaid support"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.8.3
+TERMUX_PKG_VERSION=0.10.0
 TERMUX_PKG_SRCURL=https://github.com/badboy/mdbook-mermaid.git
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_rust
