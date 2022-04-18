dc097b462a4431ffb2d65d86cfe63fa9fc353e9a
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-18 16:12:52 +0530

    feat(pdftk): enable auto-update, upgrade to 3.3.2
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/pdftk/build.sh
+++ packages/pdftk/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/pdftk-java/pdftk
 TERMUX_PKG_DESCRIPTION="A simple tool for doing everyday things with PDF documents"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.3.1
+TERMUX_PKG_VERSION=3.3.2
 TERMUX_PKG_SRCURL=https://gitlab.com/pdftk-java/pdftk/-/archive/v${TERMUX_PKG_VERSION}/pdftk-v${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=1d1199b02108b4f762209534b8342e79f474c007384dfa70174514e91205a2a7
+TERMUX_PKG_SHA256=2389120140dc43cab4d3cdaf4f9b2f6b5e37b50afe60795e0f3726312c29fb18
 TERMUX_PKG_DEPENDS="libbcprov-java, libcommons-lang3-java, openjdk-17"
 TERMUX_PKG_BUILD_DEPENDS="ant"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	mkdir -p lib
