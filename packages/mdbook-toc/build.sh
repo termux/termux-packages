b50a61bb012cec8fdfeb0a1edbe3330d8c424b0e
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-18 16:30:20 +0530

    feat(mdbook-toc): enable auto-update, upgrade to 0.8.0
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/mdbook-toc/build.sh
+++ packages/mdbook-toc/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/badboy/mdbook-toc
 TERMUX_PKG_DESCRIPTION="A preprocessor for mdbook to add inline Table of Contents support"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.0
+TERMUX_PKG_VERSION=0.8.0
 TERMUX_PKG_SRCURL=https://github.com/badboy/mdbook-toc.git
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_rust
