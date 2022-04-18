8dee3fffbace1fdb0e814cde5c6f7eb0443b128e
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-18 16:27:48 +0530

    feat(mpdscribble): enable auto-update, upgrade to 0.24
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/mpdscribble/build.sh
+++ packages/mpdscribble/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.musicpd.org/clients/mpdscribble/
 TERMUX_PKG_DESCRIPTION="A Music Player Daemon (MPD) client which submits information about tracks being played to a scrobbler"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=0.23
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=0.24
 TERMUX_PKG_SRCURL=https://github.com/MusicPlayerDaemon/mpdscribble/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8b3b79538fa9871ce61d7fb793d442c72b6aadf80af4c5eb664286f613d1feee
+TERMUX_PKG_SHA256=efd9dabc9850ff29412eaebd3377f107fe1f0fbc0e7967ee2d73d1967b375bf6
 TERMUX_PKG_DEPENDS="libcurl, libgcrypt, mpd, libmpdclient, glib"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_CONFFILES="etc/mpdscribble.conf"
@@ -15,6 +14,8 @@ TERMUX_PKG_SERVICE_SCRIPT=(
 	"mpdscribble" "if [ -f \"$TERMUX_ANDROID_HOME/.mpdscribble/mpdscribble.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.mpdscribble/mpdscribble.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/mpdscribble.conf\"; fi\nexec mpdscribble -D --log /proc/self/fd/1 --conf \$CONFIG"
 	"mpdscribble/log" 'mkdir -p "$LOGDIR/sv/mpdscribble"\nexec svlogd "$LOGDIR/sv/mpdscribble"'
 )
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 termux_step_pre_configure() {
 	export BOOST_ROOT=$TERMUX_PREFIX
