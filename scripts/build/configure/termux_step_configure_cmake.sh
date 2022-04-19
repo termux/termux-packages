termux/termux-packages

commit 9155acd04029e648cc711d1ddf8a41491e62fa24

termux_step_configure_cmake: set CMAKE_INSTALL_LIBDIR
Some packages seem to need it, but maybe just when building outside
docker image?  Set in termux_step_configure_cmake instead of in
individual build recipes.

%ci:no-build

scripts/build/configure/termux_step_configure_cmake.sh
@@ -40,6 +40,7 @@ termux_step_configure_cmake() {
 		-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
 		-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
 		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
+		-DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib \
 		-DCMAKE_MAKE_PROGRAM=$MAKE_PROGRAM_PATH \
 		-DCMAKE_SKIP_INSTALL_RPATH=ON \
 		-DCMAKE_USE_SYSTEM_LIBRARIES=True \

packages/c-toxcore/build.sh
@@ -11,8 +11,6 @@ TERMUX_PKG_BREAKS="c-toxcore-dev"
 TERMUX_PKG_REPLACES="c-toxcore-dev"
 #TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}
--DCMAKE_INSTALL_LIBDIR=${TERMUX_PREFIX}/lib
 -DBOOTSTRAP_DAEMON=off
 -DDHT_BOOTSTRAP=off
 -DBUILD_TOXAV=off

packages/cmark/build.sh
@@ -8,4 +8,3 @@ TERMUX_PKG_SHA256=6c7d2bcaea1433d977d8fed0b55b71c9d045a7cdf616e3cd2dce9007da753d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="cmark-dev"
 TERMUX_PKG_REPLACES="cmark-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib"

packages/libjasper/build.sh
@@ -13,5 +13,4 @@ TERMUX_PKG_REPLACES="libjasper-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -H$TERMUX_PKG_SRCDIR
 -B$TERMUX_PKG_BUILDDIR
--DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib
 "

commit 2af00064dd399a68ea9d8515bb10525648878c5e

termux_step_configure_cmake: fix CMAKE_INSTALL_LIBDIR var
CMake docs [1] show that the variable should be a path relative to
prefix, not an absolute path.

[1] https://cmake.org/cmake/help/latest/module/GNUInstallDirs.html
Fixes: 9155acd04029 ("termux_step_configure_cmake: set CMAKE_INSTALL_LIBDIR")
Fixes: https://github.com/termux/termux-packages/issues/10068

scripts/build/configure/termux_step_configure_cmake.sh
@@ -40,7 +40,7 @@ termux_step_configure_cmake() {
 		-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
 		-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
 		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX \
-		-DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib \
+		-DCMAKE_INSTALL_LIBDIR=lib \
 		-DCMAKE_MAKE_PROGRAM=$MAKE_PROGRAM_PATH \
 		-DCMAKE_SKIP_INSTALL_RPATH=ON \
 		-DCMAKE_USE_SYSTEM_LIBRARIES=True \