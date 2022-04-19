30c61eaa07706658d300ba1cf865d6af8f5564c0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-20 00:44:20 +0900

    xpdf: Remove files conflicting with poppler

--- x11-packages/xpdf/build.sh
+++ x11-packages/xpdf/build.sh
@@ -3,7 +3,28 @@ TERMUX_PKG_DESCRIPTION="Xpdf is an open source viewer for Portable Document Form
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.03
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://dl.xpdfreader.com/xpdf-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_REVISION=4
+#TERMUX_PKG_SRCURL=https://dl.xpdfreader.com/xpdf-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SRCURL=https://ftp-osl.osuosl.org/pub/gentoo/distfiles/xpdf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0fe4274374c330feaadcebb7bd7700cb91203e153b26aa95952f02bf130be846
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg"
+
+# Remove files conflicting with poppler:
+TERMUX_PKG_RM_AFTER_INSTALL="
+bin/pdfdetach
+bin/pdffonts
+bin/pdfimages
+bin/pdfinfo
+bin/pdftohtml
+bin/pdftoppm
+bin/pdftops
+bin/pdftotext
+share/man/man1/pdfdetach.1
+share/man/man1/pdffonts.1
+share/man/man1/pdfimages.1
+share/man/man1/pdfinfo.1
+share/man/man1/pdftohtml.1
+share/man/man1/pdftoppm.1
+share/man/man1/pdftops.1
+share/man/man1/pdftotext.1
+"
