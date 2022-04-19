ef00d239958078a36331249abea144b00aa6e3eb
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-19 15:55:56 +0530

    feat(libvmaf): enable auto-update, upgrade to 2.3.1
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/libvmaf/build.sh
+++ packages/libvmaf/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A perceptual video quality assessment algorithm develope
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.3.0
+TERMUX_PKG_VERSION=2.3.1
 TERMUX_PKG_SRCURL=https://github.com/Netflix/vmaf/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d8dcc83f8e9686e6855da4c33d8c373f1735d87294edbd86ed662ba2f2f89277
+TERMUX_PKG_SHA256=8d60b1ddab043ada25ff11ced821da6e0c37fd7730dd81c24f1fc12be7293ef2
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/libvmaf
