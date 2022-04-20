6d6f0c91f36fbdc4633936a81e443999fc16f4d1
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-20 19:51:20 +0530

    fix(hicolor-icon-theme): disable auto-update
    there is no SRC URL to update from
    
    %ci:no-build
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- x11-packages/hicolor-icon-theme/build.sh
+++ x11-packages/hicolor-icon-theme/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=0.17
 TERMUX_PKG_REVISION=28
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false # No src url to update from.
 
 termux_step_make_install() {
 	install -Dm644 "${TERMUX_PKG_BUILDER_DIR}/index.theme" "${TERMUX_PREFIX}/share/icons/hicolor/index.theme"
