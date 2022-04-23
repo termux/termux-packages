cb2a4d84f2cc6b8e793a109f5a4b6f8bf0753ff0
Henrik Grimler <grimler@termux.org>
2022-04-23 22:17:21 +0200

    science-repo: update repo url
    Use repo hosted at github instead of bintray, as bintray has been shut
    down.

--- packages/science-repo/build.sh
+++ packages/science-repo/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/science-packages
 TERMUX_PKG_DESCRIPTION="Package repository containing science software"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.1
 TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
-	echo "deb https://dl.bintray.com/grimler/science-packages-21 science stable" > $TERMUX_PREFIX/etc/apt/sources.list.d/science.list
+	echo "deb https://termux.org/science-packages-21-bin science stable" > $TERMUX_PREFIX/etc/apt/sources.list.d/science.list
 }
 
 termux_step_create_debscripts() {
