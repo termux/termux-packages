0454e89195ddf9f9b9012c865ee93d11f2efad85
Henrik Grimler <grimler@termux.org>
2022-04-23 22:13:00 +0200

    game-repo: update repo url
    Use repo hosted at github instead of bintray, as bintray has been shut
    down.

--- packages/game-repo/build.sh
+++ packages/game-repo/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/game-packages
 TERMUX_PKG_DESCRIPTION="Package repository containing games for termux"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.1
 TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
-	echo "deb https://dl.bintray.com/grimler/game-packages-21 games stable" > $TERMUX_PREFIX/etc/apt/sources.list.d/game.list
+	echo "deb https://termux.org/game-packages-21-bin games stable" > $TERMUX_PREFIX/etc/apt/sources.list.d/game.list
 }
 
 termux_step_create_debscripts() {
