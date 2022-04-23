dfb4de296ea688ad47d0a406fd984f3e35be9971
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-24 00:10:43 +0900

    vtm: Specify `TERMUX_PKG_LICENSE_FILE`
    instead of using custom `termux_step_install_license` function.
    
    %ci:no-build

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://vtm.netxs.online/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-party session sharing"
 TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.7.1"
 TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
@@ -13,9 +14,4 @@ termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+=/src
 	CXXFLAGS+=" -pthread"
 	LDFLAGS+=" -landroid-spawn"
-}
-
-termux_step_install_license() {
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/vtm \
-		$TERMUX_PKG_SRCDIR/../LICENSE
 }
