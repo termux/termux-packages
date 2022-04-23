509b68701f9b5dcf141f3a13f59b4653e0ad0515
Luka Mamukashvili <ultraluka0@gmail.com>
2022-04-23 14:05:41 +0400

    Create build.sh

---
+++ packages/vtmd/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://vtm.netxs.online/
+TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-party session sharing"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.7.1"
+TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8047f7d9508b5c94f469ba9c9aa7b8bede1dccae21624608a00f3395df594d54
+TERMUX_PKG_DEPENDS="libandroid-spawn, libc++"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	TERMUX_PKG_SRCDIR+=/src
+	CXXFLAGS+=" -pthread"
+	LDFLAGS+=" -landroid-spawn"
+}
+
+termux_step_install_license() {
+	install -Dm600 -t $TERMUX_PREFIX/share/doc/vtm \
+		$TERMUX_PKG_SRCDIR/../LICENSE
+}
