edfc5064a74a281bad41082f689be0da4a5645db
Henrik Grimler <grimler@termux.org>
2022-04-24 10:34:06 +0200

    ca-certificates: sync with master branch and update to 2022-03-29

--- packages/ca-certificates/build.sh
+++ packages/ca-certificates/build.sh
@@ -1,10 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://curl.haxx.se/docs/caextract.html
+TERMUX_PKG_HOMEPAGE=https://curl.se/docs/caextract.html
 TERMUX_PKG_DESCRIPTION="Common CA certificates"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=20191129
-TERMUX_PKG_SRCURL=https://curl.haxx.se/ca/cacert.pem
-# If the checksum has changed, it may be time to update the package version:
-TERMUX_PKG_SHA256=0d98a1a961aab523c9dc547e315e1d79e887dea575426ff03567e455fc0b66b4
+TERMUX_PKG_MAINTAINER="@termux"
+_DATE=2022-03-29
+TERMUX_PKG_VERSION=1:${_DATE//-/.}
+TERMUX_PKG_SRCURL=https://curl.se/ca/cacert-${_DATE}.pem
+TERMUX_PKG_SHA256=1979e7fe618c51ed1c9df43bba92f977a0d3fe7497ffa2a5e80dfc559a1e5a29
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -26,7 +27,7 @@ termux_step_make_install() {
 		$KEYUTIL_JAR \
 		18f1d2c82839d84949b1ad015343c509e81ef678c24db6112acc6c0761314610
 
-	local JAVA_KEYSTORE_DIR=$PREFIX/lib/jvm/openjdk-9/lib/security
+	local JAVA_KEYSTORE_DIR=$TERMUX_PREFIX/opt/openjdk-17/lib/security
 	mkdir -p $JAVA_KEYSTORE_DIR
 
 	java -jar $KEYUTIL_JAR \
