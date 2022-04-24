4e434de0f534b52397805a0f3694dc6c856e7c7c
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-24 19:33:54 +0530

    fix(zoxide): add `-r` options to `cp`, update to 0.8.1
    copy manpages recursively
    
    fix #10306
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/zoxide/build.sh
+++ packages/zoxide/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ajeetdsouza/zoxide
 TERMUX_PKG_DESCRIPTION="A faster way to navigate your filesystem"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_VERSION=0.8.1
 TERMUX_PKG_SRCURL=https://github.com/ajeetdsouza/zoxide/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=111c5f1cd92b1cb54e2f0a801003098a601c653ec5a378f3d0ea1c9659854477
+TERMUX_PKG_SHA256=55f4bf374421f842f5541d95db14d7a63f5d24e269613fb95f769f00cdb9ffc3
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -15,5 +15,5 @@ termux_step_pre_configure() {
 termux_step_post_make_install() {
 	# Install man page:
 	mkdir -p $TERMUX_PREFIX/share/man/man1/
-	cp $TERMUX_PKG_SRCDIR/man/* $TERMUX_PREFIX/share/man/man1/
+	cp -r $TERMUX_PKG_SRCDIR/man/* $TERMUX_PREFIX/share/man/man1/
 }
