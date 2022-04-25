76cf1f3ca6556ef310ffcc2a22a298d9a42d17d2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-25 09:38:35 +0900

    libpipeline: Update to 1.5.6

--- packages/libpipeline/build.sh
+++ packages/libpipeline/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://libpipeline.nongnu.org/
 TERMUX_PKG_DESCRIPTION="C library for manipulating pipelines of subprocesses in a flexible and convenient way"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.5.4
-TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/libpipeline/libpipeline-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=db785bddba0a37ef14b4ef82ae2d18b8824e6983dfb9910319385e28df3f1a9c
+TERMUX_PKG_VERSION=1.5.6
+TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/libpipeline/libpipeline-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=60fbb9e7dc398528e5f3a776af57bb28ca3fe5d9f0cd8a961ac6cebfe6e9b797
 TERMUX_PKG_BREAKS="libpipeline-dev"
 TERMUX_PKG_REPLACES="libpipeline-dev"
