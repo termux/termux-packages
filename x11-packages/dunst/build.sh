3e123608f9df09a6c7ed8ce5ed711d657da8f10d
NoNameMan1231 <87801676+NoNameMan1231@users.noreply.github.com>
2022-04-26 05:39:12 +0800

    New package:dunst (#10275)

---
+++ x11-packages/dunst/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://dunst-project.org/
+TERMUX_PKG_DESCRIPTION="Lightweight and customizable notification daemon"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.8.1
+TERMUX_PKG_SRCURL=https://github.com/dunst-project/dunst/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz 
+TERMUX_PKG_SHA256=90325a4004626ce516ac7b5b0ed1aaab11c5315385d31f781d6f26064601c3a1
+TERMUX_PKG_DEPENDS="dbus, libxinerama, libxrandr, libxss, glib, pango, libcairo, libnotify"
+TERMUX_PKG_BUILD_IN_SRC=true
