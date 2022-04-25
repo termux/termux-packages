ae239ad86f596970837b20c3638322712535ec44
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-26 04:41:47 +0900

    webkit2gtk: Update to 2.36.1

--- x11-packages/webkit2gtk/build.sh
+++ x11-packages/webkit2gtk/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://webkitgtk.org
 TERMUX_PKG_DESCRIPTION="A full-featured port of the WebKit rendering engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.32.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.36.1
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=00ce2d3f798d7bc5e9039d9059f0c3c974d51de38c8b716f00e94452a177d3fd
-TERMUX_PKG_DEPENDS="atk, enchant, fontconfig, freetype, glib, gst-plugins-base, gstreamer, gtk3, harfbuzz, harfbuzz-icu, libc++, libcairo, libgcrypt, libhyphen, libicu, libjpeg-turbo, libnotify, libpng, libsoup, libtasn1, libwebp, libxml2, libx11, libxcomposite, libxdamage, libxslt, libxt, openjpeg, pango, woff2"
+TERMUX_PKG_SHA256=0149ea5fb1d20f2a9981677d45c952a047330001ea24a8dc29035239f12c0c8f
+TERMUX_PKG_DEPENDS="atk, enchant, fontconfig, freetype, glib, gst-plugins-base, gstreamer, gtk3, harfbuzz, harfbuzz-icu, libc++, libcairo, libgcrypt, libhyphen, libicu, libjpeg-turbo, libnotify, libpng, libsoup, libtasn1, libwebp, libxml2, libx11, libxcomposite, libxdamage, libxslt, libxt, littlecms, openjpeg, pango, woff2"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_RECOMMENDS="glib-networking"
 TERMUX_PKG_BREAKS="webkit, webkitgtk"
 TERMUX_PKG_REPLACES="webkit, webkitgtk"
 
@@ -20,4 +20,12 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DUSE_WPE_RENDERER=OFF
 -DENABLE_BUBBLEWRAP_SANDBOX=OFF
 -DUSE_LD_GOLD=OFF
+-DUSE_OPENGL_OR_ES=OFF
+-DENABLE_JOURNALD_LOG=OFF
+-DUSE_SOUP2=ON
 "
+
+termux_step_pre_configure() {
+	CPPFLAGS+=" -DHAVE_MISSING_STD_FILESYSTEM_PATH_CONSTRUCTOR"
+	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
+}
