0d527227349bd3d7a4564e0863cf67698703095c
rsteube <rsteube@users.noreply.github.com>
2022-04-26 00:03:54 +0200

    elvish: updated to v0.18.0

--- packages/elvish/build.sh
+++ packages/elvish/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/elves/elvish
 TERMUX_PKG_DESCRIPTION="A friendly and expressive Unix shell"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.15.0
+TERMUX_PKG_VERSION=0.18.0
 TERMUX_PKG_SRCURL=https://github.com/elves/elvish/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=761739307c68fcbc51fd46c052c0a20ae848a30dba1ce3fbb6d27f99672f58e0
+TERMUX_PKG_SHA256=f4635db90af2241bfd37e17ac1a72567b92d18a396598da2099a908b3d88c590
 
 termux_step_make() {
 	termux_setup_golang
@@ -13,12 +13,12 @@ termux_step_make() {
 	mkdir -p "$GOPATH"/src/github.com/elves
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/elves/elvish
 
-	cd "$GOPATH"/src/github.com/elves/elvish
+	cd "$GOPATH"/src/github.com/elves/elvish/cmd/elvish
 	go build
 }
 
 termux_step_make_install() {
 	install -Dm700 \
-		"$GOPATH"/src/github.com/elves/elvish/elvish \
+		"$GOPATH"/src/github.com/elves/elvish/cmd/elvish/elvish \
 		"$TERMUX_PREFIX"/bin/elvish
 }
