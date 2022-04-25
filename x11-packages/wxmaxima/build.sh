bb4279c69e6d56955a71bf235167fbda2906472a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-26 02:26:24 +0900

    new package: wxmaxima

---
+++ x11-packages/wxmaxima/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://wxmaxima-developers.github.io/wxmaxima/
+TERMUX_PKG_DESCRIPTION="A document based interface for the computer algebra system Maxima"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=22.04.0
+TERMUX_PKG_SRCURL=https://github.com/wxMaxima-developers/wxmaxima/archive/refs/tags/Version-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=48744a3f29fa9abf20788bc0b136c283cbbb0397c74ec79b41e5e6d856d7d65a
+TERMUX_PKG_DEPENDS="libc++, maxima, wxwidgets"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DwxWidgets_CONFIG_EXECUTABLE=$TERMUX_PREFIX/bin/wx-config
+-DWXM_INCLUDE_FONTS=OFF
+-DWXM_UNIT_TESTS=OFF
+"
