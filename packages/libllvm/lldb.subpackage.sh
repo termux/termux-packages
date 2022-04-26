b9fb7f0f3152090c42c3e4f9c369207740bb6b9d
Yaksh Bariya <yakshbari4@gmail.com>
2022-04-26 10:05:55 +0530

    feat(libllvm/lldb): enable python support
    rust-lldb does require python support in lldb. Or else rust-lldb will
    simply fail with "error: module importing failed: This script interpreter
    does not support importing modules." This should be fixed by enabling
    python support for lldb. Also "command script import /path/to/script"
    should now work.

--- packages/libllvm/lldb.subpackage.sh
+++ packages/libllvm/lldb.subpackage.sh
@@ -4,6 +4,6 @@ include/lldb/
 lib/liblldb.so
 "
 TERMUX_SUBPKG_DESCRIPTION="LLVM-based debugger"
-TERMUX_SUBPKG_DEPENDS="libc++, libedit, libxml2, ncurses-ui-libs"
+TERMUX_SUBPKG_DEPENDS="libc++, libedit, libxml2, python, ncurses-ui-libs"
 TERMUX_SUBPKG_BREAKS="lldb-dev, lldb-static"
 TERMUX_SUBPKG_REPLACES="lldb-dev, lldb-static"
