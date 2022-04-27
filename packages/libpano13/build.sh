dfd75fdbc1ea25afed00b15521772a9897d930e7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-27 12:13:07 +0900

    new package: libpano13

---
+++ packages/libpano13/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://panotools.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="Part of the Panorama Tools by Helmut Dersch"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.9.21
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/panotools/libpano13-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=79e5a1452199305e2961462720ef5941152779c127c5b96fc340d2492e633590
+TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff, zlib"
