a02dca75ba2d857e2cdbd96fa4beff2b620f1b1e
Henrik Grimler <grimler@termux.org>
2022-04-27 11:52:44 +0200

    termux-keyring: remove xeffyr's key

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux
 TERMUX_PKG_DESCRIPTION="GPG public keys for the official Termux repositories"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_VERSION=1.3
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -10,7 +10,6 @@ termux_step_make_install() {
 
 	# Maintainer-specific keys.
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/xeffyr.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 
 	# Key for automatic builds (via CI).
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
