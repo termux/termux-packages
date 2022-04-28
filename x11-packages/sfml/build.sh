6a53312dd3054659dbaadeeb90668ecc9353aa3a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-29 03:56:41 +0900

    sfml: Fix unresolved symbols issue

--- x11-packages/sfml/build.sh
+++ x11-packages/sfml/build.sh
@@ -3,17 +3,16 @@ TERMUX_PKG_DESCRIPTION="A simple, fast, cross-platform and object-oriented multi
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.5.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/SFML/SFML/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=438c91a917cc8aa19e82c6f59f8714da353c488584a007d401efac8368e1c785
 TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxrandr, mesa, openal-soft"
 
 termux_step_post_get_source() {
-	cp src/SFML/Window/Android/JoystickImpl.cpp src/SFML/Window/Unix/
+	cp src/SFML/Window/Android/JoystickImpl.{cpp,hpp} src/SFML/Window/Unix/
 }
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -DSFML_SYSTEM_LINUX"
-
 	_NEED_DUMMY_LIBPTHREAD_A=
 	_LIBPTHREAD_A=$TERMUX_PREFIX/lib/libpthread.a
 	if [ ! -e $_LIBPTHREAD_A ]; then
