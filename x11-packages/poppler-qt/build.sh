211f76fcd3dbec3f65c0877978e527e7e3a14693
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-29 08:21:37 +0900

    new package: poppler-qt

---
+++ x11-packages/poppler-qt/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
+TERMUX_PKG_DESCRIPTION="PDF rendering library"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=22.04.0
+TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=813fb4b90e7bda63df53205c548602bae728887a60f4048aae4dbd9b1927deff
+TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, openjpeg-tools, qt5-qtbase, zlib"
+TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
+TERMUX_PKG_BREAKS="poppler, poppler-dev"
+TERMUX_PKG_REPLACES="poppler, poppler-dev"
+TERMUX_PKG_CONFLICTS="poppler"
+TERMUX_PKG_PROVIDES="poppler"
+#texlive needs the xpdf headers
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DENABLE_GLIB=ON
+-DENABLE_UNSTABLE_API_ABI_HEADERS=ON
+-DENABLE_QT5=ON
+-DENABLE_QT6=OFF
+-DFONT_CONFIGURATION=fontconfig
+"
+
+termux_step_pre_configure() {
+	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
+}
