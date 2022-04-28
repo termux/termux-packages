ad17b20175b4d8e8e8d7d333618674aa077fddbc
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-29 03:30:05 +0900

    glfw: Fix unresolved symbols issue

--- x11-packages/glfw/build.sh
+++ x11-packages/glfw/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An Open Source, multi-platform library for OpenGL, OpenG
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.3.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/glfw/glfw/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fd21a5f65bcc0fc3c76e0f8865776e852de09ef6fbc3620e09ce96d2b2807e04
 TERMUX_PKG_DEPENDS="libx11"
@@ -13,3 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DGLFW_BUILD_TESTS=OFF
 -DGLFW_BUILD_DOCS=OFF
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lm"
+}
