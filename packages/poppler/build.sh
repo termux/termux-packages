df81f4b0a8581f9d18f4df8e0220ef1892fbdb4b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-28 13:08:53 +0900

    poppler: Update to 22.04.0

--- packages/poppler/build.sh
+++ packages/poppler/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="PDF rendering library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.08.0
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=22.04.0
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e9cf5dc5964bce4bb0264d1c4f8122706c910588b421cfc30abc97d6b23e602d
+TERMUX_PKG_SHA256=813fb4b90e7bda63df53205c548602bae728887a60f4048aae4dbd9b1927deff
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, openjpeg-tools, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_BREAKS="poppler-dev"
@@ -17,3 +16,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_QT5=OFF
 -DFONT_CONFIGURATION=fontconfig
 "
+
+termux_step_pre_configure() {
+	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
+}
