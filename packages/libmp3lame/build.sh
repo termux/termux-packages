df50f30e2f2cca6eeaf517b108fe9ed089ba43f9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-04-29 11:37:23 +0900

    libmp3lame: Install `.pc` file

--- packages/libmp3lame/build.sh
+++ packages/libmp3lame/build.sh
@@ -3,8 +3,25 @@ TERMUX_PKG_DESCRIPTION="High quality MPEG Audio Layer III (MP3) encoder"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.100
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/lame/lame/${TERMUX_PKG_VERSION}/lame-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e
 TERMUX_PKG_BREAKS="libmp3lame-dev"
 TERMUX_PKG_REPLACES="libmp3lame-dev"
+
+termux_step_post_make_install() {
+	mkdir -p $TERMUX_PKG_CONFIG_LIBDIR
+	cat <<-EOF > $TERMUX_PKG_CONFIG_LIBDIR/lame.pc
+		prefix=$TERMUX_PREFIX
+		exec_prefix=\${prefix}
+		libdir=\${exec_prefix}/lib
+		includedir=\${prefix}/include
+
+		Name: lame
+		Description: MP3 encoding library
+		Requires:
+		Version: $TERMUX_PKG_VERSION
+		Libs: -L\${libdir} -lmp3lame
+		Cflags: -I\${includedir}
+	EOF
+}
