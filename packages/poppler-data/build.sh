f7ae939c5dac23543337bb76f67ab21c971c2274
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-01 07:51:36 +0900

    new package: poppler-data

---
+++ packages/poppler-data/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://poppler.freedesktop.org/
+TERMUX_PKG_DESCRIPTION="Consists of encoding files for use with poppler"
+# Makefile is licensed under HPND.
+TERMUX_PKG_LICENSE="BSD 3-Clause, GPL-2.0"
+TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.adobe, COPYING.gpl2"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4.11
+TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-data-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2cec05cd1bb03af98a8b06a1e22f6e6e1a65b1e2f3816cb3069bb0874825f08c
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
