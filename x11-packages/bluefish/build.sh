4313d90046db0c7d92f5af72f5b0a5f8b671c3c8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-01 14:55:23 +0900

    new package: bluefish

---
+++ x11-packages/bluefish/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://bluefish.openoffice.nl/
+TERMUX_PKG_DESCRIPTION="A powerful editor targeted towards programmers and webdevelopers"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.2.12
+TERMUX_PKG_SRCURL=https://www.bennewitz.com/bluefish/stable/source/bluefish-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=0df9905ba2c601cd767fa53e147969e9c90bc0cc4c005c4011c150b248979e6a
+TERMUX_PKG_DEPENDS="atk, enchant, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxml2, pango, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-xml-catalog-update
+--disable-update-databases
+--disable-python
+"
