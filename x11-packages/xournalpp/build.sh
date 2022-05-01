9a05280d40266e1aab6a02964a0e45dcea5bea05
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-01 13:53:21 +0900

    new package: xournalpp

---
+++ x11-packages/xournalpp/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/xournalpp/xournalpp
+TERMUX_PKG_DESCRIPTION="A hand note taking software"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE_FILE="LICENSE, copyright.txt"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SRCURL=https://github.com/xournalpp/xournalpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0d7fea5392758d5180eb24cc219660e997f087ae5a62d5f30515a063f81252e2
+TERMUX_PKG_DEPENDS="glib, gtk3, libc++, librsvg, libsndfile, libxml2, libzip, poppler, portaudio, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DHELP2MAN=NO
+"
