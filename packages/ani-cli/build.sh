c998cb47586c053651299ee6e96d6c67e7ddc4fc
tjhexf <jhonatan@justin.com.br>
2022-05-01 18:45:53 -0300

    ani-cli: update to 2.1

--- packages/ani-cli/build.sh
+++ packages/ani-cli/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pystardust/ani-cli
 TERMUX_PKG_DESCRIPTION="A cli to browse and watch anime"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.9
+TERMUX_PKG_VERSION=2.1
 TERMUX_PKG_SRCURL=https://github.com/pystardust/ani-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d01e5acc1961dfb2d1ea4219dcbc8a7b745a1066166216590592d2021aae7736
-TERMUX_PKG_DEPENDS="aria2, curl, git, grep, mpv, openssl, sed"
+TERMUX_PKG_SHA256=5e4ba037cd2a74e483cf2d9ddfe69335a81ee4d178270b8c6ee946024026ca00
+TERMUX_PKG_DEPENDS="aria2, curl, git, grep, mpv, openssl, sed, ffmpeg"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
