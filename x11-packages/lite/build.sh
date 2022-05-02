71884e055c899c814ef087064868c02d5f3b2fe7
Anshiq <66484628+anshiq@users.noreply.github.com>
2022-05-02 20:32:10 +0530

    adds lite editor in x11-packages
    Lite is a lightweight and beautiful code editor.

---
+++ x11-packages/lite/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/rxi/lite
+TERMUX_PKG_DESCRIPTION="Lightweight and Beautiful IDE "
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.11
+TERMUX_PKG_SRCURL=https://github.com/rxi/lite.git
+TERMUX_PKG_DEPENDS="libsdl2-dev or MinGW   "
+
