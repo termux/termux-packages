71904e38b9149eca47332745a469f945c3de6f55
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-03 12:47:26 +0900

    new package: tuxpaint

---
+++ x11-packages/tuxpaint/build.sh
@@ -0,0 +1,36 @@
+TERMUX_PKG_HOMEPAGE=https://tuxpaint.org/
+TERMUX_PKG_DESCRIPTION="A free, award-winning drawing program for children ages 3 to 12"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.9.27
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/tuxpaint/tuxpaint-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ab2b80e89df88228cd0ec0a6c90b49d543cb157aa38fb90cbd3a29f00140557a
+TERMUX_PKG_DEPENDS="fribidi, glib, libandroid-wordexp, libcairo, libimagequant, libpaper, libpng, librsvg, sdl, sdl-gfx, sdl-image, sdl-mixer, sdl-pango, sdl-ttf, tuxpaint-data, zlib"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	local _PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/_prefix
+
+	# Need imagemagick that can handle SVG format.
+	local IMAGEMAGICK_BUILD_SH=$TERMUX_SCRIPTDIR/packages/imagemagick/build.sh
+	local IMAGEMAGICK_SRCURL=$(bash -c ". $IMAGEMAGICK_BUILD_SH; echo \$TERMUX_PKG_SRCURL")
+	local IMAGEMAGICK_SHA256=$(bash -c ". $IMAGEMAGICK_BUILD_SH; echo \$TERMUX_PKG_SHA256")
+	local IMAGEMAGICK_TARFILE=$TERMUX_PKG_CACHEDIR/$(basename $IMAGEMAGICK_SRCURL)
+	termux_download $IMAGEMAGICK_SRCURL $IMAGEMAGICK_TARFILE $IMAGEMAGICK_SHA256
+	mkdir -p imagemagick
+	cd imagemagick
+	tar xf $IMAGEMAGICK_TARFILE --strip-components=1
+	./configure --prefix=$_PREFIX_FOR_BUILD \
+		--with-jpeg \
+		--with-png \
+		--with-rsvg
+	make -j $TERMUX_MAKE_PROCESSES
+	make install
+}
+
+termux_step_pre_configure() {
+	export PATH=$TERMUX_PKG_HOSTBUILD_DIR/_prefix/bin:$PATH
+
+	LDFLAGS+=" -landroid-wordexp"
+}
