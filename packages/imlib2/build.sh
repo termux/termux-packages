5a46787ff25714a19619209c182d1631c9f35827
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-05 07:35:02 +0900

    libid3tag: Revbump revdeps to rebuild
    after SONAME change.

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING-PLAIN"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.9.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5ac9e8ca7c6700919fe72749ad7243c42de4b22823c81769a1bf8e480e14c650
 TERMUX_PKG_DEPENDS="freetype, giflib, libandroid-shmem, libbz2, libid3tag, libjpeg-turbo, libpng, libtiff, libxext, zlib"
