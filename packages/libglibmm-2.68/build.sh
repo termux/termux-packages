b67fe7c3d5aef4aa50c0bfde1b737ad605afdd79
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-05 21:28:07 +0900

    new package: gtkmm4

---
+++ packages/libglibmm-2.68/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://www.gtkmm.org/
+TERMUX_PKG_DESCRIPTION="A C++ API for parts of glib that are useful for C++"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+_MAJOR_VERSION=2.72
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/glibmm/${_MAJOR_VERSION}/glibmm-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=2a7649a28ab5dc53ac4dabb76c9f61599fbc628923ab6a7dd74bf675d9155cd8
+TERMUX_PKG_DEPENDS="glib, libc++, libsigc++-3.0"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dbuild-examples=false
+"
+
+termux_step_post_massage() {
+	local _GUARD_FILE="lib/${TERMUX_PKG_NAME}.so"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "Error: file ${_GUARD_FILE} not found."
+	fi
+}
---
+++ x11-packages/gtkmm4/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://www.gtkmm.org/
+TERMUX_PKG_DESCRIPTION="The C++ API for GTK"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+# Newer version requires newer gtk4 which has build issue.
+_MAJOR_VERSION=4.2
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gtkmm/${_MAJOR_VERSION}/gtkmm-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=480c4c38f2e7ffcf58f56bb4b4d612f3f0cac9fd5908fd2cd8249fe10592a98b
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, graphene, gtk4, libc++, libcairo, libcairomm-1.16, libglibmm-2.68, libpangomm-2.48, libsigc++-3.0"
+TERMUX_PKG_BUILD_DEPENDS="libepoxy"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dbuild-demos=false
+-Dbuild-tests=false
+"
+
+termux_step_post_massage() {
+	local _GUARD_FILE="lib/libgtkmm-4.0.so"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "Error: file ${_GUARD_FILE} not found."
+	fi
+}
