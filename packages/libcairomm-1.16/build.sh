0efa339ac392f2b95fafd8da2049d228dcb1858f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-05 21:28:07 +0900

    new package: gtkmm4

---
+++ packages/libcairomm-1.16/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://www.cairographics.org/cairomm/
+TERMUX_PKG_DESCRIPTION="Provides a C++ interface to cairo"
+TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.16.1
+TERMUX_PKG_SRCURL=https://www.cairographics.org/releases/cairomm-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=6f6060d8e98dd4b8acfee2295fddbdd38cf487c07c26aad8d1a83bb9bff4a2c6
+TERMUX_PKG_DEPENDS="libc++, libcairo, libsigc++-3.0"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dbuild-examples=false
+-Dbuild-tests=false
+-Dboost-shared=true
+"
+
+termux_step_post_massage() {
+	local _GUARD_FILE="lib/${TERMUX_PKG_NAME}.so"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "Error: file ${_GUARD_FILE} not found."
+	fi
+}
---
+++ packages/libpangomm-2.48/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://www.gtkmm.org/
+TERMUX_PKG_DESCRIPTION="A C++ API for Pango"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+_MAJOR_VERSION=2.50
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/pangomm/${_MAJOR_VERSION}/pangomm-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=a27aa77e017b9afce9e751d85bd1cf890abbb3a58bf59d0fac917eef82db3b5b
+TERMUX_PKG_DEPENDS="glib, libc++, libcairomm-1.16, libglibmm-2.68, libsigc++-3.0, pango"
+
+termux_step_post_massage() {
+	local _GUARD_FILE="lib/${TERMUX_PKG_NAME}.so"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "Error: file ${_GUARD_FILE} not found."
+	fi
+}
