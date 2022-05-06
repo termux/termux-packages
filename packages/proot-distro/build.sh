af35ce06e9092283828365244b17ffa95f35e5a0
marcusz <bautista.wyatt.marcus_30@outlook.com>
2022-05-06 16:20:45 +0800

    proot-distro: update to v3.0.2
    Minor documentation updates

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.1
+TERMUX_PKG_VERSION=3.0.2
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a206909f79f443526530cf7f83a613e27280d105bddb89b3db00aad3c53defe2
+TERMUX_PKG_SHA256=4ef92a462ff35df2e4a26bec3c88ae8cce4b76838c0cb41652d2c97737cc18e4
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
