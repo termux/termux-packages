34e0d7b4b18bab6b52027b803384519c65e97c5a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-06 20:39:08 +0900

    povray: Enable OpenEXR support
    * Compile with `-Ofast`
    
    * Exclude HTML documents
    
    * Separate platform-independent data as a subpackage

---
+++ packages/povray/povray-data.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_INCLUDE="share/povray-*/"
+TERMUX_SUBPKG_DESCRIPTION="Platform-independent data for povray"
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
+TERMUX_SUBPKG_BREAKS="povray (<< 3.8.0-beta.2-2)"
+TERMUX_SUBPKG_REPLACES="povray (<< 3.8.0-beta.2-2)"
