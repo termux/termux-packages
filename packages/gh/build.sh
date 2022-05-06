3d6fa7908c3e508405e4c4fd3f01dd818237277e
ifurther <55025025+ifurther@users.noreply.github.com>
2022-05-06 20:11:48 +0800

    gh: add shell completions

--- packages/gh/build.sh
+++ packages/gh/build.sh
@@ -29,3 +29,22 @@ termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/cli/cli/cmd/gh/gh
 	install -Dm600 -t "$TERMUX_PREFIX"/share/doc/gh/ "$TERMUX_PKG_SRCDIR"/docs/*
 }
+
+termux_step_create_debscripts() {
+	cat <<-EOF >./postinst
+		#!${TERMUX_PREFIX}/bin/sh
+		mkdir -p ${TERMUX_PREFIX}/share/bash-completion/completions
+		mkdir -p ${TERMUX_PREFIX}/share/zsh/site-functions
+		mkdir -p ${TERMUX_PREFIX}/share/fish/vendor_completions.d
+		gh completion -s bash > ${TERMUX_PREFIX}/share/bash-completion/completions/gh.bash
+		gh completion -s zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_gh
+		gh completion -s fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/gh.fish
+	EOF
+
+	cat <<-EOF >./prerm
+		#!${TERMUX_PREFIX}/bin/sh
+		rm -f ${TERMUX_PREFIX}/share/bash-completion/completions/gh.bash
+		rm -f ${TERMUX_PREFIX}/share/zsh/site-functions/_gh
+		rm -f ${TERMUX_PREFIX}/share/fish/vendor_completions.d/gh.fish
+	EOF
+}
