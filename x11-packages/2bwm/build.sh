2a192b30fc582dee03b271b26404db121f5cf698
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-07 10:03:04 +0900

    new package: 2bwm

---
+++ x11-packages/2bwm/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/venam/2bwm
+TERMUX_PKG_DESCRIPTION="A fast floating WM"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_LICENSE_FILE="LICENSE.2bwm, LICENSE.hidden"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.3
+TERMUX_PKG_SRCURL=https://github.com/venam/2bwm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=a4889ea4b01b1a3d4a508daa034b9d86676913cbbca1f977858df692a6e2af95
+TERMUX_PKG_DEPENDS="libxcb, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+PREFIX=$TERMUX_PREFIX
+X11_INCLUDE=$TERMUX_PREFIX/include
+"
+
+termux_step_post_get_source() {
+	cp $TERMUX_PKG_BUILDER_DIR/LICENSE.{2bwm,hidden} ./
+}
