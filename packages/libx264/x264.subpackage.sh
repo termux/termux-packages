bd8ef7f23fbef559104249c8e1a3a4f97262c32b
ifurther <55025025+ifurther@users.noreply.github.com>
2022-05-07 18:49:37 +0800

    libx264: add completion

--- packages/libx264/x264.subpackage.sh
+++ packages/libx264/x264.subpackage.sh
@@ -1,2 +1,4 @@
-TERMUX_SUBPKG_INCLUDE=bin
+TERMUX_SUBPKG_INCLUDE="
+bin
+share/bash-completion/completions/x264"
 TERMUX_SUBPKG_DESCRIPTION="Commandline video encoder for the H.264/MPEG-4 AVC format"
