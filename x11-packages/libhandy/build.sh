53a76b1b1c9c6c0ca30a6bfab86376bddee007e1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-07 10:27:03 +0900

    new package: libhandy

---
+++ x11-packages/libhandy/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/libhandy/
+TERMUX_PKG_DESCRIPTION="Building blocks for modern adaptive GNOME apps"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+_MAJOR_VERSION=1.6
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libhandy/${_MAJOR_VERSION}/libhandy-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=7fa89aaa87966b6d0f5f4ef4d3efdaf654e2b01ea2c7ce2bd70301d1f9f42ca3
+TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, glib, gtk3, libcairo, pango"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=disabled
+-Dvapi=false
+-Dtests=false
+-Dexamples=false
+-Dglade_catalog=disabled
+"
