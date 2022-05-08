b30a0a8ef1094ca8064943e5137c870e8c584b49
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-08 12:33:25 +0900

    new package: rubberband

---
+++ packages/rubberband/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://breakfastquay.com/rubberband/
+TERMUX_PKG_DESCRIPTION="An audio time-stretching and pitch-shifting library and utility program"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.0.2
+TERMUX_PKG_SRCURL=https://breakfastquay.com/files/releases/rubberband-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=b9eac027e797789ae99611c9eaeaf1c3a44cc804f9c8a0441a0d1d26f3d6bdf9
+TERMUX_PKG_DEPENDS="fftw, ladspa-sdk, libsamplerate, libsndfile, lv2, vamp-plugin-sdk"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dfft=fftw
+-Dresampler=libsamplerate
+"
