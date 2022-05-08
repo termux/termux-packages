83568c398e1bcd9fc0c324d704e5a3e720c97910
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-08 10:05:59 +0900

    new package: liblo

---
+++ packages/liblo/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://liblo.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="A lightweight library that provides an easy to use implementation of the OSC protocol"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.31
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/liblo/liblo-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2b4f446e1220dcd624ecd8405248b08b7601e9a0d87a0b94730c2907dbccc750
