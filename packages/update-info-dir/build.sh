63917944c1963c7a1f023eda5436141145045078
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-08 15:39:43 +0900

    new package: update-info-dir

---
+++ packages/update-info-dir/build.sh
@@ -0,0 +1,36 @@
+TERMUX_PKG_HOMEPAGE=https://packages.debian.org/sid/texinfo
+TERMUX_PKG_DESCRIPTION="Update or create index file from all installed info files in directory"
+TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=6.8-4
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/t/texinfo/texinfo_${TERMUX_PKG_VERSION}.debian.tar.xz
+TERMUX_PKG_SHA256=1194b3f1a9480f1796fdf92fb366b02d855a17f8873ec5daa4b6befe51d69f57
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin update-info-dir
+	install -Dm600 -t $TERMUX_PREFIX/share/man/man8 update-info-dir.8
+}
+
+termux_step_create_debscripts() {
+	local INFODIR=$TERMUX_PREFIX/share/info
+
+	cat <<- EOF > ./triggers
+	interest-noawait $INFODIR
+	EOF
+
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	if [ -d $INFODIR ]; then
+	    $TERMUX_PREFIX/bin/update-info-dir
+	fi
+	exit 0
+	EOF
+
+	cat <<- EOF > ./prerm
+	#!$TERMUX_PREFIX/bin/sh
+	rm -rf $INFODIR/dir
+	exit 0
+	EOF
+}
