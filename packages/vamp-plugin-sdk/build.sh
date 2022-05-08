ee74b67334ee3e0981b3a29adb65a203b1efc707
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-08 10:30:14 +0900

    new package: vamp-plugin-sdk

---
+++ packages/vamp-plugin-sdk/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://www.vamp-plugins.org/
+TERMUX_PKG_DESCRIPTION="An API for audio analysis and feature extraction plugins"
+TERMUX_PKG_LICENSE="MIT, BSD 3-Clause"
+TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.10
+TERMUX_PKG_SRCURL=https://github.com/c4dm/vamp-plugin-sdk/archive/refs/tags/vamp-plugin-sdk-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b552bc91817294c7f90ea07d70938642ebf15d5e3bafc81cf7d55efab9995399
+TERMUX_PKG_DEPENDS="libc++, libsndfile"
+TERMUX_PKG_BUILD_IN_SRC=true
