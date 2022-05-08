34917c823debba7b109f89cf6cd206f0fd612d88
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-08 17:45:40 +0900

    taskwarrior: Install shell compdefs

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Utility for managing your TODO list"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.6.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${TERMUX_PKG_VERSION}/task-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b1d3a7f000cd0fd60640670064e0e001613c9e1cb2242b9b3a9066c78862cfec
 TERMUX_PKG_AUTO_UPDATE=true
@@ -13,3 +14,9 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
 }
 
+termux_step_post_make_install() {
+	install -Dm600 -T "$TERMUX_PKG_SRCDIR"/scripts/bash/task.sh \
+		$TERMUX_PREFIX/share/bash-completion/completions/task
+	install -Dm600 -t $TERMUX_PREFIX/share/fish/vendor_completions.d \
+		"$TERMUX_PKG_SRCDIR"/scripts/fish/task.fish
+}
