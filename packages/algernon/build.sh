9331dbb64f575efe6be1c6680fa300a3e8f769d0
Aditya Alok <dev.aditya.alok@gmail.com>
2022-05-10 09:09:57 +0530

    chore(algernon): update to 1.13.0
    - update URL in account for updated tag name
    - remove upsteamed patch `lib-pq-user_posix.go.patch`
    
    closes #10579
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/algernon/build.sh
+++ packages/algernon/build.sh
@@ -2,31 +2,31 @@ TERMUX_PKG_HOMEPAGE=https://algernon.roboticoverlords.org/
 TERMUX_PKG_DESCRIPTION="Small self-contained web server with Lua, Markdown, QUIC, Redis and PostgreSQL support"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.12.14
-TERMUX_PKG_SRCURL=https://github.com/xyproto/algernon/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=cab5b01923142e0326ea2a01797814bb2e8ea9f7c6c41a3ea0ae7df3b667e86e
+TERMUX_PKG_VERSION=1.13.0
+TERMUX_PKG_SRCURL="https://github.com/xyproto/algernon/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=8327c1687990386eb5a48d15fd46fc69e17d400c29d726b34f6087c9c4887b9c
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
 
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	mkdir -p "$GOPATH"/src/github.com/xyproto
-	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/xyproto/algernon
+	export GOPATH="${TERMUX_PKG_BUILDDIR}"
+	mkdir -p "${GOPATH}"/src/github.com/xyproto
+	ln -sf "${TERMUX_PKG_SRCDIR}" "${GOPATH}"/src/github.com/xyproto/algernon
 
-	cd "$GOPATH"/src/github.com/xyproto/algernon
+	cd "${GOPATH}"/src/github.com/xyproto/algernon || exit 1
 
 	go build
 }
 
 termux_step_make_install() {
 	install -Dm700 \
-		"$GOPATH"/src/github.com/xyproto/algernon/algernon \
-		"$TERMUX_PREFIX"/bin/
+		"${GOPATH}"/src/github.com/xyproto/algernon/algernon \
+		"${TERMUX_PREFIX}"/bin/
 
 	# Offline samples may be useful to get started with Algernon.
-	rm -rf "$TERMUX_PREFIX"/share/doc/algernon
-	mkdir -p "$TERMUX_PREFIX"/share/doc/algernon
-	cp -a "$GOPATH"/src/github.com/xyproto/algernon/samples \
-		"$TERMUX_PREFIX"/share/doc/algernon/
+	rm -rf "${TERMUX_PREFIX}"/share/doc/algernon
+	mkdir -p "${TERMUX_PREFIX}"/share/doc/algernon
+	cp -a "${GOPATH}"/src/github.com/xyproto/algernon/samples \
+		"${TERMUX_PREFIX}"/share/doc/algernon/
 }
