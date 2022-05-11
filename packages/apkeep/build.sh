96b64a9cd5257db10fa5b695b8d835f99daf0c77
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-12 06:55:18 +0900

    apkeep: Update to 0.12.1
    Co-authored-by: William Budington <bill@eff.org>

--- packages/apkeep/build.sh
+++ packages/apkeep/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/EFForg/apkeep
 TERMUX_PKG_DESCRIPTION="A command-line tool for downloading APK files from various sources"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.12.0
+TERMUX_PKG_VERSION=0.12.1
 TERMUX_PKG_SRCURL=https://github.com/EFForg/apkeep/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a1ab0121e53cc63cad042c378ea3c28779fd201287917186b429aec69522f224
+TERMUX_PKG_SHA256=366d65464c5cfb9a27114dcb91e7bae2d91ced09bd8412eb5e608a1e14e8f2ba
 TERMUX_PKG_DEPENDS="openssl (>= 3.0.3)"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
