7f0a27c1c036ff5faf1f72017c8c3c6bab1311be
port19 <port19@port19.xyz>
2022-05-12 10:14:44 +0200

    ani-cli: use external mpv

--- packages/ani-cli/build.sh
+++ packages/ani-cli/build.sh
@@ -5,10 +5,12 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2
 TERMUX_PKG_SRCURL=https://github.com/pystardust/ani-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=973d335a75bd7f920c244000ad6b057f702fb37752e7bea1b5bcf038785ec925
-TERMUX_PKG_DEPENDS="aria2, curl, grep, mpv, openssl-tool, sed, gawk, ffmpeg"
+TERMUX_PKG_DEPENDS="aria2, curl, grep, openssl-tool, sed, gawk, ffmpeg"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin ani-cli
+	echo 'am start --user 0 -a android.intent.action.VIEW -d "$1" -n is.xyz.mpv/.MPVActivity' > $PREFIX/bin/mpv
+	echo 'Note that you need to have the mpv android app installed'
 }
