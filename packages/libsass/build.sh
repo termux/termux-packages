0a18db739ab801bd73d4a138bce9f722933ba2b2
Jesús Chapman (Yisus7u7) <dev.yisus@hotmail.com>
2022-05-13 19:34:36 -0500

    fix depends

--- packages/libsass/build.sh
+++ packages/libsass/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Jesus Chapman (@Yisus7u7) <dev.yisus@hotmail.com>"
 TERMUX_PKG_VERSION=3.6.5
 TERMUX_PKG_SRCURL=https://github.com/sass/libsass/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=89d8f2c46ae2b1b826b58ce7dde966a176bac41975b82e84ad46b01a55080582
-TERMUX_PKG_DEPENDS="libc"
+TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure(){
--- packages/sassc/build.sh
+++ packages/sassc/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Jesus Chapman (@Yisus7u7) <dev.yisus@hotmail.com>"
 TERMUX_PKG_VERSION=3.6.2
 TERMUX_PKG_SRCURL=https://github.com/sass/sassc/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=608dc9002b45a91d11ed59e352469ecc05e4f58fc1259fc9a9f5b8f0f8348a03
-TERMUX_PKG_DEPENDS="libsass, libc"
+TERMUX_PKG_DEPENDS="libsass, libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure(){
