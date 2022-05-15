9f8a960f7fe5b0931459b518c878e73ac646e2c0
Aditya Alok <dev.aditya.alok@gmail.com>
2022-05-15 19:47:54 +0530

    feat: add package bore
    closes #10675
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

---
+++ packages/bore/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ekzhang/bore
+TERMUX_PKG_DESCRIPTION="Bore is a simple CLI tool for making tunnels to localhost."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_SRCURL="https://github.com/ekzhang/bore/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=ab1f3e924ce8a32eafe842de0bb1d23eeeb397ec0ad16455b443206f0c9ee59d
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
