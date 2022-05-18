2039ae4f8165c4960df8c6de99931020d2fecf9d
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-05-19 02:42:11 +0800

    setup-termux.sh: Install package jq and binutils-is-llvm

--- scripts/setup-termux.sh
+++ scripts/setup-termux.sh
@@ -2,13 +2,15 @@
 
 PACKAGES=""
 # Tier 1: requirements for the core build scripts in scripts/build/.
-PACKAGES+=" clang"		# Required for termux-elf-cleaner and C/C++ packages.
-PACKAGES+=" file"		# Used in termux_step_massage().
-PACKAGES+=" gnupg"		# Used in termux_get_repo_files() and build-package.sh.
-PACKAGES+=" lzip"		# Used by tar to extract *.tar.lz source archives.
-PACKAGES+=" patch"		# Used for applying patches on source code.
-PACKAGES+=" python"		# Used buildorder.py core script.
-PACKAGES+=" unzip"		# Used to extract *.zip source archives.
+PACKAGES+=" clang"				# Required for termux-elf-cleaner and C/C++ packages.
+PACKAGES+=" file"				# Used in termux_step_massage().
+PACKAGES+=" gnupg"				# Used in termux_get_repo_files() and build-package.sh.
+PACKAGES+=" lzip"				# Used by tar to extract *.tar.lz source archives.
+PACKAGES+=" patch"				# Used for applying patches on source code.
+PACKAGES+=" python"				# Used buildorder.py core script.
+PACKAGES+=" unzip"				# Used to extract *.zip source archives.
+PACKAGES+=" jq"					# Used for parsing repo.json.
+PACKAGES+=" binutils-is-llvm"	# Used for checking symbols.
 
 # Tier 2: requirements for building many other packages.
 PACKAGES+=" asciidoc"
