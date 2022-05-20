e6cf9d114372aed0b68498bdccb94b45cbb53e7f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-05-20 09:48:57 +0900

    librsvg: Update to 2.54.3

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -3,17 +3,17 @@ TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=2.54
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/librsvg/${_MAJOR_VERSION}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d5557efbdcc415a4180e1116b7f736cb711b253d110d95fa86ec830f70026625
+TERMUX_PKG_SHA256=66158f2ef46dde260026846c4da102e4a9dd4e5293010f30949c6cc26dd6efe8
 TERMUX_PKG_DEPENDS="fontconfig, freetype, gdk-pixbuf, glib, harfbuzz, libcairo, libpng, libxml2, pango, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_GDK_PIXBUF_QUERYLOADERS=$TERMUX_PREFIX/bin/gdk-pixbuf-query-loaders
+--disable-gtk-doc
 --disable-introspection
 --disable-static
-ac_cv_prog_GI_DOCGEN=:
 "
 
 termux_step_pre_configure() {
