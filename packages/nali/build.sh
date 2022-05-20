fd1899b18e2a8ebd2492853d4d3a6ecc07941b9a
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-05-20 20:58:56 +0800

    new package: nali
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

---
+++ packages/nali/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/zu1k/nali
+TERMUX_PKG_DESCRIPTION="An offline tool for querying IP geographic information and CDN provider."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_SRCURL=https://github.com/zu1k/nali/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=55838bb8884bbd0dc5ecc00e89edd170c74eb62e3b2ac426bb78df6e89062d6f
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin nali
+}
