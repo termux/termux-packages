3380aa0354ee9bf584fd709c5fd770908319a4bd
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-05-21 17:57:06 +0800

    rebuild(lv2): add libxml2,libxslt,sord to depends
    fix #10766 #10767
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/lv2/build.sh
+++ packages/lv2/build.sh
@@ -3,18 +3,17 @@ TERMUX_PKG_DESCRIPTION="A plugin standard for audio systems"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.18.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://lv2plug.in/spec/lv2-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=4e891fbc744c05855beb5dfa82e822b14917dd66e98f82b8230dbd1c7ab2e05e
-TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_DEPENDS="libxml2, libxslt, python, sord"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---no-plugins
-"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-plugins"
 
-termux_step_configure() {
+termux_step_pre_configure() {
 	./waf configure \
-		--prefix=$TERMUX_PREFIX \
-		LINKFLAGS="$LDFLAGS" \
+		--prefix="${TERMUX_PREFIX}" \
+		LINKFLAGS="${LDFLAGS}" \
 		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
 
@@ -27,9 +26,9 @@ termux_step_make_install() {
 }
 
 termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing dependencies through pip..."
-	pip3 install lxml pygments rdflib
+	cat <<-EOF >./postinst
+		#!$TERMUX_PREFIX/bin/sh
+		echo "Installing dependencies through pip..."
+		pip3 install lxml pygments rdflib
 	EOF
 }
