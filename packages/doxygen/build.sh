f8115b77930541d8456f4ef3f9af460c2d13fa3f
Aditya Alok <dev.aditya.alok@gmail.com>
2022-05-22 20:51:50 +0530

    fix(main/doxygen): use `newest-tag` for auto-update
    %ci:no-build
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/doxygen/build.sh
+++ packages/doxygen/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION="1.9.4"
 TERMUX_PKG_SRCURL=https://github.com/doxygen/doxygen/archive/Release_${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=1b083d15b29817463129ae1ae73b930d883030eeec090ea7a99b3a04fdb51c76
 TERMUX_PKG_DEPENDS="libc++, libiconv"
-TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBISON_EXECUTABLE=$(command -v bison)
 -DCMAKE_BUILD_TYPE=Release
@@ -15,8 +14,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dbuild_parse=yes
 -Dbuild_xmlparser=yes
 "
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 termux_step_post_make_install() {
-	mkdir -p $TERMUX_PREFIX/share/man/man1
-	cp $TERMUX_PKG_SRCDIR/doc/doxygen.1 $TERMUX_PREFIX/share/man/man1
+	mkdir -p "$TERMUX_PREFIX"/share/man/man1
+	cp "$TERMUX_PKG_SRCDIR"/doc/doxygen.1 "$TERMUX_PREFIX"/share/man/man1
 }
