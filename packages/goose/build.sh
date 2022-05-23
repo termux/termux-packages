ebd20fff680b20322d3365746cb88595ee56440d
Aditya Alok <dev.aditya.alok@gmail.com>
2022-05-23 11:03:44 +0530

    add(repo:main): new package goose
    closes #10751
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

---
+++ packages/goose/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://pressly.github.io/goose
+TERMUX_PKG_DESCRIPTION="A database migration tool. Supports SQL migrations and Go functions."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.5.3
+TERMUX_PKG_SRCURL="https://github.com/pressly/goose/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=9a5f9dc2b3b5f0876ad1e4609e1d1812e2b9ff03ea04e00c69ca05e7d9584601
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+	go mod tidy
+}
+
+termux_step_make() {
+	go build -o goose ./cmd/goose
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin goose
+}
+
+termux_step_install_license() {
+	install -Dm600 -t "${TERMUX_PREFIX}/share/doc/${TERMUX_PKG_NAME}" \
+		"${TERMUX_PKG_SRCDIR}/LICENSE"
+}
