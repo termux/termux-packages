0bec8e984c10e12816031b46003613a385bd39aa
Yaksh Bariya <yakshbari4@gmail.com>
2022-05-24 18:16:52 +0530

    upgpkg(main/choose): update to v1.3.4

--- packages/choose/build.sh
+++ packages/choose/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/theryangeary/choose
 TERMUX_PKG_DESCRIPTION="A human-friendly and fast alternative to cut and (sometimes) awk"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_VERSION=1.3.4
 TERMUX_PKG_SRCURL=https://github.com/theryangeary/choose.git
 TERMUX_PKG_BUILD_IN_SRC=true
 
