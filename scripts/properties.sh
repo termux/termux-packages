afad8febe60d59f72c8d3eb0f8f7cd4418264936
agnostic-apollo <agnosticapollo@gmail.com>
2022-05-29 08:44:38 +0500

    added(scripts): Add TERMUX_APPS_DIR as per termux/termux-app@bcd8f4c4

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -26,6 +26,7 @@ TERMUX_APP_PACKAGE="com.termux"
 TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
 TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
 TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
+TERMUX_APPS_DIR="${TERMUX_BASE_DIR}/apps"
 TERMUX_PREFIX="${TERMUX_BASE_DIR}/usr"
 
 # Allow to override setup.
