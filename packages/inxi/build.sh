d2f59b52383dc91353acb023131da702f423f78a
Yaksh Bariya <yakshbari4@gmail.com>
2022-05-29 20:04:32 +0530

    upgpkg(main/inxi): update to 3.3.16-1

--- packages/inxi/build.sh
+++ packages/inxi/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/smxi/inxi
 TERMUX_PKG_DESCRIPTION="Full featured CLI system information tool"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.3.08-1
-TERMUX_PKG_SRCURL=https://github.com/smxi/inxi/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=44008d9e77dc82855fd91d634f5f817813eb4653e4df7106e56a1c9986ab8abd
+TERMUX_PKG_VERSION=3.3.16-1
+TERMUX_PKG_SRCURL=https://github.com/smxi/inxi/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=aa8e1e181e34505b4c9404122306624d644dbc50acce000257a9e94b203066cd
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin/ inxi
