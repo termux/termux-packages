b9e7e0fd6ee857314318b218d6614035441641ad
Henrik Grimler <grimler@termux.org>
2022-06-02 11:07:16 +0200

    add(main/sse2neon): add package with (only) sse2neon.h
    Needed by other packages like dosbox-x:
    https://github.com/termux/termux-packages/pull/10875

---
+++ packages/sse2neon/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/DLTcollab/sse2neon
+TERMUX_PKG_DESCRIPTION="A C/C++ header file that converts Intel SSE intrinsics to Arm/Aarch64 NEON intrinsics"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.5.1
+TERMUX_PKG_SRCURL=https://github.com/DLTcollab/sse2neon/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4001e2dfb14fcf3831211581ed83bcc83cf6a3a69f638dcbaa899044a351bb2a
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
