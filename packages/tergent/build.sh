dc552e9b492e7cd278e28620ac4782c22d32ddd2
Henrik Grimler <grimler@termux.dev>
2022-06-03 17:11:46 +0200

    enhance(main/tergent): add support for arm and i686
    Tested on aarch64 as well as arm.
    
    Upstream PR: https://github.com/aeolwyr/tergent/pull/18

--- packages/tergent/build.sh
+++ packages/tergent/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="A cryptoki/PKCS#11 library for Termux that uses Android 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/aeolwyr/tergent/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0b59cf0ced3f693fb19396a986326963f3763e6bf65d3e56af0a03d206d69428
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_REVISION=4
+# Build from specific revision until patches are merged upstream, or
+# we decide to maintain a fork
+_COMMIT=831e300e3d75a9618963bbefbaad49bf37e2cf3c
+TERMUX_PKG_SRCURL=https://github.com/termux/tergent/archive/${_COMMIT}.tar.gz
+TERMUX_PKG_SHA256=8979504a0e705fca35a6ae81ba1665c5bafebe218008ee50b6dc4f8a8d611cec
 TERMUX_PKG_DEPENDS="termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
 	termux_setup_rust
