017b92e608ae393985da87918a6ecacc26ba746c
Arun <arun007@pm.me>
2022-06-04 14:40:12 +0530

    chore(main/subversion): bump revision after updating perl

--- packages/subversion/build.sh
+++ packages/subversion/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Centralized version control system characterized by its 
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.14.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.apache.org/dist/subversion/subversion-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=c9130e8d0b75728a66f0e7038fc77052e671830d785b5616aad53b4810d3cc28
 TERMUX_PKG_DEPENDS="apr, apr-util, serf, libexpat, libsqlite, liblz4, utf8proc, zlib"
