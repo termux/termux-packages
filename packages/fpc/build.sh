29ac536ee208e73c2968601285491da3701f94b0
Aditya Alok <dev.aditya.alok@gmail.com>
2022-06-05 09:29:21 +0530

    test:8

--- packages/fpc/build.sh
+++ packages/fpc/build.sh
@@ -7,15 +7,37 @@ TERMUX_PKG_VERSION=3.2.2
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/freepascal/files/Source/${TERMUX_PKG_VERSION}/fpcbuild-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=85ef993043bb83f999e2212f1bca766eb71f6f973d362e2290475dbaaf50161f
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="
-CPU_TARGET=${TERMUX_ARCH}
-OS_TARGET=android
-BINUTILSPREFIX=${TERMUX_HOST_PLATFORM}
-INSTALL_PREFIX=${TERMUX_PREFIX}"
+TERMUX_PKG_EXTRA_MAKE_ARGS="OS_TARGET=android NOGDB=1"
 
 termux_step_pre_configure() {
 	termux_setup_fpc
 
-	fpcmake -T "${TERMUX_ARCH}"-android
-	patch -p1 < "${TERMUX_PKG_BUILDER_DIR}"/Makefile.diff
+	local _ARCH="${TERMUX_ARCH}"
+	[[ "${TERMUX_ARCH}" == "i686" ]] && _ARCH=i386
+
+	TERMUX_PKG_EXTRA_MAKE_ARGS+=" CPU_TARGET=${_ARCH}"
+
+	# Setup wrapper bin.
+	local _WRAPPER="${TERMUX_PKG_TMPDIR}/wrapper"
+	mkdir -p "${_WRAPPER}"
+
+	cat <<- EOF > "${_WRAPPER}/${TERMUX_HOST_PLATFORM}-ld"
+		#!$(command -v sh)
+		exec ${LD}
+	EOF
+	cat <<- EOF > "${_WRAPPER}/${TERMUX_HOST_PLATFORM}-ar"
+		#!$(command -v sh)
+		exec ${AR}
+	EOF
+	chmod 0700 "${_WRAPPER}/${TERMUX_HOST_PLATFORM}-"{ar,ld}
+
+	export PATH="${_WRAPPER}:/usr/bin:${PATH}"
+
+	# Generate makefile for Android build.
+	fpcmake -T "${_ARCH}"-android
+
+	# fpc directly invokes linker, so we need to provode NDK libs dir.
+	local libdir && libdir="$(realpath "$("${CC}" --print-file-name libc.so)")"
+	sed "s|@LIBDIR@|${TERMUX_PREFIX} ${libdir/\/libc.so/}|g" \
+		"${TERMUX_PKG_BUILDER_DIR}"/Makefile.diff | patch -p1
 }
