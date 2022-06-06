8d415bd3e1456297c37204ee50b079c1c6690160
Yaksh Bariya <yakshbari4@gmail.com>
2022-06-06 17:40:26 +0530

    fix(main/nodejs-lts): npm do not use hard links at any cost

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environme
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
 TERMUX_PKG_VERSION=16.15.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a0f812efc43f78321eca08957960a48f5e6bf97004d5058c8dd3b03c646ea4f7
 # Note that we do not use a shared libuv to avoid an issue with the Android
