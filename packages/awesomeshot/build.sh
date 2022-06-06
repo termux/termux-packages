db5724494ac7bfe8e49ab36ec5f0867443f642f7
nihsx <xshin3373@gmail.com>
2022-06-06 21:29:16 +0700

    add update automatically

--- packages/awesomeshot/build.sh
+++ packages/awesomeshot/build.sh
@@ -7,5 +7,6 @@ TERMUX_PKG_SRCURL=https://github.com/mayTermux/awesomeshot/archive/refs/tags/v.$
 TERMUX_PKG_SHA256=abed90173c6679c2891939091ddfe56fcafa566ed6dae7ed8a77a4545ad6c332
 TERMUX_PKG_DEPENDS="bash, bc, imagemagick, inotify-tools, termux-api"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
