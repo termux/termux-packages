d8598adc22b4b8e91edea7a3cf6b3c37cdeca721
Efreak <efreak@users.noreply.github.com>
2022-06-06 18:56:20 -0700

    add jdupes metadata

---
+++ packages/jdupes/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/jbruchon/jdupes
+TERMUX_PKG_DESCRIPTION="A powerful duplicate file finder and an enhanced fork of 'fdupes'"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.20.2
+TERMUX_PKG_SRCURL=https://github.com/jbruchon/jdupes/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d079d22dc77e1d181abcb8a59216520633a8712d197d007a9a9fb64c72610824
+TERMUX_PKG_AUTO_UPDATE=true
