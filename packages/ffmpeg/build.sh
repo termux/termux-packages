f8c6ca90d4e0e6bd2bb4c099af3466278c430dcc
Aditya Alok <dev.aditya.alok@gmail.com>
2022-06-13 16:12:33 +0530

    chore(main/ffmpeg): simplify build.sh
    %ci:no-build
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -11,69 +11,66 @@ TERMUX_PKG_DEPENDS="game-music-emu, libaom, libass, libbluray, libbz2, libdav1d,
 TERMUX_PKG_CONFLICTS="libav"
 TERMUX_PKG_BREAKS="ffmpeg-dev"
 TERMUX_PKG_REPLACES="ffmpeg-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-indevs
+--disable-outdevs
+--enable-indev=lavfi
+--disable-static
+--disable-symver
+--enable-cross-compile
+--enable-gnutls
+--enable-gpl
+--enable-libaom
+--enable-libass
+--enable-libbluray
+--enable-libdav1d
+--enable-libgme
+--enable-libmp3lame
+--enable-libfreetype
+--enable-libvorbis
+--enable-libopus
+--enable-librav1e
+--enable-libsoxr
+--enable-libx264
+--enable-libx265
+--enable-libxvid
+--enable-libvidstab
+--enable-libvpx
+--enable-libwebp
+--enable-libxml2
+--enable-shared
+--target-os=android
+--extra-libs=-landroid-glob
+"
 
-
-termux_step_configure() {
-	cd $TERMUX_PKG_BUILDDIR
+termux_step_pre_configure() {
+	cd "${TERMUX_PKG_BUILDDIR}" || exit
 
 	export ASFLAGS="-no-integrated-as"
 
-	local _EXTRA_CONFIGURE_FLAGS=""
-	if [ $TERMUX_ARCH = "arm" ]; then
+	local _EXTRA_CONFIGURE_FLAGS="" _ARCH
+
+	if [[ "${TERMUX_ARCH}" == "arm" ]]; then
 		_ARCH="armeabi-v7a"
 		_EXTRA_CONFIGURE_FLAGS="--enable-neon"
-	elif [ $TERMUX_ARCH = "i686" ]; then
+	elif [[ "${TERMUX_ARCH}" == "i686" ]]; then
 		_ARCH="x86"
 		# Specify --disable-asm to prevent text relocations on i686,
 		# see https://trac.ffmpeg.org/ticket/4928
 		_EXTRA_CONFIGURE_FLAGS="--disable-asm"
-	elif [ $TERMUX_ARCH = "x86_64" ]; then
-		_ARCH="x86_64"
-	elif [ $TERMUX_ARCH = "aarch64" ]; then
-		_ARCH=$TERMUX_ARCH
+	elif [[ "${TERMUX_ARCH}" == "aarch64" ]]; then
 		_EXTRA_CONFIGURE_FLAGS="--enable-neon"
-	else
-		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
 	fi
 
-	$TERMUX_PKG_SRCDIR/configure \
-		--arch="${_ARCH}" \
-		--as="$AS" \
-		--cc="$CC" \
-		--cxx="$CXX" \
-		--nm="$NM" \
-		--pkg-config="$PKG_CONFIG" \
-		--strip="$STRIP" \
-		--cross-prefix="${TERMUX_HOST_PLATFORM}-" \
-		--disable-indevs \
-		--disable-outdevs \
-		--enable-indev=lavfi \
-		--disable-static \
-		--disable-symver \
-		--enable-cross-compile \
-		--enable-gnutls \
-		--enable-gpl \
-		--enable-libaom \
-		--enable-libass \
-		--enable-libbluray \
-		--enable-libdav1d \
-		--enable-libgme \
-		--enable-libmp3lame \
-		--enable-libfreetype \
-		--enable-libvorbis \
-		--enable-libopus \
-		--enable-librav1e \
-		--enable-libsoxr \
-		--enable-libx264 \
-		--enable-libx265 \
-		--enable-libxvid \
-		--enable-libvidstab \
-		--enable-libvpx \
-		--enable-libwebp \
-		--enable-libxml2 \
-		--enable-shared \
-		--prefix="$TERMUX_PREFIX" \
-		--target-os=android \
-		--extra-libs="-landroid-glob" \
-		$_EXTRA_CONFIGURE_FLAGS
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
+		--arch=${_ARCH:-${TERMUX_ARCH}}
+		--as=${AS}
+		--cc=${CC}
+		--cxx=${CXX}
+		--nm=${NM}
+		--pkg-config=${PKG_CONFIG}
+		--strip=${STRIP}
+		--cross-prefix=${TERMUX_HOST_PLATFORM}-
+		${_EXTRA_CONFIGURE_FLAGS}
+		"
 }
