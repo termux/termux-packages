dda70653c4a24abdf50560f06565a399ec31cc08
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-06-16 17:22:25 +0800

    add(root): new package nexttrace-enhanced
    Signed-off-by: 2096779623 <admin@utermux.dev>

---
+++ root-packages/nexttrace-enhanced/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/OwO-Network/nexttrace-enhanced
+TERMUX_PKG_DESCRIPTION="An open source visual routing tool that pursues light weight"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.1.10-beta.2"
+TERMUX_PKG_SRCURL=https://github.com/OwO-Network/nexttrace-enhanced/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f3fa511f821fd7ae32d855b18b3846adebf88e40dcc2e1b6619c1ebb0d1a9445
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	go build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin nexttrace
+}
