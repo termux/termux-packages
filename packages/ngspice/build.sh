2f64983f8688f08ad86764863374374630cb3f23
Al-buharie Amjari <harieamjari@gmail.com>
2022-06-17 11:11:48 +0800

    Fixes missing .cm at lib/ngspice

--- packages/ngspice/build.sh
+++ packages/ngspice/build.sh
@@ -9,3 +9,15 @@ TERMUX_PKG_SHA256=9beea6741a36a36a70f3152a36c82b728ee124c59a495312796376b30c8bec
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-xspice --enable-cider --with-readline=yes --enable-openmp"
 TERMUX_PKG_DEPENDS="libc++, readline, fftw"
 TERMUX_PKG_GROUPS="science"
+
+termux_step_post_make_install(){
+  export NGSPICE_ICM_PATH="$TERMUX_PKG_BUILDDIR/src/xspice/icm"
+  mkdir -p $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/analog/analog.cm $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/digital/digital.cm $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/spice2poly/spice2poly.cm $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/table/table.cm $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/xtradev/xtradev.cm $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/xtraevt/xtraevt.cm $TERMUX_PREFIX/lib/ngspice
+  install -m755 $NGSPICE_ICM_PATH/analog/analog.cm $TERMUX_PREFIX/lib/ngspice
+}
