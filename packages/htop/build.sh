941019b593beddee25b4f5b88c6b0c8de705e003
marcusz <bautista.wyatt.marcus_30@outlook.com>
2022-06-18 11:51:30 +0800

    fix(main/htop): use `access()` function instead of `getuid()`
    The getuid() root checks produces undesired behavior if this function is used
    instead of proper checks for file permissions whether the `/proc/stat` can be
    accessed or not.
    
    - If `htop` runs in fake root environments (proot/fakeroot), the getuid()
    function would obviously return the root user id. as a result, it will use the
    actual /proc/stat file and would fail in case the user does not have `/proc/stat` access.
    
    - The getuid() checks is the unreliable way of checking the
    permissions of `/proc/stat` file as suppose that it would always require root for
    checking the readability of the process status file and use it.
    using access() function should allow proper checks regardless of a logged user account.
    
    This change would likely replace the postinst checks of /proc/stat file and include
    the fake process status file in the deb package instead

--- packages/htop/build.sh
+++ packages/htop/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.2.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/htop-dev/htop/archive/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1a1dd174cc828521fe5fd0e052cff8c30aa50809cf80d3ce3a481c37d476ac54
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
@@ -23,7 +24,7 @@ termux_step_pre_configure() {
 	./autogen.sh
 }
 
-termux_step_create_debscripts() {
-	cp -f $TERMUX_PKG_BUILDER_DIR/postinst ./
-	sed -i "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" ./postinst
+termux_step_post_make_install() {
+	mkdir $TERMUX_PREFIX/var/htop -p
+	cp -a $TERMUX_PKG_BUILDER_DIR/procstat $TERMUX_PREFIX/var/htop/stat
 }
