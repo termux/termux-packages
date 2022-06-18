d438c7d3c60dd59682863156cebaec1b23028c3b
2096779623 <admin@utermux.dev>
2022-06-18 21:00:33 +0800

    add(main): new package railway-cli

---
+++ packages/railway-cli/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://railway.app
+TERMUX_PKG_DESCRIPTION="This is the command line interface for Railway"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.8.0
+TERMUX_PKG_SRCURL=https://github.com/railwayapp/cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e11c1b01b89533647252e26bb57e565512b55ff83710d5aea77b7419993f13ba
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+}
+
+termux_step_make() {
+	go build -ldflags="-s -w -X github.com/railwayapp/cli/constants.Version=${TERMUX_PKG_VERSION}" -o bin/railway
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin bin/railway
+}
