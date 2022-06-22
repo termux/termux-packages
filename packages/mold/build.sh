4723156d016f478667e03a9c758485ac3b2cc14c
Jia Yuan Lo <jylo06g@gmail.com>
2022-06-23 00:03:39 +0800

    upgpkg(main/mold): update to 1.3.0
    Includes:
    * chore(main/mold): fix build error and cleanup build.sh
      Fix compress.cc:18:10: fatal error: 'zlib.h' file not found
    
    Co-Authored-By: Jia Yuan Lo <jylo06g@gmail.com>

--- packages/mold/build.sh
+++ packages/mold/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/rui314/mold
 TERMUX_PKG_DESCRIPTION="mold: A Modern Linker"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/rui314/mold/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=41868663ff18afee3fa35e5e3fdf3d9575eb2e4ff49967b8f42f479c61c1ec34
+TERMUX_PKG_SHA256=02132ae717d7f22f8bc7e5c22642ad41541ec4c535fa85f095c60ecc81465a3d
 TERMUX_PKG_DEPENDS="libc++, openssl, zlib, libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -19,19 +19,19 @@ termux_step_make() {
 	# When building mold-wrapper.so cant find
 	# spawn.h from libandroid-spawn for some reason
 	# Manually link just in case to avoid runtime surprises
-	make -j ${TERMUX_MAKE_PROCESSES} \
-		PREFIX="${TERMUX_PREFIX}" \
-		CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include" \
-		CXXFLAGS="${CXXFLAGS}" \
-		STRIP="${STRIP}" \
+	make -j "$TERMUX_MAKE_PROCESSES" \
+		PREFIX="$TERMUX_PREFIX" \
+		CFLAGS="$CFLAGS -I${TERMUX_PREFIX}/include" \
+		CXXFLAGS="$CXXFLAGS -I${TERMUX_PREFIX}/include" \
+		STRIP="$STRIP" \
 		MOLD_WRAPPER_LDFLAGS=" -ldl -landroid-spawn"
 }
 
 termux_step_make_install() {
-	make -j ${TERMUX_MAKE_PROCESSES} install \
-		PREFIX="${TERMUX_PREFIX}" \
-		CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include" \
-		CXXFLAGS="${CXXFLAGS}" \
-		STRIP="${STRIP}" \
+	make -j "$TERMUX_MAKE_PROCESSES" install \
+		PREFIX="$TERMUX_PREFIX" \
+		CFLAGS="$CFLAGS -I${TERMUX_PREFIX}/include" \
+		CXXFLAGS="$CXXFLAGS -I${TERMUX_PREFIX}/include" \
+		STRIP="$STRIP" \
 		MOLD_WRAPPER_LDFLAGS=" -ldl -landroid-spawn"
 }
