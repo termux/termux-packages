560e13f697c3718aabb854d820d9939fe78770f3
2096779623 <admin@utermux.dev>
2022-06-24 17:41:42 +0800

    upgpkg(main/clash): update to 1.11.0
    Signed-off-by: 2096779623 <admin@utermux.dev>

--- packages/clash/build.sh
+++ packages/clash/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Dreamacro/clash
 TERMUX_PKG_DESCRIPTION="A rule-based tunnel in Go."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Philipp Schmitt <philipp@schmitt.co>"
-TERMUX_PKG_VERSION=1.9.0
-TERMUX_PKG_SRCURL="https://github.com/Dreamacro/clash/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=a276b1e7247e847fe44fe336a67bdecbb442748a1e7fa01d9c1ce0d52f9168ef
+TERMUX_PKG_VERSION=1.11.0
+TERMUX_PKG_SRCURL="https://github.com/Dreamacro/clash/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=551d6e1bfb87bb7bb7dc0cd36a763791d95d09a610c49544575baa1905b18986
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
