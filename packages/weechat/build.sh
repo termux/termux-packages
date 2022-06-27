b9b227eddd879c05d31a9250d87e9961328f2ad5
Arun <arun007@pm.me>
2022-06-27 12:25:03 +0530

    upgpkg(main/weechat): update to 3.5

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -2,19 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://weechat.org/
 TERMUX_PKG_DESCRIPTION="Fast, light and extensible IRC chat client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.1
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=3.5
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=63ac24c41e88798ad48bfbe8a7e1fd56ddf24416f86bccd3a53b258a569ca038
-TERMUX_PKG_DEPENDS="libiconv, ncurses, libgcrypt, libcurl, libgnutls, libandroid-support, zlib"
+TERMUX_PKG_SHA256=f1949ad9d40f4e20a1196d344f938048799dd02d8f6ad52d4eef46af8085e0e7
+TERMUX_PKG_DEPENDS="libiconv, ncurses, libgcrypt, libcurl, libgnutls, libandroid-support, zlib, zstd"
 TERMUX_PKG_BREAKS="weechat-dev"
 TERMUX_PKG_REPLACES="weechat-dev"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/weechat-curses share/man/man1/weechat-headless.1 share/icons"
-
-# Ruby 3.x is not supported as of weechat 3.0.1.
-#-- Checking for one of the modules 'ruby-2.7;ruby-2.6;ruby-2.5;ruby-2.4;ruby-2.3;ruby-2.2;ruby-2.1;ruby-2.0;ruby-1.9'
-#CMake Error at src/plugins/CMakeLists.txt:116 (message):
-#  Ruby not found
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCA_FILE=$TERMUX_PREFIX/etc/tls/cert.pem
 -DENABLE_HEADLESS=OFF
@@ -24,7 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_PYTHON3=ON
 -DENABLE_TCL=OFF
 -DENABLE_PHP=OFF
--DENABLE_RUBY=OFF
+-DENABLE_RUBY=ON
 -DENABLE_JAVASCRIPT=OFF
 -DENABLE_GUILE=OFF
 -DENABLE_SPELL=OFF
