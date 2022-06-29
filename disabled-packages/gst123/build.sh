74f38f54d122798b50c496152e9348291e76841c
Henrik Grimler <grimler@termux.dev>
2022-06-29 22:34:45 +0200

    updpkg(disabled/gst123): update to 0.3.5
    Switch to https and update version while we are at it.

--- disabled-packages/gst123/build.sh
+++ disabled-packages/gst123/build.sh
@@ -1,6 +1,7 @@
 # Broken - requires gtk
-TERMUX_PKG_HOMEPAGE=http://space.twc.de/~stefan/gst123.php
+TERMUX_PKG_HOMEPAGE=https://space.twc.de/~stefan/gst123.php
 TERMUX_PKG_DESCRIPTION="GStreamer based command line media player"
-TERMUX_PKG_VERSION=0.3.4
-TERMUX_PKG_SRCURL=http://space.twc.de/~stefan/gst123/gst123-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_VERSION=0.3.5
+TERMUX_PKG_SRCURL=https://space.twc.de/~stefan/gst123/gst123-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256="c7b4729773f66cc679e94df76bcc6a95a2222192730f906e527e72624f084a7d"
 TERMUX_PKG_DEPENDS="gst-plugins-bad, ncurses"
