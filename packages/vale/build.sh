0737038285bf43fd532bf59add41af1a420c8927
2096779623 <admin@utermux.dev>
2022-06-30 17:39:46 +0800

    add(main): new package vale
    Signed-off-by: 2096779623 <admin@utermux.dev>

---
+++ packages/vale/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://vale.sh
+TERMUX_PKG_DESCRIPTION="A syntax-aware linter for prose built with speed and extensibility in mind"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="2.19.0"
+TERMUX_PKG_SRCURL=https://github.com/errata-ai/vale/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bb2e3ffa0901ebc46686ee99ef52ec4c2634dd0c226d591fde4a7d585a44e2c6
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	cd "$TERMUX_PKG_SRCDIR"/cmd/vale
+	go build -o vale -ldflags="-s -w -X 'main.version=${TERMUX_PKG_VERSION}'"
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/cmd/vale/vale
+}
