2e070918e7a045be85c869ec1ec80f12afd7b6c7
Henrik Grimler <grimler@termux.dev>
2022-06-30 09:06:12 +0200

    updpkg(disabled/premake): update to 5.0.0-beta1
    To be able to download an archive and set TERMUX_PKG_SHA256.

--- disabled-packages/premake/build.sh
+++ disabled-packages/premake/build.sh
@@ -1,7 +1,8 @@
 TERMUX_PKG_HOMEPAGE=http://premake.github.io/
 TERMUX_PKG_DESCRIPTION="Build script generator"
-TERMUX_PKG_VERSION=4.4-beta5
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/premake/Premake/4.4/premake-${TERMUX_PKG_VERSION}-src.zip
+TERMUX_PKG_VERSION=5.0.0-beta1
+TERMUX_PKG_SRCURL=https://github.com/premake/premake-core/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=97fa4cef9fb6459c39da4e70756c0e13ae7b090fffe9442306c768b8b62e1589
 # TERMUX_PKG_DEPENDS="pcre, openssl, libuuid"
 # TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ssl=openssl"
 
