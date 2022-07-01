1db47528b1488e25b7cb6ffc92258f73de524767
2096779623 <admin@utermux.dev>
2022-07-01 12:43:42 +0800

    add(main): new package peco
    Signed-off-by: 2096779623 <admin@utermux.dev>

---
+++ packages/peco/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/peco/peco
+TERMUX_PKG_DESCRIPTION="Simplistic interactive filtering tool"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.5.10"
+TERMUX_PKG_SRCURL=https://github.com/peco/peco/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=781c2effc4f6a58d9ff96fb0fc8b0fba3aab56a91a34933d68c5de3aea5fe3f6
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	cd "$TERMUX_PKG_SRCDIR"/cmd/peco
+	go build -o peco
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/cmd/peco/peco
+}
