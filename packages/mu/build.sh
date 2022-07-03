6b61e2ee2790c5550cc4fea51b52a63af720697b
Aditya Alok <dev.aditya.alok@gmail.com>
2022-07-03 20:07:19 +0530

    upgpkg(main/mu): to 1.8.3
    * remove `mu4e-dont-bytecompile-meson.patch`. Byte-compiling has been
      made optional.
      See https://github.com/djcb/mu/commit/40517e8a1ea451565e9e81ce57ede0fee674fecd
    
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -2,23 +2,23 @@ TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.8.2"
+TERMUX_PKG_VERSION="1.8.3"
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=28ae5a2066a862737e389281f8b8cabb056db2d6131810a16e43aa31b5a02491
+TERMUX_PKG_SHA256=ac6110beb0f2119b0ffc5c258e20f29de91dbb890d77c36bef934ba1c02b91a6
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="emacs, glib, libc++, libxapian, libgmime"
 
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "(setq byte-compile-warnings nil)" > $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.el
-	LC_ALL=C $TERMUX_PREFIX/bin/emacs -no-site-file -q -batch -l $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.el -f batch-byte-compile $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/*.el
-	rm -f $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.elc
-	rm -f $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.el
-	chmod 644 $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/*.elc
+		#!$TERMUX_PREFIX/bin/sh
+		echo "(setq byte-compile-warnings nil)" > $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.el
+		LC_ALL=C $TERMUX_PREFIX/bin/emacs -no-site-file -q -batch -l $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.el -f batch-byte-compile $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/*.el
+		rm -f $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.elc
+		rm -f $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/nowarnings.el
+		chmod 644 $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/*.elc
 	EOF
 
 	cat <<- EOF > ./prerm
-	rm -f $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/*.elc
+		rm -f $TERMUX_PREFIX/share/emacs/site-lisp/mu4e/*.elc
 	EOF
 }
