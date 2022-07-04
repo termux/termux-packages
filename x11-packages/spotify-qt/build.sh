16db1404e585d610c4ad5686dbeb607dfc907ee2
T-Dynamos <anshdadwal298@gmail.com>
2022-07-04 15:29:51 +0530

    add(x11): new package spotify-qt

---
+++ x11-packages/spotify-qt/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/kraxarn/spotify-qt"
+TERMUX_PKG_DESCRIPTION="Lightweight Spotify client using Qt"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.8
+TERMUX_PKG_SRCURL="https://github.com/kraxarn/spotify-qt/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="deb12800b9fb4f501f8dfc97a2fdc4989849fe8bbf6f38d79503214e03afe5ad"
+TERMUX_PKG_DEPENDS="qt5-qtbase,zlib,qt5-qtsvg"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_PREFIX:PATH=${TERMUX_PREFIX}"
