1a96ec0d88d9ffdebd568595ac34f0b76f1e9406
Henrik Grimler <grimler@termux.dev>
2022-07-04 08:55:32 +0200

    fix(x11/mate-terminal): fix build with automake 1.16.4
    We need to re-generate the configure script, or else we get an error:
    
      configure.ac:15: error: version mismatch.  This is Automake 1.16.4,
      configure.ac:15: but the definition used by this AM_INIT_AUTOMAKE
      configure.ac:15: comes from Automake 1.16.1.  You should recreate
      configure.ac:15: aclocal.m4 with aclocal and run automake again.
    
    However, running autoreconf -vfi reveals that some of the autoconf
    macros are *only* defined in aclocal.m4, and autoreconfiguring
    replaces that file and causes the macros to be undefined, giving
    errors like:
    
      /home/builder/.termux-build/mate-terminal/src/configure: line 2931: MATE_COMMON_INIT: command not found
      /home/builder/.termux-build/mate-terminal/src/configure: line 2932: syntax error near unexpected token `no'
      /home/builder/.termux-build/mate-terminal/src/configure: line 2932: `MATE_DEBUG_CHECK(no)'
    
    Lets move all the custom macros from aclocal.m4 to the m4/ directory
    to solve that issue.
    
    Fixes termux/termux-packages#11145.

--- x11-packages/mate-terminal/build.sh
+++ x11-packages/mate-terminal/build.sh
@@ -8,3 +8,7 @@ TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-terminal/releases/downloa
 TERMUX_PKG_SHA256=550d38f223d21ab12d39b00af6cd75f083d3790c38d53051537df2ac6a87be62
 TERMUX_PKG_DEPENDS="libvte, dconf, gtk3, libsm"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
+
+termux_step_pre_configure() {
+	autoreconf -vfi
+}
