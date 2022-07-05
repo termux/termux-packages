5eb5a18e6aebacc331320f204d046cceb1698d09
Aditya Alok <dev.aditya.alok@gmail.com>
2022-07-05 09:34:41 +0530

    fix(main/mu): bump revision after changes in #11161
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.8.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ac6110beb0f2119b0ffc5c258e20f29de91dbb890d77c36bef934ba1c02b91a6
 TERMUX_PKG_AUTO_UPDATE=true
