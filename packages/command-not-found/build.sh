00685f60ad795e28e31658dd659ea41c780e8646
Yaksh Bariya <yakshbari4@gmail.com>
2022-07-06 14:17:18 +0530

    rebuild(main/command-not-found): to update database
    Recently added cook command from cookcli is not in command-not-found
    database.

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell se
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=20b7c0f277bb393dbe8fd341692345d224a6fcdc17aae95d290961831e7ae702
 
