ba301e3844609bf534db835b3c94ae6350bb9558
Elliana May <me@mause.me>
2022-07-09 19:40:09 +0800

    feat: bump duckdb version

--- packages/libduckdb/build.sh
+++ packages/libduckdb/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://duckdb.org/
 TERMUX_PKG_DESCRIPTION="An in-process SQL OLAP database management system"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.4
+TERMUX_PKG_VERSION=0.4.0
 # we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
 TERMUX_PKG_SRCURL=https://github.com/duckdb/duckdb.git
 TERMUX_PKG_DEPENDS="libc++"
