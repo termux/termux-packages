acc766f652b9fa648133e28cd841ef8538d0f57e
Henrik Grimler <grimler@termux.dev>
2022-07-16 08:13:21 +0200

    updpkg(main/termux-elf-cleaner): update to 2.1.1

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-elf-cleaner
 TERMUX_PKG_DESCRIPTION="Cleaner of ELF files for Android"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-# NOTE: The termux-elf-cleaner.cpp file is used by
-#       scripts/build/termux_step_start_build.sh
-#       to create a native binary. Bumping this version will need
-#       updating the checksum used there.
-TERMUX_PKG_VERSION=1.10
+TERMUX_PKG_VERSION=2.1.1
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b01a48bc6db85c66c683a2834608056ddc66f1964f7553810e8608aee3ee5648
+TERMUX_PKG_SHA256=104231f91ef6662f80694fc4a59c6bfeae50da21c4fc22adac3c9a5aac00ba98
 TERMUX_PKG_DEPENDS="libc++"
-TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	autoreconf -vfi
+}
