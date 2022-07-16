5d07daf77213d837379ed1d767a32c948e10218f
Henrik Grimler <grimler@termux.dev>
2022-07-16 08:16:13 +0200

    scripts(start_build.sh): download pre-compiled termux-elf-cleaner
    CI now compiles the program on every release, so lets download that
    instead of the source in termux_step_start_build.sh.

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -2,6 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-elf-cleaner
 TERMUX_PKG_DESCRIPTION="Cleaner of ELF files for Android"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
+# Please update checksum in termux_step_start_build.sh as well if
+# updating the package.
 TERMUX_PKG_VERSION=2.1.1
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=104231f91ef6662f80694fc4a59c6bfeae50da21c4fc22adac3c9a5aac00ba98
