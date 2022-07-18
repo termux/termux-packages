4dfaaf3c098af7837566915cfe272de2c2cfc0cb
Jia Yuan Lo <jylo06g@gmail.com>
2022-07-19 01:23:25 +0800

    upgpkg(main/clvk): to 0.0.20220717g1ec854e6
    Includes:
    * enable custom auto update
    * clean up build.sh

--- packages/clvk/build.sh
+++ packages/clvk/build.sh
@@ -2,14 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/kpet/clvk
 TERMUX_PKG_DESCRIPTION="Experimental implementation of OpenCL on Vulkan"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.0.20220626
-_COMMIT=d416170a674622f20bac04b7dd33b68bfb4096f6
+_COMMIT=1ec854e6bb3455354e40d83d81bc45a137b253cc
+_COMMIT_DATE=20220717
+_COMMIT_TIME=190549
+# termux_pkg_upgrade_version edits TERMUX_PKG_VERSION wholly
+TERMUX_PKG_VERSION="0.0.20220717g1ec854e6"
 TERMUX_PKG_SRCURL=https://github.com/kpet/clvk.git
 TERMUX_PKG_GIT_BRANCH=main
 TERMUX_PKG_BUILD_DEPENDS="vulkan-loader-android, vulkan-headers"
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_SUGGESTS="ocl-icd"
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLLVM_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/bin/llvm-tblgen
 -DCLANG_TABLEGEN=$TERMUX_PKG_HOSTBUILD_DIR/bin/clang-tblgen
@@ -19,6 +23,51 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 # clvk currently does not have proper versioning nor releases
 # Use dates and commits as versioning for now
+termux_pkg_auto_update() {
+	local latest_commit
+	latest_commit="$(curl -s https://api.github.com/repos/kpet/clvk/commits | jq .[].sha | head -1 | sed -e 's|\"||g')"
+
+	if [ -z "$latest_commit" ]; then
+		termux_error_exit "ERROR: Unable to get latest commit from upstream"
+	fi
+
+	if [ "$latest_commit" = "$_COMMIT" ]; then
+		echo "INFO: No update needed. Already at version '$TERMUX_PKG_VERSION'."
+		return 0
+	fi
+
+	local latest_commit_date_tz
+	latest_commit_date_tz="$(curl -s https://api.github.com/repos/kpet/clvk/commits/$latest_commit | jq .commit.committer.date | sed -e 's|\"||g')"
+
+	if [ -z "$latest_commit_date_tz" ]; then
+		termux_error_exit "ERROR: Unable to get latest commit date info"
+	fi
+
+	local latest_commit_date="$(echo $latest_commit_date_tz | sed -e 's|\(.*\)T\(.*\)Z|\1|' -e 's|\-||g')"
+	local latest_commit_time="$(echo $latest_commit_date_tz | sed -e 's|\(.*\)T\(.*\)Z|\2|' -e 's|\:||g')"
+	local latest_version="0.0.${latest_commit_date}g${latest_commit:0:8}"
+
+	# less likely to happen, not going to include time into an already long versioning
+	if [ "$latest_commit_date" -eq "$_COMMIT_DATE" ] && [ "$latest_commit_time" -gt "$_COMMIT_TIME" ]; then
+		if ! dpkg --compare-versions "$latest_version" gt "$TERMUX_PKG_VERSION"; then
+			termux_error_exit "ERROR: Resulting latest version is not counted as update to the current version ($latest_version < $TERMUX_PKG_VERSION)"
+		fi
+	fi
+
+	# unlikely to happen
+	if [ "$latest_commit_date" -lt "$_COMMIT_DATE" ]; then
+		termux_error_exit "ERROR: Upstream is older than current package version. Please report to upstream."
+	elif [ "$latest_commit_date" -eq "$_COMMIT_DATE" ] && [ "$latest_commit_time" -lt "$_COMMIT_TIME" ]; then
+		termux_error_exit "ERROR: Upstream is older than current package version. Please report to upstream."
+	fi
+
+	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^_COMMIT=.*|_COMMIT=${latest_commit}|"
+	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^_COMMIT_DATE=.*|_COMMIT_DATE=${latest_commit_date}|"
+	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^_COMMIT_TIME=.*|_COMMIT_TIME=${latest_commit_time}|"
+
+	# maybe save a few ms as we already done version check
+	termux_pkg_upgrade_version "$latest_version" --skip-version-check
+}
 
 # clvk prefers Khronos Vulkan Loader than the one come from NDK
 # Sticking with NDK should expose more Vulkan limitations in Android (like below)
@@ -70,11 +119,11 @@ termux_step_pre_configure() {
 	# from packages/libllvm/build.sh
 	export LLVM_DEFAULT_TARGET_TRIPLE=${CCTERMUX_HOST_PLATFORM/-/-unknown-}
 	export LLVM_TARGET_ARCH
-	if [ $TERMUX_ARCH = "arm" ]; then
+	if [ "$TERMUX_ARCH" = arm ]; then
 		LLVM_TARGET_ARCH=ARM
-	elif [ $TERMUX_ARCH = "aarch64" ]; then
+	elif [ "$TERMUX_ARCH" = aarch64 ]; then
 		LLVM_TARGET_ARCH=AArch64
-	elif [ $TERMUX_ARCH = "i686" ] || [ $TERMUX_ARCH = "x86_64" ]; then
+	elif [ "$TERMUX_ARCH" = i686 ] || [ "$TERMUX_ARCH" = x86_64 ]; then
 		LLVM_TARGET_ARCH=X86
 	else
 		termux_error_exit "Invalid arch: $TERMUX_ARCH"
