9923880cda93ed7780aa421c255213d1c99b4bb9
2096779623 <admin@utermux.dev>
2022-07-18 16:09:16 +0800

    add(main): new package tere
    Signed-off-by: 2096779623 <admin@utermux.dev>

---
+++ packages/tere/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/mgunyho/tere
+TERMUX_PKG_DESCRIPTION="Terminal file explorer written in rust"
+TERMUX_PKG_LICENSE="EUPL-1.2"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SRCURL=https://github.com/mgunyho/tere/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=a4ebd2f0b5728d6ec61749564106d67d74720fa05cb906686d559a5f15549698
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	termux_setup_rust
+
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/tere
+}
