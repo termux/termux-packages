5ffdab5de101abdf01fb94c1e017bbc94bda3bd1
Li Chong Yun <45286352+licy183@users.noreply.github.com>
2022-07-21 21:13:10 +0800

    ipfs: update to 0.14.0 and switch LICENSE

--- packages/ipfs/build.sh
+++ packages/ipfs/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://ipfs.io/
 TERMUX_PKG_DESCRIPTION="A peer-to-peer hypermedia distribution protocol"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="MIT, Apache-2.0"
+TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE-APACHE, LICENSE-MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.13.1"
-TERMUX_PKG_SRCURL=https://github.com/ipfs/go-ipfs/releases/download/v${TERMUX_PKG_VERSION}/go-ipfs-source.tar.gz
-TERMUX_PKG_SHA256=906b6a15be05c71f66c43a97f184aa9d8f3be6c53bd29ccbd81c16050839b135
+TERMUX_PKG_VERSION="0.14.0"
+TERMUX_PKG_SRCURL=https://github.com/ipfs/kubo/releases/download/v${TERMUX_PKG_VERSION}/kubo-source.tar.gz
+TERMUX_PKG_SHA256=f778ddd2be675a41ab32768f5e824c7fa747c4caed8963e090761a587d74f658
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SUGGESTS="termux-services"
 TERMUX_PKG_SERVICE_SCRIPT=("ipfs" "[ ! -d \"${TERMUX_ANDROID_HOME}/.ipfs\" ] && ipfs init --empty-repo 2>&1 && ipfs config --json Swarm.EnableRelayHop false 2>&1 && ipfs config --json Swarm.EnableAutoRelay true 2>&1; exec ipfs daemon --enable-namesys-pubsub 2>&1")
@@ -15,8 +16,8 @@ termux_step_make() {
 	export GOPATH=${TERMUX_PKG_BUILDDIR}
 
 	mkdir -p "${GOPATH}/src/github.com/ipfs"
-	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/ipfs/go-ipfs"
-	cd "${GOPATH}/src/github.com/ipfs/go-ipfs"
+	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/ipfs/kubo"
+	cd "${GOPATH}/src/github.com/ipfs/kubo"
 
 	make build
 
@@ -26,5 +27,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX"/bin "${TERMUX_PKG_BUILDDIR}/src/github.com/ipfs/go-ipfs/cmd/ipfs/ipfs"
+	install -Dm700 -t "$TERMUX_PREFIX"/bin "${TERMUX_PKG_BUILDDIR}/src/github.com/ipfs/kubo/cmd/ipfs/ipfs"
 }
