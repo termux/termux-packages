9b8eeb9944657b1cc82b8c133918727ed46f8117
Lucy Phipps <landfillbaby69@gmail.com>
2022-07-22 22:29:35 +0100

    jump: update to 0.50.0

--- packages/jump/build.sh
+++ packages/jump/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gsamokovarov/jump
 TERMUX_PKG_DESCRIPTION="Jump helps you navigate in shell faster by learning your habits"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.40.0
+TERMUX_PKG_VERSION=0.50.0
 TERMUX_PKG_SRCURL=https://github.com/gsamokovarov/jump/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f005f843fc65b7be1d4159da7d4c220eef0229ecec9935c6ac23e4963eef645e
+TERMUX_PKG_SHA256=7832e968c81659e3750b8ecaaa49eb769fff4a96e790e28ef3d1e479f11affb4
 
 termux_step_make() {
 	termux_setup_golang
