8c63462672c86409d6fec45b3a03e759a1e78024
Lucy Phipps <landfillbaby69@gmail.com>
2022-07-22 15:27:49 +0100

    Revert "fakeroot: update to 1.29"
    This reverts commit b99c2f51b7f63a0c23abc4f06f2d6eeda72dc9cf.

--- packages/fakeroot/build.sh
+++ packages/fakeroot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://packages.debian.org/fakeroot
 TERMUX_PKG_DESCRIPTION="Tool for simulating superuser privileges (with tcp ipc)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.29
+TERMUX_PKG_VERSION=1.25.3
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_${TERMUX_PKG_VERSION}.orig.tar.gz
-TERMUX_PKG_SHA256=8fbbafb780c9173e3ace4a04afbc1d900f337f3216883939f5c7db3431be7c20
+TERMUX_PKG_SHA256=8e903683357f7f5bcc31b879fd743391ad47691d4be33d24a76be3b6c21e956c
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ipc=tcp"
 TERMUX_PKG_BUILD_DEPENDS="libcap"
 
