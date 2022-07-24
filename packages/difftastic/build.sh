0f6302d8c2c24c8b74315f7723ea53bcf90761b6
Pooya Moradi <pvonmoradi@gmail.com>
2022-07-24 08:46:45 +0430

    new package: difftastic

---
+++ packages/difftastic/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/Wilfred/difftastic"
+TERMUX_PKG_DESCRIPTION="difft: A structural diff that understands syntax"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.31.0"
+TERMUX_PKG_SRCURL="https://github.com/Wilfred/difftastic/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256="0f6370aef09eed8cfd03add1b347dc2c3b84009493af7cbcf3ee5c12935cdac7"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+# needed for MIME database (optional in upstream)
+TERMUX_PKG_RECOMMENDS="file"
