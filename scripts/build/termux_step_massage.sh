7e2daf81e14fd1aedff8398de53dde51f1c6a9de
Lucinda May Phipps <landfillbaby69@gmail.com>
2022-07-26 09:59:37 +0100

    termux-step-massage: faster empty package check

--- scripts/build/termux_step_massage.sh
+++ scripts/build/termux_step_massage.sh
@@ -69,10 +69,12 @@ termux_step_massage() {
 	# Check so files were actually installed. Exclude
 	# share/doc/$TERMUX_PKG_NAME/ as a license file is always
 	# installed there.
-	if [ "$(find . -type f -not -path "./share/doc/$TERMUX_PKG_NAME/*")" = "" ] && { [ -f "$TERMUX_PKG_SRCDIR"/configure.ac ] || [ -f "$TERMUX_PKG_SRCDIR"/configure.in ]; }; then
-		termux_error_exit "No files in package. Maybe you need to run autoreconf -fi before configuring."
-	elif [ "$(find . -type f -not -path "./share/doc/$TERMUX_PKG_NAME/*")" = "" ]; then
-		termux_error_exit "No files in package."
+	if [ "$(find . -path "./share/doc/$TERMUX_PKG_NAME" -prune -o -type f -print | head -n1)" = "" ]; then
+		if [ -f "$TERMUX_PKG_SRCDIR"/configure.ac -o -f "$TERMUX_PKG_SRCDIR"/configure.in ]; then
+			termux_error_exit "No files in package. Maybe you need to run autoreconf -fi before configuring."
+		else
+			termux_error_exit "No files in package."
+		fi
 	fi
 
 	local HARDLINKS
