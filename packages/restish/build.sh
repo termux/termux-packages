ea66e0a7c77e1e0edad925b7ebe40d15966f74c4
Pooya Moradi <pvonmoradi@gmail.com>
2022-07-27 22:00:07 +0430

    new package: restish

---
+++ packages/restish/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/danielgtaylor/restish"
+TERMUX_PKG_DESCRIPTION="A CLI for interacting with REST-ish HTTP APIs"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE.md"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.14.0"
+TERMUX_PKG_SRCURL="https://github.com/danielgtaylor/restish/archive/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256="2688340ff307aa522325afab40b9eaf6847d0b4b6a7a9c05f041056408b3fc50"
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+    termux_setup_golang
+    cd "$TERMUX_PKG_SRCDIR"
+    export GOPATH="${TERMUX_PKG_BUILDDIR}"
+    go build
+}
+
+termux_step_make_install() {
+    install -Dm755 -t "${TERMUX_PREFIX}/bin" "${TERMUX_PKG_SRCDIR}/restish"
+}
