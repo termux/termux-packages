e5bc179855e40d00d54e348c76afc06f17f9758a
leapofazzam123 <leapofazzam@gmail.com>
2022-07-31 15:25:17 +0700

    new package: zig-nightly

--- packages/zig-nightly/build.sh
+++ packages/zig-nightly/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://ziglang.org
-TERMUX_PKG_DESCRIPTION="General-purpose programming language and toolchain for maintaining robust, optimal, and reusable software."
+TERMUX_PKG_DESCRIPTION="General-purpose programming language and toolchain for maintaining robust, optimal, and reusable software. (nightly version)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@leapofazzam123"
-TERMUX_PKG_VERSION=0.9.1
+TERMUX_PKG_VERSION=0.10.0-dev.3385+c0a1b4fa4
 TERMUX_PKG_SRCURL=https://ziglang.org/download/$TERMUX_PKG_VERSION/zig-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=38cf4e84481f5facc766ba72783e7462e08d6d29a5d47e3b75c8ee3142485210
+TERMUX_PKG_SHA256=ea1a28f1240a5ea00efb7047cd553e46fbe505a33173b4ed5eae12a0793dcad9
 TERMUX_PKG_DEPENDS="libandroid-spawn, libc++, libxml2, ncurses, zlib"
 TERMUX_PKG_BUILD_DEPENDS="llvm, libllvm-static"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -14,8 +14,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_pkg_auto_update() {
-	local version="$(curl -L https://ziglang.org/download/index.json | jq "del(.master) | keys | last(.[])")"
-	local sha256="$(curl -L https://ziglang.org/download/index.json | jq "del(.master) | .\"$version\".src.shasum")"
+	local version="$(curl -L https://ziglang.org/download/index.json | jq ".master.version")"
+	local sha256="$(curl -L https://ziglang.org/download/index.json | jq "del(.master) | .master.src.shasum")"
 	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^TERMUX_PKG_VERSION=.*|TERMUX_PKG_VERSION=$version|"
 	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^TERMUX_PKG_SHA256=.*|TERMUX_PKG_SHA256=$sha256|"
 }
