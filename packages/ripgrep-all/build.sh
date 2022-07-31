5df6378927cb7e9b6532d5edd2bd7deb423a0e7e
Henrik Grimler <grimler@termux.dev>
2022-07-31 19:56:09 +0200

    fix(main/ripgrep-all): fix undefined symbols error during build
    Fixes termux/termux-packages#11343

--- packages/ripgrep-all/build.sh
+++ packages/ripgrep-all/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Search tool able to locate in PDFs, E-Books, zip, tar.gz
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=0.9.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_DEPENDS="ripgrep, fzf"
 TERMUX_PKG_RECOMMENDS="ffmpeg, poppler, sqlite"
 TERMUX_PKG_SRCURL=https://github.com/phiresky/ripgrep-all/archive/v${TERMUX_PKG_VERSION}.tar.gz
@@ -13,7 +13,9 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
 	termux_setup_rust
-	
+
+	RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
+
 	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rga
 	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rga-preproc
