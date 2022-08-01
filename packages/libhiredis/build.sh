81b79f7593064752a6749206267bbc6f9d21b2ec
Henrik Grimler <grimler@termux.dev>
2022-08-01 10:57:50 +0200

    fix(main/libhiredis): bump after termux_step_massage fix
    Commit 792ccc6ca3d7 ("fix(scripts,step_massage): continue in for loop,
    not return") fixes so termux_step_massage runs fully again, rebuild
    package to make sure every fix/check is done.

--- packages/libhiredis/build.sh
+++ packages/libhiredis/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Hiredis is a minimalistic C client library for the Redis
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.2
+TERMUX_PKG_REVSION=1
 TERMUX_PKG_SRCURL="https://github.com/redis/hiredis/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=e0ab696e2f07deb4252dda45b703d09854e53b9703c7d52182ce5a22616c3819
 TERMUX_PKG_BUILD_DEPENDS="binutils"
