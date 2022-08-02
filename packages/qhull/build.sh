86d4db4333a745a732824e51cb9c002b270b46c7
Pooya Moradi <pvonmoradi@gmail.com>
2022-08-02 16:36:19 +0430

    new package: qhull

---
+++ packages/qhull/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE="http://www.qhull.org"
+TERMUX_PKG_DESCRIPTION="Calculate convex hulls and related structures"
+TERMUX_PKG_GROUPS="science"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING.txt"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="8.1-alpha1"
+TERMUX_PKG_SRCURL="https://github.com/qhull/qhull/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=09e5e4c5b2b8a9e617a46876fef5a3d33e70aa1d08a163ff05d37701327c3be7
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
