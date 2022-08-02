b1942c5f679467ae68836b2512dc40a0d96756eb
Pooya Moradi <pvonmoradi@gmail.com>
2022-08-02 16:12:54 +0430

    new package: libmuparser

---
+++ packages/libmuparser/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE="https://beltoforion.de/en/muparser"
+TERMUX_PKG_DESCRIPTION="An extensible high performance math expression parser library written in C++"
+TERMUX_PKG_GROUPS="science"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="2.3.3-1"
+TERMUX_PKG_SRCURL="https://github.com/beltoforion/muparser/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=91d26d8274ae9cd9c776ee58250aeddc6b574f369eafd03b25045b858a2b8177
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
