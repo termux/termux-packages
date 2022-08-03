24a6dbf5422434abe65311a222f22510054a2e89
Pooya Moradi <pvonmoradi@gmail.com>
2022-08-03 10:28:22 +0430

    new package: fclones

---
+++ packages/fclones/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/pkolaczk/fclones"
+TERMUX_PKG_DESCRIPTION="Efficient Duplicate File Finder"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.27.0"
+TERMUX_PKG_SRCURL="https://github.com/pkolaczk/fclones/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=ed135983bccac8f7568d51cde7752a25f46f7ba191dee7b74600ffba8f43039e
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
