191ff118d3304349c4ff9320c809a40dd8ea2ed3
Tom Yan <tom.ty89@gmail.com>
2022-08-05 16:40:46 +0800

    Remove pulseaudio from TERMUX_PKG_DEPENDS of sdl{,2}-net
    AFAIK not even sdl{,2} themselves has linkage to libpulse (in other
    words, it can be build dep and optdep instead). Removing pulseaudio
    from dep list of the -net packages to avoid causing distraction in
    future rev bump.

--- x11-packages/sdl-net/build.sh
+++ x11-packages/sdl-net/build.sh
@@ -6,6 +6,6 @@ TERMUX_PKG_VERSION=1.2.8
 TERMUX_PKG_REVISION=36
 TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_net/release/SDL_net-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5f4a7a8bb884f793c278ac3f3713be41980c5eedccecff0260411347714facb4
-TERMUX_PKG_DEPENDS="libandroid-glob, libflac, libogg, libsndfile, libvorbis, pulseaudio, sdl"
+TERMUX_PKG_DEPENDS="libandroid-glob, libflac, libogg, libsndfile, libvorbis, sdl"
 TERMUX_PKG_CONFLICTS="libsdl-net"
 TERMUX_PKG_REPLACES="libsdl-net"
