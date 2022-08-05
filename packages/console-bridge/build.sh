528f72b663e8fa2217e3dde215c6b68361dd7e47
Pooya Moradi <pvonmoradi@gmail.com>
2022-08-05 19:28:58 +0430

    new package: urdfdom

--- packages/console-bridge/build.sh
+++ packages/console-bridge/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/ros/console_bridge"
 TERMUX_PKG_DESCRIPTION="A ROS-independent package for logging that seamlessly pipes into rosconsole/rosout for ROS-dependent packages"
+TERMUX_PKG_GROUPS="science"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Pooya Moradi <pvonmoradi@gmail.com>"
 TERMUX_PKG_VERSION="1.0.2"
