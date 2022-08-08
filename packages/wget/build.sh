c06daebe1bfa2a34f6512e305d8b6cdd9245787e
Henrik Grimler <grimler@termux.dev>
2022-08-08 16:38:09 +0200

    fix(main/wget): link against libandroid-spawn
    Build fails if libandroid-spawn happen to be available during build.
    Properly depend on libandroid-spawn to prevent the error from
    occuring.

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -3,16 +3,19 @@ TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.21.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_SHA256=dbd2fb5e47149d4752d0eaa0dac68cc49cf20d46df4f8e326ffc8f18b2af4ea5
-TERMUX_PKG_DEPENDS="libiconv, pcre2, openssl, libuuid, libandroid-support, zlib"
+TERMUX_PKG_DEPENDS="libiconv, pcre2, openssl, libuuid, libandroid-support, libandroid-spawn, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_getpass=yes
-ac_cv_header_spawn_h=no
 ac_cv_libunistring=no
 --disable-iri
 --with-ssl=openssl
 --with-included-libunistring=no
 --without-libunistring-prefix
 "
+
+termux_step_pre_configure() {
+	export LDFLAGS+=" -landroid-spawn"
+}
