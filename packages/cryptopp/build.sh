22aa22f463d8fd1a68910037cfbcb11d0189beb5
2096779623 <admin@utermux.dev>
2022-08-08 09:10:07 +0800

    upgpkg(main/cryptopp): update to 8.7.0
    Signed-off-by: 2096779623 <admin@utermux.dev>
    
    Co-authored-by: Chongyun Lee <45286352+licy183@users.noreply.github.com>

--- packages/cryptopp/build.sh
+++ packages/cryptopp/build.sh
@@ -3,13 +3,12 @@ TERMUX_PKG_DESCRIPTION="A free C++ class library of cryptographic schemes"
 TERMUX_PKG_LICENSE="BSL-1.0, BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="License.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=8.6.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=8.7.0
 TERMUX_PKG_SRCURL=https://github.com/weidai11/cryptopp/archive/refs/tags/CRYPTOPP_${TERMUX_PKG_VERSION//./_}.tar.gz
-TERMUX_PKG_SHA256=9304625f4767a13e0a5f26d0f019d78cf9375604a33e5391c3bf2e81399dfeb8
+TERMUX_PKG_SHA256=8d6a4064b8e9f34cd3e838f5a12c40067ee7b95ee37d9173ec273cb0913e7ca2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+.\d+.\d+"
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_DEPENDS="libc++, libcpufeatures"
 TERMUX_PKG_BREAKS="cryptopp-dev"
 TERMUX_PKG_REPLACES="cryptopp-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -21,6 +20,8 @@ share/cryptopp/
 "
 
 termux_step_pre_configure() {
-	export CXXFLAGS+=" -fPIC -DCRYPTOPP_DISABLE_ASM"
-	export TERMUX_PKG_EXTRA_MAKE_ARGS+=" dynamic libcryptopp.pc CC=$CC CXX=$CXX"
+	export CXXFLAGS+=" -fPIC -I$TERMUX_PREFIX/include/ndk_compat -fPIC"
+	export TERMUX_PKG_EXTRA_MAKE_ARGS+=" all static dynamic libcryptopp.pc CC=$CC CXX=$CXX"
+	export CFLAGS+=" -I$TERMUX_PREFIX/include/ndk_compat"
+	export LDFLAGS+=" -l:libndk_compat.a"
 }
