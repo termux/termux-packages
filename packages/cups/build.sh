321aba6675ab9fd652575ab9d25f0e0aa2c9ad35
Henrik Grimler <grimler@termux.dev>
2022-08-11 13:48:16 +0200

    fix(main/cups): add gnutls to depends
    To try to fix compilation error.

--- packages/cups/build.sh
+++ packages/cups/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION="2.3.3"
 TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL="https://github.com/apple/cups/releases/download/v${TERMUX_PKG_VERSION}/cups-${TERMUX_PKG_VERSION}-source.tar.gz"
 TERMUX_PKG_SHA256="261fd948bce8647b6d5cb2a1784f0c24cc52b5c4e827b71d726020bcc502f3ee"
-TERMUX_PKG_DEPENDS="libc++, libiconv, libcrypt"
+TERMUX_PKG_DEPENDS="libc++, libiconv, libcrypt, gnutls"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_CONFFILES="
@@ -16,10 +16,6 @@ etc/cups/snmp.conf
 "
 
 TERMUX_PKG_SERVICE_SCRIPT=("cupsd" "mkdir -p $TERMUX_PREFIX/var/run/cups && exec cupsd -f")
-
-termux_step_make() {
-	make LIBS="-pthread $LDFLAGS -Wl,--hash-style=both -lm -lcrypt -liconv -lz" -j${TERMUX_MAKE_PROCESSES}
-}
 
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
