f32d982cd202cd0da6559e27c343e3d885a2848f
Henrik Grimler <grimler@termux.dev>
2022-08-12 12:59:44 +0200

    fix(main/gdal): add json-c to dependencies and remove libtiff
    We configure with --with-libtiff=internal (which was required by jxl),
    so no need to depend on libtiff.
    
    Also sort dependencies and configure args alphabetically.

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -7,17 +7,18 @@ TERMUX_PKG_VERSION=3.4.2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=16baf03dfccf9e3f72bb2e15cd2d5b3f4be0437cdff8a785bceab0c7be557335
-TERMUX_PKG_DEPENDS="libc++, openjpeg, libcurl, libtiff, libpng, proj, libiconv, libsqlite, libgeos, libspatialite, libexpat, openssl, postgresql, netcdf-c, libjxl"
+TERMUX_PKG_DEPENDS="json-c, libc++, libcurl, libexpat, libgeos, libiconv, libjxl, libpng, libspatialite, libsqlite, netcdf-c, openjpeg, openssl, proj, postgresql"
 TERMUX_PKG_BREAKS="gdal-dev"
 TERMUX_PKG_REPLACES="gdal-dev"
 TERMUX_PKG_GROUPS="science"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---with-sqlite3=$TERMUX_PREFIX
---with-spatialite=$TERMUX_PREFIX
 --with-expat=$TERMUX_PREFIX
---with-libtiff=internal
 --with-jxl=$TERMUX_PREFIX
+--with-libjson-c=$TERMUX_PREFIX
+--with-libtiff=internal
+--with-spatialite=$TERMUX_PREFIX
+--with-sqlite3=$TERMUX_PREFIX
 "
 
 termux_step_pre_configure() {
