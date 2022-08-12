863cc62d4cbc1aab404e474664a73f0674fcfdab
2096779623 <admin@utermux.dev>
2022-08-12 14:18:28 +0800

    upgpkg(x11/polybar): update to 3.6.3
    Signed-off-by: 2096779623 <admin@utermux.dev>

--- x11-packages/polybar/build.sh
+++ x11-packages/polybar/build.sh
@@ -2,16 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://polybar.github.io
 TERMUX_PKG_DESCRIPTION="A fast and easy-to-use status bar"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
-TERMUX_PKG_VERSION=3.6.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=3.6.3
 TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/${TERMUX_PKG_VERSION}/polybar-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=a898ba8c847895be41efe5a31bb1b839da2c10b130b8411d5016982fbaadff7b
-TERMUX_PKG_DEPENDS="jsoncpp, pulseaudio, fontconfig, freetype, libandroid-glob, libandroid-shmem, libc++, libcairo, libcurl, libuv, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm"
+TERMUX_PKG_SHA256=f25758573567208fc7b6f4d4115a6117a87389cbcc094cf605d079775be95fa5
+TERMUX_PKG_DEPENDS="jsoncpp, libnl, pulseaudio, fontconfig, freetype, libandroid-glob, libandroid-shmem, libc++, libcairo, libcurl, libuv, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm"
 TERMUX_PKG_BUILD_DEPENDS="i3"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DENABLE_I3=ON"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob -landroid-shmem"
 }
-
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DENABLE_I3=ON"
