062896e0c6f168212c3895a964ffbb104f29d320
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-13 22:18:15 +0800

    psmisc(killall): disable fdsan

--- packages/psmisc/build.sh
+++ packages/psmisc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Some small useful utilities that use the proc filesystem
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=23.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/psmisc-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=dc37ecc2f7e4a90a94956accc6e1c77adb71316b7c9cbd39b26738db0c3ae58b
 TERMUX_PKG_DEPENDS="ncurses"
