b3f92dc96fbddae5145df2fdd8177c7a5250ed91
Henrik Grimler <grimler@termux.dev>
2022-08-14 21:49:18 +0200

    chore(main/litespeedtest): install to TERMUX_PKG_MASSAGEDIR

--- packages/litespeedtest/build.sh
+++ packages/litespeedtest/build.sh
@@ -18,5 +18,5 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin bin/lite
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin bin/lite
 }
