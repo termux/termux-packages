51da4478ec7cc5a7ef4b0e6a5920e5cc15fd6e29
Henrik Grimler <grimler@termux.dev>
2022-08-14 10:14:53 +0200

    libgnustep-base: bump to rebuild with ndk-r25

--- packages/libgnustep-base/build.sh
+++ packages/libgnustep-base/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library of general-purpose, non-graphical Objective C 
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.28.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://ftp.gnustep.org/pub/gnustep/core/gnustep-base-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c7d7c6e64ac5f5d0a4d5c4369170fc24ed503209e91935eb0e2979d1601039ed
 TERMUX_PKG_DEPENDS="gnustep-make, libc++, libffi, libgmp, libgnutls, libiconv, libicu, libxml2, libxslt, zlib"
@@ -41,7 +41,7 @@ termux_step_pre_configure() {
 	local bin="$TERMUX_PKG_BUILDDIR/bin"
 	mkdir -p "$bin"
 	local sh="$(command -v sh)"
-	for cmd in CPP CC CXX; do
+	for cmd in CC CXX; do
 		local wrapper="$bin/$(basename $(eval echo \${$cmd}))"
 		cat > "$wrapper" <<-EOF
 			#!${sh}
