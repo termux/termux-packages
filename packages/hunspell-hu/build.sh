22c7c2aaee10a9252183c28a387e2325d6e44805
Henrik Grimler <grimler@termux.dev>
2022-08-14 21:22:28 +0200

    chore(main/hunspell-hu): install to TERMUX_PKG_MASSAGEDIR

--- packages/hunspell-hu/build.sh
+++ packages/hunspell-hu/build.sh
@@ -13,19 +13,19 @@ termux_step_post_get_source() {
 }
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/share/hunspell/
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/hunspell/
 	# On checksum mismatch the files may have been updated:
 	#  https://cgit.freedesktop.org/libreoffice/dictionaries/log/hu_HU/hu_HU.aff
 	#  https://cgit.freedesktop.org/libreoffice/dictionaries/log/hu_HU/hu_HU.dic
 	# In which case we need to bump version and checksum used.
 	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/hu_HU/hu_HU.aff \
-			$TERMUX_PREFIX/share/hunspell/hu_HU.aff \
+			$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/hunspell/hu_HU.aff \
 			75edc7adb7699af43374aa2ecab7bb739a78388cf9873e8475f680c9cfe1f7c2
 	termux_download https://cgit.freedesktop.org/libreoffice/dictionaries/plain/hu_HU/hu_HU.dic \
-			$TERMUX_PREFIX/share/hunspell/hu_HU.dic \
+			$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/hunspell/hu_HU.dic \
 			361558fe19023da48867493daf741ed72a57f61ff59648c83550422c1770eb8b
-	touch $TERMUX_PREFIX/share/hunspell/hu_HU.{aff,dic}
+	touch $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/hunspell/hu_HU.{aff,dic}
 
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME \
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME \
 		$TERMUX_PKG_SRCDIR/README_hu_HU.txt
 }
