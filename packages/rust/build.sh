13a6bd8d2821620324f9f360cd7950b0b5d8f931
PeroSar <perosar1111@gmail.com>
2022-08-14 10:42:08 +0530

    rust: Update to 1.63.0
    Signed-off-by: PeroSar <perosar1111@gmail.com>

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.62.1
+TERMUX_PKG_VERSION=1.63.0
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
-TERMUX_PKG_SHA256=02066a93c2f6596cc046a897d5716c86e3607c1cd0f54db9a867ae8c8265072e
+TERMUX_PKG_SHA256=8f44af6dc44cc4146634a4dd5e4cc5470b3052a2337019b870c0e025e8987e0c
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib, libllvm"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/llvm-* bin/llc bin/opt"
 
@@ -19,7 +19,7 @@ termux_step_configure() {
 	# like 30 to 40 + minutes ... so lets get it right
 
 	# upstream only tests build ver one version behind $TERMUX_PKG_VERSION
-	local BOOTSTRAP_VERSION=1.62.0
+	local BOOTSTRAP_VERSION=1.62.1
 	rustup install $BOOTSTRAP_VERSION
 	rustup default $BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu
 	export PATH=$HOME/.rustup/toolchains/$BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu/bin:$PATH
