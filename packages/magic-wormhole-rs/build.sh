b61e93a007a414a2542bb9bdfc06f38655a26697
Henrik Grimler <grimler@termux.dev>
2022-08-14 21:54:22 +0200

    chore(main/magic-wormhole-rs): install to TERMUX_PKG_MASSAGEDIR

--- packages/magic-wormhole-rs/build.sh
+++ packages/magic-wormhole-rs/build.sh
@@ -16,5 +16,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/wormhole-rs"
+	install -Dm700 -t "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/wormhole-rs"
 }
