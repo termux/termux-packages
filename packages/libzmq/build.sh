7ec216e2a19a816647656d75e5ca3f2b438f801b
Henrik Grimler <grimler@termux.dev>
2022-08-14 08:55:35 +0200

    fix(main/libzmq): explicitly link against libgcc lib
    Or else we get undefined symbols on at least aarch64.
    Fixes termux/termux-packages#11561.

--- packages/libzmq/build.sh
+++ packages/libzmq/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast messaging system built on sockets. C and C++ bindin
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.3.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/zeromq/libzmq/releases/download/v${TERMUX_PKG_VERSION}/zeromq-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c593001a89f5a85dd2ddf564805deb860e02471171b3f204944857336295c3e5
 TERMUX_PKG_AUTO_UPDATE=true
@@ -12,6 +12,8 @@ TERMUX_PKG_BREAKS="libzmq-dev"
 TERMUX_PKG_REPLACES="libzmq-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libsodium --disable-libunwind --disable-Werror"
 
-termux_step_post_get_source() {
+termux_step_pre_configure() {
 	./autogen.sh
+
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
