13fb68a16b559c6d0a8bf50d8859b6a41568d774
Henrik Grimler <grimler@termux.dev>
2022-08-15 21:24:02 +0200

    chore(main/tere): install to TERMUX_PKG_MASSAGEDIR

--- packages/tere/build.sh
+++ packages/tere/build.sh
@@ -16,5 +16,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/tere
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
+		target/${CARGO_TARGET_NAME}/release/tere
 }
