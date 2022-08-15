bbd74137a77e3066ec5462be81e50cfcfa8cfbcc
Henrik Grimler <grimler@termux.dev>
2022-08-15 21:14:49 +0200

    chore(root/nexttrace-enhanced): install to TERMUX_PKG_MASSAGEDIR

--- root-packages/nexttrace-enhanced/build.sh
+++ root-packages/nexttrace-enhanced/build.sh
@@ -20,5 +20,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin nexttrace
+	install -Dm700 -t "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/bin nexttrace
 }
