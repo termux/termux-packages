39af27ffcef2fc081a5ab41cf21d5dc858480166
Henrik Grimler <grimler@termux.dev>
2022-08-15 20:15:28 +0200

    chore(main/railway-cli): install to TERMUX_PKG_MASSAGEDIR

--- packages/railway-cli/build.sh
+++ packages/railway-cli/build.sh
@@ -17,5 +17,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin bin/railway
+	install -Dm700 -t "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/bin bin/railway
 }
