113bdacb72c628f2ead0cb2c9d4c86937793d3ee
Henrik Grimler <grimler@termux.dev>
2022-08-15 20:09:52 +0200

    chore(main/peco): install to TERMUX_PKG_MASSAGEDIR

--- packages/peco/build.sh
+++ packages/peco/build.sh
@@ -21,5 +21,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/cmd/peco/peco
+	install -Dm700 -t "$TERMUX_PKG_MASSAGEDIR/${TERMUX_PREFIX}"/bin \
+		"$TERMUX_PKG_SRCDIR"/cmd/peco/peco
 }
