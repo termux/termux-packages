c9931162fadff75007556056ecaa473c1f382a65
Henrik Grimler <grimler@termux.dev>
2022-08-15 21:15:58 +0200

    chore(main/speedtest-go): install to TERMUX_PKG_MASSAGEDIR

--- packages/speedtest-go/build.sh
+++ packages/speedtest-go/build.sh
@@ -19,5 +19,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin speedtest-go
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin speedtest-go
 }
