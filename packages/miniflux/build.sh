0bd173e9f0cc61c1df513f977276480449f5d29e
Henrik Grimler <grimler@termux.dev>
2022-08-15 20:11:01 +0200

    chore(main/miniflux): install to TERMUX_PKG_MASSAGEDIR

--- packages/miniflux/build.sh
+++ packages/miniflux/build.sh
@@ -16,6 +16,6 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin miniflux
-	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 miniflux.1
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin miniflux
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/man/man1 miniflux.1
 }
