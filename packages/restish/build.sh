501d0ec8c038e09f840e4eca4c1d59e6f915ccbb
Henrik Grimler <grimler@termux.dev>
2022-08-15 20:56:58 +0200

    chore(main/restish): fix build and simplify build recipe

--- packages/restish/build.sh
+++ packages/restish/build.sh
@@ -4,17 +4,21 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.md"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.14.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/danielgtaylor/restish/archive/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256="2688340ff307aa522325afab40b9eaf6847d0b4b6a7a9c05f041056408b3fc50"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-    termux_setup_golang
-    cd "$TERMUX_PKG_SRCDIR"
-    export GOPATH="${TERMUX_PKG_BUILDDIR}"
-    go build
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+
+	go build
 }
 
 termux_step_make_install() {
-    install -Dm755 -t "${TERMUX_PREFIX}/bin" "${TERMUX_PKG_SRCDIR}/restish"
+	install -Dm755 -t "${TERMUX_PREFIX}/bin" restish
 }
