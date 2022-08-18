2f01819339de4409b08feb68124103d82b661724
Henrik Grimler <grimler@termux.dev>
2022-08-18 08:33:43 +0200

    updpkg(main/command-not-found): update to 2.1.0
    This rebuild also re-generates the command-database.

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.1.0
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=20b7c0f277bb393dbe8fd341692345d224a6fcdc17aae95d290961831e7ae702
+TERMUX_PKG_SHA256=e8fe565d7385a682074e42960a04c4a76d10998fab1638e4b8971a8f8060c026
 
 termux_step_pre_configure() {
 	export TERMUX_PREFIX
