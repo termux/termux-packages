545ae0e3804db412ab703aba2d6930c4480d337a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-18 22:43:21 +0800

    findomain: update to 8.2.0

--- packages/findomain/build.sh
+++ packages/findomain/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://findomain.app/
 TERMUX_PKG_DESCRIPTION="Findomain is the fastest subdomain enumerator and the only one written in Rust"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="8.1.1"
+TERMUX_PKG_VERSION="8.2.0"
 TERMUX_PKG_SRCURL=https://github.com/Findomain/Findomain/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=817497a17bf131afb1ea4fa53eaeeca566949360be168b393260a18ea64c0dbb
+TERMUX_PKG_SHA256=ec795b7f044093eb36a3f74e89ce61a90d1411d509151871e2efcd5a6771888f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
