9181721a7202b81f3a7bebe9171a23e4e5815688
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-19 13:52:00 +0800

    starship: update to 1.10.0

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://starship.rs
 TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.9.1"
+TERMUX_PKG_VERSION="1.10.0"
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2b54bee07bf67504a1bb170d37dc8d6accab4594d35731bbdf0e8a631c8d1585
+TERMUX_PKG_SHA256=28730f76858cddc371666b526a50905d75f7a820ca7a2b974822789f92aed2fe
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -12,23 +12,21 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--all-features"
 
 termux_step_pre_configure() {
 	termux_setup_rust
+	termux_setup_cmake
 	: "${CARGO_HOME:=${HOME}/.cargo}"
 	export CARGO_HOME
 
+	rm -rf "${CARGO_HOME}"/registry/src/github.com-*/git-config-*/
 	cargo fetch --target "${CARGO_TARGET_NAME}"
 
-	for d in "${CARGO_HOME}"/registry/src/github.com-*/libgit2-sys-*/libgit2; do
-		patch --silent -p1 -d "${d}" <"${TERMUX_PKG_BUILDER_DIR}"/getloadavg.diff || {
-			echo "[${FUNCNAME[0]}]: failed to patch libgit2. Exiting now."
+	for d in "${CARGO_HOME}"/registry/src/github.com-*/git-config-*/; do
+		patch --silent -p1 -d "${d}" <"${TERMUX_PKG_BUILDER_DIR}"/rust-git-config-path.diff || {
+			echo "[${FUNCNAME[0]}]: failed to patch git-config. Exiting now."
 			exit 1
 		}
-		cp "${TERMUX_SCRIPTDIR}"/packages/libgit2/getloadavg.c "${d}"/src/util || exit 1
 	done
 
 	CFLAGS+=" ${CPPFLAGS}"
-	if [[ "${TERMUX_ARCH}" == "arm" ]]; then
-		CFLAGS+=" -fno-integrated-as"
-	fi
 
 	mv "${TERMUX_PREFIX}"/lib/libz.so.1{,.tmp}
 	mv "${TERMUX_PREFIX}"/lib/libz.so{,.tmp}
@@ -40,11 +38,21 @@ termux_step_pre_configure() {
 		"${_CARGO_TARGET_LIBDIR}"/libz.so.1
 	ln -sfT "$(readlink -f "${TERMUX_PREFIX}"/lib/libz.so.tmp)" \
 		"${_CARGO_TARGET_LIBDIR}"/libz.so
+
+	RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
 }
 
 termux_step_post_make_install() {
 	mv "${TERMUX_PREFIX}"/lib/libz.so.1{.tmp,}
 	mv "${TERMUX_PREFIX}"/lib/libz.so{.tmp,}
+
+	# Make a placeholder for shell-completions (to be filled with postinst)
+	mkdir -p ${TERMUX_PREFIX}/share/bash-completions/completions
+	mkdir -p ${TERMUX_PREFIX}/share/fish/vendor_completions.d
+	mkdir -p ${TERMUX_PREFIX}/share/zsh/site-functions
+	touch ${TERMUX_PREFIX}/share/bash-completions/completions/starship
+	touch ${TERMUX_PREFIX}/share/fish/vendor_completions.d/starship.fish
+	touch ${TERMUX_PREFIX}/share/zsh/site-functions/_starship
 }
 
 termux_step_post_massage() {
@@ -55,19 +63,9 @@ termux_step_post_massage() {
 termux_step_create_debscripts() {
 	cat <<-EOF >./postinst
 		#!${TERMUX_PREFIX}/bin/sh
-		mkdir -p ${TERMUX_PREFIX}/share/bash-completions/completions
-		mkdir -p ${TERMUX_PREFIX}/share/fish/vendor_completions.d
-		mkdir -p ${TERMUX_PREFIX}/share/zsh/site-functions
 
 		starship completions bash > ${TERMUX_PREFIX}/share/bash-completions/completions/starship
 		starship completions fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/starship.fish
 		starship completions zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_starship
-	EOF
-
-	cat <<-EOF >./prerm
-		#!${TERMUX_PREFIX}/bin/sh
-		rm -f ${TERMUX_PREFIX}/share/bash-completions/completions/starship
-		rm -f ${TERMUX_PREFIX}/share/fish/vendor_completions.d/starship.fish
-		rm -f ${TERMUX_PREFIX}/share/zsh/site-functions/_starship
 	EOF
 }
