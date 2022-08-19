ec6c176432d99e7348e112e9fd02063f83908b70
Jia Yuan Lo <jylo06g@gmail.com>
2022-08-19 21:10:32 +0800

    chore(main/libpixman): bump to rebuild with NDK r25

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Low-level library for pixel manipulation"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.40.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc
 TERMUX_PKG_BREAKS="libpixman-dev"
@@ -11,7 +11,8 @@ TERMUX_PKG_REPLACES="libpixman-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
 
 termux_step_pre_configure() {
-	if [ $TERMUX_ARCH = arm ]; then
+	if [ "$TERMUX_ARCH" = arm ]; then
+		termux_step_setup_toolchain_with_gas
 		CFLAGS+=" -fno-integrated-as"
 	fi
 }
