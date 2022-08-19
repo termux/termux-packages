a2434a831105f570be3badecbe21c6bacd7685de
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-19 13:23:01 +0800

    libmdbx: update to 0.11.9

--- packages/libmdbx/build.sh
+++ packages/libmdbx/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://erthink.github.io/libmdbx/
+TERMUX_PKG_HOMEPAGE=https://libmdbx.dqdkfa.ru/
 TERMUX_PKG_DESCRIPTION="An extremely fast, compact, powerful, embedded, transactional key-value database"
 TERMUX_PKG_LICENSE="OpenLDAP"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.11.4
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/abs0/libmdbx.git
+TERMUX_PKG_VERSION=0.11.9
+TERMUX_PKG_SRCURL=https://gitflic.ru/project/erthink/libmdbx.git
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_SHARED_LIBS=ON
