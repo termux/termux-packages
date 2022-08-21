d9891d9e0341fa56b741d4cdc2eb399509e0190b
2096779623 <admin@utermux.dev>
2022-08-21 18:03:50 +0800

    feat(main/libx11): add x11 license
    Signed-off-by: 2096779623 <admin@utermux.dev>
    
    [skip ci]

--- packages/libx11/build.sh
+++ packages/libx11/build.sh
@@ -1,10 +1,10 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 client-side library"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="MIT, X11"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.7.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1cfa35e37aaabbe4792e9bb690468efefbfbf6b147d9c69d6f90d13c3092ea6c
 TERMUX_PKG_DEPENDS="libandroid-shmem, libandroid-support, libxcb"
