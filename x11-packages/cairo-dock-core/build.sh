edcd2772c6076086e86d6346d180f50ee0956875
Henrik Grimler <grimler@termux.dev>
2022-08-21 15:56:32 +0200

    fix(x11/cairo-dock-core): set CMAKE_INSTALL_LIBDIR to relative path
    Cairo-dock-core's main CMakeLists.txt assumes that CMAKE_INTALL_LIBDIR
    is a relative path, even though cmake's docs state that both relative
    and absolute paths should be okay.  If we specify a relative path, as
    we do since commit 571db28a3dc9 ("termux_step_configure_cmake: Revert
    CMAKE_INSTALL_LIBDIR old behaviour changed in 2af00064"), then the
    files are installed to $PREFIX/$PREFIX/lib instead of $PREFIX/lib.
    
    Fixes: termux/termux-packages#11668

--- x11-packages/cairo-dock-core/build.sh
+++ x11-packages/cairo-dock-core/build.sh
@@ -4,11 +4,14 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
 TERMUX_PKG_VERSION=3.4.1
 _COMMIT=6c569e67a2a366e7634224a0133ede51755629cb
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/Cairo-Dock/cairo-dock-core/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=e59e99147ce9c901b46d4b56b88bd53aeda34292b86fd9fbf2c55d158153f2ec
 TERMUX_PKG_DEPENDS="atk, dbus-glib, glib, gtk3, pango, libcairo, libx11, xsltproc, libcurl, gdk-pixbuf, harfbuzz, mesa, glu, librsvg, libxcomposite, libwayland, libwayland-protocols, libxinerama, libxrandr, libxrender, libxtst, libxml2"
 TERMUX_PKG_BUILD_DEPENDS="valac"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DFORCE_NOT_LIB64=yes"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DFORCE_NOT_LIB64=yes
+-DCMAKE_INSTALL_LIBDIR=lib
+"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
 
