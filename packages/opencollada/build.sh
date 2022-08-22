8067afc8d2d2e2d1e7a381efd203dca9dca3397f
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-08-22 17:57:54 +0530

    add(main): new package opencollada

---
+++ packages/opencollada/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/OpenCOLLADA/
+TERMUX_PKG_DESCRIPTION="COLLADAMax and COLLADAMaya are new implementation of a 3ds Max or Maya plug-ins"
+TERMUX_PKG_LICENSE="GPL-3.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.6.68
+TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/OpenCOLLADA/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d9db0c0a518aa6ac0359626f222707c6ca1b63a83cbf229d97a5999c9cde347b
+TERMUX_PKG_DEPENDS="libxml2, pcre"
