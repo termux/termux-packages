68cb4b488ca8e334d854299f373c8f365142043c
Henrik Grimler <grimler@termux.dev>
2022-08-24 09:32:44 +0200

    chore(main/x11-repo): use grimler.se mirror per default
    Fosshost is decommissioning its service on August 28th, so lets use
    the grimler.se until we have managed to set up a new repo, somewhere
    else.

--- packages/x11-repo/build.sh
+++ packages/x11-repo/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/x11-packages
 TERMUX_PKG_DESCRIPTION="Package repository containing X11 programs and libraries"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=8.1
+TERMUX_PKG_VERSION=8.2
 TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
-	echo "deb https://packages.termux.dev/apt/termux-x11 x11 main" > $TERMUX_PREFIX/etc/apt/sources.list.d/x11.list
+	echo "deb https://grimler.se/termux/termux-x11 x11 main" > $TERMUX_PREFIX/etc/apt/sources.list.d/x11.list
 }
 
 termux_step_create_debscripts() {
