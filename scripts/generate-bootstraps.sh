ce120bbc77317145a73ee464cf7d4a30b8b3c49c
Henrik Grimler <grimler@termux.dev>
2022-08-24 09:34:56 +0200

    scripts: use grimler.se mirror when generating bootstraps for now
    Fosshost is decommissioning its service on August 28th, so lets use
    the grimler.se until we have managed to set up a new repo, somewhere
    else.

--- scripts/generate-bootstraps.sh
+++ scripts/generate-bootstraps.sh
@@ -23,7 +23,7 @@ TERMUX_PACKAGE_MANAGERS=("apt" "pacman")
 
 # The repository base urls mapping for package managers.
 declare -A REPO_BASE_URLS=(
-	["apt"]="https://packages-cf.termux.dev/apt/termux-main"
+	["apt"]="https://grimler.se/termux/termux-main"
 	["pacman"]="https://s3.amazonaws.com/termux-main.pacman"
 )
 
