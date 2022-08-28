7f2d99c489464d78d6711426deb51cb76908c062
Lucinda May Phipps <landfillbaby69@gmail.com>
2022-08-28 14:00:50 +0100

    git: update to 2.37.2

--- packages/git/build.sh
+++ packages/git/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://git-scm.com/
 TERMUX_PKG_DESCRIPTION="Fast, scalable, distributed revision control system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.37.1
+TERMUX_PKG_VERSION=2.37.2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/pub/software/scm/git/git-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=c8162c6b8b8f1c5db706ab01b4ee29e31061182135dc27c4860224aaec1b3500
+TERMUX_PKG_SHA256=1c3d9c821c4538e7a6dac30a4af8bd8dcfe4f651f95474c526b52f83406db003
 TERMUX_PKG_DEPENDS="libcurl, libiconv, less, openssl, pcre2, zlib"
 
 ## This requires a working $TERMUX_PREFIX/bin/sh on the host building:
