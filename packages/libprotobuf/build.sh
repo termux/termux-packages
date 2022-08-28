de09925717a41bcab27e6e5be270b4fc63f296de
Arun <arun007@pm.me>
2022-08-28 13:27:03 +0530

    upgpkg(main/libprotobuf): update to 3.21.5

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -1,13 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/protocolbuffers/protobuf
 TERMUX_PKG_DESCRIPTION="Protocol buffers C++ library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-# TERMUX_PKG_SRCDIR is overriden below so we need to specify license file
-TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2:3.19.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2:3.21.5
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=390191a0d7884b3e52bb812c440ad1497b9d484241f37bb8e2ccc8c2b72d6c36
+TERMUX_PKG_SHA256=d7d204a59fd0d2d2387bd362c2155289d5060f32122c4d1d922041b61191d522
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev"
@@ -18,7 +15,3 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_SHARED_LIBS=ON
 -DCMAKE_INSTALL_LIBDIR=lib
 "
-
-termux_step_pre_configure() {
-	TERMUX_PKG_SRCDIR+="/cmake/"
-}
