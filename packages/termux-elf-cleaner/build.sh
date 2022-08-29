803ae15df9a320841e032c45df1e5e778fcbbaae
Henrik Grimler <grimler@termux.dev>
2022-08-29 11:22:37 +0200

    fix(main/termux-elf-cleaner): set default api-level to $TERMUX_PKG_API_LEVEL
    So that users do not have to specify --api-level 24 or similar when
    using the tool on-device.
    
    Reported in termux/termux-packages#11612.

--- packages/termux-elf-cleaner/build.sh
+++ packages/termux-elf-cleaner/build.sh
@@ -5,11 +5,14 @@ TERMUX_PKG_MAINTAINER="@termux"
 # Please update checksum in termux_step_start_build.sh as well if
 # updating the package.
 TERMUX_PKG_VERSION=2.1.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-elf-cleaner/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=104231f91ef6662f80694fc4a59c6bfeae50da21c4fc22adac3c9a5aac00ba98
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
 	autoreconf -vfi
+
+	sed "s%@TERMUX_PKG_API_LEVEL@%$TERMUX_PKG_API_LEVEL%g" \
+		"$TERMUX_PKG_BUILDER_DIR"/android-api-level.diff | patch --silent -p1
 }
