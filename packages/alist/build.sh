6a9379c53452b2a7e1eb94e56bb85de6f747a0cd
Ladxn <93758443+Ladxn@users.noreply.github.com>
2022-08-29 19:18:55 +0800

    fix(main/alist): update web version to 2.6.4

--- packages/alist/build.sh
+++ packages/alist/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
 TERMUX_PKG_VERSION="2.6.4"
 TERMUX_PKG_SRCURL=https://github.com/alist-org/alist/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3860f5cec5d809159c55498874c472386cd6142a932a5e7bbfb3b0f569e3e12b
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -17,17 +18,17 @@ termux_step_make() {
 	termux_setup_golang
 
 	# Get alist-web:
-	wget https://github.com/alist-org/web-v2/releases/download/2.6.0/dist.tar.gz
-	tar -zxvf dist.tar.gz
-	rm -f dist.tar.gz
-	sed -ri 's|(lang=")zh-CN"|\1en-US"|g' dist/index.html
-	mv dist/* public/
-
 	local ldflags webTag
 	webTag=$(
 		wget -qO- -t1 -T2 "https://api.github.com/repos/alist-org/web-v2/releases/latest" \
 			| grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'
 	)
+	wget https://github.com/alist-org/web-v2/releases/download/${webTag}/dist.tar.gz
+	tar -zxvf dist.tar.gz
+	rm -f dist.tar.gz
+	sed -ri 's|(lang=")zh-CN"|\1en-US"|g' dist/index.html
+	mv dist/* public/
+
 	ldflags="-w -s \
 		-X 'github.com/Xhofe/alist/conf.BuiltAt=$(date +'%F %T %z')' \
 		-X 'github.com/Xhofe/alist/conf.GoVersion=$(go version | sed 's/go version //')' \
