97c7fb0ea7bf7de09656e90d86620a42f385ced5
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-08-29 13:51:52 +0800

    libfann: switch to newest-tag
    Closes #11751
    [skip ci]

--- packages/libfann/build.sh
+++ packages/libfann/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL="https://github.com/libfann/fann/archive/refs/tags/$TERMUX_PKG
 TERMUX_PKG_SHA256=f31c92c1589996f97d855939b37293478ac03d24b4e1c08ff21e0bd093449c3c
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag" # As of 2022-08-29T00:33:40 no github releases are available.
