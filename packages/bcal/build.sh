e1f2e7c0020c5ea55a95f1f44814dcfef8685d60
IntinteDAO <lordfervi@protonmail.com>
2022-09-02 11:26:35 +0200

    Update build.sh

--- packages/bcal/build.sh
+++ packages/bcal/build.sh
@@ -5,9 +5,8 @@ TERMUX_PKG_MAINTAINER="@fervi"
 TERMUX_PKG_VERSION="2.4"
 TERMUX_PKG_SRCURL=https://github.com/jarun/bcal/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=141f39d866f62274b2262164baaac6202f60749862c84c2e6ed231f6d03ee8df
-TERMUX_PKG_DEPENDS="bc, readline"
-TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
+TERMUX_PKG_DEPENDS="readline, bc"
+TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
-	make install
-}
+# 64-bit archs only, check https://github.com/jarun/bcal/issues/4
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
