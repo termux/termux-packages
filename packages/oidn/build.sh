b885dcb2c2b050f9140d69f58a593e3fef656728
T-Dynamos <anshdadwal298@gmail.com>
2022-09-02 13:53:19 +0000

    add(main): new package oidn

---
+++ packages/oidn/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.openimagedenoise.org
+TERMUX_PKG_DESCRIPTION="Intel® Open Image Denoise library"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.4.3
+TERMUX_PKG_SRCURL=https://github.com/OpenImageDenoise/oidn/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz 
+TERMUX_PKG_SHA256=37941f260af212183579efb6b1e07e4075e3ab9cbd63120afd007e62367efef6
+TERMUX_PKG_DEPENDS="python, libtbb"
