97e9a9baf4ddf65f3e7500a7425670a65fe46112
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2022-09-03 19:34:46 -0400

    add dependency to libandroid-glob

--- packages/clifm/build.sh
+++ packages/clifm/build.sh
@@ -6,6 +6,10 @@ TERMUX_PKG_VERSION=1.7
 TERMUX_PKG_SRCURL=https://github.com/leo-arch/clifm/releases/download/v${TERMUX_PKG_VERSION}/clifm-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6408fe75c80646f33cd1ffab5b7e81ca098e70bd01cbd7b20d3ecbe870dc47c9
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libcap, libacl, readline, file"
+TERMUX_PKG_DEPENDS="libcap, libacl, readline, file, libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="-f misc/termux/Makefile"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-glob"
+}
