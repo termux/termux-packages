55c05bf2df1af016c870af55f93b18cdd5790444
Ghassan Alduraibi <git@ghassan.dev>
2022-09-07 21:04:40 +0300

    libtreesitter: set TERMUX_PKG_REVISION=1

--- packages/libtreesitter/build.sh
+++ packages/libtreesitter/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An incremental parsing system for programming tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.20.7"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b355e968ec2d0241bbd96748e00a9038f83968f85d822ecb9940cbe4c42e182e
 TERMUX_PKG_AUTO_UPDATE=true
