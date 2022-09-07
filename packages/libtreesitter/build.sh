39d04972776a3792208fecf959f2fd250614188b
Ghassan Alduraibi <git@ghassan.dev>
2022-09-07 14:45:46 +0300

    libtreesitter: install its cli tool

--- packages/libtreesitter/build.sh
+++ packages/libtreesitter/build.sh
@@ -7,3 +7,13 @@ TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v
 TERMUX_PKG_SHA256=b355e968ec2d0241bbd96748e00a9038f83968f85d822ecb9940cbe4c42e182e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_rust
+
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/tree-sitter
+}
