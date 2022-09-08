b72be3cba923815b6a67f8632422d58b10344da8
Leonid Pliushch <sylirre@termux.dev>
2022-09-08 12:31:30 +0300

    libsixel: depend on libgd and gdk-pixbuf
    Fixes issue with some GIFs.
    
    See my comment at https://github.com/termux/termux-app/pull/2973#issuecomment-1240443633
    
    Also enable libcurl support.

--- packages/libsixel/build.sh
+++ packages/libsixel/build.sh
@@ -3,11 +3,18 @@ TERMUX_PKG_DESCRIPTION="Encoder/decoder implementation for DEC SIXEL graphics"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.10.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libsixel/libsixel/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=028552eb8f2a37c6effda88ee5e8f6d87b5d9601182ddec784a9728865f821e0
-TERMUX_PKG_DEPENDS="libcurl, libjpeg-turbo, libpng"
+TERMUX_PKG_DEPENDS="libcurl, libjpeg-turbo, libpng, libgd, gdk-pixbuf"
 TERMUX_PKG_BREAKS="libsixel-dev"
 TERMUX_PKG_REPLACES="libsixel-dev"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgdk-pixbuf2=enabled
+-Dgd=enabled
+-Dlibcurl=enabled
+"
 
 termux_step_pre_configure() {
 	export PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
