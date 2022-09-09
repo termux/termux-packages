3602e60922eeed01035b2bb59b47c94b407e441e
Leonid Pliushch <sylirre@termux.dev>
2022-09-09 09:37:07 +0300

    mpv: build with sixel support

--- packages/mpv/build.sh
+++ packages/mpv/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Command-line media player"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.34.1
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_REVISION=8
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-support, libarchive, libcaca, libiconv, liblua52, pulseaudio, openal-soft, zlib"
+TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-support, libarchive, libcaca, libiconv, liblua52, libsixel, pulseaudio, openal-soft, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="share/icons share/applications"
 
 termux_step_pre_configure() {
@@ -31,7 +31,8 @@ termux_step_make_install() {
 		--enable-openal \
 		--enable-caca \
 		--disable-alsa \
-		--disable-x11
+		--disable-x11 \
+		--enable-sixel
 
 	./waf -v install
 
