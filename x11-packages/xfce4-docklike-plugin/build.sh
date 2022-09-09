65661bfca20cf6add898ea2b1d5c5947f9d141c0
T-Dynamos <anshdadwal298@gmail.com>
2022-09-09 11:42:23 +0000

    add(x11): new package xfce4-docklike-plugin

---
+++ x11-packages/xfce4-docklike-plugin/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-docklike-plugin/start
+TERMUX_PKG_DESCRIPTION="A modern, docklike, minimalist taskbar for XFCE"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_SRCURL=https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin/-/archive/xfce4-docklike-plugin-${TERMUX_PKG_VERSION}/xfce4-docklike-plugin-xfce4-docklike-plugin-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=6270522d07388b8c8a5e64e73592f714106a4e14927b8c98db3f1dad6d73e5e1
+TERMUX_PKG_DEPENDS="xfce4-panel, libxfce4ui, libwnck, gtk3, glib, exo, ndk-sysroot"
+TERMUX_PKG_BUILD_DEPENDS="intltool, xfce4-dev-tools"
+
+termux_step_pre_configure () {
+	NOCONFIGURE=1 ./autogen.sh
+}
