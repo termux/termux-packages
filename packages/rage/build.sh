0e696a9b6630883ed0909ed8e37de7c501e00c6a
2096779623 <admin@utermux.dev>
2022-09-11 20:53:25 +0800

    add(main): new package: rage
    Signed-off-by: 2096779623 <admin@utermux.dev>

---
+++ packages/rage/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://age-encryption.org/v1
+TERMUX_PKG_DESCRIPTION="A simple, secure and modern encryption tool"
+TERMUX_PKG_LICENSE="Apache-2.0, MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE-APACHE, LICENSE-MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.8.1
+TERMUX_PKG_SRCURL=https://github.com/str4d/rage/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f9f39c987d398163bb2e843e02b0008d86842cb30d70b4c1e3470d9a4b9de87d
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	termux_setup_rust
+
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rage
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rage-keygen
+}
