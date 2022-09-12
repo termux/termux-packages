dbcc65e83087fd6e1d84ccf61766e8e360dbfcd3
port19 <port19@port19.xyz>
2022-09-12 08:48:57 +0200

    ani-cli: update to 3.4

--- packages/ani-cli/build.sh
+++ packages/ani-cli/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pystardust/ani-cli
 TERMUX_PKG_DESCRIPTION="A cli to browse and watch anime"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.4
 TERMUX_PKG_SRCURL=https://github.com/pystardust/ani-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=973d335a75bd7f920c244000ad6b057f702fb37752e7bea1b5bcf038785ec925
-TERMUX_PKG_DEPENDS="aria2, curl, ffmpeg, gawk, grep, openssl-tool, sed"
+TERMUX_PKG_SHA256=45cfd2e43b8b83ae662fc3c8724b250281aca681828101e3bd5eb287a3794167
+TERMUX_PKG_DEPENDS="axel, curl, ffmpeg, gawk, grep, openssl-tool, sed"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
