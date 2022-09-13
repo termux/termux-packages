76cb1a89df18a11ebbcee94e4a78dc71aa0a237f
Ivan Maidanski <ivmai@mail.ru>
2022-09-13 09:58:18 +0300

    libgc: Update to 8.2.2
    * Fix license (the package is covered by MIT-style license, not GPL)
    * Install README.QUICK as a license file
    * Drop include-private-gcconfig.h.patch (should not be needed any more)
    * Remove dependency on libatomic_ops (libgc uses gcc built-in atomics)

--- packages/libgc/build.sh
+++ packages/libgc/build.sh
@@ -1,19 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://www.hboehm.info/gc/
 TERMUX_PKG_DESCRIPTION="Library providing the Boehm-Demers-Weiser conservative garbage collector"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="README.QUICK"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(8.0.6
-                    7.6.12)
-TERMUX_PKG_SRCURL=(https://github.com/ivmai/bdwgc/releases/download/v$TERMUX_PKG_VERSION/gc-$TERMUX_PKG_VERSION.tar.gz
-                   https://github.com/ivmai/libatomic_ops/releases/download/v${TERMUX_PKG_VERSION[1]}/libatomic_ops-${TERMUX_PKG_VERSION[1]}.tar.gz)
-TERMUX_PKG_SHA256=(3b4914abc9fa76593596773e4da671d7ed4d5390e3d46fbf2e5f155e121bea11
-                   f0ab566e25fce08b560e1feab6a3db01db4a38e5bc687804334ef3920c549f3e)
+TERMUX_PKG_VERSION=8.2.2
+TERMUX_PKG_SRCURL=https://github.com/ivmai/bdwgc/releases/download/v$TERMUX_PKG_VERSION/gc-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=f30107bcb062e0920a790ffffa56d9512348546859364c23a14be264b38836a0
 TERMUX_PKG_BREAKS="libgc-dev"
 TERMUX_PKG_REPLACES="libgc-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libatomic-ops=none"
 TERMUX_PKG_RM_AFTER_INSTALL="share/gc"
 
-
 termux_step_post_get_source() {
-	mv libatomic_ops-${TERMUX_PKG_VERSION[1]} libatomic_ops
 	./autogen.sh
 }
