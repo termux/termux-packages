623a695e6379a8135743eea71ba053b019bc3f31
T-Dynamos <anshdadwal298@gmail.com>
2022-09-14 06:49:02 +0530

    add(main): new package ttyc

---
+++ packages/ttyc/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Depau/ttyc
+TERMUX_PKG_DESCRIPTION="ttyd protocol client"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.3"
+TERMUX_PKG_SRCURL=https://github.com/Depau/ttyc/archive/refs/tags/wistty-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c2240bff19219e5770dbe6e9ed1e5b96916bee4a5b2c5e7b636c5495724c2881
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy 
+}
+
+termux_step_make() {
+	go build -o ttyc
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin ttyc
+}
