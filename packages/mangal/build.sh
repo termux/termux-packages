d4be107a66eb7d6e9761dcf19e63e276d91b7b6b
T-Dynamos <anshdadwal298@gmail.com>
2022-09-15 15:47:22 +0530

    add(main): new package mangal

---
+++ packages/mangal/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/metafates/mangal
+TERMUX_PKG_DESCRIPTION="Cli manga downloader"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="3.9.0"
+TERMUX_PKG_SRCURL=https://github.com/metafates/mangal.git
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy 
+}
+
+termux_step_make() {
+	go build -o mangal
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin mangal
+}
