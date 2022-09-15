f4d3754cdf8490a9ff4d2675e427d04f7fea20ba
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-16 03:08:48 +0900

    graphviz: Bump to 5.0.1
    Use Autotools, partially reverting 290ae05f350c9c04d57cbf4a5cf24a41bb38d591.

--- packages/graphviz/build.sh
+++ packages/graphviz/build.sh
@@ -2,23 +2,44 @@ TERMUX_PKG_HOMEPAGE=https://www.graphviz.org/
 TERMUX_PKG_DESCRIPTION="Rich set of graph drawing tools"
 TERMUX_PKG_LICENSE="EPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.0.0
-#TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.0.1
 TERMUX_PKG_SRCURL=https://gitlab.com/graphviz/graphviz/-/archive/$TERMUX_PKG_VERSION/graphviz-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f8cc576694ae93659f453b6b4a4c2f04572a90cfccdca2d4271f2e7c60bb3d60
+TERMUX_PKG_SHA256=6c704a3ffeaf2f7ae63f7d0da453cd624d93a9306a2e5dbda07e5ec38343d2b8
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, libcairo, pango, libexpat, libltdl, librsvg, libgd, zlib"
 TERMUX_PKG_BREAKS="graphviz-dev"
 TERMUX_PKG_REPLACES="graphviz-dev"
 TERMUX_PKG_BUILD_DEPENDS="libtool"
-TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-guile=no
+--enable-java=no
+--enable-lua=no
+--enable-ocaml=no
+--enable-perl=no
+--enable-php=no
+--enable-python=no
+--enable-r=no
+--enable-ruby=no
+--enable-sharp=no
+--enable-swig=no
+--enable-tcl=no
+--with-expatlibdir=$TERMUX_PREFIX/lib
+--with-ltdl-include=$TERMUX_PREFIX/include
+--with-ltdl-lib=$TERMUX_PREFIX/lib
+--with-pangocairo=yes
+--with-pic
+--with-poppler=no
+--with-x=no
+"
+TERMUX_PKG_FORCE_CMAKE=false
 TERMUX_PKG_RM_AFTER_INSTALL="bin/*-config share/man/man1/*-config.1"
 
-termux_step_post_make_install() {
-	# Some binaries (dot_builtins, gvpack) links against these:
-	cd $TERMUX_PREFIX/lib
-	for lib in graphviz/*.so*; do
-		ln -s -f $lib $(basename $lib)
-	done
+termux_step_pre_configure() {
+	./autogen.sh NOCONFIG
+	export HOSTCC="gcc"
+
+	LDFLAGS+=" -lm -landroid-glob"
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	LDFLAGS+=" -Wl,-rpath=$TERMUX_PREFIX/lib/graphviz"
 }
 
 termux_step_create_debscripts() {
