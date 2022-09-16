f4a2f71d86e337222e72741067b6111ef685a2c8
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2022-09-16 14:26:49 -0400

    remove post-install; wrong shell files

--- packages/atuin/build.sh
+++ packages/atuin/build.sh
@@ -7,9 +7,3 @@ TERMUX_PKG_SRCURL=https://github.com/ellie/atuin/archive/v${TERMUX_PKG_VERSION}.
 TERMUX_PKG_SHA256=29689906e3fd6bc680c60c3b2f41f756da5bd677a4f4aea3d26eff87f5bebac4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_post_make_install() {
-	install -Dm600 src/shell/atuin.bash $TERMUX_PREFIX/share/bash-completion/completions/atuin.bash
-	install -Dm600 src/shell/atuin.zsh $TERMUX_PREFIX/share/zsh/site-functions/_atuin
-	install -Dm600 src/shell/atuin.fish $TERMUX_PREFIX/share/fish/vendor_completions.d/atuin.fish
-}
