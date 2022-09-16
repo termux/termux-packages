9ceb2a58556a5cef8b3c558830942020e4521115
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2022-09-16 14:36:18 -0400

    fix linker bug on x86_64, see #8029

--- packages/atuin/build.sh
+++ packages/atuin/build.sh
@@ -5,5 +5,11 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="11.0.0"
 TERMUX_PKG_SRCURL=https://github.com/ellie/atuin/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=29689906e3fd6bc680c60c3b2f41f756da5bd677a4f4aea3d26eff87f5bebac4
+TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	# required to build for x86_64, see #8029
+	export RUSTFLAGS="${RUSTFLAGS:-} -C link-args=$($CC -print-libgcc-file-name)"
+}
