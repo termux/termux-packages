db0b07fd51a4e36c1763c375d22bac3f8d3ab9cc
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2022-09-16 13:55:18 -0400

    simplify build process
    The build process already handles cargo installs automatically.

--- packages/atuin/build.sh
+++ packages/atuin/build.sh
@@ -8,23 +8,8 @@ TERMUX_PKG_SHA256=29689906e3fd6bc680c60c3b2f41f756da5bd677a4f4aea3d26eff87f5beba
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
-  	termux_setup_rust
-}
-termux_step_make() {
-	  cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release --all-features
-
-    # Generate shell completions
-    mkdir completions
-    for sh in 'bash' 'fish' 'zsh'; do
-        target/$CARGO_TARGET_NAME/release/atuin gen-completions -s $sh -o completions/
-    done
-}
-
 termux_step_post_make_install() {
-    install -Dm755 -t $TERMUX_PREFIX/bin target/$CARGO_TARGET_NAME/release/atuin
-
-    install -Dm600 completions/atuin.bash $TERMUX_PREFIX/share/bash-completion/completions/atuin.bash
-    install -Dm600 completions/_atuin $TERMUX_PREFIX/share/zsh/site-functions/_atuin
-    install -Dm600 completions/atuin.fish $TERMUX_PREFIX/share/fish/vendor_completions.d/atuin.fish
+	install -Dm600 src/shell/atuin.bash $TERMUX_PREFIX/share/bash-completion/completions/atuin.bash
+	install -Dm600 src/shell/atuin.zsh $TERMUX_PREFIX/share/zsh/site-functions/_atuin
+	install -Dm600 src/shell/atuin.fish $TERMUX_PREFIX/share/fish/vendor_completions.d/atuin.fish
 }
