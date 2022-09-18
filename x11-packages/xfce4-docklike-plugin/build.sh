71a55153cd1e1818971d71626292f556aad03d2e
Kevin Williams <admin@utermux.dev>
2022-09-18 17:03:47 +0800

    xfce4-docklike-plugin: fix build
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/xfce4-docklike-plugin/build.sh
+++ x11-packages/xfce4-docklike-plugin/build.sh
@@ -7,7 +7,10 @@ TERMUX_PKG_SRCURL=https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin/-/
 TERMUX_PKG_SHA256=6270522d07388b8c8a5e64e73592f714106a4e14927b8c98db3f1dad6d73e5e1
 TERMUX_PKG_DEPENDS="xfce4-panel, libxfce4ui, libwnck, gtk3, glib, exo, ndk-sysroot"
 TERMUX_PKG_BUILD_DEPENDS="intltool, xfce4-dev-tools"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
 	NOCONFIGURE=1 ./autogen.sh
+	
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
