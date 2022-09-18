d8c3a6703f933a2b7518265fa00b17f09ecb3d46
aicynide <97217381+aicynide@users.noreply.github.com>
2022-09-18 18:52:47 +0530

    ttyc: update to v0.4

--- packages/ttyc/build.sh
+++ packages/ttyc/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION="0.4"
 TERMUX_PKG_SRCURL=https://github.com/Depau/ttyc/archive/refs/tags/ttyc-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=375e2b27335ed3db13aee6d4525548148b8579cdbe34ed4d971d4e3cdff0f173
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_REVISION=1
 
 termux_step_pre_configure() {
 	termux_setup_golang
