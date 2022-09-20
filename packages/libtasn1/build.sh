72b1576d94a428450d6941675cf9ac457b1d8d03
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-21 03:38:56 +0900

    libtasn1: Bump to 4.19.0

--- packages/libtasn1/build.sh
+++ packages/libtasn1/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libtasn1/
 TERMUX_PKG_DESCRIPTION="This is GNU Libtasn1, a small ASN.1 library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.17.0
-TERMUX_PKG_SRCURL=https://gitlab.com/gnutls/libtasn1.git
+TERMUX_PKG_VERSION=4.19.0
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libtasn1/libtasn1-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1613f0ac1cf484d6ec0ce3b8c06d56263cc7242f1c23b30d82d23de345a63f7a
 
-termux_step_post_get_source() {
-	./bootstrap
+termux_step_pre_configure() {
+	autoreconf -fi
 }
