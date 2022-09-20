c65d8c844b5d741f8b12359680409d2f3dbe2b33
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-20 09:35:36 +0900

    lychee: Fix hardcoded `/etc/resolv.conf`

--- packages/lychee/build.sh
+++ packages/lychee/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE-MIT, LICENSE-APACHE"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
 TERMUX_PKG_VERSION="0.10.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/lycheeverse/lychee/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1ebc71f8741d644cdc1fd3f5c2f78f0991bc56e038c1132cbe8c9eee144c8f03
 TERMUX_PKG_AUTO_UPDATE=true
@@ -12,6 +13,17 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	for d in $CARGO_HOME/registry/src/github.com-*/trust-dns-resolver-*; do
+		sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" \
+			$TERMUX_PKG_BUILDER_DIR/trust-dns-resolver.diff \
+			| patch --silent -p1 -d ${d} || :
+	done
 }
 
 termux_step_make() {
