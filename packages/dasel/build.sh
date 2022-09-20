57b24f899ca4d667d510b88c75e2ae504d175a48
anon <96630237+flosnvjx@users.noreply.github.com>
2022-09-20 08:38:16 +0800

    add(main): new package dasel

---
+++ packages/dasel/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/TomWright/dasel
+TERMUX_PKG_DESCRIPTION="Select, put and delete data from JSON, TOML, YAML, XML and CSV files with a single utility"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.26.1
+TERMUX_PKG_SRCURL=https://github.com/TomWright/dasel/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=44c90753cf4c1b6e7fb82074c6701fd4b47dc6dc26fe4e5504dcccb4d273b946
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	termux_setup_golang
+	mkdir bin
+	go build -o ./bin -trimpath ./cmd/dasel
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin bin/*
+	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME README.*
+}
