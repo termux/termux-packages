ad08f24e7f41b19ec0254ff9b4866faeb92fbe2c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-21 03:23:56 +0900

    libtirpc: Bump to 1.3.3

--- packages/libtirpc/build.sh
+++ packages/libtirpc/build.sh
@@ -2,11 +2,16 @@ TERMUX_PKG_HOMEPAGE="http://git.linux-nfs.org/?p=steved/libtirpc.git"
 TERMUX_PKG_DESCRIPTION="Transport Independent RPC library"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.3.3
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/libtirpc/libtirpc-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=e24eb88b8ce7db3b7ca6eb80115dd1284abc5ec32a8deccfed2224fc2532b9fd
+TERMUX_PKG_SHA256=6474e98851d9f6f33871957ddee9714fdcd9d8a5ee9abb5a98d63ea2e60e12f3
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gssapi"
+
+termux_step_post_get_source() {
+	sed -e "s|@RELEASE_VERSION@|${TERMUX_PKG_VERSION##*:}|" \
+		$TERMUX_PKG_BUILDER_DIR/release-version.diff \
+		| patch --silent -p1
+}
 
 termux_step_pre_configure() {
 	aclocal
