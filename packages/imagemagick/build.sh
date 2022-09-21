6099c44d98b349571e956bf7ef9993d222f002ba
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-21 13:51:13 +0900

    imagemagick: Bump to 7.1.0.48

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.1.0.43
-TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=62eccb3e8c89acc66c2b10a2bea843b6659a2ddce275c7d2ffee8c34af6114fd
+_VERSION=7.1.0-48
+TERMUX_PKG_VERSION=${_VERSION//-/.}
+TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ffe0031855ab4a3f58793cbe60fcf2cf1e49ec64b4ae3345285bee40491926e0
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libheif, libjpeg-turbo, libjxl, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib, zstd"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
@@ -28,4 +29,7 @@ share/ImageMagick-7/francais.xml
 
 termux_step_pre_configure() {
 	export LDFLAGS+=" $($CC -print-libgcc-file-name)"
+
+	# Value of PKG_CONFIG becomes hardcoded in bin/*-config
+	export PKG_CONFIG=pkg-config
 }
