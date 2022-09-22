65fcbf38814d29e4fc2d73d1a02635ad685a24ed
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-23 06:59:42 +0900

    graphene: Bump to 1.10.8

--- packages/graphene/build.sh
+++ packages/graphene/build.sh
@@ -2,10 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://ebassi.github.io/graphene/
 TERMUX_PKG_DESCRIPTION="A thin layer of graphic data types"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.10.6
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://github.com/ebassi/graphene/releases/download/${TERMUX_PKG_VERSION}/graphene-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=80ae57723e4608e6875626a88aaa6f56dd25df75024bd16e9d77e718c3560b25
+_MAJOR_VERSION=1.10
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.8
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/graphene/${_MAJOR_VERSION}/graphene-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=a37bb0e78a419dcbeaa9c7027bcff52f5ec2367c25ec859da31dfde2928f279a
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_CONFLICTS="gst-plugins-base (<< 1.20.3-1)"
 TERMUX_PKG_BREAKS="gst-plugins-base (<< 1.20.3-1)"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=disabled
+"
