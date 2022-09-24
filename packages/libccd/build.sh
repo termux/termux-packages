6824258708285be2d60ce92704e9dd49ba809495
Henrik Grimler <grimler@termux.dev>
2022-09-24 21:28:02 +0200

    fix(main/libccd): do not hardcode path to libm
    And also build out of source and use " instead of '.

--- packages/libccd/build.sh
+++ packages/libccd/build.sh
@@ -5,17 +5,17 @@ TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="BSD-LICENSE"
 TERMUX_PKG_MAINTAINER="Pooya Moradi <pvonmoradi@gmail.com>"
 TERMUX_PKG_VERSION="2.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/danfis/libccd/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=542b6c47f522d581fbf39e51df32c7d1256ac0c626e7c2b41f1040d4b9d50d1e
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag" # As of 2022-09-01T00:37:36 no github releases are available.
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS='
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_DOCUMENTATION=ON
 -DBUILD_SHARED_LIBS=ON
 -DBUILD_TESTING=OFF
 -DCMAKE_BUILD_TYPE=Release
-'
+"
 
 termux_step_pre_configure() {
 	# Use double-precision for 64-bit archs, otherwise use single-precision
@@ -30,6 +30,4 @@ termux_step_pre_configure() {
 			TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=' -DENABLE_DOUBLE_PRECISION=OFF'
 			;;
 	esac
-	# Add path of system 'libm' library to cmake
-	export CMAKE_LIBRARY_PATH+="$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL"
 }
