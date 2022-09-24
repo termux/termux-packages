08d27a2b64491e89c19c33664dbee5fcddd1d0e6
Henrik Grimler <grimler@termux.dev>
2022-09-24 21:33:33 +0200

    chore(main/libfcl): do not build in source directory
    Seem to be no reason to do so.  Let's keep src directory clean.

--- packages/libfcl/build.sh
+++ packages/libfcl/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_MAINTAINER="Pooya Moradi <pvonmoradi@gmail.com>"
 TERMUX_PKG_VERSION="0.7.0"
 TERMUX_PKG_SRCURL="https://github.com/flexible-collision-library/fcl/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=90409e940b24045987506a6b239424a4222e2daf648c86dd146cbcb692ebdcbc
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_BUILD_DEPENDS="eigen"
