8ccab6b87e8c908d470231abf31c98e572ded900
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-25 23:54:13 +0900

    grep: Bump to 3.8
    Also fix locale handling. (See also 8ea853de13dbd2330fb83c0d621e9adcafd32006.)

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -2,10 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/grep/
 TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines containing a match to a specified pattern"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.7
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.8
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/grep/grep-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=5c10da312460aec721984d5d83246d24520ec438dd48d7ab5a05dbc0d6d6823c
-TERMUX_PKG_DEPENDS="libandroid-support, pcre"
+TERMUX_PKG_SHA256=498d7cc1b4fb081904d87343febb73475cf771e424fb7e6141aff66013abc382
+TERMUX_PKG_DEPENDS="libandroid-support, pcre2"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_GROUPS="base-devel"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_func_nl_langinfo=no
+ac_cv_header_langinfo_h=no
+am_cv_langinfo_codeset=no
+gl_cv_func_setlocale_works=yes
+"
