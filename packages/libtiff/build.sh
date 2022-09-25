7d11dacc02ffdf5f72ab618dc29d018717b86568
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-25 20:03:58 +0900

    libtiff: Bump to 4.4.0

--- packages/libtiff/build.sh
+++ packages/libtiff/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://www.simplesystems.org/libtiff/ # Note that http://libtiff.org/ is hijacked
 TERMUX_PKG_DESCRIPTION="Support for the Tag Image File Format (TIFF) for storing image data"
-TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYRIGHT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.3.0
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=http://download.osgeo.org/libtiff/tiff-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0e46e5acb087ce7d1ac53cf4f56a09b221537fc86dfc5daaad1c2e89e1b37ac8
+TERMUX_PKG_VERSION=4.4.0
+TERMUX_PKG_SRCURL=https://download.osgeo.org/libtiff/tiff-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=49307b510048ccc7bc40f2cba6e8439182fe6e654057c1a1683139bf2ecb1dc1
 TERMUX_PKG_DEPENDS="libc++, libjpeg-turbo, liblzma, zlib, zstd"
 TERMUX_PKG_BREAKS="libtiff-dev"
 TERMUX_PKG_REPLACES="libtiff-dev"
