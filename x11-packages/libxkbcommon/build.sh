394f1cfcbc904f14a7bca0e6f7cb20b783d5fce7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-26 08:57:44 +0900

    libxkbcommon: Bump to 1.4.1

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://xkbcommon.org/
 TERMUX_PKG_DESCRIPTION="Keymap handling library for toolkits and window systems"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.4.1
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e1b79838256549d72f42c824f720b4f14603374cfbe5587942e35a0cbb13ff9e
+TERMUX_PKG_SHA256=3b86670dd91441708dedc32bc7f684a034232fd4a9bb209f53276c9783e9d40e
 TERMUX_PKG_DEPENDS="libxcb, libxml2, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 
