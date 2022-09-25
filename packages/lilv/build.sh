7c84a402ae0e62c846d92e917253ada30360927d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-25 20:47:45 +0900

    lilv: Bump to 0.24.20

--- packages/lilv/build.sh
+++ packages/lilv/build.sh
@@ -2,35 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://drobilla.net/software/lilv.html
 TERMUX_PKG_DESCRIPTION="A C library to make the use of LV2 plugins as simple as possible for applications"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.24.12
-TERMUX_PKG_SRCURL=https://download.drobilla.net/lilv-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=26a37790890c9c1f838203b47f5b2320334fe92c02a4d26ebbe2669dbd769061
-TERMUX_PKG_DEPENDS="libsndfile, lv2, python, serd, sord, sratom"
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
-}
-
-termux_step_configure() {
-	./waf configure \
-		--prefix=$TERMUX_PREFIX \
-		LINKFLAGS="$LDFLAGS" \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
-}
-
-termux_step_make() {
-	./waf
-}
-
-termux_step_make_install() {
-	./waf install
-}
+TERMUX_PKG_VERSION=0.24.20
+TERMUX_PKG_SRCURL=https://download.drobilla.net/lilv-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=4fb082b9b8b286ea92bbb71bde6b75624cecab6df0cc639ee75a2a096212eebc
+TERMUX_PKG_DEPENDS="libsndfile, lv2, serd, sord, sratom"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dbindings_py=disabled
+"
