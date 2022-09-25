379beaabec90e574db52854a795ef773c1884716
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-09-25 11:01:02 +0530

    add(x11): new package xdg-utils

---
+++ x11-packages/xdg-utils/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/freedesktop
+TERMUX_PKG_DESCRIPTION="Desktop integration utilities"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.1.3
+TERMUX_PKG_SRCURL=https://github.com/freedesktop/xdg-utils/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=25e0192b59e2f4205d142134009faaa9c3c354d029def71bafe8186ef9893e34
+TERMUX_PKG_BUILD_IN_SRC=true
