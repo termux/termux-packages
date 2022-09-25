4c971eb99241ba4b10381928de76588e0d9c802d
tjhexf <97811916+tjhexf@users.noreply.github.com>
2022-09-25 00:58:31 -0300

    upgpkg(main/cmake): update to 3.24.2 (#12100)
    * upgpkg(main/cmake): update to 3.24.2

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package soft
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # When updating version here, please update termux_setup_cmake.sh as well.
-TERMUX_PKG_VERSION=3.24.1
+TERMUX_PKG_VERSION=3.24.2
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4931e277a4db1a805f13baa7013a7757a0cbfe5b7932882925c7061d9d1fa82b
+TERMUX_PKG_SHA256=0d9020f06f3ddf17fb537dc228e1a56c927ee506b486f55fe2dc19f69bf0c8db
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, zlib"
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_FORCE_CMAKE=true
