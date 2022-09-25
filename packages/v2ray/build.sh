a3cdc5a68760b7cb5b4fa082bb403c52db780a82
Kevin Williams <admin@utermux.dev>
2022-09-25 18:13:06 +0800

    v2ray: add geo{ip,site,ip-only-cn-private} support
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/v2ray/build.sh
+++ packages/v2ray/build.sh
@@ -11,6 +11,17 @@ termux_step_post_get_source() {
 	export GOPATH=$TERMUX_PKG_SRCDIR/go
 	go get
 	chmod +w $GOPATH -R
+	rm -rf $TERMUX_PREFIX/share/v2ray/
+	mkdir -p $TERMUX_PREFIX/share/v2ray/
+	termux_download https://raw.githubusercontent.com/v2fly/geoip/release/geoip.dat \
+		$TERMUX_PREFIX/share/v2ray/geoip.dat \
+		20f6ceaa1e39a9de5473d94979855fe3eb93a5ba4a272862a58d59c7bd4ae908
+	termux_download https://raw.githubusercontent.com/v2fly/domain-list-community/release/dlc.dat \
+		$TERMUX_PREFIX/share/v2ray/geosite.dat \
+		aa4ecf49407ab29f31d7e2b7f66fe3a805a16dc9fc9c1cdba60d92dc2a04ec11
+	termux_download https://raw.githubusercontent.com/v2fly/geoip/release/geoip-only-cn-private.dat \
+		$TERMUX_PREFIX/share/v2ray/geoip-only-cn-private.dat \
+		7f7c0c011a80148b8070ce7dde40db20e222b309ff5ab5c8008e696196830c22
 }
 
 termux_step_make() {
