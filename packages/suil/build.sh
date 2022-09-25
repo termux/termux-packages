7122be50e6c53c602ac01b07a087e293b0282295
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-25 20:44:11 +0900

    suil: Bump to 0.10.18

--- packages/suil/build.sh
+++ packages/suil/build.sh
@@ -2,28 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://drobilla.net/software/suil.html
 TERMUX_PKG_DESCRIPTION="A library for loading and wrapping LV2 plugin UIs"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.10.10
-TERMUX_PKG_SRCURL=https://download.drobilla.net/suil-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=750f08e6b7dc941a5e694c484aab02f69af5aa90edcc9fb2ffb4fb45f1574bfb
+TERMUX_PKG_VERSION=0.10.18
+TERMUX_PKG_SRCURL=https://download.drobilla.net/suil-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=84ada094fbe17ad3e765379002f3a0c7149b43b020235e4d7fa41432f206f85f
 TERMUX_PKG_DEPENDS="lv2"
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---no-gtk
---no-qt
---no-x11
+-Dgtk2=disabled
+-Dgtk3=disabled
+-Dqt5=disabled
+-Dx11=disabled
 "
-
-termux_step_configure() {
-	./waf configure \
-		--prefix=$TERMUX_PREFIX \
-		LINKFLAGS="$LDFLAGS" \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
-}
-
-termux_step_make() {
-	./waf
-}
-
-termux_step_make_install() {
-	./waf install
-}
