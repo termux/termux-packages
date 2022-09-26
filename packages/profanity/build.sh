5cce22b5fd2fa6cfa39e4c50ed579008039fed61
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-27 07:53:45 +0900

    profanity: Rebuild due to gpgme ABI change

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Profanity is a console based XMPP client written in C us
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_VERSION="0.13.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/profanity-im/profanity/releases/download/$TERMUX_PKG_VERSION/profanity-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=7a562cd4ba703a39e5caa6f00a01c5a09fe51db20a73885f56b23bc3f82fc492
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, libffi, ncurses, glib, libcurl, readline, libuuid, libotr, gpgme, python, libassuan, libgpg-error, zlib, libsignal-protocol-c, libstrophe"
 TERMUX_PKG_BREAKS="profanity-dev"
 TERMUX_PKG_REPLACES="profanity-dev"
-# pcre needed by glib:
-TERMUX_PKG_BUILD_DEPENDS="pcre, libgcrypt, libcrypt"
+# pcre2 needed by glib:
+TERMUX_PKG_BUILD_DEPENDS="pcre2, libgcrypt, libcrypt"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --enable-plugins --without-xscreensaver"
 TERMUX_PKG_BUILD_IN_SRC=true
