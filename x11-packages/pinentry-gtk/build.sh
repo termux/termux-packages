fa7b0ab2359c7dfb2212d70461b7c7dfbb64926f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-27 07:39:59 +0900

    pinentry-gtk: Bump to 1.2.1

--- x11-packages/pinentry-gtk/build.sh
+++ x11-packages/pinentry-gtk/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/pinentry/index.html
 TERMUX_PKG_DESCRIPTION="Dialog allowing secure password entry (with gtk2 support)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.1
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=1.2.1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=cd12a064013ed18e2ee8475e669b9f58db1b225a0144debdb85a68cecddba57f
+TERMUX_PKG_SHA256=457a185e5a85238fb945a955dc6352ab962dc8b48720b62fc9fa48c7540a4067
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libandroid-shmem, libassuan, libcairo, libgpg-error, ncurses, pango"
 
 TERMUX_PKG_CONFLICTS="pinentry"
