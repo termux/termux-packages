4bee4c90097368b478b78a3900275198e38d4523
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-28 02:40:56 +0900

    timg: Enable auto-update
    %ci:no-build

--- packages/timg/build.sh
+++ packages/timg/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=1.4.2
 TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://github.com/hzeller/timg/archive/24d7727a4ba44b32fcdfb8e21fc258a4638fbbfe.tar.gz
 TERMUX_PKG_SHA256=5fe2a0f68dacfe367cc82ed5c3a21a9c1455ae78df1acedb7c02ed27ef502833
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ffmpeg, graphicsmagick, libc++, libjpeg-turbo, libexif, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
