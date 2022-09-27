dfeac44fac84e20577e7636c1a578f69924a0ce6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-27 18:54:21 +0900

    pinentry-gtk: Revbump
    Trying to resolve inconsistency between mirrors.

--- x11-packages/pinentry-gtk/build.sh
+++ x11-packages/pinentry-gtk/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Dialog allowing secure password entry (with gtk2 support
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=457a185e5a85238fb945a955dc6352ab962dc8b48720b62fc9fa48c7540a4067
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libandroid-shmem, libassuan, libcairo, libgpg-error, ncurses, pango"
