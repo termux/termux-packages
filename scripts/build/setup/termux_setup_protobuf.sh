a8a957dfaf1fb544be271c928792f2d9247b59dc
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-28 05:48:20 +0900

    termux_setup_protobuf: Update checksum
    Follow-up of commit 2727d70bad7b55c932b5c124be9dc106bccefc55.

--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -14,7 +14,7 @@ termux_setup_protobuf() {
 			termux_download \
 				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
 				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				92fb4f5066a6f7b870e09c73115a2c861852af8f6555d8da9955fdb80710bf7f
+				6a9fc36363a2d05d73fc363a46cd57d849068d33305db39f77daac8ba073e818
 
 			rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
 			unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
