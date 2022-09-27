b355119cbb367454ccffa3bfbb85b6e95b45ce0d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-28 05:15:52 +0900

    aapt: Bump to 13.0.0.6

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -75,7 +75,7 @@ yes | $SDK_MANAGER --sdk_root="$ANDROID_HOME" --licenses
 yes | $SDK_MANAGER --sdk_root="$ANDROID_HOME" \
 		"platform-tools" \
 		"build-tools;${TERMUX_ANDROID_BUILD_TOOLS_VERSION}" \
-		"platforms;android-32" \
+		"platforms;android-33" \
 		"platforms;android-28" \
 		"platforms;android-24" \
 		"platforms;android-21"
