3b124eac8d1b6a356694c8d9458648ceda24849b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-29 00:28:44 +0900

    gettext: Fix implementation of `struct __sFILE`

--- packages/gettext/build.sh
+++ packages/gettext/build.sh
@@ -3,12 +3,27 @@ TERMUX_PKG_DESCRIPTION="GNU Internationalization utilities"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.21
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gettext/gettext-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192
-TERMUX_PKG_DEPENDS="libc++, libiconv, pcre, liblzma, libxml2, libcroco, ncurses, libunistring, zlib"
+TERMUX_PKG_DEPENDS="libc++, libiconv, libunistring, libxml2, ncurses"
 TERMUX_PKG_BREAKS="gettext-dev"
 TERMUX_PKG_REPLACES="gettext-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-openmp"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_have_decl_posix_spawn=no
+ac_cv_header_spawn_h=no
+--disable-openmp
+--with-included-libcroco
+--with-included-libglib
+--without-included-libxml
+"
 TERMUX_PKG_GROUPS="base-devel"
+
+termux_step_post_configure() {
+	local pv=$(awk '/^PACKAGE_VERSION =/ { print $3 }' Makefile)
+	local lib
+	for lib in libgettext{lib,src}; do
+		ln -sf ${lib}-${pv}.so $TERMUX_PREFIX/lib/${lib}.so
+	done
+}
