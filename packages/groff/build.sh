dac5ee93e30fd3c1f2495d8724a2f8857a8e8306
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-28 21:08:26 +0900

    essential tools: Fix implementation of `struct __sFILE`

--- packages/groff/build.sh
+++ packages/groff/build.sh
@@ -3,14 +3,16 @@ TERMUX_PKG_DESCRIPTION="typesetting system that reads plain text mixed with form
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.22.4
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/groff/groff-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293
 TERMUX_PKG_DEPENDS="libc++, perl, man"
 TERMUX_PKG_GROUPS="base-devel"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+am_cv_func_iconv=no
+"
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/soelim
 share/man/man1/soelim.1*
