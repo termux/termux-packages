c498746ee506812ca9a648f6311ed78433cc6cb9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-28 21:08:26 +0900

    essential tools: Fix implementation of `struct __sFILE`

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/grep/grep-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=498d7cc1b4fb081904d87343febb73475cf771e424fb7e6141aff66013abc382
 TERMUX_PKG_DEPENDS="libandroid-support, pcre2"
