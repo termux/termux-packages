bce6b8fab43c2687528dfb74f8173226e2b80719
Jia Yuan Lo <jylo06g@gmail.com>
2022-09-28 23:12:10 +0800

    fix(main/mold): correct deps

--- packages/mold/build.sh
+++ packages/mold/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="mold: A Modern Linker"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.7.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rui314/mold/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fa2558664db79a1e20f09162578632fa856b3cde966fbcb23084c352b827dfa9
-TERMUX_PKG_DEPENDS="libc++, openssl, zlib, libandroid-spawn"
+TERMUX_PKG_DEPENDS="libandroid-spawn, libc++, openssl, zlib"
 TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_pre_configure() {
-	LDFLAGS+=" -Wl,--no-as-needed -dl -landroid-spawn"
-}
+# dont depend on system libtbb, xxhash
+# https://github.com/rui314/mold/commit/add94b86266b40bc66789e26358675da9d603919#commitcomment-80494077
