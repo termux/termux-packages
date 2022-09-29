2432c02a907a1c6de779186d1a35d7b888fdbed5
Henrik Grimler <grimler@termux.dev>
2022-09-29 20:27:19 +0200

    chore(x11/libfm-qt): specify gio-unix-2.0 include dir
    Seems to be needed now for some reason, without it configuring fails:
    
    CMake Error at /home/builder/.termux-build/_cache/cmake-3.24.2/share/cmake-3.24/Modules/FindPackageHandleStandardArgs.cmake:230 (message):
      Could NOT find GLIB (missing: GLIB_GIO_UNIX_INCLUDE_DIR) (found suitable
      version "2.74.0", minimum required is "2.50.0")
    
    Fixes termux/termux-packages#12209.
    
    %ci:no-build

--- x11-packages/libfm-qt/build.sh
+++ x11-packages/libfm-qt/build.sh
@@ -8,3 +8,6 @@ TERMUX_PKG_SHA256=a5eecf0c9725c1a32470eaf539989563ba53ea6ed916454cc47e940279c04e
 TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtx11extras, glib, libxcb, libexif, menu-cache"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DGLIB_GIO_UNIX_INCLUDE_DIR=$TERMUX_PREFIX/include/gio-unix-2.0
+"
