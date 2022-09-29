a583ecb5396400f23eadaf8c57318b08c85ece0b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-09-29 15:58:22 +0900

    php: Bump to 8.1.11

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Server-side, HTML-embedded scripting language"
 TERMUX_PKG_LICENSE="PHP-3.01"
 TERMUX_PKG_LICENSE_FILE=LICENSE
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=8.1.10
+TERMUX_PKG_VERSION=8.1.11
 TERMUX_PKG_SRCURL=https://github.com/php/php-src/archive/php-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f692b276666273b2de6b1df875c0d0c6272813550fdc975db0de5060c0ebb7a6
+TERMUX_PKG_SHA256=2faa6c68b34e45485f3c65dc7e7c43aaa9d7cb3fa1822ad7a4bf78d60d334637
 # Build native php for phar to build (see pear-Makefile.frag.patch):
 TERMUX_PKG_HOSTBUILD=true
 # Build the native php without xml support as we only need phar:
