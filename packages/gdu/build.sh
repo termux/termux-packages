edd9fd4335c2f0a07191d8169cc0cdfd9d3f56f0
sk0kanik <37567788+sk0kanik@users.noreply.github.com>
2022-10-01 16:42:47 +0600

    gdu: fix description

--- packages/gdu/build.sh
+++ packages/gdu/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dundee/gdu
-TERMUX_PKG_DESCRIPTION="Modern and intuitive terminal-based text editor"
+TERMUX_PKG_DESCRIPTION="Fast disk usage analyzer with console interface written in Go"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.19.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dundee/gdu/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=69c47eaedd0fc1e664d5a08c91e7b107961145aa307d1fd11cf208dfec573f0c
 TERMUX_PKG_AUTO_UPDATE=true
