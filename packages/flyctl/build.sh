54b00b4668c7d79355ddd30cb9e00cb0fad57ef8
Kevin Williams <admin@utermux.dev>
2022-10-02 10:35:11 +0800

    upgpkg(main/flyctl): update to 0.0.402

--- packages/flyctl/build.sh
+++ packages/flyctl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://fly.io
 TERMUX_PKG_DESCRIPTION="Command line tools for fly.io services"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
-TERMUX_PKG_VERSION="0.0.378"
+TERMUX_PKG_VERSION="0.0.402"
 TERMUX_PKG_SRCURL=https://github.com/superfly/flyctl/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=3364554ea43e5103d1c00a5a647882d96dc65796d53b887eabab0f9cd380e5df
+TERMUX_PKG_SHA256=3d3adac00814e7a4bea89c5fb267df61369ebc74a3655cd2ee83ff88d70b5af5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, arm"
@@ -13,6 +13,7 @@ TERMUX_PKG_BLACKLISTED_ARCHES="i686, arm"
 termux_step_post_get_source() {
 	termux_setup_golang
 	export GOPATH=$TERMUX_PKG_SRCDIR/go
+	export GOOS="android"
 	go get
 	chmod +w $GOPATH -R
 }
