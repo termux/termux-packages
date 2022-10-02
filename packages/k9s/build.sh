f4051c8314937bb38eab86da29eef682662b843a
Kevin Williams <admin@utermux.dev>
2022-10-02 09:10:57 +0800

    upgpkg(main/k9s): update to 0.26.6

--- packages/k9s/build.sh
+++ packages/k9s/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://k9scli.io
 TERMUX_PKG_DESCRIPTION="Kubernetes CLI To Manage Your Clusters In Style!"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION="0.26.3"
+TERMUX_PKG_VERSION="0.26.6"
 TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=30174fcfe4006230e5bbe523785537e89eb88fd38d6657b878f5b0e94635a8e3
+TERMUX_PKG_SHA256=afe885d074e026ac07f8ad7f2e423ab65df812a228ca6d1f96599285751c5630
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_DEPEDNS="mesa"
 
 termux_step_make() {
         termux_setup_golang
