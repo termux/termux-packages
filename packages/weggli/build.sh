24a867d2ae51c9975037ca9cd402ab3e95044c2b
Pooya Moradi <pvonmoradi@gmail.com>
2022-10-03 21:14:27 +0330

    new package: weggli

---
+++ packages/weggli/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/googleprojectzero/weggli"
+TERMUX_PKG_DESCRIPTION="A fast and robust semantic search tool for C and C++ codebases"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.2.4"
+TERMUX_PKG_SRCURL="https://github.com/googleprojectzero/weggli/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=12fde9a0dca2852d5f819eeb9de85c4d11c5c384822f93ac66b2b7b166c3af78
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
