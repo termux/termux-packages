a155b7829136888b61a0dbf4d97b69c858a2e985
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-10-06 22:53:52 +0800

    lilypond: update to 2.23.13

--- packages/lilypond/build.sh
+++ packages/lilypond/build.sh
@@ -4,16 +4,16 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE, LICENSE.OFL"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=2.23
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.13
 TERMUX_PKG_SRCURL=https://lilypond.org/download/sources/v${_MAJOR_VERSION}/lilypond-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3b850b7af0322db720eafe8141f4a3e275fee6e00325da85decffbdb6ffced8c
+TERMUX_PKG_SHA256=17a9d1ad0ec7d1b7501b742d6b65ef371c88007537a7b8cbc9d54573270ddf0a
 TERMUX_PKG_DEPENDS="fontconfig, freetype, ghostscript, glib, guile, harfbuzz, libc++, pango, python, tex-gyre"
 TERMUX_PKG_BUILD_DEPENDS="flex"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-documentation
 --with-texgyre-dir=$TERMUX_PREFIX/share/fonts/tex-gyre
 --with-urwotf-dir=/dev/null
+GUILE_FLAVOR=guile-3.0
 "
 
 termux_step_pre_configure() {
