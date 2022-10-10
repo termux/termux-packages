4ad0db43da73c53ea24dbea97c2460ba9a1a02ac
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-10-10 20:01:56 +0300

    upgpkg(main/stunnel): v5.66

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.stunnel.org/
 TERMUX_PKG_DESCRIPTION="Socket wrapper which can provide TLS support to ordinary applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.64
+TERMUX_PKG_VERSION=5.66
 TERMUX_PKG_SRCURL=https://www.stunnel.org/downloads/stunnel-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=eebe53ed116ba43b2e786762b0c2b91511e7b74857ad4765824e7199e6faf883
+TERMUX_PKG_SHA256=558178704d1aa5f6883aac6cc5d6bbf2a5714c8a0d2e91da0392468cee9f579c
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/stunnel3 share/man/man8/stunnel.*.8"
