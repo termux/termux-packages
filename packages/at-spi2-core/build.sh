110c471ac76481d0048fca2192f80940ab896cb2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-16 08:59:13 +0900

    at-spi2-core: Fix path of `dbus-daemon`

--- packages/at-spi2-core/build.sh
+++ packages/at-spi2-core/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=2.46
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/at-spi2-core/${_MAJOR_VERSION}/at-spi2-core-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=aa0c86c79f7a8d67bae49a5b7a5ab08430c608cffe6e33bf47a72f41ab03c3d0
 TERMUX_PKG_DEPENDS="dbus, glib, libx11, libxi, libxtst"
@@ -12,6 +13,7 @@ TERMUX_PKG_PROVIDES="at-spi2-atk, atk"
 TERMUX_PKG_REPLACES="at-spi2-atk (<< 2.46.0), atk (<< 2.46.0), libatk"
 TERMUX_PKG_BREAKS="at-spi2-atk (<< 2.46.0), atk (<< 2.46.0), libatk"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Ddbus_daemon=$TERMUX_PREFIX/bin/dbus-daemon
 -Dintrospection=no
 -Dx11=yes
 "
