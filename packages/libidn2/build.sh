f93aaf0210e111f566f5e3d2f156a426c1fa6fdb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-15 16:50:40 +0900

    libidn2: Bump to 2.3.3

--- packages/libidn2/build.sh
+++ packages/libidn2/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libidn/#libidn2
 TERMUX_PKG_DESCRIPTION="Free software implementation of IDNA2008, Punycode and TR46"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-3.0, GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.3.2
+TERMUX_PKG_VERSION=2.3.3
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libidn/libidn2-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=76940cd4e778e8093579a9d195b25fff5e936e9dc6242068528b437a76764f91
-TERMUX_PKG_DEPENDS="libunistring, libandroid-support"
+TERMUX_PKG_SHA256=f3ac987522c00d33d44b323cae424e2cffcb4c63c6aa6cd1376edacbf1c36eb0
+TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libunistring"
 TERMUX_PKG_BREAKS="libidn2-dev"
 TERMUX_PKG_REPLACES="libidn2-dev"
