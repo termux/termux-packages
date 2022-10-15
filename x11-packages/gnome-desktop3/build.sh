5d25e2ff13ffc041e1044a9e2bf78f104317ca4d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-15 18:56:25 +0900

    gnome-desktop3: Bump to 43

--- x11-packages/gnome-desktop3/build.sh
+++ x11-packages/gnome-desktop3/build.sh
@@ -2,16 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gnome-desktop
 TERMUX_PKG_DESCRIPTION="Utility library for loading .desktop files"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=41
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.6
+_MAJOR_VERSION=43
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gnome-desktop/${_MAJOR_VERSION}/gnome-desktop-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8b3d942ae2ece55c1c9bbd87e7c9550f6ab9d441ac73ae54aab0c3ef621a388b
+TERMUX_PKG_SHA256=3d6e153317486157596aa3802f87676414c570738f450a94a041fe8835420a69
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gsettings-desktop-schemas, gtk3, iso-codes, libcairo, libxkbcommon, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="fontconfig"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dgnome_distributor=Termux
 -Ddesktop_docs=false
 -Ddebug_tools=false
+-Dintrospection=false
+-Dbuild_gtk4=false
+-Dlegacy_library=true
 "
 
 termux_step_pre_configure() {
