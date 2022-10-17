6fd29ab5e61e42aa654a23f3d7312d947802b216
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-17 13:23:49 +0900

    new package: python-apsw

---
+++ packages/python-apsw/build.sh
@@ -0,0 +1,43 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/rogerbinns/apsw/
+TERMUX_PKG_DESCRIPTION="Another Python SQLite Wrapper"
+TERMUX_PKG_LICENSE="ZLIB"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.39.4.0
+TERMUX_PKG_SRCURL=https://github.com/rogerbinns/apsw/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8c4336c1ab3470ac43a6ea9e11646c5296d63577c210cf89daf870cd72d80906
+TERMUX_PKG_DEPENDS="libsqlite, python"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/python${_PYTHON_VERSION}/site-packages/__pycache__
+lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
+lib/python${_PYTHON_VERSION}/site-packages/site.py
+"
+
+termux_step_post_get_source() {
+	cp $TERMUX_PKG_BUILDER_DIR/setup.cfg ./
+}
+
+termux_step_pre_configure() {
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
+}
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
+	pip install --no-deps . --prefix $TERMUX_PREFIX
+}
