6139a3a0d8e8a31998d427bdb5f62b4be93553f1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-17 20:02:26 +0900

    aircrack-ng: Bump to 1.7

--- root-packages/aircrack-ng/build.sh
+++ root-packages/aircrack-ng/build.sh
@@ -2,21 +2,29 @@ TERMUX_PKG_HOMEPAGE="https://www.aircrack-ng.org/"
 TERMUX_PKG_DESCRIPTION="WiFi security auditing tools suite"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Marlin Sööse <marlin.soose@laro.se>"
-_COMMIT=f94a3fe3f1c74938169317a6395b7f72452499c4
-TERMUX_PKG_VERSION="2:2021.12.19-${_COMMIT:0:8}"
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL="https://github.com/aircrack-ng/aircrack-ng/archive/$_COMMIT.tar.gz"
-TERMUX_PKG_SHA256="d79c02351fe389c41e6c29ef4381109f78ae5f3ba552f776ee08b6a3cbd3aa13"
-TERMUX_PKG_DEPENDS="libc++, libnl, openssl, libpcap"
+TERMUX_PKG_VERSION=3:1.7
+TERMUX_PKG_SRCURL=https://github.com/aircrack-ng/aircrack-ng/archive/refs/tags/${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=05a704e3c8f7792a17315080a21214a4448fd2452c1b0dd5226a3a55f90b58c3
+TERMUX_PKG_DEPENDS="libc++, libnl, libpcap, libsqlite, openssl, pcre, zlib"
 # static build gives errors:
 #   error: undefined reference to 'ac_crypto_engine_init'
 #   error: cannot find the library 'libaircrack-ce-wpa.la' or unhandled argument 'libaircrack-ce-wpa.la'
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-static
+ac_cv_prog_RFKILL=$TERMUX_PREFIX/bin/rfkill
+--with-libpcap-include=$TERMUX_PREFIX/include
+--with-libpcap-lib=$TERMUX_PREFIX/lib
+--with-sqlite3=$TERMUX_PREFIX
+"
 
 termux_step_pre_configure() {
-        NOCONFIGURE=1 ./autogen.sh
+	NOCONFIGURE=1 ./autogen.sh
 }
 
-termux_step_post_make_install() {
-        ln -sf libaircrack-ce-wpa-1.6.0.so $TERMUX_PREFIX/lib/libaircrack-ce-wpa.so
+termux_step_post_configure() {
+	local _LT_VER=$(awk '/^LT_VER =/ { print $3 }' "$TERMUX_PKG_BUILDDIR"/Makefile)
+	local m
+	for m in ce-wpa osdep; do
+		ln -sfr $TERMUX_PREFIX/lib/libaircrack-${m}{-$_LT_VER,}.so
+	done
 }
