84d4b8ef57f52408a1b5304bc86197d287029f40
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-18 05:21:30 +0900

    rsync: Backport patch
    from https://github.com/WayneD/rsync/commit/464555ea923b32f3504678d05bc7de9205e5c8da.

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Utility that provides fast incremental file transfer"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.2.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fb3365bab27837d41feaf42e967c57bd3a47bc8f10765a3671efd6a3835454d3
 TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl, openssl-tool, zlib, zstd"
