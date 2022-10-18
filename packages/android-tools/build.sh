6b895d6070fe2e756adfe2b1f7c8c4abffa54ce4
ifurther <55025025+ifurther@users.noreply.github.com>
2022-10-19 01:00:42 +0800

    upgpkg(main/android-tools): update to 33.0.3

--- packages/android-tools/build.sh
+++ packages/android-tools/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://developer.android.com/
 TERMUX_PKG_DESCRIPTION="Android platform tools"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=31.0.3p1
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=33.0.3
 TERMUX_PKG_SRCURL=https://github.com/nmeum/android-tools/releases/download/$TERMUX_PKG_VERSION/android-tools-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=0ef69f919d58a2bdff2083d2e83a9ef38df079ec82651b2544e9e48086df5ab8
+TERMUX_PKG_SHA256=8ce174dab781d5debd29ed0f96572231f777bee19b8ef3c167e33d3ea7670bc5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libusb, pcre2, googletest, libprotobuf, brotli, zstd, liblz4"
 
