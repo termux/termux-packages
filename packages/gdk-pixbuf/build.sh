ad2ba5829706336b5758e254dc7038b932636c9f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 02:08:48 +0900

    gdk-pixbuf: Build introspection data

--- packages/gdk-pixbuf/build.sh
+++ packages/gdk-pixbuf/build.sh
@@ -3,12 +3,22 @@ TERMUX_PKG_DESCRIPTION="Library for image loading and manipulation"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.42.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/archive/${TERMUX_PKG_VERSION}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=226d950375907857b23c5946ae6d30128f08cd75f65f14b14334c7a9fb686e36
 TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo, zstd"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="gdk-pixbuf-dev"
 TERMUX_PKG_REPLACES="gdk-pixbuf-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgio_sniffing=false"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgtk_doc=false
+-Dintrospection=enabled
+-Dgio_sniffing=false
+"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
 
 termux_step_create_debscripts() {
 	for i in postinst postrm triggers; do
