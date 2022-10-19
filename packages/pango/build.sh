47a4f78425f51fec80385d5ac77eee869a51cf76
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 02:15:52 +0900

    pango: Build introspection data

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -4,12 +4,20 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=1.50
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.11
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${_MAJOR_VERSION}/pango-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8800f812d89ee61388188703203f3a7878963c22f8695aaf1fa0a1a1428d17ae
 TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz, libcairo, libxft, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="pango-dev"
 TERMUX_PKG_REPLACES="pango-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=disabled"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=enabled
+"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
 
 termux_step_post_make_install() {
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/pango-view.1 \
