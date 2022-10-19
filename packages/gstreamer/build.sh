728ce126e53bbf6c0114f8e2fc9bbb72a20d2e38
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 02:10:07 +0900

    gstreamer: Build introspection data

--- packages/gstreamer/build.sh
+++ packages/gstreamer/build.sh
@@ -3,12 +3,15 @@ TERMUX_PKG_DESCRIPTION="Open source multimedia framework"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.20.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=607daf64bbbd5fb18af9d17e21c0d22c4d702fffe83b23cb22d1b1af2ca23a2a
 TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="gstreamer-dev"
 TERMUX_PKG_REPLACES="gstreamer-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=enabled
 -Dcheck=disabled
 -Dtests=disabled
 -Dexamples=disabled
@@ -16,3 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibunwind=disabled
 -Dlibdw=disabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
