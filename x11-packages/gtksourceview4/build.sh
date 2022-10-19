75ae919f73a2ba084d621a367837dc5c3db63277
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 08:20:01 +0900

    gtksourceview4: Build introspection data

--- x11-packages/gtksourceview4/build.sh
+++ x11-packages/gtksourceview4/build.sh
@@ -4,17 +4,23 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=4.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gtksourceview/${_MAJOR_VERSION}/gtksourceview-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c30019506320ca2474d834cced1e2217ea533e00eb2a3f4eb7879007940ec682
 TERMUX_PKG_DEPENDS="atk, fribidi, glib, gtk3, libcairo, libxml2, pango"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dgir=false
+-Dgir=true
 -Dvapi=false
 "
 
 termux_step_pre_configure() {
-	case "$TERMUX_PKG_VERSION" in
-		4.*|*:4.* ) ;;
-		* ) termux_error_exit "Invalid version '$TERMUX_PKG_VERSION' for package '$TERMUX_PKG_NAME'." ;;
-	esac
+	termux_setup_gir
+}
+
+termux_step_post_massage() {
+	local _GUARD_FILE="lib/libgtksourceview-4.so"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "Error: file ${_GUARD_FILE} not found."
+	fi
 }
