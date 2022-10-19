c9fbec746fc04a1b5e964ba6461fb694be3c8b5c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 02:11:31 +0900

    gst-plugins-base: Build introspection data

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -3,18 +3,22 @@ TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.20.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=7e30b3dd81a70380ff7554f998471d6996ff76bbe6fc5447096f851e24473c9f
 TERMUX_PKG_DEPENDS="graphene, gstreamer, libandroid-shmem, libjpeg-turbo, libopus, libpng, libvorbis, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="gst-plugins-base-dev"
 TERMUX_PKG_REPLACES="gst-plugins-base-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=enabled
 -Dtests=disabled
 -Dexamples=disabled
 -Dpango=disabled
 "
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	LDFLAGS+=" -landroid-shmem"
 }
