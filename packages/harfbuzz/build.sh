0660c418cfbadc18469f1e0bae25f6c3fdeb500c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 11:49:24 +0900

    harfbuzz: Build introspection data

--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -3,15 +3,24 @@ TERMUX_PKG_DESCRIPTION="OpenType text shaping engine"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.3.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=77c8c903f4539b050a6d3a5be79705c7ccf7b1cb66d68152a651486e261edbd2
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="freetype, glib, libbz2, libc++, libpng, libgraphite"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="harfbuzz-dev"
 TERMUX_PKG_REPLACES="harfbuzz-dev"
-TERMUX_PKG_BUILD_DEPENDS="libicu"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgraphite=enabled"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgobject=enabled
+-Dgraphite=enabled
+-Dintrospection=enabled
+"
 
 termux_step_post_get_source() {
 	mv CMakeLists.txt CMakeLists.txt.unused
+}
+
+termux_step_pre_configure() {
+	termux_setup_gir
 }
