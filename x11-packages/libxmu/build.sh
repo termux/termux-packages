a72e9971c8858b73f8ba8bfad14a94828ddfc369
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 13:43:00 +0900

    libxmu: Bump to 1.1.4

--- x11-packages/libxmu/build.sh
+++ x11-packages/libxmu/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 miscellaneous micro-utility library"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: MIT, HPND, ISC
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.3
-TERMUX_PKG_REVISION=23
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXmu-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9c343225e7c3dc0904f2122b562278da5fed639b1b5e880d25111561bac5b731
-TERMUX_PKG_DEPENDS="libice, libsm, libuuid, libx11, libxau, libxcb, libxdmcp, libxext, libxt"
+TERMUX_PKG_VERSION=1.1.4
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXmu-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=210de3ab9c3e9382572c25d17c2518a854ce6e2c62c5f8315deac7579e758244
+TERMUX_PKG_DEPENDS="libx11, libxext, libxt"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
