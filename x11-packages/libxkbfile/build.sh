726a5d59f09a9ce33a572eb8ee0c54b830bf45a2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 13:31:14 +0900

    libxkbfile: Bump to 1.1.1

--- x11-packages/libxkbfile/build.sh
+++ x11-packages/libxkbfile/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 keyboard file manipulation library"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: HPND, MIT
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.0
-TERMUX_PKG_REVISION=22
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libxkbfile-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=758dbdaa20add2db4902df0b1b7c936564b7376c02a0acd1f2a331bd334b38c7
-TERMUX_PKG_DEPENDS="libx11, libxau, libxcb, libxdmcp"
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libxkbfile-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=8623dc26e7aac3c5ad8a25e57b566f4324f5619e5db38457f0804ee4ed953443
+TERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
