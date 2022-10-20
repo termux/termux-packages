d18e63eb3b72de03d92dbc46ed94afd3544abac8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-20 13:25:44 +0900

    libxshmfence: Bump to 1.3.1

--- x11-packages/libxshmfence/build.sh
+++ x11-packages/libxshmfence/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="A library that exposes a event API on top of Linux futexes"
-TERMUX_PKG_LICENSE="GPL-2.0"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3
-TERMUX_PKG_REVISION=29
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libxshmfence-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=b884300d26a14961a076fbebc762a39831cb75f92bed5ccf9836345b459220c7
+TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libxshmfence-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=1129f95147f7bfe6052988a087f1b7cb7122283d2c47a7dbf7135ce0df69b4f8
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-futex"
