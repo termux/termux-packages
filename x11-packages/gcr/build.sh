9846b67e0e38db9861e22b2cb07e6a335548909a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-22 02:06:32 +0900

    gcr: Build introspection data

--- x11-packages/gcr/build.sh
+++ x11-packages/gcr/build.sh
@@ -5,13 +5,14 @@ TERMUX_PKG_MAINTAINER="@termux"
 # This specific package is for libgcr-3.
 _MAJOR_VERSION=3.41
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gcr/${_MAJOR_VERSION}/gcr-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=bb7128a3c2febbfee9c03b90d77d498d0ceb237b0789802d60185c71c4bea24f
 TERMUX_PKG_DEPENDS="glib, gtk3, libcairo, libgcrypt, p11-kit, pango"
-TERMUX_PKG_BUILD_DEPENDS="gnupg"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, gnupg"
 TERMUX_PKG_RECOMMENDS="gnupg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dintrospection=false
+-Dintrospection=true
 -Dgtk=true
 -Dgtk_doc=false
 -Dgpg_path=$TERMUX_PREFIX/bin/gpg
@@ -20,6 +21,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	local bin_dir=$TERMUX_PKG_BUILDDIR/_dummy/bin
 	mkdir -p $bin_dir
 	pushd $bin_dir
