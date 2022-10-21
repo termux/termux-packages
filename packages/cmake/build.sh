2f9357ed46da7eb0b23274d548c632690f1630a1
Yaksh Bariya <yakshbari4@gmail.com>
2022-10-21 21:39:10 +0530

    chore(main/cmake): fix directory structure
    Fixes #12465

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # When updating version here, please update termux_setup_cmake.sh as well.
 TERMUX_PKG_VERSION=3.24.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0d9020f06f3ddf17fb537dc228e1a56c927ee506b486f55fe2dc19f69bf0c8db
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, zlib"
@@ -12,6 +13,7 @@ TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DSPHINX_MAN=ON
 -DCMAKE_MAN_DIR=share/man
+-DCMAKE_DOC_DIR=share/doc/cmake
 -DCMAKE_USE_SYSTEM_CURL=ON
 -DCMAKE_USE_SYSTEM_EXPAT=ON
 -DCMAKE_USE_SYSTEM_FORM=ON
