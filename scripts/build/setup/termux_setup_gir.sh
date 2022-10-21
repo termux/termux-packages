75e54aed0b9e70c01b32f7f079bb375f1418fb67
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-22 00:04:53 +0900

    termux_setup_gir: Support on-device building
    %ci:no-build

--- scripts/build/setup/termux_setup_gir.sh
+++ scripts/build/setup/termux_setup_gir.sh
@@ -1,56 +1,65 @@
 termux_setup_gir() {
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
-		return
-	fi
+	# Used by gi-cross-launcher:
+	export TERMUX_PKG_GIR_PRE_GENERATED_DUMP_DIR="$TERMUX_PKG_BUILDER_DIR/gir/${TERMUX_PKG_VERSION##*:}"
 
 	local _GIR_CROSS_FOLDER="$TERMUX_COMMON_CACHEDIR/gir-cross"
 	local bin="$_GIR_CROSS_FOLDER/bin"
 	export GI_CROSS_LAUNCHER="$bin/gi-cross-launcher"
-	local scanner="$bin/g-ir-scanner"
-	local compiler="$bin/g-ir-compiler"
-	local ldd="$bin/ldd"
+
 	if [ ! -d "$_GIR_CROSS_FOLDER" ]; then
-		install -Dm700 -T \
-			"$TERMUX_SCRIPTDIR/packages/gobject-introspection/gi-cross-launcher.sh" \
-			"$GI_CROSS_LAUNCHER"
+		if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
+			unset TERMUX_G_IR_COMPILER
 
-		cat > "$scanner" <<-EOF
-			#!$(command -v sh)
-			export XDG_DATA_DIRS="$TERMUX_PREFIX/share"
-			exec /usr/bin/g-ir-scanner "\$@"
-		EOF
-		chmod 0700 "$scanner"
+			sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+				"$TERMUX_SCRIPTDIR/packages/gobject-introspection/gi-cross-launcher-on-device.in" \
+				> "$GI_CROSS_LAUNCHER"
+			chmod 0700 "$GI_CROSS_LAUNCHER"
+		else
+			local scanner="$bin/g-ir-scanner"
+			local compiler="$bin/g-ir-compiler"
+			local ldd="$bin/ldd"
+			export TERMUX_G_IR_COMPILER="$compiler"
 
-		cat > "$compiler" <<-EOF
-			#!$(command -v sh)
-			exec /usr/bin/g-ir-compiler "\$@" \
-				--includedir "$TERMUX_PREFIX/share/gir-1.0"
-		EOF
-		chmod 0700 "$compiler"
+			install -Dm700 -T \
+				"$TERMUX_SCRIPTDIR/packages/gobject-introspection/gi-cross-launcher.sh" \
+				"$GI_CROSS_LAUNCHER"
 
-		cat > "$ldd" <<-EOF
-			#!/bin/bash-static
-			unset LD_LIBRARY_PATH
-		EOF
-		sed 1d "$TERMUX_SCRIPTDIR/packages/ldd/ldd.in" >> "$ldd"
-		chmod 0700 "$ldd"
-	fi
-	local env
-	for env in CC CXX; do
-		local cmd="$(eval echo \${$env})"
-		local w="$bin/$(basename "$cmd")"
-		if [ ! -e "$w" ]; then
-			cat > "$w" <<-EOF
+			cat > "$scanner" <<-EOF
+				#!$(command -v sh)
+				export XDG_DATA_DIRS="$TERMUX_PREFIX/share"
+				exec /usr/bin/g-ir-scanner "\$@"
+			EOF
+			chmod 0700 "$scanner"
+
+			cat > "$compiler" <<-EOF
+				#!$(command -v sh)
+				exec /usr/bin/g-ir-compiler "\$@" \
+					--includedir "$TERMUX_PREFIX/share/gir-1.0"
+			EOF
+			chmod 0700 "$compiler"
+
+			cat > "$ldd" <<-EOF
 				#!/bin/bash-static
 				unset LD_LIBRARY_PATH
-				exec "$(command -v "$cmd")" "\$@"
 			EOF
-			chmod 0700 "$w"
+			sed 1d "$TERMUX_SCRIPTDIR/packages/ldd/ldd.in" >> "$ldd"
+			chmod 0700 "$ldd"
 		fi
-	done
+	fi
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		local env
+		for env in CC CXX; do
+			local cmd="$(eval echo \${$env})"
+			local w="$bin/$(basename "$cmd")"
+			if [ ! -e "$w" ]; then
+				cat > "$w" <<-EOF
+					#!/bin/bash-static
+					unset LD_LIBRARY_PATH
+					exec "$(command -v "$cmd")" "\$@"
+				EOF
+				chmod 0700 "$w"
+			fi
+		done
+	fi
 	export PATH="$bin:$PATH"
-
-	# For convenience:
-	export TERMUX_PKG_GIR_PRE_GENERATED_DUMP_DIR="$TERMUX_PKG_BUILDER_DIR/gir/${TERMUX_PKG_VERSION##*:}"
-	export TERMUX_G_IR_COMPILER="$compiler"
 }
