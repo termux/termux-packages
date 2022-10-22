67c05787623090b6e87d1b51107432a10f6a37c0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-23 02:06:31 +0900

    gegl: Build introspection data

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -4,15 +4,22 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.4
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.38
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${_MAJOR_VERSION}/gegl-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e4a33c8430a5042fba8439b595348e71870f0d95fbf885ff553f9020c1bed750
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, ffmpeg, babl, json-glib, libjpeg-turbo, libpng, libjasper, littlecms, libtiff, librsvg, zlib"
-TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac, xorgproto"
 TERMUX_PKG_BREAKS="gegl-dev"
 TERMUX_PKG_REPLACES="gegl-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false"
+TERMUX_PKG_DISABLE_GIR=false
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=true
+"
 
 # Disable ffmpeg support until fixed upstream:
 # https://gitlab.gnome.org/GNOME/gegl/-/issues/301
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dlibav=disabled"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
