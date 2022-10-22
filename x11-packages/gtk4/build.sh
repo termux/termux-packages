42997b91ccad8cd6860aa9a3755d1f8e639cffac
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-23 05:29:30 +0900

    gtk4: Build introspection data

--- x11-packages/gtk4/build.sh
+++ x11-packages/gtk4/build.sh
@@ -4,13 +4,14 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=4.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gtk/${_MAJOR_VERSION}/gtk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5ce8d8de98a23bd0c8eca1a61094e1c009b5f009dcbd60b45e990a8db1b742fd
 TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, fontconfig, gdk-pixbuf, glib, glib-bin, graphene, gtk-update-icon-cache, libcairo, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango, shared-mime-info, ttf-dejavu, libxkbcommon"
-TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
-
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xorgproto"
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=enabled
 -Dwayland-backend=false
 -Ddemos=false
 -Dbuild-examples=false
@@ -19,3 +20,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dprint-cups=disabled
 -Dmedia-gstreamer=disabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
