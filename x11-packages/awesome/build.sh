ed838c23a04438ada16164149948015befa88b98
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-22 13:33:45 +0900

    new package: awesome

---
+++ x11-packages/awesome/build.sh
@@ -0,0 +1,55 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/awesomeWM/awesome
+TERMUX_PKG_DESCRIPTION="A highly configurable, next generation framework window manager for X"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+# Latest release version 4.3 does not support Lua 5.4.
+_COMMIT=26032bf018cfac3b930e055cabc3ee07cbc2a4f9
+TERMUX_PKG_VERSION=2022.10.16
+TERMUX_PKG_SRCURL=https://github.com/awesomeWM/awesome.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="dbus, gdk-pixbuf, glib, libcairo, liblua54, libx11, libxcb, libxdg-basedir, libxkbcommon, lua-lgi, pango, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DLUA_MATH_LIBRARY=
+"
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
+		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
+}
+
+termux_step_host_build() {
+	local prefix="$TERMUX_PKG_HOSTBUILD_DIR/_prefix"
+
+	local IMAGEMAGICK_BUILD_SH=$TERMUX_SCRIPTDIR/packages/imagemagick/build.sh
+	local IMAGEMAGICK_SRCURL=$(bash -c ". $IMAGEMAGICK_BUILD_SH; echo \$TERMUX_PKG_SRCURL")
+	local IMAGEMAGICK_SHA256=$(bash -c ". $IMAGEMAGICK_BUILD_SH; echo \$TERMUX_PKG_SHA256")
+	local IMAGEMAGICK_TARFILE=$TERMUX_PKG_CACHEDIR/$(basename $IMAGEMAGICK_SRCURL)
+	termux_download $IMAGEMAGICK_SRCURL $IMAGEMAGICK_TARFILE $IMAGEMAGICK_SHA256
+	mkdir -p imagemagick
+	cd imagemagick
+	tar xf $IMAGEMAGICK_TARFILE --strip-components=1
+	./configure --prefix="$prefix" --with-png
+	make -j $TERMUX_MAKE_PROCESSES
+	make install
+}
+
+termux_step_pre_configure() {
+	export PATH="$TERMUX_PKG_HOSTBUILD_DIR/_prefix/bin:$PATH"
+
+	local bin="$TERMUX_PKG_BUILDDIR/_bin"
+	mkdir -p "$bin"
+	sed -e "s|@PREGEN_DIR@|$TERMUX_PKG_BUILDER_DIR/pregen|g" \
+		"$TERMUX_PKG_BUILDER_DIR/lua-wrapper.in" > "$bin/lua"
+	chmod 0700 "$bin/lua"
+	touch "$bin/lgi-check"
+	chmod 0700 "$bin/lgi-check"
+	export PATH="$bin:$PATH"
+}
---
+++ x11-packages/awesome/pregen/script_files/theme.lua
@@ -0,0 +1,158 @@
+---------------------------------------------------------------------------
+--- The default theme file.
+--
+-- If you wish to create a custom theme, copy this file to
+-- `~/.config/awesome/mytheme.lua`
+-- and replace:
+--
+--    beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+--
+-- with
+--
+--    beautiful.init(gears.filesystem.get_configuration_dir() .. "mytheme.lua")
+--
+-- in your `rc.lua`.
+--
+-- Here is the default theme content:
+--
+--
+--    ---------------------------
+--    -- Default awesome theme --
+--    ---------------------------
+--    
+--    local theme_assets = require("`beautiful.theme`_assets")
+--    local xresources = require("`beautiful.xresources`")
+--    local rnotification = require("ruled.notification")
+--    local dpi = xresources.apply_dpi
+--    
+--    local gfs = require("`gears.filesystem`")
+--    local themes_path = gfs.get_themes_dir()
+--    
+--    local theme = {}
+--    
+--    theme.font          = "sans 8"
+--    
+--    theme.bg_normal     = "#222222"
+--    theme.bg_focus      = "#535d6c"
+--    theme.bg_urgent     = "#ff0000"
+--    theme.bg_minimize   = "#444444"
+--    theme.bg_systray    = theme.bg_normal
+--    
+--    theme.fg_normal     = "#aaaaaa"
+--    theme.fg_focus      = "#ffffff"
+--    theme.fg_urgent     = "#ffffff"
+--    theme.fg_minimize   = "#ffffff"
+--    
+--    theme.useless_gap         = dpi(0)
+--    theme.border_width        = dpi(1)
+--    theme.border_color_normal = "#000000"
+--    theme.border_color_active = "#535d6c"
+--    theme.border_color_marked = "#91231c"
+--    
+--    -- There are other variable sets
+--    -- overriding the default one when
+--    -- defined, the sets are:
+--    -- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+--    -- tasklist_[bg|fg]_[focus|urgent]
+--    -- titlebar_[bg|fg]_[normal|focus]
+--    -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+--    -- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+--    -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+--    -- Example:
+--    --theme.taglist_bg_focus = "#ff0000"
+--    
+--    -- Generate taglist squares:
+--    local taglist_square_size = dpi(4)
+--    theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+--        taglist_square_size, theme.fg_normal
+--    )
+--    theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+--        taglist_square_size, theme.fg_normal
+--    )
+--    
+--    -- Variables set for theming notifications:
+--    -- notification_font
+--    -- notification_[bg|fg]
+--    -- notification_[width|height|margin]
+--    -- notification_[border_color|border_width|shape|opacity]
+--    
+--    -- Variables set for theming the menu:
+--    -- menu_[bg|fg]_[normal|focus]
+--    -- menu_[border_color|border_width]
+--    theme.menu_submenu_icon = themes_path.."default/submenu.png"
+--    theme.menu_height = dpi(15)
+--    theme.menu_width  = dpi(100)
+--    
+--    -- You can add as many variables as
+--    -- you wish and access them by using
+--    -- `beautiful.variable` in your rc.lua
+--    --theme.bg_widget = "#cc0000"
+--    
+--    -- Define the image to load
+--    theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
+--    theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+--    
+--    theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
+--    theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+--    
+--    theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
+--    theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
+--    theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
+--    theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+--    
+--    theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
+--    theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
+--    theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
+--    theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+--    
+--    theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
+--    theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
+--    theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
+--    theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+--    
+--    theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
+--    theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
+--    theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
+--    theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+--    
+--    theme.wallpaper = themes_path.."default/background.png"
+--    
+--    -- You can use your own layout icons like this:
+--    theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+--    theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+--    theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+--    theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+--    theme.layout_max = themes_path.."default/layouts/maxw.png"
+--    theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+--    theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+--    theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+--    theme.layout_tile = themes_path.."default/layouts/tilew.png"
+--    theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+--    theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+--    theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+--    theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+--    theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+--    theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+--    theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+--    
+--    -- Generate Awesome icon:
+--    theme.awesome_icon = theme_assets.awesome_icon(
+--        theme.menu_height, theme.bg_focus, theme.fg_focus
+--    )
+--    
+--    -- Define the icon theme for application icons. If not set then the icons
+--    -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+--    theme.icon_theme = nil
+--    
+--    -- Set different colors for urgent notifications.
+--    rnotification.connect_signal('request::rules', function()
+--        rnotification.append_rule {
+--            rule       = { urgency = 'critical' },
+--            properties = { bg = '#ff0000', fg = '#ffffff' }
+--        }
+--    end)
+--    
+--    return theme
+--    
+--    -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
+-- @script theme.lua
