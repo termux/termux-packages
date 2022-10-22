96be1ef19181a0fe8072a5cd36ec4672e100f630
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-22 15:44:44 +0900

    gnome-desktop3: Build introspection data

--- x11-packages/gnome-desktop3/build.sh
+++ x11-packages/gnome-desktop3/build.sh
@@ -4,19 +4,23 @@ TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=43
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gnome-desktop/${_MAJOR_VERSION}/gnome-desktop-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3d6e153317486157596aa3802f87676414c570738f450a94a041fe8835420a69
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gsettings-desktop-schemas, gtk3, iso-codes, libcairo, libxkbcommon, xkeyboard-config"
-TERMUX_PKG_BUILD_DEPENDS="fontconfig"
+TERMUX_PKG_BUILD_DEPENDS="fontconfig, g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddesktop_docs=false
 -Ddebug_tools=false
--Dintrospection=false
+-Dintrospection=true
 -Dbuild_gtk4=false
 -Dlegacy_library=true
 "
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	export PKG_CONFIG_PATH=$TERMUX_PREFIX/share/pkgconfig
 }
 
