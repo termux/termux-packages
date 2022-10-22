4a33389870ed032952b6198488d56b3e3a3f6bb7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-22 16:10:00 +0900

    webkit2gtk: Build introspection data

--- x11-packages/webkit2gtk/build.sh
+++ x11-packages/webkit2gtk/build.sh
@@ -4,13 +4,15 @@ TERMUX_PKG_DESCRIPTION="A full-featured port of the WebKit rendering engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.38.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f9ce6375a3b6e1329b0b609f46921e2627dc7ad6224b37b967ab2ea643bc0fbd
 TERMUX_PKG_DEPENDS="atk, enchant, fontconfig, freetype, glib, gst-plugins-base, gstreamer, gtk3, harfbuzz, harfbuzz-icu, libc++, libcairo, libgcrypt, libhyphen, libicu, libjpeg-turbo, libnotify, libpng, libsoup, libtasn1, libwebp, libxml2, libx11, libxcomposite, libxdamage, libxslt, libxt, littlecms, openjpeg, pango, woff2"
-TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xorgproto"
 #TERMUX_PKG_PROVIDES="webkit2gtk-4.0"
 TERMUX_PKG_BREAKS="webkit, webkitgtk"
 TERMUX_PKG_REPLACES="webkit, webkitgtk"
+TERMUX_PKG_DISABLE_GIR=false
 
 # USE_OPENGL_OR_ES causes crashes when enabled.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -18,7 +20,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_GAMEPAD=OFF
 -DUSE_SYSTEMD=OFF
 -DUSE_LIBSECRET=OFF
--DENABLE_INTROSPECTION=OFF
+-DENABLE_INTROSPECTION=ON
+-DENABLE_DOCUMENTATION=OFF
 -DUSE_WPE_RENDERER=OFF
 -DENABLE_BUBBLEWRAP_SANDBOX=OFF
 -DUSE_LD_GOLD=OFF
@@ -33,6 +36,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 TERMUX_PKG_RM_AFTER_INSTALL="bin/WebKitWebDriver"
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	CPPFLAGS+=" -DHAVE_MISSING_STD_FILESYSTEM_PATH_CONSTRUCTOR"
 	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
 }
