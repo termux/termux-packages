bb966a304c0ef06c69af0aca25746d1b0c66ef88
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 08:24:58 +0900

    gnumeric: Bump to 1.12.53
    and include Python components (as a subpackage).

---
+++ x11-packages/gnumeric/gnumeric-python.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+lib/gnumeric/*/plugins/gnome-glossary
+lib/gnumeric/*/plugins/python-loader
+lib/gnumeric/*/plugins/py-func
+lib/python*
+"
+TERMUX_SUBPKG_DESCRIPTION="Python components for Gnumeric"
+TERMUX_SUBPKG_DEPENDS="pygobject, python"
