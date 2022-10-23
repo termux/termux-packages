5b65195b1686e22c3f8b9c8ce9de5691677e8a85
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-23 19:47:07 +0900

    libsecret: Build introspection data

--- packages/libsecret/build.sh
+++ packages/libsecret/build.sh
@@ -4,7 +4,17 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@suhan-paradkar"
 _MAJOR_VERSION=0.20
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libsecret/${_MAJOR_VERSION}/libsecret-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3fb3ce340fcd7db54d87c893e69bfc2b1f6e4d4b279065ffe66dac9f0fd12b4d
-TERMUX_PKG_DEPENDS="gobject-introspection, libgcrypt"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dintrospection=false -Dgtk_doc=false"
+TERMUX_PKG_DEPENDS="glib, libgcrypt"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_DISABLE_GIR=false
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgtk_doc=false
+-Dintrospection=true
+"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
