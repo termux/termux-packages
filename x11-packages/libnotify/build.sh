db38f3a3d0d71d2d05d566e5d99f4a6a969995db
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-23 22:42:02 +0900

    libnotify: Bump to 0.8.1
    and build introspection data.

--- x11-packages/libnotify/build.sh
+++ x11-packages/libnotify/build.sh
@@ -1,13 +1,19 @@
 TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/notification-spec/
 TERMUX_PKG_DESCRIPTION="Library for sending desktop notifications"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.9
-TERMUX_PKG_REVISION=13
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/libnotify/0.7/libnotify-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=66c0517ed16df7af258e83208faaf5069727dfd66995c4bbc51c16954d674761
+_MAJOR_VERSION=0.8
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libnotify/${_MAJOR_VERSION}/libnotify-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=d033e6d4d6ccbf46a436c31628a4b661b36dca1f5d4174fe0173e274f4e62557
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=false
--Dintrospection=disabled
+-Dintrospection=enabled
 -Dgtk_doc=false"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
