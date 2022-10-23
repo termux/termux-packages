4b17276a9762ea8d5d230cdc5161b4eb37b086bc
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 07:07:00 +0900

    glade: Bump to 3.40.0
    and build introspection data.

--- x11-packages/glade/build.sh
+++ x11-packages/glade/build.sh
@@ -1,10 +1,21 @@
 TERMUX_PKG_HOMEPAGE=https://glade.gnome.org/
 TERMUX_PKG_DESCRIPTION="User interface designer for Gtk+ and GNOME"
-TERMUX_PKG_LICENSE="LGPL-3.0"
+TERMUX_PKG_LICENSE="LGPL-2.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.38.2
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/GNOME/glade/archive/refs/tags/GLADE_${TERMUX_PKG_VERSION//./_}.tar.gz
-TERMUX_PKG_SHA256=8abd1e7e6f943156602c6591e763ff40cffc68ea144c523d9702276f54b91b1b
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libcairo, libxml2, pango, xsltproc, libglade"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false" 
+_MAJOR_VERSION=3.40
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/glade/${_MAJOR_VERSION}/glade-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=31c9adaea849972ab9517b564e19ac19977ca97758b109edc3167008f53e3d9c
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libxml2, pango"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xsltproc"
+TERMUX_PKG_DISABLE_GIR=false
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgjs=disabled
+-Dpython=disabled
+-Dwebkit2gtk=disabled
+-Dintrospection=true
+" 
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
