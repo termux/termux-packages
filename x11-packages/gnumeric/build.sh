892c98aa6872fc616aacc764ba8a794fc051bbff
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 08:24:58 +0900

    gnumeric: Bump to 1.12.53
    and include Python components (as a subpackage).

--- x11-packages/gnumeric/build.sh
+++ x11-packages/gnumeric/build.sh
@@ -3,27 +3,64 @@ TERMUX_PKG_DESCRIPTION="The GNOME spreadsheet"
 TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=1.12
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.52
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.53
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gnumeric/${_MAJOR_VERSION}/gnumeric-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=73cf73049a22a1d828506275b2c9378ec37c5ff37b68bb1f2f494f0d6400823b
+TERMUX_PKG_SHA256=5568e4c8dceabb9028f1361d1045522f95f0a71daa59e973cbdd2d39badd4f02
 TERMUX_PKG_DEPENDS="glib, goffice, gtk3, libcairo, libgsf, libxml2, pango, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_RECOMMENDS="gnumeric-help"
+TERMUX_PKG_SUGGESTS="glpk"
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-introspection=no
+PYTHON=python
+--enable-introspection=yes
 --without-gda
 --without-psiconv
 --without-paradox
 --without-long-double
 --without-perl
---without-python
+"
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/locale
+share/glib-2.0/schemas/gschemas.compiled
 "
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
+	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	echo "Applying plugins-python-loader-Makefile.in.diff"
+	sed "s|@PYTHON_VERSION@|${_PYTHON_VERSION}|g" \
+		$TERMUX_PKG_BUILDER_DIR/plugins-python-loader-Makefile.in.diff \
+		| patch --silent -p1
+
+	export PYTHON_GIOVERRIDESDIR=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/gi/overrides
+
 	CPPFLAGS+=" -D__USE_GNU"
 }
 
 termux_step_post_configure() {
+	touch ./src/g-ir-scanner
+
 	local ver=$(awk '/^PACKAGE_VERSION =/ { print $3 }' Makefile)
 	local so=$TERMUX_PREFIX/lib/libspreadsheet.so
 	rm -f ${so}
 	echo "INPUT(-lspreadsheet-${ver})" > ${so}
+
+	# Workaround for https://github.com/android/ndk/issues/201
+	local plugins_libs="-L$TERMUX_PKG_BUILDDIR/src/.libs -lspreadsheet"
+	plugins_libs+=" $($PKG_CONFIG libgoffice-0.10 --libs)"
+	plugins_libs+=" $($PKG_CONFIG libgsf-1 --libs)"
+	plugins_libs+=" $($PKG_CONFIG gtk+-3.0 --libs)"
+	find plugins -name Makefile | xargs -n 1 \
+		sed -i 's|^LIBS = |\0'"${plugins_libs}"' |g'
 }
