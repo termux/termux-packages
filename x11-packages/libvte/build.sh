25751d93e56bbf880cd8f7c8431be3b43ca59429
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-23 23:28:01 +0900

    libvte: Bump to 0.62.3
    and build introspection data.

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -1,16 +1,24 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/vte/
 TERMUX_PKG_DESCRIPTION="Virtual Terminal library"
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Do not update to 0.64.x or later with no caution; may break xfce4-terminal
-TERMUX_PKG_VERSION=2:0.62.2
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/vte/${TERMUX_PKG_VERSION:2:4}/vte-${TERMUX_PKG_VERSION:2}.tar.xz
-TERMUX_PKG_SHA256=b0300bbcf0c02df5812a10a3cb8e4fff723bab92c08c97a0a90c167cf543aff0
-TERMUX_PKG_DEPENDS="fribidi, gtk3, libgnutls, libicu, pcre2"
+_MAJOR_VERSION=0.62
+_VERSION=${_MAJOR_VERSION}.3
+TERMUX_PKG_VERSION=2:${_VERSION}
+TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/vte/${_MAJOR_VERSION}/vte-${_VERSION}.tar.xz
+TERMUX_PKG_SHA256=f5770285a52cc23a3c0428a43d492b7c0ba458ce7b8a73768a7d4f1e8a7db3b4
+TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk3, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dgir=false -Dvapi=false"
+TERMUX_PKG_DISABLE_GIR=false
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgir=true
+-Dvapi=false
+"
 
 termux_step_pre_configure() {
-	CXXFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
+	termux_setup_gir
+
+	CPPFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
 }
