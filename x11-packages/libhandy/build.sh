0edb9fa1a038b4f356fde62c6fb444c12088f52a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 04:50:42 +0900

    libhandy: Build introspection data

--- x11-packages/libhandy/build.sh
+++ x11-packages/libhandy/build.sh
@@ -4,13 +4,20 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=1.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libhandy/${_MAJOR_VERSION}/libhandy-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=6c2542c0578924b0c29b7ae6cb44eb26df38eb01d6d5ef3d7d0b0825763230e8
 TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, glib, gtk3, libcairo, pango"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dintrospection=disabled
--Dvapi=false
+-Dintrospection=enabled
+-Dvapi=true
 -Dtests=false
 -Dexamples=false
 -Dglade_catalog=disabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
