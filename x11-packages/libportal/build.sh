b021666fcb37bacd8d747f57d397555cbdf22693
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 05:05:34 +0900

    libportal: Build introspection data

--- x11-packages/libportal/build.sh
+++ x11-packages/libportal/build.sh
@@ -3,13 +3,20 @@ TERMUX_PKG_DESCRIPTION="Flatpak portal library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/flatpak/libportal/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8ad326c4f53b7433645dc86d994fef0292bee8adda0fe67db9288ace19250a9c
 TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dbackends=gtk3,gtk4,qt5
--Dintrospection=false
--Dvapi=false
+-Dintrospection=true
+-Dvapi=true
 -Ddocs=false
 -Dtests=false
 "
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
