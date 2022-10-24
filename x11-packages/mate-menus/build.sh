e981a5b98b578fe8a480b6b3b5f4a62d9863670c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-25 01:33:41 +0900

    mate-menus: Adapt to termux_setup_gir
    %ci:no-build

--- x11-packages/mate-menus/build.sh
+++ x11-packages/mate-menus/build.sh
@@ -7,3 +7,12 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-menus/releases/download/v$TERMUX_PKG_VERSION/mate-menus-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=b3f34c34a75388a3ab710f75469e7f70803beba2329ea91142da4f70154f67ac
 TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-introspection=yes
+"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
