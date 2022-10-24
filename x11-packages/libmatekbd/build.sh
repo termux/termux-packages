50dc7f6ebec991a176f38e83c71cdee63b3c639d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 23:08:09 +0900

    libmatekbd: Explicitly disable gir
    %ci:no-build

--- x11-packages/libmatekbd/build.sh
+++ x11-packages/libmatekbd/build.sh
@@ -7,10 +7,17 @@ TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/libmatekbd/releases/download/v$TERMUX_PKG_VERSION/libmatekbd-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=0e0580370f29d867a11291805b2d39a0cd1951f84cda3f91cb9611f246a8e305
 TERMUX_PKG_DEPENDS="glib, gtk3, libxklavier"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBXKLAVIER=${TERMUX_PREFIX}/lib/libxklavier.so"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+LIBXKLAVIER=${TERMUX_PREFIX}/lib/libxklavier.so
+--enable-introspection=yes
+"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	export GLIB_COMPILE_RESOURCES="glib-compile-resources"
 	export GLIB_COMPILE_SCHEMAS="glib-compile-schemas"
 }
