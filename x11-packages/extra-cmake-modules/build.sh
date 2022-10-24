22dbd1d28d46a5e0a0c6b4b9f1b343a9a230b620
Maxython <mixython@gmail.com>
2022-10-24 09:50:47 +0300

    extra-cmake-modules: package update and compilation fix

--- x11-packages/extra-cmake-modules/build.sh
+++ x11-packages/extra-cmake-modules/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Extra CMake modules (KDE)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.87.0
+TERMUX_PKG_VERSION=5.99.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/extra-cmake-modules-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=541ca70d8e270614d19d8fd9e55f97b55fa1dc78d6538c6f6757be372ef8bcab
+TERMUX_PKG_SHA256=01818aa606628db57129f6e22dbae3532464220802d085c6e0689d032e87807e
 TERMUX_PKG_DEPENDS="cmake"
 
 termux_step_install_license() {
