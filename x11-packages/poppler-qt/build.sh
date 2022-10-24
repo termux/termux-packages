2c001576ece5e378398720c3f9c73df3a40293e4
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-10-24 11:34:55 +0100

    luarocks: bump to `3.9.1`

--- x11-packages/poppler-qt/build.sh
+++ x11-packages/poppler-qt/build.sh
@@ -4,6 +4,12 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=22.04.0
 TERMUX_PKG_REVISION=2
+<<<<<<< HEAD
+=======
+# Do not forget to bump revision of reverse dependencies and rebuild them
+# when SOVERSION is changed.
+_POPPLER_SOVERSION=120
+>>>>>>> parent of 2ce76be09 (luarocks: bump to `3.9.1`)
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=813fb4b90e7bda63df53205c548602bae728887a60f4048aae4dbd9b1927deff
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libc++, libcairo, libcurl, libiconv, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, openjpeg-tools, qt5-qtbase, zlib"
@@ -23,5 +29,20 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
+<<<<<<< HEAD
+=======
+	if ! test "${_POPPLER_SOVERSION}"; then
+		termux_error_exit "Please set _POPPLER_SOVERSION variable."
+	fi
+	local sover_main=$(. $TERMUX_SCRIPTDIR/packages/poppler/build.sh; echo $_POPPLER_SOVERSION)
+	if [ "${sover_main}" != "${_POPPLER_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION mismatch with \"poppler\" package."
+	fi
+	local sover_cmake=$(sed -En 's/^.*set_target_properties\(poppler PROPERTIES .* SOVERSION ([0-9]+).*$/\1/p' CMakeLists.txt)
+	if [ "${sover_cmake}" != "${_POPPLER_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed (CMakeLists.txt: \"${sover_cmake}\")."
+	fi
+
+>>>>>>> parent of 2ce76be09 (luarocks: bump to `3.9.1`)
 	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
 }
