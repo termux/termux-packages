8bcffc0b673374fb305f7b7114c03a88f481581d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-25 01:16:19 +0900

    caja: Adapt to termux_setup_gir
    %ci:no-build

--- x11-packages/caja/build.sh
+++ x11-packages/caja/build.sh
@@ -6,12 +6,13 @@ TERMUX_PKG_VERSION=1.24.1
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/caja/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=652244206b4f67df9b70d008c499d7f38c60741452a80bcc7a9f2b49cbd51e52
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libexif, libice, libnotify, libsm, libx11, libxml2, mate-desktop, pango"
-TERMUX_PKG_BUILD_DEPENDS="mate-common"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, mate-common"
+TERMUX_PKG_DISABLE_GIR=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-xmp
 --disable-packagekit
 --disable-schemas-compile
---disable-introspection
+--enable-introspection
 --disable-update-mimedb
 --disable-icon-update
 "
@@ -19,4 +20,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	NOCONFIGURE=1 ACLOCAL_FLAGS="-I $TERMUX_PREFIX/share/aclocal" \
 		sh $TERMUX_PREFIX/bin/mate-autogen
+	termux_setup_gir
 }
