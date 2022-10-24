56cc3228148946c75287106595e87ec312a48665
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-25 01:26:25 +0900

    thunar: Adapt to termux_setup_gir
    %ci:no-build

--- x11-packages/thunar/build.sh
+++ x11-packages/thunar/build.sh
@@ -7,7 +7,15 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/thunar/4.17/thunar-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=231d6245e627cc2f260ddd8574734607596db3acb69aac20be19d6f1ab8d4c03
 TERMUX_PKG_DEPENDS="desktop-file-utils, exo, hicolor-icon-theme, libexif, libnotify, libpng, libxfce4ui, libxfce4util"
-TERMUX_PKG_BUILD_DEPENDS="xfce4-panel"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xfce4-panel"
 TERMUX_PKG_RECOMMENDS="thunar-archive-plugin, tumbler"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk-doc-html=no --enable-introspection=no"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-gtk-doc-html=no
+--enable-introspection=yes
+"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
