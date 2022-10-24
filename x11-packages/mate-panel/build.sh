643a6a86c8a80210cfdd640d7fe6e09167bcf211
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 23:21:48 +0900

    mate-panel: Explicitly disable gir

--- x11-packages/mate-panel/build.sh
+++ x11-packages/mate-panel/build.sh
@@ -7,4 +7,13 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-panel/releases/download/v$TERMUX_PKG_VERSION/mate-panel-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=30c5ba0392ec76b110220ee6d10bbdba76af36586592a6b3d604db70602848ea
 TERMUX_PKG_DEPENDS="libsm, gtk3, libice, mate-desktop, mate-menus, libwnck, libmateweather"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-introspection=yes
+"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
