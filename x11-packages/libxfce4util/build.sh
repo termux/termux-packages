809bf359d48c4921119df55b1f86204a1755c163
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-25 01:20:40 +0900

    libxfce4util: Adapt to termux_setup_gir
    %ci:no-build

--- x11-packages/libxfce4util/build.sh
+++ x11-packages/libxfce4util/build.sh
@@ -7,4 +7,14 @@ TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4util/4.17/libxfce4util-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1942151f3c1f3732bc53dd9fd3b936f62067796dc47a33c60a0ad05d933d90f2
 TERMUX_PKG_DEPENDS="glib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-introspection=no --enable-vala=no --enable-gtk-doc-html=no"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-introspection=yes
+--enable-vala=yes
+--enable-gtk-doc-html=no
+"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
