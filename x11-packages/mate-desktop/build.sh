eddfff8e9e81fdea2582990447f020f47c746758
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-24 23:10:32 +0900

    mate-desktop: Explicitly disable gir
    %ci:no-build

--- x11-packages/mate-desktop/build.sh
+++ x11-packages/mate-desktop/build.sh
@@ -7,4 +7,13 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-desktop/releases/download/v$TERMUX_PKG_VERSION/mate-desktop-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=d1e8cfae3828d8f083d624b1bfaa332a68ff37c145286432e9e6875a916da1d9
 TERMUX_PKG_DEPENDS="libmatekbd, dconf, startup-notification"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-introspection=yes
+"
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
