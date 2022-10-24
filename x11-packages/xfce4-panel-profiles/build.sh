3c29ac672399f2a20d7fc7b03c595e0bee23dcc1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-25 01:45:28 +0900

    xfce4-panel-profiles: Fix indent in build.sh
    %ci:no-build

--- x11-packages/xfce4-panel-profiles/build.sh
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -10,12 +10,12 @@ TERMUX_PKG_DEPENDS="xfce4-panel, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
-    sed -e s,@prefix@,$TERMUX_PREFIX, Makefile.in.in > Makefile.in
-    sed \
-    -e s,@appname@,xfce4-panel-profiles,g \
-    -e s,@version@,$TERMUX_PKG_VERSION,g \
-    -e s,@mandir@,$TERMUX_PREFIX/share/man,g \
-    -e s,@docdir@,$TERMUX_PREFIX/share/doc/xfce4-panel-profiles,g \
-    -e s,@python@,$TERMUX_PREFIX/bin/python,g \
-     Makefile.in > Makefile
+	sed -e s,@prefix@,$TERMUX_PREFIX, Makefile.in.in > Makefile.in
+	sed \
+		-e s,@appname@,xfce4-panel-profiles,g \
+		-e s,@version@,$TERMUX_PKG_VERSION,g \
+		-e s,@mandir@,$TERMUX_PREFIX/share/man,g \
+		-e s,@docdir@,$TERMUX_PREFIX/share/doc/xfce4-panel-profiles,g \
+		-e s,@python@,$TERMUX_PREFIX/bin/python,g \
+		Makefile.in > Makefile
 }
