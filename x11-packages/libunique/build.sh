b5d4aa862a494df0d5180395e86426c5f4ff0358
Maxython <mixython@gmail.com>
2022-10-24 14:42:23 +0300

    libunique: compilation fix

--- x11-packages/libunique/build.sh
+++ x11-packages/libunique/build.sh
@@ -3,11 +3,16 @@ TERMUX_PKG_DESCRIPTION="Library for writing single instance applications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.6
-TERMUX_PKG_REVISION=19
+TERMUX_PKG_REVISION=20
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/libunique/1.1/libunique-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2cb918dde3554228a211925ba6165a661fd782394bd74dfe15e3853dc9c573ea
 TERMUX_PKG_DEPENDS="glib, gtk2"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	export CFLAGS="$CFLAGS -DG_CONST_RETURN=const"
+}
+
+termux_step_post_configure() {
+	touch ./gtk/g-ir-scanner
 }
