4d1cb0e02c2c5ae1e925c0e2d2ab1fe57e2c3813
Aditya Alok <alok@termux.dev>
2022-10-27 01:23:03 +0530

    bump(main/android-tools): 33.0.3p2
    - Remove no longer required `pthread` patches:
        * vendor_CMakeLists.adb.txt.patch
        * vendor_CMakeLists.fastboot.txt.patch
    See: https://github.com/nmeum/android-tools/commit/8d839f86359b90ed61942e04a0041a61b9792bc8
    
    - Update patch `vendor_CMakeLists.mke2fs.txt.patch`.
    
    Signed-off-by: Aditya Alok <alok@termux.dev>
    
    * Add `dlopen()` hack for function `int __system_properties_init(void)`
    
    Co-authored-by: Tee KOBAYASHI <xtkoba@gmail.com>

--- packages/android-tools/build.sh
+++ packages/android-tools/build.sh
@@ -1,14 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://developer.android.com/
 TERMUX_PKG_DESCRIPTION="Android platform tools"
-TERMUX_PKG_LICENSE="Apache-2.0, MIT"
+TERMUX_PKG_LICENSE="Apache-2.0, BSD 2-Clause"
+TERMUX_PKG_LICENSE_FILE="LICENSE, vendor/core/fastboot/LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=31.0.3p1
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=33.0.3p2
 TERMUX_PKG_SRCURL=https://github.com/nmeum/android-tools/releases/download/$TERMUX_PKG_VERSION/android-tools-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=0ef69f919d58a2bdff2083d2e83a9ef38df079ec82651b2544e9e48086df5ab8
+TERMUX_PKG_SHA256=6bf6b52d7389e79fc92b63cc206451ee42fc4f7da769d76922193e98d75f5604
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="brotli, libc++, liblz4, libprotobuf, libusb, zlib, zstd"
-TERMUX_PKG_BUILD_DEPENDS="googletest, pcre2"
+TERMUX_PKG_DEPENDS="brotli, libc++, liblz4, libprotobuf, libusb, pcre2, zlib, zstd"
+TERMUX_PKG_BUILD_DEPENDS="googletest"
 
 termux_step_pre_configure() {
 	termux_setup_protobuf
