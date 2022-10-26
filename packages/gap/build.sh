9763a1dd08518fa79b695231f8926258b9045629
Henrik Grimler <grimler@termux.dev>
2022-10-26 10:10:41 +0200

    enhance(main/gap): install base packages to a subpackage gap-packages
    And make gap depend on it.

--- packages/gap/build.sh
+++ packages/gap/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.12.1
 TERMUX_PKG_SRCURL=https://github.com/gap-system/gap/releases/download/v${TERMUX_PKG_VERSION}/gap-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f9ebef11ee31b210ce36e3c70960742b4e253282bbd5270adc9324273c92b016
-TERMUX_PKG_DEPENDS="readline, libgmp, zlib"
+TERMUX_PKG_DEPENDS="readline, libgmp, zlib, gap-packages"
 TERMUX_PKG_BREAKS="gap-dev"
 TERMUX_PKG_REPLACES="gap-dev"
 TERMUX_PKG_GROUPS="science"
@@ -20,16 +20,16 @@ termux_step_post_configure() {
 
 termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/lib/gap/pkg
-	# TODO: install at least gapdoc, smallgrp, transgrp, primgrp
-	# or else this package is mostly useless.
-	#cp -r $TERMUX_PKG_SRCDIR/pkg/gapdoc $TERMUX_PREFIX/lib/gap/pkg/
-	#cp -r $TERMUX_PKG_SRCDIR/pkg/smallgrp $TERMUX_PREFIX/lib/gap/pkg/
-	#cp -r $TERMUX_PKG_SRCDIR/pkg/transgrp $TERMUX_PREFIX/lib/gap/pkg/
-	#cp -r $TERMUX_PKG_SRCDIR/pkg/primgrp $TERMUX_PREFIX/lib/gap/pkg/
+	# install at least gapdoc, smallgrp, transgrp, primgrp or else
+	# this package is mostly useless.
+	cp -r $TERMUX_PKG_SRCDIR/pkg/gapdoc $TERMUX_PREFIX/lib/gap/pkg/
+	cp -r $TERMUX_PKG_SRCDIR/pkg/smallgrp $TERMUX_PREFIX/lib/gap/pkg/
+	cp -r $TERMUX_PKG_SRCDIR/pkg/transgrp $TERMUX_PREFIX/lib/gap/pkg/
+	cp -r $TERMUX_PKG_SRCDIR/pkg/primgrp $TERMUX_PREFIX/lib/gap/pkg/
 
 	# To get them to be small enough, could gzip them in place
 	# (GAP transparently allows read access to those)
-	#gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/*/data/*.*
-	#gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/smallgrp/data/id*/*.*
-	#gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/smallgrp/data/small*/*.*
+	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/*/data/*.*
+	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/smallgrp/id*/*.*
+	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/smallgrp/small*/*.*
 }
---
+++ packages/gap/gap-packages.subpackage.sh
@@ -0,0 +1,2 @@
+TERMUX_SUBPKG_DESCRIPTION="Packages for gap"
+TERMUX_SUBPKG_INCLUDE="lib/gap/pkg/"
