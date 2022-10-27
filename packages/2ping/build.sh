dd3021b51c69abdf056c368ab13f73ff47f75d5f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-27 23:29:16 +0900

    2ping: Use pip install

--- packages/2ping/build.sh
+++ packages/2ping/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A bi-directional ping utility"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.5.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rfinnie/2ping/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0f85dc21be1266daccfbba903819ca8935ebdbe002b1e0305bfda258af44fdcd
 TERMUX_PKG_DEPENDS="python"
@@ -26,8 +27,6 @@ termux_step_pre_configure() {
 		${_CROSSENV_PREFIX}
 	popd
 	. ${_CROSSENV_PREFIX}/bin/activate
-
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
 }
 
 termux_step_make() {
@@ -35,21 +34,8 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	python setup.py install --force --prefix $TERMUX_PREFIX
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
+	pip install --no-deps . --prefix $TERMUX_PREFIX
 
 	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 doc/2ping.1
-}
-
-termux_step_create_debscripts() {
-	local _2PING_EGG="2ping-${TERMUX_PKG_VERSION}-py${_PYTHON_VERSION}.egg"
-
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "./${_2PING_EGG}" >> $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	sed -i "/\.\/${_2PING_EGG//./\\.}/d" $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages/easy-install.pth
-	EOF
 }
