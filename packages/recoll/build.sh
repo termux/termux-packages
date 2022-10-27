a11f128b794d6131b7014a711ae986ad22a4c4ee
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-28 04:50:10 +0900

    recoll: Separate recoll-python as a subpackage

--- packages/recoll/build.sh
+++ packages/recoll/build.sh
@@ -3,15 +3,29 @@ TERMUX_PKG_DESCRIPTION="Full-text search for your desktop"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.33.1
-TERMUX_PKG_SRCURL=http://www.lesbonscomptes.com/recoll/recoll-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://www.lesbonscomptes.com/recoll/recoll-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=025dec0b9745e1ddacd86ee5478f9c52b2da2e5c307f831aaa5b2c7f9d7e8db9
-TERMUX_PKG_DEPENDS="aspell, libxapian, libxslt, zlib, python"
+TERMUX_PKG_DEPENDS="aspell, libiconv, libxapian, libxml2, libxslt, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-userdoc --disable-python-chm --disable-x11mon --disable-qtgui"
-TERMUX_PKG_REVISION=1
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 	CXXFLAGS+=" -fPIC"
 	_PYTHON_VERSION=$(source $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
 	CPPFLAGS+=" -I${TERMUX_PREFIX}/include/python${_PYTHON_VERSION}/"
+
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	echo "Applying python-recoll-setup.py.in.diff"
+	sed "s|@PYTHON_VERSION@|${_PYTHON_VERSION}|g" \
+		$TERMUX_PKG_BUILDER_DIR/python-recoll-setup.py.in.diff \
+		| patch --silent -p1
 }
