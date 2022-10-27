5086fdf67d370787429c23eb33a255e7de78ddb1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-28 03:25:45 +0900

    xcb-proto: Bump to 1.15.2
    and separate python-xcbgen as a subpackage.

---
+++ packages/xcb-proto/python-xcbgen.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_INCLUDE="lib/python*"
+TERMUX_SUBPKG_DESCRIPTION="The xcbgen Python module"
+# Cannot depend on python due to circular dependency.
+TERMUX_SUBPKG_BREAKS="xcb-proto (<< 1.15.2)"
+TERMUX_SUBPKG_REPLACES="xcb-proto (<< 1.15.2)"
