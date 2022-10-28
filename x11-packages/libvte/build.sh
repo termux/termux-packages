908bb6bf953a7a4381aaa4d105e42f0ade35773d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-28 13:40:59 +0900

    libvte: Bump to 0.70.1
    The issue https://github.com/termux/x11-packages/issues/537 (crashing
    `xfce4-terminal`) has been resolved; it was caused by `close_range`:
    
    https://gitlab.gnome.org/GNOME/vte/-/commit/6ff6542e902a1b7db6eca18cf8bccf24da431679

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -1,13 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/vte/
 TERMUX_PKG_DESCRIPTION="Virtual Terminal library"
-TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
+TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0, MIT"
+TERMUX_PKG_LICENSE_FILE="COPYING.GPL3, COPYING.LGPL3, COPYING.XTERM"
 TERMUX_PKG_MAINTAINER="@termux"
-# Do not update to 0.64.x or later with no caution; may break xfce4-terminal
-_MAJOR_VERSION=0.62
-_VERSION=${_MAJOR_VERSION}.3
+_MAJOR_VERSION=0.70
+_VERSION=${_MAJOR_VERSION}.1
 TERMUX_PKG_VERSION=2:${_VERSION}
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/vte/${_MAJOR_VERSION}/vte-${_VERSION}.tar.xz
-TERMUX_PKG_SHA256=f5770285a52cc23a3c0428a43d492b7c0ba458ce7b8a73768a7d4f1e8a7db3b4
+TERMUX_PKG_SHA256=1f4601cbfea5302b96902208c8f185e5b18b259b5358bc93cf392bf59871c5b6
 TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk3, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
