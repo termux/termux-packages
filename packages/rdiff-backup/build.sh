77ffab45c55a7bf4b04d8061c759b02eed3e2d9e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 08:13:18 +0900

    rdiff-backup: Revbump to rebuild against Python 3.11
    * Use crossenv.

--- packages/rdiff-backup/build.sh
+++ packages/rdiff-backup/build.sh
@@ -3,22 +3,33 @@ TERMUX_PKG_DESCRIPTION="A utility for local/remote mirroring and incremental bac
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.0.5
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/rdiff-backup/rdiff-backup/releases/download/v${TERMUX_PKG_VERSION/\~/}/rdiff-backup-${TERMUX_PKG_VERSION/\~/}.tar.gz
 TERMUX_PKG_SHA256=2bb7837b4a9712b6efaebfa7da8ed6348ffcb02fcecff0e19d8fff732e933b87
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="librsync, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_pre_configure() {
+	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
+}
+
 termux_step_make() {
 	continue
 }
 
 termux_step_make_install() {
-	local _PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
 	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	export CPPFLAGS+=" -I${TERMUX_PREFIX}/include/python${_PYTHON_VERSION}"
-	export LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-	export LDSHARED="$CC -shared"
-	python${_PYTHON_VERSION} setup.py install --prefix=$TERMUX_PREFIX --force
+	pip install --no-deps . --prefix $TERMUX_PREFIX
 }
