533b312476137eff8dd27f598f2f1a0f24cb27d9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 07:14:11 +0900

    unbound: Revbump to rebuild against Python 3.11

--- packages/unbound/build.sh
+++ packages/unbound/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A validating, recursive, caching DNS resolver"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.16.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nlnetlabs.nl/downloads/unbound/unbound-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ea0c6665e2c3325b769eac1dfccd60fe1828d5fcf662650039eccb3f67edb28e
 TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl"
