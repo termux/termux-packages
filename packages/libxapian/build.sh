50b27b34c2a5c0a536eb59cd4c9d66bd5069f8ad
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 02:19:48 +0900

    libxapian: Bump to 1.4.21

--- packages/libxapian/build.sh
+++ packages/libxapian/build.sh
@@ -2,15 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://xapian.org
 TERMUX_PKG_DESCRIPTION="Xapian search engine library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.18
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.4.21
 TERMUX_PKG_SRCURL=https://oligarchy.co.uk/xapian/${TERMUX_PKG_VERSION}/xapian-core-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=196ddbb4ad10450100f0991a599e4ed944cbad92e4a6fe813be6dce160244b77
+TERMUX_PKG_SHA256=80f86034d2fb55900795481dfae681bfaa10efbe818abad3622cdc0c55e06f88
 # Note that we cannot /proc/sys/kernel/random/uuid (permission denied on
 # new android versions) so need libuuid.
 TERMUX_PKG_DEPENDS="libc++, libuuid, zlib"
 TERMUX_PKG_BREAKS="libxapian-dev"
 TERMUX_PKG_REPLACES="libxapian-dev"
+TERMUX_PKG_RM_AFTER_INSTALL="
+share/doc/xapian-core/
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
