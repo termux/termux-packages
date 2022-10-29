af804874eb0dbc586b5d3aa4fd7de3e8c82f040d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 07:06:34 +0900

    pycairo: Revbump to rebuild against Python 3.11

--- packages/pycairo/build.sh
+++ packages/pycairo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python bindings for the cairo graphics library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.21.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pygobject/pycairo/releases/download/v${TERMUX_PKG_VERSION}/pycairo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=251907f18a552df938aa3386657ff4b5a4937dde70e11aa042bc297957f4b74b
 TERMUX_PKG_DEPENDS="libcairo, python"
