477ca91428c73655737f299d87bcfd64ce60c159
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 07:03:48 +0900

    lilypond: Revbump to rebuild against Python 3.11

--- packages/lilypond/build.sh
+++ packages/lilypond/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE, LICENSE.OFL"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=2.23
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.13
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://lilypond.org/download/sources/v${_MAJOR_VERSION}/lilypond-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=17a9d1ad0ec7d1b7501b742d6b65ef371c88007537a7b8cbc9d54573270ddf0a
 TERMUX_PKG_DEPENDS="fontconfig, freetype, ghostscript, glib, guile, harfbuzz, libc++, pango, python, tex-gyre"
