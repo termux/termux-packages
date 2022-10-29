1f60ab0fcd7eecaefb748c49d083eb66ce9fb464
Lucy Phipps <landfillbaby69@gmail.com>
2022-10-29 23:29:50 +0100

    setup-ubuntu.sh: install python3-wheel-common
    hopefully this fixes e.g. asciinema

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -62,6 +62,7 @@ PACKAGES+=" python3.10"
 PACKAGES+=" python3.11"
 PACKAGES+=" python3-pip"
 PACKAGES+=" python3-setuptools"
+PACKAGES+=" python3-wheel-common"
 PACKAGES+=" python3.10-venv"
 PACKAGES+=" python3.11-venv"
 
