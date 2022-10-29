87b848c837c4869db00e6d47c6fef7f851196905
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 07:12:40 +0900

    python-torch: Revbump to rebuild against Python 3.11

--- packages/python-torch/build.sh
+++ packages/python-torch/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tensors and Dynamic neural networks in Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.12.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pytorch/pytorch.git
 TERMUX_PKG_DEPENDS="python, python-numpy, libopenblas, libprotobuf, libzmq, ffmpeg, opencv"
 TERMUX_PKG_HOSTBUILD=true
