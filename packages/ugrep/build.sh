142641d1e48c940eae2aa4499477bc0a105497fb
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-29 12:49:52 +0100

    new package: ugrep
    thanks for helping now i think that would works

--- packages/ugrep/build.sh
+++ packages/ugrep/build.sh
@@ -1,28 +1,26 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/Genivia/ugrep"
 TERMUX_PKG_DESCRIPTION="A faster, user-friendly and compatible grep replacement"
-TERMUX_PKG_GROUPS="science"
-TERMUX_PKG_LICENSE="BSD-3-Clause"
-TERMUX_PKG_MAINTAINER="@Genivia"
-TERMUX_PKG_VERSION="v3.9.2"
+TERMUX_PKG_GROUPS="util-linux"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@AndVer2"
+TERMUX_PKG_VERSION="3.9.2"
+TERMUX_PKG_SRCURL=https://github.com/Genivia/ugrep/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_SHA256=3416267ac5a4dd2938ca91e7bd91db958d65510c9fd33b221f067bd3c6b3fc6a
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 # TERMUX_PKG_DEPENDS=""
 
 termux_step_get_source() {
-        mkdir -p "$TERMUX_PKG_SRCDIR"
-        cd "$TERMUX_PKG_SRCDIR"
-        git clone "https://github.com/Genivia/ugrep"
+
 }
 
 termux_step_make() {
-        termux_setup_clang
-        termux_setup_binutils
         cd "$TERMUX_PKG_SRCDIR/ugrep"
-        ./configure
+        ./build.sh
 }
 
 termux_step_make_install() {
-        cd "$TERMUX_PKG_SRCDIR/ugrep"
+        cd "$TERMUX_PKG_SRCDIR/ugrep"                                                                                                                
         install -Dm755 "bin/ugrep" "$TERMUX_PREFIX/bin"
 }
