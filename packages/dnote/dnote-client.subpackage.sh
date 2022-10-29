80eeec3730f7919449168be833cdeacf959d0c3a
Kevin Williams <admin@utermux.dev>
2022-10-29 17:56:01 +0800

    add(main): new package dnote
    Signed-off-by: Kevin Williams <admin@utermux.dev>

---
+++ packages/dnote/dnote-client.subpackage.sh
@@ -0,0 +1,2 @@
+TERMUX_SUBPKG_DESCRIPTION="dnote server"
+TERMUX_SUBPKG_INCLUDE="bin/dnote, share/doc/${TERMUX_PKG_NAME}/GPLv3.txt"
---
+++ packages/dnote/dnote-server.subpackage.sh
@@ -0,0 +1,2 @@
+TERMUX_SUBPKG_DESCRIPTION="dnote server"
+TERMUX_SUBPKG_INCLUDE="bin/dnote-server, share/doc/${TERMUX_PKG_NAME}/AGPLv3.txt"
