adc44d7445c1b330ee323db6a14244d92820ec76
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 02:56:39 +0900

    xfce4-settings: Bump to 4.16.3

--- x11-packages/xfce4-settings/build.sh
+++ x11-packages/xfce4-settings/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Settings manager for XFCE environment"
-TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.16.2
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-settings/4.16/xfce4-settings-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4dd7cb420860535e687f673c0b5c0274e0d2fb67181281d4b85be9197da03d7e
+_MAJOR_VERSION=4.16
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-settings/${_MAJOR_VERSION}/xfce4-settings-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=f299e6d26d8e142c0b7edeb9b8917d6bd50bc8647254ea585069c68f2bab8e64
 TERMUX_PKG_DEPENDS="exo, garcon, libnotify, libxfce4ui, libxklavier, lxde-icon-theme, libcanberra, gtk3, gsettings-desktop-schemas"
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/icons/hicolor/icon-theme.cache
