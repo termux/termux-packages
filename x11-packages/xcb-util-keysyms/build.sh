6cd90b05d003c2c85ec4648acd53814a5da61c00
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 21:38:17 +0900

    xcb-util-keysyms: Bump to 0.4.1

--- x11-packages/xcb-util-keysyms/build.sh
+++ x11-packages/xcb-util-keysyms/build.sh
@@ -2,14 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://xcb.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Utility libraries for XC Binding - Standard X key constants and conversion to/from keycodes"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4.0
-TERMUX_PKG_REVISION=24
-TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-keysyms-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=0ef8490ff1dede52b7de533158547f8b454b241aa3e4dcca369507f66f216dd9
+TERMUX_PKG_VERSION=0.4.1
+TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-keysyms-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=7c260a5294412aed429df1da2f8afd3bd07b7cba3fec772fba15a613a6d5c638
 TERMUX_PKG_DEPENDS="libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 
-termux_step_install_license() {
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME \
-		$TERMUX_PKG_BUILDER_DIR/LICENSE
+termux_step_post_massage() {
+	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
+	if [ ! -e "./libxcb-keysyms.so.1" ]; then
+		ln -sf libxcb-keysyms.so libxcb-keysyms.so.1
+	fi
 }
