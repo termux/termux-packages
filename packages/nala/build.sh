5bf317af00ae9723dc6277d205a68d2791f1b917
David Brochero <davidfbg00@gmail.com>
2022-10-30 17:07:30 -0300

    nala: update to version 0.11.1 (patches and build script)

--- packages/nala/build.sh
+++ packages/nala/build.sh
@@ -2,21 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/volian/nala
 TERMUX_PKG_DESCRIPTION="Commandline frontend for the apt package manager"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.11.1
 TERMUX_PKG_SRCURL=https://gitlab.com/volian/nala/-/archive/v${TERMUX_PKG_VERSION}/nala-v${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=0ac394ab2bd924c28c513a8e8bfc9e899a21f2f8fa5e5d77366cb20e0be8676e
+TERMUX_PKG_SHA256=4ce87d75e785e45b1638817077797a3fcd4a15059615455fc1af0127c70cf5cd
 TERMUX_PKG_DEPENDS="python-apt"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 _PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
 
-termux_step_post_get_source() {
-	sed 's:@TERMUX_PKG_VERSION@:'${TERMUX_PKG_VERSION##*:}':g' \
-		${TERMUX_PKG_BUILDER_DIR}/pyproject.toml.diff | \
-		patch --silent -p1
-}
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/python${_PYTHON_VERSION}/site-packages/nala/__pycache__
+"
 
 termux_step_pre_configure() {
 	termux_setup_python_crossenv
@@ -28,13 +26,11 @@ termux_step_pre_configure() {
 	popd
 	. ${_CROSSENV_PREFIX}/bin/activate
 
-	build-pip install wheel tomli
+	build-pip install poetry
 }
 
 termux_step_make_install() {
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	pip install --no-deps . --prefix $TERMUX_PREFIX
-
+	python${_PYTHON_VERSION} -m pip install . --no-deps --prefix $TERMUX_PREFIX
 	install -Dm600 -t $TERMUX_PREFIX/etc/nala debian/nala.conf
 }
 
@@ -43,7 +39,8 @@ termux_step_create_debscripts() {
 	#!$TERMUX_PREFIX/bin/sh
 	mkdir -p $TERMUX_PREFIX/var/lib/nala
 	mkdir -p $TERMUX_PREFIX/var/log/nala
+	mkdir -p $TERMUX_PREFIX/var/lock
 	echo "Installing dependencies through pip..."
-	pip3 install anyio httpx jsbeautifier pexpect rich
+	pip3 install anyio httpx jsbeautifier pexpect python-debian rich tomli typer typing-extensions
 	EOF
 }
