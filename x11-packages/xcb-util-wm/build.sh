83f5dda7704196537a230d46639192f06c868b8d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-30 21:47:48 +0900

    xcb-util-wm: Bump to 0.4.2

--- x11-packages/xcb-util-wm/build.sh
+++ x11-packages/xcb-util-wm/build.sh
@@ -2,9 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://xcb.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Utility libraries for XC Binding - client and window-manager helpers for ICCCM"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4.1
-TERMUX_PKG_REVISION=25
-TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-wm-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f
+TERMUX_PKG_VERSION=0.4.2
+TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-wm-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=62c34e21d06264687faea7edbf63632c9f04d55e72114aa4a57bb95e4f888a0b
 TERMUX_PKG_DEPENDS="libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
+
+termux_step_post_massage() {
+	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
+	if [ ! -e "./libxcb-ewmh.so.2" ]; then
+		ln -sf libxcb-ewmh.so libxcb-ewmh.so.2
+	fi
+	if [ ! -e "./libxcb-icccm.so.4" ]; then
+		ln -sf libxcb-icccm.so libxcb-icccm.so.4
+	fi
+}
