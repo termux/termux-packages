b21206258d55c01cd05da5f03c43f5223ed4fb98
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 12:35:13 +0900

    i3: Bump to 4.21.1

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://i3wm.org/
 TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.20.1
+TERMUX_PKG_VERSION=4.21.1
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=23e1ecaf208c1d162a0c8c66c4590b301a424ee5c011227eabb96e36fd6bfce6
-TERMUX_PKG_DEPENDS="glib, i3status, libandroid-glob, libandroid-shmem, libandroid-wordexp, libcairo, libev, libxcb, libxkbcommon, pango, pcre, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
+TERMUX_PKG_SHA256=edfd781285c654a05dc6db272ce0300c4428e6fbd875d99964e57de25b41bc1e
+TERMUX_PKG_DEPENDS="glib, libandroid-glob, libandroid-wordexp, libcairo, libev, libiconv, libxcb, libxkbcommon, pango, pcre2, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
+TERMUX_PKG_RECOMMENDS="i3status"
 
 TERMUX_PKG_CONFFILES="
 i3/config
@@ -13,5 +14,5 @@ i3/config.keycodes
 "
 
 termux_step_pre_configure() {
-	LDFLAGS+=" -Wl,--no-as-needed -landroid-glob -landroid-shmem -landroid-wordexp"
+	LDFLAGS+=" -landroid-glob -landroid-wordexp"
 }
