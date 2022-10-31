085c9ad418e03cfa7ccae2dda37d5b4467197b65
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 15:07:57 +0900

    sdl2-mixer: Bump to 2.6.2

--- x11-packages/sdl2-mixer/build.sh
+++ x11-packages/sdl2-mixer/build.sh
@@ -3,8 +3,21 @@ TERMUX_PKG_DESCRIPTION="A simple multi-channel audio mixer"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.6.2
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_mixer/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_mixer-${TERMUX_PKG_VERSION}.tar.gz 
-TERMUX_PKG_SHA256=bc5c7b85f297e5f3cb34c99c366a4746922fc41365b15fd78a164dda71a861a3
-TERMUX_PKG_DEPENDS="libflac, libmodplug, libvorbis, mpg123, opusfile, sdl2"
+TERMUX_PKG_SHA256=8cdea810366decba3c33d32b8071bccd1c309b2499a54946d92b48e6922aa371
+TERMUX_PKG_DEPENDS="fluidsynth, libflac, libmodplug, libvorbis, mpg123, opusfile, sdl2"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-music-mod-modplug-shared
+--disable-music-midi-fluidsynth-shared
+--disable-music-ogg-stb
+--enable-music-ogg-vorbis
+--disable-music-ogg-vorbis-shared
+--disable-music-flac-drflac
+--enable-music-flac-libflac
+--disable-music-flac-libflac-shared
+--disable-music-mp3-drmp3
+--enable-music-mp3-mpg123
+--disable-music-mp3-mpg123-shared
+--disable-music-opus-shared
+"
