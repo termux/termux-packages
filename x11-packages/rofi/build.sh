7087dd0806845d34bba078e99cf8d8a9745efc47
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 12:53:44 +0900

    rofi: Bump to 1.7.5

--- x11-packages/rofi/build.sh
+++ x11-packages/rofi/build.sh
@@ -2,15 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://github.com/DaveDavenport/rofi
 TERMUX_PKG_DESCRIPTION="A window switcher, application launcher and dmenu replacement"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
-TERMUX_PKG_VERSION=1.7.4
+TERMUX_PKG_VERSION=1.7.5
 TERMUX_PKG_SRCURL="https://github.com/DaveDavenport/rofi/releases/download/$TERMUX_PKG_VERSION/rofi-$TERMUX_PKG_VERSION.tar.xz"
-TERMUX_PKG_SHA256=1922e7dec6d591335384e4b14e94e592d96be1e4ff07f1506e9c3c367da62cdf
-TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-shmem, libcairo, librsvg, libxkbcommon, startup-notification, xcb-util-wm, xcb-util-xrm, xcb-util-cursor"
+TERMUX_PKG_SHA256=caffcf66d165cb32b748c1db7f229d6d75da58c1685eb17455f65c60e8220c8d
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, libandroid-glob, libcairo, libxcb, libxkbcommon, pango, startup-notification, xcb-util, xcb-util-cursor, xcb-util-wm"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-check"
 
 termux_step_pre_configure() {
-	export LIBS="-landroid-glob -landroid-shmem"
+	export LIBS="-landroid-glob"
 	export GLIB_GENMARSHAL=glib-genmarshal
 	export GOBJECT_QUERY=gobject-query
 	export GLIB_MKENUMS=glib-mkenums
