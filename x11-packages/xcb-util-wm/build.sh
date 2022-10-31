7d621258d01d55e2560eee49b35205527e1be088
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 04:54:45 +0900

    xcb-util-wm: Remove no longer necessary symlinks
    %ci:no-build

--- x11-packages/xcb-util-wm/build.sh
+++ x11-packages/xcb-util-wm/build.sh
@@ -7,13 +7,3 @@ TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-wm-${TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=62c34e21d06264687faea7edbf63632c9f04d55e72114aa4a57bb95e4f888a0b
 TERMUX_PKG_DEPENDS="libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
-
-termux_step_post_massage() {
-	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
-	if [ ! -e "./libxcb-ewmh.so.2" ]; then
-		ln -sf libxcb-ewmh.so libxcb-ewmh.so.2
-	fi
-	if [ ! -e "./libxcb-icccm.so.4" ]; then
-		ln -sf libxcb-icccm.so libxcb-icccm.so.4
-	fi
-}
