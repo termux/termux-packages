40910553509a7c1e94d699e7a4ecb269aaab99f6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 16:15:55 +0900

    sdl2-ttf: Bump to 2.20.1

--- x11-packages/sdl2-ttf/build.sh
+++ x11-packages/sdl2-ttf/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="A library that allows you to use TrueType fonts in your 
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.20.0
+TERMUX_PKG_VERSION=2.20.1
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_ttf/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_ttf-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=874680232b72839555a558b48d71666b562e280f379e673b6f0c7445ea3b9b8a
+TERMUX_PKG_SHA256=78cdad51f3cc3ada6932b1bb6e914b33798ab970a1e817763f22ddbfd97d0c57
 TERMUX_PKG_DEPENDS="freetype, mesa, sdl2"
-
-termux_step_pre_configure() {
-	./autogen.sh
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-freetype-builtin
+--disable-harfbuzz
+"
