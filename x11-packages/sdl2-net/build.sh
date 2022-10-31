6d58a4c8b1435a485fdf89f8ba9e1e227e210234
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 14:52:20 +0900

    sdl2-net: Bump to 2.2.0

--- x11-packages/sdl2-net/build.sh
+++ x11-packages/sdl2-net/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.libsdl.org/projects/SDL_net/
 TERMUX_PKG_DESCRIPTION="A small sample cross-platform networking library (version 2)"
-TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_LICENSE_FILE="COPYING.txt"
+TERMUX_PKG_LICENSE="ZLIB"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@leapofazzam123"
-TERMUX_PKG_VERSION=2.0.1
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=2.2.0
 TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_net/release/SDL2_net-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21
-TERMUX_PKG_DEPENDS="sdl2, libandroid-glob, libflac, libogg, libsndfile, libvorbis, libx11, libxau, libxcb, libxcursor, libxdmcp, libxext, libxfixes, libxi, libxinerama, libxrandr, libxrender, libxss, libxxf86vm"
+TERMUX_PKG_SHA256=4e4a891988316271974ff4e9585ed1ef729a123d22c08bd473129179dc857feb
+TERMUX_PKG_DEPENDS="sdl2"
+TERMUX_PKG_BUILD_DEPENDS="sdl2-static"
