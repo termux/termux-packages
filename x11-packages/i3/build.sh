3650ac25d74b07373a472f75ede98f9c88548152
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 03:49:42 +0900

    i3: Adapt to SONAME change in xcb-util*

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.21.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=edfd781285c654a05dc6db272ce0300c4428e6fbd875d99964e57de25b41bc1e
 TERMUX_PKG_DEPENDS="glib, libandroid-glob, libandroid-wordexp, libcairo, libev, libiconv, libxcb, libxkbcommon, pango, pcre2, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
