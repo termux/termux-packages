3a726d529ee566d84a9c0d61690829d922ba6703
Aditya Alok <alok@termux.dev>
2022-11-01 01:23:31 +0530

    upgpkg(main/luarocks): to 3.9.1
    - Also use lua-5.1 as default as it will allow users to use packages like
      neovim and luajit without any special configuration.
    
    Closes(supersedes) #12535
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/luarocks/build.sh
+++ packages/luarocks/build.sh
@@ -2,38 +2,40 @@ TERMUX_PKG_HOMEPAGE=https://luarocks.org/
 TERMUX_PKG_DESCRIPTION="Deployment and management system for Lua modules"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.8.0
+TERMUX_PKG_VERSION=3.9.1
 TERMUX_PKG_SRCURL=https://luarocks.org/releases/luarocks-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=56ab9b90f5acbc42eb7a94cf482e6c058a63e8a1effdf572b8b2a6323a06d923
-TERMUX_PKG_DEPENDS="curl, lua53"
-TERMUX_PKG_BUILD_DEPENDS="liblua53"
+TERMUX_PKG_SHA256=ffafd83b1c42aa38042166a59ac3b618c838ce4e63f4ace9d961a5679ef58253
+__LUA_VERSION=5.1 # Lua version against which it will be built.
+# Do not use varible here since buildorder.py do not evaluate bash before reading.
+TERMUX_PKG_DEPENDS="curl, lua51"
+TERMUX_PKG_BUILD_DEPENDS="liblua51"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_configure() {
-	if $TERMUX_ON_DEVICE_BUILD; then
-		TERMUX_PKG_EXTRA_MAKE_ARGS="LUA=$TERMUX_PREFIX/bin/lua5.3"
-	else
-		TERMUX_PKG_EXTRA_MAKE_ARGS="LUA=/usr/bin/lua5.3"
+	if [ "$TERMUX_ON_DEVICE_BUILD" != true ]; then
+		# Create temporary symlink to workaround luarock bootstrap
+		# script trying to run cross-compiled lua
+		mv "$TERMUX_PREFIX"/bin/lua"$__LUA_VERSION"{,.bak}
+		ln -sf /usr/bin/lua"$__LUA_VERSION" "$TERMUX_PREFIX"/bin/lua"$__LUA_VERSION"
 	fi
 
-	./configure --prefix=$TERMUX_PREFIX \
-		--with-lua=$TERMUX_PREFIX
-
-	# Create temporary symlink to workaround luarock bootstrap
-	# script trying to run cross-compiled lua
-	mv $TERMUX_PREFIX/bin/lua5.3{,.bak}
-	ln -sf /usr/bin/lua5.3 $TERMUX_PREFIX/bin/lua5.3
+	./configure --prefix="$TERMUX_PREFIX" \
+		--with-lua="$TERMUX_PREFIX"
 }
 
 termux_step_post_make_install() {
-	# Restore lua
-	unlink $TERMUX_PREFIX/bin/lua5.3
-	mv $TERMUX_PREFIX/bin/lua5.3{.bak,}
+	if [ "$TERMUX_ON_DEVICE_BUILD" != "true" ]; then
+		# Restore lua
+		unlink "$TERMUX_PREFIX"/bin/lua"$__LUA_VERSION"
+		mv "$TERMUX_PREFIX"/bin/lua"$__LUA_VERSION"{.bak,}
+	fi
 }
 
 termux_step_post_massage() {
-	# Remove lua, due to us moving it back and fourth the package
-	# thinks it is a newly compiled program
-	rm bin/lua5.3
+	if [ "$TERMUX_ON_DEVICE_BUILD" != true ]; then
+		# Remove lua, due to us moving it back and fourth, the build system
+		# thinks it is a newly compiled package.
+		rm bin/lua"$__LUA_VERSION"
+	fi
 }
