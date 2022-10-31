62ae90e8171ecaea52e1f0b4f1847170586b22d6
Kevin Williams <admin@utermux.dev>
2022-10-31 23:20:57 +0800

    fix(main/dnote): subpackages
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/dnote/build.sh
+++ packages/dnote/build.sh
@@ -3,13 +3,12 @@ TERMUX_PKG_DESCRIPTION="A simple command line notebook for programmers"
 TERMUX_PKG_LICENSE="AGPL-V3,GPL-3.0"
 TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
 TERMUX_PKG_VERSION=2.0.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/dnote/dnote/archive/refs/tags/server-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=28cc3bf93b3849b9a4a5e65e531f8a34d6be6048427d924c56ab8c7887676bad
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="dnote-server,dnote-client"
-TERMUX_PKG_SUGGESTS="postgresql"
+TERMUX_PKG_SUGGESTS="postgresql, dnote-server, dnote-client"
 
 termux_step_pre_configure() {
         termux_setup_nodejs
