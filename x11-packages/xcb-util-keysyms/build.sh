688759084e4fcf456a3c298983b98820df551967
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 04:53:58 +0900

    xcb-util-keysyms: Remove no longer necessary symlink
    %ci:no-build

--- x11-packages/xcb-util-keysyms/build.sh
+++ x11-packages/xcb-util-keysyms/build.sh
@@ -7,10 +7,3 @@ TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-keysyms-${TERMUX_PKG
 TERMUX_PKG_SHA256=7c260a5294412aed429df1da2f8afd3bd07b7cba3fec772fba15a613a6d5c638
 TERMUX_PKG_DEPENDS="libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
-
-termux_step_post_massage() {
-	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
-	if [ ! -e "./libxcb-keysyms.so.1" ]; then
-		ln -sf libxcb-keysyms.so libxcb-keysyms.so.1
-	fi
-}
