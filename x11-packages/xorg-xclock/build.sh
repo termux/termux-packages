833a3ca621c686c0f46b6c4743e646de50096c07
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 10:55:14 +0900

    xorg-xclock: Bump to 1.1.1

--- x11-packages/xorg-xclock/build.sh
+++ x11-packages/xorg-xclock/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X clock"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: MIT, HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.9
-TERMUX_PKG_REVISION=23
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xclock-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=cf461fb2c6f2ac42c54d8429ee2010fdb9a1442a370adfbfe8a7bfaf33c123bb
-TERMUX_PKG_DEPENDS="libx11, libxaw, libxft, libxkbfile, libxmu, libxrender, libxt"
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xclock-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=df7ceabf8f07044a2fde4924d794554996811640a45de40cb12c2cf1f90f742c
+TERMUX_PKG_DEPENDS="libiconv, libx11, libxaw, libxft, libxkbfile, libxmu, libxrender, libxt"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 TERMUX_PKG_CONFLICTS="xclock"
 TERMUX_PKG_REPLACES="xclock"
