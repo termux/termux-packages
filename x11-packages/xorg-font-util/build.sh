562f977e86732a6f05f9de166b203bd0c83a753e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-10-31 13:56:31 +0900

    xorg-font-util: Bump to 1.3.3

--- x11-packages/xorg-font-util/build.sh
+++ x11-packages/xorg-font-util/build.sh
@@ -1,11 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X.Org font utilities"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: MIT, BSD 2-Clause, UCD
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.2
-TERMUX_PKG_REVISION=21
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/font/font-util-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=3ad880444123ac06a7238546fa38a2a6ad7f7e0cc3614de7e103863616522282
+TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/font/font-util-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=e791c890779c40056ab63aaed5e031bb6e2890a98418ca09c534e6261a2eebd2
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-mapdir=${TERMUX_PREFIX}/share/fonts/util
