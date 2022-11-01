b51f0c8c05c2657bbf274e5dd466fee680a99be1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 17:40:12 +0900

    libvips: Explicitly disable gir
    %ci:no-build

--- packages/libvips/build.sh
+++ packages/libvips/build.sh
@@ -7,8 +7,14 @@ TERMUX_PKG_SRCURL=https://github.com/libvips/libvips/releases/download/v${TERMUX
 TERMUX_PKG_SHA256=41db5ed40a22ad50f8f37782e1ae5bac76f9f2e0b5731728f97c55f245f1da2a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, giflib, glib, harfbuzz, imagemagick, libc++, libcairo, libexif, libexpat, libheif, libjpeg-turbo, libjxl, libpng, librsvg, libtiff, libwebp, littlecms, openjpeg, pango"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-doc"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-introspection=yes
+"
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
