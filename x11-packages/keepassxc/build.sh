0794f701a34ee055d3adcbc608fd066dd900595c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 09:12:41 +0900

    keepassxc: Bump to 2.7.4

--- x11-packages/keepassxc/build.sh
+++ x11-packages/keepassxc/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://keepassxc.org/
 TERMUX_PKG_DESCRIPTION="Cross-platform community-driven port of Keepass password manager"
-TERMUX_PKG_LICENSE="GPL-2.0"
+# KeePassXC code is licensed under GPL-2 or GPL-3.
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE.BSD, LICENSE.CC0, LICENSE.GPL-2, LICENSE.GPL-3, LICENSE.LGPL-2.1, LICENSE.LGPL-3, LICENSE.MIT, LICENSE.NOKIA-LGPL-EXCEPTION, LICENSE.OFL"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.6
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.7.4
 TERMUX_PKG_SRCURL="https://github.com/keepassxreboot/keepassxc/releases/download/${TERMUX_PKG_VERSION}/keepassxc-${TERMUX_PKG_VERSION}-src.tar.xz"
-TERMUX_PKG_SHA256=3603b11ac39b289c47fac77fa150e05fd64b393d8cfdf5732dc3ef106650a4e2
-TERMUX_PKG_DEPENDS="argon2, libcurl, libgcrypt, libqrencode, libsodium, libxtst, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras"
+TERMUX_PKG_SHA256=560052961da0389327e759171f660230dfa4e0f4e1fab6139600fb85c6e5dece
+TERMUX_PKG_DEPENDS="argon2, botan, libc++, libqrencode, libx11, libxtst, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, readline, zlib"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWITH_XC_NETWORKING=ON
