fec5c67c0eaf405b32f763c051e9e82d8548c12e
Henrik Grimler <grimler@termux.dev>
2022-11-01 21:15:10 +0100

    scripts: bump toolchain after GLES header removal

--- scripts/build/termux_step_setup_toolchain.sh
+++ scripts/build/termux_step_setup_toolchain.sh
@@ -6,7 +6,7 @@ termux_step_setup_toolchain() {
 	# toolchain setup to ensure that everyone gets an updated
 	# toolchain
 	if [ "${TERMUX_NDK_VERSION}" = 25b ]; then
-		TERMUX_STANDALONE_TOOLCHAIN+="-v0"
+		TERMUX_STANDALONE_TOOLCHAIN+="-v1"
 		termux_setup_toolchain_25b
 	elif [ "${TERMUX_NDK_VERSION}" = 23c ]; then
 		TERMUX_STANDALONE_TOOLCHAIN+="-v2"
