85d90cb424a3ca67385439610dbdfdfbbae0eda2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 00:37:19 +0900

    xfce4-settings: Bump to 4.17.0

--- x11-packages/xfce4-settings/build.sh
+++ x11-packages/xfce4-settings/build.sh
@@ -2,15 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Settings manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=4.16
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
+_MAJOR_VERSION=4.17
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-settings/${_MAJOR_VERSION}/xfce4-settings-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=f299e6d26d8e142c0b7edeb9b8917d6bd50bc8647254ea585069c68f2bab8e64
-TERMUX_PKG_DEPENDS="exo, garcon, libnotify, libxfce4ui, libxklavier, lxde-icon-theme, libcanberra, gtk3, gsettings-desktop-schemas"
+TERMUX_PKG_SHA256=2838054e7d4d77275577051aa0e183963344d9affba087b65478211ba893cfa4
+TERMUX_PKG_DEPENDS="atk, exo, fontconfig, garcon, gdk-pixbuf, glib, gtk3, libcairo, libnotify, libx11, libxcursor, libxfce4ui, libxfce4util, libxi, libxklavier, libxrandr, pango, xfconf"
+TERMUX_PKG_RECOMMENDS="gsettings-desktop-schemas, libcanberra, lxde-icon-theme"
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/icons/hicolor/icon-theme.cache
 "
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-xrandr
 --enable-xcursor
