5919e6d90e96ada91a171f91ac973c1d6d8d16e1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 08:49:01 +0900

    evince: Adapt to termux_setup_gir
    %ci:no-build

--- x11-packages/evince/build.sh
+++ x11-packages/evince/build.sh
@@ -7,9 +7,15 @@ TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/evince/${_MAJOR_VERSION}/evince-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=66be0de4b47b1130486103988cc152c04aea95950ba3ef16dc20c2ef6b681d47
 TERMUX_PKG_DEPENDS="gdk-pixbuf, gtk3, libhandy, libarchive, djvulibre, texlive-bin, libspectre, poppler, poppler-data, libxml2, libtiff, libgxps, libsecret, gstreamer, gst-plugins-good"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dnautilus=false
--Dintrospection=false
+-Dintrospection=true
 -Dgtk_doc=false
 -Dps=enabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
