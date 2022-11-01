fd034729fc643dfdfc9974c4339a1a1a6f85fa58
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 17:11:05 +0900

    pidgin: Bump to 2.14.10

--- x11-packages/pidgin/build.sh
+++ x11-packages/pidgin/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://pidgin.im/
 TERMUX_PKG_DESCRIPTION="Multi-protocol instant messaging client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.14.9
+TERMUX_PKG_VERSION=2.14.10
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pidgin/pidgin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=1872d1629a26e9b775d9ec0bdd3d3fd348f721376f320b29bcd5ff3f0443222b
+TERMUX_PKG_SHA256=454b1b928bc6bcbb183353af30fbfde5595f2245a3423a1a46e6c97a2df22810
 TERMUX_PKG_DEPENDS="gtk2, libgnutls, libidn, libsasl, libsm, libxext, libxss"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
