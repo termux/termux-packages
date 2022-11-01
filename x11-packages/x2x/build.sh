2edbf86b9612663955948aafd52db7afd07fda63
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 09:28:20 +0900

    x2x: Bump to 1.30-10

--- x11-packages/x2x/build.sh
+++ x11-packages/x2x/build.sh
@@ -1,13 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dottedmag/x2x
 TERMUX_PKG_DESCRIPTION="Allows the keyboard, mouse on one X display to be used to control another X display"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20180709
-TERMUX_PKG_REVISION=23
-_COMMIT=e62a535f9bace33c8b61ea9ff95f040622cb34a2
-TERMUX_PKG_SRCURL=https://github.com/dottedmag/x2x/archive/${_COMMIT}.tar.gz
-TERMUX_PKG_SHA256=f21d064ed8d6952adbc9cae3261dda9b279a3b330c622a559757f78f5141a54c
-TERMUX_PKG_DEPENDS="libxtst"
+TERMUX_PKG_VERSION=1:1.30-10
+TERMUX_PKG_SRCURL=https://github.com/dottedmag/x2x/archive/refs/tags/debian/${TERMUX_PKG_VERSION#*:}.tar.gz
+TERMUX_PKG_SHA256=f41d5ed55d4b05fe28ab8c07bf41e19cdafcc6ecd08f588679aa0ee48f1eb627
+TERMUX_PKG_DEPENDS="libx11, libxext, libxtst"
 
 termux_step_pre_configure() {
 	./bootstrap.sh
