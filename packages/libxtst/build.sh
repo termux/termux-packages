17a7e4c651ed4012f714d6f82f3141e338435110
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 17:57:43 +0900

    libxtst: Remove no longer necessary symlink
    %ci:no-build

--- packages/libxtst/build.sh
+++ packages/libxtst/build.sh
@@ -8,10 +8,3 @@ TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXtst-$
 TERMUX_PKG_SHA256=84f5f30b9254b4ffee14b5b0940e2622153b0d3aed8286a3c5b7eeb340ca33c8
 TERMUX_PKG_DEPENDS="libx11, libxext, libxi"
 TERMUX_PKG_BUILD_DEPENDS="libxfixes, xorg-util-macros"
-
-termux_step_post_massage() {
-	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
-	if [ ! -e "./libXtst.so.6" ]; then
-		ln -sf libXtst.so libXtst.so.6
-	fi
-}
