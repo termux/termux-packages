457f0c4b9f28a163b518ef5dc3dea8e2877e7017
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 15:12:15 +0900

    kwindowsystem: Bump to 5.99.0

--- x11-packages/kwindowsystem/build.sh
+++ x11-packages/kwindowsystem/build.sh
@@ -1,15 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="KDE Access to window manager"
-TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE="LGPL-2.1, LGPL-3.0, MIT"
+TERMUX_PKG_LICENSE_FILE="\
+LICENSES/CC0-1.0.txt
+LICENSES/LGPL-2.1-only.txt
+LICENSES/LGPL-2.1-or-later.txt
+LICENSES/LGPL-3.0-only.txt
+LICENSES/LicenseRef-KDE-Accepted-LGPL.txt
+LICENSES/MIT.txt"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.71.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=5.99.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kwindowsystem-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=a3613aea6fa73ebc53f28c011a6bca31ed157e29f85df767e617c44399360cda
-TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtx11extras"
+TERMUX_PKG_SHA256=f4d13fbf562c05075bb59f997a0de567f96c008a379b7c796d2ca26c01a7f877
+TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libxfixes, qt5-qtbase, qt5-qtx11extras, xcb-util-keysyms"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qttools-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}"
-
-termux_step_install_license() {
-    install -Dm644 "${TERMUX_PKG_SRCDIR}/COPYING.LIB" "${TERMUX_PREFIX}/share/doc/${TERMUX_PKG_NAME}/LICENSE"
-}
