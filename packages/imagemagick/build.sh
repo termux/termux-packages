52faa2dd3a4d8825d20e2a9fb8a9d2d2602d2ac0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 03:11:07 +0900

    imagemagick: Bump to 7.1.0.51
    * Enable OpenEXR support

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
-_VERSION=7.1.0-50
+_VERSION=7.1.0-51
 TERMUX_PKG_VERSION=${_VERSION//-/.}
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3fac7d2be943f754822c19f80b2b1fa431b1cd68efb35a4ba8eb309fb5bfa79f
-TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libheif, libjpeg-turbo, libjxl, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib, zstd"
+TERMUX_PKG_SHA256=4333ef2fe63f2510988af82b726f5b1919ebd54037ea6674566c69fcceb67e11
+TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, gdk-pixbuf, glib, harfbuzz, libandroid-shmem, libandroid-support, libbz2, libc++, libcairo, libheif, libjpeg-turbo, libjxl, liblzma, libpixman, libpng, librsvg, libtiff, libwebp, libx11, libxcb, libxext, libxml2, libxrender, littlecms, openexr, openjpeg, pango, zlib"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -18,6 +18,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-rsvg=yes
 --with-lzma
 --with-jxl=yes
+--with-openexr
 --with-fftw
 --disable-openmp
 ac_cv_func_ftime=no
