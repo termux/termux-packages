f018be751cd2b30cc69df84943e94783178e18f4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-01 09:01:24 +0900

    fltk: Bump to 1.3.8

--- x11-packages/fltk/build.sh
+++ x11-packages/fltk/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://www.fltk.org/
 TERMUX_PKG_DESCRIPTION="Graphical user interface toolkit for X"
 TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.7
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.3.8
 TERMUX_PKG_SRCURL=https://www.fltk.org/pub/fltk/${TERMUX_PKG_VERSION}/fltk-${TERMUX_PKG_VERSION}-source.tar.gz
-TERMUX_PKG_SHA256=5d2ccb7ad94e595d3d97509c7a931554e059dd970b7b29e6fd84cb70fd5491c6
+TERMUX_PKG_SHA256=f3c1102b07eb0e7a50538f9fc9037c18387165bc70d4b626e94ab725b9d4d1bf
 TERMUX_PKG_DEPENDS="fontconfig, glu, libc++, libjpeg-turbo, libpng, libx11, libxcursor, libxext, libxfixes, libxft, libxinerama, libxrender, mesa, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
