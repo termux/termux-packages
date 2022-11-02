72d4ebdfdb220e90a8ca0ed7c958c0a71876e6da
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 05:01:09 +0900

    mousepad: Depend on libxfce4ui

--- x11-packages/mousepad/build.sh
+++ x11-packages/mousepad/build.sh
@@ -4,9 +4,10 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.5
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/mousepad/${_MAJOR_VERSION}/mousepad-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=6ebaf38d52bee5560d9650c52a693f8a6ed0a67d88cc938d73f7d5ce13552bad
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk3, gtksourceview4, harfbuzz, libandroid-shmem, libcairo, libpixman, libpng, libx11, libxcb, libxext, libxrender, pango, zlib"
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, gtksourceview4, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfconf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-gtksourceview4
 "
