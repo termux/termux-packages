57cfa88336076093848535107be0de54a32087ae
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 00:48:38 +0900

    geany: Bump to 1.38

--- x11-packages/geany/build.sh
+++ x11-packages/geany/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.geany.org/
 TERMUX_PKG_DESCRIPTION="Fast and lightweight IDE"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.37.1
-TERMUX_PKG_REVISION=10
-TERMUX_PKG_SRCURL=https://download.geany.org/geany-${TERMUX_PKG_VERSION/.0}.tar.bz2
-TERMUX_PKG_SHA256=18c5756444c1d8bcd737c8ecfd4ef0b3607c924fc02560d4e8b78f6121531a18
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, fribidi, gdk-pixbuf, glib, gtk3, harfbuzz, libandroid-glob, libandroid-shmem, libbz2, libc++, libcairo, libffi, libgraphite, liblzma, libpixman, libpng, libuuid, libvte, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, libxrender, pango, pcre, zlib"
+TERMUX_PKG_VERSION=1.38
+TERMUX_PKG_SRCURL=https://download.geany.org/geany-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=abff176e4d48bea35ee53037c49c82f90b6d4c23e69aed6e4a5ca8ccd3aad546
+# libvte is dlopen(3)ed:
+TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk3, harfbuzz, libandroid-glob, libandroid-shmem, libc++, libcairo, libpixman, libpng, libvte, libx11, libxcb, libxext, libxrender, pango, zlib"
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk3 --enable-vte"
 
