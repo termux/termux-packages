097efac8c9fb28e8ebb1bdca34fb2e0bf5db47de
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 11:31:39 +0900

    sdl-image: Bump to 1.2.12-p20220527

--- x11-packages/sdl-image/build.sh
+++ x11-packages/sdl-image/build.sh
@@ -1,9 +1,23 @@
 TERMUX_PKG_HOMEPAGE=https://www.libsdl.org/projects/SDL_image/
 TERMUX_PKG_DESCRIPTION="A simple library to load images of various formats as SDL surfaces"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.12
-TERMUX_PKG_REVISION=20
-TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_image/release/SDL_image-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699
-TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff, sdl, zlib"
+_COMMIT=633dc522f5114f6d473c910dace62e8ca27a1f7d
+_COMMIT_DATE=20220527
+TERMUX_PKG_VERSION=1.2.12-p${_COMMIT_DATE}
+TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_image.git
+TERMUX_PKG_GIT_BRANCH=SDL-1.2
+TERMUX_PKG_DEPENDS="libjpeg-turbo, libpng, libtiff, sdl"
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
+	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
+		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
+		echo -n " different from what is expected to be; should end"
+		echo " with \"${pdate}\"."
+		return 1
+	fi
+}
