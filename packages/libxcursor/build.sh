6875f0fe82c440677826fe1095286b6e18f291ef
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 12:37:11 +0900

    libxcursor: Adapt to SONAME change in X11 libs

--- packages/libxcursor/build.sh
+++ packages/libxcursor/build.sh
@@ -1,9 +1,12 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X cursor management library"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXcursor-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=46c143731610bafd2070159a844571b287ac26192537d047a39df06155492104
 TERMUX_PKG_DEPENDS="libx11, libxfixes, libxrender"
