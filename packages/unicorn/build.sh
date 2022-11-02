156f55002b532d2f2fc33ae3441c1bcec76eb978
Kevin Williams <admin@utermux.dev>
2022-11-02 12:20:28 +0800

    upgpkg(main/unicorn): 2.0.1
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/unicorn/build.sh
+++ packages/unicorn/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.unicorn-engine.org/
-TERMUX_PKG_DESCRIPTION="Unicorn is a lightweight multi-platform, multi-architecture CPU emulator framework."
+TERMUX_PKG_DESCRIPTION="Unicorn is a lightweight multi-platform, multi-architecture CPU emulator framework"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_VERSION=2.0.1
 TERMUX_PKG_SRCURL=https://github.com/unicorn-engine/unicorn/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=67b445c760e2bbac663e8c8bc410e43311c7fc92df4dfa8d90e06a021d07f634
+TERMUX_PKG_SHA256=0c1586f6b079e705d760403141db0ea65d0e22791cf0f43f38172d49497923fd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="unicorn-dev"
 TERMUX_PKG_REPLACES="unicorn-dev"
