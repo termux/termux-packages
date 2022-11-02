344799bfa57c3a15dcedfd1cb3517657a25a3a75
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 14:01:27 +0900

    fluxbox: Adapt to SONAME change in X11 libs

--- x11-packages/fluxbox/build.sh
+++ x11-packages/fluxbox/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="A lightweight and highly-configurable window manager"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.7
-TERMUX_PKG_REVISION=34
+TERMUX_PKG_REVISION=35
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/fluxbox/fluxbox-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=fc8c75fe94c54ed5a5dd3fd4a752109f8949d6df67a48e5b11a261403c382ec0
-TERMUX_PKG_DEPENDS="feh, fribidi, imlib2, libc++, libxft, libxinerama, libxpm, libxrandr, xorg-xmessage"
-TERMUX_PKG_RECOMMENDS="aterm"
+TERMUX_PKG_DEPENDS="fontconfig, fribidi, imlib2, libc++, libiconv, libx11, libxext, libxft, libxinerama, libxpm, libxrandr, libxrender, xorg-xmessage"
+TERMUX_PKG_RECOMMENDS="aterm, feh"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
