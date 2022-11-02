125557c9090dfb0fb6230a1006f174718bf90845
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 11:36:44 +0900

    sdl-net: Bump to 1.2.8-p20221010

--- x11-packages/sdl-net/build.sh
+++ x11-packages/sdl-net/build.sh
@@ -1,11 +1,25 @@
 TERMUX_PKG_HOMEPAGE=https://www.libsdl.org/projects/SDL_net/
 TERMUX_PKG_DESCRIPTION="A small sample cross-platform networking library"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.8
-TERMUX_PKG_REVISION=38
-TERMUX_PKG_SRCURL=https://www.libsdl.org/projects/SDL_net/release/SDL_net-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5f4a7a8bb884f793c278ac3f3713be41980c5eedccecff0260411347714facb4
-TERMUX_PKG_DEPENDS="libandroid-glob, libflac, libogg, libsndfile, libvorbis, sdl"
+_COMMIT=091c95c031769f48d3ffaacddfdea1af999f4446
+_COMMIT_DATE=20221010
+TERMUX_PKG_VERSION=1.2.8-p${_COMMIT_DATE}
+TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_net.git
+TERMUX_PKG_GIT_BRANCH=SDL-1.2
+TERMUX_PKG_DEPENDS="sdl"
 TERMUX_PKG_CONFLICTS="libsdl-net"
 TERMUX_PKG_REPLACES="libsdl-net"
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
+	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
+		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
+		echo -n " different from what is expected to be; should end"
+		echo " with \"${pdate}\"."
+		return 1
+	fi
+}
