fffe77142a1ed7e5a485550301bf42ec03c9dac6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 12:43:12 +0900

    libxrandr: Adapt to SONAME change in X11 libs

--- packages/libxrandr/build.sh
+++ packages/libxrandr/build.sh
@@ -1,10 +1,12 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 RandR extension library"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.2
-TERMUX_PKG_REVISION=24
+TERMUX_PKG_REVISION=25
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXrandr-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=8aea0ebe403d62330bb741ed595b53741acf45033d3bda1792f1d4cc3daee023
 TERMUX_PKG_DEPENDS="libx11, libxext, libxrender"
