531e8fbbb899b9baf269bd7c97bba22962577cab
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 12:40:39 +0900

    libxft: Adapt to SONAME change in X11 libs

--- packages/libxft/build.sh
+++ packages/libxft/build.sh
@@ -1,9 +1,12 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="FreeType-based font drawing library for X"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXft-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=60a6e7319fc938bbb8d098c9bcc86031cc2327b5d086d3335fc5c76323c03022
-TERMUX_PKG_DEPENDS="fontconfig, freetype, libbz2, liblzma, libpng, libuuid, libx11, libxau, libxcb, libxdmcp, libxml2, libxrender"
+TERMUX_PKG_DEPENDS="fontconfig, freetype, libx11, libxrender"
