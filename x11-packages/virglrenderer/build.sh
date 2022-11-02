cdbe704d8484a56e6128609a64041b21987f8f3c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 01:26:54 +0900

    virglrenderer: Bump to 0.10.1

--- x11-packages/virglrenderer/build.sh
+++ x11-packages/virglrenderer/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://virgil3d.github.io/
 TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.1
+TERMUX_PKG_VERSION=0.10.1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=dd4a8008ca7bcaaf56666c94fcd738d705cdeda6313a82b3cea78bc3fb1b1ba5
+TERMUX_PKG_SHA256=081dada3d66a8c626f92ae1eb3aa4440feb9ea34e178909f45840a9322f0a17e
 TERMUX_PKG_DEPENDS="libdrm, libepoxy"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=glx"
