0110964d3ee7e876229c7233446c506dd29a39b5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 23:22:56 +0900

    libprotobuf: Bump to 3.21.9

--- packages/libprotobuf/protobuf.subpackage.sh
+++ packages/libprotobuf/protobuf.subpackage.sh
@@ -1,3 +1,8 @@
-TERMUX_SUBPKG_INCLUDE="bin/ lib/libprotobuf-lite.so* lib/libprotoc.so*"
+TERMUX_SUBPKG_INCLUDE="
+bin/
+lib/libprotobuf-lite.so*
+lib/libprotoc.so*
+lib/pkgconfig/protobuf-lite.pc
+"
 TERMUX_SUBPKG_DESCRIPTION="Compiler for protocol buffer definition files"
 
