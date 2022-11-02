dfdc4fc2428be33f23bdbac49279ac435c1be3fe
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 13:53:25 +0900

    keybinder: Adapt to SONAME change in X11 libs

--- x11-packages/keybinder/build.sh
+++ x11-packages/keybinder/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library for registering global keyboard shortcuts"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kupferlauncher/keybinder/releases/download/keybinder-3.0-v${TERMUX_PKG_VERSION}/keybinder-3.0-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e6e3de4e1f3b201814a956ab8f16dfc8a262db1937ff1eee4d855365398c6020
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libx11, libxext, libxrender, pango"
