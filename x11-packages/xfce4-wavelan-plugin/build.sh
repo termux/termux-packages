8deb58aff6f2c119ca429173984bd227290708ed
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 00:25:43 +0900

    xfce4-wavelan-plugin: Bump to 0.6.3

--- x11-packages/xfce4-wavelan-plugin/build.sh
+++ x11-packages/xfce4-wavelan-plugin/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-wavelan-plugin/start
 TERMUX_PKG_DESCRIPTION="wavelan status plugin for the Xfce4 panel"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
-TERMUX_PKG_VERSION=0.6.2
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-wavelan-plugin/0.6/xfce4-wavelan-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ea7aa36650c596b1a29567d100776c68ed732aaf0f48c92245c4466058b5481d
-TERMUX_PKG_DEPENDS="gtk3, atk, libcairo, pango, gdk-pixbuf, glib, libxfce4util, libxfce4ui, xfce4-panel, harfbuzz"
+_MAJOR_VERSION=0.6
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-wavelan-plugin/${_MAJOR_VERSION}/xfce4-wavelan-plugin-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=61c0c2f56cb70872d403b770dd76349df9ff24c0dbe905ee1b4f913c34d8f72b
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfce4-panel"
 TERMUX_PKG_BUILD_IN_SRC=true
