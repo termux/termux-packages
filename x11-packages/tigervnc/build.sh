77f166abf7f3757aeb5b206ebc1ed4b676726539
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 03:46:47 +0900

    tigervnc: Fix staticsplit

--- x11-packages/tigervnc/build.sh
+++ x11-packages/tigervnc/build.sh
@@ -5,14 +5,13 @@ TERMUX_PKG_MAINTAINER="@termux"
 # No update anymore. v1.11.x requires support of PAM.
 TERMUX_PKG_VERSION=(1.10.1
 		    1.20.14)
-TERMUX_PKG_REVISION=26
+TERMUX_PKG_REVISION=27
 TERMUX_PKG_SRCURL=(https://github.com/TigerVNC/tigervnc/archive/v${TERMUX_PKG_VERSION}.tar.gz
 		   https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz)
 TERMUX_PKG_SHA256=(19fcc80d7d35dd58115262e53cac87d8903180261d94c2a6b0c19224f50b58c4
 		   5cc5b70b9be89443e2594b93656c60bd5e82cd7f01deb4ce4faf81dcf546a16b)
-
-TERMUX_PKG_DEPENDS="freetype, libandroid-shmem, libbz2, libc++, libdrm, libexpat, libgnutls, libjpeg-turbo, libpixman, libpng, libuuid, libx11, libxau, libxcb, libxdamage, libxdmcp, libxext, libxxf86vm, libxfixes, libxfont2, libxshmfence, mesa, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp"
-TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-font-util, xorg-util-macros, xtrans"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libgnutls, libjpeg-turbo, libpixman, libx11, libxau, libxdamage, libxdmcp, libxext, libxfixes, libxfont2, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp, zlib"
+TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-util-macros, xorgproto, xtrans"
 TERMUX_PKG_SUGGESTS="aterm, xorg-twm"
 
 TERMUX_PKG_FOLDERNAME=tigervnc-${TERMUX_PKG_VERSION}
--- x11-packages/tigervnc/tigervnc-viewer.subpackage.sh
+++ x11-packages/tigervnc/tigervnc-viewer.subpackage.sh
@@ -4,7 +4,7 @@ share/applications
 share/icons
 share/man/man1/vncviewer.1
 "
-
 TERMUX_SUBPKG_DESCRIPTION="A VNC viewer from TigerVNC package"
-TERMUX_SUBPKG_DEPENDS="fltk, fontconfig, libandroid-shmem, libc++, libgnutls, libice, libjpeg-turbo, libsm, libx11, libxcursor, libxext, libxfixes, libxft, libxinerama, libxrender"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
+TERMUX_SUBPKG_DEPENDS="fltk, libandroid-shmem, libc++, libgnutls, libjpeg-turbo, libx11, libxext, libxrender, zlib"
 TERMUX_SUBPKG_CONFLICTS="tigervnc (<< 1.9.0-4)"
