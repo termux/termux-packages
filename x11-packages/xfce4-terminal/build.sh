3c6155b42f7a5c9413475c40626bc673dff2d358
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 00:23:09 +0900

    xfce4-terminal: Bump to 1.0.4

--- x11-packages/xfce4-terminal/build.sh
+++ x11-packages/xfce4-terminal/build.sh
@@ -2,8 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/terminal/start
 TERMUX_PKG_DESCRIPTION="Terminal Emulator for the XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.1
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-terminal/0.9/xfce4-terminal-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=8746cf1435f6da7f508b0af126d21133ccb69ca0b623339df5559bc5f8177db2
-TERMUX_PKG_DEPENDS="desktop-file-utils, exo, hicolor-icon-theme, garcon, libxfce4ui, xfconf, libvte"
+_MAJOR_VERSION=1.0
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-terminal/${_MAJOR_VERSION}/xfce4-terminal-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=78e55957af7c6fc1f283e90be33988661593a4da98383da1b0b54fdf6554baf4
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libvte, libx11, libxfce4ui, libxfce4util, pango, xfconf"
+TERMUX_PKG_RECOMMENDS="desktop-file-utils, hicolor-icon-theme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk-doc-html=no"
