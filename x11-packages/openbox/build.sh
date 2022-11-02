8583056b33457d30aeb48d731a380671def90b9a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-02 14:01:55 +0900

    openbox: Adapt to SONAME change in X11 libs

--- x11-packages/openbox/build.sh
+++ x11-packages/openbox/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Highly configurable and lightweight X11 window manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.6.1
-TERMUX_PKG_REVISION=54
+TERMUX_PKG_REVISION=55
 TERMUX_PKG_SRCURL=http://openbox.org/dist/openbox/openbox-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8b4ac0760018c77c0044fab06a4f0c510ba87eae934d9983b10878483bde7ef7
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-session-management"
-TERMUX_PKG_DEPENDS="bash, fontconfig, freetype, fribidi, gdk-pixbuf, glib, harfbuzz, imlib2, libandroid-shmem, libbz2, libcairo, libcroco, libffi, libgraphite, libice, liblzma, libpixman, libpng, librsvg, libsm, libuuid, libx11, libxau, libxcb, libxcursor, libxdmcp, libxext, libxfixes, libxft, libxinerama, libxml2, libxrandr, libxrender, pango, pcre, startup-notification, xcb-util, zlib"
+TERMUX_PKG_DEPENDS="bash, fontconfig, freetype, gdk-pixbuf, glib, gtk3, harfbuzz, imlib2, libandroid-shmem, libcairo, libice, libpixman, libpng, librsvg, libsm, libx11, libxcb, libxcursor, libxext, libxft, libxinerama, libxml2, libxrandr, libxrender, pango, startup-notification, zlib"
 
 # Configuration utility.
 TERMUX_PKG_RECOMMENDS="obconf"
