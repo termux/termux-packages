ea25d886e1836f05c2af3465e19c4e6b8361c764
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 05:14:53 +0900

    libpixman: Bump to 0.42.2

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -2,16 +2,15 @@ TERMUX_PKG_HOMEPAGE=http://www.pixman.org/
 TERMUX_PKG_DESCRIPTION="Low-level library for pixel manipulation"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.40.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.42.2
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc
+TERMUX_PKG_SHA256=ea1480efada2fd948bc75366f7c349e1c96d3297d09a3fe62626e38e234a625e
 TERMUX_PKG_BREAKS="libpixman-dev"
 TERMUX_PKG_REPLACES="libpixman-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
 
 termux_step_pre_configure() {
-	if [ "$TERMUX_ARCH" = arm ]; then
+	if [ "$TERMUX_ARCH" = arm ] || [ "$TERMUX_ARCH" = aarch64 ]; then
 		termux_setup_gnu_as_23c
 		CFLAGS+=" -fno-integrated-as"
 	fi
