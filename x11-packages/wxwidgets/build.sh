66f78c3001d8d0d85b80d07adcfb49b9780fb36a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 15:13:26 +0900

    wxwidgets: Bump to 3.2.1

--- x11-packages/wxwidgets/build.sh
+++ x11-packages/wxwidgets/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="A free and open source cross-platform C++ framework for 
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="docs/gpl.txt, docs/lgpl.txt, docs/licence.txt, docs/licendoc.txt, docs/preamble.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.1.6
+TERMUX_PKG_VERSION=3.2.1
 TERMUX_PKG_SRCURL=https://github.com/wxWidgets/wxWidgets/releases/download/v${TERMUX_PKG_VERSION}/wxWidgets-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4980e86c6494adcd527a41fc0a4e436777ba41d1893717d7b7176c59c2061c25
-TERMUX_PKG_DEPENDS="glu, gtk3, libcurl, libexpat, libiconv, libjpeg-turbo, liblzma, libnotify, libpng, libsecret, libsm, libtiff, mesa, sdl2"
+TERMUX_PKG_SHA256=c229976bb413eb88e45cb5dfb68b27890d450149c09b331abd751e7ae0f5fa66
+TERMUX_PKG_DEPENDS="fontconfig, gdk-pixbuf, glib, glu, gtk3, libandroid-execinfo, libc++, libcairo, libcurl, libexpat, libiconv, libjpeg-turbo, libnotify, libpng, libsecret, libsm, libtiff, libx11, libxtst, libxxf86vm, mesa, pango, pcre2, sdl2, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-option-checking
 --disable-mediactrl
