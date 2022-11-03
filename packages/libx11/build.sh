f8bdd05b0ada30f9a421ed72594f6e984e1917be
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-04 06:55:30 +0900

    libx11: Bump to 1.8.1

--- packages/libx11/build.sh
+++ packages/libx11/build.sh
@@ -2,15 +2,20 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 client-side library"
 TERMUX_PKG_LICENSE="MIT, X11"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.7.5
+TERMUX_PKG_VERSION=1.8.1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=5a1847e2e2c248e1a203a24a1338a5586d6f48472eac58f6f08539110a965151
+TERMUX_PKG_SHA256=1bc41aa1bbe01401f330d76dfa19f386b79c51881c7bbfee9eb4e27f22f2d9f7
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros, xtrans"
 TERMUX_PKG_RECOMMENDS="xorg-xauth"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
 
-termux_step_post_make_install() {
-	ln -sfr "${TERMUX_PREFIX}/lib/libX11.so" "${TERMUX_PREFIX}/lib/libX11.so.6"
+termux_step_post_massage() {
+	# Seems like some programs in the wild try to dlopen(3) `libX11.so.6`.
+	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
+	if [ ! -e "./libX11.so.6" ]; then
+		ln -sf libX11.so libX11.so.6
+	fi
 }
