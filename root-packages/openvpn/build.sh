1e7ff2252bd67eb482ba63419b42ecb374a4fa9a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-04 05:32:07 +0900

    openvpn: Bump to 2.5.8

--- root-packages/openvpn/build.sh
+++ root-packages/openvpn/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://openvpn.net
 TERMUX_PKG_DESCRIPTION="An easy-to-use, robust, and highly configurable VPN (Virtual Private Network)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.4.12
+TERMUX_PKG_VERSION=2.5.8
 TERMUX_PKG_SRCURL=https://swupdate.openvpn.net/community/releases/openvpn-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7426b99b2058b942552af2680ee58546fbf63712992557328bd0014093aa7da4
-TERMUX_PKG_DEPENDS="liblz4, liblzo, net-tools, openssl-1.1"
+TERMUX_PKG_SHA256=2bbd0026469902037ee6499b68283d5ab36c74e36cae3112082cfdf6c77a0c57
+TERMUX_PKG_DEPENDS="liblz4, liblzo, net-tools, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -21,13 +21,6 @@ ROUTE=$TERMUX_PREFIX/bin/route
 IPROUTE=$TERMUX_PREFIX/bin/ip
 NETSTAT=$TERMUX_PREFIX/bin/netstat
 "
-
-termux_step_pre_configure() {
-	CFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CFLAGS"
-	CPPFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CPPFLAGS"
-	CXXFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CXXFLAGS"
-	LDFLAGS="-L$TERMUX_PREFIX/lib/openssl-1.1 -Wl,-rpath=$TERMUX_PREFIX/lib/openssl-1.1 $LDFLAGS"	
-}
 
 termux_step_post_make_install() {
 	# Examples.
