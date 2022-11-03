3aa2f1be596a711b7d8dd68990be9bf272ccb913
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-04 06:11:19 +0900

    nmap: Bump to 7.93

--- packages/nmap/build.sh
+++ packages/nmap/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nmap.org/
 TERMUX_PKG_DESCRIPTION="Utility for network discovery and security auditing"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.92
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=7.93
 TERMUX_PKG_SRCURL=https://nmap.org/dist/nmap-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a5479f2f8a6b0b2516767d2f7189c386c1dc858d997167d7ec5cfc798c7571a1
+TERMUX_PKG_SHA256=55bcfe4793e25acc96ba4274d8c4228db550b8e8efd72004b38ec55a2dd16651
 TERMUX_PKG_DEPENDS="libc++, libpcap, pcre, openssl, resolv-conf, liblua53, libssh2, zlib"
 TERMUX_PKG_RECOMMENDS="nmap-ncat"
 # --without-nmap-update to avoid linking against libsvn_client:
