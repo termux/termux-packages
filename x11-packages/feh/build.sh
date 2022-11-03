d58075f5d34c4cf2dda593745333e332ec34c6b3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-03 12:59:26 +0900

    feh: Bump to 3.9.1

--- x11-packages/feh/build.sh
+++ x11-packages/feh/build.sh
@@ -1,11 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://feh.finalrewind.org/
 TERMUX_PKG_DESCRIPTION="Fast and light imlib2-based image viewer"
-TERMUX_PKG_LICENSE="MIT"
+# License: MIT-feh
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.8
+TERMUX_PKG_VERSION=3.9.1
 TERMUX_PKG_SRCURL=https://feh.finalrewind.org/feh-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=7f3c34552b39336d7ebee2d7c4bf5697aaaa2c6c102c357f6e82ea240bd62ba9
-TERMUX_PKG_DEPENDS="imlib2, libandroid-shmem, libcurl, libexif, libjpeg-turbo, libpng, libx11, libxinerama"
+TERMUX_PKG_SHA256=455c92711b588af149b945edc5c145f3e9aa137ed9689dabed49d5e4acac75fa
+TERMUX_PKG_DEPENDS="imlib2, libcurl, libexif, libjpeg-turbo, libpng, libx11, libxinerama"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="exif=1 help=1 verscmp=0"
