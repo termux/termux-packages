0e0c0843b5adc6399cd989a9aa2616b659eb6b14
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-04 06:38:56 +0900

    libcairo: Bump to 1.17.6

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -2,27 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://cairographics.org
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.16.0
-TERMUX_PKG_REVISION=12
-TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/cairo-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331
+TERMUX_PKG_VERSION=1.17.6
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/cairo/cairo/-/archive/${TERMUX_PKG_VERSION}/cairo-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=90496d135c9ef7612c98f8ee358390cdec0825534573778a896ea021155599d2
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libandroid-shmem, liblzo, libpixman, libpng, libx11, libxcb, libxext, libxrender, zlib"
 TERMUX_PKG_BREAKS="libcairo-dev, libcairo-gobject"
 TERMUX_PKG_REPLACES="libcairo-dev, libcairo-gobject"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-gtk-doc-html
---disable-gl
---enable-gobject
---enable-pdf
---enable-svg
---enable-ps
+-Dgl-backend=disabled
+-Dpng=enabled
+-Dzlib=enabled
+-Dglib=enabled
+-Dgtk_doc=false
 "
 
-TERMUX_PKG_RM_AFTER_INSTALL="share/gtk-doc/html"
-
 termux_step_pre_configure() {
-	autoreconf -fi
-
 	LDFLAGS+=" -landroid-shmem"
 }
