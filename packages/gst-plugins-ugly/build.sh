41df3498cd3a25725c98dc9a0f91690d932ec9d8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-04 11:35:50 +0900

    gst-plugins-ugly: Adjust dependency
    %ci:no-build

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.20.3
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8caa20789a09c304b49cf563d33cca9421b1875b84fcc187e4a385fa01d6aefd
-TERMUX_PKG_DEPENDS="gst-plugins-base, libx264"
+TERMUX_PKG_DEPENDS="glib, gst-plugins-base"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=disabled
 "
