b70e211966f7e905a517a93f43674074c78df01b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-04 18:11:04 +0900

    zenity: Bump to 3.43.0

--- x11-packages/zenity/build.sh
+++ x11-packages/zenity/build.sh
@@ -1,12 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/zenity
 TERMUX_PKG_DESCRIPTION="a rewrite of gdialog, the GNOME port of dialog"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.32.0
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/zenity/${TERMUX_PKG_VERSION:0:4}/zenity-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=e786e733569c97372c3ef1776e71be7e7599ebe87e11e8ad67dcc2e63a82cd95
-TERMUX_PKG_DEPENDS="glib, gtk3"
-termux_step_pre_configure() {
-	autoreconf -fi
-}
+_MAJOR_VERSION=3.43
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/zenity/${_MAJOR_VERSION}/zenity-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=b0d7ca1e0c1868fa18f05c210260d8a7be1f08ee13b7f5cfdbab9b61fa16f833
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libnotify, libx11, pango, webkit2gtk-4.1"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dlibnotify=true
+-Dwebkitgtk=true
+"
