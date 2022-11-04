eb9e2eeb6727fd2e82578fe8e45ead8cf66163a2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-05 07:05:40 +0900

    loqui: Bump to 0.7.0

--- x11-packages/loqui/build.sh
+++ x11-packages/loqui/build.sh
@@ -1,16 +1,18 @@
-TERMUX_PKG_HOMEPAGE=https://launchpad.net/loqui
-TERMUX_PKG_DESCRIPTION="IRC client for the Gtk+-2.0 environment"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_HOMEPAGE=https://github.com/sunnyone/loqui
+TERMUX_PKG_DESCRIPTION="IRC client for Gtk environment"
+TERMUX_PKG_LICENSE="LGPL-2.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.6.4
-TERMUX_PKG_REVISION=26
-TERMUX_PKG_SRCURL=https://launchpad.net/loqui/${TERMUX_PKG_VERSION:0:3}/${TERMUX_PKG_VERSION}/+download/loqui-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d0718f0eb095fbfe2af1b4ca5a0d05cd85e969322be3f5bc9fad26f042910b36
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk2, libandroid-shmem, libcairo, libffi, pango, pcre"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-glibtest --disable-gtktest"
+TERMUX_PKG_VERSION=0.7.0
+TERMUX_PKG_SRCURL=https://github.com/sunnyone/loqui/releases/download/${TERMUX_PKG_VERSION}/loqui-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c593211d6bb477d5477ec9b81143e3faf96e859ad2edaf527fbc370333e5e0e7
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, pango"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+GLIB_GENMARSHAL=glib-genmarshal
+--disable-glibtestr
+--disable-gtktest
+"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
 
 termux_step_pre_configure() {
-	export CFLAGS="${CFLAGS} -Wno-return-type"
-	export LIBS="-landroid-shmem"
+	CFLAGS+=" -Wno-error=return-type"
 }
