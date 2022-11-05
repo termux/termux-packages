dc776e5dd00dd2fc07a125fe382dd1667f21d37b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-05 12:53:56 +0900

    ghex: Bump to 43.0

--- x11-packages/ghex/build.sh
+++ x11-packages/ghex/build.sh
@@ -2,7 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Ghex
 TERMUX_PKG_DESCRIPTION="A simple binary editor for the Gnome desktop."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.41.1
-TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/ghex/-/archive/${TERMUX_PKG_VERSION}/ghex-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1a003c84d020518c1507b067fd095b96f98cfe8c43af5084c63db5f90702b975
-TERMUX_PKG_DEPENDS="atk, gtk3, libiconv, libxml2"
+_MAJOR_VERSION=43
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/ghex/${_MAJOR_VERSION}/ghex-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=866c0622c66fdb5ad2a475e9cfcccb219a1c6431f009acb2291d43f2140b147e
+TERMUX_PKG_DEPENDS="glib, gtk4, libadwaita, libcairo, pango"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DISABLE_GIR=false
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=enabled
+"
+
+termux_step_pre_configure() {
+	termux_setup_gir
+}
