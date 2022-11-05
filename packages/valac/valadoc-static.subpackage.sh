7625cbb8085fba3a2dd4f755182e64aac2e74684
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-06 06:58:06 +0900

    valac: Separate valadoc-static as a subpackage

---
+++ packages/valac/valadoc-static.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+lib/libvaladoc*.a
+lib/libvaladoc*.la
+lib/valadoc*/**/*.a
+lib/valadoc*/**/*.la
+"
+TERMUX_SUBPKG_DESCRIPTION="Static libraries for valadoc"
+TERMUX_SUBPKG_DEPENDS="valadoc"
