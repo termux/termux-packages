0f7d40a4f6a02521a21246017df55c78c86bc15c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-05 10:25:32 +0900

    tvheadend: Depend on libandroid-execinfo

--- packages/tvheadend/build.sh
+++ packages/tvheadend/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="TV streaming server for Linux and Android supporting DVB
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION=4.2.8
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL="https://github.com/tvheadend/tvheadend/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=1aef889373d5fad2a7bd2f139156d4d5e34a64b6d38b87b868a2df415f01f7ad
-TERMUX_PKG_DEPENDS="dbus, libdvbcsa, libiconv, openssl, tvheadend-data, zlib"
+TERMUX_PKG_DEPENDS="dbus, libandroid-execinfo, libdvbcsa, libiconv, openssl, tvheadend-data, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -23,6 +23,7 @@ termux_step_pre_configure() {
 	termux_setup_cmake
 
 	CFLAGS=" -I$TERMUX_PKG_BUILDDIR/src $CFLAGS $CPPFLAGS -fcommon"
+	LDFLAGS+=" -landroid-execinfo"
 
 	# Arm does not support mmx and sse2 instructions, still checks return true
 	if [ "${TERMUX_ARCH}" = "arm" ] || [ "${TERMUX_ARCH}" = "aarch64" ]; then
