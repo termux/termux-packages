a683c75308e396e4325849ad81bbf7b48c015985
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-06 20:15:23 +0900

    evince: Bump to 43.1

--- x11-packages/evince/build.sh
+++ x11-packages/evince/build.sh
@@ -3,19 +3,22 @@ TERMUX_PKG_DESCRIPTION="document viewer for multiple document formats"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=43
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/evince/${_MAJOR_VERSION}/evince-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=66be0de4b47b1130486103988cc152c04aea95950ba3ef16dc20c2ef6b681d47
-TERMUX_PKG_DEPENDS="gdk-pixbuf, gtk3, libhandy, libarchive, djvulibre, texlive-bin, libspectre, poppler, poppler-data, libxml2, libtiff, libgxps, libsecret, gstreamer, gst-plugins-good"
+TERMUX_PKG_SHA256=6d75ca62b73bfbb600f718a098103dc6b813f9050b9594be929e29b4589d2335
+TERMUX_PKG_DEPENDS="atk, djvulibre, gdk-pixbuf, glib, gnome-desktop3, gst-plugins-base, gst-plugins-good, gstreamer, gtk3, libarchive, libcairo, libgxps, libhandy, libsecret, libspectre, libtiff, libxml2, pango, poppler, poppler-data, texlive-bin"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
-TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dnautilus=false
--Dintrospection=true
--Dgtk_doc=false
 -Dps=enabled
+-Dgtk_doc=false
+-Dintrospection=true
+-Dgspell=disabled
 "
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	export PKG_CONFIG_PATH=$TERMUX_PREFIX/share/pkgconfig
 }
