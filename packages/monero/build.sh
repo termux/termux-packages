46461734d6412a1809a70782958903be3cd95d99
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-07 10:47:25 +0900

    monero: Bump to 0.18.1.2

--- packages/monero/build.sh
+++ packages/monero/build.sh
@@ -2,21 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://getmonero.org/
 TERMUX_PKG_DESCRIPTION="A private, secure, untraceable, decentralised digital currency"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.17.3.0
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=0.18.1.2
 TERMUX_PKG_SRCURL=https://github.com/monero-project/monero.git
-TERMUX_PKG_DEPENDS="boost, libprotobuf, libzmq, openssl, readline, unbound"
+TERMUX_PKG_DEPENDS="boost, libc++, libprotobuf, libsodium, libzmq, miniupnpc, openssl, readline, unbound"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DReadline_ROOT_DIR=$TERMUX_PREFIX
+"
 
 termux_step_pre_configure() {
 	termux_setup_protobuf
 
-	_NEED_DUMMY_LIBRT_A=
-	_LIBRT_A=$TERMUX_PREFIX/lib/librt.a
-	if [ ! -e $_LIBRT_A ]; then
-		_NEED_DUMMY_LIBRT_A=true
-		echo '!<arch>' > $_LIBRT_A
-	fi
+	LDFLAGS+=" -lminiupnpc"
 }
 
 termux_step_post_configure() {
@@ -26,10 +23,4 @@ termux_step_post_configure() {
 		$TERMUX_PKG_SRCDIR/translations/generate_translations_header.c \
 		-o $bin/generate_translations_header_for_build
 	export PATH=$bin:$PATH
-}
-
-termux_step_post_make_install() {
-	if [ $_NEED_DUMMY_LIBRT_A ]; then
-		rm -f $_LIBRT_A
-	fi
 }
