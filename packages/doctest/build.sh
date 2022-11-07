aed1b8545517dfb726990f2d590869aa25152328
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-11-07 15:47:51 +0800

    addpkg(main/doctest): a C++ single header testing framework

---
+++ packages/doctest/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/doctest/doctest
+TERMUX_PKG_DESCRIPTION="The fastest feature-rich C++11/14/17/20 single-header testing framework"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.4.9
+TERMUX_PKG_SRCURL=https://github.com/doctest/doctest/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=19b2df757f2f3703a5e63cee553d85596875f06d91a3333acd80a969ef210856
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_AUTO_UPDATE=true
