050d7c78fb6727f403123602108026fceb82fb2c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-09 06:57:30 +0900

    termux_setup_no_integrated_as: Add new function
    %ci:no-build

--- build-package.sh
+++ build-package.sh
@@ -86,6 +86,10 @@ source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_gn.sh"
 # shellcheck source=scripts/build/setup/termux_setup_golang.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_golang.sh"
 
+# Utility function for setting up no-integrated (GNU Binutils) as.
+# shellcheck source=scripts/build/setup/termux_setup_no_integrated_as.sh
+source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_no_integrated_as.sh"
+
 # Utility function for python packages to setup a python crossenv.
 # shellcheck source=scripts/build/setup/termux_setup_python_crossenv.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_python_crossenv.sh"
