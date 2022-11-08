b28dd59fd547a0f11adcbfaca2e4d6f97de5fd65
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-08 18:13:09 +0900

    libicu: Add note for revdep rebuild
    %ci:no-build

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -5,6 +5,8 @@ TERMUX_PKG_LICENSE="BSD"
 # this hack to be able to find the license file.
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
+# Never forget to always bump revision of reverse dependencies and rebuild them
+# when bumping version.
 TERMUX_PKG_VERSION=72.1
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
 TERMUX_PKG_SHA256=a2d2d38217092a7ed56635e34467f92f976b370e20182ad325edea6681a71d68
