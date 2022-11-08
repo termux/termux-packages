86cfab2c2e1a4641ae69a1c7ed8db834de61217f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-09 05:29:15 +0900

    binutils: Create binutils-cross subpackage

--- packages/binutils/build.sh
+++ packages/binutils/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Collection of binary tools, the main ones being ld, the 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.39
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/binutils/binutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00
 TERMUX_PKG_DEPENDS="binutils-libs (>= ${TERMUX_PKG_VERSION}), libc++, zlib"
@@ -19,9 +19,27 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-new-dtags
 "
 TERMUX_PKG_EXTRA_MAKE_ARGS="tooldir=$TERMUX_PREFIX"
-TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/windmc.1 share/man/man1/windres.1 bin/ld.bfd"
+TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/windmc.1 share/man/man1/windres.1"
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_GROUPS="base-devel"
+
+# For binutils-cross:
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
+--prefix=$TERMUX_PREFIX/opt/binutils/cross
+--target=$TERMUX_HOST_PLATFORM
+--enable-shared
+--disable-static
+--disable-nls
+--with-system-zlib
+--disable-gprofng
+"
+
+termux_step_host_build() {
+	$TERMUX_PKG_SRCDIR/configure $TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS
+	make -j $TERMUX_MAKE_PROCESSES
+	make install-strip
+}
 
 # Avoid linking against libfl.so from flex if available:
 export LEXLIB=
@@ -43,5 +61,6 @@ termux_step_post_make_install() {
 	for b in ar ld nm objdump ranlib readelf strip; do
 		ln -s -f $b $TERMUX_HOST_PLATFORM-$b
 	done
+	mv ld.bfd ld
 	ln -sf ld.gold gold
 }
