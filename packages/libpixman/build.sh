3efa550e6b26998ef554e9efdc7602eba3ac8035
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-09 06:06:59 +0900

    libpixman: Use termux_setup_no_integrated_as
    %ci:no-build

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -5,13 +5,13 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.42.2
 TERMUX_PKG_SRCURL=https://cairographics.org/releases/pixman-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ea1480efada2fd948bc75366f7c349e1c96d3297d09a3fe62626e38e234a625e
+TERMUX_PKG_BUILD_DEPENDS="binutils-cross"
 TERMUX_PKG_BREAKS="libpixman-dev"
 TERMUX_PKG_REPLACES="libpixman-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" = arm ] || [ "$TERMUX_ARCH" = aarch64 ]; then
-		termux_setup_gnu_as_23c
-		CFLAGS+=" -fno-integrated-as"
+		termux_setup_no_integrated_as
 	fi
 }
