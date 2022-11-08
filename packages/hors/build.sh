1c6eddc7fcecf0de4c12108bcdbcc70bbdc80da6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-09 05:59:11 +0900

    hors: Use termux_setup_no_integrated_as
    %ci:no-build

--- packages/hors/build.sh
+++ packages/hors/build.sh
@@ -6,13 +6,13 @@ TERMUX_PKG_VERSION=0.8.2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/WindSoilder/hors/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=22419b26f64a2793759d3a3616df58196897cd9227074f475aeb3e1c366296a9
+TERMUX_PKG_BUILD_DEPENDS="binutils-cross"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" = arm ]; then
-		termux_setup_gnu_as_23c
-		CFLAGS+=" -fno-integrated-as"
+		termux_setup_no_integrated_as
 	fi
 
 	rm -f Makefile
