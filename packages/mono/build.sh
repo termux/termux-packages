f02b4cab4a8abfab01d4e1be5a7e5ad8a4471bad
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-09 19:48:38 +0900

    mono: Separate platform-independent libraries
    as a subpackage named mono-libs.

--- packages/mono/build.sh
+++ packages/mono/build.sh
@@ -4,10 +4,10 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=6.12.0.122
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.mono-project.com/sources/mono/mono-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=29c277660fc5e7513107aee1cbf8c5057c9370a4cdfeda2fc781be6986d89d23
-TERMUX_PKG_DEPENDS="krb5, zlib"
+TERMUX_PKG_DEPENDS="krb5, mono-libs, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-btls
 --without-ikvm-native
@@ -36,10 +36,12 @@ termux_step_pre_configure() {
 }
 
 termux_step_post_make_install() {
-	local _PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/prefix
+	pushd $TERMUX_PKG_HOSTBUILD_DIR/prefix/lib/mono
+	find . -name '*.so' -exec rm -f \{\} \;
+	rm -f ./4.5/mono-shlib-cop.exe.config
+	cp -rT . $TERMUX_PREFIX/lib/mono
+	popd
 
-	find $_PREFIX_FOR_BUILD/lib/mono -name '*.so' -exec rm -f \{\} \;
-	cp -rT $_PREFIX_FOR_BUILD/lib/mono $TERMUX_PREFIX/lib/mono
-	# XXX: Map libc to libc.so rather than libc.so.6
-	sed -i "s/libc.so.6/libc.so/g" $TERMUX_PREFIX/etc/mono/config
+	# Strip off SOVERSION suffix for shared libraries.
+	sed -i -E 's/\.so\.[0-9]+/.so/g' $TERMUX_PREFIX/etc/mono/config
 }
