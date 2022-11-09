bcbbcb28bf7ac1cb4f61aaca88ad66048988dd8a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 05:37:07 +0900

    *: Remove `share/pkgconfig` hack
    after commit 3f1be51372b6f33762eac18c3096e3b240cb056a.
    
    On-device `pkg-config` searches `$PREFIX/share/pkgconfig` by default.

--- packages/xorg-util-macros/build.sh
+++ packages/xorg-util-macros/build.sh
@@ -1,14 +1,12 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X.Org Autotools macros"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.19.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/util/util-macros-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=0f812e6e9d2786ba8f54b960ee563c0663ddbe2434bf24ff193f5feab1f31971
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
-termux_step_post_make_install() {
-	mkdir -p ${TERMUX_PREFIX}/lib/pkgconfig
-	mv ${TERMUX_PREFIX}/share/pkgconfig/xorg-macros.pc ${TERMUX_PREFIX}/lib/pkgconfig/
-}
