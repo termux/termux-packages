c07ada7ca9bc5f2b9345a2a24b4f541678c0dd7b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 08:20:54 +0900

    adwaita-icon-theme: Depend on librsvg

--- x11-packages/adwaita-icon-theme/build.sh
+++ x11-packages/adwaita-icon-theme/build.sh
@@ -1,23 +1,12 @@
-TERMUX_PKG_HOMEPAGE=https://www.gnome.org
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/adwaita-icon-theme
 TERMUX_PKG_DESCRIPTION="GNOME standard icons"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="43"
-TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/${TERMUX_PKG_VERSION}/adwaita-icon-theme-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=15c635c227f85d78121841e40815ed5277c7580c9c80e01f1613a807d931b4c1
+_MAJOR_VERSION=43
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/adwaita-icon-theme/${_MAJOR_VERSION}/adwaita-icon-theme-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=2e3ac77d32a6aa5554155df37e8f0a0dd54fc5a65fd721e88d505f970da32ec6
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_DEPENDS="hicolor-icon-theme"
+TERMUX_PKG_DEPENDS="hicolor-icon-theme, librsvg"
 TERMUX_PKG_RM_AFTER_INSTALL="share/icons/Adwaita/icon-theme.cache"
-TERMUX_PKG_AUTO_UPDATE=true
-
-termux_step_pre_configure() {
-	autoreconf -fvi
-}
-
-termux_step_post_make_install() {
-	# fix location of adwaita-icon-theme.pc
-	if [ -f "${TERMUX_PREFIX}/share/pkgconfig/adwaita-icon-theme.pc" ]; then
-		mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig"
-		mv -f "${TERMUX_PREFIX}/share/pkgconfig/adwaita-icon-theme.pc" "${TERMUX_PREFIX}/lib/pkgconfig/adwaita-icon-theme.pc"
-	fi
-}
