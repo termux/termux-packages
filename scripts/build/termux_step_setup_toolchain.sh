ab4987db119059e0004824b7531fa71fe9236094
Henrik Grimler <grimler@termux.dev>
2022-11-09 22:05:15 +0100

    termux_step_setup_toolchain: bump toolchains after ndk-patches update

--- scripts/build/termux_step_setup_toolchain.sh
+++ scripts/build/termux_step_setup_toolchain.sh
@@ -6,10 +6,10 @@ termux_step_setup_toolchain() {
 	# toolchain setup to ensure that everyone gets an updated
 	# toolchain
 	if [ "${TERMUX_NDK_VERSION}" = 25b ]; then
-		TERMUX_STANDALONE_TOOLCHAIN+="-v1"
+		TERMUX_STANDALONE_TOOLCHAIN+="-v2"
 		termux_setup_toolchain_25b
 	elif [ "${TERMUX_NDK_VERSION}" = 23c ]; then
-		TERMUX_STANDALONE_TOOLCHAIN+="-v2"
+		TERMUX_STANDALONE_TOOLCHAIN+="-v3"
 		termux_setup_toolchain_23c
 	else
 		termux_error_exit "We do not have a setup_toolchain function for NDK version $TERMUX_NDK_VERSION"
