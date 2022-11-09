8d0e9b0f66404007d513cf17117c8bc5c852b639
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 06:34:26 +0900

    libwayland-protocols: Bump to 1.28

--- x11-packages/libwayland-protocols/build.sh
+++ x11-packages/libwayland-protocols/build.sh
@@ -2,13 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Wayland protocols library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17
-TERMUX_PKG_REVISION=6
-TERMUX_PKG_SRCURL=https://wayland.freedesktop.org/releases/wayland-protocols-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=df1319cf9705643aea9fd16f9056f4e5b2471bd10c0cc3713d4a4cdc23d6812f
-TERMUX_PKG_DEPENDS="libandroid-support"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-documentation --with-host-scanner"
-
-termux_step_post_make_install() {
-	mv ${TERMUX_PREFIX}/share/pkgconfig/wayland-protocols.pc ${TERMUX_PREFIX}/lib/pkgconfig/
-}
+TERMUX_PKG_VERSION=1.28
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${TERMUX_PKG_VERSION}/downloads/wayland-protocols-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=c7659fb6bf14905e68ef605f898de60d1c066bf66dbea92798573dddec1535b6
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dtests=false
+"
