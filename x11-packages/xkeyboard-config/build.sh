15aba1b42eaab8f90614874665549c65b886cbca
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 06:17:00 +0900

    xkeyboard-config: Bump to 2.37

--- x11-packages/xkeyboard-config/build.sh
+++ x11-packages/xkeyboard-config/build.sh
@@ -1,23 +1,16 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/XKeyboardConfig/
 TERMUX_PKG_DESCRIPTION="X keyboard configuration files"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: HPND, MIT
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.32
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=1feee317ba39b91902b0cbd2987c0c73e6afbfc8f4c096367a5c86c216c036a8
+TERMUX_PKG_VERSION=2.37
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=eb1383a5ac4b6210d7c7302b9d6fab052abdf51c5d2c9b55f1f779997ba68c6c
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_BUILD_DEPENDS="libx11"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---with-xkb-rules-symlink=xorg
---with-xkb-base=${TERMUX_PREFIX}/share/X11/xkb
---enable-compat-rules=yes
+-Dxkb-base=${TERMUX_PREFIX}/share/X11/xkb
+-Dcompat-rules=true
+-Dxorg-rules-symlinks=true
 "
-
-termux_step_post_make_install() {
-	if [ -f "${TERMUX_PREFIX}/share/pkgconfig/xkeyboard-config.pc" ]; then
-		mkdir -p "${TERMUX_PREFIX}/lib/pkgconfig" || exit 1
-		mv -f "${TERMUX_PREFIX}/share/pkgconfig/xkeyboard-config.pc" "${TERMUX_PREFIX}/lib/pkgconfig/xkeyboard-config.pc" || exit 1
-	fi
-}
