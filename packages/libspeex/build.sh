5ca0072ee23b9e39cfe9435e52ba05985a8ff415
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 12:54:49 +0900

    new package: libspeex

---
+++ packages/libspeex/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.speex.org/
+TERMUX_PKG_DESCRIPTION="A patent-free, Open Source/Free Software voice codec"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_SRCURL=https://downloads.xiph.org/releases/speex/speex-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4b44d4f2b38a370a2d98a78329fefc56a0cf93d1c1be70029217baae6628feea
