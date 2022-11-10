8aed0dd8482512e243b7fb2639b846b0a27238a6
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 18:28:24 +0900

    mpv: Add dependencies
    and add guard from automagic dependency.

--- packages/mpv/build.sh
+++ packages/mpv/build.sh
@@ -3,12 +3,30 @@ TERMUX_PKG_DESCRIPTION="Command-line media player"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.34.1
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_REVISION=9
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-support, libarchive, libcaca, libiconv, liblua52, libsixel, pulseaudio, openal-soft, zlib"
+TERMUX_PKG_DEPENDS="ffmpeg, fftw, libandroid-glob, libandroid-support, libarchive, libass, libcaca, libiconv, liblua52, libsamplerate, libsixel, libuchardet, openal-soft, pulseaudio, rubberband, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="share/icons share/applications"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-libmpv-shared
+--enable-lua
+--disable-libbluray
+--disable-dvdnav
+--disable-zimg
+--disable-lcms2
+--disable-vapoursynth
+--enable-openal
+--disable-gbm
+--disable-wayland
+--disable-xv
+--disable-vdpau
+--disable-vaapi
+--disable-jpeg
+--disable-gl
+--disable-vulkan
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
@@ -21,18 +39,7 @@ termux_step_make_install() {
 
 	./waf configure \
 		--prefix=$TERMUX_PREFIX \
-		--disable-gl \
-		--disable-jpeg \
-		--disable-lcms2 \
-		--enable-libarchive \
-		--enable-libmpv-shared \
-		--enable-lua \
-		--enable-pulse \
-		--enable-openal \
-		--enable-caca \
-		--disable-alsa \
-		--disable-x11 \
-		--enable-sixel
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 
 	./waf -v install
 
