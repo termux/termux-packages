67e7be99588e16e648b3491b72ce87120fadb283
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 12:54:13 +0900

    new package: libdvdnav

---
+++ packages/libdvdnav/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://www.videolan.org/developers/libdvdnav.html
+TERMUX_PKG_DESCRIPTION="A library that allows easy use of sophisticated DVD navigation features"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=6.1.1
+TERMUX_PKG_SRCURL=https://downloads.videolan.org/pub/videolan/libdvdnav/${TERMUX_PKG_VERSION}/libdvdnav-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=c191a7475947d323ff7680cf92c0fb1be8237701885f37656c64d04e98d18d48
+TERMUX_PKG_DEPENDS="libdvdread"
