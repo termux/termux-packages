23001793c753a51198901e48502d24af94ee4f49
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 11:05:38 +0900

    bdftopcf: Bump to 1.1.1

--- x11-packages/bdftopcf/build.sh
+++ x11-packages/bdftopcf/build.sh
@@ -1,14 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.freedesktop.org/xorg/util/bdftopcf
 TERMUX_PKG_DESCRIPTION="convert X font from Bitmap Distribution Format to Portable Compiled Format"
+# License: HPND
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1
-TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xorg/util/bdftopcf/-/archive/bdftopcf-${TERMUX_PKG_VERSION}/bdftopcf-bdftopcf-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cd8510d3db57aa999bbc7abc30b6f60d7a685e3344eba662db685747779cb25b
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/util/bdftopcf-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=11c953d53c0f3ed349d0198dfb0a40000b5121df7eef09f2615a262892fed908
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="xorgproto, libx11, xorg-util-macros"
-
-termux_step_pre_configure() {
-	NOCONFIGURE=1 ./autogen.sh
-}
+TERMUX_PKG_BUILD_DEPENDS="xorgproto"
