8f46893152576d7c02fe861f5e9e2fdd48b54305
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-10 17:50:59 +0900

    mpv-x: Bump to 0.34.1

--- x11-packages/mpv-x/build.sh
+++ x11-packages/mpv-x/build.sh
@@ -1,14 +1,28 @@
 TERMUX_PKG_HOMEPAGE=https://mpv.io/
 TERMUX_PKG_DESCRIPTION="Command-line media player"
-TERMUX_PKG_LICENSE="GPL-3.0"
+# License: GPL-2.0-or-later
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.33.0
-TERMUX_PKG_REVISION=11
+TERMUX_PKG_VERSION=0.34.1
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f1b9baf5dc2eeaf376597c28a6281facf6ed98ff3d567e3955c95bf2459520b4
-TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-shmem, libarchive, libass, libdrm, littlecms, libjpeg-turbo, libcaca, liblua52, libx11, libxext, libxinerama, libxss, libxrandr, openal-soft, pulseaudio, zlib"
+TERMUX_PKG_SHA256=32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746
+TERMUX_PKG_DEPENDS="ffmpeg, fftw, libandroid-glob, libandroid-shmem, libarchive, libass, libbluray, libcaca, libdrm, libdvdnav, libdvdread, libiconv, libjpeg-turbo, liblua52, libsamplerate, libsixel, libuchardet, libx11, libxext, libxinerama, libxrandr, libxss, libzimg, littlecms, openal-soft, pulseaudio, rubberband, zlib"
 TERMUX_PKG_CONFLICTS="mpv"
 TERMUX_PKG_REPLACES="mpv"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-libmpv-shared
+--enable-lua
+--enable-dvdnav
+--disable-vapoursynth
+--enable-openal
+--disable-gbm
+--disable-wayland
+--disable-xv
+--disable-vdpau
+--disable-vaapi
+--disable-gl
+--disable-vulkan
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob -landroid-shmem"
@@ -21,17 +35,7 @@ termux_step_make_install() {
 
 	./waf configure \
 		--prefix=$TERMUX_PREFIX \
-		--disable-gl \
-		--enable-jpeg \
-		--enable-lcms2 \
-		--enable-libarchive \
-		--enable-lua \
-		--enable-pulse \
-		--enable-openal \
-		--enable-caca \
-		--disable-alsa \
-		--enable-x11 \
-		--disable-wayland
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 
 	./waf -v install
 	install -Dm600 -t $TERMUX_PREFIX/etc/mpv/ $TERMUX_PKG_BUILDER_DIR/mpv.conf
