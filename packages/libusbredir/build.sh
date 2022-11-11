1531b7fe1f918431feef731c124f7988a1e947b3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-12 04:13:50 +0900

    libusbredir: Bump to 0.13.0

--- packages/libusbredir/build.sh
+++ packages/libusbredir/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://www.spice-space.org/
 TERMUX_PKG_DESCRIPTION="network protocol for sending USB device traffic over a network connection"
-TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.1
-TERMUX_PKG_SRCURL=https://www.spice-space.org/download/usbredir/usbredir-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=407e9e27a1369f01264d5501ffbe88935ddd7d5de675f5835db05dc9c9ac56f3
-TERMUX_PKG_DEPENDS="libusb"
+TERMUX_PKG_VERSION=0.13.0
+TERMUX_PKG_SRCURL=https://www.spice-space.org/download/usbredir/usbredir-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=4ba6faa02c0ae6deeb4c53883d66ab54b3a5899bead42ce4ded9568b9a7dc46e
+TERMUX_PKG_DEPENDS="glib, libusb"
