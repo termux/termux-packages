b59b9649779965cf7f01c3dd3beb89828c8f1759
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-11 13:19:13 +0900

    vobsub2srt: Enable i686 build

--- packages/vobsub2srt/build.sh
+++ packages/vobsub2srt/build.sh
@@ -4,14 +4,11 @@ TERMUX_PKG_LICENSE="GPL-3.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _COMMIT=0ba6e25e078a040195d7295e860cc9064bef7c2c
 TERMUX_PKG_VERSION=2017.12.18
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/ruediger/VobSub2SRT.git
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libc++, tesseract"
 TERMUX_PKG_FORCE_CMAKE=true
-
-# Due to dependency on tesseract:
-TERMUX_PKG_BLACKLISTED_ARCHES="i686"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
