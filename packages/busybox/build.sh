46c8cd16794bed652fc74dde7f0dc342682a5837
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-11-11 21:12:23 +0200

    busybox: build shared library
    Explained there: https://github.com/termux/termux-packages/issues/13000

--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tiny versions of many common UNIX utilities into a singl
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.35.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://busybox.net/downloads/busybox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=faeeb244c35a348a334f4a59e44626ee870fb07b6884d68c10ae8bc19f83a694
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -33,15 +33,15 @@ termux_step_configure() {
 		-e "s|@TERMUX_LDFLAGS@|$LDFLAGS|g" \
 		-e "s|@TERMUX_LDLIBS@|log|g" \
 		$TERMUX_PKG_BUILDER_DIR/busybox.config > .config
-
 	unset CFLAGS LDFLAGS
 	make oldconfig
 }
 
-termux_step_post_make_install() {
-	if $TERMUX_DEBUG_BUILD; then
-		install -Dm700 busybox_unstripped $TERMUX_PREFIX/bin/busybox
-	fi
+termux_step_make_install() {
+	# Using unstripped variant. The post-massage step will strip binaries anyway.
+	install -Dm700 ./0_lib/busybox_unstripped $TERMUX_PREFIX/bin/busybox
+	install -Dm700 ./0_lib/libbusybox.so.${TERMUX_PKG_VERSION}_unstripped $TERMUX_PREFIX/lib/libbusybox.so.${TERMUX_PKG_VERSION}
+	ln -sfr $TERMUX_PREFIX/lib/libbusybox.so.${TERMUX_PKG_VERSION} $TERMUX_PREFIX/lib/libbusybox.so
 
 	# Install busybox man page.
 	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 $TERMUX_PKG_SRCDIR/docs/busybox.1
