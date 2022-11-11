7dc2a160f712bd4315c80dfe3e723f903663e492
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-11 16:10:15 +0900

    diffstat: Bump to 1.65

--- packages/diffstat/build.sh
+++ packages/diffstat/build.sh
@@ -1,10 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://invisible-island.net/diffstat/diffstat.html
 TERMUX_PKG_DESCRIPTION="Displays a histogram of changes to a file"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.64
+TERMUX_PKG_VERSION=1.65
 TERMUX_PKG_SRCURL=https://ftp-osl.osuosl.org/pub/gentoo/distfiles/diffstat-${TERMUX_PKG_VERSION}.tgz
 # invisible-mirror.net is not suitable for CI due to bad responsiveness.
 #TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/diffstat/diffstat-${TERMUX_PKG_VERSION}.tgz
 #TERMUX_PKG_SRCURL=https://invisible-island.net/datafiles/release/diffstat.tar.gz
-TERMUX_PKG_SHA256=b8aee38d9d2e1d05926e6b55810a9d2c2dd407f24d6a267387563a4436e3f7fc
+TERMUX_PKG_SHA256=8cf27424926debcd85921755c3915559288244bd103f62d740dc6b83b557a28a
