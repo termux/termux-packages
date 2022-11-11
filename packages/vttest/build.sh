8dea4cfe0e964fb51fe565ff2eff087fdafd9059
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-12 04:01:18 +0900

    vttest: Bump to 20220827

--- packages/vttest/build.sh
+++ packages/vttest/build.sh
@@ -1,7 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://invisible-island.net/vttest/
 TERMUX_PKG_DESCRIPTION="Program for testing the VT100 compatibility of terminal emulators"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20210210
-TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/vttest/vttest-$TERMUX_PKG_VERSION.tgz
-TERMUX_PKG_SHA256=0f98a2e305982915f1520984c3e8698e3acd508ee210711528c89f5a7ea7f046
+TERMUX_PKG_VERSION=20220827
+TERMUX_PKG_SRCURL=https://invisible-island.net/archives/vttest/vttest-${TERMUX_PKG_VERSION}.tgz
+TERMUX_PKG_SHA256=5726aae58137773ce6ce01fe6a86fc0f83c47763e30488bff35b9bc4fc946ce2
