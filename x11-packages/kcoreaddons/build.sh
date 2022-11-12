4157625a1dcd990c2eff73df7085ee2d52f95ab8
Kevin Williams <admin@utermux.dev>
2022-11-12 15:20:42 +0800

    upgpkg(x11/kcoreaddons): 5.99.0
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/kcoreaddons/build.sh
+++ x11-packages/kcoreaddons/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Utilities for core application functionality and accessing the OS (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.71.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=5.99.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcoreaddons-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=e95008b032e299cf47f596739d9236701e2f55e507734f33b8ea497882fd130b
+TERMUX_PKG_SHA256=5a76a33d3971b1d748db13faf76ab7a863310dccb374ebff69f24ef9c7f4131b
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, shared-mime-info"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
