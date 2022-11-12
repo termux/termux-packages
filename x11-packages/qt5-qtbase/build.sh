c3647caf5fd1671219c5fd99788310242e4edb51
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-12 17:48:18 +0900

    qt5-qtbase: Place revision adjacent to version
    %ci:no-build

--- x11-packages/qt5-qtbase/build.sh
+++ x11-packages/qt5-qtbase/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="A cross-platform application and UI framework"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=5.15.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/5.15/${TERMUX_PKG_VERSION}/submodules/qtbase-everywhere-opensource-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=0c42c799aa7c89e479a07c451bf5a301e291266ba789e81afc18f95049524edc
 TERMUX_PKG_DEPENDS="dbus, double-conversion, harfbuzz, libandroid-shmem, libandroid-sysv-semaphore, libc++, libice, libicu, libjpeg-turbo, libpng, libsm, libuuid, libx11, libxcb, libxi, libxkbcommon, openssl, pcre2, ttf-dejavu, freetype, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm, zlib, glib"
 # gtk3 dependency is a run-time dependency only for the gtk platformtheme subpackage
 TERMUX_PKG_BUILD_DEPENDS="gtk3"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_NO_STATICSPLIT=true
 
 TERMUX_PKG_RM_AFTER_INSTALL="
