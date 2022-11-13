1e5231d467493e5a9a87ebdf8182195302fa394a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-13 21:29:05 +0900

    mruby: Bump to 3.1.0

--- packages/mruby/build.sh
+++ packages/mruby/build.sh
@@ -2,19 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://mruby.org/
 TERMUX_PKG_DESCRIPTION="Lightweight implementation of the Ruby language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.0
+TERMUX_PKG_VERSION=3.1.0
 TERMUX_PKG_SRCURL=https://github.com/mruby/mruby/archive/${TERMUX_PKG_VERSION}.zip
-TERMUX_PKG_SHA256=c7d95fdc05efdc8f5a0df4d5f0fbad5092596d61927c39515d03b8dde4743a8e
-TERMUX_PKG_DEPENDS="ncurses, readline"
+TERMUX_PKG_SHA256=826d8410f2a6965846679f0aa53234eeb20c9d9483fa714835937492197d2677
+TERMUX_PKG_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_configure() {
-	:
-}
 
 termux_step_make() {
 	export CC_FOR_TARGET="$CC"
-	export CFLAGS_FOR_TARGET="$CPPFLAGS $CFLAGS -DMRB_USE_READLINE"
+	export CFLAGS_FOR_TARGET="$CPPFLAGS $CFLAGS \
+		-DMRB_USE_READLINE \
+		-DMRB_READLINE_HEADER=\\<readline/readline.h\\> \
+		-DMRB_READLINE_HISTORY=\\<readline/history.h\\> \
+		"
 	export LDFLAGS_FOR_TARGET="$LDFLAGS -lncurses -lreadline"
 	unset CPPFLAGS CFLAGS LDFLAGS
 	export CC="$CC_FOR_BUILD"
