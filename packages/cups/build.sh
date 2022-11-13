0efc47b96cd3ffccf77265ff8962ab0ac5ba45b5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-13 21:21:23 +0900

    cups: Bump to 2.3.6

--- packages/cups/build.sh
+++ packages/cups/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE="https://www.cups.org/"
 TERMUX_PKG_DESCRIPTION="Common UNIX Printing System"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.3.3"
-TERMUX_PKG_REVISION=10
-TERMUX_PKG_SRCURL="https://github.com/apple/cups/releases/download/v${TERMUX_PKG_VERSION}/cups-${TERMUX_PKG_VERSION}-source.tar.gz"
-TERMUX_PKG_SHA256="261fd948bce8647b6d5cb2a1784f0c24cc52b5c4e827b71d726020bcc502f3ee"
-TERMUX_PKG_DEPENDS="libc++, libiconv, libcrypt, gnutls"
+TERMUX_PKG_VERSION=2.3.6
+TERMUX_PKG_SRCURL=https://github.com/apple/cups/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3a96912fc88f62c5d8ac9b251bf0652f1cbe7e69cde16919103aea39f31a1a84
+TERMUX_PKG_DEPENDS="gnutls, libc++, libcrypt, libiconv, zlib"
+TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_CONFFILES="
