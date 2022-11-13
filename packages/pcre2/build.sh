d70c8356eddb9bfce0afe8923f2ee8c32c87a53f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-13 23:52:46 +0900

    pcre2: Include libpcre2-posix

--- packages/pcre2/build.sh
+++ packages/pcre2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Perl 5 compatible regular expression library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="10.40"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/PhilipHazel/pcre2/releases/download/pcre2-${TERMUX_PKG_VERSION}/pcre2-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=14e4b83c4783933dc17e964318e6324f7cae1bc75d8f3c79bc6969f00c159d68
 TERMUX_PKG_AUTO_UPDATE=true
@@ -12,8 +13,6 @@ TERMUX_PKG_REPLACES="pcre2-dev"
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/pcre2test
 share/man/man1/pcre2test.1
-lib/libpcre2-posix.so
-lib/pkgconfig/libpcre2-posix.pc
 "
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-jit
