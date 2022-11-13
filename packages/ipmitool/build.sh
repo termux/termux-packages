2ac89c5dc879ace5aad5681852d4da369e2d2525
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-13 21:15:45 +0900

    ipmitool: Bump to 1.8.19

--- packages/ipmitool/build.sh
+++ packages/ipmitool/build.sh
@@ -1,9 +1,12 @@
-TERMUX_PKG_HOMEPAGE=http://ipmitool.sourceforge.net
+TERMUX_PKG_HOMEPAGE=https://github.com/ipmitool/ipmitool
 TERMUX_PKG_DESCRIPTION="Command-line interface to IPMI-enabled devices"
-TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.8.18
-TERMUX_PKG_REVISION=9
-TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/ipmitool/ipmitool/$TERMUX_PKG_VERSION/ipmitool-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=0c1ba3b1555edefb7c32ae8cd6a3e04322056bc087918f07189eeedfc8b81e01
-TERMUX_PKG_DEPENDS="ncurses, openssl, readline"
+TERMUX_PKG_VERSION=1.8.19
+TERMUX_PKG_SRCURL=https://github.com/ipmitool/ipmitool/archive/refs/tags/IPMITOOL_${TERMUX_PKG_VERSION//./_}.tar.gz
+TERMUX_PKG_SHA256=48b010e7bcdf93e4e4b6e43c53c7f60aa6873d574cbd45a8d86fa7aaeebaff9c
+TERMUX_PKG_DEPENDS="openssl, readline"
+
+termux_step_pre_configure() {
+	sh bootstrap
+}
