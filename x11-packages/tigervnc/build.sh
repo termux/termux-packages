9cc5781a71367cff9b51f50410e2ee85cb6e58be
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-14 02:01:20 +0900

    tigervnc: Drop tigervnc-viewer subpackage
    due to vulnerability CVE-2020-26117.
    
    Planned to be packaged separately with a newer version. (Viewer does not
    require PAM support.)

--- x11-packages/tigervnc/build.sh
+++ x11-packages/tigervnc/build.sh
@@ -5,17 +5,18 @@ TERMUX_PKG_MAINTAINER="@termux"
 # No update anymore. v1.11.x requires support of PAM.
 TERMUX_PKG_VERSION=(1.10.1
 		    1.20.14)
-TERMUX_PKG_REVISION=27
+TERMUX_PKG_REVISION=28
 TERMUX_PKG_SRCURL=(https://github.com/TigerVNC/tigervnc/archive/v${TERMUX_PKG_VERSION}.tar.gz
 		   https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz)
 TERMUX_PKG_SHA256=(19fcc80d7d35dd58115262e53cac87d8903180261d94c2a6b0c19224f50b58c4
 		   5cc5b70b9be89443e2594b93656c60bd5e82cd7f01deb4ce4faf81dcf546a16b)
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libgnutls, libjpeg-turbo, libpixman, libx11, libxau, libxdamage, libxdmcp, libxext, libxfixes, libxfont2, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp, zlib"
-TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-util-macros, xorgproto, xtrans"
+TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-server-xvfb, xorg-util-macros, xorgproto, xtrans"
 TERMUX_PKG_SUGGESTS="aterm, xorg-twm"
 
 TERMUX_PKG_FOLDERNAME=tigervnc-${TERMUX_PKG_VERSION}
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_VIEWER=ON -DENABLE_NLS=OFF -DENABLE_PAM=OFF -DENABLE_GNUTLS=ON -DFLTK_MATH_LIBRARY=libm.a"
+# Viewer has a separate package tigervnc-viewer. Do not build viewer here.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_VIEWER=OFF -DENABLE_NLS=OFF -DENABLE_PAM=OFF -DENABLE_GNUTLS=ON -DFLTK_MATH_LIBRARY="
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
@@ -51,9 +52,6 @@ termux_step_pre_configure() {
 		${xorg_server_xvfb_configure_args}
 
 	LDFLAGS="${LDFLAGS} -landroid-shmem"
-
-	# Fix for FLTK_MATH_LIBRARY.
-	cp "$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/libm.a" "$TERMUX_PKG_SRCDIR"/
 }
 
 termux_step_post_make_install() {
