70b3aac9cc9526df89c104651a6eae01ce1d6324
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-14 00:46:40 +0900

    redis: Use backtrace

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=6.2.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b7a79cc3b46d3c6eb52fa37dde34a4a60824079ebdfb3abfbbfa035947c55319
+TERMUX_PKG_DEPENDS="libandroid-execinfo"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/redis.conf"
 
@@ -12,9 +14,9 @@ termux_step_pre_configure() {
 	export PREFIX=$TERMUX_PREFIX
 	export USE_JEMALLOC=no
 
-	if [ $TERMUX_ARCH = "i686" ]; then
-		sed -i 's/FINAL_LIBS=-lm/FINAL_LIBS=-lm -latomic/' $TERMUX_PKG_SRCDIR/src/Makefile
-	fi
+	CPPFLAGS+=" -DHAVE_BACKTRACE"
+	CFLAGS+=" $CPPFLAGS"
+	LDFLAGS+=" -landroid-execinfo"
 }
 
 termux_step_post_make_install() {
