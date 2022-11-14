0478ca52ae34e767a7e72ce43eb70f2983164420
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-14 13:12:00 +0900

    zile: Bump to 2.6.2

--- packages/zile/build.sh
+++ packages/zile/build.sh
@@ -2,19 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/zile/
 TERMUX_PKG_DESCRIPTION="Lightweight clone of the Emacs text editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.4.15
+TERMUX_PKG_VERSION=2.6.2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/zile/zile-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=39c300a34f78c37ba67793cf74685935a15568e14237a3a66fda8fcf40e3035e
-TERMUX_PKG_DEPENDS="libgc, ncurses"
+TERMUX_PKG_SHA256=77eb7daff3c98bdc88daa1ac040dccca72b81dc32fc3166e079dd7a63e42c741
+TERMUX_PKG_DEPENDS="glib, libgee, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_header_spawn_h=no
+gl_cv_have_weak=no
 "
 
 termux_step_post_configure() {
 	# zile uses help2man to build the zile.1 man page, which would require
-	# a host build. To avoid that just copy a pre-built man page.
-	cp $TERMUX_PKG_BUILDER_DIR/zile.1 $TERMUX_PKG_BUILDDIR/doc/zile.1
+	# a host build.
+	sed 's|@docdir@|$PREFIX/share/doc/zile|g' \
+		$TERMUX_PKG_SRCDIR/doc/zile.1.in \
+		> $TERMUX_PKG_BUILDDIR/doc/zile.1
 	touch -d "next hour" $TERMUX_PKG_BUILDDIR/doc/zile.1*
 }
 
