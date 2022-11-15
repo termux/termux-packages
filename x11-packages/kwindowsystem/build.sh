3a5c30eb21ed82c258b45b8a9ab7f25c46139a93
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-15 18:32:00 +0900

    kwindowsystem: Bump to 5.100.0

--- x11-packages/kwindowsystem/build.sh
+++ x11-packages/kwindowsystem/build.sh
@@ -9,9 +9,9 @@ LICENSES/LGPL-3.0-only.txt
 LICENSES/LicenseRef-KDE-Accepted-LGPL.txt
 LICENSES/MIT.txt"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.99.0
+TERMUX_PKG_VERSION=5.100.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kwindowsystem-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=f4d13fbf562c05075bb59f997a0de567f96c008a379b7c796d2ca26c01a7f877
+TERMUX_PKG_SHA256=69b24ebaa5c94aa040c46dc1270a8fc9e44ed35d497c16b9b3244bb200834fb3
 TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libxfixes, qt5-qtbase, qt5-qtx11extras, xcb-util-keysyms"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qttools-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_PREFIX=${TERMUX_PREFIX}"
