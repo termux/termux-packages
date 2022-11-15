137e33edbf8a68992867f0bde578cd6da756a936
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-15 13:44:50 +0900

    gegl: Bump to 0.4.40

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -3,12 +3,10 @@ TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.4
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.38
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.40
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${_MAJOR_VERSION}/gegl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e4a33c8430a5042fba8439b595348e71870f0d95fbf885ff553f9020c1bed750
-# For ffmpeg dependency see below.
-TERMUX_PKG_DEPENDS="babl, gdk-pixbuf, glib, json-glib, libandroid-support, libcairo, libjasper, libjpeg-turbo, libpng, librsvg, libtiff, libwebp, littlecms, pango"
+TERMUX_PKG_SHA256=cdde80d15a49dab9a614ef98f804c8ce6e4cfe1339a3c240c34f3fb45436b85d
+TERMUX_PKG_DEPENDS="babl, ffmpeg, gdk-pixbuf, glib, json-glib, libandroid-support, libc++, libcairo, libjasper, libjpeg-turbo, libpng, librsvg, libtiff, libwebp, littlecms, openexr, pango, poppler"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac, xorgproto"
 TERMUX_PKG_BREAKS="gegl-dev"
 TERMUX_PKG_REPLACES="gegl-dev"
@@ -16,10 +14,6 @@ TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dintrospection=true
 "
-
-# Disable ffmpeg support until fixed upstream:
-# https://gitlab.gnome.org/GNOME/gegl/-/issues/301
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dlibav=disabled"
 
 termux_step_pre_configure() {
 	termux_setup_gir
