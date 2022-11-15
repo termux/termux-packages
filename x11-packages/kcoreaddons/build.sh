d011f2c35011ac5898f86ab65a32d22830ff29a2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-15 18:28:39 +0900

    kcoreaddons: Bump to 5.100.0

--- x11-packages/kcoreaddons/build.sh
+++ x11-packages/kcoreaddons/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Utilities for core application functionality and accessing the OS (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.99.0
+TERMUX_PKG_VERSION=5.100.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcoreaddons-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=5a76a33d3971b1d748db13faf76ab7a863310dccb374ebff69f24ef9c7f4131b
+TERMUX_PKG_SHA256=dfed68386f919bd214f07837a2a5356e61958237d845115b632e35349e5c1790
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, shared-mime-info"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
