6aa7205607798397568a7bd8244809e741e81f9a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-16 03:10:41 +0900

    calcurse: Use alternative `pthread_cancel` workaround
    Use of atomic flags does not work well when the loop involves long wait.

--- packages/calcurse/build.sh
+++ packages/calcurse/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="calcurse is a calendar and scheduling application for th
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.8.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://calcurse.org/files/calcurse-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=48a736666cc4b6b53012d73b3aa70152c18b41e6c7b4807fab0f168d645ae32c
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
