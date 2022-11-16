bc53c90ff5af3eb99584da92f726a7869148150a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-16 18:50:40 +0900

    minicom: Bump to 2.8

--- packages/minicom/build.sh
+++ packages/minicom/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://salsa.debian.org/minicom-team/minicom
 TERMUX_PKG_DESCRIPTION="Friendly menu driven serial communication program"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.7.1
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://alioth-archive.debian.org/releases/minicom/Source/$TERMUX_PKG_VERSION/minicom-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=532f836b7a677eb0cb1dca8d70302b73729c3d30df26d58368d712e5cca041f1
+TERMUX_PKG_VERSION=2.8
+TERMUX_PKG_SRCURL=https://salsa.debian.org/minicom-team/minicom/-/archive/${TERMUX_PKG_VERSION}/minicom-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9e8dee8e7e1f6d6115d0e1955da5b801e44b91289f6b3e320842949488d4b22f
 TERMUX_PKG_DEPENDS="libiconv, ncurses"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -16,4 +15,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	export CFLAGS+=" -fcommon"
+	CPPFLAGS+=" -Dushort=u_short"
 }
