a3bf9692e0fc69881679564e3a0d17a2f49ebc6d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-16 18:31:25 +0900

    mailutils: Bump to 3.15

--- packages/mailutils/build.sh
+++ packages/mailutils/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://mailutils.org/
 TERMUX_PKG_DESCRIPTION="Mailutils is a swiss army knife of electronic mail handling. "
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@suhan-paradkar"
-TERMUX_PKG_VERSION=3.13
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.15
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/mailutils/mailutils-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=d920971dcb49878a009911774fd6404f13d27bd101e2d59b664a28659a4094c7
-TERMUX_PKG_BUILD_DEPENDS="libandroid-glob, libcrypt"
+TERMUX_PKG_SHA256=b7d0c286c352fcc7da7978cfd617cc66736b21fa891aa4f88855f516354f2ddb
+TERMUX_PKG_DEPENDS="libandroid-glob, libcrypt"
 
 termux_step_pre_configure() {
 	export LIBS="-landroid-glob"
