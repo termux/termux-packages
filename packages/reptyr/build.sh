4a48726a295a89067c9a8663bc6bd91e9a794414
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-16 18:09:36 +0900

    reptyr: Bump to 0.9.0

--- packages/reptyr/build.sh
+++ packages/reptyr/build.sh
@@ -2,12 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://github.com/nelhage/reptyr"
 TERMUX_PKG_DESCRIPTION="Tool for reparenting a running program to a new terminal"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_VERSION=0.9.0
 TERMUX_PKG_SRCURL=https://github.com/nelhage/reptyr/archive/refs/tags/reptyr-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4b470ed2a0d25fed591739fa9613ce7ad3d0377891eb56cbe914e3c85db46ca8
+TERMUX_PKG_SHA256=b442fbb80a1003b1985974c6fc9eeb8124a43a9bf014ae6af8cde0ca5e587731
+TERMUX_PKG_BUILD_DEPENDS="bash-completion"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make_install() {
-	install -Dm700 -t ${TERMUX_PREFIX}/bin reptyr
-	install -Dm600 -t ${TERMUX_PREFIX}/share/man/man1 ${TERMUX_PKG_SRCDIR}/reptyr.1
-}
+TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
