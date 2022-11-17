f541af478befd52747b6eecf75a09e9e101a2e7b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 13:39:12 +0900

    new package: unixodbc

---
+++ packages/unixodbc/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://www.unixodbc.org/
+TERMUX_PKG_DESCRIPTION="An Open Source ODBC sub-system and an ODBC SDK for Linux, Mac OSX, and UNIX"
+TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.3.11
+TERMUX_PKG_SRCURL=ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d9e55c8e7118347e3c66c87338856dad1516b490fb7c756c1562a2c267c73b5c
+TERMUX_PKG_DEPENDS="libiconv, libltdl, readline"
