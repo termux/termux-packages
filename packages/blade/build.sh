9b2d5ae1e8593a872d583f26e3b8370e0ac2d316
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:26:45 +0900

    blade: Bump to 0.0.72

--- packages/blade/build.sh
+++ packages/blade/build.sh
@@ -1,11 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://bladelang.com/
 TERMUX_PKG_DESCRIPTION="A simple, fast, clean and dynamic language"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.0.6
+TERMUX_PKG_VERSION=0.0.72
 TERMUX_PKG_SRCURL=https://github.com/blade-lang/blade/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3c13f2a81dc027871993e8a369691a470ed7f62b9fb4a72237ff75db36abe35d
-TERMUX_PKG_DEPENDS="libsqlite, readline"
+TERMUX_PKG_SHA256=541e8db24360e6bfda9bfb718a0a7bc884c8ca7ef1dc571be7ce9fddbc0b8f9d
+TERMUX_PKG_BUILD_DEPENDS="libcurl, openssl"
 TERMUX_PKG_HOSTBUILD=true
 
 termux_step_host_build() {
@@ -15,7 +16,7 @@ termux_step_host_build() {
 }
 
 termux_step_pre_configure() {
-	PATH=$TERMUX_PKG_HOSTBUILD_DIR/bin:$PATH
+	PATH=$TERMUX_PKG_HOSTBUILD_DIR/blade:$PATH
 
 	_NEED_DUMMY_LIBPTHREAD_A=
 	_LIBPTHREAD_A=$TERMUX_PREFIX/lib/libpthread.a
@@ -26,7 +27,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	pushd bin
+	pushd blade
 	install -Dm700 -t $TERMUX_PREFIX/bin blade
 	install -Dm600 -t $TERMUX_PREFIX/lib libblade.so
 	local sharedir=$TERMUX_PREFIX/share/blade
