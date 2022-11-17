6d27aa5828f556c140fad54c73f1bdd426a82346
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:46:44 +0900

    scc: Bump to 3.1.0

--- packages/scc/build.sh
+++ packages/scc/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/boyter/scc
 TERMUX_PKG_DESCRIPTION="Counts physical the lines of code, blank lines, comment lines, and physical lines of source code"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.0
-TERMUX_PKG_SRCURL=https://github.com/boyter/scc.git
+TERMUX_PKG_VERSION=3.1.0
+TERMUX_PKG_SRCURL=https://github.com/boyter/scc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bffea99c7f178bc48bfba3c64397d53a20a751dfc78221d347aabdce3422fd20
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
