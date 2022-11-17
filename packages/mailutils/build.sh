10eab5c343df3a948f0fa8c81bb5180d57450cfc
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 10:00:40 +0900

    mailutils: Add dependencies
    and avoid automagic dependencies.

--- packages/mailutils/build.sh
+++ packages/mailutils/build.sh
@@ -3,9 +3,22 @@ TERMUX_PKG_DESCRIPTION="Mailutils is a swiss army knife of electronic mail handl
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@suhan-paradkar"
 TERMUX_PKG_VERSION=3.15
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/mailutils/mailutils-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=b7d0c286c352fcc7da7978cfd617cc66736b21fa891aa4f88855f516354f2ddb
-TERMUX_PKG_DEPENDS="libandroid-glob, libcrypt"
+TERMUX_PKG_DEPENDS="libandroid-glob, libcrypt, libiconv, libltdl, libunistring, ncurses, readline"
+# Most of these configure arguments are for avoiding automagic dependencies.
+# You may instead add dependencies properly and enable them.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-python
+--without-gssapi
+--without-gnutls
+--without-gdbm
+--without-berkeley-db
+--without-fribidi
+--without-ldap
+--without-guile
+"
 
 termux_step_pre_configure() {
 	export LIBS="-landroid-glob"
