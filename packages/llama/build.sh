801eadf7cf5fc944e44a6de67afb4cd3b21db8d9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:39:40 +0900

    llama: Bump to 1.1.1

--- packages/llama/build.sh
+++ packages/llama/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/antonmedv/llama
 TERMUX_PKG_DESCRIPTION="A terminal file manager"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.2
-TERMUX_PKG_SRCURL=https://github.com/antonmedv/llama.git
+TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_SRCURL=https://github.com/antonmedv/llama/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fbe387c567d4be018c7b031a87c311d866fb892a306c8d0619e5ce800a466bb6
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
