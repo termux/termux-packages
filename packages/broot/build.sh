29b12dc7253ed96b5807c52a74c4cd37f1740c62
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:28:33 +0900

    broot: Bump to 1.16.2

--- packages/broot/build.sh
+++ packages/broot/build.sh
@@ -2,15 +2,34 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Canop/broot
 TERMUX_PKG_DESCRIPTION="A better way to navigate directories"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.8.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.16.2
 TERMUX_PKG_SRCURL=https://github.com/Canop/broot/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=baa107a2cd458c9f4eda9cafa8fac54570918efafaadfc34df43442f7cd237b9
-TERMUX_PKG_DEPENDS="libgit2"
+TERMUX_PKG_SHA256=5a84bc72e861e9f2e70eb278e0bb26424e249448cf1d67884f419aacc98fec06
+TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make() {
+termux_step_pre_configure() {
+	CPPFLAGS+=" -DGIT_ERROR_SHA1=GIT_ERROR_SHA"
+
 	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	local _patch=$TERMUX_SCRIPTDIR/packages/libgit2/src-util-rand.c.patch
+	local d
+	for d in $CARGO_HOME/registry/src/github.com-*/libgit2-sys-*/libgit2; do
+		(
+			t=${d}/src/util/
+			cp $TERMUX_SCRIPTDIR/packages/libgit2/getloadavg.c ${t}
+			patch --silent -d ${t} < ${_patch}
+		) || :
+	done
+}
+
+termux_step_make() {
 	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 
 	mkdir -p build
