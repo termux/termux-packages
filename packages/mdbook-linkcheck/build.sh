4cacdf7a1192008a378e742a362d906cc930cbbf
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:41:55 +0900

    mdbook-linkcheck: Bump to 0.7.7

--- packages/mdbook-linkcheck/build.sh
+++ packages/mdbook-linkcheck/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://michael-f-bryan.github.io/mdbook-linkcheck/
 TERMUX_PKG_DESCRIPTION="A backend for mdbook which will check your links for you"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.6
-TERMUX_PKG_SRCURL=https://github.com/Michael-F-Bryan/mdbook-linkcheck.git
+TERMUX_PKG_VERSION=0.7.7
+TERMUX_PKG_SRCURL=https://github.com/Michael-F-Bryan/mdbook-linkcheck/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3194243acf12383bd328a9440ab1ae304e9ba244d3bd7f85f1c23b0745c4847a
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
