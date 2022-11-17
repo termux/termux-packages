15b9381e9530fa4ca3a37ec80f081d9ddcd5d236
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 23:09:39 +0900

    viddy: Bump to 0.3.6

--- packages/viddy/build.sh
+++ packages/viddy/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/sachaos/viddy
 TERMUX_PKG_DESCRIPTION="A modern watch command"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.4
-TERMUX_PKG_SRCURL=https://github.com/sachaos/viddy.git
+TERMUX_PKG_VERSION=0.3.6
+TERMUX_PKG_SRCURL=https://github.com/sachaos/viddy/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ce000cf3fbea3f4d6ade7bf464a91d4f3fa2f3b3a7abc8a09de1e83ac400b9af
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
