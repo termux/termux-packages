3cf328053bdef7392e6b1e4eb5ce11a8e3cda099
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:44:27 +0900

    rust-analyzer: Bump to 20221114

--- packages/rust-analyzer/build.sh
+++ packages/rust-analyzer/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A Rust compiler front-end for IDEs"
 TERMUX_PKG_LICENSE="MIT, Apache-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE-MIT, LICENSE-APACHE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20220124
-TERMUX_PKG_SRCURL=https://github.com/rust-analyzer/rust-analyzer/archive/refs/tags/${TERMUX_PKG_VERSION:0:4}-${TERMUX_PKG_VERSION:4:2}-${TERMUX_PKG_VERSION:6:2}.tar.gz
-TERMUX_PKG_SHA256=634dc4fda60674b10d1f945dfb9f64a6a8c0e41c6fbb42bffbeeb3bae16fb1db
+_VERSION=2022-11-14
+TERMUX_PKG_VERSION=${_VERSION//-/}
+TERMUX_PKG_SRCURL=https://github.com/rust-analyzer/rust-analyzer/archive/refs/tags/${_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4e6e723e6d70391f1557c7a89cb991791f4fd46fde82d76b95482c393a1341c1
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
