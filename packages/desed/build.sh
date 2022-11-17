12c4cf805c45645e0e08e9bd44adab09b07e035e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:31:35 +0900

    desed: Bump to 1.2.1

--- packages/desed/build.sh
+++ packages/desed/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/SoptikHa2/desed
 TERMUX_PKG_DESCRIPTION="Demystifies and debugs your sed scripts"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_SRCURL=https://github.com/SoptikHa2/desed.git
+TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_SRCURL=https://github.com/SoptikHa2/desed/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bee8c60d58f11472c036277b0318bdceb5520cce5a61965bc028b26ccbdeb706
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
