7860b66651b6ee526b0880ae2b37ff5352e5c694
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-17 22:33:50 +0900

    grex: Bump to 1.4.1

--- packages/grex/build.sh
+++ packages/grex/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pemistahl/grex
 TERMUX_PKG_DESCRIPTION="Simplifies the task of creating regular expressions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.0
-TERMUX_PKG_SRCURL=https://github.com/pemistahl/grex.git
+TERMUX_PKG_VERSION=1.4.1
+TERMUX_PKG_SRCURL=https://github.com/pemistahl/grex/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8413aae520d696969525961438d22e31cd966058ce3510e91e77da18603c96b9
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
