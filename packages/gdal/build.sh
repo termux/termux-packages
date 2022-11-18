a24842f92e61ded0b178b71c508c7f6d9884e0cb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-19 03:56:31 +0900

    gdal: Bump to 3.5.3

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="A translator library for raster and vector geospatial da
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.4.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.5.3
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=16baf03dfccf9e3f72bb2e15cd2d5b3f4be0437cdff8a785bceab0c7be557335
+TERMUX_PKG_SHA256=d32223ddf145aafbbaec5ccfa5dbc164147fb3348a3413057f9b1600bb5b3890
 TERMUX_PKG_DEPENDS="json-c, libc++, libcurl, libexpat, libgeos, libiconv, libjxl, libpng, libspatialite, libsqlite, libwebp, netcdf-c, openjpeg, openssl, proj, postgresql"
 TERMUX_PKG_BREAKS="gdal-dev"
 TERMUX_PKG_REPLACES="gdal-dev"
@@ -19,6 +18,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-libtiff=internal
 --with-spatialite=$TERMUX_PREFIX
 --with-sqlite3=$TERMUX_PREFIX
+--with-rename-internal-libtiff-symbols=no
+--with-rename-internal-libgeotiff-symbols=no
+--with-rename-internal-shapelib-symbols=no
 "
 
 termux_step_pre_configure() {
