6888c257bb5af7cf73c06c262e39fa9430ac4c36
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-18 13:30:17 +0900

    mumble-server: Bump to 1.4.287

--- x11-packages/mumble-server/build.sh
+++ x11-packages/mumble-server/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mumble.info/
 TERMUX_PKG_DESCRIPTION="Server module for Mumble, an open source voice-chat software"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.230
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.4.287
 TERMUX_PKG_SRCURL=https://github.com/mumble-voip/mumble.git
-TERMUX_PKG_DEPENDS="libcap, libdns-sd, libprotobuf, openssl-1.1, qt5-qtbase"
+TERMUX_PKG_DEPENDS="libc++, libcap, libdns-sd, libprotobuf, openssl-1.1, qt5-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dclient=OFF
@@ -33,7 +32,7 @@ termux_step_pre_configure() {
 
 termux_step_post_configure() {
 	if [ "$TERMUX_CMAKE_BUILD" == "Ninja" ]; then
-		sed -i s:${TERMUX_PREFIX//./\\.}'/bin/protoc[0-9.-]*:'$(which protoc):g \
+		sed -i s:${TERMUX_PREFIX//./\\.}'/bin/protoc[0-9.-]*:'$(command -v protoc):g \
 			build.ninja
 	fi
 }
@@ -43,6 +42,6 @@ termux_step_post_make_install() {
 	cd $TERMUX_PKG_SRCDIR/scripts
 	install -Dm600 -t $TERMUX_PREFIX/etc/dbus-1/system.d murmur.conf
 	install -Dm600 -t $TERMUX_PREFIX/etc murmur.ini
-	install -Dm700 -t $TERMUX_PREFIX/bin murmur-user-wrapper
+	install -Dm700 -t $TERMUX_PREFIX/bin mumble-server-user-wrapper
 	install -Dm600 -t $TERMUX_PREFIX/share/doc/mumble-server/examples murmur.ini
 }
