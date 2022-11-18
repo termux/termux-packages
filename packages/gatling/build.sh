090244b05ea11b38d8f9101270ab6e95179b293b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-18 17:26:18 +0900

    gatling: Bump to 0.16

--- packages/gatling/build.sh
+++ packages/gatling/build.sh
@@ -2,20 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://www.fefe.de/gatling/
 TERMUX_PKG_DESCRIPTION="A high performance http, ftp and smb server"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.15
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.16
 TERMUX_PKG_SRCURL=https://www.fefe.de/gatling/gatling-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=6fa329d0ced0c80deb8dde5460e9d9e984bee94f265043d7fdec0e253dce9aa4
+TERMUX_PKG_SHA256=5f96438ee201d7f1f6c2e0849ff273b196bdc7493f29a719ce8ed08c8be6365b
 TERMUX_PKG_DEPENDS="libcrypt, libiconv, openssl, zlib"
-TERMUX_PKG_BUILD_DEPENDS="libowfat"
+TERMUX_PKG_BUILD_DEPENDS="libcap, libowfat"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+prefix=$TERMUX_PREFIX
+MANDIR=$TERMUX_PREFIX/share/man
+"
 
-termux_step_make() {
-	CPPFLAGS+=" -I$TERMUX_PREFIX/include/libowfat"
+termux_step_pre_configure() {
+	CFLAGS+=" $CPPFLAGS"
 	LDFLAGS+=" -lcrypt -lcrypto -liconv"
-	make -j $TERMUX_MAKE_PROCESSES CROSS="${CC/clang}"
-}
-
-termux_step_make_install() {
-	make install prefix=$TERMUX_PREFIX MANDIR=$TERMUX_PREFIX/share/man
 }
