40e4df57ad6687276310aecc65393941e1089eae
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-19 03:40:34 +0900

    mediainfo: Bump to 22.09

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=22.03
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=22.09
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6236f92a1d57a8174b3f30ea6dde8aab538d8f620f728224d851373169802be4
+TERMUX_PKG_SHA256=67f301276a2b82d76cb9f4bd525caa51d2dc6d4359fce774fbd5f25f1d513044
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libmediainfo, libzen"
 
 termux_step_pre_configure() {
@@ -14,6 +13,4 @@ termux_step_pre_configure() {
 	TERMUX_PKG_BUILDDIR="${TERMUX_PKG_SRCDIR}"
 	cd "${TERMUX_PKG_SRCDIR}"
 	./autogen.sh
-
-	LDFLAGS+=" -Wl,--no-as-needed,-landroid-support,--as-needed"
 }
