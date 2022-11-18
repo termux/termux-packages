e75bb6439272fc1a8cf986ae714e5a408c6f5287
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-18 19:53:08 +0900

    libowfat: Bump to 0.33

--- packages/libowfat/build.sh
+++ packages/libowfat/build.sh
@@ -2,18 +2,20 @@ TERMUX_PKG_HOMEPAGE=http://www.fefe.de/libowfat/
 TERMUX_PKG_DESCRIPTION="GPL reimplementation of libdjb"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.32
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=http://www.fefe.de/libowfat/libowfat-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=f4b9b3d9922dc25bc93adedf9e9ff8ddbebaf623f14c8e7a5f2301bfef7998c1
+TERMUX_PKG_VERSION=0.33
+TERMUX_PKG_SRCURL=https://www.fefe.de/libowfat/libowfat-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=311ec8b3f4b72bb442e323fb013a98f956fa745547f2bc9456287b20d027cd7d
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+prefix=$TERMUX_PREFIX
+LIBDIR=$TERMUX_PREFIX/lib
+MAN3DIR=$TERMUX_PREFIX/share/man/man3
+"
+TERMUX_MAKE_PROCESSES=1
 
-termux_step_make() {
-	make -j $TERMUX_MAKE_PROCESSES CROSS="${CC/clang}"
-}
-
-termux_step_make_install() {
-	make install prefix=$TERMUX_PREFIX LIBDIR=$TERMUX_PREFIX/lib \
-		MAN3DIR=$TERMUX_PREFIX/share/man/man3
+termux_step_pre_configure() {
+	# Use pregenerated entities.h.
+	cp $TERMUX_PKG_BUILDER_DIR/entities.h $TERMUX_PKG_BUILDDIR/
+	touch -d "next hour" $TERMUX_PKG_BUILDDIR/entities.h
 }
