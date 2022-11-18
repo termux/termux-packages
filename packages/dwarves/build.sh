1211e9d1ce37f4872eb4fd685a0504bc2a3b740a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-18 17:12:13 +0900

    dwarves: Bump to 1.24

--- packages/dwarves/build.sh
+++ packages/dwarves/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://git.kernel.org/cgit/devel/pahole/pahole.git/
 TERMUX_PKG_DESCRIPTION="Pahole and other DWARF utils"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.23
+TERMUX_PKG_VERSION=1.24
 TERMUX_PKG_SRCURL=http://fedorapeople.org/~acme/dwarves/dwarves-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=f085c25f068627d10e54bd030464f8952f5b2211d4ba26047fe209377470862a
-TERMUX_PKG_DEPENDS="argp, libdw, zlib"
+TERMUX_PKG_SHA256=576bc112b95937dfbcd347c423696ee9e1992a338fdca1acacca736fd95f69c2
+TERMUX_PKG_DEPENDS="libdw, libelf, zlib"
+TERMUX_PKG_BUILD_DEPENDS="argp"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-D__LIB=lib"
 
 termux_step_pre_configure() {
