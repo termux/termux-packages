d7c3d430dd9d14f46f6bb5d0aec6db001fac0ae1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-18 13:21:16 +0900

    gawk: Bump to 5.2.1

--- packages/gawk/build.sh
+++ packages/gawk/build.sh
@@ -2,16 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/gawk/
 TERMUX_PKG_DESCRIPTION="Programming language designed for text processing"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.1.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.2.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gawk/gawk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d87629386e894bbea11a5e00515fc909dc9b7249529dad9e6a3a2c77085f7ea2
+TERMUX_PKG_SHA256=673553b91f9e18cc5792ed51075df8d510c9040f550a6f74e09c9add243a7e4f
 TERMUX_PKG_DEPENDS="libandroid-support, libgmp, libmpfr, readline"
 TERMUX_PKG_BREAKS="gawk-dev"
 TERMUX_PKG_REPLACES="gawk-dev"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_RM_AFTER_INSTALL="bin/gawk-* bin/igawk share/man/man1/igawk.1"
 TERMUX_PKG_GROUPS="base-devel"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-pma
+"
 
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
