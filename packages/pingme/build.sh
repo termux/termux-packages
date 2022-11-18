1ef0fd3e9f633e8edee7c46e7290e2fdd952bc87
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-18 23:07:38 +0900

    pingme: Bump to 0.2.5

--- packages/pingme/build.sh
+++ packages/pingme/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/kha7iq/pingme
 TERMUX_PKG_DESCRIPTION="A small utility which can be called from anywhere to send a message with particular information"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.2.4
+TERMUX_PKG_VERSION=0.2.5
 TERMUX_PKG_SRCURL=https://github.com/kha7iq/pingme/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6f65858f7b44d9c4a327128ef020d34c4e8294244bece5f8867a08c6da81bc62
+TERMUX_PKG_SHA256=5496d13630ae5988fbc6b2353bfed7eba7c5658875e019d72faa217be2fa8a61
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
