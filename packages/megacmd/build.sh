942ccb446eda9a5b2b4818466bb6d65b7491b616
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-19 13:46:37 +0900

    megacmd: Bump to 1.5.1

--- packages/megacmd/build.sh
+++ packages/megacmd/build.sh
@@ -2,25 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://mega.io/
 TERMUX_PKG_DESCRIPTION="Provides non UI access to MEGA services"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.1
-TERMUX_PKG_REVISION=9
-_COMMIT=fd50adec27d1c2fdea8f7264cb30b8d8117d0699
+TERMUX_PKG_VERSION=1.5.1
 TERMUX_PKG_SRCURL=https://github.com/meganz/MEGAcmd.git
-TERMUX_PKG_GIT_BRANCH=master
-_SDK_COMMIT=0e79b2739f695d08efed5a61bbf44362e127c30b
-TERMUX_PKG_DEPENDS="c-ares, cryptopp, ffmpeg, freeimage, libc++, libcurl, libsodium, libsqlite, libuv, libzen, mediainfo, openssl, pcre, readline, zlib"
+TERMUX_PKG_GIT_BRANCH=${TERMUX_PKG_VERSION}_Linux
+TERMUX_PKG_DEPENDS="c-ares, cryptopp, ffmpeg, freeimage, libc++, libcurl, libsodium, libsqlite, libuv, mediainfo, openssl, pcre, readline, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-static
 --with-pcre=$TERMUX_PREFIX
 ac_cv_lib_pthread_pthread_create=yes
 "
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-	( cd sdk && git checkout $_SDK_COMMIT )
-}
 
 termux_step_pre_configure() {
 	autoreconf -fi
