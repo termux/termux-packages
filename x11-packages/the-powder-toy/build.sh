7a0bc878a43b3e77d2b5ad427a566042db7036a4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-19 17:11:47 +0900

    the-powder-toy: Bump to 96.2.350

--- x11-packages/the-powder-toy/build.sh
+++ x11-packages/the-powder-toy/build.sh
@@ -2,22 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://powdertoy.co.uk/
 TERMUX_PKG_DESCRIPTION="The Powder Toy is a free physics sandbox game"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=95.0
-TERMUX_PKG_REVISION=19
+TERMUX_PKG_VERSION=96.2.350
 TERMUX_PKG_SRCURL=https://github.com/ThePowderToy/The-Powder-Toy/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f60c3dc93e4ceddeda92b768e75a2d218f8df3da4a569b7d7cb57fff5515e15b
-TERMUX_PKG_DEPENDS="fftw, libbz2, libc++, libcurl, liblua52, sdl2, libx11, zlib"
-TERMUX_PKG_FOLDERNAME=The-Powder-Toy-${TERMUX_PKG_VERSION}
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make() {
-	export CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include"
-	export CXXFLAGS="${CFLAGS}"
-	export LINKFLAGS="${LDFLAGS}"
-	scons -j4 --lin --64bit --no-sse --lua52
-}
+TERMUX_PKG_SHA256=d95cbadee22632687661e8fc488bd64405d81c0dca737e16420f26e93ea5bf58
+TERMUX_PKG_DEPENDS="fftw, libc++, libcurl, libluajit, sdl2, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Db_pie=true
+"
 
 termux_step_make_install() {
-	install -Dm755 "${TERMUX_PKG_SRCDIR}/build/powder64-legacy" "${TERMUX_PREFIX}/bin/the-powder-toy"
-	ln -sfr "${TERMUX_PREFIX}/bin/the-powder-toy" "${TERMUX_PREFIX}/bin/powder"
+	install -Dm700 -t $TERMUX_PREFIX/bin powder
+	ln -sf powder $TERMUX_PREFIX/bin/the-powder-toy
 }
