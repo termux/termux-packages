a951f0667eb22d8368fab6712e471fcaa09b773e
leap123 <leapofazzam@gmail.com>
2022-11-19 07:11:54 +0700

    fix

--- packages/zig/build.sh
+++ packages/zig/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@leapofazzam123"
 TERMUX_PKG_VERSION=0.10.0
 TERMUX_PKG_SRCURL=https://ziglang.org/download/$TERMUX_PKG_VERSION/zig-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=d8409f7aafc624770dcd050c8fa7e62578be8e6a10956bca3c86e8531c64c136
-TERMUX_PKG_DEPENDS="libandroid-spawn, libc++, libxml2, ncurses, zlib"
+TERMUX_PKG_DEPENDS="libandroid-support, libandroid-spawn, libc++, libxml2, ncurses, zlib"
 TERMUX_PKG_BUILD_DEPENDS="llvm, libllvm-static"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DZIG_PREFER_CLANG_CPP_DYLIB=OFF
@@ -28,5 +28,5 @@ termux_step_pre_configure() {
 		-DZIG_EXECUTABLE=$(command -v zig)
 		-DZIG_TARGET_TRIPLE=$ZIG_TARGET_NAME
 		"
-        LDFLAGS+=" -landroid-spawn -lncursesw -lxml2 -lz $($CC -print-libgcc-file-name)"
+        LDFLAGS+=" -landroid-support -landroid-spawn -lncursesw -lxml2 -lz $($CC -print-libgcc-file-name)"
 }
