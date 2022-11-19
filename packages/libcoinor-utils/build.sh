ac0216816be556128eee37c7eb03ecee8f0fc201
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-19 14:25:39 +0900

    libcoinor-utils: Bump to 2.11.6

--- packages/libcoinor-utils/build.sh
+++ packages/libcoinor-utils/build.sh
@@ -2,24 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/coin-or/CoinUtils
 TERMUX_PKG_DESCRIPTION="An open-source collection of classes and helper functions for COIN-OR projects"
 TERMUX_PKG_LICENSE="EPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=9035d7a1b5b6daed9b067f16f440420e4477abac
-TERMUX_PKG_VERSION=2022.01.03
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/coin-or/CoinUtils.git
-TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_VERSION=1:2.11.6
+TERMUX_PKG_SRCURL=https://github.com/coin-or/CoinUtils/archive/refs/tags/releases/${TERMUX_PKG_VERSION#*:}.tar.gz
+TERMUX_PKG_SHA256=6ea31d5214f7eb27fa3ffb2bdad7ec96499dd2aaaeb4a7d0abd90ef852fc79ca
 TERMUX_PKG_DEPENDS="libc++"
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
-}
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
