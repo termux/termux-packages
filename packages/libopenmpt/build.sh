9c72d7426ec11f8560a22ee367dce1dc4c17faa7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 20:21:26 +0900

    libopenmpt: Bump to 0.5.20

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://lib.openmpt.org/libopenmpt/
 TERMUX_PKG_DESCRIPTION="OpenMPT based module player library and libopenmpt based command-line player"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.5.12
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=0.5.20
 TERMUX_PKG_SRCURL=https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${TERMUX_PKG_VERSION}+release.autotools.tar.gz
-TERMUX_PKG_SHA256=892aea7a599b5d21842bebf463b5aafdad5711be7008dd84401920c6234820af
-TERMUX_PKG_DEPENDS="libc++, libflac, mpg123, pulseaudio"
+TERMUX_PKG_SHA256=3e056fb30d1ccf88e10ed798b57cfcc0d7c8ec275d087ff6d6cf0fecdc332ba3
+TERMUX_PKG_DEPENDS="libc++, libflac, libogg, libsndfile, libvorbis, mpg123, pulseaudio, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-portaudio
