64845fb81a6499c614d5ee674f514fd175f9fd9f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 21:44:34 +0900

    synaptic: Bump to 0.91.2

--- x11-packages/synaptic/build.sh
+++ x11-packages/synaptic/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.nongnu.org/synaptic/
 TERMUX_PKG_DESCRIPTION="Synaptic is a graphical package management tool based on GTK+ and APT."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
-TERMUX_PKG_VERSION=0.90.1
-_COMMIT=a85c743e10e39a43737d5f8a0f0cc4ea755eb545
-TERMUX_PKG_SRCURL=https://github.com/mvo5/synaptic/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=a500cfeea6c4ec4463b42495cba224b9548acbd557e9da695730acf7e49e0798
-TERMUX_PKG_DEPENDS="libc++, apt, dpkg, gtk3, atk, libvte, hicolor-icon-theme"
-TERMUX_PKG_RECOMMENDS="netsurf"
+TERMUX_PKG_VERSION=0.91.2
+TERMUX_PKG_SRCURL=https://github.com/mvo5/synaptic/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=686c6b3f1eb96c3d9711ff39aaac365928e934d0b32ff4d0df29cb28ee5c41f0
+TERMUX_PKG_DEPENDS="apt, dpkg, gdk-pixbuf, glib, gtk3, libc++, libvte, pango"
+TERMUX_PKG_RECOMMENDS="hicolor-icon-theme, netsurf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure(){
