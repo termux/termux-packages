c13195c221fa286fc0073e192e9a3b3341f6873b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 18:15:57 +0900

    libtorrent-rasterbar: Bump to 2.0.8

--- packages/libtorrent-rasterbar/build.sh
+++ packages/libtorrent-rasterbar/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://libtorrent.org/
 TERMUX_PKG_DESCRIPTION="A feature complete C++ bittorrent implementation focusing on efficiency and scalability"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.7
+TERMUX_PKG_VERSION=2.0.8
 TERMUX_PKG_SRCURL=https://github.com/arvidn/libtorrent/releases/download/v${TERMUX_PKG_VERSION}/libtorrent-rasterbar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3850a27aacb79fcc4d352c1f02a7a59e0e8322afdaa1f5d58d676c02edfcfa36
+TERMUX_PKG_SHA256=09dd399b4477638cf140183f5f85d376abffb9c192bc2910002988e27d69e13e
 TERMUX_PKG_DEPENDS="boost, libc++, openssl, python"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 
