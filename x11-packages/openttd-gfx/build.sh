be60895e1048c8536b6b489ecfeab48707f90010
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 15:32:02 +0900

    openttd-gfx: Bump to 7.1

--- x11-packages/openttd-gfx/build.sh
+++ x11-packages/openttd-gfx/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://dev.openttdcoop.org/projects/opengfx
 TERMUX_PKG_DESCRIPTION="A free graphics set for openttd"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.5.5
-TERMUX_PKG_REVISION=24
+TERMUX_PKG_VERSION=7.1
 TERMUX_PKG_SRCURL=https://cdn.openttd.org/opengfx-releases/$TERMUX_PKG_VERSION/opengfx-$TERMUX_PKG_VERSION-all.zip
-TERMUX_PKG_SHA256=c648d56c41641f04e48873d83f13f089135909cc55342a91ed27c5c1683f0dfe
+TERMUX_PKG_SHA256=928fcf34efd0719a3560cbab6821d71ce686b6315e8825360fba87a7a94d7846
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
@@ -17,11 +16,10 @@ termux_step_get_source() {
 	unzip -d "$TERMUX_PKG_SRCDIR" "$TERMUX_PKG_CACHEDIR/opengfx-$TERMUX_PKG_VERSION.zip"
 
 	cd "$TERMUX_PKG_SRCDIR"
-	tar xf "opengfx-$TERMUX_PKG_VERSION.tar"
+	tar xf "opengfx-$TERMUX_PKG_VERSION.tar" --strip-components=1
 }
 
 termux_step_make_install() {
-	cd "opengfx-$TERMUX_PKG_VERSION"
 	install -d "$TERMUX_PREFIX"/share/openttd/data
 	install -m600 *.grf "$TERMUX_PREFIX"/share/openttd/data
 	install -m600 *.obg "$TERMUX_PREFIX"/share/openttd/data
