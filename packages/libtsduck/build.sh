1d9f407a2c236dea1e34225d57c856935f79b3c9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 20:40:58 +0900

    libtsduck: Bump to 3.32.2983

--- packages/libtsduck/build.sh
+++ packages/libtsduck/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://tsduck.io/
 TERMUX_PKG_DESCRIPTION="An extensible toolkit for MPEG transport streams"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-_VERSION=3.29-2651
+_VERSION=3.32-2983
 TERMUX_PKG_VERSION=${_VERSION//-/.}
 TERMUX_PKG_SRCURL=https://github.com/tsduck/tsduck/archive/refs/tags/v${_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cab8f5838993aa1abd1a6a4c2ef7f2afba801da02a4001904f3f5ba5c5fe85a0
+TERMUX_PKG_SHA256=2f0c35422a4f3e8876e9efa2683794336102fc225b826d5d3c4e033c4b666465
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, libcurl, libedit"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="
@@ -14,6 +14,8 @@ CROSS_TARGET=$TERMUX_ARCH
 CROSS=1
 NOTEST=1
 NODEKTEC=1
+NOHIDES=
+NOVATEK=1
 NOCURL=
 NOPCSC=1
 NOSRT=1
@@ -23,6 +25,7 @@ NOTELETEXT=1
 NOGITHUB=1 
 "
 TERMUX_PKG_RM_AFTER_INSTALL="etc/security etc/udev"
+TERMUX_PKG_HOSTBUILD=true
 
 termux_step_post_get_source() {
 	# Needs to call `termux_step_configure_autotools` for `*-config` hack.
@@ -30,7 +33,25 @@ termux_step_post_get_source() {
 	chmod u+x configure
 }
 
+termux_step_host_build() {
+	find $TERMUX_PKG_SRCDIR -mindepth 1 -maxdepth 1 -exec cp -a \{\} ./ \;
+	make -j $TERMUX_MAKE_PROCESSES \
+		NOTEST=1 \
+		NODEKTEC=1 \
+		NOHIDES=1 \
+		NOVATEK=1 \
+		NOCURL=1 \
+		NOPCSC=1 \
+		NOSRT=1 \
+		NORIST=1 \
+		NOEDITLINE=1 \
+		NOTELETEXT=1 \
+		NOGITHUB=1
+}
+
 termux_step_pre_configure() {
+	PATH=$TERMUX_PKG_HOSTBUILD_DIR/bin/release:$PATH
+
 	CXXFLAGS+=" -fno-strict-aliasing"
 	LDFLAGS+=" -landroid-glob"
 }
