a19a90bfe34f935cd58f483c19cbd50e03c24b90
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 20:45:29 +0900

    dar: Bump to 2.7.7

--- packages/dar/build.sh
+++ packages/dar/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=http://dar.linux.free.fr/
 TERMUX_PKG_DESCRIPTION="A full featured command-line backup tool, short for Disk ARchive"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.7.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.7.7
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/dar/dar/${TERMUX_PKG_VERSION}/dar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=973fa977c19b32b1f9ecb62153c810ba8696f644eca048f214c77ad0e8eca255
-TERMUX_PKG_DEPENDS="attr, libbz2, libc++, libgcrypt, liblzma, liblzo, zlib"
+TERMUX_PKG_SHA256=c03e2f52efd65a2f047b60bbeda2460cb525165e1be32f110b60e0cece3f2cc9
+TERMUX_PKG_DEPENDS="attr, libbz2, libc++, libgcrypt, libgpg-error, liblzma, liblzo, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
@@ -16,4 +15,5 @@ termux_step_pre_configure() {
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-mode=64"
 	fi
 	CXXFLAGS+=" $CPPFLAGS"
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
