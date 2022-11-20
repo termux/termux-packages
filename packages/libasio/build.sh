a6fcf57df77cd0351a0c5eca4abb9ea1964f26e4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 18:25:23 +0900

    libasio: Bump to 1.24.0

--- packages/libasio/build.sh
+++ packages/libasio/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE="https://think-async.com/Asio"
 TERMUX_PKG_DESCRIPTION="Cross-platform C++ library for network and low-level I/O programming"
 TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.22.2"
-TERMUX_PKG_SRCURL="http://downloads.sourceforge.net/project/asio/asio/$TERMUX_PKG_VERSION%20%28Stable%29/asio-$TERMUX_PKG_VERSION.tar.bz2"
-TERMUX_PKG_SHA256=776bb781eee8022a3eed71de28f05bf8ba09741db2e57ad8cc420aa0884cbc6c
+TERMUX_PKG_VERSION=1.24.0
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/asio/asio/${TERMUX_PKG_VERSION}%20%28Stable%29/asio-${TERMUX_PKG_VERSION}.zip
+TERMUX_PKG_SHA256=b628291471aee837ff951ea38c8a3a0b00ff99fd2a1ff3b7c7d3d993c0030b73
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, openssl"
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_pre_configure() {
 	sed --in-place -e 's/-lrt//' configure.ac
