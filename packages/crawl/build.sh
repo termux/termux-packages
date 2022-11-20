4159a8290b507fb5deca1b78cbb315b57a3bc6fb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 21:02:21 +0900

    crawl: Bump to 0.30-a0

--- packages/crawl/build.sh
+++ packages/crawl/build.sh
@@ -2,20 +2,28 @@ TERMUX_PKG_HOMEPAGE=https://crawl.develz.org/
 TERMUX_PKG_DESCRIPTION="Roguelike adventure through dungeons filled with dangerous monsters"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_SRCURL=https://github.com/crawl/crawl.git
-TERMUX_PKG_VERSION=0.29-a0
-TERMUX_PKG_GIT_BRANCH=0.29-a0
-TERMUX_PKG_DEPENDS="libc++, ncurses, zlib"
+TERMUX_PKG_VERSION=0.30-a0
+TERMUX_PKG_SRCURL=https://github.com/crawl/crawl/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1f2c2d30f5cbde71da551ccc75bbc803ac01dd2507f66ecb19911dc81c49a59b
+TERMUX_PKG_DEPENDS="libc++, liblua51, libsqlite, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+FHS=yes
+--debug
+"
 
-termux_step_pre_configure() {
-	export CROSSHOST="$TERMUX_HOST_PLATFORM"
-	export TERMUX_PKG_BUILDDIR="$TERMUX_PKG_SRCDIR/crawl-ref/source"
-
-	export INCLUDES_L="-I$TERMUX_PREFIX/include"
-	export LIBS="-llog -Wl,--rpath=$TERMUX_PREFIX/lib"
+termux_step_post_get_source() {
+	echo "Applying util-gen_ver.pl.diff"
+	sed "s|@VERSION@|${TERMUX_PKG_VERSION#*:}|g" \
+		$TERMUX_PKG_BUILDER_DIR/util-gen_ver.pl.diff \
+		| patch --silent -p1
+	sed -i 's|\(__ANDROID_\)_|\1_NO_TERMUX__|g' crawl-ref/source/syscalls.cc
 }
 
-termux_step_post_configure() {
-	sed -i 's,#ifdef __ANDROID__,#ifdef __NO_THANKS__,g' "$TERMUX_PKG_BUILDDIR/syscalls.cc"
+termux_step_pre_configure() {
+	TERMUX_PKG_SRCDIR+="/crawl-ref/source"
+	TERMUX_PKG_BUILDDIR=$TERMUX_PKG_SRCDIR
+
+	export DEFINES_L="-DHAVE_STAT"
+	export LIBS="-llog -Wl,--rpath=$TERMUX_PREFIX/lib"
 }
