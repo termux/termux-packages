8ef23ecd0969fd997ad8e067aad0067c66c449df
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-11-20 15:48:22 +0800

    bump(main/tur-repo): 1.0.1
    Add tur-continuous

--- packages/tur-repo/build.sh
+++ packages/tur-repo/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux-user-repository/tur
 TERMUX_PKG_DESCRIPTION="A single and trusted place for all unofficial/less popular termux packages"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_VERSION=1.0.1
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
-	echo "deb https://tur.kcubeterm.com tur-packages tur tur-on-device" > $TERMUX_PREFIX/etc/apt/sources.list.d/tur.list
+	echo "deb https://tur.kcubeterm.com tur-packages tur tur-on-device tur-continuous" > $TERMUX_PREFIX/etc/apt/sources.list.d/tur.list
 	## tur gpg key
 	mkdir -p $TERMUX_PREFIX/etc/apt/trusted.gpg.d
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/tur.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
