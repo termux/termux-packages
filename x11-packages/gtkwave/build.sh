2cf619a0069c2fd9560a0548ea4f374c85c110d5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 21:30:37 +0900

    gtkwave: Bump to 3.3.113

--- x11-packages/gtkwave/build.sh
+++ x11-packages/gtkwave/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=http://gtkwave.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="A wave viewer which reads LXT, LXT2, VZT, GHW and VCD/EVCD files"
 TERMUX_PKG_LICENSE="GPL-2.0, MIT"
+TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.3.110
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.3.113
 TERMUX_PKG_SRCURL=http://gtkwave.sourceforge.net/gtkwave-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e4c6edd53a59cf43188707df264b61dead9cf747502bcf80f62a2799c6ab54c3
-TERMUX_PKG_DEPENDS="libc++, bzip2, gtk2, desktop-file-utils, libandroid-shmem, xz-utils"
+TERMUX_PKG_SHA256=d6c546ee0bdfa5e5211dbfe51a5cdfa32be8561a6f22d53148bd685b217313ff
+TERMUX_PKG_DEPENDS="glib, gtk2, libandroid-shmem, libbz2, libc++, liblzma, pango, zlib"
+TERMUX_PKG_RECOMMENDS="desktop-file-utils"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-tcl --disable-mime-update"
