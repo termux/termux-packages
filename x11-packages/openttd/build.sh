07a67bb4f294efb437bfa2b9e40e7c1f9d12863d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 16:47:58 +0900

    openttd: Bump to 12.2

--- x11-packages/openttd/build.sh
+++ x11-packages/openttd/build.sh
@@ -2,21 +2,30 @@ TERMUX_PKG_HOMEPAGE=https://www.openttd.org/
 TERMUX_PKG_DESCRIPTION="An engine for running Transport Tycoon Deluxe"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.10.3
-TERMUX_PKG_REVISION=10
+TERMUX_PKG_VERSION=12.2
 TERMUX_PKG_SRCURL=https://github.com/OpenTTD/OpenTTD/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=eb57249c221b9b7cb1c86d81e60bbc54661d7e2ead50f529a2c1d733f6bbf381
-TERMUX_PKG_DEPENDS="desktop-file-utils, fontconfig, hicolor-icon-theme, libc++, libicu, liblzma, liblzo, libpng, openttd-gfx, openttd-msx, openttd-sfx, sdl"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_SHA256=05cecb1a12fd5e2d6d70010ba8a3ef1c6bea8c49a4a9cf7351bc9594343a714f
+TERMUX_PKG_DEPENDS="fontconfig, freetype, libc++, libicu, liblzma, liblzo, libpng, openttd-gfx, openttd-msx, openttd-sfx, sdl2, zlib"
+TERMUX_PKG_RECOMMENDS="desktop-file-utils, hicolor-icon-theme"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBINARY_NAME=openttd
+-DCMAKE_INSTALL_DATADIR=share
+-DCMAKE_INSTALL_BINDIR=bin
+"
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
+-DOPTION_DEDICATED=ON
+"
 
-termux_step_configure() {
+termux_step_host_build() {
+	termux_setup_cmake
+	cmake "$TERMUX_PKG_SRCDIR" $TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS
+	make -j $TERMUX_MAKE_PROCESSES
+}
+
+termux_step_pre_configure() {
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DHOST_BINARY_DIR=$TERMUX_PKG_HOSTBUILD_DIR"
+	fi
 	CXXFLAGS+=" -DU_USING_ICU_NAMESPACE=1"
-
-	./configure \
-		--prefix-dir="$TERMUX_PREFIX" \
-		--binary-name=openttd \
-		--binary-dir=bin \
-		--data-dir=share/openttd \
-		--doc-dir=share/doc/openttd \
-		--menu-name=OpenTTD
 }
