1adbc04004f57ceacbe952f90c249ba0cda140ee
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-20 15:40:29 +0900

    openttd-sfx: Bump to 1.0.3

--- x11-packages/openttd-sfx/build.sh
+++ x11-packages/openttd-sfx/build.sh
@@ -3,12 +3,22 @@ TERMUX_PKG_DESCRIPTION="Free sound set for openttd"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="license.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.2.3
-TERMUX_PKG_REVISION=24
+TERMUX_PKG_VERSION=1.0.3
 TERMUX_PKG_SRCURL=https://cdn.openttd.org/opensfx-releases/$TERMUX_PKG_VERSION/opensfx-$TERMUX_PKG_VERSION-all.zip
-TERMUX_PKG_SHA256=6831b651b3dc8b494026f7277989a1d757961b67c17b75d3c2e097451f75af02
+TERMUX_PKG_SHA256=e0a218b7dd9438e701503b0f84c25a97c1c11b7c2f025323fb19d6db16ef3759
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+termux_step_get_source() {
+	termux_download \
+		"$TERMUX_PKG_SRCURL" \
+		"$TERMUX_PKG_CACHEDIR/opensfx-$TERMUX_PKG_VERSION.zip" \
+		"$TERMUX_PKG_SHA256"
+	unzip -d "$TERMUX_PKG_SRCDIR" "$TERMUX_PKG_CACHEDIR/opensfx-$TERMUX_PKG_VERSION.zip"
+
+	cd "$TERMUX_PKG_SRCDIR"
+	tar xf "opensfx-$TERMUX_PKG_VERSION.tar" --strip-components=1
+}
 
 termux_step_make_install() {
 	install -d "$TERMUX_PREFIX"/share/openttd/data
