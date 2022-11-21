559462661158ea2f109efde75508121af5985899
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-21 21:09:54 +0900

    gst-plugins-ugly: Bump to 1.20.4

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer Ugly Plug-ins"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.20.3
+TERMUX_PKG_VERSION=1.20.4
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8caa20789a09c304b49cf563d33cca9421b1875b84fcc187e4a385fa01d6aefd
+TERMUX_PKG_SHA256=5c9ec6bab96517e438b3f9bae0ceb84d3436f3da9bbe180cf4d28e32a7251b59
 TERMUX_PKG_DEPENDS="glib, gst-plugins-base"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=disabled
