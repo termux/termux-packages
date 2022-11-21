6beac65fb8111d83fc056b4f3c21a456251f1126
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-21 21:07:33 +0900

    gst-plugins-base: Bump to 1.20.4

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.20.3
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.20.4
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7e30b3dd81a70380ff7554f998471d6996ff76bbe6fc5447096f851e24473c9f
+TERMUX_PKG_SHA256=8d181b7abe4caf23ee9f9ec5b4d3e232640452464e39495bfffb6d776fc97225
 TERMUX_PKG_DEPENDS="glib, graphene, gstreamer, libandroid-shmem, libjpeg-turbo, libogg, libopus, libpng, libtheora, libvorbis, libx11, libxcb, libxext, libxv, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, mesa-dev"
 TERMUX_PKG_BREAKS="gst-plugins-base-dev"
