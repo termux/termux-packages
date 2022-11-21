8d5698ae68027de8471eb97a5f8cb86281dcb90d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-21 18:15:40 +0900

    new package: libwolfssl

---
+++ packages/libwolfssl/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.wolfssl.com/
+TERMUX_PKG_DESCRIPTION="A small, fast, portable implementation of TLS/SSL for embedded devices to the cloud"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=5.5.3
+TERMUX_PKG_SRCURL=https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${TERMUX_PKG_VERSION}-stable.tar.gz
+TERMUX_PKG_SHA256=fd3135b8657d09fb96a8aad16585da850b96ea420ae8ce5ac4d5fdfc614c2683
