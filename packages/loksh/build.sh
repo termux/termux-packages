3d351ba8f8d7b85b0301c13ea3f5af65288809ff
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-21 16:41:55 +0900

    loksh: Bump to 7.2

--- packages/loksh/build.sh
+++ packages/loksh/build.sh
@@ -2,19 +2,29 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dimkr/loksh
 TERMUX_PKG_DESCRIPTION="A Linux port of OpenBSD's ksh"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux"
-# Versions >6.7 fail with lots errors in NDK headers.
-#
-#  Example error:
-#
-#  In file included from ../src/subprojects/lolibc/include/sys/types.h:5:
-#  /home/builder/.termux-build/_cache/android-r20-api-24-v3/bin/../sysroot/usr/include/time.h:96:1: error: unknown type name 'clock_t'
-#  clock_t clock(void);
-#  ^
-#  /home/builder/.termux-build/_cache/android-r20-api-24-v3/bin/../sysroot/usr/include/time.h:100:25: error: unknown type name 'pid_t'
-#  int clock_getcpuclockid(pid_t __pid, clockid_t* __clock) __INTRODUCED_IN(23);
-TERMUX_PKG_VERSION=6.6
-TERMUX_PKG_SRCURL=https://github.com/dimkr/loksh/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=97a020df82ceebe216c5a306e87360c5e3398d7403347aaff50978446ccb764d
-#TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_VERSION=7.2
+TERMUX_PKG_SRCURL=https://github.com/dimkr/loksh.git
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	pushd subprojects/lolibc
+	mv include _include_lolibc
+	mkdir include
+	mv _include_lolibc include/lolibc
+	pushd include/lolibc
+	local _LOLIBC_HEADERS=$(find * -name '*.h')
+	popd
+	popd
+	local f
+	for f in $(find . -name '*.[ch]'); do
+		local h
+		for h in ${_LOLIBC_HEADERS}; do
+			sed -i "s:#include <${h//./\\.}>:#include <lolibc/${h}>:g" ${f}
+		done
+	done
+	cd subprojects/lolibc/include/lolibc
+	for f in ${_LOLIBC_HEADERS}; do
+		sed -i "s:#include_next :#include :g" ${f}
+	done
+}
