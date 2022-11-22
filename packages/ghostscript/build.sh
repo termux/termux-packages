f7af3d12671c4c4e377ea80f2a592802fc453b36
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-22 18:12:45 +0900

    ghostscript: Bump to 10.0.0

--- packages/ghostscript/build.sh
+++ packages/ghostscript/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.ghostscript.com/
 TERMUX_PKG_DESCRIPTION="Interpreter for the PostScript language and for PDF"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=9.56.1
+TERMUX_PKG_VERSION=10.0.0
 TERMUX_PKG_SRCURL=https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs${TERMUX_PKG_VERSION//.}/ghostpdl-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6bf362286e359e31f934e5aad49db3d88a2382a3cac44b40572861ee5c536664
-TERMUX_PKG_DEPENDS="libc++, fontconfig, libandroid-support, libiconv, libtiff, libjpeg-turbo, libpng, libexpat, freetype, libidn, littlecms, openjpeg, zstd"
+TERMUX_PKG_SHA256=f30283f01a6966009a2e7b7553decdb5ec805501f3e24e5d86b0017fe16fbdba
+TERMUX_PKG_DEPENDS="fontconfig, freetype, jbig2dec, libandroid-support, libc++, libiconv, libidn, libjpeg-turbo, libpng, libtiff, littlecms, openjpeg, zlib"
+TERMUX_PKG_BUILD_DEPENDS="libexpat"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_lcms2__cmsCreateMutex=yes
@@ -22,7 +23,7 @@ CCAUX=gcc
 TERMUX_PKG_MAKE_INSTALL_TARGET="install-so install"
 
 termux_step_post_get_source() {
-	rm -rdf $TERMUX_PKG_SRCDIR/{jpeg,libpng,expat,jasper,freetype,lcms2,tiff,openjpeg}
+	rm -rdf $TERMUX_PKG_SRCDIR/{expat,freetype,jbig2dec,jpeg,lcms2mt,libpng,openjpeg,tiff,zlib}
 }
 
 termux_step_pre_configure() {
