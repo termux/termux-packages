7720dc8bf8a6118cfcae52e0ea11d2f6ed3a9e51
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 04:13:17 +0900

    shellinabox: Bump to 2.21

--- packages/shellinabox/build.sh
+++ packages/shellinabox/build.sh
@@ -2,11 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/shellinabox/shellinabox
 TERMUX_PKG_DESCRIPTION="Implementation of a web server that can export arbitrary command line tools to a web based terminal emulator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.20
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/shellinabox/shellinabox/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=27a5ec6c3439f87aee238c47cc56e7357a6249e5ca9ed0f044f0057ef389d81e
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_VERSION=2.21
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/s/shellinabox/shellinabox_${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=4ec657182b3ec628c2a7b036b360011cef51a23104b2eb332eafede56528a632
 TERMUX_PKG_DEPENDS="openssl, openssl-tool, termux-auth (>= 1.2), zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-login
