cec1c4e3fdbcffcf5e45514e5c71106a404e183a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 02:28:29 +0900

    liblua53: Bump to 5.3.6

--- packages/liblua53/lua53.subpackage.sh
+++ packages/liblua53/lua53.subpackage.sh
@@ -1,5 +1,6 @@
 TERMUX_SUBPKG_INCLUDE="bin/ share/man/man1/"
 TERMUX_SUBPKG_DESCRIPTION="Simple, extensible, embeddable programming language"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
 TERMUX_SUBPKG_DEPENDS="readline"
 TERMUX_SUBPKG_BREAKS="lua (<< 5.3.5-6)"
 TERMUX_SUBPKG_REPLACES="lua (<< 5.3.5-6)"
