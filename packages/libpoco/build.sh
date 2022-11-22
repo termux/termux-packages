dac857640e17cd9c2be525cef7b4b1a399a7933e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 02:57:36 +0900

    libpoco: Fix dependency

--- packages/libpoco/build.sh
+++ packages/libpoco/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="A comprehensive set of C++ libraries that cover all mode
 TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.12.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pocoproject/poco/archive/refs/tags/poco-${TERMUX_PKG_VERSION}-release.tar.gz
 TERMUX_PKG_SHA256=71ef96c35fced367d6da74da294510ad2c912563f12cd716ab02b6ed10a733ef
-TERMUX_PKG_DEPENDS="libc++, libexpat, openssl, pcre2, sqlite, zlib"
+TERMUX_PKG_DEPENDS="libc++, libexpat, libsqlite, openssl, pcre2, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DPOCO_UNBUNDLED=ON"
