6690ad65f6c48891a7685157b2d0d7e22476515c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 03:20:23 +0900

    lnav: Bump to 0.11.1

--- packages/lnav/build.sh
+++ packages/lnav/build.sh
@@ -2,19 +2,23 @@ TERMUX_PKG_HOMEPAGE=https://lnav.org/
 TERMUX_PKG_DESCRIPTION="An advanced log file viewer for the small-scale"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.10.1
+TERMUX_PKG_VERSION=0.11.1
 TERMUX_PKG_SRCURL=https://github.com/tstack/lnav/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4af855a463493105ae0746fc0da80304a689b5394eb6abfeede4dd843127c8bc
-TERMUX_PKG_DEPENDS="libandroid-glob, libarchive, libbz2, libcurl, ncurses, pcre, readline, sqlite, zlib"
+TERMUX_PKG_SHA256=7d73ceeeae0fdc61cffce0f8a7a3435aa91b401b67d233ffaa018faab00d7d3f
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-system-paths
-ac_cv_header_execinfo_h=no
+--disable-static
+--with-pcre2=$TERMUX_PREFIX
 "
+
+termux_step_post_get_source() {
+	cp $TERMUX_PKG_BUILDER_DIR/sys_time.c src/
+}
 
 termux_step_pre_configure() {
 	autoreconf -fi
 
-	CPPFLAGS+=" -D_PATH_VARTMP=\\\"$TERMUX_PREFIX/tmp\\\""
 	LDFLAGS+=" -landroid-glob"
 }
