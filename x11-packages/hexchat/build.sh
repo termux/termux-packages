6aaed68c906bb3e78b0f0f6249b777e39617d129
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 15:05:46 +0900

    hexchat: Bump to 2.16.1

--- x11-packages/hexchat/build.sh
+++ x11-packages/hexchat/build.sh
@@ -2,30 +2,33 @@ TERMUX_PKG_HOMEPAGE=https://hexchat.github.io/
 TERMUX_PKG_DESCRIPTION="A popular and easy to use graphical IRC (chat) client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.14.3
-TERMUX_PKG_REVISION=24
-TERMUX_PKG_SRCURL=https://github.com/hexchat/hexchat/archive/v2.14.2.tar.gz
-TERMUX_PKG_SHA256=4f2c2137020913513ea559f788c41039ca6230764d8158862d5d1ee8785592d9
-TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk2, liblua53, libnotify, libx11, openssl, pango, python"
+TERMUX_PKG_VERSION=2.16.1
+TERMUX_PKG_SRCURL=https://github.com/hexchat/hexchat/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f15bc487312a96a902e042e8f197a8494a29bcf4a00bbfd276a5e8154263bfe5
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk2, liblua53, libx11, openssl, pango, python"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dwith-libproxy=false
--Dwith-libcanberra=false
--Dwith-dbus=false
+-Dlibcanberra=disabled
+-Ddbus=disabled
 -Dwith-lua=lua53
--Dwith-text=true
+-Dtext-frontend=true
 -Dwith-perl=false
 -Dwith-sysinfo=false
 "
 
 TERMUX_PKG_RM_AFTER_INSTALL="share/locale"
 
-termux_step_post_make_install() {
-	## TODO: patch it to force link with libandroid-shmem instead of
-	## using wrapper.
-	mkdir -p "${TERMUX_PREFIX}/libexec/"
-	mv "${TERMUX_PREFIX}/bin/hexchat" "${TERMUX_PREFIX}/libexec/"
-	sed "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
-		"${TERMUX_PKG_BUILDER_DIR}/hexchat.in" > "${TERMUX_PREFIX}/bin/hexchat"
-	chmod 700 "${TERMUX_PREFIX}/bin/hexchat"
+termux_step_pre_configure() {
+	local _PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	build-pip install cffi
 }
