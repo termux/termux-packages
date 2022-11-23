b7525f608b0c1d33e71e8465bfa8516af7e2289b
Aditya Alok <alok@termux.dev>
2022-11-24 03:32:45 +0530

    rebuild(packages/haskell-edit-distance): after ghc update to 9.2.5
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/haskell-edit-distance/build.sh
+++ packages/haskell-edit-distance/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Levenshtein and restricted Damerau-Levenshtein edit dist
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION="0.2.2.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/edit-distance-${TERMUX_PKG_VERSION}/edit-distance-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="3e8885ee2f56ad4da940f043ae8f981ee2fe336b5e8e4ba3f7436cff4f526c4a"
 TERMUX_PKG_BUILD_IN_SRC=true
