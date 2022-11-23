75af015d4c5631f72898ca140919a4b0798871c5
Aditya Alok <alok@termux.dev>
2022-11-24 03:34:13 +0530

    rebuild(packages/haskell-semialign): after ghc update to 9.2.5
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/haskell-semialign/build.sh
+++ packages/haskell-semialign/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Align and Zip type-classes from the common Semialign anc
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION="1.2.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/semialign-${TERMUX_PKG_VERSION}/semialign-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="d900697041ae4b0cca3243273a2b3e80bcf74d937405d6a5ff34dc33ee952132"
 TERMUX_PKG_BUILD_IN_SRC=true
