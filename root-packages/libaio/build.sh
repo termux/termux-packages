1ee004985869722e87726435cd5fb1d16ddfa94e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 02:35:32 +0900

    libaio: Bump to 0.3.113

--- root-packages/libaio/build.sh
+++ root-packages/libaio/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://lse.sourceforge.net/io/aio.html
+TERMUX_PKG_HOMEPAGE=https://pagure.io/libaio
 TERMUX_PKG_DESCRIPTION="Linux kernel AIO access library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.112
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=http://ftp.de.debian.org/debian/pool/main/liba/libaio/libaio_${TERMUX_PKG_VERSION}.orig.tar.xz
-TERMUX_PKG_SHA256=f69e5800425f4ea957426693ac09f9896bb993db5490fa021644454adcc72a32
+TERMUX_PKG_VERSION=0.3.113
+TERMUX_PKG_SRCURL=https://releases.pagure.org/libaio/libaio-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2c44d1c5fd0d43752287c9ae1eb9c023f04ef848ea8d4aafa46e9aedb678200b
 TERMUX_PKG_BREAKS="libaio-dev"
 TERMUX_PKG_REPLACES="libaio-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
