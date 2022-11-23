d96d74b35d136f1fb6c68534e3b770332567dae3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 16:16:10 +0900

    st: Bump to 0.9

--- x11-packages/st/build.sh
+++ x11-packages/st/build.sh
@@ -2,18 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://st.suckless.org/
 TERMUX_PKG_DESCRIPTION="A simple virtual terminal emulator for X"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
-TERMUX_PKG_VERSION=0.8.4
-TERMUX_PKG_REVISION=6
-TERMUX_PKG_SRCURL="http://dl.suckless.org/st/st-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=d42d3ceceb4d6a65e32e90a5336e3d446db612c3fbd9ebc1780bc6c9a03346a6
+TERMUX_PKG_VERSION=0.9
+TERMUX_PKG_SRCURL="https://dl.suckless.org/st/st-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=f36359799734eae785becb374063f0be833cf22f88b4f169cd251b99324e08e7
 # FIXME: config.h specified a Liberation Mono font which is not available in Termux.
 # Needs a patch for ttf-dejavu font package or liberation font package should be added.
-TERMUX_PKG_DEPENDS="libxft, libxext"
+TERMUX_PKG_DEPENDS="fontconfig, freetype, libx11, libxft"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="TERMINFO=$TERMUX_PREFIX/share/terminfo"
 TERMUX_PKG_RM_AFTER_INSTALL="share/terminfo"
 
-termux_step_configure() {
-	sed "s:@TERMUX_PREFIX@:${TERMUX_PREFIX}:g" \
-		"$TERMUX_PKG_BUILDER_DIR/config.h" > config.h
+termux_step_pre_configure() {
+	cp config.def.h config.h
 }
