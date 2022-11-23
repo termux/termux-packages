08e8b23b3ca9ed80976465df269e4f2583d5fbca
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 18:04:56 +0900

    megatools: Bump to 1.11.0.20220519

--- packages/megatools/build.sh
+++ packages/megatools/build.sh
@@ -1,19 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://megatools.megous.com/
 TERMUX_PKG_DESCRIPTION="Open-source command line tools and C library (libmega) for accessing Mega.co.nz cloud storage"
 TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.10.3
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION=1.11.0.20220519
 TERMUX_PKG_SRCURL=https://megatools.megous.com/builds/megatools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8dc1ca348633fd49de7eb832b323e8dc295f1c55aefb484d30e6475218558bdb
-TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, libgmp, openssl"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-fuse --enable-docs-build"
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	mkdir -p m4
-	autoreconf -v --install
-
-	sed -i -e 's/-V -qversion//' configure
-	sed -i -e 's/GOBJECT_INTROSPECTION_CHECK/#GOBJECT_INTROSPECTION_CHECK/' configure
-}
+TERMUX_PKG_SHA256=b30b1d87d916570f7aa6d36777dd378e83215d75ea5a2c14106028b6bddc261b
+TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dsymlinks=true
+"
