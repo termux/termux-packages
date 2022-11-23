bb2f15ac0187431e647cbf7f1b99ab7635e46a57
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 02:44:52 +0900

    libfuse3: Bump to 3.12.0

--- root-packages/libfuse3/build.sh
+++ root-packages/libfuse3/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/libfuse/libfuse
 TERMUX_PKG_DESCRIPTION="FUSE (Filesystem in Userspace) is an interface for userspace programs to export a filesystem to the Linux kernel"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=3.10.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.12.0
 TERMUX_PKG_SRCURL=https://github.com/libfuse/libfuse/archive/fuse-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c32527782cef620df58b162aa29901d1fb13253b029375d5860a2253a810344e
+TERMUX_PKG_SHA256=df6cc8807c4fd36b6b0ebef2b738dad6d19a9c7c085ccc3775063688d0bfcc0b
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddisable-mtab=true
 -Dexamples=false
+-Dtests=false
 -Dsbindir=bin
 -Dmandir=share/man
 -Dudevrulesdir=$TERMUX_PREFIX/etc/udev/rules.d
