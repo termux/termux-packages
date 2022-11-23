0643eae021f80f8a9a91c91d2d2bed51475844bb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 15:14:55 +0900

    netsurf: Bump to 3.10

--- x11-packages/netsurf/build.sh
+++ x11-packages/netsurf/build.sh
@@ -1,14 +1,17 @@
 TERMUX_PKG_HOMEPAGE=http://www.netsurf-browser.org/
 TERMUX_PKG_DESCRIPTION="NetSurf is a free, open source web browser"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="MIT, GPL-2.0"
+TERMUX_PKG_LICENSE_FILE="netsurf/COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.9
-TERMUX_PKG_REVISION=28
+TERMUX_PKG_VERSION=3.10
 TERMUX_PKG_SRCURL=http://download.netsurf-browser.org/netsurf/releases/source-full/netsurf-all-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=11e384eaf47e65c186da5001f1055a02f2d72ff17b50f403b8392546a2cf65ff
-TERMUX_PKG_DEPENDS="desktop-file-utils, exo, libjpeg-turbo, libpng, gtk3, openssl, libcurl, libiconv, libwebp"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk3"
+TERMUX_PKG_SHA256=495adf6b6614ce36fca6c605f7c321f9cb4a3df838043158122678ce2b3325b7
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libcurl, libexpat, libiconv, libjpeg-turbo, libpng, librsvg, libwebp, openssl, pango, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	export TARGET=gtk3
+}
 
 termux_step_configure () {
 	echo CC=$CC
