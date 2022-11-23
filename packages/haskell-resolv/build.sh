b31148ea0c55a000284d939f5de748274b44b8c7
Aditya Alok <alok@termux.dev>
2022-11-24 03:32:08 +0530

    rebuild(packages/haskell-resolv): after ghc update to 9.2.5
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/haskell-resolv/build.sh
+++ packages/haskell-resolv/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Domain Name Service (DNS) lookup via the libresolv stand
 TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION="0.1.2.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/resolv-${TERMUX_PKG_VERSION}/resolv-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="81a2bafad484db123cf8d17a02d98bb388a127fd0f822fa022589468a0e64671"
 TERMUX_PKG_BUILD_IN_SRC=true
