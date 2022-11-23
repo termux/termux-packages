6af783bca9702ef3ea6129e319d6f1c355ec0978
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 17:25:03 +0900

    recordmydesktop: Bump to 0.4.0

--- x11-packages/recordmydesktop/build.sh
+++ x11-packages/recordmydesktop/build.sh
@@ -1,18 +1,23 @@
-TERMUX_PKG_HOMEPAGE=http://recordmydesktop.sourceforge.net/
+TERMUX_PKG_HOMEPAGE=https://github.com/Enselic/recordmydesktop
 TERMUX_PKG_DESCRIPTION="Produces a OGG encapsulated Theora/Vorbis recording of your desktop"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.8.1
-TERMUX_PKG_REVISION=22
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/recordmydesktop/recordmydesktop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=33a2e208186ae78e2db2a27b0f5047b50fb7819c47fe15483b0765200b9d738c
-TERMUX_PKG_DEPENDS="libandroid-shmem, libsm, libtheora, libvorbis, libxdamage, libxext, zlib"
+TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_SRCURL=https://github.com/Enselic/recordmydesktop/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=69602d32c1be82cd92083152c7c44c0206ca0d6419d76a6144ffcfe07b157a72
+TERMUX_PKG_DEPENDS="libandroid-shmem, libice, libogg, libpopt, libsm, libtheora, libvorbis, libx11, libxdamage, libxext, libxfixes, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_lib_pthread_pthread_mutex_lock=yes
 --enable-oss=no
 --enable-jack=no
 "
 
+termux_step_post_get_source() {
+	TERMUX_PKG_SRCDIR+="/recordmydesktop"
+}
+
 termux_step_pre_configure() {
+	autoreconf -fi
+
 	export LIBS="-landroid-shmem"
 }
