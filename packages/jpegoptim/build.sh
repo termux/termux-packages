4f530912115bcc4b094f7b3c0d3fb0ad8a3ad124
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 17:55:33 +0900

    jpegoptim: Bump to 1.5.0

--- packages/jpegoptim/build.sh
+++ packages/jpegoptim/build.sh
@@ -1,15 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.kokkonen.net/tjko/projects.html
 TERMUX_PKG_DESCRIPTION="JPEG optimizer that recompresses image files to a smaller size, without losing any information"
-TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.6
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://www.kokkonen.net/tjko/src/jpegoptim-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=88b1eb64c2a33a2f013f068df8b0331f42c019267401ae3fa28e3277403a5ab7
+TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_SRCURL=https://github.com/tjko/jpegoptim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=67b0feba73fd72f0bd383f25bf84149a73378d34c0c25bc0b9b25b0264d85824
 TERMUX_PKG_DEPENDS="libjpeg-turbo"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --mandir=$TERMUX_PREFIX/share/man"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	LD=$CC
-}
