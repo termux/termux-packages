ad083ab611f66336c214a7214e0617e4aa62adbb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 19:39:37 +0900

    wavemon: Bump to 0.9.4

--- root-packages/wavemon/build.sh
+++ root-packages/wavemon/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/uoaerg/wavemon
 TERMUX_PKG_DESCRIPTION="Ncurses-based monitoring application for wireless network devices"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.9.4
 TERMUX_PKG_SRCURL=https://github.com/uoaerg/wavemon/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=5ebd5b79d3b7c546bc16b95161872c699a75e9acdfc6e3f02ec48dad10802067
-TERMUX_PKG_DEPENDS="libcap, libnl, ncurses"
+TERMUX_PKG_SHA256=e7e4167292136ca7ff12ddeb880904c10f33e97620ea6aa5f7838e859fdb40ed
+TERMUX_PKG_DEPENDS="libcap, libnl, libnl-cli, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_pthread_pthread_create=yes"
 
