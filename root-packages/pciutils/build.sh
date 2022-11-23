bcd1c10638301d9b8c9c121b3ba317fe8ba922de
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 19:28:06 +0900

    pciutils: Bump to 3.9.0

--- root-packages/pciutils/build.sh
+++ root-packages/pciutils/build.sh
@@ -2,20 +2,26 @@ TERMUX_PKG_HOMEPAGE=https://mj.ucw.cz/sw/pciutils/
 TERMUX_PKG_DESCRIPTION="a collection of programs for inspecting and manipulating configuration of PCI devices"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.7.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.9.0
 TERMUX_PKG_SRCURL=https://mj.ucw.cz/download/linux/pci/pciutils-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=08c27e01030d1fcc700d02bc2ea66c638f58a3d150e45e58852aa82ad4160d84
+TERMUX_PKG_SHA256=8953a785b2e3af414434b8fdcbfb75c90758819631001e60dd3afb89b22b2331
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
-	local ARCH=${TERMUX_ARCH}
-	if [[ ${ARCH} == "arm" ]]; then
-		ARCH="armv7l"
+	# ${str^^} returns upper case string
+	local _ARCH=${TERMUX_ARCH^^}
+	if [[ ${_ARCH} == "ARM" ]]; then
+		_ARCH="ARMV7L"
 	fi
 
-	# ${str^^} returns upper case string
-	sed -i "s%\@TERMUX_ARCH\@%${ARCH^^}%g" ./lib/config.h
-	sed -i "s%\@TERMUX_ARCH\@%${ARCH^^}%g" ./lib/config.mk
+	local f
+	for f in config.h config.mk; do
+		local in=$TERMUX_PKG_BUILDER_DIR/$TERMUX_PKG_VERSION/${f}.in
+		local out=$TERMUX_PKG_SRCDIR/lib/${f}
+		sed \
+			-e 's|@TERMUX_ARCH@|'"${_ARCH}"'|g' \
+			-e 's|@TERMUX_PREFIX@|'"${TERMUX_PREFIX}"'|g' \
+			${in} > ${out}
+	done
 }
