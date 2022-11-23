9b6725e5a53edd001bbd1605f5f593cbd4056b48
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-23 14:48:49 +0900

    poke: Bump to 2.4

--- packages/poke/build.sh
+++ packages/poke/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.jemarch.net/poke.html
 TERMUX_PKG_DESCRIPTION="Interactive, extensible editor for binary data."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.4
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/poke/poke-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ba7863034a1b08004b0ee8f88bda94160707e9a781d5556f555b507583cf0f1b
+TERMUX_PKG_SHA256=841e285917c6738ce982a6930e30ebeadecfb0655a79d9184f16f876a9fe6e47
 TERMUX_PKG_DEPENDS="readline, gettext, json-c, libgc"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-sysroot=$(dirname $TERMUX_PREFIX) --disable-threads --disable-hserver"
