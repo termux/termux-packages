bfcacbb5c9767dcf4e82a42b995215c6ad984550
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 19:25:15 +0900

    entr: Bump to 5.2

--- packages/entr/build.sh
+++ packages/entr/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://eradman.com/entrproject/
 TERMUX_PKG_DESCRIPTION="Event Notify Test Runner - run arbitrary commands when files change"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.1
-TERMUX_PKG_SRCURL=http://eradman.com/entrproject/code/entr-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0f87f577bce87641c525addb9bcc60bbaa579fe981dab759043e3ce1556dbb92
+TERMUX_PKG_VERSION=5.2
+TERMUX_PKG_SRCURL=https://eradman.com/entrproject/code/entr-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=237e309d46b075210c0e4cb789bfd0c9c777eddf6cb30341c3fe3dbcc658c380
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
