c1bd7cace19dd9dc5ec09e32bbc054bc8e772df3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 19:13:47 +0900

    chafa: Bump to 1.12.4

--- packages/chafa/build.sh
+++ packages/chafa/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE="https://hpjansson.org/chafa/"
 TERMUX_PKG_DESCRIPTION="Image-to-text converter supporting a wide range of symbols, etc."
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Yonle <yonle@protonmail.com>"
-TERMUX_PKG_VERSION="1.12.3"
+TERMUX_PKG_VERSION=1.12.4
 TERMUX_PKG_SRCURL="https://github.com/hpjansson/chafa/releases/download/${TERMUX_PKG_VERSION}/chafa-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256="2456a0b6c1150e25b64cd6a92810d59bed3f061f8b86f91aba5a77bc7cc76cfa"
-TERMUX_PKG_DEPENDS="imagemagick"
+TERMUX_PKG_SHA256=9774bd1a7076ea3124f7fea811e371d0e1da2e76b7ac06260d63a86c7b1a573f
+TERMUX_PKG_DEPENDS="freetype, glib, imagemagick, libcairo, libjpeg-turbo, librsvg, libtiff, libwebp"
 
