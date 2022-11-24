3677f29b560a8a44f3204bab8726b16ce4c4fa14
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-25 07:15:09 +0900

    php: Bump to 8.1.13

--- packages/php/php-apache.subpackage.sh
+++ packages/php/php-apache.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="Apache 2.0 Handler module for PHP"
-TERMUX_SUBPKG_DEPENDS="apache2"
+TERMUX_SUBPKG_DEPENDS="apache2, apr-util"
 TERMUX_SUBPKG_INCLUDE="libexec/apache2/libphp.so"
