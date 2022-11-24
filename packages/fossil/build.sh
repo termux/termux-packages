bd27b5c3f8850bb58ba1427b6dfbae835b09c52a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-25 02:08:22 +0900

    fossil: Use system SQLite
    properly.

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT-BSD2.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.20
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/home/tarball/version-$TERMUX_PKG_VERSION/fossil-src-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=0892ea4faa573701ca285a3d4a2d203e8abbb022affe3b1be35658845e8de721
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
@@ -21,6 +22,7 @@ termux_step_configure() {
 		--host=$TERMUX_HOST_PLATFORM \
 		--json \
 		--disable-internal-sqlite \
+		--with-sqlite=$TERMUX_PREFIX \
 		--with-openssl=$TERMUX_PREFIX \
 		--with-zlib=$TERMUX_PREFIX
 }
