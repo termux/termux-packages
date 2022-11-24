1edd38df29ed693edb21e6749dc268d8c44eb8ce
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 18:59:48 +0900

    arp-scan: Bump to 1.9.8

--- root-packages/arp-scan/build.sh
+++ root-packages/arp-scan/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/royhills/arp-scan
 TERMUX_PKG_DESCRIPTION="arp-scan is a command-line tool for system discovery and fingerprinting. It constructs and sends ARP requests to the specified IP addresses, and displays any responses that are received."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.9.7
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.9.8
 TERMUX_PKG_SRCURL=https://github.com/royhills/arp-scan/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e03c36e4933c655bd0e4a841272554a347cd0136faf42c4a6564059e0761c039
+TERMUX_PKG_SHA256=b9b75ceaef6348f5951b06c773ec7a243a9e780e160eafebc369f9c27f6a7d3f
 TERMUX_PKG_DEPENDS="libpcap"
 
 if [[ ${TERMUX_ARCH_BITS} == 32 ]]; then
