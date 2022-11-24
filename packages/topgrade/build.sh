9d6c049ce81324c902df483419596009cb74aaf3
Laurent le Beau-Martin <1180863+laurentlbm@users.noreply.github.com>
2022-11-24 14:10:06 -0500

    upgpkg(main/topgrade): 10.2.0
    The original maintainer has stopped development
    and a new team has taken over to continue the project.

--- packages/topgrade/build.sh
+++ packages/topgrade/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/r-darwish/topgrade/
+TERMUX_PKG_HOMEPAGE=https://github.com/topgrade-rs/topgrade/
 TERMUX_PKG_DESCRIPTION="Upgrade all the things"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@laurentlbm"
-TERMUX_PKG_VERSION="9.0.1"
-TERMUX_PKG_SRCURL="https://github.com/r-darwish/topgrade/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=70a1cf2c6a4de41e4c708409842968f3cf05dd5f238efac7ca0f1c9064be670a
+TERMUX_PKG_VERSION="10.2.0"
+TERMUX_PKG_SRCURL="https://github.com/topgrade-rs/topgrade/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=66f11d3a08981a883c20afd40d036a7e42d8e12f8d88e0671455a83f70b495da
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
