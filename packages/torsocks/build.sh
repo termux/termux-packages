7d2e7c5ead79f7d6bf8c1f8a7a9e258576420b6a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-25 00:31:30 +0900

    torsocks: Bump to 2.4.0

--- packages/torsocks/build.sh
+++ packages/torsocks/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/dgoulet/torsocks
+TERMUX_PKG_HOMEPAGE=https://gitlab.torproject.org/tpo/core/torsocks
 TERMUX_PKG_DESCRIPTION="Wrapper to safely torify applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.3.0
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://github.com/dgoulet/torsocks/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=817c143e8a9d217f41a223a85139c6ca28e1b99556c547fcdb4c72dbc170b6c9
+TERMUX_PKG_VERSION=2.4.0
+TERMUX_PKG_SRCURL=https://gitlab.torproject.org/tpo/core/torsocks/-/archive/v${TERMUX_PKG_VERSION}/torsocks-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c01b471d89eda9f3c8dcb85a448e8066692d0707f9ff8b2ac7e665a602291b87
 TERMUX_PKG_DEPENDS="tor"
 
 termux_step_pre_configure() {
