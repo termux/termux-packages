cee6c71890c47bbcc217d0b41c20e7552763878c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 23:15:33 +0900

    libnova: Bump to 0.16

--- packages/libnova/build.sh
+++ packages/libnova/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE="http://libnova.sourceforge.net"
 TERMUX_PKG_DESCRIPTION="A general purpose, double precision, Celestial Mechanics, Astrometry and Astrodynamics library"
 TERMUX_PKG_GROUPS="science"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.15.0"
-TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/sourceforge/libnova/libnova-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=7c5aa33e45a3e7118d77df05af7341e61784284f1e8d0d965307f1663f415bb1
+TERMUX_PKG_VERSION=0.16
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/libn/libnova/libnova_${TERMUX_PKG_VERSION}.orig.tar.xz
+TERMUX_PKG_SHA256=5dea5b29cba777ab8de4fd30cdfdbc1728fe1b3c573902270c1106bad55439a2
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
