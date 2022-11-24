0e6850d008694911e20d4e224f68201352e17c11
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-25 07:52:40 +0900

    open-adventure: Bump to 1.11

--- packages/open-adventure/build.sh
+++ packages/open-adventure/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.catb.org/~esr/open-adventure/
 TERMUX_PKG_DESCRIPTION="Forward-port of the original Colossal Cave Adventure from 1976-77"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=1.9
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=13a8bf9556014021b0e1f144f23e00afc05e92e4952b2ee7ad1dab0c079a6694
+TERMUX_PKG_VERSION=1.11
+TERMUX_PKG_SHA256=2dae27a5e59598da20d1bc49dbb50907d4d05ff9bc0ef2efecc6fccd1231378c
 TERMUX_PKG_SRCURL=https://gitlab.com/esr/open-adventure/-/archive/${TERMUX_PKG_VERSION}/open-adventure-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="libedit"
 TERMUX_PKG_BUILD_IN_SRC=true
