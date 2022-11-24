a5d6b9d85aa582106802d89df402b6ccb4cc45e2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 17:42:33 +0900

    lvm2: Bump to 2.03.17

--- root-packages/lvm2/build.sh
+++ root-packages/lvm2/build.sh
@@ -1,15 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://sourceware.org/lvm2/
 TERMUX_PKG_DESCRIPTION="A device-mapper library from LVM2 package"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1, BSD 2-Clause"
+TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.BSD, COPYING.LIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.03.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.03.17
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/sourceware/lvm2/releases/LVM2.${TERMUX_PKG_VERSION}.tgz
-TERMUX_PKG_SHA256=e661ece15b5d88d8abe39a4c1e1db2f43e1896f019948bb98b0e15d777680786
+TERMUX_PKG_SHA256=7286cfa9651828c589389509546333b8da965dfa84a1a4c8ab3e681a47fabae7
 TERMUX_PKG_DEPENDS="libandroid-support, libaio, readline"
 TERMUX_PKG_BREAKS="libdevmapper-dev"
 TERMUX_PKG_REPLACES="libdevmapper-dev"
-
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-pkgconfig
 --disable-selinux
@@ -23,4 +23,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	export CFLAGS="$CFLAGS $CPPFLAGS"
+
+	find "$TERMUX_PKG_SRCDIR" -name '*.[ch]' | xargs -n 1 \
+		sed -i 's/\([^A-Za-z0-9_]\)\(stack[^A-Za-z0-9_]\)/\1log_\2/g'
 }
