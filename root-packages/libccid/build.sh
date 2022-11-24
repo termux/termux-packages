3fcaf8b015648377569d45aa8906a1ef275ba391
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 19:42:59 +0900

    libccid: Bump to 1.4.36

--- root-packages/libccid/build.sh
+++ root-packages/libccid/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://ccid.apdu.fr/
 TERMUX_PKG_DESCRIPTION="A generic USB CCID (Chip/Smart Card Interface Devices) driver and ICCD (Integrated Circuit(s) Card Devices)."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.31
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.4.36
 TERMUX_PKG_SRCURL=https://ccid.apdu.fr/files/ccid-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6b48d7b6e4390e038d25630f8664fe81618ab00f232d6efbe0e3cc6df28ce8f7
-TERMUX_PKG_DEPENDS="libpcsclite, libusb, flex"
-
-termux_step_pre_configure() {
-	export LEXLIB=$TERMUX_PREFIX/lib/libfl.so
-}
+TERMUX_PKG_SHA256=2b73ac885f1bcab819dc3578944d17406241e0c724042989cea437353b6749c1
+TERMUX_PKG_DEPENDS="libusb, pcscd"
+TERMUX_PKG_BUILD_DEPENDS="libpcsclite"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_prog_LEX=:
+"
