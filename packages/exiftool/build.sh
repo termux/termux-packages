642b9bb16891d3e7241a7a29d77105f899daef4f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 19:26:46 +0900

    exiftool: Bump to 12.51

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://exiftool.org/
 TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta information in a wide variety of files."
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=12.44
+TERMUX_PKG_VERSION=12.51
 TERMUX_PKG_SRCURL="https://exiftool.org/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=002b5f849d34674f802608611563abdc665ea89018e21a6ca73e71578f5c9e12
+TERMUX_PKG_SHA256=efa99e350a7d73467ef3580d48c9c34ed26677fa8e1aee2ea9e1ec1a14e70e44
 TERMUX_PKG_DEPENDS="perl"
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
 	local current_perl_version=$(. $TERMUX_SCRIPTDIR/packages/perl/build.sh; echo $TERMUX_PKG_VERSION)
