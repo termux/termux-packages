87db6c23ab6d35dab7c81c9cc0a406562621273f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 14:34:41 +0900

    roxterm: Bump to 3.12.1

--- x11-packages/roxterm/build.sh
+++ x11-packages/roxterm/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/realh/roxterm
 TERMUX_PKG_DESCRIPTION="ROXTerm is a terminal emulator based on the VTE library"
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
-TERMUX_PKG_VERSION=3.11.1
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=3.12.1
 TERMUX_PKG_SRCURL=https://github.com/realh/roxterm/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a362d4b6ca89091d277c710c6a4dcec67a9429d519086b911941b66af1e8e3e3
-TERMUX_PKG_DEPENDS="gtk3, libvte, xsltproc, pcre2, dbus-glib"
+TERMUX_PKG_SHA256=9a662a00fe555ae9ff38301a1707a3432d8f678326062c98740f20827280a5aa
+TERMUX_PKG_DEPENDS="dbus, dbus-glib, glib, gtk3, libcairo, libvte, pango"
+TERMUX_PKG_BUILD_DEPENDS="xsltproc"
 TERMUX_PKG_BUILD_IN_SRC=true
