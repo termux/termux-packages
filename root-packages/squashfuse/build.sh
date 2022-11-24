b7d7a98c39f91b4e158fa046aa498b20dea7a828
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 14:28:10 +0900

    squashfuse: Bump to 0.1.105

--- root-packages/squashfuse/build.sh
+++ root-packages/squashfuse/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vasi/squashfuse
 TERMUX_PKG_DESCRIPTION="FUSE filesystem to mount squashfs archives"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.1.104
-TERMUX_PKG_SHA256=9e6f4fb65bb3e5de60c8714bb7f5cbb08b5534f7915d6a4aeea008e1c669bd35
+TERMUX_PKG_VERSION=0.1.105
+TERMUX_PKG_SHA256=3f776892ab2044ecca417be348e482fee2839db75e35d165b53737cb8153ab1e
 TERMUX_PKG_SRCURL=https://github.com/vasi/squashfuse/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libfuse2, zstd, liblz4, liblzo"
+TERMUX_PKG_DEPENDS="libfuse2, liblz4, liblzma, liblzo, zlib, zstd"
 
 termux_step_pre_configure () {
 	./autogen.sh
