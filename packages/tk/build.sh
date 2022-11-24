22b6dd1022caf71679980bdac90dc414f9988d51
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-24 14:16:16 +0900

    tk: Bump to 8.6.13

--- packages/tk/build.sh
+++ packages/tk/build.sh
@@ -3,14 +3,15 @@ TERMUX_PKG_DESCRIPTION="A windowing toolkit for use with tcl"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="license.terms"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=8.6.12
+TERMUX_PKG_VERSION=8.6.13
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/tcl/tk${TERMUX_PKG_VERSION}-src.tar.gz
-TERMUX_PKG_SHA256=12395c1f3fcb6bed2938689f797ea3cdf41ed5cb6c4766eec8ac949560310630
+TERMUX_PKG_SHA256=2e65fa069a23365440a3c56c556b8673b5e32a283800d8d9b257e3f584ce0675
 TERMUX_PKG_DEPENDS="fontconfig, libx11, libxft, libxss, tcl"
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_MAKE_INSTALL_TARGET="install install-private-headers"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--mandir=$TERMUX_PREFIX/share/man
 --enable-threads
 --enable-64bit
 "
