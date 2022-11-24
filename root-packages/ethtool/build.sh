ff514095db3379702d7d5e50043a75f551c7815f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-25 05:30:00 +0900

    ethtool: Bump to 5.19

--- root-packages/ethtool/build.sh
+++ root-packages/ethtool/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://mirrors.edge.kernel.org/pub/software/network/ethtool/
 TERMUX_PKG_DESCRIPTION="standard Linux utility for controlling network drivers and hardware, particularly for wired Ethernet devices"
-TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.19
 TERMUX_PKG_SRCURL=https://mirrors.edge.kernel.org/pub/software/network/ethtool/ethtool-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=aa2fef1936dd4a11755dfa0bdb93f0ec5bea45208d27c9754bc3abe1aa42c1cb
+TERMUX_PKG_SHA256=3b752a3329827907ac3812f2831dfecf51c8c41c55d2d69cfb9c53ca06449fc6
 TERMUX_PKG_DEPENDS="libmnl"
