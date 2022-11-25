d9838bf7765e1ded0e1ecb4383857a82339f8648
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-26 04:49:30 +0900

    argp: Revbump revdeps to rebuild

--- packages/dwarves/build.sh
+++ packages/dwarves/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Pahole and other DWARF utils"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.24
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://fedorapeople.org/~acme/dwarves/dwarves-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=576bc112b95937dfbcd347c423696ee9e1992a338fdca1acacca736fd95f69c2
-TERMUX_PKG_DEPENDS="libdw, libelf, zlib"
-TERMUX_PKG_BUILD_DEPENDS="argp"
+TERMUX_PKG_DEPENDS="argp, libdw, libelf, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-D__LIB=lib"
 
 termux_step_pre_configure() {
--- packages/libelf/elfutils.subpackage.sh
+++ packages/libelf/elfutils.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="A collection of utilities to read, create and modify ELF binary files"
 TERMUX_SUBPKG_INCLUDE="bin/eu-* bin/debuginfod* etc/profile.d/debuginfod.* share/man/man8/* share/man/man1/*"
-TERMUX_SUBPKG_DEPENDS="libarchive, libasm, libc++, libdebuginfod, libdw, libmicrohttpd, libsqlite"
+TERMUX_SUBPKG_DEPENDS="argp, libarchive, libasm, libc++, libdebuginfod, libdw, libmicrohttpd, libsqlite"
