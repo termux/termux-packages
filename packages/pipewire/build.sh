0ed40aeaa19079fcf1f012858ad6f2a217c9f39c
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-26 02:04:49 +0900

    pipewire: Bump to 0.3.61

--- packages/pipewire/build.sh
+++ packages/pipewire/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://pipewire.org/
 TERMUX_PKG_DESCRIPTION="A server and user space API to deal with multimedia pipelines"
 TERMUX_PKG_LICENSE="MIT, LGPL-2.1, LGPL-3.0, GPL-2.0"
+TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.60
+TERMUX_PKG_VERSION=0.3.61
 TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/${TERMUX_PKG_VERSION}/pipewire-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=8164eb53c9eafedfa22fe1adc4b8e38f3173c6f33695c735a17ed1a3d43c664e
+TERMUX_PKG_SHA256=c9e6584286787e332ed463d89c90c7a0edcbc836fb597de104a6a50d2ffe745c
 TERMUX_PKG_DEPENDS="avahi, ffmpeg, glib, libc++, liblua54, libsndfile, libwebrtc-audio-processing, lilv, openssl, pulseaudio"
 
 # 'media-session' session-managers is disabled as it requires alsa.
@@ -27,9 +28,4 @@ termux_step_pre_configure() {
 	CFLAGS+=" -Dindex=strchr -Drindex=strrchr"
 	sed "s|@TERMUX_PKG_BUILDER_DIR@|${TERMUX_PKG_BUILDER_DIR}|g" \
 		"${TERMUX_PKG_BUILDER_DIR}"/reallocarray.diff | patch -p1
-}
-
-termux_step_install_license() {
-	install -Dm600 -t "${TERMUX_PREFIX}/share/doc/${TERMUX_PKG_NAME}" \
-		"${TERMUX_PKG_SRCDIR}"/LICENSE "${TERMUX_PKG_SRCDIR}"/COPYING
 }
