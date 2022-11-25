a521b9f1d818acaab8a4a7b62136562a746a0df2
Jia Yuan Lo <jylo06g@gmail.com>
2022-11-25 11:23:18 +0800

    enhance(main): enable LTO for boinc and boinctui
    * cleanup the patch to be less reliant build arguments

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open-source software for volunteer computing"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.20.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/BOINC/boinc/archive/client_release/${TERMUX_PKG_VERSION:0:4}/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=35467e55da28e802e045f8dc777f518d60b3945badd606e1738b545276aca03d
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-shmem, libc++, libcurl, openssl, zlib"
@@ -19,14 +20,12 @@ etc/boinc-client.conf
 
 termux_step_pre_configure() {
 	# for benchmark purposes
-	CFLAGS="${CFLAGS/-Oz/-Os}"
-	CXXFLAGS="${CXXFLAGS/-Oz/-Os}"
-	CFLAGS+=" -fPIC -DTERMUX_PKG_ANDROID_VERSION=\\\"7.0\\\""
-	CXXFLAGS+=" -fPIC -DTERMUX_PKG_ANDROID_VERSION=\\\"7.0\\\""
-	LDFLAGS+=" -landroid-shmem -landroid-execinfo $($CC -print-libgcc-file-name)"
+	export CFLAGS="${CFLAGS/-Oz/-Os} -flto -fPIC"
+	export CXXFLAGS="${CXXFLAGS/-Oz/-Os} -flto -fPIC"
+	export LDFLAGS+=" -landroid-shmem -landroid-execinfo $(${CC} -print-libgcc-file-name)"
 	./_autosetup
 }
 
 termux_step_post_make_install() {
-	install -Dm644 "$TERMUX_PKG_SRCDIR/client/scripts/boinc.bash" "$TERMUX_PREFIX/share/bash-completion/completions/boinc"
+	install -Dm644 "${TERMUX_PKG_SRCDIR}/client/scripts/boinc.bash" "${TERMUX_PREFIX}/share/bash-completion/completions/boinc"
 }
