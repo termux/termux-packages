6c1639e3c529250fe51210b88b745e2f7e83c9c7
Jia Yuan Lo <jylo06g@gmail.com>
2022-11-25 11:28:32 +0800

    bump(main/box86): 0.2.8 and enable LTO
    Probably need to figure out a way to auto bump later

--- packages/box86/build.sh
+++ packages/box86/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 _COMMIT=d76efc849f9b8c26eb4f154b4f39068a4f901834
 _COMMIT_DATE=20221124
 _COMMIT_TIME=161335
-TERMUX_PKG_VERSION="0.2.6.20221124.161335gd76efc84"
+TERMUX_PKG_VERSION="0.2.8.20221124.161335gd76efc84"
 TERMUX_PKG_SRCURL=https://github.com/ptitSeb/box86.git
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libandroid-complex-math, libandroid-glob, libandroid-spawn, libandroid-sysv-semaphore"
@@ -21,22 +21,22 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 # this should only be used until release new version
 termux_pkg_auto_update() {
-	local latest_commit_date_tz latest_commit_date latest_commit_time latest_version
+	local latest_commit_date_tz latest_commit_date latest_commit_time latest_version current_date current_date_diff
 	local latest_commit=$(curl -s https://api.github.com/repos/ptitSeb/box86/commits | jq .[].sha | head -1 | sed -e 's|\"||g')
 
-	if [ -z "${latest_commit}" ]; then
+	if [[ -z "${latest_commit}" ]]; then
 		echo "WARN: Unable to get latest commit from upstream. Try again later." >&2
 		return 0
 	fi
 
-	if [ "${latest_commit}" = "${_COMMIT}" ]; then
+	if [[ "${latest_commit}" == "${_COMMIT}" ]]; then
 		echo "INFO: No update needed. Already at version '${TERMUX_PKG_VERSION}'."
 		return 0
 	fi
 
 	latest_commit_date_tz=$(curl -s "https://api.github.com/repos/ptitSeb/box86/commits/${latest_commit}" | jq .commit.committer.date | sed -e 's|\"||g')
 
-	if [ -z "${latest_commit_date_tz}" ]; then
+	if [[ -z "${latest_commit_date_tz}" ]]; then
 		termux_error_exit "ERROR: Unable to get latest commit date info"
 	fi
 
@@ -44,12 +44,12 @@ termux_pkg_auto_update() {
 	latest_commit_time=$(echo "${latest_commit_date_tz}" | sed -e 's|\(.*\)T\(.*\)Z|\2|' -e 's|\:||g')
 
 	# always check this in case upstream change the version format
-	latest_version="0.2.6.${latest_commit_date}.${latest_commit_time}g${latest_commit:0:8}"
+	latest_version="0.2.8.${latest_commit_date}.${latest_commit_time}g${latest_commit:0:8}"
 
 	# rough estimate weekly push
-	local current_date=$(date "+%Y%m%d")
-	local current_date_diff=$((current_date-_COMMIT_DATE))
-	if [ "${current_date_diff}" -lt 7 ]; then
+	current_date=$(date "+%Y%m%d")
+	current_date_diff=$((current_date-_COMMIT_DATE))
+	if [[ "${current_date_diff}" -lt 7 ]]; then
 		echo "INFO: Queuing updates after 7 days since last push, currently its ${current_date_diff}"
 		return 0
 	fi
@@ -59,15 +59,16 @@ termux_pkg_auto_update() {
 	fi
 
 	# unlikely to happen
-	if [ "${latest_commit_date}" -lt "${_COMMIT_DATE}" ]; then
+	if [[ "${latest_commit_date}" -lt "${_COMMIT_DATE}" ]]; then
 		termux_error_exit "ERROR: Upstream is older than current package version. Please report to upstream."
-	elif [ "${latest_commit_date}" -eq "${_COMMIT_DATE}" ] && [ "${latest_commit_time}" -lt "${_COMMIT_TIME}" ]; then
+	elif [[ "${latest_commit_date}" -eq "${_COMMIT_DATE}" ]] && [[ "${latest_commit_time}" -lt "${_COMMIT_TIME}" ]]; then
 		termux_error_exit "ERROR: Upstream is older than current package version. Please report to upstream."
 	fi
 
-	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^_COMMIT=.*|_COMMIT=${latest_commit}|"
-	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^_COMMIT_DATE=.*|_COMMIT_DATE=${latest_commit_date}|"
-	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" -e "s|^_COMMIT_TIME=.*|_COMMIT_TIME=${latest_commit_time}|"
+	sed -i "${TERMUX_PKG_BUILDER_DIR}/build.sh" \
+		-e "s|^_COMMIT=.*|_COMMIT=${latest_commit}|" \
+		-e "s|^_COMMIT_DATE=.*|_COMMIT_DATE=${latest_commit_date}|" \
+		-e "s|^_COMMIT_TIME=.*|_COMMIT_TIME=${latest_commit_time}|"
 
 	# maybe save a few ms as we already done version check
 	termux_pkg_upgrade_version "${latest_version}" --skip-version-check
@@ -79,7 +80,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	export CFLAGS="${CFLAGS/-Oz/-O2}"
+	export CFLAGS="${CFLAGS/-Oz/-O2} -flto"
 }
 
 termux_step_make_install() {
