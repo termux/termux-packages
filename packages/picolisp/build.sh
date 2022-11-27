4000c7bfa31d37c6af0959e93cd2483f3920f6bb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-28 04:52:54 +0900

    picolisp: Bump to 22.9

--- packages/picolisp/build.sh
+++ packages/picolisp/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Lisp interpreter and application server framework"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.12
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=http://archive.ubuntu.com/ubuntu/pool/universe/p/picolisp/picolisp_$TERMUX_PKG_VERSION.orig.tar.gz
-TERMUX_PKG_SHA256=a06838236b7f5b52c5d587d32d31627f73cdb9775cc02a80f2cdaedd12888c7d
+TERMUX_PKG_VERSION=22.9
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/p/picolisp/picolisp_${TERMUX_PKG_VERSION}.orig.tar.gz
+TERMUX_PKG_SHA256=2cf0ed9e176c9df76a2c5cffc87c48d838b7560fb678a16a45569521102da1e8
 TERMUX_PKG_DEPENDS="libcrypt, libffi, openssl, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
 # For 32-bit archs we nees to build minipicolisp
