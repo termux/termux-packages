320acf79854b75e081499db898d1951062162360
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-28 05:49:22 +0900

    openldap: Bump to 2.6.3

--- packages/openldap/build.sh
+++ packages/openldap/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://openldap.org
 TERMUX_PKG_DESCRIPTION="OpenLDAP server"
 TERMUX_PKG_LICENSE="OpenLDAP"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.5.13
+TERMUX_PKG_VERSION=2.6.3
 TERMUX_PKG_SRCURL=https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-${TERMUX_PKG_VERSION}.tgz
-TERMUX_PKG_SHA256=ee3c430c4ef7b87c57b622108c7339376d6c27fbbf2767770be3de1df63d008c
+TERMUX_PKG_SHA256=d2a2a1d71df3d77396b1c16ad7502e674df446e06072b0e5a4e941c3d06c0d46
 TERMUX_PKG_DEPENDS="libsasl, libuuid, openssl"
 TERMUX_PKG_BREAKS="openldap-dev"
 TERMUX_PKG_REPLACES="openldap-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-
+TERMUX_PKG_EXTRA_MAKE_ARGS="STRIP="
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ol_cv_lib_icu=no
 ac_cv_func_memcmp_working=yes
@@ -23,16 +23,4 @@ ac_cv_func_memcmp_working=yes
 termux_step_pre_configure() {
 	CFLAGS+=" -DMDB_USE_ROBUST=0"
 	LDFLAGS+=" -lcrypto -llog"
-}
-
-termux_step_make_install() {
-	make STRIP="" install
-
-	local slot=${TERMUX_PKG_VERSION%.*}
-	for lib in lber ldap; do
-		local target=lib${lib}-${slot}.so
-		if [ -e $TERMUX_PREFIX/lib/${target} ]; then
-			ln -sf ${target} $TERMUX_PREFIX/lib/lib${lib}.so
-		fi
-	done
 }
