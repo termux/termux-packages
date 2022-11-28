9f5804ee645f4b9edcfba7577cb796d6a7a8f669
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-28 18:34:36 +0900

    squeezelite: Bump to 1.9.9.1414

--- packages/squeezelite/build.sh
+++ packages/squeezelite/build.sh
@@ -1,18 +1,27 @@
 TERMUX_PKG_HOMEPAGE=https://ralph-irving.github.io/squeezelite.html
 TERMUX_PKG_DESCRIPTION="A small headless Logitech Squeezebox emulator"
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="GPL-3.0, BSD 2-Clause"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.9.9
-TERMUX_PKG_REVISION=2
-_COMMIT=370020f2dd572f0ab5464d8ef4e47ebf21b19468
+_COMMIT=dbe69eb8aa88f644cfb46541d6cef72fa666570d
+TERMUX_PKG_VERSION=1.9.9.1414
 TERMUX_PKG_SRCURL=https://github.com/ralph-irving/squeezelite.git
 TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_DEPENDS="libmad, mpg123, pulseaudio"
+TERMUX_PKG_DEPENDS="libflac, libmad, libvorbis, mpg123, pulseaudio"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
 	git fetch --unshallow
 	git checkout $_COMMIT
+
+	local ver=()
+	local k
+	for k in MAJOR MINOR MICRO; do
+		ver+=($(sed -En 's/^#define '"${k}"'_VERSION "([^"]+)"/\1/p' squeezelite.h))
+	done
+	if [ "$(IFS=.; echo "${ver[*]}")" != "${TERMUX_PKG_VERSION#*:}" ]; then
+		termux_error_exit "Version string '$TERMUX_PKG_VERSION' does not seem to be correct."
+	fi
 }
 
 termux_step_pre_configure() {
