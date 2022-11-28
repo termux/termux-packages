44d663143da58f381ffbecb82dd02ab256c8d0aa
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-28 20:07:04 +0900

    new package: mdbook-cat-prep

---
+++ packages/mdbook-cat-prep/build.sh
@@ -0,0 +1,43 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/gjk-cat/cat-prep
+TERMUX_PKG_DESCRIPTION="A preprocessor for mdbook which provides teacher, subject, material and tag functionality"
+TERMUX_PKG_LICENSE="Fair"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=fbf5ca360337452ca5ef7437d64c2efd7d891aec
+TERMUX_PKG_VERSION=1.0.9
+TERMUX_PKG_SRCURL=https://github.com/gjk-cat/cat-prep.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local ver=$(sed -En 's/^version = "([^"]+)".*/\1/p' Cargo.toml)
+	if [ "${ver}" != "${TERMUX_PKG_VERSION#*:}" ]; then
+		termux_error_exit "Version string '$TERMUX_PKG_VERSION' does not seem to be correct."
+	fi
+}
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	local _patch=$TERMUX_PKG_BUILDER_DIR/mdbook-src-renderer-html_handlebars-helpers-navigation.rs.diff
+	local d
+	for d in $CARGO_HOME/registry/src/github.com-*/mdbook-*; do
+		patch --silent -p1 -d ${d} < ${_patch} || :
+	done
+}
+
+termux_step_make() {
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-cat-prep
+}
