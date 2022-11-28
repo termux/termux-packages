b3bda2c96e2d49e5d0dfcf7d85130f0c596b014f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-28 18:06:34 +0900

    libwavpack: Bump to 5.6.0

--- packages/libwavpack/build.sh
+++ packages/libwavpack/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.wavpack.com/
 TERMUX_PKG_DESCRIPTION="A completely open audio compression format providing lossless, high-quality lossy, and a unique hybrid compression mode"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.5.0
+TERMUX_PKG_VERSION=5.6.0
 TERMUX_PKG_SRCURL=https://github.com/dbry/WavPack/releases/download/${TERMUX_PKG_VERSION}/wavpack-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ef749d98df46925bc2916993e601cc7ee9114d99653e63e0e304f031ba73b8e6
+TERMUX_PKG_SHA256=af8035f457509c3d338b895875228a9b81de276c88c79bb2d3e31d9b605da9a9
 TERMUX_PKG_DEPENDS="libandroid-glob"
 
 termux_step_pre_configure() {
