4c7f28a19d947601e693b7a290ba404807f62c38
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-29 22:33:40 +0900

    new package: liblqr

---
+++ packages/liblqr/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/carlobaldassi/liblqr
+TERMUX_PKG_DESCRIPTION="Provides a C/C++ API for performing non-uniform resizing of images by the seam-carving technique"
+TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4.2
+TERMUX_PKG_SRCURL=https://github.com/carlobaldassi/liblqr/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=1019a2d91f3935f1f817eb204a51ec977a060d39704c6dafa183b110fd6280b0
+TERMUX_PKG_DEPENDS="glib"
