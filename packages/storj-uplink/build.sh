a538b9a6bc5321b6089a7bd568946a21cff8ede2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-30 04:58:51 +0900

    new package: storj-uplink

---
+++ packages/storj-uplink/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://www.storj.io/integrations/uplink-cli
+TERMUX_PKG_DESCRIPTION="Storj DCS Uplink CLI"
+TERMUX_PKG_LICENSE="AGPL-V3"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.68.1
+TERMUX_PKG_SRCURL=https://github.com/storj/storj/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=33b7bfb646daa65789e1e1662a08ac998325cb848ddb3021743f345485b2e06e
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	go build ./cmd/uplink
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin uplink
+}
