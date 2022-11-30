fb7e17c66dc8ec03afa620601aa2aa865ecf19b4
Elliana May <me@mause.me>
2022-12-01 01:49:44 +0800

    libduckdb: correct abi config

--- packages/libduckdb/build.sh
+++ packages/libduckdb/build.sh
@@ -3,10 +3,12 @@ TERMUX_PKG_DESCRIPTION="An in-process SQL OLAP database management system"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.6.0
+TERMUX_PKG_REVISION=1
 # we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
 TERMUX_PKG_SRCURL=https://github.com/duckdb/duckdb.git
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
+	CXXFLAGS+=" -D_GLIBCXX_USE_CXX11_ABI=1"
 }
