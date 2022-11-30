a61b99dd054461cfc0a71b0ff67c9756560eeb0d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-30 22:22:19 +0900

    nsis: Include prebuilt stubs

--- packages/nsis/build.sh
+++ packages/nsis/build.sh
@@ -5,10 +5,10 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.08
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/nsis/nsis-${TERMUX_PKG_VERSION}-src.tar.bz2
 TERMUX_PKG_SHA256=a85270ad5386182abecb2470e3d7e9bec9fe4efd95210b13551cb386830d1e87
-TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, zlib"
+TERMUX_PKG_DEPENDS="libandroid-support, libc++, libiconv, nsis-stubs, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
@@ -27,5 +27,12 @@ termux_step_make_install() {
 }
 
 termux_step_post_make_install() {
-	ln -sf $TERMUX_PREFIX/opt/nsis/nsis/makensis $TERMUX_PREFIX/bin/
+	ln -sfr $TERMUX_PREFIX/opt/nsis/nsis/makensis $TERMUX_PREFIX/bin/
+
+	rm -rf _nsis-stubs
+	mkdir -p _nsis-stubs
+	pushd _nsis-stubs
+	tar xf $TERMUX_PKG_BUILDER_DIR/nsis-stubs.tar.xz --strip-components=1
+	install -Dm600 -t $TERMUX_PREFIX/opt/nsis/Stubs *
+	popd
 }
