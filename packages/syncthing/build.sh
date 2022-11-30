4940f3701df3f173123c1b88157e65ee9b08e5ef
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-30 21:03:26 +0900

    syncthing: Bump to 1.22.2

--- packages/syncthing/build.sh
+++ packages/syncthing/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # NOTE: as of 1.12.0 compilation fails when package zstd is
 # present in TERMUX_PREFIX.
-TERMUX_PKG_VERSION="1.22.1"
-TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/releases/download/v${TERMUX_PKG_VERSION}/syncthing-source-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b024d112167e0e81a785ab98c1a044aee1ac041dfe57a62772c08284f875a5bd
+TERMUX_PKG_VERSION=1.22.2
+TERMUX_PKG_SRCURL=https://github.com/syncthing/syncthing/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=957d0c5a25a29e4971d044c8278409ac5ea7b961fda59ca0ac412b8bc41dd8f5
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
