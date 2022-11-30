92e59bc9a9def8ed0fe6f335dd2f938cb36c990f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-30 20:56:23 +0900

    libbluray: Bump to 1.3.4

--- packages/libbluray/build.sh
+++ packages/libbluray/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://code.videolan.org/videolan/libbluray/
 TERMUX_PKG_DESCRIPTION="An open-source library designed for Blu-Ray Discs playback for media players"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.3
+TERMUX_PKG_VERSION=1.3.4
 TERMUX_PKG_SRCURL=https://downloads.videolan.org/pub/videolan/libbluray/${TERMUX_PKG_VERSION}/libbluray-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=58ff52cdcee64c55dcc3c777a1c39fb41abd951b927978e4d2b6811b9193a488
+TERMUX_PKG_SHA256=478ffd68a0f5dde8ef6ca989b7f035b5a0a22c599142e5cd3ff7b03bbebe5f2b
 TERMUX_PKG_DEPENDS="fontconfig, freetype, libudfread, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-bdjava-jar
