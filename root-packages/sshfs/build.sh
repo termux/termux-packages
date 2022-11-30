7bd6fecf8e64f0dc9e5b21c4814fb5eeb554f8fe
Tee KOBAYASHI <xtkoba@gmail.com>
2022-11-30 21:00:16 +0900

    sshfs: Bump to 3.7.3

--- root-packages/sshfs/build.sh
+++ root-packages/sshfs/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/libfuse/sshfs
 TERMUX_PKG_DESCRIPTION="FUSE client based on the SSH File Transfer Protocol"
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.7.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.7.3
 TERMUX_PKG_SRCURL=https://github.com/libfuse/sshfs/archive/refs/tags/sshfs-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8a9b0d980e9d34d0d18eacb9e1ca77fc499d1cf70b3674cc3e02f3eafad8ab14
+TERMUX_PKG_SHA256=52a1a1e017859dfe72a550e6fef8ad4f8703ce312ae165f74b579fd7344e3a26
 TERMUX_PKG_DEPENDS="libfuse3, glib, openssh | dropbear"
