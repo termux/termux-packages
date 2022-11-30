2b6517c67f5647822cf95791189545212a293a0d
Sunlight <73415680+SunPodder@users.noreply.github.com>
2022-11-30 09:49:45 +0000

    new package: pocketbase

---
+++ packages/pocketbase/build.sh
@@ -0,0 +1,29 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/pocketbase/pocketbase
+TERMUX_PKG_DESCRIPTION="An open source Go backend"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.8.0"
+TERMUX_PKG_SRCURL="https://github.com/pocketbase/pocketbase/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=3d5130f21d1d74a58b663b1b9946ce1fbf467a6ac5335d7acc51a396e93ea330
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	mkdir ./gopath
+	export GOPATH="$PWD/gopath"
+
+	cd $TERMUX_PKG_SRCDIR/examples/base
+
+	export GOBUILD=CGO_ENABLED=0
+
+	go build \
+		-trimpath \
+		-o "pocketbase.bin" \
+		main.go
+}
+
+termux_step_make_install() {
+	install -m700 examples/base/pocketbase.bin "${TERMUX_PREFIX}"/bin/pocketbase
+}
