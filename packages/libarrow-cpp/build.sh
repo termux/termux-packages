4104d9a6b07ec3266eaf735bf98454399d7f6e31
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-01 21:11:53 +0900

    libre2: Revbump revdep (libarrow-cpp) to rebuild

--- packages/libarrow-cpp/build.sh
+++ packages/libarrow-cpp/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Align the version with `libarrow-python` package.
 TERMUX_PKG_VERSION=10.0.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/apache/arrow/archive/refs/tags/apache-arrow-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=28c3e0402bc1c3c1e047b6e26cedb8d1d89b2b9497d576af24b0b700eef11701
 TERMUX_PKG_DEPENDS="libc++, libre2, utf8proc"
