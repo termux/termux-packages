a0214f0e8520e1ec6f470d7cccc90658fa02cc5e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-02 03:28:15 +0900

    new package: kiwix-tools

---
+++ packages/kiwix-tools/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/kiwix/kiwix-tools
+TERMUX_PKG_DESCRIPTION="A collection of Kiwix related command line tools"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.4.0
+TERMUX_PKG_SRCURL=https://github.com/kiwix/kiwix-tools/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e44d2b07bff5093784f12547ae1f32b2ceb9da49b10a48c7378bd2e1f7c13507
+TERMUX_PKG_DEPENDS="libc++, libkiwix, libzim"
