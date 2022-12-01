a8f2ee9b97f61ce55a53c67b1e273b213a7e35e4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-02 04:46:46 +0900

    new package: hstr

---
+++ packages/hstr/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dvorka/hstr
+TERMUX_PKG_DESCRIPTION="Shell history suggest box for bash and zsh"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.5
+TERMUX_PKG_SRCURL=https://github.com/dvorka/hstr/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7f5933fc07d55d09d5f7f9a6fbfdfc556d8a7d8575c3890ac1e672adabd2bec4
+TERMUX_PKG_DEPENDS="ncurses, readline"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_file__tmp_hstr_ms_wsl=no
+"
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
