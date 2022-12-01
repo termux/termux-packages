88669d0e931bacbb7a8faf161256e26bed518e93
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-01 20:34:52 +0900

    topgrade: Bump to 10.2.1

--- packages/topgrade/build.sh
+++ packages/topgrade/build.sh
@@ -2,12 +2,34 @@ TERMUX_PKG_HOMEPAGE=https://github.com/topgrade-rs/topgrade/
 TERMUX_PKG_DESCRIPTION="Upgrade all the things"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@laurentlbm"
-TERMUX_PKG_VERSION="10.2.0"
+TERMUX_PKG_VERSION=10.2.1
 TERMUX_PKG_SRCURL="https://github.com/topgrade-rs/topgrade/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=66f11d3a08981a883c20afd40d036a7e42d8e12f8d88e0671455a83f70b495da
+TERMUX_PKG_SHA256=0e98f5c61914e88fa2437c539899c4f98ca70d9dc91492e35afba1cc8eb2092f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_post_make_install() {
-	install -Dm600 -t "${TERMUX_PREFIX}"/share/man/man8 "${TERMUX_PKG_SRCDIR}"/topgrade.8
+termux_step_post_massage() {
+	mkdir -p ./share/bash-completion/completions
+	mkdir -p ./share/zsh/site-functions
+	mkdir -p ./share/fish/vendor_completions.d
+	mkdir -p ./share/man/man1
+}
+
+termux_step_create_debscripts() {
+	cat <<-EOF > ./postinst
+		#!${TERMUX_PREFIX}/bin/sh
+		${TERMUX_PREFIX}/bin/topgrade --gen-completion bash > ${TERMUX_PREFIX}/share/bash-completion/completions/topgrade
+		${TERMUX_PREFIX}/bin/topgrade --gen-completion zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_topgrade
+		${TERMUX_PREFIX}/bin/topgrade --gen-completion fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/topgrade.fish
+		${TERMUX_PREFIX}/bin/topgrade --gen-manpage > ${TERMUX_PREFIX}/share/man/man1/topgrade.1
+		exit 0
+	EOF
+	cat <<-EOF > ./prerm
+		#!${TERMUX_PREFIX}/bin/sh
+		rm -f ${TERMUX_PREFIX}/share/bash-completion/completions/topgrade
+		rm -f ${TERMUX_PREFIX}/share/zsh/site-functions/_topgrade
+		rm -f ${TERMUX_PREFIX}/share/fish/vendor_completions.d/topgrade.fish
+		rm -f ${TERMUX_PREFIX}/share/man/man1/topgrade.1
+		exit 0
+	EOF
 }
