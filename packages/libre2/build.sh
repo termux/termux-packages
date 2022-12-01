16d3e03d95d306ab585ead040db9dc5eab19e6b2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-01 21:01:28 +0900

    libre2: Bump to 2022.12.01

--- packages/libre2/build.sh
+++ packages/libre2/build.sh
@@ -2,8 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://github.com/google/re2
 TERMUX_PKG_DESCRIPTION="A regular expression library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.06.01
+TERMUX_PKG_VERSION=2022.12.01
 TERMUX_PKG_SRCURL=https://github.com/google/re2/archive/${TERMUX_PKG_VERSION//./-}.tar.gz
-TERMUX_PKG_SHA256=f89c61410a072e5cbcf8c27e3a778da7d6fd2f2b5b1445cd4f4508bee946ab0f
+TERMUX_PKG_SHA256=665b65b6668156db2b46dddd33405cd422bd611352c5052ab3dae6a5fbac5506
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=10
+
+	local v=$(sed -E -n 's/^SONAME=([0-9]+)$/\1/p' Makefile)
+	if [ "${_SOVERSION}" != "${v}" ]; then
+		termux_error_exit "Error: SOVERSION guard check failed."
+	fi
+}
