201d25a3b8c91b067515deba8f6793410d3552ad
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-03 02:23:55 +0900

    xfdesktop: Bump to 4.17.2

--- x11-packages/xfdesktop/build.sh
+++ x11-packages/xfdesktop/build.sh
@@ -2,13 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="A desktop manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.17.1
-TERMUX_PKG_SRCURL=https://gitlab.xfce.org/xfce/xfdesktop/-/archive/xfdesktop-${TERMUX_PKG_VERSION}/xfdesktop-xfdesktop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d5a70c23ca599a69cd15d4a353d5195b1bde2386a754f7f11a0c7dd058022908
+_MAJOR_VERSION=4.17
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfdesktop/${_MAJOR_VERSION}/xfdesktop-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=e68128dc72924680ed384d32b95e3a18d94a1ec490c05dab43155cac737bcdd1
 TERMUX_PKG_DEPENDS="exo, garcon, gdk-pixbuf, glib, gtk3, libcairo, libnotify, libwnck, libx11, libxfce4ui, libxfce4util, pango, thunar, xfconf"
 TERMUX_PKG_RECOMMENDS="hicolor-icon-theme"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-notifications --enable-maintainer-mode"
-
-termux_step_pre_configure() {
-	NOCONFIGURE=1 ./autogen.sh
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-notifications
+"
