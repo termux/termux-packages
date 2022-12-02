078d50a03a3a0b9c96bb66adbe58305e02690257
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-03 02:06:12 +0900

    libnl: Build CLI utils
    and subpackage them as libnl-cli.

---
+++ packages/libnl/libnl-cli.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_DESCRIPTION="Command-line interface utilities for libnl"
+TERMUX_SUBPKG_INCLUDE="
+bin/
+include/libnl*/netlink/cli/
+lib/libnl-cli-*.so
+lib/libnl/cli/
+lib/pkgconfig/libnl-cli-*.pc
+"
