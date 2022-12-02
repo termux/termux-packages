5a7500290b84977d0a5a14bd25cff3540726dabb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-02 09:29:22 +0900

    new package: esbuild

---
+++ packages/esbuild/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://esbuild.github.io/
+TERMUX_PKG_DESCRIPTION="An extremely fast JavaScript bundler"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.15.16
+TERMUX_PKG_SRCURL=https://github.com/evanw/esbuild/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c82913c251a912e672522df66de0798cc99bde11e1ea90756f8268995f0268b2
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
+
+termux_step_make() {
+	go build ./cmd/esbuild
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin esbuild
+}
