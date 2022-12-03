76299059364150d284a7b2b9d2e883d8b03223c3
Kevin Williams <admin@utermux.dev>
2022-12-03 13:01:59 +0800

    fix(x11/kirigami2): patches
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/kirigami2/build.sh
+++ x11-packages/kirigami2/build.sh
@@ -5,5 +5,6 @@ TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=5.100.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kirigami2-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=d6080af0c5cece525fcf261e3f71562f7f7a654599c602d530e99d99cc3501df
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtdeclarative, qt5-qtquickcontrols2, qt5-qtsvg"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
