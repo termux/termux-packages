14fe15ddcd637bc09e9a838e95c92bb7126a0ff9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-03 13:41:56 +0900

    tabbed: Bump to 0.7

--- x11-packages/tabbed/build.sh
+++ x11-packages/tabbed/build.sh
@@ -2,9 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://tools.suckless.org/tabbed/
 TERMUX_PKG_DESCRIPTION="Generic tabbed interface"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.6
+TERMUX_PKG_VERSION=0.7
 TERMUX_PKG_SRCURL=https://dl.suckless.org/tools/tabbed-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7651ea3acbec5d6a25469e8665da7fc70aba2b4fa61a2a6a5449eafdfd641c42
-TERMUX_PKG_DEPENDS="libx11"
+TERMUX_PKG_SHA256=6e8682230a213d7dabf8a79306bd3ce023875b2295a9097db427d65c1c68f322
+TERMUX_PKG_DEPENDS="libx11, libxft"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	if [ ! -e ./xembed.1 ]; then
+		cp $TERMUX_PKG_BUILDER_DIR/xembed.1 ./
+	fi
+}
