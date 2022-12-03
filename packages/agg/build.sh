90b99ccff3a31d5c6757090fb427ae86a55a34fb
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-12-03 19:35:33 +0800

    new package: agg

---
+++ packages/agg/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/asciinema/agg"
+TERMUX_PKG_DESCRIPTION="asciinema gif generator"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.3.0 (43977bf)"
+_COMMIT="43977bf11493650e76de0c9d4267bf0b4533255b"
+TERMUX_PKG_SRCURL="https://github.com/asciinema/agg/archive/${_COMMIT}.tar.gz"
+TERMUX_PKG_SHA256="4122e997de475b2f6d75aabe515407d93adbedb6ba5e573cb373621c67e2c5fd"
+TERMUX_PKG_DEPENDS="asciinema"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_setup_make_install() {
+	termux_setup_rust
+
+	_VT_REVHASH="065d4e7"
+	git clone https://github.com/asciinema/vt-rs vt
+
+	(cd vt && git checkout "${_VT_REVHASH}")
+	cargo add vt
+
+	cargo build --jobs "${TERMUX_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
+
+	install -Dm700 target/"${CARGO_TARGET_NAME}"/release/agg /usr/bin/agg
+}
