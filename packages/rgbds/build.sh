29e58a41a3c06608881b358beb835398859c6b06
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-04 10:49:30 +0900

    rgbds: Bump to 0.6.1

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://rgbds.gbdev.io
 TERMUX_PKG_DESCRIPTION="Rednex Game Boy Development System - An assembly toolchain for the Nintendo Game Boy & Game Boy Color"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.6.0
+TERMUX_PKG_VERSION=0.6.1
 TERMUX_PKG_SRCURL=https://github.com/gbdev/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a5fa40b92e0562c6a092bc81ca56614316730aca7b10b8177285023781ff0e32
+TERMUX_PKG_SHA256=872018509c6cbc2d9e947a1a6d509fd9dc56a010827f608c5f4fa46de2fa186a
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, libpng, libandroid-spawn"
+TERMUX_PKG_DEPENDS="libandroid-spawn, libandroid-support, libc++, libpng"
 
 termux_step_pre_configure() {
 	export LDFLAGS+=" -landroid-spawn"
