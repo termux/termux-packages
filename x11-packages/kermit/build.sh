9e7571684614e96d8ef80bcd9570235958fd1585
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-04 10:44:23 +0900

    kermit: Bump to 3.8

--- x11-packages/kermit/build.sh
+++ x11-packages/kermit/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/orhun/kermit
 TERMUX_PKG_DESCRIPTION="A VTE-based simple and froggy terminal emulator"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@AnGelXoG"
-TERMUX_PKG_VERSION=3.7
+TERMUX_PKG_VERSION=3.8
 TERMUX_PKG_SRCURL=https://github.com/orhun/kermit/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b45d3403de4c1feb158a468c6067b27f14b99873041113233afb9dce75846ecd
-TERMUX_PKG_DEPENDS="libvte"
+TERMUX_PKG_SHA256=7b2ad85f73bccee4f6d890693afae2002ca2c51965b83c42e1ca4f4d980468c8
+TERMUX_PKG_DEPENDS="glib, gtk3, libvte, pango"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
