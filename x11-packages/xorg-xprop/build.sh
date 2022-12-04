ec03d2520d009cd4c60178de00c5dcd3ea9bc65b
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-04 10:58:48 +0900

    xorg-xprop: Bump to 1.2.6

--- x11-packages/xorg-xprop/build.sh
+++ x11-packages/xorg-xprop/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Utility to print properties of X11 windows"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: MIT, HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="Rafael Kitover <rkitover@gmail.com>"
-TERMUX_PKG_VERSION=1.2.5
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xprop-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9b92ed0316bf2486121d8bac88bd1878f16b43bd335f18009b1f941f1eca93a1
+TERMUX_PKG_VERSION=1.2.6
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xprop-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=580b8525b12ecc0144aa16c88b0aafa76d2e799b44c8c6c50f9ce92788b5586e
 TERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
