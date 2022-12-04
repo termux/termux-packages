00626e151a47cf13e1ad0a9b88b9e2e2dd44d9f9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-12-04 16:09:11 +0800

    libc++: mark as not available for on-device build
    [skip ci]

--- packages/libc++/build.sh
+++ packages/libc++/build.sh
@@ -7,5 +7,8 @@ TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_ESSENTIAL=true
 
 termux_step_post_make_install() {
+	if $TERMUX_ON_DEVICE_BUILD; then
+		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
+	fi
 	cp "$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/${TERMUX_HOST_PLATFORM}/libc++_shared.so" $TERMUX_PREFIX/lib
 }
