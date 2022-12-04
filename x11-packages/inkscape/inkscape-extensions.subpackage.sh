64c2f39050a666c877d3a8885273b1f30f050727
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-05 02:33:26 +0900

    inkscape: Bump to 1.2.2

--- x11-packages/inkscape/inkscape-extensions.subpackage.sh
+++ x11-packages/inkscape/inkscape-extensions.subpackage.sh
@@ -1,12 +1,12 @@
 TERMUX_SUBPKG_INCLUDE="share/inkscape/extensions"
 TERMUX_SUBPKG_DESCRIPTION="Inkscape extensions"
-TERMUX_SUBPKG_DEPENDS="python,libxml2,libxslt"
+TERMUX_SUBPKG_DEPENDS="python, python-numpy"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 
 termux_step_create_subpkg_debscripts() {
 	cat <<- EOF > ./postinst
 	#!$TERMUX_PREFIX/bin/sh
 	echo "Installing dependencies through pip..."
-	MATHLIB=m pip3 install lxml numpy scour
+	pip3 install lxml scour
 	EOF
 }
