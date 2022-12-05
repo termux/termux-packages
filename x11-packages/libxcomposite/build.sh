7c62177f8d5dcc122b7e6e06ad0579301e52c1d2
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-06 02:06:20 +0900

    libxcomposite: Bump to 0.4.6

--- x11-packages/libxcomposite/build.sh
+++ x11-packages/libxcomposite/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 Composite extension library"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: HPND, MIT
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4.5
-TERMUX_PKG_REVISION=20
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXcomposite-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=b3218a2c15bab8035d16810df5b8251ffc7132ff3aa70651a1fba0bfe9634e8f
-TERMUX_PKG_DEPENDS="libx11, libxau, libxcb, libxdmcp"
+TERMUX_PKG_VERSION=0.4.6
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXcomposite-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=fe40bcf0ae1a09070eba24088a5eb9810efe57453779ec1e20a55080c6dc2c87
+TERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_BUILD_DEPENDS="libxfixes, xorgproto, xorg-util-macros"
