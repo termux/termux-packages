15e6d5198e77337d14858d998b2f5c30508b404f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-06 02:52:38 +0900

    dnsmasq: Bump to 2.88

--- root-packages/dnsmasq/build.sh
+++ root-packages/dnsmasq/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Dnsmasq provides network infrastructure for small networ
 # License: GPL-2.0-only or GPL-3.0-only
 TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.87
+TERMUX_PKG_VERSION=2.88
 TERMUX_PKG_SRCURL=https://www.thekelleys.org.uk/dnsmasq/dnsmasq-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=0228c0364a7f2356fd7e7f1549937cbf3099a78d3b2eb1ba5bb0c31e2b89de7a
+TERMUX_PKG_SHA256=23544deda10340c053bea6f15a93fed6ea7f5aaa85316bfc671ffa6d22fbc1b3
 TERMUX_PKG_BUILD_IN_SRC=true
