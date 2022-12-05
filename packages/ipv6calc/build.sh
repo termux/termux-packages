32cb963b290008e85a4ece5400ff17df4445575a
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-06 02:49:28 +0900

    ipv6calc: Bump to 4.0.2

--- packages/ipv6calc/build.sh
+++ packages/ipv6calc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.deepspace6.net/projects/ipv6calc.html
 TERMUX_PKG_DESCRIPTION="Does some format changes and calculations of IPv6 addresses"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.0.1
+TERMUX_PKG_VERSION=4.0.2
 TERMUX_PKG_SRCURL=https://github.com/pbiering/ipv6calc/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0de7dbe66c0e3602becc18bb8f69a278a02eeaf5d58779cd1c0e0069eb80a7e6
+TERMUX_PKG_SHA256=f96a89bdce201ec313f66514ee52eeab5f5ead3d2ba9efe5ed9f757632cd01a1
 TERMUX_PKG_DEPENDS="openssl, perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -16,5 +16,5 @@ TERMUX_PKG_EXTRA_MAKE_ARGS="exec_prefix=$TERMUX_PREFIX"
 
 termux_step_pre_configure() {
 	CFLAGS+=" $CPPFLAGS"
-	LDFLAGS+=" -lcrypto"
+	LDFLAGS+=" -lcrypto -lm"
 }
