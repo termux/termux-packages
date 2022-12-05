a50c5e197334bcc56938089a368903acbe904022
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-06 02:09:43 +0900

    xorg-xrandr: Bump to 1.5.2

--- x11-packages/xorg-xrandr/build.sh
+++ x11-packages/xorg-xrandr/build.sh
@@ -1,14 +1,11 @@
 TERMUX_PKG_HOMEPAGE="https://xorg.freedesktop.org/"
 TERMUX_PKG_DESCRIPTION="Primitive command line interface to RandR extension"
-TERMUX_PKG_LICENSE="MIT"
+# License: HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.5.1"
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/xorg/app/xrandr/-/archive/xrandr-${TERMUX_PKG_VERSION}/xrandr-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256="ca4d5083c86660cb00b465365aab738bcdb310ee9715ad2cf1571f679226229e"
+TERMUX_PKG_VERSION=1.5.2
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xrandr-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=c8bee4790d9058bacc4b6246456c58021db58a87ddda1a9d0139bf5f18f1f240
 TERMUX_PKG_DEPENDS="libx11, libxrandr"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros, xorgproto"
-
-termux_step_pre_configure() {
-	NOCONFIGURE=1 ./autogen.sh
-}
