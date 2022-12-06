5aef8938d4278fbc508576503955d66ed728f6c5
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-07 03:04:36 +0900

    pinentry{,-gtk}: Include pinentry-tty

--- packages/pinentry/build.sh
+++ packages/pinentry/build.sh
@@ -3,8 +3,12 @@ TERMUX_PKG_DESCRIPTION="Dialog allowing secure password entry"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=457a185e5a85238fb945a955dc6352ab962dc8b48720b62fc9fa48c7540a4067
-TERMUX_PKG_DEPENDS="libandroid-support, libassuan, libiconv, ncurses"
-TERMUX_PKG_BUILD_DEPENDS="libgpg-error"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pinentry-fltk --without-libcap"
+TERMUX_PKG_DEPENDS="libandroid-support, libassuan, libgpg-error, libiconv, ncurses"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-pinentry-fltk
+--enable-pinentry-tty
+--without-libcap
+"
