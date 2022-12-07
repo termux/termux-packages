bad28a78a2745135388f5f70dc6a2d399be9cec9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-07 13:38:03 +0900

    yosys: Bump to 0.24

--- packages/yosys/build.sh
+++ packages/yosys/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://yosyshq.net/yosys/
 TERMUX_PKG_DESCRIPTION="A framework for RTL synthesis tools"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.23
+TERMUX_PKG_VERSION=0.24
 TERMUX_PKG_SRCURL=https://github.com/YosysHQ/yosys/archive/refs/tags/yosys-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ec982a9393b3217deecfbd3cf9a64109b85310a949e46a51cf2e07fba1071aeb
+TERMUX_PKG_SHA256=6a00b60e2d6bc8df0db1e66aa27af42a0694121cfcd6a3cf6f39c9329ed91263
 TERMUX_PKG_DEPENDS="graphviz, libandroid-glob, libandroid-spawn, libc++, libffi, readline, tcl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
