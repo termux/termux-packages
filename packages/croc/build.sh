b329c2088758e828492a4eced9e1419da403c8d8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-07 13:30:17 +0900

    croc: Bump to 9.6.2

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another"
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="9.6.1"
-TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=16c0e2b2605463fc1066dc0c31cfede690396740705205fc380f05e03fb3d785
+TERMUX_PKG_VERSION=9.6.2
+TERMUX_PKG_SRCURL=https://github.com/schollz/croc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=af1889daca1cd71445975aebb80dc416b01b1e8d63cd261a79a5dd1a3fd74b29
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
