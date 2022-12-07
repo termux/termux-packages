ca4e15d9d5fe34657cbeac27adf46c8fc15f4af3
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-07 13:40:14 +0900

    yuma123: Bump to 2.13

--- packages/yuma123/build.sh
+++ packages/yuma123/build.sh
@@ -3,27 +3,19 @@ TERMUX_PKG_DESCRIPTION="Provides an opensource YANG API in C"
 TERMUX_PKG_LICENSE="BSD 3-Clause, MIT, Public Domain"
 TERMUX_PKG_LICENSE_FILE="debian/copyright"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.12
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.13
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/yuma123/yuma123_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=81a0e4d4b4420891fb0a091e9cf4c36d50dbdc5dfedb09609e9b1fee58377ea9
-TERMUX_PKG_DEPENDS="libiconv, liblzma, libssh2, libxml2, openssl, readline, zlib"
+TERMUX_PKG_SHA256=e304b253236a279f10b133fdd19f366f271581ebf12647cea84667fcfada1f0c
+TERMUX_PKG_DEPENDS="libssh2, libxml2, openssl, readline"
 
 termux_step_pre_configure() {
+	local f
+	for f in netconf/src/*/Makefile.am; do
+		sed -i '/_la_LDFLAGS = /s/ -lrt$//' ${f}
+		sed -i '/_la_LDFLAGS = /s/ -lrt / /' ${f}
+	done
+
 	autoreconf -fi
 
 	CPPFLAGS+=" -D__USE_BSD"
-
-	_NEED_DUMMY_LIBRT_A=
-	_LIBRT_A=$TERMUX_PREFIX/lib/librt.a
-	if [ ! -e $_LIBRT_A ]; then
-		_NEED_DUMMY_LIBRT_A=true
-		echo '!<arch>' > $_LIBRT_A
-	fi
-}
-
-termux_step_post_make_install() {
-	if [ $_NEED_DUMMY_LIBRT_A ]; then
-		rm -f $_LIBRT_A
-	fi
 }
