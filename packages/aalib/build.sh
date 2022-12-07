9217c2ec423ff28b7ae554e88b5eac9bbcdf3f50
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-07 11:14:08 +0900

    aalib: Fix infodir and mandir
    which should be `$PREFIX/share/{info,man}`.

--- packages/aalib/build.sh
+++ packages/aalib/build.sh
@@ -3,10 +3,14 @@ TERMUX_PKG_DESCRIPTION="A portable ASCII art graphic library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4rc5
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/aa-project/aalib-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fbddda9230cf6ee2a4f5706b4b11e2190ae45f5eda1f0409dc4f99b35e0a70ee
 TERMUX_PKG_DEPENDS="ncurses"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--infodir=$TERMUX_PREFIX/share/info
+--mandir=$TERMUX_PREFIX/share/man
+"
 
 termux_step_pre_configure() {
 	local _bin=$TERMUX_PKG_BUILDDIR/_wrapper/bin
