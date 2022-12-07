2c0e54e8693f7495d48b5da2f6d63469e5b324f4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-08 06:31:11 +0900

    new package: vgmstream

---
+++ packages/vgmstream/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/vgmstream/vgmstream
+TERMUX_PKG_DESCRIPTION="A library for playback of various streamed audio formats used in video games"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1800
+TERMUX_PKG_SRCURL=https://github.com/vgmstream/vgmstream/archive/refs/tags/r${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c3d3918264101662dd35f6be6024bc4c5412f7fe8a49806da0c60cc9574450b0
+TERMUX_PKG_DEPENDS="ffmpeg, libao, libjansson, libspeex, libvorbis, mpg123"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DUSE_G719=OFF
+-DUSE_ATRAC9=OFF
+-DUSE_CELT=OFF
+-DBUILD_AUDACIOUS=OFF
+"
