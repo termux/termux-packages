42e852e2c719cd537aa1dcc204219e1ddb53f1c7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-08 03:40:21 +0900

    esbuild: Bump to 0.16.1

--- packages/esbuild/build.sh
+++ packages/esbuild/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://esbuild.github.io/
 TERMUX_PKG_DESCRIPTION="An extremely fast JavaScript bundler"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.15.18
+TERMUX_PKG_VERSION=0.16.1
 TERMUX_PKG_SRCURL=https://github.com/evanw/esbuild/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fb685104ccb45201325adb998f346d15c23cab9cdb8b8d3e6d5a3cdafcabd55a
+TERMUX_PKG_SHA256=10be8f862a2976cbc8d2b9c4a4a2564c7879f5aeeb1933f3ccf7fb5aa979dbd9
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
