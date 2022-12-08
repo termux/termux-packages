8b28c64e476a02ebf18c3e116d85a2bb1b43029d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-09 05:24:07 +0900

    xorg-xkbcomp: Bump to 1.4.6

--- x11-packages/xorg-xkbcomp/build.sh
+++ x11-packages/xorg-xkbcomp/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X Keyboard description compiler"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: HPND, MIT
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.5
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xkbcomp-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6851086c4244b6fd0cc562880d8ff193fb2bbf1e141c73632e10731b31d4b05e
+TERMUX_PKG_VERSION=1.4.6
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xkbcomp-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=fa50d611ef41e034487af7bd8d8c718df53dd18002f591cca16b0384afc58e98
 TERMUX_PKG_DEPENDS="libx11, libxkbfile"
