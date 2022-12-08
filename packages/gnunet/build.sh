9ea4be488f5254a605a9060f9511615f6e77ea11
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-09 04:10:49 +0900

    gnunet: Bump to 0.19.0

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_SRCURL=https://git.gnunet.org/git/gnunet.git
-TERMUX_PKG_VERSION=0.18.1
+TERMUX_PKG_VERSION=0.19.0
 TERMUX_PKG_DEPENDS="libcurl, libgcrypt, libgnutls, libgpg-error, libidn2, libjansson, libltdl, libmicrohttpd, libsodium, libsqlite, libunistring, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_have_decl_struct_in6_ifreq=yes"
