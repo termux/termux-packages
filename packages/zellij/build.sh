f169b8cd0033b03df825e131b02760a7041b3cd7
Jonathan LEI <me@xjonathan.dev>
2022-12-09 13:54:21 +0000

    zellij: Bump to 0.34.3

--- packages/zellij/build.sh
+++ packages/zellij/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://zellij.dev/"
 TERMUX_PKG_DESCRIPTION="A terminal workspace with batteries included"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Jonathan Lei <me@xjonathan.dev>"
-TERMUX_PKG_VERSION="0.33.0"
+TERMUX_PKG_VERSION="0.34.3"
 TERMUX_PKG_SRCURL="https://github.com/zellij-org/zellij/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256="63eaf8ca0c1235389281e3ee5f599b810de3921d220e500cb35c46ec9b5125ff"
+TERMUX_PKG_SHA256="1cf1e5a87eb3bd458487b2db91cd8918b6df308de2809041fc62df30c50a735c"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
