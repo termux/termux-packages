4de4aa5ee329809b72d327228d98e18c0ccf8ccb
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-10 09:17:10 +0900

    libprotobuf: Bump to 3.21.11

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -5,9 +5,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 # Please as well update SHA256 checksum for $_PROTOBUF_ZIP in
 #   $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
 # when bumping version.
-TERMUX_PKG_VERSION=2:3.21.10
+TERMUX_PKG_VERSION=2:3.21.11
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=90de7e780db97e0ee8cfabc3aecc0da56c3d443824b968ec0c7c600f9585b9ba
+TERMUX_PKG_SHA256=902f0db07d0e4726b4b649c1aaecc7c514e1c0724841e7c0b49691b28fa3d9ec
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev"
--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -14,7 +14,7 @@ termux_setup_protobuf() {
 			termux_download \
 				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
 				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				0d13805474b85611c74f00c738a85ad00a25828dbf6e501de29d7f56b3dbcc03
+				3c3eaee739187ad375daae3d74648efd4bc3eaf82c92b57d0fa2cce6460eb291
 
 			rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
 			unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
