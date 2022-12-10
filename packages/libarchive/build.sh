da6cfa15f4d9c7987cedb614c093699ec02c769e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-11 07:36:49 +0900

    libarchive: Fix pkgconfig file
    Upstream: https://github.com/libarchive/libarchive/issues/1766

--- packages/libarchive/build.sh
+++ packages/libarchive/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Multi-format archive and compression library"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.6.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libarchive/libarchive/releases/download/v$TERMUX_PKG_VERSION/libarchive-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ba6d02f15ba04aba9c23fd5f236bb234eab9d5209e95d1c4df85c44d5f19b9b3
 TERMUX_PKG_AUTO_UPDATE=true
@@ -18,3 +19,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-acl
 --disable-xattr
 "
+
+termux_step_post_make_install() {
+	# https://github.com/libarchive/libarchive/issues/1766
+	sed -i '/^Requires\.private:/s/ iconv//' \
+		$TERMUX_PREFIX/lib/pkgconfig/libarchive.pc
+}
