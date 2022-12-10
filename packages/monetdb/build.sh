79711aae920e9afa83a4b6d85798af86897c1746
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-10 09:11:48 +0900

    monetdb: Bump to 11.45.11

--- packages/monetdb/build.sh
+++ packages/monetdb/build.sh
@@ -2,10 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.monetdb.org/
 TERMUX_PKG_DESCRIPTION="A high-performance database kernel for query-intensive applications"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=11.45.7
+TERMUX_PKG_VERSION=11.45.11
 TERMUX_PKG_SRCURL=https://www.monetdb.org/downloads/sources/archive/MonetDB-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3707897bb84ecbb73b196bed06a017a7f8a9f50bc8cfca87eb496e87d9254a0e
+TERMUX_PKG_SHA256=f35f3d13facc959f117dcb96ec68c7501d7152078a75ac57641c5b41893a6b73
 TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libbz2, libcurl, libiconv, liblz4, liblzma, libxml2, netcdf-c, pcre, readline, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DTESTING=OFF
+"
 
 termux_step_post_get_source() {
 	find . -name '*.c' | xargs -n 1 sed -i \
@@ -13,5 +16,5 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	LDFLAGS+=" -landroid-sysv-semaphore"
+	LDFLAGS+=" -landroid-sysv-semaphore -lm"
 }
