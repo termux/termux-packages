49344715d9bd0c6abcb479d4f23c1e75d7834532
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-12 12:19:51 +0900

    global: Bump to 6.6.9

--- packages/global/build.sh
+++ packages/global/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/global/
 TERMUX_PKG_DESCRIPTION="Source code search and browse tools"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.6.8
+TERMUX_PKG_VERSION=6.6.9
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/global/global-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6f93d9732a07175817907d26640a90dc1009918e02be761bba09d1fa068357cd
+TERMUX_PKG_SHA256=aacba0fa8d60ca645e62f312dcd23b47ed48a081aa0fb1563cff4702d9d1fad5
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_posix1_2008_realpath=yes
 --with-posix-sort=$TERMUX_PREFIX/bin/sort
