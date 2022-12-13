3e8fce2eef274643c849e741d56e00945b1d3a47
Leonid Pliushch <leonid.pliushch@gmail.com>
2022-12-13 10:52:07 +0200

    add(main): agg

---
+++ packages/agg/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/asciinema/agg"
+TERMUX_PKG_DESCRIPTION="asciinema gif generator"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.3.0"
+TERMUX_PKG_SRCURL="https://github.com/asciinema/agg/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="51cb553f9adde28f85e5e945c0013679c545820c4c023fefb9e74b765549e709"
+TERMUX_PKG_RECOMMENDS="asciinema"
+TERMUX_PKG_BUILD_IN_SRC=true
