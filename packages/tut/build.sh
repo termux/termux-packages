a372b700fc51b096a606507a0ceacdd19337bd6d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-14 12:45:38 +0900

    tut: Bump to 1.0.25

--- packages/tut/build.sh
+++ packages/tut/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/RasmusLindroth/tut
 TERMUX_PKG_DESCRIPTION="A TUI for Mastodon with vim inspired keys"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.24
+TERMUX_PKG_VERSION=1.0.25
 TERMUX_PKG_SRCURL=https://github.com/RasmusLindroth/tut/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9d4f75dbf5a7d1ad9e2857838612a507eed4d297cfdfe59bef48856127bb6efb
+TERMUX_PKG_SHA256=df1b4532a31fd90213c666dc508f6299a8785fe99e32317e86af739c76724b0f
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
