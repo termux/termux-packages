90cf224d1d1374ea73258b5ebc0eeef8b023f005
Max Horn <max@quendi.de>
2022-12-19 00:33:17 +0100

    gap: update to 4.12.2
    Several workarounds are not needed anymore

--- packages/gap/build.sh
+++ packages/gap/build.sh
@@ -2,21 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.gap-system.org/
 TERMUX_PKG_DESCRIPTION="GAP is a system for computational discrete algebra, with particular emphasis on Computational Group Theory"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.12.1
+TERMUX_PKG_VERSION=4.12.2
 TERMUX_PKG_SRCURL=https://github.com/gap-system/gap/releases/download/v${TERMUX_PKG_VERSION}/gap-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f9ebef11ee31b210ce36e3c70960742b4e253282bbd5270adc9324273c92b016
+TERMUX_PKG_SHA256=672308745eb78a222494ee8dd6786edd5bc331456fcc6456ac064bdb28d587a8
 TERMUX_PKG_DEPENDS="readline, libgmp, zlib, gap-packages"
 TERMUX_PKG_BREAKS="gap-dev"
 TERMUX_PKG_REPLACES="gap-dev"
 TERMUX_PKG_GROUPS="science"
-
-termux_step_post_configure() {
-	# workaround build system bug that occurs when doing
-	# an out-of-tree cross compilation build
-	mkdir -p src
-	cp $TERMUX_PKG_SRCDIR/src/ffdata.* src/
-	cp $TERMUX_PKG_SRCDIR/src/c_*.c src/
-}
 
 termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/lib/gap/pkg
@@ -27,9 +19,7 @@ termux_step_post_make_install() {
 	cp -r $TERMUX_PKG_SRCDIR/pkg/transgrp $TERMUX_PREFIX/lib/gap/pkg/
 	cp -r $TERMUX_PKG_SRCDIR/pkg/primgrp $TERMUX_PREFIX/lib/gap/pkg/
 
-	# To get them to be small enough, could gzip them in place
+	# To save some disk space, compress transgrp data in place
 	# (GAP transparently allows read access to those)
-	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/*/data/*.*
-	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/smallgrp/id*/*.*
-	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/smallgrp/small*/*.*
+	gzip -9 -n -f $TERMUX_PREFIX/lib/gap/pkg/transgrp/data/*.*
 }
