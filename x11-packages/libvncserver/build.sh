895b9ba62f42c9a9436ee85a3366f56548f6af90
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-19 14:24:20 +0900

    libvncserver: Bump to 0.9.14

--- x11-packages/libvncserver/build.sh
+++ x11-packages/libvncserver/build.sh
@@ -2,8 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://libvnc.github.io/
 TERMUX_PKG_DESCRIPTION="Cross-platform C libraries that allow you to easily implement VNC server or client functionality"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.13
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=0.9.14
 TERMUX_PKG_SRCURL=https://github.com/LibVNC/libvncserver/archive/LibVNCServer-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0ae5bb9175dc0a602fe85c1cf591ac47ee5247b87f2bf164c16b05f87cbfa81a
-TERMUX_PKG_DEPENDS="libgcrypt, libgnutls, libjpeg-turbo, libpng, openssl"
+TERMUX_PKG_SHA256=83104e4f7e28b02f8bf6b010d69b626fae591f887e949816305daebae527c9a5
+TERMUX_PKG_DEPENDS="libgcrypt, libgnutls, libjpeg-turbo, liblzo, libpng, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DWITH_OPENSSL=OFF
+-DWITH_SASL=OFF
+"
+
+termux_step_post_massage() {
+	local f
+	for f in ./lib/pkgconfig/libvnc{client,server}.pc; do
+		if [ -e "${f}" ]; then
+			sed -i '/^Libs\.private:/s/ -l\(-pthread\)/ \1/' "${f}"
+		fi
+	done
+}
