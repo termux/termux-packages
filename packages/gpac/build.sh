dfd7c210932ce32727d63fb19198d11633369091
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-20 23:24:42 +0900

    gpac: Bump to 2.2.0

--- packages/gpac/build.sh
+++ packages/gpac/build.sh
@@ -3,12 +3,10 @@ TERMUX_PKG_DESCRIPTION="An open-source multimedia framework focused on modularit
 # License: LGPL-2.1-or-later
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.2.0
 TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c820386f9d583c740fc3f57cb93864a8164838516dabc96de2f61d5b7429e0b6
-TERMUX_PKG_DEPENDS="ffmpeg, freetype, liba52, libjpeg-turbo, liblzma, libmad, libnghttp2, libpng, libtheora, libvorbis, openjpeg, openssl, pulseaudio, xvidcore, zlib"
-TERMUX_PKG_BUILD_DEPENDS="libogg"
+TERMUX_PKG_SHA256=c20c204b57da76e4726109993c1abcdb3231a9b2ee2c8e21126d000cda7fc00d
+TERMUX_PKG_DEPENDS="ffmpeg, freetype, liba52, libjpeg-turbo, liblzma, libmad, libnghttp2, libogg, libpng, libtheora, libvorbis, openjpeg, openssl, pulseaudio, xvidcore, zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="STRIP=:"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11"
 
