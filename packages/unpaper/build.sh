e34322fdf6bca9bbbad03e8f6e198de19c703c03
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-20 23:19:39 +0900

    unpaper: Bump to 7.0.0

--- packages/unpaper/build.sh
+++ packages/unpaper/build.sh
@@ -2,21 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.flameeyes.com/projects/unpaper
 TERMUX_PKG_DESCRIPTION="A post-processing tool for scanned sheets of paper"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=e5154081c1c944719ff6ba70a8c114d1656ea799
-TERMUX_PKG_VERSION=2022.01.21
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/unpaper/unpaper.git
+TERMUX_PKG_VERSION=1:7.0.0
+TERMUX_PKG_SRCURL=https://github.com/unpaper/unpaper/archive/refs/tags/unpaper-${TERMUX_PKG_VERSION#*:}.tar.gz
+TERMUX_PKG_SHA256=601b64b030edb4a734510a7bdcab371c7a33c4501f9a0e231e3d326df50e2313
 TERMUX_PKG_DEPENDS="ffmpeg"
-TERMUX_PKG_GIT_BRANCH=main
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
-}
