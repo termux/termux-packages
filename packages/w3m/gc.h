50ee46b7b3341afbb9d7de1dbde88a67432ab9ad
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-20 23:51:02 +0900

    w3m: Bump to 0.5.3.20220429

--- packages/w3m/gc.h
+++ packages/w3m/gc.h
@@ -3,6 +3,7 @@
 #define GC_INIT()
 #define GC_MALLOC(arg) malloc(arg)
 #define GC_malloc(arg) malloc(arg)
+#define GC_REALLOC(arg1,arg2) realloc(arg1,arg2)
 #define GC_MALLOC_ATOMIC(arg) malloc(arg)
 
 #define GC_free(arg) free(arg)
