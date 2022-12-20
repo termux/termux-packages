7146cd52d29fb844692276b8ee4e64e7a80eeee1
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-20 23:10:14 +0900

    gdrive-downloader: Bump to 1.0

--- packages/gdrive-downloader/build.sh
+++ packages/gdrive-downloader/build.sh
@@ -1,25 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/Akianonymus/gdrive-downloader
 TERMUX_PKG_DESCRIPTION="Download a gdrive folder or file easily, shell ftw"
 TERMUX_PKG_LICENSE="Unlicense"
-_COMMIT=c29e82b8635fa0dda3e93ad019d2ce58ebaa5656
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.07.14
-TERMUX_PKG_SRCURL=$TERMUX_PKG_HOMEPAGE.git
-TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_VERSION=1:1.0
+TERMUX_PKG_SRCURL=https://github.com/Akianonymus/gdrive-downloader/archive/refs/tags/v${TERMUX_PKG_VERSION#*:}.tar.gz
+TERMUX_PKG_SHA256=26c726bce41bff3b58c1f819a5c1f2e54d66b4ee3d592a5d52088de605c48d95
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS='bash, curl'
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
-}
 
 termux_step_make_install() {
 	install -D release/bash/* -t "$TERMUX_PREFIX/bin"
