b43dbca74a89cba237805c5853f7f19f8ba3066e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-20 23:42:47 +0900

    perl-rename: Bump to 1.12

--- packages/perl-rename/build.sh
+++ packages/perl-rename/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://search.cpan.org/~pederst/rename/
 TERMUX_PKG_DESCRIPTION="renames multiple files using perl expressions."
 TERMUX_PKG_LICENSE="Artistic-License-2.0, GPL-2.0" # https://metacpan.org/pod/Software::License::Perl_5
 TERMUX_PKG_MAINTAINER="@ELWAER-M"
-TERMUX_PKG_VERSION=1.11
+TERMUX_PKG_VERSION=1.12
 TERMUX_PKG_SRCURL=https://cpan.metacpan.org/authors/id/P/PE/PEDERST/rename-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2f03f33d3a17d3a3599c83f514f0694fd833b606920132925e3fdbd8a1a3e44b
+TERMUX_PKG_SHA256=bca72eda72a8e826c0050738a5f5a69a289785aeb2ffc6d71f389da04fbcedd2
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
