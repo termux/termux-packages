707cf95aac97bfe0a2c0daaa10db96d2e4addc11
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-20 23:33:55 +0900

    libspice-server: Bump to 0.15.1

--- packages/libspice-server/build.sh
+++ packages/libspice-server/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.spice-space.org/
 TERMUX_PKG_DESCRIPTION="Implements the server side of the SPICE protocol"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.15.0
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=0.15.1
 TERMUX_PKG_SRCURL=https://www.spice-space.org/download/releases/spice-server/spice-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=b320cf8f4bd2852750acb703c15b72856027e5a8554f8217dfbb3cc09deba0f5
+TERMUX_PKG_SHA256=ada9af67ab321916bd7eb59e3d619a4a7796c08a28c732edfc7f02fc80b1a37a
 TERMUX_PKG_DEPENDS="glib, gst-plugins-base, gstreamer, libc++, libiconv, libjpeg-turbo, liblz4, libopus, liborc, libpixman, libsasl, libspice-protocol, openssl, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
