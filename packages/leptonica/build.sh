1b97a3a2bb595fbf0de2511e30546e8062c09fb7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-23 00:16:04 +0900

    leptonica: Bump to 1.83.0

--- packages/leptonica/build.sh
+++ packages/leptonica/build.sh
@@ -3,15 +3,18 @@ TERMUX_PKG_DESCRIPTION="Library for image processing and image analysis"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="leptonica-license.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.82.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.83.0
 TERMUX_PKG_SRCURL=https://github.com/DanBloomberg/leptonica/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=40fa9ac1e815b91e0fa73f0737e60c9eec433a95fa123f95f2573dd3127dd669
+TERMUX_PKG_SHA256=14cf531c2219a1414e8e3c51a3caa5cf021a52e782c4a6561bf64d0ef2119282
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="giflib, libjpeg-turbo, libpng, libtiff, libwebp, openjpeg, zlib"
 TERMUX_PKG_BREAKS="leptonica-dev"
 TERMUX_PKG_REPLACES="leptonica-dev"
 
 termux_step_pre_configure() {
+	# Silence tmpfile warnings:
+	find src -name '*.c' | xargs -n 1 \
+		sed -i 's/L_INFO("work-around: writing to a temp file\\n", __func__)/((void)0)/'
+
 	./autogen.sh
 }
