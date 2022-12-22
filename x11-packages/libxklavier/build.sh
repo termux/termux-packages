6c007f3f1327e55216806bb3ffa77b455d6546f8
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-23 04:26:30 +0900

    libxklavier: Specify xkb-bin-base

--- x11-packages/libxklavier/build.sh
+++ x11-packages/libxklavier/build.sh
@@ -1,14 +1,22 @@
 TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/LibXklavier/
 TERMUX_PKG_DESCRIPTION="High-level API for X Keyboard Extension"
-TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.4
-TERMUX_PKG_REVISION=21
+TERMUX_PKG_REVISION=22
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/archived-projects/libxklavier/-/archive/libxklavier-${TERMUX_PKG_VERSION}/libxklavier-libxklavier-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e1638599e9229e6f6267b70b02e41940b98ba29b3a37e221f6e59ff90100c3da
-TERMUX_PKG_DEPENDS="glib, iso-codes, libxi, libxkbfile, libxml2, xkeyboard-config"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-xkb-base=$TERMUX_PREFIX/share/X11/xkb"
+TERMUX_PKG_DEPENDS="glib, iso-codes, libx11, libxi, libxkbfile, libxml2, xkeyboard-config, xorg-xkbcomp"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-introspection
+--with-xkb-base=$TERMUX_PREFIX/share/X11/xkb
+--with-xkb-bin-base=$TERMUX_PREFIX/bin
+"
 
 termux_step_pre_configure() {
+	termux_setup_gir
+
 	NOCONFIGURE=1 ./autogen.sh
 }
