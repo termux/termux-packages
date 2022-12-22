d4129ab4ed8f6448d2adb462554df8b147fecf5e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-23 06:37:59 +0900

    scrcpy: Bump to 1.25

--- packages/scrcpy/build.sh
+++ packages/scrcpy/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Genymobile/scrcpy
 TERMUX_PKG_DESCRIPTION="Provides display and control of Android devices connected via USB or over TCP/IP"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.24
+TERMUX_PKG_VERSION=1.25
 TERMUX_PKG_SRCURL=https://github.com/Genymobile/scrcpy/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e3054ad453ac577b941f8df0eabc94e842affc6e1d10ba8d21cededfa2eacc73
+TERMUX_PKG_SHA256=dfecc9dcffd45540bef88a7e346d37bead3665a5c868a5a95c5ec7bfed43ad07
 TERMUX_PKG_DEPENDS="ffmpeg, libusb, sdl2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dprebuilt_server=$TERMUX_PKG_SRCDIR/scrcpy-server-v${TERMUX_PKG_VERSION}
@@ -12,6 +12,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_post_get_source() {
 	local _url=https://github.com/Genymobile/scrcpy/releases/download/v${TERMUX_PKG_VERSION}/scrcpy-server-v${TERMUX_PKG_VERSION}
-	local _sha256=ae74a81ea79c0dc7250e586627c278c0a9a8c5de46c9fb5c38c167fb1a36f056
+	local _sha256=ce0306c7bbd06ae72f6d06f7ec0ee33774995a65de71e0a83813ecb67aec9bdb
 	termux_download ${_url} $(basename ${_url}) ${_sha256}
 }
