51a96890a743ef26b0bed6c8abb577ce6fd1640d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-23 12:15:55 +0900

    libnghttp2: Add SOVERSION guard
    %ci:no-build

--- packages/libnghttp2/build.sh
+++ packages/libnghttp2/build.sh
@@ -11,3 +11,19 @@ TERMUX_PKG_REPLACES="libnghttp2-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-lib-only"
 # The tools are not built due to --enable-lib-only:
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1 share/nghttp2/fetch-ocsp-response"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=14
+
+	local a
+	for a in LT_CURRENT LT_AGE; do
+		local _${a}=$(sed -En 's/^AC_SUBST\('"${a}"',\s*([0-9]+).*/\1/p' \
+				configure.ac)
+	done
+	local v=$(( _LT_CURRENT - _LT_AGE ))
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
