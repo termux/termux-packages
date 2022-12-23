6c61f1321e2cb4f0e8a26cb19bf6e575f5e38ac0
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-23 15:22:04 +0900

    {lib,}mediainfo: Bump to 22.12

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Library for reading information from media files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=22.09
+TERMUX_PKG_VERSION=22.12
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/libmediainfo/${TERMUX_PKG_VERSION}/libmediainfo_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ea797dfc3d1f41408915945b22eba2d6d4d9f27ab2b31a37fe907cf7ab1e3384
+TERMUX_PKG_SHA256=fd8f69a74ff66e057b94e9fc3450adb9679f85655d73e5fef335f3f554de0cfc
 TERMUX_PKG_DEPENDS="libc++, libcurl, libzen, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-shared --enable-static --with-libcurl"
 
