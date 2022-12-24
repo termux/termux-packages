e1af94c8c2cc1e8ec53ac1db6dae1f6df1528e4b
ruki <waruqi@gmail.com>
2022-12-24 16:59:28 +0800

    Update build.sh

--- packages/xmake/build.sh
+++ packages/xmake/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION="2.7.4"
 TERMUX_PKG_SRCURL=https://github.com/xmake-io/xmake/releases/download/v${TERMUX_PKG_VERSION}/xmake-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d490ff8825fa53fe5abfb549310cb54a2dfef1ebd3f82e24548483772994e06a
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure () {
 	$TERMUX_PKG_SRCDIR/configure --prefix=$TERMUX_PREFIX
