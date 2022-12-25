151361732803e9366b544e498fbfa4757f818f6a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2022-12-26 03:13:45 +0800

    python-numpy: link against libandroid-complex-math
    To fix the following error on Android 7.
    
    dlopen failed: cannot locate symbol "csinl" referenced by "@TERMUX_PREFIX@/python3.11/site-packages/numpy/core/_multiarray_umath.cpython-311.so"

--- packages/python-numpy/build.sh
+++ packages/python-numpy/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="The fundamental package for scientific computing with Py
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.24.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/numpy/numpy.git
-TERMUX_PKG_DEPENDS="libc++, libopenblas, python"
+TERMUX_PKG_DEPENDS="libandroid-complex-math, libc++, libopenblas, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
@@ -31,7 +32,7 @@ termux_step_configure() {
 	. ${_CROSSENV_PREFIX}/bin/activate
 	build-pip install wheel
 
-	LDFLAGS+=" -lpython${_PYTHON_VERSION} -lm"
+	LDFLAGS+=" -lpython${_PYTHON_VERSION} -landroid-complex-math -lm"
 
 	build-pip install pybind11 Cython pythran wheel
 }
