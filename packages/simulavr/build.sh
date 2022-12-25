d10143c869ee3c8de1685a535f6fe51cd25e86a4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-26 01:48:36 +0900

    simulavr: Refactor build.sh

--- packages/simulavr/build.sh
+++ packages/simulavr/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE="https://www.nongnu.org/simulavr"
 TERMUX_PKG_DESCRIPTION="Simulator for Microchip AVR (formerly Atmel) microcontrollers"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
+_VERSION_MAJOR=1
+_VERSION_MINOR=1
+_VERSION_PATCH=0
+TERMUX_PKG_VERSION=1:${_VERSION_MAJOR}.${_VERSION_MINOR}.${_VERSION_PATCH}
 TERMUX_PKG_SRCURL="https://git.savannah.nongnu.org/git/simulavr.git"
-TERMUX_PKG_GIT_BRANCH="master"
-# tag: release-1.1.0
-_COMMIT="e6eac877e8eb497e1620adfd48f5bc85099d306f"
-TERMUX_PKG_VERSION="2019.12.20"
+TERMUX_PKG_GIT_BRANCH=release-${TERMUX_PKG_VERSION#*:}
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS='
@@ -17,13 +18,11 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS='
 '
 
 termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local version="$(git log -1 --date=format:"%Y.%m.%d" --format="%ad")"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
+	echo "Applying hardcode-version.diff"
+	sed \
+		-e "s|@VERSION_MAJOR@|${_VERSION_MAJOR}|g" \
+		-e "s|@VERSION_MINOR@|${_VERSION_MINOR}|g" \
+		-e "s|@VERSION_PATCH@|${_VERSION_PATCH}|g" \
+		$TERMUX_PKG_BUILDER_DIR/hardcode-version.diff \
+		| patch --silent -p1
 }
