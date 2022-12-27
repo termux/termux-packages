25e4076aebd407e32b2a667eb154ed6db11c15f4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 05:15:04 +0900

    double-conversion: Add SOVERSION guard
    %ci:no-build

--- packages/double-conversion/build.sh
+++ packages/double-conversion/build.sh
@@ -8,3 +8,15 @@ TERMUX_PKG_SHA256=e40d236343cad807e83d192265f139481c51fc83a1c49e406ac6ce0a0ba7cd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=3
+
+	local v=$(sed -En 's/^\s*set_target_properties\(double-conversion\s+.*\s+SOVERSION\s+([0-9]+).*/\1/p' \
+			CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
