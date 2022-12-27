8037a1b12036078980ead98be8909bc3988f7bca
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 02:18:51 +0900

    libgit2: Add SOVERSION guard
    %ci:no-build

--- packages/libgit2/build.sh
+++ packages/libgit2/build.sh
@@ -17,6 +17,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DREGEX_BACKEND=pcre
 "
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=1.5
+
+	local v=$(echo ${TERMUX_PKG_VERSION#*:} | cut -d . -f 1-2)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_pre_configure() {
 	find "$TERMUX_PKG_SRCDIR" -name CMakeLists.txt | xargs -n 1 \
 		sed -i 's/\( PROPERTIES C_STANDARD\) 90/\1 99/g'
