35de141530250e7650aa2e75306780e09273aec9
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-27 18:18:45 +0900

    graphicsmagick: Bump to 1.3.39

--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=http://www.graphicsmagick.org/
 TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.38
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=1.3.39
 # Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d60cd9db59351d2b9cb19beb443170acaa28f073d13d258f67b3627635e32675
+TERMUX_PKG_SHA256=e30b1ca58e873d0a1ee208384724424db2d3c33a54034e261d14e8fbb8f8d04f
 TERMUX_PKG_DEPENDS="freetype, libbz2, libc++, libde265, libheif, libjasper, libjpeg-turbo, liblzma, libpng, libtiff, libwebp, libxml2, littlecms, zlib, zstd"
 TERMUX_PKG_BREAKS="graphicsmagick-dev"
 TERMUX_PKG_REPLACES="graphicsmagick++, graphicsmagick-dev"
