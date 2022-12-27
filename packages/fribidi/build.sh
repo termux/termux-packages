7367a0566922d0eff84266fdeb8fadaee1fe9c09
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-27 22:14:08 +0900

    fribidi: Add SOVERSION guard
    %ci:no-build

--- packages/fribidi/build.sh
+++ packages/fribidi/build.sh
@@ -10,3 +10,19 @@ TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BREAKS="fribidi-dev"
 TERMUX_PKG_REPLACES="fribidi-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-docs"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=0
+
+	local a
+	for a in INTERFACE_VERSION BINARY_AGE; do
+		local _${a}=$(sed -En 's/^m4_define\(fribidi_'"${a,,}"',\s*([0-9]+).*/\1/p' \
+				configure.ac)
+	done
+	local v=$(( _INTERFACE_VERSION - _BINARY_AGE ))
+	if [ ! "${_INTERFACE_VERSION}" ] || [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
