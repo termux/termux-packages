845eb1e0a6a6e1fdcacc8c6b0aeaf29c16a9029f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-27 22:56:26 +0900

    libexif: Add SOVERSION guard
    %ci:no-build

--- packages/libexif/build.sh
+++ packages/libexif/build.sh
@@ -10,6 +10,22 @@ TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+.\d+.\d+"
 TERMUX_PKG_BREAKS="libexif-dev"
 TERMUX_PKG_REPLACES="libexif-dev"
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=12
+
+	local a
+	for a in LIBEXIF_CURRENT LIBEXIF_AGE; do
+		local _${a}=$(sed -En 's/^'"${a}"'=([0-9]+).*/\1/p' \
+				configure.ac)
+	done
+	local v=$(( _LIBEXIF_CURRENT - _LIBEXIF_AGE ))
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_pre_configure() {
 	autoreconf -fi
 }
