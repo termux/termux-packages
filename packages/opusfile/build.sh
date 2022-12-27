f01401eb5cbfc08bf2e396d48f1d589b4241ee85
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 01:39:14 +0900

    opusfile: Add SOVERSION guard
    %ci:no-build

--- packages/opusfile/build.sh
+++ packages/opusfile/build.sh
@@ -13,6 +13,22 @@ TERMUX_PKG_BREAKS="opusfile-dev"
 TERMUX_PKG_REPLACES="opusfile-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-http"
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=0
+
+	local a
+	for a in LT_CURRENT LT_AGE; do
+		local _${a}=$(sed -En 's/^OP_'"${a}"'=([0-9]+).*/\1/p' \
+				configure.ac)
+	done
+	local v=$(( _LT_CURRENT - _LT_AGE ))
+	if [ ! "${_LT_CURRENT}" ] || [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_pre_configure() {
 	echo "PACKAGE_VERSION=$TERMUX_PKG_VERSION" > package_version
 	./autogen.sh
