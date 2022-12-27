cfa26a558c39dd1b01fd991293cc97d563695c83
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-27 19:18:58 +0900

    age: Bump to 1.1.1

--- packages/age/build.sh
+++ packages/age/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/FiloSottile/age
 TERMUX_PKG_DESCRIPTION="A simple, modern and secure encryption tool with small explicit keys, no config options, and UNIX-style composability"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:1.0.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1:1.1.1
 TERMUX_PKG_SRCURL=https://github.com/FiloSottile/age/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=8d27684f62f9dc74014035e31619e2e07f8b56257b1075560456cbf05ddbcfce
+TERMUX_PKG_SHA256=f1f3dbade631976701cd295aa89308681318d73118f5673cced13f127a91178c
 
 termux_step_make() {
 	termux_setup_golang
