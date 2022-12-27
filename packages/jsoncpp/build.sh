d0b82dc6673dd7b80df8d44a4e5d7ce59d552e9d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 02:30:03 +0900

    jsoncpp: Add SOVERSION guard
    %ci:no-build

--- packages/jsoncpp/build.sh
+++ packages/jsoncpp/build.sh
@@ -17,6 +17,18 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCCACHE_FOUND=OFF
 "
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=25
+
+	local v=$(sed -En 's/^set\(PROJECT_SOVERSION\s+([0-9]+).*/\1/p' \
+			CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
