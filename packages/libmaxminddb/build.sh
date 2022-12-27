c5817400d4f49183cba50668ea180cca436e818f
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 06:37:47 +0900

    libmaxminddb: Add SOVERSION guard
    %ci:no-build

--- packages/libmaxminddb/build.sh
+++ packages/libmaxminddb/build.sh
@@ -11,6 +11,18 @@ TERMUX_PKG_REPLACES="libmaxminddb-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-tests"
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=0.0.7
+
+	local v=$(sed -En 's/^set\(MAXMINDDB_SOVERSION\s+([0-9.]+).*/\1/p' \
+			CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_pre_configure() {
 	./bootstrap
 }
