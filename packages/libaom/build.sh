a0f80b06f91bc964a61bf4d5dfaf84a0a521e4cf
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-27 23:48:37 +0900

    libaom: Refine SOVERSION guard
    %ci:no-build

--- packages/libaom/build.sh
+++ packages/libaom/build.sh
@@ -14,11 +14,15 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	# Not exactly the same as the SOVERSION though.
-	local _MAJOR_VERSION=3
+	local _SOVERSION=3
 
-	case "$TERMUX_PKG_VERSION" in
-		${_MAJOR_VERSION}.*|*:${_MAJOR_VERSION}.* ) ;;
-		* ) termux_error_exit "SOVERSION check failed." ;;
-	esac
+	local a
+	for a in LT_CURRENT LT_AGE; do
+		local _${a}=$(sed -En 's/^set\('"${a}"'\s+([0-9]+).*/\1/p' \
+				CMakeLists.txt)
+	done
+	local v=$(( _LT_CURRENT - _LT_AGE ))
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
 }
