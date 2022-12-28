1aaea57394f1440585a632fa49edf1b7dadad359
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 18:58:26 +0900

    tmux: Bump to 3.3a

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -4,10 +4,9 @@ TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 # Link against libandroid-support for wcwidth(), see https://github.com/termux/termux-packages/issues/224
 TERMUX_PKG_DEPENDS="ncurses, libevent, libandroid-support, libandroid-glob"
-TERMUX_PKG_VERSION=3.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.3a
 TERMUX_PKG_SRCURL="https://github.com/tmux/tmux/archive/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=967044a34cf69197355f18f2f66e7300b29799576f91fbe04200ab71e5ef6913
+TERMUX_PKG_SHA256=f9687493203f86d346791a9327cde9148b9b4be959381b1effc575a9364a043f
 TERMUX_PKG_AUTO_UPDATE=true
 # Set default TERM to screen-256color, see: https://raw.githubusercontent.com/tmux/tmux/3.3/CHANGES
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static --with-TERM=screen-256color"
