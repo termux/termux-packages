492da9a70700a376695b173c33d4319718141f5d
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-28 19:15:04 +0900

    cups: Bump to 2.4.2

--- packages/cups/build.sh
+++ packages/cups/build.sh
@@ -2,13 +2,15 @@ TERMUX_PKG_HOMEPAGE="https://www.cups.org/"
 TERMUX_PKG_DESCRIPTION="Common UNIX Printing System"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.3.6
-TERMUX_PKG_SRCURL=https://github.com/apple/cups/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3a96912fc88f62c5d8ac9b251bf0652f1cbe7e69cde16919103aea39f31a1a84
-TERMUX_PKG_DEPENDS="gnutls, libc++, libcrypt, libiconv, zlib"
+TERMUX_PKG_VERSION=2.4.2
+TERMUX_PKG_SRCURL=https://github.com/OpenPrinting/cups/releases/download/v${TERMUX_PKG_VERSION}/cups-${TERMUX_PKG_VERSION}-source.tar.gz
+TERMUX_PKG_SHA256=f03ccb40b087d1e30940a40e0141dcbba263f39974c20eb9f2521066c9c6c908
+TERMUX_PKG_DEPENDS="libc++, libcrypt, libgnutls, libiconv, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--with-tls=gnutls
+"
 TERMUX_PKG_CONFFILES="
 etc/cups/cups-files.conf
 etc/cups/cupsd.conf
