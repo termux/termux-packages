70db66b3b4c94d53f1de8973db340835dd91afc4
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-29 23:19:57 +0900

    fluidsynth: Bump to 2.3.1

--- packages/fluidsynth/build.sh
+++ packages/fluidsynth/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/FluidSynth/fluidsynth
 TERMUX_PKG_DESCRIPTION="Software synthesizer based on the SoundFont 2 specifications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Yonle <yonle@duck.com>"
-TERMUX_PKG_VERSION=2.3.0
+TERMUX_PKG_VERSION=2.3.1
 TERMUX_PKG_SRCURL=https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1df5a1afb91acf3b945b7fdb89ac0d99877622161d9b5155533da59113eaaa20
-TERMUX_PKG_DEPENDS="glib, libc++, pulseaudio"
+TERMUX_PKG_SHA256=d734e4cf488be763cf123e5976f3154f0094815093eecdf71e0e9ae148431883
+TERMUX_PKG_DEPENDS="dbus, glib, libc++, libsndfile, pulseaudio, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DLIB_INSTALL_DIR=${TERMUX_PREFIX}/lib"
 
