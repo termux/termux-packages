7870e56531d978e9bee5d2b58857164753c7a94e
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 07:04:56 +0900

    photon-rss: Bump to 2022.12.01

--- packages/photon-rss/build.sh
+++ packages/photon-rss/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://git.sr.ht/~ghost08/photon
 TERMUX_PKG_DESCRIPTION="An RSS/Atom reader with the focus on speed, usability and a bit of unix philosophy"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=53f440659b3871d233eddc700ad8f10e2b67b6f2
-TERMUX_PKG_VERSION=2022.11.17
+_COMMIT=02e0faa0a40f4772e996b58ec203337f7c0dd51c
+TERMUX_PKG_VERSION=2022.12.01
 # This repository does not accept ".git" suffix:
 TERMUX_PKG_SRCURL=https://git.sr.ht/~ghost08/photon
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
