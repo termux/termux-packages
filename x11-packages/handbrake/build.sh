4f8b926bce0ea5f2ad9aedfb9946aa32b61d22dd
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-29 23:23:14 +0900

    handbrake: Bump to 1.6.0

--- x11-packages/handbrake/build.sh
+++ x11-packages/handbrake/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="A GPL-licensed, multiplatform, multithreaded video trans
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.5.1
+TERMUX_PKG_VERSION=1.6.0
 TERMUX_PKG_SRCURL=https://github.com/HandBrake/HandBrake/releases/download/${TERMUX_PKG_VERSION}/HandBrake-${TERMUX_PKG_VERSION}-source.tar.bz2
-TERMUX_PKG_SHA256=3999fe06d5309c819799a73a968a8ec3840e7840c2b64af8f5cdb7fd8c9430f0
+TERMUX_PKG_SHA256=7f23c76038b7bf329089d0eb33c14898400fcc0426e310e87dc11e538c103cda
 TERMUX_PKG_DEPENDS="ffmpeg, gdk-pixbuf, gst-plugins-base, gstreamer, gtk3, libass, libbluray, libcairo, libdvdnav, libdvdread, libiconv, libjansson, libjpeg-turbo, libtheora, libvorbis, libx264, libx265, libxml2, pango"
-TERMUX_PKG_BUILD_DEPENDS="liba52, libspeex, libzimg"
+TERMUX_PKG_BUILD_DEPENDS="liba52, libspeex, libzimg, svt-av1"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --force
 --prefix $TERMUX_PREFIX
