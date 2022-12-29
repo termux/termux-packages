9488d6e41109230f417d9abe1c7ed948d5b9b874
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 05:21:28 +0900

    seafile-client: Bump to 8.0.10

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -1,14 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://seafile.com
 TERMUX_PKG_DESCRIPTION="Seafile is a file syncing and sharing software with file encryption and group sharing"
-TERMUX_PKG_LICENSE="Apache-2.0"
+# License: GPL-2.0-with-OpenSSL-exception
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="8.0.9"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=8.0.10
 TERMUX_PKG_SRCURL=https://github.com/haiwen/seafile/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=64bbf99f3640ca28f5e59aca13b292ed8ae3898bb68b1d726cc5492a613f3ddc
+TERMUX_PKG_SHA256=bcfa6576a272486cf30b7fa2207992b01b071bcc5744f0923a0d924c0257b8a5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
-TERMUX_PKG_DEPENDS="libcurl, libevent, libsearpc, libsqlite, libuuid (>> 2.38.1), openssl, python"
+TERMUX_PKG_DEPENDS="glib, libcurl, libevent, libjansson, libsearpc, libsqlite, libuuid (>> 2.38.1), libwebsockets, openssl, python, zlib"
 TERMUX_PKG_BREAKS="seafile-client-dev, ccnet"
 TERMUX_PKG_REPLACES="seafile-client-dev, ccnet"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -21,7 +22,15 @@ termux_step_pre_configure() {
 	./autogen.sh
 	export CPPFLAGS="-I$TERMUX_PKG_SRCDIR/lib $CPPFLAGS"
 	local _PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
-	export PYTHON="python${_PYTHON_VERSION}"
+
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
 }
 
 termux_step_post_configure() {
