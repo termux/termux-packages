bb59aafe502e0fae7393e53b166c4398f5a58e76
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 02:37:41 +0900

    libzim: Add SOVERSION guard
    %ci:no-build

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -8,3 +8,14 @@ TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.
 TERMUX_PKG_SHA256=16ed7f2e6f39a5c15a70609b776657b3152a6f4f91ac1a95f331d4ca79cb89f0
 TERMUX_PKG_DEPENDS="libc++, zstd, libuuid (>> 2.38.1), zlib, libicu, liblzma, libxapian, googletest"
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=8
+
+	local v=$(echo ${TERMUX_PKG_VERSION#*:} | cut -d . -f 1)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
