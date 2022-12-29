5c02b412c18ab054dffe3c5e6ad07ce2ee5e76cf
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 04:14:12 +0900

    libraqm: Add SOVERSION guard
    %ci:no-build

--- packages/libraqm/build.sh
+++ packages/libraqm/build.sh
@@ -7,3 +7,14 @@ TERMUX_PKG_SRCURL=https://github.com/HOST-Oman/libraqm/releases/download/v$TERMU
 TERMUX_PKG_SHA256=9ed6fdf41da6391fc9bf7038662cbe412c330aa6eb22b19704af2258e448107c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, harfbuzz, fribidi"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=0
+
+	local v=$(echo ${TERMUX_PKG_VERSION#*:} | cut -d . -f 1)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
