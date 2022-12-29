19f65dc3ae5922ba54d89dead5db22d985c1c364
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 05:07:14 +0900

    libusbmuxd: Add SOVERSION guard
    %ci:no-build

--- packages/libusbmuxd/build.sh
+++ packages/libusbmuxd/build.sh
@@ -10,10 +10,34 @@ TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libusb, libplist, usbmuxd"
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=6
+
+	local e=$(sed -En 's/LIBUSBMUXD_SO_VERSION="?([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
+				configure.ac)
+	if [ ! "${e}" ] || [ "${_SOVERSION}" != "$(( "${e}" ))" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_configure() {
 	./autogen.sh \
 		--prefix=$TERMUX_PREFIX \
 		--without-preflight \
 		--without-systemd \
 		--host=$TERMUX_HOST_PLATFORM
+}
+
+termux_step_post_massage() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION_GUARD_FILES="lib/libusbmuxd-2.0.so"
+	local f
+	for f in ${_SOVERSION_GUARD_FILES}; do
+		if [ ! -e "${f}" ]; then
+			termux_error_exit "SOVERSION guard check failed."
+		fi
+	done
 }
