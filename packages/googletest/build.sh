7575e68947310549b624a451f80369595b00e9b7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 01:07:52 +0900

    googletest: Add SOVERSION guard
    %ci:no-build

--- packages/googletest/build.sh
+++ packages/googletest/build.sh
@@ -13,3 +13,14 @@ TERMUX_PKG_CONFLICTS="libgtest"
 TERMUX_PKG_REPLACES="libgtest"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=1.12.1
+
+	local v=$(sed -En 's/^set\(GOOGLETEST_VERSION\s+([0-9.]+).*/\1/p' \
+			CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
