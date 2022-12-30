d3fdc3980089c2a7218139db790ec76959799ad7
Tee KOBAYASHI <xtkoba@gmail.com>
2022-12-30 23:23:04 +0900

    recoll: Fix hardcoded paths

--- packages/recoll/build.sh
+++ packages/recoll/build.sh
@@ -3,10 +3,18 @@ TERMUX_PKG_DESCRIPTION="Full-text search for your desktop"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.33.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.lesbonscomptes.com/recoll/recoll-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7df0f8f6c1985981160053d1b69c8353f09816f92b1032f6d4376add0b170af7
-TERMUX_PKG_DEPENDS="aspell, libc++, libiconv, libxapian, libxml2, libxslt, zlib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --disable-userdoc --disable-python-chm --disable-x11mon --disable-qtgui"
+TERMUX_PKG_DEPENDS="aspell, file, libc++, libiconv, libxapian, libxml2, libxslt, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_path_aspellProg=$TERMUX_PREFIX/bin/aspell
+--with-file-command=$TERMUX_PREFIX/bin/file
+--disable-userdoc
+--disable-python-chm
+--disable-x11mon
+--disable-qtgui
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
