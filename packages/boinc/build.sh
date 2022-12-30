009ea407c92fb88e75c80e6a7446f1fbbbf9d5d7
Jia Yuan Lo <jylo06g@gmail.com>
2022-12-30 12:22:20 +0800

    fix(main/boinc): make benchmark LTO resistant

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open-source software for volunteer computing"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.20.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/BOINC/boinc/archive/client_release/${TERMUX_PKG_VERSION:0:4}/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=34f32398ee5981fb3216b103271f0051c807e57c3368eb00654e5bbf89dc5065
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-shmem, libc++, libcurl, openssl, zlib"
