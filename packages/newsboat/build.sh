e5956436cf7f9808e5541c718ce409276c6d7600
Henrik Grimler <grimler@termux.dev>
2023-01-02 14:13:54 +0100

    newsboat: update locale related patch
    To remove issue where feed titles show up as just "UTF-8".  Not sure
    why this line was added in the first place.
    
    Fixes newsboat/newsboat#2299.
    
    Suggested-by: @xtkoba

--- packages/newsboat/build.sh
+++ packages/newsboat/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="RSS/Atom feed reader for the text console"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.30
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://newsboat.org/releases/${TERMUX_PKG_VERSION}/newsboat-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e0ac957487d444cc6c4674e0dc82bbc8129b56a43ecd7ea9fc726c65e3b471d5
 TERMUX_PKG_DEPENDS="json-c, libandroid-glob, libandroid-support, libc++, libcurl, libsqlite, libxml2, ncurses, stfl"
 TERMUX_PKG_BUILD_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_RM_AFTER_INSTALL="share/locale"
+# TERMUX_PKG_RM_AFTER_INSTALL="share/locale"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_bsd_main=no"
 TERMUX_PKG_CONFLICTS=newsbeuter
 TERMUX_PKG_REPLACES=newsbeuter
@@ -21,8 +22,6 @@ termux_step_pre_configure() {
 
 	# Used by newsboat Makefile:
 	export CARGO_BUILD_TARGET=$CARGO_TARGET_NAME
-
-	LDFLAGS+=" -liconv"
 
 	export PKG_CONFIG_ALLOW_CROSS=1
 }
