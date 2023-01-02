9f0a66e1e62017aa8d1b56abfe94d42e1e67652f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-03 07:35:11 +0900

    sdl2: Bump to 2.26.1

--- x11-packages/sdl2/build.sh
+++ x11-packages/sdl2/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.libsdl.org
 TERMUX_PKG_DESCRIPTION="A library for portable low-level access to a video framebuffer, audio output, mouse, and keyboard (version 2)"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.26.0"
+TERMUX_PKG_VERSION=2.26.1
 TERMUX_PKG_SRCURL=https://www.libsdl.org/release/SDL2-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8000d7169febce93c84b6bdf376631f8179132fd69f7015d4dadb8b9c2bdb295
+TERMUX_PKG_SHA256=02537cc7ebd74071631038b237ec4bfbb3f4830ba019e569434da33f42373e04
 TERMUX_PKG_DEPENDS="libx11, libxcursor, libxext, libxfixes, libxi, libxrandr, libxss, pulseaudio"
 TERMUX_PKG_BUILD_DEPENDS="mesa"
 TERMUX_PKG_RECOMMENDS="mesa"
@@ -34,6 +34,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-video-vivante
 --disable-video-cocoa
 --disable-render-metal
+--disable-video-kmsdrm
 --enable-video-opengl
 --disable-video-opengles
 --disable-video-opengles2
