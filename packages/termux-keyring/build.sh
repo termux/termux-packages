576920b9f7b290399e3d9db86a201846538475d9
Maxython <mixython@gmail.com>
2023-01-02 17:42:34 +0300

    termux-keyring: updating the termux-pacman key and configuring the package to work with package managers

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -2,36 +2,43 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux
 TERMUX_PKG_DESCRIPTION="GPG public keys for the official Termux repositories"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.10
+TERMUX_PKG_VERSION=3.11
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_ESSENTIAL=true
 
 termux_step_make_install() {
-	if [ "$TERMUX_PACKAGE_FORMAT" = "debian" ]; then
-		local GPG_DIR="$TERMUX_PREFIX/etc/apt/trusted.gpg.d"
-	else
-		local GPG_DIR="$TERMUX_PREFIX/share/pacman/keyrings"
-	fi
+	local GPG_SHARE_DIR="$TERMUX_PREFIX/share/termux-keyring"
 
-	mkdir -p $GPG_DIR
+	mkdir -p $GPG_SHARE_DIR
 
 	# Maintainer-specific keys.
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/agnostic-apollo.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/mradityaalok.gpg $GPG_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/2096779623.gpg $GPG_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/agnostic-apollo.gpg $GPG_SHARE_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $GPG_SHARE_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg $GPG_SHARE_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg $GPG_SHARE_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/mradityaalok.gpg $GPG_SHARE_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/2096779623.gpg $GPG_SHARE_DIR
 
 	# Key for automatic builds (via CI).
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $GPG_DIR
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $GPG_SHARE_DIR
 
-	# Creation of trusted files
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ]; then
-		install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-pacman.gpg $GPG_DIR
-		echo "998DE27318E867EA976BA877389CEED64573DFCA:4:" > $GPG_DIR/termux-pacman-trusted
-	fi
+	# Key for pacman package manager.
+	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-pacman.gpg $GPG_SHARE_DIR
+
+	for GPG_DIR in "$TERMUX_PREFIX/etc/apt/trusted.gpg.d" "$TERMUX_PREFIX/share/pacman/keyrings"; do
+		mkdir -p $GPG_DIR
+		for GPG_FILE in $GPG_SHARE_DIR/*.gpg; do
+			if [[ "$GPG_DIR" == *"/apt/"* && "$GPG_FILE" == *"termux-pacman.gpg"* ]]; then
+				continue
+			fi
+			ln -s "$GPG_FILE" "$GPG_DIR/$(basename $GPG_FILE)"
+		done
+		# Creation of trusted files
+		if [[ "$GPG_DIR" == *"/pacman/"* ]]; then
+			echo "998DE27318E867EA976BA877389CEED64573DFCA:4:" > $GPG_DIR/termux-pacman-trusted
+		fi
+	done
 }
 
 termux_step_create_debscripts() {
