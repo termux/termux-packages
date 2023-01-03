06db5c1a83eeb41b555be21df86a3ccb97930e47
Henrik Grimler <grimler@termux.dev>
2023-01-03 20:45:37 +0100

    enhance(main/libc++): make package buildable on device
    By downloading android-ndk zip instead of relying on the toolchain
    directory.

--- packages/libc++/build.sh
+++ packages/libc++/build.sh
@@ -2,11 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://libcxx.llvm.org/
 TERMUX_PKG_DESCRIPTION="C++ Standard Library"
 TERMUX_PKG_LICENSE="NCSA"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=$TERMUX_NDK_VERSION
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+# Version should be equal to TERMUX_NDK_{VERSION_NUM,REVISION} in
+# scripts/properties.sh
+TERMUX_PKG_VERSION=25b
+TERMUX_PKG_SRCURL=https://dl.google.com/android/repository/android-ndk-r${TERMUX_PKG_VERSION}-linux.zip
+TERMUX_PKG_SHA256=403ac3e3020dd0db63a848dcaba6ceb2603bf64de90949d5c4361f848e44b005
 TERMUX_PKG_ESSENTIAL=true
-TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
-	cp "$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/${TERMUX_HOST_PLATFORM}/libc++_shared.so" $TERMUX_PREFIX/lib
+	install -m700 -t "$TERMUX_PREFIX"/lib \
+		toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"${TERMUX_HOST_PLATFORM}"/libc++_shared.so
 }
