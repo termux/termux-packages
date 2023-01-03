04d7c5e4bf24607217fbaa27d6ea54399fb98879
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-04 05:29:33 +0900

    photon-rss, ratt: Restore termux_step_post_get_source
    as a follow-up of commit 3ec82277d7773e29aac53d525fdf519f74246bd4.
    
    A specific commit needs to be checked out, instead of HEAD. And this is
    nothing to do with the changes in `termux_git_clone_src`, ever.
    
    %ci:no-build

--- packages/photon-rss/build.sh
+++ packages/photon-rss/build.sh
@@ -9,6 +9,18 @@ TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
 
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
+		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+		echo " is different from what is expected to be: \"$version\""
+		return 1
+	fi
+}
+
 termux_step_pre_configure() {
 	termux_setup_golang
 
