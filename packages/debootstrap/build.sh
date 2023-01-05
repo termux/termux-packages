9c096e24936cbe58f7de68ea0f5a53f9906f436f
Leonid Pliushch <leonid.pliushch@gmail.com>
2023-01-05 11:19:10 +0200

    fix(main/debootstrap): more improvements
    AR utility from LLVM should work fine, allow use of binutils-is-llvm
    package instead of binutils.
    
    Disable steps like setup_proc which may lead to (non-fatal) errors shown
    during bootstrap.

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -4,10 +4,10 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="debian/copyright"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.128+nmu2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=528523228d93a31c9e0cd4eb932977ea8ceec2bfbf8db1103bec2397cc7434fa
-TERMUX_PKG_DEPENDS="binutils, perl, proot, wget"
+TERMUX_PKG_DEPENDS="binutils | binutils-is-llvm, perl, proot, wget"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
