738e213005e5b16cf18ea900d428212bffc988f3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-06 08:00:35 +0900

    libxfce4ui: Bump to 4.18.1

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=4.18
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4ui/${_MAJOR_VERSION}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=532247c4387c17bb9ef94a73147039b8d013c3131c95cdbd2fa85fbcc848d06b
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libice, libsm, libx11, libxfce4util, pango, startup-notification, xfconf"
+TERMUX_PKG_SHA256=6ebf7b30021b7bce75824b4c6dc2571ffb3dcd6a74468e823a74719313e5e52e
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libice, libsm, libx11, libxfce4util, pango, startup-notification, xfconf, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_RECOMMENDS="hicolor-icon-theme"
 TERMUX_PKG_DISABLE_GIR=false
@@ -14,6 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-vendor-info=Termux
 --enable-introspection=yes
 --enable-vala=no
+--enable-gladeui2=no
 --enable-gtk-doc-html=no
 "
 
