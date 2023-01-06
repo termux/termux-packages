67bf1603554a9072b4ed43b8d1ad517fe32d25fb
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-01-06 23:30:07 +0800

    new package: qt5-qtwebengine

---
+++ x11-packages/qt5-qtwebengine/ffmpeg-headers/Chromium/linux-noasm/ia32/libavcodec/parser_list.c
@@ -0,0 +1,9 @@
+static const AVCodecParser * const parser_list[] = {
+    &ff_flac_parser,
+    &ff_mpegaudio_parser,
+    &ff_opus_parser,
+    &ff_vorbis_parser,
+    &ff_vp3_parser,
+    &ff_vp8_parser,
+    &ff_vp9_parser,
+    NULL };
