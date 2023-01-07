122ce950276c6890720fb1617d8d71d2a5e5b74a
Leonid Pliushch <leonid.pliushch@gmail.com>
2023-01-07 15:01:36 +0200

    fix(main/clash): depend on resolv-conf
    Based on strace logs.
    
    26879 openat(AT_FDCWD, "/data/data/com.termux/files/usr/etc/nsswitch.conf", O_RDONLY|O_CLOEXEC <unfinished ...>
    26879 <... openat resumed>)             = -1 ENOENT (No such file or directory)
    26879 openat(AT_FDCWD, "/data/data/com.termux/files/usr/etc/resolv.conf", O_RDONLY|O_CLOEXEC <unfinished ...>
    26879 <... openat resumed>)             = -1 ENOENT (No such file or directory)
    
    Related to https://github.com/termux/termux-packages/issues/14332.
    
    Even though I can't confirm whether absent resolv.conf may lead to DNS
    errors when using this package, a dependency will be assigned anyway.

--- packages/clash/build.sh
+++ packages/clash/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="A rule-based tunnel in Go"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Philipp Schmitt <philipp@schmitt.co>"
 TERMUX_PKG_VERSION="1.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/Dreamacro/clash/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=9b8f28c2adf378e4da5b139dd72c3e13bf19394e2555080832dc47c64fbcdb9a
+TERMUX_PKG_DEPENDS="resolv-conf"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
