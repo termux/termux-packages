c9019b58dec276f6c8284fa6eccd07f403fa3fe8
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-08 06:24:15 +0900

    softether-vpn: Depend on resolv-conf

--- packages/softether-vpn/build.sh
+++ packages/softether-vpn/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="An open-source cross-platform multi-protocol VPN program
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=(5.02.5180)
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_VERSION+=(1.0.18)
 TERMUX_PKG_SRCURL=(https://github.com/SoftEtherVPN/SoftEtherVPN/releases/download/${TERMUX_PKG_VERSION}/SoftEtherVPN-${TERMUX_PKG_VERSION}.tar.xz
                    https://github.com/jedisct1/libsodium/archive/${TERMUX_PKG_VERSION[1]}-RELEASE.tar.gz)
 TERMUX_PKG_SHA256=(b5649a8ea3cc6477325e09e2248ef708d434ee3b2251eb8764bcfc15fb1de456
                    b7292dd1da67a049c8e78415cd498ec138d194cfdb302e716b08d26b80fecc10)
-TERMUX_PKG_DEPENDS="libiconv, libsodium, ncurses, openssl, readline, zlib"
+TERMUX_PKG_DEPENDS="libiconv, libsodium, ncurses, openssl, readline, resolv-conf, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DHAS_SSE2=OFF
