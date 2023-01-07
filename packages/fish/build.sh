1cd99a8b6ec0bc1e65ebee18fad9fb6d7b1bddf2
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-08 08:23:53 +0900

    fish: Bump to 3.6.0

--- packages/fish/build.sh
+++ packages/fish/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://fishshell.com/
 TERMUX_PKG_DESCRIPTION="The user-friendly command line shell"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.5.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.6.0
 TERMUX_PKG_SRCURL=https://github.com/fish-shell/fish-shell/releases/download/$TERMUX_PKG_VERSION/fish-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a6d45b3dc5a45dd31772e7f8dfdfecabc063986e8f67d60bd7ca60cc81db6928
+TERMUX_PKG_SHA256=97044d57773ee7ca15634f693d917ed1c3dc0fa7fde1017f1626d60b83ea6181
 TERMUX_PKG_AUTO_UPDATE=true
 # fish calls 'tput' from ncurses-utils, at least when cancelling (Ctrl+C) a command line.
 # man is needed since fish calls apropos during command completion.
