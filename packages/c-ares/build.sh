0d505189ce8a523e316db46557970551444fee49
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-08 06:17:51 +0900

    c-ares: Depend on resolv-conf

--- packages/c-ares/build.sh
+++ packages/c-ares/build.sh
@@ -3,10 +3,12 @@ TERMUX_PKG_DESCRIPTION="Library for asynchronous DNS requests (including name re
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.18.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/c-ares/c-ares/archive/cares-${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=414872549eec4e221b576693fdc9c9bce44ff794d0f1f06f2515b56a7f6ec9c9
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+.\d+.\d+"
+TERMUX_PKG_DEPENDS="resolv-conf"
 TERMUX_PKG_BREAKS="c-ares-dev"
 TERMUX_PKG_REPLACES="c-ares-dev"
 # Build with cmake to install cmake/c-ares/*.cmake files:
