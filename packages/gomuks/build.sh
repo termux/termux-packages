335cb30a684b3e8c6b2a24eebcf1d82f02dfc3c4
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-08 05:47:25 +0900

    gomuks: Depend on resolv-conf

--- packages/gomuks/build.sh
+++ packages/gomuks/build.sh
@@ -3,9 +3,11 @@ TERMUX_PKG_DESCRIPTION="A terminal Matrix client written in Go"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tulir/gomuks/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0710a63cc3ec9a4f525510497ba64aa94170498eb536411d089871f336d99ab4
-TERMUX_PKG_DEPENDS="libolm"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libolm, resolv-conf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
