a9b00bb7d09769fa2d7da513a4b968c666c506b1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-08 17:57:59 +0900

    i3: Bump to 4.22

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://i3wm.org/
 TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.21.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.22
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=edfd781285c654a05dc6db272ce0300c4428e6fbd875d99964e57de25b41bc1e
+TERMUX_PKG_SHA256=28639911e59d95639f092642a982f5e1dea592250c2b5ce98eda100833513e4b
 TERMUX_PKG_DEPENDS="glib, libandroid-glob, libandroid-wordexp, libcairo, libev, libiconv, libxcb, libxkbcommon, pango, pcre2, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
 TERMUX_PKG_RECOMMENDS="i3status"
 
