a4daf872e64af81f3ba65d62d239b74b55ae9be8
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-08 15:12:22 +0900

    recoll: Bump to 1.34.0

--- packages/recoll/build.sh
+++ packages/recoll/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.lesbonscomptes.com/recoll/index.html
 TERMUX_PKG_DESCRIPTION="Full-text search for your desktop"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.33.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.34.0
 TERMUX_PKG_SRCURL=https://www.lesbonscomptes.com/recoll/recoll-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7df0f8f6c1985981160053d1b69c8353f09816f92b1032f6d4376add0b170af7
+TERMUX_PKG_SHA256=7baeaac2c79dcbff6d866f986c2d538603f25378dd71862f2cb7e775c6594668
 TERMUX_PKG_DEPENDS="aspell, file, libc++, libiconv, libxapian, libxml2, libxslt, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_aspellProg=$TERMUX_PREFIX/bin/aspell
 --with-file-command=$TERMUX_PREFIX/bin/file
 --disable-userdoc
 --disable-python-chm
+--disable-python-aspell
 --disable-x11mon
 --disable-qtgui
 "
@@ -36,4 +36,12 @@ termux_step_pre_configure() {
 	sed "s|@PYTHON_VERSION@|${_PYTHON_VERSION}|g" \
 		$TERMUX_PKG_BUILDER_DIR/python-recoll-setup.py.in.diff \
 		| patch --silent -p1
+}
+
+termux_step_post_massage() {
+	# Regression test for https://github.com/termux/termux-packages/issues/14293
+	if ! readelf -d bin/recollindex | grep -E -q \
+		'\(RUNPATH\).*(\[|:)'"${TERMUX_PREFIX//./\\.}"'/lib/recoll(:|\])'; then
+		termux_error_exit "RUNPATH for recollindex is not properly set."
+	fi
 }
