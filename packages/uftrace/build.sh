bab797febb3de7e0bd710199b6700ddf3d0fc321
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-10 03:46:04 +0900

    uftrace: Bump to 0.13

--- packages/uftrace/build.sh
+++ packages/uftrace/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://uftrace.github.io/slide
 TERMUX_PKG_DESCRIPTION="Function (graph) tracer for user-space"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.12"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.13
 TERMUX_PKG_SRCURL=https://github.com/namhyung/uftrace/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2aad01f27d4f18717b681824c7a28ac3e1efd5e7bbed3ec888a3ea5af60e3700
+TERMUX_PKG_SHA256=cffae82c68446c20cc3c7e87e71e57498805767a0d4085b4846f3c49f9e472d9
 # Hardcoded libpython${_PYTHON_VERSION}.so is dlopen(3)ed by uftrace.
 # Please revbump and rebuild when bumping _PYTHON_VERSION, where
 # _PYTHON_VERSION == _MAJOR_VERSION in build.sh of python package.
-TERMUX_PKG_DEPENDS="capstone, libandroid-glob, libandroid-spawn, libc++, libdw, libelf, libluajit, ncurses, python"
+# libandroid-{execinfo,spawn} are dlopen(3)ed.
+TERMUX_PKG_DEPENDS="capstone, libandroid-execinfo, libandroid-glob, libandroid-spawn, libc++, libdw, libelf, libluajit, ncurses, python"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
