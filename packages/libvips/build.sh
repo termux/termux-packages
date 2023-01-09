0950f9f51a2276379f85032e863dadf3053cf3d8
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-10 03:54:19 +0900

    libvips: Bump to 8.14.1

--- packages/libvips/build.sh
+++ packages/libvips/build.sh
@@ -2,20 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://libvips.github.io/libvips/
 TERMUX_PKG_DESCRIPTION="A fast image processing library with low memory needs"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Thibault Meyer <meyer.thibault@gmail.com>"
-TERMUX_PKG_VERSION="8.13.3"
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/libvips/libvips/releases/download/v${TERMUX_PKG_VERSION}/vips-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4eff5cdc8dbe1a05a926290a99014e20ba386f5dcca38d9774bef61413435d4c
+TERMUX_PKG_VERSION=8.14.1
+TERMUX_PKG_SRCURL=https://github.com/libvips/libvips/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=a4a6b282216f7522b42ebb5cd64cfd82a0d1a632033e9c0502f021e945fed641
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="cgif, fftw, fontconfig, freetype, glib, harfbuzz, imagemagick, imath, libc++, libcairo, libexif, libexpat, libheif, libimagequant, libjpeg-turbo, libjxl, libpng, librsvg, libtiff, libwebp, littlecms, openexr, openjpeg, pango, poppler"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_DEPENDS="cgif, fftw, fontconfig, glib, imagemagick, libc++, libcairo, libexif, libexpat, libheif, libimagequant, libjpeg-turbo, libjxl, libpng, librsvg, libtiff, libwebp, littlecms, openexr, openjpeg, pango, poppler, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
 TERMUX_PKG_DISABLE_GIR=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-introspection=yes
+-Dintrospection=true
+-Dvapi=true
+-Dgsf=disabled
+-Dorc=disabled
 "
 
 termux_step_pre_configure() {
 	termux_setup_gir
-
-	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
