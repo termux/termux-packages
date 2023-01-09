254ea7ffc4862f28ad9bece5c6b61e559e96aed9
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-09 14:54:19 +0900

    lyx: Bump to 2.3.7p1

--- x11-packages/lyx/build.sh
+++ x11-packages/lyx/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://www.lyx.org
 TERMUX_PKG_DESCRIPTION="WYSIWYM (What You See Is What You Mean) Document Processor"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=2.3.6.1
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL="https://ftp.lip6.fr/pub/lyx/stable/2.3.x/lyx-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=c6bed2633419898e01a7cc350310d7d934bf591cab7259fc2581aa4c00eafa78
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtsvg, zlib, imagemagick, ghostscript, texlive-bin, hunspell"
+_VERSION=2.3.7-1
+TERMUX_PKG_VERSION=${_VERSION/-/p}
+TERMUX_PKG_SRCURL="https://ftp.lip6.fr/pub/lyx/stable/$(echo $_VERSION | cut -d . -f 1-2).x/lyx-${_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=39be8864fb86b34e88310e70fb80e5e9e296601f0856cf161aa094171718d8ed
+TERMUX_PKG_DEPENDS="ghostscript, hunspell, imagemagick, libandroid-execinfo, libc++, libxcb, lyx-data, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, texlive-bin, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-build-type=rel
@@ -17,8 +17,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/lyx/examples"
 
-
 termux_step_pre_configure() {
-    # This is to allow the build script find the `moc` on cross-build host
-    export PATH+=":${TERMUX_PREFIX}/opt/qt/cross/bin"
+	LDFLAGS+=" -landroid-execinfo"
+
+	# This is to allow the build script find the `moc` on cross-build host
+	export PATH+=":${TERMUX_PREFIX}/opt/qt/cross/bin"
 }
