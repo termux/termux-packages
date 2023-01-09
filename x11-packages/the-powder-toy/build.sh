0b8e501b3ac258bc9577e4322e855cf468e1ab88
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-09 20:06:37 +0900

    the-powder-toy: Bump to 97.0.352

--- x11-packages/the-powder-toy/build.sh
+++ x11-packages/the-powder-toy/build.sh
@@ -2,12 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://powdertoy.co.uk/
 TERMUX_PKG_DESCRIPTION="The Powder Toy is a free physics sandbox game"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=96.2.350
+TERMUX_PKG_VERSION=97.0.352
 TERMUX_PKG_SRCURL=https://github.com/ThePowderToy/The-Powder-Toy/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d95cbadee22632687661e8fc488bd64405d81c0dca737e16420f26e93ea5bf58
-TERMUX_PKG_DEPENDS="fftw, libc++, libcurl, libluajit, sdl2, zlib"
+TERMUX_PKG_SHA256=3ab27e1b9a84db1da7342e61232ad5be981ca1ddf001c4924fd08b61cc8d287a
+TERMUX_PKG_DEPENDS="fftw, jsoncpp, libbz2, libc++, libcurl, libluajit, libpng, sdl2"
 TERMUX_PKG_GROUPS="games"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dworkaround_elusive_bzip2_lib_dir=$TERMUX_PREFIX/lib
+-Dworkaround_elusive_bzip2_include_dir=$TERMUX_PREFIX/include
 -Db_pie=true
 "
 
