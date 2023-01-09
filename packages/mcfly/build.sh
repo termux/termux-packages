8502c55f7b98974479bc100ab7250871f02aba11
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-09 18:19:07 +0900

    mcfly: Bump to 0.7.1

--- packages/mcfly/build.sh
+++ packages/mcfly/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cantino/mcfly
 TERMUX_PKG_DESCRIPTION="Replaces your default ctrl-r shell history search with an intelligent search engine"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.6.1"
+TERMUX_PKG_VERSION=0.7.1
 TERMUX_PKG_SRCURL=https://github.com/cantino/mcfly/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e2eebca8f66ec99ff8582886a10e8dfa1a250329ac02c27855698c8d4a33a3f2
+TERMUX_PKG_SHA256=75bb4e64bcfe339181baadb8adc1ec47001d3e0fb5d20c6e13ec3e071076d4dd
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -27,4 +27,18 @@ termux_step_make() {
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mcfly
 	install -Dm600 -t $TERMUX_PREFIX/share/mcfly mcfly.{fi,z}sh
+}
+
+termux_step_create_debscripts() {
+	cat <<-EOF > ./postinst
+		#!$TERMUX_PREFIX/bin/sh
+		echo
+		echo "********"
+		echo "McFly does not support Bash on Android."
+		echo
+		echo "https://github.com/termux/termux-packages/issues/8722"
+		echo "https://github.com/cantino/mcfly/issues/215"
+		echo "********"
+		echo
+	EOF
 }
