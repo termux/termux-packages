b5a0e75ccb7dfa278d83169d6dcfd1869967fceb
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-11 08:44:41 +0900

    edbrowse: Bump to 3.8.6

--- packages/edbrowse/build.sh
+++ packages/edbrowse/build.sh
@@ -1,15 +1,20 @@
 TERMUX_PKG_HOMEPAGE=https://edbrowse.org/
 TERMUX_PKG_DESCRIPTION="Line based editor, browser, and mail client"
-TERMUX_PKG_LICENSE="GPL-2.0, MIT, MPL-2.0"
-TERMUX_PKG_LICENSE_FILE="COPYING"
+# License: GPL-2.0-or-later
+TERMUX_PKG_LICENSE="GPL-2.0, MIT"
+TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE.quickjs"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.8.5
+TERMUX_PKG_VERSION=3.8.6
 TERMUX_PKG_SRCURL=https://github.com/CMB/edbrowse/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4fb6ce7962560552b370c55467dc65f6d751de06ba373ac7c4678625daed37b4
-TERMUX_PKG_DEPENDS="libandroid-glob, libcurl, pcre2, readline, unixodbc"
+TERMUX_PKG_SHA256=85ce43c2832e1e79ea24e23c7726757080ef966d7c2c387f8aa9be108f36bf26
+TERMUX_PKG_DEPENDS="libandroid-glob, libcurl, openssl, pcre2, readline, unixodbc"
 TERMUX_PKG_BUILD_DEPENDS="quickjs"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="-C src
 PREFIX=$TERMUX_PREFIX
 QUICKJS_DIR=$TERMUX_PREFIX/include/quickjs
 "
+
+termux_step_post_get_source() {
+	cp $TERMUX_PKG_BUILDER_DIR/LICENSE.quickjs ./
+}
