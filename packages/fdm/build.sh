4f50ef5a914081e6993ab32381a627992dcd3406
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-11 08:47:11 +0900

    fdm: Bump to 2.2

--- packages/fdm/build.sh
+++ packages/fdm/build.sh
@@ -3,16 +3,15 @@ TERMUX_PKG_DESCRIPTION="A program designed to fetch mail from POP3 or IMAP serve
 TERMUX_PKG_LICENSE="ISC, BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.2
 TERMUX_PKG_SRCURL=https://github.com/nicm/fdm/releases/download/${TERMUX_PKG_VERSION}/fdm-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=858df2e6ef0836d940e8b8cc4fec333770fa9c97ba0f2485a9e63ed18b2cadff
-TERMUX_PKG_DEPENDS="libandroid-glob, libtdb, openssl, pcre, zlib"
+TERMUX_PKG_SHA256=53aad117829834e21c1b9bf20496a1aa1c0e0fb98fe7735e1e73314266fb6c16
+TERMUX_PKG_DEPENDS="libandroid-glob, libtdb, openssl, pcre2, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --sysconfdir=$TERMUX_PREFIX/etc
 --localstatedir=$TERMUX_PREFIX/var
 --disable-static
---enable-pcre
+--enable-pcre2
 "
 
 termux_step_pre_configure() {
