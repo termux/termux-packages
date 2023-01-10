f71dab945be7f4ce9adf06b763906d22a2309181
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-11 08:07:35 +0900

    lesspipe: Bump to 2.07

--- packages/lesspipe/build.sh
+++ packages/lesspipe/build.sh
@@ -2,19 +2,19 @@ TERMUX_PKG_HOMEPAGE=http://www-zeuthen.desy.de/~friebel/unix/lesspipe.html
 TERMUX_PKG_DESCRIPTION="An input filter for the pager less"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.06"
+TERMUX_PKG_VERSION=2.07
 TERMUX_PKG_SRCURL=https://github.com/wofr06/lesspipe/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f46c7b0b06f951613535a480d22ba7e8563f4f08d3d3f8e370047df122cb1637
+TERMUX_PKG_SHA256=b6a591c053057c3968d0d1fbd32e4a0a8026cd5c27e861023e3542772eda1cba
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="less"
+TERMUX_PKG_BUILD_DEPENDS="bash-completion"
 TERMUX_PKG_SUGGESTS="imagemagick, p7zip, unrar, unzip"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
 	./configure \
-		--prefix="$TERMUX_PREFIX" \
-		--yes
+		--prefix="$TERMUX_PREFIX"
 }
 
 termux_step_post_make_install() {
