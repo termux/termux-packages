a7007c3c71d29914fa5e30809e8eff22c417c8d7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-11 18:09:47 +0900

    tome2: Bump to 2022.12.27

--- packages/tome2/build.sh
+++ packages/tome2/build.sh
@@ -1,24 +1,23 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/tome2/tome2
 TERMUX_PKG_DESCRIPTION="An open world roguelike adventure set in middle earth"
-TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE="non-free"
 TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=1e26568b084104edd2a696e86118a3e71c78d61e
+TERMUX_PKG_VERSION=2022.12.27
 TERMUX_PKG_SRCURL=git+https://github.com/tome2/tome2
-TERMUX_PKG_VERSION=2022.02.24
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_GIT_BRANCH=master
-_COMMIT=d25bdae09bffea46ac54e51b99b2c166d9be7db8
-TERMUX_PKG_DEPENDS="libc++, ncurses, boost"
+TERMUX_PKG_DEPENDS="boost, libc++, libx11, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DSYSTEM_INSTALL=YES"
 
 termux_step_post_get_source() {
-	git fetch --unshallow || true
+	git fetch --unshallow
 	git checkout $_COMMIT
 
-	local version="$(git rev-parse HEAD)"
-	if [ "$version" != "$_COMMIT" ]; then
-		echo -n "ERROR: Failed to check out the specified version; \"$_COMMIT\""
-		echo " differs from the current HEAD: \"$version\""
+	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
+	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
+		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
+		echo " is different from what is expected to be: \"$version\""
 		return 1
 	fi
 }
