a72ab864fa7901cb3a21e20fa148a9ac3fb56f46
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-12 06:02:41 +0900

    liblzma: Bump to 5.4.1

--- packages/liblzma/build.sh
+++ packages/liblzma/build.sh
@@ -3,9 +3,26 @@ TERMUX_PKG_DESCRIPTION="XZ-format compression library"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0, GPL-3.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.GPLv2, COPYING.GPLv3, COPYING.LGPLv2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.4.0
+TERMUX_PKG_VERSION=5.4.1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/lzmautils/xz-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7471ef5991f690268a8f2be019acec2e0564b7b233ca40035f339fe9a07f830b
+TERMUX_PKG_SHA256=e4b0f81582efa155ccf27bb88275254a429d44968e488fc94b806f2a61cd3e22
 TERMUX_PKG_BREAKS="liblzma-dev"
 TERMUX_PKG_REPLACES="liblzma-dev"
 TERMUX_PKG_ESSENTIAL=true
+
+termux_step_post_massage() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION_GUARD_FILES="lib/liblzma.so.5"
+	local f
+	for f in ${_SOVERSION_GUARD_FILES}; do
+		if [ ! -e "${f}" ]; then
+			termux_error_exit "SOVERSION guard check failed."
+		fi
+	done
+
+	# Check if SONAME is properly set:
+	if ! readelf -d lib/liblzma.so | grep -q '(SONAME).*\[liblzma\.so\.'; then
+		termux_error_exit "SONAME of liblzma.so is not properly set."
+	fi
+}
