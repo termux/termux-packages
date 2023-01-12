1661bbc4f4db3d2fe132b92aca05be81255f81a9
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-13 05:25:46 +0900

    unbound: Bump to 1.17.1

--- packages/unbound/build.sh
+++ packages/unbound/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://unbound.net/
 TERMUX_PKG_DESCRIPTION="A validating, recursive, caching DNS resolver"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.17.1
 TERMUX_PKG_SRCURL=https://nlnetlabs.nl/downloads/unbound/unbound-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=dcbc95d7891d9f910c66e4edc9f1f2fde4dea2eec18e3af9f75aed44a02f1341
+TERMUX_PKG_SHA256=ee4085cecce12584e600f3d814a28fa822dfaacec1f94c84bfd67f8a5571a5f4
 TERMUX_PKG_DEPENDS="libevent, libexpat, libnghttp2, openssl, resolv-conf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
