bdc6608c134f92bd76c0decd07f36cabcf97203d
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-13 06:05:46 +0900

    iptables: Bump to 1.8.9

--- root-packages/iptables/build.sh
+++ root-packages/iptables/build.sh
@@ -2,14 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://www.netfilter.org/projects/iptables
 TERMUX_PKG_DESCRIPTION="Program used to configure the Linux 2.4 and later kernel packet filtering ruleset"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.8.8
-TERMUX_PKG_SRCURL=https://www.netfilter.org/projects/iptables/files/iptables-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=71c75889dc710676631553eb1511da0177bbaaf1b551265b912d236c3f51859f
+TERMUX_PKG_VERSION=1.8.9
+TERMUX_PKG_SRCURL=https://www.netfilter.org/projects/iptables/files/iptables-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=ef6639a43be8325a4f8ea68123ffac236cb696e8c78501b64e8106afb008c87f
 TERMUX_PKG_DEPENDS="libmnl, libnftnl, libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-xt-lock-name=$TERMUX_PREFIX/var/run/xtables.lock
 "
+
+termux_step_post_get_source() {
+	cp $TERMUX_PKG_BUILDER_DIR/syslog-names.h ./extensions/
+}
 
 termux_step_pre_configure() {
 	export CFLAGS+=" -Dindex=strchr -Drindex=strrchr -D__STDC_FORMAT_MACROS=1"
