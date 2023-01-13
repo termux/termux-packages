6deaddd764b37e773f8fb44de86426e671ced863
Yaksh Bariya <yakshbari4@gmail.com>
2023-01-13 20:00:33 +0530

    bump(main/httping): v2.9
    Also change homepage url to GitHub repository, since the website seems
    to be quite out of date.
    
    Link to GitHub repository was found by searching for httping repository
    on the GitHub username mentioned on the website

--- packages/httping/build.sh
+++ packages/httping/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://www.vanheusden.com/httping/
+TERMUX_PKG_HOMEPAGE=https://github.com/folkertvanheusden/HTTPing
 TERMUX_PKG_DESCRIPTION="ping-like program for http-requests"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.5
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://fossies.org/linux/www/httping-${TERMUX_PKG_VERSION}.tgz
-TERMUX_PKG_SHA256=3e895a0a6d7bd79de25a255a1376d4da88eb09c34efdd0476ab5a907e75bfaf8
+TERMUX_PKG_VERSION=2.9
+TERMUX_PKG_SRCURL=https://github.com/folkertvanheusden/HTTPing/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=37da3c89b917611d2ff81e2f6c9e9de39d160ef0ca2cb6ffec0bebcb9b45ef5d
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="fftw, libandroid-support, ncurses, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-fftw3 --with-ncurses --with-openssl"
