termux/termux-packages

commit 849112f9e7681beb9adfee0f75247422ff67ee42

scripts(massage): remove symbol loop in undefined syms check
Instead create a (rather large) grep pattern, and call egrep only
once.

scripts/build/termux_step_massage.sh
@@ -84,24 +84,22 @@ termux_step_massage() {
 		termux_error_exit "Package contains hard links: $HARDLINKS"
 	fi
 
-	# Check so that package is not affected by https://github.com/android/ndk/issues/1614
-	SYMBOLS="$(llvm-readelf -s $($TERMUX_HOST_PLATFORM-clang -print-libgcc-file-name) | grep "FUNC    GLOBAL HIDDEN" | awk '{print $8}')"
-	# Also check for unresolved symbols defined in libandroid-* (#9944)
-	SYMBOLS+=" $(echo libandroid_{sem_{open,close,unlink},shm{ctl,get,at,dt}})"
-	LIBRARIES=""
+	# Check so that package is not affected by
+	# https://github.com/android/ndk/issues/1614, or
+	# https://github.com/termux/termux-packages/issues/9944
 	if [ -d "lib" ]; then
-		LIBRARIES="$(find lib -name "*.so")"
-	fi
-	for lib in $LIBRARIES; do
-		for sym in $SYMBOLS; do
-			if ! llvm-readelf -h $lib &> /dev/null; then
-				continue
+		SYMBOLS="$(llvm-readelf -s $($TERMUX_HOST_PLATFORM-clang -print-libgcc-file-name) | grep "FUNC    GLOBAL HIDDEN" | awk '{print $8}')"
+		SYMBOLS+=" $(echo libandroid_{sem_{open,close,unlink},shm{ctl,get,at,dt}})"
+		grep_pattern="$(create_grep_pattern $SYMBOLS)"
+		for lib in "$(find lib -name "*.so")"; do
+			if ! llvm-readelf -h "$lib" &> /dev/null; then
+				return
 			fi
-			if llvm-readelf -s $lib | egrep 'NOTYPE[[:space:]]+GLOBAL[[:space:]]+DEFAULT[[:space:]]+UND[[:space:]]+'$sym'$' &> /dev/null; then
-				termux_error_exit "$lib contains undefined symbol $sym"
+			if llvm-readelf -s "$lib" | egrep "${grep_pattern}" &> /dev/null; then
+				termux_error_exit "$lib contains undefined symbols:\n$(llvm-readelf -s "$lib" | egrep "${grep_pattern}")"
 			fi
 		done
-	done
+	fi
 
 	if [ "$TERMUX_PACKAGE_FORMAT" = "debian" ]; then
 		termux_create_debian_subpackages
@@ -116,4 +114,14 @@ termux_step_massage() {
 
 	# .. remove empty directories (NOTE: keep this last):
 	find . -type d -empty -delete
+}
+
+# Local function called by termux_step_massage
+create_grep_pattern() {
+	symbol_type='NOTYPE[[:space:]]+GLOBAL[[:space:]]+DEFAULT[[:space:]]+UND[[:space:]]+'
+	echo -n "$symbol_type$1"'$'
+	shift 1
+	for arg in "$@"; do
+		echo -n "\|$symbol_type$arg"'$'
+	done
 }

commit 78715de5beb6cefb93081db0be465a38093b7287

termux_step_massage: Fix logic for undefined symbols
The pattern `for lib in "$(find [...])"` does not work if `find` matches
more than one file. Double quotes around `$(...)` must be removed.

This bug was introduced in 849112f9e7681beb9adfee0f75247422ff67ee42, and
the check did not work correctly from then on, until now.

scripts/build/termux_step_massage.sh
@@ -109,7 +109,7 @@ termux_step_massage() {
 		SYMBOLS+=" $(echo libandroid_{sem_{open,close,unlink},shm{ctl,get,at,dt}})"
 		SYMBOLS+=" $(echo backtrace{,_symbols{,_fd}})"
 		grep_pattern="$(create_grep_pattern $SYMBOLS)"
-		for lib in "$(find lib -name "*.so")"; do
+		for lib in $(find lib -name "*.so"); do
 			if ! $READELF -h "$lib" &> /dev/null; then
 				continue
 			fi