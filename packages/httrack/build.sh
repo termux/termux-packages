2c4ec6bb38e8c1e594bbe0a93a7ce089a11c45fb
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-14 19:23:19 +0900

    httrack: Bump to 3.49.3

--- packages/httrack/build.sh
+++ packages/httrack/build.sh
@@ -2,12 +2,19 @@ TERMUX_PKG_HOMEPAGE=http://www.httrack.com
 TERMUX_PKG_DESCRIPTION="It allows you to download a World Wide Web site from the Internet"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.49.2
-TERMUX_PKG_REVISION=10
-TERMUX_PKG_SRCURL=http://mirror.httrack.com/historical/httrack-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=3477a0e5568e241c63c9899accbfcdb6aadef2812fcce0173688567b4c7d4025
-TERMUX_PKG_DEPENDS="libiconv, openssl, zlib"
+TERMUX_PKG_VERSION=3.49.3
+TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/h/httrack/httrack_${TERMUX_PKG_VERSION}.orig.tar.gz
+TERMUX_PKG_SHA256=fc8b72e2b5a08e564a2abfd42432e63f7ed564e67f8a08693605eaa20878a2f5
+TERMUX_PKG_DEPENDS="httrack-data, libandroid-execinfo, libiconv, openssl, zlib"
 TERMUX_PKG_BREAKS="httrack-dev"
 TERMUX_PKG_REPLACES="httrack-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-zlib=$TERMUX_PREFIX LIBS=-liconv"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--docdir=$TERMUX_PREFIX/share/httrack/html
+--with-zlib=$TERMUX_PREFIX
+LIBS=-liconv
+"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_configure() {
+	make clean
+}
