8ab6943bcabd846489d89183e392dd56c6439c90
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-14 10:13:40 +0900

    libwebp: Bump to 1.3.0

--- packages/libwebp/build.sh
+++ packages/libwebp/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/webmproject/libwebp
 TERMUX_PKG_DESCRIPTION="Library to encode and decode images in WebP format"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/webmproject/libwebp/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=dfe7bff3390cd4958da11e760b65318f0a48c32913e4d5bc5e8d55abaaa2d32e
+TERMUX_PKG_SHA256=dc9860d3fe06013266c237959e1416b71c63b36f343aae1d65ea9c94832630e1
 TERMUX_PKG_DEPENDS="giflib, libjpeg-turbo, libpng, libtiff"
 TERMUX_PKG_BREAKS="libwebp-dev"
 TERMUX_PKG_REPLACES="libwebp-dev"
@@ -22,6 +21,18 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-wic
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=7
+
+	local e=$(sed -En 's/^libwebp_la_LDFLAGS\s*=.*\s+-version-info\s+([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
+			src/Makefile.am)
+	if [ ! "${e}" ] || [ "${_SOVERSION}" != "$(( "${e}" ))" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
 
 termux_step_pre_configure() {
 	./autogen.sh
