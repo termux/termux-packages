85a2d27f232500875eb48e6790a6f13b58e5d605
Krush206 <37114863+Krush206@users.noreply.github.com>
2023-01-15 18:42:29 -0300

    pcsx-rearmed: install plugins

--- x11-packages/pcsx-rearmed/build.sh
+++ x11-packages/pcsx-rearmed/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Yet another PCSX fork based on the PCSX-Reloaded project
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=23
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=git+https://github.com/notaz/pcsx_rearmed
 TERMUX_PKG_GIT_BRANCH=r${TERMUX_PKG_VERSION}
 TERMUX_PKG_SHA256=887e9b5ee7b8115d35099c730372b4158fd3e215955a06d68e20928b339646af
@@ -25,6 +25,8 @@ termux_step_configure() {
 
 termux_step_make_install() {
 	install -Dm755 pcsx ${PREFIX}/bin/pcsx
-	mkdir -p ${PREFIX}/etc/pcsx
+	mkdir -p ${PREFIX}/etc/pcsx ${PREFIX}/lib/pcsx_plugins
 	cp -r frontend/pandora/skin ${PREFIX}/etc/pcsx/
+	install -m755 plugins/*.so ${PREFIX}/lib/pcsx_plugins/
+	ln -fs ../../lib/pcsx_plugins ${PREFIX}/etc/pcsx/plugins
 }
