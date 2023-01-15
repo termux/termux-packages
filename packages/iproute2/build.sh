762c646fbd98a684179e9b14d0d41ae4c68e4160
Leonid Pliushch <sylirre@termux.dev>
2023-01-15 23:07:57 +0200

    chore(main/iproute2): upstream changed archives
    and their url...

--- packages/iproute2/build.sh
+++ packages/iproute2/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="Utilities for controlling networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=6.1.0
-TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b58b95f34c5b6e8171d1833ccfcf43c86aaa064c99419bd1b2a2eb7ee741f089
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5ce12a0fec6b212725ef218735941b2dab76244db7e72646a76021b0537b43ab
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
 
