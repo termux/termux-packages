f99823fd455b226d69e386a86eba58a47235b39a
Maxython <mixython@gmail.com>
2023-01-16 10:08:28 +0300

    tree-wide: make python packages use termux_setup_python_pip (part 2)
    This happened because I didn't separate the packages correctly

--- packages/python-apt/build.sh
+++ packages/python-apt/build.sh
@@ -7,24 +7,4 @@ TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/p/python-apt/python-ap
 TERMUX_PKG_SHA256=07ece069cdc9f5523a405f46ab5437260dca5e34909601c4540f160c476bb982
 TERMUX_PKG_DEPENDS="apt, build-essential, libc++, python, texinfo"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
-
-termux_step_pre_configure() {
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
-	build-pip install wheel
-
-	LDFLAGS+=" -lpython${_PYTHON_VERSION}"
-}
-
-termux_step_make_install() {
-	DEBVER=$TERMUX_PKG_VERSION \
-		python setup.py install --force --prefix $TERMUX_PREFIX
-}
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
--- packages/python-cryptography/build.sh
+++ packages/python-cryptography/build.sh
@@ -11,41 +11,25 @@ TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-
-_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, cffi, setuptools-rust"
+TERMUX_PKG_PYTHON_TARGET_DEPS="'cffi>=1.12'"
 
 termux_step_post_get_source() {
 	echo "Applying openssl-libs.diff"
-	sed "s%@PYTHON_VERSION@%$_PYTHON_VERSION%g" \
+	sed "s%@PYTHON_VERSION@%$TERMUX_PYTHON_VERSION%g" \
 		$TERMUX_PKG_BUILDER_DIR/openssl-libs.diff | patch --silent -p1
 }
 
-termux_step_pre_configure() {
+termux_step_configure() {
 	termux_setup_rust
-
-	termux_setup_python_crossenv
-	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
-	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
-	python${_PYTHON_VERSION} -m crossenv \
-		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
-		${_CROSSENV_PREFIX}
-	popd
-	. ${_CROSSENV_PREFIX}/bin/activate
-
-	build-pip install wheel cffi setuptools-rust
-}
-
-termux_step_make_install() {
 	export CARGO_BUILD_TARGET=${CARGO_TARGET_NAME}
-	export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
-	export PYTHONPATH=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
-	pip install --no-deps . --prefix $TERMUX_PREFIX
+        export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
 }
 
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
 	#!$TERMUX_PREFIX/bin/sh
 	echo "Installing dependencies through pip..."
-	pip3 install --no-binary cffi 'cffi>=1.12'
+	pip3 install --no-binary $TERMUX_PKG_PYTHON_TARGET_DEPS
 	EOF
 }
