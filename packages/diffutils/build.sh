2fe86595f6e1f02c2a029a88e7004fcbf449238f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-16 19:26:15 +0900

    diffutils: Bump to 3.9

--- packages/diffutils/build.sh
+++ packages/diffutils/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/diffutils/
 TERMUX_PKG_DESCRIPTION="Programs (cmp, diff, diff3 and sdiff) related to finding differences between files"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.8
+TERMUX_PKG_VERSION=3.9
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/diffutils/diffutils-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec
+TERMUX_PKG_SHA256=d80d3be90a201868de83d78dad3413ad88160cc53bcc36eb9eaf7c20dbf023f1
 TERMUX_PKG_DEPENDS="libiconv"
 TERMUX_PKG_ESSENTIAL=true
 
