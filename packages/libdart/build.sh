8cdf2b82c6133e2dcbf909f3c9563221e2f30787
Pooya Moradi <pvonmoradi@gmail.com>
2023-01-17 14:59:21 +0330

    libdart: fix update to 6.13.0

--- packages/libdart/build.sh
+++ packages/libdart/build.sh
@@ -3,25 +3,26 @@ TERMUX_PKG_DESCRIPTION="Dynamic Animation and Robotics Toolkit"
 TERMUX_PKG_GROUPS="science"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Pooya Moradi <pvonmoradi@gmail.com>"
-TERMUX_PKG_VERSION="6.12.2"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="6.13.0"
 TERMUX_PKG_SRCURL="https://github.com/dartsim/dart/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=db1b3ef888d37f0dbc567bc291ab2cdb5699172523a58dd5a5fe513ee38f83b0
+TERMUX_PKG_SHA256=4da3ff8cee056252a558b05625a5ff29b21e71f2995e6d7f789abbf6261895f7
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 # FIXME add ipopt, nlopt, snopt  after a proper FORTRAN compiler is
 # added (hence LAPACK)
 # FIXME why pagmo is not detected by build system?
 # FIXME add urdfdom
-TERMUX_PKG_DEPENDS="libc++, eigen, assimp, libccd, libfcl, boost, libbullet, libode, libpagmo, octomap-static, libtinyxml2"
+TERMUX_PKG_DEPENDS="libc++, eigen, assimp, libccd, libfcl, fmt, libspdlog, libbullet, libode, libpagmo, octomap-static, libtinyxml2"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DDART_VERBOSE=ON
 -DBUILD_SHARED_LIBS=ON
 -DDART_ENABLE_SIMD=OFF
 -DDART_BUILD_GUI_OSG=OFF
--DDART_BUILD_DARTPY=ON
+-DDART_BUILD_DARTPY=OFF
 -DDART_CODECOV=OFF
 -DDART_FAST_DEBUG=OFF
 -DDART_FORCE_COLORED_OUTPUT=OFF
+-DDART_DOWNLOAD_DEPENDENT_PACKAGES=OFF
 "
 
