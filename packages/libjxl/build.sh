67ce70eef1df4e41fc808d73261bab390ab6578a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-19 03:45:04 +0900

    libjxl: Bump to 0.8.0

--- packages/libjxl/build.sh
+++ packages/libjxl/build.sh
@@ -2,18 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://jpegxl.info/
 TERMUX_PKG_DESCRIPTION="JPEG XL image format reference implementation"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.8.0
 TERMUX_PKG_SRCURL=https://github.com/libjxl/libjxl/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3114bba1fabb36f6f4adc2632717209aa6f84077bc4e93b420e0d63fa0455c5e
+TERMUX_PKG_SHA256=6b4c140c1738acbed6b7d22858e0526373f0e9938e3f6c0a6b8943189195aad1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="brotli, libc++"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DJPEGXL_FORCE_SYSTEM_BROTLI=True"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DJPEGXL_ENABLE_JNI=False
+-DJPEGXL_FORCE_SYSTEM_BROTLI=True
+"
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after RELEASE / SOVERSION is changed.
-	local _SOVERSION=0.7
+	local _SOVERSION=0.8
 
 	for a in MAJOR SO_MINOR; do
 		local _${a}=$(sed -En 's/^set\(JPEGXL_'"${a}"'_VERSION\s+([0-9]+).*/\1/p' \
--- packages/libjxl/libjxl-progs.subpackage.sh
+++ packages/libjxl/libjxl-progs.subpackage.sh
@@ -1,3 +1,3 @@
 TERMUX_SUBPKG_DESCRIPTION="Programs for manipulating JPEG XL files"
 TERMUX_SUBPKG_INCLUDE="bin/ share/man/man1/"
-TERMUX_SUBPKG_DEPENDS="giflib, libjpeg-turbo, libpng, libwebp, libandroid-spawn"
+TERMUX_SUBPKG_DEPENDS="giflib, libandroid-glob, libandroid-spawn, libjpeg-turbo, libpng, libwebp"
