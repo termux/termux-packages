db2fcbc34ad684c5aaaffd22c334b939f25acef7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-18 14:50:11 +0900

    git: Bump to 2.39.1

--- packages/git/build.sh
+++ packages/git/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://git-scm.com/
 TERMUX_PKG_DESCRIPTION="Fast, scalable, distributed revision control system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.39.0
+TERMUX_PKG_VERSION=2.39.1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/pub/software/scm/git/git-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ba199b13fb5a99ca3dec917b0bd736bc0eb5a9df87737d435eddfdf10d69265b
+TERMUX_PKG_SHA256=40a38a0847b30c371b35873b3afcf123885dd41ea3ecbbf510efa97f3ce5c161
 TERMUX_PKG_DEPENDS="libcurl, libiconv, less, openssl, pcre2, zlib"
 
 ## This requires a working $TERMUX_PREFIX/bin/sh on the host building:
