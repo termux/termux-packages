1a7165767247b5776678facc16e7bc9ec2c91cd7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-18 22:26:38 +0900

    tigervnc: Bump xserver to 21.1.6

--- x11-packages/tigervnc/build.sh
+++ x11-packages/tigervnc/build.sh
@@ -4,12 +4,12 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # No update anymore. v1.11.x requires support of PAM.
 TERMUX_PKG_VERSION=(1.10.1
-		    1.20.14)
-TERMUX_PKG_REVISION=31
+		    21.1.6)
+TERMUX_PKG_REVISION=32
 TERMUX_PKG_SRCURL=(https://github.com/TigerVNC/tigervnc/archive/v${TERMUX_PKG_VERSION}.tar.gz
 		   https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz)
 TERMUX_PKG_SHA256=(19fcc80d7d35dd58115262e53cac87d8903180261d94c2a6b0c19224f50b58c4
-		   5cc5b70b9be89443e2594b93656c60bd5e82cd7f01deb4ce4faf81dcf546a16b)
+		   1eb86ed674d042b6c8b1f9135e59395cbbca35ed551b122f73a7d8bb3bb22484)
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libgnutls, libjpeg-turbo, libpixman, libx11, libxau, libxdamage, libxdmcp, libxext, libxfixes, libxfont2, mesa, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp, zlib"
 TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-server-xvfb, xorg-util-macros, xorgproto, xtrans"
 TERMUX_PKG_SUGGESTS="aterm, xorg-twm"
@@ -20,17 +20,26 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_VIEWER=OFF -DENABLE_NLS=OFF -DENABLE_PA
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
+	local p=tigervnc-xserver21-patch.patch
+	termux_download \
+		"https://github.com/TigerVNC/tigervnc/commit/0c5a2b2e7759c2829c07186cfce4d24aa9b5274e.patch" \
+		"$TERMUX_PKG_CACHEDIR/${p}" \
+		1fd6858fbc7c67aa3ab82347c5b9dc54e3bb7a9386f373a155acbaca5d8db3c6
+	echo "Applying ${p}"
+	cat "$TERMUX_PKG_CACHEDIR/${p}" | patch --silent -p1
+
 	## TigerVNC requires sources of X server (either Xorg or Xvfb).
 	cp -r xorg-server-${TERMUX_PKG_VERSION[1]}/* unix/xserver/
 
 	cd ${TERMUX_PKG_BUILDDIR}/unix/xserver
 	for p in "$TERMUX_SCRIPTDIR/x11-packages/xorg-server-xvfb"/*.patch; do
+		echo "Applying $(basename "${p}")"
 		sed -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
 			-e "s%\@TERMUX_HOME\@%${TERMUX_ANDROID_HOME}%g" "$p" \
 			| patch --silent -p1
 	done
 
-	patch -p1 -i ${TERMUX_PKG_SRCDIR}/unix/xserver120.patch
+	patch -p1 -i ${TERMUX_PKG_SRCDIR}/unix/xserver21.1.1.patch
 }
 
 termux_step_pre_configure() {
