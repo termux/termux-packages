1cfe4180d64e9ef2f8c85ad03cef8e2503e39b4c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-18 12:43:10 +0900

    googletest: Bump to 1.13.0

--- packages/googletest/build.sh
+++ packages/googletest/build.sh
@@ -2,12 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/google/googletest
 TERMUX_PKG_DESCRIPTION="Google C++ testing framework"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.1"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/google/googletest/archive/release-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=81964fe578e9bd7c94dfdb09c8e4d6e6759e19967e397dbea48d1c10e45d0df2
+TERMUX_PKG_VERSION=1.13.0
+TERMUX_PKG_SRCURL=https://github.com/google/googletest/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ad7fdba11ea011c1d925b3289cf4af2c66a352e18d4c7264392fead75e919363
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_CONFLICTS="libgtest"
 TERMUX_PKG_REPLACES="libgtest"
@@ -16,7 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=1.12.1
+	local _SOVERSION=1.13.0
 
 	local v=$(sed -En 's/^set\(GOOGLETEST_VERSION\s+([0-9.]+).*/\1/p' \
 			CMakeLists.txt)
