3dbe11e98b7cf92852e9c00e4aec6fd187c0805b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-19 20:38:06 +0900

    keybase: Bump to 6.0.4

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://keybase.io
 TERMUX_PKG_DESCRIPTION="Key directory that maps social media identities to encryption keys"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.0.2"
+TERMUX_PKG_VERSION=6.0.4
 TERMUX_PKG_SRCURL=https://github.com/keybase/client/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bdc42b44727614d92768aaaf1ea4e0f01c6b24d5a478bb4b89e0abc93bc67ed5
+TERMUX_PKG_SHA256=10f1b0fbee30520091a2005886947e34ea8d0f3dc3197a80563e5eb12664cc59
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_REPLACES="kbfs"
 TERMUX_PKG_CONFLICTS="kbfs"
@@ -14,7 +14,7 @@ termux_step_make() {
 	termux_setup_golang
 	cd go
 	go mod init || :
-	go mod tidy
+	go mod tidy -compat=1.17
 	mkdir .bin
 	go build -v -tags 'production' -o ./.bin/keybase ./keybase
 	go build -v -tags 'production' -o ./.bin/git-remote-keybase \
