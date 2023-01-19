6b4cdf47db144567bde4eb14121498b115d098ca
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-20 08:53:34 +0900

    termux_step_configure_meson: Allow for debug build

--- scripts/build/configure/termux_step_configure_meson.sh
+++ scripts/build/configure/termux_step_configure_meson.sh
@@ -1,13 +1,21 @@
 termux_step_configure_meson() {
 	termux_setup_meson
+
+	local _meson_buildtype="minsize"
+	local _meson_stripflag="--strip"
+	if [ "$TERMUX_DEBUG_BUILD" = "true" ]; then
+		_meson_buildtype="debug"
+		_meson_stripflag=
+	fi
+
 	CC=gcc CXX=g++ CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS= $TERMUX_MESON \
 		$TERMUX_PKG_SRCDIR \
 		$TERMUX_PKG_BUILDDIR \
 		--cross-file $TERMUX_MESON_CROSSFILE \
 		--prefix $TERMUX_PREFIX \
 		--libdir lib \
-		--buildtype minsize \
-		--strip \
+		--buildtype ${_meson_buildtype} \
+		${_meson_stripflag} \
 		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS \
 		|| (termux_step_configure_meson_failure_hook && false)
 }
