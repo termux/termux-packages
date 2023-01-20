eb607242234160a2a2cd691441c8848467fae0ef
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-20 11:33:36 +0900

    cmake: Bump to 3.25.2

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package soft
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # When updating version here, please update termux_setup_cmake.sh as well.
-TERMUX_PKG_VERSION=3.25.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=3.25.2
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8
+TERMUX_PKG_SHA256=c026f22cb931dd532f648f087d587f07a1843c6e66a3dfca4fb0ea21944ed33c
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, zlib"
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_FORCE_CMAKE=true
