96010a4d7ed3ac21cd06663ebc121080eaffd1d4
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-01-21 23:43:07 +0800

    libepoxy: enable egl support

--- x11-packages/libepoxy/build.sh
+++ x11-packages/libepoxy/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Library handling OpenGL function pointer management"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/anholt/libepoxy/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a7ced37f4102b745ac86d6a70a9da399cc139ff168ba6b8002b4d8d43c900c15
 TERMUX_PKG_DEPENDS="mesa"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dglx=yes
--Degl=no
+-Degl=yes
 -Dx11=true
 -Dtests=false
 "
