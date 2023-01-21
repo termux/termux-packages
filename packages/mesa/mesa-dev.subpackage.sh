04f3fc5cd20bc3c71a2350424eb69da67caa12cf
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-01-21 23:45:07 +0800

    mesa: adapt to libglvnd

--- packages/mesa/mesa-dev.subpackage.sh
+++ packages/mesa/mesa-dev.subpackage.sh
@@ -1,9 +1,11 @@
-TERMUX_SUBPKG_DESCRIPTION="Mesa's EGL and GLES headers"
+TERMUX_SUBPKG_DESCRIPTION="Mesa's OpenGL headers"
 TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
-TERMUX_SUBPKG_BREAKS="mesa (<< 22.2.2-1), ndk-sysroot (<< 25b-3)"
-TERMUX_SUBPKG_REPLACES="mesa (<< 22.2.2-1), ndk-sysroot (<< 25b-3)"
+TERMUX_SUBPKG_DEPENDS="libglvnd-dev"
+TERMUX_SUBPKG_BREAKS="mesa (<< 22.3.3-2), ndk-sysroot (<< 25b-3)"
+TERMUX_SUBPKG_REPLACES="mesa (<< 22.3.3-2), ndk-sysroot (<< 25b-3)"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 TERMUX_SUBPKG_INCLUDE="
-include/EGL
-include/GLES*
+include/GL/!(osmesa.h)
+include/EGL/
+include/gbm.h
 "
