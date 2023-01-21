c7f892e284b876079ef42e88e134aa8f7e5a5b16
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-22 00:09:52 +0900

    root-repo: Fix repos URL

--- packages/root-repo/build.sh
+++ packages/root-repo/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Package repository containing programs for rooted device
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION=2.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -12,9 +12,9 @@ termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
 	{
 		echo "# The root termux repository, with cloudflare cache"
-		echo "deb https://packages-cf.termux.dev/apt/termux-main/ root stable"
+		echo "deb https://packages-cf.termux.dev/apt/termux-root/ root stable"
 		echo "# The root termux repository, without cloudflare cache"
-		echo "# deb https://packages.termux.dev/apt/termux-main/ root stable"
+		echo "# deb https://packages.termux.dev/apt/termux-root/ root stable"
 	} > $TERMUX_PREFIX/etc/apt/sources.list.d/root.list
 }
 
