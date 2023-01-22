ba6707dfa358c9e7e48ee67cc49490e491d91156
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-22 21:25:09 +0900

    libx11: Fix compose key tables
    Reference: https://github.com/termux/termux-packages/issues/14491

--- packages/libx11/build.sh
+++ packages/libx11/build.sh
@@ -5,14 +5,17 @@ TERMUX_PKG_LICENSE="MIT, X11"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.8.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 #TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SRCURL=https://ftp-osl.osuosl.org/pub/gentoo/distfiles/libX11-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e31565c84006b6b8e01dc9399c806085739710bc2db2e0930f1511ed9d6585bd
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros, xtrans"
 TERMUX_PKG_RECOMMENDS="xorg-xauth"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_path_RAWCPP=/usr/bin/cpp
+--enable-malloc0returnsnull
+"
 
 termux_step_post_massage() {
 	# Seems like some programs in the wild try to dlopen(3) `libX11.so.6`.
