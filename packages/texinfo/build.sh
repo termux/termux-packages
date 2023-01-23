1013b3b13503127570612a273f61434ecf3affb5
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-23 21:23:41 +0900

    texinfo: Bump to 7.0.2

--- packages/texinfo/build.sh
+++ packages/texinfo/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/texinfo/
 TERMUX_PKG_DESCRIPTION="Documentation system for on-line information and printed output"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.0.1
+TERMUX_PKG_VERSION=7.0.2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/texinfo/texinfo-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=bcd221fdb2d807a8a09938a0f8d5e010ebd2b58fca16075483d6fcb78db2c6b2
+TERMUX_PKG_SHA256=f211ec3261383e1a89e4555a93b9d017fe807b9c3992fb2dff4871dae6da54ad
 # gawk is used by texindex:
 TERMUX_PKG_DEPENDS="libiconv, ncurses, perl, gawk"
 TERMUX_PKG_RECOMMENDS="update-info-dir"
