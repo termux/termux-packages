e6452bc8ba23d6be9a03204af5016af9971e114f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-24 20:22:42 +0900

    findomain: Fix hardcoded `/etc/resolv.conf`

--- packages/findomain/build.sh
+++ packages/findomain/build.sh
@@ -3,8 +3,24 @@ TERMUX_PKG_DESCRIPTION="Findomain is the fastest subdomain enumerator and the on
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="8.2.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Findomain/Findomain/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=93c580c9773e991e1073b9b597bb7ccf77f54cebfb9761e4b1180ff97fb15bf6
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="resolv-conf"
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	for d in $CARGO_HOME/registry/src/github.com-*/trust-dns-resolver-*; do
+		sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" \
+			$TERMUX_PKG_BUILDER_DIR/trust-dns-resolver.diff \
+			| patch --silent -p1 -d ${d} || :
+	done
+}
