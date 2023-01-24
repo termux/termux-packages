03c5384cab3cb46d49b82dbb138c9964dd46259c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-24 13:15:20 +0900

    dte: Bump to 1.11

--- packages/dte/build.sh
+++ packages/dte/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE='https://craigbarnes.gitlab.io/dte/'
 TERMUX_PKG_DESCRIPTION='A small, configurable console text editor'
 TERMUX_PKG_LICENSE='GPL-2.0'
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.10
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL="https://craigbarnes.gitlab.io/dist/dte/dte-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=db62aab235764f735adc8378f796d6474596582b7dae357e0bddf31304189800
+TERMUX_PKG_VERSION=1.11
+TERMUX_PKG_SRCURL=https://gitlab.com/craigbarnes/dte/-/archive/v${TERMUX_PKG_VERSION}/dte-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=549847135224db125371f4c8bc57119eeb31b5e99af2923f601b55800eac1153
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, libiconv"
 TERMUX_PKG_BUILD_IN_SRC=true
 
