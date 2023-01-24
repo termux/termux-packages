9a47f6478a69f91ff72b410b9a319e71c45f5460
Kevin Williams <admin@utermux.dev>
2023-01-24 09:43:18 +0800

    upgpkg(main/tut): 2.0.0
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/tut/build.sh
+++ packages/tut/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/RasmusLindroth/tut
 TERMUX_PKG_DESCRIPTION="A TUI for Mastodon with vim inspired keys"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.34"
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL=https://github.com/RasmusLindroth/tut/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4340e41395cb8cf56e11f500738ef85b838dbfeddf219c988a058e4789009a69
+TERMUX_PKG_SHA256=f0deeaf923c05b713a72a338684c54159a433952475656f8b46be6bc0d4489ca
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -21,5 +21,5 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin tut
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME config.example.ini
+	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME config.example.toml
 }
