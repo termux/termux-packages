43f872965bf9edea6ba5da7b9714320c17835208
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-24 17:33:50 +0900

    asymptote: Bump to 2.84

--- packages/asymptote/build.sh
+++ packages/asymptote/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://asymptote.sourceforge.io/
 TERMUX_PKG_DESCRIPTION="A powerful descriptive vector graphics language for technical drawing"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(2.83)
+TERMUX_PKG_VERSION=(2.84)
 TERMUX_PKG_VERSION+=(0.9.9.8)
 TERMUX_PKG_SRCURL=(https://downloads.sourceforge.net/asymptote/asymptote-${TERMUX_PKG_VERSION}.src.tgz
                    https://github.com/g-truc/glm/archive/${TERMUX_PKG_VERSION[1]}.tar.gz)
-TERMUX_PKG_SHA256=(fe3ca71f49e59e68633887c41613c08abd82a749fcac30353970ac7081b388a3
+TERMUX_PKG_SHA256=(6765702d04b8121ca4e49f4b0d23244e623c904981f3588bc4210f0d696dccdb
                    7d508ab72cb5d43227a3711420f06ff99b0a0cb63ee2f93631b162bfe1fe9592)
-TERMUX_PKG_DEPENDS="libc++, libtirpc, zlib"
+TERMUX_PKG_DEPENDS="fftw, libc++, libtirpc, zlib"
 TERMUX_PKG_BUILD_DEPENDS="ncurses-static, readline-static"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
