e13df427f484e1a3b03d50d26f869711614f07c3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-24 18:52:54 +0900

    helix: Separate grammar-related files as subpackages
    * Install under `$PREFIX/opt`
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/helix/build.sh
+++ packages/helix/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A post-modern modal text editor written in rust"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION="22.12"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="git+https://github.com/helix-editor/helix"
 TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}"
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_SUGGESTS="helix-grammars"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -14,7 +15,7 @@ termux_step_make_install() {
 
 	cargo build --jobs "${TERMUX_MAKE_PROCESSES}" --target "${CARGO_TARGET_NAME}" --release
 
-	local datadir="${TERMUX_PREFIX}/share/${TERMUX_PKG_NAME}"
+	local datadir="${TERMUX_PREFIX}/opt/${TERMUX_PKG_NAME}"
 	mkdir -p "${datadir}"
 
 	cat >"${TERMUX_PREFIX}/bin/hx" <<-EOF
---
+++ packages/helix/helix-grammars.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_INCLUDE="opt/helix/runtime/grammars/*.so"
+TERMUX_SUBPKG_DESCRIPTION="Helix grammars"
+TERMUX_SUBPKG_DEPENDS="libc++"
