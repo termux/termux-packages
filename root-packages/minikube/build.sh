502836358dc6a084c77b6bc105a11a16684f7fc9
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-28 18:19:55 +0900

    minikube: Bump to 1.29.0

--- root-packages/minikube/build.sh
+++ root-packages/minikube/build.sh
@@ -1,25 +1,25 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/kubernetes/minikube"
 TERMUX_PKG_DESCRIPTION="minikube implements a local Kubernetes cluster."
-TERMUX_PKG_VERSION=1.28.0
-TERMUX_PKG_SRCURL="https://github.com/kubernetes/minikube/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=0e64c007b7423999da506aa4fb4002b1ef5847cefafd29d800cc56dbd2b38cc4
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.29.0
+TERMUX_PKG_SRCURL="https://github.com/kubernetes/minikube/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=90fd915150fc69221ed9824bdbd59974bede1470bff8144145fcfadea8a8d27f
 TERMUX_PKG_DEPENDS="docker, kubectl"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	go mod init || :
+	go mod tidy
+}
 
 termux_step_make() {
-	set -x
-	termux_setup_golang
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	cd $TERMUX_PKG_SRCDIR 
-	mkdir bin/
-	GIT_SHA=$(wget -qO- "https://api.github.com/repos/kubernetes/minikube/git/ref/tags/v${TERMUX_PKG_VERSION}" | grep "sha"|cut -d '"' -f 4);
-	ISO_VERSION=$(wget -qO- "https://github.com/kubernetes/minikube/raw/v${TERMUX_PKG_VERSION}/Makefile" | grep "ISO_VERSION" | grep "=" | cut -d '=' -f 2 | head -n 1);
-	storageProvisionerVersion=$(wget -qO- "https://github.com/kubernetes/minikube/raw/v${TERMUX_PKG_VERSION}/Makefile" | grep "STORAGE_PROVISIONER_TAG" | grep "=" | cut -d '=' -f 2 | head -n 1);
-	export GOOS=android;
-	go build -o bin/minikube k8s.io/minikube/cmd/minikube
+	mkdir -p bin
+	go build -o bin/minikube ./cmd/minikube
 }
 
 termux_step_make_install() {
-	install -Dm755 -t "${TERMUX_PREFIX}"/bin "${TERMUX_PKG_SRCDIR}"/bin/minikube
+	install -Dm755 -t "${TERMUX_PREFIX}"/bin bin/minikube
 }
