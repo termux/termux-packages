c17b79de845935ca066d2bf8a919182ca1dd2145
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-29 08:28:45 +0900

    libnotify: Fix gir

--- x11-packages/libnotify/build.sh
+++ x11-packages/libnotify/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libnotify/${_MAJOR_VERSION}/libnotify-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d033e6d4d6ccbf46a436c31628a4b661b36dca1f5d4174fe0173e274f4e62557
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib"
@@ -16,4 +17,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		# Pre-installed headers affect GIR generation:
+		rm -rf "$TERMUX_PREFIX/include/libnotify"
+	fi
 }
