6593fbaf4dbac1186898c1f51c11db8530b32159
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-28 15:31:25 +0900

    new package: opengl
    to be specified as a dependency of packages that require `lib/libGL.so`
    and friends.
    
    Currently `lib/libGL.so` is provided by `libglvnd` package. Depending on
    `libglvnd`, however, would not work, because `libglvnd` does not depend
    on any provider of OpenGL implementation (`mesa` is a recommendation.)
    At least one provider should be hard-depended on, not only recommended.

---
+++ packages/opengl/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-packages
+TERMUX_PKG_DESCRIPTION="A metapackage for OpenGL implementation"
+TERMUX_PKG_LICENSE="Public Domain"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.1
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_DEPENDS="libglvnd, mesa"
+TERMUX_PKG_METAPACKAGE=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
