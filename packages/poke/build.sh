ed9f6a0d7956247318ace0e342b96a6d90d1c890
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-30 04:33:50 +0900

    poke: Bump to 3.0
    The `_offset` type bug in Gnulib has been fixed in this release.

--- packages/poke/build.sh
+++ packages/poke/build.sh
@@ -2,9 +2,24 @@ TERMUX_PKG_HOMEPAGE=http://www.jemarch.net/poke.html
 TERMUX_PKG_DESCRIPTION="Interactive, extensible editor for binary data."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.4
+TERMUX_PKG_VERSION=3.0
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/poke/poke-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=841e285917c6738ce982a6930e30ebeadecfb0655a79d9184f16f876a9fe6e47
-TERMUX_PKG_DEPENDS="readline, gettext, json-c, libgc"
+TERMUX_PKG_SHA256=79a9b2f33c9f8c327c499afadaeeabfeecf6ad4988924d2c6c6f317e50317add
+TERMUX_PKG_DEPENDS="gettext, libgc, ncurses, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-sysroot=$(dirname $TERMUX_PREFIX) --disable-threads --disable-hserver"
+
+termux_step_pre_configure() {
+	_NEED_DUMMY_LIBPTHREAD_A=
+	_LIBPTHREAD_A=$TERMUX_PREFIX/lib/libpthread.a
+	if [ ! -e $_LIBPTHREAD_A ]; then
+		_NEED_DUMMY_LIBPTHREAD_A=true
+		echo '!<arch>' > $_LIBPTHREAD_A
+	fi
+}
+
+termux_step_post_make_install() {
+	if [ $_NEED_DUMMY_LIBPTHREAD_A ]; then
+		rm -f $_LIBPTHREAD_A
+	fi
+}
