5317e995162fbec2b0f595146a1a0005cbad53c1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-30 04:26:23 +0900

    libopenmpt: Bump to 0.6.8

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://lib.openmpt.org/libopenmpt/
 TERMUX_PKG_DESCRIPTION="OpenMPT based module player library and libopenmpt based command-line player"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.6.7
+TERMUX_PKG_VERSION=0.6.8
 TERMUX_PKG_SRCURL=https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${TERMUX_PKG_VERSION}+release.autotools.tar.gz
-TERMUX_PKG_SHA256=2174ac0f5a148ba684db768a47edf783eff9084fbca5fef6c997501643100163
+TERMUX_PKG_SHA256=1c618b3dff1a7cb6a24f431755920fa243756268138ffa31d3db7c607c2c2d69
 TERMUX_PKG_DEPENDS="libc++, libflac, libogg, libsndfile, libvorbis, mpg123, pulseaudio, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
