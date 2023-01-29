5968c9c479f6fd960a26737f152d7b9fc2aea822
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-30 03:35:02 +0900

    procs: Bump to 0.13.4
    The patch to fix build failure (termux/termux-packages#12265) is no
    longer needed.

--- packages/procs/build.sh
+++ packages/procs/build.sh
@@ -2,29 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/dalance/procs
 TERMUX_PKG_DESCRIPTION="A modern replacement for ps"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.13.3"
+TERMUX_PKG_VERSION=0.13.4
 TERMUX_PKG_SRCURL=https://github.com/dalance/procs/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=aa93a588504dcc74df699d8a3bc2a27d3da94a772106a42d3d862a5fd17725c3
+TERMUX_PKG_SHA256=9b9b59b79049cf6ae2c39d9cc5b0c5af81411ba898a414fda41f68921c3c9539
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-
-termux_step_pre_configure() {
-	termux_setup_rust
-
-	export CFLAGS="${TARGET_CFLAGS}"
-
-	: "${CARGO_HOME:=$HOME/.cargo}"
-	export CARGO_HOME
-
-	rm -rf $CARGO_HOME/registry/src/github.com-*/libc-*
-	rm -rf $CARGO_HOME/registry/src/github.com-*/rustix-*
-	cargo fetch --target "${CARGO_TARGET_NAME}"
-
-	for d in $CARGO_HOME/registry/src/github.com-*/rustix-*; do
-		patch --silent -p1 -d ${d} < $TERMUX_PKG_BUILDER_DIR/0001-rustix-upstream-fix-libc-removing-unsafe-on-makedev.diff
-	done
-
-}
 
 # This package contains makefiles to run the tests. So, we need to override build steps.
 termux_step_make() {
