a75c7bd34eca5eea9c095b3269358d68ccd05a0a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-31 03:32:57 +0900

    git-crypt: Migrate to using OpenSSL 3

--- packages/git-crypt/build.sh
+++ packages/git-crypt/build.sh
@@ -3,17 +3,16 @@ TERMUX_PKG_DESCRIPTION="Enables transparent encryption and decryption of files f
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@jottr"
 TERMUX_PKG_VERSION=0.7.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/AGWA/git-crypt/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2210a89588169ae9a54988c7fdd9717333f0c6053ff704d335631a387bd3bcff
-TERMUX_PKG_DEPENDS="git, libc++, openssl-1.1"
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://www.agwa.name/projects/git-crypt/downloads/git-crypt-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=50f100816a636a682404703b6c23a459e4d30248b2886a5cf571b0d52527c7d8
+TERMUX_PKG_DEPENDS="git, libc++, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DCMAKE_BUILD_TYPE=Release -Dbuild_parse=yes -Dbuild_xmlparser=yes"
 TERMUX_PKG_EXTRA_MAKE_ARGS="ENABLE_MAN=yes"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CFLAGS"
-	CPPFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CPPFLAGS"
-	CXXFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CXXFLAGS"
-	LDFLAGS="-L$TERMUX_PREFIX/lib/openssl-1.1 -Wl,-rpath=$TERMUX_PREFIX/lib/openssl-1.1 $LDFLAGS"
+	# https://github.com/AGWA/git-crypt/issues/232
+	CPPFLAGS+=" -DOPENSSL_API_COMPAT=0x30000000L"
+	CXXFLAGS+=" $CPPFLAGS"
 }
