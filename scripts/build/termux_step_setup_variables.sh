6ac0c2fad63c2308a05c1d5fe3615f23733f09ac
Tee KOBAYASHI <xtkoba@gmail.com>
2023-01-31 11:51:30 +0900

    scripts: Add new variable `TERMUX_PKG_NO_STRIP`
    to disable stripping binaries.
    
    To be used by `guile` package.

--- scripts/build/termux_step_setup_variables.sh
+++ scripts/build/termux_step_setup_variables.sh
@@ -107,6 +107,8 @@ termux_step_setup_variables() {
 	TERMUX_PKG_MASSAGEDIR=$TERMUX_TOPDIR/$TERMUX_PKG_NAME/massage
 	TERMUX_PKG_METAPACKAGE=false
 	TERMUX_PKG_NO_ELF_CLEANER=false # set this to true to disable running of termux-elf-cleaner on built binaries
+	TERMUX_PKG_NO_SHEBANG_FIX=false # if true, skip fixing shebang accordingly to TERMUX_PREFIX
+	TERMUX_PKG_NO_STRIP=false # set this to true to disable stripping binaries
 	TERMUX_PKG_NO_STATICSPLIT=false
 	TERMUX_PKG_STATICSPLIT_EXTRA_PATTERNS=""
 	TERMUX_PKG_PACKAGEDIR=$TERMUX_TOPDIR/$TERMUX_PKG_NAME/package
@@ -123,7 +125,6 @@ termux_step_setup_variables() {
 	TERMUX_PKG_TMPDIR=$TERMUX_TOPDIR/$TERMUX_PKG_NAME/tmp
 	TERMUX_PKG_SERVICE_SCRIPT=() # Fill with entries like: ("daemon name" 'script to execute'). Script is echoed with -e so can contain \n for multiple lines
 	TERMUX_PKG_GROUPS="" # https://wiki.archlinux.org/title/Pacman#Installing_package_groups
-	TERMUX_PKG_NO_SHEBANG_FIX=false # if true, skip fixing shebang accordingly to TERMUX_PREFIX
 	TERMUX_PKG_IS_HASKELL_LIB=true # by default assume haskell package is lib package as most of them will always be libs.
 	TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=false # if the package does not support compilation on a device, then this package should not be compiled on devices
 	TERMUX_PKG_SETUP_PYTHON=false # setting python to compile a package
