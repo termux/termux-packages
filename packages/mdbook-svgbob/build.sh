e1a8753c6b999abde84857d500aee5bbac7ceed5
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-01 08:02:44 +0900

    mdbook-svgbob: Bump to 0.2.1

--- packages/mdbook-svgbob/build.sh
+++ packages/mdbook-svgbob/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/boozook/mdbook-svgbob
 TERMUX_PKG_DESCRIPTION="SvgBob mdbook preprocessor which swaps code-blocks with neat SVG"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.1.1
+TERMUX_PKG_VERSION=0.2.1
 TERMUX_PKG_SRCURL=https://github.com/boozook/mdbook-svgbob/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9fc957a197474fa88bbec62b1a2bde00f9e7bcdda0b4d76b80584b8a562091dc
+TERMUX_PKG_SHA256=cd5c6a58db33e87db04a1f9a5773de860b81cf75bafbe7a9fdb3f4b9f85cbbdc
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
