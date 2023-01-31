638598610aa57ab9439c7413569b68978f6737f3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-01 07:19:18 +0900

    libminizip-ng: Bump to 3.0.8

--- packages/libminizip-ng/build.sh
+++ packages/libminizip-ng/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/zlib-ng/minizip-ng
 TERMUX_PKG_DESCRIPTION="A zip manipulation library written in C"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.0.7
+TERMUX_PKG_VERSION=3.0.8
 TERMUX_PKG_SRCURL=https://github.com/zlib-ng/minizip-ng/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=39981a0db1bb6da504909bce63d7493286c5e50825c056564544c990d15c55cf
+TERMUX_PKG_SHA256=27cc2f62cd02d79b71b346fc6ace02728385f8ba9c6b5f124062b0790a04629a
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libbz2, libiconv, liblzma, openssl, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_INCLUDEDIR=include/minizip-ng
@@ -15,3 +16,15 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 # This may be resolved by building zstd with CMake, but that needs extra care
 # such as SONAME change. I just cannot be bothered to do that for now.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DMZ_ZSTD=OFF"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=3
+
+	local v=$(sed -En 's/^set\(SOVERSION\s+"?([0-9]+).*/\1/p' \
+			CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
