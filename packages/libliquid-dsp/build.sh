904845b2a9fa09f430552e284e9815b1d01d24bf
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-01 07:11:55 +0900

    libliquid-dsp: Bump to 1.5.0

--- packages/libliquid-dsp/build.sh
+++ packages/libliquid-dsp/build.sh
@@ -2,12 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://liquidsdr.org/
 TERMUX_PKG_DESCRIPTION="Software-defined radio digital signal processing library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.0
+TERMUX_PKG_VERSION=1.5.0
 TERMUX_PKG_SRCURL=https://github.com/jgaeddert/liquid-dsp/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=66f38d509aa8f6207d2035bae5ee081a3d9df0f2cab516bc2118b5b1c6ce3333
+TERMUX_PKG_SHA256=93003edb6e74090b41009b1fae6f273a3e711dc4c8c56a0cca3e89167b765953
 TERMUX_PKG_DEPENDS="fftw"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	autoreconf -fi
+}
+
+termux_step_post_massage() {
+	shopt -s nullglob
+	local f
+	for f in lib/libliquid.a.*; do
+		termux_error_exit "File ${f} should not be contained herein."
+	done
+	shopt -u nullglob
 }
