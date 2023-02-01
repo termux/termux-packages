0e930f40925cba1065985eb4d1630ee82ef8633c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-01 16:00:37 +0900

    nfs-utils: Fix hardcoded paths

--- root-packages/nfs-utils/build.sh
+++ root-packages/nfs-utils/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Linux NFS userland utilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.6.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/nfs/nfs-utils-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=958890e4a2fe981c2e449002709224bedf009934bf69e73fe911cbc96aad0b7d
 TERMUX_PKG_DEPENDS="keyutils, libblkid, libcap, libdevmapper, libevent, libmount, libsqlite, libtirpc, libuuid, openldap"
@@ -16,6 +17,7 @@ libsqlite3_cv_is_recent=yes
 --with-modprobedir=$TERMUX_PREFIX/lib/modprobe.d
 --with-mountfile=$TERMUX_PREFIX/etc/nfsmounts.conf
 --with-nfsconfig=$TERMUX_PREFIX/etc/nfs.conf
+--with-start-statd=$TERMUX_PREFIX/bin/start-statd
 --with-statedir=$TERMUX_PREFIX/var/lib/nfs
 "
 TERMUX_PKG_RM_AFTER_INSTALL="
