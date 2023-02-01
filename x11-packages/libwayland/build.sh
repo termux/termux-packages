a68db1263a18c44c14d0c98edf17fac3f844bb6c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-01 11:35:31 +0900

    libwayland: Bump to 1.21.0

--- x11-packages/libwayland/build.sh
+++ x11-packages/libwayland/build.sh
@@ -2,33 +2,35 @@ TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Wayland protocol library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17.0
-TERMUX_PKG_REVISION=10
-TERMUX_PKG_SRCURL=https://wayland.freedesktop.org/releases/wayland-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=72aa11b8ac6e22f4777302c9251e8fec7655dc22f9d94ee676c6b276f95f91a4
-TERMUX_PKG_DEPENDS="libandroid-support, libffi, libxml2, libexpat"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-documentation --with-host-scanner"
+TERMUX_PKG_VERSION=1.21.0
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/wayland/-/releases/${TERMUX_PKG_VERSION}/downloads/wayland-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=6dc64d7fc16837a693a51cfdb2e568db538bfdc9f457d4656285bb9594ef11ac
+TERMUX_PKG_DEPENDS="libandroid-support, libexpat, libffi, libxml2"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dtests=false
+-Ddocumentation=false
+"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
---disable-libraries
---disable-documentation
---disable-dtd-validation
---prefix=$TERMUX_PREFIX/opt/$TERMUX_PKG_NAME/cross
+-Dlibraries=false
+-Dtests=false
+-Ddocumentation=false
+-Ddtd_validation=false
+--prefix $TERMUX_PREFIX/opt/$TERMUX_PKG_NAME/cross
 "
 
 termux_step_host_build() {
-	"$TERMUX_PKG_SRCDIR/configure" ${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
-	make -j "$TERMUX_MAKE_PROCESSES"
-	make install
+	# XXX: termux_setup_meson is not expected to be called in host build
+	AR=;CC=;CFLAGS=;CPPFLAGS=;CXX=;CXXFLAGS=;LD=;LDFLAGS=;PKG_CONFIG=;STRIP=
+	termux_setup_meson
+	unset AR CC CFLAGS CPPFLAGS CXX CXXFLAGS LD LDFLAGS PKG_CONFIG STRIP
+
+	$TERMUX_MESON $TERMUX_PKG_SRCDIR . \
+		$TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS
+	ninja -j $TERMUX_MAKE_PROCESSES
+	ninja install
 }
 
 termux_step_pre_configure() {
-	autoreconf -fi
-
-	_HOST_PREFIX=$TERMUX_PREFIX/opt/$TERMUX_PKG_NAME/cross
-	export PATH=${_HOST_PREFIX}/bin:$PATH
-}
-
-termux_step_post_make_install() {
-	sed -i 's/wayland_scanner=${exec_prefix}\/bin\/wayland-scanner/wayland_scanner=\/usr\/bin\/wayland-scanner/g' $TERMUX_PREFIX/lib/pkgconfig/wayland-scanner.pc
+	export PATH="$TERMUX_PREFIX/opt/$TERMUX_PKG_NAME/cross/bin:$PATH"
 }
