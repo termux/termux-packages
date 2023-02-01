daf2da703da0068161e4d4f21d38f7afcc84f590
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-02-01 15:25:08 +0800

    gtk-update-icon-cache: add triggers

--- x11-packages/gtk3/gtk-update-icon-cache.subpackage.sh
+++ x11-packages/gtk3/gtk-update-icon-cache.subpackage.sh
@@ -5,3 +5,18 @@ share/man/man1/gtk-update-icon-cache.1
 
 TERMUX_SUBPKG_DEPENDS="glib, gdk-pixbuf, libandroid-shmem"
 TERMUX_SUBPKG_DESCRIPTION="GTK+ icon cache updater"
+
+termux_step_create_subpkg_debscripts() {
+	cat <<- EOF > ./triggers
+	interest-noawait $TERMUX_PREFIX/share/icons
+	EOF
+
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	for i in \$(find "$TERMUX_PREFIX/share/icons" -type f -iname index.theme); do
+		gtk-update-icon-cache --force --quiet \$(dirname "\${i}")
+	done
+	unset i
+	exit 0
+	EOF
+}
