5e1485bc726da850db69d032319e6f4a118e1e1f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-03 05:29:54 +0900

    libcairo: Bump to 1.17.8
    GL backend has been removed upstream.

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://cairographics.org
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17.6
+TERMUX_PKG_VERSION=1.17.8
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/cairo/cairo/-/archive/${TERMUX_PKG_VERSION}/cairo-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=90496d135c9ef7612c98f8ee358390cdec0825534573778a896ea021155599d2
+TERMUX_PKG_SHA256=ead4724423eb969f98b456fe1e3ee1e1741fe1c8dfb1a41ca12afa81a6c1665f
 TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libandroid-shmem, liblzo, libpixman, libpng, libx11, libxcb, libxext, libxrender, zlib"
 TERMUX_PKG_BREAKS="libcairo-dev, libcairo-gobject"
 TERMUX_PKG_REPLACES="libcairo-dev, libcairo-gobject"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dgl-backend=disabled
 -Dpng=enabled
 -Dzlib=enabled
 -Dglib=enabled
