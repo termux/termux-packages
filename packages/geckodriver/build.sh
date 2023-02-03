a49781168ba4ffe0963741db24d67ef2e14aae0c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-03 14:33:00 +0900

    geckodriver: Bump to 0.32.1

--- packages/geckodriver/build.sh
+++ packages/geckodriver/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_VERSION=0.32.0
 TERMUX_PKG_HOMEPAGE=https://github.com/mozilla/geckodriver
 TERMUX_PKG_DESCRIPTION="Proxy for using W3C WebDriver-compatible clients to interact with Gecko-based browsers"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.32.1
 TERMUX_PKG_SRCURL=https://github.com/mozilla/geckodriver/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256="97e10921f615e84c1869fa8e1443810914e285e4675e8e45378b99a95c2d0f14"
+TERMUX_PKG_SHA256=d4aff6d728ee3da3f9390f72641af2e65b47cd9be72342f3ac5bb6e983a18406
 TERMUX_PKG_RECOMMENDS="firefox"
 TERMUX_PKG_BUILD_IN_SRC=true
 
