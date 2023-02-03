613c1ad64d0637340e53d411ebb4f62740418d72
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-04 03:41:56 +0900

    broot: Bump to 1.20.0

--- packages/broot/build.sh
+++ packages/broot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Canop/broot
 TERMUX_PKG_DESCRIPTION="A better way to navigate directories"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.19.0"
+TERMUX_PKG_VERSION=1.20.0
 TERMUX_PKG_SRCURL=https://github.com/Canop/broot/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3345bd0eb17f954212bd12528c476acaa0e4dacb38ad78a047b5aedc2f638c15
+TERMUX_PKG_SHA256=4637dfb91575ad6da36ab32f10ea5d363709587f7cf2e728fab25b2c73d30311
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -23,7 +23,7 @@ termux_step_pre_configure() {
 	local d
 	for d in $CARGO_HOME/registry/src/github.com-*/libgit2-sys-*/libgit2; do
 		(
-			t=${d}/src/util/
+			t=${d}/src/
 			cp $TERMUX_SCRIPTDIR/packages/libgit2/getloadavg.c ${t}
 			patch --silent -d ${t} < ${_patch}
 		) || :
