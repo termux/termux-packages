5a88953b0b4a7a6523774889ffed312b6d37829c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-04 09:30:59 +0900

    netcdf-c: Bump to 4.9.1

--- packages/netcdf-c/build.sh
+++ packages/netcdf-c/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.unidata.ucar.edu/software/netcdf/
 TERMUX_PKG_DESCRIPTION="NetCDF is a set of software libraries and self-describing, machine-independent data formats that support the creation, access, and sharing of array-oriented scientific data"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION="4.9.0"
-TERMUX_PKG_SHA256=9f4cb864f3ab54adb75409984c6202323d2fc66c003e5308f3cdf224ed41c0a6
+TERMUX_PKG_VERSION=4.9.1
 TERMUX_PKG_SRCURL=https://github.com/Unidata/netcdf-c/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="libcurl, openssl, libnghttp2"
+TERMUX_PKG_SHA256=4ee8d5f6b50a1eb4ad4c10f24531e36261fd1882410fb08435eb2ddfd49a0908
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libcurl, zlib"
 TERMUX_PKG_BREAKS="netcdf-c-dev"
 TERMUX_PKG_REPLACES="netcdf-c-dev"
 TERMUX_PKG_GROUPS="science"
@@ -23,4 +23,8 @@ termux_step_post_get_source() {
 	if [ ! "${e}" ] || [ "${_SOVERSION}" != "$(( "${e}" ))" ]; then
 		termux_error_exit "SOVERSION guard check failed."
 	fi
+}
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-execinfo"
 }
