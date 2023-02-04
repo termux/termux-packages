63c0fe905f241d2333349b03e7a798e236c1a33f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-04 17:48:16 +0900

    python-apt: Bump to 2.5.2

--- packages/python-apt/build.sh
+++ packages/python-apt/build.sh
@@ -2,10 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://apt-team.pages.debian.net/python-apt/
 TERMUX_PKG_DESCRIPTION="Python bindings for APT"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.5.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.5.2
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/p/python-apt/python-apt_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=07ece069cdc9f5523a405f46ab5437260dca5e34909601c4540f160c476bb982
+TERMUX_PKG_SHA256=b852011da60c261bd2aedc8b8e7b8a1259e6aa974245d4b8d02b0f7adb5c33f2
 TERMUX_PKG_DEPENDS="apt, build-essential, libc++, python, texinfo"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
+
+termux_step_pre_configure() {
+	export DEBVER="${TERMUX_PKG_VERSION#*:}"
+}
