c30b284c32faef7f42f3f0e05e90083ef5553dfb
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-05 11:54:33 +0900

    htop: Bump to 3.2.2

--- packages/htop/build.sh
+++ packages/htop/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://htop.dev/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.2.1
+TERMUX_PKG_VERSION=3.2.2
 TERMUX_PKG_SRCURL=https://github.com/htop-dev/htop/archive/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b5ffac1949a8daaabcffa659c0964360b5008782aae4dfa7702d2323cfb4f438
+TERMUX_PKG_SHA256=3829c742a835a0426db41bb039d1b976420c21ec65e93b35cd9bfd2d57f44ac8
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BREAKS="htop-legacy"
