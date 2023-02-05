8ef35fd85fb7ff464f0acfb1b4ee4ecee6aec3c8
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-06 04:13:06 +0900

    snapcast-server: Bump to 0.27.0

--- packages/snapcast-server/build.sh
+++ packages/snapcast-server/build.sh
@@ -2,17 +2,23 @@ TERMUX_PKG_HOMEPAGE=https://github.com/badaix/snapcast
 TERMUX_PKG_DESCRIPTION="A multiroom client-server audio player (server)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.26.0
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION=0.27.0
 TERMUX_PKG_SRCURL=https://github.com/badaix/snapcast/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=166353267a5c461a3a0e7cbd05d78c4bfdaebeda078801df3b76820b54f27683
+TERMUX_PKG_SHA256=c662c6eafbaa42a4797a4ed6ba4a7602332abf99f6ba6ea88ff8ae59978a86ba
 TERMUX_PKG_DEPENDS="libc++, libexpat, libflac, libopus, libsoxr, libvorbis"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DANDROID_NO_TERMUX=OFF
 -DBUILD_TESTS=OFF
 -DBoost_INCLUDE_DIR=$TERMUX_PREFIX/include
 "
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_post_get_source() {
+	# Future-proof way of pretending not to be Android.
+	find . -name CMakeLists.txt | xargs -n 1 \
+		sed -i -E 's/if\s*\((.*\s|)ANDROID/\0_NO_TERMUX/g'
+}
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
