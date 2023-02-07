e55fe31d291ba1dee0c17b6d61ddff42966669a3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 03:13:37 +0900

    termux_setup_nodejs: Bump Node.js to 18.14.0

--- scripts/build/setup/termux_setup_nodejs.sh
+++ scripts/build/setup/termux_setup_nodejs.sh
@@ -1,6 +1,6 @@
 termux_setup_nodejs() {
 	# Use LTS version for now
-	local NODEJS_VERSION=16.18.1
+	local NODEJS_VERSION=18.14.0
 	local NODEJS_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
@@ -15,7 +15,7 @@ termux_setup_nodejs() {
 			local NODEJS_TAR_FILE=$TERMUX_PKG_TMPDIR/nodejs-$NODEJS_VERSION.tar.xz
 			termux_download https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz \
 				"$NODEJS_TAR_FILE" \
-				de2c694e7081c37022817d27a65b02f69ecf4c49699d65585e8e24431b7bc920
+				1ccec74b6240fce8754813e31fdbc93ad520df2e814729cea29efe9075e48350
 			tar -xf "$NODEJS_TAR_FILE" -C "$NODEJS_FOLDER" --strip-components=1
 		fi
 		export PATH=$NODEJS_FOLDER/bin:$PATH
