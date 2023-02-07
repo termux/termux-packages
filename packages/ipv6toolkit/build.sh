0b8689e233eecacbce22a36ffefcd36f622a2ac7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-07 21:33:53 +0900

    ipv6toolkit: Fix manpath

--- packages/ipv6toolkit/build.sh
+++ packages/ipv6toolkit/build.sh
@@ -4,8 +4,9 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _COMMIT=367bbe60489a6ae68898b9c81e672b48ad81df43
 TERMUX_PKG_VERSION=2022.09.30
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=git+https://github.com/fgont/ipv6toolkit
+TERMUX_PKG_SHA256=56afd0b9a4c749b502b86ac640f21121aabc734afbfd49db8eb1feea90d4edbb
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libpcap, resolv-conf"
 TERMUX_PKG_RECOMMENDS="curl, perl"
@@ -21,6 +22,11 @@ termux_step_post_get_source() {
 		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
 		echo " is different from what is expected to be: \"$version\""
 		return 1
+	fi
+
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		termux_error_exit "Checksum mismatch for source files."
 	fi
 }
 
