79f787b773755d5e8673c170aeb5d969557eb2ad
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 21:17:39 +0900

    sdl2-mixer: Bump to 2.6.3

--- x11-packages/sdl2-mixer/build.sh
+++ x11-packages/sdl2-mixer/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A simple multi-channel audio mixer"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.2
+TERMUX_PKG_VERSION=2.6.3
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_mixer/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_mixer-${TERMUX_PKG_VERSION}.tar.gz 
-TERMUX_PKG_SHA256=8cdea810366decba3c33d32b8071bccd1c309b2499a54946d92b48e6922aa371
+TERMUX_PKG_SHA256=7a6ba86a478648ce617e3a5e9277181bc67f7ce9876605eea6affd4a0d6eea8f
 TERMUX_PKG_DEPENDS="fluidsynth, libflac, libmodplug, libvorbis, mpg123, opusfile, sdl2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-music-mod-modplug-shared
