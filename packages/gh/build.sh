bfa4443a61b81a51bb3f4066fba4e96d1ec3657c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 19:44:03 +0900

    gh: Bump to 2.23.0

--- packages/gh/build.sh
+++ packages/gh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://cli.github.com/
 TERMUX_PKG_DESCRIPTION="GitHub’s official command line tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION="2.22.1"
+TERMUX_PKG_VERSION=2.23.0
 TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=09cdd1c435d453a0c610f407979ecf8d314aec41d7b8004794f136f05b0fe688
+TERMUX_PKG_SHA256=1e9f92a47caa92efedc06b22cfe9c951c5163c4a9bc60a45d477fd5d9b592e54
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
