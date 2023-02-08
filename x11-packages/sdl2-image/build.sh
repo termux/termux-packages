63ea96882e141a2e4743dd73c2aba9a079d92c3f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 21:16:37 +0900

    sdl2-image: Bump to 2.6.3

--- x11-packages/sdl2-image/build.sh
+++ x11-packages/sdl2-image/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="A simple library to load images of various formats as SD
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=2.6.3
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_image/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_image-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=48355fb4d8d00bac639cd1c4f4a7661c4afef2c212af60b340e06b7059814777
+TERMUX_PKG_SHA256=931c9be5bf1d7c8fae9b7dc157828b7eee874e23c7f24b44ba7eff6b4836312c
 TERMUX_PKG_DEPENDS="libjpeg-turbo, libjxl, libpng, libtiff, libwebp, sdl2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-stb-image
