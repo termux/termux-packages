70fc7391e22f3e59bd639516efbb9c73088035ba
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 12:06:13 +0900

    libaom: Bump to 3.6.0

--- packages/libaom/build.sh
+++ packages/libaom/build.sh
@@ -3,13 +3,21 @@ TERMUX_PKG_DESCRIPTION="AV1 Video Codec Library"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE, PATENTS"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.5.0
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_VERSION=3.6.0
 TERMUX_PKG_SRCURL=git+https://aomedia.googlesource.com/aom
+TERMUX_PKG_SHA256=051113f4f8b1e117ccf7414c796d680b67ba99e52cda741d58f7b7c82c71c419
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_SHARED_LIBS=ON
 -DCMAKE_INSTALL_LIBDIR=lib
 "
+
+termux_step_post_get_source() {
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
 
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
