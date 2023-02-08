e891aed7a926986572d2c915835a6c545dbcafb3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 23:31:55 +0900

    docker-compose: Bump to 2.16.0

--- root-packages/docker-compose/build.sh
+++ root-packages/docker-compose/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/docker/compose
 TERMUX_PKG_DESCRIPTION="Compose is a tool for defining and running multi-container Docker applications."
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.15.1
+TERMUX_PKG_VERSION=2.16.0
 TERMUX_PKG_SRCURL="https://github.com/docker/compose/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=8ace5441826c6f07f5aa12c1864f73d30a362703492d9ed4b2e2314d3b353b1b
+TERMUX_PKG_SHA256=556dc59075280442128f5b45a8ff37638fb357c2a956bd751dd0ba747c93e71d
 TERMUX_PKG_DEPENDS=docker
 
 termux_step_make() {
