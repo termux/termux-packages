71e36f9e9ad9618e1562e23804114420d3926f96
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-08 19:39:34 +0900

    kcptun: Bump to 20230207

--- packages/kcptun/build.sh
+++ packages/kcptun/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xtaci/kcptun
 TERMUX_PKG_DESCRIPTION="A Stable & Secure Tunnel based on KCP with N:M multiplexing and FEC"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="20221015"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=20230207
 TERMUX_PKG_SRCURL=https://github.com/xtaci/kcptun/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5163d439473252cdbfb870d8ebb322951dc047f3bad7ba93096ce1a953258e3a
+TERMUX_PKG_SHA256=09054b52d5799a8e47edb36f2db335d929d5bbb63a26f7ba2fe03c64ec39d550
 # Depend on its subpackages.
 TERMUX_PKG_DEPENDS="kcptun-client, kcptun-server"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -13,8 +12,7 @@ TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
-	VERSION=`date -u +%Y%m%d`
-	LDFLAGS="-X main.VERSION=$VERSION -s -w"
+	LDFLAGS="-X main.VERSION=${TERMUX_PKG_VERSION#*:} -s -w"
 	GCFLAGS=""
 
 	go build -mod=vendor -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcptun-client github.com/xtaci/kcptun/client
