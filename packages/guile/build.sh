2fbd910af840b6d02f13442c215f0d63cdbae4e5
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-09 13:40:50 +0900

    guile: Fix path to `pkg-config` in `guile-config`

--- packages/guile/build.sh
+++ packages/guile/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Portable, embeddable Scheme implementation written in C"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.0.9
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/guile/guile-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=18525079ad29a0d46d15c76581b5d91c8702301bfd821666d2e1d13726162811
 TERMUX_PKG_DEPENDS="libandroid-spawn, libandroid-support, libffi, libgc, libgmp, libiconv, libunistring, ncurses, readline"
@@ -38,12 +38,11 @@ termux_step_pre_configure() {
 	export LD_LIBRARY_PATH="$TERMUX_PKG_HOSTBUILD_DIR"/HOSTBUILDINSTALL/lib
 
 	export CC_FOR_BUILD="gcc -m${TERMUX_ARCH_BITS}"
+
+	# Value of PKG_CONFIG becomes hardcoded in bin/*-config
+	export PKG_CONFIG=pkg-config
 }
 
 termux_step_post_configure() {
 	cp $TERMUX_PKG_BUILDER_DIR/malloc.h $TERMUX_PKG_BUILDDIR/lib/
-}
-
-termux_step_post_massage() {
-	sed -i 's',"$TERMUX_STANDALONE_TOOLCHAIN/bin/$TERMUX_HOST_PLATFORM-pkg-config","$TERMUX_PREFIX/bin/pkg-config",'g' $TERMUX_PKG_MASSAGEDIR$TERMUX_PREFIX/bin/guile-config
 }
