249cb9fe058d597fdb3b5989b0567caea6824dfa
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-11 13:22:43 +0900

    libarrow-cpp: Depend on libandroid-execinfo

--- packages/libarrow-cpp/build.sh
+++ packages/libarrow-cpp/build.sh
@@ -4,9 +4,10 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Align the version with `libarrow-python` package.
 TERMUX_PKG_VERSION=11.0.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/apache/arrow/archive/refs/tags/apache-arrow-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4a8c0c3d5b39ca81f4a636a41863f1cf5e0ed199f994bf5ead0854ca037eb741
-TERMUX_PKG_DEPENDS="libc++, libre2, utf8proc"
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libc++, libre2, utf8proc"
 TERMUX_PKG_BUILD_DEPENDS="rapidjson"
 TERMUX_PKG_BREAKS="libarrow-python (<< ${TERMUX_PKG_VERSION})"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -24,16 +25,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+="/cpp"
 
-	_NEED_DUMMY_LIBRT_A=
-	_LIBRT_A=$TERMUX_PREFIX/lib/librt.a
-	if [ ! -e $_LIBRT_A ]; then
-		_NEED_DUMMY_LIBRT_A=true
-		echo '!<arch>' > $_LIBRT_A
-	fi
-}
-
-termux_step_post_make_install() {
-	if [ $_NEED_DUMMY_LIBRT_A ]; then
-		rm -f $_LIBRT_A
-	fi
+	LDFLAGS+=" -landroid-execinfo"
 }
