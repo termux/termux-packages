6c1624ded9678c50b4f11d3d966e2832dbeb4b82
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-11 15:01:37 +0900

    poke: Fix build when libandroid-glob is installed
    %ci:no-build

--- packages/poke/build.sh
+++ packages/poke/build.sh
@@ -7,19 +7,9 @@ TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/poke/poke-${TERMUX_PKG_VERSION}
 TERMUX_PKG_SHA256=79a9b2f33c9f8c327c499afadaeeabfeecf6ad4988924d2c6c6f317e50317add
 TERMUX_PKG_DEPENDS="gettext, libgc, ncurses, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-sysroot=$(dirname $TERMUX_PREFIX) --disable-threads --disable-hserver"
-
-termux_step_pre_configure() {
-	_NEED_DUMMY_LIBPTHREAD_A=
-	_LIBPTHREAD_A=$TERMUX_PREFIX/lib/libpthread.a
-	if [ ! -e $_LIBPTHREAD_A ]; then
-		_NEED_DUMMY_LIBPTHREAD_A=true
-		echo '!<arch>' > $_LIBPTHREAD_A
-	fi
-}
-
-termux_step_post_make_install() {
-	if [ $_NEED_DUMMY_LIBPTHREAD_A ]; then
-		rm -f $_LIBPTHREAD_A
-	fi
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_header_glob_h=no
+--disable-hserver
+--disable-threads
+--with-sysroot=$TERMUX_BASE_DIR
+"
