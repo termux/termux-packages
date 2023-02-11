ccfa4b67d058f9d9a836c2b6a1956fc6c49bd40a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-11 13:36:12 +0900

    less: Add patch for CVE-2022-46663

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a te
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=608
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a69abe2e0a126777e021d3b73aa3222e1b261f10e64624d41ec079685a6ac209
 TERMUX_PKG_DEPENDS="ncurses, pcre2"
