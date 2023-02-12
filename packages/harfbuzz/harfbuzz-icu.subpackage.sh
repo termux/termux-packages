575fe22979e13f151533ee422f932d8e96aa115b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-12 16:04:33 +0900

    harfbuzz: Bump to 7.0.0

--- packages/harfbuzz/harfbuzz-icu.subpackage.sh
+++ packages/harfbuzz/harfbuzz-icu.subpackage.sh
@@ -1,3 +1,9 @@
-TERMUX_SUBPKG_INCLUDE="lib/libharfbuzz-icu*"
+TERMUX_SUBPKG_INCLUDE="
+include/harfbuzz/hb-icu.h
+lib/libharfbuzz-icu*
+lib/pkgconfig/harfbuzz-icu.pc
+"
 TERMUX_SUBPKG_DESCRIPTION="OpenType text shaping engine ICU backend"
 TERMUX_SUBPKG_DEPENDS="libicu"
+TERMUX_SUBPKG_BREAKS="harfbuzz (<< 7.0.0)"
+TERMUX_SUBPKG_REPLACES="harfbuzz (<< 7.0.0)"
