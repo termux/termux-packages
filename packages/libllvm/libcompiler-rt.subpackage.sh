716bf76852f3f85ba41da5abe2600b08b125afdf
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-02-12 17:55:41 +0800

    libcompiler-rt: Revert "libcompiler-rt: add trigger for llvm-mingw-w64"
    This reverts commit 7113d60ab6def680a5839646936cdca751c14479.

--- packages/libllvm/libcompiler-rt.subpackage.sh
+++ packages/libllvm/libcompiler-rt.subpackage.sh
@@ -16,20 +16,13 @@ TERMUX_SUBPKG_CONFLICTS="ndk-multilib (<< 23b-6)"
 termux_step_create_subpkg_debscripts() {
 	local RT_OPT_DIR=$TERMUX_PREFIX/opt/ndk-multilib/cross-compiler-rt
 	local RT_PATH=$TERMUX_PREFIX/lib/clang/$TERMUX_PKG_VERSION/lib/linux
-	local LLVM_MINGW_RT_OPT_DIR=$TERMUX_PREFIX/opt/llvm-mingw-w64/opt/compiler-rt
-	local LLVM_MINGW_RT_PATH=$TERMUX_PREFIX/lib/clang/$TERMUX_PKG_VERSION/lib/windows
 
 	cat <<- EOF > ./triggers
-	interest-noawait $RT_OPT_DIR $LLVM_MINGW_RT_OPT_DIR
+	interest-noawait $RT_OPT_DIR
 	EOF
 
 	cat <<- EOF > ./postinst
 	#!$TERMUX_PREFIX/bin/sh
-	if [ "\$1" = "triggered" -a "\$2" = "$LLVM_MINGW_RT_OPT_DIR" ]; then
-	    ln -sf $LLVM_MINGW_RT_OPT_DIR/windows $LLVM_MINGW_RT_PATH
-	    exit 0
-	fi
-
 	find $RT_PATH -type l ! -name "libclang_rt*$TERMUX_ARCH*" -exec rm -rf "{}" \;
 	if [ -e $RT_OPT_DIR ]; then
 	    find $RT_OPT_DIR -type f ! -name "libclang_rt*$TERMUX_ARCH*" -exec ln -sf "{}" $RT_PATH \;
@@ -39,11 +32,6 @@ termux_step_create_subpkg_debscripts() {
 
 	cat <<- EOF > ./prerm
 	#!$TERMUX_PREFIX/bin/sh
-	if [ "\$1" = "triggered" -a "\$2" = "$LLVM_MINGW_RT_OPT_DIR" ]; then
-	    rm -f $LLVM_MINGW_RT_PATH
-	    exit 0
-	fi
-
 	find $RT_PATH -type l ! -name "libclang_rt*$TERMUX_ARCH*" -exec rm -rf "{}" \;
 	exit 0
 	EOF
