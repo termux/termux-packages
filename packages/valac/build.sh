2d6cb4b7d33926681bc5a08b8869496ce6f8d68e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-12 16:36:17 +0900

    valac: Bump to 0.56.4

--- packages/valac/build.sh
+++ packages/valac/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/Vala
 TERMUX_PKG_DESCRIPTION="C# like language for the GObject system"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.56.3
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/vala/${TERMUX_PKG_VERSION:0:4}/vala-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=e1066221bf7b89cb1fa7327a3888645cb33b604de3bf45aa81132fd040b699bf
+_MAJOR_VERSION=0.56
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/vala/${_MAJOR_VERSION}/vala-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=862c41d938543ed3d8d86c8219a61087797193defee8da0c50caf49993c66b6a
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_RECOMMENDS="clang, pkg-config"
 TERMUX_PKG_BREAKS="valac-dev"
@@ -13,6 +13,6 @@ TERMUX_PKG_REPLACES="valac-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-cgraph=no"
 
 termux_step_post_make_install() {
-	cd ${TERMUX_PREFIX}/lib
-	ln -s -f vala-${TERMUX_PKG_VERSION:0:4}/libvalaccodegen.so libvalaccodegen.so
+	local v=$(echo ${TERMUX_PKG_VERSION#*:} | cut -d . -f 1-2)
+	ln -sf vala-${v}/libvalaccodegen.so $TERMUX_PREFIX/lib/libvalaccodegen.so
 }
