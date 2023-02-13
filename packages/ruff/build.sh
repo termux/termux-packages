f65eb6c69da661b3121a8b805b0a3b513331f477
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-13 17:08:54 +0900

    ruff: Bump to 0.0.246

--- packages/ruff/build.sh
+++ packages/ruff/build.sh
@@ -3,13 +3,23 @@ TERMUX_PKG_DESCRIPTION="An extremely fast Python linter, written in Rust"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.0.245"
+TERMUX_PKG_VERSION=0.0.246
 TERMUX_PKG_SRCURL="https://github.com/charliermarsh/ruff/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=beedf7cf9ea0d128aabee957a4293e4f9241ba3fe5de5a79762028ea1b6a7727
+TERMUX_PKG_SHA256=07ee2dc861cad70503a4d8cd7923e6905eaba8a750b7bf435fcc933b3f9d12c0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+="/crates/ruff_cli"
 	TERMUX_PKG_BUILDDIR="${TERMUX_PKG_SRCDIR}"
+
+	cd $TERMUX_PKG_BUILDDIR
+	rm -rf _lib
+	mkdir -p _lib
+	cd _lib
+	$CC $CPPFLAGS $CFLAGS -fvisibility=hidden \
+		-c $TERMUX_PKG_BUILDER_DIR/ctermid.c 
+	$AR cru libctermid.a ctermid.o
+
+	RUSTFLAGS+=" -C link-arg=$TERMUX_PKG_BUILDDIR/_lib/libctermid.a"
 }
