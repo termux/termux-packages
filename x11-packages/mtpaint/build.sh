ce230c4622ffca6aa5e524082cb83532255eefda
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-14 11:21:25 +0900

    mtpaint: Migrate to GTK+3
    and correctly bump to 3.50.09.

--- x11-packages/mtpaint/build.sh
+++ x11-packages/mtpaint/build.sh
@@ -2,16 +2,34 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/mtpaint/
 TERMUX_PKG_DESCRIPTION="Simple paint program for creating icons and pixel based artwork"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.50.09
-TERMUX_PKG_REVISION=5
-_COMMIT=5272e2b1e773c8e02ac3506b2d3bde82ad946b21
+_COMMIT=199472ad6a4ecee6c8583fb5a504a2e99712b4fc
+TERMUX_PKG_VERSION=1:3.50.09
 TERMUX_PKG_SRCURL=https://github.com/wjaguar/mtPaint/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=6ed73fc7ac18499c7e83c8077c85dfc0bf778f0663c051515da408e2fcfea512
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, giflib, glib, gtk2, harfbuzz, libandroid-glob, libcairo, libiconv, libjpeg-turbo, libpng, libtiff, libwebp, libx11, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxinerama, libxrandr, libxrender, littlecms, openjpeg, pango, zlib"
+TERMUX_PKG_SHA256=a5bcf9e98959506817cf39aafcf07a0403a4aab405031a46603f0eb7ca80f028
+TERMUX_PKG_DEPENDS="atk, freetype, gdk-pixbuf, glib, gtk3, harfbuzz, libandroid-glob, libcairo, libiconv, libjpeg-turbo, libpng, libtiff, libwebp, libx11, littlecms, openjpeg, pango, zlib"
 TERMUX_PKG_RECOMMENDS="gifsicle"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=${TERMUX_PREFIX}/share/man man GIF jpeg tiff cflags lcms2"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--prefix=$TERMUX_PREFIX
+cflags
+gtk3
+jpeg
+lcms2
+man
+tiff
+"
+
+termux_step_post_get_source() {
+	local v=$(sed -En 's/^MT_V="([^"]+)".*/\1/p' configure)
+	if [ "${v}" != "${TERMUX_PKG_VERSION#*:}" ]; then
+		termux_error_exit "Version string '$TERMUX_PKG_VERSION' does not seem to be correct."
+	fi
+}
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
+}
+
+termux_step_configure() {
+	sh $TERMUX_PKG_SRCDIR/configure ${TERMUX_PKG_EXTRA_CONFIGURE_ARGS}
 }
