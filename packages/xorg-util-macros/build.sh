a061d05d3d984856dee1cdd50f1b64f7229ddf62
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-14 20:48:25 +0900

    xorg-util-macros: Bump to 1.20.0

--- packages/xorg-util-macros/build.sh
+++ packages/xorg-util-macros/build.sh
@@ -1,12 +1,11 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X.Org Autotools macros"
-# License: HPND
+# Licenses: HPND, MIT
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.19.3
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/util/util-macros-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=0f812e6e9d2786ba8f54b960ee563c0663ddbe2434bf24ff193f5feab1f31971
+TERMUX_PKG_VERSION=1.20.0
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/util/util-macros-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=0b86b262dbe971edb4ff233bc370dfad9f241d09f078a3f6d5b7f4b8ea4430db
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
