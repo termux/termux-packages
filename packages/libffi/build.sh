e5d8e4976ea3a23fbd69c1ed9e692b580962049e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-16 20:03:20 +0900

    libffi: Add SOVERSION guard
    %ci:no-build

--- packages/libffi/build.sh
+++ packages/libffi/build.sh
@@ -10,6 +10,18 @@ TERMUX_PKG_REPLACES="libffi-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-multi-os-directory"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/libffi-${TERMUX_PKG_VERSION}/include"
 
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=8
+
+	local e=$(sed -En 's/^[^0-9#]*([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
+			libtool-version)
+	if [ ! "${e}" ] || [ "${_SOVERSION}" != "$(( "${e}" ))" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
+
 termux_step_post_configure() {
 	# work around since mmap can't be written and marked executable in android anymore from userspace
 	echo "#define FFI_MMAP_EXEC_WRIT 1" >> fficonfig.h
