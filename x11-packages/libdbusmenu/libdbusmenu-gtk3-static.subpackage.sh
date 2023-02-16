d143531f2b0af089240fa00d3cec9dbb9bea9380
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-16 22:17:27 +0900

    new package: libdbusmenu

---
+++ x11-packages/libdbusmenu/libdbusmenu-gtk3-static.subpackage.sh
@@ -0,0 +1,6 @@
+TERMUX_SUBPKG_INCLUDE="
+lib/libdbusmenu-gtk3*.a
+lib/libdbusmenu-gtk3*.la
+"
+TERMUX_SUBPKG_DESCRIPTION="Static libraries for libdbusmenu-gtk3"
+TERMUX_SUBPKG_DEPENDS="libdbusmenu-gtk3"
