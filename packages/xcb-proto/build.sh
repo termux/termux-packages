0b0610c4f5742efdd023b9365f606144c7b93763
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-16 21:13:43 +0900

    xcb-proto: Have python-xcbgen depend on python
    I'm aware this is an abuse of `TERMUX_PKG_ANTI_BUILD_DEPENDS`.

--- packages/xcb-proto/build.sh
+++ packages/xcb-proto/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_DESCRIPTION="XML-XCB protocol descriptions"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.15.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=7072beb1f680a2fe3f9e535b797c146d22528990c72f63ddb49d2f350a3653ed
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -14,3 +14,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 PYTHON=python${TERMUX_PYTHON_VERSION}
 am_cv_python_pythondir=$TERMUX_PYTHON_HOME/site-packages
 "
+
+# Without this python-xcbgen cannot depend on python due to circular dependency.
+TERMUX_PKG_ANTI_BUILD_DEPENDS="python"
