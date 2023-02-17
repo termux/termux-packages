b15af4a9965426f3bbe3f7b608071306fe80e60b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-17 20:58:39 +0900

    libprotobuf: Bump to 22.0
    Revdep rebuild follows.

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -1,32 +1,30 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/protocolbuffers/protobuf
 TERMUX_PKG_DESCRIPTION="Protocol buffers C++ library"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
+# utf8_range is licensed under MIT
+TERMUX_PKG_LICENSE="BSD 3-Clause, MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-# Please as well update SHA256 checksum for $_PROTOBUF_ZIP in
-#   $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
-# when bumping version.
-TERMUX_PKG_VERSION=2:21.12
+# When bumping version:
+# - update SHA256 checksum for $_PROTOBUF_ZIP in
+#     $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
+# - ALWAYS bump revision of reverse dependencies and rebuild them.
+TERMUX_PKG_VERSION=2:22.0
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION#*:}.tar.gz
-TERMUX_PKG_SHA256=22fdaf641b31655d4b2297f9981fa5203b2866f8332d3c6333f6b0107bb320de
+TERMUX_PKG_SHA256=782160a6eae4bddfa4061ff5f7dcf04c9ed1494a0f0c6408b4af6b4a31ab9876
 TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="libc++, zlib"
+TERMUX_PKG_DEPENDS="abseil-cpp, libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev, protobuf-static (<< ${TERMUX_PKG_VERSION#*:})"
 TERMUX_PKG_REPLACES="libprotobuf-dev"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dprotobuf_ABSL_PROVIDER=package
 -Dprotobuf_BUILD_TESTS=OFF
 -DBUILD_SHARED_LIBS=ON
 -DCMAKE_INSTALL_LIBDIR=lib
 "
+TERMUX_PKG_NO_STATICSPLIT=true
 
-termux_step_post_get_source() {
-	# Do not forget to bump revision of reverse dependencies and rebuild them
-	# after SOVERSION is changed.
-	local _SOVERSION=32
-
-	local v="$(sed -E -n 's/^\s*SOVERSION\s+([0-9]+)\s*$/\1/p' \
-			cmake/libprotobuf.cmake)"
-	if [ "${_SOVERSION}" != "${v}" ]; then
-		termux_error_exit "Error: SOVERSION guard check failed."
-	fi
+termux_step_post_make_install() {
+	install -Dm600 -t $TERMUX_PREFIX/share/doc/libutf8-range \
+		$TERMUX_PKG_SRCDIR/third_party/utf8_range/LICENSE
 }
---
+++ packages/libprotobuf/interface_link_libraries.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+$PKG_CONFIG --libs \
+	absl_check \
+	absl_log \
+	absl_algorithm \
+	absl_base \
+	absl_bind_front \
+	absl_bits \
+	absl_btree \
+	absl_cleanup \
+	absl_cord \
+	absl_core_headers \
+	absl_debugging \
+	absl_die_if_null \
+	absl_dynamic_annotations \
+	absl_flags \
+	absl_flat_hash_map \
+	absl_flat_hash_set \
+	absl_function_ref \
+	absl_hash \
+	absl_layout \
+	absl_log_initialize \
+	absl_log_severity \
+	absl_memory \
+	absl_node_hash_map \
+	absl_node_hash_set \
+	absl_optional \
+	absl_span \
+	absl_status \
+	absl_statusor \
+	absl_strings \
+	absl_synchronization \
+	absl_time \
+	absl_type_traits \
+	absl_utility \
+	absl_variant
