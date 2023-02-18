f09d27bee0228a0b8266f9d48a679bbe1e6f474f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-18 23:51:22 +0900

    abseil-cpp: Specify TERMUX_PKG_CONFLICTS
    with current libgrpc.

--- packages/abseil-cpp/build.sh
+++ packages/abseil-cpp/build.sh
@@ -4,9 +4,11 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Do not forget to rebuild revdeps along with EVERY "major" version bump.
 TERMUX_PKG_VERSION=20230125.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/abseil/abseil-cpp/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3ea49a7d97421b88a8c48a0de16c16048e17725c7ec0f1d3ea2683a2a75adc21
 TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_CONFLICTS="libgrpc (<< 1.52.0-1)"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_SHARED_LIBS=ON
 "
