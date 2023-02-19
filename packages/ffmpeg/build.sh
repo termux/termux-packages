9ab6495113b722baa281da77e8165f83615a86bf
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-20 07:43:05 +0900

    ffmpeg: Bump to 6.0

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://ffmpeg.org
 TERMUX_PKG_DESCRIPTION="Tools and libraries to manipulate a wide range of multimedia formats and protocols"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.1.2
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_VERSION=6.0
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=619e706d662c8420859832ddc259cd4d4096a48a2ce1eefd052db9e440eef3dc
+TERMUX_PKG_SHA256=57be87c22d9b49c112b6d24bc67d42508660e6b718b3db89c44e47e289137082
 TERMUX_PKG_DEPENDS="freetype, game-music-emu, libaom, libandroid-glob, libass, libbluray, libbz2, libdav1d, libgnutls, libiconv, liblzma, libmp3lame, libopus, librav1e, libsoxr, libssh, libtheora, libvorbis, libvpx, libvidstab, libwebp, libx264, libx265, libxml2, libzimg, littlecms, ocl-icd, xvidcore, zlib"
 TERMUX_PKG_BUILD_DEPENDS="opencl-headers"
 TERMUX_PKG_CONFLICTS="libav"
@@ -15,9 +14,9 @@ TERMUX_PKG_REPLACES="ffmpeg-dev"
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed. (These variables are also used afterwards.)
-	_FFMPEG_SOVER_avutil=57
-	_FFMPEG_SOVER_avcodec=59
-	_FFMPEG_SOVER_avformat=59
+	_FFMPEG_SOVER_avutil=58
+	_FFMPEG_SOVER_avcodec=60
+	_FFMPEG_SOVER_avformat=60
 
 	local f
 	for f in util codec format; do
