6b7f12243dad78226a1a1bf599ba4c643c53ef03
Twaik Yont <twaikyont@gmail.com>
2023-02-22 22:14:54 +0200

    xorg-server, xorg-server-xvfb, xwayland: bumping version.

--- x11-packages/xf86-video-dummy/build.sh
+++ x11-packages/xf86-video-dummy/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 # Please, do not update it to 0.4.0.
 TERMUX_PKG_VERSION=0.3.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/driver/xf86-video-dummy-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ee5ad51e80c8cc90d4c76ac3dec2269a3c769f4232ed418b29d60d618074631b
 TERMUX_PKG_DEPENDS="xorg-server"
