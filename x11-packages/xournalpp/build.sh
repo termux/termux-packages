20740a53db47df0df6b295179047bbde18158a01
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-23 05:11:27 +0900

    poppler: Revbump revdeps to rebuild

--- x11-packages/xournalpp/build.sh
+++ x11-packages/xournalpp/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE, copyright.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xournalpp/xournalpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5021c7a8f53687edb3a759e079c54d23d5918d83f8d1f5fcd08a1da2ce81bd8b
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libandroid-execinfo, libc++, libcairo, librsvg, libsndfile, libx11, libxi, libxml2, libzip, pango, poppler, portaudio, zlib"
