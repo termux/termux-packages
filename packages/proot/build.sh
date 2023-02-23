a2e8882f0ed1b06b43ce26253fa88895d063550a
Michal Bednarski <bednarski.michal2@gmail.com>
2023-02-23 19:10:12 +0100

    proot: Update package
    termux/proot#258

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
 # Just bump commit and version when needed:
-_COMMIT=f1dd76b86d3a4efb09885bb42de6237c96cc2731
+_COMMIT=d4658fcfc30d9dfddbfb66e95865fd84bf684abe
 TERMUX_PKG_VERSION=5.1.107
-TERMUX_PKG_REVISION=56
+TERMUX_PKG_REVISION=57
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=32a1c05d5e4b1b9f5203106e97cf6de7e249e0cfdd495d3378306a342b497e2d
+TERMUX_PKG_SHA256=49b88f84d7efc2825b352fea2b5c4ab23831aa8f17b65959b0b4c46a263b5111
 TERMUX_PKG_DEPENDS="libtalloc"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="-C src"
