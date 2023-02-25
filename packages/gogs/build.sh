6c7d7255308ebe9337c2bd1297c7634c53261728
Medzik <medzik@duck.com>
2023-02-25 23:48:25 +0100

    gogs: update to 0.13.0

--- packages/gogs/build.sh
+++ packages/gogs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gogs.io
 TERMUX_PKG_DESCRIPTION="A painless self-hosted Git service"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Injamul Mohammad Mollah <mrinjamul@gmail.com>"
-TERMUX_PKG_VERSION="0.12.11"
+TERMUX_PKG_VERSION="0.13.0"
 TERMUX_PKG_SRCURL=https://github.com/gogs/gogs/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b64814ab166071da1fffb19cdebd8e14102e3fb283662389482e4aa7c12deae4
+TERMUX_PKG_SHA256=59a8c4349ed104ccd44985e940a6cdb25fca1a6019212e7f65b30f1252f627ce
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="dash, git"
 TERMUX_PKG_CONFFILES="etc/gogs/app.ini"
@@ -31,7 +31,7 @@ termux_step_make() {
 	LDFLAGS+=" -X gogs.io/gogs/internal/conf.AppWorkPath=$TERMUX_PREFIX/var/lib/gogs"
 	LDFLAGS+=" -X gogs.io/gogs/internal/conf.CustomPath=$TERMUX_PREFIX/var/lib/gogs"
 
-	PATH=$PATH:$TERMUX_PKG_HOSTBUILD_DIR/bin GOGS_VERSION=v"$TERMUX_PKG_VERSION" TAGS="bindata sqlite" make all
+	PATH=$PATH:$TERMUX_PKG_HOSTBUILD_DIR/bin go build -ldflags "${LDFLAGS}" -tags "bindata sqlite" -trimpath -o gogs
 }
 
 termux_step_make_install() {
