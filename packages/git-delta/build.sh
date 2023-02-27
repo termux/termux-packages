3880d4c0e351782eb293c38d71af551c9e7a0fed
Tee KOBAYASHI <xtkoba@gmail.com>
2023-02-27 12:57:29 +0900

    git-delta: Use system libgit2
    and oniguruma.

--- packages/git-delta/build.sh
+++ packages/git-delta/build.sh
@@ -3,13 +3,18 @@ TERMUX_PKG_DESCRIPTION="A syntax-highlighter for git and diff output"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.15.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dandavison/delta/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b9afd2f80ae1d57991a19832d6979c7080a568d42290a24e59d6a2a82cbc1728
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="git, zlib"
+TERMUX_PKG_DEPENDS="git, libgit2, oniguruma"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	export LIBGIT2_SYS_USE_PKG_CONFIG=1
+	export RUSTONIG_SYSTEM_LIBONIG=1
+	export PKG_CONFIG_ALLOW_CROSS=1
+
 	rm -f Makefile release.Makefile
 	export CC_x86_64_unknown_linux_gnu=gcc
 	export CFLAGS_x86_64_unknown_linux_gnu="-O2"
@@ -17,21 +22,6 @@ termux_step_pre_configure() {
 	local _ORIG_CFLAGS="$CFLAGS"
 	termux_setup_rust
 	export CFLAGS="$_ORIG_CFLAGS"
-
-	: "${CARGO_HOME:=$HOME/.cargo}"
-	export CARGO_HOME
-
-	cargo fetch --target "${CARGO_TARGET_NAME}"
-
-	local _patch=$TERMUX_SCRIPTDIR/packages/libgit2/src-util-rand.c.patch
-	local d
-	for d in $CARGO_HOME/registry/src/github.com-*/libgit2-sys-*/libgit2; do
-		(
-			t=${d}/src/
-			cp $TERMUX_SCRIPTDIR/packages/libgit2/getloadavg.c ${t}
-			patch --silent -d ${t} < ${_patch}
-		) || :
-	done
 }
 
 termux_step_post_make_install() {
