52aba45f4981a41d2669045ff68070a13185adb5
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-01 08:46:27 +0900

    libtpms: Bump to 0.9.6

--- packages/libtpms/build.sh
+++ packages/libtpms/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/stefanberger/libtpms
 TERMUX_PKG_DESCRIPTION="Provides software emulation of a Trusted Platform Module (TPM 1.2 and TPM 2.0)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.5
+TERMUX_PKG_VERSION=0.9.6
 TERMUX_PKG_SRCURL=https://github.com/stefanberger/libtpms/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9522c69001e46a3b0e1ccd646d36db611b2366c395099d29037f2b067bf1bc60
+TERMUX_PKG_SHA256=2807466f1563ebe45fdd12dd26e501e8a0c4fbb99c7c428fbb508789efd221c0
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-openssl
