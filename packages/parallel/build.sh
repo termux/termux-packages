4583a4703d2c58087f1342020e6ef0116c537d5f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-02 19:52:58 +0900

    parallel: Bump to 20230222

--- packages/parallel/build.sh
+++ packages/parallel/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/parallel/
 TERMUX_PKG_DESCRIPTION="GNU Parallel is a shell tool for executing jobs in parallel using one or more machines"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20230122
+TERMUX_PKG_VERSION=20230222
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/parallel/parallel-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9c7c44070cca3118e72380086cb6cc3662be27c3e56b6ac93449cd4ef3edc204
+TERMUX_PKG_SHA256=6d36a5ea097668ddb780e74e2ff00252da163995a4b1e96ef633a99b209c980e
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
