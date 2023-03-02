de5aab4f50b1fffc485f6413b52a2f5fbde3c2ee
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-02 19:50:17 +0900

    poppler{,-qt}: Bump to 23.03.0

--- x11-packages/poppler-qt/build.sh
+++ x11-packages/poppler-qt/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_DESCRIPTION="Poppler Qt wrapper"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Please align the version with `poppler` package.
-TERMUX_PKG_VERSION=23.02.0
+TERMUX_PKG_VERSION=23.03.0
 # Do not forget to bump revision of reverse dependencies and rebuild them
 # when SOVERSION is changed.
 _POPPLER_SOVERSION=126
 TERMUX_PKG_SRCURL=https://poppler.freedesktop.org/poppler-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3315dda270fe2b35cf1f41d275948c39652fa863b90de0766f6b293d9a558fc9
+TERMUX_PKG_SHA256=b04148bf849c1965ada7eff6be4685130e3a18a84e0cce73bf9bc472ec32f2b4
 TERMUX_PKG_DEPENDS="libc++, poppler (>= ${TERMUX_PKG_VERSION}), qt5-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
 #texlive needs the xpdf headers
