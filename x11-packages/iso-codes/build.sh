7b774a039721c779d8131a7738264c964aa9bb2a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-02 20:57:02 +0900

    iso-codes: Bump to 4.13.0

--- x11-packages/iso-codes/build.sh
+++ x11-packages/iso-codes/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://salsa.debian.org/iso-codes-team/iso-codes
 TERMUX_PKG_DESCRIPTION="Lists of the country, language, and currency names"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.12.0
+TERMUX_PKG_VERSION=4.13.0
 TERMUX_PKG_SRCURL=https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/v${TERMUX_PKG_VERSION}/iso-codes-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ead53ac6cb000a726350eda10381b2339e7bed0680924d761d63f28f32da94b5
+TERMUX_PKG_SHA256=1925a2fe7d4b7aa2805ea7059cbede5c0788da2cc0f1a6fae17ae2b2b03337b6
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 #If you install ISO codes over a previous installed version, the install step will fail when creating some symlinks
