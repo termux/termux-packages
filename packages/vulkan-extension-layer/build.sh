73d0ad6e37e5ac147fa7c0d32ffa9022ef56dcea
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-03-02 15:38:58 +0800

    new package: vulkan-extension-layer

---
+++ packages/vulkan-extension-layer/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-ExtensionLayer
+TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+# This package and vulkan-headers should be updated at same time. Otherwise, they do not compile successfully.
+TERMUX_PKG_VERSION="1.3.241"
+TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-ExtensionLayer/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=aeb0710179b4f1fd3e880b1f3baa26f0c9810ba42be2cfd8be9ec36396e8312f
+TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
+TERMUX_PKG_BUILD_DEPENDS="vulkan-headers (=${TERMUX_PKG_VERSION})"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_TESTS=OFF
+-DVULKAN_HEADERS_INSTALL_DIR=$TERMUX_PREFIX
+"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
