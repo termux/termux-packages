07e2bceab362c3867e9224320622a6bc38b8e1a8
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-03 18:17:28 +0900

    cloudflared: Bump to 2023.3.0

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2023.2.1"
+TERMUX_PKG_VERSION=2023.3.0
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=468b0953eca6ab189859073067857062c91ed9adb18e12a43527e0dba3aa6409
+TERMUX_PKG_SHA256=90fad1f6eb59689e06d09837c5ec9a3e7e3d177fea15190027f60c5b9e7950be
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_GO_USE_OLDER=true
 
 termux_step_make() {
 	termux_setup_golang
