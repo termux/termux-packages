a2b9ed3e2e9918f367236f817dad10b97f88e648
Elliana May <me@mause.me>
2023-03-03 15:41:14 +0800

    libduckdb: move cmake flags to build.sh

--- packages/libduckdb/build.sh
+++ packages/libduckdb/build.sh
@@ -7,6 +7,12 @@ TERMUX_PKG_REVISION=1
 # we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
 TERMUX_PKG_SRCURL=git+https://github.com/duckdb/duckdb
 TERMUX_PKG_DEPENDS="libc++, openssl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_ICU_EXTENSION=TRUE 
+-DBUILD_PARQUET_EXTENSION=TRUE 
+-DBUILD_HTTPFS_EXTENSION=TRUE 
+-DBUILD_JSON_EXTENSION=TRUE
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
