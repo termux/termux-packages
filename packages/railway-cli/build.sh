f3b71f9164a66694123f2e9d07c2c3c714741b43
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-04 15:30:45 +0900

    railway-cli: Bump to 3.0.3

--- packages/railway-cli/build.sh
+++ packages/railway-cli/build.sh
@@ -2,20 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://railway.app
 TERMUX_PKG_DESCRIPTION="This is the command line interface for Railway"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.1.0"
+TERMUX_PKG_VERSION=3.0.3
 TERMUX_PKG_SRCURL=https://github.com/railwayapp/cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1fa1496c6f5aaadd1656db1d03ce58624ffab42511057d1addf3b8a4ab4f2372
+TERMUX_PKG_SHA256=7aa6e3b0a9db07dcdc44264c6d012ce5474c9f75c4c66ffdf643719b2af7f0fa
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-
-termux_step_pre_configure() {
-	termux_setup_golang
-}
-
-termux_step_make() {
-	go build -ldflags="-s -w -X github.com/railwayapp/cli/constants.Version=${TERMUX_PKG_VERSION}" -o bin/railway
-}
-
-termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin bin/railway
-}
