bf3cf43bcb9db05c91e10cd55eaf229483a21c92
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-05 18:30:21 +0900

    algernon: Bump to 1.14.0-p20230301

--- packages/algernon/build.sh
+++ packages/algernon/build.sh
@@ -2,11 +2,32 @@ TERMUX_PKG_HOMEPAGE=https://algernon.roboticoverlords.org/
 TERMUX_PKG_DESCRIPTION="Small self-contained web server with Lua, Markdown, QUIC, Redis and PostgreSQL support"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.14.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL="https://github.com/xyproto/algernon/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=2d30fe7a3f7c9b985f5fde7d6035888ad0c31ae4342fb38a96404de320ccd883
-TERMUX_PKG_AUTO_UPDATE=true
+_COMMIT=3d7448c5211ce33de0dbed186689146e497dc405
+_COMMIT_DATE=20230301
+TERMUX_PKG_VERSION=1.14.0-p${_COMMIT_DATE}
+#TERMUX_PKG_SRCURL="https://github.com/xyproto/algernon/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SRCURL=git+https://github.com/xyproto/algernon
+TERMUX_PKG_SHA256=5ed11b7371d8a8be3c62bbf234e18228d920f1c0dc28da33daf6a0f9b316fa3d
+TERMUX_PKG_GIT_BRANCH=main
+TERMUX_PKG_AUTO_UPDATE=false
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
+	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
+		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
+		echo -n " different from what is expected to be; should end"
+		echo " with \"${pdate}\"."
+		return 1
+	fi
+
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
 
 termux_step_make() {
 	termux_setup_golang
