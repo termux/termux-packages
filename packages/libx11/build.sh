cb0efca918bd5ada1a645c89555e55be36232fc3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-06 16:40:21 +0900

    libx11: Fix locale support

--- packages/libx11/build.sh
+++ packages/libx11/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_LICENSE="MIT, X11"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.8.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libX11-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c9a287a5aefa9804ce3cfafcf516fe96ed3f7e8e45c0e2ee59e84c86757df518
 TERMUX_PKG_DEPENDS="libandroid-support, libxcb"
@@ -16,6 +17,17 @@ ac_cv_path_RAWCPP=/usr/bin/cpp
 "
 
 termux_step_post_massage() {
+	# Regression test for broken XLC_LOCALE files. Do not remove.
+	local f
+	for f in share/X11/locale/*/XLC_LOCALE; do
+		if [ ! -f "${f}" ]; then
+			termux_error_exit "File not found: ${f}"
+		fi
+		if LC_ALL=C grep -E 'ct_encoding.*;\s*$' "${f}"; then
+			termux_error_exit "Broken XLC_LOCALE file found: ${f}"
+		fi
+	done
+
 	# Seems like some programs in the wild try to dlopen(3) `libX11.so.6`.
 	cd ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/lib || exit 1
 	if [ ! -e "./libX11.so.6" ]; then
