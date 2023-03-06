cd9cab9f2bcd2fd336f3ac7941f2983923dcf1e1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-06 23:36:55 +0900

    praat: Bump to 6.3.09

--- x11-packages/praat/build.sh
+++ x11-packages/praat/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.praat.org
 TERMUX_PKG_DESCRIPTION="Doing phonetics by computer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.3.08
+TERMUX_PKG_VERSION=6.3.09
 TERMUX_PKG_SRCURL=https://github.com/praat/praat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9c861a6f81260e5d5a8637bfcbec4b1d6e7fcac060dd469123d8d7b310a80fc0
+TERMUX_PKG_SHA256=047a8295a5097a4c101b62d49cf9e4e3f13655d94e35b4d9c00be0b3d2ca3330
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libc++, libcairo, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
