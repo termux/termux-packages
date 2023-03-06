83652145962d5225072118efa5a7dc82f39d2a52
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-06 23:28:06 +0900

    libmodplug: Bump to 0.8.9.1.r461

--- packages/libmodplug/build.sh
+++ packages/libmodplug/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="The ModPlug mod file playing library"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.8.9.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/modplug-xmms/libmodplug-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de
+TERMUX_PKG_VERSION=0.8.9.1.r461
+TERMUX_PKG_SRCURL=https://github.com/ShiftMediaProject/modplug/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d489a13cc863180b0f8209ad7b69d4413df454858d6f4ce94a03669213dc56cd
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
+	autoreconf -fi
+
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 }
