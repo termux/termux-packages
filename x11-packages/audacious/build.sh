dcbd2e8a91bf1421463b4f3bbf698204e7dbd98e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-07 10:48:26 +0900

    audacious{,-plugins}: Bump to 4.3

--- x11-packages/audacious/build.sh
+++ x11-packages/audacious/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://audacious-media-player.org
 TERMUX_PKG_DESCRIPTION="An advanced audio player"
-TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=4.2
+TERMUX_PKG_VERSION=4.3
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://distfiles.audacious-media-player.org/audacious-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=feb304e470a481fe2b3c4ca1c9cb3b23ec262540c12d0d1e6c22a5eb625e04b3
+TERMUX_PKG_SHA256=27584dc845c7e70db8c9267990945f17322a1ecc80ff8b452e9ca916a0ce9091
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, dbus-glib"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools"
 TERMUX_PKG_RECOMMENDS="audacious-plugins"
