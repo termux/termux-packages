c93059a5a8a15268062c031820020ab923c2854e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-07 10:45:15 +0900

    elvish: Bump to 0.19.2

--- packages/elvish/build.sh
+++ packages/elvish/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/elves/elvish
 TERMUX_PKG_DESCRIPTION="A friendly and expressive Unix shell"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.18.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.19.2
 TERMUX_PKG_SRCURL=https://github.com/elves/elvish/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f4635db90af2241bfd37e17ac1a72567b92d18a396598da2099a908b3d88c590
+TERMUX_PKG_SHA256=ef8032507c74c84369d49b098afcf1da65701aa071be9ee762f8bc456576ac94
 
 termux_step_make() {
 	termux_setup_golang
