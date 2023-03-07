7cf2891476fd558e01c03e16a55f277a3b7e7e4d
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-07 10:40:21 +0900

    bluefish: Bump to 2.2.13

--- x11-packages/bluefish/build.sh
+++ x11-packages/bluefish/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://bluefish.openoffice.nl/
 TERMUX_PKG_DESCRIPTION="A powerful editor targeted towards programmers and webdevelopers"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2.12
+TERMUX_PKG_VERSION=2.2.13
 TERMUX_PKG_SRCURL=https://www.bennewitz.com/bluefish/stable/source/bluefish-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=0df9905ba2c601cd767fa53e147969e9c90bc0cc4c005c4011c150b248979e6a
+TERMUX_PKG_SHA256=9b56966209d50951326a2ae21c7fd692bd91661d047ad3a01c97ba731aa477fb
 TERMUX_PKG_DEPENDS="atk, enchant, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxml2, pango, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-xml-catalog-update
