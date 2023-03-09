3f3381f35b95d47ed5f98273c75aebc62c5394f6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-10 00:29:45 +0900

    pipewire: Bump to 0.3.67

--- packages/pipewire/build.sh
+++ packages/pipewire/build.sh
@@ -3,15 +3,16 @@ TERMUX_PKG_DESCRIPTION="A server and user space API to deal with multimedia pipe
 TERMUX_PKG_LICENSE="MIT, LGPL-2.1, LGPL-3.0, GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.66
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.3.67
 TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/${TERMUX_PKG_VERSION}/pipewire-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=e627a06d7a68732abb56af7ba35d1846288a2db5c7d9f34ce86529ac96fd8502
+TERMUX_PKG_SHA256=43dfad8ee64c90613fdc67db0d7706e4ae747ee5e4aed03c34d3869f3926e081
 TERMUX_PKG_DEPENDS="avahi, ffmpeg, glib, libc++, liblua54, libsndfile, libwebrtc-audio-processing, lilv, ncurses, openssl, pulseaudio, readline"
 
 # 'media-session' session-managers is disabled as it requires alsa.
 # Since we are building without x11, dbus is disabled.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgstreamer=disabled
+-Dgstreamer-device-provider=disabled
 -Dtests=disabled
 -Dexamples=disabled
 -Dpipewire-alsa=disabled
