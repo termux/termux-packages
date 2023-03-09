dbd5bd78d4eed878d0e0cd25c29327ba0050273f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-09 13:40:28 +0900

    scrot: Migrate to maintained fork
    and bump to 1.8.1.

--- x11-packages/scrot/build.sh
+++ x11-packages/scrot/build.sh
@@ -1,10 +1,30 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/dreamer/scrot
 TERMUX_PKG_DESCRIPTION="Simple command-line screenshot utility for X"
+# License: MIT-feh
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.10.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL="https://github.com/dreamer/scrot/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=c5f6b1adcd235c5e4368658593eef310385b95fc1a76b68ef6d633ad24800135
-TERMUX_PKG_DEPENDS="imlib2, libxfixes, libx11"
+TERMUX_PKG_VERSION=1.8.1
+TERMUX_PKG_SRCURL=https://github.com/resurrecting-open-source-projects/scrot/releases/download/${TERMUX_PKG_VERSION}/scrot-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7d57f41166739075b6aa8d2eba7e303f85512f12b9c5372c6b9cecd10d800375
+TERMUX_PKG_DEPENDS="imlib2, libx11, libxcomposite, libxext, libxfixes, libxinerama"
 
+termux_step_pre_configure() {
+	local _inc="$TERMUX_PKG_SRCDIR/_getsubopt/include"
+	rm -rf "${_inc}"
+	mkdir -p "${_inc}"
+	cp "$TERMUX_PKG_BUILDER_DIR/getsubopt.h" "${_inc}"
+
+	CPPFLAGS+=" -I${_inc} -D__force= -UANDROID"
+
+	local _lib="$TERMUX_PKG_BUILDDIR/_getsubopt/lib"
+	rm -rf "${_lib}"
+	mkdir -p "${_lib}"
+	pushd "${_lib}"/..
+	$CC $CFLAGS $CPPFLAGS "$TERMUX_PKG_BUILDER_DIR/getsubopt.c" \
+		-fvisibility=hidden -c -o ./getsubopt.o
+	$AR cru "${_lib}"/libgetsubopt.a ./getsubopt.o
+	popd
+
+	LDFLAGS+=" -L${_lib} -l:libgetsubopt.a"
+}
