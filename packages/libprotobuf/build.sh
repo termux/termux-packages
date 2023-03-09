02e95796c24b4b31bbff16db69dbf196701c6cc7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-09 15:48:49 +0900

    libprotobuf: Bump to 22.1

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -8,9 +8,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 # - update SHA256 checksum for $_PROTOBUF_ZIP in
 #     $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
 # - ALWAYS bump revision of reverse dependencies and rebuild them.
-TERMUX_PKG_VERSION=2:22.0
+TERMUX_PKG_VERSION=2:22.1
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION#*:}.tar.gz
-TERMUX_PKG_SHA256=782160a6eae4bddfa4061ff5f7dcf04c9ed1494a0f0c6408b4af6b4a31ab9876
+TERMUX_PKG_SHA256=0b6494b6e1a8d197f6626ca0c5aa9ab35fc1e5aa3f724787133ce4fa4aa78499
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="abseil-cpp, libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev, protobuf-static (<< ${TERMUX_PKG_VERSION#*:})"
--- x11-packages/mumble-server/build.sh
+++ x11-packages/mumble-server/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Server module for Mumble, an open source voice-chat soft
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.517
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/mumble-voip/mumble
 TERMUX_PKG_DEPENDS="libc++, libcap, libdns-sd, libprotobuf, openssl, qt5-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
@@ -20,13 +21,8 @@ termux_step_pre_configure() {
 	termux_setup_protobuf
 
 	LDFLAGS+=" -lcap"
-}
 
-termux_step_post_configure() {
-	if [ "$TERMUX_CMAKE_BUILD" == "Ninja" ]; then
-		sed -i s:${TERMUX_PREFIX//./\\.}'/bin/protoc[0-9.-]*:'$(command -v protoc):g \
-			build.ninja
-	fi
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dprotobuf_PROTOC_EXE=$(command -v protoc)"
 }
 
 termux_step_post_make_install() {
