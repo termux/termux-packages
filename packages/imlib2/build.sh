d929852308301c00441691ff43d4f90d8ef8bcbe
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-10 23:02:36 +0900

    imlib2: Bump to 1.11.0

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -3,9 +3,8 @@ TERMUX_PKG_DESCRIPTION="Library that does image file loading and saving as well 
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING-PLAIN"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.10.0
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=1.11.0
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=727c1a937c885c080c37217e476dc88b5a3e60d737f1bf040b343520b781cbba
+TERMUX_PKG_SHA256=6121196a05a82cc49d104d066ef1bd1e68b4b6394064d244985d0d6aa5fd8170
 TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, giflib, glib, libandroid-shmem, libbz2, libcairo, libheif, libid3tag, libjpeg-turbo, libjxl, liblzma, libpng, librsvg, libtiff, libwebp, libx11, libxcb, libxext, openjpeg, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBS=-landroid-shmem"
