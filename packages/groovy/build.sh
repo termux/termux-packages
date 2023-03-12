bf0544808faa1b01f1b20add8658c467d8bc3119
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-13 06:31:24 +0900

    groovy: Bump to 4.0.10

--- packages/groovy/build.sh
+++ packages/groovy/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://groovy-lang.org/
 TERMUX_PKG_DESCRIPTION="A powerful multi-faceted programming language for the JVM platform"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.0.9
+TERMUX_PKG_VERSION=4.0.10
 _JANSI_VERSION=2.4.0
 _JLINE_VERSION=2.14.6
 _JANSI_TAGNAME="jansi-${_JANSI_VERSION}"
@@ -10,7 +10,7 @@ _JLINE2_TAGNAME="jline-${_JLINE_VERSION}"
 TERMUX_PKG_SRCURL=(https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-$TERMUX_PKG_VERSION.zip
                    https://github.com/fusesource/jansi/archive/refs/tags/${_JANSI_TAGNAME}.tar.gz
                    https://github.com/jline/jline2/archive/refs/tags/${_JLINE2_TAGNAME}.tar.gz)
-TERMUX_PKG_SHA256=(e0d26363ec798976b448857126b42823290c2007d63ecd97843a1718797131d4
+TERMUX_PKG_SHA256=(084bbad7bee4d23f6a03f0d567391f75211b4df1892406e13bf26fe08123fc62
                    ba18c50770eebb0b6f472254ada8608138e70045a564efa43626a09691dcf553
                    c6205afb214288cd8ef53f1ea1243ba9388c84b55c929f0b9e6cee7757c6efac)
 TERMUX_PKG_DEPENDS="libjansi (>= 2.4.0-1), openjdk-17"
