487ee9081183d51e3f84dc41f464391cb3c82a04
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2023-03-15 16:55:51 +0900

    freetype: enable harfbuzz

--- packages/freetype/build.sh
+++ packages/freetype/build.sh
@@ -3,18 +3,32 @@ TERMUX_PKG_DESCRIPTION="Software font engine capable of producing high-quality o
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.13.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/freetype/freetype-${TERMUX_PKG_VERSION}.tar.xz
 #TERMUX_PKG_SRCURL=https://download.savannah.nongnu.org/releases/freetype/freetype-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c
-TERMUX_PKG_DEPENDS="brotli, libbz2, libpng, zlib"
+TERMUX_PKG_DEPENDS="brotli, libbz2, libpng, zlib, harfbuzz"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="harfbuzz"
 TERMUX_PKG_BREAKS="freetype-dev"
 TERMUX_PKG_REPLACES="freetype-dev"
-# Use with-harfbuzz=no to avoid circular dependency between freetype and harfbuzz:
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-harfbuzz=no"
+
+termux_step_override_config_scripts() {
+	if ${_BUILD_FREETYPE_WITHOUT_HARFBUZZ:=false}; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-harfbuzz=no"
+	else
+		# Build harfbuzz
+		_BUILD_FREETYPE_WITHOUT_HARFBUZZ=true TERMUX_BUILD_IGNORE_LOCK=true $TERMUX_SCRIPTDIR/build-package.sh $(test "$TERMUX_INSTALL_DEPS" = true && echo -I || echo -s) $(test "${TERMUX_FORCE_BUILD_DEPENDENCIES}" = "true" && echo "-F" || true) packages/harfbuzz
+		termux_step_setup_build_folders
+	fi
+}
+
 # not install these files anymore so install them manually.
 termux_step_post_make_install() {
 	install -Dm700 freetype-config $TERMUX_PREFIX/bin/freetype-config
 	install -Dm600 ../src/docs/freetype-config.1 $TERMUX_PREFIX/share/man/man1/freetype-config.1
-}
 
+	if $_BUILD_FREETYPE_WITHOUT_HARFBUZZ; then
+		# Skip packaging
+		exit 0
+	fi
+}
--- packages/harfbuzz/build.sh
+++ packages/harfbuzz/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/harfbuzz/harfbuzz/archive/${TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=6c7a358c6e134bd6da4fe39f59ec273ff0ee461697945027b7538287b8c73b1e
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="freetype, glib, libcairo, libgraphite"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="freetype"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="harfbuzz-dev"
 TERMUX_PKG_REPLACES="harfbuzz-dev"
@@ -19,6 +20,12 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/gtk-doc
 "
+
+termux_step_override_config_scripts() {
+	# Build freetype without harfbuzz
+	_BUILD_FREETYPE_WITHOUT_HARFBUZZ=true TERMUX_BUILD_IGNORE_LOCK=true $TERMUX_SCRIPTDIR/build-package.sh $(test "$TERMUX_INSTALL_DEPS" = true && echo -I || echo -s) $(test "${TERMUX_FORCE_BUILD_DEPENDENCIES}" = "true" && echo "-F" || true) packages/freetype
+	termux_step_setup_build_folders
+}
 
 termux_step_post_get_source() {
 	mv CMakeLists.txt CMakeLists.txt.unused
