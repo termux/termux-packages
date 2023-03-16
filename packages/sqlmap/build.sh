8aadb8418162126a3cb3905cc63e39fcd47a1f33
bread-up <breadup096@gmail.com>
2023-03-16 22:35:36 +0100

    add support for sqlmap

---
+++ packages/sqlmap/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=http://sqlmap.org/
+TERMUX_PKG_DESCRIPTION="Automatic SQL injection and database takeover tool"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@bread-up"
+TERMUX_PKG_VERSION=1.7
+TERMUX_PKG_SRCURL=https://github.com/sqlmapproject/sqlmap/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=aa00e08007bfdb06a362a0c2798073af8e7053a97ead8ed7cca86393a94ec2e1
+TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_GROUPS="util-linux"
+
+termux_step_make_install() {
+      mkdir -p $TERMUX_PREFIX/opt/sqlmap
+      mv $TERMUX_PKG_SRCDIR/sqlmap.py $TERMUX_PKG_SRCDIR/sqlmap
+      cp $TERMUX_PKG_SRCDIR/sqlmap $TERMUX_PREFIX/bin
+      cp $TERMUX_PKG_SRCDIR/* $TERMUX_PREFIX/opt/sqlmap
+}
