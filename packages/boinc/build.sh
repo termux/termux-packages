f848a503d744b2b5c032e458280199da10220c8b
Jia Yuan Lo <jylo06g@gmail.com>
2023-03-18 07:23:56 +0800

    boinc: always disallow ldd usage on Android

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open-source software for volunteer computing"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.22.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/BOINC/boinc/archive/client_release/${TERMUX_PKG_VERSION:0:4}/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7c7014300cb6e859b6f784f5c76ff2cb96d36ae35d90e03d1e2e59174cb7c927
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-shmem, libc++, libcurl, openssl, zlib"
