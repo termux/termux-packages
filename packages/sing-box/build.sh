289b58799b7afbb32b0edb55d338193bd7d3d071
CHIZI-0618 <51962278+CHIZI-0618@users.noreply.github.com>
2023-03-17 13:36:15 +0800

    sing-box: Bump to 1.2-beta9

--- packages/sing-box/build.sh
+++ packages/sing-box/build.sh
@@ -2,15 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://sing-box.sagernet.org
 TERMUX_PKG_DESCRIPTION="The universal proxy platform"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="kay9925@outlook.com"
-TERMUX_PKG_VERSION="1.2-beta8"
+TERMUX_PKG_VERSION="1.2-beta9"
 TERMUX_PKG_SRCURL="https://github.com/SagerNet/sing-box/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=dd2e843cbb9698f32ececd946d5f0a414977bc92f605f50b60f424bf7a5bee56
+TERMUX_PKG_SHA256=03b5009e8886a8466bb82f86bc41ab614a211b393edc75eac6f9e7a832681d58
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_golang
 
-	local tags="with_quic,with_grpc,with_dhcp,with_wireguard,with_shadowsocksr,with_ech,with_utls,with_reality_server,with_clash_api,with_gvisor"
+	local tags="with_quic,with_grpc,with_dhcp,with_wireguard,with_shadowsocksr,with_ech,with_utls,with_reality_server,with_clash_api,with_v2ray_api,with_gvisor"
 	local ldflags="\
 	-w -s \
 	-X 'github.com/sagernet/sing-box/constant.Version=${TERMUX_PKG_VERSION}' \
