efd16c12fcbbbf59baa7ee237d61d66496fc83f7
Butta <repo@butta.fastem.com>
2023-03-20 07:34:47 +0530

    bump(libllvm): 15.0.7 to 16.0.0

--- packages/libllvm/llvm-tools.subpackage.sh
+++ packages/libllvm/llvm-tools.subpackage.sh
@@ -1,13 +1,14 @@
 TERMUX_SUBPKG_INCLUDE="
 bin/FileCheck
+bin/UnicodeNameMappingGenerator
 bin/count
 bin/lli-child-target
 bin/llvm-PerfectShuffle
 bin/llvm-jitlink-executor
 bin/not
 bin/obj2yaml
-bin/yaml2obj
 bin/yaml-bench
+bin/yaml2obj
 share/man/man1/FileCheck.1.gz
 "
 TERMUX_SUBPKG_DESCRIPTION="LLVM Development Tools"
