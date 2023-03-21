4cd0f8f3c03c151242df566a415292260069af88
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-22 08:09:55 +0900

    dnslookup: Bump to 1.9.1

--- packages/dnslookup/build.sh
+++ packages/dnslookup/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ameshkov/dnslookup
 TERMUX_PKG_DESCRIPTION="Simple command line utility to make DNS lookups. Supports all known DNS protocols: plain DNS, DoH, DoT, DoQ, DNSCrypt."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="kay9925@outlook.com"
-TERMUX_PKG_VERSION="1.8.1"
+TERMUX_PKG_VERSION=1.9.1
 TERMUX_PKG_SRCURL="https://github.com/ameshkov/dnslookup/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=51b9cbc626e091eb7a98bc326ad026f36d95f8b5917f71f13011466fcdddb3f9
+TERMUX_PKG_SHA256=ac108b80f5d7b510ce1fdb1880470d177c5b89cf228c34302442527cf68bb76e
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_GO_USE_OLDER=true
+TERMUX_PKG_GO_USE_OLDER=false
 
 termux_step_make() {
 	termux_setup_golang
