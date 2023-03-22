997c922f69d42a5896ea4c2e2f52bab6bbcacd8e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-22 10:10:19 +0900

    xfce4-screensaver: Bump to 4.18.0

--- x11-packages/xfce4-screensaver/build.sh
+++ x11-packages/xfce4-screensaver/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/screensaver/start
 TERMUX_PKG_DESCRIPTION="Xfce Screensaver is a screen saver and locker that aims to have simple, sane, secure defaults and be well integrated with the desktop."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
-_MAJOR_VERSION=4.16
+_MAJOR_VERSION=4.18
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
-TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-screensaver/${_MAJOR_VERSION}/xfce4-screensaver-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6d4d143e3e62db679ce83ce7da97903390773ee0a8ceb05ff4c3dac36616268d
-TERMUX_PKG_DEPENDS="dbus, dbus-glib, garcon, gdk-pixbuf, glib, gtk3, libcairo, libwnck, libx11, libxext, libxfce4ui, libxklavier, libxss, opengl, pango, termux-auth, xfconf"
+TERMUX_PKG_SHA256=c72b5b42242368069568dbb27a37d9549680b59e87fece413091af7ddeeee4e2
+TERMUX_PKG_DEPENDS="dbus, dbus-glib, garcon, gdk-pixbuf, glib, gtk3, libcairo, libwnck, libx11, libxext, libxfce4ui, libxfce4util, libxklavier, libxss, opengl, pango, termux-auth, xfconf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-locking --sysconfdir=$TERMUX_PREFIX/etc"
 TERMUX_PKG_BUILD_IN_SRC=true
