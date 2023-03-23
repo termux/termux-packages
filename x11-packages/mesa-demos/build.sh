d14ebd69495c04d06d9fd0e609b1f73e6a72fe69
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-23 12:28:26 +0900

    mesa-demos: Bump to 9.0.0

--- x11-packages/mesa-demos/build.sh
+++ x11-packages/mesa-demos/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.mesa3d.org
 TERMUX_PKG_DESCRIPTION="OpenGL demonstration and test programs"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Rafael Kitover <rkitover@gmail.com>"
-TERMUX_PKG_VERSION=8.5.0
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/demos/${TERMUX_PKG_VERSION}/mesa-demos-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=cea2df0a80f09a30f635c4eb1a672bf90c5ddee0b8e77f4d70041668ef71aac1
+TERMUX_PKG_VERSION=9.0.0
+TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/demos/mesa-demos-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=3046a3d26a7b051af7ebdd257a5f23bfeb160cad6ed952329cdff1e9f1ed496b
 TERMUX_PKG_DEPENDS="freeglut, glu, libx11, libxext, opengl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibdrm=disabled
+-Dvulkan=disabled
 -Dwayland=disabled
 "
 TERMUX_PKG_HOSTBUILD=true
