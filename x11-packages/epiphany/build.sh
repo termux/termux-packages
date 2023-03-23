d4102e3365d69c21c361402431969ca6270ddaba
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-23 11:26:36 +0900

    epiphany: Bump to 44.0

--- x11-packages/epiphany/build.sh
+++ x11-packages/epiphany/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Web
 TERMUX_PKG_DESCRIPTION="A GNOME web browser based on the WebKit rendering engine"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=43
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+_MAJOR_VERSION=44
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/epiphany/${_MAJOR_VERSION}/epiphany-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e86ead27cb9982815150664de3bf20faf375f77b8065b02b31180c65b6bbebb4
-TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, gcr, gdk-pixbuf, glib, gsettings-desktop-schemas, gtk3, iso-codes, json-glib, libarchive, libcairo, libdazzle, libgmp, libhandy, libnettle, libportal-gtk3, libsecret, libsoup3, libsqlite, libxml2, pango, webkit2gtk-4.1"
+TERMUX_PKG_SHA256=aabdc9de80c409073676e00e15ba97187715e4b84bc776fe86db86d0f8140bb1
+TERMUX_PKG_DEPENDS="adwaita-icon-theme, gcr4, gdk-pixbuf, glib, graphene, gsettings-desktop-schemas, gstreamer, gtk4, iso-codes, json-glib, libadwaita, libarchive, libcairo, libgmp, libnettle, libportal-gtk4, libsecret, libsoup3, libsqlite, libxml2, pango, webkitgtk-6.0"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dunit_tests=disabled
 "
