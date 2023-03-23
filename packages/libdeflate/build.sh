ff403a957b8417733cb7bb28821229ad752f3751
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-23 19:12:39 +0000

    new package: libdeflate (#15772)

---
+++ packages/libdeflate/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/ebiggers/libdeflate
+TERMUX_PKG_DESCRIPTION="C library for fast compression and decompression"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.17
+TERMUX_PKG_SRCURL=https://github.com/ebiggers/libdeflate/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fa4615af671513fa2a53dc2e7a89ff502792e2bdfc046869ef35160fcc373763
+TERMUX_PKG_FORCE_CMAKE=true
