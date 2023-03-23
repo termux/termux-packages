2b276cbde876d5442a5a164b70e4f4c719399b64
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-23 14:07:37 +0000

    Update build.sh

--- packages/libdeflate/build.sh
+++ packages/libdeflate/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="C library for fast compression and decompression"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.17
-TERMUX_PKG_SRCURL=https://github.com/ebiggers/libdeflate/releases/download/${TERMUX_PKG_VERSION}/htslib-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SRCURL=https://github.com/ebiggers/libdeflate/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fa4615af671513fa2a53dc2e7a89ff502792e2bdfc046869ef35160fcc373763
 
 
