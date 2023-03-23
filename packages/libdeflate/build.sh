e5850fd462abb74f5394562c911d9b905ab76a43
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-23 15:04:30 +0000

    Update build.sh

--- packages/libdeflate/build.sh
+++ packages/libdeflate/build.sh
@@ -6,7 +6,3 @@ TERMUX_PKG_VERSION=1.17
 TERMUX_PKG_SRCURL=https://github.com/ebiggers/libdeflate/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fa4615af671513fa2a53dc2e7a89ff502792e2bdfc046869ef35160fcc373763
 TERMUX_PKG_FORCE_CMAKE=true
-
-termux_step_make() {
-	cmake -B build && cmake --build build
-}
