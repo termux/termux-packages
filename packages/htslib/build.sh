d3c1ab5c0614dd9e19c63a305f31ff791e975ec7
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-24 10:00:02 +0000

    htslib: Depends on libdeflate and libcurl (#15781)

--- packages/htslib/build.sh
+++ packages/htslib/build.sh
@@ -5,7 +5,8 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.17
 TERMUX_PKG_SRCURL=https://github.com/samtools/htslib/releases/download/${TERMUX_PKG_VERSION}/htslib-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=763779288c40f07646ec7ad98b96c378c739171d162ad98398868783b721839f
-TERMUX_PKG_DEPENDS="libbz2, liblzma, zlib"
+TERMUX_PKG_DEPENDS="libbz2, liblzma, zlib, libdeflate, libcurl"
+TERMUX_PKG_REVISION=1
 
 # error: assigning to 'uint8x8_t' (vector of 8 'uint8_t' values) from incompatible type 'int'
 TERMUX_PKG_BLACKLISTED_ARCHES="arm"
