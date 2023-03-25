ed68bbd3342c53776d8214522f7ef72d01384116
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-25 22:06:46 +0900

    ndk-multilib: Exclude native stubs for shared libs
    which are now installed with subpackage named ndk-multilib-native-stubs.

---
+++ packages/ndk-multilib/ndk-multilib-native-stubs.subpackage.sh
@@ -0,0 +1,17 @@
+TERMUX_SUBPKG_DESCRIPTION="Install native stubs for shared libs from NDK"
+TERMUX_SUBPKG_INCLUDE=""
+
+termux_step_create_subpkg_debscripts() {
+	local f
+	for f in postinst prerm; do
+		sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+			-e "s|@TERMUX_PACKAGE_FORMAT@|${TERMUX_PACKAGE_FORMAT}|g" \
+			$TERMUX_PKG_BUILDER_DIR/postinst-header.in > "${f}"
+	done
+	sed 's|@COMMAND@|ln -sf "'$TERMUX_PREFIX'/opt/ndk-multilib/$triple/lib/$so" "'$TERMUX_PREFIX'/\$triple/lib"|' \
+		$TERMUX_PKG_BUILDER_DIR/postinst-native.in >> postinst
+	sed 's|@COMMAND@|rm -f "'$TERMUX_PREFIX'/$triple/lib/$so"|' \
+		$TERMUX_PKG_BUILDER_DIR/postinst-native.in >> prerm
+	chmod 0700 postinst prerm
+}
+
