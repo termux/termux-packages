693650e1378fd75e9bd9ae7d077bf931e42f8aa4
Jia Yuan Lo <jylo06g@gmail.com>
2023-03-25 14:51:13 +0800

    bump(main/vulkan-tools): 1.3.245

--- packages/vulkan-tools/build.sh
+++ packages/vulkan-tools/build.sh
@@ -3,19 +3,20 @@ TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # This package and vulkan-headers should be updated at same time. Otherwise, they do not compile successfully.
-TERMUX_PKG_VERSION="1.3.244"
+TERMUX_PKG_VERSION="1.3.245"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Tools/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3fa97bc986e4a7c7d1a1877dff3be1383f42abc8c00fac5a75b8233660e91e5e
-TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libwayland, vulkan-loader"
+TERMUX_PKG_SHA256=7bf5c7328a7d0114a23e95039b74f48d0cee6de594fa64714c3ed9bcf9d80b54
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-loader-generic (=${TERMUX_PKG_VERSION})"
+TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libwayland"
+TERMUX_PKG_RECOMMENDS="vulkan-loader"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_CUBE=ON
 -DBUILD_ICD=OFF
+-DBUILD_WSI_WAYLAND_SUPPORT=ON
 -DBUILD_WSI_XCB_SUPPORT=ON
 -DBUILD_WSI_XLIB_SUPPORT=ON
--DBUILD_WSI_WAYLAND_SUPPORT=ON
--DWAYLAND_SCANNER_EXECUTABLE=$TERMUX_PREFIX/opt/libwayland/cross/bin/wayland-scanner
--DVULKAN_HEADERS_INSTALL_DIR=$TERMUX_PREFIX
+-DVULKAN_HEADERS_INSTALL_DIR=${TERMUX_PREFIX}
+-DWAYLAND_SCANNER_EXECUTABLE=${TERMUX_PREFIX}/opt/libwayland/cross/bin/wayland-scanner
 "
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
