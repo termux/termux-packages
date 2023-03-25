e8c307e30c605e4e4e716f8290b785221c7b7404
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-25 17:17:39 +0900

    v2ray: Bump to 5.4.1

--- packages/v2ray/build.sh
+++ packages/v2ray/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://www.v2fly.org/
 TERMUX_PKG_DESCRIPTION="A platform for building proxies to bypass network restrictions"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.4.0
+TERMUX_PKG_VERSION=5.4.1
 TERMUX_PKG_SRCURL=git+https://github.com/v2fly/v2ray-core
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -13,15 +13,15 @@ termux_step_post_get_source() {
 	chmod +w $GOPATH -R
 	rm -rf $TERMUX_PREFIX/share/v2ray/
 	mkdir -p $TERMUX_PREFIX/share/v2ray/
-	termux_download https://github.com/v2fly/geoip/releases/download/202303020053/geoip.dat \
+	termux_download https://github.com/v2fly/geoip/releases/download/202303230043/geoip.dat \
 		$TERMUX_PREFIX/share/v2ray/geoip.dat \
-		4cd53acc7e2896cab0cbdd39fd43fd6fc60073be6a025f4fb80710e6bb55090e
-	termux_download https://github.com/v2fly/domain-list-community/releases/download/20230306032504/dlc.dat \
+		7086861a6016319264248baea07b3fee74d44521f46ead4338f14cb2dd4779a6
+	termux_download https://github.com/v2fly/domain-list-community/releases/download/20230324131400/dlc.dat \
 		$TERMUX_PREFIX/share/v2ray/geosite.dat \
-		5a2f8f3945911e1e7fbf8971d3a24de17f3e7479ba837ff2281a6d03448bbbed
-	termux_download https://github.com/v2fly/geoip/releases/download/202303020053/geoip-only-cn-private.dat \
+		7b20a6e466e985f0c6301fb77b00f56367987dd85ef7ea01cb4a72b11d118f89
+	termux_download https://github.com/v2fly/geoip/releases/download/202303230043/geoip-only-cn-private.dat \
 		$TERMUX_PREFIX/share/v2ray/geoip-only-cn-private.dat \
-		9578e6cd3a49b5111f46d1ca7bc9e736f1c16d34625c1c0bf061d23485335b5b
+		ed3d13a5cf8c9629d5f74b9ee2dd645b90760bae3939b5de8f6b5eb415cfca07
 }
 
 termux_step_make() {
