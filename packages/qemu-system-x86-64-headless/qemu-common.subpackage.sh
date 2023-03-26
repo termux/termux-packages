3fd37a2b44513862a40ba33f2ee46b1ca69f831a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-27 00:24:50 +0900

    qemu-system-x86-64-headless: Fix dependency
    libslirp dep was missing from qemu-system-*-headless subpackages. Use
    TERMUX_SUBPKG_DEPEND_ON_PARENT=deps to avoid this type of bug. Note that
    qemu-system-m68k-headless actually does not depend on libusb{,redir}.

--- packages/qemu-system-x86-64-headless/qemu-common.subpackage.sh
+++ packages/qemu-system-x86-64-headless/qemu-common.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="A set common files used by the QEMU emulators"
-TERMUX_SUBPKG_DEPENDS="glib, libgcrypt"
+TERMUX_SUBPKG_DEPENDS="glib, libbz2, libcap-ng, libcurl, libgmp, libgnutls, libnettle, libnfs, libpixman, libssh, zlib, zstd"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=no
 
 TERMUX_SUBPKG_INCLUDE="
