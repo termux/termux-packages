61f40df056397a6c6ab6015195435657509a7af5
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-26 21:30:21 +0900

    librocksdb: Bump to 8.0.0

--- packages/librocksdb/build.sh
+++ packages/librocksdb/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A persistent key-value store for flash and RAM storage"
 TERMUX_PKG_LICENSE="GPL-2.0, Apache-2.0, BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE.Apache, LICENSE.leveldb"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="7.10.2"
+TERMUX_PKG_VERSION=8.0.0
 TERMUX_PKG_SRCURL=https://github.com/facebook/rocksdb/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4619ae7308cd3d11cdd36f0bfad3fb03a1ad399ca333f192b77b6b95b08e2f78
+TERMUX_PKG_SHA256=05ff6b0e89bffdf78b5a9d6fca46cb06bde6189f5787b9eeaef0511b782c1033
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gflags, libc++"
 TERMUX_PKG_BUILD_DEPENDS="gflags-static"
@@ -17,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=7
+	local _SOVERSION=8
 
 	local v=$(grep -E "ROCKSDB_MAJOR.[0-9]" include/rocksdb/version.h | \
 			cut -d ' ' -f 3)
