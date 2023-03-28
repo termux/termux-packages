db312779c69deebea54f6bacf316151c380ee4a2
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-03-28 13:27:58 +0800

    llvm-mingw-w64*: bump to 20230320

--- packages/llvm-mingw-w64-libcompiler-rt/build.sh
+++ packages/llvm-mingw-w64-libcompiler-rt/build.sh
@@ -2,19 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://www.llvm.org/
 TERMUX_PKG_DESCRIPTION="Compiler runtime libraries for LLVM-MinGW"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=20220906
-# Note: This package should revbump after libllvm gets minor update
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=20230320
 TERMUX_PKG_SRCURL=https://github.com/mstorsjo/llvm-mingw/releases/download/$TERMUX_PKG_VERSION/llvm-mingw-$TERMUX_PKG_VERSION-ucrt-ubuntu-18.04-x86_64.tar.xz
-TERMUX_PKG_SHA256=ee00708bdd65eeaa88d5fa89ad7e3fa1d6bae8093ee4559748e431e55f7568ec
+TERMUX_PKG_SHA256=bc367753dea829d219be32e2e64e2d15d03158ce8e700ae5210ca3d78e6a07ea
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_NO_STATICSPLIT=true
 
 termux_step_make_install() {
 	# Install compier-rt libraries
-	local _LLVM_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $TERMUX_PKG_VERSION)
-	mkdir -p $TERMUX_PREFIX/lib/clang/$_LLVM_VERSION/lib/windows
-	mv $TERMUX_PKG_SRCDIR/lib/clang/*/lib/windows $TERMUX_PREFIX/lib/clang/$_LLVM_VERSION/lib/
+	local LLVM_MAJOR_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $LLVM_MAJOR_VERSION)
+	mkdir -p $TERMUX_PREFIX/lib/clang/$LLVM_MAJOR_VERSION/lib/windows
+	mv $TERMUX_PKG_SRCDIR/lib/clang/*/lib/windows $TERMUX_PREFIX/lib/clang/$LLVM_MAJOR_VERSION/lib/
 }
 
 termux_step_install_license() {
