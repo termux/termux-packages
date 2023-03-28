a8db07405c61d7581b57920768ea9024cd9bf8c1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-03-28 23:05:20 +0900

    redis: Add workaround for unaligned access on arm

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=7.0.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.redis.io/releases/redis-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=1dee4c6487341cae7bd6432ff7590906522215a061fdef87c7d040a0cb600131
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob"
@@ -16,6 +17,11 @@ termux_step_pre_configure() {
 	CPPFLAGS+=" -DHAVE_BACKTRACE"
 	CFLAGS+=" $CPPFLAGS"
 	LDFLAGS+=" -landroid-execinfo -landroid-glob"
+
+	# https://github.com/termux/termux-packages/issues/15849
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		CFLAGS="${CFLAGS/-mfpu=neon/} -mfpu=vfpv3"
+	fi
 }
 
 termux_step_post_make_install() {
