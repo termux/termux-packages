335d192a6b683664e4e93e90c9f796e0eca11702
Ivan Maidanski <ivmai@mail.ru>
2023-03-29 08:35:55 +0300

    libatomic-ops: Update to 7.8.0

--- packages/libatomic-ops/build.sh
+++ packages/libatomic-ops/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ivmai/libatomic_ops
 TERMUX_PKG_DESCRIPTION="Provides semi-portable access to hardware-provided atomic memory update operations"
 TERMUX_PKG_LICENSE="GPL-2.0, MIT"
-TERMUX_PKG_LICENSE_FILE="README.md, doc/LICENSING.txt"
+TERMUX_PKG_LICENSE_FILE="README.md, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.6.14
+TERMUX_PKG_VERSION=7.8.0
 TERMUX_PKG_SRCURL=https://github.com/ivmai/libatomic_ops/releases/download/v${TERMUX_PKG_VERSION}/libatomic_ops-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=390f244d424714735b7050d056567615b3b8f29008a663c262fb548f1802d292
+TERMUX_PKG_SHA256=15676e7674e11bda5a7e50a73f4d9e7d60452271b8acf6fd39a71fefdf89fa31
