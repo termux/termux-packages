a7013df8e8fc01e5e5117b77819910ab718f2884
CHIZI-0618 <51962278+CHIZI-0618@users.noreply.github.com>
2023-03-31 16:23:05 +0800

    new package: q-dns-client (#15871)

---
+++ packages/q-dns-client/build.sh
@@ -0,0 +1,41 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/natesales/q
+TERMUX_PKG_DESCRIPTION="A tiny command line DNS client with support for UDP, TCP, DoT, DoH, DoQ and ODoH"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="kay9925@outlook.com"
+TERMUX_PKG_VERSION="0.10.1"
+_COMMIT=4444da01237f5c53e49539129d2611e125f343d5
+TERMUX_PKG_GIT_BRANCH=main
+TERMUX_PKG_SRCURL="git+https://github.com/natesales/q"
+TERMUX_PKG_SHA256=5944362d564bb43f261fc44ee2e2dd790c6d4e2886633642dbc88249e32dceda
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git reset --hard $_COMMIT
+
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]] ; then
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
+
+termux_step_make() {
+	termux_setup_golang
+
+	local _builtAt=$(date +'%FT%T%Z')
+
+	local ldflags="\
+	-w -s \
+	-X 'main.version=${TERMUX_PKG_VERSION}' \
+	-X 'main.commit=${_COMMIT}' \
+	-X 'main.date=${_builtAt}' \
+	"
+
+	export CGO_ENABLED=1
+
+	go build -o "${TERMUX_PKG_NAME}" -ldflags="$ldflags"
+}
+
+termux_step_make_install() {
+	install -Dm700 ./${TERMUX_PKG_NAME} ${TERMUX_PREFIX}/bin/q
+}
