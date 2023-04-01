715da16f547594078e4087f3ba0e446c5196bf2a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-02 04:19:00 +0900

    libffi: Fix build with strict C99

--- packages/libffi/build.sh
+++ packages/libffi/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library providing a portable, high level programming int
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.4.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libffi/libffi/releases/download/v${TERMUX_PKG_VERSION}/libffi-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d66c56ad259a82cf2a9dfc408b32bf5da52371500b84745f7fb8b645712df676
 TERMUX_PKG_BREAKS="libffi-dev"
