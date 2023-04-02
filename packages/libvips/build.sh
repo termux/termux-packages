25876ce792b5281cf0d0964c0d4adfb5d905a7e6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-02 09:22:03 +0900

    libvips: Re-enable clang16-porting
    %ci:no-build

--- packages/libvips/build.sh
+++ packages/libvips/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION="8.14.2"
 TERMUX_PKG_SRCURL=https://github.com/libvips/libvips/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=feb30b82161cfc30d5ba396c95b08bf9af3110bc960ccc0efecebb45db22deda
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 TERMUX_PKG_DEPENDS="cgif, fftw, fontconfig, glib, imagemagick, libc++, libcairo, libexif, libexpat, libheif, libimagequant, libjpeg-turbo, libjxl, libpng, librsvg, libtiff, libwebp, littlecms, openexr, openjpeg, pango, poppler, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
 TERMUX_PKG_DISABLE_GIR=true
