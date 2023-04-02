0873bda2faf5684753d96cdc55665ae902f34916
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-02 09:51:42 +0900

    gegl: Fix build with strict C99

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.4
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.44
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${_MAJOR_VERSION}/gegl-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0a4cdb41635e406a0849cd0d3f03caf7d97cab8aa13d28707d532d0089d56126
 TERMUX_PKG_DEPENDS="babl, ffmpeg, gdk-pixbuf, glib, json-glib, libandroid-support, libc++, libcairo, libjasper, libjpeg-turbo, libpng, librsvg, libtiff, libwebp, littlecms, openexr, pango, poppler"
