107e1cf191423fff2c16be3fa3ce34c2ce5ec751
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-05 08:02:22 +0900

    coinor-clp: Apply compiler-rt builtins hack

--- packages/coinor-clp/build.sh
+++ packages/coinor-clp/build.sh
@@ -3,6 +3,11 @@ TERMUX_PKG_DESCRIPTION="An open-source linear programming solver"
 TERMUX_PKG_LICENSE="EPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1:1.17.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/coin-or/Clp/archive/refs/tags/releases/${TERMUX_PKG_VERSION#*:}.tar.gz
 TERMUX_PKG_SHA256=c4c2c0e014220ce8b6294f3be0f3a595a37bef58a14bf9bac406016e9e73b0f5
 TERMUX_PKG_DEPENDS="libc++, libcoinor-osi, libcoinor-utils"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
