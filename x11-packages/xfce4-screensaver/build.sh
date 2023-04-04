09454f2d0014236bebceffe95944320515f45b44
Leonid Pliushch <sylirre@termux.dev>
2023-04-04 13:35:55 +0300

    fix(x11/xfce4-screensaver): use $TERMUX_PREFIX/tmp

--- x11-packages/xfce4-screensaver/build.sh
+++ x11-packages/xfce4-screensaver/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
 _MAJOR_VERSION=4.18
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-screensaver/${_MAJOR_VERSION}/xfce4-screensaver-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=c72b5b42242368069568dbb27a37d9549680b59e87fece413091af7ddeeee4e2
 TERMUX_PKG_DEPENDS="dbus, dbus-glib, garcon, gdk-pixbuf, glib, gtk3, libcairo, libwnck, libx11, libxext, libxfce4ui, libxfce4util, libxklavier, libxss, opengl, pango, termux-auth, xfconf"
