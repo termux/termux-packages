3333040a60226810406a696131a2588a3513812e
Leonid Pliushch <sylirre@termux.dev>
2023-04-04 13:36:06 +0300

    fix(x11/xfce4-notifyd): use $TERMUX_PREFIX/tmp

--- x11-packages/xfce4-notifyd/build.sh
+++ x11-packages/xfce4-notifyd/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
 _MAJOR_VERSION=0.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-notifyd/${_MAJOR_VERSION}/xfce4-notifyd-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=e3a28adb08daa1411135142a0d421e4d6050c4035a4e513a673a59460ff2ae84
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libnotify, libsqlite, libx11, libxfce4ui, libxfce4util, pango, xfce4-panel, xfconf, zlib"
