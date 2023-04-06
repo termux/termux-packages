340439a00682168d47e511cac85e481169c8ace8
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-07 00:19:16 +0900

    postgis: Fix build
    %ci:no-build

--- packages/postgis/build.sh
+++ packages/postgis/build.sh
@@ -6,7 +6,12 @@ TERMUX_PKG_VERSION=3.3.2
 TERMUX_PKG_SRCURL=https://download.osgeo.org/postgis/source/postgis-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9a2a219da005a1730a39d1959a1c7cec619b1efb009b65be80ffc25bad299068
 TERMUX_PKG_DEPENDS="gdal, json-c, libc++, libgeos, libiconv, libprotobuf-c, libxml2, pcre2, postgresql, proj"
+TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 # both configure script and Makefile(s) look for files in current
 # directory rather than srcdir, so need to build in source
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	CXXFLAGS+=" -std=c++14"
+}
