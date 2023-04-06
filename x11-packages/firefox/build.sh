253582f730c94c8fcc49213a5bcc2d13c4a92ce4
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-07 01:07:44 +0900

    firefox: Bump to 112.0

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mozilla.org/firefox
 TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=111.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=112.0
 TERMUX_PKG_SRCURL=https://ftp.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION}/source/firefox-${TERMUX_PKG_VERSION}.source.tar.xz
-TERMUX_PKG_SHA256=e1006c0872aa7eb30fb5a689413957f1e5fc8d2048b1637bf6f6fafdbd4ea55f
+TERMUX_PKG_SHA256=eb19185f7bfa5c3b0c73edaa57160b44bf3bb2139db83539809607486b8075d9
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="at-spi2-atk, ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-sysv-semaphore, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxi, libxrandr, libxrender, libxtst, pango, pulseaudio"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures"
@@ -15,33 +14,6 @@ termux_step_post_get_source() {
 	local f="media/ffvpx/config_unix_aarch64.h"
 	echo "Applying sed substitution to ${f}"
 	sed -i -E '/^#define (CONFIG_LINUX_PERF|HAVE_SYSCTL) /s/1$/0/' ${f}
-
-	# Add support for FFmpeg 6.0 (please remove when added upstream)
-	local _FFMPEG60_DIR=$TERMUX_PKG_SRCDIR/dom/media/platforms/ffmpeg/ffmpeg60
-	if [ -d "${_FFMPEG60_DIR}" ]; then
-		termux_error_exit "Directory ${_FFMPEG60_DIR} already exists."
-	fi
-	rm -rf ${_FFMPEG60_DIR}
-	mkdir -p ${_FFMPEG60_DIR}
-	cd ${_FFMPEG60_DIR}
-	cp ../ffmpeg59/moz.build ./
-	mkdir -p include
-	cd include
-	mkdir -p libav{codec,util}
-	for f in avcodec.h avfft.h bsf.h codec.h codec_desc.h codec_id.h codec_par.h defs.h packet.h vdpau.h version.h version_major.h; do
-		cp $TERMUX_PREFIX/include/libavcodec/${f} ./libavcodec/
-	done
-	for f in attributes.h avutil.h buffer.h channel_layout.h common.h cpu.h dict.h error.h frame.h hwcontext.h hwcontext_vaapi.h intfloat.h log.h macros.h mathematics.h mem.h pixfmt.h rational.h samplefmt.h version.h avconfig.h; do
-		cp $TERMUX_PREFIX/include/libavutil/${f} ./libavutil/
-	done
-	if [ $TERMUX_ARCH = "i686" ]; then
-		sed -i -E '/^#define AV_HAVE_FAST_UNALIGNED /s/0$/1/' \
-			./libavutil/avconfig.h
-	fi
-	local s=$(find . -type f -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "66d7de01c50d46b588021d1b7be87056b95ec295aeb98b3f37adad1ba9154319  "* ]]; then
-		termux_error_exit "Checksum mismatch for ffmpeg60 header files."
-	fi
 }
 
 termux_step_pre_configure() {
