d368cd4bb5fcad409ea9b58beeb44d2ffa9bd1dc
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-09 08:37:16 +0900

    gitui: Fix build with strict C99

--- packages/gitui/build.sh
+++ packages/gitui/build.sh
@@ -4,18 +4,30 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.md"
 TERMUX_PKG_MAINTAINER="@PeroSar"
 TERMUX_PKG_VERSION=0.22.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/extrawurst/gitui/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=285e86c136ee7f410fdd52c5284ccf0d19011cc5f7709e5e10bb02f439a218ea
-TERMUX_PKG_DEPENDS="git, zlib, openssl"
+TERMUX_PKG_DEPENDS="git, libgit2, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	CPPFLAGS+=" -Dindex=strchr"
 	export OPENSSL_NO_VENDOR=1
+	export LIBGIT2_SYS_USE_PKG_CONFIG=1
+
+	termux_setup_rust
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	local f
+	for f in $CARGO_HOME/registry/src/github.com-*/libgit2-sys-*/build.rs; do
+		sed -i -E 's/\.range_version\(([^)]*)\.\.[^)]*\)/.atleast_version(\1)/g' "${f}"
+	done
 }
 
 termux_step_make() {
-	termux_setup_rust
-
 	cargo build --release \
 		--jobs "$TERMUX_MAKE_PROCESSES" \
 		--target "$CARGO_TARGET_NAME" \
