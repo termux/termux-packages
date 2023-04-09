bb5da37dec0627d063eae10a935b309b15e7cfe5
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-10 01:50:02 +0900

    tsocks: Fix implicit int in `configure`
    %ci:no-build

--- packages/tsocks/build.sh
+++ packages/tsocks/build.sh
@@ -8,7 +8,6 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/tsocks/tsocks/1.8%20
 TERMUX_PKG_SHA256=849d7ef5af80d03e76cc05ed9fb8fa2bcc2b724b51ebfd1b6be11c7863f5b347
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-conf=$TERMUX_PREFIX/etc/tsocks.conf"
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 termux_step_post_get_source() {
 	cp $TERMUX_PKG_SRCDIR/tsocks-1.8/* $TERMUX_PKG_SRCDIR/
