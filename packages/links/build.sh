d5757bff985bf4c715b8d1462042334623dc93a6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-10 02:08:15 +0900

    links: Fix `configure` for strict C99
    %ci:no-build

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -8,7 +8,6 @@ TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.
 TERMUX_PKG_SHA256=22aa96c0b38e1a6f8f7ed9d7a4167a47fc37246097759ef6059ecf8f9ead7998
 TERMUX_PKG_DEPENDS="brotli, fontconfig, freetype, glib, libbz2, libcairo, libevent, libjpeg-turbo, liblzma, libpng, librsvg, libtiff, libwebp, libx11, openssl, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --mandir=$TERMUX_PREFIX/share/man
