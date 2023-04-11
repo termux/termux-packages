e57c70aecf2e04f005e9ee819940f7d35fe3b465
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-12 06:58:25 +0900

    cvs: Fix implicit function declaration

--- packages/cvs/build.sh
+++ packages/cvs/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Concurrent Versions System"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1:1.12.13"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://github.com/termux/distfiles/releases/download/2021.01.04/cvs-${TERMUX_PKG_VERSION:2}+real-26.tar.xz"
 TERMUX_PKG_SHA256=0eda91f5e8091b676c90b2a171f24f9293acb552f4e4f77b590ae8d92a547256
 TERMUX_PKG_DEPENDS="zlib, libandroid-support"
@@ -14,4 +14,3 @@ ac_cv_header_syslog_h=no
 --with-external-zlib
 "
 TERMUX_PKG_RM_AFTER_INSTALL="bin/cvsbug share/man/man8/cvsbug.8"
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
