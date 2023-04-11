73cb16357a7ad64c7433ede546de9c9e0c1d4f88
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-11 15:56:21 +0900

    cpulimit: Fix for `basename`

--- packages/cpulimit/build.sh
+++ packages/cpulimit/build.sh
@@ -3,10 +3,15 @@ TERMUX_PKG_DESCRIPTION="CPU usage limiter"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/opsengine/cpulimit/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=64312f9ac569ddcadb615593cd002c94b76e93a0d4625d3ce1abb49e08e2c2da
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
+
+termux_step_pre_configure() {
+	CPPFLAGS+=" -D__USE_GNU"
+	CFLAGS+=" $CPPFLAGS"
+}
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin src/cpulimit
