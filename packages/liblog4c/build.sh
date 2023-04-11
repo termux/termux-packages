53425ac24998b08ff4e50b2869d3ca17428adea0
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-11 12:01:28 +0900

    liblog4c: Fix implicit declaration of function
    declared in system `<malloc.h>`.
    
    %ci:no-build

--- packages/liblog4c/build.sh
+++ packages/liblog4c/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION=1.2.4
 TERMUX_PKG_SRCURL=http://prdownloads.sourceforge.net/log4c/log4c-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5991020192f52cc40fa852fbf6bbf5bd5db5d5d00aa9905c67f6f0eadeed48ea
 TERMUX_PKG_DEPENDS="libexpat"
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 termux_step_pre_configure() {
 	autoreconf -fi
