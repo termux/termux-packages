205d12ac70c28fd30b9c806960b30879c090962f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-11 12:44:33 +0900

    libwv: Fix build for Clang 16

--- packages/libwv/build.sh
+++ packages/libwv/build.sh
@@ -3,11 +3,10 @@ TERMUX_PKG_DESCRIPTION="A library which allows access to Microsoft Word files"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2.9
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=http://abiword.org/downloads/wv/${TERMUX_PKG_VERSION}/wv-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4c730d3b325c0785450dd3a043eeb53e1518598c4f41f155558385dd2635c19d
 TERMUX_PKG_DEPENDS="glib, libgsf, libpng, libxml2, zlib"
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 termux_step_pre_configure() {
 	NOCONFIGURE=1 ./autogen.sh
