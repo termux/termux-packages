3ec31c9c0390e093e74f10e4901d3bc45fe5c713
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-12 07:44:16 +0900

    libisoburn: Fix for `wait3`

--- packages/libisoburn/build.sh
+++ packages/libisoburn/build.sh
@@ -3,14 +3,13 @@ TERMUX_PKG_DESCRIPTION="Frontend for libraries libburn and libisofs"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.4
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://files.libburnia-project.org/releases/libisoburn-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=2d89846d43880f17fa591c53b3bea42ffb803628e4e630c680fc2c9184f79132
 TERMUX_PKG_DEPENDS="libburn, libisofs, readline"
 TERMUX_PKG_CONFLICTS="xorriso"
 TERMUX_PKG_BREAKS="libisoburn-dev"
 TERMUX_PKG_REPLACES="libisoburn-dev"
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 # We don't have tk.
 TERMUX_PKG_RM_AFTER_INSTALL="bin/xorriso-tcltk"
