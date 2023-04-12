e8b67c0b8d7e50b274eefc85b78af5e8304e40cc
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-12 17:43:18 +0900

    html-xml-utils: Fix implicit function declaration

--- packages/html-xml-utils/build.sh
+++ packages/html-xml-utils/build.sh
@@ -4,13 +4,14 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=8.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.w3.org/Tools/HTML-XML-utils/html-xml-utils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f20a46ac4ed30d028cd78476e5f20f5e2917a95cb7bce7df7f17d8fb3e4f79e7
-TERMUX_PKG_DEPENDS="libcurl, libidn2"
+TERMUX_PKG_DEPENDS="libcurl, libiconv, libidn2"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_ENABLE_CLANG16_PORTING=false
 
 termux_step_pre_configure() {
-	aclocal
-	automake
+	autoreconf -fi
+
+	CPPFLAGS+=" -D__USE_BSD"
 }
