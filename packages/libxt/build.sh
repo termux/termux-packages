c69450609d0d3aaa98664c2ef2c80ca7b8079c4e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-15 01:56:09 +0900

    libxt: Bump to 1.3.0

--- packages/libxt/build.sh
+++ packages/libxt/build.sh
@@ -1,15 +1,19 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="X11 toolkit intrinsics library"
-TERMUX_PKG_LICENSE="MIT"
+# Licenses: MIT, HPND
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.1
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXt-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=679cc08f1646dbd27f5e48ffe8dd49406102937109130caab02ca32c083a3d60
+TERMUX_PKG_VERSION=1.3.0
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXt-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=52820b3cdb827d08dc90bdfd1b0022a3ad8919b57a39808b12591973b331bf91
 TERMUX_PKG_DEPENDS="libice, libsm, libx11"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_path_RAWCPP=/usr/bin/cpp
+--enable-malloc0returnsnull
+"
 
 termux_step_pre_configure() {
 	export CFLAGS_FOR_BUILD=" "
