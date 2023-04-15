afd59f967662f78d1b8a805ddaaecd96ea738581
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-16 01:23:46 +0900

    kde-frameworks: Bump to 5.105.0

--- x11-packages/kcoreaddons/build.sh
+++ x11-packages/kcoreaddons/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Utilities for core application functionality and accessing the OS (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=5.104.0
+TERMUX_PKG_VERSION=5.105.0
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcoreaddons-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=ed760d4a7fed6c03480dcc3cfe621a49ac5ca9853c846080afe393f7ce794e40
+TERMUX_PKG_SHA256=915cd3ac5bb3963fe79a643d70f109a9aa198f97e6a2b5c72f09cd955238daa2
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, shared-mime-info"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 
