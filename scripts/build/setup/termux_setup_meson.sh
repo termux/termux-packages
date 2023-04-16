3aa6d9d1048f050b9ea82201bf2dde0d80b559f9
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-17 05:31:26 +0900

    termux_setup_meson: Bump Meson to 1.1.0

--- scripts/build/setup/termux_setup_meson.sh
+++ scripts/build/setup/termux_setup_meson.sh
@@ -1,6 +1,6 @@
 termux_setup_meson() {
 	termux_setup_ninja
-	local MESON_VERSION=1.0.1
+	local MESON_VERSION=1.1.0
 	local MESON_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
@@ -16,7 +16,7 @@ termux_setup_meson() {
 		termux_download \
 			"https://github.com/mesonbuild/meson/releases/download/$MESON_VERSION/meson-$MESON_VERSION.tar.gz" \
 			"$MESON_TAR_FILE" \
-			d926b730de6f518728cc7c57bc5e701667bae0c3522f9e369427b2cc7839d3c1
+			d9616c44cd6c53689ff8f05fc6958a693f2e17c3472a8daf83cee55dabff829f
 		tar xf "$MESON_TAR_FILE" -C "$TERMUX_PKG_TMPDIR"
 		shopt -s nullglob
 		local f
