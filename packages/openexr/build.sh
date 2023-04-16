05cf343b7e7d2c42a95c792cee1bdaeec9cea6b6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-16 10:31:25 +0900

    openexr: Bump to 3.1.7

--- packages/openexr/build.sh
+++ packages/openexr/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Provides the specification and reference implementation 
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # Align the version with `imath` package.
-TERMUX_PKG_VERSION=3.1.6
+TERMUX_PKG_VERSION=3.1.7
 TERMUX_PKG_SRCURL=https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=daa33d93a7b706e27368a162060df0246a7750c39a01a122d33b13f5c45d2029
+TERMUX_PKG_SHA256=78dbca39115a1c526e6728588753955ee75fa7f5bb1a6e238bed5b6d66f91fd7
 TERMUX_PKG_DEPENDS="imath (>= ${TERMUX_PKG_VERSION}), libc++, zlib"
 TERMUX_PKG_CONFLICTS="openexr2"
 TERMUX_PKG_REPLACES="openexr2"
