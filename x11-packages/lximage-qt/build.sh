9554494ece2fad1f28909f57704dd0fa8f68f892
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-16 09:48:44 +0900

    lximage-qt: Bump revision
    %ci:no-build

--- x11-packages/lximage-qt/build.sh
+++ x11-packages/lximage-qt/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="LXQt Image Viewer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION="1.3.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lximage-qt/releases/download/${TERMUX_PKG_VERSION}/lximage-qt-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=68d8d11129e5130bf6e30cfee0cb0ec775a5a5609c6a20c3c496e12f4eff34ff
 TERMUX_PKG_DEPENDS="glib, libc++, libexif, libfm-qt, libx11, libxfixes, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras"
