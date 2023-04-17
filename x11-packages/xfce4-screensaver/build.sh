1e2d7008301e57e1d1f8cbd42595c4aae32bd36b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-18 04:11:15 +0900

    xfce4-screensaver: Bump to 4.18.1

--- x11-packages/xfce4-screensaver/build.sh
+++ x11-packages/xfce4-screensaver/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Xfce Screensaver is a screen saver and locker that aims 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
 _MAJOR_VERSION=4.18
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-screensaver/${_MAJOR_VERSION}/xfce4-screensaver-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=c72b5b42242368069568dbb27a37d9549680b59e87fece413091af7ddeeee4e2
+TERMUX_PKG_SHA256=b731e8abd68a18cc5d3b523351aa8f9f293c9f63290c1781c9155259deb04a5c
 TERMUX_PKG_DEPENDS="dbus, dbus-glib, garcon, gdk-pixbuf, glib, gtk3, libcairo, libwnck, libx11, libxext, libxfce4ui, libxfce4util, libxklavier, libxss, opengl, pango, termux-auth, xfconf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-locking --sysconfdir=$TERMUX_PREFIX/etc"
 TERMUX_PKG_BUILD_IN_SRC=true
