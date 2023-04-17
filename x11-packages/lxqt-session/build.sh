5acd6211b8bdaec0dc04c5e5e8485a1b280e0415
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-18 07:15:23 +0900

    lxqt-session: Bump to 1.3.0

--- x11-packages/lxqt-session/build.sh
+++ x11-packages/lxqt-session/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="The LXQt session manager"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION="1.2.0"
+TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-session/releases/download/${TERMUX_PKG_VERSION}/lxqt-session-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=74b2937619de662bf4b338589b9febadf6551e2e00a897351b4add69781e5c66
+TERMUX_PKG_SHA256=54d435116afee8650902da774be855b7dec7042331246ead77bcd78423b64575
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtx11extras, qtxdg-tools, kwindowsystem, liblxqt, procps, libandroid-wordexp"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWITH_LIBUDEV=OFF"
