f27844fc4e33942be6718d2f288367f5cfc75e9b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-18 05:38:42 +0900

    mediainfo: Bump to 23.03

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=22.12
+TERMUX_PKG_VERSION=23.03
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=932b82739f738e7db603cf5bb170720731a9e7c61d145c2a54aabb3cd0b753bc
+TERMUX_PKG_SHA256=f061a4257565676a234b63bdd6d497e3170c8e4a2341a420bba39c4ea8baa498
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libmediainfo, libzen"
 
 termux_step_pre_configure() {
