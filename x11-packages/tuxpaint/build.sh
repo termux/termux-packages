5c30a5da4c65c96001982b2ad54faa8df899f058
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-18 11:58:36 +0900

    tuxpaint: Bump to 0.9.29

--- x11-packages/tuxpaint/build.sh
+++ x11-packages/tuxpaint/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://tuxpaint.org/
 TERMUX_PKG_DESCRIPTION="A free, award-winning drawing program for children ages 3 to 12"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.28
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/tuxpaint/tuxpaint-${TERMUX_PKG_VERSION}-sdl1.tar.gz
-TERMUX_PKG_SHA256=6f846ef46ab219fda3326336e2ccc65cedaf6d2c42c2f3e603ab601145a1868b
-TERMUX_PKG_DEPENDS="fribidi, glib, libandroid-wordexp, libcairo, libimagequant, libpaper, libpng, librsvg, sdl, sdl-gfx, sdl-image, sdl-mixer, sdl-pango, sdl-ttf, tuxpaint-data, zlib"
+TERMUX_PKG_VERSION=0.9.29
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/tuxpaint/tuxpaint-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=575403c54c7243e5b269a71fc1aa0738e3937764787e2acf89686bd77c5ae6ca
+TERMUX_PKG_DEPENDS="fribidi, libandroid-wordexp, libcairo, libimagequant, libpaper, libpng, librsvg, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf, tuxpaint-data, zlib"
+TERMUX_PKG_BUILD_DEPENDS="glib, sdl2-gfx"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
 
@@ -34,6 +34,7 @@ termux_step_pre_configure() {
 	local _PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/prefix
 	export PATH=$_PREFIX_FOR_BUILD/bin:$PATH
 
+	CPPFLAGS+=" -U__ANDROID__"
 	LDFLAGS+=" -landroid-wordexp"
 }
 
