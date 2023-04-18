b286eeb615d8c2b8cb298aa8f48b21e92a01fbf3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-18 17:44:16 +0900

    bombadillo: Bump to 2.4.0

--- packages/bombadillo/build.sh
+++ packages/bombadillo/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://bombadillo.colorfield.space/
 TERMUX_PKG_DESCRIPTION="A non-web client for the terminal, supporting Gopher, Gemini and much more"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.3.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=2.4.0
 TERMUX_PKG_SRCURL=https://tildegit.org/sloum/bombadillo/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2d4ec15cac6d3324f13a4039cca86fecf3141503f556a6fa48bdbafb86325f1c
+TERMUX_PKG_SHA256=e0daed1d9d0fe7cbea52bc3e6ecff327749b54e792774e6b985e0d64b7a36437
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
