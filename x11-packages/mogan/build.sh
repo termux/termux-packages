7bc060d68c18349366f223412ed2f43666264216
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-18 18:08:48 +0900

    mogan: Bump to 1.1.2

--- x11-packages/mogan/build.sh
+++ x11-packages/mogan/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/XmacsLabs/mogan
 TERMUX_PKG_DESCRIPTION="A structure editor forked from GNU TeXmacs"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.1
+TERMUX_PKG_VERSION=1.1.2
 TERMUX_PKG_SRCURL=https://github.com/XmacsLabs/mogan/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0d474267b7a777ae0fb22705c8515bfc24eb2de15ed69aa758dc51377fc6d9f9
+TERMUX_PKG_SHA256=1ba26b0d43e0727c9aa5c3cf7fb84890f6014b03e6409031c0512dc1c5c42348
 TERMUX_PKG_DEPENDS="freetype, ghostscript, libandroid-complex-math, libandroid-execinfo, libandroid-spawn, libc++, libcurl, libiconv, libjpeg-turbo, libpng, libsqlite, mogan-data, qt5-qtbase, qt5-qtsvg, zlib"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools"
 
