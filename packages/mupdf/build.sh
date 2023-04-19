7e6efedc047d2e7ac79e19fb74297bc12a919136
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-19 12:48:16 +0900

    mupdf: Bump to 1.22.0

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mupdf.com/
 TERMUX_PKG_DESCRIPTION="Lightweight PDF and XPS viewer (library)"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.21.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.22.0
 TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.gz
-TERMUX_PKG_SHA256=b24ddbe12506cc82de08d5cd494614ba08346f8175db1d98bcf938ff94925a54
+TERMUX_PKG_SHA256=5cae91c6332fa6cb08f1258addd27b736c4663f92698c8897b382c453534ee60
 TERMUX_PKG_DEPENDS="freetype, gumbo-parser, harfbuzz, jbig2dec, leptonica, libc++, libjpeg-turbo, openjpeg, tesseract, zlib"
 TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX build=release libs shared=yes tesseract=yes"
 TERMUX_PKG_BUILD_IN_SRC=true
