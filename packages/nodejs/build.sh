ba686ff3f5e2ebcd6cea36057b566e0c831fceec
Yaksh Bariya <yakshbari4@gmail.com>
2023-04-20 18:08:45 +0530

    upgpkg(main/nodejs): v19.9.0

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <yakshbari4@gmail.com>"
-TERMUX_PKG_VERSION=19.6.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=19.9.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7710e6c2851c956be4926fc2540678f3fddf8e6d3826ea3aa9b8c28d6ea89a9b
+TERMUX_PKG_SHA256=c7fce9d46ca6ce0d8990433cbf601bb9279c0eaed8705b357018cf50be9bed29
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
