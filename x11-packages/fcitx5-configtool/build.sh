bae0ee1a158c264f44c627c4347b286d861532e5
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2023-04-22 02:32:19 +0900

    add fcitx5-configtool

---
+++ x11-packages/fcitx5-configtool/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://fcitx-im.org/
+TERMUX_PKG_DESCRIPTION="Configuration tool for Fcitx5"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=5.0.17
+TERMUX_PKG_SRCURL=https://github.com/fcitx/fcitx5-configtool/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=cb17484745027b339fcea58d6b17d0fde37a891f33aa4c1205625045ce41364d
+TERMUX_PKG_DEPENDS="fcitx5, fcitx5-qt, iso-codes, kitemviews, kwidgetsaddons, libc++, libx11, libxkbfile, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, xkeyboard-config"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DENABLE_TEST=OFF
+-DENABLE_CONFIG_QT=ON
+-DENABLE_KCM=OFF
+"
