bfc1b25a9d2e8c3cce33d60cfdb083a1b5a4f1d7
be2020 <be2020>
2023-04-24 01:13:02 +0200

    tcc : fix arm build
    * armv7a (androideabi) config fixed
    * obsolete patches removed
    * add ndk-sysroot dependency (needed to use the compiler)
    
    Note:
    - patch was NOT tested under the termux package system really
    - however the tcc configuration was tested for armv7a to produce
      a working package that, when installed on device, can compile
      tcc again and pass the tests. As in
        $ git clone git://repo.or.cz/tinycc.git
        $ cd tinycc
        $ ./configure --cc=tcc
        $ make && make test

--- packages/tcc/build.sh
+++ packages/tcc/build.sh
@@ -2,14 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://bellard.org/tcc/
 TERMUX_PKG_DESCRIPTION="Tiny C Compiler"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=6a24b762d3e1086dcffd002c68cb5ca3a33a5c6d
-_COMMIT_DATE=20230415
+_COMMIT=bb93bf8cd29140610f0d94f6565501d30215bd98
+_COMMIT_DATE=20230425
 TERMUX_PKG_VERSION=1:0.9.27-p${_COMMIT_DATE}
 TERMUX_PKG_SRCURL=git+https://repo.or.cz/tinycc.git
-TERMUX_PKG_SHA256=467792219d0172f594ec71bcd6bac9dbb25308cbe9f708bab455b717148b491b
+TERMUX_PKG_SHA256=ed447c67ae71a7ebc0754a6cbd3bd11eb06cd24b70b81ac9227eb0e2d01dabdf
 TERMUX_PKG_GIT_BRANCH=mob
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_DEPENDS="ndk-sysroot"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
@@ -29,22 +30,12 @@ termux_step_post_get_source() {
 	fi
 }
 
-termux_step_pre_configure() {
-	cd $TERMUX_PKG_BUILDDIR
-	rm -rf _bin
-	mkdir _bin
-	cd _bin
-	local _ar="$TERMUX_HOST_PLATFORM-ar"
-	cat > "${_ar}" <<-EOF
-		#!$(command -v sh)
-		exec $(command -v $AR) "\$@"
-	EOF
-	chmod 0700 "${_ar}"
-	export PATH="$(pwd):$PATH"
+termux_step_configure() {
+
+        echo "configuring later..."
 }
 
-termux_step_configure() {
-	unset CFLAGS CXXFLAGS
+termux_step_make() {
 
 	if [ "${TERMUX_ARCH}" = "arm" ] || [ "${TERMUX_ARCH}" = "i686" ]; then
 		ELF_INTERPRETER_PATH="/system/bin/linker"
@@ -53,56 +44,54 @@ termux_step_configure() {
 		ELF_INTERPRETER_PATH="/system/bin/linker64"
 		ANDROID_LIB_PATH="/system/lib64:/system/vendor/lib64"
 	fi
-}
 
-termux_step_make() {
+	# armeabi needs specific configuration
+	if [ "${TERMUX_ARCH}" = "arm" ]; then
+		TCC_CONFIG="--cpu=armv7a --config-arm_eabi --config-arm_vfp"
+	else
+		TCC_CONFIG="--cpu=$TERMUX_ARCH"
+	fi
+
+	TCC_CONFIG="$TCC_CONFIG --config-Android --config-new-dtags"
+
+	if [ "${TERMUX_ARCH}" != "i686" ]; then
+		TCC_CONFIG="$TCC_CONFIG --config-pie"
+	fi
+
 	(
-		sysinc=
-		otherinc=
-		for d in $(echo | $CC -E -x c - -v 2>&1 | \
-				sed -n '/^#include <...> search/,/^End/p' | \
-				grep '^\s'); do
-			p="$(readlink -f "${d}"):"
-			if [[ "${d}" = */sysroot/usr/* ]]; then
-				sysinc+="${p}"
-			else
-				otherinc+="${p}"
-			fi
-		done
-		sysinc="${sysinc}${otherinc%:}"
+		# hope to find 'include' there
+		local NDK_SYSROOT="${TERMUX_STANDALONE_TOOLCHAIN}/sysroot/usr"
+
 		unset CC CFLAGS LDFLAGS
+		# make runtime (libtcc1.a and b*.o) for target
 		./configure \
-			--prefix="/tmp/tcc.host" \
-			--cpu="${TERMUX_ARCH}" \
-			--sysincludepaths="${sysinc}"
-		make -j $TERMUX_MAKE_PROCESSES tcc
-		mv -f tcc tcc.host
-		make distclean
+			$TCC_CONFIG \
+			--sysroot="$NDK_SYSROOT" \
+			--sysincludepaths="{B}/include:{R}/include/$TERMUX_HOST_PLATFORM:{R}/include"
+
+		make -j $TERMUX_MAKE_PROCESSES libtcc1.a
 	)
 
+	# cross-compile tcc for target
 	./configure \
+		--cross-prefix="${CC%-*}-" --cc="${CC##*-}" \
+		$TCC_CONFIG \
 		--prefix="$TERMUX_PREFIX" \
-		--cross-prefix="${CC//clang}" \
-		--cc="clang" \
-		--cpu="$TERMUX_ARCH" \
+		--sysroot="$TERMUX_PREFIX" \
 		--disable-rpath \
+		--disable-static \
+		--sysincludepaths="{B}/include:{R}/include/$TERMUX_HOST_PLATFORM:{R}/include" \
+		--libpaths="{B}:{R}/lib:$ANDROID_LIB_PATH" \
+		--crtprefix="{R}/lib" \
 		--elfinterp="$ELF_INTERPRETER_PATH" \
-		--crtprefix="$TERMUX_PREFIX/lib/tcc/crt" \
-		--sysincludepaths="$TERMUX_PREFIX/include/$TERMUX_HOST_PLATFORM:$TERMUX_PREFIX/include:$TERMUX_PREFIX/lib/tcc/include" \
-		--libpaths="$TERMUX_PREFIX/lib:$TERMUX_PREFIX/lib/tcc:$ANDROID_LIB_PATH"
+		--tcc-switches="-Wl,-rpath=$TERMUX_PREFIX/lib,-section-alignment=0x1000"
 
-	mv tcc.host tcc
-	touch -d "next minute" tcc
-	make -j ${TERMUX_MAKE_PROCESSES} libtcc1.a
+	# override AR from config.mak and LIBS (-lpthread not needed)
+	make -j ${TERMUX_MAKE_PROCESSES} tcc AR="$AR" LIBS="-lm -ldl"
+	make doc
+}
 
-	rm -f tcc
-	make -j ${TERMUX_MAKE_PROCESSES} tcc
+termux_step_make_install() {
 
 	make install
-
-	for file in crtbegin_dynamic.o crtbegin_so.o crtend_android.o crtend_so.o; do
-		install -Dm600 \
-			"${TERMUX_STANDALONE_TOOLCHAIN}/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/$file" \
-			"${TERMUX_PREFIX}/lib/tcc/crt/$file"
-	done
 }
