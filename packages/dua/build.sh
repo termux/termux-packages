4d615811facc0f963093476591d023dc54c8fc33
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-04-25 17:33:19 +0600

    new package: dua

---
+++ packages/dua/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/Byron/dua-cli
+TERMUX_PKG_DESCRIPTION="View disk space usage and delete unwanted data, fast"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="SunPodder <contact.sunpodder09@gmail.com>"
+TERMUX_PKG_VERSION=2.19.2
+TERMUX_PKG_SRCURL=https://github.com/Byron/dua-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=5d508ea6374702f04f4b21f6dfb3d287922d3b7d5860dfc3fce2366128f371a0
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
+
+termux_step_make() {
+	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release --no-default-features --features tui-crossplatform
+}
+
+termux_step_make_install() {
+	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/dua
+}
+
