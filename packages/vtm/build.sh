401258d6eea5cdd85f7c74f6f06482d66030d262
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-27 15:00:29 +0900

    vtm: Bump to 0.9.9f

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-p
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.9c"
+TERMUX_PKG_VERSION=0.9.9f
 TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=de0b0134ab2a4a8640ac35b5fe3805eeda77f2575b65720c272ccd8aed388c69
-TERMUX_PKG_DEPENDS="libandroid-spawn, libc++"
+TERMUX_PKG_SHA256=9681e59ba0f95959000b12bcd68bf7c58d1c16ae7211a3846ae378b9864d6b1d
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+=/src
 	CXXFLAGS+=" -pthread"
-	LDFLAGS+=" -landroid-spawn"
 }
