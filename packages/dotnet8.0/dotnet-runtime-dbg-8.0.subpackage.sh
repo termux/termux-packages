c071f9e7ddc94fea4183e391d2a78784165508f3
Jia Yuan Lo <jylo06g@gmail.com>
2023-04-27 16:05:40 +0800

    addpkg(main): dotnet8.0

---
+++ packages/dotnet8.0/dotnet-runtime-dbg-8.0.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DESCRIPTION=".NET 8.0 Runtime Managed Debug Symbols"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
+TERMUX_SUBPKG_DEPENDS="dotnet-runtime-8.0"
+TERMUX_SUBPKG_INCLUDE=$(cat "${TERMUX_PKG_TMPDIR}"/dotnet-runtime-dbg.txt)
---
+++ packages/netstandard-targeting-pack-2.1/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=https://dotnet.microsoft.com/en-us/
+TERMUX_PKG_DESCRIPTION="A metapackage for NETStandard.Library 2.1 Targeting Pack"
+TERMUX_PKG_LICENSE="Public Domain"
+TERMUX_PKG_MAINTAINER="@truboxl"
+TERMUX_PKG_VERSION="8.0"
+TERMUX_PKG_DEPENDS="netstandard-targeting-pack-2.1-8.0"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="netstandard-targeting-pack-2.1-8.0"
+TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_METAPACKAGE=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
