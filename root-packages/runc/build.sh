22c2fdf93e8464bce08510e69b1cd87555e9943c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-28 17:45:26 +0900

    runc: Bump to 1.1.7

--- root-packages/runc/build.sh
+++ root-packages/runc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.opencontainers.org/
 TERMUX_PKG_DESCRIPTION="A tool for spawning and running containers according to the OCI specification"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.6
+TERMUX_PKG_VERSION=1.1.7
 TERMUX_PKG_SRCURL=https://github.com/opencontainers/runc/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1401d11f8543af6c4fd5f9c7654ffb3ad21f89ea86be74dba481213c2afb6af3
+TERMUX_PKG_SHA256=f1885d6dfa188f8112328ac2355e5d67346174a2e2e795ec514a972bcbfcc2fa
 TERMUX_PKG_DEPENDS="libseccomp"
 
 termux_step_make() {
