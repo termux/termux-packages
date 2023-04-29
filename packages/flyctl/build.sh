6101f222a0c726acf7405717ae6b1c53c36a5b0c
Yaksh Bariya <thunder-coding@termux.dev>
2023-04-29 19:55:45 +0530

    fixup: also revbump packages whose email ids have been updated

--- packages/flyctl/build.sh
+++ packages/flyctl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command line tools for fly.io services"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
 TERMUX_PKG_VERSION="0.0.548"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/superfly/flyctl/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=06844bbc4e430a68399e25786414aaee029ef1238252be2f221b7056703de695
 TERMUX_PKG_AUTO_UPDATE=true
