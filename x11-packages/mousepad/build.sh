5258831414991a1e38a93d2642fd8f36162cbc99
Tee KOBAYASHI <xtkoba@gmail.com>
2023-04-30 22:37:08 +0900

    mousepad: Rebuild to remove `gschemas.compiled`

--- x11-packages/mousepad/build.sh
+++ x11-packages/mousepad/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.6
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/mousepad/${_MAJOR_VERSION}/mousepad-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=2253a5c582b8a899d842a8e4311d6b760435ad7cca493ff4edf305b89c1913d4
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, gtksourceview4, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfconf, zlib"
