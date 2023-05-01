d1f94ad0bbd39de63fdcc63efe13fab86017aeae
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-01 17:26:26 +0900

    new package: libimobiledevice-glue

---
+++ packages/libimobiledevice-glue/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org/
+TERMUX_PKG_DESCRIPTION="Library with common code used by the libraries and tools around the libimobiledevice project"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/${TERMUX_PKG_VERSION}/libimobiledevice-glue-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=160a70e2edc318312fc40b6a71f85bfdfabdfba10bcfc5bb6fb40ed95088f4a0
+TERMUX_PKG_DEPENDS="libplist"
