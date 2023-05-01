50532cc0f99918cc51c50db279e74cfe0fec64e6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-01 13:48:35 +0900

    less: Bump to 632

--- packages/less/build.sh
+++ packages/less/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.greenwoodsoftware.com/less/
 TERMUX_PKG_DESCRIPTION="Terminal pager program used to view the contents of a text file one screen at a time"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=608
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=632
 TERMUX_PKG_SRCURL=https://www.greenwoodsoftware.com/less/less-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a69abe2e0a126777e021d3b73aa3222e1b261f10e64624d41ec079685a6ac209
+TERMUX_PKG_SHA256=6f71b2a9178ddad8a238259032f3c9d21929846ce453af2a77fc906ccc31d4d9
 TERMUX_PKG_DEPENDS="ncurses, pcre2"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-regex=pcre2"
