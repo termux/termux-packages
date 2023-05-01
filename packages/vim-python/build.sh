16189907188b982d953f5ae398bbf07bc0bec9bc
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-01 11:49:00 +0900

    vim{,-python,-gtk}: Bump to 9.0.1500

--- packages/vim-python/build.sh
+++ packages/vim-python/build.sh
@@ -6,9 +6,9 @@ TERMUX_PKG_DEPENDS="libiconv, ncurses, vim-runtime, python"
 TERMUX_PKG_RECOMMENDS="diffutils"
 # vim should only be updated every 50 releases on multiples of 50.
 # Update all of vim, vim-python and vim-gtk to the same version in one PR.
-TERMUX_PKG_VERSION=9.0.1450
+TERMUX_PKG_VERSION=9.0.1500
 TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=432373dd7c5eb5f8c8b9ea5b9d98c0ac73d7ccdb8c4f4076310a5353692e8fe5
+TERMUX_PKG_SHA256=4a473e8e387a0f947b7e27db4978c162f7ddc03273e080206b5adebd2ef9355e
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 vim_cv_getcwd_broken=no
 vim_cv_memmove_handles_overlap=yes
