b9054e31d245d9fc660780cd9d53fd29cc6790db
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-03 17:08:27 +0900

    debianutils: Remove `bin/which`
    which will be provided by another package named `which`.

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installa
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.orig.tar.gz
 TERMUX_PKG_SHA256=27ec9e0e7e44dc8ab611aa576330471bacb07e4491ffecf0d3aa6909c92f9022
 
@@ -11,6 +12,8 @@ bin/add-shell
 bin/installkernel
 bin/remove-shell
 bin/update-shells
+bin/which
+share/man/man1/which.1
 share/man/man8/add-shell.8
 share/man/man8/installkernel.8
 share/man/man8/remove-shell.8
