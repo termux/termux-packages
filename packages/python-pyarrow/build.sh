21b2a0d97712ef8292c649471ae093f756db3c66
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-04 02:07:50 +0900

    libarrow-cpp, python-pyarrow: Bump to 12.0.0

--- packages/python-pyarrow/build.sh
+++ packages/python-pyarrow/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Python bindings for Apache Arrow"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Align the version with `libarrow-cpp` package.
-TERMUX_PKG_VERSION=11.0.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=12.0.0
 TERMUX_PKG_SRCURL=https://github.com/apache/arrow/archive/refs/tags/apache-arrow-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4a8c0c3d5b39ca81f4a636a41863f1cf5e0ed199f994bf5ead0854ca037eb741
+TERMUX_PKG_SHA256=f25901c486e1e79cde8b78b3e7b1d889919f942549996003a7341a8ee86addaa
 TERMUX_PKG_DEPENDS="libarrow-cpp (>= ${TERMUX_PKG_VERSION}), libc++, python, python-numpy"
 TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, numpy, wheel"
 TERMUX_PKG_PROVIDES="libarrow-python"
