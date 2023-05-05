6ba9e5d490f38f6fa39f81d81bc21a9d98d6fd29
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-06 02:11:14 +0900

    libprotobuf: Bump to 22.4

--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -14,7 +14,7 @@ termux_setup_protobuf() {
 			termux_download \
 				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
 				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				3c830b09192a8c40c599856eb184c89ee5029d7dab9df8ec6d3d6741dcb94b93
+				b3cff4d4076efe2965e8560cae6307c429349eda0af7755950d6ea5db6d72870
 
 			rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
 			unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
