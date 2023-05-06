8cd1d3ae71828fb4bc0afd65cbb39af43ac12294
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-06 22:55:07 +0900

    botan: Avoid conflict with potential botan3 package

--- packages/botan/build.sh
+++ packages/botan/build.sh
@@ -2,7 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://botan.randombit.net/
 TERMUX_PKG_DESCRIPTION="Crypto and TLS for Modern C++"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
+# This specific package is for libbotan-2.
 TERMUX_PKG_VERSION=2.19.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://botan.randombit.net/releases/Botan-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=dae047f399c5a47f087db5d3d9d9e8f11ae4985d14c928d71da1aff801802d55
 TERMUX_PKG_DEPENDS="libbz2, libc++, liblzma, libsqlite, zlib"
@@ -18,6 +20,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-sqlite3
 --with-zlib
 --prefix=$TERMUX_PREFIX
+--program-suffix=$(echo ${TERMUX_PKG_VERSION#*:} | cut -d . -f 1)
 "
 
 termux_step_pre_configure() {
@@ -27,4 +30,11 @@ termux_step_pre_configure() {
 termux_step_configure() {
 	python3 $TERMUX_PKG_SRCDIR/configure.py \
 		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+}
+
+termux_step_post_massage() {
+	local _GUARD_FILE="lib/libbotan-2.so"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "Error: file ${_GUARD_FILE} not found."
+	fi
 }
