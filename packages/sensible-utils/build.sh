227bed3651e09a3e77e4b152e095bcaebf8aed5f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-09 03:57:37 +0900

    sensible-utils: Bump to 0.0.19

--- packages/sensible-utils/build.sh
+++ packages/sensible-utils/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/sid/sensible-utils
 TERMUX_PKG_DESCRIPTION="Small utilities used by programs to sensibly select and spawn an appropriate browser, editor, or pager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.0.18
+TERMUX_PKG_VERSION=0.0.19
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/s/sensible-utils/sensible-utils_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ccb0a442ed720e815e914089962a01a45bb2ea7ba01dbaa03676ebef53430dc5
+TERMUX_PKG_SHA256=3af5fe874c9e643b3d413d4f1b71009194c9093225e9dd49de86e2e604721d69
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_prog_PO4A=/bin/echo"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
