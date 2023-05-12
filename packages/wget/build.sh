24a373cdcf5bec59373b92eac9167a4e23461f20
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-13 05:24:40 +0900

    wget: Bump to 1.21.4

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/wget/
 TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS and FTP"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.21.3
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=1.21.4
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
-TERMUX_PKG_SHA256=dbd2fb5e47149d4752d0eaa0dac68cc49cf20d46df4f8e326ffc8f18b2af4ea5
-TERMUX_PKG_DEPENDS="libiconv, pcre2, openssl, libuuid, libandroid-support, libandroid-spawn, zlib"
+TERMUX_PKG_SHA256=3683619a5f50edcbccb1720a79006fa37bf9b9a255a8c5b48048bc3c7a874bd9
+TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libuuid, openssl, pcre2, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_getpass=yes
 ac_cv_libunistring=no
@@ -15,7 +14,3 @@ ac_cv_libunistring=no
 --with-included-libunistring=no
 --without-libunistring-prefix
 "
-
-termux_step_pre_configure() {
-	export LDFLAGS+=" -landroid-spawn"
-}
