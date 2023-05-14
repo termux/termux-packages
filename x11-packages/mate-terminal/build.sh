69709c43f60fec73a7c4221dae732e0d7a9df1dd
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-15 03:34:58 +0900

    mate-terminal: Bump to 1.26.1

--- x11-packages/mate-terminal/build.sh
+++ x11-packages/mate-terminal/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://mate-terminal.mate-desktop.dev/
 TERMUX_PKG_DESCRIPTION="This is the MATE terminal emulator application"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.26.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1.26.1
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-terminal/releases/download/v$TERMUX_PKG_VERSION/mate-terminal-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=7727e714c191c3c55e535e30931974e229ca5128e052b62ce74dcc18f7eaaf22
+TERMUX_PKG_SHA256=7c130206f0b47887e8c9274e73f8c19fae511134572869a7c23111b789e1e1d0
 TERMUX_PKG_DEPENDS="libvte, dconf, gtk3, libsm, mate-desktop"
 
 termux_step_pre_configure() {
