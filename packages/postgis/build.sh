52d1422b96ecb1d3a8ed1275e8c6907329687093
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-15 07:32:11 +0900

    gdal: Revbump revdeps to rebuild
    against GDAL 3.7.

--- packages/postgis/build.sh
+++ packages/postgis/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Spatial database extender for PostgreSQL object-relation
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.3.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.osgeo.org/postgis/source/postgis-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9a2a219da005a1730a39d1959a1c7cec619b1efb009b65be80ffc25bad299068
 TERMUX_PKG_DEPENDS="gdal, json-c, libc++, libgeos, libiconv, libprotobuf-c, libxml2, pcre2, postgresql, proj"
