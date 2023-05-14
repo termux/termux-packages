70bac7497eaafc4da345a4e8a1996272c48748c1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-15 05:41:18 +0900

    lighttpd: Bump to 1.4.70

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.lighttpd.net
 TERMUX_PKG_DESCRIPTION="Fast webserver with minimal memory footprint"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.69
+TERMUX_PKG_VERSION=1.4.70
 TERMUX_PKG_SRCURL=https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=16ac8db95e719629ba61949b99f8a26feba946a81d185215b28379bb4116b0b4
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-pcre --with-zlib"
-TERMUX_PKG_DEPENDS="libbz2, openssl, pcre, libcrypt, libandroid-glob, zlib"
+TERMUX_PKG_SHA256=921ebe1cf4b6b9897e03779ab7a23a31f4ba40a1abe2067525c33cd3ce61fe85
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-zlib"
+TERMUX_PKG_DEPENDS="libandroid-glob, libbz2, libcrypt, openssl, pcre2, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/lighttpd-angel"
 TERMUX_PKG_SERVICE_SCRIPT=("lighttpd" "if [ -f \"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\" ]; then CONFIG=\"$TERMUX_ANDROID_HOME/.lighttpd/lighttpd.conf\"; else CONFIG=\"$TERMUX_PREFIX/etc/lighttpd/lighttpd.conf\"; fi\nexec lighttpd -D -f \$CONFIG 2>&1")
 
