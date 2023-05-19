bc27099eee586da48faafb15d2fe9657a4e17698
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-20 08:07:51 +0900

    lnd: Bump to 0.16.2-beta

--- packages/lnd/build.sh
+++ packages/lnd/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/lightningnetwork/lnd
 TERMUX_PKG_DESCRIPTION="Lightning Network Daemon"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.16.0-beta
+TERMUX_PKG_VERSION=0.16.2-beta
 TERMUX_PKG_SRCURL=(https://github.com/lightningnetwork/lnd/archive/v${TERMUX_PKG_VERSION}.tar.gz
                    https://github.com/lightningnetwork/lnd/releases/download/v${TERMUX_PKG_VERSION}/vendor.tar.gz)
-TERMUX_PKG_SHA256=(d56d1ac46d80eea42d6e3bec470874891410b18696602e684888a17c6ea22949
-                   6655d2db7a2db3ead93d8d48d81a7b81828eb0e4403dbaa33c44d40013230985)
+TERMUX_PKG_SHA256=(358310e2f7745e7b9f0a6de69aee7764677d6b55842ab9df343acd612f8c69f2
+                   2d1cd328be014e96d7295199479bb35e080ed48af1561f4e5bcb2d4af3f456eb)
 TERMUX_PKG_DEPENDS="bitcoin"
 TERMUX_PKG_SERVICE_SCRIPT=("lnd" 'exec lnd 2>&1')
 TERMUX_PKG_BUILD_IN_SRC=true
