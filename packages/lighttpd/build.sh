2011d22a18fb10c89c1d6d7becf91805aa45c63b
Leonid Pliushch <sylirre@termux.dev>
2023-05-20 02:14:46 +0300

    fix(main/lighttpd): update default configuration

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast webserver with minimal memory footprint"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4.70
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=921ebe1cf4b6b9897e03779ab7a23a31f4ba40a1abe2067525c33cd3ce61fe85
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-bzip2 --with-openssl --with-zlib"
@@ -16,6 +17,11 @@ termux_step_pre_configure() {
 
 termux_step_post_make_install() {
 	# Install example config file
-	mkdir -p $TERMUX_PREFIX/etc/lighttpd
-	install -Dm600 $TERMUX_PKG_SRCDIR/doc/config/lighttpd.conf $TERMUX_PREFIX/etc/lighttpd/
+	install -Dm600 -t $TERMUX_PREFIX/etc/lighttpd/ \
+		$TERMUX_PKG_SRCDIR/doc/config/lighttpd.conf \
+		$TERMUX_PKG_SRCDIR/doc/config/modules.conf
+	install -Dm600 -t $TERMUX_PREFIX/etc/lighttpd/conf.d \
+		$TERMUX_PKG_SRCDIR/doc/config/conf.d/*.conf
+	install -Dm600 -t $TERMUX_PREFIX/etc/lighttpd/vhosts.d \
+		$TERMUX_PKG_SRCDIR/doc/config/vhosts.d/vhosts.template
 }
