ed319805f47b3b8794ca0c287ffa3a26a6111cfa
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-20 07:23:54 +0900

    pipewire: Bump to 0.3.71

--- packages/pipewire/build.sh
+++ packages/pipewire/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A server and user space API to deal with multimedia pipe
 TERMUX_PKG_LICENSE="MIT, LGPL-2.1, LGPL-3.0, GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.70
+TERMUX_PKG_VERSION=0.3.71
 TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/${TERMUX_PKG_VERSION}/pipewire-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=78c0fd2dd0eb37f24e0e802df32cdaabf0a7e4f03fed572bb0dbb0506edbbe40
+TERMUX_PKG_SHA256=91a6dd0b6b7dd478bdebd5e9de66c6ac2327b40a34a54a133fd0f6a0fb986e7f
 TERMUX_PKG_DEPENDS="avahi, ffmpeg, glib, libc++, liblua54, libopus, libsndfile, libwebrtc-audio-processing, lilv, ncurses, openssl, pulseaudio, readline"
 
 # 'media-session' session-managers is disabled as it requires alsa.
