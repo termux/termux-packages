fa2aa45a66a1d4e7a07f34da4f727593a27814ad
Tee KOBAYASHI <xtkoba@gmail.com>
2023-05-21 13:09:26 +0900

    algernon: Bump to 1.15.1

--- packages/algernon/build.sh
+++ packages/algernon/build.sh
@@ -2,33 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://algernon.roboticoverlords.org/
 TERMUX_PKG_DESCRIPTION="Small self-contained web server with Lua, Markdown, QUIC, Redis and PostgreSQL support"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=3d7448c5211ce33de0dbed186689146e497dc405
-_COMMIT_DATE=20230301
-TERMUX_PKG_VERSION=1.14.0-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=1
-#TERMUX_PKG_SRCURL="https://github.com/xyproto/algernon/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SRCURL=git+https://github.com/xyproto/algernon
-TERMUX_PKG_SHA256=5ed11b7371d8a8be3c62bbf234e18228d920f1c0dc28da33daf6a0f9b316fa3d
-TERMUX_PKG_GIT_BRANCH=main
-TERMUX_PKG_AUTO_UPDATE=false
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
-	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
-		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
-		echo -n " different from what is expected to be; should end"
-		echo " with \"${pdate}\"."
-		return 1
-	fi
-
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
-	fi
-}
+TERMUX_PKG_VERSION=1.15.1
+TERMUX_PKG_SRCURL="https://github.com/xyproto/algernon/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=023779d8516da9ce5f3b1af9d0173652f2f1fab0d036b636e4185c3b8d5242c5
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
