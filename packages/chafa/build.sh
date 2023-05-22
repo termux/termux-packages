52a9264512bcf1155121c38fdb226ef1c732ed2c
Yonle <yonle@lecturify.net>
2023-05-22 20:34:14 +0700

    Chafa: Upgrade to 1.12.5
    https://github.com/hpjansson/chafa/releases/tag/1.12.5

--- packages/chafa/build.sh
+++ packages/chafa/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE="https://hpjansson.org/chafa/"
 TERMUX_PKG_DESCRIPTION="Image-to-text converter supporting a wide range of symbols, etc."
 TERMUX_PKG_LICENSE="LGPL-3.0"
-TERMUX_PKG_MAINTAINER="Yonle <yonle@protonmail.com>"
-TERMUX_PKG_VERSION=1.12.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_MAINTAINER="Yonle <yonle@lecturify.net>"
+TERMUX_PKG_VERSION=1.12.5
 TERMUX_PKG_SRCURL="https://github.com/hpjansson/chafa/releases/download/${TERMUX_PKG_VERSION}/chafa-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=9774bd1a7076ea3124f7fea811e371d0e1da2e76b7ac06260d63a86c7b1a573f
+TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 TERMUX_PKG_DEPENDS="freetype, glib, imagemagick, libcairo, libjpeg-turbo, librsvg, libtiff, libwebp"
 
