1d46077779b042fe5ce325866d9e3c3f961909c7
Yonle <yonle@duck.com>
2023-05-23 21:38:10 +0700

    upgpkg(main/chafa): 1.12.5 (#16744)

--- packages/chafa/build.sh
+++ packages/chafa/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE="https://hpjansson.org/chafa/"
 TERMUX_PKG_DESCRIPTION="Image-to-text converter supporting a wide range of symbols, etc."
 TERMUX_PKG_LICENSE="LGPL-3.0"
-TERMUX_PKG_MAINTAINER="Yonle <yonle@protonmail.com>"
-TERMUX_PKG_VERSION=1.12.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_MAINTAINER="Yonle <yonle@lecturify.net>"
+TERMUX_PKG_VERSION=1.12.5
 TERMUX_PKG_SRCURL="https://github.com/hpjansson/chafa/releases/download/${TERMUX_PKG_VERSION}/chafa-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=9774bd1a7076ea3124f7fea811e371d0e1da2e76b7ac06260d63a86c7b1a573f
+TERMUX_PKG_SHA256=0f5490d52a500a6b386f15cc04c6e8702afd0285d422b9575b332e0c683957f2
 TERMUX_PKG_DEPENDS="freetype, glib, imagemagick, libcairo, libjpeg-turbo, librsvg, libtiff, libwebp"
 
