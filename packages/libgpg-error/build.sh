293c884047f853f09a69760b6f5cb0c04d5eb53f
Biswapriyo Nath <nathbappai@gmail.com>
2023-05-24 10:51:25 +0530

    libgpg-error: Remove 'unknown' string in version number
    This changes the version string from '1.47-unknown' to '1.47'

--- packages/libgpg-error/build.sh
+++ packages/libgpg-error/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small library that defines common error values for all G
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.47
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1bcbbb9bdb
 TERMUX_PKG_BREAKS="libgpg-error-dev"
