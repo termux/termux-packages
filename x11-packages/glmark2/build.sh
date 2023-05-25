3bddc3922a06d57ef61cb66c02977d9a97f7b344
Twaik Yont <twaikyont@gmail.com>
2023-05-25 11:57:48 +0300

    fix build dependencies for glmark2

--- x11-packages/glmark2/build.sh
+++ x11-packages/glmark2/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=2023.01
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/glmark2/glmark2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8fece3fc323b643644a525be163dc4931a4189971eda1de8ad4c1712c5db3d67
-TERMUX_PKG_DEPENDS="libjpeg-turbo, libx11, opengl"
+TERMUX_PKG_DEPENDS="libjpeg-turbo, libx11, opengl, libpng, libjpeg-turbo"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dflavors=x11-gl,x11-glesv2,x11-gl-egl
 "
