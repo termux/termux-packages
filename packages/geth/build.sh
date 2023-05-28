5b6b3ee90df2eacf93763785ebc1ec8013c96b08
Biswapriyo Nath <nathbappai@gmail.com>
2023-05-28 11:09:32 +0530

    bump(main/geth): 1.12.0
    checkpoint-admin was removed in https://github.com/ethereum/go-ethereum/commit/1e556d220c3a40286dd90b37a08bb5fc659ee6ee

--- packages/geth/build.sh
+++ packages/geth/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://geth.ethereum.org/
 TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.11.6"
+TERMUX_PKG_VERSION="1.12.0"
 TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1c34d609bc9d308ce1e0d4fbf5015ae07ef5ffa5144594824f85e39a54fd2b3f
+TERMUX_PKG_SHA256=dcd73f341d89f6a195bcdd115066bc7b0ea2ca9983be351ea06a3d41e621f678
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SUGGESTS="geth-utils"
 
@@ -16,14 +16,14 @@ termux_step_make() {
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/ethereum/go-ethereum
 
 	cd "$GOPATH"/src/github.com/ethereum/go-ethereum
-	for applet in geth abidump bootnode checkpoint-admin clef devp2p ethkey evm faucet p2psim rlpdump; do
+	for applet in abidump abigen bootnode clef devp2p ethkey evm faucet geth p2psim rlpdump; do
 		(cd ./cmd/"$applet" && go build -v)
 	done
 	unset applet
 }
 
 termux_step_make_install() {
-	for applet in geth abidump bootnode checkpoint-admin clef devp2p ethkey evm faucet p2psim rlpdump; do
+	for applet in abidump abigen bootnode clef devp2p ethkey evm faucet geth p2psim rlpdump; do
 		install -Dm700 \
 			"$TERMUX_PKG_SRCDIR/cmd/$applet/$applet" \
 			"$TERMUX_PREFIX"/bin/
