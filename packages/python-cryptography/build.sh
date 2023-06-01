5ebbce060c6a289222df7e8e144e7ada61d0de43
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-06-01 12:31:11 +0800

    python-cryptography: bump to 41.0.0

--- packages/python-cryptography/build.sh
+++ packages/python-cryptography/build.sh
@@ -1,24 +1,18 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/pyca/cryptography
 TERMUX_PKG_DESCRIPTION="Provides cryptographic recipes and primitives to Python developers"
-# Licenses: Apache-2.0, BSD 3-Clause, PSFL
+# Licenses: Apache-2.0, BSD 3-Clause
 TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.APACHE, LICENSE.BSD, LICENSE.PSF"
+TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.APACHE, LICENSE.BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="40.0.2"
+TERMUX_PKG_VERSION="41.0.0"
 TERMUX_PKG_SRCURL=https://github.com/pyca/cryptography/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=501826c7ff839e5d5f31d451996057229b093094c296667fa32219356911380d
+TERMUX_PKG_SHA256=7513a27e7d5b8be18997a1b8ca52ede76295d7b90697eb2bed6c336f266b1786
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, python, python-pip"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, cffi, setuptools-rust"
 TERMUX_PKG_PYTHON_TARGET_DEPS="'cffi>=1.12'"
-
-termux_step_post_get_source() {
-	echo "Applying openssl-libs.diff"
-	sed "s%@PYTHON_VERSION@%$TERMUX_PYTHON_VERSION%g" \
-		$TERMUX_PKG_BUILDER_DIR/openssl-libs.diff | patch --silent -p1
-}
 
 termux_step_configure() {
 	termux_setup_rust
