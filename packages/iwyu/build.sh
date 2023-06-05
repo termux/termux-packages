fda9f040b8b2c7c422c63f10c70effa2f779e76b
Lucy Phipps <landfillbaby69@gmail.com>
2023-06-05 03:53:48 +0100

    iwyu: revbump to rebuild

--- packages/iwyu/build.sh
+++ packages/iwyu/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE=NCSA
 TERMUX_PKG_MAINTAINER="@termux"
 # Update this when libllvm is updated:
 TERMUX_PKG_VERSION=0.20
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/include-what-you-use/include-what-you-use/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=4fe4bd5581ee60bb2bcf1bf40f087a8c5b090d943d0376233638f30f29af0e2e
 TERMUX_PKG_AUTO_UPDATE=false # can't be auto-updated since release correspond to clang version.
