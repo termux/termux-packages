c9be0f944534409b483b8378832e83d4effa804d
Yaksh Bariya <thunder-coding@termux.dev>
2023-06-08 18:08:31 +0530

    chore(main/nodejs): use ninja to build instead of make

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environme
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
 TERMUX_PKG_VERSION=20.2.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=22523df2316c35569714ff1f69b053c2e286ced460898417dee46945efcdf989
 # Note that we do not use a shared libuv to avoid an issue with the Android
@@ -15,34 +16,9 @@ TERMUX_PKG_REPLACES="nodejs-current, nodejs-dev"
 TERMUX_PKG_SUGGESTS="clang, make, pkg-config, python"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_HOSTBUILD=true
 
-termux_step_post_get_source() {
-	# Prevent caching of host build:
-	rm -Rf $TERMUX_PKG_HOSTBUILD_DIR
-}
-
-termux_step_host_build() {
-	local ICU_VERSION=73.1
-	local ICU_TAR=icu4c-${ICU_VERSION//./_}-src.tgz
-	local ICU_DOWNLOAD=https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION//./-}/$ICU_TAR
-	termux_download \
-		$ICU_DOWNLOAD\
-		$TERMUX_PKG_CACHEDIR/$ICU_TAR \
-		a457431de164b4aa7eca00ed134d00dfbf88a77c6986a10ae7774fc076bb8c45
-	tar xf $TERMUX_PKG_CACHEDIR/$ICU_TAR
-	cd icu/source
-	if [ "$TERMUX_ARCH_BITS" = 32 ]; then
-		./configure --prefix $TERMUX_PKG_HOSTBUILD_DIR/icu-installed \
-			--disable-samples \
-			--disable-tests \
-			--build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-	else
-		./configure --prefix $TERMUX_PKG_HOSTBUILD_DIR/icu-installed \
-			--disable-samples \
-			--disable-tests
-	fi
-	make -j $TERMUX_MAKE_PROCESSES install
+termux_step_pre_configure() {
+	termux_setup_ninja
 }
 
 termux_step_configure() {
@@ -64,8 +40,8 @@ termux_step_configure() {
 	export CXX_host=g++
 	export LINK_host=g++
 
-	LDFLAGS+=" -ldl"
 	# See note above TERMUX_PKG_DEPENDS why we do not use a shared libuv.
+	# When building with ninja, build.ninja is geenrated for both Debug and Release builds.
 	./configure \
 		--prefix=$TERMUX_PREFIX \
 		--dest-cpu=$DEST_CPU \
@@ -74,19 +50,29 @@ termux_step_configure() {
 		--shared-openssl \
 		--shared-zlib \
 		--with-intl=system-icu \
-		--cross-compiling
+		--cross-compiling \
+		--ninja
 
-	export LD_LIBRARY_PATH=$TERMUX_PKG_HOSTBUILD_DIR/icu-installed/lib
-	perl -p -i -e "s@LIBS := \\$\\(LIBS\\)@LIBS := -L$TERMUX_PKG_HOSTBUILD_DIR/icu-installed/lib -lpthread -licui18n -licuuc -licudata -ldl -lz@" \
-		$TERMUX_PKG_SRCDIR/out/tools/v8_gypfiles/mksnapshot.host.mk \
-		$TERMUX_PKG_SRCDIR/out/tools/v8_gypfiles/torque.host.mk \
-		$TERMUX_PKG_SRCDIR/out/tools/v8_gypfiles/bytecode_builtins_list_generator.host.mk \
-		$TERMUX_PKG_SRCDIR/out/tools/v8_gypfiles/v8_libbase.host.mk \
-		$TERMUX_PKG_SRCDIR/out/tools/v8_gypfiles/gen-regexp-special-case.host.mk
+	sed -i -e "s|\-I$TERMUX_PREFIX/include||g" \
+		$TERMUX_PKG_SRCDIR/out/Release/obj.host/tools/v8_gypfiles/*.ninja
+	sed -i -e "s|\-L$TERMUX_PREFIX/lib||g" \
+		$TERMUX_PKG_SRCDIR/out/Release/obj.host/tools/v8_gypfiles/*.ninja
+}
+
+termux_step_make() {
+	if [ "${TERMUX_DEBUG_BUILD}" = "true" ]; then
+		ninja -C out/Debug -j "${TERMUX_MAKE_PROCESSES}"
+	else
+		ninja -C out/Release -j "${TERMUX_MAKE_PROCESSES}"
+	fi
 }
 
 termux_step_make_install() {
-	python ./tools/install.py install '' $TERMUX_PREFIX
+	if [ "${TERMUX_DEBUG_BUILD}" = "true" ]; then
+		python tools/install.py install "" "${TERMUX_PREFIX}" out/Debug/
+	else
+		python tools/install.py install "" "${TERMUX_PREFIX}" out/Release/
+	fi
 }
 
 termux_step_create_debscripts() {
