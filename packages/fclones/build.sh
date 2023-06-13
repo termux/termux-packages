e3bbf328437dae9caf7e7a7aa09b1024544a8f8a
Lucy Phipps <landfillbaby69@gmail.com>
2023-06-13 02:38:03 +0100

    fclones: update to 0.32.1

--- packages/fclones/build.sh
+++ packages/fclones/build.sh
@@ -2,8 +2,12 @@ TERMUX_PKG_HOMEPAGE="https://github.com/pkolaczk/fclones"
 TERMUX_PKG_DESCRIPTION="Efficient Duplicate File Finder"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.31.1"
+TERMUX_PKG_VERSION="0.32.1"
 TERMUX_PKG_SRCURL="https://github.com/pkolaczk/fclones/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=9c006cfd23f09315fb2cfedf6c4204986d39560b30f02f782b531f13b3df82e5
+TERMUX_PKG_SHA256=313d4dad30ed1db4d74abd78f30a7a9917c361918d2bc6d84c9d97a2a8c7c5cb
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	TERMUX_PKG_SRCDIR+=/fclones
+}
