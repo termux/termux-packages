54a01a8298b29ac605ab9e1fddff4251fc82e9a7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 05:24:07 +0900

    python-apt: Anti-build-depend on build-essential

--- packages/python-apt/build.sh
+++ packages/python-apt/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=2.6.0
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/p/python-apt/python-apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=557a705723f8acbb62c8af2989d0258dccb0a71f35e34aca53a9b492dbfbcfdd
 TERMUX_PKG_DEPENDS="apt, build-essential, libc++, python, texinfo"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="build-essential"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
 
