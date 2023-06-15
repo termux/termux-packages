f678e26b615f3e3cf6df51bbda759872a4cc4c60
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 03:52:42 +0900

    asymptote: Bump to 2.86

--- packages/asymptote/build.sh
+++ packages/asymptote/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://asymptote.sourceforge.io/
 TERMUX_PKG_DESCRIPTION="A powerful descriptive vector graphics language for technical drawing"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(2.85)
+TERMUX_PKG_VERSION=(2.86)
 TERMUX_PKG_VERSION+=(0.9.9.8)
 TERMUX_PKG_SRCURL=(https://downloads.sourceforge.net/asymptote/asymptote-${TERMUX_PKG_VERSION}.src.tgz
                    https://github.com/g-truc/glm/archive/${TERMUX_PKG_VERSION[1]}.tar.gz)
-TERMUX_PKG_SHA256=(4c0559b62c41f947b5fbf044b4d091bd3cf0abe599c85138087069809875ec87
+TERMUX_PKG_SHA256=(c4ebad1fc3c7b3ce52d89f5fd7e731830d2e6147de7e4c04f8f5cd36cff3c91f
                    7d508ab72cb5d43227a3711420f06ff99b0a0cb63ee2f93631b162bfe1fe9592)
 TERMUX_PKG_DEPENDS="fftw, libc++, libtirpc, zlib"
 TERMUX_PKG_BUILD_DEPENDS="ncurses-static, readline-static"
