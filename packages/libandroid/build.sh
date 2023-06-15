14abf5192bd626a40c95a1542d74b57fc6b021d9
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-15 11:32:29 +0900

    libandroid: Add better description

--- packages/libandroid/build.sh
+++ packages/libandroid/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://android.googlesource.com/platform/frameworks/base/+/master/native/android
-TERMUX_PKG_DESCRIPTION="libandroid"
+TERMUX_PKG_DESCRIPTION="Stub libandroid.so needed on Termux Docker"
 TERMUX_PKG_LICENSE="NCSA"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=25c
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://dl.google.com/android/repository/android-ndk-r${TERMUX_PKG_VERSION}-linux.zip
 TERMUX_PKG_SHA256=769ee342ea75f80619d985c2da990c48b3d8eaf45f48783a2d48870d04b46108
 TERMUX_PKG_BUILD_IN_SRC=true
