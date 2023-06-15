06865e7b51ce93864012a376b76b93892d619cb6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 04:02:22 +0900

    scrot: Bump to 1.10

--- x11-packages/scrot/build.sh
+++ x11-packages/scrot/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_DESCRIPTION="Simple command-line screenshot utility for X"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.9
+TERMUX_PKG_VERSION=1.10
 TERMUX_PKG_SRCURL=https://github.com/resurrecting-open-source-projects/scrot/releases/download/${TERMUX_PKG_VERSION}/scrot-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0a29373b57139d72a14ea94c9566d17305ffcc1e5bc2e74c81bcf60024a0fcc3
+TERMUX_PKG_SHA256=d3fb2fb962c1921030a442448afb1994d77d40192868da1b4cdeade0066bf36f
 TERMUX_PKG_DEPENDS="imlib2, libx11, libxcomposite, libxext, libxfixes, libxinerama"
 
 termux_step_pre_configure() {
