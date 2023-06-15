44c8a54987c9ee307c16855f968dee85aacffcdb
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 05:59:50 +0900

    libre2: Bump to 2023.06.02

--- packages/libre2/build.sh
+++ packages/libre2/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://github.com/google/re2
 TERMUX_PKG_DESCRIPTION="A regular expression library"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2023.03.01
+TERMUX_PKG_VERSION=2023.06.02
 TERMUX_PKG_SRCURL=https://github.com/google/re2/archive/${TERMUX_PKG_VERSION//./-}.tar.gz
-TERMUX_PKG_SHA256=7a9a4824958586980926a300b4717202485c4b4115ac031822e29aa4ef207e48
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_SHA256=4ccdd5aafaa1bcc24181e6dd3581c3eee0354734bb9f3cb4306273ffa434b94f
+TERMUX_PKG_DEPENDS="abseil-cpp, libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=10
+	local _SOVERSION=11
 
 	local v=$(sed -E -n 's/^SONAME=([0-9]+)$/\1/p' Makefile)
 	if [ "${_SOVERSION}" != "${v}" ]; then
