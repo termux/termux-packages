8d13c34c31aba6f415d16bd66ca1ec15a4e0c009
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 03:54:09 +0900

    fossil: Bump to 2.22

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tick
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT-BSD2.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.21
+TERMUX_PKG_VERSION=2.22
 TERMUX_PKG_SRCURL=https://www.fossil-scm.org/home/tarball/version-$TERMUX_PKG_VERSION/fossil-src-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=195faf0b20c101834a809979d0a9cdf04a0dcbae1fdabae792c9db900b73bda0
+TERMUX_PKG_SHA256=b90c40beb75308b0eba46b5add6e54a7a9e65ac90ce86ec8d78ae2a4bb8bae60
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
