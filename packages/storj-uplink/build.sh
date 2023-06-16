1ede3c3dfc5e6c6727fda90309433768631a4ea3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 21:18:10 +0900

    storj-uplink: Bump to 1.81.3

--- packages/storj-uplink/build.sh
+++ packages/storj-uplink/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.storj.io/integrations/uplink-cli
 TERMUX_PKG_DESCRIPTION="Storj DCS Uplink CLI"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.76.2
+TERMUX_PKG_VERSION=1.81.3
 TERMUX_PKG_SRCURL=https://github.com/storj/storj/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7987191edecff53da2560b00d3b11e6c63e479e1bbbd90ce37ad491ac4ed2ce2
+TERMUX_PKG_SHA256=f1c22ac526c7b324379bc517d386746aa4fafb880b574d5c0bf4795355b2f753
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
