39d5731c8a17bb2bc4060d9a7401e19371bec561
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-16 21:03:56 +0900

    libfuse3: Bump to 3.15.0

--- root-packages/libfuse3/build.sh
+++ root-packages/libfuse3/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/libfuse/libfuse
 TERMUX_PKG_DESCRIPTION="FUSE (Filesystem in Userspace) is an interface for userspace programs to export a filesystem to the Linux kernel"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=3.14.1
+TERMUX_PKG_VERSION=3.15.0
 TERMUX_PKG_SRCURL=https://github.com/libfuse/libfuse/archive/fuse-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=99688d68a753ee97eed51d536b216b8ae5d7fe0236c1a45d439357903bfd4d88
+TERMUX_PKG_SHA256=1e79f1dd60eaf79645c21411c4a7028c97b5e745ea677ac5bda747c90497a785
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddisable-mtab=true
