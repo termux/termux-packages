6f5ce400b84a12154cb1119504133f75840e48a3
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-06-17 14:31:03 +0000

    qt5-qtwebengine: bump to 5.15.14

--- x11-packages/qt5-qtwebengine/ffmpeg-headers/Chrome/linux-noasm/ia32/libavformat/demuxer_list.c
+++
@@ -1,9 +0,0 @@
-static const AVInputFormat * const demuxer_list[] = {
-    &ff_aac_demuxer,
-    &ff_flac_demuxer,
-    &ff_matroska_demuxer,
-    &ff_mov_demuxer,
-    &ff_mp3_demuxer,
-    &ff_ogg_demuxer,
-    &ff_wav_demuxer,
-    NULL };
