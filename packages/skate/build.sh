50cb21321e07cbb174306240efacc6f83aeb7cdb
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-17 12:43:30 +0900

    skate: Bump to 0.2.2

--- packages/skate/build.sh
+++ packages/skate/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/charmbracelet/skate
 TERMUX_PKG_DESCRIPTION="A personal key-value store"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.2.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=0.2.2
 TERMUX_PKG_SRCURL=https://github.com/charmbracelet/skate/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a8ae0daeaae71462b8b50b042c428074eb0a69cdcfcb37b508f4f0617901d697
+TERMUX_PKG_SHA256=e982348a89a54c0f9fafe855ec705c91b12eb3bb9aceb70b37abf7504106b04e
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
