79f41ae1039a3412a70177830a726cfdb6c83a2b
Jia Yuan Lo <jylo06g@gmail.com>
2023-06-18 06:48:18 +0800

    fix(main/fastfetch): correct deps

--- packages/fastfetch/build.sh
+++ packages/fastfetch/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="A neofetch-like tool for fetching system information and
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.12.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/LinusDierheimer/fastfetch/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e3d7384de0aa306effdcbe1b7e8095b40649773086b838d925fbfc2ec5027ab0
-TERMUX_PKG_DEPENDS="vulkan-headers, vulkan-loader-android, freetype"
+TERMUX_PKG_DEPENDS="vulkan-loader"
+TERMUX_PKG_BUILD_DEPENDS="freetype, vulkan-headers, vulkan-loader-android"
 TERMUX_PKG_AUTO_UPDATE=true
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DTARGET_DIR_ROOT=${TERMUX_PREFIX}
 -DTARGET_DIR_USR=${TERMUX_PREFIX}
