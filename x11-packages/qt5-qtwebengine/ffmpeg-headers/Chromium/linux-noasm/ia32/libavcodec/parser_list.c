bfce79f5c3374e52b50e87825cb2f15931365020
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-06-17 14:31:03 +0000

    qt5-qtwebengine: bump to 5.15.14

--- x11-packages/qt5-qtwebengine/ffmpeg-headers/Chromium/linux-noasm/ia32/libavcodec/parser_list.c
+++
@@ -1,9 +0,0 @@
-static const AVCodecParser * const parser_list[] = {
-    &ff_flac_parser,
-    &ff_mpegaudio_parser,
-    &ff_opus_parser,
-    &ff_vorbis_parser,
-    &ff_vp3_parser,
-    &ff_vp8_parser,
-    &ff_vp9_parser,
-    NULL };
--- x11-packages/qt5-qtwebengine/ffmpeg-headers/Chromium/linux-noasm/ia32/libavformat/demuxer_list.c
+++
@@ -1,8 +0,0 @@
-static const AVInputFormat * const demuxer_list[] = {
-    &ff_flac_demuxer,
-    &ff_matroska_demuxer,
-    &ff_mov_demuxer,
-    &ff_mp3_demuxer,
-    &ff_ogg_demuxer,
-    &ff_wav_demuxer,
-    NULL };
