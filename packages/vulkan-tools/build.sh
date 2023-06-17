f286b803f6afe8363b813817c1181d166f32d50e
Jia Yuan Lo <jylo06g@gmail.com>
2023-06-18 05:17:46 +0800

    bump(main/vulkan-tools): 1.3.254

--- packages/vulkan-tools/build.sh
+++ packages/vulkan-tools/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # This package and vulkan-headers should be updated at same time. Otherwise, they do not compile successfully.
-TERMUX_PKG_VERSION="1.3.251"
+# Do not TERMUX_PKG_DEPENDS on vulkan-loader
+TERMUX_PKG_VERSION="1.3.254"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Tools/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=03851334d9751464f96b2d3c49cac884b7be5178c04d162a445bb2ca93f56519
+TERMUX_PKG_SHA256=9d46697d0df39e20950db2f8581b63b3d02ab5dce6b67777b9f7bdf876ae4921
 TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libwayland"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-loader-generic (=${TERMUX_PKG_VERSION})"
 TERMUX_PKG_RECOMMENDS="vulkan-loader"
