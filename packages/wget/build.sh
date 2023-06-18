87bcd5c83cef449dfbd2270fc8a002fdade6c33b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-18 15:03:34 +0900

    wget: Enable IRI

--- packages/wget/build.sh
+++ packages/wget/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Commandline tool for retrieving files using HTTP, HTTPS 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.21.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/wget/wget-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_SHA256=3683619a5f50edcbccb1720a79006fa37bf9b9a255a8c5b48048bc3c7a874bd9
-TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libuuid, openssl, pcre2, zlib"
+TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libidn2, libuuid, openssl, pcre2, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_getpass=yes
 ac_cv_libunistring=no
---disable-iri
+--enable-iri
 --with-ssl=openssl
 --with-included-libunistring=no
 --without-libunistring-prefix
