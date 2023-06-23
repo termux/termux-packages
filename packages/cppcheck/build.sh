d6a368ea256a37e3f091685daa5b701ff31f051a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-23 09:00:44 +0900

    cppcheck: Bump to 2.11

--- packages/cppcheck/build.sh
+++ packages/cppcheck/build.sh
@@ -2,9 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://github.com/danmar/cppcheck
 TERMUX_PKG_DESCRIPTION="tool for static C/C++ code analysis"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.10.3
+TERMUX_PKG_VERSION=2.11
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology # Upstream only releases major versions theough GitHub. Other minor updates are released using git tags, better rely on repology for updated versiom
 TERMUX_PKG_SRCURL=https://github.com/danmar/cppcheck/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=8aae5e116daeaaf5d19f3efa61b91c06f161cb97412a1d1af6e1e20686e48967
+TERMUX_PKG_SHA256=ef7d77c16e6903834cc016986a60157918a90958e981863746a7f3147bfb94c6
 TERMUX_PKG_DEPENDS="libc++"
+
+# Prevent running dmake during builds. dmake just generates Makefile which we
+# aren't using, and QT translation files, but as we are not building the GUI,
+# there is no need.  And anyways will lead to "Exec format" error as running
+# target binaries on host
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DDISABLE_DMAKE=ON"
