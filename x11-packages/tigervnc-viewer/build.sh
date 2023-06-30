350b84cdcb4d46059bb30402d66e536565fc385b
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-30 17:09:54 +0900

    tigervnc-viewer: Revbump to rebuild
    due to SONAME change in libjpeg-turbo.

--- x11-packages/tigervnc-viewer/build.sh
+++ x11-packages/tigervnc-viewer/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A viewer (client) for Virtual Network Computing"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.13.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/TigerVNC/tigervnc/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b7c5b8ed9e4e2c2f48c7b2c9f21927db345e542243b4be88e066b2daa3d1ae25
 TERMUX_PKG_DEPENDS="fltk, libandroid-shmem, libc++, libgmp, libgnutls, libjpeg-turbo, libnettle, libpixman, libx11, libxext, libxi, libxrandr, libxrender, zlib"
