61276d842c3259d2811c6fcae500a5927a63929e
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-30 10:11:47 +0900

    p11-kit: Bump to 0.25.0

--- packages/p11-kit/build.sh
+++ packages/p11-kit/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE="https://p11-glue.github.io/p11-glue/p11-kit.html"
 TERMUX_PKG_DESCRIPTION="Provides a way to load and enumerate PKCS#11 modules"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@leapofazzam123"
-TERMUX_PKG_VERSION=0.24.1
+TERMUX_PKG_VERSION=0.25.0
 TERMUX_PKG_SRCURL="https://github.com/p11-glue/p11-kit/releases/download/$TERMUX_PKG_VERSION/p11-kit-$TERMUX_PKG_VERSION.tar.xz"
-TERMUX_PKG_SHA256=d8be783efd5cd4ae534cee4132338e3f40f182c3205d23b200094ec85faaaef8
-TERMUX_PKG_DEPENDS="gettext, libffi, libtasn1"
+TERMUX_PKG_SHA256=d55583bcdde83d86579cabe3a8f7f2638675fef01d23cace733ff748fc354706
+TERMUX_PKG_DEPENDS="libffi, libtasn1"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-trust-paths --disable-static"
 TERMUX_PKG_AUTO_UPDATE=true
 
