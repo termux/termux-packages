5468936e1a0f029846d7708d6199aad7a4f10f07
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-30 18:19:51 +0900

    feh: Remove libjpeg-turbo from dependency
    and recommend libjpeg-turbo-progs instead.

--- x11-packages/feh/build.sh
+++ x11-packages/feh/build.sh
@@ -5,10 +5,12 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.10
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://feh.finalrewind.org/feh-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1d71d1f6dcfe9ffee5e2766969a11978f7eb4fac7d7ae556f104c11f9c57cc98
-TERMUX_PKG_DEPENDS="imlib2, libcurl, libexif, libjpeg-turbo, libpng, libx11, libxinerama"
+TERMUX_PKG_DEPENDS="imlib2, libcurl, libexif, libpng, libx11, libxinerama"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
+TERMUX_PKG_RECOMMENDS="libjpeg-turbo-progs"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="exif=1 help=1 verscmp=0"
 
