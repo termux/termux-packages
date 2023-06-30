65131f0467a11c5c3bbffc48b83959282333e1c1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-30 15:01:13 +0900

    webkit2gtk-4.1, webkitgtk-6.0: Revbump to rebuild
    due to SONAME change in libjpeg-turbo.

--- x11-packages/webkitgtk-6.0/build.sh
+++ x11-packages/webkitgtk-6.0/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A full-featured port of the WebKit rendering engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.40.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=96898870d994da406ee7a632816dcde9a3bb395ee5f344fcb3f3b8cc8a77e000
 TERMUX_PKG_DEPENDS="enchant, fontconfig, freetype, glib, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gstreamer, gtk4, harfbuzz, harfbuzz-icu, libc++, libcairo, libgcrypt, libhyphen, libicu, libjpeg-turbo, libpng, libsoup3, libtasn1, libwebp, libxml2, libx11, libxcomposite, libxdamage, libxslt, libxt, littlecms, openjpeg, pango, woff2, zlib"
