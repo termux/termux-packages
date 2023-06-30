03ba36475ac5f79dd1846bc3418ee90e3445cf72
Tee KOBAYASHI <xtkoba@gmail.com>
2023-06-30 16:10:12 +0900

    gpac: Revbump to rebuild
    due to SONAME change in libjpeg-turbo.

--- packages/gpac/build.sh
+++ packages/gpac/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="An open-source multimedia framework focused on modularit
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/gpac/gpac/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8173ecc4143631d7f2c59f77e1144b429ccadb8de0d53a4e254389fb5948d8b8
 TERMUX_PKG_DEPENDS="ffmpeg, freetype, liba52, libjpeg-turbo, liblzma, libmad, libnghttp2, libogg, libpng, libtheora, libvorbis, openjpeg, openssl, pulseaudio, xvidcore, zlib"
