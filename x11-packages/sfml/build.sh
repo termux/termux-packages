e61682c849d6bce3b0ec29089b3ddd597c0f6eb9
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-01 10:45:09 +0900

    sfml: Bump to 2.6.0

--- x11-packages/sfml/build.sh
+++ x11-packages/sfml/build.sh
@@ -2,12 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.sfml-dev.org/
 TERMUX_PKG_DESCRIPTION="A simple, fast, cross-platform and object-oriented multimedia API"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.5.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=2.6.0
 TERMUX_PKG_SRCURL=https://github.com/SFML/SFML/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=438c91a917cc8aa19e82c6f59f8714da353c488584a007d401efac8368e1c785
-TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxrandr, openal-soft, opengl"
+TERMUX_PKG_SHA256=0c3f84898ea1db07dc46fa92e85038d8c449e3c8653fe09997383173de96bc06
+TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxcursor, libxrandr, openal-soft, opengl"
 
 termux_step_post_get_source() {
 	cp src/SFML/Window/Android/JoystickImpl.{cpp,hpp} src/SFML/Window/Unix/
+
+	find tools/pkg-config -name '*.pc.in' | xargs -n 1 \
+		sed -i -E 's|^(libdir=)\$\{exec_prefix\}/(@CMAKE_INSTALL_LIBDIR@)$|\1\2|'
 }
