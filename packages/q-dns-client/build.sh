2b593ff202a827f683fa38a46f8e858ad4b2f534
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-01 17:59:01 +0900

    q-dns-client: Do not specify Git commit ID
    %ci:no-build

--- packages/q-dns-client/build.sh
+++ packages/q-dns-client/build.sh
@@ -3,16 +3,11 @@ TERMUX_PKG_DESCRIPTION="A tiny command line DNS client with support for UDP, TCP
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="kay9925@outlook.com"
 TERMUX_PKG_VERSION="0.11.1"
-_COMMIT=15b493d0fee4d3432d70ac5ce42f9e19923ff0a8
-TERMUX_PKG_GIT_BRANCH=main
 TERMUX_PKG_SRCURL="git+https://github.com/natesales/q"
 TERMUX_PKG_SHA256=d947965772a4c30c371feb63ee0f0f3c5c62de3f8ac25b4f09e34b0b320dea2d
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
-	git fetch --unshallow
-	git reset --hard $_COMMIT
-
 	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
 	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]] ; then
 		termux_error_exit "Checksum mismatch for source files."
@@ -27,7 +22,7 @@ termux_step_make() {
 	local ldflags="\
 	-w -s \
 	-X 'main.version=${TERMUX_PKG_VERSION}' \
-	-X 'main.commit=${_COMMIT}' \
+	-X 'main.commit=$(git log -1 --format=%H)' \
 	-X 'main.date=${_builtAt}' \
 	"
 
