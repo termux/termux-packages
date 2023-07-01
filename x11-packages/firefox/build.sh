c8e1e1cc83ad0ee89027535e12978e3e2d0f9cf6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-01 16:43:01 +0900

    firefox: Bump to 115.0

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.mozilla.org/firefox
 TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=114.0
+TERMUX_PKG_VERSION=115.0
 TERMUX_PKG_SRCURL=https://ftp.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION}/source/firefox-${TERMUX_PKG_VERSION}.source.tar.xz
-TERMUX_PKG_SHA256=d23a0502742f52110ce496837ba82b47bf38d40585633787508ae5be9a5b4bc6
+TERMUX_PKG_SHA256=7cdb23fe5ccb54334c8fe14c1e5dd89d5788f8e7b79a2bd9cc4eabdc7e54e898
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
-TERMUX_PKG_DEPENDS="at-spi2-atk, ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-sysv-semaphore, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxi, libxrandr, libxrender, libxtst, pango, pulseaudio"
-TERMUX_PKG_BUILD_DEPENDS="libcpufeatures"
+TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
+TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
 TERMUX_MAKE_PROCESSES=1
 
 termux_step_post_get_source() {
@@ -27,7 +27,7 @@ termux_step_pre_configure() {
 	export HOST_CXX=$(command -v clang++)
 
 	CXXFLAGS+=" -U__ANDROID__"
-	LDFLAGS+=" -landroid-shmem -landroid-sysv-semaphore -llog"
+	LDFLAGS+=" -landroid-shmem -llog"
 }
 
 termux_step_configure() {
