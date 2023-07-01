35602a8f7054c20aa8b6b2d488cb34d023329779
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-01 10:00:22 +0900

    maven: Bump to 3.9.3

--- packages/maven/build.sh
+++ packages/maven/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://maven.apache.org/
 TERMUX_PKG_DESCRIPTION="A Java software project management and comprehension tool"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@masterjavaofficial"
-TERMUX_PKG_VERSION=3.9.2
+TERMUX_PKG_VERSION=3.9.3
 TERMUX_PKG_SRCURL=https://dlcdn.apache.org/maven/maven-3/${TERMUX_PKG_VERSION}/binaries/apache-maven-${TERMUX_PKG_VERSION}-bin.tar.gz
-TERMUX_PKG_SHA256=809ef3220c6d179195c06c324cb9a6d34d8ecba566c5cfd8eb83167bc034117d
+TERMUX_PKG_SHA256=e1e13ac0c42f3b64d900c57ffc652ecef682b8255d7d354efbbb4f62519da4f1
 TERMUX_PKG_DEPENDS="libjansi (>= 2.4.0-1), openjdk-17"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
