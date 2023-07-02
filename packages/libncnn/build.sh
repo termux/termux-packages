6bb9ee22cd176c316fe461d1cfcc4eb3545be49f
Jia Yuan Lo <jylo06g@gmail.com>
2023-07-02 11:40:32 +0800

    fix(main/libncnn): correct deps

--- packages/libncnn/build.sh
+++ packages/libncnn/build.sh
@@ -6,6 +6,9 @@ _COMMIT=4b97730b0d033b4dc2a790e5c35745e0dbf51569
 TERMUX_PKG_VERSION="20230627"
 TERMUX_PKG_SRCURL=git+https://github.com/Tencent/ncnn
 TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="abseil-cpp, glslang, libc++, vulkan-loader"
+TERMUX_PKG_BUILD_DEPENDS="protobuf-static, python, vulkan-headers, vulkan-loader-android"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, pybind11"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -23,10 +26,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DNCNN_PYTHON=ON
 -DCMAKE_EXE_LINKER_FLAGS=-ldl
 "
-
-TERMUX_PKG_BUILD_DEPENDS="glslang"
-TERMUX_PKG_DEPENDS="libc++, libprotobuf, vulkan-headers, vulkan-tools"
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, pybind11"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
