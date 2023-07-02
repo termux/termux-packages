2998b88ed13ff693fd17433d584262f8cccbf0b4
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 12:31:52 +0900

    gvfs: Revbump to rebuild
    due to SONAME change in libxml2.

--- x11-packages/gvfs/build.sh
+++ x11-packages/gvfs/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=1.50
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gvfs/${_MAJOR_VERSION}/gvfs-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b86f09b7331c8642ecebf46a3cda0692f5eb26086f132326a5483c2ebf86a4cb
 TERMUX_PKG_DEPENDS="dbus, gcr, glib, gsettings-desktop-schemas, libarchive, libsoup3, libxml2 (>= 2.11.4-2)"
