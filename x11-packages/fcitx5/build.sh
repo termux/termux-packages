fc4e98a966d6932a3c4f5560e2cb4e07ed76aec7
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 20:05:05 +0900

    fcitx5: Revbump to rebuild
    due to SONAME change in libexpat.

--- x11-packages/fcitx5/build.sh
+++ x11-packages/fcitx5/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A generic input method framework"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.0.23
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fcitx/fcitx5/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=50bc76a883942ec7bcfa93d1af3bc5961194f9a61d600253cf61588ae47bf02d
 TERMUX_PKG_DEPENDS="dbus, enchant, fcitx5-data, gdk-pixbuf, iso-codes, json-c, libandroid-execinfo, libc++, libcairo, libevent, libexpat, libuuid, libxcb, libxkbcommon, libxkbfile, pango, xcb-imdkit, xcb-util, xcb-util-keysyms, xcb-util-wm, zlib"
