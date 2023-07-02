7b79c5facafbd219aeec1c9f3d0088ace84b5bb1
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 20:04:45 +0900

    elinks: Revbump to rebuild
    due to SONAME change in libexpat.

--- packages/elinks/build.sh
+++ packages/elinks/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Full-Featured Text WWW Browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.16.1.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rkd77/elinks/releases/download/v${TERMUX_PKG_VERSION}/elinks-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=303c6f830b98658dcb813b68432ecde27d3857ccf1e765109fb6b0edb89f5095
 TERMUX_PKG_DEPENDS="libexpat, libiconv, libidn, openssl, libbz2, zlib"
