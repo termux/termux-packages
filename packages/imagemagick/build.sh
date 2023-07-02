c5b8c025d5417d6d5c45d4a9e1273133c0d52c4a
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 11:17:17 +0900

    imagemagick: Revbump to rebuild
    due to SONAME change in libxml2.

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
 _VERSION=7.1.1-12
 TERMUX_PKG_VERSION=${_VERSION//-/.}
+TERMUX_PKG_REVISION=1
 #TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=https://imagemagick.org/archive/releases/ImageMagick-${_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a761aa8c3b0690910600ba838d15379b676820f1ed912382d31c9b5da1ca1878
