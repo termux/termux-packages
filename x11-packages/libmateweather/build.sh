e7197ced1607856e9a825047706875c560ebabf3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 12:24:26 +0900

    libmateweather: Revbump to rebuild
    due to SONAME change in libxml2.

--- x11-packages/libmateweather/build.sh
+++ x11-packages/libmateweather/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="libmateweather is a libgnomeweather fork."
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.26.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/libmateweather/releases/download/v$TERMUX_PKG_VERSION/libmateweather-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=c200990f4b8e9d4d0e2c6f7d31a5876374c3d2a36c6b8cb590440043a860ef3a
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libsoup, libxml2 (>= 2.11.4-2), pango, zlib"
