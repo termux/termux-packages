8ea5318abb0e62076909381e48d71e0c3d290b8c
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 12:37:31 +0900

    bluefish: Revbump to rebuild
    due to SONAME change in libxml2.

--- x11-packages/bluefish/build.sh
+++ x11-packages/bluefish/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A powerful editor targeted towards programmers and webde
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2.13
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.bennewitz.com/bluefish/stable/source/bluefish-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=9b56966209d50951326a2ae21c7fd692bd91661d047ad3a01c97ba731aa477fb
 TERMUX_PKG_DEPENDS="atk, enchant, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxml2 (>= 2.11.4-2), pango, zlib"
