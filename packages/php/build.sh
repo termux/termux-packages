035ba0fc6d9492d90ec2a3146864ebf318d77ef3
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 11:19:21 +0900

    php: Revbump to rebuild
    due to SONAME change in libxml2.

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_LICENSE_FILE=LICENSE
 TERMUX_PKG_MAINTAINER="@termux"
 # Please revbump php-* extensions along with "minor" bump (e.g. 8.1.x to 8.2.0)
 TERMUX_PKG_VERSION=8.2.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/php/php-src/archive/php-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=18884adf2b7052f5ec7122ae4e38a8fdc0d2b95f805e040d4a5051568aa160d1
 # Build native php for phar to build (see pear-Makefile.frag.patch):
