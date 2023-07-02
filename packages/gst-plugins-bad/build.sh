643b7a6e21ca4a2290c2baccdc0a479398f633b6
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 10:14:27 +0900

    gst-plugins-bad: Revbump to rebuild
    due to SONAME change in libxml2.

--- packages/gst-plugins-bad/build.sh
+++ packages/gst-plugins-bad/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GStreamer Bad Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.22.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=eaaf53224565eaabd505ca39c6d5769719b45795cf532ce1ceb60e1b2ebe99ac
 TERMUX_PKG_DEPENDS="game-music-emu, glib, gst-plugins-base, gstreamer, libaom, libass, libbz2, libcairo, libcurl, libopus, librsvg, libsndfile, libsrt, libx11, libxml2 (>= 2.11.4-2), littlecms, openal-soft, openjpeg, openssl, pango"
