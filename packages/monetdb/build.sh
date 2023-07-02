7418d655da7dec9a3c12102ce46ff742cd5d837f
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 11:02:43 +0900

    monetdb: Revbump to rebuild
    due to SONAME change in libxml2.

--- packages/monetdb/build.sh
+++ packages/monetdb/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A high-performance database kernel for query-intensive a
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=11.47.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.monetdb.org/downloads/sources/archive/MonetDB-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e248df0e41803ed9c3b43e2d1179808c12769ed2ccc9314f89a308c111401a44
 TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libbz2, libcurl, libiconv, liblz4, liblzma, libxml2 (>= 2.11.4-2), netcdf-c, pcre, readline, zlib"
