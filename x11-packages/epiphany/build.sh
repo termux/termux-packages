f879ba99c42a59caf481c1c26999af13124c35de
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 14:02:16 +0900

    epiphany: Revbump to rebuild
    due to SONAME change in libxml2.

--- x11-packages/epiphany/build.sh
+++ x11-packages/epiphany/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=44
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/epiphany/${_MAJOR_VERSION}/epiphany-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3c475e04ed2a0f410cb44b96937563697079193ac9b7a50c91485fb9b08315ab
 TERMUX_PKG_DEPENDS="adwaita-icon-theme, gcr4, gdk-pixbuf, glib, graphene, gsettings-desktop-schemas, gstreamer, gtk4, iso-codes, json-glib, libadwaita, libarchive, libcairo, libgmp, libnettle, libportal-gtk4, libsecret, libsoup3, libsqlite, libxml2 (>= 2.11.4-2), pango, webkitgtk-6.0"
