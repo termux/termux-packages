5bb7a0fa1e52a8951ae3c625231137c0ec7efc02
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-02 14:49:59 +0900

    libxml2: Revbump and unbreak the package

--- packages/libxml2/build.sh
+++ packages/libxml2/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=2.11
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libxml2/${_MAJOR_VERSION}/libxml2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=737e1d7f8ab3f139729ca13a2494fd17bf30ddb4b7a427cf336252cab57f57f7
 TERMUX_PKG_SETUP_PYTHON=true
@@ -27,12 +27,4 @@ termux_step_post_massage() {
 	if ! readelf -d lib/libxml2.so | grep -q '(SONAME).*\[libxml2\.so\.'; then
 		termux_error_exit "SONAME for libxml2.so is not properly set."
 	fi
-
-	# Temporarily break the package so that it is not installed:
-	mkdir -p bin
-	cat > bin/sh <<-EOF
-		#!/bin/sh
-		exec /bin/sh "\$@"
-	EOF
-	chmod 0700 bin/sh
 }
