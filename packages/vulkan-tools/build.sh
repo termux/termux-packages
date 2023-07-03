88ad240e47e2fea93157410e8d959a47e1785570
Jia Yuan Lo <jylo06g@gmail.com>
2023-07-03 19:28:01 +0800

    fix(main/vulkan-tools): correct deps

--- packages/vulkan-tools/build.sh
+++ packages/vulkan-tools/build.sh
@@ -3,13 +3,12 @@ TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # This package and vulkan-headers should be updated at same time. Otherwise, they do not compile successfully.
-# Do not TERMUX_PKG_DEPENDS on vulkan-loader
 TERMUX_PKG_VERSION="1.3.255"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Tools/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ccd9175eec671b9ebff2486d1fb3e3d2f318028bbc873378ff9e1382b432abc6
-TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libwayland"
+TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libwayland, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-loader-generic (=${TERMUX_PKG_VERSION})"
-TERMUX_PKG_RECOMMENDS="vulkan-loader"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -22,5 +21,3 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DVULKAN_HEADERS_INSTALL_DIR=${TERMUX_PREFIX}
 -DWAYLAND_SCANNER_EXECUTABLE=${TERMUX_PREFIX}/opt/libwayland/cross/bin/wayland-scanner
 "
-
-# https://github.com/KhronosGroup/Vulkan-Tools/blob/main/CMakeLists.txt
