baffceb9a84e147e87c24056c6ef49840498da87
Tee KOBAYASHI <xtkoba@gmail.com>
2023-07-05 13:18:25 +0900

    lnav: Bump to 0.11.2

--- packages/lnav/build.sh
+++ packages/lnav/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://lnav.org/
 TERMUX_PKG_DESCRIPTION="An advanced log file viewer for the small-scale"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.11.1
+TERMUX_PKG_VERSION=0.11.2
 TERMUX_PKG_SRCURL=https://github.com/tstack/lnav/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7d73ceeeae0fdc61cffce0f8a7a3435aa91b401b67d233ffaa018faab00d7d3f
+TERMUX_PKG_SHA256=03b72fd02faccdbf98fcdeba62306794b677b8bcf49d6023117808f88ed627dc
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
