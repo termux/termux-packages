85d42251fe8eec6f3c77eebd45a850cc84a992f5
Ivan Max <mixython@gmail.com>
2023-07-07 16:20:14 +0000

    pacman: intentional update of pacman.conf (#17386)

--- packages/pacman/build.sh
+++ packages/pacman/build.sh
@@ -3,14 +3,15 @@ TERMUX_PKG_DESCRIPTION="A library-based package manager with dependency support"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Maxython <mixython@gmail.com>"
 TERMUX_PKG_VERSION=6.0.2
-TERMUX_PKG_REVISION=11
+TERMUX_PKG_REVISION=12
 TERMUX_PKG_SRCURL=https://sources.archlinux.org/other/pacman/pacman-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=7d8e3e8c5121aec0965df71f59bedf46052c6cf14f96365c4411ec3de0a4c1a5
 TERMUX_PKG_DEPENDS="bash, curl, gpgme, libandroid-glob, libarchive, libcurl, openssl, termux-licenses"
 TERMUX_PKG_BUILD_DEPENDS="doxygen, asciidoc, nettle"
 TERMUX_PKG_RECOMMENDS="termux-keyring"
 TERMUX_PKG_GROUPS="base-devel"
-TERMUX_PKG_CONFFILES="etc/pacman.conf, etc/makepkg.conf, var/log/pacman.log"
+#TERMUX_PKG_CONFFILES="etc/pacman.conf, etc/pacman.d/serverlist, etc/makepkg.conf, var/log/pacman.log"
+TERMUX_PKG_CONFFILES="etc/pacman.d/serverlist, etc/makepkg.conf, var/log/pacman.log"
 
 termux_step_pre_configure() {
 	rm -f ./scripts/libmakepkg/executable/sudo.sh.in
