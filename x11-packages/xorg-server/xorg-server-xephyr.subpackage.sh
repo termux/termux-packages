833ba976d5cade1755941b33d9eb848c51d13f79
zavocc <zavocc1030@outlook.com>
2023-07-10 18:19:01 +0800

    xorg-server-xephyr: Remove license file
    File doesn't exist

--- x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
+++ x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
@@ -2,6 +2,5 @@ TERMUX_SUBPKG_DESCRIPTION="nested X server"
 TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libdrm, libpixman, libx11, libxau, libxfont2, opengl, openssl, xorg-protocol-txt, xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm"
 TERMUX_SUBPKG_INCLUDE="
 bin/Xephyr
-share/doc/xorg-server-xephyr/LICENSE
 share/man/man1/Xephyr.1.gz
 "
