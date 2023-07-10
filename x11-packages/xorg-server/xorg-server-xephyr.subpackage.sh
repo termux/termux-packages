358decd05d71dd9b5ca5d507a02421ffeaae1b50
zavocc <zavocc1030@outlook.com>
2023-07-10 17:24:05 +0800

    xorg-server: Add Xephyr as subpackage

---
+++ x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_DESCRIPTION="nested X server"
+TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libdrm, libpixman, libx11, libxau, libxfont2, libxkbfile, libxshmfence, opengl, openssl, xkeyboard-config, xorg-protocol-txt, xorg-xkbcomp, xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util,wm"
+TERMUX_SUBPKG_INCLUDE="
+bin/Xephyr
+share/doc/xorg-server-xephyr/LICENSE
+share/man/man1/Xephyr.1.gz
+"
