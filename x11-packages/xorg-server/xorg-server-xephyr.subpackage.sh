9a363672303124f1b3f6d7585b127fa4e3b7b939
zavocc <zavocc1030@outlook.com>
2023-07-10 17:33:12 +0800

    xorg-server-xephyr: Remove libxkbfile as a dependency

--- x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
+++ x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="nested X server"
-TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libdrm, libpixman, libx11, libxau, libxfont2, libxkbfile, opengl, openssl, xkeyboard-config, xorg-protocol-txt, xorg-xkbcomp, xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm"
+TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libdrm, libpixman, libx11, libxau, libxfont2, opengl, openssl, xkeyboard-config, xorg-protocol-txt, xorg-xkbcomp, xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm"
 TERMUX_SUBPKG_INCLUDE="
 bin/Xephyr
 share/doc/xorg-server-xephyr/LICENSE
