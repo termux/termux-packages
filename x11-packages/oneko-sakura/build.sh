25c6de4e7fcb57394d2d1ada66e5b46d1f371647
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:19:33 +0200

    chore(x11/oneko-sakura): install to TERMUX_PKG_MASSAGEDIR

--- x11-packages/oneko-sakura/build.sh
+++ x11-packages/oneko-sakura/build.sh
@@ -11,5 +11,5 @@ TERMUX_PKG_GROUPS="games"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/oneko
+	install -Dm700 -t "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/oneko
 }
