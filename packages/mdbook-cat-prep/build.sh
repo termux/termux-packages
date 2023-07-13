cd6f39868db4ff35dae107dd3626a7d57749976e
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:06 +0200

    chore(main/mdbook-cat-prep): install to TERMUX_PKG_MASSAGEDIR

--- packages/mdbook-cat-prep/build.sh
+++ packages/mdbook-cat-prep/build.sh
@@ -42,5 +42,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-cat-prep
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-cat-prep
 }
