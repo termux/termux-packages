5cd6df2a2735643462a28e7d110a93ed7cd4ead6
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:08 +0200

    chore(x11/the-powder-toy): install to TERMUX_PKG_MASSAGEDIR

--- x11-packages/the-powder-toy/build.sh
+++ x11-packages/the-powder-toy/build.sh
@@ -14,6 +14,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin powder
-	ln -sf powder $TERMUX_PREFIX/bin/the-powder-toy
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin powder
+	ln -sf powder $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/the-powder-toy
 }
