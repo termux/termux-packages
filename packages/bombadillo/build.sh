3b2b476e3b58bbcb3445a68b184ffad310237d17
Henrik Grimler <grimler@termux.dev>
2023-07-13 11:26:42 +0200

    chore(main/bombadillo): install to TERMUX_PKG_MASSAGEDIR

--- packages/bombadillo/build.sh
+++ packages/bombadillo/build.sh
@@ -19,7 +19,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin bombadillo
-	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 \
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin bombadillo
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/man/man1 \
 		$TERMUX_PKG_SRCDIR/bombadillo.1
 }
