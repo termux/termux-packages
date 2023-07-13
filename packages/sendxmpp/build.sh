3cf4105c1798e3b6787188fc551712bd63cc39b4
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:07 +0200

    chore(main/sendxmpp): install to TERMUX_PKG_MASSAGEDIR

--- packages/sendxmpp/build.sh
+++ packages/sendxmpp/build.sh
@@ -10,7 +10,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin sendxmpp
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin sendxmpp
 }
 
 termux_step_create_debscripts()  {
