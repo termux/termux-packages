296f1277407aacc63decead9797c1d1bb3a1b726
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:07 +0200

    chore(main/mp3gain): install to TERMUX_PKG_MASSAGEDIR

--- packages/mp3gain/build.sh
+++ packages/mp3gain/build.sh
@@ -18,5 +18,5 @@ termux_extract_src_archive() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin mp3gain
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin mp3gain
 }
