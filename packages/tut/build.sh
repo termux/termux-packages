b5252d063a601a692bf166ed2c165fa57dd76feb
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:08 +0200

    chore(main/tut): install to TERMUX_PKG_MASSAGEDIR

--- packages/tut/build.sh
+++ packages/tut/build.sh
@@ -21,6 +21,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin tut
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME config.example.toml
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin tut
+	install -Dm600 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME config.example.toml
 }
