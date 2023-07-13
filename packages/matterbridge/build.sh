630920e034105ce93346d2dfc391ca57bfd888bc
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:06 +0200

    chore(main/matterbridge): install to TERMUX_PKG_MASSAGEDIR

--- packages/matterbridge/build.sh
+++ packages/matterbridge/build.sh
@@ -27,5 +27,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin matterbridge
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin matterbridge
 }
