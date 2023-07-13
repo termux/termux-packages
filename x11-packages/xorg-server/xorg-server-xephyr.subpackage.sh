c38090be55aa83d66fda968143e907ff3871c98f
marcusz <zavocc1030@outlook.com>
2023-07-13 19:27:11 +0800

    Update x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
    Co-authored-by: Jia Yuan Lo <jylo06g@gmail.com>

--- x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
+++ x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
@@ -1,6 +1,6 @@
 TERMUX_SUBPKG_DESCRIPTION="nested X server"
 TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
-TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libpixman, libxau, libxcb, libxdmcp, libxfont2, opengl, openssl, xorg-xkbcomp, xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm"
+TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libpixman, libxau, libxcb, libxdmcp, libxfont2, opengl, openssl, xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm, xkeyboard-config, xorg-xkbcomp"
 TERMUX_SUBPKG_INCLUDE="
 bin/Xephyr
 share/man/man1/Xephyr.1.gz
