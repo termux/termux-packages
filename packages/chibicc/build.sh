666db16f113ddd45f4a78f284c73f8845f5f8758
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:05 +0200

    chore(main/chibicc): install to TERMUX_PKG_MASSAGEDIR

--- packages/chibicc/build.sh
+++ packages/chibicc/build.sh
@@ -34,5 +34,5 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin chibicc
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin chibicc
 }
