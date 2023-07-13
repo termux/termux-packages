d4b7da013c18927e2d25302e1787c0e1f8a0377d
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:06 +0200

    chore(main/mdbook-epub): install to TERMUX_PKG_MASSAGEDIR

--- packages/mdbook-epub/build.sh
+++ packages/mdbook-epub/build.sh
@@ -27,5 +27,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-epub
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mdbook-epub
 }
