975967a922254b3eaec72947ba72a0f619350021
Henrik Grimler <grimler@termux.dev>
2023-07-13 11:29:54 +0200

    chore(main/bsd-finger): install to TERMUX_PKG_MASSAGEDIR

--- packages/bsd-finger/build.sh
+++ packages/bsd-finger/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=0.17
 TERMUX_PKG_SRCURL=http://ftp.linux.org.uk/pub/linux/Networking/netkit/bsd-finger-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=84885d668d117ef50e01c7034a45d8343d747cec6212e40e8d08151bc18e13fa
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="INSTALLROOT=$TERMUX_PKG_MASSAGEDIR"
 
 termux_step_post_get_source() {
 	sed -n '1,/*\//p' finger/finger.c > LICENSE
