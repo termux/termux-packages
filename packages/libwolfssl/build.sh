e01409b181200c6db09a16ad66062488e9cfa4e7
Lucy Phipps <landfillbaby69@gmail.com>
2023-07-13 17:59:46 +0100

    libwolfssl: fix auto-update

--- packages/libwolfssl/build.sh
+++ packages/libwolfssl/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=5.6.3
 TERMUX_PKG_SRCURL=https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${TERMUX_PKG_VERSION}-stable.tar.gz
 TERMUX_PKG_SHA256=2e74a397fa797c2902d7467d500de904907666afb4ff80f6464f6efd5afb114a
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
