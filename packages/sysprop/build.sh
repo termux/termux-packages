072491045d431a77fcbfa3f7c27c98fe337e0966
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:07 +0200

    chore(main/sysprop): install to TERMUX_PKG_MASSAGEDIR

--- packages/sysprop/build.sh
+++ packages/sysprop/build.sh
@@ -101,6 +101,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin \
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin \
 		$_TMP_BINDIR/sysprop_{cpp,java}
 }
