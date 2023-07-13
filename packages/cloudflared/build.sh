2ded20fb380d52da7ccf519031384128ba7c0b23
Henrik Grimler <grimler@termux.dev>
2023-07-13 11:57:26 +0200

    chore(main/cloudflared): install to TERMUX_PKG_MASSAGEDIR

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -18,5 +18,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin cloudflared
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin cloudflared
 }
