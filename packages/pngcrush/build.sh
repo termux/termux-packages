7c2945cc4918f5ff9dfaffeb4d3da88dd65abdbd
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:07 +0200

    chore(main/pngcrush): install to TERMUX_PKG_MASSAGEDIR

--- packages/pngcrush/build.sh
+++ packages/pngcrush/build.sh
@@ -15,5 +15,5 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin pngcrush
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin pngcrush
 }
