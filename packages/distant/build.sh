d49b4399125e5001fcf3d4ce69af16b47a8618eb
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:06 +0200

    chore(main/distant): install to TERMUX_PKG_MASSAGEDIR

--- packages/distant/build.sh
+++ packages/distant/build.sh
@@ -39,7 +39,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/distant
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/distant
 }
 
 termux_step_post_massage() {
