5b16ae4244e4408f5e8649a5086460be1607b321
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:07 +0200

    chore(main/pcal): install to TERMUX_PKG_MASSAGEDIR

--- packages/pcal/build.sh
+++ packages/pcal/build.sh
@@ -19,6 +19,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin ./exec/pcal
-	install -Dm700 -T ./doc/pcal.man $TERMUX_PREFIX/share/man/man1/pcal.1
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin ./exec/pcal
+	install -Dm700 -T ./doc/pcal.man $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/man/man1/pcal.1
 }
