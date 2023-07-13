d8b35eec2201768df519d88a24192c19bee70592
Henrik Grimler <grimler@termux.dev>
2023-07-13 12:13:05 +0200

    chore(main/cccc): install to TERMUX_PKG_MASSAGEDIR

--- packages/cccc/build.sh
+++ packages/cccc/build.sh
@@ -30,5 +30,5 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin cccc/cccc
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin cccc/cccc
 }
