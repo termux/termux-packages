ed164605584712d86c2a37e1a962a9794c8df3c9
Henrik Grimler <grimler@termux.dev>
2023-07-14 10:08:24 +0200

    chore(main/gitoxide): install to TERMUX_PKG_MASSAGEDIR

--- packages/gitoxide/build.sh
+++ packages/gitoxide/build.sh
@@ -51,7 +51,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/gix
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/ein
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/gix
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/ein
 }
 
