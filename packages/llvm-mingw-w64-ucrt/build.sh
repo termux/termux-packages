3c83d2f8e379568b95a8765aae4238b63f6d51f7
Henrik Grimler <grimler@termux.dev>
2023-07-14 21:28:17 +0200

    chore(main/llvm-mingw-w64-ucrt): install to TERMUX_PKG_MASSAGEDIR

--- packages/llvm-mingw-w64-ucrt/build.sh
+++ packages/llvm-mingw-w64-ucrt/build.sh
@@ -9,20 +9,20 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_NO_STATICSPLIT=true
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/opt/llvm-mingw-w64
-	mv $TERMUX_PKG_SRCDIR/{aarch64,armv7,i686,x86_64,generic}-w64-mingw32 $TERMUX_PREFIX/opt/llvm-mingw-w64
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/opt/llvm-mingw-w64
+	mv $TERMUX_PKG_SRCDIR/{aarch64,armv7,i686,x86_64,generic}-w64-mingw32 $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/opt/llvm-mingw-w64
 }
 
 termux_step_install_license() {
 	# Runtimes are consist of runtimes libraries from mingw-w64 and libunwind/libc++ from LLVM
-	mkdir -p $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME
 
 	# Install the license of mingw-w64 and mingw-w64-runtimes
 	local _file
-	for _file in $TERMUX_PREFIX/opt/llvm-mingw-w64/aarch64-w64-mingw32/share/mingw32/*; do
-		cp $_file $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/
+	for _file in $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/opt/llvm-mingw-w64/aarch64-w64-mingw32/share/mingw32/*; do
+		cp $_file $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/
 	done
 
 	# Install the license of LLVM
-	cp $TERMUX_PKG_SRCDIR/LICENSE.TXT $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE-LLVM.TXT
+	cp $TERMUX_PKG_SRCDIR/LICENSE.TXT $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/LICENSE-LLVM.TXT
 }
