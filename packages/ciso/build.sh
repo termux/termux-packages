e3493e445dde5cdcb804f35846365c50018c5a3e
Henrik Grimler <grimler@termux.dev>
2023-07-14 10:05:16 +0200

    chore(main/ciso): install to TERMUX_PKG_MASSAGEDIR

--- packages/ciso/build.sh
+++ packages/ciso/build.sh
@@ -10,5 +10,5 @@ TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
-	install -Dm700 ./"${TERMUX_PKG_NAME}" "${TERMUX_PREFIX}"/bin
+	install -Dm700 ./"${TERMUX_PKG_NAME}" "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/bin
 }
