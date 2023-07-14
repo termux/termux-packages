fbf4659ec84e1d1359d575848504a401354af968
Henrik Grimler <grimler@termux.dev>
2023-07-14 10:42:19 +0200

    chore(main/gdu): use both termux_step_{make,make_install}

--- packages/gdu/build.sh
+++ packages/gdu/build.sh
@@ -3,20 +3,20 @@ TERMUX_PKG_DESCRIPTION="Fast disk usage analyzer with console interface written 
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.25.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dundee/gdu/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=83fe876d953b4f2f7a856552e758aae4aa0cd9569dcf1aded61bdc834b834275
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	return
-}
-
-termux_step_make_install() {
 	termux_setup_golang
 
 	make build
-	install -D dist/gdu -t $TERMUX_PREFIX/bin
 	make gdu.1
+}
+
+termux_step_make_install() {
+	install -D dist/gdu -t $TERMUX_PREFIX/bin
 	install -Dm644 gdu.1 -t $TERMUX_PREFIX/share/man/man1
 }
