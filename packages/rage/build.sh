2eb5a1cfdd093ec80ec7c1f24495cddc713d062d
Henrik Grimler <grimler@termux.dev>
2023-07-14 10:08:24 +0200

    chore(main/rage): install to TERMUX_PKG_MASSAGEDIR

--- packages/rage/build.sh
+++ packages/rage/build.sh
@@ -16,6 +16,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rage
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rage-keygen
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rage
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rage-keygen
 }
