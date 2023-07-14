59257644eb18056ec70baa2c97c90fabc2d3f028
Henrik Grimler <grimler@termux.dev>
2023-07-14 10:08:23 +0200

    chore(main/dua): install to TERMUX_PKG_MASSAGEDIR

--- packages/dua/build.sh
+++ packages/dua/build.sh
@@ -17,6 +17,6 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/dua
+	install -Dm700 -t $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/dua
 }
 
