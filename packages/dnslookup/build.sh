60177b1b459e3b406aa0a5e5f342f12e384930b4
Henrik Grimler <grimler@termux.dev>
2023-07-14 10:05:16 +0200

    chore(main/dnslookup): install to TERMUX_PKG_MASSAGEDIR

--- packages/dnslookup/build.sh
+++ packages/dnslookup/build.sh
@@ -16,5 +16,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 ./${TERMUX_PKG_NAME} ${TERMUX_PREFIX}/bin
+	install -Dm700 ./${TERMUX_PKG_NAME} ${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/bin
 }
