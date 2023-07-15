004698f7af1a01131ba4a83573171f5efe096956
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-07-15 16:21:47 +0800

    rhash: bump to 1.4.4

--- packages/rhash/build.sh
+++ packages/rhash/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/rhash/RHash
 TERMUX_PKG_DESCRIPTION="Console utility for calculation and verification of magnet links and a wide range of hash sums"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.3
+TERMUX_PKG_VERSION=1.4.4
 TERMUX_PKG_SRCURL=https://github.com/rhash/RHash/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1e40fa66966306920f043866cbe8612f4b939b033ba5e2708c3f41be257c8a3e
+TERMUX_PKG_SHA256=8e7d1a8ccac0143c8fe9b68ebac67d485df119ea17a613f4038cda52f84ef52a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_CONFLICTS="librhash, rhash-dev"
@@ -31,13 +31,13 @@ termux_step_make_install() {
 	make install install-pkg-config
 	make -C librhash install-lib-headers
 
-	ln -sf $TERMUX_PREFIX/lib/librhash.so.0 $TERMUX_PREFIX/lib/librhash.so
+	ln -sf $TERMUX_PREFIX/lib/librhash.so.1 $TERMUX_PREFIX/lib/librhash.so
 }
 
 termux_step_post_massage() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION_GUARD_FILES="lib/librhash.so.0"
+	local _SOVERSION_GUARD_FILES="lib/librhash.so.1"
 	local f
 	for f in ${_SOVERSION_GUARD_FILES}; do
 		if [ ! -e "${f}" ]; then
