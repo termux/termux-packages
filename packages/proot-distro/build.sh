9da996c66fd1af24fbd4450028120835e8eb025c
Leonid Pliushch <sylirre@termux.dev>
2023-07-17 12:38:19 +0300

    bump(main/proot-distro): 3.17.0

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.16.0
+TERMUX_PKG_VERSION=3.17.0
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=00475536f7bd8257dac9b072652d4849f66c2b6efad1d778bcdb4b5578e93d15
+TERMUX_PKG_SHA256=81e42025f044419e521ce0f1bd5e9e0104c9b49c5589117e3e16e07d915c6c70
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -15,21 +15,4 @@ termux_step_make_install() {
 		TERMUX_PREFIX="$TERMUX_PREFIX" \
 		TERMUX_ANDROID_HOME="$TERMUX_ANDROID_HOME" \
 		./install.sh
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./preinst
-	#!${TERMUX_PREFIX}/bin/bash
-	set -e
-	shopt -s nullglob
-
-	for i in ${TERMUX_PREFIX}/etc/proot-distro/*.sh; do
-	  if ! grep -qP "^\s*TARBALL_URL" "\$i"; then
-	    echo "Disabling old style v1.x proot-distro plug-in: \$(basename "\$i")"
-	    mv -f "\${i}" "\${i}.bak"
-	  fi
-	done
-
-	exit 0
-	EOF
 }
