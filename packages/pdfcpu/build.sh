ed373b904620294c1160e8443ed7ec6dbebc1a0e
Henrik Grimler <grimler@termux.dev>
2023-07-17 14:31:08 +0200

    chore(main/pdfcpu): install to TERMUX_PKG_MASSAGEDIR

--- packages/pdfcpu/build.sh
+++ packages/pdfcpu/build.sh
@@ -16,5 +16,5 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 "${TERMUX_PKG_SRCDIR}/cmd/pdfcpu/pdfcpu" \
-		"$TERMUX_PREFIX/bin/"
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/"
 }
