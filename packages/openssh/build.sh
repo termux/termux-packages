b631553efbf58ce3650ddbf02e51821653dff177
Yaksh Bariya <thunder-coding@termux.dev>
2023-07-20 17:37:01 +0530

    upgpkg(main/openssh): 9.3p2
    This release contains a security fix. See https://www.openssh.com/txt/release-9.3p2 for more information

--- packages/openssh/build.sh
+++ packages/openssh/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.openssh.com/
 TERMUX_PKG_DESCRIPTION="Secure shell for logging into a remote machine"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=9.3p1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=9.3p2
 TERMUX_PKG_SRCURL=https://fastly.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e9baba7701a76a51f3d85a62c383a3c9dcd97fa900b859bc7db114c1868af8a8
+TERMUX_PKG_SHA256=200ebe147f6cb3f101fd0cdf9e02442af7ddca298dffd9f456878e7ccac676e8
 TERMUX_PKG_DEPENDS="krb5, ldns, libandroid-support, libedit, openssh-sftp-server, openssl, termux-auth, zlib"
 TERMUX_PKG_CONFLICTS="dropbear"
 # --disable-strip to prevent host "install" command to use "-s", which won't work for target binaries:
