724d36ec85e2fb96e10dbe52cea373a8a216631e
J9B10 <98818736+J9B10@users.noreply.github.com>
2023-07-24 00:30:03 -0300

    Update nodejs to 20.5.0

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=20.2.0
+TERMUX_PKG_VERSION=20.5.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=22523df2316c35569714ff1f69b053c2e286ced460898417dee46945efcdf989
 # Note that we do not use a shared libuv to avoid an issue with the Android
