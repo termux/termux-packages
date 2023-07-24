f24b1598289f3a75ab7bdc843b4a2ed13a9ecc9b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-07-24 21:26:12 +0800

    tere: bump to 1.5.0

--- packages/tere/build.sh
+++ packages/tere/build.sh
@@ -3,11 +3,27 @@ TERMUX_PKG_DESCRIPTION="Terminal file explorer written in rust"
 TERMUX_PKG_LICENSE="EUPL-1.2"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="1.5.0"
 TERMUX_PKG_SRCURL=https://github.com/mgunyho/tere/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=84eeafc346ee2207bcfb0a9e29a6a4e7748817741a0f7245a204d16da0ef651f
+TERMUX_PKG_SHA256=84195f45b738fb7c805d7b348185658d0dc58aa26e7f92fcad9578bc7bd694bf
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	export CFLAGS="${TARGET_CFLAGS}"
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	rm -rf $CARGO_HOME/registry/src/*/rustix-*
+	cargo fetch --target "${CARGO_TARGET_NAME}"
+
+	for d in $CARGO_HOME/registry/src/*/rustix-*; do
+		patch --silent -p1 -d ${d} < $TERMUX_PKG_BUILDER_DIR/0001-rustix-fix-libc-removing-unsafe-on-makedev.diff || :
+	done
+}
 
 termux_step_make() {
 	termux_setup_rust
