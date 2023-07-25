f276a3a4f15f40e6692debd716c2f269a90595d1
spvkgn <spvkgn@users.noreply.github.com>
2023-07-25 17:25:10 +0500

    Update x11-packages/far2l/far2l-plugins-netrocks.subpackage.sh
    Co-authored-by: Jia Yuan Lo <jylo06g@gmail.com>

--- x11-packages/far2l/far2l-plugins-netrocks.subpackage.sh
+++ x11-packages/far2l/far2l-plugins-netrocks.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="FAR2L NetRocks plugin (SFTP/SCP/FTP/SMB/NFS/WebDAV)"
-TERMUX_SUBPKG_DEPENDS="libneon, libnfs, libssh, openssl, samba"
+TERMUX_SUBPKG_DEPENDS="libc++, libneon, libnfs, libssh, openssl, samba"
 TERMUX_SUBPKG_INCLUDE="
 lib/far2l/Plugins/NetRocks
 share/far2l/Plugins/NetRocks
