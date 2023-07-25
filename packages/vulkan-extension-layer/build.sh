d0c7a36ab04bdc90736296c81f3ae6fcee512aae
Jia Yuan Lo <jylo06g@gmail.com>
2023-07-25 08:34:17 +0800

    bump(main/vulkan-extension-layer): 1.3.259

--- packages/vulkan-extension-layer/build.sh
+++ packages/vulkan-extension-layer/build.sh
@@ -1,17 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-ExtensionLayer
-TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
+TERMUX_PKG_DESCRIPTION="Vulkan Extension Layer"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # This package and vulkan-headers should be updated at same time. Otherwise, they do not compile successfully.
-TERMUX_PKG_VERSION="1.3.257"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.3.259"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-ExtensionLayer/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=27227d904072228b44fea3e2b766fffb03fb0acc0864ac3c917ce399b0516676
+TERMUX_PKG_SHA256=7f12ddf2c6d4a26c354844bbad3ab7037b647aab5540fe87c8c34663d0d3b1f8
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-loader-generic (=${TERMUX_PKG_VERSION})"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DBUILD_TESTS=OFF
--DVULKAN_HEADERS_INSTALL_DIR=$TERMUX_PREFIX
-"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_TESTS=OFF
+-DVULKAN_HEADERS_INSTALL_DIR=${TERMUX_PREFIX}
+"
