a28c40fafafe4bc341f190f08c077b9e33b50f10
Jia Yuan Lo <jylo06g@gmail.com>
2023-07-30 23:25:30 +0800

    vulkan-tools: cleanup
    * Avoid pull stub deb during building
    * Remove comment as maintainers understand vulkan deps version must match
      by now
    
    %ci:no-build

--- packages/vulkan-tools/build.sh
+++ packages/vulkan-tools/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-Tools
 TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-# This package and vulkan-headers should be updated at same time. Otherwise, they do not compile successfully.
 TERMUX_PKG_VERSION="1.3.261"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Tools/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2e9a1b380f4e494934c72e2b2fb77d2c3ae03ba19fd36ffad65529413108db2f
 TERMUX_PKG_DEPENDS="libc++, libx11, libxcb, libwayland, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-loader-generic (=${TERMUX_PKG_VERSION})"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
