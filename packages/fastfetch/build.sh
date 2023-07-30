1f017b3d0f7d11c0b7a4ebd948259179f2170b4e
Jia Yuan Lo <jylo06g@gmail.com>
2023-07-30 23:21:54 +0800

    chore(main/fastfetch): cleanup
    * Update upstream URL
    * Avoid pull stub deb during build
    
    %ci:no-build

--- packages/fastfetch/build.sh
+++ packages/fastfetch/build.sh
@@ -1,15 +1,17 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/LinusDierheimer/fastfetch
+TERMUX_PKG_HOMEPAGE=https://github.com/fastfetch-cli/fastfetch
 TERMUX_PKG_DESCRIPTION="A neofetch-like tool for fetching system information and displaying them in a pretty way"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.5"
-TERMUX_PKG_SRCURL=https://github.com/LinusDierheimer/fastfetch/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8046eb856fd95d9896220238c966125fb05e451e65d8cfc7eb25483767612235
 TERMUX_PKG_DEPENDS="vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="freetype, vulkan-headers, vulkan-loader-android, libandroid-wordexp-static"
+TERMUX_PKG_BUILD_DEPENDS="freetype, libandroid-wordexp-static, vulkan-headers, vulkan-loader-android"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DTARGET_DIR_HOME=${TERMUX_ANDROID_HOME}
 -DTARGET_DIR_ROOT=${TERMUX_PREFIX}
 -DTARGET_DIR_USR=${TERMUX_PREFIX}
--DTARGET_DIR_HOME=${TERMUX_ANDROID_HOME}
 "
