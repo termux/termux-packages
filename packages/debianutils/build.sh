74ef6850caf693abfc970297de948efeceab82db
Lucy Phipps <landfillbaby69@gmail.com>
2023-08-03 15:08:11 +0100

    bump(main/debianutils): 5.8

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/debianutils
 TERMUX_PKG_DESCRIPTION="Small utilities which are used primarily by the installation scripts of Debian packages"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.7
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=5.8
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.orig.tar.gz
-TERMUX_PKG_SHA256=27ec9e0e7e44dc8ab611aa576330471bacb07e4491ffecf0d3aa6909c92f9022
+TERMUX_PKG_SHA256=5b086d27eb9063de4d746760d0faeb40d9464fb855fc8a8e7fb93b03efcec622
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/add-shell
