f99f08eadc0fe8df5c15439403ddfd74387c87b0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-08-05 14:39:38 +0800

    kakoune: remove unnecessary patch
    Upstream has fixed this build issue

--- packages/kakoune/build.sh
+++ packages/kakoune/build.sh
@@ -3,20 +3,13 @@ TERMUX_PKG_DESCRIPTION="Code editor heavily inspired by Vim"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2023.08.05"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mawww/kakoune/releases/download/v$TERMUX_PKG_VERSION/kakoune-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=3e45151e0addd3500de2d6a29b5aacf2267c42bb256d44a782e73defb29cda5c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src debug=no "
-
-termux_step_pre_configure() {
-	# This patch should be no longer needed when NDK bumps to r26
-	local _file="$TERMUX_PKG_BUILDER_DIR/src-display_buffer.hh.patch"
-	if [ -f $_file && "$TERMUX_NDK_VERSION" != 25c ]; then
-		termux_error_exit "Please remove $_file"
-	fi
-}
 
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
