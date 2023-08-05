ddc9371f40c4c7790261dbd4ecd0025b97ef125e
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2023-08-05 21:36:57 +0000

    remove REVISION

--- x11-packages/xournalpp/build.sh
+++ x11-packages/xournalpp/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE, copyright.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xournalpp/xournalpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=39e6e9b9c9eee44170a81e05c152e31a83d6a42ac7f8eebf7eea6b752a446377
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libandroid-execinfo, libc++, libcairo, librsvg, libsndfile, libx11, libxi, libxml2, libzip, pango, poppler, portaudio, zlib"
