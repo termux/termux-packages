550a6dbee4021531878577eb390053b00affb6eb
Kevin WIlliams <admin@utermux.dev>
2023-08-12 11:39:14 +0800

    upgpkg(main/kcptun): 20230811
    Signed-off-by: Kevin WIlliams <admin@utermux.dev>

--- packages/kcptun/build.sh
+++ packages/kcptun/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xtaci/kcptun
 TERMUX_PKG_DESCRIPTION="A Stable & Secure Tunnel based on KCP with N:M multiplexing and FEC"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="20230214"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="20230811"
 TERMUX_PKG_SRCURL=https://github.com/xtaci/kcptun/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3ab7b2cc3cdf1705faa76d474419a2d9e8868c8b46a24c93a218bd6a5acb2de3
+TERMUX_PKG_SHA256=dd88c7ddb85cc74ff22940ba2dc22f65d3b6737153b225d611abb801a0694c4d
 # Depend on its subpackages.
 TERMUX_PKG_DEPENDS="kcptun-client, kcptun-server"
 TERMUX_PKG_BUILD_IN_SRC=true
