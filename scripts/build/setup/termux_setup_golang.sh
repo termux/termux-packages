6044d8532598a1fd870c22b13d6863d3c7cafde5
Kevin WIlliams <admin@utermux.dev>
2023-08-12 10:43:19 +0800

    golang: bump to 1.21.0
    Signed-off-by: Kevin WIlliams <admin@utermux.dev>

--- scripts/build/setup/termux_setup_golang.sh
+++ scripts/build/setup/termux_setup_golang.sh
@@ -1,11 +1,11 @@
 # Utility function for golang-using packages to setup a go toolchain.
 termux_setup_golang() {
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		local TERMUX_GO_VERSION=go1.20.7
-		local TERMUX_GO_SHA256=f0a87f1bcae91c4b69f8dc2bc6d7e6bfcd7524fceec130af525058c0c17b1b44
+		local TERMUX_GO_VERSION=go1.21.0
+		local TERMUX_GO_SHA256=d0398903a16ba2232b389fb31032ddf57cac34efda306a0eebac34f0965a0742
 		if [ "$TERMUX_PKG_GO_USE_OLDER" = "true" ]; then
-			TERMUX_GO_VERSION=go1.19.12
-			TERMUX_GO_SHA256=48e4fcfb6abfdaa01aaf1429e43bdd49cea5e4687bd5f5b96df1e193fcfd3e7e
+			TERMUX_GO_VERSION=go1.20.7
+			TERMUX_GO_SHA256=f0a87f1bcae91c4b69f8dc2bc6d7e6bfcd7524fceec130af525058c0c17b1b44
 		fi
 		local TERMUX_GO_PLATFORM=linux-amd64
 
