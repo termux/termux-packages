ad34c72db44e47320dcb2367b8cc2f77790f168c
Kevin WIlliams <admin@utermux.dev>
2023-08-12 12:39:27 +0800

    *: Rebuild with Go 1.21.0/1.20.7 (6)
    Signed-off-by: Kevin WIlliams <admin@utermux.dev>

--- packages/speedtest-go/build.sh
+++ packages/speedtest-go/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command line interface to test internet speed using spee
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.6.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/showwin/speedtest-go/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6df0bc4836e3a52da14726bbd863b5db2c8dc413d5533e2762b434d8e01466c5
 TERMUX_PKG_AUTO_UPDATE=true
--- packages/trzsz-go/build.sh
+++ packages/trzsz-go/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A simple file transfer tools, similar to lrzsz ( rz / sz
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/trzsz/trzsz-go/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ac52d4e468c983e03ddb76483789a95cbaf1426450fd65da79a9972882300f72
 TERMUX_PKG_BUILD_IN_SRC=true
