551df4d1e5328719acc79ecc0a3d794f26044549
Kevin WIlliams <admin@utermux.dev>
2023-08-12 19:03:18 +0800

    *: Rebuild with Go 1.21.0/1.20.7 (9)
    Signed-off-by: Kevin WIlliams <admin@utermux.dev>

--- packages/wakatime-cli/build.sh
+++ packages/wakatime-cli/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command line interface used by all WakaTime text editor 
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.73.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/wakatime/wakatime-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e12239c51c3bc075a9ff4455de5c85feec4c73c551466739c2f5d3d24d8983ad
 TERMUX_PKG_BUILD_IN_SRC=true
