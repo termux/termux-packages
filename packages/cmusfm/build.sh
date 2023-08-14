fba5ea62a8854f63d83cd1a0cf0f6bf5d875c4e0
valpogus <32718480+valpogus@users.noreply.github.com>
2023-08-14 20:54:15 +0200

    Remove unnecessary patch

--- packages/cmusfm/build.sh
+++ packages/cmusfm/build.sh
@@ -5,8 +5,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.0
 TERMUX_PKG_SRCURL=https://github.com/Arkq/cmusfm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=17aae8fc805e79b367053ad170854edceee5f4c51a9880200d193db9862d8363
-TERMUX_PKG_DEPENDS="libcurl, openssl"
+TERMUX_PKG_DEPENDS="libandroid-spawn, libcurl, openssl"
 
 termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-spawn"
 	autoreconf --force --install
 }
