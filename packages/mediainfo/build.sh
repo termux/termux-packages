df0aa902e028081fcf180b9268dd6d852b6545d7
valpogus <32718480+valpogus@users.noreply.github.com>
2023-08-14 18:34:09 +0200

    bump(main/mediainfo): 23.07

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=23.06
+TERMUX_PKG_VERSION=23.07
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4c977eeb0ce8378728e64ef24f27fe62b2c5116fbf0541c824a44309f80ff88a
+TERMUX_PKG_SHA256=b6d7da9e29995fd34a22100825b843e74c32c7bc67adb01166b1beedea49f5d0
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libmediainfo, libzen"
 
 termux_step_pre_configure() {
