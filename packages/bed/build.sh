f01bc3d0f1e22eb2ee598325e4b6fad3fbc3fbe5
iamwho001 <akshityadav47279@gmail.com>
2023-08-15 14:38:10 +0530

    Added new package Bed

---
+++ packages/bed/build.sh
@@ -0,0 +1,34 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/itchyny/bed"
+
+TERMUX_PKG_DESCRIPTION="Binary editor written in GO"
+
+TERMUX_PKG_LICENSE="MIT"
+
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+
+TERMUX_PKG_MAINTAINER="@termux"
+
+TERMUX_PKG_VERSION="0.2.3"
+
+TERMUX_PKG_SRCURL="https://github.com/itchyny/bed/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+
+TERMUX_PKG_SHA256=b4b5addb2622152073ae95202d61bae0e2c793b27220b7bf5c64f09517e94225
+
+TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PKG_AUTO_UPDATE=true
+
+
+termux_step_make() {
+
+        termux_setup_golang
+
+        go build -ldflags="-s -w" -o bed ./cmd/bed
+
+}
+
+termux_step_make_install() {
+
+        install -Dm700 -t $TERMUX_PREFIX/bin bed
+
+}
