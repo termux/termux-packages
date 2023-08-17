1cf04dda99e6a41fbec623b8b1fc4a2626cf866d
Jia Yuan Lo <jylo06g@gmail.com>
2023-08-18 00:57:32 +0800

    bump(main/opencl-vendor-driver): 0.3
    Fix minor bug for fail to install user override lib

--- packages/opencl-vendor-driver/build.sh
+++ packages/opencl-vendor-driver/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://termux.dev
 TERMUX_PKG_DESCRIPTION="OpenCL driver from system vendor"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_VERSION=0.3
 TERMUX_PKG_RECOMMENDS="binutils | binutils-is-llvm, ocl-icd, patchelf"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 
