a11d81c40cb703d4a0ce4ab038a9b58670cf51b0
Jia Yuan Lo <jylo06g@gmail.com>
2023-08-20 11:53:12 +0800

    bump(main/opencl-vendor-driver): 0.3 (#17670)
    Fix minor bug for fail to install user override lib

--- packages/opencl-vendor-driver/postinst.sh
+++ packages/opencl-vendor-driver/postinst.sh
@@ -12,29 +12,68 @@ check_cmd() {
 	[ "${status}" = 0 ] && echo "OK" && return
 	[ "${status}" = 1 ] && echo "NOT WORKING"
 	[ "${status}" = 2 ] && echo "NOT FOUND"
-	echo "error" > "${DEPS_STATUS}"
+	echo "error" > "${CMD_STATUS}"
 }
 
-DEPS_STATUS=$(mktemp)
-DEPS="
+install_deps() { (
+	LIB=$(basename "$1")
+	NEEDED_LIBS=$(readelf -d "$1" | sed -ne "s|.*NEEDED.* \[\(.*\)\]|\1|p" | sort)
+	SET_RPATH="0"
+	NOT_SYSTEM_LIBS=""
+	echo "INFO: ${LIB} ... checking for dependencies"
+	for needed_lib in ${NEEDED_LIBS}; do
+		IS_SYSTEM_LIB="0"
+		for system_lib in ${SYSTEM_LIBS}; do
+			[ "${DEBUG}" = "1" ] && echo "DEBUG: ${LIB} ... ${needed_lib} ... ${system_lib}"
+			[ "${needed_lib}" = "${system_lib}" ] && IS_SYSTEM_LIB="1" && break
+		done
+		if [ "${IS_SYSTEM_LIB}" != "1" ]; then
+			NOT_SYSTEM_LIBS="${NOT_SYSTEM_LIBS} ${needed_lib}"
+		fi
+	done
+	for needed_lib in ${NOT_SYSTEM_LIBS}; do
+		target_needed_lib="${PREFIX}/opt/vendor/lib/${needed_lib}"
+		vendor_needed_lib="${VENDOR_LIBDIR}/${needed_lib}"
+		if [ -e "${vendor_needed_lib}" ]; then
+			echo "INFO: ${LIB} ... found ${vendor_needed_lib}"
+			install -Dm644 "${vendor_needed_lib}" "${target_needed_lib}"
+			install_deps "${target_needed_lib}"
+			SET_RPATH="1"
+			continue
+		fi
+		echo "WARN: ${LIB} ... unable to find ${needed_lib}, this package is now useless" >&2
+	done
+	if [ "${SET_RPATH}" = "1" ]; then
+		echo "INFO: ${LIB} ... found dependencies, patching runpath"
+		patchelf --set-rpath '$ORIGIN' "$1"
+		return
+	fi
+	echo "INFO: ${LIB} ... no dependency found, erasing runpath"
+	patchelf --remove-rpath "$1"
+) }
+
+CMD_STATUS=$(mktemp)
+CMDS="
 basename
+dirname
 install
 ls
 patchelf
 readelf
+rm
 sed
 sort
 uname
 "
-for dep in ${DEPS}; do
-	echo "INFO: Checking command ${dep} ... $(check_cmd "${dep}" "${DEPS_STATUS}")"
+for cmd in ${CMDS}; do
+	echo "INFO: Checking command ${cmd} ... $(check_cmd "${cmd}" "${CMD_STATUS}")"
 done
-if [ "$(cat "${DEPS_STATUS}")" = "error" ]; then
-	rm -f "${DEPS_STATUS}"
-	echo "WARN: One or more dependencies are not installed. Install them then try again. Exiting ..." >&2
+if [ "$(cat "${CMD_STATUS}")" = "error" ]; then
+	rm -f "${CMD_STATUS}"
+	echo "WARN: Some commands are not installed! Install them then try again." >&2
 	exit
 fi
-rm -f "${DEPS_STATUS}"
+rm -f "${CMD_STATUS}"
 
 UNAME=$(uname -m)
 BIT=""
@@ -44,8 +83,10 @@ armv*|i*86) ;;
 *) echo "WARN: Unknown arch ${UNAME}" >&2
 esac
 
+SYSTEM_LIBS=$(ls /system/lib${BIT})
 TARGET_LIBOPENCL="${PREFIX}/opt/vendor/lib/libOpenCL.so"
 VENDOR_LIBOPENCL=""
+VENDOR_LIBDIR=""
 HAS_OPENCL="0"
 HAS_MALI="0"
 HAS_PVR="0"
@@ -55,20 +96,30 @@ if [ -n "${OVERRIDE_LIBOPENCL}" ]; then
 	echo "WARN: Forcing to use ${OVERRIDE_LIBOPENCL} as vendor OpenCL ..." >&2
 	if [ -e "${OVERRIDE_LIBOPENCL}" ]; then
 		VENDOR_LIBOPENCL="${OVERRIDE_LIBOPENCL}"
+		case "${VENDOR_LIBOPENCL}" in
+		/vendor/lib/*) VENDOR_LIBDIR="/vendor/lib" ;;
+		/vendor/lib64/*) VENDOR_LIBDIR="/vendor/lib64" ;;
+		/system/vendor/lib/*) VENDOR_LIBDIR="/system/vendor/lib" ;;
+		/system/vendor/lib64/*) VENDOR_LIBDIR="/system/vendor/lib64" ;;
+		*) VENDOR_LIBDIR=$(dirname "${VENDOR_LIBOPENCL}") ;;
+		esac
+		case "${VENDOR_LIBDIR}" in
+		*64) SYSTEM_LIBS=$(ls /system/lib64) ;;
+		*) SYSTEM_LIBS=$(ls /system/lib) ;;
+		esac
 	else
-		echo "INFO: Falling back to autodetect as provided path is invalid."
+		echo "WARN: Falling back to autodetect as provided path is invalid" >&2
 	fi
 fi
 
 # autodetect
 if [ -z "${VENDOR_LIBOPENCL}" ]; then
-	VENDOR_LIBDIR=""
 	if [ -e "/vendor/lib${BIT}" ]; then
 		VENDOR_LIBDIR="/vendor/lib${BIT}"
 	elif [ -e "/system/vendor/lib${BIT}" ]; then
 		VENDOR_LIBDIR="/system/vendor/lib${BIT}"
 	else
-		echo "WARN: /vendor and /system are not accessible. This package is now useless." >&2
+		echo "WARN: /vendor and /system are not accessible! This package is now useless!" >&2
 		exit
 	fi
 
@@ -83,57 +134,17 @@ if [ -z "${VENDOR_LIBOPENCL}" ]; then
 	1:0:0) VENDOR_LIBOPENCL="${VENDOR_LIBDIR}/libOpenCL.so" ;;
 	1:0:1) VENDOR_LIBOPENCL="${VENDOR_LIBDIR}/libPVROCL.so" ;;
 	1:1:0) VENDOR_LIBOPENCL="${VENDOR_LIBDIR}/egl/libGLES_mali.so" ;;
-	*) echo "WARN: No drivers found! This package is now useless." >&2 && exit
+	*) echo "WARN: No drivers found! This package is now useless!" >&2 && exit
 	esac
 fi
 
-echo "INFO: Found ${VENDOR_LIBOPENCL}, installing as ${TARGET_LIBOPENCL} ..."
+echo "INFO: VENDOR_LIBOPENCL = ${VENDOR_LIBOPENCL}"
+echo "INFO: VENDOR_LIBDIR    = ${VENDOR_LIBDIR}"
+echo "INFO: Installing as ${TARGET_LIBOPENCL} ..."
 install -Dm644 "${VENDOR_LIBOPENCL}" "${TARGET_LIBOPENCL}"
+install_deps "${TARGET_LIBOPENCL}"
 
-install_deps() { (
-	LIB=$(basename "$1")
-	NEEDED_LIBS=$(readelf -d "$1" | sed -ne "s|.*NEEDED.* \[\(.*\)\]|\1|p" | sort)
-	SET_RPATH="0"
-	NOT_SYSTEM_LIBS=""
-	echo "INFO: Checking missing dependencies for ${LIB} ..."
-	for needed_lib in ${NEEDED_LIBS}; do
-		IS_SYSTEM_LIB="0"
-		for system_lib in ${SYSTEM_LIBS}; do
-			[ "${DEBUG}" = "1" ] && echo "DEBUG: ${LIB} ... ${needed_lib} ... ${system_lib}"
-			[ "${needed_lib}" = "${system_lib}" ] && IS_SYSTEM_LIB="1" && break
-		done
-		if [ "${IS_SYSTEM_LIB}" != "1" ]; then
-			NOT_SYSTEM_LIBS="${NOT_SYSTEM_LIBS} ${needed_lib}"
-		fi
-	done
-	for needed_lib in ${NOT_SYSTEM_LIBS}; do
-		target_needed_lib="${PREFIX}/opt/vendor/lib/${needed_lib}"
-		vendor_needed_lib=""
-		if [ -e "${VENDOR_LIBDIR}/${needed_lib}" ]; then
-			vendor_needed_lib="${VENDOR_LIBDIR}/${needed_lib}"
-		else
-			echo "WARN: Unable to find ${needed_lib}. This package may not work properly." >&2
-		fi
-		if [ -n "${vendor_needed_lib}" ]; then
-			echo "INFO: Installing missing dependency ${vendor_needed_lib} ..."
-			install -Dm644 "${vendor_needed_lib}" "${target_needed_lib}"
-			install_deps "${target_needed_lib}"
-			SET_RPATH="1"
-		fi
-	done
-	if [ "${SET_RPATH}" = "1" ]; then
-		echo "INFO: Patching rpath for $1 ..."
-		patchelf --set-rpath '$ORIGIN' "$1"
-	else
-		echo "INFO: Removing rpath for $1 ..."
-		patchelf --remove-rpath "$1"
-	fi
-) }
-
-if [ -n "${VENDOR_LIBOPENCL}" ]; then
-	SYSTEM_LIBS=$(ls /system/lib${BIT})
-	install_deps "${TARGET_LIBOPENCL}"
-	echo "
+echo "
 ===== Post install notice =====
 
 If there's any ROM upgrades taken place after
@@ -143,4 +154,3 @@ updated OpenCL drivers.
 
 ===== Post install notice =====
 "
-fi
