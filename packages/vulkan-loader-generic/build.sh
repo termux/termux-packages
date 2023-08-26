55b75813acf2b2c2a20982958e5d0e67f91ee71b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-08-26 10:27:01 +0800

    vulkan-loader-generic: bump to 1.3.262

--- packages/vulkan-loader-generic/build.sh
+++ packages/vulkan-loader-generic/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-Loader
 TERMUX_PKG_DESCRIPTION="Vulkan Loader"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.3.261"
+TERMUX_PKG_VERSION="1.3.262"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Loader/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=85d13004c81b032baf7cc4c2de0b2cb57072a86855d7ca7fc9a813621da275ba
+TERMUX_PKG_SHA256=3bbaa5ee64058a89949eb777de66ce94bfe3141892514172cfc9451c756802d5
 TERMUX_PKG_BUILD_DEPENDS="vulkan-headers (=${TERMUX_PKG_VERSION}), libx11, libxcb, libxrandr"
 TERMUX_PKG_CONFLICTS="vulkan-loader-android"
 TERMUX_PKG_PROVIDES="vulkan-loader-android"
