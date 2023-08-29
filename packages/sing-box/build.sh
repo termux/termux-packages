d36b37e16ed70369f16211bae3dbde77d4b67b3c
CHIZI-0618 <kay9925@outlook.com>
2023-08-29 11:51:29 +0800

    sing-box: Bump to 1.4.0

--- packages/sing-box/build.sh
+++ packages/sing-box/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://sing-box.sagernet.org
 TERMUX_PKG_DESCRIPTION="The universal proxy platform"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="kay9925@outlook.com"
-TERMUX_PKG_VERSION="1.3.6"
+TERMUX_PKG_VERSION="1.4.0"
 TERMUX_PKG_SRCURL="https://github.com/SagerNet/sing-box/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=a6462c1c41acfea0ac1c815985e6573dc92895541f75f678c805203879421927
+TERMUX_PKG_SHA256=ab6698545442e9197339f459553e241ff91396ba39a8e5d14e0a792e78d290a0
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
 
-	local tags="with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api"
+	local tags="with_gvisor,with_quic,with_dhcp,with_wireguard,with_utls,with_clash_api"
 	local ldflags="\
 	-w -s \
 	-X 'github.com/sagernet/sing-box/constant.Version=${TERMUX_PKG_VERSION}' \
@@ -29,7 +29,7 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 ./${TERMUX_PKG_NAME} ${TERMUX_PREFIX}/bin
+	install -Dm700 ${TERMUX_PKG_NAME} ${TERMUX_PREFIX}/bin
 
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/bash-completion/completions/sing-box.bash"
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/fish/vendor_completions.d/sing-box.fish"
