ee816c172f5850f2dad06e74c0fecfa585f16704
Lucy Phipps <landfillbaby69@gmail.com>
2023-08-30 14:07:37 +0100

    vim{,-python,-gtk}: update to 9.0.1800

--- x11-packages/vim-gtk/build.sh
+++ x11-packages/vim-gtk/build.sh
@@ -5,9 +5,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 
 # vim should only be updated every 50 releases on multiples of 50.
 # Update all of vim, vim-python and vim-gtk to the same version in one PR.
-TERMUX_PKG_VERSION=9.0.1750
-TERMUX_PKG_SRCURL=https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=730127765e120ed3582e5b2de433ca6d3c74feb77cb6cb8780fd13b406642111
+TERMUX_PKG_VERSION=9.0.1800
+TERMUX_PKG_SRCURL="https://github.com/vim/vim/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=bc65a1361a7473c79c63f3a9000474f5340eb73d9c5b8c987100eafdb7016f80
 
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libcanberra, libice, libiconv, liblua52, libsm, libx11, libxt, ncurses, pango, python"
 TERMUX_PKG_CONFLICTS="vim, vim-python, vim-runtime"
