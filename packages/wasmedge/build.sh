b3d6f1a82dbe8a5b0825361f7e429706c0c57a0a
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-03 08:10:15 +0800

    bump(main/wasmedge): 0.13.3 (#17744)
    * Enable auto update
    * Enable full features for arm
    * Preliminary x86 support
      Add lib-system-allocator.cpp.patch to build further but someone has to
      fix remaining issues for i686
    * Remove getsockopt-socklen_t.patch
      No longer needed to fix type uint32_t to socklen_t (int32_t) for 32bit
      platforms

--- packages/wasmedge/build.sh
+++ packages/wasmedge/build.sh
@@ -3,18 +3,19 @@ TERMUX_PKG_DESCRIPTION="A lightweight, high-performance, and extensible WebAssem
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.spdx"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.12.1
+TERMUX_PKG_VERSION=0.13.3
 TERMUX_PKG_SRCURL=https://github.com/WasmEdge/WasmEdge/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=68812775e5f5237300101f712eadf9d666b18859021edfa672e914db140ef957
+TERMUX_PKG_SHA256=ad7a09f1a0f245473af748244f656387841e9f496b894a66feee954066bb6454
 TERMUX_PKG_DEPENDS="libc++, libllvm, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, boost-static, libllvm-static, libpolly, lld, llvm"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWASMEDGE_FORCE_DISABLE_LTO=ON
 "
 
-# WASMEDGE_BUILD_AOT_RUNTIME is not supported on 32-bit archs.
-# See https://github.com/WasmEdge/WasmEdge/blob/2414c83047f2bf8fe241716be6ef8c0de34dc245/lib/aot/compiler.cpp#L4874
-if [ $TERMUX_ARCH_BITS = 32 ]; then
+# WASMEDGE_BUILD_AOT_RUNTIME is not supported on i686
+# https://github.com/WasmEdge/WasmEdge/blob/f6d99c87fef0db160d17ccf3f7f3cd87cbd56e68/lib/aot/compiler.cpp#L5032
+if [[ "${TERMUX_ARCH_BITS}" == "i686" ]]; then
 	TERMUX_PKG_DEPENDS="libc++"
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DWASMEDGE_BUILD_AOT_RUNTIME=OFF"
 fi
@@ -26,16 +27,4 @@ fi
 # static_assert(alignof(int64_t) == 8, "non-wasi data layout");
 # ^             ~~~~~~~~~~~~~~~~~~~~~
 # ```
-# Build failure for i686 (0.10.1):
-# [14/83] Building CXX object lib/system/CMakeFiles/wasmedgeSystem.dir/allocator.cpp.o
-# FAILED: lib/system/CMakeFiles/wasmedgeSystem.dir/allocator.cpp.o
-# ...
-# /home/builder/.termux-build/wasmedge/src/lib/system/allocator.cpp:149:42: error: use of undeclared identifier 'PROT_READ'
-#  if (auto Pointer = mmap(nullptr, Size, PROT_READ | PROT_WRITE,
-#                                         ^
-# Both i686 and arm warning (0.10.1):
-# /home/builder/.termux-build/wasmedge/src/lib/aot/compiler.cpp:1202:24: warning: unused variable 'VectorSize' [-Wunused-variable]
-#         const uint32_t VectorSize = IsFloat ? 4 : 2;
-#                        ^
-# Upstream doesnt seem to support 32bit platforms well
 TERMUX_PKG_BLACKLISTED_ARCHES="i686"
