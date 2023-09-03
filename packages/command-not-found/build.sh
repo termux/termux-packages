6c9892bee6940465bb03082db2438626dfef0e2b
Lucy Phipps <landfillbaby69@gmail.com>
2023-09-03 09:29:04 +0100

    command-not-found: update to 2.3.0

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2.0
-TERMUX_PKG_REVISION=15
+TERMUX_PKG_VERSION=2.3.0
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=857b99ca436079133ebd3d87c1b9ce0cdbbd1c88d2f6015ec237b531c3c306b8
+TERMUX_PKG_SHA256=78a407f3ef3bce2ada4b286be5e31d6b9cb03b6385ee8ac184609e64d86018f7
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
