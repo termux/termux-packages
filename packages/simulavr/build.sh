4dc97b4b31a768318c5d7f404bd95f721b86c687
Lucy Phipps <landfillbaby69@gmail.com>
2023-09-11 07:32:21 +0100

    simulavr: fix conflicts and docs location
    moves its headers from `$PREFIX/include` to `$PREFIX/include/simulavr`
    to stop e.g. `memory.h` conflicting with `ndk-sysroot`.
    copied from Arch Linux `simulavr-git`

--- packages/simulavr/build.sh
+++ packages/simulavr/build.sh
@@ -6,6 +6,7 @@ _VERSION_MAJOR=1
 _VERSION_MINOR=1
 _VERSION_PATCH=0
 TERMUX_PKG_VERSION=1:${_VERSION_MAJOR}.${_VERSION_MINOR}.${_VERSION_PATCH}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="git+https://git.savannah.nongnu.org/git/simulavr"
 TERMUX_PKG_GIT_BRANCH=release-${TERMUX_PKG_VERSION#*:}
 TERMUX_PKG_DEPENDS="libc++"
@@ -25,4 +26,13 @@ termux_step_post_get_source() {
 		-e "s|@VERSION_PATCH@|${_VERSION_PATCH}|g" \
 		$TERMUX_PKG_BUILDER_DIR/hardcode-version.diff \
 		| patch --silent -p1
+}
+
+termux_step_post_make_install() {
+	mv "$TERMUX_PREFIX/share/doc/common" "$TERMUX_PREFIX/share/doc/simulavr"
+	# Headers are moved into their own subdirectory to prevent conflicts.
+	# Might cause issues when using them.
+	mv "$TERMUX_PREFIX/include" "$TERMUX_PREFIX/include-simulavr"
+	mkdir "$TERMUX_PREFIX/include"
+	mv "$TERMUX_PREFIX/include-simulavr" "$TERMUX_PREFIX/include/simulavr"
 }
