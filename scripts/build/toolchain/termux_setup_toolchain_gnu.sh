a649837ed106e8783cd76df044849984600bf119
Ivan Max <mixython@gmail.com>
2023-09-13 12:18:12 +0000

    scripts/*: change PATH setting location (#17813)

--- scripts/build/toolchain/termux_setup_toolchain_gnu.sh
+++ scripts/build/toolchain/termux_setup_toolchain_gnu.sh
@@ -20,14 +20,19 @@ termux_setup_toolchain_gnu() {
 		export CXXFILT=$TERMUX_HOST_PLATFORM-c++filt
 	fi
 
+	export PATH_DYNAMIC_LINKER="$TERMUX_PREFIX/lib/"
 	if [ "$TERMUX_ARCH" = "aarch64" ]; then
 		CFLAGS+=" -march=armv8-a"
+		PATH_DYNAMIC_LINKER+="ld-linux-aarch64.so.1"
 	elif [ "$TERMUX_ARCH" = "arm" ]; then
 		CFLAGS+=" -march=armv7-a -mfloat-abi=hard -mfpu=neon"
+		PATH_DYNAMIC_LINKER+="ld-linux-armhf.so.3"
 	elif [ "$TERMUX_ARCH" = "x86_64" ]; then
 		CFLAGS+=" -fPIC -march=x86-64"
+		PATH_DYNAMIC_LINKER+="ld-linux-x86-64.so.2"
 	elif [ "$TERMUX_ARCH" = "i686" ]; then
 		CFLAGS+=" -march=i686"
+		PATH_DYNAMIC_LINKER+="ld-linux.so.2"
 	fi
 
 	case "$TERMUX_ARCH" in
@@ -40,6 +45,12 @@ termux_setup_toolchain_gnu() {
 	export PKG_CONFIG=pkg-config
 	export PKGCONFIG=$PKG_CONFIG
  	export PKG_CONFIG_LIBDIR="$TERMUX_PKG_CONFIG_LIBDIR"
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+		if ! $(echo "$PATH" | grep -q "^$TERMUX_STANDALONE_TOOLCHAIN/bin:$TERMUX_PREFIX/bin"); then
+			export PATH="$TERMUX_STANDALONE_TOOLCHAIN/bin:$TERMUX_PREFIX/bin:$PATH"
+		fi
+	fi
 
 	export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
 }
