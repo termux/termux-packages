679a59f748172bfc643ddc3cbf221d7cdb857f28
dev-bz <32380878+dev-bz@users.noreply.github.com>
2023-09-18 07:54:45 +0800

    libprotobuf: Bump to v4.24.3

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # - update SHA256 checksum for $_PROTOBUF_ZIP in
 #     $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
 # - ALWAYS bump revision of reverse dependencies and rebuild them.
-TERMUX_PKG_VERSION=2:22.4
+TERMUX_PKG_VERSION=4:24.3
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION#*:}.tar.gz
 TERMUX_PKG_SHA256=def8683aafc1ebaddbc777da252dfdc8e324a197757e3bfcd8b4de90d4b8cf6a
 TERMUX_PKG_AUTO_UPDATE=false
