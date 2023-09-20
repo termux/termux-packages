920cceed92b802fd58bb839504711cd04d6033bc
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-09-20 16:07:05 +0800

    fx: bump to 30.0.2

--- packages/fx/build.sh
+++ packages/fx/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://github.com/antonmedv/fx"
 TERMUX_PKG_DESCRIPTION="Interactive JSON viewer on your terminal"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@flosnvjx"
-TERMUX_PKG_VERSION="30.0.0"
+TERMUX_PKG_VERSION="30.0.2"
 TERMUX_PKG_SRCURL="https://github.com/antonmedv/fx/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=bd9c5827c83ba791c13bacbc6223ea190fd9c9d5d03520e6966f59973dccc049
+TERMUX_PKG_SHA256=8ac17c5fd0beecbd9e94b8a8a0af8ed93111a66afe55b596d9fcd032f73f9365
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -16,5 +16,4 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin bin/*
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME README.* doc/*.*
 }
