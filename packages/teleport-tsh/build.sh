e6a057b576f35b3b4ca6fd6deffd1033faf9d4dd
Roman Kredentser <shareed2k@gmail.com>
2023-09-20 14:51:35 +0300

    bump(main/teleport-tsh): 13.3.8 (#17852)

--- packages/teleport-tsh/build.sh
+++ packages/teleport-tsh/build.sh
@@ -2,21 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gravitational/teleport
 TERMUX_PKG_DESCRIPTION="Secure Access for Developers that doesn't get in the way"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=11.0.3
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_VERSION=13.3.8
+TERMUX_PKG_SRCURL=https://github.com/gravitational/teleport/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1eb2471c144c480abe7effa0e90e921e050ab6964f06ab51381f22c119871967
+TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make_install() {
+termux_step_make() {
 	termux_setup_golang
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	export BUILDDIR=$TERMUX_PREFIX/bin
-
-	mkdir -p $GOPATH/src/github.com/gravitational
-	cd $GOPATH/src/github.com/gravitational
-	git clone https://github.com/gravitational/teleport.git
-	cd teleport
-
-	git checkout "v$TERMUX_PKG_VERSION"
+	export GOPATH=$TERMUX_PKG_CACHEDIR/go
+	export BUILDDIR=$TERMUX_PKG_SRCDIR/cmd
 
 	make $BUILDDIR/tsh
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin $BUILDDIR/tsh
 }
