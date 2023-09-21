62203fdbe7bf0f6c394a654e155bcc9f8b691cdd
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-22 07:14:44 +0800

    fix(main/tinygo): custom auto update
    Rework auto update a bit
    
    %ci:no-build

--- packages/tinygo/build.sh
+++ packages/tinygo/build.sh
@@ -43,13 +43,12 @@ lib/libLLVMXRay.a
 termux_pkg_auto_update() {
 	local latest_tag
 	latest_tag=$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}")
-
-	if [[ -z "${latest_tag}" ]]; then
-		termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
-	fi
-
 	if [[ "${latest_tag}" == "${TERMUX_PKG_VERSION}" ]]; then
 		echo "INFO: No update needed. Already at version '${TERMUX_PKG_VERSION}'."
+		return
+	fi
+	if [[ -z "${latest_tag}" ]]; then
+		echo "WARN: Auto update failure!" >&2
 		return
 	fi
 
@@ -78,7 +77,7 @@ termux_pkg_auto_update() {
 
 	local tmpdir=$(mktemp -d)
 	git clone --branch "${latest_tag}" --depth=1 --recursive \
-		"${TERMUX_PKG_SRCDIR#git+}" "${tmpdir}"
+		"${TERMUX_PKG_SRCURL#git+}" "${tmpdir}"
 	make -C "${tmpdir}" llvm-source GO=:
 	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum | cut -d" " -f1)
 
