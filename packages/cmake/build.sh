8625c7362c2ac01c45cbbe6004bad6c742a528e0
Maxython <mixython@gmail.com>
2023-09-22 19:17:18 +0300

    bump(main/cmake): to 3.27.5

--- packages/cmake/build.sh
+++ packages/cmake/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Family of tools designed to build, test and package soft
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # When updating version here, please update termux_setup_cmake.sh as well.
-TERMUX_PKG_VERSION=3.26.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.27.5
 TERMUX_PKG_SRCURL=https://www.cmake.org/files/v${TERMUX_PKG_VERSION:0:4}/cmake-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=313b6880c291bd4fe31c0aa51d6e62659282a521e695f30d5cc0d25abbd5c208
+TERMUX_PKG_SHA256=5175e8fe1ca9b1dd09090130db7201968bcce1595971ff9e9998c2f0765004c9
 TERMUX_PKG_DEPENDS="libarchive, libc++, libcurl, libexpat, jsoncpp, libuv, rhash, zlib"
 TERMUX_PKG_RECOMMENDS="clang, make"
 TERMUX_PKG_FORCE_CMAKE=true
