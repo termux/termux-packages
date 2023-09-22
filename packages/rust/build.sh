8ba33c80b4858cd6e57c8234fa63973f54e16329
Skyler Hawthorne <skyler@dead10ck.dev>
2023-09-22 06:29:04 -0400

    remove revision
    Co-authored-by: Jia Yuan Lo <jylo06g@gmail.com>

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed an
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.71.1
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
 TERMUX_PKG_SHA256=371af0fbe04051e20a74dbea6d4e4e548f10f15309c49cae2688afb882b6c7f1
 _LLVM_MAJOR_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $LLVM_MAJOR_VERSION)
