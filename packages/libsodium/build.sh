6652093cf042a4a43d73169d6ba9c7d476538f1a
Twaik Yont <twaikyont@gmail.com>
2023-09-27 20:22:49 +0300

    bump(main/libsodium): bump 1.0.19

--- packages/libsodium/build.sh
+++ packages/libsodium/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://libsodium.org/
 TERMUX_PKG_DESCRIPTION="Network communication, cryptography and signaturing library"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.18
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="1.0.19"
 TERMUX_PKG_SRCURL=https://github.com/jedisct1/libsodium/archive/${TERMUX_PKG_VERSION}-RELEASE.tar.gz
-TERMUX_PKG_SHA256=b7292dd1da67a049c8e78415cd498ec138d194cfdb302e716b08d26b80fecc10
+TERMUX_PKG_SHA256=4fb996013283f482f46a457c8ff2c1495e797788e78e8ec56b1aa1b19253bf75
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_BREAKS="libsodium-dev"
@@ -14,7 +13,7 @@ TERMUX_PKG_REPLACES="libsodium-dev"
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=23
+	local _SOVERSION=26
 
 	local e=$(sed -En 's/^SODIUM_LIBRARY_VERSION=([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
 				configure.ac)
