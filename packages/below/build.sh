3375c6d0e6ca389af92946285d8f69b5a3f2fd3c
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-28 21:30:42 +0800

    chore(main/below): switch to auto update newest-tag (#17965)
    %ci:no-build

--- packages/below/build.sh
+++ packages/below/build.sh
@@ -5,9 +5,10 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.7.1
 TERMUX_PKG_SRCURL=https://github.com/facebookincubator/below/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9b70d010189e8d343dc67b730a1d8eeb0e1d19805688e4f70662e216fb4cd6b5
-TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libelf, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 # ```
 # error[E0308]: mismatched types
