b0417a5b798d03969c5da5ad7307abe6c7e397b8
Twaik Yont <twaikyont@gmail.com>
2023-09-28 14:49:56 +0300

    bump(main/libsecp256k1): 0.4.0

--- packages/libsecp256k1/build.sh
+++ packages/libsecp256k1/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/bitcoin-core/secp256k1
 TERMUX_PKG_DESCRIPTION="Optimized c library for ECDSA signatures and seret/public key operations on curve secp256k1"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:0.3.2
+TERMUX_PKG_VERSION="1:0.4.0"
 TERMUX_PKG_SRCURL=https://github.com/bitcoin-core/secp256k1/archive/refs/tags/v${TERMUX_PKG_VERSION#*:}.tar.gz
-TERMUX_PKG_SHA256=ef2e1061951b8cf94a7597b4e60fd7810613e327e25305e8d73dfdff67d12190
+TERMUX_PKG_SHA256=d7c956606e7f52b7703fd2967cb31d2e21ec90c0b440ff1cc7c7d764a4092b98
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 # These flags are suggested by electrum
