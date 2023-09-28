081ed676759f35d340968a5452b3bc4ab213a076
Twaik Yont <twaikyont@gmail.com>
2023-09-28 10:00:06 +0300

    rebuild: boost reverse dependencies after boost upgrade

--- packages/libtorrent-rasterbar/build.sh
+++ packages/libtorrent-rasterbar/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A feature complete C++ bittorrent implementation focusin
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.0.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/arvidn/libtorrent/releases/download/v${TERMUX_PKG_VERSION}/libtorrent-rasterbar-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=90cd92b6061c5b664840c3d5e151d43fedb24f5b2b24e14425ffbb884ef1798e
 TERMUX_PKG_DEPENDS="boost, libc++, openssl, python"
