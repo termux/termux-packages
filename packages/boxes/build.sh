d0fd287a94b534349902208c204a846487980205
Jia Yuan Lo <jylo06g@gmail.com>
2023-10-01 07:38:32 +0800

    bump(main/boxes): 2.2.1 (#18018)

--- packages/boxes/build.sh
+++ packages/boxes/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://boxes.thomasjensen.com/
 TERMUX_PKG_DESCRIPTION="A command line filter program which draws ASCII art boxes around your input text"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2.0
-TERMUX_PKG_SRCURL=https://github.com/ascii-boxes/boxes/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=98b8e3cf5008f46f096d5775d129c34db9f718728bffb0f5d67ae89bb494102e
+TERMUX_PKG_VERSION=2.2.1
+TERMUX_PKG_SRCURL=https://github.com/ascii-boxes/boxes/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fa4f7cd1876e4b22e950b4ca7c90776eb8edcf137316bdfd9c1780cf7cfb5d73
 TERMUX_PKG_DEPENDS="libunistring, pcre2"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_CONFFILES="
 share/boxes/boxes-config
 "
@@ -23,3 +24,7 @@ termux_step_make_install() {
 	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 doc/boxes.1
 	install -Dm600 -t $TERMUX_PREFIX/share/boxes boxes-config
 }
+
+# Do not use /archive/ but /archive/refs/tags/ as SRCURL
+# https://github.com/ascii-boxes/boxes/archive/v2.2.1.tar.gz
+# the given path has multiple possibilities: #<Git::Ref:0x00007f3805df9d98>, #<Git::Ref:0x00007f3805df94d8>
