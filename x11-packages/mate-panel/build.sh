d85f0e5d1d2a6badaf7f7e163266448ff154deb5
Twaik Yont <twaikyont@gmail.com>
2023-10-02 01:39:01 +0300

    bump(x11/mate-panel): 1.27.2

--- x11-packages/mate-panel/build.sh
+++ x11-packages/mate-panel/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://mate-panel.mate-desktop.dev/
 TERMUX_PKG_DESCRIPTION="mate-panel contains the MATE panel, the libmate-panel-applet library and several applets"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
-TERMUX_PKG_VERSION=1.26.3
+TERMUX_PKG_VERSION="1.27.2"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-panel/releases/download/v$TERMUX_PKG_VERSION/mate-panel-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=f33f10d527450ba7e98cc70ab255ac481330aa9e66dbcc7c511b6baa171de029
+TERMUX_PKG_SHA256=7a19e270bded652d9c200b97c346b2e7d3be6077ca995c1eb8596cfc1866047f
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libice, libmateweather, libsm, libwnck, libx11, libxrandr, mate-desktop, mate-menus, pango, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_DISABLE_GIR=true
