580f9aa79f78818c80fc3955f83cb8518c05d9e1
Twaik Yont <twaikyont@gmail.com>
2023-10-01 12:17:18 +0300

    bump(main/htslib): 1.18

--- packages/htslib/build.sh
+++ packages/htslib/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/samtools/htslib
 TERMUX_PKG_DESCRIPTION="C library for high-throughput sequencing data formats"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.18"
 TERMUX_PKG_SRCURL=https://github.com/samtools/htslib/releases/download/${TERMUX_PKG_VERSION}/htslib-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=763779288c40f07646ec7ad98b96c378c739171d162ad98398868783b721839f
+TERMUX_PKG_SHA256=f1ab53a593a2320a1bfadf4ef915dae784006c5b5c922c8a8174d7530a9af18f
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libbz2, liblzma, zlib, libdeflate, libcurl"
 
 # error: assigning to 'uint8x8_t' (vector of 8 'uint8_t' values) from incompatible type 'int'
