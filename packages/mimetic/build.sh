969b77e58333363ef7caa4bd9108bb55f5eda5f0
Fredrik Fornwall <fredrik@fornwall.net>
2023-10-02 14:34:48 +0200

    mimetic: Add patch for c++17 (and ndk 26 compatibility)

--- packages/mimetic/build.sh
+++ packages/mimetic/build.sh
@@ -6,3 +6,9 @@ TERMUX_PKG_VERSION=0.9.8
 TERMUX_PKG_SRCURL=https://www.codesink.org/download/mimetic-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3a07d68d125f5e132949b078c7275d5eb0078dd649079bd510dd12b969096700
 TERMUX_PKG_DEPENDS="libc++"
+# 32-bit arm does not build:
+# ERROR: lib/libmimetic.so contains undefined symbols:
+# 21: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_l2d
+# 75: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_uidivmod
+# 131: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_idiv
+TERMUX_PKG_BLACKLISTED_ARCHES="arm"
