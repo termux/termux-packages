b34aa796c3c327d57674c5729eeeb092ab487449
Henrik Grimler <grimler@termux.dev>
2023-10-02 22:54:59 +0200

    fix(main/mimetic): fix undefined symbols in arm build
    Need to link against libclang_rt.builtins-arm-android.a.

--- packages/mimetic/build.sh
+++ packages/mimetic/build.sh
@@ -3,12 +3,11 @@ TERMUX_PKG_DESCRIPTION="A C++ Email library (MIME)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=0.9.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.codesink.org/download/mimetic-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3a07d68d125f5e132949b078c7275d5eb0078dd649079bd510dd12b969096700
 TERMUX_PKG_DEPENDS="libc++"
-# 32-bit arm does not build:
-# ERROR: lib/libmimetic.so contains undefined symbols:
-# 21: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_l2d
-# 75: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_uidivmod
-# 131: 00000000     0 NOTYPE  GLOBAL DEFAULT   UND __aeabi_idiv
-TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+}
