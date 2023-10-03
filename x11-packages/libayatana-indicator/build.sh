75d2e47df3b6cc7050e45f36a9576dac46e7b7f6
Twaik Yont <twaikyont@gmail.com>
2023-10-03 21:46:27 +0300

    chore(x11/libayatana-indicator): enable auto updates [no ci]

--- x11-packages/libayatana-indicator/build.sh
+++ x11-packages/libayatana-indicator/build.sh
@@ -5,4 +5,6 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.9.3
 TERMUX_PKG_SRCURL=https://github.com/AyatanaIndicators/libayatana-indicator/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=09c5456fcb430b6ee0626fafdf99a32eb8746b267d56ab2bd4c8a8dd6ca731da
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="ayatana-ido, gdk-pixbuf, glib, gtk3"
