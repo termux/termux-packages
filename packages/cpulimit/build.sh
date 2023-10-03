63b291e2d942c2268a3377da014925735402d0ee
Twaik Yont <twaikyont@gmail.com>
2023-10-03 14:59:20 +0300

    chore(main/cpulimit): enable auto updates [no ci]

--- packages/cpulimit/build.sh
+++ packages/cpulimit/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_VERSION=0.2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/opsengine/cpulimit/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=64312f9ac569ddcadb615593cd002c94b76e93a0d4625d3ce1abb49e08e2c2da
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
