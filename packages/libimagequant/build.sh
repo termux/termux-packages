7631870cf8253e473e93b92fadbc81d7ddb8aa07
Twaik Yont <twaikyont@gmail.com>
2023-10-03 18:40:57 +0300

    chore(main/libimagequant): enable auto updates [no ci]

--- packages/libimagequant/build.sh
+++ packages/libimagequant/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.2.1
 TERMUX_PKG_SRCURL=https://github.com/ImageOptim/libimagequant/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=14193219fa194d6baee4347bd13bd01fd46d09fe3b59ae35c89b698da96d1198
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
