8d36c3ebd586aadde65da463d5cc8c53ada6b6a0
Kevin Williams <admin@utermux.dev>
2023-10-03 10:11:00 +0800

    fix(x11/lite-xl): desktop shortcuts
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/lite-xl/build.sh
+++ x11-packages/lite-xl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A lightweight text editor written in Lua"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@leapofazzam123"
 TERMUX_PKG_VERSION=2.1.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lite-xl/lite-xl/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=75c0cbda2d34a88e86d3aa48abe9f0366093df3019796c03067a7594cc55abe6
 TERMUX_PKG_DEPENDS="freetype, liblua54, pcre2, sdl2"
