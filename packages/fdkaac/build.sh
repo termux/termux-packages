0b8b74329e6699d4f92a35b60b6db0e4ccdf6a80
Twaik Yont <twaikyont@gmail.com>
2023-10-03 16:01:43 +0300

    chore(main/fdkaac): enable auto updates [no ci]

--- packages/fdkaac/build.sh
+++ packages/fdkaac/build.sh
@@ -5,6 +5,8 @@ TERMUX_PKG_MAINTAINER="@DLC01"
 TERMUX_PKG_VERSION=1.0.5
 TERMUX_PKG_SRCURL=https://github.com/nu774/fdkaac/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=87b2d2cc913a1f90bd19315061ede81c1c3364e160802c70117a7ea81e80bd33
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="libfdk-aac"
 
 termux_step_pre_configure() {
