0f2f631f19393ea0e2e12623f862d93c0a1cc930
Twaik Yont <twaikyont@gmail.com>
2023-10-04 13:26:33 +0300

    chore(main/libcoinor-utils): enable auto updates [no ci]

--- packages/libcoinor-utils/build.sh
+++ packages/libcoinor-utils/build.sh
@@ -5,6 +5,8 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1:2.11.9
 TERMUX_PKG_SRCURL=https://github.com/coin-or/CoinUtils/archive/refs/tags/releases/${TERMUX_PKG_VERSION#*:}.tar.gz
 TERMUX_PKG_SHA256=15d572ace4cd3b7c8ce117081b65a2bd5b5a4ebaba54fadc99c7a244160f88b8
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
