b7a41dc16619bba3e99fafeea5083eb5394fd5db
Twaik Yont <twaikyont@gmail.com>
2023-10-04 18:38:31 +0300

    bump(main/openexr): 3.2.1 (+revdeps revbump)

--- packages/libvips/build.sh
+++ packages/libvips/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A fast image processing library with low memory needs"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Thibault Meyer <meyer.thibault@gmail.com>"
 TERMUX_PKG_VERSION="8.14.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libvips/libvips/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=de5ab7ffa5c1ad9d68f97bfa8c31b6a7e4a184c89947552ea92107318b0208b4
 TERMUX_PKG_AUTO_UPDATE=true
