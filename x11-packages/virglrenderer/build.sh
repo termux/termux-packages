2edc3efae0bde70bcad7d5c9cfec1cdc80d7aa19
Twaik Yont <twaikyont@gmail.com>
2023-10-05 12:25:28 +0300

    bump(x11/virglrenderer): 1.0.0

--- x11-packages/virglrenderer/build.sh
+++ x11-packages/virglrenderer/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://virgil3d.github.io/
 TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.10.4
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="1.0.0"
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fd9a1b12473f4cda8d87e6ba1a6e5714a24355e16b69ed85df5c21bf48f797fa
+TERMUX_PKG_SHA256=4f864c9ba80a5ab474a5c6c103a1fc5bb2e7ab29a61a01fc121fbd04e7dcc3ac
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Dtracing=stderr"
