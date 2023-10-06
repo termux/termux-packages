966d28a67159503ee5205192590428bf5b37649b
Fredrik Fornwall <fredrik@fornwall.net>
2023-10-06 10:15:51 +0200

    fix(main/frp): Break dependency cycle by merging subpackages
    Currently frp have two subpackages: frpc and frps
    
    There is a cyclic dependency between these: frp depends each subpackage,
    and each subpackage depends on frp.
    
    This makes scripts/buildorder.py (used by build-all.sh) error out.
    
    And there is also no point in this current package split, as all three
    packages will always be installed together due to the cyclic dependency.
    So let's remove the subpackage split.

--- packages/frp/build.sh
+++ packages/frp/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="A fast reverse proxy to expose a local server behind a N
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
 TERMUX_PKG_VERSION="0.51.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fatedier/frp/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef
-# Depend on its subpackages.
-TERMUX_PKG_DEPENDS="frpc, frps"
+TERMUX_PKG_REPLACES="frpc, frps"
+TERMUX_PKG_BREAKS="frpc, frps"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
