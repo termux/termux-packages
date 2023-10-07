6e3881560469bf00e8982e03a118180543cc35ce
Fredrik Fornwall <fredrik@fornwall.net>
2023-10-07 13:37:14 +0200

    fix(main/gobang): Fix build failure due to newer rust version

--- packages/gobang/build.sh
+++ packages/gobang/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A cross-platform TUI database management tool written in
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.1.0-alpha.5"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://github.com/TaKO8Ki/gobang/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=923210d500f070ac862c73d1a43a10520ee8c54f6692bbce99bbd073dfa72653
 TERMUX_PKG_AUTO_UPDATE=true
@@ -11,6 +11,8 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	cargo update
 
 	if [ "$TERMUX_ARCH" = "x86_64" ]; then
 		local libdir=target/$CARGO_TARGET_NAME/release/deps
