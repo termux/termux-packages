ee004a51771ace7103373c063a6c5eed3ff4a18a
Twaik Yont <twaikyont@gmail.com>
2023-10-08 16:47:16 +0300

    fix(main/openssh): automatic updates [no ci]

--- packages/openssh/build.sh
+++ packages/openssh/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION=9.5p1
 TERMUX_PKG_SRCURL=https://github.com/openssh/openssh-portable/archive/refs/tags/V_$(sed 's/\./_/g; s/p/_P/g' <<< $TERMUX_PKG_VERSION).tar.gz
 TERMUX_PKG_SHA256=41760d6bfea3e6e35c6acc40d24f14863cfdc92c6e56ae9401db9c658cbd93b5
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="krb5, ldns, libandroid-support, libedit, openssh-sftp-server, openssl, termux-auth, zlib"
 TERMUX_PKG_CONFLICTS="dropbear"
 # --disable-strip to prevent host "install" command to use "-s", which won't work for target binaries:
@@ -114,4 +113,12 @@ termux_step_create_debscripts() {
 	echo "done" >> postinst
 	echo "exit 0" >> postinst
 	chmod 0755 postinst
+}
+
+termux_pkg_auto_update() {
+	local latest_tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" newest-tag)"
+	[[ -z "${latest_tag}" ]] && termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
+	local version="$(echo ${latest_tag} | sed -E 's/V_([0-9]+)_([0-9]+)_P([0-9]+)/\1.\2p\3/')"
+	echo $version
+	termux_pkg_upgrade_version $version
 }
