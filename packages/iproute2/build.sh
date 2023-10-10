e181bf314f8456d2e4ae6a3bf7828548a2723619
Twaik Yont <twaikyont@gmail.com>
2023-10-10 10:23:52 +0300

    bump(main/iproute2): 6.5.0

--- packages/iproute2/build.sh
+++ packages/iproute2/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wiki.linuxfoundation.org/networking/iproute2
 TERMUX_PKG_DESCRIPTION="Utilities for controlling networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.4.0
+TERMUX_PKG_VERSION="6.5.0"
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4c51b8decbc7e4da159ffb066f590cfb93dbf9af7ff86b1647ce42b7c179a272
+TERMUX_PKG_SHA256=a70179085fa1b96d3c33b040c809b75e2b57563adc505a4ad05e2609df373463
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
 
