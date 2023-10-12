d16b38dd3628d1d2678b32cba6c0af274e26abc9
Twaik Yont <twaikyont@gmail.com>
2023-10-12 22:31:29 +0300

    bump(x11/xfce4-terminal): 1.1.0

--- x11-packages/xfce4-terminal/build.sh
+++ x11-packages/xfce4-terminal/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/terminal/start
 TERMUX_PKG_DESCRIPTION="Terminal Emulator for the XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=1.0
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-terminal/${_MAJOR_VERSION}/xfce4-terminal-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=78e55957af7c6fc1f283e90be33988661593a4da98383da1b0b54fdf6554baf4
+TERMUX_PKG_VERSION="1.1.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-terminal/${TERMUX_PKG_VERSION%.*}/xfce4-terminal-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=40823377242b9b09749f5d4a550f41d465153c235c31f34052f804d3453ad4a3
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libvte, libx11, libxfce4ui, libxfce4util, pango, xfconf"
 TERMUX_PKG_RECOMMENDS="desktop-file-utils, hicolor-icon-theme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk-doc-html=no"
