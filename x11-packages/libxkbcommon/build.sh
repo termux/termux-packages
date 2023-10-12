8d6684c17f5bae3f287c9bae654c988dc41845be
Twaik Yont <twaikyont@gmail.com>
2023-10-12 20:57:08 +0300

    Revert "Merge branch 'master' of https://github.com/termux/termux-packages"
    This reverts commit 9442cc806bdd27f88d2976768eff077db6c7324e, reversing
    changes made to 0376ea698d62f194266e87e995507580836354f1.

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://xkbcommon.org/
 TERMUX_PKG_DESCRIPTION="Keymap handling library for toolkits and window systems"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.6.0"
+TERMUX_PKG_VERSION=1.5.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4aa6c1cad7dce1238d6f48b6729f1998c7e3f0667a21100d5268c91a5830ad7b
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_VERSION_REGEXP='(?<=-).+'
+TERMUX_PKG_SHA256=053e6a6a2c3179eba20c3ada827fb8833a6663b7ffd278fdb8530c3cbf924780
 TERMUX_PKG_DEPENDS="libxcb, libxml2, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
 
