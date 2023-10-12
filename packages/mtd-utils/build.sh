aa898140765567fc313f171603f68c1b4f8e0bb5
Twaik Yont <twaikyont@gmail.com>
2023-10-12 17:38:18 +0300

    bump(main/mtd-utils): 2.1.6

--- packages/mtd-utils/build.sh
+++ packages/mtd-utils/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=http://www.linux-mtd.infradead.org/
 TERMUX_PKG_DESCRIPTION="Utilities for dealing with MTD devices"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.1.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.1.6"
 TERMUX_PKG_SRCURL=ftp://ftp.infradead.org/pub/mtd-utils/mtd-utils-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=386e27fd121699b6b729bc2e8e04dda987b31cca6b16e12fb6cc6dcf26449f46
-TERMUX_PKG_DEPENDS="liblzo, libuuid, openssl, zlib, zstd"
+TERMUX_PKG_SHA256=c1d853bc4adf83bcabd2792fc95af33bdd8643c97e8f7b3f0180af36af76f0e5
+TERMUX_PKG_DEPENDS="liblzo, libuuid, openssl, zlib, zstd, libandroid-execinfo"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-execinfo"
+}
