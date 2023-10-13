19a29fe2a3a8b4da878542ff280eee2a911cedbe
Twaik Yont <twaikyont@gmail.com>
2023-10-13 10:57:03 +0300

    chore(main/osm2pgsql): enable auto updates [no ci]

--- packages/osm2pgsql/build.sh
+++ packages/osm2pgsql/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.9.2"
 TERMUX_PKG_SRCURL=https://github.com/openstreetmap/osm2pgsql/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dc30a3ad9a27f944e4169be9a8e07ee09711901536ddc8fcf4a292bd3aec51d9
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="boost, libbz2, libc++, libexpat, liblua54, postgresql, proj, zlib, nlohmann-json"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 TERMUX_PKG_GROUPS="science"
