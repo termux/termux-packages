9514a1d68ed232ab0dbedb946bc7ace4a0c985ea
Twaik Yont <twaikyont@gmail.com>
2023-10-15 23:45:57 +0300

    bump(x11/foot): 1.16.1

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://codeberg.org/dnkl/foot
 TERMUX_PKG_DESCRIPTION="Fast, lightweight and minimalistic Wayland terminal emulator"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.15.3
+TERMUX_PKG_VERSION="1.16.1"
 TERMUX_PKG_SRCURL=https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=65006a0643ec185a0e24578365e2e7469ddeba6bd543645a15d9bbb5ec501670
+TERMUX_PKG_SHA256=43df31d457ee1f30909d12875a18845e66277def32a25d9754c96995c58d179a
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, fontconfig, freetype, libfcft, libpixman, libwayland, libxkbcommon, utf8proc"
 TERMUX_PKG_BUILD_DEPENDS="libtllist, libwayland-protocols"
 
