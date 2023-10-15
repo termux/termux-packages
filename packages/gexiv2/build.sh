a3510684b678f79c68d83bdfba6827b427ebfe76
Twaik Yont <twaikyont@gmail.com>
2023-10-15 11:04:52 +0300

    bump(main/gexiv2): 0.14.2

--- packages/gexiv2/build.sh
+++ packages/gexiv2/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/gexiv2
 TERMUX_PKG_DESCRIPTION="A GObject-based Exiv2 wrapper"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=0.14
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gexiv2/${_MAJOR_VERSION}/gexiv2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ec3ee3ec3860b9c78958a55da89cf76ae2305848e12f41945b7b52124d8f6cf9
+TERMUX_PKG_VERSION="0.14.2"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gexiv2/${TERMUX_PKG_VERSION%.*}/gexiv2-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=2a0c9cf48fbe8b3435008866ffd40b8eddb0667d2212b42396fdf688e93ce0be
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="exiv2, glib, libc++"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
 TERMUX_PKG_DISABLE_GIR=false
@@ -20,4 +20,16 @@ termux_step_pre_configure() {
 	termux_setup_gir
 
 	CPPFLAGS+=" -D_LIBCPP_ENABLE_CXX17_REMOVED_FEATURES"
+}
+
+termux_pkg_auto_update() {
+	local LATEST_VERSION="$(termux_repology_api_get_latest_version "${TERMUX_PKG_NAME}")"
+	if [[ "LATEST_VERSION" == "null" ]]; then
+		echo "INFO: Already up to date."
+		return 0
+	fi
+	if termux_pkg_is_update_needed "${TERMUX_PKG_VERSION#*:}" "${LATEST_VERSION}"; then
+		mv "$TERMUX_PKG_BUILDER_DIR/gir/${TERMUX_PKG_VERSION##*:}" "$TERMUX_PKG_BUILDER_DIR/gir/${LATEST_VERSION##*:}"
+	fi
+	termux_repology_auto_update
 }
