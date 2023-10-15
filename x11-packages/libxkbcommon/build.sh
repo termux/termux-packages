3330b4b327513991c1abe945e0acff687875260c
Jia Yuan Lo <jylo06g@gmail.com>
2023-10-15 09:19:05 +0800

    enhance(x11/libxkbcommon): enable wayland

--- x11-packages/libxkbcommon/build.sh
+++ x11-packages/libxkbcommon/build.sh
@@ -3,14 +3,30 @@ TERMUX_PKG_DESCRIPTION="Keymap handling library for toolkits and window systems"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.6.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xkbcommon/libxkbcommon/archive/xkbcommon-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4aa6c1cad7dce1238d6f48b6729f1998c7e3f0667a21100d5268c91a5830ad7b
+TERMUX_PKG_DEPENDS="libxcb, libxml2, libwayland, xkeyboard-config"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, xorg-util-macros"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP='(?<=-).+'
-TERMUX_PKG_DEPENDS="libxcb, libxml2, xkeyboard-config"
-TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Denable-docs=false
--Denable-wayland=false
+-Denable-wayland=true
 "
+
+termux_step_pre_configure() {
+	termux_setup_cmake
+	termux_setup_ninja
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/libwayland/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
+}
