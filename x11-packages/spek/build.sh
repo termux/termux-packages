b5db87f885763116f4fc483d2243f61d78d6f911
Kevin Williams <admin@utermux.dev>
2023-10-15 10:11:47 +0800

    fix(x11/spek): add libandroid-stub to depends
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/spek/build.sh
+++ x11-packages/spek/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="An acoustic spectrum analyser"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/alexkay/spek/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9053d2dec452dcde421daa0f5f59a9dee47927540f41d9c0c66800cb6dbf6996
-TERMUX_PKG_DEPENDS="ffmpeg, libc++, wxwidgets"
+TERMUX_PKG_DEPENDS="ffmpeg, libc++, wxwidgets, libandroid-stub"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_path_WX_CONFIG_PATH=$TERMUX_PREFIX/bin/wx-config"
 
 termux_step_pre_configure() {
