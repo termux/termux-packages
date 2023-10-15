c44c13854d59d7c7a8b64717613885b49cd7b23c
Launch Lee <80872691+LaunchLee@users.noreply.github.com>
2023-10-15 11:56:02 +0800

    mympd: Bump to 12.1.0
    Add dependency: liblua54

--- packages/mympd/build.sh
+++ packages/mympd/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://jcorporation.github.io/myMPD/
 TERMUX_PKG_DESCRIPTION="A standalone and lightweight web-based MPD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=12.0.4
+TERMUX_PKG_VERSION=12.1.0
 TERMUX_PKG_SRCURL=https://github.com/jcorporation/myMPD/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=97bae393ea9d447d2b69d5231e37a91cd2549c7ec4ee87bcae4dcfa64becdf3c
-TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre2, resolv-conf"
+TERMUX_PKG_SHA256=1937a021c32b635402a0ac89d9d4957fcf936297c48642a812d4bac89d3947f0
+TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre2, resolv-conf, liblua54"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DMATH_LIB=m
 "
