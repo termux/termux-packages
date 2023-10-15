f54cccf76bacaa64cc037cdb763b8a5f1f3f5973
Twaik Yont <twaikyont@gmail.com>
2023-10-15 11:05:12 +0300

    bump(main/gstreamer): 1.22.6

--- packages/gstreamer/build.sh
+++ packages/gstreamer/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Open source multimedia framework"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.22.4
+TERMUX_PKG_VERSION="1.22.6"
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=11cb0498bc16b93d8b99d22f75f829b8d0abfd8254840b2120618db5532dc655
+TERMUX_PKG_SHA256=f500e6cfddff55908f937711fc26a0840de28a1e9ec49621c0b6f1adbd8f818e
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="gstreamer-dev"
@@ -22,4 +23,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+}
+
+termux_pkg_auto_update() {
+	local LATEST_VERSION="$(termux_repology_api_get_latest_version "${TERMUX_PKG_NAME}")"
+	if [[ "LATEST_VERSION" == "null" ]]; then
+		echo "INFO: Already up to date."
+		return 0
+	fi
+	if termux_pkg_is_update_needed "${TERMUX_PKG_VERSION#*:}" "${LATEST_VERSION}"; then
+		mv "$TERMUX_PKG_BUILDER_DIR/gir/${TERMUX_PKG_VERSION##*:}" "$TERMUX_PKG_BUILDER_DIR/gir/${LATEST_VERSION##*:}"
+	fi
+	termux_repology_auto_update
 }
