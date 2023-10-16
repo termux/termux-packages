f28410a5b581cc701d808341e92362eb98b3370c
Launch Lee <80872691+LaunchLee@users.noreply.github.com>
2023-10-16 18:31:15 +0800

    enhance(main/mympd): Enable Lua

--- packages/mympd/build.sh
+++ packages/mympd/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="A standalone and lightweight web-based MPD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="12.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jcorporation/myMPD/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1937a021c32b635402a0ac89d9d4957fcf936297c48642a812d4bac89d3947f0
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre2, resolv-conf"
+TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre2, resolv-conf, liblua54"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DMATH_LIB=m
 "
