4828528c969ed2cfc2efd2f34c420530f4b7f345
Twaik Yont <twaikyont@gmail.com>
2023-10-16 20:25:35 +0300

    bump(main/remind): 04.02.07

--- packages/remind/build.sh
+++ packages/remind/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1:04.02.07"
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/remind-${TERMUX_PKG_VERSION:2}.tar.xz
 TERMUX_PKG_SHA256=12efa37b0dedec098a8ed7f574ac4d2e2fb0234510b71f5153fa50ba13d1a026
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="bin/tkremind share/man/man1/tkremind.1 bin/cm2rem.tcl share/man/man1/cm2rem.1"
