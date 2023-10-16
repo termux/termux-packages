96bab0b3bab91350b8b8a26e23f71ee29bf69384
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2023-10-16 01:51:30 +0000

    rizin: add package
    Co-developed-by: Joshua Kahn @TomJo2000

---
+++ packages/rizin/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://rizin.re
+TERMUX_PKG_DESCRIPTION="UNIX-like reverse engineering framework and command-line toolset."
+TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.6.2
+TERMUX_PKG_SRCURL=https://github.com/rizinorg/rizin/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SUGGESTS="python, apk-tools, apktool, apksigner"
+TERMUX_PKG_SHA256=ab4bd4c767940b28d683f664f27180665f98bfab2aa7972ba4c331e8bed30da8
