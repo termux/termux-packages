1d07d87246a40a3e423eaf14e118533e4ca57054
Twaik Yont <twaikyont@gmail.com>
2023-10-17 23:55:44 +0300

    chore(x11/lyx): enable automatic updates [no ci]

--- x11-packages/lyx/build.sh
+++ x11-packages/lyx/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://www.lyx.org
 TERMUX_PKG_DESCRIPTION="WYSIWYM (What You See Is What You Mean) Document Processor"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-_VERSION=2.3.7-1
-TERMUX_PKG_VERSION=${_VERSION/-/p}
-TERMUX_PKG_SRCURL="https://ftp.lip6.fr/pub/lyx/stable/$(echo $_VERSION | cut -d . -f 1-2).x/lyx-${_VERSION}.tar.xz"
+TERMUX_PKG_VERSION="2.3.7p1"
+TERMUX_PKG_SRCURL="https://ftp.lip6.fr/pub/lyx/stable/${TERMUX_PKG_VERSION%.*}.x/lyx-${TERMUX_PKG_VERSION/p/-}.tar.xz"
 TERMUX_PKG_SHA256=39be8864fb86b34e88310e70fb80e5e9e296601f0856cf161aa094171718d8ed
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/\./p/3; s/-/p/'
 TERMUX_PKG_DEPENDS="ghostscript, hunspell, imagemagick, libandroid-execinfo, libc++, libxcb, lyx-data, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, texlive-bin, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, qt5-qtbase-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -21,5 +22,5 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-execinfo"
 
 	# This is to allow the build script find the `moc` on cross-build host
-	export PATH+=":${TERMUX_PREFIX}/opt/qt/cross/bin"
+	export PATH="${TERMUX_PREFIX}/opt/qt/cross/bin:${PATH}"
 }
