b573c1f6880a871dbbc325b87bdde91ae0d97342
Twaik Yont <twaikyont@gmail.com>
2023-10-17 14:44:52 +0300

    bump(x11/qt5ct): 1.8

--- x11-packages/qt5ct/build.sh
+++ x11-packages/qt5ct/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/qt5ct
 TERMUX_PKG_DESCRIPTION="Qt5 Configuration Tool"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION=1.7
+TERMUX_PKG_VERSION="1.8"
 TERMUX_PKG_SRCURL="https://downloads.sf.net/qt5ct/qt5ct-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=ed58546a6e4c50dfed1b9ff6a148d4a468fe9b4d7b5b2727b96501de2955b8d7
+TERMUX_PKG_SHA256=23b74054415ea4124328772ef9a6f95083a9b86569e128034a3ff75dfad808e9
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -15,4 +16,3 @@ termux_step_configure () {
         -spec "${TERMUX_PREFIX}/lib/qt/mkspecs/termux-cross" \
         PREFIX="${TERMUX_PREFIX}"
 }
-
