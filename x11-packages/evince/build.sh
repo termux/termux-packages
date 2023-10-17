71ae4e763d772b3f825d834b4b25c24ef84a339b
Twaik Yont <twaikyont@gmail.com>
2023-10-18 00:16:33 +0300

    upgpkg(x11/evince): 45.0

--- x11-packages/evince/build.sh
+++ x11-packages/evince/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Evince
 TERMUX_PKG_DESCRIPTION="document viewer for multiple document formats"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=44.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="45.0"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/evince/${TERMUX_PKG_VERSION%.*}/evince-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3b8ba1581a47a6e9f57f6b6aa08f0fb67549c60121aa24e31140e93947b83622
+TERMUX_PKG_SHA256=d18647d4275cbddf0d32817b1d04e307342a85be914ec4dad2d8082aaf8aa4a8
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, djvulibre, gdk-pixbuf, glib, gnome-desktop3, gst-plugins-base, gst-plugins-good, gstreamer, gtk3, libarchive, libcairo, libgxps, libhandy, libsecret, libspectre, libtiff, libxml2, pango, poppler, poppler-data, texlive-bin"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_RECOMMENDS="evince-help"
@@ -20,4 +20,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+}
+
+termux_pkg_auto_update() {
+	local LATEST_VERSION="$(termux_repology_api_get_latest_version "${TERMUX_PKG_NAME}")"
+	if [[ "$LATEST_VERSION" == "null" ]]; then
+		echo "INFO: Already up to date."
+		return 0
+	fi
+	if termux_pkg_is_update_needed "${TERMUX_PKG_VERSION#*:}" "${LATEST_VERSION}"; then
+		mv "$TERMUX_PKG_BUILDER_DIR/gir/${TERMUX_PKG_VERSION##*:}" "$TERMUX_PKG_BUILDER_DIR/gir/${LATEST_VERSION##*:}"
+	fi
+	termux_repology_auto_update
 }
