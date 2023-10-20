76cd65e13efe955dd83675bc2bcfb203c55ed861
Twaik Yont <twaikyont@gmail.com>
2023-10-20 14:08:26 +0300

    upgpkg(main/borgbackup): 1.2.6

--- packages/borgbackup/build.sh
+++ packages/borgbackup/build.sh
@@ -2,12 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://www.borgbackup.org/
 TERMUX_PKG_DESCRIPTION="Deduplicating and compressing backup program"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.17
-TERMUX_PKG_REVISION=10
+TERMUX_PKG_VERSION="1.2.6"
 TERMUX_PKG_SRCURL=https://github.com/borgbackup/borg/releases/download/${TERMUX_PKG_VERSION}/borgbackup-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7ab924fc017b24929bedceba0dcce16d56f9868bf9b5050d2aae2eb080671674
-# Cannot be updated to 1.2.0 (or newer) as it requires external python package
-#TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256=b7a6f8f086039eeec79070b914f3c651ed7f3612c965374af910d277c7a2139d
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libacl, liblz4, openssl, python, xxhash, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, wheel"
+TERMUX_PKG_PYTHON_TARGET_DEPS="msgpack==1.0.5"
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./postinst
+	#!$TERMUX_PREFIX/bin/sh
+	echo "Installing dependencies through pip..."
+	pip3 install $TERMUX_PKG_PYTHON_TARGET_DEPS
+	EOF
+}
