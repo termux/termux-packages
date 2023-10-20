7a4ccef9611245e52546ba833ab46ca1d7c96d50
Twaik Yont <twaikyont@gmail.com>
2023-10-20 16:20:45 +0300

    bump(main/libopenmpt): 0.7.3

--- packages/libopenmpt/build.sh
+++ packages/libopenmpt/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://lib.openmpt.org/libopenmpt/
 TERMUX_PKG_DESCRIPTION="OpenMPT based module player library and libopenmpt based command-line player"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.6.10
+TERMUX_PKG_VERSION="0.7.3"
 TERMUX_PKG_SRCURL=https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-${TERMUX_PKG_VERSION}+release.autotools.tar.gz
-TERMUX_PKG_SHA256=c25be8dc0dac23cec025487e58f195e4d14f7d94aeddd5fe46b21c04f1ce2774
+TERMUX_PKG_SHA256=2cf8369b7916b09264f3f14b9fb6cef35a6e9bee0328dec4f49d98211ccfd722
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libflac, libogg, libsndfile, libvorbis, mpg123, pulseaudio, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -14,4 +15,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	CXXFLAGS+=" -std=c++17"
 }
