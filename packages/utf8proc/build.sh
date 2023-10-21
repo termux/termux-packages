7016afdd9140008e161ef092dfeaadc347b2715b
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-21 23:54:42 +0800

    utf8proc: bump to 2.9.0

--- packages/utf8proc/build.sh
+++ packages/utf8proc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/JuliaLang/utf8proc
 TERMUX_PKG_DESCRIPTION="Library for processing UTF-8 Unicode data"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.8.0"
+TERMUX_PKG_VERSION="2.9.0"
 TERMUX_PKG_SRCURL=https://github.com/JuliaLang/utf8proc/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a0a60a79fe6f6d54e7d411facbfcc867a6e198608f2cd992490e46f04b1bcecc
+TERMUX_PKG_SHA256=18c1626e9fc5a2e192311e36b3010bfc698078f692888940f1fa150547abb0c1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="utf8proc-dev"
 TERMUX_PKG_REPLACES="utf8proc-dev"
@@ -13,7 +13,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=2
+	local _SOVERSION=3
 
 	local v=$(sed -En 's/^MAJOR=([0-9]+).*/\1/p' Makefile)
 	if [ "${v}" != "${_SOVERSION}" ]; then
