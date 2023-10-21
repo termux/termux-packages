5bcde1d628cabea8d96022c48e5c324b6b6171f8
Jia Yuan Lo <jylo06g@gmail.com>
2023-10-21 20:44:17 +0800

    addpkg(main): glib-cross

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -3,32 +3,58 @@ TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.78.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION%.*}/glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=44eaab8b720877ce303c5540b657b126f12dc94972d9880b52959f43fb537b30
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, libffi, libiconv, pcre2, resolv-conf, zlib"
 TERMUX_PKG_BREAKS="glib-dev"
 TERMUX_PKG_REPLACES="glib-dev"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Druntime_dir=$TERMUX_PREFIX/var/run
 -Dlibmount=disabled
 "
-
 TERMUX_PKG_RM_AFTER_INSTALL="
+bin/glib-gettextize
+bin/gtester-report
 lib/locale
 share/gdb/auto-load
 share/glib-2.0/gdb
 share/glib-2.0/gettext
 share/gtk-doc
-bin/gtester-report
-bin/glib-gettextize
 "
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
+-Ddefault_library=static
+-Dlibmount=disabled
+-Dtests=false
+--prefix ${TERMUX_PREFIX}/opt/${TERMUX_PKG_NAME}/cross
+"
+
+termux_step_host_build() {
+	# XXX: termux_setup_meson is not expected to be called in host build
+	AR=;CC=;CFLAGS=;CPPFLAGS=;CXX=;CXXFLAGS=;LD=;LDFLAGS=;PKG_CONFIG=;STRIP=
+	termux_setup_meson
+	unset AR CC CFLAGS CPPFLAGS CXX CXXFLAGS LD LDFLAGS PKG_CONFIG STRIP
+
+	${TERMUX_MESON} ${TERMUX_PKG_SRCDIR} . \
+		${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
+	ninja -j "${TERMUX_MAKE_PROCESSES}" install
+}
 
 termux_step_pre_configure() {
 	# glib checks for __BIONIC__ instead of __ANDROID__:
 	CFLAGS+=" -D__BIONIC__=1"
+}
+
+termux_step_post_make_install() {
+	local pc_files=$(ls "${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig")
+	for pc in ${pc_files}; do
+		echo "INFO: Patching cross pkgconfig ${pc}"
+		sed "s|\${bindir}|${TERMUX_PREFIX}/opt/glib/cross/bin|g" \
+			"${TERMUX_PREFIX}/lib/pkgconfig/${pc}" \
+			> "${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig/${pc}"
+	done
 }
 
 termux_step_create_debscripts() {
