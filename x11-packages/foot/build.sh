a9c72e66b72e6f6d45fb9ca7b044699caf96c706
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-21 23:55:38 +0800

    *: revbump to rebuild with utf8proc

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast, lightweight and minimalistic Wayland terminal emul
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.16.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0e02af376e5f4a96eeb90470b7ad2e79a1d660db2a7d1aa772be43c7db00e475
 TERMUX_PKG_AUTO_UPDATE=true
