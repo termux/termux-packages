0a56a6ac22b0a5874497dfcbb6569e4f99e37e7d
Twaik Yont <twaikyont@gmail.com>
2023-10-22 12:41:44 +0300

    bump(main/texinfo): 7.1

--- packages/texinfo/build.sh
+++ packages/texinfo/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/texinfo/
 TERMUX_PKG_DESCRIPTION="Documentation system for on-line information and printed output"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.0.3
+TERMUX_PKG_VERSION="7.1"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/texinfo/texinfo-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=74b420d09d7f528e84f97aa330f0dd69a98a6053e7a4e01767eed115038807bf
+TERMUX_PKG_SHA256=deeec9f19f159e046fdf8ad22231981806dac332cc372f1c763504ad82b30953
+TERMUX_PKG_AUTO_UPDATE=true
 # gawk is used by texindex:
 TERMUX_PKG_DEPENDS="libiconv, ncurses, perl, gawk"
 TERMUX_PKG_RECOMMENDS="update-info-dir"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-perl-xs"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="texinfo_cv_sys_iconv_converts_euc_cn=no --disable-perl-xs"
 TERMUX_PKG_GROUPS="base-devel"
