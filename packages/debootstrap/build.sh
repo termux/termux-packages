2c07d27ef04541299a31c8b9d1947e6a05616f16
Twaik Yont <twaikyont@gmail.com>
2023-10-22 15:59:30 +0300

    bump(main/debootstrap): 1.0.133

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -3,19 +3,28 @@ TERMUX_PKG_DESCRIPTION="Bootstrap a basic Debian system"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="debian/copyright"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.132"
+TERMUX_PKG_VERSION="1.0.133"
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/d/debootstrap/debootstrap_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d963a465314ac0e8fd5392573def042e6663e8edf3d08ace4bbd0d27ee8431f7
+TERMUX_PKG_SHA256=11931eec3aafaff1cd0b840f450395fe3f156cef96a5458e7c91b653dfb0d6d6
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="binutils | binutils-is-llvm, perl, proot, sed, wget"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
-termux_step_post_make_install() {
-	mkdir -p ${TERMUX_PREFIX}/share/man/man8/
-	install ${TERMUX_PKG_SRCDIR}/debootstrap.8 ${TERMUX_PREFIX}/share/man/man8/
+termux_step_make() {
+    :
 }
 
-termux_step_post_massage() {
-	rm -f ./share/debootstrap/scripts/*.orig
+termux_step_make_install() {
+	find ${TERMUX_PKG_SRCDIR}/.. -name "*.orig" -delete
+
+	local VERSION=$(sed 's/.*(\(.*\)).*/\1/; q' debian/changelog)
+	mkdir -p ${TERMUX_PREFIX}/share/debootstrap/scripts
+	cp -a scripts/* ${TERMUX_PREFIX}/share/debootstrap/scripts/
+	install -m 0644 functions ${TERMUX_PREFIX}/share/debootstrap/
+	sed "s/@VERSION@/${VERSION}/g" debootstrap > ${TERMUX_PREFIX}/bin/debootstrap
+	chmod 0755 ${TERMUX_PREFIX}/bin/debootstrap
+
+	mkdir -p ${TERMUX_PREFIX}/share/man/man8/
+	install ${TERMUX_PKG_SRCDIR}/debootstrap.8 ${TERMUX_PREFIX}/share/man/man8/
 }
