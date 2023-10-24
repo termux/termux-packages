5203c031480bce25976b3aa276358a4f5feb5d64
Twaik Yont <twaikyont@gmail.com>
2023-10-24 19:51:27 +0300

    chore(main/sdl2-mixer): enable automatic updates [no ci]

--- x11-packages/sdl2-mixer/build.sh
+++ x11-packages/sdl2-mixer/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.6.3
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_mixer/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_mixer-${TERMUX_PKG_VERSION}.tar.gz 
 TERMUX_PKG_SHA256=7a6ba86a478648ce617e3a5e9277181bc67f7ce9876605eea6affd4a0d6eea8f
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/.*-//'
 TERMUX_PKG_DEPENDS="fluidsynth, libflac, libmodplug, libvorbis, mpg123, opusfile, sdl2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-music-mod-modplug-shared
