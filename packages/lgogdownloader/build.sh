6b61fb1c6ec7832f6dccacd4c7c92d0efbc7ba06
Twaik Yont <twaikyont@gmail.com>
2023-10-24 19:16:29 +0300

    bump(main/lgogdownloader): 3.12

--- packages/lgogdownloader/build.sh
+++ packages/lgogdownloader/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://sites.google.com/site/gogdownloader/
 TERMUX_PKG_DESCRIPTION="Open source downloader to GOG.com for Linux users using the same API as the official GOGDownloader"
 TERMUX_PKG_LICENSE="WTFPL"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.11
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="3.12"
 TERMUX_PKG_SRCURL=https://github.com/Sude-/lgogdownloader/releases/download/v${TERMUX_PKG_VERSION}/lgogdownloader-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d8d015cce6e002876305517367dc006c332e4d492263173b58bfe5a94b057b09
+TERMUX_PKG_SHA256=bf3a16c1b2ff09152f9ac52ea9b52dfc0afae799ed1b370913149cec87154529
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="boost, jsoncpp, libc++, libcurl, libhtmlcxx, libtinyxml2, rhash"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DHELP2MAN=OFF"
