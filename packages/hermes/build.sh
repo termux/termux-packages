1967aa0cc3fe4509147016147c120c103e31784d
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2023-10-24 03:59:53 +0000

    alt. way

--- packages/hermes/build.sh
+++ packages/hermes/build.sh
@@ -9,5 +9,11 @@ TERMUX_PKG_DEPENDS="libicu, readline"
 TERMUX_PKG_SRCURL=https://github.com/facebook/hermes/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=bd8fd158381813483123eb1ab553ed08db68e4949f314c99ee8fa79fa8f3e7ed
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DIMPORT_HERMESC:PATH=/home/builder/.termux-build/hermes/build/ImportHermesc.cmake"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DIMPORT_HERMESC:PATH=$TERMUX_PKG_SRCDIR/../build/ImportHermesc.cmake"
 
+termux_step_make(){
+    cmake -S $TERMUX_PKG_SRCDIR -B ./build_host_hermesc
+    cmake --build ./build_host_hermesc --target hermesc
+    cmake -S $TERMUX_PKG_SRCDIR -B build -G Ninja
+    cmake --build ./build -DIMPORT_HERMESC:PATH=$TERMUX_PKG_SRCDIR/build_host_hermesc/ImportHermesc.cmake
+}
