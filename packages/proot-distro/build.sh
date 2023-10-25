3f45014c305acb48cb8cc089454218294f68e44b
Leonid Pliushch <sylirre@termux.dev>
2023-10-25 21:43:31 +0300

    upgpkg(main/proot-distro): 4.0.1
    v4.0.0 didn't include updated docs + I didn't set new sha256 in build.sh

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.0.0
+TERMUX_PKG_VERSION=4.0.1
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=96c5a7979c25d81411e538e46ab08701c9653f18777f0650eab02e374a4b8ff3
+TERMUX_PKG_SHA256=bbb9118d20be831c6ee02c9d9da65c9ac8a7ca9075c4d9fe0972c11e5c23dc49
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, termux-tools, xz-utils"
 TERMUX_PKG_SUGGESTS="termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
