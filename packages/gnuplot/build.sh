14040aaccef27fb212017086d0e1b097d5ad86c0
Twaik Yont <twaikyont@gmail.com>
2023-10-25 14:00:37 +0300

    bump(main/gnuplot): 5.4.10

--- packages/gnuplot/build.sh
+++ packages/gnuplot/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Command-line driven graphing utility"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="Copyright"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.4.8
+TERMUX_PKG_VERSION="5.4.10"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gnuplot/gnuplot/${TERMUX_PKG_VERSION}/gnuplot-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=931279c7caad1aff7d46cb4766f1ff41c26d9be9daf0bcf0c79deeee3d91f5cf
+TERMUX_PKG_SHA256=975d8c1cc2c41c7cedc4e323aff035d977feb9a97f0296dd2a8a66d197a5b27c
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libcairo, libgd, libiconv, libx11, pango, readline"
 TERMUX_PKG_BREAKS="gnuplot-x"
 TERMUX_PKG_REPLACES="gnuplot-x"
@@ -13,6 +14,7 @@ TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
 --disable-wxwidgets
 --without-lua
+--without-qt
 "
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-wxwidgets
@@ -20,6 +22,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-lua
 --with-bitmap-terminals
 --without-latex
+--without-qt
 ac_cv_search_iconv_open=-liconv
 "
 
