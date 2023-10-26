2105d53a02b2019f61f0b5d06fb0126d654c96fb
ElectricPulse <40593297+ElectricPulse@users.noreply.github.com>
2023-10-26 11:13:36 +0200

    Update build.sh
    Flang doesn't support some architectures.

--- packages/liblapack/build.sh
+++ packages/liblapack/build.sh
@@ -7,4 +7,6 @@ TERMUX_PKG_MAINTAINER="Adam Jozef Labus <adam.labuznik@gmail.com>"
 TERMUX_PKG_VERSION=3.11.0
 TERMUX_PKG_SRCURL=https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4b9ba79bfd4921ca820e83979db76ab3363155709444a787979e81c22285ffa9
-TERMUX_PKG_DEPENDS="flang, libopenblas"
+TERMUX_PKG_DEPENDS="flang, libopenblas, clang"
+#Because of flang
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
