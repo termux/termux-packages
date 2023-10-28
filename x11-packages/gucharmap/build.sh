13bb605186c47a4d388eae73c67f0461cbfeb9d8
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-10-29 02:18:22 +0800

    gucharmap: bump to 15.1.2

--- x11-packages/gucharmap/build.sh
+++ x11-packages/gucharmap/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Gucharmap
 TERMUX_PKG_DESCRIPTION="GTK+ Unicode Character Map"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="15.1.1"
+TERMUX_PKG_VERSION="15.1.2"
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gucharmap/-/archive/${TERMUX_PKG_VERSION}/gucharmap-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=f05b21586e6a762fb01561892b48f917230f29a115aa7f8405396843feccc9de
+TERMUX_PKG_SHA256=f8580cb191d0a430513d0384b1f619a5eb8ad40dbd609d0c0f8370afa756c1fe
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, glib, gtk3, libcairo, pango, pcre2, unicode-data"
-TERMUX_PKG_BUILD_DEPENDS="freetype, g-ir-scanner, valac"
+TERMUX_PKG_BUILD_DEPENDS="freetype, g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ducd_path=$TERMUX_PREFIX/share/unicode-data
@@ -18,6 +18,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
 
 termux_pkg_auto_update() {
