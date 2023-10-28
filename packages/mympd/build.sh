641a158117c3b70f3b800ac16380c4516c3183bf
Launch Lee <80872691+LaunchLee@users.noreply.github.com>
2023-10-28 11:23:56 +0800

    chore,enhance(main/mympd): simplify and enable lua (#18287)

--- packages/mympd/build.sh
+++ packages/mympd/build.sh
@@ -3,11 +3,13 @@ TERMUX_PKG_DESCRIPTION="A standalone and lightweight web-based MPD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="12.1.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jcorporation/myMPD/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f730a016ca63617becec75a8a85ab07fa4c6681d62eda374ca46f3aa25664644
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libflac, libid3tag, openssl, pcre2, resolv-conf"
+TERMUX_PKG_DEPENDS="libflac, libid3tag, liblua54, openssl, pcre2, resolv-conf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DMATH_LIB=m
+-DMYMPD_STARTUP_SCRIPT=OFF
 "
 TERMUX_CMAKE_BUILD="Unix Makefiles"
