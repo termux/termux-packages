8dd857fe16eba5d637280750d34129165c1b04b0
Twaik Yont <twaikyont@gmail.com>
2023-10-30 19:37:30 +0200

    upgpkg(main/task-spooler): 2.0.0

--- packages/task-spooler/build.sh
+++ packages/task-spooler/build.sh
@@ -2,12 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://vicerveza.homeunix.net/~viric/soft/ts/
 TERMUX_PKG_DESCRIPTION="Task spooler is a Unix batch system where the tasks spooled run one after the other"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.2
-TERMUX_PKG_SRCURL=https://vicerveza.homeunix.net/~viric/soft/ts/ts-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f73452aed80e2f9a7764883e9353aa7f40e65d3c199ad1f3be60fd58b58eafec
+TERMUX_PKG_VERSION="2.0.0"
+TERMUX_PKG_SRCURL=https://github.com/justanhduc/task-spooler/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ffffa86f95071e837af619e23fb4a037432b0b079d872d58dc530883d1d33557
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_CONFLICTS="moreutils"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DTASK_SPOOLER_COMPILE_CUDA=OFF
+"
 
-termux_step_post_make_install() {
-	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 \
-		$TERMUX_PKG_SRCDIR/ts.1
+termux_step_pre_configure() {
+	# if $TERMUX_ON_DEVICE_BUILD; then
+	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+	    sed -i '/add_executable(makeman man.c)/d' ${TERMUX_PKG_SRCDIR}/CMakeLists.txt
+	    gcc -o ${TERMUX_PKG_BUILDDIR}/makeman ${TERMUX_PKG_SRCDIR}/man.c
+	fi
 }
