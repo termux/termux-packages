42c0d5cc4daf402e109df88189a71c89fd581e60
Twaik Yont <twaikyont@gmail.com>
2023-11-01 15:17:56 +0200

    bump(main/libwolfssl): 5.6.4, rebuild(main/vde2): libwolfssl bump

--- packages/libwolfssl/build.sh
+++ packages/libwolfssl/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.wolfssl.com/
 TERMUX_PKG_DESCRIPTION="A small, fast, portable implementation of TLS/SSL for embedded devices to the cloud"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.6.3
+TERMUX_PKG_VERSION="5.6.4"
 TERMUX_PKG_SRCURL=https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${TERMUX_PKG_VERSION}-stable.tar.gz
-TERMUX_PKG_SHA256=2e74a397fa797c2902d7467d500de904907666afb4ff80f6464f6efd5afb114a
+TERMUX_PKG_SHA256=031691906794ff45e1e792561cf31759f5d29ac74936bc8dffb8b14f16d820b4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=35
+	local _SOVERSION=41
 
 	local a
 	for a in LIBTOOL_CURRENT LIBTOOL_AGE; do
