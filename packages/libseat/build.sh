2e60b5e9b4f6f9f0f584899adb9a35b435370367
Uchiha Kakashi <45286352+licy183@users.noreply.github.com>
2023-11-05 14:38:15 +0800

    libseat: change `newest-tag`
    Closes #18410
    
    [skip ci]

--- packages/libseat/build.sh
+++ packages/libseat/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION="0.8.0"
 TERMUX_PKG_SRCURL=https://github.com/kennylevinsen/seatd/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=a562a44ee33ccb20954a1c1ec9a90ecb2db7a07ad6b18d0ac904328efbcf65a0
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddefaultpath=$TERMUX_PREFIX/var/run/seatd.sock
 "
