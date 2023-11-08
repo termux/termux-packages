4e3eebc5410298cc873b7a59d3b8536ff173a03e
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-08 22:18:16 +0800

    lxqt-config: bump to 1.4.0

--- x11-packages/lxqt-config/build.sh
+++ x11-packages/lxqt-config/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="Tools to configure LXQt and the underlying operating system"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION="1.3.0"
+TERMUX_PKG_VERSION="1.4.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-config/releases/download/${TERMUX_PKG_VERSION}/lxqt-config-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=929f60b58cf8dbf84f0f778a59c9794bfcc74abb8ea68177e28deb666eae698f
-TERMUX_PKG_DEPENDS="libc++, liblxqt, libqtxdg, libxcb, libxcursor, libxfixes, qt5-qtbase, qt5-qtx11extras, shared-mime-info, zlib"
+TERMUX_PKG_SHA256=e60d5c3f00588fb373b48fa0c65f7a164500738467359472409a29b9db11c84b
+TERMUX_PKG_DEPENDS="libc++, liblxqt, libqtxdg, libxcb, libxcursor, libxfixes, lxqt-menu-data, qt5-qtbase, qt5-qtx11extras, shared-mime-info, zlib"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 # libinput is required to switch on input configuration
 # libkscreen is required to switch on monitor configuration, which in turn requires wayland
@@ -17,4 +17,3 @@ termux_step_pre_configure() {
     LDFLAGS+=" -Wl,-rpath=${TERMUX_PREFIX}/lib/lxqt-config"
     export LDFLAGS
 }
-
