d80c4dd85c03f2ff9323ac7b10332c0a9f853689
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-08 22:18:59 +0800

    lxqt-qtplugin: bump to 1.4.0

--- x11-packages/lxqt-qtplugin/build.sh
+++ x11-packages/lxqt-qtplugin/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="LXQt platform integration plugin for Qt 5"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION="1.3.0"
+TERMUX_PKG_VERSION="1.4.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-qtplugin/releases/download/${TERMUX_PKG_VERSION}/lxqt-qtplugin-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=1be21cae812d6e061ce026b4456e4a32def8e4618c7b134ca773a7b15852bd41
+TERMUX_PKG_SHA256=2afd9af1de8cc07b276c160e9831bde6fbc9b65a367df8227a3aa7df5e856eae
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, kwindowsystem, libqtxdg, libfm-qt, libexif, libdbusmenu-qt"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
-
