df6e112e48457e9ad4e9f39128642486304faf50
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-08 22:15:05 +0800

    lxqt-panel: bump to 1.4.0

--- x11-packages/lxqt-panel/build.sh
+++ x11-packages/lxqt-panel/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="The LXQt desktop panel"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION="1.3.0"
+TERMUX_PKG_VERSION="1.4.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-panel/releases/download/${TERMUX_PKG_VERSION}/lxqt-panel-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=431b399445c8b01b4989cb9521739e88d99236eaf43c9dcd29a99696038fd8d2
-TERMUX_PKG_DEPENDS="kwindowsystem, libc++, libdbusmenu-qt, liblxqt, libsysstat, libxcb, libxkbcommon, libxtst, libxtst, lxmenu-data, lxqt-globalkeys, pulseaudio, qt5-qtbase, qt5-qtx11extras, xcb-util, xcb-util-image"
+TERMUX_PKG_SHA256=0e660c0397c96a28f0fcf316b20c72d203c85793a884e1487b3b14e3790defc9
+TERMUX_PKG_DEPENDS="kwindowsystem, libc++, libdbusmenu-qt, liblxqt, libsysstat, libxcb, libxkbcommon, libxtst, libxtst, lxmenu-data, lxqt-globalkeys, lxqt-menu-data, pulseaudio, qt5-qtbase, qt5-qtx11extras, xcb-util, xcb-util-image"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 # TODO
 # CPULOAD and NETWORKMONITOR require libstatgrab
