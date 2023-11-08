e7ac00469daa4e7c1093832146c49c153433370c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-08 22:15:43 +0800

    lxqt-archiver: bump to 0.9.0

--- x11-packages/lxqt-archiver/build.sh
+++ x11-packages/lxqt-archiver/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="A simple & lightweight Qt file archiver"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
-TERMUX_PKG_VERSION="0.8.0"
+TERMUX_PKG_VERSION="0.9.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-archiver/releases/download/${TERMUX_PKG_VERSION}/lxqt-archiver-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=29d3195026185f88107b849bfd546d4534042621b3c28d3e9a23106e5097ce24
+TERMUX_PKG_SHA256=be2d49669220700d71b5f4a62d340eab36e29b4a8327c1f551ae95eede793158
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtx11extras, libfm-qt, glib, json-glib"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
-
