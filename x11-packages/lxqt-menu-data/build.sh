22d555ba423ba64ef75e346423fcc073e82e8018
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-08 22:04:46 +0800

    new package: lxqt-menu-data

---
+++ x11-packages/lxqt-menu-data/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/lxqt/lxqt-menu-data
+TERMUX_PKG_DESCRIPTION="Menu files for LXQt Panel, Configuration Center and PCManFM-Qt/libfm-qt"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-menu-data/releases/download/$TERMUX_PKG_VERSION/lxqt-menu-data-$TERMUX_PKG_VERSION.tar.xz"
+TERMUX_PKG_SHA256=820549ebd50a7437a9af5f229d0a5611944473e1f74bc7c8a2829a7606a2f99e
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_AUTO_UPDATE=true
