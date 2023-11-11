40e9afd45efaed1f216d1a216b4769766191be12
Kevin Williams <admin@utermux.dev>
2023-11-11 11:12:31 +0800

    upgpkg(main/fakeroot): 1.32.2
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/fakeroot/build.sh
+++ packages/fakeroot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://packages.qa.debian.org/f/fakeroot.html
 TERMUX_PKG_DESCRIPTION="Tool for simulating superuser privileges (with tcp ipc)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.32.1"
+TERMUX_PKG_VERSION="1.32.2"
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_${TERMUX_PKG_VERSION}.orig.tar.gz
-TERMUX_PKG_SHA256=c072b0f65bafc4cc5b6112f7c61185f5170ce4cb0c410d1681c1af4a183e94e6
+TERMUX_PKG_SHA256=f0f72b504f288eea5b043cd5fe37585bc163f5acaacd386e1976b1055686116d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-ipc=tcp"
 TERMUX_PKG_BUILD_DEPENDS="libcap"
