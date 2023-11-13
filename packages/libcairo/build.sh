58db2e9ff9ef9b608f6e56744e4623eb6455b4ac
Twaik Yont <twaikyont@gmail.com>
2023-11-13 11:08:31 +0200

    bump(main/libcairo): 1.18.0

--- packages/libcairo/build.sh
+++ packages/libcairo/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://cairographics.org
 TERMUX_PKG_DESCRIPTION="Cairo 2D vector graphics library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17.8
+TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/cairo/cairo/-/archive/${TERMUX_PKG_VERSION}/cairo-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ead4724423eb969f98b456fe1e3ee1e1741fe1c8dfb1a41ca12afa81a6c1665f
-TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libandroid-shmem, liblzo, libpixman, libpng, libx11, libxcb, libxext, libxrender, zlib"
+TERMUX_PKG_SHA256=abf8fba4d510086a492783c3e0828e90b32734738fd80906595617d229d02bab
+TERMUX_PKG_DEPENDS="fontconfig, freetype, glib, libandroid-shmem, libandroid-execinfo, liblzo, libpixman, libpng, libx11, libxcb, libxext, libxrender, zlib"
 TERMUX_PKG_BREAKS="libcairo-dev, libcairo-gobject"
 TERMUX_PKG_REPLACES="libcairo-dev, libcairo-gobject"
 
@@ -17,5 +17,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	LDFLAGS+=" -landroid-shmem"
+	LDFLAGS+=" -landroid-shmem -landroid-execinfo"
 }
