f5fb85c7e3e4b2f2ab48ce788e985a376990c557
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-14 19:40:59 +0800

    libicu: revbump revdeps (1) (#18486)

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -6,11 +6,12 @@ TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.72
 _VERSION=${_MAJOR_VERSION}.2
 TERMUX_PKG_VERSION=2:${_VERSION}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/vte/-/archive/${_VERSION}/vte-${_VERSION}.tar.bz2
 #TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/vte/${_MAJOR_VERSION}/vte-${_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5c8f789aaf76154de9d58205fda3b90790092d93791a5229c8b67c50d3f01068
 TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk3, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -20,6 +21,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 
 	CPPFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
 }
