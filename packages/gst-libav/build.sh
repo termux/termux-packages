bcce9d4b0b9b29d0e69f06a1e773df976e4a0a88
Twaik Yont <twaikyont@gmail.com>
2023-11-15 08:25:08 +0200

    bump(main/gst-libav): 1.22.7

--- packages/gst-libav/build.sh
+++ packages/gst-libav/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/modules/gst-libav.html
 TERMUX_PKG_DESCRIPTION="GStreamer Libav plug-in contains one plugin with a set of elements using the Libav library code"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
-TERMUX_PKG_VERSION=1.22.4
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.22.7"
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=9a751bc740de768e791c37a95f0a924c6a41d12fd7f37f54ce6a4e834be122d3
+TERMUX_PKG_SHA256=1525b917141b895fe5cf618fe8867622b2528278a0286e9f727b5f37317daca1
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ffmpeg, glib, gst-plugins-base, gstreamer"
