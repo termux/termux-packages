850d66fc3a35f77af4417519b63e7ca9571c07f8
Twaik Yont <twaikyont@gmail.com>
2023-11-15 11:48:23 +0200

    bump(main/libelf): 0.190

--- packages/libelf/build.sh
+++ packages/libelf/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://sourceware.org/elfutils/
 TERMUX_PKG_DESCRIPTION="ELF object file access library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.189
+TERMUX_PKG_VERSION="0.190"
 TERMUX_PKG_SRCURL="https://sourceware.org/elfutils/ftp/${TERMUX_PKG_VERSION}/elfutils-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=39bd8f1a338e2b7cd4abc3ff11a0eddc6e690f69578a57478d8179b4148708c8
+TERMUX_PKG_SHA256=8e00a3a9b5f04bc1dc273ae86281d2d26ed412020b391ffcc23198f10231d692
 # libandroid-support for langinfo.
 TERMUX_PKG_DEPENDS="libandroid-support, zlib, zstd"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_c99=yes --disable-symbol-versioning"
@@ -13,8 +13,8 @@ TERMUX_PKG_REPLACES="libelf-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CXXFLAGS+=" -Wno-unused-const-variable"
-	CFLAGS+=" -Wno-error=unused-value -Wno-error=format-nonliteral -Wno-error"
+	CXXFLAGS+=" -Wno-unused-const-variable -Wno-error=unused-function"
+	CFLAGS+=" -Wno-error=unused-value -Wno-error=format-nonliteral -Wno-error -Wno-error=unused-function"
 
 	# Exposes ACCESSPERMS in <sys/stat.h> which elfutils uses
 	CFLAGS+=" -D__USE_BSD"
