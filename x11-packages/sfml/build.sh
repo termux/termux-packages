c0a1a93f76b09009dc5eda3ecea2ec3c8671d8a4
Twaik Yont <twaikyont@gmail.com>
2023-11-15 10:03:22 +0200

    bump(x11/sfml): 2.6.1

--- x11-packages/sfml/build.sh
+++ x11-packages/sfml/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.sfml-dev.org/
 TERMUX_PKG_DESCRIPTION="A simple, fast, cross-platform and object-oriented multimedia API"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.6.0
+TERMUX_PKG_VERSION="2.6.1"
 TERMUX_PKG_SRCURL=https://github.com/SFML/SFML/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0c3f84898ea1db07dc46fa92e85038d8c449e3c8653fe09997383173de96bc06
+TERMUX_PKG_SHA256=82535db9e57105d4f3a8aedabd138631defaedc593cab589c924b7d7a11ffb9d
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxcursor, libxrandr, openal-soft, opengl"
 
 termux_step_post_get_source() {
