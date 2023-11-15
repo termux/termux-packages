891cad02f0099d42f4023a40383c67bba15adeaa
Twaik Yont <twaikyont@gmail.com>
2023-11-16 00:06:40 +0200

    bump(x11/libvte): 0.74.1

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -3,15 +3,12 @@ TERMUX_PKG_DESCRIPTION="Virtual Terminal library"
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING.GPL3, COPYING.LGPL3, COPYING.XTERM"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=0.72
-_VERSION=${_MAJOR_VERSION}.2
-TERMUX_PKG_VERSION=2:${_VERSION}
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/vte/-/archive/${_VERSION}/vte-${_VERSION}.tar.bz2
+TERMUX_PKG_VERSION="2:0.74.1"
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/vte/-/archive/${TERMUX_PKG_VERSION:2}/vte-${TERMUX_PKG_VERSION:2}.tar.bz2
 #TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/vte/${_MAJOR_VERSION}/vte-${_VERSION}.tar.xz
-TERMUX_PKG_SHA256=5c8f789aaf76154de9d58205fda3b90790092d93791a5229c8b67c50d3f01068
-TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk3, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
+TERMUX_PKG_SHA256=193496182428a34cfe555ee3df7ac6185de7eb7fc4af60b5dd175be46854b21a
+TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk4, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, bionic-host, ldd, glib-cross"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -20,18 +17,23 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	CPPFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX -Wno-cast-function-type-strict -Wno-deprecated-declarations"
+	mkdir -p ${TERMUX_PKG_TMPDIR}/bin
 
-	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
-	mkdir -p "${_WRAPPER_BIN}"
-	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
 		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
 			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
-			> "${_WRAPPER_BIN}/pkg-config"
-		chmod +x "${_WRAPPER_BIN}/pkg-config"
-		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
-	fi
-	export PATH="${_WRAPPER_BIN}:${PATH}"
-
-	CPPFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
+			> "${TERMUX_PKG_TMPDIR}/bin/pkg-config"
+		chmod +x "${TERMUX_PKG_TMPDIR}/bin/pkg-config"
+		export PKG_CONFIG="${TERMUX_PKG_TMPDIR}/bin/pkg-config"
+	
+	
+	for i in ldd; do
+	    echo -e "#!/bin/sh\nunset LD_LIBRARY_PATH\nexec $TERMUX_PREFIX/bin/$i \"\$@\"" > ${TERMUX_PKG_TMPDIR}/bin/$i \
+	        && chmod +x ${TERMUX_PKG_TMPDIR}/bin/$i
+	done
+	for i in bash $CC $CXX $AR $LD $STRIP; do 
+	    echo -e "#!/bin/sh\nunset LD_LIBRARY_PATH\nexec $(command -v $i) \"\$@\"" > ${TERMUX_PKG_TMPDIR}/bin/$i \
+	        && chmod +x ${TERMUX_PKG_TMPDIR}/bin/$i
+	done
+	export PATH="${TERMUX_PKG_TMPDIR}/bin:$PATH"
 }
