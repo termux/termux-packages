5bf558ffcf736d62fafceb9a06053763418d95bf
Twaik Yont <twaikyont@gmail.com>
2023-11-15 10:04:24 +0200

    bump(main/traceroute): 2.1.3

--- packages/traceroute/build.sh
+++ packages/traceroute/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://traceroute.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="A new modern implementation of traceroute(8) utility for Linux systems"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.1.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.1.3"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/traceroute/traceroute/traceroute-${TERMUX_PKG_VERSION}/traceroute-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=507c268f2977b4e218ce73e7ebed45ba0d970a8ca4995dd9cbb1ffe8e99b5b1f
+TERMUX_PKG_SHA256=05ebc7aba28a9100f9bbae54ceecbf75c82ccf46bdfce8b5d64806459a7e0412
 TERMUX_PKG_CONFLICTS="tracepath (<< 20221126-1)"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX -e"
