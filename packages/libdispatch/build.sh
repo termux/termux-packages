d10fd452662d3c39c0f19a901433bce8711b4688
Finagolfin <repo@butta.fastem.com>
2023-11-15 22:19:17 +0530

    rebuild(swift): patch to build with NDK 26b, including libdispatch and llbuild

--- packages/libdispatch/build.sh
+++ packages/libdispatch/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The libdispatch project, for concurrency on multicore ha
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@finagolfin"
 TERMUX_PKG_VERSION="1:5.9"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${TERMUX_PKG_VERSION:2}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=db30ccf6b20963112ab3aad256c2e49a18041a9806cf2f05854fe63a90d688c2
 TERMUX_PKG_AUTO_UPDATE=false
--- packages/llbuild/build.sh
+++ packages/llbuild/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A low-level build system, used by the Swift Package Mana
 TERMUX_PKG_LICENSE="Apache-2.0, NCSA"
 TERMUX_PKG_MAINTAINER="@finagolfin"
 TERMUX_PKG_VERSION=5.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-llbuild/archive/swift-${TERMUX_PKG_VERSION}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=cc8dfb098db4b0210ca5466f59d3e5fc55c4eedcd835263e6eae7dd719ba8203
 TERMUX_PKG_DEPENDS="libc++, libandroid-spawn, libsqlite"
