7811ec4a74644bcfa2c8cdc79c77b17daae0cd06
Kevin Williams <admin@utermux.dev>
2023-11-18 14:19:02 +0800

    upgpkg(main/kak-lsp): 15.0.0
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/kak-lsp/build.sh
+++ packages/kak-lsp/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/kak-lsp/kak-lsp
 TERMUX_PKG_DESCRIPTION="Language Server Protocol Client for the Kakoune editor"
 TERMUX_PKG_LICENSE="MIT, Unlicense"
 TERMUX_PKG_MAINTAINER="@finagolfin"
-TERMUX_PKG_VERSION="14.2.0"
+TERMUX_PKG_VERSION="15.0.0"
 TERMUX_PKG_SRCURL=https://github.com/kak-lsp/kak-lsp/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b81ba87bc8d6896041b96aa67b58011c23490fc9fdb47d1c89f176884b504fee
+TERMUX_PKG_SHA256=badd6420a2feba07a68f07a77cd1e6350a5f3d01fef7a7961e4a5e8f42a30f1c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
