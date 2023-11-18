8bde2a806f0ec7dcbf52ed3448f345962e81e613
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-11-18 14:11:30 +0800

    libtbb: bump to 2021.11.0

--- packages/libtbb/build.sh
+++ packages/libtbb/build.sh
@@ -2,12 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://oneapi-src.github.io/oneTBB/
 TERMUX_PKG_DESCRIPTION="oneAPI Threading Building Blocks"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2021.10.0"
+TERMUX_PKG_VERSION="2021.11.0"
 TERMUX_PKG_SRCURL=https://github.com/oneapi-src/oneTBB/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=487023a955e5a3cc6d3a0d5f89179f9b6c0ae7222613a7185b0227ba0c83700b
+TERMUX_PKG_SHA256=782ce0cab62df9ea125cdea253a50534862b563f1d85d4cda7ad4e77550ac363
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DTBB_STRICT=OFF
 -DTBB_TEST=OFF
 "
+
+termux_step_pre_configure() {
+	# Many symbols in the linker version scripts are undefined because link time
+	# optimization (-flto=thin) removes them. Suppress errors with lld >= 17 due to
+	# these undefined symbols.
+	# See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=274337
+	LDFLAGS+=" -Wl,--undefined-version"
+}
