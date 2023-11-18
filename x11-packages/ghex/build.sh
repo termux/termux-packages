857a963631b5819092f4e06539ace916803f877e
Jia Yuan Lo <jylo06g@gmail.com>
2023-11-18 21:54:16 +0800

    bump(x11/ghex): 45.1

--- x11-packages/ghex/build.sh
+++ x11-packages/ghex/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Ghex
 TERMUX_PKG_DESCRIPTION="A simple binary editor for the Gnome desktop."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="45.0"
+TERMUX_PKG_VERSION="45.1"
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/ghex/${TERMUX_PKG_VERSION%.*}/ghex-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=05cecc4561ca40b257c5db31da9f68d696133efc0ae427ed82fb985a986e840e
+TERMUX_PKG_SHA256=fb2b0823cd16249edbeaee8302f9bd5005e0150368b35f1e47c26680cacac2fa
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gtk4, libadwaita, libcairo, pango"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dintrospection=enabled
@@ -15,6 +15,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
 
 termux_pkg_auto_update() {
