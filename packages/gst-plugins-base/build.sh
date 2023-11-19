21aabc209ebd5ef622308824fe7a1b580989ec1c
Twaik Yont <twaikyont@gmail.com>
2023-11-19 21:34:20 +0200

    bump(main/gst-plugins-base): 1.22.7

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.22.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.22.7"
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=292424e82dea170528c42b456f62a89532bcabc0508f192e34672fb86f68e5b8
+TERMUX_PKG_SHA256=62519e0d8f969ebf62a9a7996f2d23efdda330217a635f4a32c0bf1c71577468
 TERMUX_PKG_DEPENDS="glib, graphene, gstreamer, libandroid-shmem, libjpeg-turbo, libogg, libopus, libpng, libtheora, libvorbis, libx11, libxcb, libxext, libxv, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, opengl"
 TERMUX_PKG_RECOMMENDS="opengl"
@@ -21,6 +20,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 
 	LDFLAGS+=" -landroid-shmem"
 }
