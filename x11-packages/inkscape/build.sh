8c432d2253034b21c0765ca7068a07770e013400
Twaik Yont <twaikyont@gmail.com>
2023-11-20 14:42:36 +0200

    fix(x11/inkscape): forcing to include xmlmemory.h

--- x11-packages/inkscape/build.sh
+++ x11-packages/inkscape/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Free and open source vector graphics editor"
 TERMUX_PKG_LICENSE="GPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://media.inkscape.org/dl/resources/file/inkscape-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=421e0035fe5b3b054a0865dc8235be3f9e6e2dea54190d926b880a4ce05b00d8
 TERMUX_PKG_DEPENDS="boost, double-conversion, fontconfig, freetype, gdk-pixbuf, glib, gsl, gtk3, gtkmm3, harfbuzz, libatkmm-1.6, libc++, libcairo, libcairomm-1.0, libgc, libglibmm-2.4, libiconv, libjpeg-turbo, libpangomm-1.4, libpng, libsigc++-2.0, libsoup, libx11, libxml2, libxslt, littlecms, pango, poppler, potrace, readline, zlib"
@@ -19,7 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD -DxmlFree=free"
+	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD -I${TERMUX_PREFIX}/include/libxml2 -include libxml/xmlmemory.h"
 	LDFLAGS+=" -Wl,-rpath=$TERMUX_PREFIX/lib/inkscape"
 }
 
