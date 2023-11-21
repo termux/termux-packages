4676f621e1b8043dd2541a493a43819551560c83
Twaik Yont <twaikyont@gmail.com>
2023-11-21 09:46:03 +0200

    bump(main/crowbook): 0.16.1

--- packages/crowbook/build.sh
+++ packages/crowbook/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/lise-henry
 TERMUX_PKG_DESCRIPTION="Allows you to write a book in Markdown without worrying about formatting or typography"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.15.2e
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.16.1"
 TERMUX_PKG_SRCURL=https://github.com/lise-henry/crowbook/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ac950c051faece985821c28c17206188a6211247a040a8e7afaa34a5f45cf81f
+TERMUX_PKG_SHA256=2e49a10f1b14666d4f740e9a22a588d44b137c3fca0932afc50ded0280450311
 TERMUX_PKG_DEPENDS="openssl-1.1"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 # https://github.com/termux/termux-packages/issues/12824
-TERMUX_RUST_VERSION=1.63.0
+TERMUX_RUST_VERSION=1.73.0
 
 termux_step_pre_configure() {
 	# openssl-sys supports OpenSSL 3 in >= 0.9.69
