2a0f6e81e637c38f718ef7f3bd8b742578ecdb1f
Twaik Yont <twaikyont@gmail.com>
2023-11-22 23:09:05 +0200

    rebuild(main/libpano13): tiff update; bump(x11/hugin): 2023.0.0

--- x11-packages/hugin/build.sh
+++ x11-packages/hugin/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://hugin.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Toolchain to create panoramic images for every occasion"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.0.0
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION="2023.0.0"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/hugin/hugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=97c8562a0ba9a743e0b955a43dfde048b1c60cd9e5f2ee2b69de1a81646e05a7
+TERMUX_PKG_SHA256=04a39fccc6017e0544c639bd22309472c574d35b3455cbade1fc3870e631632b
 TERMUX_PKG_DEPENDS="boost, enblend, exiftool, exiv2, fftw, glew, glu, imath, libc++, libflann, liblz4, libpano13, libsqlite, libtiff, libvigra, libx11, littlecms, openexr, opengl, wxwidgets"
 # libjpeg-turbo, libpng and zlib are detected but not linked against
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, libjpeg-turbo, libpng, zlib"
@@ -15,6 +14,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -D__USE_GNU"
+	CPPFLAGS+=" -D__USE_GNU -Wno-deprecated-register -Wno-deprecated-declarations"
 	LDFLAGS+=" -Wl,-rpath=$TERMUX_PREFIX/lib/hugin"
 }
