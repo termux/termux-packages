886afacf61fd7424f1ad90873401000f31ac3126
Twaik Yont <twaikyont@gmail.com>
2023-11-22 19:10:29 +0200

    bump(x11/webkit2gtk-4.1): 2.42.2

--- x11-packages/webkit2gtk-4.1/build.sh
+++ x11-packages/webkit2gtk-4.1/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://webkitgtk.org
 TERMUX_PKG_DESCRIPTION="A full-featured port of the WebKit rendering engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.40.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="2.42.2"
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cc0aa83f40dbc64c1c6ae42ec6b85af4be2a9dbf524cfcb95f89a367fb5098dd
+TERMUX_PKG_SHA256=5720aa3e8627f1b9f63252187d4df0f8233ae71d697b1796ebfbe5ca750bd118
 TERMUX_PKG_DEPENDS="atk, enchant, fontconfig, freetype, glib, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gstreamer, gtk3, harfbuzz, harfbuzz-icu, libc++, libcairo, libgcrypt, libhyphen, libicu, libjpeg-turbo, libpng, libsoup3, libtasn1, libwebp, libxml2, libx11, libxcomposite, libxdamage, libxslt, libxt, littlecms, openjpeg, pango, woff2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xorgproto"
 TERMUX_PKG_DISABLE_GIR=false
@@ -29,10 +28,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
+
+	# Workaround for https://github.com/android/ndk/issues/1973
+	[ "$TERMUX_ARCH" == "arm" ] && sed -i '/#define MUST_TAIL_CALL \[\[clang::musttail]]/d' Source/WTF/wtf/Compiler.h
 
 	CPPFLAGS+=" -DHAVE_MISSING_STD_FILESYSTEM_PATH_CONSTRUCTOR"
 	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
+	CPPFLAGS+=" -I${TERMUX_PREFIX}/lib/gstreamer-1.0/include"
 }
 
 termux_step_post_massage() {
