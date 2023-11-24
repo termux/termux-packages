6024c90692b7153513311e4a3e026ab19111a939
Twaik Yont <twaikyont@gmail.com>
2023-11-24 12:48:23 +0200

    bump(main/libzim): 9.0.0

--- packages/libzim/build.sh
+++ packages/libzim/build.sh
@@ -2,21 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://openzim.org
 TERMUX_PKG_DESCRIPTION="The ZIM library is the reference implementation for the ZIM file format."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="8.2.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="9.0.0"
 TERMUX_PKG_SRCURL=https://github.com/openzim/libzim/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=b8296644b04b02c04d2ff1458fed829df39b54e8fd1bcd23c10440e160819f13
+TERMUX_PKG_SHA256=681f0419d421be05ffd14dbab65c42103ebffb86deceed70b8bd6f9b0590ecd6
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libicu, liblzma, libxapian, zstd"
 TERMUX_PKG_BUILD_DEPENDS="googletest, libuuid"
-TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=8
+	local _SOVERSION=9
 
 	local v=$(echo ${TERMUX_PKG_VERSION#*:} | cut -d . -f 1)
 	if [ "${v}" != "${_SOVERSION}" ]; then
-		termux_error_exit "SOVERSION guard check failed."
+		termux_error_exit "SOVERSION guard check failed." 
 	fi
 }
