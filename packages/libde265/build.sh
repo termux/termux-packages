fb74eab2b6b9a410b3fc2fcaf2d799b990ac5ac6
Twaik Yont <twaikyont@gmail.com>
2023-11-26 15:12:05 +0200

    upgpkg(main/libde265): 1.0.14

--- packages/libde265/build.sh
+++ packages/libde265/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="H.265/HEVC video stream decoder library"
 TERMUX_PKG_LICENSE="LGPL-3.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.12"
+TERMUX_PKG_VERSION="1.0.14"
 TERMUX_PKG_SRCURL=https://github.com/strukturag/libde265/releases/download/v$TERMUX_PKG_VERSION/libde265-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=62185ea2182e68cf68bba20cc6eb4c287407b509cf0a827d7ddb75614db77b5c
+TERMUX_PKG_SHA256=99f46ef77a438be639aa3c5d9632c0670541c5ed5d386524d4199da2d30df28f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sherlock265 --disable-arm --disable-encoder"
@@ -27,6 +27,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
+	sed -i "s#tools##;s#acceleration-speed##" ${TERMUX_PKG_SRCDIR}/Makefile.am
 	autoreconf -fi
 
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
