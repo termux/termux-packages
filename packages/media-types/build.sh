5eae8a116340ba9ffc5cd5454d80e97adae96742
Twaik Yont <twaikyont@gmail.com>
2023-11-27 18:23:58 +0200

    fix(main/media-types): replacing deprecated mime-support package

--- packages/media-types/build.sh
+++ packages/media-types/build.sh
@@ -7,6 +7,9 @@ TERMUX_PKG_SRCURL=https://salsa.debian.org/debian/media-types/-/archive/${TERMUX
 TERMUX_PKG_SHA256=d24e54447bf7cdf6dcea4d55faeb74c8156b7aed0757b962f94a5b1f1ab0de5c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BREAKS="mime-support"
+TERMUX_PKG_REPLACES="mime-support"
+TERMUX_PKG_PROVIDES="mime-support"
 TERMUX_PKG_CONFFILES="etc/mime.types"
 # etc/mime.types was previously in mutt:
 TERMUX_PKG_CONFLICTS="mutt (<< 1.8.3-1)"
