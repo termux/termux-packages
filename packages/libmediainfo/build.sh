e6c5d98c20e0ec09c98dc59109d41aac2e44ff9b
valpogus <32718480+valpogus@users.noreply.github.com>
2023-12-02 13:19:39 +0100

    upgpkg(main/libmediainfo): 23.11

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Library for reading information from media files"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=23.10
+TERMUX_PKG_VERSION=23.11
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/libmediainfo/${TERMUX_PKG_VERSION}/libmediainfo_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=76ebe502e0f310b559d5dd90727d9aafd5fabaaeca3442f38e629dfc07da0d22
+TERMUX_PKG_SHA256=197e54fcc79e3c0d5df44a8f58dc4e018bc2f85d13fa3bed54af3dc56d5e853d
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libcurl, libzen, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-shared --enable-static --with-libcurl"
 
