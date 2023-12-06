a09f00db5b49c190eb8f6d2fdb23830c4544f31f
Twaik Yont <twaikyont@gmail.com>
2023-12-06 20:09:45 +0200

    bump(dev/chrony): 4.5

--- packages/chrony/build.sh
+++ packages/chrony/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://chrony-project.org/
 TERMUX_PKG_DESCRIPTION="chrony is an implementation of the Network Time Protocol (NTP)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@marek22k"
-TERMUX_PKG_VERSION=4.4
+TERMUX_PKG_VERSION="4.5"
 TERMUX_PKG_SRCURL=https://chrony-project.org/releases/chrony-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=eafb07e6daf92b142200f478856dfed6efc9ea2d146eeded5edcb09b93127088
+TERMUX_PKG_SHA256=19fe1d9f4664d445a69a96c71e8fdb60bcd8df24c73d1386e02287f7366ad422
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-shmem, libcap, libgnutls, libnettle, libnss, libtomcrypt, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--chronyvardir=${TERMUX_PREFIX}/var/lib/chrony"
