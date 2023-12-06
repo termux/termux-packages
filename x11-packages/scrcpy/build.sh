028a93563f4e27df4d674595dd5305118963fc82
Twaik Yont <twaikyont@gmail.com>
2023-12-06 20:07:45 +0200

    upgpkg(x11/scrcpy): 2.3.1

--- x11-packages/scrcpy/build.sh
+++ x11-packages/scrcpy/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Genymobile/scrcpy
 TERMUX_PKG_DESCRIPTION="Provides display and control of Android devices connected via USB or over TCP/IP"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.3"
+TERMUX_PKG_VERSION="2.3.1"
 TERMUX_PKG_SRCURL=https://github.com/Genymobile/scrcpy/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=70937335be7c8b0be3dcf4ae2b0371e5dbe6cf340bf4ccb341be3d10fc039c36
+TERMUX_PKG_SHA256=76f38779f00d91d0b46a399ebca32c82ff1facdbd843871b7e46c2e7cad38a42
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ffmpeg, libusb, sdl2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dprebuilt_server=$TERMUX_PKG_SRCDIR/scrcpy-server-v${TERMUX_PKG_VERSION}
@@ -12,6 +13,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_post_get_source() {
 	local _url=https://github.com/Genymobile/scrcpy/releases/download/v${TERMUX_PKG_VERSION}/scrcpy-server-v${TERMUX_PKG_VERSION}
-	local _sha256=8daed514d7796fca6987dc973e201bd15ba51d0f7258973dec92d9ded00dbd5f
-	termux_download ${_url} $(basename ${_url}) ${_sha256}
+	termux_download ${_url} $(basename ${_url}) SKIP_CHECKSUM
+	# We are skipping checksum checking, but we should ensure it is android package.
+	[[ "$(file $(basename ${_url}))"==*"Android package"*  || "$(file $(basename ${_url}))"==*"Zip archive data"* ]] \
+		|| termux_error_exit "$(basename ${_url}) has wrong signature: $(file $(basename ${_url}))"
 }
