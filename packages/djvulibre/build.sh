f7750dfca1e785690a70c02b86d29d1403893f2d
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-12-07 17:45:49 +0800

    djvulibre: revbump to rebuild with libjpeg

--- packages/djvulibre/build.sh
+++ packages/djvulibre/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Suite to create, manipulate and view DjVu ('déjà vu') 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.5.28
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/djvu/djvulibre-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fcd009ea7654fde5a83600eb80757bd3a76998e47d13c66b54c8db849f8f2edc
-TERMUX_PKG_DEPENDS="libc++, libtiff"
+TERMUX_PKG_DEPENDS="libc++, libjpeg-turbo, libtiff"
 
 termux_step_pre_configure() {
 	NOCONFIGURE=1 ./autogen.sh
