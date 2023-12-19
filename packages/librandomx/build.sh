14033397d8a9f33d62172636eddaf32d58ed9ce7
Al-buharie Amjari <harieamjari@gmail.com>
2023-12-20 02:32:46 +0800

    addpkg(master/librandomx): a proof-of-work (PoW) algorithm that is optimized for general-purpose CPUs.

---
+++ packages/librandomx/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tevador/RandomX
+TERMUX_PKG_DESCRIPTION="A proof-of-work (PoW) algorithm that is optimized for general-purpose CPUs."
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.2.1
+TERMUX_PKG_SRCURL=https://github.com/tevador/RandomX/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2e6dd3bed96479332c4c8e4cab2505699ade418a07797f64ee0d4fa394555032
+TERMUX_PKG_DEPENDS="libc++"
