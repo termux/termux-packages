3bf63efb4ffadef9dd706d9a261ae9e37e32fb55
Twaik Yont <twaikyont@gmail.com>
2023-12-19 22:23:20 +0200

    bump(x11/libvte): 0.74.2

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Virtual Terminal library"
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING.GPL3, COPYING.LGPL3, COPYING.XTERM"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2:0.74.1"
+TERMUX_PKG_VERSION="2:0.74.2"
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/vte/-/archive/${TERMUX_PKG_VERSION:2}/vte-${TERMUX_PKG_VERSION:2}.tar.bz2
 #TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/vte/${_MAJOR_VERSION}/vte-${_VERSION}.tar.xz
-TERMUX_PKG_SHA256=193496182428a34cfe555ee3df7ac6185de7eb7fc4af60b5dd175be46854b21a
+TERMUX_PKG_SHA256=2a1162738c9bccfac1bb801125c1889d3980d857499909439803cf1def4c25d1
 TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk3, gtk4, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
@@ -17,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
 
 	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
 	mkdir -p "${_WRAPPER_BIN}"
