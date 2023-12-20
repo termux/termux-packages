0601615f50bdc15549767281687fbb4a50de313a
Twaik Yont <twaikyont@gmail.com>
2023-12-20 10:10:59 +0200

    bump(main/cfengine): 3.23.0

--- packages/cfengine/build.sh
+++ packages/cfengine/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://cfengine.com/
 TERMUX_PKG_DESCRIPTION="CFEngine is a configuration management technology"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@craigcomstock"
-TERMUX_PKG_VERSION=1:3.22.0
+TERMUX_PKG_VERSION=1:3.23.0
 TERMUX_PKG_SRCURL=git+https://github.com/cfengine/core
-TERMUX_PKG_SHA256=70bc344ed09c512ae7a2a19bf1627b3ce38473d226a19db423d95908664018d5
+TERMUX_PKG_SHA256=6d0b827d396a6e5ddefce8c12f80ec764ecc3ea70633f0c8524d06e0ae5ffbfc
 # "-build[n]" suffix in tag name is not a part of version string.
 _CFENGINE_GIT_TAG_SUFFIX=
 TERMUX_PKG_GIT_BRANCH=${TERMUX_PKG_VERSION#*:}${_CFENGINE_GIT_TAG_SUFFIX}
@@ -27,13 +27,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_post_get_source() {
 	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
 	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
+		termux_error_exit "Checksum mismatch for source files. \nExpected ${TERMUX_PKG_SHA256}\nActual ${s% *}"
 	fi
 
 	: ${_CFENGINE_GIT_TAG_SUFFIX:=}
 	local _MASTERFILES_VERSION=${TERMUX_PKG_VERSION#*:}${_CFENGINE_GIT_TAG_SUFFIX}
 	local _MASTERFILES_SRCURL=https://github.com/cfengine/masterfiles/archive/${_MASTERFILES_VERSION}.zip
-	local _MASTERFILES_SHA256=6cb9a639075f71898c1fffdbb841de5fbf60d12222db63fffca1d49615a944fd
+	local _MASTERFILES_SHA256=030c34f802a961feea9ed5cc74c5121287710d9f5da39a41bd49b99468f5caab
 	local _MASTERFILES_FILE=${TERMUX_PKG_CACHEDIR}/masterfiles-${_MASTERFILES_VERSION}.zip
 	termux_download \
 		${_MASTERFILES_SRCURL} \
