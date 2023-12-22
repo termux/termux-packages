cd1b939f5cbd4d07e8728b5d2567375dd7fee55a
Twaik Yont <twaikyont@gmail.com>
2023-12-22 14:31:46 +0200

    fix(root-erofs-utils): enable automatic updates

--- root-packages/erofs-utils/build.sh
+++ root-packages/erofs-utils/build.sh
@@ -6,6 +6,8 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.7.1
 TERMUX_PKG_SRCURL=https://github.com/erofs/erofs-utils/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=196083d63e5e231fb5799e7ce86a944bbca564daabce3de9225a8aca9dcaff15
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="liblz4, liblzma, libfuse2, libuuid"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--prefix=$TERMUX_PREFIX --enable-lzma --enable-fuse"
 TERMUX_PKG_BUILD_IN_SRC=true
