ef6e92fa9ddfb8b9cc32cffa8df056de28cd7a7a
Ansh Dadwal <anshdadwal298@gmail.com>
2023-12-24 14:34:30 +0530

    updpkg(main/opencolorio): update to v2.3.1

--- packages/opencolorio/build.sh
+++ packages/opencolorio/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://opencolorio.org
 TERMUX_PKG_DESCRIPTION="A color management framework for visual effects and animation"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.3.0
+TERMUX_PKG_VERSION=2.3.1
 TERMUX_PKG_SRCURL=https://github.com/AcademySoftwareFoundation/OpenColorIO/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=32b7be676c110d849a77886d8a409159f0367309b2b2f5dae5aa0c38f42b445a
+TERMUX_PKG_SHA256=7196e979a0449ce28afd46a78383476f3b8fc1cc1d3a417192be439ede83437b
 TERMUX_PKG_DEPENDS="imath, libc++, libexpat, libminizip-ng, libyaml-cpp, pystring"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dpystring_INCLUDE_DIR=$TERMUX_PREFIX/lib
@@ -12,3 +12,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 # Command-line apps depend on packages in x11 repo (for OpenGL functionality):
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DOCIO_BUILD_APPS=OFF"
+
+termux_step_pre_configure() {
+    # error: constant expression evaluates to -1 which cannot be narrowed to type 'char' [-Wc++11-narrowing]
+    # also same is used while building apt
+    CXXFLAGS+=" -Wno-c++11-narrowing"
+    CXXFLAGS+=" -I$PREFIX/include/pystring"
+}
