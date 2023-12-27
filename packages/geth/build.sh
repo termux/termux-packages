a08edf99b1bb34a38fc8a506cd8fcca78a8a5b41
Jia Yuan Lo <jylo06g@gmail.com>
2023-12-28 05:42:34 +0800

    bump(main/geth): 1.13.6 (#18837)
    faucet has been removed in upstream

--- packages/geth/build.sh
+++ packages/geth/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://geth.ethereum.org/
 TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.13.5"
-TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=7cfce349683e68145fcc1246c74d0b45097cc32b4e407d96b70576e38767942a
+TERMUX_PKG_VERSION="1.13.6"
+TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fe17444db3110c9f7f0d34b286bd76c9ac43b84f4e36a6bfbccf18de815cd5e3
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SUGGESTS="geth-utils"
 
@@ -16,14 +16,14 @@ termux_step_make() {
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/ethereum/go-ethereum
 
 	cd "$GOPATH"/src/github.com/ethereum/go-ethereum
-	for applet in abidump abigen bootnode clef devp2p ethkey evm faucet geth p2psim rlpdump; do
-		(cd ./cmd/"$applet" && go build -v)
+	for applet in abidump abigen bootnode clef devp2p ethkey evm geth p2psim rlpdump; do
+		go -C ./cmd/"$applet" build -v
 	done
 	unset applet
 }
 
 termux_step_make_install() {
-	for applet in abidump abigen bootnode clef devp2p ethkey evm faucet geth p2psim rlpdump; do
+	for applet in abidump abigen bootnode clef devp2p ethkey evm geth p2psim rlpdump; do
 		install -Dm700 \
 			"$TERMUX_PKG_SRCDIR/cmd/$applet/$applet" \
 			"$TERMUX_PREFIX"/bin/
--- packages/geth/geth-utils.subpackage.sh
+++ packages/geth/geth-utils.subpackage.sh
@@ -1,5 +1,4 @@
 TERMUX_SUBPKG_DESCRIPTION="Additional utilities for Geth (like abigen, bootnode, evm, puppeth)"
-
 TERMUX_SUBPKG_INCLUDE="
 bin/abidump
 bin/abigen
@@ -8,6 +7,6 @@ bin/clef
 bin/devp2p
 bin/ethkey
 bin/evm
-bin/faucet
 bin/p2psim
-bin/rlpdump"
+bin/rlpdump
+"
