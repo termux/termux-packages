1f58b8e0b8c786b7d3bf3f6d79375da56128cf36
Jia Yuan Lo <jylo06g@gmail.com>
2023-12-30 17:38:40 +0800

    fix(x11/weston): hardcoded paths (#18855)

--- x11-packages/weston/build.sh
+++ x11-packages/weston/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Reference implementation of a wayland compositor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="13.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/weston/-/archive/${TERMUX_PKG_VERSION}/weston-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a4848b7cc510e3e88c9f2fcc808fd44c51fae622f13242f801a7c5107983335c
 TERMUX_PKG_DEPENDS="freerdp, libaml, libandroid-shmem, libcairo, libcairo, libevdev, libglvnd, libneatvnc, libseat, libwayland, libwebp, libxcb, libxcursor, libxkbcommon, littlecms, pango, xcb-util-cursor"
