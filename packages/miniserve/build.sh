dc3b3a04c7eb20300a5771892fe025f2a3e9ed21
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-08 07:09:09 +0800

    miniserve: bump to 0.25.0

--- packages/miniserve/build.sh
+++ packages/miniserve/build.sh
@@ -2,31 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/svenstaro/miniserve
 TERMUX_PKG_DESCRIPTION="Tool to serve files and dirs over HTTP"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.24.0"
+TERMUX_PKG_VERSION="0.25.0"
 TERMUX_PKG_SRCURL=https://github.com/svenstaro/miniserve/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ed0619bfbad3f9ea635aa266a9c4e96247ad94683d50743f0464f48f9e48ae88
+TERMUX_PKG_SHA256=27986ea4f3ba6670798e6c78709b7c11d5bbd1417b93826123e829c40b5bd000
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
-
-	export CFLAGS="${TARGET_CFLAGS}"
-
-	: "${CARGO_HOME:=$HOME/.cargo}"
-	export CARGO_HOME
-
-	rm -rf $CARGO_HOME/registry/src/*/socket2-*
-	cargo fetch --target "${CARGO_TARGET_NAME}"
-
-	if [ $TERMUX_ARCH_BITS = 32 ]; then
-		local p="socket2-0.5.2-src-sys-unix.rs.diff32"
-		local d
-		for d in $CARGO_HOME/registry/src/*/socket2-*; do
-			patch --silent -p1 -d ${d} \
-				< "$TERMUX_PKG_BUILDER_DIR/${p}" || :
-		done
-	fi
 
 	rm -f Makefile
 }
