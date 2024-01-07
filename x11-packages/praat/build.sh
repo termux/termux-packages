fd006fefb65450775dc2e98f8e482cd0d06b2746
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-08 07:18:04 +0800

    praat: bump to 6.4.04

--- x11-packages/praat/build.sh
+++ x11-packages/praat/build.sh
@@ -2,12 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.praat.org
 TERMUX_PKG_DESCRIPTION="Doing phonetics by computer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.4.03"
+TERMUX_PKG_VERSION="6.4.04"
 TERMUX_PKG_SRCURL=https://github.com/praat/praat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d829c135533247e5983450e387953e2f0ca54a488c76f2f75d9554565bd11071
+TERMUX_PKG_SHA256=ed70efaa23d649bb49d859e372b2e94cd364fbc5e5c3ea34d173849f46867cac
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libc++, libcairo, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	rm -f meson.build
+}
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin praat
