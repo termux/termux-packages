00c00ee10f20f8b285ecc4073c210a3b07d08084
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-08 19:26:36 +0800

    uftrace: bump to 0.15

--- packages/uftrace/build.sh
+++ packages/uftrace/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://uftrace.github.io/slide
 TERMUX_PKG_DESCRIPTION="Function (graph) tracer for user-space"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.14"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.15"
 TERMUX_PKG_SRCURL=https://github.com/namhyung/uftrace/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b81255bc288e79e96a5b158d4875fa6425ad51c85f4c4c8523c1defb9366ad12
+TERMUX_PKG_SHA256=c4f2a45687fd39dbde509635ebf094d7ed301793920f37bcaabb8161ff69f2fd
 # Hardcoded libpython${TERMUX_PYTHON_VERSION}.so is dlopen(3)ed by uftrace.
 # Please revbump and rebuild when bumping TERMUX_PYTHON_VERSION.
 # libandroid-{execinfo,spawn} are dlopen(3)ed.
