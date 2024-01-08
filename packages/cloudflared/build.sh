ccb157a7a431f2e9f7e02c93eac9135b526be0e6
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-08 20:30:33 +0800

    cloudflared: bump to 2024.1.0

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2023.10.0"
+TERMUX_PKG_VERSION="2024.1.0"
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2d2df4dd4992eef485f7ffebc0a1e9e6292b42ca42341f2e46224f17155e9532
+TERMUX_PKG_SHA256=14ae1967ccd4bf59659152988557e08adb284029d1e8da62f116e0372d1f31d9
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_GO_USE_OLDER=true
 
 termux_step_make() {
 	termux_setup_golang
