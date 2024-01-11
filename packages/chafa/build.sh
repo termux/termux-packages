56d698cac138a54bae3b27ebfbf9990d44cede99
Launch Lee <80872691+LaunchLee@users.noreply.github.com>
2024-01-11 18:07:21 +0800

    Bump(main/chafa): 1.14.0
    Co-authored-by: Jia Yuan Lo <jylo06g@gmail.com>

--- packages/chafa/build.sh
+++ packages/chafa/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="Image-to-text converter supporting a wide range of symbo
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Yonle <yonle@lecturify.net>"
 TERMUX_PKG_VERSION=1.14.0
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/hpjansson/chafa/releases/download/${TERMUX_PKG_VERSION}/chafa-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=670e55c28b5ecd4c8187bd97f0898762712a480ec8ea439dae4a4836b178e084
 TERMUX_PKG_DEPENDS="freetype, glib, libcairo, libjpeg-turbo, librsvg, libtiff, libwebp"
