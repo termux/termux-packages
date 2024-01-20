aa037a3d3f2941700c1cd47bd0083c05151fabb7
Leonid P <sylirre@termux.dev>
2024-01-20 17:24:53 +0200

    bump(main/apt): 2.7.9

--- packages/apt/build.sh
+++ packages/apt/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/apt
 TERMUX_PKG_DESCRIPTION="Front-end for the dpkg package manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.7.7"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.7.9"
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/a/apt/apt_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d3b3f3b7014f6e561b86c059b89040d0c742cbabc576593c6efaa0dd7b24de77
+TERMUX_PKG_SHA256=666f41cd641ff9894cba531b0cc4fcf199e6494587fcb82a39b00dc88b46bb27
 # apt-key requires utilities from coreutils, findutils, gpgv, grep, sed.
 TERMUX_PKG_DEPENDS="coreutils, dpkg, findutils, gpgv, grep, libandroid-glob, libbz2, libc++, libcurl, libgnutls, liblz4, liblzma, sed, termux-keyring, termux-licenses, xxhash, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="docbook-xsl"
