3e79c5fe300fa0710f565cd218d37731ef4e6a94
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-23 00:07:30 +0800

    qt5-qtbase: append `undefined-version` to `LDFLAGS`

--- x11-packages/qt5-qtbase/build.sh
+++ x11-packages/qt5-qtbase/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A cross-platform application and UI framework"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=5.15.10
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/5.15/${TERMUX_PKG_VERSION}/submodules/qtbase-everywhere-opensource-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=c0d06cb18d20f10bf7ad53552099e097ec39362d30a5d6f104724f55fa1c8fb9
 TERMUX_PKG_DEPENDS="dbus, double-conversion, freetype, glib, harfbuzz, krb5, libandroid-execinfo, libandroid-shmem, libandroid-posix-semaphore, libc++, libice, libicu, libjpeg-turbo, libpng, libsm, libuuid, libx11, libxcb, libxi, libxkbcommon, openssl, pcre2, postgresql, ttf-dejavu, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm, zlib"
@@ -28,6 +28,11 @@ termux_step_pre_configure () {
         CFLAGS="${CFLAGS/-mfpu=neon/} -mfpu=vfp"
         CXXFLAGS="${CXXFLAGS/-mfpu=neon/} -mfpu=vfp"
     fi
+
+    # This is needed for some packages depends on qt5-qtbase, such
+    # as qt5-qtwebengine
+    # https://github.com/termux/termux-packages/issues/18810
+    export LDFLAGS+=" -Wl,--undefined-version"
 
     ## Create qmake.conf suitable for cross-compiling.
     sed \
