0341c8033ca933aa3925f10ec5a21e846aedbc23
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-22 23:25:39 +0800

    wine-stable: do not use `sudo apt install`
    [skip ci]

--- x11-packages/wine-stable/build.sh
+++ x11-packages/wine-stable/build.sh
@@ -102,7 +102,6 @@ termux_step_host_build() {
 	_setup_llvm_mingw_toolchain
 
 	# Make host wine-tools
-	(unset sudo; sudo apt update; sudo apt install libfreetype-dev:i386 -yqq)
 	"$TERMUX_PKG_SRCDIR/configure" ${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
 	make -j "$TERMUX_MAKE_PROCESSES" __tooldeps__ nls/all
 }
