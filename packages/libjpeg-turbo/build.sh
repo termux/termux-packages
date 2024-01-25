a892a92d30242450e1a9df8df0605c05b973937c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-01-25 23:30:47 +0800

    libjpeg-turbo: bump to 3.0.2

--- packages/libjpeg-turbo/build.sh
+++ packages/libjpeg-turbo/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Library for reading and writing JPEG image files"
 TERMUX_PKG_LICENSE="IJG, BSD 3-Clause, ZLIB"
 TERMUX_PKG_LICENSE_FILE="README.ijg, LICENSE.md"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.0.1"
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/libjpeg-turbo/${TERMUX_PKG_VERSION}/libjpeg-turbo-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=22429507714ae147b3acacd299e82099fce5d9f456882fc28e252e4579ba2a75
+TERMUX_PKG_VERSION="3.0.2"
+TERMUX_PKG_SRCURL=https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/${TERMUX_PKG_VERSION}/libjpeg-turbo-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c2ce515a78d91b09023773ef2770d6b0df77d674e144de80d63e0389b3a15ca6
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="libjpeg-turbo-dev"
 TERMUX_PKG_REPLACES="libjpeg-turbo-dev"
@@ -22,4 +22,17 @@ termux_step_post_massage() {
 	if ! readelf -d lib/libjpeg.so | grep -q '(SONAME).*\[libjpeg\.so\.'; then
 		termux_error_exit "SONAME for libjpeg.so is not properly set."
 	fi
+
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION_GUARD_FILES="
+lib/libjpeg.so.8
+lib/libturbojpeg.so.0
+"
+	local f
+	for f in ${_SOVERSION_GUARD_FILES}; do
+		if [ ! -e "${f}" ]; then
+			termux_error_exit "SOVERSION guard check failed."
+		fi
+	done
 }
