d6281751741db4881a85a570329e3830061c01d1
Lucy Phipps <landfillbaby69@gmail.com>
2024-01-28 16:29:13 +0000

    fdupes: depend on libsqlite

--- packages/fdupes/build.sh
+++ packages/fdupes/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Duplicates file detector"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.3.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/adrianlopezroche/fdupes/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8f38d21eb53e27a43f6652f0c6fa80c673f18466760281e812e84f56c1d359e3
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_DEPENDS="libsqlite"
-TERMUX_PKG_DEPENDS="ncurses, pcre2"
+TERMUX_PKG_DEPENDS="libsqlite, ncurses, pcre2"
 
 termux_step_pre_configure() {
 	autoreconf --install
