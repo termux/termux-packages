60eaaf934b0158708f0f5de6fdf7aa1a9d55f0c8
Kevin Williams <admin@utermux.dev>
2024-02-02 15:01:31 +0800

    upgpkg(main/neomutt): 20240201
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/neomutt/build.sh
+++ packages/neomutt/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A version of mutt with added features"
 # License: GPL-2.0-or-later
 TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="20231221"
+TERMUX_PKG_VERSION="20240201"
 TERMUX_PKG_SRCURL=https://github.com/neomutt/neomutt/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b2add3823db78d8c0a86507ef3b244a3487d7e7a70c0d9e4e4b136acda9004d2
+TERMUX_PKG_SHA256=5229c4fdd6fd6ef870b94032bb1073f7f881ce97cf5754b1a4f4579a97b918da
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d{8}"
 TERMUX_PKG_DEPENDS="gdbm, krb5, libandroid-support, libiconv, libsasl, ncurses, notmuch, openssl, zlib, zstd"
