f4ff7ea2607001b1458b63e4a0c4c4cc48e5f0ae
Biswapriyo Nath <nathbappai@gmail.com>
2024-02-03 11:10:05 +0530

    bump(root/ipset): 7.20

--- root-packages/ipset/build.sh
+++ root-packages/ipset/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://ipset.netfilter.org
 TERMUX_PKG_DESCRIPTION="Administration tool for kernel IP sets"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_VERSION="7.19"
+TERMUX_PKG_VERSION="7.20"
 TERMUX_PKG_SRCURL=https://ipset.netfilter.org/ipset-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9bc1fba48d65786e3e0b63dc6b669a866823d77840c6990c0c6b23078ec2c4d6
+TERMUX_PKG_SHA256=6ae85b1d81832c2b67df7330d3671a3462e39ec98328056a18a29d44c27709e5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libmnl"
 TERMUX_PKG_BREAKS="ipset-dev"
@@ -13,3 +13,8 @@ TERMUX_PKG_BUILD_DEPENDS="libtool"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-kmod=no
 "
+
+termux_step_pre_configure() {
+	# Workaround for version script error
+	LDFLAGS+=" -Wl,--undefined-version"
+}
