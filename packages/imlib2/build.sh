3635ff5eb315991d6ea5353362116f09d6b65261
Kevin Williams <admin@utermux.dev>
2024-02-04 11:31:35 +0800

    upgpkg(main/imlib2): 1.12.2
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Library that does image file loading and saving as well 
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING-PLAIN"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.12.2"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8c24d2d189c4d5ae602dbf2fc0fbb117aa923eab6c883041f0feeca4e8c6774e
+TERMUX_PKG_SHA256=cc49931a20560968a8648c9ca079085976085ea96d59a01b1e17cb55af0ffe42
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, giflib, glib, libandroid-shmem, libbz2, libcairo, libheif, libid3tag, libjpeg-turbo, libjxl, liblzma, libpng, librsvg, libtiff, libwebp, libx11, libxcb, libxext, openjpeg, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBS=-landroid-shmem"
