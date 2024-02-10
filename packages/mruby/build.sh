70c69eb7da08bc5bb350cc7512d3ec14e0dc0ef0
Jia Yuan Lo <jylo06g@gmail.com>
2024-02-10 10:43:15 +0800

    fix(main/mruby): use repology update method
    %ci:no-build

--- packages/mruby/build.sh
+++ packages/mruby/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://mruby.org/
 TERMUX_PKG_DESCRIPTION="Lightweight implementation of the Ruby language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.2.0
-TERMUX_PKG_SRCURL=https://github.com/mruby/mruby/archive/${TERMUX_PKG_VERSION}.zip
-TERMUX_PKG_SHA256=64a0dd8b65825f2e7cdac6699e818648dc73bfdfc68aa1bfd58417a35315e5cc
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+TERMUX_PKG_VERSION="3.2.0"
+TERMUX_PKG_SRCURL=https://github.com/mruby/mruby/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3c198e4a31d31fe8524013066fac84a67fe6cd6067d92c25a1c79089744cb608
 TERMUX_PKG_DEPENDS="libandroid-complex-math, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_METHOD=repology
 
 termux_step_make() {
 	export CC_FOR_TARGET="$CC"
