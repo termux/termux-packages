06e28041579d7e3a756eb9b1b98ee3bb70a2c9dc
Biswapriyo Nath <nathbappai@gmail.com>
2024-02-14 20:20:15 +0530

    osm2pgsql: Disable 32 bit CPU architectures
    upstream does not support 32 bit architecture
    https://github.com/osm2pgsql-dev/osm2pgsql/commit/389904269a1b1f246641d203abadfcd5cebb97cc

--- packages/osm2pgsql/build.sh
+++ packages/osm2pgsql/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/openstreetmap/osm2pgsql/archive/${TERMUX_PK
 TERMUX_PKG_SHA256=6b46313813b816f15ce906c04cd4108bbb05362740e0a1a8889055f4e25977d2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="boost, libbz2, libc++, libexpat, liblua54, postgresql, proj, zlib, nlohmann-json"
+TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 TERMUX_PKG_GROUPS="science"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
