41bd228232f169c92308f0c49d5a8dfaff3b8343
Biswapriyo Nath <nathbappai@gmail.com>
2024-02-17 21:00:17 +0530

    bump(main/miniupnpc): 2.2.6
    Enable auto update also

--- packages/miniupnpc/build.sh
+++ packages/miniupnpc/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://miniupnp.tuxfamily.org/
 TERMUX_PKG_DESCRIPTION="Small UPnP client library and tool to access Internet Gateway Devices"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2.5
+TERMUX_PKG_VERSION=2.2.6
 TERMUX_PKG_SRCURL=https://miniupnp.tuxfamily.org/files/miniupnpc-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=38acd5f4602f7cf8bcdc1ec30b2d58db2e9912e5d9f5350dd99b06bfdffb517c
+TERMUX_PKG_SHA256=37fcd91953508c3e62d6964bb8ffbc5d47f3e13481fa54e6214fcc68704c66f1
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="miniupnpc-dev"
 TERMUX_PKG_REPLACES="miniupnpc-dev"
 
