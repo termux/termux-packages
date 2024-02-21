fcba07397ff7e99731f9f2bbaa3096cecc6d6436
s00se <95649696+s00se@users.noreply.github.com>
2024-02-20 23:39:28 -0700

    proxmark3: include resources, dictionaries, and various scripts (#19305)

--- packages/proxmark3/build.sh
+++ packages/proxmark3/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Swiss Army Knife of RFID Research - RRG/Iceman repo"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Marlin Sööse <marlin.soose@esque.ca>"
 TERMUX_PKG_VERSION="1:4.18218"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/RfidResearchGroup/proxmark3/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=535ace3d2395d745aab82b77f7bf83ac08ab9ffb328c07ee2e4ddf340d09536a
 TERMUX_PKG_AUTO_UPDATE=true
@@ -11,12 +12,17 @@ TERMUX_PKG_BUILD_IN_SRC="true"
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, x86_64"
 
 termux_step_post_configure() {
-	export LDLIBS="-L${TERMUX_PREFIX}/lib"
-	export INCLUDES="-I${TERMUX_PREFIX}/include"
-	TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=$TERMUX_ARCH SKIPREVENGTEST=1 SKIPQT=1 SKIPPTHREAD=1 PLATFORM=PM3GENERIC DEBUG=1"
+        export LDLIBS="-L${TERMUX_PREFIX}/lib"
+        export INCLUDES="-I${TERMUX_PREFIX}/include"
+        TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=$TERMUX_ARCH SKIPREVENGTEST=1 SKIPQT=1 SKIPPTHREAD=1 SKIPGD=1 PLATFORM=PM3GENERIC"
 }
 
 termux_step_make_install() {
-	install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 \
-		"$TERMUX_PREFIX"/bin/proxmark3
+        install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 "$TERMUX_PREFIX"/bin/proxmark3
+        mkdir -p "$TERMUX_PREFIX"/share/proxmark3/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/resources/ "$TERMUX_PREFIX"/share/proxmark3/resources/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/dictionaries/ "$TERMUX_PREFIX"/share/proxmark3/dictionaries/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/pyscripts/ "$TERMUX_PREFIX"/share/proxmark3/pyscripts/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/luascripts/ "$TERMUX_PREFIX"/share/proxmark3/luascripts/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/lualibs/ "$TERMUX_PREFIX"/share/proxmark3/lualibs/
 }
