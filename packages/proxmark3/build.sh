3ed1379500ec74879fdc26e5ac672a2e974494e6
s00se <95649696+s00se@users.noreply.github.com>
2024-02-20 23:04:56 -0700

    proxmark3: update build.sh
    Include resources, dictionaries, and various scripts

--- packages/proxmark3/build.sh
+++ packages/proxmark3/build.sh
@@ -11,12 +11,17 @@ TERMUX_PKG_BUILD_IN_SRC="true"
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, x86_64"
 
 termux_step_post_configure() {
-	export LDLIBS="-L${TERMUX_PREFIX}/lib"
-	export INCLUDES="-I${TERMUX_PREFIX}/include"
-	TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=$TERMUX_ARCH SKIPREVENGTEST=1 SKIPQT=1 SKIPPTHREAD=1 PLATFORM=PM3GENERIC DEBUG=1"
+        export LDLIBS="-L${TERMUX_PREFIX}/lib"
+        export INCLUDES="-I${TERMUX_PREFIX}/include"
+        TERMUX_PKG_EXTRA_MAKE_ARGS="client CC=$CC CXX=$CXX LD=$CXX cpu_arch=$TERMUX_ARCH SKIPREVENGTEST=1 SKIPQT=1 SKIPPTHREAD=1 SKIPGD=1 PLATFORM=PM3GENERIC"
 }
 
 termux_step_make_install() {
-	install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 \
-		"$TERMUX_PREFIX"/bin/proxmark3
+        install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 "$TERMUX_PREFIX"/bin/proxmark3
+        mkdir -p "$TERMUX_PREFIX"/share/proxmark3/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/resources/ "$TERMUX_PREFIX"/share/proxmark3/resources/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/dictionaries/ "$TERMUX_PREFIX"/share/proxmark3/dictionaries/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/pyscripts/ "$TERMUX_PREFIX"/share/proxmark3/pyscripts/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/luascripts/ "$TERMUX_PREFIX"/share/proxmark3/luascripts/
+        cp -R "$TERMUX_PKG_BUILDDIR"/client/lualibs/ "$TERMUX_PREFIX"/share/proxmark3/lualibs/
 }
