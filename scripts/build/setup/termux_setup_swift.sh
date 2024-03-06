30f5e988e6a30dd68676713593cb75d039f4681b
Finagolfin <finagolfin@tuta.io>
2024-03-06 06:27:00 +0530

    bump(swift): 5.9.2 to 5.10

--- scripts/build/setup/termux_setup_swift.sh
+++ scripts/build/setup/termux_setup_swift.sh
@@ -19,7 +19,7 @@ termux_setup_swift() {
 			termux_download \
 				https://download.swift.org/swift-$TERMUX_SWIFT_VERSION-release/ubuntu2204/swift-$TERMUX_SWIFT_VERSION-$SWIFT_RELEASE/$SWIFT_BIN.tar.gz \
 				$SWIFT_TAR \
-				6407e39eed7eaefcf7837d192d71765fb0f7cf8bf282c35b021171e8b15617c1
+				ded6983736ab5e15862e5a924ebd5c4f9459802317eff719936442c832698d06
 
 			(cd $TERMUX_PKG_TMPDIR ; tar xf $SWIFT_TAR ; mv $SWIFT_BIN $SWIFT_FOLDER; rm $SWIFT_TAR)
 		fi
