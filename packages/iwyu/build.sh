c88cf4329c50529629050d3a885c0aa147974f2b
Lucy Phipps <landfillbaby69@gmail.com>
2024-03-12 22:52:08 +0000

    bump(iwyu): 0.21 to 0.22

--- packages/iwyu/build.sh
+++ packages/iwyu/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A tool to analyze #includes in C and C++ source files"
 TERMUX_PKG_LICENSE=NCSA
 TERMUX_PKG_MAINTAINER="@termux"
 # Update this when libllvm is updated:
-TERMUX_PKG_VERSION=0.21
+TERMUX_PKG_VERSION=0.22
 TERMUX_PKG_SRCURL=https://github.com/include-what-you-use/include-what-you-use/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=a472fe8587376d041585c72e5643200f8929899f787725f0ba9e5b3d3820d401
+TERMUX_PKG_SHA256=34c7636da2abe7b86580b53b762f5269e71efff460f24f17d5913c56eb99cb7c
 TERMUX_PKG_AUTO_UPDATE=false # can't be auto-updated since release correspond to clang version.
 TERMUX_PKG_DEPENDS="clang, libc++, python"
 TERMUX_PKG_BUILD_DEPENDS="libllvm-static"
