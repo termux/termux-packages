4f629ec92ba50c7faec437b7a8a9e5faaed35529
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-03-12 15:21:17 +0800

    new package: vulkan-icd-sysvk

---
+++ packages/vulkan-icd-sysvk/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/xMeM/sysvk
+TERMUX_PKG_DESCRIPTION="Android Vulkan ICD"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=23ecd775ed6fe06bb5ac0063b5f981f70c543c67
+TERMUX_PKG_VERSION="0.0.1"
+TERMUX_PKG_SRCURL=https://github.com/xMeM/sysvk/archive/$_COMMIT.zip
+TERMUX_PKG_SHA256=dc79ef44c64ca31507bda953f18b6fc61b863e05b10380f3fe8ed956dad9dbd3
+TERMUX_PKG_DEPENDS="vulkan-loader-generic"
+TERMUX_PKG_BUILD_DEPENDS="vulkan-headers"
