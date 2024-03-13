025c951b7baad567bfe921d6470b5417a22e577e
Yaksh Bariya <yakshbari4@gmail.com>
2024-03-13 11:52:57 +0530

    bump(main/abseil-cpp): 20240116.2

--- packages/abseil-cpp/build.sh
+++ packages/abseil-cpp/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Abseil C++ Common Libraries"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Do not forget to rebuild revdeps along with EVERY "major" version bump.
-TERMUX_PKG_VERSION="20230802.1"
+TERMUX_PKG_VERSION="20240116.2"
 TERMUX_PKG_SRCURL=https://github.com/abseil/abseil-cpp/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=987ce98f02eefbaf930d6e38ab16aa05737234d7afbab2d5c4ea7adbe50c28ed
+TERMUX_PKG_SHA256=733726b8c3a6d39a4120d7e45ea8b41a434cdacde401cba500f14236c49b39dc
 # updating this will break libprotobuf
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libc++"
