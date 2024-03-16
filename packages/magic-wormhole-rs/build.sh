670a4b24cff832948fcf566a71f70775ef24811b
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-16 16:00:45 +0000

    bump(main/magic-wormhole-rs): 0.6.1

--- packages/magic-wormhole-rs/build.sh
+++ packages/magic-wormhole-rs/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION=" Rust implementation of Magic Wormhole, with new feature
 TERMUX_PKG_LICENSE="EUPL-1.2"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.6.0
+TERMUX_PKG_VERSION="0.6.1"
 TERMUX_PKG_SRCURL="https://github.com/magic-wormhole/magic-wormhole.rs/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=cfa72215b1b0f519b9986523d0c41d6b3e562c41668f28ee80c23ce0aa3fe685
+TERMUX_PKG_SHA256=522db57161bb7df10feb4b0ca8dec912186f24a0974647dc4fccdd8f70649f96
 TERMUX_PKG_BUILD_IN_SRC=true
 # disable auto-update since 'remove-clipboard' patch was maually crafted for v0.6.0
 TERMUX_PKG_AUTO_UPDATE=false
