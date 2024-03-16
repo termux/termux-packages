880c458663eb1886d5380e333e80640e1392aedf
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-03-16 22:38:30 +0100

    bump(main/typst-lsp): 0.13.0 (#19500)
    Also enabled auto updates (follow up for #15975 and #19071)

--- packages/typst-lsp/build.sh
+++ packages/typst-lsp/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/nvarner/typst-lsp
 TERMUX_PKG_DESCRIPTION="Language server for Typst"
 TERMUX_PKG_LICENSE="MIT, Apache-2.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
-TERMUX_PKG_VERSION=0.12.1
+TERMUX_PKG_VERSION=0.13.0
 TERMUX_PKG_SRCURL=https://github.com/nvarner/typst-lsp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=88c2053678147e6a3a01389644892f32244317f763622d19eaf7a64fe7e7e2dc
+TERMUX_PKG_SHA256=860d56653b719402736b00ac9bc09e5e418dea2577cead30644252e85ab5d1a1
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	# We're not shipping the VS Code plugin
