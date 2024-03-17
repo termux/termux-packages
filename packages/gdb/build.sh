d6d84ea9eb1f6fe4a365669fdb7ea6e5fd57a766
Twaik Yont <twaikyont@gmail.com>
2024-03-17 10:35:10 +0200

    bump(main/gdb): 14.2

--- packages/gdb/build.sh
+++ packages/gdb/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # This package depends on libpython${TERMUX_PYTHON_VERSION}.so.
 # Please revbump and rebuild when bumping TERMUX_PYTHON_VERSION.
-TERMUX_PKG_VERSION="14.1"
+TERMUX_PKG_VERSION="14.2"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gdb/gdb-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d66df51276143451fcbff464cc8723d68f1e9df45a6a2d5635a54e71643edb80
+TERMUX_PKG_SHA256=2d4dd8061d8ded12b6c63f55e45344881e8226105f4d2a9b234040efa5ce7772
 TERMUX_PKG_DEPENDS="guile, libc++, libexpat, libgmp, libiconv, liblzma, libmpfr, libthread-db, ncurses, python, readline, zlib, zstd"
 TERMUX_PKG_BREAKS="gdb-dev"
 TERMUX_PKG_REPLACES="gdb-dev"
