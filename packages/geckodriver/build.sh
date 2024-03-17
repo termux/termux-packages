0480519af6e368f7ac0cc5b60b7382905d8f5834
Twaik Yont <twaikyont@gmail.com>
2024-03-17 10:36:36 +0200

    bump(main/geckodriver): 0.34.0

--- packages/geckodriver/build.sh
+++ packages/geckodriver/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mozilla/geckodriver
 TERMUX_PKG_DESCRIPTION="Proxy for using W3C WebDriver-compatible clients to interact with Gecko-based browsers"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.33.0
+TERMUX_PKG_VERSION="0.34.0"
 TERMUX_PKG_SRCURL=https://github.com/mozilla/geckodriver/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f796679ae35a4c32b9248da9401f058578e06eec1808f4ce0abb76e2ee64b653
+TERMUX_PKG_SHA256=8cba3459446ef94a96357c7d57c009095fff801f719cbb511e59caf17e746336
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_RECOMMENDS="firefox"
 TERMUX_PKG_BUILD_IN_SRC=true
 
