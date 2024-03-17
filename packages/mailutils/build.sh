da2884762aada46125a0471716852b7b2e1e9896
Twaik Yont <twaikyont@gmail.com>
2024-03-17 10:47:30 +0200

    bump(main/mailutils): 3.17

--- packages/mailutils/build.sh
+++ packages/mailutils/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://mailutils.org/
 TERMUX_PKG_DESCRIPTION="Mailutils is a swiss army knife of electronic mail handling. "
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
-TERMUX_PKG_MAINTAINER="@suhan-paradkar"
-TERMUX_PKG_VERSION=3.16
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="3.17"
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/mailutils/mailutils-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=041d158d308c0376184388e9c936cfa841a51cdc25d4db75984a771a3fa002c0
+TERMUX_PKG_SHA256=fa49bacec37566fe52f88221d3871673a633aeee0cd923cc3a8e65bbef2b84e9
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, libcrypt, libiconv, libltdl, libunistring, ncurses, readline"
 # Most of these configure arguments are for avoiding automagic dependencies.
 # You may instead add dependencies properly and enable them.
