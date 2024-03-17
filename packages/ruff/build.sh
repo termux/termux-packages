cf8555610238e93979dc4c4e16e71c979cc313c0
Lucy Phipps <landfillbaby69@gmail.com>
2024-03-17 04:58:55 +0000

    Update build.sh

--- packages/ruff/build.sh
+++ packages/ruff/build.sh
@@ -28,6 +28,7 @@ termux_step_pre_configure() {
 	local _patch=$TERMUX_PKG_BUILDER_DIR/tikv-jemalloc-sys-0.5.3+5.3.0-patched-src-lib.rs.diff
 	local d
 	for d in $CARGO_HOME/registry/src/*/tikv-jemalloc-sys-*; do
+ 		printf "%s\n" "$d" #DELETEME
 		patch --silent -p1 -d ${d} < ${_patch} || :
 	done
 }
