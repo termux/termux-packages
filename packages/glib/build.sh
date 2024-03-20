a0dfef48b5414b17c030aeba7f40c8a27959d848
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-20 07:33:15 +0000

    bump(main/glib): 2.80.0

--- packages/glib/build.sh
+++ packages/glib/build.sh
@@ -2,16 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://developer.gnome.org/glib/
 TERMUX_PKG_DESCRIPTION="Library providing core building blocks for libraries and applications written in C"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.78.4"
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib/${TERMUX_PKG_VERSION%.*}/glib-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=24b8e0672dca120cc32d394bccb85844e732e04fe75d18bb0573b2dbc7548f63
+TERMUX_PKG_VERSION="2.80.0"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/glib/${TERMUX_PKG_VERSION%.*}/glib-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=8228a92f92a412160b139ae68b6345bd28f24434a7b5af150ebe21ff587a561d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, libffi, libiconv, pcre2, resolv-conf, zlib"
 TERMUX_PKG_BREAKS="glib-dev"
 TERMUX_PKG_REPLACES="glib-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dintrospection=disabled
 -Druntime_dir=$TERMUX_PREFIX/var/run
 -Dlibmount=disabled
+-Dtests=false
 "
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/glib-gettextize
@@ -25,6 +27,7 @@ share/gtk-doc
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
 -Ddefault_library=static
+-Dintrospection=disabled
 -Dlibmount=disabled
 -Dtests=false
 --prefix ${TERMUX_PREFIX}/opt/${TERMUX_PKG_NAME}/cross
@@ -45,7 +48,7 @@ termux_step_host_build() {
 	termux_setup_meson
 	unset AR CC CFLAGS CPPFLAGS CXX CXXFLAGS LD LDFLAGS PKG_CONFIG STRIP
 
-	${TERMUX_MESON} ${TERMUX_PKG_SRCDIR} . \
+	${TERMUX_MESON} setup ${TERMUX_PKG_SRCDIR} . \
 		${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
 	ninja -j "${TERMUX_MAKE_PROCESSES}" install
 
