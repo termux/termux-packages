48014401537080dc670d0754e3a843929feadc30
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-20 09:41:35 +0000

    bump(main/libarrow-cpp, python-pyarrow): 15.0.2

--- packages/libarrow-cpp/build.sh
+++ packages/libarrow-cpp/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="C++ libraries for Apache Arrow"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Align the version with `python-pyarrow` package.
-TERMUX_PKG_VERSION=15.0.1
+TERMUX_PKG_VERSION="15.0.2"
 TERMUX_PKG_SRCURL=https://github.com/apache/arrow/archive/refs/tags/apache-arrow-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a8b154a2870aa998019baeb22a6514477918a5831d4760a85b516cb4c9a3402a
+TERMUX_PKG_SHA256=4735b349845bff1fe95ed11abbfed204eb092cabc37523aa13a80cb830fe5b5e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libc++, liblz4, libprotobuf, libre2, libsnappy, utf8proc, zlib, zstd"
