6e65c289eab6ce176f2be1bd165eb68eecec075d
Henrik Grimler <grimler@termux.dev>
2024-03-21 17:25:11 +0100

    updpkg(main/texlive-installer): update to 20240310
    Starting from this release we switch and install to
    $PREFIX/share/texlive/2024 instead of $PREFIX/share/texlive.

--- packages/texlive-installer/build.sh
+++ packages/texlive-installer/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.tug.org/texlive/
 TERMUX_PKG_DESCRIPTION="Wrapper around texlive's install-tl script"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=20230313
+TERMUX_PKG_VERSION=20240310
 TERMUX_PKG_SRCURL=https://ftp.math.utah.edu/pub/tex/historic/systems/texlive/${TERMUX_PKG_VERSION:0:4}/install-tl-unx.tar.gz
-TERMUX_PKG_SHA256=d97bdb3b1903428e56373e70861b24db448243d74d950cdff96f4e888f008605
+TERMUX_PKG_SHA256=fa845fbbd8d5b78c93fb5e9f97e5d908b42fb50c1ae164f7d9aa31c8ad8c31c7
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_DEPENDS="perl, texlive-bin (>= 20200406-4), gnupg, curl, lz4, xz-utils"
+TERMUX_PKG_DEPENDS="perl, texlive-bin (>= 20240310), gnupg, curl, lz4, xz-utils"
 TERMUX_PKG_REPLACES="texlive"
 TERMUX_PKG_BREAKS="texlive"
 TERMUX_PKG_RM_AFTER_INSTALL="
@@ -24,6 +24,7 @@ termux_step_make_install() {
 	chmod 700 $TERMUX_PREFIX/bin/termux-install-tl
 
 	sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+		-e "s|@YEAR@|${TERMUX_PKG_VERSION:0:4}|g" \
 		"$TERMUX_PKG_BUILDER_DIR"/termux-patch-texlive.sh \
 		> $TERMUX_PREFIX/bin/termux-patch-texlive
 	chmod 700 $TERMUX_PREFIX/bin/termux-patch-texlive
@@ -35,7 +36,8 @@ termux_step_make_install() {
 	chmod 600 $TERMUX_PREFIX/opt/texlive/termux.profile
 
 	for DIFF in "$TERMUX_PKG_BUILDER_DIR"/*.diff; do
-		sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" $DIFF \
+		sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+                    -e "s|@YEAR@|${TERMUX_PKG_VERSION:0:4}|g" $DIFF \
 			> $TERMUX_PREFIX/opt/texlive/$(basename $DIFF)
 		chmod 600 $TERMUX_PREFIX/opt/texlive/$(basename $DIFF)
 	done
@@ -48,7 +50,7 @@ termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/profile.d/
 	{
 		echo "export PATH=\$PATH:$TERMUX_PREFIX/bin/texlive"
-		echo "export TEXMFROOT=$TERMUX_PREFIX/share/texlive"
+		echo "export TEXMFROOT=$TERMUX_PREFIX/share/texlive/${TERMUX_PKG_VERSION:0:4}"
 		echo "export TEXMFLOCAL=$TERMUX_PREFIX/share/texlive/texmf-local"
 		echo "export OSFONTDIR=$TERMUX_PREFIX/share/fonts/TTF"
 		echo "export TRFONTS=$TERMUX_PREFIX/share/groff/{current/font,site-font}/devps"
@@ -64,5 +66,9 @@ termux_step_create_debscripts() {
 		echo "echo '      termux-install-tl'"
 		echo "echo ''"
 		echo "echo '    It forwards extra arguments to the install-tl script.'"
+		echo "echo ''"
+		echo "echo '    NOTE: texlive-${TERMUX_PKG_VERSION:0:4} is installed into \$PREFIX/share/texlive/${TERMUX_PKG_VERSION:0:4} instead of \$PREFIX/share/texlive'"
+		echo "echo '          If you have an old version installed you can rm -rf \$PREFIX/share/texlive/texmf-dist to'"
+		echo "echo '          delete (most of) old version and free up space.'"
 	} > ./postinst
 }
