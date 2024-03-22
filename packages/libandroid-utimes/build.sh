93004a3ef598bf95f937999615b39fc3fe966345
Henrik Grimler <grimler@termux.dev>
2024-03-22 16:46:49 +0100

    addpkg(main/libandroid-utimes): add package
    Contains futimes, futimesat and lutimes implementations from bionic.

---
+++ packages/libandroid-utimes/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/termux/libandroid-utimes
+TERMUX_PKG_DESCRIPTION="futimes, futimesat and lutimes from bionic"
+TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.2
+TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-utimes/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=fe0c8990b84f2165560ee404a3b815ed4db759412a029c051e4203aebf0d7846
+TERMUX_PKG_BUILD_IN_SRC=true
