906e087f14239204011e8a7d23dd496e8f998ddb
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-22 07:39:20 +0000

    bump(main/libwolfssl): 5.7.0
    Change SOVERSION check logic.

--- packages/libwolfssl/build.sh
+++ packages/libwolfssl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.wolfssl.com/
 TERMUX_PKG_DESCRIPTION="A small, fast, portable implementation of TLS/SSL for embedded devices to the cloud"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.6.6"
+TERMUX_PKG_VERSION="5.7.0"
 TERMUX_PKG_SRCURL=https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${TERMUX_PKG_VERSION}-stable.tar.gz
-TERMUX_PKG_SHA256=3d2ca672d41c2c2fa667885a80d6fa03c3e91f0f4f72f87aef2bc947e8c87237
+TERMUX_PKG_SHA256=2de93e8af588ee856fe67a6d7fce23fc1b226b74d710b0e3946bc8061f6aa18f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
@@ -14,12 +14,12 @@ termux_step_pre_configure() {
 	local _SOVERSION=42
 
 	local a
-	for a in LIBTOOL_CURRENT LIBTOOL_AGE; do
+	for a in WOLFSSL_LIBRARY_VERSION_FIRST; do
 		local _${a}=$(sed -En 's/^set\('"${a}"'\s+([0-9]+).*/\1/p' \
 				CMakeLists.txt)
 	done
-	local v=$(( _LIBTOOL_CURRENT - _LIBTOOL_AGE ))
-	if [ ! "${_LIBTOOL_CURRENT}" ] || [ "${v}" != "${_SOVERSION}" ]; then
+	local v=${_WOLFSSL_LIBRARY_VERSION_FIRST}
+	if [ ! "${v}" ] || [ "${v}" != "${_SOVERSION}" ]; then
 		termux_error_exit "SOVERSION guard check failed."
 	fi
 }
