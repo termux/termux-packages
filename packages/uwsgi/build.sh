adc31b26c928d05e8657e5fa9d876dd44a2c173b
Henrik Grimler <grimler@termux.dev>
2024-03-23 21:33:29 +0100

    chore(main/uwsgi): link against libandroid-utimes
    Instead of adding a sys_time.c source file with the implementation.

--- packages/uwsgi/build.sh
+++ packages/uwsgi/build.sh
@@ -3,16 +3,13 @@ TERMUX_PKG_DESCRIPTION="uWSGI application server container"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.24"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/unbit/uwsgi/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6be644f8c36379a97ca1c71c03c146af109983a58eacefbcfdaaff3c62d7edf8
-TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-sysv-semaphore, libcap, libcrypt, libjansson, libuuid, libxml2, openssl, pcre, python"
+TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-sysv-semaphore, libandroid-utimes, libcap, libcrypt, libjansson, libuuid, libxml2, openssl, pcre, python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-
-termux_step_post_get_source() {
-	cp $TERMUX_PKG_BUILDER_DIR/sys_time.c ./core/
-}
 
 termux_step_pre_configure() {
 	export UWSGI_PYTHON_NOLIB=true
@@ -25,6 +22,7 @@ termux_step_pre_configure() {
 		-lpython${TERMUX_PYTHON_VERSION}
 		-landroid-glob
 		-landroid-sysv-semaphore
+		-landroid-utimes
 		"
 }
 
