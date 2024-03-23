393c22ac28440afcbedc6b1b77067ac488eb3806
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-03-23 01:24:20 +0100

    chore(x11/hicolor-icon-theme): Set source URL (#19545)

--- x11-packages/hicolor-icon-theme/build.sh
+++ x11-packages/hicolor-icon-theme/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_DESCRIPTION="Freedesktop.org Hicolor icon theme"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.17
-TERMUX_PKG_REVISION=28
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_REVISION=29
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/default-icon-theme/-/archive/${TERMUX_PKG_VERSION}/default-icon-theme-master.tar.gz
+TERMUX_PKG_SHA256=a0bd3e5a3b0c109a2ccb5aa2d150c3b8a485de532f674c5fe3ffd18e97cf17cd
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_AUTO_UPDATE=false # No src url to update from.
+TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make_install() {
-	install -Dm644 "${TERMUX_PKG_BUILDER_DIR}/index.theme" "${TERMUX_PREFIX}/share/icons/hicolor/index.theme"
+termux_step_pre_configure() {
+	./autogen.sh
 }
