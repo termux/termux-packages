b3d730487295531d3f5906392292f9d134724ae0
Henrik Grimler <grimler@termux.dev>
2024-03-23 21:52:20 +0100

    chore(main/lnav): link against libandroid-utimes
    Instead of adding a sys_time.c source file with the implementation.

--- packages/lnav/build.sh
+++ packages/lnav/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="An advanced log file viewer for the small-scale"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.11.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tstack/lnav/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=03b72fd02faccdbf98fcdeba62306794b677b8bcf49d6023117808f88ed627dc
-TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libandroid-utimes, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-system-paths
@@ -13,12 +14,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-pcre2=$TERMUX_PREFIX
 "
 
-termux_step_post_get_source() {
-	cp $TERMUX_PKG_BUILDER_DIR/sys_time.c src/
-}
-
 termux_step_pre_configure() {
 	autoreconf -fi
 
-	LDFLAGS+=" -landroid-glob"
+	LDFLAGS+=" -landroid-glob -landroid-utimes"
 }
