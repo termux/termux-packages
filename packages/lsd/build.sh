37e012ffd3579c8de1a9024e4d0c6ca1d5b6d2d2
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-26 20:00:46 +0000

    chore(main/lsd): Remove workaround with zlib library
    This partially reverts c1563167830b077953244fe9727d7fe5d084021a commit.

--- packages/lsd/build.sh
+++ packages/lsd/build.sh
@@ -8,28 +8,3 @@ TERMUX_PKG_SHA256=cd80dae9a8f6c4c2061f79084468ea6e04c372e932e3712a165119417960e1
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	termux_setup_rust
-
-	mv $TERMUX_PREFIX/lib/libz.so.1{,.tmp}
-	mv $TERMUX_PREFIX/lib/libz.so{,.tmp}
-
-	local _CARGO_TARGET_LIBDIR="target/${CARGO_TARGET_NAME}/release/deps"
-	mkdir -p "${_CARGO_TARGET_LIBDIR}"
-
-	ln -sfT "$(readlink -f "${TERMUX_PREFIX}"/lib/libz.so.1.tmp)" \
-		"${_CARGO_TARGET_LIBDIR}"/libz.so.1
-	ln -sfT "$(readlink -f "${TERMUX_PREFIX}"/lib/libz.so.tmp)" \
-		"${_CARGO_TARGET_LIBDIR}"/libz.so
-}
-
-termux_step_post_make_install() {
-	mv $TERMUX_PREFIX/lib/libz.so.1{.tmp,}
-	mv $TERMUX_PREFIX/lib/libz.so{.tmp,}
-}
-
-termux_step_post_massage() {
-	rm -f lib/libz.so.1
-	rm -f lib/libz.so
-}
