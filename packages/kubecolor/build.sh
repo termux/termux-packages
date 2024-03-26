6779895e2e43caff9ac0ba3a36bd35e888f2335d
kalle (jag) <2477952+applejag@users.noreply.github.com>
2024-03-26 16:12:27 +0100

    addpkg(main/kubecolor): Colorize your kubectl output (#19486)
    successor to hidetatz/kubecolor.

---
+++ packages/kubecolor/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/kubecolor/kubecolor
+TERMUX_PKG_DESCRIPTION="Colorize your kubectl output"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Kalle Fagerberg @applejag"
+TERMUX_PKG_VERSION="0.2.2"
+TERMUX_PKG_SRCURL=https://github.com/kubecolor/kubecolor/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ba0894a8e26fefff47a0691529964303bdd8fdc2d7ce74e7d241cb5a2f2ade50
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kubectl"
+
+termux_step_pre_configure() {
+	termux_setup_golang
+}
+
+termux_step_make() {
+	go build -o kubecolor -ldflags "-X main.Version=v${TERMUX_PKG_VERSION}"
+}
+
+termux_step_make_install() {
+	install -Dm700 kubecolor "$TERMUX_PREFIX/bin"
+}
