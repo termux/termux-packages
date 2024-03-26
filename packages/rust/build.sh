329d6b85ecd396ed9dfd35d5cf69da29fb5d8f86
Jia Yuan Lo <jylo06g@gmail.com>
2024-03-27 04:52:20 +0800

    bump(main/rust): 1.77.0

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.76.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.77.0"
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-${TERMUX_PKG_VERSION}-src.tar.xz
-TERMUX_PKG_SHA256=805482b436442a6786d270cacbab8f00529e06141b27b7fb01909b97ce4f3464
+TERMUX_PKG_SHA256=66126989782cbf77fa3aff121bbb108429f2d46fe19328c3de231553de711b90
 _LLVM_MAJOR_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $LLVM_MAJOR_VERSION)
 _LLVM_MAJOR_VERSION_NEXT=$((_LLVM_MAJOR_VERSION + 1))
+_LZMA_VERSION=$(. $TERMUX_SCRIPTDIR/packages/liblzma/build.sh; echo $TERMUX_PKG_VERSION)
 TERMUX_PKG_DEPENDS="clang, libc++, libllvm (<< ${_LLVM_MAJOR_VERSION_NEXT}), lld, openssl, zlib"
 TERMUX_PKG_BUILD_DEPENDS="wasi-libc"
 TERMUX_PKG_NO_STATICSPLIT=true
@@ -15,6 +15,12 @@ TERMUX_PKG_RM_AFTER_INSTALL="
 bin/llc
 bin/llvm-*
 bin/opt
+bin/sh
+lib/liblzma.a
+lib/liblzma.so.${_LZMA_VERSION}
+lib/libtinfo.so.6
+lib/libz.so
+lib/libz.so.1
 share/wasi-sysroot
 "
 
@@ -25,51 +31,44 @@ termux_step_pre_configure() {
 	# default rust-std package to be installed
 	TERMUX_PKG_DEPENDS+=", rust-std-${CARGO_TARGET_NAME/_/-}"
 
-	local p="${TERMUX_PKG_BUILDER_DIR}/src-bootstrap-src-utils-cc_detect.rs.diff"
-	echo "Applying $(basename "${p}")"
+	local p="${TERMUX_PKG_BUILDER_DIR}/0001-set-TERMUX_PKG_API_LEVEL.diff"
+	echo "Applying patch: $(basename "${p}")"
 	sed "s|@TERMUX_PKG_API_LEVEL@|${TERMUX_PKG_API_LEVEL}|g" "${p}" \
 		| patch --silent -p1
 
 	export RUST_LIBDIR=$TERMUX_PKG_BUILDDIR/_lib
 	mkdir -p $RUST_LIBDIR
 
-	export LLVM_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $TERMUX_PKG_VERSION)
-	export LZMA_VERSION=$(. $TERMUX_SCRIPTDIR/packages/liblzma/build.sh; echo $TERMUX_PKG_VERSION)
-
 	# we can't use -L$PREFIX/lib since it breaks things but we need to link against libLLVM-9.so
-	ln -fst "${RUST_LIBDIR}" \
-		${TERMUX_PREFIX}/lib/libLLVM-${LLVM_VERSION/.*/}.so \
-		${TERMUX_PREFIX}/lib/libLLVM-${LLVM_VERSION}.so
+	ln -vfst "${RUST_LIBDIR}" \
+		${TERMUX_PREFIX}/lib/libLLVM-${_LLVM_MAJOR_VERSION}.so
 
 	# rust tries to find static library 'c++_shared'
-	ln -sf $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/libc++_static.a \
+	ln -vfs $TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/libc++_static.a \
 		$RUST_LIBDIR/libc++_shared.a
 
 	# https://github.com/termux/termux-packages/issues/18379
 	# NDK r26 multiple ld.lld: error: undefined symbol: __cxa_*
-	ln -fst "${RUST_LIBDIR}" "${TERMUX_PREFIX}"/lib/libc++_shared.so
+	ln -vfst "${RUST_LIBDIR}" "${TERMUX_PREFIX}"/lib/libc++_shared.so
 
 	# https://github.com/termux/termux-packages/issues/11640
 	# https://github.com/termux/termux-packages/issues/11658
 	# The build system somehow tries to link binaries against a wrong libc,
 	# leading to build failures for arm and runtime errors for others.
 	# The following command is equivalent to
-	#	ln -sft $RUST_LIBDIR \
+	#	ln -vfst $RUST_LIBDIR \
 	#		$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/lib{c,dl}.so
 	# but written in a future-proof manner.
-	ln -sft $RUST_LIBDIR $(echo | $CC -x c - -Wl,-t -shared | grep '\.so$')
+	ln -vfst $RUST_LIBDIR $(echo | $CC -x c - -Wl,-t -shared | grep '\.so$')
 
 	# rust checks libs in PREFIX/lib. It then can't find libc.so and libdl.so because rust program doesn't
 	# know where those are. Putting them temporarly in $PREFIX/lib prevents that failure
-	mv $TERMUX_PREFIX/lib/libtinfo.so.6 $TERMUX_PREFIX/lib/libtinfo.so.6.tmp
-	mv $TERMUX_PREFIX/lib/libz.so.1 $TERMUX_PREFIX/lib/libz.so.1.tmp
-	mv $TERMUX_PREFIX/lib/libz.so $TERMUX_PREFIX/lib/libz.so.tmp
-	mv $TERMUX_PREFIX/lib/liblzma.so.$LZMA_VERSION $TERMUX_PREFIX/lib/liblzma.so.tmp
-
 	# https://github.com/termux/termux-packages/issues/11427
-	# Fresh build conflict: liblzma -> rust
-	# ld: error: /data/data/com.termux/files/usr/lib/liblzma.a(liblzma_la-common.o) is incompatible with elf64-x86-64
-	mv "${TERMUX_PREFIX}"/lib/liblzma.a{,.tmp} || :
+	mv $TERMUX_PREFIX/lib/liblzma.a{,.tmp} || :
+	mv $TERMUX_PREFIX/lib/liblzma.so.${_LZMA_VERSION}{,.tmp} || :
+	mv $TERMUX_PREFIX/lib/libtinfo.so.6{,.tmp} || :
+	mv $TERMUX_PREFIX/lib/libz.so.1{,.tmp} || :
+	mv $TERMUX_PREFIX/lib/libz.so{,.tmp} || :
 }
 
 termux_step_configure() {
@@ -81,9 +80,15 @@ termux_step_configure() {
 
 	# upstream tests build using versions N and N-1
 	local BOOTSTRAP_VERSION="${TERMUX_PKG_VERSION}"
-	rustup install $BOOTSTRAP_VERSION
+	if rustup install $BOOTSTRAP_VERSION; then
 	rustup default $BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu
 	export PATH=$HOME/.rustup/toolchains/$BOOTSTRAP_VERSION-x86_64-unknown-linux-gnu/bin:$PATH
+	else
+	echo "WARN: $BOOTSTRAP_VERSION is unavailable, fallback to stable version!"
+	rustup install stable
+	rustup default stable-x86_64-unknown-linux-gnu
+	export PATH=$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin:$PATH
+	fi
 	local RUSTC=$(command -v rustc)
 	local CARGO=$(command -v cargo)
 
@@ -122,11 +127,17 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	unset CC CXX CPP LD CFLAGS CXXFLAGS CPPFLAGS LDFLAGS PKG_CONFIG RANLIB
+	unset CC CFLAGS CPP CPPFLAGS CXX CXXFLAGS LD LDFLAGS PKG_CONFIG RANLIB
 
 	# remove version suffix: beta, nightly
 	local TERMUX_PKG_VERSION=${TERMUX_PKG_VERSION//~*}
 
+	if ! :; then
+	# speed up building rust for testing
+	$TERMUX_PKG_SRCDIR/x.py install --stage 1 --target $CARGO_TARGET_NAME
+	$TERMUX_PKG_SRCDIR/x.py dist rustc-dev --host $CARGO_TARGET_NAME --target $CARGO_TARGET_NAME
+	else
+	# otherwise always build all the supported targets
 	$TERMUX_PKG_SRCDIR/x.py install --stage 1 --target aarch64-linux-android
 	$TERMUX_PKG_SRCDIR/x.py install --stage 1 --target armv7-linux-androideabi
 	$TERMUX_PKG_SRCDIR/x.py install --stage 1 --target i686-linux-android
@@ -140,23 +151,25 @@ termux_step_make_install() {
 	$TERMUX_PKG_SRCDIR/x.py dist rustc-dev --host $CARGO_TARGET_NAME --target x86_64-linux-android
 	$TERMUX_PKG_SRCDIR/x.py dist rustc-dev --host $CARGO_TARGET_NAME --target wasm32-unknown-unknown
 	$TERMUX_PKG_SRCDIR/x.py dist rustc-dev --host $CARGO_TARGET_NAME --target wasm32-wasi
+	fi
 
-	tar xvf build/dist/rustc-dev-$TERMUX_PKG_VERSION-$CARGO_TARGET_NAME.tar.gz
+	tar -xvf build/dist/rustc-dev-$TERMUX_PKG_VERSION-$CARGO_TARGET_NAME.tar.gz
 	./rustc-dev-$TERMUX_PKG_VERSION-$CARGO_TARGET_NAME/install.sh --prefix=$TERMUX_PREFIX
 
 	cd "$TERMUX_PREFIX/lib"
 	rm -f libc.so libdl.so
-	mv $TERMUX_PREFIX/lib/libtinfo.so.6.tmp $TERMUX_PREFIX/lib/libtinfo.so.6
-	mv $TERMUX_PREFIX/lib/libz.so.1.tmp $TERMUX_PREFIX/lib/libz.so.1
-	mv $TERMUX_PREFIX/lib/libz.so.tmp $TERMUX_PREFIX/lib/libz.so
-	mv $TERMUX_PREFIX/lib/liblzma.so.tmp $TERMUX_PREFIX/lib/liblzma.so.$LZMA_VERSION
-	mv "${TERMUX_PREFIX}"/lib/liblzma.a{.tmp,} || :
+	mv liblzma.a{.tmp,} || :
+	mv liblzma.so.${_LZMA_VERSION}{.tmp,} || :
+	mv libtinfo.so.6{.tmp,} || :
+	mv libz.so.1{.tmp,} || :
+	mv libz.so{.tmp,} || :
 
-	ln -fs rustlib/${CARGO_TARGET_NAME}/lib/*.so .
-	ln -fs lld ${TERMUX_PREFIX}/bin/rust-lld
+	ln -vfs rustlib/${CARGO_TARGET_NAME}/lib/*.so .
+	ln -vfs lld ${TERMUX_PREFIX}/bin/rust-lld
 
 	cd "$TERMUX_PREFIX/lib/rustlib"
-	rm -rf components \
+	rm -fr \
+		components \
 		install.log \
 		uninstall.sh \
 		rust-installer-version \
@@ -181,12 +194,12 @@ termux_step_make_install() {
 	local _included=$(echo "${_included}" | grep -v '\.rlib$')
 	local _included=$(echo "${_included}" | grep -v '\.so$')
 
-	echo _rlib
+	echo "INFO: _rlib"
 	while IFS= read -r _rlib; do
 		echo "${_rlib}"
 		local _included_rlib=$(echo "${_included_rlib}" | grep -v "${_rlib}")
 	done < "${TERMUX_PKG_BUILDDIR}/rustlib-rlib.txt"
-	echo _so
+	echo "INFO: _so"
 	while IFS= read -r _so; do
 		echo "${_so}"
 		local _included_so=$(echo "${_included_so}" | grep -v "${_so}")
@@ -194,12 +207,4 @@ termux_step_make_install() {
 
 	export _INCLUDED=$(echo -e "${_included}\n${_included_rlib}\n${_included_so}")
 	echo -e "INFO: _INCLUDED:\n${_INCLUDED}"
-}
-
-termux_step_post_massage() {
-	rm -f lib/libtinfo.so.6
-	rm -f lib/libz.so
-	rm -f lib/libz.so.1
-	rm -f lib/liblzma.so.$LZMA_VERSION
-	rm -f lib/liblzma.a
 }
