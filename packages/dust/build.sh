0ca3bde53d30552c33792bc638dbb3368a70475c
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-27 02:15:51 +0530

    chore(main/dust): Install shell completions and man page (#19580)

--- packages/dust/build.sh
+++ packages/dust/build.sh
@@ -3,7 +3,15 @@ TERMUX_PKG_DESCRIPTION="A more intuitive version of du in rust"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/bootandy/dust/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=34b72116ab6db9bdb97bc1e49dadf392a1619838204b44b0a4695539d54ffbe8
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_make_install() {
+	install -Dm644 "completions/${TERMUX_PKG_NAME}.bash" "${TERMUX_PREFIX}/share/bash-completion/completions/${TERMUX_PKG_NAME}"
+	install -Dm644 "completions/${TERMUX_PKG_NAME}.fish" "${TERMUX_PREFIX}/share/fish/vendor_completions.d/${TERMUX_PKG_NAME}.fish"
+	install -Dm644 "completions/_${TERMUX_PKG_NAME}" "${TERMUX_PREFIX}/share/zsh/site-functions/_${TERMUX_PKG_NAME}"
+	install -Dm644 "man-page/${TERMUX_PKG_NAME}.1" "${TERMUX_PREFIX}/share/man/man1/${TERMUX_PKG_NAME}.1"
+}
