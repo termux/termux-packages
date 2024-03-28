adf86c331736620ff3b83487901f084696d0f971
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-03-29 00:32:50 +0100

    bump(main/llvm-mingw-w64{,-ucrt,-libcompiler-rt}): (#19582)
    Update to `20240320`, and rebuild against LLVM 18
    closes #19579

--- packages/llvm-mingw-w64-libcompiler-rt/build.sh
+++ packages/llvm-mingw-w64-libcompiler-rt/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Compiler runtime libraries for LLVM-MinGW"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@licy183"
 # Bump llvm-mingw-w64* to the same version in one PR.
-TERMUX_PKG_VERSION="20231031"
+TERMUX_PKG_VERSION="20240320"
 TERMUX_PKG_SRCURL=https://github.com/mstorsjo/llvm-mingw/releases/download/$TERMUX_PKG_VERSION/llvm-mingw-$TERMUX_PKG_VERSION-ucrt-ubuntu-20.04-x86_64.tar.xz
-TERMUX_PKG_SHA256=95a4fb16425ce3234a2bf94c6d7c6c1fbf6833a4fa80d84c82ccf2ad533f0737
+TERMUX_PKG_SHA256=45461bc1634fda1d1ceaa09a476a3a279454d4e237c2d1723787c9be66e57141
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_NO_STATICSPLIT=true
@@ -13,8 +13,9 @@ TERMUX_PKG_NO_STATICSPLIT=true
 termux_step_make_install() {
 	# Install compier-rt libraries
 	local LLVM_MAJOR_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $LLVM_MAJOR_VERSION)
+	rm -rf $TERMUX_PREFIX/lib/clang/$LLVM_MAJOR_VERSION/lib/windows
 	mkdir -p $TERMUX_PREFIX/lib/clang/$LLVM_MAJOR_VERSION/lib/windows
-	mv $TERMUX_PKG_SRCDIR/lib/clang/*/lib/windows $TERMUX_PREFIX/lib/clang/$LLVM_MAJOR_VERSION/lib/
+	mv $TERMUX_PKG_SRCDIR/lib/clang/$LLVM_MAJOR_VERSION/lib/windows $TERMUX_PREFIX/lib/clang/$LLVM_MAJOR_VERSION/lib/
 }
 
 termux_step_install_license() {
