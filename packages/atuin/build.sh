ccdce409e5d060d53ae5233094a640f42bcbecc9
Biswapriyo Nath <nathbappai@gmail.com>
2024-03-30 21:19:24 +0530

    chore(main/atuin): Generate shell completions at install time (#19597)
    This removes the host build step and reduces total package build time.

--- packages/atuin/build.sh
+++ packages/atuin/build.sh
@@ -4,11 +4,11 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="18.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ellie/atuin/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=17712bed6528a7f82cc1dffd56b7effe28270ee2f99247908d7a6adff9474338
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_HOSTBUILD=true
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+="/atuin"
@@ -18,27 +18,17 @@ termux_step_pre_configure() {
 	export RUSTFLAGS="${RUSTFLAGS:-} -C link-args=$($CC -print-libgcc-file-name)"
 }
 
-termux_step_host_build() {
-	export CC=""
-	export CFLAGS=""
-	export CPPFLAGS=""
-	termux_setup_rust
-
-	cd "$TERMUX_PKG_SRCDIR"
-	cargo build \
-		--jobs $TERMUX_MAKE_PROCESSES \
-		--locked \
-		--target-dir $TERMUX_PKG_HOSTBUILD_DIR
+termux_step_post_make_install() {
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/bash-completion/completions/atuin
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/zsh/site-functions/_atuin
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/fish/vendor_completions.d/atuin.fish
 }
 
-termux_step_post_make_install() {
-	# Generate and install shell completions
-	mkdir completions/
-	for sh in 'bash' 'fish' 'zsh'; do
-		$TERMUX_PKG_HOSTBUILD_DIR/debug/atuin gen-completions -s $sh -o completions/
-	done
-
-	install -Dm600 completions/atuin.bash $TERMUX_PREFIX/share/bash-completion/completions/atuin.bash
-	install -Dm600 completions/_atuin $TERMUX_PREFIX/share/zsh/site-functions/_atuin
-	install -Dm600 completions/atuin.fish $TERMUX_PREFIX/share/fish/vendor_completions.d/atuin.fish
+termux_step_create_debscripts() {
+	cat <<-EOF >./postinst
+		#!${TERMUX_PREFIX}/bin/sh
+		atuin gen-completions -s bash > ${TERMUX_PREFIX}/share/bash-completion/completions/atuin
+		atuin gen-completions -s zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_atuin
+		atuin gen-completions -s fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/atuin.fish
+	EOF
 }
