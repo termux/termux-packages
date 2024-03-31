e139795b2f1b88b390ec5e4fedf97788e6255e55
Henrik Grimler <grimler@termux.dev>
2024-03-31 23:12:49 +0200

    updpkg(main/openjdk-21): add package
    Based on tag jdk-21+26.
    
    Replaces openjdk-17.

--- packages/openjdk-21/build.sh
+++ packages/openjdk-21/build.sh
@@ -2,14 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/openjdk/mobile
 TERMUX_PKG_DESCRIPTION="Java development kit and runtime"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=17.0
-TERMUX_PKG_REVISION=32
-TERMUX_PKG_SRCURL=git+https://github.com/termux/openjdk-mobile-termux
-TERMUX_PKG_GIT_BRANCH=termux/jdk-17+35
+TERMUX_PKG_VERSION=21.0
+TERMUX_PKG_SRCURL=git+https://github.com/openjdk-mobile-termux
+TERMUX_PKG_GIT_BRANCH=termux/jdk-21
 TERMUX_PKG_DEPENDS="libiconv, libjpeg-turbo, zlib, libandroid-spawn"
 TERMUX_PKG_BUILD_DEPENDS="cups, libandroid-spawn, libandroid-shmem, xorgproto"
-# openjdk-17-x is recommended because X11 separation is still very experimental.
-TERMUX_PKG_RECOMMENDS="ca-certificates-java, openjdk-17-x, resolv-conf"
+# openjdk-21-x is recommended because X11 separation is still very experimental.
+TERMUX_PKG_RECOMMENDS="ca-certificates-java, openjdk-21-x, resolv-conf"
 TERMUX_PKG_SUGGESTS="cups"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HAS_DEBUG=false
@@ -19,7 +18,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_configure() {
-	local jdk_ldflags="-L${TERMUX_PREFIX}/lib -Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-17-openjdk/lib -Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
+	local jdk_ldflags="-L${TERMUX_PREFIX}/lib -Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-21-openjdk/lib -Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
 	bash ./configure \
 		--disable-precompiled-headers \
 		--disable-warnings-as-errors \
@@ -62,42 +61,42 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	mkdir -p $TERMUX_PREFIX/lib/jvm/java-17-openjdk
+	mkdir -p $TERMUX_PREFIX/lib/jvm/java-21-openjdk
 	cp -r build/linux-${TERMUX_ARCH/i686/x86}-server-release/images/jdk/* \
-		$TERMUX_PREFIX/lib/jvm/java-17-openjdk/
-	find $TERMUX_PREFIX/lib/jvm/java-17-openjdk/ -name "*.debuginfo" -delete
+		$TERMUX_PREFIX/lib/jvm/java-21-openjdk/
+	find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/ -name "*.debuginfo" -delete
 
 	# Dependent projects may need JAVA_HOME.
-	mkdir -p $TERMUX_PREFIX/lib/jvm/java-17-openjdk/etc/profile.d
-	echo "export JAVA_HOME=$TERMUX_PREFIX/lib/jvm/java-17-openjdk/" > \
-		$TERMUX_PREFIX/lib/jvm/java-17-openjdk/etc/profile.d/java.sh
+	mkdir -p $TERMUX_PREFIX/lib/jvm/java-21-openjdk/etc/profile.d
+	echo "export JAVA_HOME=$TERMUX_PREFIX/lib/jvm/java-21-openjdk/" > \
+		$TERMUX_PREFIX/lib/jvm/java-21-openjdk/etc/profile.d/java.sh
 }
 
 termux_step_post_make_install() {
-	cd $TERMUX_PREFIX/lib/jvm/java-17-openjdk/man/man1
+	cd $TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1
 	for manpage in *.1; do
 		gzip "$manpage"
 	done
 }
 
 termux_step_create_debscripts() {
-	local binaries="$(find $TERMUX_PREFIX/lib/jvm/java-17-openjdk/bin -executable -type f | xargs -I{} basename "{}" | xargs echo)"
-	local manpages="$(find $TERMUX_PREFIX/lib/jvm/java-17-openjdk/man/man1 -name "*.1.gz" | xargs -I{} basename "{}" | xargs echo)"
+	local binaries="$(find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/bin -executable -type f | xargs -I{} basename "{}" | xargs echo)"
+	local manpages="$(find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1 -name "*.1.gz" | xargs -I{} basename "{}" | xargs echo)"
 	cat <<-EOF >./postinst
 		#!$TERMUX_PREFIX/bin/sh
 		if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" = "configure" ] || [ "\$1" = "abort-upgrade" ]; then
 			if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-				update-alternatives --install $TERMUX_PREFIX/etc/profile.d/java.sh java-profile	$TERMUX_PREFIX/lib/jvm/java-17-openjdk/etc/profile.d/java.sh 40
+				update-alternatives --install $TERMUX_PREFIX/etc/profile.d/java.sh java-profile	$TERMUX_PREFIX/lib/jvm/java-21-openjdk/etc/profile.d/java.sh 40
 				for tool in $binaries; do
 					update-alternatives --install \
 						$TERMUX_PREFIX/bin/\$tool \$tool \
-						$TERMUX_PREFIX/lib/jvm/java-17-openjdk/bin/\$tool 40
+						$TERMUX_PREFIX/lib/jvm/java-21-openjdk/bin/\$tool 40
 				done
 
 				for manpage in $manpages; do
 					update-alternatives --install \
-						$TERMUX_PREFIX/share/man/man1/\$manpage \$manpage \
-						$TERMUX_PREFIX/lib/jvm/java-17-openjdk/man/man1/\$manpage 60
+						$TERMUX_PREFIX/share/man/man1/\$manpage.gz \$manpage \
+						$TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1/\$manpage.gz 60
 				done
 			fi
 		fi
@@ -113,7 +112,7 @@ termux_step_create_debscripts() {
 				done
 
 				for manpage in $manpages; do
-					update-alternatives --remove \$manpage $TERMUX_PREFIX/share/man/man1/\$manpage
+					update-alternatives --remove \$manpage $TERMUX_PREFIX/share/man/man1/\$manpage.gz
 				done
 			fi
 		fi
