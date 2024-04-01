6fe7438b367a81654079a55baf1ac05fddf21349
agnostic-apollo <agnosticapollo@gmail.com>
2024-04-01 10:38:37 +0500

    fix(scripts): fix building a package from a local git repository set in `$TERMUX_PKG_SRCURL` if its in the format `git+file:///path/to/source/git/dir`
    This got broken a while ago due to changes in pulling sources, although current format wasn't mandated.
    
    The `$TERMUX_PKG_SRCURL` must start with `git+` prefix like already used for other git `http` urls, followed by a `file://` url, where `/path/to/source/git/dir` is an absolute and normalized path to a source git directory on the local file system, with a `.git` sub directory.
    
    If the source directory has been cloned already in a previous build, then it will NOT be cloned again if `-f/-F` flags are passed for rebuilds, and the `-r` flag (to be added later) will be required to clone latest sources again/every time.
    
    Any uncommitted changes to `git` branch will NOT get built.
    
    An additional requirement is that the local git repository must have its `origin` url in `.git/config` as a `https` URL instead of a `ssh` (`git@`) URL if running in termux-packages docker container and `$TERMUX_PKG_GIT_BRANCH` is set, as it doesn't have `ssh` installed by default and `git fetch` would fail otherwise. So if local git repository needs to be cloned from an upstream git url itself, like GitHub, then use `https://github.com/org/repo.git` to clone instead of `git@github.com:org/repo.git`. Or you can install `ssh` inside docker and set up ssh keys manually.

--- scripts/build/get_source/termux_git_clone_src.sh
+++ scripts/build/get_source/termux_git_clone_src.sh
@@ -1,16 +1,50 @@
 termux_git_clone_src() {
 	local TMP_CHECKOUT=$TERMUX_PKG_CACHEDIR/tmp-checkout
 	local TMP_CHECKOUT_VERSION=$TERMUX_PKG_CACHEDIR/tmp-checkout-version
+	local termux_pkg_srcurl="${TERMUX_PKG_SRCURL:4}"
+	local termux_pkg_local_srcpath=""
+	local termux_pkg_branch_flags=""
+
+	if [[ "$termux_pkg_srcurl" =~ ^file://(/[^/]+)+$ ]]; then
+		termux_pkg_local_srcpath="${termux_pkg_srcurl:7}" # Remove `file://` prefix
+
+		if [ ! -d "$termux_pkg_local_srcpath" ]; then
+			echo "No source directory found at path of TERMUX_PKG_SRCURL '$TERMUX_PKG_SRCURL' of package '$TERMUX_PKG_NAME'"
+			return 1
+		elif [ ! -d "$termux_pkg_local_srcpath/.git" ]; then
+			echo "The source directory at path of TERMUX_PKG_SRCURL '$TERMUX_PKG_SRCURL' of package '$TERMUX_PKG_NAME' does not a contain a '.git' sub directory"
+			return 1
+		fi
+	fi
 
 	if [ ! -f $TMP_CHECKOUT_VERSION ] || [ "$(cat $TMP_CHECKOUT_VERSION)" != "$TERMUX_PKG_VERSION" ]; then
-		if [ "$TERMUX_PKG_GIT_BRANCH" == "" ]; then
-			TERMUX_PKG_GIT_BRANCH=v${TERMUX_PKG_VERSION#*:}
+		if [[ -n "$termux_pkg_local_srcpath" ]]; then
+			if [ "$TERMUX_PKG_GIT_BRANCH" != "" ]; then
+				# The local git repository that needs to be cloned may
+				# not have a branch created that is tracking its remote
+				# branch, so we create it if it doesn't exist without
+				# checking it out, otherwise when we clone below,
+				# git will fail to find the branch in its own origin
+				# i.e the local git repository, as it will not look
+				# into the origin of the local git repository recursively.
+				(cd "$termux_pkg_local_srcpath" && git fetch origin $TERMUX_PKG_GIT_BRANCH:$TERMUX_PKG_GIT_BRANCH)
+				termux_pkg_branch_flags="--branch $TERMUX_PKG_GIT_BRANCH"
+			fi
+		else
+			if [ "$TERMUX_PKG_GIT_BRANCH" == "" ]; then
+				termux_pkg_branch_flags="--branch v${TERMUX_PKG_VERSION#*:}"
+			else
+				termux_pkg_branch_flags="--branch $TERMUX_PKG_GIT_BRANCH"
+			fi
 		fi
 
+		echo "Downloading git source $([[ "$termux_pkg_branch_flags" != "" ]] && echo "with branch '${termux_pkg_branch_flags:9}' ")from '$termux_pkg_srcurl'"
+
 		rm -rf $TMP_CHECKOUT
-		git clone --depth 1 \
-			--branch $TERMUX_PKG_GIT_BRANCH \
-			${TERMUX_PKG_SRCURL:4} \
+		git clone \
+			--depth 1 \
+			$termux_pkg_branch_flags \
+			"$termux_pkg_srcurl" \
 			$TMP_CHECKOUT
 
 		pushd $TMP_CHECKOUT
@@ -38,6 +72,8 @@ termux_git_clone_src() {
 		popd
 
 		echo "$TERMUX_PKG_VERSION" > $TMP_CHECKOUT_VERSION
+	else
+		echo "Skipped downloading of git source from '$termux_pkg_srcurl'"
 	fi
 
 	rm -rf $TERMUX_PKG_SRCDIR
