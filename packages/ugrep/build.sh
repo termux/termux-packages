34df19c5cefafe026bb7d9342513159646b5d329
Lucy Phipps <landfillbaby69@gmail.com>
2024-04-05 06:18:34 +0100

    ugrep 5.1.3

--- packages/ugrep/build.sh
+++ packages/ugrep/build.sh
@@ -2,19 +2,19 @@ TERMUX_PKG_HOMEPAGE="https://github.com/Genivia/ugrep"
 TERMUX_PKG_DESCRIPTION="A faster, user-friendly and compatible grep replacement"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.1.2"
+TERMUX_PKG_VERSION="5.1.3"
 TERMUX_PKG_SRCURL="https://github.com/Genivia/ugrep/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=24bac5053e994ce55ad1f587cf9333bedfe6e073412b835111969c8f4878131a
+TERMUX_PKG_SHA256=a349abb1a619e33dddbf105528bc5ba5f8d97b4f7b2c69b85d432853df3a9aee
 TERMUX_PKG_DEPENDS="libbz2, libc++, liblz4, liblzma, pcre2, zlib, zstd"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-pcre2
---enable-zlib
---enable-bzip2
---enable-lzma
---enable-lz4
---enable-zstd
+--with-pcre2=$TERMUX_PREFIX/include
+--with-zlib=$TERMUX_PREFIX/include
+--with-bzlib=$TERMUX_PREFIX/include
+--with-lzma=$TERMUX_PREFIX/include
+--with-lz4=$TERMUX_PREFIX/include
+--with-zstd=$TERMUX_PREFIX/include
 --includedir=$TERMUX_PREFIX/include
 --disable-sse2
 --disable-avx2
