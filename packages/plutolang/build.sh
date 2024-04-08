23e964b5ef32ca3076a30dbec87df3fa8167975e
komothecat <71205197+komothecat@users.noreply.github.com>
2024-04-08 11:26:07 +0800

    bump(main/plutolang): 0.9.0

--- packages/plutolang/build.sh
+++ packages/plutolang/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://plutolang.github.io/
 TERMUX_PKG_DESCRIPTION="A superset of Lua 5.4, with unique features, optimizations, and improvements"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.8.2"
+TERMUX_PKG_MAINTAINER="Komo @cattokomo"
+TERMUX_PKG_VERSION="0.9.0"
 TERMUX_PKG_SRCURL=https://github.com/PlutoLang/Pluto/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6d4c1f1348182fd96af43dd7f477c5b51fb793da80f064f9c29ac3ab5dc42854
+TERMUX_PKG_SHA256=1401cc2e25c9e8e23c9d30bd4ed83be48d48967a01f0cea3961ecaef4f97be8e
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_DEPENDS="libc++,readline"
 TERMUX_PKG_BUILD_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
@@ -19,7 +19,8 @@ INSTALL_INC=$TERMUX_PREFIX/include/pluto
 termux_step_pre_configure() {
 	CXXFLAGS+=" -std=c++17"
 	export MYCFLAGS="-fPIC $CXXFLAGS $CPPFLAGS"
-	export MYLDFLAGS="$LDFLAGS"
+	export MYLDFLAGS="-fstack-protector-strong $LDFLAGS"
+	export TERMUX_ARCH
 
 	TERMUX_PKG_EXTRA_MAKE_ARGS+=" CXX=$(command -v $CXX)"
 }
