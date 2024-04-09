22d87052197198b62b88c82a59907911aae1d815
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-09 14:18:50 +0000

    bump(main/nudoku): 3.0.0

--- packages/nudoku/build.sh
+++ packages/nudoku/build.sh
@@ -2,12 +2,16 @@ TERMUX_PKG_HOMEPAGE=http://jubalh.github.io/nudoku/
 TERMUX_PKG_DESCRIPTION="ncurses based sudoku game"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.1.0
+TERMUX_PKG_VERSION="3.0.0"
 TERMUX_PKG_SRCURL=https://github.com/jubalh/nudoku/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=eeff7f3adea5bfe7b88bf7683d68e9a597aabd1442d1621f21760c746400b924
-TERMUX_PKG_DEPENDS="ncurses"
+TERMUX_PKG_SHA256=56c9f8d70ca350411dccfdc5a0e2dc39aaa83da08f87ad874f7f4b7fb64b3541
+TERMUX_PKG_DEPENDS="libcairo, ncurses"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_GROUPS="games"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-cairo
+"
 
 termux_step_pre_configure() {
 	autoreconf -i
