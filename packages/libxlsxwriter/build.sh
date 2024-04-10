a82c835ba4f321d1c083b4620e388029cd4720b2
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-10 20:21:16 +0530

    bump(main/libxlsxwriter): 1.1.7 (#19690)
    * bump(main/libxlsxwriter): 1.1.7
    
    Remove CMakeLists.txt.patch file which was added in upstream.
    https://github.com/jmcnamara/libxlsxwriter/commit/f477741dd3782101eefb35f9c6f9ed93ee3f642d
    
    * rebuild(main/sc-im): link with libxlsxwriter 1.1.7

--- packages/libxlsxwriter/build.sh
+++ packages/libxlsxwriter/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="A C library for creating Excel XLSX files"
 TERMUX_PKG_LICENSE="BSD 2-Clause, BSD 3-Clause, ZLIB, MPL-2.0, MIT, Public Domain"
 TERMUX_PKG_LICENSE_FILE="License.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.5
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.1.7"
 TERMUX_PKG_SRCURL=https://github.com/jmcnamara/libxlsxwriter/archive/refs/tags/RELEASE_${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=12843587d591cf679e6ec63ecc629245befec2951736804a837696cdb5d61946
+TERMUX_PKG_SHA256=1f378e25d8bb5be258d3e04d3d24b8c23ff21bf206e6e206661844a96ca25eda
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+.\d+.\d+"
 TERMUX_PKG_DEPENDS="libminizip"
@@ -18,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=5
+	local _SOVERSION=6
 
 	local v=$(sed -En 's/.*LXW_SOVERSION .*"(.*)".*/\1/p' \
 			include/xlsxwriter.h)
