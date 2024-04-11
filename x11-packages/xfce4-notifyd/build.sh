7012b6fda526dbc31b5fb0e53e1aabc4e494086a
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-11 09:32:36 +0000

    bump(x11/xfce4-notifyd): 0.9.4

--- x11-packages/xfce4-notifyd/build.sh
+++ x11-packages/xfce4-notifyd/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/notifyd/start
 TERMUX_PKG_DESCRIPTION="simple, visually-appealing notification daemon for Xfce"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
-TERMUX_PKG_VERSION="0.9.3"
+TERMUX_PKG_VERSION="0.9.4"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-notifyd/${TERMUX_PKG_VERSION%.*}/xfce4-notifyd-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=79ee4701e2f8715a700de2431aa33682933cab18d76938bb18e2820302bbe030
+TERMUX_PKG_SHA256=ae6c128c055c44bd07202f73ae69ad833c5e4754f3530696965136e4d9ea7818
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libnotify, libsqlite, libx11, libxfce4ui, libxfce4util, pango, xfce4-panel, xfconf, zlib"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
