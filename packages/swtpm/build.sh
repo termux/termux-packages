6a042ff0e0534451f5d52c92d891b6604547c29f
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-12 06:28:09 +0000

    bump(main/swtpm): 0.8.2
    Remove configure.ac.patch file because the required tools are required
    for testing. Tests are disabled using '--disable-tests` option which
    was added in the following commit.
    https://github.com/stefanberger/swtpm/commit/d258668d51df83563324a3364ee99673221b4c43

--- packages/swtpm/build.sh
+++ packages/swtpm/build.sh
@@ -3,15 +3,16 @@ TERMUX_PKG_DESCRIPTION="Software TPM Emulator"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.8.1"
+TERMUX_PKG_VERSION="0.8.2"
 TERMUX_PKG_SRCURL=https://github.com/stefanberger/swtpm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7bba52aa41090f75087034fac5fe8daed10c3e7e7234df7c9558849318927f41
+TERMUX_PKG_SHA256=b281b4e8c39f68e1928675f788abc70072e4081152e129144359a8c5c304c06b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, json-glib, libseccomp, libtpms, openssl"
 TERMUX_PKG_BUILD_DEPENDS="libtasn1"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-openssl
 --without-gnutls
+--disable-tests
 "
 
 termux_step_pre_configure() {
