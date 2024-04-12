192bbceb844e996eced6db92beda00365a4006ae
Twaik Yont <twaikyont@gmail.com>
2024-04-12 13:32:50 +0300

    bump(x11/termux-x11-nightly): updating the loader to latest version

--- x11-packages/termux-x11-nightly/build.sh
+++ x11-packages/termux-x11-nightly/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Termux X11 add-on application. Still in early developmen
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Twaik Yont @twaik"
 TERMUX_PKG_VERSION=1.03.00
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-x11/archive/a50f18ef2d148a299387435edbde8fe690e9c504.tar.gz
+TERMUX_PKG_REVISION=4
+TERMUX_PKG_SRCURL=https://github.com/termux/termux-x11/archive/ecd3a3e292baec2ea76d95527ae7db654df93141.tar.gz
 TERMUX_PKG_SHA256=040ac1d57f610263d374b105ee7c5cd20099f98e3d8e43cb9bd203b803a93684
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -52,6 +52,7 @@ termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/bin
 	mkdir -p $TERMUX_PREFIX/libexec/termux-x11
 	cp $TERMUX_PKG_SRCDIR/termux-x11 $TERMUX_PREFIX/bin/termux-x11
+	cp $TERMUX_PKG_SRCDIR/termux-x11-preference $TERMUX_PREFIX/bin/termux-x11-preference
 	cp $TERMUX_PKG_SRCDIR/shell-loader/build/outputs/apk/debug/shell-loader-debug.apk \
 		$TERMUX_PREFIX/libexec/termux-x11/loader.apk
 }
