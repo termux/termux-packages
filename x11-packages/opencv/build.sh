0b8de1c1066807ca33fe1d1e830cc256c3f89890
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-13 15:03:02 +0000

    enhance(x11/opencv): Enable opencv_contrib modules

--- x11-packages/opencv/build.sh
+++ x11-packages/opencv/build.sh
@@ -4,9 +4,10 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.9.0"
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_SRCURL=https://github.com/opencv/opencv/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ddf76f9dffd322c7c3cb1f721d0887f62d747b82059342213138dc190f28bc6c
+TERMUX_PKG_SRCURL=(https://github.com/opencv/opencv/archive/${TERMUX_PKG_VERSION}/opencv-${TERMUX_PKG_VERSION}.tar.gz)
+TERMUX_PKG_SRCURL+=(https://github.com/opencv/opencv_contrib/archive/${TERMUX_PKG_VERSION}/opencv_contrib-${TERMUX_PKG_VERSION}.tar.gz)
+TERMUX_PKG_SHA256=(ddf76f9dffd322c7c3cb1f721d0887f62d747b82059342213138dc190f28bc6c)
+TERMUX_PKG_SHA256+=(8952c45a73b75676c522dd574229f563e43c271ae1d5bbbd26f8e2b6bc1a4dae)
 TERMUX_PKG_DEPENDS="abseil-cpp, ffmpeg, libc++, libjpeg-turbo, libopenblas, libpng, libtiff, libwebp, openjpeg, openjpeg-tools, qt5-qtbase, zlib"
 # For static libprotobuf see
 # https://github.com/termux/termux-packages/issues/16979
@@ -21,6 +22,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_PROTOBUF=OFF
 -DBUILD_TESTS=OFF
 -DPROTOBUF_UPDATE_FILES=ON
+-DOPENCV_EXTRA_MODULES_PATH=$TERMUX_PKG_SRCDIR/opencv_contrib-$TERMUX_PKG_VERSION/modules \
 -DOPENCV_GENERATE_PKGCONFIG=ON
 -DOPENCV_SKIP_CMAKE_CXX_STANDARD=ON
 "
