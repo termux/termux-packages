161d934a827dddf7838313ae5a528bf8caa631b2
Arun <arun007@pm.me>
2024-04-14 13:59:19 +0530

    upgpkg(main/php): Bump to 8.3.6

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -4,10 +4,9 @@ TERMUX_PKG_LICENSE="PHP-3.01"
 TERMUX_PKG_LICENSE_FILE=LICENSE
 TERMUX_PKG_MAINTAINER="@termux"
 # Please revbump php-* extensions along with "minor" bump (e.g. 8.1.x to 8.2.0)
-TERMUX_PKG_VERSION=8.3.4
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=8.3.6
 TERMUX_PKG_SRCURL=https://github.com/php/php-src/archive/php-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4e633d4709afa5301d84d3821471bfb136f281bb71f9acca2fe9d29cc0407d2a
+TERMUX_PKG_SHA256=af9ca6ea711391eaf74793beeebeb66e8bf7756297d1bc97a293166282a0ba77
 TERMUX_PKG_AUTO_UPDATE=false
 # Build native php for phar to build (see pear-Makefile.frag.patch):
 TERMUX_PKG_HOSTBUILD=true
