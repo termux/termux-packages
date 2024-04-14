3ef5bfbad9d89584639bbbde54b00a35dcd6cfc4
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-14 15:22:59 +0000

    bump(x11/vala-panel-appmenu): 24.02
    * Enable gobject introspection data.
    * xfce4-appmenu-plugin now depends on vala-appmenu-plugin which provides libappmenu-glib-translator.so.

--- x11-packages/vala-panel-appmenu/build.sh
+++ x11-packages/vala-panel-appmenu/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/vala-panel-project/vala-panel-appmenu
 TERMUX_PKG_DESCRIPTION="Global Menu for Vala Panel (metapackage)"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.6
+TERMUX_PKG_VERSION="24.02"
 TERMUX_PKG_SRCURL=https://gitlab.com/vala-panel-project/vala-panel-appmenu/-/archive/${TERMUX_PKG_VERSION}/vala-panel-appmenu-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=c137f8f30ab5925a4a236a8a047b7962ec9be987fe25dd2d092666e0580fdacf
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_SHA256=83b1803ea18afa9edfaad008ff378ef4b560d4ef968eadf552915d3569029c52
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dwm_backend=wnck
 -Dvalapanel=disabled
@@ -18,8 +20,19 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
 
 	CPPFLAGS+=" -Dulong=u_long"
 	LDFLAGS+=" -lX11"
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
