a9889f30a7f762f5930ffe1536baf32ea5488352
Jia Yuan Lo <jylo06g@gmail.com>
2024-04-14 09:39:16 +0800

    bump(main/ninja): 1.12.0

--- packages/ninja/build.sh
+++ packages/ninja/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://ninja-build.org
 TERMUX_PKG_DESCRIPTION="A small build system with a focus on speed"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.11.1"
-TERMUX_PKG_SRCURL=https://github.com/ninja-build/ninja/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=31747ae633213f1eda3842686f83c2aa1412e0f5691d1c14dbbcc67fe7400cea
+TERMUX_PKG_VERSION="1.12.0"
+TERMUX_PKG_SRCURL=https://github.com/ninja-build/ninja/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8b2c86cd483dc7fcb7975c5ec7329135d210099a89bc7db0590a07b0bbfe49a5
+TERMUX_PKG_DEPENDS="libandroid-spawn, libc++"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, libandroid-spawn"
 
 termux_step_pre_configure() {
 	CXXFLAGS+=" $CPPFLAGS"
