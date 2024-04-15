a94dccd8b999ef3f9d93caaec8b5ec3187899bad
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-15 15:00:21 +0000

    bump(main/libpangomm-1.4): 2.46.4

--- packages/libpangomm-1.4/build.sh
+++ packages/libpangomm-1.4/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gtkmm.org/
 TERMUX_PKG_DESCRIPTION="A C++ API for Pango"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=2.46
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/pangomm/${_MAJOR_VERSION}/pangomm-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=410fe04d471a608f3f0273d3a17d840241d911ed0ff2c758a9859c66c6f24379
+TERMUX_PKG_VERSION="2.46.4"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/pangomm/${TERMUX_PKG_VERSION%.*}/pangomm-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=b92016661526424de4b9377f1512f59781f41fb16c9c0267d6133ba1cd68db22
 TERMUX_PKG_DEPENDS="glib, libc++, libcairomm-1.0, libglibmm-2.4, libsigc++-2.0, pango"
 
 termux_step_post_massage() {
