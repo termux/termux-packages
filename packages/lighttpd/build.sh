7fce5deea7209d100853b317402d0d3c82ada97e
Jia Yuan Lo <jylo06g@gmail.com>
2024-04-15 08:18:08 +0800

    bump(main/lighttpd): 1.4.76 (#19767)

--- packages/lighttpd/build.sh
+++ packages/lighttpd/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.lighttpd.net
 TERMUX_PKG_DESCRIPTION="Fast webserver with minimal memory footprint"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.75"
+TERMUX_PKG_VERSION="1.4.76"
 TERMUX_PKG_SRCURL=https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8b721ca939d312afaa6ef31dcbd6afb5161ed385ac828e6fccd4c5b76be189d6
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256=8cbf4296e373cfd0cedfe9d978760b5b05c58fdc4048b4e2bcaf0a61ac8f5011
 TERMUX_PKG_DEPENDS="libandroid-glob, libbz2, libcrypt, openssl, pcre2, zlib"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dwith_bzip=enabled
 -Dwith_openssl=true
@@ -21,7 +21,6 @@ etc/lighttpd/modules.conf
 "
 
 termux_step_post_get_source() {
-	mv configure{,.unused}
 	mv CMakeLists.txt{,.unused}
 }
 
