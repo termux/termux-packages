0c91eba4fa9d7edb82ee03005feb987783f8c4ba
qvalentin <valentin.theodor@web.de>
2024-04-16 08:09:37 +0200

    fix(main/delve): patch debugger.go to contain android
    fixes https://github.com/termux/termux-packages/issues/18672

--- packages/delve/build.sh
+++ packages/delve/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A debugger for the Go programming language"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION="1.22.1"
+TERMUX_PKG_REVISION="1"
 TERMUX_PKG_DEPENDS="golang, git"
 TERMUX_PKG_SRCURL=https://github.com/go-delve/delve/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fe6f0d97c233d4f0f1ed422c11508cc57c14e9e0915f9a258f1912c46824cbfb
@@ -13,7 +14,7 @@ termux_step_make() {
 	termux_setup_golang
 	cd $TERMUX_PKG_SRCDIR
 
-	mkdir -p "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/    
+	mkdir -p "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/
 	mkdir -p "$TERMUX_PREFIX"/share/doc/delve
 	cp -a "$TERMUX_PKG_SRCDIR" "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/delve/
 	cd "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/delve/cmd/dlv/
