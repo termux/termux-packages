30972bb1419ead72edbd3e3ab2d6756e9d8137e0
Suyun <108112549+Suyun114@users.noreply.github.com>
2024-04-16 19:00:12 +0800

    fix(x11-packages/abiword): update homepage and srcurl

--- x11-packages/abiword/build.sh
+++ x11-packages/abiword/build.sh
@@ -1,12 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://www.abisource.com/
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/World/AbiWord
 TERMUX_PKG_DESCRIPTION="A free word processing program"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.0.5
 TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://ftp-osl.osuosl.org/pub/gentoo/distfiles/abiword-${TERMUX_PKG_VERSION}.tar.gz
-#TERMUX_PKG_SRCURL=http://www.abisource.com/downloads/abiword/${TERMUX_PKG_VERSION}/source/abiword-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1257247e9970508d6d1456d3e330cd1909c4b42b25e0f0a1bc32526d6f3a21b4
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/World/AbiWord/-/archive/release-${TERMUX_PKG_VERSION}/AbiWord-release-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=078a8d2d1ff1713557a88f7067af6b40b79fa8d0b893bd138de19f8772c30329
 TERMUX_PKG_DEPENDS="atk, enchant, fontconfig, fribidi, gdk-pixbuf, glib, goffice, gtk3, libc++, libcairo, libgsf, libical, libjpeg-turbo, libpng, librsvg, libwv, libx11, libxml2, libxslt, pango, readline, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, g-ir-scanner"
 TERMUX_PKG_DISABLE_GIR=false
