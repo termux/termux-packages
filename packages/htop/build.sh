e06ddc9e14fc56e2de43fae5d9d8599de84b3518
tqfx <tqfx@tqfx.org>
2024-04-20 23:35:22 +0800

    bump(main/htop): 3.3.0

--- packages/htop/build.sh
+++ packages/htop/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://htop.dev/
 TERMUX_PKG_DESCRIPTION="Interactive process viewer for Linux"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.2.2
+TERMUX_PKG_VERSION=3.3.0
 TERMUX_PKG_SRCURL=https://github.com/htop-dev/htop/archive/${TERMUX_PKG_VERSION}/htop-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3829c742a835a0426db41bb039d1b976420c21ec65e93b35cd9bfd2d57f44ac8
+TERMUX_PKG_SHA256=1e5cc328eee2bd1acff89f860e3179ea24b85df3ac483433f92a29977b14b045
 # htop checks setlocale() return value for UTF-8 support, so use libandroid-support.
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BREAKS="htop-legacy"
