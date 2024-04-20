61144e5cea1a5bf87f62e363a9644af798f6b761
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-20 20:32:56 +0530

    fix(main/enchant): Fix building in 32 bit environment (#19815)
    Swap to "release" tarball for build so bootstrap step is not necessary.

--- packages/enchant/build.sh
+++ packages/enchant/build.sh
@@ -3,15 +3,12 @@ TERMUX_PKG_DESCRIPTION="Wraps a number of different spelling libraries and progr
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.6.9"
-TERMUX_PKG_SRCURL=https://github.com/AbiWord/enchant/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8738710a1f73f3fd19eb1c603e9353c61595e288ca0a43b0805418136df99c4e
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/AbiWord/enchant/releases/download/v${TERMUX_PKG_VERSION}/enchant-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d9a5a10dc9b38a43b3a0fa22c76ed6ebb7e09eb535aff62954afcdbd40efff6b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-relocatable" 
 TERMUX_PKG_DEPENDS="aspell, glib, hunspell, libc++"
-
-termux_step_post_get_source() {
-	./bootstrap
-}
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
