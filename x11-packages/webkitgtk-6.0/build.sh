a94375d167fefaa8496edb597a2a590945c74ee1
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-04-22 03:44:51 +0800

    webkitgtk-6.0: bump to 2.42.4

--- x11-packages/webkitgtk-6.0/build.sh
+++ x11-packages/webkitgtk-6.0/build.sh
@@ -2,15 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://webkitgtk.org
 TERMUX_PKG_DESCRIPTION="A full-featured port of the WebKit rendering engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.40.3
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="2.42.4"
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cc0aa83f40dbc64c1c6ae42ec6b85af4be2a9dbf524cfcb95f89a367fb5098dd
+TERMUX_PKG_SHA256=52288b30bda22373442cecb86f9c9a569ad8d4769a1f97b352290ed92a67ed86
 TERMUX_PKG_DEPENDS="enchant, fontconfig, freetype, glib, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gstreamer, gtk4, harfbuzz, harfbuzz-icu, libc++, libcairo, libgcrypt, libhyphen, libicu, libjpeg-turbo, libpng, libsoup3, libtasn1, libwebp, libxml2, libx11, libxcomposite, libxdamage, libxslt, libxt, littlecms, openjpeg, pango, woff2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xorgproto"
 TERMUX_PKG_DISABLE_GIR=false
 
 termux_step_post_get_source() {
+	# Version guard
+	local ver_e=${TERMUX_PKG_VERSION#*:}
+	local ver_x=$(. $TERMUX_SCRIPTDIR/x11-packages/webkit2gtk-4.1/build.sh; echo ${TERMUX_PKG_VERSION#*:})
+	if [ "${ver_e}" != "${ver_x}" ]; then
+		termux_error_exit "Version mismatch between webkit2gtk-4.1 and webkitgtk-6.0."
+	fi
+
 	local p
 	for p in $TERMUX_SCRIPTDIR/x11-packages/webkit2gtk-4.1/*.patch; do
 		echo "Applying $(basename "${p}")"
@@ -31,8 +37,12 @@ termux_step_pre_configure() {
 
 	termux_setup_gir
 
+	# Workaround for https://github.com/android/ndk/issues/1973
+	[ "$TERMUX_ARCH" == "arm" ] && sed -i '/#define MUST_TAIL_CALL \[\[clang::musttail]]/d' Source/WTF/wtf/Compiler.h
+
 	CPPFLAGS+=" -DHAVE_MISSING_STD_FILESYSTEM_PATH_CONSTRUCTOR"
 	CPPFLAGS+=" -DCMS_NO_REGISTER_KEYWORD"
+	CPPFLAGS+=" -I${TERMUX_PREFIX}/lib/gstreamer-1.0/include"
 }
 
 termux_step_post_massage() {
