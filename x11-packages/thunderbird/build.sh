e5985f51b2ae1a924c9e6ddaa192ed4cec03a524
Kevin Williams <kevin8w@duck.com>
2024-04-21 18:26:26 +0800

    libicu: revbump revdeps(6)

--- x11-packages/thunderbird/build.sh
+++ x11-packages/thunderbird/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Unofficial Thunderbird email client"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=115.6.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://ftp.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}/source/thunderbird-${TERMUX_PKG_VERSION}.source.tar.xz
 TERMUX_PKG_SHA256=638beb0d2907c6adbe441b7cd371f205728ac65489c04cb29bb40e71ea2846e3
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libotr, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
@@ -19,6 +19,13 @@ termux_step_post_get_source() {
 termux_step_pre_configure() {
 	termux_setup_nodejs
 	termux_setup_rust
+
+	# Out of memory when building gkrust
+	if [ "$TERMUX_DEBUG_BUILD" = false ]; then
+		case "${TERMUX_ARCH}" in
+		aarch64|arm|i686|x86_64) RUSTFLAGS+=" -C debuginfo=1" ;;
+		esac
+	fi
 
 	cargo install cbindgen
 
@@ -46,6 +53,13 @@ termux_step_configure() {
 		-e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|" \
 		-e "s|@CARGO_TARGET_NAME@|${CARGO_TARGET_NAME}|" \
 		$TERMUX_PKG_BUILDER_DIR/mozconfig.cfg > .mozconfig
+
+	if [ "$TERMUX_DEBUG_BUILD" = true ]; then
+		cat >>.mozconfig - <<END
+ac_add_options --enable-debug-symbols
+ac_add_options --disable-install-strip
+END
+	fi
 
 	./mach configure
 }
