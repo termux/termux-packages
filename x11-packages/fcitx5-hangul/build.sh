a37e9ee1182bb2402a6c8efb2b41e0f7bd3bf3cc
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-21 14:19:57 +0530

    addpkg(x11/fcitx5-hangul): 5.1.2 (#19858)
    * addpkg(main/libhangul): 0.1.0-p20230415
    
    * addpkg(x11/fcitx5-hangul): 5.1.2

---
+++ x11-packages/fcitx5-hangul/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/fcitx/fcitx5-hangul
+TERMUX_PKG_DESCRIPTION="libhangul input method support for fcitx5"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="5.1.2"
+TERMUX_PKG_SRCURL="https://github.com/fcitx/fcitx5-hangul/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=805e0f1cf6b3ecb061d147fceb9f5aa690e2f2935fc282b61146c4a849ae4fe5
+TERMUX_PKG_DEPENDS="fcitx5, libc++, libhangul"
