bcdaf06282ce95cf8f146039d9f5e5af29e685e5
TomIO <tomjo00@web.de>
2024-04-22 14:09:17 +0200

    bump(main/zig): 0.12.0
    Co-authored-by: Jia Yuan Lo <jylo06g@gmail.com>

--- packages/zig/build.sh
+++ packages/zig/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="General-purpose programming language and toolchain"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="zig/LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.11.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/ziglang/zig-bootstrap/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=046cede54ae0627c6ac98a1b3915242b35bc550ac7aaec3ec4cef6904c95019e
+TERMUX_PKG_VERSION="0.12.0"
+TERMUX_PKG_SRCURL=https://github.com/ziglang/zig/releases/download/${TERMUX_PKG_VERSION}/zig-bootstrap-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=3efc643d56421fa68072af94d5512cb71c61acf1c32512f77c0b4590bff63187
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
@@ -15,14 +14,16 @@ termux_step_pre_configure() {
 	termux_setup_cmake
 	termux_setup_ninja
 	termux_setup_zig
-}
 
-termux_step_make() {
+	export TERMUX_MAKE_PROCESSES
+
 	# zig 0.11.0+ uses 3 stages bootstrapping build system
 	# which NDK cant be used anymore
 	unset AS CC CFLAGS CPP CPPFLAGS CXX CXXFLAGS LD LDFLAGS
+}
 
-	# build.patch skipped various steps to make CI build <6 hours
+termux_step_make() {
+	# build.patch switch to ninja to make CI build <6 hours
 	./build "${ZIG_TARGET_NAME}" baseline
 }
 
--- scripts/build/setup/termux_setup_zig.sh
+++ scripts/build/setup/termux_setup_zig.sh
@@ -1,35 +1,48 @@
 # shellcheck shell=bash disable=SC2155
 termux_setup_zig() {
-	local ZIG_VERSION=0.11.0
-	local ZIG_TXZ_URL=https://ziglang.org/download/${ZIG_VERSION}/zig-linux-x86_64-${ZIG_VERSION}.tar.xz
-	local ZIG_TXZ_SHA256=2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047
-	local ZIG_TXZ_FILE=${TERMUX_PKG_TMPDIR}/zig-${ZIG_VERSION}.tar.xz
-	local ZIG_FOLDER=${TERMUX_COMMON_CACHEDIR}/zig-${ZIG_VERSION}
-	if [[ "${TERMUX_PACKAGES_OFFLINE-false}" == "true" ]]; then
-		ZIG_FOLDER=${TERMUX_SCRIPTDIR}/build-tools/zig-${ZIG_VERSION}
-	fi
-	local ZIG_PKG_VERSION=$(. "${TERMUX_SCRIPTDIR}/packages/zig/build.sh"; echo ${TERMUX_PKG_VERSION})
-
-	# zig 0.9.1 android triples never worked and uses musl
+	# TODO need to figure out if zig supports android targets
 	export ZIG_TARGET_NAME="${TERMUX_ARCH}-linux-musl"
 	case "${TERMUX_ARCH}" in
 	arm) ZIG_TARGET_NAME="arm-linux-musleabihf" ;;
 	i686) ZIG_TARGET_NAME="x86-linux-musl" ;;
 	esac
 
+	if [[ -z "${TERMUX_ZIG_VERSION-}" ]]; then
+		TERMUX_ZIG_VERSION=$(. "${TERMUX_SCRIPTDIR}/packages/zig/build.sh"; echo ${TERMUX_PKG_VERSION})
+	fi
+
+	local ZIG_TXZ_SHA256
+	case "${TERMUX_ZIG_VERSION}" in
+	0.12.0) ZIG_TXZ_SHA256=c7ae866b8a76a568e2d5cfd31fe89cdb629bdd161fdd5018b29a4a0a17045cad ;;
+	0.11.0) ZIG_TXZ_SHA256=2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047 ;;
+	0.9.1) ZIG_TXZ_SHA256=be8da632c1d3273f766b69244d80669fe4f5e27798654681d77c992f17c237d7 ;;
+	*) termux_error_exit "Please add ${TERMUX_ZIG_VERSION} archive checksum to termux_setup_zig and update patches in packages/zig" ;;
+	esac
+
+	local ZIG_TXZ_URL=https://ziglang.org/download/${TERMUX_ZIG_VERSION}/zig-linux-x86_64-${TERMUX_ZIG_VERSION}.tar.xz
+	local ZIG_TXZ_FILE=${TERMUX_PKG_TMPDIR}/zig-${TERMUX_ZIG_VERSION}.tar.xz
+	local ZIG_FOLDER=${TERMUX_COMMON_CACHEDIR}/zig-${TERMUX_ZIG_VERSION}
+	if [[ "${TERMUX_PACKAGES_OFFLINE-false}" == "true" ]]; then
+		ZIG_FOLDER=${TERMUX_SCRIPTDIR}/build-tools/zig-${TERMUX_ZIG_VERSION}
+	fi
+
 	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "true" ]]; then
-		if [[ "$(cat "${TERMUX_BUILT_PACKAGES_DIRECTORY}/zig" 2>/dev/null)" != "${ZIG_PKG_VERSION}" && -z "$(command -v zig)" ]]; then
+		if [[ -z "$(command -v zig)" ]]; then
 			cat <<- EOL
 			Package 'zig' is not installed.
 			You can install it with
 
 			pkg install zig
-
-			or build it from source with
-
-			./build-package.sh zig
 			EOL
 			exit 1
+		fi
+		local ZIG_VERSION=$(zig version)
+		if [[ "${TERMUX_ZIG_VERSION}" != "${ZIG_VERSION}" ]]; then
+			cat <<- EOL >&2
+			WARN: On device build with old zig version is not possible!
+			TERMUX_ZIG_VERSION = ${TERMUX_ZIG_VERSION}
+			ZIG_VERSION        = ${ZIG_VERSION}
+			EOL
 		fi
 		return
 	fi
@@ -39,10 +52,13 @@ termux_setup_zig() {
 		termux_download "${ZIG_TXZ_URL}" "${ZIG_TXZ_FILE}" "${ZIG_TXZ_SHA256}"
 		tar -xf "${ZIG_TXZ_FILE}" -C "${ZIG_FOLDER}" --strip-components=1
 
-		echo "termux_setup_zig: Applying patches from packages/zig"
-		for p in "${TERMUX_SCRIPTDIR}"/packages/zig/zig-*.patch; do
-			patch -d "${ZIG_FOLDER}" -p2 -i "${p}"
-		done
+		if [[ -n "$(find "${TERMUX_SCRIPTDIR}/packages/zig/${TERMUX_ZIG_VERSION}" -name 'zig-*.patch')" ]]; then
+			echo "termux_setup_zig: Applying patches from packages/zig/${TERMUX_ZIG_VERSION}"
+			local p
+			for p in "${TERMUX_SCRIPTDIR}"/packages/zig/${TERMUX_ZIG_VERSION}/zig-*.patch; do
+				patch -d "${ZIG_FOLDER}" -p2 -i "${p}"
+			done
+		fi
 	fi
 
 	export PATH="${ZIG_FOLDER}:${PATH}"
