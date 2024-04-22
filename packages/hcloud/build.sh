6bc7408bb0c4e441c793daf4e968c5a8deacc954
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-22 13:16:31 +0000

    chore(main/hcloud): Install shell completion

--- packages/hcloud/build.sh
+++ packages/hcloud/build.sh
@@ -19,9 +19,23 @@ termux_step_pre_configure() {
 termux_step_make() {
 	# Below are taken from github.com/hetznercloud/cli@v1.30.1/.goreleaser.yml
 	local LD_FLAGS="-s -w -X 'github.com/hetznercloud/cli/internal/version.Version=v${TERMUX_PKG_VERSION}'"
-	go build -ldflags "${LD_FLAGS}" -o hcloud  cmd/hcloud/main.go 
+	export GOFLAGS="-buildmode=pie -trimpath -mod=readonly -modcacherw"
+	go build -ldflags "${LD_FLAGS}" -o hcloud  cmd/hcloud/main.go
 }
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin hcloud
+
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/bash-completion/completions/hcloud
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/zsh/site-functions/_hcloud
+	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/fish/vendor_completions.d/hcloud.fish
+}
+
+termux_step_create_debscripts() {
+	cat <<-EOF >./postinst
+		#!${TERMUX_PREFIX}/bin/sh
+		hcloud completion bash > ${TERMUX_PREFIX}/share/bash-completion/completions/hcloud
+		hcloud completion zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_hcloud
+		hcloud completion fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/hcloud.fish
+	EOF
 }
