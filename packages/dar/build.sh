96e1445d35c58366c3b53771dd163d7057de3bca
tqfx <tqfx@tqfx.org>
2024-04-22 21:05:07 +0800

    bump(main/dar): 2.7.14

--- packages/dar/build.sh
+++ packages/dar/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=http://dar.linux.free.fr/
 TERMUX_PKG_DESCRIPTION="A full featured command-line backup tool, short for Disk ARchive"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.7.13"
+TERMUX_PKG_VERSION="2.7.14"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dar/dar/${TERMUX_PKG_VERSION}/dar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2d563b5d1d928a3eecab719cc22d43320786c52053f4e3a557cdf1c84b120f4c
+TERMUX_PKG_SHA256=40d4dba44260df3a8ddce1e61f411ea9ab43c2cfc47bd83ab868c939d19dc582
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="attr, libbz2, libc++, libgcrypt, libgpg-error, liblzma, liblzo, zlib, zstd"
 TERMUX_PKG_BUILD_IN_SRC=
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-dar-static "
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-dar-static"
 
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH_BITS" = "32" ]; then
