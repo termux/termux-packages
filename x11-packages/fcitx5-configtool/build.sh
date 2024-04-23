ffc139f402c2cfddea37d96ccc73707f7a7f7826
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-23 13:41:17 +0000

    bump(x11/fcitx5-configtool): 5.1.5

--- x11-packages/fcitx5-configtool/build.sh
+++ x11-packages/fcitx5-configtool/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://fcitx-im.org/
 TERMUX_PKG_DESCRIPTION="Configuration tool for Fcitx5"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.1.4"
+TERMUX_PKG_VERSION="5.1.5"
 TERMUX_PKG_SRCURL=https://github.com/fcitx/fcitx5-configtool/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7ae28a817807da7a102143382503e1fa32732e0475661c68cece47915f238ed8
+TERMUX_PKG_SHA256=26d949116915e870f345171d24a052cda301628e8f6cf4ee62fac2a3baebd101
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="fcitx5, fcitx5-qt, iso-codes, kitemviews, kwidgetsaddons, libc++, libx11, libxkbfile, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, xkeyboard-config"
@@ -12,4 +12,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_TEST=OFF
 -DENABLE_CONFIG_QT=ON
 -DENABLE_KCM=OFF
+-DUSE_QT6=OFF
 "
