a419cc83f69b0bb92a60ccbcf8c908e2c60eace6
tqfx <tqfx@tqfx.org>
2024-04-23 22:27:35 +0800

    bump(main/yosys): 0.40

--- packages/yosys/build.sh
+++ packages/yosys/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://yosyshq.net/yosys/
 TERMUX_PKG_DESCRIPTION="A framework for RTL synthesis tools"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.38"
+TERMUX_PKG_VERSION="0.40"
 TERMUX_PKG_SRCURL=https://github.com/YosysHQ/yosys/archive/refs/tags/yosys-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5f3d7bb12c5371db00586700a658a9196008a9457839f046403a660fe0c7a1df
+TERMUX_PKG_SHA256=c1d42ad90d587b587210b40cf3c5584e41e20f656e8630c33b6583322e8b764e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+"
 TERMUX_PKG_DEPENDS="graphviz, libandroid-glob, libandroid-spawn, libc++, libffi, readline, tcl, zlib"
@@ -12,5 +12,5 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
 
 termux_step_pre_configure() {
-	LDFLAGS+=" -landroid-glob -landroid-spawn"
+	export LIBS="-landroid-glob -landroid-spawn"
 }
