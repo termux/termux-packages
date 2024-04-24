14b725c7b68e51b716909fd47919cfd0107e946f
tqfx <tqfx@tqfx.org>
2024-04-24 13:49:09 +0800

    bump(x11/weston): 13.0.1

--- x11-packages/weston/build.sh
+++ x11-packages/weston/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.freedesktop.org/wayland/weston
 TERMUX_PKG_DESCRIPTION="Reference implementation of a wayland compositor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="13.0.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="13.0.1"
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/weston/-/archive/${TERMUX_PKG_VERSION}/weston-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a4848b7cc510e3e88c9f2fcc808fd44c51fae622f13242f801a7c5107983335c
+TERMUX_PKG_SHA256=4c82bb0fb4d2929194ee052e37dd21c79373a443cbbf89bf337fc2094df7cbaf
 TERMUX_PKG_DEPENDS="freerdp, libaml, libandroid-shmem, libcairo, libcairo, libevdev, libglvnd, libneatvnc, libseat, libwayland, libwebp, libxcb, libxcursor, libxkbcommon, littlecms, pango, xcb-util-cursor"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-cross-scanner, libwayland-protocols"
 # XXX: Do not depend on gbm
