c2134d3ee0bced1f212b0605e3bb627c22470090
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-04-25 23:55:12 +0800

    gtk-update-icon-cache: add BREAKS field
    `share/man/man1/gtk-update-icon-cache.1.gz` is moved from
    gtk3 to gtk-update-icon-cache since version 3.24.41, the
    latter should have BREAKS field to let the installation
    process work as intended.

--- x11-packages/gtk3/gtk-update-icon-cache.subpackage.sh
+++ x11-packages/gtk3/gtk-update-icon-cache.subpackage.sh
@@ -5,6 +5,8 @@ share/man/man1/gtk-update-icon-cache.1.gz
 
 TERMUX_SUBPKG_DEPENDS="gdk-pixbuf, glib"
 TERMUX_SUBPKG_DESCRIPTION="GTK+ icon cache updater"
+TERMUX_SUBPKG_BREAKS="gtk3 (<< 3.24.41)"
+TERMUX_SUBPKG_REPLACES="gtk3 (<< 3.24.41)"
 
 termux_step_create_subpkg_debscripts() {
 	cat <<- EOF > ./triggers
