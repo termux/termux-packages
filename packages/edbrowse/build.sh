1764f8cbbd828739bf8205a7915b502e42d9fc03
tqfx <tqfx@tqfx.org>
2024-04-25 16:19:03 +0800

    bump(main/edbrowse): 3.8.10

--- packages/edbrowse/build.sh
+++ packages/edbrowse/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_DESCRIPTION="Line based editor, browser, and mail client"
 TERMUX_PKG_LICENSE="GPL-2.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE.quickjs"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.8.9"
+TERMUX_PKG_VERSION="3.8.10"
 TERMUX_PKG_SRCURL=https://github.com/CMB/edbrowse/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=dae133d6b52be88864f8e696b8fc4ca4185e04857707713da8a0085bedf04e6b
+TERMUX_PKG_SHA256=3c194ce45b7348211ce3ad8e3304a0eacf8b27e623cbf8c08687785f88174e03
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="libandroid-glob, libcurl, openssl, pcre2, readline, unixodbc"
@@ -15,8 +15,13 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="-C src
 PREFIX=$TERMUX_PREFIX
 QUICKJS_INCLUDE=$TERMUX_PREFIX/include/quickjs
+QUICKJS_LIB=$TERMUX_PREFIX/lib/quickjs
 "
 
 termux_step_post_get_source() {
 	cp $TERMUX_PKG_BUILDER_DIR/LICENSE.quickjs ./
+}
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-glob"
 }
