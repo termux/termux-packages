bd7a31d15888ea4a460db9e4acd574ba9f45ec92
Ivan Max <mixython@gmail.com>
2024-04-25 09:34:19 +0300

    scripts/setup-cgct.sh: adding the glibc-cgct installation (#19900)

--- scripts/setup-cgct.sh
+++ scripts/setup-cgct.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# setup CGCT - Cross Gnu Compiler for Termux
+# setup CGCT - Cross Gnu Compilers for Termux
 # compile glibc-based binaries for Termux
 
 . $(dirname "$(realpath "$0")")/properties.sh
@@ -9,8 +9,6 @@ set -e -u
 
 ARCH="x86_64"
 REPO_URL="https://service.termux-pacman.dev/gpkg-dev/${ARCH}"
-VERSION_OF_CBT="2.42-1"
-VERSION_OF_CGT="13.2.0-5"
 
 if [ "$ARCH" != "$(uname -m)" ]; then
 	echo "Error: the requested CGCT is not supported on your architecture"
@@ -18,8 +16,9 @@ if [ "$ARCH" != "$(uname -m)" ]; then
 fi
 
 declare -A CGCT=(
-	["cbt"]="cbt-${VERSION_OF_CBT}-${ARCH}.pkg.tar.xz" # Cross Binutils for Termux
-	["cgt"]="cgt-${VERSION_OF_CGT}-${ARCH}.pkg.tar.xz" # Cross GCC for Termux
+	["cbt"]="2.42-1" # Cross Binutils for Termux
+	["cgt"]="13.2.0-5" # Cross GCCs for Termux
+	["glibc-cgct"]="2.39-0" # Glibc for CGCT
 )
 
 : "${TERMUX_PKG_TMPDIR:="/tmp"}"
@@ -45,43 +44,34 @@ for pkgname in ${!CGCT[@]}; do
 		echo "Error: package '${pkgname}' not found"
 		exit 1
 	fi
-	filename="${CGCT[$pkgname]}"
-	filename_of_json=$(jq -r '."'$pkgname'"."FILENAME"' "${TMPDIR_CGCT}/cgct.json")
-	if [ "$filename" != "$filename_of_json" ]; then
-		echo "Error: files do not match: requested - '$filename'; actual - '$filename_of_json'"
+	version="${CGCT[$pkgname]}"
+	version_of_json=$(jq -r '."'$pkgname'"."VERSION"' "${TMPDIR_CGCT}/cgct.json")
+	if [ "${version}" != "${version_of_json}" ]; then
+		echo "Error: versions do not match: requested - '${version}'; actual - '${version_of_json}'"
 		exit 1
 	fi
+	filename=$(jq -r '."'$pkgname'"."FILENAME"' "${TMPDIR_CGCT}/cgct.json")
 	if [ ! -f "${TMPDIR_CGCT}/${filename}" ]; then
 		termux_download "${REPO_URL}/${filename}" \
 			"${TMPDIR_CGCT}/${filename}" \
-			"$SHA256SUM"
+			"${SHA256SUM}"
 	fi
 	tar xJf "${TMPDIR_CGCT}/${filename}" -C / data
 done
 
-# Installing glibc for CGCT
-if [ ! -d "${CGCT_DIR}/lib" ]; then
-	echo "Installing glibc for CGCT..."
-	for i in glibc gcc-libs; do
-		curl -L "https://archlinux.org/packages/core/${ARCH}/${i}/download/" -o "${TMPDIR_CGCT}/${i}.pkg.zstd"
-		tar --use-compress-program=unzstd -xf "${TMPDIR_CGCT}/${i}.pkg.zstd" -C "${TMPDIR_CGCT}" usr
-	done
+# Installing gcc-libs for CGCT
+if [ ! -f "${CGCT_DIR}/lib/libgcc_s.so" ]; then
+	pkgname="gcc-libs"
+	echo "Installing ${pkgname} for CGCT..."
+	curl -L "https://archlinux.org/packages/core/${ARCH}/${pkgname}/download/" -o "${TMPDIR_CGCT}/${pkgname}.pkg.zstd"
+	tar --use-compress-program=unzstd -xf "${TMPDIR_CGCT}/${pkgname}.pkg.zstd" -C "${TMPDIR_CGCT}" usr/lib
 	cp -r "${TMPDIR_CGCT}/usr/lib" "${CGCT_DIR}/lib"
+	rm -fr "${TMPDIR_CGCT}/usr"
 fi
 
-# Setting up CGCT for this glibc
-echo "Setting up CGCT for this glibc..."
-LD_LIB=$(ls ${CGCT_DIR}/lib/ld-* 2> /dev/null)
-if [ ! -n "$LD_LIB" ]; then
-	echo "Error: interpreter not found in lib directory"
+# Setting up CGCT
+if [ ! -f "${CGCT_DIR}"/bin/setup-cgct ]; then
+	echo "Error: setup-cgct command not found in CGCT directory"
 	exit 1
 fi
-for i in aarch64 arm x86_64 i686; do
-	for j in bin lib/gcc; do
-		for f in $(find "${CGCT_DIR}/${i}/${j}" -type f -exec grep -IL . "{}" \; | grep -v -e '\.a' -e '\.o' -e '\.so'); do
-			patchelf --set-rpath "${CGCT_DIR}/lib:/usr/lib:/usr/lib64" \
-				--set-interpreter "$LD_LIB" "$f"
-			echo "Configured '${f}'"
-		done
-	done
-done
+"${CGCT_DIR}"/bin/setup-cgct "/usr/lib/x86_64-linux-gnu"
