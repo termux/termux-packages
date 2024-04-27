6f87b0b03e25f27e94408c8190a4706a54f4f514
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-27 10:10:04 +0530

    bump(x11/evince): 46.0 (#19927)
    Remove patch which disables post install updates.

--- x11-packages/evince/build.sh
+++ x11-packages/evince/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Evince
 TERMUX_PKG_DESCRIPTION="document viewer for multiple document formats"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="45.0"
+TERMUX_PKG_VERSION="46.0"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/evince/${TERMUX_PKG_VERSION%.*}/evince-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=d18647d4275cbddf0d32817b1d04e307342a85be914ec4dad2d8082aaf8aa4a8
+TERMUX_PKG_SHA256=aff6af69392c04956bfad976dec5d1583b41d5a334e937995f7c3ca0740de221
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, djvulibre, gdk-pixbuf, glib, gnome-desktop3, gst-plugins-base, gst-plugins-good, gstreamer, gtk3, libarchive, libcairo, libgxps, libhandy, libsecret, libspectre, libtiff, libxml2, pango, poppler, poppler-data, texlive-bin"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
 TERMUX_PKG_RECOMMENDS="evince-help"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -20,4 +20,15 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	TERMUX_PKG_VERSION=. termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
