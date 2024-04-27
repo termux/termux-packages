be8b01305f3a05229c64696fcb4182a846845522
tqfx <tqfx@tqfx.org>
2024-04-27 13:44:28 +0800

    bump(main/sqlcipher): 4.5.7 (#19928)

--- packages/sqlcipher/build.sh
+++ packages/sqlcipher/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/sqlcipher/sqlcipher
 TERMUX_PKG_DESCRIPTION="SQLCipher is an SQLite extension that provides 256 bit AES encryption of database files"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.5.6"
+TERMUX_PKG_VERSION="4.5.7"
 TERMUX_PKG_SRCURL=https://github.com/sqlcipher/sqlcipher/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e4a527e38e67090c1d2dc41df28270d16c15f7ca5210a3e7ec4c4b8fda36e28f
+TERMUX_PKG_SHA256=b670845f28da0a3c717e991e9f18a334e08f6bc977190bbce6be864ca229f96d
 TERMUX_PKG_DEPENDS="libedit, openssl"
 TERMUX_PKG_BUILD_DEPENDS="tcl"
 TERMUX_PKG_AUTO_UPDATE=true
@@ -16,5 +16,5 @@ TCLLIBDIR=${TERMUX_PREFIX}/lib/tcl8.6/sqlite
 "
 
 termux_step_pre_configure() {
-	CPPFLAGS+=" -DSQLITE_HAS_CODEC"
+	CPPFLAGS+=" -DSQLCIPHER_OMIT_LOG_DEVICE -DSQLITE_HAS_CODEC"
 }
