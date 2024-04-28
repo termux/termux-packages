9c7b69dd45189474d1349b4a7a691283df3b1a52
Biswapriyo Nath <nathbappai@gmail.com>
2024-04-28 13:45:01 +0000

    bump(main/gdk-pixbuf): 2.42.11
    * Remove docs-meson.build.patch file because gi-docgen is not searched if gtk_doc is false.
    * Enable building less supported image formats which was disabled in this version.
      See https://gitlab.gnome.org/GNOME/gdk-pixbuf/-/commit/e052a112075a19fb75f1f2ff3de4c82923de13f2

--- packages/gdk-pixbuf/build.sh
+++ packages/gdk-pixbuf/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/GdkPixbuf
 TERMUX_PKG_DESCRIPTION="Library for image loading and manipulation"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=2.42
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.10
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/${_MAJOR_VERSION}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ee9b6c75d13ba096907a2e3c6b27b61bcd17f5c7ebeab5a5b439d2f2e39fe44b
+TERMUX_PKG_VERSION="2.42.11"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gdk-pixbuf/${TERMUX_PKG_VERSION%.*}/gdk-pixbuf-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=49dcb402388708647e8c321d56b6fb30f21e51e515d0c5a942268d23052a2f00
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, libpng, libtiff, libjpeg-turbo, zstd"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="gdk-pixbuf-dev"
@@ -16,10 +15,23 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dgtk_doc=false
 -Dintrospection=enabled
 -Dgio_sniffing=false
+-Dothers=enabled
+-Dtests=false
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
 
 termux_step_create_debscripts() {
