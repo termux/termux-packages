7403852eaa356bf4dc26d693233dde40f3b12102
Suyun <108112549+Suyun114@users.noreply.github.com>
2024-04-30 11:08:45 +0800

    fix(main/frp): fix dead homepage link (#19844)

--- packages/frp/build.sh
+++ packages/frp/build.sh
@@ -1,8 +1,9 @@
-TERMUX_PKG_HOMEPAGE=https://gofrp.org/doc
+TERMUX_PKG_HOMEPAGE=https://github.com/fatedier/frp
 TERMUX_PKG_DESCRIPTION="A fast reverse proxy to expose a local server behind a NAT or firewall to the internet"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
 TERMUX_PKG_VERSION="0.57.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fatedier/frp/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=460e3ea0aa18c63f21fd5e31663743dedaed2b2f75772050a7627e8534b5f47d
 TERMUX_PKG_REPLACES="frpc, frps"
