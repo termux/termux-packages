b06c647e5223335ed849aa034439e685888c4063
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-01 15:44:11 +0800

    f

--- x11-packages/dconf/build.sh
+++ x11-packages/dconf/build.sh
@@ -12,3 +12,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dbash_completion=false
 -Dvapi=false
 "
+
+termux_step_pre_configure() {
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
+}
