73f7f2ecd52dc556bc675fe8cd5c5f1f6152008e
Biswapriyo Nath <nathbappai@gmail.com>
2024-05-03 23:11:25 +0530

    bump(x11/libxfce4ui): 4.18.6 (#20016)

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=4.18
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.4
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4ui/${_MAJOR_VERSION}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=87eefe797c6d26de3f754de48872faf131f1b5fc93fb88e22f5c7886a842cb4c
+TERMUX_PKG_VERSION="4.18.6"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4ui/${TERMUX_PKG_VERSION%.*}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=77dd99206cc8c6c7f69c269c83c7ee6a037bca9d4a89b1a6d9765e5a09ce30cd
+# Prevent updating to unstable branch
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libice, libsm, libx11, libxfce4util, pango, startup-notification, xfconf, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_RECOMMENDS="hicolor-icon-theme"
@@ -19,5 +20,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
 }
