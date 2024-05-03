fa8252bc67524099a88c3808b11aac62748829e5
Biswapriyo Nath <nathbappai@gmail.com>
2024-05-03 20:05:21 +0530

    bump(x11/libxfce4util): 4.18.2 (#19973)
    * Enable vala bindings which was disabled in edc24482c7a3dabf457dfb2d274a0bbe39fd6f29 commit.
      The vala build failure is resolved by using vapigen script in gir-cross directory.
      The same workaround is appiled in gspell, gtksourceview3 etc.
    * Disable auto update to prevent updating to unstable branch 4.19.x

--- x11-packages/libxfce4util/build.sh
+++ x11-packages/libxfce4util/build.sh
@@ -2,19 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="Basic utility non-GUI functions for XFCE"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=4.18
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4util/${_MAJOR_VERSION}/libxfce4util-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=8a52063a5adc66252238cad9ee6997909b59983ed21c77eb83c5e67829d1b01f
+TERMUX_PKG_VERSION="4.18.2"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4util/${TERMUX_PKG_VERSION%.*}/libxfce4util-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=d9a329182b78f7e2520cd4aafcbb276bbbf162f6a89191676539ad2e3889c353
+# Prevent updating to unstable branch
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="glib"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-introspection=yes
---enable-vala=no
+--enable-vala=yes
 --enable-gtk-doc-html=no
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
 }
