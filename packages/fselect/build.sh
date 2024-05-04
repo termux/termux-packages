6b7871c34014514158492d428405298b2a92e904
Biswapriyo Nath <nathbappai@gmail.com>
2024-05-04 04:27:19 +0000

    bump(main/fselect): 0.8.6
    Add workaround for libz-ng-sys crate build failure.

--- packages/fselect/build.sh
+++ packages/fselect/build.sh
@@ -1,12 +1,25 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/jhspetersson/fselect
+TERMUX_PKG_HOMEPAGE=https://fselect.rocks/
 TERMUX_PKG_DESCRIPTION="Find files with SQL-like queries"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.8.5"
+TERMUX_PKG_VERSION="0.8.6"
 TERMUX_PKG_SRCURL=https://github.com/jhspetersson/fselect/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e5742da80606630e310bb1567f2d72d7874f0b2537440e2800507abd786d912d
+TERMUX_PKG_SHA256=4b7a6dc5f6f3da39c3242856a1c78734c7b14bd801dc4d7e32bc6f5a1809bc63
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_cmake
+	termux_setup_rust
+
+	# Dummy CMake toolchain file to workaround build error:
+	# error: failed to run custom build command for `libz-ng-sys v1.1.15`
+	# ...
+	# CMake Error at /home/builder/.termux-build/_cache/cmake-3.28.3/share/cmake-3.28/Modules/Platform/Android-Determine.cmake:217 (message):
+	# Android: Neither the NDK or a standalone toolchain was found.
+	export TARGET_CMAKE_TOOLCHAIN_FILE="${TERMUX_PKG_BUILDDIR}/android.toolchain.cmake"
+	touch "${TERMUX_PKG_BUILDDIR}/android.toolchain.cmake"
+}
 
 termux_step_post_make_install() {
 	install -Dm700 \
