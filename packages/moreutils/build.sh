858fede5d6b557588daf4a7d0e6424b4c2b9b8b4
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-05-07 13:23:20 +0200

    bump(main/moreutils): 0.69 (#20045)
    additional housekeeping:
    - Updated the package homepage to the HTTPS variant
    - Set a maintainer
    - Tarball compression switched from Gzip to Xz in upstream
    - Enabled auto updates

--- packages/moreutils/build.sh
+++ packages/moreutils/build.sh
@@ -1,12 +1,13 @@
-TERMUX_PKG_HOMEPAGE=http://joeyh.name/code/moreutils/
+TERMUX_PKG_HOMEPAGE=https://joeyh.name/code/moreutils/
 TERMUX_PKG_DESCRIPTION="A growing collection of the unix tools that nobody thought to write thirty years ago"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.67
-TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/m/moreutils/moreutils_$TERMUX_PKG_VERSION.orig.tar.gz
-TERMUX_PKG_SHA256=03872f42c22943b21c62f711b693c422a4b8df9d1b8b2872ef9a34bd5d13ad10
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
+TERMUX_PKG_VERSION=0.69
+TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/m/moreutils/moreutils_$TERMUX_PKG_VERSION.orig.tar.xz
+TERMUX_PKG_SHA256=2170c46219ce8d6f17702321534769dfbfece52148a78cd12ea73b5d3a72ff7c
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 # chronic requires set of external perl modules.
 TERMUX_PKG_RM_AFTER_INSTALL="
