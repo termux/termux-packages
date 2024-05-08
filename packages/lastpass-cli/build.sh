bb23108dd41548ed48662b69b56a6a72de1bdb6e
Biswapriyo Nath <nathbappai@gmail.com>
2024-05-08 15:35:33 +0000

    enhance(main/lastpass-cli): Enable bash-completion

--- packages/lastpass-cli/build.sh
+++ packages/lastpass-cli/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="LastPass command line interface tool"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/lastpass/lastpass-cli/archive/v$TERMUX_PKG_VERSION/lastpass-cli-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=99517545e4906230a1fb21a0c6c575210f127057ad5b50eeb3c8e9acb7edfca3
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libcurl, libxml2, openssl, pinentry"
+TERMUX_PKG_BUILD_DEPENDS="bash-completion"
 TERMUX_PKG_SUGGESTS="termux-api"
 
 termux_step_post_make_install() {
