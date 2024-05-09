8e7de6b536d514637ecf90ed0e2f3f7bdd04433e
Biswapriyo Nath <nathbappai@gmail.com>
2024-05-09 16:03:55 +0000

    rebuild(deps:main/poppler): link against poppler 24.05.0

--- packages/gegl/build.sh
+++ packages/gegl/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Data flow based image processing framework"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.4.48"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gimp.org/pub/gegl/${TERMUX_PKG_VERSION%.*}/gegl-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=418c26d94be8805d7d98f6de0c6825ca26bd74fcacb6c188da47533d9ee28247
 TERMUX_PKG_DEPENDS="babl, ffmpeg, gdk-pixbuf, glib, json-glib, libandroid-support, libc++, libcairo, libjasper, libjpeg-turbo, libpng, librsvg, libtiff, libwebp, littlecms, openexr, pango, poppler"
