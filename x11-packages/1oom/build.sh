0886fbb71a29251c9fee31a0cead96315d348ffd
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-05-09 11:50:11 +0200

    bump(main/1oom): 1.8.1 (#20030)
    * bump(main/1oom): 1.8.1
    * switch to maintained fork
    * fix(x11/1oom): build against libandroid-shmem
    * fix(x11/1oom): depend on xorgproto
    
    ---------
    
    Co-authored-by: Twaik Yont <twaikyont@gmail.com>

--- x11-packages/1oom/build.sh
+++ x11-packages/1oom/build.sh
@@ -1,13 +1,14 @@
-TERMUX_PKG_HOMEPAGE=https://gitlab.com/Tapani_/1oom
+TERMUX_PKG_HOMEPAGE=https://github.com/1oom-fork/1oom
 TERMUX_PKG_DESCRIPTION="A Master of Orion (1993) game engine recreation"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_SRCURL=https://gitlab.com/Tapani_/1oom/-/archive/v${TERMUX_PKG_VERSION}/1oom-v${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a4273966dde5f3296c08f903ad2e12f522b7d536919bc00e4a0a1d69d457ced0
+TERMUX_PKG_VERSION=1.8.1
+TERMUX_PKG_SRCURL=https://github.com/1oom-fork/1oom/archive/refs/tags/f${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=410c4cac16c8e1b5dcb988a7ed71e1f23eca9afdccd34acf8cd44381c91802cf
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP='\d+\.\d+\.\d+'
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_DEPENDS="readline"
+TERMUX_PKG_DEPENDS="libandroid-shmem, xorgproto, readline"
 TERMUX_PKG_GROUPS="games"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-static
@@ -22,4 +23,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	autoreconf -fi
+	LIBS='-landroid-shmem'
 }
