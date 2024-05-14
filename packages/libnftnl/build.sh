5502eb59c1dbd07a913a4f1dc00554a2461f02a7
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-15 01:54:31 +0200

    fix(main/libnftnl): Fix building with current clang

--- packages/libnftnl/build.sh
+++ packages/libnftnl/build.sh
@@ -3,9 +3,19 @@ TERMUX_PKG_DESCRIPTION="Netfilter library providing interface to the nf_tables s
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.2.6"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://netfilter.org/projects/libnftnl/files/libnftnl-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=ceeaea2cd92147da19f13a35a7f1a4bc2767ff897e838e4b479cf54b59c777f4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libmnl"
 TERMUX_PKG_BREAKS="libnftnl-dev"
 TERMUX_PKG_REPLACES="libnftnl-dev"
+
+termux_step_pre_configure() {
+	# Avoid the below errors:
+	# error: version script assignment of 'LIBNFTNL_11' to symbol 'nftnl_chain_parse' failed: symbol not defined
+	# error: version script assignment of 'LIBNFTNL_11' to symbol 'nftnl_chain_parse_file' failed: symbol not defined
+	# error: version script assignment of 'LIBNFTNL_11' to symbol 'nftnl_set_elems_foreach' failed: symbol not defined
+	# See https://bugs.gentoo.org/914710
+	LDFLAGS+=" -Wl,-undefined-version"
+}
