37cf5059cde7e9e68064b13fc05be844f385b789
Twaik Yont <twaikyont@gmail.com>
2024-05-14 11:17:38 +0300

    bump(x11/gimp): 2.10.38

--- x11-packages/gimp/build.sh
+++ x11-packages/gimp/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gimp.org/
 TERMUX_PKG_DESCRIPTION="GNU Image Manipulation Program"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.10.36"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION="2.10.38"
 TERMUX_PKG_SRCURL=https://download.gimp.org/mirror/pub/gimp/v${TERMUX_PKG_VERSION%.*}/gimp-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=3d3bc3c69a4bdb3aea9ba2d5385ed98ea03953f3857aafd1d6976011ed7cdbb2
+TERMUX_PKG_SHA256=50a845eec11c8831fe8661707950f5b8446e35f30edfb9acf98f85c1133f856e
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="aalib, atk, babl, fontconfig, freetype, gdk-pixbuf, gegl, gexiv2, ghostscript, gimp-data, glib, glib-networking, gtk2, harfbuzz, hicolor-icon-theme, json-glib, libandroid-execinfo, libandroid-shmem, libc++, libcairo, libheif, libjpeg-turbo, libjxl, libmypaint, libpng, librsvg, libtiff, libwebp, libxcursor, libxmu, libxpm, littlecms, mypaint-brushes, openexr, openjpeg, pango, poppler, poppler-data, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-python
