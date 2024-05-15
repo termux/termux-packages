bc09fc8ce7cdcdb53ec7c1b8fac8776256d6e80c
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-15 11:51:35 +0200

    fix(main/libtirpc): Fix building with current clang

--- packages/libtirpc/build.sh
+++ packages/libtirpc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Transport Independent RPC library"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/libtirpc/libtirpc-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=1e0b0c7231c5fa122e06c0609a76723664d068b0dba3b8219b63e6340b347860
 TERMUX_PKG_AUTO_UPDATE=true
@@ -13,6 +14,9 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
+	# Avoid build errors such as: version script assignment of 'TIRPC_0.3.0' to symbol '_svcauth_gss' failed: symbol not defined
+	LDFLAGS+=" -Wl,-undefined-version"
+
 	aclocal
 	automake
 	autoconf
