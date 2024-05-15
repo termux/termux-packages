b33e17f2159a3a3195c2abd5cdaaa84bbf4798e6
pacspedd <pacspedd@outlook.com>
2024-05-15 14:35:12 +0000

    Update

--- packages/termux-switch-manager/build.sh
+++ packages/termux-switch-manager/build.sh
@@ -1,15 +1,16 @@
-TERMUX_PKG_HOMEPAG=https://gist.github.com/DevMasterLinux/ba96cfb7ec6b7e0960f400a1b0d80a24
+TERMUX_PKGHOMEPAGE=https://gist.github.com/DevMasterLinux/ba96cfb7ec6b7e0960f400a1b0d80a24
 TERMUX_PKG_DESCRIPTION="A Tool to Switch the Package Manager fast and safe"
-TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@DevMasterLinux"
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION="1.0"
 
 termux_step_make_install() {
-    cp termux-switch-manager $TERMUX_PREFIX/bin/termux-switch-manager
+    mkdir -p $TERMUX_PREFIX/bin
+    cp $TERMUX_PKG_BUILDER_DIR/termux-switch-manager $TERMUX_PREFIX/bin/
+    cp $TERMUX_PKG_BUILDER_DIR/LICENSE $TERMUX_PREFIX/share/doc/termux-switch-manager/
 }
 
+
 termux_step_create_debscripts() {
-    # Keine Notwendigkeit für Vor-/Nachinstallations-Skripte
-    return 0
+	return 0
 }
-E
