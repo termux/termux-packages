29884960025846a57c28bbaa2e72d3622f65de16
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-16 01:02:47 +0200

    fix(main/libpoco): Update src checksum

--- packages/libpoco/build.sh
+++ packages/libpoco/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="A comprehensive set of C++ libraries that cover all mode
 TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.13.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pocoproject/poco/archive/refs/tags/poco-${TERMUX_PKG_VERSION}-release.tar.gz
-TERMUX_PKG_SHA256=0f0012944924052ebbe90d74cd684f5bc5264be805010a177c5b8df1ce313e43
+TERMUX_PKG_SHA256=9f074d230daf30f550c5bde5528037bdab6aa83b2a06c81a25e89dd3bcb7e419
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP='(?<=-)[^-]+(?=-)'
 TERMUX_PKG_DEPENDS="libc++, libexpat, libsqlite, openssl, pcre2, zlib"
