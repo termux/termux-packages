b61ed4b96527f3700396d27a0247fc9425f6f1fe
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-16 10:32:14 +0200

    fix(main/libltdl): Let the libltdl subpackage contain its header files

--- packages/libtool/libltdl.subpackage.sh
+++ packages/libtool/libltdl.subpackage.sh
@@ -1,3 +1,7 @@
-TERMUX_SUBPKG_INCLUDE="lib/libltdl.so"
+TERMUX_SUBPKG_INCLUDE="
+include/libltdl/
+include/ltdl.h
+lib/libltdl.so
+"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=no
 TERMUX_SUBPKG_DESCRIPTION="Library for dlopening libraries"
