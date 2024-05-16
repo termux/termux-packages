96a1f9d4280403a3338177f0a61625a8854c8aa3
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-16 13:13:32 +0200

    fix(main/libgts): Fix undefined symbols
    Fix the following build error:
    
    > ERROR: ./lib/libgts-0.7.so.5.0.1 contains undefined symbols:
    >    32: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND atan2
    >    64: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND sincos
    >    67: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND cos
    >    77: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND log
    >    78: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND exp

--- packages/libgts/build.sh
+++ packages/libgts/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Provides useful functions to deal with 3D surfaces meshe
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.7.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gts/gts/${TERMUX_PKG_VERSION}/gts-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=059c3e13e3e3b796d775ec9f96abdce8f2b3b5144df8514eda0cc12e13e8b81e
 TERMUX_PKG_DEPENDS="glib"
