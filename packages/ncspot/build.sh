2a1a62d9e445410152e4786494c22bcc1a4f0b10
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-05-19 23:30:46 +0800

    ncspot: bump to 1.1.1

--- packages/ncspot/build.sh
+++ packages/ncspot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/hrkfdn/ncspot
 TERMUX_PKG_DESCRIPTION="An ncurses Spotify client written in Rust"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.0"
+TERMUX_PKG_VERSION="1.1.1"
 TERMUX_PKG_SRCURL=https://github.com/hrkfdn/ncspot/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=516663b62b9536cb18e6d8eb69470a5b6560f2890e010e8a3d2e8cfc65df9497
+TERMUX_PKG_SHA256=89fd70d625304ad882f2b3fcd048ea0e3910a7df371f942feded2ec57b4012b4
 TERMUX_PKG_DEPENDS="dbus, pulseaudio"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -13,7 +13,8 @@ TERMUX_PKG_CONFLICTS="ncspot-mpris"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --no-default-features
---features cursive/termion-backend,share_clipboard,pulseaudio_backend
+--features termion_backend,pulseaudio_backend
 "
 # NOTE: ncurses-rs runs a test while building which fails while cross compiling:
-# therefore, we use cursive/termion-backend instead.
+# therefore, we use termion_backend instead.
+# share_clipboard cannot be used due to 1Password/arboard#56.
