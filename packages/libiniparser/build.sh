6851813906495c5eea2b7f21fbacc133a5b08ee4
tqfx <tqfx@tqfx.org>
2024-05-21 23:48:39 +0800

    bump(main/libiniparser): 4.2.2

--- packages/libiniparser/build.sh
+++ packages/libiniparser/build.sh
@@ -2,15 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ndevilla/iniparser
 TERMUX_PKG_DESCRIPTION="Offers parsing of ini files from the C level"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.2.1"
+TERMUX_PKG_VERSION="4.2.2"
 TERMUX_PKG_SRCURL=https://github.com/ndevilla/iniparser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9120fd13260be1dbec74b8aaf47777c434976626f3b3288c0d17b70e21cce2d2
+TERMUX_PKG_SHA256=82983d9712e321494ee67e1d90963545b6788c198611dab094804a7b7414b233
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make_install() {
-	install -Dm600 -t $TERMUX_PREFIX/lib libiniparser.so.1
-	ln -sf libiniparser.so.1 $TERMUX_PREFIX/lib/libiniparser.so
-	install -Dm600 -t $TERMUX_PREFIX/include/iniparser src/*.h
-}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_EXAMPLES=OFF
+-DBUILD_TESTS=OFF
+-DBUILD_DOCS=OFF
+"
