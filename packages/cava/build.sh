516bbc82d3b863141b56a6dd42774ec07479aace
tqfx <tqfx@tqfx.org>
2024-05-21 23:48:39 +0800

    bump(main/libiniparser): 4.2.2

--- packages/cava/build.sh
+++ packages/cava/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Console-based Audio Visualizer. Works with MPD and Pulse
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_VERSION="0.10.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/karlstav/cava/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=853ee78729ed3501d0cdf9c1947967ad3bfe6526d66a029b4ddf9adaa6334d4f
 TERMUX_PKG_AUTO_UPDATE=true
