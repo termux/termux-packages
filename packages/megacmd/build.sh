3a3c490c478806d2d24f48f039d4b6cd078a9ad0
tqfx <tqfx@tqfx.org>
2024-05-23 23:22:31 +0800

    bump(main/megacmd): 1.7.0

--- packages/megacmd/build.sh
+++ packages/megacmd/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://mega.io/
 TERMUX_PKG_DESCRIPTION="Provides non UI access to MEGA services"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.6.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.7.0"
 TERMUX_PKG_SRCURL=git+https://github.com/meganz/MEGAcmd
 TERMUX_PKG_GIT_BRANCH=${TERMUX_PKG_VERSION}_Linux
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,6 +13,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-static
 --with-pcre=$TERMUX_PREFIX
+--with-ffmpeg=$TERMUX_PREFIX
 ac_cv_lib_pthread_pthread_create=yes
 "
 
