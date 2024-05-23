6a7cddd3009985ebade2bb42c7de9c82059dd6dc
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-24 05:19:32 +0800

    bump(x11/mkvtoolnix): 84.0

--- x11-packages/mkvtoolnix/build.sh
+++ x11-packages/mkvtoolnix/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.bunkus.org/videotools/mkvtoolnix/
 TERMUX_PKG_DESCRIPTION="Set of tools to create, edit and inspect Matroska files"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="81.0"
+TERMUX_PKG_VERSION="84.0"
 TERMUX_PKG_SRCURL=git+https://gitlab.com/mbunkus/mkvtoolnix
 TERMUX_PKG_GIT_BRANCH=release-$TERMUX_PKG_VERSION
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+"
-TERMUX_PKG_DEPENDS="boost, file, libc++, libebml, libflac, libiconv, libmatroska, libogg, libvorbis, pcre2, zlib, qt5-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="boost-headers, qt5-qtbase-cross-tools"
+TERMUX_PKG_DEPENDS="boost, libc++, libebml, libflac, libgmp, libiconv, libmatroska, libogg, libvorbis, qt6-qtbase, zlib"
+TERMUX_PKG_BUILD_DEPENDS="boost-headers, qt6-qtbase-cross-tools"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_fmt=no
@@ -17,7 +17,7 @@ enable_gui=no
 --with-boost-filesystem=boost_filesystem
 --with-boost-system=boost_system
 --with-boost-date-time=boost_date_time
---with-qmake=${TERMUX_PREFIX}/opt/qt/cross/bin/qmake
+--with-qmake6=${TERMUX_PREFIX}/lib/qt6/bin/qmake6
 "
 
 termux_step_pre_configure() {
