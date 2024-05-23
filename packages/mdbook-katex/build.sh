d20477b0458502c078cee556151f8490512fbebc
tqfx <tqfx@tqfx.org>
2024-05-23 20:14:34 +0800

    bump(main/mdbook-katex): 0.9.0
    * fix auto updates

--- packages/mdbook-katex/build.sh
+++ packages/mdbook-katex/build.sh
@@ -2,12 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://github.com/lzanini/mdbook-katex
 TERMUX_PKG_DESCRIPTION="A preprocessor for mdBook, pre-rendering LaTex equations to HTML at build time"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.8.1"
+TERMUX_PKG_VERSION="0.9.0"
 TERMUX_PKG_SRCURL=https://github.com/lzanini/mdbook-katex/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=621425b80cd03fd7c0c462a2fc95feff2d18639228b234dac2cf0353efeb6466
+TERMUX_PKG_SHA256=098b6554fcf87705e1902584b4c352b04ed6f31c3a995aba9a36bc087e22c409
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+$"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_pkg_auto_update() {
+	local latest_tag="$(termux_github_api_get_tag \
+		"${TERMUX_PKG_SRCURL}" latest-regex "${TERMUX_PKG_UPDATE_VERSION_REGEXP}")"
+	if [[ -z "${latest_tag}" ]]; then
+		termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
+	fi
+	termux_pkg_upgrade_version "${latest_tag}"
+}
 
 termux_step_pre_configure() {
 	export OPENSSL_INCLUDE_DIR=$TERMUX_PREFIX/include/openssl
