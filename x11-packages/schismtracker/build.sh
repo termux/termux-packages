9ee6d8910cb6941bf3e6c03f8c803bab962722a2
tqfx <tqfx@tqfx.org>
2024-05-24 21:03:36 +0800

    bump(x11/schismtracker): 20240523

--- x11-packages/schismtracker/build.sh
+++ x11-packages/schismtracker/build.sh
@@ -2,20 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://schismtracker.org/
 TERMUX_PKG_DESCRIPTION="A free and open-source reimplementation of Impulse Tracker, a program used to create high quality music"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="20240515"
+TERMUX_PKG_VERSION="20240523"
 TERMUX_PKG_SRCURL=https://github.com/schismtracker/schismtracker/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=34a016d85af2aff3d9cf250edfe543b06ff9b65b7e87c1d7d326238d6835a6de
+TERMUX_PKG_SHA256=956e844c0da6ff57a97ee27a173cb07c9c9c550f24bce23a3525f37936ee4fb2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libflac, libx11, libxv, sdl2"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-ac_cv_prog_SDL_CONFIG=$TERMUX_PREFIX/bin/sdl2-config
 ac_cv_prog_WINDRES=
 ac_cv_prog_ac_ct_WINDRES=
 "
 
 termux_step_pre_configure() {
-	autoreconf -fi
+	autoreconf -fi -I$TERMUX_PREFIX/share/aclocal
 }
 
 termux_step_post_configure() {
