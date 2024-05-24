cba39224dc4ba80148e3ec46d67f2813397cab9e
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-24 09:06:44 +0200

    chore(scripts/setup-ninja): Update ninja from 1.12.0 to 1.12.1
    [no ci]

--- scripts/build/setup/termux_setup_ninja.sh
+++ scripts/build/setup/termux_setup_ninja.sh
@@ -1,5 +1,5 @@
 termux_setup_ninja() {
-	local NINJA_VERSION=1.12.0
+	local NINJA_VERSION=1.12.1
 	local NINJA_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
@@ -14,7 +14,7 @@ termux_setup_ninja() {
 			local NINJA_ZIP_FILE=$TERMUX_PKG_TMPDIR/ninja-$NINJA_VERSION.zip
 			termux_download https://github.com/ninja-build/ninja/releases/download/v$NINJA_VERSION/ninja-linux.zip \
 				"$NINJA_ZIP_FILE" \
-				ddc96efa3c7c9d41de733d15e2eda07a8a212555cb43f35d727e080d2ca687ab
+				6f98805688d19672bd699fbbfa2c2cf0fc054ac3df1f0e6a47664d963d530255
 			unzip "$NINJA_ZIP_FILE" -d "$NINJA_FOLDER"
 			chmod 755 $NINJA_FOLDER/ninja
 		fi
