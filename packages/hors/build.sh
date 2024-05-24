395ab55b391464f5b396d86a104e6a2c9da0be10
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-24 09:05:01 +0200

    chore(main/hors): Remove no longer no-integrated-as on arm

--- packages/hors/build.sh
+++ packages/hors/build.sh
@@ -3,17 +3,12 @@ TERMUX_PKG_DESCRIPTION="Instant coding answers via the command line (howdoi in r
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=0.8.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/WindSoilder/hors/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=22419b26f64a2793759d3a3616df58196897cd9227074f475aeb3e1c366296a9
-TERMUX_PKG_BUILD_DEPENDS="binutils-cross"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	if [ "$TERMUX_ARCH" = arm ]; then
-		termux_setup_no_integrated_as
-	fi
-
 	rm -f Makefile
 }
