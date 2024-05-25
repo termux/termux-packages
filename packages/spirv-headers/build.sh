d6b91881f1eec44213da8a157856cd5318504887
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-25 15:57:18 +0800

    chore(main/spirv-headers): auto update use sort -V
    %ci:no-build

--- packages/spirv-headers/build.sh
+++ packages/spirv-headers/build.sh
@@ -11,11 +11,11 @@ TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+\.\d+"
 
 termux_pkg_auto_update() {
 	local api_url="https://api.github.com/repos/KhronosGroup/SPIRV-Headers/git/refs/tags"
-	local latest_refs_tags=$(curl -s "${api_url}" | jq .[].ref | grep -oP vulkan-sdk-${TERMUX_PKG_UPDATE_VERSION_REGEXP} | sort)
+	local latest_refs_tags=$(curl -s "${api_url}" | jq .[].ref | grep -oP vulkan-sdk-${TERMUX_PKG_UPDATE_VERSION_REGEXP})
 	if [[ -z "${latest_refs_tags}" ]]; then
 		echo "WARN: Unable to get latest refs tags from upstream. Try again later." >&2
 		return
 	fi
-	local latest_version=$(echo "${latest_refs_tags}" | tail -n1)
+	local latest_version=$(echo "${latest_refs_tags}" | sort -V | tail -n1)
 	termux_pkg_upgrade_version "${latest_version//vulkan-sdk-}"
 }
