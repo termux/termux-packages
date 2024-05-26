ba02c3a7b393842d74d2b7b134b12d45ce3c10b0
Sylirre <sylirre@termux.dev>
2024-05-26 11:29:28 +0300

    fix(main/croc): upstream fixed tag from 10.04 to 10.0.4

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another"
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="10.04"
-TERMUX_PKG_SRCURL=https://github.com/schollz/croc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c9bbfcd2503c8d5c33ec91b0a628b116be71ac4114ad17b6afa3aed99424caf5
+TERMUX_PKG_VERSION="1:10.0.4"
+TERMUX_PKG_SRCURL=https://github.com/schollz/croc/archive/refs/tags/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=ca373c086995f4f415fb2a79526577fbc1cc5eb260caf750180a7773f7cd373f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
