9626f6de8d6d1ceb6ce3738ca3fd36f216180e43
Twaik Yont <twaikyont@gmail.com>
2024-05-29 14:35:11 +0300

    bump(main/k2pdfopt): 2.55

--- packages/k2pdfopt/build.sh
+++ packages/k2pdfopt/build.sh
@@ -2,14 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://www.willus.com/k2pdfopt/
 TERMUX_PKG_DESCRIPTION="A tool that optimizes PDF files for viewing on mobile readers"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.54
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION="2.55"
 TERMUX_PKG_SRCURL=https://www.willus.com/k2pdfopt/src/k2pdfopt_v${TERMUX_PKG_VERSION}_src.zip
-TERMUX_PKG_SHA256=43490a55bdaecdd2300276221b6a713b754e7c617e80481191c04c67b3357b38
+TERMUX_PKG_SHA256=3e78b4c7dd6227fde12138fd2468dd13c0c45b5251592a4f0aac67fd139ab953
 TERMUX_PKG_DEPENDS="djvulibre, gsl, leptonica, libjasper, libjpeg-turbo, libpng, mupdf, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DHAVE_MUPDF_LIB=1"
-	LDFLAGS+=" -lmupdf"
+	LDFLAGS+=" -lmupdf -L$TERMUX_PREFIX/lib"
 }
