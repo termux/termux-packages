1426d34ba6a41e54cd2b7bbe707f02e6c272de50
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-30 18:13:44 +0800

    bump(x11/qterminal): 2.0.0

--- x11-packages/qterminal/build.sh
+++ x11-packages/qterminal/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="A lightweight Qt terminal emulator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/qterminal/releases/download/${TERMUX_PKG_VERSION}/qterminal-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=8313326ac3ef728924271cd60a8f79e17e5e755a6bfa4e4419a4b7a53528659f
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtx11extras, qtermwidget"
-TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_SHA256=28f00a5c8c31d9108a54b7dcbf87d46f14e460492d2c56e3ecf5b5c3bba4059f
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qtermwidget"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
+"
