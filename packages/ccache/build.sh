a0cbfa08cf20343200ebd688c927d4ff96918b66
tqfx <tqfx@tqfx.org>
2024-05-30 22:20:39 +0800

    bump(main/ccache): 4.10

--- packages/ccache/build.sh
+++ packages/ccache/build.sh
@@ -2,18 +2,24 @@ TERMUX_PKG_HOMEPAGE=https://ccache.samba.org
 TERMUX_PKG_DESCRIPTION="Compiler cache for fast recompilation of C/C++ code"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.9.1"
+TERMUX_PKG_VERSION="4.10"
 TERMUX_PKG_SRCURL=https://github.com/ccache/ccache/releases/download/v$TERMUX_PKG_VERSION/ccache-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=4c03bc840699127d16c3f0e6112e3f40ce6a230d5873daa78c60a59c7ef59d25
+TERMUX_PKG_SHA256=83630b5e922b998ab2538823e0cad962c0f956fad1fcf443dd5288269a069660
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, zlib, zstd"
+TERMUX_PKG_DEPENDS="libandroid-spawn, libc++, libhiredis, xxhash, zlib, zstd"
+TERMUX_PKG_BUILD_DEPENDS="fmt"
 
 #[46/89] Building ASM object src/third_party/blake3/CMakeFiles/blake3.dir/blake3_sse2_x86-64_unix.S.o
 #FAILED: src/third_party/blake3/CMakeFiles/blake3.dir/blake3_sse2_x86-64_unix.S.o
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DDEPS=LOCAL
+-DENABLE_TESTING=OFF
 -DHAVE_ASM_AVX2=FALSE
 -DHAVE_ASM_AVX512=FALSE
 -DHAVE_ASM_SSE2=FALSE
 -DHAVE_ASM_SSE41=FALSE
--DREDIS_STORAGE_BACKEND=OFF
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-spawn"
+}
