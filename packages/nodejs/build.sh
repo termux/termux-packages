073b90bc23658391e246ccb50f7e600fec3c78a5
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-31 00:19:45 +0200

    bump(main/nodejs): 22.2.0

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=21.7.3
+TERMUX_PKG_VERSION=22.2.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=668b26fb1bfc1cff60622bbcf3d715843e16f50c8f30e9f64fb4141814d79a21
+TERMUX_PKG_SHA256=889908a8828d1484910d7e659b6aa57ade8d528ff0e390e9a77ef659a7628474
 # thunder-coding: don't try to autoupdate nodejs, that thing takes 2 whole hours to build for a single arch, and requires a lot of patch updates everytime. Also I run tests everytime I update it to ensure least bugs
 TERMUX_PKG_AUTO_UPDATE=false
 # Note that we do not use a shared libuv to avoid an issue with the Android
