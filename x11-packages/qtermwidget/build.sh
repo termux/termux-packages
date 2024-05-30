97eb8ac7d52d79e255398abbabc6d4d47b72f8ef
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-30 18:14:14 +0800

    bump(x11/qtermwidget): 2.0.0

--- x11-packages/qtermwidget/build.sh
+++ x11-packages/qtermwidget/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="A terminal emulator widget for Qt 5"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/qtermwidget/releases/download/${TERMUX_PKG_VERSION}/qtermwidget-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=e7253065f74fa056843c95f9543e690bcaf7a03647ef5e02379eec4eb162026b
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_SHA256=33ff5c84b93f7a46e5255ae6a25e073093e0f80cc7dacf0233e01b643663f700
+TERMUX_PKG_DEPENDS="libc++, libx11, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
+"
