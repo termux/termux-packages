5e60b541364ba1719735c1b1cbc7d961617043d0
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-05-31 15:59:15 +0200

    bump(x11/hicolor-icon-theme): 0.18 (#20361)
    - enable auto updates

--- x11-packages/hicolor-icon-theme/build.sh
+++ x11-packages/hicolor-icon-theme/build.sh
@@ -2,13 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/icon-theme/
 TERMUX_PKG_DESCRIPTION="Freedesktop.org Hicolor icon theme"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.17
-TERMUX_PKG_REVISION=29
-TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/default-icon-theme/-/archive/${TERMUX_PKG_VERSION}/default-icon-theme-master.tar.gz
-TERMUX_PKG_SHA256=a0bd3e5a3b0c109a2ccb5aa2d150c3b8a485de532f674c5fe3ffd18e97cf17cd
+TERMUX_PKG_VERSION=0.18
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xdg/default-icon-theme/-/archive/v${TERMUX_PKG_VERSION}/default-icon-theme-v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9227ec70c6b59a715a18dcedbed590cb08edc9eadb73fb2b0a57034e15c18f36
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	./autogen.sh
-}
+TERMUX_PKG_AUTO_UPDATE=true
