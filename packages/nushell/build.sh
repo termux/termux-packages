71e3f09414d2508e03a9e13bccd6e72c6a0f819d
tqfx <tqfx@tqfx.org>
2024-06-01 21:25:05 +0800

    bump(main/nushell): 0.94.0 (#20346)

--- packages/nushell/build.sh
+++ packages/nushell/build.sh
@@ -2,13 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.nushell.sh
 TERMUX_PKG_DESCRIPTION="A new type of shell operating on structured data"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.93.0"
+TERMUX_PKG_VERSION="0.94.0"
 TERMUX_PKG_SRCURL=https://github.com/nushell/nushell/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=00dcd5ab112d8afd683aa0b87b65b2e47a45487857a6d2481ce7eeb0045c2c00
+TERMUX_PKG_SHA256=697a3cc040f673c9eb74e31d8f9cce85f5d4d5302ea34277cfd16aacf9a495a5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=("--no-default-features")
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--no-default-features
+--features=default-no-clipboard
+"
 
 termux_step_pre_configure() {
 	termux_setup_rust
@@ -25,25 +28,13 @@ termux_step_pre_configure() {
 		popd
 	fi
 
-	local _features="default-no-clipboard"
-	if [ $TERMUX_ARCH != "i686" ] && [ $TERMUX_ARCH != "arm" ]; then
-		_features+=" dataframe"
-	fi
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=("--features=$_features")
-
 	: "${CARGO_HOME:=$HOME/.cargo}"
 	export CARGO_HOME
 
-	rm -rf $CARGO_HOME/registry/src/*/interprocess-*
 	rm -rf $CARGO_HOME/registry/src/*/libmimalloc-sys-*
 	cargo fetch --target "${CARGO_TARGET_NAME}"
 
 	local d p
-	p="interprocess-socklen_t.diff"
-	for d in $CARGO_HOME/registry/src/*/interprocess-*; do
-		patch --silent -p1 -d ${d} < "${TERMUX_PKG_BUILDER_DIR}/${p}"
-	done
-
 	p="libmimalloc-sys-tls.diff"
 	for d in $CARGO_HOME/registry/src/*/libmimalloc-sys-*; do
 		patch --silent -p1 -d ${d} < "${TERMUX_PKG_BUILDER_DIR}/${p}"
@@ -65,16 +56,6 @@ termux_step_pre_configure() {
 		$_CARGO_TARGET_LIBDIR/libz.so
 }
 
-termux_step_make_install() {
-	cargo install \
-			--path . \
-			--jobs $TERMUX_MAKE_PROCESSES \
-			--no-track \
-			--target $CARGO_TARGET_NAME \
-			--root $TERMUX_PREFIX \
-			"${TERMUX_PKG_EXTRA_CONFIGURE_ARGS[@]}"
-}
-
 termux_step_post_make_install() {
 	mv $TERMUX_PREFIX/lib/libz.so.1{.tmp,}
 	mv $TERMUX_PREFIX/lib/libz.so{.tmp,}
@@ -84,6 +65,5 @@ termux_step_post_massage() {
 	rm -f lib/libz.so.1
 	rm -f lib/libz.so
 
-	rm -rf $CARGO_HOME/registry/src/*/interprocess-*
 	rm -rf $CARGO_HOME/registry/src/*/libmimalloc-sys-*
 }
