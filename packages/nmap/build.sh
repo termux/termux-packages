e377e10668a79420fa4204ee848103440bc9dbb5
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-02 10:58:01 +0200

    bump(main/nmap): 7.95

--- packages/nmap/build.sh
+++ packages/nmap/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Utility for network discovery and security auditing"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=7.94
+TERMUX_PKG_VERSION=7.95
 TERMUX_PKG_SRCURL=https://nmap.org/dist/nmap-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=d71be189eec43d7e099bac8571509d316c4577ca79491832ac3e1217bc8f92cc
-TERMUX_PKG_DEPENDS="libc++, liblua54, libpcap, libssh2, openssl, pcre, resolv-conf, zlib"
+TERMUX_PKG_SHA256=e14ab530e47b5afd88f1c8a2bac7f89cd8fe6b478e22d255c5b9bddb7a1c5778
+TERMUX_PKG_DEPENDS="libc++, liblua54, libpcap, libssh2, openssl, pcre2, resolv-conf, zlib"
 TERMUX_PKG_RECOMMENDS="nmap-ncat"
 # --without-nmap-update to avoid linking against libsvn_client:
 # --without-zenmap to avoid python scripts for graphical gtk frontend:
