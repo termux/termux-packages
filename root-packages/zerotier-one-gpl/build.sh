0aff5bb79a647b0719e64f2eb617c6b2c6754474
eval Nya <60149113+nexplorer-3e@users.noreply.github.com>
2024-06-04 01:21:13 +0800

    chore: ci: use tab for indent

--- root-packages/zerotier-one-gpl/build.sh
+++ root-packages/zerotier-one-gpl/build.sh
@@ -8,13 +8,13 @@ TERMUX_PKG_SHA256=557a444127812384265ec97232bae43dce1d4b1545ddd72e2b1646c971dad7
 TERMUX_PKG_DEPENDS="natpmpc, miniupnpc"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_SERVICE_SCRIPT=(
-    'zerotier-one' 'exec su -c "':$TERMUX_PREFIX'/bin/zerotier-one -d"'
+	'zerotier-one' 'exec su -c "':$TERMUX_PREFIX'/bin/zerotier-one -d"'
 )
 
 termux_step_configure() {
-    :
+	:
 }
 
 termux_step_post_configure() {
-    export AS="as"
+	export AS="as"
 }
