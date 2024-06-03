ad474a0452ed3e5c615073ebfecc406a4e4880c5
Jia Yuan Lo <jylo06g@gmail.com>
2024-06-04 07:52:27 +0800

    fix(main/librav1e): zlib build conflict (#20412)

--- packages/librav1e/build.sh
+++ packages/librav1e/build.sh
@@ -3,11 +3,16 @@ TERMUX_PKG_DESCRIPTION="An AV1 encoder library focused on speed and safety"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.7.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xiph/rav1e/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=da7ae0df2b608e539de5d443c096e109442cdfa6c5e9b4014361211cf61d030c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/libz.a
+lib/libz.so
+"
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
@@ -20,16 +25,21 @@ termux_step_post_get_source() {
 	fi
 }
 
-termux_step_make_install() {
+termux_step_pre_configure() {
 	termux_setup_rust
 	termux_setup_cargo_c
+
+	# https://github.com/termux/termux-packages/issues/20100
+	mv ${TERMUX_PREFIX}/lib/libz.a{,.tmp} || :
+	mv ${TERMUX_PREFIX}/lib/libz.so{,.tmp} || :
 
 	export CARGO_BUILD_TARGET=$CARGO_TARGET_NAME
 
 	cargo fetch \
-		--target $CARGO_TARGET_NAME \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+		--target $CARGO_TARGET_NAME
+}
 
+termux_step_make_install() {
 	cargo install \
 		--jobs $TERMUX_MAKE_PROCESSES \
 		--path . \
@@ -37,8 +47,7 @@ termux_step_make_install() {
 		--locked \
 		--no-track \
 		--target $CARGO_TARGET_NAME \
-		--root $TERMUX_PREFIX \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+		--root $TERMUX_PREFIX
 
 	# `cargo cinstall` refuses to work with Android
 	cargo cbuild \
@@ -46,17 +55,19 @@ termux_step_make_install() {
 		--prefix $TERMUX_PREFIX \
 		--jobs $TERMUX_MAKE_PROCESSES \
 		--frozen \
-		--target $CARGO_TARGET_NAME \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
+		--target $CARGO_TARGET_NAME
 
 	cd target/$CARGO_TARGET_NAME/release/
-	mkdir -p $TERMUX_PREFIX/include/rav1e/
-	cp rav1e.h $TERMUX_PREFIX/include/rav1e/
-	mkdir -p $TERMUX_PREFIX/lib/pkgconfig/
-	cp rav1e.pc $TERMUX_PREFIX/lib/pkgconfig/
-	cp librav1e.a $TERMUX_PREFIX/lib/
-	cp librav1e.so $TERMUX_PREFIX/lib/librav1e.so.$TERMUX_PKG_VERSION
-	ln -s librav1e.so.$TERMUX_PKG_VERSION \
+	install -Dm644 -t $TERMUX_PREFIX/include/rav1e/ rav1e.h
+	install -Dm644 -t $TERMUX_PREFIX/lib/pkgconfig/ rav1e.pc
+	install -Dm644 -t $TERMUX_PREFIX/lib/ librav1e.a
+	install -Dm644 librav1e.so $TERMUX_PREFIX/lib/librav1e.so.$TERMUX_PKG_VERSION
+	ln -fs librav1e.so.$TERMUX_PKG_VERSION \
 		$TERMUX_PREFIX/lib/librav1e.so.${TERMUX_PKG_VERSION%%.*}
-	ln -s librav1e.so.$TERMUX_PKG_VERSION $TERMUX_PREFIX/lib/librav1e.so
+	ln -fs librav1e.so.$TERMUX_PKG_VERSION $TERMUX_PREFIX/lib/librav1e.so
+}
+
+termux_step_post_make_install() {
+	mv ${TERMUX_PREFIX}/lib/libz.a{.tmp,} || :
+	mv ${TERMUX_PREFIX}/lib/libz.so{.tmp,} || :
 }
