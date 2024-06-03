39bac07b077bcba544ae5dc059bb6dd9f28768ef
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-03 10:05:56 +0200

    fix(main/libsamplerate): Fix undefined symbols on 32-bit builds

--- packages/libsamplerate/build.sh
+++ packages/libsamplerate/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library for performing sample rate conversion of audio
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libsndfile/libsamplerate/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=16e881487f184250deb4fcb60432d7556ab12cb58caea71ef23960aec6c0405a
 TERMUX_PKG_FORCE_CMAKE=true
