9bd0dd63cac6a9cb23645aeb75623d6293ad16e0
tqfx <tqfx@tqfx.org>
2024-06-05 13:56:06 +0800

    libmediainfo,mediainfo: remove termux_pkg_auto_update introduced by #19852 (#20388)

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -4,34 +4,12 @@ TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="24.05"
-TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/libmediainfo/${TERMUX_PKG_VERSION}/libmediainfo_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b14f79104b1d0d78de9fb42805f29d54fdf01c3e4291cc3638e04d09d145b8d6
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/MediaArea/MediaInfoLib/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=98fa7516fc50772231575da0362bb03ab0bf050c90a8e2de75987127263154af
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libcurl, libzen, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-shared --enable-static --with-libcurl"
-
-termux_pkg_auto_update() {
-	local e=0
-	local api_url="https://mediaarea.net/en/MediaInfo"
-	local api_url_r=$(curl -Ls "${api_url}/")
-	local r1=$(echo "${api_url_r}" | grep -o '"softwareVersion"\s*:\s*"\([^"]\+\)"')
-	local latest_version=$(echo "${r1}" | grep -o '[0-9.]\+')
-
-	[[ -z "${api_url_r}" ]] && e=1
-	[[ -z "${r1}" ]] && e=1
-	[[ -z "${latest_version}" ]] && e=1
-	if [[ "${e}" != 0 ]]; then
-		cat <<- EOL >&2
-		WARN: Auto update failure!
-		api_url_r=${api_url_r}
-		r1=${r1}
-		latest_version=${latest_version}
-		EOL
-		return
-	fi
-
-	termux_pkg_upgrade_version "${latest_version}"
-}
 
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR="${TERMUX_PKG_SRCDIR}/Project/GNU/Library"
