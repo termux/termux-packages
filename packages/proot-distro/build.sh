6532bdc4ad800f9d703a286ac6fb30a0354040f9
Sylirre <sylirre@termux.dev>
2024-06-06 02:54:38 +0300

    bump(main/proot-distro): 4.14.0

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.13.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=4.14.0
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d2f7e9edd99303ef7d1163c3b8d19c9355ed0af0f203f96ea30d49530f7773a9
-TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, termux-tools, xz-utils"
+TERMUX_PKG_SHA256=ff2fd8cf1837fdc6c3d40145f89dbbceb10e90e2ab16ff82fddc60648bb34b0f
+TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, termux-tools, util-linux, xz-utils"
 TERMUX_PKG_SUGGESTS="bash-completion, termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
