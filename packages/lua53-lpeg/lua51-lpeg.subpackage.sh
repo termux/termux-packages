624528d18fdd440ed07421f23105633989f97b40
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-06-05 22:30:30 +0200

    enhance(main/lua-lpeg): build for 5.3 and 5.1 (#20418)
    part of #20273, lua-lpeg5.1 required by neovim >= 0.10.0

---
+++ packages/lua53-lpeg/lua51-lpeg.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
+TERMUX_SUBPKG_INCLUDE='lib/lua/5.1/lpeg.so share/lua/5.1/re.lua'
+TERMUX_SUBPKG_DESCRIPTION='Pattern-matching library for Lua 5.1'
+TERMUX_SUBPKG_DEPENDS='liblua51'
+
