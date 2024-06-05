12b3e16e6d778f3dd6bb5b6bd0fff0c3def7f266
Twaik Yont <twaikyont@gmail.com>
2024-06-06 01:35:48 +0300

    bump(main/ta-lib): 0.5.0 (#20128)
    Co-authored-by: TomIO <43716232+TomJo2000@users.noreply.github.com>

--- packages/ta-lib/build.sh
+++ packages/ta-lib/build.sh
@@ -1,15 +1,16 @@
-TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/ta-lib/
+TERMUX_PKG_HOMEPAGE=https://ta-lib.org/
 TERMUX_PKG_DESCRIPTION="Technical analysis library with indicators like ADX"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/ta-lib/ta-lib-$TERMUX_PKG_VERSION-src.tar.gz
-TERMUX_PKG_SHA256=9ff41efcb1c011a4b4b6dfc91610b06e39b1d7973ed5d4dee55029a0ac4dc651
+TERMUX_PKG_VERSION=0.5.0
+TERMUX_PKG_SRCURL=https://github.com/TA-Lib/ta-lib/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=43e3761cf6bc4a5ab6c675268a09a72ea074643c6e06defe5e4b4e51eae1ea50
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BREAKS="talib"
 TERMUX_PKG_REPLACES="talib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	curl https://sourceforge.net/p/ta-lib/code/HEAD/tree/tags/release-${TERMUX_PKG_VERSION//./-}/ta-lib/LICENSE.TXT?format=raw -o $TERMUX_PKG_SRCDIR/LICENSE
+	LDFLAGS+=" -lm"
+	autoreconf -fi
 }
