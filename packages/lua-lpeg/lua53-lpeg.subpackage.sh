d4206e14aaacce1d07f0d6d404bccc20055c2d0b
Komo <71205197+cattokomo@users.noreply.github.com>
2024-06-09 23:02:45 +0800

    enhance(main/lua-lpeg): build for all liblua5x and fix linking (#20473)
    Co-authored-by: TomIO <43716232+TomJo2000@users.noreply.github.com>

---
+++ packages/lua-lpeg/lua53-lpeg.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
+TERMUX_SUBPKG_INCLUDE='lib/liblpeg-5.3.so lib/lua/5.3/lpeg.so share/lua/5.3/re.lua'
+TERMUX_SUBPKG_DESCRIPTION='Pattern-matching library for Lua 5.3'
+TERMUX_SUBPKG_DEPENDS='liblua53'
