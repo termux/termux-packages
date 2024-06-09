cafd8ed1a7859632ece66f54d3189044d161e3e4
Kevin Williams <admin@utermux.dev>
2024-06-09 21:22:16 +0800

    upgpkg(main/artalk): 2.8.7
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/artalk/build.sh
+++ packages/artalk/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://artalk.js.org/
 TERMUX_PKG_DESCRIPTION="A self-hosted comment system"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Moraxyc <termux@qaq.li>"
-TERMUX_PKG_VERSION=2.8.6
+TERMUX_PKG_VERSION=2.8.7
 TERMUX_PKG_SRCURL=(https://github.com/ArtalkJS/Artalk/archive/v${TERMUX_PKG_VERSION}.tar.gz
                    https://github.com/ArtalkJS/Artalk/releases/download/v${TERMUX_PKG_VERSION}/artalk_ui.tar.gz)
-TERMUX_PKG_SHA256=(1d6abad32da1fe88dcc38bda3a61070820126598b2fdba2c9f5808b72d0d0fd1
-                   dd183998216280b919f97f05c5ee80d7a4c777d8fa85c4d830400a5354ab2ccc)
+TERMUX_PKG_SHA256=(62e6fb2dbd8fa5c93a639b83874dafbe1ea3893477760a9e764a8085cdbc94e1
+                   619b2d823cfaab7d9e98a4520b9f3f4d1f0e61d4aaf6022b13fb12f1830540ba)
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_AUTO_UPDATE=true
+# termux_pkg_upgrade_version couldn't check multiple versions now.
+TERMUX_PKG_AUTO_UPDATE=false
 
 termux_step_post_get_source() {
 	mv artalk_ui/* public
