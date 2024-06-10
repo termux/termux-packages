66ca340e1de4b5e6335b2818b59a4566859eed96
Lucy Phipps <landfillbaby69@gmail.com>
2024-06-10 18:49:19 +0100

    invisible typo

--- packages/7zip/build.sh
+++ packages/7zip/build.sh
@@ -56,5 +56,5 @@ termux_step_make_install() {
 		-t "$TERMUX_PREFIX"/share/doc/"$TERMUX_PKG_NAME" \
 		"$TERMUX_PKG_BUILDDIR"/DOC/{7zC,7zFormat,copying,License,lzma,Methods,readme,src-history}.txt
 	tar -C "$TERMUX_PREFIX"/share/doc/"$TERMUX_PKG_NAME" \
- 		-xf "$TERMUX_PKG_CACHEDIR/$(basename "$TERMUX_PKG_SRCURL[1]")" MANUAL
+		-xf "$TERMUX_PKG_CACHEDIR/$(basename "$TERMUX_PKG_SRCURL[1]")" MANUAL
 }
