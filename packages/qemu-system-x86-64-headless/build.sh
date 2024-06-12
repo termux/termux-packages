28264444656f0a21c0d949d45bf398ba403d42aa
Sylirre <sylirre@termux.dev>
2024-06-13 00:10:05 +0300

    revbump(main/qemu-system-x86-64-headless): trigger build again
    Previous ci run will fail due to issue in build recipe in one of submitted packages

--- packages/qemu-system-x86-64-headless/build.sh
+++ packages/qemu-system-x86-64-headless/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualiz
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1:8.2.5
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION:2}.tar.xz
 TERMUX_PKG_SHA256=65693b4a64f28be7501ed0dd3e042478780c516de3f17813d1c32ebb2eb249a9
 TERMUX_PKG_DEPENDS="glib, libbz2, libcurl, libgmp, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libslirp, libspice-server, libssh, libusb, libusbredir, ncurses, pulseaudio, qemu-common, resolv-conf, zlib, zstd"
