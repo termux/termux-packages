7bd9b3b98945f99d21e2ec43b56e070be01e4011
Sylirre <sylirre@termux.dev>
2024-06-13 00:03:17 +0300

    upgpkg(main/qemu-system-x86-64-headless): 8.2.5

--- packages/qemu-system-x86-64-headless/build.sh
+++ packages/qemu-system-x86-64-headless/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.qemu.org
 TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (headless)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:8.0.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1:8.2.5
 TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION:2}.tar.xz
-TERMUX_PKG_SHA256=f060abd435fbe6794125e2c398568ffc3cfa540042596907a8b18edca34cf6a5
+TERMUX_PKG_SHA256=65693b4a64f28be7501ed0dd3e042478780c516de3f17813d1c32ebb2eb249a9
 TERMUX_PKG_DEPENDS="glib, libbz2, libcurl, libgmp, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libslirp, libspice-server, libssh, libusb, libusbredir, ncurses, pulseaudio, qemu-common, resolv-conf, zlib, zstd"
 
 # Required by configuration script, but I can't find any binary that uses it.
@@ -108,7 +107,6 @@ termux_step_configure() {
 		--enable-curl \
 		--enable-fdt \
 		--enable-kvm \
-		--disable-hax \
 		--disable-hvf \
 		--disable-whpx \
 		--enable-libnfs \
