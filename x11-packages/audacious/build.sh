003516d76e04e48dd48757b2f1698e3a4bd7c139
tqfx <tqfx@tqfx.org>
2024-06-13 23:27:47 +0800

    bump(x11/audacious): 4.4

--- x11-packages/audacious/build.sh
+++ x11-packages/audacious/build.sh
@@ -2,19 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://audacious-media-player.org
 TERMUX_PKG_DESCRIPTION="An advanced audio player"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.3.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="4.4"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://distfiles.audacious-media-player.org/audacious-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=85e9e26841505b51e342ee72a2d05f19bef894f567a029ebb3f3e0c1adb42042
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, dbus-glib"
+TERMUX_PKG_SHA256=aadc5d26ea2954236a00153e424094d9e6eb55c5c324c08fd0491b7c2ae2f830
+TERMUX_PKG_DEPENDS="libarchive, libc++, qt5-qtbase, qt5-qtsvg, dbus-glib"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools"
 TERMUX_PKG_RECOMMENDS="audacious-plugins"
 # Audacious out-of-source build doesn't seem to work
 TERMUX_PKG_BUILD_IN_SRC=true
 # Audacious has switched to Qt toolkit and it's the default GUI option now
 # Disable GTK to reduce the size and dependencies
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-qt --disable-gtk"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-libarchive --enable-qt5 --disable-gtk"
 
 termux_step_pre_configure() {
 	CFLAGS+=" -Wno-enum-constexpr-conversion"
