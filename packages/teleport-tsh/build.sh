6e94eb021e5a19c414eb5cc39c65776c88ccefb3
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-17 08:20:34 +0530

    fix(main/teleport-tsh): Fix missing version in k8s go module (#20457)
    Previously, the aarch64 binary from kubectl-version/main.go was run
    inside x86_64 container and the kubectl_version became empty string.
    It causes the following runtime error.
    
    panic: version string "" doesn't match expected regular expression: "^v(\d+\.\d+\.\d+)"
    
    Now, the kubectl_version is retrieved from host build.

--- packages/teleport-tsh/build.sh
+++ packages/teleport-tsh/build.sh
@@ -3,10 +3,21 @@ TERMUX_PKG_DESCRIPTION="Secure Access for Developers that doesn't get in the way
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="16.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/gravitational/teleport/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=64546cb907a1e8df66604d8597c3c94ec00ea6b3065981e9935677908c6755da
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	termux_setup_golang
+	pushd "$TERMUX_PKG_SRCDIR"
+
+	# from Makefile
+	export KUBECTL_VERSION=$(go run ./build.assets/kubectl-version/main.go)
+	popd
+}
 
 termux_step_make() {
 	termux_setup_golang
