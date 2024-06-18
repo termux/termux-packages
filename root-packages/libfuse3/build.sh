4b4ab84ea868b0ddb22e579a34ac448d6a0ac430
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-18 11:26:38 +0200

    bump(root/libfuse3): 3.16.2
    Specifically this update fixes the build of libfuse3, which on current
    3.15.1 version and NDK toolchain fails with:
    
    > ld.lld: error: version script assignment of 'FUSE_3.0' to symbol 'fuse_register_module' failed: symbol not defined
    > ld.lld: error: version script assignment of 'FUSE_3.4' to symbol 'fuse_reply_copy_file_range' failed: symbol not defined
    
    This was fixed upstream in
    https://github.com/libfuse/libfuse/commit/624783d73baf151747fa72359af16c37260bfee2.

--- root-packages/libfuse3/build.sh
+++ root-packages/libfuse3/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/libfuse/libfuse
 TERMUX_PKG_DESCRIPTION="FUSE (Filesystem in Userspace) is an interface for userspace programs to export a filesystem to the Linux kernel"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=3.15.1
+TERMUX_PKG_VERSION=3.16.2
 TERMUX_PKG_SRCURL=https://github.com/libfuse/libfuse/archive/fuse-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cb93e170288804d2e83da9b69925d968655ed75883476773ba5268d08bb1d335
+TERMUX_PKG_SHA256=1bc306be1a1f4f6c8965fbdd79c9ccca021fdc4b277d501483a711cbd7dbcd6c
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddisable-mtab=true
