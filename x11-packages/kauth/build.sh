d24389aa091e04e8cd0f67fab96d69997bc4800f
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-18 14:43:11 +0000

    bump(x11/kauth): 6.3.0

--- x11-packages/kauth/build.sh
+++ x11-packages/kauth/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Framework which lets applications perform actions as a privileged user (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.112.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="6.3.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kauth-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=67cbe41ea269b34ffe9cab1833d18fa203394d8432964b509fdeb7712d9405f7
+TERMUX_PKG_SHA256=ebcf36816ff826a2cfb3ed0a0d3746d7da9a23f79901ec43b7a4e7b156af33b5
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, kcoreaddons"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, kcoreaddons"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
