02b6551939d244ab64e9724204d991cc54c8ba68
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-18 14:43:11 +0000

    bump(x11/kconfig): 6.3.0

--- x11-packages/kconfig/build.sh
+++ x11-packages/kconfig/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Advanced configuration system (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.112.0"
+TERMUX_PKG_VERSION="6.3.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kconfig-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=86de26d25a4c135d60d8511b7fd9d00db26e03a27f1631d26f321e7146d91820
+TERMUX_PKG_SHA256=8d809aaf7b5151ecd797c601c7f2701bc426b66d431b52069c7ff3b4002d7979
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
