10172f9eea184136673e42abe7ec60a2ef15418f
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-18 14:43:11 +0000

    bump(x11/ki18n): 6.3.0

--- x11-packages/ki18n/build.sh
+++ x11-packages/ki18n/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="KDE Gettext-based UI text internationalization"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.112.0"
+TERMUX_PKG_VERSION="6.3.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/ki18n-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=33d542e760c2bd5dd2d3511624cac3311c60187d7c7b155a4b968a7c6b7a961b
+TERMUX_PKG_SHA256=4d95341eba2070fec3901396eb0a68f4a8423337de5ea23fb86b0ea70c957282
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="gettext, libc++, qt5-qtbase, qt5-qtdeclarative"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, python, qt5-qtbase-cross-tools, qt5-qtdeclarative-cross-tools"
+TERMUX_PKG_DEPENDS="gettext, iso-codes, libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, python, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
