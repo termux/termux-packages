b81240c20f791671b45a49d8df2c744c0517804d
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-18 14:43:11 +0000

    bump(x11/kitemmodels): 6.3.0

--- x11-packages/kitemmodels/build.sh
+++ x11-packages/kitemmodels/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Set of item models extending the Qt model-view framework (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.112.0"
+TERMUX_PKG_VERSION="6.3.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kitemmodels-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=5aec7f9040ce0640494bcf4e65f082dbcb3c2e50b6a6f1d4450e614aeede0a8c
+TERMUX_PKG_SHA256=b3a984fb62919c1f8bb3fb77a2e207e51a52f5c596415ea9fc8ffa618b56acba
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtdeclarative"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qtdeclarative-cross-tools"
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
