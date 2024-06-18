cb0ec648401f6ae38e5ff820832d44394cd87357
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-18 14:43:11 +0000

    bump(x11/kcodecs): 6.3.0

--- x11-packages/kcodecs/build.sh
+++ x11-packages/kcodecs/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Method collection to manipulate strings using various encodings (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.112.0"
+TERMUX_PKG_VERSION="6.3.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcodecs-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=dd8bc7cb31ce18832e45d0822cfb176b39a535bf6a87e9520f6648ad0ac6d55e
+TERMUX_PKG_SHA256=6902531afc3d47d543feb971c2bc04901af265e6730a477af4400073b22ec6ec
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
