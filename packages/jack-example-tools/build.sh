daaab46524b5f2494d7aa4f700cf8c625ca3e958
Ronald Y <knyipab@gmail.com>
2024-06-19 01:42:09 +0800

    addpkg(main/jack-example-tools): 4

---
+++ packages/jack-example-tools/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://jackaudio.org/
+TERMUX_PKG_DESCRIPTION="Official JACK example clients and tools"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
+TERMUX_PKG_VERSION=4
+TERMUX_PKG_SRCURL=https://github.com/jackaudio/jack-example-tools/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=2b1e0dc3cb3b5bfb0423f0aeb21eb611437cf71ee0ace2ca199f05f02705f174
+TERMUX_PKG_DEPENDS="alsa-lib, jack, libsamplerate, libsndfile"
