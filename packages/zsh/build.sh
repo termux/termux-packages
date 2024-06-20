83794d21c710687b3c71722f84642404eb98c6c5
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-06-20 09:52:56 +0200

    enhance(main/zsh): enable zsh/newuser module. (#20604)
    - set a maintainer
    - clean up module listing and history in build script
    - enable `zsh-newuser-install` if no .zshrc is found

--- packages/zsh/build.sh
+++ packages/zsh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.zsh.org
 TERMUX_PKG_DESCRIPTION="Shell with lots of features"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENCE"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION=5.9
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL="https://sourceforge.net/projects/zsh/files/zsh/$TERMUX_PKG_VERSION/zsh-$TERMUX_PKG_VERSION".tar.xz
 TERMUX_PKG_SHA256=9b8d1ecedd5b5e81fbf1918e876752a7dd948e05c1a0dba10ab863842d45acd5
 # Remove hard link to bin/zsh as Android does not support hard links:
@@ -68,24 +68,29 @@ termux_step_post_configure() {
 	fi
 
 	# INSTALL file: "For a non-dynamic zsh, the default is to compile the complete, compctl, zle,
-	# computil, complist, sched, # parameter, zleparameter and rlimits modules into the shell,
+	# computil, complist, sched, parameter, zleparameter and rlimits modules into the shell,
 	# and you will need to edit config.modules to make any other modules available."
 	# Since we build zsh non-dynamically (since dynamic loading doesn't work on Android when enabled),
 	# we need to explicitly enable the additional modules we want.
-	# - The files module is needed by `compinstall` (https://github.com/termux/termux-packages/issues/61).
-	# - The regex module seems to be used by several extensions.
-	# - The curses, socket and zprof modules was desired by BrainDamage on IRC (#termux).
-	# - The deltochar and mathfunc modules is used by grml-zshrc (https://github.com/termux/termux-packages/issues/494).
-	# - The system module is needed by zplug (https://github.com/termux/termux-packages/issues/659).
-	# - The zpty module is needed by zsh-async (https://github.com/termux/termux-packages/issues/672).
-	# - The stat module is needed by zui (https://github.com/termux/termux-packages/issues/2829).
-	# - The mapfile module was requested in https://github.com/termux/termux-packages/issues/3116.
-	# - The zselect module is used by multiple plugins (https://github.com/termux/termux-packages/issues/4939)
-	# - The param_private module was requested in https://github.com/termux/termux-packages/issues/7391.
-	# - The cap module was requested in https://github.com/termux/termux-packages/issues/3102.
-	for module in cap curses deltochar files mapfile mathfunc pcre regex \
-		socket stat system zprof zpty zselect param_private
-	do
+	local modules=(
+	'cap'           # - The cap module was requested in https://github.com/termux/termux-packages/issues/3102.
+	'curses'        # - The curses module was requested by BrainDamage on IRC (#termux).
+	'deltochar'     # - The deltochar module is used by grml-zshrc https://github.com/termux/termux-packages/issues/494.
+	'files'         # - The files module is needed by `compinstall` https://github.com/termux/termux-packages/issues/61.
+	'mapfile'       # - The mapfile module was requested in https://github.com/termux/termux-packages/issues/3116.
+	'mathfunc'      # - The mathfunc module is used by grml-zshrc https://github.com/termux/termux-packages/issues/494.
+	'newuser'       # - The newuser module was requested in https://github.com/termux/termux-packages/discussions/20603.
+	'param_private' # - The param_private module was requested in https://github.com/termux/termux-packages/issues/7391.
+	'pcre'          # - The pcre module expands on the regex modules capabilities and is used by several extensions.
+	'regex'         # - The regex module seems to be used by several extensions.
+	'socket'        # - The socket module was requested by BrainDamage on IRC (#termux).
+	'stat'          # - The stat module is needed by zui https://github.com/termux/termux-packages/issues/2829.
+	'system'        # - The system module is needed by zplug https://github.com/termux/termux-packages/issues/659.
+	'zprof'         # - The zprof module was requested by BrainDamage on IRC (#termux).
+	'zpty'          # - The zpty module is needed by zsh-async https://github.com/termux/termux-packages/issues/672.
+	'zselect'       # - The zselect module is used by multiple plugins https://github.com/termux/termux-packages/issues/4939
+	)
+	for module in "${modules[@]}"; do
 		perl -p -i -e "s|${module}.mdd link=no|${module}.mdd link=static|" $TERMUX_PKG_BUILDDIR/config.modules
 	done
 }
