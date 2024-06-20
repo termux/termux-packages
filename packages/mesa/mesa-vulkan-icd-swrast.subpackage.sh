4b0b7960388b0df18e88059f3ab310429e469258
Henrik Grimler <grimler@termux.dev>
2024-06-20 21:54:58 +0200

    tree-wide: use SUBPKG_DEPEND_ON_PARENT=false instead of =no
    Fixed already in commit 2b65bc0e6717, but I missed the quoted "no":es.
    Convert those as well to false.
    
    No functional changes:
    %ci:no-build
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch depend-on-parent-fix
    # Changes to be committed:
    #	modified:   packages/libglvnd/libglvnd-dev.subpackage.sh
    #	modified:   packages/mesa/mesa-dev.subpackage.sh
    #	modified:   packages/mesa/mesa-vulkan-icd-freedreno.subpackage.sh
    #	modified:   packages/mesa/mesa-vulkan-icd-swrast.subpackage.sh
    #	modified:   packages/util-linux/blk-utils.subpackage.sh
    #	modified:   packages/util-linux/fdisk.subpackage.sh
    #	modified:   packages/util-linux/libblkid.subpackage.sh
    #	modified:   packages/util-linux/libfdisk.subpackage.sh
    #	modified:   packages/util-linux/libmount.subpackage.sh
    #	modified:   packages/util-linux/libsmartcols.subpackage.sh
    #	modified:   packages/util-linux/libuuid.subpackage.sh
    #	modified:   packages/util-linux/mount-utils.subpackage.sh
    #	modified:   packages/util-linux/uuid-utils.subpackage.sh
    #	modified:   x11-packages/mesa-demos/eglinfo-system.subpackage.sh
    #	modified:   x11-packages/xorg-server/xorg-server-xephyr.subpackage.sh
    #

--- packages/mesa/mesa-vulkan-icd-swrast.subpackage.sh
+++ packages/mesa/mesa-vulkan-icd-swrast.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="Mesa's Swrast Vulkan ICD"
-TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, ncurses, vulkan-loader-generic, zlib, zstd"
 TERMUX_SUBPKG_INCLUDE="
 lib/libvulkan_lvp.so
--- packages/util-linux/libsmartcols.subpackage.sh
+++ packages/util-linux/libsmartcols.subpackage.sh
@@ -1,7 +1,7 @@
 TERMUX_SUBPKG_DESCRIPTION="Library for smart adaptive formatting of tabular data"
 TERMUX_SUBPKG_BREAKS="util-linux (<< 2.38.1-1)"
 TERMUX_SUBPKG_REPLACES="util-linux (<< 2.38.1-1)"
-TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_INCLUDE="
 lib/libsmartcols.so
 lib/pkgconfig/smartcols.pc
