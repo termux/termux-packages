cc92f43fef181f32f4f3124ba359aa8bdf614339
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-22 05:17:52 +0000

    enhance(x11/libxfce4ui): Add libgtop dependency for showing system info

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.18.6"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4ui/${TERMUX_PKG_VERSION%.*}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=77dd99206cc8c6c7f69c269c83c7ee6a037bca9d4a89b1a6d9765e5a09ce30cd
 # Prevent updating to unstable branch
 TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libice, libsm, libx11, libxfce4util, pango, startup-notification, xfconf, zlib"
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libgtop, libice, libsm, libx11, libxfce4util, pango, startup-notification, xfconf, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_RECOMMENDS="hicolor-icon-theme"
 TERMUX_PKG_DISABLE_GIR=false
