d80b0cf780236640efa05489601ff95fe87713c1
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-22 19:24:12 +0000

    enhance(main/neovim): Add tree sitter parsers
    Backport fix for tree-sitter-vimdoc 3.0.0 parser

---
+++ packages/tree-sitter-query/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-query
+TERMUX_PKG_DESCRIPTION="TS query grammar for tree-sitter"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.4.0"
+TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-query/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d3a423ab66dc62b2969625e280116678a8a22582b5ff087795222108db2f6a6e
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+PARSER_URL="${TERMUX_PKG_HOMEPAGE}"
+"
+
+termux_step_pre_configure() {
+	rm setup.py pyproject.toml
+}
+
+termux_step_post_make_install() {
+	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
+	ln -s "${TERMUX_PREFIX}"/lib/libtree-sitter-query.so "${TERMUX_PREFIX}"/lib/tree_sitter/query.so
+}
