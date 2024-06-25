cca7d205cc7bebe092b0c6e0226a9ec058a45c85
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-06-26 00:21:46 +0800

    python-scipy: bump to 1.14.0

--- packages/python-scipy/build.sh
+++ packages/python-scipy/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://scipy.org/
 TERMUX_PKG_DESCRIPTION="Fundamental algorithms for scientific computing in Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.13.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.14.0"
 TERMUX_PKG_SRCURL=git+https://github.com/scipy/scipy
 TERMUX_PKG_DEPENDS="libc++, libopenblas, python, python-numpy"
 TERMUX_PKG_BUILD_DEPENDS="python-numpy-static"
@@ -54,7 +53,7 @@ termux_step_configure() {
 	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/core/include\''|g' \
 		$TERMUX_MESON_WHEEL_CROSSFILE
 
-	termux_step_configure_meson
+	(unset PYTHONPATH && termux_step_configure_meson)
 }
 
 termux_step_make() {
