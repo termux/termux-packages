7d6135ea5c382253651d73a60c6e1fc9909fea32
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-26 12:01:07 +0200

    fix(x11/wxwidgets): Fix build with current toolchain

--- x11-packages/wxwidgets/build.sh
+++ x11-packages/wxwidgets/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="docs/gpl.txt, docs/lgpl.txt, docs/licence.txt, docs/licendoc.txt, docs/preamble.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.2.2.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/wxWidgets/wxWidgets/releases/download/v${TERMUX_PKG_VERSION}/wxWidgets-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=dffcb6be71296fff4b7f8840eb1b510178f57aa2eb236b20da41182009242c02
 TERMUX_PKG_DEPENDS="fontconfig, gdk-pixbuf, glib, glu, gtk3, libandroid-execinfo, libc++, libcairo, libcurl, libexpat, libiconv, libjpeg-turbo, libnotify, libpng, libsecret, libsm, libtiff, libx11, libxtst, libxxf86vm, opengl, pango, pcre2, sdl2, webkit2gtk-4.1, zlib"
@@ -21,6 +21,12 @@ termux_step_pre_configure() {
 	sed -i 's/\(webkit2gtk-4\.\)0/\11/g' configure
 
 	_WX_RELEASE=$(awk '/^WX_RELEASE =/ { print $3 }' "$TERMUX_PKG_SRCDIR"/Makefile.in)
+
+	# https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=273753:
+	# "Add -Wl,--undefined-version to LDFLAGS to suppress these errors, since
+	# wxWidgets reuses the same linker version script for all its shared
+	# libraries"
+	LDFLAGS+=" -Wl,--undefined-version"
 }
 
 termux_step_post_make_install() {
