1e6f0f66c97cb9ff0e952cf7b08b46c9fe17f1e7
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-06-30 01:13:02 +0800

    libxfce4ui: add cpu-info fallback

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.18.6"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4ui/${TERMUX_PKG_VERSION%.*}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=77dd99206cc8c6c7f69c269c83c7ee6a037bca9d4a89b1a6d9765e5a09ce30cd
 # Prevent updating to unstable branch
