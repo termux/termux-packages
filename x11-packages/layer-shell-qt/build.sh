90cee7cd099618db1c55caacd37a1de4a4295d74
Biswapriyo Nath <nathbappai@gmail.com>
2024-06-29 12:52:17 +0000

    addpkg(x11/layer-shell-qt): Add package for qterminal

---
+++ x11-packages/layer-shell-qt/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://invent.kde.org/plasma/layer-shell-qt
+TERMUX_PKG_DESCRIPTION="Qt component to allow applications to make use of the Wayland wl-layer-shell protocol"
+TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.1.1"
+TERMUX_PKG_SRCURL=https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/layer-shell-qt-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=7c60e37301e8f343ef4965494b60186fda82798dd29aadbb35712daf62e34f53
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative, qt6-qtwayland, libwayland"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, libwayland-cross-scanner, libwayland-protocols"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
