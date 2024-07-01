f62bd7cb1ab062cd255f5d7d6f3c0815ca1560ba
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-01 19:36:30 +0000

    addpkg(x11/kf6-kwidgetsaddons): 6.3.0

---
+++ x11-packages/kf6-kwidgetsaddons/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_DESCRIPTION="Large set of desktop Qt widgets (KDE)"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.3.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kwidgetsaddons-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=0e3f9f0db9a0a6ad423cb50ffe8a01beb5feef8bc7872c35b7576180e4d1844c
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
