9bc186df3ab28fa94469f15023a49542b853e2c5
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-01 09:50:11 +0000

    addpkg(x11/kf6-kconfig): 6.3.0

---
+++ x11-packages/kf6-kconfig/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_DESCRIPTION="Advanced configuration system (KDE)"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.3.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kconfig-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=8d809aaf7b5151ecd797c601c7f2701bc426b66d431b52069c7ff3b4002d7979
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+"
