08804ac447729d2f16ed52fb7d271ccfe80fade6
Kevin Williams <admin@utermux.dev>
2024-07-03 13:43:27 +0800

    fix(main/openssh): Privilege separation user sshd does not exist
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/openssh/build.sh
+++ packages/openssh/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Secure shell for logging into a remote machine"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="9.8p1"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/openssh/openssh-portable/archive/refs/tags/V_$(sed 's/\./_/g; s/p/_P/g' <<< $TERMUX_PKG_VERSION).tar.gz
 TERMUX_PKG_SHA256=d8f6802914e4c344dc74599c29915651554bb318102d71cb4063e1f4a0d8286f
 TERMUX_PKG_AUTO_UPDATE=true
@@ -103,6 +103,7 @@ termux_step_post_massage() {
 
 termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
+	echo "mkdir -p \$PREFIX/var/empty" >> postinst
 	echo "mkdir -p \$HOME/.ssh" >> postinst
 	echo "touch \$HOME/.ssh/authorized_keys" >> postinst
 	echo "chmod 700 \$HOME/.ssh" >> postinst
