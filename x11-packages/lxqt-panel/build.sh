0038ed6edef4421b96c73f5cb394b46948274bf2
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-03 20:16:48 +0000

    bump(x11/lxqt-panel): 2.0.1
    Fixes #19798

--- x11-packages/lxqt-panel/build.sh
+++ x11-packages/lxqt-panel/build.sh
@@ -2,23 +2,22 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="The LXQt desktop panel"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="2.0.1"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-panel/releases/download/${TERMUX_PKG_VERSION}/lxqt-panel-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=0e660c0397c96a28f0fcf316b20c72d203c85793a884e1487b3b14e3790defc9
-TERMUX_PKG_DEPENDS="kwindowsystem, libc++, libdbusmenu-qt, liblxqt, libsysstat, libxcb, libxkbcommon, libxtst, libxtst, lxmenu-data, lxqt-globalkeys, lxqt-menu-data, pulseaudio, qt5-qtbase, qt5-qtx11extras, xcb-util, xcb-util-image"
-TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_SHA256=73483c36e411496f8e958b7e56ba8bb06ae0b4300a62cf4c4a78964da6a59407
+TERMUX_PKG_DEPENDS="kf6-kwindowsystem, layer-shell-qt, libc++, libdbusmenu-lxqt, liblxqt, libqtxdg, libsysstat, libxcb, libxkbcommon, libxtst, libx11, lxqt-globalkeys, lxqt-menu-data, pulseaudio, qt6-qtbase, xcb-util, xcb-util-image"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qttools"
 # TODO
 # CPULOAD and NETWORKMONITOR require libstatgrab
 # MOUNT plugin requires KF5Solid
 # SENSORS plugin requires lm_sensors
-# COLORPICKER gives an error when building: no member named 'setAccessibleName' in 'QMenu'
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DCOLORPICKER_PLUGIN=OFF
 -DCPULOAD_PLUGIN=OFF
 -DNETWORKMONITOR_PLUGIN=OFF
 -DMOUNT_PLUGIN=OFF
 -DSENSORS_PLUGIN=OFF
 -DVOLUME_USE_ALSA=OFF
+-DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
 "
 TERMUX_PKG_AUTO_UPDATE=true
 
