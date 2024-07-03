c8fde8d51f19dbef803ce4c9b87b3c4946da0473
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-03 19:53:08 +0000

    addpkg(x11/libdbusmenu-lxqt): for lxqt-panel and lxqt-qtplugin

---
+++ x11-packages/libdbusmenu-lxqt/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/lxqt/libdbusmenu-lxqt
+TERMUX_PKG_DESCRIPTION="A library that provides a Qt implementation of the DBusMenu protocol"
+TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.1.0"
+TERMUX_PKG_SRCURL="https://github.com/lxqt/libdbusmenu-lxqt/releases/download/${TERMUX_PKG_VERSION}/libdbusmenu-lxqt-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=a82d77375034b0f27e6e08b5c7ad9c19ee88e8d7bb699ee0423a5a0e781fb291
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
+TERMUX_PKG_AUTO_UPDATE=true
