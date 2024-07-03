334da6c6c3caf57f349fd29cf4d0ed62aa499e87
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-03 17:34:38 +0000

    bump(x11/lxqt-globalkeys): 2.0.0
    Fixes #19812

--- x11-packages/lxqt-globalkeys/build.sh
+++ x11-packages/lxqt-globalkeys/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="Tools to set global keyboard shortcuts in LXQt sessions"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-globalkeys/releases/download/${TERMUX_PKG_VERSION}/lxqt-globalkeys-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=ffed3e299d11b3b6298bf5558cff9ba2b979d6d7a5cad72af0ae640a991b6203
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, kwindowsystem, liblxqt, libx11"
-TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_SHA256=13e7a72686890a40b65d8ae13c79ed287eb971fbc0285769c40c8b97e7af43f7
+TERMUX_PKG_DEPENDS="libc++, liblxqt, libx11, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
+"
