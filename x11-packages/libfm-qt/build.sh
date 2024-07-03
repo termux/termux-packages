9dd376b0acb5ce990a543efe3ba53b94330d6c09
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-03 14:51:00 +0000

    bump(x11/libfm-qt): 2.0.2
    Fixes #19797

--- x11-packages/libfm-qt/build.sh
+++ x11-packages/libfm-qt/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="Library providing components to build desktop file managers"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="2.0.2"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/libfm-qt/releases/download/${TERMUX_PKG_VERSION}/libfm-qt-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=2ee3882e023df1df31a53f03575856d38364aa63bec39e9a47fde3730d6f7753
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtx11extras, glib, libxcb, libexif, lxqt-menu-data, menu-cache"
-TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_SHA256=411ae1d7d549f34d10389953ded47fba030f128d716373c6af6d45a1bddc2755
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, glib, libxcb, libexif, lxqt-menu-data, menu-cache"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DGLIB_GIO_UNIX_INCLUDE_DIR=$TERMUX_PREFIX/include/gio-unix-2.0
+-DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
 "
