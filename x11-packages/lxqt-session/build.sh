89ceccdc3e07b065b8092341a03399f6f0fc705d
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-04 04:35:12 +0000

    bump(x11/lxqt-session): 2.0.0
    Fixes #19806

--- x11-packages/lxqt-session/build.sh
+++ x11-packages/lxqt-session/build.sh
@@ -2,13 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
 TERMUX_PKG_DESCRIPTION="The LXQt session manager"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-session/releases/download/${TERMUX_PKG_VERSION}/lxqt-session-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=9dcdc846601f1972d01429f2203d36976088edcca5c166eef2b21ad73fcef656
-TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtx11extras, qtxdg-tools, kwindowsystem, liblxqt, procps, libandroid-wordexp"
-TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWITH_LIBUDEV=OFF"
+TERMUX_PKG_SHA256=74ea3b998fecb50834b8b09952c31a1bf128fde9f7cfdd31284f7397665cb428
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kwindowsystem, layer-shell-qt, libandroid-wordexp, libc++, liblxqt, libqtxdg, libx11, procps, qt6-qtbase, qtxdg-tools"
+TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DWITH_LIBUDEV=OFF
+-DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
+"
 
 termux_step_pre_configure(){
 	LDFLAGS+=" -landroid-wordexp"
