dfb08b3a5a449ee7663c71627cc9aa3c393e26ed
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-04 10:19:45 +0000

    bump(x11/libvte): 0.76.3

--- x11-packages/libvte/build.sh
+++ x11-packages/libvte/build.sh
@@ -3,18 +3,17 @@ TERMUX_PKG_DESCRIPTION="Virtual Terminal library"
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING.GPL3, COPYING.LGPL3, COPYING.XTERM"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2:0.74.2"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2:0.76.3"
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/vte/-/archive/${TERMUX_PKG_VERSION:2}/vte-${TERMUX_PKG_VERSION:2}.tar.bz2
 #TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/vte/${_MAJOR_VERSION}/vte-${_VERSION}.tar.xz
-TERMUX_PKG_SHA256=2a1162738c9bccfac1bb801125c1889d3980d857499909439803cf1def4c25d1
-TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, gtk3, gtk4, libc++, libcairo, libgnutls, libicu, pango, pcre2, zlib"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
+TERMUX_PKG_SHA256=fc345b1d493095babf12bf6d812b60447ebd2747a06ef0947c6d7e20c4c8c317
+TERMUX_PKG_DEPENDS="atk, fribidi, gdk-pixbuf, glib, gtk3, gtk4, libc++, libcairo, libgnutls, libicu, liblz4, pango, pcre2, zlib"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/locale"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dgir=true
--Dvapi=false
+-Dvapi=true
 "
 
 termux_step_pre_configure() {
