87d441d4fa4b895f9f0969cf82902ed6f26d7d2c
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-07 07:26:40 +0000

    fix(main/json-glib): Fix building gir data
    This fixes the following erro by moving gir files to correct directory.
    gi-cross-launcher: ERROR: Expected file: /home/builder/termux-packages/packages/json-glib/gir/./Json-1.0.xml

--- packages/json-glib/build.sh
+++ packages/json-glib/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GLib JSON manipulation library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.8.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/json-glib/${TERMUX_PKG_VERSION%.*}/json-glib-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=97ef5eb92ca811039ad50a65f06633f1aae64792789307be7170795d8b319454
 TERMUX_PKG_AUTO_UPDATE=true
@@ -23,4 +24,15 @@ bin/
 
 termux_step_pre_configure() {
 	TERMUX_PKG_VERSION=. termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
