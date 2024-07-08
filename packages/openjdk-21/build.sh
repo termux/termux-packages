488f995098139385d035b3b11fb8e21e0aad738f
eval Nya <60149113+nexplorer-3e@users.noreply.github.com>
2024-07-08 12:32:27 +0800

    chore(main/openjdk-21): build flavor, version change, config args rearrange
    - change base version to 21.0.3, and track GA build for upgrade
    - move make JOBS arg to configure stage
    - configure args resort
    - strip version string, add vendor name (Termux)

--- packages/openjdk-21/build.sh
+++ packages/openjdk-21/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://openjdk.java.net
 TERMUX_PKG_DESCRIPTION="Java development kit and runtime"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.0.3+6
-TERMUX_PKG_REVISION=0
+TERMUX_PKG_VERSION=21.0.3-ga
 TERMUX_PKG_SRCURL=https://github.com/openjdk/jdk21u/archive/refs/tags/jdk-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=75a6ca52299ee2f5b028a5a4acedd6139ae740cbf1af034e2d50489e74598dd3
-TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libandroid-shmem, libandroid-spawn, libiconv, libjpeg-turbo, zlib"
+TERMUX_PKG_SHA256=818e9dee28ae390f2781406d594690fc42bd994d078ad9f8360a4fbca6a3df1f
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+-ga"
 TERMUX_PKG_BUILD_DEPENDS="cups, fontconfig, libxrandr, libxt, xorgproto"
 # openjdk-21-x is recommended because X11 separation is still very experimental.
 TERMUX_PKG_RECOMMENDS="ca-certificates-java, openjdk-21-x, resolv-conf"
@@ -20,29 +20,34 @@ termux_step_pre_configure() {
 }
 
 termux_step_configure() {
-	local jdk_ldflags="-L${TERMUX_PREFIX}/lib -Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-21-openjdk/lib -Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
+	local jdk_ldflags="-L${TERMUX_PREFIX}/lib \
+		-Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-21-openjdk/lib \
+		-Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
 	bash ./configure \
 		--disable-precompiled-headers \
 		--disable-warnings-as-errors \
 		--enable-option-checking=fatal \
-		--openjdk-target=$TERMUX_HOST_PLATFORM \
-		--with-cups-include="$TERMUX_PREFIX/include" \
+		--with-version-pre="" \
+		--with-version-opt="" \
+		--with-jvm-variants=server \
 		--with-debug-level=release \
+		--openjdk-target=$TERMUX_HOST_PLATFORM \
+		--with-toolchain-type=clang \
 		--with-extra-cflags="$CFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
 		--with-extra-cxxflags="$CXXFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
 		--with-extra-ldflags="${jdk_ldflags} -Wl,--as-needed -landroid-shmem -landroid-spawn" \
+		--with-cups-include="$TERMUX_PREFIX/include" \
 		--with-fontconfig-include="$TERMUX_PREFIX/include" \
 		--with-freetype-include="$TERMUX_PREFIX/include/freetype2" \
 		--with-freetype-lib="$TERMUX_PREFIX/lib" \
-		--with-giflib=system \
-		--with-jvm-variants=server \
-		--with-libjpeg=system \
-		--with-libpng=system \
-		--with-toolchain-type=clang \
 		--with-x="$TERMUX_PREFIX/include/X11" \
-		--with-zlib=system \
 		--x-includes="$TERMUX_PREFIX/include/X11" \
 		--x-libraries="$TERMUX_PREFIX/lib" \
+		--with-giflib=system \
+		--with-libjpeg=system \
+		--with-libpng=system \
+		--with-zlib=system \
+		--with-vendor-name="Termux" \
 		AR="$AR" \
 		NM="$NM" \
 		OBJCOPY="$OBJCOPY" \
@@ -54,12 +59,13 @@ termux_step_configure() {
 		BUILD_NM="/usr/bin/llvm-nm-17" \
 		BUILD_AR="/usr/bin/llvm-ar-17" \
 		BUILD_OBJCOPY="/usr/bin/llvm-objcopy-17" \
-		BUILD_STRIP="/usr/bin/llvm-strip-17"
+		BUILD_STRIP="/usr/bin/llvm-strip-17" \
+		$TERMUX_PKG_MAKE_PROCESSES
 }
 
 termux_step_make() {
 	cd build/linux-${TERMUX_ARCH/i686/x86}-server-release
-	make JOBS=$TERMUX_PKG_MAKE_PROCESSES images
+	make images
 }
 
 termux_step_make_install() {
