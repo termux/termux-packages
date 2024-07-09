e6123941c0d9284d56c6f8fb359f687561f4193f
agnostic-apollo <agnosticapollo@gmail.com>
2024-07-09 12:01:58 +0500

    fix(main/termux-keyring): add missing double quotes around variables

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -11,33 +11,33 @@ TERMUX_PKG_ESSENTIAL=true
 termux_step_make_install() {
 	local GPG_SHARE_DIR="$TERMUX_PREFIX/share/termux-keyring"
 
-	mkdir -p $GPG_SHARE_DIR
+	mkdir -p "$GPG_SHARE_DIR"
 
 	# Maintainer-specific keys.
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/agnostic-apollo.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/mradityaalok.gpg $GPG_SHARE_DIR
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/2096779623.gpg $GPG_SHARE_DIR
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/agnostic-apollo.gpg" "$GPG_SHARE_DIR"
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/grimler.gpg" "$GPG_SHARE_DIR"
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/kcubeterm.gpg" "$GPG_SHARE_DIR"
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/landfillbaby.gpg" "$GPG_SHARE_DIR"
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/mradityaalok.gpg" "$GPG_SHARE_DIR"
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/2096779623.gpg" "$GPG_SHARE_DIR"
 
 	# Key for automatic builds (via CI).
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $GPG_SHARE_DIR
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg" "$GPG_SHARE_DIR"
 
 	# Key for pacman package manager.
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-pacman.gpg $GPG_SHARE_DIR
+	install -Dm600 "$TERMUX_PKG_BUILDER_DIR/termux-pacman.gpg" "$GPG_SHARE_DIR"
 
 	for GPG_DIR in "$TERMUX_PREFIX/etc/apt/trusted.gpg.d" "$TERMUX_PREFIX/share/pacman/keyrings"; do
-		mkdir -p $GPG_DIR
-		for GPG_FILE in $GPG_SHARE_DIR/*.gpg; do
+		mkdir -p "$GPG_DIR"
+		for GPG_FILE in "$GPG_SHARE_DIR"/*.gpg; do
 			if [[ "$GPG_DIR" == *"/apt/"* && "$GPG_FILE" == *"termux-pacman.gpg"* ]]; then
 				continue
 			fi
-			ln -s "$GPG_FILE" "$GPG_DIR/$(basename $GPG_FILE)"
+			ln -s "$GPG_FILE" "$GPG_DIR/$(basename "$GPG_FILE")"
 		done
 		# Creation of trusted files
 		if [[ "$GPG_DIR" == *"/pacman/"* ]]; then
-			echo "998DE27318E867EA976BA877389CEED64573DFCA:4:" > $GPG_DIR/termux-pacman-trusted
+			echo "998DE27318E867EA976BA877389CEED64573DFCA:4:" > "$GPG_DIR/termux-pacman-trusted"
 		fi
 	done
 }
