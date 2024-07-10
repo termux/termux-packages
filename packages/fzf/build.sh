6aec6f8f15cd887d9dfa77abf35ec1606b8b8e3c
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-10 15:51:24 +0000

    bump(main/fzf): 0.54.0
    New tags will have `v` prefix.
    https://github.com/junegunn/fzf/commit/9e92b6f11e0f59272de410f56493893334071e6e

--- packages/fzf/build.sh
+++ packages/fzf/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/junegunn/fzf
+TERMUX_PKG_HOMEPAGE=https://junegunn.github.io/fzf/
 TERMUX_PKG_DESCRIPTION="Command-line fuzzy finder"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.53.0"
-TERMUX_PKG_SRCURL=https://github.com/junegunn/fzf/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d45abbfb64f21913c633d46818d9d3eb3d7ebc7e94bd16f45941958aa5480e1d
+TERMUX_PKG_VERSION="0.54.0"
+TERMUX_PKG_SRCURL=https://github.com/junegunn/fzf/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ce658b153254e25cf4244365d639cf3ac5b91230a20f76bd6845d88e1267a2db
 TERMUX_PKG_AUTO_UPDATE=true
 
 # Depend on findutils as fzf uses the -fstype option, which busybox
