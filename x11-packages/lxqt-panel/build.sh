eb585880c94211d3b17ecc4267c8c3e2a937e6c1
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-11 04:39:53 +0000

    fix(x11/lxqt-panel): Add TERMUX_PREFIX with hardcoded paths

--- x11-packages/lxqt-panel/build.sh
+++ x11-packages/lxqt-panel/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The LXQt desktop panel"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-panel/releases/download/${TERMUX_PKG_VERSION}/lxqt-panel-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=73483c36e411496f8e958b7e56ba8bb06ae0b4300a62cf4c4a78964da6a59407
 TERMUX_PKG_DEPENDS="kf6-kwindowsystem, layer-shell-qt, libc++, libdbusmenu-lxqt, liblxqt, libqtxdg, libsysstat, libxcb, libxkbcommon, libxtst, libx11, lxqt-globalkeys, lxqt-menu-data, pulseaudio, qt6-qtbase, xcb-util, xcb-util-image"
