1a603533dc97984774a02302ab46c2bbc1c62287
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-11 06:37:05 +0000

    fix(x11/lxqt-qtplugin): Add TERMUX_PREFIX with hardcoded paths

--- x11-packages/lxqt-qtplugin/build.sh
+++ x11-packages/lxqt-qtplugin/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://lxqt.github.io
-TERMUX_PKG_DESCRIPTION="LXQt platform integration plugin for Qt 5"
+TERMUX_PKG_DESCRIPTION="LXQt platform integration plugin for Qt"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-qtplugin/releases/download/${TERMUX_PKG_VERSION}/lxqt-qtplugin-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=cd9563cfad565d1e3b3fbf2fee2224824f3c76d6b97fec868e332234cb245d8e
 TERMUX_PKG_DEPENDS="libc++, libdbusmenu-lxqt, libfm-qt, libqtxdg, qt6-qtbase"
