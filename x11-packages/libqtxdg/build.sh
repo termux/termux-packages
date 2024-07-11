d216169a263108a998e030d1f170f08bada9df45
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-11 18:31:19 +0000

    fix(x11/libqtxdg): Add TERMUX_PREFIX with hardcoded paths

--- x11-packages/libqtxdg/build.sh
+++ x11-packages/libqtxdg/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Qt implementation of freedesktop.org XDG specifications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/libqtxdg/releases/download/${TERMUX_PKG_VERSION}/libqtxdg-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=8c1b250de914b2e1fb451c213ee6f249b7b5729c85ac3283fd75615a47a66b62
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtsvg, glib"
