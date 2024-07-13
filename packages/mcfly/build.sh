c0efef3093c1aac787e6013e288e7a9ad55c0d2b
tqfx <tqfx@tqfx.org>
2024-07-13 13:59:49 +0800

    bump(main/mcfly): 0.9.1
    Fixed by https://github.com/cantino/mcfly/commit/3ddb9967f445531db7cb2e949202699423073076

--- packages/mcfly/build.sh
+++ packages/mcfly/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cantino/mcfly
 TERMUX_PKG_DESCRIPTION="Replaces your default ctrl-r shell history search with an intelligent search engine"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.0"
+TERMUX_PKG_VERSION="0.9.1"
 TERMUX_PKG_SRCURL=https://github.com/cantino/mcfly/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ac292c65a0cf031d583961b3bb3a93156fc0c8247dfa9aec0a35c786a25d23da
+TERMUX_PKG_SHA256=3f2f7ff1d8c4ccf5e7f98b185723c415a38883068cb8533ddd551ed4a8f059e9
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -26,19 +26,5 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/mcfly
-	install -Dm600 -t $TERMUX_PREFIX/share/mcfly mcfly.{fi,z}sh
-}
-
-termux_step_create_debscripts() {
-	cat <<-EOF > ./postinst
-		#!$TERMUX_PREFIX/bin/sh
-		echo
-		echo "********"
-		echo "McFly does not support Bash on Android."
-		echo
-		echo "https://github.com/termux/termux-packages/issues/8722"
-		echo "https://github.com/cantino/mcfly/issues/215"
-		echo "********"
-		echo
-	EOF
+	install -Dm600 -t $TERMUX_PREFIX/share/mcfly mcfly.{ba,fi,z}sh
 }
