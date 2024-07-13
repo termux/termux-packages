8a1758f68f4b0ef2cdc5e1b03849ec01376ad497
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2024-07-13 08:03:08 +0100

    Update x11-packages/gw/build.sh
    Co-authored-by: tqfx <tqfx@tqfx.org>

--- x11-packages/gw/build.sh
+++ x11-packages/gw/build.sh
@@ -3,7 +3,6 @@ TERMUX_PKG_DESCRIPTION="A browser for genomic sequencing data (.bam/.cram format
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="clealk@cardiff.ac.uk"
 TERMUX_PKG_VERSION=0.10.1
-TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kcleal/gw/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a864010d9ba329feb9211d554ea9c410f5bcc1e1462369bef56a8f5b8914f3d2
 TERMUX_PKG_DEPENDS="glfw, htslib, libc++, libjpeg-turbo, opengl, libcurl"
