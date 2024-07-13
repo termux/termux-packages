c8da2e75f61bc535dc6a01058a6edea3ba2e5173
tqfx <tqfx@tqfx.org>
2024-07-13 14:42:02 +0800

    bump(main/neocmakelsp): 0.7.8

--- packages/neocmakelsp/build.sh
+++ packages/neocmakelsp/build.sh
@@ -2,8 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Decodetalkers/neocmakelsp
 TERMUX_PKG_DESCRIPTION="a cmake lsp based on tower-lsp and treesitter"
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER=@termux
-TERMUX_PKG_VERSION="0.7.7"
+TERMUX_PKG_VERSION="0.7.8"
 TERMUX_PKG_SRCURL=https://github.com/Decodetalkers/neocmakelsp/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9c761a54ae8a6b298eabc9b7cb215fceafd27e9193eb61e1d69cd7a5dc6dd1c1
+TERMUX_PKG_SHA256=1026ab9f7c60b2c9f880df12830f2927b28b50a06bb5732d5047aefe22fa9b2f
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	rm -f meson.build
+}
+
+termux_step_post_make_install() {
+	install -Dm600 -t "${TERMUX_PREFIX}/share/bash-completion/completions" completions/bash/neocmakelsp
+	install -Dm600 -t "${TERMUX_PREFIX}/share/fish/vendor_completions.d" completions/fish/neocmakelsp.fish
+	install -Dm600 -t "${TERMUX_PREFIX}/share/zsh/site-functions" completions/zsh/_neocmakelsp
+}
