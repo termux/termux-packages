4467d0e532b1c1c1e30dc29127616454e187205c
TomIO <tomjo00@web.de>
2024-07-15 08:21:37 +0200

    bump(main/nerdfix): 0.4.1 also enable auto updates

--- packages/nerdfix/build.sh
+++ packages/nerdfix/build.sh
@@ -2,14 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://github.com/loichyan/nerdfix
 TERMUX_PKG_DESCRIPTION="nerdfix helps you to find/fix obsolete Nerd Font icons in your project."
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
-TERMUX_PKG_VERSION=0.4.0
+TERMUX_PKG_VERSION=0.4.1
 TERMUX_PKG_SRCURL=https://github.com/loichyan/nerdfix/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=72e835aeb349495be87e92f74f405b43dac982ec137cfd7e180e72146b6f6fb7
+TERMUX_PKG_SHA256=37f13ccf0eb6567b31862bc46e694a60177cbf5b76667fe73f22cef8f7ea68df
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE=latest-release-tag
 
 termux_step_pre_configure() {
 	# do not pin the version of the rust toolchain
-	rm -f rust-toolchain
+	rm -f rust-toolchain.toml
 	termux_setup_rust
 }
 
