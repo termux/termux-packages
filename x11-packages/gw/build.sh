72c3dac5cf88a78c1126d47624e47eb05279eb7d
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-07-15 08:02:53 +0200

    Undo LDFLAGS override.
    Co-authored-by: tqfx <tqfx@tqfx.org>

--- x11-packages/gw/build.sh
+++ x11-packages/gw/build.sh
@@ -54,7 +54,7 @@ termux_step_make() {
 	unzip -o "${TERMUX_PKG_CACHEDIR}/skia-${TERMUX_ARCH}.zip"
 	cd ../../
 
-	LDFLAGS="${LDFLAGS} -L${TERMUX_PREFIX}/lib" make -j "$TERMUX_PKG_MAKE_PROCESSES"
+	make -j "$TERMUX_PKG_MAKE_PROCESSES"
 }
 
 termux_step_make_install() {
