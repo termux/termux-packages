5c764ec3474963da3fe48d9bc0895c809e2d0e18
Sylirre <sylirre@termux.dev>
2024-07-16 11:16:04 +0300

    revbump(main/monero): rebuild package to fix linking in monero-wallet-cli

--- packages/monero/build.sh
+++ packages/monero/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A private, secure, untraceable, decentralised digital cu
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.18.3.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=git+https://github.com/monero-project/monero
 TERMUX_PKG_DEPENDS="boost, libc++, libprotobuf, libsodium, libunbound, libusb, libzmq, miniupnpc, openssl, readline"
