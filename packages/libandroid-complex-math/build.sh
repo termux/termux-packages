706caf5e5ec39084ec19dc4c4fb99282fca85434
Jia Yuan Lo <jylo06g@gmail.com>
2024-07-19 19:09:10 +0800

    fix(main/libandroid-complex-math): link with libm (#20896)

--- packages/libandroid-complex-math/build.sh
+++ packages/libandroid-complex-math/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://android.googlesource.com/platform/bionic/+/refs/head
 TERMUX_PKG_DESCRIPTION="A shared library providing libm complex math functions"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.1
-TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_VERSION="0.2"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=false
 
 # https://android.googlesource.com/platform/bionic/+/9ee6adb003eb5a9855ff6c47f9c150b415a11299
 # https://android.googlesource.com/platform/bionic/+/refs/tags/android-8.1.0_r81/libm/upstream-netbsd/lib/libm/complex/
-# https://android.googlesource.com/platform/bionic/+/master/libm/libm.map.txt
-# https://android.googlesource.com/platform/bionic/+/master/docs/status.md#libm
+# https://android.googlesource.com/platform/bionic/+/main/libm/libm.map.txt
+# https://android.googlesource.com/platform/bionic/+/main/docs/status.md#libm
 
 # Use the full NetBSD implementation as is from Android O
 # instead of matching the latest Android implementation which is a mix of FreeBSD and NetBSD
@@ -18,6 +17,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_configure() {
 	CPPFLAGS+=" -D__USE_GNU"
 	CFLAGS+=" -fPIC"
+	LDFLAGS+=" -lm"
 }
 
 termux_step_make() {
