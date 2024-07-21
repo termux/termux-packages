772fbcf70b8a8b830bbe91aaf008c81796bc3e56
Jia Yuan Lo <jylo06g@gmail.com>
2024-07-21 13:27:27 +0800

    bump(x11/tuxpaint): 0.9.33 (#20926)

--- x11-packages/tuxpaint/build.sh
+++ x11-packages/tuxpaint/build.sh
@@ -2,22 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://tuxpaint.org/
 TERMUX_PKG_DESCRIPTION="A free, award-winning drawing program for children ages 3 to 12"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.32"
+TERMUX_PKG_VERSION="0.9.33"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/tuxpaint/tuxpaint-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=09cce22241481dc1360fc4bc5d4da1d31815d7a2563b9e9fa217a672ba974bf2
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="fribidi, libandroid-wordexp, libcairo, libimagequant, libpaper, libpng, librsvg, sdl2, sdl2-gfx, sdl2-image, sdl2-mixer, sdl2-pango, sdl2-ttf, tuxpaint-data, zlib"
-TERMUX_PKG_BUILD_DEPENDS="glib"
+TERMUX_PKG_SHA256=8701ec5082d48a00ba2f408c7c48cf592aa82b60f50dd6d4b6574fac6b48dc76
+TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, libandroid-wordexp, libcairo, libimagequant, libpaper, libpng, librsvg, pango, sdl2, sdl2-gfx, sdl2-image, sdl2-mixer, sdl2-pango, sdl2-ttf, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_host_build() {
 	local _PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/prefix
 
 	# Need imagemagick that can handle SVG format.
 	local IMAGEMAGICK_BUILD_SH=$TERMUX_SCRIPTDIR/packages/imagemagick/build.sh
-	local IMAGEMAGICK_SRCURL=$(bash -c ". $IMAGEMAGICK_BUILD_SH; echo \$TERMUX_PKG_SRCURL")
-	local IMAGEMAGICK_SHA256=$(bash -c ". $IMAGEMAGICK_BUILD_SH; echo \$TERMUX_PKG_SHA256")
+	local IMAGEMAGICK_SRCURL=$(. $IMAGEMAGICK_BUILD_SH; echo $TERMUX_PKG_SRCURL)
+	local IMAGEMAGICK_SHA256=$(. $IMAGEMAGICK_BUILD_SH; echo $TERMUX_PKG_SHA256)
 	local IMAGEMAGICK_TARFILE=$TERMUX_PKG_CACHEDIR/$(basename $IMAGEMAGICK_SRCURL)
 	termux_download $IMAGEMAGICK_SRCURL $IMAGEMAGICK_TARFILE $IMAGEMAGICK_SHA256
 	mkdir -p imagemagick
@@ -27,11 +26,13 @@ termux_step_host_build() {
 		--with-jpeg \
 		--with-png \
 		--with-rsvg
-	make -j $TERMUX_PKG_MAKE_PROCESSES
-	make install
+	make -j ${TERMUX_PKG_MAKE_PROCESSES} install
 }
 
 termux_step_pre_configure() {
+	# this is a workaround for build-all.sh issue
+	TERMUX_PKG_DEPENDS+=", tuxpaint-data"
+
 	local _PREFIX_FOR_BUILD=$TERMUX_PKG_HOSTBUILD_DIR/prefix
 	export PATH=$_PREFIX_FOR_BUILD/bin:$PATH
 
