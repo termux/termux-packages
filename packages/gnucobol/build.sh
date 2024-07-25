bdb17a2a7b7cc6d9b5f30649b9054122af8069b7
abraham sofer <alexbodn@gmail.com>
2024-07-25 23:56:18 +0200

    install_deps=true

--- packages/gnucobol/build.sh
+++ packages/gnucobol/build.sh
@@ -9,7 +9,8 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=git+https://github.com/alexbodn/gnucobol-3.2.git
 #TERMUX_PKG_SHA256=bcdc555d76cbc951bb51d66f1cae5e92bf5bcb5349d0330f5c58f5954e62c8c3
 TERMUX_PKG_GIT_BRANCH="termux"
-TERMUX_PKG_DEPENDS="json-c, libgmp, libdb, libxml2, ncurses, libvbisam"
+TERMUX_INSTALL_DEPS=true
+TERMUX_PKG_DEPENDS="json-c, libgmp, libdb, libxml2, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-db
 --with-json=json-c
