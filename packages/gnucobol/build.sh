fe66bdb5f905a3b4bef58f9e7984a97aeeca71e2
abraham sofer <alexbodn@gmail.com>
2024-07-26 11:54:57 +0200

    reintegrate to upstream archive

--- packages/gnucobol/build.sh
+++ packages/gnucobol/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="A free/libre COBOL compiler"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.2
-TERMUX_PKG_REVISION=3
-##TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/gnucobol/gnucobol-${TERMUX_PKG_VERSION}.tar.xz
-##TERMUX_PKG_SHA256=3bb48af46ced4779facf41fdc2ee60e4ccb86eaa99d010b36685315df39c2ee2
-TERMUX_PKG_SRCURL=git+https://github.com/alexbodn/gnucobol-3.2.git
-#TERMUX_PKG_SHA256=bcdc555d76cbc951bb51d66f1cae5e92bf5bcb5349d0330f5c58f5954e62c8c3
-TERMUX_PKG_GIT_BRANCH="termux"
+TERMUX_PKG_REVISION=4
+
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/gnucobol/gnucobol-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=3bb48af46ced4779facf41fdc2ee60e4ccb86eaa99d010b36685315df39c2ee2
+##TERMUX_PKG_SRCURL=git+https://github.com/alexbodn/gnucobol-3.2.git
+##TERMUX_PKG_GIT_BRANCH="termux"
+
 TERMUX_INSTALL_DEPS=true
 
 #TERMUX_PKG_DEPENDS="json-c, libgmp, libdb, libxml2, ncurses"
@@ -28,29 +29,9 @@ termux_step_pre_configure() {
 	export COB_32_BIT_LONG="$(( 1 - ${lp64} ))"
 	export COB_HAS_64_BIT_POINTER="${lp64}"
 	
-#	export BDB_LIBS="-ldb"
-#	export BDB_CFLAGS="-DWITH_DB=1"
-#	export LIBCOB_CPPFLAGS="-DWITH_DB=1"
-#	export COBC_CPPFLAGS="-DWITH_DB=1"
 	export BDB_HEADER=$(cat db_header.c |cc -I$TERMUX_PREFIX/include -P -E -| tail -n 1| sed -e 's/[\ \t]//g')
-	echo $BDB_HEADER
-	export BDB_HEADER="18.1"
-
-#	echo '#ifdef WITH_DB' >> config.h.in
-#	echo '#undef WITH_DB' >> config.h.in
-#	echo '#endif /*WITH_DB*/' >> config.h.in
-#	echo '#define WITH_DB 1' >> config.h.in
-	#echo "==================="
-	#cat config.h
-	#echo "==================="
 }
 
 termux_step_post_configure() {
-#	echo "==================="
-#	cat config.log
-#	echo $DEFS
-#	echo $CFLAGS
-#	find -name confdefs.h
-#	echo "==================="
 	touch bin/cobcrun.1 cobc/cobc.1
 }
