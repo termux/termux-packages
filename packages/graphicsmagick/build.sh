54264a11b47dfd6aa6eb39ad278975491edd856e
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-28 07:48:21 +0000

    enhance(main/graphicsmagick): Enable jpeg-xl support
    Here is a sample output.
    
    $ gm identify red-room.jxl
    red-room.jxl JXL 555x812+0+0 DirectClass 8-bit 178.3Ki 0.000u 0m:0.000014s

--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3.43"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 # Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2b88580732cd7e409d9e22c6116238bef4ae06fcda11451bf33d259f9cbf399f
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="freetype, libbz2, libc++, libde265, libheif, libjasper, libjpeg-turbo, liblzma, libpng, libtiff, libwebp, libxml2, littlecms, zlib, zstd"
+TERMUX_PKG_DEPENDS="freetype, libbz2, libc++, libde265, libheif, libjasper, libjpeg-turbo, libjxl, liblzma, libpng, libtiff, libwebp, libxml2, littlecms, zlib, zstd"
 TERMUX_PKG_BREAKS="graphicsmagick-dev"
 TERMUX_PKG_REPLACES="graphicsmagick++, graphicsmagick-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
