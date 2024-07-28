67d5bbb53dcd2e3184c9a2aab72051d21bc6d9e9
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-07-28 18:16:36 +0200

    bump(main/starship): 1.20.1 (#20972)
    - set maintainer
    - add elvish completions
    - clean up build script
    - update CODEOWNERS file

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://starship.rs
 TERMUX_PKG_DESCRIPTION="A minimal, blazing fast, and extremely customizable prompt for any shell"
 TERMUX_PKG_LICENSE="ISC"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.19.0"
-TERMUX_PKG_REVISION="1"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
+TERMUX_PKG_VERSION="1.20.1"
 TERMUX_PKG_SRCURL=https://github.com/starship/starship/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cf789791b5c11d6d7a00628590696627bb8f980e3d7c7a0200026787b08aba37
+TERMUX_PKG_SHA256=851d84be69f9171f10890e3b58b8c5ec6057dd873dc83bfe0bdf965f9844b5dc
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -18,48 +17,36 @@ termux_step_pre_configure() {
 	: "${CARGO_HOME:=${HOME}/.cargo}"
 	export CARGO_HOME
 
-	rm -rf $CARGO_HOME/registry/src/*/cmake-*
+	rm -rf "$CARGO_HOME"/registry/src/*/cmake-*
 	cargo fetch --target "${CARGO_TARGET_NAME}"
 
-	local d p
-	p="cmake-0.1.50-src-lib.rs.diff"
-	for d in $CARGO_HOME/registry/src/*/cmake-*; do
-		patch --silent -p1 -d ${d} \
-			< "$TERMUX_PKG_BUILDER_DIR/${p}"
+	local dir patch
+	patch="cmake-0.1.50-src-lib.rs.diff"
+	for dir in "$CARGO_HOME"/registry/src/*/cmake-*; do
+		patch --silent -p1 -d "${dir}" \
+			< "$TERMUX_PKG_BUILDER_DIR/${patch}"
 	done
-
-	mv "${TERMUX_PREFIX}"/lib/libz.so.1{,.tmp}
-	mv "${TERMUX_PREFIX}"/lib/libz.so{,.tmp}
 
 	local _CARGO_TARGET_LIBDIR="target/${CARGO_TARGET_NAME}/release/deps"
 	mkdir -p "${_CARGO_TARGET_LIBDIR}"
-
-	ln -sfT "$(readlink -f "${TERMUX_PREFIX}"/lib/libz.so.1.tmp)" \
-		"${_CARGO_TARGET_LIBDIR}"/libz.so.1
-	ln -sfT "$(readlink -f "${TERMUX_PREFIX}"/lib/libz.so.tmp)" \
-		"${_CARGO_TARGET_LIBDIR}"/libz.so
 
 	RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
 }
 
 termux_step_post_make_install() {
-	mv "${TERMUX_PREFIX}"/lib/libz.so.1{.tmp,}
-	mv "${TERMUX_PREFIX}"/lib/libz.so{.tmp,}
-
 	# Make a placeholder for shell-completions (to be filled with postinst)
-	mkdir -p ${TERMUX_PREFIX}/share/bash-completions/completions
-	mkdir -p ${TERMUX_PREFIX}/share/fish/vendor_completions.d
-	mkdir -p ${TERMUX_PREFIX}/share/zsh/site-functions
-	touch ${TERMUX_PREFIX}/share/bash-completions/completions/starship
-	touch ${TERMUX_PREFIX}/share/fish/vendor_completions.d/starship.fish
-	touch ${TERMUX_PREFIX}/share/zsh/site-functions/_starship
+	mkdir -p "${TERMUX_PREFIX}"/share/bash-completions/completions
+	mkdir -p "${TERMUX_PREFIX}"/share/elvish/lib
+	mkdir -p "${TERMUX_PREFIX}"/share/fish/vendor_completions.d
+	mkdir -p "${TERMUX_PREFIX}"/share/zsh/site-functions
+	touch "${TERMUX_PREFIX}"/share/bash-completions/completions/starship
+	touch "${TERMUX_PREFIX}"/share/elvish/lib/starship.elv
+	touch "${TERMUX_PREFIX}"/share/fish/vendor_completions.d/starship.fish
+	touch "${TERMUX_PREFIX}"/share/zsh/site-functions/_starship
 }
 
 termux_step_post_massage() {
-	rm -f lib/libz.so.1
-	rm -f lib/libz.so
-
-	rm -rf $CARGO_HOME/registry/src/*/cmake-*
+	rm -rf "$CARGO_HOME"/registry/src/*/cmake-*
 }
 
 termux_step_create_debscripts() {
@@ -67,6 +54,7 @@ termux_step_create_debscripts() {
 		#!${TERMUX_PREFIX}/bin/sh
 
 		starship completions bash > ${TERMUX_PREFIX}/share/bash-completions/completions/starship
+		starship completions elvish > "$TERMUX_PREFIX/share/elvish/lib/starship.elv"
 		starship completions fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/starship.fish
 		starship completions zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_starship
 	EOF
