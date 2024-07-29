6a4631f57249d44111adf78fbf716a0285b91960
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-29 07:51:10 +0000

    fix(main/miniupnpc): Switch to active upstream mirror

--- packages/miniupnpc/build.sh
+++ packages/miniupnpc/build.sh
@@ -1,9 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://miniupnp.free.fr/
+TERMUX_PKG_HOMEPAGE=https://miniupnp.tuxfamily.org/
 TERMUX_PKG_DESCRIPTION="Small UPnP client library and tool to access Internet Gateway Devices"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.2.8"
-TERMUX_PKG_SRCURL=http://miniupnp.free.fr/files/miniupnpc-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://miniupnp.tuxfamily.org/files/miniupnpc-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=05b929679091b9921b6b6c1f25e39e4c8d1f4d46c8feb55a412aa697aee03a93
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="miniupnpc-dev"
