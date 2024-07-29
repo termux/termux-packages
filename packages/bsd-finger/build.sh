56203bb3bf7a483912440d5623bb954f897709d3
TomIO <tomjo00@web.de>
2024-07-29 13:46:45 +0200

    fix(main/bsd-finger): remember to revbump package after build fixes

--- packages/bsd-finger/build.sh
+++ packages/bsd-finger/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="User information lookup program"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.17
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://salsa.debian.org/debian/bsd-finger/-/archive/upstream/${TERMUX_PKG_VERSION}/bsd-finger-upstream-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=56e18928a04b38eadea741f9f07db6155ce56b6992defba3c0e32f9caeee9109
 TERMUX_PKG_BUILD_IN_SRC=true
