b7f0d468450e57eb5db03853020d09fc8ee10ddc
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-30 08:06:24 +0000

    fix(x11/gcr): Fix building with glib-cross for cross compilation
    This fixes the following error.
    /data/data/com.termux/files/usr/bin/glib-genmarshal: 20: import: not found

--- x11-packages/gcr/build.sh
+++ x11-packages/gcr/build.sh
@@ -5,11 +5,11 @@ TERMUX_PKG_MAINTAINER="@termux"
 # This specific package is for libgcr-3.
 _MAJOR_VERSION=3.41
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gcr/${_MAJOR_VERSION}/gcr-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=bb7128a3c2febbfee9c03b90d77d498d0ceb237b0789802d60185c71c4bea24f
-TERMUX_PKG_DEPENDS="glib, gtk3, libcairo, libgcrypt, p11-kit, pango"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, gnupg"
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libgcrypt, p11-kit, pango"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, gnupg, valac"
 TERMUX_PKG_RECOMMENDS="gnupg"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -37,6 +37,17 @@ termux_step_pre_configure() {
 	done
 	popd
 	export PATH+=":$bin_dir"
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
 
 termux_step_post_massage() {
