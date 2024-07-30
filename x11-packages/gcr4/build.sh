dd51808387afa0c78f8841818cb2e71d1d26b356
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-30 14:59:44 +0000

    enhance(x11/gcr4): Move gi data to version independent directory

--- x11-packages/gcr4/build.sh
+++ x11-packages/gcr4/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gcr
 TERMUX_PKG_DESCRIPTION="A library for displaying certificates and crypto UI, accessing key stores"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=4.1
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
+TERMUX_PKG_VERSION="4.1.0"
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gcr/${_MAJOR_VERSION}/gcr-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gcr/${TERMUX_PKG_VERSION%.*}/gcr-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=9ceaad29284ba919b9216e2888c18ec67240c2c93b3a4856bc5488bbc1f3a383
 TERMUX_PKG_DEPENDS="glib, libgcrypt, p11-kit"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, gnupg, valac"
@@ -21,7 +20,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
 
 	local bin_dir=$TERMUX_PKG_BUILDDIR/_dummy/bin
 	mkdir -p $bin_dir
