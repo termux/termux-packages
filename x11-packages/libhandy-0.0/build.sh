a8dec894bd3bfe8c3430cbfdf59dc3adcbbe26ca
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-30 19:15:21 +0000

    fix(x11/libhandy-0.0): Fix building with glib-cross for cross compilation
    This fixes the following error.
    OSError: [Errno 8] Exec format error: '/data/data/com.termux/files/usr/bin/glib-mkenums'

--- x11-packages/libhandy-0.0/build.sh
+++ x11-packages/libhandy-0.0/build.sh
@@ -2,13 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/libhandy/
 TERMUX_PKG_DESCRIPTION="Building blocks for modern adaptive GNOME apps"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=0.0
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.13
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.0.13"
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/libh/libhandy/libhandy_${TERMUX_PKG_VERSION}.orig.tar.gz
 TERMUX_PKG_SHA256=4dcd9d249558834bd5430445d3674e9e3cff356e35f0c1dd368c3af50fa15b6d
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, libcairo, pango"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dintrospection=enabled
@@ -20,6 +19,17 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
 
 termux_step_post_massage() {
