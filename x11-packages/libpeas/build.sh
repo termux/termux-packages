02cc6ba470b72b434dec313d15c45b6e3d4d0d65
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-30 19:35:01 +0000

    fix(x11/libpeas): Fix building with glib-cross for cross compilation
    This fixes the following error.
    /data/data/com.termux/files/usr/bin/glib-genmarshal: 20: import: not found

--- x11-packages/libpeas/build.sh
+++ x11-packages/libpeas/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/Libpeas
 TERMUX_PKG_DESCRIPTION="A gobject-based plugins engine"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=1.36
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libpeas/${_MAJOR_VERSION}/libpeas-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION="1.36.0"
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libpeas/${TERMUX_PKG_VERSION%.*}/libpeas-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=297cb9c2cccd8e8617623d1a3e8415b4530b8e5a893e3527bbfd1edd13237b4c
 TERMUX_PKG_DEPENDS="glib, gobject-introspection, gtk3"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlua51=false
@@ -19,4 +19,15 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
