2c98902ab3e273de26f6505826c0318462b4c231
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-30 15:46:17 +0000

    fix(x11/gvfs): Fix building with glib-cross for cross compilation
    This fixes the following error.
    /data/data/com.termux/files/usr/bin/gdbus-codegen: 23: import: not found

--- x11-packages/gvfs/build.sh
+++ x11-packages/gvfs/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/gvfs
 TERMUX_PKG_DESCRIPTION="A userspace virtual filesystem implementation for GIO"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=1.50
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gvfs/${_MAJOR_VERSION}/gvfs-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION="1.50.5"
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gvfs/${TERMUX_PKG_VERSION%.*}/gvfs-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b86f09b7331c8642ecebf46a3cda0692f5eb26086f132326a5483c2ebf86a4cb
 TERMUX_PKG_DEPENDS="dbus, gcr, glib, gsettings-desktop-schemas, libarchive, libsoup3, libxml2"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -36,3 +35,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlogind=false
 -Dlibusb=false
 "
+
+termux_step_pre_configure() {
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
+}
