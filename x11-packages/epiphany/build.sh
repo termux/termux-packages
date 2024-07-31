29fa8ec57bc21294930f7591cb83c2464f34a140
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-31 09:03:41 +0000

    fix(x11/epiphany): Fix building with glib-cross for cross compilation
    This fixes the following error.
    OSError: [Errno 8] Exec format error: '/data/data/com.termux/files/usr/bin/glib-mkenums'

--- x11-packages/epiphany/build.sh
+++ x11-packages/epiphany/build.sh
@@ -2,12 +2,25 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Apps/Web
 TERMUX_PKG_DESCRIPTION="A GNOME web browser based on the WebKit rendering engine"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=44
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.5
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/epiphany/${_MAJOR_VERSION}/epiphany-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION="44.5"
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/epiphany/${TERMUX_PKG_VERSION%.*}/epiphany-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3c475e04ed2a0f410cb44b96937563697079193ac9b7a50c91485fb9b08315ab
 TERMUX_PKG_DEPENDS="adwaita-icon-theme, gcr4, gdk-pixbuf, glib, graphene, gsettings-desktop-schemas, gstreamer, gtk4, iso-codes, json-glib, libadwaita, libarchive, libcairo, libgmp, libnettle, libportal-gtk4, libsecret, libsoup3, libsqlite, libxml2, pango, webkitgtk-6.0"
+TERMUX_PKG_BUILD_DEPENDS="glib-cross"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dunit_tests=disabled
 "
+
+termux_step_pre_configure() {
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
+}
