e71de0b7c9448bcb9c16eb9a82a7fd3976dc6c2e
Biswapriyo Nath <nathbappai@gmail.com>
2024-07-31 05:43:55 +0000

    fix(x11/pulseeffects): Fix building with glib-cross for cross compilation
    This fixes the following error.
    /bin/sh: 1: /data/data/com.termux/files/usr/bin/glib-compile-resources: Exec format error

--- x11-packages/pulseeffects/build.sh
+++ x11-packages/pulseeffects/build.sh
@@ -4,12 +4,12 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Do not upgrade to EasyEffects version.
 TERMUX_PKG_VERSION=4.8.7
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/wwmm/easyeffects/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d841f27df87b99747349be6b8de62d131422369908fcb57a81f39590437a8099
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="boost, glib, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gstreamer, gtk3, gtkmm3, libbs2b, libc++, libebur128, librnnoise, libsndfile, libzita-convolver, lilv, pulseaudio"
-TERMUX_PKG_BUILD_DEPENDS="boost-headers, libsamplerate"
+TERMUX_PKG_BUILD_DEPENDS="boost-headers, glib-cross, libsamplerate"
 
 termux_step_pre_configure() {
 	case "$TERMUX_PKG_VERSION" in
@@ -18,4 +18,15 @@ termux_step_pre_configure() {
 	esac
 
 	export BOOST_ROOT=$TERMUX_PREFIX
+
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
