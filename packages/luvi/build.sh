51922f2ac6cbe89220e0653685de5d26def066f6
Komo <71205197+cattokomo@users.noreply.github.com>
2024-08-03 16:22:31 +0000

    fix(main/luvi): fix build (#20990)
    Co-authored-by: komothecat <71205197+komothecat@users.noreply.github.com>

--- packages/luvi/build.sh
+++ packages/luvi/build.sh
@@ -2,21 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://luvit.io
 TERMUX_PKG_DESCRIPTION="A project in-between luv and luvit."
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Komo @cattokomo"
-TERMUX_PKG_VERSION=20240216
-TERMUX_PKG_REVISION=2
-_commit=b85da58f2549a519486a7296f5b836a8f6a64880
-_version=2.14.0
+TERMUX_PKG_VERSION=20240723
+_commit=4e92094134161601a841d2ff5c8b0759fcc743b1
+_version=2.14.0+g${_commit::7}
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_SRCURL=git+https://github.com/luvit/luvi.git
-TERMUX_PKG_DEPENDS="pcre, openssl, luv, lua51-lpeg, libluajit"
+TERMUX_PKG_DEPENDS="pcre2, openssl, luv, libluajit"
 TERMUX_PKG_SUGGESTS="lit, luvit"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 	-DWithSharedLibluv=On
 	-DWithOpenSSL=On
 	-DWithSharedOpenSSL=On
-	-DWithPCRE=On
-	-DWithSharedPCRE=On
+	-DWithPCRE2=On
+	-DWithSharedPCRE2=On
 	-DWithLPEG=On
 "
 
@@ -35,9 +34,8 @@ termux_step_pre_configure() {
 		-DLIBUV_LIBRARIES=$lib/libuv.so
 		-DOPENSSL_INCLUDE_DIR=$include
 		-DOPENSSL_LIBRARIES=$lib
-		-DPCRE_INCLUDE_DIR=$include
-		-DPCRE_LIBRARIES=$lib
-		-DLPEG_LIBRARY=$lib/liblpeg-5.1.so
+		-DPCRE2_INCLUDE_DIR=$include
+		-DPCRE2_LIBRARIES=$lib
 	"
 
 	case "${TERMUX_ARCH}" in
