03e9e6782123d416e096950c3f8e3d87b373e243
Patrick Gaskin <patrick@pgaskin.net>
2024-08-08 10:09:26 -0400

    enhance(main/cmus): Add AAudio output plugin
    Support native audio on Android 8.1 and later.
    
    - Better battery life.
    - Configurable latency.
    - Configurable capture policy.
    - Configurable sharing mode.
    - Configurable spatialization.
    - Full multi-channel audio support.
    - Supports `pause_on_output_change`.
    
    Also includes fix for pause_on_output change with softvol enabled
    since aaudio doesn't have volume control.
    
    cmus/cmus#1346
    cmus/cmus#1353

--- packages/cmus/build.sh
+++ packages/cmus/build.sh
@@ -10,6 +10,25 @@ TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	# cherry-pick patches
+	commit=(
+		319b9a4ae59d942eecffb699c27356d9ba7e1f73 # aaudio output plugin (cmus/cmus#1346)
+		be3a140a6ead1a01dbd21fbaaa9b37cd4a5f6c84 # fix pause_on_output_change with softvol (cmus/cmus#1353)
+	)
+	for sha in ${commit[@]}; do
+		termux_download https://github.com/cmus/cmus/commit/$sha.patch $sha.patch SKIP_CHECKSUM # skip checksum since we're already referencing an exact commit hash
+		git apply $sha.patch
+	done
+
+	# we need to be able to link against aaudio even on older api levels (it will fall back properly at runtime)
+	if [ $TERMUX_PKG_API_LEVEL -lt 26 ]; then
+		local _libdir="$TERMUX_PKG_TMPDIR/libaaudio"
+		rm -rf "${_libdir}"
+		mkdir -p "${_libdir}"
+		cp "$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/26/libaaudio.so" "${_libdir}"
+		LDFLAGS+=" -L${_libdir}"
+	fi
+
 	LD=$CC
 	LDFLAGS+=" -lm"
 	export CUE_LIBS=" -lm"
@@ -18,4 +37,11 @@ termux_step_pre_configure() {
 
 termux_step_configure() {
 	./configure prefix=$TERMUX_PREFIX
+}
+
+termux_step_post_massage() {
+	# it's weakly linked and we do funny stuff with it, so ensure it actually got linked properly
+	if ! $READELF --needed-libs lib/cmus/op/aaudio.so | grep -E '^\s*libaaudio.so$' -q; then
+		termux_error_exit "DT_NEEDED for aaudio is not correctly set"
+	fi
 }
