ec437f14b0ebd136733dd4d5582a0d7014576503
Biswapriyo Nath <nathbappai@gmail.com>
2024-08-09 07:44:11 +0000

    bump(main/poac): 0.10.0

--- packages/poac/build.sh
+++ packages/poac/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/poac-dev/poac
 TERMUX_PKG_DESCRIPTION="A package manager and build system for C++"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.10.0"
 TERMUX_PKG_SRCURL="https://github.com/poac-dev/poac/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=122aa46923e3e93235305b726617df7df747ed7a26072ccd6b87ffaf84a33aed
+TERMUX_PKG_SHA256=4bdede67b28f9622c071bef8c7eae76062c9ef2ad122deee49d994668e846288
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BUILD_DEPENDS="nlohmann-json"
-TERMUX_PKG_DEPENDS="libtbb, libgit2, libcurl"
-TERMUX_PKG_SUGGESTS="clang, make, pkg-config, fmt"
+TERMUX_PKG_BUILD_DEPENDS="fmt, nlohmann-json"
+TERMUX_PKG_DEPENDS="libc++, libcurl, libgit2, libtbb"
+TERMUX_PKG_SUGGESTS="clang, make, pkg-config"
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
 termux_step_make() {
