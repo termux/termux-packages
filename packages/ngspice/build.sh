943fc2c09616c6bcc6a8b19889acf5b94340dc80
Jia Yuan Lo <jylo06g@gmail.com>
2024-08-09 15:08:03 +0800

    revbump(main/ngspice): mark this package OpenMP enabled

--- packages/ngspice/build.sh
+++ packages/ngspice/build.sh
@@ -4,22 +4,24 @@ TERMUX_PKG_LICENSE="BSD 3-Clause, LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="43"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/imr/ngspice/archive/refs/tags/ngspice-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4c0443250f777f4e24884263674e33cfe4bdff9756bd846ac7a69ac36192bf73
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_UPDATE_VERSION_SED_REGEXP="s/ngspice-//"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-xspice
 --enable-cider
---with-readline=yes
 --enable-openmp
+--enable-xspice
+--with-x=no
+--with-readline=yes
 "
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="
---with-x=no
 --enable-cider
 --enable-xspice
+--with-x=no
 "
 TERMUX_PKG_DEPENDS="fftw, libc++, ncurses, readline"
 TERMUX_PKG_GROUPS="science"
@@ -29,10 +31,11 @@ termux_step_host_build() {
 	$TERMUX_PKG_SRCDIR/configure $TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS
 
 	# compiles ngspice codemodel preprocessor
-	cd src/xspice/cmpp && make
+	cd src/xspice/cmpp && make -j $TERMUX_PKG_MAKE_PROCESSES
 }
 
 termux_step_pre_configure() {
+	LDFLAGS+=" -fopenmp -static-openmp"
 	autoreconf -fi
 }
 
