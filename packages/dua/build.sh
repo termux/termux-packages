717dbdf947fa5b6c3a4d5a12d88c2635912d77d9
Biswapriyo Nath <nathbappai@gmail.com>
2024-08-10 08:43:36 +0000

    bump(main/dua): 2.29.1

--- packages/dua/build.sh
+++ packages/dua/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Byron/dua-cli
 TERMUX_PKG_DESCRIPTION="View disk space usage and delete unwanted data, fast"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.29.0"
+TERMUX_PKG_VERSION="2.29.1"
 TERMUX_PKG_SRCURL=https://github.com/Byron/dua-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=af58bfc5146b296ced1ed711b0bbd21bce731a69fb6bea6622e6acfbe180a91a
+TERMUX_PKG_SHA256=f818175cc675af37361c8800b298bb0d87be4e5354d964cfe8e36e478f739b7d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
