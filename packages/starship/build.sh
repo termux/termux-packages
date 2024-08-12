130a349df24b67fe5bf072e2c015683a53a908ac
TomIO <tomjo00@web.de>
2024-08-12 13:45:36 +0200

    fix(main/starship): typo in Bash completions dir - enhance(main/starship): run `post_install` after reinstalling or upgrading with Pacman

--- packages/starship/build.sh
+++ packages/starship/build.sh
@@ -35,11 +35,11 @@ termux_step_pre_configure() {
 
 termux_step_post_make_install() {
 	# Make a placeholder for shell-completions (to be filled with postinst)
-	mkdir -p "${TERMUX_PREFIX}"/share/bash-completions/completions
+	mkdir -p "${TERMUX_PREFIX}"/share/bash-completion/completions
 	mkdir -p "${TERMUX_PREFIX}"/share/elvish/lib
 	mkdir -p "${TERMUX_PREFIX}"/share/fish/vendor_completions.d
 	mkdir -p "${TERMUX_PREFIX}"/share/zsh/site-functions
-	touch "${TERMUX_PREFIX}"/share/bash-completions/completions/starship
+	touch "${TERMUX_PREFIX}"/share/bash-completion/completions/starship
 	touch "${TERMUX_PREFIX}"/share/elvish/lib/starship.elv
 	touch "${TERMUX_PREFIX}"/share/fish/vendor_completions.d/starship.fish
 	touch "${TERMUX_PREFIX}"/share/zsh/site-functions/_starship
@@ -53,9 +53,13 @@ termux_step_create_debscripts() {
 	cat <<-EOF >./postinst
 		#!${TERMUX_PREFIX}/bin/sh
 
-		starship completions bash > ${TERMUX_PREFIX}/share/bash-completions/completions/starship
+		starship completions bash > ${TERMUX_PREFIX}/share/bash-completion/completions/starship
 		starship completions elvish > "$TERMUX_PREFIX/share/elvish/lib/starship.elv"
 		starship completions fish > ${TERMUX_PREFIX}/share/fish/vendor_completions.d/starship.fish
 		starship completions zsh > ${TERMUX_PREFIX}/share/zsh/site-functions/_starship
 	EOF
+
+	if [[ "$TERMUX_PACKAGE_FORMAT" == 'pacman' ]]; then
+		echo 'post_install' > postupg
+	fi
 }
