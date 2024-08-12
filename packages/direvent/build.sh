fe9f39184426c3409b11d889502f3f100faa8e9a
Twaik Yont <twaikyont@gmail.com>
2024-08-12 20:46:43 +0300

    bump(main/direvent): 5.4

--- packages/direvent/build.sh
+++ packages/direvent/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org.ua/software/direvent/
 TERMUX_PKG_DESCRIPTION="Monitor of events in file system directories"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=5.3
+TERMUX_PKG_VERSION="5.4"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/direvent/direvent-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9405a8a77da49fe92bbe4af18bf925ff91f6d3374c10b7d700a031bacb94c497
+TERMUX_PKG_SHA256=1dbbc6192aab67e345725148603d570c6a2828380c964215762af91524d795ba
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob"
 
 termux_step_pre_configure() {
