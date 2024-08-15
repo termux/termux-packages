ace24deb8e02dd4fe98325518b6401c2194a15c4
Kevin WIlliams <admin@utermux.dev>
2024-08-15 10:06:22 +0800

    fix(root/fio): auto update
    %ci:no-build

--- root-packages/fio/build.sh
+++ root-packages/fio/build.sh
@@ -9,6 +9,16 @@ TERMUX_PKG_DEPENDS="openssl, libandroid-shmem, libaio"
 TERMUX_PKG_SUGGESTS="python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+"
+
+termux_pkg_auto_update() {
+	local tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}")"
+	if grep -qP "^fio-${TERMUX_PKG_UPDATE_VERSION_REGEXP}\$" <<<"$tag"; then
+		termux_pkg_upgrade_version "$tag"
+	else
+		echo "WARNING: Skipping auto-update: Not stable release($tag)"
+	fi
+}
 
 termux_step_pre_configure() {
 	sed -i "s/@VERSION@/${TERMUX_PKG_VERSION}/g" $TERMUX_PKG_SRCDIR/Makefile
