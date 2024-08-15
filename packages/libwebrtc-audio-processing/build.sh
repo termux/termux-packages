983514d74c29b3dec24b0f87f37658b1b4faa4b1
Fredrik Fornwall <fredrik@fornwall.net>
2024-08-15 12:52:25 +0200

    bump(main/libwebrtc-audio-processing): 1.3
    Switch from freedesktop.org to www.freedesktop.org as the former has an
    invalid certificate, fixing the build error noted in #21130.

--- packages/libwebrtc-audio-processing/build.sh
+++ packages/libwebrtc-audio-processing/build.sh
@@ -1,13 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://freedesktop.org/software/pulseaudio/webrtc-audio-processing/
+TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/software/pulseaudio/webrtc-audio-processing/
 TERMUX_PKG_DESCRIPTION="A library containing the AudioProcessing module from the WebRTC project"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.1
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://freedesktop.org/software/pulseaudio/webrtc-audio-processing/webrtc-audio-processing-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4dabdd0789acd117d88d53f0a793cf4e906c6a93ee9aa6975ec928eafbf1dfe5
-TERMUX_PKG_DEPENDS="libc++"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_VERSION=1.3
+TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/pulseaudio/webrtc-audio-processing/webrtc-audio-processing-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=95552fc17faa0202133707bbb3727e8c2cf64d4266fe31bfdb2298d769c1db75
+TERMUX_PKG_DEPENDS="libc++, abseil-cpp"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
