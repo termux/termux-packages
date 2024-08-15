9c504a523239cf66efabcc636b27403cfc541b2a
Fredrik Fornwall <fredrik@fornwall.net>
2024-08-15 12:43:48 +0200

    bump(main/magic-wormhole-rs): 0.7.1
    Fixes the current rust 1.80 build failure (#21130).

--- packages/magic-wormhole-rs/build.sh
+++ packages/magic-wormhole-rs/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION=" Rust implementation of Magic Wormhole, with new feature
 TERMUX_PKG_LICENSE="EUPL-1.2"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.6.1"
+TERMUX_PKG_VERSION="0.7.1"
 TERMUX_PKG_SRCURL="https://github.com/magic-wormhole/magic-wormhole.rs/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=522db57161bb7df10feb4b0ca8dec912186f24a0974647dc4fccdd8f70649f96
+TERMUX_PKG_SHA256=c6e2acd3cccd982f449d26184d714d4cf813f51b8b75b3e36ecbb78565b3f4e8
 TERMUX_PKG_BUILD_IN_SRC=true
-# disable auto-update since 'remove-clipboard' patch was maually crafted for v0.6.0
+# disable auto-update since 'remove-clipboard' patch was maually crafted for v0.7.1
 TERMUX_PKG_AUTO_UPDATE=false
 
 termux_step_make() {
