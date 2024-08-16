310ed2ed68c29d3b00f40821e5fe4726d572d664
Fredrik Fornwall <fredrik@fornwall.net>
2024-08-16 12:23:49 +0200

    fix(main/marisa): Fix build by specifying license file
    See https://github.com/s-yata/marisa-trie/blob/master/COPYING.md
    
    Fixes build failure noted in #21130

--- packages/marisa/build.sh
+++ packages/marisa/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/s-yata/marisa-trie
 TERMUX_PKG_DESCRIPTION="Matching Algorithm with Recursively Implemented StorAge"
 TERMUX_PKG_LICENSE="BSD 2-Clause, LGPL-2.1"
+TERMUX_PKG_LICENSE_FILE="COPYING.md"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.2.6
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/s-yata/marisa-trie/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1063a27c789e75afa2ee6f1716cc6a5486631dcfcb7f4d56d6485d2462e566de
 TERMUX_PKG_DEPENDS="libc++"
