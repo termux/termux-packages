058360b7350de01008821afd4ef44b1836fa2b56
Biswapriyo Nath <nathbappai@gmail.com>
2024-08-18 04:46:06 +0000

    bump(main/ncdc): 1.24.1

--- packages/ncdc/build.sh
+++ packages/ncdc/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://dev.yorhel.nl/ncdc
 TERMUX_PKG_DESCRIPTION="Modern and lightweight direct connect client with a friendly ncurses interface"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.24
+TERMUX_PKG_VERSION="1.24.1"
 TERMUX_PKG_SRCURL=https://dev.yorhel.nl/download/ncdc-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=233510d5355fc72fdad3578ebc8ab35d1da95b21d2774990f3a1356b7112da1e
+TERMUX_PKG_SHA256=2a8ab9ad7d43f018fc73ba8babd689dfa44aba8cec53b88e4770185cb97778f7
 TERMUX_PKG_DEPENDS="glib, libbz2, libgnutls, libsqlite, ncurses, zlib"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_configure() {
