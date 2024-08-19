564083ff7a067a4e88f3ed02a814633deee63739
Biswapriyo Nath <nathbappai@gmail.com>
2024-08-19 07:40:21 +0000

    bump(main/graphicsmagick): 1.3.44
    * Remove coders-url.c.patch file which was applied in upstream.
    https://sourceforge.net/p/graphicsmagick/code/ci/ffe38e523f547a43219c8cdefc01c51fa4b51671/
    
    * Fix linking with static openmp library.

--- packages/graphicsmagick/build.sh
+++ packages/graphicsmagick/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=http://www.graphicsmagick.org/
 TERMUX_PKG_DESCRIPTION="Collection of image processing tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.3.43"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="1.3.44"
 # Bandwith limited on main ftp site, so it's asked to use sourceforge instead:
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/${TERMUX_PKG_VERSION}/GraphicsMagick-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=2b88580732cd7e409d9e22c6116238bef4ae06fcda11451bf33d259f9cbf399f
+TERMUX_PKG_SHA256=6ac28470d2fbd3d5f60859dd43f3cee2585e955e32896f892b4dc61dda101ea0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, libbz2, libc++, libde265, libheif, libjasper, libjpeg-turbo, libjxl, liblzma, libpng, libtiff, libwebp, libxml2, littlecms, zlib, zstd"
 TERMUX_PKG_BREAKS="graphicsmagick-dev"
@@ -16,3 +15,11 @@ ac_cv_func_ftime=no
 --with-fontpath=/system/fonts
 --without-x
 "
+
+termux_step_pre_configure() {
+	pushd "${TERMUX_PKG_SRCDIR}"
+	# Otherwise shared libraries depend on libomp.so
+	sed -i "s/-lomp/-l:libomp.a/g" configure.ac
+	autoreconf -fi
+	popd
+}
