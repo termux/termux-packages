530d6e388bc029cfdecc78a386599037749343cd
TomIO <tomjo00@web.de>
2024-08-19 20:26:39 +0200

    bump(main/dpkg): 1.22.11 and switch to gitlab release tarballs

--- packages/dpkg/build.sh
+++ packages/dpkg/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://packages.debian.org/dpkg
 TERMUX_PKG_DESCRIPTION="Debian package management system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.22.6"
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/debian/pool/main/d/dpkg/dpkg_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4379123466cf1804f82aaac7fbea7133c58aefa178dfbf7029cdc61a8d220655
+TERMUX_PKG_VERSION="1.22.11"
+# old tarball are removed in https://mirrors.kernel.org/debian/pool/main/d/dpkg/dpkg_${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SRCURL=https://salsa.debian.org/dpkg-team/dpkg/-/archive/${TERMUX_PKG_VERSION}/dpkg-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=fcda5f642c07f7c56f3bb77cb937862819323e1d0acf01cd8cd62668aeb6c454
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="bzip2, coreutils, diffutils, gzip, less, libbz2, liblzma, libmd, tar, xz-utils, zlib, zstd"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="clang"
@@ -70,6 +71,7 @@ share/polkit-1
 "
 
 termux_step_pre_configure() {
+	( cd "${TERMUX_PKG_SRCDIR}" && autoreconf -fi )
 	export TAR=tar # To make sure dpkg tries to use "tar" instead of e.g. "gnutar" (which happens when building on OS X)
 	perl -p -i -e "s/TERMUX_ARCH/$TERMUX_ARCH/" $TERMUX_PKG_SRCDIR/configure
 	sed -i 's/$req_vars = \$arch_vars.$varname./if ($varname eq "DEB_HOST_ARCH_CPU" or $varname eq "DEB_HOST_ARCH"){ print("'$TERMUX_ARCH'");exit; }; $req_vars = $arch_vars{$varname}/' scripts/dpkg-architecture.pl
