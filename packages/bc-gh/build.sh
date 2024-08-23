836b7898f3b8c95de663fe73d566f85ad5368cb3
Gavin D. Howard <gavin@gavinhoward.com>
2024-08-22 21:08:28 -0600

    bump(main/bc-gh): 7.0.0
    Signed-off-by: Gavin D. Howard <gavin@gavinhoward.com>

--- packages/bc-gh/build.sh
+++ packages/bc-gh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://git.gavinhoward.com/gavin/bc
 TERMUX_PKG_DESCRIPTION="Unix dc and POSIX bc with GNU and BSD extensions"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Gavin D. Howard <gavin@gavinhoward.com>"
-TERMUX_PKG_VERSION=6.7.6
+TERMUX_PKG_VERSION=7.0.0
 TERMUX_PKG_SRCURL=https://github.com/gavinhoward/bc/releases/download/${TERMUX_PKG_VERSION}/bc-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e553ac9e4d19b8f597a18df2552992d66aa6e5d5d73043616710fd82f55f7b8f
+TERMUX_PKG_SHA256=ea99f18482c2fd3775f1b6de4b2faf6dd2eed9e8e6699c0d4ecf54b539affd43
 TERMUX_PKG_DEPENDS="readline"
 
 termux_step_configure() {
