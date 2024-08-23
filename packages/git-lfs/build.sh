1c541e73837781556adba14ad4e88a6f65d5e79f
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-08-24 00:53:21 +0800

    git-lfs: revbump to rebuild

--- packages/git-lfs/build.sh
+++ packages/git-lfs/build.sh
@@ -3,30 +3,19 @@ TERMUX_PKG_DESCRIPTION="Git extension for versioning large files"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.5.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/git-lfs/git-lfs/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d682a12c0bc48d08d28834dd0d575c91d53dd6c6db63c45c2db7c3dd2fb69ea4
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_golang
-	export GOPATH=$TERMUX_PKG_BUILDDIR
 
-	mkdir -p "$GOPATH"/github.com/git-lfs
-	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/github.com/git-lfs/git-lfs
-
-	cd "$GOPATH"/github.com/git-lfs/git-lfs
-	! $TERMUX_ON_DEVICE_BUILD && GOOS=linux GOARCH=amd64 CC=gcc LD=gcc go generate github.com/git-lfs/git-lfs/commands
+	! $TERMUX_ON_DEVICE_BUILD && GOOS=linux GOARCH=amd64 CC=gcc LD=gcc go generate ./commands
 	go build git-lfs.go
 }
 
 termux_step_make_install() {
-	install -Dm700 \
-		"$GOPATH"/github.com/git-lfs/git-lfs/git-lfs \
-		"$TERMUX_PREFIX"/bin/git-lfs
-}
-
-termux_step_post_make_install() {
-	# Remove read-only files generated in build process.
-	chmod -R 700 "$TERMUX_PKG_BUILDDIR"/pkg
-	rm -rf "$TERMUX_PKG_BUILDDIR"/pkg
+	install -Dm700 git-lfs "$TERMUX_PREFIX"/bin/git-lfs
 }
