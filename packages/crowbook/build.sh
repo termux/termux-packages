928da004f862d1df9a1bb477c03e392191ab10a3
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-08-25 03:29:40 +0800

    crowbook: revbump to rebuild

--- packages/crowbook/build.sh
+++ packages/crowbook/build.sh
@@ -3,11 +3,27 @@ TERMUX_PKG_DESCRIPTION="Allows you to write a book in Markdown without worrying 
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.16.1"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/crowdagger/crowbook/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2e49a10f1b14666d4f740e9a22a588d44b137c3fca0932afc50ded0280450311
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo vendor
+	patch --silent -p1 \
+		-d ./vendor/time/ \
+		< "$TERMUX_PKG_BUILDER_DIR"/time-items-format_items.diff
+
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo 'time = { path = "./vendor/time" }' >> Cargo.toml
+}
 
 termux_step_make() {
 	termux_setup_rust
@@ -16,4 +32,9 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/crowbook
+}
+
+termux_step_post_make_install() {
+	# Remove the vendor sources to save space
+	rm -rf "$TERMUX_PKG_SRCDIR"/vendor
 }
