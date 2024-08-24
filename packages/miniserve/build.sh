d847aea32c0c7888805b1e9ea70f6f017e9023a3
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-08-25 02:48:11 +0800

    miniserve: revbump to rebuild

--- packages/miniserve/build.sh
+++ packages/miniserve/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tool to serve files and dirs over HTTP"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.27.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/svenstaro/miniserve/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b65580574ca624072b1a94d59ebf201ab664eacacb46a5043ef7b81ebb538f80
 TERMUX_PKG_AUTO_UPDATE=true
@@ -10,6 +11,18 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo vendor
+	patch --silent -p1 \
+		-d ./vendor/time/ \
+		< "$TERMUX_PKG_BUILDER_DIR"/time-items-format_items.diff
+
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo 'time = { path = "./vendor/time" }' >> Cargo.toml
 
 	rm -f Makefile
 }
@@ -22,6 +35,9 @@ termux_step_post_make_install() {
 
 	# manpage
 	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/man/man1/miniserve.1
+
+	# remove the vendor sources to save space
+	rm -rf "$TERMUX_PKG_SRCDIR"/vendor
 }
 
 termux_step_create_debscripts() {
