88bb2f414eb58eba4a99a7cb5278684a803ebf8f
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-08-25 03:24:02 +0800

    typst: revbump to rebuild

--- packages/typst/build.sh
+++ packages/typst/build.sh
@@ -3,10 +3,27 @@ TERMUX_PKG_DESCRIPTION="A new markup-based typesetting system that is powerful a
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.11.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/typst/typst
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl"
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo vendor
+	patch --silent -p1 \
+		-d ./vendor/time/ \
+		< "$TERMUX_PKG_BUILDER_DIR"/time-items-format_items.diff
+
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo 'time = { path = "./vendor/time" }' >> Cargo.toml
+}
 
 termux_step_make() {
 	termux_setup_rust
@@ -24,4 +41,9 @@ termux_step_make_install() {
 	install -Dm644 -t "${TERMUX_PREFIX}/share/zsh/site-functions/" "${_artifacts}/_${TERMUX_PKG_NAME}"
 	install -Dm644 -t "${TERMUX_PREFIX}/share/fish/vendor_completions.d/" "${_artifacts}/${TERMUX_PKG_NAME}.fish"
 	install -Dm644 "${_artifacts}/${TERMUX_PKG_NAME}.bash" "${TERMUX_PREFIX}/share/bash-completion/completions/${TERMUX_PKG_NAME}"
+}
+
+termux_step_post_make_install() {
+	# Remove the vendor sources to save space
+	rm -rf "$TERMUX_PKG_SRCDIR"/vendor
 }
