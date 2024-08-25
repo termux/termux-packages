d7d833dd258cf5dbcf53f5a9c29e93a49923c242
Jia Yuan Lo <jylo06g@gmail.com>
2024-08-25 17:39:08 +0800

    fix(main/fluidsynth): mark this package OpenMP enabled (#21234)

--- packages/fluidsynth/build.sh
+++ packages/fluidsynth/build.sh
@@ -3,8 +3,15 @@ TERMUX_PKG_DESCRIPTION="Software synthesizer based on the SoundFont 2 specificat
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.3.6"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3340d73286b28fe6e5150fbe12648d4640e86c64c228878b572773bd08cac531
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="dbus, glib, libc++, libsndfile, pulseaudio, readline"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DLIB_INSTALL_DIR=${TERMUX_PREFIX}/lib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DLIB_INSTALL_DIR=${TERMUX_PREFIX}/lib
+"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -l:libomp.a"
+}
