4705593f40c845678d49853c9ac448daa9886582
Biswapriyo Nath <nathbappai@gmail.com>
2024-08-25 10:03:11 +0000

    addpkg(main/groonga): 14.0.6

---
+++ packages/groonga/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/groonga/groonga/
+TERMUX_PKG_DESCRIPTION="An embeddable fulltext search engine"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="14.0.6"
+TERMUX_PKG_SRCURL=https://github.com/groonga/groonga/releases/download/v${TERMUX_PKG_VERSION}/groonga-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d5c693400c9cbe91b63e0b5bc489f9f07af1bd68f12a089ccd1482ef69d6cc6c
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libarrow-cpp, libc++, liblz4, libstemmer, onigmo, xxhash, zlib, zstd"
+TERMUX_PKG_BUILD_DEPEPNDS="rapidjson"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DGRN_WITH_APACHE_ARROW=ON
+-DGRN_WITH_LIBSTEMMER=system
+-DGRN_WITH_LZ4=system
+-DGRN_WITH_ONIGMO=ON
+-DGRN_WITH_BUNDLED_ONIGMO=OFF
+-DGRN_WITH_RAPIDJSON=system
+-DGRN_WITH_XXHASH=system
+-DGRN_WITH_ZLIB=system
+-DGRN_WITH_ZSTD=system
+"
