08d52b920e7924d21baacff0b0d628c7bbe381a1
Biswapriyo Nath <nathbappai@gmail.com>
2024-08-25 20:22:50 +0000

    bump(x11/gnome-desktop3): 44.1

--- x11-packages/gnome-desktop3/build.sh
+++ x11-packages/gnome-desktop3/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gnome-desktop
 TERMUX_PKG_DESCRIPTION="Utility library for loading .desktop files"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="44.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="44.1"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gnome-desktop/${TERMUX_PKG_VERSION%.*}/gnome-desktop-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=42c773745d84ba14bc1cf1c4c6f4606148803a5cd337941c63964795f3c59d42
+TERMUX_PKG_SHA256=ae7ca55dc9e08914999741523a17d29ce223915626bd2462a120bf96f47a79ab
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gsettings-desktop-schemas, gtk3, iso-codes, libcairo, libxkbcommon, xkeyboard-config"
 TERMUX_PKG_BUILD_DEPENDS="fontconfig, g-ir-scanner, glib-cross"
 TERMUX_PKG_DISABLE_GIR=false
@@ -18,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_gir
+	TERMUX_PKG_VERSION=. termux_setup_gir
 
 	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
 	mkdir -p "${_WRAPPER_BIN}"
