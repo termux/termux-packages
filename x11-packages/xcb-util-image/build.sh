7f6bab049a7da7a0fba87ceb838047c4925ea2da
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-08-29 09:23:35 +0200

    chore(x11/xcb-util-image): link against libandroid-shmem explicitly (#21172)
    part of termux#21130

--- x11-packages/xcb-util-image/build.sh
+++ x11-packages/xcb-util-image/build.sh
@@ -3,7 +3,12 @@ TERMUX_PKG_DESCRIPTION="Utility libraries for XC Binding - Port of Xlib's XImage
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.4.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xcb.freedesktop.org/dist/xcb-util-image-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ccad8ee5dadb1271fd4727ad14d9bd77a64e505608766c4e98267d9aede40d3d
-TERMUX_PKG_DEPENDS="libxcb, xcb-util"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libxcb, xcb-util"
 TERMUX_PKG_BUILD_DEPENDS="xorg-util-macros"
+
+termux_step_pre_configure() {
+	LDFLAGS+=' -landroid-shmem'
+}
