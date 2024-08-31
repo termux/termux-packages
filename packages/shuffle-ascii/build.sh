5ef2110de3f0db7ff03a69e878b8fd372858ddb4
Lennart Martens <monkeynator78@gmail.com>
2024-08-31 07:05:15 +0200

    added pkg shuffle-ascii
    'shuffle-ascii' is a CLI command to print colored ASCII art with a nice shuffle effetc. It can also shuffle every kind of text file.

---
+++ packages/shuffle-ascii/build.sh
@@ -0,0 +1,25 @@
+# http(s) link to package home page.
+TERMUX_PKG_HOMEPAGE=https://github.com/Lennart1978/shuffle/tree/termux
+
+# One-line, short package description.
+TERMUX_PKG_DESCRIPTION="Print colored ASCII art with a nice shuffle effect"
+
+# License.
+# Use SPDX identifier: https://spdx.org/licenses/
+TERMUX_PKG_LICENSE="MIT"
+
+# Who cares about package.
+# Specify yourself (Github nick, or name + email) if you wish to maintain the
+# package, fix its bugs, etc. Otherwise specify "@termux".
+# Please note that unofficial repositories are not allowed to reference @termux
+# as their maintainer.
+TERMUX_PKG_MAINTAINER="Lennart Martens monkeynator78@gmail.com"
+
+# Version.
+TERMUX_PKG_VERSION=1.1.2
+
+# URL to archive with source code.
+TERMUX_PKG_SRCURL=https://github.com/Lennart1978/shuffle/archive/refs/tags/V${TERMUX_PKG_VERSION}-3TermuxAarch64.tar.gz
+
+# SHA-256 checksum of the source code archive.
+TERMUX_PKG_SHA256=6aeb85d96fb2cf72644831d5bac0866fc33c4ca565b6e4b47e4501cc5c41ff32
