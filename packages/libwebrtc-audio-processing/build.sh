01dcda69eb7b88787ae302d0e321d499e95980b7
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-08-31 20:41:28 +0800

    libwebrtc-audio-processing: revbump
    due to symbol changes in abseil-cpp

--- packages/libwebrtc-audio-processing/build.sh
+++ packages/libwebrtc-audio-processing/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library containing the AudioProcessing module from the
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/pulseaudio/webrtc-audio-processing/webrtc-audio-processing-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=95552fc17faa0202133707bbb3727e8c2cf64d4266fe31bfdb2298d769c1db75
 TERMUX_PKG_DEPENDS="libc++, abseil-cpp"
