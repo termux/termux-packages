d04f5386b00f51154c8e78a1eb57307a2dfea120
jbara <med.anis@jbara.xyz>
2024-09-01 11:50:36 +0100

    fix(main/vis): bump lua dep to 5.4

--- packages/vis/build.sh
+++ packages/vis/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="Modern, legacy free, simple yet efficient vim-like edito
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.9"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/martanne/vis/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=bd37ffba5535e665c1e883c25ba5f4e3307569b6d392c60f3c7d5dedd2efcfca
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="liblua53, libtermkey, lua53-lpeg, ncurses"
+TERMUX_PKG_DEPENDS="liblua54, libunibilium, libtermkey, lua-lpeg, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	CFLAGS+=" -I$TERMUX_PREFIX/include -I$TERMUX_PREFIX/include/lua/5.3"
+	CFLAGS+=" -I$TERMUX_PREFIX/include -I$TERMUX_PREFIX/include/lua/5.4"
 }
 
 termux_step_create_debscripts() {
