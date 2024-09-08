ee0f7d4919525b57360367d463e03e68514d9a36
Biswapriyo Nath <nathbappai@gmail.com>
2024-09-08 16:18:40 +0000

    bump(main/plantuml): 1.2024.7

--- packages/plantuml/build.sh
+++ packages/plantuml/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://plantuml.com/
 TERMUX_PKG_DESCRIPTION="Draws UML diagrams, using a simple and human readable text description"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.2024.6"
+TERMUX_PKG_VERSION="1.2024.7"
 TERMUX_PKG_SRCURL=https://github.com/plantuml/plantuml/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7bb44fc1c53e73b5ac940df5ab47c625eb39453b128686ba456f06d1db361c94
+TERMUX_PKG_SHA256=4cdfdf38093b29b41cbafbd88e30c598c903618015c6807cfa6fc16f6e97a31e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openjdk-17"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
 termux_step_make() {
 	$TERMUX_PKG_SRCDIR/gradlew --no-daemon --parallel --stacktrace assemble
