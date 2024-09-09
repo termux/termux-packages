d85306b8fdb9da58e33850782b665eccb6369b61
Jia Yuan Lo <jylo06g@gmail.com>
2024-09-09 14:58:37 +0800

    bump(x11/thunderbird): 128.2.0

--- x11-packages/thunderbird/build.sh
+++ x11-packages/thunderbird/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.thunderbird.net
 TERMUX_PKG_DESCRIPTION="Unofficial Thunderbird email client"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="115.13.0"
-TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}/source/thunderbird-${TERMUX_PKG_VERSION}.source.tar.xz
-TERMUX_PKG_SHA256=f14ec9cb219194d5b6eabf26333f9e752ccc6d3fecb76750e6120bd8d6ae6d1f
+TERMUX_PKG_VERSION="128.2.0"
+TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}esr/source/thunderbird-${TERMUX_PKG_VERSION}esr.source.tar.xz
+TERMUX_PKG_SHA256=ca67c2b8648527548788848b94c80227be681f6262ca518114f787fb2180f6e1
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libotr, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="binutils-cross, libcpufeatures, libice, libsm"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -12,10 +12,26 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_post_get_source() {
 	local f="media/ffvpx/config_unix_aarch64.h"
 	echo "Applying sed substitution to ${f}"
-	sed -i -E '/^#define (CONFIG_LINUX_PERF|HAVE_SYSCTL) /s/1$/0/' ${f}
+	sed -E '/^#define (CONFIG_LINUX_PERF|HAVE_SYSCTL) /s/1$/0/' -i ${f}
 }
 
 termux_step_pre_configure() {
+	# XXX: flang toolchain provides libclang.so
+	termux_setup_flang
+	local __fc_dir="$(dirname $(command -v $FC))"
+	local __flang_toolchain_folder="$(realpath "$__fc_dir"/..)"
+	if [ ! -d "$TERMUX_PKG_TMPDIR/thunderbird-toolchain" ]; then
+		rm -rf "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp
+		mv "$__flang_toolchain_folder" "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp
+
+		cp "$(command -v "$CC")" "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp/bin/
+		cp "$(command -v "$CXX")" "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp/bin/
+		cp "$(command -v "$CPP")" "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp/bin/
+
+		mv "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain
+	fi
+	export PATH="$TERMUX_PKG_TMPDIR/thunderbird-toolchain/bin:$PATH"
+
 	termux_setup_nodejs
 	termux_setup_rust
 
@@ -28,19 +44,19 @@ termux_step_pre_configure() {
 
 	cargo install cbindgen
 
-	sed \
-		-e "s|@CARGO_TARGET_NAME@|${CARGO_TARGET_NAME}|" \
-		-i "${TERMUX_PKG_SRCDIR}"/build/moz.configure/rust.configure
-
 	export HOST_CC=$(command -v clang)
 	export HOST_CXX=$(command -v clang++)
+
+	export BINDGEN_CFLAGS="--target=$CCTERMUX_HOST_PLATFORM --sysroot=$TERMUX_PKG_TMPDIR/thunderbird-toolchain/sysroot"
+	local env_name=BINDGEN_EXTRA_CLANG_ARGS_${CARGO_TARGET_NAME@U}
+	env_name=${env_name//-/_}
+	export $env_name="$BINDGEN_CFLAGS"
 
 	# https://reviews.llvm.org/D141184
 	CXXFLAGS+=" -U__ANDROID__ -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC"
 	LDFLAGS+=" -landroid-shmem -landroid-spawn -llog"
 
 	if [ "$TERMUX_ARCH" = "arm" ]; then
-		termux_setup_no_integrated_as
 		# For symbol android_getCpuFeatures
 		LDFLAGS+=" -l:libndk_compat.a"
 	fi
@@ -61,6 +77,7 @@ END
 	fi
 
 	./mach configure
+	./mach tb-rust vendor
 }
 
 termux_step_make() {
