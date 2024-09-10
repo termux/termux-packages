acb6e992f82a0956c985757c48adac76108c2ab1
Ivan Maidanski <ivmai@mail.ru>
2024-09-10 09:08:47 +0300

    libgc: turn on package autoupdate

--- packages/libgc/build.sh
+++ packages/libgc/build.sh
@@ -10,6 +10,8 @@ TERMUX_PKG_BREAKS="libgc-dev"
 TERMUX_PKG_REPLACES="libgc-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-libatomic-ops=none"
 TERMUX_PKG_RM_AFTER_INSTALL="share/gc"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+.\d+.[1-9]\d*" # all excluding experimental releases (ending with ".0")
 
 termux_step_post_get_source() {
 	./autogen.sh
