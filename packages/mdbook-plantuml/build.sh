c956ae0a770c5106bdc90e3aaddf47de375d9faa
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-11 01:27:27 +0200

    chore(main/mdbook-plantuml): Update away from openssl-1.1 dependency

--- packages/mdbook-plantuml/build.sh
+++ packages/mdbook-plantuml/build.sh
@@ -3,20 +3,10 @@ TERMUX_PKG_DESCRIPTION="mdBook preprocessor to render PlantUML code blocks as im
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/sytsereitsma/mdbook-plantuml
-TERMUX_PKG_DEPENDS="openssl-1.1"
+TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	export OPENSSL_INCLUDE_DIR=$TERMUX_PREFIX/include/openssl-1.1
-	export OPENSSL_LIB_DIR=$TERMUX_PREFIX/lib/openssl-1.1
-	CFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CFLAGS"
-	CPPFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CPPFLAGS"
-	CXXFLAGS="-I$TERMUX_PREFIX/include/openssl-1.1 $CXXFLAGS"
-	LDFLAGS="-L$TERMUX_PREFIX/lib/openssl-1.1 -Wl,-rpath=$TERMUX_PREFIX/lib/openssl-1.1 $LDFLAGS"
-	
-	RUSTFLAGS+=" -C link-arg=-Wl,-rpath=$TERMUX_PREFIX/lib/openssl-1.1"
-}
 
 termux_step_make() {
 	termux_setup_rust
