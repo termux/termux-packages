termux/termux-packages

commit 6445645d7ed263f7dc90279bd573a75049380a25

build-package: mv code for setting up src and build to to its own function
%ci:no-build

build-package.sh
@@ -103,6 +103,10 @@ source "$TERMUX_SCRIPTDIR/scripts/build/termux_get_repo_files.sh"
 # shellcheck source=scripts/build/termux_step_get_dependencies.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_get_dependencies.sh"
 
+# Remove old src and build folders and create new ones
+# shellcheck source=scripts/build/termux_step_setup_build_folders.sh
+source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_setup_build_folders.sh"
+
 # Source the package build script and start building. Not to be overridden by packages.
 # shellcheck source=scripts/build/termux_step_start_build.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_start_build.sh"
@@ -373,7 +377,13 @@ while (($# > 0)); do
 
 		termux_step_setup_variables
 		termux_step_handle_buildarch
+
+		if [ "$TERMUX_CONTINUE_BUILD" == "false" ]; then
+			termux_step_setup_build_folders
+		fi
+
 		termux_step_start_build
+
 		if [ "$TERMUX_CONTINUE_BUILD" == "false" ]; then
 			termux_step_get_dependencies
 		fi

scripts/build/termux_step_setup_build_folders.sh
@@ -1,0 +1,25 @@
+termux_step_setup_build_folders() {
+	# Following directories may contain files with read-only permissions which
+	# makes them undeletable. We need to fix that.
+	[ -d "$TERMUX_PKG_BUILDDIR" ] && chmod +w -R "$TERMUX_PKG_BUILDDIR"
+	[ -d "$TERMUX_PKG_SRCDIR" ] && chmod +w -R "$TERMUX_PKG_SRCDIR"
+
+	# Cleanup old build state:
+	rm -Rf "$TERMUX_PKG_BUILDDIR" \
+		"$TERMUX_PKG_SRCDIR"
+
+	# Cleanup old packaging state:
+	rm -Rf "$TERMUX_PKG_PACKAGEDIR" \
+		"$TERMUX_PKG_TMPDIR" \
+		"$TERMUX_PKG_MASSAGEDIR"
+
+	# Ensure folders present (but not $TERMUX_PKG_SRCDIR, it will be created in build)
+	mkdir -p "$TERMUX_COMMON_CACHEDIR" \
+		 "$TERMUX_DEBDIR" \
+		 "$TERMUX_PKG_BUILDDIR" \
+		 "$TERMUX_PKG_PACKAGEDIR" \
+		 "$TERMUX_PKG_TMPDIR" \
+		 "$TERMUX_PKG_CACHEDIR" \
+		 "$TERMUX_PKG_MASSAGEDIR" \
+		 $TERMUX_PREFIX/{bin,etc,lib,libexec,share,share/LICENSES,tmp,include}
+}

scripts/build/termux_step_start_build.sh
@@ -92,29 +92,6 @@ termux_step_start_build() {
 			-e "s|@TERMUX_ARCH@|$TERMUX_ARCH|g" > $TERMUX_PREFIX/bin/llvm-config
 		chmod 755 $TERMUX_PREFIX/bin/llvm-config
 	fi
-	# Following directories may contain files with read-only permissions which
-	# makes them undeletable. We need to fix that.
-	[ -d "$TERMUX_PKG_BUILDDIR" ] && chmod +w -R "$TERMUX_PKG_BUILDDIR"
-	[ -d "$TERMUX_PKG_SRCDIR" ] && chmod +w -R "$TERMUX_PKG_SRCDIR"
-
-	# Cleanup old build state:
-	rm -Rf "$TERMUX_PKG_BUILDDIR" \
-		"$TERMUX_PKG_SRCDIR"
-
-	# Cleanup old packaging state:
-	rm -Rf "$TERMUX_PKG_PACKAGEDIR" \
-		"$TERMUX_PKG_TMPDIR" \
-		"$TERMUX_PKG_MASSAGEDIR"
-
-	# Ensure folders present (but not $TERMUX_PKG_SRCDIR, it will be created in build)
-	mkdir -p "$TERMUX_COMMON_CACHEDIR" \
-		 "$TERMUX_DEBDIR" \
-		 "$TERMUX_PKG_BUILDDIR" \
-		 "$TERMUX_PKG_PACKAGEDIR" \
-		 "$TERMUX_PKG_TMPDIR" \
-		 "$TERMUX_PKG_CACHEDIR" \
-		 "$TERMUX_PKG_MASSAGEDIR" \
-		 $TERMUX_PREFIX/{bin,etc,lib,libexec,share,share/LICENSES,tmp,include}
 
 	# Make $TERMUX_PREFIX/bin/sh executable on the builder, so that build
 	# scripts can assume that it works on both builder and host later on:

commit fc94cbfb2c61c084c7092ccbb7afbefec5a79e95

fix(scripts): do not delete `/home/builder/.termux-build` build directories for package before checking if `/data/data/.built-packages` file exists for package in `termux_step_start_build`
This was caused by 6445645d, where removal was called before even calling `termux_step_start_build` method in `build-package.sh`. So if you built a package, then ran build command again, it would exit with the "skipping (rm $TERMUX_BUILT_PACKAGES_DIRECTORY/$TERMUX_PKG_NAME to force rebuild" message, but built files would already have been deleted and everything will need to be built again even if you just removed `$TERMUX_BUILT_PACKAGES_DIRECTORY/$TERMUX_PKG_NAME` (`/data/data/.built-packages`) file for the package.

build-package.sh
@@ -611,10 +611,6 @@ for ((i=0; i<${#PACKAGE_LIST[@]}; i++)); do
 		termux_step_setup_variables
 		termux_step_handle_buildarch
 
-		if [ "$TERMUX_CONTINUE_BUILD" == "false" ]; then
-			termux_step_setup_build_folders
-		fi
-
 		termux_step_start_build
 
 		if ! termux_check_package_in_building_packages_list "${TERMUX_PKG_BUILDER_DIR#${TERMUX_SCRIPTDIR}/}"; then

scripts/build/termux_step_setup_build_folders.sh
@@ -41,4 +41,10 @@ termux_step_setup_build_folders() {
 		mkdir -p $TERMUX_PREFIX/{bin,etc,lib,share,share/LICENSES,include}
 		mkdir -p $TERMUX_PREFIX_CLASSICAL/{bin,etc,tmp}
 	fi
+
+	# Required for creating `BUILDING_IN_SRC.txt` file in termux_step_start_build
+	if [ "$TERMUX_PKG_BUILDDIR_ORIG" != "$TERMUX_PKG_BUILDDIR" ]; then
+		rm -Rf "$TERMUX_PKG_BUILDDIR_ORIG"
+		mkdir -p "$TERMUX_PKG_BUILDDIR_ORIG"
+	fi
 }

scripts/build/termux_step_start_build.sh
@@ -68,8 +68,8 @@ termux_step_start_build() {
 	# Avoid exporting PKG_CONFIG_LIBDIR until after termux_step_host_build.
 	export TERMUX_PKG_CONFIG_LIBDIR=$TERMUX_PREFIX/lib/pkgconfig:$TERMUX_PREFIX/share/pkgconfig
 
+	local TERMUX_PKG_BUILDDIR_ORIG="$TERMUX_PKG_BUILDDIR"
 	if [ "$TERMUX_PKG_BUILD_IN_SRC" = "true" ]; then
-		echo "Building in src due to TERMUX_PKG_BUILD_IN_SRC being set to true" > "$TERMUX_PKG_BUILDDIR/BUILDING_IN_SRC.txt"
 		TERMUX_PKG_BUILDDIR=$TERMUX_PKG_SRCDIR
 	fi
 
@@ -86,6 +86,14 @@ termux_step_start_build() {
 
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ] && [ "$TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED" = "true" ]; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
+	fi
+
+	# Delete and re-create the directories used for building the package
+	termux_step_setup_build_folders
+
+	if [ "$TERMUX_PKG_BUILD_IN_SRC" = "true" ]; then
+		# Create a file for users to know that the build directory not containing any built files is expected behaviour
+		echo "Building in src due to TERMUX_PKG_BUILD_IN_SRC being set to true" > "$TERMUX_PKG_BUILDDIR_ORIG/BUILDING_IN_SRC.txt"
 	fi
 
 	if [ "$TERMUX_PACKAGE_LIBRARY" = "bionic" ]; then