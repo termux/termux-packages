f5d6e8cfd301a605f06e7c981ae1cdef470288e4
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-10 10:30:45 +0200

    chore(x11/xournalpp): Mark xournalpp as replacing xournal

--- x11-packages/xournalpp/build.sh
+++ x11-packages/xournalpp/build.sh
@@ -4,11 +4,12 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE, copyright.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.2.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/xournalpp/xournalpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8817abd1794760c2a3be3a35e56a5588a51e517bc591384fa321994d50e14e7c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libandroid-execinfo, libc++, libcairo, librsvg, libsndfile, libx11, libxi, libxml2, libzip, pango, poppler, portaudio, zlib"
+TERMUX_PKG_REPLACES="xournal"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DHELP2MAN=NO
 "
