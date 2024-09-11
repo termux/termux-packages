6533f4386e8bc0bf7bd25c4dc74da6bf1899e929
TomIO <tomjo00@web.de>
2024-09-11 20:15:16 +0200

    addpkg(main/seanime): 2.1.1 Co-authored-by: aicynide <97217381+aicynide@users.noreply.github.com>

---
+++ packages/seanime/build.sh
@@ -0,0 +1,34 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/5rahim/seanime
+TERMUX_PKG_DESCRIPTION="Self-hosted anime and manga server for sea rovers."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="2.1.1"
+TERMUX_PKG_SRCURL=https://github.com/5rahim/seanime/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f3cfdf8e36d9cb2e4e5fa1e9c8bff81a4d1f3fe32c2fec848004bf02433b4833
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	termux_setup_nodejs
+
+	cp -r $TERMUX_PKG_SRCDIR/seanime-web ./seanime-web
+	cd seanime-web
+	npm install
+	npm run build
+}
+
+termux_step_pre_configure() {
+	mkdir $TERMUX_PKG_SRCDIR/web
+	cp -r $TERMUX_PKG_HOSTBUILD_DIR/seanime-web/out $TERMUX_PKG_SRCDIR/web/
+
+	termux_setup_golang
+}
+
+termux_step_make() {
+	go build -o seanime -trimpath -ldflags="-s -w"
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/seanime
+}
