7297ac6297fa351247575f011a68c9200f913419
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-14 16:37:45 +0200

    scripts(setup-ubuntu.sh): Add [arch=amd64] to the llvm repo
    Avoids a warning on `apt update`:
    
    > N: Skipping acquire of configured file 'main/binary-i386/Packages' as repository 'http://apt.llvm.org/noble llvm-toolchain-noble-17 InRelease' doesn't support architecture 'i386'

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -323,7 +323,7 @@ $SUDO dpkg --add-architecture i386
 $SUDO cp $(dirname "$(realpath "$0")")/llvm-snapshot.gpg.key /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 $SUDO chmod a+r /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 {
-	echo "deb http://apt.llvm.org/noble/ llvm-toolchain-noble-17 main"
+	echo "deb [arch=amd64] http://apt.llvm.org/noble/ llvm-toolchain-noble-17 main"
 } | $SUDO tee /etc/apt/sources.list.d/apt-llvm-org.list > /dev/null
 
 $SUDO apt-get -yq update
