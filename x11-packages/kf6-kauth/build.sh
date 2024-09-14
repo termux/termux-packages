0dce226822a20747a03ce7672d0784ece748450a
Biswapriyo Nath <nathbappai@gmail.com>
2024-09-14 06:25:57 +0000

    chore(x11/kf6-kauth): Rebuild after kf6-kcoreaddons update

--- x11-packages/kf6-kauth/build.sh
+++ x11-packages/kf6-kauth/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Framework which lets applications perform actions as a p
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.6.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kauth-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=04fb5e3ff672f31f6c3f2e40a59d9c7447a5251b99dbe5f5ea67f54a4c981f3a
 TERMUX_PKG_AUTO_UPDATE=true
