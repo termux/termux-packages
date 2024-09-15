f8c5ccadfc66b1dfa537420bb7fada76a117fd08
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-15 18:43:15 +0200

    bump(main/util-linux): 2.40.2

--- packages/util-linux/build.sh
+++ packages/util-linux/build.sh
@@ -9,12 +9,11 @@ Documentation/licenses/COPYING.BSD-3-Clause
 Documentation/licenses/COPYING.BSD-4-Clause-UC
 Documentation/licenses/COPYING.ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.39.3"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION="2.40.2"
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/util-linux/v${TERMUX_PKG_VERSION:0:4}/util-linux-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7b6605e48d1a49f43cc4b4cfc59f313d0dd5402fa40b96810bd572e167dfed0f
+TERMUX_PKG_SHA256=d78b37a66f5922d70edf3bdfb01a6b33d34ed3c3cafd6628203b2a2b67c8e8b3
 # libcrypt is required for only newgrp and sulogin, which are not built anyways
-TERMUX_PKG_DEPENDS="libcap-ng, libsmartcols, ncurses, zlib"
+TERMUX_PKG_DEPENDS="libcap-ng, libsmartcols, ncurses, zlib, libandroid-glob"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BREAKS="util-linux-dev"
 TERMUX_PKG_REPLACES="util-linux-dev"
@@ -38,6 +37,7 @@ ac_cv_func_uselocale=no
 --disable-ipcs
 --disable-kill
 --disable-last
+--disable-liblastlog2
 --disable-logger
 --disable-mesg
 --disable-makeinstall-chown
@@ -58,5 +58,7 @@ termux_step_pre_configure() {
 	if [ $TERMUX_ARCH_BITS = 64 ]; then
 		#prlimit() is only available in 64-bit bionic.
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_prlimit=yes"
+	elif [ $TERMUX_ARCH_BITS = 32 ]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-year2038"
 	fi
 }
