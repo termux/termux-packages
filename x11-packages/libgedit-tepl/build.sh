274cc470c6212c6b7b655c9aef471c6aaf21737f
Biswapriyo Nath <nathbappai@gmail.com>
2024-09-16 14:24:15 +0000

    bump(x11/libgedit-tepl): 6.11.0

--- x11-packages/libgedit-tepl/build.sh
+++ x11-packages/libgedit-tepl/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/gedit-technology/libgedit-tepl
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/World/gedit/libgedit-tepl
 TERMUX_PKG_DESCRIPTION="Library that eases the development of GtkSourceView-based text editors and IDEs"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
-TERMUX_PKG_SRCURL=https://github.com/gedit-technology/libgedit-tepl/releases/download/${TERMUX_PKG_VERSION}/libgedit-tepl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=242c4be67719dc4a5361cf91930b1d124091c2fc93c2269628c98d7375b1214d
+TERMUX_PKG_VERSION="6.11.0"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libgedit-tepl/${TERMUX_PKG_VERSION%.*}/libgedit-tepl-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=30814663de8324acbbdce623ead4690ed75a9ff7c90023a04997d67e2c0121aa
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gsettings-desktop-schemas, gtk3, libcairo, libgedit-amtk, libgedit-gfls, libgedit-gtksourceview, libhandy, libicu, pango"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
@@ -12,6 +12,7 @@ TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dgobject_introspection=true
 -Dgtk_doc=false
+-Dtests=false
 "
 
 termux_step_pre_configure() {
@@ -34,7 +35,7 @@ termux_step_pre_configure() {
 termux_step_post_massage() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _GUARD_FILE="lib/libgedit-tepl-6.so.0"
+	local _GUARD_FILE="lib/libgedit-tepl-6.so.1"
 	if [ ! -e "${_GUARD_FILE}" ]; then
 		termux_error_exit "Error: file ${_GUARD_FILE} not found."
 	fi
