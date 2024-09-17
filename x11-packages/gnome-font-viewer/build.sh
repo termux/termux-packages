08cbb74ca29b54088043e00eb8dd73687809dc20
Biswapriyo Nath <nathbappai@gmail.com>
2024-09-17 15:09:32 +0000

    bump(x11/gnome-font-viewer): 47.0
    The removed patch was added in upstream.
    https://gitlab.gnome.org/GNOME/gnome-font-viewer/-/commit/565d795731471c27542bb9ee60820a2d0d15534e
    
    Remove unused gnome-desktop dependency which was removed in upstream.
    https://gitlab.gnome.org/GNOME/gnome-font-viewer/-/commit/8572296b31061fd71f61bee3abaef734f099c9f7

--- x11-packages/gnome-font-viewer/build.sh
+++ x11-packages/gnome-font-viewer/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gnome-font-viewer
 TERMUX_PKG_DESCRIPTION="A font viewer utility for GNOME"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="46.0"
+TERMUX_PKG_VERSION="47.0"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gnome-font-viewer/${TERMUX_PKG_VERSION%.*}/gnome-font-viewer-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=592f401e485d02cc044d487bb5c8e04c961da6856216768a59f1ff98bd2d537c
+TERMUX_PKG_SHA256=b8e5a042e0b241b0c7cae43f74da0d5f88e6423017a91feb86e7617edb4080ed
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="fontconfig, freetype, fribidi, glib, graphene, gtk4, gnome-desktop4, harfbuzz, libadwaita, libcairo, pango"
+TERMUX_PKG_DEPENDS="fontconfig, freetype, fribidi, glib, graphene, gtk4, harfbuzz, libadwaita, libcairo, pango"
 TERMUX_PKG_BUILD_DEPENDS="glib-cross"
 
 termux_step_pre_configure() {
