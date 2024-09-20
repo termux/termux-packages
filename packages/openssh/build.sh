87aa6ce757cba1842aed7f606963566939bf8e9d
Git User <107305601+sylirre@users.noreply.github.com>
2024-09-20 13:03:07 +0300

    enhance(main/openssh): include full configuration files for ssh/sshd, disable DSA host key generation attempt (#21519)
    Implementing proposal from https://github.com/termux/termux-packages/issues/21504
    
    Additionally exclude DSA host key generation in post installation script to prevent error "unknown key type dsa".

--- packages/openssh/build.sh
+++ packages/openssh/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Secure shell for logging into a remote machine"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="9.9p1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/openssh/openssh-portable/archive/refs/tags/V_$(sed 's/\./_/g; s/p/_P/g' <<< $TERMUX_PKG_VERSION).tar.gz
 TERMUX_PKG_SHA256=e8858153f188754d0bbf109477690eba226132879b6840cf08b51afb38151040
 TERMUX_PKG_AUTO_UPDATE=true
@@ -70,8 +71,6 @@ termux_step_post_configure() {
 }
 
 termux_step_post_make_install() {
-	echo -e "PrintMotd yes\nPasswordAuthentication yes\nSubsystem sftp $TERMUX_PREFIX/libexec/sftp-server" > $TERMUX_PREFIX/etc/ssh/sshd_config
-	printf "SendEnv LANG\n" > $TERMUX_PREFIX/etc/ssh/ssh_config
 	install -Dm700 $TERMUX_PKG_BUILDER_DIR/source-ssh-agent.sh $TERMUX_PREFIX/bin/source-ssh-agent
 	install -Dm700 $TERMUX_PKG_BUILDER_DIR/ssh-with-agent.sh $TERMUX_PREFIX/bin/ssha
 	install -Dm700 $TERMUX_PKG_BUILDER_DIR/sftp-with-agent.sh $TERMUX_PREFIX/bin/sftpa
@@ -108,7 +107,7 @@ termux_step_create_debscripts() {
 	echo "chmod 700 \$HOME/.ssh" >> postinst
 	echo "chmod 600 \$HOME/.ssh/authorized_keys" >> postinst
 	echo "" >> postinst
-	echo "for a in rsa dsa ecdsa ed25519; do" >> postinst
+	echo "for a in rsa ecdsa ed25519; do" >> postinst
 	echo "	  KEYFILE=$TERMUX_PREFIX/etc/ssh/ssh_host_\${a}_key" >> postinst
 	echo "	  test ! -f \$KEYFILE && ssh-keygen -N '' -t \$a -f \$KEYFILE" >> postinst
 	echo "done" >> postinst
