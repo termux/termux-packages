8883972650bc9208678c8430e0f26d35734adc99
Biswapriyo Nath <nathbappai@gmail.com>
2024-09-22 06:26:04 +0000

    bump(x11/gspell): 1.14.0

--- x11-packages/gspell/build.sh
+++ x11-packages/gspell/build.sh
@@ -2,22 +2,32 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gspell
 TERMUX_PKG_DESCRIPTION="Spell-checking for GTK applications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.2"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="1.14.0"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gspell/${TERMUX_PKG_VERSION%.*}/gspell-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b4e993bd827e4ceb6a770b1b5e8950fce3be9c8b2b0cbeb22fdf992808dd2139
+TERMUX_PKG_SHA256=64ea1d8e9edc1c25b45a920e80daf67559d1866ffcd7f8432fecfea6d0fe8897
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, enchant, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libicu, pango, zlib"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
+TERMUX_PKG_DEPENDS="enchant, glib, gtk3, libicu, pango"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-introspection=yes
---enable-vala=yes
+-Dgobject_introspection=true
+-Dgtk_doc=false
+-Dinstall_tests=false
+-Dtests=false
+-Dvapi=true
 "
 
 termux_step_pre_configure() {
 	TERMUX_PKG_VERSION=. termux_setup_gir
 
-	export GLIB_MKENUMS=glib-mkenums
-	export GLIB_COMPILE_RESOURCES=glib-compile-resources
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
 }
