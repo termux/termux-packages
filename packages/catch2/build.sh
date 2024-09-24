d630ce2b16fe3a4316017fe1dff91b49bd434d17
Ronald Y <knyipab@gmail.com>
2024-09-25 07:58:37 +0800

    addpkg(main/catch2): 3.13.0
    Co-authored-by: Joshua "TomIO" Kahn <tom@termux.dev>

---
+++ packages/catch2/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/catchorg/catch2"
+TERMUX_PKG_DESCRIPTION="A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
+TERMUX_PKG_LICENSE="BSL-1.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.13.0
+TERMUX_PKG_SRCURL="https://github.com/catchorg/Catch2/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=650795f6501af514f806e78c554729847b98db6935e69076f36bb03ed2e985ef
+TERMUX_PKG_AUTO_UPDATE=true
