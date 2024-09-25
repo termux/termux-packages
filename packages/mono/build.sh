8e9a1d0db94d8eedaf0f0e3bc12517d5f4a307dd
Jia Yuan Lo <jylo06g@gmail.com>
2024-09-25 14:45:55 +0800

    chore(main/mono): disable auto update
    Upstream will be changing soon
    
    %ci:no-build

--- packages/mono/build.sh
+++ packages/mono/build.sh
@@ -3,40 +3,18 @@ TERMUX_PKG_DESCRIPTION="Cross platform, open source .NET framework"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.12.0.199
+TERMUX_PKG_VERSION="6.12.0.199"
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.mono-project.com/sources/mono/mono-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c0850d545353a6ba2238d45f0914490c6a14a0017f151d3905b558f033478ef5
 TERMUX_PKG_DEPENDS="krb5, mono-libs, zlib"
 TERMUX_PKG_HOSTBUILD=true
-TERMUX_PKG_AUTO_UPDATE=true
+# https://github.com/mono/mono/issues/21796
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-btls
 --without-ikvm-native
 "
-
-termux_pkg_auto_update() {
-	local e=0
-	local api_url="https://download.mono-project.com/sources/mono"
-	local api_url_r=$(curl -s "${api_url}/")
-	local r1=$(echo "${api_url_r}" | sed -nE 's/.*mono-(.*).tar.*/\1/p' | sort -V)
-	local latest_version=$(echo "${r1}" | tail -n1)
-
-	[[ -z "${api_url_r}" ]] && e=1
-	[[ -z "${r1}" ]] && e=1
-	[[ -z "${latest_version}" ]] && e=1
-	if [[ "${e}" != 0 ]]; then
-		cat <<- EOL >&2
-		WARN: Auto update failure!
-		api_url_r=${api_url_r}
-		r1=${r1}
-		latest_version=${latest_version}
-		EOL
-		return
-	fi
-
-	termux_pkg_upgrade_version "${latest_version}"
-}
 
 termux_step_post_get_source() {
 	rm -f external/bdwgc/config.status
