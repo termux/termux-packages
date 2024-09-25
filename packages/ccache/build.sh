61ac698bce4b01c2d432ac22561a70c8257fbe01
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-25 19:27:22 +0200

    revbump(main/ccache): After fmt 11 update

--- packages/ccache/build.sh
+++ packages/ccache/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Compiler cache for fast recompilation of C/C++ code"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.10.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ccache/ccache/releases/download/v$TERMUX_PKG_VERSION/ccache-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=c0b85ddfc1a3e77b105ec9ada2d24aad617fa0b447c6a94d55890972810f0f5a
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-spawn, libc++, libhiredis, xxhash, zlib, zstd"
-TERMUX_PKG_BUILD_DEPENDS="fmt"
+TERMUX_PKG_DEPENDS="fmt, libandroid-spawn, libc++, libhiredis, xxhash, zlib, zstd"
 
 #[46/89] Building ASM object src/third_party/blake3/CMakeFiles/blake3.dir/blake3_sse2_x86-64_unix.S.o
 #FAILED: src/third_party/blake3/CMakeFiles/blake3.dir/blake3_sse2_x86-64_unix.S.o
