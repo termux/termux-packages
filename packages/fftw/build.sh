cda99d6ab03de93de378a10aeddf72dcefdaefbd
Ronald Y <knyipab@gmail.com>
2024-09-30 01:14:04 +0800

    fftw: provides FFTW3LibraryDepends.cmake

--- packages/fftw/build.sh
+++ packages/fftw/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for computing the Discrete Fourier Transform (DF
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.3.10
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=http://www.fftw.org/fftw-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467
 TERMUX_PKG_BREAKS="fftw-dev"
@@ -19,9 +19,20 @@ termux_step_post_make_install() {
 	local feature
 	for feature in float long-double; do
 		make clean
+		# cmake: create FFTW3LibraryDepends.cmake by configuring (but not building)
+		local -a _config_cmake=""
+		if [ "${feature}" = "float" ]; then
+			_config_cmake="-DENABLE_FLOAT=ON"
+		elif [ "${feature}" = "long-double" ]; then
+			_config_cmake="-DENABLE_LONG_DOUBLE=ON"
+		fi
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS="$_config_cmake" TERMUX_PKG_FORCE_CMAKE=true termux_step_configure
+		# ./configure
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS="$COMMON_ARGS --enable-$feature"
 		rm -Rf $TERMUX_PKG_TMPDIR/config-scripts
 		termux_step_configure
 		make -j $TERMUX_PKG_MAKE_PROCESSES install
 	done
+	sed -e 's|\(IMPORTED_LOCATION_NONE\).*|\1 "'$TERMUX_PREFIX'/lib/libfftw3.so"|' -i ./FFTW3LibraryDepends.cmake
+	install -vDm 644 ./FFTW3LibraryDepends.cmake -t "$TERMUX_PREFIX/lib/cmake/fftw3/"
 }
