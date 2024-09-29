82b5b7703de6f8aa566d584bde948d6bcfd027d7
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-29 17:56:29 +0200

    bump(main/rtorrent): 0.10.0
    Fixes #21602.

--- packages/rtorrent/build.sh
+++ packages/rtorrent/build.sh
@@ -2,17 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://rakshasa.github.io/rtorrent/
 TERMUX_PKG_DESCRIPTION="Ncurses BitTorrent client based on libTorrent"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.8
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.10.0
 TERMUX_PKG_SRCURL=https://github.com/rakshasa/rtorrent/releases/download/v${TERMUX_PKG_VERSION}/rtorrent-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9edf0304bf142215d3bc85a0771446b6a72d0ad8218efbe184b41e4c9c7542af
+TERMUX_PKG_SHA256=cc65bba7abead24151f10af116eca2342b0c320fdff3cb8d604c0af09215d3aa
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libcurl, libtorrent, libxmlrpc, ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-xmlrpc-c
 "
-
-# error: no member named 'bind2nd' in namespace 'std'
-termux_step_pre_configure() {
-	CXXFLAGS+=" -std=c++11"
-}
