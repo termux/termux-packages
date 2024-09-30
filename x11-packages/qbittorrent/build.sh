7bc3a79d798f3d1a7fc5483ee80fc18b362b0643
TomIO <tomjo00@web.de>
2024-09-30 20:28:54 +0200

    enhance(x11/qbittorrent{,-nox}): suggest `python` used by the search functionality plugins

--- x11-packages/qbittorrent/build.sh
+++ x11-packages/qbittorrent/build.sh
@@ -1,20 +1,21 @@
 TERMUX_PKG_HOMEPAGE=https://www.qbittorrent.org/
-TERMUX_PKG_DESCRIPTION="A Qt based BitTorrent client"
+TERMUX_PKG_DESCRIPTION="A Qt6 based BitTorrent client"
 TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="5.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/qbittorrent/qBittorrent/archive/refs/tags/release-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9a24a6b95e9afac826295b8203685a515b13e77eb98bb8ed21c814916b999f6e
-TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_BUILD_DEPENDS="qt6-qtsvg, qt6-qttools, boost"
 TERMUX_PKG_DEPENDS="libc++, libtorrent-rasterbar, openssl, qt6-qtbase, zlib"
-TERMUX_PKG_BUILD_DEPENDS="qt6-qttools, qt6-qtsvg, boost"
+TERMUX_PKG_RECOMMENDS="python"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE=newest-tag
 TERMUX_PKG_UPDATE_VERSION_REGEXP='\d+\.\d+\.\d+'
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS='
--DSTACKTRACE=OFF
+-DBUILD_SHARED_LIBS=OFF
 -DCMAKE_BUILD_TYPE=Release
--DBUILD_SHARED_LIBS=off
+-DSTACKTRACE=OFF
 '
 
 # based on the secondary `-shared` build in `libncnn`
