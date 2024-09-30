02ccf76327f4c7f537aa43cacede920b8e2d2693
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-09-30 21:49:31 +0200

    enhance(x11/qbittorrent{,-nox}): suggest `python` (#21617)
    used by the search functionality plugins

--- x11-packages/qbittorrent/qbittorrent-nox.subpackage.sh
+++ x11-packages/qbittorrent/qbittorrent-nox.subpackage.sh
@@ -1,4 +1,5 @@
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_INCLUDE='bin/qbittorrent-nox share/man/man1/qbittorrent-nox.1.gz'
-TERMUX_SUBPKG_DESCRIPTION='A Qt based BitTorrent client - headless version'
+TERMUX_SUBPKG_DESCRIPTION='A Qt6 based BitTorrent client - headless version'
 TERMUX_SUBPKG_DEPENDS='libc++, libtorrent-rasterbar, openssl, qt6-qtbase, zlib'
+TERMUX_SUBPKG_RECOMMENDS="python"
