1e099fa94a84451fa6766a5f223fc09fddb5cf7e
Ronald Y <knyipab@gmail.com>
2024-10-01 14:00:46 +0800

    new package: kf6-attica

---
+++ x11-packages/kf6-attica/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
+TERMUX_PKG_DESCRIPTION='Qt library that implements the Open Collaboration Services API'
+TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=6.7.0
+TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/attica-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=daa5c07b474899e16b42710e03d0590936bce0f54800239d633d48caa3be0d7d
+TERMUX_PKG_DEPENDS="qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
