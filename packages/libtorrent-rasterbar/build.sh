5b44207222646968cbc386968661a6f1bbea7487
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-05 00:01:11 +0200

    fix(main/libtorrent-rasterbar): Fix build with python 3.12

--- packages/libtorrent-rasterbar/build.sh
+++ packages/libtorrent-rasterbar/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A feature complete C++ bittorrent implementation focusin
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.10"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/arvidn/libtorrent/releases/download/v${TERMUX_PKG_VERSION}/libtorrent-rasterbar-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fc935b8c1daca5c0a4d304bff59e64e532be16bb877c012aea4bda73d9ca885d
 TERMUX_PKG_AUTO_UPDATE=true
