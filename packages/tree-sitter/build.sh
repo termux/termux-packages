2feeadcc1723982252133fa8d8a0db4dfbd2fdbc
Chongyun Lee <uchkks@protonmail.com>
2024-10-04 22:52:29 +0800

    tree-sitter: fix soversion check

--- packages/tree-sitter/build.sh
+++ packages/tree-sitter/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An incremental parsing system for programming tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.24.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7adb5bb3b3c2c4f4fdc980a9a13df8fbf3526a82b5c37dd9cf2ed29de56a4683
 TERMUX_PKG_BREAKS="libtreesitter"
@@ -13,7 +14,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=0
+	local _SOVERSION=0.24
 
 	# New SO version is the major version of the package
 	if [[ $TERMUX_PKG_VERSION != $_SOVERSION.* ]]; then
