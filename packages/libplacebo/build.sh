180e516c97166ca888dbfdfc1a9a7c80933915ab
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-06 19:35:05 +0200

    revbump(main/{electrum,gst-python,libplacebo,panda3d},root/libpcsclite)
    Revbump some python packages after python 3.12 (#18078).

--- packages/libplacebo/build.sh
+++ packages/libplacebo/build.sh
@@ -3,5 +3,6 @@ TERMUX_PKG_DESCRIPTION="Reusable library for GPU-accelerated video/image renderi
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.338.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/haasn/libplacebo
 TERMUX_PKG_DEPENDS="python"
