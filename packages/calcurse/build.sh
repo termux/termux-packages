de33bc015bc82d387b014a74bb3e31ad868e7bfb
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-06 19:07:53 +0200

    revbump(main/{calcurse,dbus-python,profanity,python-lameenc,weechat})
    Revbump some python packages after python 3.12 (#18078).

--- packages/calcurse/build.sh
+++ packages/calcurse/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="calcurse is a calendar and scheduling application for th
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.8.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://calcurse.org/files/calcurse-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d86bb37014fd69b8d83ccb904ac979c6b8ddf59ee3dbc80f5a274525e4d5830a
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
