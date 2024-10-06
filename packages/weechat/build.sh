2f68b7075b6314b6fc15a84584e983ff6b354fcb
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-06 19:07:53 +0200

    revbump(main/{calcurse,dbus-python,profanity,python-lameenc,weechat})
    Revbump some python packages after python 3.12 (#18078).

--- packages/weechat/build.sh
+++ packages/weechat/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # `weechat-python-plugin` depends on libpython${TERMUX_PYTHON_VERSION}.so.
 # Please revbump and rebuild when bumping TERMUX_PYTHON_VERSION.
 TERMUX_PKG_VERSION="4.4.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.weechat.org/files/src/weechat-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d4df289a9c5bca03a6d4fae006e52037064ef03bad6fbe959c538f3197434dec
 TERMUX_PKG_DEPENDS="libandroid-support, libcurl, libgcrypt, libgnutls, libiconv, ncurses, zlib, zstd"
