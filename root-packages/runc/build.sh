e3cb865aa793a24672f9e8b4ef10a85fd781d990
Tomasz Duda <tomaszduda23@gmai.com>
2024-10-06 22:19:30 +0200

    fix review comments

--- root-packages/runc/build.sh
+++ root-packages/runc/build.sh
@@ -9,48 +9,8 @@ TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="libseccomp-static"
 
 termux_step_make() {
-	cat > fakes.c << EOF
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/system_properties.h>
-
-int __android_log_vprint(int prio, const char* tag, const char* fmt, va_list ap) {
-    char buf[1024];
-    vsnprintf(buf, sizeof(buf), fmt, ap);
-    fprintf(stdout, "%s: %s\n", tag, buf);
-    return 0;
-}
-
-void* dlopen(const char* filename, int flag) {
-    return (void*)1;
-}
-
-void* dlsym(void* handle, const char* symbol) {
-	// https://github.com/golang/go/issues/59942
-	if (strcmp(symbol, "android_get_device_api_level") != 0) {
-		fprintf(stderr, "Error: Unexpected symbol requested: %s\n", symbol);
-		exit(1);
-	}
-	char buff[PROP_VALUE_MAX];
-	int n = __system_property_get("ro.build.version.sdk", buff);
-	if (n <= 0) {
-		fprintf(stderr, "Error: Failed to get device API level\n");
-		exit(1);
-	}
-	int api_level = atoi(buff);
-	if (api_level < 29) {
-		return NULL;
-	}
-	return (void*)1;
-}
-
-int dlclose(void* handle) {
-    return 0;
-}
-EOF
-
-	${CC:-gcc} -c -o fakes.o fakes.c
-	${AR:-ar} rcs liblog.a fakes.o
+	${CC} -c -o fakes.o "$TERMUX_PKG_BUILDER_DIR/fakes.c"
+	${AR} rcs liblog.a fakes.o
 
 	export CGO_LDFLAGS="-L$TERMUX_PKG_BUILDDIR"
 
