663c09396a54ab01243948eaf774e63a1c2e0f16
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-06 23:29:56 +0200

    bump(scripts/setup-meson): Update meson from 1.5.1 to 1.5.2

--- scripts/build/setup/termux_setup_meson.sh
+++ scripts/build/setup/termux_setup_meson.sh
@@ -1,6 +1,6 @@
 termux_setup_meson() {
 	termux_setup_ninja
-	local MESON_VERSION=1.5.1
+	local MESON_VERSION=1.5.2
 	local MESON_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
@@ -16,7 +16,7 @@ termux_setup_meson() {
 		termux_download \
 			"https://github.com/mesonbuild/meson/releases/download/$MESON_VERSION/meson-$MESON_VERSION.tar.gz" \
 			"$MESON_TAR_FILE" \
-			567e533adf255de73a2de35049b99923caf872a455af9ce03e01077e0d384bed
+			f955e09ab0d71ef180ae85df65991d58ed8430323de7d77a37e11c9ea630910b
 		tar xf "$MESON_TAR_FILE" -C "$TERMUX_PKG_TMPDIR"
 		shopt -s nullglob
 		local f
