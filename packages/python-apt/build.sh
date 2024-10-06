3bcdc2a4f3252066115759e7d55f5935af159100
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-06 18:40:40 +0200

    revbump(main/python-{apt,cryptography,libsass,pillow,pynvim})
    Revbump some python packages after python 3.12 (#18078).

--- packages/python-apt/build.sh
+++ packages/python-apt/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python bindings for APT"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.9.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/p/python-apt/python-apt_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5b7bf0b19a798a5ee0b8d027e162282db41b957f6586ffc8b90df827b650feb2
 TERMUX_PKG_DEPENDS="apt, libandroid-support, libc++, python"
--- packages/python-libsass/build.sh
+++ packages/python-libsass/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A straightforward binding of libsass for Python"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.22.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/sass/libsass-python/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f26d466918496fbce0890a3c388c78ee25ef9165a7affc591f846d0f8f1a671e
 TERMUX_PKG_DEPENDS="libsass, python"
