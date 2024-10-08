10b969dfedc19fce1f8cafb6c1ed7ad06e3e9d93
TomIO <tomjo00@web.de>
2024-10-08 18:29:34 +0200

    enhance(main/ncdu{,2}): enable auto updates and set a maintainer

--- packages/ncdu2/build.sh
+++ packages/ncdu2/build.sh
@@ -2,11 +2,28 @@ TERMUX_PKG_HOMEPAGE=https://dev.yorhel.nl/ncdu
 TERMUX_PKG_DESCRIPTION="Disk usage analyzer"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSES/MIT.txt"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="2.6"
 TERMUX_PKG_SRCURL=https://dev.yorhel.nl/download/ncdu-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3f471ebd38b56e60dab80c27e3af1d826666957f02d9e8419b149daaf7ade505
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP='2\.\d+(\.\d+)?'
+
+termux_pkg_auto_update() {
+	local latest_release
+	latest_release="$(git ls-remote --tags https://code.blicky.net/yorhel/ncdu.git \
+	| grep -oP "refs/tags/v\K${TERMUX_PKG_UPDATE_VERSION_REGEXP}$" \
+	| sort -V \
+	| tail -n1)"
+
+	if [[ "${latest_release}" == "${TERMUX_PKG_VERSION}" ]]; then
+		echo "INFO: No update needed. Already at version '${TERMUX_PKG_VERSION}'."
+		return
+	fi
+
+	termux_pkg_upgrade_version "${latest_release}"
+}
 
 termux_step_post_get_source() {
 	# TODO drop all this once figure out how zig can work with bionic libc
