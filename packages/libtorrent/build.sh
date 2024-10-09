c136e47b17c5589cf550d6a85cc46bcd65de2df2
Sylirre <sylirre@termux.dev>
2024-10-10 01:32:57 +0300

    revbump(main/libtorrent): rebuild package to try solving linking error in rtorrent

--- packages/libtorrent/build.sh
+++ packages/libtorrent/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Libtorrent BitTorrent library"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.14.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rakshasa/libtorrent/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0ec8ef7544a551ccbf6fce5c6c535f69cb3ad10e4d5e70e620ecd47fef90a13e
 TERMUX_PKG_AUTO_UPDATE=true
