8fc6de482d823c823a693956be8a05bc3d1275c8
Biswapriyo Nath <nathbappai@gmail.com>
2024-10-11 13:42:32 +0000

    bump(x11/kf6-kcodecs): 6.7.0

--- x11-packages/kf6-kcodecs/build.sh
+++ x11-packages/kf6-kcodecs/build.sh
@@ -2,12 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Method collection to manipulate strings using various encodings (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.6.0"
+TERMUX_PKG_VERSION="6.7.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcodecs-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=d9dc2e3294aa32684990bde01bb88dedb2610a28d7c1d62a178a393fa566123b
+TERMUX_PKG_SHA256=9ab494055e06bdce45568b7c47edb2aa9ecaa9dec0c0fb152367b4321921b9f2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
