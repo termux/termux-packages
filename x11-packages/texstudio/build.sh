7a17735248e3d3d9e919acdf3e5eee96a2d7664f
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-11 23:18:29 +0200

    chore(x11/{poppler-qt,quazip,texstudio,texworks}) Switch to Qt6

--- x11-packages/texstudio/build.sh
+++ x11-packages/texstudio/build.sh
@@ -3,23 +3,12 @@ TERMUX_PKG_DESCRIPTION="A fully featured LaTeX editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.8.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/texstudio-org/texstudio/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=aec719cb21b788a41576375eeeba734600a54ff64130dd93f5d0da1efa37414c
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="hunspell, libc++, libx11, poppler-qt, qt5-qtbase, qt5-qtdeclarative, qt5-qtsvg, qt5-qttools, quazip, texstudio-data, zlib"
-TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qtdeclarative-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_DEPENDS="hunspell, libc++, libx11, poppler-qt, qt6-qtbase, qt6-qtdeclarative, qt6-qtsvg, qt6-qttools, quazip, texstudio-data, zlib"
+TERMUX_PKG_BUILD_DEPENDS="qt6-qtbase-cross-tools, qt6-qtdeclarative-cross-tools, qt6-qttools-cross-tools"
 TERMUX_PKG_RECOMMENDS="ghostscript"
 TERMUX_PKG_SUGGESTS="texlive-installer"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-PKG_CONFIG=pkg-config
-PREFIX=$TERMUX_PREFIX
-USE_SYSTEM_HUNSPELL=1
-USE_SYSTEM_QUAZIP=1
-"
-
-termux_step_configure() {
-	"${TERMUX_PREFIX}/opt/qt/cross/bin/qmake" \
-		-spec "${TERMUX_PREFIX}/lib/qt/mkspecs/termux-cross" \
-		${TERMUX_PKG_EXTRA_CONFIGURE_ARGS}
-}
--- x11-packages/texworks/build.sh
+++ x11-packages/texworks/build.sh
@@ -4,13 +4,14 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.6.9"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/TeXworks/texworks/archive/refs/tags/release-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a0c8e4b9f2fbb38f681b9d91f331366e5cdcb35dba7a94cb2988ccfca113ac2b
-TERMUX_PKG_DEPENDS="hunspell, libc++, liblua53, poppler-qt, qt5-qtbase, qt5-qtdeclarative, qt5-qtscript, zlib"
-TERMUX_PKG_BUILD_DEPENDS="git, qt5-qtbase-cross-tools, qt5-qtdeclarative-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_DEPENDS="hunspell, libc++, liblua53, poppler-qt, qt6-qtbase, qt6-qtdeclarative, qt6-qt5compat, zlib"
+TERMUX_PKG_BUILD_DEPENDS="git, qt6-qtbase-cross-tools, qt6-qtdeclarative-cross-tools, qt6-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLUA_MATH_LIBRARY=
+-DQT_DEFAULT_MAJOR_VERSION=6
 "
