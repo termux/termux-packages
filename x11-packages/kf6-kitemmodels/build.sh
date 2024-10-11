952557e1c138babdd8812c7c62be0e767c159cb7
Biswapriyo Nath <nathbappai@gmail.com>
2024-10-11 14:00:59 +0000

    bump(x11/kf6-kitemmodels): 6.7.0

--- x11-packages/kf6-kitemmodels/build.sh
+++ x11-packages/kf6-kitemmodels/build.sh
@@ -2,12 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Set of item models extending the Qt model-view framework (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.6.0"
+TERMUX_PKG_VERSION="6.7.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kitemmodels-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=fe0677bd6eaf2bccd350e7b80eebac382908fc2388af6716171ad77417ee7802
+TERMUX_PKG_SHA256=0aa0205323baf2d519ebb8f96ce7ed5524a8164a7440d905f977418bfbe6d349
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
