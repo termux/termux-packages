055b0753cd65646086d78681dd8dc0d57ebcc461
Biswapriyo Nath <nathbappai@gmail.com>
2024-10-11 13:37:37 +0000

    bump(x11/kf6-kauth): 6.7.0

--- x11-packages/kf6-kauth/build.sh
+++ x11-packages/kf6-kauth/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Framework which lets applications perform actions as a privileged user (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.6.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="6.7.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kauth-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=04fb5e3ff672f31f6c3f2e40a59d9c7447a5251b99dbe5f5ea67f54a4c981f3a
+TERMUX_PKG_SHA256=173654eee2891acd41538d31ace8b9d7ae60863bc7faef1cacec7e21c7eb1223
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, kf6-kcoreaddons"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION})"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
