fa28f12e1c2a935c80b777b06dad2b328a4242be
TomIO <43716232+TomJo2000@users.noreply.github.com>
2024-10-13 06:44:11 +0200

    enhance(x11/thunderbird): enable updates to latest ESR release (#21792)
    Also cleanup Shellcheck's complaints.
    %ci:no-build

--- x11-packages/thunderbird/build.sh
+++ x11-packages/thunderbird/build.sh
@@ -3,11 +3,39 @@ TERMUX_PKG_DESCRIPTION="Unofficial Thunderbird email client"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="128.3.0"
-TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}esr/source/thunderbird-${TERMUX_PKG_VERSION}esr.source.tar.xz
+TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}esr/source/thunderbird-${TERMUX_PKG_VERSION}esr.source.tar.xz"
 TERMUX_PKG_SHA256=160d961930713e8797d8f9f35417fcdd954fd5e19f50d437bf8ae85dd4a13c2f
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libotr, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="binutils-cross, libcpufeatures, libice, libsm"
 TERMUX_PKG_BUILD_IN_SRC=true
+# Mozilla does not provide a simple way to find the latest ESR release
+TERMUX_PKG_UPDATE_VERSION_REGEXP='\d+\.\d+\.\d+(?=esr/)'
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_pkg_auto_update() {
+	# Adapted from the auto_update function in x11/firefox and packages/ncdu.
+	# Unfortunately there is no 'latest-esr/' directory for Thunderbird.
+	# https://archive.mozilla.org/pub/thunderbird/releases/latest/README.txt
+	local api_url latest_esr
+	api_url="https://archive.mozilla.org/pub/thunderbird/releases/"
+	latest_esr="$(curl -s "$api_url" \
+		| grep -oP "$TERMUX_PKG_UPDATE_VERSION_REGEXP" \
+		| sort -V \
+		| tail -n1)"
+
+	if [[ "${latest_esr}" == "${TERMUX_PKG_VERSION}" ]]; then
+		echo "INFO: No update needed. Already at version '${latest_esr}'."
+		return
+	fi
+
+	# We want to avoid re-filtering the version.
+	# It's already cleaned up, so unset the regexp.
+	# See: https://github.com/termux/termux-packages/issues/20836
+	# See also: packages/taplo/build.sh
+	unset TERMUX_PKG_UPDATE_VERSION_REGEXP
+	termux_pkg_upgrade_version "${latest_esr}"
+}
+
 
 termux_step_post_get_source() {
 	local f="media/ffvpx/config_unix_aarch64.h"
@@ -18,8 +46,9 @@ termux_step_post_get_source() {
 termux_step_pre_configure() {
 	# XXX: flang toolchain provides libclang.so
 	termux_setup_flang
-	local __fc_dir="$(dirname $(command -v $FC))"
-	local __flang_toolchain_folder="$(realpath "$__fc_dir"/..)"
+	local __fc_dir __flang_toolchain_folder
+	__fc_dir="$(dirname "$(command -v "$FC")")"
+	__flang_toolchain_folder="$(realpath "$__fc_dir"/..)"
 	if [ ! -d "$TERMUX_PKG_TMPDIR/thunderbird-toolchain" ]; then
 		rm -rf "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp
 		mv "$__flang_toolchain_folder" "$TERMUX_PKG_TMPDIR"/thunderbird-toolchain-tmp
@@ -44,13 +73,14 @@ termux_step_pre_configure() {
 
 	cargo install cbindgen
 
-	export HOST_CC=$(command -v clang)
-	export HOST_CXX=$(command -v clang++)
+	HOST_CC="$(command -v clang)"
+	HOST_CXX="$(command -v clang++)"
+	export HOST_CC HOST_CXX
 
 	export BINDGEN_CFLAGS="--target=$CCTERMUX_HOST_PLATFORM --sysroot=$TERMUX_PKG_TMPDIR/thunderbird-toolchain/sysroot"
 	local env_name=BINDGEN_EXTRA_CLANG_ARGS_${CARGO_TARGET_NAME@U}
 	env_name=${env_name//-/_}
-	export $env_name="$BINDGEN_CFLAGS"
+	export "$env_name"="$BINDGEN_CFLAGS"
 
 	# https://reviews.llvm.org/D141184
 	CXXFLAGS+=" -U__ANDROID__ -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC"
@@ -67,7 +97,7 @@ termux_step_configure() {
 		-e "s|@TERMUX_HOST_PLATFORM@|${TERMUX_HOST_PLATFORM}|" \
 		-e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|" \
 		-e "s|@CARGO_TARGET_NAME@|${CARGO_TARGET_NAME}|" \
-		$TERMUX_PKG_BUILDER_DIR/mozconfig.cfg > .mozconfig
+		"$TERMUX_PKG_BUILDER_DIR/mozconfig.cfg" > .mozconfig
 
 	if [ "$TERMUX_DEBUG_BUILD" = true ]; then
 		cat >>.mozconfig - <<END
@@ -95,8 +125,9 @@ termux_step_post_make_install() {
 	# https://github.com/termux/termux-packages/issues/21511
 	# https://phabricator.services.mozilla.com/D181687
 	# Android 8.x and older not support "-z pack-relative-relocs" / DT_RELR
-	local r=$("${READELF}" -d "${TERMUX_PREFIX}/bin/thunderbird")
-	if [[ -n "$(echo "${r}" | grep "(RELR)")" ]]; then
+	local r
+	r=$("${READELF}" -d "${TERMUX_PREFIX}/bin/thunderbird")
+	if grep -q "(RELR)" "${r}"; then
 		termux_error_exit "DT_RELR is unsupported on Android 8.x and older\n${r}"
 	fi
 }
