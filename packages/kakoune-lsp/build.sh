7fb42236976ea5b17e62e160289a978330766eef
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-14 16:44:43 +0200

    chore(main/kakoune-lsp): Rename from kak-lsp

--- packages/kakoune-lsp/build.sh
+++ packages/kakoune-lsp/build.sh
@@ -1,10 +1,12 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/kak-lsp/kak-lsp
+TERMUX_PKG_HOMEPAGE=https://github.com/kakoune-lsp/kakoune-lsp
 TERMUX_PKG_DESCRIPTION="Language Server Protocol Client for the Kakoune editor"
 TERMUX_PKG_LICENSE="MIT, Unlicense"
 TERMUX_PKG_MAINTAINER="@finagolfin"
 TERMUX_PKG_VERSION="18.0.1"
-TERMUX_PKG_SRCURL=https://github.com/kak-lsp/kak-lsp/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/kakoune-lsp/kakoune-lsp/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=509726ffc214e0a7e62362a53ce2353413ea81bd3c04fdb55c9978f637d64184
+TERMUX_PKG_CONFLICTS="kak-lsp"
+TERMUX_PKG_REPLACES="kak-lsp"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
