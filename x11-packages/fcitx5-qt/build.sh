bc91c7c7fd8aec4a24d498826cf771e26fe7570a
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-15 15:07:36 +0200

    chore(x11/fcitx5-{configtool,qt}): Switch to Qt6

--- x11-packages/fcitx5-qt/build.sh
+++ x11-packages/fcitx5-qt/build.sh
@@ -3,14 +3,22 @@ TERMUX_PKG_DESCRIPTION="Fcitx Qt immodule & library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.1.7"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fcitx/fcitx5-qt/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=586e35e70e656ce387d7c87b58af1294599bd10385f983580235a2efeca666ee
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_DEPENDS="fcitx5, libc++, libx11, libxcb, libxkbcommon, qt5-qtbase"
+TERMUX_PKG_DEPENDS="fcitx5, libc++, libx11, libxcb, libxkbcommon, qt6-qtbase"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_TEST=OFF
 -DENABLE_QT4=OFF
--DENABLE_QT5=ON
--DENABLE_QT6=OFF
+-DENABLE_QT5=OFF
+-DENABLE_QT6=ON
+-DENABLE_QT6_WAYLAND_WORKAROUND=OFF
 "
+
+termux_step_post_make_install() {
+	local _QT6_PLATFORM_INPUT_CONTEXTS_DIR=$TERMUX_PREFIX/lib/qt6/plugins/platforminputcontexts
+	mkdir -p "$_QT6_PLATFORM_INPUT_CONTEXTS_DIR"
+	mv $TERMUX_PREFIX/opt/qt6/cross/lib/qt6/plugins/platforminputcontexts/libfcitx5platforminputcontextplugin.so $_QT6_PLATFORM_INPUT_CONTEXTS_DIR
+}
