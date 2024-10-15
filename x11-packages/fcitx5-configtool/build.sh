fd8304e91c9f53f417571377c146a388c1ab8806
Fredrik Fornwall <fredrik@fornwall.net>
2024-10-15 15:07:36 +0200

    chore(x11/fcitx5-{configtool,qt}): Switch to Qt6

--- x11-packages/fcitx5-configtool/build.sh
+++ x11-packages/fcitx5-configtool/build.sh
@@ -3,14 +3,15 @@ TERMUX_PKG_DESCRIPTION="Configuration tool for Fcitx5"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.1.7"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fcitx/fcitx5-configtool/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c134b082ea9cab0102b9427f39a3714f4a71146ac082e4e6c7c18e4b8dd2aaa7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_DEPENDS="fcitx5, fcitx5-qt, iso-codes, kitemviews, kwidgetsaddons, libc++, libx11, libxkbfile, qt5-qtbase, qt5-qtsvg, qt5-qtx11extras, xkeyboard-config"
+TERMUX_PKG_DEPENDS="fcitx5, fcitx5-qt, iso-codes, kf6-kitemviews, kf6-kwidgetsaddons, libc++, libx11, libxkbfile, qt6-qtbase, qt6-qtsvg, xkeyboard-config"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_TEST=OFF
 -DENABLE_CONFIG_QT=ON
 -DENABLE_KCM=OFF
--DUSE_QT6=OFF
+-DUSE_QT6=ON
 "
