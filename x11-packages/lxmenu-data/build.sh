cdad4fe979d70524d44030c5d8b478b3d050716a
Biswapriyo Nath <nathbappai@gmail.com>
2024-10-16 09:30:01 +0000

    bump(x11/lxmenu-data): 0.1.6
    Change upstream source from Sourceforge to GitHub.

--- x11-packages/lxmenu-data/build.sh
+++ x11-packages/lxmenu-data/build.sh
@@ -1,9 +1,13 @@
-TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/lxde/
+TERMUX_PKG_HOMEPAGE=https://github.com/lxde/lxmenu-data
 TERMUX_PKG_DESCRIPTION="Freedesktop.org desktop menus for LXDE"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.1.5
-TERMUX_PKG_REVISION=26
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/lxde/lxmenu-data-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=9fe3218d2ef50b91190162f4f923d6524c364849f87bcda8b4ed8eb59b80bab8
+TERMUX_PKG_VERSION="0.1.6"
+TERMUX_PKG_SRCURL=https://github.com/lxde/lxmenu-data/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b237e732609fb2a521a942e08bb825ac7973ee478db7567d3606665a3793b2e8
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	./autogen.sh
+}
