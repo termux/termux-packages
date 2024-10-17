25a23d9bec7cc93b41b18224f99f0ce618f10454
owokitty <misternumberone@live.com>
2024-10-17 08:25:21 -0500

    fix(main/simulavr): fix prefix pollution

--- packages/simulavr/build.sh
+++ packages/simulavr/build.sh
@@ -6,7 +6,7 @@ _VERSION_MAJOR=1
 _VERSION_MINOR=1
 _VERSION_PATCH=0
 TERMUX_PKG_VERSION=1:${_VERSION_MAJOR}.${_VERSION_MINOR}.${_VERSION_PATCH}
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="git+https://git.savannah.nongnu.org/git/simulavr"
 TERMUX_PKG_GIT_BRANCH=release-${TERMUX_PKG_VERSION#*:}
 TERMUX_PKG_DEPENDS="libc++"
@@ -28,11 +28,17 @@ termux_step_post_get_source() {
 		| patch --silent -p1
 }
 
-termux_step_post_make_install() {
-	mv "$TERMUX_PREFIX/share/doc/common" "$TERMUX_PREFIX/share/doc/simulavr"
+termux_step_make_install() {
+	install -DTm755 "$TERMUX_PKG_BUILDDIR"/app/"$TERMUX_PKG_NAME" \
+		"$TERMUX_PREFIX"/bin/"$TERMUX_PKG_NAME"
+	install -Dm644 "$TERMUX_PKG_BUILDDIR"/libsim/libsim.so \
+		-t "$TERMUX_PREFIX"/lib/
+	install -Dm644 "$TERMUX_PKG_BUILDDIR"/doc/{copyright,SUPPORT,AUTHORS,README.gdb,NEWS,TODO,README} \
+		-t "$TERMUX_PREFIX"/share/doc/"$TERMUX_PKG_NAME"
+	install -DTm644 "$TERMUX_PKG_BUILDDIR"/doc/COPYING \
+		"$TERMUX_PREFIX"/share/doc/"$TERMUX_PKG_NAME"/LICENSE
 	# Headers are moved into their own subdirectory to prevent conflicts.
 	# Might cause issues when using them.
-	mv "$TERMUX_PREFIX/include" "$TERMUX_PREFIX/include-simulavr"
-	mkdir "$TERMUX_PREFIX/include"
-	mv "$TERMUX_PREFIX/include-simulavr" "$TERMUX_PREFIX/include/simulavr"
+	mkdir -p "$TERMUX_PREFIX"/include
+	cp -r "$TERMUX_PKG_BUILDDIR"/include/* "$TERMUX_PREFIX"/include/"$TERMUX_PKG_NAME"
 }
