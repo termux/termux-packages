ec04985f7189015da4e559e05af4b0d566d13f49
Twaik Yont <twaikyont@gmail.com>
2024-10-17 21:21:47 +0300

    bump(main/cups): 2.4.11

--- packages/cups/build.sh
+++ packages/cups/build.sh
@@ -2,15 +2,18 @@ TERMUX_PKG_HOMEPAGE="https://www.cups.org/"
 TERMUX_PKG_DESCRIPTION="Common UNIX Printing System"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.4.10"
+TERMUX_PKG_VERSION="2.4.11"
 TERMUX_PKG_SRCURL=https://github.com/OpenPrinting/cups/releases/download/v${TERMUX_PKG_VERSION}/cups-${TERMUX_PKG_VERSION}-source.tar.gz
-TERMUX_PKG_SHA256=d75757c2bc0f7a28b02ee4d52ca9e4b1aa1ba2affe16b985854f5336940e5ad7
+TERMUX_PKG_SHA256=9a88fe1da3a29a917c3fc67ce6eb3178399d68e1a548c6d86c70d9b13651fd71
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libcrypt, libgnutls, libiconv, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-tls=gnutls
+"
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+DBUSDIR=$PREFIX/etc/dbus-1
 "
 TERMUX_PKG_CONFFILES="
 etc/cups/cups-files.conf
@@ -19,6 +22,11 @@ etc/cups/snmp.conf
 "
 
 TERMUX_PKG_SERVICE_SCRIPT=("cupsd" "mkdir -p $TERMUX_PREFIX/var/run/cups && exec cupsd -f")
+
+termux_step_pre_configure() {
+	export CHOWNPROG=true CHGRPPROG=true
+}
+
 
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
