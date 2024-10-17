58eefb7f2d800babfd39e7d9f72ef3d6c279f93b
owokitty <misternumberone@live.com>
2024-10-17 08:28:01 -0500

    scripts(build/termux_step_override_config_scripts): do not override any binaries when TERMUX_PACKAGE_LIBRARY is glibc, including bin/sh

--- scripts/build/termux_step_override_config_scripts.sh
+++ scripts/build/termux_step_override_config_scripts.sh
@@ -1,5 +1,5 @@
 termux_step_override_config_scripts() {
-	if [ "$TERMUX_ON_DEVICE_BUILD" = true ]; then
+	if [ "$TERMUX_ON_DEVICE_BUILD" = true ] || [ "$TERMUX_PACKAGE_LIBRARY" = "glibc" ]; then
 		return
 	fi
 
@@ -23,10 +23,6 @@ termux_step_override_config_scripts() {
 	# Make $TERMUX_PREFIX/bin/sh executable on the builder, so that build
 	# scripts can assume that it works on both builder and host later on:
 	ln -sf /bin/sh "$TERMUX_PREFIX/bin/sh"
-
-	if [ "$TERMUX_PACKAGE_LIBRARY" = "glibc" ]; then
-		return
-	fi
 
 	if [ "$TERMUX_PKG_DEPENDS" != "${TERMUX_PKG_DEPENDS/libllvm/}" ] ||
 		[ "$TERMUX_PKG_BUILD_DEPENDS" != "${TERMUX_PKG_BUILD_DEPENDS/libllvm/}" ]; then
