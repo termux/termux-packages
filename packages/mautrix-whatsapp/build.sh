22bce027774d407270d2695a53c4a89d1b01a419
Biswapriyo Nath <nathbappai@gmail.com>
2024-10-17 04:39:21 +0000

    bump(main/mautrix-whatsapp): 0.11.0
    example-config.yaml was removed from upstream repository. The program
    now has a command line option to generate config.yaml file.

--- packages/mautrix-whatsapp/build.sh
+++ packages/mautrix-whatsapp/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://maunium.net/go/mautrix-whatsapp/
 TERMUX_PKG_DESCRIPTION="A Matrix-WhatsApp puppeting bridge"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.10.9"
+TERMUX_PKG_VERSION="0.11.0"
 TERMUX_PKG_SRCURL=https://github.com/mautrix/whatsapp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bf0c79dd572045d485e340a5b0b4dcac1eb27eca5d0d302645db0341da1d3cf8
+TERMUX_PKG_SHA256=79dfe10ede64a54b829c60d049b28d7c284a35e73058c3c375df0b474581315d
 TERMUX_PKG_DEPENDS="libolm"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -17,10 +17,11 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	go build -ldflags "-X 'main.BuildTime=$(date '+%b %_d %Y, %H:%M:%S')'"
+	go build \
+	-ldflags "-X main.Tag=$TERMUX_PKG_VERSION -X 'main.BuildTime=$(date -d @"$SOURCE_DATE_EPOCH" '+%b %_d %Y, %H:%M:%S')'" \
+	./cmd/mautrix-whatsapp
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin mautrix-whatsapp
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME example-config.yaml
+	install -Dm700 -t "$TERMUX_PREFIX"/bin mautrix-whatsapp
 }
