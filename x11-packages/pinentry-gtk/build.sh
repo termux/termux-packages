36a8dbeb575de0e3614e4a7e0c0bdc3ffc07f012
Twaik Yont <twaikyont@gmail.com>
2024-10-17 12:37:42 +0300

    bump(main/libassuan): 3.0.1, bump(x11/pinentry-gtk): 1.3.1, revbump(revdeps)

--- x11-packages/pinentry-gtk/build.sh
+++ x11-packages/pinentry-gtk/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnupg.org/related_software/pinentry/index.html
 TERMUX_PKG_DESCRIPTION="Dialog allowing secure password entry (with gtk2 support)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.2.1
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION="1.3.1"
 TERMUX_PKG_SRCURL=https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=457a185e5a85238fb945a955dc6352ab962dc8b48720b62fc9fa48c7540a4067
+TERMUX_PKG_SHA256=bc72ee27c7239007ab1896c3c2fae53b076e2c9bd2483dc2769a16902bce8c04
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gtk2, libandroid-support, libassuan, libgpg-error, libiconv, ncurses"
 
 TERMUX_PKG_CONFLICTS="pinentry"
