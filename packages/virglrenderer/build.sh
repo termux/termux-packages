a1f6c484fe33426b9994b81224aa550101441847
Twaik Yont <twaikyont@gmail.com>
2024-10-20 23:52:27 +0300

    mvpkg(x11/{angle-android/libepoxy,virglrenderer{,-android}}): move to main repo

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8ed35fdfdffd75f8f06680b2599823b9d905df2f5be835c83e723a574c6a755d
 TERMUX_PKG_AUTO_UPDATE=true
