f54653e26c12649d7d9828168925b3d991942e42
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-21 22:58:45 +0800

    scripts(toolchain): update to ndk r27c

--- build-package.sh
+++ build-package.sh
@@ -241,8 +241,8 @@ source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_handle_host_build.sh"
 source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_host_build.sh"
 
 # Setup a standalone Android NDK toolchain. Called from termux_step_setup_toolchain.
-# shellcheck source=scripts/build/toolchain/termux_setup_toolchain_27b.sh
-source "$TERMUX_SCRIPTDIR/scripts/build/toolchain/termux_setup_toolchain_27b.sh"
+# shellcheck source=scripts/build/toolchain/termux_setup_toolchain_27c.sh
+source "$TERMUX_SCRIPTDIR/scripts/build/toolchain/termux_setup_toolchain_27c.sh"
 
 # Setup a standalone Android NDK 23c toolchain. Called from termux_step_setup_toolchain.
 # shellcheck source=scripts/build/toolchain/termux_setup_toolchain_23c.sh
