ff6d66b479ccac07508557a36c303bcfc36c0555
Twaik Yont <twaikyont@gmail.com>
2024-10-21 05:48:43 +0300

    fix(root/docker-compose): install to docker cli plugins folder instead of $PREFIX/bin

--- root-packages/docker-compose/build.sh
+++ root-packages/docker-compose/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Compose is a tool for defining and running multi-contain
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.29.7"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/docker/compose/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=01b759bc7c301096079dd51c5573751fe7b08ebcc7fde46b40d318539b0d4d8a
 TERMUX_PKG_AUTO_UPDATE=true
@@ -18,5 +19,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm755 -t "${TERMUX_PREFIX}"/bin "${TERMUX_PKG_SRCDIR}"/bin/docker-compose
+	install -Dm755 -t "${TERMUX_PREFIX}"/libexec/docker/cli-plugins "${TERMUX_PKG_SRCDIR}"/bin/docker-compose
 }
