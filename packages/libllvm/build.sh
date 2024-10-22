8f079ec7ebb3dcca9bd750f4cd03a5ff0677d005
Jia Yuan Lo <jylo06g@gmail.com>
2024-10-22 22:49:45 +0800

    fix(main/libllvm): revert non-4k pages ASAN Android

--- packages/libllvm/build.sh
+++ packages/libllvm/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_MAINTAINER="@finagolfin"
 # Keep flang version and revision in sync when updating (enforced by check in termux_step_pre_configure).
 LLVM_MAJOR_VERSION=19
 TERMUX_PKG_VERSION=${LLVM_MAJOR_VERSION}.1.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=324d483ff0b714c8ce7819a1b679dd9e4706cf91c6caf7336dc4ac0c1d3bf636
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/llvm-project-${TERMUX_PKG_VERSION}.src.tar.xz
