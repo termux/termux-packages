16a828ad1cff3c4f00bf2ad950afd37de600dd34
Twaik Yont <twaikyont@gmail.com>
2024-10-24 01:10:33 +0300

    bump(main/lnav): 0.12.2

--- packages/lnav/build.sh
+++ packages/lnav/build.sh
@@ -2,20 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://lnav.org/
 TERMUX_PKG_DESCRIPTION="An advanced log file viewer for the small-scale"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.11.2
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.12.2"
 TERMUX_PKG_SRCURL=https://github.com/tstack/lnav/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=03b72fd02faccdbf98fcdeba62306794b677b8bcf49d6023117808f88ed627dc
+TERMUX_PKG_SHA256=9f12e17f9f2b2f1ddf00ef3f35f61ab3c9709ddf2beaa0eeaf441462e812ca7f
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libandroid-utimes, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-system-paths
 --disable-static
 --with-pcre2=$TERMUX_PREFIX
+ac_cv_path_CARGO_CMD=
 "
 
 termux_step_pre_configure() {
 	autoreconf -fi
 
+	CXXFLAGS+=" -DC4_LINUX"
 	LDFLAGS+=" -landroid-glob -landroid-utimes"
 }
