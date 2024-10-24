4c13bb92b872184c3c4287b04509994a561a1728
Twaik Yont <twaikyont@gmail.com>
2024-10-24 22:46:25 +0300

    bump(main/librsvg): 2.59.0
    Fixes #21465

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.58.3"
+TERMUX_PKG_VERSION="2.59.0"
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION%.*}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=49f29a0a92f4c2d19a2cb41e96ab2fce7eb5bde41850c8a914fcf655e3110944
+TERMUX_PKG_SHA256=370d6ada5cf0de91ceb70d849ed069523ce5de2b33b4c7e86bc640673ad65483
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="fontconfig, freetype, gdk-pixbuf, glib, harfbuzz, libcairo, libpng, libxml2, pango"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
@@ -13,15 +13,22 @@ TERMUX_PKG_REPLACES="librsvg-dev"
 TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-ac_cv_path_GDK_PIXBUF_QUERYLOADERS=$TERMUX_PREFIX/bin/gdk-pixbuf-query-loaders
---disable-gtk-doc
---enable-introspection
---disable-static
+-Ddocs=disabled
+-Dintrospection=enabled
+-Dtests=false
 "
 
 termux_step_pre_configure() {
 	termux_setup_gir
+	termux_setup_meson
 	termux_setup_rust
+	termux_setup_cargo_c
+	
+	# termux_setup_rust unsets CFLAGS so we called termux_setup_meson before
+	# we need to reset termux_setup_meson to avoid `line 70: CFLAGS: unbound variable` error
+	termux_setup_meson() { :; }
+
+	sed -i 's/@BUILD_TRIPLET@/'"$CARGO_TARGET_NAME"'/' "meson.build"
 
 	LDFLAGS+=" -fuse-ld=lld"
 
