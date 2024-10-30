e630d221338e49ef2f89f41ae184dbf4a7195069
Biswapriyo Nath <nathbappai@gmail.com>
2024-10-30 14:04:38 +0000

    bump(main/await): 1.0.7

--- packages/await/build.sh
+++ packages/await/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://await-cli.app/
 TERMUX_PKG_DESCRIPTION="Runs list of commands in parallel and waits for their termination"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.5"
-TERMUX_PKG_SRCURL=https://github.com/slavaGanzin/await/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7f53fdcde929471078da6a0e9257dec17dea7b3d86045bb4038d38592e416ae6
+TERMUX_PKG_VERSION="1.0.7"
+TERMUX_PKG_SRCURL=https://github.com/slavaGanzin/await/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4986978b95c9ef0f1845428500a74c23a411afbba64095b719e3d4b8899d5706
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
