5bf87897785cbfc2287af34a88019655c111d0fb
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-02 02:18:08 +0800

    libicu: bump to 76.1

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -7,9 +7,9 @@ TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 # Never forget to always bump revision of reverse dependencies and rebuild them
 # when bumping "major" version.
-TERMUX_PKG_VERSION="75.1"
+TERMUX_PKG_VERSION="76.1"
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
-TERMUX_PKG_SHA256=cb968df3e4d2e87e8b11c49a5d01c787bd13b9545280fc6642f826527618caef
+TERMUX_PKG_SHA256=dfacb46bfe4747410472ce3e1144bf28a102feeaa4e3875bac9b4c6cf30f4f3e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_DEPENDS="libc++"
@@ -27,7 +27,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_post_massage() {
-	local _GUARD_FILE="lib/libicuuc.so.75"
+	local _GUARD_FILE="lib/libicuuc.so.76"
 	if [ ! -e "${_GUARD_FILE}" ]; then
 		termux_error_exit "Error: file ${_GUARD_FILE} not found."
 	fi
