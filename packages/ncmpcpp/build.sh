7b424992ec43f3a33aa3dc3207b443c58c6cb588
Twaik Yont <twaikyont@gmail.com>
2024-11-03 17:38:14 +0200

    bump(main/ncmpcpp): 0.10.1 (#22093)

--- packages/ncmpcpp/build.sh
+++ packages/ncmpcpp/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://rybczak.net/ncmpcpp/
 TERMUX_PKG_DESCRIPTION="NCurses Music Player Client (Plus Plus)"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.2
-TERMUX_PKG_REVISION=13
-TERMUX_PKG_SRCURL=https://rybczak.net/ncmpcpp/stable/ncmpcpp-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=faabf6157c8cb1b24a059af276e162fa9f9a3b9cd3810c43b9128860c9383a1b
+TERMUX_PKG_VERSION="0.10.1"
+TERMUX_PKG_SRCURL=https://github.com/ncmpcpp/ncmpcpp/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=ddc89da86595d272282ae8726cc7913867b9517eec6e765e66e6da860b58e2f9
 TERMUX_PKG_DEPENDS="boost, fftw, libandroid-support, libc++, libcurl, libicu, libmpdclient, ncurses, readline, taglib"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -17,6 +16,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	./autogen.sh
+	autoreconf -fi
 	CXXFLAGS+=" -DNCURSES_WIDECHAR -U_XOPEN_SOURCE"
 }
