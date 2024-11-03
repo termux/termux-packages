c8566e53a6bc61d5ef91bae62c3e368430f129f0
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-03 18:27:52 +0800

    fix(main/{lit,luvit}): don't embed luvi into {lit,luvit} bin
    reason: to reduce the time for rebuilding lit and luvit after luvi package has been changed (i.e bumped or fixed)

--- packages/luvit/build.sh
+++ packages/luvit/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Asynchronous I/O for Lua"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.18.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/luvit/luvit/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b792781d77028edb7e5761e96618c96162bd68747b8fced9a6fc52f123837c2c
-TERMUX_PKG_BUILD_DEPENDS="luvi"
-TERMUX_PKG_SUGGESTS="luvi, lit"
+TERMUX_PKG_DEPENDS="luvi"
+TERMUX_PKG_SUGGESTS="lit"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STRIP=true
 
@@ -19,9 +20,17 @@ termux_step_configure() {
 }
 
 termux_step_make() {
-	./_lit make . ./luvit "${TERMUX_PREFIX}/bin/luvi"
+	touch dummy
+	./_lit make . ./luvit dummy
 }
 
 termux_step_make_install() {
-	install -Dm700 luvit "${TERMUX_PREFIX}/bin/luvit"
+	mkdir -p "${TERMUX_PREFIX}/share/luvit"
+	unzip -d "${TERMUX_PREFIX}/share/luvit" luvit
+
+	cat > "${TERMUX_PREFIX}/bin/luvit" <<-EOF
+	#!${TERMUX_PREFIX}/bin/env bash
+	exec luvi ${TERMUX_PREFIX}/share/luvit -- \$@
+	EOF
+	chmod 700 "${TERMUX_PREFIX}/bin/luvit"
 }
