4a8d460db03de0e8e4b3e22fbdc03e8fb520f52f
Twaik Yont <twaikyont@gmail.com>
2024-11-03 19:35:51 +0200

    bump(x11/the-powder-toy): 98.2.365
    This update adds missing desktop icon and allows users to save their progress in folders other than default one.

--- x11-packages/the-powder-toy/build.sh
+++ x11-packages/the-powder-toy/build.sh
@@ -2,19 +2,28 @@ TERMUX_PKG_HOMEPAGE=https://powdertoy.co.uk/
 TERMUX_PKG_DESCRIPTION="The Powder Toy is a free physics sandbox game"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=97.0.352
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="98.2.365"
 TERMUX_PKG_SRCURL=https://github.com/ThePowderToy/The-Powder-Toy/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3ab27e1b9a84db1da7342e61232ad5be981ca1ddf001c4924fd08b61cc8d287a
-TERMUX_PKG_DEPENDS="fftw, jsoncpp, libbz2, libc++, libcurl, libluajit, libpng, sdl2"
+TERMUX_PKG_SHA256=21900b6b022535d0e56126b023538cc4f44b64feceafb5640492618a42a60080
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="fftw, jsoncpp, libandroid-execinfo, libbz2, libc++, libcurl, libluajit, libpng, sdl2"
 TERMUX_PKG_GROUPS="games"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dworkaround_elusive_bzip2_lib_dir=$TERMUX_PREFIX/lib
 -Dworkaround_elusive_bzip2_include_dir=$TERMUX_PREFIX/include
 -Db_pie=true
+-Dignore_updates=true
+-Dapp_data=$TERMUX_ANDROID_HOME/.powdertoy
+-Dcan_install=no
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-execinfo"
+}
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin powder
 	ln -sf powder $TERMUX_PREFIX/bin/the-powder-toy
+	install -Dm700 -t $TERMUX_PREFIX/share/applications resources/powder.desktop
+	install -Dm700 -T $TERMUX_PKG_SRCDIR/resources/generated_icons/icon_exe.png $TERMUX_PREFIX/share/pixmaps/powdertoy-powder.png
 }
