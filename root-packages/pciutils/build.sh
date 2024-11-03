3f94b7d4a73fe25826461ad4d93e89e07820555b
Twaik Yont <twaikyont@gmail.com>
2024-11-03 13:13:26 +0200

    bump(root/pciutils): 3.13.0

--- root-packages/pciutils/build.sh
+++ root-packages/pciutils/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://mj.ucw.cz/sw/pciutils/
 TERMUX_PKG_DESCRIPTION="a collection of programs for inspecting and manipulating configuration of PCI devices"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.10.0
+TERMUX_PKG_VERSION="3.13.0"
 TERMUX_PKG_SRCURL=https://mj.ucw.cz/download/linux/pci/pciutils-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7deabe38ae5fa88a96a8c4947975cf31c591506db546e9665a10dddbf350ead0
+TERMUX_PKG_SHA256=a03c88a0aab709b1697ca1753766f443e78297f84c7bcd80a82c1422edd7cb42
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -19,9 +20,10 @@ termux_step_pre_configure() {
 
 	local f
 	for f in config.h config.mk; do
-		local in=$TERMUX_PKG_BUILDER_DIR/$TERMUX_PKG_VERSION/${f}.in
+		local in=$TERMUX_PKG_BUILDER_DIR/${f}.in
 		local out=$TERMUX_PKG_SRCDIR/lib/${f}
 		sed \
+			-e 's|@TERMUX_PKG_VERSION@|'"$TERMUX_PKG_VERSION"'|g' \
 			-e 's|@TERMUX_ARCH@|'"${_ARCH}"'|g' \
 			-e 's|@TERMUX_PREFIX@|'"${TERMUX_PREFIX}"'|g' \
 			${in} > ${out}
