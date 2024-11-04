a7875d374c8232cf42845d1e626da65beeb1ff9d
fervi <fervi@Shinzan>
2024-11-04 15:31:20 +0100

    addpkg: Enlightenment 16

---
+++ x11-packages/e16/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.enlightenment.org/e16
+TERMUX_PKG_DESCRIPTION='E16 (Enlightenment) is a lightweight, highly customizable window manager for X11, known for its unique aesthetic, efficiency, and low resource usage, making it ideal for older or limited-resource systems.'
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.0.30
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/e16-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b8b7748a2c48c4c7c9758d9ad12b14f566d2bec38f2eda533e6d874f5ce9074c
