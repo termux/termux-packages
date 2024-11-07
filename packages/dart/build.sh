18d4a0bf1dd0ed4052da4c7157cb6b8eab8b4754
Samujjal Gogoi <169282928+samujjal-gogoi@users.noreply.github.com>
2024-11-07 12:18:09 +0530

    bump(main/dart): 3.5.4

--- packages/dart/build.sh
+++ packages/dart/build.sh
@@ -2,80 +2,77 @@ TERMUX_PKG_HOMEPAGE=https://dart.dev/
 TERMUX_PKG_DESCRIPTION="Dart is a general-purpose programming language"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_LICENSE_FILE="sdk/LICENSE"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.4.4
-TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_MAINTAINER="@samujjal-gogoi"
+TERMUX_PKG_VERSION=3.5.4
+TERMUX_PKG_SRCURL=https://github.com/dart-lang/sdk/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=c6bdf7591d3ba8d353dfaa0b10af58918610e65a1de9f3c9a644e7f3aecab16c
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
 # Dart uses tar and gzip to extract downloaded packages.
 # Busybox-based versions of such utilities cause issues so
 # complete ones should be used.
 TERMUX_PKG_DEPENDS="gzip, tar"
 
+termux_pkg_auto_update() {
+	curl -fLSso VERSION https://storage.googleapis.com/dart-archive/channels/stable/release/latest/VERSION
+	local latest_version=$(jq -r .version VERSION)
+	if [[ ${latest_version} = "null" ]]; then
+		echo "ERROR: Failed to get latest version."
+		exit 1
+	fi
+	if [[ ${latest_version} = ${TERMUX_PKG_VERSION} ]]; then
+		echo "INFO: No update needed. Already at version '${TERMUX_PKG_VERSION}'."
+		return
+	fi
+	termux_pkg_upgrade_version ${latest_version}
+}
+
 termux_step_get_source() {
-	mkdir -p $TERMUX_PKG_SRCDIR
-	cd $TERMUX_PKG_SRCDIR
-
-	git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
-	mkdir -p depot_tools/fakebin
-	ln -sfr /usr/bin/python depot_tools/fakebin/python
-	export PATH="$(pwd)/depot_tools/fakebin:$(pwd)/depot_tools:${PATH}"
-
-	fetch dart
-
+	mkdir -p ${TERMUX_PKG_SRCDIR}
+	cd ${TERMUX_PKG_SRCDIR}
+	git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+	export PATH="${PWD}/depot_tools:${PATH}"
+	fetch --no-history --no-hooks dart
 	cd sdk
-	git checkout $TERMUX_PKG_VERSION
-	cd ../
-
-	echo "target_os = ['android']" >> .gclient
-	gclient sync -D --force --reset
-}
-
-termux_step_pre_configure() {
-	sed -i -e 's:\([^A-Za-z0-9_]\)/usr/bin:\1'$TERMUX_PREFIX'/local/bin:g' \
-		-e 's:\([^A-Za-z0-9_]\)/bin:\1'$TERMUX_PREFIX'/bin:g' \
-		"$TERMUX_PKG_SRCDIR/sdk/third_party/pkg/pub/lib/src/io.dart"
-}
-
-termux_step_make() {
-	:
+	git fetch --depth 1 origin tag ${TERMUX_PKG_VERSION}
+	git checkout ${TERMUX_PKG_VERSION}
+	echo 'target_os = ["android"]' >> ../.gclient
+	gclient sync -DRf
 }
 
 termux_step_make_install() {
 	cd sdk
-
-	rm -f ./out/*/args.gn
-
-	if [ $TERMUX_ARCH = "arm" ]; then
-		python3 ./tools/build.py --no-goma --mode release --arch=arm --os=android create_sdk
-		chmod +x ./out/ReleaseAndroidARM/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidARM/dart-sdk ${TERMUX_PREFIX}/lib
-	elif [ $TERMUX_ARCH = "i686" ]; then
-		python3 ./tools/build.py --no-goma --mode release --arch=ia32 --os=android create_sdk
-		chmod +x ./out/ReleaseAndroidIA32/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidIA32/dart-sdk ${TERMUX_PREFIX}/lib
-	elif [ $TERMUX_ARCH = "aarch64" ]; then
-		python3 ./tools/build.py --no-goma --mode release --arch=arm64c --os=android create_sdk
-		chmod +x ./out/ReleaseAndroidARM64C/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidARM64C/dart-sdk ${TERMUX_PREFIX}/lib
-	elif [ $TERMUX_ARCH = "x86_64" ]; then
-		python3 ./tools/build.py --no-goma --mode release --arch=x64c --os=android create_sdk
-		chmod +x ./out/ReleaseAndroidX64C/dart-sdk/bin/*
-		cp -r ./out/ReleaseAndroidX64C/dart-sdk ${TERMUX_PREFIX}/lib
-	else
-		termux_error_exit "Unsupported arch '$TERMUX_ARCH'"
-	fi
-
+	case "$TERMUX_ARCH" in
+		arm)
+			./tools/build.py --no-rbe -m release -a arm --os android create_sdk
+			mv ./out/ReleaseAndroidARM/dart-sdk "${TERMUX_PREFIX}/lib"
+		;;
+		i686)
+			./tools/build.py --no-rbe -m release -a ia32 --os android create_sdk
+			mv ./out/ReleaseAndroidIA32/dart-sdk "${TERMUX_PREFIX}/lib"
+		;;
+		aarch64)
+			./tools/build.py --no-rbe -m release -a arm64c --os android create_sdk
+			mv ./out/ReleaseAndroidARM64C/dart-sdk "${TERMUX_PREFIX}/lib"
+		;;
+		x86_64)
+			./tools/build.py --no-rbe -m release -a x64c --os android create_sdk
+			mv ./out/ReleaseAndroidX64C/dart-sdk "${TERMUX_PREFIX}/lib"
+		;;
+		*)
+			termux_error_exit "Unsupported arch '${TERMUX_ARCH}'"
+		;;
+	esac
 	for file in ${TERMUX_PREFIX}/lib/dart-sdk/bin/*; do
-		if [[ -f "$file" ]]; then
-			echo -e "#!${TERMUX_PREFIX}/bin/sh\nexec $file  \"\$@\"" > ${TERMUX_PREFIX}/bin/$(basename $file)
-			chmod +x ${TERMUX_PREFIX}/bin/$(basename $file)
+		if [[ -f ${file} && -x ${file} ]]; then
+			echo -e "#!${TERMUX_PREFIX}/bin/sh\nexec ${file} \"\$@\"" > "${TERMUX_PREFIX}/bin/$(basename "${file}")"
+			chmod +x "${TERMUX_PREFIX}/bin/$(basename "${file}")"
 		fi
 	done
 }
 
 termux_step_post_make_install() {
-	install -Dm600 $TERMUX_PKG_BUILDER_DIR/dart-pub-bin.sh \
-		$TERMUX_PREFIX/etc/profile.d/dart-pub-bin.sh
+	install -Dm 600 ${TERMUX_PKG_BUILDER_DIR}/dart-pub-bin.sh ${TERMUX_PREFIX}/etc/profile.d/dart-pub-bin.sh
 }
