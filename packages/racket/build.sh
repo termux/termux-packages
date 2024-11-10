7563b757130ceec919d993948fda69028cad51cf
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-11-10 13:13:42 +0800

    racket: avoid `AI_V4MAPPED` on Android (#22206)

--- packages/racket/build.sh
+++ packages/racket/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Full-spectrum programming language going beyond Lisp and
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="8.15"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.cs.utah.edu/plt/installers/${TERMUX_PKG_VERSION}/racket-minimal-${TERMUX_PKG_VERSION}-src-builtpkgs.tgz
 TERMUX_PKG_SHA256=df5bd52b243ca16ef7fc0647ca5404c6a0c4399518f427ac22e9f85a3694ab5d
 TERMUX_PKG_AUTO_UPDATE=true
