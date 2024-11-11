2ad4e335af43a2cbb7329d3e997d73afeb94aa48
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-11 07:28:03 +0000

    bump(x11/sfml): 2.6.2
    Remove workaround with pkgconfig file which was fixed in
    https://github.com/SFML/SFML/commit/5e18e5403b3ef583064723a3f7169a76df6d521a

--- x11-packages/sfml/build.sh
+++ x11-packages/sfml/build.sh
@@ -2,15 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.sfml-dev.org/
 TERMUX_PKG_DESCRIPTION="A simple, fast, cross-platform and object-oriented multimedia API"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.6.1"
+TERMUX_PKG_VERSION="2.6.2"
 TERMUX_PKG_SRCURL=https://github.com/SFML/SFML/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=82535db9e57105d4f3a8aedabd138631defaedc593cab589c924b7d7a11ffb9d
+TERMUX_PKG_SHA256=15ff4d608a018f287c6a885db0a2da86ea389e516d2323629e4d4407a7ce047f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxcursor, libxrandr, openal-soft, opengl"
 
 termux_step_post_get_source() {
 	cp src/SFML/Window/Android/JoystickImpl.{cpp,hpp} src/SFML/Window/Unix/
-
-	find tools/pkg-config -name '*.pc.in' | xargs -n 1 \
-		sed -i -E 's|^(libdir=)\$\{exec_prefix\}/(@CMAKE_INSTALL_LIBDIR@)$|\1\2|'
 }
