496753e342ff2da421bf9ee5cc4cf62cbcc8e3b7
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-15 18:44:05 +0000

    enhance(x11/ghex): Add vapi files for vala
    Also change source url to official one.

--- x11-packages/ghex/build.sh
+++ x11-packages/ghex/build.sh
@@ -3,15 +3,18 @@ TERMUX_PKG_DESCRIPTION="A simple binary editor for the Gnome desktop."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="46.1"
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/ghex/${TERMUX_PKG_VERSION%.*}/ghex-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/ghex/${TERMUX_PKG_VERSION%.*}/ghex-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8a13975474eee27719b29ad763f1b24760a1aede6d7da4b623703070bc26e9fd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gtk4, libadwaita, libcairo, pango"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dgtk_doc=false
 -Dintrospection=enabled
+-Dvapi=true
 "
 
 termux_step_pre_configure() {
