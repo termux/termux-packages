9189fb77dad8860adcdfb0280a933e33fa3de2e1
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-16 05:41:20 +0000

    bump(main/swtpm): 0.10.0
    Remove unnecessary installed tests explicitly.

--- packages/swtpm/build.sh
+++ packages/swtpm/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Software TPM Emulator"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.0"
+TERMUX_PKG_VERSION="0.10.0"
 TERMUX_PKG_SRCURL=https://github.com/stefanberger/swtpm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9679ca171e8aaa3c4e4053e8bc1d10c8dabf0220bd4b16aba78743511c25f731
+TERMUX_PKG_SHA256=9f10ae0d3123ab05c3808f8c8d39f633cf1a0cf142d6ac9b87b8364a682ac842
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, json-glib, libseccomp, libtpms, openssl"
 TERMUX_PKG_BUILD_DEPENDS="libtasn1"
@@ -19,4 +19,8 @@ termux_step_pre_configure() {
 	autoreconf -fi
 
 	CPPFLAGS+=" -Dindex=strchr"
+}
+
+termux_step_post_massage() {
+	rm -r "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}"/libexec/installed-tests
 }
