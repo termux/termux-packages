fa4e1951bb21d028758994822efad43f11f77fc0
Max Ivan <mixython@gmail.com>
2024-11-16 13:14:37 +0300

    x11/polybar: mark file `etc/polybar/config.ini` as CONFFILES (#22284)

--- x11-packages/polybar/build.sh
+++ x11-packages/polybar/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A fast and easy-to-use status bar"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.7.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/${TERMUX_PKG_VERSION}/polybar-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=e2feacbd02e7c94baed7f50b13bcbf307d95df0325c3ecae443289ba5b56af29
 TERMUX_PKG_AUTO_UPDATE=true
@@ -10,6 +11,7 @@ TERMUX_PKG_DEPENDS="fontconfig, freetype, jsoncpp, libandroid-glob, libc++, libc
 TERMUX_PKG_BUILD_DEPENDS="i3"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DENABLE_I3=ON"
+TERMUX_PKG_CONFFILES="etc/polybar/config.ini"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
