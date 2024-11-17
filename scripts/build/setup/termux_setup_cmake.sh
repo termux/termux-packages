3f6e1fbcde67527299e1c6d79c20dd9e402f03a7
Jia Yuan Lo <jylo06g@gmail.com>
2024-11-17 17:14:51 +0800

    scripts(termux_setup_cmake): 3.31.0

--- scripts/build/setup/termux_setup_cmake.sh
+++ scripts/build/setup/termux_setup_cmake.sh
@@ -1,37 +1,26 @@
 termux_setup_cmake() {
-	local TERMUX_CMAKE_MAJORVERSION=3.30
-	local TERMUX_CMAKE_MINORVERSION=4
-	local TERMUX_CMAKE_VERSION=$TERMUX_CMAKE_MAJORVERSION.$TERMUX_CMAKE_MINORVERSION
-	local TERMUX_CMAKE_TARNAME=cmake-${TERMUX_CMAKE_VERSION}-linux-x86_64.tar.gz
-	local TERMUX_CMAKE_TARFILE=$TERMUX_PKG_TMPDIR/$TERMUX_CMAKE_TARNAME
-	local TERMUX_CMAKE_FOLDER
+	local TERMUX_CMAKE_VERSION=3.31.0
+	local TERMUX_CMAKE_SHA256=0fcb338b4515044f9ac77543550ac92c314c58f6f95aafcac5cd36aa75db6924
+	local TERMUX_CMAKE_MAJORVERSION="${TERMUX_CMAKE_VERSION%.*}"
+	local TERMUX_CMAKE_TARNAME="cmake-${TERMUX_CMAKE_VERSION}-linux-x86_64.tar.gz"
+	local TERMUX_CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v${TERMUX_CMAKE_VERSION}/${TERMUX_CMAKE_TARNAME}"
+	local TERMUX_CMAKE_TARFILE="${TERMUX_PKG_TMPDIR}/${TERMUX_CMAKE_TARNAME}"
+	local TERMUX_CMAKE_FOLDER="${TERMUX_COMMON_CACHEDIR}/cmake-${TERMUX_CMAKE_VERSION}"
+	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
+		TERMUX_CMAKE_FOLDER="${TERMUX_SCRIPTDIR}/build-tools/cmake-${TERMUX_CMAKE_VERSION}"
+	fi
+
 	if [ "$TERMUX_PACKAGE_LIBRARY" = "bionic" ]; then
 		local TERMUX_CMAKE_NAME="cmake"
 	elif [ "$TERMUX_PACKAGE_LIBRARY" = "glibc" ]; then
 		local TERMUX_CMAKE_NAME="cmake-glibc"
 	fi
 
-	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
-		TERMUX_CMAKE_FOLDER=$TERMUX_SCRIPTDIR/build-tools/cmake-$TERMUX_CMAKE_VERSION
-	else
-		TERMUX_CMAKE_FOLDER=$TERMUX_COMMON_CACHEDIR/cmake-$TERMUX_CMAKE_VERSION
-	fi
+	export CMAKE_INSTALL_ALWAYS=1
 
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		if [ ! -d "$TERMUX_CMAKE_FOLDER" ]; then
-			termux_download https://github.com/Kitware/CMake/releases/download/v${TERMUX_CMAKE_VERSION}/$TERMUX_CMAKE_TARNAME \
-				"$TERMUX_CMAKE_TARFILE" \
-				c959e6d15714f798424960cd296632634f3ef57c2712559a7945170f0bcad205
-			rm -Rf "$TERMUX_PKG_TMPDIR/cmake-${TERMUX_CMAKE_VERSION}-linux-x86_64"
-			tar xf "$TERMUX_CMAKE_TARFILE" -C "$TERMUX_PKG_TMPDIR"
-			mv "$TERMUX_PKG_TMPDIR/cmake-${TERMUX_CMAKE_VERSION}-linux-x86_64" \
-				"$TERMUX_CMAKE_FOLDER"
-		fi
-
-		export PATH=$TERMUX_CMAKE_FOLDER/bin:$PATH
-	else
+	if [ "${TERMUX_ON_DEVICE_BUILD}" = "true" ]; then
 		if [[ "$TERMUX_APP_PACKAGE_MANAGER" = "apt" && "$(dpkg-query -W -f '${db:Status-Status}\n' $TERMUX_CMAKE_NAME 2>/dev/null)" != "installed" ]] ||
-                   [[ "$TERMUX_APP_PACKAGE_MANAGER" = "pacman" && ! "$(pacman -Q $TERMUX_CMAKE_NAME 2>/dev/null)" ]]; then
+			[[ "$TERMUX_APP_PACKAGE_MANAGER" = "pacman" && ! "$(pacman -Q $TERMUX_CMAKE_NAME 2>/dev/null)" ]]; then
 			echo "Package '$TERMUX_CMAKE_NAME' is not installed."
 			echo "You can install it with"
 			echo
@@ -41,7 +30,18 @@ termux_setup_cmake() {
 			echo
 			exit 1
 		fi
+		return
 	fi
 
-	export CMAKE_INSTALL_ALWAYS=1
+	if [ ! -d "${TERMUX_CMAKE_FOLDER}" ]; then
+		termux_download "${TERMUX_CMAKE_URL}" \
+			"${TERMUX_CMAKE_TARFILE}" \
+			"${TERMUX_CMAKE_SHA256}"
+		rm -Rf "${TERMUX_PKG_TMPDIR}/cmake-${TERMUX_CMAKE_VERSION}-linux-x86_64"
+		tar xf "${TERMUX_CMAKE_TARFILE}" -C "${TERMUX_PKG_TMPDIR}"
+		mv "${TERMUX_PKG_TMPDIR}/cmake-${TERMUX_CMAKE_VERSION}-linux-x86_64" \
+			"${TERMUX_CMAKE_FOLDER}"
+	fi
+
+	export PATH="${TERMUX_CMAKE_FOLDER}/bin:${PATH}"
 }
