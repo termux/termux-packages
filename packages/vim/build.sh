2a151ee819e15fde7f9f81b83c571591704efa4d
TomIO <tomjo00@web.de>
2024-11-18 22:33:15 +0100

    enhance(main/vim{,-python}, x11/vim-gtk): enable auto updates

--- packages/vim/build.sh
+++ packages/vim/build.sh
@@ -40,19 +40,34 @@ TERMUX_PKG_CONFFILES="share/vim/vimrc"
 
 TERMUX_PKG_CONFLICTS="vim-python"
 
+# Vim releases every commit as a new patch release.
+# To avoid auto update spam, we only update Vim every 50th patch.
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP='\d+\.\d+\.\d{2}(5|0)0'
+
+termux_pkg_auto_update() {
+	# This auto_update function is shared by `vim`, `vim-python` and `vim-gtk`
+	# If you make changes to one of them,
+	# remember to apply that change to the other two as well.
+	local release
+	release="$(git ls-remote --tags https://github.com/vim/vim.git \
+	| grep -oP "refs/tags/v\K${TERMUX_PKG_UPDATE_VERSION_REGEXP}$" \
+	| sort -V \
+	| tail -n1)"
+
+	if [[ "${release}" == "${TERMUX_PKG_VERSION}" ]]; then
+		echo "INFO: No update needed. Already at version '${TERMUX_PKG_VERSION}'."
+		return
+	fi
+
+	termux_pkg_upgrade_version "${release}"
+}
+
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
 	if $TERMUX_ON_DEVICE_BUILD; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
-	fi
-
-	# Version guard
-	local ver_v=$(. $TERMUX_SCRIPTDIR/packages/vim/build.sh; echo ${TERMUX_PKG_VERSION#*:})
-	local ver_p=$(. $TERMUX_SCRIPTDIR/packages/vim-python/build.sh; echo ${TERMUX_PKG_VERSION#*:})
-	local ver_g=$(. $TERMUX_SCRIPTDIR/x11-packages/vim-gtk/build.sh; echo ${TERMUX_PKG_VERSION#*:})
-	if [ "${ver_v}" != "${ver_p}" ] || [ "${ver_p}" != "${ver_g}" ]; then
-		termux_error_exit "Version mismatch between vim, vim-python and vim-gtk."
 	fi
 
 	make distclean
