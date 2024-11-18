ed2ba72b34c3894f7ab7234716fd93f2b42b1541
Jia Yuan Lo <jylo06g@gmail.com>
2024-11-18 16:09:26 +0800

    chore(x11/i3): replace deprecated i3-gaps
    i3-gaps is merged into i3 since i3 version 4.22

--- x11-packages/i3/build.sh
+++ x11-packages/i3/build.sh
@@ -3,12 +3,14 @@ TERMUX_PKG_DESCRIPTION="An improved dynamic tiling window manager"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.24"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://i3wm.org/downloads/i3-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5baefd0e5e78f1bafb7ac85deea42bcd3cbfe65f1279aa96f7e49661637ac981
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, libandroid-glob, libandroid-wordexp, libcairo, libev, libiconv, libxcb, libxkbcommon, pango, pcre2, perl, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm, xcb-util-xrm, yajl"
 TERMUX_PKG_RECOMMENDS="i3status"
-
+TERMUX_PKG_BREAKS="i3-gaps (<< 4.21.1)"
+TERMUX_PKG_REPLACES="i3-gaps (<< 4.21.1)"
 TERMUX_PKG_CONFFILES="
 i3/config
 i3/config.keycodes
