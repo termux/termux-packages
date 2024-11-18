3c1c33b4a2068c0b251a559c6dc77756321d973d
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-18 04:52:26 +0000

    chore(main/imagemagick): Fix libtool warnings about linking with static library
    This replaces previous workaround in f041d6dc5179a4a2bd4006230a56da463edb5b51
    commit because it produces the following warning.
    
    Warning: Linking the shared library MagickCore/libMagickCore-7.Q16HDRI.la against the
    static library NDK/lib/clang/18/lib/linux/libclang_rt.builtins-aarch64-android.a is not portable!
    
    The warning is explained here https://stackoverflow.com/a/8352756

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -31,7 +31,11 @@ share/ImageMagick-7/francais.xml
 "
 
 termux_step_pre_configure() {
-	export LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	# ERROR: ./lib/libMagick++-7.Q16HDRI.so contains undefined symbols: __aeabi_idiv (arm)
+	local _libgcc_file="$($CC -print-libgcc-file-name)"
+	local _libgcc_path="$(dirname $_libgcc_file)"
+	local _libgcc_name="$(basename $_libgcc_file)"
+	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
 
 	# Value of PKG_CONFIG becomes hardcoded in bin/*-config
 	export PKG_CONFIG=pkg-config
