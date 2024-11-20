ec2e0ff830f342fccfd39de20acbc9c3831422e3
TomIO <tomjo00@web.de>
2024-11-20 19:32:03 +0100

    chore(main/git): move `git-citool` to `git-gui` subpackage

--- packages/git/build.sh
+++ packages/git/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast, scalable, distributed revision control system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.47.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/pub/software/scm/git/git-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=1ce114da88704271b43e027c51e04d9399f8c88e9ef7542dae7aebae7d87bc4e
 TERMUX_PKG_AUTO_UPDATE=true
