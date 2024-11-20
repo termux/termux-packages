a2534531075e21a7cf4438b37df860df79a0c7af
Komo <afiqquraisyzulkarnain@gmail.com>
2024-11-20 21:30:35 +0800

    fix,enhance,bump(main/luvi): bump to fixed version 2.15.0
    Bump to fixed version, use shared lpeg, enables zlib, and fix luajit version symbol.

--- packages/luvi/build.sh
+++ packages/luvi/build.sh
@@ -1,15 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://luvit.io
-TERMUX_PKG_DESCRIPTION="A project in-between luv and luvit."
+TERMUX_PKG_DESCRIPTION="A project in-between luv and luvit"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Komo @mbekkomo"
-TERMUX_PKG_VERSION=20241009
-_commit=32b274bed2a291068eda6cf7a296328b86d682a9
-_version=2.14.0+g${_commit::7}
-TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_SRCURL=git+https://github.com/luvit/luvi.git
-TERMUX_PKG_DEPENDS="pcre2, openssl, luv, libluajit"
+TERMUX_PKG_VERSION=2.15.0
+TERMUX_PKG_SRCURL=git+https://github.com/luvit/luvi
+TERMUX_PKG_DEPENDS="libluajit, lua51-lpeg, luv, openssl, pcre2, zlib"
 TERMUX_PKG_SUGGESTS="lit, luvit"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 	-DWithSharedLibluv=On
 	-DWithOpenSSL=On
@@ -17,22 +15,23 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 	-DWithPCRE2=On
 	-DWithSharedPCRE2=On
 	-DWithLPEG=On
+	-DWithSharedLPEG=On
+	-DWithZLIB=On
+	-DWithSharedZLIB=ON
 	-DLIBLUV_INCLUDE_DIR=${TERMUX_PREFIX}/include/luv
-	-DLIBLUV_LIBRARIES=${TERMUX_PREFIX}/lib/libluv.so
 	-DLUAJIT_INCLUDE_DIR=${TERMUX_PREFIX}/include/luajit-2.1
-	-DLUAJIT_LIBRARIES=${TERMUX_PREFIX}/lib/libluajit.so
 	-DLIBUV_INCLUDE_DIR=${TERMUX_PREFIX}/include
-	-DLIBUV_LIBRARIES=${TERMUX_PREFIX}/lib/libuv.so
 	-DOPENSSL_INCLUDE_DIR=${TERMUX_PREFIX}/include
-	-DOPENSSL_LIBRARIES=${TERMUX_PREFIX}/lib
 	-DPCRE2_INCLUDE_DIR=${TERMUX_PREFIX}/include
-	-DPCRE2_LIBRARIES=${TERMUX_PREFIX}/lib
+	-DZLIB_INCLUDE_DIR=${TERMUX_PREFIX}/include
+	-DLIBLUV_LIBRARIES=${TERMUX_PREFIX}/lib/libluv.so
+	-DLUAJIT_LIBRARIES=${TERMUX_PREFIX}/lib/libluajit.so
+	-DLIBUV_LIBRARIES=${TERMUX_PREFIX}/lib/libuv.so
+	-DOPENSSL_LIBRARIES=${TERMUX_PREFIX}/lib/libssl.so;${TERMUX_PREFIX}/lib/libcrypto.so
+	-DPCRE2_LIBRARIES=${TERMUX_PREFIX}/lib/libpcre2-8.so
+	-DZLIB_LIBRARIES=${TERMUX_PREFIX}/lib/libz.so
+	-DLPEG_LIBRARIES=${TERMUX_PREFIX}/lib/liblpeg-5.1.so
 "
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout "${_commit}"
-}
 
 termux_step_pre_configure() {
 	local dlp_commit="7adf5b3" script_checksum="4b4412c4e93c3cebfc830c643a04b31108a12f10abf7489ab0a80077a1a1ccdb"
@@ -41,6 +40,6 @@ termux_step_pre_configure() {
 		"${TERMUX_PKG_CACHEDIR}/dumbParser.lua" \
 		"${script_checksum}"
 	export LUA_PATH=";;${TERMUX_PKG_CACHEDIR}/?.lua"
-	
-	echo "$_version" >"${TERMUX_PKG_SRCDIR}/VERSION"
+
+	echo "${TERMUX_PKG_VERSION}" > "${TERMUX_PKG_SRCDIR}/VERSION"
 }
