a8ffef6831a482db913fd668fbaf908f11107cba
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-21 04:49:18 +0000

    enhance(x11/wayvnc): Enable man pages

--- x11-packages/wayvnc/build.sh
+++ x11-packages/wayvnc/build.sh
@@ -3,13 +3,15 @@ TERMUX_PKG_DESCRIPTION="A VNC server for wlroots based Wayland compositors"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.9.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/any1/wayvnc/archive/refs/tags/v${TERMUX_PKG_VERSION[0]}.tar.gz
 TERMUX_PKG_SHA256=aaaca02d36e54ec6ecf457dc266251946d895ac91521fbabb3470c3c09b3753c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libaml, libdrm, libjansson, libneatvnc, libpixman, libwayland, libxkbcommon"
-TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, scdoc"
 TERMUX_PKG_SUGGESTS="sway"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dman-pages=enabled
 -Dscreencopy-dmabuf=disabled
 -Dpam=disabled
 "
