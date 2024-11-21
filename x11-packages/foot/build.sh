8108750faa85328f84f3b20d4576ff01099a52f3
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-21 07:17:37 +0000

    enhance(x11/foot): Enable man pages

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -8,8 +8,9 @@ TERMUX_PKG_SRCURL=https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.t
 TERMUX_PKG_SHA256=148b0b545ca37e15b877ff9f6a768a4ce6feb0ed256f8a5f853cb2e16e3323c1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, fontconfig, freetype, libfcft, libpixman, libwayland, libxkbcommon, utf8proc"
-TERMUX_PKG_BUILD_DEPENDS="libtllist, libwayland-protocols"
+TERMUX_PKG_BUILD_DEPENDS="libtllist, libwayland-protocols, scdoc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Ddocs=enabled
 -Dterminfo-base-name=foot-extra
 "
 
