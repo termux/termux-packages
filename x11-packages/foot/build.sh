687302602985d01fae319cfd34dd419ce1ad8034
Biswapriyo Nath <nathbappai@gmail.com>
2024-11-21 07:16:54 +0000

    fix(x11/foot): Fix terminfo file conflict with ncurses

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -3,11 +3,15 @@ TERMUX_PKG_DESCRIPTION="Fast, lightweight and minimalistic Wayland terminal emul
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.19.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=148b0b545ca37e15b877ff9f6a768a4ce6feb0ed256f8a5f853cb2e16e3323c1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, fontconfig, freetype, libfcft, libpixman, libwayland, libxkbcommon, utf8proc"
 TERMUX_PKG_BUILD_DEPENDS="libtllist, libwayland-protocols"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dterminfo-base-name=foot-extra
+"
 
 termux_step_pre_configure() {
 	export PATH="$TERMUX_PREFIX/opt/libwayland/cross/bin:$PATH"
