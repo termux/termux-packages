0877c4ffa98f03eae7a9d3b710ffb419c57057aa
Jia Yuan Lo <jylo06g@gmail.com>
2024-12-01 23:30:34 +0800

    chore(main/mcfly): replace RUSTFLAGS

--- packages/mcfly/build.sh
+++ packages/mcfly/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Replaces your default ctrl-r shell history search with a
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.9.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/cantino/mcfly/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bfb6ca73c6a03047e3c61edf2b3c770e24ddbb0720e2a7dad3ea13a759572bb6
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -13,7 +14,8 @@ termux_step_pre_configure() {
 		local libdir=target/x86_64-linux-android/release/deps
 		mkdir -p $libdir
 		pushd $libdir
-		RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
+		local env_host=$(printf $CARGO_TARGET_NAME | tr a-z A-Z | sed s/-/_/g)
+		export CARGO_TARGET_${env_host}_RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
 		echo "INPUT(-l:libunwind.a)" > libgcc.so
 		popd
 	fi
