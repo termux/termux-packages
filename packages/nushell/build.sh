b1228f426255d00d6dc7d560ccd4e441ba61e2e9
Jia Yuan Lo <jylo06g@gmail.com>
2024-12-01 23:35:27 +0800

    chore(main/nushell): replace RUSTFLAGS

--- packages/nushell/build.sh
+++ packages/nushell/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="A new type of shell operating on structured data"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.100.0"
-TERMUX_PKG_SRCURL=https://github.com/nushell/nushell/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/nushell/nushell/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=00b94984d3140e286e06d4beca2926ef80e0a0cc3ada75916e4fb9fa66ec28ad
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl"
@@ -13,6 +14,7 @@ termux_step_pre_configure() {
 	termux_setup_rust
 
 	if [ "$TERMUX_ARCH" = "x86_64" ]; then
-		RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
+		local env_host=$(printf $CARGO_TARGET_NAME | tr a-z A-Z | sed s/-/_/g)
+		export CARGO_TARGET_${env_host}_RUSTFLAGS+=" -C link-arg=$($CC -print-libgcc-file-name)"
 	fi
 }
