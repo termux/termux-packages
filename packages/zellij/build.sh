c12c791432d67e4464b65e3dfe75e8108f3d4add
Jia Yuan Lo <jylo06g@gmail.com>
2024-12-01 21:14:13 +0800

    chore(main/zellij): use latest curl-sys 0.4.78 crate
    Also fix openssl pollution

--- packages/zellij/build.sh
+++ packages/zellij/build.sh
@@ -3,17 +3,22 @@ TERMUX_PKG_DESCRIPTION="A terminal workspace with batteries included"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Jonathan Lei <me@xjonathan.dev>"
 TERMUX_PKG_VERSION="0.41.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/zellij-org/zellij/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=12e7f0f80c1e39deed5638c4662fc070855cee0250a7eb1d76cefbeef8c2f376
-TERMUX_PKG_BUILD_DEPENDS="zlib"
+TERMUX_PKG_BUILD_DEPENDS="openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 # wasmer doesn't support these platforms yet
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
 
-termux_step_make() {
+termux_step_pre_configure() {
 	termux_setup_rust
+	export OPENSSL_NO_VENDOR=1
+}
+
+termux_step_make() {
 	cargo build --jobs ${TERMUX_PKG_MAKE_PROCESSES} --target ${CARGO_TARGET_NAME} --release
 }
 
