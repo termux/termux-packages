1f419f93fd3bf89db14d3a70c5a2b629560a5846
Jia Yuan Lo <jylo06g@gmail.com>
2024-12-01 20:51:40 +0800

    chore(main/ytui-music): set correct OPENSSL_INCLUDE_DIR

--- packages/ytui-music/build.sh
+++ packages/ytui-music/build.sh
@@ -3,14 +3,14 @@ TERMUX_PKG_DESCRIPTION="Youtube client in terminal for music"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.0.0-beta
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/sudipghimire533/ytui-music/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=43deb6b3cb9eb836b7122ac2542106f46519f240f99a0af67eecdfa5b200cca7
 TERMUX_PKG_DEPENDS="libsqlite, mpv, openssl, python-pip"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	export OPENSSL_INCLUDE_DIR=$TERMUX_PREFIX/include/openssl
+	export OPENSSL_INCLUDE_DIR=$TERMUX_PREFIX/include
 	export OPENSSL_LIB_DIR=$TERMUX_PREFIX/lib
 
 	TERMUX_PKG_SRCDIR+="/front-end"
