d0548bd432783c771fb2ac49dfdea67fb2b96cd6
Jia Yuan Lo <jylo06g@gmail.com>
2024-12-01 23:40:43 +0800

    chore(main/ruff): replace RUSTFLAGS

--- packages/ruff/build.sh
+++ packages/ruff/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.8.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/charliermarsh/ruff/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=46ae2a51c96783a2a9afef1b63639f78e043431793206d1db1406fa5e85db903
 TERMUX_PKG_AUTO_UPDATE=true
@@ -21,7 +22,8 @@ termux_step_pre_configure() {
 		-c $TERMUX_PKG_BUILDER_DIR/ctermid.c
 	$AR cru libctermid.a ctermid.o
 
-	RUSTFLAGS+=" -C link-arg=$TERMUX_PKG_BUILDDIR/_lib/libctermid.a"
+	local env_host=$(printf $CARGO_TARGET_NAME | tr a-z A-Z | sed s/-/_/g)
+	export CARGO_TARGET_${env_host}_RUSTFLAGS+=" -C link-arg=$TERMUX_PKG_BUILDDIR/_lib/libctermid.a"
 
 	termux_setup_rust
 
