ac692e0bee1f2afb8ee47a3f71b8e04bb47ba360
Robert Kirkman <misternumberone@live.com>
2024-12-03 17:30:14 -0600

    addpkg(x11/luanti{-common,-server}): moved from TUR
    Copied and pasted from https://github.com/termux-user-repository/tur/tree/master/tur/luanti
    
    Previous documentation here:
    - termux-user-repository/tur#1295
    - termux-user-repository/tur#1296
    - termux-user-repository/tur#1331
    - termux-user-repository/tur#1327
    
    Summary of key points:
    
    - for better performance than normal virglrenderer-android, test using [termux-wsi-layer](#22353) or [new virglrenderer](#22385)
    
    - to use touchscreen input, set "Touchscreen input mode: Direct touch" in Termux:X11 settings
    
    - to use keyboard input, set "Prefer scancodes" in Termux:X11 settings
    
    - to use mouse input, set "Capture external pointer devices" in Termux:X11 settings
    
    - 6 patches are unique to TUR/termux, but the patch 0001-enable-egl-without-sdl2.patch is
      a cherry pick of minetest/minetest@4838eb2

---
+++ x11-packages/luanti/luanti-common.subpackage.sh
@@ -0,0 +1,6 @@
+TERMUX_SUBPKG_INCLUDE="share/luanti/ share/man/ share/doc/"
+TERMUX_SUBPKG_DESCRIPTION="Common files, including docs, shared between luanti and luanti-server"
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
+TERMUX_SUBPKG_BREAKS="luanti (<< 1:5.10.0-4)"
+TERMUX_SUBPKG_REPLACES="luanti (<< 1:5.10.0-4)"
