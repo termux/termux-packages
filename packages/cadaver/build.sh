eb711d92f78d0ebac968b980f067ad269694abb7
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-05 20:11:52 +0000

    bump(main/cadaver): 0.26
    Remove neon-33.patch file which was added in upstream.
    https://github.com/notroj/cadaver/commit/2433126db25ffd38dc11ef847614e479141cc229

--- packages/cadaver/build.sh
+++ packages/cadaver/build.sh
@@ -2,15 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://notroj.github.io/cadaver/
 TERMUX_PKG_DESCRIPTION="A command-line WebDAV client for Unix"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.24"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.26"
 TERMUX_PKG_SRCURL=https://notroj.github.io/cadaver/cadaver-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=46cff2f3ebd32cd32836812ca47bcc75353fc2be757f093da88c0dd8f10fd5f6
-TERMUX_PKG_DEPENDS="libneon, readline"
+TERMUX_PKG_SHA256=9236e43cdf3505d9ef06185fda43252840105c0c02d9370b6e1077d866357b55
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="libiconv, libneon, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-nls
 "
 
 termux_step_pre_configure() {
-	autoreconf -vfi
+	./autogen.sh
 }
