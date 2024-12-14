71fca31f33caf53156c53043ce1aaccf950bdc11
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-14 09:51:31 +0000

    bump(root/erofs-utils): 1.8.3

--- root-packages/erofs-utils/build.sh
+++ root-packages/erofs-utils/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_DESCRIPTION="A github erofs-utils fork for community development"
 TERMUX_PKG_LICENSE="GPL-2.0, Apache-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSES/GPL-2.0, LICENSES/Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.8.2"
+TERMUX_PKG_VERSION="1.8.3"
 TERMUX_PKG_SRCURL=https://github.com/erofs/erofs-utils/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=64b6ff7e899f62480283cee63787f37f0f9c4be7a6bc7a23d734aaa873a6cff4
+TERMUX_PKG_SHA256=3c5cc03603ea08ba9ae5e0420eeaea5ff17ed29e2280685310356cbf25304e85
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_DEPENDS="liblz4, liblzma, libfuse2, libuuid"
+TERMUX_PKG_DEPENDS="liblz4, liblzma, libfuse2, libuuid, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--prefix=$TERMUX_PREFIX --enable-lzma --enable-fuse"
 TERMUX_PKG_BUILD_IN_SRC=true
 
