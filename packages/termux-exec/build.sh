18fd826849a4961ce8f3703de7aadd1b217c4be6
agnostic-apollo <agnosticapollo@gmail.com>
2024-12-16 00:54:51 +0500

    bump(main/termux-exec): 2.0.0

--- packages/termux-exec/build.sh
+++ packages/termux-exec/build.sh
@@ -2,9 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-exec
 TERMUX_PKG_DESCRIPTION="An execve() wrapper to make /bin and /usr/bin shebangs work"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:1.0
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=b977592f197bf3a87e8a005ea0ccefb3e144edc81d5e3dc8d1ad1a12512f4a68
+TERMUX_PKG_VERSION=1:2.0.0
+
+# FIXME: Fix urls
+TERMUX_PKG_SRCURL=file:///home/builder/termux-packages/sources/termux-exec
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
+
+#TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+#TERMUX_PKG_SHA256=xxx
+
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="TERMUX_EXEC_PKG__VERSION=${TERMUX_PKG_VERSION} TERMUX_EXEC_PKG__ARCH=${TERMUX_ARCH} \
