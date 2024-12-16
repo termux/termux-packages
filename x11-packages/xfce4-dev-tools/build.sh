0c046a178911dc1ad948bca2eb7e44033df1f543
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-16 09:22:45 +0000

    bump(x11/xfce4-dev-tools): 4.20.0

--- x11-packages/xfce4-dev-tools/build.sh
+++ x11-packages/xfce4-dev-tools/build.sh
@@ -2,8 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.xfce.org/xfce/xfce4-dev-tools
 TERMUX_PKG_DESCRIPTION="A collection of tools and macros for Xfce developers"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.18.1"
+TERMUX_PKG_VERSION="4.20.0"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-dev-tools/${TERMUX_PKG_VERSION%.*}/xfce4-dev-tools-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=812cabe7048922ebc176564b73c3e427e467c9566365ee3e54c0487d305a7681
+TERMUX_PKG_SHA256=1fba39a08a0ecc771eaa3a3b6e4272a4f0b9e7c67d0f66e780cd6090cd4466aa
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_prog_MESON=meson
+"
