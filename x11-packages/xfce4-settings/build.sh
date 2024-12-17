aa8e0e8b985a0a65f5c373cb11805464b7dde16e
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-17 15:11:58 +0000

    bump(x11/xfce4-settings): 4.20.0

--- x11-packages/xfce4-settings/build.sh
+++ x11-packages/xfce4-settings/build.sh
@@ -1,14 +1,15 @@
-TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/xfce/xfce4-settings/start
 TERMUX_PKG_DESCRIPTION="Settings manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.18.6"
+TERMUX_PKG_VERSION="4.20.0"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-settings/${TERMUX_PKG_VERSION%.*}/xfce4-settings-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=d9a9051b6026edd6766c64bb403b51e9167e4d31e7f1c7f843d3aed19f667bfe
+TERMUX_PKG_SHA256=23548da3429a296501fbfdbc98a861ee241b9fdd47e8d5de1781f57c6bbce5a9
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, exo, fontconfig, garcon, gdk-pixbuf, glib, gtk3, libcairo, libnotify, libx11, libxcursor, libxfce4ui, libxfce4util, libxi, libxklavier, libxrandr, pango, xfconf"
+TERMUX_PKG_DEPENDS="atk, exo, fontconfig, garcon, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libnotify, libwayland, libx11, libxcursor, libxfce4ui, libxfce4util, libxi, libxklavier, libxrandr, pango, xfconf, zlib"
 TERMUX_PKG_RECOMMENDS="gsettings-desktop-schemas, libcanberra, lxde-icon-theme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
 --enable-xrandr
 --enable-xcursor
 --enable-libnotify
@@ -16,3 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-pluggable-dialogs
 --enable-sound-settings
 "
+
+termux_step_pre_configure() {
+	termux_setup_glib_cross_pkg_config_wrapper
+}
