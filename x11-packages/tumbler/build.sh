16c1a443def9973a6e205fa387cc06986a04e298
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-17 06:38:29 +0000

    bump(x11/tumbler): 4.20.0

--- x11-packages/tumbler/build.sh
+++ x11-packages/tumbler/build.sh
@@ -2,9 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/xfce/tumbler/start
 TERMUX_PKG_DESCRIPTION="Tumbler is a D-Bus service for applications to request thumbnails for various URI schemes and MIME type"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.18.2"
+TERMUX_PKG_VERSION="4.20.0"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/tumbler/${TERMUX_PKG_VERSION%.*}/tumbler-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=b530eec635eac7f898c0d8d3a3ff79d76a145d3bed3e786d54b1ec058132be7a
+TERMUX_PKG_SHA256=74b1647d55926547e98bfac70838ff63c5a84299a5e10c81c38d1fab90e25880
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, glib, libcurl, libjpeg-turbo, libpng, libxfce4util"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/systemd"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
+--enable-gtk-doc-html=no
+"
+
+termux_step_pre_configure() {
+	termux_setup_glib_cross_pkg_config_wrapper
+}
