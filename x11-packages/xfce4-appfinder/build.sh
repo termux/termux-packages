6fd39b592bf5a178868efe2d5a9dbbd3645fa7fb
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-17 16:20:51 +0000

    bump(x11/xfce4-appfinder): 4.20.0

--- x11-packages/xfce4-appfinder/build.sh
+++ x11-packages/xfce4-appfinder/build.sh
@@ -2,7 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/xfce/xfce4-appfinder/start
 TERMUX_PKG_DESCRIPTION="Application launcher and finder"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.18.1"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-appfinder/${TERMUX_PKG_VERSION%.*}/xfce4-appfinder-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=9854ea653981be544ad545850477716c4c92d0c43eb47b75f78534837c0893f9
-TERMUX_PKG_DEPENDS="garcon, gdk-pixbuf, gtk3, libcairo, libxfce4ui, libxfce4util, xfconf"
+TERMUX_PKG_VERSION="4.20.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-appfinder/${TERMUX_PKG_VERSION%.*}/xfce4-appfinder-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=82ca82f77dc83e285db45438c2fe31df445148aa986ffebf2faabee4af9e7304
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="garcon, gdk-pixbuf, glib, gtk3, libcairo, libxfce4ui, libxfce4util, xfconf"
+TERMUX_PKG_BUILD_DEPENDS="xfce4-dev-tools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
+"
