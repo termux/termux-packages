3a7b652a91a12bb43649bd040395b4bc879749d4
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-18 14:49:34 +0000

    fix(x11/xfce4-settings): Add gtk-layer-shell dependency
    Add required configure options explicitly. Otherwise,
    configure script continues with any missing dependencies.

--- x11-packages/xfce4-settings/build.sh
+++ x11-packages/xfce4-settings/build.sh
@@ -3,15 +3,19 @@ TERMUX_PKG_DESCRIPTION="Settings manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.20.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-settings/${TERMUX_PKG_VERSION%.*}/xfce4-settings-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=23548da3429a296501fbfdbc98a861ee241b9fdd47e8d5de1781f57c6bbce5a9
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, exo, fontconfig, garcon, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libnotify, libwayland, libx11, libxcursor, libxfce4ui, libxfce4util, libxi, libxklavier, libxrandr, pango, xfconf, zlib"
+TERMUX_PKG_DEPENDS="atk, exo, fontconfig, garcon, gdk-pixbuf, glib, gtk3, gtk-layer-shell, harfbuzz, libcairo, libnotify, libwayland, libx11, libxcursor, libxfce4ui, libxfce4util, libxi, libxklavier, libxrandr, pango, xfconf, zlib"
 TERMUX_PKG_RECOMMENDS="gsettings-desktop-schemas, libcanberra, lxde-icon-theme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-debug
+--enable-wayland
+--enable-x11
 --enable-xrandr
 --enable-xcursor
+--enable-gtk-layer-shell
 --enable-libnotify
 --enable-libxklavier
 --enable-pluggable-dialogs
