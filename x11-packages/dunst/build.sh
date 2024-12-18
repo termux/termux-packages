5fe46f6a864c0fc617a821744e14d397dac3b56b
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-18 04:27:46 +0000

    bump(x11/dunst): 1.12.1

--- x11-packages/dunst/build.sh
+++ x11-packages/dunst/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://dunst-project.org/
 TERMUX_PKG_DESCRIPTION="Lightweight and customizable notification daemon"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.0"
+TERMUX_PKG_VERSION="1.12.1"
 TERMUX_PKG_SRCURL=https://github.com/dunst-project/dunst/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=217075fa52ac9cd9b87b6dafb094019c4b07a1572259f7bd2adc4fc31771ed95
+TERMUX_PKG_SHA256=340b10c38ee519a75b14040f65505d72817857358ce7a6fe23190ab68782f892
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-wordexp, dbus, gdk-pixbuf, glib, libcairo, libnotify, libx11, libxext, libxinerama, libxrandr, libxss, pango"
+TERMUX_PKG_DEPENDS="libandroid-wordexp, dbus, gdk-pixbuf, glib, harfbuzz, libcairo, libnotify, libx11, libxext, libxinerama, libxrandr, libxss, pango"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
