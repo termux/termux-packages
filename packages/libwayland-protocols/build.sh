26081769696294af9633b3dce9df8916ee55d463
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-18 15:57:31 +0000

    bump(main/libwayland-protocols): 1.38
    Use cross wayland scanner because older Ubuntu's wayland-scanner fails
    to build the package with new command line options.

--- packages/libwayland-protocols/build.sh
+++ packages/libwayland-protocols/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Wayland protocols library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.36"
+TERMUX_PKG_VERSION="1.38"
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${TERMUX_PKG_VERSION}/downloads/wayland-protocols-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=71fd4de05e79f9a1ca559fac30c1f8365fa10346422f9fe795f74d77b9ef7e92
+TERMUX_PKG_SHA256=ff17292c05159d2b20ce6cacfe42d7e31a28198fa1429a769b03af7c38581dbe
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_BUILD_DEPENDS="libwayland, libwayland-cross-scanner"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=false
 "
