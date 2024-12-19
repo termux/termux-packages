093d725cde406c9503e611ead2d7a2ebace4e95f
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-19 07:59:49 +0000

    bump(x11/xfce4-session): 4.20.0
    * Remove unnecessary dont-update-icon-cache.patch file.
    * Add required configure options explicitly for strict dependency checking.

--- x11-packages/xfce4-session/build.sh
+++ x11-packages/xfce4-session/build.sh
@@ -1,15 +1,22 @@
-TERMUX_PKG_HOMEPAGE=https://www.xfce.org/
+TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/xfce/xfce4-session/start
 TERMUX_PKG_DESCRIPTION="A session manager for XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.18.4"
+TERMUX_PKG_VERSION="4.20.0"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-session/${TERMUX_PKG_VERSION%.*}/xfce4-session-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9a9c5074c7338b881a5259d3b643619bf84901360c03478e1a697938ece06516
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, libcairo, libice, libsm, libwnck, libx11, libxfce4ui, libxfce4util, pango, xfconf, xorg-iceauth, xorg-xrdb"
+TERMUX_PKG_SHA256=5229233fe6ee692361cc28724886c5b08e0216d89f09c42d273191d38fd64f85
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, gtk-layer-shell, libcairo, libice, libsm, libwnck, libx11, libxfce4ui, libxfce4util, libxfce4windowing, pango, xfconf, xorg-iceauth, xorg-xrdb"
+TERMUX_PKG_BUILD_DEPENDS="xfce4-dev-tools"
 TERMUX_PKG_RECOMMENDS="gnupg, hicolor-icon-theme, xfce4-settings, xfdesktop, xfwm4"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_ICEAUTH=${TERMUX_PREFIX}/bin/iceauth
---with-xsession-prefix=$TERMUX_PREFIX
+--disable-debug
+--enable-gtk-layer-shell
+--enable-wayland
+--enable-x11
+--with-wayland-session-prefix=${TERMUX_PREFIX}
+--with-xsession-prefix=${TERMUX_PREFIX}
 "
 
 termux_step_create_debscripts() {
