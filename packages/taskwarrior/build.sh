a480bde3bdbcbd9b14a4f8049749066d5e4c9584
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-19 18:57:24 +0000

    bump(main/taskwarrior): 3.3.0
    Remove 0001-libcxx-18-sort.patch file which was added in upstream.
    https://github.com/GothenburgBitFactory/taskwarrior/commit/8cc4c461d6902539e12bf6a3e9597813b400fa1e

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://taskwarrior.org
 TERMUX_PKG_DESCRIPTION="Utility for managing your TODO list"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.2.0"
+TERMUX_PKG_VERSION="3.3.0"
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${TERMUX_PKG_VERSION}/task-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1a543ba373a319f924a2242c8e0db9dac2691c4cf654c2d0c6827a06b3b32592
+TERMUX_PKG_SHA256=7fd1e3571f673679758f001b5f44963eee59fd0d2cac887a5807cf2fd90856a1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, libgnutls, libuuid"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
@@ -12,6 +12,7 @@ TERMUX_CMAKE_BUILD="Unix Makefiles"
 termux_step_pre_configure() {
 	termux_setup_rust
 
+	CXXFLAGS+=" -Wno-c++11-narrowing"
 	LDFLAGS+=" -landroid-glob"
 
 	if [ "$TERMUX_ARCH" = "arm" ]; then
