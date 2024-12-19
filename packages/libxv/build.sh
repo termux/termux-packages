ac8e78cab600c8f574dd609848b5f6d9ad5028bb
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-19 18:25:42 +0000

    bump(main/libxv): 1.0.13

--- packages/libxv/build.sh
+++ packages/libxv/build.sh
@@ -1,11 +1,14 @@
-# X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Library for the X Video (Xv) extension to the X Window System"
 TERMUX_PKG_LICENSE="HPND"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.12
+TERMUX_PKG_VERSION="1.0.13"
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXv-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=aaf7fa09f689f7a2000fe493c0d64d1487a1210db154053e9e2336b860c63848
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
+TERMUX_PKG_SHA256=7d34910958e1c1f8d193d828fea1b7da192297280a35437af0692f003ba03755
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libx11, libxext"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-static
+--enable-malloc0returnsnull
+"
