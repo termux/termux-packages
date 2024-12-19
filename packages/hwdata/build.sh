0ece8402af998ad06d207cd84fb86b58e6f3b536
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-19 04:29:46 +0000

    addpkg(main/hwdata): 0.390, for libdisplay-info

---
+++ packages/hwdata/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/vcrhonek/hwdata
+TERMUX_PKG_DESCRIPTION="Database of hardware identification and configuration data"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.390"
+TERMUX_PKG_SRCURL=https://github.com/vcrhonek/hwdata/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f10684093d2a7780de8a96d3dd8a2dd544ed6136dd359197750c42bb08ce526f
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--datadir=${TERMUX_PREFIX}/share
+--disable-blacklist
+"
+
+termux_step_pre_configure() {
+	mv Makefile{,.unused}
+}
+
+termux_step_post_configure() {
+	mv Makefile{.unused,}
+}
