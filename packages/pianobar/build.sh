67231bf2bfcc10923055044da5399c1552aadebf
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-21 13:48:38 +0000

    bump(main/pianobar): 2024.12.21
    * Remove patch which was added in this release.
    * Remove src-player.h.patch which is not required anymore.

--- packages/pianobar/build.sh
+++ packages/pianobar/build.sh
@@ -2,33 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://6xq.net/pianobar/
 TERMUX_PKG_DESCRIPTION="pianobar is a free/open-source, console-based client for the personalized online radio Pandora."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2022.04.01
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="2024.12.21"
 TERMUX_PKG_SRCURL=https://github.com/PromyLOPh/pianobar/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2653c6659a141868625ab24ecf04210d20347d50e0bd03e670e2daefa9f4fb2d
+TERMUX_PKG_SHA256=e988dff4a4b7cc6a19e944b7516f697d7e6c41d6dc0ff25a708bcb6b92d72a89
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="libao, ffmpeg, libgcrypt, libcurl, json-c"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	declare -a _commits=(
-	8bf4c1bb
-	)
-
-	declare -a _checksums=(
-	ab8f7539189a21064b1773551b393df16c34a371d23f38bf5394ca7b6e0ec411
-	)
-
-	for i in "${!_commits[@]}"; do
-		PATCHFILE="${TERMUX_PKG_CACHEDIR}/pianobar_patch_${_commits[i]}.patch"
-		termux_download \
-			"https://github.com/PromyLOPh/pianobar/commit/${_commits[i]}.patch" \
-			"$PATCHFILE" \
-			"${_checksums[i]}"
-		patch -p1 -i "$PATCHFILE"
-	done
-}
 
 termux_step_post_make_install(){
 	#install useful script
