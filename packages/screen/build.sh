08619510bf45a7f29710a84e69212f1caaafe4b0
TomIO <tomjo00@web.de>
2024-12-21 16:41:56 +0100

    bump(main/screen): 5.0.0

--- packages/screen/build.sh
+++ packages/screen/build.sh
@@ -2,12 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/screen/
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with VT100/ANSI terminal emulation"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.9.1"
+TERMUX_PKG_VERSION="5.0.0"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/screen/screen-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=26cef3e3c42571c0d484ad6faf110c5c15091fbf872b06fa7aa4766c7405ac69
+TERMUX_PKG_SHA256=f04a39d00a0e5c7c86a55338808903082ad5df4d73df1a2fd3425976aed94971
 TERMUX_PKG_DEPENDS="ncurses, libcrypt"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-socket-dir
+--disable-pam
 --enable-colors256
 "
 
@@ -19,6 +22,6 @@ termux_step_pre_configure() {
 }
 
 termux_step_post_configure() {
-	echo '#define HAVE_SVR4_PTYS 1' >> $TERMUX_PKG_BUILDDIR/config.h
+	echo '#define HAVE_SVR4_PTYS 1' >> "$TERMUX_PKG_BUILDDIR/config.h"
 	echo 'mousetrack on' > "$TERMUX_PREFIX/etc/screenrc"
 }
