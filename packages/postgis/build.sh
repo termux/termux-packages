f7033d1a7e3fae15da3a46c83c1091189f49bf4c
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-23 13:44:55 +0000

    bump(main/postgis): 3.5.1

--- packages/postgis/build.sh
+++ packages/postgis/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://postgis.net
 TERMUX_PKG_DESCRIPTION="Spatial database extender for PostgreSQL object-relational database"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.5.0"
+TERMUX_PKG_VERSION="3.5.1"
 TERMUX_PKG_SRCURL=https://download.osgeo.org/postgis/source/postgis-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=ca698a22cc2b2b3467ac4e063b43a28413f3004ddd505bdccdd74c56a647f510
+TERMUX_PKG_SHA256=23706abc117fb1bab45a27a263b589f52fc08ebaff318c0bc0bdc940905306b5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gdal, json-c, libc++, libgeos, libiconv, libprotobuf-c, libxml2, pcre2, postgresql, proj"
 
@@ -13,5 +13,6 @@ TERMUX_PKG_DEPENDS="gdal, json-c, libc++, libgeos, libiconv, libprotobuf-c, libx
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	CPPFLAGS+=" -D_GNU_SOURCE" # for preadv and pwritev
 	CXXFLAGS+=" -std=c++14"
 }
