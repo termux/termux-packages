f2f7d30fb9d1110a99b626c7bc7d7694b838feb8
Robert Kirkman <misternumberone@live.com>
2024-12-23 18:40:36 -0600

    fix(x11/firefox): fix Segmentation fault in the command "firefox -headless"
    Fixes termux#22286

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="133.0.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION}/source/firefox-${TERMUX_PKG_VERSION}.source.tar.xz
 TERMUX_PKG_SHA256=f134a5420200bb03ab460f9d2867507c0edb222ce73faf4064cdbea02a0aca1b
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
