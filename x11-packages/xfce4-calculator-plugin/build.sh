aefc3295f0983429992bb268e3aec96098328ccd
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-25 10:24:17 +0000

    bump(x11/xfce4-calculator-plugin): 0.7.3

--- x11-packages/xfce4-calculator-plugin/build.sh
+++ x11-packages/xfce4-calculator-plugin/build.sh
@@ -1,9 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-calculator-plugin/start
-TERMUX_PKG_DESCRIPTION="xfce4-calculator-plugin is a calculator plugin for the Xfce4 panel."
+TERMUX_PKG_DESCRIPTION="Simple command line based calculator for the Xfce panel"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=0.7
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-calculator-plugin/${_MAJOR_VERSION}/xfce4-calculator-plugin-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=d1f622bea41a90c1686bf9f13c488ab28e995e2762b84712dea9027e0c94028b
+TERMUX_PKG_VERSION="0.7.3"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-calculator-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-calculator-plugin-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=3feb5f56092ceef2858c3c1bd443317d4caf289a6409f9db506f49088e19a2e8
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfce4-panel, zlib"
+TERMUX_PKG_BUILD_DEPENDS="xfce4-dev-tools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
+--disable-static
+"
