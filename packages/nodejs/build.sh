d24529ace0237a979ad160fbff0a5b8bbc510987
Yaksh Bariya <yakshbari4@gmail.com>
2024-12-25 07:39:14 +0530

    bump(main/nodejs): 23.5.0
    Also use clang instead of gcc for host build, as it consumes much less
    memory than gcc and should also be faster in terms of compilation time.
    On my 20 core machine, when building with docker, the build takes up
    more than 16G, when using clang peak memory usage drops to 12-13G and
    averages around 9-10G

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=23.4.0
+TERMUX_PKG_VERSION=23.5.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4bb14bf3faf790bb122a537b05d7bf4006fc51e78761b157ce453b63fa7e3371
+TERMUX_PKG_SHA256=32e77b36c0774c68baab41bc7c2acc58663ca0a2b7c4d3e9bec6f761c15fdac0
 # thunder-coding: don't try to autoupdate nodejs, that thing takes 2 whole hours to build for a single arch, and requires a lot of patch updates everytime. Also I run tests everytime I update it to ensure least bugs
 TERMUX_PKG_AUTO_UPDATE=false
 # Note that we do not use a shared libuv to avoid an issue with the Android
