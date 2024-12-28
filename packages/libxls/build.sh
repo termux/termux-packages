e543142e71713752701000239d9d73d878575cff
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-28 04:36:21 +0000

    bump(main/libxls): 1.6.3

--- packages/libxls/build.sh
+++ packages/libxls/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/libxls/libxls
 TERMUX_PKG_DESCRIPTION="A C library for reading Excel files in the nasty old binary OLE format"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.6.2
+TERMUX_PKG_VERSION="1.6.3"
 TERMUX_PKG_SRCURL=https://github.com/libxls/libxls/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2f836fcef6372c6bc882c3e80bfd3043812e2fc4d4534022a25b24fe801b1770
+TERMUX_PKG_SHA256=587c9f0ebb5647eb68ec1e0ed8c3f7f6102622d6dd83473a21d3a36dee04eed7
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	autoreconf -fi
