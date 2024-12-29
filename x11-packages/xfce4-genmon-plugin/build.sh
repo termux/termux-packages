53856ef3d19670e4ec7eca6cfd2c6d8189deb4b5
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-29 05:14:58 +0000

    bump(x11/xfce4-genmon-plugin): 4.2.1

--- x11-packages/xfce4-genmon-plugin/build.sh
+++ x11-packages/xfce4-genmon-plugin/build.sh
@@ -2,9 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-genmon-plugin
 TERMUX_PKG_DESCRIPTION="Display cyclically run script or program output onto the panel"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=4.2
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.0
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-genmon-plugin/${_MAJOR_VERSION}/xfce4-genmon-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=948d08ee5f2140847f109b531bc1d4cc6268496913ea7600d3c5ad89025a0362
+TERMUX_PKG_VERSION="4.2.1"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-genmon-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-genmon-plugin-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=de540562e1ea58f35a9c815e20736d26af541a0a9372011148cb75b5f0b65951
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfce4-panel, xfconf, zlib"
 TERMUX_PKG_RECOMMENDS="hicolor-icon-theme"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
+--disable-static
+"
