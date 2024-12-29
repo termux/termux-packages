258430d415d777c3e2e1dc77fa9ade03b2917f05
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-29 10:32:45 +0000

    bump(x11/xfce4-taskmanager): 1.5.8

--- x11-packages/xfce4-taskmanager/build.sh
+++ x11-packages/xfce4-taskmanager/build.sh
@@ -2,9 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-taskmanager/start
 TERMUX_PKG_DESCRIPTION="Easy to use task manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.5.7
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="1.5.8"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-taskmanager/${TERMUX_PKG_VERSION%.*}/xfce4-taskmanager-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6736832f5a64533e536f4994280bd907da19666cda8d2f465c8a53bb581f68bb
+TERMUX_PKG_SHA256=14b9d68b8feb88a642a9885b8549efe7fc9e6c155f638003f2a4a58d9eb2baab
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libcairo, libwnck, libx11, libxfce4ui, libxfce4util, libxmu, xfconf"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
+--disable-static
+--enable-libx11
+--enable-wnck3
+"
