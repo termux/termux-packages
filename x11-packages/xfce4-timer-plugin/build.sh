2fbd9bac32da918b6d77f66a7d6aed0df41eaf61
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-29 13:57:42 +0000

    bump(x11/xfce4-timer-plugin): 1.7.3

--- x11-packages/xfce4-timer-plugin/build.sh
+++ x11-packages/xfce4-timer-plugin/build.sh
@@ -2,9 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-timer-plugin/start
 TERMUX_PKG_DESCRIPTION="alarm and timer module for Xfce panel"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
-_MAJOR_VERSION=1.7
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.2
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-timer-plugin/${_MAJOR_VERSION}/xfce4-timer-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=feb3b8c2d39505e816683540a3226bd7bda870ccbcb4c7d0f6abfeeff5c58b7d
+TERMUX_PKG_VERSION="1.7.3"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-timer-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-timer-plugin-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_SHA256=acf4c861af88608b9e802a76a4b05846bd30189e0085e826680cc179b6df4cd3
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfce4-panel, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-debug
+--disable-static
+"
