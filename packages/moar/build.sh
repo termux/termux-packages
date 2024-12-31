2f482f6aba8985c45b0131736657714637ef44df
Biswapriyo Nath <nathbappai@gmail.com>
2024-12-31 10:00:10 +0000

    bump(main/moar): 1.30.1

--- packages/moar/build.sh
+++ packages/moar/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/walles/moar
 TERMUX_PKG_DESCRIPTION="A pager designed to just do the right thing without any configuration"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.30.0"
+TERMUX_PKG_VERSION="1.30.1"
 TERMUX_PKG_SRCURL=https://github.com/walles/moar/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a26954fca06c95604a94b6f208f62eff247e50c8d239262322a13d0959074de1
+TERMUX_PKG_SHA256=1e20f8a8eefb2594b57f6253e6e44b00555b3aece63a3dcf0f8e9e9ba7700d58
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
