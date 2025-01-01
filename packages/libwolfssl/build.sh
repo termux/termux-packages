4cb10c58684dce5f9747707c3fb9d21d1df9c296
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-01 07:16:19 +0000

    bump(main/libwolfssl): 5.7.6

--- packages/libwolfssl/build.sh
+++ packages/libwolfssl/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.wolfssl.com/
 TERMUX_PKG_DESCRIPTION="A small, fast, portable implementation of TLS/SSL for embedded devices to the cloud"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.7.4"
+TERMUX_PKG_VERSION="5.7.6"
 TERMUX_PKG_SRCURL=https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${TERMUX_PKG_VERSION}-stable.tar.gz
-TERMUX_PKG_SHA256=6c70f6fab34e2f963a255c3637c0c6f7250df2d1ef7c34eebca4f8e8d7323399
+TERMUX_PKG_SHA256=52b1e439e30d1ed8162a16308a8525a862183b67aa30373b11166ecbab000d63
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=42
+	local _SOVERSION=43
 
 	local a
 	for a in WOLFSSL_LIBRARY_VERSION_FIRST; do
