0835622ec52ec3bb74c05eeb07314bcd49dcfe83
TomIO <tomjo00@web.de>
2025-01-02 07:51:34 +0100

    chore(main/ffmpeg): clean up architecture specific build configuration

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -51,26 +51,25 @@ termux_step_pre_configure() {
 termux_step_configure() {
 	cd $TERMUX_PKG_BUILDDIR
 
-	local _EXTRA_CONFIGURE_FLAGS=""
-	if [ $TERMUX_ARCH = "arm" ]; then
+	local _ARCH_CONFIGURE_FLAGS=""
+	case "$TERMUX_ARCH" in
+		"aarch64") _ARCH_CONFIGURE_FLAGS="--enable-neon";;
+		"arm")
 		_ARCH="armeabi-v7a"
-		_EXTRA_CONFIGURE_FLAGS="--enable-neon"
-	elif [ $TERMUX_ARCH = "i686" ]; then
+		_ARCH_CONFIGURE_FLAGS="--enable-neon"
+		;;
+		"i686")
 		_ARCH="x86"
 		# Specify --disable-asm to prevent text relocations on i686,
 		# see https://trac.ffmpeg.org/ticket/4928
-		_EXTRA_CONFIGURE_FLAGS="--disable-asm"
-	elif [ $TERMUX_ARCH = "x86_64" ]; then
-		_ARCH="x86_64"
-	elif [ $TERMUX_ARCH = "aarch64" ]; then
-		_ARCH=$TERMUX_ARCH
-		_EXTRA_CONFIGURE_FLAGS="--enable-neon"
-	else
-		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
-	fi
+		_ARCH_CONFIGURE_FLAGS="--disable-asm"
+		;;
+	"x86_64") ;;
+		*) termux_error_exit "Unsupported arch: $TERMUX_ARCH";;
+	esac
 
-	$TERMUX_PKG_SRCDIR/configure \
-		--arch="${_ARCH}" \
+	"$TERMUX_PKG_SRCDIR"/configure \
+		--arch="${_ARCH:-"$TERMUX_ARCH"}" \
 		--as="$AS" \
 		--cc="$CC" \
 		--cxx="$CXX" \
@@ -131,7 +130,7 @@ termux_step_configure() {
 		--target-os=android \
 		--extra-libs="-landroid-glob" \
 		--disable-vulkan \
-		$_EXTRA_CONFIGURE_FLAGS \
+		$_ARCH_CONFIGURE_FLAGS \
 		--disable-libfdk-aac
 	# GPLed FFmpeg binaries linked against fdk-aac are not redistributable.
 }
