0de063c9ba7c24354d19022bf13aa7e5d16767cb
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-04 09:49:52 +0000

    bump(main/fmt): 11.1.1

--- packages/fmt/build.sh
+++ packages/fmt/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Open-source formatting library for C++"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:11.0.2
+TERMUX_PKG_VERSION=1:11.1.1
 TERMUX_PKG_SRCURL=https://github.com/fmtlib/fmt/archive/${TERMUX_PKG_VERSION#*:}.tar.gz
-TERMUX_PKG_SHA256=6cb1e6d37bdcb756dbbe59be438790db409cdb4868c66e888d5df9f13f7c027f
+TERMUX_PKG_SHA256=482eed9efbc98388dbaee5cb5f368be5eca4893456bb358c18b7ff71f835ae43
 # Avoid silently breaking build of revdeps:
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DFMT_TEST=OFF -DBUILD_SHARED_LIBS=TRUE"
@@ -15,7 +15,7 @@ termux_step_post_massage() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SONAME is changed.
 	local _EXPECTED_SOVERSION=11
-	local _FMT_VERSION=$(grep '#define FMT_VERSION' $TERMUX_PKG_SRCDIR/include/fmt/base.h  | cut -d ' ' -f 3)
+	local _FMT_VERSION=$(grep '#define FMT_VERSION' "$TERMUX_PKG_SRCDIR"/include/fmt/base.h  | cut -d ' ' -f 3)
 	# The fmt library version in the form major * 10000 + minor * 100 + patch:
 	local _ACTUAL_SOVERSION=$(( _FMT_VERSION / 10000 ))
 	if [ "$_EXPECTED_SOVERSION" != "$_ACTUAL_SOVERSION" ]; then
