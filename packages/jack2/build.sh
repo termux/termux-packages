597251f9f6f8a9bc7b8ffe2b49591eefd905a82e
Fredrik Fornwall <fredrik@fornwall.net>
2025-01-04 15:43:55 +0100

    fix(main/jack2): Add patch to make jack2 buildable with host python 3.12

--- packages/jack2/build.sh
+++ packages/jack2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The JACK low-latency audio server"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.9.22
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jackaudio/jack2/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=1e42b9fc4ad7db7befd414d45ab2f8a159c0b30fcd6eee452be662298766a849
 TERMUX_PKG_DEPENDS="alsa-lib, dbus, libandroid-posix-semaphore, libdb, libexpat, libsamplerate, libopus"
