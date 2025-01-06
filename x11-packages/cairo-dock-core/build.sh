37addf70f87b614c89613ee8a88d210f21cfcabf
Fredrik Fornwall <fredrik@fornwall.net>
2025-01-06 13:36:00 +0100

    fix(x11/cairo-dock-core): Fix build by linking libgldi to libm
    Fix build errors such as:
    
    ld.lld: error: undefined reference due to --no-allow-shlib-undefined: tan                                             >>> referenced by gldit/libgldi.so                                                                                    ld.lld: error: undefined reference due to --no-allow-shlib-undefined: atan                                            >>> referenced by gldit/libgldi.so

--- x11-packages/cairo-dock-core/build.sh
+++ x11-packages/cairo-dock-core/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Cairo-Dock is a simple and avanzed dock for linux deskto
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.5.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Cairo-Dock/cairo-dock-core/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a03e71025aa44c01eaccf1ed922bd5497ac4ac1df581f81ec7e8429dcd1c57f4
 TERMUX_PKG_AUTO_UPDATE=true
