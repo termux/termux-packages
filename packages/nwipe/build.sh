b17d40e24ced8af03f4ffa32b7f5d27aa31bc605
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-09 09:45:15 +0000

    bump(main/nwipe): 0.38

--- packages/nwipe/build.sh
+++ packages/nwipe/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/martijnvanbrummelen/nwipe
 TERMUX_PKG_DESCRIPTION="A program that will securely erase the entire contents of disks"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.37"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.38"
 TERMUX_PKG_SRCURL=https://github.com/martijnvanbrummelen/nwipe/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a548bd097da491990d1b0db3fe0ed849340d89281badb46800d3a85ba7df89e0
+TERMUX_PKG_SHA256=0c1b19d1a721b995504fc01c21363555f207b25d6749650355a8cd7be09824dd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, parted, libconfig"
 
