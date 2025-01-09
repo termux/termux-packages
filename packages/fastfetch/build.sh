933dc3bcbd51adb754890275deba6b7bfe46642f
Carter Li <CarterLi@users.noreply.github.com>
2025-01-09 22:26:13 +0800

    enhance(main/fastfetch): add dependencies
    * `ocl-icd` `opencl-headers` for querying OpenCL version
    * `mesa-dev` for querying OpenGL / EGL version
    
    Ref: https://github.com/fastfetch-cli/fastfetch/wiki/Dependencies#android

--- packages/fastfetch/build.sh
+++ packages/fastfetch/build.sh
@@ -1,13 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/fastfetch-cli/fastfetch
-TERMUX_PKG_DESCRIPTION="A neofetch-like tool for fetching system information and displaying them in a pretty way"
+TERMUX_PKG_DESCRIPTION="A maintained, feature-rich and performance oriented, neofetch like system information tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.34.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2dae260d604557c6bf5a9781b3c960aa4265a067e54a984f8c68889a578193a9
-TERMUX_PKG_DEPENDS="vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="freetype, libandroid-wordexp-static, vulkan-headers, vulkan-loader-android"
-TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
+TERMUX_PKG_BUILD_DEPENDS="freetype, libandroid-wordexp-static, mesa-dev, ocl-icd, opencl-headers, vulkan-headers, vulkan-loader-generic"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DTARGET_DIR_HOME=${TERMUX_ANDROID_HOME}
