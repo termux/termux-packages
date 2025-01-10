a0186dcda3954a4136a7a82995d0b28316fc3b57
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-10 07:55:05 +0000

    bump(main/pango): 1.56.0
    * Enable auto-update.
    * Use new source and home page url.

--- packages/pango/build.sh
+++ packages/pango/build.sh
@@ -1,10 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://www.pango.org/
+TERMUX_PKG_HOMEPAGE=https://www.gtk.org/docs/architecture/pango
 TERMUX_PKG_DESCRIPTION="Library for laying out and rendering text"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.54.0"
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/pango/${TERMUX_PKG_VERSION%.*}/pango-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=8a9eed75021ee734d7fc0fdf3a65c3bba51dfefe4ae51a9b414a60c70b2d1ed8
+TERMUX_PKG_VERSION="1.56.0"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/pango/${TERMUX_PKG_VERSION%.*}/pango-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=1fb98b338ee6f7cf8ef96153b7d242f4568fe60f9b7434524eca630a57bd538b
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="fontconfig, freetype, fribidi, glib, harfbuzz, libcairo, libx11, libxft, libxrender"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
 TERMUX_PKG_BREAKS="pango-dev"
@@ -12,6 +13,7 @@ TERMUX_PKG_REPLACES="pango-dev"
 TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dbuild-testsuite=false
 -Dintrospection=enabled
 "
 
