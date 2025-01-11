714d666003538fa45ca7e786a10ef1a7d521c7ba
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-11 07:36:41 +0000

    bump(main/toxic): 0.16.1

--- packages/toxic/build.sh
+++ packages/toxic/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/JFreegman/toxic
 TERMUX_PKG_DESCRIPTION="A command line client for Tox"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.16.0"
+TERMUX_PKG_VERSION="0.16.1"
 TERMUX_PKG_SRCURL=https://github.com/JFreegman/toxic/archive/v${TERMUX_PKG_VERSION}/toxic-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9b58f87941c5638e6169f972292351205bb6335bde8121c103d7dc6fc5174ac7
+TERMUX_PKG_SHA256=4969f0a72e40e0ed296cfff5a5bcd58b999ace52759327c29f23866c96d64f00
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="c-toxcore, libconfig, libcurl, libqrencode, ncurses, zlib"
+TERMUX_PKG_DEPENDS="c-toxcore, libconfig, libcurl, libpng, libqrencode, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
