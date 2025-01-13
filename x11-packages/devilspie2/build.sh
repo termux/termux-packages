fbac9a09b38f2b80f8ba8b07c66fa9540e8f4ac0
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-13 14:18:01 +0000

    bump(x11/devilspie2): 0.45

--- x11-packages/devilspie2/build.sh
+++ x11-packages/devilspie2/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.nongnu.org/devilspie2/
 TERMUX_PKG_DESCRIPTION="A window-matching utility"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.44
-TERMUX_PKG_SRCURL=https://download.savannah.nongnu.org/releases/devilspie2/devilspie2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=0a9f1eadd2b22a318163e4180065d495221ba1a43ad2021ea6866cd118042640
-TERMUX_PKG_DEPENDS="glib, gtk3, liblua54, libwnck, libx11, libxinerama"
+TERMUX_PKG_VERSION="0.45"
+TERMUX_PKG_SRCURL=https://download.savannah.nongnu.org/releases/devilspie2/devilspie2_${TERMUX_PKG_VERSION}-src.tar.gz
+TERMUX_PKG_SHA256=6cc55a68ccfd8bb5620f4cd7c9913ef29aba8a9e96648497c5b448fdb97cb034
+TERMUX_PKG_DEPENDS="glib, gtk3, liblua54, libwnck, libx11, libxinerama, libxrandr"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="LUA=lua54"
 
