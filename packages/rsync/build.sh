2f3f616529c9cd70e342b1fc1188a73669d96090
TomIO <tomjo00@web.de>
2025-01-16 06:34:23 +0100

    bump(main/rsync): 3.4.1 - enable auto updates - update package description - set a maintainer

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -1,11 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://rsync.samba.org/
-TERMUX_PKG_DESCRIPTION="Utility that provides fast incremental file transfer"
+TERMUX_PKG_DESCRIPTION="Fast incremental file transfer utility"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.3.0
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
+TERMUX_PKG_VERSION=3.4.1
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7399e9a6708c32d678a72a63219e96f23be0be2336e50fd1348498d07041df90
+TERMUX_PKG_SHA256=2924bcb3a1ed8b551fc101f740b9f0fe0a202b115027647cf69850d65fd88c52
 TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl, openssl-tool, xxhash, zlib, zstd"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
