ed794e97e937c7ead65eb1009f45009018e019c3
Aditya Alok <alok@termux.dev>
2025-01-16 17:55:59 +0530

    bump(main/tinygo): 0.35.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/tinygo/tinygo-common.subpackage.sh
+++ packages/tinygo/tinygo-common.subpackage.sh
@@ -2,7 +2,6 @@ TERMUX_SUBPKG_DESCRIPTION="TinyGo common files"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 TERMUX_SUBPKG_INCLUDE="
 lib/tinygo/lib
-lib/tinygo/pkg
 lib/tinygo/src
 lib/tinygo/targets
 "
