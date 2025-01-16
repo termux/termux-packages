10e68a57484e1b365b537648996a3025066d593d
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-16 05:46:47 +0000

    bump(main/libsixel): 1.10.5

--- packages/libsixel/build.sh
+++ packages/libsixel/build.sh
@@ -2,19 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://saitoha.github.io/libsixel/
 TERMUX_PKG_DESCRIPTION="Encoder/decoder implementation for DEC SIXEL graphics"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.10.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="1.10.5"
 TERMUX_PKG_SRCURL=https://github.com/libsixel/libsixel/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=028552eb8f2a37c6effda88ee5e8f6d87b5d9601182ddec784a9728865f821e0
+TERMUX_PKG_SHA256=b6654928bd423f92e6da39eb1f40f10000ae2cc6247247fc1882dcff6acbdfc8
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, libcurl, libgd, libjpeg-turbo, libpng"
 TERMUX_PKG_BREAKS="libsixel-dev"
 TERMUX_PKG_REPLACES="libsixel-dev"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dgd=enabled
 -Dgdk-pixbuf2=enabled
 -Djpeg=enabled
 -Dlibcurl=enabled
 -Dpng=enabled
--Dpython2=disabled
+-Dpython=disabled
 "
