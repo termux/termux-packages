e0fdef857ce90167baec069d4bc5f98348da8f01
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-18 05:04:39 +0000

    bump(main/libgee): 0.20.8
    * Enable auto update.
    * Switch to new upstream source link.

--- packages/libgee/build.sh
+++ packages/libgee/build.sh
@@ -2,19 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/Libgee
 TERMUX_PKG_DESCRIPTION="A collection library providing GObject-based interfaces and classes for commonly used data structures"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-_MAJOR_VERSION=0.20
-TERMUX_PKG_VERSION=${_MAJOR_VERSION}.6
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/GNOME/sources/libgee/${_MAJOR_VERSION}/libgee-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=1bf834f5e10d60cc6124d74ed3c1dd38da646787fbf7872220b8b4068e476d4d
+TERMUX_PKG_VERSION="0.20.8"
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libgee/${TERMUX_PKG_VERSION%.*}/libgee-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=189815ac143d89867193b0c52b7dc31f3aa108a15f04d6b5dca2b6adfad0b0ee
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-introspection=yes
+--enable-vala
 "
 
 termux_step_pre_configure() {
-	CFLAGS+=" -Wno-incompatible-function-pointer-types"
 	termux_setup_gir
 }
