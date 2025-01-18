b2982b8b681bc9d5824f321540e8134208f5bc15
Henrik Grimler <grimler@termux.dev>
2025-01-18 21:32:45 +0100

    fix(main/electrum): sync dependency list with electrum's requirements.txt
    And disable auto updates. The auto updater only check if package
    builds, but not if the python dependencies are installed without
    issues. At the very least the python dependencies should be compared
    with the requirements.txt file in the upstream source code.  Disable
    auto updates for now to not run into similar issues in the future.

--- packages/electrum/build.sh
+++ packages/electrum/build.sh
@@ -3,12 +3,16 @@ TERMUX_PKG_DESCRIPTION="Electrum is a lightweight Bitcoin wallet"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.5.8"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.electrum.org/$TERMUX_PKG_VERSION/Electrum-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=dd8595a138132dee87cee76ce760a1d622fc2fd65d3b6ac7df7e53b7fb6ea7e8
-TERMUX_PKG_AUTO_UPDATE=true
+# The python dependency list should be compared to
+# contrib/requirements/requirements.txt in upstream project on every
+# update (or at least every major update).  Disable auto updates for
+# now.
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="python, libsecp256k1, python-pip, python-cryptography"
-TERMUX_PKG_PYTHON_TARGET_DEPS="'qrcode', 'protobuf<4,>=3.20', 'qdarkstyle>=2.7', 'aiorpcx<0.23,>=0.22.0', 'aiohttp<4.0.0,>=3.3.0', 'aiohttp_socks>=0.3', 'certifi', 'bitstring', 'attrs>=19.2.0', 'dnspython>=2.0', 'jsonpatch'"
+TERMUX_PKG_PYTHON_TARGET_DEPS="'qrcode', 'protobuf<4,>=3.20', 'qdarkstyle>=2.7', 'aiorpcx<0.24,>=0.22.0', 'aiohttp<4.0.0,>=3.3.0', 'aiohttp_socks>=0.8.4', 'certifi', 'attrs>=20.1.0', 'jsonpatch', 'dnspython>=2.0'"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 # asciinema previously contained some files that python packages have in common
