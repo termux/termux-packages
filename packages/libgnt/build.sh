d10f61eb1b389f2f44a809bdcc292fb043fae34d
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-18 05:38:22 +0000

    bump(main/libgnt): 2.14.4

--- packages/libgnt/build.sh
+++ packages/libgnt/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://keep.imfreedom.org/libgnt/libgnt
 TERMUX_PKG_DESCRIPTION="An ncurses toolkit for creating text-mode graphical user interfaces in a fast and easy way"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.14.3
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/pidgin/libgnt/${TERMUX_PKG_VERSION}/libgnt-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=57f5457f72999d0bb1a139a37f2746ec1b5a02c094f2710a339d8bcea4236123
+TERMUX_PKG_VERSION="2.14.4"
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/pidgin/libgnt/${TERMUX_PKG_VERSION}/libgnt-${TERMUX_PKG_VERSION}-dev.tar.xz
+TERMUX_PKG_SHA256=195933a9a731d3575791b881ba5cc0ad2a715e1e9c4c23ccaaa2a17e164c96ec
 TERMUX_PKG_DEPENDS="glib, libxml2, ncurses, ncurses-ui-libs"
 TERMUX_PKG_BUILD_DEPENDS="glib-cross"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Ddoc=false -Dpython2=false"
