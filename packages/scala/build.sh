fc7ca1e7dc99d7389208729c923a1d4b62787afe
Kevin Williams <admin@utermux.dev>
2025-01-18 11:11:18 +0800

    fix(main/scala): missing softlink and depends
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/scala/build.sh
+++ packages/scala/build.sh
@@ -4,8 +4,9 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.6.2"
 TERMUX_PKG_SRCURL=https://github.com/lampepfl/dotty/releases/download/$TERMUX_PKG_VERSION/scala3-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9525b93f8b9488330ecbdb85df3046d3ef46c6760ac23248902c4d89194c5206
-TERMUX_PKG_DEPENDS="openjdk-17"
+TERMUX_PKG_SHA256=690f278f88ff8465dd2e8cd314d6c507ad79fef9533e89051f858c8192b7856d
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_DEPENDS="openjdk-17, which"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,6 +16,7 @@ termux_step_make_install() {
 	rm -rf $TERMUX_PREFIX/opt/scala
 	mkdir -p $TERMUX_PREFIX/opt/scala
 	cp -r ./* $TERMUX_PREFIX/opt/scala/
+	ln -sfr $TERMUX_PREFIX/opt/scala/libexec/scala-cli.jar $TERMUX_PREFIX/opt/scala/bin/scala-cli.jar
 	for i in $TERMUX_PREFIX/opt/scala/bin/*; do
 		if [ ! -f "$i" ]; then
 			continue
