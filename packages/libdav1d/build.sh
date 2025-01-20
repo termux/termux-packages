843362c2967046c251f620ecefa252c22b62d81a
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-20 07:46:55 +0000

    bump(main/libdav1d): 1.5.1
    Also enable building dav1d program.

--- packages/libdav1d/build.sh
+++ packages/libdav1d/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://code.videolan.org/videolan/dav1d/
 TERMUX_PKG_DESCRIPTION="AV1 cross-platform decoder focused on speed and correctness"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.5.0"
+TERMUX_PKG_VERSION="1.5.1"
 TERMUX_PKG_SRCURL=https://code.videolan.org/videolan/dav1d/-/archive/${TERMUX_PKG_VERSION}/dav1d-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a6ca64e34cec56ae1c2d359e1da5c5386ecd7a3a62f931d026ac4f2ff72ade64
+TERMUX_PKG_SHA256=4eddffd108f098e307b93c9da57b6125224dc5877b1b3d157b31be6ae8f1f093
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Denable_tools=false
+-Denable_tools=true
 -Denable_tests=false
 "
 
