99d695a605b1f0d55e8c5791c1477e6f108a5c3f
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-21 19:56:41 +0000

    bump(main/plzip): 1.12
    Change URLs to https.

--- packages/plzip/build.sh
+++ packages/plzip/build.sh
@@ -1,8 +1,9 @@
-TERMUX_PKG_HOMEPAGE=http://www.nongnu.org/lzip/plzip.html
+TERMUX_PKG_HOMEPAGE=https://www.nongnu.org/lzip/plzip.html
 TERMUX_PKG_DESCRIPTION="A massively parallel lossless data compressor based on the lzlib compression library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.11"
-TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/plzip/plzip-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=d8e3cbe45c9222383339130e1bcc6e5e884d776b63f188896e6df67bc1d5626b
+TERMUX_PKG_VERSION="1.12"
+TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/lzip/plzip/plzip-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=50d71aad6fa154ad8c824279e86eade4bcf3bb4932d757d8f281ac09cfadae30
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, lzlib"
