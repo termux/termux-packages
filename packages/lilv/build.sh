fdd79050e9567cea2a08e12baee1b5bdf57912d9
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-21 19:40:54 +0000

    bump(main/lilv): 0.24.26

--- packages/lilv/build.sh
+++ packages/lilv/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://drobilla.net/software/lilv.html
 TERMUX_PKG_DESCRIPTION="A C library to make the use of LV2 plugins as simple as possible for applications"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.24.24"
+TERMUX_PKG_VERSION="0.24.26"
 TERMUX_PKG_SRCURL=https://download.drobilla.net/lilv-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6bb6be9f88504176d0642f12de809b2b9e2dc55621a68adb8c7edb99aefabb4f
+TERMUX_PKG_SHA256=22feed30bc0f952384a25c2f6f4b04e6d43836408798ed65a8a934c055d5d8ac
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libsndfile, lv2, serd, sord, sratom"
+TERMUX_PKG_DEPENDS="libsndfile, libzix, lv2, serd, sord, sratom"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dbindings_py=disabled
 "
