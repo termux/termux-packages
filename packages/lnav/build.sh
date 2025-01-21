8a6e07d4152869a47f1e47789e4082317b87652d
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-21 15:16:48 +0000

    bump(main/lnav): 0.12.3

--- packages/lnav/build.sh
+++ packages/lnav/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://lnav.org/
 TERMUX_PKG_DESCRIPTION="An advanced log file viewer for the small-scale"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.12.2"
+TERMUX_PKG_VERSION="0.12.3"
 TERMUX_PKG_SRCURL=https://github.com/tstack/lnav/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9f12e17f9f2b2f1ddf00ef3f35f61ab3c9709ddf2beaa0eeaf441462e812ca7f
+TERMUX_PKG_SHA256=7ba030e5da5b59cd9ea7c0d82aef3af1a6ee1969cab6e22e4bb0f37a96005c7a
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libandroid-utimes, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-system-paths
 --disable-static
