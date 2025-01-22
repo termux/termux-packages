1be31d84f0227f635f9476f8453a29397e48eba5
Aditya Alok <alok@termux.dev>
2025-01-22 16:47:19 +0530

    enhance(main/glm): Package docs
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/glm/build.sh
+++ packages/glm/build.sh
@@ -8,3 +8,8 @@ TERMUX_PKG_SRCURL=https://github.com/g-truc/glm/archive/${TERMUX_PKG_VERSION}.ta
 TERMUX_PKG_SHA256=9f3174561fd26904b23f0db5e560971cbf9b3cbda0b280f04d5c379d03bf234c
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DGLM_BUILD_TESTS=OFF"
 TERMUX_PKG_NO_STATICSPLIT=true
+
+termux_step_post_make_install() {
+	mkdir -p "$TERMUX_PREFIX"/share/doc/glm
+	cp -r "$TERMUX_PKG_SRCDIR"/doc "$TERMUX_PREFIX"/share/doc/glm
+}
