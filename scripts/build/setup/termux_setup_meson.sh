21422decd692f7185d46cd977c7465af20eb846a
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-22 16:29:45 +0800

    scripts(termux_setup_meson): add rust

--- scripts/build/setup/termux_setup_meson.sh
+++ scripts/build/setup/termux_setup_meson.sh
@@ -57,6 +57,7 @@ termux_setup_meson() {
 	echo "ld = '$LD'" >> $TERMUX_MESON_CROSSFILE
 	echo "pkg-config = '$PKG_CONFIG'" >> $TERMUX_MESON_CROSSFILE
 	echo "strip = '$STRIP'" >> $TERMUX_MESON_CROSSFILE
+	echo "rust = ['rustc', '--target', '$CARGO_TARGET_NAME']" >> $TERMUX_MESON_CROSSFILE
 
 	if [ "$TERMUX_PACKAGE_LIBRARY" = "bionic" ]; then
 		echo '' >> $TERMUX_MESON_CROSSFILE
