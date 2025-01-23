fc97c06fa25051da4e74d9d654c1dce34aff6971
Kevin Williams <admin@utermux.dev>
2025-01-23 11:46:21 +0800

    fix(main/sox): should depend on wget
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/sox/build.sh
+++ packages/sox/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Command line utility for converting between and applying
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="14.4.2"
-TERMUX_PKG_REVISION=26
+TERMUX_PKG_REVISION=27
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/sox/sox/${TERMUX_PKG_VERSION}/sox-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=81a6956d4330e75b5827316e44ae381e6f1e8928003c6aa45896da9041ea149c
-TERMUX_PKG_DEPENDS="file, libandroid-glob, libao, libflac, libiconv, libid3tag, libltdl, libmad, libmp3lame, libogg, libpng, libsndfile, libvorbis, opusfile, pulseaudio, zlib"
+TERMUX_PKG_DEPENDS="file, libandroid-glob, libao, libflac, libiconv, libid3tag, libltdl, libmad, libmp3lame, libogg, libpng, libsndfile, libvorbis, opusfile, pulseaudio, zlib, wget"
 TERMUX_PKG_BREAKS="sox-dev"
 TERMUX_PKG_REPLACES="sox-dev"
 
