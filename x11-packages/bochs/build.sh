94cc067ebad7dc646b059cb6b5edf6bdf9dd295f
Aditya Alok <alok@termux.dev>
2025-01-24 17:25:39 +0530

    bump(x11/bochs): 2.8
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- x11-packages/bochs/build.sh
+++ x11-packages/bochs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://bochs.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Bochs is a highly portable open source IA-32 (x86) PC emulator and debugger written in C++."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.7
+TERMUX_PKG_VERSION=2.8
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a010ab1bfdc72ac5a08d2e2412cd471c0febd66af1d9349bc0d796879de5b17a
+TERMUX_PKG_SHA256=a85b13aff7d8411f7a9f356ba6c33b5f5dc1fbb107eb5018cc23a62639da0059
 TERMUX_PKG_DEPENDS="glib, gtk2, libc++, libx11, libxpm, libxrandr, ncurses, pango, readline"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -22,6 +22,7 @@ ac_cv_func_strtouq=no
 --enable-avx
 --enable-usb
 --enable-usb-ehci
+--enable-usb-ohci
 --enable-ne2000
 --enable-e1000
 --enable-clgd54xx
