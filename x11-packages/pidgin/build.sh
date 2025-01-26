6bf2f802e43efba5a885dcb7596a00b82879bb97
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-26 14:41:58 +0000

    bump(x11/pidgin): 2.14.14

--- x11-packages/pidgin/build.sh
+++ x11-packages/pidgin/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Multi-protocol instant messaging client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # please sync version and patches with packages/finch
-TERMUX_PKG_VERSION="2.14.13"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="2.14.14"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pidgin/pidgin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=120049dc8e17e09a2a7d256aff2191ff8491abb840c8c7eb319a161e2df16ba8
+TERMUX_PKG_SHA256=0ffc9994def10260f98a55cd132deefa8dc4a9835451cc0e982747bd458e2356
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, dbus, dbus-glib, fontconfig, freetype, gdk-pixbuf, glib, gst-plugins-base, gstreamer, gtk2, harfbuzz, libcairo, libgnt, libgnutls, libice, libidn, libsasl, libsm, libx11, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxinerama, libxml2, libxrandr, libxrender, libxss, ncurses, pango, tcl, tk"
 TERMUX_PKG_CONFLICTS="finch"
 TERMUX_PKG_REPLACES="finch"
