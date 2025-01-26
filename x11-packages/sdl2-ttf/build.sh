46aad37ac89be7fa3f41b0eddb377f67386ed63b
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-26 18:00:44 +0800

    chore(x11/sdl2-ttf): sdl2-compat co-exist

--- x11-packages/sdl2-ttf/build.sh
+++ x11-packages/sdl2-ttf/build.sh
@@ -4,11 +4,13 @@ TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.24.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_ttf/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_ttf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0b2bf1e7b6568adbdbc9bb924643f79d9dedafe061fa1ed687d1d9ac4e453bfd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/.*-//'
-TERMUX_PKG_DEPENDS="freetype, opengl, sdl2"
+TERMUX_PKG_DEPENDS="freetype, opengl, sdl2 | sdl2-compat"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="sdl2-compat"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-freetype-builtin
 --disable-harfbuzz
