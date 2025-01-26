76af3078f89e5d14f0988591bbb839cf79b71fac
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-26 17:56:55 +0800

    chore(x11/sdl2-mixer): sdl2-compat co-exist

--- x11-packages/sdl2-mixer/build.sh
+++ x11-packages/sdl2-mixer/build.sh
@@ -4,11 +4,13 @@ TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.8.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_mixer/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_mixer-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1cfb34c87b26dbdbc7afd68c4f545c0116ab5f90bbfecc5aebe2a9cb4bb31549
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/.*-//'
-TERMUX_PKG_DEPENDS="fluidsynth, libflac, libmodplug, libvorbis, mpg123, opusfile, sdl2"
+TERMUX_PKG_DEPENDS="fluidsynth, libflac, libmodplug, libvorbis, mpg123, opusfile, sdl2 | sdl2-compat"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="sdl2-compat"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-music-mod-modplug-shared
 --disable-music-midi-fluidsynth-shared
