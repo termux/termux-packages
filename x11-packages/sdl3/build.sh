bb2010da0e83b40e8e48fa21f878a4e6d34f0f54
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-26 10:15:42 +0800

    addpkg(x11): sdl3

---
+++ x11-packages/sdl3/build.sh
@@ -0,0 +1,39 @@
+TERMUX_PKG_HOMEPAGE=https://www.libsdl.org
+TERMUX_PKG_DESCRIPTION="Simple DirectMedia Layer 3"
+TERMUX_PKG_LICENSE="ZLIB"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="3.2.0"
+TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL/releases/download/release-${TERMUX_PKG_VERSION}/SDL3-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=bf308f92c5688b1479faf5cfe24af72f3cd4ce08d0c0670d6ce55bc2ec1e9a5e
+TERMUX_PKG_DEPENDS="libandroid-shmem, libdecor, libiconv, libwayland, libx11, libxcursor, libxext, libxfixes, libxi, libxkbcommon, libxrandr, libxss, pulseaudio"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
+TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DSDL_INSTALL_TESTS=OFF
+-DSDL_TESTS=OFF
+"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=0
+
+	local v=$(sed -En 's/.*SDL_SO_VERSION_MAJOR "([0-9]+)".*/\1/p' CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed: ${v} != ${_SOVERSION}"
+	fi
+}
+
+termux_step_pre_configure() {
+	cp -fr "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_TMPDIR}/a"
+	find "${TERMUX_PKG_SRCDIR}" -type f -print0 | xargs -0r -n1 sed -i \
+		-e 's/\([^A-Za-z0-9_]__ANDROID\)\(__[^A-Za-z0-9_]\)/\1_NO_TERMUX\2/g' \
+		-e 's/\([^A-Za-z0-9_]__ANDROID\)__$/\1_NO_TERMUX__/g'
+	cp -fr "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_TMPDIR}/b"
+	echo "INFO: Modified files:"
+	diff -uNr "${TERMUX_PKG_TMPDIR}"/{a,b} --color || :
+
+	LDFLAGS+=" -landroid-shmem"
+}
