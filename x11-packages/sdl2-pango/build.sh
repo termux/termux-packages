752d0f68e29cdbe597ede9d5dc5e72154461d0c3
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-26 17:59:23 +0800

    chore(x11/sdl2-pango): sdl2-compat co-exist

--- x11-packages/sdl2-pango/build.sh
+++ x11-packages/sdl2-pango/build.sh
@@ -4,10 +4,12 @@ TERMUX_PKG_DESCRIPTION="SDL2 library for internationalized text rendering"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.1.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/tuxpaint-sdl2/SDL2_Pango/SDL2_Pango-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=https://github.com/markuskimius/SDL2_Pango/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3b3fe6008445feb1fca61b17b9d950d688e49dcca60dbbf8667c0f980ddfc563
-TERMUX_PKG_DEPENDS="glib, pango, sdl2"
+TERMUX_PKG_DEPENDS="glib, pango, sdl2 | sdl2-compat"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="sdl2-compat"
 
 termux_step_pre_configure() {
 	mkdir -p m4
