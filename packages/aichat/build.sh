69c149c1dec71f871fc516fc92536398f55f6f81
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-29 13:12:25 +0000

    bump(main/aichat): 0.27.0

--- packages/aichat/build.sh
+++ packages/aichat/build.sh
@@ -3,18 +3,18 @@ TERMUX_PKG_DESCRIPTION="A powerful chatgpt cli"
 TERMUX_PKG_LICENSE="Apache-2.0,MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE-APACHE,LICENSE-MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.26.0"
+TERMUX_PKG_VERSION="0.27.0"
 TERMUX_PKG_SRCURL=https://github.com/sigoden/aichat/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1743534a68acb9fdf4fee11cd2b38cc5282c85debf89a4052b61a09dcaf51185
+TERMUX_PKG_SHA256=eb97284be786b2b625992ab875bb95da2aa3707c063cb0c34521cb01024b38cd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 # This package contains makefiles to run the tests. So, we need to override build steps.
 termux_step_make() {
 	termux_setup_rust
-	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+	cargo build --jobs "$TERMUX_PKG_MAKE_PROCESSES" --target "$CARGO_TARGET_NAME" --release
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/aichat
+	install -Dm700 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/aichat"
 }
