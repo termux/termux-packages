d37bf5d16802066dd75e3c97d0462dcb453f4d7d
Aditya Alok <alok@termux.dev>
2025-01-30 03:10:20 +0530

    bump(x11/hugin): 2024.0.1
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- x11-packages/hugin/build.sh
+++ x11-packages/hugin/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://hugin.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Toolchain to create panoramic images for every occasion"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2023.0.0"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="2024.0.1"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/hugin/hugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=04a39fccc6017e0544c639bd22309472c574d35b3455cbade1fc3870e631632b
+TERMUX_PKG_SHA256=13ec0cdeeb4eb63149c833768d3e374e7cf5a6a8d8d02d50885ce496f05ba7e4
 TERMUX_PKG_DEPENDS="boost, enblend, exiftool, exiv2, fftw, glew, glu, imath, libc++, libflann, liblz4, libpano13, libsqlite, libtiff, libvigra, libx11, littlecms, openexr, opengl, wxwidgets"
 # libjpeg-turbo, libpng and zlib are detected but not linked against
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, libjpeg-turbo, libpng, zlib"
