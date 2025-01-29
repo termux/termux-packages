79bc3ee74b269313ce56e53fa95cddcec0f951bc
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-29 14:40:31 +0000

    bump(x11/sway): 1.10.1
    Enable man pages.

--- x11-packages/sway/build.sh
+++ x11-packages/sway/build.sh
@@ -2,12 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://github.com/swaywm/sway
 TERMUX_PKG_DESCRIPTION="i3-compatible Wayland compositor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.10"
+TERMUX_PKG_VERSION="1.10.1"
 TERMUX_PKG_SRCURL=https://github.com/swaywm/sway/releases/download/$TERMUX_PKG_VERSION/sway-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=7472a7f11150212e0bed0bd0af9f591c9caac9f9ea87c35486e475a21f5ce81f
+TERMUX_PKG_SHA256=b2fbf3a2f94c8926efa18d6af59bb9f5f1eafa6d46491284b1610c57bef2d105
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gdk-pixbuf, json-c, libandroid-wordexp, libcairo, libevdev, libwayland, pango, pcre2, wlroots (>= 0.18.1)"
-TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, scdoc"
 TERMUX_PKG_RECOMMENDS="xwayland"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dman-pages=enabled
+"
 
 termux_step_pre_configure() {
 	export PATH="$TERMUX_PREFIX/opt/libwayland/cross/bin:$PATH"
