1aeb52fc1cd829accaf4e944d72ecca571fbe593
Aditya Alok <alok@termux.dev>
2025-01-30 00:20:28 +0530

    revbump(main/libc++utilities): After boost update
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/libc++utilities/build.sh
+++ packages/libc++utilities/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Useful C++ classes and routines such as argument parser,
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.27.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Martchus/cpp-utilities/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ec1e88177f8541c73ba75c6b46cc610046f77ff2a00ff961f8f9bb5e6ab13dcf
 TERMUX_PKG_AUTO_UPDATE=true
