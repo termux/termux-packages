297c4160ecd8581584918749cf37d0226ebd1312
Aditya Alok <alok@termux.dev>
2025-01-30 17:41:33 +0530

    bump(main/libarrow-cpp): 19.0.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/libarrow-cpp/build.sh
+++ packages/libarrow-cpp/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="C++ libraries for Apache Arrow"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Align the version with `python-pyarrow` package.
-TERMUX_PKG_VERSION="18.1.0"
+TERMUX_PKG_VERSION="19.0.0"
 TERMUX_PKG_SRCURL=https://github.com/apache/arrow/archive/refs/tags/apache-arrow-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=026ecabd74f7b075f6c74e5448132ba40f35688a29d07616bcc1bd976676706c
+TERMUX_PKG_SHA256=7bee51bb6c1176eb08070bd2c7fb7e9e4d17f277e59c9cf80a88082443b124de
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_DEPENDS="abseil-cpp, apache-orc, libandroid-execinfo, libc++, liblz4, libprotobuf, libre2, libsnappy, thrift, utf8proc, zlib, zstd"
@@ -22,6 +22,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DARROW_PARQUET=ON
 -DARROW_RUNTIME_SIMD_LEVEL=NONE
 -DARROW_SIMD_LEVEL=NONE
+-DLZ4_HOME=$TERMUX_PREFIX
+-DSNAPPY_HOME=$TERMUX_PREFIX
+-DZLIB_HOME=$TERMUX_PREFIX
+-DZSTD_HOME=$TERMUX_PREFIX
 "
 
 termux_step_pre_configure() {
