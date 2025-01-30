26c928ad9aad14536f7c32c926eb79b737233b67
Chongyun Lee <licy183@termux.dev>
2025-01-30 14:03:30 +0800

    libandroid-stub: ship `libmediandk.so`

--- packages/libandroid-stub/build.sh
+++ packages/libandroid-stub/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # Version should be equal to TERMUX_NDK_{VERSION_NUM,REVISION} in
 # scripts/properties.sh
 TERMUX_PKG_VERSION=27c
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://dl.google.com/android/repository/android-ndk-r${TERMUX_PKG_VERSION}-linux.zip
 TERMUX_PKG_SHA256=59c2f6dc96743b5daf5d1626684640b20a6bd2b1d85b13156b90333741bad5cc
 TERMUX_PKG_AUTO_UPDATE=false
@@ -22,6 +23,7 @@ termux_step_get_source() {
 		local lib_path="toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${TERMUX_HOST_PLATFORM}/${TERMUX_PKG_API_LEVEL}"
 		mkdir -p "$TERMUX_PKG_SRCDIR"/"$lib_path"
 		cp "$NDK"/"$lib_path"/libandroid.so "$TERMUX_PKG_SRCDIR"/"$lib_path"
+		cp "$NDK"/"$lib_path"/libmediandk.so "$TERMUX_PKG_SRCDIR"/"$lib_path"
 	fi
 }
 
@@ -29,6 +31,9 @@ termux_step_post_make_install() {
 	install -v -Dm644 \
 		"toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${TERMUX_HOST_PLATFORM}/${TERMUX_PKG_API_LEVEL}/libandroid.so" \
 		"${TERMUX_PREFIX}/lib/libandroid.so"
+	install -v -Dm644 \
+		"toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/${TERMUX_HOST_PLATFORM}/${TERMUX_PKG_API_LEVEL}/libmediandk.so" \
+		"${TERMUX_PREFIX}/lib/libmediandk.so"
 }
 
 # Please do NOT depend on this package; do NOT include this package in
