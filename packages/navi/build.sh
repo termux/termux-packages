599c9ff7e6a8df8f11686741a9ffd2d5b374bc86
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-31 19:15:38 +0000

    bump(main/navi): 2.24.0

--- packages/navi/build.sh
+++ packages/navi/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/denisidoro/navi
 TERMUX_PKG_DESCRIPTION="An interactive cheatsheet tool for the command-line"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.23.0"
+TERMUX_PKG_VERSION="2.24.0"
 TERMUX_PKG_SRCURL=https://github.com/denisidoro/navi/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=579a72814e7ba07dae697a58dc13b0f7d853532ec07229aff07a11e5828f3799
+TERMUX_PKG_SHA256=4c10f47c306826255b07483b7e94eed8ffc1401555c52434a56246295d3f2728
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="fzf, git"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -15,6 +15,6 @@ termux_step_pre_configure() {
 
 termux_step_make_install() {
 	termux_setup_rust
-	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/navi
+	cargo build --jobs "$TERMUX_PKG_MAKE_PROCESSES" --target "$CARGO_TARGET_NAME" --release
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/navi"
 }
