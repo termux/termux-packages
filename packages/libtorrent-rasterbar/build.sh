3f9183b5f2cf0f3384180f7d84dca418a37a6c15
Biswapriyo Nath <nathbappai@gmail.com>
2025-01-31 18:42:42 +0000

    bump(main/libtorrent-rasterbar): 2.0.11
    Remove 7649.patch file which was added in upstream.
    https://github.com/arvidn/libtorrent/commit/31003b7c8d08ba2e4b5956f3e2c0cfab6da7bdd5

--- packages/libtorrent-rasterbar/build.sh
+++ packages/libtorrent-rasterbar/build.sh
@@ -2,15 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://libtorrent.org/
 TERMUX_PKG_DESCRIPTION="A feature complete C++ bittorrent implementation focusing on efficiency and scalability"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.0.10"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.0.11"
 TERMUX_PKG_SRCURL=https://github.com/arvidn/libtorrent/releases/download/v${TERMUX_PKG_VERSION}/libtorrent-rasterbar-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fc935b8c1daca5c0a4d304bff59e64e532be16bb877c012aea4bda73d9ca885d
+TERMUX_PKG_SHA256=f0db58580f4f29ade6cc40fa4ba80e2c9a70c90265cd77332d3cdec37ecf1e6d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="boost, libc++, openssl, python"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers"
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dboost-python-module-name=python
 -Dpython-bindings=ON
