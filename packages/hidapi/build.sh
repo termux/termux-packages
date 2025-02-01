7c9d62d84d0c362ccb51ba0a59c6ce7eb4413e7e
Henrik Grimler <grimler@termux.dev>
2025-02-01 14:34:48 +0100

    addpkg(main/hidapi): 0.14.0
    hidapi can be used with libusb, or the kernel driver "hidraw" (likely
    only relevant for rooted devices), to access HID devices. Goal is to
    patch it further and use it together with termux-usb.
    
    Add it now for testing and development purposes.

---
+++ packages/hidapi/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://libusb.info/hidapi
+TERMUX_PKG_DESCRIPTION="Simple cross-platform library for communicating with HID devices"
+TERMUX_PKG_LICENSE="GPL-3.0, BSD 3-Clause, custom"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt, LICENSE-gpl3.txt, LICENSE-bsd.txt, LICENSE-orig.txt"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.14.0
+TERMUX_PKG_SRCURL="https://github.com/libusb/hidapi/archive/refs/tags/hidapi-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=a5714234abe6e1f53647dd8cba7d69f65f71c558b7896ed218864ffcf405bcbd
+TERMUX_PKG_DEPENDS="libiconv, libusb"
