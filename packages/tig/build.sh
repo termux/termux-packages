c7187fdb24c259c735bf179da6792e153f556c5c
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-03 13:36:03 +0000

    bump(main/tig): 2.5.11

--- packages/tig/build.sh
+++ packages/tig/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://jonas.github.io/tig/
 TERMUX_PKG_DESCRIPTION="Ncurses-based text-mode interface for git"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.5.10"
+TERMUX_PKG_VERSION="2.5.11"
 TERMUX_PKG_SRCURL=https://github.com/jonas/tig/releases/download/tig-$TERMUX_PKG_VERSION/tig-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f655cc1366fc10058a2bd505bb88ca78e653ff7526c1b81774c44b9d841210e3
+TERMUX_PKG_SHA256=b36ef52b1a19d87257adfa9047cc3e21d6bd528bbd28458fe49003eb3e374aa7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="libiconv, ncurses, git, libandroid-support"
+TERMUX_PKG_DEPENDS="git, libandroid-support, libiconv, ncurses, pcre2"
 
 termux_step_post_make_install() {
 	make -j 1 install-doc
