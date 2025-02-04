468073a34e0f0559503972e8a307e784cd8730f7
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-04 18:25:26 +0000

    bump(main/cloudflared): 2025.1.1

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2025.1.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2025.1.1"
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f9223cdefaa4b75aa1c49638936af9d5007b6c7bd943ab70203bb75bf32467da
+TERMUX_PKG_SHA256=37913efe6b7aa3f84fbffc53f0f4a02c6e4fa587049c422eb27fdc452b624725
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -18,5 +17,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin cloudflared
+	install -Dm700 -t "$TERMUX_PREFIX"/bin cloudflared
 }
