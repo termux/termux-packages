e2391bc5bcbcf4335ab2c0d384cbf602cd543f73
Aditya Alok <alok@termux.dev>
2025-02-05 21:15:41 +0530

    bump(main/cloudflared): 2025.2.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2025.1.1"
+TERMUX_PKG_VERSION="2025.2.0"
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=37913efe6b7aa3f84fbffc53f0f4a02c6e4fa587049c422eb27fdc452b624725
+TERMUX_PKG_SHA256=54de1e3bfb3ebcddd8bdc70837b6e91cdd2221673117356c022d0ca3376dcca4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
