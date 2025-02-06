2385f5826dcab83c663302005dec440b640d74cf
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-06 14:11:40 +0000

    bump(main/neocmakelsp): 0.8.18

--- packages/neocmakelsp/build.sh
+++ packages/neocmakelsp/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Decodetalkers/neocmakelsp
 TERMUX_PKG_DESCRIPTION="a cmake lsp based on tower-lsp and treesitter"
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER=@termux
-TERMUX_PKG_VERSION="0.8.16"
+TERMUX_PKG_VERSION="0.8.18"
 TERMUX_PKG_SRCURL=https://github.com/Decodetalkers/neocmakelsp/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=fd533468e0ab23977243d4923715dc010ddd9d299f8a3d21c1252cfc1ee009b9
+TERMUX_PKG_SHA256=41bc9334cc99abb92a1879a240268ffb176423cebddd4db39d9f40e932926adb
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
