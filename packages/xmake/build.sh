034846d3d6fc6ed60a14edd6b2faffa6633d234b
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-06 19:02:55 +0000

    bump(main/xmake): 2.9.8

--- packages/xmake/build.sh
+++ packages/xmake/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://xmake.io/
 TERMUX_PKG_DESCRIPTION="A cross-platform build utility based on Lua"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Ruki Wang @waruqi"
-TERMUX_PKG_VERSION="2.9.7"
+TERMUX_PKG_VERSION="2.9.8"
 TERMUX_PKG_SRCURL=https://github.com/xmake-io/xmake/releases/download/v${TERMUX_PKG_VERSION}/xmake-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=248e496a5a734c89e167f931e2620c0ea4109e7ca913dc1022735129a5ffe9d3
+TERMUX_PKG_SHA256=e797636aadf072c9b0851dba39b121e93c739d12d78398c91f12e8ed355d6a95
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure () {
-	$TERMUX_PKG_SRCDIR/configure --prefix=$TERMUX_PREFIX
+	"$TERMUX_PKG_SRCDIR"/configure --prefix="$TERMUX_PREFIX"
 }
