04d3fad488667a607378b91e528a6f8d8b24f356
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-07 05:27:26 +0000

    bump(main/entr): 5.7

--- packages/entr/build.sh
+++ packages/entr/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://eradman.com/entrproject/
+TERMUX_PKG_HOMEPAGE=https://eradman.com/entrproject/
 TERMUX_PKG_DESCRIPTION="Event Notify Test Runner - run arbitrary commands when files change"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.6"
-TERMUX_PKG_SRCURL=https://eradman.com/entrproject/code/entr-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=0222b8df928d3b5a3b5194d63e7de098533e04190d9d9a154b926c6c1f9dd14e
+TERMUX_PKG_VERSION="5.7"
+TERMUX_PKG_SRCURL=https://eradman.com/entrproject/code/entr-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=90c5d943820c70cef37eb41a382a6ea4f5dd7fd95efef13b2b5520d320f5d067
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
