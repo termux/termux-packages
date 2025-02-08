45c05023e443bbe8f8df06281c0fccc9e9355260
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-08 10:35:31 +0000

    bump(main/vulkan-extension-layer): 1.4.307

--- packages/vulkan-extension-layer/build.sh
+++ packages/vulkan-extension-layer/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-ExtensionLayer
 TERMUX_PKG_DESCRIPTION="Vulkan Extension Layer"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.306"
+TERMUX_PKG_VERSION="1.4.307"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-ExtensionLayer/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a13afff9c07271c32899826b4cdbb29b5e2b94c3435e0a196071ce66bee377a7
+TERMUX_PKG_SHA256=ffdd74166618e051123cccf9d7fbe1785587324cf51392d0daeb5774c3bb0417
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-utility-libraries (=${TERMUX_PKG_VERSION})"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
