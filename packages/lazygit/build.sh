0129fca106d0ba736961538415f078171c6f4eee
drjaska <drjaska83@gmail.com>
2025-02-08 06:12:02 +0200

    Suggest diff-so-fancy for lazygit
    https://github.com/jesseduffield/lazygit/blob/master/docs/Custom_Pagers.md#diff-so-fancy

--- packages/lazygit/build.sh
+++ packages/lazygit/build.sh
@@ -3,10 +3,12 @@ TERMUX_PKG_DESCRIPTION="Simple terminal UI for git commands"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION="0.45.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jesseduffield/lazygit/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dd3d6645ee429f0c554338c1fdb940733793ad915ae72653132664aa7c26bbcb
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_RECOMMENDS=git
+TERMUX_PKG_SUGGESTS=diff-so-fancy
 
 termux_step_make() {
 	termux_setup_golang
