83ec5ee7dae95e90366562cfefc30061a0af4492
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-08 06:48:58 +0000

    bump(main/zk): 0.14.2
    Add variables with make command to fix version output.

--- packages/zk/build.sh
+++ packages/zk/build.sh
@@ -1,12 +1,16 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/zk-org/zk
+TERMUX_PKG_HOMEPAGE=https://zk-org.github.io/zk/
 TERMUX_PKG_DESCRIPTION="A plain text note-taking assistant"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@kirasok"
-TERMUX_PKG_VERSION="0.14.1"
+TERMUX_PKG_VERSION="0.14.2"
 TERMUX_PKG_SRCURL=https://github.com/zk-org/zk/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=563331e1f5a03b4dd3a4ff642cc205cc7b6c3c350c98f627a3273067e7ec234c
+TERMUX_PKG_SHA256=51956ab37595f2c95d97594e1a825d35de9be0c31af2f32f2e2d4468b7b88e0c
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+VERSION=$TERMUX_PKG_VERSION
+BUILD=
+"
 
 termux_step_pre_configure() {
 	termux_setup_golang
@@ -16,5 +20,5 @@ termux_step_pre_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin zk
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin zk
 }
