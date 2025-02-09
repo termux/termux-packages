a0ba86c86aed7b797403ae1124975f1197bd320c
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-09 06:07:17 +0000

    bump(main/libtasn1): 4.20.0
    Remove src-gl-stdio-impl.h.patch file which was added in gnulib.
    https://git.savannah.gnu.org/gitweb/?p=gnulib.git;a=commit;h=d91fe44f159cfe7614d9e1547624579161bd25c5

--- packages/libtasn1/build.sh
+++ packages/libtasn1/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libtasn1/
 TERMUX_PKG_DESCRIPTION="This is GNU Libtasn1, a small ASN.1 library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.19.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="4.20.0"
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libtasn1/libtasn1-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1613f0ac1cf484d6ec0ce3b8c06d56263cc7242f1c23b30d82d23de345a63f7a
+TERMUX_PKG_SHA256=92e0e3bd4c02d4aeee76036b2ddd83f0c732ba4cda5cb71d583272b23587a76c
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	autoreconf -fi
