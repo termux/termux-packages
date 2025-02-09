ffdba6c40a251d77dce4733a8e41c77b704072e2
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-09 04:28:21 +0000

    bump(main/chezmoi): 2.59.1

--- packages/chezmoi/build.sh
+++ packages/chezmoi/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://chezmoi.io
 TERMUX_PKG_DESCRIPTION="Manage your dotfiles across multiple machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION="2.59.0"
+TERMUX_PKG_VERSION="2.59.1"
 TERMUX_PKG_SRCURL=https://github.com/twpayne/chezmoi/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=af1244c07ff5f3b04d6a2af186a9f2bdd33044399c94a051b64162f238a6b8bb
+TERMUX_PKG_SHA256=577bce7c9038ca17cda2c61c1ff3df90c4b366b68629e3056e274cf4b319be30
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
