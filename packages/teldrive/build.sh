8be957fd510772ffe70260176562d00db3ee64aa
Your Name <triangle@projectsegfau.lt>
2025-02-11 07:52:02 +0530

    addpkg(main/teldrive): 1.6.12

---
+++ packages/teldrive/build.sh
@@ -0,0 +1,31 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tgdrive/teldrive
+TERMUX_PKG_DESCRIPTION="A high-speed Telegram file management utility"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.6.12"
+TERMUX_PKG_SRCURL=https://github.com/tgdrive/teldrive/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=dbde2663622f2a5c2e21975649e4e00531d4d0d8511932ac5c1f4ea220a82437
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	termux_setup_nodejs
+	git clone https://github.com/tgdrive/teldrive-ui && cd telrive-ui
+	npm install
+	npm run build
+	mv dist ui/dist
+}
+
+termux_step_pre_configure() {
+	termux_setup_golang
+}
+
+termux_step_make() {
+	go build -o teldrive -trimpath -ldflags="-checklinkname=0 -s -w -X github.com/tgdrive/teldrive/internal/version.Version=${TERMUX_PKG_VERSION} -X github.com/tgdrive/teldrive/internal/version.CommitSHA=$(git rev-parse --short HEAD)"
+}
+
+termux_step_make_install() {
+	install -Dm700 -t "${TERMUX_PREFIX}"/bin "$TERMUX_PKG_SRCDIR"/teldrive
+}
+
