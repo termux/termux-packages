38ca424d5c961d358143da4edf82774ba830225b
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-11 10:29:59 +0530

    chore(x11/sdl2-image): sdl2-compat co-exist (#23150)

--- x11-packages/sdl2-image/build.sh
+++ x11-packages/sdl2-image/build.sh
@@ -4,12 +4,14 @@ TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.8.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libsdl-org/SDL_image/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_image-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8bc4c57f41e2c0db7f9b749b253ef6cecdc6f0b689ecbe36ee97b50115fff645
 # Prevent updating to SDL3 version
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/.*-//'
-TERMUX_PKG_DEPENDS="libjpeg-turbo, libjxl, libpng, libtiff, libwebp, sdl2"
+TERMUX_PKG_DEPENDS="libjpeg-turbo, libjxl, libpng, libtiff, libwebp, sdl2 | sdl2-compat"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="sdl2-compat"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-stb-image
 --disable-jpg-shared
