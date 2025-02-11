de90ac0fc96a1f899c1f860489b364ac10ecce41
Your Name <triangle@projectsegfau.lt>
2025-02-11 08:55:05 +0530

    bump(main/seanime): 1.7.2

--- packages/teldrive/build.sh
+++ packages/teldrive/build.sh
@@ -17,7 +17,7 @@ termux_step_host_build() {
 }
 
 termux_step_pre_configure() {
-	cp $TERMUX_PKG_HOSTBUILD_DIR/teldrive-ui/dist $TERMUX_PKG_SRCDIR/ui/
+	cp -r $TERMUX_PKG_HOSTBUILD_DIR/teldrive-ui/dist $TERMUX_PKG_SRCDIR/ui/
 	termux_setup_golang
 }
 
