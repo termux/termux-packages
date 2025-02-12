9f9d81d394817b65f4c4a14a197ef6f30a66002c
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-12 08:55:55 +0000

    bump(main/gnustep-make): 2.9.3

--- packages/gnustep-make/build.sh
+++ packages/gnustep-make/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnustep.org
 TERMUX_PKG_DESCRIPTION="The GNUstep makefile package"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.9.2"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/gnustep/tools-make/archive/make-${TERMUX_PKG_VERSION//./_}.tar.gz
-TERMUX_PKG_SHA256=b3fdee058879675f6979c553fb6172b160ca79ffd0f170f51379326b7922941a
+TERMUX_PKG_VERSION="2.9.3"
+TERMUX_PKG_SRCURL=https://github.com/gnustep/tools-make/archive/refs/tags/make-${TERMUX_PKG_VERSION//./_}.tar.gz
+TERMUX_PKG_SHA256=aad12caecb0398b099f3b8b0282cecc3f01a9f371200641b2e1e535ae6ee2543
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP='(?<=make-)\d+\_\d+\_\d+'
 TERMUX_PKG_DEPENDS="libobjc2"
