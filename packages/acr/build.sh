9f0874c542ce94ee6dee9619c3d4034b1e0a1952
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-12 06:40:24 +0000

    bump(main/acr): 2.2.0

--- packages/acr/build.sh
+++ packages/acr/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/radare/acr
+TERMUX_PKG_HOMEPAGE=https://github.com/radareorg/acr
 TERMUX_PKG_DESCRIPTION="A fully compatible autoconf replacement"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.1.4"
-TERMUX_PKG_SRCURL=https://github.com/radare/acr/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6ab55ddd64963176eb7ffd88b2c63c107ce1208e4a3a9b6239df26a86426e7f1
+TERMUX_PKG_VERSION="2.2.0"
+TERMUX_PKG_SRCURL=https://github.com/radareorg/acr/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=76428cafa16a95114724b63f682466e24c3947076dec5dc6bd99b85aee18fc9d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="dash"
 TERMUX_PKG_BUILD_IN_SRC=true
