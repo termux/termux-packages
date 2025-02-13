46f8c0533f418bfa325b76341c3d95cef9fd19fc
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-13 05:23:12 +0000

    bump(main/diffutils): 3.11

--- packages/diffutils/build.sh
+++ packages/diffutils/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/diffutils/
 TERMUX_PKG_DESCRIPTION="Programs (cmp, diff, diff3 and sdiff) related to finding differences between files"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.10
+TERMUX_PKG_VERSION="3.11"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/diffutils/diffutils-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=90e5e93cc724e4ebe12ede80df1634063c7a855692685919bfe60b556c9bd09e
+TERMUX_PKG_SHA256=a73ef05fe37dd585f7d87068e4a0639760419f810138bd75c61ddaa1f9e2131e
 TERMUX_PKG_DEPENDS="libiconv"
 TERMUX_PKG_ESSENTIAL=true
 
