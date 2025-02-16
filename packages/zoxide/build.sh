82fc7b324381b5cc84e44c889f32281594af6bb3
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-16 13:45:33 +0000

    bump(main/zoxide): 0.9.7
    Makefile is not added anymore.

--- packages/zoxide/build.sh
+++ packages/zoxide/build.sh
@@ -2,15 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ajeetdsouza/zoxide
 TERMUX_PKG_DESCRIPTION="A faster way to navigate your filesystem"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.6"
+TERMUX_PKG_VERSION="0.9.7"
 TERMUX_PKG_SRCURL=https://github.com/ajeetdsouza/zoxide/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e1811511a4a9caafa18b7d1505147d4328b39f6ec88b88097fe0dad59919f19c
+TERMUX_PKG_SHA256=d93ab17a01de68529ef37b599524f882dc654835cc121a9097dd3f2952fd207e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	rm -f ./Makefile
-}
 
 termux_step_post_make_install() {
 	install -Dm644 contrib/completions/zoxide.bash "$TERMUX_PREFIX"/share/bash-completion/completions/zoxide
