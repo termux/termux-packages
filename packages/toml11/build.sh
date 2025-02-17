a3be04150d51d8eb76b8110a537bd3d79690246a
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-17 05:21:58 +0000

    bump(main/toml11): 4.4.0
    Remove unnecessary C++ standard cmake option.

--- packages/toml11/build.sh
+++ packages/toml11/build.sh
@@ -2,10 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ToruNiina/toml11
 TERMUX_PKG_DESCRIPTION="toml11 is a C++11 (or later) header-only toml parser/encoder depending only on C++ standard library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.3.0"
+TERMUX_PKG_VERSION="4.4.0"
 TERMUX_PKG_SRCURL=https://github.com/ToruNiina/toml11/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=af95dab1bbb9b05a597e73d529a7269e13f1869e9ca9bd4779906c5cd96e282b
+TERMUX_PKG_SHA256=815bfe6792aa11a13a133b86e7f0f45edc5d71eb78f5fb6686c49c7f792b9049
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DCMAKE_CXX_STANDARD=11
-"
