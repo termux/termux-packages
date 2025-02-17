ce7b1a94cd2a00674c2c12750d4e473f6b72a4a6
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-17 07:19:37 +0200

    chore(main/mesa): deprecate mesa-vulkan-icd-freedreno-dri3 package...
    ... and replace it with regular mesa-vulkan-icd-freedreno

--- packages/mesa/mesa-vulkan-icd-freedreno.subpackage.sh
+++ packages/mesa/mesa-vulkan-icd-freedreno.subpackage.sh
@@ -2,6 +2,9 @@ TERMUX_SUBPKG_DESCRIPTION="Mesa's Freedreno Vulkan ICD"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, vulkan-loader-generic, zlib, zstd"
 TERMUX_SUBPKG_EXCLUDED_ARCHES="i686, x86_64"
+TERMUX_SUBPKG_REPLACES="mesa-vulkan-icd-freedreno-dri3"
+TERMUX_SUBPKG_BREAKS="mesa-vulkan-icd-freedreno-dri3"
+TERMUX_SUBPKG_PROVIDES="mesa-vulkan-icd-freedreno-dri3"
 TERMUX_SUBPKG_INCLUDE="
 lib/libvulkan_freedreno.so
 share/vulkan/icd.d/freedreno_icd.*.json
