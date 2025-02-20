13154cecc67f1f9a26501ed3f9897ba3eef3872f
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-20 09:54:06 +0000

    bump(x11/xfce4-notifyd): 0.9.7

--- x11-packages/xfce4-notifyd/build.sh
+++ x11-packages/xfce4-notifyd/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/notifyd/start
 TERMUX_PKG_DESCRIPTION="simple, visually-appealing notification daemon for Xfce"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
-TERMUX_PKG_VERSION="0.9.6"
+TERMUX_PKG_VERSION="0.9.7"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-notifyd/${TERMUX_PKG_VERSION%.*}/xfce4-notifyd-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=9e53265cca7d835c31b3c2c0d3ae961704870839ef583dcca3e4cc98ae3d2671
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libnotify, libsqlite, libx11, libxfce4ui, libxfce4util, pango, xfce4-panel, xfconf, zlib"
+TERMUX_PKG_SHA256=170d18fd5f40cce823ffc7ae3d7e21644007c3f45808ab4835f0401d21b3516a
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk-layer-shell, gtk3, harfbuzz, libcairo, libnotify, libsqlite, libx11, libxfce4ui, libxfce4util, pango, xfce4-panel, xfconf, zlib"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
