8252908cfe13f6e608a91cb62f1d06fa0aaabc0b
TomIO <tomjo00@web.de>
2025-02-21 20:05:22 +0100

    bump(main/gopls): 0.18.0

--- packages/gopls/build.sh
+++ packages/gopls/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/golang/tools
 TERMUX_PKG_DESCRIPTION="The official Go language server"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.17.1"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
+TERMUX_PKG_VERSION="0.18.0"
 TERMUX_PKG_SRCURL=https://github.com/golang/tools/archive/refs/tags/gopls/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5794ebd3302ef4fd08de284834b22810dbb17b7e08efeeaa9b96d5c94eb90d6d
+TERMUX_PKG_SHA256=2fec8592c5f2e447cc1671c2addd4fe682e67a4ef0c10e7aa38376eb18378be9
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
@@ -23,10 +23,10 @@ termux_pkg_auto_update() {
 termux_step_make() {
 	termux_setup_golang
 
-	cd $TERMUX_PKG_SRCDIR/gopls
+	cd "$TERMUX_PKG_SRCDIR/gopls"
 	go build -o gopls
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin $TERMUX_PKG_SRCDIR/gopls/gopls
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "$TERMUX_PKG_SRCDIR/gopls/gopls"
 }
