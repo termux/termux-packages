cadb18fe93820ba14bc6b9a7d03430967b67639f
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-21 18:09:22 +0000

    bump(main/scc): 3.5.0

--- packages/scc/build.sh
+++ packages/scc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/boyter/scc
 TERMUX_PKG_DESCRIPTION="Counts physical the lines of code, blank lines, comment lines, and physical lines of source code"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.4.0"
+TERMUX_PKG_VERSION="3.5.0"
 TERMUX_PKG_SRCURL=https://github.com/boyter/scc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bdedb6f32d1c3d73ac7e55780021c742bc8ed32f6fb878ee3e419f9acc76bdaa
+TERMUX_PKG_SHA256=161f5d9bb359c6440114b7d2e0f98d588c02aa66fbe474d7660b244687fefb70
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -17,5 +17,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin scc
+	install -Dm700 -t "$TERMUX_PREFIX"/bin scc
 }
