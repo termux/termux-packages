17763b8a99313c3a1051760350bd0109db9906e5
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-22 19:30:47 +0000

    bump(main/b3sum): 1.6.0

--- packages/b3sum/build.sh
+++ packages/b3sum/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/BLAKE3-team/BLAKE3/tree/master/b3sum
 TERMUX_PKG_DESCRIPTION="A command line utility for calculating BLAKE3 hashes, similar to Coreutils tools like b2sum or md5sum"
 TERMUX_PKG_LICENSE="CC0-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.5.5"
+TERMUX_PKG_VERSION="1.6.0"
 TERMUX_PKG_SRCURL=https://github.com/BLAKE3-team/BLAKE3/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6feba0750efc1a99a79fb9a495e2628b5cd1603e15f56a06b1d6cb13ac55c618
+TERMUX_PKG_SHA256=cc6839962144126bc6cc1cde89a50c3bb000b42a93d7e5295b1414d9bdf70c12
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
