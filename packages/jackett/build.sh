0bdb7f32571ecdcd7b7afaae6280c599d406bebc
aicynide <trippintoise@skiff.com>
2025-02-23 17:53:21 +0530

    addpkg(main/jackett): 0.22.1468
    Co-authored-by: Jia Yuan Lo <jylo06g@gmail.com>
    Co-authored-by: TomIO <43716232+TomJo2000@users.noreply.github.com>

---
+++ packages/jackett/build.sh
@@ -0,0 +1,47 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/jackett/jackett"
+TERMUX_PKG_DESCRIPTION="API Support for your favorite torrent trackers"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.22.1646"
+TERMUX_PKG_SRCURL="https://github.com/Jackett/Jackett/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="01a4d29ea06bde030296dfd57f92f2d99044c91dd0cf00e3249e8a5f043edbc5"
+TERMUX_PKG_BUILD_DEPENDS="aspnetcore-targeting-pack-8.0, dotnet-targeting-pack-8.0"
+TERMUX_PKG_DEPENDS="aspnetcore-runtime-8.0, dotnet-host, dotnet-runtime-8.0"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SERVICE_SCRIPT=("jackett" "exec ${TERMUX_PREFIX}/bin/jackett --DataFolder ${TERMUX_ANDROID_HOME}/.config/jackett 2>&1")
+TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/jackett/README.md
+lib/jackett/LICENSE
+lib/jackett/jackett.pdb
+lib/jackett/Jackett.Common.pdb
+lib/jackett/DateTimeRoutines.pdb
+"
+
+termux_step_pre_configure() {
+	termux_setup_dotnet
+}
+
+termux_step_make() {
+	dotnet publish src/Jackett.Server \
+	--framework "net8.0" \
+	--no-self-contained \
+	--runtime "$DOTNET_TARGET_NAME" \
+	--configuration Release \
+	--output build/ \
+	/p:AssemblyVersion="${TERMUX_PKG_VERSION}" \
+	/p:FileVersion="${TERMUX_PKG_VERSION}" \
+	/p:InformationalVersion="${TERMUX_PKG_VERSION}" \
+	/p:Version="${TERMUX_PKG_VERSION}"
+	dotnet build-server shutdown
+}
+
+termux_step_make_install() {
+	cp -r build "${TERMUX_PREFIX}/lib/jackett"
+	cat > $TERMUX_PREFIX/bin/jackett <<-HERE
+	#!$TERMUX_PREFIX/bin/sh
+	exec dotnet $TERMUX_PREFIX/lib/jackett/jackett.dll --NoUpdates "\$@"
+	HERE
+	chmod u+x $TERMUX_PREFIX/bin/jackett
+}
