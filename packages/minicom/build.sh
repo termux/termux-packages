2d920ab784fe75889ecde940007c97a5fcf0ff46
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-24 16:33:41 +0200

    bump(main/minicom): 2.10

--- packages/minicom/build.sh
+++ packages/minicom/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://salsa.debian.org/minicom-team/minicom
 TERMUX_PKG_DESCRIPTION="Friendly menu driven serial communication program"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.9"
+TERMUX_PKG_VERSION="2.10"
 TERMUX_PKG_SRCURL=https://salsa.debian.org/minicom-team/minicom/-/archive/${TERMUX_PKG_VERSION}/minicom-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=21e609d1b58c5de06400f6e36ed3a5060711847a63bc984b6e994d9ad1641d37
+TERMUX_PKG_SHA256=66ff82661c3cc49ab2e447f8a070ec1a64ba71d64219906d80a49da284a5d43e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libiconv, ncurses"
 
