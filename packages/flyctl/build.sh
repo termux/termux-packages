92a7cf943e3d4c6d6f03f0eaaee8811e57b9f5e1
Chongyun Lee <licy183@termux.dev>
2025-02-24 23:02:05 +0800

    flyctl: bump to 0.3.66

--- packages/flyctl/build.sh
+++ packages/flyctl/build.sh
@@ -2,24 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://fly.io
 TERMUX_PKG_DESCRIPTION="Command line tools for fly.io services"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION="0.3.65"
+TERMUX_PKG_VERSION="0.3.66"
 TERMUX_PKG_SRCURL=https://github.com/superfly/flyctl/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=f3112b2c555c6a3bacfbf59fb41b7caa9ed4279fc80eb13397503c4092c47033
+TERMUX_PKG_SHA256=edbd477e6dbfca3f7d7451d30dbd57fda48cef4a22d9b07ba8ed1906f9a8609d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, arm"
 
-
 termux_step_post_get_source() {
 	termux_setup_golang
-	export GOPATH=$TERMUX_PKG_SRCDIR/go
-	export GOOS="android"
-	go get
-	chmod +w $GOPATH -R
+	go mod tidy
+	go mod vendor
 }
 
 termux_step_make() {
-	export GOPATH=$TERMUX_PKG_SRCDIR/go
 	go build -o bin/flyctl
 }
 
