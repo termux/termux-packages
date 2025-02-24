a28c22a5053ad7aae42285d609836a317deb18a7
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-24 14:01:20 +0000

    fix(main/libduckdb): Remove httpfs extension to fix build
    It was moved out-of-tree in https://github.com/duckdb/duckdb/commit/b2cd12758c1b0b3b22408cf883b4a5ebf0e44a94

--- packages/libduckdb/build.sh
+++ packages/libduckdb/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_AUTO_UPDATE=true
 # we clone to retain the .git directory, to ensure the version in the built executable is correctly populated
 TERMUX_PKG_SRCURL=git+https://github.com/duckdb/duckdb
 TERMUX_PKG_DEPENDS="libc++, openssl"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_EXTENSIONS='icu;parquet;httpfs;json;autocomplete' -DDUCKDB_EXPLICIT_PLATFORM=linux_arm64_android"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_EXTENSIONS='icu;parquet;json;autocomplete' -DDUCKDB_EXPLICIT_PLATFORM=linux_arm64_android"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
