b893be8600f7952f2817a49bacc0594b97c048b5
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-24 15:30:53 +0000

    bump(main/asm-lsp): 0.10.0

--- packages/asm-lsp/build.sh
+++ packages/asm-lsp/build.sh
@@ -1,18 +1,24 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/bergercookie/asm-lsp
 TERMUX_PKG_DESCRIPTION="language server for NASM/GAS/GO assembly"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
+TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.0"
+TERMUX_PKG_VERSION="0.10.0"
 TERMUX_PKG_BUILD_DEPENDS="openssl"
 TERMUX_PKG_SRCURL=https://github.com/bergercookie/asm-lsp/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=863bdc5aad7039f7f355e09a2a759adbef6ea40b9efb3085f7bee4a382d79e11
+TERMUX_PKG_SHA256=4755848aa7d88856be7e40d0930990b95b46c4593a53db3809d3ba7214d9d16d
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_make() {
+termux_step_pre_configure() {
 	termux_setup_rust
 
+	TERMUX_PKG_SRCDIR+="/asm-lsp"
+	TERMUX_PKG_BUILDDIR="$TERMUX_PKG_SRCDIR"
+}
+
+termux_step_make() {
 	cargo fetch --target "${CARGO_TARGET_NAME}"
 
 	local d
