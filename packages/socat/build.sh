10d66b8bcf31167733c979ecc77498dbbd7115b2
Chongyun Lee <licy183@termux.dev>
2025-02-25 00:34:05 +0800

    socat: bump to 1.8.0.3

--- packages/socat/build.sh
+++ packages/socat/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=http://www.dest-unreach.org/socat/
 TERMUX_PKG_DESCRIPTION="Relay for bidirectional data transfer between two independent data channels"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.8.0.2"
+TERMUX_PKG_VERSION="1.8.0.3"
 TERMUX_PKG_SRCURL=http://www.dest-unreach.org/socat/download/socat-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e9498367cb765d44bb06be9709c950f436b30bf7071a224a0fee2522f9cbb417
+TERMUX_PKG_SHA256=a9f9eb6cfb9aa6b1b4b8fe260edbac3f2c743f294db1e362b932eb3feca37ba4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-posixmq
-ac_header_resolv_h=no
+ac_cv_header_resolv_h=no
 ac_cv_c_compiler_gnu=yes
 ac_compiler_gnu=yes
 sc_cv_getprotobynumber_r=
