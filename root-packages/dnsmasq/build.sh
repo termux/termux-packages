25cce384b13a67f77a67ae6eb9082cb4c70e6cf9
TomIO <tomjo00@web.de>
2025-02-25 11:55:30 +0100

    chore(root/dnsmasq): Add `-only` qualifer to GPL licenses

--- root-packages/dnsmasq/build.sh
+++ root-packages/dnsmasq/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=http://www.thekelleys.org.uk/dnsmasq/doc.html
 TERMUX_PKG_DESCRIPTION="Dnsmasq provides network infrastructure for small networks"
-# License: GPL-2.0-only or GPL-3.0-only
-TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
+TERMUX_PKG_LICENSE="GPL-2.0-only, GPL-3.0-only"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.90"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.thekelleys.org.uk/dnsmasq/dnsmasq-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8e50309bd837bfec9649a812e066c09b6988b73d749b7d293c06c57d46a109e4
 TERMUX_PKG_AUTO_UPDATE=true
