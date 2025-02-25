1c124b060f150aaa43934edef13ceabae79125d9
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 09:58:52 +0200

    bump(x11/kf6-kpackage): 6.11.0
    Fixes #23341

--- x11-packages/kf6-kpackage/build.sh
+++ x11-packages/kf6-kpackage/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Framework that lets applications manage user installable packages of non-binary assets'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kpackage-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=0f49c1cdb49e01c6dce372abbc9814ccbd74b7f2b130c7310674345e3498cec1
+TERMUX_PKG_SHA256=bfc704ea1708f37150a9d14edcbb9eed8ebf8d54753930da63432cda954a1ea7
 TERMUX_PKG_DEPENDS="kf6-karchive (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
