593e907f85f90b5bc8162a6236472eeee4daa6fb
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 08:37:04 +0200

    bump(x11/kf6/kservice): 6.11.0
    Fixes #23332

--- x11-packages/kf6-kservice/build.sh
+++ x11-packages/kf6-kservice/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Advanced plugin and service introspection'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kservice-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=04ad53850967e38822f8af1652b118992cd1bfa382e2718278bb6de03a0bdbb3
+TERMUX_PKG_SHA256=161cd296577ff9802b088b06866ec5ab4c1c1a6fe19f76bb5134cdf8a6ab4005
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
