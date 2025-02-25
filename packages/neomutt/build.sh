e18f86026c67cb25295c383da5cbfc1c603af9b8
TomIO <tomjo00@web.de>
2025-02-25 11:23:59 +0100

    chore(main/neomutt): Properly attribute license as GPL-2.0-or-later

--- packages/neomutt/build.sh
+++ packages/neomutt/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://neomutt.org/
 TERMUX_PKG_DESCRIPTION="A version of mutt with added features"
-# License: GPL-2.0-or-later
-TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="20250113"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/neomutt/neomutt/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cc7835e80fd72af104a8e146e009e93e687cefbc6c11725ee2ed11d7377486ff
 TERMUX_PKG_AUTO_UPDATE=true
