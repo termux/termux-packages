293d5507f809689e6fb01981134feccb2f9d8e2a
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 10:26:58 +0200

    bump(x11/kf6-solid): 6.11.0
    Fixes #23349

--- x11-packages/kf6-solid/build.sh
+++ x11-packages/kf6-solid/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Hardware integration and detection'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/solid-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=24892e81a3047f753519dbd384b47635c5a2543d8ee0bf3c299b0fcfef318e8c
+TERMUX_PKG_SHA256=cdecc7665b801a508b9ad0929b025ac93fe287be503589c8d8c38a520c2508d7
 TERMUX_PKG_DEPENDS="libimobiledevice, libplist, qt6-qtbase, libc++, util-linux"
 # media-player-info, systemd-libs, udisks2, upower can be added to TERMUX_PKG_DEPENDS when available
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qtdeclarative, qt6-qttools"
