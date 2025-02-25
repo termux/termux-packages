89466f6822b3865d1f44aa4e17997e07cb0b307a
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 08:53:56 +0200

    bump(x11/kf6-kguiaddons): 6.11.0
    Fixes #23275

--- x11-packages/kf6-kguiaddons/build.sh
+++ x11-packages/kf6-kguiaddons/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="The KDE GUI Add-ons"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kguiaddons-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=b3be04077313e559c5a8f66491d5d286cefe947aaf7c8937544ce85af4853ffa
+TERMUX_PKG_SHA256=ef0925f609ae5b62ed689451dfe9937719ce4ec26714952b5496e3e128d5cc5c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libwayland, libx11, qt6-qtbase, qt6-qtwayland"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), plasma-wayland-protocols, qt6-qttools"
