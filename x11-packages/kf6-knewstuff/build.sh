e62b22bca157b6dbab8de3b6a1f16d07294b9dd9
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 10:03:19 +0200

    bump(x11/kf6-knewstuff): 6.11.0
    Fixes #23280

--- x11-packages/kf6-knewstuff/build.sh
+++ x11-packages/kf6-knewstuff/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Support for downloading application assets from the network'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/knewstuff-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=81cb5ea54fe03d27f80a481dde18a767ca1a95267403bd87483cfdd81981e4e7
+TERMUX_PKG_SHA256=8b3802b6b64309ab6709af350f248dc62e3e6d50b0db4ecb0c968acfbfb23520
 TERMUX_PKG_DEPENDS="kf6-attica (>= ${TERMUX_PKG_VERSION}), kf6-karchive (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), kf6-kpackage (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), kf6-syndication (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase, qt6-qtdeclarative"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), kf6-kirigami (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_RECOMMENDS="kf6-kirigami"
