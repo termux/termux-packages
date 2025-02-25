b5b07fe6503eaf2c57b80b7619fe260cad0f49e9
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 09:00:17 +0200

    bump(x11/kf6-kcompletion): 6.11.0
    Fixes #23295

--- x11-packages/kf6-kcompletion/build.sh
+++ x11-packages/kf6-kcompletion/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION='Text completion helpers and widgets'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcompletion-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=b56e925bbe881c89fce9c80441e1565ad1adfcb16f1cac5bb08a281fb9334bc9
+TERMUX_PKG_SHA256=c812b60de4530de3003916e45fbab121c5f9f576e2430ab2cdf0e3e4c9041852
 TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
