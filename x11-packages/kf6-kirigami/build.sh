bbdc773bee03a3dd5a4fcee09afbea6d949aef2e
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 11:06:11 +0200

    bump(x11/kf6-kirigami): 6.11.0
    Fixes #23391

--- x11-packages/kf6-kirigami/build.sh
+++ x11-packages/kf6-kirigami/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='A QtQuick based components set'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kirigami-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=2e245ffd79eca1fcfb591f43ff39e7c2f5160e868a36e20ebbe2d66c550da8d4
+TERMUX_PKG_SHA256=22392c95bb835f11626250f0728ce73590db638814e7181148fcf66a1f442ea6
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-shadertools, qt6-qtsvg, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
