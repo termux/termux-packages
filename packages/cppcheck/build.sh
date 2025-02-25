617a11c59d52fa27e88807a2caff0d05031abd78
Fredrik Fornwall <fredrik@fornwall.net>
2025-02-25 14:25:31 +0100

    fix(main/cppcheck): Update source checksum

--- packages/cppcheck/build.sh
+++ packages/cppcheck/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="tool for static C/C++ code analysis"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.17.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology # Upstream only releases major versions theough GitHub. Other minor updates are released using git tags, better rely on repology for updated versiom
 TERMUX_PKG_SRCURL=https://github.com/danmar/cppcheck/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=54c31a5ba6913a760ca6c5a1138e6a8c97e74e6b532e0d0aeb4ae9dd3dda2649
+TERMUX_PKG_SHA256=5a639adf8d5f2520a280eb0f0fc25605cd8a3d4336a27f4a3e3e0f0c0c9789da
 TERMUX_PKG_DEPENDS="libc++"
 
 # Prevent running dmake during builds. dmake just generates Makefile which we
