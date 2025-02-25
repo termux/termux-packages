74418ec21b1cb57bae23e4f85d06a5f560217a51
TomIO <tomjo00@web.de>
2025-02-25 11:15:26 +0100

    chore(main/privoxy): Properly attribute license as GPL-2.0-or-later

--- packages/privoxy/build.sh
+++ packages/privoxy/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://www.privoxy.org
 TERMUX_PKG_DESCRIPTION="Non-caching web proxy with advanced filtering capabilities"
-# License: GPL-2.0-or-later
-TERMUX_PKG_LICENSE="GPL-2.0, GPL-3.0"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.GPLv3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.0.34
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/ijbswa/Sources/$TERMUX_PKG_VERSION%20%28stable%29/privoxy-$TERMUX_PKG_VERSION-stable-src.tar.gz
 TERMUX_PKG_SHA256=e6ccbca1656f4e616b4657f8514e33a70f6697e9d7294356577839322a3c5d2c
 # Termux-services adds the run scripts to TERMUX_PKG_CONFFILES. Those ones can
