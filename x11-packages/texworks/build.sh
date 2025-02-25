73e805962db9455fe4e1457f2281b48d3c07716f
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-25 09:20:04 +0000

    bump(x11/texworks): 0.6.10

--- x11-packages/texworks/build.sh
+++ x11-packages/texworks/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="TeXworks is an environment for authoring TeX (LaTeX, Con
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.6.9"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="0.6.10"
 TERMUX_PKG_SRCURL=https://github.com/TeXworks/texworks/archive/refs/tags/release-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a0c8e4b9f2fbb38f681b9d91f331366e5cdcb35dba7a94cb2988ccfca113ac2b
+TERMUX_PKG_SHA256=bbc29d5aba451355222b29dd02ef318fd7fec55a721a61fc6d9cd933a0abd270
 TERMUX_PKG_DEPENDS="hunspell, libc++, liblua53, poppler-qt, qt6-qtbase, qt6-qtdeclarative, qt6-qt5compat, zlib"
 TERMUX_PKG_BUILD_DEPENDS="git, qt6-qtbase-cross-tools, qt6-qtdeclarative-cross-tools, qt6-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,3 +14,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLUA_MATH_LIBRARY=
 -DQT_DEFAULT_MAJOR_VERSION=6
 "
+
+termux_step_pre_configure() {
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
+		-DCMAKE_CXX_COMPILER_CLANG_SCAN_DEPS=${TERMUX_STANDALONE_TOOLCHAIN}/bin/clang-scan-deps
+	"
+}
