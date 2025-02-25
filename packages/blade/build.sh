02ff37a5a50e588833b664fcb10b839b5f20823a
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 08:02:11 +0200

    bump(main/blade): 0.0.87

--- packages/blade/build.sh
+++ packages/blade/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A simple, fast, clean and dynamic language"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.0.86"
+TERMUX_PKG_VERSION="0.0.87"
 TERMUX_PKG_SRCURL=https://github.com/blade-lang/blade/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=abbf411abd64e5fc66411f37bcd4efec1ec71ba8b000d7f9d3249c1c726f468e
+TERMUX_PKG_SHA256=7a438f126eed74077d6112b89c9d890a8cc0a3affbccde0b023ad43639fed4de
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="libgd, libcurl, openssl"
 TERMUX_PKG_HOSTBUILD=true
@@ -29,6 +29,6 @@ termux_step_make_install() {
 	install -Dm600 -t $TERMUX_PREFIX/lib libblade.so
 	local sharedir=$TERMUX_PREFIX/share/blade
 	mkdir -p $sharedir
-	cp -r benchmarks includes libs tests $sharedir/
+	cp -r $TERMUX_PKG_SRCDIR/benchmarks $TERMUX_PKG_BUILDDIR/blade/includes $TERMUX_PKG_SRCDIR/libs $TERMUX_PKG_SRCDIR/tests $sharedir/
 	popd
 }
