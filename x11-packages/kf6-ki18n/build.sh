36792325a3b895dd455d723ad0e4959da984ab7f
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-25 08:35:14 +0200

    bump(x11/kf6-ki18n): 6.11.0
    Fixes #23287

--- x11-packages/kf6-ki18n/build.sh
+++ x11-packages/kf6-ki18n/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="KDE Gettext-based UI text internationalization"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.10.0"
+TERMUX_PKG_VERSION="6.11.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/ki18n-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=2f59f093f8ce340ab46c556b35c2ead2b96dfeb2ff0024c553ac8c53e9b8a11a
+TERMUX_PKG_SHA256=658a05ceca184ba31ce58a6e9c51ee76f2829459c56dbcd3bad3aa157eaf11fe
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gettext, iso-codes, libc++, qt6-qtbase, qt6-qtdeclarative"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), python, qt6-qttools"
