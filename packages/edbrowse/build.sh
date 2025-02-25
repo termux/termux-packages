30b1eeec9dcdcb6f661d9a57bb4cc4a7671649e3
TomIO <tomjo00@web.de>
2025-02-25 11:20:39 +0100

    chore(main/edbrowse): Properly attribute all licenses for the project. edbrowse is licensed under: GPL-2.0-or-later, MIT, CC0-1.0, curl See: https://github.com/edbrowse/edbrowse/blob/v3.8.10/COPYING#L22-L23

--- packages/edbrowse/build.sh
+++ packages/edbrowse/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://edbrowse.org/
 TERMUX_PKG_DESCRIPTION="Line based editor, browser, and mail client"
-# License: GPL-2.0-or-later
-TERMUX_PKG_LICENSE="GPL-2.0, MIT"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later, MIT, CC0-1.0, curl"
 TERMUX_PKG_LICENSE_FILE="COPYING, LICENSE.quickjs"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.8.10"
-TERMUX_PKG_SRCURL=https://github.com/CMB/edbrowse/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/edbrowse/edbrowse/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3c194ce45b7348211ce3ad8e3304a0eacf8b27e623cbf8c08687785f88174e03
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
