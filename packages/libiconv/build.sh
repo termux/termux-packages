4019fc504c516d9593da5a39ff296c7763872bea
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-26 07:18:34 +0200

    bump(main/libiconv): 1.18

--- packages/libiconv/build.sh
+++ packages/libiconv/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libiconv/
 TERMUX_PKG_DESCRIPTION="An implementation of iconv()"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17
+TERMUX_PKG_VERSION="1.18"
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/libiconv/libiconv-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313
+TERMUX_PKG_SHA256=3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8
 TERMUX_PKG_BREAKS="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 TERMUX_PKG_REPLACES="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 
