ff5c2d6b2b0510a2fd3e2946917c4101190a53bf
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-02-27 18:39:16 +0200

    fix(ci): fixing package cleanup to work with toybox df.

--- scripts/build/termux_step_cleanup_packages.sh
+++ scripts/build/termux_step_cleanup_packages.sh
@@ -2,24 +2,27 @@ termux_step_cleanup_packages() {
 	[[ "${TERMUX_CLEANUP_BUILT_PACKAGES_ON_LOW_DISK_SPACE:=false}" == "true" ]] || return 0
 	[[ -d "$TERMUX_TOPDIR" ]] || return 0
 
-	# How much space is considered to be enough
-	local CLEANUP_THRESHOLD="$(( 5 * 1024 ** 3 ))" # 5 GiB
+	local AVAILABLE TERMUX_PACKAGES_DIRECTORIES PKGS PKG_REGEX
 
 	# Extract available disk space in bytes
-	local AVAILABLE="$(df "$TERMUX_TOPDIR" | awk 'NR==2 {print $4 * 1024}')"
+	AVAILABLE="$(df "$TERMUX_TOPDIR" | awk 'NR==2 {print $4 * 1024}')"
 
 	# No need to cleanup if there is enough disk space
-	(( AVAILABLE <= CLEANUP_THRESHOLD )) || return 0
+	(( AVAILABLE <= TERMUX_CLEANUP_BUILT_PACKAGES_THRESHOLD )) || return 0
 
-	local TERMUX_PACKAGES_DIRECTORIES="$(jq --raw-output 'del(.pkg_format) | keys | .[]' "${TERMUX_SCRIPTDIR}"/repo.json)"
+	TERMUX_PACKAGES_DIRECTORIES="$(jq --raw-output 'del(.pkg_format) | keys | .[]' "${TERMUX_SCRIPTDIR}"/repo.json)"
 
 	# Build package name regex to be used with `find`, avoiding loops.
-	local PKG_REGEX="$(find ${TERMUX_PACKAGES_DIRECTORIES} -mindepth 1 -maxdepth 1 -type d -printf '%f|')^$"
+	PKGS="$(find ${TERMUX_PACKAGES_DIRECTORIES} -mindepth 1 -maxdepth 1 -type d -printf '%f\n')"
+	[[ -z "$PKGS" ]] && return 0
 
 	# Exclude current package from the list.
-	PKG_REGEX="${PKG_REGEX//$TERMUX_PKG_NAME|/}"
+	PKGS="$(printf "%s" "$PKGS" | grep -Fxv "$TERMUX_PKG_NAME")"
+	[[ -z "$PKGS" ]] && return 0
+
+	PKG_REGEX="$(printf "%s" "$PKGS" | sed -zE 's/[][\.|$(){}?+*^]/\\&/g' | sed -E 's/(.*)/(\1)/g' | sed -zE -e 's/[\n]+/|/g' -e 's/(.*)/(\1)/g')"
 
 	echo "INFO: cleaning up some disk space for building \"${TERMUX_PKG_NAME}\"."
 
-	find "$TERMUX_TOPDIR" -mindepth 1 -maxdepth 1 -type d -regextype posix-extended -regex ".*/($PKG_REGEX)$" -exec rm -rf {} +
+	(cd "$TERMUX_TOPDIR" && find . -mindepth 1 -maxdepth 1 -type d -regextype posix-extended -regex "^\./$PKG_REGEX$" -exec rm -rf "{}" +)
 }
