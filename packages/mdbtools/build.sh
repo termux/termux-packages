d2fc717da4fea0dbfd9a62650646a40d72e9fb58
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-28 14:37:40 +0000

    bump(main/mdbtools): 1.0.1

--- packages/mdbtools/build.sh
+++ packages/mdbtools/build.sh
@@ -2,14 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mdbtools/mdbtools
 TERMUX_PKG_DESCRIPTION="A set of programs to help you extract data from Microsoft Access files in various settings"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="1.0.1"
 TERMUX_PKG_SRCURL=https://github.com/mdbtools/mdbtools/releases/download/v${TERMUX_PKG_VERSION}/mdbtools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3446e1d71abdeb98d41e252777e67e1909b186496fda59f98f67032f7fbcd955
+TERMUX_PKG_SHA256=ff9c425a88bc20bf9318a332eec50b17e77896eef65a0e69415ccb4e396d1812
 TERMUX_PKG_DEPENDS="glib, libiconv, readline, libiodbc"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-iodbc=$TERMUX_PREFIX"
-
-termux_step_pre_configure() {
-	CFLAGS+=" -Wno-error=unused-but-set-variable"
-}
