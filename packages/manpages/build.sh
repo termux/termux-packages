eaa9755d092829ddeac5575ff4d54f57e4e0908b
Biswapriyo Nath <nathbappai@gmail.com>
2025-02-28 20:25:36 +0000

    bump(main/manpages): 6.12

--- packages/manpages/build.sh
+++ packages/manpages/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Man pages for linux kernel and C library interfaces"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSES/Linux-man-pages-copyleft.txt, _man-pages-posix/POSIX-COPYRIGHT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(6.11
+TERMUX_PKG_VERSION=(6.12
                     2017)
 TERMUX_PKG_SHA256=(
-	ddaa2eda2e8d286fbec221d115f12d3fff5d36cc5066cdfecc8d24a258d58b19
+	b6cb5d67e0bb00a3b3f3d1bcb3fe06c26b045ba63923ed7ae79412350c5e1cb5
 	ce67bb25b5048b20dad772e405a83f4bc70faf051afa289361c81f9660318bc3
 )
 TERMUX_PKG_SRCURL=(https://www.kernel.org/pub/linux/docs/man-pages/man-pages-${TERMUX_PKG_VERSION[0]}.tar.xz
