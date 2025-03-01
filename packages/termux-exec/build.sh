3b0487b8994dac4b06692c6b4640f46f8ffdc971
agnostic-apollo <agnosticapollo@gmail.com>
2025-03-01 08:11:07 +0500

    bump(main/termux-exec): 1:2.0.0

--- packages/termux-exec/build.sh
+++ packages/termux-exec/build.sh
@@ -2,11 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-exec-package
 TERMUX_PKG_DESCRIPTION="Utils and libraries for Termux exec including a LD_PRELOAD shared library for proper functioning of the Termux execution environment"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.4
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec/archive/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=9a8d42d211a7d461d61dcd4e3ef984014c2c2c696cfd6394bae389af13572627
-
+TERMUX_PKG_VERSION=1:2.0.0
+TERMUX_PKG_SRCURL=https://github.com/termux/termux-exec-package/archive/refs/tags/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=480452bd0b9cf7b6eb6549825a8faebb2a4afa8c88b9e1d621fb05528ba7ee6e
 TERMUX_PKG_BUILD_DEPENDS="termux-core"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
