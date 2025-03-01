82692d6a5e3086300c868b746f011e45c4789e73
Aditya Alok <alok@termux.dev>
2025-03-02 01:16:58 +0530

    pkg(main/shellcheck): Remove `ghc-libs` from build dependency
    - After renaming of `ghc-libs` to `ghc`
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/shellcheck/build.sh
+++ packages/shellcheck/build.sh
@@ -8,7 +8,6 @@ TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/ShellCheck-${TERMUX_PKG_V
 TERMUX_PKG_SHA256=4d08db432d75a34486a55f6fff9d3e3340ce56125c7804b7f8fd14421b936d21
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libffi"
-TERMUX_PKG_BUILD_DEPENDS="ghc-libs"
 TERMUX_PKG_BLACKLISTED_ARCHES="arm"
 # arm build fails complaining that it can't find `opt-13`
 # symlinking `opt-16` to `opt-13` does not work. Blacklisting it for now.
