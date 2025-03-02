f93df16a824a54309bcac50a38f59bce28ccd044
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-02 21:19:09 +0200

    fix(ci): longer delay between requests to avoid excessive GitHub resource usage.
    Closes #23564.

--- scripts/updates/internal/termux_github_graphql.sh
+++ scripts/updates/internal/termux_github_graphql.sh
@@ -44,7 +44,7 @@ termux_github_graphql() {
 		# printf '%s' "${QUERY}"  >> /tmp/query-12345 # Uncomment for debugging GraphQL queries
 
 		# We use graphql intensively so we should slowdown our requests to avoid hitting github ratelimits.
-		sleep 1
+		sleep 5
 
 		local response
 		response="$(printf '{ "query": "%s" }' "${QUERY//$'\n'/ }" | curl -fL \
