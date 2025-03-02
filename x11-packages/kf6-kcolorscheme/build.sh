bfab9e06d72011f1802c1c6409bc20597039041c
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-02 07:44:00 +0200

    revbump(x11/kf6-{breeze-icons,karchive,kauth,kcolorscheme}): rebuild

--- x11-packages/kf6-kcolorscheme/build.sh
+++ x11-packages/kf6-kcolorscheme/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Classes to read and interact with KColorScheme'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.11.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcolorscheme-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=8b27bddb830f0173f44ae9aac05213579909b85fa696a2871b2ee11cb239bcc8
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kguiaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
