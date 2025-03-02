8a1c8ae2799d2d4fb661a674e69e1bcdd3b4d027
Chongyun Lee <licy183@termux.dev>
2025-03-02 14:24:31 +0800

    llama-cpp: bump to 0.0.0-b4793

---
+++ packages/llama-cpp/llama-cpp-backend-opencl.subpackage.sh
@@ -0,0 +1,6 @@
+TERMUX_SUBPKG_DESCRIPTION="OpenCL Backend for LLaMA.cpp"
+TERMUX_SUBPKG_RECOMMENDS="ocl-icd"
+TERMUX_SUBPKG_SUGGESTS="opencl-vendor-driver | clvk"
+TERMUX_SUBPKG_INCLUDE="
+lib/libggml-opencl.so
+"
