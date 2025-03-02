1a3609e3b34bfc6c477ecafe43a3d3d1f49e8d80
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-03 02:45:53 +0530

    bump(main/nodejs-lts): 23.8.0
    use clang instead of gcc for host builds as it takes much less memory
    and also compiles faster. gcc always gets OOM killed on my setup here
    with 16G of RAM and 20 jobs, with llvm, the RAM usage doesn't even reach
    14G. With swap, gcc builds take around 6G swap and 12G physical memory,
    which is absurd. Also clang requires -latomic
    
    This also effectively cherry picks nodejs/node@6b5a43f45423f931099f4e20a6c40c8a5ef5bcc0
    which for some reason hasn't been backported to v23 and v22 branch.

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=22.13.1
+TERMUX_PKG_VERSION=22.14.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cfce282119390f7e0c2220410924428e90dadcb2df1744c0c4a0e7baae387cc2
+TERMUX_PKG_SHA256=c609946bf793b55c7954c26582760808d54c16185d79cb2fb88065e52de21914
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
@@ -30,6 +30,9 @@ termux_step_host_build() {
 	local ICU_VERSION=76.1
 	local ICU_TAR=icu4c-${ICU_VERSION//./_}-src.tgz
 	local ICU_DOWNLOAD=https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION//./-}/$ICU_TAR
+	export CC=/usr/bin/clang-18
+	export CXX=/usr/bin/clang++-18
+	export LD=/usr/bin/clang++-18
 	termux_download \
 		$ICU_DOWNLOAD\
 		$TERMUX_PKG_CACHEDIR/$ICU_TAR \
@@ -64,9 +67,9 @@ termux_step_configure() {
 	fi
 
 	export GYP_DEFINES="host_os=linux"
-	export CC_host=gcc
-	export CXX_host=g++
-	export LINK_host=g++
+	export CC_host=/usr/bin/clang
+	export CXX_host=/usr/bin/clang++
+	export LINK_host=/usr/bin/clang++
 
 	# See note above TERMUX_PKG_DEPENDS why we do not use a shared libuv
 	# When building with ninja, build.ninja is geenrated for both Debug and Release builds.
@@ -92,6 +95,7 @@ termux_step_configure() {
 	sed -i \
 		-e "s|\-I$TERMUX_PREFIX/include|-I$TERMUX_PKG_HOSTBUILD_DIR/icu-installed/include|g" \
 		-e "s|\-L$TERMUX_PREFIX/lib|-L$TERMUX_PKG_HOSTBUILD_DIR/icu-installed/lib|g" \
+		-e "s|\-L$TERMUX_PREFIX/$TERMUX_PREFIX/lib||g" \
 		$(find $TERMUX_PKG_SRCDIR/out/{Release,Debug}/obj.host -name '*.ninja')
 
 }
