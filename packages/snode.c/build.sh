868b9e012860b96fb6b90251d5609f5e1ecfebb0
Volker Christian <me@vchrist.at>
2025-03-03 23:59:51 +0100

    SNode.C: Modifications following hints from @TomJo2000
    Signed-off-by: Volker Christian <me@vchrist.at>

--- packages/snode.c/build.sh
+++ packages/snode.c/build.sh
@@ -1,10 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/SNodeC/snode.c
 TERMUX_PKG_DESCRIPTION="Simple NODE in C++ (SNode.C)"
-TERMUX_PKG_LICENSE="MIT OR LGPL-3.0-or-later"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_LICENSE="MIT, LGPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="VolkerChristian"
 TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_SRCURL="git+https://github.com/SNodeC/snode.c.git"
-TERMUX_PKG_DEPENDS="file,openssl"
-TERMUX_PKG_BUILD_DEPENDS="easyloggingpp,nlohmann-json"
-TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_SRCURL="https://github.com/SNodeC/snode.c/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_DEPENDS="file, openssl"
+TERMUX_PKG_BUILD_DEPENDS="easyloggingpp, nlohmann-json"
