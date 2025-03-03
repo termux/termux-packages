74ee9e3cba059c15d792b2560ccb08f4d8a31f9d
TomIO <tomjo00@web.de>
2025-03-03 22:47:10 +0100

    chore(main/neovim): Use absolute paths at build time in `LD_PRELOAD` shim See: https://github.com/termux/termux-packages/issues/23569

--- packages/neovim/nvim-shim.sh
+++ packages/neovim/nvim-shim.sh
@@ -5,5 +5,9 @@
 # https://github.com/termux/termux-packages/wiki/Common-porting-problems#android-dynamic-linker
 # https://github.com/android/ndk/issues/201
 #
+# Using /data/data/com.termux/files/usr to substitute full path at build time.
+# This avoids a conflict with `pass`.
+# See: https://github.com/termux/termux-packages/issues/23569
+#
 # Shim to properly expose LuaJIT runtime symbols to dynamically linked plugin modules
-LD_PRELOAD="$LD_PRELOAD:$PREFIX/lib/libluajit.so" exec "$PREFIX/libexec/nvim" "$@"
+LD_PRELOAD="$LD_PRELOAD:@TERMUX_PREFIX@/lib/libluajit.so" exec "@TERMUX_PREFIX@/libexec/nvim" "$@"
