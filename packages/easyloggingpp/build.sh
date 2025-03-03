9d4a3492f01aa99d6f44a600808a300149b2de33
Volker Christian <me@vchrist.at>
2025-03-04 00:04:37 +0100

    Easyloggingpp: Modifications following hints from @TomJo2000
    Signed-off-by: Volker Christian <me@vchrist.at>

--- packages/easyloggingpp/build.sh
+++ packages/easyloggingpp/build.sh
@@ -1,7 +1,6 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/SNodeC/snode.c
+TERMUX_PKG_HOMEPAGE=https://github.com/abumq/easyloggingpp
 TERMUX_PKG_DESCRIPTION="Cross-platform logging made easier for C** applications"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="VolkerChristian"
 TERMUX_PKG_VERSION=9.97.1
-TERMUX_PKG_SRCURL="git+https://github.com/abumq/easyloggingpp.git"
-TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_SRCURL="https://github.com/abumq/easyloggingpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
