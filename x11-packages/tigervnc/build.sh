15b5d8ec1202d3093c9b1a1b0311967af758fafc
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-03 17:51:14 +0200

    revbump(xorg-server,tigervnc): 21.1.16
    `xorg-server` and `xorg-server-xvfb` packages were built from the same source with same patches so they were united into package and its subpackage to reduce maintenance cost.
    `tigervnc` is being revbumped every time `xvfb` version or patches are changed so it is being revbumped now as well.

--- x11-packages/tigervnc/build.sh
+++ x11-packages/tigervnc/build.sh
@@ -3,13 +3,15 @@ TERMUX_PKG_DESCRIPTION="Suite of VNC servers. Based on the VNC 4 branch of Tight
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=(1.14.1
-                    21.1.8)
+# Align the version with `xorg-server` package.
+                    21.1.16)
 TERMUX_PKG_SRCURL=(https://github.com/TigerVNC/tigervnc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
                    https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz)
 TERMUX_PKG_SHA256=(579d0d04eb5b806d240e99a3c756b38936859e6f7db2f4af0d5656cc9a989d7c
-                   38aadb735650c8024ee25211c190bf8aad844c5f59632761ab1ef4c4d5aeb152)
+                   b14a116d2d805debc5b5b2aac505a279e69b217dae2fae2dfcb62400471a9970)
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libgmp, libgnutls, libjpeg-turbo, libnettle, libpixman, libx11, libxau, libxdamage, libxdmcp, libxext, libxfixes, libxfont2, libxrandr, libxshmfence, libxtst, opengl, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp, zlib"
-TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-server-xvfb, xorg-util-macros, xorgproto, xtrans"
+TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-server, xorg-util-macros, xorgproto, xtrans"
 TERMUX_PKG_SUGGESTS="aterm, xorg-twm"
 
 TERMUX_PKG_FOLDERNAME=tigervnc-${TERMUX_PKG_VERSION}
@@ -22,7 +24,7 @@ termux_step_post_get_source() {
 	cp -r xorg-server-${TERMUX_PKG_VERSION[1]}/* unix/xserver/
 
 	cd ${TERMUX_PKG_BUILDDIR}/unix/xserver
-	for p in "$TERMUX_SCRIPTDIR/x11-packages/xorg-server-xvfb"/*.patch; do
+	for p in "$TERMUX_SCRIPTDIR/x11-packages/xorg-server"/*.patch; do
 		echo "Applying $(basename "${p}")"
 		sed -e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
 			-e "s%\@TERMUX_HOME\@%${TERMUX_ANDROID_HOME}%g" "$p" \
@@ -45,14 +47,15 @@ termux_step_pre_configure() {
 	local _libgcc_name="$(basename $_libgcc_file)"
 	LDFLAGS="${LDFLAGS} -llog -L$_libgcc_path -l:$_libgcc_name"
 
-	local xorg_server_xvfb_configure_args="$(. $TERMUX_SCRIPTDIR/x11-packages/xorg-server-xvfb/build.sh; echo $TERMUX_PKG_EXTRA_CONFIGURE_ARGS)"
+	local xorg_server_configure_args="$(. $TERMUX_SCRIPTDIR/x11-packages/xorg-server/build.sh; echo $TERMUX_PKG_EXTRA_CONFIGURE_ARGS)"
+	xorg_server_configure_args+=" --disable-xorg --disable-xephyr --disable-kdrive"
 	./configure \
 		--host="${TERMUX_HOST_PLATFORM}" \
 		--prefix="${TERMUX_PREFIX}" \
 		--disable-static \
 		--disable-nls \
 		--enable-debug \
-		${xorg_server_xvfb_configure_args}
+		${xorg_server_configure_args}
 
 	LDFLAGS="${LDFLAGS} -landroid-shmem"
 }
--- x11-packages/xorg-server/build.sh
+++ x11-packages/xorg-server/build.sh
@@ -2,20 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/wiki/
 TERMUX_PKG_DESCRIPTION="Xorg server"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
+# Keep version of `tigervnc` package aligned with this package, revbump tigervnc after modifying patches of this package
 TERMUX_PKG_VERSION="21.1.16"
+TERMUX_PKG_REVISION="1"
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b14a116d2d805debc5b5b2aac505a279e69b217dae2fae2dfcb62400471a9970
-TERMUX_PKG_AUTO_UPDATE=true
+# We can not update it automatically because tigervnc server version must be aligned with xorg-server.
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libandroid-shmem, libdrm, libpciaccess, libpixman, libx11, libxau, libxcvt, libxfont2, libxinerama, libxkbfile, libxshmfence, opengl, openssl, xkeyboard-config, xorg-protocol-txt, xorg-xkbcomp"
+TERMUX_PKG_BUILD_DEPENDS="mesa-dev"
 
 # Needed for Xephyr
 TERMUX_PKG_BUILD_DEPENDS="xcb-util, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm"
 
 TERMUX_PKG_RECOMMENDS="xf86-video-dummy, xf86-input-void"
 TERMUX_PKG_NO_STATICSPLIT=true
-
-# Provided by xorg-protocol-txt (subpackage of xorg-server-xvfb):
-TERMUX_PKG_RM_AFTER_INSTALL="lib/xorg/protocol.txt"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_path_RAWCPP=/usr/bin/cpp
@@ -51,7 +52,7 @@ ac_cv_path_RAWCPP=/usr/bin/cpp
 --enable-xorg
 --disable-glamor
 --disable-dmx
---disable-xvfb
+--enable-xvfb
 --disable-xnest
 --disable-xwayland
 --disable-xwin
@@ -79,14 +80,19 @@ termux_step_pre_configure() {
 	for file in $files; do rm $file; done
 	unset files
 
-	#you will need xutils-dev package for xorg-macros installed
+	# you will need xutils-dev package for xorg-macros installed
 	autoreconf -if
 	cd -
+
+	local XVFB_RUN_URL="https://salsa.debian.org/xorg-team/xserver/xorg-server/-/raw/debian-bookworm/debian/local/xvfb-run"
+	termux_download ${XVFB_RUN_URL} "${TERMUX_PKG_CACHEDIR}/xvfb-run" fd05e0f8e6207c3984b980a0f037381c9c4a6f22a6dd94fdcfa995318db2a0a4
+	sed -i "1s|.*|#!$TERMUX_PREFIX/bin/bash|" "${TERMUX_PKG_CACHEDIR}/xvfb-run"
 }
 
 termux_step_post_make_install () {
 	rm -f "${TERMUX_PREFIX}/usr/share/X11/xkb/compiled"
-	install -Dm644 -t "$TERMUX_PREFIX/etc/X11/" "${TERMUX_PKG_BUILDER_DIR}/xorg.conf"
+	install -Dm644 -t "${TERMUX_PREFIX}/etc/X11/" "${TERMUX_PKG_BUILDER_DIR}/xorg.conf"
+	install -Dm755 "${TERMUX_PKG_CACHEDIR}/xvfb-run" "${TERMUX_PREFIX}/bin/"
 }
 
 ## The following is required for package 'tigervnc'.
