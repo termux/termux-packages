5beb6764b3b2fe6c9c6cae74a18ea1d89cee75d7
Volker Christian <me@vchrist.at>
2025-03-04 00:14:46 +0100

    MQTTSuite: Add TERMUX_PKG_SHA256
    Signed-off-by: Volker Christian <me@vchrist.at>

--- packages/mqttsuite/build.sh
+++ packages/mqttsuite/build.sh
@@ -4,4 +4,5 @@ TERMUX_PKG_LICENSE="MIT, LGPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="VolkerChristian"
 TERMUX_PKG_VERSION=1.0
 TERMUX_PKG_SRCURL="https://github.com/SNodeC/mqttsuite/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=e741ed45e8739a3fae7cc692b5883b22b98efed450f315a99c47c7c1e36b09e0
 TERMUX_PKG_DEPENDS="fmt, snode.c"
