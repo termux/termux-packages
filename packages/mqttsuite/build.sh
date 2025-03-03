92fd5ae104acb2d3ece91586212be2a7f9c50dc9
Volker Christian <me@vchrist.at>
2025-03-04 00:07:10 +0100

    MQTTSuite: Modifications following hints from @TomJo2000
    Signed-off-by: Volker Christian <me@vchrist.at>

--- packages/mqttsuite/build.sh
+++ packages/mqttsuite/build.sh
@@ -1,9 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/SNodeC/mqttsuite
 TERMUX_PKG_DESCRIPTION="MQTTSuite: A lightweight MQTT Integration System"
-TERMUX_PKG_LICENSE="MIT OR LGPL-3.0-or-later"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_LICENSE="MIT, LGPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="VolkerChristian"
 TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_SRCURL="git+https://github.com/SNodeC/mqttsuite.git"
-TERMUX_PKG_DEPENDS="fmt,snode.c"
-TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_SRCURL="https://github.com/SNodeC/mqttsuite/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_DEPENDS="fmt, snode.c"
