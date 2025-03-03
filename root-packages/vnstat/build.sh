a29bc7931569ed82faf3f3445d6411a9a0710323
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-03 08:44:39 +0200

    bump(root/vnstat): 2.13

--- root-packages/vnstat/build.sh
+++ root-packages/vnstat/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/vergoh/vnstat
 TERMUX_PKG_DESCRIPTION="A console-based network traffic monitor"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Denzy7"
-TERMUX_PKG_VERSION=2.12
+TERMUX_PKG_VERSION="2.13"
 TERMUX_PKG_SRCURL=https://github.com/vergoh/vnstat/releases/download/v${TERMUX_PKG_VERSION}/vnstat-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b7386b12fc1fc6f47fab31f208b12eda61862e63e229e84e95a6fa80406d2852
+TERMUX_PKG_SHA256=c9fe19312d1ec3ddfbc4672aa951cf9e61ca98dc14cad3d3565f7d9803a6b187
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libsqlite"
 TERMUX_PKG_SERVICE_SCRIPT=("vnstat" "exec su -c \"PATH=\$PATH $TERMUX_PREFIX/bin/vnstatd -n 2>&1\"")
 
