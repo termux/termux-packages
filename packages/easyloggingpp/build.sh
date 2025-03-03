f0f184d77e04ae1db14aaaa883068187d86276d0
Volker Christian <me@vchrist.at>
2025-03-03 23:06:03 +0100

    Add package easyloggingpp used in coming SNode.C and MQTTSuite packages
    Easyloggingpp: https://github.com/abumq/easyloggingpp
    Signed-off-by: Volker Christian <me@vchrist.at>

---
+++ packages/easyloggingpp/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/SNodeC/snode.c
+TERMUX_PKG_DESCRIPTION="Cross-platform logging made easier for C** applications"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="VolkerChristian"
+TERMUX_PKG_VERSION=9.97.1
+TERMUX_PKG_SRCURL="git+https://github.com/abumq/easyloggingpp.git"
+TERMUX_PKG_FORCE_CMAKE=true
+
