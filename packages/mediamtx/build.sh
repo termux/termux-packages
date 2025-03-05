f4b49d9f794ac2d5dcdebe5bd33b65e8cc14e9e6
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-05 23:15:11 +0200

    bump(main/golang): 1.24.1

--- packages/mediamtx/build.sh
+++ packages/mediamtx/build.sh
@@ -3,20 +3,19 @@ TERMUX_PKG_DESCRIPTION="Ready-to-use SRT / WebRTC / RTSP / RTMP / LL-HLS media s
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.11.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/bluenviron/mediamtx/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cdd46bbbfe2be95057df8eb5016b88328063255c38e0e42d27abccd7d3ebbad6
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_host_build() {
+termux_step_post_get_source() {
 	termux_setup_golang
-	pushd "${TERMUX_PKG_SRCDIR}"
 	go generate ./...
-	popd
 }
 
 termux_step_make() {
+	termux_setup_golang
 	echo "v${TERMUX_PKG_VERSION}" > "${TERMUX_PKG_SRCDIR}"/internal/core/VERSION
 	export GOFLAGS="-buildmode=pie -trimpath -mod=readonly -modcacherw"
 
