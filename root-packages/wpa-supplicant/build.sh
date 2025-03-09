bbc500f8cc7add47cda82a71e3783882f9a60247
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-09 14:04:37 +0200

    bump(root/wpa-supplicant): 2.11

--- root-packages/wpa-supplicant/build.sh
+++ root-packages/wpa-supplicant/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://hostap.epitest.fi/wpa_supplicant
 TERMUX_PKG_DESCRIPTION="Utility providing key negotiation for WPA wireless networks"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.10
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.11"
 TERMUX_PKG_SRCURL=https://w1.fi/releases/wpa_supplicant-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="openssl, readline, libnl"
-TERMUX_PKG_SHA256=20df7ae5154b3830355f8ab4269123a87affdea59fe74fe9292a91d0d7e17b2f
+TERMUX_PKG_SHA256=912ea06f74e30a8e36fbb68064d6cdff218d8d591db0fc5d75dee6c81ac7fc0a
 TERMUX_PKG_EXTRA_MAKE_ARGS="-C wpa_supplicant"
 TERMUX_PKG_BUILD_IN_SRC=true
 
