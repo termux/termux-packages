53dc35642f392746965021ac1fcd040fd7911ab9
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-11 14:49:21 +0200

    bump(x11/pcmanfm): 1.4.0

--- x11-packages/pcmanfm/build.sh
+++ x11-packages/pcmanfm/build.sh
@@ -2,13 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/pcmanfm/
 TERMUX_PKG_DESCRIPTION="Extremely fast and lightweight file manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.2
-TERMUX_PKG_REVISION=5
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pcmanfm/pcmanfm-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=14cb7b247493c4cce65fbb5902611e3ad00a7a870fbc1e50adc50428c5140cf7
+TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_SRCURL=https://github.com/lxde/pcmanfm/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=cda4f5ad7e049dcdf3b051b9de4c779adcd55bd720e9c96c45275389aa84264e
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="atk, glib, gtk3, libcairo, libfm, libx11, lxmenu-data, pango"
 TERMUX_PKG_RECOMMENDS="xarchiver"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-gtk=3
 "
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
