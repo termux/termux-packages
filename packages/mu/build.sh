51c1be68f170128ce91fea2fd2839c743b1a19f0
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-11 15:52:30 +0000

    bump(main/mu): 1.12.9
    Rebase mu4e-dont-bytecompile-meson.patch after addition of mu4e/mu4e-transient.el
    https://github.com/djcb/mu/commit/a93a41f8ee8f5144069d8a7e9bcd7d0b76cc6ed8

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.djcbsoftware.nl/code/mu/
 TERMUX_PKG_DESCRIPTION="Maildir indexer/searcher and Emacs client (mu4e)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.8"
+TERMUX_PKG_VERSION="1.12.9"
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bdb6fbeb8870aa422671fa0144d699900492a6f575c77fb5a7d60ac744214c27
+TERMUX_PKG_SHA256=4a45e18b8868f892273674ce24b3ab0687a65d4bf52279debcd40f807b3d9caf
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="emacs, glib, libc++, libxapian, libgmime"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dtests=disabled"
