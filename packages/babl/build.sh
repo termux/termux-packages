273e056caa88da0d0fc5b38359609e885368e809
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-11 08:05:32 +0200

    bump(main/babl): 0.1.112

--- packages/babl/build.sh
+++ packages/babl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gegl.org/babl/
 TERMUX_PKG_DESCRIPTION="Dynamic pixel format translation library"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.1.110"
-TERMUX_PKG_SRCURL=https://download.gimp.org/pub/babl/${TERMUX_PKG_VERSION%.*}/babl-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=bf47be7540d6275389f66431ef03064df5376315e243d0bab448c6aa713f5743
+TERMUX_PKG_VERSION="0.1.112"
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/babl/-/archive/BABL_${TERMUX_PKG_VERSION//./_}/babl-BABL_${TERMUX_PKG_VERSION//./_}.tar.gz
+TERMUX_PKG_SHA256=c2726c9c68085b8329c7d7cc41c415e42c21b9e8affb2ba09673175a94131ca7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="littlecms"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
@@ -18,4 +18,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	termux_setup_gir
+	cat <<- EOF > "$TERMUX_PKG_SRCDIR/git-version.h"
+	#ifndef BABL_GIT_VERSION
+	#define BABL_GIT_VERSION "$TERMUX_PKG_VERSION termux"
+	#endif
+	EOF
 }
