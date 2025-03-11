a92e23bab673d0a60398fe7e62c50474d761ca97
TomIO <tomjo00@web.de>
2025-03-11 09:27:32 +0100

    enhance(main/imagemagick): enable additional features - document enable status for all compile options - enable DJVu support - enable GSlib support - enable Graphviz support - enable JBIG2 support - enable Liblqr support - enable Libltdl support - enable Libraqm support - enable Zip support

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -4,27 +4,62 @@ TERMUX_PKG_LICENSE="ImageMagick"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="7.1.1.45"
 _VERSION="${TERMUX_PKG_VERSION%.*}-${TERMUX_PKG_VERSION##*.}"
-#TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://imagemagick.org/archive/releases/ImageMagick-${_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a02466ef5bbc0ed2a25975da72d1274f1b4818b359decec0995a387453115c7d
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, gdk-pixbuf, glib, harfbuzz, imath, libandroid-support, libbz2, libc++, libcairo, libheif, libjpeg-turbo, libjxl, liblzma, libpng, libraw, librsvg, libtiff, libwebp, libx11, libxext, libxml2, littlecms, openexr, openjpeg, pango, zlib"
+TERMUX_PKG_DEPENDS="djvulibre, fftw, fontconfig, freetype, gdk-pixbuf, ghostscript, glib, graphviz, harfbuzz, imath, jbig2dec, libandroid-support, libbz2, libc++, libcairo, libheif, libjpeg-turbo, libjxl, liblqr, libltdl, liblzma, libpng, libraqm, libraw, librsvg, libtiff, libwebp, libx11, libxext, libxml2, libzip, littlecms, openexr, openjpeg, pango, zlib"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---with-x
---without-gvc
---with-magick-plus-plus=yes
---with-bzlib=yes
---with-xml=yes
---with-rsvg=yes
---with-lzma
+--with-autotrace=no
+--with-djvu=yes
+--with-dmr=no
+--with-dps=no
+--with-fftw=yes
+--with-flif=no
+--with-fontconfig=yes
+--with-fpx=no
+--with-freetype=yes
+--with-gslib=yes
+--with-gvc=yes
+--with-heic=yes
+--with-jbig=yes
+--with-jpeg=yes
 --with-jxl=yes
---with-openexr
---with-fftw
+--with-lcms=yes
+--with-lqr=yes
+--with-ltdl=yes
+--with-lzma=yes
+--with-magick-plus-plus=yes
+--with-openexr=yes
+--with-openjp2=yes
+--with-pango=yes
+--with-perl=no
+--with-png=yes
+--with-raqm=yes
+--with-raw=yes
+--with-rsvg=yes
+--with-tiff=yes
+--with-uhdr=no
+--with-webp=yes
+--with-wmf=no
+--with-x=yes
+--with-xml=yes
+--with-zip=yes
+--with-zlib=yes
+--with-zstd=yes
 --disable-openmp
 ac_cv_func_ftime=no
 "
+# We do not currently have packages for:
+# `autotrace` AutoTrace
+# `dmr`       digital media repository
+# `dps`       <<Obsolete>>
+# `flif`      Free Lossless Image Format
+# `fpx`       libfpx (Flashpix)
+# `uhdr`      libultrahdr
+# `wmf`       Windows Metafile Format
 
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/ImageMagick-7/francais.xml
