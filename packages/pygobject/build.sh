1be042c57aa806d799b9f7249adf1687b83b3ed4
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-12 13:36:01 +0000

    rebuild(main/pygobject): Rebuild to fix the python native library
    The file names are fixed after f78036ddfab0ba83ea251942892f52f688b211ff commit.

--- packages/pygobject/build.sh
+++ packages/pygobject/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python package which provides bindings for GObject based
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.52.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/pygobject/${TERMUX_PKG_VERSION%.*}/pygobject-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8d487ff9b9fbef423919e97f7f1c93fee1fcd8f78b8d920c68e46847bcaee381
 TERMUX_PKG_AUTO_UPDATE=true
