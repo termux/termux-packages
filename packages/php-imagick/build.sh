385bb1de0e520b505f82f834d905a92dba79a639
TomIO <tomjo00@web.de>
2025-03-14 00:07:23 +0100

    chore(main/php-imagick): do not update to RC releases %ci:no-build

--- packages/php-imagick/build.sh
+++ packages/php-imagick/build.sh
@@ -9,7 +9,25 @@ TERMUX_PKG_SRCURL=https://github.com/Imagick/imagick/archive/refs/tags/${TERMUX_
 TERMUX_PKG_SHA256=aa2e311efb7348350c7332876252720af6fb71210d13268de765bc41f51128f9
 TERMUX_PKG_DEPENDS="php, imagemagick"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+TERMUX_PKG_UPDATE_VERSION_REGEXP='\d+\.\d+\.\d+'
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-regex"
+
+termux_pkg_auto_update() {
+	#
+	local latest_release
+	latest_release="$(termux_github_api_get_tag \
+		"${TERMUX_PKG_SRCURL}" \
+		"${TERMUX_PKG_UPDATE_TAG_TYPE}" \
+		"${TERMUX_PKG_UPDATE_VERSION_REGEXP}"
+	)"
+
+	if [[ "${latest_release}" == "${TERMUX_PKG_VERSION}" ]]; then
+		echo "INFO: No update needed. Already at version '${TERMUX_PKG_VERSION}'."
+		return
+	fi
+
+	termux_pkg_upgrade_version "${latest_release}"
+}
 
 termux_step_pre_configure() {
 	$TERMUX_PREFIX/bin/phpize
