a69a44e819612939887d8dd3049e992b3a81630a
TomIO <tomjo00@web.de>
2025-03-13 21:04:06 +0100

    DO NOT MERGE(main/coreutils): set optimization level to -O0 -g for debugging SELinux related issues

--- packages/coreutils/build.sh
+++ packages/coreutils/build.sh
@@ -2,13 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/coreutils/
 TERMUX_PKG_DESCRIPTION="Basic file, shell and text manipulation utilities from the GNU project"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
-TERMUX_PKG_VERSION=9.6
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/coreutils/coreutils-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_VERSION=9.6~debug
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/coreutils/coreutils-9.6.tar.xz
 TERMUX_PKG_SHA256=7a0124327b398fd9eb1a6abde583389821422c744ffa10734b24f557610d3283
 TERMUX_PKG_DEPENDS="libandroid-selinux, libandroid-support, libgmp, libiconv"
 TERMUX_PKG_BREAKS="chroot, busybox (<< 1.30.1-4)"
 TERMUX_PKG_REPLACES="chroot, busybox (<< 1.30.1-4)"
 TERMUX_PKG_ESSENTIAL=true
+
+TERMUX_DEBUG_BUILD=true
 
 # pinky has no usage on Android.
 # df does not work either, let system binary prevail.
