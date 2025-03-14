b1c3771092cebdc587f9e8d332116b74d4071fd9
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-14 22:53:25 +0200

    bump(main/megacmd): 2.0.0 (#23734)
    Fixes #23427

--- packages/megacmd/build.sh
+++ packages/megacmd/build.sh
@@ -2,20 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://mega.io/
 TERMUX_PKG_DESCRIPTION="Provides non UI access to MEGA services"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.7.0"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL=git+https://github.com/meganz/MEGAcmd
 TERMUX_PKG_GIT_BRANCH=${TERMUX_PKG_VERSION}_Linux
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 # dbus is required for $PREFIX/var/lib/dbus/machine-id
 TERMUX_PKG_DEPENDS="c-ares, cryptopp, dbus, ffmpeg, freeimage, libandroid-glob, libc++, libcurl, libicu, libsodium, libsqlite, libuv, mediainfo, openssl, pcre, readline, zlib"
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-static
---with-pcre=$TERMUX_PREFIX
---with-ffmpeg=$TERMUX_PREFIX
-ac_cv_lib_pthread_pthread_create=yes
+-DUSE_FFMPEG=ON
+-DUSE_PCRE=ON
+-DUSE_LIBUV=ON
+-DUSE_PDFIUM=OFF
+-DFULL_REQS=OFF
+-DVCPKG_ROOT=FALSE
+-DENABLE_MEGACMD_TESTS=OFF
 "
 
 termux_step_post_get_source() {
@@ -30,17 +31,11 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	autoreconf -fi
-
 	export OBJCXX="$CXX"
 
-	LDFLAGS+=" -landroid-glob"
+	LDFLAGS+=" -landroid-glob -lpcrecpp -lz"
 	LDFLAGS+=" $($CC -print-libgcc-file-name)"
 
 	# Fix build against FFmpeg 6.0:
 	CPPFLAGS+=" -DCODEC_CAP_TRUNCATED=0"
-}
-
-termux_step_post_massage() {
-	find lib -name '*.la' -delete
 }
