d5068a91f129ba571fccea57d38a623b1368e937
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-15 06:44:11 +0000

    bump(x11/kf6-knotifyconfig): 6.12.0

--- x11-packages/kf6-knotifyconfig/build.sh
+++ x11-packages/kf6-knotifyconfig/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Configuration system for KNotify'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.11.0"
+TERMUX_PKG_VERSION="6.12.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/knotifyconfig-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=612511161758144a9d3d99d4f0a9eb75931c3f8af5966e045a088023ca1d0c6f
+TERMUX_PKG_SHA256=53448f981d1656e423352a01723de2ab993e5bba39a73f6de422eb4deb2daaab
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), kf6-kio (>= ${TERMUX_PKG_VERSION}), libc++, libcanberra, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
