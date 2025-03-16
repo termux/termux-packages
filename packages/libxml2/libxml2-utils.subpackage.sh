d4902914cf89a74df30d68287cf5fa7b7be023b6
TomIO <43716232+TomJo2000@users.noreply.github.com>
2025-03-16 03:22:50 +0100

    enhance(main/libxml2): mark `libxml2-utils` as providing `xmllint` (#23807)

--- packages/libxml2/libxml2-utils.subpackage.sh
+++ packages/libxml2/libxml2-utils.subpackage.sh
@@ -1,2 +1,3 @@
 TERMUX_SUBPKG_INCLUDE="bin/xmllint bin/xmlcatalog share/man/man1/xmllint.1.gz share/man/man1/xmlcatalog.1.gz"
 TERMUX_SUBPKG_DESCRIPTION="XML utilities"
+TERMUX_SUBPKG_PROVIDES="xmllint"
