9424920925793f4d82c6ecc6645a42b4a7edc727
Robert Kirkman <rkirkman@termux.dev>
2025-03-16 11:06:15 -0500

    tree-wide: port debpython to termux
    - debpython is the commands `py3compile` and `py3clean` from Debian. I
      am calling them that because a large chunk of their source code is
      found inside a folder inside Debian's source code named "debpython"
      - https://salsa.debian.org/cpython-team/python3-defaults/-/tree/5348f704668c0b6c360b6c6fb10153b9c2898af5
    
    - rather than packaging `.pyc` files into `.deb` files, `py3compile` and
      `py3clean` can be called from `postinst` and `prerm` scripts to
      generate all `.pyc` for the `.py` files in the package immediately
      after the package is installed, and remove all `.pyc` files immediately
      before uninstalling the package, respectively
    
    - fixes the error `trying to overwrite '/data/data/com.termux/files/usr/lib/python3.12/__pycache__/cProfile.cpython-312.pyc'` when packages were built on-device, but at the same time, also:
    
    - prevents the warnings `dpkg: warning: while removing python, directory '/data/data/com.termux/files/usr/lib/python3.12/site-packages' not empty so not removed` as long as no packages were installed using `pip`
    
    - The revision-bumps that change `control.tar.xz` (debscripts) of packages only and are not otherwise visible are omitted from this PR, and will come in a second PR afterward.

--- packages/termux-gui-package/build.sh
+++ packages/termux-gui-package/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A Termux package containing utilities for Termux:GUI"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@tareksander"
 TERMUX_PKG_VERSION="0.1.6"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_DEPENDS="python, python-pip"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -12,11 +12,3 @@ TERMUX_PKG_SHA256="79a231f6550bde39c0bdd4eca0fce91b21df9c817345072c4859567437e48
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_PYTHON_TARGET_DEPS="termuxgui"
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!${TERMUX_PREFIX}/bin/sh
-	echo "Installing python bindings for Termux:GUI"
-	pip3 install --upgrade $TERMUX_PKG_PYTHON_TARGET_DEPS
-	EOF
-}
