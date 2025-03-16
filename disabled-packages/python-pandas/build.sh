343eefa71d0f5f10bcb40bba34d3825607a73fb4
Robert Kirkman <rkirkman@termux.dev>
2025-03-16 11:06:15 -0500

    tree-wide: port debpython to termux
    - debpython is the commands `py3compile` and `py3clean` from Debian. I
      am calling them that because a large chunk of their source code is
      found inside a folder inside Debian's source code named "debpython"
      - https://salsa.debian.org/cpython-team/python3-defaults/-/tree/5348f704668c0b6c360b6c6fb10153b9c2898af5
    
    - rather than packaging `.pyc` files into packages, `py3compile` and
      `py3clean` can be called from `postinst` and `prerm` scripts to
      generate all `.pyc` for the `.py` files in the package immediately
      after the package is installed, and remove all `.pyc` files immediately
      before uninstalling the package, respectively
    
    - fixes the error `trying to overwrite '/data/data/com.termux/files/usr/lib/python3.12/__pycache__/cProfile.cpython-312.pyc'` when packages were built on-device, but at the same time, also:
    
    - prevents the warnings `dpkg: warning: while removing python, directory '/data/data/com.termux/files/usr/lib/python3.12/site-packages' not empty so not removed` as long as no packages were installed using `pip`
    
    - The revision-bumps that change `control.tar.xz` (debscripts) of packages only and are not otherwise visible are omitted from this PR, and will come in a second PR afterward.
    
    - The `termux_step_create_python_debscripts.sh` can configure work on debpython (i.e. its `py3copile` and `py3clean` commands) from the glibc package `python-glibc`, if some glibc package is being compiled.
    
    - New variables have been implemented:
    - `TERMUX_PYTHON_CROSSENV_BUILDHOME` - location of crossenv's python build libraries.
    - `TERMUX_PKG_PYTHON_RUNTIME_DEPS` - configures the installation of the python modules via pip3 in the pkg's debscripts. If not configured in the package, it will use the value from `TERMUX_PKG_PYTHON_TARGET_DEPS`. If the variable is set to `false`, then the customization of installing python modules will be disabled, even if the `TERMUX_PKG_PYTHON_TARGET_DEPS` variable is set in the package.
    - `TERMUX_SUBPKG_PYTHON_RUNTIME_DEPS` - configures the installation of the python modules via pip3 in the subpkg's debscripts.
    
    - Implemented reconfiguration of prefixes in python module `sysconfig` and setting in `TERMUX_PYTHON_CROSSENV_BUILDHOME`, so that python modules from crossenv building can specify system paths of termux for correct compilation.
    
    - Added automatic addition of `python-glibc{-glibc}` dependency when using the `TERMUX_PKG_PYTHON_RUNTIME_DEPS` (for pkg; will be disabled, i.e. will not be added, if the variable is set to `false`) or `TERMUX_SUBPKG_PYTHON_RUNTIME_DEPS` (for subpkg) value.
    
    - setup-termux:
      - add comment and disabled dependency 'pandoc' to prepare for steps towards fixing on-device build of 'nala'
      - add 'python-pip' and implementation of installing 'system-wide' (Termux-wide) pip packages outside of venv in order to work towards closer accuracy of setup-termux.sh to setup-ubuntu.sh
      - add 'itstool' from PyPi system-wide to approximate the installation through 'apt' of 'itstool' system-wide in the Ubuntu cross-builder image
    
    Co-authored-by: Maxython <mixython@gmail.com>

--- disabled-packages/python-pandas/build.sh
+++ disabled-packages/python-pandas/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=1.5.3
 TERMUX_PKG_SRCURL=https://github.com/pandas-dev/pandas/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d8abf9c2bf33cac75b28f32c174c29778414eb249e5e2ccb69b1079b97a8fc66
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pip"
+TERMUX_PKG_DEPENDS="libc++, python, python-numpy"
 TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, numpy, wheel"
 TERMUX_PKG_PYTHON_TARGET_DEPS="python-dateutil, pytz"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -17,12 +17,4 @@ termux_step_pre_configure() {
 
 termux_step_make_install() {
 	pip install --no-deps --no-build-isolation . --prefix $TERMUX_PREFIX
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing dependencies through pip..."
-	pip3 install ${TERMUX_PKG_PYTHON_TARGET_DEPS//, / }
-	EOF
 }
