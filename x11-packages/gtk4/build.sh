ca46ea1c9a7cd49199bdafefd21a4dd868e6fef4
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-17 12:36:04 +0200

    bump(x11/gtk4): 4.18.2

--- x11-packages/gtk4/build.sh
+++ x11-packages/gtk4/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gtk.org/
 TERMUX_PKG_DESCRIPTION="GObject-based multi-platform GUI toolkit"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.16.12"
+TERMUX_PKG_VERSION="4.18.2"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gtk/${TERMUX_PKG_VERSION%.*}/gtk-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ef31bdbd6f082c4401634a20c850b0050c9bf252ef1e079764ee95a2a0c4c95a
-TERMUX_PKG_DEPENDS="adwaita-icon-theme, fontconfig, fribidi, gdk-pixbuf, glib, glib-bin, graphene, gtk-update-icon-cache, harfbuzz, iso-codes, libcairo, libepoxy, libjpeg-turbo, libpng, libtiff, libwayland, libx11, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxkbcommon, libxrandr, pango, shared-mime-info"
+TERMUX_PKG_SHA256=cca5677be4c3e501b49f1c50bcafacba8dcf4bd3a2d016388f834eff274dc159
+TERMUX_PKG_DEPENDS="adwaita-icon-theme, fontconfig, fribidi, gdk-pixbuf, glib, glib-bin, graphene, gtk-update-icon-cache, harfbuzz, iso-codes, libcairo, libdrm, libepoxy, libjpeg-turbo, libpng, libtiff, libwayland, libx11, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxkbcommon, libxrandr, pango, shared-mime-info"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, libwayland-protocols, xorgproto"
 TERMUX_PKG_RECOMMENDS="desktop-file-utils, librsvg, ttf-dejavu"
 TERMUX_PKG_VERSIONED_GIR=false
@@ -21,6 +21,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dmedia-gstreamer=disabled
 -Dprint-cups=disabled
 -Dvulkan=disabled
+-Dwayland-backend=true
+-Dx11-backend=true
 -Dwayland-backend=true
 "
 
