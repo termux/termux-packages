3c984cb2beb1c17e5fb6f5b65335fd92864bf7e6
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-17 19:11:11 +0000

    bump(x11/foot): 1.21.0

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://codeberg.org/dnkl/foot
 TERMUX_PKG_DESCRIPTION="Fast, lightweight and minimalistic Wayland terminal emulator"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.20.2"
+TERMUX_PKG_VERSION="1.21.0"
 TERMUX_PKG_SRCURL=https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9a8832e2485923c8a79e77958c8b0357abd159dfa8958609402362da8e56c073
+TERMUX_PKG_SHA256=b93b196a3fbab86678c54be627557bdc7b1fc8042d99b14c4a74b149f60bcd52
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, fontconfig, freetype, libfcft, libpixman, libwayland, libxkbcommon, utf8proc"
 TERMUX_PKG_BUILD_DEPENDS="libtllist, libwayland-protocols, scdoc, xdg-utils"
@@ -19,4 +19,6 @@ termux_step_pre_configure() {
 
 	# libandroid-support provides this
 	export CPPFLAGS+=" -D__STDC_ISO_10646__=201103L"
+
+	cp "${TERMUX_PKG_BUILDER_DIR}/reallocarray.c" "${TERMUX_PKG_SRCDIR}"
 }
