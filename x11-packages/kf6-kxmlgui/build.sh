7131c91cdbcca7bf3e7feb3a9c8b122cc716a433
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:21 +0530

    x11-packages/kf6-kxmlgui

--- x11-packages/kf6-kxmlgui/build.sh
+++ x11-packages/kf6-kxmlgui/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='User configurable main windows'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kxmlgui-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3c30a44e3d5176de36af56b6c29d86413fd58c6386679c6222aa4d0b46d0d88c
 TERMUX_PKG_DEPENDS="kf6-kcolorscheme (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kconfigwidgets (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-kglobalaccel (>= ${TERMUX_PKG_VERSION}), kf6-kguiaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), kf6-kiconthemes (>= ${TERMUX_PKG_VERSION}), kf6-kitemviews (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
