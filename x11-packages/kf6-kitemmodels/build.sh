14c2954a9672908f480b0d7a51fccf24b1de9862
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:19 +0530

    x11-packages/kf6-kitemmodels

--- x11-packages/kf6-kitemmodels/build.sh
+++ x11-packages/kf6-kitemmodels/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Set of item models extending the Qt model-view framework
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kitemmodels-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=62688311082b6aca226117810bc3991b0cb95158dec4d4c800d70a99bdcfdfc8
 TERMUX_PKG_AUTO_UPDATE=true
