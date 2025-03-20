89af31c4954db71ac2a5a528ea1738f0e8441658
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:17 +0530

    x11-packages/kf6-kcompletion

--- x11-packages/kf6-kcompletion/build.sh
+++ x11-packages/kf6-kcompletion/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Text completion helpers and widgets'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcompletion-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=8cd311795b5d15c07b21b36c1124ce4f1aa0a18a3a03fff29608e4cadd091a9c
 TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
