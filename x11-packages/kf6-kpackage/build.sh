a3bce82fa8d7dfcc8c93763029f0218983088af9
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:20 +0530

    x11-packages/kf6-kpackage

--- x11-packages/kf6-kpackage/build.sh
+++ x11-packages/kf6-kpackage/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Framework that lets applications manage user installable
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kpackage-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a8d6505fa6a53bfdcc31fa8f2ad0a9226ec9fafabc0c0120490fbeb6b017fb4a
 TERMUX_PKG_DEPENDS="kf6-karchive (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
