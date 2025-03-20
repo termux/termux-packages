9ce918964b622b32e9e1a638cc1dfd8b79c1fe9b
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:18 +0530

    x11-packages/kf6-kiconthemes

--- x11-packages/kf6-kiconthemes/build.sh
+++ x11-packages/kf6-kiconthemes/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Support for icon themes'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kiconthemes-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=54b1b5f2839162d980873328716b30cf08f90fd079ad854cc09878ad9b376ecf
 TERMUX_PKG_DEPENDS="kf6-breeze-icons (>= ${TERMUX_PKG_VERSION}), kf6-karchive (>= ${TERMUX_PKG_VERSION}), kf6-kcolorscheme (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kconfigwidgets (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase, qt6-qtdeclarative"
