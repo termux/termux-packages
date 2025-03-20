24f25e911c0606700c981ff5d0abad7ea1962361
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:18 +0530

    x11-packages/kf6-kcoreaddons

--- x11-packages/kf6-kcoreaddons/build.sh
+++ x11-packages/kf6-kcoreaddons/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Utilities for core application functionality and accessi
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcoreaddons-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=7b37e8d4e185bcdd8bfd166c739f7adbcb0730cdf140d64a308f7b96b1845736
 TERMUX_PKG_AUTO_UPDATE=true
