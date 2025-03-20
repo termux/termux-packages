4906d21d27ae2e1929065476575c9b5f6ecb3067
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:17 +0530

    x11-packages/kf6-kbookmarks

--- x11-packages/kf6-kbookmarks/build.sh
+++ x11-packages/kf6-kbookmarks/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Support for bookmarks and the XBEL format'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kbookmarks-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=1b6edc20a7a797fe2a190d5a78a3b6d0f46d1e7b1b2c81646de3176bf74f8e3a
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kconfigwidgets (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
