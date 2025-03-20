7211ee01f13c2fea75c134e34db0525742637167
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:20 +0530

    x11-packages/kf6-kservice

--- x11-packages/kf6-kservice/build.sh
+++ x11-packages/kf6-kservice/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Advanced plugin and service introspection'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kservice-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=9533b93a9504ddf2f1e8e696bcfada0c3791019ee8e8cfbee04d9d1d14b6b964
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
