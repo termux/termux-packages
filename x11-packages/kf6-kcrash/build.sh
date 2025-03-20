5e5955a195bcd47fcacc6245dee0398e35e33ad4
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:18 +0530

    x11-packages/kf6-kcrash

--- x11-packages/kf6-kcrash/build.sh
+++ x11-packages/kf6-kcrash/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Support for application crash analysis and bug report fr
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcrash-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c0b96110d25faa35587ac4af30cd1e3a8012133fcac12276e8b25977cd9b84a4
 TERMUX_PKG_DEPENDS="kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), libc++, libx11, qt6-qtbase"
