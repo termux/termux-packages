6ec27674e6bde655ecad3272c612b31037a62702
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-20 12:01:17 +0530

    x11-packages/kf6-kconfigwidgets

--- x11-packages/kf6-kconfigwidgets/build.sh
+++ x11-packages/kf6-kconfigwidgets/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Widgets for KConfig'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kconfigwidgets-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=66da62c90b6b06e5bec68e691f70c9c302fe4dc5fd4de50035957da7f468d266
 TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${TERMUX_PKG_VERSION}), kf6-kcolorscheme (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION}), libc++, qt6-qtbase"
