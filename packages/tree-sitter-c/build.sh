6d0955102603a32e73a209fe4faafeb94d637e0c
TomIO <tomjo00@web.de>
2025-03-21 21:19:18 +0100

    chore(main/tree-sitter-{c,lua,query,vim,vimdoc}): fix build process

--- packages/tree-sitter-c/build.sh
+++ packages/tree-sitter-c/build.sh
@@ -1,20 +1,14 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter
+TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter/tree-sitter-c
 TERMUX_PKG_DESCRIPTION="C grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.23.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter-c/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f7e50412230150ce514efcccb962ff9b452d9f358e0a2c89f2a0a0256c2ec886
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="
-PARSER_URL=https://github.com/tree-sitter/tree-sitter-c
-"
-
-termux_step_pre_configure() {
-	rm setup.py pyproject.toml
-}
 
 termux_step_post_make_install() {
 	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
--- packages/tree-sitter-vimdoc/build.sh
+++ packages/tree-sitter-vimdoc/build.sh
@@ -1,18 +1,22 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/neovim/tree-sitter-vimdoc
 TERMUX_PKG_DESCRIPTION="Tree-sitter parser for Vim help files"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="3.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/neovim/tree-sitter-vimdoc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=76b65e5bee9ff78eb21256619b1995aac4d80f252c19e1c710a4839481ded09e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
+# This one is different from the tree-sitter-grammars/* parsers
+# It does not have a CMakeLists.txt file, so we're using `make`
 TERMUX_PKG_EXTRA_MAKE_ARGS="
 PARSER_URL=https://github.com/neovim/tree-sitter-vimdoc
 "
 
 termux_step_pre_configure() {
+	# these need to be removed for this one, hell if I know why
 	rm setup.py pyproject.toml
 }
 
