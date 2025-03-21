9893734989d223faf1d2238db9949fd5fb3d6a8f
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-21 16:35:56 +0000

    fix(main/imlib2): Link modules with main imlib library
    This fixes loading images with feh, imlib2_view etc. programs.

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING-PLAIN"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.12.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=bf07ff09255ed30f05a1c252d4b578426c2dcdbbc28e8c714adf060690395720
 TERMUX_PKG_AUTO_UPDATE=true
@@ -11,4 +12,5 @@ TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, giflib, glib, libandroid-shmem, libbz2
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-shmem -lm"
+	autoreconf -fi
 }
