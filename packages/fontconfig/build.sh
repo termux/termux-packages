65f92d791f2e187e8fb76de03d283253f6478649
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-21 14:29:31 +0200

    bump(main/fontconfig): 2.16.1

--- packages/fontconfig/build.sh
+++ packages/fontconfig/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/fontconfig/
 TERMUX_PKG_DESCRIPTION="Library for configuring and customizing font access"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.16.0"
-TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/fontconfig/release/fontconfig-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=6a33dc555cc9ba8b10caf7695878ef134eeb36d0af366041f639b1da9b6ed220
+TERMUX_PKG_VERSION="2.16.1"
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/$TERMUX_PKG_VERSION/fontconfig-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=570e70cc481b871683dec38040d6a40a08068c685277cae9ad7ec5a717040203
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, libexpat, ttf-dejavu"
 TERMUX_PKG_BREAKS="fontconfig-dev"
@@ -16,3 +16,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-default-fonts=/system/fonts
 --with-add-fonts=$TERMUX_PREFIX/share/fonts
 "
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
