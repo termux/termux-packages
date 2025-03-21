3d9c08c53732f6c429e0fb507a4d7bba97797e89
Henrik Grimler <grimler@termux.dev>
2025-03-21 10:44:16 +0100

    radare2: sync with master branch
    radare2 4.0.0 segfaults on android-5. I needed radare2 to do some
    debugging, so congraulations radare2, you will now be the most up
    to date android-5 package.

--- packages/radare2/build.sh
+++ packages/radare2/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://rada.re
 TERMUX_PKG_DESCRIPTION="Advanced Hexadecimal Editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION=4.0.0
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256=7621e38558917dc4b469acf2aa33d7cab6414420a0a1d205c55b9c96423e9cee
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="5.9.8"
 TERMUX_PKG_SRCURL=https://github.com/radare/radare2/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=e45e4fd342f04b2e00363bc1b68cc375c1cf36041085d3d59caa7a3b7be43836
 TERMUX_PKG_DEPENDS="libuv"
 TERMUX_PKG_BREAKS="radare2-dev"
 TERMUX_PKG_REPLACES="radare2-dev"
@@ -19,9 +19,8 @@ termux_step_pre_configure() {
 	fi
 
 	# Unset CPPFLAGS to avoid -I$TERMUX_PREFIX/include. This is because
-	# radare2 build will put it's own -I flags after ours, which causes
+	# radare2 build will put its own -I flags after ours, which causes
 	# problems due to name clashes (binutils header files).
-	cp libr/util/mem.c libr/hash/mem.c
 	unset CPPFLAGS
 
 	# If this variable is not set, then build will fail on linking with 'pthread'
