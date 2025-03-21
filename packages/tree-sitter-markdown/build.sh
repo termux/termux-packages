50f37d345ea83f6c816ce3791752509e117e0b34
TomIO <tomjo00@web.de>
2025-03-21 20:26:34 +0100

    bump(main/tree-sitter-markdown): 0.4.1 - setup nodejs for building - properly symlink markdown-inline parser

--- packages/tree-sitter-markdown/build.sh
+++ packages/tree-sitter-markdown/build.sh
@@ -1,22 +1,23 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars
-TERMUX_PKG_DESCRIPTION="Tree-sitter parser for Markdown"
+TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-markdown
+TERMUX_PKG_DESCRIPTION="Markdown grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.3.2"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
+TERMUX_PKG_VERSION="0.4.1"
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-markdown/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5dac48a6d971eb545aab665d59a18180d21963afc781bbf40f9077c06cb82ae5
+TERMUX_PKG_SHA256=e0fdb2dca1eb3063940122e1475c9c2b069062a638c95939e374c5427eddee9f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="
-PARSER_URL=https://github.com/tree-sitter-grammars/tree-sitter-markdown
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DALL_EXTENSIONS=ON
 "
 
 termux_step_pre_configure() {
-	rm setup.py pyproject.toml
+	termux_setup_nodejs
 }
 
 termux_step_post_make_install() {
 	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
 	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown.so
+	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown-inline.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown-inline.so
 }
