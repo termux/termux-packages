cd1a179e41edc52f0f7a372f2e28f82b48a748bb
TomIO <tomjo00@web.de>
2025-03-21 21:19:18 +0100

    chore(main/tree-sitter-{c,lua,query,vim,vimdoc}): fix build process

--- packages/tree-sitter-vim/build.sh
+++ packages/tree-sitter-vim/build.sh
@@ -1,18 +1,17 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars
+TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-vim
 TERMUX_PKG_DESCRIPTION="Vimscript grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.5.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-vim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=90019d12d2da0751c027124f27f5335babf069a050457adaed53693b5e9cf10a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="
-PARSER_URL=https://github.com/tree-sitter-grammars/tree-sitter-vim
-"
 
 termux_step_pre_configure() {
+	# these need to be removed for this one, hell if I know why
 	rm setup.py pyproject.toml
 }
 
