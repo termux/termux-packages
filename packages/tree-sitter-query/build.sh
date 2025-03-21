115bf09fc773cd2cc432356132fc138aa84ec926
TomIO <tomjo00@web.de>
2025-03-21 21:19:18 +0100

    chore(main/tree-sitter-{c,lua,query,vim,vimdoc}): fix build process

--- packages/tree-sitter-query/build.sh
+++ packages/tree-sitter-query/build.sh
@@ -1,20 +1,14 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars
+TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-query
 TERMUX_PKG_DESCRIPTION="TS query grammar for tree-sitter"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.5.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-query/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fe8c712880a529d454347cd4c58336ac2db22243bae5055bdb5844fb3ea56192
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="
-PARSER_URL=https://github.com/tree-sitter-grammars/tree-sitter-query
-"
-
-termux_step_pre_configure() {
-	rm setup.py pyproject.toml
-}
 
 termux_step_post_make_install() {
 	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
