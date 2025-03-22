c2a005af778c814c12e1074389765c60987a7558
Robert Kirkman <rkirkman@termux.dev>
2025-03-21 21:46:48 -0500

    bump(x11/sfml): 3.0.0

--- x11-packages/sfml/build.sh
+++ x11-packages/sfml/build.sh
@@ -2,11 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://www.sfml-dev.org/
 TERMUX_PKG_DESCRIPTION="A simple, fast, cross-platform and object-oriented multimedia API"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.6.2"
+TERMUX_PKG_VERSION="3.0.0"
 TERMUX_PKG_SRCURL=https://github.com/SFML/SFML/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=15ff4d608a018f287c6a885db0a2da86ea389e516d2323629e4d4407a7ce047f
+TERMUX_PKG_SHA256=37506fafbd618b1f8e153bbca8811e62203a70b32a1183279fb9612fd0501d2b
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxcursor, libxrandr, openal-soft, opengl"
+TERMUX_PKG_DEPENDS="freetype, libc++, libflac, libogg, libvorbis, libx11, libxcursor, libxi, libxrandr, openal-soft, opengl"
+# -DBUILD_SHARED_LIBS=ON is required to prevent this error when importing SFML using CMake:
+# Requested SFML configuration (Shared) was not found
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_SHARED_LIBS=ON
+-DSFML_USE_SYSTEM_DEPS=ON
+"
 
 termux_step_post_get_source() {
 	cp src/SFML/Window/Android/JoystickImpl.{cpp,hpp} src/SFML/Window/Unix/
