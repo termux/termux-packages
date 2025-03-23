a1d803550851465ba3aea77690d4d02b6f319c53
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-03-23 23:13:27 +0200

    bump(x11/menu-cache): 1.1.1, revbump(x11/libfm{,-qt}): revdeps

--- x11-packages/menu-cache/build.sh
+++ x11-packages/menu-cache/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/lxde/
 TERMUX_PKG_DESCRIPTION="Caching mechanism for freedesktop.org compliant menus"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.1.0
-TERMUX_PKG_REVISION=26
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/lxde/menu-cache-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=ed02eb459dcb398f69b9fa5bf4dd813020405afc84331115469cdf7be9273ec7
+TERMUX_PKG_VERSION="1.1.1"
+TERMUX_PKG_SRCURL=https://github.com/lxde/menu-cache/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e8af90467df271c3c8700c840ca470ca2915699c6f213c502a87d74608748f08
 TERMUX_PKG_DEPENDS="glib, libfm-extra"
 
 termux_step_pre_configure() {
 	CFLAGS+=" -fcommon"
+	autoreconf -fi
 }
