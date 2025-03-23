ff0cbf349c4a4c94eb2db712493e6882a6cbdb26
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-23 18:52:10 +0000

    bump(x11/lxde-icon-theme): 0.5.2
    Move to new upstream repository.

--- x11-packages/lxde-icon-theme/build.sh
+++ x11-packages/lxde-icon-theme/build.sh
@@ -1,12 +1,16 @@
-TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/lxde/
+TERMUX_PKG_HOMEPAGE=https://github.com/lxde/lxde-icon-theme
 TERMUX_PKG_DESCRIPTION="LXDE default icon theme based on nuoveXT2"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.5.1
-TERMUX_PKG_REVISION=22
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/lxde/lxde-icon-theme-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e3d0b7399f28a360a3755171c9a08147d68f853f518be5485f5064675037916c
+TERMUX_PKG_VERSION="0.5.2"
+TERMUX_PKG_SRCURL=https://github.com/lxde/lxde-icon-theme/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=5b71da247ba25ddcd991a3a184ca5ac92f40b7676766e1e59437067a20f7ecf7
 TERMUX_PKG_DEPENDS="gtk-update-icon-cache"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+
+termux_step_pre_configure() {
+	autoreconf -fiv
+}
