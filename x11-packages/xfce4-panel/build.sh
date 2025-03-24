f01a842c0a46d66ac6bf72f57b0297dd1433ef8a
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-24 07:47:49 +0000

    bump(x11/xfce4-panel): 4.20.4
    Add workaround for the following autotools error.
    configure: error: Could not find xdt-gen-visibility in PATH, please install xfce4-dev-tools

--- x11-packages/xfce4-panel/build.sh
+++ x11-packages/xfce4-panel/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/xfce/xfce4-panel/start
 TERMUX_PKG_DESCRIPTION="Panel for the XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.20.3"
+TERMUX_PKG_VERSION="4.20.4"
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/xfce4-panel/${TERMUX_PKG_VERSION%.*}/xfce4-panel-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4006dddf465a4ae02e14355941458c718f6da0896eae68435c9fd24fcd89b6b8
+TERMUX_PKG_SHA256=695b23af490719e734c8659394821b43cc94d3bee69994bafdc42ef40daa0d2c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, exo, garcon, gdk-pixbuf, glib, gtk3, gtk-layer-shell, harfbuzz, libcairo, libdisplay-info, libice, libsm, libwayland, libwnck, libx11, libxext, libxrandr, libxfce4ui, libxfce4util, libxfce4windowing, pango, xfconf, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xfce4-dev-tools"
@@ -21,9 +21,12 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-wayland
 --enable-x11
 --disable-dbusmenu-gtk3
+XDT_GEN_VISIBILITY=${TERMUX_PREFIX}/bin/xdt-gen-visibility
 "
 
 termux_step_pre_configure() {
 	termux_setup_gir
 	termux_setup_glib_cross_pkg_config_wrapper
+
+	autoreconf -fiv
 }
