6004b8df1166722c5f1bd0df2fbc8c6a1ca158b2
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-26 15:19:09 +0000

    bump(x11/lxappearance): 0.6.4
    Move to new upstream repository, previously https://sourceforge.net/projects/lxde/

--- x11-packages/lxappearance/build.sh
+++ x11-packages/lxappearance/build.sh
@@ -2,11 +2,16 @@ TERMUX_PKG_HOMEPAGE=http://www.lxde.org/
 TERMUX_PKG_DESCRIPTION="LXDE GTK+ theme switcher"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
-TERMUX_PKG_VERSION=0.6.3
-TERMUX_PKG_REVISION=7
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/lxde/lxappearance-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7222d858b8fef4b7967c42142d61e82ded6dd42dc5ef1d59caad775795928b38
-TERMUX_PKG_DEPENDS="dbus-glib, glib, gtk3, libx11, xsltproc"
+TERMUX_PKG_VERSION="0.6.4"
+TERMUX_PKG_SRCURL=https://github.com/lxde/lxappearance/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9f067a8a126b9779ba12648c76136d9ba3e7ec7920c568df7819d128fdf39e03
+TERMUX_PKG_DEPENDS="dbus-glib, gdk-pixbuf, glib, gtk3, libx11, xsltproc"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gtk3 --enable-dbus"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_MAKE_PROCESSES=1
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+
+termux_step_pre_configure() {
+	autoreconf -fiv
+}
