f08d93d6b8944c2402a820c152639e282b81274b
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-26 13:26:20 +0000

    bump(x11/gtkmm3): 3.24.10

--- x11-packages/gtkmm3/build.sh
+++ x11-packages/gtkmm3/build.sh
@@ -2,9 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.gtkmm.org/
 TERMUX_PKG_DESCRIPTION="The C++ API for GTK"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.24.9"
+TERMUX_PKG_VERSION="3.24.10"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gtkmm/${TERMUX_PKG_VERSION%.*}/gtkmm-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=30d5bfe404571ce566a8e938c8bac17576420eb508f1e257837da63f14ad44ce
+TERMUX_PKG_SHA256=7ab7e2266808716e26c39924ace1fb46da86c17ef39d989624c42314b32b5a76
+# Prevent updating to incompatible 4.x.x versions
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libatkmm-1.6, libc++, libcairomm-1.0, libglibmm-2.4, libpangomm-1.4, libsigc++-2.0"
 TERMUX_PKG_BUILD_DEPENDS="glib-cross, libepoxy"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
