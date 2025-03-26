57832aa3740dc80e8aa466cc06a41db0d1478b1a
Aditya Alok <alok@termux.dev>
2025-03-26 15:37:34 +0530

    pkg(main/shellcheck): Adapt to ghc 9.12.2 changes
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/shellcheck/build.sh
+++ packages/shellcheck/build.sh
@@ -3,15 +3,11 @@ TERMUX_PKG_DESCRIPTION="Shell script analysis tool"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION=0.10.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/ShellCheck-${TERMUX_PKG_VERSION}/ShellCheck-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=4d08db432d75a34486a55f6fff9d3e3340ce56125c7804b7f8fd14421b936d21
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="libffi"
-TERMUX_PKG_BUILD_DEPENDS="ghc-libs"
-TERMUX_PKG_EXCLUDED_ARCHES="arm"
-# arm build fails complaining that it can't find `opt-13`
-# symlinking `opt-16` to `opt-13` does not work. Blacklisting it for now.
+TERMUX_PKG_DEPENDS="libffi, libgmp, libiconv"
 
 termux_step_pre_configure() {
 	chmod u+x ./striptests
