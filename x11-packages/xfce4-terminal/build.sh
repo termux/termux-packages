a0b9884c8f9a6bf5d987d371949d4143c9d7407e
Biswapriyo Nath <nathbappai@gmail.com>
2025-03-27 07:06:49 +0000

    bump(x11/xfce4-terminal): 1.1.5
    Port to meson which is default now.

--- x11-packages/xfce4-terminal/build.sh
+++ x11-packages/xfce4-terminal/build.sh
@@ -2,17 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/terminal/start
 TERMUX_PKG_DESCRIPTION="Terminal Emulator for the XFCE environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.1.4"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-terminal/${TERMUX_PKG_VERSION%.*}/xfce4-terminal-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=873c921da1f4b986ffb459d4960789c9c063af98648c9f0ca146dc6f6f5b71b7
+TERMUX_PKG_VERSION="1.1.5"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-terminal/${TERMUX_PKG_VERSION%.*}/xfce4-terminal-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=3c5b1d3a01a9a113852ac0f77d1c85bf3a356b43de33ec805b21ceca7d6f0a63
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, gtk-layer-shell, libcairo, libvte, libx11, libxfce4ui, libxfce4util, pango, pcre2, xfconf"
 TERMUX_PKG_RECOMMENDS="desktop-file-utils, hicolor-icon-theme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-debug
---enable-gtk-doc-html=no
---enable-gtk-layer-shell
---enable-wayland
---enable-x11
+-Dgtk-layer-shell=enabled
+-Dwayland=enabled
+-Dx11=enabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_glib_cross_pkg_config_wrapper
+}
