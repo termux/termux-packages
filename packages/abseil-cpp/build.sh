17beb11500d47d0a25f37e2342da89bfc0cfd433
Yaksh Bariya <yakshbari4@gmail.com>
2025-03-29 12:48:48 +0530

    bump(main/abseil-cpp): 20250127.1
    A patch release with just a minor change in internal symbol demangling
    logic. Does not need a rebuild of dependent packages
    
    Diff between the previous release and current patch release:
    
    https://github.com/abseil/abseil-cpp/compare/20250127.0...20250127.1

--- packages/abseil-cpp/build.sh
+++ packages/abseil-cpp/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Abseil C++ Common Libraries"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Do not forget to rebuild revdeps along with EVERY "major" version bump.
-TERMUX_PKG_VERSION="20250127.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="20250127.1"
 TERMUX_PKG_SRCURL=https://github.com/abseil/abseil-cpp/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=16242f394245627e508ec6bb296b433c90f8d914f73b9c026fddb905e27276e8
+TERMUX_PKG_SHA256=b396401fd29e2e679cace77867481d388c807671dc2acc602a0259eeb79b7811
 # updating this will break libprotobuf
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libc++"
