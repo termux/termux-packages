955971e64bf99d1fcd82fdb6f04211941155c0ed
Henrik Grimler <grimler@termux.dev>
2025-03-29 22:09:47 +0100

    updpkg(main/termux-api): 0.59.1

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.59.0
+TERMUX_PKG_VERSION=0.59.1
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c2f68cc3bd848bc386c64b06bffdb5fe96f14820017c3e80c0afc77f36a8789e
+TERMUX_PKG_SHA256=92c2da07991a0191735539428aabb793e0ddb8e33baac305bcf38d77aa1eda80
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash, util-linux, termux-am (>= 0.8.0)"
