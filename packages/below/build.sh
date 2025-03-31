8968bd7c92bfad938e218bd806fde4d5accfbff2
Aditya Alok <alok@termux.dev>
2025-03-31 22:05:10 +0530

    bump(main/below): 0.9.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/below/build.sh
+++ packages/below/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/facebookincubator/below
 TERMUX_PKG_DESCRIPTION="An interactive tool to view and record historical system data"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.8.1"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.9.0"
 TERMUX_PKG_SRCURL=https://github.com/facebookincubator/below/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bfeb9cd911e0477a5428ee0b6cbf7cbdc7eba90b716ac1e4f6cbadcde2ffbcb1
+TERMUX_PKG_SHA256=c51c202e9c15aafc1c741f053ba374c302dc0a737aef6a1120b5c7957ab013fe
 TERMUX_PKG_DEPENDS="libelf, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -33,6 +32,10 @@ termux_step_pre_configure() {
 			patch --silent -p1 -d ${d} \
 				< "$TERMUX_PKG_BUILDER_DIR/${p}" || :
 		done
+	done
+
+	for d in "$CARGO_HOME"/registry/src/*/nix-*; do
+		patch --silent -p1 -d "$d" <  "$TERMUX_PKG_BUILDER_DIR"/nix-0.20.0-src-net-if_.rs.diff || :
 	done
 
 	local _CARGO_TARGET_LIBDIR=target/$CARGO_TARGET_NAME/release/deps
