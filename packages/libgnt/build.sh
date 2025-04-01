359f7d28914cb1030c94d9b633ae5c6f6e500012
TomIO <tomjo00@web.de>
2025-04-01 05:14:50 +0200

    revbump(main/libgnt): after libxml2 update

--- packages/libgnt/build.sh
+++ packages/libgnt/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An ncurses toolkit for creating text-mode graphical user
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.14.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/pidgin/libgnt/${TERMUX_PKG_VERSION}/libgnt-${TERMUX_PKG_VERSION}-dev.tar.xz
 TERMUX_PKG_SHA256=195933a9a731d3575791b881ba5cc0ad2a715e1e9c4c23ccaaa2a17e164c96ec
 TERMUX_PKG_DEPENDS="glib, libxml2, ncurses, ncurses-ui-libs"
