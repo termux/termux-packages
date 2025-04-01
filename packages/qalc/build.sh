a800285a88fccf48de07742599f466cecfeb0a3e
TomIO <tomjo00@web.de>
2025-04-01 04:27:38 +0200

    revbump(main/qalc): after libxml2 update

--- packages/qalc/build.sh
+++ packages/qalc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Powerful and easy to use command line calculator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.5.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Qalculate/libqalculate/releases/download/v$TERMUX_PKG_VERSION/libqalculate-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fc14d4527dccfc9189684b9fcd64d78aa67ae5383012c66e1596b2531f5daef1
 TERMUX_PKG_AUTO_UPDATE=true
