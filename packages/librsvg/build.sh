8915f8c2eeddc7b743534f68abb8c99aa626e7a6
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-01 15:50:05 +0000

    enhance(main/librsvg): Enable building vala bindings

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.60.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/librsvg/${TERMUX_PKG_VERSION%.*}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0b6ffccdf6e70afc9876882f5d2ce9ffcf2c713cbaaf1ad90170daa752e1eec3
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="fontconfig, freetype, gdk-pixbuf, glib, harfbuzz, libcairo, libpng, libxml2, pango"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner"
+TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, valac"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
 TERMUX_PKG_VERSIONED_GIR=false
@@ -16,6 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Ddocs=disabled
 -Dintrospection=enabled
 -Dtests=false
+-Dvala=enabled
 "
 
 termux_step_pre_configure() {
