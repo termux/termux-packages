092f99e5045c4b429372faf94443d9a758a26c57
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-04-03 09:08:46 +0300

    bump(main/unshield): 1.6.1

--- packages/unshield/build.sh
+++ packages/unshield/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/twogood/unshield
 TERMUX_PKG_DESCRIPTION="Tool and library to extract CAB files from InstallShield installers"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.6.0"
+TERMUX_PKG_VERSION="1.6.1"
 TERMUX_PKG_SRCURL=https://github.com/twogood/unshield/archive/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=c3974a906ddbdc2805b3f6b36cb01f11fe0ede7a7702514acb2ad4a66ec7ae62
+TERMUX_PKG_SHA256=3f477d177e5ab805d41e5d06bb8cc42540769dd937ddc78e2e07f9f853034d66
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libiconv, openssl, openssh, zlib"
 
