726c5467a408366424742d3054f7832cbf304ec2
strobecat <203321455+strobecat@users.noreply.github.com>
2025-04-03 13:57:37 +0000

    bump(main/libusbmuxd): 2.1.0-p20250216

--- packages/libusbmuxd/build.sh
+++ packages/libusbmuxd/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org
 TERMUX_PKG_DESCRIPTION="A client library for applications to handle usbmux protocol connections with iOS devices"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=f47c36f5bd2a653a3bd7fb1cf1d2c50b0e6193fb
-_COMMIT_DATE=20230430
-TERMUX_PKG_VERSION=2.0.2-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=2
+_COMMIT=19d6bec393c9f9b31ccb090059f59268da32e281
+_COMMIT_DATE=20250216
+TERMUX_PKG_VERSION=2.1.0-p${_COMMIT_DATE}
 TERMUX_PKG_SRCURL=git+https://github.com/libimobiledevice/libusbmuxd
-TERMUX_PKG_SHA256=c415b8859ccae494387c14dc9c297910843b16b5ba62aab15d51116137b513c4
+TERMUX_PKG_SHA256=93a2995dfc0b7d690bbf2da62f9e523f7505086baf64fd8d338911114c931686
 TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="libimobiledevice-glue, libplist, libusb"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libimobiledevice-glue, libplist"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-preflight
 --without-systemd
@@ -35,7 +34,7 @@ termux_step_post_get_source() {
 
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=6
+	local _SOVERSION=7
 
 	local e=$(sed -En 's/LIBUSBMUXD_SO_VERSION="?([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
 				configure.ac)
