149ba67c2432b4dcdf308a50e415135872fa1469
Aditya Alok <alok@termux.dev>
2025-04-03 20:34:00 +0530

    scripts(free-space): Create swapfile to account for large memory requirements
    - See: https://github.com/termux/termux-packages/pull/23911
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- scripts/free-space.sh
+++ scripts/free-space.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# This script clears about ~22G of space.
+# This script clears about ~22G of space. It also sets up a swapfile to account
+# for packages requiring large memory.
 
 # Test:
 # echo "Listing 100 largest packages after"
 # dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 # exit 0
+
+create_swapfile() {
+	sudo fallocate -l 4G /swapfile
+	sudo chmod 600 /swapfile
+	sudo mkswap /swapfile
+	sudo swapon /swapfile
+}
 
 if [ "${CI-false}" != "true" ]; then
 	echo "ERROR: not running on CI, not deleting system files to free space!"
@@ -59,4 +67,6 @@ else
 
 	sudo apt autoremove -yq
 	sudo apt clean
+
+	create_swapfile
 fi
