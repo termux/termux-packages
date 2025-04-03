7fb269eb28b0e744d9705f54866b1e65cf72cac3
strobecat <203321455+strobecat@users.noreply.github.com>
2025-04-03 13:58:14 +0000

    bump(main/libimobiledevice): 1.3.0-p20250228

--- packages/libimobiledevice/build.sh
+++ packages/libimobiledevice/build.sh
@@ -1,16 +1,15 @@
 TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org/
 TERMUX_PKG_DESCRIPTION="A library to communicate with services on iOS devices using native protocols"
 TERMUX_PKG_LICENSE="LGPL-2.1-or-later"
-_COMMIT=860ffb707af3af94467d2ece4ad258dda957c6cd
-_COMMIT_DATE=20230430
+_COMMIT=a6b6c35d1550acbd2552d49c2fe38115deec8fc0
+_COMMIT_DATE=20250228
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.0-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=git+https://github.com/libimobiledevice/libimobiledevice
-TERMUX_PKG_SHA256=5b88f3b348f06fe9d1b5ffc639f813b5956d757e527d5c7ca724a64c1b0b3b4e
+TERMUX_PKG_SHA256=262a9c35252bf1418de40454268cfd958012c25aab8b6f8067673717b9db9e42
 TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="libimobiledevice-glue, libplist, libusbmuxd, openssl"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libimobiledevice-glue, libplist, libtatsu, libusbmuxd, openssl"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
