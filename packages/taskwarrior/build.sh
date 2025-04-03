493197318f1d7dbff954db8fcd1cbe8bb4293971
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-04-03 19:31:00 +0300

    bump(main/taskwarrior): 3.4.1
    Fixes #23796

--- packages/taskwarrior/build.sh
+++ packages/taskwarrior/build.sh
@@ -2,18 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://taskwarrior.org
 TERMUX_PKG_DESCRIPTION="Utility for managing your TODO list"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.4.0"
+TERMUX_PKG_VERSION="3.4.1"
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskwarrior/releases/download/v${TERMUX_PKG_VERSION}/task-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8a4b1a062ff656e31c8abf842da9f1bfe8189102ce9240bf730e92378d52ecc2
+TERMUX_PKG_SHA256=23eb60f73e42f16111cc3912b44ee12be6768860a2db2a9c6a47f8ac4786bac3
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++, libgnutls, libuuid"
 TERMUX_CMAKE_BUILD="Unix Makefiles"
 
 termux_step_pre_configure() {
 	termux_setup_rust
+	cargo install --force --locked bindgen-cli
 
 	CXXFLAGS+=" -Wno-c++11-narrowing"
 	LDFLAGS+=" -landroid-glob"
+	export ANDROID_STANDALONE_TOOLCHAIN="$TERMUX_STANDALONE_TOOLCHAIN"
+	export CARGO_TARGET_$(printf $CARGO_TARGET_NAME | tr a-z A-Z | sed s/-/_/g)_RUSTFLAGS+=" -C linker=$(command -v $CC)"
 
 	if [ "$TERMUX_ARCH" = "arm" ]; then
 		# See https://cmake.org/cmake/help/latest/variable/CMAKE_ANDROID_ARM_MODE.html
