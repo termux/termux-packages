5f3b7c1e45e78f66eb0b7d386ffce06fd81c04d3
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-04-03 12:19:18 +0300

    bump(main/cloudflared): 2025.4.0

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared
 TERMUX_PKG_DESCRIPTION="A tunneling daemon that proxies traffic from the Cloudflare network to your origins"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2025.2.1"
+TERMUX_PKG_VERSION="2025.4.0"
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c82ac315fa081cc18c98c9f63657375151a8c6141e7597d794232a1d35f93a4c
+TERMUX_PKG_SHA256=731694e178c7671ee9210cc7aca87aa35a5f0114e834c4e83f49dbb97b2b2b0f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
