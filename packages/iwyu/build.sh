3c9b753d06afdd03d7f9e679be91707dd07f6846
lucy lovebuny <landfillbaby69@gmail.com>
2025-04-05 16:04:31 +0100

    iwyu version dependency

--- packages/iwyu/build.sh
+++ packages/iwyu/build.sh
@@ -7,5 +7,5 @@ TERMUX_PKG_VERSION=0.24
 TERMUX_PKG_SRCURL=https://github.com/include-what-you-use/include-what-you-use/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=897b4c864a983f493c8efef4a1a9a2d429fd7ead1011f7a41743ed7b6dbe8c2e
 TERMUX_PKG_AUTO_UPDATE=false # can't be auto-updated since release correspond to clang version.
-TERMUX_PKG_DEPENDS="clang, libc++, python"
+TERMUX_PKG_DEPENDS="clang (>= 20.0.0, < 21.0.0), libc++, python"
 TERMUX_PKG_BUILD_DEPENDS="libllvm-static"
