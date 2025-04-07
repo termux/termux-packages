64b038dd21a6d728ae472f6db1bc7cd913b70487
Lucy Phipps <landfillbaby69@gmail.com>
2025-04-07 06:20:41 +0100

    binutils fix2

--- packages/binutils-libs/binutils-bin.subpackage.sh
+++ packages/binutils-libs/binutils-bin.subpackage.sh
@@ -11,6 +11,7 @@ share/info/ld.info
 share/man/
 "
 TERMUX_SUBPKG_DESCRIPTION="Collection of binary tools, the main ones being ld, the GNU linker, and as, the GNU assembler"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=true # TODO: subpkg of specific needed?
 TERMUX_SUBPKG_DEPENDS="libc++"
 TERMUX_SUBPKG_BREAKS="binutils (<< 2.39-3)"
 TERMUX_SUBPKG_REPLACES="binutils (<< 2.39-3)"
