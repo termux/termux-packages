76a644162b8b13ae7f53df89e4af8e3d6ba680ba
lucy lovebuny <landfillbaby69@gmail.com>
2025-04-07 15:32:33 +0100

    2038

--- packages/patch/build.sh
+++ packages/patch/build.sh
@@ -8,3 +8,10 @@ TERMUX_PKG_SHA256=f87cee69eec2b4fcbf60a396b030ad6aa3415f192aa5f7ee84cad5e11f7f5a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-xattr ac_cv_path_ED=$TERMUX_PREFIX/bin/ed"
 TERMUX_PKG_GROUPS="base-devel"
+
+termux_step_pre_configure() { 
+ 	# https://android.googlesource.com/platform/bionic/+/master/docs/32-bit-abi.md#is-32_bit-on-lp32-y2038 
+ 	if [ $TERMUX_ARCH_BITS = 32 ]; then 
+ 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-year2038" 
+ 	fi
+}
