7b279b3ca00f5e7ecd92b5b70f9fa5564cf7c565
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-07 14:49:06 +0000

    bump(main/imlib2): 1.12.5
    Remove patch file which was fixed in upstream.
    * https://git.enlightenment.org/old/legacy-imlib2/commit/46d2faec1e72d78ce512f3eb2386b74b7c9ebcd2
    * https://git.enlightenment.org/old/legacy-imlib2/commit/aa8e2a103a627ad14bdd21065c402e713a6719fa
    * https://git.enlightenment.org/old/legacy-imlib2/commit/d6a086dbddbdcaa9a07e21a60b6f35165d45ce59

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Library that does image file loading and saving as well 
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING-PLAIN"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.4"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.12.5"
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=bf07ff09255ed30f05a1c252d4b578426c2dcdbbc28e8c714adf060690395720
+TERMUX_PKG_SHA256=fa2315f28379b430a6e6605b4284b07be06a3ef422d4f5e1c9bb24714c4cf6dd
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, giflib, glib, libandroid-shmem, libbz2, libcairo, libheif, libid3tag, libjpeg-turbo, libjxl, liblzma, libpng, librsvg, libtiff, libwebp, libx11, libxcb, libxext, openjpeg, zlib"
 
