2d616198d1d694931c6688ab8000aa8a07c906ff
TomIO <tomjo00@web.de>
2025-04-08 22:38:37 +0200

    revbump(x11/gvfs): after libxml2 update

--- x11-packages/gvfs/build.sh
+++ x11-packages/gvfs/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A userspace virtual filesystem implementation for GIO"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.57.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gvfs/${TERMUX_PKG_VERSION%.*}/gvfs-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f16bef8eca1fd6c117e85db011d21e915669790d55867349c5f1b291299e9585
 TERMUX_PKG_DEPENDS="dbus, gcr4, glib, gsettings-desktop-schemas, libarchive, libsoup3, libxml2"
