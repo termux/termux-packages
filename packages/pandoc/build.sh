b8d692e809f1a24d853eac9d0232825502ef3995
Aditya Alok <alok@termux.dev>
2025-04-09 01:48:48 +0530

    pkg(main/pandoc): Enable self build
    - Updated to version 3.7.0.1
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/pandoc/build.sh
+++ packages/pandoc/build.sh
@@ -1,43 +1,50 @@
 TERMUX_PKG_HOMEPAGE=https://pandoc.org/
 TERMUX_PKG_DESCRIPTION="Universal markup converter"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.1.13
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.dev>"
+TERMUX_PKG_VERSION=3.7.0.1
+TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/pandoc-cli-$TERMUX_PKG_VERSION/pandoc-cli-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=62cfd812ed0e980bb7da2100983bc3842856625c006c3b393f186e297d181754
+TERMUX_PKG_DEPENDS="libffi, libiconv, libgmp, zlib"
+TERMUX_PKG_BUILD_DEPENDS="aosp-libs"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-f-lua -f+server
+-c pandoc+embed_data_files
+"
 
-termux_step_make_install() {
-	local srcurl
-	local sha256
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686" # Upstream doesn't support 32bit.
 
-	case "$TERMUX_ARCH" in
-		aarch64)
-			srcurl="https://github.com/jgm/pandoc/releases/download/${TERMUX_PKG_VERSION}/pandoc-${TERMUX_PKG_VERSION}-linux-arm64.tar.gz"
-			sha256="678c09ac4227c88b491f6e75491e6da871fd08d79b8c0f0ee37b611f01ad3d25"
-			;;
-		x86_64)
-			srcurl="https://github.com/jgm/pandoc/releases/download/${TERMUX_PKG_VERSION}/pandoc-${TERMUX_PKG_VERSION}-linux-amd64.tar.gz"
-			sha256="db556c98cf207d2fddc088d12d2e2f367d9401784d4a3e914b068fa895dcf3f0"
-			;;
-		*)
-			termux_error_exit "Unsupported arch: $TERMUX_ARCH"
-			;;
-	esac
+termux_step_post_configure() {
+	cabal get xml-conduit-1.10.0.0 # NOTE: Confirm version before updating.
+	mv xml-conduit{-1.10.0.0,}
 
-	termux_download "$srcurl" "pandoc-${TERMUX_PKG_VERSION}.tar.gz" "$sha256"
-	tar xf "pandoc-${TERMUX_PKG_VERSION}.tar.gz"
-	cd "pandoc-${TERMUX_PKG_VERSION}"
+	# We cannot use `Custom` build while cross-compiling:
+	sed -i -E 's|(build-type:\s*)Custom|\1Simple|' ./xml-conduit/xml-conduit.cabal
 
-	install -Dm700 "./bin/pandoc" "$TERMUX_PREFIX/bin/pandoc"
-	install -Dm600 "./share/man/man1/pandoc.1.gz" "$TERMUX_PREFIX/share/man/man1/pandoc.1.gz"
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == false ]]; then # We do not need iserv for on device builds.
+		termux_setup_ghc_iserv
+		cat <<-EOF >>cabal.project.local
+			packages: ./xml-conduit
+			          ./
+			package *
+			    ghc-options: -fexternal-interpreter -pgmi=$(command -v termux-ghc-iserv)
+		EOF
+	fi
+}
+
+termux_step_post_make_install() {
+	ln -sfv "$TERMUX_PREFIX"/bin/pandoc "$TERMUX_PREFIX"/bin/pandoc-server
+
+	install -Dm600 ./man/*.1 -t "$TERMUX_PREFIX"/share/man/man1/
+
+	# Create empty completions file so that it is removed while uninstalling the package:
 	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/bash-completion/completions/pandoc
 }
 
 termux_step_create_debscripts() {
 	cat <<-EOF >./postinst
-		#!${TERMUX_PREFIX}/bin/sh
-		pandoc --bash-completion > ${TERMUX_PREFIX}/share/bash-completion/completions/pandoc
+		#!$TERMUX_PREFIX/bin/sh
+		pandoc --bash-completion > $TERMUX_PREFIX/share/bash-completion/completions/pandoc
 	EOF
 }
