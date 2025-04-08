706384289e5f41a97999cb4ef8e80db3259ec341
TomIO <tomjo00@web.de>
2025-04-08 22:38:37 +0200

    revbump(x11/bluefish): after libxml2 update

--- x11-packages/bluefish/build.sh
+++ x11-packages/bluefish/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A powerful editor targeted towards programmers and webde
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.2.17"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.bennewitz.com/bluefish/stable/source/bluefish-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=3a79f6425e14939ea134f96c1424e3aac05bee95904be434581883078a7b7253
 TERMUX_PKG_AUTO_UPDATE=true
