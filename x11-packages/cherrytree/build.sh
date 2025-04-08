b35bd754cb6f93f91b62212df6bcf947226c0672
TomIO <tomjo00@web.de>
2025-04-08 22:38:37 +0200

    revbump(x11/cherrytree): after libxml2 update

--- x11-packages/cherrytree/build.sh
+++ x11-packages/cherrytree/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A hierarchical note taking application"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/giuspen/cherrytree/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ebfafc251f29b0b051f9a3d00502c608c7cc2412c7c1246dfeee07fa76a80162
 TERMUX_PKG_AUTO_UPDATE=true
