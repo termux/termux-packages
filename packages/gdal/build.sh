ec6df24fb38a4ee6b6d2bf200165ecd9f52f4e21
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-08 02:59:54 +0000

    bump(main/gdal): 3.10.3

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="A translator library for raster and vector geospatial da
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.10.2"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="3.10.3"
 TERMUX_PKG_SRCURL=https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=67b4e08acd1cc4b6bd67b97d580be5a8118b586ad6a426b09d5853898deeada5
+TERMUX_PKG_SHA256=335a8d2c7567d783563d3fed37e8b58d72d9c1723f6fd1d8c299fe4c0d936781
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="giflib, json-c, libc++, libcurl, libexpat, libfreexl, libgeos, libiconv, libjpeg-turbo, libjxl, liblzma, libpng, libspatialite, libsqlite, libwebp, libxml2, netcdf-c (>= 4.9.3), openjpeg, openssl, proj, postgresql, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="json-c-static"
