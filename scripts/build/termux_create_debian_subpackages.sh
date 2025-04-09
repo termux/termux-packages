termux/termux-packages

commit 97a63bdb78dcd042750708253ed4fcdf09fac3f2

chore(subpackages): make `TERMUX_SUBPKG_DEPENDS` check more readable

scripts/build/termux_create_debian_subpackages.sh
@@ -101,15 +101,16 @@ termux_create_debian_subpackages() {
 			Homepage: $TERMUX_PKG_HOMEPAGE
 		HERE
 
-		local PKG_DEPS_SPC=" ${TERMUX_PKG_DEPENDS//,/} "
+		# If the subpackage is not in the $TERMUX_PKG_DEPENDS for the parent package,
+		# and TERMUX_SUBPKG_DEPEND_ON_PARENT doesn't have a value, the subpackage should depend on its parent
+		[[ " ${TERMUX_PKG_DEPENDS//,/ } " == *" $SUB_PKG_NAME "* ]] && : "${TERMUX_SUBPKG_DEPEND_ON_PARENT:=true}"
 
-		if [ -z "$TERMUX_SUBPKG_DEPEND_ON_PARENT" ] && [ "${PKG_DEPS_SPC/ $SUB_PKG_NAME /}" = "$PKG_DEPS_SPC" ] || [ "$TERMUX_SUBPKG_DEPEND_ON_PARENT" = "true" ]; then
-			TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME (= $TERMUX_PKG_FULLVERSION)"
-		elif [ "$TERMUX_SUBPKG_DEPEND_ON_PARENT" = unversioned ]; then
-			TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME"
-		elif [ "$TERMUX_SUBPKG_DEPEND_ON_PARENT" = deps ]; then
-			TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_DEPENDS"
-		fi
+		case "$TERMUX_SUBPKG_DEPEND_ON_PARENT" in
+			'unversioned') TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME";;
+			'deps')        TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_DEPENDS";;
+			'true')        TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME (= $TERMUX_PKG_FULLVERSION)";;
+			*) ;;
+		esac
 
 		if [ "$TERMUX_GLOBAL_LIBRARY" = "true" ] && [ "$TERMUX_PACKAGE_LIBRARY" = "glibc" ]; then
 			test ! -z "$TERMUX_SUBPKG_DEPENDS" && TERMUX_SUBPKG_DEPENDS=$(termux_package__add_prefix_glibc_to_package_list "$TERMUX_SUBPKG_DEPENDS")

scripts/build/termux_create_pacman_subpackages.sh
@@ -82,16 +82,18 @@ termux_create_pacman_subpackages() {
 		local SUB_PKG_INSTALLSIZE
 		SUB_PKG_INSTALLSIZE=$(du -bs . | cut -f 1)
 
-		local PKG_DEPS_SPC=" ${TERMUX_PKG_DEPENDS//,/} "
-		if [ -z "$TERMUX_SUBPKG_DEPEND_ON_PARENT" ] && [ "${PKG_DEPS_SPC/ $SUB_PKG_NAME /}" = "$PKG_DEPS_SPC" ] || [ "$TERMUX_SUBPKG_DEPEND_ON_PARENT" = "true" ]; then
-			# Does pacman supports versioned dependencies?
-			#TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME (= $TERMUX_PKG_FULLVERSION)"
-			TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME"
-		elif [ "$TERMUX_SUBPKG_DEPEND_ON_PARENT" = unversioned ]; then
-			TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME"
-		elif [ "$TERMUX_SUBPKG_DEPEND_ON_PARENT" = deps ]; then
-			TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_DEPENDS"
-		fi
+		# If the subpackage is not in the $TERMUX_PKG_DEPENDS for the parent package,
+		# and TERMUX_SUBPKG_DEPEND_ON_PARENT doesn't have a value, the subpackage should depend on its parent
+		[[ " ${TERMUX_PKG_DEPENDS//,/ } " == *" $SUB_PKG_NAME "* ]] && : "${TERMUX_SUBPKG_DEPEND_ON_PARENT:=true}"
+
+		case "$TERMUX_SUBPKG_DEPEND_ON_PARENT" in
+			'unversioned') TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME";;
+			'deps')        TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_DEPENDS";;
+			# TODO: pacman does support versioned dependencies
+			# but we are not currently translating the .DEB notation to pacman's
+			'true')        TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME";;
+			*) ;;
+		esac
 
 		if [ "$TERMUX_GLOBAL_LIBRARY" = "true" ] && [ "$TERMUX_PACKAGE_LIBRARY" = "glibc" ]; then
 			test ! -z "$TERMUX_SUBPKG_DEPENDS" && TERMUX_SUBPKG_DEPENDS=$(termux_package__add_prefix_glibc_to_package_list "$TERMUX_SUBPKG_DEPENDS")

commit f07eee390a224f68f9c6f935a7a7d1dc6e12ce80

fix(subpackages): Fix check for if subpackage should depend on parent
Fixes a regression in 97a63bdb78dcd042750708253ed4fcdf09fac3f2

One quick-building test case is the `dpkg` package with the `dpkg-perl`
subpackage: `dpkg-perl` should depend on `dpkg`, its parent package.

scripts/build/termux_create_debian_subpackages.sh
@@ -103,7 +103,7 @@ termux_create_debian_subpackages() {
 
 		# If the subpackage is not in the $TERMUX_PKG_DEPENDS for the parent package,
 		# and TERMUX_SUBPKG_DEPEND_ON_PARENT doesn't have a value, the subpackage should depend on its parent
-		[[ " ${TERMUX_PKG_DEPENDS//,/ } " == *" $SUB_PKG_NAME "* ]] && : "${TERMUX_SUBPKG_DEPEND_ON_PARENT:=true}"
+		[[ " ${TERMUX_PKG_DEPENDS//,/ } " != *" $SUB_PKG_NAME "* ]] && : "${TERMUX_SUBPKG_DEPEND_ON_PARENT:=true}"
 
 		case "$TERMUX_SUBPKG_DEPEND_ON_PARENT" in
 			'unversioned') TERMUX_SUBPKG_DEPENDS+=", $TERMUX_PKG_NAME";;