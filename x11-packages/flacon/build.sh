d101bbc29b1111f321c5dd35b011c05b9dd2d71c
Kevin Williams <admin@utermux.dev>
2025-04-12 20:25:47 +0800

    taglib: revbump revdeps(1)
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/flacon/build.sh
+++ x11-packages/flacon/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Extracts individual tracks from one big audio file and s
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="12.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/flacon/flacon/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=78199ff925b7cd0ffeb628d47909ca4172f8ff0d8fd8192bb537e0c012e6f4c6
 TERMUX_PKG_AUTO_UPDATE=true
