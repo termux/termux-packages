1ccdfce023850f10bdb730345dee5b4cf19577d8
Kevin Williams <admin@utermux.dev>
2025-04-12 20:25:47 +0800

    taglib: revbump revdeps(1)
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- x11-packages/kf6-kfilemetadata/build.sh
+++ x11-packages/kf6-kfilemetadata/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='A library for extracting file metadata'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.13.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kfilemetadata-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=090fcd05577cd7940e7072fcf8855f1a7f076ff09ae3397bbfa40681dea4be9e
 TERMUX_PKG_DEPENDS="attr, ebook-tools, exiv2, ffmpeg, kf6-karchive (>= ${TERMUX_PKG_VERSION}), kf6-kcodecs (>= ${TERMUX_PKG_VERSION}), kf6-kconfig (>= ${TERMUX_PKG_VERSION}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION}), kf6-ki18n (>= ${TERMUX_PKG_VERSION}), libc++, poppler-qt, qt6-qtbase, taglib"
