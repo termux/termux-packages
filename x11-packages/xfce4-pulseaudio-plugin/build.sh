fb784160d0118be497b4ce8eebe9294dde2b1155
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-13 20:26:52 +0000

    bump(x11/xfce4-pulseaudio-plugin): 0.5.0

--- x11-packages/xfce4-pulseaudio-plugin/build.sh
+++ x11-packages/xfce4-pulseaudio-plugin/build.sh
@@ -2,9 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-pulseaudio-plugin/
 TERMUX_PKG_DESCRIPTION="Pulseaudio plugin for the Xfce4 panel"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.4.9"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-pulseaudio-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-pulseaudio-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=a0807615fb2848d0361b7e4568a44f26d189fda48011c7ba074986c8bfddc99a
-TERMUX_PKG_DEPENDS="libnotify, libcanberra, pulseaudio, xfce4-panel, pavucontrol"
+TERMUX_PKG_VERSION="0.5.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-pulseaudio-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-pulseaudio-plugin-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=3fe69bc6f9c0dd68bd317c0a7813975cf162ba1dd64e23c2ffef372d4b4f808a
+TERMUX_PKG_DEPENDS="exo, gdk-pixbuf, glib, gtk3, libcairo, libcanberra, libnotify, libxfce4ui, libxfce4util, libxfce4windowing, pulseaudio, xfce4-panel, xfconf"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_glib_cross_pkg_config_wrapper
+}
