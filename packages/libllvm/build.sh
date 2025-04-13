fe83631ec292010b4d81b735d4186bf3533e3689
TomIO <tomjo00@web.de>
2025-04-14 00:48:10 +0200

    revbump(main/libllvm): after libxml2 update

--- packages/libllvm/build.sh
+++ packages/libllvm/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_MAINTAINER="@finagolfin"
 # Keep flang version and revision in sync when updating (enforced by check in termux_step_pre_configure).
 LLVM_MAJOR_VERSION=20
 TERMUX_PKG_VERSION=${LLVM_MAJOR_VERSION}.1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=f0a4a240aabc9b056142d14d5478bb6d962aeac549cbd75b809f5499240a8b38
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/llvm-project-${TERMUX_PKG_VERSION}.src.tar.xz
