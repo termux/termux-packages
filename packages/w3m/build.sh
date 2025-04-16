cb7fc3906d77817f3dee479cf3f020e9ffc93719
TomIO <tomjo00@web.de>
2025-04-16 11:16:38 +0200

    fix(main/w3m): `RECOMMENDS` -> `SUGGESTS`

--- packages/w3m/build.sh
+++ packages/w3m/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.5.3
 _MINOR_VERSION=20230121
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.${_MINOR_VERSION}
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 # The upstream w3m project is dead, but every linux distribution uses
 # this maintained fork in debian:
 TERMUX_PKG_SRCURL=https://github.com/tats/w3m/archive/v${_MAJOR_VERSION}+git${_MINOR_VERSION}.tar.gz
@@ -15,8 +15,7 @@ TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libgc, ncurses, openssl, zlib"
-TERMUX_PKG_RECOMMENDS="libsixel"
-TERMUX_PKG_SUGGESTS="perl"
+TERMUX_PKG_SUGGESTS="libsixel, perl"
 
 # ac_cv_func_bcopy=yes to avoid w3m defining it's own bcopy function, which
 # breaks 64-bit builds where NDK headers define bcopy as a macro:
