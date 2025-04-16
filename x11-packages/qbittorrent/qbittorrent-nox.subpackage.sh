435ef4fd96dcb2d90f4037d6100ec4ce6cb16f86
TomIO <tomjo00@web.de>
2025-04-16 11:16:38 +0200

    fix(x11/qbittorrent): `RECOMMENDS` -> `SUGGESTS`

--- x11-packages/qbittorrent/qbittorrent-nox.subpackage.sh
+++ x11-packages/qbittorrent/qbittorrent-nox.subpackage.sh
@@ -2,4 +2,5 @@ TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_INCLUDE='bin/qbittorrent-nox share/man/man1/qbittorrent-nox.1.gz'
 TERMUX_SUBPKG_DESCRIPTION='A Qt6 based BitTorrent client - headless version'
 TERMUX_SUBPKG_DEPENDS='libc++, libtorrent-rasterbar, openssl, qt6-qtbase, zlib'
-TERMUX_SUBPKG_RECOMMENDS="jackett, python"
+TERMUX_SUBPKG_RECOMMENDS="python"
+TERMUX_SUBPKG_SUGGESTS="jackett"
