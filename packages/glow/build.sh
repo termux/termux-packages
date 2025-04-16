b5e38f8515ae977a9e95b36b845ddd3a92c3012f
TomIO <tomjo00@web.de>
2025-04-16 11:16:38 +0200

    enhance(main/glow): ship completions and man page - chore: clean up build script - fix: `RECOMMENDS` -> `SUGGESTS`

--- packages/glow/build.sh
+++ packages/glow/build.sh
@@ -3,25 +3,36 @@ TERMUX_PKG_DESCRIPTION="Render markdown on the CLI, with pizzazz!"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/charmbracelet/glow/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f1875a73ed81e5d8e6c81443e9a9d18bd9d1489c563c9fa2ff5425f2f8e2af6f
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_RECOMMENDS=git
+TERMUX_PKG_SUGGESTS=git
+
+termux_step_pre_configure() {
+	termux_setup_golang
+}
 
 termux_step_make() {
-	termux_setup_golang
-
-	cd "$TERMUX_PKG_SRCDIR"
-
 	mkdir -p "${TERMUX_PKG_BUILDDIR}/src/github.com/charmbracelet"
-	cp -a "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/src/github.com/charmbracelet/glow"
-	cd "${TERMUX_PKG_BUILDDIR}/src/github.com/charmbracelet/glow"
 
-	go get -d -v
+	go get -v
 	go build
 }
 
 termux_step_make_install() {
-	install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/charmbracelet/glow/glow \
-		$TERMUX_PREFIX/bin/glow
+	mkdir -p "${TERMUX_PREFIX}/share/man/man1"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+
+	# borrowed from packages/gh
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	go run .             man > "${TERMUX_PREFIX}/share/man/man1/${TERMUX_PKG_NAME}.1"
+	go run . completion  zsh > "${TERMUX_PREFIX}/share/zsh/site-functions/_${TERMUX_PKG_NAME}"
+	go run . completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/${TERMUX_PKG_NAME}"
+	go run . completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/${TERMUX_PKG_NAME}.fish"
+	install -Dm700 glow "$TERMUX_PREFIX/bin/glow"
 }
