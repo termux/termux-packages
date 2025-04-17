705a0aa8c051efcf8a1bbba98aa712a03cd54236
Maxython <mixython@gmail.com>
2025-04-18 00:16:26 +0300

    desktop-file-utils: implement target alpm-hooks for pacman

--- x11-packages/desktop-file-utils/build.sh
+++ x11-packages/desktop-file-utils/build.sh
@@ -3,15 +3,17 @@ TERMUX_PKG_DESCRIPTION="Command line utilities for working with desktop entries"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.28"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4401d4e231d842c2de8242395a74a395ca468cd96f5f610d822df33594898a70
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib"
 
 termux_step_create_debscripts() {
-	for i in postinst postrm triggers; do
+	for i in $(test "$TERMUX_PACKAGE_FORMAT" != "pacman" && echo postinst) postrm triggers; do
 		sed \
-			"s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+			-e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+			-e "s|@TERMUX_PACKAGE_FORMAT@|${TERMUX_PACKAGE_FORMAT}|g" \
 			"${TERMUX_PKG_BUILDER_DIR}/hooks/${i}.in" > ./${i}
 		chmod 755 ./${i}
 	done
