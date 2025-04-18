b0b5463ee028bfb7377bc7a7a98244c0022f8482
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-04-18 11:11:59 +0300

    chore(main/brotli): move python-brotli to subpackage

---
+++ packages/brotli/python-brotli.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DESCRIPTION="lossless compression algorithm and format (Python bindings)"
+TERMUX_SUBPKG_INCLUDE="lib/python${TERMUX_PYTHON_VERSION}/site-packages"
+TERMUX_SUBPKG_DEPENDS="python, python-pip"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
