dca2340e9ad1a91377e5b439ec69743e5a6cda72
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-19 20:26:17 +0000

    bump(x11/gedit): 48.2
    Switched to gitlab.gnome.org release (download.gnome.org is outdated)

--- x11-packages/gedit/build.sh
+++ x11-packages/gedit/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://gedit-technology.github.io/apps/gedit/
 TERMUX_PKG_DESCRIPTION="GNOME Text Editor"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="48.1"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gedit/${TERMUX_PKG_VERSION%.*}/gedit-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=971e7ac26bc0a3a3ded27a7563772415687db0e5a092b4547e5b10a55858b30a
+TERMUX_PKG_VERSION="48.2"
+# for submodules https://gitlab.gnome.org/World/gedit/gedit/-/issues/611
+TERMUX_PKG_SRCURL="git+https://gitlab.gnome.org/World/gedit/gedit"
+TERMUX_PKG_SHA256=fb13faeda36b2c79dff47e5ebcb84981b7f08fbbef866123a64f63b2ead8bb33
+TERMUX_PKG_GIT_BRANCH="${TERMUX_PKG_VERSION}"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gobject-introspection, gsettings-desktop-schemas, gspell, gtk3, libcairo, libgedit-amtk, libgedit-gfls, libgedit-gtksourceview, libgedit-tepl, libpeas, pango"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
@@ -15,6 +16,13 @@ TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dgtk_doc=false
 "
+
+termux_step_post_get_source() {
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]] ; then
+		termux_error_exit "Checksum mismatch for source files.\nExpected: ${TERMUX_PKG_SHA256}\nActual:   ${s%% *}"
+	fi
+}
 
 termux_step_pre_configure() {
 	termux_setup_gir
