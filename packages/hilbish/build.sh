6c5fcc6d39a5baba60f0300ad148b9ee17fc1964
Xronin <109928669+xronin01@users.noreply.github.com>
2025-04-19 14:26:56 +0000

    bump(main/hilbish): 2.3.4 Refactor build to use release tarballs and slightly clean up build process.
    Co-authored-by: TomIO <tom@termux.dev>

--- packages/hilbish/build.sh
+++ packages/hilbish/build.sh
@@ -2,40 +2,47 @@ TERMUX_PKG_HOMEPAGE=https://rosettea.github.io/Hilbish/
 TERMUX_PKG_DESCRIPTION="The Moon-powered shell! A comfy and extensible shell for Lua fans!"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=0582fbd30c75e5915108df0183ac05747c69a7d9 # v2.3.2
-TERMUX_PKG_VERSION="2024.07.30"
-TERMUX_PKG_SRCURL=git+https://github.com/Rosettea/Hilbish
-TERMUX_PKG_SHA256=7cbba35c2def313c5b60c1b2d5d16e80c4bcc788c3aad7bc36a56a0d4d22de22
-TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_VERSION=(
+	"1:2.3.4" # hilbish
+	"3.1.2"   # inspect.lua (Lua dependency)
+	"0.2.1"   # lunacolors (Lua dependency)
+	"0.1.0"   # succulent (Lua dependency)
+)
+TERMUX_PKG_REPOLOGY_METADATA_VERSION="${TERMUX_PKG_VERSION[0]:2}"
+TERMUX_PKG_SRCURL=(
+	"https://github.com/Rosettea/Hilbish/archive/refs/tags/v${TERMUX_PKG_VERSION[0]:2}.tar.gz"
+	"https://github.com/kikito/inspect.lua/archive/refs/tags/v${TERMUX_PKG_VERSION[1]}.tar.gz"
+	"https://github.com/Rosettea/Lunacolors/archive/refs/tags/v${TERMUX_PKG_VERSION[2]}.tar.gz"
+	"https://github.com/Rosettea/Succulent/archive/refs/tags/v${TERMUX_PKG_VERSION[3]}.tar.gz"
+)
+TERMUX_PKG_SHA256=(
+	747f509bf8091cd0decb631c0402e6caeb6ba82f35328d2125ae730b332ea0fc
+	6b5856d04bc9ab12a5849dd529bb5f6a986a8cb7447f8824479aedbaca259622
+	6cc64419c5783d8f91589f69353deec5160d5a6f606f4bbc6258492cc2717642
+	a76aa3050bfc03b728c9181eb26ca84c19b2ff6a75facdb10a184dea4c6a97e2
+)
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-	git submodule update --init --recursive
-
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
-
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
-	fi
+	rm -vrf libs/{inspect,lunacolors,succulent}
+	mv "inspect.lua-${TERMUX_PKG_VERSION[1]}" libs/inspect
+	mv "Lunacolors-${TERMUX_PKG_VERSION[2]}" libs/lunacolors
+	mv "Succulent-${TERMUX_PKG_VERSION[3]}" libs/succulent
 }
 
 termux_step_make() {
 	termux_setup_golang
 	export GOPATH=$TERMUX_PKG_SRCDIR
-	GOOS=android CGO_ENABLED=1 go build -ldflags "-checklinkname=0 -s -w -X main.dataDir=${TERMUX_PREFIX}/share/hilbish -X main.gitCommit=$(git ls-remote https://github.com/rosettea/hilbish refs/tags/v$TERMUX_PKG_VERSION) -X main.gitBranch=$TERMUX_PKG_VERSION"
+	LDFLAGS=" -checklinkname=0"
+	LDFLAGS+=" -s -w"
+	LDFLAGS+=" -X main.gitCommit=$(git ls-remote https://github.com/Rosettea/Hilbish "refs/tags/v${TERMUX_PKG_VERSION[0]:2}" | cut -f1)"
+	LDFLAGS+=" -X main.releaseName=${TERMUX_PKG_VERSION[0]:2}"
+
+	GOOS=android CGO_ENABLED=1 go build -ldflags "${LDFLAGS}"
 }
 
 termux_step_make_install() {
 	mkdir -p "$TERMUX_PREFIX/share/hilbish"
-	install -v -d "$TERMUX_PREFIX/bin" && install -m 0755 -v hilbish "$TERMUX_PREFIX/bin/hilbish"
+	install -Dm 0700 hilbish "$TERMUX_PREFIX/bin/hilbish"
 	cp -r libs docs emmyLuaDocs nature .hilbishrc.lua "$TERMUX_PREFIX/share/hilbish"
-	grep -qxF "$TERMUX_PREFIX/bin/hilbish" "$TERMUX_PREFIX/etc/shells" || echo "$TERMUX_PREFIX/bin/hilbish" >> "$TERMUX_PREFIX/etc/shells"
 }
