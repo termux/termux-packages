2d27ba02814c174553daed5102d4ebde9c324378
Robert Kirkman <rkirkman@termux.dev>
2025-04-20 01:18:15 -0500

    fix(x11/spek): libandroid-stub now pulled in by ffmpeg

--- x11-packages/spek/build.sh
+++ x11-packages/spek/build.sh
@@ -3,11 +3,10 @@ TERMUX_PKG_DESCRIPTION="An acoustic spectrum analyser"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8.5
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/alexkay/spek/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9053d2dec452dcde421daa0f5f59a9dee47927540f41d9c0c66800cb6dbf6996
 TERMUX_PKG_DEPENDS="ffmpeg, libc++, wxwidgets"
-TERMUX_PKG_SUGGESTS="libandroid-stub"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_path_WX_CONFIG_PATH=$TERMUX_PREFIX/bin/wx-config"
 
 termux_step_pre_configure() {
@@ -19,7 +18,6 @@ termux_step_pre_configure() {
 termux_step_create_subpkg_debscripts() {
 	cat <<- EOF > ./postinst
 	#!$TERMUX_PREFIX/bin/sh
-	echo "Note: If you encounter Segmentation Fault, execute the following command:"
-	echo "pkg install libandroid-stub"
+	echo "Note: If you encounter Segmentation Fault, then please open an issue at https://github.com/termux/termux-packages/issues"
 	EOF
 }
