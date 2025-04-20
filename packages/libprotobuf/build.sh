61df159684dcd773938fb09d88bd5eebfdced91b
Robert Kirkman <rkirkman@termux.dev>
2025-04-20 01:22:01 -0500

    fix(main/{libprotobuf,protobuf-static}): fix conflicts and provide licenses
    - `libprotobuf`:
      - conflict with `protobuf-static` (because of `libupb.a`)
      - add license file for `libutf8-range`
    
    - `protobuf-static`:
      - align `TERMUX_PKG_REVISION` with `libprotobuf`
      - conflict with `libprotobuf` (because of `libupb.a`)
      - provide its own `libutf8-range` and `protoc` (because of conflict with `libprotobuf` preventing it from depending on `protobuf` [which is `protoc`])
      - add license file for `libutf8-range`

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -2,28 +2,32 @@ TERMUX_PKG_HOMEPAGE=https://github.com/protocolbuffers/protobuf
 TERMUX_PKG_DESCRIPTION="Protocol buffers C++ library"
 # utf8_range is licensed under MIT
 TERMUX_PKG_LICENSE="BSD 3-Clause, MIT"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_LICENSE_FILE="
+LICENSE
+third_party/utf8_range/LICENSE
+"
 TERMUX_PKG_MAINTAINER="@termux"
 # When bumping version:
 # - update SHA256 checksum for $_PROTOBUF_ZIP in
 #     $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
 # - ALWAYS bump revision of reverse dependencies and rebuild them.
 TERMUX_PKG_VERSION="2:30.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/v${TERMUX_PKG_VERSION#*:}.tar.gz
 TERMUX_PKG_SHA256=9df0e9e8ebe39f4fbbb9cf7db3d811287fe3616b2f191eb2bf5eaa12539c881f
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="abseil-cpp, libc++, zlib"
-TERMUX_PKG_BREAKS="libprotobuf-dev, protobuf-static (<< ${TERMUX_PKG_VERSION#*:}), libutf8-range"
-TERMUX_PKG_REPLACES="libprotobuf-dev, libutf8-range"
+TERMUX_PKG_BREAKS="libprotobuf-dev, protobuf-static, libutf8-range"
+TERMUX_PKG_REPLACES="libprotobuf-dev, protobuf-static, libutf8-range"
+TERMUX_PKG_CONFLICTS="protobuf-static"
 TERMUX_PKG_FORCE_CMAKE=true
+TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dprotobuf_ABSL_PROVIDER=package
 -Dprotobuf_BUILD_TESTS=OFF
 -DBUILD_SHARED_LIBS=ON
 -DCMAKE_INSTALL_LIBDIR=lib
 "
-TERMUX_PKG_NO_STATICSPLIT=true
 
 termux_step_post_get_source() {
 	# Version guard
@@ -35,9 +39,6 @@ termux_step_post_get_source() {
 }
 
 termux_step_post_make_install() {
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/libutf8-range \
-		$TERMUX_PKG_SRCDIR/third_party/utf8_range/LICENSE
-
 	# Copy lib/*.cmake to opt/protobuf-cmake/shared for future use
 	mkdir -p $TERMUX_PREFIX/opt/protobuf-cmake/shared
 	cp $TERMUX_PREFIX/lib/cmake/protobuf/protobuf-targets{,-release}.cmake \
