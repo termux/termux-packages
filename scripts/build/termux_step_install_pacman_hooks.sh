129a2405f02fad49931a78444b7a5ea1f169e113
Maxython <mixython@gmail.com>
2025-04-23 00:05:59 +0300

    termux_step_install_pacman_hooks.sh: don't repeat sed arguments

--- scripts/build/termux_step_install_pacman_hooks.sh
+++ scripts/build/termux_step_install_pacman_hooks.sh
@@ -1,6 +1,8 @@
 termux_step_install_pacman_hooks() {
 	[[ "$TERMUX_PACKAGE_FORMAT" != "pacman" ]] && return
 
+	local sed="sed -e s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g -e s|@TERMUX_PREFIX_TARGET@|${TERMUX_PREFIX:1}|g -e s|@TERMUX_ARCH@|${TERMUX_ARCH}|g"
+
 	# Installing hooks
 	local hooks
 	hooks=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name "*.alpm.hook")
@@ -8,9 +10,7 @@ termux_step_install_pacman_hooks() {
 		mkdir -p ${TERMUX_PREFIX}/share/libalpm/hooks
 		local hook
 		for hook in ${hooks}; do
-			sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
-				-e "s|@TERMUX_PREFIX_TARGET@|${TERMUX_PREFIX:1}|g" \
-				"${hook}" > "${TERMUX_PREFIX}/share/libalpm/hooks/$(sed 's|.alpm.hook$|.hook|' <<< "${hook##*/}")"
+			${sed} "${hook}" > "${TERMUX_PREFIX}/share/libalpm/hooks/$(sed 's|.alpm.hook$|.hook|' <<< "${hook##*/}")"
 		done
 	fi
 
@@ -22,9 +22,7 @@ termux_step_install_pacman_hooks() {
 		local script script_alpm
 		for script in ${scripts}; do
 			script_alpm="${TERMUX_PREFIX}/share/libalpm/scripts/$(sed 's|.alpm.script$||' <<< "${script##*/}")"
-			sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
-				-e "s|@TERMUX_PREFIX_TARGET@|${TERMUX_PREFIX:1}|g" \
-				"${script}" > "${script_alpm}"
+			${sed} "${script}" > "${script_alpm}"
 			chmod +x "${script_alpm}"
 		done
 	fi
