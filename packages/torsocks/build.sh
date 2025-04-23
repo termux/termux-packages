1aa42cf4c1dfa6008bd7ed31b189e80d873d3ecd
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-23 06:30:09 +0000

    bump(main/torsocks): 2.5.0
    Remove torsocks-2.4.0-clang16.patch file which was added in upstream.
    https://gitlab.torproject.org/tpo/core/torsocks/-/commit/1171bf2fd4e7a0cab02cf5fca59090b65af9cd29

--- packages/torsocks/build.sh
+++ packages/torsocks/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.torproject.org/tpo/core/torsocks
 TERMUX_PKG_DESCRIPTION="Wrapper to safely torify applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.4.0
+TERMUX_PKG_VERSION="2.5.0"
 TERMUX_PKG_SRCURL=https://gitlab.torproject.org/tpo/core/torsocks/-/archive/v${TERMUX_PKG_VERSION}/torsocks-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c01b471d89eda9f3c8dcb85a448e8066692d0707f9ff8b2ac7e665a602291b87
+TERMUX_PKG_SHA256=0fc8e18f2dc2e12f1129054f6d5acc7ecc3f0345bb57ed653fc8c6674e6ecc7e
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="tor"
 
 termux_step_pre_configure() {
