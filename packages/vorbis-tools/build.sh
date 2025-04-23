84a96b67c14ef2d490bcfe261aa84a9b6cd9e38d
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-23 06:35:03 +0000

    bump(main/vorbis-tools): 1.4.3
    Remove vorbis-tools-1.4.2-clang16.patch file which was added in upstream.
    https://gitlab.xiph.org/xiph/vorbis-tools/-/commit/68c5a33685f5b86e7f18f239ceb8861484fee552

--- packages/vorbis-tools/build.sh
+++ packages/vorbis-tools/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/xiph/vorbis-tools
 TERMUX_PKG_DESCRIPTION="Ogg Vorbis tools"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="1.4.3"
 TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/vorbis-tools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=db7774ec2bf2c939b139452183669be84fda5774d6400fc57fde37f77624f0b0
+TERMUX_PKG_SHA256=a1fe3ddc6777bdcebf6b797e7edfe0437954b24756ffcc8c6b816b63e0460dde
+TERMUX_PKG_AUTO_UPDATE=true
 # libflac for flac support in oggenc:
 TERMUX_PKG_DEPENDS="libiconv, libvorbis, libflac, libogg"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
