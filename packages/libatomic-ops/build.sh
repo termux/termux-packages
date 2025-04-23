c7bade0ac3297ccddbb74d605786978709895774
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-23 03:37:57 +0000

    fix(main/libatomic-ops): Use newest tag for auto update
    %ci:no-build

--- packages/libatomic-ops/build.sh
+++ packages/libatomic-ops/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_VERSION="7.8.2"
 TERMUX_PKG_SRCURL=https://github.com/ivmai/libatomic_ops/releases/download/v${TERMUX_PKG_VERSION}/libatomic_ops-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d305207fe207f2b3fb5cb4c019da12b44ce3fcbc593dfd5080d867b1a2419b51
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
