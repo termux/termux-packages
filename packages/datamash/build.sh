9c36baf6fd3938e6e243ab9294e39880b595138e
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-23 13:34:53 +0000

    bump(main/datamash): 1.9

--- packages/datamash/build.sh
+++ packages/datamash/build.sh
@@ -2,7 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/datamash/
 TERMUX_PKG_DESCRIPTION="Program performing numeric, textual and statistical operations"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.8
+TERMUX_PKG_VERSION="1.9"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/datamash/datamash-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7ad97e8c7ef616dd03aa5bd67ae24c488272db3e7d1f5774161c18b75f29f6fd
+TERMUX_PKG_SHA256=f382ebda03650dd679161f758f9c0a6cc9293213438d4a77a8eda325aacb87d2
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+gl_cv_func_strcasecmp_works=yes
+"
