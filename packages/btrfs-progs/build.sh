af04db7f2c2fb1b80e368384da60aaacf3dbb917
Shadichy <shadichy@blisslabs.org>
2025-04-25 01:28:53 +0700

    bump(main/btrfs-progs): 6.14
    Added workarounds for btrfs-progs to handle `pthread_cancel` and `qsort_r`
    
    Signed-off-by: Shadichy <shadichy@blisslabs.org>

--- packages/btrfs-progs/build.sh
+++ packages/btrfs-progs/build.sh
@@ -2,19 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://btrfs.readthedocs.io/en/latest/
 TERMUX_PKG_DESCRIPTION="Utilities for Btrfs filesystem"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.0.2
+TERMUX_PKG_VERSION=6.14
 TERMUX_PKG_SRCURL=https://github.com/kdave/btrfs-progs/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=561a99d402d2788586756fb2b90295f709ec339bd4466c05a6e7c439dd0fe2f4
+TERMUX_PKG_SHA256=5a85b791f0f32a4994e864ac4cb7abccce08e56db3010a1855ad0edeebc70b4c
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="e2fsprogs, liblzo, libuuid, util-linux, zlib, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-backtrace
 --disable-libudev
 --disable-python
+--disable-static
+--disable-option-checking
 "
 
 termux_step_pre_configure() {
-	sh autogen.sh
-
+	./autogen.sh
 	CFLAGS+=" $CPPFLAGS"
 }
