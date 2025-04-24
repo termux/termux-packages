1852df2003c6e465612558a9914ad8e16d51abb7
Maxython <mixython@gmail.com>
2025-04-24 13:45:24 +0300

    chore(x11/desktop-file-utils): use prerm instead of postrm
    Additional: include postinst script for pacman to update desktop MINE cache after installing or updating a package.

--- x11-packages/desktop-file-utils/build.sh
+++ x11-packages/desktop-file-utils/build.sh
@@ -3,14 +3,14 @@ TERMUX_PKG_DESCRIPTION="Command line utilities for working with desktop entries"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.28"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=4401d4e231d842c2de8242395a74a395ca468cd96f5f610d822df33594898a70
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib"
 
 termux_step_create_debscripts() {
-	for i in $(test "$TERMUX_PACKAGE_FORMAT" != "pacman" && echo postinst) postrm triggers; do
+	for i in postinst prerm triggers; do
 		sed \
 			-e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
 			-e "s|@TERMUX_PACKAGE_FORMAT@|${TERMUX_PACKAGE_FORMAT}|g" \
@@ -18,5 +18,8 @@ termux_step_create_debscripts() {
 		chmod 755 ./${i}
 	done
 	unset i
+	if [[ "$TERMUX_PACKAGE_FORMAT" == "pacman" ]]; then
+		echo "post_install" > postupg
+	fi
 	chmod 644 ./triggers
 }
