232752872f3159f0fae2c4fe7f6a7d14858e6861
Maxython <mixython@gmail.com>
2025-04-25 15:39:15 +0300

    chore(x11/xorg-mkfontscale): enable postinst for pacman
    It is necessary that the fontdir setting be done even if the aplm-hook was not triggered

--- x11-packages/xorg-mkfontscale/build.sh
+++ x11-packages/xorg-mkfontscale/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Create an index of scalable font files for X"
 TERMUX_PKG_LICENSE="MIT, HPND"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.2.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/app/mkfontscale-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2921cdc344f1acee04bcd6ea1e29565c1308263006e134a9ee38cf9c9d6fe75e
 TERMUX_PKG_AUTO_UPDATE=true
@@ -13,9 +13,12 @@ TERMUX_PKG_CONFLICTS="xorg-mkfontdir"
 TERMUX_PKG_REPLACES="xorg-mkfontdir"
 
 termux_step_create_debscripts() {
-	for i in $(test "$TERMUX_PACKAGE_FORMAT" != "pacman" && echo postinst) postrm triggers; do
+	for i in postinst postrm triggers; do
 		cp "${TERMUX_PKG_BUILDER_DIR}/${i}" ./${i}
 		sed -i "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" ./${i}
 	done
 	unset i
+	if [[ "$TERMUX_PACKAGE_FORMAT" == "pacman" ]]; then
+		echo "post_install" > postupg
+	fi
 }
