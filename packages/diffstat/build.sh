ecfbc19c9b909d12e4d89743f9f5e3b56a0d923d
Biswapriyo Nath <nathbappai@gmail.com>
2025-04-25 07:09:15 +0000

    bump(main/diffstat): 1.68

--- packages/diffstat/build.sh
+++ packages/diffstat/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://invisible-island.net/diffstat/diffstat.html
 TERMUX_PKG_DESCRIPTION="Displays a histogram of changes to a file"
 TERMUX_PKG_LICENSE="HPND"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.67"
+TERMUX_PKG_VERSION="1.68"
 TERMUX_PKG_SRCURL=https://github.com/ThomasDickey/diffstat-snapshots/archive/refs/tags/v${TERMUX_PKG_VERSION/./_}.tar.gz
 # invisible-mirror.net is not suitable for CI due to bad responsiveness.
 #TERMUX_PKG_SRCURL=https://invisible-mirror.net/archives/diffstat/diffstat-${TERMUX_PKG_VERSION}.tgz
 #TERMUX_PKG_SRCURL=https://invisible-island.net/datafiles/release/diffstat.tar.gz
-TERMUX_PKG_SHA256=c649165f991d3f4666090ac086281ceab149358419fb04a053d3e1759340f074
+TERMUX_PKG_SHA256=1a4d60b630e120c9e1213112be2c52f6ba757ae09cb39273ecd39653c450aef8
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
