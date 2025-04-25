c25f6abf91c4ce44f9b35c1a5f997af1507941b5
tcely <tcely@users.noreply.github.com>
2025-04-25 13:09:05 -0400

    Use the same socket path as the `sv` service

--- packages/openssh/source-ssh-agent.sh
+++ packages/openssh/source-ssh-agent.sh
@@ -11,7 +11,7 @@ if [ -r "${PREFIX}/etc/ssh/start_agent.sh" ]; then
 	. "${PREFIX}/etc/ssh/start_agent.sh"
 fi
 
-export SSH_AUTH_SOCK="${PREFIX}/var/run/ssh-agent"
+export SSH_AUTH_SOCK="${PREFIX}/var/run/ssh-agent.socket"
 
 MESSAGE=$(ssh-add -L 2>&1)
 if [ "$MESSAGE" = 'Could not open a connection to your authentication agent.' -o \
