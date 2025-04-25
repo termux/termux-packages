ce61316c28c42683aeb7ca16fa7cf1d9fbcd91a3
tcely <tcely@users.noreply.github.com>
2025-04-25 14:03:47 -0400

    Use `XDG_RUNTIME_DIR` in source-ssh-agent.sh

--- packages/openssh/source-ssh-agent.sh
+++ packages/openssh/source-ssh-agent.sh
@@ -11,7 +11,7 @@ if [ -r "${PREFIX}/etc/ssh/start_agent.sh" ]; then
 	. "${PREFIX}/etc/ssh/start_agent.sh"
 fi
 
-export SSH_AUTH_SOCK="${PREFIX}/var/run/ssh-agent.socket"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-${TERMUX__PREFIX:-${PREFIX}}"/var/run/ssh-agent.socket
 
 MESSAGE=$(ssh-add -L 2>&1)
 if [ "$MESSAGE" = 'Could not open a connection to your authentication agent.' -o \
