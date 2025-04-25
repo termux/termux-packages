95fb00bc7b840739af00c9029d2bf0bb26d785b6
tcely <tcely@users.noreply.github.com>
2025-04-25 15:04:08 -0400

    enhance(main/openssh): standardize `ssh-agent` socket handling

--- packages/openssh/source-ssh-agent.sh
+++ packages/openssh/source-ssh-agent.sh
@@ -7,11 +7,11 @@ start_agent() {
 }
 
 # Allow overriding the start_agent function easily.
-if [ -r "${PREFIX}/etc/ssh/start_agent.sh" ]; then
-	. "${PREFIX}/etc/ssh/start_agent.sh"
+if [ -r "${TERMUX__PREFIX:-"${PREFIX}"}"/etc/ssh/start_agent.sh ]; then
+	. "${TERMUX__PREFIX:-"${PREFIX}"}"/etc/ssh/start_agent.sh
 fi
 
-export SSH_AUTH_SOCK="${PREFIX}/var/run/ssh-agent"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-"${TERMUX__PREFIX:-"${PREFIX}"}/var/run"}"/ssh-agent.socket
 
 MESSAGE=$(ssh-add -L 2>&1)
 if [ "$MESSAGE" = 'Could not open a connection to your authentication agent.' -o \
--- packages/openssh/wrap-ssh-agent.sh
+++ packages/openssh/wrap-ssh-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-. "${TERMUX__PREFIX:-${PREFIX}}"/libexec/source-ssh-agent.sh
+. "${TERMUX__PREFIX:-"${PREFIX}"}"/libexec/source-ssh-agent.sh
 "${wrapped_cmd}" "$@"
