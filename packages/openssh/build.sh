termux/termux-packages

commit 7b5bd568c27b1d2d08d349e6431c95b1e6195e28

bump(main/openssh): 10.0p2

packages/openssh/build.sh
@@ -2,12 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.openssh.com/
 TERMUX_PKG_DESCRIPTION="Secure shell for logging into a remote machine"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
-TERMUX_PKG_VERSION="9.9p2"
+TERMUX_PKG_VERSION="10.0p2"
 TERMUX_PKG_SRCURL=https://github.com/openssh/openssh-portable/archive/refs/tags/V_$(sed 's/\./_/g; s/p/_P/g' <<< $TERMUX_PKG_VERSION).tar.gz
-TERMUX_PKG_SHA256=082dffcf651b9db762ddbe56ca25cc75a0355a7bea41960b47f3c139974c5e3e
+TERMUX_PKG_SHA256=a25b32645dc6b474064b9deb07afc9d8e37b127d026a1170b54feb929145140c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="krb5, ldns, libandroid-support, libedit, openssh-sftp-server, openssl, termux-auth, zlib"
+TERMUX_PKG_RECOMMENDS="termux-services"
 TERMUX_PKG_CONFLICTS="dropbear"
+# Certain packages are not safe to build on device because their
+# build.sh script deletes specific files in $TERMUX_PREFIX.
+TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=true
 # --disable-strip to prevent host "install" command to use "-s", which won't work for target binaries:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-etc-default-login
@@ -42,21 +46,22 @@ ac_cv_lib_crypt_crypt=no
 ac_cv_search_getrrsetbyname=no
 ac_cv_func_bzero=yes
 "
-# Configure script require this variable to set
-# prefixed path to program 'passwd'
+# Configure script requires this variable to set prefixed path to program 'passwd'
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="PATH_PASSWD_PROG=${TERMUX_PREFIX}/bin/passwd"
 
 TERMUX_PKG_MAKE_INSTALL_TARGET="install-nokeys"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/slogin share/man/man1/slogin.1"
 TERMUX_PKG_CONFFILES="etc/ssh/ssh_config etc/ssh/sshd_config"
 
-termux_step_pre_configure() {
-	# Certain packages are not safe to build on device because their
-	# build.sh script deletes specific files in $TERMUX_PREFIX.
-	if $TERMUX_ON_DEVICE_BUILD; then
-		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
-	fi
+termux_pkg_auto_update() {
+	local latest_tag version
+	latest_tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" newest-tag)"
+	[[ -z "${latest_tag}" ]] && termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
+	version="$(sed -E 's/V_([0-9]+)_([0-9]+)_P([0-9]+)/\1.\2p\3/' <<< "${latest_tag}")"
+	termux_pkg_upgrade_version "$version"
+}
 
+termux_step_pre_configure() {
 	autoreconf
 
 	CPPFLAGS+=" -DHAVE_ATTRIBUTE__SENTINEL__=1 -DBROKEN_SETRESGID"
@@ -66,41 +71,38 @@ termux_step_pre_configure() {
 termux_step_post_configure() {
 	# We need to remove this file before installing, since otherwise the
 	# install leaves it alone which means no updated timestamps.
-	rm -f $TERMUX_PREFIX/etc/ssh/moduli
-	rm -f $TERMUX_PREFIX/etc/ssh/ssh_config
-	rm -f $TERMUX_PREFIX/etc/ssh/sshd_config
+	rm -f "$TERMUX_PREFIX/etc/ssh/moduli"
+	rm -f "$TERMUX_PREFIX/etc/ssh/ssh_config"
+	rm -f "$TERMUX_PREFIX/etc/ssh/sshd_config"
 }
 
 termux_step_post_make_install() {
-	install -Dm700 $TERMUX_PKG_BUILDER_DIR/source-ssh-agent.sh $TERMUX_PREFIX/bin/source-ssh-agent
-	install -Dm700 $TERMUX_PKG_BUILDER_DIR/ssh-with-agent.sh $TERMUX_PREFIX/bin/ssha
-	install -Dm700 $TERMUX_PKG_BUILDER_DIR/sftp-with-agent.sh $TERMUX_PREFIX/bin/sftpa
-	install -Dm700 $TERMUX_PKG_BUILDER_DIR/scp-with-agent.sh $TERMUX_PREFIX/bin/scpa
-
+	mkdir -p "$TERMUX_PREFIX/var/run"
+	echo "OpenSSH needs this directory to put sshd.pid in" > "$TERMUX_PREFIX/var/run/README.openssh"
 	# Install ssh-copy-id:
-	sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
-		$TERMUX_PKG_BUILDER_DIR/ssh-copy-id.sh \
-		> $TERMUX_PREFIX/bin/ssh-copy-id
-	chmod +x $TERMUX_PREFIX/bin/ssh-copy-id
+	echo "#!$TERMUX_PREFIX/bin/sh" > "$TERMUX_PREFIX/bin/ssh-copy-id"
+	tail -n+2 "$TERMUX_PKG_SRCDIR/contrib/ssh-copy-id" >> "$TERMUX_PREFIX/bin/ssh-copy-id"
+	sed -i -e "s|SANE_SH:-.*|SANE_SH:-$TERMUX_PREFIX/bin/bash}|g" "$TERMUX_PREFIX/bin/ssh-copy-id"
+	chmod 0700 "$TERMUX_PREFIX/bin/ssh-copy-id"
+	# Install ssh-copy-id's man page
+	mkdir -p "$TERMUX_PREFIX/share/man/man1"
+	cp "$TERMUX_PKG_SRCDIR/contrib/ssh-copy-id.1" "$TERMUX_PREFIX/share/man/man1"
 
-	mkdir -p $TERMUX_PREFIX/var/run
-	echo "OpenSSH needs this folder to put sshd.pid in" >> $TERMUX_PREFIX/var/run/README.openssh
-
-	mkdir -p $TERMUX_PREFIX/etc/ssh/
-	cp $TERMUX_PKG_SRCDIR/moduli $TERMUX_PREFIX/etc/ssh/moduli
+	mkdir -p "$TERMUX_PREFIX/etc/ssh/"
+	cp "$TERMUX_PKG_SRCDIR/moduli" "$TERMUX_PREFIX/etc/ssh/moduli"
 
 	# Setup termux-services scripts
-	mkdir -p $TERMUX_PREFIX/var/service/sshd/log
-	ln -sf $TERMUX_PREFIX/share/termux-services/svlogger $TERMUX_PREFIX/var/service/sshd/log/run
-	sed "s%@TERMUX_PREFIX@%$TERMUX_PREFIX%g" $TERMUX_PKG_BUILDER_DIR/sv/sshd.run.in > $TERMUX_PREFIX/var/service/sshd/run
-	chmod 700 $TERMUX_PREFIX/var/service/sshd/run
-	touch $TERMUX_PREFIX/var/service/sshd/down
+	mkdir -p "$TERMUX_PREFIX/var/service/sshd/log"
+	ln -sf "$TERMUX_PREFIX/share/termux-services/svlogger" "$TERMUX_PREFIX/var/service/sshd/log/run"
+	sed "s%@TERMUX_PREFIX@%$TERMUX_PREFIX%g" "$TERMUX_PKG_BUILDER_DIR/sv/sshd.run.in" > "$TERMUX_PREFIX/var/service/sshd/run"
+	chmod 700 "$TERMUX_PREFIX/var/service/sshd/run"
+	touch "$TERMUX_PREFIX/var/service/sshd/down"
 
-	mkdir -p $TERMUX_PREFIX/var/service/ssh-agent/log
-	ln -sf $TERMUX_PREFIX/share/termux-services/svlogger $TERMUX_PREFIX/var/service/ssh-agent/log/run
-	sed "s%@TERMUX_PREFIX@%$TERMUX_PREFIX%g" $TERMUX_PKG_BUILDER_DIR/sv/ssh-agent.run.in > $TERMUX_PREFIX/var/service/ssh-agent/run
-	chmod 700 $TERMUX_PREFIX/var/service/ssh-agent/run
-	touch $TERMUX_PREFIX/var/service/ssh-agent/down
+	mkdir -p "$TERMUX_PREFIX/var/service/ssh-agent/log"
+	ln -sf "$TERMUX_PREFIX/share/termux-services/svlogger" "$TERMUX_PREFIX/var/service/ssh-agent/log/run"
+	sed "s%@TERMUX_PREFIX@%$TERMUX_PREFIX%g" "$TERMUX_PKG_BUILDER_DIR/sv/ssh-agent.run.in" > "$TERMUX_PREFIX/var/service/ssh-agent/run"
+	chmod 700 "$TERMUX_PREFIX/var/service/ssh-agent/run"
+	touch "$TERMUX_PREFIX/var/service/ssh-agent/down"
 }
 
 termux_step_post_massage() {
@@ -108,34 +110,50 @@ termux_step_post_massage() {
 	mkdir -p etc/ssh/ssh_config.d
 	mkdir -p etc/ssh/sshd_config.d
 
-	# Verify that we have man pages packaged (#1538).
+	# Verify that we have man pages packaged.
+	# https://github.com/termux/termux-packages/issues/1538
 	local manpage
-	for manpage in ssh-keyscan.1 ssh-add.1 scp.1 ssh-agent.1 ssh.1; do
-		if [ ! -f share/man/man1/$manpage.gz ]; then
-			termux_error_exit "Missing man page $manpage"
-		fi
+	local -a EXPECTED_MAN_PAGES=(
+		'scp.1' 'ssh-add.1' 'ssh-agent.1' 'ssh-copy-id.1' 'ssh-keygen.1' 'ssh-keyscan.1' 'ssh.1'
+		'moduli.5' 'ssh_config.5' 'sshd_config.5'
+		'ssh-keysign.8' 'ssh-pkcs11-helper.8' 'ssh-sk-helper.8' 'sshd.8'
+	)
+
+	for manpage in "${EXPECTED_MAN_PAGES[@]}"; do
+		[[ -f "share/man/man${manpage#*.}/$manpage.gz" ]] || termux_error_exit "Missing man page $manpage in openssh"
+	done
+
+	cd "$TERMUX_PKG_MASSAGEDIR/../subpackages/openssh-sftp-server/massage/$TERMUX_PREFIX" || termux_error_exit "Failed to check openssh-sftp-server man pages"
+	for manpage in 'sftp.1' 'sftp-server.8'; do
+		[[ -f "share/man/man${manpage#*.}/$manpage.gz" ]] || termux_error_exit "Missing man page $manpage in openssh"
 	done
 }
 
 termux_step_create_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo "mkdir -p \$PREFIX/var/empty" >> postinst
-	echo "mkdir -p \$HOME/.ssh" >> postinst
-	echo "touch \$HOME/.ssh/authorized_keys" >> postinst
-	echo "chmod 700 \$HOME/.ssh" >> postinst
-	echo "chmod 600 \$HOME/.ssh/authorized_keys" >> postinst
-	echo "" >> postinst
-	echo "for a in rsa ecdsa ed25519; do" >> postinst
-	echo "	  KEYFILE=$TERMUX_PREFIX/etc/ssh/ssh_host_\${a}_key" >> postinst
-	echo "	  test ! -f \$KEYFILE && ssh-keygen -N '' -t \$a -f \$KEYFILE" >> postinst
-	echo "done" >> postinst
-	echo "exit 0" >> postinst
-	chmod 0755 postinst
-}
-
-termux_pkg_auto_update() {
-	local latest_tag="$(termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" newest-tag)"
-	[[ -z "${latest_tag}" ]] && termux_error_exit "ERROR: Unable to get tag from ${TERMUX_PKG_SRCURL}"
-	local version="$(echo ${latest_tag} | sed -E 's/V_([0-9]+)_([0-9]+)_P([0-9]+)/\1.\2p\3/')"
-	termux_pkg_upgrade_version $version
+	{
+	echo "#!$TERMUX_PREFIX/bin/sh"
+	echo "mkdir -p \"$TERMUX_PREFIX/var/empty\""
+	echo "mkdir -p \"\$HOME/.ssh\""
+	echo "touch \"\$HOME/.ssh/authorized_keys\""
+	echo "chmod 700 \"\$HOME/.ssh\""
+	echo "chmod 600 \"\$HOME/.ssh/authorized_keys\""
+	echo ""
+	echo "for a in rsa ecdsa ed25519; do"
+	echo "	KEYFILE=\"$TERMUX_PREFIX/etc/ssh/ssh_host_\${a}_key\""
+	echo "	test ! -f \"\$KEYFILE\" && ssh-keygen -N '' -t \$a -f \"\$KEYFILE\""
+	echo "done"
+	echo ""
+	echo "echo \"\""
+	echo "echo \"If you plan to use the 'ssh-agent'\""
+	echo "echo \"it is recommended to run it as a service.\""
+	echo "echo \"Run 'pkg i termux-services'\""
+	echo "echo \"to install the ('runit') service manager\""
+	echo "echo \"\""
+	echo "echo \"You can enable the ssh-agent service\""
+	echo "echo \"using 'sv-enable ssh-agent'\""
+	echo "echo \"You can also enable sshd to autostart\""
+	echo "echo \"using 'sv-enable sshd'\""
+	echo "exit 0"
+	} > postinst
+	chmod 0700 postinst
 }

packages/openssh/openssh-sftp-server.subpackage.sh
@@ -1,9 +1,10 @@
 TERMUX_SUBPKG_INCLUDE="
 libexec/sftp-server
+share/man/man1/sftp.1.gz
 share/man/man8/sftp-server.8.gz
 "
 TERMUX_SUBPKG_DESCRIPTION="OpenSSH SFTP server subsystem"
 TERMUX_SUBPKG_DEPENDS="libandroid-support"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
-TERMUX_SUBPKG_REPLACES="openssh (<< 9.0p1-1)"
-TERMUX_SUBPKG_BREAKS="openssh (<< 9.0p1-1)"
+TERMUX_SUBPKG_REPLACES="openssh (<< 9.9p2-1)"
+TERMUX_SUBPKG_BREAKS="openssh (<< 9.9p2-1)"

packages/openssh/Makefile.in.patch
@@ -1,16 +1,17 @@
-diff -uNr openssh-portable-V_9_8_P1/Makefile.in openssh-portable-V_9_8_P1.mod/Makefile.in
---- openssh-portable-V_9_8_P1/Makefile.in	2024-07-01 12:36:28.000000000 +0800
-+++ openssh-portable-V_9_8_P1.mod/Makefile.in	2024-07-01 18:12:36.935637270 +0800
-@@ -215,10 +215,10 @@
- 	$(LD) -o $@ $(SSHOBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(LIBS) $(GSSLIBS) $(CHANNELLIBS)
- 
- sshd$(EXEEXT): libssh.a	$(LIBCOMPAT) $(SSHDOBJS)
--	$(LD) -o $@ $(SSHDOBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(CHANNELLIBS)
-+	$(LD) -o $@ $(SSHDOBJS) $(LDFLAGS) -ltermux-auth -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(CHANNELLIBS)
+diff --git a/Makefile.in b/Makefile.in
+index 4617cebcd..e5af98964 100644
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -234,10 +234,10 @@ sshd$(EXEEXT): libssh.a	$(LIBCOMPAT) $(SSHDOBJS)
+ 	$(LD) -o $@ $(SSHDOBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(CHANNELLIBS)
  
  sshd-session$(EXEEXT): libssh.a	$(LIBCOMPAT) $(SSHD_SESSION_OBJS)
--	$(LD) -o $@ $(SSHD_SESSION_OBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(GSSLIBS) $(K5LIBS) $(CHANNELLIBS)
-+	$(LD) -o $@ $(SSHD_SESSION_OBJS) $(LDFLAGS) -ltermux-auth -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(GSSLIBS) $(K5LIBS) $(CHANNELLIBS)
+-	$(LD) -o $@ $(SSHD_SESSION_OBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(GSSLIBS) $(K5LIBS) $(CHANNELLIBS) $(LIBWTMPDB)
++	$(LD) -o $@ $(SSHD_SESSION_OBJS) $(LDFLAGS) -ltermux-auth -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(GSSLIBS) $(K5LIBS) $(CHANNELLIBS) $(LIBWTMPDB)
+ 
+ sshd-auth$(EXEEXT): libssh.a $(LIBCOMPAT) $(SSHD_AUTH_OBJS)
+-	$(LD) -o $@ $(SSHD_AUTH_OBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(GSSLIBS) $(K5LIBS) $(CHANNELLIBS) $(LIBWTMPDB)
++	$(LD) -o $@ $(SSHD_AUTH_OBJS) $(LDFLAGS) -ltermux-auth -lssh -lopenbsd-compat $(SSHDLIBS) $(LIBS) $(GSSLIBS) $(K5LIBS) $(CHANNELLIBS) $(LIBWTMPDB)
  
  scp$(EXEEXT): $(LIBCOMPAT) libssh.a $(SCP_OBJS)
  	$(LD) -o $@ $(SCP_OBJS) $(LDFLAGS) -lssh -lopenbsd-compat $(LIBS)

packages/openssh/scp-with-agent.sh
@@ -1,4 +1,0 @@
-#!/bin/sh
-
-. source-ssh-agent
-scp "$@"

packages/openssh/sftp-with-agent.sh
@@ -1,4 +1,0 @@
-#!/bin/sh
-
-. source-ssh-agent
-sftp "$@"

packages/openssh/source-ssh-agent.sh
@@ -1,24 +1,0 @@
-#!/bin/sh
-# source-ssh-agent: Script to source for ssh-agent to work.
-
-start_agent() {
-	ssh-agent -a "$1" > /dev/null
-	ssh-add
-}
-
-# Allow overriding the start_agent function easily.
-if [ -r "${PREFIX}/etc/ssh/start_agent.sh" ]; then
-	. "${PREFIX}/etc/ssh/start_agent.sh"
-fi
-
-export SSH_AUTH_SOCK="${PREFIX}/var/run/ssh-agent"
-
-MESSAGE=$(ssh-add -L 2>&1)
-if [ "$MESSAGE" = 'Could not open a connection to your authentication agent.' -o \
-     "$MESSAGE" = 'Error connecting to agent: Connection refused' -o \
-     "$MESSAGE" = 'Error connecting to agent: No such file or directory' ]; then
-	rm -f "${SSH_AUTH_SOCK}"
-	start_agent "${SSH_AUTH_SOCK}"
-elif [ "$MESSAGE" = "The agent has no identities." ]; then
-	ssh-add
-fi

packages/openssh/ssh-copy-id.sh
@@ -1,324 +1,0 @@
-#!/bin/sh
-
-# Copyright (c) 1999-2016 Philip Hands <phil@hands.com>
-#               2013 Martin Kletzander <mkletzan@redhat.com>
-#               2010 Adeodato =?iso-8859-1?Q?Sim=F3?= <asp16@alu.ua.es>
-#               2010 Eric Moret <eric.moret@gmail.com>
-#               2009 Xr <xr@i-jeuxvideo.com>
-#               2007 Justin Pryzby <justinpryzby@users.sourceforge.net>
-#               2004 Reini Urban <rurban@x-ray.at>
-#               2003 Colin Watson <cjwatson@debian.org>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Shell script to install your public key(s) on a remote machine
-# See the ssh-copy-id(1) man page for details
-
-# check that we have something mildly sane as our shell, or try to find something better
-if false ^ printf "%s: WARNING: ancient shell, hunting for a more modern one... " "$0"
-then
-  SANE_SH=${SANE_SH:-@TERMUX_PREFIX@/bin/bash}
-  if printf 'true ^ false\n' | "$SANE_SH"
-  then
-    printf "'%s' seems viable.\n" "$SANE_SH"
-    exec "$SANE_SH" "$0" "$@"
-  else
-    cat <<-EOF
-	oh dear.
-
-	  If you have a more recent shell available, that supports \$(...) etc.
-	  please try setting the environment variable SANE_SH to the path of that
-	  shell, and then retry running this script. If that works, please report
-	  a bug describing your setup, and the shell you used to make it work.
-
-	EOF
-    printf "%s: ERROR: Less dimwitted shell required.\n" "$0"
-    exit 1
-  fi
-fi
-
-most_recent_id="$(cd "$HOME" ; ls -t .ssh/id*.pub 2>/dev/null | grep -v -- '-cert.pub$' | head -n 1)"
-DEFAULT_PUB_ID_FILE="${most_recent_id:+$HOME/}$most_recent_id"
-
-usage () {
-  printf 'Usage: %s [-h|-?|-f|-n] [-i [identity_file]] [-p port] [[-o <ssh -o options>] ...] [user@]hostname\n' "$0" >&2
-  printf '\t-f: force mode -- copy keys without trying to check if they are already installed\n' >&2
-  printf '\t-n: dry run    -- no keys are actually copied\n' >&2
-  printf '\t-h|-?: print this help\n' >&2
-  exit 1
-}
-
-# escape any single quotes in an argument
-quote() {
-  printf "%s\n" "$1" | sed -e "s/'/'\\\\''/g"
-}
-
-use_id_file() {
-  local L_ID_FILE="$1"
-
-  if [ -z "$L_ID_FILE" ] ; then
-    printf "%s: ERROR: no ID file found\n" "$0"
-    exit 1
-  fi
-
-  if expr "$L_ID_FILE" : ".*\.pub$" >/dev/null ; then
-    PUB_ID_FILE="$L_ID_FILE"
-  else
-    PUB_ID_FILE="$L_ID_FILE.pub"
-  fi
-
-  [ "$FORCED" ] || PRIV_ID_FILE=$(dirname "$PUB_ID_FILE")/$(basename "$PUB_ID_FILE" .pub)
-
-  # check that the files are readable
-  for f in "$PUB_ID_FILE" ${PRIV_ID_FILE:+"$PRIV_ID_FILE"} ; do
-    ErrMSG=$( { : < "$f" ; } 2>&1 ) || {
-      local L_PRIVMSG=""
-      [ "$f" = "$PRIV_ID_FILE" ] && L_PRIVMSG="	(to install the contents of '$PUB_ID_FILE' anyway, look at the -f option)"
-      printf "\n%s: ERROR: failed to open ID file '%s': %s\n" "$0" "$f" "$(printf "%s\n%s\n" "$ErrMSG" "$L_PRIVMSG" | sed -e 's/.*: *//')"
-      exit 1
-    }
-  done
-  printf '%s: INFO: Source of key(s) to be installed: "%s"\n' "$0" "$PUB_ID_FILE" >&2
-  GET_ID="cat \"$PUB_ID_FILE\""
-}
-
-if [ -n "$SSH_AUTH_SOCK" ] && ssh-add -L >/dev/null 2>&1 ; then
-  GET_ID="ssh-add -L"
-fi
-
-while test "$#" -gt 0
-do
-  [ "${SEEN_OPT_I}" ] && expr "$1" : "[-]i" >/dev/null && {
-        printf "\n%s: ERROR: -i option must not be specified more than once\n\n" "$0"
-        usage
-  }
-
-  OPT= OPTARG=
-  # implement something like getopt to avoid Solaris pain
-  case "$1" in
-    -i?*|-o?*|-p?*)
-      OPT="$(printf -- "$1"|cut -c1-2)"
-      OPTARG="$(printf -- "$1"|cut -c3-)"
-      shift
-      ;;
-    -o|-p)
-      OPT="$1"
-      OPTARG="$2"
-      shift 2
-      ;;
-    -i)
-      OPT="$1"
-      test "$#" -le 2 || expr "$2" : "[-]" >/dev/null || {
-        OPTARG="$2"
-        shift
-      }
-      shift
-      ;;
-    -f|-n|-h|-\?)
-      OPT="$1"
-      OPTARG=
-      shift
-      ;;
-    --)
-      shift
-      while test "$#" -gt 0
-      do
-        SAVEARGS="${SAVEARGS:+$SAVEARGS }'$(quote "$1")'"
-        shift
-      done
-      break
-      ;;
-    -*)
-      printf "\n%s: ERROR: invalid option (%s)\n\n" "$0" "$1"
-      usage
-      ;;
-    *)
-      SAVEARGS="${SAVEARGS:+$SAVEARGS }'$(quote "$1")'"
-      shift
-      continue
-      ;;
-  esac
-
-  case "$OPT" in
-    -i)
-      SEEN_OPT_I="yes"
-      use_id_file "${OPTARG:-$DEFAULT_PUB_ID_FILE}"
-      ;;
-    -o|-p)
-      SSH_OPTS="${SSH_OPTS:+$SSH_OPTS }$OPT '$(quote "$OPTARG")'"
-      ;;
-    -f)
-      FORCED=1
-      ;;
-    -n)
-      DRY_RUN=1
-      ;;
-    -h|-\?)
-      usage
-      ;;
-  esac
-done 
-
-eval set -- "$SAVEARGS"
-
-if [ $# = 0 ] ; then
-  usage
-fi
-if [ $# != 1 ] ; then
-  printf '%s: ERROR: Too many arguments.  Expecting a target hostname, got: %s\n\n' "$0" "$SAVEARGS" >&2
-  usage
-fi
-
-# drop trailing colon
-USER_HOST=$(printf "%s\n" "$1" | sed 's/:$//')
-# tack the hostname onto SSH_OPTS
-SSH_OPTS="${SSH_OPTS:+$SSH_OPTS }'$(quote "$USER_HOST")'"
-# and populate "$@" for later use (only way to get proper quoting of options)
-eval set -- "$SSH_OPTS"
-
-if [ -z "$(eval $GET_ID)" ] && [ -r "${PUB_ID_FILE:=$DEFAULT_PUB_ID_FILE}" ] ; then
-  use_id_file "$PUB_ID_FILE"
-fi
-
-if [ -z "$(eval $GET_ID)" ] ; then
-  printf '%s: ERROR: No identities found\n' "$0" >&2
-  exit 1
-fi
-
-# populate_new_ids() uses several global variables ($USER_HOST, $SSH_OPTS ...)
-# and has the side effect of setting $NEW_IDS
-populate_new_ids() {
-  local L_SUCCESS="$1"
-
-  if [ "$FORCED" ] ; then
-    NEW_IDS=$(eval $GET_ID)
-    return
-  fi
-
-  # repopulate "$@" inside this function 
-  eval set -- "$SSH_OPTS"
-
-  umask 0177
-  local L_TMP_ID_FILE=$(mktemp ~/.ssh/ssh-copy-id_id.XXXXXXXXXX)
-  if test $? -ne 0 || test "x$L_TMP_ID_FILE" = "x" ; then
-    printf '%s: ERROR: mktemp failed\n' "$0" >&2
-    exit 1
-  fi
-  local L_CLEANUP="rm -f \"$L_TMP_ID_FILE\" \"${L_TMP_ID_FILE}.stderr\""
-  trap "$L_CLEANUP" EXIT TERM INT QUIT
-  printf '%s: INFO: attempting to log in with the new key(s), to filter out any that are already installed\n' "$0" >&2
-  NEW_IDS=$(
-    eval $GET_ID | {
-      while read ID || [ "$ID" ] ; do
-        printf '%s\n' "$ID" > "$L_TMP_ID_FILE"
-
-        # the next line assumes $PRIV_ID_FILE only set if using a single id file - this
-        # assumption will break if we implement the possibility of multiple -i options.
-        # The point being that if file based, ssh needs the private key, which it cannot
-        # find if only given the contents of the .pub file in an unrelated tmpfile
-        ssh -i "${PRIV_ID_FILE:-$L_TMP_ID_FILE}" \
-            -o ControlPath=none \
-            -o LogLevel=INFO \
-            -o PreferredAuthentications=publickey \
-            -o IdentitiesOnly=yes "$@" exit 2>"$L_TMP_ID_FILE.stderr" </dev/null
-        if [ "$?" = "$L_SUCCESS" ] ; then
-          : > "$L_TMP_ID_FILE"
-        else
-          grep 'Permission denied' "$L_TMP_ID_FILE.stderr" >/dev/null || {
-            sed -e 's/^/ERROR: /' <"$L_TMP_ID_FILE.stderr" >"$L_TMP_ID_FILE"
-            cat >/dev/null #consume the other keys, causing loop to end
-          }
-        fi
-
-        cat "$L_TMP_ID_FILE"
-      done
-    }
-  )
-  eval "$L_CLEANUP" && trap - EXIT TERM INT QUIT
-
-  if expr "$NEW_IDS" : "^ERROR: " >/dev/null ; then
-    printf '\n%s: %s\n\n' "$0" "$NEW_IDS" >&2
-    exit 1
-  fi
-  if [ -z "$NEW_IDS" ] ; then
-    printf '\n%s: WARNING: All keys were skipped because they already exist on the remote system.\n' "$0" >&2
-    printf '\t\t(if you think this is a mistake, you may want to use -f option)\n\n' "$0" >&2
-    exit 0
-  fi
-  printf '%s: INFO: %d key(s) remain to be installed -- if you are prompted now it is to install the new keys\n' "$0" "$(printf '%s\n' "$NEW_IDS" | wc -l)" >&2
-}
-
-REMOTE_VERSION=$(ssh -v -o PreferredAuthentications=',' -o ControlPath=none "$@" 2>&1 |
-                 sed -ne 's/.*remote software version //p')
-
-case "$REMOTE_VERSION" in
-  NetScreen*)
-    populate_new_ids 1
-    for KEY in $(printf "%s" "$NEW_IDS" | cut -d' ' -f2) ; do
-      KEY_NO=$(($KEY_NO + 1))
-      printf "%s\n" "$KEY" | grep ssh-dss >/dev/null || {
-         printf '%s: WARNING: Non-dsa key (#%d) skipped (NetScreen only supports DSA keys)\n' "$0" "$KEY_NO" >&2
-         continue
-      }
-      [ "$DRY_RUN" ] || printf 'set ssh pka-dsa key %s\nsave\nexit\n' "$KEY" | ssh -T "$@" >/dev/null 2>&1
-      if [ $? = 255 ] ; then
-        printf '%s: ERROR: installation of key #%d failed (please report a bug describing what caused this, so that we can make this message useful)\n' "$0" "$KEY_NO" >&2
-      else
-        ADDED=$(($ADDED + 1))
-      fi
-    done
-    if [ -z "$ADDED" ] ; then
-      exit 1
-    fi
-    ;;
-  *)
-    # Assuming that the remote host treats ~/.ssh/authorized_keys as one might expect
-    populate_new_ids 0
-    # in ssh below - to defend against quirky remote shells: use 'exec sh -c' to get POSIX;
-    #     'cd' to be at $HOME; add a newline if it's missing; and all on one line, because tcsh.
-    [ "$DRY_RUN" ] || printf '%s\n' "$NEW_IDS" | \
-      ssh "$@" "exec sh -c 'cd ; umask 077 ; mkdir -p .ssh && { [ -z "'`tail -1c .ssh/authorized_keys 2>/dev/null`'" ] || echo >> .ssh/authorized_keys ; } && cat >> .ssh/authorized_keys || exit 1 ; if type restorecon >/dev/null 2>&1 ; then restorecon -F .ssh .ssh/authorized_keys ; fi'" \
-      || exit 1
-    ADDED=$(printf '%s\n' "$NEW_IDS" | wc -l)
-    ;;
-esac
-
-if [ "$DRY_RUN" ] ; then
-  cat <<-EOF
-	=-=-=-=-=-=-=-=
-	Would have added the following key(s):
-
-	$NEW_IDS
-	=-=-=-=-=-=-=-=
-	EOF
-else
-  cat <<-EOF
-
-	Number of key(s) added: $ADDED
-
-	Now try logging into the machine, with:   "ssh $SSH_OPTS"
-	and check to make sure that only the key(s) you wanted were added.
-
-	EOF
-fi
-
-# =-=-=-=

packages/openssh/ssh-with-agent.sh
@@ -1,4 +1,0 @@
-#!/bin/sh
-
-. source-ssh-agent
-ssh "$@"

commit 864e035a793e72c9feb3a157223c10b015c96753

fix(main/openssh): fix conflict with openssh-sftp-server
openssh-sftp-server should conflict with openssh <= 9.9p2-1, or else
upgrade fails with:

Preparing to unpack .../81-openssh-sftp-server_10.0p2-4_arm.deb ...
Unpacking openssh-sftp-server (10.0p2-4) over (9.9p2-1) ...
dpkg: error processing archive /data/data/com.termux/files/usr/tmp/apt-dpkg-install-41JbqT/81-openssh-sftp-server_10.0p2-4_arm.deb (--unpack):
 trying to overwrite '/data/data/com.termux/files/usr/share/man/man1/sftp.1.gz', which is also in package openssh 9.9p2-1
Preparing to unpack .../82-openssh_10.0p2-4_arm.deb ...
Unpacking openssh (10.0p2-4) over (9.9p2-1) ...

Fixes: 7b5bd568c27b ("bump(main/openssh): 10.0p2")

packages/openssh/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Secure shell for logging into a remote machine"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="10.0p2"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/openssh/openssh-portable/archive/refs/tags/V_$(sed 's/\./_/g; s/p/_P/g' <<< $TERMUX_PKG_VERSION).tar.gz
 TERMUX_PKG_SHA256=a25b32645dc6b474064b9deb07afc9d8e37b127d026a1170b54feb929145140c
 TERMUX_PKG_AUTO_UPDATE=true

packages/openssh/openssh-sftp-server.subpackage.sh
@@ -6,5 +6,5 @@ share/man/man8/sftp-server.8.gz
 TERMUX_SUBPKG_DESCRIPTION="OpenSSH SFTP server subsystem"
 TERMUX_SUBPKG_DEPENDS="libandroid-support"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
-TERMUX_SUBPKG_REPLACES="openssh (<< 9.9p2-1)"
-TERMUX_SUBPKG_BREAKS="openssh (<< 9.9p2-1)"
+TERMUX_SUBPKG_REPLACES="openssh (<= 9.9p2-1)"
+TERMUX_SUBPKG_BREAKS="openssh (<= 9.9p2-1)"