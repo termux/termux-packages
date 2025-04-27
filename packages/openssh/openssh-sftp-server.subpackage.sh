4756daac57b1c30faf6eae741bcabe2c9c2bb0a3
Henrik Grimler <grimler@termux.dev>
2025-04-27 21:50:43 +0200

    fix(main/openssh): fix conflict with openssh-sftp-server
    openssh-sftp-server should conflict with openssh <= 9.9p2-1, or else
    upgrade fails with:
    
    Preparing to unpack .../81-openssh-sftp-server_10.0p2-4_arm.deb ...
    Unpacking openssh-sftp-server (10.0p2-4) over (9.9p2-1) ...
    dpkg: error processing archive /data/data/com.termux/files/usr/tmp/apt-dpkg-install-41JbqT/81-openssh-sftp-server_10.0p2-4_arm.deb (--unpack):
     trying to overwrite '/data/data/com.termux/files/usr/share/man/man1/sftp.1.gz', which is also in package openssh 9.9p2-1
    Preparing to unpack .../82-openssh_10.0p2-4_arm.deb ...
    Unpacking openssh (10.0p2-4) over (9.9p2-1) ...
    
    Fixes: 7b5bd568c27b ("bump(main/openssh): 10.0p2")

--- packages/openssh/openssh-sftp-server.subpackage.sh
+++ packages/openssh/openssh-sftp-server.subpackage.sh
@@ -6,5 +6,5 @@ share/man/man8/sftp-server.8.gz
 TERMUX_SUBPKG_DESCRIPTION="OpenSSH SFTP server subsystem"
 TERMUX_SUBPKG_DEPENDS="libandroid-support"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
-TERMUX_SUBPKG_REPLACES="openssh (<< 9.9p2-1)"
-TERMUX_SUBPKG_BREAKS="openssh (<< 9.9p2-1)"
+TERMUX_SUBPKG_REPLACES="openssh (<= 9.9p2-1)"
+TERMUX_SUBPKG_BREAKS="openssh (<= 9.9p2-1)"
