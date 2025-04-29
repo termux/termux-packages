8d6a3e0474429a0e0a8676f666ea2a13b7aadcfa
Robert Kirkman <rkirkman@termux.dev>
2025-04-29 16:19:59 -0500

    bump(x11/firefox): 138.0
    - Fixes https://github.com/termux/termux-packages/issues/24535
    
    - As predicted and prepared for, `SharedMemory_posix.cpp` has been transitioned away from and was deleted in commit https://github.com/mozilla/gecko-dev/commit/8cbcede05e9563dae119db2105668dfa134c6691. Its patch is thought to have already been replaced with a patch to the new `SharedMemoryPlatform_posix.cpp` instead, which is already prepared in the same patch file here.

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mozilla.org/firefox
 TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="137.0.2"
+TERMUX_PKG_VERSION="138.0"
 TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION}/source/firefox-${TERMUX_PKG_VERSION}.source.tar.xz
-TERMUX_PKG_SHA256=06fc3abaa822148bfa5ee7b759428d74052493876c053c05379c3f6c302bcd07
+TERMUX_PKG_SHA256=04623d7ee88c1b456228dca1aac4d40b18fee82c803b6562a34ce30b46f19b23
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
