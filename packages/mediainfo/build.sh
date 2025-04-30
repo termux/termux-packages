e6f618de9da5e2dfccf15ba7caff304d84e7aac8
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-04-30 23:25:11 +0300

    bump(main/mediainfo): 25.04

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Command-line utility for reading information from media 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="../../../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="25.03"
+TERMUX_PKG_VERSION="25.04"
 TERMUX_PKG_SRCURL=https://github.com/MediaArea/MediaInfo/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a03490cddb72840882600bde4d0b9c329e4ef3d4f37a2aeed59720bb93498283
+TERMUX_PKG_SHA256=131b27960490887efee8db6a8471f98c37b478360060f9058d047e3c6a3afc44
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libmediainfo, libzen"
 
