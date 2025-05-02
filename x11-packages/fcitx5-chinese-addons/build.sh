2640b8a14e5632f7f41506a533ad66a658e1d31f
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-02 10:01:20 +0000

    enhance(x11/fcitx5-chinese-addons): Build dictionary file

--- x11-packages/fcitx5-chinese-addons/build.sh
+++ x11-packages/fcitx5-chinese-addons/build.sh
@@ -3,16 +3,38 @@ TERMUX_PKG_DESCRIPTION="Addons related to Chinese, including IME previous bundle
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.1.8"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL="https://github.com/fcitx/fcitx5-chinese-addons/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=0182025f7451adb2df488812b31b900dfb59fb264a6b485fa2339e8ecd63bb41
 TERMUX_PKG_DEPENDS="boost, fcitx5, fcitx5-qt, libc++, libcurl, libime, libopencc, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="boost-headers, extra-cmake-modules"
+TERMUX_PKG_BUILD_DEPENDS="aosp-libs, boost-headers, extra-cmake-modules"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-# FIXME: Enable generating dictionary data
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_BROWSER=OFF
 -DENABLE_DATA=OFF
 -DENABLE_TEST=OFF
 "
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		termux_setup_proot
+		patch -p1 -i "$TERMUX_PKG_BUILDER_DIR"/im-pinyin-CMakeLists.txt.diff
+	fi
+}
+
+termux_step_post_make_install() {
+	echo -e "termux - building fcitx5-chinese-addons dictionary..."
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=' -DENABLE_DATA=ON'
+	termux_step_configure
+	termux_step_make
+
+	# from add_custom_commands in im/pinyin/CMakeLists.txt
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		pushd im/pinyin
+		termux-proot-run "${TERMUX_PREFIX}"/bin/libime_pinyindict "${TERMUX_PKG_SRCDIR}"/im/pinyin/chaizi.txt chaizi.dict
+		popd
+	fi
+
+	termux_step_make_install
+}
