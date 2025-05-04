85a12c25bbd74efc9a59abf745329a83bdf12abc
Chongyun Lee <licy183@termux.dev>
2025-05-04 23:38:30 +0800

    libical: enable glib

--- packages/libical/build.sh
+++ packages/libical/build.sh
@@ -3,14 +3,38 @@ TERMUX_PKG_DESCRIPTION="Libical is an Open Source implementation of the iCalenda
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.0.20"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/libical/libical/releases/download/v$TERMUX_PKG_VERSION/libical-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e73de92f5a6ce84c1b00306446b290a2b08cdf0a80988eca0a2c9d5c3510b4c2
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, libicu"
+TERMUX_PKG_DEPENDS="glib, libc++, libicu, libxml2"
 TERMUX_PKG_BREAKS="libical-dev"
 TERMUX_PKG_REPLACES="libical-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DSHARED_ONLY=true -DICAL_GLIB=false -DUSE_BUILTIN_TZDATA=true -DPERL_EXECUTABLE=/usr/bin/perl"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DSHARED_ONLY=true
+-DUSE_BUILTIN_TZDATA=true
+-DPERL_EXECUTABLE=/usr/bin/perl
+-DICAL_GLIB=true
+-DIMPORT_ICAL_GLIB_SRC_GENERATOR=$TERMUX_PKG_HOSTBUILD_DIR/host-prefix/lib/cmake/LibIcal/IcalGlibSrcGenerator.cmake
+-DENABLE_GTK_DOC=false
+"
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	termux_setup_cmake
+	termux_setup_ninja
+
+	mkdir -p "$TERMUX_PKG_HOSTBUILD_DIR"/host-prefix
+
+	cmake \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX="$TERMUX_PKG_HOSTBUILD_DIR"/host-prefix \
+		-DENABLE_GTK_DOC=OFF \
+		-G Ninja \
+		"$TERMUX_PKG_SRCDIR"
+	cmake --build .
+	cmake --install . --prefix "$TERMUX_PKG_HOSTBUILD_DIR"/host-prefix
+}
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
