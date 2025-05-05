d07f727c37869a75e06b99804a849e9b697a56f5
TomIO <tomjo00@web.de>
2025-05-05 15:34:59 +0200

    revbump(main/nala): after `apt` update

--- packages/nala/build.sh
+++ packages/nala/build.sh
@@ -3,8 +3,9 @@ TERMUX_PKG_DESCRIPTION="Commandline frontend for the apt package manager"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.16.0"
-TERMUX_PKG_SRCURL=https://gitlab.com/volian/nala/-/archive/v${TERMUX_PKG_VERSION}/nala-v${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=49e384aa3b94597d09c61b7accc41d1cf10cb6beea85d4620c80c28d7cdc4d5f
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://gitlab.com/volian/nala/-/archive/v${TERMUX_PKG_VERSION}/nala-v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=e36488f96facf4dbbeaf2fb252eb3bfa421a96f290e666d9a37137ec58eccd4c
 TERMUX_PKG_DEPENDS="python-apt, python-pip"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -23,17 +24,17 @@ termux_step_post_make_install() {
 		pandoc "${file}" --output="${file%.*}" --standalone \
 			--variable=header:"Nala User Manual" \
 			--variable=footer:"${TERMUX_PKG_VERSION}" \
-			--variable=date:"$(date -d @${SOURCE_DATE_EPOCH})" \
+			--variable=date:"$(date -d "@${SOURCE_DATE_EPOCH}")" \
 			--variable=section:8 \
 			--from rst --to man
 
 		install -Dm600 -t "$TERMUX_PREFIX"/share/man/man8/ "${file%.*}"
 	done
 
-	install -Dm600 -t $TERMUX_PREFIX/etc/nala debian/nala.conf
-	install -Dm600 debian/nala.fish "$TERMUX_PREFIX"/share/fish/vendor_completions.d/nala.fish
-	install -Dm600 debian/bash-completion "$TERMUX_PREFIX"/share/bash-completion/completions/nala
-	install -Dm600 debian/_nala "$TERMUX_PREFIX"/share/zsh/site-functions/_nala
+	install -Dm600 -t "$TERMUX_PREFIX/etc/nala" debian/nala.conf
+	install -Dm600 debian/nala.fish "$TERMUX_PREFIX/share/fish/vendor_completions.d/nala.fish"
+	install -Dm600 debian/bash-completion "$TERMUX_PREFIX/share/bash-completion/completions/nala"
+	install -Dm600 debian/_nala "$TERMUX_PREFIX/share/zsh/site-functions/_nala"
 }
 
 termux_step_create_debscripts() {
