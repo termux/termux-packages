971bda4805277583870dbe7e183fbe7f0e3b0a9d
Tomasz Duda <tomaszduda23@gmail.com>
2025-05-06 17:45:07 +0200

    use runc 1.3.0

--- root-packages/runc/build.sh
+++ root-packages/runc/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.opencontainers.org/
 TERMUX_PKG_DESCRIPTION="A tool for spawning and running containers according to the OCI specification"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.2.6"
+TERMUX_PKG_VERSION="1.3.0"
 TERMUX_PKG_SRCURL=https://github.com/opencontainers/runc/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=19b280702341f33ff353fa254d1dbdb67f6aab2c74395f6d651a17524f68e752
+TERMUX_PKG_SHA256=585369f7279fe6bf56cc130b19d0db3cdd9164d55fa330991f3d090fb909f34c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="libseccomp-static"
 
