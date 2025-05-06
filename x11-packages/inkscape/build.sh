e1c9a1b28fd42d45206356975985e7f4dd976a7e
Robert Kirkman <rkirkman@termux.dev>
2025-05-06 16:06:42 -0500

    fix(x11/inkscape): mark as depending on `libjasper`
    - Fixes https://github.com/termux/termux-packages/issues/24635

--- x11-packages/inkscape/build.sh
+++ x11-packages/inkscape/build.sh
@@ -15,10 +15,10 @@ LICENSES/OFL-1.1.txt
 "
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://media.inkscape.org/dl/resources/file/inkscape-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=c59a85453b699addebcd51c1dc07684dd96a10c8aec716b19551db50562e13f5
-TERMUX_PKG_DEPENDS="boost, double-conversion, fontconfig, freetype, gdk-pixbuf, glib, gsl, gspell, gtk3, gtkmm3, gtksourceview4, harfbuzz, libatkmm-1.6, libc++, libcairo, libcairomm-1.0, libgc, libglibmm-2.4, libiconv, libjpeg-turbo, libpangomm-1.4, libpng, libsigc++-2.0, libsoup, libx11, libxml2, libxslt, littlecms, pango, poppler, potrace, readline, zlib"
+TERMUX_PKG_DEPENDS="boost, double-conversion, fontconfig, freetype, gdk-pixbuf, glib, gsl, gspell, gtk3, gtkmm3, gtksourceview4, harfbuzz, libatkmm-1.6, libc++, libcairo, libcairomm-1.0, libgc, libglibmm-2.4, libiconv, libjasper, libjpeg-turbo, libpangomm-1.4, libpng, libsigc++-2.0, libsoup, libx11, libxml2, libxslt, littlecms, pango, poppler, potrace, readline, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, graphicsmagick-static"
 TERMUX_PKG_RECOMMENDS="inkscape-extensions, inkscape-tutorials"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
