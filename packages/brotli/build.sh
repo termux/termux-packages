d09a983412799378c88ae95e128f001daefb5bf4
Robert Kirkman <rkirkman@termux.dev>
2025-05-10 00:55:11 -0500

    fix(main/python-brotli): separate `build.sh` from `brotli` again
    - Reverts https://github.com/termux/termux-packages/pull/24397 because unfortunately, it causes the error `ERROR: /data/data/com.termux/files/usr/bin/python3.12 does not exist` when the command `scripts/run-docker.sh ./build-package.sh python` (**without `-I`**) is used

--- packages/brotli/build.sh
+++ packages/brotli/build.sh
@@ -3,11 +3,10 @@ TERMUX_PKG_DESCRIPTION="lossless compression algorithm and format (command line 
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/google/brotli/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=e720a6ca29428b803f4ad165371771f5398faba397edf6778837a18599ea13ff
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
 TERMUX_PKG_BREAKS="brotli-dev"
 TERMUX_PKG_REPLACES="brotli-dev"
 TERMUX_PKG_FORCE_CMAKE=true
@@ -31,11 +30,4 @@ termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/share/man/man{1,3}
 	cp "$TERMUX_PKG_SRCDIR/docs/brotli.1" "$TERMUX_PREFIX/share/man/man1/"
 	cp "$TERMUX_PKG_SRCDIR"/docs/*.3 "$TERMUX_PREFIX/share/man/man3/"
-
-	cd "$TERMUX_PKG_SRCDIR"
-	# ERROR: ./lib/python3.12/site-packages/_brotli.cpython-312.so contains undefined symbols:
-	# 31: 0000000000000000     0 NOTYPE  GLOBAL DEFAULT   UND log2
-	LDFLAGS+=" -lm"
-	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
-	pip install . --prefix="$TERMUX_PREFIX" -vv --no-build-isolation --no-deps
 }
