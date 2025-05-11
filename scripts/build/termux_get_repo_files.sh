8d1ac1fcd1deeb3c3b8a37f03c48a75d314958bf
Twaik Yont <9674930+twaik@users.noreply.github.com>
2025-05-11 14:33:59 +0300

    scripts(termux_get_repo_files): redownload metadata if corrupted

--- scripts/build/termux_get_repo_files.sh
+++ scripts/build/termux_get_repo_files.sh
@@ -32,23 +32,29 @@ termux_get_repo_files() {
 		esac
 
 		(
-			if termux_download "${RELEASE_FILE_URL}" "${RELEASE_FILE}" SKIP_CHECKSUM && \
-				termux_download "${RELEASE_FILE_SIG_URL}" "${RELEASE_FILE}.gpg" SKIP_CHECKSUM && \
-				gpg --verify "${RELEASE_FILE}.gpg" "${RELEASE_FILE}"; then
+			for retry in {1..3}; do
+				if termux_download "${RELEASE_FILE_URL}" "${RELEASE_FILE}" SKIP_CHECKSUM \
+						&& termux_download "${RELEASE_FILE_SIG_URL}" "${RELEASE_FILE}.gpg" SKIP_CHECKSUM; then
+					if ! gpg --verify "${RELEASE_FILE}.gpg" "${RELEASE_FILE}"; then
+						echo "GPG verification failed, probably we downloaded corrupted metadata. Retrying in 5 seconds."
+						sleep 5
+						continue
+					fi
 
-				if [[ "$TERMUX_REPO_PKG_FORMAT" == "debian" ]]; then
-					for arch in all "${TERMUX_ARCH}"; do
-						PACKAGES_HASH="$(./scripts/get_hash_from_file.py "${RELEASE_FILE}" "${arch}" "${TERMUX_REPO_COMPONENT[$idx]}")"
+					if [[ "$TERMUX_REPO_PKG_FORMAT" == "debian" ]]; then
+						for arch in all "${TERMUX_ARCH}"; do
+							PACKAGES_HASH="$(./scripts/get_hash_from_file.py "${RELEASE_FILE}" "${arch}" "${TERMUX_REPO_COMPONENT[$idx]}")"
 
-						# If packages_hash = "" then the repo probably doesn't contain debs for $arch
-						[[ -n "$PACKAGES_HASH" ]] && \
-							termux_download "${repo_base}/${TERMUX_REPO_COMPONENT[$idx]}/binary-$arch/Packages" \
-									"${TERMUX_COMMON_CACHEDIR}-$arch/${dl_prefix}-Packages" "$PACKAGES_HASH" && \
-							exit 0
-					done
+							# If packages_hash = "" then the repo probably doesn't contain debs for $arch
+							[[ -n "$PACKAGES_HASH" ]] && \
+								termux_download "${repo_base}/${TERMUX_REPO_COMPONENT[$idx]}/binary-$arch/Packages" \
+										"${TERMUX_COMMON_CACHEDIR}-$arch/${dl_prefix}-Packages" "$PACKAGES_HASH" && \
+								exit 0
+						done
+					fi
+					exit 0
 				fi
-				exit 0
-			fi
+			done
 			termux_error_exit "Failed to download package repository metadata. Try to build without -i/-I option."
 		) 2>&1 | (
 			set +e
