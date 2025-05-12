515c34126273c661aa6a95648749d7bc575315ed
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-12 13:38:26 +0000

    bump(x11/x11vnc): 0.9.17

--- x11-packages/x11vnc/build.sh
+++ x11-packages/x11vnc/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/LibVNC/x11vnc
 TERMUX_PKG_DESCRIPTION="VNC server for real X displays"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.16
-TERMUX_PKG_REVISION=11
-TERMUX_PKG_SRCURL=https://github.com/LibVNC/x11vnc/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=885e5b5f5f25eec6f9e4a1e8be3d0ac71a686331ee1cfb442dba391111bd32bd
+TERMUX_PKG_VERSION="0.9.17"
+TERMUX_PKG_SRCURL=https://github.com/LibVNC/x11vnc/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3ab47c042bc1c33f00c7e9273ab674665b85ab10592a8e0425589fe7f3eb1a69
 TERMUX_PKG_DEPENDS="libandroid-shmem, libcairo, libvncserver, libx11, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxrandr, libxtst, openssl, xorg-xdpyinfo"
 
 # https://github.com/termux/termux-packages/issues/15240
@@ -13,6 +12,5 @@ TERMUX_PKG_RM_AFTER_INSTALL="bin/Xdummy"
 
 termux_step_pre_configure() {
 	autoreconf -vi
-	CFLAGS+=" -fcommon"
 	export LIBS="-landroid-shmem"
 }
