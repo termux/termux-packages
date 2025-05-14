c2ba3473a8c8c11ffcd269d788e5ab2e11e51d3b
Robert Kirkman <rkirkman@termux.dev>
2025-05-14 08:17:36 -0500

    addpkg(main/python-ruff): 0.11.10
    - Fixes https://github.com/termux/termux-packages/issues/24682
      - (fixes the command `pip install changedetection.io` on-device because `python-ruff` is a dependency of that and cannot currently be built on-device)
    
    - Fixes https://github.com/dgtlmoon/changedetection.io/issues/3188
      - (other workaround found there was to [edit the `requirements.txt` and remove `ruff`](https://github.com/dgtlmoon/changedetection.io/blob/c162ec9d52d9977cdd2a434351d7ab149bb6427e/requirements.txt#L116), now it should also work without needing to do that)
    
    - Arch Linux `ruff`, `python-ruff` packages used to help: https://gitlab.archlinux.org/archlinux/packaging/packages/ruff/-/blob/d3288eff68a26c8f1d6268a6a42128f39e264410/PKGBUILD
    
    - `--skip-auditwheel` workaround for `maturin`-related build errors when targeting Termux found here: https://github.com/PyO3/pyo3/issues/2324
    
    - `tikv-jemalloc-sys-0.5.3+5.3.0-patched-src-lib.rs.diff` is no longer applying + no longer necessary after https://github.com/tikv/jemallocator/pull/83

---
+++ packages/ruff/python-ruff.subpackage.sh
@@ -0,0 +1,3 @@
+TERMUX_SUBPKG_DESCRIPTION="An extremely fast Python linter, written in Rust (Python bindings)"
+TERMUX_SUBPKG_INCLUDE="lib/python${TERMUX_PYTHON_VERSION}/site-packages"
+TERMUX_SUBPKG_DEPENDS="python, python-pip"
