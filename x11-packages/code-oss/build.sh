0b5a82ad94b5ac1321f824cb15a46f82cc27df9c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-05-16 23:42:52 +0800

    code-oss: include vcse-sign modules (#24740)

--- x11-packages/code-oss/build.sh
+++ x11-packages/code-oss/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Visual Studio Code - OSS"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="1.100.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/microsoft/vscode
 TERMUX_PKG_GIT_BRANCH="$TERMUX_PKG_VERSION"
 TERMUX_PKG_DEPENDS="electron-for-code-oss, libx11, libxkbfile, libsecret, ripgrep"
@@ -10,6 +11,7 @@ TERMUX_PKG_BUILD_DEPENDS="electron-headers-for-code-oss"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="electron-for-code-oss"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
+TERMUX_PKG_NO_STRIP=true
 TERMUX_PKG_HOSTBUILD=true
 # Chromium doesn't support i686 on Linux.
 TERMUX_PKG_EXCLUDED_ARCHES="i686"
