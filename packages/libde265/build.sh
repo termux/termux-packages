129c56d07e8ffa7a40ab3a29b42acb57c2a75e75
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-21 04:27:09 +0000

    bump(main/libde265): 1.0.16
    Import updated patch from debian.
    https://salsa.debian.org/multimedia-team/libde265/-/blob/f1eb80470340756d3a0de6bf13c48fc2d1e435b2/debian/patches/only_export_decoder_api.patch
    
    Fix the following warning with proper LDFLAGS.
    llvm-readelf: warning: './lib/libde265.a':
    libclang_rt.builtins-aarch64-android.a has an unsupported file type

--- packages/libde265/build.sh
+++ packages/libde265/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="H.265/HEVC video stream decoder library"
 TERMUX_PKG_LICENSE="LGPL-3.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.15"
+TERMUX_PKG_VERSION="1.0.16"
 TERMUX_PKG_SRCURL=https://github.com/strukturag/libde265/releases/download/v$TERMUX_PKG_VERSION/libde265-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=00251986c29d34d3af7117ed05874950c875dd9292d016be29d3b3762666511d
+TERMUX_PKG_SHA256=b92beb6b53c346db9a8fae968d686ab706240099cdd5aff87777362d668b0de7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-sherlock265 --disable-arm --disable-encoder"
@@ -30,5 +30,9 @@ termux_step_pre_configure() {
 	sed -i "s#tools##;s#acceleration-speed##" ${TERMUX_PKG_SRCDIR}/Makefile.am
 	autoreconf -fi
 
-	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	# ld.lld: error: non-exported symbol '__aarch64_ldadd8_acq_rel' in libclang_rt.builtins-aarch64-android.a
+	local _libgcc_file="$($CC -print-libgcc-file-name)"
+	local _libgcc_path="$(dirname $_libgcc_file)"
+	local _libgcc_name="$(basename $_libgcc_file)"
+	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
 }
