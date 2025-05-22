82579257ec04aac28625da4bb0334c48afbec132
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-22 05:19:23 +0000

    bump(x11/xfce4-mailwatch-plugin): 1.4.0
    Replace autotools with meson.

--- x11-packages/xfce4-mailwatch-plugin/build.sh
+++ x11-packages/xfce4-mailwatch-plugin/build.sh
@@ -2,14 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-mailwatch-plugin/s
 TERMUX_PKG_DESCRIPTION="Xfce4 Mailwatch Plugin is a multi-protocol, multi-mailbox mail watcher for the Xfce4 panel."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.3.2"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-mailwatch-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-mailwatch-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=c4783f1533891cd2e0c34066da859864dce45a23caa6015b58cb9fa9d65a7e44
+TERMUX_PKG_VERSION="1.4.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-mailwatch-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-mailwatch-plugin-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5c211025db1096663fa6b8cc41213464a6d71f24e76326499d857ff81ea3861f
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, exo, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libgnutls, libxfce4ui, libxfce4util, pango, xfce4-panel, zlib"
+TERMUX_PKG_DEPENDS="exo, gdk-pixbuf, glib, gtk3, libcairo, libgnutls, libxfce4ui, libxfce4util, xfce4-panel"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-debug
---disable-static
---enable-ssl
---enable-ipv6
+-Dgnutls=enabled
+-Dipv6=enabled
 "
