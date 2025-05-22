60d39e27d7a189e011c13a42d71ce95ce8f4d47e
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-22 06:04:07 +0000

    bump(x11/xfce4-wavelan-plugin): 0.7.0
    Replace autotools with meson.

--- x11-packages/xfce4-wavelan-plugin/build.sh
+++ x11-packages/xfce4-wavelan-plugin/build.sh
@@ -2,13 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-wavelan-plugin/sta
 TERMUX_PKG_DESCRIPTION="wavelan status plugin for the Xfce4 panel"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
-TERMUX_PKG_VERSION="0.6.4"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-wavelan-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-wavelan-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=129c917b40ffa10d96f3d2c0d03f1e8ad8037c79133e9a6436661e37dd7bb3de
+TERMUX_PKG_VERSION="0.7.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-wavelan-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-wavelan-plugin-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5fdce1e1b0d29d0a258dca86bab9d4edcbc12098134b92e00ea1502086a49116
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfce4-panel, zlib"
-TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-debug
---disable-static
-"
+TERMUX_PKG_DEPENDS="glib, gtk3, libxfce4ui, libxfce4util, xfce4-panel"
