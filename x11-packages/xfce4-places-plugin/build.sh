9950e3f012569d1313d03d4ad4015af862a4bd39
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-22 15:40:26 +0000

    bump(x11/xfce4-places-plugin): 1.9.0
    Replace autotools with meson.

--- x11-packages/xfce4-places-plugin/build.sh
+++ x11-packages/xfce4-places-plugin/build.sh
@@ -2,13 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-places-plugin/star
 TERMUX_PKG_DESCRIPTION="This plugin brings much of the functionality of GNOME Places menu to Xfce"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
-TERMUX_PKG_VERSION="1.8.4"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-places-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-places-plugin-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=ba766a5d31580fad043fbd1fd66b811cbda706229473d24a734a590d49ef710e
+TERMUX_PKG_VERSION="1.9.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-places-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-places-plugin-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=76d95687e0bea267465e832eea6266563a18d2219192f9e8af6f88e899262e43
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, exo, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libice, libnotify, libsm, libx11, libxfce4ui, libxfce4util, pango, xfce4-panel, xfconf, zlib"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="exo, gdk-pixbuf, glib, gtk3, libcairo, libnotify, libxfce4ui, libxfce4util, xfce4-panel, xfconf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-debug
---disable-static
+-Dlibnotify=enabled
 "
