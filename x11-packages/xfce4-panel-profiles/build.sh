df24040f32b52fc4b00a8e2ac1c385fae7e2a79b
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-23 07:51:15 +0000

    bump(x11/xfce4-panel-profiles): 1.1.1

--- x11-packages/xfce4-panel-profiles/build.sh
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -2,15 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-panel-profiles/start
 TERMUX_PKG_DESCRIPTION="A simple application to manage Xfce panel layouts."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.15"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION%.*}/xfce4-panel-profiles-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=56cce1a27e88a18a282d568cbc601547a5dd704f0449a75bc284f0171aebaf3b
+TERMUX_PKG_VERSION="1.1.1"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION%.*}/xfce4-panel-profiles-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=0126373a03778bb4894afa151de28d36bfc563ddab96d3bd7c63962350d34ba2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gtk3, libxfce4ui, libxfce4util, pygobject, python, xfce4-panel"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
+	# from configure
 	sed -e s,@prefix@,$TERMUX_PREFIX, Makefile.in.in > Makefile.in
 	sed \
 		-e s,@appname@,xfce4-panel-profiles,g \
@@ -19,4 +20,8 @@ termux_step_configure() {
 		-e s,@docdir@,$TERMUX_PREFIX/share/doc/xfce4-panel-profiles,g \
 		-e s,@python@,$TERMUX_PREFIX/bin/python,g \
 		Makefile.in > Makefile
+	sed \
+		-e s,@VERSION@,$TERMUX_PKG_VERSION,g \
+		-e s,@COPYRIGHT_YEAR@,2025,g \
+	xfce4-panel-profiles.1.in > xfce4-panel-profiles.1
 }
