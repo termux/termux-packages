4d989e3fcf37d52a701ab5efb22f8ae2cd222a7b
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-23 04:44:55 +0000

    bump(x11/gigolo): 0.6.0

--- x11-packages/gigolo/build.sh
+++ x11-packages/gigolo/build.sh
@@ -2,8 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/gigolo/start
 TERMUX_PKG_DESCRIPTION="Gigolo is a frontend to easily manage connections to local and remote filesystems using GIO/GVfs."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.5.4"
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/gigolo/0.5/gigolo-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=29951a16ca48c5350fa862417a253bc45c2762106027c216bb7a56eabdd7f0f6
+TERMUX_PKG_VERSION="0.6.0"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/gigolo/${TERMUX_PKG_VERSION%.*}/gigolo-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=f27dbb51abe8144c1b981f2d820ad1b279c1bc4623d7333b7d4f5f4777eb45ed
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="gtk3"
+TERMUX_PKG_DEPENDS="glib, gtk3"
+
+termux_step_pre_configure() {
+	termux_setup_glib_cross_pkg_config_wrapper
+}
