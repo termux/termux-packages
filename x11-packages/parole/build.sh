471079d0db20b18072f110d6baf951dcf0fc3985
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-23 05:40:31 +0000

    bump(x11/parole): 4.20.0

--- x11-packages/parole/build.sh
+++ x11-packages/parole/build.sh
@@ -2,19 +2,23 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/parole/start
 TERMUX_PKG_DESCRIPTION="Parole is a media player for the Xfce desktop environment, written using the GStreamer framework."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.18.2"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/parole/${TERMUX_PKG_VERSION%.*}/parole-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=6625288b760d38a15c295051ecf66c556fcad21dd1516d6d661c2a582421ee0e
+TERMUX_PKG_VERSION="4.20.0"
+
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/parole/${TERMUX_PKG_VERSION%.*}/parole-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5cf753e670d6518701133eb860d8bceb3a08a496af6a2b7cc67b93320230c983
 TERMUX_PKG_AUTO_UPDATE=true
 # gstreamer all plugins for all support in parole
-TERMUX_PKG_DEPENDS="atk, dbus, dbus-glib, gdk-pixbuf, glib, gst-libav, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gst-plugins-ugly, gstreamer, gtk3, harfbuzz, libcairo, libnotify, libx11, libxfce4ui, libxfce4util, pango, taglib, xfconf, zlib"
-TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_DEPENDS="dbus, dbus-glib, gdk-pixbuf, glib, gst-libav, gst-plugins-bad, gst-plugins-base, gst-plugins-good, gst-plugins-ugly, gstreamer, gtk3, libcairo, libnotify, libx11, libxfce4ui, libxfce4util, taglib, xfconf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-debug
---disable-static
---enable-gtk-doc-html=no
---enable-taglib
---enable-wayland
---enable-x11
+-Dgtk-doc=false
+-Dmpris2-plugin=enabled
+-Dnotify-plugin=enabled
+-Dtaglib=enabled
+-Dtray-plugin=enabled
+-Dwayland=enabled
+-Dx11=enabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_glib_cross_pkg_config_wrapper
+}
