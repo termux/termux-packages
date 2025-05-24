3ae053f49ff6ad03755ddf2e75598cab2a77c4d8
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-24 05:48:20 +0000

    bump(x11/xfce4-screenshooter): 1.11.2

--- x11-packages/xfce4-screenshooter/build.sh
+++ x11-packages/xfce4-screenshooter/build.sh
@@ -2,13 +2,17 @@ TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/apps/xfce4-screenshooter/start
 TERMUX_PKG_DESCRIPTION="The Xfce4-screenshooter is an application that can be used to take snapshots of your desktop screen."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
-TERMUX_PKG_VERSION="1.11.1"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-screenshooter/${TERMUX_PKG_VERSION%.*}/xfce4-screenshooter-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=d94c4a37ac9b26f6d73214bdc254624a4ede4e111bee8d34e689f8f04c37d34d
+TERMUX_PKG_VERSION="1.11.2"
+TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-screenshooter/${TERMUX_PKG_VERSION%.*}/xfce4-screenshooter-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=6ae5bc4823d43e770b3a11700d048d56bdcaafdef37de7deacb8970b55fc1565
 TERMUX_PKG_DEPENDS="atk, exo, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libx11, libxext, libxfce4ui, libxfce4util, libxfixes, libwayland, pango, xfce4-panel, xfconf, zlib"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-ac_cv_prog_HELP2MAN=
+-Dwayland=enabled
+-Dx11=enabled
+-Dxfixes=enabled
 "
+
+termux_step_pre_configure() {
+	termux_setup_wayland_cross_pkg_config_wrapper
+}
