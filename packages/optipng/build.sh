055418f2591f3b60f6b66b6245ca9f69d1950b2f
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-25 03:07:55 +0000

    bump(main/optipng): 7.9.1

--- packages/optipng/build.sh
+++ packages/optipng/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://optipng.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="PNG optimizer that recompresses image files to a smaller size, without losing any information"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.7.8"
+TERMUX_PKG_VERSION="7.9.1"
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-${TERMUX_PKG_VERSION}/optipng-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=25a3bd68481f21502ccaa0f4c13f84dcf6b20338e4c4e8c51f2cefbd8513398c
+TERMUX_PKG_SHA256=c2579be58c2c66dae9d63154edcb3d427fef64cb00ec0aff079c9d156ec46f29
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libpng, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-system-zlib --with-system-libpng --mandir=$TERMUX_PREFIX/share/man"
