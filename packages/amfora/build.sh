1dea136b9abe6fb61ebbe26be5ce8bcd17305e22
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-28 17:45:08 +0000

    fix(main/amfora): Fix building due to module repository change
    cview migrated the repository to codeberg:
    https://codeberg.org/tslocum/cview/commit/242e7c1f1b61a4b3722a1afb45ca1165aefa9a59
    
    Also update amfora source repository url.

--- packages/amfora/build.sh
+++ packages/amfora/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/makeworld-the-better-one/amfora
+TERMUX_PKG_HOMEPAGE=https://github.com/makew0rld/amfora
 TERMUX_PKG_DESCRIPTION="Aims to be the best looking Gemini client"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.10.0"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/makeworld-the-better-one/amfora/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=2
+TERMUX_PKG_SRCURL=https://github.com/makew0rld/amfora/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0bc9964ccefb3ea0d66944231492f66c3b0009ab0040e19cc115d0b4cd9b8078
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
