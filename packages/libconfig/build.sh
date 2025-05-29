617f04caa1b88604d48be480b0f5ed3d9b874e00
Biswapriyo Nath <nathbappai@gmail.com>
2025-05-29 07:07:43 +0000

    bump(main/libconfig): 1.8.1

--- packages/libconfig/build.sh
+++ packages/libconfig/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/hyperrealm/libconfig
 TERMUX_PKG_DESCRIPTION="C/C++ Configuration File Library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.8"
+TERMUX_PKG_VERSION="1.8.1"
 TERMUX_PKG_SRCURL=https://github.com/hyperrealm/libconfig/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=22e13253e652ec583ba0dd5b474bd9c7bd85dc724f2deb0d76a6299c421358ef
+TERMUX_PKG_SHA256=e95798d2992a66ecd547ce3651d7e10642ff2211427c43a7238186ff4c372627
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BREAKS="libconfig-dev"
@@ -13,7 +13,7 @@ TERMUX_PKG_REPLACES="libconfig-dev"
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=11
+	local _SOVERSION=15
 
 	local e=$(sed -En 's/^VERINFO\s*=\s*-version-info\s+([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
 			lib/Makefile.am)
@@ -25,5 +25,9 @@ termux_step_post_get_source() {
 termux_step_pre_configure() {
 	autoreconf -fi
 
-	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	# ld.lld: error: non-exported symbol '__aeabi_d2lz' is referenced by 'libconfig++.so'
+	local _libgcc_file="$($CC -print-libgcc-file-name)"
+	local _libgcc_path="$(dirname $_libgcc_file)"
+	local _libgcc_name="$(basename $_libgcc_file)"
+	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
 }
