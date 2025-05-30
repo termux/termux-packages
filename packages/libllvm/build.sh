cfd67a45bcbf35015212d0c72bf6dd1df349a558
Finagolfin <finagolfin@tuta.io>
2025-05-30 22:09:59 +0530

    bump(libllvm): 20.1.5 to 20.1.6

--- packages/libllvm/build.sh
+++ packages/libllvm/build.sh
@@ -5,8 +5,8 @@ TERMUX_PKG_LICENSE_FILE="llvm/LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@finagolfin"
 # Keep flang version and revision in sync when updating (enforced by check in termux_step_pre_configure).
 LLVM_MAJOR_VERSION=20
-TERMUX_PKG_VERSION=${LLVM_MAJOR_VERSION}.1.5
-TERMUX_PKG_SHA256=a069565cd1c6aee48ee0f36de300635b5781f355d7b3c96a28062d50d575fa3e
+TERMUX_PKG_VERSION=${LLVM_MAJOR_VERSION}.1.6
+TERMUX_PKG_SHA256=5c70549d524284c184fe9fbff862c3d2d7a61b787570611b5a30e5cc345f145e
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-$TERMUX_PKG_VERSION/llvm-project-${TERMUX_PKG_VERSION}.src.tar.xz
 TERMUX_PKG_HOSTBUILD=true
