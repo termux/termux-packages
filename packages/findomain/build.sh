f24513d3ad4ab118950119372f589157952aec10
Chongyun Lee <licy183@termux.dev>
2025-06-01 03:22:11 +0800

    findomain: fix build

--- packages/findomain/build.sh
+++ packages/findomain/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Findomain is the fastest subdomain enumerator and the on
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="9.0.4"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/Findomain/Findomain/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=98c142e2e6ed67726bdea7a1726a54fb6773a8d1ccaa262e008804432af29190
 TERMUX_PKG_AUTO_UPDATE=true
@@ -24,13 +24,27 @@ termux_step_pre_configure() {
 	: "${CARGO_HOME:=$HOME/.cargo}"
 	export CARGO_HOME
 
-	cargo fetch --target "${CARGO_TARGET_NAME}"
+	cargo vendor
+	find ./vendor \
+		-mindepth 1 -maxdepth 1 -type d \
+		! -wholename ./vendor/traitobject \
+		-exec rm -rf '{}' \;
 
-	for d in $CARGO_HOME/registry/src/*/trust-dns-resolver-*; do
-		sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" \
-			$TERMUX_PKG_BUILDER_DIR/trust-dns-resolver.diff \
-			| patch --silent -p1 -d ${d} || :
-	done
+	patch --silent -p1 \
+		-d ./vendor/traitobject/ \
+		< "$TERMUX_PKG_BUILDER_DIR"/traitobject-rust-1.87.diff
+
+	git clone https://github.com/Findomain/trust-dns \
+		-b custombranch \
+		./vendor/trust-dns
+
+	patch --silent -p1 \
+		-d ./vendor/trust-dns/crates/resolver/ \
+		< "$TERMUX_PKG_BUILDER_DIR"/trust-dns-resolver.diff
+
+	patch --silent -p1 \
+		-d "$TERMUX_PKG_SRCDIR" \
+		< "$TERMUX_PKG_BUILDER_DIR"/patch-root-Cargo.diff
 
 	# clash with rust host build
 	unset CFLAGS
