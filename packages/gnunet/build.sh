b80387a530e2661c6182b77acf51f24fd9597d98
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-01 18:19:28 +0000

    bump(main/gnunet): 0.24.2

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gnunet.org
 TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.24.1"
+TERMUX_PKG_VERSION="0.24.2"
 TERMUX_PKG_SRCURL=https://ftpmirror.gnu.org/gnu/gnunet/gnunet-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c4f8f9d25d3a00f80709583b87aa8312d01454090b73a413b43ec7ec7c07ba39
+TERMUX_PKG_SHA256=2e4e4a907d9427f0c3dd4d6795cceaf72ccf397e9dc961f60edbef3006f6af47
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libcurl, libgcrypt, libgnutls, libgpg-error, libidn2, libjansson, libltdl, libmicrohttpd, libsodium, libsqlite, libunistring, zlib"
 
