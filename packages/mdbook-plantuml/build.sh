642de03a457c8e4186d396eed71607727ab6bd92
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-06-02 22:54:17 +0800

    mdbook-plantuml: fix build (#24934)

--- packages/mdbook-plantuml/build.sh
+++ packages/mdbook-plantuml/build.sh
@@ -3,10 +3,31 @@ TERMUX_PKG_DESCRIPTION="mdBook preprocessor to render PlantUML code blocks as im
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.8.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=git+https://github.com/sytsereitsma/mdbook-plantuml
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	: "${CARGO_HOME:=$HOME/.cargo}"
+	export CARGO_HOME
+
+	cargo vendor
+	find ./vendor \
+		-mindepth 1 -maxdepth 1 -type d \
+		! -wholename ./vendor/traitobject \
+		-exec rm -rf '{}' \;
+
+	patch --silent -p1 \
+		-d ./vendor/traitobject/ \
+		< "$TERMUX_PKG_BUILDER_DIR"/traitobject-rust-1.87.diff
+
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo 'traitobject = { path = "./vendor/traitobject" }' >> Cargo.toml
+}
 
 termux_step_make() {
 	termux_setup_rust
