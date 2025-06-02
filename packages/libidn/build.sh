918655a642fa68f82f30674210685551a1d5eb54
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-02 05:50:53 +0000

    bump(main/libidn): 1.43
    Add soverion check and enable auto update.

--- packages/libidn/build.sh
+++ packages/libidn/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libidn/
 TERMUX_PKG_DESCRIPTION="GNU Libidn library, implementation of IETF IDN specifications"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-3.0, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.42"
+TERMUX_PKG_VERSION="1.43"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libidn/libidn-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d6c199dcd806e4fe279360cb4b08349a0d39560ed548ffd1ccadda8cdecb4723
+TERMUX_PKG_SHA256=bdc662c12d041b2539d0e638f3a6e741130cdb33a644ef3496963a443482d164
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libiconv"
 TERMUX_PKG_BREAKS="libidn-dev"
 TERMUX_PKG_REPLACES="libidn-dev"
@@ -12,3 +13,19 @@ TERMUX_PKG_REPLACES="libidn-dev"
 # Remove the idn tool for now, add it as subpackage if desired::
 TERMUX_PKG_RM_AFTER_INSTALL="bin/idn share/man/man1/idn.1 share/emacs"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-ld-version-script"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=12
+
+	local a
+	for a in LT_CURRENT LT_AGE; do
+		local _${a}=$(sed -En 's/^AC_SUBST\('"${a}"',\s*([0-9]+).*/\1/p' \
+				configure.ac)
+	done
+	local v=$(( _LT_CURRENT - _LT_AGE ))
+	if [ ! "${_LT_CURRENT}" ] || [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
