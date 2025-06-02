0391c94a749d40a06c7a75892272b7de19d11390
Chongyun Lee <licy183@termux.dev>
2025-06-02 16:17:34 +0800

    libatomic-ops: switch source
    [skip ci]

--- packages/libatomic-ops/build.sh
+++ packages/libatomic-ops/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/ivmai/libatomic_ops
+TERMUX_PKG_HOMEPAGE=https://github.com/bdwgc/libatomic_ops
 TERMUX_PKG_DESCRIPTION="Provides semi-portable access to hardware-provided atomic memory update operations"
 TERMUX_PKG_LICENSE="GPL-2.0, MIT"
 TERMUX_PKG_LICENSE_FILE="README.md, LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="7.8.2"
-TERMUX_PKG_SRCURL=https://github.com/ivmai/libatomic_ops/releases/download/v${TERMUX_PKG_VERSION}/libatomic_ops-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/bdwgc/libatomic_ops/releases/download/v${TERMUX_PKG_VERSION}/libatomic_ops-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d305207fe207f2b3fb5cb4c019da12b44ce3fcbc593dfd5080d867b1a2419b51
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
