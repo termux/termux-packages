faf3fb52a9f90787fd745e957b01ab1e06b092a8
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-06-03 00:25:48 +0800

    libpcsclite: revbump to rebuild
    To get headers installed

--- packages/libpcsclite/build.sh
+++ packages/libpcsclite/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD 3-Clause, GPL-3.0, BSD 2-Clause, ISC"
 TERMUX_PKG_LICENSE_FILE="COPYING, GPL-3.0.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.3.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/LudovicRousseau/PCSC/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=00b667aa71504ed1d39a48ad377de048c70dbe47229e8c48a3239ab62979c70f
 TERMUX_PKG_DEPENDS="python"
@@ -17,7 +18,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibsystemd=false
 -Dlibudev=false
 "
-
 
 termux_step_create_debscripts() {
 	# "pcscd fails to start if this folder does not exist"
