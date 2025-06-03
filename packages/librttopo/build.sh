c6c568644b72217988c7e5369b5e56582da10f0e
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-06-03 11:36:15 +0800

    librttopo: switch source (#24940)

--- packages/librttopo/build.sh
+++ packages/librttopo/build.sh
@@ -3,11 +3,20 @@ TERMUX_PKG_DESCRIPTION="The RT Topology Library exposes an API to create and man
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://git.osgeo.org/gitea/rttopo/librttopo/archive/librttopo-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=60b49acb493c1ab545116fb0b0d223ee115166874902ad8165eb39e9fd98eaa9
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL=git+https://git.osgeo.org/gitea/rttopo/librttopo
+TERMUX_PKG_GIT_BRANCH="librttopo-$TERMUX_PKG_VERSION"
+TERMUX_PKG_SHA256=98c8a5acbc4db5fbe5ccb03c9577221bda1135c50301f45d67d6f8d2405feb3f
 TERMUX_PKG_DEPENDS="libgeos, proj"
 TERMUX_PKG_GROUPS="science"
+
+termux_step_post_get_source() {
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		echo "$s"
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
 
 termux_step_pre_configure() {
 	./autogen.sh
