167079ea15b794a91aa6f965810fcaa3f644ac11
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-03 05:14:11 +0000

    bump(main/taglib): 2.1
    Add soversion check and enable auto update.

--- packages/taglib/build.sh
+++ packages/taglib/build.sh
@@ -4,9 +4,10 @@ TERMUX_PKG_DESCRIPTION="A Library for reading and editing the meta-data of sever
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING.LGPL, COPYING.MPL"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.2
+TERMUX_PKG_VERSION="2.1"
 TERMUX_PKG_SRCURL=https://github.com/taglib/taglib/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0de288d7fe34ba133199fd8512f19cc1100196826eafcb67a33b224ec3a59737
+TERMUX_PKG_SHA256=95b788b39eaebab41f7e6d1c1d05ceee01a5d1225e4b6d11ed8976e96ba90b0c
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, zlib, utf8cpp"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_BREAKS="taglib-dev"
@@ -18,3 +19,15 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWITH_MP4=ON
 -DWITH_ASF=ON
 -DWITH_DSF=ON"
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=2
+
+	local v=$(sed -En 's/^set\(TAGLIB_SOVERSION_MAJOR\s+([0-9]+).*/\1/p' \
+			CMakeLists.txt)
+	if [ "${v}" != "${_SOVERSION}" ]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
