8c314c298c767bfa005d7196268ada37714c3214
Robert Kirkman <rkirkman@termux.dev>
2025-06-04 12:18:00 -0500

    bump(main/oidn): 2.3.3
    - Context: This package is currently orphaned because @T-Dynamos wanted it for Blender in 2022, so it was added, but appears to have never used it despite it being in the main termux-packages repository because it was failing to build for aarch64: https://github.com/termux/termux-packages/pull/11800#issuecomment-1236346483
      - However, the reason I am bumping it is because I want it for an improved Blender package in TUR, but awkwardly, its package already exists unused in the main repository, so in order to build against it in TUR, I must first edit the main repository so that it propagates into TUR.
    
    - No longer failing to build for aarch64
    
    - `0002-fix-ambiguous-user.patch` is no longer necessary

--- packages/oidn/build.sh
+++ packages/oidn/build.sh
@@ -2,22 +2,20 @@ TERMUX_PKG_HOMEPAGE=https://www.openimagedenoise.org
 TERMUX_PKG_DESCRIPTION="Intel® Open Image Denoise library"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.4.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.3.3"
 TERMUX_PKG_SRCURL=https://github.com/OpenImageDenoise/oidn/releases/download/v$TERMUX_PKG_VERSION/oidn-$TERMUX_PKG_VERSION.src.tar.gz
-TERMUX_PKG_SHA256=3276e252297ebad67a999298d8f0c30cfb221e166b166ae5c955d88b94ad062a
+TERMUX_PKG_SHA256=ccf221535b4007607fb53d3ff5afa74de25413bb8ef5d03d215f46c7cc2f96cf
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libc++, libllvm, libtbb"
-# OIDN supports 64-bit platforms only and won't build on Linux ARM64, see
-# https://github.com/OpenImageDenoise/oidn/issues/125#issuecomment-916479769
-# and https://github.com/OpenImageDenoise/oidn/#prerequisites.
-TERMUX_PKG_EXCLUDED_ARCHES="aarch64, arm, i686"
+# OIDN supports 64-bit platforms only,
+# see https://github.com/OpenImageDenoise/oidn/#prerequisites.
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
-	local ISPC_VERSION=1.18.0
+	local ISPC_VERSION=1.27.0
 	local ISPC_URL=https://github.com/ispc/ispc/releases/download/v$ISPC_VERSION/ispc-v$ISPC_VERSION-linux.tar.gz
 	local ISPC_TARFILE=$TERMUX_PKG_CACHEDIR/$(basename $ISPC_URL)
-	local ISPC_SHA256=6c379bb97962e9de7d24fd48b3f7e647dc42be898e9d187948220268c646b692
+	local ISPC_SHA256=e9c3c653ff3241fce8e2f5c2f26fc1d8e80b8dd73a135bc59627221c00f4d30a
 	termux_download $ISPC_URL $ISPC_TARFILE $ISPC_SHA256
 	if [ ! -e "$TERMUX_PKG_CACHEDIR/.placeholder-ispc-v$ISPC_VERSION" ]; then
 		rm -rf $TERMUX_PKG_CACHEDIR/ispc-v$ISPC_VERSION-linux
