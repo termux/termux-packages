d78d6f95322a0e21fb4608a47664d6c22e4c7304
ArchGuy <52102426+ArchGuyWu@users.noreply.github.com>
2025-06-04 16:25:58 +0800

    Update build.sh

--- packages/dinit/build.sh
+++ packages/dinit/build.sh
@@ -7,8 +7,7 @@ TERMUX_PKG_SRCURL=git+https://github.com/davmac314/dinit.git
 TERMUX_PKG_GIT_BRANCH=r${TERMUX_PKG_VERSION}
 TERMUX_PKG_BUILD_DEPENDS="make, clang, m4, binutils-is-llvm, git"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shutdown"
-termux_step_post_massage="Please add dinitctl shutdown to .bash_logout for proper shutdown services." 
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shutdown" 
 
 termux_step_post_make_install() {
 	mkdir -p $(DESTDIR)$(PREFIX)/etc/profile.d/
@@ -25,7 +24,7 @@ termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh"
 	echo "echo \"Add 'dinitctl shutdown' in .bash_logout\""
 	echo "echo \"to shutdown services properly\""
- 	echo "echo \"dinitctl shutdown\" >> \"\$HOME/.ssh/authorized_keys\""
+ 	echo "echo \"dinitctl shutdown\" >> \"\$HOME/.bash_logout\""
 	echo "exit 0"
 	} > postinst
 	chmod 0700 postinst
