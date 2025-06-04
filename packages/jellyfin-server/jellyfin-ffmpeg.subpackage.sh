f050b2deaa046aeb97e96ec50dc5a03825b8d1fe
NORALDM <stopdigging@example.com>
2025-06-04 20:51:29 +0000

    addpkg(main/jellyfin-server): 10.11.5

---
+++ packages/jellyfin-server/jellyfin-ffmpeg.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DESCRIPTION="FFmpeg for Jellyfin with custom extensions and enhancements"
+TERMUX_SUBPKG_DEPENDS="libchromaprint, fontconfig, freetype, fribidi, harfbuzz, libandroid-glob, libass, libbluray, libbz2, libdav1d, libgmp, libiconv, liblzma, libmp3lame, libopenmpt, libopus, libsrt, libtheora, libvorbis, libvpx, libwebp, libx264, libx265, libxml2, libzimg, ocl-icd, openssl, svt-av1, zlib"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
+TERMUX_SUBPKG_INCLUDE="opt/jellyfin"
