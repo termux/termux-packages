e592cb64c409bfc7e5003cbfcdb9630cff5283ca
Robert Kirkman <rkirkman@termux.dev>
2025-06-06 07:38:19 -0500

    rmpkg(main/gst-plugins-gl-headers): orphaned subpackage
    - Fixes https://github.com/termux/termux-packages/issues/24979
    
    - The package has been orphaned ever since it was introduced in 2022: https://github.com/termux/termux-packages/commit/e96459c7c7d57ef0f8f5a34fe02cca859389a807
    
    - The package no longer conflicts with `mesa`, `libglvnd-dev` or any other package. It contains one file:
    
    ```
    ~ $ dpkg -L gst-plugins-gl-headers
    /.
    /data
    /data/data
    /data/data/com.termux
    /data/data/com.termux/files
    /data/data/com.termux/files/usr
    /data/data/com.termux/files/usr/include
    /data/data/com.termux/files/usr/include/GL
    /data/data/com.termux/files/usr/include/GL/wglext.h
    ```
    
    - This file contains large amounts of Windows-specific code and is not found in any other package except for mingw-related packages and highly generalzed frameworks. It is not expected to be necessary for anything on any Linux distro:
    
    ```
    ~ $ apt-file search wglext.h
    gst-plugins-gl-headers: /data/data/com.termux/files/usr/include/GL/wglext.h
    llvm-mingw-w64-ucrt: /data/data/com.termux/files/usr/opt/llvm-mingw-w64/generic-w64-mingw32/include/GL/wglext.h
    panda3d: /data/data/com.termux/files/usr/include/panda3d/wglext.h
    qb64: /data/data/com.termux/files/usr/share/qb64/internal/c/parts/core/gl_headers/opengl_org_registery/wglext.h
    ```
    
    - This file is not found in any `gst-plugins-*` package of any other Linux distribution:
    
    ```
    tacokoneko@debian:~$ lsb_release -d
    Description:	Debian GNU/Linux 13 (trixie)
    tacokoneko@debian:~$ apt-file search wglext.h
    khronos-api: /usr/include/khronos-api/GL/wglext.h
    mingw-w64-common: /usr/share/mingw-w64/include/GL/wglext.h
    mingw-w64-i686-dev: /usr/i686-w64-mingw32/include/GL/wglext.h
    mingw-w64-ucrt64-dev: /usr/x86_64-w64-mingw32ucrt/include/GL/wglext.h
    mingw-w64-x86-64-dev: /usr/x86_64-w64-mingw32/include/GL/wglext.h
    tacokoneko@debian:~$
    ```

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.26.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f4b9fc0be852fe5f65401d18ae6218e4aea3ff7a3c9f8d265939b9c4704915f7
 TERMUX_PKG_AUTO_UPDATE=true
@@ -19,10 +20,22 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dexamples=disabled
 -Dpango=disabled
 "
+# This file is only relevant to Windows
+# and is not found in any gst-plugins package of other Linux distributions
+TERMUX_PKG_RM_AFTER_INSTALL="
+include/GL/wglext.h
+"
 
 termux_step_pre_configure() {
 	termux_setup_gir
 	termux_setup_glib_cross_pkg_config_wrapper
 
 	LDFLAGS+=" -landroid-shmem"
+}
+
+termux_step_post_massage() {
+	local dir="include/GL"
+	if [[ -d "${TERMUX_PKG_MASSAGEDIR}${TERMUX_PREFIX}/$dir" ]]; then
+		termux_error_exit "$dir should not exist in $TERMUX_PKG_NAME!"
+	fi
 }
