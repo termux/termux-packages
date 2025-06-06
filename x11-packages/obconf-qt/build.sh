bfe26bb00ef4dce83d4b56e119ab2719a9af920e
Robert Kirkman <rkirkman@termux.dev>
2025-06-06 01:03:05 -0500

    fix(x11/qt6-qtbase): only send the `opt/qt6/cross` `QT_HOST_PATH` to cross-compilations of revdeps, not on-device builds
    - Fixes https://github.com/termux/termux-packages/issues/24975
    
    Fixes the build in `TERMUX_ON_DEVICE_BUILD=true` mode of reverse dependencies of qt6 like `tageditor`,
    where `CMAKE_HOST_SYSTEM_NAME` is "`Android`" and the path `$TERMUX_PREFIX/opt/qt6/cross` should never be
    used because it contains GNU/Linux binaries that are sent there by the automatic installation
    of `qt6-*-cross-tools packages` during `termux_step_get_dependencies()` by `scripts/buildorder.py`
    installing the `qt6-*-cross-tools` subpackages of `qt6-*` packages during the builds of reverse dependencies
    of qt6,
    https://github.com/termux/termux-packages/blob/da3717f43930c480bd8dfcf1c002430e43e49013/scripts/buildorder.py#L159
    
    without breaking the builds of anything in `TERMUX_ON_DEVICE_BUILD=false` mode,
    where `CMAKE_HOST_SYSTEM_NAME` is "`Linux`" so the paths set by
    "`-DQT_HOST_PATH=${TERMUX_PREFIX}/opt/qt6/cross`" in `TERMUX_PKG_EXTRA_CONFIGURE_ARGS` of `qt6-qtbase`
    are used here instead.
    
    The patch `do-not-use-cross-tools-paths-ondevice-for-reverse-deps.patch` fixes only the `moc: 1: ELF: not found` part of the error message,
    and the changes to the use of `-DQt6LinguistTools_DIR` fix the `lupdate: 1: Syntax error: word unexpected (expecting ")")` part of the error message.

--- x11-packages/obconf-qt/build.sh
+++ x11-packages/obconf-qt/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="OpenBox window manager configuration tool"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.16.5"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://github.com/lxqt/obconf-qt/releases/download/${TERMUX_PKG_VERSION}/obconf-qt-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=034883680912f7ea24dbd4c28b8ee4cf096774aa588e39431d319d56a924160c
 TERMUX_PKG_REPLACES="obconf"
@@ -11,11 +11,13 @@ TERMUX_PKG_BREAKS="obconf"
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, glib, openbox, liblxqt, hicolor-icon-theme, libxml2"
 TERMUX_PKG_BUILD_DEPENDS="lxqt-build-tools, qt6-qtbase-cross-tools, qt6-qttools-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools
-"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DQt6LinguistTools_DIR=${TERMUX_PREFIX}/opt/qt6/cross/lib/cmake/Qt6LinguistTools"
+	fi
+}
 
 termux_step_post_make_install() {
-	cd "$TERMUX_PREFIX"/bin
-	ln -s obconf-qt obconf
+	ln -sf obconf-qt "$TERMUX_PREFIX"/bin/obconf
 }
