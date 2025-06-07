13324f4a9f2a22e1307b2c5ea83570227e69c36e
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-07 13:37:10 +0000

    bump(main/tree-sitter-query): 0.6.1

--- packages/tree-sitter-query/build.sh
+++ packages/tree-sitter-query/build.sh
@@ -2,12 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-query
 TERMUX_PKG_DESCRIPTION="TS query grammar for tree-sitter"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
-TERMUX_PKG_VERSION="0.6.0"
+TERMUX_PKG_VERSION="0.6.1"
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-query/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0fda401a86ed800be15b7209b9942b7fc2e6907a0a9d1a49580306d82ce744f1
+TERMUX_PKG_SHA256=18e158092789e4391668760e9ad1b5b4135c8f483d29163912d29e5619518d40
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_DEPENDS="tree-sitter-cross-tools"
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" != "true" ]]; then
+		export PATH="$TERMUX_PREFIX"/opt/tree-sitter/cross/bin:$PATH
+	fi
+}
 
 termux_step_post_make_install() {
 	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
