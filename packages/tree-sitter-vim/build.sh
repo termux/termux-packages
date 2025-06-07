c1891dff140d11901890494c832615e62ee7f780
TomIO <tomjo00@web.de>
2025-06-07 23:42:29 +0200

    chore(main/tree-sitter-parsers): rebuild/revbump all parsers to use `tree-sitter-cross-tools` when not building on-device

--- packages/tree-sitter-vim/build.sh
+++ packages/tree-sitter-vim/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="Vimscript grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.6.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-vim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b36080be8f9ec53d6413447a792985b984b6f89a223ff758f1acfd380b469a74
+TERMUX_PKG_BUILD_DEPENDS="tree-sitter-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -12,6 +14,9 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_configure() {
 	# these need to be removed for this one, hell if I know why
 	rm setup.py pyproject.toml
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" != "true" ]]; then
+		export PATH="$TERMUX_PREFIX"/opt/tree-sitter/cross/bin:$PATH
+	fi
 }
 
 termux_step_post_make_install() {
