1b818906920b0f74b6e9822ddf7aebbd70667bc2
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-07 07:10:20 +0000

    bump(main/tree-sitter-lua): 0.4.0

--- packages/tree-sitter-lua/build.sh
+++ packages/tree-sitter-lua/build.sh
@@ -2,13 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-lua
 TERMUX_PKG_DESCRIPTION="Lua grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
-TERMUX_PKG_VERSION="0.3.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.4.0"
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-lua/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a34cc70abfd8d2d4b0fabf01403ea05f848e1a4bc37d8a4bfea7164657b35d31
+TERMUX_PKG_SHA256=b0977aced4a63bb75f26725787e047b8f5f4a092712c840ea7070765d4049559
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" != "true" ]]; then
+		export PATH="$TERMUX_PREFIX"/opt/tree-sitter/cross/bin:$PATH
+	fi
+}
 
 termux_step_post_make_install() {
 	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
