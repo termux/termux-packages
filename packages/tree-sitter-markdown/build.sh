0760494307873602f8e8ed28ae17bb3d70829b30
TomIO <tomjo00@web.de>
2025-06-08 17:17:20 +0200

    fix(main/tree-sitter-markdown): fix typo in parser symlink

--- packages/tree-sitter-markdown/build.sh
+++ packages/tree-sitter-markdown/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Markdown grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.5.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-markdown/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=14c2c948ccf0e9b606eec39b09286c59dddf28307849f71b7ce2b1d1ef06937e
 TERMUX_PKG_BUILD_DEPENDS="tree-sitter-cross-tools"
@@ -24,5 +24,5 @@ termux_step_pre_configure() {
 termux_step_post_make_install() {
 	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
 	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown.so
-	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown-inline.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown-inline.so
+	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown-inline.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown_inline.so
 }
