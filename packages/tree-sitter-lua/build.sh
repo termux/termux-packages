03da3923aeea35018baea0a995c44e63833386d2
TomIO <tomjo00@web.de>
2025-06-09 22:00:25 +0200

    enhance(main/tree-sitter-*): switch to using `termux_setup_treesitter`

--- packages/tree-sitter-lua/build.sh
+++ packages/tree-sitter-lua/build.sh
@@ -3,18 +3,15 @@ TERMUX_PKG_DESCRIPTION="Lua grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.4.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-lua/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b0977aced4a63bb75f26725787e047b8f5f4a092712c840ea7070765d4049559
-TERMUX_PKG_BUILD_DEPENDS="tree-sitter-cross-tools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	if [[ "${TERMUX_ON_DEVICE_BUILD}" != "true" ]]; then
-		export PATH="$TERMUX_PREFIX"/opt/tree-sitter/cross/bin:$PATH
-	fi
+	termux_setup_treesitter
 }
 
 termux_step_post_make_install() {
--- packages/tree-sitter-query/build.sh
+++ packages/tree-sitter-query/build.sh
@@ -3,17 +3,15 @@ TERMUX_PKG_DESCRIPTION="TS query grammar for tree-sitter"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.6.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-query/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=18e158092789e4391668760e9ad1b5b4135c8f483d29163912d29e5619518d40
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_DEPENDS="tree-sitter-cross-tools"
 TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	if [[ "${TERMUX_ON_DEVICE_BUILD}" != "true" ]]; then
-		export PATH="$TERMUX_PREFIX"/opt/tree-sitter/cross/bin:$PATH
-	fi
+	termux_setup_treesitter
 }
 
 termux_step_post_make_install() {
