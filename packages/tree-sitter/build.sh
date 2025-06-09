37a388bfd1e73cecdcec7b07b1828ddd027f6206
TomIO <tomjo00@web.de>
2025-06-09 21:58:57 +0200

    rmpkg(main/tree-sitter-cross-tools): replace with `termux_setup_treesitter`
    script

--- packages/tree-sitter/build.sh
+++ packages/tree-sitter/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="An incremental parsing system for programming tools"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.25.6"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ac6ed919c6d849e8553e246d5cd3fa22661f6c7b6497299264af433f3629957c
 TERMUX_PKG_BREAKS="libtreesitter"
@@ -21,45 +21,27 @@ termux_pkg_auto_update() {
 		return
 	fi
 
-	# Figure out the new SHA256 for the `tree-sitter-cross-tools` release file.
-	local TS_GZ_URL TS_GZFILE NEW_TS_SHA256
-	TS_GZ_URL="https://github.com/tree-sitter/tree-sitter/releases/download/v${latest_release}/tree-sitter-linux-x64.gz"
-	TS_GZFILE="$(mktemp)"
-	curl -sL "$TS_GZ_URL" -o "$TS_GZFILE"
-	NEW_TS_SHA256=$(sha256sum "${TS_GZFILE}" | cut -d' ' -f1)
-
-	local TS_GZ_SHA256=c300ea9f2ca368186ce1308793aaad650c3f6db78225257cbb5be961aeff4038
-
-	if [[ "$NEW_TS_SHA256" != "$TS_GZ_SHA256" ]]; then
-		echo "Need to update SHA256 sum for tree-sitter-host-tools"
-		echo "-$TS_GZ_SHA256"
-		echo "+$NEW_TS_SHA256"
-		sed \
-			-e "s|local TS_GZ_SHA256=${TS_GZ_SHA256}|local TS_GZ_SHA256=${NEW_TS_SHA256}|" \
-			-i "${TERMUX_SCRIPTDIR}/packages/tree-sitter/build.sh"
-	fi
-
-	termux_pkg_upgrade_version "${latest_release}"
-}
-
-termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
 	local _SOVERSION=0.25
 
 	# This blocks auto-updates to an incompatible SO version.
-	if [[ "$TERMUX_PKG_VERSION" != "${_SOVERSION}".* ]]; then
+	if [[ "${latest_release}" != "${_SOVERSION}".* ]]; then
 		termux_error_exit "SOVERSION guard check failed."
 	fi
 
-	local TS_GZ_SHA256=c300ea9f2ca368186ce1308793aaad650c3f6db78225257cbb5be961aeff4038
-	termux_download \
-		https://github.com/tree-sitter/tree-sitter/releases/download/v${TERMUX_PKG_VERSION}/tree-sitter-linux-x64.gz \
-		"$TERMUX_PKG_CACHEDIR"/tree-sitter-linux-x64.gz \
-		"$TS_GZ_SHA256"
-	gunzip -k -v "$TERMUX_PKG_CACHEDIR"/tree-sitter-linux-x64.gz
-	mv -v "$TERMUX_PKG_CACHEDIR"/{tree-sitter-linux-x64,tree-sitter}
-	install -Dm700 -t "$TERMUX_PREFIX"/opt/tree-sitter/cross/bin "$TERMUX_PKG_CACHEDIR"/tree-sitter
+	# Figure out the new SHA256 for the `termux_setup_treesitter` function
+	local TS_BIN_URL TS_TMPFILE NEW_TS_SHA256
+	TS_BIN_URL="https://github.com/tree-sitter/tree-sitter/releases/download/v${latest_release}/tree-sitter-linux-x64.gz"
+	TS_TMPFILE="$(mktemp)"
+	curl -sL "$TS_BIN_URL" -o "$TS_TMPFILE"
+	NEW_TS_SHA256=$(sha256sum "$TS_TMPFILE" | cut -d' ' -f1)
+
+	sed \
+		-e "s|\(^\s*\)local TERMUX_TREE_SITTER_SHA256=[0-9a-f]*|\1local TS_GZ_SHA256=${NEW_TS_SHA256}|" \
+		-i "${TERMUX_SCRIPTDIR}/scripts/build/setup/termux_setup_treesitter.sh"
+
+	termux_pkg_upgrade_version "${latest_release}"
 }
 
 termux_step_pre_configure() {
