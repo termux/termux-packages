bf460af539f2e8e2e9a2f383b353f63d7cf24a42
Jia Yuan Lo <jylo06g@gmail.com>
2025-06-11 16:25:15 +0800

    fix(main/ocl-icd): set correct layer dir
    Also create symlink libOpenCL.so.1 pointing to libOpenCL.so

--- packages/ocl-icd/build.sh
+++ packages/ocl-icd/build.sh
@@ -3,17 +3,24 @@ TERMUX_PKG_DESCRIPTION="OpenCL ICD Loader"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.3.3"
-TERMUX_PKG_SRCURL=https://github.com/OCL-dev/ocl-icd/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/OCL-dev/ocl-icd/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8cd8e8e129db3081a64090fc1252bec39dc88cdb7b3f929315e014b75069bd9d
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--enable-custom-layerdir=${TERMUX_PREFIX}/etc/OpenCL/layers
 --enable-custom-vendordir=${TERMUX_PREFIX}/etc/OpenCL/vendors
 --enable-official-khronos-headers
 "
 
 termux_step_pre_configure() {
 	./bootstrap
+}
+
+termux_step_post_make_install() {
+	mkdir -p "${TERMUX_PREFIX}/lib"
+	ln -fs libOpenCL.so "${TERMUX_PREFIX}/lib/libOpenCL.so.1"
 }
 
 # https://www.khronos.org/registry/OpenCL/specs/2.2/html/OpenCL_ICD_Installation.html
