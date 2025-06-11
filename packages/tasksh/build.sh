be4555b01c6352e0f5bf9a92c5a88cce04287cb2
Yaksh Bariya <yakshbari4@gmail.com>
2025-06-11 16:45:46 +0530

    fix(main/tasksh): builds with CMake 4.x

--- packages/tasksh/build.sh
+++ packages/tasksh/build.sh
@@ -3,10 +3,13 @@ TERMUX_PKG_DESCRIPTION="Shell command wrapping Taskwarrior commands"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2.0
-TERMUX_PKG_REVISION=9
+TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/taskshell/releases/download/v${TERMUX_PKG_VERSION}/tasksh-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6e42f949bfd7fbdde4870af0e7b923114cc96c4344f82d9d924e984629e21ffd
 TERMUX_PKG_DEPENDS="libc++, readline, taskwarrior, libandroid-glob"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_POLICY_VERSION_MINIMUM=3.5
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
