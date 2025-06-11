9804c4261c11e86f8fe57c8ab8b34f8b711b2b60
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-11 14:01:42 +0000

    fix(main/libhangul): Fix version check for auto update
    %ci:no-build

--- packages/libhangul/build.sh
+++ packages/libhangul/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION="0.2.0"
 TERMUX_PKG_SRCURL=https://github.com/libhangul/libhangul/releases/download/libhangul-${TERMUX_PKG_VERSION}/libhangul-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ea04e6a0cf4840a2a3b5641c1761068c78691036db839d0838f4e7a6553a5120
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP="s/libhangul-//"
 TERMUX_PKG_DEPENDS="libandroid-glob, libexpat, libiconv"
 
 termux_step_post_get_source() {
