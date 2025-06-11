dc94d24ee0179448816ffc29aa6b06f78cb4bb78
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-11 05:17:12 +0000

    rebuild(x11/fcitx5-hangul): link with libhangul 0.2.0

--- x11-packages/fcitx5-hangul/build.sh
+++ x11-packages/fcitx5-hangul/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="libhangul input method support for fcitx5"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.1.6"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/fcitx/fcitx5-hangul/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=320ddd9f5832b94f0ae5edd914de25ad63c8c3af176d6d00ae4e130341610970
 TERMUX_PKG_DEPENDS="fcitx5, libc++, libhangul"
