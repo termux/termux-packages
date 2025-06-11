fbb79e4e57cd8208de6bd4d99fe94771b0f03362
Yaksh Bariya <yakshbari4@gmail.com>
2025-06-11 16:47:06 +0530

    fix(main/libforestdb): builds with CMake 4.x

--- packages/libforestdb/build.sh
+++ packages/libforestdb/build.sh
@@ -3,11 +3,15 @@ TERMUX_PKG_DESCRIPTION="A key-value storage engine"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/couchbase/forestdb/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=52463e4e3bd94ff70503b8a278ec0304c13acb6862e5d5fd3d2b3f05e60b7aa0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="libsnappy"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_POLICY_VERSION_MINIMUM=3.5
+"
 
 termux_step_post_configure() {
 	if [ "$TERMUX_CMAKE_BUILD" == "Ninja" ]; then
