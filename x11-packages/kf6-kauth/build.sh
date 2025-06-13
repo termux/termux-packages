3ccdc8a32b6b32b983fe90096164a06c643a073b
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-13 18:55:59 +0000

    bump(x11/kf6-kauth): 6.15.0

--- x11-packages/kf6-kauth/build.sh
+++ x11-packages/kf6-kauth/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.kde.org/
 TERMUX_PKG_DESCRIPTION="Framework which lets applications perform actions as a privileged user (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.14.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="6.15.0"
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kauth-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=f5a16e489f26b5741d3f11bf6b203fd99a84ecaae798eb6b0eaa0d71c88f6645
+TERMUX_PKG_SHA256=19c7b598a780ba5befd30f25de694536811a46a5622c4a2c28892f2f3ba5a617
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, kf6-kcoreaddons (>= ${_KF6_MINOR_VERSION})"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
