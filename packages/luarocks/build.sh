d274421699e331b0716c99938cb002690fc88e28
tqfx <tqfx@tqfx.org>
2025-06-13 12:43:59 +0800

    bump(main/luarocks): 3.12.1

--- packages/luarocks/build.sh
+++ packages/luarocks/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://luarocks.org/
 TERMUX_PKG_DESCRIPTION="Deployment and management system for Lua modules"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.11.1"
+TERMUX_PKG_VERSION="3.12.1"
 TERMUX_PKG_SRCURL=https://luarocks.org/releases/luarocks-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c3fb3d960dffb2b2fe9de7e3cb004dc4d0b34bb3d342578af84f84325c669102
+TERMUX_PKG_SHA256=f56b85a2a7a481f0321845807b79a05237860b04e4a9d186da632770029b3290
 TERMUX_PKG_AUTO_UPDATE=true
 __LUA_VERSION=5.1 # Lua version against which it will be built.
 # Do not use varible here since buildorder.py do not evaluate bash before reading.
