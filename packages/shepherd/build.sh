4b902ab83c46087c59be28b740b0475e28841d6d
ArchGuy <52102426+ArchGuyWu@users.noreply.github.com>
2025-06-13 11:10:46 +0800

    addpkg(main/shepherd): 1.0.5

---
+++ packages/shepherd/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/shepherd/
+TERMUX_PKG_DESCRIPTION='The Shepherd—extensible service manager'
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.0.5
+TERMUX_PKG_SRCURL=https://codeberg.org/shepherd/shepherd/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f09f0dab1fd0cd11988e5069be96eb3601182e94db7fed8cf328f99de506306c
+TERMUX_PKG_DEPENDS="guile guile-fibers"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-gzip=$TERMUX_PREFIX/bin/gzip --with-zstd=$TERMUX_PREFIX/bin/zstd"
+TERMUX_PKG_RM_AFTER_INSTALL="
+bin/reboot
+bin/halt
+bin/shutdown
+share/man/man8/halt.8
+share/man/man8/reboot.8
+"
+
+termux_step_pre_configure() {
+	autoreconf -vif
+	export GUILE_AUTO_COMPILE=0
+}
