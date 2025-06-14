59006fc23f11ad3e338b627b8f8afd1fd4486740
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-14 07:11:20 +0000

    bump(x11/kf6-kwallet): 6.15.0

--- x11-packages/kf6-kwallet/build.sh
+++ x11-packages/kf6-kwallet/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Secure and unified container for user passwords'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.14.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="6.15.0"
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kwallet-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=ef482be2d5211f711d578aa1d1f37f71e641a6a52fd37e11327e19915fd42127
+TERMUX_PKG_SHA256=fdbac44c2525b12e24b3ba5971912a2ffd91fdfea79c3a124bce89621735893c
 TERMUX_PKG_DEPENDS="gpgme, gpgmepp, kf6-kcolorscheme (>= ${_KF6_MINOR_VERSION}), kf6-kconfig (>= ${_KF6_MINOR_VERSION}), kf6-kcoreaddons (>= ${_KF6_MINOR_VERSION}), kf6-kcrash (>= ${_KF6_MINOR_VERSION}), kf6-kdbusaddons (>= ${_KF6_MINOR_VERSION}), kf6-ki18n (>= ${_KF6_MINOR_VERSION}), kf6-knotifications (>= ${_KF6_MINOR_VERSION}), kf6-kwidgetsaddons (>= ${_KF6_MINOR_VERSION}), kf6-kwindowsystem (>= ${_KF6_MINOR_VERSION}), libc++, libgcrypt, libsecret, qca, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), kf6-kservice (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
 # TERMUX_PKG_RECOMMENDS="kwalletmanager"
