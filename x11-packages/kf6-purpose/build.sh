6f6aa18d9bac04f138291b6ebac93a1ae08fdcbf
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-14 06:29:10 +0000

    bump(x11/kf6-purpose): 6.15.0

--- x11-packages/kf6-purpose/build.sh
+++ x11-packages/kf6-purpose/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Framework for providing abstractions to get the developers purposes fulfilled'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.14.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="6.15.0"
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/purpose-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=dd9f23baf4c4a44f1c71f7d3b0bfe7fbfbcdda2002d74d4b49cd84631ac899e9
+TERMUX_PKG_SHA256=d1c0c94a6a6ca11e2dc447431d005e11a73dec33692bef619b0b97bb525fc49e
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${_KF6_MINOR_VERSION}), kf6-kcoreaddons (>= ${_KF6_MINOR_VERSION}), kf6-ki18n (>= ${_KF6_MINOR_VERSION}), kf6-kio (>= ${_KF6_MINOR_VERSION}), kf6-knotifications (>= ${_KF6_MINOR_VERSION}), kf6-kservice (>= ${_KF6_MINOR_VERSION}), libc++, qt6-qtbase, qt6-qtdeclarative"
 # kaccounts-integration, libaccounts-qt, accounts-qml-module, kcmutils can be added to TERMUX_PKG_DEPENDS when available
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), kf6-kirigami (>= ${_KF6_MINOR_VERSION}), intltool"
