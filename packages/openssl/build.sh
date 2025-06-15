2bbb6b1d828bc733b1ae8f4b0d7bf420c2e46db0
TomIO <tomjo00@web.de>
2025-06-15 14:58:10 +0200

    bump(main/openssl): 3.5.0 - Build script cleanup - Set a maintainer

--- packages/openssl/build.sh
+++ packages/openssl/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.openssl.org/
 TERMUX_PKG_DESCRIPTION="Library implementing the SSL and TLS protocols as well as general purpose cryptography functions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:3.4.1
+TERMUX_PKG_VERSION=1:3.5.0
 TERMUX_PKG_SRCURL=https://github.com/openssl/openssl/releases/download/openssl-${TERMUX_PKG_VERSION:2}/openssl-${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3
+TERMUX_PKG_SHA256=344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="ca-certificates, zlib"
 TERMUX_PKG_CONFFILES="etc/tls/openssl.cnf"
@@ -17,21 +17,26 @@ TERMUX_PKG_REPLACES="openssl-tool (<< 1.1.1b-1), openssl-dev"
 termux_step_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
-	if $TERMUX_ON_DEVICE_BUILD; then
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == 'true' ]]; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
 
 	CFLAGS+=" -DNO_SYSLOG"
 
-	perl -p -i -e "s@TERMUX_CFLAGS@$CFLAGS@g" Configure
-	rm -Rf $TERMUX_PREFIX/lib/libcrypto.* $TERMUX_PREFIX/lib/libssl.*
-	test $TERMUX_ARCH = "arm" && TERMUX_OPENSSL_PLATFORM="android-arm"
-	test $TERMUX_ARCH = "aarch64" && TERMUX_OPENSSL_PLATFORM="android-arm64"
-	test $TERMUX_ARCH = "i686" && TERMUX_OPENSSL_PLATFORM="android-x86"
-	test $TERMUX_ARCH = "x86_64" && TERMUX_OPENSSL_PLATFORM="android-x86_64"
-	./Configure $TERMUX_OPENSSL_PLATFORM \
-		--prefix=$TERMUX_PREFIX \
-		--openssldir=$TERMUX_PREFIX/etc/tls \
+	sed -i "s@TERMUX_CFLAGS@$CFLAGS@g" Configure
+	rm -rf "$TERMUX_PREFIX/lib"/libcrypto.* "$TERMUX_PREFIX/lib"/libssl.*
+
+	local TERMUX_OPENSSL_PLATFORM="android-${TERMUX_ARCH}"
+	case "$TERMUX_ARCH" in
+		"arm"|"x86_64");;
+		"aarch64") TERMUX_OPENSSL_PLATFORM="android-arm64";;
+		"i686") TERMUX_OPENSSL_PLATFORM="android-x86";;
+		*) termux_error_exit "Unsupported architecture: '$TERMUX_ARCH'"
+	esac
+
+	./Configure "$TERMUX_OPENSSL_PLATFORM" \
+		--prefix="$TERMUX_PREFIX" \
+		--openssldir="$TERMUX_PREFIX/etc/tls" \
 		shared \
 		zlib-dynamic \
 		no-ssl \
@@ -43,19 +48,19 @@ termux_step_configure() {
 
 termux_step_make() {
 	make depend
-	make -j $TERMUX_PKG_MAKE_PROCESSES all
+	make -j"$TERMUX_PKG_MAKE_PROCESSES" all
 }
 
 termux_step_make_install() {
 	# "install_sw" instead of "install" to not install man pages:
-	make -j 1 install_sw MANDIR=$TERMUX_PREFIX/share/man MANSUFFIX=.ssl
+	make -j1 install_sw MANDIR="$TERMUX_PREFIX/share/man" MANSUFFIX=.ssl
 
-	mkdir -p $TERMUX_PREFIX/etc/tls/
+	mkdir -p "$TERMUX_PREFIX/etc/tls/"
 
-	cp apps/openssl.cnf $TERMUX_PREFIX/etc/tls/openssl.cnf
+	cp apps/openssl.cnf "$TERMUX_PREFIX/etc/tls/openssl.cnf"
 
 	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
-		$TERMUX_PKG_BUILDER_DIR/add-trusted-certificate \
-		> $TERMUX_PREFIX/bin/add-trusted-certificate
-	chmod 700 $TERMUX_PREFIX/bin/add-trusted-certificate
+		"$TERMUX_PKG_BUILDER_DIR/add-trusted-certificate" \
+		> "$TERMUX_PREFIX/bin/add-trusted-certificate"
+	chmod 700 "$TERMUX_PREFIX/bin/add-trusted-certificate"
 }
