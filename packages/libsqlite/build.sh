c0c0d0e3a4d444c8f40f980ae82546506d4a3783
TomIO <tom@termux.dev>
2025-06-17 20:50:32 +0200

    bump(main/libsqlite): 3.50.1

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.sqlite.org
 TERMUX_PKG_DESCRIPTION="Library implementing a self-contained and transactional SQL database engine"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.49.2"
+TERMUX_PKG_VERSION="3.50.1"
 _SQLITE_YEAR=2025
-TERMUX_PKG_SRCURL=https://www.sqlite.org/${_SQLITE_YEAR}/sqlite-autoconf-$(sed 's/\./''/; s/\./0/' <<< "$TERMUX_PKG_VERSION")00.tar.gz
-TERMUX_PKG_SHA256=5c6d8697e8a32a1512a9be5ad2b2e7a891241c334f56f8b0fb4fc6051e1652e8
+TERMUX_PKG_SRCURL=https://www.sqlite.org/${_SQLITE_YEAR}/sqlite-src-$(sed 's/\./''/; s/\./0/' <<< "$TERMUX_PKG_VERSION")00.zip
+TERMUX_PKG_SHA256=9090597773c60a49caebb3c1ac57db626fac4d97cb51890815a8b529a4d9c3dc
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_DEPENDS="tcl"
@@ -18,11 +18,15 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-readline
 --enable-rtree
 --enable-session
+--with-tcl=$TERMUX_PREFIX/lib
+--with-tclsh=$(command -v tclsh)
 "
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -Werror -DSQLITE_ENABLE_DBSTAT_VTAB=1 -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1"
 	LDFLAGS+=" -lm"
+	export TCL_CONFIG_SH="$TERMUX_PREFIX/lib/tclConfig.sh"
+	export TCLLIBDIR="$TERMUX_PREFIX/lib"
 }
 
 # See: https://github.com/termux/termux-packages/issues/23268#issuecomment-2685308408
@@ -34,14 +38,15 @@ termux_step_configure() {
 
 termux_step_make_install() {
 	make install INSTALL.strip=/usr/bin/install
-}
+	mkdir -p "$TERMUX_PKG_TMPDIR/libsqlite${TERMUX_PKG_VERSION}"
+	make tclextension-install DESTDIR="$TERMUX_PKG_TMPDIR/libsqlite${TERMUX_PKG_VERSION}" OPTS="-lm"
 
-termux_step_post_make_install() {
-	echo -e "termux - building libsqlite-tcl for arch ${TERMUX_ARCH}..."
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --with-tcl=${TERMUX_PREFIX}/lib --with-system-sqlite"
-	TERMUX_PKG_SRCDIR+="/tea"
-	rm -rf "$TERMUX_PKG_TMPDIR/config-scripts"
-	termux_step_configure_autotools
-	termux_step_make
-	termux_step_make_install
+	# Move the TCL extension files into their proper place
+	find "$TERMUX_PKG_TMPDIR/libsqlite${TERMUX_PKG_VERSION}" -name "libsqlite${TERMUX_PKG_VERSION}.so" \
+		-exec install -vDm700 "{}" "${TERMUX_PREFIX}/lib/sqlite3/libtclsqlite3.so" \;
+	find "$TERMUX_PKG_TMPDIR/libsqlite${TERMUX_PKG_VERSION}" -name pkgIndex.tcl \
+		-exec install -vDm600 "{}" "${TERMUX_PREFIX}/lib/sqlite3/pkgIndex.tcl" \;
+
+	# Remove duplicates
+	rm "$TERMUX_PREFIX/lib"/{pkgIndex.tcl,libtclsqlite3.so}
 }
--- packages/libsqlite/libsqlite-tcl.subpackage.sh
+++ packages/libsqlite/libsqlite-tcl.subpackage.sh
@@ -1,4 +1,4 @@
 TERMUX_SUBPKG_DESCRIPTION="tcl bindings for SQLite"
 TERMUX_SUBPKG_DEPENDS="tcl"
 TERMUX_SUBPKG_BREAKS="sqlcipher (<< 4.4.2-1), tcl (<< 8.6.10-4)"
-TERMUX_SUBPKG_INCLUDE="lib/sqlite*/* share/man/mann/sqlite3.n.gz"
+TERMUX_SUBPKG_INCLUDE="lib/sqlite3/libtclsqlite3.so lib/sqlite3/pkgIndex.tcl"
