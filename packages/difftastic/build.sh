734fda049b2ef965bea72a52d2fda399fa349b04
Juhan Ahmed <azurarobo280@gmail.com>
2025-06-17 14:40:09 +0600

    enhance(main/difftastic): add man page

--- packages/difftastic/build.sh
+++ packages/difftastic/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.64.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/Wilfred/difftastic/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=54c7c93309ff9a2cbe87153ac1d16e80bacac4042c80f6b7206e9b71a6f10d0b
 TERMUX_PKG_AUTO_UPDATE=true
@@ -11,3 +12,7 @@ TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_IN_SRC=true
 # needed for MIME database (optional in upstream)
 TERMUX_PKG_RECOMMENDS="file"
+
+termux_step_post_make_install() {
+	install -Dm644 -t "$TERMUX_PREFIX/share/man/man1/" difft.1
+}
