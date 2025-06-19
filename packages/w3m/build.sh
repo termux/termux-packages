aed1d12da845b13c60a0977c538406799e4f4114
TomIO <tom@termux.dev>
2025-06-20 00:06:30 +0200

    enhance(main/w3m): use the user's selected default `editor`

--- packages/w3m/build.sh
+++ packages/w3m/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://w3m.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Text based Web browser and pager"
-TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.5.3
 _MINOR_VERSION=20230121
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.${_MINOR_VERSION}
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 # The upstream w3m project is dead, but every linux distribution uses
 # this maintained fork in debian:
 TERMUX_PKG_SRCURL=https://github.com/tats/w3m/archive/v${_MAJOR_VERSION}+git${_MINOR_VERSION}.tar.gz
@@ -24,7 +23,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_setpgrp_void=yes ac_cv_func_bcopy=ye
 #Overwrite the default /usr/bin/firefox with termux-open-url as default external browser. That way, pressing "M" on a URL will open a link in Androids default Browser.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-browser=termux-open-url"
 #Overwrite the default editor to just vi, as the default was /usr/bin/vi.
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-editor=nano"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-editor=editor"
 # Build w3mimg with X11/imlib2.
 # w3mimgdisplay is in w3m-img subpackage.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-image=x11 --with-imagelib=imlib2"
