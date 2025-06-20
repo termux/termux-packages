0e47e3842d14079b0a278ae1ac11ae377a9a86f2
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-20 03:57:27 +0000

    bump(main/ninja): 1.13.0
    Remove src-util.cc.patch which was added in upstream.
    https://github.com/ninja-build/ninja/commit/0fefeb3c7ced5ca211154789a2935b3ae27b70f7

--- packages/ninja/build.sh
+++ packages/ninja/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://ninja-build.org
 TERMUX_PKG_DESCRIPTION="A small build system with a focus on speed"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.12.1"
+TERMUX_PKG_VERSION="1.13.0"
 TERMUX_PKG_SRCURL=https://github.com/ninja-build/ninja/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=821bdff48a3f683bc4bb3b6f0b5fe7b2d647cf65d52aeb63328c91a6c6df285a
+TERMUX_PKG_SHA256=f08641d00099a9e40d44ec0146f841c472ae58b7e6dd517bee3945cfd923cedf
 TERMUX_PKG_DEPENDS="libandroid-spawn, libc++"
 TERMUX_PKG_AUTO_UPDATE=true
 
