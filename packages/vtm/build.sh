88b09533794dc7476553272b009e6a0590c2fb08
Biswapriyo Nath <nathbappai@gmail.com>
2025-06-23 18:45:08 +0000

    bump(main/vtm): 2025.06.18
    Update source repository url.

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://vtm.netxs.online/
+TERMUX_PKG_HOMEPAGE=https://github.com/directvt/vtm
 TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-party session sharing"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2025.06.15"
-TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bf8992c4f91b54f949b023bf4a0bf2396ad89682ff2130de5a067f56f0fce19c
+TERMUX_PKG_VERSION="2025.06.18"
+TERMUX_PKG_SRCURL=https://github.com/directvt/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f155cc88d4a295c18e34bc48ff6bc3f874fb075c9dc8fdd53c92a24c0a658dfb
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_BUILD_IN_SRC=true
