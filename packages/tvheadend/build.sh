ac15f2cab2d014101f9c0764634cb3fff022247c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-06-24 19:30:44 +0800

    tvheadend: revbump to rebuild (#25161)

--- packages/tvheadend/build.sh
+++ packages/tvheadend/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="TV streaming server for Linux and Android supporting DVB
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION=4.2.8
-TERMUX_PKG_REVISION=10
+TERMUX_PKG_REVISION=11
 TERMUX_PKG_SRCURL="https://github.com/tvheadend/tvheadend/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=1aef889373d5fad2a7bd2f139156d4d5e34a64b6d38b87b868a2df415f01f7ad
 TERMUX_PKG_AUTO_UPDATE=true
@@ -19,6 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-hdhomerun_static
 --disable-ffmpeg_static
 --disable-avahi
+--nowerror
 "
 
 termux_step_pre_configure() {
