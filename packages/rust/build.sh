860f642e97189a6723a5034b2a9c06905029f415
Jia Yuan Lo <jylo06g@gmail.com>
2025-06-27 09:20:04 +0800

    bump(main/rust): 1.88.0

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_HOMEPAGE=https://www.rust-lang.org/
 TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed and concurrency"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.87.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.88.0"
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-${TERMUX_PKG_VERSION}-src.tar.xz
-TERMUX_PKG_SHA256=8623b8651893e8c6aebfa45b6a90645a4f652f7b18189a0992a90d11ac2631f4
+TERMUX_PKG_SHA256=0c1dcbb4f762513d021e1a282c0ac58c0a423642b3a6bf581cafb5414df4193e
 _LLVM_MAJOR_VERSION=$(. $TERMUX_SCRIPTDIR/packages/libllvm/build.sh; echo $LLVM_MAJOR_VERSION)
 _LLVM_MAJOR_VERSION_NEXT=$((_LLVM_MAJOR_VERSION + 1))
 _LZMA_VERSION=$(. $TERMUX_SCRIPTDIR/packages/liblzma/build.sh; echo $TERMUX_PKG_VERSION)
@@ -122,7 +121,7 @@ termux_step_configure() {
 	# like 30 to 40 + minutes ... so lets get it right
 
 	# upstream tests build using versions N and N-1
-	local BOOTSTRAP_VERSION=1.86.0
+	local BOOTSTRAP_VERSION=1.88.0
 	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
 		if ! rustup install "${BOOTSTRAP_VERSION}"; then
 			echo "WARN: ${BOOTSTRAP_VERSION} is unavailable, fallback to stable version!"
