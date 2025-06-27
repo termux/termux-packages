731e036570a1883f95fcf0e4a2ce0b9a2a192c00
TomIO <tom@termux.dev>
2025-06-27 18:38:30 +0200

    chore(packages/zile): adopt `.alternatives` system

--- packages/zile/build.sh
+++ packages/zile/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lightweight clone of the Emacs text editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.6.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/zile/zile-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d5d44b85cb490643d0707e1a2186f3a32998c2f6eabaa9481479b65caeee57c0
 TERMUX_PKG_AUTO_UPDATE=true
@@ -17,28 +18,7 @@ termux_step_post_configure() {
 	# zile uses help2man to build the zile.1 man page, which would require
 	# a host build.
 	sed 's|@docdir@|$PREFIX/share/doc/zile|g' \
-		$TERMUX_PKG_SRCDIR/doc/zile.1.in \
-		> $TERMUX_PKG_BUILDDIR/doc/zile.1
-	touch -d "next hour" $TERMUX_PKG_BUILDDIR/doc/zile.1*
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" = "configure" ] || [ "\$1" = "abort-upgrade" ]; then
-		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-			update-alternatives --install \
-				$TERMUX_PREFIX/bin/editor editor $TERMUX_PREFIX/bin/zile 35
-		fi
-	fi
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" != "upgrade" ]; then
-		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-			update-alternatives --remove editor $TERMUX_PREFIX/bin/zile
-		fi
-	fi
-	EOF
+		"$TERMUX_PKG_SRCDIR/doc/zile.1.in" \
+		> "$TERMUX_PKG_BUILDDIR/doc/zile.1"
+	touch -d "next hour" "$TERMUX_PKG_BUILDDIR/doc/zile.1"*
 }
