c28b6338e64a869c8ec8fdf6bb70b9f50caaf41d
TomIO <tom@termux.dev>
2025-06-27 18:38:29 +0200

    chore(packages/jupp): adopt `.alternatives` system

--- packages/jupp/build.sh
+++ packages/jupp/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="User friendly full screen text editor"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.1jupp41
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://www.mirbsd.org/MirOS/dist/jupp/joe-${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_SHA256=7bb8ea8af519befefff93ec3c9e32108d7f2b83216c9bc7b01aef5098861c82f
 TERMUX_PKG_DEPENDS="ncurses"
@@ -16,26 +17,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_post_get_source() {
-	chmod +x $TERMUX_PKG_SRCDIR/configure
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" = "configure" ] || [ "\$1" = "abort-upgrade" ]; then
-		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-			update-alternatives --install \
-				$TERMUX_PREFIX/bin/editor editor $TERMUX_PREFIX/bin/jupp 10
-		fi
-	fi
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" != "upgrade" ]; then
-		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-			update-alternatives --remove editor $TERMUX_PREFIX/bin/jupp
-		fi
-	fi
-	EOF
+	chmod +x "$TERMUX_PKG_SRCDIR/configure"
 }
