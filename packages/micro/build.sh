9a8d1e3df5ab4d8545879c43dda454cde321532a
TomIO <tom@termux.dev>
2025-06-27 18:38:29 +0200

    chore(packages/micro): adopt `.alternatives` system

--- packages/micro/build.sh
+++ packages/micro/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Modern and intuitive terminal-based text editor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.14"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_SRCURL=git+https://github.com/zyedidia/micro
@@ -18,33 +18,12 @@ termux_step_make_install() {
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	local MICRO_SRC=$GOPATH/src/github.com/zyedidia/micro
 
-	cd $TERMUX_PKG_SRCDIR
-	mkdir -p $MICRO_SRC
-	cp -R . $MICRO_SRC
+	cd "$TERMUX_PKG_SRCDIR"
+	mkdir -p "$MICRO_SRC"
+	cp -R . "$MICRO_SRC"
 
-	cd $MICRO_SRC
+	cd "$MICRO_SRC"
 	sed -zEi 's/VERSION = \$\(.+\\\n.+\)/VERSION = '"$TERMUX_PKG_VERSION"'/gm' Makefile
 	make build
-	mv micro $TERMUX_PREFIX/bin/micro
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" = "configure" ] || [ "\$1" = "abort-upgrade" ]; then
-		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-			update-alternatives --install \
-				$TERMUX_PREFIX/bin/editor editor $TERMUX_PREFIX/bin/micro 25
-		fi
-	fi
-	EOF
-
-	cat <<- EOF > ./prerm
-	#!$TERMUX_PREFIX/bin/sh
-	if [ "$TERMUX_PACKAGE_FORMAT" = "pacman" ] || [ "\$1" != "upgrade" ]; then
-		if [ -x "$TERMUX_PREFIX/bin/update-alternatives" ]; then
-			update-alternatives --remove editor $TERMUX_PREFIX/bin/micro
-		fi
-	fi
-	EOF
+	mv micro "$TERMUX_PREFIX/bin/micro"
 }
