b25a30031bc42559c3b5c09a7f638586039e6029
Maxython <mixython@gmail.com>
2025-07-01 01:36:56 +0300

    Recompiling packages with debpython
    - packages/borgbackup
    - packages/electrum
    - packages/hash-slinger
    - packages/lv2
    - packages/manim
    - packages/nala
    - packages/python-cryptography
    - packages/python-tldp
    - packages/python-xlib
    - packages/rdiff-backup
    - packages/rdircd
    - packages/sabnzbd
    - packages/srt2vobsub
    - packages/termux-gui-package
    - packages/termux-gui-pm
    - packages/ytui-music
    - x11-packages/orca
    - x11-packages/pyqt5
    - packages/matplotlib
    - packages/python-onnxruntime
    - packages/python-torch
    - packages/python-torchvision
    - packages/python-yt-dlp
    - x11-packages/xfce4-panel-profiles
    - disabled-packages/python-pandas
    
    Co-authored-by: @robertkirkman

--- packages/python-onnxruntime/build.sh
+++ packages/python-onnxruntime/build.sh
@@ -3,11 +3,13 @@ TERMUX_PKG_DESCRIPTION="Cross-platform, high performance ML inferencing and trai
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.22.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/microsoft/onnxruntime
 TERMUX_PKG_DEPENDS="abseil-cpp, libc++, protobuf, libre2, python"
 TERMUX_PKG_BUILD_DEPENDS="python-numpy"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, build, packaging"
+TERMUX_PKG_PYTHON_TARGET_DEPS="onnxruntime"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
@@ -28,7 +30,7 @@ termux_step_pre_configure() {
 
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DPYTHON_EXECUTABLE=$(command -v python3)"
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DONNX_CUSTOM_PROTOC_EXECUTABLE=$(command -v protoc)"
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DPython_NumPy_INCLUDE_DIR=$TERMUX_PREFIX/lib/python$TERMUX_PYTHON_VERSION/site-packages/numpy/_core/include"
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DPython_NumPy_INCLUDE_DIR=$TERMUX_PYTHON_HOME/site-packages/numpy/_core/include"
 
 	local TERMUX_PKG_SRCDIR_SAVE="$TERMUX_PKG_SRCDIR"
 	TERMUX_PKG_SRCDIR+="/cmake"
@@ -46,12 +48,4 @@ termux_step_make_install() {
 	local _pyver="${TERMUX_PYTHON_VERSION//./}"
 	local _wheel="onnxruntime-${TERMUX_PKG_VERSION}-cp${_pyver}-cp${_pyver}-linux_${TERMUX_ARCH}.whl"
 	pip install --no-deps --prefix="$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR/dist/${_wheel}"
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing dependencies through pip..."
-	pip3 install onnxruntime
-	EOF
 }
