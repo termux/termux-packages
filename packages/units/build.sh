7b1a11868b10b9d25b08e4c6e7cf2a81616834b9
Maxython <mixython@gmail.com>
2025-07-01 01:36:56 +0300

    Recompiling packages with debpython
    - packages/borgbackup
    - packages/electrum
    - packages/hash-slinger
    - packages/lv2
    - packages/manim
    - packages/nala
    - packages/python-cryptography
    - packages/python-tldp
    - packages/python-xlib
    - packages/rdiff-backup
    - packages/rdircd
    - packages/sabnzbd
    - packages/srt2vobsub
    - packages/termux-gui-package
    - packages/termux-gui-pm
    - packages/ytui-music
    - x11-packages/orca
    - x11-packages/pyqt5
    - packages/matplotlib
    - packages/python-onnxruntime
    - packages/python-torch
    - packages/python-torchvision
    - packages/python-yt-dlp
    - x11-packages/xfce4-panel-profiles
    - disabled-packages/python-pandas
    - packages/calcurse
    - packages/units
    - root-packages/frida
    - x11-packages/inkscape
    
    Co-authored-by: @robertkirkman

--- packages/units/build.sh
+++ packages/units/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Converts between different systems of units"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.24"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/units/units-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1e502c4edfacf20b29284716c72e5ddb51a495a2365d7b03e7960494c4a0c902
 TERMUX_PKG_DEPENDS="readline, libandroid-support"
