ecbf0d132996c988817e69c26668cc61fa14c44f
Maxython <mixython@gmail.com>
2025-07-01 01:36:56 +0300

    Recompiling packages with debpython
    - packages/borgbackup
    - packages/electrum
    - packages/hash-slinger
    - packages/lv2
    - packages/manim
    - packages/nala
    - packages/python-cryptography
    - packages/python-tldp
    - packages/python-xlib
    - packages/rdiff-backup
    - packages/rdircd
    - packages/sabnzbd
    - packages/srt2vobsub
    - packages/termux-gui-package
    - packages/termux-gui-pm
    - packages/ytui-music
    - x11-packages/orca
    - x11-packages/pyqt5
    - packages/matplotlib
    - packages/python-onnxruntime
    - packages/python-torch
    - packages/python-torchvision
    - packages/python-yt-dlp
    - x11-packages/xfce4-panel-profiles
    - disabled-packages/python-pandas
    - packages/calcurse
    - packages/units
    - root-packages/frida
    - x11-packages/inkscape
    
    Co-authored-by: @robertkirkman

--- packages/rdircd/build.sh
+++ packages/rdircd/build.sh
@@ -4,12 +4,12 @@ TERMUX_PKG_LICENSE="WTFPL"
 TERMUX_PKG_MAINTAINER="@termux"
 _COMMIT=026f1aef9857ae6ce06bfb00860898e6113adfc0
 TERMUX_PKG_VERSION=2023.02.07
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=git+https://github.com/mk-fg/reliable-discord-client-irc-daemon
 TERMUX_PKG_SHA256=c2cc88d6e1616d27f6f7849d536ba7613c7f13f0d16cac6022f9b1952ad537e2
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_DEPENDS="python, python-pip"
+TERMUX_PKG_DEPENDS="python"
 TERMUX_PKG_PYTHON_TARGET_DEPS="aiohttp"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -33,12 +33,4 @@ termux_step_post_get_source() {
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin rdircd
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./postinst
-	#!$TERMUX_PREFIX/bin/sh
-	echo "Installing dependencies through pip..."
-	pip3 install ${TERMUX_PKG_PYTHON_TARGET_DEPS}
-	EOF
 }
