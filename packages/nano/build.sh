0cd058df066dce06463e7b8b106cd8b2ba8c99b2
Sylirre <sylirre@termux.dev>
2025-06-30 23:52:58 +0300

    fix(main/nano): wildcards in nanorc shouldn't be prefixed by backslash

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small, free and friendly text editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="8.5"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://nano-editor.org/dist/latest/nano-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=000b011d339c141af9646d43288f54325ff5c6e8d39d6e482b787bbc6654c26a
 TERMUX_PKG_AUTO_UPDATE=true
@@ -22,5 +22,5 @@ TERMUX_PKG_RM_AFTER_INSTALL="bin/rnano share/man/man1/rnano.1 share/nano/man-htm
 termux_step_post_make_install() {
 	# Configure nano to use syntax highlighting:
 	NANORC=$TERMUX_PREFIX/etc/nanorc
-	echo "include \"$TERMUX_PREFIX/share/nano/\*nanorc\"" > "$NANORC"
+	echo "include \"$TERMUX_PREFIX/share/nano/*nanorc\"" > "$NANORC"
 }
