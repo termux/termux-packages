0c532e81cab7430f7d3dc5146f2279c2da122516
Robert Kirkman <rkirkman@termux.dev>
2025-06-30 19:51:33 -0500

    addpkg(main/update-alternatives): 1.22.6-4
    - The `update-alternatives` tool can also be used by `pacman`, and is necessary for convenient management of some symbolic links,like `$PREFIX/bin/pager`, so split from `dpkg` into a subpackage that `pacman` can depend on the same way that `apt`-based bootstraps pull in `update-alternatives`

---
+++ packages/dpkg/update-alternatives.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_DESCRIPTION="Utility to maintain symbolic links determining default commands"
+TERMUX_SUBPKG_INCLUDE="
+bin/update-alternatives
+share/man/man1/update-alternatives.1.gz
+"
+TERMUX_SUBPKG_DEPENDS="libmd"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
