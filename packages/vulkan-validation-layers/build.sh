aa7bfc6fa3e1e0fe97fbffc09460448b638b05de
Jia Yuan Lo <jylo06g@gmail.com>
2025-07-01 12:11:01 +0800

    bump(main/vulkan-validation-layers): 1.4.319 (#25223)

--- packages/vulkan-validation-layers/build.sh
+++ packages/vulkan-validation-layers/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-ValidationLayers
 TERMUX_PKG_DESCRIPTION="Vulkan Validation Layers"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.315"
+TERMUX_PKG_VERSION="1.4.319"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-ValidationLayers/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9e85fcd793fb1ee871ea8e293cb1f9736473614be23dd03a0dec8a5d1cee918c
+TERMUX_PKG_SHA256=e46cb2ba7190cd134497c9841c967e39b8e5f45d4c1cd85f0d3a827e8b4bf037
 TERMUX_PKG_DEPENDS="libc++, vulkan-loader"
-TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr, spirv-headers, spirv-tools, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-utility-libraries (=${TERMUX_PKG_VERSION})"
+TERMUX_PKG_BUILD_DEPENDS="libwayland, libx11, libxcb, libxrandr"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
@@ -20,4 +20,51 @@ termux_pkg_auto_update() {
 	fi
 	local latest_version=$(echo "${latest_refs_tags}" | sort -V | tail -n1)
 	termux_pkg_upgrade_version "${latest_version}"
+}
+
+termux_step_pre_configure() {
+	termux_setup_cmake
+	termux_setup_ninja
+
+	# use upstream known_good.json to fetch and build deps
+	"${TERMUX_PKG_SRCDIR}/scripts/update_deps.py" --generator Ninja
+
+	find "$PWD" -name helper.cmake | sort | xargs -i bash -c "echo '===== {} =====' && cat {}"
+
+	# prioritises build deps from source instead of system prefix
+	# avoiding pollution from old dependencies
+	local GLSLANG_INSTALL_DIR="${TERMUX_PKG_SRCDIR}/glslang/build/install"
+	local GOOGLETEST_INSTALL_DIR="${TERMUX_PKG_SRCDIR}/googletest/build/install"
+	local SPIRV_HEADERS_INSTALL_DIR="${TERMUX_PKG_SRCDIR}/SPIRV-Headers/build/install"
+	local SPIRV_TOOLS_INSTALL_DIR="${TERMUX_PKG_SRCDIR}/SPIRV-Tools/build/install"
+	local VULKAN_HEADERS_INSTALL_DIR="${TERMUX_PKG_SRCDIR}/Vulkan-Headers/build/install"
+	local VULKAN_UTILITY_LIBRARIES_INSTALL_DIR="${TERMUX_PKG_SRCDIR}/Vulkan-Utility-Libraries/build/install"
+
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
+	-DGLSLANG_INSTALL_DIR=${GLSLANG_INSTALL_DIR}
+	-DGOOGLETEST_INSTALL_DIR=${GOOGLETEST_INSTALL_DIR}
+	-DSPIRV_HEADERS_INSTALL_DIR=${SPIRV_HEADERS_INSTALL_DIR}
+	-DSPIRV_TOOLS_INSTALL_DIR=${SPIRV_TOOLS_INSTALL_DIR}
+	-DVULKAN_HEADERS_INSTALL_DIR=${VULKAN_HEADERS_INSTALL_DIR}
+	-DVULKAN_UTILITY_LIBRARIES_INSTALL_DIR=${VULKAN_UTILITY_LIBRARIES_INSTALL_DIR}
+	"
+
+	CFLAGS="-I${GLSLANG_INSTALL_DIR}/include ${CFLAGS}"
+	CFLAGS="-I${GOOGLETEST_INSTALL_DIR}/include ${CFLAGS}"
+	CFLAGS="-I${SPIRV_HEADERS_INSTALL_DIR}/include ${CFLAGS}"
+	CFLAGS="-I${SPIRV_TOOLS_INSTALL_DIR}/include ${CFLAGS}"
+	CFLAGS="-I${VULKAN_HEADERS_INSTALL_DIR}/include ${CFLAGS}"
+	CFLAGS="-I${VULKAN_UTILITY_LIBRARIES_INSTALL_DIR}/include ${CFLAGS}"
+	CXXFLAGS="-I${GLSLANG_INSTALL_DIR}/include ${CXXFLAGS}"
+	CXXFLAGS="-I${GOOGLETEST_INSTALL_DIR}/include ${CXXFLAGS}"
+	CXXFLAGS="-I${SPIRV_HEADERS_INSTALL_DIR}/include ${CXXFLAGS}"
+	CXXFLAGS="-I${SPIRV_TOOLS_INSTALL_DIR}/include ${CXXFLAGS}"
+	CXXFLAGS="-I${VULKAN_HEADERS_INSTALL_DIR}/include ${CXXFLAGS}"
+	CXXFLAGS="-I${VULKAN_UTILITY_LIBRARIES_INSTALL_DIR}/include ${CXXFLAGS}"
+	LDFLAGS="-L${GLSLANG_INSTALL_DIR}/lib ${LDFLAGS}"
+	LDFLAGS="-L${GOOGLETEST_INSTALL_DIR}/lib ${LDFLAGS}"
+	LDFLAGS="-L${SPIRV_HEADERS_INSTALL_DIR}/lib ${LDFLAGS}"
+	LDFLAGS="-L${SPIRV_TOOLS_INSTALL_DIR}/lib ${LDFLAGS}"
+	LDFLAGS="-L${VULKAN_HEADERS_INSTALL_DIR}/lib ${LDFLAGS}"
+	LDFLAGS="-L${VULKAN_UTILITY_LIBRARIES_INSTALL_DIR}/lib ${LDFLAGS}"
 }
