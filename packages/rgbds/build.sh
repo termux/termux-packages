1b69e2b6de2a44b7531df9c8da725f8eaebc8daf
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-01 05:03:44 +0000

    bump(main/rgbds): 0.9.3

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://rgbds.gbdev.io
 TERMUX_PKG_DESCRIPTION="Rednex Game Boy Development System - An assembly toolchain for the Nintendo Game Boy & Game Boy Color"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.2"
-TERMUX_PKG_SRCURL=https://github.com/gbdev/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=07143d2c1bb4a03cccd76b1990c843c307c2360702510bee9920dea8eea4a5b2
+TERMUX_PKG_VERSION="0.9.3"
+TERMUX_PKG_SRCURL=https://github.com/gbdev/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-source.tar.gz
+TERMUX_PKG_SHA256=87e56678fa2e8ddeec552a9149e4f2983fc1d3f8d2dbc3606d4b434e64d9baa5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-spawn, libandroid-support, libc++, libpng"
 
