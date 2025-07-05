d72da6eb560c917df29a6fb3be9ebb1f7a7692ae
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-07-05 13:02:46 +0200

    update depends

--- packages/hledger/build.sh
+++ packages/hledger/build.sh
@@ -29,8 +29,7 @@ TERMUX_PKG_SHA256=60b74c70ddfc6b84ca87debd2ac302aac754da3c0d9089821182e56796cb84
 
 TERMUX_PKG_BUILD_IN_SRC=true
 
-# TERMUX_PKG_DEPENDS="libffi"
-TERMUX_PKG_BUILD_DEPENDS="aosp-libs, libffi"
+TERMUX_PKG_BUILD_DEPENDS="aosp-libs, libffi, libiconv, libgmp"
 
 termux_step_pre_configure() {
 	termux_setup_ghc
