ffd4d1bcf01cb08712856502d578dfb3822247f7
Yaksh Bariya <yakshbari4@gmail.com>
2025-07-07 02:17:33 +0530

    fix(x11/xorg-server): typo for TERMUX_SUBPKG_*

--- x11-packages/xorg-server/xorg-server-xvfb.subpackage.sh
+++ x11-packages/xorg-server/xorg-server-xvfb.subpackage.sh
@@ -1,8 +1,8 @@
 TERMUX_SUBPKG_DESCRIPTION="X virtual framebuffer"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libdrm, libpixman, libx11, libxau, libxfont2, libxinerama, libxkbfile, libxshmfence, opengl, openssl, xkeyboard-config, xorg-protocol-txt, xorg-xkbcomp"
-TERMUX_SUBPPKG_CONFLICTS="xorg-xvfb"
-TERMUX_SUBPPKG_REPLACES="xorg-xvfb"
+TERMUX_SUBPKG_CONFLICTS="xorg-xvfb"
+TERMUX_SUBPKG_REPLACES="xorg-xvfb"
 TERMUX_SUBPKG_INCLUDE="
 bin/Xvfb
 bin/xvfb-run
