77cda159e680418d8f4f7c71c383034a75a49570
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-07-06 11:57:02 +0200

    also detect cabal projects in configure step

--- scripts/build/configure/termux_step_configure.sh
+++ scripts/build/configure/termux_step_configure.sh
@@ -3,7 +3,7 @@ termux_step_configure() {
 
 	# This check should be above autotools check as haskell package too makes use of configure scripts which
 	# should be executed by its own build system.
-	if ls "${TERMUX_PKG_SRCDIR}"/*.cabal &>/dev/null; then
+	if ls "${TERMUX_PKG_SRCDIR}"/*.cabal &>/dev/null || ls "${TERMUX_PKG_SRCDIR}"/cabal.project &>/dev/null; then
 		[ "$TERMUX_CONTINUE_BUILD" == "true" ] && return
 		termux_step_configure_cabal
 	elif [ "$TERMUX_PKG_FORCE_CMAKE" = "false" ] && [ -f "$TERMUX_PKG_SRCDIR/configure" ]; then
