d0a61918a6ccaa770aa51f998b478cea95588ab2
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-07-06 13:43:26 +0200

    adjust description

--- packages/hledger-cli/build.sh
+++ packages/hledger-cli/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://hledger.org/
-TERMUX_PKG_DESCRIPTION="Robust, friendly, fast plain text accounting software"
+TERMUX_PKG_DESCRIPTION="Robust, friendly, fast plain text accounting software. (CLI only)"
 TERMUX_PKG_LICENSE="GPL-3.0-or-later"
 TERMUX_PKG_MAINTAINER="@erplsf"
 TERMUX_PKG_VERSION=1.43.2
