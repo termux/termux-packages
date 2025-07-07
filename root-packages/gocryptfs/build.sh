2a269a384f098f51e410a8165e123748cab46be5
okhex <Gabrielluc983@proton.me>
2025-07-07 15:59:03 +0530

    bump(root/gocryptfs): 2.5.4

--- root-packages/gocryptfs/build.sh
+++ root-packages/gocryptfs/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://nuetzlich.net/gocryptfs/
 TERMUX_PKG_DESCRIPTION="An encrypted overlay filesystem written in Go"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.5.1
+TERMUX_PKG_VERSION=2.5.4
 TERMUX_PKG_SRCURL=https://github.com/rfjakob/gocryptfs/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b37fc5aefd552c8d78acb896d115bfee1ed8aa4c9880b307b150f505f0a4d52e
-TERMUX_PKG_DEPENDS="openssl"
+TERMUX_PKG_SHA256=bbdfb574ad08faed19b724022bc167b00236967c742b23c25f95f8c31837342c
+TERMUX_PKG_DEPENDS="openssl, libfuse2"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
