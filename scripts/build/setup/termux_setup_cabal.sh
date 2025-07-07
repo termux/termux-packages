23bad745e82e45efeecd851690c671709ccafbab
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-07-07 11:53:41 +0200

    same for cabal

--- scripts/build/setup/termux_setup_cabal.sh
+++ scripts/build/setup/termux_setup_cabal.sh
@@ -50,8 +50,8 @@ termux_setup_cabal() {
 			 $([[ "$TERMUX_ON_DEVICE_BUILD" == false ]] && printf "%s" "hsc2hs-options: --cross-compile")
 		EOF
 	else
-		if [[ "${TERMUX_APP_PACKAGE_MANAGER}" == "apt" ]] && "$(dpkg-query -W -f '${db:Status-Status}\n' cabal-install 2>/dev/null)" != "installed" ||
-			[[ "${TERMUX_APP_PACKAGE_MANAGER}" == "pacman" ]] && ! "$(pacman -Q cabal-install 2>/dev/null)"; then
+		if [[ "${TERMUX_APP_PACKAGE_MANAGER}" == "apt" ]] && [[ "$(dpkg-query -W -f '${db:Status-Status}\n' cabal-install 2>/dev/null)" != "installed" ]] ||
+			[[ "${TERMUX_APP_PACKAGE_MANAGER}" == "pacman" ]] && ! pacman -Q cabal-install 2>/dev/null; then
 			echo "Package 'cabal-install' is not installed."
 			exit 1
 		fi
