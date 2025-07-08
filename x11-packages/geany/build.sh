faf9ab232b6eeb53f4370fa712bd45805d25e231
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-08 04:45:37 +0000

    bump(x11/geany): 2.1

--- x11-packages/geany/build.sh
+++ x11-packages/geany/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.geany.org/
 TERMUX_PKG_DESCRIPTION="Fast and lightweight IDE"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.0"
+TERMUX_PKG_VERSION="2.1"
 TERMUX_PKG_SRCURL=https://download.geany.org/geany-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=565b4cd2f0311c1e3a167ec71c4a32dba642e0fe554ae5bb6b8177b7a74ccc92
+TERMUX_PKG_SHA256=6b96a8844463300c10b9692a0a5edad8236eec9e84342f575f83d4fc89331228
 TERMUX_PKG_AUTO_UPDATE=true
 # libvte is dlopen(3)ed:
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libandroid-glob, libc++, libcairo, libvte, pango"
@@ -16,5 +16,11 @@ share/icons/Tango/icon-theme.cache
 "
 
 termux_step_pre_configure() {
-	export LIBS="-landroid-glob $($CC -print-libgcc-file-name)"
+	export LIBS="-landroid-glob"
+
+	# error: non-exported symbol '__aarch64_ldadd4_acq_rel' in libclang_rt.builtins-aarch64-android.a
+	local _libgcc_file="$($CC -print-libgcc-file-name)"
+	local _libgcc_path="$(dirname $_libgcc_file)"
+	local _libgcc_name="$(basename $_libgcc_file)"
+	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
 }
