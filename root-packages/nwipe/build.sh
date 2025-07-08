1033e5303bfe85675e71fa423cf12bf2e4f61020
Sylirre <sylirre@termux.dev>
2025-07-08 13:44:41 +0300

    fix(main/nwipe): package belongs to root-repo
    This package refuses to run without root permissions and should be
    placed to the root-repo.
    
    Additionally add more fixes for hardcoded paths to prevent errors
    related to configuration files.

--- root-packages/nwipe/build.sh
+++ root-packages/nwipe/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="A program that will securely erase the entire contents o
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.38"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/martijnvanbrummelen/nwipe/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0c1b19d1a721b995504fc01c21363555f207b25d6749650355a8cd7be09824dd
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, parted, libconfig"
+TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, parted, libconfig, hdparm"
+TERMUX_PKG_SUGGESTS="smartmontools"
 
 termux_step_pre_configure() {
 	autoreconf -fi
