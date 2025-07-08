71555dac0dcf732e44d76f4c4b53eaa9b4b91461
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-08 05:56:33 +0000

    bump(main/q-dns-client): 0.19.5
    Switch to tagged source tarball instead of git repository.

--- packages/q-dns-client/build.sh
+++ packages/q-dns-client/build.sh
@@ -2,18 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/natesales/q
 TERMUX_PKG_DESCRIPTION="A tiny command line DNS client with support for UDP, TCP, DoT, DoH, DoQ and ODoH"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="kay9925@outlook.com"
-TERMUX_PKG_VERSION="0.19.2"
-TERMUX_PKG_SRCURL="git+https://github.com/natesales/q"
-TERMUX_PKG_SHA256=71462c2b4c490821a0cfa3c0673768343d00f4a1625bfe13b0a09c7dc8a2ede5
+TERMUX_PKG_VERSION="0.19.5"
+TERMUX_PKG_SRCURL="https://github.com/natesales/q/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=6d85b6e5ebb99a2c2e69dfba4fdb758b676664fde5af7cd258f4fdc2b6c4702a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_post_get_source() {
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]] ; then
-		termux_error_exit "Checksum mismatch for source files.\nExpected: ${TERMUX_PKG_SHA256}\nActual:   ${s%% *}"
-	fi
-}
 
 termux_step_make() {
 	termux_setup_golang
