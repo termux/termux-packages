43a2a32ba5772206b6ddc9e2ad926eaa3eec47c4
Robert Kirkman <rkirkman@termux.dev>
2025-07-09 03:51:05 -0500

    addpkg(main/sendme): 0.27.0
    - Fixes https://github.com/termux/termux-packages/issues/25293
    
    - It is true that the package is very easy to compile, however, the
      problem explained in the issue is that it takes an extremely long time
      to compile on a slow device. I have written this entire `build.sh` and
      committed it, and it still did not finish building on my device, so I
      agree with the issue reporter regarding that.

---
+++ packages/sendme/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/n0-computer/sendme
+TERMUX_PKG_DESCRIPTION="A tool to send files and directories, based on iroh"
+TERMUX_PKG_LICENSE="Apache-2.0, MIT"
+TERMUX_PKG_LICENSE_FILE="
+LICENSE-APACHE
+LICENSE-MIT
+"
+TERMUX_PKG_MAINTAINER="@termux"
+# version 0.27.0 is currently untagged on GitHub
+_COMMIT=3db5ea170234650b626315d8d18ed106ea1b5612
+TERMUX_PKG_VERSION="0.27.0"
+TERMUX_PKG_SRCURL="https://github.com/n0-computer/sendme/archive/${_COMMIT}.tar.gz"
+TERMUX_PKG_SHA256=756d52a9592ff0b7c10b18e5b77b0967bf21995ed6290ff48b27b9e66168c696
+TERMUX_PKG_BUILD_IN_SRC=true
