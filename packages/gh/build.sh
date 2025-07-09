f543119b6e4b9413192d65f5e94d6bcb183d0320
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-09 19:19:52 +0000

    bump(main/gh): 2.75.0
    Remove unnecessary documentation related to developing gh which is not
    related to general usage of gh. See this readme
    https://github.com/cli/cli/blob/v2.75.0/docs/README.md

--- packages/gh/build.sh
+++ packages/gh/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://cli.github.com/
 TERMUX_PKG_DESCRIPTION="GitHub’s official command line tool"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION="2.74.2"
+TERMUX_PKG_VERSION="2.75.0"
 TERMUX_PKG_SRCURL=https://github.com/cli/cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=58d383e75e1a6f3eb5e5694f232d1ed6f7f53681fda9c6a997e6e1be344edd94
+TERMUX_PKG_SHA256=a99fce70ccb8e0a311a504eda0cfd24e23431e158bf136d81ac7ad25f0431597
 TERMUX_PKG_RECOMMENDS="openssh"
 TERMUX_PKG_AUTO_UPDATE=true
 
@@ -28,7 +28,6 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/cli/cli/cmd/gh/gh
-	install -Dm600 -t "$TERMUX_PREFIX"/share/doc/gh/ "$TERMUX_PKG_SRCDIR"/docs/*
 
 	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/bash-completion/completions/gh.bash
 	install -Dm644 /dev/null "$TERMUX_PREFIX"/share/zsh/site-functions/_gh
