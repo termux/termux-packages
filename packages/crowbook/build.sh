9cdf1098162c62d19b0418e89b8ae2abfebefd73
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-10 05:49:23 +0000

    bump(main/crowbook): 0.17.0
    Remove time-items-format_items.diff file which was added in
    https://github.com/time-rs/time/commit/5b0c627366babea1636b35db641c0fec964ddbd1

--- packages/crowbook/build.sh
+++ packages/crowbook/build.sh
@@ -2,28 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://github.com/crowdagger/crowbook
 TERMUX_PKG_DESCRIPTION="Allows you to write a book in Markdown without worrying about formatting or typography"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.16.1"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.17.0"
 TERMUX_PKG_SRCURL=https://github.com/crowdagger/crowbook/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2e49a10f1b14666d4f740e9a22a588d44b137c3fca0932afc50ded0280450311
+TERMUX_PKG_SHA256=13c628722fedf1bfebc0fd334e9ffa41a62888e3195d2ad1e5ea851694dc4a4c
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	termux_setup_rust
-
-	: "${CARGO_HOME:=$HOME/.cargo}"
-	export CARGO_HOME
-
-	cargo vendor
-	patch --silent -p1 \
-		-d ./vendor/time/ \
-		< "$TERMUX_PKG_BUILDER_DIR"/time-items-format_items.diff
-
-	echo "" >> Cargo.toml
-	echo '[patch.crates-io]' >> Cargo.toml
-	echo 'time = { path = "./vendor/time" }' >> Cargo.toml
-}
 
 termux_step_make() {
 	termux_setup_rust
