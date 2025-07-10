c39f221f2ee60e2cd5c2180228c42d401f572e8f
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-10 07:39:52 +0000

    bump(main/djvulibre): 3.5.29

--- packages/djvulibre/build.sh
+++ packages/djvulibre/build.sh
@@ -2,14 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://djvu.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Suite to create, manipulate and view DjVu ('déjà vu') documents"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.5.28
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_VERSION="3.5.29"
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/djvu/djvulibre-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fcd009ea7654fde5a83600eb80757bd3a76998e47d13c66b54c8db849f8f2edc
+TERMUX_PKG_SHA256=d3b4b03ae2bdca8516a36ef6eb27b777f0528c9eda26745d9962824a3fdfeccf
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libjpeg-turbo, libtiff"
 
 termux_step_pre_configure() {
 	NOCONFIGURE=1 ./autogen.sh
 
-	LDFLAGS+=" $($CC -print-libgcc-file-name)"
+	# ERROR: ./lib/libdjvulibre.so contains undefined symbols: __aeabi_idiv
+	local _libgcc_file="$($CC -print-libgcc-file-name)"
+	local _libgcc_path="$(dirname $_libgcc_file)"
+	local _libgcc_name="$(basename $_libgcc_file)"
+	LDFLAGS+=" -L$_libgcc_path -l:$_libgcc_name"
 }
