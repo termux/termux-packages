3c3c3364536cf74fb121e5082ce9820329deefe9
Robert Kirkman <rkirkman@termux.dev>
2025-07-17 03:39:15 -0500

    addpkg(x11/openimageio): 3.0.8.1
    - Fixes #25379
    
    - This package is desired as a dependency for on-device building of a potential improved Blender package

--- x11-packages/openvdb/build.sh
+++ x11-packages/openvdb/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL="https://github.com/AcademySoftwareFoundation/openvdb/archive/
 TERMUX_PKG_SHA256=a3c8724ecadabaf558b6e1bd6f1d695e93b82a7cfdf144b8551e5253340ddce0
 TERMUX_PKG_DEPENDS="boost, imath, libblosc, libtbb, zlib"
 TERMUX_PKG_BUILD_DEPENDS="mesa, glfw, glu"
+TERMUX_PKG_AUTO_UPDATE=true
 # Numpy support requires nanobind which is not packaged at time of writing
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DUSE_IMATH_HALF=ON
