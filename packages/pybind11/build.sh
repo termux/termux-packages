d25c29082b19ab613639e231944f8c3862fd16dc
Robert Kirkman <rkirkman@termux.dev>
2025-07-17 03:36:03 -0500

    addpkg(main/pybind11): 3.0.0

---
+++ packages/pybind11/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://pybind11.readthedocs.io/
+TERMUX_PKG_DESCRIPTION="A lightweight header-only library that exposes C++ types in Python and vice versa"
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="3.0.0"
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/pybind/pybind11/archive/v${TERMUX_PKG_VERSION}/pybind11-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=453b1a3e2b266c3ae9da872411cadb6d693ac18063bd73226d96cfb7015a200c
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_BUILD_DEPENDS="boost, eigen"
+TERMUX_PKG_PYTHON_COMMON_DEPS="setuptools"
+TERMUX_CMAKE_BUILD="Unix Makefiles"
