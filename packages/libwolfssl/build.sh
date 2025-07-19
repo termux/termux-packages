e329deb0a06a126a562d79de2565402badb0106a
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-19 04:21:16 +0000

    bump(main/libwolfssl): 5.8.2

--- packages/libwolfssl/build.sh
+++ packages/libwolfssl/build.sh
@@ -2,16 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.wolfssl.com/
 TERMUX_PKG_DESCRIPTION="A small, fast, portable implementation of TLS/SSL for embedded devices to the cloud"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.8.0"
+TERMUX_PKG_VERSION="5.8.2"
 TERMUX_PKG_SRCURL=https://github.com/wolfSSL/wolfssl/archive/refs/tags/v${TERMUX_PKG_VERSION}-stable.tar.gz
-TERMUX_PKG_SHA256=f90f18c7f12913a0b351b1f4305e768697ea1380794df1f2984b9452ab0aeeaf
+TERMUX_PKG_SHA256=3ef126e3466e2f8f6ebb62b916a7f8fb26c6709dbdf2b63a167759f2fdb53068
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 
 termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=43
+	local _SOVERSION=44
 
 	local a
 	for a in WOLFSSL_LIBRARY_VERSION_FIRST; do
@@ -20,6 +20,6 @@ termux_step_pre_configure() {
 	done
 	local v=${_WOLFSSL_LIBRARY_VERSION_FIRST}
 	if [ ! "${v}" ] || [ "${v}" != "${_SOVERSION}" ]; then
-		termux_error_exit "SOVERSION guard check failed."
+		termux_error_exit "SOVERSION guard check failed. Expected ${_SOVERSION}, got ${v}."
 	fi
 }
