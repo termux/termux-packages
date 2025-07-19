dbce2de6648559e371b14c17aa46c0cbc69684ec
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-07-19 13:47:44 +0800

    chromium-host-tools: bump to 138.0.7204.157

--- x11-packages/chromium-host-tools/build.sh
+++ x11-packages/chromium-host-tools/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.chromium.org/Home
 TERMUX_PKG_DESCRIPTION="Chromium web browser (Host tools)"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=138.0.7204.100
+TERMUX_PKG_VERSION=138.0.7204.157
 TERMUX_PKG_SRCURL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=368d1a1d73c8703b9f9252340680468e3e3073b33b5127ac9e5fd8e9431b18b8
+TERMUX_PKG_SHA256=835dfd3228f6adb2a8c78e296c7d4981aee35daee401a2fe1493b4657d736d25
 TERMUX_PKG_DEPENDS="atk, cups, dbus, fontconfig, gtk3, krb5, libc++, libdrm, libevdev, libxkbcommon, libminizip, libnss, libx11, mesa, openssl, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libffi-static"
 # TODO: Split chromium-common and chromium-headless
