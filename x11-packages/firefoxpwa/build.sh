28d9168f681661787f145869745f6ce974fddb7f
Robert Kirkman <rkirkman@termux.dev>
2025-07-19 01:25:54 -0500

    bump(x11/firefoxpwa): 2.15.0
    - Fixes https://github.com/termux/termux-packages/issues/25404
    
    - Rebase all patches
    
    - Adjust formatting
    
    - This package, while working already under ideal conditions, has some bugs in common edge cases, which I believe I have now fixed here.
      - Add `TERMUX_DEBUG_BUILD=true` support
      - `termux_step_create_debscipts()`->`termux_step_create_debscripts()`
      - Add patch that prevents `No such file or directory (os error 2)` during `firefoxpwa runtime install --link` during `postinst` script during clean installation by creating the `~/.local/share/firefoxpwa/runtime/` folder automatically if it does not exist right before it's needed

--- x11-packages/firefoxpwa/build.sh
+++ x11-packages/firefoxpwa/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://pwasforfirefox.filips.si/
-TERMUX_PKG_DESCRIPTION="PWAs for Firefox"
+TERMUX_PKG_DESCRIPTION="A tool to install, manage and use Progressive Web Apps (PWAs) in Mozilla Firefox (native component)"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.14.1"
-TERMUX_PKG_SRCURL=https://github.com/filips123/PWAsForFirefox/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=40d2f4987e473a312947829e1ac57b58d2c322250c335aa1e148a75c9c40aa74
+TERMUX_PKG_VERSION="2.15.0"
+TERMUX_PKG_SRCURL="https://github.com/filips123/PWAsForFirefox/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=772ef9461aaed98f90f772f8e01070272048f7607ac061b56f1af4ab5f8bbade
 TERMUX_PKG_DEPENDS="firefox"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -17,21 +17,30 @@ termux_step_make() {
 	sed -i "s/version = \"0.0.0\"/version = \"${TERMUX_PKG_VERSION}\"/g" Cargo.toml
 	sed -i "s/DISTRIBUTION_VERSION = '0.0.0'/DISTRIBUTION_VERSION = '${TERMUX_PKG_VERSION}'/g" userchrome/profile/chrome/pwa/chrome.sys.mjs
 
-	cargo build --release --jobs "$TERMUX_PKG_MAKE_PROCESSES" --target "$CARGO_TARGET_NAME"
+	local release_arg="--release"
+	if [[ "$TERMUX_DEBUG_BUILD" == "true" ]]; then
+		release_arg=""
+	fi
+
+	cargo build $release_arg --jobs "$TERMUX_PKG_MAKE_PROCESSES" --target "$CARGO_TARGET_NAME"
 }
 
 termux_step_make_install() {
-
 	cd native
 
-	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/firefoxpwa"
-	install -Dm755 -t "$TERMUX_PREFIX/libexec" "target/${CARGO_TARGET_NAME}/release/firefoxpwa-connector"
+	local build_dir="target/${CARGO_TARGET_NAME}/release"
+	if [[ "$TERMUX_DEBUG_BUILD" == "true" ]]; then
+		build_dir="target/${CARGO_TARGET_NAME}/debug"
+	fi
+
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "$build_dir/firefoxpwa"
+	install -Dm755 -t "$TERMUX_PREFIX/libexec" "$build_dir/firefoxpwa-connector"
 	# manifest
 	install -DTm644 manifests/termux.json "$TERMUX_PREFIX/lib/mozilla/native-messaging-hosts/firefoxpwa.json"
 	# completions
-	install -Dm644 -t "$TERMUX_PREFIX/share/bash-completion/completions" "target/${CARGO_TARGET_NAME}/release/completions/firefoxpwa.bash"
-	install -Dm644 -t "$TERMUX_PREFIX/share/fish/vendor_completions.d" "target/${CARGO_TARGET_NAME}/release/completions/firefoxpwa.fish"
-	install -Dm644 -t "$TERMUX_PREFIX/share/zsh/vendor-completions" "target/${CARGO_TARGET_NAME}/release/completions/_firefoxpwa"
+	install -Dm644 -t "$TERMUX_PREFIX/share/bash-completion/completions" "$build_dir/completions/firefoxpwa.bash"
+	install -Dm644 -t "$TERMUX_PREFIX/share/fish/vendor_completions.d" "$build_dir/completions/firefoxpwa.fish"
+	install -Dm644 -t "$TERMUX_PREFIX/share/zsh/vendor-completions" "$build_dir/completions/_firefoxpwa"
 	# UserChrome
 	install -dm755 "$TERMUX_PREFIX/share/firefoxpwa/userchrome"
 	cp -r userchrome/* "$TERMUX_PREFIX/share/firefoxpwa/userchrome/"
@@ -42,20 +51,18 @@ termux_step_make_install() {
 	# AppStream Metadata
 	install -Dm644 -t "$TERMUX_PREFIX/share/metainfo" "packages/appstream/si.filips.FirefoxPWA.metainfo.xml"
 	install -Dm644 -t "$TERMUX_PREFIX/share/icons/hicolor/scalable/apps" "packages/appstream/si.filips.FirefoxPWA.svg"
-
 }
 
-termux_step_create_debscipts() {
-	cat <<-EOF >./postinst
+termux_step_create_debscripts() {
+	cat <<-EOF > ./postinst
 		#!${TERMUX_PREFIX}/bin/sh
 		firefoxpwa runtime install --link
 		exit 0
 	EOF
-	cat <<-EOF >./prerm
+	cat <<-EOF > ./prerm
 		#!${TERMUX_PREFIX}/bin/sh
 		firefoxpwa runtime uninstall
 		exit 0
 	EOF
 	chmod +x ./postinst ./prerm
-
 }
