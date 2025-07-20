daf5c2900c7e4fca0b73ccb5781791b9f178c819
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-07-20 15:48:14 +0530

    addpkg(x11/dconf-editor): 45.0.1

--- x11-packages/dconf/build.sh
+++ x11-packages/dconf/build.sh
@@ -3,15 +3,17 @@ TERMUX_PKG_DESCRIPTION="dconf is a simple key/value storage system that is heavi
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.40.0"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/dconf/-/archive/${TERMUX_PKG_VERSION}/dconf-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c144c314a5ab1b0e2c2e5cb381658d19b9577511b62db5df3fac463028b61bca
 TERMUX_PKG_DEPENDS="dbus, glib"
-TERMUX_PKG_BUILD_DEPENDS="glib-cross"
+TERMUX_PKG_BUILD_DEPENDS="glib-cross, valac"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dbash_completion=false
--Dvapi=false
+"
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/systemd
 "
 
 termux_step_pre_configure() {
