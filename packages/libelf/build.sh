95952cae4cd15cb7f1d095b3d351fd9901339fa9
Biswapriyo Nath <nathbappai@gmail.com>
2025-07-21 04:51:01 +0000

    bump(main/libelf): 0.193

--- packages/libelf/build.sh
+++ packages/libelf/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://sourceware.org/elfutils/
 TERMUX_PKG_DESCRIPTION="ELF object file access library"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.192"
+TERMUX_PKG_VERSION="0.193"
 TERMUX_PKG_SRCURL="https://sourceware.org/elfutils/ftp/${TERMUX_PKG_VERSION}/elfutils-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=616099beae24aba11f9b63d86ca6cc8d566d968b802391334c91df54eab416b4
+TERMUX_PKG_SHA256=7857f44b624f4d8d421df851aaae7b1402cfe6bcdd2d8049f15fc07d3dde7635
 # libandroid-support for langinfo.
 TERMUX_PKG_DEPENDS="libandroid-support, zlib, zstd, json-c"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_c99=yes --disable-symbol-versioning"
