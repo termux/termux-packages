6fdaaa6cd689bc3d3d9bdb2ab3bcd2a17646e021
Kevin Williams <admin@utermux.dev>
2025-07-22 10:08:36 +0800

    bump(main/vulkan-tools): 1.4.323
    Signed-off-by: Kevin Williams <admin@utermux.dev>

--- packages/vulkan-tools/build.sh
+++ packages/vulkan-tools/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/KhronosGroup/Vulkan-Tools
 TERMUX_PKG_DESCRIPTION="Vulkan Tools and Utilities"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.4.322"
+TERMUX_PKG_VERSION="1.4.323"
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=41a67124d9090c248c814ba826733420766920b976207ab25ae8fa3a3221b1c9
+TERMUX_PKG_SHA256=3fcb24083690e2ba577b89faa95fd37462954520b4af165ae7cd9d978613d931
 TERMUX_PKG_DEPENDS="libc++, libwayland, libx11, libxcb, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, vulkan-headers (=${TERMUX_PKG_VERSION}), vulkan-volk"
 TERMUX_PKG_ANTI_BUILD_DEPENDS="vulkan-loader"
