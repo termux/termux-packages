97ac1ac458d41a535ca0386bc197d52f41f020c9
lucinda lovebuny <landfillbaby69@gmail.com>
2025-07-24 07:05:10 +0100

    bump(main/megatools): 1.11.5.20250706

--- packages/megatools/build.sh
+++ packages/megatools/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://megatools.megous.com/
+TERMUX_PKG_HOMEPAGE=https://xff.cz/megatools/
 TERMUX_PKG_DESCRIPTION="Open-source command line tools and C library (libmega) for accessing Mega.co.nz cloud storage"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.11.1.20230212
-TERMUX_PKG_SRCURL=https://megatools.megous.com/builds/megatools-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ecfa2ee4b277c601ebae648287311030aa4ca73ea61ee730bc66bef24ef19a34
+TERMUX_PKG_VERSION=1.11.5.20250706
+TERMUX_PKG_SRCURL=https://xff.cz/megatools/builds/megatools-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=51f78a03748a64b1066ce28a2ca75d98dbef5f00fe9789dc894827f9a913b362
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dsymlinks=true
