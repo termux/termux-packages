238f12b995b2a5d1a9f5d0c11f9a188f6ef34634
lucinda lovebuny <landfillbaby69@gmail.com>
2025-07-24 07:16:54 +0100

    revert 8ece87198d0240770b32900688343c14d0ef9bc0

--- packages/megacmd/build.sh
+++ packages/megacmd/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://mega.io/
 TERMUX_PKG_DESCRIPTION="Provides non UI access to MEGA services"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.1.1"
+TERMUX_PKG_VERSION="2.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=git+https://github.com/meganz/MEGAcmd
 TERMUX_PKG_GIT_BRANCH=${TERMUX_PKG_VERSION}_Linux
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 # dbus is required for $PREFIX/var/lib/dbus/machine-id
 TERMUX_PKG_DEPENDS="c-ares, cryptopp, dbus, ffmpeg, freeimage, libandroid-glob, libc++, libcurl, libicu, libsodium, libsqlite, libuv, mediainfo, openssl, pcre, readline, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
