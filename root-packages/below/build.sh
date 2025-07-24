84315a72ef5ada31705e723aa6bc72364d8f3893
Robert Kirkman <rkirkman@termux.dev>
2025-07-24 10:26:57 -0500

    bump(root/below): 0.10.0
    - Fixes https://github.com/termux/termux-packages/issues/25445
    
    - Fixes building targeting 32-bit; tested working, but keep in mind that `below` only works on devices that have a kernel with cgroupv2 support, which usually means a kernel with version 5.8 or newer.

--- root-packages/below/build.sh
+++ root-packages/below/build.sh
@@ -2,32 +2,29 @@ TERMUX_PKG_HOMEPAGE=https://github.com/facebookincubator/below
 TERMUX_PKG_DESCRIPTION="An interactive tool to view and record historical system data"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9.0"
+TERMUX_PKG_VERSION="0.10.0"
 TERMUX_PKG_SRCURL=https://github.com/facebookincubator/below/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c51c202e9c15aafc1c741f053ba374c302dc0a737aef6a1120b5c7957ab013fe
+TERMUX_PKG_SHA256=04d837417acbfbbbcf58f028a4e527f4c392a459b6c1bf0fcf91a837a6cbc1dc
 TERMUX_PKG_DEPENDS="libelf, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-
-# ```
-# error[E0308]: mismatched types
-#    --> /home/builder/.cargo/registry/src/github.com-1ecc6299db9ec823/openat-0.1.21/src/dir.rs:465:34
-#     |
-# 465 |             match stat.st_mode & libc::S_IFMT {
-#     |                                  ^^^^^^^^^^^^ expected `u32`, found `u16`
-# ```
-TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 
 termux_step_pre_configure() {
 	termux_setup_rust
 	: "${CARGO_HOME:=$HOME/.cargo}"
 	export CARGO_HOME
 
+	rm -rf "$CARGO_HOME"/registry/src/*/nix-*
+	rm -rf "$CARGO_HOME"/registry/src/*/libbpf-sys-*
+	rm -rf "$CARGO_HOME"/registry/src/*/openat-*
+
+	cargo update
+
 	cargo fetch --target $CARGO_TARGET_NAME
 
 	local d p
-	for d in $CARGO_HOME/registry/src/*/libbpf-sys-*; do
+	for d in "$CARGO_HOME"/registry/src/*/libbpf-sys-*; do
 		for p in libbpf-sys-0.6.0-1-libbpf-include-linux-{compiler,types}.h.diff; do
 			patch --silent -p1 -d ${d} \
 				< "$TERMUX_PKG_BUILDER_DIR/${p}" || :
@@ -38,12 +35,19 @@ termux_step_pre_configure() {
 		patch --silent -p1 -d "$d" <  "$TERMUX_PKG_BUILDER_DIR"/nix-0.20.0-src-net-if_.rs.diff || :
 	done
 
+	for d in "$CARGO_HOME"/registry/src/*/openat-*; do
+		patch --silent -p1 -d "$d" <  "$TERMUX_PKG_BUILDER_DIR"/openat-st_mode-32-bit.diff || :
+	done
+
 	local _CARGO_TARGET_LIBDIR=target/$CARGO_TARGET_NAME/release/deps
 	mkdir -p $_CARGO_TARGET_LIBDIR
 	local lib
 	for lib in lib{elf,z}.so; do
 		ln -sf $TERMUX_PREFIX/lib/${lib} $_CARGO_TARGET_LIBDIR/
 	done
+
+	# prevents "gcc: error: unrecognized command-line option '-mfpu=neon'" when targeting 32-bit
+	unset CFLAGS
 }
 
 termux_step_make() {
