c4eda079ae3367081eae801d88a62e8694a20f64
Robert Kirkman <rkirkman@termux.dev>
2025-07-24 06:01:06 -0500

    bump(x11/vala-panel-appmenu): 25.04
    - Fixes https://github.com/termux/termux-packages/issues/24381
    
    - Progress on https://github.com/termux/termux-packages/issues/24930
    
    - Progress on https://github.com/termux/termux-packages/issues/23492
    
    - Tested working

--- x11-packages/vala-panel-appmenu/build.sh
+++ x11-packages/vala-panel-appmenu/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.com/vala-panel-project/vala-panel-appmenu
 TERMUX_PKG_DESCRIPTION="Global Menu for Vala Panel (metapackage)"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="24.05"
+TERMUX_PKG_VERSION="25.04"
 TERMUX_PKG_SRCURL=https://gitlab.com/vala-panel-project/vala-panel-appmenu/-/archive/${TERMUX_PKG_VERSION}/vala-panel-appmenu-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4dd891578429199d2310dc6ff37008be2bb26d045e1fdbbaed8d607af70f7cb2
+TERMUX_PKG_SHA256=ff270de372c41f18f64e8788629dd4cc9f116a89ee8947e3fc2657b19182e2dc
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, gtk2, gtk3, valac"
@@ -26,4 +26,18 @@ termux_step_pre_configure() {
 	CPPFLAGS+=" -Dulong=u_long"
 	LDFLAGS+=" -lX11"
 	termux_setup_glib_cross_pkg_config_wrapper
+
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		# adjust valac wrapper created by the cross-compilation mode
+		# of termux_setup_gir, to avoid a specific error
+		# that only happens to vala-panel-appmenu and
+		# only happens when cross-compiling it
+		# error: The name `Property' does not exist in the context of `Xfconf' (libxfconf-0)
+		rm -f "$TERMUX_PREFIX/lib/pkgconfig/appmenu-glib-translator.pc"
+		mkdir -p "$TERMUX_PKG_TMPDIR/custom-bin"
+		cp "$(command -v valac)" "$TERMUX_PKG_TMPDIR/custom-bin"
+		sed -i "s|--vapidir=\"$TERMUX_PREFIX/share/vala/vapi\"||g" \
+			"$TERMUX_PKG_TMPDIR/custom-bin/valac"
+		export PATH="$TERMUX_PKG_TMPDIR/custom-bin:$PATH"
+	fi
 }
