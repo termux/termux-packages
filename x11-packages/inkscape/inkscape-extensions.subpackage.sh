e7ebe8b3b7e4f9260c1a579d97d26eec7cd3d0fb
Robert Kirkman <rkirkman@termux.dev>
2025-07-25 03:33:07 -0500

    robertkirkman debpython temporary commit 3

--- x11-packages/inkscape/inkscape-extensions.subpackage.sh
+++ x11-packages/inkscape/inkscape-extensions.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_INCLUDE="share/inkscape/extensions"
 TERMUX_SUBPKG_DESCRIPTION="Inkscape extensions"
-TERMUX_SUBPKG_DEPENDS="python, python-numpy, python-pip, python-lxml"
+TERMUX_SUBPKG_DEPENDS="python, python-numpy, python-lxml"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 TERMUX_SUBPKG_PYTHON_RUNTIME_DEPS="scour"
