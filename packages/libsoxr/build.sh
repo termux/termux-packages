889c43bab97490bc88144bc6b1b7b5353fcfe9d8
Robert Kirkman <rkirkman@termux.dev>
2025-08-03 07:03:41 -0500

    tree-wide: fixing the builds of some packages for CMake 4
    - Will fix https://github.com/termux/termux-packages/issues/25579
    
    - It is hard to find all of these, but pretty soon (right after is https://github.com/termux/termux-packages/pull/24866 merged), I will begin focusing on https://github.com/termux/termux-packages/issues/23492 and that will hopefully help me add many more packages to this PR.
    
    %ci:no-build
    
    Co-authored-by: TomIO <tom@termux.dev>

--- packages/libsoxr/build.sh
+++ packages/libsoxr/build.sh
@@ -8,6 +8,9 @@ TERMUX_PKG_SRCURL=https://sourceforge.net/projects/soxr/files/soxr-$TERMUX_PKG_V
 TERMUX_PKG_SHA256=b111c15fdc8c029989330ff559184198c161100a59312f5dc19ddeb9b5a15889
 TERMUX_PKG_BREAKS="libsoxr-dev"
 TERMUX_PKG_REPLACES="libsoxr-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_POLICY_VERSION_MINIMUM=3.5
+"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -fopenmp -static-openmp"
