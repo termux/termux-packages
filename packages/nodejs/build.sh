5cef1c2f4f8e463a79b9c2ca7286c90dde152925
Yaksh Bariya <yakshbari4@gmail.com>
2025-08-03 10:08:50 +0530

    bump(main/nodejs): 24.5.0
    Tested on aarch64 device + termux-docker (all architectures).
    
    No regressions found when running the test suite on device,
    termux-docker is still failing a lot of tests, but it will need
    significantly more effort to debug

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=24.4.1
+TERMUX_PKG_VERSION=24.5.0
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=adb79ca0987486ed66136213da19ff17ef6724dcb340c320e010c9442101652f
+TERMUX_PKG_SHA256=f1ba96204724bd1c6de7758e08b3718ba0b45d87fb3bebd7e30097874ccc8130
 # thunder-coding: don't try to autoupdate nodejs, that thing takes 2 whole hours to build for a single arch, and requires a lot of patch updates everytime. Also I run tests everytime I update it to ensure least bugs
 TERMUX_PKG_AUTO_UPDATE=false
 # Note that we do not use a shared libuv to avoid an issue with the Android
