cbc7ece3b29377b46840f48c9b4a63ed50f68a8a
BullyMaguire-lol <temp9040@gmail.com>
2025-08-07 14:34:24 +0530

    addpkg(x11/plasma-workspace-wallpapers): 6.5.2

---
+++ x11-packages/plasma-workspace-wallpapers/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/plasma-workspace-wallpapers"
+TERMUX_PKG_DESCRIPTION="Additional wallpapers for the Plasma Workspace"
+TERMUX_PKG_LICENSE="LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.2"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/plasma-workspace-wallpapers-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="05ac1d3ac607563bf18de5cec49f6c0a5bf51c82e6b8bf4466886e0d19d3593b"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DBUILD_WITH_QT6=ON
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
