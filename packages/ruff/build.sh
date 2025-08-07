4a79cb81b05d26c7fc1e268a435020936078cbf0
Robert Kirkman <rkirkman@termux.dev>
2025-08-07 10:51:33 -0500

    robertkirkman debpython temporary commit 4

--- packages/ruff/build.sh
+++ packages/ruff/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An extremely fast Python linter, written in Rust"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.14.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/charliermarsh/ruff/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=c70834fa2c2ff7c9daae8f69010310be4f97d796b9caf9b9a3a83b3a82113193
 TERMUX_PKG_AUTO_UPDATE=true
