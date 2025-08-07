fef1a201c45ab0c051380064112ca4af3f846c4b
BullyMaguire-lol <temp9040@gmail.com>
2025-08-07 14:46:36 +0530

    addpkg(x11/kf6-kded): 6.18.0

---
+++ x11-packages/kf6-kded/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kded"
+TERMUX_PKG_DESCRIPTION="Extensible deamon for providing system level services"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.18.0"
+_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kded-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="c90e3ee6bf991f320ac428b2c126afbfe1130a74eee6724058105883072e5882"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kconfig, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kservice, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
