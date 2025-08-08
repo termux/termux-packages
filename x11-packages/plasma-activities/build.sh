26c80524928f5f62beffb3ad309cd9df804620ae
BullyMaguire-lol <temp9040@gmail.com>
2025-08-08 15:55:23 +0530

    Add kf6-kdeclarative, kf6-frameworkintegration, kwayland, plasma-activities, plasma-activities-stats, plasma5support

---
+++ x11-packages/plasma-activities/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/plasma-activities"
+TERMUX_PKG_DESCRIPTION="Core components for KDE Activities"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.4"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/plasma-activities-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="ab810aa594ed3386f6a8564705ea2a46cc62ec367de2039625e030f6af955fe8"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, kf6-kcoreaddons, kf6-kconfig, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, boost, qt6-qtdeclarative, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
