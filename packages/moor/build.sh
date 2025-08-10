301a75c65b64616950f9d21c05404c70a57f5612
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-10 06:40:00 +0000

    fix(main/moor): rename from moar

--- packages/moor/build.sh
+++ packages/moor/build.sh
@@ -1,19 +1,21 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/walles/moar
+TERMUX_PKG_HOMEPAGE=https://github.com/walles/moor
 TERMUX_PKG_DESCRIPTION="A pager designed to just do the right thing without any configuration"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.1"
-TERMUX_PKG_SRCURL=https://github.com/walles/moar/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/walles/moor/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ac6f99ac2dae2aeba5aea56ba79045968501b3708bb2b673e58d7d6118af8309
+TERMUX_PKG_CONFLICTS="moar"
+TERMUX_PKG_REPLACES="moar"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
 	termux_setup_golang
-	go build -trimpath -ldflags="-s -w -X main.versionString=${TERMUX_PKG_VERSION}"
+	go build -trimpath -ldflags="-s -w -X main.versionString=${TERMUX_PKG_VERSION}" ./cmd/moor
 }
 
 termux_step_make_install() {
-	install -Dm700 -t "${TERMUX_PREFIX}/bin" moar
-	install -Dm600 -t "${TERMUX_PREFIX}/share/man/man1" moar.1
+	install -Dm700 -t "${TERMUX_PREFIX}/bin" moor
+	install -Dm600 -t "${TERMUX_PREFIX}/share/man/man1" moor.1
 }
