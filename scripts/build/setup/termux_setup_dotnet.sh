9e01a74333e093d2e346f501a548cdb413f54a0a
Jia Yuan Lo <jylo06g@gmail.com>
2025-08-11 01:07:11 +0800

    scripts(termux_setup_dotnet): address out of sync issue

--- scripts/build/setup/termux_setup_dotnet.sh
+++ scripts/build/setup/termux_setup_dotnet.sh
@@ -1,4 +1,4 @@
-# shellcheck shell=bash disable=SC1091 disable=SC2086 disable=SC2155
+# shellcheck shell=bash disable=SC1091 disable=SC2086 disable=SC2155 disable=SC2164
 termux_setup_dotnet() {
 	# Microsoft distribution of dotnet enables telemetry
 	# This has no effect on Termux dotnet (telemetry is already disabled)
@@ -46,8 +46,7 @@ termux_setup_dotnet() {
 
 	export PATH="${HOME}/.dotnet:${HOME}/.dotnet/tools:${PATH}"
 
-	# Install targeting packs that would not be found
-	# in nuget.org
+	# install targeting packs that would not be found in nuget.org
 	local _DOTNET_ROOT="${TERMUX_PREFIX}/lib/dotnet"
 	if [[ ! -d "${_DOTNET_ROOT}" ]]; then
 		echo "WARN: ${_DOTNET_ROOT} is not a directory! Build may fail! Skipping install symlinks." >&2
@@ -59,13 +58,15 @@ termux_setup_dotnet() {
 	fi
 
 	pushd "${HOME}/.dotnet/packs"
-	local pack targeting_pack
+
+	# point to use our own SDK
+	local targeting_pack version
 	for targeting_pack in "${_DOTNET_ROOT}"/packs/*; do
 		if [[ -d "$(basename "${targeting_pack}")" ]]; then
 			pushd "$(basename "${targeting_pack}")"
-			for pack in "${targeting_pack}"/*; do
-				if [[ ! -e "$(basename "${pack}")" ]]; then
-					ln -fsv "${pack}" .
+			for version in "${targeting_pack}"/*; do
+				if [[ ! -e "$(basename "${version}")" ]]; then
+					ln -fsv "${version}" .
 				fi
 			done
 			popd
@@ -73,9 +74,38 @@ termux_setup_dotnet() {
 			ln -fsv "${targeting_pack}" .
 		fi
 	done
+
+	# ours (older) SDK sometimes out of sync with Microsoft (newer) SDK
+	# usually causes build failure on unofficial supported RID, eg: linux-bionic-x86
+	# so we need to point latest version to what old version we have
+	local dotnet_runtime_versions=$(dotnet --list-runtimes | awk '{ print $2 }' | sort -Vu)
+	local latest_dotnet8_version=$(echo "${dotnet_runtime_versions}" | grep "^8.0." | tail -n1)
+	local latest_dotnet9_version=$(echo "${dotnet_runtime_versions}" | grep "^9.0." | tail -n1)
+	for targeting_pack in "${HOME}"/.dotnet/packs/*; do
+		if [[ -d "$(basename "${targeting_pack}")" ]]; then
+			pushd "$(basename "${targeting_pack}")"
+			for version in "${targeting_pack}"/*; do
+				if [[ "$(basename "${version}")" == "8.0."* ]]; then
+					if [[ -n "${latest_dotnet8_version}" && "$(basename "${version}")" != "${latest_dotnet8_version}" ]]; then
+						rm -fr "${latest_dotnet8_version}"
+						ln -fsvT "$(basename "${version}")" "${latest_dotnet8_version}"
+					fi
+				fi
+				if [[ "$(basename "${version}")" == "9.0."* ]]; then
+					if [[ -n "${latest_dotnet9_version}" && "$(basename "${version}")" != "${latest_dotnet9_version}" ]]; then
+						rm -fr "${latest_dotnet9_version}"
+						ln -fsvT "$(basename "${version}")" "${latest_dotnet9_version}"
+					fi
+				fi
+			done
+			popd
+		fi
+	done
+
 	popd
+
 	pushd "${HOME}/.dotnet"
 	echo "INFO: Installed symbolic links:"
-	find ./packs -maxdepth 3 -type l | sort
+	find ./packs -mindepth 1 -maxdepth 3 -type l | sort
 	popd
 }
