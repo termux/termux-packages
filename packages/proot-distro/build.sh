09a71eaefba0843351aab580d5e563ed1fbaced2
Sylirre <sylirre@termux.dev>
2025-08-10 18:09:47 +0300

    bump(main/proot-distro): 4.27.0

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.26.0
+TERMUX_PKG_VERSION=4.27.0
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=31f75fc2a2a4c41884cac223b65bb8b7cdf51335b464389c98a853f205e576d3
+TERMUX_PKG_SHA256=d4d5e7962c80cfc980d95da207abd8eb03377590edc2ba68c5ab96fe66db6c7b
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, file, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, termux-tools, util-linux, xz-utils"
 TERMUX_PKG_SUGGESTS="bash-completion, termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
