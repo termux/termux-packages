b2997cf13c35ee4a148b9901c4d5dab3a975e2af
Robert Kirkman <rkirkman@termux.dev>
2025-08-10 20:04:06 -0500

    revbump(x11/openimageio): after `openvdb` version 12.1.0
    - After https://github.com/termux/termux-packages/commit/0e350635234558e3ab267f1a21269eca84a13c04, `openimagio` has not yet been rebuilt, and unfortunately that resulted in this error in the reverse dependency in my PR, `blender`: https://github.com/termux-user-repository/tur/issues/1880#issuecomment-3172920686
    
    - Also, enable major+minor version checks for both `openvdb` and `openimageio`

--- x11-packages/openimageio/build.sh
+++ x11-packages/openimageio/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library for reading and writing images, including clas
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.0.9.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/OpenImageIO/oiio/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=bbc5db069b7d8e4e2fccf994d55a262953057745a5e398e44a2e5235a8736d1d
 # configure-time error if ptex and ptex-static are not both installed
@@ -10,6 +11,7 @@ TERMUX_PKG_DEPENDS="boost, dcmtk, ffmpeg, fmt, freetype, glew, libc++, libhdf5, 
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, fontconfig, libjpeg-turbo-static, libpugixml, libxrender, mesa, ptex-static, robin-map"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
 -DCMAKE_CXX_STANDARD=17
 -DUSE_PYTHON=ON
 -DINTERNALIZE_FMT=OFF
@@ -17,3 +19,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DOIIO_BUILD_TESTS=OFF
 -DUSE_EXTERNAL_PUGIXML=ON
 "
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=30
+
+	local v=$(sed -En 's/^set \(OpenImageIO_VERSION "([0-9]+.[0-9]+).*/\1/p' "$TERMUX_PKG_SRCDIR"/CMakeLists.txt)
+	v="${v//./}"
+
+	if [[ "${v}" != "${_SOVERSION}" ]]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
--- x11-packages/openvdb/build.sh
+++ x11-packages/openvdb/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Sparse volume data structure and tools"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="12.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/AcademySoftwareFoundation/openvdb/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=ebb9652ad1d67274e2c85e6736cced5f04e313c5671ae1ae548f174cc76e9e64
 TERMUX_PKG_DEPENDS="boost, imath, libblosc, libtbb, zlib"
@@ -10,6 +11,7 @@ TERMUX_PKG_BUILD_DEPENDS="mesa, glfw, glu"
 TERMUX_PKG_AUTO_UPDATE=true
 # Numpy support requires nanobind which is not packaged at time of writing
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
 -DUSE_IMATH_HALF=ON
 -DUSE_NUMPY=OFF
 -DUSE_LOG4CPLUS=OFF
@@ -17,3 +19,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DOPENVDB_BUILD_DOCS=OFF
 -DOPENVDB_BUILD_UNITTESTS=OFF
 "
+
+termux_step_post_get_source() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION=121
+
+	local major_ver=$(sed -En 's/^set\(OpenVDB_MAJOR_VERSION\s+([0-9]+).*/\1/p' "$TERMUX_PKG_SRCDIR"/CMakeLists.txt)
+	local minor_ver=$(sed -En 's/^set\(OpenVDB_MINOR_VERSION\s+([0-9]+).*/\1/p' "$TERMUX_PKG_SRCDIR"/CMakeLists.txt)
+
+	if [[ "${major_ver}${minor_ver}" != "${_SOVERSION}" ]]; then
+		termux_error_exit "SOVERSION guard check failed."
+	fi
+}
