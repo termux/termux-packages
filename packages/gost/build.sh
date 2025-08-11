dda797f3bc5d52897d3ba4a061b06443694fb36e
Chongyun Lee <licy183@termux.dev>
2025-08-11 23:38:30 +0800

    gost: refactor build commands

--- packages/gost/build.sh
+++ packages/gost/build.sh
@@ -5,20 +5,19 @@ TERMUX_PKG_MAINTAINER="@ian4hu"
 TERMUX_PKG_VERSION="2.12.0"
 TERMUX_PKG_SRCURL=https://github.com/ginuerzh/gost/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ed575807b0490411670556d4471338f418c326bb1ffe25f52977735012851765
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
 
-	export GOPATH=$TERMUX_PKG_BUILDDIR
-	mkdir -p "$GOPATH/src/github.com/ginuerzh"
-	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH/src/github.com/ginuerzh/gost"
-	cd "$GOPATH/src/github.com/ginuerzh/gost/cmd/gost"
-	go build
+	go mod tidy
+
+	go build --ldflags="-s -w" -a -o bin/gost cmd/gost/*.go
 }
 
 termux_step_make_install() {
 	install -Dm700 \
-		"$GOPATH/src/github.com/ginuerzh/gost/cmd/gost/gost" \
+		"$TERMUX_PKG_BUILDDIR/bin/gost" \
 		"$TERMUX_PREFIX/bin/"
 }
