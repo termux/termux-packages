74c5242a4b86b2923f908b30db922a0398073f96
Robert Kirkman <rkirkman@termux.dev>
2025-08-12 19:09:05 -0500

    fix(main/googletest): deduplicate import prefix
    - Fixes #25693

--- packages/googletest/build.sh
+++ packages/googletest/build.sh
@@ -3,13 +3,19 @@ TERMUX_PKG_DESCRIPTION="Google C++ testing framework"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.17.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/google/googletest/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=65fab701d9829d38cb77c14acdc431d2108bfdbf8979e40eb8ae567edf10b27c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_CONFLICTS="libgtest"
 TERMUX_PKG_REPLACES="libgtest"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_SHARED_LIBS=ON"
+# -DCMAKE_INSTALL_INCLUDEDIR=$TERMUX__PREFIX__INCLUDE_SUBDIR
+# is needed in order to fix https://github.com/termux/termux-packages/issues/25693
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_SHARED_LIBS=ON
+-DCMAKE_INSTALL_INCLUDEDIR=$TERMUX__PREFIX__INCLUDE_SUBDIR
+"
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
