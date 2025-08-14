540e790ca8e2d5fe1fabb25081658cd5b29ecbd0
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-08-14 21:50:35 +0800

    chromium: bump to 139.0.7258.127 (#25713)

--- x11-packages/chromium/build.sh
+++ x11-packages/chromium/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.chromium.org/Home
 TERMUX_PKG_DESCRIPTION="Chromium web browser"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=139.0.7258.66
+TERMUX_PKG_VERSION=139.0.7258.127
 TERMUX_PKG_SRCURL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=5abd8ab0189d686789f7c7c61c98800a9962b79c12305bdec0d16fc76929fcb7
+TERMUX_PKG_SHA256=c9a3cf7edb8030245cc378f9949130600610e3a41578c9d02a3b45747790f181
 TERMUX_PKG_DEPENDS="atk, cups, dbus, fontconfig, gtk3, krb5, libc++, libevdev, libxkbcommon, libminizip, libnss, libx11, mesa, openssl, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="chromium-host-tools, libffi-static"
 # TODO: Split chromium-common and chromium-headless
