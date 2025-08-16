4e7fca322d4abe82aa0edf60d10895876b915d98
Jia Yuan Lo <jylo06g@gmail.com>
2025-08-16 14:43:34 +0800

    scripts(auto-update): allow update termux_setup_* scripts
    Also update indents

--- scripts/updates/utils/termux_pkg_upgrade_version.sh
+++ scripts/updates/utils/termux_pkg_upgrade_version.sh
@@ -141,9 +141,14 @@ termux_pkg_upgrade_version() {
 	if [[ "${GIT_COMMIT_PACKAGES}" == "true" ]]; then
 		echo "INFO: Committing package."
 		stderr="$(
-			git add "${TERMUX_PKG_BUILDER_DIR}" 2>&1 >/dev/null
-			git commit -m "bump(${repo}/${TERMUX_PKG_NAME}): ${LATEST_VERSION}" \
-				-m "This commit has been automatically submitted by Github Actions." 2>&1 >/dev/null
+			git add \
+				"${TERMUX_PKG_BUILDER_DIR}" \
+				"${TERMUX_SCRIPTDIR}/scripts/build/setup/" \
+				2>&1 >/dev/null
+			git commit \
+				-m "bump(${repo}/${TERMUX_PKG_NAME}): ${LATEST_VERSION}" \
+				-m "This commit has been automatically submitted by Github Actions." \
+				2>&1 >/dev/null
 		)" || {
 			git reset HEAD --hard
 			termux_error_exit <<-EndOfError
