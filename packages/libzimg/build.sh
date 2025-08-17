93dc6ba615d10d0d1ffa114cbd4e725fd1d93818
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-17 05:13:27 +0000

    fix(main/libzimg): Fix tags for auto update
    %ci:no-build

--- packages/libzimg/build.sh
+++ packages/libzimg/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION="3.0.6"
 TERMUX_PKG_SRCURL=https://github.com/sekrit-twc/zimg/archive/release-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=be89390f13a5c9b2388ce0f44a5e89364a20c1c57ce46d382b1fcc3967057577
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_METHOD=repology
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/.*-//'
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
