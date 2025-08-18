9dca6ea1899ec0da3e083bb1a2cc123c89d9b380
Yaksh Bariya <yakshbari4@gmail.com>
2025-08-18 18:18:54 +0530

    enhance(main/libllvm): enable lldb to be built for 32-bit architectures again
    lldb seems to work for i686 on termux-docker atleast, I assume the
    underlying issue has been fixed now. Let's try to package this again. If
    we receive any complaints, we can go ahead and investigate

--- packages/libllvm/lldb.subpackage.sh
+++ packages/libllvm/lldb.subpackage.sh
@@ -8,6 +8,3 @@ TERMUX_SUBPKG_DESCRIPTION="LLVM-based debugger"
 TERMUX_SUBPKG_DEPENDS="clang, libandroid-spawn, libc++, libedit, libxml2, python, ncurses-ui-libs"
 TERMUX_SUBPKG_BREAKS="lldb-dev, lldb-static"
 TERMUX_SUBPKG_REPLACES="lldb-dev, lldb-static"
-
-# https://github.com/termux/termux-packages/issues/8880
-TERMUX_SUBPKG_EXCLUDED_ARCHES="arm, i686"
