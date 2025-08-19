d27a753467d1bc23818c5cda629ccff12fefbf90
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-19 13:03:26 +0000

    bump(main/wakatime-cli): 1.130.4

--- packages/wakatime-cli/build.sh
+++ packages/wakatime-cli/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wakatime.com/plugins/
 TERMUX_PKG_DESCRIPTION="Command line interface used by all WakaTime text editor plugins"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.130.3"
+TERMUX_PKG_VERSION="1.130.4"
 TERMUX_PKG_SRCURL=https://github.com/wakatime/wakatime-cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7a758b3ef9cf29596580ac056b802dfeaddae15b825e649ed432b57fa515a768
+TERMUX_PKG_SHA256=c849c847bb19ac4e54b91af6bece1f0b0770b144b876b0cb47ff2afb7e6e3cb1
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
