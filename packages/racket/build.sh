7873cc39d6292bb6a22a1a92ab7e819c92b9f835
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-20 16:04:20 +0000

    bump(main/racket): 8.18

--- packages/racket/build.sh
+++ packages/racket/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://racket-lang.org
 TERMUX_PKG_DESCRIPTION="Full-spectrum programming language going beyond Lisp and Scheme"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="8.17"
+TERMUX_PKG_VERSION="8.18"
 TERMUX_PKG_SRCURL=https://www.cs.utah.edu/plt/installers/${TERMUX_PKG_VERSION}/racket-minimal-${TERMUX_PKG_VERSION}-src-builtpkgs.tgz
-TERMUX_PKG_SHA256=a5a7d87aef6899ee9e7466133ea7b88347221a32f6636323f60908b641d10634
+TERMUX_PKG_SHA256=d584811db6e4a95c6c3d0091a17dfd7ae8ddf6b3fb46fd8709d395df9b65b171
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libffi, libiconv"
 TERMUX_PKG_NO_DEVELSPLIT=true
@@ -33,4 +33,12 @@ termux_step_pre_configure() {
 	CPPFLAGS+=" -I$TERMUX_PKG_SRCDIR/src/bc/include -I$TERMUX_PKG_BUILDDIR/bc"
 	LDFLAGS+=" -liconv -llog"
 	export TERMUX_PKG_SRCDIR="$TERMUX_PKG_SRCDIR/src"
+}
+
+# See: https://github.com/termux/termux-packages/issues/25761
+termux_step_configure() {
+	"$TERMUX_PKG_SRCDIR"/configure \
+		--prefix="$TERMUX_PREFIX" \
+		--host=$TERMUX_HOST_PLATFORM \
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
