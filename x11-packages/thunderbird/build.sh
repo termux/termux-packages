29ca55449eb39ef1f19349dfc1bca6840b664388
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-08-21 02:14:38 +0000

    thunderbird: bump to 142.0

--- x11-packages/thunderbird/build.sh
+++ x11-packages/thunderbird/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.thunderbird.net
 TERMUX_PKG_DESCRIPTION="Unofficial Thunderbird email client"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="141.0"
+TERMUX_PKG_VERSION="142.0"
 TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}/source/thunderbird-${TERMUX_PKG_VERSION}.source.tar.xz"
-TERMUX_PKG_SHA256=694a6b11f8afa9ff2b0899f1f59473160b3a92be271892df8c2f6bb748fb4666
+TERMUX_PKG_SHA256=18f625452d2001ef1b5a1b47a77521c73c83e1bae824f783a0965d229c434e37
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libotr, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -99,7 +99,6 @@ END
 	fi
 
 	./mach configure
-	./mach tb-rust vendor
 }
 
 termux_step_make() {
