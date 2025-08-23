12e51c0e1f24b21ad757a9dacc9994faac070866
Robert Kirkman <rkirkman@termux.dev>
2025-08-23 01:47:21 -0500

    fix(ndk): fix on-device building and remove custom `__ANDROID_API__` header patches
    - Fixes https://github.com/termux/termux-packages/issues/26838
    
    - Fixes partly https://github.com/termux/termux-packages/issues/23401
    
    - After https://github.com/termux/termux-packages/pull/25627, it is believed that the custom definition of `__ANDROID_API__` is no longer needed, because:
      - There is no longer a way to reproduce any of the errors that it fixed before
      - Even if there were a way to, it would no longer be able to fix those errors by itself because of the update to NDK r28c (see https://github.com/termux/termux-packages/issues/25622 for more details)
      - It is replaced with https://github.com/termux/termux-packages/pull/25627, which does what it used to a different way that also fixes other errors and seems more reliable

--- scripts/build/termux_step_setup_toolchain.sh
+++ scripts/build/termux_step_setup_toolchain.sh
@@ -10,7 +10,7 @@ termux_step_setup_toolchain() {
 			TERMUX_STANDALONE_TOOLCHAIN+="-v2"
 			termux_setup_toolchain_28c
 		elif [ "${TERMUX_NDK_VERSION}" = 23c ]; then
-			TERMUX_STANDALONE_TOOLCHAIN+="-v8"
+			TERMUX_STANDALONE_TOOLCHAIN+="-v9"
 			termux_setup_toolchain_23c
 		else
 			termux_error_exit "We do not have a setup_toolchain function for NDK version $TERMUX_NDK_VERSION"
