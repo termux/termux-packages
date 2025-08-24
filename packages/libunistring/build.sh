aa55d5550262d9bef7893eab61be8792309f2c28
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-24 17:06:20 +0200

    chore(main/libunistring,nano,tar,unzip,zlib,zstd): Rebuild
    Rebuild some core packages to get 16 kb page size (#21688).

--- packages/libunistring/build.sh
+++ packages/libunistring/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library providing functions for manipulating Unicode str
 TERMUX_PKG_LICENSE="LGPL-3.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libunistring/libunistring-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f245786c831d25150f3dfb4317cda1acc5e3f79a5da4ad073ddca58886569527
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv"
