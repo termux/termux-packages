4f302b5584d8c5c68b8e7fc921dee7fe5eef903f
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-25 01:59:48 +0200

    chore(main/libgpg-error,libidn2,libksba,libmpfr,patch,pcre2): Rebuild
    Rebuild some core packages to get 16 kb page size (#21688).

--- packages/patch/build.sh
+++ packages/patch/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GNU patch which applies diff files to create patched fil
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.8
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/patch/patch-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=f87cee69eec2b4fcbf60a396b030ad6aa3415f192aa5f7ee84cad5e11f7f5ae3
 TERMUX_PKG_AUTO_UPDATE=true
