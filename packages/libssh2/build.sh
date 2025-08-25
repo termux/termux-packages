cdf34f8a446b488ed270543d05c3beedf0b2d20d
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-25 02:02:47 +0200

    chore(main/libnghttp{2,3},libnpth,libssh2,libxml2,libxslt): Rebuild
    Rebuild some core packages to get 16 kb page size (#21688).

--- packages/libssh2/build.sh
+++ packages/libssh2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Client-side library implementing the SSH2 protocol"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.11.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.libssh2.org/download/libssh2-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=d9ec76cbe34db98eec3539fe2c899d26b0c837cb3eb466a56b0f109cabf658f7
 TERMUX_PKG_DEPENDS="openssl, zlib"
