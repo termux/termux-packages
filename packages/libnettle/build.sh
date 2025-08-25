9bd63886ba6c6d64252aaf719507297ed0883360
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-25 02:22:30 +0200

    chore(main/liblz4,libmd,libnettle,libtirpc,libunbound): Rebuild
    Rebuild some core packages to get 16 kb page size (#21688).

--- packages/libnettle/build.sh
+++ packages/libnettle/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Cryptographic library that is designed to fit easily in 
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.10.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/nettle/nettle-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fe9ff51cb1f2abb5e65a6b8c10a92da0ab5ab6eaf26e7fc2b675c45f1fb519b5
 TERMUX_PKG_AUTO_UPDATE=true
