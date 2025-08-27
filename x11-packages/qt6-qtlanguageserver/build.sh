a432fcdda110f2156f2e436fb0a2e2418476e8c7
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-27 05:12:07 +0000

    bump(x11/qt6-qtlanguageserver): 6.9.2

--- x11-packages/qt6-qtlanguageserver/build.sh
+++ x11-packages/qt6-qtlanguageserver/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.qt.io/
 TERMUX_PKG_DESCRIPTION="An implementation of the Language Server Protocol"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.9.1"
+TERMUX_PKG_VERSION="6.9.2"
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/qtlanguageserver-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=ca17353612760b9bb6e3115bfacc9ba5e06d0d18b522f4f62ae6d510d63488ec
+TERMUX_PKG_SHA256=781b6eafe1fe1ab7e942fb15dc220b79c602db6efdce7fdc272bf8512c068bee
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase (>= ${TERMUX_PKG_VERSION})"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_NO_STATICSPLIT=true
