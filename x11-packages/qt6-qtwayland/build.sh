c0089506b41edf00211b239b956ee08fa2c5cacc
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-27 07:46:23 +0000

    bump(x11/qt6-qtwayland): 6.9.2

--- x11-packages/qt6-qtwayland/build.sh
+++ x11-packages/qt6-qtwayland/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.qt.io/
 TERMUX_PKG_DESCRIPTION="Provides APIs for Wayland"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.9.1"
+TERMUX_PKG_VERSION="6.9.2"
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/qtwayland-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=7d21ea0e687180ebb19b9a1f86ae9cfa7a25b4f02d5db05ec834164409932e3e
+TERMUX_PKG_SHA256=cad79806565568f12f9983fed69219416abcee9d5deef4abdfcf94aa2eef7781
 TERMUX_PKG_DEPENDS="libc++, libglvnd, libwayland, libxkbcommon, qt6-qtbase (>= ${TERMUX_PKG_VERSION}), qt6-qtdeclarative (>= ${TERMUX_PKG_VERSION}), qt6-qtsvg (>= ${TERMUX_PKG_VERSION})"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_NO_STATICSPLIT=true
