4edd980026dc7920fb8f99521ef34b1a3a8e39a8
BullyMaguire-lol <temp9040@gmail.com>
2025-08-29 11:13:21 +0530

    addpkg(x11/spectacle): 6.4.4
    Co-authored-by: TomIO <43716232+TomJo2000@users.noreply.github.com>

---
+++ x11-packages/spectacle/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE="https://apps.kde.org/spectacle/"
+TERMUX_PKG_DESCRIPTION="KDE screenshot capture utility"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.4"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/spectacle-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="9108a739c1acc43b23e0a6aa17ba4194a564ab1548c6887328c76a073bf3d373"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, kf6-kconfig, kf6-kconfigwidgets, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kglobalaccel, kf6-kguiaddons, kf6-ki18n, kf6-kio, kf6-kirigami, kf6-kjobwidgets, kf6-knotifications, kpipewire, kf6-kservice, kf6-kstatusnotifieritem, kf6-kwidgetsaddons, kf6-kwindowsystem, kf6-kxmlgui, layer-shell-qt, libxcb, opencv (< 5), kf6-prison, kf6-purpose, qt6-qtbase, qt6-qtdeclarative, qt6-qtimageformats, qt6-qtmultimedia, qt6-qtwayland, libwayland, xcb-util, xcb-util-cursor, xcb-util-image"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, plasma-wayland-protocols"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DCMAKE_CXX_FLAGS=-I$TERMUX_PREFIX/include/opencv4
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
