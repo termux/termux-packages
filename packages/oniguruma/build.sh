19f2e95e93690b581deec87f755fd52394d71e66
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-29 17:22:04 +0200

    chore(main/jq,make,oniguruma,ruby): Rebuild

--- packages/oniguruma/build.sh
+++ packages/oniguruma/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/kkos/oniguruma
 TERMUX_PKG_DESCRIPTION="Regular expressions library"
 TERMUX_PKG_VERSION="6.9.10"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kkos/oniguruma/releases/download/v$TERMUX_PKG_VERSION/onig-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=2a5cfc5ae259e4e97f86b68dfffc152cdaffe94e2060b770cb827238d769fc05
 TERMUX_PKG_AUTO_UPDATE=true
