06ed652c3f3309c60d8215fba3ef920997c44b15
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-29 20:42:51 +0200

    chore(main/busybox,libpopt,mandoc,rsync,runit): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast incremental file transfer utility"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION=3.4.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=2924bcb3a1ed8b551fc101f740b9f0fe0a202b115027647cf69850d65fd88c52
 TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl, openssl-tool, xxhash, zlib, zstd"
