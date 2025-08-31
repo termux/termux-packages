983e5b53d942308659b9019fa8c1d1124f02ccda
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-31 06:32:58 +0000

    bump(x11/icewm): 3.9.0
    See the dependency changes in https://github.com/ice-wm/icewm/releases/tag/3.9.0

--- x11-packages/icewm/build.sh
+++ x11-packages/icewm/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://ice-wm.org/
 TERMUX_PKG_DESCRIPTION="Window manager with goals of speed, simplicity, and usability"
 TERMUX_PKG_LICENSE="LGPL-2.0-only"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.8.2"
+TERMUX_PKG_VERSION="3.9.0"
 TERMUX_PKG_SRCURL="https://github.com/ice-wm/icewm/releases/download/$TERMUX_PKG_VERSION/icewm-$TERMUX_PKG_VERSION.tar.lz"
-TERMUX_PKG_SHA256=c64b1b7c8ee262a3c4796be57349a3198998564d70ed26f11e724e1b73d48394
+TERMUX_PKG_SHA256=1323527a9a49db66e9ce7b08e6ec43c87700243432fc6679681df367c67b2dc0
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="alsa-lib, imlib2, libandroid-glob, libandroid-wordexp, librsvg, libsndfile, libxcomposite, libxdamage, libxinerama, libxpm, libxrandr, libxres"
+TERMUX_PKG_DEPENDS="alsa-lib, imlib2, libandroid-glob, libandroid-wordexp, libice, librsvg, libsm, libsndfile, libxcomposite, libxcursor, libxdamage, libxinerama, libxrandr, libxres"
 TERMUX_PKG_BUILD_DEPENDS="aosp-libs"
 TERMUX_PKG_SUGGESTS="xdg-menu"
 
