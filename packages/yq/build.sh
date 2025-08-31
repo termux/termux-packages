24e2d7b915a2f002a16c407bdf3e690eb1e04713
TomIO <tom@termux.dev>
2025-08-31 22:00:44 +0200

    rmpkg(main/xpup): unmaintained and doesn't build with Go 1.25.0 We're also the only place packaging it. https://repology.org/project/xpup/history

--- packages/yq/build.sh
+++ packages/yq/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="A lightweight and portable command-line YAML, JSON and X
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.47.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/mikefarah/yq/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=48ef09b9ffdb80f26ee516fa2fc83f713c1b49f503cedef79a96435509d456af
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_REPLACES="xpup"
 
 termux_step_make() {
 	termux_setup_golang
