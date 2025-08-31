3ae39d7519d1c3b387fc1f440c09be6e534b2847
Fredrik Fornwall <fredrik@fornwall.net>
2025-08-31 23:52:07 +0200

    chore(main/bc,fish,fontconfig,fribidi,libx264): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/fontconfig/build.sh
+++ packages/fontconfig/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library for configuring and customizing font access"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.17.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/$TERMUX_PKG_VERSION/fontconfig-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=82e73b26adad651b236e5f5d4b3074daf8ff0910188808496326bd3449e5261d
 TERMUX_PKG_AUTO_UPDATE=true
