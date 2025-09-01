3468f163d7b829f88bef89f6bcdaf2546d72854b
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-09-01 20:27:13 +0530

    addpkg (x11/libadapta): 1.5.0

---
+++ x11-packages/libadapta/libadapta-demo.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/adapta-1-demo
+share/applications/org.gnome.Adapta1.Demo.desktop
+share/icons/hicolor/scalable/apps/org.gnome.Adapta1.Demo.svg
+share/icons/hicolor/symbolic/apps/org.gnome.Adapta1.Demo-symbolic.svg
+share/metainfo/org.gnome.Adapta1.Demo.metainfo.xml
+"
+TERMUX_SUBPKG_DESCRIPTION="Example programs for libadapta"
