3be0961b6498f08d2ad7b1d018f5be81a421d658
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-09-01 20:35:30 +0530

    addpkg (x11/python3-xapp): 2.4.2

---
+++ x11-packages/python3-xapp/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/linuxmint/python3-xapp
+TERMUX_PKG_DESCRIPTION="XApp library Python bindings"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2.4.2
+TERMUX_PKG_SRCURL=https://github.com/linuxmint/python3-xapp/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=39e4c3f06732e9d197b9aed31444653da2976c1d66dded870b52cc9782f2237d
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="python, xapp"
+
+termux_step_configure() {
+	termux_setup_meson
+	export PYTHON_SITELIB="$TERMUX_PYTHON_HOME/site-packages"
+	$TERMUX_MESON setup "$TERMUX_PKG_BUILDDIR" "$TERMUX_PKG_SRCDIR" \
+		-Dpython.purelibdir="$PYTHON_SITELIB"
+}
