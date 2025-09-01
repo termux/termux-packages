046a2d48c9d710bfe46014f4214724340ff8b64a
TomIO <tom@termux.dev>
2025-09-01 20:47:10 +0200

    fix(scripts/termux_pkg_upgrade_version): handle `-suffix` versions

--- packages/asciinema/build.sh
+++ packages/asciinema/build.sh
@@ -2,8 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://asciinema.org/
 TERMUX_PKG_DESCRIPTION="Record and share your terminal sessions, the right way"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.0.0-rc.5"
-TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${TERMUX_PKG_VERSION//\~/-}.tar.gz
+TERMUX_PKG_VERSION="1:3.0.0~rc.5"
+: "${TERMUX_PKG_VERSION:2}" # We need to remove both the epoch and the '~' from the version
+TERMUX_PKG_SRCURL=https://github.com/asciinema/asciinema/archive/v${_//\~/-}.tar.gz
 TERMUX_PKG_SHA256=31b56c062309316a961eb4fa86b73b7a478ac0a8c89ab44d978958e81a420dd0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
