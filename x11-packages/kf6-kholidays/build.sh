063100b22632682acf47fb3d5e312bf65b0bc716
BullyMaguire-lol <temp9040@gmail.com>
2025-09-02 12:09:21 +0530

    addpkg(x11/kf6-kholidays): 6.22.0

---
+++ x11-packages/kf6-kholidays/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kholidays"
+TERMUX_PKG_DESCRIPTION="KDE library for regional holiday information"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.22.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kholidays-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="39b7f1c713d6c5ae225bfa18a6dece20ff7a7f4c325a0c6fc4a48cf3e4e0a690"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qtdeclarative, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
