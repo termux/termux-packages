5243bbd4a3d5e4c1b4df60c08204e72428108b13
Robert Kirkman <rkirkman@termux.dev>
2025-09-02 07:58:48 -0500

    fix(x11/cinnamon): disable auto update
    - Unfortunately, the auto update system messed up the bumps of some of these packages: https://github.com/termux/termux-packages/commit/d9a9c4160d80465530ce7b5c9e6ea11430072a7d, so disable auto update on them until the auto update functionality can be fully implemented for them.

--- x11-packages/gnome-backgrounds/build.sh
+++ x11-packages/gnome-backgrounds/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="A collection of GNOME desktop wallpapers"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=48.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/GNOME/gnome-backgrounds/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=33523d10fded9b32776710db3bdfe8120b02d2836c321e229285d7e716e0da3a
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false
 
 termux_step_pre_configure() {
 	termux_setup_meson
--- x11-packages/mint-y-icon-theme/build.sh
+++ x11-packages/mint-y-icon-theme/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="The Mint-Y icon theme for cinnamon"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.8.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/linuxmint/mint-y-icons/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7f50ed723fe02eb4972aaf53cacf838e0bb90995814530c991f6de6ab0d77d9b
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="hicolor-icon-theme, adwaita-icon-theme, adwaita-icon-theme-legacy, mint-x-icon-theme, gtk-update-icon-cache"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
