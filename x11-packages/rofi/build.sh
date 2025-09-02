36f17c490664285d1dbaac2a3140a97d7bd16653
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-02 15:29:51 +0000

    bump(x11/rofi): 2.0.0
    * Add wayland dependency for the new rofi-wayland merge.
    * Add workaround to generate version header file due to cross-compilation.

--- x11-packages/rofi/build.sh
+++ x11-packages/rofi/build.sh
@@ -2,18 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://davatorium.github.io/rofi/
 TERMUX_PKG_DESCRIPTION="A window switcher, application launcher and dmenu replacement"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.7.9.1"
+TERMUX_PKG_VERSION="2.0.0"
 TERMUX_PKG_SRCURL="https://github.com/davatorium/rofi/releases/download/$TERMUX_PKG_VERSION/rofi-$TERMUX_PKG_VERSION.tar.xz"
-TERMUX_PKG_SHA256=04c128f8c56f043cd229545285ee773322d0eafaffad100b8604338108c5f5ec
+TERMUX_PKG_SHA256=6a55ee27f189ef9a1435cea329b146805b5dc830d8abc7a08c50a971521a8d8d
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, libandroid-glob, libcairo, libxcb, libxkbcommon, pango, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm"
-TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-check"
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, libandroid-glob, libcairo, libwayland, libxcb, libxkbcommon, pango, startup-notification, xcb-util, xcb-util-cursor, xcb-util-keysyms, xcb-util-wm"
 
 termux_step_pre_configure() {
-	export LIBS="-landroid-glob"
-	export GLIB_GENMARSHAL=glib-genmarshal
-	export GOBJECT_QUERY=gobject-query
-	export GLIB_MKENUMS=glib-mkenums
-	export GLIB_COMPILE_RESOURCES=glib-compile-resources
+	termux_setup_glib_cross_pkg_config_wrapper
+
+	# ld.lld: error: undefined symbol: glob, globfree
+	LDFLAGS+=" -landroid-glob"
+
+	sed \
+	"s|@TERMUX_PKG_VERSION@|${TERMUX_PKG_VERSION}|g" \
+	"$TERMUX_PKG_BUILDER_DIR"/nkutils-git-version.h.in > "$TERMUX_PKG_SRCDIR"/subprojects/libnkutils/core/include/nkutils-git-version.h
 }
