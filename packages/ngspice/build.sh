f26d93991753fce79e90e838bc8f4f74a3f34938
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-02 05:24:54 +0000

    bump(main/ngspice): 45

--- packages/ngspice/build.sh
+++ packages/ngspice/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A mixed-level/mixed-signal circuit simulator"
 TERMUX_PKG_LICENSE="BSD 3-Clause, LGPL-2.1"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="44.2"
+TERMUX_PKG_VERSION="45"
 TERMUX_PKG_SRCURL=https://github.com/imr/ngspice/archive/refs/tags/ngspice-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=74c4338b96b746327adf6a4ffd0312c19692c8244061ca7c16f249167a6b79b7
+TERMUX_PKG_SHA256=f32435556d063419442c264bb6756df9663c57a85cd3d286847b8c21ab5c498b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_UPDATE_VERSION_SED_REGEXP="s/ngspice-//"
@@ -36,6 +36,10 @@ termux_step_host_build() {
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -fopenmp -static-openmp"
+
+	# ERROR: ./lib/ngspice/ivlng.vpi contains undefined symbols: pow
+	LDFLAGS+=" -lm"
+
 	autoreconf -fi
 }
 
