9cd85a0f9e625e4d1a225f834be705bf4147812a
BullyMaguire-lol <temp9040@gmail.com>
2025-09-02 11:54:19 +0530

    addpkg(x11/kf6-kdeclarative): 6.20.0

---
+++ x11-packages/kf6-kdeclarative/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kdeclarative"
+TERMUX_PKG_DESCRIPTION="Provides integration of QML and KDE Frameworks"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.20.0"
+_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kdeclarative-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="b01c7fa0272dd2cccc962a606e63328825bd3b8c8ec08ee36c3cea75fa8a314d"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kconfig, kf6-ki18n, kf6-kguiaddons, kf6-kwidgetsaddons, kf6-kglobalaccel, libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools, qt6-shadertools, spirv-tools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
