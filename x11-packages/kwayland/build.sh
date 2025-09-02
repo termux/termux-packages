5ab2f0f2ff764c8e6d1bce51d6e0567e263e570d
BullyMaguire-lol <temp9040@gmail.com>
2025-09-02 11:56:20 +0530

    addpkg(x11/kwayland): 6.4.4

---
+++ x11-packages/kwayland/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/kwayland"
+TERMUX_PKG_DESCRIPTION="Qt-style Client and Server library wrapper for Wayland libraries"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.4"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/kwayland-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="f458ae7ed1118e1a2165a37378c8ce0e9fff80d1a4cfe86217f06cf24ca49f9b"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtwayland, libwayland"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools, plasma-wayland-protocols, libwayland-protocols"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
