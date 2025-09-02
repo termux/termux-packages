979ad686b206dde0feb32e0008bbd763cc1ff7dc
BullyMaguire-lol <temp9040@gmail.com>
2025-09-02 12:09:42 +0530

    addpkg(x11/kf6-kunitconversion): 6.22.0

---
+++ x11-packages/kf6-kunitconversion/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kunitconversion"
+TERMUX_PKG_DESCRIPTION="Support for unit conversion"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.22.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kunitconversion-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="580357277fad7659d33555f25783a6cd27286b73cdd74c781b4989e4f6247b91"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-ki18n, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_PYTHON_BINDINGS=OFF
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
