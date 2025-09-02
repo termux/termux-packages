069aeb5d47b20440ad94be8fc4fc26d986743629
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-02 17:45:16 +0200

    revbump(x11/thunderbird): Rebuild after libvpx bump (#25916)

--- x11-packages/thunderbird/build.sh
+++ x11-packages/thunderbird/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Unofficial Thunderbird email client"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="142.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/thunderbird/releases/${TERMUX_PKG_VERSION}/source/thunderbird-${TERMUX_PKG_VERSION}.source.tar.xz"
 TERMUX_PKG_SHA256=18f625452d2001ef1b5a1b47a77521c73c83e1bae824f783a0965d229c434e37
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libotr, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
