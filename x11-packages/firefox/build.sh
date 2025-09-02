4f7256b440f15b94d90fc995df65faf8b9a9ee99
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-02 17:44:29 +0200

    revbump(x11/firefox): Rebuild after libvpx bump (#25916)

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="142.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION}/source/firefox-${TERMUX_PKG_VERSION}.source.tar.xz
 TERMUX_PKG_SHA256=b0adb44ed4c3383e752a5947adbfb0d03f24172cb468831bd49978de25e810c0
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
