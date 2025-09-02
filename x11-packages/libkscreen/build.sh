57f17f7c5697ade975446dab56f493b4fe85b770
BullyMaguire-lol <temp9040@gmail.com>
2025-09-02 12:11:25 +0530

    addpkg(x11/libkscreen): 6.4.5

---
+++ x11-packages/libkscreen/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/libkscreen"
+TERMUX_PKG_DESCRIPTION="KDE screen management software"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.5"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/libkscreen-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="68d26e27656b450f15a78f44784c50a22034e57c14f96a402019d52bc5b3e2b4"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, libwayland, libxcb, qt6-qtbase, qt6-qtwayland"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, plasma-wayland-protocols, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
