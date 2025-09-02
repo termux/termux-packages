a780dc5a64a3695219c7179f79d1a538b37f5871
BullyMaguire-lol <temp9040@gmail.com>
2025-09-02 15:54:37 +0530

    addpkg(x11/kf6-kdnssd): 6.18.0

---
+++ x11-packages/kf6-kdnssd/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kdnssd"
+TERMUX_PKG_DESCRIPTION="Abstraction to system DNSSD features"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.18.0"
+_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kdnssd-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="c48f2881d9664c0ee12b7091c56cbb1e959a7e960cf2d52c6c87e6fcaaf13c7f"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
