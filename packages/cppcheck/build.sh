dd83bfc24a7414a6c462c82aeb5c210d50a0a4be
X1DoA <184099697+x1doa@users.noreply.github.com>
2025-09-03 14:47:23 +0000

    bump(main/cppcheck): 2.18.2

--- packages/cppcheck/build.sh
+++ packages/cppcheck/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/danmar/cppcheck
 TERMUX_PKG_DESCRIPTION="tool for static C/C++ code analysis"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.18.1"
+TERMUX_PKG_VERSION="2.18.2"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology # Upstream only releases major versions theough GitHub. Other minor updates are released using git tags, better rely on repology for updated versiom
 TERMUX_PKG_SRCURL=https://github.com/danmar/cppcheck/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=528841c0f00de5ed41428269df7a30b102af0b1f8ad50f5b7d4ee2997b54c04c
+TERMUX_PKG_SHA256=81dcb33767b4a6959698901d7a63e52e13923f90c6a37f60645ae92917e2fa20
 TERMUX_PKG_DEPENDS="libc++"
 
 # Prevent running dmake during builds. dmake just generates Makefile which we
