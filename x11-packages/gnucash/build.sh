7f9584a95716c6210f08dd243ca7a8dd2fe4935d
Adam Cozzette <acozzette@google.com>
2025-09-04 21:05:05 -0700

    Apply suggestions from code review
    Co-authored-by: TomIO <43716232+TomJo2000@users.noreply.github.com>

--- x11-packages/gnucash/build.sh
+++ x11-packages/gnucash/build.sh
@@ -1,14 +1,14 @@
 TERMUX_PKG_HOMEPAGE="https://gnucash.org"
 TERMUX_PKG_DESCRIPTION="Personal and small-business financial-accounting software"
-# Licenses: GPL-2.0-or-later, GPL 2 or 3, public domain, special exception for
-# linking OpenSSL.
-TERMUX_PKG_LICENSE="custom"
-TERMUX_PKG_MAINTAINER="@acozzette"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later" # with OpenSSL linking exceptions
+TERMUX_PKG_LICENSE_FILE="LICENSE"     # specified for additional nuance.
+TERMUX_PKG_MAINTAINER="@acozzette <acozzette@google.com>"
 TERMUX_PKG_VERSION="5.12"
-TERMUX_PKG_SRCURL="https://github.com/Gnucash/gnucash/releases/download/5.12/gnucash-5.12.tar.bz2"
+TERMUX_PKG_SRCURL="https://github.com/Gnucash/gnucash/releases/download/${TERMUX_PKG_VERSION}/gnucash-${TERMUX_PKG_VERSION}.tar.bz2"
 TERMUX_PKG_SHA256="b35b4756be12bcfdbed54468f30443fa53f238520a9cead5bde2e6c4773fbf39"
 TERMUX_PKG_DEPENDS="boost, gettext, guile, glib, gtk3, libxml2, libxslt, perl, swig, webkit2gtk-4.1, xsltproc, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, googletest"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DWITH_SQL=OFF
 -DWITH_OFX=OFF
