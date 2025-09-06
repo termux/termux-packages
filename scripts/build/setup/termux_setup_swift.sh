b0a64dde26d93b0e0ab1ec0df15bc97478d4048d
Finagolfin <finagolfin@tuta.io>
2025-09-06 11:38:50 +0530

    bump(swift): 6.1.2 to 6.1.3
    Also, update to build the Swift stdlib with NDK 28 and fix CMake 4 issue with llbuild.

--- scripts/build/setup/termux_setup_swift.sh
+++ scripts/build/setup/termux_setup_swift.sh
@@ -19,7 +19,7 @@ termux_setup_swift() {
 			termux_download \
 				https://download.swift.org/swift-$TERMUX_SWIFT_VERSION-release/ubuntu2404/swift-$TERMUX_SWIFT_VERSION-$SWIFT_RELEASE/$SWIFT_BIN.tar.gz \
 				$SWIFT_TAR \
-				d749d5fe2d6709ee988e96b16f02bca7b53304d09925e31063fd5ec56019de9f
+				f19a228960e1d8174db796dcea3defe6963b588af45fa3ddf0412adbe4b7bb84
 
 			(cd $TERMUX_PKG_TMPDIR ; tar xf $SWIFT_TAR ; mv $SWIFT_BIN $SWIFT_FOLDER; rm $SWIFT_TAR)
 		fi
