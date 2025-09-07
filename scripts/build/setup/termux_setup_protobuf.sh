1701bdf8086290be12af59ccf7539ffd715705af
Robert Kirkman <rkirkman@termux.dev>
2025-09-06 21:45:42 -0500

    bump(main/libprotobuf): 32.0
    - Includes revision-bumps of all reverse dependencies; only one package, `apache-orc`, has a build failure because of newer `libprotobuf`. Its patch is copied and pasted from this PR: https://github.com/apache/orc/pull/2316
    
    - `protobuf-static` is also bumped. It is an orphaned package and there have been, in the past several months since
      https://github.com/termux/termux-packages/pull/24131, **no known cases of anyone encountering any issues for which they need `protobuf-static` anymore**. This strongly suggests that the **new `libandroid-stub` invented by twaik** that I helped with https://github.com/termux/termux-packages/pull/23712 is probably a completely successful workaround in Android to this upstream `libprotobuf` issue: https://github.com/protocolbuffers/protobuf/issues/5254. However, because the original bug is very rare and only affects a very few Android devices, and among those few Android device where it is reproducible, there are some subtle variations in the content of the protobuf error message, for the time being, I think it is probably a good idea to keep `protobuf-static` unchanged, and eventually, maybe in 1 year from now, if still nobody has ever needed `protobuf-static` at that time, it can be completely disabled.

--- scripts/build/setup/termux_setup_protobuf.sh
+++ scripts/build/setup/termux_setup_protobuf.sh
@@ -1,27 +1,28 @@
 termux_setup_protobuf() {
 	local _PROTOBUF_VERSION=$(bash -c ". $TERMUX_SCRIPTDIR/packages/libprotobuf/build.sh; echo \${TERMUX_PKG_VERSION#*:}")
-	local _PROTOBUF_ZIP=protoc-$_PROTOBUF_VERSION-linux-x86_64.zip
+	local _PROTOBUF_SHA256=7ca037bfe5e5cabd4255ccd21dd265f79eb82d3c010117994f5dc81d2140ee88
+	local _PROTOBUF_ZIP="protoc-$_PROTOBUF_VERSION-linux-x86_64.zip"
 	local _PROTOBUF_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
-		_PROTOBUF_FOLDER=${TERMUX_SCRIPTDIR}/build-tools/protobuf-${_PROTOBUF_VERSION}
+		_PROTOBUF_FOLDER="${TERMUX_SCRIPTDIR}/build-tools/protobuf-${_PROTOBUF_VERSION}"
 	else
-		_PROTOBUF_FOLDER=${TERMUX_COMMON_CACHEDIR}/protobuf-${_PROTOBUF_VERSION}
+		_PROTOBUF_FOLDER="${TERMUX_COMMON_CACHEDIR}/protobuf-${_PROTOBUF_VERSION}"
 	fi
 
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
 		if [ ! -d "$_PROTOBUF_FOLDER" ]; then
 			termux_download \
-				https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP \
-				$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP \
-				2fbbc1818463d7e6d93c19a8dea839e663ca5f8579a52ef78c7688188335fa6c
+				"https://github.com/protocolbuffers/protobuf/releases/download/v$_PROTOBUF_VERSION/$_PROTOBUF_ZIP" \
+				"$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP" \
+				"$_PROTOBUF_SHA256"
 
 			rm -Rf "$TERMUX_PKG_TMPDIR/protoc-$_PROTOBUF_VERSION-linux-x86_64"
-			unzip $TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP -d $TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION
+			unzip "$TERMUX_PKG_TMPDIR/$_PROTOBUF_ZIP" -d "$TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION"
 			mv "$TERMUX_PKG_TMPDIR/protobuf-$_PROTOBUF_VERSION" \
-				$_PROTOBUF_FOLDER
+				"$_PROTOBUF_FOLDER"
 		fi
 
-		export PATH=$_PROTOBUF_FOLDER/bin/:$PATH
+		export PATH="$_PROTOBUF_FOLDER/bin/:$PATH"
 	fi
 }
