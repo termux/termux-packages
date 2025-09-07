d9ea516a8a9aa770245dbb3cade540fcebeb448a
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-07 23:44:19 +0200

    chore(main/apr-util,aptly,argon2,argp): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/aptly/build.sh
+++ packages/aptly/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A Swiss Army knife for Debian repository management"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.6.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/aptly-dev/aptly/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=cadfabda2a59f397adfe6f9ce3c9ddc6fe4c6052f0e03a300ba1f22d7cf0e09a
 TERMUX_PKG_AUTO_UPDATE=true
