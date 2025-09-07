47f9023946d192a86b90a58bbd5fe120ccce19f1
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-07 23:44:14 +0200

    chore(main/abootimg,adms,age,agg): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/age/build.sh
+++ packages/age/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A simple, modern and secure encryption tool with small e
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1:1.2.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/FiloSottile/age/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SHA256=93bd89a16c74949ee7c69ef580d8e4cf5ce03e7d9c461b68cf1ace3e4017eef5
