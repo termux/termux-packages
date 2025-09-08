dacba187592b32fcdd5339d5b55ca73d2303d5d6
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-08 18:20:10 +0200

    chore(main/ctags,ctypes-sh,cuetools,cups): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/ctags/build.sh
+++ packages/ctags/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Universal ctags: Source code index builder"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2:6.2.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/universal-ctags/ctags/archive/refs/tags/v${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=313c864bb19c7da79aea77b94db1bb44d14c1f88b992285d7ea8968b3cbc125f
 TERMUX_PKG_AUTO_UPDATE=true
