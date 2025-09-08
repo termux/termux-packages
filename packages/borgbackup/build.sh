e4830c17664e8574fe9acdd07df52ec05b64c57b
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-08 14:30:04 +0200

    chore(main/borgbackup,boxes,brainfuck,brogue): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/borgbackup/build.sh
+++ packages/borgbackup/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Deduplicating and compressing backup program"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="1.4.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/borgbackup/borg/releases/download/${TERMUX_PKG_VERSION}/borgbackup-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b8fbf8f1c19d900b6b32a5a1dc131c5d8665a7c7eea409e9095209100b903839
 TERMUX_PKG_DEPENDS="libacl, liblz4, openssl, python, xxhash, zstd"
