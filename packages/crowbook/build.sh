a5ae50063425af16d1958bdd817e452647422658
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-08 18:20:09 +0200

    chore(main/cronie,crowbook,crunch,cryptopp): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/crowbook/build.sh
+++ packages/crowbook/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Allows you to write a book in Markdown without worrying 
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.17.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/crowdagger/crowbook/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=13c628722fedf1bfebc0fd334e9ffa41a62888e3195d2ad1e5ea851694dc4a4c
 TERMUX_PKG_AUTO_UPDATE=true
