2aad50bb0374342b9e9bbb2459dd3676124873af
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-08 18:20:06 +0200

    chore(main/cmusfm,codecrypt,coinor-cbc,coinor-clp): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/coinor-cbc/build.sh
+++ packages/coinor-cbc/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An open-source mixed integer linear programming solver"
 TERMUX_PKG_LICENSE="EPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.10.12"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/coin-or/Cbc/archive/refs/tags/releases/${TERMUX_PKG_VERSION#*:}.tar.gz
 TERMUX_PKG_SHA256=9ed71e4b61668462fc3794c102e26b4bb01a047efbbbcbd69ae7bde1f04f46a8
 TERMUX_PKG_AUTO_UPDATE=true
