74e7017ad09d1cb106e2dd913a8aafbedf595a71
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:25 +0200

    chore(main/hz,icecast,ices,icoutils): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/hz/build.sh
+++ packages/hz/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A high-performance and strong-extensibility Go HTTP fram
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.10.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/cloudwego/hertz/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=85d146e754f5fa5a23e02fe90d462737532749bf32e17a6c85ede21e6ded8b5b
 TERMUX_PKG_BUILD_IN_SRC=true
