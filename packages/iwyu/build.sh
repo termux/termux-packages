adc3a90916e59c7e1f73f1093ab59aa8baa61b5b
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:29 +0200

    chore(main/ired,isync,iverilog,iwyu): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/iwyu/build.sh
+++ packages/iwyu/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE=NCSA
 TERMUX_PKG_MAINTAINER="@termux"
 # Update this and the clang version below when libllvm is updated:
 TERMUX_PKG_VERSION=0.24
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/include-what-you-use/include-what-you-use/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=897b4c864a983f493c8efef4a1a9a2d429fd7ead1011f7a41743ed7b6dbe8c2e
 TERMUX_PKG_AUTO_UPDATE=false # can't be auto-updated since release correspond to clang version.
