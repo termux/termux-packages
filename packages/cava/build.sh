3d12994557f6e265d699488043c9c2e72b550b1e
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:37:44 +0200

    chore(main/catdoc,catgirl,catimg,cava): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/cava/build.sh
+++ packages/cava/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="Console-based Audio Visualizer. Works with MPD and Pulse
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 TERMUX_PKG_VERSION="0.10.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/karlstav/cava/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5a2efedf2d809d70770f49349f28a5c056f1ba9b3f5476e78744291a468e206a
 TERMUX_PKG_AUTO_UPDATE=true
