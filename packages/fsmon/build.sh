7b6e3465c006bff8f684ea6ea579ec73d9f233f5
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 18:51:29 +0200

    chore(main/fselect,fsmon,fwknop,fzy): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/fsmon/build.sh
+++ packages/fsmon/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Filesystem monitor with fanotify and inotify backends"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.8.8"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/nowsecure/fsmon/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b7f9a7310d7721c58e6e5bc432c5b2099be55b374ab9bf5f639420d2b9e0ab0a
 TERMUX_PKG_AUTO_UPDATE=true
--- packages/fzy/build.sh
+++ packages/fzy/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A simple, fast fuzzy finder for the terminal"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION="1.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jhawthorn/fzy/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=93d300d9c6c7063b2c6bda4e08a9704a029ec33f609718cd95443d1a890aff4e
 TERMUX_PKG_AUTO_UPDATE=true
