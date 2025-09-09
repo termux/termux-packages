76a3a80d5151336a74374e66a27e91f3b8b3ec50
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:12 +0200

    chore(main/grex,groff,gron,gsl): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/grex/build.sh
+++ packages/grex/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simplifies the task of creating regular expressions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pemistahl/grex/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4e849b29b387afc583856f24923b76052ad90e320c2caacfc6452e6d9deb6b14
 TERMUX_PKG_AUTO_UPDATE=true
