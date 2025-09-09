6a19ee5ece803db55b6b172bcf4cbad72456394b
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:39:40 +0200

    chore(main/global,glow,glpk,glslang): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/glslang/build.sh
+++ packages/glslang/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="OpenGL and OpenGL ES shader front end and validator"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="15.4.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/glslang/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b16c78e7604b9be9f546ee35ad8b6db6f39bbbbfb19e8d038b6fe2ea5bba4ff4
 TERMUX_PKG_DEPENDS="libc++"
