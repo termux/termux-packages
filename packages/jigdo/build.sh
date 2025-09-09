60f8d212f5e922870872dfa5138bf26b7affdadc
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:40 +0200

    chore(main/jhead,jigdo,jira-go,jless): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/jigdo/build.sh
+++ packages/jigdo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Distribute large images by sending and receiving the fil
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.8.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.einval.com/~steve/software/jigdo/download/jigdo-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=36f286d93fa6b6bf7885f4899c997894d21da3a62176592ac162d9c6a8644f9e
 TERMUX_PKG_AUTO_UPDATE=true
