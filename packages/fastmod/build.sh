b9791dd6a5bcbc70b7ccb8933aa8a8e461a911a0
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 18:51:13 +0200

    chore(main/fakeroot,fastmod,fatsort,faust): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/fastmod/build.sh
+++ packages/fastmod/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Regex-based code refactoring utility"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.4.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/facebookincubator/fastmod/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b438cc7564ef34d01f27cdd3cd50ee66a9915b9c50939ca021c6bee2e9c1f069
 TERMUX_PKG_AUTO_UPDATE=true
