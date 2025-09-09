8ffb9864ba127de50dbc5863b6bb26765dcec651
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:28 +0200

    chore(main/ipmitool,iproute2,ipv6toolkit,ircd-irc2): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/ipmitool/build.sh
+++ packages/ipmitool/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command-line interface to IPMI-enabled devices"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.8.19
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ipmitool/ipmitool/archive/refs/tags/IPMITOOL_${TERMUX_PKG_VERSION//./_}.tar.gz
 TERMUX_PKG_SHA256=48b010e7bcdf93e4e4b6e43c53c7f60aa6873d574cbd45a8d86fa7aaeebaff9c
 TERMUX_PKG_DEPENDS="openssl, readline"
