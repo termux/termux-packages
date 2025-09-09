c3074f20cf6d51efcf0ac6720d0d428cec8b7755
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:39:39 +0200

    chore(main/gitoxide,gkermit,glib-networking): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/glib-networking/build.sh
+++ packages/glib-networking/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Network-related giomodules for glib"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.80.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/glib-networking/${TERMUX_PKG_VERSION%.*}/glib-networking-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=b80e2874157cd55071f1b6710fa0b911d5ac5de106a9ee2a4c9c7bee61782f8e
 TERMUX_PKG_AUTO_UPDATE=true
