f01b33f6488264652e4bdf9f397d73f33219d26e
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:28 +0200

    chore(main/indent,influxdb,inotify-tools,iperf3): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/indent/build.sh
+++ packages/indent/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="C language source code formatting program"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2.13
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/indent/indent-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=1b81ba4e9a006ca8e6eb5cbbe4cf4f75dfc1fc9301b459aa0d40393e85590a0b
 TERMUX_PKG_DEPENDS="libandroid-support"
