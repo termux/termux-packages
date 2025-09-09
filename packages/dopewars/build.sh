30d2017e156c7ebb3dd91179d33b7cb759985df9
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 18:50:43 +0200

    chore(main/dnstop,docopt,doge,dopewars): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/dopewars/build.sh
+++ packages/dopewars/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Drug-dealing game set in streets of New York City"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.6.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://prdownloads.sourceforge.net/dopewars/dopewars-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=623b9d1d4d576f8b1155150975308861c4ec23a78f9cc2b24913b022764eaae1
 TERMUX_PKG_DEPENDS="glib, libcurl, ncurses"
