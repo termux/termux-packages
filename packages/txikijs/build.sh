75f72790be764dd1613a7d8378267d43a0fc91fe
TomIO <tom@termux.dev>
2025-09-09 13:59:51 +0200

    bump(main/txikijs): 24.12.0
    Co-authored-by: xdrlmm <983751615@qq.com>
    Co-authored-by: Robert Kirkman <rkirkman@termux.dev>

--- packages/txikijs/build.sh
+++ packages/txikijs/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/saghul/txiki.js
 TERMUX_PKG_DESCRIPTION="A small and powerful JavaScript runtime"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:22.11.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=1:24.12.0
 TERMUX_PKG_SRCURL=git+https://github.com/saghul/txiki.js
 TERMUX_PKG_DEPENDS="libcurl, libffi"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -12,7 +11,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DUSE_EXTERNAL_FFI=ON
 -DFFI_INCLUDE_DIR=$TERMUX_PREFIX/include
 -DFFI_LIB=$TERMUX_PREFIX/lib/libffi.so
+-DCMAKE_POLICY_VERSION_MINIMUM=3.5
 "
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_HOSTBUILD=true
 
 # Build failure for i686:
@@ -24,13 +25,13 @@ TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXCLUDED_ARCHES="i686"
 
 termux_step_host_build() {
-	find $TERMUX_PKG_SRCDIR -mindepth 1 -maxdepth 1 ! -name '.git*' \
+	find "$TERMUX_PKG_SRCDIR" -mindepth 1 -maxdepth 1 ! -name '.git*' \
 		-exec cp -a \{\} ./ \;
 
 	termux_setup_cmake
 
-	cmake .
-	make -j $TERMUX_PKG_MAKE_PROCESSES
+	cmake . -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+	make -j "$TERMUX_PKG_MAKE_PROCESSES"
 }
 
 termux_step_post_configure() {
@@ -38,5 +39,5 @@ termux_step_post_configure() {
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin tjs
+	install -Dm700 -t "$TERMUX_PREFIX/bin" tjs
 }
