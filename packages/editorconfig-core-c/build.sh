1df46731ad8f42a77c12ac56a81a7425dc37404f
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 18:51:01 +0200

    chore(main/editorconfig-core-c,eja,electric-fence,elinks): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/editorconfig-core-c/build.sh
+++ packages/editorconfig-core-c/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="EditorConfig core code written in C (for use by plugins 
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.12.9"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/editorconfig/editorconfig-core-c/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4aaa4e3883332aac7ec19c169dcf128f5f0f963f61d09beb299eb2bce5944e2c
 TERMUX_PKG_AUTO_UPDATE=true
--- packages/elinks/build.sh
+++ packages/elinks/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Full-Featured Text WWW Browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.18.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rkd77/elinks/releases/download/v${TERMUX_PKG_VERSION}/elinks-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e56ef15996a1ca130789293ee6d49cbecf175c06266acfa676fa6edb271a1173
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libexpat, libiconv, libidn, openssl, libbz2, zlib"
