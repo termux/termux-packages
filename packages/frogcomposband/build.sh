79c72b29e78c6cc8e1ccf971b9597eca6910282f
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 18:51:28 +0200

    chore(main/freeimage,frobtads,frogcomposband,frotz): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/frogcomposband/build.sh
+++ packages/frogcomposband/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_SRCURL=git+https://github.com/sulkasormi/frogcomposband
 TERMUX_PKG_VERSION=7.1.salmiak
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11 --bindir=$TERMUX_PREFIX/bin --sysconfdir=$TERMUX_PREFIX/share/frogcomposband"
