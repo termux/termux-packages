f119e2a31a63866271c52004ec8a25b6c50157c5
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-09 15:40:21 +0200

    chore(main/hors,hstr,htop,htslib): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/htslib/build.sh
+++ packages/htslib/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="C library for high-throughput sequencing data formats"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.22.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/samtools/htslib/releases/download/${TERMUX_PKG_VERSION}/htslib-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=3dfa6eeb71db719907fe3ef7c72cb2ec9965b20b58036547c858c89b58c342f7
 TERMUX_PKG_AUTO_UPDATE=true
