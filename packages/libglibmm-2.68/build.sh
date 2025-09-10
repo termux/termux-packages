e1bbe8a78bd9c86d6f74b97f23272c813a48058c
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:31:48 +0200

    chore(main/libgfshare,libglibmm-2.4,libglibmm-2.68,libglvnd): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libglibmm-2.68/build.sh
+++ packages/libglibmm-2.68/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A C++ API for parts of glib that are useful for C++"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.84.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/glibmm/${TERMUX_PKG_VERSION%.*}/glibmm-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=56ee5f51c8acfc0afdf46959316e4c8554cb50ed2b6bc5ce389d979cbb642509
 TERMUX_PKG_DEPENDS="glib, libc++, libsigc++-3.0"
