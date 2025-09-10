15bfcea88d39cd987ba15df1621b0d67294e39a3
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:33:06 +0200

    chore(main/libxext,libxft,libxi,libxlsxwriter): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libxext/build.sh
+++ packages/libxext/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="X11 miscellaneous extensions library"
 TERMUX_PKG_LICENSE="MIT, HPND, ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3.6"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXext-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=edb59fa23994e405fdc5b400afdf5820ae6160b94f35e3dc3da4457a16e89753
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
