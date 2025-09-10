9b2ac6980f570b7819964d2e5e6609b8e9e4c261
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:32:58 +0200

    chore(main/libtwolame,libuber-h3,libuchardet,libucontext): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libuber-h3/build.sh
+++ packages/libuber-h3/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A hexagonal hierarchical geospatial indexing system"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.3.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/uber/h3/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a47cfa36e255e4bf16c63015aaff8e6fe2afc14ffaa3f6b281718158446c0e9b
 TERMUX_PKG_AUTO_UPDATE=true
