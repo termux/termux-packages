99eb1ea1a604ed788db0a38579883b31560573c6
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:31:07 +0200

    chore(main/keybase,kibi,kiwix-tools,knockd): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Key directory that maps social media identities to encry
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.5.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/keybase/client/releases/download/v$TERMUX_PKG_VERSION/keybase-v$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=b09428b0d37208085e49daf9fdb12f27f3c24417e6a575b755a194318b7f8984
 TERMUX_PKG_AUTO_UPDATE=true
