cb40a7c30a169dd79d9b6f5ed833f23550e63827
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:32:43 +0200

    chore(main/libsignal-protocol-c,libsigsegv,libsixel,libslirp): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libsixel/build.sh
+++ packages/libsixel/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Encoder/decoder implementation for DEC SIXEL graphics"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.10.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/libsixel/libsixel/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b6654928bd423f92e6da39eb1f40f10000ae2cc6247247fc1882dcff6acbdfc8
 TERMUX_PKG_AUTO_UPDATE=true
