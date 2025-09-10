df672f02180f9e5e0a241b256b88db6c13b067a5
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:05 +0200

    chore(main/pkg-config,play-audio,plzip,pngcrush): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/plzip/build.sh
+++ packages/plzip/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A massively parallel lossless data compressor based on t
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.12"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/lzip/plzip/plzip-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=50d71aad6fa154ad8c824279e86eade4bcf3bb4932d757d8f281ac09cfadae30
 TERMUX_PKG_AUTO_UPDATE=true
