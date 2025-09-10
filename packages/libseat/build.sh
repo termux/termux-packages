c991e49f298ce53cbe6ea97c67dfc58d33765188
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:32:42 +0200

    chore(main/libseat,libseccomp,libsecp256k1,libsecret): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libseat/build.sh
+++ packages/libseat/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Reference implementation of a wayland compositor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.9.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/kennylevinsen/seatd/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=819979c922a0be258aed133d93920bce6a3d3565a60588d6d372ce9db2712cd3
 TERMUX_PKG_AUTO_UPDATE=true
