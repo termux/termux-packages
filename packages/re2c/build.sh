2b492cc8de9b81d15df57a407162c7fddb250b00
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:39 +0200

    chore(main/rdiff-backup,rdrview,re2c,recode): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/re2c/build.sh
+++ packages/re2c/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="Lexer generator for C, C++ and Go."
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
 TERMUX_PKG_VERSION="4.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/skvadrik/re2c/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=39cd7048a817cf3d7d0c2e58a52fb3597d6e1bc86b1df32b8a3cd755c458adfd
 TERMUX_PKG_AUTO_UPDATE=true
