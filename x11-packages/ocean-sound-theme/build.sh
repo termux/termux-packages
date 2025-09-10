35a59d3973d151f8c4c9ece36c2a97db1ae3b44a
BullyMaguire-lol <temp9040@gmail.com>
2025-09-10 11:58:28 +0530

    addpkg(x11/ocean-sound-theme): 6.5.0

---
+++ x11-packages/ocean-sound-theme/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/ocean-sound-theme"
+TERMUX_PKG_DESCRIPTION="Ocean Sound Theme for Plasma"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/ocean-sound-theme-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="c912e53930826442e9842a263679f1920b9a3dc3b63d9b02209fcfb769d75a0e"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qtbase"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
