de8b3124b7c327466a599b9a50ae4b2b7e2f5460
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:33:36 +0200

    chore(main/mdbook-plantuml,mdbook-presentation-preprocessor,mdbook-svgbob,mdbook-svgbob2): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/mdbook-svgbob2/build.sh
+++ packages/mdbook-svgbob2/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Alternative mdbook preprocessor for svgbob"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/matthiasbeyer/mdbook-svgbob2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9e36746b4975dfa3db996a3e890fea57810493c48aa18f7bd09dc4b5a76f5a96
 TERMUX_PKG_AUTO_UPDATE=true
