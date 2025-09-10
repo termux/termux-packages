594f7a1a9504daf8ffa5a6ab3fe5c583e3fe6638
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:34:39 +0200

    chore(main/openethereum,openfoam,openh264,openldap): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/openh264/build.sh
+++ packages/openh264/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Codec library supporting H.264 encoding and decoding"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.6.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/cisco/openh264/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=558544ad358283a7ab2930d69a9ceddf913f4a51ee9bf1bfb9e377322af81a69
 TERMUX_PKG_AUTO_UPDATE=true
