9ccab5805be7bd1c1448e799c2260632ce913d32
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:33:19 +0200

    chore(main/llbuild,llvm-mingw-w64-tools,lnav,lnd): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/llbuild/build.sh
+++ packages/llbuild/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A low-level build system, used by the Swift Package Mana
 TERMUX_PKG_LICENSE="Apache-2.0, NCSA"
 TERMUX_PKG_MAINTAINER="@finagolfin"
 TERMUX_PKG_VERSION=6.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/swiftlang/swift-llbuild/archive/swift-${TERMUX_PKG_VERSION}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=68df17453b52ac9fd334dec53e05f87c49e3458db0800273ff1f675525504996
 TERMUX_PKG_DEPENDS="libc++, libandroid-spawn, libsqlite"
