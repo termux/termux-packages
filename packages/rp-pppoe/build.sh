f46f6bbafb6d9ad600e244abac0fd3f6e3307109
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:43 +0200

    chore(main/rnr,robotfindskitten,rp-pppoe,rpm): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/rp-pppoe/build.sh
+++ packages/rp-pppoe/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A PPP-over-Ethernet redirector for pppd"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.0
+TERMUX_PKG_REVISION=1
 #TERMUX_PKG_SRCURL=https://dianne.skoll.ca/projects/rp-pppoe/download/rp-pppoe-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SRCURL=https://fossies.org/linux/misc/rp-pppoe-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=41ac34e5db4482f7a558780d3b897bdbb21fae3fef4645d2852c3c0c19d81cea
