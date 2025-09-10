769842f52cda22d3c417861f5702dbab06b39851
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:31:47 +0200

    chore(main/libgedit-gfls,libgee,libgeotiff,libgf2x): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libgee/build.sh
+++ packages/libgee/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A collection library providing GObject-based interfaces 
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.20.8"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libgee/${TERMUX_PKG_VERSION%.*}/libgee-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=189815ac143d89867193b0c52b7dc31f3aa108a15f04d6b5dca2b6adfad0b0ee
 TERMUX_PKG_AUTO_UPDATE=true
