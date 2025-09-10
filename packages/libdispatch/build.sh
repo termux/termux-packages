45e7fcb140614614b03074daf2450de94da36926
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:31:38 +0200

    chore(main/libdevil,libdispatch,libdmtx,libdrm): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libdispatch/build.sh
+++ packages/libdispatch/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The libdispatch project, for concurrency on multicore ha
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@finagolfin"
 TERMUX_PKG_VERSION="1:6.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/swiftlang/swift-corelibs-libdispatch/archive/swift-${TERMUX_PKG_VERSION:2}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=5bba8d7442890f7dbd37a9245340c5bb0c4c924dee6180ba30385b24e3fdf121
 TERMUX_PKG_AUTO_UPDATE=false
