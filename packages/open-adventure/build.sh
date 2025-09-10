d21e83260758821be52c13e6a78b5e164dc6ee40
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:34:25 +0200

    chore(main/onigmo,oorexx,open-adventure,opencolorio): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/open-adventure/build.sh
+++ packages/open-adventure/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Forward-port of the original Colossal Cave Adventure fro
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION="1.20"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.com/esr/open-adventure/-/archive/${TERMUX_PKG_VERSION}/open-adventure-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fa2e47cf30aaa8d90bb68a6f6b2d4f7e3bc65cc794f7b81edb7dce378c313e01
 TERMUX_PKG_AUTO_UPDATE=true
