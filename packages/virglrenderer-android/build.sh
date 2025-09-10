55d214b173b7809148386304e44b27c65a213fc6
Fervi (IntinteDAO) <lordfervi@protonmail.com>
2025-09-10 22:42:35 +0200

    bump(packages/virglrenderer-android): 1.2.0

--- packages/virglrenderer-android/build.sh
+++ packages/virglrenderer-android/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://virgil3d.github.io/
 TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines over OpenGLES libraries on Android"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION="1.0.1"
+TERMUX_PKG_VERSION="1.2.0"
 _LIBEPOXY_VERSION="1.5.10"
-TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=(
 	https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
 	https://github.com/anholt/libepoxy/archive/refs/tags/${_LIBEPOXY_VERSION}.tar.gz
 )
 TERMUX_PKG_SHA256=(
-	446ab3e265f574ec598e77bdfbf0616ee3c77361f0574bec733ba4bac4df730a
+	b181b668afae817953c84635fac2dc4c2e5786c710b7d225ae215d15674a15c7
 	a7ced37f4102b745ac86d6a70a9da399cc139ff168ba6b8002b4d8d43c900c15
 )
 TERMUX_PKG_DEPENDS="angle-android"
@@ -67,7 +66,9 @@ termux_step_host_build() {
 		--cross-file $TERMUX_MESON_CROSSFILE \
 		--prefix=$_INSTALL_PREFIX \
 		--libdir lib \
-		-Dplatforms=egl
+		-Dplatforms=egl \
+	-Dc_args="-D__TERMUX__" \
+	-Dcpp_args="-D__TERMUX__"
 	ninja -C virglrenderer-build install -j $TERMUX_PKG_MAKE_PROCESSES
 
 	# Move our virglrenderer binary to regular bin directory.
