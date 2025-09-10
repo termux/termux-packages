71ad81a195fc6ab097d6833f9b7809981ceef675
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:42 +0200

    chore(main/rgbds,rhash,rig,rinetd): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/rhash/build.sh
+++ packages/rhash/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Console utility for calculation and verification of magn
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4.6"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rhash/RHash/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9f6019cfeeae8ace7067ad22da4e4f857bb2cfa6c2deaa2258f55b2227ec937a
 TERMUX_PKG_AUTO_UPDATE=true
