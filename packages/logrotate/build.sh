45a90dad7f069a845a75f691c31c19baa837b6fa
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:33:20 +0200

    chore(main/locustdb,logo-ls,logrotate,loksh): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/logrotate/build.sh
+++ packages/logrotate/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simplify the administration of log files on a system whi
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.22.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/logrotate/logrotate/releases/download/${TERMUX_PKG_VERSION}/logrotate-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=42b4080ee99c9fb6a7d12d8e787637d057a635194e25971997eebbe8d5e57618
 TERMUX_PKG_AUTO_UPDATE=true
