02f403fbcef01221223e28f3c7982941da4bf12a
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:34:15 +0200

    chore(main/nmon,nnn,no-more-secrets,notcurses): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/nnn/build.sh
+++ packages/nnn/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Free, fast, friendly file browser"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jarun/nnn/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9faaff1e3f5a2fd3ed570a83f6fb3baf0bfc6ebd6a9abac16203d057ac3fffe3
 TERMUX_PKG_AUTO_UPDATE=true
--- packages/notcurses/build.sh
+++ packages/notcurses/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="blingful TUIs and character graphics"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.0.16"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dankamongmen/notcurses/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e893c507eab2183b6c598a8071f2a695efa9e4de4b7f7819a457d4b579bacf05
 TERMUX_PKG_AUTO_UPDATE=true
