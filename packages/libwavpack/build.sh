82c6d33da3dfd394615149dbfc824c66f4461e73
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:33:02 +0200

    chore(main/libvxl,libwavpack,libwebsockets,libwolfssl): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libwavpack/build.sh
+++ packages/libwavpack/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A completely open audio compression format providing los
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.8.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dbry/WavPack/releases/download/${TERMUX_PKG_VERSION}/wavpack-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=7322775498602c8850afcfc1ae38f99df4cbcd51386e873d6b0f8047e55c0c26
 TERMUX_PKG_AUTO_UPDATE=true
