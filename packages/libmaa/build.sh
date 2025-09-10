7729f9a47deefcae95117228e43aeda46e2edc13
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:32:14 +0200

    chore(main/liblua53,liblua54,liblzo,libmaa): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libmaa/build.sh
+++ packages/libmaa/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Provides many low-level data structures which are helpfu
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4.7"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dict/libmaa/libmaa-${TERMUX_PKG_VERSION}/libmaa-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4e01a9ebc5d96bc9284b6706aa82bddc2a11047fa9bd02e94cf8753ec7dcb98e
 TERMUX_PKG_AUTO_UPDATE=false # This package requires mkcmake which is not accessible while building packages
