3bdb94d543fa9b3abdca7590a46628da3a1c2451
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:14 +0200

    chore(main/pure-ftpd,pv,pwgen,pycairo): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/pycairo/build.sh
+++ packages/pycairo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python bindings for the cairo graphics library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.28.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pygobject/pycairo/releases/download/v${TERMUX_PKG_VERSION}/pycairo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=26ec5c6126781eb167089a123919f87baa2740da2cca9098be8b3a6b91cc5fbc
 TERMUX_PKG_AUTO_UPDATE=true
