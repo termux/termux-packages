ae44be2109430f19aba1502cf6ac91da8e5df9ad
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:32:51 +0200

    chore(main/libtatsu,libtbb,libtd,libtermkey): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/libtatsu/build.sh
+++ packages/libtatsu/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Library handling the communication with Apple's Tatsu Si
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.0.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/libtatsu/releases/download/${TERMUX_PKG_VERSION}/libtatsu-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=536fa228b14f156258e801a7f4d25a3a9dd91bb936bf6344e23171403c57e440
