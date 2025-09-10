009f375deba03b0a80a02d27ab8498f2e58f98d8
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:45 +0200

    chore(main/rq,rsgain,rtmpdump,rubberband): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/rubberband/build.sh
+++ packages/rubberband/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="An audio time-stretching and pitch-shifting library and 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://breakfastquay.com/files/releases/rubberband-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=af050313ee63bc18b35b2e064e5dce05b276aaf6d1aa2b8a82ced1fe2f8028e9
 TERMUX_PKG_AUTO_UPDATE=true
