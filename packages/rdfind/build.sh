01a63811d9d9705630944e80ad1cd6a6a919c735
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:38 +0200

    chore(main/ratt,rc,rcs,rdfind): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/rdfind/build.sh
+++ packages/rdfind/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A tool for finding duplicate files"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.7.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pauldreik/rdfind/archive/refs/tags/releases/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=bd17dbd9c6c9fc0c0b016b3e77ecf5cd718eee428172c767f429ba30405816d8
 TERMUX_PKG_AUTO_UPDATE=true
