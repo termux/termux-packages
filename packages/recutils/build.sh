e279c647366e7b93b9a62353cea047209ae1b627
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-10 23:35:39 +0200

    chore(main/recutils,redir,redis,renameutils): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/recutils/build.sh
+++ packages/recutils/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Set of tools and libraries to access human-editable, pla
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.9
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/recutils/recutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6301592b0020c14b456757ef5d434d49f6027b8e5f3a499d13362f205c486e0e
 # Prevent libandroid-spawn from being picked up
--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # Frozen! Do not update to 7.4.0
 # until the license/replacement discussion is concluded
 TERMUX_PKG_VERSION="1:7.2.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.redis.io/releases/redis-${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=5981179706f8391f03be91d951acafaeda91af7fac56beffb2701963103e423d
 TERMUX_PKG_AUTO_UPDATE=false
