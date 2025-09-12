5a9aab51d3acda612d37cf31a6c2cca0e2fc8dbe
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-12 02:20:42 +0200

    fix(main/lib{seat,soup,vo-amrwbenc,voikko,vorbis,vterm}): Rebuild
    Rebuild packages not in the repository (see #26261 and #26227).

--- packages/libsoup/build.sh
+++ packages/libsoup/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # This specific package is for libsoup-2.4.
 # libsoup-3.0 is packaged as libsoup3.
 TERMUX_PKG_VERSION=2.74.3
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libsoup/${TERMUX_PKG_VERSION%.*}/libsoup-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=e4b77c41cfc4c8c5a035fcdc320c7bc6cfb75ef7c5a034153df1413fa1d92f13
 TERMUX_PKG_AUTO_UPDATE=false
