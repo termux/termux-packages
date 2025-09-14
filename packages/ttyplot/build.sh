e11a106e1133b700551976f33e18c847401522a5
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-14 12:15:38 +0200

    chore(main/ttyper,ttyplot,ttyrec,tut): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/ttyplot/build.sh
+++ packages/ttyplot/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A realtime plotting utility for terminal with data input
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.7.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/tenox7/ttyplot/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=d1624eea52abec5538c9b19bae00f81642c2d2886cd7755988466b74424ce9ca
 TERMUX_PKG_AUTO_UPDATE=true
