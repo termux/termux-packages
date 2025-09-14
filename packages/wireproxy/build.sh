ddc024154897a934889ad48172cf1616a5064531
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-14 12:15:57 +0200

    chore(main/wireproxy,wiz,woff2,wol): Rebuild
    Rebuild some packages to get 16 kb page size.

--- packages/wireproxy/build.sh
+++ packages/wireproxy/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Wireguard client that exposes itself as a socks5 proxy"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.0.10"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pufferffish/wireproxy/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=3e08e79e3c1b66b7e129426dc6e84b93a1fb3827edde96d85bbf863a5a66c8ff
 TERMUX_PKG_BUILD_IN_SRC=true
