8b89b3b7cce38f882882a227410f4e83ff8a83fd
Maxython <mixython@gmail.com>
2025-09-15 18:52:59 +0300

    Implementation of working alternatives on pacman
    main/pacman: add `pacman-alternatives` - alternative management system for pacman
    termux_step_setup_alternatives.sh (termux_step_update_alternatives.sh): add installation of alternatives for pacman packages using pacman-alternatives

--- packages/pacman/build.sh
+++ packages/pacman/build.sh
@@ -3,9 +3,12 @@ TERMUX_PKG_DESCRIPTION="A library-based package manager with dependency support"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Maxython <mixython@gmail.com>"
 TERMUX_PKG_VERSION=7.1.0
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://gitlab.archlinux.org/pacman/pacman/-/releases/v${TERMUX_PKG_VERSION}/downloads/pacman-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=530e50d7edbb2a22581c6d6707d2113240276c1bec4ee39a99488e1243c32171
+TERMUX_PKG_REVISION=4
+_PALT_VERSION=1.0.0
+TERMUX_PKG_SRCURL=(https://gitlab.archlinux.org/pacman/pacman/-/releases/v${TERMUX_PKG_VERSION}/downloads/pacman-${TERMUX_PKG_VERSION}.tar.xz
+		https://github.com/termux-pacman/pacman-alternatives/archive/refs/tags/v${_PALT_VERSION}.tar.gz)
+TERMUX_PKG_SHA256=(530e50d7edbb2a22581c6d6707d2113240276c1bec4ee39a99488e1243c32171
+		919f8905a086ab5d25d476d6b502971b421bc68bb5612059a3bbade3d6b3f802)
 TERMUX_PKG_DEPENDS="bash, curl, gpgme, libandroid-glob, libarchive, libcurl, openssl, termux-licenses, termux-keyring"
 TERMUX_PKG_BUILD_DEPENDS="doxygen, asciidoc, nettle"
 TERMUX_PKG_GROUPS="base-devel"
@@ -34,12 +37,15 @@ termux_step_post_configure() {
 termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/pacman.d
 	install -m644 $TERMUX_PKG_BUILDER_DIR/mirrorlist $TERMUX_PREFIX/etc/pacman.d/mirrorlist
-}
 
-termux_step_create_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/bash" > postinst
-	echo "mkdir -p $TERMUX_PREFIX/var/lib/pacman/sync" >> postinst
-	echo "mkdir -p $TERMUX_PREFIX/var/lib/pacman/local" >> postinst
-	echo "mkdir -p $TERMUX_PREFIX/var/cache/pacman/pkg" >> postinst
-	chmod 755 postinst
+	cd $TERMUX_PKG_SRCDIR/pacman-alternatives-$_PALT_VERSION/
+	make DEF_OS="android" \
+		PREFIX="$TERMUX_PREFIX" \
+		SYSDIR="$TERMUX_PREFIX/"
+
+	for dir in var/lib/pacman/{sync,local,alternatives} var/cache/pacman/pkg share/pacman-alternatives; do
+		dir="${TERMUX_PREFIX}/${dir}"
+		mkdir -p ${dir}
+		touch ${dir}/.placeholder
+	done
 }
