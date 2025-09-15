d1c6245d3770f565f3dc9b7e2f224766794e3d63
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-15 12:41:46 +0200

    fix(*): Replace ftp.gnu.org with mirrors.kernel.org
    Replace ftp.gnu.org with mirrors.kernel.org since the second has been a
    lot more stable and performant over time than the first (and ftp.gnu.org
    is currently experiencing intermittent problems).
    
    See e.g. 9937b7487379513651c67649e6adcc36d8262100
    
    %ci:no-build

--- packages/recutils/build.sh
+++ packages/recutils/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.9
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/recutils/recutils-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/recutils/recutils-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6301592b0020c14b456757ef5d434d49f6027b8e5f3a499d13362f205c486e0e
 # Prevent libandroid-spawn from being picked up
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_header_spawn_h=no"
