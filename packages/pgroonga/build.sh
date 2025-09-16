ee915f1839a33fa5f9b4e68f3778c1ad165fbe43
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-16 05:37:38 +0000

    bump(main/pgroonga): 4.0.2

--- packages/pgroonga/build.sh
+++ packages/pgroonga/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A PostgreSQL extension to use Groonga as index"
 TERMUX_PKG_LICENSE="PostgreSQL"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.0.1"
+TERMUX_PKG_VERSION="4.0.2"
 TERMUX_PKG_SRCURL=https://github.com/pgroonga/pgroonga/releases/download/${TERMUX_PKG_VERSION}/pgroonga-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e2dfe40f3a0342e9ce4f1212043c46564fda3678e8cfda8587bbc37b103ebd17
+TERMUX_PKG_SHA256=2c121978b610efb50ee8919184fa69c06fff1c16af1502c77f37367817d06823
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="groonga, libmsgpack, xxhash"
@@ -17,7 +17,7 @@ MSGPACK_PACKAGE_NAME=msgpack-c
 PG_CONFIG=${TERMUX_PREFIX}/bin/pg_config
 "
 
-termux_step_pre_configure() {
-	# CMake files are broken
-	mv CMakeLists.txt{,.unused}
+termux_step_post_get_source() {
+	# force make
+	rm CMakeLists.txt meson.build
 }
