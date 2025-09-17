382120e1f813799d98d84d84199f6e589cc317e9
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-17 13:42:38 +0000

    bump(x11/gnome-calculator): 49.0
    Add workaround for cross compiling blueprint files.

--- x11-packages/gnome-calculator/build.sh
+++ x11-packages/gnome-calculator/build.sh
@@ -2,17 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://apps.gnome.org/Calculator
 TERMUX_PKG_DESCRIPTION="GNOME Scientific calculator"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="48.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="49.0"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gnome-calculator/${TERMUX_PKG_VERSION%%.*}/gnome-calculator-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=bc4bd41a9ba190f45cbee0d8c6752cdc5d28b0cef1c6bd0c01e2dae1f3c19162
+TERMUX_PKG_SHA256=df44acbdec1043d04b52c74db288f713cfc0752c17bd67171b941d6d13ead049
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gtk4, gtksourceview5, libadwaita, libgee, libmpc, libmpfr, libsoup3, libxml2"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross, valac"
+TERMUX_PKG_BUILD_DEPENDS="blueprint-compiler, g-ir-scanner, glib-cross, valac"
 TERMUX_PKG_RECOMMENDS="gnome-calculator-help"
 
 termux_step_pre_configure() {
 	termux_setup_gir
 	termux_setup_glib_cross_pkg_config_wrapper
+
+	# for blueprint-compiler
+	export GI_TYPELIB_PATH="$TERMUX_PREFIX"/lib/girepository-1.0
 }
