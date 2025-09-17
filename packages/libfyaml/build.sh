7841e7945e1b4b3e6a3270b2da609045543d0c8f
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-17 06:35:01 +0000

    addpkg(main/libfyaml): 0.9, for appstream

---
+++ packages/libfyaml/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://pantoniou.github.io/libfyaml/
+TERMUX_PKG_DESCRIPTION="Fully feature complete YAML parser and emitter"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.9"
+TERMUX_PKG_SRCURL=https://github.com/pantoniou/libfyaml/releases/download/v${TERMUX_PKG_VERSION}/libfyaml-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7731edc5dfcc345d5c5c9f6ce597133991a689dabede393cd77bae89b327cd6d
+TERMUX_PKG_AUTO_UPDATE=true
