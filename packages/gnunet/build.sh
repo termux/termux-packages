6874a86539dfe0b113f2adf3e75e3fb702f9b580
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-19 06:25:19 +0000

    bump(main/gnunet): 0.25.0
    Fix compiler error based on this patch
    https://github.com/Homebrew/homebrew-core/commit/6baabc2ed63e31ff2bd43108fd9cad496ed0f74d

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gnunet.org
 TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.24.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.25.0"
 TERMUX_PKG_SRCURL=https://ftpmirror.gnu.org/gnu/gnunet/gnunet-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5b06897b0e84489bbb438278ec73e4362442b2e05a63e40023ec1d0cccc6c576
+TERMUX_PKG_SHA256=2dea662ee8605946852af02d2806ca64fdadedcc718eeef6b86e0b26822c36ff
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libcurl, libgcrypt, libgnutls, libgpg-error, libidn2, libjansson, libltdl, libmicrohttpd, libsodium, libsqlite, libunistring, zlib"
 
