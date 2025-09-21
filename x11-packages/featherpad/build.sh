e59084265a4c7fed83768887ce04fb747022ce2e
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-21 10:36:58 +0200

    chore(x11/fcitx5-hangul,fcitx5-qt,feathernotes,featherpad): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/featherpad/build.sh
+++ x11-packages/featherpad/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lightweight Qt Plain-Text Editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.6.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tsujan/FeatherPad/releases/download/V${TERMUX_PKG_VERSION}/FeatherPad-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=0c6ad7687d933e48f5e64047dacb4d4611155210ef1557874fb708e805a50daa
 TERMUX_PKG_DEPENDS="hunspell, libc++, libx11, qt6-qtbase, qt6-qtsvg"
