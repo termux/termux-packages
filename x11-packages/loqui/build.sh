bf9af6924485778adb10c2a7f47ba44596b1fec8
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-21 10:37:37 +0200

    chore(x11/lite-xl,loqui,love,lxappearance): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/loqui/build.sh
+++ x11-packages/loqui/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="IRC client for Gtk environment"
 TERMUX_PKG_LICENSE="LGPL-2.0, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.7.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/sunnyone/loqui/releases/download/${TERMUX_PKG_VERSION}/loqui-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c593211d6bb477d5477ec9b81143e3faf96e859ad2edaf527fbc370333e5e0e7
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, pango"
--- x11-packages/lxappearance/build.sh
+++ x11-packages/lxappearance/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="LXDE GTK+ theme switcher"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
 TERMUX_PKG_VERSION="0.6.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/lxde/lxappearance/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9f067a8a126b9779ba12648c76136d9ba3e7ec7920c568df7819d128fdf39e03
 TERMUX_PKG_DEPENDS="dbus-glib, gdk-pixbuf, glib, gtk3, libx11, xsltproc"
