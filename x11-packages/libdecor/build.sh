47f9981565c4fd5d74954670c39d727643a5dfe7
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-21 10:37:32 +0200

    chore(x11/libdbusmenu-lxqt,libdbusmenu-qt,libdecor,libevdev): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/libdecor/build.sh
+++ x11-packages/libdecor/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Client-side decorations library for Wayland clients"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.2.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/libdecor/libdecor/-/archive/${TERMUX_PKG_VERSION}/libdecor-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=21a471e3f48088d3fd8ecc5999c45258a32198782c0157482f7ebe82de42f79c
 # gtk3 dependency makes libdecor a "x11" package
