6bb0a64e46270ba2130339e34a1e6ffa93dbf1d2
Biswapriyo Nath <nathbappai@gmail.com>
2025-09-21 16:02:12 +0000

    bump(main/lnav): 0.13.2
    Add patches for bundled notcurses repository.

--- packages/lnav/build.sh
+++ packages/lnav/build.sh
@@ -2,12 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://lnav.org/
 TERMUX_PKG_DESCRIPTION="An advanced log file viewer for the small-scale"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.12.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="0.13.2"
 TERMUX_PKG_SRCURL=https://github.com/tstack/lnav/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=7ba030e5da5b59cd9ea7c0d82aef3af1a6ee1969cab6e22e4bb0f37a96005c7a
+TERMUX_PKG_SHA256=3501b921c0acb435f85bd2ce589b4d8ba205a87ed855926e65a6ad165431b331
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libandroid-utimes, libarchive, libbz2, libc++, libcurl, libsqlite, ncurses, pcre2, readline, zlib"
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob, libandroid-spawn, libandroid-utimes, libarchive, libbz2, libc++, libcurl, libsqlite, libunistring, pcre2, readline, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-system-paths
 --disable-static
@@ -16,8 +15,15 @@ ac_cv_path_CARGO_CMD=
 "
 
 termux_step_pre_configure() {
+	# for bundled notcurses repository
+	(
+		cd src/third-party/notcurses
+		patch -p1 -i "$TERMUX_PKG_BUILDER_DIR"/../notcurses/include-notcurses-ncport.h.patch
+		patch -p1 -i "$TERMUX_PKG_BUILDER_DIR"/../notcurses/src-lib-termdesc.h.patch
+	)
+
 	autoreconf -fi
 
-	CXXFLAGS+=" -DC4_LINUX"
-	LDFLAGS+=" -landroid-glob -landroid-utimes"
+	CXXFLAGS+=" -DC4_LINUX -Wno-c++11-narrowing"
+	LDFLAGS+=" -landroid-glob -landroid-spawn -landroid-utimes"
 }
