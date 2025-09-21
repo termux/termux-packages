851dcb9c3710839224f68847104539a4ed211121
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-21 10:36:51 +0200

    chore(x11/devilspie2,dosbox,dosbox-x,e16): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/dosbox-x/build.sh
+++ x11-packages/dosbox-x/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A cross-platform DOS emulator based on the DOSBox projec
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2025.02.01"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/joncampbell123/dosbox-x/archive/refs/tags/dosbox-x-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3a6fdfd659bb05db82bf2d850af806f666562cce9a37609fd33b59f7e4bd8fa4
 TERMUX_PKG_DEPENDS="dosbox-x-data, fluidsynth, freetype, libandroid-posix-semaphore, libc++, libiconv, libpcap, libpng, libslirp, libx11, libxkbfile, libxrandr, opengl, sdl2 | sdl2-compat, sdl2-net, zlib"
