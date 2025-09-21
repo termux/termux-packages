fe356755553b652a59f3567d66f8ea6dee59f7c0
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-21 10:37:30 +0200

    chore(x11/keybinder,kf6-kirigami-addons,kid3,kitty): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/kf6-kirigami-addons/build.sh
+++ x11-packages/kf6-kirigami-addons/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Add-ons for the Kirigami framework"
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.9.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/kirigami-addons/kirigami-addons-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=21314a91f26b1c962def3fd7ff2e762d3358b075f63f4d7e0144fb2c63b7ebc7
 TERMUX_PKG_DEPENDS="kf6-kconfig, kf6-kcoreaddons, kf6-kglobalaccel, kf6-kguiaddons, kf6-ki18n, kf6-kirigami, libc++, qt6-qtbase, qt6-qtdeclarative"
