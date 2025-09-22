98ff2f773a8b5ac26df2d6f3a95210172177e1ff
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:09:47 +0200

    chore(x11/xfce4-notifyd,xfce4-places-plugin,xfce4-pulseaudio-plugin,xfce4-screenshooter): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/xfce4-places-plugin/build.sh
+++ x11-packages/xfce4-places-plugin/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="This plugin brings much of the functionality of GNOME Pl
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
 TERMUX_PKG_VERSION="1.9.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-places-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-places-plugin-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=76d95687e0bea267465e832eea6266563a18d2219192f9e8af6f88e899262e43
 TERMUX_PKG_AUTO_UPDATE=true
