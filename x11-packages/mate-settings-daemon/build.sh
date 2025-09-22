172ff4c41217f88608bfb39b6574af4594665bd9
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:08:29 +0200

    chore(x11/libhandy,mate-settings-daemon,mate-terminal): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/mate-settings-daemon/build.sh
+++ x11-packages/mate-settings-daemon/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="mate-settings-daemon is a fork of gnome-settings-daemon"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.28.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-settings-daemon/releases/download/v$TERMUX_PKG_VERSION/mate-settings-daemon-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=4ed7cdadaaa4c99efffc0282b8411703bb76e072c41c4b57989f8c5b40611a3a
 TERMUX_PKG_AUTO_UPDATE=true
