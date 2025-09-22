d9f5411a8e857231738caa201509160bc1ceb2ac
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:09:47 +0200

    chore(x11/xfce4-dev-tools,xfce4-dict,xfce4-docklike-plugin,xfce4-eyes-plugin): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/xfce4-eyes-plugin/build.sh
+++ x11-packages/xfce4-eyes-plugin/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="This plugin adds eyes to the Xfce panel which follow you
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7"
 TERMUX_PKG_VERSION="4.7.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-eyes-plugin/${TERMUX_PKG_VERSION%.*}/xfce4-eyes-plugin-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=87f9b978ca75abb3aa5edb1315eb65ef98654a662c14621847ddffe8aa6574ad
 TERMUX_PKG_AUTO_UPDATE=true
