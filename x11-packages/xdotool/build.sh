07814227d29c2786e40836a99d22c9ab48cd79b1
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:09:46 +0200

    chore(x11/xclip,xcompmgr,xdotool,xf86-input-void): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/xdotool/build.sh
+++ x11-packages/xdotool/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="simulate (generate) X11 keyboard/mouse input events"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.20211022.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/jordansissel/xdotool/releases/download/v${TERMUX_PKG_VERSION}/xdotool-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=96f0facfde6d78eacad35b91b0f46fecd0b35e474c03e00e30da3fdd345f9ada
 TERMUX_PKG_DEPENDS="libx11, libxtst, libxinerama, libxkbcommon"
