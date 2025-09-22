34f130beb2a66843c491ef94d6cf12c13575a6ba
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:08:29 +0200

    chore(x11/menu-cache,mesa-demos,mgba): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/menu-cache/build.sh
+++ x11-packages/menu-cache/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Caching mechanism for freedesktop.org compliant menus"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.1.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/lxde/menu-cache/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e8af90467df271c3c8700c840ca470ca2915699c6f213c502a87d74608748f08
 TERMUX_PKG_DEPENDS="glib, libfm-extra"
