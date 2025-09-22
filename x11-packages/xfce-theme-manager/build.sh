705bb0171e3d2003eaa137ea9e2af0996d4ab7ca
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:09:46 +0200

    chore(x11/xf86-video-dummy,xfce-theme-manager,xfce4-appfinder,xfce4-battery-plugin): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/xfce-theme-manager/build.sh
+++ x11-packages/xfce-theme-manager/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Integrated theme manager for xfce4"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1:0.3.9"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/KeithDHedger/Xfce-Theme-Manager/archive/refs/tags/v${TERMUX_PKG_VERSION#*:}.tar.gz
 TERMUX_PKG_SHA256=3637984378e7b2a40232809dd0cd116ba8b7090dc47f411cbed3c6ef2b271d44
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk2, libcairo, libxcursor, xfconf"
