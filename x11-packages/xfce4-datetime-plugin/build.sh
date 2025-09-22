a52981e26fc7468aff0e6285a0834fea33969994
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:09:46 +0200

    chore(x11/xfce4-calculator-plugin,xfce4-clipman-plugin,xfce4-cpugraph-plugin,xfce4-datetime-plugin): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/xfce4-datetime-plugin/build.sh
+++ x11-packages/xfce4-datetime-plugin/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 _MAJOR_VERSION=0.8
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.3
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-datetime-plugin/${_MAJOR_VERSION}/xfce4-datetime-plugin-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=6b2eeb79fb586868737426cbd2a4cd43c7f8c58507d8a2f578e0150187cc00b0
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libxfce4ui, libxfce4util, pango, xfce4-panel, zlib"
