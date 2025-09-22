489866bbbd9ac93991a1d415207833a87d0e8736
Fredrik Fornwall <fredrik@fornwall.net>
2025-09-22 17:08:39 +0200

    chore(x11/mogan,mousepad,mpv-x): Rebuild
    Rebuild some packages to get 16 kb page size.

--- x11-packages/mousepad/build.sh
+++ x11-packages/mousepad/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A simple text editor for the Xfce desktop environment"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.6.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/mousepad/${TERMUX_PKG_VERSION%.*}/mousepad-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=21762bc8c3c4f120a4a509ce39f4a5a58dbc10e3f0da66cdc6d9a8c735fff2ac
 TERMUX_PKG_AUTO_UPDATE=true
