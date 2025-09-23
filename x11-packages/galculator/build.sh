55596111189296e8cfe826ac0c5bf2dd6bac2234
Robert Kirkman <rkirkman@termux.dev>
2025-09-23 02:14:50 -0500

    fix(x11/galculator): switch to official GitHub mirror because something is wrong with the original `TERMUX_PKG_SRCURL`
    - It's hard to describe what happened to the original download URL except maybe "it became a `.html` file filled with random symbols" https://galculator.mnim.org/downloads.html

--- x11-packages/galculator/build.sh
+++ x11-packages/galculator/build.sh
@@ -3,11 +3,12 @@ TERMUX_PKG_DESCRIPTION="GTK+ based scientific calculator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.1.4
-TERMUX_PKG_REVISION=24
-TERMUX_PKG_SRCURL=http://galculator.mnim.org/downloads/galculator-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=01cfafe6606e7ec45facb708ef85efd6c1e8bb41001a999d28212a825ef778ae
+TERMUX_PKG_REVISION=25
+TERMUX_PKG_SRCURL="https://github.com/galculator/galculator/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=dcbdb48ddf8a3f68b9aa5902f880f174fd269de2b7410988148d05871012e142
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, libcairo, pango"
 
 termux_step_pre_configure() {
+	autoreconf -fi
 	CFLAGS+=" -fcommon"
 }
