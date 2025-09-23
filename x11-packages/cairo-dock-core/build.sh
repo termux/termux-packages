ccca87ed0c6fdec90d42f9d0dffcc7ccb58896ba
Robert Kirkman <rkirkman@termux.dev>
2025-09-23 02:13:31 -0500

    fix(x11/cairo-dock-core): fix build with CMake 4
    - Also fix the CMake header installation path to prevent `ERROR: Package contains directory "$PREFIX/$PREFIX"`

--- x11-packages/cairo-dock-core/build.sh
+++ x11-packages/cairo-dock-core/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Cairo-Dock is a simple and avanzed dock for linux deskto
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.5.2"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/Cairo-Dock/cairo-dock-core/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9a08efe15e257f18e33161b17c4bbab7c6889854a9fab96a69d6568f0e91475f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="dbus-glib, gdk-pixbuf, glib, glu, gtk3, libcairo, libcurl, librsvg, libx11, libxcomposite, libxinerama, libxml2, libxrandr, libxrender, libxtst, opengl, pango, which"
 TERMUX_PKG_BUILD_DEPENDS="valac"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_POLICY_VERSION_MINIMUM=3.5
 -DFORCE_NOT_LIB64=yes
 -DCMAKE_INSTALL_LIBDIR=lib
 "
