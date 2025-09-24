ae6b05c4b69581f7c2b0f1c744990b6b7cc05890
Robert Kirkman <rkirkman@termux.dev>
2025-09-24 02:15:07 -0500

    fix(packages/tex-gyre): update license file

--- packages/tex-gyre/build.sh
+++ packages/tex-gyre/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="GUST-FONT-LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.501
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://www.gust.org.pl/projects/e-foundry/tex-gyre/whole/tg${TERMUX_PKG_VERSION//./_}otf.zip
 TERMUX_PKG_SHA256=d7f8be5317bec4e644cf16c5abf876abeeb83c43dbec0ccb4eee4516b73b1bbe
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -13,7 +13,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_post_get_source() {
 	termux_download https://www.gust.org.pl/fonts/licenses/GUST-FONT-LICENSE.txt \
 		$TERMUX_PKG_SRCDIR/GUST-FONT-LICENSE.txt \
-		a746108477b2fa685845e7596b7ad8342bc358704b2b7da355f2df0a0cb8ad85
+		5eb61bb836bb1845ef668717cb15b382e997748ce2629e4388cc5e4c3fa4e433
 }
 
 termux_step_make_install() {
