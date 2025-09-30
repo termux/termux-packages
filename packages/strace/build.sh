4c87facc45740b1066f2d7cbfe0a6e023630c4fd
TomIO <tom@termux.dev>
2025-09-30 15:10:36 +0200

    bump(main/strace): 6.17

--- packages/strace/build.sh
+++ packages/strace/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals re
 TERMUX_PKG_LICENSE="LGPL-2.1-or-later, GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, LGPL-2.1-or-later, bundled/linux/COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.16"
+TERMUX_PKG_VERSION="6.17"
 TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=3d7aee7e4f044b2f67f3d51a8a76eda18076e9fb2774de54ac351d777d4ebffa
+TERMUX_PKG_SHA256=0a7c7bedc7efc076f3242a0310af2ae63c292a36dd4236f079e88a93e98cb9c0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdw"
 
@@ -13,15 +13,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-libdw
 "
 
-# This is a perl script.
-TERMUX_PKG_RM_AFTER_INSTALL="bin/strace-graph"
-
 termux_step_pre_configure() {
-	if [ "$TERMUX_ARCH" = "arm" ] || [ "$TERMUX_ARCH" = "i686" ] || [ "$TERMUX_ARCH" = "x86_64" ]; then
-		# Without st_cv_m32_mpers=no the build fails if gawk is installed.
+	case "$TERMUX_ARCH" in
+		"x86_64") # mpers support seems to break the build on x86_64
+		# This is likely an issue in `src/mpers.sh` but I can't track it down.
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" st_cv_m32_mpers=no"
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-mpers=no"
-	fi
+		;;
+	esac
 	autoreconf # for configure.ac in configure-find-armv7-cc.patch
 	CPPFLAGS+=" -Dfputs_unlocked=fputs"
 }
