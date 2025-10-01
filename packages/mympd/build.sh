bb5fdb485c48d1144ca31177d8fff0ba15c9772c
Biswapriyo Nath <nathbappai@gmail.com>
2025-10-01 18:48:54 +0000

    bump(main/mympd): 22.1.0

--- packages/mympd/build.sh
+++ packages/mympd/build.sh
@@ -2,13 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://jcorporation.github.io/myMPD/
 TERMUX_PKG_DESCRIPTION="A standalone and lightweight web-based MPD client"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="22.0.4"
+TERMUX_PKG_VERSION="22.1.0"
 TERMUX_PKG_SRCURL=https://github.com/jcorporation/myMPD/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=68115f3cac2ecc22df7048183f0187b17c7bffc1f08ec8318e2ac47c0fd9bec7
+TERMUX_PKG_SHA256=c9f46b2d48e9138d767fb0234bb2d3d7200a6fa66bfa373371e25423a49bd43b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libflac, libid3tag, liblua54, openssl, pcre2, resolv-conf"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DMATH_LIB=m
 -DMYMPD_STARTUP_SCRIPT=OFF
 "
-TERMUX_CMAKE_BUILD="Unix Makefiles"
