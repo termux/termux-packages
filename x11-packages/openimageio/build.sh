02e4e7c64a4c5e2ad6ff6a944df4eac2f2aea62d
Robert Kirkman <rkirkman@termux.dev>
2025-10-04 00:39:55 -0500

    bump(x11/openimageio): 3.1.6.2

--- x11-packages/openimageio/build.sh
+++ x11-packages/openimageio/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.openimageio.org/
 TERMUX_PKG_DESCRIPTION="A library for reading and writing images, including classes, utilities, and applications"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.0.11.0"
+TERMUX_PKG_VERSION="3.1.6.2"
 TERMUX_PKG_SRCURL="https://github.com/OpenImageIO/oiio/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=826f16e1afc6b145d934efed01780cd31c3ef75a9e5d73e3cb188a37efb954da
+TERMUX_PKG_SHA256=a1e515e6a6ea9925a70c23db21dda37f3ce2c24780a6bfedd8685fea063f698a
 # configure-time error if ptex and ptex-static are not both installed
 TERMUX_PKG_DEPENDS="boost, dcmtk, ffmpeg, fmt, freetype, glew, libc++, libhdf5, libheif, libjpeg-turbo, libjxl, libpng, libraw, libtbb, libtiff, libwebp, libyaml-cpp, opencolorio, opencv, openexr, openjpeg, openvdb, ptex, pybind11, python, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, fontconfig, libjpeg-turbo-static, libpugixml, libxrender, mesa, ptex-static, robin-map"
@@ -22,7 +22,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=30
+	local _SOVERSION=31
 
 	local v=$(sed -En 's/^set \(OpenImageIO_VERSION "([0-9]+.[0-9]+).*/\1/p' "$TERMUX_PKG_SRCDIR"/CMakeLists.txt)
 	v="${v//./}"
