9128ad007bd4f7b6bc1306839061bf6ac348ad59
Max Ivan <mixython@gmail.com>
2025-10-04 20:36:22 +0300

    fix(main/fastfetch): disable `statx` system call (#26770)

--- packages/fastfetch/build.sh
+++ packages/fastfetch/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A maintained, feature-rich and performance oriented, neo
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.53.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1488d9b738474e8ef8e8d78e2463722bf706e435857c849b3f480354ad62366e
 TERMUX_PKG_BUILD_DEPENDS="chafa, dbus, freetype, glib, imagemagick, libandroid-wordexp-static, libelf, libxcb, libxrandr, mesa-dev, ocl-icd, opencl-headers, pulseaudio, vulkan-headers, vulkan-loader-generic, zlib"
