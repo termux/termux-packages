c1262edad3c6a7d9d37266f2a0261938aa73e067
Biswapriyo Nath <nathbappai@gmail.com>
2025-10-05 09:51:59 +0000

    bump(main/liblo): 0.33
    Replace sourceforge with github repository link for source tarball
    because it is not added in sourceforge.

--- packages/liblo/build.sh
+++ packages/liblo/build.sh
@@ -2,7 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://liblo.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="A lightweight library that provides an easy to use implementation of the OSC protocol"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.32"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/liblo/liblo-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=5df05f2a0395fc5ac90f6b538b8c82bb21941406fd1a70a765c7336a47d70208
+TERMUX_PKG_VERSION="0.33"
+# https://downloads.sourceforge.net/liblo/liblo-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/radarsat1/liblo/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0b538bd429e2e47309244912184c00163b5c28154105efe3167f3632e12cfbc7
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	autoreconf -fiv
+}
