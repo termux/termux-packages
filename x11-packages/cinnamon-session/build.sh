17c8443e3420776fc3b97e973f83a6ac09a2ebdf
Robert Kirkman <rkirkman@termux.dev>
2025-10-06 23:08:02 -0500

    robertkirkman debpython temporary commit 5: cinnamon update

--- x11-packages/cinnamon-session/build.sh
+++ x11-packages/cinnamon-session/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Cinnamon session manager"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.4.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/linuxmint/cinnamon-session/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=e4f0380ef45be08366fcdfbda431c6b0b20760d251ea78b810b4e0e223134690
 TERMUX_PKG_AUTO_UPDATE=true
@@ -36,12 +37,4 @@ termux_step_pre_configure() {
 	termux_setup_glib_cross_pkg_config_wrapper
 
 	export TERMUX_MESON_ENABLE_SOVERSION=1
-}
-
-termux_step_create_debscripts() {
-	cat <<-EOF >./postinst
-		#!$TERMUX_PREFIX/bin/sh
-		echo "Installing dependencies through pip..."
-		pip3 install ${TERMUX_PKG_PYTHON_TARGET_DEPS//, / }
-	EOF
 }
