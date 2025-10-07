6763f6fb94f753a123a3e06ddeaf66d1430fcf89
Aditya Alok <alok@termux.dev>
2025-10-08 02:45:06 +0530

    New faster implementation
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- docs/buildorder.md
+++ docs/buildorder.md
@@ -1,79 +1,49 @@
-# Build Order Generation
+# Documentation for `new_buildorder.py`
 
-This script is used to determine the order in which to build the dependencies
-of a given package. It performs a topological sort
-(post-order Depth First Traversal) on the dependency graph to generate a linear order.
+This script is used to determine the order in which to build the dependencies of a given package. It performs a topological sort (post-order Depth First Traversal) on the dependency graph to generate a linear order.
 
-## Core Concepts
+---
 
-### PackageType
+## Concepts
 
-All packages are divided into two categories:
+* **`Package`**: Something which has a build script (`build.sh`) or a subpackage script (`*.subpackage.sh`).
+A package can be one of two types:
+  * **`REAL`**: A package that compiles to an artifact when built.
+  * **`VIRTUAL`**: A concept package that doesn't produce any artifacts. It just acts as a placeholder or an alias to one or more `REAL` packages. For example: Metapackages, `static` packages, and subpackages (in `OFFLINE` mode).
 
-- `REAL`: A standard package that can be compiled.
-- `VIRTUAL`: A package that isn't a *real* package but points to one or more
-other packages. For example: static packages and metapackages.
+* **Build Modes**: There are two modes of operation:
+  * **`BuildMode.OFFLINE`**: It assumes no access to pre-built packages. The package and all its dependencies are to be built from source. It considers **build-time dependencies** (`TERMUX_PKG_BUILD_DEPENDS`) in addition to runtime dependencies.
+  * **`BuildMode.ONLINE`**: This mode assumes pre-built packages can be downloaded. It only considers runtime dependencies (`TERMUX_PKG_DEPENDS`), as build dependencies are not needed.
 
-### BuildMode
+> [!NOTE]
+> The `root_pkg` is always operated in `BuildMode.OFFLINE` as it has be built from source.
 
-There are two modes of operation:
+* **`AlternativeDependency`**: Represents a choice between several possible dependencies (e.g., `libA | libB`). It holds a tuple of options and assumes the first one as the default.
 
-- `ONLINE`: Assumes subpackages can be downloaded and treats them as separate, `REAL` packages. They only have relation with their parent as defined by `TERMUX_SUBPKG_DEPEND_ON_PARENT`
-- `OFFLINE`: Assumes no network access. It merges the dependencies of subpackages into their parent package, and treats them as `VIRTUAL` packages
-(as they cannot be built independently).
+---
 
-### Dependencies
+## Dependency Graph
 
-Dependencies are simply `REAL` or `VIRTUAL` packages on which a package depends.
+It is represented as a [memoized](https://docs.python.org/3/library/functools.html#functools.cache) `__get_package` function.
 
-There is a special type of dependency:
+### `__get_package(package_name)`
 
-- `AlternativeDependency`: It represents a choice between several possible dependencies (e.g., `libA` | `libB`). It holds a tuple of options and assumes the first one as the default.
+This function parses a package's build script and creates a `Package` object. `Package` creation logic differs based on the package type and build mode.
 
-## BuildOrder
+* **Virtual Static Packages**: If a package name ends in `-static` and has no corresponding build script, it's treated as a `VIRTUAL` package that simply depends on its parent.
+* **Metapackages**: Packages with `TERMUX_PKG_METAPACKAGE="true"` are marked as `VIRTUAL`.
+* **Subpackages**:
+  * In **`OFFLINE` mode**, a subpackage is considered `VIRTUAL`. It cannot be built separately, as it's created during the parent package's build process. Its only depends upon its parent.
+  * In **`ONLINE` mode**, a subpackage is considered as `REAL` because it can be downloaded independently. `TERMUX_SUBPKG_DEPENDS` are its dependencies.
 
-### Dependency graph
+### `__resolve_dependencies(package)`
 
-A dictionary (`PackageMap`) mapping of package to `Package` is built. Each package of every repo is processed.
+This function eliminates all the `VIRTUAL` packages from the graph. If package `A` depends upon a `VIRTUAL` package `V`, and `V` in provided by/depends upon  a `REAL` packages `B`, this function ensures that the DFS sees `A`'s dependencies as `B`, not `V`.
 
-#### Step 1
+It works recursively:
 
-##### Metapackages
-
-Marked as `VIRTUAL` packages and added to the graph.
-
-##### Subpackages
-
-The logic differs based on the `build_mode`:
-
-- `OFFLINE`: Dependencies from all subpackages are merged into the parent package. The subpackages themselves are treated as `VIRTUAL` packages that simply depend on the parent. This ensures the parent package has everything it needs to build completely.
-- `ONLINE`: Subpackages are treated as independent `REAL` packages. Their dependencies are parsed, and a dependency on the parent package is added based on the `TERMUX_SUBPKG_DEPEND_ON_PARENT` flag.
-
-##### Static Packages
-
-If a package allows static splitting, a `VIRTUAL` package with a `-static` suffix is created, with dependency on the main package.
-
-#### Step 2 (Sanitization)
-
-The `VIRTUAL` packages are resolved to `REAL` packages that provide them. The `resolve` function recursively traverses the dependencies of a `VIRTUAL` package until it finds the `REAL` package that provides it. This flattens the dependency chain to only include `REAL` packages.
-
-### Order Generation
-
-A *post-order* Depth-First Search (DFS) algorithm is applied to the sanitized package map. It uses a `path` list to detect cycles.
-
-#### DFS Traversal
-
-The algorithm traverses the dependency graph starting from the `root_pkg` (if provided) or all packages. It recursively explores the dependencies of each package.
-
-##### Cycle Detection
-
-If it encounters a package that is already in the current traversal path, it means a circular dependency exists, and it raises an exception.
-
-Once all dependencies of a package have been visited, the package is added to the final `build_order` list. The resulting list is the correct build order, where each package appears after all of its dependencies.
-
-##### Choice of AlternativeDependency
-
-An option from `AlternativeDependency` is determined using `next(iter(visited.intersection(dep.options)), dep.default)` logic. It ensures that if one of the options is already selected, we do not make a redundant choice.
+1. If a dependency is `REAL`, it's returned as is.
+2. If a dependency is `VIRTUAL`, the function calls itself on that package's dependencies until it flattens them all back to `REAL` packages.
 
 ## Refrences
 
--- scripts/new_buildorder.py
+++ scripts/new_buildorder.py
@@ -24,6 +24,11 @@ PackageName: TypeAlias = str
 Dependency: TypeAlias = PackageName
 
 
+def die(msg: str) -> None:
+    print("ERROR:", msg)
+    exit(1)
+
+
 class BuildMode(Enum):
     ONLINE = 0
     OFFLINE = 1
@@ -42,11 +47,6 @@ class AlternativeDependency:
     def __post_init__(self) -> None:
         self.default = self.options[0]  # By default choose first option.
 
-    def __eq__(self, value: object, /) -> bool:
-        if not isinstance(value, AlternativeDependency):
-            return NotImplemented
-        return value == self.options
-
     def __hash__(self) -> int:
         return hash(self.options)
 
@@ -58,58 +58,60 @@ class Package:
     path: Path
     dependencies: Set[Dependency | AlternativeDependency]
 
-
-PackageMap: TypeAlias = Dict[PackageName, Package]
+    def __hash__(self) -> int:
+        return hash(self.name)
 
 
 class BuildScriptParser:
+    _VARS_REGEX = re.compile(
+        r"""
+        ^(
+        TERMUX_PKG_METAPACKAGE
+        | TERMUX_PKG_NO_STATICSPLIT
+        | TERMUX_PKG_BUILD_DEPENDS
+        | TERMUX_PKG_DEPENDS
+        | TERMUX_SUBPKG_DEPENDS
+        | TERMUX_SUBPKG_DEPEND_ON_PARENT
+        )
+        [+]?=["']?(.*?)["']?$
+        """,
+        re.MULTILINE | re.VERBOSE,
+    )
+
     def __init__(self, script_path: Path) -> None:
-        with open(script_path, encoding="utf-8") as build_script:
-            self.content = build_script.read()
+        self.variables: Dict[str, str] = {}
 
-    def __parse_bool_variable(self, var: str) -> bool:
-        value = "false"
-
-        m = re.search(r'^%s="?(true|false)"?$' % var, self.content, re.MULTILINE)
-        if m:
-            value = m.group(1)
-
-        return value == "true"
+        for m in self._VARS_REGEX.finditer(script_path.read_text(encoding="utf-8")):
+            if m:
+                var, value = m.groups()
+                if self.variables.setdefault(var, value) != value:
+                    self.variables[var] += f",{value}"
 
     def is_metapackage(self) -> bool:
-        return self.__parse_bool_variable("TERMUX_PKG_METAPACKAGE")
+        return self.variables.get("TERMUX_PKG_METAPACKAGE", "false") == "true"
 
     def is_static_splitting_allowed(self) -> bool:
-        return not self.__parse_bool_variable("TERMUX_PKG_NO_STATICSPLIT")
+        return self.variables.get("TERMUX_PKG_NO_STATICSPLIT", "false") != "true"
 
     def subpackage_relation_with_parent(self) -> str:
-        m = re.search(r'^TERMUX_SUBPKG_DEPEND_ON_PARENT="?(true|false|deps|unversioned)"?$', self.content, re.MULTILINE)
-        if m:
-            return m.group(1)
-        return "true"  # Default to having dependence on parent.
+        # Default to having dependence on parent.
+        return self.variables.get("TERMUX_SUBPKG_DEPEND_ON_PARENT", "true")
 
     def get_dependencies(self, build_mode: BuildMode) -> Set[Dependency | AlternativeDependency]:
-        dependencies = []
+        keys = ["TERMUX_PKG_DEPENDS", "TERMUX_SUBPKG_DEPENDS"]
+        if build_mode == BuildMode.OFFLINE:
+            keys.append("TERMUX_PKG_BUILD_DEPENDS")
 
-        for m in re.findall(
-            # TERMUX_PKG_BUILD_DEPENDS should only list dependencies required at build time.
-            r'^(TERMUX_PKG_DEPENDS|TERMUX_SUBPKG_DEPENDS%s)[+]?="(.*?)"$'
-            % ("|TERMUX_PKG_BUILD_DEPENDS" if build_mode == BuildMode.OFFLINE else ""),
-            self.content,
-            re.DOTALL | re.MULTILINE,
-        ):
-            _, ds = m  # m: (DEPENDENCY_VAR, dependency string)
+        ds = ",".join(self.variables[k] for k in keys if k in self.variables)
 
-            ds = re.sub(r"\${TERMUX_ARCH/_/-}", TERMUX_ARCH, ds)
-            ds = re.sub(r"\(.*\)", "", ds)
+        ds = re.sub(r"\${TERMUX_ARCH/_/-}", TERMUX_ARCH, ds)
+        ds = re.sub(r"\(.*\)", "", ds)
 
-            for d in ds.split(","):
-                if "|" in d:
-                    dependencies.append(AlternativeDependency(options=tuple(re.findall(r"[\w+.-]+", d))))
-                else:
-                    dependencies.append(d.strip())
-
-        return set(dependencies)
+        return {
+            AlternativeDependency(options=tuple(re.findall(r"[\w+.-]+", d))) if "|" in d else d.strip()
+            for d in ds.split(",")
+            if d
+        }
 
 
 class BuildOrder:
@@ -118,14 +120,21 @@ class BuildOrder:
         self.repos = repos
         self.build_mode = build_mode
 
-    def get_order(self) -> List[Package]:
-        return self.__generate_order()
+        # A map of package name to it's build script:
+        self._package_map: Dict[PackageName, Path] = {}
 
-    def __generate_order(self) -> List[Package]:
-        package_map: PackageMap = self.__generate_package_map()
+        for repo in self.repos:
+            for package_path in repo.iterdir():
+                package_name = package_path.name
 
-        logger.debug("Total packages in repo: %d" % len(package_map))
+                for subpackage_path in package_path.glob("*.subpackage.sh"):
+                    self._package_map[subpackage_path.name.removesuffix(".subpackage.sh")] = subpackage_path
 
+                self._package_map[package_name] = package_path / "build.sh"
+
+        logger.debug("Total packages in repo: %d" % len(self._package_map))
+
+    def generate_order(self) -> List[Package]:
         build_order: List[Package] = []
 
         stack: List[Tuple[PackageName, bool]] = []
@@ -135,8 +144,7 @@ class BuildOrder:
         def dfs():
             while stack:
                 node, is_backtracking = stack.pop()
-
-                package = package_map[node]
+                package = self.__get_package(node)
 
                 if is_backtracking:
                     path.pop()
@@ -149,8 +157,8 @@ class BuildOrder:
 
                 stack.append((node, True))  # Backtracking.
                 path.append(node)
-                logger.debug("'%s' depends upon: %s", package.name, package.dependencies)
-                for dep in package.dependencies:
+
+                for dep in self.__resolve_dependencies(package):
                     if isinstance(dep, AlternativeDependency):
                         dep = next(iter(visited.intersection(dep.options)), dep.default)
                     if dep in path:
@@ -159,157 +167,122 @@ class BuildOrder:
                         stack.append((dep, False))
 
         if self.root_pkg:
+            if self.__get_package(self.root_pkg).type == PackageType.VIRTUAL:
+                die("%s is either a metapackage or subpackage. Exiting..." % self.root_pkg)
             stack.append((self.root_pkg, False))
             dfs()
         else:
-            for pkg in package_map.keys():
-                if pkg not in visited:
+            for pkg in self._package_map.keys():
+                if pkg not in visited and self.__get_package(pkg).type != PackageType.VIRTUAL:
                     stack.append((pkg, False))
                     dfs()
 
         logger.debug("Total in build_order: %d", len(build_order))
 
-        build_order.pop()  # Remove self.root_pkg from the set.
+        build_order.pop()  # Remove root_pkg from the set.
         return build_order
 
-    def __generate_package_map(self) -> PackageMap:
-        package_map: Dict[PackageName, Package] = {}
+    @cache
+    def __get_package(self, package_name: PackageName) -> Package:
+        # A non-existent, virtual static package:
+        if package_name.endswith("-static") and package_name not in self._package_map:
+            parent = package_name.removesuffix("-static")
+            return Package(
+                name=package_name,
+                dependencies={parent},
+                type=PackageType.VIRTUAL,
+                path=self._package_map[parent].parent,
+            )
 
-        for repo in self.repos:
-            for package_path in repo.iterdir():
-                package_name = package_path.name
+        # It should exist now.
+        if package_name not in self._package_map:
+            die("%s is a non-existent package." % package_name)
 
-                if repo.name == "gpkg":
-                    s = package_name.split("-")
-                    package_name = (
-                        package_name
-                        if "glibc" in s or "glibc32" in s
-                        else (
-                            package_name.replace("-static", "-glibc-static")
-                            if "static" == s[-1]
-                            else package_name + "-glibc"
-                        )
-                    )
+        package_script_path = self._package_map[package_name]
+        package_path = package_script_path.parent
 
-                # For root package we are always in OFFLINE mode as we are building it:
-                build_mode = BuildMode.OFFLINE if package_name == self.root_pkg else self.build_mode
+        # For root package we are always in OFFLINE mode as we are building it:
+        build_mode = BuildMode.OFFLINE if package_name == self.root_pkg else self.build_mode
 
-                package_info = BuildScriptParser(package_path / "build.sh")
-                dependencies = package_info.get_dependencies(build_mode)
+        package_info = self.__parse_build_script(package_script_path)
+        dependencies = package_info.get_dependencies(build_mode)
 
-                if package_info.is_metapackage():
-                    package_map[package_name] = Package(
-                        name=package_name, path=package_path, dependencies=dependencies, type=PackageType.VIRTUAL
-                    )
-                    continue
+        if package_info.is_metapackage():
+            return Package(name=package_name, path=package_path, dependencies=dependencies, type=PackageType.VIRTUAL)
 
-                # This is not needed for metapackages.
-                if TERMUX_ON_DEVICE_BUILD and TERMUX_PACKAGE_LIBRARY == "bionic":
-                    for d in ON_DEVICE_ALWAYS_DEPENDENCIES:
-                        if package_name != d:
-                            dependencies.add(d)
-
-                # Note for self: See page 6 of your notebook.
-                if build_mode == BuildMode.OFFLINE:
-                    subpackages = []
-
-                    # NOTE: When package_name == self.root_pkg, we do not need to add subpackges as
-                    # no one should depend upon them (will cause circular dependency otherwise), but we
-                    # add them to catch circular dependencies later in __generate_order.
-
-                    for subpackage_path in package_path.glob("*.subpackage.sh"):
-                        subpackage_name = subpackage_path.name.removesuffix(".subpackage.sh")
-
-                        dependencies.update(BuildScriptParser(subpackage_path).get_dependencies(build_mode))
-                        subpackages.append(subpackage_name)
-
-                        package_map[subpackage_name] = Package(
-                            name=subpackage_name,
-                            path=package_path,
-                            dependencies={package_name},
-                            type=PackageType.VIRTUAL,
-                        )
-
-                    # Remove all the subpackages (of this parent) that might be in it's dependency set.
-                    dependencies.difference_update(subpackages)  # Note for self: See page 7 of your notebook.
-                    dependencies.discard(package_name)  # Some subpackages might explicitly list dependence on parent.
-                else:
-                    for subpackage_path in package_path.glob("*.subpackage.sh"):
-                        subpackage_name = subpackage_path.name.removesuffix(".subpackage.sh")
-                        subpackage_info = BuildScriptParser(subpackage_path)
-                        subpackage_dependencies = subpackage_info.get_dependencies(build_mode)
-
-                        # See the following link to understand this behaviour:
-                        # https://github.com/termux/termux-packages/blob/abdfb2a00f7d2f2bdd6de42a1a085948c68c050c/scripts/build/termux_create_debian_subpackages.sh#L104
-                        match subpackage_info.subpackage_relation_with_parent():
-                            case "true" | "unversioned" if subpackage_name not in dependencies:
-                                subpackage_dependencies.add(package_name)
-                            case "deps":
-                                subpackage_dependencies.update(dependencies)
-
-                        package_map[subpackage_name] = Package(
-                            name=subpackage_name,
-                            path=package_path,
-                            dependencies=subpackage_dependencies,
-                            type=PackageType.REAL,
-                        )
-
-                package_map[package_name] = Package(
-                    name=package_name, path=package_path, dependencies=dependencies, type=PackageType.REAL
+        if package_script_path.name.endswith(".subpackage.sh"):
+            if build_mode == BuildMode.OFFLINE:
+                return Package(
+                    name=package_name, path=package_path, dependencies={package_path.name}, type=PackageType.VIRTUAL
                 )
+            else:
+                # See the following link to understand this behaviour:
+                # https://github.com/termux/termux-packages/blob/abdfb2a00f7d2f2bdd6de42a1a085948c68c050c/scripts/build/termux_create_debian_subpackages.sh#L104
+                match package_info.subpackage_relation_with_parent():
+                    case "true" | "unversioned" if package_name not in dependencies:
+                        dependencies.add(package_path.name)  # Parent.
+                    case "deps":
+                        dependencies.update(self.__parse_build_script(package_path).get_dependencies(build_mode))
+                return Package(name=package_name, path=package_path, dependencies=dependencies, type=PackageType.REAL)
 
-                static_package = package_name + "-static"
-                if static_package not in package_map and package_info.is_static_splitting_allowed():
-                    package_map[static_package] = Package(
-                        name=static_package, dependencies={package_name}, type=PackageType.VIRTUAL, path=package_path
-                    )
+        if build_mode == BuildMode.OFFLINE:
+            subpackages = []
 
-        sanitized_map: PackageMap = {}
+            for subpackage_path in package_path.glob("*.subpackage.sh"):
+                dependencies.update(self.__parse_build_script(subpackage_path).get_dependencies(build_mode))
+                subpackages.append(subpackage_path.name.removesuffix(".subpackage.sh"))
 
-        @cache
-        def resolve(dep: Dependency | AlternativeDependency) -> List[Dependency | AlternativeDependency]:
+            # Remove all the subpackages (of this parent) that might be in it's dependency set.
+            dependencies.difference_update(subpackages)
+            dependencies.discard(package_name)  # Some subpackages might explicitly list dependence on parent.
+
+        # This is not needed for metapackages and subpackages.
+        if TERMUX_ON_DEVICE_BUILD and TERMUX_PACKAGE_LIBRARY == "bionic":
+            for d in ON_DEVICE_ALWAYS_DEPENDENCIES:
+                if package_name != d:
+                    dependencies.add(d)
+
+        return Package(name=package_name, path=package_path, dependencies=dependencies, type=PackageType.REAL)
+
+    @cache
+    def __parse_build_script(self, script_path: Path) -> BuildScriptParser:
+        return BuildScriptParser(script_path)
+
+    @cache
+    def __resolve_dependencies(self, package: Package) -> List[Dependency | AlternativeDependency]:
+        def resolve(dep: Dependency | AlternativeDependency):
             if isinstance(dep, AlternativeDependency):
                 flattened_options = []
                 for d in dep.options:
                     flattened_options.extend(resolve(d))
                 return [AlternativeDependency(options=tuple(flattened_options))]
 
-            if package_map[dep].type == PackageType.REAL:
+            package = self.__get_package(dep)
+
+            if package.type == PackageType.REAL:
                 return [dep]
 
-            # For PackageType.VIRTUAL:
             flattened_dependencies = []
-            for d in package_map[dep].dependencies:
+            for d in package.dependencies:
                 flattened_dependencies.extend(resolve(d))
             return flattened_dependencies
 
-        for package in package_map.values():
-            if package.type == PackageType.REAL:
-                sanitized_deps = []
-                for dep in package.dependencies:
-                    # Why this skip?
-                    # initially: dotnet8.0 -> dotnet-host -> dotnet-host-8.0 | dotnet-host-9.0
-                    # resolved: dotnet8.0 -> dotnet8.0 | dotnet9.0
-                    # Since, dotnet8.0 is in resolved AlternativeDependency, dotnet-host is built "out of" dotnet8.0.
-                    # Hence, we skip.
-                    logger.debug("Resloving '%s' ==> %s", dep, resolve(dep))
-                    sanitized_deps.extend(
-                        d
-                        for d in resolve(dep)
-                        if not (isinstance(d, AlternativeDependency) and package.name in d.options)
-                    )
-                sanitized_map[package.name] = Package(
-                    name=package.name, path=package.path, dependencies=set(sanitized_deps), type=package.type
-                )
-        return sanitized_map
+        resolved_deps = []
+        for dep in package.dependencies:
+            # Why this skip?
+            # initially: dotnet8.0 -> dotnet-host -> dotnet-host-8.0 | dotnet-host-9.0
+            # resolved: dotnet8.0 -> dotnet8.0 | dotnet9.0
+            # Since, dotnet8.0 is in resolved AlternativeDependency, dotnet-host is built "out of" dotnet8.0.
+            # Hence, we skip.
+            resolved_deps.extend(
+                d for d in resolve(dep) if not (isinstance(d, AlternativeDependency) and package.name in d.options)
+            )
+        return resolved_deps
 
 
-def die(msg: str) -> None:
-    print("ERROR:", msg)
-    exit(1)
+start_time = time.perf_counter()
 
-
-def main() -> None:
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(
@@ -369,29 +342,11 @@ def main() -> None:
     """)
     # END TEMP
 
-    for order in BuildOrder(package, repos, build_mode).get_order():
+    for order in BuildOrder(package, repos, build_mode).generate_order():
         package_name = order.name
-
-        # TODO: This is not complete. Implement for dfs too as there would be no `glibc-` packages in the package
-        # map. We need to add there too.
-        if TERMUX_GLOBAL_LIBRARY and TERMUX_PACKAGE_LIBRARY == "glibc":
-            s = package_name.split("-")
-            package_name = (
-                package_name
-                if "glibc" in s or "glibc32" in s
-                else (
-                    package_name.replace("-static", "-glibc-static") if "static" == s[-1] else package_name + "-glibc"
-                )
-            )
 
         logger.debug("%-40s %s" % (package_name, f"{order.path.parent.name}/{order.path.name}"))
         print("%-40s %s" % (package_name, f"{order.path.parent.name}/{order.path.name}"))
-
-
-start_time = time.perf_counter()
-
-if __name__ == "__main__":
-    main()
 
 end_time = time.perf_counter()
 
