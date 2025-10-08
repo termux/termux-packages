911799bfdc05394927597d900c310276f5a1938a
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-10-08 23:35:50 +0800

    telegram-desktop: bump to 6.1.4 (#26802)

--- x11-packages/telegram-desktop/build.sh
+++ x11-packages/telegram-desktop/build.sh
@@ -4,10 +4,9 @@ TERMUX_PKG_DESCRIPTION="Telegram Desktop Client"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LEGAL"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.1.3"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="6.1.4"
 TERMUX_PKG_SRCURL=https://github.com/telegramdesktop/tdesktop/releases/download/v$TERMUX_PKG_VERSION/tdesktop-$TERMUX_PKG_VERSION-full.tar.gz
-TERMUX_PKG_SHA256=1c6a531abf106d5f4b6d9179fc802f93cb8ab62630cc07e73d64688780125869
+TERMUX_PKG_SHA256=0dc2a36eea9e5f71892530b5c8218274b16999ba6050a396a9dc4dc052033f83
 TERMUX_PKG_DEPENDS="abseil-cpp, boost, ffmpeg, glib, hicolor-icon-theme, hunspell, kf6-kcoreaddons, libandroid-shmem, libc++, libdispatch, libdrm, libjxl, liblz4, libminizip, protobuf, librnnoise, libsigc++-3.0, libx11, libxcomposite, libxdamage, libxrandr, libxtst, openal-soft, opengl, openh264, openssl, pipewire, pulseaudio, qt6-qtbase, qt6-qtimageformats, qt6-qtsvg, xxhash, zlib"
 TERMUX_PKG_BUILD_DEPENDS="ada, aosp-libs, boost-headers, glib-cross, qt6-qtbase-cross-tools"
 TERMUX_PKG_VERSIONED_GIR=false
@@ -85,8 +84,7 @@ __cppgir_build() {
 	pushd cppgir-host-build
 	local _extra_args=""
 	if [ "$TERMUX_ON_DEVICE_BUILD" = true ]; then
-		_extra_args+=" -DGI_DEFAULT_GIRPATH=$TERMUX_PREFIX/share"
-		_extra_args+=" -DGI_DEF_DIR=$TERMUX_PREFIX/share/gir-1.0"
+		_extra_args+=" -DGIR_DEFAULT_DIRS=$TERMUX_PREFIX/share"
 	fi
 	cmake \
 		-DCMAKE_INSTALL_PREFIX=$TERMUX_PKG_HOSTBUILD_DIR/cppgir-host-build/prefix \
