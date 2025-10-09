cce43292432ec93647e3192186239a6c3611ef40
BullyMaguire-lol <temp9040@gmail.com>
2025-10-09 15:16:16 +0530

    addpkg(x11/kf6-ki18n-cross-tools): 6.19.0

--- x11-packages/kf6-ki18n/build.sh
+++ x11-packages/kf6-ki18n/build.sh
@@ -1,12 +1,14 @@
-TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/ki18n"
 TERMUX_PKG_DESCRIPTION="KDE Gettext-based UI text internationalization"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.19.0"
+TERMUX_PKG_REVISION=1
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/ki18n-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=608ff2634cd19736a8091c750c71b23db0b33fd81e82fb9fc4bcce76712284a6
+TERMUX_PKG_SHA256="608ff2634cd19736a8091c750c71b23db0b33fd81e82fb9fc4bcce76712284a6"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_DEPENDS="gettext, iso-codes, libc++, qt6-qtbase, qt6-qtdeclarative"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), python, qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -14,3 +16,29 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
+
+termux_step_host_build() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "true" ]]; then
+		return
+	fi
+
+	termux_setup_cmake
+	termux_setup_ninja
+
+	cmake -G Ninja \
+		-S "${TERMUX_PKG_SRCDIR}" \
+		-B "${TERMUX_PKG_HOSTBUILD_DIR}" \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX=$TERMUX_PREFIX/opt/kf6/cross \
+		-DCMAKE_PREFIX_PATH="$TERMUX_PREFIX/opt/qt6/cross/lib/cmake" \
+		-DCMAKE_MODULE_PATH="$TERMUX_PREFIX/share/ECM/modules" \
+		-DECM_DIR="$TERMUX_PREFIX/share/ECM/cmake" \
+		-DTERMUX_PREFIX="$TERMUX_PREFIX" \
+		-DCMAKE_INSTALL_LIBDIR=lib
+
+	ninja -j ${TERMUX_PKG_MAKE_PROCESSES} install
+}
+
+termux_step_pre_configure() {
+	rm -rf $TERMUX_HOSTBUILD_MARKER
+}
