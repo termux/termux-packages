12d3b5c56f9cd36dadc46cf458014c061fd8231e
TomIO <tom@termux.dev>
2025-10-12 16:43:00 +0200

    bump(main/redis): 8.2.2 + change to AGPLv3 license

--- packages/redis/build.sh
+++ packages/redis/build.sh
@@ -1,17 +1,17 @@
 TERMUX_PKG_HOMEPAGE=https://redis.io/
 TERMUX_PKG_DESCRIPTION="In-memory data structure store used as a database, cache and message broker"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-# Frozen! Do not update to 7.4.0
-# until the license/replacement discussion is concluded
-TERMUX_PKG_VERSION="1:7.2.5"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1:8.2.2"
 TERMUX_PKG_SRCURL=https://download.redis.io/releases/redis-${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=5981179706f8391f03be91d951acafaeda91af7fac56beffb2701963103e423d
-TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_SHA256=4e340e8e822a82114b6fb0f7ca581b749fa876e31e36e9fbcb75416bec9d0608
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/redis.conf"
+TERMUX_PKG_BREAKS="valkey"
+TERMUX_PKG_CONFLICTS="valkey"
+TERMUX_PKG_REPLACES="valkey"
 
 termux_step_pre_configure() {
 	export PREFIX=$TERMUX_PREFIX
