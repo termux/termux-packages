c87a79a1b9c5d7a9ac82aa6ee121de23cd6f8459
Biswapriyo Nath <nathbappai@gmail.com>
2025-10-12 14:16:59 +0000

    fix(x11/kf6-kwindowsystem): remove qt6-qtwayland dependency
    wayland libraries are provided by qt6-qtbase >= 6.10.0

--- x11-packages/kf6-kwindowsystem/build.sh
+++ x11-packages/kf6-kwindowsystem/build.sh
@@ -10,11 +10,12 @@ LICENSES/LicenseRef-KDE-Accepted-LGPL.txt
 LICENSES/MIT.txt"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.19.0"
+TERMUX_PKG_REVISION=1
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kwindowsystem-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=7ef870cc6c37fc98e4655c267cda069b28259640524d1088d4434350827e59da
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, libwayland, libx11, libxcb, libxfixes, qt6-qtbase, qt6-qtdeclarative, qt6-qtwayland, xcb-util-keysyms"
+TERMUX_PKG_DEPENDS="libc++, libwayland, libx11, libxcb, libxfixes, qt6-qtbase, qt6-qtdeclarative, xcb-util-keysyms"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), libwayland-protocols, plasma-wayland-protocols, qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
