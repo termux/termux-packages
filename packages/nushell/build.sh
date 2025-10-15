c295a05854f706da0e08937535bc1663b32eec26
Juhan280 <azurarobo280@gmail.com>
2025-10-15 21:10:41 +0600

    bump(main/nushell): 0.108.0
    See https://www.nushell.sh/blog/2025-10-15-nushell_v0_108_0.html#add-network-feature-to-top-level-crate-16686-toc

--- packages/nushell/build.sh
+++ packages/nushell/build.sh
@@ -2,15 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.nushell.sh
 TERMUX_PKG_DESCRIPTION="A new type of shell operating on structured data"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.107.0"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.108.0"
 TERMUX_PKG_SRCURL=https://github.com/nushell/nushell/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e5b91463b915c26e72a7be48ddac411e02e25daca1887ec4cf7b98b3b4e6e72e
+TERMUX_PKG_SHA256=5995c211411ad1d5dd7da904b9db238a543958675b9e45f5e84fbdf217499eee
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_RECOMMENDS="command-not-found, termux-api"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features plugin,trash-support,sqlite,native-tls"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--no-default-features --features plugin,trash-support,sqlite,network,native-tls"
 
 termux_step_pre_configure() {
 	termux_setup_rust
