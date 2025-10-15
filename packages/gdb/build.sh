9485dd585dac0145679c62b4a6af6143256e30b5
Robert Kirkman <rkirkman@termux.dev>
2025-10-15 09:26:08 -0500

    bump(main/guile): 3.0.10
    - Fixes https://github.com/termux/termux-packages/issues/26903
    
    - Consolidate `TERMUX_PKG_EXTRA_CONFIGURE_ARGS` for readability
    
    - Apply cross-compiling fix patch for Guile 3.0.10 found at https://lists.debian.org/debian-cross/2025/04/msg00001.html
    
    - Revision-bump reverse dependencies

--- packages/gdb/build.sh
+++ packages/gdb/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 # This package depends on libpython${TERMUX_PYTHON_VERSION}.so.
 # Please revbump and rebuild when bumping TERMUX_PYTHON_VERSION.
 TERMUX_PKG_VERSION="16.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/gdb/gdb-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=bcfcd095528a987917acf9fff3f1672181694926cc18d609c99d0042c00224c5
 TERMUX_PKG_DEPENDS="guile, libc++, libexpat, libgmp, libiconv, liblzma, libmpfr, libthread-db, ncurses, python, readline, zlib, zstd"
