1c901f5efb3d3e1c4bd1a5af22965564a1e99d01
Finagolfin <finagolfin@tuta.io>
2025-10-16 09:02:54 +0530

    rebuild(cmus): Remove time.h reset that's no longer needed with new Bionic guard patch, as pointed out by @robertkirkman

--- packages/cmus/build.sh
+++ packages/cmus/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small, fast and powerful console music player"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.12.0"
-TERMUX_PKG_REVISION="5"
+TERMUX_PKG_REVISION="6"
 TERMUX_PKG_DEPENDS="ffmpeg, libandroid-support, libflac, libiconv, libmad, libmodplug, libvorbis, libwavpack, ncurses, opusfile, pulseaudio, alsa-lib"
 TERMUX_PKG_SRCURL=https://github.com/cmus/cmus/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=44b96cd5f84b0d84c33097c48454232d5e6a19cd33b9b6503ba9c13b6686bfc7
@@ -27,14 +27,6 @@ termux_step_pre_configure() {
 		mkdir -p "${_libdir}"
 		cp "$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/26/libaaudio.so" "${_libdir}"
 		LDFLAGS+=" -L${_libdir}"
-		# this commit https://github.com/termux/termux-packages/commit/0970a2f2d89f64acd7a3fc94845d298bee010cc6 ,
-		# which was necessary to compile coreutils with NDK 27,
-		# is, on the other hand, when combined with NDK r28c,
-		# incompatible with '-D__ANDROID_UNAVAILABLE_SYMBOLS_ARE_WEAK__ -Werror=unguarded-availability,'
-		# which is used internally by the cmus package,
-		# so the vanilla header must be used for cmus in order to pass its termux_step_post_massage() successfully.
-		cp "$HOME/lib/android-ndk-r$TERMUX_NDK_VERSION/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/time.h" "${_libdir}"
-		CFLAGS+=" -I${_libdir}/"
 	fi
 
 	LD=$CC
