aab71dc9287cb0d1a21fe055dd652618d2314a41
Robert Kirkman <rkirkman@termux.dev>
2025-10-21 09:01:28 -0500

    bump(main/ripgrep-all): 0.10.9
    - Change maintainer; @kcubeterm is inactive
    
    - Bump epoch
    
    - Update dependencies to match Arch Linux package
    
    - Replace custom `rga-fzf` script with official executable
    
    - Provide new `rgz-fzf-open` executable
    
    - Enable `--all-features`

--- packages/ripgrep-all/build.sh
+++ packages/ripgrep-all/build.sh
@@ -1,13 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/phiresky/ripgrep-all
 TERMUX_PKG_DESCRIPTION="Search tool able to locate in PDFs, E-Books, zip, tar.gz, etc"
 TERMUX_PKG_LICENSE="AGPL-V3"
-TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
-TERMUX_PKG_VERSION="1.0.0-alpha.5"
-TERMUX_PKG_REVISION=4
-TERMUX_PKG_DEPENDS="ripgrep, fzf"
-TERMUX_PKG_RECOMMENDS="ffmpeg, poppler, sqlite"
-TERMUX_PKG_SRCURL=https://github.com/phiresky/ripgrep-all/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bda5944e3fdfba4348b1b27b3ab770c5daf5a62feff86a1dcaa4221f450c7f19
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1:0.10.9"
+TERMUX_PKG_DEPENDS="fzf, ripgrep, xz-utils"
+TERMUX_PKG_RECOMMENDS="ffmpeg, graphicsmagick, pandoc, poppler, tesseract"
+TERMUX_PKG_SRCURL="https://github.com/phiresky/ripgrep-all/archive/refs/tags/v${TERMUX_PKG_VERSION:2}.tar.gz"
+TERMUX_PKG_SHA256=a5b3150940dcddd35a26e9de398f11a563d0466a335e5450ceb7ff369e9fef45
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -18,11 +17,16 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+	cargo build \
+		--jobs "$TERMUX_PKG_MAKE_PROCESSES" \
+		--target "$CARGO_TARGET_NAME" \
+		--release \
+		--all-features
 }
 
 termux_step_make_install() {
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rga
-	install -Dm755 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/rga-preproc
-	install -m755 $TERMUX_PKG_BUILDER_DIR/rga-fzf  $TERMUX_PREFIX/bin/rga-fzf
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/rga"
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/rga-preproc"
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/rga-fzf"
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "target/${CARGO_TARGET_NAME}/release/rga-fzf-open"
 }
