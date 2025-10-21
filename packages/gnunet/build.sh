1022434abe12dedbb15bacf708e2816ab2b52f75
Biswapriyo Nath <nathbappai@gmail.com>
2025-10-21 07:19:47 +0000

    bump(main/gnunet): 0.25.2
    Remove patch file which was added in upstream.
    https://git.gnunet.org/gnunet.git/commit/src/include/gnunet_testing_lib.h?id=9ac6841eadc9f4b3b1e71e2ec08e75d94e851149

--- packages/gnunet/build.sh
+++ packages/gnunet/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gnunet.org
 TERMUX_PKG_DESCRIPTION="A framework for secure peer-to-peer networking"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.25.1"
+TERMUX_PKG_VERSION="0.25.2"
 TERMUX_PKG_SRCURL=https://ftpmirror.gnu.org/gnu/gnunet/gnunet-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=21336c16cd57f91f9d5fd5359482d9151a7cdf0d6396f8b61828c17ccc668f5c
+TERMUX_PKG_SHA256=eab76fc35d393ab15fadb6344f843525c165619400735aa6dde0966f6a26c2e6
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libcurl, libgcrypt, libgnutls, libgpg-error, libidn2, libjansson, libltdl, libmicrohttpd, libsodium, libsqlite, libunistring, zlib"
 
