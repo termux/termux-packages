5d15fcc2dac54578b5fc3f25fc01bf710d0c8cbe
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-10-25 23:19:55 +0800

    tgpt: bump to 2.10.1

--- packages/tgpt/build.sh
+++ packages/tgpt/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/aandrew-me/tgpt
 TERMUX_PKG_DESCRIPTION="AI Chatbots in terminal without needing API keys"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.10.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.10.1"
 TERMUX_PKG_SRCURL=https://github.com/aandrew-me/tgpt/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0e312176908d581eeb7f0df8fcd0524a4aa4702029d50f553f0f75d6c15bc0d9
+TERMUX_PKG_SHA256=a0fc596fd2b8aabac222e22e96fc02709e185031e31a15b522f4c5c59db892d9
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
@@ -14,7 +13,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	go build
+	go build -trimpath -ldflags="-s -w"
 }
 
 termux_step_make_install() {
