0fafa6e13a4b76dc3f70fdac985fa9bd448103a2
ilyas-ozt <ilyasozt4@gmail.com>
2025-10-27 13:11:08 +0100

    Update build.sh

--- packages/vlang/build.sh
+++ packages/vlang/build.sh
@@ -1,19 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://vlang.io/
 TERMUX_PKG_DESCRIPTION="V programming language compiler"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@ilyas-ozt"
-TERMUX_PKG_VERSION="0.1.0"
-TERMUX_PKG_GIT_URL=https://github.com/vlang/v
-TERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_DEPENDS="clang, libandroid-execinfo, libgc, libgc-static, make"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.4.12
+TERMUX_PKG_SRCURL=https://github.com/vlang/v/archive/refs/tags/weekly.2025.43.tar.gz
+TERMUX_PKG_SHA256=4aefc5b0228815735011b4f49edd4877b5712b7a7539aaadd6ea8c2ca514f1a9
+TERMUX_PKG_DEPENDS="clang, libandroid-execinfo, libgc"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make() {
-    make
-}
 
 termux_step_make_install() {
     install -Dm700 v "$TERMUX_PREFIX/bin/v"
-    install -Dm644 LICENSE "$TERMUX_PREFIX/share/doc/vlang/LICENSE"
-    [ -f README.md ] && install -Dm644 README.md "$TERMUX_PREFIX/share/doc/vlang/README.md"
+    termux_step_install_license
 }
