90d899ac1bc66c289b8a822ea6c67fdf77439f99
Biswapriyo Nath <nathbappai@gmail.com>
2025-10-29 05:33:27 +0000

    bump(main/libdvdnav): 7.0.0
    Enable auto update and soversion check.

--- packages/libdvdnav/build.sh
+++ packages/libdvdnav/build.sh
@@ -2,8 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://www.videolan.org/developers/libdvdnav.html
 TERMUX_PKG_DESCRIPTION="A library that allows easy use of sophisticated DVD navigation features"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.1.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.videolan.org/pub/videolan/libdvdnav/${TERMUX_PKG_VERSION}/libdvdnav-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=c191a7475947d323ff7680cf92c0fb1be8237701885f37656c64d04e98d18d48
+TERMUX_PKG_VERSION="7.0.0"
+TERMUX_PKG_SRCURL=https://code.videolan.org/videolan/libdvdnav/-/archive/${TERMUX_PKG_VERSION}/libdvdnav-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=15d28086937647a95c3d6b083f0a86678cd4dd428914e319c64adf52cadec786
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdvdread"
+
+termux_step_pre_configure() {
+	export TERMUX_MESON_ENABLE_SOVERSION=1
+}
+
+termux_step_post_massage() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _GUARD_FILE="lib/libdvdnav.so.4"
+	if [ ! -e "${_GUARD_FILE}" ]; then
+		termux_error_exit "file ${_GUARD_FILE} not found."
+	fi
+}
