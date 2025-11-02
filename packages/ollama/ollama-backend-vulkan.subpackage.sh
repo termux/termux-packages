d56559f5f3a440c18fe3d2fa7b01dfbbde4418c9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-11-02 12:50:11 +0800

    ollama: bump to 0.12.9 (#27080)

--- packages/ollama/ollama-backend-vulkan.subpackage.sh
+++ packages/ollama/ollama-backend-vulkan.subpackage.sh
@@ -2,5 +2,5 @@ TERMUX_SUBPKG_DESCRIPTION="Vulkan Backend for Ollama"
 TERMUX_SUBPKG_RECOMMENDS="vulkan-loader"
 TERMUX_SUBPKG_SUGGESTS="vulkan-loader-generic, mesa-vulkan-icd-freedreno"
 TERMUX_SUBPKG_INCLUDE="
-lib/libggml-vulkan.so
+lib/ollama/libggml-vulkan.so
 "
