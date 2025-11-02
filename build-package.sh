2974a3ba9794b3925bd578b88d174d9eeb051cef
Fredrik Fornwall <fredrik@fornwall.net>
2025-11-03 00:43:14 +0100

    scripts(toolchain): Update NDK to r29

--- build-package.sh
+++ build-package.sh
@@ -269,8 +269,8 @@ source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_handle_host_build.sh"
 source "$TERMUX_SCRIPTDIR/scripts/build/termux_step_host_build.sh"
 
 # Setup a standalone Android NDK toolchain. Called from termux_step_setup_toolchain.
-# shellcheck source=scripts/build/toolchain/termux_setup_toolchain_28c.sh
-source "$TERMUX_SCRIPTDIR/scripts/build/toolchain/termux_setup_toolchain_28c.sh"
+# shellcheck source=scripts/build/toolchain/termux_setup_toolchain_29.sh
+source "$TERMUX_SCRIPTDIR/scripts/build/toolchain/termux_setup_toolchain_29.sh"
 
 # Setup a standalone Android NDK 23c toolchain. Called from termux_step_setup_toolchain.
 # shellcheck source=scripts/build/toolchain/termux_setup_toolchain_23c.sh
