4056592230609b5c2bb3f80daf8cc303be4d358d
Robert Kirkman <rkirkman@termux.dev>
2025-11-03 05:34:26 -0600

    fix(main/{cabal-install,pandoc}): fix build with NDK r28c
    - Progess on https://github.com/termux/termux-packages/issues/23492

--- packages/cabal-install/build.sh
+++ packages/cabal-install/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The command-line interface for Haskell-Cabal and Hackage
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.org>"
 TERMUX_PKG_VERSION=3.14.1.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/cabal-install-${TERMUX_PKG_VERSION}/cabal-install-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=f11d364ab87fb46275a987e60453857732147780a8c592460eec8a16dbb6bace
 TERMUX_PKG_AUTO_UPDATE=false
@@ -22,6 +23,21 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-f-native-dns --ghc-options=-optl-landroid-posi
 TERMUX_PKG_EXCLUDED_ARCHES="i686"
 
 termux_step_post_configure() {
+	cabal get splitmix-0.1.3.1
+	mv splitmix{-*,}
+
+	for f in "$TERMUX_PKG_BUILDER_DIR"/splitmix-patches/*.patch; do
+		patch --silent -p1 -d splitmix < "$f"
+	done
+
+	cat <<-EOF >>cabal.project.local
+		packages: splitmix
+
+		package splitmix
+			benchmarks: False
+			tests: False
+	EOF
+
 	if [[ "$TERMUX_ON_DEVICE_BUILD" == false ]]; then # We do not need iserv for on device builds.
 		termux_setup_ghc_iserv
 		cat <<-EOF >>cabal.project.local
