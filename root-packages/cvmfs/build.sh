e958e627c9682d340e73304f19499aef87ab916d
crivella <davide.grassano@epfl.ch>
2025-11-05 16:25:11 +0100

    Order patches + fix for dlopen from termux_prefix + test internal leveldb

--- root-packages/cvmfs/build.sh
+++ root-packages/cvmfs/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@Crivella"
 TERMUX_PKG_VERSION="2.13.3"
 TERMUX_PKG_SRCURL=https://github.com/cvmfs/cvmfs/archive/refs/tags/cvmfs-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1ee9db980608d6cd25c6566c49acf5903b67e9110774563df4ca2397eb137393
-TERMUX_PKG_DEPENDS="c-ares, libc++, libandroid-execinfo, libcurl, zlib, leveldb, libprotobuf, protobuf, libsqlite, libarchive, libuuid, libcap, sparsehash, libfuse3"
+TERMUX_PKG_DEPENDS="c-ares, libc++, libandroid-execinfo, libcurl, zlib, libprotobuf, protobuf, libsqlite, libarchive, libuuid, libcap, sparsehash, libfuse3"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -20,7 +20,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_STRESS_TESTS=OFF
 
 -DBUILTIN_EXTERNALS=ON
--DBUILTIN_EXTERNALS_LIST=vjson;sha3;libcrypto;pacparser
+-DBUILTIN_EXTERNALS_LIST=vjson;sha3;libcrypto;pacparser;leveldb;
 
 -DCARES_INCLUDE_DIR=$TERMUX_PREFIX/include
 -DCARES_LIBRARY=$TERMUX_PREFIX/lib/libcares.so
@@ -32,14 +32,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLibArchive_LIBRARY=$TERMUX_PREFIX/lib/libarchive.so
 -DSPARSEHASH_INCLUDE_DIR=$TERMUX_PREFIX/include/sparsehash
 
--DPACPARSER_INCLUDE_DIR=$TERMUX_PKG_SRCDIR/externals_install.aarch64/include
--DPACPARSER_LIBRARY=$TERMUX_PKG_SRCDIR/externals_install.aarch64/lib/libpacparser.a
--DVJSON_INCLUDE_DIRS=$TERMUX_PKG_SRCDIR/externals_install.aarch64/include
--DVJSON_LIBRARIES=$TERMUX_PKG_SRCDIR/externals_install.aarch64/lib/libvjson.a
--DSHA3_INCLUDE_DIRS=$TERMUX_PKG_SRCDIR/externals_install.aarch64/include
--DSHA3_LIBRARIES=$TERMUX_PKG_SRCDIR/externals_install.aarch64/lib/libsha3.a
--DLibcrypto_INCLUDE_DIRS=$TERMUX_PKG_SRCDIR/externals_install.aarch64/crypto/include
--DLibcrypto_LIBRARIES=$TERMUX_PKG_SRCDIR/externals_install.aarch64/crypto/lib/libcrypto.a
+-DLEVELDB_INCLUDE_DIR=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/include/leveldb
+-DLEVELDB_LIBRARY=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/lib/libleveldb.a
+-DPACPARSER_INCLUDE_DIR=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/include
+-DPACPARSER_LIBRARY=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/lib/libpacparser.a
+-DVJSON_INCLUDE_DIRS=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/include
+-DVJSON_LIBRARIES=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/lib/libvjson.a
+-DSHA3_INCLUDE_DIRS=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/include
+-DSHA3_LIBRARIES=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/lib/libsha3.a
+-DLibcrypto_INCLUDE_DIRS=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/crypto/include
+-DLibcrypto_LIBRARIES=$TERMUX_PKG_SRCDIR/externals_install.${TERMUX_ARCH}/crypto/lib/libcrypto.a
 "
 
 termux_step_pre_configure () {
@@ -56,3 +58,9 @@ termux_step_pre_configure () {
     export TERMUX_STANDALONE_TOOLCHAIN
     export LDFLAGS
 }
+
+# Potential issues:
+# when cvmfs compiles its external pacparser dependency, it needs to generate an executable that than writes som header files
+# This will also dumb the length of certain sizeofs into the generated files.
+# When cross-compiling this executable needs to be runnable on the build host, but if the bitness differs between host and target,
+# The compilation will fail afterward due to a precompiler check that the sizeof is what is expected.
