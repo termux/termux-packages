3a36d8b8eb460f9140a60d4f9e3180742b061aa4
TomIO <tom@termux.dev>
2025-11-05 07:18:00 +0100

    bump(main/libsqlite): 3.51.0

--- packages/libsqlite/build.sh
+++ packages/libsqlite/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.sqlite.org
 TERMUX_PKG_DESCRIPTION="Library implementing a self-contained and transactional SQL database engine"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.50.4"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="3.51.0"
 _SQLITE_YEAR=2025
 TERMUX_PKG_SRCURL=https://www.sqlite.org/${_SQLITE_YEAR}/sqlite-src-$(sed 's/\./''/; s/\./0/' <<< "$TERMUX_PKG_VERSION")00.zip
-TERMUX_PKG_SHA256=b7b4dc060f36053902fb65b344bbbed592e64b2291a26ac06fe77eec097850e9
+TERMUX_PKG_SHA256=5330719b8b80bf563991ff7a373052943f5357aae76cd1f3367eab845d3a75b7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="zlib"
 TERMUX_PKG_BUILD_DEPENDS="tcl"
@@ -47,7 +46,4 @@ termux_step_make_install() {
 		-exec install -vDm700 "{}" "${TERMUX_PREFIX}/lib/sqlite3/libtclsqlite3.so" \;
 	find "$TERMUX_PKG_TMPDIR/libsqlite${TERMUX_PKG_VERSION}" -name pkgIndex.tcl \
 		-exec install -vDm600 "{}" "${TERMUX_PREFIX}/lib/sqlite3/pkgIndex.tcl" \;
-
-	# Remove duplicates
-	rm "$TERMUX_PREFIX/lib"/{pkgIndex.tcl,libtclsqlite3.so}
 }
