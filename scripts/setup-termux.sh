73bca0ff86a388638d0ac4bcbff4e450cdae959a
Robert Kirkman <rkirkman@termux.dev>
2025-11-05 04:29:42 -0600

    fix(scripts/setup-termux): add some more packages to continue to synchronize `setup-termux.sh` closer to `setup-ubuntu.sh`
    - Follow-up to https://github.com/termux/termux-packages/pull/24977
    
    - Fixes https://github.com/termux/termux-packages/issues/26373
    
    - Split from https://github.com/termux/termux-packages/pull/23652 because it does not really depend on the other things in that PR and it is now needed separately

--- scripts/setup-termux.sh
+++ scripts/setup-termux.sh
@@ -8,6 +8,7 @@ PACKAGES+=" gnupg"				# Used in termux_get_repo_files() and build-package.sh.
 PACKAGES+=" lzip"				# Used by tar to extract *.tar.lz source archives.
 PACKAGES+=" patch"				# Used for applying patches on source code.
 PACKAGES+=" python"				# Used buildorder.py core script.
+PACKAGES+=" python-pip" # Necessary to install 'itstool' for on-device-building (since Ubuntu gets it from 'apt')
 PACKAGES+=" unzip"				# Used to extract *.zip source archives.
 PACKAGES+=" jq"					# Used for parsing repo.json.
 PACKAGES+=" binutils-is-llvm"			# Used for checking symbols.
@@ -25,9 +26,11 @@ PACKAGES+=" ed"
 PACKAGES+=" flex"
 PACKAGES+=" gettext"
 PACKAGES+=" git"
+PACKAGES+=" glslang"                    # Needed by mesa
 PACKAGES+=" golang"
 PACKAGES+=" gperf"
 PACKAGES+=" help2man"
+PACKAGES+=" intltool"                   # Needed by qalc
 PACKAGES+=" libtool"
 PACKAGES+=" llvm-tools"		# Needed to build rust
 PACKAGES+=" m4"
@@ -42,10 +45,24 @@ PACKAGES+=" re2c"                       # Needed by kphp-timelib
 PACKAGES+=" rust"
 PACKAGES+=" scdoc"
 PACKAGES+=" texinfo"
+PACKAGES+=" spirv-tools"                # Needed by mesa
 PACKAGES+=" uuid-utils"
 PACKAGES+=" valac"
 PACKAGES+=" xmlto"                      # Needed by git's manpage generation
 PACKAGES+=" zip"
+
+PYTHON_PACKAGES=""
+PYTHON_PACKAGES+=" itstool"      # necessary to build orca and some other packages
+PYTHON_PACKAGES+=" pygments"     # necessary to build mesa (dependency of mako that _must_ be kept `--upgrade`d)
+PYTHON_PACKAGES+=" mako"         # necessary to build mesa
+PYTHON_PACKAGES+=" pyyaml"       # necessary to build mesa
+PYTHON_PACKAGES+=" setuptools"   # necessary to build mesa (explicitly 'system'-wide unlike the setuptools in termux_setup_python_pip)
+# More 'system-wide' python packages should be added here if working towards the goal
+# of setup-termux.sh for on-device building having closer behavior
+# to setup-ubuntu.sh for cross-compilation. If adding packages here, please add a comment
+# for each one naming at least one of its reverse build dependencies, for which least one
+# error during on-device building is solved by installing the dependency through pip.
+#PYTHON_PACKAGES+=" "
 
 # Definition of a package manager
 export TERMUX_SCRIPTDIR=$(dirname "$(realpath "$0")")/../
@@ -62,3 +79,9 @@ else
 	echo "Error: no package manager defined"
 	exit 1
 fi
+
+# Should not be installed inside venv because on Ubuntu cross-builder image, these
+# particular python packages are installed system-wide,
+# so should be installed Termux-wide for on-device building to be reasonably accurate
+# compared with the behavior of the Ubuntu cross-builder image.
+pip install --upgrade $PYTHON_PACKAGES
