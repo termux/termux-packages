9446c71701beec79b908da11bf8d0dc1cef307cd
TomIO <tom@termux.dev>
2025-11-06 13:09:37 +0100

    bump(main/python-apsw): 3.51.0.0

--- packages/python-apsw/build.sh
+++ packages/python-apsw/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/rogerbinns/apsw/
+TERMUX_PKG_HOMEPAGE=https://rogerbinns.github.io/apsw/
 TERMUX_PKG_DESCRIPTION="Another Python SQLite Wrapper"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.50.4.0"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/rogerbinns/apsw/releases/download/${TERMUX_PKG_VERSION}/apsw-${TERMUX_PKG_VERSION}.zip
-TERMUX_PKG_SHA256=3af0263b8e1f71acd1ef4684f51baa02646c949a8e44c82e19f7c4d14a496bbe
+TERMUX_PKG_VERSION="3.51.0.0"
+TERMUX_PKG_SRCURL=https://github.com/rogerbinns/apsw/releases/download/${TERMUX_PKG_VERSION}/apsw-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f08d7f1e718ef5e3acf42505c2f37905ca47b42c570fbaa517d581038135465b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libsqlite, python"
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
