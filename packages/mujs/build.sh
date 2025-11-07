5064430868f74a3f3288b889f7e635bd502ebec2
Robert Kirkman <rkirkman@termux.dev>
2025-11-06 19:27:32 -0600

    fix(main/mujs): condense `build.sh`
    - See https://github.com/termux/termux-packages/pull/26862#issuecomment-3397630045
    
    - Prefer `$CPPFLAGS` over explicit `-I$TERMUX_PREFIX/include`
    
    - Prefer `TERMUX_PKG_EXTRA_MAKE_ARGS` over explicit `termux_step_make()`
    
    - Fix `Makefile` to respect environment `CFLAGS` and `prefix`

--- packages/mujs/build.sh
+++ packages/mujs/build.sh
@@ -3,26 +3,14 @@ TERMUX_PKG_DESCRIPTION="A lightweight Javascript interpreter designed for embedd
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.7
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://codeberg.org/ccxvii/mujs/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5701ac8314d7cb9c792d620c066d93682a74c43f2a49a8966014de05afec5d6a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="HAVE_READLINE=yes"
 
 termux_step_pre_configure() {
-	CFLAGS+=" -I$TERMUX_PREFIX/include"
-	LDFLAGS+=" -L$TERMUX_PREFIX/lib"
-}
-
-termux_step_make() {
-	make release \
-		prefix=$TERMUX_PREFIX \
-		HAVE_READLINE=yes \
-		CC="$CC" \
-		CFLAGS="$CFLAGS $CPPFLAGS" \
-		LDFLAGS="$LDFLAGS"
-}
-
-termux_step_make_install() {
-	make install prefix=$TERMUX_PREFIX
+	CFLAGS+=" $CPPFLAGS"
 }
