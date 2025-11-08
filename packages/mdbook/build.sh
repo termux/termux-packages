0eeb68673fc3f91b19e4ab4588e18b91ba299373
pranav <pranavsuresh107@gmail.com>
2025-11-08 23:46:08 +0530

    bump(main/mdbook)

--- packages/mdbook/build.sh
+++ packages/mdbook/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://rust-lang.github.io/mdBook/
 TERMUX_PKG_DESCRIPTION="Creates book from markdown files"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.4.52"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/rust-lang/mdBook/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d46f3b79e210eed383b6966847ea86ec441b6b505e9d9d868294bb9742130c9c
+TERMUX_PKG_VERSION="0.5.0~beta.2"
+TERMUX_PKG_SRCURL=https://github.com/rust-lang/mdBook/archive/refs/tags/v${TERMUX_PKG_VERSION//\~/-}.tar.gz
+TERMUX_PKG_SHA256=25c0a60f4075692b250bfde88d7990bd6b43e93145d2783c22706bcbd86c4f10
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
