f40b4ca9ab64b55e115c45e103cf19923eddbc81
Robert Kirkman <rkirkman@termux.dev>
2025-11-09 19:48:32 -0600

    revbump(main/libdvdread): 7.0.0-1
    - Test 3 related to https://github.com/termux/termux-packages/pull/27168

--- packages/libdvdread/build.sh
+++ packages/libdvdread/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library that allows easy use of sophisticated DVD navi
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="7.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://code.videolan.org/videolan/libdvdread/-/archive/${TERMUX_PKG_VERSION}/libdvdread-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=5eeda23c75405c89d04f4c601cbb99447477ea5c8c05b341c59da07b1651155b
 TERMUX_PKG_AUTO_UPDATE=true
