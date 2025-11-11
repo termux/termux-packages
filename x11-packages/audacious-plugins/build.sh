6d463666bb90b1976c29aa5a863d11153e25a280
TomIO <tom@termux.dev>
2025-11-11 15:16:27 +0100

    revbump(x11/audacious-plugins): revbump for ffmpeg-8.0

--- x11-packages/audacious-plugins/build.sh
+++ x11-packages/audacious-plugins/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.5.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://distfiles.audacious-media-player.org/audacious-plugins-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=f4feedc32776acfa9d24701d3b794fc97822f76da6991e91e627e70e561fdd3b
 TERMUX_PKG_DEPENDS="audacious, ffmpeg, fluidsynth, glib, libc++, libcue, libcurl, libflac, libmp3lame, libogg, libopenmpt, libsamplerate, libsndfile, libsoxr, libvorbis, libx11, libxml2, libmpg123, opusfile, pulseaudio, qt6-qtbase, qt6-qtmultimedia, sdl2 | sdl2-compat, zlib"
