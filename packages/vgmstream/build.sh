74c0822ff871ef340bd0c3bbaa27f4f9c3c73f23
TomIO <tom@termux.dev>
2025-11-11 15:16:27 +0100

    revbump(main/vgmstream): revbump for ffmpeg-8.0

--- packages/vgmstream/build.sh
+++ packages/vgmstream/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A library for playback of various streamed audio formats
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2055"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/vgmstream/vgmstream/archive/refs/tags/r${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=86eb37caa8d4cc882de00b42787fe557d313e58f6055c6e4d3a27403b9048f96
 TERMUX_PKG_AUTO_UPDATE=true
