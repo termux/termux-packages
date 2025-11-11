dd7780343680a575f0ddc41adad030cfafcf2d56
TomIO <tom@termux.dev>
2025-11-11 15:16:27 +0100

    revbump(x11/telegram-desktop): revbump for ffmpeg-8.0 - And mark as a big package so the CI doesn't choke on it.

--- x11-packages/telegram-desktop/build.sh
+++ x11-packages/telegram-desktop/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LEGAL"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.2.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/telegramdesktop/tdesktop/releases/download/v$TERMUX_PKG_VERSION/tdesktop-$TERMUX_PKG_VERSION-full.tar.gz
 TERMUX_PKG_SHA256=8ddde8ee7fd8bced7837a7e2a4a83a6c88fbbd1dda383a38bb064777f875f78f
 TERMUX_PKG_DEPENDS="abseil-cpp, boost, ffmpeg, glib, hicolor-icon-theme, hunspell, kf6-kcoreaddons, libandroid-shmem, libc++, libdispatch, libdrm, libjxl, liblz4, libminizip, protobuf, librnnoise, libsigc++-3.0, libx11, libxcomposite, libxdamage, libxrandr, libxtst, openal-soft, opengl, openh264, openssl, pipewire, pulseaudio, qt6-qtbase, qt6-qtimageformats, qt6-qtsvg, xxhash, zlib"
