acdc5a3b09cfbfe14cef6876aa1aac19ad059a28
TomIO <tom@termux.dev>
2025-11-13 17:30:43 +0100

    revbump(main/nodejs-lts): libicu 78.1 rebuild

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
 # Also update version in termux_setup_nodejs.sh when updating this package
 TERMUX_PKG_VERSION=22.20.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ff7a6a6e8a1312af5875e40058351c4f890d28ab64c32f12b2cc199afa22002d
 # Note that we do not use a shared libuv to avoid an issue with the Android
