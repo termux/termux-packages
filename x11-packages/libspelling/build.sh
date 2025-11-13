70741f16663fd4dc4206a86d20f615cde669ad08
TomIO <tom@termux.dev>
2025-11-13 17:22:36 +0100

    revbump(x11/libspelling): libicu 78.1 rebuild

--- x11-packages/libspelling/build.sh
+++ x11-packages/libspelling/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Spellcheck library for GTK 4"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.4.9"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libspelling/${TERMUX_PKG_VERSION%.*}/libspelling-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d093fd35ae0f1c98fb58876505287fc0a885e47da9d2411d6d7cd595f34d4ebf
 TERMUX_PKG_DEPENDS="enchant, glib, gtk4, gtksourceview5, libicu, pango"
