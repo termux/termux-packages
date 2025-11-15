ab45ea8dd17aab39c705ec54abe8f0c1d74d3969
Aditya Alok <alok@termux.dev>
2025-11-15 11:08:26 +0530

    bump(x11/kf6-kparts): 6.20.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- x11-packages/kf6-kparts/build.sh
+++ x11-packages/kf6-kparts/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Document centric plugin system'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.19.0"
+TERMUX_PKG_VERSION="6.20.0"
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kparts-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=cddc3b6a4fc0a3f41fa5791997c9ddf4ca98855839be465ebe5d0c3cefe350d8
+TERMUX_PKG_SHA256=9e71e3d20c9525f66533dd7a5846f0d982eefdd7425bfbadecd6a17908ebd709
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${_KF6_MINOR_VERSION}), kf6-kcoreaddons (>= ${_KF6_MINOR_VERSION}), kf6-ki18n (>= ${_KF6_MINOR_VERSION}), kf6-kio (>= ${_KF6_MINOR_VERSION}), kf6-kwidgetsaddons (>= ${_KF6_MINOR_VERSION}), kf6-kxmlgui (>= ${_KF6_MINOR_VERSION}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
