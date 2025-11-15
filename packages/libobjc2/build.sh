501e11f6e38669ea13e675bc778eb59217c72288
Robert Kirkman <rkirkman@termux.dev>
2025-11-15 17:09:11 -0600

    bump(main/libobjc2): 2.3
    - Use system `robin-map` instead of package-specific one

--- packages/libobjc2/build.sh
+++ packages/libobjc2/build.sh
@@ -2,14 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.gnustep.org/
 TERMUX_PKG_DESCRIPTION="The GNUstep Objective-C runtime"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.1
-TERMUX_PKG_REVISION=3
-_ROBIN_MAP_COMMIT=757de829927489bee55ab02147484850c687b620
-TERMUX_PKG_SRCURL=(https://github.com/gnustep/libobjc2/archive/v${TERMUX_PKG_VERSION}.tar.gz
-                   https://github.com/Tessil/robin-map/archive/${_ROBIN_MAP_COMMIT:0:7}.tar.gz)
-TERMUX_PKG_SHA256=(78fc3711db14bf863040ae98f7bdca08f41623ebeaf7efaea7dd49a38b5f054c
-                   0abd2a272947d1d403ce7467e75aae5bdcfe839f4fc8d513ba5bfe170d5f2057)
+TERMUX_PKG_VERSION="2.3"
+TERMUX_PKG_SRCURL="https://github.com/gnustep/libobjc2/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=5ead2276b42a534ac40437ce53b2231320b985539dc325453d93874be8d92869
 TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="robin-map"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 -DENABLE_OBJCXX=OFF
@@ -20,11 +17,11 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 # https://github.com/gnustep/libobjc2/issues/199
 TERMUX_PKG_CONFLICTS="libblocksruntime"
 
-termux_step_post_get_source() {
-	mv robin-map-${_ROBIN_MAP_COMMIT}/* third_party/robin-map/
-}
-
 termux_step_pre_configure() {
-	sed -i -e 's|@CMAKE_CXX_COMPILER@|'$CXX'|g' \
-		"$TERMUX_PKG_SRCDIR"/CMakeLists.txt
+	local target="$CCTERMUX_HOST_PLATFORM"
+	if [[ "$TERMUX_ARCH" == "arm" ]]; then
+		target="armv7a-linux-androideabi$TERMUX_PKG_API_LEVEL"
+	fi
+	export OBJCFLAGS="$CFLAGS --target=$target"
+	export OBJCXXFLAGS="$CXXFLAGS --target=$target"
 }
