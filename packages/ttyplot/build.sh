4ca4c3ed74075122dfd7adcd7b92d844f3e55ec9
Biswapriyo Nath <nathbappai@gmail.com>
2025-11-16 08:07:05 +0000

    bump(main/ttyplot): 1.7.4
    Remove `-e` option from make command because it disables adding version
    string with CPPFLAGS in makefile.

--- packages/ttyplot/build.sh
+++ packages/ttyplot/build.sh
@@ -2,14 +2,12 @@ TERMUX_PKG_HOMEPAGE="https://github.com/tenox7/ttyplot"
 TERMUX_PKG_DESCRIPTION="A realtime plotting utility for terminal with data input from stdin"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.7.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.7.4"
 TERMUX_PKG_SRCURL="https://github.com/tenox7/ttyplot/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=d1624eea52abec5538c9b19bae00f81642c2d2886cd7755988466b74424ce9ca
+TERMUX_PKG_SHA256=d4a690cb4ca6d52885ebfdc6230bfb550eecf4b8edb9b54453829a77f63ea7b9
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_MAKE_ARGS="-e"
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DNCURSES_WIDECHAR=1"
