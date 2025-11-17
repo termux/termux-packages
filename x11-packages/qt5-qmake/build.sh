c2e2773aca031172a91ae52f7f0e73573122e4f7
Jia Yuan Lo <jylo06g@gmail.com>
2025-11-17 10:27:11 +0800

    chore({disabled,x11}-packages/qt5-*): fix dead links
    %ci:no-build

--- x11-packages/qt5-qmake/build.sh
+++ x11-packages/qt5-qmake/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=5.15.5
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/5.15/${TERMUX_PKG_VERSION}/submodules/qtbase-everywhere-opensource-src-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SRCURL="https://download.qt.io/archive/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/qtbase-everywhere-opensource-src-${TERMUX_PKG_VERSION}.tar.xz"
 # TERMUX_PKG_SHA256 is not used in termux-build-qmake.sh.
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
