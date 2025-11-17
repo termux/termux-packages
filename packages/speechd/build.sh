04dcca333cbe4b65292d80ab482ba88911a2b0f2
Aditya Alok <alok@termux.dev>
2025-11-17 14:09:03 +0530

    bump(main/speechd): 0.12.1
    - Deleted upstreamed patches and added new {ldflags,prefix}.patch
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/speechd/build.sh
+++ packages/speechd/build.sh
@@ -2,11 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/brailcom/speechd
 TERMUX_PKG_DESCRIPTION="Common interface to speech synthesis"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.11.5"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SHA256=cc4b388fce40681eaff3545e9cc0642216c13c420d5676a4d28c957ddcb916de
+TERMUX_PKG_VERSION="0.12.1"
+TERMUX_PKG_SHA256=32a730f6fb5981b9bec7e04f3674fd7d29e54935f46cf6354dbb9ab1f9b23b2d
 TERMUX_PKG_SRCURL=https://github.com/brailcom/speechd/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="dotconf, espeak, glib, libiconv, libltdl, libsndfile, pulseaudio, python, speechd-data"
+TERMUX_PKG_DEPENDS="dotconf, espeak, glib, libiconv, libltdl, libsndfile, pulseaudio, python, speechd-data, libandroid-posix-semaphore"
 TERMUX_PKG_BUILD_DEPENDS="libiconv-static, libsndfile-static"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SETUP_PYTHON=true
@@ -29,6 +28,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="ac_cv_prog_HELP2MAN="
 termux_step_pre_configure() {
 	export am_cv_python_pythondir="${TERMUX_PREFIX}/lib/python${TERMUX_PYTHON_VERSION}/site-packages"
 	export am_cv_python_pyexecdir="$am_cv_python_pythondir"
+	LDFLAGS+=" -landroid-posix-semaphore"
 	./build.sh
 }
 
