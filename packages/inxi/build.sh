316e42022eccbca86539120e74f2254e0d67ae1a
Aditya Alok <alok@termux.dev>
2025-11-17 16:54:59 +0530

    fix(main/inxi): Correct auto-update version extraction
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/inxi/build.sh
+++ packages/inxi/build.sh
@@ -5,6 +5,11 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.3.39-1"
 TERMUX_PKG_SRCURL=https://codeberg.org/smxi/inxi/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c0441f21dc5ea365a6d63466070d00e6858aed3b3c42276a1bf18ab3c57c013c
+# NOTE: auto-update first checks whether the latest version matches this regex and then
+# replaces the last `.` with '-'. Thus, it shouldn't update to wrong version.
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+(-|\.)\d+"
+# Sometimes repology returns ds.ds.ds.ds instead of ds.ds.ds-ds, fix that:
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP="s/\.([0-9]+)$/-\1/"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="perl"
