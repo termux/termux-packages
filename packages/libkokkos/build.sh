61e209b63436a3bad627b99dfd7ce8e9f89585db
TomIO <tom@termux.dev>
2025-11-18 06:47:50 +0100

    bump(main/libkokkos): 5.0.0

--- packages/libkokkos/build.sh
+++ packages/libkokkos/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/kokkos
 TERMUX_PKG_DESCRIPTION="Implements a programming model in C++ for writing performance portable applications"
-TERMUX_PKG_LICENSE="BSD 3-Clause"
-TERMUX_PKG_LICENSE_FILE="Copyright.txt"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_LICENSE_FILE="COPYRIGHT.md"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.7.01"
+TERMUX_PKG_VERSION="5.0.0"
 TERMUX_PKG_SRCURL=https://github.com/kokkos/kokkos/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cebf6daeb99c95e3d4116ea0d97c94b6a521c0cff1f5e613f127f6beea960ac7
+TERMUX_PKG_SHA256=b028b797ba8ef3b439ddccf4aa3d651f2fa225cc2195bf1c1aaf096831b611c7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
