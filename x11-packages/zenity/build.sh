f07d858bb11dc1c7fefedbb581582d84733f66d2
Robert Kirkman <rkirkman@termux.dev>
2025-11-22 03:28:31 -0600

    revbump(x11/zenity): for `webkitgtk-6.0` 2.51.2

--- x11-packages/zenity/build.sh
+++ x11-packages/zenity/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="a rewrite of gdialog, the GNOME port of dialog"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.2.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/zenity/${TERMUX_PKG_VERSION%.*}/zenity-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=5a9fd8d8316f90cb2e1a5a8f0d411eb9fcaf85957a8229ea3e803e81004a1ebd
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, gtk3, libadwaita, libx11, pango, webkitgtk-6.0"
