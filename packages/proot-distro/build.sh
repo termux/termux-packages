b09eb9c931758fae2275e59d160306b2a0b1e573
Sylirre <sylirre@termux.dev>
2025-11-23 19:53:13 +0200

    bump(main/proot-distro): v4.33.0, added unzip dependency

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=4.32.0
+TERMUX_PKG_VERSION=4.33.0
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1023234f151de0aff6609de59c6654bd2961876e5bb58c3935fc4bec80868b2d
-TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, file, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, termux-tools, util-linux, xz-utils"
+TERMUX_PKG_SHA256=3865ebd25f71c25a6f1c3dca1d471ce9059d768257701989e25eb5fb3d9b5732
+TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, file, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, termux-tools, unzip, util-linux, xz-utils"
 TERMUX_PKG_SUGGESTS="bash-completion, termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
