e77ef4340071543f4de7dc2dd1e17c65e3f4e2e3
BullyMaguire-lol <tem9040@gmail.com>
2025-11-29 17:24:46 +0530

    addpkg(x11/kf6-kquickcharts): 6.21.0

---
+++ x11-packages/kf6-kquickcharts/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kquickcharts"
+TERMUX_PKG_DESCRIPTION="A QtQuick plugin providing high-performance charts"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.21.0"
+_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kquickcharts-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="d00aa71dae98b9dbdc3d2f1bb90d39647b3f2ae5e10c3aa4ec611735ee3aa548"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kirigami, libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-shadertools, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
