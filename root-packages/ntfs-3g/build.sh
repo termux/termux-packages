2555923dac3d2e32209692f5e54c5923e8627326
Robert Kirkman <rkirkman@termux.dev>
2025-11-30 03:54:48 -0600

    fix(root/ntfs-3g): fix include directory and `autoreconf` invocation
    - Remove `NOCONFIGURE=1` and `-I"$TERMUX_PREFIX/share/aclocal/"` from `termux_step_pre_configure` to stop error:
    
    ```
    libtool: Version mismatch error.  This is libtool 2.4.7 Debian-2.4.7-7build1, but the
    libtool: definition of this LT_INIT comes from libtool 2.5.4.
    libtool: You should recreate aclocal.m4 with macros from libtool 2.4.7 Debian-2.4.7-7build1
    libtool: and run autoconf again.
    ```
    
    - Add `includedir="/include"` to `termux_step_make_install` to avoid the `termux_step_massage` duplicate prefix error

--- root-packages/ntfs-3g/build.sh
+++ root-packages/ntfs-3g/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.LIB"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2022.10.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/tuxera/ntfs-3g/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8bd7749ea9d8534c9f0664d48b576e90b96d45ec8803c9427f6ffaa2f0dde299
 TERMUX_PKG_DEPENDS="libfuse2"
@@ -16,9 +16,17 @@ TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	CFLAGS+=" -I${TERMUX_PREFIX}/include/fuse"
-	NOCONFIGURE=1 autoreconf -vfi -I"$TERMUX_PREFIX/share/aclocal/"
+	autoreconf -vfi
 }
 
 termux_step_make_install() {
-	make install DESTDIR="$TERMUX_PREFIX" man8dir="/share/man/man8" rootlibdir="/lib/" libdir="/lib/" rootbindir="/bin/" bindir="/bin/" sbindir="/bin"
+	make install \
+		DESTDIR="$TERMUX_PREFIX" \
+		man8dir="/share/man/man8" \
+		rootlibdir="/lib/" \
+		libdir="/lib/" \
+		rootbindir="/bin/" \
+		bindir="/bin/" \
+		sbindir="/bin" \
+		includedir="/include"
 }
