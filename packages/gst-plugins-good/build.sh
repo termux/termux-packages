7448310f36bf0c39e82d40216124e697933679d3
Robert Kirkman <rkirkman@termux.dev>
2025-11-30 03:38:41 -0600

    fix(main/gst-plugins-good): fix cross-compilation with Qt packages in the same container

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -3,10 +3,13 @@ TERMUX_PKG_DESCRIPTION="GStreamer Good Plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.26.8"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=061e84efae31dfb4d96e4517659aca82bad9d5625b8f64d3290604385edd1d14
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gst-plugins-base, gstreamer, libandroid-shmem, libbz2, libcaca, libflac, libjpeg-turbo, libmp3lame, libnettle, libpng, libvpx, libx11, libxext, libxfixes, libxml2, libmpg123, pulseaudio, zlib"
+# -Dqt5=disabled and -Dqt6=disabled avoid
+# /bin/bash: line 1: /data/data/com.termux/files/usr/lib/qt6/moc: cannot execute binary file: Exec format error
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dcairo=disabled
 -Dexamples=disabled
@@ -16,6 +19,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dtests=disabled
 -Dv4l2=disabled
 -Daalib=disabled
+-Dqt5=disabled
+-Dqt6=disabled
 "
 
 termux_step_pre_configure() {
