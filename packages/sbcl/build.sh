5378953cb04a67764f814b660f71444d434c122f
Robert Kirkman <rkirkman@termux.dev>
2025-11-30 19:04:40 -0600

    bump(main/sbcl): 2.5.11
    - Fixes https://github.com/termux/termux-packages/issues/27485
    
    - Rebase `disable-failing-tests-that-were-only-enabled-on-x86.patch`

--- packages/sbcl/build.sh
+++ packages/sbcl/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="A high performance Common Lisp compiler"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.5.10"
+TERMUX_PKG_VERSION="2.5.11"
 # sourceforge archive is a precompiled SBCL release for GNU/Linux to use as host Lisp for bootstrapping
 TERMUX_PKG_SRCURL=(
 	https://github.com/sbcl/sbcl/archive/refs/tags/sbcl-${TERMUX_PKG_VERSION}.tar.gz
 	https://sourceforge.net/projects/sbcl/files/sbcl/${TERMUX_PKG_VERSION}/sbcl-${TERMUX_PKG_VERSION}-x86-64-linux-binary.tar.bz2
 )
 TERMUX_PKG_SHA256=(
-	5c8aefa7b745b45a2d169f76845c4c75630662494f419307c256c3fa7d0805d1
-	1f7b0b7226bfcebc4027333b0ad170f36486b589e2c492edaa78ab7f7a6cbd88
+	a06ad98fb59611e5d66539bdc6c31fae7d9cf72b5039a23d7e2adc955f0b615e
+	4f229e3e4106080820d095bb30ded0cd475a5c3202cbb8e580179d6b1cbaad2a
 )
 TERMUX_PKG_DEPENDS="zstd"
 # TERMUX_ON_DEVICE_BUILD=true  build dependencies: ecl, strace
