c4afa9a288a9875cc39f0a99f172aff1eb3ede70
TomIO <tom@termux.dev>
2025-12-02 05:36:03 +0100

    revbump(main/apt-show-versions): after `apt` update

--- packages/apt-show-versions/build.sh
+++ packages/apt-show-versions/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lists available package versions with distribution"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.22.16"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://salsa.debian.org/debian/apt-show-versions/-/archive/$TERMUX_PKG_VERSION/apt-show-versions-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=15308503b113209a551e495f9ca8cf002acf8d3e4ce31df58e1fcc4fa7713310
 TERMUX_PKG_DEPENDS="apt, libapt-pkg-perl, perl"
@@ -23,37 +24,39 @@ TERMUX_PKG_RM_AFTER_INSTALL="lib"
 # install step may pollute lib folder with weird perl files
 TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=true
 
-# the original creator of perl.makemaker decided in 2003 that formal accomodation
-# of distros like Termux that can be described as "distros with timestamp-based packaging systems"
-# is undesirable because such a distro was either unheard of to them at the time, or
-# they imagined the hypothetical possibility of the present or future existence of such a distro
-# as an unsupportable edge case,
-# so the Makefile that perl.makemaker generates will not reliably install everything
-# unless all desired files are forcibly removed before make install is invoked
-# https://www.nntp.perl.org/group/perl.makemaker/2003/04/msg1092.html
-
-# files that should be in $TERMUX_PREFIX after make install
-_MAKE_INSTALL_PROVIDED_FILES="
-bin/apt-show-versions
-share/man/man1/apt-show-versions.1p
-"
-
 termux_step_pre_configure() {
-	pushd "$TERMUX_PREFIX"
-	rm -f $_MAKE_INSTALL_PROVIDED_FILES
-	popd
+	# the original creator of perl.makemaker decided in 2003 that formal accomodation
+	# of distros like Termux that can be described as "distros with timestamp-based packaging systems"
+	# is undesirable because such a distro was either unheard of to them at the time, or
+	# they imagined the hypothetical possibility of the present or future existence of such a distro
+	# as an unsupportable edge case,
+	# so the Makefile that perl.makemaker generates will not reliably install everything
+	# unless all desired files are forcibly removed before make install is invoked
+	# https://www.nntp.perl.org/group/perl.makemaker/2003/04/msg1092.html
+
+	# files that should be in $TERMUX_PREFIX after make install
+	local -a _MAKE_INSTALL_PROVIDED_FILES=(
+		"bin/apt-show-versions"
+		"share/man/man1/apt-show-versions.1p"
+	)
+
+	local marker_file
+	for marker_file in "${_MAKE_INSTALL_PROVIDED_FILES[@]}"; do
+		rm -f "$TERMUX_PREFIX/$marker_file"
+	done
 }
 
 # termux_step_configure() for Makefile.PLs based on package perl-rename
 termux_step_configure() {
+	# shellcheck disable=SC2086
 	perl Makefile.PL $TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
 
 termux_step_post_make_install() {
-	install -D --mode=644 "$TERMUX_PKG_SRCDIR/debian/apt-show-versions.apt.conf" \
+	install -Dm644 "$TERMUX_PKG_SRCDIR/debian/apt-show-versions.apt.conf" \
 		"$TERMUX_PREFIX/etc/apt/apt.conf.d/20apt-show-versions"
 
-	install -D --mode=644 "$TERMUX_PKG_SRCDIR/debian/apt-show-versions.bash-completion" \
+	install -Dm644 "$TERMUX_PKG_SRCDIR/debian/apt-show-versions.bash-completion" \
 		"$TERMUX_PREFIX/share/bash-completion/completions/apt-show-versions"
 }
 
