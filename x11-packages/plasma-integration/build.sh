b39b075dd5d68d3125f64ed0659e8cf7fcf40b6e
BullyMaguire-lol <tem9040@gmail.com>
2025-12-02 12:48:26 +0530

    addpkg(x11/plasma-integration): 6.5.5
    Co-authored-by: Robert Kirkman <rkirkman@termux.dev>

---
+++ x11-packages/plasma-integration/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/plasma-integration"
+TERMUX_PKG_DESCRIPTION="Qt Platform Theme integration plugins for the Plasma workspaces"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.5"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/plasma-integration-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="ad5af85adc0772a1f9bcdcca51b1cbdda2ef2891b51c2b1b0a388a28521de3c5"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kcolorscheme, kf6-kcompletion, kf6-kconfig, kf6-kcoreaddons, kf6-kguiaddons, kf6-ki18n, kf6-kiconthemes, kf6-kio, kf6-kjobwidgets, kf6-kservice, kf6-kstatusnotifieritem, kf6-kwidgetsaddons, kf6-kwindowsystem, kf6-kxmlgui, qqc2-breeze-style, kf6-qqc2-desktop-style, libc++, libwayland, libxcb, libxcursor, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, plasma-wayland-protocols"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+-DBUILD_QT5=OFF
+"
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
