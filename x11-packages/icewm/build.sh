16dfc1c86996c55c797357a3f8a01a807639583e
Robert Kirkman <rkirkman@termux.dev>
2025-12-04 04:43:54 -0600

    fix(x11/icewm): switch to `xdg-utils-xdg-open`
    - Fixes https://github.com/termux/termux-packages/issues/27518

--- x11-packages/icewm/build.sh
+++ x11-packages/icewm/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="Window manager with goals of speed, simplicity, and usab
 TERMUX_PKG_LICENSE="LGPL-2.0-only"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.9.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/ice-wm/icewm/releases/download/$TERMUX_PKG_VERSION/icewm-$TERMUX_PKG_VERSION.tar.lz"
 TERMUX_PKG_SHA256=1323527a9a49db66e9ce7b08e6ec43c87700243432fc6679681df367c67b2dc0
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="alsa-lib, imlib2, libandroid-glob, libandroid-wordexp, libice, librsvg, libsm, libsndfile, libxcomposite, libxcursor, libxdamage, libxinerama, libxrandr, libxres"
+TERMUX_PKG_DEPENDS="alsa-lib, imlib2, libandroid-glob, libandroid-wordexp, libice, librsvg, libsm, libsndfile, libxcomposite, libxcursor, libxdamage, libxinerama, libxrandr, libxres, xdg-utils"
 TERMUX_PKG_BUILD_DEPENDS="aosp-libs"
 TERMUX_PKG_SUGGESTS="xdg-menu"
 
@@ -21,8 +22,10 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob -landroid-wordexp"
 
 	# Every instance of '/usr' in the code is replaceable with '$TERMUX_PREFIX'.
+	# Every instance of 'xdg-open' in the code is replaceable with 'xdg-utils-xdg-open'.
 	find "$TERMUX_PKG_SRCDIR" -type f | \
 		xargs -n 1 sed -i \
 		-e "s|/usr|$TERMUX_PREFIX|g" \
-		-e "s|/etc|$TERMUX_PREFIX/etc|g"
+		-e "s|/etc|$TERMUX_PREFIX/etc|g" \
+		-e "s|xdg-open|xdg-utils-xdg-open|g"
 }
