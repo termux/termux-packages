ad3e7875b4b5c8e286858f55173fdbdfff01581c
TomIO <tom@termux.dev>
2025-12-06 16:31:21 +0100

    chore(main/jackett): throttle auto updates to once a week

--- packages/jackett/build.sh
+++ packages/jackett/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="API Support for your favorite torrent trackers"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.24.408"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/Jackett/Jackett/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=2797251cb8da07b69183c982d9d8d064b2c60b5868cbb4186c1b8f3440ff6aaf
 TERMUX_PKG_BUILD_DEPENDS="aspnetcore-targeting-pack-9.0, dotnet-targeting-pack-9.0"
@@ -18,6 +19,54 @@ lib/jackett/jackett.pdb
 lib/jackett/Jackett.Common.pdb
 lib/jackett/DateTimeRoutines.pdb
 "
+
+# This auto update function throttles the update frequency
+# of the package to set `$update_interval`, this is useful
+# for packages that make very frequent tags like `jackett`
+# or `llama-cpp` to not spam the commit history, CI and repos.
+termux_pkg_auto_update() {
+	local origin_url last_autoupdate
+	# Throttle auto updates to once a week.
+	local update_interval="$((7 * 86400))"
+
+	# Get the git history
+	if origin_url="$(git config --get remote.origin.url)"; then
+		git fetch --quiet "${origin_url}" || {
+			echo "WARN: Unable to fetch '${origin_url}'"
+			echo "WARN: Skipping auto update for '$TERMUX_PKG_NAME'"
+			return
+		}
+	fi
+
+	# When was `jackett` last autoupdated? (Unix epoch timestamp)
+	last_autoupdate="$(
+		git log \
+		--author="Termux Github Actions <contact@termux.dev>" \
+		-n1 \
+		--pretty=format:%at \
+		-- "$TERMUX_PKG_BUILDER_DIR/build.sh"
+	)"
+
+
+	if (( last_autoupdate > EPOCHSECONDS - update_interval )); then
+		local t days hrs mins secs
+		(( t = EPOCHSECONDS - last_autoupdate, days = t/86400, t %= 86400, secs= t%60, t /= 60, mins = t%60, hrs = t/60 ))
+
+		printf 'INFO: Last updated %dd%dh%02dm%02ds ago.\n' "$days" "$hrs" "$mins" "$secs"
+		printf 'INFO: Which is less than the desired %sd minimum update interval.\n' "$(( update_interval / 86400 ))"
+		return
+	fi
+
+	local latest_tag
+	latest_tag="$(
+		termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}"
+	)"
+
+	if [[ -z "${latest_tag}" ]]; then
+		termux_error_exit "Unable to get tag from ${TERMUX_PKG_SRCURL}"
+	fi
+	termux_pkg_upgrade_version "${latest_tag}"
+}
 
 termux_step_pre_configure() {
 	TERMUX_DOTNET_VERSION=9.0
