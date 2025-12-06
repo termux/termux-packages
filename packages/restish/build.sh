b37b66825b6d67e160a3853bb8412b181c55ef5b
Biswapriyo Nath <nathbappai@gmail.com>
2025-12-06 15:58:21 +0000

    bump(main/restish): 0.21.1

--- packages/restish/build.sh
+++ packages/restish/build.sh
@@ -1,12 +1,13 @@
-TERMUX_PKG_HOMEPAGE="https://github.com/danielgtaylor/restish"
+TERMUX_PKG_HOMEPAGE="https://github.com/rest-sh/restish"
 TERMUX_PKG_DESCRIPTION="A CLI for interacting with REST-ish HTTP APIs"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.md"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.21.0-cifix"
-TERMUX_PKG_SRCURL="https://github.com/danielgtaylor/restish/archive/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=76796bc3403fbebf7f8ee7ea35c8d04f9a54015ae8af0d2830e374c2aede586f
+TERMUX_PKG_VERSION="0.21.1"
+TERMUX_PKG_SRCURL="https://github.com/rest-sh/restish/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=ca8033595641c96d94c1c787550181ddb6f1a8333d7af5c24123a93ff75d0ff7
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
