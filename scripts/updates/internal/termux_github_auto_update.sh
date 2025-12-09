56008d8fa1e3222a9ccea816c48a201e2b68309b
TomIO <tom@termux.dev>
2025-12-09 21:20:38 +0100

    enhance(scripts/termux_github_api_get_tag): use inherited variables - Refactor to match the implementation of `termux_gitlab_api_get_tag`   as closely as possible to reduce auto-update code differences between   between different project hosts - Add our User Agent to API requests sent via `termux_github_api_get_tag` - Make tag validation the responsibility of `termux_pkg_upgrade_version`   we only report HTTP errors from the GitHub API in this function
    Co-authored-by: Aditya Alok <alok@termux.dev>

--- scripts/updates/internal/termux_github_auto_update.sh
+++ scripts/updates/internal/termux_github_auto_update.sh
@@ -2,9 +2,7 @@
 # Default algorithm to use for packages hosted on github.com
 termux_github_auto_update() {
 	local latest_tag
-	latest_tag="$(
-		termux_github_api_get_tag "${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}"
-	)"
+	latest_tag="$(termux_github_api_get_tag)"
 
 	if [[ -z "${latest_tag}" ]]; then
 		termux_error_exit "Unable to get tag from ${TERMUX_PKG_SRCURL}"
