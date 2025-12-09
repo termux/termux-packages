e2cff4a23bf998aa530642ebcc647bddd4fc8901
TomIO <tom@termux.dev>
2025-12-09 21:01:22 +0100

    enhance(scripts/termux_gitlab_api_get_tag): use inherited variables - Deprecates `$TERMUX_GITLAB_API_HOST` - `api_host` can be parsed from the `$TERMUX_PKG_SRCURL` - Makes `termux_gitlab_api_get_tag` agnostic to the API host   '/-/archive/' in the $TERMUX_PKG_SRCURL is a reliable indicator   for a package being hosted on a Gitlab instance - Add our User Agent to API requests sent via `termux_gitlab_api_get_tag` - Implemented 'latest-regex' update type for `termux_gitlab_api_get_tag` - Make tag validation the responsibility of `termux_pkg_upgrade_version`   we only report HTTP errors from the Gitlab API host in this function
    Co-authored-by: Aditya Alok <alok@termux.dev>

--- scripts/updates/internal/termux_gitlab_auto_update.sh
+++ scripts/updates/internal/termux_gitlab_auto_update.sh
@@ -1,11 +1,9 @@
 # shellcheck shell=bash
-# Default algorithm to use for packages hosted on hosts using gitlab-ci.
+# Default algorithm to use for packages hosted on gitlab instances.
 termux_gitlab_auto_update() {
 	local latest_tag
-	latest_tag="$(
-		termux_gitlab_api_get_tag \
-			"${TERMUX_PKG_SRCURL}" "${TERMUX_PKG_UPDATE_TAG_TYPE}" "${TERMUX_GITLAB_API_HOST}"
-	)"
+	latest_tag="$(termux_gitlab_api_get_tag)"
+
 	if [[ -z "${latest_tag}" ]]; then
 		termux_error_exit "Unable to get tag from ${TERMUX_PKG_SRCURL}"
 	fi
