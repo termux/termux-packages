cac0a1d4e584df5e42f17cb73f33028f1aa16401
Yaksh Bariya <yakshbari4@gmail.com>
2025-12-10 15:41:37 +0530

    bump(main/nodejs-lts): 24.11.1

--- packages/nodejs-lts/build.sh
+++ packages/nodejs-lts/build.sh
@@ -3,14 +3,16 @@ TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environme
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
 # Also update version in termux_setup_nodejs.sh when updating this package
-TERMUX_PKG_VERSION=22.21.1
+TERMUX_PKG_VERSION=24.11.1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=487d73fd4db00dc2420d659a8221b181a7937fbc5bc73f31c30b1680ad6ded6a
+TERMUX_PKG_SHA256=ea4da35f1c9ca376ec6837e1e30cee30d491847fe152a3f0378dc1156d954bbd
+# thunder-coding: don't try to autoupdate nodejs, that thing takes 2 whole hours to build for a single arch, and requires a lot of patch updates everytime. Also I run tests everytime I update it to ensure least bugs
+TERMUX_PKG_AUTO_UPDATE=false
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
-TERMUX_PKG_DEPENDS="libc++, openssl, c-ares, libicu, zlib"
-TERMUX_PKG_CONFLICTS="nodejs, nodejs-current"
+TERMUX_PKG_DEPENDS="libc++, openssl, c-ares, libicu, libsqlite, zlib"
+TERMUX_PKG_CONFLICTS="nodejs-lts, nodejs-current"
 TERMUX_PKG_BREAKS="nodejs-dev"
 TERMUX_PKG_REPLACES="nodejs-current, nodejs-dev"
 TERMUX_PKG_SUGGESTS="clang, make, pkg-config, python"
@@ -23,10 +25,6 @@ termux_step_post_get_source() {
 	rm -Rf $TERMUX_PKG_HOSTBUILD_DIR
 }
 
-termux_step_pre_configure() {
-	termux_setup_ninja
-}
-
 termux_step_host_build() {
 	######
 	# Do host-build of ICU, which is required for nodejs
@@ -34,15 +32,15 @@ termux_step_host_build() {
 	local ICU_VERSION=78.1
 	local ICU_TAR=icu4c-${ICU_VERSION}-sources.tgz
 	local ICU_DOWNLOAD=https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION}/$ICU_TAR
-	export CC="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang"
-	export CXX="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang++"
-	export LD="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang++"
 	termux_download \
 		$ICU_DOWNLOAD\
 		$TERMUX_PKG_CACHEDIR/$ICU_TAR \
 		6217f58ca39b23127605cfc6c7e0d3475fe4b0d63157011383d716cb41617886
 	tar xf $TERMUX_PKG_CACHEDIR/$ICU_TAR
 	cd icu/source
+	export CC="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang"
+	export CXX="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang++"
+	export LD="$TERMUX_HOST_LLVM_BASE_DIR/bin/clang++"
 	if [ "$TERMUX_ARCH_BITS" = 32 ]; then
 		./configure --prefix $TERMUX_PKG_HOSTBUILD_DIR/icu-installed \
 			--disable-samples \
@@ -55,6 +53,7 @@ termux_step_host_build() {
 	fi
 	make -j $TERMUX_PKG_MAKE_PROCESSES install
 
+
 	######
 	# Download LLVM toolchain used by the upstream v8 project.
 	# Upstream v8 uses LLVM tooling from the main branch of the LLVM project as
@@ -63,33 +62,34 @@ termux_step_host_build() {
 	# what Google uses.
 	######
 
-	# Instructions to find the LLVM_COMMIT and LLVM_TAR_HASH used by the v8
-	# version in nodejs:
+	# The LLVM_COMMIT, as well as the tarball of the LLVM build by Google in use
+	# can be found in deps/v8/DEPS file,
 	#
-	# Look into the deps/v8/DEPS file, and look for the 'tools/clang' entry.
-	#  'tools/clang':
-	#    Var('chromium_url') + '/chromium/src/tools/clang.git' + '@' + '6c4f037a983abf14a4c8bf00e44db73cdf330a97',
+	# For instance, if the deps/v8/DEPS file contains:
 	#
-	# You can now choose to either choose to do a full checkout of the v8 commit
-	# and do `gclient sync` to get the full tree, or just peek at
-	# https://chromium.googlesource.com/chromium/src/tools/clang.git/+/6c4f037a983abf14a4c8bf00e44db73cdf330a97/scripts/update.py
-	# Look at the CLANG_REVISION and CLANG_SUB_REVISION variable,
-	# LLVM_TAR="${CLANG_REVISION}-${CLANG_SUB_REVISION}.tar.xz"
+	#   'third_party/llvm-build/Release+Asserts': {
+	#  'dep_type': 'gcs',
+	#  'bucket': 'chromium-browser-clang',
+	#  'objects': [
+	#    {
+	#      'object_name': 'Linux_x64/clang-llvmorg-21-init-5118-g52cd27e6-5.tar.xz',
+	#      'sha256sum': '790fcc5b04e96882e8227ba7994161ab945c0e096057fc165a0f71e32a7cb061',
+	#      'size_bytes': 54517328,
+	#      'generation': 1742541959624765,
+	#      'condition': 'host_os == "linux"',
+	#    },
 	#
-	# From scripts/update.py
-	# 39 | CLANG_REVISION = 'llvmorg-21-init-9266-g09006611'
-	# 40 | CLANG_SUB_REVISION = 1
+	# then the LLVM_COMMIT is 52cd27e6. The g before the hash is not part of the
+	# hash, weird that they decided to include a 'g' for no reason, but 'g' isn't
+	# a part of the hexadecimal characters so anyways.. Also v8 project only
+	# stores the short-hash in the DEPS file, but we are using full hash here for
+	# clarity. The full hash can be obtained by having a full checkout of the
+	# llvm-project locally and then running `git log --format=%H -n 1` in the
+	# llvm-project directory.
 	#
-	# then the LLVM_COMMIT is 09006611.
-	# LLVM_TAR_HASH is not available in the DEPS file, so you need to do a
-	# download yourself to find it
-	#
-	# NOTE: If you are not able to find the LLVM_COMMIT according to the above instructions,
-	#       this is because of https://chromium.googlesource.com/v8/v8.git/+/e5ffb0f66d122129a04cf1f4ffcf6a6e3b956ee0
-	#       nodejs-lts comes with an older version of v8 which does not have this patch.
-	#       Updated instructions can be found in the build.sh file for nodejs package
-	local LLVM_TAR="clang-llvmorg-21-init-9266-g09006611-1.tar.xz"
-	local LLVM_TAR_HASH=2cccd3a5b04461f17a2e78d2f8bd18b448443a9dd4d6dfac50e8e84b4d5176f1
+	# Also the sha256sum is the hash of the tarball, which we can directly use
+	local LLVM_TAR="clang-llvmorg-21-init-5118-g52cd27e6-5.tar.xz"
+	local LLVM_TAR_HASH=790fcc5b04e96882e8227ba7994161ab945c0e096057fc165a0f71e32a7cb061
 	cd $TERMUX_PKG_HOSTBUILD_DIR
 	mkdir llvm-project-build
 	termux_download \
@@ -97,6 +97,10 @@ termux_step_host_build() {
 			"${TERMUX_PKG_CACHEDIR}/${LLVM_TAR}" \
 			"${LLVM_TAR_HASH}"
 	tar --extract -f "${TERMUX_PKG_CACHEDIR}/${LLVM_TAR}" --directory=llvm-project-build
+}
+
+termux_step_pre_configure() {
+	termux_setup_ninja
 }
 
 termux_step_configure() {
@@ -136,15 +140,16 @@ termux_step_configure() {
 		export CXX_host="$TERMUX_PKG_HOSTBUILD_DIR/llvm-project-build/bin/clang++ -m32"
 		export LINK_host="$TERMUX_PKG_HOSTBUILD_DIR/llvm-project-build/bin/clang++ -m32"
 	fi
-
-	# See note above TERMUX_PKG_DEPENDS why we do not use a shared libuv
-	# When building with ninja, build.ninja is geenrated for both Debug and Release builds.
+	LDFLAGS+=" -ldl"
+	# See note above TERMUX_PKG_DEPENDS why we do not use a shared libuv.
+	# When building with ninja, build.ninja is generated for both Debug and Release builds.
 	./configure \
 		--prefix=$TERMUX_PREFIX \
 		--dest-cpu=$DEST_CPU \
 		--dest-os=android \
 		--shared-cares \
 		--shared-openssl \
+		--shared-sqlite \
 		--shared-zlib \
 		--with-intl=system-icu \
 		--cross-compiling \
@@ -161,9 +166,8 @@ termux_step_configure() {
 	sed -i \
 		-e "s|\-I$TERMUX_PREFIX/include|-I$TERMUX_PKG_HOSTBUILD_DIR/icu-installed/include|g" \
 		-e "s|\-L$TERMUX_PREFIX/lib|-L$TERMUX_PKG_HOSTBUILD_DIR/icu-installed/lib|g" \
-		-e "s|\-L$TERMUX_PREFIX/$TERMUX_PREFIX/lib||g" \
+		-e "s|\-L$TERMUX_PREFIX$TERMUX_PREFIX/lib||g" \
 		$(find $TERMUX_PKG_SRCDIR/out/{Release,Debug}/obj.host -name '*.ninja')
-
 }
 
 termux_step_make() {
