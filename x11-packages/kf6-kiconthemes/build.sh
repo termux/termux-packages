92228e54cf4f7317b20199276c2a645acf30924c
Biswapriyo Nath <nathbappai@gmail.com>
2025-12-13 04:20:13 +0000

    bump(x11/kf6-kiconthemes): 6.21.0

--- x11-packages/kf6-kiconthemes/build.sh
+++ x11-packages/kf6-kiconthemes/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
 TERMUX_PKG_DESCRIPTION='Support for icon themes'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.20.0"
+TERMUX_PKG_VERSION="6.21.0"
 _KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kiconthemes-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=61b906051cd66db1dd259ec76d08885d3fe749e0d3947aea749021d2c96537a5
+TERMUX_PKG_SHA256=873477e37e6fd03ba56430a84f80e2e043ccec61d13de876263b8f220131dd62
 TERMUX_PKG_DEPENDS="kf6-breeze-icons (>= ${_KF6_MINOR_VERSION}), kf6-karchive (>= ${_KF6_MINOR_VERSION}), kf6-kcolorscheme (>= ${_KF6_MINOR_VERSION}), kf6-kconfig (>= ${_KF6_MINOR_VERSION}), kf6-kconfigwidgets (>= ${_KF6_MINOR_VERSION}), kf6-ki18n (>= ${_KF6_MINOR_VERSION}), kf6-kwidgetsaddons (>= ${_KF6_MINOR_VERSION}), libc++, qt6-qtbase, qt6-qtdeclarative"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qtsvg, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
