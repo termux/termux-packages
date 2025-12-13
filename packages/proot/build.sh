cc864299f3e2ebc010da6afdc6c83fa5f23c8574
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-12-13 20:28:06 +0800

    proot: fix build with ndk r29

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
 # Just bump commit and version when needed:
 _COMMIT=228a5f28b078f4e2504de46758ce17948f73f507
 TERMUX_PKG_VERSION=5.1.107
-TERMUX_PKG_REVISION=67
+TERMUX_PKG_REVISION=68
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=39ed5eeb6ebde37be8e53591c670bf35d05869f196eff697c8dd1c20af3c0034
 TERMUX_PKG_AUTO_UPDATE=false
