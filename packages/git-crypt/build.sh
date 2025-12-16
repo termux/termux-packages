ebe9593997e05215f6cf2b4f83dc105d91baad78
okhex <Gabrielluc983@proton.me>
2025-12-16 12:51:14 +0530

    bump(main/git-crypt): 0.8.0

--- packages/git-crypt/build.sh
+++ packages/git-crypt/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.agwa.name/projects/git-crypt/
 TERMUX_PKG_DESCRIPTION="Enables transparent encryption and decryption of files for a git repository"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.8.0
 TERMUX_PKG_SRCURL=https://www.agwa.name/projects/git-crypt/downloads/git-crypt-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=50f100816a636a682404703b6c23a459e4d30248b2886a5cf571b0d52527c7d8
+TERMUX_PKG_SHA256=540d424f87bed7994a4551a8c24b16e50d3248a5b7c3fd8ceffe94bfd4af0ad9
 TERMUX_PKG_DEPENDS="git, libc++, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" -DCMAKE_BUILD_TYPE=Release -Dbuild_parse=yes -Dbuild_xmlparser=yes"
 TERMUX_PKG_EXTRA_MAKE_ARGS="ENABLE_MAN=yes"
