384932cb69b52650de50df09d818c72983024161
Biswapriyo Nath <nathbappai@gmail.com>
2025-12-16 13:45:33 +0000

    bump(x11/falkon): 25.12.0

--- x11-packages/falkon/build.sh
+++ x11-packages/falkon/build.sh
@@ -2,14 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.falkon.org/
 TERMUX_PKG_DESCRIPTION="Cross-platform QtWebEngine Browser"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="25.08.3"
+TERMUX_PKG_VERSION="25.12.0"
 TERMUX_PKG_SRCURL=https://download.kde.org/stable/release-service/$TERMUX_PKG_VERSION/src/falkon-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=16252d8a42028bf54b88e1e4a5afb95875309de44f203156c99c4a643720e129
+TERMUX_PKG_SHA256=6d9b4dba1bde0d04570736ed7bb9bc1288cd7fb94b4ffe934a990912b9b7a959
 TERMUX_PKG_DEPENDS="kf6-karchive, kf6-ki18n, qt6-qtbase, qt6-qtdeclarative, qt6-qtwebsockets, qt6-qtwebengine"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
 # Qt6-Webengine doesn't support i686 on Termux.
 TERMUX_PKG_EXCLUDED_ARCHES="i686"
-TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_BUILD_TYPE=Release
 -DCMAKE_SYSTEM_NAME=Linux
