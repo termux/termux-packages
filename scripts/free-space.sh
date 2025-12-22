22094fb7711d95aad8cbb47e3f534e3ba10c8089
Yaksh Bariya <yakshbari4@gmail.com>
2025-12-22 16:18:22 +0530

    ci(packages): use zram for large packages
    Should improve performance when compiling large packages. And zram
    should be unused unless there is requirement for it's usage. Not sure
    how much will it affect packages that do not need additional memory
    (needs to be monitored for CI usage after getting merged)

--- scripts/free-space.sh
+++ scripts/free-space.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-# This script clears about ~22G of space. It also sets up a swapfile to account
-# for packages requiring large memory.
+# This script clears about ~22G of space.
 
 # Test:
 # echo "Listing 100 largest packages after"
 # dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 # exit 0
-
-create_swapfile() {
-	sudo fallocate -l 8G /swapfile
-	sudo chmod 600 /swapfile
-	sudo mkswap /swapfile
-	sudo swapon /swapfile
-}
 
 if [ "${CI-false}" != "true" ]; then
 	echo "ERROR: not running on CI, not deleting system files to free space!"
@@ -61,6 +53,4 @@ else
 
 	sudo apt autoremove -yq
 	sudo apt clean
-
-	create_swapfile
 fi
