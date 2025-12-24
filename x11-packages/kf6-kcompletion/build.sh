2e79542c9c7109cbeadcf97d1f18a610232163a5
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 21:30:17 +0530

    chore(x11/kf6-kcompletion): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-kcompletion/build.sh
+++ x11-packages/kf6-kcompletion/build.sh
@@ -1,13 +1,12 @@
-TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kcompletion"
 TERMUX_PKG_DESCRIPTION='Text completion helpers and widgets'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kcompletion-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=90d51f35d0997623448fe4773372bfdd96e869afd46fda809d7ed8d3e88a35ba
-TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${_KF6_MINOR_VERSION}), kf6-kconfig (>= ${_KF6_MINOR_VERSION}), kf6-kwidgetsaddons (>= ${_KF6_MINOR_VERSION}), libc++, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcompletion-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="90d51f35d0997623448fe4773372bfdd96e869afd46fda809d7ed8d3e88a35ba"
+TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${TERMUX_PKG_VERSION%.*}), kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
