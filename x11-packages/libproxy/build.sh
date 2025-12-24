3c56a9c96170a22f2a33852c3413ff645350c578
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 09:47:02 +0530

    addpkg(x11/libproxy): 0.5.12

---
+++ x11-packages/libproxy/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://libproxy.github.io/"
+TERMUX_PKG_DESCRIPTION="Automatic proxy configuration management library"
+TERMUX_PKG_LICENSE="LGPL-2.1-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.5.12"
+TERMUX_PKG_SRCURL="https://github.com/libproxy/libproxy/archive/${TERMUX_PKG_VERSION}/libproxy-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="a1fa55991998b80a567450a9e84382421a7176a84446c95caaa8b72cf09fa86f"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="curl, duktape, glib"
+TERMUX_PKG_BUILD_DEPENDS="gsettings-desktop-schemas, g-ir-scanner"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dvapi=false
+-Ddocs=false
+-Dintrospection=false
+"
