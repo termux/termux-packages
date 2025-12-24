8d01c938d021676c85630c8d83f65b9d8e59a1ac
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 23:20:14 +0530

    chore(x11/kf6-kitemviews): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-kitemviews/build.sh
+++ x11-packages/kf6-kitemviews/build.sh
@@ -1,14 +1,13 @@
-TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kitemviews"
 TERMUX_PKG_DESCRIPTION="Set of item views extending the Qt model-view framework (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kitemviews-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=3baa836eff923e1dcb3d989dd464fb3c3cb9ce9cac6a6723e7e7c1008f156a46
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kitemviews-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="3baa836eff923e1dcb3d989dd464fb3c3cb9ce9cac6a6723e7e7c1008f156a46"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
