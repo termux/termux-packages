30d9f5b5564d09d60ce282907120efeed1610ba0
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 09:15:39 +0530

    addpkg(x11/baloo-widgets): 25.12.0

---
+++ x11-packages/baloo-widgets/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/libraries/baloo-widgets"
+TERMUX_PKG_DESCRIPTION="Widgets for Baloo"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="25.12.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/baloo-widgets-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="8edf855fb9347bed348c8be2bafc01d18380a7887a25abe2e716c0a11d9a6f83"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-baloo, kf6-kconfig, kf6-kcoreaddons, kf6-kfilemetadata, kf6-ki18n, kf6-kio, kf6-kservice, kf6-kwidgetsaddons, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
