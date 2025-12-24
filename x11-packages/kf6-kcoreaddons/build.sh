c605db537c9b8706a664e4be4ab2428f2f4b5d83
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 21:41:47 +0530

    chore(x11/kf6-kcoreaddons): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-kcoreaddons/build.sh
+++ x11-packages/kf6-kcoreaddons/build.sh
@@ -1,14 +1,13 @@
-TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kcoreaddons"
 TERMUX_PKG_DESCRIPTION="Utilities for core application functionality and accessing the OS (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kcoreaddons-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=4461dcb7be66cc55101c2a957c08c7871f2b3ea7dd6f017183866c4534163e49
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcoreaddons-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="4461dcb7be66cc55101c2a957c08c7871f2b3ea7dd6f017183866c4534163e49"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtdeclarative, shared-mime-info"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_PYTHON_BINDINGS=OFF
 -DCMAKE_SYSTEM_NAME=Linux
