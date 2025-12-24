d42904c4d065a4bb8efbffd496c4f60dd703629f
BullyMaguire-lol <temp9040@gmail.com>
2025-12-25 01:07:36 +0530

    chore(x11/kf6-solid): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-solid/build.sh
+++ x11-packages/kf6-solid/build.sh
@@ -1,14 +1,13 @@
-TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
+TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/solid'
 TERMUX_PKG_DESCRIPTION='Hardware integration and detection'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/solid-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3ffd07e8b623364c83c36b237b28383c916512725129fcf0fa3ff46b72890628
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/solid-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="3ffd07e8b623364c83c36b237b28383c916512725129fcf0fa3ff46b72890628"
 TERMUX_PKG_DEPENDS="libimobiledevice, libplist, qt6-qtbase, libc++, util-linux"
 # media-player-info, systemd-libs, udisks2, upower can be added to TERMUX_PKG_DEPENDS when available
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qtdeclarative, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qtdeclarative, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
