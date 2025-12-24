eea19dcbdb59f50bc50901abb2e4b3f42375d5b1
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 20:55:57 +0530

    chore(x11/kf6-karchive): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-karchive/build.sh
+++ x11-packages/kf6-karchive/build.sh
@@ -1,14 +1,13 @@
-TERMUX_PKG_HOMEPAGE=https://www.kde.org/
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/karchive"
 TERMUX_PKG_DESCRIPTION="Qt addon providing access to numerous types of archives (KDE)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/karchive-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=a5f7ccd904105083c442bc825c198872bdb7a009b2b2bb30b038dabd6bb1c6c4
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/karchive-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="a5f7ccd904105083c442bc825c198872bdb7a009b2b2bb30b038dabd6bb1c6c4"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libbz2, libc++, liblzma, qt6-qtbase, zlib, zstd"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
