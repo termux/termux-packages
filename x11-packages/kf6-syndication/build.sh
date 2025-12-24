cf66d46c7f629864cbf2c850dcf71df68e3676d5
BullyMaguire-lol <temp9040@gmail.com>
2025-12-25 01:13:24 +0530

    chore(x11/kf6-syndication): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE
    %ci:no-build

--- x11-packages/kf6-syndication/build.sh
+++ x11-packages/kf6-syndication/build.sh
@@ -1,13 +1,12 @@
-TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
+TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/syndication'
 TERMUX_PKG_DESCRIPTION='RSS/Atom parser library'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/syndication-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=7ea3f6d46710a6ee12c3764b3f5b3a2240b22ca01f106700262b46b25b0e0abb
-TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${_KF6_MINOR_VERSION}), qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), libc++, qt6-qttools"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/syndication-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="7ea3f6d46710a6ee12c3764b3f5b3a2240b22ca01f106700262b46b25b0e0abb"
+TERMUX_PKG_DEPENDS="kf6-kcodecs (>= ${TERMUX_PKG_VERSION%.*}), qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
