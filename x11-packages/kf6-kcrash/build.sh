9744ff6ed51d9f37d9e0efd49a886ba8f01b33dd
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 21:53:51 +0530

    chore(x11/kf6-kcrash): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-kcrash/build.sh
+++ x11-packages/kf6-kcrash/build.sh
@@ -1,13 +1,12 @@
-TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
+TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/kcrash'
 TERMUX_PKG_DESCRIPTION='Support for application crash analysis and bug report from apps'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kcrash-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=08455c27b668452dca2bb37956ac82e4c1e7052df6949b8d622ce11e5c9f4c94
-TERMUX_PKG_DEPENDS="kf6-kcoreaddons (>= ${_KF6_MINOR_VERSION}), libc++, libx11, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), qt6-qttools"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcrash-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="08455c27b668452dca2bb37956ac82e4c1e7052df6949b8d622ce11e5c9f4c94"
+TERMUX_PKG_DEPENDS="kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), libc++, libx11, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 # TERMUX_PKG_RECOMMENDS="drkonqi"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
