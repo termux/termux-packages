149bd2ae62a7a198d8feeee0b25dac04e2e87c2d
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 22:12:55 +0530

    chore(x11/kf6-kglobalaccel): remove _KF6_MINOR_VERSION and normalize TERMUX_PKG_HOMEPAGE

--- x11-packages/kf6-kglobalaccel/build.sh
+++ x11-packages/kf6-kglobalaccel/build.sh
@@ -1,13 +1,12 @@
-TERMUX_PKG_HOMEPAGE='https://community.kde.org/Frameworks'
+TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/kglobalaccel'
 TERMUX_PKG_DESCRIPTION='Add support for global workspace shortcuts'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
-_KF6_MINOR_VERSION="${TERMUX_PKG_VERSION%.*}"
-TERMUX_PKG_SRCURL=https://download.kde.org/stable/frameworks/${_KF6_MINOR_VERSION}/kglobalaccel-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3e68b8fb5722d75982c99534478d286dfe359560ca5d8a10293ca90f301aefe3
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kglobalaccel-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="3e68b8fb5722d75982c99534478d286dfe359560ca5d8a10293ca90f301aefe3"
 TERMUX_PKG_DEPENDS="qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${_KF6_MINOR_VERSION}), libc++, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
