37c3efb775ed481c299bfdd99a3fafc3f1168fc0
BullyMaguire-lol <temp9040@gmail.com>
2025-12-24 09:24:31 +0530

    addpkg(x11/plasma-activities-stats): 6.5.4

---
+++ x11-packages/plasma-activities-stats/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/plasma-activities-stats"
+TERMUX_PKG_DESCRIPTION="Provides usage data for KDE activities"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.4"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/plasma-activities-stats-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="df12623a30f4bb0eb41fceccb4bea538172ef09b71d6ab40a1b443afa8dfbcca"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kconfig, libc++, plasma-activities, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
