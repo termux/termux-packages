f601c80d84149656416b7e9762350b8cd92728df
TomIO <tom@termux.dev>
2025-12-25 09:56:51 +0100

    bump(main/mpv): 0.41.0 and build against LuaJIT

--- packages/mpv/build.sh
+++ packages/mpv/build.sh
@@ -3,16 +3,15 @@ TERMUX_PKG_DESCRIPTION="Command-line media player"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 # Update both mpv and mpv-x to the same version in one PR.
-TERMUX_PKG_VERSION="0.40.0"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.41.0"
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=10a0f4654f62140a6dd4d380dcf0bbdbdcf6e697556863dc499c296182f081a3
+TERMUX_PKG_SHA256=ee21092a5ee427353392360929dc64645c54479aefdb5babc5cfbb5fad626209
 TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="alsa-lib, ffmpeg, jack, libandroid-glob, libandroid-support, libarchive, libass, libcaca, libiconv, liblua52, libsixel, libuchardet, openal-soft, pulseaudio, rubberband, zlib, libplacebo"
+TERMUX_PKG_DEPENDS="alsa-lib, ffmpeg, jack, libandroid-glob, libandroid-support, libarchive, libass, libcaca, libiconv, libluajit, libplacebo, libsixel, libuchardet, openal-soft, pulseaudio, rubberband, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="share/icons share/applications"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibmpv=true
--Dlua=lua52
+-Dlua=luajit
 -Ddvdnav=disabled
 -Dlcms2=disabled
 -Dlibbluray=disabled
@@ -32,22 +31,25 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dandroid-media-ndk=disabled
 "
 
+# shellcheck disable=SC2031
 termux_step_post_get_source() {
 	# Version guard
-	local ver_m=${TERMUX_PKG_VERSION#*:}
-	local ver_x=$(. $TERMUX_SCRIPTDIR/x11-packages/mpv-x/build.sh; echo ${TERMUX_PKG_VERSION#*:})
-	if [ "${ver_m}" != "${ver_x}" ]; then
+	local ver_m ver_x
+	ver_m="${TERMUX_PKG_VERSION#*:}"
+	ver_x="$(. "$TERMUX_SCRIPTDIR/x11-packages/mpv-x/build.sh"; echo "${TERMUX_PKG_VERSION#*:}")"
+	if [[ "${ver_m}" != "${ver_x}" ]]; then
 		termux_error_exit "Version mismatch between mpv and mpv-x."
 	fi
 }
 
+# shellcheck disable=SC2031
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
-	sed -i "s/host_machine.system() == 'android'/false/" ${TERMUX_PKG_SRCDIR}/meson.build
+	sed -i "s/host_machine.system() == 'android'/false/" "${TERMUX_PKG_SRCDIR}/meson.build"
 }
 
 termux_step_post_make_install() {
 	# Use opensles audio out by default:
-	install -Dm600 -t $TERMUX_PREFIX/etc/mpv/ $TERMUX_PKG_BUILDER_DIR/mpv.conf
-	install -Dm600 -t $TERMUX_PREFIX/share/mpv/scripts/ $TERMUX_PKG_SRCDIR/TOOLS/lua/*
+	install -Dm600 -t "$TERMUX_PREFIX/etc/mpv/" "$TERMUX_PKG_BUILDER_DIR/mpv.conf"
+	install -Dm600 -t "$TERMUX_PREFIX/share/mpv/scripts/" "$TERMUX_PKG_SRCDIR/TOOLS/lua"/*
 }
