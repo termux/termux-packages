9000b145580a2cff88d80c8c93a87abbb0eb951a
Biswapriyo Nath <nathbappai@gmail.com>
2025-12-28 05:59:24 +0000

    bump(main/cppcheck): 2.19.0

--- packages/cppcheck/build.sh
+++ packages/cppcheck/build.sh
@@ -2,15 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://github.com/danmar/cppcheck
 TERMUX_PKG_DESCRIPTION="tool for static C/C++ code analysis"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.18.3"
+TERMUX_PKG_VERSION="2.19.0"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology # Upstream only releases major versions theough GitHub. Other minor updates are released using git tags, better rely on repology for updated versiom
 TERMUX_PKG_SRCURL=https://github.com/danmar/cppcheck/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=e37c94e190cdddc65682649b02b72939761585bddd8ada595f922e190a26a2be
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_SHA256=c6cff9d3bbcb3da941bf7f525ae974b6c7af3d610c4c5519fcd1be3f21f5ae09
+TERMUX_PKG_DEPENDS="libandroid-execinfo, libc++"
 
 # Prevent running dmake during builds. dmake just generates Makefile which we
 # aren't using, and QT translation files, but as we are not building the GUI,
 # there is no need.  And anyways will lead to "Exec format" error as running
 # target binaries on host
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DUSE_MATCHCOMPILER=On -DDISABLE_DMAKE=ON"
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-execinfo"
+}
