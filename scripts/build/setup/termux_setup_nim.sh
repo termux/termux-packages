05c6092de7990644dc8bec29cd7f719d7bd2563b
xiliuya <xiliuya@aliyun.com>
2025-12-28 18:45:14 +0800

    Better download And logic

--- scripts/build/setup/termux_setup_nim.sh
+++ scripts/build/setup/termux_setup_nim.sh
@@ -1,59 +1,66 @@
 termux_setup_nim() {
+	local CHOOSENIM_VERSION=0.8.16
+	local CHOOSENIM_URL="https://github.com/nim-lang/choosenim/releases/download/v${CHOOSENIM_VERSION}/choosenim-${CHOOSENIM_VERSION}_linux_amd64"
+	local CHOOSENIM_FILE=${TERMUX_PKG_TMPDIR}/choosenim
+	local CHOOSENIM_SHA256="1034e03564b3e5b5321646f9ce290cc4d3cdff0cf0e9b523d43f0db8b9a45c74"
 
-	if [[ -z "${TERMUX_NIM_VERSION-}" ]]; then
-		TERMUX_NIM_VERSION=$(
-			. "${TERMUX_SCRIPTDIR}/packages/nim/build.sh"
-			echo ${TERMUX_PKG_VERSION}
-		)
+	local NIM_VERSION
+	local NIM_FOLDER
+	local NIM_PKG_VERSION=$(bash -c ". $TERMUX_SCRIPTDIR/packages/nim/build.sh; echo \$TERMUX_PKG_VERSION")
+
+	if [[ -z "${NIM_VERSION-}" ]]; then
+		NIM_VERSION=${NIM_PKG_VERSION}
 	fi
 
-	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "true" ]]; then
-		if [[ -z "$(command -v nim)" ]]; then
-			cat <<-EOL
-				Package 'nim' is not installed.
-				You can install it with
-
-				pkg install nim
-			EOL
-			exit 1
-		fi
-		local NIM_VERSION=$(nim -v | cut -d ' ' -z -f 4)
-		if [[ "${TERMUX_NIM_VERSION}" != "${NIM_VERSION}" ]]; then
-			cat <<-EOL >&2
-				WARN: On device build with old nim version is not possible!
-				TERMUX_NIM_VERSION = ${TERMUX_NIM_VERSION}
-				NIM_VERSION        = ${NIM_VERSION}
-			EOL
-		fi
-		return
+	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
+		NIM_FOLDER=${TERMUX_SCRIPTDIR}/build-tools/nim-${NIM_VERSION}
 	else
+		NIM_FOLDER=${TERMUX_COMMON_CACHEDIR}/nim-$NIM_VERSION
+	fi
 
-		export CHOOSENIM_CHOOSE_VERSION=${TERMUX_NIM_VERSION}
-		curl https://nim-lang.org/choosenim/init.sh -sSf | bash -s -- -y
-		local NIM_PATH="$HOME/.nimble/bin"
-		export PATH=$NIM_PATH:$PATH
-                
-		local NIM_CPU
-		case "$TERMUX_ARCH" in
-		aarch64) NIM_CPU="arm64" ;;
-		arm) NIM_CPU="arm" ;;
-		i686) NIM_CPU="i386" ;;
-		x86_64) NIM_CPU="amd64" ;;
-		*) NIM_CPU="$TERMUX_ARCH" ;;
-		esac
+	local NIM_CPU
+	case "$TERMUX_ARCH" in
+	aarch64) NIM_CPU="arm64" ;;
+	arm) NIM_CPU="arm" ;;
+	i686) NIM_CPU="i386" ;;
+	x86_64) NIM_CPU="amd64" ;;
+	*) NIM_CPU="$TERMUX_ARCH" ;;
+	esac
 
-		# Cross compile to TERMUX_ARCH, no need to configure `nim.cfg'
-		# Directly compile : nim c ${NIM_FLAGS} *.nim
-		export NIM_FLAGS=" --cc:clang \
+	# Cross compile to TERMUX_ARCH, no need to configure `nim.cfg'
+	# Directly compile : nim c ${NIM_FLAGS} *.nim
+	export NIM_FLAGS=" --cc:clang \
                        --clang.exe=${CC} \
                        --clang.linkerexe=${CC} \
                        --os:android \
                        --cpu:${NIM_CPU} "
 
-	fi
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		if [[ ! -x "${NIM_FOLDER}/bin/nim" ]] && [[ -z "$(command -v nim)" ]]; then
+			termux_download "${CHOOSENIM_URL}" "${CHOOSENIM_FILE}" "${CHOOSENIM_SHA256}"
+			chmod +x ${CHOOSENIM_FILE}
+			"${CHOOSENIM_FILE}" ${NIM_PKG_VERSION} ----nimbleDir:"${NIM_FOLDER}"
+		fi
 
-	if [[ -z "$(command -v nim)" ]]; then
-		termux_error_exit "termux_setup_nim: No nim executable found!"
+		export PATH=$NIM_FOLDER/bin:$PATH
+	else
+		if ([ ! -e "$TERMUX_BUILT_PACKAGES_DIRECTORY/nim" ] ||
+			[ "$(cat "$TERMUX_BUILT_PACKAGES_DIRECTORY/nim")" != "$NIM_PKG_VERSION" ]) &&
+			([[ "$TERMUX_APP_PACKAGE_MANAGER" = "apt" && "$(dpkg-query -W -f '${db:Status-Status}\n' nim 2>/dev/null)" != "installed" ]] ||
+				[[ "$TERMUX_APP_PACKAGE_MANAGER" = "pacman" && ! "$(pacman -Q nim 2>/dev/null)" ]]); then
+			echo "Package 'nim' is not installed."
+			echo "You can install it with"
+			echo
+			echo "  pkg install nim"
+			echo
+			echo "  pacman -S nim"
+			echo
+			echo "or build it from source with"
+			echo
+			echo "  ./build-package.sh nim"
+			echo
+			exit 1
+		fi
+		return
 	fi
-
 }
