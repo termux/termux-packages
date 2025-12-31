13f8310a00016d72251e2193938524b7c680ce8d
BullyMaguire-lol <temp9040@gmail.com>
2025-12-31 17:13:04 +0530

    fix(x11/kf6-qqc2-desktop-style): add post-massage check for org.kde.desktop.so

--- x11-packages/kf6-qqc2-desktop-style/build.sh
+++ x11-packages/kf6-qqc2-desktop-style/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='A style for Qt Quick Controls 2 to make it follow your d
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.21.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/qqc2-desktop-style-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256="e2017b372eb5e1b1297e48533b0b5ad602a85b5687e59312f7ece91cd14b7638"
 TERMUX_PKG_DEPENDS="kf6-kcolorscheme (>= ${TERMUX_PKG_VERSION%.*}), kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kiconthemes (>= ${TERMUX_PKG_VERSION%.*}), kf6-kirigami (>= ${TERMUX_PKG_VERSION%.*}), kf6-sonnet (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase, qt6-qtdeclarative"
@@ -13,3 +14,10 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
+
+termux_step_post_massage() {
+	local file="lib/qt6/plugins/kf6/kirigami/platform/org.kde.desktop.so"
+	if [[ ! -f "${TERMUX_PKG_MASSAGEDIR}${TERMUX_PREFIX}/${file}" ]]; then
+		termux_error_exit "'$TERMUX_PKG_NAME' is malformed, '$file' must exist!"
+	fi
+}
