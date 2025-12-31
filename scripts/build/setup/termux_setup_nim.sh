9e0c5c722c825ba7c0fef061d7da28baa526e88b
xiliuya <xiliuya@aliyun.com>
2025-12-31 17:21:29 +0800

    some fix

--- scripts/build/setup/termux_setup_nim.sh
+++ scripts/build/setup/termux_setup_nim.sh
@@ -73,7 +73,7 @@ termux_setup_nim() {
 
 		[[ -x "${NIM_FOLDER}/bin/nim" ]] && return
 
-		if [[ -z "$(command -v nim)" ]]; then
+		if command -v nim &> /dev/null; then
 			local LOCAL_NIM_VERSION=$(nim --version 2>/dev/null | head -n 1 | awk '{print $4}')
 			if [[ "${LOCAL_NIM_VERSION}" != "${NIM_VERSION}" ]]; then
 				echo "WARN: On device build with old nim version may not possible!"
