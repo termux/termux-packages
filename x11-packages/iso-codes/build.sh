b09a8342e793fafcd0fcff860c2e76da6e0f4991
Biswapriyo Nath <nathbappai@gmail.com>
2026-01-02 07:17:02 +0000

    bump(x11/iso-codes): 4.20.1
    Remove makefile patch because project was migrated to meson.
    https://salsa.debian.org/iso-codes-team/iso-codes/-/blob/v4.20.1/CHANGELOG.md

--- x11-packages/iso-codes/build.sh
+++ x11-packages/iso-codes/build.sh
@@ -2,15 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://salsa.debian.org/iso-codes-team/iso-codes
 TERMUX_PKG_DESCRIPTION="Lists of the country, language, and currency names"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.19.0"
+TERMUX_PKG_VERSION="4.20.1"
 TERMUX_PKG_SRCURL=https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/v${TERMUX_PKG_VERSION}/iso-codes-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=4b143a891feb7d1bb64e44fe3ef253ef36ba1185d1d129c1425338dc6e46e27d
+TERMUX_PKG_SHA256=2d7d9f6084ab9ce6c534ce71a3dd5144b6e474f3c97616459a88f73f44a64bff
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE=newest-tag
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
-#If you install ISO codes over a previous installed version, the install step will fail when creating some symlinks
-
-termux_step_post_configure() {
-	sed -i '/^LN_S/s/s/sfvn/' */Makefile
-}
