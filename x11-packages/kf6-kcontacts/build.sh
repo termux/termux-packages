b8ae31ea2cbe738f14fd8f4e25de9279f0f10197
BullyMaguire-lol <temp9040@gmail.com>
2026-01-03 14:18:11 +0530

    addpkg(x11/kf6-kcontacts): 6.21.0

---
+++ x11-packages/kf6-kcontacts/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/kcontacts"
+TERMUX_PKG_DESCRIPTION="Address book API for KDE"
+TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.21.0"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcontacts-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="a6bf78f10d7680b66366b14431746b3b1d34dc313f1ee7be89b19a476162af64"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kcodecs, kf6-kconfig, kf6-kcoreaddons, kf6-ki18n, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qtdeclarative, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
