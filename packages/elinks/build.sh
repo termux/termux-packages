8f068b85980c9615841fcad8466ed87ef1cf75fe
Yaksh Bariya <thunder-coding@termux.dev>
2026-01-04 13:46:35 +0530

    rebuild(main/elinks): x86_64 wasn't picked up by scripts to reuse CI artifacts somehow.
    Rerunning didn't help. I'm not sure why, so let's just do a rebuild

--- packages/elinks/build.sh
+++ packages/elinks/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Full-Featured Text WWW Browser"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.19.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rkd77/elinks/releases/download/v${TERMUX_PKG_VERSION}/elinks-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a993a4870cadce60abbc724cf6a5c2a80f6be9020243b9e5ce075c16c6665c04
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libexpat, libiconv, libidn, openssl, libbz2, zlib"
