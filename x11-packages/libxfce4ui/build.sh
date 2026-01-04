1daf9b41dd3f7d990322c382c016ae15afff1619
Robert Kirkman <rkirkman@termux.dev>
2026-01-04 02:11:49 -0600

    fix(x11/libxfce4ui): enable vala
    - Necessary for `xfce4-notes-plugin` version 1.12.0
    
    - Add `0002-use-wrapped-vapigen.patch` to work around `/bin/bash: line 1: /data/data/com.termux/files/usr/bin/vapigen-0.56: cannot execute: required file not found`
    
    - Add `TERMUX_PKG_BUILD_IN_SRC=true` to work around `Libxfce4ui-2.0.gir:121.7-121.47: error: Xfce.FilenameInput: Classes cannot have multiple base classes (`Gtk.Box' and `Atk.Implementor')`

--- x11-packages/libxfce4ui/build.sh
+++ x11-packages/libxfce4ui/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Commonly used XFCE widgets among XFCE applications"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.20.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/xfce/libxfce4ui/${TERMUX_PKG_VERSION%.*}/libxfce4ui-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=5d3d67b1244a10cee0e89b045766c05fe1035f7938f0410ac6a3d8222b5df907
 TERMUX_PKG_AUTO_UPDATE=true
@@ -11,12 +12,17 @@ TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, xfce4-dev-tools"
 TERMUX_PKG_RECOMMENDS="hicolor-icon-theme"
 TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
+# for some reason, TERMUX_PKG_BUILD_IN_SRC=true prevents this error:
+# Libxfce4ui-2.0.gir:121.7-121.47: error: Xfce.FilenameInput:
+# Classes cannot have multiple base classes (`Gtk.Box' and `Atk.Implementor')
+# when --enable-vala=yes
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-vendor-info=Termux
 --disable-debug
 --enable-introspection=yes
 --enable-epoxy
---enable-vala=no
+--enable-vala=yes
 --enable-gladeui2=no
 --enable-glibtop
 --enable-gtk-doc-html=no
