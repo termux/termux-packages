c64009474e57ccd0c1ca7a408c7f8d3f053d0185
TomIO <tom@termux.dev>
2026-01-04 08:10:57 +0100

    addpkg(main/tree-sitter-yaml): 0.7.2

---
+++ packages/tree-sitter-yaml/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/tree-sitter-grammars/tree-sitter-yaml
+TERMUX_PKG_DESCRIPTION="YAML grammar for tree-sitter"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
+TERMUX_PKG_VERSION="0.7.2"
+TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-yaml/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=aeaff5731bb8b66c7054c8aed33cd5edea5f4cd2ac71654f3f6c2ba2073d8fac
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE='newest-tag'
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_FORCE_CMAKE=true
+
+termux_step_configure() {
+	termux_setup_treesitter
+}
+
+termux_step_make() {
+	termux-tree-sitter build
+}
+
+termux_step_make_install() {
+	termux-tree-sitter install
+}
