a27be57d5a6d2315c88757eb270a36a8ab296150
Biswapriyo Nath <nathbappai@gmail.com>
2026-01-05 16:16:24 +0000

    bump(x11/libgedit-tepl): 6.14.0

--- x11-packages/libgedit-tepl/build.sh
+++ x11-packages/libgedit-tepl/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/World/gedit/libgedit-tepl
 TERMUX_PKG_DESCRIPTION="Library that eases the development of GtkSourceView-based text editors and IDEs"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.13.0"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="6.14.0"
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/World/gedit/libgedit-tepl/-/archive/$TERMUX_PKG_VERSION/libgedit-tepl-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=39d97a69c6f75a1e86f72976159a4ec3f51996971d1cc5b7289df1315a7d6bab
+TERMUX_PKG_SHA256=9ddd8653a673a80a4184efa822578e3f8abdd00a85ec40e3851dee56ff47450c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE=newest-tag
 TERMUX_PKG_DEPENDS="glib, gsettings-desktop-schemas, gtk3, libcairo, libgedit-amtk, libgedit-gfls, libgedit-gtksourceview, libhandy, libicu, pango"
@@ -28,7 +27,7 @@ termux_step_pre_configure() {
 termux_step_post_massage() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _GUARD_FILE="lib/libgedit-tepl-6.so.3"
+	local _GUARD_FILE="lib/libgedit-tepl-6.so.4"
 	if [ ! -e "${_GUARD_FILE}" ]; then
 		termux_error_exit "file ${_GUARD_FILE} not found."
 	fi
