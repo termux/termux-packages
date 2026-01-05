bbdf0d9d53218b98f0159eafe60ce109efd33a9f
NoNameWasDefined <169661348+NoNameWasDefined@users.noreply.github.com>
2026-01-05 11:34:25 +0100

    enhance(main/virglrenderer): enable Venus

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.2.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b181b668afae817953c84635fac2dc4c2e5786c710b7d225ae215d15674a15c7
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,4 +15,8 @@ termux_step_pre_configure() {
 	# error: using an array subscript expression within 'offsetof' is a Clang extension [-Werror,-Wgnu-offsetof-extensions]
 	# list_for_each_entry_safe(struct vrend_linked_shader_program, ent, &shader->programs, sl[shader->sel->type])
 	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
+
+	if [[ $TERMUX_ARCH != "arm" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
+	fi
 }
