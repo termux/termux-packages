ff484afe22f7361a1a538195a7e4acdfe038e130
Robert Kirkman <rkirkman@termux.dev>
2026-01-07 09:52:05 -0600

    bump(x11/bochs): 3.0
    - Fixes https://github.com/termux/termux-packages/issues/19677
    
    - Drop `iodev-network-slirp-misc.cc.patch`; no longer necessary after https://github.com/bochs-emu/Bochs/pull/299
    
    - Add `--enable-evex` to fix `error: no member named 'opmask' in 'bxInstruction_c'` (https://github.com/bochs-emu/Bochs/issues/480)

--- x11-packages/bochs/build.sh
+++ x11-packages/bochs/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://bochs.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Bochs is a highly portable open source IA-32 (x86) PC emulator and debugger written in C++."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.8
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a85b13aff7d8411f7a9f356ba6c33b5f5dc1fbb107eb5018cc23a62639da0059
+TERMUX_PKG_VERSION="3.0"
+TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=cb6f542b51f35a2cc9206b2a980db5602b7cd1b7cf2e4ed4f116acd5507781aa
 TERMUX_PKG_DEPENDS="glib, gtk2, libc++, libx11, libxpm, libxrandr, ncurses, pango, readline"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -21,6 +20,7 @@ ac_cv_func_strtouq=no
 --enable-disasm
 --enable-3dnow
 --enable-avx
+--enable-evex
 --enable-usb
 --enable-usb-ehci
 --enable-usb-ohci
