e616cbef03fb4f66c924c2460b243dd494dfe21a
Fredrik Fornwall <fredrik@fornwall.net>
2026-01-08 23:28:23 +0100

    chore(x11/bochs): Use gtk3 instead of gtk2

--- x11-packages/bochs/build.sh
+++ x11-packages/bochs/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Bochs is a highly portable open source IA-32 (x86) PC em
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=cb6f542b51f35a2cc9206b2a980db5602b7cd1b7cf2e4ed4f116acd5507781aa
-TERMUX_PKG_DEPENDS="glib, gtk2, libc++, libx11, libxpm, libxrandr, ncurses, pango, readline"
+TERMUX_PKG_DEPENDS="glib, gtk3, libc++, libx11, libxpm, libxrandr, ncurses, pango, readline"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_strtouq=no
