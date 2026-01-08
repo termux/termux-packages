9d73c255aa2050bd64e86a2fe115cf7a4949b670
cnjhb <hikariz@qq.com>
2026-01-08 23:27:12 +0800

    bump(main/luajit-lgi): 0.9.2-p20251219

--- packages/luajit-lgi/build.sh
+++ packages/luajit-lgi/build.sh
@@ -2,11 +2,37 @@ TERMUX_PKG_HOMEPAGE=https://github.com/lgi-devs/lgi
 TERMUX_PKG_DESCRIPTION="Dynamic Lua binding to GObject libraries using GObject-Introspection"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.9.2
-TERMUX_PKG_SRCURL=https://github.com/lgi-devs/lgi/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=cfc4105482b4730b3a40097c9d9e7e35c46df2fb255370bdeb2f45a886548c4f
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+_COMMIT_DATE=20251219
+_COMMIT_HASH=a1308b23b07a787d21fad86157b0b60eb3079f64
+TERMUX_PKG_VERSION=0.9.2-p$_COMMIT_DATE
+TERMUX_PKG_SRCURL=git+https://github.com/lgi-devs/lgi
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_SHA256=23fd016fecd027b456f8fb38b2b79fb45f85bf580895caf2b4aa4f651502f080
 TERMUX_PKG_DEPENDS="glib, gobject-introspection, libffi, libluajit"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout "$_COMMIT_HASH"
+
+	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
+	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
+		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
+		echo -n " different from what is expected to be; should end"
+		echo " with \"${pdate}\"."
+		return 1
+	fi
+
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	pwd
+	echo $s
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		termux_error_exit "Checksum mismatch for source files."
+	fi
+}
+
+termux_step_pre_configure() {
+	# Use makefile instead of meson.
+	rm -f "$TERMUX_PKG_SRCDIR"/meson*
+}
