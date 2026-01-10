7b5fcbf50ae6b71f1cde5fd6a2358362bc2e2e07
TomIO <tom@termux.dev>
2026-01-10 15:19:33 +0100

    chore(main/lua55): rebuild revdeps against new package name

--- packages/lua-lpeg/build.sh
+++ packages/lua-lpeg/build.sh
@@ -3,14 +3,14 @@ TERMUX_PKG_DESCRIPTION="Pattern-matching library for Lua 5.4"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION=1.1.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://www.inf.puc-rio.br/~roberto/lpeg/lpeg-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=4b155d67d2246c1ffa7ad7bc466c1ea899bbc40fef0257cc9c03cecbaed4352a
-TERMUX_PKG_BUILD_DEPENDS="liblua51, liblua52, liblua53, liblua54"
-TERMUX_PKG_DEPENDS="liblua54"
+TERMUX_PKG_BUILD_DEPENDS="lua51, lua52, lua53, lua54, lua55"
+TERMUX_PKG_DEPENDS="lua55"
 
-LUA=('5.1' '5.2' '5.3' '5.4')
 termux_step_pre_configure() {
+	declare -ag LUA=('5.1' '5.2' '5.3' '5.4' '5.5')
 	for lua in "${LUA[@]}"; do
 		cp -r "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/lpeg-${lua//./}"
 	done
@@ -36,6 +36,6 @@ termux_step_make_install() {
 		ln -s "${TERMUX_PREFIX}/lib/liblpeg-$lua.so" "${TERMUX_PREFIX}/lib/lua/$lua/lpeg.so"
 		install -Dm600 lpeg-"${lua//./}"/re.lua "$TERMUX_PREFIX/share/lua/$lua"/re.lua
 	done
-	# make liblpeg-5.4.so the default one
-	ln -s "${TERMUX_PREFIX}/lib/liblpeg-5.4.so" "${TERMUX_PREFIX}/lib/liblpeg.so"
+	# make liblpeg-5.5.so the default one
+	ln -s "${TERMUX_PREFIX}/lib/liblpeg-5.5.so" "${TERMUX_PREFIX}/lib/liblpeg.so"
 }
