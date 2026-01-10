4ca0383d0bee4c1c5e5ee68e771b913fa9cdfb2c
BullyMaguire-lol <temp9040@gmail.com>
2026-01-10 19:20:32 +0530

    addpkg(x11/kmenuedit): 6.5.4

---
+++ x11-packages/kmenuedit/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/kmenuedit"
+TERMUX_PKG_DESCRIPTION="KDE menu editor"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.4"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/kmenuedit-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="e57d76b4231e56f237957c9cd7d8a6881a42638cede400d1e42a6bb48a1896a9"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kcompletion, kf6-kconfig, kf6-kconfigwidgets, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kglobalaccel, kf6-ki18n, kf6-kiconthemes, kf6-kio, kf6-kitemviews, kf6-kservice, kf6-kwidgetsaddons, kf6-kwindowsystem, kf6-kxmlgui, kf6-sonnet, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DBUILD_DOC=OFF
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
