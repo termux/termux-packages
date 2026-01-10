08d9f6e6f7d5d08c5072e0890f2f41597435faf7
Biswapriyo Nath <nathbappai@gmail.com>
2026-01-10 05:47:31 +0000

    bump(x11/kf6-kxmlgui): 6.22.0

--- x11-packages/kf6-kxmlgui/build.sh
+++ x11-packages/kf6-kxmlgui/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/kxmlgui'
 TERMUX_PKG_DESCRIPTION='User configurable main windows'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.21.0"
+TERMUX_PKG_VERSION="6.22.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kxmlgui-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256="cebf390b65bd5355d641b5bedbbc807129d3984a8bc21b08d726815c6d9b970c"
+TERMUX_PKG_SHA256=063511b8a2c498cdd03a18366ec9182f3f121a4d93668888c9aa2bed49973ac3
 TERMUX_PKG_DEPENDS="kf6-kcolorscheme (>= ${TERMUX_PKG_VERSION%.*}), kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kconfigwidgets (>= ${TERMUX_PKG_VERSION%.*}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-kglobalaccel (>= ${TERMUX_PKG_VERSION%.*}), kf6-kguiaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-ki18n (>= ${TERMUX_PKG_VERSION%.*}), kf6-kiconthemes (>= ${TERMUX_PKG_VERSION%.*}), kf6-kitemviews (>= ${TERMUX_PKG_VERSION%.*}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
