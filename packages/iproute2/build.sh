c5c22958b428a087d9f81f8a7bb4e19785b8280c
Juhan280 <azurarobo280@gmail.com>
2026-01-11 17:42:56 +0600

    enhance(main/iproute2): globally set --color=auto

--- packages/iproute2/build.sh
+++ packages/iproute2/build.sh
@@ -3,11 +3,14 @@ TERMUX_PKG_DESCRIPTION="Utilities for controlling networking"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.18.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=6ba520e1975e4c50dc931eeae91ea37c198b8a173744885f8895b84325f9d456
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-support"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--color=auto"
 
 termux_step_pre_configure() {
 	CFLAGS+=" -fPIC"
