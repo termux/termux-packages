1df44e9489789629714d8c05ef8fcc392cb497c8
TomIO <tom@termux.dev>
2026-01-11 18:39:50 +0100

    chore(main/lua54): rebuild revdeps against new package name

---
+++ packages/lua-lpeg/lua54-lpeg.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_DEPEND_ON_PARENT=false
+TERMUX_SUBPKG_INCLUDE='lib/liblpeg-5.4.so lib/lua/5.4/lpeg.so share/lua/5.4/re.lua'
+TERMUX_SUBPKG_DESCRIPTION='Pattern-matching library for Lua 5.4'
+TERMUX_SUBPKG_DEPENDS='lua54'
--- packages/luarocks/build.sh
+++ packages/luarocks/build.sh
@@ -3,17 +3,17 @@ TERMUX_PKG_DESCRIPTION="Deployment and management system for Lua modules"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.13.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://luarocks.org/releases/luarocks-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=245bf6ec560c042cb8948e3d661189292587c5949104677f1eecddc54dbe7e37
 TERMUX_PKG_AUTO_UPDATE=true
-__LUA_VERSION=5.4 # Lua version against which it will be built.
 # Do not use varible here since buildorder.py do not evaluate bash before reading.
 TERMUX_PKG_DEPENDS="curl, lua54"
-TERMUX_PKG_BUILD_DEPENDS="liblua54"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_configure() {
+	declare -g __LUA_VERSION=5.4 # Lua version against which it will be built.
 	if [ "$TERMUX_ON_DEVICE_BUILD" != true ]; then
 		# Create temporary symlink to workaround luarock bootstrap
 		# script trying to run cross-compiled lua
