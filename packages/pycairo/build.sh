a56b0eebabd03e6547fc8f8bdbe701ee95799989
Yaksh Bariya <thunder-coding@termux.dev>
2026-01-12 06:44:57 +0530

    rebuild(main/pycairo): with python 3.13

--- packages/pycairo/build.sh
+++ packages/pycairo/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Python bindings for the cairo graphics library"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.29.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pygobject/pycairo/releases/download/v${TERMUX_PKG_VERSION}/pycairo-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f3f7fde97325cae80224c09f12564ef58d0d0f655da0e3b040f5807bd5bd3142
 TERMUX_PKG_AUTO_UPDATE=true
