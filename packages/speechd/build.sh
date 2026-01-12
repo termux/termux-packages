7dcb18aa82f43c1239a8453010bae9d1bc1a2fc0
Yaksh Bariya <thunder-coding@termux.dev>
2026-01-12 22:14:06 +0530

    rebuild(main/speechd): with python 3.13

--- packages/speechd/build.sh
+++ packages/speechd/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Common interface to speech synthesis"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.12.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=32a730f6fb5981b9bec7e04f3674fd7d29e54935f46cf6354dbb9ab1f9b23b2d
 TERMUX_PKG_SRCURL=https://github.com/brailcom/speechd/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="dotconf, espeak, glib, libiconv, libltdl, libsndfile, pulseaudio, python, speechd-data, libandroid-posix-semaphore"
