a352aab181c901847cf45cf850f2093caef8b67b
Yaksh Bariya <thunder-coding@termux.dev>
2026-01-12 22:28:44 +0530

    rebuild(x11/xkeyboard-config): with python 3.13

--- x11-packages/xkeyboard-config/build.sh
+++ x11-packages/xkeyboard-config/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="X keyboard configuration files"
 TERMUX_PKG_LICENSE="HPND, MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.46"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config/-/archive/xkeyboard-config-${TERMUX_PKG_VERSION}/xkeyboard-config-xkeyboard-config-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=81107e12f71087b3f1d7dea43c186805d46abaffead0cafca9bdd24b94c2007e
 TERMUX_PKG_AUTO_UPDATE=true
