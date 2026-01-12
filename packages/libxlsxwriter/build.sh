2e7923dbb2332a218e18e6522367df7b6e138ef1
Biswapriyo Nath <nathbappai@gmail.com>
2026-01-12 13:23:11 +0000

    bump(main/libxlsxwriter): 1.2.4

--- packages/libxlsxwriter/build.sh
+++ packages/libxlsxwriter/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="A C library for creating Excel XLSX files"
 TERMUX_PKG_LICENSE="BSD 2-Clause, BSD 3-Clause, ZLIB, MPL-2.0, MIT, Public Domain"
 TERMUX_PKG_LICENSE_FILE="License.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.2.3"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.2.4"
 TERMUX_PKG_SRCURL=https://github.com/jmcnamara/libxlsxwriter/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=63f070c19c97ce4d5dfcbc1fa8cc5237d4c9decf39341a31188dbdceef93b542
+TERMUX_PKG_SHA256=e0db59fc248a5ffa465a05ea83a9d466d4bca0e53ab42771515d4ebb467a41c1
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+.\d+.\d+"
 TERMUX_PKG_DEPENDS="libminizip"
@@ -18,7 +17,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=10
+	local _SOVERSION=11
 
 	local v=$(sed -En 's/.*LXW_SOVERSION .*"(.*)".*/\1/p' \
 			include/xlsxwriter.h)
