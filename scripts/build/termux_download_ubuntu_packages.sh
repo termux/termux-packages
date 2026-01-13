e5b8be9aa6930dce165041a3ccdb29e523abd3b3
TomIO <tom@termux.dev>
2026-01-13 06:14:55 +0100

    chore(scripts/termux_download_ubuntu_packages): use inherited vars for overwrites

--- scripts/build/termux_download_ubuntu_packages.sh
+++ scripts/build/termux_download_ubuntu_packages.sh
@@ -2,43 +2,53 @@
 
 termux_download_ubuntu_packages() {
 	{
-		if (( $# < 1 || $# > 4 )); then
-			echo "termux_download_ubuntu_packages(): Invalid arguments - expected <PACKAGES> [<DESTINATION>] [<ARCHITECTURE>] [<MIRROR_URL>]"
+		(( $# )) || {
+			echo "termux_download_ubuntu_packages():"
+			echo "Usage: termux_download_ubuntu_packages [PACKAGES...]"
+			echo "You can additionally overwrite the following parameters via environment variables"
+			echo "\$VAR [default value]"
+			echo ""
+			echo "DESTINATION    [\$TERMUX_PKG_HOSTBUILD_DIR/ubuntu_packages]"
+			echo "ARCHITECTURE   [amd64]"
+			echo "MIRROR_URL     [https://archive.ubuntu.com/ubuntu]"
+			echo "UBUNTU_RELEASE [noble] # latest Ubuntu LTS"
 			return 1
-		fi
+		}
+
 		if [[ "$TERMUX_ON_DEVICE_BUILD" == "true" ]]; then
 			echo "termux_download_ubuntu_packages(): This function should not be used during on-device building!"
 			return 1
 		fi
 	} 1>&2
 
-	local PACKAGES="${1//,/ }"
-	local DESTINATION="${2:-"$TERMUX_PKG_HOSTBUILD_DIR/ubuntu_packages"}"
-	local ARCHITECTURE="${3:-amd64}"
-	local ubuntu_release="noble" # 24.04
-	local MIRROR_URL="${4:-"https://archive.ubuntu.com/ubuntu"}"
+	local DESTINATION ARCHITECTURE MIRROR_URL UBUNTU_RELEASE
+
+	: "${DESTINATION:="$TERMUX_PKG_HOSTBUILD_DIR/ubuntu_packages"}"
+	: "${ARCHITECTURE:=amd64}"
+	: "${MIRROR_URL:="https://archive.ubuntu.com/ubuntu"}"
+	: "${UBUNTU_RELEASE:=noble}" # Default to latest Ubuntu LTS, currently 24.04 Noble Numbat
 
 	mkdir -p "$DESTINATION"
 
-	local cache_directory="${TERMUX_PKG_TMPDIR}/Packages_ubuntu_${ubuntu_release}_${ARCHITECTURE}"
+	local cache_directory="${TERMUX_PKG_TMPDIR}/Packages_ubuntu_${UBUNTU_RELEASE}_${ARCHITECTURE}"
 
 	# Fetch and cache the package lists for the repo channels
 	local channel
 	for channel in "main" "restricted" "universe" "multiverse"; do
 		# Do we already have this?
 		if [[ ! -e "${cache_directory}_$channel.gz" ]]; then
-			echo "Downloading package index for Ubuntu '$ubuntu_release:${ARCHITECTURE}' ${channel}"
+			echo "Downloading package index for Ubuntu '$UBUNTU_RELEASE:${ARCHITECTURE}' ${channel}"
 			curl -sL \
 				-o "${cache_directory}_$channel.gz" \
-				"${MIRROR_URL}/dists/${ubuntu_release}/${channel}/binary-${ARCHITECTURE}/Packages.gz"
+				"${MIRROR_URL}/dists/${UBUNTU_RELEASE}/${channel}/binary-${ARCHITECTURE}/Packages.gz"
 		else
-			echo "Already have package index for Ubuntu '$ubuntu_release:${ARCHITECTURE}' ${channel}"
+			echo "Already have package index for Ubuntu '$UBUNTU_RELEASE:${ARCHITECTURE}' ${channel}"
 		fi
 	done
 
 
 	local package package_filename package_sha256
-	for package in $PACKAGES; do
+	for package in "$@"; do
 
 		read -rd$'\n' package_filename package_sha256 < <(
 			zcat  "$cache_directory"*.gz | \
