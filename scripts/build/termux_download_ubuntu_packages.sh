256b9b5b5deb3660d3e73679fe2055083067642d
TomIO <tom@termux.dev>
2026-01-13 04:02:54 +0100

    enhance(scripts/termux_download_ubuntu_packages): parse from repo index this fully eliminates the flakey resolution behavior we previously ran into with the 'https://packages.ubuntu.com/$rel/$arch/$pkg/download' URLs, additionally we are now able to use the SHA256 sums from the repo manifests to verify the integrity of the downloaded packages.

--- scripts/build/termux_download_ubuntu_packages.sh
+++ scripts/build/termux_download_ubuntu_packages.sh
@@ -3,7 +3,7 @@
 termux_download_ubuntu_packages() {
 	{
 		if (( $# < 1 || $# > 4 )); then
-			echo "termux_download_ubuntu_packages(): Invalid arguments - expected <PACKAGES> [<DESTINATION>] [<ARCHITECTURE>] [<REPOSITORY_URL>]"
+			echo "termux_download_ubuntu_packages(): Invalid arguments - expected <PACKAGES> [<DESTINATION>] [<ARCHITECTURE>] [<MIRROR_URL>]"
 			return 1
 		fi
 		if [[ "$TERMUX_ON_DEVICE_BUILD" == "true" ]]; then
@@ -16,48 +16,46 @@ termux_download_ubuntu_packages() {
 	local DESTINATION="${2:-"$TERMUX_PKG_HOSTBUILD_DIR/ubuntu_packages"}"
 	local ARCHITECTURE="${3:-amd64}"
 	local ubuntu_release="noble" # 24.04
-	local REPOSITORY_URL="${4:-"https://packages.ubuntu.com/$ubuntu_release"}"
+	local MIRROR_URL="${4:-"https://archive.ubuntu.com/ubuntu"}"
 
 	mkdir -p "$DESTINATION"
 
-	local package
-	for package in $PACKAGES; do
-		local summary_url
+	local cache_directory="${TERMUX_PKG_TMPDIR}/Packages_ubuntu_${ubuntu_release}_${ARCHITECTURE}"
 
-		echo "termux_download_ubuntu_packages(): Downloading summary page for '$package'"
-		summary_url="$REPOSITORY_URL/$ARCHITECTURE/$package/download"
-
-		local attempt deb_url wait=50 retries=10
-		for (( attempt = 1; attempt <= retries; attempt++ )); do
-			local summary_file
-
-			summary_file="${TERMUX_COMMON_CACHEDIR}/${package}-summary-page.html"
-			# sometimes, error 500 occurs when attempting to download the summary page,
-			# and termux_download can successfully handle that case using its own retries
-			termux_download "$summary_url" "$summary_file" SKIP_CHECKSUM
-			# capturing any nonzero return value from 'grep' using 'if' instead of terminating is intentional
-			if deb_url="$(grep -oE 'https?://.*\.deb' "$summary_file" | head -n1)"; then
-				break
-			else
-				# sometimes, a summary page is downloaded, but does not contain any valid package URLs,
-				# so needs to be discarded and redownloaded after a medium amount of time, but if the summary page is
-				# redownloaded too soon after an invalid summary page was downloaded, it is often still invalid
-				echo "termux_download_ubuntu_packages(): Attempt $attempt: Failed to obtain valid summary page for '$package'. Retrying in $wait seconds..." 1>&2
-				rm -f "$summary_file"
-			fi
-
-			sleep "$wait"
-		done
-
-		if [[ -z "$deb_url" ]]; then
-			echo "termux_download_ubuntu_packages(): Failed to obtain '$package' after $retries attempts." 1>&2
-			return 1
+	# Fetch and cache the package lists for the repo channels
+	local channel
+	for channel in "main" "restricted" "universe" "multiverse"; do
+		# Do we already have this?
+		if [[ ! -e "${cache_directory}_$channel.gz" ]]; then
+			echo "Downloading package index for Ubuntu '$ubuntu_release:${ARCHITECTURE}' ${channel}"
+			curl -sL \
+				-o "${cache_directory}_$channel.gz" \
+				"${MIRROR_URL}/dists/${ubuntu_release}/${channel}/binary-${ARCHITECTURE}/Packages.gz"
+		else
+			echo "Already have package index for Ubuntu '$ubuntu_release:${ARCHITECTURE}' ${channel}"
 		fi
+	done
+
+
+	local package package_filename package_sha256
+	for package in $PACKAGES; do
+
+		read -rd$'\n' package_filename package_sha256 < <(
+			zcat  "$cache_directory"*.gz | \
+			sed -n "/^Package: ${package}\$/,/^Package:/ {
+					s/^Filename: //p
+					s/^SHA256: //p
+			}"
+		)
+
+		printf -v deb_url '%s/%s' \
+			"$MIRROR_URL" \
+			"$package_filename"
 
 		echo "termux_download_ubuntu_packages(): Downloading '$package'"
 		local deb_name
 		deb_name="${deb_url##*/}"
-		termux_download "$deb_url" "${TERMUX_COMMON_CACHEDIR}/${deb_name}" SKIP_CHECKSUM
+		termux_download "$deb_url" "${TERMUX_COMMON_CACHEDIR}/${deb_name}" "${package_sha256}"
 		mkdir -p "${TERMUX_PKG_TMPDIR}/${deb_name}"
 		ar x "${TERMUX_COMMON_CACHEDIR}/${deb_name}" --output="${TERMUX_PKG_TMPDIR}/${deb_name}"
 		tar xf "${TERMUX_PKG_TMPDIR}/${deb_name}"/data.tar.* -C "${DESTINATION}"
