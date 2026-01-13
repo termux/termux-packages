9d9384afbab28d1ce0b459e48bd9d701da9c07ff
BullyMaguire-lol <temp9040@gmail.com>
2026-01-13 21:36:05 +0530

    fix(x11/libcanberra): correct XDG_DATA_DIRS fallback path

--- x11-packages/libcanberra/build.sh
+++ x11-packages/libcanberra/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="libcanberra defines a simple abstract interface for play
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.30
-TERMUX_PKG_REVISION=8
+TERMUX_PKG_REVISION=9
 TERMUX_PKG_SRCURL=http://0pointer.de/lennart/projects/libcanberra/libcanberra-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=c2b671e67e0c288a69fc33dc1b6f1b534d07882c2aceed37004bf48c601afa72
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gstreamer, gtk3, harfbuzz, libcairo, libltdl, libvorbis, libx11, pango, pulseaudio"
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gstreamer, gtk3, harfbuzz, libcairo, libltdl, libvorbis, libx11, pango, pulseaudio, sound-theme-freedesktop"
 TERMUX_PKG_RECOMMENDS="zenity"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-gtk
