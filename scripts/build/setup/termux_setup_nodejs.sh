b0d7adf3cd07849c6db318d1bf748fdd5fd01349
Yaksh Bariya <thunder-coding@termux.dev>
2026-01-13 22:04:13 +0530

    script(termux_setup_nodejs): update to node v22.22.0
    Node v22.22.0 is a security release

--- scripts/build/setup/termux_setup_nodejs.sh
+++ scripts/build/setup/termux_setup_nodejs.sh
@@ -5,7 +5,7 @@ termux_setup_nodejs() {
 	# Ref: https://github.com/nodejs/node/commit/506b79e888
 	NODE_OPTIONS+=" --network-family-autoselection-attempt-timeout=500"
 	# Use LTS version for now
-	local NODEJS_VERSION=22.21.1
+	local NODEJS_VERSION=22.22.0
 	local NODEJS_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
@@ -20,7 +20,7 @@ termux_setup_nodejs() {
 			local NODEJS_TAR_FILE=$TERMUX_PKG_TMPDIR/nodejs-$NODEJS_VERSION.tar.xz
 			termux_download https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz \
 				"$NODEJS_TAR_FILE" \
-				680d3f30b24a7ff24b98db5e96f294c0070f8f9078df658da1bce1b9c9873c88
+				9aa8e9d2298ab68c600bd6fb86a6c13bce11a4eca1ba9b39d79fa021755d7c37
 			tar -xf "$NODEJS_TAR_FILE" -C "$NODEJS_FOLDER" --strip-components=1
 		fi
 		export PATH=$NODEJS_FOLDER/bin:$PATH
