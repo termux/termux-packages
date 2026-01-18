4065a0b772b28975e8389268f994e084a649f988
Robert Kirkman <rkirkman@termux.dev>
2026-01-18 04:20:29 -0600

    fix(main/libgts): always hostbuild, even during repeated builds
    - This package's `termux_step_host_build()` build in a way that stores a partial build within `$TERMUX_PKG_SRCDIR`, which is erased during repeated builds, so it needs to always be rebuilt in order for repeated builds to succeed

--- packages/libgts/build.sh
+++ packages/libgts/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://gts.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Provides useful functions to deal with 3D surfaces meshed with interconnected triangles"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.7.6
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/gts/gts/${TERMUX_PKG_VERSION}/gts-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION="0.7.6"
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/project/gts/gts/${TERMUX_PKG_VERSION}/gts-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=059c3e13e3e3b796d775ec9f96abdce8f2b3b5144df8514eda0cc12e13e8b81e
 TERMUX_PKG_DEPENDS="glib"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -14,4 +14,11 @@ termux_step_host_build() {
 	# predicates_init executable generates predicates_init.h
 	$TERMUX_PKG_SRCDIR/configure
 	make -C src predicates_init
+}
+
+# prevents error
+# /bin/bash: line 1: ./predicates_init: cannot execute binary file: Exec format error
+# during repeated builds
+termux_step_pre_configure() {
+	rm -rf "$TERMUX_HOSTBUILD_MARKER"
 }
