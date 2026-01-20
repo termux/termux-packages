d8ccaa1d7e964180e0535da608fb088de070a3ee
Robert Kirkman <rkirkman@termux.dev>
2026-01-20 16:23:32 -0600

    bump(x11/luanti): 5.15.0
    - Fixes https://github.com/termux/termux-packages/issues/28107
    
    - Switch to SDL3
    
    - Force on EGL when using SDL; fixes the problems with performance and driver compatibility when using SDL

--- x11-packages/luanti/build.sh
+++ x11-packages/luanti/build.sh
@@ -2,19 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.luanti.org
 TERMUX_PKG_DESCRIPTION="An open source voxel game engine."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1:5.14.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1:5.15.0"
 TERMUX_PKG_SRCURL=https://github.com/luanti-org/luanti/archive/refs/tags/${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=b9f561fa37db3c7ea1b8ba15cfede8282b7a79b9e939b0357269c8b037cf5aea
+TERMUX_PKG_SHA256=07e044df0b87c83f78d8fe143f01693203b0ccc9dad6ed9c99b7f7b886c35313
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="freetype, jsoncpp, libandroid-spawn, libc++, libcurl, libgmp, libjpeg-turbo, libiconv, luajit, libpng, libsqlite, libvorbis, libx11, libxi, luanti-common, openal-soft, opengl, xdg-utils, zlib, zstd"
-# In 5.12.0, luanti upstream officially migrated to SDL2,
-# but for now, enabling SDL2 in Termux would:
-# - disable EGL support, which would significantly reduce performance and driver compatibility
-# - make the colors of everything strangely washed out
-# so it makes more sense to keep SDL2 disabled in this luanti for now,
-# since the non-SDL2 mode seems to continue to work without problems.
-# Enable SDL2 in the future when possible, after the above problems are gone.
+TERMUX_PKG_DEPENDS="freetype, jsoncpp, libandroid-spawn, libc++, libcurl, libgmp, libjpeg-turbo, libiconv, luajit, libpng, libsqlite, libvorbis, luanti-common, openal-soft, opengl, sdl3, xdg-utils, zlib, zstd"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_SERVER=TRUE
 -DBUILD_BENCHMARKS=TRUE
@@ -32,7 +24,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_LTO=FALSE
 -DENABLE_REDIS=FALSE
 -DENABLE_PROMETHEUS=FALSE
--DUSE_SDL2=FALSE
+-DUSE_SDL3=TRUE
 "
 
 termux_step_pre_configure() {
