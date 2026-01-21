7e6cfbe6399940c358722a5691ff10fb2d369c67
BullyMaguire-lol <temp9040@gmail.com>
2026-01-21 17:31:57 +0530

    fix(x11/kf6-kglobalaccel): add D-Bus service file

--- x11-packages/kf6-kglobalaccel/build.sh
+++ x11-packages/kf6-kglobalaccel/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Add support for global workspace shortcuts'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.22.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kglobalaccel-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=332e3be3d0ac2aec8e786419c1e875a1b33ae84b8aada3283639deccc6ffd4d8
 TERMUX_PKG_DEPENDS="qt6-qtbase"
@@ -13,3 +14,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
+
+termux_step_post_make_install() {
+	mkdir -p "$PREFIX/share/dbus-1/services"
+
+	cat > "$PREFIX/share/dbus-1/services/org.kde.kglobalaccel.service" <<EOF
+[D-BUS Service]
+Name=org.kde.kglobalaccel
+Exec=$PREFIX/lib/libexec/kglobalacceld
+EOF
+}
