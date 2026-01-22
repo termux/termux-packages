43847467d0578d46de9cb2e0bd924b4e173d4a03
BullyMaguire-lol <temp9040@gmail.com>
2026-01-22 19:25:02 +0530

    fix(x11/kf6-kcrash): add remove-setgroups-and-setuid.patch
    Co-authored-by: Robert Kirkman <rkirkman@termux.dev>

--- x11-packages/kf6-kcrash/build.sh
+++ x11-packages/kf6-kcrash/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION='Support for application crash analysis and bug report fr
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.22.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kcrash-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=322d7ca0588c008746e997b398111328767b4bc87318e24a0ededa0c191e700e
 TERMUX_PKG_DEPENDS="kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), libc++, libx11, qt6-qtbase"
