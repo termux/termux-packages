1ef089b9aa6f506a01273c14a72fcbbf3e5ea2f4
TomIO <tom@termux.dev>
2026-01-23 00:43:48 +0100

    fix(main/navidrome): fix taglib include error The `GO_VERSION` and `NODE_VERSION` variables are unused in the actual navidrome program, they are only used in the Makefile, which we do not use. And update the description to the more common variation.

--- packages/navidrome/build.sh
+++ packages/navidrome/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.navidrome.org/
-TERMUX_PKG_DESCRIPTION="🎧☁️ Modern Music Server and Streamer compatible with Subsonic/Airsonic"
+TERMUX_PKG_DESCRIPTION="Modern Music Server and Streamer compatible with Subsonic/Airsonic"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
 TERMUX_PKG_VERSION="0.59.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/navidrome/navidrome/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b2a61c975029e59981cc2b443d9382ef92526325dcf26f82c9dd4b004454736d
 TERMUX_PKG_DEPENDS="taglib, ffmpeg"
@@ -15,21 +16,27 @@ termux_step_make() {
 	termux_setup_golang
 	termux_setup_nodejs
 
-	local GO_VERSION=$(grep "^go " $TERMUX_PKG_SRCDIR/go.mod | cut -f 2 -d ' ')
-	local NODE_VERSION=$(. $TERMUX_SCRIPTDIR/packages/golang/build.sh; echo $TERMUX_PKG_VERSION)
-	local GIT_SHA=$(git ls-remote https://github.com/navidrome/navidrome refs/tags/v$TERMUX_PKG_VERSION | head -c 7)
+	local GIT_SHA
+	GIT_SHA="$(git ls-remote https://github.com/navidrome/navidrome "refs/tags/v$TERMUX_PKG_VERSION" | head -c 7)"
 	export GIT_TAG="v$TERMUX_PKG_VERSION"
+
+	# Build backend
+	cd $TERMUX_PKG_SRCDIR
+	export CGO_ENABLED=1 CGO_CFLAGS_ALLOW="--define-prefix"
+	go build -v -ldflags="
+	-X github.com/navidrome/navidrome/consts.gitSha=$GIT_SHA \
+	-X github.com/navidrome/navidrome/consts.gitTag=$GIT_TAG-SNAPSHOT" \
+	-tags=netgo \
+	-o navidrome
+
 	# Build frontend
 	cd $TERMUX_PKG_SRCDIR/ui
 	npm ci && npm run build
 
-	# Build backend
-	cd $TERMUX_PKG_SRCDIR
-	go build -o navidrome -ldflags="-X github.com/navidrome/navidrome/consts.gitSha=$GIT_SHA -X github.com/navidrome/navidrome/consts.gitTag=$GIT_TAG-SNAPSHOT" -tags=netgo
 }
 
 termux_step_make_install() {
-	install -Dm755 -t "${TERMUX_PREFIX}"/bin ${TERMUX_PKG_SRCDIR}/navidrome
+	install -Dm755 -t "${TERMUX_PREFIX}"/bin "${TERMUX_PKG_SRCDIR}/navidrome"
 
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/bash-completion/completions/navidrome.bash"
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/zsh/site-functions/_navidrome"
