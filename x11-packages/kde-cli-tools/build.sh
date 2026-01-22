edc22d781a796d1aa63a28155519b3c70f6757f0
BullyMaguire-lol <temp9040@gmail.com>
2026-01-22 15:58:30 +0530

    chore(x11/kde-cli-tools): enable docs

--- x11-packages/kde-cli-tools/build.sh
+++ x11-packages/kde-cli-tools/build.sh
@@ -3,16 +3,16 @@ TERMUX_PKG_DESCRIPTION="Tools based on KDE Frameworks to better interact with th
 TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.5.5"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/kde-cli-tools-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=083b5d3c8e6315554547e1fc538c7d9f0671e08f7c52f2bc0a5a345027a12279
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="bash, kf6-kcmutils, kf6-kcompletion, kf6-kconfig, kf6-kcoreaddons, kf6-ki18n, kf6-kio, kf6-kparts, kf6-kservice, kf6-kwidgetsaddons, kf6-kwindowsystem, libc++, qt6-qtbase, qt6-qtsvg"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
--DBUILD_DOC=OFF
 "
 termux_step_pre_configure() {
 	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
