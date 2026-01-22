b903a8ecc058b9caee0e8fc1cf81db81a3ac1cd5
BullyMaguire-lol <temp9040@gmail.com>
2026-01-22 15:54:15 +0530

    chore(x11/ark): enable docs

--- x11-packages/ark/build.sh
+++ x11-packages/ark/build.sh
@@ -3,14 +3,14 @@ TERMUX_PKG_DESCRIPTION="KDE Archiving Tool"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="25.12.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/ark-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=e9961ffdfa908959cec65d443572cb8ccedaa8d62651f2a5f6d72cf95aff27fe
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="7zip, arj, kf6-breeze-icons, kf6-kcolorscheme, kf6-kcompletion, kf6-kconfig, kf6-kconfigwidgets, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kfilemetadata, kf6-ki18n, kf6-kio, kf6-kjobwidgets, kf6-kparts, kf6-kpty, kf6-kservice, kf6-kwidgetsaddons, kf6-kwindowsystem, kf6-kxmlgui, libarchive, libc++, libzip, lrzip, lzop, qt6-qtbase, unrar, zlib"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
--DBUILD_DOC=OFF
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
