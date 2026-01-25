182b575f08cb9434044b111de120c8c738f7b43c
BullyMaguire-lol <temp9040@gmail.com>
2026-01-25 18:10:10 +0530

    addpkg(x11/print-manager): 6.5.5

---
+++ x11-packages/print-manager/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/print-manager"
+TERMUX_PKG_DESCRIPTION=" A tool for managing print jobs and printers"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.5"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/print-manager-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="3a0486d53e7a317acc47c31a9cd3cc2a9bdb03196ee5f6a6befaba8d61f8ba25"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="cups, kf6-kcmutils, kf6-kconfig, kf6-kcoreaddons, kf6-kdbusaddons, kf6-ki18n, kf6-kio, kf6-kirigami, kf6-kitemmodels, kf6-knotifications, kf6-kwidgetsaddons, kf6-kwindowsystem, libc++, libplasma, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
