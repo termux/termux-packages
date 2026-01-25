0be3e98771f64cdd26b8650d1bbd6e8eb814d2ad
TomIO <tom@termux.dev>
2026-01-25 17:56:16 +0100

    fix(main/{adms,exiftool,lib{p11,rav1e,vigra}}): remove auto-update regex no longer needed for these packages
    %ci:no-build

--- packages/libp11/build.sh
+++ packages/libp11/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_SRCURL=https://github.com/OpenSC/libp11/releases/download/libp11-${TE
 TERMUX_PKG_SHA256=97777640492fa9e5831497e5892e291dfbf39a7b119d9cb6abb3ec8c56d17553
 TERMUX_PKG_AUTO_UPDATE=true
 # Make sure we strip off the entire `libp11-` prefix from the tag name.
-TERMUX_PKG_UPDATE_VERSION_REGEXP='libp11-\K\d+\.\d+\.\d+$'
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/^libp11-//'
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-static
