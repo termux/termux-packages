ddd2eec7c6b9c239cbf8658e8d47e0ee799a1dd7
Robert Kirkman <rkirkman@termux.dev>
2026-01-25 04:27:25 -0600

    bump(main/vis): 0.9-p20250111
    - The stable release is too old and is not properly compatible with Lua 5.5, but the development branch appears to be
    
    - Build with Lua 5.5
    
    - Fixes this error at runtime: `attempt to index a nil value`

--- packages/vis/build.sh
+++ packages/vis/build.sh
@@ -2,14 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.brain-dump.org/projects/vis/
 TERMUX_PKG_DESCRIPTION="Modern, legacy free, simple yet efficient vim-like editor"
 TERMUX_PKG_LICENSE="ISC"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.9"
-TERMUX_PKG_REVISION=6
-TERMUX_PKG_SRCURL=https://github.com/martanne/vis/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=bd37ffba5535e665c1e883c25ba5f4e3307569b6d392c60f3c7d5dedd2efcfca
+TERMUX_PKG_VERSION="0.9-p20250111"
+_COMMIT=b7ec2672d626e964da70cda46b132a8306add993
+TERMUX_PKG_SRCURL="https://github.com/martanne/vis/archive/$_COMMIT.zip"
+TERMUX_PKG_SHA256=f3839581663071952c273cfc48762a3d343d5f8ac2dd7a8bceb8df4e82d89607
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="lua54, libunibilium, libtermkey, lua-lpeg, ncurses"
+TERMUX_PKG_DEPENDS="lua55, libunibilium, libtermkey, lua-lpeg, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_pre_configure() {
-	CFLAGS+=" -I$TERMUX_PREFIX/include -I$TERMUX_PREFIX/include/lua/5.4"
-}
