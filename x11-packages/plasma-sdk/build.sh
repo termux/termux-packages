3a4c2802599f70ea4155c888b6c0430187ad430b
BullyMaguire-lol <temp9040@gmail.com>
2026-01-25 14:40:41 +0530

    addpkg(x11/plasma-sdk): 6.5.5

---
+++ x11-packages/plasma-sdk/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/plasma-sdk"
+TERMUX_PKG_DESCRIPTION="Applications useful for Plasma development"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.5.5"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/plasma-sdk-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="b6dc48520ccda4415b0aa6a66502db7180bcd3e1d8975a5f2040519ef70939c4"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-karchive, kf6-kcmutils, kf6-kcolorscheme, kf6-kcompletion, kf6-kconfig, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kdeclarative, kf6-ki18n, kf6-kiconthemes, kf6-kio, kf6-kirigami, kf6-kitemmodels, kf6-knewstuff, kf6-kpackage, kf6-ksvg, kf6-ktexteditor, kf6-kwidgetsaddons, libc++, libplasma, plasma5support, qt6-qt5compat, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
