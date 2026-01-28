1096fb50a9569b73c0d96032c8e08b6bab0c61ca
TomIO <tom@termux.dev>
2026-01-28 03:31:46 +0100

    enhance(main/tree-sitter-c): use `termux-tree-sitter` build helper

--- packages/tree-sitter-c/build.sh
+++ packages/tree-sitter-c/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="C grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.24.1"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter/tree-sitter-c/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=25dd4bb3dec770769a407e0fc803f424ce02c494a56ce95fedc525316dcf9b48
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,11 +14,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_INCLUDEDIR=$TERMUX__PREFIX__INCLUDE_SUBDIR
 "
 
-termux_step_pre_configure() {
+termux_step_configure() {
 	termux_setup_treesitter
 }
 
-termux_step_post_make_install() {
-	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
-	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-c.so "${TERMUX_PREFIX}"/lib/tree_sitter/c.so
+termux_step_make() {
+	termux-tree-sitter build
+}
+
+termux_step_make_install() {
+	termux-tree-sitter install
 }
