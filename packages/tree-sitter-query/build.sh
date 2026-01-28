b9f96a62e631b6b1444f8ffbe0f575f7907aed2c
TomIO <tom@termux.dev>
2026-01-28 03:33:13 +0100

    enhance(main/tree-sitter-query): use `termux-tree-sitter` build helper

--- packages/tree-sitter-query/build.sh
+++ packages/tree-sitter-query/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="TS query grammar for tree-sitter"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.8.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-query/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c2b23b9a54cffcc999ded4a5d3949daf338bebb7945dece229f832332e6e6a7d
 TERMUX_PKG_AUTO_UPDATE=true
@@ -13,11 +14,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_INCLUDEDIR=$TERMUX__PREFIX__INCLUDE_SUBDIR
 "
 
-termux_step_pre_configure() {
+termux_step_configure() {
 	termux_setup_treesitter
 }
 
-termux_step_post_make_install() {
-	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
-	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-query.so "${TERMUX_PREFIX}"/lib/tree_sitter/query.so
+termux_step_make() {
+	termux-tree-sitter build
+}
+
+termux_step_make_install() {
+	termux-tree-sitter install
 }
