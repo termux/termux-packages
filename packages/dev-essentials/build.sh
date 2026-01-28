2c0a79b862abababcf0b7120b06c222be5f866fd
Dess-Services <dessservicesofc@gmail.com>
2026-01-28 10:28:37 -0300

    dev-essentials: new package for beginner developers

--- packages/dev-essentials/build.sh
+++ packages/dev-essentials/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/Dess-Services/dev-essentials
-TERMUX_PKG_DESCRIPTION="Unified development suite for APK building, Web, and Native coding. This package is dedicated to all beginner programmers and those who have recently discovered Termux, providing essential tools and exclusive diagnostic commands to streamline the learning curve."
+TERMUX_PKG_DESCRIPTION="Unified development suite for APK building, Web, and Native coding. Includes D8, DX, JQ, and full compilers for Termux beginners."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Dess-Services <dessservicesofc@gmail.com>"
 TERMUX_PKG_VERSION=1.0.1
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/Dess-Services/dev-essentials/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=720dc8725fdca377a847e4708576093e544250558870c34c2e641369f13f9198
-TERMUX_PKG_DEPENDS="python, openjdk-17, clang, binutils, coreutils, nano, php, git, aapt2, dx, ecj, zlib, libandroid-support, p7zip, zstd, lz4, xz-utils, apksigner, make, cmake, gdb"
+TERMUX_PKG_DEPENDS="python, openjdk-17, clang, binutils, coreutils, nano, php, git, jq, aapt2, dx, ecj, zlib, libandroid-support, p7zip, zstd, lz4, xz-utils, apksigner, make, cmake, gdb, tar, unzip, zip, curl, wget"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make_install() {
