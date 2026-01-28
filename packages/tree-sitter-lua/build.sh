b3f0b1a3440b5ccd1da350e6c35aede8ba675629
TomIO <tom@termux.dev>
2026-01-28 03:32:36 +0100

    enhance(main/tree-sitter-lua): use `termux-tree-sitter` build helper

--- packages/tree-sitter-lua/build.sh
+++ packages/tree-sitter-lua/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lua grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.4.0"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-lua/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=b0977aced4a63bb75f26725787e047b8f5f4a092712c840ea7070765d4049559
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,11 +14,14 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_INCLUDEDIR=$TERMUX__PREFIX__INCLUDE_SUBDIR
 "
 
-termux_step_pre_configure() {
+termux_step_configure() {
 	termux_setup_treesitter
 }
 
-termux_step_post_make_install() {
-	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
-	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-lua.so "${TERMUX_PREFIX}"/lib/tree_sitter/lua.so
+termux_step_make() {
+	termux-tree-sitter build
+}
+
+termux_step_make_install() {
+	termux-tree-sitter install
 }
