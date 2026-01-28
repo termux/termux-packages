88e70dc28e5881201bd9eaf82196347322f7c242
TomIO <tom@termux.dev>
2026-01-28 03:32:51 +0100

    enhance(main/tree-sitter-markdown): use `termux-tree-sitter` build helper

--- packages/tree-sitter-markdown/build.sh
+++ packages/tree-sitter-markdown/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Markdown grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="0.5.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-markdown/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=122be47d4a50ac75a4f6861a17c3c88144197e0949f5d83ca917f2382b37761b
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,13 +15,20 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DALL_EXTENSIONS=ON
 "
 
-termux_step_pre_configure() {
-	termux_setup_nodejs
+termux_step_configure() {
 	termux_setup_treesitter
 }
 
-termux_step_post_make_install() {
-	install -d "${TERMUX_PREFIX}"/lib/tree_sitter
-	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown.so
-	ln -sf "${TERMUX_PREFIX}"/lib/libtree-sitter-markdown-inline.so "${TERMUX_PREFIX}"/lib/tree_sitter/markdown_inline.so
+termux_step_make() {
+	local parser
+	for parser in "markdown" "markdown-inline"; do
+		termux-tree-sitter build -s "tree-sitter-$parser/src"
+	done
+}
+
+termux_step_make_install() {
+	local parser
+	for parser in "markdown" "markdown-inline"; do
+		termux-tree-sitter install -s "tree-sitter-$parser/src"
+	done
 }
