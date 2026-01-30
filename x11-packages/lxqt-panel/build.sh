d9beb7204310ccebf6c2996d630986e5e93c8fce
Fredrik Fornwall <fredrik@fornwall.net>
2026-01-30 15:34:09 +0100

    revbump(x11/{libqtxdg,lxqt-panel,lxqt-qtplugin,qt6ct}): for Qt 6.10.2
    These packages use Qt private APIs, so they must be recompiled every
    single time that Qt is bumped, otherwise they can have errors.
    
    See https://github.com/termux/termux-packages/pull/27371 for previous
    bump and linked errors.

--- x11-packages/lxqt-panel/build.sh
+++ x11-packages/lxqt-panel/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The LXQt desktop panel"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.3.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-panel/releases/download/${TERMUX_PKG_VERSION}/lxqt-panel-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=f1ab0a2a645765045078621b6a510ec61685fbe4446422f9f9fb1cded62eb2d0
 TERMUX_PKG_DEPENDS="kf6-kwindowsystem, layer-shell-qt, libc++, libdbusmenu-lxqt, liblxqt, libqtxdg, libsysstat, libxcb, libxkbcommon, libxtst, libx11, lxqt-globalkeys, lxqt-menu-data, pulseaudio, qt6-qtbase, xcb-util, xcb-util-image"
