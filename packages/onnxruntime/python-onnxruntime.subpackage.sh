f15be7f932147b4702f845ec51c9732887000569
Robert Kirkman <rkirkman@termux.dev>
2026-02-02 15:14:49 -0600

    addpkg(main/onnxruntime): 1.23.2-1

---
+++ packages/onnxruntime/python-onnxruntime.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_DESCRIPTION="Cross-platform, high performance ML inferencing and training accelerator (Python bindings)"
+TERMUX_SUBPKG_INCLUDE="
+bin
+lib/python*
+"
+TERMUX_SUBPKG_DEPENDS="python, python-numpy, python-pip"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=deps
