15dff67931e5bdd85c787a861c873c84dda26ab7
Robert Kirkman <rkirkman@termux.dev>
2026-02-02 09:27:31 -0600

    bump(x11/tigervnc): 1.16.0
    - Rebase `disable-pam.patch`
    
    - `tigervnc-1.9.0_common_os_Thread.cxx.patch` no longer necessary because all calls to `pthread_cancel()` have been removed from the upstream repository

--- x11-packages/tigervnc/build.sh
+++ x11-packages/tigervnc/build.sh
@@ -2,21 +2,26 @@ TERMUX_PKG_HOMEPAGE=https://tigervnc.org/
 TERMUX_PKG_DESCRIPTION="Suite of VNC servers. Based on the VNC 4 branch of TightVNC."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(1.15.0
+TERMUX_PKG_VERSION=(1.16.0
 # Align the version with `xorg-server` package.
                     21.1.16)
-TERMUX_PKG_SRCURL=(https://github.com/TigerVNC/tigervnc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-                   https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz)
-TERMUX_PKG_SHA256=(7f231906801e89f09a212e86701f3df1722e36767d6055a4e619390570548537
+TERMUX_PKG_SRCURL=("https://github.com/TigerVNC/tigervnc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+                   "https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION[1]}.tar.xz")
+TERMUX_PKG_SHA256=(10512fc0254ae3bde41c19d18c15f7ebd8cd476261afe0611c41965d635d46e8
                    b14a116d2d805debc5b5b2aac505a279e69b217dae2fae2dfcb62400471a9970)
-TERMUX_PKG_REVISION=2
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libgmp, libgnutls, libjpeg-turbo, libnettle, libpixman, libx11, libxau, libxdamage, libxdmcp, libxext, libxfixes, libxfont2, libxrandr, libxshmfence, libxtst, opengl, openssl, perl, xkeyboard-config, xorg-xauth, xorg-xkbcomp, zlib"
 TERMUX_PKG_BUILD_DEPENDS="xorg-font-util, xorg-server, xorg-util-macros, xorgproto, xtrans"
 TERMUX_PKG_SUGGESTS="aterm, xorg-twm"
 
-TERMUX_PKG_FOLDERNAME=tigervnc-${TERMUX_PKG_VERSION}
+TERMUX_PKG_FOLDERNAME="tigervnc-${TERMUX_PKG_VERSION}"
 # Viewer has a separate package tigervnc-viewer. Do not build viewer here.
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DBUILD_VIEWER=OFF -DENABLE_NLS=OFF -DENABLE_PAM=OFF -DENABLE_GNUTLS=ON -DFLTK_MATH_LIBRARY="
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_VIEWER=OFF
+-DENABLE_NLS=OFF
+-DENABLE_PAM=OFF
+-DENABLE_GNUTLS=ON
+-DFLTK_MATH_LIBRARY=
+"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
