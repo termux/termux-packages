5215f9057d9db6a4d0c98f6b63291ecd02c9a3f3
Robert Kirkman <rkirkman@termux.dev>
2026-02-04 08:25:33 -0600

    fix(main/gst-plugins-base): revert commit that breaks the build of `vlc` and explicitly disable wayland

--- packages/gst-plugins-base/build.sh
+++ packages/gst-plugins-base/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="GStreamer base plug-ins"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.28.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=eace79d63bd2edeb2048777ea9f432d8b6e7336e656cbc20da450f6235758b31
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,12 +16,16 @@ TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
 
 # wrap-mode=nodownload prevents downloading gl-headers which conflicts with libglvnd-dev
+# -Dgl_winsys=egl,surfaceless,x11,android (disabling wayland)
+# prevents 'ld.lld: error: undefined symbol: gst_gl_display_wayland_get_type'
+# if wayland libraries are present in $TERMUX_PREFIX before building gst-plugins-base
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dintrospection=enabled
 -Dtests=disabled
 -Dexamples=disabled
 -Dpango=disabled
 --wrap-mode=nodownload
+-Dgl_winsys=egl,surfaceless,x11,android
 "
 
 termux_step_pre_configure() {
