953aad371a4d72a6e04a6f7757729308f25b5e3d
Robert Kirkman <rkirkman@termux.dev>
2026-02-06 11:33:11 -0600

    bump(x11/glslviewer): 3.5.1
    - Fixes https://github.com/termux/termux-packages/issues/28288
    
    - Rebase `disable-update-database.patch`
    
    - Drop `cast-getexposure-setuniform.patch`, `vera-disable-exiv2.patch` and `vera-ffmpeg8-support.patch`; all present upstream in the new release
    
    - This package should update to release tags only, because a "5.5.0" tag appeared recently that is not associated with a release.

--- x11-packages/glslviewer/build.sh
+++ x11-packages/glslviewer/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE="https://patriciogonzalezvivo.com/2015/glslViewer/"
 TERMUX_PKG_DESCRIPTION="Console-based GLSL Sandbox for 2D/3D shaders"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.2.4"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="3.5.1"
 TERMUX_PKG_SRCURL=git+https://github.com/patriciogonzalezvivo/glslViewer
-TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_GIT_BRANCH="$TERMUX_PKG_VERSION"
 TERMUX_PKG_DEPENDS="ffmpeg, glfw, glu, libdrm, liblo, libxcb, mesa-dev, ncurses"
 TERMUX_PKG_RECOMMENDS="xorg-server-xvfb"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE=latest-release-tag
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 "
