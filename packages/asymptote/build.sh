17f1843af3bbd034beb59cb040abc870c931e3dc
Robert Kirkman <rkirkman@termux.dev>
2026-02-06 13:10:05 -0600

    bump(main/asymptote): 3.06
    - Fixes https://github.com/termux/termux-packages/issues/28304
    
    - Disable LSP to avoid `/bin/sh: 1: cmake: not found`
    
    - Disable GUI to avoid `ERROR: PySide6 is not installed`
    
    - Remove `CMakeLists.txt` to continue using AutoTools build

--- packages/asymptote/build.sh
+++ packages/asymptote/build.sh
@@ -2,17 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://asymptote.sourceforge.io/
 TERMUX_PKG_DESCRIPTION="A powerful descriptive vector graphics language for technical drawing"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.05"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/asymptote/asymptote-${TERMUX_PKG_VERSION}.src.tgz
-TERMUX_PKG_SHA256=35c16d0a3bdd869a56e4efff4638f81c3a88b2f6b664d196471015dbf4c69a87
+TERMUX_PKG_VERSION="3.06"
+TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/asymptote/asymptote-${TERMUX_PKG_VERSION}.src.tgz"
+TERMUX_PKG_SHA256=5cc861968fe8102fc5564b6075db2837dd5698672688b3bfb71406c0da0f8cef
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="fftw, libc++, libtirpc, zlib, ncurses, readline"
 TERMUX_PKG_BUILD_DEPENDS="glm"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-gc
+--disable-lsp
 "
+
+termux_step_pre_configure() {
+	rm -f CMakeLists.txt
+}
 
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin asy
