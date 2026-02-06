1fea396014808b151dbf801a5419bd0158cb8f9c
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-02-06 21:36:07 +0800

    libnettle: revert to 3.x

--- packages/libnettle/build.sh
+++ packages/libnettle/build.sh
@@ -2,10 +2,24 @@ TERMUX_PKG_HOMEPAGE=https://www.lysator.liu.se/~nisse/nettle/
 TERMUX_PKG_DESCRIPTION="Cryptographic library that is designed to fit easily in more or less any context"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.0"
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/nettle/nettle-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3addbc00da01846b232fb3bc453538ea5468da43033f21bb345cb1e9073f5094
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_VERSION="4.0+really3.10.2"
+TERMUX_PKG_SRCURL="https://mirrors.kernel.org/gnu/nettle/nettle-${TERMUX_PKG_VERSION#*really}.tar.gz"
+TERMUX_PKG_SHA256=fe9ff51cb1f2abb5e65a6b8c10a92da0ab5ab6eaf26e7fc2b675c45f1fb519b5
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_BREAKS="libnettle-dev"
 TERMUX_PKG_REPLACES="libnettle-dev"
+
+termux_step_post_massage() {
+	# Do not forget to bump revision of reverse dependencies and rebuild them
+	# after SOVERSION is changed.
+	local _SOVERSION_GUARD_FILES=(
+		'lib/libhogweed.so.6'
+		'lib/libnettle.so.8'
+	)
+
+	local f
+	for f in "${_SOVERSION_GUARD_FILES[@]}"; do
+		[ -e "${f}" ] || termux_error_exit "SOVERSION guard check failed."
+	done
+}
