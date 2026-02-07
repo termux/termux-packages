f31bd3f69e241701eeb0f885ba71d08536ad4ac0
Sovan <lunsokhasovan@proton.me>
2026-02-07 20:33:35 +0700

    addpkg(main/gweather-locations): 2025.1

---
+++ packages/gweather-locations/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://gitlab.gnome.org/GNOME/gweather-locations"
+TERMUX_PKG_DESCRIPTION="The GWeather locations database"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=2025.1
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/gweather-locations/${TERMUX_PKG_VERSION%.*}/gweather-locations-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=5609ee04cf6ac4cc7721f3241bd8d69ffebf0f1b60210fadd88c869a4b884d87
+TERMUX_PKG_BUILD_DEPENDS="gettext"
+TERMUX_PKG_AUTO_UPDATE=true
