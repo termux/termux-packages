4078f76ab1977882565d1e5d4a3ce0b200971b48
Robert Kirkman <rkirkman@termux.dev>
2026-02-09 09:06:01 -0600

    fix(main/navidrome): implement configuration file
    - Fixes https://github.com/termux/termux-packages/issues/28386
    
    - Drop `conf-configuration.go.patch` - not necessary after other fixes
    
    - Build frontend before backend in order to correctly bundle it into the binary

--- packages/navidrome/build.sh
+++ packages/navidrome/build.sh
@@ -3,11 +3,13 @@ TERMUX_PKG_DESCRIPTION="Modern Music Server and Streamer compatible with Subsoni
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="2096779623 <admin@utermux.dev>"
 TERMUX_PKG_VERSION="0.60.2"
-TERMUX_PKG_SRCURL=https://github.com/navidrome/navidrome/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/navidrome/navidrome/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=1189697b8de66d443fea512de20c2a7064a985e171c6f0ff0cc8f0d7fb496e68
 TERMUX_PKG_DEPENDS="taglib, ffmpeg"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_CONFFILES="etc/navidrome/navidrome.toml"
 
 termux_step_make() {
 	rm -f Makefile
@@ -19,19 +21,19 @@ termux_step_make() {
 	GIT_SHA="$(git ls-remote https://github.com/navidrome/navidrome "refs/tags/v$TERMUX_PKG_VERSION" | head -c 7)"
 	export GIT_TAG="v$TERMUX_PKG_VERSION"
 
+	# Build frontend
+	pushd "$TERMUX_PKG_SRCDIR/ui"
+	npm ci && npm run build
+	popd
+
 	# Build backend
-	cd $TERMUX_PKG_SRCDIR
+	cd "$TERMUX_PKG_SRCDIR"
 	export CGO_ENABLED=1 CGO_CFLAGS_ALLOW="--define-prefix"
 	go build -v -ldflags="
 	-X github.com/navidrome/navidrome/consts.gitSha=$GIT_SHA \
 	-X github.com/navidrome/navidrome/consts.gitTag=$GIT_TAG-SNAPSHOT" \
 	-tags=netgo \
 	-o navidrome
-
-	# Build frontend
-	cd $TERMUX_PKG_SRCDIR/ui
-	npm ci && npm run build
-
 }
 
 termux_step_make_install() {
@@ -40,6 +42,10 @@ termux_step_make_install() {
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/bash-completion/completions/navidrome.bash"
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/zsh/site-functions/_navidrome"
 	install -Dm644 /dev/null "${TERMUX_PREFIX}/share/fish/vendor_completions.d/navidrome.fish"
+
+	install -Dm644 -t "${TERMUX_PREFIX}/etc/navidrome" release/linux/navidrome.toml
+
+	install -Dm644 /dev/null "${TERMUX_PREFIX}/opt/navidrome/music/.placeholder"
 }
 
 termux_step_create_debscripts() {
