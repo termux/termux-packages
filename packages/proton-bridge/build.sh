4a286bb04a7a88789485970a6216af05fcdef8cf
Robert Kirkman <rkirkman@termux.dev>
2026-02-09 09:53:59 -0600

    bump(main/proton-bridge): 3.22.0
    - Fixes https://github.com/termux/termux-packages/issues/28390
    
    - Disable `libfido2` because Termux does not currently have a `libfido2` package

--- packages/proton-bridge/build.sh
+++ packages/proton-bridge/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/ProtonMail/proton-bridge
 TERMUX_PKG_DESCRIPTION="ProtonMail Bridge application"
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_VERSION="3.21.2"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="3.22.0"
 TERMUX_PKG_SRCURL=git+https://github.com/ProtonMail/proton-bridge
-TERMUX_PKG_GIT_BRANCH=v${TERMUX_PKG_VERSION}
+TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_DEPENDS="glib, libsecret"
 TERMUX_PKG_AUTO_UPDATE=true
@@ -18,6 +17,9 @@ termux_step_make() {
 	termux_setup_golang
 	export GOPATH="${TERMUX_PKG_BUILDDIR}"
 	cd "${TERMUX_PKG_SRCDIR}" || exit 1
+
+	rm -rf internal/fido \
+			internal/frontend/grpc/fido.go
 
 	make build-nogui
 }
