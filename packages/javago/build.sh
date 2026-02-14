2814538ba86025b248b439c4a14af6d81f666ed9
GokuXZI <gestaccacc@gmail.com>
2026-02-14 19:18:27 +0530

    Use official release and add checksum

--- packages/javago/build.sh
+++ packages/javago/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/GokuXZI/javago
 TERMUX_PKG_DESCRIPTION="Automatic Java compiler and runner for Termux"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="GokuXZI"
-TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_SRCURL=https://github.com/GokuXZI/javago/archive/refs/tags/v1.0.tar.gz
-TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/GokuXZI/javago/archive/refs/tags/v1.0.0.tar.gz
+TERMUX_PKG_SHA256=2d158eab159a067c212c4f208517ae7f5622c3a120993b00ee35f76e5a9ec8c5
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="openjdk"
 
