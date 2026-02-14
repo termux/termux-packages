513dc67cfc9ff2ba15142ac92a755ba6b7992dc4
Elliana May <me@mause.me>
2026-02-15 03:43:18 +0800

    addpkg(main/CTranslate2): 4.7.1

---
+++ packages/CTranslate2/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE='https://github.com/OpenNMT/CTranslate2.git'
+TERMUX_PKG_DESCRIPTION='Fast inference engine for Transformer models'
+TERMUX_PKG_LICENSE='MIT'
+TERMUX_PKG_MAINTAINER='@termux'
+TERMUX_PKG_VERSION='4.7.1'
+TERMUX_PKG_SRCURL=https://github.com/OpenNMT/CTranslate2/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256='64beb499c1e33500a691dfbe10cc5def7b914413c7f7c2830b0e0d8d544a9f7e'
