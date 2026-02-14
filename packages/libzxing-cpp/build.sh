e7f027caf69c79b3c0492ca0aeb0cf43426fc9c3
Biswapriyo Nath <nathbappai@gmail.com>
2026-02-14 14:00:16 +0000

    bump(main/libzxing-cpp): 3.0.1
    See the release notes for source url change and -DZXING_WRITERS=BOTH option.
    https://github.com/zxing-cpp/zxing-cpp/releases/tag/v3.0.1

--- packages/libzxing-cpp/build.sh
+++ packages/libzxing-cpp/build.sh
@@ -2,21 +2,21 @@ TERMUX_PKG_HOMEPAGE=https://github.com/zxing-cpp/zxing-cpp
 TERMUX_PKG_DESCRIPTION="An open-source, multi-format 1D/2D barcode image processing library implemented in C++"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.3.0"
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/zxing-cpp/zxing-cpp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=64e4139103fdbc57752698ee15b5f0b0f7af9a0331ecbdc492047e0772c417ba
+TERMUX_PKG_VERSION="3.0.1"
+TERMUX_PKG_SRCURL="https://github.com/zxing-cpp/zxing-cpp/releases/download/v${TERMUX_PKG_VERSION}/zxing-cpp-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=6499792c44b6c297654565539abf67f02e0baa7c35aade6299e5cc722e0a897f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DZXING_EXAMPLES=ON
 -DZXING_BLACKBOX_TESTS=OFF
+-DZXING_WRITERS=BOTH
 "
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=3
+	local _SOVERSION=4
 
 	local _ver=$(sed -En 's/^set \(ZXING_SONAME\s+([0-9]+).*/\1/p' "$TERMUX_PKG_SRCDIR"/core/CMakeLists.txt)
 
