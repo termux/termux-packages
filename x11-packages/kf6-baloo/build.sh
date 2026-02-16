a0ec41e5d69f209458b86152b3138543e779c277
Robert Kirkman <rkirkman@termux.dev>
2026-02-16 15:03:42 -0600

    bump(x11/kf6-baloo): 6.23.0

--- x11-packages/kf6-baloo/build.sh
+++ x11-packages/kf6-baloo/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://invent.kde.org/frameworks/baloo"
 TERMUX_PKG_DESCRIPTION="A framework for searching and managing metadata"
 TERMUX_PKG_LICENSE="LGPL-2.0-only, LGPL-3.0-only"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.22.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="6.23.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/baloo-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=57b2ac1ae953d499b7364125fe3e7aa5857fb532456ecf0bda2635e875a66d2a
+TERMUX_PKG_SHA256=61cec3ea772d083421a9d94dac2d4b9d7bd1bd74abf3dece97df2ec646d20113
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="kf6-kconfig, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kfilemetadata, kf6-ki18n, kf6-kidletime, kf6-kio, kf6-solid, libc++, liblmdb, qt6-qtbase, qt6-qtdeclarative"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
@@ -17,6 +16,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
 	fi
 }
