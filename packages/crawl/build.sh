930c4c77acd4998035f7a1d0e8516679728e5fcb
Robert Kirkman <rkirkman@termux.dev>
2026-02-16 23:09:18 -0600

    bump(main/crawl): 0.34.0
    - Fixes https://github.com/termux/termux-packages/issues/28349

--- packages/crawl/build.sh
+++ packages/crawl/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://crawl.develz.org/
 TERMUX_PKG_DESCRIPTION="Roguelike adventure through dungeons filled with dangerous monsters"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.33.1"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/crawl/crawl/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=c9b998dc7d3247c3ac68be6ac81fa8c2e743a232a7bbd6b1aff3011478e7e3d9
+TERMUX_PKG_VERSION="0.34.0"
+TERMUX_PKG_SRCURL="https://github.com/crawl/crawl/releases/download/$TERMUX_PKG_VERSION/stone_soup-$TERMUX_PKG_VERSION.tar.xz"
+TERMUX_PKG_SHA256=b9bc4d2866534625146fa1887f2ac06547b62e4a71dc78ed8f3a7b5d7f011cd7
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, lua51, libsqlite, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -19,7 +18,7 @@ termux_step_post_get_source() {
 	sed "s|@VERSION@|${TERMUX_PKG_VERSION#*:}|g" \
 		$TERMUX_PKG_BUILDER_DIR/util-gen_ver.pl.diff \
 		| patch --silent -p1
-	pushd crawl-ref/source
+	pushd source
 	local f
 	for f in initfile.cc main.cc startup.cc syscalls.cc; do
 		sed -i 's|\(__ANDROID_\)_|\1_NO_TERMUX__|g' "${f}"
@@ -28,7 +27,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	TERMUX_PKG_SRCDIR+="/crawl-ref/source"
+	TERMUX_PKG_SRCDIR+="/source"
 	TERMUX_PKG_BUILDDIR=$TERMUX_PKG_SRCDIR
 
 	export DEFINES_L="-DHAVE_STAT"
