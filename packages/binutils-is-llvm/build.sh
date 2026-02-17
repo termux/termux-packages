620ccb205b5140f8ca38a894d6cfe09da6dbcfdc
TomIO <tom@termux.dev>
2026-02-17 16:44:34 +0100

    revbump(main/binutils-is-llvm): remove `dwp` util dropped by binutils

--- packages/binutils-is-llvm/build.sh
+++ packages/binutils-is-llvm/build.sh
@@ -4,35 +4,36 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 # The version number is different from the original one.
 TERMUX_PKG_VERSION=0.3
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_DEPENDS="lld, llvm"
 TERMUX_PKG_CONFLICTS="binutils"
 
 termux_step_make_install() {
-	ln -sf lld $TERMUX_PREFIX/bin/ld
-	local f
-	# Please do not include `as`. `llvm-as` is pretty much different from
-	# GNU as. Clang's `-fno-integrated-as` will not work as expected when
-	# `as` is a symlink to `llvm-as`. `bin/as` is provided by `binutils-bin`
-	# package which does not collide with this package.
-	for f in addr2line ar dwp nm objcopy objdump ranlib readelf size strings strip; do
-		ln -sf llvm-${f} $TERMUX_PREFIX/bin/${f}
+	ln -sf lld "$TERMUX_PREFIX/bin/ld"
+	local bin
+	# Please do not include `as`. `llvm-as` is pretty different from GNU as.
+	# Clang's `-fno-integrated-as` will not work as expected when `as` is a symlink to `llvm-as`.
+	# `bin/as` is provided by the `binutils-bin` subpackage which does not collide with this package.
+	local -a _UTILS=("addr2line" "ar" "nm" "objcopy" "objdump" "ranlib" "readelf" "size" "strings" "strip")
+	for bin in "${_UTILS[@]}"; do
+		ln -sf "llvm-${bin}" "$TERMUX_PREFIX/bin/${bin}"
 	done
-	ln -sf llvm-cxxfilt $TERMUX_PREFIX/bin/c++filt
+	ln -sf llvm-cxxfilt "$TERMUX_PREFIX/bin/c++filt"
 
 	local dir=$TERMUX_PREFIX/share/$TERMUX_PKG_NAME
-	mkdir -p $dir
-	touch $dir/.placeholder
+	mkdir -p "$dir"
+	touch "$dir/.placeholder"
 
 	# Add some arch-prefixed symlinks like binutils.
-	for b in ar ld nm objdump ranlib readelf strip; do
-		ln -sf $b $TERMUX_PREFIX/bin/$TERMUX_HOST_PLATFORM-$b
+	local -a _UTILS_WITH_HOST_PREFIX=("ar" "ld" "nm" "objdump" "ranlib" "readelf" "strip")
+	for bin in "${_UTILS_WITH_HOST_PREFIX[@]}"; do
+		ln -sf "$bin" "$TERMUX_PREFIX/bin/$TERMUX_HOST_PLATFORM-$bin"
 	done
 }
 
 termux_step_install_license() {
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME \
-		$TERMUX_PKG_BUILDER_DIR/LICENSE
+	install -Dm600 -t "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME" \
+		"$TERMUX_PKG_BUILDER_DIR/LICENSE"
 }
