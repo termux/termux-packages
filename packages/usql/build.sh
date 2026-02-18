746ffd98f89a0da3d676514d40f249a0267a5f8f
Luis Davim <ldavim@nebius.com>
2026-02-18 19:14:51 +0000

    fix: make usql work with go 1.26

--- packages/usql/build.sh
+++ packages/usql/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A universal command-line interface for SQL databases"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@flipee"
 TERMUX_PKG_VERSION="0.20.8"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xo/usql/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=71c25971a7ab8558a34d106eb60bf9d06acdfd8b5e6417062748821005aac1a7
 TERMUX_PKG_AUTO_UPDATE=true
@@ -16,6 +17,9 @@ termux_step_make() {
 		tags="$tags no_netezza no_chai"
 	fi
 
+	# TODO: remove this once the upstream package is updated to suport go 1.26
+	go mod edit -replace github.com/cockroachdb/swiss=github.com/cockroachdb/swiss@b0f6560
+	go mod tidy
 	go build \
 		-trimpath \
 		-tags="$tags" \
