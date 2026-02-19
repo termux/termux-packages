06c89e54831c6c16ecb85f39441285a900c1cf47
Robert Kirkman <rkirkman@termux.dev>
2026-02-18 23:42:53 -0600

    bump(main/jbig2enc): 0.30

--- packages/jbig2enc/build.sh
+++ packages/jbig2enc/build.sh
@@ -2,30 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/agl/jbig2enc
 TERMUX_PKG_DESCRIPTION="An encoder for JBIG2"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=ea050190466f5336c69c6a11baa1cb686677fcab
-TERMUX_PKG_VERSION=2023.01.08
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=git+https://github.com/agl/jbig2enc
-TERMUX_PKG_SHA256=cb098a85a236c930588c20138d2720636f914e98b6d9211261f78afb3eea2506
-TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_VERSION="1:0.30"
+TERMUX_PKG_SRCURL="https://github.com/agl/jbig2enc/archive/refs/tags/${TERMUX_PKG_VERSION:2}.tar.gz"
+TERMUX_PKG_SHA256=4468442f666edc2cc4d38b11cde2123071a94edc3b403ebe60eb20ea3b2cc67b
 TERMUX_PKG_DEPENDS="giflib, leptonica, libc++, libjpeg-turbo, libpng, libtiff, libwebp, python, zlib"
-
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [ "$version" != "$TERMUX_PKG_VERSION" ]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
-	fi
-
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
-	fi
-}
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
 	sh autogen.sh
