d1ad307ffea84702d0a5a2a97ea5d2824b3eaa9f
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-02-19 10:30:11 +0800

    chromium: downgrade to 144.0.7559.132

--- x11-packages/chromium/build.sh
+++ x11-packages/chromium/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.chromium.org/Home
 TERMUX_PKG_DESCRIPTION="Chromium web browser"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=145.0.7632.45
-TERMUX_PKG_SRCURL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$TERMUX_PKG_VERSION-lite.tar.xz
-TERMUX_PKG_SHA256=a7ce8bd85d36e6c01d382e71c9018b0d118553a848e32dd399aea2e437476be1
+_CHROMIUM_VERSION=144.0.7559.132
+TERMUX_PKG_VERSION="145.0.7632.75+really$_CHROMIUM_VERSION"
+TERMUX_PKG_SRCURL=https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$_CHROMIUM_VERSION-lite.tar.xz
+TERMUX_PKG_SHA256=41cc60391836575f4a40ffd576f647c0b9105219acb494e739c9ea2c66f5ddb9
 TERMUX_PKG_DEPENDS="atk, cups, dbus, fontconfig, gtk3, krb5, libc++, libevdev, libxkbcommon, libminizip, libnss, libx11, mesa, openssl, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="chromium-host-tools, libffi-static"
 # TODO: Split chromium-common and chromium-headless
@@ -14,16 +15,15 @@ TERMUX_PKG_BUILD_DEPENDS="chromium-host-tools, libffi-static"
 TERMUX_PKG_EXCLUDED_ARCHES="i686"
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=true
-TERMUX_PKG_AUTO_UPDATE=false
 
 SYSTEM_LIBRARIES="    fontconfig"
 # TERMUX_PKG_DEPENDS="fontconfig"
 
 termux_pkg_auto_update() {
-	local latest_version="$(. $TERMUX_SCRIPTDIR/x11-packages/chromium-host-tools/build.sh; echo ${TERMUX_PKG_VERSION})"
+	local latest_version="$(. $TERMUX_SCRIPTDIR/x11-packages/chromium-host-tools/build.sh; echo ${_CHROMIUM_VERSION})"
 
 	if ! termux_pkg_is_update_needed \
-		"${TERMUX_PKG_VERSION#*:}" "${latest_version}"; then
+		"${_CHROMIUM_VERSION#*:}" "${latest_version}"; then
 		echo "INFO: No update needed. Already at version '${latest_version}'."
 		return 0
 	fi
@@ -59,8 +59,8 @@ termux_pkg_auto_update() {
 
 termux_step_post_get_source() {
 	# Version guard
-	local version_tools=$(. $TERMUX_SCRIPTDIR/x11-packages/chromium-host-tools/build.sh; echo ${TERMUX_PKG_VERSION})
-	if [ "${version_tools}" != "${TERMUX_PKG_VERSION}" ]; then
+	local version_tools=$(. $TERMUX_SCRIPTDIR/x11-packages/chromium-host-tools/build.sh; echo ${_CHROMIUM_VERSION})
+	if [ "${version_tools}" != "${_CHROMIUM_VERSION}" ]; then
 		termux_error_exit "Version mismatch between chromium-host-tools and chromium."
 	fi
 
@@ -83,7 +83,7 @@ termux_step_post_get_source() {
 		$SYSTEM_LIBRARIES
 
 	# Remove the source file to keep more space
-	rm -f "$TERMUX_PKG_CACHEDIR/chromium-$TERMUX_PKG_VERSION-lite.tar.xz"
+	rm -f "$TERMUX_PKG_CACHEDIR/chromium-$_CHROMIUM_VERSION-lite.tar.xz"
 }
 
 termux_step_pre_configure() {
@@ -142,38 +142,6 @@ EOF
 	# Install nodejs
 	if [ ! -f "third_party/node/linux/node-linux-x64/bin/node" ]; then
 		./third_party/node/update_node_binaries
-	fi
-
-	# Download npm deps
-	if [ ! -d "third_party/node/node_modules" ]; then
-		local _npm_object_name=$(python -c "Var = str
-Str = str
-exec(open('$TERMUX_PKG_SRCDIR/DEPS').read())
-print(deps['src/third_party/node/node_modules']['objects'][0]['object_name'])
-")
-		local _npm_sha256sum=$(python -c "Var = str
-Str = str
-exec(open('$TERMUX_PKG_SRCDIR/DEPS').read())
-print(deps['src/third_party/node/node_modules']['objects'][0]['sha256sum'])
-")
-		local _npm_file="$TERMUX_PKG_SRCDIR/third_party/node/node_modules.tar.gz"
-		termux_download \
-			"https://commondatastorage.googleapis.com/chromium-nodejs/$_npm_object_name" \
-			"${_npm_file}" \
-			"${_npm_sha256sum}"
-		mkdir -p $TERMUX_PKG_SRCDIR/third_party/node/node_modules-tmp
-		tar -xf "$_npm_file" --strip-components=1 -C "$TERMUX_PKG_SRCDIR/third_party/node/node_modules-tmp"
-		mv "$TERMUX_PKG_SRCDIR/third_party/node/node_modules-tmp" "$TERMUX_PKG_SRCDIR/third_party/node/node_modules"
-	fi
-
-	# Sync rollup-related native deps in devtools
-	if [ ! -d "third_party/devtools-frontend/src/node_modules/@rollup/rollup-linux-x64-gnu" ]; then
-		if [ ! -f "third_party/devtools-frontend/src/third_party/rollup_libs/rollup.linux-x64-gnu.node" ]; then
-			termux_error_exit "rollup.linux-x64-gnu.node not found"
-		fi
-		pushd third_party/devtools-frontend/src
-		python3 scripts/deps/sync_rollup_libs.py
-		popd # third_party/devtools-frontend/src
 	fi
 
 	local CARGO_TARGET_NAME="${TERMUX_ARCH}-linux-android"
