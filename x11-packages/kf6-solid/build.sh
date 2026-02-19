74d23fc083334f117c0fa6825a1b06c7532dfbc3
BullyMaguire-lol <temp9040@gmail.com>
2026-02-19 10:33:23 +0530

    enhance(x11/kf6-solid): add upower

--- x11-packages/kf6-solid/build.sh
+++ x11-packages/kf6-solid/build.sh
@@ -3,13 +3,18 @@ TERMUX_PKG_DESCRIPTION='Hardware integration and detection'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.23.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/solid-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=448502ea32c8e049ebd3caf34c5c768fce7f879f25f8b5e700fd3300d317a0cc
-TERMUX_PKG_DEPENDS="libimobiledevice, libplist, qt6-qtbase, libc++, util-linux"
-# media-player-info, systemd-libs, udisks2, upower can be added to TERMUX_PKG_DEPENDS when available
+TERMUX_PKG_DEPENDS="libimobiledevice, libplist, qt6-qtbase, libc++, upower, util-linux"
+# media-player-info, systemd-libs, udisks2 can be added to TERMUX_PKG_DEPENDS when available
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qtdeclarative, qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DBUILD_DEVICE_BACKEND_upower=ON
+-DUSE_DBUS=ON
+-DUDEV_DISABLED=ON
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
