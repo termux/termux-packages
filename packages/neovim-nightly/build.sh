0ed1a38891c7e450c691481fca85aa382898ad36
Robert Kirkman <rkirkman@termux.dev>
2026-02-21 00:39:17 -0600

    fix(main/neovim-nightly): apply `TERMUX_PKG_REPOLOGY_METADATA_VERSION="${TERMUX_PKG_VERSION%%~*}"`
    - Will make Repology detect Termux's `neovim-nightly` package as up-to-date https://repology.org/project/neovim-nightly/versions
    
    %ci:no-build

--- packages/neovim-nightly/build.sh
+++ packages/neovim-nightly/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION="0.12.0~dev-2377+gd79a9dcd42"
 TERMUX_PKG_SRCURL="https://github.com/neovim/neovim/archive/${TERMUX_PKG_VERSION##*+g}.tar.gz"
 TERMUX_PKG_SHA256=b0353d3df2cd5aba3a58c679de359b37233c1d6975cc2558f33fd2f01ebcef3e
+TERMUX_PKG_REPOLOGY_METADATA_VERSION="${TERMUX_PKG_VERSION%%~*}"
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libmsgpack, libunibilium, libuv, libvterm (>= 1:0.3-0), lua51-lpeg, luajit, luv, tree-sitter, tree-sitter-parsers, utf8proc"
 TERMUX_PKG_BREAKS="neovim"
 TERMUX_PKG_CONFLICTS="neovim"
