70be63483a7130feec83d845b023431994fae199
Robert Kirkman <rkirkman@termux.dev>
2026-02-21 01:47:56 -0600

    bump(x11/eom): 1.28.1
    - Cherry-pick https://github.com/mate-desktop/eom/commit/92f1d8e710749f43d5bf80afe6955da8785bd63d to fix a girepository-related build error
    
    - Disable help to prevent `Error: cannot open XML file ../src/help/C/index.docbook`
    
    - Use `-Dgdk-pixbuf-thumbnailer=false` because `gdk-pixbuf` does not support building `gdk-pixbuf-thumbnailer` during cross-compilation, so Termux does not provide it

--- x11-packages/eom/build.sh
+++ x11-packages/eom/build.sh
@@ -2,17 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://mate-desktop.org/
 TERMUX_PKG_DESCRIPTION="Image viewer for MATE"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.28.0"
+TERMUX_PKG_VERSION="1.28.1"
 TERMUX_PKG_SRCURL="https://github.com/mate-desktop/eom/releases/download/v$TERMUX_PKG_VERSION/eom-$TERMUX_PKG_VERSION.tar.xz"
-TERMUX_PKG_SHA256=9a01cab2995a1a8c7258c865eae5f182ed4730c44672afdc3a07e423edd53abc
-# version 1.28.1 has a build failure not only in Termux, but also the same error in upstream's own CI;
-# wait for them to fix it
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="dbus-glib, gobject-introspection, gettext, librsvg, littlecms, libexif, libjpeg-turbo, mate-desktop, libpeas"
+TERMUX_PKG_SHA256=ccc169b8e240828b36965dfd84fa1478957dec2028ffeba553ae97e542e15120
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="dbus-glib, gobject-introspection, gettext, imagemagick, librsvg, littlecms, libexif, libjpeg-turbo, mate-desktop, libpeas"
 TERMUX_PKG_RECOMMENDS="webp-pixbuf-loader"
 TERMUX_PKG_BUILD_DEPENDS="autoconf-archive, glib, mate-common"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --localstatedir=$TERMUX_PREFIX/var
+-Dgdk-pixbuf-thumbnailer=false
 "
 
 termux_step_pre_configure() {
