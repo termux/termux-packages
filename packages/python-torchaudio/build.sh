6ef69b142d239b62273a943fc3032292049fd748
Robert Kirkman <rkirkman@termux.dev>
2026-02-21 04:53:54 -0600

    bump(main/python-torch{,audio,codec}): 2.10.0

--- packages/python-torchaudio/build.sh
+++ packages/python-torchaudio/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pytorch/audio
 TERMUX_PKG_DESCRIPTION="Data manipulation and transformation for audio signal processing, powered by PyTorch"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.9.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.10.0"
 TERMUX_PKG_SRCURL="https://github.com/pytorch/audio/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=590492c90552959b3df6f601eb733135064bf2d9e53c516adcf6845a4e545662
+TERMUX_PKG_SHA256=d0d0d9575025eb85150356a0b0de75b553484838006af17a62470b52d59845d1
 TERMUX_PKG_DEPENDS="libc++, python, python-pip, python-torch, python-torchcodec"
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel, setuptools"
 
