7cdb03d10626b65f7c47cf5ecdcfaacd0626a114
Robert Kirkman <rkirkman@termux.dev>
2026-02-22 21:42:00 -0600

    bump(main/c-toxcore): 0.2.22
    - Version 0.16.2 of `toxic` is actually supposed to be built with `c-toxcore` version 0.2.22 https://github.com/JFreegman/toxic/blob/07dba324aa06916c6c96ed5104240d304a662050/script/build-minimal-static-toxic.sh#L146

--- packages/c-toxcore/build.sh
+++ packages/c-toxcore/build.sh
@@ -3,14 +3,13 @@ TERMUX_PKG_DESCRIPTION="Backend library for the Tox protocol"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Match commit SHA with toxic/blob/master/script/build-minimal-static-toxic.sh
-_COMMIT=0f12f384c8cf62310b9cff6c31e94af7126b7478
-_COMMIT_DATE=20250105
-TERMUX_PKG_VERSION=0.2.20-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=git+https://github.com/TokTok/c-toxcore
-TERMUX_PKG_SHA256=26fb56c1818f9aed9e507ed2ed8f2e6e6e5d05f7a264eb8856be5b6b887c656f
-TERMUX_PKG_GIT_BRANCH=master
+# (for example, toxic 0.16.2 has the commit of c-toxcore version 0.2.22 in it)
+# https://github.com/JFreegman/toxic/blob/07dba324aa06916c6c96ed5104240d304a662050/script/build-minimal-static-toxic.sh#L146
+TERMUX_PKG_VERSION="0.2.22"
+TERMUX_PKG_SRCURL="https://github.com/TokTok/c-toxcore/releases/download/v$TERMUX_PKG_VERSION/c-toxcore-v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=276d447eb94e9d76e802cecc5ca7660c6c15128a83dfbe4353b678972aeb950a
 TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_DEPENDS="libsodium, libopus, libvpx"
 TERMUX_PKG_BREAKS="c-toxcore-dev"
 TERMUX_PKG_REPLACES="c-toxcore-dev"
@@ -21,31 +20,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DDHT_BOOTSTRAP=off
 "
 
-termux_step_get_source() {
-	rm -rf $TERMUX_PKG_SRCDIR
-	mkdir -p $TERMUX_PKG_SRCDIR
-	cd $TERMUX_PKG_SRCDIR
-	git clone --depth 1 --branch ${TERMUX_PKG_GIT_BRANCH} \
-		${TERMUX_PKG_SRCURL#git+} .
-	git fetch --unshallow
-	git checkout $_COMMIT
-	git submodule update --init --recursive --depth=1
-}
-
 termux_step_post_get_source() {
-	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
-	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
-		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
-		echo -n " different from what is expected to be; should end"
-		echo " with \"${pdate}\"."
-		return 1
-	fi
-
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
-	fi
-
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
 	local _SOVERSION=2
