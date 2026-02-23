819cf3b48c69a55eb749aa1a8b851bcc8fff38e5
Robert Kirkman <rkirkman@termux.dev>
2026-02-22 21:31:53 -0600

    bump(main/byobu): 6.14

--- packages/byobu/build.sh
+++ packages/byobu/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.byobu.org/
 TERMUX_PKG_DESCRIPTION="Byobu is a GPLv3 open source text-based window manager and terminal multiplexer"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.13"
-TERMUX_PKG_SRCURL=https://github.com/dustinkirkland/byobu/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9690c629588e8f95d16b2461950d39934faaf8005dd2a283886d4e3bd6c86df6
+TERMUX_PKG_VERSION="6.14"
+TERMUX_PKG_SRCURL="https://github.com/dustinkirkland/byobu/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=478e15a38a57678e4bd2cd55994ea1edece2d10bb6bf0a3de8f0b2dd8df35485
 TERMUX_PKG_DEPENDS="gawk, tmux"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
