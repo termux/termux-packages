f38aa2bbec7ee6148a811b21f59b3733e82676a3
Robert Kirkman <rkirkman@termux.dev>
2026-02-24 23:40:21 -0600

    bump(x11/firefox): 148.0
    - Fixes https://github.com/termux/termux-packages/issues/28611
    
    - Rebase `0024-disable-native-https-dns-resolve.patch`

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mozilla.org/firefox
 TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="147.0.4"
+TERMUX_PKG_VERSION="148.0"
 TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION}/source/firefox-${TERMUX_PKG_VERSION}.source.tar.xz"
-TERMUX_PKG_SHA256=bc0742ac62b177987d3312352194d4a088396ccf7b02e94be88f8072d82e94f7
+TERMUX_PKG_SHA256=ec93e5040a23b7dbe9ec77eb4a7ccda9820856d7851bf2f617f3673b6354cb6f
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
