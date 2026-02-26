b74a8df20a851614374632b7b6d594266d06ca55
Robert Kirkman <rkirkman@termux.dev>
2026-02-26 02:57:45 -0600

    bump(main/netcdf-c): 4.10.0
    - Fixes https://github.com/termux/termux-packages/issues/28633

--- packages/netcdf-c/build.sh
+++ packages/netcdf-c/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.unidata.ucar.edu/software/netcdf/
 TERMUX_PKG_DESCRIPTION="NetCDF is a set of software libraries and self-describing, machine-independent data formats that support the creation, access, and sharing of array-oriented scientific data"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION="4.9.3"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/Unidata/netcdf-c/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=990f46d49525d6ab5dc4249f8684c6deeaf54de6fec63a187e9fb382cc0ffdff
+TERMUX_PKG_VERSION="4.10.0"
+TERMUX_PKG_SRCURL="https://github.com/Unidata/netcdf-c/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=ce160f9c1483b32d1ba8b7633d7984510259e4e439c48a218b95a023dc02fd4c
 TERMUX_PKG_DEPENDS="libandroid-execinfo, libcurl, zlib"
 TERMUX_PKG_BREAKS="netcdf-c-dev"
 TERMUX_PKG_REPLACES="netcdf-c-dev"
@@ -18,7 +17,7 @@ TERMUX_PKG_BREAKS="gdal (<= 3.10.2), monetdb (<= 11.51.7)"
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION="23-1"
+	local _SOVERSION="24-2"
 
 	local e=$(sed -En 's/.*\s+netCDF_SO_VERSION="?([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
 				configure.ac)
