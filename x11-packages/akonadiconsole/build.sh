ef232c10fe69268451bee2a4b9308e71260aa00e
BullyMaguire-lol <temp9040@gmail.com>
2026-02-27 21:25:15 +0530

    addpkg(x11/akonadiconsole): 25.12.3

---
+++ x11-packages/akonadiconsole/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/pim/akonadiconsole"
+TERMUX_PKG_DESCRIPTION="Akonadi management and debugging console"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="25.12.3"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/akonadiconsole-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="1effdb62375b66435de8b75cfe1429eeda5facc546d9b192e8a8affeea216bba"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="akonadi, akonadi-contacts, akonadi-search, calendarsupport, kf6-kcalendarcore, kf6-kcolorscheme, kf6-kcompletion, kf6-kconfig, kf6-kconfigwidgets, kf6-kcontacts, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-ki18n, kf6-kitemmodels, kf6-ktextwidgets, kf6-kwidgetsaddons, kf6-kxmlgui, kmime, libc++, libkdepim, libxapian, messagelib, qgpgme, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
+TERMUX_PKG_EXCLUDED_ARCHES="i686"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
