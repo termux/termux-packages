8df6ef9562c7f72d7fe94a0a63f59f0553320507
BullyMaguire-lol <temp9040@gmail.com>
2026-02-27 21:24:18 +0530

    addpkg(x11/kaddressbook): 26.04.3

---
+++ x11-packages/kaddressbook/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/pim/kaddressbook"
+TERMUX_PKG_DESCRIPTION="KDE contact manager"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="26.04.3"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/kaddressbook-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="300e313205d6eb83ac9ba134f9d333741e96ed1518342cd63d06a0234ed3d9dc"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="akonadi, akonadi-contacts, akonadi-search, grantleetheme, kdepim-runtime, kf6-kcmutils, kf6-kcolorscheme, kf6-kconfig, kf6-kconfigwidgets, kf6-kcontacts, kf6-kcoreaddons, kf6-kcrash, kf6-ki18n, kf6-kiconthemes, kf6-kitemmodels, kf6-kparts, kf6-kuserfeedback, kf6-kwidgetsaddons, kf6-kxmlgui, kldap, kontactinterface, libc++, libkdepim, pimcommon, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
+# akonadi, akonadi-contacts, akonadi-search, kdepim-runtime, pimcommon depends on qt6-qtwebengine
+# qt6-qtwebengine is not supported on the i686 architecture
+TERMUX_PKG_EXCLUDED_ARCHES="i686"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
