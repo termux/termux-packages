bbb78e94cfc4df16872b5b5896a6e2fbcdffd358
BullyMaguire-lol <temp9040@gmail.com>
2026-02-28 17:43:14 +0530

    addpkg(x11/kmail-account-wizard): 25.12.3

---
+++ x11-packages/kmail-account-wizard/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/pim/kmail-account-wizard"
+TERMUX_PKG_DESCRIPTION="KMail account wizard"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="25.12.3"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/kmail-account-wizard-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="0a41268e6b153700c4c815da1baa40637efa61943f4e387f66cf5599d8e8d502"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="akonadi, kf6-kconfig, kf6-kconfigwidgets, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-ki18n, kf6-kiconthemes, kf6-kirigami, kidentitymanagement, kirigami-addons, kmailtransport, kmime, libc++, qt6-qtbase, qt6-qtdeclarative"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, kf6-kdoctools"
+TERMUX_PKG_EXCLUDED_ARCHES="i686"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
