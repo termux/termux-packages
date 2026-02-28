93111eec9aab4e866bd9cbb7da1effdeb35e0b22
Biswapriyo Nath <nathbappai@gmail.com>
2026-02-28 15:29:27 +0000

    bump(main/byobu): 6.15
    Also, enable auto update.

--- packages/byobu/build.sh
+++ packages/byobu/build.sh
@@ -2,9 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://www.byobu.org/
 TERMUX_PKG_DESCRIPTION="Byobu is a GPLv3 open source text-based window manager and terminal multiplexer"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.14"
+TERMUX_PKG_VERSION="6.15"
 TERMUX_PKG_SRCURL="https://github.com/dustinkirkland/byobu/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=478e15a38a57678e4bd2cd55994ea1edece2d10bb6bf0a3de8f0b2dd8df35485
+TERMUX_PKG_SHA256=2d670627aeb068447654b78fd83901ea4b0d08df59f6fa0721d61cb1fc2f56ae
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="gawk, tmux"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
