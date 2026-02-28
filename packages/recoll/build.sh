08685613e55596d17849a53d1fb610fce6bde9c4
Robert Kirkman <rkirkman@termux.dev>
2026-02-27 20:21:18 -0600

    bump(main/recoll): 1.43.13
    - Fixes https://github.com/termux/termux-packages/issues/28648
    
    - Replace `utils-pathut.cpp.patch` with `-Dext4-birthtime=false` after upstream changes that made the `ext4-birthtime` Meson option control whether the `statx` syscall is used
    
    - Now depends on `jsoncpp`

--- packages/recoll/build.sh
+++ packages/recoll/build.sh
@@ -2,13 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://www.recoll.org/
 TERMUX_PKG_DESCRIPTION="Full-text search for your desktop"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.43.12"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.43.13"
 TERMUX_PKG_SRCURL="https://www.recoll.org/recoll-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=3e2a538000bab2013b455303d52fbe604eebcef3bb8f60ff968d2cbc92a853e0
+TERMUX_PKG_SHA256=140bf1e4fc51299f60dad580dffd64733e1d06fb14c6f752e2a34d4d70540c19
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="aspell, file, libc++, libiconv, libxapian, libxml2, libxslt, zlib"
+TERMUX_PKG_DEPENDS="aspell, file, jsoncpp,  libc++, libiconv, libxapian, libxml2, libxslt, zlib"
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel"
+# -Dext4-birthtime=false disables the use of the statx syscall
+# it is also set to false by default at time of writing,
+# but set it explicitly because Termux previously
+# had a patch forcibly disabling the use of the statx syscall in recoll
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dpython-chm=false
 -Dpython-aspell=false
@@ -16,6 +19,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dx11mon=false
 -Dqtgui=false
 -Dsystemd=false
+-Dext4-birthtime=false
 "
 
 termux_step_pre_configure() {
