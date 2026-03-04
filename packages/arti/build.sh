685d0bed3bdad2647fd8b4aa71923289838295e0
Yaksh Bariya <thunder-coding@termux.dev>
2026-03-05 00:39:59 +0530

    bump(main/arti): 2.1.0

--- packages/arti/build.sh
+++ packages/arti/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://tpo.pages.torproject.net/core/arti/
 TERMUX_PKG_DESCRIPTION="Arti is a work-in-progress project to write a full-featured Tor implementation in Rust."
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION="1.8.0"
+TERMUX_PKG_VERSION="2.1.0"
 TERMUX_PKG_SRCURL="https://gitlab.torproject.org/tpo/core/arti/-/archive/arti-v${TERMUX_PKG_VERSION}/arti-arti-v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=92ea0731bea19d91f2de9ea0f408bd578d11ea2dd270949ba0abc2d0aeaabb60
+TERMUX_PKG_SHA256=9187d6a7b789de4e6b09445c8f3ba63f3ec24bc080a36468b923cda38070208d
 TERMUX_PKG_DEPENDS="liblzma, libsqlite, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
