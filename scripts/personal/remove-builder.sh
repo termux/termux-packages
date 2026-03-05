c7d0c76cf84273894b892d0dc4fb095803041a0d
Yaksh Bariya <thunder-coding@termux.dev>
2026-03-05 19:44:27 +0530

    add more helper scripts

---
+++ scripts/personal/remove-builder.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+set -eou pipefail
+TERMUX_SCRIPTDIR=$(cd "$(realpath "$(dirname "$0")")"; cd ../..; pwd)
+
+if [ -n "${TERMUX_DOCKER_USE_SUDO-}" ]; then
+	SUDO="sudo"
+else
+	SUDO=""
+fi
+
+: ${CONTAINER_NAME:=termux-package-builder}
+
+
+_remove_container() {
+	$SUDO docker container rm -f "$1"
+	rm -rf "output/_builder/$1"
+}
+
+if (( "$#" > 1 )); then
+	for CONTAINER_NAME in "$@"; do
+		_remove_container "$CONTAINER_NAME"
+	done
+else
+	_remove_container "$CONTAINER_NAME"
+fi
