e888f841ed46bce96cfd3afb0381839f5269c722
Robert Kirkman <rkirkman@termux.dev>
2026-03-04 23:14:17 -0600

    addpkg(main/python-psutil): 7.2.2
    - Fixes https://github.com/termux/termux-packages/issues/28770
    
    - Package `python-psutil` for `pkg install` because it is now necessary to patch it to make it installable on Android after the update to Python 3.13

---
+++ packages/python-psutil/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/giampaolo/psutil
+TERMUX_PKG_DESCRIPTION="Cross-platform process and system utilities module for Python"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="7.2.2"
+TERMUX_PKG_SRCURL="https://github.com/giampaolo/psutil/archive/refs/tags/release-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=38f406bf21acc67e45f414b7980463b2e6e6270ba3616ffd41995d997078cbe6
+TERMUX_PKG_DEPENDS="python, python-pip"
+TERMUX_PKG_SETUP_PYTHON=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
--- x11-packages/mate-tweak/build.sh
+++ x11-packages/mate-tweak/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="Tweak tool for the MATE Desktop"
 TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="22.10.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/ubuntu-mate/mate-tweak/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=c33c092b0151b50d8a5706825f1bcef57f1738f8f5cf22af49c11f45bc14e84a
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_PYTHON_RUNTIME_DEPS="distro, psutil, setproctitle"
-TERMUX_PKG_DEPENDS="dconf, gsettings-desktop-schemas, ldd, libnotify, libmatekbd, mate-applets, mate-panel, pygobject, python, python-pip"
+TERMUX_PKG_PYTHON_RUNTIME_DEPS="distro, setproctitle"
+TERMUX_PKG_DEPENDS="dconf, gsettings-desktop-schemas, ldd, libnotify, libmatekbd, mate-applets, mate-panel, pygobject, python, python-pip, python-psutil"
 TERMUX_PKG_SUGGESTS="mate-applet-brisk-menu, plank-reloaded, tilda, mate-applet-appmenu"
-TERMUX_PKG_BUILD_DEPENDS="rsync"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="
@@ -20,6 +20,14 @@ share/polkit-1
 termux_step_post_make_install() {
 	# this software installs itself in a really messed-up directory structure.
 	# The easiest way to explain what is wrong is to just show commands fixing it after installation:
-	rsync -av "$TERMUX_PREFIX/local/bin/" "$TERMUX_PREFIX/bin"
-	rsync -av "$TERMUX_PREFIX/local/lib/python$TERMUX_PYTHON_VERSION/dist-packages/usr/" "$TERMUX_PREFIX"
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "$TERMUX_PREFIX/local/bin/marco-compton"
+	install -Dm755 -t "$TERMUX_PREFIX/bin" "$TERMUX_PREFIX/local/bin/marco-picom"
+	local prog
+	for prog in marco-glx marco-no-composite marco-xr_glx_hybrid marco-xrender mate-tweak; do
+		install -Dm755 -t "$TERMUX_PREFIX/bin" "$TERMUX_PREFIX/local/bin/$prog"
+		install -Dm644 -t "$TERMUX_PREFIX/share/applications" "data/$prog.desktop"
+		install -Dm644 -t "$TERMUX_PREFIX/share/man/man1" "data/$prog.1"
+	done
+	install -Dm644 -t "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME" data/mate-tweak.ui
+	install -Dm644 -t "$TERMUX_PREFIX/lib/$TERMUX_PKG_NAME" util/mate-tweak-helper
 }
