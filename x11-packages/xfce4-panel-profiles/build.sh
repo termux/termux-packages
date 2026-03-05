7a45446f5ecbe142a63f68962c50959ff4878538
Robert Kirkman <rkirkman@termux.dev>
2026-03-04 23:14:17 -0600

    addpkg(main/python-psutil): 7.2.2
    - Fixes https://github.com/termux/termux-packages/issues/28770
    
    - Package `python-psutil` for `pkg install` because it is now necessary to patch it to make it installable on Android after the update to Python 3.13

--- x11-packages/xfce4-panel-profiles/build.sh
+++ x11-packages/xfce4-panel-profiles/build.sh
@@ -3,12 +3,11 @@ TERMUX_PKG_DESCRIPTION="A simple application to manage Xfce panel layouts."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.1.1"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION%.*}/xfce4-panel-profiles-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SRCURL="https://archive.xfce.org/src/apps/xfce4-panel-profiles/${TERMUX_PKG_VERSION%.*}/xfce4-panel-profiles-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=0126373a03778bb4894afa151de28d36bfc563ddab96d3bd7c63962350d34ba2
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="gtk3, libxfce4ui, libxfce4util, pygobject, python, python-pip, xfce4-panel"
-TERMUX_PKG_PYTHON_TARGET_DEPS="psutil"
+TERMUX_PKG_DEPENDS="gtk3, libxfce4ui, libxfce4util, pygobject, python, python-pip, python-psutil, xfce4-panel"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
