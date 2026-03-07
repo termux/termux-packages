c3011e02e526f55ceb20ac18200dee0837f8cbe7
pranav <pranavsuresh107@gmail.com>
2026-03-07 15:42:01 +0530

    bump(main/neovim-nightly): 0.12.0-dev-2496+gf1c82be1a1

--- packages/neovim-nightly/build.sh
+++ packages/neovim-nightly/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Ambitious Vim-fork focused on extensibility and agility 
 TERMUX_PKG_LICENSE="Apache-2.0, VIM License"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
-TERMUX_PKG_VERSION="0.12.0~dev-2435+g18c5f06c9f"
+TERMUX_PKG_VERSION="0.12.0~dev-2496+gf1c82be1a1"
 TERMUX_PKG_SRCURL="https://github.com/neovim/neovim/archive/${TERMUX_PKG_VERSION##*+g}.tar.gz"
-TERMUX_PKG_SHA256=3518260bbe8e755204b8d6fc25d1238be940b7138792fa3934022450c8a959cd
+TERMUX_PKG_SHA256=e8c27d7504fc1fd9c0086b0cc80fb933f25114e79a43324688dde7aa95aa833a
 TERMUX_PKG_REPOLOGY_METADATA_VERSION="${TERMUX_PKG_VERSION%%~*}"
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libmsgpack, libunibilium, libuv, libvterm (>= 1:0.3-0), lua51-lpeg, luajit, luv, tree-sitter, tree-sitter-parsers, utf8proc"
 TERMUX_PKG_BREAKS="neovim"
