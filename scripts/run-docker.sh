3dce39f5e88fa04b9457d322de26e4b9425f047f
Yaksh Bariya <thunder-coding@termux.dev>
2026-03-09 04:16:07 +0530

    fix: setting /proc/sys/kernel/pid_max for proot
    This was broken around the time when overlayfs changes and freeing
    additional disk space was merged along with Python 3.13, so took some
    time to figure out that it was indeed the kernel upgrade on GitHub
    Action runners that broke it, and not any of the changes which we did
    
    Since Linux kernel >= 6.14, the pid_max value is now pid namespaced.
    This patch upstream specifically mentioned about the need for this patch
    in bionic libc used in Android which requires pid numbers less than
    65536. Although this patch is merged only in upstream on kernel >= 6.14,
    it is possible that distros may decide to backport it to older kernel
    releases. So first, we try to change the pid_max value for the pid
    namespace of the docker container, if we succeed in doing so, we don't
    change the pid_max for the host.
    
    If we aren't able to change the pid_max for the pid namespace of the
    container, we just change the pid_max for the host as this means that
    the kernel doesn't have pid_max value pid namespaced. So we change the
    pid_max for the host, and check if it has changed for the container as
    well. It should be changed for the container as well.
    
    For changing the pid_max for the container, we run another privileged
    container in the pid namespace of the builder's container, and change
    the pid_max value there. We can't do this inside unprivileged container
    as Docker will be mounting /proc as ro (read-only), with privileged
    container, /proc is mounted as rw (read-write), which is needed to
    modify the pid_max value.
    
    Another solution would have been to use unshare command from util-linux
    to change launch proot inside another namespace inside the container.
    But that'd require more permissions to be granted via seccomp and
    apparmor profile. Also it'd require the container to be run in
    privileged mode for /proc to be mounted as rw. If the container is not
    run in privileged mode, /proc is mounted as ro, which prevents the
    unshare command from mounting procfs inside the unshare(d) namespace
    with rw. dmesg reveals the following log:
    
    VFS: Mount too revealing
    
    Looking at the kernel sources, this comes from fs/namespace.c:3774:
    
    mount_too_revealing(sb, &mnt_flags)
    
    mount_too_revealing() further calls mnt_already_visible(), and refuses
    to mount if one of the filesystems the unshared instance tries to mount
    is with more permissions than what the namespace already has. Thus
    unprivileged containers can't be used even if the required changes are
    made to allow the mounts in the restricted apparmor profile. AppArmor
    can't simply allow it as it's being rejected by the kernel itself.
    
    'unshare --pid --mount-proc --fork' thus can't work inside the container
    thus we have to change the pid_max of the builder container's namespace
    only
    
    References:
    
    Upstream kernel patch for pid namespacing of pid_max:
    https://lore.kernel.org/lkml/20250118-kernel-pid-ae69412addff@brauner/
    
    unshare command from util-linux:
    https://man7.org/linux/man-pages/man2/unshare.2.html
    
    Linux kernel namespaces documentation for Linux sysadmins:
    https://docs.kernel.org/admin-guide/namespaces/index.html
    
    Linux kernel manpage for namespaces:
    https://man7.org/linux/man-pages/man7/namespaces.7.html
    
    Linux kernel manpage for PID namespaces:
    https://man7.org/linux/man-pages/man7/pid_namespaces.7.html

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e -u
+set -eou pipefail
 
 TERMUX_SCRIPTDIR=$(cd "$(realpath "$(dirname "$0")")"; cd ..; pwd)
 : ${TERMUX_BUILDER_IMAGE_NAME:=ghcr.io/termux/package-builder}
@@ -171,6 +171,24 @@ $SUDO docker start $CONTAINER_NAME >/dev/null 2>&1 || {
 			$SUDO docker exec $DOCKER_TTY $TERMUX_DOCKER_EXEC_EXTRA_ARGS $CONTAINER_NAME sudo chown -R $(id -u):$(id -g) /data
 			$SUDO docker exec $DOCKER_TTY $TERMUX_DOCKER_EXEC_EXTRA_ARGS $CONTAINER_NAME sudo usermod -u $(id -u) builder
 			$SUDO docker exec $DOCKER_TTY $TERMUX_DOCKER_EXEC_EXTRA_ARGS $CONTAINER_NAME sudo groupmod -g $(id -g) builder
+		fi
+		echo "Changing /proc/sys/kernel/pid_max to 65535 for packages that need to run native executables using proot (for 32-bit architectures)"
+		if [[ "$($SUDO docker exec $CONTAINER_NAME cat /proc/sys/kernel/pid_max)" -le 65535 ]]; then
+			echo "No need to change /proc/sys/kernel/pid_max, current value is $($SUDO docker exec $DOCKER_TTY $CONTAINER_NAME cat /proc/sys/kernel/pid_max)"
+		else
+			# On kernel versions >= 6.14, the pid_max value is pid namespaced, so we need to set it in the container namespace instead of host.
+			# But some distributions may backport the pid namespacing to older kernels, so we check whether it's effective by checking the value in the container after setting it.
+			$SUDO docker run --privileged --pid="container:$CONTAINER_NAME" --rm "$TERMUX_BUILDER_IMAGE_NAME" sh -c "echo 65535 | sudo tee /proc/sys/kernel/pid_max > /dev/null" || :
+			if [[ "$($SUDO docker exec $CONTAINER_NAME cat /proc/sys/kernel/pid_max)" -eq 65535 ]]; then
+				echo "Successfully changed /proc/sys/kernel/pid_max for container namespace"
+			else
+				echo "Failed to change /proc/sys/kernel/pid_max for container, failing back to setting it on host..."
+				if ( echo 65535 | sudo tee /proc/sys/kernel/pid_max >/dev/null ); then
+					echo "Successfully changed /proc/sys/kernel/pid_max on host, but it may affect other processes on the host system"
+				else
+					echo "Failed to change /proc/sys/kernel/pid_max on host as well, some packages that need to run native executables using proot (for 32-bit architectures) may not work properly"
+				fi
+			fi
 		fi
 	fi
 }
