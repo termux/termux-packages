faee95a6dfacefae28974442c2ffd33d81c0098f
Yaksh Bariya <thunder-coding@termux.dev>
2026-03-08 17:39:34 +0530

    bump(main/command-not-found): 3.4.1

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=3.2
-TERMUX_PKG_REVISION=20
-TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fb9fd13995a0b4ce3e72c4a679c25cb525d973f6d3eb480f5bb68807e26bd3b9
+TERMUX_PKG_VERSION=3.4.1
+TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=8dfec3aeb340d2359bf8c0b220497128f4b17bcb3967e4fd64f7a46773b40a3e
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
