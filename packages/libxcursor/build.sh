61e6a0135a9ba0d7fc1851671936abd5b40008a1
Fredrik Fornwall <fredrik@fornwall.net>
2026-03-10 04:40:59 +0100

    revbump(main/bgrep,gatling,ladspa-sdk,libxcursor,simulavr)
    Rebuild some packages that have not been rebuilt with NDK r28 or r29, so
    that they receive support for 16 kb page sizes.

--- packages/libxcursor/build.sh
+++ packages/libxcursor/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_DESCRIPTION="X cursor management library"
 TERMUX_PKG_LICENSE="HPND"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.2.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXcursor-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=fde9402dd4cfe79da71e2d96bb980afc5e6ff4f8a7d74c159e1966afb2b2c2c0
 TERMUX_PKG_AUTO_UPDATE=true
