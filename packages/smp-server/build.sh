2ae6baf70e61b95e59296fcd1b382fc3fa20cd4e
Izumi Sena Sora (UnOrdinary) <c588968a-3f54-48af-baa1-41a7a625bd4e@8shield.net>
2026-03-10 21:20:19 +0600

    addpkg(main/smp-server): 6.4.4

---
+++ packages/smp-server/build.sh
@@ -0,0 +1,25 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/simplex-chat/simplexmq
+TERMUX_PKG_DESCRIPTION="SimpleX Messaging Protocol"
+TERMUX_PKG_LICENSE="AGPL-3.0"
+TERMUX_PKG_MAINTAINER="Izumi Sena Sora <info@unordinary.eu.org>"
+TERMUX_PKG_VERSION="6.4.4"
+TERMUX_PKG_SRCURL="https://github.com/simplex-chat/simplexmq/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=b3643ade5a4dd2dbf78f19ce662c23ad8083397dabcdfbfe277d97ef30a340fb
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="cabal-install, ghc"
+
+termux_step_make() {
+	export BOOTSTRAP_HASKELL_GHC_VERSION=9.6.3
+	export BOOTSTRAP_HASKELL_CABAL_VERSION=3.10.3.0
+	curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 sh
+	ghcup set ghc "${BOOTSTRAP_HASKELL_GHC_VERSION}"
+	ghcup set cabal "${BOOTSTRAP_HASKELL_CABAL_VERSION}"
+	source ~/.ghcup/env
+	cabal update
+	cabal build exe:"${TERMUX_PKG_NAME}"
+}
+
+termux_step_make_install() {
+	install -Dm700 "${TERMUX_PKG_NAME}" "${TERMUX_PREFIX}/bin"
+}
