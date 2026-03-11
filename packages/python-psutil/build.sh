aa932b4d8da8d92ee350389832627a61f551d491
Robert Kirkman <rkirkman@termux.dev>
2026-03-11 07:07:36 -0500

    fix(main/python{,-psutil}): Implement the fake `/storage/emulated/0` mountpoint for the Termux derivative of the XDG Trash specification in Python packages
    - https://github.com/termux/termux-packages/pull/28186 but for Python ecosystem instead of Rust ecosystem
    
    In some Termux packages, a fake mountpoint at `/storage/emulated/0`
    is mocked because it successfully causes Trashing files from `/storage/emulated/0`
    and subfolders to go to a new or existing `.Trash-$(id -u)` folder in `/storage/emulated/0` instead of
    `/storage/emulated` being detected as the real mountpoint (it is) and causing
    permission denied errors because Termux only has permission for `/storage/emulated/0`,
    not `/storage/emulated`.
    Doing that also reportedly allows Termux to detect and manage `.Trash-$(id -u)` folders
    created in `/storage/emulated/0` by desktop PCs when the Android
    device was plugged into them and files were then Trashed from the PC file browser,
    because there, that location does technically use a real mountpoint.
    
    While such a behavior of special-casing `/storage/emulated/0` when it's not
    actually a real mountpoint is not part of the XDG Trash specification,
    it has been reported that the most natural expected behavior for users trying to
    use XDG Trash tools in Termux is for them to treat `/storage/emulated/0` as though
    it were a mountpoint that should be calculated as a valid directory for `.Trash-$(id -u)` folders, so this is deviating from the XDG Trash specification in the same way as the Rust packages currently are, meaning that this will allow Python to be able to find and manage the same Trashed files that were Trashed from the Rust packages like `yazi`, and vice versa.
    
    - Fixes the error `Permission denied: '/proc/filesystems'` in the command `python -c "import psutil; print(psutil.disk_partitions())"`
    
    - Fixes this series of commands:
    
    ```
    pip install trash-cli
    termux-setup-storage
    cd /storage/emulated/0
    touch tempfile
    trash-put tempfile
    ```

--- packages/python-psutil/build.sh
+++ packages/python-psutil/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Cross-platform process and system utilities module for P
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="7.2.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/giampaolo/psutil/archive/refs/tags/release-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=38f406bf21acc67e45f414b7980463b2e6e6270ba3616ffd41995d997078cbe6
 TERMUX_PKG_DEPENDS="python, python-pip"
@@ -10,3 +11,47 @@ TERMUX_PKG_SETUP_PYTHON=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
+
+termux_step_post_make_install() {
+	# extracted from 'sudo cat /proc/filesystems'
+	# on Samsung Galaxy A70 SM-A705FN with Android 13
+	# makes the command
+	# 'python -c "import psutil; print(psutil.disk_partitions())"'
+	# work successfully without root on Termux.
+	mkdir -p "$TERMUX_PREFIX/etc/psutil"
+	cat > "$TERMUX_PREFIX/etc/psutil/filesystems" << EOF
+nodev	sysfs
+nodev	rootfs
+nodev	ramfs
+nodev	bdev
+nodev	proc
+nodev	cpuset
+nodev	cgroup
+nodev	cgroup2
+nodev	tmpfs
+nodev	configfs
+nodev	debugfs
+nodev	tracefs
+nodev	sockfs
+nodev	dax
+nodev	bpf
+nodev	pipefs
+nodev	devpts
+	ext3
+	ext2
+	ext4
+	vfat
+	msdos
+	sdfat
+	exfat
+nodev	ecryptfs
+nodev	sdcardfs
+	fuseblk
+nodev	fuse
+nodev	fusectl
+	f2fs
+nodev	pstore
+nodev	selinuxfs
+nodev	functionfs
+EOF
+}
