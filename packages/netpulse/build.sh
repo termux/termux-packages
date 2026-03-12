1f99877992c4df179144498f1f2ffd35767dce97
noahdev-sub <emmreycool@gmail.com>
2026-03-12 15:16:44 +0800

    Add netpulse package

---
+++ packages/netpulse/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/username/netpulse
+TERMUX_PKG_DESCRIPTION="Netpulse simulation terminal tool"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_SRCURL=https://github.com/username/netpulse/archive/refs/tags/v1.0.tar.gz
+TERMUX_PKG_SHA256=isi_hash_file
+TERMUX_PKG_BUILD_IN_SRC=true
