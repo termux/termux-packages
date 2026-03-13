61d4187229ee91812df4f96e258aab804e009b92
Biswapriyo Nath <nathbappai@gmail.com>
2026-03-13 15:53:08 +0000

    bump(main/snapcast-server): 0.35.0
    Rebase a patch file and update project repository URL.

--- packages/snapcast-server/build.sh
+++ packages/snapcast-server/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/badaix/snapcast
+TERMUX_PKG_HOMEPAGE=https://github.com/snapcast/snapcast
 TERMUX_PKG_DESCRIPTION="A multiroom client-server audio player (server)"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.34.0"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL="https://github.com/badaix/snapcast/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=a2918ea4d1f9b2df9c4247fd71bd452ea03a5d20ac44f60a736df90488858944
+TERMUX_PKG_VERSION="0.35.0"
+TERMUX_PKG_SRCURL="https://github.com/snapcast/snapcast/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=cb75a71479bf52910bf5f47ae8120ec41c89459b0d77d7cd560e674e437ef050
 TERMUX_PKG_DEPENDS="libc++, libexpat, libflac, libogg, libopus, libsoxr, libvorbis, openssl"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
