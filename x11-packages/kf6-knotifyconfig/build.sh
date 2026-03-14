47f49eb693d1d2ef8e3c1d6b1fa438cfb0d36646
Robert Kirkman <rkirkman@termux.dev>
2026-03-14 12:25:53 -0500

    bump(x11/kf6): 6.24.0
    - Fixes https://github.com/termux/termux-packages/issues/28919
    - Fixes https://github.com/termux/termux-packages/issues/28922
    - Fixes https://github.com/termux/termux-packages/issues/28923
    - Fixes https://github.com/termux/termux-packages/issues/28925
    - Fixes https://github.com/termux/termux-packages/issues/28926
    - Fixes https://github.com/termux/termux-packages/issues/28928
    - Fixes https://github.com/termux/termux-packages/issues/28929
    - Fixes https://github.com/termux/termux-packages/issues/28930

--- x11-packages/kf6-knotifyconfig/build.sh
+++ x11-packages/kf6-knotifyconfig/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/knotifyconfig'
 TERMUX_PKG_DESCRIPTION='Configuration system for KNotify'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.23.0"
+TERMUX_PKG_VERSION="6.24.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/knotifyconfig-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=9d199d4e439797ce5734039379dd6b340583362d46e246b76620364b977aab9e
+TERMUX_PKG_SHA256=46ea907ebc6a538888fb32434ba8cde3e8ab4423df11ded693bcbe0b6e1d83b6
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-ki18n (>= ${TERMUX_PKG_VERSION%.*}), kf6-kio (>= ${TERMUX_PKG_VERSION%.*}), libc++, libcanberra, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
