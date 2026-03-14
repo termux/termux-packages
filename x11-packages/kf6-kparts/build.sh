96c304f4cba2099cfc1dde5663499263ef1ea0c9
Robert Kirkman <rkirkman@termux.dev>
2026-03-14 12:25:53 -0500

    bump(x11/kf6): 6.24.0
    - Fixes https://github.com/termux/termux-packages/issues/28919
    - Fixes https://github.com/termux/termux-packages/issues/28922
    - Fixes https://github.com/termux/termux-packages/issues/28923
    - Fixes https://github.com/termux/termux-packages/issues/28925
    - Fixes https://github.com/termux/termux-packages/issues/28926
    - Fixes https://github.com/termux/termux-packages/issues/28928
    - Fixes https://github.com/termux/termux-packages/issues/28929
    - Fixes https://github.com/termux/termux-packages/issues/28930

--- x11-packages/kf6-kparts/build.sh
+++ x11-packages/kf6-kparts/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/kparts'
 TERMUX_PKG_DESCRIPTION='Document centric plugin system'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.23.0"
+TERMUX_PKG_VERSION="6.24.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kparts-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=799e14c2b42f9f73f6dfb35d1faaed632f144171f090aba2f74d04605b9bcc12
+TERMUX_PKG_SHA256=b4a0b72167e0f77974aafe93305c18dcc99c0e66c1f2fd56a3e44d78e72f54e7
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-ki18n (>= ${TERMUX_PKG_VERSION%.*}), kf6-kio (>= ${TERMUX_PKG_VERSION%.*}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-kxmlgui (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
