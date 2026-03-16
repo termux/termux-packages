1a94b01a2e36df9952f5e9fe3cc81415bc56f589
Fredrik Fornwall <fredrik@fornwall.net>
2026-03-17 00:09:53 +0100

    rmpkg(main/libsoup): Remove unmaintained and unused package
    Remove libsoup as it's unmaintained and contains known vulnerabilities.
    
    The declared dependency from `inkscape` was outdated.
    
    Note that the `libsoup3` package remains as a newer version.

--- x11-packages/libmateweather/build.sh
+++ x11-packages/libmateweather/build.sh
@@ -3,10 +3,11 @@ TERMUX_PKG_DESCRIPTION="libmateweather is a libgnomeweather fork."
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.28.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/libmateweather/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=d81e65e2bddd22edffe42a2c8748321d6f7ad2da1bce2f9c275d88c567ec29eb
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libsoup, libxml2, pango, zlib"
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libcairo, libsoup3, libxml2, pango, zlib"
 
 termux_step_pre_configure() {
 	autoreconf -fiv
