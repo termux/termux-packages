fb404041f4a40746d7a0fe82abddb28f8108f788
Biswapriyo Nath <nathbappai@gmail.com>
2026-03-19 05:32:24 +0000

    bump(x11/fcitx5): 5.1.19
    * Add yoga library in dependency.
    * Remove duplicate TERMUX_ variables.

--- x11-packages/fcitx5/build.sh
+++ x11-packages/fcitx5/build.sh
@@ -2,14 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://fcitx-im.org/
 TERMUX_PKG_DESCRIPTION="A generic input method framework"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.1.17"
+TERMUX_PKG_VERSION="5.1.19"
 TERMUX_PKG_SRCURL=https://github.com/fcitx/fcitx5/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=84a927fa5f3a3c713c9388a126a2e9b516f6ca7e6402b140cd82ff6614e61eaa
+TERMUX_PKG_SHA256=a6e4d99a82298845df9f8dc5036c1aaed258c2d3f5bd215b8f91c75410167ff0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_DEPENDS="dbus, enchant, fcitx5-data, fmt, gdk-pixbuf, glib, iso-codes, libandroid-execinfo, libc++, libcairo, libevent, libexpat, libuuid, libuv, libxcb, libxkbcommon, libxkbfile, pango, xcb-imdkit, xcb-util, xcb-util-keysyms, xcb-util-wm, zlib"
+TERMUX_PKG_DEPENDS="dbus, enchant, fcitx5-data, fmt, gdk-pixbuf, glib, iso-codes, libandroid-execinfo, libc++, libcairo, libevent, libexpat, libuuid, libuv, libxcb, libxkbcommon, libxkbfile, pango, xcb-imdkit, xcb-util, xcb-util-keysyms, xcb-util-wm, yoga, zlib"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, nlohmann-json"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DDL_INCLUDE_DIR=$TERMUX_PREFIX/include
@@ -17,6 +15,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DPTHREAD_INCLUDE_DIR=$TERMUX_PREFIX/include
 -DENABLE_TEST=OFF
 -DENABLE_WAYLAND=OFF
+-DUSE_SYSTEM_YOGA=ON
 "
 
 termux_step_pre_configure() {
