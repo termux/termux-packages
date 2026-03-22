25b92d014685ed811051d2749b51e1389f5d11e5
TomIO <tom@termux.dev>
2026-03-22 14:17:27 +0100

    bump(main/lesspipe): 2.23

--- packages/lesspipe/build.sh
+++ packages/lesspipe/build.sh
@@ -1,20 +1,22 @@
-TERMUX_PKG_HOMEPAGE=http://www-zeuthen.desy.de/~friebel/unix/lesspipe.html
+TERMUX_PKG_HOMEPAGE=https://lesspipe.org/
 TERMUX_PKG_DESCRIPTION="An input filter for the pager less"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
-TERMUX_PKG_VERSION="2.22"
+TERMUX_PKG_VERSION="2.23"
 TERMUX_PKG_SRCURL=https://github.com/wofr06/lesspipe/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=05220b4b4f1c6c56d3b4acf6998d79768dccd22c379639a6cf3589fbbd54ba1d
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="less, file"
+TERMUX_PKG_SHA256=89b255a20a2f9a9b0145a297006487eec1a08ca68c073d21e6e5b9179ec86d16
+TERMUX_PKG_DEPENDS="file, less"
 TERMUX_PKG_BUILD_DEPENDS="bash-completion"
-TERMUX_PKG_SUGGESTS="imagemagick, p7zip, unrar, unzip"
+TERMUX_PKG_SUGGESTS="7zip | p7zip, imagemagick, perl, unrar, unzip"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_configure() {
 	./configure \
-		--prefix="$TERMUX_PREFIX"
+		--prefix="$TERMUX_PREFIX" \
+		--bash-completion-dir="${TERMUX_PREFIX}/share/bash-completion/completions" \
+		--zsh-completion-dir="${TERMUX_PREFIX}/share/zsh/site-functions"
 }
 
 termux_step_post_make_install() {
