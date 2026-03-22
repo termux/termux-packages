e15484d93555f397290717acea131478cb8dbe0a
TomIO <tom@termux.dev>
2026-03-22 19:43:00 +0100

    enhance(main/gdal): build with `xerces-c` support

--- packages/gdal/build.sh
+++ packages/gdal/build.sh
@@ -4,10 +4,11 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.12.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.osgeo.org/gdal/${TERMUX_PKG_VERSION}/gdal-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=398a5a32ee6e75040598a7f8e895126a8225118317f272d715867c844f932848
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="giflib, json-c, libc++, libcurl, libexpat, libfreexl, libgeos, libiconv, libjpeg-turbo, libjxl, liblzma, libpng, libspatialite, libsqlite, libwebp, libxml2, netcdf-c (>= 4.9.3), openjpeg, openssl, proj, postgresql, zlib, zstd"
+TERMUX_PKG_DEPENDS="giflib, json-c, libc++, libcurl, libexpat, libfreexl, libgeos, libiconv, libjpeg-turbo, libjxl, liblzma, libpng, libspatialite, libsqlite, libwebp, libxml2, netcdf-c (>= 4.9.3), openjpeg, openssl, postgresql, proj, xerces-c, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="json-c-static"
 TERMUX_PKG_BREAKS="gdal-dev"
 TERMUX_PKG_REPLACES="gdal-dev"
@@ -24,7 +25,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure () {
-	if [ "${TERMUX_ARCH}" = "arm" ]; then
+	if [[ "${TERMUX_ARCH}" == "arm" ]]; then
 		## -mfpu=neon causes build failure on ARM.
 		CFLAGS="${CFLAGS/-mfpu=neon/} -mfpu=vfp"
 		CXXFLAGS="${CXXFLAGS/-mfpu=neon/} -mfpu=vfp"
