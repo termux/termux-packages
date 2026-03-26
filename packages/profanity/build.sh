424a69cb32f044d49d6efb84da188fe6966c53f3
Biswapriyo Nath <nathbappai@gmail.com>
2026-03-26 13:05:39 +0000

    bump(main/profanity): 0.17.0
    Remove upstream patch file
    https://github.com/profanity-im/profanity/commit/e6bad112aa534b8b42e51ab7d75ebe6b638ef9d7

--- packages/profanity/build.sh
+++ packages/profanity/build.sh
@@ -5,13 +5,16 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
 # This package depends on libpython${TERMUX_PYTHON_VERSION}.so.
 # Please revbump and rebuild when bumping TERMUX_PYTHON_VERSION.
-TERMUX_PKG_VERSION="0.16.0"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL="https://github.com/profanity-im/profanity/releases/download/$TERMUX_PKG_VERSION/profanity-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=1f2e36a081cd299173f1c12b64b1cef971063e67bf226fb3f7048f2e67bd6a70
+TERMUX_PKG_VERSION="0.17.0"
+TERMUX_PKG_SRCURL="https://github.com/profanity-im/profanity/releases/download/$TERMUX_PKG_VERSION/profanity-$TERMUX_PKG_VERSION.tar.xz"
+TERMUX_PKG_SHA256=508e18c0e797d46cc38779eb207480fc3e93b814e202a351050f395c1b262804
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, gpgme, libandroid-support, libassuan, libcurl, libgcrypt, libgpg-error, libotr, libsignal-protocol-c, libsqlite, libstrophe, ncurses, python, readline"
 TERMUX_PKG_BREAKS="profanity-dev"
 TERMUX_PKG_REPLACES="profanity-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS=" --enable-plugins --without-xscreensaver"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	autoreconf -fi
+}
