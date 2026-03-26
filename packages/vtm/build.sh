0f41abcc118425a6e77155d307694ecf2bcf678b
Biswapriyo Nath <nathbappai@gmail.com>
2026-03-26 14:26:12 +0000

    bump(main/vtm): 2026.03.26

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-p
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2026.03.21"
+TERMUX_PKG_VERSION="2026.03.26"
 TERMUX_PKG_SRCURL=https://github.com/directvt/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1bbd911c957534beb6cdc92083f2cb2a6faaa5ee73cd95afed5e4f98418bb062
-TERMUX_PKG_DEPENDS="freetype, harfbuzz, libc++, lua54"
+TERMUX_PKG_SHA256=cb30a309c5892277ffdbcdeae02aabb9042265a62d9c59ea806e80126165caa2
+TERMUX_PKG_DEPENDS="freetype, harfbuzz, libc++, lua54, lunasvg"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
 TERMUX_PKG_AUTO_UPDATE=true
 
