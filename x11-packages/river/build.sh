fe40bcb4a0f089ac65e473095f2904dfb928ba85
怪异软糖 <219617512+WeirdGumi@users.noreply.github.com>
2026-03-27 08:24:44 +0800

    chore(main/river): format

--- x11-packages/river/build.sh
+++ x11-packages/river/build.sh
@@ -1,9 +1,9 @@
-TERMUX_PKG_HOMEPAGE=https://codeberg.org/ifreund/$TERMUX_PKG_NAME
+TERMUX_PKG_HOMEPAGE=https://codeberg.org/river/river
 TERMUX_PKG_DESCRIPTION="A dynamic tiling wayland compositor"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.9
-TERMUX_PKG_SRCURL=$TERMUX_PKG_HOMEPAGE/releases/download/v$TERMUX_PKG_VERSION/$TERMUX_PKG_NAME-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SRCURL="https://codeberg.org/river/river/releases/download/v${TERMUX_PKG_VERSION}/river-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=49d8a2b679597cf5dff07008a1215892cc05525fb4085b5226b51611666335e5
 TERMUX_PKG_BUILD_DEPENDS="libevdev, libpixman, libwayland, wlroots, libxkbcommon, xwayland"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols, libwayland-cross-scanner, scdoc, zig-wlroots, zig-pixman, zig-wayland, zig-xkbcommon"
@@ -16,12 +16,12 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	zig build --system $TERMUX_PREFIX/lib/zig/packages \
-		-Dtarget=$ZIG_TARGET_NAME \
+	zig build --system ${TERMUX_PREFIX}/lib/zig/packages \
+		-Dtarget=${ZIG_TARGET_NAME} \
 		-Doptimize=ReleaseSafe \
 		-Dxwayland
 }
 
 termux_step_make_install() {
-	install -Dm700 -t $TERMUX_PREFIX/bin zig-out/bin/*
+	install -Dm700 -t ${TERMUX_PREFIX}/bin zig-out/bin/*
 }
