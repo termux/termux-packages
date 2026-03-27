9d613c0d7a4fbde16cb552a91c604313501babd9
怪异软糖 <219617512+WeirdGumi@users.noreply.github.com>
2026-03-27 08:23:59 +0800

    chore(main/zig-wayland): format

--- packages/zig-wayland/build.sh
+++ packages/zig-wayland/build.sh
@@ -1,25 +1,25 @@
-TERMUX_PKG_HOMEPAGE=https://codeberg.org/ifreund/$TERMUX_PKG_NAME
+TERMUX_PKG_HOMEPAGE=https://codeberg.org/ifreund/zig-wayland
 TERMUX_PKG_DESCRIPTION="Zig wayland scanner and libwayland bindings"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.3.0
-TERMUX_PKG_SRCURL=$TERMUX_PKG_HOMEPAGE/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SRCURL="https://codeberg.org/ifreund/zig-wayland/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=c54f08ac44d214e28a410320c15c9128bc0669e93851268a5e0e3d4bda074934
 TERMUX_PKG_DEPENDS="libwayland"
 
 termux_step_pre_configure() {
 	termux_setup_zig
-	ZIG_PKG_HASH=$(zig fetch --global-cache-dir $TERMUX_PKG_BUILDDIR .)
+	ZIG_PKG_HASH=$(zig fetch --global-cache-dir ${TERMUX_PKG_BUILDDIR} .)
 }
 
 termux_step_make_install() {
-	install -Dm600 -t $TERMUX_PREFIX/lib/zig/packages/$ZIG_PKG_HASH \
-		$TERMUX_PKG_BUILDDIR/p/$ZIG_PKG_HASH/build.zig
-	install -Dm600 -t $TERMUX_PREFIX/lib/zig/packages/$ZIG_PKG_HASH \
-		$TERMUX_PKG_BUILDDIR/p/$ZIG_PKG_HASH/build.zig.zon
-	install -Dm600 -t $TERMUX_PREFIX/lib/zig/packages/$ZIG_PKG_HASH \
-		$TERMUX_PKG_BUILDDIR/p/$ZIG_PKG_HASH/LICENSE
-	install -m700 -d $TERMUX_PREFIX/lib/zig/packages/$ZIG_PKG_HASH/src
-	install -Dm600 -t $TERMUX_PREFIX/lib/zig/packages/$ZIG_PKG_HASH/src \
-		$TERMUX_PKG_BUILDDIR/p/$ZIG_PKG_HASH/src/*
+	install -Dm600 -t ${TERMUX_PREFIX}/lib/zig/packages/${ZIG_PKG_HASH} \
+		${TERMUX_PKG_BUILDDIR}/p/${ZIG_PKG_HASH}/build.zig
+	install -Dm600 -t ${TERMUX_PREFIX}/lib/zig/packages/${ZIG_PKG_HASH} \
+		${TERMUX_PKG_BUILDDIR}/p/${ZIG_PKG_HASH}/build.zig.zon
+	install -Dm600 -t ${TERMUX_PREFIX}/lib/zig/packages/${ZIG_PKG_HASH} \
+		${TERMUX_PKG_BUILDDIR}/p/${ZIG_PKG_HASH}/LICENSE
+	install -m700 -d ${TERMUX_PREFIX}/lib/zig/packages/${ZIG_PKG_HASH}/src
+	install -Dm600 -t ${TERMUX_PREFIX}/lib/zig/packages/${ZIG_PKG_HASH}/src \
+		${TERMUX_PKG_BUILDDIR}/p/${ZIG_PKG_HASH}/src/*
 }
