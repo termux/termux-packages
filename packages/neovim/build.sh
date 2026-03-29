034fdec25a1b02aebf2147c88fb5f684a1fcdc26
Joshua "TomIO" Kahn <tom@termux.dev>
2026-03-30 00:28:34 +0200

    revbump(main/neovim): tree-sitter 0.26 rebuild and clean up the leftover add-target-to-gen-preprocessing.diff file

--- packages/neovim/build.sh
+++ packages/neovim/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0, VIM License"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION="0.12.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/neovim/neovim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=76b4875fc1a4805a807a9fa53ff0c8fb081620137a40fb879b32436e375aeb65
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv, libmsgpack, libunibilium, libuv, libvterm (>= 1:0.3-0), lua51-lpeg, luajit, luv, tree-sitter, tree-sitter-parsers, utf8proc"
