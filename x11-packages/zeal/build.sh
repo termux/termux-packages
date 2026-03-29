32a2e7cd130724a5ae75c917b680dc58dd0fe13a
Biswapriyo Nath <nathbappai@gmail.com>
2026-03-29 19:49:04 +0000

    addpkg(x11/zeal): 0.8.0

---
+++ x11-packages/zeal/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://zealdocs.org/
+TERMUX_PKG_DESCRIPTION="Offline documentation browser"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.8.0"
+TERMUX_PKG_SRCURL="https://github.com/zealdocs/zeal/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=0fc04840b18c6323c5bbb80f475958f146bd6fc53cff8952e957b9d0048a8cf0
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libarchive, libc++, libsqlite, libx11, libxcb, qt6-qtbase, qt6-qtwebchannel, qt6-qtwebengine, xcb-util-keysyms"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+# error: cpp-httplib doesn't support 32-bit platforms
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DZEAL_RELEASE_BUILD=ON
+"
