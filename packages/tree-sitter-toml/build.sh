f406af80f0c5aed162cc3da54dc68d2d1b77cab2
Joshua "TomIO" Kahn <tom@termux.dev>
2026-03-30 00:28:35 +0200

    revbump(main/tree-sitter-toml): tree-sitter 0.26 rebuild

--- packages/tree-sitter-toml/build.sh
+++ packages/tree-sitter-toml/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="TOML grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION="0.7.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-toml/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7d52a7d4884f307aabc872867c69084d94456d8afcdc63b0a73031a8b29036dc
 TERMUX_PKG_AUTO_UPDATE=true
