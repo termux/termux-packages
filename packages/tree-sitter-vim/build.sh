c4a170a40f45030d4ed069121830942d64d4bdce
Joshua "TomIO" Kahn <tom@termux.dev>
2026-03-30 00:28:35 +0200

    revbump(main/tree-sitter-vim): tree-sitter 0.26 rebuild

--- packages/tree-sitter-vim/build.sh
+++ packages/tree-sitter-vim/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Vimscript grammar for tree-sitter"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION="0.8.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tree-sitter-grammars/tree-sitter-vim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=93cafb9a0269420362454ace725a118ff1c3e08dcdfdc228aa86334b54d53c2a
 TERMUX_PKG_AUTO_UPDATE=true
