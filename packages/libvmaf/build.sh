32c3ad09da6ce865df086a45bf1e82066a418248
Robert Kirkman <rkirkman@termux.dev>
2026-04-05 01:32:56 -0500

    fix(main/libvmaf): fix build for 32-bit x86 by applying `-Denable_asm=false`
    - To work around this upstream issue: https://github.com/Netflix/vmaf/issues/1481

--- packages/libvmaf/build.sh
+++ packages/libvmaf/build.sh
@@ -4,11 +4,16 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.1.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Netflix/vmaf/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=80090e29d7fd0db472ddc663513f5be89bc936815e62b767e630c1d627279fe2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
-	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/libvmaf
+	TERMUX_PKG_SRCDIR="$TERMUX_PKG_SRCDIR/libvmaf"
+	# https://github.com/Netflix/vmaf/issues/1481
+	if [[ "$TERMUX_ARCH" == "i686" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Denable_asm=false"
+	fi
 }
