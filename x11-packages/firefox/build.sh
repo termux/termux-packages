c9c0d70a6eb07d435f2a2fabc548da7581dd8e75
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-04-06 18:37:21 +0800

    firefox: bump to 149.0 (#29222)

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -2,16 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://www.mozilla.org/firefox
 TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="149.0+really148.0.2"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="149.0+really149.0"
 TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION#*really}/source/firefox-${TERMUX_PKG_VERSION#*really}.source.tar.xz"
-TERMUX_PKG_SHA256=a6cb8e4d5e596cd52475bab9b4d399240f10c4211718b9d72ca6b2e9c9244e90
+TERMUX_PKG_SHA256=b861fdee999d9b6404e1e865d6f707c41b4bded1b5ea62affc176288c1484b8a
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
 TERMUX_PKG_BUILD_IN_SRC=true
-# https://github.com/termux/termux-packages/issues/29105
-TERMUX_PKG_AUTO_UPDATE=false
+TERMUX_PKG_AUTO_UPDATE=true
 
 # NOTE:
 # Firefox's patches are also used by Thunderbird.
