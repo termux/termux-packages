8a8ca1e5f5d194f67ec878e4f7a3b5ede5818e25
Biswapriyo Nath <nathbappai@gmail.com>
2026-04-09 14:46:19 +0000

    bump(main/pgroonga): 4.0.6
    - Port to meson build system because upstream removed cmake and make files.
    - Add workaround for meson to find pg_config from TERMUX_PREFIX.

--- packages/pgroonga/build.sh
+++ packages/pgroonga/build.sh
@@ -3,21 +3,14 @@ TERMUX_PKG_DESCRIPTION="A PostgreSQL extension to use Groonga as index"
 TERMUX_PKG_LICENSE="PostgreSQL"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.0.5"
+TERMUX_PKG_VERSION="4.0.6"
 TERMUX_PKG_SRCURL=https://github.com/pgroonga/pgroonga/releases/download/${TERMUX_PKG_VERSION}/pgroonga-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8fb7f0e559525594b7f2b20eae4495925c842ab86b3e09e256abfdc5be793133
+TERMUX_PKG_SHA256=d0048944763c18f91bc67e043aafa64c2c53f6246547c9474311efbc05ccfe66
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="groonga, libmsgpack, xxhash"
 TERMUX_PKG_BUILD_DEPENDS="postgresql"
 TERMUX_PKG_EXTRA_MAKE_ARGS="
-HAVE_MSGPACK=1
-HAVE_XXHASH=1
-MSGPACK_PACKAGE_NAME=msgpack-c
-PG_CONFIG=${TERMUX_PREFIX}/bin/pg_config
+-Dmessage_pack=enabled
+-Dtest=false
+-Dxxhash=enabled
 "
-
-termux_step_post_get_source() {
-	# force make
-	rm CMakeLists.txt meson.build
-}
