114ebe6b80b77c6fe9d7f8a142700984b2ec84eb
Robert Kirkman <rkirkman@termux.dev>
2026-04-09 08:02:05 -0500

    bump(x11/pcsx-rearmed): 26
    - Fixes https://github.com/termux/termux-packages/issues/29140
    
    - `@TERMUX_ANDROID_HOME@` in `menu.c.patch` does not work and causes `menu_loop_romsel failed, dir: @TERMUX_ANDROID_HOME@` at runtime; use `@TERMUX_HOME@` instead, which is correct and is working
    
    - `configure.patch` needs to be replaced with `export SDL_CONFIG="$TERMUX_PREFIX/bin/sdl-config"` instead
    
    - After this commit, huge chunks of `main.c.patch`, `main.h.patch`, and `menu.c.patch` appear to no longer be necessary, having been  replaced with an upstream equivalent of the same thing https://github.com/notaz/pcsx_rearmed/commit/1ae743b1f67e225d74fa08839dc794630c1f8e7c . The remaining code unqiue to Termux not found in the upstream implementation is small.

--- x11-packages/pcsx-rearmed/build.sh
+++ x11-packages/pcsx-rearmed/build.sh
@@ -2,8 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/notaz/pcsx_rearmed
 TERMUX_PKG_DESCRIPTION="Yet another PCSX fork based on the PCSX-Reloaded project"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=23
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION="26"
 TERMUX_PKG_SRCURL=git+https://github.com/notaz/pcsx_rearmed
 TERMUX_PKG_GIT_BRANCH=r${TERMUX_PKG_VERSION}
 TERMUX_PKG_SHA256=887e9b5ee7b8115d35099c730372b4158fd3e215955a06d68e20928b339646af
@@ -22,12 +21,28 @@ termux_pkg_auto_update() {
 	fi
 }
 
+termux_step_post_get_source() {
+	# convert CRLF to LF globally while working with this package in termux
+	# https://github.com/termux/termux-packages/issues/11744
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		DOS2UNIX="$TERMUX_PKG_TMPDIR/dos2unix"
+		(. "$TERMUX_SCRIPTDIR/packages/dos2unix/build.sh"; TERMUX_PKG_SRCDIR="$DOS2UNIX" termux_step_get_source)
+		pushd "$DOS2UNIX"
+		make dos2unix
+		popd # DOS2UNIX
+		export PATH="$DOS2UNIX:$PATH"
+	fi
+
+	find "$TERMUX_PKG_SRCDIR" -type f -print0 | xargs -0 dos2unix
+}
+
 termux_step_pre_configure() {
 	CFLAGS+=" $CPPFLAGS"
 	CFLAGS="${CFLAGS/-Oz/-Os}"
 	if [ "$TERMUX_ARCH" = "arm" ]; then
 		termux_setup_no_integrated_as
 	fi
+	export SDL_CONFIG="$TERMUX_PREFIX/bin/sdl-config"
 }
 
 termux_step_configure() {
