8804df6b5a438f2b44be1ad690a008f371e1be6f
BullyMaguire-lol <temp9040@gmail.com>
2026-04-11 07:26:06 +0530

    bump(x11/kf6-kparts): 6.25.0

--- x11-packages/kf6-kparts/build.sh
+++ x11-packages/kf6-kparts/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/kparts'
 TERMUX_PKG_DESCRIPTION='Document centric plugin system'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.24.0"
+TERMUX_PKG_VERSION="6.25.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kparts-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=b4a0b72167e0f77974aafe93305c18dcc99c0e66c1f2fd56a3e44d78e72f54e7
+TERMUX_PKG_SHA256=f0d6346e3127d1e44506733113be467d8b50cb82731d300a57990dd09baaedf1
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-ki18n (>= ${TERMUX_PKG_VERSION%.*}), kf6-kio (>= ${TERMUX_PKG_VERSION%.*}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-kxmlgui (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
