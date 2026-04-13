47081717bc1ca134727632222ff9f292fc984495
Biswapriyo Nath <nathbappai@gmail.com>
2026-04-13 14:41:57 +0000

    bump(x11/sdl2-image): 2.8.10
    https://github.com/libsdl-org/SDL_image/releases/tag/release-2.8.10

--- x11-packages/sdl2-image/build.sh
+++ x11-packages/sdl2-image/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="A simple library to load images of various formats as SD
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.8.8"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION="2.8.10"
 TERMUX_PKG_SRCURL="https://github.com/libsdl-org/SDL_image/releases/download/release-${TERMUX_PKG_VERSION}/SDL2_image-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=2213b56fdaff2220d0e38c8e420cbe1a83c87374190cba8c70af2156097ce30a
+TERMUX_PKG_SHA256=ebc059d01c007a62f4b04f10cf858527c875062532296943174df9a80264fd65
 # Prevent updating to SDL3 version
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libavif, libjpeg-turbo, libjxl, libpng, libtiff, libwebp, sdl2 | sdl2-compat"
