5921265adf3bf725f8ec263e7cacb609183b3f64
Robert Kirkman <rkirkman@termux.dev>
2026-04-15 19:08:04 -0500

    bump(main/python-pyppmd): 1.3.1
    - Switch to `TERMUX_PKG_UPDATE_TAG_TYPE=newest-tag`. I did not previously understand that the recent releases of pyppmd are only tags, not "releases".

--- packages/python-pyppmd/build.sh
+++ packages/python-pyppmd/build.sh
@@ -2,13 +2,14 @@ TERMUX_PKG_HOMEPAGE=https://github.com/miurahr/pyppmd
 TERMUX_PKG_DESCRIPTION="PPM compression/decompression library"
 TERMUX_PKG_LICENSE="LGPL-2.1-only"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.2.0"
+TERMUX_PKG_VERSION="1.3.1"
 TERMUX_PKG_SRCURL="https://github.com/miurahr/pyppmd/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=7da07545b5764d91011d5f8e9740fab6eb92265ab04990a7815a139ca4757443
+TERMUX_PKG_SHA256=ca5328cfff8be532fe834f1844c281b503f3b069e6ccb6232971eaf82474dbd3
 TERMUX_PKG_DEPENDS="python, python-pip"
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="build, installer"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE=newest-tag
 
 termux_step_pre_configure() {
 	rm CMakeLists.txt
