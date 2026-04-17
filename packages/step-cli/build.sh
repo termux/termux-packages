c40dda0ec268d11fa6f69676fa5cde9f19d766d9
Jia Yuan Lo <jylo06g@gmail.com>
2026-04-17 22:23:51 +0800

    fix(main/step-cli): use release tarball with persistent checksum

--- packages/step-cli/build.sh
+++ packages/step-cli/build.sh
@@ -3,11 +3,26 @@ TERMUX_PKG_DESCRIPTION="An easy-to-use CLI tool for building, operating, and aut
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.30.2"
-TERMUX_PKG_SRCURL=https://github.com/smallstep/cli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=25a1b459048e1be2f43907c8aac47f5dbf3a74451a52ff4fd486e26a95db09da
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/smallstep/cli/releases/download/v${TERMUX_PKG_VERSION}/step_${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=db62a88ebec709de591dd86eec9759e15bdff4c6b96f3d7db6f53b6cf86bd3ec
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="termux-tools"
+
+termux_step_post_get_source() {
+	# termux_unpack_src_archive defaults to strip-components=1
+	# which works for majority of archives with source code
+	# in a subfolder
+	# unfortunately this breaks archives with source code at
+	# root level
+	# manually extract without strip-components
+	termux_download_src_archive
+	local file="$TERMUX_PKG_CACHEDIR/$(basename "${TERMUX_PKG_SRCURL}")"
+	mkdir -p "$TERMUX_PKG_SRCDIR"
+	tar xf "$file" -C "$TERMUX_PKG_SRCDIR"
+}
 
 termux_step_make() {
 	termux_setup_golang
