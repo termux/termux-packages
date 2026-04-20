eb7b768d59da8afd821a8c4322b7d4349b5d470e
Suggon <maharjansagun99@protonmail.com>
2026-04-20 22:46:22 +0545

    fix(main/translate-shell): make hunspell and mpv optional suggestions

--- packages/translate-shell/build.sh
+++ packages/translate-shell/build.sh
@@ -3,13 +3,14 @@ TERMUX_PKG_DESCRIPTION="Command-line translator using Google Translate, Bing Tra
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.9.7.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/soimort/translate-shell/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=f949f379779b9e746bccb20fcd180d041fb90da95816615575b49886032bcefa
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="bash, curl, gawk, less, rlwrap"
 # hunspell - spell checking
 # mpv - text-to-speech functionality
-TERMUX_PKG_RECOMMENDS="hunspell, mpv"
+TERMUX_PKG_SUGGESTS="hunspell, mpv"
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
