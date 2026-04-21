6344b7f0ea85ad8675a04f1b462d9c5fe99205ed
xatandcatch-blip <xatandcatch@gmail.com>
2026-04-21 18:04:58 +0300

    Update source URL to use refs/tags

--- packages/javax-comp/build.sh
+++ packages/javax-comp/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="iMYou Engine - Native Multi-Stage Compiler"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="xatandcatch-blip"
 TERMUX_PKG_VERSION=3.5.2
-TERMUX_PKG_SRCURL=https://github.com/xatandcatch-blip/javax-comp/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/xatandcatch-blip/javax-comp/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5
 TERMUX_PKG_AUTO_UPDATE=true
 
