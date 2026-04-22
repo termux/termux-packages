5de575d0da3c1e4bef3a8539f2fefffc861c635e
Robert Kirkman <rkirkman@termux.dev>
2026-04-22 09:01:58 -0500

    bump(x11/xorg-fonts-alias): 1.0.6
    - Fixes https://github.com/termux/termux-x11/issues/880
    - Fixes https://github.com/termux/termux-packages/issues/23002
    
    - Recommend `xorg-fonts-75dpi`, `xorg-fonts-100dpi`, `xorg-fonts-cyrillic` and `xorg-fonts-misc`, because if any of these are not installed while `xorg-fonts-alias` is installed, in particular `xorg-fonts-misc`, then strange and unxpected behavior can happen in Termux:X11 particularly with certain programs such as `xcalc` and `xmessage`, resulting in '`Error: Aborting: no font found`'.

--- x11-packages/xorg-fonts-alias/build.sh
+++ x11-packages/xorg-fonts-alias/build.sh
@@ -3,7 +3,13 @@ TERMUX_PKG_DESCRIPTION="X.org font alias files"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.5
-TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/font/font-alias-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=9f89e217bb73e0e3636a0a493fbf8b7c995156e0c53d9a0476d201b67c2d6b6e
+TERMUX_PKG_VERSION="1.0.6"
+TERMUX_PKG_SRCURL="https://xorg.freedesktop.org/releases/individual/font/font-alias-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=f6a5e968b616e728a981fd1fd9ed5fe2ca0fe18fbe072b1888cb3f71b701e4c0
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+# if any of these are not installed while xorg-fonts-misc is installed, in particular xorg-fonts-misc,
+# then strange and unxpected behavior can happen in Termux:X11 particularly with certain programs
+# such as xcalc and xmessage, resulting in 'Error: Aborting: no font found'
+# https://github.com/termux/termux-x11/issues/880
+# https://github.com/termux/termux-packages/issues/23002
+TERMUX_PKG_RECOMMENDS="xorg-fonts-75dpi, xorg-fonts-100dpi, xorg-fonts-cyrillic, xorg-fonts-misc"
