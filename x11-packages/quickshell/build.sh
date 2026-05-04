106d4d17de93c26f2fe5ff8f4985bf3f0143e3c4
Robert Kirkman <rkirkman@termux.dev>
2026-05-04 08:30:50 -0500

    bump(x11/quickshell): 0.3.0
    - Fixes https://github.com/termux/termux-packages/issues/29654
    
    - `CRASH_REPORTER` is now named `CRASH_HANDLER`
    
    - `SERVICE_POLKIT` is now enabled by default and needs to be disabled

--- x11-packages/quickshell/build.sh
+++ x11-packages/quickshell/build.sh
@@ -2,17 +2,18 @@ TERMUX_PKG_HOMEPAGE="https://git.outfoxxed.me/quickshell/quickshell"
 TERMUX_PKG_DESCRIPTION="Flexible toolkit for making desktop shells with QtQuick"
 TERMUX_PKG_LICENSE="LGPL-3.0-only"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.2.1"
+TERMUX_PKG_VERSION="0.3.0"
 TERMUX_PKG_SRCURL="https://github.com/quickshell-mirror/quickshell/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256="337bab5d4badb92e51d01ac1cfbeaeb784478bad82f3c8297fd7be42a6995af2"
+TERMUX_PKG_SHA256="5229069b0f1d375f34b0a04a4e6a69156e2f010995d9ec5a943793424e589b5d"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="hicolor-icon-theme, libc++, libdrm, libglvnd, libwayland, libxcb, mesa, pipewire, qt6-qtbase, qt6-qtdeclarative, qt6-qtsvg, qt6-qtwayland"
 TERMUX_PKG_BUILD_DEPENDS="cli11, libwayland-protocols, qt6-shadertools, spirv-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
--DCRASH_REPORTER=OFF
+-DCRASH_HANDLER=OFF
 -DUSE_JEMALLOC=OFF
 -DSERVICE_PAM=OFF
+-DSERVICE_POLKIT=OFF
 -DINSTALL_QML_PREFIX=lib/qt6/qml
 "
 
