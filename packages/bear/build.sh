f86c50407ff7a787ff1bc70e3ba40e1b64eb8bab
Robert Kirkman <rkirkman@termux.dev>
2026-05-04 08:39:03 -0500

    bump(main/bear): 4.1.3
    - Fixes https://github.com/termux/termux-packages/issues/29655
    
    - Rebase `disable-posix-spawn.patch`

--- packages/bear/build.sh
+++ packages/bear/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/rizsotto/Bear
 TERMUX_PKG_DESCRIPTION="Bear is a tool that generates a compilation database for clang tooling."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Nguyen Khanh @nguynkhn"
-TERMUX_PKG_VERSION="4.1.2"
+TERMUX_PKG_VERSION="4.1.3"
 TERMUX_PKG_SRCURL="https://github.com/rizsotto/Bear/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=34e20693d9f3f6820bf330d5dc426eeafb33092c24208f25954d401a92083673
+TERMUX_PKG_SHA256=87a5b385b01000a3ae2c69f535384dca33da7f23925a523ba177f98b1bb7f301
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
