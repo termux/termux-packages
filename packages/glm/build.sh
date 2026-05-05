c2166be8bc25b55a56a8d3b26839b37b1858b458
Joshua "TomIO" Kahn <tom@termux.dev>
2026-05-05 04:06:24 +0200

    bump(main/glm): 1.0.3

--- packages/glm/build.sh
+++ packages/glm/build.sh
@@ -1,11 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://glm.g-truc.net/
-TERMUX_PKG_DESCRIPTION="C++ mathematics library for graphics software based on the GLSL specifications"
+TERMUX_PKG_DESCRIPTION="C++ mathematics library for graphics programming"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="copying.txt"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.1
-TERMUX_PKG_SRCURL=https://github.com/g-truc/glm/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=9f3174561fd26904b23f0db5e560971cbf9b3cbda0b280f04d5c379d03bf234c
+TERMUX_PKG_VERSION=1.0.3
+TERMUX_PKG_SRCURL="https://github.com/g-truc/glm/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=6775e47231a446fd086d660ecc18bcd076531cfedd912fbd66e576b118607001
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DGLM_BUILD_TESTS=OFF"
 TERMUX_PKG_NO_STATICSPLIT=true
 
