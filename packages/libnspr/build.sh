b9a1855f597b7ea7da1b6da525b9728006ca1816
Robert Kirkman <rkirkman@termux.dev>
2026-05-09 16:15:47 -0500

    fix(main/libnspr): do not link to `liblog.so` or transitively `libc++.so` - test for `libreoffice`
    - Test for https://github.com/termux/termux-packages/issues/29690

--- packages/libnspr/build.sh
+++ packages/libnspr/build.sh
@@ -3,25 +3,21 @@ TERMUX_PKG_DESCRIPTION="Netscape Portable Runtime (NSPR)"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.39"
-TERMUX_PKG_SRCURL=https://archive.mozilla.org/pub/nspr/releases/v${TERMUX_PKG_VERSION}/src/nspr-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/nspr/releases/v${TERMUX_PKG_VERSION}/src/nspr-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=bbd02ee87a55676063a63e5bc819e0227de2666b47307b2a0134414cdf42368e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+NSINSTALL=$TERMUX_PKG_HOSTBUILD_DIR/config/nsinstall
+NOW=$TERMUX_PKG_HOSTBUILD_DIR/config/now
+"
 
 termux_step_post_get_source() {
 	TERMUX_PKG_SRCDIR+="/nspr"
+	cd "$TERMUX_PKG_SRCDIR"
 }
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DANDROID"
-	LDFLAGS+=" -llog"
-
-	if [ $TERMUX_ARCH_BITS -eq 64 ]; then
-		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-64bit"
-	fi
-
-	TERMUX_PKG_EXTRA_MAKE_ARGS+="
-		NSINSTALL=$TERMUX_PKG_HOSTBUILD_DIR/config/nsinstall
-		NOW=$TERMUX_PKG_HOSTBUILD_DIR/config/now
-		"
 }
