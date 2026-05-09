cd2e6b5ae4c2b35f49a978b2d477bcb1318e50ec
Package Fix <fix@termux.dev>
2026-05-09 14:39:27 +0000

    openclaw-game: simplify build - actual game binary

--- packages/openclaw-game/build.sh
+++ packages/openclaw-game/build.sh
@@ -7,20 +7,29 @@ TERMUX_PKG_SRCURL=https://github.com/pjasicek/OpenClaw/archive/refs/tags/v${TERM
 TERMUX_PKG_SHA256=8b6c210a5ab1235aa8da6639a55f352e3b9c9e0b7a404628b4c1f61d7c3a0885
 TERMUX_PKG_DEPENDS="sdl2, sdl2-image, sdl2-mixer, sdl2-ttf, libpng, zlib"
 TERMUX_PKG_BUILD_DEPENDS="cmake, make, clang"
-TERMUX_PKG_BUILD_IN_SRC=false
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=Release -DUSE_SDL2=ON"
+TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
-	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/OpenClaw
+termux_step_configure() {
+	cd "$TERMUX_PKG_SRCDIR"
+	mkdir -p build && cd build
+	
+	cmake ../OpenClaw \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_INSTALL_PREFIX="$TERMUX_PREFIX" \
+		-DCMAKE_SYSTEM_NAME=Linux \
+		-DUSE_SDL2=ON \
+		-DBUILD_SHARED_LIBS=OFF
+}
+
+termux_step_make() {
+	cd "$TERMUX_PKG_SRCDIR/build"
+	make -j$TERMUX_MAKE_PROCESSES
 }
 
 termux_step_make_install() {
-	install -Dm700 $TERMUX_PKG_BUILDDIR/openclaw $TERMUX_PREFIX/bin/openclaw
-	install -Dm600 $TERMUX_PKG_SRCDIR/../LICENSE.txt $TERMUX_PREFIX/share/doc/openclaw-game/LICENSE
+	cd "$TERMUX_PKG_SRCDIR/build"
+	install -Dm700 openclaw "$TERMUX_PREFIX/bin/openclaw"
 	
-	# Create game data directory
-	mkdir -p $TERMUX_PREFIX/share/openclaw-game
-	
-	# Install launcher with controller and save state support
-	install -Dm700 $TERMUX_PKG_BUILDER_DIR/openclaw-game-launcher $TERMUX_PREFIX/bin/openclaw-game
+	# Install launcher script
+	install -Dm700 "$TERMUX_PKG_BUILDER_DIR/openclaw-game" "$TERMUX_PREFIX/bin/openclaw-game"
 }
