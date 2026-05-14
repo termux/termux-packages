bc0bdac04f290b1f1b5814de100aa5a5fec74e81
Robert Kirkman <rkirkman@termux.dev>
2026-05-14 10:07:54 -0500

    revbump(x11/libqtxdg): 4.4.0-1

--- x11-packages/libqtxdg/build.sh
+++ x11-packages/libqtxdg/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Qt implementation of freedesktop.org XDG specifications"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.4.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/libqtxdg/releases/download/${TERMUX_PKG_VERSION}/libqtxdg-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=34d25949ae7b6275fb54da46187dd8ba41771600353405b15e53bdc90b9e287a
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, qt6-qtsvg, glib"
