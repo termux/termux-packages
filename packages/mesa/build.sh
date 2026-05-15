e48e19cf2b042addb8d007eb171bd6614cb17e91
Joshua "TomIO" Kahn <tom@termux.dev>
2026-05-15 14:55:25 +0200

    enhance(main/mesa): build script cleanup This is prep for Intel driver support (i915/iris) on x86_64, but I am unable to get this working with LLVM 22 at this time.
    Co-authored-by: exfurr <banatos80@gmail.com>
    Co-authored-by: Robert Kirkman <rkirkman@termux.dev>

--- packages/mesa/build.sh
+++ packages/mesa/build.sh
@@ -4,15 +4,15 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="26.0.6"
-TERMUX_PKG_SRCURL=https://archive.mesa3d.org/mesa-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SRCURL="https://archive.mesa3d.org/mesa-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=1d3c3b8a8363b8cc354175bb4a684ad8b035211cc1d6fa17aeb9b9623c513f89
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_DEPENDS="libclc, libwayland-protocols, libxrandr, llvm, llvm-tools, mlir, spirv-tools, xorgproto"
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libdrm, libglvnd, libllvm (<< $TERMUX_LLVM_NEXT_MAJOR_VERSION), libwayland, libx11, libxext, libxfixes, libxshmfence, libxxf86vm, ncurses, vulkan-loader, zlib, zstd"
 TERMUX_PKG_SUGGESTS="mesa-dev"
-TERMUX_PKG_BUILD_DEPENDS="libclc, libwayland-protocols, libxrandr, llvm, llvm-tools, mlir, spirv-tools, xorgproto"
 TERMUX_PKG_BREAKS="osmesa, osmesa-demos"
 TERMUX_PKG_CONFLICTS="libmesa, ndk-sysroot (<= 25b), osmesa"
 TERMUX_PKG_REPLACES="libmesa, osmesa"
+TERMUX_PKG_AUTO_UPDATE=true
 
 # FIXME: Set `shared-llvm` to disabled if possible
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -27,7 +27,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dllvm=enabled
 -Dshared-llvm=enabled
 -Dplatforms=x11,wayland
--Dgallium-drivers=llvmpipe,softpipe,virgl,zink
 -Dgallium-rusticl=true
 -Dglvnd=enabled
 -Dxmlconfig=disabled
@@ -39,7 +38,7 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	if [ "$TERMUX_PKG_API_LEVEL" -lt 29 ]; then
+	if (( TERMUX_PKG_API_LEVEL < 29 )); then
 		# ELF TLS is supported starting with API level 29.
 		patch --silent -p1 < "$TERMUX_PKG_BUILDER_DIR/0011-lld-undefined-version.diff"
 	fi
@@ -54,61 +53,51 @@ termux_step_pre_configure() {
 	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
 		export BINDGEN_EXTRA_CLANG_ARGS="--sysroot ${TERMUX_STANDALONE_TOOLCHAIN}/sysroot"
 		case "${TERMUX_ARCH}" in
-		arm) BINDGEN_EXTRA_CLANG_ARGS+=" --target=arm-linux-androideabi${TERMUX_PKG_API_LEVEL}" ;;
-		*) BINDGEN_EXTRA_CLANG_ARGS+=" --target=${TERMUX_ARCH}-linux-android${TERMUX_PKG_API_LEVEL}" ;;
+			arm) BINDGEN_EXTRA_CLANG_ARGS+=" --target=arm-linux-androideabi${TERMUX_PKG_API_LEVEL}" ;;
+			*) BINDGEN_EXTRA_CLANG_ARGS+=" --target=${TERMUX_ARCH}-linux-android${TERMUX_PKG_API_LEVEL}" ;;
 		esac
 	fi
 
 	CPPFLAGS+=" -D__USE_GNU"
 	LDFLAGS+=" -landroid-shmem"
 
-	_WRAPPER_BIN=$TERMUX_PKG_BUILDDIR/_wrapper/bin
-	mkdir -p $_WRAPPER_BIN
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
+	_WRAPPER_BIN="$TERMUX_PKG_BUILDDIR/_wrapper/bin"
+	mkdir -p "$_WRAPPER_BIN"
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
 		sed 's|@CMAKE@|'"$(command -v cmake)"'|g' \
-			$TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in \
-			> $_WRAPPER_BIN/cmake
-		chmod 0700 $_WRAPPER_BIN/cmake
+			"$TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in" \
+			> "$_WRAPPER_BIN/cmake"
+		chmod 0700 "$_WRAPPER_BIN/cmake"
 		termux_setup_wayland_cross_pkg_config_wrapper
 	fi
 	export LLVM_CONFIG="${TERMUX_PREFIX}/bin/llvm-config"
 	export PATH="${_WRAPPER_BIN}:${CARGO_HOME}/bin:${PATH}"
 
+	local _gallium_drivers="llvmpipe,softpipe,virgl,zink"
 	local _vk_drivers="swrast"
-	if [ $TERMUX_ARCH = "arm" ] || [ $TERMUX_ARCH = "aarch64" ]; then
-		_vk_drivers+=",freedreno"
-		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dfreedreno-kmds=msm,kgsl"
-	fi
+	case "${TERMUX_ARCH}" in
+		aarch64|arm)
+			_vk_drivers+=",freedreno"
+			TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dfreedreno-kmds=msm,kgsl"
+		;;
+		x86_64)
+			# TODO: figure out why these aren't building with LLVM 22.
+			# _vk_drivers+=",intel"
+		;;
+	esac
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dgallium-drivers=$_gallium_drivers"
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=$_vk_drivers"
 }
 
 termux_step_post_configure() {
-	rm -f $_WRAPPER_BIN/cmake
+	rm -f "$_WRAPPER_BIN/cmake"
 }
 
 termux_step_post_make_install() {
-	# Avoid hard links
-	local f1
-	for f1 in $TERMUX_PREFIX/lib/dri/*; do
-		if [ ! -f "${f1}" ]; then
-			continue
-		fi
-		local f2
-		for f2 in $TERMUX_PREFIX/lib/dri/*; do
-			if [ -f "${f2}" ] && [ "${f1}" != "${f2}" ]; then
-				local s1=$(stat -c "%i" "${f1}")
-				local s2=$(stat -c "%i" "${f2}")
-				if [ "${s1}" = "${s2}" ]; then
-					ln -sfr "${f1}" "${f2}"
-				fi
-			fi
-		done
-	done
-
-	# Create symlinks
-	ln -sf libEGL_mesa.so ${TERMUX_PREFIX}/lib/libEGL_mesa.so.0
-	ln -sf libGLX_mesa.so ${TERMUX_PREFIX}/lib/libGLX_mesa.so.0
-	ln -sf libRusticlOpenCL.so ${TERMUX_PREFIX}/lib/libRusticlOpenCL.so.1
+	# Create SO symlinks
+	ln -sf libEGL_mesa.so "${TERMUX_PREFIX}/lib/libEGL_mesa.so.0"
+	ln -sf libGLX_mesa.so "${TERMUX_PREFIX}/lib/libGLX_mesa.so.0"
+	ln -sf libRusticlOpenCL.so "${TERMUX_PREFIX}/lib/libRusticlOpenCL.so.1"
 
 	unset BINDGEN_EXTRA_CLANG_ARGS LLVM_CONFIG
 }
