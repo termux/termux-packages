722e59ba97c35da1d0f750d4c2b9768dc50614ce
Joshua "TomIO" Kahn <tom@termux.dev>
2026-05-16 23:32:57 +0200

    enhance(scripts/update-packages): log the update method combination of packages

--- scripts/updates/termux_pkg_auto_update.sh
+++ scripts/updates/termux_pkg_auto_update.sh
@@ -1,9 +1,7 @@
 # shellcheck shell=bash
-termux_pkg_auto_update() {
-	if [[ -n "${__CACHED_TAG:-}" ]]; then
-		termux_pkg_upgrade_version "${__CACHED_TAG}"
-		return $?
-	fi
+
+termux_pkg_get_update_method() {
+	local source_url="$1"
 
 	# Example:
 	# https://github.com/vim/vim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
@@ -12,26 +10,36 @@ termux_pkg_auto_update() {
 	# project_host="github.com"
 	#            _="vim/vim/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 	local project_host
-	IFS='/' read -r _ _ project_host _ <<< "${TERMUX_PKG_SRCURL}"
+	IFS='/' read -r _ _ project_host _ <<< "${source_url}"
 
 	# gitlab.gnome.org started responding to API requests originating from
 	# GitHub Actions with HTTP 403 errors in January 2026.
 	# Example command failing in GitHub Actions:
 	# curl https://gitlab.gnome.org/api/v4/projects/GNOME%2Fvte/releases/permalink/latest
 	# See: https://github.com/termux/termux-packages/issues/28242
-	if [[ -z "${TERMUX_PKG_UPDATE_METHOD}" ]]; then
+	if [[ -z "${TERMUX_PKG_UPDATE_METHOD:-}" ]]; then
 		if [[ "${project_host}" == "github.com" ]]; then
 			TERMUX_PKG_UPDATE_METHOD="github"
-		elif [[ "$TERMUX_PKG_SRCURL" == *"/-/archive/"* && "$TERMUX_PKG_SRCURL" != *"gitlab.gnome.org"* ]]; then
+		elif [[ "$source_url" == *"/-/archive/"* && "$source_url" != *"gitlab.gnome.org"* ]]; then
 			TERMUX_PKG_UPDATE_METHOD="gitlab"
 		else
 			TERMUX_PKG_UPDATE_METHOD="repology"
 		fi
 	fi
+	printf '%s' "$TERMUX_PKG_UPDATE_METHOD"
+}
 
-	case "${TERMUX_PKG_UPDATE_METHOD}" in
+termux_pkg_auto_update() {
+	if [[ -n "${__CACHED_TAG:-}" ]]; then
+		termux_pkg_upgrade_version "${__CACHED_TAG}"
+		return $?
+	fi
+
+	local TERMUX_PKG_UPDATE_METHOD
+	TERMUX_PKG_UPDATE_METHOD="$(termux_pkg_get_update_method "${TERMUX_PKG_SRCURL}")"
+	case "$TERMUX_PKG_UPDATE_METHOD" in
 		github)
-			if [[ "${project_host}" != "${TERMUX_PKG_UPDATE_METHOD}.com" ]]; then
+			if [[ "${TERMUX_PKG_SRCURL}" != *"${TERMUX_PKG_UPDATE_METHOD}.com"* ]]; then
 				termux_error_exit <<-EndOfError
 					source url's hostname is not ${TERMUX_PKG_UPDATE_METHOD}.com, but has been
 					configured to use ${TERMUX_PKG_UPDATE_METHOD}'s method.
