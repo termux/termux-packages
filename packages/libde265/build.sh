f55affe4f99ea35891746edea5351fa62f3fdea7
Joshua "TomIO" Kahn <tom@termux.dev>
2026-05-19 21:23:14 +0200

    bump(packages/libde265): 1.0.19

--- packages/libde265/build.sh
+++ packages/libde265/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="H.265/HEVC video stream decoder library"
 TERMUX_PKG_LICENSE="LGPL-3.0, MIT"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.0.18"
-TERMUX_PKG_SRCURL=https://github.com/strukturag/libde265/releases/download/v$TERMUX_PKG_VERSION/libde265-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=800478f3bf35f0621b14928ceb317579f3e8b23de4bd2aac29b6cb8be962bbd8
+TERMUX_PKG_VERSION="1.0.19"
+TERMUX_PKG_SRCURL="https://github.com/strukturag/libde265/releases/download/v$TERMUX_PKG_VERSION/libde265-$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=bb19a0b485d2643e0eeb7e91f3ab32d1ad617e7c487dbedc91214ca3dbd8d7eb
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++"
 
@@ -14,8 +14,9 @@ termux_step_post_get_source() {
 	# after SOVERSION is changed.
 	local _SOVERSION=0
 
-	local v=$(sed -En 's/^set\(DE265_SOVERSION ([0-9]+)\)$/\1/p' CMakeLists.txt)
-	if [ ! "${v}" ] || [ "${v}" != "${_SOVERSION}" ]; then
+	local v
+	v=$(sed -En 's/^set\(DE265_SOVERSION ([0-9]+)\)$/\1/p' CMakeLists.txt)
+	if [[ "${v:-}" != "${_SOVERSION}" ]]; then
 		termux_error_exit "SOVERSION guard check failed."
 	fi
 }
