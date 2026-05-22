e315f967a56639d95bf9577352060f5ee80f46b8
Biswapriyo Nath <nathbappai@gmail.com>
2026-05-22 04:33:49 +0000

    bump(main/fontconfig): 2.18.0

--- packages/fontconfig/build.sh
+++ packages/fontconfig/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/fontconfig/
 TERMUX_PKG_DESCRIPTION="Library for configuring and customizing font access"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.17.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2.18.0"
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/$TERMUX_PKG_VERSION/fontconfig-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=82e73b26adad651b236e5f5d4b3074daf8ff0910188808496326bd3449e5261d
+TERMUX_PKG_SHA256=5c94af4828988af6b1a8484ddba13b521162687f9e5129bd8f267b8f4cfbf619
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="freetype, libexpat, ttf-dejavu"
 TERMUX_PKG_BREAKS="fontconfig-dev"
@@ -19,5 +18,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
+	export ac_cv_va_copy=C99
 	autoreconf -fi
 }
