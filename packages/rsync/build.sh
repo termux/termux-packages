3326a2db3ffdc2920d99376e990f17ca9151b5d2
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-05-23 23:59:51 +0530

    fix(main/rsync): connection unexpectedly closed

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Fast incremental file transfer utility"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION="3.4.3"
-TERMUX_PKG_SRCURL=https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=c72e63ca3021cbc80ba86ec30102773f4c5631fbc492b52e773b3958f82a53d3
 TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl, openssl-tool, xxhash, zlib, zstd"
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,7 +16,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-included-zlib=no
 --enable-ipv6=yes
 --disable-debug
---disable-simd
 --disable-xattr-support
 --enable-xxhash
 "
