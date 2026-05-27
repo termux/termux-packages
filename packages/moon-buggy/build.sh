61d356bc33739815be6a1655448c37dd15f4bf44
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-05-27 17:16:50 +0700

    bump(main/moon-buggy): 1.1.0

--- packages/moon-buggy/build.sh
+++ packages/moon-buggy/build.sh
@@ -1,14 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/seehuhn/moon-buggy
+TERMUX_PKG_HOMEPAGE=https://www.seehuhn.de/programs/moon-buggy
 TERMUX_PKG_DESCRIPTION="Simple game where you drive a car across the moon's surface"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_VERSION=1.0.51
-TERMUX_PKG_REVISION=6
-# Main site down 2017-01-06.
-# TERMUX_PKG_SRCURL=http://m.seehuhn.de/programs/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SRCURL=ftp://ftp.netbsd.org/pub/pkgsrc/distfiles/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=352dc16ccae4c66f1e87ab071e6a4ebeb94ff4e4f744ce1b12a769d02fe5d23f
+TERMUX_PKG_VERSION=1.1.0
+TERMUX_PKG_SRCURL="https://www.seehuhn.de/programs/moon-buggy/moon-buggy-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=259ae6e7b1838c40532af5c0f20cd7c6173cd5c552ede408114064475bcfc5b6
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--sharedstatedir=$TERMUX_PREFIX/var"
 TERMUX_PKG_GROUPS="games"
 
