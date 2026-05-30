138a0a4c0147a632ec211faa1625b4cd7af8eb8f
Robert Kirkman <rkirkman@termux.dev>
2026-05-29 20:04:12 -0500

    bump(main/sbcl): 2.6.5
    - https://github.com/termux/termux-packages/issues/29956
    
    - After discovering independently that some operating systems do not have `c_ispeed` or `c_ospeed`, upstream removed their hardcoded `c_ispeed` and `c_ospeed` and replaced them with a more dynamic solution in https://github.com/sbcl/sbcl/commit/a72425d87f09bd5bf8af1ba019b676669ac4096a, making `revert-termios-c_ispeed.patch` no longer necessary.

--- packages/sbcl/build.sh
+++ packages/sbcl/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="A high performance Common Lisp compiler"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.6.4"
+TERMUX_PKG_VERSION="2.6.5"
 # sourceforge archive is a precompiled SBCL release for GNU/Linux to use as host Lisp for bootstrapping
 TERMUX_PKG_SRCURL=(
 	https://github.com/sbcl/sbcl/archive/refs/tags/sbcl-${TERMUX_PKG_VERSION}.tar.gz
 	https://sourceforge.net/projects/sbcl/files/sbcl/${TERMUX_PKG_VERSION}/sbcl-${TERMUX_PKG_VERSION}-x86-64-linux-binary.tar.bz2
 )
 TERMUX_PKG_SHA256=(
-	15cc279326263bfba27d18a8a493ab1889d3178fa7c0c5b1a4a026a918401da1
-	466933f9b4f403b6a49b2a7cc755fdd5d827c3b354aa3c8c0d96697e5da1e9fc
+	d42c0cc4d9411d466d75d2846eb35b1da44ca798bea4ad2f39b023c8b1fccb6b
+	9319462950f52c070d4c642b327d35a2e4350937821f1c3eba002193859368a1
 )
 TERMUX_PKG_DEPENDS="zstd"
 # TERMUX_ON_DEVICE_BUILD=true  build dependencies: ecl, strace
