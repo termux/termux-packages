ce503339cf54be8a9aaee521470df43815430a23
Joshua "TomIO" Kahn <tom@termux.dev>
2026-06-05 00:38:41 +0200

    revbump(main/python-apsw): rebuild for sqlite package rename

--- packages/python-apsw/build.sh
+++ packages/python-apsw/build.sh
@@ -3,15 +3,16 @@ TERMUX_PKG_DESCRIPTION="Another Python SQLite Wrapper"
 TERMUX_PKG_LICENSE="ZLIB"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.53.2.0"
-TERMUX_PKG_SRCURL=https://github.com/rogerbinns/apsw/releases/download/${TERMUX_PKG_VERSION}/apsw-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL="https://github.com/rogerbinns/apsw/releases/download/${TERMUX_PKG_VERSION}/apsw-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=12509869b5dd9d105acd3b66c4641e592c2fc5d4fc3510d8404c623cbb0dd215
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libsqlite, python"
+TERMUX_PKG_DEPENDS="python, sqlite"
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
-	cp $TERMUX_PKG_BUILDER_DIR/setup.cfg ./
+	cp "$TERMUX_PKG_BUILDER_DIR/setup.cfg" ./
 }
 
 termux_step_make() {
