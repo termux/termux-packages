5c10b6346cb0df94ea42d3f4faf938864dd5e18d
Joshua "TomIO" Kahn <tom@termux.dev>
2026-06-05 00:38:40 +0200

    revbump(main/botan3): rebuild for sqlite package rename

--- packages/botan3/build.sh
+++ packages/botan3/build.sh
@@ -4,10 +4,10 @@ TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 # This specific package is for libbotan-3.
 TERMUX_PKG_VERSION="3.10.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL="https://botan.randombit.net/releases/Botan-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=fde194236f6d5434f136ea0a0627f6cc9d26af8b96e9f1e1c7d8c82cd90f4f24
-TERMUX_PKG_DEPENDS="libbz2, libc++, liblzma, libsqlite, zlib"
+TERMUX_PKG_DEPENDS="libbz2, libc++, liblzma, sqlite, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --cpu=$TERMUX_ARCH
@@ -28,7 +28,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_configure() {
-	python3 $TERMUX_PKG_SRCDIR/configure.py \
+	python3 "$TERMUX_PKG_SRCDIR/configure.py" \
 		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
 
