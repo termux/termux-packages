b00edc2898a977859390be4b059f4715fb9aed9f
Joshua "TomIO" Kahn <tom@termux.dev>
2026-06-05 00:31:03 +0200

    rename(libsqlite -> sqlite): adopt more common package name Termux is the only distro besides OpenMamba and SliTaz that names its main sqlite package libsqlite. This commit renames the package to adopt the more common package name.

--- packages/sqlite/sqlite-tcl.subpackage.sh
+++ packages/sqlite/sqlite-tcl.subpackage.sh
@@ -1,4 +1,6 @@
 TERMUX_SUBPKG_DESCRIPTION="tcl bindings for SQLite"
 TERMUX_SUBPKG_DEPENDS="tcl"
-TERMUX_SUBPKG_BREAKS="sqlcipher (<< 4.4.2-1), tcl (<< 8.6.10-4)"
+TERMUX_SUBPKG_BREAKS="libsqlite-tcl, sqlcipher (<< 4.4.2-1), tcl (<< 8.6.10-4)"
+TERMUX_PKG_REPLACES="libsqlite-tcl"
+
 TERMUX_SUBPKG_INCLUDE="lib/sqlite3/libtclsqlite3.so lib/sqlite3/pkgIndex.tcl"
