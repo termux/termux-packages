01b36dfa31dcc1c6833783e81cee59ee351da2a1
Biswapriyo Nath <nathbappai@gmail.com>
2026-06-05 07:37:33 +0000

    bump(x11/atril): 1.28.6

--- x11-packages/atril/build.sh
+++ x11-packages/atril/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://mate-desktop.org
 TERMUX_PKG_DESCRIPTION="MATE document viewer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.28.4"
+TERMUX_PKG_VERSION="1.28.6"
 # https://pub.mate-desktop.org/releases/${TERMUX_PKG_VERSION%.*}/atril-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SRCURL="https://github.com/mate-desktop/atril/releases/download/v${TERMUX_PKG_VERSION}/atril-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=a6f5fbf0ab4305c1b8162c5d5a1146a669784b00dec69242e04f148de1b79653
+TERMUX_PKG_SHA256=814427e0d5a2dc4e4b060b99ac3c80d8ef39052984d04df49d39135f424c624a
 TERMUX_PKG_AUTO_UPDATE=true
 # links with poppler-glib, not poppler
 TERMUX_PKG_DEPENDS="atk, djvulibre, gdk-pixbuf, glib, gtk3, harfbuzz, libarchive, libc++, libcairo, libice, libsecret, libsm, libspectre, libsoup3, libtiff, libxml2, mate-desktop, pango, poppler, texlive-bin, webkit2gtk-4.1, zlib"
@@ -22,6 +22,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
+	# force meson
+	rm configure
+
 	# Workaround strict compiler error
 	CFLAGS+=" -Wno-format-security -Wno-format-nonliteral"
 
