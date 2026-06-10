4295a708db804e76a197b5432dff05d873207823
J Adams <jfa63@duck.com>
2026-06-10 09:55:10 -0400

    addpkg(x11/kpat): 26.04.2 KDE KPatience and deps
    This PR adds KDE KPatience, which offers a selection of solitaire card
    games.
    
    Also added are the following dependencies:
        addpkg(x11/libkdegames): 26.04.2 KDE games shared libs,
        addpkg(x11/freecell-solver): 6.16.0 solitaire solver library,
        addpkg(x11/black-hole-solver): 1.14.0 solitaire solver library.
        revbump(x11/kde-applications): 4, added kpat

---
+++ x11-packages/kpat/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/games/kpat"
+TERMUX_PKG_DESCRIPTION="KPatience offers a selection of solitaire card games"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="3ls-it <3ls-it@pm.me>"
+TERMUX_PKG_VERSION="26.04.2"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/kpat-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=c0ae10f2c10e2413351ea79cbc83d9a56ec1c37c57065dc3d5e481d37c76afd7
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="black-hole-solver, freecell-solver, kf6-kcompletion, kf6-kconfig, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kguiaddons, kf6-ki18n, kf6-kio, kf6-kwidgetsaddons, kf6-kxmlgui, libc++, libkdegames, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="kf6-kconfig-cross-tools, extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
+
+termux_step_post_make_install() {
+	rm -f $TERMUX_PREFIX/share/mime/mime.cache
+}
---
+++ x11-packages/libkdegames/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/games/libkdegames"
+TERMUX_PKG_DESCRIPTION="Common code and data for many KDE games"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="3ls-it <3ls-it@pm.me>"
+TERMUX_PKG_VERSION="26.04.2"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/libkdegames-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=af44d46b28a3449b6f805d0316ddce4674296719de0400f3e3a53a8dc59f1905
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kcompletion, kf6-kconfig, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kguiaddons, kf6-ki18n, kf6-kio, kf6-knewstuff, kf6-kwidgetsaddons, kf6-kxmlgui, libc++, libsndfile, openal-soft, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="kf6-kdnssd, kf6-kconfig-cross-tools, extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
