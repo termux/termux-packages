976cff5c2ddf5796f8f16cd16ace078a90e6569f
Biswapriyo Nath <nathbappai@gmail.com>
2026-06-12 04:37:00 +0000

    bump(x11/kf6-kbookmarks): 6.27.0

--- x11-packages/kf6-kbookmarks/build.sh
+++ x11-packages/kf6-kbookmarks/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/kbookmarks'
 TERMUX_PKG_DESCRIPTION='Support for bookmarks and the XBEL format'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.26.0"
+TERMUX_PKG_VERSION="6.27.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/kbookmarks-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=82e8794281870686da9e7e7b5ddc0839f50b15d919357490d508faccb2635030
+TERMUX_PKG_SHA256=75a43775ef03cb0c577c70d9605203789b3c75b786efa17a2e8c286d0c55fb93
 TERMUX_PKG_DEPENDS="kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kconfigwidgets (>= ${TERMUX_PKG_VERSION%.*}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-kwidgetsaddons (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), qt6-qttools"
 TERMUX_PKG_AUTO_UPDATE=true
