7c2a42d97f87bcf0d92f26f4bcbf6d057c000a72
Jia Yuan Lo <jylo06g@gmail.com>
2026-06-13 11:35:59 +0800

    chore(main/rust): update rust-std-* description
    The inclusion of target wasm32v1-none causes rust-std-* naming no longer
    makes sense because wasm32v1-none is a no_std target. The new tests also
    do not work and lack scrutiny.
    
    Fix the tests to be negative std tests for wasm32v1-none.
    Add no_std lib compile test for wasm32* targets.
    Update the description of rust-std-* subpackages instead of a total
    renaming due the upstream build scripts still use rust-std-* naming for
    components and avoid unnecessary breakage.

--- packages/rust/rust-std-wasm32-unknown-unknown.subpackage.sh
+++ packages/rust/rust-std-wasm32-unknown-unknown.subpackage.sh
@@ -1,4 +1,4 @@
-TERMUX_SUBPKG_DESCRIPTION="Rust std for target wasm32-unknown-unknown"
+TERMUX_SUBPKG_DESCRIPTION="Component files for target wasm32-unknown-unknown"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
 TERMUX_SUBPKG_BREAKS="rust-std-wasm32 (<< 1.74.1-1)"
