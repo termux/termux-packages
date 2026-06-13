6053fe3184822fa0d24cf4b518ba838b45c41614
Matheus Garcia <tateusg@protonmail.com>
2026-06-13 15:56:07 -0300

    plan9port: minor update

--- packages/plan9port/build.sh
+++ packages/plan9port/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=1.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/9fans/plan9port/archive/b36c747d62a690ffb9cff6f17fbe207b23aa4b84.tar.gz
 TERMUX_PKG_SHA256=b3c5dc0f2813c6f51178ada1765b6771910060ec3efaa8f05ea3f93c0334b5a8
-TERMUX_PKG_DEPENDS="libresolv-wrapper"
+TERMUX_PKG_DEPENDS="libresolv-wrapper, libx11, libfreetype"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
@@ -15,11 +15,10 @@ termux_step_make() {
 
 termux_step_make_install() {
 	local dir=${TERMUX_PREFIX}/lib/plan9
-	install -d ${TERMUX_PREFIX}/lib
 	cp -r . ${dir}
 	(
 		cd ${dir}
-		./INSTALL -c -r ${dir}
+		./INSTALL -c
 	)
 	ln -f -s ../lib/plan9/bin/9 ${TERMUX_PREFIX}/bin/9
 }
