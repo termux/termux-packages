88738ff8dc8a2606e42bafa47895efd3b994148c
J Adams <jfa63@duck.com>
2026-06-13 11:30:52 -0400

    addpkg(x11/kigo): 26.04.2 KDE Kigo
    This PR adds KDE Kigo and revbumps kde-applications
            revbump(x11/kde-applications): 26.04, 7

---
+++ x11-packages/kigo/build.sh
@@ -0,0 +1,20 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/games/kigo"
+TERMUX_PKG_DESCRIPTION="Kigo is an open-source implementation of the popular Go game."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="3ls-it <3ls-it@pm.me>"
+TERMUX_PKG_VERSION="26.04.2"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/kigo-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256=e27883d1dc08578a2e80d2342ec847810066bf6d96de0ba4a2c6fb04c9ab40c6
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="gnugo, kf6-kconfig, kf6-kconfigwidgets, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kdoctools, kf6-ki18n, kf6-kiconthemes, kf6-kio, kf6-knewstuff, kf6-kwidgetsaddons, kf6-ktextwidgets, kf6-kxmlgui, libkdegames, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="kf6-kconfig-cross-tools, extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
