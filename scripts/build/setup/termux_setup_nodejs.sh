bb6e5800f9c5bdda59e0169ecb29eed79f9d8184
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-16 02:24:45 +0530

    script(termux_setup_nodejs): 22.22.3
    Security support ends in 10 months as of this commit. So will be moving
    to v24 by next month
    
    https://endoflife.date/nodejs

--- scripts/build/setup/termux_setup_nodejs.sh
+++ scripts/build/setup/termux_setup_nodejs.sh
@@ -5,7 +5,7 @@ termux_setup_nodejs() {
 	# Ref: https://github.com/nodejs/node/commit/506b79e888
 	NODE_OPTIONS+=" --network-family-autoselection-attempt-timeout=500"
 	# Use LTS version for now
-	local NODEJS_VERSION=22.22.2
+	local NODEJS_VERSION=22.22.3
 	local NODEJS_FOLDER
 
 	if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
@@ -20,7 +20,7 @@ termux_setup_nodejs() {
 			local NODEJS_TAR_FILE=$TERMUX_PKG_TMPDIR/nodejs-$NODEJS_VERSION.tar.xz
 			termux_download https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz \
 				"$NODEJS_TAR_FILE" \
-				88fd1ce767091fd8d4a99fdb2356e98c819f93f3b1f8663853a2dee9b438068a
+				2e5d13569282d016861fae7c8f935e741693c269101a5bebcf761a5376d1f99f
 			tar -xf "$NODEJS_TAR_FILE" -C "$NODEJS_FOLDER" --strip-components=1
 		fi
 		export PATH=$NODEJS_FOLDER/bin:$PATH
