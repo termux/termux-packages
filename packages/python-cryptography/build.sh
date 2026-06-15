815a346a5787f424307e6271deccfce213e88aa6
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-16 03:56:07 +0530

    fix(main/python-cryptography): link against libpython
    Fixes #30187

--- packages/python-cryptography/build.sh
+++ packages/python-cryptography/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0, BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE, LICENSE.APACHE, LICENSE.BSD"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="48.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/pyca/cryptography/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7f5f25290d718ef861f7efd9fa3bddf2d15426a3d58cecf5155519c683f06cbb
 TERMUX_PKG_AUTO_UPDATE=true
@@ -30,4 +31,15 @@ termux_step_make_install() {
 	# cross-python and picked up for execution instead of maturin built for
 	# build-python
 	cross-pip install --no-build-isolation --no-deps . --prefix $TERMUX_PREFIX
+}
+
+termux_step_post_make_install() {
+	# maturin doesn't honor python-config, so it doesn't link the built module against libpython
+	# Due to differences in between the Android linker and on Linux, linking against libpython is needed
+	# Looking at https://github.com/PyO3/pyo3/issues/1082, it seems like maturin does try to link against libpython
+	# but looking at the source, it seems to do only for binary targets, not library.
+	# Anyways, do it ourselves not worth the effort to ask upstream for a single package we have
+	patchelf \
+		--add-needed libpython${TERMUX_PYTHON_VERSION}.so \
+		"${TERMUX_PREFIX}/lib/python${TERMUX_PYTHON_VERSION}/site-packages/cryptography/hazmat/bindings/_rust.abi3.so"
 }
