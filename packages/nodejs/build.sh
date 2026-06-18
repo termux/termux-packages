fec44f9972d1b7fe1973d6b707c6dc971ebb2f17
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-18 18:59:49 +0530

    bump(main/nodejs): 26.3.1
    This is a security release and hence is being fast tracked without
    testing builds and tests

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Open Source, cross-platform JavaScript runtime environment"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=26.3.0
+TERMUX_PKG_VERSION=26.3.1
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=319ad5d7d20cc622e55eb75b9f1a2546b77a08bd462b67030d0c89316c2c2349
+TERMUX_PKG_SHA256=979b9b8308a8d2d4a27c662ed50448c85f970c0fd4f5ce8b98e8da78c441f2bc
 # thunder-coding: don't try to autoupdate nodejs, that thing takes 2 whole hours to build for a single arch, and requires a lot of patch updates everytime. Also I run tests everytime I update it to ensure least bugs
 TERMUX_PKG_AUTO_UPDATE=false
 # Note that we do not use a shared libuv to avoid an issue with the Android
