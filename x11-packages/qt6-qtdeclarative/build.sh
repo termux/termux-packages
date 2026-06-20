49c34b7eaed375264d35359d85af2d9d8cab7e8c
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-20 12:53:47 +0530

    rebuild(x11/qt6-qtdeclarative): for cross tools to rebuild with Ubuntu 26.04

--- x11-packages/qt6-qtdeclarative/build.sh
+++ x11-packages/qt6-qtdeclarative/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Classes for QML and JavaScript languages"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.11.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/qtdeclarative-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=52e670f670b0304f534b24f98c47ceb8a41bb710464414ebc9527ec71cc86aa4
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase (>= ${TERMUX_PKG_VERSION})"
