0077b95c4a9fb430491a25e4ebad0f3fd7d732de
Robert Kirkman <rkirkman@termux.dev>
2026-06-20 11:57:44 -0500

    bump(x11/zen-browser): 1.21.3b
    - Fixes https://github.com/termux/termux-packages/issues/30251

--- x11-packages/zen-browser/build.sh
+++ x11-packages/zen-browser/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://zen-browser.app
 TERMUX_PKG_DESCRIPTION="Zen is a firefox-based browser with the aim of pushing your productivity to a new level!"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@sabamdarif"
-TERMUX_PKG_VERSION="1.21.2b"
+TERMUX_PKG_VERSION="1.21.3b"
 TERMUX_PKG_SRCURL="https://github.com/zen-browser/desktop/releases/download/${TERMUX_PKG_VERSION}/zen.source.tar.zst"
-TERMUX_PKG_SHA256=d35fb731b2cf7d44d27a1eb83f9fc5f20d6dc962679b36a2d20e635a9945746e
+TERMUX_PKG_SHA256=25e8ffde8402fb301145c1ac723ee6e06e246e5b5f9b7fd55b5af6dd2033f1f3
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
