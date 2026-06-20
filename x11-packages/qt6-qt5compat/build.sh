1d8f0d94369447dfde36dbea7aba3362623442e9
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-20 12:53:46 +0530

    rebuild(x11/qt6-qt5compat): for cross tools to rebuild with Ubuntu 26.04

--- x11-packages/qt6-qt5compat/build.sh
+++ x11-packages/qt6-qt5compat/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Qt6 Module that contains unsupported Qt 5 APIs"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.11.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/qt5compat-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=cfcb9fdaa051aad54b0e61b24ac5693b4887a86e07609f665fea67328a6f161b
 TERMUX_PKG_DEPENDS="libc++, libicu, qt6-qtbase (>= ${TERMUX_PKG_VERSION}), qt6-qtdeclarative (>= ${TERMUX_PKG_VERSION}), qt6-shadertools (>= ${TERMUX_PKG_VERSION})"
