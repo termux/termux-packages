6097f8657f2c2bab57886b07b143478c6f5e0d75
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-20 12:53:47 +0530

    rebuild(x11/qt6-qttools): for cross tools to rebuild with Ubuntu 26.04

--- x11-packages/qt6-qttools/build.sh
+++ x11-packages/qt6-qttools/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Qt Development Tools (Linguist, Assistant, Designer, etc
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="6.11.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/submodules/qttools-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=8e61835a679c93fa9c6065b142353c2071ba68e297898937c32a03777fcaf50d
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase (>= ${TERMUX_PKG_VERSION}), zstd"
