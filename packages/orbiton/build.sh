432b577e0fb563963eea7056776ee512109158b0
Maxython <mixython@gmail.com>
2026-06-22 13:39:04 +0300

    rebuild(main/orbiton): reassembly of alternatives

--- packages/orbiton/build.sh
+++ packages/orbiton/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Small, fast and limited text editor"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Alexander F. Rødseth <xyproto@archlinux.org>"
 TERMUX_PKG_VERSION="2.74.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xyproto/orbiton/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=5b4004fe86f2335e4b16c11a118d72fee29ecf115f24d76022454a2f8a8a1fa8
 TERMUX_PKG_AUTO_UPDATE=true
