7c14e0a8430a5ee8e40aec2f5098de579d11fe8b
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-25 17:19:45 +0530

    rebuild(main/blueprint-compiler): with python 3.14

--- packages/blueprint-compiler/build.sh
+++ packages/blueprint-compiler/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Markup language for GTK user interfaces"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.20.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/blueprint-compiler/${TERMUX_PKG_VERSION%.*}/blueprint-compiler-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=1f1ecc84bcd698902d422f7de83d39229a209dd3016f6d2c3b0ed0ab123f6891
 TERMUX_PKG_AUTO_UPDATE=true
