f4756b2bf9219af6e2ea1f77fd4b9d641c44b859
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-25 17:17:42 +0530

    rebuild(main/2ping): with python 3.14

--- packages/2ping/build.sh
+++ packages/2ping/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A bi-directional ping utility"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.6.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/rfinnie/2ping/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=c8352b6653c3194af1f869107655df3f98ab18b560e8bce86eabac08d73c72eb
 TERMUX_PKG_DEPENDS="python, python-pip"
