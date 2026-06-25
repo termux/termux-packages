2bacca56af205e891c2169a5a1dc51e2e762bb8d
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2026-06-25 12:55:13 -0400

    bump(main/aapt): update to Android 16.0.0 r4
    Use termux/android-build-tools' CMake build, add the sysprop subpackage, and stop shipping an SDK android.jar.

--- packages/aapt/aapt2.subpackage.sh
+++ packages/aapt/aapt2.subpackage.sh
@@ -1,3 +1,4 @@
 TERMUX_SUBPKG_INCLUDE="bin/aapt2"
 TERMUX_SUBPKG_DESCRIPTION="AAPT2 (Android Asset Packaging Tool)"
 TERMUX_SUBPKG_DEPENDS="abseil-cpp, libprotobuf"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=deps
