3f86f4d84cf5143a6fb8d29dc0f0ed6dfae1498d
Yaksh Bariya <thunder-coding@termux.dev>
2026-06-26 07:17:51 +0530

    rebuild(main/python-pyppmd): with python 3.14

--- packages/python-pyppmd/build.sh
+++ packages/python-pyppmd/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="PPM compression/decompression library"
 TERMUX_PKG_LICENSE="LGPL-2.1-only"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/miurahr/pyppmd/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=ca5328cfff8be532fe834f1844c281b503f3b069e6ccb6232971eaf82474dbd3
 TERMUX_PKG_DEPENDS="python, python-pip"
