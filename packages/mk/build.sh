ab099f6c393c0d52839872fb73d94b92a5dee9d9
Matheus Garcia <tateusg@protonmail.com>
2026-06-27 21:14:34 -0300

    mk: minor update

--- packages/mk/build.sh
+++ packages/mk/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=1.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/9fans/plan9port/archive/b36c747d62a690ffb9cff6f17fbe207b23aa4b84.tar.gz
 TERMUX_PKG_SHA256=b3c5dc0f2813c6f51178ada1765b6771910060ec3efaa8f05ea3f93c0334b5a8
-TERMUX_PKG_DEPENDS="libresolv-wrapper, libx11, freetype"
+TERMUX_PKG_DEPENDS="libresolv-wrapper"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
