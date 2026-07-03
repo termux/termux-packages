a85cdebacef49ac0562d00d34a246cb8836f8708
Robert Kirkman <rkirkman@termux.dev>
2026-07-03 07:18:06 -0500

    bump(main/libprotobuf): 35.1

--- packages/libprotobuf/build.sh
+++ packages/libprotobuf/build.sh
@@ -11,10 +11,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 # - update SHA256 checksum for $_PROTOBUF_ZIP in
 #     $TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_protobuf.sh
 # - ALWAYS bump revision of reverse dependencies and rebuild them.
-TERMUX_PKG_VERSION="2:33.1"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="2:35.1"
 TERMUX_PKG_SRCURL=https://github.com/protocolbuffers/protobuf/archive/refs/tags/v${TERMUX_PKG_VERSION#*:}.tar.gz
-TERMUX_PKG_SHA256=0c98bb704ceb4e68c92f93907951ca3c36130bc73f87264e8c0771a80362ac97
+TERMUX_PKG_SHA256=22775f9376938295efa2d59a59bde4cd075a42df5a9b4d27aa9b99fa6a413bd2
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="abseil-cpp, libc++, zlib"
 TERMUX_PKG_BREAKS="libprotobuf-dev, protobuf-dev, protobuf-static, libutf8-range"
