b3b9042d7d692e67308a517e0233522128459b85
Yaksh Bariya <thunder-coding@termux.dev>
2026-07-03 15:20:55 +0530

    rebuild(main/python-trash-cli): with python 3.14
    Although the previous commit was pushed after python 3.14 merge, CI debs
    were reused. As the earlier builds of the PR were against Python 3.13,
    the debs pushed to the repo were also the same. So needs to be rebuilt
    to ensure that it is built for Python 3.14

--- packages/python-trash-cli/build.sh
+++ packages/python-trash-cli/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command line trashcan (recycle bin) interface"
 TERMUX_PKG_LICENSE="GPL-2.0-only"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.24.5.26"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/andreafrancia/trash-cli/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=1d7dec1ad8f0264ceb1b0211d25fffee99c9409cd2e1d36dcc82ac5540f39ce5
 TERMUX_PKG_DEPENDS="python, python-psutil, python-pip"
