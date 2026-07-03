4ade117cf784c3a0b44dbcb0171c94781134ce31
Maxython <mixython@gmail.com>
2026-07-03 17:12:38 +0300

    bump(main/command-not-found): 3.5.0

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=3.4.1
-TERMUX_PKG_REVISION=17
+TERMUX_PKG_VERSION=3.5.0
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8dfec3aeb340d2359bf8c0b220497128f4b17bcb3967e4fd64f7a46773b40a3e
+TERMUX_PKG_SHA256=1c3681c9c5046a2f9ecaa250361e78cdc30ba2baabc0dfa11accfb6982721b13
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
