8aa20890475a2b0856b426afc147027b5f9750e0
Aditya Alok <alok@termux.dev>
2026-07-05 22:16:01 +0530

    revbump(main/ghc): for libllvm 21.2.8
    - added a note to revbump always when updating llvm
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/ghc/build.sh
+++ packages/ghc/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Glasgow Haskell Compiler"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.dev>"
 TERMUX_PKG_VERSION=9.12.2
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL="https://downloads.haskell.org/~ghc/$TERMUX_PKG_VERSION/ghc-$TERMUX_PKG_VERSION-src.tar.xz"
 TERMUX_PKG_SHA256=0e49cd5dde43f348c5716e5de9a5d7a0f8d68d945dc41cf75dfdefe65084f933
 TERMUX_PKG_DEPENDS="libiconv, libffi, libgmp, libandroid-posix-semaphore, ncurses"
@@ -16,6 +16,8 @@ TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_REPLACES="ghc-libs-static, ghc-libs"
 TERMUX_PKG_PROVIDES="ghc-libs, ghc-libs-static"
+
+# NOTE: Always revbump when updating libllvm.
 
 __setup_bootstrap_compiler() {
 	local version=9.10.1
