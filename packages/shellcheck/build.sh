6369cbe24027947a074932075e03c811d9c4d54d
Aditya Alok <alok@termux.dev>
2026-07-08 18:13:26 +0530

    enhance(main/shellcheck): enable `i686`
    - After https://github.com/termux/ghc-cross-tools/pull/6
    
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/shellcheck/build.sh
+++ packages/shellcheck/build.sh
@@ -3,15 +3,13 @@ TERMUX_PKG_DESCRIPTION="Shell script analysis tool"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION=0.11.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/ShellCheck-${TERMUX_PKG_VERSION}/ShellCheck-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=81a72e9c195788301f38e4b2e250ab916cf3778993d428786bfb2fac2a847400
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libffi, libgmp, libiconv"
 TERMUX_PKG_BUILD_DEPENDS="aosp-libs"
 TERMUX_PKG_AUTO_UPDATE=true
-# i686 is currently unsupported pending;
-# https://github.com/termux/ghc-cross-tools/pull/6
-TERMUX_PKG_EXCLUDED_ARCHES="i686"
 
 termux_step_pre_configure() {
 	chmod u+x ./striptests
