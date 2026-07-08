363da9924cf50bd397659738602347ecc51e4b3e
kyufie <imkyufie@gmail.com>
2026-07-08 20:32:01 +0700

    addpkg(main/mdf2iso): Small utility that converts MDF images to ISO format

---
+++ packages/mdf2iso/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://salsa.debian.org/debian/mdf2iso
+TERMUX_PKG_DESCRIPTION="Small utility that converts MDF images to ISO format"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.3.1"
+TERMUX_PKG_SRCURL="https://salsa.debian.org/debian/mdf2iso/-/archive/upstream/${TERMUX_PKG_VERSION}/mdf2iso-upstream-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=821181f95dbc1646b63f5b1035249cce30a50137a46b4d7ab6b98e4266cec089
+TERMUX_PKG_BUILD_IN_SRC=true
