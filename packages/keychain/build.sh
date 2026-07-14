c0b3e5ea24fffc98c6c8883ddf6180fa67c36e6b
Joshua "TomIO" Kahn <tom@termux.dev>
2026-07-15 01:46:06 +0200

    fix(main/keychain): exclude betas from auto-updates and update package metadata

--- packages/keychain/build.sh
+++ packages/keychain/build.sh
@@ -1,13 +1,14 @@
-TERMUX_PKG_HOMEPAGE=https://www.funtoo.org/Keychain
-TERMUX_PKG_DESCRIPTION="keychain ssh-agent front-end"
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_HOMEPAGE=https://kernel-seeds.org/projects/keychain/
+TERMUX_PKG_DESCRIPTION="Keychain manager for ssh-agent and gpg-agent"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.9.8"
-TERMUX_PKG_SRCURL=https://github.com/funtoo/keychain/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/danielrobbins/keychain/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=589cf55ae5c4b65af1d977d705beb319006efca5bcdda8352b8558d0dcff5a84
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_DEPENDS="dash, gnupg"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP='\d+\.\d+\.\d+(?!.beta)'
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
