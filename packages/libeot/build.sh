08ac4efb6562f86c0df395768521b74247b981bd
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-07-18 12:37:36 +0530

    addpkg(main/libeot): 0.01
    Co-authored-by: Robert Kirkman <rkirkman@termux.dev>

---
+++ packages/libeot/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/umanwizard/libeot
+TERMUX_PKG_DESCRIPTION="Library for parsing Embedded OpenType font files"
+TERMUX_PKG_LICENSE="MPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.01"
+TERMUX_PKG_SRCURL="https://dev-www.libreoffice.org/src/libeot-${TERMUX_PKG_VERSION}.tar.bz2"
+TERMUX_PKG_SHA256=cf5091fa8e7dcdbe667335eb90a2cfdd0a3fe8f8c7c8d1ece44d9d055736a06a
