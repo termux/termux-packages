2ca9cd14d8fac0877962c90323df926483ed0178
Robert Kirkman <rkirkman@termux.dev>
2026-07-18 12:13:33 -0500

    revbump(main/libasio): for `boost` 1.91.0

--- packages/libasio/build.sh
+++ packages/libasio/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Cross-platform C++ library for network and low-level I/O
 TERMUX_PKG_LICENSE="BSL-1.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.36.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/project/asio/asio/${TERMUX_PKG_VERSION}%20%28Stable%29/asio-${TERMUX_PKG_VERSION}.zip"
 TERMUX_PKG_SHA256=e4613701d74113dbc691f53aaf29266147b3d55249fa046c74a57a2c9ed2fb27
 TERMUX_PKG_BUILD_DEPENDS="boost, boost-headers, openssl"
