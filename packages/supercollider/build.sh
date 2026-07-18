316c90ae4bc991450750261e48a5d6c60a4e555a
Robert Kirkman <rkirkman@termux.dev>
2026-07-18 12:34:46 -0500

    revbump(main/supercollider): for `boost` 1.91.0

--- packages/supercollider/build.sh
+++ packages/supercollider/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A platform for audio synthesis and algorithmic compositi
 TERMUX_PKG_LICENSE="GPL-2.0-or-later, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@rene-descartes2021"
 TERMUX_PKG_VERSION="3.14.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/supercollider/supercollider/releases/download/Version-${TERMUX_PKG_VERSION}/SuperCollider-${TERMUX_PKG_VERSION}-Source.tar.bz2"
 TERMUX_PKG_SHA256=ee640c68777ae697682066ce5c4a8b7e56c5b223e76c79c13b5be5387ee55bb2
 TERMUX_PKG_DEPENDS="boost, fftw, jack2, libandroid-glob, libsndfile, libyaml-cpp, readline"
