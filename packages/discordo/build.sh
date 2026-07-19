e83c3e09f4d4c45a4d59b15425e03ac846802c5d
Joshua "TomIO" Kahn <tom@termux.dev>
2026-07-19 05:20:44 +0200

    enhance(main/discordo): enable auto-updates

--- packages/discordo/build.sh
+++ packages/discordo/build.sh
@@ -10,22 +10,71 @@ TERMUX_PKG_SHA256=030bfd86b518586ca520891c0af5b1b32c0285260d825a0ddccdd7eec5d920
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BUILD_DEPENDS="libx11"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout "$_COMMIT"
+# This auto update function is based on x11/wezterm-nightly
+termux_pkg_auto_update() {
+	local origin_url last_autoupdate
+	# Throttle auto updates to once every two weeks
+	local update_interval="$((2 * 7 * 86400))"
 
-	local version="$(git log -1 --format=%cs | sed 's/-/./g')"
-	if [[ "$version" != "$TERMUX_PKG_VERSION" ]]; then
-		echo -n "ERROR: The specified version \"$TERMUX_PKG_VERSION\""
-		echo " is different from what is expected to be: \"$version\""
-		return 1
+	# Get the git history
+	if origin_url="$(git config --get remote.origin.url)"; then
+		git fetch --quiet "${origin_url}" || {
+			echo "WARN: Unable to fetch '${origin_url}'"
+			echo "WARN: Skipping auto update for '$TERMUX_PKG_NAME'"
+			return
+		}
 	fi
 
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
+	# When was the last autoupdate to this package? (Unix epoch timestamp)
+	last_autoupdate="$(
+		git log \
+		--author="Termux Github Actions <contact@termux.dev>" \
+		-n1 \
+		--pretty=format:%at \
+		-- "$TERMUX_PKG_BUILDER_DIR/build.sh"
+	)"
+
+	# If we're still in the $update_interval skip the actual checking.
+	if (( last_autoupdate + update_interval > EPOCHSECONDS )); then
+		local t days hrs mins secs
+		(( t = EPOCHSECONDS - last_autoupdate, days = t/86400, t %= 86400, secs = t%60, t /= 60, mins = t%60, hrs = t/60 ))
+
+		printf 'INFO: Last updated %dd%dh%02dm%02ds ago.\n' "$days" "$hrs" "$mins" "$secs"
+		printf 'INFO: Which is less than the desired %sd minimum update interval.\n' "$(( update_interval / 86400 ))"
+		return
 	fi
+
+	# Get the latest commit's date and SHA.
+	local response latest_commit_date latest_commit_sha
+	response="$(curl -sL \
+		-H "X-GitHub-Api-Version: 2026-03-10" \
+		-H "Accept: application/vnd.github+json" \
+		"https://api.github.com/repos/ayn2op/discordo/commits/HEAD"
+	)"
+
+
+	read -rd' ' latest_commit_date latest_commit_sha < <(
+		jq -r '.commit.author.date, .sha' <<< "$response"
+	)
+
+	# Are they valid?
+	if ! date -d "${latest_commit_date:=null}" > /dev/null || [[ ! "${latest_commit_sha:=null}" =~ [0-9a-f]* ]]; then
+		{
+			echo "Unable to get commit date and SHA from ${TERMUX_PKG_SRCURL}."
+			echo "Date: $latest_commit_date"
+			echo "SHA:  $latest_commit_sha"
+			jq . <<< "$response"
+			return
+		} | tee "${GITHUB_STEP_SUMMARY:-/dev/null}" >&2
+	fi
+
+	# Massage the date into shape.
+	latest_commit_date="${latest_commit_date//-/.}" # 2026-07-16T03:01:40Z -> 2026.07.16T03:01:40Z
+	latest_commit_date="${latest_commit_date%T*}"   # 2026.07.16T03:01:40Z -> 2026.07.16
+
+	termux_pkg_upgrade_version "${latest_commit_date}+g${latest_commit_sha::8}"
 }
 
 termux_step_pre_configure() {
