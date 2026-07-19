884dd49bed4a26a16b705a918794167da5b476a5
Robert Kirkman <rkirkman@termux.dev>
2026-07-19 10:37:08 -0500

    fix(main/sendme): fix dns resolution error
    ```
    android context was not initialized
    ```
    
    Co-authored-by: Ranadeep Biswas <mail@rnbguy.at>

--- packages/sendme/build.sh
+++ packages/sendme/build.sh
@@ -7,11 +7,42 @@ LICENSE-MIT
 "
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.36.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/n0-computer/sendme/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=69897a9785c5b87c769417f77de8bff6e34e5391eed3d2df5d270599daf485ad
+TERMUX_PKG_DEPENDS="resolv-conf"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	export CARGO_PROFILE_RELEASE_PANIC=unwind
 	termux_setup_rust
+
+	cargo vendor
+	find ./vendor \
+		-mindepth 1 -maxdepth 1 -type d \
+		! -wholename ./vendor/rustls-platform-verifier \
+		! -wholename ./vendor/hickory-resolver \
+		! -wholename ./vendor/resolv-conf \
+		! -wholename ./vendor/netdev \
+		-exec rm -rf '{}' \;
+
+	find vendor/{rustls-platform-verifier,hickory-resolver} -type f -print0 | \
+		xargs -0 sed -i \
+		-e 's|"android"|"disabling_this_because_it_is_for_building_an_apk"|g' \
+		-e "s|ANDROID|DISABLING_THIS_BECAUSE_IT_IS_FOR_BUILDING_AN_APK|g" \
+		-e 's|"linux"|"android"|g'
+
+	find . -type f -print0 | \
+		xargs -0 sed -i \
+		-e "s|/etc|$TERMUX_PREFIX/etc|g"
+
+	cat >> Cargo.toml <<-EOF
+
+		[patch.crates-io]
+		rustls-platform-verifier = { path = "./vendor/rustls-platform-verifier" }
+		hickory-resolver = { path = "./vendor/hickory-resolver" }
+		resolv-conf = { path = "./vendor/resolv-conf" }
+		netdev = { path = "./vendor/netdev" }
+	EOF
 }
