6798f1bf2783202a4ed659de21e7b768a9e258dd
Robert Kirkman <rkirkman@termux.dev>
2026-07-19 13:04:08 -0500

    revbump(x11/hugin): for `boost` 1.91.0

--- x11-packages/hugin/build.sh
+++ x11-packages/hugin/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Toolchain to create panoramic images for every occasion"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2025.0.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/hugin/hugin-${TERMUX_PKG_VERSION}.tar.bz2"
 TERMUX_PKG_SHA256=7cf8eb33a6a8848cc7f816faf4bc88389228883d5513136dccb5cb243912ab79
 TERMUX_PKG_DEPENDS="boost, enblend, exiftool, exiv2, fftw, glew, glu, imath, libc++, libflann, liblz4, libpano13, libsqlite, libtiff, libvigra, libx11, littlecms, openexr, opengl, wxwidgets"
