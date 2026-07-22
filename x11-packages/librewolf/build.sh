0bd263f96e868eb5bfae6b6d7f9ff504dd1cad10
Robert Kirkman <rkirkman@termux.dev>
2026-07-22 13:20:25 -0500

    bump(x11/librewolf): 153.0-3
    - Fixes https://github.com/termux/termux-packages/issues/30671

--- x11-packages/librewolf/build.sh
+++ x11-packages/librewolf/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://librewolf.net/
 TERMUX_PKG_DESCRIPTION="A custom version of Firefox, focused on privacy, security and freedom."
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@3ls-it"
-TERMUX_PKG_VERSION="152.0.6-1"
+TERMUX_PKG_VERSION="153.0-3"
 TERMUX_PKG_SRCURL="https://codeberg.org/api/packages/librewolf/generic/librewolf-source/${TERMUX_PKG_VERSION}/librewolf-${TERMUX_PKG_VERSION}.source.tar.gz"
-TERMUX_PKG_SHA256=041930cb71eeb565b736ff8e6d044381314633b2c3c2b19ab674bf3e19f7f870
+TERMUX_PKG_SHA256=48ace24a1d6af6c65e3a663402e7b0af154ff7206ab76fbe5c08311a745e39e3
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
