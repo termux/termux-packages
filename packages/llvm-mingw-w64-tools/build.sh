8bccf01d3fe60042046adf8b6e5282b630a24416
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-07-25 00:19:33 +0800

    llvm-mingw-w64-tools: bump to 14.0.0

--- packages/llvm-mingw-w64-tools/build.sh
+++ packages/llvm-mingw-w64-tools/build.sh
@@ -2,13 +2,28 @@ TERMUX_PKG_HOMEPAGE=https://github.com/mstorsjo/llvm-mingw
 TERMUX_PKG_DESCRIPTION="MinGW-w64 tools for LLVM-MinGW"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_MAINTAINER="@licy183"
-TERMUX_PKG_VERSION=12.0.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=cc41898aac4b6e8dd5cffd7331b9d9515b912df4420a3a612b5ea2955bbeed2f
+_COMMIT=c4ae0f47b9adf0b6b91dcc906f5b1fbfe342bb42
+TERMUX_PKG_VERSION=14.0.0+dev
+TERMUX_PKG_SRCURL=git+https://github.com/mingw-w64/mingw-w64
+TERMUX_PKG_SHA256=746fed5ddd005dfb4a062eeaa939b71deecdfc6460ba8b55541d354fee7af6c0
+TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BUILD_DEPENDS="llvm-mingw-w64-ucrt"
 TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	git fetch --unshallow
+	git checkout $_COMMIT
+
+	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
+	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
+		termux_error_exit <<-EOF
+		Checksum mismatch for source files.
+		Expected: $TERMUX_PKG_SHA256
+		Actual:   ${s% *}
+		EOF
+	fi
+}
 
 termux_step_configure() {
 	:
@@ -45,10 +60,10 @@ termux_step_make() {
 	# The build above produced x86_64-w64-mingw32-widl, add symlinks to it
 	# with other prefixes.
 	local _arch
-	for _arch in aarch64 armv7 i686; do
+	for _arch in aarch64 arm64ec armv7 i686; do
 		ln -sf x86_64-w64-mingw32-widl "$_INSTALL_PREFIX/bin/$_arch-w64-mingw32-widl"
 	done
-	for _arch in aarch64 armv7 i686 x86_64; do
+	for _arch in aarch64 arm64ec armv7 i686 x86_64; do
 		ln -sf x86_64-w64-mingw32-widl "$_INSTALL_PREFIX/bin/$_arch-w64-mingw32uwp-widl"
 	done
 }
@@ -59,7 +74,7 @@ termux_step_make_install() {
 
 	# Symlinks tools to $PREFIX/bin
 	local _tool
-	for _tool in gendef {aarch64,armv7,i686,x86_64}-w64-mingw32{,uwp}-widl; do
+	for _tool in gendef {aarch64,arm64ec,armv7,i686,x86_64}-w64-mingw32{,uwp}-widl; do
 		ln -sfr "$_INSTALL_PREFIX/bin/$_tool" "$TERMUX_PREFIX/bin/$_tool"
 	done
 }
