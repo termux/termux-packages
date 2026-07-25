12118ba60d4724138062ae5e835eced26fd62594
Aditya Alok <alok@termux.dev>
2026-07-25 23:28:59 +0530

    bump(main/cabal-install): 3.16.1.0
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/cabal-install/build.sh
+++ packages/cabal-install/build.sh
@@ -2,14 +2,13 @@ TERMUX_PKG_HOMEPAGE="https://www.haskell.org/cabal/"
 TERMUX_PKG_DESCRIPTION="The command-line interface for Haskell-Cabal and Hackage"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.dev>"
-TERMUX_PKG_VERSION=3.14.1.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=3.16.1.0
 TERMUX_PKG_SRCURL="https://hackage.haskell.org/package/cabal-install-${TERMUX_PKG_VERSION}/cabal-install-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=f11d364ab87fb46275a987e60453857732147780a8c592460eec8a16dbb6bace
+TERMUX_PKG_SHA256=9d27bc22989f3933486a7bba6ac0a2d8fef16891bf46a973f4d80f429ae95120
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_SUGGESTS="ghc, dnsutils"
-TERMUX_PKG_DEPENDS="libffi, libiconv, libgmp, zlib, libandroid-posix-semaphore"
+TERMUX_PKG_DEPENDS="libffi, libiconv, libgmp, zlib, libandroid-posix-semaphore, libandroid-utimes"
 TERMUX_PKG_BUILD_DEPENDS="aosp-libs"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-f-native-dns --ghc-options=-optl-landroid-posix-semaphore"
 
@@ -18,7 +17,7 @@ termux_step_post_configure() {
 	mv splitmix{-*,}
 
 	for f in "$TERMUX_PKG_BUILDER_DIR"/splitmix-patches/*.patch; do
-		patch --silent -p1 -d splitmix < "$f"
+		patch --silent -p1 -d splitmix <"$f"
 	done
 
 	cat <<-EOF >>cabal.project.local
