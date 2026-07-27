fb53b0bb7c30f6058f375112cd237fa0dfbb0926
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-07-27 11:26:16 +0300

    fix(libbsd): update automake scripts before configuring libbsd

--- packages/libbsd/build.sh
+++ packages/libbsd/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="utility functions from BSD systems"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.11.7"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://libbsd.freedesktop.org/releases/libbsd-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=9baa186059ebbf25c06308e9f991fda31f7183c0f24931826d83aa6abd8a0261
 TERMUX_PKG_DEPENDS="libmd"
@@ -12,6 +12,8 @@ TERMUX_PKG_REPLACES="libbsd-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
+	# Update build scripts from automake 1.16 to whatever automake version is currently installed
+	autoreconf -fi
 	# Fix linker script error
 	LDFLAGS+=" -Wl,--undefined-version"
 }
