ee4658e77df159a6e843a632fadace9ca507dbb5
CVFA1 <cavifeal@duck.com>
2026-08-01 20:55:59 -0300

    addpkg(packages/ctranslate2): 4.8.1

---
+++ packages/ctranslate2/python-ctranslate2.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/ct2-*-converter
+lib/python*/site-packages/ctranslate2/
+lib/python*/site-packages/ctranslate2-*.dist-info/
+"
+TERMUX_SUBPKG_DESCRIPTION="Python bindings for CTranslate2"
+TERMUX_SUBPKG_DEPENDS="python, python-numpy, python-pip"
+TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
