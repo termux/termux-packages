cdf3dd7e6369d9109eaa2c4fd0ce71ae0c28a950
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-02 16:02:48 +0000

    bump(x11/openimageio): 3.1.16.0
    Change source url to new redirected GitHub repository.

--- x11-packages/openimageio/build.sh
+++ x11-packages/openimageio/build.sh
@@ -1,11 +1,10 @@
-TERMUX_PKG_HOMEPAGE=http://www.openimageio.org/
+TERMUX_PKG_HOMEPAGE=https://openimageio.org
 TERMUX_PKG_DESCRIPTION="A library for reading and writing images, including classes, utilities, and applications"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.1.15.0"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL="https://github.com/OpenImageIO/oiio/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=7837a2058638dd9caa352d6936677dba7883105c6db6caf6fb58ba40fa2c12c8
+TERMUX_PKG_VERSION="3.1.16.0"
+TERMUX_PKG_SRCURL="https://github.com/AcademySoftwareFoundation/OpenImageIO/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=5af7221be05bbe69d7ec4eb74b5656c6d15b8d1a332410d6af937b1df48647c0
 # configure-time error if ptex and ptex-static are not both installed
 TERMUX_PKG_DEPENDS="boost, dcmtk, ffmpeg, fmt, freetype, glew, imath, libc++, libhdf5, libheif, libjpeg-turbo, libjxl, libpng, libraw, libtbb, libtiff, libwebp, libyaml-cpp, opencolorio, opencv, openexr, openjpeg, openvdb, ptex, pybind11, python, qt6-qtbase"
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, fontconfig, libjpeg-turbo-static, libpugixml, libxrender, mesa, ptex-static, robin-map"
