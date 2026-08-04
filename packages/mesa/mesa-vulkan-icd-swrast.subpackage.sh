a66b7a88006184eec86bfaaf740d4a6d8d09a21c
Robert Kirkman <rkirkman@termux.dev>
2026-08-04 02:14:06 -0500

    fix(main/mesa-vulkan-icd-swrast): mark as depending on `libllvm`
    - See https://github.com/termux/termux-packages/pull/30848#issuecomment-5175673767

--- packages/mesa/mesa-vulkan-icd-swrast.subpackage.sh
+++ packages/mesa/mesa-vulkan-icd-swrast.subpackage.sh
@@ -1,6 +1,6 @@
 TERMUX_SUBPKG_DESCRIPTION="Mesa's Swrast Vulkan ICD"
 TERMUX_SUBPKG_DEPEND_ON_PARENT=false
-TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libc++, libdrm, libx11, libxcb, libxshmfence, libwayland, ncurses, vulkan-loader-generic, zlib, zstd"
+TERMUX_SUBPKG_DEPENDS="libandroid-shmem, libc++, libdrm, libllvm (<< $TERMUX_LLVM_NEXT_MAJOR_VERSION), libx11, libxcb, libxshmfence, libwayland, ncurses, vulkan-loader-generic, zlib, zstd"
 TERMUX_SUBPKG_INCLUDE="
 lib/libvulkan_lvp.so
 share/vulkan/icd.d/lvp_icd.*.json
