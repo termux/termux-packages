500729edcd1821d88ea375bfa51dac93cd65149b
Robert Kirkman <rkirkman@termux.dev>
2026-08-05 09:03:53 -0500

    bump(main/mupdf): 1.28.2
    - Fixes https://github.com/termux/termux-packages/issues/30602
    
    - Unvendor `brotli`
    
    - Enable Python
    
    - Enable installing C++ libraries
    
    - Disable static libraries; causing errors now and also, were added in order to try to get `python-pymupdf`, but now I have gotten `python-pymupdf` seemingly using only the shared libraries, so the static libraries are no longer needed. https://github.com/termux/termux-packages/issues/7583
      - Reverts https://github.com/termux/termux-packages/pull/8670

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -2,13 +2,26 @@ TERMUX_PKG_HOMEPAGE=https://mupdf.com/
 TERMUX_PKG_DESCRIPTION="Lightweight PDF and XPS viewer (library)"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.24.10"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.gz
-TERMUX_PKG_SHA256=939285b5f97caf770fd46cbe7e6cc3a695ab19bb5bfaf5712904549cef390b7b
-TERMUX_PKG_DEPENDS="freetype, gumbo-parser, harfbuzz, jbig2dec, leptonica, libc++, libjpeg-turbo, openjpeg, tesseract, zlib"
-TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX build=release libs shared=yes tesseract=yes V=1"
+TERMUX_PKG_VERSION="1.28.2"
+TERMUX_PKG_SRCURL="https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.gz"
+TERMUX_PKG_SHA256=44075a84e329db55b9bef5f342a70fd26d69e48ad1d33cb89d9664581c641156
+TERMUX_PKG_DEPENDS="brotli, freetype, gumbo-parser, harfbuzz, jbig2dec, leptonica, libandroid-shmem, libc++, libjpeg-turbo, openjpeg, tesseract, zlib"
+TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="clang"
+# clang crash occurs if removed
+# Arch Linux also sets this
+TERMUX_PKG_MAKE_PROCESSES=1
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+prefix=$TERMUX_PREFIX
+pydir=$TERMUX_PYTHON_HOME/site-packages
+build=release
+libs
+c++
+shared=yes
+tesseract=yes
+VENV_FLAG=
+V=1
+"
 
 # Automatic updates break k2pdfopt on regular basis
 TERMUX_PKG_AUTO_UPDATE=false
@@ -20,14 +33,30 @@ termux_step_post_get_source() {
 }
 
 termux_step_pre_configure() {
-	rm -rf thirdparty/{freeglut,freetype,harfbuzz,jbig2dec,leptonica,libjpeg,openjpeg,tesseract,zlib}
+	rm -rf thirdparty/{brotli,freeglut,freetype,harfbuzz,jbig2dec,leptonica,libjpeg,openjpeg,tesseract,zlib}
 	export USE_SYSTEM_LIBS=yes
-	LDFLAGS+=" -llog"
+	LDFLAGS+=" -llog -landroid-shmem"
+
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		patch="$TERMUX_PKG_BUILDER_DIR/libclang.diff"
+		echo "Applying patch: $(basename "$patch")"
+		sed -e "s%\@TERMUX_HOST_LLVM_MAJOR_VERSION\@%${TERMUX_HOST_LLVM_MAJOR_VERSION}%g" \
+			"$patch" | patch --silent -p1
+		# workaround for problem with cross-compiling using pipcl
+		local python_cross_bindir="$(dirname "$(dirname "$(command -v cross-pip)")")/cross/bin"
+		local python_config="$python_cross_bindir/python$TERMUX_PYTHON_VERSION-config"
+		# not complete implementation of python3.XX-config - only implements --includes
+		if [[ ! -f "$python_config" ]]; then
+			echo '#!/bin/bash' > "$python_config"
+			echo "echo '-I$TERMUX_PREFIX/include/python$TERMUX_PYTHON_VERSION'" > "$python_config"
+			chmod +x "$python_config"
+		fi
+	fi
 }
 
 termux_step_post_make_install() {
-	TERMUX_PKG_EXTRA_MAKE_ARGS="${TERMUX_PKG_EXTRA_MAKE_ARGS/shared=yes/}"
-	termux_step_make
-	install -Dm600 -t $TERMUX_PREFIX/lib build/release*/libmupdf{-third,}.a
-	ln -sf $TERMUX_PREFIX/lib/libmupdf.so.* $TERMUX_PREFIX/lib/libmupdf.so
+	# needs to be after everything else because it needs to find
+	# libmupdfcpp.so inside $TERMUX_PREFIX/lib
+	make $TERMUX_PKG_EXTRA_MAKE_ARGS python
+	make $TERMUX_PKG_EXTRA_MAKE_ARGS install-shared-python
 }
---
+++ packages/mupdf/python-mupdf.subpackage.sh
@@ -0,0 +1,5 @@
+TERMUX_SUBPKG_DESCRIPTION="Lightweight PDF and XPS viewer (Python bindings)"
+TERMUX_SUBPKG_INCLUDE="
+lib/python*
+"
+TERMUX_SUBPKG_DEPENDS="python"
