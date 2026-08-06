f410745f1f4ed4b8311344779f90ba2efa24ee95
PLGCesar <cesarlazy1@gmail.com>
2026-08-06 16:35:42 -0300

    utilis-in-c: new package

---
+++ packages/utils-in-c/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/PLGCesar/utils-in-c"
+TERMUX_PKG_DESCRIPTION="A collection of small C utilities including calc and passgen"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@PLGCesar"
+TERMUX_PKG_VERSION="1.0.0"
+TERMUX_PKG_SRCURL="https://github.com/PLGCesar/utils-in-c/archive/refs/tags/v${TERMUX_PK>
+TERMUX_PKG_SHA256="d29aab0b3e58528333b1a8506c7f79bb6ecb4cd04bd7343b54196995e3418c6c"
+TERMUX_PKG_BUILD_IN_SRC=true
