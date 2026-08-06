ec114f2902048e9aa5d6a57c78ec1b811d7dbf63
Samir Hossains <samirhossain34250@gmail.com>
2026-08-06 18:51:09 +0600

    new package: moviebox-tui

---
+++ packages/moviebox-tui/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/mesamirh/MovieBox-Tui
+TERMUX_PKG_DESCRIPTION="Terminal client for streaming movies, series, and anime"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@mesamirh"
+TERMUX_PKG_VERSION=0.1.10
+TERMUX_PKG_SRCURL=https://github.com/mesamirh/MovieBox-Tui/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=212c8ee4e390b06042ee8b459539217ca6c1858aebbb98ced38f83ad91f04578
+TERMUX_PKG_DEPENDS="openssl, pkg-config"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+    termux_setup_rust
+    cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
+}
+
+termux_step_make_install() {
+    install -Dm700 -t $TERMUX_PREFIX/bin target/${CARGO_TARGET_NAME}/release/moviebox-tui
+}
