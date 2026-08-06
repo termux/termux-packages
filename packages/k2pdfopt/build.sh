e7cbd05f4b1e4f4042cc55609a1848db14ce68a2
Joshua "TomIO" Kahn <tom@termux.dev>
2026-08-06 04:45:56 +0200

    chore(main/k2pdfopt): fix license (GPL-3.0)

--- packages/k2pdfopt/build.sh
+++ packages/k2pdfopt/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.willus.com/k2pdfopt/
 TERMUX_PKG_DESCRIPTION="A tool that optimizes PDF files for viewing on mobile readers"
-TERMUX_PKG_LICENSE="AGPL-V3"
+TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.55"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=6
 TERMUX_PKG_SRCURL="https://www.willus.com/k2pdfopt/src/k2pdfopt_v${TERMUX_PKG_VERSION}_src.zip"
 TERMUX_PKG_SHA256=3e78b4c7dd6227fde12138fd2468dd13c0c45b5251592a4f0aac67fd139ab953
 TERMUX_PKG_DEPENDS="djvulibre, gsl, leptonica, libjasper, libjpeg-turbo, libpng, mupdf, zlib"
