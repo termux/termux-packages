d005cf77d706cbf596c071b5c2fe93b45f598f73
Aditya Alok <alok@termux.dev>
2026-08-09 04:07:11 +0530

    enhance(main/nano): enable libmagic support
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -3,15 +3,15 @@ TERMUX_PKG_DESCRIPTION="Small, free and friendly text editor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="9.2"
-TERMUX_PKG_SRCURL=https://nano-editor.org/dist/latest/nano-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://nano-editor.org/dist/latest/nano-$TERMUX_PKG_VERSION.tar.xz"
 TERMUX_PKG_SHA256=05ecb99247b782e8a5b3a25ed4101dd034b0236902f7449bc9795b717642f7e9
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+TERMUX_PKG_DEPENDS="libandroid-support, ncurses, libmagic"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_header_glob_h=no
 ac_cv_header_pwd_h=no
 gl_cv_func_strcasecmp_works=yes
---disable-libmagic
 --enable-utf8
 --with-wordbounds
 "
@@ -21,5 +21,5 @@ TERMUX_PKG_RM_AFTER_INSTALL="bin/rnano share/man/man1/rnano.1 share/nano/man-htm
 termux_step_post_make_install() {
 	# Configure nano to use syntax highlighting:
 	NANORC=$TERMUX_PREFIX/etc/nanorc
-	echo "include \"$TERMUX_PREFIX/share/nano/*nanorc\"" > "$NANORC"
+	echo "include \"$TERMUX_PREFIX/share/nano/*nanorc\"" >"$NANORC"
 }
