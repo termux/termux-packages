8f6005e33256277e72d165c91eeec523d31f316f
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-08 10:04:49 +0000

    bump(main/libwebsockets): 5.0.0
    Disable werror for some compiler warnings in 32 bit environment.
    Disable http3 because gnutls is required for that. ttyd requires openssl.

--- packages/libwebsockets/build.sh
+++ packages/libwebsockets/build.sh
@@ -1,17 +1,19 @@
 TERMUX_PKG_HOMEPAGE=https://libwebsockets.org
 TERMUX_PKG_DESCRIPTION="Lightweight C websockets library"
-TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.5.8"
-TERMUX_PKG_SRCURL=https://github.com/warmcat/libwebsockets/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b6ade658f4af3a823d0dc806ae5ef0623f0f4f5e2aeb895a0f77c4783840c30e
+TERMUX_PKG_VERSION="5.0.0"
+TERMUX_PKG_SRCURL="https://github.com/warmcat/libwebsockets/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=f853c6582101cfcee3a5a9e28ae92ab19d9735c5f31f0bb2e9794b5106123962
 TERMUX_PKG_DEPENDS="openssl, libcap, libuv, zlib"
 TERMUX_PKG_BREAKS="libwebsockets-dev"
 TERMUX_PKG_REPLACES="libwebsockets-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_INSTALL_LIBDIR=$TERMUX__PREFIX__LIB_SUBDIR
 -DCMAKE_INSTALL_INCLUDEDIR=$TERMUX__PREFIX__INCLUDE_SUBDIR
+-DDISABLE_WERROR=ON
 -DLWS_WITHOUT_TESTAPPS=ON
+-DLWS_WITH_HTTP3=OFF
 -DLWS_WITH_STATIC=OFF
 -DLWS_WITH_LIBUV=ON
 -DLWS_WITHOUT_EXTENSIONS=OFF
@@ -19,17 +21,18 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 TERMUX_PKG_RM_AFTER_INSTALL="lib/pkgconfig/libwebsockets_static.pc"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
-TERMUX_PKG_UPDATE_TAG_TYPE="latest-regex"
+# Upstream started making a ton of tags with non-standard versions like:
+# `sai-9e1c70ac4` this messes with the regex update method, so use repology.
+TERMUX_PKG_UPDATE_METHOD="repology"
 
 termux_step_post_get_source() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=21
+	local _SOVERSION=22
 
-	local v=$(sed -En 's/^set\(SOVERSION\s+"?([0-9]+).*/\1/p' \
-			CMakeLists.txt)
-	if [ "${v}" != "${_SOVERSION}" ]; then
+	local v
+	v="$(sed -En 's/^set\(SOVERSION\s+"?([0-9]+).*/\1/p' CMakeLists.txt)"
+	if [[ "${v}" != "${_SOVERSION}" ]]; then
 		termux_error_exit "SOVERSION guard check failed. Expected ${_SOVERSION}, got ${v}."
 	fi
 }
