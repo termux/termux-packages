a6896756e6b4dc08c29ba5021354dd054fef7338
Aditya Alok <alok@termux.dev>
2026-08-10 20:21:59 +0530

    enhance(main/yara): enable libmagic support
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/yara/build.sh
+++ packages/yara/build.sh
@@ -3,13 +3,15 @@ TERMUX_PKG_DESCRIPTION="Tool aimed at helping malware researchers to identify an
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.5.8"
-TERMUX_PKG_SRCURL=https://github.com/VirusTotal/yara/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/VirusTotal/yara/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=c322414975ff6f701149856613afdcd92a7e6939c284c798ae3c85618197efaa
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
-TERMUX_PKG_DEPENDS="file, openssl, libandroid-posix-semaphore"
+TERMUX_PKG_DEPENDS="libandroid-posix-semaphore, libmagic, openssl"
 TERMUX_PKG_BREAKS="yara-dev"
 TERMUX_PKG_REPLACES="yara-dev"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-magic"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-posix-semaphore"
