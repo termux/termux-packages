3666f4a71eb1e11d110c9b873b54c10f20c8b39f
nguynkhn <nguynkhan479@gmail.com>
2026-08-13 16:47:33 +0700

    bump(main/manim): 0.21.0

--- packages/manim/build.sh
+++ packages/manim/build.sh
@@ -2,13 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://manim.community
 TERMUX_PKG_DESCRIPTION="A community-maintained Python framework for creating mathematical animations"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Nguyen Khanh @nguynkhn"
-TERMUX_PKG_VERSION="0.20.1"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="0.21.0"
 TERMUX_PKG_SRCURL="https://github.com/ManimCommunity/manim/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=384791b838ae5a6f408dcf4eb86e7fe04957fd2910fb26038690457a44204caf
+TERMUX_PKG_SHA256=d4657f29981c745a124090a70e302ed28208f809aee467a9735eed8f92bd0e28
 TERMUX_PKG_DEPENDS="cmake, ffmpeg, libcairo, pango, xorgproto, python-numpy, python-pillow, python-pip, pycairo, python-scipy, python-skia-pathops"
 TERMUX_PKG_SUGGESTS="texlive-installer"
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="poetry"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
+
+termux_step_make() {
+	termux_setup_rust
+}
