a8a4a9919526d9a532faec841b726a705709247a
TheMightyArie <arpaga61@gmail.com>
2026-08-15 20:39:32 +0300

    addpkg(main/luau): 0.734
    - Fixes #30283

---
+++ packages/luau/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/luau-lang/luau
+TERMUX_PKG_DESCRIPTION="A small, fast, and embeddable programming language based on Lua with a gradual type system."
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="
+LICENSE.txt
+extern/isocline/LICENSE
+"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.734"
+TERMUX_PKG_SRCURL="https://github.com/luau-lang/luau/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=cb55a891226d8c70284e22eb9281cc2b4496c709a4050f52aaa18a355fe7b1a3
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DLUAU_BUILD_TESTS=OFF
+"
+
+termux_step_make_install() {
+	install -Dm755 -t "$TERMUX_PREFIX/bin" luau luau-*
+}
