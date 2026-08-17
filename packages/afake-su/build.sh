4c0ff32cd178192a80c8ca08be86845888938db9
httpspycharmhelpers <cgx_0925@foxmail.com>
2026-08-17 22:16:06 +0800

    fix: update SHA256 for new source zip and enable build deps

--- packages/afake-su/build.sh
+++ packages/afake-su/build.sh
@@ -6,8 +6,8 @@ TERMUX_PKG_VERSION="1.0.0"
 # 使用你上传的 Release 附件 zip 的链接
 TERMUX_PKG_SRCURL="https://github.com/httpspycharmhelpers/aFakeSU/releases/download/1.0.0/aFakeSU-src.zip"
 # SHA256 先留空，第一次构建时会提示正确值
-TERMUX_PKG_SHA256="sha256:a2f07f5cc752730a79c3ca174ff0f2bf48ffd6a6312e9227e87fc90a13a0edd3"
-#TERMUX_PKG_BUILD_DEPENDS="clang, make, binutils"
+TERMUX_PKG_SHA256="a2f07f5cc752730a79c3ca174ff0f2bf48ffd6a6312e9227e87fc90a13a0edd3"
+TERMUX_PKG_BUILD_DEPENDS="clang, make, binutils"
 TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_configure() {
     ls -l "$TERMUX_PKG_SRCDIR"
