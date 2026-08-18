43b63dd1d7877ec2d617761fd8bccbd91bf48ab4
Joshua "TomIO" Kahn <tom@termux.dev>
2026-08-18 10:10:20 +0100

    fix(main/rsync): backport upstream directory traversal fix

--- packages/rsync/build.sh
+++ packages/rsync/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast incremental file transfer utility"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Joshua Kahn <tom@termux.dev>"
 TERMUX_PKG_VERSION="3.5.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://rsync.samba.org/ftp/rsync/src/rsync-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=c7ffd1ef653e99540f661e47cb00b7f9cad1ee6b972399b16f93d672656e0d33
 TERMUX_PKG_DEPENDS="libiconv, liblz4, libpopt, openssh | dropbear, openssl, openssl-tool, xxhash, zlib, zstd"
