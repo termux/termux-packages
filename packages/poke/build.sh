824ecaeb63455f62a067d80f6e81dc650429b978
Robert Kirkman <rkirkman@termux.dev>
2026-08-18 01:55:18 -0500

    bump(main/poke): 5.0
    - Fixes https://github.com/termux/termux-packages/issues/31117

--- packages/poke/build.sh
+++ packages/poke/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=http://www.jemarch.net/poke.html
 TERMUX_PKG_DESCRIPTION="Interactive, extensible editor for binary data."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.3"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/poke/poke-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a84cb9175d50d45a411f2481fd0662b83cb32ce517316b889cfb570819579373
+TERMUX_PKG_VERSION="5.0"
+TERMUX_PKG_SRCURL="https://mirrors.kernel.org/gnu/poke/poke-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=6873d59abe821c8111b88623ea7ad9e090892fa95c75562606dd88374e2f5b8f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="gettext, libgc, ncurses, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -16,3 +15,7 @@ gl_cv_func_strcasecmp_works=yes
 --disable-threads
 --with-sysroot=$TERMUX_BASE_DIR
 "
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -lm"
+}
