662fc6482bd431de6211d5e9949efb0e10c5383f
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-19 19:32:55 +0000

    bump(x11/nextcloud-client): 34.0.2

--- x11-packages/nextcloud-client/build.sh
+++ x11-packages/nextcloud-client/build.sh
@@ -1,18 +1,38 @@
 TERMUX_PKG_HOMEPAGE=https://nextcloud.com/
-TERMUX_PKG_DESCRIPTION="Command-line client tool for Nextcloud."
+TERMUX_PKG_DESCRIPTION="Nextcloud desktop client"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="34.0.1"
+TERMUX_PKG_VERSION="34.0.2"
 TERMUX_PKG_SRCURL="https://github.com/nextcloud/desktop/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=0cad3ac4b6a90a7a720a75a8cb52902114b5a13796b5caadc37da71c6f7b1ece
+TERMUX_PKG_SHA256=769a2f2eff446a1cfbe344903a3974243df707a8a3b8cb4a915f1dd9c9a6d7ea
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="dbus, dbus-glib, libc++, libp11, openssl, qtkeychain, qt6-qtbase, inotify-tools, libsqlite, kdsingleapplication, kf6-karchive, qt6-qtwebsockets, qt6-qtsvg, qt6-qt5compat"
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_DEPENDS="dbus, dbus-glib, libc++, libp11, openssl, qtkeychain, qt6-qtbase, inotify-tools, libsqlite, kdsingleapplication, kf6-karchive, kf6-kdbusaddons, kf6-kguiaddons, qt6-qtwebsockets, qt6-qtsvg, qt6-qt5compat"
 TERMUX_PKG_BUILD_DEPENDS="qt6-qtbase-cross-tools, qt6-qttools-cross-tools, pkg-config, qt6-qttools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DBUILD_GUI=OFF
 -DBUILD_SHELL_INTEGRATION=OFF
--DBUILD_WITH_WEBENGINE=OFF
 -DBUILD_UPDATER=OFF
+-DCMAKE_SYSTEM_NAME=Linux
 -DTOKEN_AUTH_ONLY=OFF
 -DBUILD_TESTING=OFF
 "
+
+termux_step_host_build() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		termux_download_ubuntu_packages librsvg2-bin librsvg2-2
+	fi
+}
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper_bin"
+		mkdir -p "${_WRAPPER_BIN}"
+		cat <<-EOF >"${_WRAPPER_BIN}/rsvg-convert"
+			#!/bin/sh
+			export LD_LIBRARY_PATH="${TERMUX_PKG_HOSTBUILD_DIR}/ubuntu_packages/usr/lib/x86_64-linux-gnu"
+			exec "${TERMUX_PKG_HOSTBUILD_DIR}/ubuntu_packages/usr/bin/rsvg-convert" "\$@"
+		EOF
+		chmod +x "${_WRAPPER_BIN}/rsvg-convert"
+		export PATH="${_WRAPPER_BIN}:${PATH}"
+	fi
+}
