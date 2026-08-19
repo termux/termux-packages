d2a2d5430b24af23e03ea3d2b914d124c0fb02e0
Robert Kirkman <rkirkman@termux.dev>
2026-08-19 14:12:06 -0500

    revbump(x11/lxqt-panel): for Qt 6.11.2

--- x11-packages/lxqt-panel/build.sh
+++ x11-packages/lxqt-panel/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The LXQt desktop panel"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.4.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/lxqt/lxqt-panel/releases/download/${TERMUX_PKG_VERSION}/lxqt-panel-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=7828d7e8fabf66ef1491e50b609b8eb041d74c433554f7d073ac07af26843d02
 TERMUX_PKG_DEPENDS="kf6-kwindowsystem, layer-shell-qt, libc++, libdbusmenu-lxqt, liblxqt, libqtxdg, libsysstat, libxcb, libxkbcommon, libxtst, libx11, lxqt-globalkeys, lxqt-menu-data, pulseaudio, qt6-qtbase, xcb-util, xcb-util-image"
