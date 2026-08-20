b6fe7a2fa5edd6280cea1fa994ac48ae75a9cdff
Pankaj Kumar <pankaj.pal9@gmail.com>
2026-08-20 17:54:44 +0530

    bump(main/tgpt): 2.13.0
    - Add X11 clipboard support
    
    Closes #30737

--- packages/tgpt/build.sh
+++ packages/tgpt/build.sh
@@ -2,14 +2,35 @@ TERMUX_PKG_HOMEPAGE=https://github.com/aandrew-me/tgpt
 TERMUX_PKG_DESCRIPTION="AI Chatbots in terminal without needing API keys"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.11.1"
+TERMUX_PKG_VERSION="2.13.0"
 TERMUX_PKG_SRCURL=https://github.com/aandrew-me/tgpt/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e7a02a0d40b7a6761e5e4550210db04baca7c6113430c3efc9643b9ebca01e32
+TERMUX_PKG_SHA256=950af3b39f5870d0659c88ae195b46553580e5d96c31f2230b7eb159d774e7b4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
+termux_step_post_get_source() {
+	# Vendor and sanitize go modules ahead of patching step.
 	termux_setup_golang
+	go mod tidy
+	go mod vendor
+
+	# golang's "mobile" module contains both code
+	# related to SurfaceFlinger(ANativeWindow[For Building an APK]),
+	# and also X11-related code that upstream connects to "linux && !android".
+	# apply the pattern "treat Android as linux" here,
+	# to force the disabling of the SurfaceFlinger-dependent
+	# code and the enabling of the X11-related code,
+	# fixing the error when building using NDK r28c:
+	# android.c:171:52: error: incompatible pointer to integer conversion
+	# passing 'ANativeWindow *' (aka 'struct ANativeWindow *') to parameter
+	# of type 'EGLNativeWindowType' (aka 'unsigned long') [-Wint-conversion]
+	find \
+		vendor/golang.org/x/mobile \
+		-type f -print0 | \
+		xargs -0 -n 1 sed -i \
+		-e 's|build android|build disabling_this_because_it_is_for_building_an_apk|g' \
+		-e 's|linux && !android|linux|g' \
+		-e 's|linux,!android|linux|g'
 }
 
 termux_step_make() {
