49343e3c26bb7ea3a4c9d0afad32c9c128f07460
Frostre_ <n9043395@gmail.com>
2026-08-21 14:59:14 +0200

    fix(prep): correct typos in build.sh

--- packages/prep/build.sh
+++ packages/prep/build.sh
@@ -1,12 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/frostre1997/prep
-TERMUX_PKG_DESCRIPTION="Repository health auditor and build error parser"
+TERMUX_PKG_DESCRIPTION="a repository health auditor and build error parser"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="frostre1997 <n9043395@gmail.com>"
 TERMUX_PKG_VERSION="0.100.0"
 TERMUX_PKG_SRCURL=https://github.com/frostre1997/prep/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256="d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make() {
     cargo build --release --target $CARGO_TARGET_NAME
