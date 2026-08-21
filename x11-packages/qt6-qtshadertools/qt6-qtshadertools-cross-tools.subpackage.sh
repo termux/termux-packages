177d71bd9b9cc56414c4c9451b83c0275fb63a14
BullyMaguire-lol <temp9040@gmail.com>
2026-08-21 19:05:21 +0530

    rename(x11/qt6-qtshadertools): rename from qt6-shadertools

--- x11-packages/qt6-qtshadertools/qt6-qtshadertools-cross-tools.subpackage.sh
+++ x11-packages/qt6-qtshadertools/qt6-qtshadertools-cross-tools.subpackage.sh
@@ -1,5 +1,8 @@
 TERMUX_SUBPKG_DESCRIPTION="Tools for cross build on the host (NOT for Termux)"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
+TERMUX_SUBPKG_BREAKS="qt6-shadertools-cross-tools"
+TERMUX_SUBPKG_REPLACES="qt6-shadertools-cross-tools"
+TERMUX_SUBPKG_PROVIDES="qt6-shadertools-cross-tools"
 TERMUX_SUBPKG_INCLUDE="
 opt/qt6/cross
 "
