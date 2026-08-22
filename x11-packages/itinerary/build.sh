faf02daff460060dcbfb9c57eacb8f83a0418c16
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-22 19:21:03 +0000

    bump(x11/itinerary): 26.08.0

--- x11-packages/itinerary/build.sh
+++ x11-packages/itinerary/build.sh
@@ -5,9 +5,9 @@ TERMUX_PKG_LICENSE_FILE="
 LICENSES/BSD-3-Clause.txt
 "
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="26.04.3"
+TERMUX_PKG_VERSION="26.08.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/itinerary-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256="9ad5a9228cea5042e293e01a8bb61f643ea5efe6ea4dfdf7f3a62bf4f5b07293"
+TERMUX_PKG_SHA256=21df6501f6eb723eb0522e73a919346847a714383a55f241d5bc6f2a5061892e
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="kf6-kcalendarcore, kf6-kcontacts, kf6-kcoreaddons, kf6-kcrash, kf6-kdbusaddons, kf6-kfilemetadata, kf6-kholidays, kf6-ki18n, kf6-kio, kf6-kirigami, kf6-kitemmodels, kf6-knotifications, kf6-kunitconversion, kf6-kwindowsystem, kf6-prison, kf6-qqc2-desktop-style, khealthcertificate, kirigami-addons, kitinerary, kosmindoormap, kpkpass, kpublictransport, libc++, libquotient, qcoro, qt6-qtbase, qt6-qtdeclarative, qt6-qtlocation, qt6-qtmultimedia, qt6-qtpositioning, qtkeychain, zlib"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, libquotient-static, python, qcoro-static"
