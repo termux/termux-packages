98ebf80b8020925d17a6315e6e9f598552b67482
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-22 05:23:19 +0000

    bump(x11/mimetreeparser): 26.08.0

--- x11-packages/mimetreeparser/build.sh
+++ x11-packages/mimetreeparser/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE="https://invent.kde.org/pim/mimetreeparser"
 TERMUX_PKG_DESCRIPTION="Parser for MIME trees"
 TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="26.04.3"
+TERMUX_PKG_VERSION="26.08.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/mimetreeparser-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256="88750429310e9dc60e1f962dc9d7e58e1936e878db54aa41cb7f5c4b4fc524dc"
+TERMUX_PKG_SHA256=3c3da58d03d789c1f67a83c2f618cfb0d2153a16c67f23a59409f24aadf55e1e
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="gpgmepp, kf6-kcalendarcore, kf6-kcolorscheme, kf6-ki18n, kf6-kirigami, kf6-kwidgetsaddons, kmbox, kf6-kmime, libc++, libkleo, qgpgme, qt6-qtbase, qt6-qtdeclarative, qt6-qtwebengine"
+TERMUX_PKG_DEPENDS="gpgmepp, kf6-kcalendarcore, kf6-kcolorscheme, kf6-ki18n, kf6-kio, kf6-kirigami, kf6-kwidgetsaddons, kmbox, kf6-kmime, libc++, libkleo, qgpgme, qt6-qtbase, qt6-qtdeclarative, qt6-qtwebengine"
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
 # qt6-qtwebengine is not supported on the i686 architecture
 TERMUX_PKG_EXCLUDED_ARCHES="i686"
