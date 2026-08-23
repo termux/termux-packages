68988e1d925106bb470dc84bbfc0ff75016b4f94
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-08-23 17:35:09 +0600

    addpkg(main/goreleaser): 2.17.1

---
+++ packages/goreleaser/build.sh
@@ -0,0 +1,26 @@
+TERMUX_PKG_HOMEPAGE=https://goreleaser.com
+TERMUX_PKG_DESCRIPTION="Deliver Go binaries as fast and easily as possible"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
+TERMUX_PKG_VERSION=2.17.1
+TERMUX_PKG_SRCURL=https://github.com/goreleaser/goreleaser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7570a245c67cbf3f468d698b410fed1755525bb48624ed4198babb372c15ca76
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_DEPENDS="golang"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+	cp LICENSE.md LICENSE
+}
+
+termux_step_make() {
+	termux_setup_golang
+	go build \
+		-trimpath \
+		-ldflags="-s -w" \
+		-o goreleaser \
+		.
+}
+termux_step_make_install() {
+	install -Dm755 goreleaser "$TERMUX_PREFIX/bin/goreleaser"
+}
