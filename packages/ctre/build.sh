26d46b9d68f89508438012edf8e708b1f85beeee
Robert Kirkman <rkirkman@termux.dev>
2026-08-24 15:31:31 -0500

    addpkg(main/ctre): 3.11.0

---
+++ packages/ctre/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/hanickadot/compile-time-regular-expressions
+TERMUX_PKG_DESCRIPTION="Compile Time Regular Expression in C++"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="3.11.0"
+TERMUX_PKG_SRCURL="https://github.com/hanickadot/compile-time-regular-expressions/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=7d4b30d0bdd8864a47cceb2ab8e7c4d1846f0ec62383f8c45122435d32f19530
+TERMUX_PKG_AUTO_UPDATE=true
