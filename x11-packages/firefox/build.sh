0abaf85f440a8ff4214a435020507916fefe7eb4
Robert Kirkman <rkirkman@termux.dev>
2026-08-25 12:47:45 -0500

    bump(x11/firefox): 154.0.1
    - Fixes https://github.com/termux/termux-packages/issues/31311
    
    - `0031-Bug-2053518-Handle-the-oe-linux-rust-targets-added-in.patch` for `firefox` like `thunderbird` in https://github.com/termux/termux-packages/pull/31134

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.mozilla.org/firefox
 TERMUX_PKG_DESCRIPTION="Mozilla Firefox web browser"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="154.0"
+TERMUX_PKG_VERSION="154.0.1"
 TERMUX_PKG_SRCURL="https://archive.mozilla.org/pub/firefox/releases/${TERMUX_PKG_VERSION#*really}/source/firefox-${TERMUX_PKG_VERSION#*really}.source.tar.xz"
-TERMUX_PKG_SHA256=36cec5b3688a60f78a6d20dcaee15b598f84e03c66f6587056aead6cb498b99a
+TERMUX_PKG_SHA256=9cbe191fc74b46108376b1d9bf607c0a40288074e0f2333671253789d6ebbd15
 # ffmpeg and pulseaudio are dependencies through dlopen(3):
 TERMUX_PKG_DEPENDS="ffmpeg, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libandroid-shmem, libandroid-spawn, libc++, libcairo, libevent, libffi, libice, libicu, libjpeg-turbo, libnspr, libnss, libpixman, libsm, libvpx, libwebp, libx11, libxcb, libxcomposite, libxdamage, libxext, libxfixes, libxrandr, libxtst, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libcpufeatures, libice, libsm"
