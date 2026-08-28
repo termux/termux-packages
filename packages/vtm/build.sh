fcef2752c6b7cb8727ad2c74fb5e32d540111783
Robert Kirkman <rkirkman@termux.dev>
2026-08-27 19:02:51 -0500

    bump(main/vtm): 2026.07.30
    - Fixes https://github.com/termux/termux-packages/issues/30815

--- packages/vtm/build.sh
+++ packages/vtm/build.sh
@@ -3,14 +3,14 @@ TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-p
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2026.07.22"
+TERMUX_PKG_VERSION="2026.07.30"
 TERMUX_PKG_SRCURL="https://github.com/directvt/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=aee65f50ad59ec25e0ed50c2e97daecaa8fa415c2d791c8f6bdec06f72137e83
+TERMUX_PKG_SHA256=c3da8b3dd3757a6ab6dd2f7757df6365c2122ed64a43645157bcbff48ab0f3e7
 TERMUX_PKG_DEPENDS="freetype, harfbuzz, libc++, lua54, lunasvg"
 TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn, stb"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DSTB_INCLUDE_DIR=$TERMUX__PREFIX__INCLUDE_DIR/stb"
 
 termux_step_pre_configure() {
 	CXXFLAGS+=" -pthread"
+	export STB_INCLUDE_DIR="$TERMUX__PREFIX__INCLUDE_DIR/stb"
 }
