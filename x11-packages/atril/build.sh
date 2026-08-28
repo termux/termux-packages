df7cd004d3ee9ba1222303f34eb278497a8e277a
Robert Kirkman <rkirkman@termux.dev>
2026-08-27 20:55:54 -0500

    bump(x11/atril): 1.28.7
    - Fixes https://github.com/termux/termux-packages/issues/31354

--- x11-packages/atril/build.sh
+++ x11-packages/atril/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://mate-desktop.org
 TERMUX_PKG_DESCRIPTION="MATE document viewer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.28.6"
+TERMUX_PKG_VERSION="1.28.7"
 # https://pub.mate-desktop.org/releases/${TERMUX_PKG_VERSION%.*}/atril-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SRCURL="https://github.com/mate-desktop/atril/releases/download/v${TERMUX_PKG_VERSION}/atril-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=814427e0d5a2dc4e4b060b99ac3c80d8ef39052984d04df49d39135f424c624a
+TERMUX_PKG_SHA256=91942545e858cb0036b52e2f5d4372cdea813d9fde9702daefdf84854e6bb667
 TERMUX_PKG_AUTO_UPDATE=true
 # links with poppler-glib, not poppler
-TERMUX_PKG_DEPENDS="atk, djvulibre, gdk-pixbuf, glib, gtk3, harfbuzz, libarchive, libc++, libcairo, libice, libsecret, libsm, libspectre, libsoup3, libtiff, libxml2, mate-desktop, pango, poppler, texlive-bin, webkit2gtk-4.1, zlib"
+TERMUX_PKG_DEPENDS="atk, djvulibre, gdk-pixbuf, glib, gtk3, harfbuzz, libarchive, libc++, libcairo, libgepub, libice, libsecret, libsm, libspectre, libsoup3, libtiff, libxml2, mate-desktop, pango, poppler, texlive-bin, webkit2gtk-4.1, zlib"
 TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
 TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
