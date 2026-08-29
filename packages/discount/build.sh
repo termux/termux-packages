290d80df73d874226a8a22d8dbfce143aea555d6
Biswapriyo Nath <nathbappai@gmail.com>
2026-08-29 09:40:32 +0000

    bump(main/discount): 3.0.2.0
    Add workaround for undefined MAX_RECURSION macro which is defined by
    configure script only.

--- packages/discount/build.sh
+++ packages/discount/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Markdown implementation written in C"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="COPYRIGHT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.0.1.3"
-TERMUX_PKG_SRCURL="https://github.com/Orc/discount/archive/v${TERMUX_PKG_VERSION}/discount-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=ec04f366cd0a5036598c6a0bf4f7f2582d8ff04b31e81121314b8fde6db6899d
+TERMUX_PKG_VERSION="3.0.2.0"
+TERMUX_PKG_SRCURL="https://github.com/Orc/discount/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=99f6db36d3fff6c99acd21d4c6096176d4ac8785eab319d0faf77ad995b8b7c5
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_HOSTBUILD=true
@@ -22,12 +22,16 @@ termux_step_host_build() {
 	termux_setup_cmake
 	termux_setup_ninja
 
+	# from configure.sh
+	export CFLAGS+=" -DMAX_RECURSION=200"
+
 	cmake "$TERMUX_PKG_SRCDIR/cmake" -GNinja $TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 	ninja -j "$TERMUX_PKG_MAKE_PROCESSES"
 }
 
 termux_step_pre_configure() {
 	export PATH="$TERMUX_PKG_HOSTBUILD_DIR:$PATH"
+	export CFLAGS+=" -DMAX_RECURSION=200"
 	TERMUX_PKG_SRCDIR+="/cmake"
 }
 
