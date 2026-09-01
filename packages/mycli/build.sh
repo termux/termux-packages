dfed33af9f1cea9d54c8669881b5ba31149925d7
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-01 11:32:52 +0600

    addpkg(main/mycli): 2.18.5

---
+++ packages/mycli/build.sh
@@ -0,0 +1,24 @@
+TERMUX_PKG_HOMEPAGE=https://mycli.net
+TERMUX_PKG_DESCRIPTION="CLI for MySQL/MariaDB with auto-completion and syntax highlighting"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
+TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
+TERMUX_PKG_VERSION="2.18.5"
+TERMUX_PKG_SRCURL=https://github.com/dbcli/mycli/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=3fa634dad5fc9284e4c92eb42d6179ca06f4aba97e823fc9e0bafaee058f5af2
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="python, python-pip, python-cryptography, python-pycryptodomex"
+TERMUX_PKG_RECOMMENDS="fzf"
+TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="setuptools>=64, setuptools-scm>=8, wheel"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	# GitHub tarballs have no .git dir, so setuptools-scm can't infer the
+	# version on its own; pin it explicitly to TERMUX_PKG_VERSION.
+	export SETUPTOOLS_SCM_PRETEND_VERSION_FOR_MYCLI="$TERMUX_PKG_VERSION"
+	cross-pip install --prefix="$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
+}
