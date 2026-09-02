d048ee6406d01c1fd2b2c86613e0524efd748953
Joshua "TomIO" Kahn <tom@termux.dev>
2026-09-02 22:41:16 +0200

    revbump(main/texlive-bin): rebuild with PCRE2

--- packages/texlive-bin/build.sh
+++ packages/texlive-bin/build.sh
@@ -3,26 +3,23 @@ TERMUX_PKG_DESCRIPTION="TeX Live is a distribution of the TeX typesetting system
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
 TERMUX_PKG_VERSION="1:2026.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/TeX-Live/texlive-source/archive/refs/heads/tags/texlive-${TERMUX_PKG_VERSION:2}.tar.gz"
 TERMUX_PKG_SHA256=f92e1be0fe4b3ad4e596f8443c5e4e7315ecf0554c2fc153d7af52f854865e24
 TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="freetype, harfbuzz, harfbuzz-icu, libandroid-complex-math, libc++, libcairo, libgd, libgmp, libgraphite, libiconv, libicu, lua52, libmpfr, libpaper, libpixman, libpng, teckit, zlib"
-# libpcre, glib, fontconfig are dependencies of libcairo. pkg-config gives an error if they are missing
+TERMUX_PKG_DEPENDS="freetype, harfbuzz, harfbuzz-icu, libandroid-complex-math, libc++, libcairo, libgd, libgmp, libgraphite, libiconv, libicu, libmpfr, libpaper, libpixman, libpng, lua52, teckit, zlib"
+# pcre2, glib, fontconfig are dependencies of libcairo. pkg-config gives an error if they are missing
 # libuuid, libxml2 are needed by fontconfig
-TERMUX_PKG_BUILD_DEPENDS="icu-devtools, pcre, glib, fontconfig, libuuid, libxml2"
+TERMUX_PKG_BUILD_DEPENDS="icu-devtools, glib, fontconfig, libuuid, libxml2, pcre2"
 TERMUX_PKG_BREAKS="texlive (<< 20180414), texlive-bin-dev"
 TERMUX_PKG_REPLACES="texlive (<< 20170524-3), texlive-bin-dev"
 TERMUX_PKG_RECOMMENDS="texlive-installer"
 TERMUX_PKG_HOSTBUILD=true
-
-TL_ROOT=$TERMUX_PREFIX/share/texlive/${TERMUX_PKG_VERSION:2:6}
-TL_BINDIR=$TERMUX_PREFIX/bin/texlive
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 RANLIB=ranlib
---bindir=$TL_BINDIR
+--bindir=$TERMUX_PREFIX/bin/texlive
 --build=$TERMUX_BUILD_TUPLE
---datarootdir=$TL_ROOT
+--datarootdir=$TERMUX_PREFIX/share/texlive/${TERMUX_PKG_VERSION:2:6}
 --disable-dialog
 --disable-gregorio
 --disable-luajittex
@@ -60,7 +57,8 @@ RANLIB=ranlib
 --without-texinfo
 --without-x
 --without-xdvipdfmx
---with-banner-add=/Termux"
+--with-banner-add=/Termux
+"
 
 # These files are provided by texlive:
 TERMUX_PKG_RM_AFTER_INSTALL="
@@ -88,20 +86,21 @@ share/texlive/texmf-dist/chktex
 share/texlive/texmf-dist/hbf2gf
 "
 
+# shellcheck disable=SC2164
 termux_step_host_build() {
 	mkdir -p auxdir/auxsub
 	mkdir -p texk/kpathsea
 	mkdir -p texk/web2c
 
-	cd $TERMUX_PKG_HOSTBUILD_DIR/auxdir/auxsub
-	$TERMUX_PKG_SRCDIR/auxdir/auxsub/configure
+	cd "$TERMUX_PKG_HOSTBUILD_DIR/auxdir/auxsub"
+	"$TERMUX_PKG_SRCDIR/auxdir/auxsub/configure"
 	make
 
-	cd $TERMUX_PKG_HOSTBUILD_DIR/texk/kpathsea
-	$TERMUX_PKG_SRCDIR/texk/kpathsea/configure
+	cd "$TERMUX_PKG_HOSTBUILD_DIR/texk/kpathsea"
+	"$TERMUX_PKG_SRCDIR/texk/kpathsea/configure"
 
-	cd $TERMUX_PKG_HOSTBUILD_DIR/texk/web2c
-	$TERMUX_PKG_SRCDIR/texk/web2c/configure --without-x
+	cd "$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c"
+	"$TERMUX_PKG_SRCDIR/texk/web2c/configure" --without-x
 	make tangle
 	make ctangle
 	make tie
@@ -110,13 +109,13 @@ termux_step_host_build() {
 }
 
 termux_step_pre_configure() {
-	export TANGLE=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/tangle
-	export TANGLEBOOT=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/tangleboot
-	export CTANGLE=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/ctangle
-	export CTANGLEBOOT=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/ctangleboot
-	export TIE=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/tie
-	export OTANGLE=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/otangle
-	export HIMKTABLES=$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/himktables
+	export TANGLE="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/tangle"
+	export TANGLEBOOT="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/tangleboot"
+	export CTANGLE="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/ctangle"
+	export CTANGLEBOOT="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/ctangleboot"
+	export TIE="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/tie"
+	export OTANGLE="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/otangle"
+	export HIMKTABLES="$TERMUX_PKG_HOSTBUILD_DIR/texk/web2c/himktables"
 
 	sed -e "s%@TERMUX_PREFIX@%$TERMUX_PREFIX%g" \
 		-e "s%@YEAR@%${TERMUX_PKG_VERSION:2:6}%g" \
