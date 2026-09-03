bc218677f13a242c18eb6cc1f32bb5b3dbe4ba04
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-03 06:27:03 +0600

    litecli: pip install --no-deps to avoid bundling shared pygmentize script

--- packages/litecli/build.sh
+++ packages/litecli/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="1.17.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/dbcli/litecli/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f02a1014add7c581e4ee720a146005d3099d561d2079f2d7c850977e6ac35a4c
 TERMUX_PKG_AUTO_UPDATE=true
@@ -19,5 +20,5 @@ termux_step_make_install() {
 	# GitHub tarballs have no .git dir, so setuptools-scm can't infer the
 	# version on its own; pin it explicitly to TERMUX_PKG_VERSION.
 	export SETUPTOOLS_SCM_PRETEND_VERSION_FOR_LITECLI="$TERMUX_PKG_VERSION"
-	cross-pip install --prefix="$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
+	cross-pip install --no-deps --prefix="$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
 }
