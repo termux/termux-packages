2d30719c42d3edf5e2d66032f452fbf4d346f728
PepeDiedrich <pepediedrich@proton.me>
2026-09-03 19:20:16 +0200

    Update war-dodger to 0.1.4

--- packages/war-dodger/build.sh
+++ packages/war-dodger/build.sh
@@ -1,9 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/PepeDiedrich/war-dodge
 TERMUX_PKG_DESCRIPTION="Location-aware monitor for U.S. State Department travel advisory changes"
-TERMUX_PKG_VERSION=0.1.3
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.1.4
 TERMUX_PKG_SRCURL=https://github.com/PepeDiedrich/war-dodge/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=beab8aaaa6d87dd7f4ee6a1d219a91ef9afe91a379f66332a76794c92abcdce3
+TERMUX_PKG_SHA256=68deaf92007713c3e039f0d40206b19e3786e54b787815f04bc05ccc93d1fda2
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@PepeDiedrich"
 TERMUX_PKG_DEPENDS="termux-api"
