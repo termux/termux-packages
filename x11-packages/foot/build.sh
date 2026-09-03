c3022ad2fba2fe123c5a8d032fd983523c245466
Rishabh Sharma <rishabhsharma14426@gmail.com>
2026-09-03 14:25:54 +0530

    bump(x11/foot): 1.28.0
    - Remove obsolete reallocarray patch as upstream 1.28.0 added native xreallocarray
    
    Fixes #31461

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://codeberg.org/dnkl/foot
 TERMUX_PKG_DESCRIPTION="Fast, lightweight and minimalistic Wayland terminal emulator"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.27.0"
+TERMUX_PKG_VERSION="1.28.0"
 TERMUX_PKG_SRCURL="https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=f5917cad2d7b723b99873e53d78fd10ea202923d189aed5086591fc53b70b7e3
+TERMUX_PKG_SHA256=4296be402b5684d049534598e69db92b918f92beac9dab76b585207045f0b037
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libandroid-support, fontconfig, freetype, libfcft, libpixman, libwayland, libxkbcommon, utf8proc"
 TERMUX_PKG_BUILD_DEPENDS="libtllist, libwayland-protocols, libwayland-cross-scanner, scdoc, xdg-utils"
@@ -19,6 +19,4 @@ termux_step_pre_configure() {
 
 	# libandroid-support provides this
 	export CPPFLAGS+=" -D__STDC_ISO_10646__=201103L"
-
-	cp "${TERMUX_PKG_BUILDER_DIR}/reallocarray.c" "${TERMUX_PKG_SRCDIR}"
 }
