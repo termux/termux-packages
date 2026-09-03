aa3cd259210cc63412efab206a5ae32c0e6dd489
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-03 06:26:40 +0600

    iredis: pip install --no-deps to avoid bundling shared pygmentize script

--- packages/iredis/build.sh
+++ packages/iredis/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Interactive CLI for Redis with auto-completion and synta
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="1.16.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/laixintao/iredis/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c9d6794b21eaa78a5647e0d2b98907a31d0243728290a80ca2937efd09ee07bb
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,5 +16,5 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
-	cross-pip install --prefix="$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
+	cross-pip install --no-deps --prefix="$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
 }
