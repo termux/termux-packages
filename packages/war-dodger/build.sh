8f82b8abcff0214d9d38cf2284405fb8e50fcbe2
Pepe Diedrich <pepediedrich@proton.me>
2026-09-03 15:18:34 +0200

    Add war-dodger package

---
+++ packages/war-dodger/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/PepeDiedrich/war-dodge
+TERMUX_PKG_DESCRIPTION="Location-aware monitor for U.S. State Department travel advisory changes"
+TERMUX_PKG_VERSION=0.1.2
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/PepeDiedrich/war-dodge/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7bca689bd18883fede94582179fd0e9905a2e09fd480e98febdfdd3dc48820aa
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@PepeDiedrich"
+TERMUX_PKG_DEPENDS="termux-api"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_pre_configure() {
+    termux_setup_rust
+}
+
+termux_step_make() {
+    cargo build --target "${CARGO_TARGET_NAME}" --release
+}
+
+termux_step_make_install() {
+    install -Dm755 "target/${CARGO_TARGET_NAME}/release/war-dodger" \
+        "${TERMUX_PREFIX}/bin/war-dodger"
+}
