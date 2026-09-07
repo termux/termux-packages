ef88dfe9352f27b3b6ee2866569a9f781f91f258
Maxython <mixython@gmail.com>
2026-09-07 15:50:08 +0300

    bump(main/libabw): 0.1.4

--- packages/libabw/build.sh
+++ packages/libabw/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://wiki.documentfoundation.org/DLP/Libraries/libabw"
 TERMUX_PKG_DESCRIPTION="a library that parses the file format of AbiWord documents"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.1.3"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION="0.1.4"
 TERMUX_PKG_SRCURL="https://dev-www.libreoffice.org/src/libabw/libabw-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=e763a9dc21c3d2667402d66e202e3f8ef4db51b34b79ef41f56cacb86dcd6eed
+TERMUX_PKG_SHA256=fa2685a3440da6e03a66a778480d93cb95f6064e4541e58e37397680760fd6a0
 TERMUX_PKG_DEPENDS="librevenge, libxml2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, gperf"
 
