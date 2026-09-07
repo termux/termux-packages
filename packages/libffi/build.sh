ff3b5a8373a8fccc354d2e01fb05396dcea21ee3
Maxython <mixython@gmail.com>
2026-09-07 15:50:55 +0300

    bump(main/libffi): 3.8.0

--- packages/libffi/build.sh
+++ packages/libffi/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://sourceware.org/libffi/
 TERMUX_PKG_DESCRIPTION="Library providing a portable, high level programming interface to various calling conventions"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.5.2"
+TERMUX_PKG_VERSION="3.8.0"
 TERMUX_PKG_SRCURL=https://github.com/libffi/libffi/releases/download/v${TERMUX_PKG_VERSION}/libffi-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f3a3082a23b37c293a4fcd1053147b371f2ff91fa7ea1b2a52e335676bac82dc
+TERMUX_PKG_SHA256=7da3e2d9a171eb0a038f592ecad3ff2bb2550f3496d87b3b29ad0cf4430c0db4
 TERMUX_PKG_BREAKS="libffi-dev"
 TERMUX_PKG_REPLACES="libffi-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-multi-os-directory"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-multi-os-directory ac_cv_func_memfd_create=no"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/libffi-${TERMUX_PKG_VERSION}/include"
 
 termux_step_post_get_source() {
