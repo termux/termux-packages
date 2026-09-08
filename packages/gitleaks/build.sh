b77b063991b8cbdc46da703a54cf13607ac7a4b1
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-08 10:55:02 +0600

    addpkg(main/gitleaks): 8.30.1

---
+++ packages/gitleaks/build.sh
@@ -0,0 +1,23 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/gitleaks/gitleaks
+TERMUX_PKG_DESCRIPTION="Detect secrets like passwords, API keys, and tokens in git repos and files"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
+TERMUX_PKG_VERSION="8.30.1"
+TERMUX_PKG_SRCURL="https://github.com/gitleaks/gitleaks/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=e90fb266d75837e75894c778bf594ab8e2787f12dce5a62651f21b893eaf9abb
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	go build \
+		-trimpath \
+		-ldflags="-s -w" \
+		-o gitleaks \
+		.
+}
+
+termux_step_make_install() {
+	install -Dm755 gitleaks "$TERMUX_PREFIX/bin/gitleaks"
+}
