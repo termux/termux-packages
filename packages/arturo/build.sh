b65ce326371f80a8789f5177a4755bd12c2fb818
Joshua "TomIO" Kahn <tom@termux.dev>
2026-09-08 22:27:48 +0200

    enhance(main/arturo): use `termux_setup_nim`

--- packages/arturo/build.sh
+++ packages/arturo/build.sh
@@ -3,17 +3,18 @@ TERMUX_PKG_DESCRIPTION="Simple, expressive & portable programming language for e
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Komo @mbekkomo"
 TERMUX_PKG_VERSION=0.10.0
-TERMUX_PKG_SRCURL=https://github.com/arturo-lang/arturo/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/arturo-lang/arturo/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=408646496895753608ad9dc6ddfbfa25921c03c4c7356f2832a9a63f4a7dc351
 TERMUX_PKG_DEPENDS="libgmp, libmpfr, libandroid-glob, libsqlite, openssl, pcre"
 TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
-	declare arch=${TERMUX_ARCH}
+	declare arch="${TERMUX_ARCH}"
 	case "${arch}" in
-	aarch64) arch=arm64 ;;
-	x86_64)  arch=amd64 ;;
+		aarch64) arch=arm64;;
+		x86_64)  arch=amd64;;
 	esac
 	cat <<-EOF >nim.cfg
 		--os:android
@@ -32,12 +33,8 @@ termux_step_pre_configure() {
 	EOF
 }
 
-termux_step_configure() {
-	curl https://nim-lang.org/choosenim/init.sh -sSf | bash -s -- -y
-	export PATH="$HOME/.nimble/bin:$PATH"
-}
-
 termux_step_make() {
+	termux_setup_nim
 	nim build.nims build --mode full --log --release
 }
 
