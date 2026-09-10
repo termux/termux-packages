53ef4c5461864a893ccf456755d1aef5e11b2307
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-10 08:41:17 +0600

    enhance(main/gtrash): add shell completions

--- packages/gtrash/build.sh
+++ packages/gtrash/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="A featureful trash CLI manager, alternative to rm and tr
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="0.0.6"
-TERMUX_PKG_SRCURL=https://github.com/umlx5h/gtrash/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/umlx5h/gtrash/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=66003276073d9da03cbb4347a4b161f89c81f3706012b77c3e91a154c91f3586
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -19,4 +20,14 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm755 gtrash "$TERMUX_PREFIX/bin/gtrash"
+
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	go run . completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/gtrash"
+	go run . completion zsh  > "${TERMUX_PREFIX}/share/zsh/site-functions/_gtrash"
+	go run . completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/gtrash.fish"
 }
