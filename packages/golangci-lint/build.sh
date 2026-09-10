be02f65986e00960d95919ef92a12e0d71a20192
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-10 08:48:35 +0600

    enhance(main/golangci-lint): add shell completions

--- packages/golangci-lint/build.sh
+++ packages/golangci-lint/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Fast linters runner for Go, aggregating many Go linters 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="2.13.2"
-TERMUX_PKG_SRCURL=https://github.com/golangci/golangci-lint/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/golangci/golangci-lint/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=a79a7a1faad9c1538e3f7f8b32843a53bbeedfa9ead45d9e8ca3bb210d55ece0
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -21,4 +22,14 @@ termux_step_make() {
 termux_step_make_install() {
 	install -Dm755 golangci-lint "$TERMUX_PREFIX/bin/golangci-lint"
 	install -Dm644 README.md "$TERMUX_PREFIX/share/doc/golangci-lint/README.md"
+
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	go run ./cmd/golangci-lint completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/golangci-lint"
+	go run ./cmd/golangci-lint completion zsh  > "${TERMUX_PREFIX}/share/zsh/site-functions/_golangci-lint"
+	go run ./cmd/golangci-lint completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/golangci-lint.fish"
 }
