c8ed3069f41eaa485ceccc211383024baa5ffb4c
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-10 08:46:56 +0600

    enhance(main/goreleaser): add shell completions

--- packages/goreleaser/build.sh
+++ packages/goreleaser/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Deliver Go binaries as fast and easily as possible"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="2.18.1"
-TERMUX_PKG_SRCURL=https://github.com/goreleaser/goreleaser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/goreleaser/goreleaser/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=5f47712f272842b1b51f599403a266e1a304b9689cfb31da22bd279c79af5afc
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -20,6 +21,17 @@ termux_step_make() {
 		-o goreleaser \
 		.
 }
+
 termux_step_make_install() {
 	install -Dm755 goreleaser "$TERMUX_PREFIX/bin/goreleaser"
+
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	go run . completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/goreleaser"
+	go run . completion zsh  > "${TERMUX_PREFIX}/share/zsh/site-functions/_goreleaser"
+	go run . completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/goreleaser.fish"
 }
