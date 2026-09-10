81884f8dfe4ba288fdbbab1df8607b0704caecd2
BullyMaguire-lol <temp9040@gmail.com>
2026-09-10 09:34:24 +0530

    bump(x11/kf6-purpose): 6.30.0

--- x11-packages/kf6-purpose/build.sh
+++ x11-packages/kf6-purpose/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE='https://invent.kde.org/frameworks/purpose'
 TERMUX_PKG_DESCRIPTION='Framework for providing abstractions to get the developers purposes fulfilled'
 TERMUX_PKG_LICENSE="LGPL-2.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.29.0"
+TERMUX_PKG_VERSION="6.30.0"
 TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSION%.*}/purpose-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=743445e6a1b3aee156aec6e84ac8d36161124784f9e5730b69b868fdd6fb79c2
+TERMUX_PKG_SHA256="4fda64d235927e3cc230416d5c9304f5d3e040dc2553b77b2c69bf54c6f04862"
 TERMUX_PKG_DEPENDS="kf6-kcmutils (>= ${TERMUX_PKG_VERSION%.*}), kf6-kconfig (>= ${TERMUX_PKG_VERSION%.*}), kf6-kcoreaddons (>= ${TERMUX_PKG_VERSION%.*}), kf6-ki18n (>= ${TERMUX_PKG_VERSION%.*}), kf6-kio (>= ${TERMUX_PKG_VERSION%.*}), kf6-knotifications (>= ${TERMUX_PKG_VERSION%.*}), kf6-kservice (>= ${TERMUX_PKG_VERSION%.*}), kf6-kitemmodels (>= ${TERMUX_PKG_VERSION%.*}), kf6-prison (>= ${TERMUX_PKG_VERSION%.*}), libc++, qt6-qtbase, qt6-qtdeclarative"
 # kaccounts-integration, libaccounts-qt, accounts-qml-module can be added to TERMUX_PKG_DEPENDS when available
 TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules (>= ${TERMUX_PKG_VERSION%.*}), kf6-kirigami (>= ${TERMUX_PKG_VERSION%.*}), intltool"
