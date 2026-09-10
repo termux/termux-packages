94fa70b23f74189cb4715f5bed05957a7eb8f9c5
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-10 08:45:35 +0600

    enhance(main/sqlc): add shell completions

--- packages/sqlc/build.sh
+++ packages/sqlc/build.sh
@@ -3,7 +3,8 @@ TERMUX_PKG_DESCRIPTION="Generate type-safe code from SQL"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="1.31.1"
-TERMUX_PKG_SRCURL=https://github.com/sqlc-dev/sqlc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/sqlc-dev/sqlc/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=de82593a200e4130dc2a0413a808f93fc30fdc7b5ecd402913ed08a8fea06c4a
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -22,4 +23,15 @@ termux_step_make() {
 
 termux_step_make_install() {
 	install -Dm755 sqlc "$TERMUX_PREFIX/bin/sqlc"
+
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	export CGO_CFLAGS="-D_GNU_SOURCE"
+	go run ./cmd/sqlc completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/sqlc"
+	go run ./cmd/sqlc completion zsh  > "${TERMUX_PREFIX}/share/zsh/site-functions/_sqlc"
+	go run ./cmd/sqlc completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/sqlc.fish"
 }
