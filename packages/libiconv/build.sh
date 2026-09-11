c29c8a07c1982c640cfed0fda5cf8df95a0a6ec8
Maxython <mixython@gmail.com>
2026-09-11 12:38:53 +0300

    bump(packages/libiconv): 1.19

--- packages/libiconv/build.sh
+++ packages/libiconv/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libiconv/
 TERMUX_PKG_DESCRIPTION="An implementation of iconv()"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.18"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="1.19"
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/libiconv/libiconv-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8
+TERMUX_PKG_SHA256=88dd96a8c0464eca144fc791ae60cd31cd8ee78321e67397e25fc095c4a19aa6
 TERMUX_PKG_BREAKS="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 TERMUX_PKG_REPLACES="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 
