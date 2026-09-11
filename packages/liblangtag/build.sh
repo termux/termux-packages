c6462a368dba63ee6b665319f5c8838da79c7725
Maxython <mixython@gmail.com>
2026-09-11 12:38:56 +0300

    bump(packages/liblangtag): 0.6.8
    Co-authored-by: TomJo2000 <tom@termux.dev>

--- packages/liblangtag/build.sh
+++ packages/liblangtag/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://bitbucket.org/tagoh/liblangtag/wiki/Home
+TERMUX_PKG_HOMEPAGE=https://gitlab.com/tagoh/liblangtag
 TERMUX_PKG_DESCRIPTION="interface library to access/deal with tags for identifying languages"
-TERMUX_PKG_LICENSE="LGPL-2.1, MPL-2.0"
+TERMUX_PKG_LICENSE="LGPL-3.0-or-later, MPL-2.0, custom"
+TERMUX_PKG_LICENSE_FILE="COPYING, COPYING.MPL, COPYING.Unicode"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.6.7"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL="https://bitbucket.org/tagoh/liblangtag/downloads/liblangtag-${TERMUX_PKG_VERSION}.tar.bz2"
-TERMUX_PKG_SHA256=5ed6bcd4ae3f3c05c912e62f216cd1a44123846147f729a49fb5668da51e030e
+TERMUX_PKG_VERSION="0.6.8"
+TERMUX_PKG_SRCURL=https://gitlab.com/tagoh/liblangtag/-/releases/${TERMUX_PKG_VERSION}/downloads/liblangtag-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f98d15a2039a523e6ad7796bba0fb003f214db57cc4ad2e12e2f8ab12d309694
 TERMUX_PKG_DEPENDS="libxml2"
 # --disable-introspection avoids LangTag-0.6: cannot execute binary file: Exec format error
 # if gobject-introspection was installed in the same container before cross-compiling
@@ -19,6 +19,18 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	export ac_cv_va_copy=C99
+
+	declare -A LICENSE_CHECKSUMS
+
+	LICENSE_CHECKSUMS[COPYING.MPL]=fab3dd6bdab226f1c08630b1dd917e11fcb4ec5e1e020e2c16f83a0a13863e85
+	LICENSE_CHECKSUMS[COPYING.Unicode]=ecab49b0a28761bedcd38bc5a237a81acdfaacb655afea02c72ca1c120267749
+
+	for LICENSE in ${!LICENSE_CHECKSUMS[@]}; do
+		termux_download \
+			"https://gitlab.com/tagoh/liblangtag/-/raw/${TERMUX_PKG_VERSION}/${LICENSE}" \
+			"${TERMUX_PKG_SRCDIR}/${LICENSE}" \
+			"${LICENSE_CHECKSUMS[${LICENSE}]}"
+	done
 }
 
 termux_step_post_configure() {
