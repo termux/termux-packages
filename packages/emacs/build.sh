5bbc4ab2eacba6fa978abf75b60dd50c9a068cc8
Robert Kirkman <rkirkman@termux.dev>
2026-09-11 03:46:43 -0500

    chore(main/emacs): reorder `TERMUX_PKG_EXTRA_CONFIGURE_ARGS`

--- packages/emacs/build.sh
+++ packages/emacs/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # Update both emacs and emacs-x to the same version in one PR.
 TERMUX_PKG_VERSION="31.1"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL="https://mirrors.kernel.org/gnu/emacs/emacs-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=1da5790d9580c81932b5bf700633114468da7b3412d69faa767daebf974f4586
 TERMUX_PKG_DEPENDS="libacl, libgmp, libgnutls, libsqlite, libxml2, ncurses, tree-sitter, zlib"
@@ -13,23 +13,23 @@ TERMUX_PKG_REPLACES="emacs-dev"
 TERMUX_PKG_SERVICE_SCRIPT=("emacsd" 'exec emacs --fg-daemon 2>&1')
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-autodepend
---with-dumping=none
---with-gif=no
---with-gnutls
---with-jpeg=no
---with-modules
---with-pdumper=yes
---with-png=no
---with-tiff=no
---with-xml2
---with-xpm=no
---with-tree-sitter
 --without-dbus
 --without-gconf
+--without-gif
 --without-gsettings
+--without-jpeg
 --without-lcms2
+--without-png
 --without-selinux
+--without-tiff
 --without-x
+--without-xpm
+--with-dumping=none
+--with-gnutls
+--with-modules
+--with-pdumper=yes
+--with-tree-sitter
+--with-xml2
 "
 
 if [[ "$TERMUX_DEBUG_BUILD" == "true" ]]; then
