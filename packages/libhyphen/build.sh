0437ff8a085c2085ee9cf74208e6f8d8520e1e6f
Maxython <mixython@gmail.com>
2026-09-11 12:38:52 +0300

    bump(packages/libhyphen): 2.8.9

--- packages/libhyphen/build.sh
+++ packages/libhyphen/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/hunspell/hyphen
 TERMUX_PKG_DESCRIPTION="hyphenation library to use converted TeX hyphenation patterns"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.8.8
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/hunspell/hyphen-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=304636d4eccd81a14b6914d07b84c79ebb815288c76fe027b9ebff6ff24d5705
+TERMUX_PKG_VERSION=2.8.9
+TERMUX_PKG_SRCURL=https://github.com/hunspell/hyphen/releases/download/v${TERMUX_PKG_VERSION}/hyphen-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=783743daf477de8c4d16e3c74b4d2827377017718d8e17e2d9440210246f6abe
 
 termux_step_pre_configure() {
 	autoreconf -fvi
