731b3e9be924dc68890299de0012a90c38fd6b26
Maxython <mixython@gmail.com>
2026-09-11 12:38:47 +0300

    bump(packages/libe-book): 0.1.4

--- packages/libe-book/build.sh
+++ packages/libe-book/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE="https://sourceforge.net/projects/libebook/"
 TERMUX_PKG_DESCRIPTION="Library for import of reflowable e-book formats."
 TERMUX_PKG_LICENSE="LGPL-2.1, MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.1.3"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION="0.1.4"
 TERMUX_PKG_SRCURL="https://sourceforge.net/projects/libebook/files//libe-book-${TERMUX_PKG_VERSION}/libe-book-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=7e8d8ff34f27831aca3bc6f9cc532c2f90d2057c778963b884ff3d1e34dfe1f9
+TERMUX_PKG_SHA256=104b7e791b6632745898e9b6a0037b7540235771a4d8c3bde2c764466ad912f1
 TERMUX_PKG_DEPENDS="libicu, liblangtag, librevenge, libxml2, zlib"
 TERMUX_PKG_BUILD_DEPENDS="boost, cppunit, gperf"
 
