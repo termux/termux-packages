3f94b906abdb6a63dd698d84984e966c57399cb8
Joshua "TomIO" Kahn <tom@termux.dev>
2026-09-13 21:06:42 +0200

    bump(main/bacula): 17.0.0 and update SRCURL Use the upstream Bacula Gitlab as the SRCURL. And sort the configure args

--- packages/bacula/build.sh
+++ packages/bacula/build.sh
@@ -1,34 +1,33 @@
 TERMUX_PKG_HOMEPAGE=https://www.bacula.org
 TERMUX_PKG_DESCRIPTION="Bacula backup software"
 TERMUX_PKG_LICENSE="AGPL-3.0-only"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="15.0.3"
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL="https://sourceforge.net/projects/bacula/files/bacula/${TERMUX_PKG_VERSION}/bacula-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=294afd3d2eb9d5b71c3d0e88fdf19eb513bfdb843b28d35c0552e4ae062827a1
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_VERSION="17.0.0"
+TERMUX_PKG_SRCURL="https://gitlab.bacula.org/bacula-community-edition/bacula-community/-/archive/Release-${TERMUX_PKG_VERSION}/bacula-community-Release-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=7577f2d9b2d0e4daa826473ad63d652095ca2d862e517b947cc2d38bada5726a
 TERMUX_PKG_DEPENDS="libc++, liblzo, openssl, zlib"
 TERMUX_PKG_BREAKS="bacula-fd"
 TERMUX_PKG_REPLACES="bacula-fd"
+TERMUX_PKG_PROVIDES="bacula-fd"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_CONFFILES=etc/bacula/bacula-fd.conf
+TERMUX_PKG_CONFFILES="etc/bacula/bacula-fd.conf"
 TERMUX_PKG_SERVICE_SCRIPT=("bacula-fd" "${TERMUX_PREFIX}/bin/bacula-fd")
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---sysconfdir=${TERMUX_PREFIX}/etc/bacula
---with-plugindir=${TERMUX_PREFIX}/lib/bacula
---mandir=${TERMUX_PREFIX}/share/man
---with-logdir=${TERMUX_PREFIX}/var/log
---with-working-dir=${TERMUX_PREFIX}/var/run/bacula
---with-pid-dir=${TERMUX_PREFIX}/var/run/bacula
---with-scriptdir=${TERMUX_PREFIX}/etc/bacula/scripts
---with-lzo=${TERMUX_PREFIX}
---with-ssl
---enable-smartalloc
---enable-conio
---enable-client-only
---with-baseport=9102
 ac_cv_func_setpgrp_void=yes
+--enable-client-only
+--enable-conio
+--enable-smartalloc
+--mandir=${TERMUX_PREFIX}/share/man
+--sysconfdir=${TERMUX_PREFIX}/etc/bacula
+--with-baseport=9102
+--with-logdir=${TERMUX_PREFIX}/var/log
+--with-lzo=${TERMUX_PREFIX}
+--with-pid-dir=${TERMUX_PREFIX}/var/run/bacula
+--with-plugindir=${TERMUX_PREFIX}/lib/bacula
+--with-scriptdir=${TERMUX_PREFIX}/etc/bacula/scripts
+--with-ssl
+--with-working-dir=${TERMUX_PREFIX}/var/run/bacula
 "
 
 termux_step_pre_configure() {
