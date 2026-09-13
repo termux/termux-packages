f231c67c66fa791ef94947af5a25893a0292fc4f
Finagolfin <finagolfin@tuta.io>
2026-09-14 02:57:37 +0530

    bump(ndk-sysroot): add NDK C++ patch from #27583

--- scripts/build/termux_step_setup_toolchain.sh
+++ scripts/build/termux_step_setup_toolchain.sh
@@ -7,7 +7,7 @@ termux_step_setup_toolchain() {
 		# toolchain setup to ensure that everyone gets an updated
 		# toolchain
 		if [ "${TERMUX_NDK_VERSION}" = "29" ]; then
-			TERMUX_STANDALONE_TOOLCHAIN+="-v5"
+			TERMUX_STANDALONE_TOOLCHAIN+="-v6"
 			termux_setup_toolchain_29
 		elif [ "${TERMUX_NDK_VERSION}" = 23c ]; then
 			TERMUX_STANDALONE_TOOLCHAIN+="-v12"
