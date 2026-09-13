d8d08420f249e5aae93ea043890b11758c37f333
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-13 10:08:02 +0600

    addpkg(main/libiptcdata): 1.0.4
    Co-authored-by: Ronald Y <46513942+knyipab@users.noreply.github.com>

---
+++ packages/libiptcdata/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://libiptcdata.sourceforge.net/
+TERMUX_PKG_DESCRIPTION="a C library for manipulating the IPTC metadata stored within multimedia files"
+TERMUX_PKG_LICENSE="LGPL-2.0"
+TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
+TERMUX_PKG_VERSION="1.0.4"
+TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/libiptcdata/libiptcdata/libiptcdata-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=79f63b8ce71ee45cefd34efbb66e39a22101443f4060809b8fc29c5eebdcee0e
