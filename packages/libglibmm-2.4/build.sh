8654db18a3a2c0e1625c8e8f00318de5960ed52a
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-14 12:34:48 +0600

    fix(libglibmm-2.4): strip redundant GDBus typedefs from giomm headers

--- packages/libglibmm-2.4/build.sh
+++ packages/libglibmm-2.4/build.sh
@@ -3,12 +3,28 @@ TERMUX_PKG_DESCRIPTION="A C++ API for parts of glib that are useful for C++"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.66.9"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/glibmm/${TERMUX_PKG_VERSION%.*}/glibmm-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=5a026e5602085307c7dcb72b71b07261c40f80914277bef5f8d7f2ecab739bec
 TERMUX_PKG_DEPENDS="glib, libc++, libsigc++-2.0"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dbuild-examples=false
 "
+
+termux_step_post_get_source() {
+	local _hdr _cname
+	for _hdr in \
+		"${TERMUX_PKG_SRCDIR}/untracked/gio/giomm/dbusactiongroup.h:GDBusActionGroup" \
+		"${TERMUX_PKG_SRCDIR}/untracked/gio/giomm/emblem.h:GEmblem" \
+	; do
+		_cname="${_hdr##*:}"
+		_hdr="${_hdr%%:*}"
+		if [ ! -e "${_hdr}" ]; then
+			termux_error_exit "file ${_hdr} not found."
+		fi
+		sed -i -E "s|^using ${_cname}Class = struct _${_cname}Class;\$|#include <gio/gio.h>|" "${_hdr}"
+	done
+}
 
 termux_step_post_massage() {
 	local _GUARD_FILE="lib/${TERMUX_PKG_NAME}.so"
