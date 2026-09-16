b0a27e806c0c6be2b534c09503a7835252198fad
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-16 19:28:10 +0600

    rawtherapee: bump revision after libiptcdata 1.0.5

--- x11-packages/rawtherapee/build.sh
+++ x11-packages/rawtherapee/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="raw image converter and digital photo processor"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="5.13"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Beep6581/RawTherapee/releases/download/${TERMUX_PKG_VERSION}/rawtherapee-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=23bdff4b1817e1274c11a94b11d21cac59e9c1b1d209795d3194710ee73e77bf
 TERMUX_PKG_DEPENDS="exiv2, fftw, glib, gtk3, gtkmm3, lensfun, libcanberra, libexpat, libglibmm-2.4, libiptcdata, libjpeg-turbo, libpng, libraw, librsvg, libsigc++-2.0, libtiff, littlecms, zlib"
