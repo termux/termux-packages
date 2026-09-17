c923293b5748ee751134ca266a6bf806421478d0
padukalutz <rusmanto.co@gmail.com>
2026-09-18 04:15:19 +0700

    chore(main/lutzkit): update to 0.1.4

--- packages/lutzkit/build.sh
+++ packages/lutzkit/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/padukalutz/lutzkit
 TERMUX_PKG_DESCRIPTION="Developer CLI and project generator"
 TERMUX_PKG_LICENSE=MIT
 TERMUX_PKG_MAINTAINER="padukalutz"
-TERMUX_PKG_VERSION="0.1.3"
+TERMUX_PKG_VERSION="0.1.4"
 TERMUX_PKG_SRCURL=https://github.com/padukalutz/lutzkit/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 
