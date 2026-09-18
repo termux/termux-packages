2c9ab84c65dcacffc4ea58e59bb6bc4a08412a8f
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-18 19:14:53 +0300

    chore(main/libmp3lame): enable mpg123 decoder support

--- packages/libmp3lame/build.sh
+++ packages/libmp3lame/build.sh
@@ -3,12 +3,12 @@ TERMUX_PKG_DESCRIPTION="High quality MPEG Audio Layer III (MP3) encoder"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/lame/lame/${TERMUX_PKG_VERSION}/lame-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3df5124d5ad3a98312ffd7ba6a9b36230e4f8a3e66d3ce0f425e336c32d216eb
 TERMUX_PKG_BREAKS="libmp3lame-dev"
 TERMUX_PKG_REPLACES="libmp3lame-dev"
-TERMUX_PKG_DEPENDS="libiconv"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-decoder"
+TERMUX_PKG_DEPENDS="libiconv, libmpg123"
 TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
