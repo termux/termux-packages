58e79bc456d31564e68bc7682b48ba1f836da1be
Joshua "TomIO" Kahn <tom@termux.dev>
2026-09-20 20:25:23 +0200

    bump(main/readline): 8.3.6

--- packages/readline/build.sh
+++ packages/readline/build.sh
@@ -5,16 +5,15 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BREAKS="bash (<< 5.0), readline-dev"
 TERMUX_PKG_REPLACES="readline-dev"
-_MAIN_VERSION=8.3
-_PATCH_VERSION=3
-TERMUX_PKG_VERSION=$_MAIN_VERSION.$_PATCH_VERSION
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/readline/readline-${_MAIN_VERSION}.tar.gz
+TERMUX_PKG_VERSION=8.3.6
+TERMUX_PKG_SRCURL="https://mirrors.kernel.org/gnu/readline/readline-${TERMUX_PKG_VERSION%.*}.tar.gz"
 TERMUX_PKG_SHA256=fe5383204467828cd495ee8d1d3c037a7eba1389c22bc6a041f627976f9061cc
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-curses --enable-multibyte bash_cv_wcwidth_broken=no"
 TERMUX_PKG_EXTRA_MAKE_ARGS="SHLIB_LIBS=-lncursesw"
 TERMUX_PKG_CONFFILES="etc/inputrc"
 
 termux_step_pre_configure() {
+	local _MAIN_VERSION="${TERMUX_PKG_VERSION%.*}" _PATCH_VERSION="${TERMUX_PKG_VERSION##*.}"
 	(( _PATCH_VERSION == 0 )) && return
 	local PATCH_NUM PATCHFILE
 	local -A PATCH_CHECKSUMS=()
@@ -22,11 +21,14 @@ termux_step_pre_configure() {
 	PATCH_CHECKSUMS[001]=21f0a03106dbe697337cd25c70eb0edbaa2bdb6d595b45f83285cdd35bac84de
 	PATCH_CHECKSUMS[002]=e27364396ba9f6debf7cbaaf1a669e2b2854241ae07f7eca74ca8a8ba0c97472
 	PATCH_CHECKSUMS[003]=72dee13601ce38f6746eb15239999a7c56f8e1ff5eb1ec8153a1f213e4acdb29
+	PATCH_CHECKSUMS[004]=1f189c4566aa35950964647b9ec3be1a821e42b86048129c63eb7f9d4d2f5a74
+	PATCH_CHECKSUMS[005]=5481adbe161c9f6ba972c74db9e18c7aa351d3df3035cc4a673a1aa06247d1c6
+	PATCH_CHECKSUMS[006]=4ebd261a608287796e171af0ea9af5a1e3122e46672accda2484b6698c47c1a4
 
-	for PATCH_NUM in $(seq -f '%03g' ${_PATCH_VERSION}); do
+	for PATCH_NUM in $(seq -f '%03g' "${_PATCH_VERSION}"); do
 		PATCHFILE=$TERMUX_PKG_CACHEDIR/readline_patch_${PATCH_NUM}.patch
 		termux_download \
-			"https://mirrors.kernel.org/gnu/readline/readline-$_MAIN_VERSION-patches/readline${_MAIN_VERSION/./}-$PATCH_NUM" \
+			"https://mirrors.kernel.org/gnu/readline/readline-${_MAIN_VERSION}-patches/readline${_MAIN_VERSION/./}-$PATCH_NUM" \
 			"$PATCHFILE" \
 			"${PATCH_CHECKSUMS[$PATCH_NUM]}"
 		patch -p0 -i "$PATCHFILE"
