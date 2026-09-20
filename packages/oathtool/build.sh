b671a6b0597d604fcc1d528a03a9b6c393951fc3
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-20 16:48:10 +0300

    fix(main/oathtool): fix build on bionic and out-of-tree liboath docs
    umask() FORTIFY check rejects bits outside 0-0777; liboath/man/gdoc
    symlink was missing for out-of-tree builds, only libpskc's was handled.

--- packages/oathtool/build.sh
+++ packages/oathtool/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="One-time password components"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.6.12"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=http://download.savannah.nongnu.org/releases/oath-toolkit/oath-toolkit-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=cafdf739b1ec4b276441c6aedae6411434bbd870071f66154b909cc6e2d9e8ba
 TERMUX_PKG_DEPENDS="libxml2, xmlsec"
@@ -13,10 +13,13 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-pam"
 
 termux_step_post_configure() {
 	# Fix out-of-tree build
-	local _gdoc="./libpskc/man/gdoc"
-	if [ ! -e "${_gdoc}" ]; then
-		ln -sf "$TERMUX_PKG_SRCDIR/libpskc/man/gdoc" "${_gdoc}"
-	fi
+	local _dir _gdoc
+	for _dir in libpskc liboath; do
+		_gdoc="./${_dir}/man/gdoc"
+		if [ ! -e "${_gdoc}" ]; then
+			ln -sf "$TERMUX_PKG_SRCDIR/${_dir}/man/gdoc" "${_gdoc}"
+		fi
+	done
 
 	# Avoid overlinking
 	sed -i 's/ -shared / -Wl,--as-needed\0/g' ./libpskc/libtool
