aba8267beed4b1b95c7c8110d561ba53f76151eb
Pankaj Kumar <pankaj.pal9@gmail.com>
2026-09-21 17:26:35 +0530

    revbump(x11/libfm): avoid file conflict with libfm-extra-static
    Closes #31673.

--- x11-packages/libfm/build.sh
+++ x11-packages/libfm/build.sh
@@ -3,11 +3,17 @@ TERMUX_PKG_DESCRIPTION="Library for file management"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.3.2
-TERMUX_PKG_REVISION=6
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/pcmanfm/libfm-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=a5042630304cf8e5d8cff9d565c6bd546f228b48c960153ed366a34e87cad1e5
 TERMUX_PKG_DEPENDS="atk, glib, gtk3, libandroid-support, libcairo, libexif, libffi, menu-cache, pango, pcre"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-gtk=3"
+
+# Avoid conflict with libfm-extra-static which already ships these
+TERMUX_PKG_RM_AFTER_INSTALL="
+lib/libfm-extra.a
+lib/libfm-extra.la
+"
 
 TERMUX_PKG_CONFLICTS="libfm-extra"
 TERMUX_PKG_REPLACES="libfm-extra"
