31014a5f3eac89f238d8efbda7e4ae9d275fbb38
SjnExe <muhammedshajahan100@gmail.com>
2026-09-21 11:54:41 +0530

    feat(scripts/buildorder): support alternate dependencies

--- scripts/buildorder.py
+++ scripts/buildorder.py
@@ -46,18 +46,10 @@ def parse_build_file_dependencies_with_vars(path, vars):
             if line.startswith(vars):
                 dependencies_string = remove_nl_and_quotes(line.split('DEPENDS=')[1])
 
-                # Split also on '|' to dependencies with '|', as in 'nodejs | nodejs-current':
-                for dependency_value in re.split(',|\\|', dependencies_string):
-                    # Replace parenthesis to ignore version qualifiers as in "gcc (>= 5.0)":
-                    dependency_value = re.sub(r'\(.*?\)', '', dependency_value).strip()
-                    arch = os.getenv('TERMUX_ARCH')
-                    if arch is None:
-                        arch = 'aarch64'
-                    if arch == "x86_64":
-                        arch = "x86-64"
-                    dependency_value = re.sub(r'\${TERMUX_ARCH/_/-}', arch, dependency_value)
-
-                    dependencies.append(dependency_value)
+                arch = termux_arch.replace('_', '-')
+                dependencies_string = re.sub(r"\${TERMUX_ARCH/_/-}", arch, dependencies_string)
+                dependencies_string = re.sub(r"\(.*?\)", "", dependencies_string)
+                dependencies.extend(tuple(re.findall(r"[\w+.-]+", d)) if "|" in d else d.strip() for d in dependencies_string.split(",") if d.strip())
 
     return set(dependencies)
 
@@ -220,6 +212,22 @@ class TermuxSubPackage:
                 result += [dependency_package]
         return unique_everseen(result)
 
+def resolve_package_dependencies(all_packages, pkgs_map):
+    "Resolve alternative dependencies for the target architecture."
+    for pkg in all_packages:
+        resolved_deps = set()
+        for dep in pkg.deps:
+            if isinstance(dep, tuple):
+                resolved_dep = next((alt for alt in dep if alt in pkgs_map), None)
+                if not resolved_dep:
+                    die('Package %s depends on non-existing package(s) "%s"' % (pkg.name, ' | '.join(dep)))
+                resolved_deps.add(resolved_dep)
+            else:
+                if dep not in pkgs_map:
+                    die('Package %s depends on non-existing package "%s"' % (pkg.name, dep))
+                resolved_deps.add(dep)
+        pkg.deps = resolved_deps
+
 def read_packages_from_directories(directories, fast_build_mode, full_buildmode):
     """Construct a map from package name to TermuxPackage.
     Subpackages are mapped to the parent package if fast_build_mode is false."""
@@ -261,10 +269,10 @@ def read_packages_from_directories(directories, fast_build_mode, full_buildmode)
                         pkgs_map[subpkg.name] = new_package
                     all_packages.append(subpkg)
 
+    resolve_package_dependencies(all_packages, pkgs_map)
+
     for pkg in all_packages:
         for dependency_name in pkg.deps:
-            if dependency_name not in pkgs_map:
-                die('Package %s depends on non-existing package "%s"' % (pkg.name, dependency_name))
             dep_pkg = pkgs_map[dependency_name]
             if fast_build_mode or not isinstance(pkg, TermuxSubPackage):
                 dep_pkg.needed_by.add(pkg)
