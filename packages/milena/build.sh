08c0f543514171df027f34b05b16d1044ac23e3b
46Neon <j7942281@gmail.com>
2026-09-20 21:44:11 -0700

    Describe Milena as the canonical data-analysis language

--- packages/milena/build.sh
+++ packages/milena/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/46Neon/Milena
-TERMUX_PKG_DESCRIPTION="Milena SST data analysis language"
+TERMUX_PKG_DESCRIPTION="Milena programming language for data analysis"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Milena contributors"
 TERMUX_PKG_VERSION=0.1.1
