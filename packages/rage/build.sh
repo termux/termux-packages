8ec91157c9a348dbfe24e03cd8702484de5759b5
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-23 00:28:54 +0600

    bump(main/rage): 0.12.0

--- packages/rage/build.sh
+++ packages/rage/build.sh
@@ -3,15 +3,19 @@ TERMUX_PKG_DESCRIPTION="A simple, secure and modern encryption tool"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE-APACHE, LICENSE-MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.11.2"
-TERMUX_PKG_SRCURL=https://github.com/str4d/rage/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a8e5c57f131683f86957af7fddeccf61a3882383dd24e752bab3855f570c086c
+TERMUX_PKG_VERSION="0.12.0"
+TERMUX_PKG_SRCURL="https://github.com/str4d/rage/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=fb5255ff9faa944a15a128f812499cd5c08e6155790e284af34c914cbc3d0103
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_make() {
-	termux_setup_rust
+termux_step_pre_configure() {
+	rm -f rust-toolchain.toml rust-toolchain
 
+	termux_setup_rust
+}
+
+termux_step_make() {
 	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
