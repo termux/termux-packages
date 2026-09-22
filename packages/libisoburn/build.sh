a39baa84e52581f46254de304972bb1bd023b190
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-22 15:49:02 +0300

    fix(main/libisoburn): use release index for auto updates [no ci]

--- packages/libisoburn/build.sh
+++ packages/libisoburn/build.sh
@@ -11,5 +11,14 @@ TERMUX_PKG_CONFLICTS="xorriso"
 TERMUX_PKG_BREAKS="libisoburn-dev"
 TERMUX_PKG_REPLACES="libisoburn-dev"
 
+termux_pkg_auto_update() {
+	local url="https://files.libburnia-project.org/releases/" latest_version
+	latest_version="$(curl --fail --silent --show-error --location --retry 5 "$url" | sed -rn 's|.*libisoburn-([0-9]+(\.[0-9]+)+(\.pl[0-9]+)?)\.tar\.gz.*|\1|p' | sort -Vr | head -n1)"
+	if [[ -z "$latest_version" ]]; then
+		termux_error_exit "Unable to get the latest libisoburn version."
+	fi
+	termux_pkg_upgrade_version "$latest_version"
+}
+
 # We don't have tk.
 TERMUX_PKG_RM_AFTER_INSTALL="bin/xorriso-tcltk"
