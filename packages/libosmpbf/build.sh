08d5c78cf9ceb47535418cd39442a64d00511fac
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-23 14:52:50 +0300

    bump(main/libosmpbf): 1.8.0
    Regenerate protoc-fix.patch against 1.8.0's restructured CMakeLists.txt.

--- packages/libosmpbf/build.sh
+++ packages/libosmpbf/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/openstreetmap/OSM-binary/
 TERMUX_PKG_DESCRIPTION="osmpbf is a Java/C library to read and write OpenStreetMap PBF files"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.5.1"
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_VERSION="1.8.0"
 TERMUX_PKG_SRCURL="https://github.com/openstreetmap/OSM-binary/archive/refs/tags/v${TERMUX_PKG_VERSION}.zip"
-TERMUX_PKG_SHA256=edd98ba252c81372113747a131466f5ba45e0cff97b597e863a26e83943ba84f
+TERMUX_PKG_SHA256=72d4a6abe7d98bd02febea5459477988862164d861b8c0163ba87f88cc034bf0
 TERMUX_PKG_DEPENDS="libc++, libprotobuf"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_GROUPS="science"
 
 termux_step_pre_configure() {
