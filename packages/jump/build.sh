fb6ea9bebfac3d515e88a0e4dce0e8ab8a1b8af8
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-23 14:50:30 +0300

    bump(main/jump): 0.69.0

--- packages/jump/build.sh
+++ packages/jump/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/gsamokovarov/jump
 TERMUX_PKG_DESCRIPTION="Jump helps you navigate in shell faster by learning your habits"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.51.0
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=0.69.0
 TERMUX_PKG_SRCURL=https://github.com/gsamokovarov/jump/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ce297cada71e1dca33cd7759e55b28518d2bf317cdced1f3b3f79f40fa1958b5
+TERMUX_PKG_SHA256=17567f7acd305e2e8093f49e591aa03d74bc5c94204b0461550b0bd8055490af
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
