fba84cebea294bdd39c44f3d566f8d7f5ef0385c
Biswapriyo Nath <nathbappai@gmail.com>
2026-09-23 15:27:32 +0000

    bump(main/nzbget): 26.3

--- packages/nzbget/build.sh
+++ packages/nzbget/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="The most efficient usenet downloader"
 # License: GPL-2.0-with-OpenSSL-exception
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="26.1"
+TERMUX_PKG_VERSION="26.3"
 TERMUX_PKG_SRCURL="https://github.com/nzbgetcom/nzbget/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=7ed0940f18635986f096d666e5e0b737ef8ae362e4a538b46a507bacb61b09ed
+TERMUX_PKG_SHA256=3a6a2a7c8bf5e7156dacb563e76d65df4aa53390e24040115c4406771ec3a19b
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="boost, libc++, libxml2, ncurses, openssl, 7zip, zlib"
 TERMUX_PKG_RECOMMENDS="unrar"
