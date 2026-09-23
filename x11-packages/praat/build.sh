d8ce20680bd29847ca5cf6942a567dc61384421f
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-23 09:21:26 +0300

    bump(x11/praat): 7.0.02

--- x11-packages/praat/build.sh
+++ x11-packages/praat/build.sh
@@ -2,15 +2,24 @@ TERMUX_PKG_HOMEPAGE=https://www.praat.org
 TERMUX_PKG_DESCRIPTION="Doing phonetics by computer"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="6.4.65"
+TERMUX_PKG_VERSION="7.0.02"
 TERMUX_PKG_SRCURL=https://github.com/praat/praat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=bb3f9c520f93f4786903d59d7f64acdc4704d40ed88b4bbfd52ec75f07be6182
+TERMUX_PKG_SHA256=6d97d6b97b673df2cd46a96338390a910c76d930627fb96af0b05892d46ede2c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libc++, libcairo, pango, pulseaudio, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
 	rm -f meson.build
+	sed -i 's/ -no-pie//g' Makefile
+}
+
+termux_step_pre_configure() {
+	export PRAAT_AUDIO=pulseaudio
+}
+
+termux_step_make() {
+	make AR="$AR" CC="$CC" CXX="$CXX" LINKER_COMMAND="$CXX"
 }
 
 termux_step_make_install() {
