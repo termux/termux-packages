95e001ca051321ff9921493403cc544f95c35426
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-23 16:42:58 +0300

    bump(main/ravencoin): 4.8.0

--- packages/ravencoin/build.sh
+++ packages/ravencoin/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://ravencoin.org/
 TERMUX_PKG_DESCRIPTION="A peer-to-peer blockchain, handling the efficient creation and transfer of assets from one party to another"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="4.6.1"
-TERMUX_PKG_REVISION=10
+TERMUX_PKG_VERSION="4.8.0"
 TERMUX_PKG_SRCURL="https://github.com/RavenProject/Ravencoin/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=42e8444e9e21eecfed1a546dffe6f2418271e890038a7d9d9a856b376a6284e8
+TERMUX_PKG_SHA256=9753c290cd42ba127f95f736d4ef478ed72f3843e54ff4f11d09aaa9512b7e8d
 TERMUX_PKG_DEPENDS="boost, libc++, libevent, libzmq, miniupnpc, openssl"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="boost-headers, libdb"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-tests
