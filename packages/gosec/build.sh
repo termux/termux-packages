bdda1ebdada6409927802c05633af8a21d0e85fe
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-24 11:59:05 +0600

    refactor(main/gosec): remove unnecessary license file's path
    %ci:no-build

--- packages/gosec/build.sh
+++ packages/gosec/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/securego/gosec
 TERMUX_PKG_DESCRIPTION="Golang security checker"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_LICENSE_FILE="LICENSE.txt"
 TERMUX_PKG_MAINTAINER="Gouranga Das Samrat <gouranga.das.khulna@gmail.com>"
 TERMUX_PKG_VERSION="2.29.0"
-TERMUX_PKG_SRCURL=https://github.com/securego/gosec/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL="https://github.com/securego/gosec/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=083422c2d64f311062e7fe36ff1bd22c98b029f0a4d69f3e81fd0a4724139092
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
