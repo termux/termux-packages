0dc95a1d41e69f00b0ae93ec8ab0807559f696e5
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 08:15:26 +0300

    bump(main/libmspub): 0.1.5

--- packages/libmspub/build.sh
+++ packages/libmspub/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://wiki.documentfoundation.org/DLP/Libraries/libcdr
 TERMUX_PKG_DESCRIPTION="Microsoft Publisher file format parser library (latest snapshot)"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.1.4"
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION="0.1.5"
 TERMUX_PKG_SRCURL="https://dev-www.libreoffice.org/src/libmspub/libmspub-${TERMUX_PKG_VERSION}.tar.xz"
-TERMUX_PKG_SHA256=ef36c1a1aabb2ba3b0bedaaafe717bf4480be2ba8de6f3894be5fd3702b013ba
+TERMUX_PKG_SHA256=3671095f5a10bee8a755052a30576952c5b16d8b0f2ba9f2fb998338c18cb119
 TERMUX_PKG_DEPENDS="libwpd, libicu, libxml2, librevenge, zlib"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="boost, libwpg"
 
 termux_step_pre_configure() {
