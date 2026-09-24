865b786f4012543f9b0057522c0cf90d600cd51a
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 08:15:26 +0300

    bump(main/libmwaw): 0.3.23

--- packages/libmwaw/build.sh
+++ packages/libmwaw/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE="https://sourceforge.net/projects/libmwaw/"
 TERMUX_PKG_DESCRIPTION="Import library for some old mac text documents."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.3.22
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION=0.3.23
 TERMUX_PKG_SRCURL=https://sourceforge.net/projects/libmwaw/files/libmwaw/libmwaw-${TERMUX_PKG_VERSION}/libmwaw-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a1a39ffcea3ff2a7a7aae0c23877ddf4918b554bf82b0de5d7ce8e7f61ea8e32
+TERMUX_PKG_SHA256=ac3590f691a2904eb8c7dc8b757b8a29f125f592449e421459ae8fa928b399eb
 TERMUX_PKG_DEPENDS="doxygen, librevenge"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="librevenge"
 
 termux_step_pre_configure() {
