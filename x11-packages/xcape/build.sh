a739bb3f6c2b9a8cb98c36508a006ca55c7d93af
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 18:33:41 +0300

    fix(x11/xcape): Restore source download [no ci]

--- x11-packages/xcape/build.sh
+++ x11-packages/xcape/build.sh
@@ -4,9 +4,9 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/alols/xcape/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/x/xcape/xcape_${TERMUX_PKG_VERSION}.orig.tar.gz # Original repository is no longer available.
 TERMUX_PKG_SHA256=a27ed884fd94f03058af65a39edfe3af3f2f8fbb76ba9920002a76be07fb2821
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BUILD_DEPENDS="libx11,libxtst"
 TERMUX_PKG_EXTRA_MAKE_ARGS="PREFIX=$TERMUX_PREFIX"
