22073f1feb8da57e5b4fae19433122a6bc2876de
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 18:16:06 +0300

    bump(main/unicode-cldr): 48.2

--- packages/unicode-cldr/build.sh
+++ packages/unicode-cldr/build.sh
@@ -3,9 +3,10 @@ TERMUX_PKG_DESCRIPTION="Unicode Common Locale Data Repository"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="48.1"
+TERMUX_PKG_VERSION="48.2"
 TERMUX_PKG_SRCURL="https://unicode.org/Public/cldr/$TERMUX_PKG_VERSION/cldr-common-$TERMUX_PKG_VERSION.zip"
-TERMUX_PKG_SHA256=c691e94f217486fed259871429ae71cc4855e8a7f22b1586eb6eb52262c9c307
+TERMUX_PKG_SHA256=d2844f9dbf6124d11a7b047f5381a467902d82a673be3d658f4c0791ffa0b83b
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 # Extract like libncnn
@@ -13,6 +14,16 @@ termux_extract_src_archive() {
 	local file="$TERMUX_PKG_CACHEDIR/$(basename "$TERMUX_PKG_SRCURL")"
 	mkdir -p "$TERMUX_PKG_SRCDIR"
 	unzip -q "$file" -d "$TERMUX_PKG_SRCDIR"
+}
+
+termux_pkg_auto_update() {
+	local latest_version
+	latest_version="$(termux_repology_api_get_latest_version "${TERMUX_PKG_NAME}")"
+	if [[ "${latest_version}" == "null" ]] || [[ ! "${latest_version}" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+		echo "INFO: Already up to date."
+		return 0
+	fi
+	termux_pkg_upgrade_version "${latest_version}"
 }
 
 termux_step_make_install() {
