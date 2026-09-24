426ddab482843e7f82433a1d8a4fe3be63cede59
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 22:37:08 +0300

    chore(tree-wide): Force-disable updates for packages with explicitly frozen versions [no ci]
    Unset TERMUX_PKG_AUTO_UPDATE is treated as opted-in when PACKAGES_OPT_OUT=true.

--- packages/libpangomm-1.4/build.sh
+++ packages/libpangomm-1.4/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.46.5"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/pangomm/${TERMUX_PKG_VERSION%.*}/pangomm-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=38ca0b050b065de4e3da0c182df657437757063bbf0c4b6c9567ddba019b1d68
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="glib, libc++, libcairomm-1.0, libglibmm-2.4, libsigc++-2.0, pango"
 
 termux_step_post_massage() {
