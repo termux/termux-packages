6303d26d44b67e97756cb5248a1a2fc63b10797a
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-24 12:52:05 +0600

    refactor(main/locustdb): remove unnecessary license file's path %ci:no-build

--- packages/locustdb/build.sh
+++ packages/locustdb/build.sh
@@ -1,24 +1,13 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/cswinter/LocustDB
 TERMUX_PKG_DESCRIPTION="An experimental analytics database"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.4.6"
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://github.com/cswinter/LocustDB/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL="https://github.com/cswinter/LocustDB/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=db1ee671dab19c1226a0c2d56007fd186b5c6b2ce230b9c1775bba20e19d8c28
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-
-# ```
-# error: this arithmetic operation will overflow
-#    --> src/locustdb.rs:189:36
-#     |
-# 189 |             mem_size_limit_tables: 8 * 1024 * 1024 * 1024, // 8 GiB
-#     |                                    ^^^^^^^^^^^^^^^^^^^^^^ attempt to multiply with overflow
-#     |
-#     = note: `#[deny(arithmetic_overflow)]` on by default
-# ```
 TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
 
 # Github CI seems pretty upset
