0b0ae7d1d5f2cc05aa78a0814e1d05f2ecb48279
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 08:05:54 +0300

    bump(x11/bochs): 3.1

--- x11-packages/bochs/build.sh
+++ x11-packages/bochs/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://bochs.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="Bochs is a highly portable open source IA-32 (x86) PC emulator and debugger written in C++."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.0"
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_VERSION="3.1"
 TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/bochs/bochs-${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=cb6f542b51f35a2cc9206b2a980db5602b7cd1b7cf2e4ed4f116acd5507781aa
+TERMUX_PKG_SHA256=14aaf78dbe1337987923fffc4e7a962ae56abcf9a87474ace39e593f9f84ee84
 TERMUX_PKG_DEPENDS="glib, gtk3, libc++, libx11, libxpm, libxrandr, ncurses, pango, readline"
+TERMUX_PKG_AUTO_UPDATE=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_strtouq=no
