8ed3c4dbbcea4feb1a2d27548166924fcdc8a4ff
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 20:24:26 +0300

    revbump(main/vulkan-loader-generic): re-publish 1.4.363

--- packages/vulkan-loader-generic/build.sh
+++ packages/vulkan-loader-generic/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Vulkan Loader"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.4.363"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=cc520304a4d8de9a6a1699553d6090aef0c25f87ef6cbc16b7c84ebd99093264
 TERMUX_PKG_BUILD_DEPENDS="vulkan-headers (=${TERMUX_PKG_VERSION}), libx11, libxcb, libxrandr"
