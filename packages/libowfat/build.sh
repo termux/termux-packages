1f855b609c3a92e1264e0336a0e7a59f4dcc122b
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-24 20:55:08 +0300

    bump(main/libowfat): 0.34

--- packages/libowfat/build.sh
+++ packages/libowfat/build.sh
@@ -2,10 +2,11 @@ TERMUX_PKG_HOMEPAGE=http://www.fefe.de/libowfat/
 TERMUX_PKG_DESCRIPTION="GPL reimplementation of libdjb"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.33
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://www.fefe.de/libowfat/libowfat-$TERMUX_PKG_VERSION.tar.xz
-TERMUX_PKG_SHA256=311ec8b3f4b72bb442e323fb013a98f956fa745547f2bc9456287b20d027cd7d
+TERMUX_PKG_VERSION=0.34
+# Official CVS (cvs.fefe.de) is still reachable, but last commit was 2025-05.
+TERMUX_PKG_SRCURL=https://web.archive.org/web/20251124195235/http://www.fefe.de/libowfat/libowfat-$TERMUX_PKG_VERSION.tar.xz # original site is down
+TERMUX_PKG_SHA256=d4330d373ac9581b397bc24a22ad1f7f5d58a7fe36d9d239fe352ceffc5d304b
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="
@@ -19,4 +20,9 @@ termux_step_pre_configure() {
 	# Use pregenerated entities.h.
 	cp $TERMUX_PKG_BUILDER_DIR/entities.h $TERMUX_PKG_BUILDDIR/
 	touch -d "next hour" $TERMUX_PKG_BUILDDIR/entities.h
+
+	# GNUmakefile's compile_commands.json target runs the cross-compiled
+	# json tool, which can't execute in the build container. Makefile is
+	# the release-ready file with that target stripped.
+	rm -f $TERMUX_PKG_BUILDDIR/GNUmakefile
 }
