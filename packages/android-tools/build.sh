5eccfdafb798733d70000bf2be5ac9f99ca73e2a
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-09-25 08:33:44 +0300

    fix(android-tools): bypass fwmarkd hook for local adb

--- packages/android-tools/build.sh
+++ packages/android-tools/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0, BSD 2-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE, vendor/core/fastboot/LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="37.0.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/nmeum/android-tools/releases/download/${TERMUX_PKG_VERSION#*really}/android-tools-${TERMUX_PKG_VERSION#*really}.tar.xz"
 TERMUX_PKG_SHA256=2725d09f892a3a38e534429f47a321f58ecf6a3169caa42c915fb2cb7d46be0e
 TERMUX_PKG_AUTO_UPDATE=true
