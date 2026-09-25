f7c4d0a9466a64c4bad36ae7e635c3413033d48e
Biswapriyo Nath <nathbappai@gmail.com>
2026-09-25 04:38:01 +0000

    bump(main/httrack): 3.50.4
    Disable multipath TCP option to fix the following error.
    htslib.c:2429:23: error: use of undeclared identifier 'SOL_MPTCP'

--- packages/httrack/build.sh
+++ packages/httrack/build.sh
@@ -2,14 +2,15 @@ TERMUX_PKG_HOMEPAGE=http://www.httrack.com
 TERMUX_PKG_DESCRIPTION="It allows you to download a World Wide Web site from the Internet"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.50.3"
+TERMUX_PKG_VERSION="3.50.4"
 TERMUX_PKG_SRCURL=https://github.com/xroche/httrack/releases/download/${TERMUX_PKG_VERSION}/httrack-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=644d4ec0e48ad596dacd7f8017b68d8a3f1dfc140284b412b53086e7d1664e9d
+TERMUX_PKG_SHA256=f97dbb96d110681b4349912c8bc5c4011a6c227a7d4294ea1d4f0093baea51b6
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="httrack-data, libandroid-execinfo, libiconv, openssl, zlib"
 TERMUX_PKG_BREAKS="httrack-dev"
 TERMUX_PKG_REPLACES="httrack-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-mptcp
 --docdir=$TERMUX_PREFIX/share/httrack
 --with-zlib=$TERMUX_PREFIX
 LIBS=-liconv
