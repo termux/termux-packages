3ecb35cf699c518b530c1758abce87ac77a87463
Biswapriyo Nath <nathbappai@gmail.com>
2026-09-25 05:03:07 +0000

    bump(main/libuv): 1.53.0

--- packages/libuv/build.sh
+++ packages/libuv/build.sh
@@ -3,14 +3,16 @@ TERMUX_PKG_DESCRIPTION="Support library with a focus on asynchronous I/O"
 TERMUX_PKG_LICENSE="MIT, BSD 2-Clause, ISC, BSD 3-Clause"
 TERMUX_PKG_LICENSE_FILE="LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.52.1"
+TERMUX_PKG_VERSION="1.53.0"
 TERMUX_PKG_SRCURL=https://dist.libuv.org/dist/v${TERMUX_PKG_VERSION}/libuv-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=66d511b9e6e334c0e62279eb234fbfb2b3110b1479c09b95b44c7afca8cff9e7
+TERMUX_PKG_SHA256=cb0d6dd2128d5a95bd242c6cc982a24fe608fa93da57b6b4ec763b0018c53e64
+TERMUX_PKG_DEPENDS="libandroid-spawn"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BREAKS="libuv-dev"
 TERMUX_PKG_REPLACES="libuv-dev"
 
 termux_step_pre_configure() {
 	export PLATFORM=android
+	LDFLAGS+=" -landroid-spawn"
 	sh autogen.sh
 }
